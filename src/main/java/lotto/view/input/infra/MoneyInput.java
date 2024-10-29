1e483c08bbf6e442882161325dfcf9c587dc23e3
EHCTH <lees90896@gmail.com>
2024-10-29 17:30:01 +0900

    feat(MoneyInput): InputService 를 구현하는 MoneyInput 작성

--- src/main/java/lotto/view/input/infra/MoneyInput.java
+++ src/main/java/lotto/view/input/infra/MoneyInput.java
@@ -1,4 +1,19 @@
 package lotto.view.input.infra;
 
-public class MoneyInput {
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.input.domain.InputService;
+import lotto.view.output.domain.ViewService;
+
+public class MoneyInput implements InputService {
+    private final ViewService viewService;
+    public MoneyInput(ViewService viewService) {
+        this.viewService = viewService;
+    }
+    @Override
+    public String input() {
+        viewService.view();
+        String input = Console.readLine();
+        // TODO 예 외 처 리
+        return input;
+    }
 }
