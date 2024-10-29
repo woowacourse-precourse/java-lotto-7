2945a292b63c078dfd0763cbf9a3c5295c71597b
Firedrago95 <yonghwa95@naver.com>
2024-10-29 16:49:47 +0900

    Feat(OutputView): 생성된 로또 번호 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printLottoCount(int count) {
         System.out.printf("%d개를 구매했습니다.", count);
+    }
+
+    public static void printLottoNumbers(List<String> convertedNumbers) {
+        convertedNumbers.stream()
+            .forEach(System.out::println);
     }
 }
