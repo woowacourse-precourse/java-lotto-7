b39b40a41d086fd0d9c8ab367eb3b4a310fff82c
SeongbinPark <hyucse22@gmail.com>
2024-10-29 15:45:44 +0900

    feat(Application): 구입 횟수 만큼 로또 구매

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,5 +8,10 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         int amount = Integer.parseInt(input);
+
+        // 구입 횟수 만큼 로또 구매
+        int count = amount / 1000;
+        System.out.println();
+        System.out.println(count + "개를 구매했습니다.");
     }
 }
