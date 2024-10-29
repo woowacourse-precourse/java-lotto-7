2cf62297df3a1318170c542decb8cc5ddaa61750
jae0000 <jyyp3395@gmail.com>
2024-10-29 17:45:28 +0900

    README.md 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String amountPaid = Console.readLine();
+
+
+
+
+
+
+
     }
 }
