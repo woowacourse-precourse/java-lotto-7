23da3d0e8d7778807af815ba42ecfe01b557d58b
Etwashoeren <sungmin0609@nate.com>
2024-10-30 01:19:58 +0900

    feat. Rank 클래스 추가 및 우승 상금 필드 구현

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,7 +1,35 @@
 package lotto;
 
-public enum Rank {
+import java.util.List;
 
+public enum Rank {
+    FIFTH(3, false, "5,000원"),                 // 3개 일치
+    FOURTH(4, false, "50,000원"),               // 4개 일치
+    THIRD(5, false,  "1,500,000원"),             // 5개 일치
+    SECOND(5, true, "30,000,000원"),           // 5개 일치 + 보너스 번호
+    FIRST(6, false, "2,000,000,000원");         // 6개 일치
+
+    int balls;
+    String winningPrize;
+    boolean bonus;
+
+    Rank(int balls, boolean bonus, String winningPrize) {
+        this.balls = balls;
+        this.bonus = bonus;
+        this.winningPrize = winningPrize;
+    }
+
+    int getBalls() {
+        return balls;
+    }
+
+    boolean getBonus() {
+        return bonus;
+    }
+
+    String getWinningPrize() {
+        return winningPrize;
+    }
 }
 
 
