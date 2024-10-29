a82072f5629fe9e5cace5c25cfcaa7230dbe8998
kssk3 <kss8014@daum.net>
2024-10-29 20:53:27 +0900

    Docs amount

--- src/main/java/lotto/Amount.java
+++ src/main/java/lotto/Amount.java
@@ -2,17 +2,31 @@ package lotto;
 
 public class Amount {
     private static final String REQUEST_POSITIVE_NUMBER_MESSAGE = "양수를 입력해주세요.";
+    private final int originalAmount;
     private int amount;
 
     public Amount(int amount) {
         checkAmountPositive(amount);
         this.amount = amount;
+        this.originalAmount = amount;
     }
 
     private void checkAmountPositive(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException(REQUEST_POSITIVE_NUMBER_MESSAGE);
         }
+    }
+
+    public void decrease(int amount) {
+        this.amount -= amount;
+    }
+
+    public boolean isPurchasable(int amount) {
+        return this.amount >= amount;
+    }
+
+    public int getOriginalAmount() {
+        return originalAmount;
     }
 
 }
