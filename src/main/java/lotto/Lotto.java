c96120e15f1c02bef3c3137b85d49a1a5545c811
msk226 <kminseok5167@gmail.com>
2024-10-29 16:04:29 +0900

    fix(model): Lotto 번호 갯수 검증 로직 메서드 추출

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,6 +11,10 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkLottoNumbersSize(numbers);
+    }
+
+    private void checkLottoNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
