67335ef64342d58927932ba7cb06c990b60844bd
tjfwltn <tjfwltn1234@gmail.com>
2024-10-29 22:45:09 +0900

    feat : 로또의 숫자 범위 유효성 검증 기능 추가
    - Lotto 클래스에 validateInRange() 메서드 추가
    	- 생성자에서 validateInRange()를 수행하게 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,10 +3,14 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateInRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateInRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+        });
+    }
 }
