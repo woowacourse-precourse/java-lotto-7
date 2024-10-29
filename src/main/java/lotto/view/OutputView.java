4cfb2c8ab02359759977efae981082cb877951b6
Yoon-Jemin <yjm1800@naver.com>
2024-10-29 17:55:10 +0900

    feat(view): 메시지를 출력해주기 위한 OutputView 클래스 추가
    - 구입금액 입력 메시지를 출력하기 위한 printPriceInputMessage() 메서드 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import static lotto.constant.InputMessage.PRICE_INPUT_MESSAGE;
+
+public class OutputView {
+
+    public void printPriceInputMessage(){
+        System.out.println(PRICE_INPUT_MESSAGE.getMessage());
+    }
+}
