c273e3b0ccb61126ec7d7b3fa5571b1237916e0c
msk226 <kminseok5167@gmail.com>
2024-10-29 18:31:07 +0900

    feat(model): LottoMachine 클래스 Lotto 번호 생성 로직 구현

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoMachine {
 
@@ -21,9 +23,19 @@ public class LottoMachine {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
     }
 
-    private Integer countLotto(int price){
+    private Integer countLotto(Integer price){
         return price / 1000;
     }
 
+    private List<Lotto> generateLotto(Integer count){
+        for (int i = 0; i < count; i++){
+            lottos.add(createRandomLotto());
+        }
+        return this.lottos;
+    }
+
+    private Lotto createRandomLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
 
 }
