487248743061ffbddf1a2e28d1dbaaa3fb22844b
YongLeeCode <yonghwan311@gmail.com>
2024-10-30 00:01:43 +0900

    feat: 당첨 결과 클래스 선언

---
+++ src/main/java/lotto/MatchResult.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import java.util.List;
+
+// TicketPrizeMatcher에 대한 당첨 결과를 담는 클래스
+public class MatchResult {
+    private final List<MatchCondition> matchedConditions;
+    private final Double profitRatio;
+
+    public MatchResult(
+            List<MatchCondition> matchedConditions,
+            Double profitRatio
+    ) {
+        this.matchedConditions = matchedConditions;
+        this.profitRatio = profitRatio;
+    }
+}
