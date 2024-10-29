b7f5dbce14eec3f082cbd6b52dfc54e321fcc16c
mohyerolo <garam2101@naver.com>
2024-10-29 21:22:26 +0900

    test: 구입 금액 관련 테스트 추가

---
+++ src/test/java/lotto/domain/PurchasePriceTest.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchasePriceTest {
+
+    private static final String UNIT = "1000";
+    private static final String BIGGER_THAN_ZERO = "0원보다 커야됩니다";
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 10000, 2000})
+    void 객체생성_성공(int input) {
+        PurchasePrice purchasePrice = new PurchasePrice(input);
+        Assertions.assertThat(purchasePrice.getPurchasePrice()).isEqualTo(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void 구입금액이_양수가_아님_실패(int price) {
+        assertThatThrownBy(() -> new PurchasePrice(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BIGGER_THAN_ZERO);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 1024, 9999, 10500})
+    void 구입금액이_1000단위가_아님_실패(int price) {
+        assertThatThrownBy(() -> new PurchasePrice(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(UNIT);
+    }
+
+}
---
+++ src/test/java/lotto/service/InputServiceTest.java
@@ -0,0 +1,34 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.PurchasePrice;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+
+class InputServiceTest {
+
+    private final InputService inputService = new InputService(new InputView(), new OutputView());
+
+    @AfterEach
+    void closeConsole() {
+        Console.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "10000"})
+    void 구입_금액_입력_성공(String input) {
+        setInput(input);
+        PurchasePrice purchasePrice = inputService.parsePurchasePrice();
+        Assertions.assertThat(purchasePrice.getPurchasePrice()).isEqualTo(Integer.parseInt(input));
+    }
+
+    private void setInput(final String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
