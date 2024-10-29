1e6291fa7ec94870b48a291220c7572158874c4f
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-29 19:39:45 +0900

    chore(Application) : 상수 선언

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final String LOTTO_AMOUNT_PHRASE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_COUNT_PHRASE = "개를 구매했습니다.";
+    public static final String LOTTO_NUMBER_INPUT = "당첨 번호를 입력해 주세요.";
+    public static final String LOTTO_BONUS_INPUT = "보너스 번호를 입력해 주세요.";
+    public static final String WIN_STATICS = "당첨 통계\n---";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+
 }
