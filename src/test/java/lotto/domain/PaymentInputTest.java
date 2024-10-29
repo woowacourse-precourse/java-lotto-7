e353e4fb9406e76cb6ac98484e0ee44b82624824
dnwldla <leeemjw@naver.com>
2024-10-30 08:10:16 +0900

    test: 구입금액이 숫자가 아닐 때 예외가 발생하는 테스트 추가

---
+++ src/test/java/lotto/domain/PaymentInputTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.common.ErrorMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PaymentInputTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"+1","-1",".!3","1.5"})
+    void 숫자가_아닌_값을_입력하면_예외_발생(String input){
+        assertThatThrownBy(() -> new PaymentInput(input)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining(ErrorMessage.NOT_INTEGER_INPUT);
+    }
+
+}
