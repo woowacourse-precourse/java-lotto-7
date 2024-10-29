4826bf37cd75a03cd71ca866faa6dc3790317d26
msk226 <kminseok5167@gmail.com>
2024-10-29 16:25:04 +0900

    feat(model): BonusNumber 범위 검증 로직 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -4,6 +4,16 @@ public class BonusNumber {
     private final Integer bonusNumber;
 
     public BonusNumber(Integer bonusNumber){
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Integer bonusNumber) {
+        checkBonusNumberRange(bonusNumber);
+    }
+
+    private void checkBonusNumberRange(Integer bonusNumber){
+        if (bonusNumber <= 1 || bonusNumber >= 45)
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
 }
