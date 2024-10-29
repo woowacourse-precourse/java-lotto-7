b728fbf1995f16e417cec0e70a327122b408efe5
sunmok <tjsahr9191@naver.com>
2024-10-29 18:23:14 +0900

    feat : 로또 당첨 금액 및 로또 가격 설정

---
+++ src/main/java/lotto/config/LottoGameConfig.java
@@ -0,0 +1,13 @@
+package lotto.config;
+
+public class LottoGameConfig {
+
+    public static int FIRST_PLACE_AMOUNT = 2_000_000_000;
+    public static int SECOND_PLACE_AMOUNT = 30_000_000;
+    public static int THIRD_PLACE_AMOUNT = 1_500_000;
+    public static int FOURTH_PLACE_AMOUNT = 50_000;
+    public static int FIFTH_PLACE_AMOUNT = 5_000;
+
+    public static int LOTTO_PRICE = 1_000;
+
+}
