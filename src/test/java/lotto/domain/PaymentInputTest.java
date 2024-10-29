2dffe362772003324cc5542d4e7c3937952bf06c
dnwldla <leeemjw@naver.com>
2024-10-30 08:38:27 +0900

    test: 금액이 1000으로 나누어떨어지지 않는 경우 예외를 발생시키는 테스트 추가

--- src/test/java/lotto/domain/PaymentInputTest.java
+++ src/test/java/lotto/domain/PaymentInputTest.java
@@ -17,7 +17,6 @@ class PaymentInputTest {
                 hasMessageContaining(ErrorMessage.BLANK_OR_NULL_INPUT);
     }
 
-
     @ParameterizedTest
     @ValueSource(strings = {"+1","-1",".!3","1.5"})
     void 숫자가_아닌_값을_입력하면_예외_발생(String input){
@@ -25,5 +24,15 @@ class PaymentInputTest {
                 isInstanceOf(IllegalArgumentException.class).
                 hasMessageContaining(ErrorMessage.NOT_INTEGER_INPUT);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0","1","50","100","900"})
+    void 입력이_1000으로_나누어떨어지지_않으면_예외_발생(String input){
+        assertThatThrownBy(() -> new PaymentInput(input)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining(ErrorMessage.NOT_UNIT_INPUT);
+    }
+
+
 
 }
