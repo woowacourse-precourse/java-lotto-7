742eef9b4d1ddf8e9abcbf3af4d5ba486f0f9c0e
qyinm <qusseun@gmail.com>
2024-10-29 19:43:17 +0900

    feat: 발행한 로또 번호 값 출력 포맷에 맞게 반환 기능 작성

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTickets {
+
+    private final List<Lotto> lottos;
+
+    public LottoTickets(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    @Override
+    public String toString() {
+        return lottos.stream().map(Lotto::toString).collect(Collectors.joining("\n"));
+    }
+}
