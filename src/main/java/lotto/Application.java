455a1e07d1c5af67b0cd5f96d66aee63cf4cc82f
iamdonghyuk <iamdonghyuk@gmail.com>
2024-10-29 16:42:53 +0900

    feat: validate_division 메서드 추가 - 1000원으로 나누어 떨어지지 않을 경우 에러 발생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,15 @@ package lotto;
 import java.util.List;
 
 public class Application {
+    public static final int one_ticket = 1000;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public void validate_division(int amount){
+        if(amount % one_ticket != 0){
+            throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지지 않습니다. ");
+        }
     }
 
     public class Lotto {
