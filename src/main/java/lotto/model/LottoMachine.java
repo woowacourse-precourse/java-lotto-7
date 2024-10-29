95c75cafe9c8e19fad469346764332fe571df20a
msk226 <kminseok5167@gmail.com>
2024-10-29 18:23:52 +0900

    feat(model): LottoMachine 클래스에 Lotto 갯수 저장할 count 필드 추가

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -6,11 +6,17 @@ import java.util.List;
 public class LottoMachine {
 
     private final Integer price;
+    private final Integer count;
     private final List<Lotto> lottos;
 
     public LottoMachine(Integer price){
         this.price = price;
+        this.count = countLotto(price);
         this.lottos = new ArrayList<>();
+    }
+
+    private Integer countLotto(int price){
+        return price / 1000;
     }
 
 
