06863d670e42f91b4f54ec393a25c6a8b9bae10f
EHCTH <lees90896@gmail.com>
2024-10-29 18:03:56 +0900

    refator(Won): 생성자 private 으로 변환 of 기능 추가

--- src/main/java/lotto/buyer/infrastructure/Won.java
+++ src/main/java/lotto/buyer/infrastructure/Won.java
@@ -1,15 +1,26 @@
 package lotto.buyer.infrastructure;
 
 import lotto.buyer.domain.Money;
+import lotto.buyer.validator.MoneyValidator;
+import lotto.util.Convertor;
 
 public class Won implements Money {
     private final Long money;
-    public Won(String money) {
-        this.money = Long.parseLong(money);
+    private Won(Long money) {
+        this.money = money;
+    }
+    public static Won of(String input) {
+        Long money = Convertor.stringToLong(input);
+        MoneyValidator.validate(money);
+        return new Won(money);
     }
 
     @Override
     public Long getMoney() {
         return money;
+    }
+    @Override
+    public String toString() {
+        return money.toString();
     }
 }
