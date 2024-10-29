9f7294d86cddf9ee2cc3f8e94199f42b8c0beab6
qyinm <qusseun@gmail.com>
2024-10-29 18:51:00 +0900

    feat: 로또 구입 금액 입력 기능 작성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,10 @@
+package lotto.controller;
+
+import lotto.utils.InputView;
+
+public class LottoController {
+
+    public void runLotto() {
+        int lottoPurchaseAmount = InputView.inputLottoPurchaseAmount();
+    }
+}
