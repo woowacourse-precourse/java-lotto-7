8df912d7e91fa4342f0a44bcd4c6f27a0a744a86
Yoon-Jemin <yjm1800@naver.com>
2024-10-30 00:55:46 +0900

    test(util): 가격 입력 메서드를 테스트하기 위한 테스트 코드 작성
    - 1000으로 나누어 떨어지지 않는 경우에 대한 validatePrice 메서드 테스트 코드 추가
    - 정상적인 입력이 주어진 경우에 대한 validatePrice 메서드 테스트 코드 추가

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -43,5 +43,31 @@ public class ValidatorTest {
         assertThat(result).isFalse();
     }
 
+    @Test
+    @DisplayName("입력이 1000원으로 나누어 떨어지지 않은 경우 예외를 발생시킨다")
+    void 금액_입력_예외_테스트_3() throws Exception {
+        // given
+        String price = "20500";
+
+        // when
+        boolean result = validator.validatePrice(price);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("정상적인 입력이 주어진 경우 True를 반환한다")
+    void 정상_금액_입력_테스트() throws Exception {
+        // given
+        String price = "20000";
+
+        // when
+        boolean result = validator.validatePrice(price);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
 
 }
