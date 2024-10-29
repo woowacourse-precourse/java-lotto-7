71af01d0feb37007cc40ef56a2b5be17655c431f
Firedrago95 <yonghwa95@naver.com>
2024-10-29 17:57:52 +0900

    Test(InputValidatorTest): 당첨 번호 검사 테스트 작성

--- src/test/java/lotto/util/validator/InputValidatorTest.java
+++ src/test/java/lotto/util/validator/InputValidatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
@@ -32,5 +34,20 @@ class InputValidatorTest {
             () -> InputValidator.validateMoney(wrongInput));
         Assertions.assertThrows(IllegalArgumentException.class,
             () -> InputValidator.validateMoney(empty));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,46,3,4,5", "-1,2,3,4,5,6", "1;2;3;4;5;6", "1,2,3,4,5,6,7"})
+    void 당첨_번호_입력_검사_예외_테스트(String input) {
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+            () -> InputValidator.validateWinningNumbers(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "11,21,3,4,5,6"})
+    void 당첨_번호_입력_검사_테스트(String input) {
+        //when & then
+        assertDoesNotThrow(() -> InputValidator.validateWinningNumbers(input));
     }
 }
