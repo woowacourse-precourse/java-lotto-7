b586406628644dadb0857bcad543e0e0b56409c2
param <spsummerdoc@gmail.com>
2024-10-29 16:10:46 +0900

    test(lotto): 1000원 단위 금액일 경우 로또를 구매하는 테스트 로직 추가

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,31 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.mock.FakeRandomHolder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoServiceTest {
+    LottoService lottoService = new LottoService(new FakeRandomHolder(List.of(1,2,3,4,5,6)));
+
+    @Test
+    @DisplayName("1000원 단위로 구매하면 로또를 살 수 있다")
+    void purchaseLotto() throws Exception {
+        // given
+        int amount = 2000;
+
+        // when
+        List<Lotto> lottos = lottoService.purchaseLottos(amount);
+
+        // then
+        assertThat(lottos).hasSize(2)
+                .extracting("numbers")
+                .containsExactlyInAnyOrder(List.of(1,2,3,4,5,6),
+                        List.of(1,2,3,4,5,6));
+    }
+
+}
