e303f862f779fc8312264058264d9cec0a9e8f59
차왕준 <ckdhdkwns@gmail.com>
2024-10-30 04:45:09 +0900

    feat(view): 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,29 @@
 package lotto.view;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+
     private static final int LOTTO_AMOUNT = 1000;
 
-    public void readPurchaseAmount() {
+    public int readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         String input = readLine();
         validateNumber(input, "구입금액");
 
         int amount = Integer.parseInt(input);
         validateDivisible(amount);
+        return amount;
+    }
+
+    public void readWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        String input = readLine();
+
     }
 
     private void validateNumber(String input, String fieldName) {
