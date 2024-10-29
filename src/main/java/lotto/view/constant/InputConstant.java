b88e7d116018916b8960c1466262bba238da25df
iceprins <junghankyul@gmail.com>
2024-10-30 01:10:38 +0900

    feat: 입력 받는 기능 구현 및 상수 클래스 생성
    InputView에 입력 받는 기능을 구현하고, 출력해야 하는 문자열은 상수로 만들어 상수 클래스에 분리했다.

---
+++ src/main/java/lotto/view/constant/InputConstant.java
@@ -0,0 +1,7 @@
+package lotto.view.constant;
+
+public class InputConstant {
+    public static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String ENTER_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}
