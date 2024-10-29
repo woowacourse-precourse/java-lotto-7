54a7a49c3ffc03bf4f3c5685174b1b4f76941309
Firedrago95 <yonghwa95@naver.com>
2024-10-29 21:40:24 +0900

    Feat(Money): 수익률 구하는 기능 구현

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Map;
+
 public class Money {
 
     public static final int LOTTO_PRICE = 1000;
@@ -11,5 +13,13 @@ public class Money {
 
     public int getTicketCount() {
         return money / LOTTO_PRICE;
+    }
+
+    public String getRateOfReturn(Map<Rank, Integer> result) {
+        double income = 0;
+        for (Rank rank : result.keySet()) {
+            income += rank.getPrice() * result.get(rank);
+        }
+        return String.format("%.1f" ,income / money);
     }
 }
