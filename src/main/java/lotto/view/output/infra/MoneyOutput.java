fb2fc68b93884086b7c2724f9fac74e3ba38e96d
EHCTH <lees90896@gmail.com>
2024-10-29 17:28:57 +0900

    feat(MoneyOutput): Money입력시 Info 반환 작성

--- src/main/java/lotto/view/output/infra/MoneyOutput.java
+++ src/main/java/lotto/view/output/infra/MoneyOutput.java
@@ -1,4 +1,11 @@
-package lotto.view.output;
+package lotto.view.output.infra;
 
-public class MoneyOutput {
+import lotto.view.output.domain.ViewService;
+
+public class MoneyOutput implements ViewService {
+
+    @Override
+    public void view() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 }
