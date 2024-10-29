ff284816ccfd8fc15aa36680bbbf00c3de65222a
msk226 <kminseok5167@gmail.com>
2024-10-29 21:45:16 +0900

    fix(service): 서비스 메서드 역할 따라 분리

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,1 +1,50 @@
 package lotto.service;
+
+import static lotto.utils.ErrorMessages.*;
+
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
+import lotto.model.LottoMachine;
+import lotto.model.LottoResultManager;
+import lotto.utils.ErrorMessages;
+
+public class LottoService {
+
+    public LottoMachine initializeLottoMachine(String inputPrice){
+        return new LottoMachine(parseToInteger(inputPrice));
+    }
+
+    public Lotto initializeWinningLotto(String inputWinningLotto){
+        return new Lotto(parseInputToList(inputWinningLotto));
+    }
+
+    public BonusNumber initializeBonusNumber(String inputBonusNumber){
+        return new BonusNumber(parseToInteger(inputBonusNumber));
+    }
+
+
+    private List<Integer> parseInputToList(String inputLottoNumbers) {
+        String[] splitLottoNumbers = inputLottoNumbers.split(",");
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String number : splitLottoNumbers) {
+            numbers.add(parseToInteger(number));
+        }
+
+        return numbers;
+    }
+
+    private Integer parseToInteger(String input) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_MUST_INTEGER);
+        }
+    }
+
+
+
+}
