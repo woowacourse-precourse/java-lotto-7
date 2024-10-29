15e7c3d6775b8a021cbb03f957689a722934ad41
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-30 01:03:08 +0900

    refactor(validate): 작은 메서validate 메서드를 작은 메서드로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,13 +13,25 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateNoDuplicates(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateNoDuplicates(List<Integer> numbers) {
         Set<Integer> set = new HashSet<>(numbers);
         if (set.size() < 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+    }
+
+    private void validateRange(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < 1 || 45 < number) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 값이어야 합니다.");
