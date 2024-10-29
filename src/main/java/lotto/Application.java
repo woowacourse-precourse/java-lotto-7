4bddbea53fcb0a2da66774ae1310a2617412ce76
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-29 17:16:00 +0900

    feat: add functionality for input lotto purchase quantity
    로또 구매 장수를 입력 받는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.constant.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lottoTickets = inputPayment();
+    }
+
+    public static int inputPayment(){
+        System.out.println(PAYMENT_MASSAGE);
+        int payment = Integer.parseInt(Console.readLine());
+        validatePayment(payment);
+        return payment/LOTTOPRICE;
+    }
+
+    public static void validatePayment(int payment){
+        if(payment % LOTTOPRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+        }
     }
 }
---
+++ src/main/java/lotto/constant.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class constant {
+    // 숫자 상수
+    public static final int LOTTOPRICE = 1000;
+
+    // 문자열 상수
+    public static final String PAYMENT_MASSAGE = "구입 금액을 입력해 주세요.";
+}
