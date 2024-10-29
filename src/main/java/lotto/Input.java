12ee0fbff503a5031a80ea9f347e6aaaa2c4b955
urim96 <qusdbfla1@gmail.com>
2024-10-30 03:46:45 +0900

    refactor: 입력값 받는 메소드 분리
    차후에 다른 입력값 반복하여 받기 위해 분리
    테스트 코드를 위해 분리

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -7,11 +7,17 @@ public class Input {
 	public void amountInput() {
 		try {
 			System.out.println("구입금액을 입력해 주세요.");
-			String amount = Console.readLine();
+			String amount = readLine();
 			Amount amountCheck = new Amount(amount);
 		} catch (IllegalArgumentException e) {
 			System.err.println("[ERROR] 숫자만 입력해주세요.");
 			amountInput();
 		}
 	}
+	
+	public String readLine() {
+		return Console.readLine();
+	}
+	
+	
 }
