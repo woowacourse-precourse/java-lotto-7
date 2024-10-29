2c89588178e850ae0af827e25610243f5a187f6b
Yoon-Jemin <yjm1800@naver.com>
2024-10-29 17:46:35 +0900

    feat(view): 입력을 받을 수 있는 InputView 클래스 추가
    - 사용자로부터 가격을 입력 받기 위한 readPriceInput() 메서드 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int readPriceInput(){
+        String price = Console.readLine();
+        return Integer.parseInt(price);
+    }
+
+
+}
