b6ec5d6b6a52958a6b33c7de6ee173d527e3f865
sunmok <tjsahr9191@naver.com>
2024-10-29 23:13:04 +0900

    feat : 구입 금액 코멘트, 로또 리스트 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,28 @@
 package lotto.view;
 
 import lotto.exception.LottoGameException;
+import lotto.model.Lottos;
+import lotto.model.Money;
 
 public class OutputView {
 
     public void commentErrorMessage(LottoGameException e) {
         System.out.println(e.getMessage());
+    }
+
+    public void commentsForPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public int commentForBuyLotto(Money money) {
+        int lottoCount = money.getPossibleLottoCount();
+        System.out.println();
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        return lottoCount;
+    }
+
+    public void commentForLottoList(Lottos lottos) {
+        lottos.getLottos().forEach(System.out::println);
     }
 
 }
