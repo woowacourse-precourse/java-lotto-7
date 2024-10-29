37871df3736d6c00a305dc934134d2f5fddbe5c3
iamdonghyuk <iamdonghyuk@gmail.com>
2024-10-29 15:14:22 +0900

    feat: Lotto 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public class Lotto {
+        private final List<Integer> numbers;
+
+        public Lotto(List<Integer> numbers) {
+            validate(numbers);
+            this.numbers = numbers;
+        }
+
+        private void validate(List<Integer> numbers) {
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            }
+        }
+
+        // TODO: 추가 기능 구현
+    }
 }
+
