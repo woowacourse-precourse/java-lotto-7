7b9704a15fb4e7267e07db8c3252505cc21079c8
msk226 <kminseok5167@gmail.com>
2024-10-29 22:02:36 +0900

    feat(controller): LottoController 클래스 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,14 @@
+package lotto.controller;
+
+import lotto.service.LottoService;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService){
+        this.lottoService = lottoService;
+    }
+
+
+}
