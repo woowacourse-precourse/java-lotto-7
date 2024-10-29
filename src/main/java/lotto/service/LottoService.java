9f8e714483ce0ba9b63b1bed3948687aa5426b5b
Orion-Yoon <insertlock8@gmail.com>
2024-10-30 02:41:42 +0900

    feat : 구입 금액에 따라 로또 번호를 생성하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,21 +1,28 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.model.Lotto;
 import lotto.model.LottoRank;
 
 public class LottoService {
-    public TreeSet<Integer> makeLottoNumber() {
-        TreeSet<Integer> lottoNumbers = new TreeSet<>();
-        List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<Lotto> createLottos(int purchaseAmount) {
+        int numberOfLottos = purchaseAmount / 1000;
+        List<Lotto> lottos = new ArrayList<>();
 
-        for (int numbers : lottoNumber) {
-            lottoNumbers.add(numbers);
-        }
-
-        return lottoNumbers;
+        return IntStream.range(0, numberOfLottos)
+                .mapToObj(i -> {
+                    List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                    Collections.sort(lottoNumbers);
+                    return new Lotto(lottoNumbers);
+                })
+                .collect(Collectors.toList());
 
     }
 
@@ -26,7 +33,7 @@ public class LottoService {
     }
 
     private LottoRank determineRank(int matchCount, boolean bonusMatch) {
-        if (matchCount == 6) return LottoRank.FIFTH;
+        if (matchCount == 6) return LottoRank.FIRST;
         if (matchCount == 5 && bonusMatch) return LottoRank.SECOND;
         if (matchCount == 5) return LottoRank.THIRD;
         if (matchCount == 4) return LottoRank.FOURTH;
