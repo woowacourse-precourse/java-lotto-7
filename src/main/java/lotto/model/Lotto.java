686c9943cefd16429ca6ead84365aae69cb9c179
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-29 22:43:04 +0900

    feat: 로또 객체 작성

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class Lotto {
+
+    private int[] lotto;
+
+    public Lotto(int[] lotto) {
+        this.lotto = lotto;
+    }
+
+    public int[] getLotto() {
+        return lotto;
+    }
+}
