a672c8ca2dd4e5f5a48f822a65f2e9662e2f243b
JunBe <wnsqowid0901@gmail.com>
2024-10-29 20:53:47 +0900

    style: 출력문 수정
    줄 바꿈 및 필요한 출력문을 추가했다.

--- src/main/java/lotto/view/io/Input.java
+++ src/main/java/lotto/view/io/Input.java
@@ -21,11 +21,13 @@ public class Input {
     }
 
     public String winningNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public String bonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
--- src/main/java/lotto/view/io/Output.java
+++ src/main/java/lotto/view/io/Output.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Output {
     public void lottoCount(int count) {
+        System.out.println();
         System.out.println(count + "개를 구매했습니다.");
     }
 
@@ -13,10 +14,15 @@ public class Output {
 
     public void winningResult() {
         int ans = 0;
+        double rate = 0;
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + ans + "개");
         System.out.println("4개 일치 (50,000원) - " + ans + "개");
         System.out.println("5개 일치 (1,500,000원) - " + ans + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + ans + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + ans + "개");
+        System.out.println("총 수익률은 " + rate + "% 입니다");
     }
 }
