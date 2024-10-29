3e9dca10d288b15e0f5493330531d5f8bca2fc47
doronggyu <rbehddl13@gmail.com>
2024-10-29 17:58:52 +0900

    feat:로또 번호 리스트 저장 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,5 +16,23 @@ public class Application {
 
         System.out.println("보너스 번호를 입력해주세요.");
         String bonusNumber = Console.readLine();
+
+        
+        int[][] lottoNumberList = new int[price / 1000][6];
+
+        for(int i=0; i<lottoNumberList.length; i++){
+            for(int j=0; j<lottoNumberList[0].length; j++){
+                lottoNumberList[i][j] = Randoms.pickNumberInRange(1,45);
+                for(int k=0; k<j; k++){
+                    if(lottoNumberList[i][j] == lottoNumberList[i][k]){
+                        j--;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<lottoNumberList.length; i++){
+            Arrays.sort(lottoNumberList[i]);
+        }
     }
 }
