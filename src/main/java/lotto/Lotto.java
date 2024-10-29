2f28ea2a32ee2315c20cfa6ebf4d032929d4038b
Yehun <yhyh4420@naver.com>
2024-10-29 18:36:46 +0900

    feat(main):새로운 로직 추가
    예외처리하는 로직 추가
        - 중복 숫자 입력 시(예시 : 1,2,3,4,4,5) 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,12 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다." );
+        }
     }
+
 
     // TODO: 추가 기능 구현
 }
