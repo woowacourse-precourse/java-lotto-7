ed04f44041586ede4b7cb197fd1f716e3ea2d320
JunBe <wnsqowid0901@gmail.com>
2024-10-29 19:17:54 +0900

    feat: 로또 번호 중복 검사 메서드 추가
    validateDuplicate() 메서드를 구현하여 로또 번호가 중복되지 않았는지 확인한다.
    중복되었다면 IllegalArgumentException을 발생시킨다.
    validate 함수 내부에 validateDuplicate() 메서드를 넣어 관련된 역할끼리 묶었다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.zip.ZipEntry;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +15,18 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            //[ERROR]를 상수 문자열로 만들어서 바꿀 것
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        validateDuplicate(numbers);
+
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> checkDuplicate = new HashSet<>(numbers);
+        if (checkDuplicate.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
