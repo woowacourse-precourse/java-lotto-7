678ba7dba06b8b9a05a37b349f9e1d8943acb678
msk226 <kminseok5167@gmail.com>
2024-10-29 18:22:16 +0900

    feat(model): LottoMachine 테스트 코드 작성

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.model.BonusNumber;
+import lotto.model.LottoMachine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LottoMachineTest {
+
+    @Test
+    void 정상_케이스(){
+        LottoMachine lottoMachine = new LottoMachine(10000);
+        assertEquals(lottoMachine.getCount(), 10);
+        assertEquals(lottoMachine.getLotto().size(), 10);
+    }
+    @Test
+    void 구입_금액은_1000으로_나누어_떨어져야_한다() {
+        assertThatThrownBy(() -> new LottoMachine(1110))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
