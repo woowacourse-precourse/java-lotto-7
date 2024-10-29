f22266641bb495a8db7f31d784eb94a2c006c9f4
Spinichi <spinichi@naver.com>
2024-10-30 05:22:11 +0900

    feat: 사용자 금액 입력 기능 코드 변경
    메서드를 더 세분하여 분리함으로써 모듈화와 구조화를 진행함. 코드 가독성을 높이고 유지보수를 용이하게 함.

--- src/main/java/lotto/UserAmountInput.java
+++ src/main/java/lotto/UserAmountInput.java
@@ -9,25 +9,30 @@ public class UserAmountInput {
     public void userAmountInput() {
 
         System.out.println("구입금액을 입력해 주세요.");
+        String amountInput = Console.readLine();
 
+        processUserAmount(amountInput);
+    }
+
+    private void processUserAmount(String amountInput) {
         try {
-            String amountInput = Console.readLine();
 
-            amount = Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
-
+            amount = parseAmount(amountInput); // 파싱된 값을 amount에 할당
             validate(amount);
-
             lottoTicketCount = amount / 1000;
+
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 정수여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 천 원단위의 정수여야 합니다.");
         }
+    }
+
+    private int parseAmount(String amountInput) {
+        return Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
     }
 
     private void validate(int amount) {
         if (amount <= 0 || amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 천 원단위의 양수여야 합니다.");
         }
-
     }
-
 }
