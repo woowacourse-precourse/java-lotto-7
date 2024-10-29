ef5d855ca78f73525025af349e317e7560fe8476
Yoon-Jemin <yjm1800@naver.com>
2024-10-30 00:49:40 +0900

    feat(util): Validator 클래스 수정
    - 입력이 1000원 보다 작은지 확인 하기 위한 checkPriceThreshold 메서드 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.constant;
 public enum ErrorMessage {
 
     ERROR("[ERROR] "),
-    PRICE_TYPE_ERROR(ERROR.getMessage() + "입력된 가격이 올바른 숫자 형식이 아닙니다.");
+    PRICE_TYPE_ERROR(ERROR.getMessage() + "입력된 가격이 올바른 숫자 형식이 아닙니다."),
+    PRICE_TOO_LOW_ERROR(ERROR.getMessage() + "입력된 가격이 너무 낮습니다.");
 
     private String message;
 
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,25 +1,39 @@
 package lotto.util;
 
+import static lotto.constant.ErrorMessage.PRICE_TOO_LOW_ERROR;
 import static lotto.constant.ErrorMessage.PRICE_TYPE_ERROR;
 
 public class Validator {
 
+    private static int THRESHOLD_PRICE = 1000;
+
     public boolean validatePrice(String price) {
         try{
             checkNumberType(price);
+            checkPriceThreshold(price);
         } catch (IllegalArgumentException e) {
             return false;
         }
         return true;
     }
 
-    private void checkNumberType(String price) {
+    private void checkNumberType(String input) {
         try{
-            Integer.parseInt(price);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println(PRICE_TYPE_ERROR.getMessage());
             throw new IllegalArgumentException();
         }
     }
+
+    private void checkPriceThreshold(String input) {
+        int price = Integer.parseInt(input);
+        if (price < THRESHOLD_PRICE) {
+            System.out.println(PRICE_TOO_LOW_ERROR.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+
 
 }
