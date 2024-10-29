f4ab16377338750ebd14ba372cc7a9ca269cf12f
doronggyu <rbehddl13@gmail.com>
2024-10-29 17:39:08 +0900

    feat:입력 기능 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입 가격을 입력해주세요.");
+        int price = Integer.parseInt(Console.readLine());
+
+        System.out.println("당첨번호를 입력해주세요.");
+        String winnigNumber = Console.readLine();
+
+        System.out.println("보너스 번호를 입력해주세요.");
+        String bonusNumber = Console.readLine();
     }
 }
