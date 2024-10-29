e3c43105f9ed991bfee74c1d0f187695ddcdce56
EHCTH <lees90896@gmail.com>
2024-10-29 18:02:50 +0900

    feat(Buyer): Buyer 구현 * feat: insertMoney(): Money

--- src/main/java/lotto/buyer/domain/Buyer.java
+++ src/main/java/lotto/buyer/domain/Buyer.java
@@ -1,4 +1,11 @@
 package lotto.buyer.domain;
 
 public class Buyer {
+    private final InsertMoneyService insertMoneyService;
+    public Buyer(InsertMoneyService insertMoneyService) {
+        this.insertMoneyService= insertMoneyService;
+    }
+    public Money insertMoney() {
+        return insertMoneyService.insert();
+    }
 }
