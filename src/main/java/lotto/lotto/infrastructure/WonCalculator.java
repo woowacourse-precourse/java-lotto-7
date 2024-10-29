bc2f740dc1cc1431221761d6754c00d036aecfc7
EHCTH <lees90896@gmail.com>
2024-10-29 17:36:05 +0900

    feat(WonCalculator): 한국 통화 Won 을 계산하는 Calulator구현 작성

--- src/main/java/lotto/lotto/infrastructure/WonCalculator.java
+++ src/main/java/lotto/lotto/infrastructure/WonCalculator.java
@@ -1,4 +1,12 @@
 package lotto.lotto.infrastructure;
 
-public class Calculator {
+import lotto.buyer.domain.Money;
+import lotto.lotto.domain.Calculator;
+
+public class WonCalculator implements Calculator {
+    private final Long PRICE_PER_LOTTO = 1000L;
+    @Override
+    public int divideByThousand(Money money) {
+        return (int) (money.getMoney() / PRICE_PER_LOTTO);
+    }
 }
