5777c5a4bb737d64e2af9e933e68d02b52c1a99b
Firedrago95 <yonghwa95@naver.com>
2024-10-29 15:26:51 +0900

    Feat(InputView): 구입 금액 입력기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static int readMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = Console.readLine();
+        InputValidator.validateMoney(money);
+        return Integer.parseInt(money);
+    }
+}
