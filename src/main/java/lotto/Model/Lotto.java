b1543ce6f283b028dff8ca6150dc11e00da7ffb6
eusan <goodsan0330@gamil.com>
2024-10-29 21:11:10 +0900

    feat(lotto): add exception handling for duplicate numbers

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,18 +1,34 @@
 package lotto.Model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+	private static final String LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+	private static final String LOTT_NUMBER_DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복이 불가능 합니다.";
+
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-		validate(numbers);
+		validateSize(numbers);
+		validateDuplicate(numbers);
+
 		this.numbers = numbers;
 	}
 
-	private void validate(List<Integer> numbers) {
+	private void validateSize(List<Integer> numbers) {
 		if (numbers.size() != 6) {
-			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+			throw new IllegalArgumentException(LOTTO_NUMBER_ERROR_MESSAGE);
+		}
+	}
+
+	private void validateDuplicate(List<Integer> numbers) {
+		Set<Integer> removeDuplicate = new HashSet<>(numbers);
+
+		if (numbers.size() != removeDuplicate.size()) {
+			throw new IllegalArgumentException(LOTT_NUMBER_DUPLICATE_ERROR_MESSAGE);
 		}
 	}
 
