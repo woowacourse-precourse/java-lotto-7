aa29ff724583c665c812d71e56036ce80dc10281
urim96 <qusdbfla1@gmail.com>
2024-10-30 04:14:58 +0900

    feat: 구매 금액 유효성 검사
    1,000원 이상 100,000원 이하인지 검사하는 메소드 생성

--- src/main/java/lotto/Amount.java
+++ src/main/java/lotto/Amount.java
@@ -6,13 +6,23 @@ public class Amount {
 
 	public Amount(String amount) {
 		this.amount = amountNumberformat(amount);
+		amountCheck();
 	}
 
 	private int amountNumberformat(String amount) {
 		try {
 			return Integer.parseInt(amount);
 		} catch (NumberFormatException ne) {
-			throw new IllegalArgumentException();
+			throw new NumberFormatException("[ERROR] 숫자만 입력해주세요.");
+		}
+	}
+
+	private void amountCheck() {
+		if (amount < 1000) {
+			throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상 입력해주세요.");
+		}
+		if (amount > 100000) {
+			throw new IllegalArgumentException("[ERROR] 구매 금액은 100000원 이하로 입력해주세요.");
 		}
 	}
 }
