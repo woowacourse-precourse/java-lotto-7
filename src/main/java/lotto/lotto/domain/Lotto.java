f424e0003ab613289f6c161314ea1b5d06b932ae
EHCTH <lees90896@gmail.com>
2024-10-29 17:36:46 +0900

    feat(Lotto): 정렬 기능 추가 * feat: 정렬 기능

--- src/main/java/lotto/lotto/domain/Lotto.java
+++ src/main/java/lotto/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
-package lotto.domain;
+package lotto.lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +18,11 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void sort() {
+        Collections.sort(numbers);
+    }
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
