112f5edf0d382d1890920c6c6523a5a1a486852d
ChoiDongJoon <adong0808@naver.com>
2024-10-30 01:33:28 +0900

    refactor(lotto): 이미 구현된 Lotto를 사용하도록 수정
    이미 구현된 Lotto를 사용하도록 수정

--- src/main/java/lotto/controller/OutputController.java
+++ src/main/java/lotto/controller/OutputController.java
@@ -2,13 +2,14 @@ package lotto.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.Lotto;
 
 public class OutputController {
 
-    public void printGeneratedLottoTickets(int lottoAmount, ArrayList<List<Integer>> lottoTickets) {
+    public void printGeneratedLottoTickets(int lottoAmount, ArrayList<Lotto> lottoTickets) {
         System.out.println(lottoAmount + "개를 구매했습니다.");
 
-        for (List<Integer> lottoTicket : lottoTickets) {
+        for (Lotto lottoTicket : lottoTickets) {
             System.out.println(lottoTicket.toString());
         }
     }
