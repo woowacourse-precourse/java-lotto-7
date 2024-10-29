6b907c112fe98d0e2019cc74ffee71c8c8c8dc7c
DESKTOP-GRC68NA\foski <foskingson@naver.com>
2024-10-30 00:03:33 +0900

    feat: 당첨기록을 저장하는 메서드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,7 +2,11 @@ package lotto.service;
 
 import java.util.ArrayList;
 
+import lotto.util.Rank;
+
 public class LottoService {
+
+    private static ArrayList<Rank> winningRecord = new ArrayList<>();
 
     public static int countLottoNumber(int purchaseAmount){
         return purchaseAmount/1000;
@@ -27,5 +31,9 @@ public class LottoService {
 
     public static double calculateProfitMargin(int totalPrize,int purchaseAmount){
         return (totalPrize/(double)purchaseAmount)*100;
+    }
+
+    public static void addWinningRecord(Rank rank){
+        winningRecord.add(rank);
     }
 }
