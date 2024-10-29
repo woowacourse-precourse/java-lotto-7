634de58cd6642b7746025352f1168dade402dd6e
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-29 20:01:29 +0900

    feat(Application) : 로또 숫자 범위 체크 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,15 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Application {
     public static final String LOTTO_AMOUNT_PHRASE = "구입금액을 입력해 주세요.";
     public static final String LOTTO_COUNT_PHRASE = "개를 구매했습니다.";
     public static final String LOTTO_NUMBER_INPUT = "당첨 번호를 입력해 주세요.";
     public static final String LOTTO_BONUS_INPUT = "보너스 번호를 입력해 주세요.";
     public static final String WIN_STATICS = "당첨 통계\n---";
+    public static final String COMMA = ",";
 
     public static void main(String[] args) {
 
@@ -29,6 +32,11 @@ public class Application {
             throw new IllegalArgumentException("1000원으로 나누어 떨어지는 금액을 입력해주세요.");
         }
         return input/1000;
+    }
+
+    public static boolean checkLottoRange(Integer input){
+        if(input >= 1 && input <= 45) return true;
+        return false;
     }
 
 }
