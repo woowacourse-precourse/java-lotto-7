37324242ed17d66e3a0cc47299655cc9def0696b
dnwldla <leeemjw@naver.com>
2024-10-30 08:28:39 +0900

    test: 공백을 입력하면 예외를 발생시키는 테스트 추가

--- src/test/java/lotto/domain/PaymentInputTest.java
+++ src/test/java/lotto/domain/PaymentInputTest.java
@@ -2,11 +2,21 @@ package lotto.domain;
 
 import lotto.common.ErrorMessage;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PaymentInputTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 공백을_입력하면_예외_발생(String input){
+        assertThatThrownBy(() -> new PaymentInput(input)).
+                isInstanceOf(IllegalArgumentException.class).
+                hasMessageContaining(ErrorMessage.BLANK_OR_NULL_INPUT);
+    }
+
 
     @ParameterizedTest
     @ValueSource(strings = {"+1","-1",".!3","1.5"})
