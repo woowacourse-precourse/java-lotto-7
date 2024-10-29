2517761ae30bd1555b05dc81fef18faaf1fab901
ChiSaeHwang <cwh730909@gmail.com>
2024-10-29 17:36:08 +0900

    refactor(Lotto): Lotto Number Size 상수화

--- src/main/java/lotto/domain/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/lotto/domain/Lotto.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
+
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -30,7 +33,7 @@ public class Lotto {
     }
 
     private void checkLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
