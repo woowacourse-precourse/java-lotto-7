30743e5e1d4a8f39e6e42b05da96427cec698b16
kssk3 <kss8014@daum.net>
2024-10-29 21:04:46 +0900

    Ticket 클래스 추가

--- src/main/java/lotto/Ticket.java
+++ src/main/java/lotto/Ticket.java
@@ -1,5 +1,14 @@
 package lotto;
 
 public class Ticket {
+    private static final String REQUEST_NON_DUPLICATED_NUMBER = "중복되지 않은 숫자 6개를 입력해주세요.";
+    private static final String REQUEST_RIGHT_SIZE_MESSAGE = "숫자 6개를 입력해주세요. 예) 1,2,3,4,5,6";
+    private static final int TICKET_SIZE = 6;
 
+
+    private static void checkTicketSize(int size) {
+        if (size != TICKET_SIZE) {
+            throw new IllegalArgumentException(REQUEST_NON_DUPLICATED_NUMBER);
+        }
+    }
 }
