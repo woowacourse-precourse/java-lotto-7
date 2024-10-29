7cce7e58f47e23db5e17b41d6207c884aff8db30
qyinm <qusseun@gmail.com>
2024-10-29 18:58:18 +0900

    refactor: 로또 구입금액 입력에 대한 validate 함수명 변경

--- src/main/java/lotto/utils/InputView.java
+++ src/main/java/lotto/utils/InputView.java
@@ -2,6 +2,7 @@ package lotto.utils;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class InputView {
@@ -11,12 +12,12 @@ public class InputView {
 
     public static int inputLottoPurchaseAmount() {
         String userInput = readLine();
-        validate(userInput);
+        validateLottoPurchaseAmount(userInput);
 
         return Integer.parseInt(userInput);
     }
 
-    private static void validate(String userInput) {
+    private static void validateLottoPurchaseAmount(String userInput) {
         if (!validLottoPurchaseAmountPattern.matcher(userInput).matches()) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력 해야합니다.");
         }
