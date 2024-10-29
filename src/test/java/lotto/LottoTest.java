465fa808f25b789e652c6d5da97f71720fbf9561
(minjae8563) <(minjae8563@naver.com)>
2024-10-29 19:59:27 +0900

    test: LottoTest 테스트 케이스 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -20,6 +20,11 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1미만 45초과면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1미만_45초과면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 47, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
 }
