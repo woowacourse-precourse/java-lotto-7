e07b9e1da2b21cc7ceaeb60811c8457864a1c436
박성춘 <songchoon10@naver.com>
2024-10-29 19:54:31 +0900

    feat: 구입금액 입력받는 로직 구현 완료

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,19 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
+    private final InputView inputView;
+
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        readPurchaseAmount();
+    }
+
+    private String readPurchaseAmount() {
+        return inputView.readPurchaseAmount();
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,21 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public String readPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        String userPurchaseAmount = Console.readLine();
+        checkUserInputIsNull(userPurchaseAmount);
+
+        return userPurchaseAmount;
+    }
+
+    private void checkUserInputIsNull(String userInput) {
+        if (userInput == null || userInput.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 공백입니다.");
+        }
+    }
 }
