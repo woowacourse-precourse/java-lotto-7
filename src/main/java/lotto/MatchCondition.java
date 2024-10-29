4026b73fe1ba92f637bbb61a5a613c5c1c1063f5
YongLeeCode <yonghwan311@gmail.com>
2024-10-29 23:56:53 +0900

    feat: 당첨 조건 충족 클래스 선언

---
+++ src/main/java/lotto/MatchCondition.java
@@ -0,0 +1,138 @@
+package lotto;
+
+public interface MatchCondition {
+    Integer getNumberCount();
+    Integer getPrizedAmount();
+    String prizeDescription();
+    Boolean hasBonusNumber();
+
+    class FirstPrize implements MatchCondition {
+        @Override
+        public Integer getNumberCount() {
+            return 6;
+        }
+
+        @Override
+        public Integer getPrizedAmount() {
+            return 2000000000;
+        }
+
+        @Override
+        public String prizeDescription() {
+            return String.format(
+                    "%d개 일치 (%s원)",
+                    getNumberCount(),
+                    getPrizedAmount()
+            );
+        }
+
+        @Override
+        public Boolean hasBonusNumber() {
+            return false;
+        }
+    }
+
+    class SecondPrize implements MatchCondition {
+        @Override
+        public Integer getNumberCount() {
+            return 5;
+        }
+
+        @Override
+        public Integer getPrizedAmount() {
+            return 30000000;
+        }
+
+        @Override
+        public String prizeDescription() {
+            return String.format(
+                    "%d개 일치, 보너스 볼 일치 (%s원)",
+                    getNumberCount(),
+                    getPrizedAmount()
+            );
+        }
+
+        @Override
+        public Boolean hasBonusNumber() {
+            return true;
+        }
+    }
+
+    class ThirdPrize implements MatchCondition {
+        @Override
+        public Integer getNumberCount() {
+            return 5;
+        }
+
+        @Override
+        public Integer getPrizedAmount() {
+            return 1500000;
+        }
+
+        @Override
+        public String prizeDescription() {
+            return String.format(
+                    "%d개 일치 (%s원)",
+                    getNumberCount(),
+                    getPrizedAmount()
+            );
+        }
+
+        @Override
+        public Boolean hasBonusNumber() {
+            return false;
+        }
+    }
+
+    class FourthPrize implements MatchCondition {
+        @Override
+        public Integer getNumberCount() {
+            return 4;
+        }
+
+        @Override
+        public Integer getPrizedAmount() {
+            return 50000;
+        }
+
+        @Override
+        public String prizeDescription() {
+            return String.format(
+                    "%d개 일치 (%s원)",
+                    getNumberCount(),
+                    getPrizedAmount()
+            );
+        }
+
+        @Override
+        public Boolean hasBonusNumber() {
+            return false;
+        }
+    }
+
+    class FifthPrize implements MatchCondition {
+        @Override
+        public Integer getNumberCount() {
+            return 3;
+        }
+
+        @Override
+        public Integer getPrizedAmount() {
+            return 5000;
+        }
+
+        @Override
+        public String prizeDescription() {
+            return String.format(
+                    "%d개 일치 (%s원)",
+                    getNumberCount(),
+                    getPrizedAmount()
+            );
+        }
+
+        @Override
+        public Boolean hasBonusNumber() {
+            return false;
+        }
+    }
+}
