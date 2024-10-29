2792936906574ced06d890c7202fd67feb9ea412
JunBe <wnsqowid0901@gmail.com>
2024-10-29 22:38:17 +0900

    style: 주석 삭제

--- src/main/java/lotto/view/io/Input.java
+++ src/main/java/lotto/view/io/Input.java
@@ -6,18 +6,6 @@ public class Input {
     public String price() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
-//        int price;
-//        try {
-//            price = Integer.parseInt(inputPrice);
-//        } catch (NumberFormatException e) {
-//            throw new IllegalArgumentException("[ERROR] 문자열은 입력 불가합니다.");
-//        }
-//        if (price <= 0) {
-//            throw new IllegalArgumentException("0 이하의 수는 입력할 수 없습니다.");
-//        }
-//        if (price % 1000 != 0) {
-//            throw new IllegalArgumentException("가격은 1000원 단위로 입력 가능합니다.");
-//        }
     }
 
     public String winningNumber() {
