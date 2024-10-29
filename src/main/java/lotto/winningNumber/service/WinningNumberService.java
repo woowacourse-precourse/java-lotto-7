f55264d70afb738d3a7d0375a00681f771ef542f
param <spsummerdoc@gmail.com>
2024-10-29 18:21:25 +0900

    feat(winningNumberService): 당첨 번호 생성과 당첨 개수 반환 로직 추가

---
+++ src/main/java/lotto/winningNumber/service/WinningNumberService.java
@@ -0,0 +1,32 @@
+package lotto.winningNumber.service;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.global.util.Parser;
+import lotto.lottery.domain.Lotto;
+import lotto.winningNumber.domain.WinningNumber;
+
+public class WinningNumberService {
+
+    public WinningNumber create(String number, String bonus) {
+        List<Integer> numbers = parseWinningNumbers(number);
+        int bonusNumber = Parser.toIntWinningNumber(bonus);
+        return new WinningNumber(numbers, bonusNumber);
+    }
+
+    private List<Integer> parseWinningNumbers(String number) {
+        return Arrays.stream(number.split(",")).filter(s -> !s.trim().isEmpty())
+                .map(Parser::toIntWinningNumber).toList();
+    }
+
+    public int calculateMatchCount(Lotto lotto, WinningNumber winningNumber) {
+        return (int) lotto.getNumbers().stream()
+                .filter(l -> winningNumber.getNumbers().contains(l))
+                .count();
+
+    }
+
+    public boolean isBonusMatched(Lotto lotto, WinningNumber winningNumber) {
+        return lotto.getNumbers().contains(winningNumber.getBonus());
+    }
+}
