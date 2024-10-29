93e14d6d3ca294b931460203594e626f7f3b9afe
dnwldla <leeemjw@naver.com>
2024-10-30 08:38:01 +0900

    feat: 금액이 1000으로 나누어떨어지지 않는 경우 예외를 발생시키는 함수 추가

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -3,4 +3,5 @@ package lotto.common;
 public class ErrorMessage {
     public static final String NOT_INTEGER_INPUT ="입력은 양의 정수이어야 합니다";
     public static final String BLANK_OR_NULL_INPUT = "입력은 공백일 수 없습니다";
+    public static final String NOT_UNIT_INPUT = "구입 금액은 1000 단위이어야 합니다";
 }
--- src/main/java/lotto/domain/PaymentInput.java
+++ src/main/java/lotto/domain/PaymentInput.java
@@ -1,14 +1,24 @@
 package lotto.domain;
 
 import lotto.common.CommonValidator;
+import lotto.common.ErrorMessage;
 
 
 public class PaymentInput {
+    private static final int UNIT=1000;
+
     int payment;
 
     public PaymentInput(String input){
         CommonValidator.validateNullAndBlank(input);
         payment=CommonValidator.validateInteger(input);
+        validateUnit(payment);
+    }
+
+    private void validateUnit(int payment) {
+        if (payment%UNIT!=0 || payment==0){
+            throw new IllegalArgumentException(ErrorMessage.NOT_UNIT_INPUT);
+        }
     }
 
 
