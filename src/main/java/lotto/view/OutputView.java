e3c2725252612aecdf7e38bb6368fb9e720834c7
eusan <goodsan0330@gamil.com>
2024-10-29 22:41:03 +0900

    style: add blank line

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,13 +9,14 @@ public class OutputView {
 	private static final String BUYING_NUMBER_MESSAGE = "%d개를 구매했습니다.";
 
 	public void printBuyingNumber(int buyingNumber) {
-		System.out.printf(BUYING_NUMBER_MESSAGE, buyingNumber);
 		System.out.println();
+		System.out.printf(BUYING_NUMBER_MESSAGE, buyingNumber);
 	}
 
 	public void printLottos(List<Lotto> lottos) {
 		for (Lotto lotto : lottos) {
 			System.out.println(lotto);
 		}
+		System.out.println();
 	}
 }
