47a61e6a6fd70db6e3dc1be0f86c8479fcb5ca9a
msk226 <kminseok5167@gmail.com>
2024-10-29 18:25:15 +0900

    feat(model): LottoMachine 클래스에 price 검증 로직 구현

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -10,9 +10,15 @@ public class LottoMachine {
     private final List<Lotto> lottos;
 
     public LottoMachine(Integer price){
+        validateLottoPrice(price);
         this.price = price;
         this.count = countLotto(price);
         this.lottos = new ArrayList<>();
+    }
+
+    private void validateLottoPrice(Integer price){
+        if (price % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
     }
 
     private Integer countLotto(int price){
