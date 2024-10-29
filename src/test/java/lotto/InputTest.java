c97cb781452536d4a402bb4cad3e9b833e4b7cc5
urim96 <qusdbfla1@gmail.com>
2024-10-30 03:52:03 +0900

    test: 숫자값을 입력 받는지 확인
    테스트 코드에서는 재귀 호출 제거함

---
+++ src/test/java/lotto/InputTest.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InputTest {
+
+    private TestInput input;
+
+    @BeforeEach
+    void 테스트_인스턴스_생성() {
+        input = new TestInput();
+    }
+
+    @Test
+    void 올바른_금액_입력() {
+        input.입력값("1000"); 
+        assertDoesNotThrow(() -> input.amountInput());
+    }
+
+    @Test
+    void 예외처리_숫자가_아닌_값_입력() {
+        input.입력값("abc");  
+        assertThrows(IllegalArgumentException.class, () -> input.amountInput());
+    }
+
+    static class TestInput extends Input {
+        private String testInput;
+
+        void 입력값(String input) {
+            this.testInput = input;
+        }
+
+        @Override
+        public String readLine() {
+            return testInput;
+        }
+        
+        @Override
+        public void amountInput() {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String amount = readLine();
+                Amount amountCheck = new Amount(amount);
+            } catch (IllegalArgumentException e) {
+                System.err.println("[ERROR] 숫자만 입력해주세요.");
+                //테스트 코드에서 무한 반복을 막기위해 재귀 호출 코드 제거
+                throw e;
+            }
+        }
+    }
+}
