1d32a820f26a70e2290e91393f340114f0406a37
Etwashoeren <sungmin0609@nate.com>
2024-10-29 17:13:02 +0900

    feat. 로또 발매 기능 구현 need to add execptioin handling

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,6 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
 
+    private static final int LOTTO_PRICE = 1000;
+    private List<Lotto> lottos;
 
+    public List<Lotto> creatLotto(int amount) {
+        int count = amountToCount(amount);
+
+        lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
+        }
+
+        return lottos;
+    }
+
+    private int amountToCount(int amount) {
+        return amount / LOTTO_PRICE;
+    }
 }
