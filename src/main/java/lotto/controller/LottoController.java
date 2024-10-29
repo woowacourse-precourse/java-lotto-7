6da6b44af0814988e1a01fd804c013d7a9a270a6
msk226 <kminseok5167@gmail.com>
2024-10-29 22:10:50 +0900

    feat(controller): LottoController 클래스 내 startLottoGame 로직 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
+import lotto.model.LottoMachine;
+import lotto.model.LottoResultManager;
 import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
@@ -10,5 +16,48 @@ public class LottoController {
         this.lottoService = lottoService;
     }
 
+    public void startLottoGame(){
+
+        LottoMachine lottoMachine = lottoService.generateLottoNumbers(getPrice());
+        OutputView.printBoughtLotto(lottoMachine);
+
+        Lotto winningLotto = lottoService.initializeWinningLotto(getWinningLotto());
+
+        BonusNumber bonusNumber = lottoService.initializeBonusNumber(getBonusNumber());
+
+        LottoResultManager lottoResultManager = lottoService.checkLottoResult(winningLotto, bonusNumber, lottoMachine);
+        OutputView.printLottoResult();
+
+        OutputView.printLottoProfit(lottoResultManager);
+
+    }
+
+
+    private static String getWinningLotto() {
+        try {
+            return InputView.inputWinningNumbers();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getWinningLotto();
+        }
+    }
+
+    private static String getBonusNumber() {
+        try {
+            return InputView.inputBonusNumber();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getBonusNumber();
+        }
+    }
+
+    private static String getPrice() {
+        try {
+            return InputView.inputPrice();
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getPrice();
+        }
+    }
 
 }
