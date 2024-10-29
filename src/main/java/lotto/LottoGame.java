91694580bd548b943122adb9ee1ecf671cf3849d
MinseokGo <rhalstjr1999@naver.com>
2024-10-29 16:40:22 +0900

    feat: 로또 구입금액을 입력 받는 기능

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoGame {
+
+    public void run() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+    }
+}
