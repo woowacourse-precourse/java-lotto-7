91cce9660c7ba197f555bf0ebc750d7bae3859a7
ppzepi <hsunguk1999@gmail.com>
2024-10-30 04:52:32 +0900

    refactor: return을 직접 하도록 수정

--- src/main/java/lotto/lottoView/InputView.java
+++ src/main/java/lotto/lottoView/InputView.java
@@ -8,22 +8,19 @@ public class InputView {
     private static final String BONUS_LOTTO_MESSAGE = "보너스 번호를 입력해 주세요.";
 
 
-    public String readInput() {
+    public String PrintStartMsg() {
+        System.out.println(START_MESSAGE);
         return Console.readLine();
     }
 
-    public void PrintStartMsg() {
-        System.out.println(START_MESSAGE);
-        readInput();
-    }
-
-    public void PrintLottoInputMsg() {
+    public String PrintLottoInputMsg() {
         System.out.println(HIT_LOTTO_MESSAGE);
-        readInput();
+        return Console.readLine();
+
     }
 
-    public void PrintBonusLottoInputMsg() {
+    public String PrintBonusLottoInputMsg() {
         System.out.println(BONUS_LOTTO_MESSAGE);
-        readInput();
+        return Console.readLine();
     }
 }
