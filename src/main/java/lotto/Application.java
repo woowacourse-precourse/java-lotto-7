fc636d897c02a8c7f429d141c7053f24a9cefea1
SeongbinPark <hyucse22@gmail.com>
2024-10-29 15:44:22 +0900

    feat(Application): 구입 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구입 금액 입력 받기
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int amount = Integer.parseInt(input);
     }
 }
