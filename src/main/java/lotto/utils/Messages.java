933b085333dfb2ed68c1824ed5ca886c3f724ae0
msk226 <kminseok5167@gmail.com>
2024-10-29 20:47:48 +0900

    feat(view): InputView 생성 및 안내 메세지 상수로 관리

---
+++ src/main/java/lotto/utils/Messages.java
@@ -0,0 +1,8 @@
+package lotto.utils;
+
+public class Messages {
+
+    public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+    public final static String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}
