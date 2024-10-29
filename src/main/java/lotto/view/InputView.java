1013e76da3cf67a14a775d9371623700949e5158
Zrfv <youngoh07015@gmail.com>
2024-10-30 06:41:21 +0900

    feat: 로또 구입 금액 입력받는 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public void close() {
+        Console.close();
+    }
+}
