2ca299189fd0405553057569bf7b25a1a635d5c9
byeongdoohan <quden04@gmail.com>
2024-10-30 04:48:14 +0900

    feat: lotto generator 추가

---
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -0,0 +1,14 @@
+package lotto.generator;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+    private static final int COUNT_NUM = 6;
+
+    public List<Integer> getLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, COUNT_NUM);
+    }
+}
