d9e80b8bd1959f9da4df0e91e6d40fb4c68484a7
정소민 <yjk70702002@gmail.com>
2024-10-29 16:25:24 +0900

    feat: 로또 수량 계산

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,12 +1,12 @@
 package lotto;
 
 import lotto.io.InputHandler;
-import lotto.io.OutputHandler;
 
 public class Controller {
     private final InputHandler inputHandler = new InputHandler();
     public void run() {
         int lottoPrice = inputHandler.getLottoPrice();
+        int lottoCount = lottoPrice / 1000;
     }
 
 }
