f01fbd0a620263b0d6567275aef95f4e186b7103
Firedrago95 <yonghwa95@naver.com>
2024-10-29 16:25:28 +0900

    Test(LottoTicketTest): 구입 매수 만큼 로또 번호를 생성하는 기능 테스트 작성

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -1,4 +1,24 @@
+package lotto.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class LottoTicketTest {
-  
+
+    @Test
+    void 로또_티켓_생성_테스트() {
+        //given
+        int ticketCount  = 10;
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
+        LottoTicket lottoTicket = new LottoTicket(ticketCount, () -> nums);
+
+        //when
+        List<Lotto> ticket = lottoTicket.getLottoTicket();
+
+        //then
+        assertEquals(ticket.size(), ticketCount);
+    }
 }
