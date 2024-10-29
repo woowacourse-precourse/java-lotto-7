d6b2f1435ff5b2b9ee7ea92a37b42f31582f70e0
EHCTH <lees90896@gmail.com>
2024-10-29 17:33:52 +0900

    feat(LottoMachine): 로또 생성기 작성

--- src/main/java/lotto/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/lotto/domain/LottoMachine.java
@@ -1,4 +1,25 @@
 package lotto.lotto.domain;
 
+import lotto.buyer.domain.Money;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class LottoMachine {
+    private final Calculator calculator;
+    private final NumberGenerator numberGenerator;
+    public LottoMachine(Calculator calculator, NumberGenerator numberGenerator) {
+        this.calculator = calculator;
+        this.numberGenerator = numberGenerator;
+    }
+    public LottoTickets purchaseLottoTickets(Money money) {
+        int count = calculator.divideByThousand(money);
+        return LottoTickets.of(Stream.generate(this::createLotto)
+                .limit(count)
+                .collect(toList()));
+    }
+    private Lotto createLotto() {
+        return new Lotto(numberGenerator.generate());
+    }
 }
