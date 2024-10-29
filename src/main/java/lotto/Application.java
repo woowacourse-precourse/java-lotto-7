5a4c3ce4aac748368462cd0f259cae901b0f7a72
Yehun <yhyh4420@naver.com>
2024-10-29 20:11:13 +0900

    feat(main):돈 받는 로직 추가
    돈을 받을 때 예외상황 추가
        - money가 1000원단위가 아닐 때 예외 던짐

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static int getMoney() throws Exception {
+        int money = 0;
+        System.out.print("금액를 입력하세요 : ");
+        money = Integer.parseInt(Console.readLine());
+        if (money % 1000 != 0){
+            throw new Exception();
+        }
+        return money;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        while (true){
+            try{
+                int money = getMoney();
+                break;
+            } catch (Exception e) {
+                System.out.println("[ERROR]");
+            }
+        }
     }
 }
