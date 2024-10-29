ee30f7420ee42f84294deba67e05acddb9ef7635
msk226 <kminseok5167@gmail.com>
2024-10-29 16:17:35 +0900

    feat(model): BonusNumber 클래스 생성

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,9 @@
+package lotto.model;
+
+public class BonusNumber {
+    private final Integer bonusNumber;
+
+    public BonusNumber(Integer bonusNumber){
+        this.bonusNumber = bonusNumber;
+    }
+}
