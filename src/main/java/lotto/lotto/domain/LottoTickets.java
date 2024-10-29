68756bea8fbef8bd6261cbe84828e230d59f6cbc
EHCTH <lees90896@gmail.com>
2024-10-29 17:37:09 +0900

    feat(LottoTickets): 로또 일급 컬레션 작성 * feat: 정렬 기능 추가

--- src/main/java/lotto/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/lotto/domain/LottoTickets.java
@@ -1,4 +1,25 @@
 package lotto.lotto.domain;
 
+import java.util.List;
+
 public class LottoTickets {
+    private final List<Lotto> tickets;
+    private LottoTickets(List<Lotto> tickets) {
+        this.tickets = tickets;
+        sort();
+    }
+    public static LottoTickets of(List<Lotto> tickets) {
+        return new LottoTickets(tickets);
+    }
+    private void sort() {
+        tickets.forEach(Lotto::sort);
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        tickets.forEach(
+                (lotto) -> sb.append(lotto).append("\n")
+        );
+        return sb.toString();
+    }
 }
