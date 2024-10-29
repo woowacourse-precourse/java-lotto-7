6b625c95bd5808c0543cb02b043b902443e3ce74
차왕준 <ckdhdkwns@gmail.com>
2024-10-30 04:39:48 +0900

    feat(view): 숫자인지 검증

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,19 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final int LOTTO_AMOUNT = 1000;
 
     public void readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         String input = readLine();
+        validateNumber(input, "구입금액");
+    }
+
+    private void validateNumber(String input, String fieldName) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(String.format("[ERROR] %s은(는) 숫자여야 합니다.", fieldName));
+        }
     }
 }
