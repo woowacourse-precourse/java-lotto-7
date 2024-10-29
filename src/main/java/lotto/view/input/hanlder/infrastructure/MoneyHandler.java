3d0dc2664a840cd0e15e3e6d4c58f7044accd75f
EHCTH <lees90896@gmail.com>
2024-10-29 17:27:37 +0900

    feat(MoneyHandler): Money를 받는 input hanlder 작성

--- src/main/java/lotto/view/input/hanlder/infrastructure/MoneyHandler.java
+++ src/main/java/lotto/view/input/hanlder/infrastructure/MoneyHandler.java
@@ -1,4 +1,25 @@
 package lotto.view.input.hanlder.infrastructure;
 
-public class MoneyHandler {
+import lotto.view.input.domain.InputService;
+import lotto.view.input.hanlder.domain.InputHandlerService;
+
+import java.util.function.Function;
+
+public class MoneyHandler implements InputHandlerService {
+
+    private final InputService inputService;
+    public MoneyHandler(InputService inputService) {
+        this.inputService = inputService;
+    }
+
+    @Override
+    public <R> R retrieveReceive(Function<String, R> function) {
+        try {
+            String input = inputService.input();
+            return function.apply(input);
+        }
+        catch (IllegalArgumentException illegalArgumentException) {
+            return retrieveReceive(function);
+        }
+    }
 }
