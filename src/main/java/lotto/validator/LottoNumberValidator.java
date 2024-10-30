1036c962155c2e7c7b5986c26a7fdf104ee6e5af
sinminseok12 <sinminseok12@naver.com>
2024-10-31 04:11:51 +0900

    refactor(LottoValue) : 로또 관련 상수 enum 으로 분리
    로또와 관련된 상수를 공용으로 사용할 수 있게 enum 으로 분리했다.

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -1,13 +1,14 @@
 package lotto.validator;
 
+import static lotto.constants.LottoValue.MAX_LOTTO_NUMBER_RANGE;
+import static lotto.constants.LottoValue.MIN_LOTTO_NUMBER_RANGE;
+
 public class LottoNumberValidator {
 
     private static final String ERROR_LOTTO_NUMBER_RANGE = "로또 번호는 1~45 사이여야 합니다.";
-    private static final Integer MIN_LOTTO_NUMBER_RANGE = 1;
-    private static final Integer MAX_LOTTO_NUMBER_RANGE = 45;
 
     public static void validate(int number){
-        if(number < MIN_LOTTO_NUMBER_RANGE || number > MAX_LOTTO_NUMBER_RANGE) {
+        if(number < MIN_LOTTO_NUMBER_RANGE.getValue() || number > MAX_LOTTO_NUMBER_RANGE.getValue()) {
             throw new IllegalArgumentException(ERROR_LOTTO_NUMBER_RANGE);
         }
     }
--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -3,10 +3,11 @@ package lotto.validator;
 import java.util.HashSet;
 import java.util.List;
 
+import static lotto.constants.LottoValue.LOTTO_NUMBER_COUNT;
+
 public class LottoValidator {
     private static final String ERROR_LOTTO_COUNT = "로또 번호는 6개여야 합니다.";
     private static final String ERROR_LOTTO_NUMBER_DUPLICATE = "로또 번호는 중복을 허용하지 않습니다.";
-    private static final Integer LOTTO_NUMBER_COUNT = 6;
 
     public static void validateLotto(List<Integer> numbers){
         checkNumberCount(numbers);
@@ -14,7 +15,7 @@ public class LottoValidator {
     }
 
     private static void checkNumberCount(List<Integer> numbers) {
-        if(numbers.size() != LOTTO_NUMBER_COUNT){
+        if(numbers.size() != LOTTO_NUMBER_COUNT.getValue()){
             throw new IllegalArgumentException(ERROR_LOTTO_COUNT);
         }
     }
