69cf765115452810246673fc0b757cac37aa0e95
kssk3 <kss8014@daum.net>
2024-10-30 19:17:21 +0900

    Feat LottoNumber 클래스

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class LottoNumber {
+    private static final String REQUEST_1_TO_45_NUMBER = "1부터 45 사이의 수를 입력해주세요.";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+}
