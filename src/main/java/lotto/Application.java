329a865dd1eb0a5bc1b9c895619675cb0ffc34df
exena <exena01@gmail.com>
2024-10-30 17:11:23 +0900

    feat: 사용자 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String input_pay = readLine();
+        Integer number_of_times = Integer.getInteger(input_pay) / 1000;
+        System.out.println("\n" + number_of_times + "개를 구매했습니다.");
+
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input_numbers = readLine();
+        List<Integer> golden_numbers = Arrays.stream(input_numbers.split(","))
+                                          .map(Integer::parseInt)
+                                          .collect(Collectors.toList());
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String input_bonus = readLine();
+        Integer bonus_number = Integer.getInteger(input_bonus);
+        System.out.println("\n당첨 통계\n---");
     }
 }
