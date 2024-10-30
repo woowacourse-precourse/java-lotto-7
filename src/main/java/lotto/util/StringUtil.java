4dec492d42a1c88f8df2ba1aa47a80ea24665268
whdmscks11 <whdmscks11@gmail.com>
2024-10-30 17:57:28 +0900

    feat(StringUtil): 문자열을 양의 정수로 변환하는 메서드 구현

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -1,4 +1,15 @@
 package lotto.util;
 
 public class StringUtil {
+    public static Integer parseToPositiveInt(String strNum){
+        try{
+            int num = Integer.parseInt(strNum);
+            if (num <= 0) {
+                throw new IllegalArgumentException("[ERROR] 0 또는 음수 값을 입력 할 수 없습니다.");
+            }
+            return num;
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 1-45 사이의 값을 입력해 주세요.");
+        }
+    }
 }
