0bbdd74dd73a5687eb7705fe6a5571cc66700449
corpi <lj845854@gmail.com>
2024-10-30 15:56:56 +0900

    feat(LottoHistory): 로또 리스트를 가질 수 있다.

---
+++ src/main/java/lotto/model/LottoHistory.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoHistory {
+    private List<Lotto> lottoList;
+
+    public LottoHistory(List<Lotto> lottoList) {
+        this.lottoList = lottoList;
+    }
+}
