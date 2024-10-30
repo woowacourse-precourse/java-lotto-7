f6e39db56049192d29e67add6d57c25492be75be
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-30 20:51:05 +0900

    feat: 로또 구입 금액 입력 및 예외처리기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,8 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구입 금액 입력기능
+        String inputMoney = Money.inputMoney();
+        
     }
 }
---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Money {
+
+    public static String inputMoney() {
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            String inputMoney = Console.readLine();
+            return validate(inputMoney);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputMoney();
+        }
+    }
+
+    private static String validate(String inputNumber) {
+        try {
+            int money = Integer.parseInt(inputNumber);
+            if (money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1,000원으로 나누어 떨어지게 입력하세요");
+            }
+            return inputNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요");
+        }
+    }
+}
