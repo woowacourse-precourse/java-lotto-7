5a411199b96f30b36781c00eb955f0f7eb08953c
Firedrago95 <yonghwa95@naver.com>
2024-10-30 11:54:36 +0900

    Test(InputValidatorTest): 금액 입력 검사 @ParameterizedTest로 리팩토링

--- src/test/java/lotto/util/validator/InputValidatorTest.java
+++ src/test/java/lotto/util/validator/InputValidatorTest.java
@@ -9,35 +9,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
-    @Test
-    void 금액_입력_검사_테스트() {
-        //given
-        String input1 = "2000";
-        String input2 = "8000";
-
-        //then
-        Assertions.assertDoesNotThrow(() -> InputValidator.validateMoney(input1));
-        Assertions.assertDoesNotThrow(() -> InputValidator.validateMoney(input2));
-    }
-
-    @Test
-    void 금액_입력_검사_예외_테스트() {
-        //given
-        String notNum = "이천";
-        String wrongInput = "3212";
-        String empty = "";
-
-        //then
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> InputValidator.validateMoney(notNum));
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> InputValidator.validateMoney(wrongInput));
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> InputValidator.validateMoney(empty));
+    @ParameterizedTest
+    @ValueSource(strings = {"1000", "20000", "323000"})
+    void 금액_입력_검사_테스트(String input) {
+        //when & then
+        assertDoesNotThrow(() -> InputValidator.validateMoney(input));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1,2,46,3,4,5", "-1,2,3,4,5,6", "1;2;3;4;5;6", "1,2,3,4,5,6,7"})
+    @ValueSource(strings = {"이천", "3212", "0", "", "12", "201"})
+    void 금액_입력_검사_예외_테스트(String input) {
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+            () -> InputValidator.validateMoney(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,46,3,4,5", "-1,2,3,4,5,6", "1;2;3;4;5;6", "1,2,3,4,5,6,7",
+    "1,1,2,3,4,5"})
     void 당첨_번호_입력_검사_예외_테스트(String input) {
         //when & then
         assertThrows(IllegalArgumentException.class,
