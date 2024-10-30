8635d2490604a7e5040578a0ac5487d923943ded
Zrfv <youngoh07015@gmail.com>
2024-10-30 19:20:34 +0900

    feat: 당첨 번호 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,12 +6,19 @@ import lotto.view.validator.InputValidator;
 public class InputView {
 
     private static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public String inputPurchasePrice() {
         printMessage(INPUT_PURCHASE_PRICE_MESSAGE);
         String input = Console.readLine();
         InputValidator.validateNull(input);
         InputValidator.validateInteger(input);
+        return input;
+    }
+
+    public String inputWinningNumbers() {
+        printMessage(INPUT_WINNING_NUMBERS_MESSAGE);
+        String input = Console.readLine();
         return input;
     }
 
