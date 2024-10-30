60915f80185d7f1b66d85d3fd37d65bd419d4a9f
Zrfv <youngoh07015@gmail.com>
2024-10-31 01:39:14 +0900

    feat: 당첨 번호에서 중복된 번호가 존재할 때 예외 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,30 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private final static String NUMBER_AMOUNT_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private final static String DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateAmount(numbers);
+        validateDuplicate(numbers);
+        this.numbers =numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateAmount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(NUMBER_AMOUNT_ERROR_MESSAGE);
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream()
+                .distinct()
+                .count()) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE);
+        }
+    }
 }
+
