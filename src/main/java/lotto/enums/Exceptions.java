cc6cd757c97c6204908b7f8f24d634975dde5b54
Hyso <hsk7953@gmail.com>
2024-10-31 00:03:06 +0900

    feat(Exceptions) : 예외 상황에 대한 상수 추가 - 빈 칸 여부 체크 - 금액 단위 체크 - 로또 번호 중복 체크 - 로또 번호 범위 체크

--- src/main/java/lotto/enums/Exceptions.java
+++ src/main/java/lotto/enums/Exceptions.java
@@ -1,7 +1,10 @@
 package lotto.enums;
 
 public enum Exceptions {
-    NOT_BLANK("빈 칸은 입력할 수 없습니다.");
+    NOT_BLANK("빈 칸은 입력할 수 없습니다."),
+    INVALID_MONEY_UNIT("금액의 단위는 1000원이어야 합니다."),
+    DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
+    OUT_OF_RANGE("1부터 45 사이의 숫자만 입력 가능합니다.");
 
     private final String message;
 
