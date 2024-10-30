cf8794dd5357c87cf1356041e08b1fb40dac97a8
alpin87 <qortmdals94@naver.com>
2024-10-30 19:19:49 +0900

    feat: 로또 enum 기본 구조 생성

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000, "6개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    FOURTH(4, 50_000, "4개 일치"),
+    FIFTH(3, 5_000, "3개 일치"),
+    NONE(0, 0, "꽝");
+}
