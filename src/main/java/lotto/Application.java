5576c6f6ca6329783096803284443275cee9d0f1
iamdonghyuk <iamdonghyuk@gmail.com>
2024-10-30 19:44:46 +0900

    refactoring: 당첨 번호 입력 메서드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,30 +6,51 @@ import java.util.List;
 
 public class Application {
     public static final int one_ticket = 1000;
+    public static final int numSize = 6;
+    public static final String amount[] = {"5,000","50,000","1,500,000",
+            "30,000,000","2,000,000,000"};
     public class print_msg{
         static final String inputMsg = "구입금액을 입력해 주세요.";
         static final String checkMsg = "개를 구매했습니다.";
-        static final String winning_num = "당첨 번호를 입력해 주세요.";
-        static final String bonus_num = "보너스 번호를 입력해 주세요.";
-        static final String statistic = "당첨 통계";
+        static final String winning_numMsg = "당첨 번호를 입력해 주세요.";
+        static final String bonus_numMsg = "보너스 번호를 입력해 주세요.";
+        static final String statisticMsg = "당첨 통계";
+        static String matchMsg = "%d개 일치 (%s원) - %d개";
     }
-
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        Application ap = new Application();
+        ap.logic();
     }
     public void logic(){
         int num = purchase_amount();
+        purchase_history(num);
+    }
+    public Lotto inputWinningNumber(){
+        Lotto lotto = new Lotto(new ArrayList<>());
+        for(int i = 0; i < numSize; i++){
+            lotto.numbers.add(Integer.parseInt(Console.readLine()));
+        }
+        lotto.validate(lotto.numbers);
+        lotto.numbers.sort(Integer::compareTo);
+        return lotto;
     }
 
+    public void winning_history(){
+        for(int i = 3; i < 7; i++){
+            System.out.printf(print_msg.matchMsg,i,amount[i-3],3);
+        }
+    }
     public void purchase_history(int num){
-        System.out.println(num+print_msg.checkMsg);
+        System.out.println("\n"+num+print_msg.checkMsg);
         List<Integer> lotto_list [] = initialLottoArray(num);
         for(int i = 0; i < num; i++){
-            List<Integer> list = new ArrayList<>();
-            Lotto lotto = new Lotto(list);
+            Lotto lotto = new Lotto(new ArrayList<>());
             lotto_list[i] = lotto.random_range();
+            lotto.validate(lotto_list[i]);
+            lotto_list[i].sort(Integer::compare);
+            System.out.println(lotto_list[i]);
         }
     }
     public List[] initialLottoArray(int num){
@@ -55,12 +76,11 @@ public class Application {
         private final List<Integer> numbers;
 
         public Lotto(List<Integer> numbers) {
-            validate(numbers);
             this.numbers = numbers;
         }
 
         private void validate(List<Integer> numbers) {
-            if (numbers.size() != 6) {
+            if (numbers.size() != numSize) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
             }
         }
