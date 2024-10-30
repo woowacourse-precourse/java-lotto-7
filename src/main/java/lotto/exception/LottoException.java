49173a218b3ccb94f55fda90f42d2c0ab2c216cd
qyinm <qusseun@gmail.com>
2024-10-31 01:57:25 +0900

    refactor: Error 메시지 접두사 상수화 분리

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -2,7 +2,9 @@ package lotto.exception;
 
 public class LottoException extends IllegalArgumentException {
 
+    private static final String PREFIX_ERROR_MESSAGE = "[ERROR]";
+
     public LottoException(ErrorCode errorCode) {
-        super(errorCode.getErrorMessage());
+        super(PREFIX_ERROR_MESSAGE + errorCode.getMessage());
     }
 }
