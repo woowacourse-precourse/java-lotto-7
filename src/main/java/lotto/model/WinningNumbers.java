87171ef1ace277d42237e8850b01ce9e862c7db0
Firedrago95 <yonghwa95@naver.com>
2024-10-30 13:44:03 +0900

    Refactor(WinningNumbers): 메서드 분리

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -7,8 +7,11 @@ public class WinningNumbers {
     private Lotto winningNumbers;
     private int bonusNumber;
 
-    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+    public WinningNumbers(List<Integer> numbers) {
         this.winningNumbers = new Lotto(numbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
         winningNumbers.checkBonusNumberDuplication(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
