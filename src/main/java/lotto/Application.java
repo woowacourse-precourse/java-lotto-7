46bc7e945b660a4853c85d774a47f7a26ee5159b
choitaehoon <hugebigdream@gmail.com>
2024-10-30 19:36:37 +0900

    feat: 로또 구입 금액만큼의 로또 구입 개수를 구한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,10 +6,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int lottoPurchasePrice = Integer.parseInt(validate(Console.readLine()));
-
+        int lottoPurchaseQuantity = calculateLottoQuantity(lottoPurchasePrice);
     }
 
-    private static String validate(String enterPrice) {
+    public static int calculateLottoQuantity(int lottoPurchasePrice) {
+        return lottoPurchasePrice / 1000;
+    }
+
+    public static String validate(String enterPrice) {
         return enterPrice;
     }
 }
