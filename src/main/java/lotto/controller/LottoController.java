0bce9e1e57495efe7c413256582800425806a4c9
Son JunHyung <kingjh1125@gmail.com>
2024-10-30 17:22:53 +0900

    chore(Appconfig): Set AppConfig and directory structue

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,14 @@
+package lotto.controller;
+
+import lotto.view.View;
+
+public class LottoController {
+    private final View view;
+
+    public LottoController(View view) {
+        this.view = view;
+    }
+
+    public void start() {
+    }
+}
---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class View {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public View(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+}
