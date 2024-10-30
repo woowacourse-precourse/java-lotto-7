eed51d7fed92a0ca208f401085c0ad7fd8e933b1
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-30 23:52:41 +0900

    feat: 당첨번호 생성

---
+++ src/main/java/lotto/service/WinningNumber.java
@@ -0,0 +1,54 @@
+package lotto.service;
+
+import lotto.model.Lotto;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class WinningNumber {
+    private static final Integer WINNING_NUMBERS_COUNT=6;
+    private final List<Integer> numbers;
+
+    public WinningNumber(List<Integer> numbers) {
+        validateWinningNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateWinningNumbers(List<Integer> numbers) {
+        validateNull(numbers);
+        validateWinningNumbersCount(numbers);
+        validateWinningNumbersDuplicates(numbers);
+        validateWinningNumbersRange(numbers);
+    }
+
+    private void validateNull(List<Integer> numbers) {
+        if(numbers==null){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하세요");
+        }
+    }
+
+    private void validateWinningNumbersRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (1 > number || number > 46) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호가 범위를 벗어납니다.");
+            }
+        }
+    }
+
+    private void validateWinningNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != WINNING_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateWinningNumbersDuplicates(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != WINNING_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
