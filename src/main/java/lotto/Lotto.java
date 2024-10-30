3ee9c40af8797ad8e6752641099d6c5ceb0147b3
RaZel713 <razel7130@gmail.com>
2024-10-30 21:08:43 +0900

    docs(README.md): 구현할 기능 목록 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // lotto 클래스 조건
+    // Lotto에 numbers 이외의 필드(인스턴스 변수)를 추가할 수 없다.
+    // numbers의 접근 제어자인 private은 변경할 수 없다.
+    // Lotto의 패키지를 변경할 수 있다.
 }
