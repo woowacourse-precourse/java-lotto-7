16981d5b587f0714b6f0d224a14fbac03bfa14b9
hyojun <hjlim45678@gmail.com>
2024-10-30 15:10:25 +0900

    test: "일치한 로또 번호 갯수를 세는 기능" 컴파일 실패하는 단위테스트 작성
    같이 커밋한 "SetTest","StringTest"는 단위테스트 작성을 연습하기위한 용도로 로또 프로젝트와 무관합니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
@@ -21,5 +21,15 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("당첨된 로또 번호 갯수를 세는 기능 테스트")
+    @Test
+    void 당첨된_로또_번호_갯수를_세는_기능_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto  = new Lotto(List.of(1,2,3,4,5,10));
+
+        int matchCount = lotto.countMatchingNumbers(winningLotto);
+
+        Assertions.assertThat(matchCount).isEqualTo(5);
+
+    }
 }
