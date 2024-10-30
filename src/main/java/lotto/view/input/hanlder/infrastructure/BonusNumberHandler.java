79b512a8f84719e29944290534fd00470eb077bc
EHCTH <lees90896@gmail.com>
2024-10-30 18:28:18 +0900

    feat(BonusNumberHandler): 기능 구현 * feat: retrieveReceive(Function<String, R>): R (InputHandlerService 구현) BonusNumber는 WinningLotto 중복 검증을 위하여 BonusNumberCreator 클래스에서 재귀 호출

--- src/main/java/lotto/view/input/hanlder/infrastructure/BonusNumberHandler.java
+++ src/main/java/lotto/view/input/hanlder/infrastructure/BonusNumberHandler.java
@@ -5,19 +5,16 @@ import lotto.view.input.hanlder.domain.InputHandlerService;
 
 import java.util.function.Function;
 
-public class BonusLottoHandler implements InputHandlerService {
+public class BonusNumberHandler implements InputHandlerService {
     private final InputService inputService;
-    public BonusLottoHandler(InputService inputService) {
+
+    public BonusNumberHandler(InputService inputService) {
         this.inputService = inputService;
     }
+
     @Override
     public <R> R retrieveReceive(Function<String, R> function) {
-        try {
-            String input = inputService.input();
-            return function.apply(input);
-        }
-        catch (IllegalArgumentException illegalArgumentException) {
-            return retrieveReceive(function);
-        }
+        String input = inputService.input();
+        return function.apply(input);
     }
 }
