c824a44f130e6781072c6e6ea0dff7ea709d1653
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-30 18:12:46 +0900

    feat: 로또를 몇개를 구매해야하는지 계산하는 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,20 +3,25 @@ package lotto.controller;
 import lotto.lotto.Lotto;
 import lotto.handler.InputHandler;
 import lotto.handler.PrintHandler;
+import lotto.lotto.LottoStore;
 
 public class Controller {
     InputHandler inputHandler;
     PrintHandler printHandler;
     Lotto lotto;
+    LottoStore lottoStore;
 
     public Controller() {
         inputHandler = new InputHandler();
         printHandler = new PrintHandler();
+        lottoStore = new LottoStore();
     }
 
     public void run () {
         printHandler.PrintBuyMoneyAmount();
         inputHandler.setInputMoney();
+        int money = inputHandler.getInputMoney();
+        lottoStore.calculateNumberOfPurchases(money);
 
     }
 }
--- src/main/java/lotto/lotto/LottoStore.java
+++ src/main/java/lotto/lotto/LottoStore.java
@@ -1,4 +1,15 @@
 package lotto.lotto;
 
 public class LottoStore {
+    private static final int LOTTO_PRICE = 1000;
+    private int lottoNumberOfPurchases;
+
+
+
+    public void calculateNumberOfPurchases(int money) {
+        if (money % LOTTO_PRICE != 0){
+            throw new IllegalArgumentException("잘못된 입력 값 입니다. 확인 바랍니다.");
+        }
+        lottoNumberOfPurchases = money % LOTTO_PRICE;
+    }
 }
