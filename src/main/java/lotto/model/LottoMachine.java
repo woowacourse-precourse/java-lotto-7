ed73ba8962658c369cf7b83160353d68662e22cb
joshinhyeok <shtree07@naver.com>
2024-10-31 05:17:09 +0900

    feat: 구입 금액에 맞게 로또를 구매하는 기능 추가

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -6,9 +6,21 @@ import java.util.List;
 public class LottoMachine {
     private final int LOTTO_PRICE = 1000;
 
+    private final List<Lotto> lottos = new ArrayList<>();
+
     public void buyLotto(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("로또 구매 금액이 로또 금액으로 나누어 떨어지지 않습니다.");
         }
+
+        int numberOfLotto = purchaseAmount / LOTTO_PRICE;
+
+        for (int i = 0; i < numberOfLotto; i++) {
+            lottos.add(new Lotto(LottoGenerator.generate()));
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }
