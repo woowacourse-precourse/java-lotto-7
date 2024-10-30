53b1882bcec2b4857d61f05e6e9d1e7ce0c499e7
ehgur062300 <ehgur062300@gmail.com>
2024-10-31 02:20:30 +0900

    refactor(ErrorCause): Move position from exception package to enums package

--- src/main/java/lotto/enums/ErrorCause.java
+++ src/main/java/lotto/enums/ErrorCause.java
@@ -1,9 +1,11 @@
-package lotto.exception;
+package lotto.enums;
 
 public enum ErrorCause {
+
+    PURCHASE_PRICE_CANNOT_BE_ZERO_OR_NEGATIVE("구입 금액은 0보다 커야합니다."),
     ACCESS_ATTEMPT_EXCEEDED("회 이상 실패로 앱이 종료됩니다."),
-    PURCHASE_AMOUNT_UNIT("구입 금액은 딱 떨어지게 입력해야 합니다."),
-    INPUT_VALUE("숫자를 입력해 주세요."),
+    PURCHASE_PRICE_UNIT("구입 금액은 딱 떨어지게 입력해야 합니다."),
+    INPUT_VALUE("숫자를 입력해 주세요.")
     ;
 
     private final String message;
