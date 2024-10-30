a7026f8bb3d9cac6cbaf6beabd9778cf0359da7b
CheChe903 <cheche903@naver.com>
2024-10-30 17:11:07 +0900

    feat(Lotto): 로또 중복 검증 메서드 구현
    이전의 커밋에서 구현한 객체 비교 메서드들로 validateDuplicate 메서드를 구현합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.domain.LottoNumber;
 
 public class Lotto {
@@ -8,12 +10,20 @@ public class Lotto {
 
     public Lotto(List<LottoNumber> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validateSize(List<LottoNumber> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<LottoNumber> numbers) {
+        Set<LottoNumber> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
