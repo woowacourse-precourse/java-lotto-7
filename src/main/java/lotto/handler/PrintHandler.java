d5263220b4a2b351c4a1347242ab07d385364af4
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-30 18:46:10 +0900

    style: 메소드 컨벤션에 맞게 수정

--- src/main/java/lotto/handler/PrintHandler.java
+++ src/main/java/lotto/handler/PrintHandler.java
@@ -3,11 +3,13 @@ package lotto.handler;
 public class PrintHandler {
     private static final String PRINT_BUY_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
 
-    public void PrintBuyMoneyAmount() {
+    public void printBuyMoneyAmount() {
         System.out.println(PRINT_BUY_MONEY_AMOUNT);
     }
 
-    public void PrintBuyLottoNumbersOfPurchases(int numberOfPurchases) {
+    public void printBuyLottoNumbersOfPurchases(int numberOfPurchases) {
         System.out.println(numberOfPurchases + "개를 구매했습니다.");
     }
+
+    public void Print
 }
