487deaed051aa041cdf86fb9674de0d8096e176f
kungbi <kipper12335@gmail.com>
2024-10-30 18:22:14 +0900

    refactor(model): 하드코딩된 부분 상수화

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -20,7 +20,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throwIllegalArgument(INVALID_NUMBER_COUNT);
         }
         if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
