241602c6745976b5ea265c2a77e5eb2887a0b74a
dhrgodms <dhrgodms123@gmail.com>
2024-10-31 01:01:45 +0900

    test: 로또_금액_입력 - 로또 금액 입력 및 예외 처리 test
    - ValidatorTest.java: PRICE_01, TYPE_01 예외처리 test를 추가하였다.

---
+++ src/test/java/lotto/ValidatorTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import lotto.Utils.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    Validator validator = new Validator();
+
+    @DisplayName("PRICE_01: 1000원 단위가 아니면 예외 발생")
+    @Test
+    void 가격이_1000단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> validator.validatePrice1000(45))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 구매 금액은 1000원 단위로만 입력 가능합니다.");
+    }
+
+    @DisplayName("TYPE_01: 숫자가 아닌 값이 포함되어 있으면 예외 발생")
+    @Test
+    void 숫자가_아닌_값이면_예외가_발생한다() {
+        assertThatThrownBy(() -> validator.validateInteger("1000;"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 숫자만 입력 가능합니다.");
+    }
+}
