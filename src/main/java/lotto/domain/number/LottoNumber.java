66756dae3fd2a8213502f2839eef2c5c9e0bc830
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 20:34:41 +0900

    refactor: 정적 펙토리 매서드를 생성자 오버로딩으로 변경

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -7,19 +7,15 @@ public class LottoNumber {
 
     private final int lottoNumber;
 
-    private LottoNumber(int lottoNumber) {
+    public LottoNumber(int lottoNumber) {
         if(lottoNumber<MINIMUM_LOTTO_NUMBER || lottoNumber >MAXIMUM_LOTTO_NUMBER){
             throw new IllegalArgumentException("//여기 enum 에러값");
         }
         this.lottoNumber = lottoNumber;
     }
 
-    public static LottoNumber from(int number) {
-        return new LottoNumber(number);
-    }
-
-    public static LottoNumber from(NumberGenerator numberGenerator){
-        return new LottoNumber(numberGenerator.create(MINIMUM_LOTTO_NUMBER,MAXIMUM_LOTTO_NUMBER));
+    public LottoNumber(NumberGenerator numberGenerator) {
+        this.lottoNumber= numberGenerator.create(MINIMUM_LOTTO_NUMBER,MAXIMUM_LOTTO_NUMBER);
     }
 
 }
