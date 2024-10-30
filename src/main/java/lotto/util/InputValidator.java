2c1d36a51cd55e03f6d663632e504db517a95c3d
mininiii <didals0521@gmail.com>
2024-10-31 02:51:41 +0900

    feat: 사용자 입력 내용 검증 기능 추가

---
+++ src/main/java/lotto/util/InputValidator.java
@@ -0,0 +1,24 @@
+package lotto.util;
+
+public class InputValidator {
+    public void validateRange(int number) {
+        if (number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 최대값은 45 이하여야 합니다.");
+        }
+        if(number < 1) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 최소값은 1 이상이어야 합니다.");
+        }
+    }
+
+    public void canDividedByThousand(long purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 나누어 떨어져야 합니다.");
+        }
+    }
+
+    public void isNotNegativeNumber(long purchaseAmount) {
+        if (purchaseAmount < 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
+        }
+    }
+}
