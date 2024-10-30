0e0098b348fe02286f390e6e117a438b3ee5f858
bowook <mbosx316@naver.com>
2024-10-31 00:59:07 +0900

    feat(Parser) : Parser 기능 추가
    - String -> List
    - String -> Integer

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,2 +1,30 @@
-package lotto.utils;public class Parser {
+package lotto.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.validator.exception.ErrorMessage;
+import lotto.validator.exception.LottoException;
+
+public class Parser {
+
+    public static List<Integer> parsingNumbers(String winningNumbers) {
+        List<Integer> convertedWinningNumbers = new ArrayList<>();
+        List<String> splitWinningNumbers = List.of(winningNumbers.split(","));
+        for(String number : splitWinningNumbers) {
+            try {
+                convertedWinningNumbers.add(Integer.parseInt(number));
+            } catch (NumberFormatException e) {
+                throw LottoException.from(ErrorMessage.WINNING_LOTTO_IS_NOT_NUMBER);
+            }
+        }
+        return convertedWinningNumbers;
+    }
+
+    public static int parsingPurchaseAmount(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw LottoException.from(ErrorMessage.LOTTO_PURCHASE_IS_NOT_NUMBER);
+        }
+    }
 }
