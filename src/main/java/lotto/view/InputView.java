52c3d87488e220f39d9d1cf9a52ee232ca94d5b2
corpi <lj845854@gmail.com>
2024-10-30 15:45:09 +0900

    feat(InputView): 6개의 당첨 번호를 입력받을 수 있다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
 
         return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        String[] splitInput = input.split(",");
+
+        return Arrays.stream(splitInput).map(Integer::parseInt).toList();
     }
 }
