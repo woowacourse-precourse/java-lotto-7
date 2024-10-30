a840b2068d0a852aa3f32a6ae29072f848c3b325
corpi <lj845854@gmail.com>
2024-10-30 16:43:05 +0900

    refactor(LottoRank): 로또 등수로 이름 변경

---
+++ src/main/java/lotto/model/LottoRank.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public enum LottoRank {
+    RANK_1(2_000_000_000),
+    RANK_2(30_000_000),
+    RANK_3(1_500_000),
+    RANK_4(50_000),
+    RANK_5(5_000);
+
+    private final int prizeMoney;
+
+    LottoRank(int prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+}
