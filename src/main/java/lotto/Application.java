3a67ef6c17de3b4176a23a98084ffb1b0f418a5b
LeeJaeHoon <dlwogns3413@naver.com>
2024-10-30 17:46:57 +0900

    feat: 구입 금액만큼 로또 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.view.InputView;
 
 public class Application {
@@ -7,6 +11,13 @@ public class Application {
 
         InputView inputView = new InputView();
         String lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
-        System.out.println(lottoPurchaseMoney);
+
+        int lottoCount = Integer.parseInt(lottoPurchaseMoney) / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
