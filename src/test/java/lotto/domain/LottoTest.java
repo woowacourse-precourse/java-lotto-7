2050bc7e040505e9bcf7ea48581d91f75b4936ce
dnwldla <leeemjw@naver.com>
2024-10-30 23:33:20 +0900

    test: 보너스 번호 유무에 따라 서로 다른 enum을 반환하는 테스트 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @Test
@@ -22,7 +24,6 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
     @Test
     void 로또번호_생성시_오름차순으로_정렬된다(){
         Lotto lotto=new Lotto(List.of(6,5,4,3,2,1));
@@ -30,5 +31,17 @@ class LottoTest {
         Lotto expected=new Lotto(List.of(1,2,3,4,5,6));
 
         assertThat(lotto).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+   @Test
+    void 보너스_여부에_따라_서로_다른_enum을_반환한다(){
+        Lotto lotto=new Lotto(List.of(6,5,4,3,2,1));
+
+        assertAll(
+                ()->assertEquals(lotto.findBonus(5,6), BonusExistence.HAVE_BONUS),
+                ()->assertEquals(lotto.findBonus(5,10), BonusExistence.NO_BONUS),
+                ()->assertEquals(lotto.findBonus(4,10), BonusExistence.IRRELEVANT)
+        );
+
     }
 }
