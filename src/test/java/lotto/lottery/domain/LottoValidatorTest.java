d9c460f934433fbf2499a05acf4926b854354ef8
param <spsummerdoc@gmail.com>
2024-10-30 10:12:53 +0900

    test(lottoValidator): 로또 구매 금액 1,000~100,000 범위 테스트 로직 추가

--- src/test/java/lotto/lottery/domain/LottoValidatorTest.java
+++ src/test/java/lotto/lottery/domain/LottoValidatorTest.java
@@ -1,10 +1,13 @@
 package lotto.lottery.domain;
 
+import static lotto.global.util.ErrorMessage.INVALID_AMOUNT;
+import static lotto.global.util.ErrorMessage.INVALID_AMOUNT_RANGE;
+import static lotto.global.util.LottoConst.MAX_PRICE;
+import static lotto.global.util.LottoConst.MIN_PRICE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.stream.Stream;
-import lotto.global.util.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,7 +23,7 @@ class LottoValidatorTest {
         int amount = 5000;
 
         // then
-        assertDoesNotThrow(() -> LottoValidator.validateAmount(amount));
+        assertDoesNotThrow(() -> LottoValidator.validate(amount));
     }
 
     @ParameterizedTest
@@ -28,9 +31,9 @@ class LottoValidatorTest {
     @MethodSource("providedInvalidAmount")
     void invalidAmountError(int amount) throws Exception {
         // then
-        assertThatThrownBy(() -> LottoValidator.validateAmount(amount))
+        assertThatThrownBy(() -> LottoValidator.validate(amount))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_AMOUNT.getMessage());
+                .hasMessage(INVALID_AMOUNT.getMessage());
     }
 
     private static Stream<Arguments> providedInvalidAmount(){
@@ -41,5 +44,41 @@ class LottoValidatorTest {
                 Arguments.arguments(4100)
         );
     }
+
+    @ParameterizedTest
+    @DisplayName("구매 금액이 1,000 ~ 100,000 사이라면 정상적으로 작동한다")
+    @MethodSource("providedPurchaseLotto")
+    void purchaseLotto(int amount) throws Exception {
+        // then
+        assertDoesNotThrow(() -> LottoValidator.validate(amount));
+    }
+
+    private static Stream<Arguments> providedPurchaseLotto(){
+        return Stream.of(
+                Arguments.arguments(1000),
+                Arguments.arguments(100_000),
+                Arguments.arguments(99_000)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매 금액이 1,000 ~ 100,000가 아니라면 에러를 반환한다")
+    @MethodSource("providedPurchaseOutOfRange")
+    void purchaseLottoOutOfRange(int amount) throws Exception {
+        // then
+        assertThatThrownBy(() -> LottoValidator.validate(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format(INVALID_AMOUNT_RANGE.getMessage(), MIN_PRICE,MAX_PRICE));
+    }
+
+    private static Stream<Arguments> providedPurchaseOutOfRange(){
+        return Stream.of(
+                Arguments.arguments(0),
+                Arguments.arguments(-3000),
+                Arguments.arguments(101_000)
+        );
+    }
+
+
 
 }
