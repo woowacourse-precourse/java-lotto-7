c7761fe063fec7a7e470ce5982c532489c750b80
bowook <mbosx316@naver.com>
2024-10-31 00:58:17 +0900

    feat(ErrorMessage) : ErrorMessage 메시지 추가
    - 로또 관련 예외 메시지 추가

--- src/main/java/lotto/validator/exception/ErrorMessage.java
+++ src/main/java/lotto/validator/exception/ErrorMessage.java
@@ -2,7 +2,11 @@ package lotto.validator.exception;
 
 public enum ErrorMessage {
     LOTTO_PURCHASE_IS_NOT_NUMBER("[ERROR] 구입 금액은 숫자만 입력하셔야 합니다."),
-    LOTTO_PURCHASE_IS_NOT_THOUSAND_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력하셔야 합니다.");
+    LOTTO_PURCHASE_IS_NOT_THOUSAND_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력하셔야 합니다."),
+    WINNING_LOTTO_IS_NOT_NUMBER("[ERROR] 당첨 번호는 숫자를 입력하셔야 합니다."),
+    WINNING_LOTTO_IS_NOT_SIX_LENGTH("[ERROR] 당첨 번호는 6자리 입력하셔야 합니다."),
+    WINNING_LOTTO_RANGE_ERROR("[ERROR] 당첨 번호는 1~45 사이의 수를 입력하셔야 합니다."),
+    WINNING_LOTTO_DUPLICATE_ERROR("[ERROR] 당첨 번호는 중복된 수 없이 입력하셔야 합니다.");
 
     private final String errorMessage;
     ErrorMessage(String errorMessage) {
