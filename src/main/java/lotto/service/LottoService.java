34ffc57c6f94451d92fa31f453fb9225df01db90
Ferrorist <ferrorist@naver.com>
2024-10-31 00:28:37 +0900

    feat(issue Lotto): 로또 발행 기능 구현
    주어진 금액에 맞춰 로또를 발행합니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,17 +1,41 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domains.Lotto;
+
 public class LottoService {
-    private final int lottoCost = 1_000;
+    private static final int LOTTO_COST = 1_000;
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     public int getLottoCost() {
-        return lottoCost;
+        return LOTTO_COST;
     }
 
     public int issueRottoCount(int cost) throws IllegalArgumentException {
-        if (cost % lottoCost > 0) {
-            throw new IllegalArgumentException("[Error] 구입 금액은 " + lottoCost + "원 단위이어야 합니다.");
+        if (cost % LOTTO_COST > 0) {
+            throw new IllegalArgumentException("[Error] 구입 금액은 " + LOTTO_COST + "원 단위이어야 합니다.");
         }
 
-        return cost / lottoCost;
+        return cost / LOTTO_COST;
+    }
+
+
+    public List<Lotto> issueLotto(int cost) {
+        int lottoCount = issueRottoCount(cost);
+        List<Lotto> issueLottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            issueLottos.add(generateLotto());
+        }
+
+        return issueLottos;
+    }
+
+    private Lotto generateLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, LOTTO_NUMBER_COUNT));
     }
 }
