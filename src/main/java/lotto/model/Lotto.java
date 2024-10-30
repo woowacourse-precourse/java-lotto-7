684a7c3a348726d8b8c64f99d6f05af9adaebce7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-30 19:48:43 +0900

    feat: add validation of lotto numbers
    - validate count of lotto numbers
    - validate range of lotto numbers
    - validate duplicated lotto numbers

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,57 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(final List<Integer> numbers) {
+        checkCountOfLottoNumbers(numbers.size());
+        checkMinimumOfLottoNumber(numbers);
+        checkMaximumOfLottoNumber(numbers);
+        checkDuplicatedNumbers(numbers);
+    }
+
+    private void checkCountOfLottoNumbers(final int size) {
+        if (size != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkMinimumOfLottoNumber(final List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number < MIN_LOTTO_NUMBER)
+                .findAny()
+                .ifPresent(invalidNumber -> {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1이상의 숫자여야 합니다.");
+                });
+    }
+
+    private void checkMaximumOfLottoNumber(final List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number > MAX_LOTTO_NUMBER)
+                .findAny()
+                .ifPresent(invalidNumber -> {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 45이하의 숫자여야 합니다.");
+                });
+    }
+
+    private void checkDuplicatedNumbers(final List<Integer> numbers) {
+        final long distinctCount = numbers.stream()
+                .distinct()
+                .count();
+        if (distinctCount != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 존재합니다.");
+        }
+    }
+}
