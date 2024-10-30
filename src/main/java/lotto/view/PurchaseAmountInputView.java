5d7496fcc103f89d6b7f5d39d30125ba2459244a
unh6unh6 <ruddbs3308@naver.com>
2024-10-30 18:19:35 +0900

    feat(view): 구입금액을 입력 받는다

--- src/main/java/lotto/view/PurchaseAmountInputView.java
+++ src/main/java/lotto/view/PurchaseAmountInputView.java
@@ -1,9 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class PurchaseAmountInputView {
     private static final String PURCHASE_AMOUNT_INPUT_GUIDE = "구입금액을 입력해 주세요.";
 
     public void printPurchaseAmountInputGuide() {
         System.out.println(PURCHASE_AMOUNT_INPUT_GUIDE);
+    }
+
+    public String getPurchaseAmount() {
+        String purchaseAmount = Console.readLine();
+        return purchaseAmount;
     }
 }
