05c48691f2c6a1836d5d9b50a75103b636c4c53c
jbilee <julishahn@gmail.com>
2024-10-30 17:24:33 +0900

    feat(validator): 로또 번호 검증 기능 개선
    - 중복되는 번호를 검증하는 로직 추가

--- src/main/java/lotto/validators/LottoNumberValidator.java
+++ src/main/java/lotto/validators/LottoNumberValidator.java
@@ -1,6 +1,7 @@
 package lotto.validators;
 
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,6 +13,7 @@ public class LottoNumberValidator implements Validator {
     private static final String INCORRECT_TYPE = "소수점이 없는 양수를 입력해 주세요.";
     private static final String NOT_WITHIN_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String INCORRECT_NUMBER_COUNT = "콤마(,)로 구분된 6개의 숫자를 입력해 주세요.";
+    private static final String CONTAINS_DUPLICATE = "중복되는 번호가 있습니다.";
 
     @Override
     public void validate(String input) {
@@ -20,6 +22,7 @@ public class LottoNumberValidator implements Validator {
         if (numbers.size() != ARRAY_SIZE) {
             throw new IllegalArgumentException(INCORRECT_NUMBER_COUNT);
         }
+        checkDuplicate(input);
 
         for (String number : numbers) {
             checkInputType(number);
@@ -39,6 +42,14 @@ public class LottoNumberValidator implements Validator {
         int amount = Integer.parseInt(input);
         if (amount < MIN_VALUE || amount > MAX_VALUE) {
             throw new IllegalArgumentException(NOT_WITHIN_RANGE);
+        }
+    }
+
+    private void checkDuplicate(String input) {
+        try {
+            Set.of(input.split(SPLIT_DELIMITER));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(CONTAINS_DUPLICATE);
         }
     }
 }
