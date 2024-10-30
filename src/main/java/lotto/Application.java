e086ab7866c30a56b4f777f1e76c8b79a05244ee
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-30 23:04:34 +0900

    feat: 로또 구매 수 count 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         int lottoPrice = Integer.parseInt(Console.readLine());
+
+        int lottoCount = lottoPrice / 1000;
+        System.out.println("\n" + lottoCount + "개를 구매했습니다.");
     }
 }
