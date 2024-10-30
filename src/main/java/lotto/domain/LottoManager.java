95eaff7018d14e9c02d56bc8bc3fee7184b8e163
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-30 19:15:52 +0900

    feat(LottoManager): 로또 수량만큼 로또를 발행하는 기능 구현
    1부터 45까지의 중복되지 않는 6개의 숫자를 뽑는 기능 포함

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -1,10 +1,30 @@
 package lotto.domain;
 
+import static lotto.utils.Constant.MAX_LOTTO_NUMBER;
+import static lotto.utils.Constant.MAX_LOTTO_NUMBER_COUNT;
+import static lotto.utils.Constant.MIN_LOTTO_NUMBER;
 import static lotto.utils.Constant.MIN_PURCHASE_AMOUNT;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
+    private final List<Lotto> lottos = new ArrayList<>();
 
     public void generateLottoNumbers(int purchaseAmount) {
         int lottoCount = purchaseAmount / MIN_PURCHASE_AMOUNT;
+
+        for (int count = 0; count < lottoCount; count++) {
+            lottos.add(new Lotto(generateRandomNumbers()));
+        }
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER_COUNT
+        );
     }
 }
--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -7,6 +7,9 @@ public class Constant {
     public static final String INTEGER_REGEX = "[0-9]+";
 
     public static final int MIN_PURCHASE_AMOUNT = 1000;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MAX_LOTTO_NUMBER_COUNT = 6;
 
     private Constant() {
     }
