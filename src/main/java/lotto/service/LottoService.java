daa90d6c133ee53f50aafba691ec30a91cb88f0c
bowook <mbosx316@naver.com>
2024-10-30 19:53:40 +0900

    feat(LottoService) : LottoSerivce 기능 추가
    - Controller에서 각 서비스에 대한 의존을 갖는것보다 로또 서비스 하나로 관리를 하면 더 좋을 것 같다는 생각에 추가하였습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,18 @@
 package lotto.service;
 
-public class PurchaseService {
+public class LottoService {
+    private final PurchaseService purchaseService;
+    private final LottoGeneratorService lottoGeneratorService;
+    private final StatisticsService statisticsService;
+    private final ProfitCalculatorService profitCalculatorService;
+    public LottoService(PurchaseService purchaseService, LottoGeneratorService lottoGeneratorService, StatisticsService statisticsService, ProfitCalculatorService profitCalculatorService) {
+        this.purchaseService = purchaseService;
+        this.lottoGeneratorService = lottoGeneratorService;
+        this.statisticsService = statisticsService;
+        this.profitCalculatorService = profitCalculatorService;
+    }
+
+    public void purchaseLotto(String purchaseAmount) {
+        purchaseService.processPurchase(purchaseAmount);
+    }
 }
