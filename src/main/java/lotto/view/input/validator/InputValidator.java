f499e6968e4a0833a4bdf5ad8b323088b8a8e9db
EHCTH <lees90896@gmail.com>
2024-10-30 18:35:36 +0900

    feat(InputValdiator): 검증 기능 구현 * feat: moneyValidate(String): void * feat: bonusNumberValidate(String): void * feat: winningLottoValidate(String): void

--- src/main/java/lotto/view/input/validator/InputValidator.java
+++ src/main/java/lotto/view/input/validator/InputValidator.java
@@ -1,4 +1,15 @@
 package lotto.view.input.validator;
 
+import lotto.view.input.domain.RegexPattern;
+
 public class InputValidator {
+    public static void moneyValidate(String input) {
+        if (!RegexPattern.NUMBER_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 양의 정수만 입력해주세요");
+    }
+    public static void bonusNumberValidate(String input) {
+        if (!RegexPattern.NUMBER_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 양의 정수만 입력해주세요");
+    }
+    public static void winningLottoValidate(String input) {
+        if (!RegexPattern.LOTTO_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 ex)1,2,3,4,5,6 형식으로 입력해주세요");
+    }
 }
