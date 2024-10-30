5ec96e34e5f2f07a4e52b8b638a609e72f2a959d
bowook <mbosx316@naver.com>
2024-10-30 23:01:36 +0900

    feat(OutputMessage) : OutputMessage 기능 추가
    - Enum으로 메시지 관리

--- src/main/java/lotto/view/output/OutputMessage.java
+++ src/main/java/lotto/view/output/OutputMessage.java
@@ -1,4 +1,15 @@
 package lotto.view.output;
 
 public enum OutputMessage {
+    PURCHASED_COUNT_MESSAGE("\n%d개를 구매했습니다.\n");
+
+    private final String outputMessage;
+
+    OutputMessage(String outputMessage) {
+        this.outputMessage = outputMessage;
+    }
+
+    public String getOutputMessage() {
+        return outputMessage;
+    }
 }
