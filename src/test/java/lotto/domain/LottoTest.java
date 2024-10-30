060e5ec4beced0ff47393ff25132df9a4319ba0f
mohyerolo <garam2101@naver.com>
2024-10-30 15:53:27 +0900

    test: 로또 번호 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,12 +4,17 @@ import camp.nextstep.edu.missionutils.test.Assertions;
 import lotto.service.RandomUniqueLottoNumGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LottoTest {
     @Test
@@ -25,6 +30,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    static Stream<Arguments> listParameterProvider() {
+        return Stream.of(
+                arguments(List.of(1, 2, 3, 4, 5, 46)),
+                arguments(List.of(0, 1, 2, 3, 4, 5))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("listParameterProvider")
+    void 로또_번호_1보다작고_45보다크면_에러(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_객체_생성_성공() {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)).toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+
     @Test
     void 로또_번호_오름차순_정렬() {
         String sortedLotto = "[1, 2, 3, 4, 5, 6]";
@@ -34,6 +58,8 @@ class LottoTest {
                 },
                 new ArrayList<>(List.of(6, 2, 1, 3, 5, 4))  // List.of는 ImmutableCollections라 리스트 메서드 지원 X
         );
+
+
     }
 
 }
