cbb3132f84db4bd3686d0a2652d18f37b740d8da
menzzi <minmin3927@naver.com>
2024-10-30 18:15:10 +0900

    feat(controller) : LottoController 구현(미완성)

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,57 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.util.Calculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView input;
+    private final OutputView output;
+
+
+    public LottoController(InputView input, OutputView output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void proceed(){
+        int lottoAmount = inputLottoAmount();
+        int lottoTicketNumber = Calculator.calculateLottoTicketNumber(lottoAmount);
+        List<Lotto> userLotto = LottoMachineController.issueLotto(lottoTicketNumber);
+        output.printLottoTicket(userLotto,lottoTicketNumber);
+
+        List<Integer> winningLotto = inputWinningNumber();
+        int bonusNumber = inputBonusNumber();
+        // 보너스 번호 당첨 번호와 중복되는지 확인
+        // 발행된 로또와 당첨 로또, 보너스 비교
+    }
+
+    public int inputLottoAmount(){
+        try{
+            return input.inputLottoAmount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputLottoAmount();
+        }
+    }
+
+    public List<Integer> inputWinningNumber(){
+        try{
+            return input.inputWinningNumber();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningNumber();
+        }
+    }
+
+    public int inputBonusNumber(){
+        try{
+            return input.inputBonusNumber();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputBonusNumber();
+        }
+    }
+}
