0e06e402f3f1bb6086dc4e0c8550b1702bf437b7
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-10-30 20:03:51 +0900

    test(purchase) : 구입금액에 숫자이외의 입력시 오류테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.validator.Validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,13 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구입금액에 숫자 이외의 입력이 들어오면 예외가 발생한다.")
+    @Test
+    void 구입금액에_숫자_이외의_입력이_들어오면_예외가_발생한다() {
+        assertThatThrownBy(() -> Validator.validateAmount("8000qwer"))
+                .isInstanceOf(NumberFormatException.class); //NumberFormatException은 IllegalArgumentException을 상속받아 둘다 나옴
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
