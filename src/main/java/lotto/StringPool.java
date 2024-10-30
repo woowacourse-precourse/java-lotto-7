f654984f0627b37e22d22b23c8b968ccbf795227
LimJaeEun0930 <je9513666@gmail.com>
2024-10-30 23:37:02 +0900

    refactor: extract Strings to StringPool class
    프로그램에서 콘솔에 출력하는 문구들을 한 클래스에 몰아넣기로 함.

---
+++ src/main/java/lotto/StringPool.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class StringPool {
+    public static final String INSERT_MONEY = "구입금액을 입력해 주세요.";
+    public static final String PRINT_SOLD_LOTTO_COUNT = "개를 구매했습니다.";
+    public static final String MONEY_NOT_DIVIDED_BY_1000 = "[ERROR] 로또 구입 금액은 1,000원 단위로 입력 가능합니다.";
+}
