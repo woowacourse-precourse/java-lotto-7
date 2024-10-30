dd2304c611e6702ddb331958b3b4bb702bd80db8
EHCTH <lees90896@gmail.com>
2024-10-30 18:21:29 +0900

    feat(Convertor): 기능 구현 feat(Convertor): 기능 추가 * feat: splitByList(String): List<Integer> * feat: stringToInt(String): int

--- src/main/java/lotto/util/Convertor.java
+++ src/main/java/lotto/util/Convertor.java
@@ -1,7 +1,20 @@
 package lotto.util;
 
+import lotto.constant.Separator;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Convertor {
     public static Long stringToLong(String input) {
         return Long.parseLong(input);
+    }
+    public static List<Integer> splitByList(String input) {
+        return Arrays.stream(input.split(Separator.COMMA.getSeparator()))
+                .map(Integer::parseInt)
+                .toList();
+    }
+    public static int stringToInt(String input) {
+        return Integer.parseInt(input);
     }
 }
