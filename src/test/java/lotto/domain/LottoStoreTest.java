f8dd9b5ab0fd10ba1273cd5b24f58553aa0ccd59
sinminseok12 <sinminseok12@naver.com>
2024-10-31 05:06:55 +0900

    test(LottoStoreTest) : 로또 구입시 구입 금액 예외처리 테스트 구현
    로또 구입 금액이 1000원 단위가 아닐 경우 예외를 발생시키는 테스트를 만들었다.

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -3,8 +3,12 @@ package lotto.domain;
 import lotto.utils.TestNumbersGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoStoreTest {
 
@@ -16,5 +20,16 @@ public class LottoStoreTest {
         LottoTicket lottoTicket = lottoStore.buyLottoTicket(3000);
 
         Assertions.assertThat(lottoTicket.getLottos().size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 999, -1})
+    void 구입_금액이_1000원_딘위가_아니면_예외가_발생한다(int price){
+        TestNumbersGenerator testNumbersGenerator = new TestNumbersGenerator(List.of(1, 2, 3, 4, 5, 6));
+        LottoStore lottoStore = new LottoStore(testNumbersGenerator);
+
+        assertThatThrownBy(() -> lottoStore.buyLottoTicket(price))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
