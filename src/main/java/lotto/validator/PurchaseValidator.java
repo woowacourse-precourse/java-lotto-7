8dd06030c4b132b107b8d94af78f83a21cf2e004
bowook <mbosx316@naver.com>
2024-10-30 23:06:45 +0900

    refactor(PurchaseValidator) : PurchaseValidator 리팩토링
    - ErrorMessage 사용

--- src/main/java/lotto/validator/PurchaseValidator.java
+++ src/main/java/lotto/validator/PurchaseValidator.java
@@ -1,4 +1,25 @@
 package lotto.validator;
 
+import lotto.validator.exception.ErrorMessage;
+import lotto.validator.exception.LottoException;
+
 public class PurchaseValidator {
+
+    public void validateProcess(String purchaseAmount) {
+        validateIsNumber(purchaseAmount);
+        validateThousandUnit(purchaseAmount);
+    }
+
+    private void validateIsNumber(String purchaseAmount) {
+        try {
+            Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw LottoException.from(ErrorMessage.LOTTO_PURCHASE_IS_NOT_NUMBER);
+        }
+    }
+    private void validateThousandUnit(String convertedPurchaseAmount) {
+        if(Integer.parseInt(convertedPurchaseAmount) % 1000 != 0) {
+            throw LottoException.from(ErrorMessage.LOTTO_PURCHASE_IS_NOT_THOUSAND_UNIT);
+        }
+    }
 }
