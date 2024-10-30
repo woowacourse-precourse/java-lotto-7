c32c7433f96748d55ed291f8e8586594501134c7
joshinhyeok <shtree07@naver.com>
2024-10-31 00:27:42 +0900

    feat: 로또 번호에 중복된 숫자가 있는지 확인하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,7 +10,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validateThereAreSixNumbers(numbers);
         validateMeetNumberRangeLimit(numbers);
-
+        validateThereAreDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -23,6 +25,13 @@ public class Lotto {
             if (number < 1 || 45 < number) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+    }
+
+    private void validateThereAreDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> duplicateNumbers = new HashSet<>(numbers);
+        if (duplicateNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 }
