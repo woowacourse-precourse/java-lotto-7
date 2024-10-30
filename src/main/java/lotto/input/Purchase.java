a1478ac4cbb4993edb3cf4968812ef304c8bccff
bmg1203 <bmg1203@naver.com>
2024-10-31 02:15:35 +0900

    [feat]: 구입 금액 양수 유효성 검사 추가
    - 구입 금액이 양수가 아니면 예외 발생

--- src/main/java/lotto/input/Purchase.java
+++ src/main/java/lotto/input/Purchase.java
@@ -1,5 +1,6 @@
 package lotto.input;
 
+import static lotto.constants.ErrorMessage.INPUT_MUST_BE_NUMBER;
 import static lotto.constants.ErrorMessage.PURCHASE_AMOUNT_UNIT_ERROR;
 
 public class Purchase {
@@ -11,9 +12,19 @@ public class Purchase {
         this.purchase = purchase;
     }
 
+    public int getPurchase() {
+        return purchase;
+    }
+
     private void isDivisibleByThousand(int purchase) {
         if (purchase % 1000 != 0) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_UNIT_ERROR.getMessage());
+        }
+    }
+
+    private void isPositiveAmount(int purchase) {
+        if (purchase <= 0) {
+            throw new IllegalArgumentException(INPUT_MUST_BE_NUMBER.getMessage());
         }
     }
 }
