8ad9ae005ed6db2f176dd753de2dd48f544abcf8
dnwldla <leeemjw@naver.com>
2024-10-31 00:50:42 +0900

    test: 각각의 로또가 몇등인지 확인하는 테스트 작성

---
+++ src/test/java/lotto/domain/PrizeMachineTest.java
@@ -0,0 +1,49 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrizeMachineTest {
+    PrizeMachine prizeMachine;
+    List<Lotto> lottos;
+    List<Integer> winningNumbers;
+    int bonusNumber;
+
+    @BeforeEach
+    void setUp(){
+        lottos=List.of(
+                new Lotto(List.of(1,2,3,10,11,12)),
+                new Lotto(List.of(4,5,6,7,8,13)),
+                new Lotto(List.of(4,5,6,7,8,12)),
+                new Lotto(List.of(4,5,6,7,8,9)),
+                new Lotto(List.of(4,5,6,15,16,17))
+        );
+
+        prizeMachine=new PrizeMachine(lottos);
+        winningNumbers=List.of(4,5,6,7,8,9);
+        bonusNumber=13;
+    }
+
+    @Test
+    void 등수를_잘_반환하는지_확인한다(){
+
+        Map<Rank, Integer> expected = new EnumMap<>(Rank.class);
+
+        expected.put(Rank.UNDER_TWO_HIT, 1);
+        expected.put(Rank.THREE_HIT, 1);
+        expected.put(Rank.FOUR_HIT, 0);
+        expected.put(Rank.FIVE_HIT_WITHOUT_BONUS, 1);
+        expected.put(Rank.FIVE_HIT_WITH_BONUS, 1);
+        expected.put(Rank.SIX_HIT, 1);
+
+        assertThat(prizeMachine.getAmountOfPrize(winningNumbers,bonusNumber))
+                .isEqualTo(expected);
+    }
+
+}
