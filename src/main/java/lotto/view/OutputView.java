033c0a0126df2d049a998b1d26368ecf8d31e7c9
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-30 19:57:24 +0900

    feat($OutputView): 구입 로또 내역 출력 함수 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,18 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        System.out.printf("\n%d개를 구매했습니다.%n", lottos.size());
+
+        lottos.forEach(this::printLotto);
+    }
+
+    private void printLotto(Lotto lotto) {
+        System.out.println(lotto.getNumbers());
+    }
 }
