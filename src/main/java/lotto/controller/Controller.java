ad1c4f35955e19831b5427946e22f11b53688274
joshinhyeok <shtree07@naver.com>
2024-10-31 05:43:19 +0900

    feat: Controller 클래스에 발행한 로또수량, 로또번호 출력 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,11 +1,18 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoMachine;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Controller {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         int lottoPurchaseAmount = inputView.readLottoPurchaseAmount();
 
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> Lottos = lottoMachine.buyLotto(lottoPurchaseAmount);
+        OutputView.printLottos(Lottos);
     }
 }
