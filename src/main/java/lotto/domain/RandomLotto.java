34b2a69023f967e7763f34db211978fad023117e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 13:02:05 +0900

    feat : 랜덤 로도 객체 작성

---
+++ src/main/java/lotto/domain/RandomLotto.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class RandomLotto {
+    private final List<Lotto> lottos;
+
+    public RandomLotto(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+
+}
