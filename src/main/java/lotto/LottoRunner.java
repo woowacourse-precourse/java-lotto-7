8b441fb3e58b53bf96427d87e50967dca7c924af
JinGoo <zzgg3040@naver.com>
2024-10-30 19:22:08 +0900

    feat: 로또 구입 금액 무한 예외 처리 (1000 단위 강제)

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -8,6 +8,17 @@ public class LottoRunner {
         while (money % 1000 != 0) {
             System.out.println("구입금액을 입력해 주세요.");
             money = Integer.parseInt(Console.readLine());
+            try {
+                validate(money);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void validate(int money) throws IllegalArgumentException {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000 단위의 숫자로 입력해주세요.");
         }
     }
 }
