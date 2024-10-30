d4c8e5dbaab56b9d3822f6bbc18e85bda9e24c3c
Hyso <hsk7953@gmail.com>
2024-10-30 20:44:45 +0900

    feat : 사용자 입력 안내 메시지 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+public class OutputView {
+
+    public void requestLotteryPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void requestLotteryWinningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void requestLotteryBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+}
