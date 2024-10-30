a0dfdbd6a5e5e31a9148b707a85d6d1504f51da0
junghun00 <ji8657447@gmail.com>
2024-10-30 23:43:50 +0900

    feat: 구매 한 로또 번호 출력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,8 @@ package lotto.view;
 import static lotto.utils.ErrorMessage.*;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
 
 public class InputView {
     private static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
@@ -20,6 +22,15 @@ public class InputView {
         } catch (NumberFormatException e) {
             errorPrint("inputPrice");
             return inputPrice();
+        }
+    }
+
+    public static void getLottoList(LottoMachine lottoMachine) {
+        int buy = lottoMachine.getLottoTicketSize();
+
+        System.out.println("\n" + buy + BUY_COUNT);
+        for (Lotto lotto : lottoMachine.getLottoTickets()) {
+            System.out.println(lotto.getNumbers());
         }
     }
 }
