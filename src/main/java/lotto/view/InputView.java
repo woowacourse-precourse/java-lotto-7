6aa0ee9889c5c959b99117180c8d0b65c9b4fe69
변우영 <likevangogh24@gmail.com>
2024-10-31 08:59:11 +0900

    feat(InputView): 당첨 번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,19 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import validation.InputValidator;
+import lotto.validation.InputValidator;
+import lotto.validation.LottoNumberValidator;
 
 public class InputView {
 
     public String inputPurchaseAmount() {
         String input = inputWithValidation();
+        return input;
+    }
+
+    public String inputWinningNumbers() {
+        String input = inputWithValidation();
+        LottoNumberValidator.validateDelimitedByComma(input);
         return input;
     }
 
