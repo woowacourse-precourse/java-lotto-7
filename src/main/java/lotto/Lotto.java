68f068cb71a951c96d0bfe13a6451d98392cd8fa
awaykim <wonj010101@gmail.com>
2024-10-30 15:20:38 +0900

    refac: validate 함수 개선

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
+/*
+- Lotto에 numbers 이외의 필드(인스턴스 변수)를 추가할 수 없다.
+- numbers의 접근 제어자인 private은 변경할 수 없다.
+- Lotto의 패키지를 변경할 수 있다.
+*/
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +18,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (new HashSet<>(numbers).size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        for (int number: numbers) {
+            if (number < 0 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다. ");
+            }
         }
     }
 
-    // TODO: 추가 기능 구현
 }
