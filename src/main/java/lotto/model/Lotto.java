efe8fa37d438dc337425db4d08f6bc57406aab53
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 00:02:37 +0900

    feat(Lotto): 로또 번호에 중복이 있다면 예외 발생

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,12 @@
 package lotto.model;
 
+import static lotto.ExceptionMessage.LOTTO_NUMBER_DUPLICATE_EXCEPTION;
 import static lotto.ExceptionMessage.LOTTO_NUMBER_LENGTH_EXCEPTION;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -19,11 +22,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateNumbersSize(numbers);
+        validateNoDuplicateNumbers(numbers);
     }
 
     private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(LOTTO_NUMBER_LENGTH_EXCEPTION.message());
+        }
+    }
+
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for(int number : numbers) {
+            if(!set.add(number)) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_DUPLICATE_EXCEPTION.message());
+            }
         }
     }
 
