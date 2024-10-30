36df772699e96495c2e9486c79b1b597f0be1769
mininiii <didals0521@gmail.com>
2024-10-31 02:03:26 +0900

    feat: 결과 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.Map;
+import lotto.domain.LottoRank;
+import lotto.model.Lotto;
+import lotto.util.PrizeFormatter;
+
+public class OutputView {
+    public void printPurchasedLotto(List<Lotto> purchasedLotto) {
+        System.out.println(purchasedLotto.size() + "개를 구매했습니다.");
+        purchasedLotto.forEach(lotto -> {
+            System.out.println(lotto.toString());
+        });
+    }
+
+    public void printStatistics(Map<LottoRank, Integer> statistics) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (LottoRank rank : LottoRank.values()) {
+            if (!rank.equals(LottoRank.NONE)) {
+                System.out.println(
+                        rank.getMatchingCount() + "개 일치 " + "(" + PrizeFormatter.formatPrize(rank.getPrize()) + "원)"
+                                + " - " + statistics.get(rank)
+                                + "개");
+            }
+        }
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        System.out.println("총 수익률은 " + String.format("%.1f", rateOfReturn) + "%입니다.");
+    }
+}
