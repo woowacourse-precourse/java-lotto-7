c3f719c6396cd2892c4b6d6995f7520346c787c4
AwesomeMinsik <beaut3579@gmail.com>
2024-10-30 14:36:28 +0900

    feat : 입력된 금액에 따라 구매할 로또 개수를 구하는 기능을 추가하였습니다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         int price = Integer.parseInt(Console.readLine());
+        int total = price / 1000;
     }
 }
