6e06437f18632bb9362e5d06594c2ab7967d0e62
CheChe903 <cheche903@naver.com>
2024-10-30 16:38:49 +0900

    feat(LottoNumber): Lotto의 Integer를 LottoNumber로 래핑 작업 진행
    Integer를 LottoNumber로 래핑합니다.
    이에 따라 로또 번호의 유효값 검증이 해당 객체로 책임이 넘어갑니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,21 @@
 package lotto;
 
 import java.util.List;
+import lotto.domain.LottoNumber;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(List<LottoNumber> numbers) {
+        validateSize(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<LottoNumber> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+
 }
--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,4 +1,17 @@
 package lotto.domain;
 
 public class LottoNumber {
+
+    private final Integer number;
+
+    public LottoNumber(Integer number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
