c67a4b95fdb462ea5efd85d473b817979697fbc2
wkqkel <wkqkel@naver.com>
2024-10-31 02:46:29 +0900

    refactor: Lotto Number 클래스 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,21 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final Set<Number> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> i_numbers) {
+        Set<Number> numbers = new HashSet<>(i_numbers.stream().map(Number::new).toList());
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(Set<Number> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
