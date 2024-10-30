91eb46fbc4f2e7876a96e6c0d377edfbd2bfe72d
LimJaeEun0930 <je9513666@gmail.com>
2024-10-30 23:39:46 +0900

    feat: Remove sellLotto() and Add buyLotto() method
    기존에 있던 sellLotto()를 지우고 buyLotto() 메서드를 추가함. 사유: sellLotto() 메서드는 시그니처를 보고 무엇을 하는것인지 판단이 어려웠음.
    List<Lotto> buyLotto(int money)는 더욱 직관적임

--- src/main/java/lotto/LottoSystem.java
+++ src/main/java/lotto/LottoSystem.java
@@ -1,25 +1,25 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
+import static lotto.StringPool.MONEY_NOT_DIVIDED_BY_1000;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoSystem {
-    private static final String MONEY_NOT_DIVIDED_BY_1000 = "[ERROR] 로또 구입 금액은 1,000원 단위로 입력 가능합니다.";
-
     private static LottoCustomer customer = new LottoCustomer();
+    private int printLottoCount;
 
-    public void sellLotto() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
+    public List<Lotto> buyLotto(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(MONEY_NOT_DIVIDED_BY_1000);
         }
+        printLottoCount = money / 1000;
         customer.addBetMoney(money);
-        for (int i = 0; i < money / 1000; ++i) {
-            Lotto luckyLotto = createAutoLotto();
-            customer.collectLotto(luckyLotto);
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < printLottoCount; ++i) {
+            lottos.add(createAutoLotto());
         }
+        return lottos;
     }
 
     private Lotto createAutoLotto() {
