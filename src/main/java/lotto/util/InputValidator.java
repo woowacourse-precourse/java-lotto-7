85cc7393c351dbe14f460bc99997657d547f23a8
mininiii <didals0521@gmail.com>
2024-10-31 02:56:03 +0900

    refactor: static으로 변환

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,7 +1,7 @@
 package lotto.util;
 
 public class InputValidator {
-    public void validateRange(int number) {
+    public static void validateRange(int number) {
         if (number > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호의 최대값은 45 이하여야 합니다.");
         }
@@ -10,13 +10,13 @@ public class InputValidator {
         }
     }
 
-    public void canDividedByThousand(long purchaseAmount) {
+    public static void canDividedByThousand(long purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 나누어 떨어져야 합니다.");
         }
     }
 
-    public void isNotNegativeNumber(long purchaseAmount) {
+    public static void isNotNegativeNumber(long purchaseAmount) {
         if (purchaseAmount < 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
         }
