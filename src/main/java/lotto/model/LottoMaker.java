01abb7b57afd66028e4aafbd1c5457866430fc00
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-30 19:42:59 +0900

    feat($LottoMaker): 로또 생성 함수 추가
    - 구입 금액을 받아 로또를 생성한다.
    - 구입 금액에 대한 검증을 수행한다:
      - 구입 금액이 1,000원 이상인지 검증한다.
      - 구입 금액이 1,000원 단위인지 검증한다.
    - 1 ~ 45 사이의 숫자 중 6 개의랜덤 번호를 생성해 로또를 생성한다.

--- src/main/java/lotto/model/LottoMaker.java
+++ src/main/java/lotto/model/LottoMaker.java
@@ -1,4 +1,42 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoMaker {
+
+    private static final int PRICE = 1000;
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_MIN = 1;
+    private static final int LOTTO_MAX = 45;
+
+    public List<Lotto> makeLottos(int money) {
+
+        validateMoney(money);
+        int lottoCount = money / PRICE;
+
+        return IntStream.range(0, lottoCount)
+                .mapToObj(count -> makeLotto())
+                .toList();
+    }
+
+    private Lotto makeLotto() {
+        return new Lotto(makeLottoNumbers());
+    }
+
+    private List<Integer> makeLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, LOTTO_SIZE).stream().sorted().toList();
+    }
+
+    private void validateMoney(int money) {
+        if (money < PRICE) {
+            throw new IllegalArgumentException("[ERROR] 로또 구매 금액은 1000원 이상이어야 합니다.");
+        }
+
+        if (money % PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구매 금액은 1000원 단위여야 합니다.");
+        }
+    }
 }
