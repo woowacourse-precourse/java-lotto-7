6f52d1306e26a055ce65e509c5e80478d1a219b5
Seonjeongg <sj030205@naver.com>
2024-10-30 11:23:21 +0900

    feat: 로또 구입 금액 입력 받기

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,4 +1,31 @@
 package lotto.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    private static final String ASK_PurchaseAmount="구입금액을 입력해 주세요.";
+    private static final String ERROR_PurchaseAmount="[ERROR] 구입 금액은 1000원 단위로 입력해주세요.";
+
+    public static int input_purchaseAmount(){
+        boolean validInput=false;
+        int parchase_amount = 0;
+        while(!validInput){
+            System.out.println(ASK_PurchaseAmount);
+            String input_purchase_amount= readLine();
+            if(check_invalidAmount(input_purchase_amount)){
+                parchase_amount=Integer.parseInt(input_purchase_amount);
+                validInput=true;
+            }else{
+                throw new IllegalArgumentException(ERROR_PurchaseAmount);
+            }
+        }
+        return parchase_amount;
+    }
+
+    private static boolean check_invalidAmount(String purchase_amount){
+        if(Integer.parseInt(purchase_amount)%1000==0) return true;
+        return false;
+    }
 }
