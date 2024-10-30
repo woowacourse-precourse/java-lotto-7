c1b867327f21ab86b40646d7a8ab9a92ef62a948
EHCTH <lees90896@gmail.com>
2024-10-30 18:26:33 +0900

    feat(WinningLottoHandler): 기능 구현 * feat: retrieveREceive(Function<String R>): R (InputHandelrService 구현) WinningLotto랑 Money는 여기에서 예외가 발생할 경우 재귀호출

--- src/main/java/lotto/view/input/hanlder/infrastructure/WinningLottoHandler.java
+++ src/main/java/lotto/view/input/hanlder/infrastructure/WinningLottoHandler.java
@@ -1,4 +1,25 @@
 package lotto.view.input.hanlder.infrastructure;
 
-public class WinningLottoHandler {
+import lotto.view.input.domain.InputService;
+import lotto.view.input.hanlder.domain.InputHandlerService;
+import lotto.view.output.infra.ErrorOutput;
+
+import java.util.function.Function;
+
+public class WinningLottoHandler implements InputHandlerService {
+    private final InputService inputService;
+    public WinningLottoHandler(InputService inputService) {
+        this.inputService = inputService;
+    }
+    @Override
+    public <R> R retrieveReceive(Function<String, R> function) {
+        try {
+            String input = inputService.input();
+            return function.apply(input);
+        }
+        catch (IllegalArgumentException illegalArgumentException) {
+            ErrorOutput.view(illegalArgumentException);
+            return retrieveReceive(function);
+        }
+    }
 }
