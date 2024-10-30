e17be4525910de837524325a80a6bfa3c3cd9ff1
taegyunB <taegyunb99@gmail.com>
2024-10-30 14:09:35 +0900

    feat(Controller): 사용자 구입금액 입력 및 구입금액 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static final int LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int purchaseAmount = readPurchaseAmount();
+
+
+    }
+
+    private static int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요");
+        int amount = Integer.parseInt(Console.readLine());
+        validateAmount(amount);
+        return amount;
+    }
+
+    private static void validateAmount(int amount) {
+        if (amount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+        }
     }
 }
+
+
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -48,4 +48,6 @@ public class Lotto {
         }
         return matchCount;
     }
+
+
 }
