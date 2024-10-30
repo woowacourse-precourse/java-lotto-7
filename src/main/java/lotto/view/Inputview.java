bfd09951b2e3cbb517e39c3d74257dbae509b17a
YunCheol1 <naro2727@naver.com>
2024-10-30 20:12:00 +0900

    feat(inputview):금액과 당첨번호, 보너스번호 입력받는 기능 추가 -금액입력과 유효성검사,당첨번호입력과 보너스번호 입력하는 기능 구현

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -1,4 +1,18 @@
 package lotto.view;
 
 public class Inputview {
+    public static int inputMoney(int money) {
+        if (money % 10 != 0) {
+            throw new IllegalArgumentException();
+        }
+        return money;
+    }
+
+    public static String inputWinningNumbers(String winningNumbers) {
+        return winningNumbers;
+    }
+
+    public static int inputBonusNumber(int BonusNumber) {
+        return BonusNumber;
+    }
 }
