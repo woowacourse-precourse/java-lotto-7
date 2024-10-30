0f41c0ce5589e67e0472680f80789c9665c3d9a5
Orion-Yoon <insertlock8@gmail.com>
2024-10-31 02:44:11 +0900

    feat : 수익률, 통계, 로또 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,18 +1,35 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
 public class OutputView {
-    public void printResult() {
+    public void printLottos(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.%n", lottos.size());
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+        }
+
+    public void printWinningStats(Map<LottoRank, Integer> winningStats) {
         System.out.println("당첨 통계");
         System.out.println("---");
+        winningStats.forEach((rank, count) -> {
+            if (rank != LottoRank.NONE) {
+                System.out.printf("%d개 일치 (%d원) - %d개%n",
+                        rank.getMatchCount(),
+                        rank.getPrizeMoney(),
+                        count);
+            }
+        });
+
     }
 
-    public void printLottos(List<Lotto> lottos)  {
-        System.out.println(lottos.size() + "개를 구매했습니다.");
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto.getNumbers());
-        }
+    public void printYield(double yield) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
+        String formattedYield = decimalFormat.format(yield);
+        System.out.printf("총 수익률은 %s%%입니다.%n", formattedYield);
     }
 }
+
