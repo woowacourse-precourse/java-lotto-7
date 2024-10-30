662b2ab386f2d1d558645734c6db35bc1fe576aa
Orion-Yoon <insertlock8@gmail.com>
2024-10-31 01:31:14 +0900

    test : 사용자에게 입력받은 로또번호 검증 단위 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import lotto.domain.Lotto;
 import lotto.utils.Validator;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ class LottoTest {
     public void setUp() {
         validator = new Validator();
     }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -52,11 +54,47 @@ class LottoTest {
     @Test
     void 금액이_1000_미만일_때_예외발생() {
         String input = "500";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->{
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             validator.validateInputPurchaseAmount(input);
         });
         assertEquals("1000 이상의 금액을 입력해야 합니다.", exception.getMessage());
     }
 
+    @Test
+    void 로또_번호가_6개가_아니면_예외발생() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+        assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.getMessage());
+        }
 
-}
+    @Test
+    void 중복이_있을_때_예외발생() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+        assertEquals("[ERROR] 로또 번호는 중복될 수 없습니다.", exception.getMessage());
+        }
+    @Test
+    void 번호가_범위를_벗어나면_예외빌생() {
+        List<Integer> numbers = Arrays.asList(0, 2, 5, 10, 50, 100);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+        assertEquals("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.", exception.getMessage());
+    }
+
+    @Test
+    void 유효한_로또번호일_경우_정상생성() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertDoesNotThrow(() -> new Lotto(numbers));
+    }
+    }
+
+
+
+
+
+
