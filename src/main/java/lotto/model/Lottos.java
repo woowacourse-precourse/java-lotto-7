3f03ff57a8542066482cf8cc1f16f796aaaed613
unh6unh6 <ruddbs3308@naver.com>
2024-10-30 19:36:52 +0900

    feat(Lottos): 구입금액이 1000원으로 나누어 떨어지지 않는 경우 예외 발생

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,11 +1,20 @@
 package lotto.model;
 
+import static lotto.ExceptionMessage.PURCHASE_AMOUNT_NOT_DIVISIBLE_BY_1000_EXCEPTION;
+
 public class Lottos {
     private long lottoPurchaseCount;
 
     public void calculateLottoPurchaseCount(String purchaseAmount) {
         long amount = Long.parseLong(purchaseAmount);
+        validatePurchaseAmountDivisibleBy1000(amount);
         lottoPurchaseCount = amount / 1000;
+    }
+
+    private void validatePurchaseAmountDivisibleBy1000(long amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_NOT_DIVISIBLE_BY_1000_EXCEPTION.message());
+        }
     }
 
     public long getLottoPurchaseCount() {
--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import static lotto.ExceptionMessage.PURCHASE_AMOUNT_NOT_DIVISIBLE_BY_1000_EXCEPTION;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,5 +22,15 @@ public class LottosTest {
 
         assertThat(lottos.getLottoPurchaseCount())
                 .isEqualTo(5);
+    }
+
+    @Test
+    void 구입금액이_1000원으로_나누어_떨어지지_않는_경우_예외가_발생한다() {
+        String purchaseAmount = "5050";
+
+        assertThatThrownBy(() ->
+                lottos.calculateLottoPurchaseCount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PURCHASE_AMOUNT_NOT_DIVISIBLE_BY_1000_EXCEPTION.message());
     }
 }
