cfb8e19f8580a81bdff6ffa77d4fa55690b21d41
binn <anyubin0724@naver.com>
2024-10-30 16:39:40 +0900

    feat: 예외처리 클래스 추가
    - 구입금액, 보너스 번호, 당첨 번호에 대한 각 예외처리 메서드 구현

---
+++ src/main/java/lotto/exceptions/ValidateError.java
@@ -0,0 +1,77 @@
+package lotto.exceptions;
+
+public class ValidateError {
+    private static final String ERROR_HEAD = "[ERROR] ";
+    private static final String NO_EXIST_PURCHASE_PRICE = "구입금액을 입력하지 않았습니다.";
+    private static final String NO_POSITIVE_PURCHASE_PRICE = "구입금액은 양수여야 합니다.";
+    private static final String NO_EXIST_BONUS_NUMBER = "보너스 번호를 입력하지 않았습니다.";
+    private static final String NO_EXIST_WINNING_NUMBER = "당첨 번호를 입력하지 않았습니다.";
+    private static final String NO_NUMBER_PURCHASE_PRICE = "구입금액은 숫자로 입력하여야 합니다.";
+    private static final String NO_NUMBER_BONUS_NUMBER = "보너스 번호는 숫자로 입력하여야 합니다.";
+    private static final String NO_NUMBER_WINNING_NUMBER = "당첨 번호는 숫자로 입력하여야 합니다.";
+    private static final String INVALID_PURCHASE_PRICE = "구입금액은 1,000원으로 나누어 떨어져야 합니다.";
+    private static final String OUT_OF_RANGE_BONUS_NUMBER = "보너스 번호는 1~45 사이의 값이어야 합니다.";
+    private static final String OUT_OF_RANGE_WINNING_NUMBER = "당첨 번호는 1~45 사이의 값이어야 합니다.";
+
+    public static int validatePurchasePrice(String purchasePriceStr) {
+        if (isBlank(purchasePriceStr)) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_EXIST_PURCHASE_PRICE);
+        }
+        int purchasePrice;
+        try {
+            purchasePrice = convertStringToInt(purchasePriceStr);
+            if (purchasePrice <= 0) {
+                throw new IllegalArgumentException(ERROR_HEAD + NO_POSITIVE_PURCHASE_PRICE);
+            }
+            if (purchasePrice % 1000 != 0) {
+                throw new IllegalArgumentException(ERROR_HEAD + INVALID_PURCHASE_PRICE);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_NUMBER_PURCHASE_PRICE);
+        }
+        return purchasePrice / 1000;
+    }
+
+    public static int validateWinningNumber(String winningNumberStr) {
+        if (isBlank(winningNumberStr)) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_EXIST_WINNING_NUMBER);
+        }
+        int winningNumber;
+        try {
+            winningNumber = convertStringToInt(winningNumberStr);
+            if (!isInRange(winningNumber)) {
+                throw new IllegalArgumentException(ERROR_HEAD + OUT_OF_RANGE_WINNING_NUMBER);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_NUMBER_WINNING_NUMBER);
+        }
+        return winningNumber;
+    }
+
+    public static void validateBonusNumber(String bonusNumberStr) {
+        if (isBlank(bonusNumberStr)) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_EXIST_BONUS_NUMBER);
+        }
+        int bonusNumber;
+        try {
+            bonusNumber = convertStringToInt(bonusNumberStr);
+            if (!isInRange(bonusNumber)) {
+                throw new IllegalArgumentException(ERROR_HEAD + OUT_OF_RANGE_BONUS_NUMBER);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_HEAD + NO_NUMBER_BONUS_NUMBER);
+        }
+    }
+
+    private static boolean isBlank(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static boolean isInRange(int input) {
+        return input <= 45 && input >= 1;
+    }
+
+    private static int convertStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
