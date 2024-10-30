001092be1baa52da415057d0a1dcd4afcccbf1df
seahwanKim <kimkimseasea@gmail.com>
2024-10-30 19:06:05 +0900

    feat: UserLottoNumber 클래스에 생성자와 getter 를 추가한다

--- src/main/java/lotto/domain/UserLottoNumber.java
+++ src/main/java/lotto/domain/UserLottoNumber.java
@@ -1,5 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class UserLottoNumber {
 
+    private final List<Integer> lottoNumber;
+
+    public UserLottoNumber(List<Integer> lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+    public List<Integer> getLottoNumber() {
+        return lottoNumber;
+    }
 }
