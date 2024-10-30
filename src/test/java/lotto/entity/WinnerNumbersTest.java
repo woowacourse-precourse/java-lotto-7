80890e898192ed9c24e8ea8937b3ef173c1410ac
kungbi <kipper12335@gmail.com>
2024-10-30 16:43:29 +0900

    test(model): WinnerNumbers 테스트 코드 추가
    보너스 번호가 아닌 메인 번호에 대한 테스트도 추가했습니다.

--- src/test/java/lotto/entity/WinnerNumbersTest.java
+++ src/test/java/lotto/entity/WinnerNumbersTest.java
@@ -8,6 +8,62 @@ import org.junit.jupiter.api.Test;
 
 public class WinnerNumbersTest {
     @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7);
+        int bonusNumber = 40;
+
+        // when
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> new WinnerNumbers(integers, bonusNumber));
+
+        // then
+        assertEquals("로또 번호는 6개여야 합니다.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 5);
+        int bonusNumber = 40;
+
+        // when
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> new WinnerNumbers(integers, bonusNumber));
+
+        // then
+        assertEquals("로또 번호는 중복될 수 없습니다.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void 로또_번호에_정해진_범위를_벗어나는_숫자가_있으면_예외가_발생한다_위로() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 46);
+        int bonusNumber = 40;
+
+        // when
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> new WinnerNumbers(integers, bonusNumber));
+
+        // then
+        assertEquals("로또 번호는 1부터 45까지의 숫자여야 합니다.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void 로또_번호에_정해진_범위를_벗어나는_숫자가_있으면_예외가_발생한다_아래로() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 0);
+        int bonusNumber = 40;
+
+        // when
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> new WinnerNumbers(integers, bonusNumber));
+
+        // then
+        assertEquals("로또 번호는 1부터 45까지의 숫자여야 합니다.", illegalArgumentException.getMessage());
+    }
+
+    @Test
     void 보너스_번호에_정해진_범위를_벗어나는_숫자가_있으면_예외가_발생한다_위로() {
         // given
         List<Integer> integers = List.of(1, 2, 3, 4, 5, 40);
@@ -46,6 +102,6 @@ public class WinnerNumbersTest {
                 () -> new WinnerNumbers(integers, bonusNumber));
 
         // then
-        assertEquals("로또 번호는 중복될 수 없습니다.", illegalArgumentException.getMessage());
+        assertEquals("보너스 번호는 당첨 번호와 중복될 수 없습니다.", illegalArgumentException.getMessage());
     }
 }
