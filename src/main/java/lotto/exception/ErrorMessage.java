ded6c252b8252326199e94a1465c371d545c14f4
DongYeop <s01054830455@gmail.com>
2024-10-31 01:14:18 +0900

    feat: 로또 번호에 대한 유효성 검사 추가
    - 중복된 숫자라면 예외가 발생 한다.
    - 범위를 초과하면 예외가 발생 한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -6,7 +6,10 @@ public enum ErrorMessage {
     INVALID_INPUT("빈 값을 입력할 수 없습니다. 다시 입력해 주세요."),
     ONLY_NUMERIC("정수만 입력할 수 있습니다."),
     NOT_CONTAIN_BLANK("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요."),
-    NOT_DIVIDE_PURCHASE_AMOUNT("구입 금액은 1000원 단위로 입력해야 합니다.");
+    NOT_DIVIDE_PURCHASE_AMOUNT("구입 금액은 1000원 단위로 입력해야 합니다."),
+    LOTTO_NUMBERS_SIZE("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBERS("로또 번호는 중복될 수 없습니다."),
+    LOTTO_NUMBER_OUT_OF_RANGE("당첨 번호는 1 부터 45 까지의 정수만 입력할 수 있습니다.");
 
     private final String message;
 
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,19 +1,47 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import lotto.exception.ErrorMessage;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER_RANGE = 1;
+    private static final int MAX_NUMBER_RANGE = 45;
+
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    protected Lotto(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateDuplicateNumbers(numbers);
+        validateOutOfRangeNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validateNumbersSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_SIZE.getErrorMessage());
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        HashSet<Integer> nonDuplicateNumbersSet = new HashSet<>(numbers);
+        if (nonDuplicateNumbersSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBERS.getErrorMessage());
+        }
+    }
+
+    private void validateOutOfRangeNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_NUMBER_RANGE || number > MAX_NUMBER_RANGE) {
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE.getErrorMessage());
+            }
         }
     }
 
