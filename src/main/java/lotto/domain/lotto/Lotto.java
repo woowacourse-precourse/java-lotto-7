ebdb9bc1f46ef314e2f99017550d1b96a75a12a1
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-31 01:00:39 +0900

    style: 코딩 컨밴션에 따라 정리

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -6,13 +6,15 @@ import lotto.domain.errors.RangeError;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Lotto extends NumberCombination{
+public class Lotto extends NumberCombination {
 
     private final List<Number> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateLength(LottoRule.COMBINATION_LENGTH.getValue(),numbers);
+
+        validateLength(LottoRule.COMBINATION_LENGTH.getValue(), numbers);
         validateDuplicate(numbers);
+
         this.numbers = numbers.stream()
                 .sorted()
                 .map(Number::new)
