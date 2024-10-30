041fe3db705aecb69d62fba4df3080500f6bde4d
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 23:18:44 +0900

    chore: 디렉토리 구조 정리

--- src/main/java/lotto/domain/errors/RangeError.java
+++ src/main/java/lotto/domain/errors/RangeError.java
@@ -1,8 +1,8 @@
 package lotto.domain.errors;
 
-import static lotto.domain.Lotto.LOTTO_COMBINATION_LENGTH;
-import static lotto.domain.LottoNumber.MAXIMUM_LOTTO_NUMBER;
-import static lotto.domain.LottoNumber.MINIMUM_LOTTO_NUMBER;
+import static lotto.domain.lotto.Lotto.LOTTO_COMBINATION_LENGTH;
+import static lotto.domain.lotto.LottoNumber.MAXIMUM_LOTTO_NUMBER;
+import static lotto.domain.lotto.LottoNumber.MINIMUM_LOTTO_NUMBER;
 
 public enum RangeError implements Errors {
     LOTTO_NUMBER_ERROR("[ERROR] 로또 번호는 "+MINIMUM_LOTTO_NUMBER+"부터 "+MAXIMUM_LOTTO_NUMBER+"사이여야 합니다."),
--- src/main/java/lotto/domain/lotto/LottoFactory.java
+++ src/main/java/lotto/domain/lotto/LottoFactory.java
@@ -1,6 +1,4 @@
-package lotto.domain;
-
-import java.util.List;
+package lotto.domain.lotto;
 
 public interface LottoFactory {
 
