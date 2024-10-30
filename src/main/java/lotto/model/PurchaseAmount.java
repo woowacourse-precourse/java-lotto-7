934bf34c61cece6210235143bf7b00b4a3c199c7
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-30 21:07:54 +0900

    feat: add validation of purchase amount
    - validate unit of amount
    - validate range of amount

---
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -0,0 +1,39 @@
+package lotto.model;
+
+import static lotto.model.LottoConstants.LOTTO_PRICE;
+import static lotto.model.LottoConstants.MAX_LOTTO_PURCHASE_AMOUNT;
+import static lotto.model.LottoConstants.MIN_LOTTO_PURCHASE_AMOUNT;
+
+public class PurchaseAmount {
+
+    private final int amount;
+
+    public PurchaseAmount(final int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(final int amount) {
+        checkDivisibilityByUnit(amount);
+        checkMinimumOfAmount(amount);
+        checkMaximumOfAmount(amount);
+    }
+
+    private void checkDivisibilityByUnit(final int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    private void checkMinimumOfAmount(final int amount) {
+        if (amount < MIN_LOTTO_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 최소 1,000원 이상이여야 합니다.");
+        }
+    }
+
+    private void checkMaximumOfAmount(final int amount) {
+        if (amount > MAX_LOTTO_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 최대 100,000원 이하여야 합니다.");
+        }
+    }
+}
