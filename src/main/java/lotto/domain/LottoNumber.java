1b7b32ba71491d8986b4674d3b0f1324a8e81e05
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 21:04:26 +0900

    refactor: 펙토리 매서드 제거
    조합을 뽑아주는 pickUniqueNumbersInRange를 사용하기 위해 랜덤 생성 매서드 삭제

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,4 +1,4 @@
-package lotto.domain.number;
+package lotto.domain;
 
 public class LottoNumber {
 
@@ -8,14 +8,9 @@ public class LottoNumber {
     private final int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
-        if(lottoNumber<MINIMUM_LOTTO_NUMBER || lottoNumber >MAXIMUM_LOTTO_NUMBER){
+        if (lottoNumber < MINIMUM_LOTTO_NUMBER || lottoNumber > MAXIMUM_LOTTO_NUMBER) {
             throw new IllegalArgumentException("//여기 enum 에러값");
         }
         this.lottoNumber = lottoNumber;
     }
-
-    public LottoNumber(NumberGenerator numberGenerator) {
-        this.lottoNumber= numberGenerator.create(MINIMUM_LOTTO_NUMBER,MAXIMUM_LOTTO_NUMBER);
-    }
-
 }
