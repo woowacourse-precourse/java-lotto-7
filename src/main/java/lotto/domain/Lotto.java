18252ba7da0aec82cb29fa22db807e10d5d1a3a0
himinqq <alsrud0966@naver.com>
2024-10-30 19:36:20 +0900

    feat(Lotto): 로또 번호가 중복된 경우 예외 발생시키는 기능 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLength(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers){
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+    private void validateDuplicate(List<Integer> numbers){
+        long nonDuplicateNumberCount = numbers.stream().distinct().count();
+        if(nonDuplicateNumberCount != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다. ");
+        }
+    }
+}
