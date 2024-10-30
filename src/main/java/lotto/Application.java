60f1a6900d2174fdbb55b0bbba2b759e84816aff
hongkikii <hyangmeeh@icloud.com>
2024-10-30 16:39:03 +0900

    feat(Price): 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Price price = null;
+        while (price == null) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String input = Console.readLine();
+            try {
+                price = Price.from(input);
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + "문자가 입력됐거나, 숫자 범위를 초과하였습니다.");
+            }
+        }
 
-        System.out.println("구입금액을 입력해 주세요.");
     }
 }
--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -1,2 +1,22 @@
-package lotto;public class Price {
+package lotto;
+
+public class Price {
+    private final int value;
+
+    public Price(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public static Price from(String input) {
+        try {
+            return new Price(Integer.parseInt(input));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validate(int value) {
+
+    }
 }
