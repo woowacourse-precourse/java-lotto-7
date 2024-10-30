3057d1f308a7cae192e58d67fc061b8a832f2198
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 13:12:10 +0900

    test : 메인 6자리 로또-보너스 중복시 예외처리 테스트

---
+++ src/test/java/lotto/domain/user/UserLottoTest.java
@@ -0,0 +1,25 @@
+package lotto.domain.user;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.Test;
+
+class UserLottoTest {
+
+    @Test
+    void 보너스_메인_로또_번호_중복_예외_처리(){
+        Lotto mainLotto = new Lotto(new ArrayList<>(List.of(
+                1,2,3,4,5,6
+        )));
+
+        BonusLotto bonusLotto = new BonusLotto(1);
+
+        assertThatThrownBy(() -> new UserLotto(mainLotto, bonusLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+}
