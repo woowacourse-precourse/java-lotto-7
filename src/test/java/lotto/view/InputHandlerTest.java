3fc716f16e9be7095df63e61fd4f6619fa8a87f5
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 00:40:48 +0900

    test(validateWinningNumbers): 당첨 번호 범위 테스트 작성

--- src/test/java/lotto/view/InputHandlerTest.java
+++ src/test/java/lotto/view/InputHandlerTest.java
@@ -1,11 +1,15 @@
 package lotto.view;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static lotto.view.InputHandler.validatePurchaseAmount;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class InputHandlerTest {
@@ -24,5 +28,20 @@ public class InputHandlerTest {
         assertDoesNotThrow(() -> {
             InputHandler.validatePurchaseAmount(purchaseAmount);
         });
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIntegerLists")
+    void 당첨_번호가_1에서_45_사이의_값이_아니면_예외_발생(List<Integer> winningNumbers) {
+        assertThatThrownBy(() -> validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당청 번호는 1에서 45 사이의 값이어야 합니다.");
+    }
+
+    static Stream<List<Integer>> provideIntegerLists() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 46),
+                List.of(0, 1, 2, 3, 4, 5)
+        );
     }
 }
