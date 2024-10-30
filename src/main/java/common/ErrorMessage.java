6b324565bdfb0f3b817d1998f2fb3c55cf41c331
githublees <leeho_@naver.com>
2024-10-30 19:36:00 +0900

    feat(error): 에러 메시지 enum 클래스 상수 처리

---
+++ src/main/java/common/ErrorMessage.java
@@ -0,0 +1,19 @@
+package common;
+
+public enum ErrorMessage {
+    PURCHASE_AMOUNT_ERROR_MESSAGE("[ERROR] 구입 금액은 %d원 단위여야 합니다."),
+    LOTTO_NUMBERS_SIZE_ERROR_MESSAGE("[ERROR] 로또 번호는 6개여야 합니다."),
+    BONUS_DUPLICATE_ERROR_MESSAGE("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다."),
+    NUMBER_RANGE_ERROR_MESSAGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
