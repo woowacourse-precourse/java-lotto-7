75ef61def7a800d2b01a22013f02653ea12d08cb
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-30 19:44:10 +0900

    feat($LottoController): 로또 구입 기능 추가
    - 입력받는 구입 금액을 통해 로또들을 발행받는다.
    - 만약, 예외가 발생한다면 다시 입력받는다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,27 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.LottoMaker;
 import lotto.view.InputView;
+
+import java.util.List;
 
 public class LottoController {
 
     private final InputView inputView = new InputView();
+    private final LottoMaker lottoMaker = new LottoMaker();
 
     public void run() {
-        int purchaseMoney = inputView.inputPurchaseMoney();
+        List<Lotto> lottos = purchaseLotto();
+    }
+
+    public List<Lotto> purchaseLotto() {
+        try {
+            int purchaseMoney = inputView.inputPurchaseMoney();
+            return lottoMaker.makeLottos(purchaseMoney);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return purchaseLotto();
+        }
     }
 }
