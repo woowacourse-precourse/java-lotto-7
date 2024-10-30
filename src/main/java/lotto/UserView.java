840d2c34bfd69e8122d084664f308a85bb6d00e6
Etwashoeren <sungmin0609@nate.com>
2024-10-30 16:38:12 +0900

    feat. 로또 구입 금액 입력에 대한 예외처리 추가

--- src/main/java/lotto/UserView.java
+++ src/main/java/lotto/UserView.java
@@ -2,25 +2,32 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserView {
 
     public int inputLottoAmount() {
-        System.out.println("구입금액을 입력해주세요.");
-        return Integer.parseInt(Console.readLine());
+        System.out.println("구입금액을 입력해 주세요.");
+
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 숫자이어야 합니다.");
+        }
     }
 
     public List<Integer> inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         String winningNumbersInput = Console.readLine();
         List<Integer> winningNumbersOutput = convertInputWinningNumbersToOutput(winningNumbersInput);
         return winningNumbersOutput;
     }
 
     public int inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 
@@ -35,14 +42,13 @@ public class UserView {
     }
 
     private List<Integer> convertInputWinningNumbersToOutput(String winningNumbers) {
-        String [] inputWinningNumbers = winningNumbers.split(",");
+        String[] inputWinningNumbers = winningNumbers.split(",");
         List<Integer> winningNumbersList = new ArrayList<>();
 
-        for (String stringWinningNumber : inputWinningNumbers) {
-            winningNumbersList.add(Integer.parseInt(stringWinningNumber));
+        for (String inputWinningNumber : inputWinningNumbers) {
+            winningNumbersList.add(Integer.parseInt(inputWinningNumber));
         }
 
         return winningNumbersList;
     }
-
 }
