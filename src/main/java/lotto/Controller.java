358d5881a783fba1a7b758d1710efe5388ac5f82
joshinhyeok <shtree07@naver.com>
2024-10-31 03:01:12 +0900

    feat: 중앙 관리용 Controller 클래스 추가

---
+++ src/main/java/lotto/Controller.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class Controller {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int lottoPurchaseAmount = inputView.readLottoPurchaseAmount();
+
+    }
+}
