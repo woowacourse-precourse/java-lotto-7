29b70155f674bd6e036ed547d47bee0360e1cb28
EHCTH <lees90896@gmail.com>
2024-10-30 17:47:54 +0900

    feat(Lotto): 기능 추가 및 변경 * feat: 기존 validate -> validate 패키지로 변경 * feat: info(): void * feat: isConatins(int) : boolean * feat: getMatchWinningCount(WinningLotto) : int * feat: getMatchWinningCOuntAndBonusNumber(WinningLotto, BonusNumber): int

--- src/main/java/lotto/lotto/domain/Lotto.java
+++ src/main/java/lotto/lotto/domain/Lotto.java
@@ -1,28 +1,41 @@
 package lotto.lotto.domain;
 
-import java.util.Collections;
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.winning.domain.WinningLotto;
+import lotto.lotto.validator.LottoValidator;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        LottoValidator.lottoValidate(numbers);
+        this.numbers = sort(numbers);
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
     // TODO: 추가 기능 구현
-    public void sort() {
-        Collections.sort(numbers);
+    public List<Integer> sort(List<Integer> numbers) {
+        return numbers.stream().sorted().toList();
+    }
+    public List<Integer> info() {
+        return numbers;
+    }
+    public boolean isContains(int number) {
+        return numbers.contains(number);
+    }
+    public int getMatchWinningCount(WinningLotto winningLotto) {
+        return (int) numbers.stream().filter(winningLotto::isContain).count();
+    }
+    public int getMatchWinningCountAndBonusNumber(WinningLotto winningLotto, BonusNumber bonusNumber) {
+        int matchCount = getMatchWinningCount(winningLotto);
+        if (isContains(bonusNumber.getNumber())) matchCount++;
+        return matchCount;
+
+
     }
     @Override
     public String toString() {
         return numbers.toString();
     }
+
 }
