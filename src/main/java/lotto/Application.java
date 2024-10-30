303882b70dc93ae030618e3086888eb98704c4b8
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-30 21:46:31 +0900

    feat: 발행한 로또 번호를 출력하는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,9 @@ public class Application {
         // 발행한 로또 수량을 출력
         int numberOfLotto = inputMoney / 1000;
         System.out.println("\n" + numberOfLotto + "개를 구매했습니다.");
+        // 로또 발행 기능
+        BuyLotto lottoList = new BuyLotto(numberOfLotto);
+        lottoList.printLottoList();
 
     }
 }
--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -19,4 +19,9 @@ public class BuyLotto {
         }
     }
 
+    public void printLottoList() {
+        for (List<Integer> integers : lottoList) {
+            System.out.println(integers);
+        }
+    }
 }
