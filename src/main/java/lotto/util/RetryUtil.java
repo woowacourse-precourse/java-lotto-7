0d4d956ff4b70f8df52dbae8b1631a2b4ba992c4
mohyerolo <garam2101@naver.com>
2024-10-30 15:21:42 +0900

    fix: Error 문구 수정

--- src/main/java/lotto/util/RetryUtil.java
+++ src/main/java/lotto/util/RetryUtil.java
@@ -3,7 +3,7 @@ package lotto.util;
 import java.util.function.Supplier;
 
 public class RetryUtil {
-    private static final String ERROR = "[Error] ";
+    private static final String ERROR = "[ERROR] ";
     private static final String RETRY = " 다시 입력해주세요.\n";
 
     public static <T> T executeWithRetry(Supplier<T> action) {
