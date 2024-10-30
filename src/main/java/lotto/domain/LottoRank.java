b9a6cc548cf22fcda9c809a4bf5822a560f90cef
mininiii <didals0521@gmail.com>
2024-10-31 01:57:46 +0900

    fix: LottoRank enum 순서 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
     FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000),
     NONE(0, 0);
 
     private final int matchingCount;
