371526b9ac3ed413ba4ebf58975f830e7e8eae07
Firedrago95 <yonghwa95@naver.com>
2024-10-30 13:47:33 +0900

    Test(WinningNumbersTest): 당첨번호, 보너스 번호 검증 테스트 작성

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -1,21 +1,37 @@
 package lotto.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningNumbersTest {
 
+    @ParameterizedTest
+    @MethodSource("provideWinningNumbers")
+    void 당첨_번호_생성_테스트(List<Integer> winningNumber) {
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+            () -> new WinningNumbers(winningNumber));
+    }
+
+    static List<List<Integer>> provideWinningNumbers() {
+        return List.of(
+            List.of(1,2,3,4,5),
+            List.of(1,2,3,4,5,5)
+        );
+    }
+
     @Test
-    void 보너스_번호_당첨_번호_중복_확인_테스트() {
+    void 보너스_번호_당첨번호_중복_테스트() {
         //given
-        List<Integer> winningNumber = List.of(1,2,3,4,5,6);
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 1;
 
-        //when & then
-        Assertions.assertThrows(IllegalArgumentException.class,
-            () -> new WinningNumbers(winningNumber, bonusNumber));
+        //then
+        assertThrows(IllegalArgumentException.class,
+            () -> winningNumbers.setBonusNumber(bonusNumber));
     }
 }
