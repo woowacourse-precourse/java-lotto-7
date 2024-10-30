ae8645c2f41fc6aa2c5da924d584e557f57fb66c
joshinhyeok <shtree07@naver.com>
2024-10-31 05:44:41 +0900

    feat: OutputView 클래스에 발행한 로또수량, 로또번호를 출력하는 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class OutputView {
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+}
