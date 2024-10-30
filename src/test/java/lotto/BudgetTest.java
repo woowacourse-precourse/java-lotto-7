bd0f581eb3d07dc9571c82a5e27525b7f05351b4
eb9862 <luclead622@gmail.com>
2024-10-30 20:08:16 +0900

    test: BudgetTest 클래스에 기능 테스트 추가

--- src/test/java/lotto/BudgetTest.java
+++ src/test/java/lotto/BudgetTest.java
@@ -1,11 +1,21 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BudgetTest {
+
+    @Test
+    @DisplayName("1000원 단위의 숫자를 정확하게 받는지 기능 테스트")
+    void BudgetUnitOfOneThousand() {
+
+        Budget testBudget = new Budget("2000");
+
+        assertThat(testBudget.getAccount()).isEqualTo(2000);
+    }
 
     @Test
     @DisplayName("구입 금액에 숫자 이외의 값이 들어오면 예외 처리")
