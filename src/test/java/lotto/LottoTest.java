d8dea530aac7173438135252d4de790ac62ed975
OhYeHyun <ohyhohyl@naver.com>
2024-10-30 21:17:22 +0900

    test(LottoTest): 보너스 번호가 일치하는지 확인하는 기능 검증 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -29,7 +29,7 @@ class LottoTest {
     @Test
     @DisplayName("로또 번호가 잘 생성되는지 확인")
     void 로또_번호_생성_테스트() {
-        lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(lotto.getNumbers()).contains(1, 2, 3, 4, 5, 6);
     }
 
@@ -37,9 +37,19 @@ class LottoTest {
     @DisplayName("로또 번호와 당첨 번호를 비교하여 당첨 개수 올바르게 반환하는지 확인")
     void 당첨_개수_확인_테스트() {
         lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int result = lotto.getMatchingCount(winningNumbers);
 
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 일치하는지 확인")
+    void 보너스_번호_일치_테스트() {
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto bonusNumber = new Lotto(List.of(6));
+        boolean result = lotto.isBonusNumberMatched(bonusNumber);
+
+        assertThat(result).isTrue();
     }
 }
