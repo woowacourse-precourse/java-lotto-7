29a046a717a6249d32be8567c592e320abeae093
bowook <mbosx316@naver.com>
2024-10-30 23:02:31 +0900

    feat(OutputView) : OutputView 기능 추가
    - OutputView에서 출력 역할 담당

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -1,4 +1,21 @@
 package lotto.view.output;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.validator.exception.LottoException;
+
 public class OutputView {
+
+    public void displayLottos(Lottos lottos) {
+        int lottosCount = lottos.getLottoCount();
+        System.out.printf(OutputMessage.PURCHASED_COUNT_MESSAGE.getOutputMessage(), lottosCount);
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+
+    public void displayErrorMessage(LottoException lottoException) {
+        System.out.println(lottoException.getMessage());
+    }
 }
