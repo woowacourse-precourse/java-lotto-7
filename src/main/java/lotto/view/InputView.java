c91cff91c7cd442750520d59d89a7ccc1e875861
Firedrago95 <yonghwa95@naver.com>
2024-10-30 14:43:26 +0900

    Refactor(InputView): 출력메시지 상수화, 메서드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,18 +9,29 @@ import lotto.util.validator.InputValidator;
 
 public class InputView {
 
+    private class InputMessage {
+
+        public static String READ_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+        public static String READ_WIN_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+        public static String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    }
+
     public static int readMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(InputMessage.READ_MONEY_MESSAGE);
         String money = Console.readLine();
         InputValidator.validateMoney(money);
         return Integer.parseInt(money);
     }
 
     public static List<Integer> readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(InputMessage.READ_WIN_NUMBER_MESSAGE);
         String winningNumbers = Console.readLine();
         InputValidator.validateWinningNumbers(winningNumbers);
         String[] nums = winningNumbers.split(LottoConst.COMMA);
+        return convertToList(nums);
+    }
+
+    private static List<Integer> convertToList(String[] nums) {
         return Arrays.stream(nums)
             .mapToInt(Integer::parseInt)
             .boxed()
@@ -28,7 +39,7 @@ public class InputView {
     }
 
     public static int readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(InputMessage.READ_BONUS_NUMBER_MESSAGE);
         String bonusNumber = Console.readLine();
         InputValidator.validateBonusNumber(bonusNumber);
         return Integer.parseInt(bonusNumber);
