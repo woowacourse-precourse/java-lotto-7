4c5a62853768a522035d2c9ac9e363734177cfb5
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 12:51:28 +0900

    test : Wallet 테스트 코드 에러 메시지 확인 임시 삭제

--- src/test/java/lotto/domain/WalletTest.java
+++ src/test/java/lotto/domain/WalletTest.java
@@ -10,8 +10,7 @@ class WalletTest {
     @Test
     void 최소_구입_금액_미달시_예외_처리(){
         assertThatThrownBy(() -> new Wallet(100))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("로또 구입 금액은 최소 1,000원 입니다.");
+                .isInstanceOf(IllegalArgumentException.class);
 
     }
 
@@ -19,8 +18,7 @@ class WalletTest {
     @Test
     void 금액_단위가_맞지_않을시_예외_처리(){
         assertThatThrownBy(() -> new Wallet(1001))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("로또 구입 금액은 1,000원 단위로 입력해 주세요.");
+                .isInstanceOf(IllegalArgumentException.class);
 
     }
 
