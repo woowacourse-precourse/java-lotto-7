26b107ea4fc033eb3292aa66f89ff98be8e25be0
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 12:58:00 +0900

    test : Lotto 예외처리 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    void 로또_번호_개수_미달_예외_처리(){
+        List<Integer> result = new ArrayList<>(List.of(
+                1,2,3,4,5
+        ));
+        assertThatThrownBy(() -> new Lotto(result))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_개수_초과_예외_처리(){
+        List<Integer> result = new ArrayList<>(List.of(
+                1,2,3,4,5,6,7,8
+        ));
+        assertThatThrownBy(() -> new Lotto(result))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_범위_예외_처리(){
+        List<Integer> result = new ArrayList<>(List.of(
+                48,1,2,3,4,5
+        ));
+        assertThatThrownBy(() -> new Lotto(result))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 중복된_로또_번호_예외_처리(){
+        List<Integer> result = new ArrayList<>(List.of(
+                1,1,2,3,4,5
+        ));
+        assertThatThrownBy(() -> new Lotto(result))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
