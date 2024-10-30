62fc18b6b5ba9cef237b3886a77384fa82aefbf0
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 04:07:14 +0900

    refactor(LottoRank) : 메서드 이름 수정 및 메서드 추가 - find 메서드의 이름을 isMatched로 변경 - 당첨인 등수에 해당하는 enum만 리스트로 저장하는 getLottoRanks 메서드 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum LottoRank {
 
@@ -33,15 +34,21 @@ public enum LottoRank {
         return isSecondRank;
     }
 
-    public static LottoRank checkRank(int sameNumberCount, boolean isMatchedToSecond) {
+    public static LottoRank findRank(int sameNumberCount, boolean isSecondRank) {
         return Arrays.stream(LottoRank.values())
-                .filter(lottoRank -> lottoRank.find(sameNumberCount, isMatchedToSecond))
+                .filter(lottoRank -> lottoRank.isMatched(sameNumberCount, isSecondRank))
                 .findFirst()
                 .orElse(NONE);
     }
 
-    private boolean find(int sameNumberCount, boolean isMatchedToSecond) {
+    private boolean isMatched(int sameNumberCount, boolean isMatchedToSecond) {
         return this.sameNumberCount == sameNumberCount
                 && this.isSecondRank == isMatchedToSecond;
+    }
+
+    public static List<LottoRank> getWinningRanks() {
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank != NONE)
+                .toList();
     }
 }
