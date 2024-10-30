a4e57c238169801cfda3145a1442d669b1cabfc1
dnwldla <leeemjw@naver.com>
2024-10-30 11:13:11 +0900

    feat: 보너스 번호가 당첨번호와 같은 경우 예외를 발생시키는 함수 추가

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -1,16 +1,36 @@
 package lotto.validator;
 
 import lotto.common.CommonValidator;
+import lotto.common.ErrorMessage;
+import lotto.domain.WinningNumbers;
+
+import java.util.List;
 
 public class BonusNumberValidator {
+    private static final Integer MININUM_NUMBER = 1;
+    private static final Integer MAXIM1UM_NUMBER = 45;
 
-    public static void validateBonusNumber(String input){
+    public static void validateBonusNumber(String input,List<Integer> winningNumbers){
         CommonValidator.validateNullAndBlank(input);
         CommonValidator.validateInteger(input);
 
         int bonusNumber=Integer.parseInt(input);
 
-        //validateNumberInRange(bonusNumber);
-        //validateDistinctToWinnerNumbers(bonusNumber);
+        validateBonusNumberInRange(bonusNumber);
+        validateDistinctToWinnerNumbers(bonusNumber,winningNumbers);
+    }
+
+    private static void validateDistinctToWinnerNumbers(int bonusNumber,List<Integer> winningNumbers) {
+       boolean haveDuplicatedNumber=winningNumbers.stream().anyMatch(winningNumber->winningNumber==bonusNumber);
+
+       if (haveDuplicatedNumber){
+           throw new IllegalArgumentException(ErrorMessage.DUPLICATED_TO_WINNING_NUMBERS);
+       }
+    }
+
+    private static void validateBonusNumberInRange(int bonusNumber) {
+        if (bonusNumber<MININUM_NUMBER || bonusNumber>MAXIM1UM_NUMBER){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE);
+        }
     }
 }
