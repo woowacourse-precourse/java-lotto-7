7b2fd0eaee1024bf5a89c0c5d51601d5d0598664
hanwool <hanwe92@gmail.com>
2024-10-30 17:56:36 +0900

    feat(inputNumberView): Get input using readLine

---
+++ src/main/java/lotto/view/InputNumberView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputNumberView {
+    public static String getInput(String question) {
+
+        System.out.println(question);
+        return readLine();
+    }
+}
