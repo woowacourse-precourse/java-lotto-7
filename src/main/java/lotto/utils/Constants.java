78ab3d0e4e8de09ecdf444860cb96c513633dc4b
Seo Jinmann <sjmmics@gmail.com>
2024-10-30 16:20:03 +0900

    feat : ($Constants) 여러 클래스에서 쓰이는 상수 값을 보관하는 클래스 추가

---
+++ src/main/java/lotto/utils/Constants.java
@@ -0,0 +1,15 @@
+package lotto.utils;
+
+public final class Constants {
+    
+    private Constants() {}
+    
+    public static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] ";
+    
+    public static final int MIN_LOTTO_NUMBER = 1;
+    
+    public static final int MAX_LOTTO_NUMBER = 45;
+    
+    public static final int CONSIST_NUMBER_OF_LOTTO = 6;
+    
+}
