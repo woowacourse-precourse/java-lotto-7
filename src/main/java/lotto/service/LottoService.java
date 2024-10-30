bfcb5b34da6f6e398d8054d1cba6342cef2b37c6
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 03:41:02 +0900

    feat(calculateLottoQuantities): 로또 수량 계산 기능 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,7 @@
+package lotto.service;
+
+public class LottoService {
+    public int calculateLottoQuantities(int purchaseAmount) {
+        return purchaseAmount / 1000;
+    }
+}
--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -6,22 +6,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoServiceTest {
 
-    private final LottoService lottoService;
-    /*
-    발행한 로또 수량 테스트
-    구입 금액을 1000으로 나눈 수가 맞는지 테스트
-     */
+    private final LottoService lottoService = new LottoService();
 
     @Test
     void 발행한_로또의_수량이_올바른지_테스트() {
         // given
         int purchaseAmount = 5000;
-        int answer = purchaseAmount % 1000;
+        int expected = purchaseAmount / 1000;
 
         // when
         int result = lottoService.calculateLottoQuantities(purchaseAmount);
 
         // then
-        assertThat(result).isEqualTo(result);
+        assertThat(result).isEqualTo(expected);
     }
 }
