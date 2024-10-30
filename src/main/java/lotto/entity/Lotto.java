73f9785b64af51cdff69c9cf3430ef8c2e6e78e3
kungbi <kipper12335@gmail.com>
2024-10-30 18:52:28 +0900

    refactor(model): Lotto에 null 또는 빈리스트에 대한 검증로직 추가

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -3,9 +3,11 @@ package lotto.entity;
 import static lotto.exception.ExceptionUtils.throwIllegalArgument;
 import static lotto.exception.LottoExceptionMessage.DUPLICATE_NUMBERS;
 import static lotto.exception.LottoExceptionMessage.INVALID_NUMBER_COUNT;
+import static lotto.exception.LottoExceptionMessage.NULL_OR_EMPTY_NUMBERS;
 import static lotto.exception.LottoExceptionMessage.NUMBER_OUT_OF_RANGE;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     public static final int LOTTO_MIN_NUMBER = 1;
@@ -20,6 +22,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throwIllegalArgument(NULL_OR_EMPTY_NUMBERS);
+        }
+        if (numbers.stream().anyMatch(Objects::isNull)) {
+            throwIllegalArgument(NULL_OR_EMPTY_NUMBERS);
+        }
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throwIllegalArgument(INVALID_NUMBER_COUNT);
         }
--- src/main/java/lotto/exception/LottoExceptionMessage.java
+++ src/main/java/lotto/exception/LottoExceptionMessage.java
@@ -3,7 +3,8 @@ package lotto.exception;
 public enum LottoExceptionMessage implements ExceptionMessage {
     INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
     DUPLICATE_NUMBERS("로또 번호는 중복될 수 없습니다."),
-    NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45까지의 숫자여야 합니다.");
+    NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45까지의 숫자여야 합니다."),
+    NULL_OR_EMPTY_NUMBERS("로또 번호는 비어있거나 null일 수 없습니다.");
 
     private final String message;
 
