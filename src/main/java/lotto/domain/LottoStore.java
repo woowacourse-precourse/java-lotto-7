3f0068782d2376c668403031e97ceca55bc1f2c3
sinminseok12 <sinminseok12@naver.com>
2024-10-31 04:39:42 +0900

    feat(LottoStore) : 로또 구입 기능 구현
    구입 금액(1000원)에 맞춰 로또 티켓(LottoTicket)을 생성하는 기능을 구현했다.

---
+++ src/main/java/lotto/domain/LottoStore.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import lotto.utils.NumbersGenerator;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static lotto.constants.LottoValue.LOTTO_PRICE;
+
+public class LottoStore {
+    private final NumbersGenerator numbersGenerator;
+
+    public LottoStore(NumbersGenerator numbersGenerator) {
+        this.numbersGenerator = numbersGenerator;
+    }
+
+    public LottoTicket buyLottoTicket(int price) {
+        return new LottoTicket(IntStream.range(0, price / LOTTO_PRICE.getValue())
+                .mapToObj(i -> Lotto.from(numbersGenerator.createNumbers()))
+                .collect(Collectors.toList()));
+    }
+}
