09d39ac5c5ed26b952e95a7eecdd9fda1294fa6f
CheChe903 <cheche903@naver.com>
2024-10-30 16:37:21 +0900

    feat(LottoGenerator): 로또 생성기 구현
    LottoGenerator를 통해 로또를 생성하는 것을 구현합니다.

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.LottoNumber;
+import lotto.number.IntegersGenerator;
+import lotto.number.NumbersGenerator;
+
+public class LottoGenerator {
+
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int COUNT = 6;
+
+    private final NumbersGenerator numbersGenerator;
+
+    public LottoGenerator() {
+        this.numbersGenerator = new IntegersGenerator();
+    }
+
+    public Lotto generate() {
+
+        List<Integer> lottoNumbers = numbersGenerator.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+        List<LottoNumber> lotto = new ArrayList<>();
+
+        for (Integer number : lottoNumbers) {
+            lotto.add(new LottoNumber(number));
+        }
+        
+        return new Lotto(lotto);
+    }
+}
