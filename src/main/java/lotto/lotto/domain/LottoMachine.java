9ef262983d2d9e36d2c79d3b32a3e8f59ae0e99b
EHCTH <lees90896@gmail.com>
2024-10-30 17:52:47 +0900

    feat(LottoMachine): 기능 추가 * feat: PurchaseCountViewService Di * style: 메서드 분리

--- src/main/java/lotto/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/lotto/domain/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto.lotto.domain;
 
 import lotto.buyer.domain.Money;
+import lotto.view.output.domain.PurchaseCountViewService;
 
 import java.util.stream.Stream;
 
@@ -9,17 +10,24 @@ import static java.util.stream.Collectors.toList;
 public class LottoMachine {
     private final Calculator calculator;
     private final NumberGenerator numberGenerator;
-    public LottoMachine(Calculator calculator, NumberGenerator numberGenerator) {
+    private final PurchaseCountViewService purchaseCountOutput;
+    public LottoMachine(Calculator calculator, NumberGenerator numberGenerator, PurchaseCountViewService purchaseCountOutput) {
         this.calculator = calculator;
         this.numberGenerator = numberGenerator;
+        this.purchaseCountOutput =purchaseCountOutput;
     }
     public LottoTickets purchaseLottoTickets(Money money) {
-        int count = calculator.divideByThousand(money);
+        int count = purchaseCount(money);
         return LottoTickets.of(Stream.generate(this::createLotto)
                 .limit(count)
                 .collect(toList()));
     }
     private Lotto createLotto() {
         return new Lotto(numberGenerator.generate());
+    }
+    private int purchaseCount(Money money) {
+        int count = calculator.divideByThousand(money);
+        purchaseCountOutput.view(count);
+        return count;
     }
 }
