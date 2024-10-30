217dc99e38af9b66f526e55062f405a2f10c74d6
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-30 17:57:19 +0900

    feat: controller 기능 연결

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,22 @@
 package lotto.controller;
 
+import lotto.Lotto;
+import lotto.handler.InputHandler;
+import lotto.handler.PrintHandler;
+
 public class Controller {
+    InputHandler inputHandler;
+    PrintHandler printHandler;
+    Lotto lotto;
+
+    public Controller() {
+        inputHandler = new InputHandler();
+        printHandler = new PrintHandler();
+        lotto = new Lotto();
+    }
+
+    public void run () {
+        printHandler.PrintBuyMoneyAmount();
+        inputHandler.setInputMoney();
+    }
 }
