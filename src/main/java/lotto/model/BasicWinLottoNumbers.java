8c5cb38b144cfa54bdfdad987a8cb84b664e3e11
Seo Jinmann <sjmmics@gmail.com>
2024-10-30 16:44:11 +0900

    feat : ($BasicWinLottoNumbers) 기본 로또 당첨 숫자를 보관한 클래스 추가

---
+++ src/main/java/lotto/model/BasicWinLottoNumbers.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class BasicWinLottoNumbers {
+    
+    private static final String NUMBER_IN_RANGE = "([1-9]|[1-3][0-9]|4[0-5])";
+    
+    private static final String REGEX_FORMAT = "^(" + NUMBER_IN_RANGE + ",){5}" + NUMBER_IN_RANGE + "$";
+    
+    private static final Pattern PATTERN = Pattern.compile(REGEX_FORMAT);
+    
+    private final Set<Integer> numbers;
+    
+    public BasicWinLottoNumbers(String numbersToValidate) {
+        validateFormat(numbersToValidate);
+        String[] numbers = numbersToValidate.split(",");
+        this.numbers = Arrays.stream(numbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+    
+    private void validateFormat(String numbersToValidate) {
+        Matcher matcher = PATTERN.matcher(numbersToValidate);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+    
+    public Set<Integer> get() {
+        return Collections.unmodifiableSet(numbers);
+    }
+    
+}
