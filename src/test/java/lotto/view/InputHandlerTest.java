0b32605e73ecea89d6c71ba4b36bd6f17854484f
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 00:58:24 +0900

    test(validateWinningNumbers): 유효한 당첨 번호 범위 테스트 작성

--- src/test/java/lotto/view/InputHandlerTest.java
+++ src/test/java/lotto/view/InputHandlerTest.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import com.sun.jdi.request.StepRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +18,7 @@ public class InputHandlerTest {
     @ParameterizedTest
     @ValueSource(ints = {-1000, -1, 0, 1, 100, 1001})
     void 구입_금액이_1000의_배수가_아니면_예외_발생(int purchaseAmount) {
-        assertThatThrownBy(() -> validatePurchaseAmount(purchaseAmount))
+        assertThatThrownBy(() -> InputHandler.validatePurchaseAmount(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
     }
@@ -31,17 +32,32 @@ public class InputHandlerTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideIntegerLists")
-    void 당첨_번호가_1에서_45_사이의_값이_아니면_예외_발생(List<Integer> winningNumbers) {
-        assertThatThrownBy(() -> validateWinningNumbers(winningNumbers))
+    @MethodSource("provideInvalidWinningNumbers")
+    void 당첨_번호가_1에서_45_사이의_값이_아니면_예외_발생(String winningNumbers) {
+        assertThatThrownBy(() -> InputHandler.validateWinningNumbers(winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 당청 번호는 1에서 45 사이의 값이어야 합니다.");
+                .hasMessage("[ERROR] 당청 번호는 1에서 45 사이의 정수 값이어야 합니다.");
     }
 
-    static Stream<List<Integer>> provideIntegerLists() {
+    static Stream<List<Integer>> provideInvalidWinningNumbers() {
         return Stream.of(
-                List.of(1, 2, 3, 4, 5, 46),
-                List.of(0, 1, 2, 3, 4, 5)
+                List.of(0, 1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 46)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidWinningNumbers")
+    void 당첨_번호가_1에서_45_사이의_값이면_예외_없음(String winningNumbers) {
+        assertDoesNotThrow(() -> {
+            InputHandler.validateWinningNumbers(winningNumbers);
+        });
+    }
+
+    static Stream<List<Integer>> provideValidWinningNumbers() {
+        return Stream.of(
+                List.of(1,2,3,4,5,6),
+                List.of(1,2,3,4,5,45)
         );
     }
 }
