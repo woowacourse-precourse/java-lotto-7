265ce50264bb4621222ca9499f6567842d7a7bff
YunCheol1 <naro2727@naver.com>
2024-10-30 20:31:31 +0900

    fix(inputview):로직수정 -금액,로또번호,보너스번호 입력받는 메서드들 로직수정

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -1,18 +1,21 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Inputview {
-    public static int inputMoney(int money) {
+    public static int inputMoney() {
+        int money = Integer.parseInt(Console.readLine());
         if (money % 10 != 0) {
             throw new IllegalArgumentException();
         }
         return money;
     }
 
-    public static String inputWinningNumbers(String winningNumbers) {
-        return winningNumbers;
+    public static String inputWinningNumbers() {
+        return Console.readLine();
     }
 
-    public static int inputBonusNumber(int BonusNumber) {
-        return BonusNumber;
+    public static int inputBonusNumber() {
+        return Integer.parseInt(Console.readLine());
     }
 }
