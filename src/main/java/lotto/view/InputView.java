91cfba53785849565d379303a7968643df13659b
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-30 18:43:06 +0900

    feat($InputView): 로또 구입 금액 입력 함수 추가
    - 사용자로부터 로또 구입 금액을 입력받는다.
    - 만약 구입 금액을 Integer 로 변환하는 데에 실패하면 예외가 발생하고, 다시 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public Integer inputPurchaseMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("올바른 숫자를 입력해 주세요.");
+            return inputPurchaseMoney();
+        }
+    }
 }
