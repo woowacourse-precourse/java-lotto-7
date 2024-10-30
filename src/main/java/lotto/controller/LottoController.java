ff83346198dd0bb5495af5ec0f453fba3ae189dc
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-30 19:21:18 +0900

    feat($LottoController): 로또 구입 금액 입력 기능 추가
    - InputView 를 통해 로또 구입 금액을 입력받는다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,12 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
 
+    private final InputView inputView = new InputView();
+
     public void run() {
+        int purchaseMoney = inputView.inputPurchaseMoney();
     }
 }
