8fbabe8cdd77318c59f19ef2ba3835d84f143684
unh6unh6 <ruddbs3308@naver.com>
2024-10-30 21:37:52 +0900

    feat(Lottos): 로또를 저장한다

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,12 +1,24 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
     public Lotto createLotto() {
         List<Integer> randomNumbers = getRandomNumbers();
         return new Lotto(randomNumbers);
+    }
+
+    public void saveLotto(final Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
     }
 
     private List<Integer> getRandomNumbers() {
--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,19 @@ public class LottosTest {
     void 로또를_발행한다() {
         assertThat(lottos.createLotto().getClass())
                 .isEqualTo(Lotto.class);
+    }
+
+    @Test
+    void 로또를_저장한다() {
+        Lotto lotto = new Lotto(
+                List.of(1,2,3,4,5,6)
+        );
+
+        lottos.saveLotto(lotto);
+
+        assertThat(lottos.getLottos()
+                .get(0)
+                .getLottoNumbers())
+                .isEqualTo(List.of(1,2,3,4,5,6));
     }
 }
