1d50c50719f1c368b9a27f61027f23e91c536407
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 21:54:57 +0900

    refactor: validator 제네릭으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,29 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private static final int LOTTO_COMBINATION_LENGTH = 6;
+
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    public Lotto(LottoFactory lottoFactory) {
+        this.numbers = lottoFactory.create();
+    }
+
+    private <T> void validate(List<T> numbers) {
+        if (numbers.size() != LOTTO_COMBINATION_LENGTH) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %개여야 합니다.", LOTTO_COMBINATION_LENGTH));
         }
     }
 
-    // TODO: 추가 기능 구현
 }
