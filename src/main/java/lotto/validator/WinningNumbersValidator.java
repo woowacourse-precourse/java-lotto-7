78e0acd9595c9833b6e21a8110b2ef81ea383865
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-30 23:32:27 +0900

    feat: 당첨 번호에 아무런 값이 없는 경우 예외 처리

--- src/main/java/lotto/validator/WinningNumbersValidator.java
+++ src/main/java/lotto/validator/WinningNumbersValidator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import lotto.exception.WinningNumbers.BlankWinningNumbersException;
 import lotto.exception.WinningNumbers.InvalidRuleException;
 
 public class WinningNumbersValidator {
@@ -7,7 +8,14 @@ public class WinningNumbersValidator {
     private static final String REGEX_INPUT_RULE = ".*[^,\\d\\s].*";
 
     public static void validate(final String input) {
+        validateBlank(input);
         validateDelimiter(input);
+    }
+
+    private static void validateBlank(final String input) {
+        if(input.isBlank()) {
+            throw new BlankWinningNumbersException();
+        }
     }
 
     private static void validateDelimiter(final String input) {
