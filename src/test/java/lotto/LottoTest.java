20a59c9295be284df2df9e141e0ed5da643518ce
dhrgodms <dhrgodms123@gmail.com>
2024-10-31 01:11:24 +0900

    test: 로또_발행 - 로또 개수에 맞추어 로또를 발행 test
    - IssueTicketTest.java: 로또 발행을 위한 메서드를 테스트하는 코드를 포함한다.
    - LottoTest.java: Lotto 클래스가 가지고 있는 메서드를 테스트하는 코드를 포함한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,25 +1,40 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+class LottoTest extends NsTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("LOTTO_03: 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("번호_정렬: List<Integer>로 받은 번호를 정렬한다.")
+    @Test
+    void 번호가_정렬_후_저장된다() {
+        assertSimpleTest(() -> new Lotto(List.of(42, 43, 41, 21, 8, 23)));
+        assertThat(output().contains("[8, 21, 23, 41, 42, 43]"));
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
 }
