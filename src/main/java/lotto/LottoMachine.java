f7acbd05f6b9322d85d8b1a0686458757187535e
Lee-Minju <mju7513@gmail.com>
2024-10-31 01:03:27 +0900

    LottoMachine.java | Purchase.java: 구매 금액을 검증하는 기능을 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -4,6 +4,16 @@ public class LottoMachine {
     InputView inputView = new InputView();
 
     public void playMachine() {
-        Purchase purchase = new Purchase(inputView.askPurchase());
+        Purchase purchase = makePurchase();
+    }
+
+    private Purchase makePurchase() {
+        try {
+            Purchase purchase = new Purchase(inputView.askPurchase());
+            return purchase;
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makePurchase();
+        }
     }
 }
--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -4,8 +4,20 @@ public class Purchase {
 
     private final int money;
 
-    public Purchase(String money) {
+    public Purchase(String money) throws IllegalArgumentException{
+        validate(money);
         this.money = Integer.parseInt(money);
+    }
+
+    private void validate(String money) {
+        try{
+            int moneyAmount = Integer.parseInt(money);
+            if(moneyAmount % 1000 != 0 || moneyAmount == 0) {
+                throw new IllegalArgumentException("[ERROR] 0이 아닌 1000으로 나누어지는 수를 입력해 주세요");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요");
+        }
     }
 
     public int numberOfPurchases() {
