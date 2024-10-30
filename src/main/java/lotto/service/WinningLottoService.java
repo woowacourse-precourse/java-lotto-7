d1706b998270231757c862b9478c2f81527bd88c
bowook <mbosx316@naver.com>
2024-10-31 01:04:16 +0900

    feat(WinningLottoService) : WinningLottoService 기능 추가
    - Parser로 List<Integer>로 먼저 변환을 수행한 후 검증할 수 있도록 기능한다.
    - Lotto 도메인에 List<Integer>로 생성자가 전달을 받기 때문이다.

--- src/main/java/lotto/service/WinningLottoService.java
+++ src/main/java/lotto/service/WinningLottoService.java
@@ -1,2 +1,15 @@
-package lotto.service;public class WinningLottoService {
+package lotto.service;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.utils.Parser;
+import lotto.validator.LottoValidator;
+
+public class WinningLottoService {
+
+    public Lotto generateWinningLotto(String winningNumbers) {
+        List<Integer> convertedWinningNumbers = Parser.parsingNumbers(winningNumbers);
+        LottoValidator.validateProcess(convertedWinningNumbers);
+        return new Lotto(convertedWinningNumbers);
+    }
 }
