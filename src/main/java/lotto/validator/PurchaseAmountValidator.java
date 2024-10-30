0678ce90e74fc3476f427192b789fbebaace4001
b1ueday <hxcva1@naver.com>
2024-10-30 19:23:52 +0900

    feat: 구매금액에 따른 예외처리 구현

---
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -0,0 +1,38 @@
+package lotto.validator;
+
+public class PurchaseAmountValidator {
+
+    private int purchaseAmount = 0;
+
+    public boolean isNotValidPurchaseAmount(String userInput) {
+        if (isNotParsableToInt(userInput)) {
+            return true;
+        }
+        if (canNotPurchase(purchaseAmount)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNotParsableToInt(String userInput) {
+        try {
+            purchaseAmount = Integer.parseInt(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 올바른 숫자를 입력해 주세요");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canNotPurchase(int purchaseAmount) {
+        try {
+            if (purchaseAmount < 1000 || (purchaseAmount % 1000) != 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또는 1000원 부터 1000원 단위로 구매 가능합니다.");
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,16 +2,23 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lotto.validator.PurchaseAmountValidator;
 
 public class InputView {
 
+    private final PurchaseAmountValidator purchaseAmountValidator = new PurchaseAmountValidator();
+
     public int inputPurchaseAmount() {
-        String userInput = Console.readLine();
+        String userInput;
+        do {
+            userInput = Console.readLine();
+        }
+        while (purchaseAmountValidator.isNotValidPurchaseAmount(userInput));
         return Integer.parseInt(userInput);
     }
+
 
     public Set<Integer> inputWinningNumbers() {
         String userInput = Console.readLine();
