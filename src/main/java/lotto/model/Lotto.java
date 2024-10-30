df999c6c561e350f1140444360ecb2693236a2c7
Dev-LHY <dev.leehy@gmail.com>
2024-10-31 05:59:29 +0900

    refactor(lotto): 예외 메세지 수정
    - ExceptionMessage 를 통해 예외 메세지 출력

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.Collections;
 import java.util.List;
+import lotto.exception.ExceptionMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +15,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_SIZE.getMessage());
         }
     }
 
