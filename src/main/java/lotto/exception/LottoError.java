ce4ea2408f9770a578b37cd434bfaba7b99bba08
yummygyudon <bang2brew@gmail.com>
2024-10-31 07:04:43 +0900

    feat(`exception`) : Lotto 에러 케이스 "숫자 중복(`NUMBER_DUPLICATED`)" 추가

--- src/main/java/lotto/exception/LottoError.java
+++ src/main/java/lotto/exception/LottoError.java
@@ -3,6 +3,7 @@ package lotto.exception;
 public enum LottoError {
 
     NUMBER_OUT_OF_RANGE("로또 번호는 1부터 46사이여야 합니다."),
+    NUMBER_DUPLICATED("로또 번호는 중복되지 않아야 합니다."),
     NUMBERS_SIZE_OVER("로또 번호는 6개여야 합니다."),
     ;
 
