d174212ea9a8f55946385611dc279427ed25ed0d
bowook <mbosx316@naver.com>
2024-10-31 01:31:36 +0900

    feat(ErrorMessage) : 보너스 번호 예외 메시지 추가

--- src/main/java/lotto/validator/exception/ErrorMessage.java
+++ src/main/java/lotto/validator/exception/ErrorMessage.java
@@ -6,7 +6,10 @@ public enum ErrorMessage {
     WINNING_LOTTO_IS_NOT_NUMBER("[ERROR] 당첨 번호는 숫자를 입력하셔야 합니다."),
     WINNING_LOTTO_IS_NOT_SIX_LENGTH("[ERROR] 당첨 번호는 6자리 입력하셔야 합니다."),
     WINNING_LOTTO_RANGE_ERROR("[ERROR] 당첨 번호는 1~45 사이의 수를 입력하셔야 합니다."),
-    WINNING_LOTTO_DUPLICATE_ERROR("[ERROR] 당첨 번호는 중복된 수 없이 입력하셔야 합니다.");
+    WINNING_LOTTO_DUPLICATE_ERROR("[ERROR] 당첨 번호는 중복된 수 없이 입력하셔야 합니다."),
+    BONUS_NUMBER_IS_NOT_NUMBER("[ERROR] 보너스 번호는 숫자를 입력하셔야 합니다"),
+    BONUS_NUMBER_RANGE_ERROR("[ERROR] 보너스 번호는 1~45 사이의 수를 입력하셔야 합니다."),
+    BONUS_NUMBER_DUPLICATE_WINNING_LOTTO("[ERROR] 보너스 번호는 당첨 번호와 중복된 수 없이 입력하셔야 합니다.");
 
     private final String errorMessage;
     ErrorMessage(String errorMessage) {
