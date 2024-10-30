e615ed3b23a86834eb7f9de76ab40c044436d31e
EHCTH <lees90896@gmail.com>
2024-10-30 17:54:15 +0900

    feat(LottoTickets): 기능 추가및 수정 * feat: info() : List<Lotto> * delete: sort() : void

--- src/main/java/lotto/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/lotto/domain/LottoTickets.java
@@ -6,13 +6,12 @@ public class LottoTickets {
     private final List<Lotto> tickets;
     private LottoTickets(List<Lotto> tickets) {
         this.tickets = tickets;
-        sort();
     }
     public static LottoTickets of(List<Lotto> tickets) {
         return new LottoTickets(tickets);
     }
-    private void sort() {
-        tickets.forEach(Lotto::sort);
+    public List<Lotto> info() {
+        return tickets;
     }
     @Override
     public String toString() {
