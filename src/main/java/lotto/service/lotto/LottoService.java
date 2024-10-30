59a709135740db00c8409f5bb08e9d14175bd92c
jiffyin7 <jiffyin7@gmail.com>
2024-10-31 05:55:06 +0900

    feat(LottoService): `Lotto` 1장 가격 반환

---
+++ src/main/java/lotto/service/lotto/LottoService.java
@@ -0,0 +1,14 @@
+package lotto.service.lotto;
+
+import lotto.service.lotto.constant.Lotto;
+
+/**
+ * @author : jiffyin7@gmail.com
+ * @since : 24. 10. 31.
+ */
+public class LottoService {
+
+  public int getPrice() {
+    return Lotto.PRICE;
+  }
+}
