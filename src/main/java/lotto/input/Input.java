226710875902c46e45c2258388ef4e5ebcda3d2f
bmg1203 <bmg1203@naver.com>
2024-10-31 03:18:28 +0900

    [feat]: 당첨 번호입력 받기 기능 추가
    - 당첨 번호를 입력 받습니다.
    - 예외 발생시 정상 입력이 나올 때까지 반복합니다.

--- src/main/java/lotto/input/Input.java
+++ src/main/java/lotto/input/Input.java
@@ -4,6 +4,8 @@ import static lotto.constants.ErrorMessage.INPUT_MUST_BE_NUMBER;
 import static lotto.constants.InputPrompts.ENTER_PURCHASE_AMOUNT;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
 
@@ -19,5 +21,29 @@ public class Input {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public static Lotto getWinningNumbers() {
+        while (true) {
+            System.out.println(ENTER_PURCHASE_AMOUNT.getPrompt());
+
+            try {
+                List<String> winningNumbersInput = List.of(Console.readLine().split(","));
+                List<Integer> winningNumbers = covertStringToInteger(winningNumbersInput);
+                return new Lotto(winningNumbers);
+            } catch (NumberFormatException e) {
+                System.out.println(INPUT_MUST_BE_NUMBER.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static List<Integer> covertStringToInteger(List<String> winningNumbersInput) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String input : winningNumbersInput) {
+            winningNumbers.add(Integer.parseInt(input));
+        }
+        return winningNumbers;
     }
 }
