88b3e23d4ed1275ac588ea9c064bea2f98899dc6
seaniiio <cnc4750@naver.com>
2024-10-30 22:38:07 +0900

    feat(Lotto): 로또 번호끼리의 중복 검사 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final static int LOTTO_NUMBER_UPPER_BOUND = 45;
     private final static int LOTTO_NUMBER_LOWER_BOUND = 1;
+    private final static int NUMBER_OF_LOTTO_NUMBERS = 6;
 
     private final List<Integer> numbers;
 
@@ -21,10 +24,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateNumberOfLottoNumbers(numbers);
         validateLottoNumberRange(numbers);
+        validateLottoDuplicated(numbers);
     }
 
     private void validateNumberOfLottoNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != NUMBER_OF_LOTTO_NUMBERS) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
@@ -34,6 +38,12 @@ public class Lotto {
             if (number < LOTTO_NUMBER_LOWER_BOUND || number > LOTTO_NUMBER_UPPER_BOUND) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호가 1~45 범위를 벗어납니다.");
             }
+        }
+    }
+
+    private void validateLottoDuplicated(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() < NUMBER_OF_LOTTO_NUMBERS) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
