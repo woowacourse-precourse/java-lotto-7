af4f778d6d7372405edb619585a4601f530a3b55
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-31 03:46:09 +0900

    feat: add handling user input for bonus number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public String requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT);
@@ -14,6 +15,11 @@ public class InputView {
 
     public String requestWinningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBERS);
+        return readWithoutTrim();
+    }
+
+    public String requestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER);
         return readWithoutTrim();
     }
 
