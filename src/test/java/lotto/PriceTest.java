fb618a9d1a2a9a9ae60a8ab6884665589e14586d
hongkikii <hyangmeeh@icloud.com>
2024-10-30 16:39:37 +0900

    test(PriceTest): 로또 구입 금액 입력 테스트 코드 작성

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -1,2 +1,35 @@
-package lotto;public class Price {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PriceTest {
+
+    @DisplayName("로또 구입 금액이 null일 경우 예외가 발생한다.")
+    @Test
+    void 로또_구입_금액이_null일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Price.from(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액에 문자가 포함될 경우 예외가 발생한다.")
+    @Test
+    void 로또_구입_금액에_문자가_포함될_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Price.from("3000.14"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Price.from("300 0"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Price.from(" "))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Price.from("*"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Price.from("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
