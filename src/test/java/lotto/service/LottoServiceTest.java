b03c7d71f6ff460df750aa73444f3bd8098d87d8
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 03:59:45 +0900

    test(sortLottoNumbersAscending): 로또 번호가 오름차순으로 정렬되는지 테스트

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -2,11 +2,28 @@ package lotto.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoServiceTest {
 
     private final LottoService lottoService = new LottoService();
+
+    @Test
+    void 로또_번호는_오름차순으로_정렬되어야_한다() {
+        // given
+        List<Integer> lottoNumber = new ArrayList<>(Arrays.asList(6, 5, 4, 3, 2, 1));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        // when
+        List<Integer> result = lottoService.sortLottoNumbersAscending(lottoNumber);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
 
     @Test
     void 발행한_로또의_수량이_올바른지_테스트() {
