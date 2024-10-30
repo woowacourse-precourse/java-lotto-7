3dd7ec2079c21a9369ab0259942f1b3d23b01a82
seaniiio <cnc4750@naver.com>
2024-10-30 21:42:41 +0900

    feat(Lotto): 로또 숫자 범위 검사 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,9 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    private final static int LOTTO_NUMBER_UPPER_BOUND = 45;
+    private final static int LOTTO_NUMBER_LOWER_BOUND = 1;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +14,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumberOfLottoNumbers(numbers);
+        validateLottoNumberRange(numbers);
+    }
+
+    private void validateNumberOfLottoNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateLottoNumberRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LOTTO_NUMBER_LOWER_BOUND || number > LOTTO_NUMBER_UPPER_BOUND) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호가 1~45 범위를 벗어납니다.");
+            }
         }
     }
 
