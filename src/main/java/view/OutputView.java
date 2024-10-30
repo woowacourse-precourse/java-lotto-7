0537c2c710379113efb451b92aad84bb9d51f407
githublees <leeho_@naver.com>
2024-10-30 19:35:28 +0900

    feat(output): 로또 결과 출력 기능 추가

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.List;
+import lotto.Lotto;
+
+public class OutputView {
+
+    public static void printLottoNumber(List<Lotto> lottos) {
+        System.out.println(ViewMessage.PURCHASE_CONFIRMATION_MESSAGE.format(lottos.size()));
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+        System.out.println();
+    }
+
+    public static void printWinningHistory() {
+        System.out.println(ViewMessage.WINNING_STATISTIC_MESSAGE);
+    }
+
+}
