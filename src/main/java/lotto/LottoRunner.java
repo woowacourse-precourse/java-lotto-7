7cc2018106887239e02c1283ea3d45f4bc7ca443
JinGoo <zzgg3040@naver.com>
2024-10-30 19:35:33 +0900

    feat: 수량 만큼 랜덤 로또 생성

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoRunner {
     public static void start() {
@@ -13,6 +16,12 @@ public class LottoRunner {
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
+        }
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < money / 1000; i++) {
+            lottos.add(new Lotto(
+                    Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
     }
 
