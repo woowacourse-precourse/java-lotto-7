13de48274e19fee52e547539863910145c277888
woo <ldw28517@naver.com>
2024-10-31 01:55:20 +0900

    feat: 보너스 번호를 입력받을 수 있다.
    - Number 객체 생성 과정에서 NumberValidator를 통해 1~45 사이의 숫자임을 검증한다.
    - 별도의 enum 클래스를 활용해 에러 메시지를 관리한다.

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class BonusNumber {
+    private final Number number;
+
+    public BonusNumber(String number) {
+        this.number = new Number(number);
+    }
+}
---
+++ src/main/java/lotto/domain/Number.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import lotto.common.validator.NumberValidator;
+
+public class Number {
+    private final int number;
+
+    public Number(String number) {
+        NumberValidator.validate(number);
+        this.number = Integer.parseInt(number);
+    }
+}
