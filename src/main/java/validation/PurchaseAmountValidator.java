f41293f6aa84f299e0ead7589dd2a47ba81c0ff9
변우영 <likevangogh24@gmail.com>
2024-10-30 23:59:42 +0900

    feat(PurchaseAmountValidator): 로또 구입 금액이 1000원 단위가 아닐 경우 예외 처리 추가

---
+++ src/main/java/validation/PurchaseAmountValidator.java
@@ -0,0 +1,13 @@
+package validation;
+
+public class PurchaseAmountValidator {
+
+    private static final String ERROR_PREFIX = "[ERROR]";
+    private static final int THOUSAND = 1000;
+
+    public void validateDivisibleByThousand(long amount) {
+        if (amount % THOUSAND != 0) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "로또 구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
+}
