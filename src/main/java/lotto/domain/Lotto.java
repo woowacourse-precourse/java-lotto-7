464ed6362e9e33178365396e401151cce6f9f123
alpin87 <qortmdals94@naver.com>
2024-10-30 18:25:14 +0900

    feat: 로또 번호 중복 검증 메서드

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,6 +9,7 @@ public class Lotto {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final String ERROR_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String ERROR_DUPLICATE = "[ERROR] 로또 번호에 중복된 숫자가 있습니다.";
     private static final String ERROR_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private final List<Integer> numbers;
@@ -20,6 +21,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplicate(numbers);
         validateRange(numbers);
     }
 
@@ -27,6 +29,18 @@ public class Lotto {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_SIZE);
         }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.size() != numbers.stream()
+                .distinct()
+                .count();
     }
 
     private void validateRange(List<Integer> numbers) {
