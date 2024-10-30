05bd5877930a47dd22a325cd34f0b90ecab705ec
corpi <lj845854@gmail.com>
2024-10-30 16:05:19 +0900

    refactor(LottoHistory): 회차 당첨번호 저장 타입 변경

--- src/main/java/lotto/model/LottoHistory.java
+++ src/main/java/lotto/model/LottoHistory.java
@@ -3,11 +3,14 @@ package lotto.model;
 import java.util.List;
 
 public class LottoHistory {
-    private final Lotto winningLotto;
-    private final List<Lotto> lottoList;
+    private final List<Integer> winningNumberList;
+    private final int winningBonusNumber;
+    private final List<Lotto> boughtLottoList;
 
-    public LottoHistory(Lotto winningLotto, List<Lotto> lottoList) {
-        this.winningLotto = winningLotto;
-        this.lottoList = lottoList;
+    public LottoHistory(List<Integer> winningNumberList, int winningBonusNumber, List<Lotto> boughtLottoList) {
+        List<Lotto> buiedLottoList;
+        this.winningNumberList = winningNumberList;
+        this.winningBonusNumber = winningBonusNumber;
+        this.boughtLottoList = boughtLottoList;
     }
 }
