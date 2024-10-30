2fd4bd984d020cd02d6b12ad2cf7199ba0b17c60
bowook <mbosx316@naver.com>
2024-10-31 01:02:58 +0900

    refactor(PurchaseService) : PurchaseService 리팩토링
    - 파서를 먼저 실행하고 전환된 int형을 검증할 수 있도록 개선

--- src/main/java/lotto/service/PurchaseService.java
+++ src/main/java/lotto/service/PurchaseService.java
@@ -1,17 +1,14 @@
 package lotto.service;
 
 import lotto.domain.Money;
+import lotto.utils.Parser;
 import lotto.validator.PurchaseValidator;
 
 public class PurchaseService {
-    private final PurchaseValidator purchaseValidator;
 
-    public PurchaseService(PurchaseValidator purchaseValidator) {
-        this.purchaseValidator = purchaseValidator;
-    }
-
-    public Money validateAndCreateMoney(String purchaseAmount) {
-        purchaseValidator.validateProcess(purchaseAmount);
+    public Money purchaseMoney(String purchaseAmount) {
+        int convertedPurchaseAmount = Parser.parsingPurchaseAmount(purchaseAmount);
+        PurchaseValidator.validateProcess(convertedPurchaseAmount);
         return new Money(Integer.parseInt(purchaseAmount));
     }
 }
