5ebc2db94330f5a72364c605b4739f8eef74e5b5
CheChe903 <cheche903@naver.com>
2024-10-30 14:36:32 +0900

    feat(IntegersGeneratorTest): 랜덤 숫자 리스트 생성 기능 단위 테스트 구현
    해당 기능의 단위 테스트를 구현합니다.
    1. 랜덤 숫자는 정해진 COUNT 만큼 size가 나와야합니다.
    2. 랜덤 숫자는 정해진 범위 안이여야합니다.
    숫자 리스트 생성기는 오직 '생성'의 책임만 있으므로, 해당 숫자가 유효한 숫자인지는 검증하지 않습니다.

---
+++ src/test/java/lotto/number/IntegersGeneratorTest.java
@@ -0,0 +1,38 @@
+package lotto.number;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IntegersGeneratorTest {
+
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int COUNT = 6;
+
+    private NumbersGenerator<Integer> numbersGenerator;
+    private List<Integer> randomPickNumbers;
+
+    @BeforeEach
+    void setUp() {
+        numbersGenerator = new IntegersGenerator();
+        randomPickNumbers = numbersGenerator.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+    }
+
+    @Test
+    void 랜덤_숫자는_정해진_COUNT만큼_나와야한다() {
+        //then
+        assertThat(randomPickNumbers.size()).isEqualTo(COUNT);
+    }
+
+    @Test
+    void 랜덤_숫자는_정해진_범위_안이여야한다() {
+        //then
+        for (Integer number : randomPickNumbers) {
+            assertThat(number).isGreaterThanOrEqualTo(START_INCLUSIVE);
+            assertThat(number).isLessThanOrEqualTo(END_INCLUSIVE);
+        }
+    }
+}
