4dec817dc72fabab4044084ce899487890afdbb8
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-30 17:52:19 +0900

    feat: 구입 금액을 입력받고 반환해주는  기능 추가

--- src/main/java/lotto/handler/InputHandler.java
+++ src/main/java/lotto/handler/InputHandler.java
@@ -1,4 +1,13 @@
 package lotto.handler;
 
 public class InputHandler {
+    private int inputMoney;
+
+    public void setInputMoney(int money) {
+        inputMoney = money;
+    }
+
+    public int getInputMoney() {
+        return inputMoney;
+    }
 }
