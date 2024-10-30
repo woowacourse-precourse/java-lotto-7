99e95f7f175ce92e1570ca4d44e052e485eb4e80
openmpy <suhwan@kakao.com>
2024-10-30 16:39:46 +0900

    test: 당첨된 로또 수익률 계산하는 기능 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -66,4 +66,33 @@ class LottoResultTest {
         // then
         assertThat(lottoResult.calculateTotalPrize()).isEqualTo(2_031_555_000);
     }
+
+    @Test
+    void 로또_수익률_확인() {
+        // given
+        List<Integer> winningLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusLottoNumber = 7;
+
+        List<Lotto> lottoList = List.of(
+                new Lotto(List.of(8, 21, 23, 41, 42, 43)),
+                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
+                new Lotto(List.of(7, 11, 16, 35, 36, 44)),
+                new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+                new Lotto(List.of(13, 14, 16, 38, 42, 45)),
+                new Lotto(List.of(7, 11, 30, 40, 42, 43)),
+                new Lotto(List.of(2, 13, 22, 32, 38, 45)),
+                new Lotto(List.of(1, 3, 5, 14, 22, 45))
+        );
+
+        Lotto winningLotto = new Lotto(winningLottoNumbers);
+        LottoNumber bonusNumber = new LottoNumber(bonusLottoNumber);
+
+        WinningLotto winningLottoResult = new WinningLotto(winningLotto, bonusNumber);
+
+        // when
+        LottoResult lottoResult = new LottoResult(lottoList, winningLottoResult);
+
+        // then
+        assertThat(lottoResult.calculateYield()).isEqualTo(62.5);
+    }
 }
