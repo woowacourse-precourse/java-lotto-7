74af6d2e1a0d3aa212019b0e8997e30b0607c93b
unh6unh6 <ruddbs3308@naver.com>
2024-10-30 18:17:40 +0900

    feat(view): 구입금액 입력 안내 문구 출력

---
+++ src/main/java/lotto/view/PurchaseAmountInputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class PurchaseAmountInputView {
+    private static final String PURCHASE_AMOUNT_INPUT_GUIDE = "구입금액을 입력해 주세요.";
+
+    public void printPurchaseAmountInputGuide() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_GUIDE);
+    }
+}
