c20a158b3a1273eab371366ae61c7515df680d4a
Kim Kyeongho <sgn07124@gmail.com>
2024-10-31 00:09:47 +0900

    feat(Delimiter): enum 클래스에 상수로 추가
    - 구분자 기호인 쉼표(,) 추가
    - 입력 관련 예외 처리 시 사용되는 [ERROR] 추가

---
+++ src/main/java/enums/Delimiter.java
@@ -0,0 +1,16 @@
+package enums;
+
+public enum Delimiter {
+    ERROR("[ERROR] "),
+    COMMA(",");
+
+    private final String delimiter;
+
+    Delimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+}
