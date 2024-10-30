f23bc0e755eb8f257be8094b69ab4cfdb1171544
choign3123 <choign3123@naver.com>
2024-10-30 17:16:18 +0900

    feat(LottoStore): 로또 구입 기능 구현

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoStore {
+
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+    private static final int TOTAL_LOTTO_COUNT = 6;
+
+    public List<Lotto> purchase(int price) {
+        int numOfLotto = price % Lotto.PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < numOfLotto; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, TOTAL_LOTTO_COUNT);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
+    }
+}
