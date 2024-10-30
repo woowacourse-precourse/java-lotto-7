eb64472a72ca3422088da52271c87846622bb414
rjswjddn <rjswjddn@gmail.com>
2024-10-31 03:56:55 +0900

    feat: 구입 금액 입력

---
+++ src/main/java/lotto/view/Request.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Request {
+
+    public String inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
