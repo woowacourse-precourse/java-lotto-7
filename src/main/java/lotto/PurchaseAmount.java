86bd3a9715dcdb6ab2d06c104bb26dab09d90e05
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-30 21:41:54 +0900

    feat: 구입 금액 예외 처리

---
+++ src/main/java/lotto/PurchaseAmount.java
@@ -0,0 +1,33 @@
+package lotto;
+
+public class PurchaseAmount{
+    private static final Integer LOTTE_PRICE=1000;
+    private final int money;
+
+    public PurchaseAmount(int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+
+    private void validateMoney(Integer money) {
+        validateNonNegative(money);
+        validateDivisibleByLottoPrice(money);
+    }
+
+    private void validateDivisibleByLottoPrice(Integer money) {
+        if(money%LOTTE_PRICE!=0){
+            throw new IllegalArgumentException("[ERROR] 1000으로 나누져야 합니다.");
+        }
+
+    }
+
+    private void validateNonNegative(Integer money) {
+        if(money<=0){
+            throw new IllegalArgumentException("[ERROR] 금액은 0보다 커야 합니다.");
+        }
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+}
