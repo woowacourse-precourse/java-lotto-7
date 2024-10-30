e6c66e7108fa1e303ecc702cff63ca4fb9d88ec7
StoneCAU <seokwoo1999@naver.com>
2024-10-31 00:15:20 +0900

    feat(ErrorMessage, LottoException): 예외 처리 클래스 추가
    - ErrorMessage: 에러 메세지를 관리하는 enum 클래스
    - LottoException: 커스텀 예외 처리 클래스

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,13 @@
+package lotto.exception;
+
+public class LottoException extends IllegalArgumentException {
+    private static final String PREFIX = "[ERROR]";
+
+    public LottoException(ErrorMessage message) {
+        super(PREFIX + message.getMessage());
+    }
+
+    public LottoException(String message) {
+        super(PREFIX + message);
+    }
+}
