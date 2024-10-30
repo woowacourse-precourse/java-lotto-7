b75345718d935ab73114bc84511321e958bb5349
seaniiio <cnc4750@naver.com>
2024-10-30 21:56:51 +0900

    feat(LottoRank): 로또 등수와 당첨 금액을 관리하는 enum 생성

---
+++ src/main/java/lotto/constant/LottoRank.java
@@ -0,0 +1,29 @@
+package lotto.constant;
+
+public enum LottoRank {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FORTH(50_000),
+    FIFTH(5_000);
+
+    private int prize;
+    private int numberOfWins;
+
+    LottoRank(int prize) {
+        this.prize = prize;
+        this.numberOfWins = 0;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public void win() {
+        this.numberOfWins++;
+    }
+
+    public int getTotalPrize() {
+        return (prize * numberOfWins);
+    }
+}
