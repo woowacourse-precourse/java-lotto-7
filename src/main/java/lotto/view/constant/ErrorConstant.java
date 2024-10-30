5ab2cb2dc81c977f39479d1b0ffdc53b8691559a
iceprins <junghankyul@gmail.com>
2024-10-31 00:57:24 +0900

    feat: 새로운 상수 문자열 추가
    WinningNumbersValidator 클래스에서 에러 메시지 출력 시 필요한 문자열들을 상수로 선언해 ErrorConstant에 분리하였다.

--- src/main/java/lotto/view/constant/ErrorConstant.java
+++ src/main/java/lotto/view/constant/ErrorConstant.java
@@ -1,8 +1,12 @@
 package lotto.view.constant;
 
 public class ErrorConstant {
+    public static final String ERROR_MARK = "[ERROR] ";
     public static final String NOT_ENTERED = "입력값이 존재하지 않습니다.";
     public static final String NOT_NUMBER = "숫자를 입력해야 합니다.";
     public static final String NOT_POSITIVE = "로또는 한 개 이상 구입해야 합니다.";
     public static final String NOT_MULTIPLE_OF_THOUSAND = "1000 단위로 입력해야 합니다.";
+    public static final String NOT_VALID_DELIMITER = "당첨 번호는 쉼표(,)로 구분해 입력해야 합니다.";
+    public static final String NOT_WITHIN_VALID_RANGE = "당첨 번호는 1부터 45 사이의 숫자를 입력해야 합니다.";
+    public static final String NOT_CORRECT_NUMBER_COUNT = "당첨 번호는 숫자 6개를 입력해야 합니다.";
 }
