3086dab0a2e83d9256c9010945927214acbe5bdd
StoneCAU <seokwoo1999@naver.com>
2024-10-30 23:16:40 +0900

    feat(InputView, OutputView): View 기본 구성
    - InputView: input 관련 메세지 출력 및 사용자 입력
    - OutputView: output 관련 메세지 출력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final static String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private final static String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private final static String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static String inputPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBERS);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumbers() {
+        System.out.println(ASK_BONUS_NUMBER);
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    private final static String INFORM_PURCHASE_LOTTO_MESSAGE = "%d개를 구매했습니다.";
+    private final static String WINNING_STATISTICS_TITLE = "당첨 통계";
+    private final static String SEPARATOR = "---";
+}
