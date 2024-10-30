96dc3272df8d4ba919bf0250c609177e4e5d8a9a
CheChe903 <cheche903@naver.com>
2024-10-30 14:34:13 +0900

    feat(NumbersGenerator): 랜덤 숫자 추출기 기능 구현
    랜덤으로 숫자를 만들어 List를 만드는 기능을 구현합니다.
    인터페이스 및 제너릭 클래스를 사용하여 구현합니다.

---
+++ src/main/java/lotto/number/IntegersGenerator.java
@@ -0,0 +1,12 @@
+package lotto.number;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class IntegersGenerator implements NumbersGenerator<Integer> {
+    @Override
+    public List<Integer> pickUniqueNumbersInRange(int startInclusive, int endInclusive, int count) {
+
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+    }
+}
---
+++ src/main/java/lotto/number/NumbersGenerator.java
@@ -0,0 +1,9 @@
+package lotto.number;
+
+import java.util.List;
+
+public interface NumbersGenerator<T extends Number> {
+
+    List<T> pickUniqueNumbersInRange(int startInclusive, int endInclusive, int count);
+
+}
