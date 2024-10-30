56a1884c248a02fcb80c3c8173a8285acd2f071f
Spinichi <spinichi@naver.com>
2024-10-31 04:08:39 +0900

    feat: Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,34 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        UserAmountInput userAmountInput = new UserAmountInput();
+        userAmountInput.userAmountInput();
+
+        System.out.println();
+
+        PrintTicketCount printTicketCount = userAmountInput.createPrintTicket();
+        printTicketCount.printCount();
+        printTicketCount.repeatPrintNumber();
+
+        System.out.println();
+
+        UserLottoNumberInput lottoInput = new UserLottoNumberInput();
+        lottoInput.userLottoNumberInput();
+
+        Lotto lotto = lottoInput.saveLottoNumber();
+
+        UserBonusNumberInput userBonusNumberInput = new UserBonusNumberInput();
+        userBonusNumberInput.bonusDuplicationCheck(lotto); // 보너스 번호 중복 체크
+
+        System.out.println();
+
+        LotteryProcess lotteryProcess = new LotteryProcess(lotto,userBonusNumberInput,printTicketCount);
+        lotteryProcess.countMatchNumbers(); // 로또 추첨
+
+        PrintResult printResult = new PrintResult(lotteryProcess);
+        printResult.printPrize(); // 당첨 통계 출력
+
+        userAmountInput.printEarningsRate(printResult.calculateEarnings()); // 수익률 출력
     }
 }
