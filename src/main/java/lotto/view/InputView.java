be0493aecbc9bf73601fe73def640aee601a2d23
LeeJiWon <dlwldnjs1009@gmail.com>
2024-10-31 04:19:50 +0900

    feat: 보너스 번호 입력, 검증 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -51,6 +51,25 @@ public class InputView {
         return inputWinningNumber;
     }
 
+    // 보너스 번호 입력
+    public static int getBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        String input = Console.readLine();
+
+        // 빈 값인지 검증
+        EmptyInputValidator.validate(input);
+        // 숫자 형식인지 검증
+        NumberFormatValidator.validate(input);
+        // 1개만 입력 받았는지
+        validate1Numbers(input);
+        // 양수로 입력 받았는지
+        int bonusBall = Integer.parseInt(input);
+        // 로또 숫자가 1~45 사이의 숫자인지 검증
+        LottoNumberRangeValidator.validate(bonusBall);
+
+        return bonusBall;
+    }
+
     public static int validatePurchaseAmount(int input) {
         if ((input % 1000) != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.");
@@ -61,6 +80,12 @@ public class InputView {
     public static void validate6Numbers(Set<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static void validate1Numbers(String input) {
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1개여야 합니다.");
         }
     }
 }
