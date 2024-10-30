9f7abdec57a9a1f1ba5b12f4424f897effd35423
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-30 23:44:08 +0900

    feat: 당첨 번호가 1부터 45까지 아닌 경우 예외 처리

--- src/main/java/lotto/validator/WinningNumbersValidator.java
+++ src/main/java/lotto/validator/WinningNumbersValidator.java
@@ -1,15 +1,25 @@
 package lotto.validator;
 
 import lotto.exception.WinningNumbers.BlankWinningNumbersException;
+import lotto.exception.WinningNumbers.InvalidRangeException;
 import lotto.exception.WinningNumbers.InvalidRuleException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningNumbersValidator {
 
     private static final String REGEX_INPUT_RULE = ".*[^,\\d\\s].*";
+    private static final String DELIMITER = ",";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public static void validate(final String input) {
         validateBlank(input);
         validateDelimiter(input);
+        List<Integer> numbers = convert(input);
+        validateNumberRange(numbers);
     }
 
     private static void validateBlank(final String input) {
@@ -22,5 +32,21 @@ public class WinningNumbersValidator {
         if(input.matches(REGEX_INPUT_RULE)) {
             throw new InvalidRuleException();
         }
+    }
+
+    private static void validateNumberRange(final List<Integer> numbers) {
+        for(int number: numbers) {
+            checkRange(number);
+        }
+    }
+
+    private static void checkRange(final int number) {
+        if(!(number >= MIN_NUMBER && number <= MAX_NUMBER)) {
+            throw new InvalidRangeException();
+        }
+    }
+
+    private static List<Integer> convert(final String input) {
+        return Arrays.stream(input.split(DELIMITER)).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
     }
 }
