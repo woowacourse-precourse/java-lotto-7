59ca90f15c48a0e41a7e6af69c36dac46c7822ec
sinminseok12 <sinminseok12@naver.com>
2024-10-31 03:56:48 +0900

    test(LottoNumberTest) : 로또 번호 검증 기능 테스트 코드 구현

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 로또_번호가_1부터_45_가_아니면_예외가_발생한다(int number) {
+        assertThatThrownBy(() -> LottoNumber.from(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
