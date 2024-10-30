eea07da9bb0f74276ceda22d4c1efba915e4bf5f
Seo Jinmann <sjmmics@gmail.com>
2024-10-30 17:01:14 +0900

    feat : ($LottoController) view 계층과 소통하여 service 계층에 비지니스 로직 처리를 요청하는 클래스 추가
    InputView로부터 입력값을 전달 받아서 주요 모델을 생성한 다음 service에 넘겨준다. service 계층에 처리 결과를 받아서 OutputView에 전달한다.

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,71 @@
+package lotto.controller;
+
+import lotto.model.*;
+import lotto.servcie.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    
+    private final InputView inputView;
+    
+    private final OutputView outputView;
+    
+    private final LottoService service;
+    
+    
+    public LottoController(InputView inputView, OutputView outputView, LottoService service) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.service = service;
+    }
+    
+    public void run() {
+        SpendingMoney money = getSpendMoney();
+        
+        service.buyLotto(money);
+        BoughtLottos boughtLottos = service.getLottos();
+        outputView.printLottos(boughtLottos);
+        
+        BasicWinLottoNumbers winLottoNumbers = getWinLottoNumbers();
+        BonusWinLottoNumber bonusNumber = getBonusWinLottoNumber(winLottoNumbers);
+        WinLotto winLotto = new WinLotto(winLottoNumbers, bonusNumber);
+        
+        service.createWinStatistics(boughtLottos, winLotto);
+        LottoResult result = service.getResult();
+        
+        outputView.printResult(result);
+    }
+    
+    private SpendingMoney getSpendMoney() {
+        while (true) {
+            try {
+                return new SpendingMoney(inputView.getBuyingMoney());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private BonusWinLottoNumber getBonusWinLottoNumber(BasicWinLottoNumbers winLottoNumbers) {
+        while (true) {
+            try {
+                return new BonusWinLottoNumber(inputView.getBonusNumber(), winLottoNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private BasicWinLottoNumbers getWinLottoNumbers() {
+        while (true) {
+            try {
+                return new BasicWinLottoNumbers(inputView.getWinNumbers());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        
+    }
+    
+}
