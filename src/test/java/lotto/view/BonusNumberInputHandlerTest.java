0bb311159e6af9d7c62a56a8ac27e87627036a59
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 03:02:34 +0900

    test(validateBonusNumberIsInteger): 정수인 보너스 숫자 테스트

--- src/test/java/lotto/view/BonusNumberInputHandlerTest.java
+++ src/test/java/lotto/view/BonusNumberInputHandlerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class BonusNumberInputHandlerTest {
 
@@ -13,5 +14,11 @@ public class BonusNumberInputHandlerTest {
         assertThatThrownBy(() -> InputHandler.validateBonusNumberIsInteger(bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 숫자는 정수여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    void 보너스_숫자가_정수가_아니면_예외_발생(String bonusNumber) {
+        assertDoesNotThrow(() -> InputHandler.validateBonusNumberIsInteger(bonusNumber));
     }
 }
