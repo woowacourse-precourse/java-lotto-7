8ec3775ef05b6f5c4023b5b6aaf4d1a2ca77c2af
Seo Jinmann <sjmmics@gmail.com>
2024-10-30 16:50:23 +0900

    feat : ($WinLotto) 우승 로또 숫자들을 담은 클래스 추가
    우승 로또 숫자, 기본 숫자 6개와 보너스 숫자 1개를 담은 클래스이며 특정 로또 티켓(Lotto class)와 비교하여 똑같은 숫자가 몇개인지 보너스 숫자가 당첨되었는지 반환하는 기능 추가함.

---
+++ src/main/java/lotto/model/WinLotto.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import java.util.Set;
+
+public class WinLotto {
+    
+    private final Set<Integer> basicNumber;
+    
+    private final int bonusNumber;
+    
+    public WinLotto(BasicWinLottoNumbers numbers, BonusWinLottoNumber bonusNumber) {
+        this.basicNumber = numbers.get();
+        this.bonusNumber = bonusNumber.get();
+    }
+    
+    public int getMatchCount(Lotto lotto) {
+        Set<Integer> matchNumbers = lotto.getCommonNumbers(basicNumber);
+        return matchNumbers.size();
+    }
+    
+    public boolean getBonusMatch(Lotto lotto) {
+        return lotto.contains(bonusNumber);
+    }
+    
+}
