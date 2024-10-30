07ec285103e665d66c22c3cc0f17c42386c9ce25
함지현 <gkawlgus01@naver.com>
2024-10-30 16:06:08 +0900

    feat: 로또 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static final int LOTTO_PRICE = 1000;
@@ -8,6 +11,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int purchaseAmount = inputMoney();
+        int lottoCount = purchaseAmount / LOTTO_PRICE;
+
+        // 로또 발행
+        List<Lotto> purchasedLottos = issueLottos(purchaseAmount / LOTTO_PRICE);
+        System.out.println("\n" + lottoCount + "개를 구매했습니다.");
+        purchasedLottos.forEach(System.out::println);
     }
 
     private static int inputMoney() {
@@ -27,5 +36,13 @@ public class Application {
         if (amount <= 0 || amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static List<Lotto> issueLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottos;
     }
 }
