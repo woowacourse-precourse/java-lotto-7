116520335043272a36e7649d942f89024e5c614b
Mini <mini@MiniMac.local>
2024-10-31 01:50:23 +0900

    feat: add getter/setter to LottoGameInfo

--- src/main/java/model/LottoGameInfo.java
+++ src/main/java/model/LottoGameInfo.java
@@ -6,4 +6,30 @@ public class LottoGameInfo {
     private int purchaseAmount;
     private List<Integer> winningNumber;
     private int bonusNumber;
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public void setPurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
+    }
+
+    public void setWinningNumber(List<Integer> winningNumber) {
+        this.winningNumber = winningNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+
 }
