f15540daca898133b12c98ba65ae2d6852f9d899
choitaehoon <hugebigdream@gmail.com>
2024-10-30 18:55:52 +0900

    feat: 로또 구매 금액 입력 받는다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int lottoPurchasePrice = Integer.parseInt(validate(Console.readLine()));
+
+    }
+
+    private static String validate(String enterPrice) {
+        return enterPrice;
     }
 }
