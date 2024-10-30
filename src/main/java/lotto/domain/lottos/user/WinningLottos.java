5831565f4713a2ba2141481f29adfe1889eaa83a
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 17:36:55 +0900

    refactor : Rank EnumMap 0으로 초기화

--- src/main/java/lotto/domain/lottos/user/WinningLottos.java
+++ src/main/java/lotto/domain/lottos/user/WinningLottos.java
@@ -1,25 +1,43 @@
 package lotto.domain.lottos.user;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 import lotto.domain.Rank;
 
+/**
+ * 맵 초기화?
+ */
 public class WinningLottos {
-    private final List<Rank> ranks = new ArrayList<>();
+    //private final List<Rank> rankss = new ArrayList<>();
+
+    private final EnumMap<Rank,Integer> ranks = new EnumMap<>(Rank.class);
 
     public WinningLottos() {
+        initRanks();
     }
 
     public void addRank(Rank rank) {
-        ranks.add(rank);
+        ranks.merge(rank,1,Integer::sum);
     }
 
     public int getTotalWinningPrizeMoney(){
         int result = 0;
-        for (Rank rank : ranks) {
+
+        for (Rank rank : ranks.keySet()) {
             result += rank.getPrizeMoney();
         }
         return result;
+    }
+
+    private void initRanks(){
+        for (Rank rank : Rank.values()) {
+            ranks.put(rank,0);
+        }
+    }
+
+    public EnumMap<Rank,Integer> getWinningStatistics(){
+        return ranks;
     }
 
 
