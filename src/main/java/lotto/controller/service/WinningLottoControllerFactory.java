2a5d22f08ff47c8a76728e076cd95807938397f3
EHCTH <lees90896@gmail.com>
2024-10-30 18:52:16 +0900

    feat(WinningLottoContollerFactory): 기능 구현 * feat: +create(): WinningLottoController

--- src/main/java/lotto/controller/service/WinningLottoControllerFactory.java
+++ src/main/java/lotto/controller/service/WinningLottoControllerFactory.java
@@ -1,4 +1,20 @@
 package lotto.controller.service;
 
+import lotto.controller.domain.WinningLottoController;
+import lotto.lotto.winning.infrastructure.BonusNumberCreator;
+import lotto.lotto.winning.infrastructure.WinningLottoCreator;
+import lotto.view.input.hanlder.infrastructure.BonusNumberHandler;
+import lotto.view.input.hanlder.infrastructure.WinningLottoHandler;
+import lotto.view.input.infrastructure.BonusNumberInput;
+import lotto.view.input.infrastructure.WinningLottoInput;
+import lotto.view.output.infrastructure.BonusNumberOutput;
+import lotto.view.output.infrastructure.WinningLottoOutput;
+
 public class WinningLottoControllerFactory {
+    public static WinningLottoController create() {
+        WinningLottoCreator winningLottoCreator = new WinningLottoCreator(new WinningLottoHandler(new WinningLottoInput(new WinningLottoOutput())));
+        BonusNumberCreator bonusNumberCreator = new BonusNumberCreator(new BonusNumberHandler(new BonusNumberInput(new BonusNumberOutput())));
+        return new WinningLottoController(winningLottoCreator, bonusNumberCreator);
+    }
+
 }
