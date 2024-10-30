20f4d29b24147909ba94d499880acd9d224694aa
ppzepi <hsunguk1999@gmail.com>
2024-10-30 20:28:35 +0900

    feat: Lotto DTO 기본 작성

--- src/main/java/lotto/lottoModel/LotteryDTO.java
+++ src/main/java/lotto/lottoModel/LotteryDTO.java
@@ -1,4 +1,20 @@
 package lotto.lottoModel;
 
+import java.util.List;
+
 public class LotteryDTO {
+    private List<Integer> numbers;
+
+    public LottoDTO(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
 }
