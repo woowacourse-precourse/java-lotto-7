04a05b1116249976a77afca11db146789b919160
inhooo00 <chldlsgh0987@naver.com>
2024-10-30 23:14:37 +0900

    feat(model): Add NumberValidator class
    Add NumberValidator class :
    - 정수만 허용하는 정규식 처리.
    - 예외처리 합치는 validation 메서드 생성.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,10 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
