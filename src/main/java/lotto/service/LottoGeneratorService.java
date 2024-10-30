8740a9a011d18f674cd3bce4905babfb9f083227
bowook <mbosx316@naver.com>
2024-10-31 01:11:17 +0900

    feat(LottoGenerateService) : 오름차순 정렬 기능 추가

--- src/main/java/lotto/service/LottoGeneratorService.java
+++ src/main/java/lotto/service/LottoGeneratorService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
@@ -20,9 +21,11 @@ public class LottoGeneratorService {
     }
 
     private Lotto generateLotto() {
-        return new Lotto(generateNumbers());
+        return new Lotto((generateNumbers()));
     }
     private List<Integer> generateNumbers() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_LENGTH);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_LENGTH);
+        Collections.sort(numbers);
+        return numbers;
     }
 }
