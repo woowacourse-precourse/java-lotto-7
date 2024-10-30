42f60f8681b36cdd18acceb07757d4b2dab02d15
sik9252 <sik9252@naver.com>
2024-10-31 00:36:14 +0900

    feat: 구매한 로또 수 만큼 로또를 발행하고, 발행된 로또를 화면에 출력하는 메서드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,20 @@
 package lotto.controller;
 
 import lotto.exception.InvalidPurchaseAmountException;
+import lotto.model.PublishLotteries;
 import lotto.model.PurchasePrice;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     private int purchaseCount;
+    private PublishLotteries publishLotteries;
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public LottoController(InputView inputView) {
+    public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView =  outputView;
     }
 
     public void play() {
@@ -51,12 +56,12 @@ public class LottoController {
 
     // 구매한 로또 수 만큼의 로또를 발행한다
     public void publishLotto() {
-
+        publishLotteries = new PublishLotteries(purchaseCount);
     }
 
     // 발행된 로또 번호를 출력한다
     public void printPublishedLotto() {
-
+        outputView.printPublishedLotteries(publishLotteries.get());
     }
 
     // 당첨 번호를 입력 받는다
