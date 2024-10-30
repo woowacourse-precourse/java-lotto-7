5629a3516da577d3881bed6c84150dc721d9f932
himinqq <alsrud0966@naver.com>
2024-10-30 19:58:49 +0900

    feat(FixedNumberGenrator): 테스트를 위해 고정된 번호 생성하는 기능 구현

---
+++ src/main/java/lotto/utils/FixedNumberGenerator.java
@@ -0,0 +1,16 @@
+package lotto.utils;
+
+import java.util.List;
+
+public class FixedNumberGenerator implements LottoNumberGenerator {
+    private final List<Integer> fixedNumber;
+
+    public FixedNumberGenerator(List<Integer> fixedNumber) {
+        this.fixedNumber = fixedNumber;
+    }
+
+    @Override
+    public List<Integer> generate() {
+        return fixedNumber;
+    }
+}
