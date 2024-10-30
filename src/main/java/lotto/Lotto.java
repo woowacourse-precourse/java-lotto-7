256610c98c652931004ce6870077e850af854481
JeongSu Park <bechalrang@gmail.com>
2024-10-30 18:41:10 +0900

    feat(Lotto, LottoTest, README): `(발행된)로또` 생성 구현
    - `로또` 생성 구현
    - 해당 기능의 테스트 코드 수정
    - 구현한 기능 항목에 체크 표시

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,22 @@
 package lotto;
 
 import java.util.List;
+import lotto.value.LottoNumbers;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private final LottoNumbers numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+//        validate(numbers);
+        this.numbers = LottoNumbers.of(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+//    private void validate(LottoNumbers numbers) {
+//        if (numbers.size() != 6) {
+//            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+//        }
+//    }
 
     // TODO: 추가 기능 구현
 }
