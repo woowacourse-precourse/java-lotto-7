a6d2829eaeb85b16d3423cca6072a872b674acf5
EHCTH <lees90896@gmail.com>
2024-10-30 19:05:41 +0900

    test(WinningCalculatorTest): 당첨에 따른 금액 테스트

--- src/test/java/lotto/lotto/winning/domain/WinningCalculatorTest.java
+++ src/test/java/lotto/lotto/winning/domain/WinningCalculatorTest.java
@@ -1,4 +1,79 @@
 package lotto.lotto.winning.domain;
 
+import lotto.buyer.infrastructure.Won;
+import lotto.lotto.domain.Lotto;
+import lotto.lotto.domain.LottoTickets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class WinningCalculatorTest {
+    private WinningCalculator winningCalculator;
+    private LottoTickets lottoTickets;
+    private Lotto lotto;
+
+    @BeforeEach
+    void init() {
+        winningCalculator = new WinningCalculator(new Won());
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lottoTickets = LottoTickets.of(List.of(lotto));
+    }
+
+    @Test
+    @DisplayName("1등")
+    void firstPlace() {
+        verifyBenefit("1,2,3,4,5,6", "7", "2000000000");
+    }
+
+    @Test
+    @DisplayName("2등")
+    void secondPlace() {
+        verifyBenefit("1,2,3,4,5,7", "6", "30000000");
+    }
+
+    @Test
+    @DisplayName("3등")
+    void thirdPlace() {
+        verifyBenefit("1,2,3,4,5,8", "9", "1500000");
+    }
+
+    @Test
+    @DisplayName("4등")
+    void fourthPlace() {
+        verifyBenefit("1,2,3,4,7,8", "9", "50000");
+    }
+
+    @Test
+    @DisplayName("5등")
+    void fifthPlace() {
+        verifyBenefit("1,2,3,7,8,9", "10", "5000");
+    }
+
+    @Test
+    @DisplayName("꼴등")
+    void lastPlace() {
+        List<WinningLotto> losingLottos = List.of(
+                WinningLotto.of("1,2,7,8,9,10"),
+                WinningLotto.of("1,7,8,9,10,11"),
+                WinningLotto.of("7,8,9,10,11,12")
+        );
+        losingLottos.forEach(winningLotto -> verifyBenefit(winningLotto, "13", "0"));
+    }
+
+    private void verifyBenefit(String winningNumbers, String bonusNumber, String expectedBenefit) {
+        WinningLotto winningLotto = WinningLotto.of(winningNumbers);
+        BonusNumber bonus = BonusNumber.of(bonusNumber);
+        Benefit benefit = winningCalculator.updateBenefit(lottoTickets, winningLotto, bonus);
+        assertThat(benefit.toString()).isEqualTo(expectedBenefit);
+    }
+
+    private void verifyBenefit(WinningLotto winningLotto, String bonusNumber, String expectedBenefit) {
+        BonusNumber bonus = BonusNumber.of(bonusNumber);
+        Benefit benefit = winningCalculator.updateBenefit(lottoTickets, winningLotto, bonus);
+        assertThat(benefit.toString()).isEqualTo(expectedBenefit);
+    }
 }
