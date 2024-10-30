ef2174e5c2755974d829124c3c1c8a3c00950c27
sik9252 <sik9252@naver.com>
2024-10-30 23:01:36 +0900

    feat: Controller에서의 View 사용을 위한 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
+        InputView inputView = new InputView();
+        LottoController lottoController = new LottoController(inputView);
 
         lottoController.play();
     }
