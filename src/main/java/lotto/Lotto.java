6f81415367b7c2ed02fe69c29e641baeaf1dec56
bill291104 <bill29123@gmail.com>
2024-10-30 13:24:34 +0900

    test(Lotto): check duplicate in Lotto.numbers
    - Check duplicate numbers in Lotto.numbers
    - Add the condition in Lotto.validate()
    - Throw exception with message

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 없어야 합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
