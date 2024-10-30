8846eb3b33a6712cc65d7a5c8acc1f033ae15303
DongYeop <s01054830455@gmail.com>
2024-10-30 17:51:49 +0900

    refactor(ErrorMessage): 예외 메세지 변경

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
 
     ERROR_SIGNATURE("[ERROR] "),
     INVALID_INPUT("빈 값을 입력할 수 없습니다. 다시 입력해 주세요."),
-    ONLY_NUMERIC("문자를 입력할 수 없습니다. 숫자만 입력해주세요."),
+    ONLY_NUMERIC("정수만 입력할 수 있습니다."),
     NOT_CONTAIN_BLANK("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요.");
 
     private final String message;
