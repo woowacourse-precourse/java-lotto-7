9d19788bf9c5a087110d83c5dcc895b757726bf4
jenna <shinysuh1992@gmail.com>
2024-10-30 17:37:03 +0900

    refactor(LottoRunner): 구입 금액 로직 메서드 분리

--- src/main/java/lotto/core/LottoRunner.java
+++ src/main/java/lotto/core/LottoRunner.java
@@ -9,7 +9,7 @@ public class LottoRunner {
     private int payment;
 
     public void executeDraw() {
-
+        int purchaseAmount = this.getPurchaseAmount();
     }
 
 
@@ -20,14 +20,8 @@ public class LottoRunner {
         while (true) {
             try {
                 payment = Integer.parseInt(this.inputPayment());
-
-                if (payment % MINIMUM_UNIT > 0) {
-                    throw new IllegalArgumentException("구매 금액은 1,000원 단위로 입력해야 합니다.");
-                }
-
-                int amount = payment / MINIMUM_UNIT;
-                System.out.printf("%d개를 구매했습니다.\n", amount);
-                return amount;
+                this.validatePayment(payment);
+                return this.calculatePurchaseAmount(payment);
             } catch (NumberFormatException e) {
                 System.out.println(ERROR_PREFIX + "숫자를 입력해주세요.");
             } catch (IllegalArgumentException e) {
@@ -39,5 +33,17 @@ public class LottoRunner {
     private String inputPayment() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    private int calculatePurchaseAmount(int payment) {
+        int amount = payment / MINIMUM_UNIT;
+        System.out.printf("%d개를 구매했습니다.\n", amount);
+        return amount;
+    }
+
+    private void validatePayment(int payment) {
+        if (payment % MINIMUM_UNIT > 0) {
+            throw new IllegalArgumentException("구매 금액은 1,000원 단위로 입력해야 합니다.");
+        }
     }
 }
