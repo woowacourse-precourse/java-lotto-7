4d4e422e024fd28d3b8873a90d9e83572718f99c
bowook <mbosx316@naver.com>
2024-10-31 01:01:12 +0900

    feat(LottoValidator) : LottoValidator 기능 추가
    - 로또 검증 기능 추가
    - 파서와 역할을 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,14 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        LottoValidator.validateProcess(numbers);
     }
 
     public List<Integer> getNumbers() {
