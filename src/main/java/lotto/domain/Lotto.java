226004ef13e05f1acc3030d0633a7dae7322b2db
sinminseok12 <sinminseok12@naver.com>
2024-10-31 04:07:49 +0900

    feat(Lotto) : 로또 생성 기능 구현
    전달받은 List<Integer> 번호들을 이용해 로또(Lotto)를 생성한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,13 +3,25 @@ package lotto.domain;
 import lotto.validator.LottoValidator;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        LottoValidator.validateLotto(numbers);
+    public Lotto(List<LottoNumber> numbers) {
         this.numbers = numbers;
+    }
+
+    public static Lotto from(List<Integer> numbers) {
+        LottoValidator.validateLotto(numbers);
+        List<LottoNumber> lottoNumbers = convertToLottoNumbers(numbers);
+        return new Lotto(lottoNumbers);
+    }
+
+    private static List<LottoNumber> convertToLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::from)
+                .collect(Collectors.toList());
     }
 
 
