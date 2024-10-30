eef48fdb2f875a23c6ef4c8636fd537cf31c04d1
iceprins <junghankyul@gmail.com>
2024-10-31 00:50:14 +0900

    style: 메서드 위치 변경
    validateLottoPurchaseAmount 메서드의 위치를 변경했다.

--- src/main/java/lotto/validation/LottoPurchaseAmountValidator.java
+++ src/main/java/lotto/validation/LottoPurchaseAmountValidator.java
@@ -3,6 +3,17 @@ package lotto.validation;
 import lotto.view.constant.ErrorConstant;
 
 public class LottoPurchaseAmountValidator {
+    public void validateLottoPurchaseAmount(String inputLottoPurchaseAmount) {
+        if (!isEntered(inputLottoPurchaseAmount))
+            throw new IllegalArgumentException(ErrorConstant.ERROR_MARK + ErrorConstant.NOT_ENTERED);
+        if (!isNumber(inputLottoPurchaseAmount))
+            throw new IllegalArgumentException(ErrorConstant.ERROR_MARK + ErrorConstant.NOT_NUMBER);
+        if (!isPositive(inputLottoPurchaseAmount))
+            throw new IllegalArgumentException(ErrorConstant.ERROR_MARK + ErrorConstant.NOT_POSITIVE);
+        if (!isMultipleOfThousand(inputLottoPurchaseAmount))
+            throw new IllegalArgumentException(ErrorConstant.ERROR_MARK + ErrorConstant.NOT_MULTIPLE_OF_THOUSAND);
+    }
+
     private static boolean isEntered(String s) {
         return !s.isEmpty();
     }
@@ -17,16 +28,5 @@ public class LottoPurchaseAmountValidator {
 
     private static boolean isMultipleOfThousand(String s) {
         return Integer.parseInt(s) % 1000 == 0;
-    }
-
-    public void validateLottoPurchaseAmount(String inputLottoPurchaseAmount) {
-        if (!isEntered(inputLottoPurchaseAmount))
-            throw new IllegalArgumentException(ErrorConstant.NOT_ENTERED);
-        if (!isNumber(inputLottoPurchaseAmount))
-            throw new IllegalArgumentException(ErrorConstant.NOT_NUMBER);
-        if (!isPositive(inputLottoPurchaseAmount))
-            throw new IllegalArgumentException(ErrorConstant.NOT_POSITIVE);
-        if (!isMultipleOfThousand(inputLottoPurchaseAmount))
-            throw new IllegalArgumentException(ErrorConstant.NOT_MULTIPLE_OF_THOUSAND);
     }
 }
