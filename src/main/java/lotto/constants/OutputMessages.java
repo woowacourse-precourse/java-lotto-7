c3c6ab48377eaf459ee82b7580e65c10d4a9bce8
yush <yush2202@gmail.com>
2024-10-30 21:19:34 +0900

    feat($ OutputMessages): 출력 메시지 구현

---
+++ src/main/java/lotto/constants/OutputMessages.java
@@ -0,0 +1,20 @@
+package lotto.constants;
+
+public class OutputMessages {
+
+    private OutputMessages() {
+
+    }
+
+    public final static String INSERT_MONEY = "구입금액을 입력해 주세요.";
+    public final static String INSERT_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public final static String INSERT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public final static String ANNOUNCE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    public final static String DIVINER = "---";
+    public final static String ANNOUNCE_WIN_RESULT = "당첨 통계";
+    public final static String WIN_RESULT_FORMAT = "%d개 일치%s (%s원) - %d개";
+    public final static String BONUS_BALL_CASE = ", 보너스 볼 일치";
+    public final static String ANNOUNCE_PROFIT_FORMAT = "총 수익률은 %s% 입니다.";
+
+}
