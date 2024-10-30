5bd2f3b7b1887bdad1d27d57bf2ad4b149385cb6
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-30 15:14:14 +0900

    fix : 보너스 번호 변수 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -8,7 +8,7 @@ package lotto.domain;
  */
 public enum Rank {
     FIFTH(5,3,false,5000L,"오천"),
-    FOURTH(4,4,true,50000L,"오만"),
+    FOURTH(4,4,false,50000L,"오만"),
     THIRD(3,5,false,1500000L,"백오십만"),
     SECOND(2,5,true,30000000L,"삼천만"),
     FIRST(1,6,false,2000000000L,"이십억"),
@@ -27,6 +27,25 @@ public enum Rank {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
         this.prizeMoneyKorean = prizeMoneyKorean;
+    }
+
+    public Long getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    /**
+     * 만약 4자리가 같은데, 보너스 점수도 같으면?
+     * @param matchCount
+     * @param hasBonus
+     * @return
+     */
+    public static Rank findRank(int matchCount, boolean hasBonus){
+        for(Rank rank : values()){
+            if(rank.matchCount == matchCount && rank.hasBonus == hasBonus){
+                return rank;
+            }
+        }
+        return NOTHING;
     }
 
 }
