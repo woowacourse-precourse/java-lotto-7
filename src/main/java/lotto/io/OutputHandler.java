c05b8b778b9145150f5c38186de22f88321891ec
NaMinhyeok <nmh9097@gmail.com>
2024-10-30 09:06:50 +0900

    feat: 안내 메시지 출력 구현

---
+++ src/main/java/lotto/io/OutputHandler.java
@@ -0,0 +1,22 @@
+package lotto.io;
+
+public class OutputHandler {
+
+    public void showPurchaseAmountInstruction() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void showWinningNumbersInstruction() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void showBonusNumberInstruction() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void showWinningStatisticsComment() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+}
