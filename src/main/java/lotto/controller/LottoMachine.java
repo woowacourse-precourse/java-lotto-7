2fc42bdb77e95283c9a1141b65a5cf74e5d829e3
corpi <lj845854@gmail.com>
2024-10-30 15:43:18 +0900

    feat(LottoMachine): 로또를 회당 1000원에 구매할 수 있다.

---
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.view.InputView;
+
+public class LottoMachine {
+    private final InputView inputView;
+
+    public LottoMachine(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        List<Lotto> lottoList = buyLottoByPrice(1000);
+    }
+
+    private List<Lotto> buyLottoByPrice(int price) {
+        int money = inputView.inputMoney();
+        int count = money % price;
+        ArrayList<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> pickedNumberList = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(pickedNumberList));
+        }
+
+        return lottoList;
+    }
+}
