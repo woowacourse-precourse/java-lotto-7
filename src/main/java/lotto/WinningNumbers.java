b49be8f6c68df3d54c532d0e3746064134ccd4c5
Lee-Minju <mju7513@gmail.com>
2024-10-31 01:59:25 +0900

    WinnigNumbers.java: 입력받은 당첨번호를 검증하는 기능 구현

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,14 +1,63 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningNumbers {
+    private static final int COUNTS_OF_LOTTO = 6;
+    private static final int MAX_VALUE_OF_LOTTO = 45;
+    private static final int MIN_VALUE_OF_LOTTO = 1;
+
     private final List<Integer> numbers = new ArrayList<>();
 
-    public WinningNumbers(List<String> numbers) {
-        for(int i = 0; i < numbers.size(); i++) {
+    public WinningNumbers(List<String> numbers) throws IllegalArgumentException {
+        validate(numbers);
+        for (int i = 0; i < numbers.size(); i++) {
             this.numbers.add(Integer.parseInt(numbers.get(i)));
+        }
+    }
+
+    private void validate(List<String> numbers) {
+        isSizeSix(numbers);
+        isDuplicate(numbers);
+        for (int i = 0; i < numbers.size(); i++) {
+            isBlank(numbers.get(i));
+            isNotNumber(numbers.get(i));
+            isOutOfBound(Integer.parseInt(numbers.get(i)));
+        }
+    }
+
+    private void isDuplicate(List<String> numbers) {
+        HashSet<String> duplicateCheck = new HashSet<>(numbers);
+        if (duplicateCheck.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void isSizeSix(List<String> numbers) {
+        if (numbers.size() != COUNTS_OF_LOTTO) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void isNotNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+        }
+    }
+
+    private void isBlank(String number) {
+        if (number.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+        }
+    }
+
+    private void isOutOfBound(int number) {
+        if (number < MIN_VALUE_OF_LOTTO || number > MAX_VALUE_OF_LOTTO) {
+            throw new IllegalArgumentException("[ERROR] 1 ~ 45 사이의 수를 입력해 주세요.");
         }
     }
 }
