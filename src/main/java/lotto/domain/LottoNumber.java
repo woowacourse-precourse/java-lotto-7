3cb167de964848cb91b1563d7ca00cb16b2832ae
openmpy <suhwan@kakao.com>
2024-10-30 13:53:51 +0900

    feat: LottoNumber 클래스 생성
    - 로또 번호가 1부터 45 사이의 숫자인지 검증

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import static lotto.constants.LottoConstants.MAX_LOTTO_NUMBER;
+import static lotto.constants.LottoConstants.MIN_LOTTO_NUMBER;
+
+public class LottoNumber {
+
+    private final Integer number;
+
+    public LottoNumber(final Integer number) {
+        validateNumber(number);
+
+        this.number = number;
+    }
+
+    private void validateNumber(final Integer number) {
+        if (number < MIN_LOTTO_NUMBER.getValue() || number > MAX_LOTTO_NUMBER.getValue()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+}
