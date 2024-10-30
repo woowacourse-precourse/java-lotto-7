5607a852817cc455010795c5d15c706046d35152
openmpy <suhwan@kakao.com>
2024-10-30 14:00:54 +0900

    refactor: LottoNumber 객체를 사용하도록 Lotto 클래스 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,16 +4,27 @@ import java.util.List;
 
 public class Lotto {
 
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(final List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+
+        this.numbers = convertToLottoNumbers(numbers);
     }
 
-    private void validate(final List<Integer> numbers) {
+    private void validateSize(final List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private List<LottoNumber> convertToLottoNumbers(final List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::new)
+                .toList();
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return numbers;
     }
 }
