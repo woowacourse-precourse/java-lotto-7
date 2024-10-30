cd7d68c2ceba92600e0828748a66e8dff0e56993
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 01:19:01 +0900

    feat(validatePurchaseAmountIsInteger): 구입 금액이 정수인지 확인하는 기능 구현

--- src/main/java/lotto/view/InputHandler.java
+++ src/main/java/lotto/view/InputHandler.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 public class InputHandler {
 
-    public static void validatePurchaseAmountIsThousandUnit(int purchaseAmount){
+    public static int validatePurchaseAmountIsInteger(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.");
+        }
+    }
+
+    public static void validatePurchaseAmountIsThousandUnit(int purchaseAmount) {
         if (purchaseAmount <= 0 || purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }
