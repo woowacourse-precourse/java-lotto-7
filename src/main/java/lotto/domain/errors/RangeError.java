3f21a0a87776276f40afef52fee556828ba6c3a7
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 22:51:22 +0900

    feat: RangeError 클래스 구현

---
+++ src/main/java/lotto/domain/errors/RangeError.java
@@ -0,0 +1,20 @@
+package lotto.domain.errors;
+
+import static lotto.domain.Lotto.LOTTO_COMBINATION_LENGTH;
+import static lotto.domain.LottoNumber.MAXIMUM_LOTTO_NUMBER;
+import static lotto.domain.LottoNumber.MINIMUM_LOTTO_NUMBER;
+
+public enum RangeError {
+    LOTTO_NUMBER_ERROR("[ERROR] 로또 번호는 "+MINIMUM_LOTTO_NUMBER+"부터 "+MAXIMUM_LOTTO_NUMBER+"사이여야 합니다."),
+    LOTTO("[ERROR] 로또의 길이가 "+LOTTO_COMBINATION_LENGTH+"과 일치하지 않습니다");
+
+    private String message;
+
+    RangeError(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
