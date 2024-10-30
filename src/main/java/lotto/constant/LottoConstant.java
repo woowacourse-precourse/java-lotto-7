b23a1b0b28e540adce17a51312a8778ff4ad11b9
EHCTH <lees90896@gmail.com>
2024-10-30 17:42:17 +0900

    feat(LottoConstant): 로또 상수 지정

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -1,4 +1,18 @@
 package lotto.constant;
 
 public enum LottoConstant {
+    MIN_LOTTO_NUMBER_RANGE(1),
+    MAX_LOTTO_NUMBER_RANGE(45),
+    NUMBER_COUNT(6),
+    PURCHASE_AMOUNT(1_000),
+    PURCHASE_LIMIT_AMOUNT(100_000);
+    private final int value;
+
+    LottoConstant(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
