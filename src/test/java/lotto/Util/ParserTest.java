14c6a369501c8251bd2c179617998c2797ee9c85
김호연 <yun1094@naver.com>
2024-10-30 16:38:04 +0900

    불 필요한 기능 구현으로 인한 삭제

--- src/test/java/lotto/Util/ParserTest.java
+++ src/test/java/lotto/Util/ParserTest.java
@@ -7,16 +7,4 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class ParserTest {
-    @Test
-    public void testParserLottoNumbers() {
-        Parser parser = new Parser();
-        String inputValue = "1,2,3,4,5,6";
-
-        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
-        List<Integer> result = parser.parseLottoNumbers(inputValue);
-
-        assertEquals(expected, result, "[ERROR] 숫자로 제대로 변환 되지 않았습니다.");
-
-        System.out.println("변환되었습니다");
-    }
 }
