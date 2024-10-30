0f2863cf7065825f02246cff7285ffff9d243985
imjunghun <dlawndgns9@naver.com>
2024-10-30 22:35:06 +0900

    feat(Payment): 구매 액수를 저장할 Payment 클래스
    - 사용자에게 입력받은 금액을 저장할 Payment
    - 로또 금액에 따른 발행 해야할 카운트를 계산
    - 계산 시 안나눠 떨어지면 예외를 검증

---
+++ src/main/java/lotto/domain/Payment.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+public class Payment {
+
+    private static final int PRICE = 1000;
+
+    private final int payment;
+
+    private Payment(int payment) {
+        Validator.validateDivisibleByPrice(payment);
+        this.payment = payment;
+    }
+
+    public static Payment of(int payment) {
+        return new Payment(payment);
+    }
+
+    public int calculateCount() {
+        return payment / PRICE;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    private static class Validator {
+        private static void validateDivisibleByPrice(int money) {
+            if (isNotDivisibleByPrice(money)) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요");
+            }
+        }
+
+        private static boolean isNotDivisibleByPrice(int money) {
+            return money % PRICE != 0;
+        }
+    }
+}
---
+++ src/test/java/lotto/domain/PaymentTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PaymentTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1_000, 10_000, 100_000})
+    void 객체생성_테스트(int money) {
+        Payment payment = Payment.of(money);
+
+        assertThat(payment).isNotNull();
+        assertThat(payment.calculateCount()).isEqualTo(money / 1000);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 10_500, 100_100})
+    void 천_단위_안떨어지면_예외(int money) {
+        assertThatThrownBy(() -> Payment.of(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
