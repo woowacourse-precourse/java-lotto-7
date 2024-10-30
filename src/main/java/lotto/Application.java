63585134400e5cb8acdcdca84c28b89eafbfe38e
choitaehoon <hugebigdream@gmail.com>
2024-10-30 19:46:10 +0900

    feat: 로또 구입 개수를 출력한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,11 @@ public class Application {
         // TODO: 프로그램 구현
         int lottoPurchasePrice = Integer.parseInt(validate(Console.readLine()));
         int lottoPurchaseQuantity = calculateLottoQuantity(lottoPurchasePrice);
+        print(lottoPurchaseQuantity);
+    }
+
+    public static void print(int lottoPurchaseQuantity) {
+        System.out.println(lottoPurchaseQuantity + "개를 구매했습니다.");
     }
 
     public static int calculateLottoQuantity(int lottoPurchasePrice) {
