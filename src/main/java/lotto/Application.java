e507fcd0c798acb62403eac4b8380ff2024f2266
LeeJaeHoon <dlwogns3413@naver.com>
2024-10-30 17:20:51 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        String lottoPurchaseMoney = inputView.inputLottoPurchaseMoney();
+        System.out.println(lottoPurchaseMoney);
     }
 }
