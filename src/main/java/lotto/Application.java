f7ab6eedff7a43afaa6b487900405679bc75e453
je488 <pje488@naver.com>
2024-10-30 21:40:59 +0900

    feat: 구입 금액만큼 로또를 발행하여 오름차순으로 정렬한 뒤 리턴하는 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Application {
+    public static List<List<Integer>> generateLottoNumbers(int lottoAmount) {
+        List<List<Integer>> userLottoNumbers = new ArrayList<>();
+        for (int i = 0; i < lottoAmount; i++) {
+            List<Integer> userLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            userLottoNumber.sort(Comparator.naturalOrder());
+            userLottoNumbers.add(userLottoNumber);
+        }
+        return userLottoNumbers;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
