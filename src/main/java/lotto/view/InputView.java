8ed7f2225895fdfe9a7fef4ef9da36b6a487169f
binn <anyubin0724@naver.com>
2024-10-30 17:26:46 +0900

    fix: inputView의 반환값 수정
    - 예외처리 후 반환하는 값 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,23 +1,22 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.exceptions.ValidateError.validateBonusNumber;
+import static lotto.exceptions.ValidateError.validatePurchasePrice;
 
 public class InputView {
-    public static String inputPurchasePrice() {
+    public static int inputPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        // TODO: 추후 예외처리 구현 후 수정
-        return readLine();
+        return validatePurchasePrice(readLine());
     }
 
     public static String inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        // TODO: 추후 예외처리 구현 후 수정
         return readLine();
     }
 
-    public static String inputBonusNumber() {
+    public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        // TODO: 추후 예외처리 구현 후 수정
-        return readLine();
+        return validateBonusNumber(readLine());
     }
 }
