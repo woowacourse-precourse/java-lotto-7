5da67732b9b8ae5e29406bed8ae71b4688651e59
woo <ldw28517@naver.com>
2024-10-30 23:13:57 +0900

    test: Cost 생성 테스트
    - 1. 구매 비용은 문자를 포함할 수 없다.
    - 2. 구매 비용은 최소한 천원이어야 한다.
    - 3. 구매 비용은 최대 백만원이다.
    - 4. 구매 비용은 1000으로 나눠져야 한다.
    - 5. 정상 테스트

---
+++ src/test/java/lotto/domain/CostTest.java
@@ -0,0 +1,63 @@
+package lotto.domain;
+
+import lotto.error.CostErrorMessage;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CostTest {
+
+    @Test
+    void 구매비용은_문자를_포함할_수_없다() {
+        //given
+        String cost = "1000원";
+
+        //when & then
+        assertThatThrownBy(() -> new Cost(cost))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CostErrorMessage.IS_NOT_NUMBER.getMessage());
+    }
+
+    @Test
+    void 구매비용은_최소한_천원이어야한다() {
+        // given
+        String cost = "10";
+
+        //when & then
+        assertThatThrownBy(() -> new Cost(cost))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CostErrorMessage.INSUFFICIENT_MONEY.getMessage());
+    }
+
+    @Test
+    void 구매비용은_최대_백만원이다() {
+        // given
+        String cost = "2000000";
+
+        //when & then
+        assertThatThrownBy(() -> new Cost(cost))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CostErrorMessage.TOO_MANY_MONEY.getMessage());
+    }
+
+    @Test
+    void 구매비용은_1000으로_나눠져야한다() {
+        //given
+        String cost = "10001";
+
+        //when & then
+        assertThatThrownBy(() -> new Cost(cost))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CostErrorMessage.CAN_NOT_DIVIDED.getMessage());
+    }
+
+    @Test
+    void 정상_테스트() {
+        //given
+        String cost = "20000";
+
+        //when & then
+        assertThatCode(() -> new Cost(cost))
+                .doesNotThrowAnyException();
+    }
+}
