800621517c409a60c292e66b3232679a1bd518e0
Zrfv <youngoh07015@gmail.com>
2024-10-30 19:10:38 +0900

    refactor: 입력 받는 메서드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.view.validator.InputValidator;
 
 public class InputView {
 
     private static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
 
     public String inputPurchasePrice() {
-        System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
-        return Console.readLine();
+        printMessage(INPUT_PURCHASE_PRICE_MESSAGE);
+        String input = Console.readLine();
+        InputValidator.validateNull(input);
+        InputValidator.validateInteger(input);
+        return input;
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
     }
 
     public void close() {
