9cb096cc61ec46dc24b5288636410dbbd226f680
Seo Jinmann <sjmmics@gmail.com>
2024-10-30 16:24:37 +0900

    feat : ($PrizeGrade) 로또 당첨 등급 클래스 추가
    로또 당첨 등급에 따른 당첨금, 조건을 다루는 클래스이며 조건을 넣으면 당첨등급을 반환하는 기능과 당첨 조건을 출력하는 기능을 담당한다.

---
+++ src/main/java/lotto/model/PrizeGrade.java
@@ -0,0 +1,51 @@
+package lotto.model;
+
+import java.text.DecimalFormat;
+
+public enum PrizeGrade {
+    THREE_MATCH("3개 일치", 5000, 3, false),
+    FOUR_MATCH("4개 일치", 50000, 4, false),
+    FIVE_MATCH("5개 일치", 1500000, 5, false),
+    FIVE_AND_BONUS_MATCH("5개 일치, 보너스 볼 일치", 30000000, 5, true),
+    SIX_MATCH("6개 일치",2000000000, 6, false);
+    
+    private static final DecimalFormat COMMA_IN_THREE = new DecimalFormat("###,###");
+    
+    private final String condition;
+    
+    private final int money;
+    
+    private final int matchCount;
+    
+    private final boolean matchBonus;
+    
+    PrizeGrade(String condition, int money, int matchCount, boolean matchBonus) {
+        this.condition = condition;
+        this.money = money;
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+    }
+    
+    
+    public static PrizeGrade get(int matchCountInput, boolean isBonusMatch) {
+        PrizeGrade gradeToReturn = null;
+        for (PrizeGrade grade : PrizeGrade.values()) {
+            if (matchCountInput == grade.matchCount && grade.matchCount != 5) {
+                gradeToReturn = grade;
+            }
+            if (matchCountInput == grade.matchCount && grade.matchBonus == isBonusMatch) {
+                gradeToReturn = grade;
+            }
+        }
+        return gradeToReturn;
+    }
+    
+    @Override
+    public String toString() {
+        return condition + " (" + COMMA_IN_THREE.format(money) + "원)";
+    }
+    
+    public long getMoney() {
+        return money;
+    }
+}
