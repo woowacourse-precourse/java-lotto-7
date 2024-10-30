3cf01a3afe50f4b2fdc1d005ac03d1bf2018b560
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-10-30 21:37:01 +0900

    feat: 사용자의 입력을 요청하는 메시지 출력과 문자열을 입력 받는 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberOfNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumberOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void printRequestingMoneyInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    private int getMoneyInput() {
+        String userInput = Console.readLine();
+        return Integer.parseInt(userInput);
+    }
+
+
 }
