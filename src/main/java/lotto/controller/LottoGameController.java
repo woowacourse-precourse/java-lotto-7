7ec8d77535990f36875853e588393d25befa63cf
StoneCAU <seokwoo1999@naver.com>
2024-10-31 00:37:47 +0900

    feat(LottoGameController, OutputView): 로또 구매 개수 출력
    - LottoGameController: getLottoQuantity 메서드 추가
    - OutputView: printPurchaseMessage 메서드 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,15 +1,31 @@
 package lotto.controller;
 
+import static lotto.constant.LottoGameRule.LOTTO_COST;
+import static lotto.view.OutputView.printPurchaseMessage;
+
+import java.util.List;
 import lotto.validator.PurchaseAmountValidator;
 import lotto.view.InputView;
 
 public class LottoGameController {
     public void run() {
-        final int purchaseAmount = getPurchaseAmount();
+        final int lottoQuantity = getLottoQuantity();
+        final List<Integer> winningNumbers;
     }
 
     private static int getPurchaseAmount() {
         String input = InputView.inputPurchaseAmount();
         return PurchaseAmountValidator.validate(input);
+    }
+
+    private static int getLottoQuantity() {
+        int lottoQuantity =  getPurchaseAmount() / LOTTO_COST.getValue();
+        printPurchaseMessage(lottoQuantity);
+
+        return lottoQuantity;
+    }
+
+    private static void getWinningNumbers() {
+        String input = InputView.inputWinningNumbers();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,13 @@
 package lotto.view;
 
+import static java.lang.String.format;
+
 public class OutputView {
     private final static String INFORM_PURCHASE_LOTTO_MESSAGE = "%d개를 구매했습니다.";
     private final static String WINNING_STATISTICS_TITLE = "당첨 통계";
     private final static String SEPARATOR = "---";
+
+    public static void printPurchaseMessage(int lottoQuantity) {
+        System.out.println(format(INFORM_PURCHASE_LOTTO_MESSAGE, lottoQuantity));
+    }
 }
