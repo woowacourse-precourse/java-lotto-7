a687b149ff312efc1a5ec7ec4262d8662e0e97a1
ratatuwee <pkqwaszx@gmail.com>
2024-10-30 15:56:54 +0900

    test(user input) : 사용자 입력 테스트 추가
    사용자 입력 기능을 개발전 요구 사항에 맞춰 안내 메시지 및 입력값을 확인하는 테스트를 추가하였습니다.

---
+++ src/test/java/lotto/view/UserInputTest.java
@@ -0,0 +1,94 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserInputTest {
+    @AfterEach
+    void closeConsole() {
+        Console.close();
+    }
+
+    private ByteArrayOutputStream getSystemSetOut() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        return outputStream;
+    }
+
+
+    @DisplayName("구입금액 입력 안내 메시지 확인")
+    @Test
+    void checkPurchaseAmountInputPrompt() {
+        ByteArrayOutputStream outputStream = getSystemSetOut();
+        UserInput userInput = new UserInput();
+        String successMessage = "구입금액을 입력해 주세요.";
+
+        userInput.promptPrtchaseAmountInput();
+
+        assertEquals(successMessage + "\n", outputStream.toString());
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입금액 입력 함수 입력값 확인")
+    @ValueSource(strings = {"7000", "adad", "1234"})
+    void checkInputPurchaseAmount(String test) {
+        System.setIn(new ByteArrayInputStream(test.getBytes()));
+        UserInput userInput = new UserInput();
+
+        assertEquals(test, userInput.promptPrtchaseAmountInput());
+    }
+
+    @DisplayName("당첨 번호 입력 안내 메시지 확인")
+    @Test
+    void checkWinningNumberInputPrompt() {
+        ByteArrayOutputStream outputStream = getSystemSetOut();
+        UserInput userInput = new UserInput();
+        String successMessage = "당첨 번호를 입력해 주세요.";
+
+        userInput.promptWinningNumberInput();
+
+        assertEquals(successMessage + "\n", outputStream.toString());
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호 입력 함수 입력값 확인")
+    @ValueSource(strings = {"1,2,3,4,5,6", "adad", "1234"})
+    void checkInputWinningNumber(String test) {
+        System.setIn(new ByteArrayInputStream(test.getBytes()));
+        UserInput userInput = new UserInput();
+
+        assertEquals(test, userInput.promptWinningNumberInput());
+    }
+
+    @DisplayName("보너스 번호 입력 안내 메시지 확인")
+    @Test
+    void checkBonusNumberInputPrompt() {
+        ByteArrayOutputStream outputStream = getSystemSetOut();
+        UserInput userInput = new UserInput();
+        String successMessage = "보너스 번호를 입력해 주세요.";
+
+        userInput.promptBonusNumberInput();
+
+        assertEquals(successMessage + "\n", outputStream.toString());
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 입력 함수 입력값 확인")
+    @ValueSource(strings = {"7", "adad", "1234"})
+    void checkInputBonusNumber(String test) {
+        System.setIn(new ByteArrayInputStream(test.getBytes()));
+        UserInput userInput = new UserInput();
+
+        assertEquals(test, userInput.promptBonusNumberInput());
+    }
+}
