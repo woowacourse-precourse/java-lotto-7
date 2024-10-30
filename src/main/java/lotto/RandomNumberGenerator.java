59342ae0902a7a0766a5da29b41d323cb859cc93
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-10-30 22:19:23 +0900

    refactor: 클래스 명 수정

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
 
-public class generateNumber {
+public class RandomNumberGenerator {
     public static List<Integer> genreateNumbers(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Arrays.sort(new List[]{numbers});
--- src/test/java/lotto/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -3,16 +3,15 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-class generateNumberTest {
+class RandomNumberGeneratorTest {
     @DisplayName("랜덤숫자 6개 생성")
     @Test
     void 랜덤_숫자6개_생성(){
-        List<Integer> numbers = generateNumber.genreateNumbers();
+        List<Integer> numbers = RandomNumberGenerator.genreateNumbers();
 
         assertThat(numbers.size()).isEqualTo(6);
     }
