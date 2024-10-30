58ef3bc8f5f5920d3f55fe19018a474e822c2312
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-30 22:12:46 +0900

    refactor: validate 매서드 추출

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,12 +3,9 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static lotto.domain.LottoNumber.MAXIMUM_LOTTO_NUMBER;
-import static lotto.domain.LottoNumber.MINIMUM_LOTTO_NUMBER;
-
 public class Lotto {
 
-    private static final int LOTTO_COMBINATION_LENGTH = 6;
+    public static final int LOTTO_COMBINATION_LENGTH = 6;
 
     private final List<LottoNumber> numbers;
 
@@ -19,7 +16,7 @@ public class Lotto {
                 .collect(Collectors.toList());
     }
 
-    private <T> void validate(List<T> numbers) {
+    private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_COMBINATION_LENGTH) {
             throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %개여야 합니다.", LOTTO_COMBINATION_LENGTH));
         }
