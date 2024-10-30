163f824a95da9200b20ad287804e819a21b0d240
unh6unh6 <ruddbs3308@naver.com>
2024-10-30 18:53:51 +0900

    style: 테스트 메소드명 변경

--- src/test/java/lotto/view/PurchaseAmountInputViewTest.java
+++ src/test/java/lotto/view/PurchaseAmountInputViewTest.java
@@ -16,7 +16,7 @@ public class PurchaseAmountInputViewTest {
     }
 
     @Test
-    void 구입금액이_숫자로만_이루어진_문자열이_아니라면_예외() {
+    void 구입금액이_숫자로만_이루어진_문자열이_아니라면_예외가_발생한다() {
         String input = "5000원";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
