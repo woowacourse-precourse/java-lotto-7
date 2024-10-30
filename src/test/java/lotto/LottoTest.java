58b68018a09cd44b12d98de71a2820641fc3d295
Seonjeongg <sj030205@naver.com>
2024-10-30 15:13:14 +0900

    test: 로또 발행 개수 파악

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.Model.Lotto;
+import lotto.Service.LottoService;
 import lotto.View.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+    LottoService lottoService = new LottoService();
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -24,6 +26,12 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+    @Test
+    void 로또_발행_개수(){
+        String input="6000";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        int result=lottoService.LottoNumber(InputView.input_purchaseAmount());
+        assertEquals(result,6);
+    }
 
 }
