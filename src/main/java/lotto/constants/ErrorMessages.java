7949d0cae60b74713647d16c345935880e0b7f60
yush <yush2202@gmail.com>
2024-10-30 21:09:48 +0900

    feat($ ErrorMessages): 에러 메시지 구현

---
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -0,0 +1,23 @@
+package lotto.constants;
+
+public enum ErrorMessages {
+    ERROR_PREFIX("[ERROR] "),
+    INVALID_USER_INPUT("입력은 공백이거나 없을 수 없습니다."),
+    INVALID_DIGIT("자연수가 아닙니다."),
+    INVALID_LOTTO_NUMBER_RANGE("로또 숫자는 1이상, 45미만의 자연수만 가능합니다."),
+    INVALID_LOTTO_COUNT("로또 숫자는 6개만 가능합니다."),
+    INVALID_BONUS_COUNT("보너스 숫자는 1개만 가능합니다."),
+    INVALID_LOTTO_DUPLICATED("로또 숫자는 중복될 수 없습니다."),
+    INVALID_MONEY_RANGE("구입 금액은 0원 초과, 10만원 이하만 가능합니다."),
+    INVALID_MONEY_UNIT("구입 금액은 천원 단위만 가능합니다.");
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX.message + message;
+    }
+}
