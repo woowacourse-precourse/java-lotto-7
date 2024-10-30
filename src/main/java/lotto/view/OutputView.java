47fcb54044f0917f06b9cd86863013eb31bc88b9
oznchex <114240463+oznchex@users.noreply.github.com>
2024-10-30 22:39:51 +0900

    feat(view): 기본 출력 형태 구현
    - 변수 값이 들어가지 않는 기본 입력 요청 메시지 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+public class OutputView {
+    public void printRequirePurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printRequireWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printRequireBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printWinningStatistics() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+}
