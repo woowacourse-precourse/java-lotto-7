92f7ca202cc5e7472ce2f7f2614a3bdd014a3421
EunSeop <doosan1112@naver.com>
2024-10-30 22:22:06 +0900

    feat: 당첨 번호를 입력하는 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getPurchasePrice(){
+        return Console.readLine();
+    }
+
+    public String getWinningNumbers(){
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputView {
+    public void printPurchaseGuide(){
+        System.out.println("로또를 구입할 금액을 입력하세요.");
+    }
+
+    public void printWinningNumbersGuide(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+}
