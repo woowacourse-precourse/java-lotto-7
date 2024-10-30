d478fa7019f4dd924d28d38c5a007b299914e843
Firedrago95 <yonghwa95@naver.com>
2024-10-30 10:21:26 +0900

    Refactor(LottoService): 서비스 객체의 도입

---
+++ src/main/java/lotto/model/LottoService.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Map;
+import lotto.util.RandomNumGenerator;
+
+public class LottoService {
+
+    private Money money;
+    private LottoTicket lottoTicket;
+    private WinningNumbers winningNumbers;
+
+    public void createMoney(int budget) {
+        money = new Money(budget);
+    }
+
+    public int getLottoCount() {
+        return money.getTicketCount();
+    }
+
+    public List<String> createLottoTicket() {
+        int ticketCount = money.getTicketCount();
+        lottoTicket = new LottoTicket(ticketCount, new RandomNumGenerator());
+        return lottoTicket.getPrintFormNumbers();
+    }
+
+    public void createWinningNumbers(List<Integer> numbers, int bonusNumber) {
+        winningNumbers = new WinningNumbers(numbers, bonusNumber);
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return lottoTicket.getResult(
+            winningNumbers.getWinningNumbers(),
+            winningNumbers.getBonusNumber()
+        );
+    }
+
+    public String getRateOfReturn() {
+        return money.getRateOfReturn(getResult());
+    }
+}
