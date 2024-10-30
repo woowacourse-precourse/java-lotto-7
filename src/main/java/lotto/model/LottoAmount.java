2969e1c997d968b30825a901f1252b0e3bd45cf5
junho767 <junho767@naver.com>
2024-10-30 22:31:46 +0900

    feat: 구입금액에 대한 검증 및 예외 처리 추가

--- src/main/java/lotto/model/LottoAmount.java
+++ src/main/java/lotto/model/LottoAmount.java
@@ -1,14 +1,31 @@
 package lotto.model;
 
+import lotto.Message.ErrorMessage;
+
 public class LottoAmount {
     private static final int THOUSAND_UNIT = 1000;
     private final int amount;
 
     public LottoAmount(String input) {
-        this.amount = Integer.parseInt(input);
+        validateNumber(input);
+        int amount = Integer.parseInt(input);
+        validateAmount(amount);
+        this.amount = amount;
     }
 
     public int getAmount() {
         return amount / THOUSAND_UNIT;
+    }
+
+    private void validateNumber(String input) {
+        if (!input.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.toString());
+        }
+    }
+
+    private void validateAmount(int amount) {
+        if ((amount % THOUSAND_UNIT) != 0 || amount < 1000) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_THOUSAND_UNIT.toString());
+        }
     }
 }
