faeef78db74aea0ed0d3fe8c35b1fbfc1cd3f4eb
sik9252 <sik9252@naver.com>
2024-10-31 01:04:03 +0900

    refactor: 내부에서만 사용하는 메서드들은 private로 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -23,25 +23,25 @@ public class LottoController {
         showResult();
     }
 
-    public void buy() {
+    private void buy() {
         inputPurchaseAmount();
         publishLotto();
         printPublishedLotto();
     }
 
-    public void assign() {
+    private void assign() {
         assignWinningNumbers();
         assignBonusNumber();
     }
 
-    public void showResult() {
+    private void showResult() {
         calculateWinnings();
         calculateTotalRateOfReturn();
         printWinningStatistics();
     }
 
     // 로또 구입 금액을 입력 받는다
-    public void inputPurchaseAmount() {
+    private void inputPurchaseAmount() {
         while (true) {
             try {
                 int amount = inputView.getPurchaseAmount();
@@ -55,37 +55,37 @@ public class LottoController {
     }
 
     // 구매한 로또 수 만큼의 로또를 발행한다
-    public void publishLotto() {
+    private void publishLotto() {
         publishLotteries = new PublishLotteries(purchaseCount);
     }
 
     // 발행된 로또 번호를 출력한다
-    public void printPublishedLotto() {
+    private void printPublishedLotto() {
         outputView.printPublishedLotteries(publishLotteries.get());
     }
 
     // 당첨 번호를 입력 받는다
-    public void assignWinningNumbers() {
+    private void assignWinningNumbers() {
 
     }
 
     // 보너스 번호를 입력 받는다
-    public void assignBonusNumber() {
+    private void assignBonusNumber() {
 
     }
 
     // 번호를 비교하여 당첨 내역을 계산한다
-    public void calculateWinnings() {
+    private void calculateWinnings() {
 
     }
 
     // 총 수익률을 계산한다
-    public void calculateTotalRateOfReturn() {
+    private void calculateTotalRateOfReturn() {
 
     }
 
     // 당첨 통계를 출력한다
-    public void printWinningStatistics() {
+    private void printWinningStatistics() {
 
     }
 
