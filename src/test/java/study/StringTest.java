7b479dcfc9a95123c75650e1c0bfef56eba2b0e3
choign3123 <choign3123@naver.com>
2024-10-30 17:30:14 +0900

    Test(StringTest): 요구사항1 테스트 코드 작성

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,41 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("콤마가 포함된 문자열을 split()")
+    public void splitComma() {
+        // given
+        String src = "1,2";
+
+        // when
+        String[] result = src.split(",");
+
+        // then
+        assertThat(result.length)
+                .isEqualTo(2);
+        assertThat(result)
+                .contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("콤마가 없는 문자열을 split()")
+    public void splitComma2() {
+        // given
+        String src = "1";
+
+        // when
+        String[] result = src.split(",");
+
+        // then
+        assertThat(result.length)
+                .isEqualTo(1);
+        assertThat(result)
+                .containsExactly("1");
+    }
+}
