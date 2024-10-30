ac7ed412e4b06b7f89b22efce68e47fb7f1a6794
CheChe903 <cheche903@naver.com>
2024-10-30 16:40:08 +0900

    feat(LottoGeneratorTest): 단위 테스트 구현
    해당 객체에 맞는 단위 테스트를 구현합니다.

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    @Test
+    void 로또_생성시_로또는_NULL이_아니다() {
+        //given
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        //when
+        Lotto lotto = lottoGenerator.generate();
+
+        //then
+        assertNotNull(lotto);
+    }
+}
