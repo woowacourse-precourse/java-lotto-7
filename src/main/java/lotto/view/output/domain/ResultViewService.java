2c6df4e699ae2548bd1b5a89bfb1f5e853415d85
EHCTH <lees90896@gmail.com>
2024-10-30 18:40:57 +0900

    feat(ReultViewService): 인터페이스 기능 구현 * feat: +viewByInsertMoney(Money): void * feat: +viewByLottoTickets(LottoTickets): void * feat: +viewByWinningLotto(WinningLotto): void * feat: +viewByBonusLotto(BonusNumber): void * feat: +viewByWinningStatistic(Benefit, Money): void

---
+++ src/main/java/lotto/view/output/domain/ResultViewService.java
@@ -0,0 +1,19 @@
+package lotto.view.output.domain;
+
+import lotto.buyer.domain.Money;
+import lotto.lotto.winning.domain.Benefit;
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.domain.LottoTickets;
+import lotto.lotto.winning.domain.WinningLotto;
+
+public interface ResultViewService {
+    void viewByInsertMoney(Money money);
+
+    void viewByLottoTickets(LottoTickets lottoTickets);
+
+    void viewByWinningLotto(WinningLotto winningLotto);
+
+    void viewByBonusLotto(BonusNumber bonusLotto);
+
+    void viewByWinningStatistic(Benefit benefit, Money purchaseMoney);
+}
