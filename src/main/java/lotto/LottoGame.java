dd10f9502bba8c19a411fadac365dd46c8cd380f
choign3123 <choign3123@naver.com>
2024-10-30 17:19:29 +0900

    feat(LottoGame): 구입금액만큼 로또 발행

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,15 +2,25 @@ package lotto;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class LottoGame {
 
-    public void run() {
-        purchaseLotto();
+    private LottoStore lottoStore;
+
+    public LottoGame() {
+        this.lottoStore = new LottoStore();
     }
 
-    private void purchaseLotto() {
+    public void run() {
+        List<Lotto> lottos = purchaseLotto();
+    }
+
+    private List<Lotto> purchaseLotto() {
         System.out.println("구입금액을 입력해 주세요.");
         String rawMoney = Console.readLine();
+
+        int money = Integer.parseInt(rawMoney);
+        return lottoStore.purchase(money);
     }
 }
