3a6dbfca3d89ab0e6b6e493ad7f43e3c2992c42a
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-30 17:48:07 +0900

    feat: 구입 금액을 입력받는 기능 추가

--- src/main/java/lotto/handler/PrintHandler.java
+++ src/main/java/lotto/handler/PrintHandler.java
@@ -1,4 +1,9 @@
 package lotto.handler;
 
 public class PrintHandler {
+    private static final String PRINT_BUY_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public void PrintBuyMoneyAmount () {
+        System.out.println(PRINT_BUY_MONEY_AMOUNT);
+    }
 }
