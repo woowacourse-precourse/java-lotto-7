eef908585b783dc237faa069f790ee5c979724b2
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-10-30 21:45:27 +0900

    feat: 구입 금액에 해당하는 만큼 로또를 발행하는 기능

---
+++ src/main/java/lotto/BuyLotto.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyLotto {
+    List<List<Integer>> lottoList; // 여러 개의 로또 번호를 저장할 리스트
+
+    public BuyLotto(int numberOfLotto) {
+        lottoList = new ArrayList<>();
+        buyLotto(numberOfLotto);
+    }
+
+    private void buyLotto(int numberOfLotto) {
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> singleLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6); // 중복되지 않는 6개의 번호 생성
+            lottoList.add(singleLotto);
+        }
+    }
+
+}
