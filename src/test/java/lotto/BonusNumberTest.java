fb1c15fdf5b2ffeb41cf7a873310b61c0dfc5f01
msk226 <kminseok5167@gmail.com>
2024-10-30 14:23:01 +0900

    test(model): 정상 테스트 케이스 추가

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -6,10 +6,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import lotto.model.BonusNumber;
 import lotto.model.Lotto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BonusNumberTest {
 
+
+    @Test
+    void 정상_케이스(){
+        BonusNumber bonusNumber = new BonusNumber(10);
+
+        Assertions.assertNotNull(bonusNumber);
+        Assertions.assertEquals(10, bonusNumber.getBonusNumber());
+    }
     @Test
     void 보너스_번호는_1과_45_사이의_값을_가져야_한다_1() {
         assertThatThrownBy(() -> new BonusNumber(100))
