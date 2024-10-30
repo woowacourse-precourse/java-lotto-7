69660532efadbebfedb5c4a1d48df989afa264bb
Dev-LHY <dev.leehy@gmail.com>
2024-10-31 05:57:42 +0900

    feat(exceptionMessage): 예외 메세지 추가
    - enum 사용하여 예외 메세지 관리

---
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -0,0 +1,22 @@
+package lotto.exception;
+
+public enum ExceptionMessage {
+    EXCEPTION_PREFIX("[ERROR] "),
+    INVALID_NUMBER_FORMAT("숫자만 입력 가능합니다."),
+    INVALID_MONEY_INPUT("최소 구입금액은 1,000원이며 1,000원 단위로만 입력 가능합니다."),
+    INVALID_LOTTO_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
+    INVALID_WINNING_NUMBER("당첨 번호는 1~45 사이의 숫자만 입력 가능합니다."),
+    DUPLICATE_WINNING_NUMBER("중복 당첨 번호는 입력이 불가능합니다."),
+    INVALID_BONUS_NUMBER("보너스 번호는 1~45 사이의 숫자만 입력 가능합니다."),
+    DUPLICATE_BONUS_NUMBER("중복 보너스 번호는 입력이 불가능합니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return EXCEPTION_PREFIX.message + message + System.lineSeparator();
+    }
+}
