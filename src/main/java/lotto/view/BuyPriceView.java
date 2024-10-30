adabd51d8ea1f13e72198bce30254d4b1db1a6ec
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-31 08:25:29 +0900

    feat : 로또 구매부분의 출력을 담당하는 View 개발
    - 구매 멘트 출력하는 부분을 먼저 개발함

---
+++ src/main/java/lotto/view/BuyPriceView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import lotto.model.BuyPrice;
+
+public class BuyPriceView {
+
+    public void printPriceInputView() {
+        System.out.println("구입금액을 입력해 주세요");
+    }
+
+
+}
