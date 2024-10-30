c265e582d8bfdbb3df846e16ed3011dff9ea6af9
Sumin <new.sumyang@gmail.com>
2024-10-30 16:30:26 +0900

    refactor(domain): 보너스 번호 예외 추가

--- src/main/java/lotto/domain/WinNumbers.java
+++ src/main/java/lotto/domain/WinNumbers.java
@@ -23,10 +23,15 @@ public record WinNumbers(
     }
 
     public WinNumbers bonusNumberFrom(String bonusNumber) {
+        int convertBonusNumber;
         try {
-            return new WinNumbers(primaryWinNumbers, Integer.parseInt(bonusNumber));
+            convertBonusNumber = Integer.parseInt(bonusNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
         }
+        if (primaryWinNumbers.contains(convertBonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 이미 존재하는 로또 번호 입니다.");
+        }
+        return new WinNumbers(primaryWinNumbers, convertBonusNumber);
     }
 }
