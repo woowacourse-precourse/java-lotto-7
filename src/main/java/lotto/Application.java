016bcba016dc4bc21c0ae23ea1424e4a489c6bb6
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-10-31 01:41:58 +0900

    feat(Application.java) Add instance StartLogic class and start

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.StartLogic;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StartLogic startLogic = new StartLogic();
+        startLogic.start();
+
     }
 }
