6324bd6fc017da6df254b9169b0350df7a39bb7a
ChoiDongJoon <adong0808@naver.com>
2024-10-31 00:52:06 +0900

    refactor(lotto): view로 명칭 변경
    view로 명칭 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,9 @@
-package lotto.controller;
+package lotto.view;
 
 import java.util.ArrayList;
-import java.util.List;
 import lotto.Lotto;
 
-public class OutputController {
+public class OutputView {
 
     public void printGeneratedLottoTickets(int lottoAmount, ArrayList<Lotto> lottoTickets) {
         System.out.println(lottoAmount + "개를 구매했습니다.");
