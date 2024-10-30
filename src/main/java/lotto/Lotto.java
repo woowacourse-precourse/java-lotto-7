9193b55ed8443d8c313dca2bc63db37bdabdfb97
SeongbinPark <hyucse22@gmail.com>
2024-10-30 21:22:35 +0900

    feat(Lotto): 로또 번호가 범위 밖인 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,6 +20,11 @@ public class Lotto {
         if (uniqueNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     public List<Integer> getNumbers() {
