b5cd5f9943e2e77af1769089efa259a383ad81f9
Hyso <hsk7953@gmail.com>
2024-10-31 00:05:53 +0900

    feat(AmountValidator) : 로또 구입 금액 단위 검증 기능 추가 - 검증 메서드 리팩토링   - validate() 메서드로 통합

--- src/main/java/lotto/validator/AmountValidator.java
+++ src/main/java/lotto/validator/AmountValidator.java
@@ -3,11 +3,21 @@ package lotto.validator;
 import lotto.enums.Exceptions;
 
 public class AmountValidator {
+    public void validate(String input) {
+        validateNull(input);
+        validateUnit(input);
+    }
 
-    public void validateNull(String input){
-        if(input.isBlank()){
+    private void validateNull(String input) {
+        if (input.isBlank()) {
             throw new IllegalArgumentException(Exceptions.NOT_BLANK.getMessage());
         }
     }
 
+    private void validateUnit(String input) {
+        int price = Integer.parseInt(input);
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(Exceptions.INVALID_MONEY_UNIT.getMessage());
+        }
+    }
 }
