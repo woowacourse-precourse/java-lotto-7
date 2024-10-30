987a8d0499c6e84d4e5812d0f1bad1c066120186
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-31 00:01:40 +0900

    feat: 예외가 발생한 경우 에러 출력 후, 입력을 다시 받기

--- src/main/java/lotto/controller/WinningNumbersController.java
+++ src/main/java/lotto/controller/WinningNumbersController.java
@@ -2,11 +2,17 @@ package lotto.controller;
 
 import lotto.validator.WinningNumbersValidator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class WinningNumbersController {
 
     public void getWinningNumbers() {
-        String input = InputView.inputWinningNumbers();
-        WinningNumbersValidator.validate(input);
+        try {
+            String input = InputView.inputWinningNumbers();
+            WinningNumbersValidator.validate(input);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            getWinningNumbers();
+        }
     }
 }
