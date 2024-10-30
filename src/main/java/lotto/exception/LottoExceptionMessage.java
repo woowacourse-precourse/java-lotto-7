a7ff3a12bc2e1b39656caf19fd26477967cdaf36
kungbi <kipper12335@gmail.com>
2024-10-31 03:57:20 +0900

    refactor(exception): 에러 메시지에 상수를 사용하여 포맷팅 적용

--- src/main/java/lotto/exception/LottoExceptionMessage.java
+++ src/main/java/lotto/exception/LottoExceptionMessage.java
@@ -1,10 +1,20 @@
 package lotto.exception;
 
+import static java.lang.String.format;
+
+import lotto.configuration.LottoConfiguration;
+
 public enum LottoExceptionMessage implements ExceptionMessage {
-    INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
     DUPLICATE_NUMBERS("로또 번호는 중복될 수 없습니다."),
-    NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45까지의 숫자여야 합니다."),
-    NULL_OR_EMPTY_NUMBERS("로또 번호는 비어있거나 null일 수 없습니다.");
+    NULL_OR_EMPTY_NUMBERS("로또 번호는 비어있거나 null일 수 없습니다."),
+
+    INVALID_NUMBER_COUNT(
+            format("로또 번호는 %d개여야 합니다.",
+                    LottoConfiguration.LOTTO_NUMBER_COUNT.getValue())),
+    NUMBER_OUT_OF_RANGE(
+            format("로또 번호는 %d에서 %d 사이의 숫자여야 합니다.",
+                    LottoConfiguration.LOTTO_MIN_NUMBER.getValue(),
+                    LottoConfiguration.LOTTO_MAX_NUMBER.getValue()));
 
     private final String message;
 
--- src/main/java/lotto/exception/WinnerNumberExceptionMessage.java
+++ src/main/java/lotto/exception/WinnerNumberExceptionMessage.java
@@ -1,8 +1,16 @@
 package lotto.exception;
 
+import static java.lang.String.format;
+
+import lotto.configuration.LottoConfiguration;
+
 public enum WinnerNumberExceptionMessage implements ExceptionMessage {
-    BONUS_NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45까지의 숫자여야 합니다."),
-    BONUS_NUMBER_DUPLICATE("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    BONUS_NUMBER_DUPLICATE("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+
+    BONUS_NUMBER_OUT_OF_RANGE(
+            format("보너스 번호는 %d에서 %d 사이의 숫자여야 합니다.",
+                    LottoConfiguration.LOTTO_MIN_NUMBER.getValue(),
+                    LottoConfiguration.LOTTO_MAX_NUMBER.getValue()));
 
     private final String message;
 
