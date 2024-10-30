fbac9dd844397ad33fecaa0c5b4ea44a84398bb0
bowook <mbosx316@naver.com>
2024-10-30 23:04:39 +0900

    feat(Money) : Money 도메인 추가
    - Money 객체

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,2 +1,13 @@
-package lotto.domain;public class Money {
+package lotto.domain;
+
+public class Money {
+    private int money;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
