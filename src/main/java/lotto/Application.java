a5c895289dcc093d5fcbacf082ae7a7edf3b4fb0
msk226 <kminseok5167@gmail.com>
2024-10-30 14:42:17 +0900

    fix(main): 불필요한 주석 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,6 @@ import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         LottoController lottoController = new LottoController(new LottoService());
         lottoController.startLottoGame();
     }
