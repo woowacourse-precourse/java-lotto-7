28c5ee071e505f3678000fa1db615af0bf440426
Sinorang <sinholang@naver.com>
2024-10-30 23:28:50 +0900

    test: 로또 구입 금액 입력값 빈 값 예외 테스트 추가
    - 입력값 null 또는 공백 예외 테스트 코드 구현

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,6 +1,10 @@
 package lotto.service;
 
+import lotto.constants.ErrorMessageConstants;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -34,5 +38,14 @@ class LottoServiceTest {
         assertThatThrownBy(() -> lottoService.calculateLottoCount(invalidPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입 금액은 양의 정수로 입력해야 합니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "    "})
+    void 로또_구입_금액_입력값_null_또는_공백_예외_테스트(String invalidInput) {
+        assertThatThrownBy(() -> lottoService.calculateLottoCount(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.EMPTY_PURCHASE_AMOUNT);
     }
 }
