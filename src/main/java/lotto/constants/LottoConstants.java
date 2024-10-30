ce4eeb6b3575a8ae9a46c7890380437ed287fb18
openmpy <suhwan@kakao.com>
2024-10-30 14:02:42 +0900

    refactor: 로또 번호 사이즈 validation 수정

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -4,6 +4,7 @@ public enum LottoConstants {
 
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
+    LOTTO_NUMBER_SIZE(6),
     ;
 
     private final Integer value;
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,21 @@
 package lotto.domain;
 
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_SIZE;
+
 import java.util.List;
 
 public class Lotto {
 
     private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(final List<Integer> numbers) {
         validateSize(numbers);
 
         this.numbers = convertToLottoNumbers(numbers);
     }
 
     private void validateSize(final List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE.getValue()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
