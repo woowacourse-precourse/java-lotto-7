ecdc1bc5e75291d1b4d3adb2f1db870239d8942b
Spinichi <spinichi@naver.com>
2024-10-30 16:08:16 +0900

    feat: 사용자 금액 입력 기능 메서드 분리
    메서드를 더 세분하여 가독성을 높이고 예외처리 메시지를 다르게 하여 어떤 오류인지 더 명확하게 파악하게 함

--- src/main/java/lotto/UserAmountInput.java
+++ src/main/java/lotto/UserAmountInput.java
@@ -3,36 +3,44 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserAmountInput {
+    private String amountInput;
     private int amount;
     private int lottoTicketCount;
 
     public void userAmountInput() {
+        while (true) {
+            try {
+                userInput();
+                parseAmount();
+                validate();
+                setLottoTicketCount();
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 입력이 잘못되었습니다. 정수를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // 예외 메시지 출력 후 다시 입력 요청
+            }
+        }
+    }
 
+    private void userInput() {
         System.out.println("구입금액을 입력해 주세요.");
-        String amountInput = Console.readLine();
-
-        processUserAmount(amountInput);
+        amountInput = Console.readLine();
     }
 
-    private void processUserAmount(String amountInput) {
-        try {
-
-            amount = parseAmount(amountInput); // 파싱된 값을 amount에 할당
-            validate(amount);
-            lottoTicketCount = amount / 1000;
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 천 원단위의 정수여야 합니다.");
-        }
+    private void parseAmount() {
+        amount = Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
     }
 
-    private int parseAmount(String amountInput) {
-        return Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
-    }
-
-    private void validate(int amount) {
+    private void validate() {
         if (amount <= 0 || amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 천 원단위의 양수여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 입력이 잘못되었습니다. 로또 구입 금액은 천 원단위의 양수여야 합니다.");
         }
+    }
+
+    private void setLottoTicketCount() {
+            lottoTicketCount = amount / 1000;
     }
 }
+
+
