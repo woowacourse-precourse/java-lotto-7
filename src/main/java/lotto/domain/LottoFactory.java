7d072be877ffce3e28e0ea9cc062cf8130562d86
yallo428 <yallo428@naver.com>
2024-10-30 22:10:36 +0900

    feat(LottoFactory) : 횟수만큼 로또 생성 구현

---
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoFactory {
+
+    public static List<Lotto> createLottos(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> randomNumbers =
+                    Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                    .stream()
+                    .sorted()
+                    .toList();
+            lottos.add(new Lotto(randomNumbers));
+        }
+        return lottos;
+    }
+}
