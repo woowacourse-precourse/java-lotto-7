0c7364b31337da2e36c446cd53d3ce42b89ba8a0
hongkikii <hyangmeeh@icloud.com>
2024-10-30 16:58:23 +0900

    test(PriceTest): 로또 구입 금액 입력 테스트 코드 추가

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -4,8 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PriceTest {
+
+    @DisplayName("로또 구입 금액을 정상적으로 저장한다.")
+    @Test
+    void 로또_구입_금액을_정상적으로_저장한다() {
+        String input = "1000";
+
+        Price price = Price.from(input);
+
+        assertEquals(1000, price.getValue());
+    }
 
     @DisplayName("로또 구입 금액이 null일 경우 예외가 발생한다.")
     @Test
@@ -30,6 +41,23 @@ public class PriceTest {
                 .isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> Price.from("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
+    @Test
+    void 로또_구입_금액이_1000원으로_나누어_떨어지지_않는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Price.from("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Price.from("1500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액이 2,000,000,000원을 초과할 경우 예외가 발생한다.")
+    @Test
+    void 로또_구입_금액이_2000000000원을_초과할_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Price.from("2000001000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
