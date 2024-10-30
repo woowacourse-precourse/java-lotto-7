4ac6fd733518c51dc45b5e65267b2bb413f01beb
dnwldla <leeemjw@naver.com>
2024-10-30 13:21:11 +0900

    feat: 로또들을 관리하는 LottosGroup 일급컬렉션 추가

---
+++ src/main/java/lotto/domain/LottosGroup.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottosGroup {
+    private final List<Lotto> lottos;
+
+    public LottosGroup(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
