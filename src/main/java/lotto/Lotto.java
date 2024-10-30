e2808b94f07271ba99fcae6426475e1a0f2c1127
OhYeHyun <ohyhohyl@naver.com>
2024-10-30 20:45:18 +0900

    feat(Lotto): 로또 번호 생성 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,16 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
     }
-
-    // TODO: 추가 기능 구현
 }
