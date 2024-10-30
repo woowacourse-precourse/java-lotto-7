3fe17435cdd1fe9a27f4340eb09b0caeb566e20c
JinGoo <zzgg3040@naver.com>
2024-10-30 21:28:26 +0900

    feat: 로또 번호의 범위를 검증한다.

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LottoRunner {
-    public static void start() {
+
+    public void start() {
         int money = 1;
         while (money % 1000 != 0) {
             System.out.println("구입금액을 입력해 주세요.");
@@ -36,6 +37,7 @@ public class LottoRunner {
             String nums = Console.readLine();
             try {
                 List<Integer> lottoNums = Arrays.stream(nums.split(",")).mapToInt(Integer::parseInt).boxed().toList();
+                this.validateLottoNum(lottoNums);
                 targetLotto = new Lotto(lottoNums);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -49,6 +51,7 @@ public class LottoRunner {
             System.out.println("\n보너스 번호를 입력해 주세요.");
             try {
                 int num = Integer.parseInt(Console.readLine());
+                validateLottoNum(num);
                 bonus.setNum(num);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -56,13 +59,26 @@ public class LottoRunner {
             }
             break;
         }
-        System.out.println(targetLotto);
-        System.out.println(bonus);
+
     }
 
-    private static void validate(int money) throws IllegalArgumentException {
+    private void validate(int money) throws IllegalArgumentException {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000 단위의 숫자로 입력해주세요.");
+        }
+    }
+
+    private void validateLottoNum(List<Integer> numbers) {
+        numbers.forEach(this::validateLottoNumRange);
+    }
+
+    private void validateLottoNum(int num) {
+        this.validateLottoNumRange(num);
+    }
+
+    private void validateLottoNumRange(int target) {
+        if (target < 1 || target > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }
