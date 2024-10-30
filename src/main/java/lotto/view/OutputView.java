d8ce6e442f677adfdfe28eabb1a96535e90f29a5
EunSeop <doosan1112@naver.com>
2024-10-30 23:36:54 +0900

    feat: 구매한 로또 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.Lotto;
+
+import java.util.List;
+
 public class OutputView {
 
     public void printPurchaseGuide() {
@@ -12,5 +16,15 @@ public class OutputView {
 
     public void printBonusNumberGuide() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
