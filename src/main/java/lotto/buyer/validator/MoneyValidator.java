c5a2b84b26cbc5fc7e22ccb6b4f91398ccc54656
EHCTH <lees90896@gmail.com>
2024-10-30 17:40:44 +0900

    feat(MoneyValidator): 검증 추가 * feat: 천원으로 안 나눠질 경우 * faet: 하루 최대 금액 10만원 이상일 경우

--- src/main/java/lotto/buyer/validator/MoneyValidator.java
+++ src/main/java/lotto/buyer/validator/MoneyValidator.java
@@ -1,29 +1,16 @@
 package lotto.buyer.validator;
 
+import lotto.constant.LottoConstant;
+
 public class MoneyValidator {
-    private static final long THOUSAND = 1_000L;
-    private static final long LiMIT_AMOUNT = 100_000L;
-    private static final long ZERO = 0L;
     public static void validate(Long money) {
-        isZero(money);
         limitAmountValidator(money);
         divisibleByThousandValidator(money);
     }
     private static void divisibleByThousandValidator(Long money) {
-        if (money % THOUSAND != 0) throw new IllegalArgumentException("[ERROR]");
+        if (money % LottoConstant.PURCHASE_AMOUNT.getValue() != 0) throw new IllegalArgumentException("[ERROR] 천원으로 나눠지게 입력해주세요");
     }
     private static void limitAmountValidator(Long money) {
-        if (money > LiMIT_AMOUNT) throw new IllegalArgumentException("[ERROR]");
+        if (money > LottoConstant.PURCHASE_LIMIT_AMOUNT.getValue()) throw new IllegalArgumentException("[ERROR] 금액은 10만원 이하로 입력해주세요");
     }
-    private static void isZero(Long money) {
-        if (money == ZERO) throw new IllegalArgumentException("[ERROR]");
-    }
-
 }
-/*
-  - [ x] 구매금액이 10만원을 초과할경우
-   - [x] 로또 구입 금액은 1000원 단위로 입력 한다
-   - [ ] 1000으로 나눠 떨어지지 않는 경우
-   - [ ] 0을 입력 했을 경우
-   - [ ] 숫자가 아닌 값을 입력할 경우
- */
