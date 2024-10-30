fc9a3df4251d6fab1e11f3a3ba94595d4ee2b333
ehgur062300 <ehgur062300@gmail.com>
2024-10-30 20:55:54 +0900

    feat: Add input view and output view for purchase price
    - Console.readLine()에 null 입력 시 " "반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import static lotto.view.OutputView.ENTER_PURCHASE_PRICE;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        OutputView.printMessage(ENTER_PURCHASE_PRICE);
+        String purchasePrice = InputView.readLine();
     }
 }
