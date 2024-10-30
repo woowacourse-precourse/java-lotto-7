d726ad3718e26c693d325dcf318abf52ebfb0b71
menzzi <minmin3927@naver.com>
2024-10-30 17:43:40 +0900

    feat(view) : OutputView - printLottoTicket 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
 import java.util.List;
+import lotto.Lotto;
 
 public class OutputView {
-    public void printLottoTicket(List<Integer> ticket){
-        System.out.println(ticket);
+    public void printLottoTicket(List<Lotto> lottoTickets, int lottoTicketNumber){
+        System.out.println(lottoTicketNumber + "개를 구매했습니다.");
+        for(Lotto lotto : lottoTickets){
+            System.out.println(lotto);
+        }
     }
 }
