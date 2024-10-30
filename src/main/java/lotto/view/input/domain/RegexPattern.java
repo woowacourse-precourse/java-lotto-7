7867faaf60ab18cccfbde6412b7bd184c598cd24
EHCTH <lees90896@gmail.com>
2024-10-30 18:24:24 +0900

    feat(RegexPattern): 기능 구현 * feat: match(String): boolean 정수인지, 로또 정규식이 맞는지

--- src/main/java/lotto/view/input/domain/RegexPattern.java
+++ src/main/java/lotto/view/input/domain/RegexPattern.java
@@ -1,4 +1,16 @@
 package lotto.view.input.domain;
 
+import java.util.regex.Pattern;
+
 public enum RegexPattern {
+    NUMBER_REGEX(Pattern.compile("^[1-9]\\d*$")),
+    LOTTO_REGEX(Pattern.compile("^\\d+(?:,\\d+)+$"));
+    private final Pattern pattern;
+
+    RegexPattern(Pattern pattern) {
+        this.pattern = pattern;
+    }
+    public boolean match(String input) {
+        return pattern.matcher(input).matches();
+    }
 }
