346bf469824ba4440d4163d6ddb7db5011a6b945
EHCTH <lees90896@gmail.com>
2024-10-30 19:01:52 +0900

    feat(LottoContollerFactory): 기능 구현 * feat: +crate(): LottoController * feat: -createInsertMoneySerivce(): InsertMoneyService * feat: -createBuyer(InsertMoneyService): Buyer * feat: -createLottoMachine(): LottoMachine * feat: -createResultViewService(): ResultViewService * feat: -createWinningCalculator(): WinningCalculator

--- src/main/java/lotto/controller/service/LottoControllerFactory.java
+++ src/main/java/lotto/controller/service/LottoControllerFactory.java
@@ -1,4 +1,48 @@
 package lotto.controller.service;
 
-public class LottoContollerFactory {
+import lotto.buyer.domain.Buyer;
+import lotto.buyer.domain.InsertMoneyService;
+import lotto.buyer.infrastructure.InsertWon;
+import lotto.buyer.infrastructure.Won;
+import lotto.controller.domain.LottoController;
+import lotto.controller.domain.WinningLottoController;
+import lotto.lotto.domain.LottoMachine;
+import lotto.lotto.winning.domain.WinningCalculator;
+import lotto.lotto.infrastructure.RandomNumberGenerate;
+import lotto.lotto.infrastructure.WonCalculator;
+import lotto.view.input.hanlder.infrastructure.MoneyHandler;
+import lotto.view.input.infrastructure.MoneyInput;
+import lotto.view.output.domain.ResultViewService;
+import lotto.view.output.infrastructure.*;
+
+public class LottoControllerFactory {
+    public static LottoController create() {
+        InsertMoneyService insertMoneyService = createInsertMoneyService();
+        Buyer buyer = createBuyer(insertMoneyService);
+        LottoMachine lottoMachine = createLottoMachine();
+        ResultViewService resultViewService = createResultViewService();
+        WinningLottoController winningLottoController = WinningLottoControllerFactory.create();
+        WinningCalculator winningCalculator = createWinningCalculator();
+        return new LottoController(buyer, lottoMachine, resultViewService, winningLottoController, winningCalculator);
+    }
+
+    private static InsertMoneyService createInsertMoneyService() {
+        return new InsertWon(new MoneyHandler(new MoneyInput(new MoneyOutput())));
+    }
+
+    private static Buyer createBuyer(InsertMoneyService insertMoneyService) {
+        return new Buyer(insertMoneyService);
+    }
+
+    private static LottoMachine createLottoMachine() {
+        return new LottoMachine(new WonCalculator(), new RandomNumberGenerate(), new PurchaseOutput());
+    }
+
+    private static ResultViewService createResultViewService() {
+        return new CommonResultOutput();
+    }
+
+    private static WinningCalculator createWinningCalculator() {
+        return new WinningCalculator(new Won());
+    }
 }
