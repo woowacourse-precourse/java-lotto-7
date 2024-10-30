a6db560f2ae66544043e1f13101e048f6b86b144
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-31 00:55:35 +0900

    feat: 당첨 번호 수, 보너스번호 여부를 저장할 Winning 클래스 구현

---
+++ src/main/java/lotto/Winning.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class Winning {
+    private int winningCount;
+    private boolean isBonus;
+
+    public Winning(int winningCount, boolean isBonus) {
+        this.winningCount = winningCount;
+        this.isBonus = isBonus;
+    }
+}
