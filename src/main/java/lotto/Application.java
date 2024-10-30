5ece4ba0c2d7a9eed5d1d417c9d78d3fe0f30134
sunjong0214 <rlatjswhd8200@gmail.com>
2024-10-30 23:03:03 +0900

    feat: 구입금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int lottoPrice = Integer.parseInt(Console.readLine());
     }
 }
