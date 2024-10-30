33442c622e24bae0eb40c124003f532438ab3d45
CinaDor <cinador@github.com>
2024-10-31 01:34:34 +0900

    feat: 사용자가 구입한 로또를 저장하는 기능
    사용자가 구입한 로또를 저장하는 클래스를 구현했습니다.

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,36 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Lottos {
+    List<Lotto> lottos;
+
+    public Lottos() {
+        lottos = new ArrayList<>();
+    }
+
+    public void insertLotto(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public Result getResult(Lotto targetLotto, int targetBonus) {
+        return new Result(convertLottosToPrizes(targetLotto, targetBonus)
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(
+                        prize -> prize,
+                        Collectors.summingInt(prize -> 1)
+                )));
+    }
+
+    private Stream<Prize> convertLottosToPrizes(Lotto targetLotto, int targetBonus) {
+        return lottos.stream()
+                .map(lotto -> lotto.lottoToPrize(
+                        lotto.countHit(targetLotto),
+                        lotto.checkBonus(targetBonus)))
+                .filter(Objects::nonNull);
+    }
+}
---
+++ src/test/java/lotto/model/LottosTest.java
@@ -0,0 +1,42 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottosTest {
+    static Lottos sampleLottos;
+
+    @BeforeEach
+    void setUpTest() {
+        sampleLottos = new Lottos();
+        sampleLottos.insertLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        sampleLottos.insertLotto(new Lotto(List.of(1, 2, 3, 11, 12, 13)));
+        sampleLottos.insertLotto(new Lotto(List.of(1, 2, 3, 4, 12, 13)));
+    }
+
+    @ParameterizedTest(name = "{index}: {2}")
+    @MethodSource("lottoTest")
+    @DisplayName("선택한 수에 따른 결과 테스트")
+    void prize_findByHitAndBonusTest(Lotto targetLotto, int targetBonus, String message, int totalMoney) {
+
+        Result sampleResult = sampleLottos.getResult(targetLotto, targetBonus);
+
+        assertEquals(sampleResult.totalMoney(), totalMoney);
+    }
+
+    static Stream<Arguments> lottoTest() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 12, 13, 45)), 4, "2등+3등+5등", 31_505_000),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7, "1등+4등+5등", 2_000_055_000),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 20, 21, 22)), 4, "5등 3개", 15_000),
+                Arguments.of(new Lotto(List.of(30, 31, 32, 33, 34, 35)), 1, "x", 0)
+        );
+    }
+}
