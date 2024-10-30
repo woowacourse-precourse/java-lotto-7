22fe5f69f14309eebe2f6298bacd87f085ffb2d5
kcs19 <changsung19@gmail.com>
2024-10-31 02:10:35 +0900

    feat: 구입 금액 입력 기능 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static String inputMoney(){
+        System.out.println(INPUT_MONEY_MESSAGE);
+
+        return Console.readLine();
+    }
+}
