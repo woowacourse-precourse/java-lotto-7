941976149a80ec21fc844b42edc03eeadc486566
menzzi <minmin3927@naver.com>
2024-10-30 14:58:44 +0900

    test : LottoMachineControllerTest 구현

--- src/main/java/lotto/controller/LottoMachineController.java
+++ src/main/java/lotto/controller/LottoMachineController.java
@@ -5,18 +5,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.Lotto;
-import lotto.view.OutputView;
 
 public class LottoMachineController {
-    private final OutputView output = new OutputView();
-
-    public List<Lotto> issueLotto(int lottoTicketNumber){
+    public static List<Lotto> issueLotto(int lottoTicketNumber){
         List<Lotto> lottoTickets = new ArrayList<>();
 
         for(int i=0;i<lottoTicketNumber;i++){
             List<Integer> ticket = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(ticket);
-            output.printLottoTicket(ticket);
             lottoTickets.add(new Lotto(ticket));
         }
 
