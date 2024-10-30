462574fe50dd513a171f298534a900a76b8243af
urim96 <qusdbfla1@gmail.com>
2024-10-30 18:05:04 +0900

    test: 입력값 받는 테스트 코드 수정
    예외처리 시 값이 제대로 전달되지 않아 코드 수정
    테스트 코드도 변경된 코드로 수정

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class InputTest {
@@ -67,30 +68,31 @@ class InputTest {
 	@Test
 	void 올바른_보너스번호_입력() {
 		input.입력값("7");
-		assertDoesNotThrow(() -> input.bonusInput(input.로또번호(List.of(1, 2, 3, 4, 5, 6))));
+		assertDoesNotThrow(() -> input.bonusInput());
 	}
 
 	@Test
 	void 예외처리_입력받은_보너스번호가_숫자값이_값이_아닌_경우() {
 		input.입력값("a");
-		assertThrows(IllegalArgumentException.class, () -> input.bonusInput(input.로또번호(List.of(1, 2, 3, 4, 5, 6))));
+		assertThrows(IllegalArgumentException.class, () -> input.bonusInput());
 	}
 
 	@Test
 	void 예외처리_입력받은_보너스번호가_당첨번호와_중복값인_경우() {
 		input.입력값("1");
-		assertThrows(IllegalArgumentException.class, () -> input.bonusInput(input.로또번호(List.of(1, 2, 3, 4, 5, 6))));
+		assertThrows(IllegalArgumentException.class, () -> input.bonusInput());
 	}
 
 	@Test
 	void 예외처리_입력받은_보너스번호가_1에서_45사이의_값이_아닌_경우() {
 		input.입력값("0");
-		assertThrows(IllegalArgumentException.class, () -> input.bonusInput(input.로또번호(List.of(1, 2, 3, 4, 5, 6))));
+		assertThrows(IllegalArgumentException.class, () -> input.bonusInput());
 	}
 
 	static class TestInput extends Input {
 		private String testInput;
 		private List<Integer> testList;
+		private static List<Integer> numbers = new ArrayList<>(List.of(1,2,3,4,5,6));
 
 		void 입력값(String input) {
 			this.testInput = input;
@@ -106,12 +108,11 @@ class InputTest {
 		}
 
 		@Override
-		public int amountInput() {
+		public void amountInput() {
 			try {
 				System.out.println("구입금액을 입력해 주세요.");
 				String amount = readLine();
 				Amount amountCheck = new Amount(amount);
-				return amountCheck.getAmount();
 			} catch (IllegalArgumentException e) {
 				System.err.println(e);
 				// 테스트 코드에서 무한 반복을 막기위해 재귀 호출 코드 제거
@@ -120,29 +121,29 @@ class InputTest {
 		}
 
 		@Override
-		public List<Integer> winningNumber() {
+		public void winningNumber() {
 			try {
 				System.out.println();
 				System.out.println("당첨 번호를 입력해 주세요.");
 				String winning = readLine();
 				WinningNumber winningNumber = new WinningNumber(winning);
 				Lotto lotto = new Lotto(winningNumber.winningCheck());
-				return lotto.listValueCheck();
+				Input.setNumbers(lotto.listValueCheck());
 			} catch (IllegalArgumentException e) {
 				System.err.println(e);
 				// 테스트 코드에서 무한 반복을 막기위해 재귀 호출 코드 제거
 				throw e;
 			}
 		}
-
+		
 		@Override
-		public int bonusInput(List<Integer> winningList) {
+		public void bonusInput() {
 			try {
 				System.out.println();
 				System.out.println("보너스 번호를 입력해 주세요.");
 				String bonus = readLine();
-				BonusNumber bonusList = new BonusNumber(bonus, winningList);
-				return bonusList.bounsCheck();
+				BonusNumber bonusList = new BonusNumber(bonus, numbers);
+				bonusList.bounsCheck();
 			} catch (IllegalArgumentException e) {
 				System.err.println(e);
 				// 테스트 코드에서 무한 반복을 막기위해 재귀 호출 코드 제거
