d077c85a203ee77572c7453cbe803a882abbe6c4
bowook <mbosx316@naver.com>
2024-10-30 19:49:02 +0900

    feat(LottoController) : LottoController 기능 추가
    - LottoController 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,26 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.service.LottoService;
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+    public void startLottoSimulation() {
+        purchaseLotto();
+    }
+
+    private void purchaseLotto() {
+        String purchaseAmount = inputView.inputPurchaseAmount();
+    }
 }
