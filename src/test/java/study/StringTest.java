a5e00cf8bb4e78fbff3a37b7528740215f1e0890
princebyun <princebyun@gmail.com>
2024-10-30 23:51:39 +0900

    2024.10.30 요구사항 및 기능 분석 1차

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+
+    /**
+     * "1,2"을 ,로 split했을때 1과 2로 잘분리되는 지 확인하는 학습테스트 구현
+     *  "1"을 ,로 split했을때 1만 포함하는 배열이 반환되는지에 대한 학습테스트 구현
+     */
+
+
+    @Test
+    void split테스트(){
+
+    }
+}
