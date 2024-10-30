160eba80a4b5c996eb1d8efa9a2e9f256bc3e46d
bowook <mbosx316@naver.com>
2024-10-30 23:01:05 +0900

    feat(LottoException) : LottoException 기능 추가
    - IllegalArgumentException을 extends해서 예외메시지 처리

--- src/main/java/lotto/validator/exception/LottoException.java
+++ src/main/java/lotto/validator/exception/LottoException.java
@@ -1,2 +1,12 @@
-package lotto.validator.exception;public class LottoException {
+package lotto.validator.exception;
+
+public class LottoException extends IllegalArgumentException {
+
+    private LottoException(final ErrorMessage errorMessage) {
+        super(errorMessage.getErrorMessage());
+    }
+
+    public static LottoException from(final ErrorMessage errorMessage) {
+        return new LottoException(errorMessage);
+    }
 }
