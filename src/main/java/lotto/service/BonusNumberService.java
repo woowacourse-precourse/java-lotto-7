d32e2528f843cf8cafb9650eddb4866c6630ce88
bowook <mbosx316@naver.com>
2024-10-31 01:34:07 +0900

    feat(BonusNumberService) : BonusNumberService 서비스 추가
    - 파싱 호출
    - 검증 호출

--- src/main/java/lotto/service/BonusNumberService.java
+++ src/main/java/lotto/service/BonusNumberService.java
@@ -1,2 +1,15 @@
-package lotto.service;public class BonusNumberService {
+package lotto.service;
+
+
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.utils.Parser;
+import lotto.validator.BonusNumberValidator;
+
+public class BonusNumberService {
+    public BonusNumber generateBonusNumber(String bonusNumber, Lotto winningLotto) {
+        int convertedBonusNumber = Parser.parsingBonusNumber(bonusNumber);
+        BonusNumberValidator.validateProcess(convertedBonusNumber, winningLotto);
+        return new BonusNumber(convertedBonusNumber);
+    }
 }
