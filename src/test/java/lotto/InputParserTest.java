e6296cbab6a62dd3373b4d2c1550a873942d51d5
leesh7807 <leesh7807@gmail.com>
2024-10-30 20:52:58 +0900

    test(InputParser): create tests
    Check exceptions
    - not a integer
    - has an unnecessary extra value
    - out of integer range
    - not a comma-separated integer list

---
+++ src/test/java/lotto/InputParserTest.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class InputParserTest {
+    @Test
+    void 숫자가_아닌_예외() {
+        assertThatThrownBy(() -> InputParser.parseInt("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 필요없는_값이_입력된_예외() {
+        assertThatThrownBy(() -> InputParser.parseInt("3000 5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 정수범위_벗어난_예외() {
+        assertThatThrownBy(() -> InputParser.parseInt("10000000000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 쉼표로_구분되지_않은_배열이_들어온_예외() {
+        assertThatThrownBy(() -> InputParser.parseIntList("1:3.4,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
