4f554596db7cbe7009612b33dcba54908d2edbd4
Spinichi <spinichi@naver.com>
2024-10-31 03:59:40 +0900

    feat: LotteryProcess 클래스 및 메서드 추가
    복권 추첨하는 프로세스를 구현

---
+++ src/main/java/lotto/LotteryProcess.java
@@ -0,0 +1,50 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LotteryProcess {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+    private final List<List<Integer>> userTicketNumbers;
+    private int matchNumberCount;
+
+    private final int RANKING_NUMBER = 5;
+    private List<Integer> rankingCount = new ArrayList<>(Collections.nCopies(RANKING_NUMBER, 0));
+
+
+    public LotteryProcess(Lotto lotto, UserBonusNumberInput userBonusNumberInput,
+                          PrintTicketCount printTicketCount) {
+        this.numbers = lotto.getNumbers();
+        this.bonusNumber = userBonusNumberInput.getBonusNumber();
+        this.userTicketNumbers = printTicketCount.getTicketNumbers();
+    }
+
+    public void countMatchNumbers() {
+        for (List<Integer> ticket : userTicketNumbers) {
+            matchNumberCount = 0;
+            searchMatchNumbers(ticket);
+
+            boolean bonusMatch = ticket.contains(bonusNumber);
+            LottoRank rank = LottoRank.calculateRank(matchNumberCount, bonusMatch);
+
+            if (rank != null) {
+                int rankIndex = rank.ordinal();  // Enum 순서에 따른 인덱스 활용
+                rankingCount.set(rankIndex, rankingCount.get(rankIndex) + 1);
+            }
+        }
+    }
+
+    public List<Integer> getRankingCount() {
+        return rankingCount;
+    }
+
+    private void searchMatchNumbers(List<Integer> ticket) {
+        for (int lottoNumber : numbers) {
+            if (ticket.contains(lottoNumber)) {
+                matchNumberCount++;
+            }
+        }
+    }
+}
