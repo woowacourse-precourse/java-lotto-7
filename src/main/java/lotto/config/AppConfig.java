90a60f97a9e20887ffc145d56a407bc76818bcc4
bowook <mbosx316@naver.com>
2024-10-30 19:54:31 +0900

    feat(AppConfig) : AppConfig 기능 추가
    - AppConfig로 사전에 의존 관계를 설정하였습니다.

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,2 +1,46 @@
-package lotto.config;public class AppConfig {
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.service.LottoGeneratorService;
+import lotto.service.LottoService;
+import lotto.service.ProfitCalculatorService;
+import lotto.service.PurchaseService;
+import lotto.service.StatisticsService;
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
+
+public class AppConfig {
+
+    public LottoController lottoController() {
+        return new LottoController(inputView(), outputView(), lottoService());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private LottoService lottoService() {
+        return new LottoService(purchaseService(), lottoGeneratorService(), statisticsService(), profitCalculatorService());
+    }
+
+    private PurchaseService purchaseService() {
+        return new PurchaseService();
+    }
+
+    private LottoGeneratorService lottoGeneratorService() {
+        return new LottoGeneratorService();
+    }
+
+    private StatisticsService statisticsService() {
+        return new StatisticsService();
+    }
+
+    private ProfitCalculatorService profitCalculatorService() {
+        return new ProfitCalculatorService();
+    }
+
 }
