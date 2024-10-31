5a617b72a7080f1cac110f3808f4a76dc1de45b6
mininiii <didals0521@gmail.com>
2024-10-31 14:21:15 +0900

    feat: 당첨 통계 관리 클래스 생성

---
+++ src/main/java/lotto/util/StatisticsGenerator.java
@@ -0,0 +1,44 @@
+package lotto.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.model.LottoRankCalculator;
+
+public class StatisticsGenerator {
+    private final Map<LottoRank, Integer> statistics = new HashMap<>();
+    private final LottoRankCalculator lottoRankCalculator;
+
+    public StatisticsGenerator(LottoRankCalculator lottoRankCalculator) {
+        this.lottoRankCalculator = lottoRankCalculator;
+        statisticsInitializer();
+    }
+
+    private void statisticsInitializer() {
+        for (LottoRank rank : LottoRank.values()) {
+            statistics.put(rank, 0);
+        }
+    }
+
+    public void makeStatistics(List<Lotto> lotto, Lotto winningNumbers, int bonusNumber) {
+        for (Lotto lottoNumbers : lotto) {
+            LottoRank rank = lottoRankCalculator.getLottoPrizeRank(lottoNumbers.getNumbers(),
+                    winningNumbers.getNumbers(), bonusNumber);
+            putCountingIntoStatistics(rank);
+        }
+    }
+
+    private void putCountingIntoStatistics(LottoRank rank) {
+        if (statistics.containsKey(rank)) {
+            statistics.put(rank, statistics.get(rank) + 1);
+            return;
+        }
+        statistics.put(rank, 1);
+    }
+
+    public Map<LottoRank, Integer> getStatistics() {
+        return statistics;
+    }
+}
