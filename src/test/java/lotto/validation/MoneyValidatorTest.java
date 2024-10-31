237b8c69b5cab002eb7adf852ecf55d91a8c5657
이윤빈 <kanado20000@naver.com>
2024-11-01 02:40:25 +0900

    :sparkles: feat: 구입 금액의 유효성 검사하는 클래스의 validateMoney 메서드의 테스트 코드 추가

---
+++ src/test/java/lotto/validation/MoneyValidatorTest.java
@@ -0,0 +1,35 @@
+package lotto.validation;
+
+import org.junit.jupiter.api.Test;
+
+import lotto.enums.ErrorMessage;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MoneyValidatorTest {
+    @Test
+    void 정상적인_구입_금액_검사(){
+        Integer money = 4000;
+
+        assertThatCode(()-> MoneyValidator.validateMoney(money))
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 비정상적인_구입_금액_검사(){
+        Integer money = 4431;
+
+        assertThatThrownBy(()-> MoneyValidator.validateMoney(money))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_MONEY_UNIT.getMessage());
+    }
+
+    @Test
+    void 자연수_아닌_숫자_입력(){
+        Integer money = -1;
+
+        assertThatThrownBy(() -> MoneyValidator.validateMoney(money))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_TURN_NUMBER.getMessage());
+    }
+}
