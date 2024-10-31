68a028ce8f24eeea1bdc36d8198b86580608f062
cux-maks <volcano020716@gmail.com>
2024-11-01 00:03:08 +0900

    refactor(OutputFormats): 출력 형식 클래스 수정
    쿨력 형식 관리 클래스를 수정하였습니다.

--- src/main/java/constants/OutputFormats.java
+++ src/main/java/constants/OutputFormats.java
@@ -1,4 +1,34 @@
 package constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum OutputFormats {
+    FIFTH_PLACE("3개 일치 (5,000원)"),
+    FOURTH_PLACE("4개 일치 (50,000원)"),
+    THIRD_PLACE("5개 일치 (1,500,000원)"),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST_PLACE("6개 일치 (2,000,000,000원)");
+
+    private final String format;
+
+    OutputFormats(String format) {
+        this.format = format;
+    }
+
+    public String getResultString(int count) {
+        return format + " - " + count + "개";
+    }
+
+    public static List<String> getAllResults(List<Integer> counts) {
+        List<Integer> reversedCounts = new ArrayList<>(counts);
+        Collections.reverse(reversedCounts);
+
+        return Arrays.stream(values())
+                .map(format -> format.getResultString(reversedCounts.get(format.ordinal())))
+                .collect(Collectors.toList());
+    }
 }
