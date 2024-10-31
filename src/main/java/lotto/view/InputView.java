de1b1b38ea7642e5bf8055abb34035efb5a26eb5
aprilnineteen <dencoding@naver.com>
2024-10-31 19:10:13 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT = "구입 금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT = "당첨 번호를 입력해 주세요";
 
     public String readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_INPUT);
+        return Console.readLine().strip();
+    }
+
+    public String readWinningNumber() {
+        System.out.println(WINNING_NUMBERS_INPUT);
         return Console.readLine().strip();
     }
 }
