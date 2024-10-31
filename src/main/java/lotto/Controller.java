726fa5c526bee59dd2940f0026ded32145b8d0c0
joshinhyeok <shtree07@naver.com>
2024-11-01 02:47:20 +0900

    feat: 당첨 번호로 변환하고 잘못됐을 경우 재입력 받는 기능 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,14 +1,17 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
 
     public static void main(String[] args) {
-int money = getPurchaseAmount();
+        int money = getPurchaseAmount();
         int lottoCount = LottoService.calculateLottoCount(money);
         List<Lotto> lottoTickets = LottoService.generateLottos(lottoCount);
         OutputView.printLottos(lottoTickets);
+        Lotto WinningLotto = getWinningLotto();
     }
 
     private static int getPurchaseAmount() {
@@ -17,6 +20,21 @@ int money = getPurchaseAmount();
             try {
                 InputValidator.validatePurchaseAmount(input);
                 return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static Lotto getWinningLotto() {
+        while (true) {
+            try {
+                String input = InputView.readWinningNumbers();
+                Lotto lotto = Arrays.stream(input.split(","))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.collectingAndThen(Collectors.toList(), Lotto::new));
+                return lotto;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
