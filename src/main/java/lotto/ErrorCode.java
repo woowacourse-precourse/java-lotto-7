07884d9d0b6ca7a45a57af50b5f76989310c5ce1
hyosang <billy04100410@gmail.com>
2024-10-31 12:53:58 +0900

    feat(validation): 당첨 번호 검증 로직 추가
    추가한 검증 로직은 다음과 같습니다:
    - 당첨 번호 입력 시 쉼표 검증
    - 당첨 번호 숫자 범위 검증
    - 당첨 번호 개수 검증
    - 당첨 번호 중복 검증

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -1,12 +1,18 @@
 package lotto;
 
 public enum ErrorCode {
-    INVALID_PURCHASE_AMOUNT("구입 금액은 1,000 단위이어야 합니다.");
+    INVALID_PURCHASE_AMOUNT("구입 금액은 1,000원 단위로만 가능합니다."),
+    CONTIGIOUS_COMMA("입력에 쉼표가 연속적으로 입력되었습니다."),
+    INVALID_NUMBER_RANGE("로또 번호의 숫자 범위는 1~45까지입니다."),
+    DUPLICATE_WINNIG_NUMBER("입력된 당첨 번호가 중복됩니다."),
+    INVALID_WINNIG_NUMBER_COUNT("당첨 번호 개수는 6개이어야 합니다.");
 
     private final String message;
+
     ErrorCode(String message) {
         this.message = message;
     }
+
     public String getMessage() {
         return message;
     }
--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -1,15 +1,52 @@
 package lotto;
 
+import static lotto.ErrorCode.CONTIGIOUS_COMMA;
+import static lotto.ErrorCode.DUPLICATE_WINNIG_NUMBER;
+import static lotto.ErrorCode.INVALID_NUMBER_RANGE;
 import static lotto.ErrorCode.INVALID_PURCHASE_AMOUNT;
+import static lotto.ErrorCode.INVALID_WINNIG_NUMBER_COUNT;
+
+import java.util.List;
 
 public class InputValidator {
 
     private static final int TICKET_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int VALID_LOTTO_NUMBER_COUNT = 6;
+    private static final String COMMAS = ",,";
 
+    //구매 금액 검증
     public static void validatePurchaseAmount(final int purchaseAmount) {
         if (purchaseAmount % TICKET_PRICE != 0) {
             throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT.getMessage());
         }
     }
 
+    //당첨 번호 검증
+    public static void validateInputWinnigNumber(final String inputWinnigNumber) {
+        if (inputWinnigNumber.contains(COMMAS)) {
+            throw new IllegalArgumentException(CONTIGIOUS_COMMA.getMessage());
+        }
+    }
+
+    public static void validateWinningNumberRange(final List<Integer> winningNumbers) {
+        for (Integer winningNumber : winningNumbers) {
+            if (winningNumber < MIN_LOTTO_NUMBER || winningNumber > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getMessage());
+            }
+        }
+    }
+
+    public static void validateWinningNumberDuplicate(final List<Integer> winningNumbers) {
+        if (winningNumbers.stream().distinct().count() != winningNumbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_WINNIG_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateWinningNumberCount(final List<Integer> winningNumbers) {
+        if (winningNumbers.size() != VALID_LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(INVALID_WINNIG_NUMBER_COUNT.getMessage());
+        }
+    }
 }
