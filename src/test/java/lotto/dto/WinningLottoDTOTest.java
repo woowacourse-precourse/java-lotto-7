14977acd5feea69d483599cea1913aa55d147531
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 22:20:36 +0900

    test(WinningLottoDTO) : 테스트 코드 추가

---
+++ src/test/java/lotto/dto/WinningLottoDTOTest.java
@@ -0,0 +1,23 @@
+package lotto.dto;
+
+import static lotto.util.message.OutputMessage.ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import lotto.model.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoDTOTest {
+
+    @Test
+    @DisplayName("입력한 6개의 당첨 번호 중 보너스 번호와 중복된 번호가 존재하면 예외 발생")
+    void throwExceptionIfDuplicatedBonusNumberContained() {
+        Lotto winningLottoNumber = new Lotto(List.of(1,2,3,4,5,6));
+        int bonusNumber = 6;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinningLottoDTO(winningLottoNumber, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+}
