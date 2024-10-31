9cd60255e9e8af0b292e5ecb88f568f68380e204
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-31 19:39:18 +0900

    test: 입력 받은 문자열이 숫자인지 검증하는 테스트 구현
    - InputValidation.parseValidateNumber 테스트 구현

--- src/test/java/lotto/validation/InputValidationTest.java
+++ src/test/java/lotto/validation/InputValidationTest.java
@@ -1,7 +1,6 @@
 package lotto.validation;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +10,13 @@ class InputValidationTest {
     @DisplayName("빈 문자열 오류 검출 확인하기")
     public void 빈_문자열_검증() {
         assertThatThrownBy(() -> InputValidation.isNotBlank("  "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자 변환 불가할때 오류 확인하기")
+    public void 숫자_변환_검증() {
+        assertThatThrownBy(() -> InputValidation.parseValidateNumber("1w3"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
