10eb2d287b495c0c5a092f7c621dde1df43ea933
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-31 18:05:33 +0900

    feat($LottoPrize) : Add enum class
    당첨 등수 및 금액에 대한 enum 클래스 생성

---
+++ src/main/java/lotto/LottoPrize.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static java.lang.Integer.parseInt;
+
+public enum LottoPrize {
+    FIRST("2,000,000,000"),
+    SECOND("30,000,000"),
+    THIRD("1,500,000"),
+    FOURTH("50,000"),
+    FIFTH("5,000");
+
+    private final String price;
+
+    LottoPrize(String price) {
+        this.price = price;
+    }
+
+    public String getStringPrice() {
+        return price;
+    }
+
+    public int getIntPrice() {
+        return parseInt(price.replaceAll(",", ""));
+    }
+}
