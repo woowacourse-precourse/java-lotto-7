83ad108b322b435b1252a81771cedf482be25b91
배현서 <haulqogustj@naver.com>
2024-10-31 18:58:30 +0900

    feat: 로또 게임 메인 애플리케이션 초기화 로직 구현
    의존성 주입을 통해 MVC 아키텍처의 핵심 컴포넌트들을 초기화한다
    기본적인 게임 실행 구조를 구성한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.model.LottoFactory;
+import lotto.util.InputValidator;
+import lotto.view.ConsoleInputView;
+import lotto.view.ConsoleOutputView;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+        InputValidator inputValidator = new InputValidator();
+        LottoFactory lottoFactory = new LottoFactory();
+
+        Controller controller = new Controller(inputView, outputView, inputValidator, lottoFactory);
+        controller.run();
     }
 }
