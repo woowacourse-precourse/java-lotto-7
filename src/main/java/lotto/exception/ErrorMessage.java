6cb6b306464bfa4cd1781885eda6ba650a4d707d
StoneCAU <seokwoo1999@naver.com>
2024-10-31 20:19:12 +0900

    refactor(ErrorMessage): purchaseAmount -> Money 변경

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -5,10 +5,9 @@ public enum ErrorMessage {
     INPUT_EMPTY_ERROR("빈 입력입니다."),
 
     // 구입 금액
-    PURCHASE_AMOUNT_NOT_DIVISIBLE("구입 금액은 %d원으로 나누어 떨어져야 합니다."),
-    PURCHASE_AMOUNT_NOT_NUMBER("구입 금액은 숫자여야 합니다."),
-    PURCHASE_AMOUNT_OUT_OF_RANGE("구입 금액은 %d이상의 정수여야 합니다.");
-
+    MONEY_NOT_DIVISIBLE("구입 금액은 %d원으로 나누어 떨어져야 합니다."),
+    MONEY_NOT_NUMBER("구입 금액은 숫자여야 합니다."),
+    MONEY_OUT_OF_RANGE("구입 금액은 %d이상의 정수여야 합니다.");
 
     private final String message;
 
