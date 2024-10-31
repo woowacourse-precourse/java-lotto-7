6351afdc260c9995ebbef6eb8d7a147d34bd2b0b
sunwon <jsw5913@naver.com>
2024-11-01 00:12:30 +0900

    refactor(LottoSellingMachine): 테스트 코드 케이스 중복 제거
    - @ParameterizedTest을 이용해 중복 제거

--- src/test/java/lotto/LottoSellingMachineTest.java
+++ src/test/java/lotto/LottoSellingMachineTest.java
@@ -7,23 +7,17 @@ import lotto.model.LottoSellingMachine;
 import lotto.model.Lottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoSellingMachineTest {
 
-    @DisplayName("1000원을 넣으면 로또 1개를 생성한다")
-    @Test
-    void createOneLottoWhenInputMoney1000() {
-        long money = 1000;
+    @ParameterizedTest
+    @ValueSource(longs = {1000L, 2000L})
+    @DisplayName("1000원 단위의 금액을 넣으면 로또 1개를 생성한다")
+    void createOneLottoWhenInputMoney1000(long money) {
         Lottos lottos = LottoSellingMachine.sell(money);
-        assertEquals(1, lottos.getSize());
-    }
-
-    @DisplayName("2000원을 넣으면 로또 2개를 생성한다")
-    @Test
-    void createTwoLottosWhenInputMoney2000() {
-        long money = 2000;
-        Lottos lottos = LottoSellingMachine.sell(money);
-        assertEquals(2, lottos.getSize());
+        assertEquals(money/1000, lottos.getSize());
     }
 
     @DisplayName("1000원 단위가 아닌 금액이 입력되면 예외가 발생한다")
