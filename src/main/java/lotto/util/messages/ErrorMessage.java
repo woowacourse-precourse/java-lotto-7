04c442ceda9787f56e3a356a34419ee1a49e66c1
kymin <park48664866@gmail.com>
2024-10-31 16:02:53 +0900

    feat($error): 예외처리 사항 추가
    예외처리 사항 추가:
    - 당첨 번호 입력 시 중복값에 대한 예외처리 추가
    - 보너스 번호 입력 시 기존 당첨 번호와의 중복여부에 대한 예외처리 추가

--- src/main/java/lotto/util/messages/ErrorMessage.java
+++ src/main/java/lotto/util/messages/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     PRICE_IS_UNDER_1000("[ERROR] 구입금액은 1000원 이상이어야 합니다."),
     NUMBER_COUNT_NOT_SIX("[ERROR] 로또 번호는 6개여야 합니다."),
     NUMBER_OVER_RANGE("[ERROR] 로또 번호는 1~45사이의 숫자여야 합니다."),
-    IS_NOT_INTEGER("[ERROR] 로또 번호는 정수여야 합니다.");
+    NUMBER_IS_NOT_INTEGER("[ERROR] 로또 번호는 정수여야 합니다."),
+    NUMBER_IS_DUPLICATED("[ERROR] 로또 번호는 중복될 수 없습니다.");
 
     private final String message;
 
