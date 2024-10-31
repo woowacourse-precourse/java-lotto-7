f310f3e18e121b428fa7836ea9b4a657b5ef0096
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-10-31 23:09:37 +0900

    feat: add calculating total prize and count of ranks
    - calculate total prize
    - calculate count of winning ranks

--- src/main/java/lotto/model/LottoTickets.java
+++ src/main/java/lotto/model/LottoTickets.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
+import java.math.BigInteger;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoTickets {
 
@@ -8,6 +11,26 @@ public class LottoTickets {
 
     public LottoTickets(final List<Lotto> tickets) {
         this.tickets = tickets;
+    }
+
+    public BigInteger calculateWinningPrize(final WinningLotto winningLotto) {
+        EnumMap<LottoRank, Integer> ranks = calculateAllRanks(winningLotto);
+        BigInteger totalPrize = BigInteger.ZERO;
+        for (Map.Entry<LottoRank, Integer> entry : ranks.entrySet()) {
+            LottoRank rank = entry.getKey();
+            int rankCount = entry.getValue();
+            totalPrize = totalPrize.add(rank.calculatePrizeByCount(rankCount));
+        }
+        return totalPrize;
+    }
+
+    public EnumMap<LottoRank, Integer> calculateAllRanks(final WinningLotto winningLotto) {
+        EnumMap<LottoRank, Integer> ranks = new EnumMap<>(LottoRank.class);
+        for (Lotto lotto : tickets) {
+            LottoRank rank = winningLotto.calculateRank(lotto);
+            ranks.put(rank, ranks.getOrDefault(rank, 0) + 1);
+        }
+        return ranks;
     }
 
     public int size() {
