7f6d216cb1d0f12ea04f759fdfd8111596e0f72b
hanwool <hanwe92@gmail.com>
2024-10-31 09:32:51 +0900

    chore(InputNumberView): add and remove line break

--- src/main/java/lotto/view/InputNumberView.java
+++ src/main/java/lotto/view/InputNumberView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputNumberView {
-    public static String getInput(String question) {
 
+    public static String getInput(String question) {
         System.out.println(question);
         return readLine();
     }
