bdb4a48b9e23941aadbfc8bc212f46e9829009bb
minjun <ijmtylhtf@naver.com>
2024-10-31 10:50:21 +0900

    feat : 사용자 구입 금액 입력 메서드 구현

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -1,5 +1,12 @@
 package lotto.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
-	private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요."; 
+	private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+	public static String readPurchaseAmount() {
+		System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+		return Console.readLine();
+	}
 }
