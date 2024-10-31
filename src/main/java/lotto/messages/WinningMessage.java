d9ed53a844ed8fee836522286dfc08179d994c62
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-10-31 20:39:41 +0900

    refactor(enum) : FIRST~FIFTH 순서 변경

--- src/main/java/lotto/messages/WinningMessage.java
+++ src/main/java/lotto/messages/WinningMessage.java
@@ -1,11 +1,11 @@
 package lotto.messages;
 
 public enum WinningMessage {
-    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
-    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
+    FIFTH(3, 5000, "3개 일치 (5,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
     private final int matchCount;
     private final int winningAmount;
@@ -24,9 +24,7 @@ public enum WinningMessage {
     public int getMatchCount() {
         return matchCount;
     }
-    public int getWinningAmount() {
-        return winningAmount;
-    }
+
     public String getMessage() {
         return message;
     }
