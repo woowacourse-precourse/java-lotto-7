d41700f44de9e80b5d04f3afde9c47fefbb18b40
qyinm <qusseun@gmail.com>
2024-10-31 14:03:32 +0900

    test: 각 로또 당첨 등수 별 설명 출력 테스트

---
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoPrizeTest {
+
+    @Test
+    @DisplayName("당첨 Enum에 대해 각 등수별 설명이 제대로 나오는 지 테스트")
+    public void testLottoPrizeDescribe() {
+        // Given
+        String expectedEnumString = """
+                3개 일치 (5,000원)
+                4개 일치 (50,000원)
+                5개 일치 (1,500,000원)
+                5개 일치, 보너스 볼 일치 (30,000,000원)
+                6개 일치 (2,000,000,000원)""";
+
+        // When
+        String resultLottoPrizeToString = Arrays.stream(LottoPrize.values()).map(LottoPrize::toString)
+                .collect(Collectors.joining("\n"));
+
+        // Then
+        assertThat(resultLottoPrizeToString).isEqualTo(expectedEnumString);
+    }
+
+}
