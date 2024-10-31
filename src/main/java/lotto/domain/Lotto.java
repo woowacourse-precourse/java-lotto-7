5a1c8e8909de8925d2902a48ff7ad1d692e732f9
parksomii <somi020605@gmail.com>
2024-11-01 02:07:06 +0900

    feat(Lotto): 로또 번호 유효성 검사 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,39 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.util.Error;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(Error.LOTTO_SIZE_ERROR.message());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> setNumbers = new HashSet<>(numbers);
+        if (setNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(Error.LOTTO_DUPLICATE_ERROR.message());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(Error.LOTTO_RANGE_ERROR.message());
+            }
+        }
+    }
+
 }
