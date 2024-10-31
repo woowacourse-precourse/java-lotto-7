9e08bcbe9f25aa994b5009c116c90ef9a804c686
sinminseok12 <sinminseok12@naver.com>
2024-10-31 14:26:26 +0900

    feat(Ranking) : 당첨 결과 계산 기능 구현
    숫자 일치 갯수와 보너스 일치 여부로 몇등인지 계산해 Ranking 을 생성하는 기능을 구현했다.

---
+++ src/main/java/lotto/constants/Ranking.java
@@ -0,0 +1,34 @@
+package lotto.constants;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
+public enum Ranking {
+    FIRST(6, 2_000_000_000, (count, isBonus) -> count == 6),
+    SECOND(5, 30_000_000, (count, isBonus) -> count == 5 && isBonus),
+    THIRD(5, 1_500_000, (count, isBonus) -> count == 5 && !isBonus),
+    FOURTH(4, 50_000, (count, isBonus) -> count == 4),
+    FIFTH(3, 5_000, (count, isBonus) -> count == 3),
+    NONE(0, 0, (count, isBonus) -> count < 3);
+
+    private final int matchCount;
+    private final long reward;
+    private final BiPredicate<Integer, Boolean> isMatchFunction;
+
+    Ranking(int matchCount, long reward, BiPredicate<Integer, Boolean> isMatchFunction) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+        this.isMatchFunction = isMatchFunction;
+    }
+
+    public static Ranking of(int matchCount, boolean isBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.isMatch(matchCount, isBonus))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean isMatch(int count, boolean isBonus) {
+        return isMatchFunction.test(count, isBonus);
+    }
+}
