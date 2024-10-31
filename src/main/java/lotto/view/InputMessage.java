cf5ac178d18e150239680282c7fe3470321a220d
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-31 14:23:50 +0900

    feat: InputView와 InputMessage 구현

---
+++ src/main/java/lotto/view/InputMessage.java
@@ -0,0 +1,21 @@
+package lotto.view;
+
+public enum InputMessage {
+
+    INPUT_AMOUNT("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    ;
+
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
+
