fa5fd239ef56c601a7f9732fe380d30901ce86a0
dbdb456 <adad568@naver.com>
2024-10-31 16:12:15 +0900

    feat(outputview): 로또 수량 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,11 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
+
+    public void printLottoQuantity(List<Lotto> lottos) {
+        System.out.println(lottos.size()+"개를 구매했습니다.");
+    }
 }
