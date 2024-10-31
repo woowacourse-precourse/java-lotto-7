195d47fa8f29d214ec50c9ae306950bb72aa9e3c
hanwool <hanwe92@gmail.com>
2024-10-31 16:33:47 +0900

    chore(NumberParser): Add comment about deciding type of validation result

--- src/main/java/lotto/view/NumberParser.java
+++ src/main/java/lotto/view/NumberParser.java
@@ -13,6 +13,10 @@ public class NumberParser {
         for(String number: splitNumbers) {
             trimNumbers.add(number.trim());
         }
+        // 1. validation의 결과로 List<Integer>을 내려주는 것과
+        // 2. validation은 진짜 validation만 수행하는 것 중 무엇이 나은지 고민됨
+        // 1번으로 가면 Integer.parseInt()를 통해 별도 로직 없이 숫자 체크를 할 수 있음
+        // 다만, validation의 책임이 늘어나는 것처럼 보이기도 함
         NumberValidator.validateLottoNumberString(trimNumbers);
         return trimNumbers.stream().map(Integer::parseInt).toList();
     }
