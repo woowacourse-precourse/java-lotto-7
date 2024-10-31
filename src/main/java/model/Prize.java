5cebb6713222c9bb15f1be6a2100d0237d1acb95
cux-maks <volcano020716@gmail.com>
2024-10-31 16:37:24 +0900

    refactor(Prize): 등수 추가
    Prize에 등수를 추가하였습니다.

--- src/main/java/model/Prize.java
+++ src/main/java/model/Prize.java
@@ -1,21 +1,27 @@
 package model;
 
 public enum Prize {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000),
-    MISS(0, false, 0);
+    FIRST(1, 6, false, 2000000000),
+    SECOND(2, 5, true, 30000000),
+    THIRD(3, 5, false, 1500000),
+    FOURTH(4, 4, false, 50000),
+    FIFTH(5, 3, false, 5000),
+    MISS(-1, 0, false, 0);
 
+    private final int rank;
     private final int matchCount;
     private final boolean matchBonus;
     private final int prizeMoney;
 
-    Prize(int matchCount, boolean matchBonus, int prizeMoney) {
+    Prize(int rank, int matchCount, boolean matchBonus, int prizeMoney) {
+        this.rank = rank;
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prizeMoney = prizeMoney;
+    }
+
+    public int getRank() {
+        return rank;
     }
 
     public int getMatchCount() {
