9832d98b313bb2ce86ab0af3e87e0bd9eac86fdd
joshinhyeok <shtree07@naver.com>
2024-11-01 00:33:39 +0900

    feat: 로또 장수에 맞게 로또를 발행하는 기능 추가

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,9 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
     private static final int LOTTO_PRICE = 1000;
 
     public static int calculateLottoCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public static List<Lotto> generateLottos(int lottoTickets) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoTickets; i++) {
+            lottos.add(generateLottoNumbers());
+        }
+        return lottos;
+    }
+
+    private static Lotto generateLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
     }
 }
