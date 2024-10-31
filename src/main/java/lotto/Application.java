ba011b789d13a833e732d4fee87f93e5ae994498
seongje00416 <seongje00416@gmail.com>
2024-10-31 15:49:11 +0900

    feat: 랜덤 숫자 6개를 뽑는 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static List<Integer> selectRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange( 1, 45, 6 );
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
