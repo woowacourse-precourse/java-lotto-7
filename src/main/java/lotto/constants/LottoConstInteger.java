bc570f4576b0d47f5abf9d8b7b6a051101d3c4f3
whrjsgml0000 <whrjsgml4515@naver.com>
2024-10-31 23:11:47 +0900

    feat : 보너스 숫자 개수를 상수로 등록

--- src/main/java/lotto/constants/LottoConstInteger.java
+++ src/main/java/lotto/constants/LottoConstInteger.java
@@ -4,6 +4,8 @@ public enum LottoConstInteger {
     LOTTO_PRICE(1000),
     LOTTO_START_NUMBER(1),
     LOTTO_END_NUMBER(45),
+    LOTTO_WINNING_NUMBER_COUNT(6),
+    BONUS_NUMBER_COUNT(1),
 
     ;
     private final int value;
