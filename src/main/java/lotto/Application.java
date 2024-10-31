219d1ad7348a9f66eafb451ba5ce9199a9189924
Ferrorist <ferrorist@naver.com>
2024-10-31 23:46:46 +0900

    feat(Input IssueCost): 구입금액 입력 기능 구현, 발행 기능 구현
    사용자에게 구입금액을 입력 받은 후, 금액에 맞게 로또를 발행하는 기능을
    추가했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domains.Lotto;
+import lotto.service.LottoService;
+
 public class Application {
+
+    private static LottoService lottoService = new LottoService();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Lotto> lottos = setLottos();
+    }
+
+    private static List<Lotto> setLottos() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputValue = Console.readLine();
+
+        try {
+            int cost = Integer.parseInt(inputValue);
+            return lottoService.issueLotto(cost);
+        } catch (IllegalArgumentException exception) {
+            if (exception.getClass() == NumberFormatException.class) {
+                throw new IllegalArgumentException("[Error] 구입금액의 입력이 올바르지 않습니다. 입력 값: " + inputValue);
+            }
+            throw exception;
+        }
     }
 }
