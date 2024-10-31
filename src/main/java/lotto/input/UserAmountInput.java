9f356a14db6e36c56066e56c37f953efe47ab786
Spinichi <spinichi@naver.com>
2024-10-31 17:50:35 +0900

    feat: userAmountInput 메서드 변경
    userAmountInput에 과중되어있던 메서드를 분리하여 책임을 분리함, 또한 로또 티켓 가격을 상수로 분리하여 리터럴을 피함

--- src/main/java/lotto/input/UserAmountInput.java
+++ src/main/java/lotto/input/UserAmountInput.java
@@ -4,17 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.service.PrintTicketCount;
 
 public class UserAmountInput {
-    private String amountInput;
     private int amount;
     private int lottoTicketCount;
+    private final int TICKET_PRICE = 1000;
 
     public void userAmountInput() {
         while (true) {
             try {
-                userInput();
-                parseAmount();
+                getInputAndParse();
                 validate();
-                setLottoTicketCount();
+                lottoTicketCount = setLottoTicketCount();
+                System.out.println();
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("[ERROR] 입력이 잘못되었습니다. 로또 구입 금액은 숫자여야 합니다.");
@@ -33,23 +33,28 @@ public class UserAmountInput {
         System.out.printf("총 수익률은 %.1f%%입니다.", earningsRate);
     }
 
-    private void userInput() {
-        System.out.println("구입금액을 입력해 주세요.");
-        amountInput = Console.readLine();
+    private void getInputAndParse() {
+        String amountInput = userInput();
+        amount = parseAmount(amountInput);
     }
 
-    private void parseAmount() {
-        amount = Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
+    private String userInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    private int parseAmount(String amountInput) {
+        return Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
     }
 
     private void validate() {
-        if (amount <= 0 || amount % 1000 != 0) {
+        if (amount <= 0 || amount % TICKET_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 입력이 잘못되었습니다. 로또 구입 금액은 천 원단위의 양수여야 합니다.");
         }
     }
 
-    private void setLottoTicketCount() {
-        lottoTicketCount = amount / 1000;
+    private int setLottoTicketCount() {
+        return amount / TICKET_PRICE;
     }
 }
 
