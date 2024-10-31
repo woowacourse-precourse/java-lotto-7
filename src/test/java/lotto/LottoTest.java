b7aeb0d4a90323dd5c6af352e979e3844806ce01
ehgur062300 <ehgur062300@gmail.com>
2024-10-31 17:30:42 +0900

    test(LottoTest): Update test method for lotto number validate check

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,17 +1,32 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidLottoNumbers")
+    void 로또_번호의_개수가_기준과_다르면_예외가_발생한다(List<Integer> lottoNumbers) {
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<Integer>> provideInvalidLottoNumbers() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5, 6, 7), // 7개
+                List.of(1, 2, 3),             // 3개
+                List.of(1, 2),                // 2개
+                List.of()                     // 0개
+        );
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -21,5 +36,5 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
 }
