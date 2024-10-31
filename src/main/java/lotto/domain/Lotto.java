374ace8c3f5e694de8af53da795a63cd98ea2cf2
taek2222 <oot309@daum.net>
2024-11-01 03:31:37 +0900

    feat(Lotto): 로또 번호 정보 반환 기능 구현
    - 로또 출력을 위해, 번호를 구분자와 함께 반환하는 메서드를 구현했습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final String NUMBER_DELIMITER = ", ";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +13,17 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public String createNumberInfo() {
+        return numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(NUMBER_DELIMITER));
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
     // TODO: 추가 기능 구현
 }
