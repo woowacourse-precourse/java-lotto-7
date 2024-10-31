eff7fc5cdecfc4f8ab9c8d30878a1a7df7710a67
StoneCAU <seokwoo1999@naver.com>
2024-11-01 00:09:39 +0900

    chore(Application): LottoService 의존성 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,15 @@
 package lotto;
 
 import lotto.controller.LottoGameController;
+import lotto.service.BuyerService;
+import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        LottoGameController lottoGameController = new LottoGameController();
+        final BuyerService buyerService = new BuyerService();
+        final LottoService lottoService = new LottoService();
+
+        LottoGameController lottoGameController = new LottoGameController(buyerService, lottoService);
         lottoGameController.run();
     }
 }
