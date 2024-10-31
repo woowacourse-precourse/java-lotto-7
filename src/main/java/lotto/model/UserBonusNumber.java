5909868b460d5cae763518a2445602b1bb56b88a
Zrfv <youngoh07015@gmail.com>
2024-10-31 12:45:00 +0900

    feat: 입력받은 보너스 번호를 저장하는 기능 구현

---
+++ src/main/java/lotto/model/UserBonusNumber.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class UserBonusNumber {
+    private final int bonus;
+
+    public UserBonusNumber(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public int get() {
+        return bonus;
+    }
+}
