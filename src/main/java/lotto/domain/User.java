e9d9563168187d17c31ba7e2cfc6e4f0b418c9d2
zzzyoonnn <dwsrh03@gmail.com>
2024-10-31 15:18:49 +0900

    feat: 로또 구입 금액을 저장할 setMoney 구현

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -4,13 +4,21 @@ import java.util.List;
 import lotto.Lotto;
 
 public class User {
-    private String input;
+    private String money;
     private int numOfLottos;
     private List<Lotto> lottos;
 
-    public User(String input, int numOfLottos, List<Lotto> lottos) {
-        this.input = input;
+    public User() {
+
+    }
+
+    public User(String money, int numOfLottos, List<Lotto> lottos) {
+        this.money = money;
         this.numOfLottos = numOfLottos;
         this.lottos = lottos;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
     }
 }
