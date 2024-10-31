9ff4d377a3329c17c0a56b97915e4246ce49d186
Hyso <hsk7953@gmail.com>
2024-10-31 21:30:43 +0900

    feat(ExceptionMessage) : 당첨 번호 입력에 관한 에러 메시지 추가

--- src/main/java/lotto/enums/ExceptionMessage.java
+++ src/main/java/lotto/enums/ExceptionMessage.java
@@ -6,7 +6,8 @@ public enum ExceptionMessage {
     INVALID_ZERO_START("0으로 시작하는 숫자는 입력할 수 없습니다."),
     INVALID_NON_NUMERIC("숫자외에 문자는 입력할 수 없습니다."),
     DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
-    OUT_OF_RANGE("1부터 45 사이의 숫자만 입력 가능합니다.");
+    OUT_OF_RANGE("1부터 45 사이의 숫자만 입력 가능합니다."),
+    INVALID_WINNING_NUMBER_FORMAT("당첨 번호는 숫자와 쉼표만 사용하여 입력해 주세요. (예: 1,2,3,4,5,6)");
 
     private static final String ERROR_FORMAT = "[ERROR] ";
     private final String message;
