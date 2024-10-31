88cbb59d205e3cc7c1c838c7fdfa131a08050669
dongyun <ehddbs452100@naver.com>
2024-10-31 19:33:43 +0900

    feat(InputView, LottoBonus, Message): input bonus number
    - Message enum 클래스를 통해 BONUS_NUMBER_MESSAGE를 통해 보너스 번호 안내 메시지 전달
    - 보너스 번호를 입력받고 정수로 변환 할 수 있는지 검증
    - 정수로 변환 할 수 없으면 NumberFormatException 오류를 생성하여 Error의 NOT_INTEGER 메시지를
    전달하여 보너스 번호 재입력 받기
    - 보너스 번호가 우승 번호들과 중복일시 IllegalArgumentException 오류를 생성하여 Error의
    LOTTO_NUMBER_IS_DUPLICATED 메시지를 전달하여 보너스 번호 재입력 받기
    - 보너스 번호가 1과 46 사이의 번호가 아닐 시 IllegalArgumentException 오류를 생성하여 Error의
    LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46 메시지를 전달하여 보너스 번호 재입력 받기

--- src/main/java/lotto/global/Message.java
+++ src/main/java/lotto/global/Message.java
@@ -4,7 +4,8 @@ public enum Message {
 
     BUY_LOTTO_MESSAGE("지불액을 입력해 주세요."),
     BUY_LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
-    WINNING_NUMBER_MESSAGE("\n당첨 번호를 입력해 주세요.");
+    WINNING_NUMBER_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_MESSAGE("\n보너스 번호를 입력해 주세요.");
 
 
     private String msg;
