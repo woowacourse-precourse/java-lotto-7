5c94f8667d6b87c7cfade00672bab3f6d246ba8c
82103 <sangjoon12345@gmail.com>
2024-10-31 22:02:03 +0900

    feat: 로또 구매 입력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import lotto.Validator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    private final Validator validator;
+
+    public InputView(Validator validator){
+        this.validator = validator;
+    }
+
+    public int getLottoPrice(){
+        while(true){
+            try{
+                String input = readLine();
+                validator.isValidPrice(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR]" + e);
+            }
+        }
+    }
+
+
+}
