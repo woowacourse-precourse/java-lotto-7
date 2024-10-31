4c5aef6cc997b4e2ea59e5d9ee1428d98ee279bb
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-31 18:25:26 +0900

    feat: LottoMachine 과 LottoProgram 작성

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import lotto.domain.lotto.Lotto;
+
+import java.util.List;
+
+public class LottoMachine {
+
+    private List<Lotto> lottos;
+    private LottoProgram lottoProgram;
+
+
+    public LottoMachine() {
+    }
+
+
+}
