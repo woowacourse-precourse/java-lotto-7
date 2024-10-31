60304ab5429f60b46a36a51d608b2dee31171459
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-31 17:30:17 +0900

    feat: NumberCombination을 상속하는 LottoAnswer 클래스 생성

---
+++ src/main/java/lotto/domain/lotto/LottoAnswer.java
@@ -0,0 +1,20 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoAnswer extends NumberCombination {
+
+    private final List<Number> answerNumbers;
+
+    public LottoAnswer(List<Integer> answerNumbers) {
+        validateDuplicate(answerNumbers);
+        validateLength(answerNumbers);
+
+        this.answerNumbers = answerNumbers.stream()
+                .sorted()
+                .map(Number::new)
+                .collect(Collectors.toList());
+    }
+
+}
