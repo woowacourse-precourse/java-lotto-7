14939492d28d58fe3eb4d858af83829a04fcc4fe
Hyungjung Kim <gudwnd1113@gmail.com>
2024-10-31 17:13:11 +0900

    feat(Lotto): implement Lotto winning statistical calculation feature
    로또 당첨 통계 계산 기능 구현.
    로또 번호와 당첨 번호 및 보너스 번호로 당첨 여부를 계산하는
    Lotto.checkLottoWin 함수 정의.
    로또 당첨 통계 저장 및 수익 계산을 위한 LottoWinner enum 클래스 정의.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,35 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+enum LottoWinner {
+    FIRST(2_000_000_000, 0),
+    SECOND(30_000_000, 0),
+    THIRD(1_500_000, 0),
+    FOURTH(50_000, 0),
+    FIFTH(5_000, 0),
+    NONE(0, 0);
+
+    private final int prize;
+    private int count;
+
+    LottoWinner(int prize, int count) {
+        this.prize = prize;
+        this.count = count;
+    }
+    
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+}
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -67,5 +96,31 @@ public class Lotto {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         numbers.sort(Comparator.naturalOrder());
         return new Lotto(numbers);
+    }
+
+    public void checkLottoWin(List<Integer> winningNumbers, int bonusNumber) {
+        int correct = 0;
+
+        for (int number : this.numbers) {
+            if (winningNumbers.contains(number)) {
+                correct++;
+            }
+        }
+        if (correct == 6) {
+            LottoWinner.FIRST.incrementCount();
+        }
+        if (correct == 5) {
+            if (this.numbers.contains(bonusNumber)) {
+                LottoWinner.SECOND.incrementCount();
+            }
+            LottoWinner.THIRD.incrementCount();
+        }
+        if (correct == 4) {
+            LottoWinner.FOURTH.incrementCount();
+        }
+        if (correct == 3) {
+            LottoWinner.FIFTH.incrementCount();
+        }
+        LottoWinner.NONE.incrementCount();
     }
 }
