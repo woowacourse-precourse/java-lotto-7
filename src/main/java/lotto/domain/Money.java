ea11d653bf125fcb5bbf8b2d0936da6f15f52211
himinqq <alsrud0966@naver.com>
2024-10-31 13:46:36 +0900

    feat(Money): 로또 구입 금액이 1000원 단위가 아니면 예외 발생시키는 기능 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public class Money {
+    private final int paymentAmount;
+
+    public Money(int paymentAmount) {
+        validateMoneyUnit(paymentAmount);
+        this.paymentAmount = paymentAmount;
+    }
+
+    private void validateMoneyUnit(int money){
+        if(money % LotteryMachine.LOTTO_PRICE != 0){
+            throw new IllegalArgumentException("[ ERROR ] 로또는 1000원 단위로 구매가능합니다.");
+        }
+    }
+
+}
