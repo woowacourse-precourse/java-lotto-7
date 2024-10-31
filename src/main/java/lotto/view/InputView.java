bf136a348c4b8227e7c080836fd041c8bebbfb1a
dnjswns98 <tngkrt2004@gmail.com>
2024-10-31 21:27:59 +0900

    feat(InputView): Input the winning numbers separated by commas

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.CustomLottoException;
 import lotto.exception.ErrorMessage;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InputView {
     public static int inputLottoAmount(){
         String inputAmount = Console.readLine();
         return convertInputAmountToInt(inputAmount);
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        String inputWinningNumbers = Console.readLine();
+        return convertInputWinningNumbersToIntegerList(inputWinningNumbers);
     }
 
     private static int convertInputAmountToInt(String inputAmount) {
@@ -33,5 +42,33 @@ public class InputView {
         if (intAmount != longAmount) {
             throw new CustomLottoException(ErrorMessage.NOT_INTEGER_NUMBER);
         }
+    }
+
+    private static List<Integer> convertInputWinningNumbersToIntegerList(String inputWinningNumbers) {
+        try {
+            List<Long> winningNumbers =
+                    Stream.of(inputWinningNumbers.split(","))
+                            .map(Long::parseLong)
+                            .collect(Collectors.toList());
+            validateNumberEquality(winningNumbers);
+            return convertWinningNumbersToInteger(winningNumbers);
+        } catch (NumberFormatException exception) {
+            throw new CustomLottoException(ErrorMessage.NOT_NUMBER);
+        }
+    }
+
+    private static void validateNumberEquality(List<Long> numbers) {
+        for (long number : numbers) {
+            int intNumber = (int)number;
+            if (number != intNumber) {
+                throw new CustomLottoException(ErrorMessage.NOT_INTEGER_NUMBER);
+            }
+        }
+    }
+
+    private static List<Integer> convertWinningNumbersToInteger(List<Long> winningNumbers) {
+        return winningNumbers.stream()
+                .map(Long::intValue)
+                .collect(Collectors.toList());
     }
 }
