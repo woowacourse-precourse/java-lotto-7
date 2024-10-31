5570bab7150cf9cdda6773bba6f1a86e184cd572
김연진 <love_homebody@naver.com>
2024-10-31 20:43:23 +0900

    feat(InputValidator): 숫자가 아닌 문자가 입력될 경우 예외 발생

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,13 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.PurchaseAmount;
+import lotto.validator.InputValidator;
 
 public class InputView {
 
     public PurchaseAmount readPurchasePrice() {
         String input = Console.readLine();
+        InputValidator.validateNumber(input);
         return new PurchaseAmount(Integer.parseInt(input));
     }
 }
