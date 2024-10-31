e08b9788d6e9304fd0de38f8ecff5b27e25c8120
dk7648 <dk7648@korea.ac.kr>
2024-10-31 13:24:51 +0900

    feat: 로또를 구입하는 기능 추가

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -1,7 +1,14 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
     private final int PRICE_OF_LOTTO = 1000;
+    private List<Lotto> lottos;
+    public void buyLotto(Store store, Money money) {
+        lottos = store.sellLottos(money, getBuyableCount(money));
+    }
     public int getBuyableCount(Money money) {
         int leftMoney = money.getLeftMoney();
         return leftMoney / PRICE_OF_LOTTO;
