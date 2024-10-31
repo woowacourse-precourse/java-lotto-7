a4da12f482155d58f66ab4c719b9b3311fe398f0
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 01:19:43 +0900

    refactor: 난수를 정렬하는 메서드 구현

--- src/main/java/lotto/model/RandomNumbers.java
+++ src/main/java/lotto/model/RandomNumbers.java
@@ -1,11 +1,17 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomNumbers {
     public RandomNumbers(){
+    }
 
+    public static List<Integer> generateSortedLottoNumbers(){
+        List<Integer> randomNumbers = randomNumberGenerator();
+        Collections.sort(randomNumbers);
+        return randomNumbers;
     }
     public static List<Integer> randomNumberGenerator(){
         return Randoms.pickUniqueNumbersInRange(1,45,6);
