7e5ab7792761d3ba85abdd0151f40a376985e04a
hwangdaesun <hds650588@gmail.com>
2024-11-01 00:45:12 +0900

    refactor : REGEXP_NUMBER에서 MONEY_NUMBER_REGEX로 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
 
     public static final InputView INSTANCE = new InputView();
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String REGEXP_NUMBER = "^[1-9][0-9]*$";
+    public static final String MONEY_NUMBER_REGEX = "^[1-9][0-9]*$";
 
     private InputView() {
     }
