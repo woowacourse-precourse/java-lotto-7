63d405996ccc0ab783e938b011b278d95a58ab0a
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 01:13:42 +0900

    refactor: OutputView에서 사용자 입력 관련된 문자열과 메서드 삭제

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
-    public static final String INPUT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     public static final String INPUT_AGAIN = "다시 입력해 주세요.";
     public static final String PURCHASED_TICKETS = "개를 구매했습니다.";
-    public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     public static final String COUNT = "개";
 
     public static final String MATCH_3_PRIZE = "3개 일치 (5,000원) - ";
@@ -18,21 +15,14 @@ public class OutputView {
     public static final String MATCH_6_PRIZE = "6개 일치 (2,000,000,000원) - ";
     public static final String TOTAL_PROFIT_RATE = "총 수익률은 %.1f%%입니다.%n";
 
-    public static void printInputMoneyPrompt(){
-        System.out.println(INPUT_PURCHASE_AMOUNT);
-    }
+
     public static void printInputAgainPrompt(){
         System.out.println(INPUT_AGAIN);
     }
     public static void printPurchasedTicketCount(int amount){
         System.out.println(amount + PURCHASED_TICKETS);
     }
-    public static void printInputWinningNumbersPrompt(){
-        System.out.println(INPUT_WINNING_NUMBERS);
-    }
-    public static void printInputBonusNumberPrompt(){
-        System.out.println(INPUT_BONUS_NUMBER);
-    }
+
     public static void printPurchasedLottoNumbers(ArrayList<List<Integer>> purchasedLottoNumbers){
         for (List<Integer> purchasedLottoNumber : purchasedLottoNumbers) {
             System.out.println(purchasedLottoNumber);
