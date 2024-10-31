21dc97856185fb2066244d2bf2b6f4761aa40fd8
kungbi <kipper12335@gmail.com>
2024-10-31 16:57:48 +0900

    feat(LottoMachine): 여러 getter함수와, 로또 생성 함수 구현

--- src/main/java/lotto/entity/LottoMachine.java
+++ src/main/java/lotto/entity/LottoMachine.java
@@ -1,13 +1,33 @@
 package lotto.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
     private final Purchase purchase;
     private final WinningNumbers winningNumbers;
+    private final List<Lotto> purchasedLottos;
 
-    public LottoMachine(int paymentAmount, List<Integer> numbers, int bonusNumber) {
+    public LottoMachine(int paymentAmount, List<Integer> winningMainNumbers, int winningBonusNumber) {
         this.purchase = new Purchase(paymentAmount);
-        this.winningNumbers = new WinningNumbers(numbers, bonusNumber);
+        this.winningNumbers = new WinningNumbers(winningMainNumbers, winningBonusNumber);
+        this.purchasedLottos = createLottos();
     }
+
+    public int getPurchaseCount() {
+        return purchase.calculateTicketCount();
+    }
+
+    public int getPurchasedAmount() {
+        return purchase.getAmount();
+    }
+
+    private List<Lotto> createLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchase.calculateTicketCount(); i++) {
+            lottos.add(Lotto.createRandomLotto());
+        }
+        return lottos;
+    }
+
 }
