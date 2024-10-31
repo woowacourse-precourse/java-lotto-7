b00bfa9cd3f8f0ceb984e7225312fb7bdf930ed8
reindeer002 <lsj000424@gmail.com>
2024-10-31 16:13:14 +0900

    feat(story1.1): add constants class
    - register in common as it is used by LottoMachine and Lotto objects

---
+++ src/main/java/lotto/common/Constants.java
@@ -0,0 +1,7 @@
+package lotto.common;
+
+public class Constants {
+    public static final int LOTTO_NUMBER_MAX_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+}
