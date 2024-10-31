ae1e28556653b0f3f52cdebb68cb935a890b01a0
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-31 19:31:29 +0900

    feat: Lottos 구현

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,11 +1,26 @@
 package lotto.domain.lotto;
 
+import lotto.domain.lottofactory.LottoFactory;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lottos {
 
+    private int lottoCount;
     private List<Lotto> lottos;
 
 
+    public Lottos(int lottoCount) {
+        this.lottoCount = lottoCount;
+    }
+
+    public void makeLottos(LottoFactory lottoFactory) {
+        List<Lotto> lottos = Stream.generate(lottoFactory::create)
+                .limit(lottoCount)
+                .collect(Collectors.toList());
+    }
 
 }
