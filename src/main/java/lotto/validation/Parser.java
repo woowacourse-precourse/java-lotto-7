1078620be558829d9a7dad6c7ffcc6b7d38161d5
(minjae8563) <(minjae8563@naver.com)>
2024-10-31 17:30:24 +0900

    refactor: 사용자의 입력을 Integer 형으로 변환하는 로직 분리하여 클래스 생성

---
+++ src/main/java/lotto/validation/Parser.java
@@ -0,0 +1,15 @@
+package lotto.validation;
+
+import static lotto.ErrorMessage.NON_NUMERIC_INPUT;
+
+public class Parser {
+    public Parser(){
+        
+    }
+    public int stringToInt(String input){
+        if (!input.matches("^[0-9]*$")){
+            throw new IllegalArgumentException(NON_NUMERIC_INPUT.format());
+        }
+        return Integer.parseInt(input);
+    }
+}
