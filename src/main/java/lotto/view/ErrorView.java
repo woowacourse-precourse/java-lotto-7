e3f90dca3720e7b222898690a38a1b3ded31766f
IM-GYURI <kgulr0517@gmail.com>
2024-11-01 05:02:17 +0900

    feat : ErrorView 기능 추가 및 수정
    - 에러 메세지 상수를 ErrorMessage에서 받아 쓰도록 수정
    - printWinningNumbersParsingError()
    - printBonusNumberParsingError()

--- src/main/java/lotto/view/ErrorView.java
+++ src/main/java/lotto/view/ErrorView.java
@@ -1,12 +1,21 @@
 package lotto.view;
 
+import lotto.exception.ErrorMessage;
+
 public class ErrorView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String MONEY_PARSING_ERROR_MESSAGE = "구입 금액은 2,147,483,000원 이하의 숫자여야 합니다.";
 
     public void printMoneyParsingError() {
-        System.out.println(ERROR_PREFIX + MONEY_PARSING_ERROR_MESSAGE);
+        System.out.println(ERROR_PREFIX + ErrorMessage.MONEY_PARSING_ERROR_MESSAGE.getMessage());
+    }
+
+    public void printWinningNumbersParsingError() {
+        System.out.println(ERROR_PREFIX + ErrorMessage.WINNING_NUMBERS_PARSING_ERROR_MESSAGE.getMessage());
+    }
+
+    public void printBonusNumberParsingError() {
+        System.out.println(ERROR_PREFIX + ErrorMessage.BONUS_NUMBER_PARSING_ERROR_MESSAGE.getMessage());
     }
 
     public void printErrorMessage(String errorMessage) {
