81752b1f4b918586a9ec8201306a4a478a82234a
corpi <lj845854@gmail.com>
2024-10-31 19:50:24 +0900

    feat(Application): 로또 머신 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoMachine lottoMachine = new LottoMachine(new InputView(), new OutputView());
+        lottoMachine.run();
     }
 }
