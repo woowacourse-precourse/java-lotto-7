4e0c695fcefdeb9aed1fbd1bd3581c44e9d7e41c
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-01 02:20:12 +0900

    refactor(test) : 테스트 메서드 이름 변경

--- src/test/java/lotto/validator/PurchasePriceValidatorTest.java
+++ src/test/java/lotto/validator/PurchasePriceValidatorTest.java
@@ -9,42 +9,42 @@ class PurchasePriceValidatorTest {
 
     @Test
     @DisplayName("입력 안했을 때")
-    void noInput() {
+    void noInputTest() {
         assertThat(PurchasePriceValidator.validate("")).isFalse();
     }
 
     @Test
     @DisplayName("공백을 입력했을 때")
-    void blankInput() {
+    void blankInputTest() {
         assertThat(PurchasePriceValidator.validate(" ")).isFalse();
     }
 
     @Test
     @DisplayName("1000원보다 낮은 가격 입력했을 때")
-    void lowerThanAtLeast() {
+    void lowerThanAtLeastTest() {
         assertThat(PurchasePriceValidator.validate("900")).isFalse();
     }
 
     @Test
     @DisplayName("int 자료형 범위 밖의 값을 입력했을 때")
-    void overflow() {
+    void overflowTest() {
         assertThat(PurchasePriceValidator.validate("10000000000")).isFalse();
     }
 
     @Test
     @DisplayName("1000으로 나눠지지 않는 수 입력했을 때")
-    void cantBeDivided() {
+    void cantBeDividedTest() {
         assertThat(PurchasePriceValidator.validate("1500")).isFalse();
     }
 
     @Test
     @DisplayName("숫자가 아닌 값을 입력했을 때")
-    void notANumber() {
+    void notANumberTest() {
         assertThat(PurchasePriceValidator.validate("1,")).isFalse();
     }
     @Test
     @DisplayName("똑바로 된 값을 입력했을 때")
-    void validate() {
+    void validateTest() {
         assertThat(PurchasePriceValidator.validate("1000")).isTrue();
     }
 }
