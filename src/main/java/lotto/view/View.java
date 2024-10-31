ff8b9f2d279bfc26bf6eee82e5e60cbb130c1822
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-31 14:15:38 +0900

    feat($view) : Add getting bonus number Input function

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -12,5 +12,10 @@ public class View {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
-    
+
+    public String getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
 }
