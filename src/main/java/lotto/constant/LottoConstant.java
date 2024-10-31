8589ca6dcc689cb5aa434ea681995b9b5a231a64
ChoiDongJoon <adong0808@naver.com>
2024-10-31 14:47:20 +0900

    feat(lotto): 보너스 넘버 체크를 위한 기준 상수로 추가
    보너스 넘버 체크를 위한 기준 상수로 추가

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -4,4 +4,5 @@ public class LottoConstant {
 
     public static final int LOTTO_LENGTH = 6;
     public static final int LOTTO_PRICE = 1000;
+    public static final int BONUS_NUMBER_CHECK_CRITERIA = 5;
 }
