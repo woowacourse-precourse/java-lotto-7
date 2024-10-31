8a80ce6b3e85270e7d880a6024016d1b8b3267a9
sc@t0a14 <gusdn6671@gmail.com>
2024-11-01 06:22:26 +0900

    feat(Controller): 당첨 번호 입력 및 파싱 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,21 +3,31 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoTickets;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Controller {
     private Lotto lotto;
 
-    public void run(){
+    public void run() {
         String inputMoney = InputView.promptPurchaseAmount();
         int lottoCount = Integer.parseInt(inputMoney) / 1000;
         OutputView.printPurchaseCount(lottoCount);
+        LottoTickets lottoTickets = LottoTickets.generateLottoTickets(lottoCount);
 
-        List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            lottoTickets.add(lotto.generateLotto());
+        List<List<Integer>> lottoNumbers = lottoTickets.getLottoTickets();
+        OutputView.printLottoTickets(lottoNumbers);
+
+        String inputWinningNumbers = InputView.promptPurchaseWinningNumber();
+        String[] inputsWinningNumbers = inputWinningNumbers.split(",");
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (int i = 0; i < inputsWinningNumbers.length; i++) {
+            winningNumbers.add(Integer.parseInt(inputsWinningNumbers[i]));
         }
+
+
 
     }
 }
