f0c9d0ad587de8ea4a3c506d0fe884ac7bf41a53
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-10-31 22:12:06 +0900

    feat(lottocontroller) : run 메서드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -15,6 +15,22 @@ public class LottoController {
         this.lottoView = lottoView;
     }
 
+    public void run() {
+        int purchaseAmount = lottoView.getInputPurchaseAmount();
+        List<Lotto> lottos = lottoMachine.generateLottos(purchaseAmount / 1000);
+        lottoView.displayLottos(lottos);
+
+        List<Integer> winningsNumbers = lottoView.getInputWinningNumbers();
+        lottoMachine.setWinningLotto(winningsNumbers);
+
+        int bonusNumber = lottoView.getInputBonusNumber();
+        lottoMachine.setBonusNumbers(bonusNumber);
+
+        List<Rank> results = lottoMachine.checkResults();
+        double profitRate = calculateProfitRate(results, purchaseAmount);
+        lottoView.displayResults(results, profitRate);
+    }
+
     private double calculateProfitRate(List<Rank> results, int purchaseAmount) {
         int totalPrize = results.stream().mapToInt(Rank::getPrize).sum();
         return (double) totalPrize / purchaseAmount * 100;
