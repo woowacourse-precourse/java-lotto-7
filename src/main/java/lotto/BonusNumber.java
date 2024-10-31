cf8d284af9d341748b5e7475dc53505d79eebc14
hyosang <billy04100410@gmail.com>
2024-11-01 00:47:40 +0900

    feat(BonusNumver): BonusNumber 도메인 클래스를 추가

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.validator.BonusNumberValidator;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+    private final BonusNumberValidator validator;
+
+    public BonusNumber(int bonusNumber, Lotto lotto, BonusNumberValidator validator) {
+        this.validator = validator;
+        validator.validate(bonusNumber, lotto);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
