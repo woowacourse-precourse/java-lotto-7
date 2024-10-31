f37a5634e9ae19ed0cbce54a1c7147b712aa456f
sleepyhoon <pokjm99@gmail.com>
2024-10-31 17:20:39 +0900

    feat (MoneyValidator) : 에러 메시지 [ERROR] 추가

--- src/main/java/lotto/MoneyValidator.java
+++ src/main/java/lotto/MoneyValidator.java
@@ -6,10 +6,10 @@ import static lotto.MoneyUtil.MINIMUM_PRICE;
 public class MoneyValidator {
     public static void validateInputMoney(long userInputMoney) {
         if(userInputMoney % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("1000원 단위로만 입력해야 합니다");
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 입력해야 합니다");
         }
         if(userInputMoney < MINIMUM_PRICE) {
-            throw new IllegalArgumentException("0원 이상 결제해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 0원 이상 결제해야 합니다.");
         }
     }
 }
