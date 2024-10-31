19c26f28a3fdb079f10705e725972607e593cd80
qyinm <qusseun@gmail.com>
2024-10-31 13:19:16 +0900

    refactor: 서비스 레이어 삭제 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,10 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        LottoService lottoService = new LottoService();
-        LottoController controller = new LottoController(lottoService);
+        LottoController controller = new LottoController();
 
         controller.runLotto();
     }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,25 +3,20 @@ package lotto.controller;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPrice;
-import lotto.service.LottoService;
+import lotto.domain.LottoTickets;
 import lotto.utils.LottoExceptionUtils;
+import lotto.utils.LottoGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
-    private final LottoService lottoService;
-
-    public LottoController(LottoService lottoService) {
-        this.lottoService = lottoService;
-    }
-
     public void runLotto() {
-        LottoPrice lottoPurchaseAmount = LottoExceptionUtils.runUntilNoneLottoException(
-                InputView::inputLottoPrice);
-        Lotto wonLotto = LottoExceptionUtils.runUntilNoneLottoException(
-                InputView::wonLottoNumbers);
+        LottoPrice lottoPurchaseAmount = LottoExceptionUtils.runUntilNoneLottoException(InputView::inputLottoPrice);
+        LottoTickets lottoTickets = LottoGenerator.generateLottoTickets(lottoPurchaseAmount.getCanPurchaseLottoCount());
+        OutputView.printLottoTickets(lottoTickets);
+
+        Lotto wonLotto = LottoExceptionUtils.runUntilNoneLottoException(InputView::wonLottoNumbers);
         BonusNumber bonusNumber = LottoExceptionUtils.runUntilNoneLottoException(InputView::inputBonusNumber);
-
-
     }
 }
