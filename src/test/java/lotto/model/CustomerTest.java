c8cdd07c40485226557f934b2160d8bc0f6f4dde
Jin409 <sualng123@gmail.com>
2024-11-01 02:32:04 +0900

    style: 사용되지 않는 import 문을 제거해라

--- src/test/java/lotto/model/CustomerTest.java
+++ src/test/java/lotto/model/CustomerTest.java
@@ -4,10 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
 
 import java.util.List;
-import lotto.model.Customer;
-import lotto.model.Lotto;
-import lotto.model.LottoTicket;
-import lotto.model.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
