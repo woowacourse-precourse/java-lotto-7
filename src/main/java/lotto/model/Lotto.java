09d996b707662a0bbc4ef0b7331743e4771b83e8
kim <hwiyul_g@daum.net>
2024-10-31 16:17:01 +0900

    feat(Lotto): Lotto 패키지 이동 및 로또 검증 로직 추가

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import lotto.utils.ExceptionMessage;
+
+public class Lotto {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateDuplicateNumbers(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_DUPLICATED_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers){
+        Predicate<Integer> isOutOfRange = number -> number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER;
+        if(numbers.stream().anyMatch(isOutOfRange)){
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+        }
+    }
+
+}
