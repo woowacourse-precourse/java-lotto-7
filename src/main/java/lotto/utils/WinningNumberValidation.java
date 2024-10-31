99abf89241b51a4383aeadf575e3c1c0dba8c3c2
Teddysir <sdzx0719@naver.com>
2024-10-31 18:34:43 +0900

    feat: 당첨 번호 입력시 예외 처리
    당첨 번호 입력시 예외 상황
    1. 당첨 번호의 개수가 6개가 아닐 경우
    2. 당첨 번호의 범위가 1~45가 아닐 경우
    3. 당첨 번호의 형식이 올바르지 않을 경우

---
+++ src/main/java/lotto/utils/WinningNumberValidation.java
@@ -0,0 +1,29 @@
+package lotto.utils;
+
+import lotto.constants.errorType.WinningNumberErrorType;
+
+import java.util.List;
+
+public class WinningNumberValidation {
+
+    public static void validateNumberCount(List<Integer> winningNumbers) {
+        if(winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(WinningNumberErrorType.INVALID_WINNING_NUMBERS_COUNT.getMessage());
+        }
+    }
+
+    public static void validateNumberRange(int winningNumber) {
+        if(winningNumber < 1 || winningNumber > 45) {
+            throw new IllegalArgumentException(WinningNumberErrorType.INVALID_WINNING_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    public static int validateNumberFormat(String winningNumber) {
+        try {
+            return Integer.parseInt(winningNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(WinningNumberErrorType.INVALID_WINNING_NUMBER_FORMAT.getMessage());
+        }
+    }
+
+}
