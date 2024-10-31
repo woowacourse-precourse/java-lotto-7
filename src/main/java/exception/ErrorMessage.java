9246519754731f891e530715917a064b4c2e5582
dudxo <father8475@naver.com>
2024-11-01 00:30:25 +0900

    feat : 에러 메시지
    - 보너스 번호가 당첨 번호에 포함 될 때 출력되는 에러 메시지를 작성한다.

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     PURCHASE_AMOUNT_INVALID_UNIT_MSG("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
     NOT_NUMBER_FORMAT("[ERROR] 숫자로 입력해야 합니다."),
     DUPLICATE_WINNING_NUMBERS("[ERROR] 당첨 번호는 중복되지 않는 숫자여야 합니다."),
-    NOT_SIX_WINNING_NUMBERS("[ERROR] 당첨 번호는 6자리여야 합니다.");
+    NOT_SIX_WINNING_NUMBERS("[ERROR] 당첨 번호는 6자리여야 합니다."),
+    BONUS_NUMBER_IN_WINNING_NUMBERS("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
 
     private final String message;
 
