b1e12b085f44481985856dda56f9faeac0d106ea
Jewan1120 <hawaii1468@gmail.com>
2024-11-01 00:31:27 +0900

    refactor(Validator): 재사용 가능성 있는 로직 분리
    다른 유효성 검사에서 사용될 수 있는 로직을 따로 분리했습니다.

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -3,14 +3,22 @@ package lotto.validator;
 public class LottoValidator {
 
     public static void validateInputMoney(String inputMoney) {
-        if (inputMoney == null || inputMoney.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        if (!inputMoney.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
-        }
+        isEmpty(inputMoney);
+        isDigit(inputMoney);
         int money = Integer.parseInt(inputMoney);
         if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isEmpty(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isDigit(String inputMoney) {
+        if (!inputMoney.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException();
         }
     }
