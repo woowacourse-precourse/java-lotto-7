5b40a7be9a6d463ac355a5eeffc02e319e148144
dnwldla <leeemjw@naver.com>
2024-10-31 09:10:19 +0900

    feat: 입력한 금액으로 몇개의 로또를 샀는지 나타내는 멤버변수 추가

--- src/main/java/lotto/domain/PaymentInput.java
+++ src/main/java/lotto/domain/PaymentInput.java
@@ -6,13 +6,23 @@ import lotto.common.ErrorMessage;
 
 public class PaymentInput {
     private static final int UNIT=1000;
-
     int payment;
+    int lottoCounts;
+
+    public int getLottoCounts() {
+        return lottoCounts;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
 
     public PaymentInput(String input){
         CommonValidator.validateNullAndBlank(input);
-        payment=CommonValidator.validateInteger(input);
+        this.payment=CommonValidator.validateInteger(input);
         validateUnit(payment);
+        this.lottoCounts=payment/UNIT;
     }
 
     private void validateUnit(int payment) {
