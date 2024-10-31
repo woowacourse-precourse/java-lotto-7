73524894a7517b658507da5058e9093754f2f7e4
corpi <lj845854@gmail.com>
2024-10-31 15:50:19 +0900

    feat(LottoRank): 매칭 카운트 수와 매칭 보너스 여부 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -1,16 +1,20 @@
 package lotto.model;
 
 public enum LottoRank {
-    RANK_1(2_000_000_000),
-    RANK_2(30_000_000),
-    RANK_3(1_500_000),
-    RANK_4(50_000),
-    RANK_5(5_000),
-    LOSE(0);
+    RANK_1(2_000_000_000, 6, false),
+    RANK_2(30_000_000, 5, true),
+    RANK_3(1_500_000, 5, false),
+    RANK_4(50_000, 4, false),
+    RANK_5(5_000, 3, false),
+    LOSE(0, 0, false);
 
     final int prizeMoney;
+    final int matchCount;
+    final boolean matchBonus;
 
-    LottoRank(int prizeMoney) {
+    LottoRank(int prizeMoney, int matchCount, boolean matchBonus) {
         this.prizeMoney = prizeMoney;
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
     }
 }
