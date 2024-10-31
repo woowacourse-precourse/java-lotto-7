b90534550664f6baee495602ed8feaeceae3a5be
rjswjddn <rjswjddn@gmail.com>
2024-10-31 17:16:49 +0900

    feat: 당첨 번호 입력, 보너스 번호 입력

--- src/main/java/lotto/view/Request.java
+++ src/main/java/lotto/view/Request.java
@@ -8,4 +8,14 @@ public class Request {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public String inputWinNum() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }
