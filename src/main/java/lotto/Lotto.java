1bdaf06c27f236614fac6084c004f4c3d41ed6ba
choign3123 <choign3123@naver.com>
2024-10-31 21:52:03 +0900

    fix(Lotto): numbers 외의 필드를 추가할 수 없는 제약사항 반영
    기존: Lotto에서 가격 저장
    변경: LottoStore에서 로또값 저장

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,7 +3,6 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    public static final int PRICE = 1000;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -9,9 +9,10 @@ public class LottoStore {
     private static final int MIN_LOTTO_NUM = 1;
     private static final int MAX_LOTTO_NUM = 45;
     private static final int TOTAL_LOTTO_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
 
     public List<Lotto> purchase(int price) {
-        int numOfLotto = price / Lotto.PRICE;
+        int numOfLotto = price / LOTTO_PRICE;
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < numOfLotto; i++) {
