2eeb44a1d00962cd28c986368c7066f8de00e894
minjun <ijmtylhtf@naver.com>
2024-10-31 10:59:37 +0900

    feat : 생성자 구현

--- src/main/java/lotto/provider/LottoProvider.java
+++ src/main/java/lotto/provider/LottoProvider.java
@@ -3,4 +3,9 @@ package lotto.provider;
 public class LottoProvider {
 	private final int numberOfLottos;
 	private final int lottoPurchaseAmount;
+
+	public LottoProvider(int numberOfLottos, int lottoPurchaseAmount) {
+		this.numberOfLottos = numberOfLottos;
+		this.lottoPurchaseAmount = lottoPurchaseAmount;
+	}
 }
