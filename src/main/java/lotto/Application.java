bec1080f853f93fe92d4c542404318b2518adaf1
hgh1472 <hgh1472@naver.com>
2024-10-31 23:00:07 +0900

    fix: LottoController 생성자 매개변수 중 LottoStore 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.generator.NumberGenerator;
+import lotto.model.LottoStore;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(new InputView(), new OutputView());
+        LottoController lottoController = new LottoController(
+                new InputView(),
+                new OutputView(),
+                new LottoStore(NumberGenerator.RANDOM));
         lottoController.run();
     }
 }
