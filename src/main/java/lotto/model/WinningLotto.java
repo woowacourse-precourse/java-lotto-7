4996d8af0555b7adfc6c8c9f63dbddfb02903ebb
kim <hwiyul_g@daum.net>
2024-11-01 00:02:45 +0900

    refactor(WinningLotto): class에서 record로 변경

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -2,14 +2,10 @@ package lotto.model;
 
 import lotto.utils.ExceptionMessage;
 
-public class WinningLotto {
-    private final Lotto lotto;
-    private final Bonus bonus;
+public record WinningLotto(Lotto winningLotto, Bonus bonus) {
 
-    public WinningLotto(Lotto lotto, Bonus bonus) {
-        validate(lotto, bonus);
-        this.lotto = lotto;
-        this.bonus = bonus;
+    public WinningLotto {
+        validate(winningLotto, bonus);
     }
 
     private void validate(Lotto winningNumbers, Bonus bonus) {
@@ -17,6 +13,4 @@ public class WinningLotto {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_BONUS_DUPLICATED_NUMBER.getMessage());
         }
     }
-
-
 }
