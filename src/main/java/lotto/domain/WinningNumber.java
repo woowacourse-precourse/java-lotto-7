177a1b549f01856e1db8f070de1e5dc253549ae7
변우영 <likevangogh24@gmail.com>
2024-10-31 12:26:50 +0900

    feat(WinningNumber): 입력값으로부터 당첨 번호 객체를 생성하는 팩토리 메서드 추가
    - Convertor를 통해 당첨 번호 입력값을 쉼표(,)를 기준으로 분리했다.

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import lotto.util.Convertor;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers;
+
+    private WinningNumber(List<Integer> winningNumbers) {
+        this.numbers = winningNumbers;
+    }
+
+    public static WinningNumber from(String input) {
+        return new WinningNumber(Convertor.convertToIntegerList(input));
+    }
+}
