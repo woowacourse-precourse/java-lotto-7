cb44d8c4ca5555f53f6eb2ea7dcbd86951272c35
qyinm <qusseun@gmail.com>
2024-10-31 15:39:59 +0900

    feat: 맞은 번호와 보너스 볼 유무에 맞는 Enum값 반환 기능 작성

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,23 +1,40 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public enum LottoPrize {
 
-    FIFTH_PRIZE(3, 5_000),
-    FOURTH_PRIZE(4, 50_000),
-    THIRD_PRIZE(5, 1_500_000),
-    SECOND_PRIZE(5, 30_000_000),
-    FIRST_PRIZE(6, 2_000_000_000);
+    FIRST_PRIZE(6, 2_000_000_000, false),
+    SECOND_PRIZE(5, 30_000_000, true),
+    THIRD_PRIZE(5, 1_500_000, false),
+    FOURTH_PRIZE(4, 50_000, false),
+    FIFTH_PRIZE(3, 5_000, false),
+    NO_PRIZE(0, 0, false);
 
     private static final String describePrize = "%d개 일치%s (%s원)";
     private static final String additionalDescribeBonusPrize = ", 보너스 볼 일치";
-    private final int matchCount;
-    private final int prizeMoney;
 
-    LottoPrize(int matchCount, int prizeMoney) {
+    private final long matchCount;
+    private final int prizeMoney;
+    private final boolean isRequiredBonusNumber;
+
+    LottoPrize(long matchCount, int prizeMoney, boolean isRequiredBonusNumber) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.isRequiredBonusNumber = isRequiredBonusNumber;
     }
 
+    public static LottoPrize of(long matchCount, boolean isRequiredBonusNumber) {
+        return Arrays.stream(values())
+                .filter(getPrizePredicate(matchCount, isRequiredBonusNumber))
+                .findAny()
+                .orElse(NO_PRIZE);
+    }
+
+    private static Predicate<LottoPrize> getPrizePredicate(long matchCount, boolean isRequiredBonusNumber) {
+        return prize -> prize.matchCount == matchCount && prize.isRequiredBonusNumber == isRequiredBonusNumber;
+    }
 
     @Override
     public String toString() {
@@ -26,5 +43,9 @@ public enum LottoPrize {
             return describePrize.formatted(matchCount, additionalDescribeBonusPrize, formattedPrizeMoney);
         }
         return describePrize.formatted(matchCount, "", formattedPrizeMoney);
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 }
