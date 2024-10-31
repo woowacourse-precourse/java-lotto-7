8b9ceb14ce00497bd634de5c1710d32e7fedb7b8
EHCTH <lees90896@gmail.com>
2024-10-31 19:55:24 +0900

    refactor(InputValidator): 기존 예외 메세지 상수 변경

--- src/main/java/lotto/view/input/validator/InputValidator.java
+++ src/main/java/lotto/view/input/validator/InputValidator.java
@@ -1,15 +1,16 @@
 package lotto.view.input.validator;
 
+import lotto.constant.ErrorMessage;
 import lotto.view.input.domain.RegexPattern;
 
 public class InputValidator {
     public static void moneyValidate(String input) {
-        if (!RegexPattern.NUMBER_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 양의 정수만 입력해주세요");
+        if (!RegexPattern.NUMBER_REGEX.match(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
     }
     public static void bonusNumberValidate(String input) {
-        if (!RegexPattern.NUMBER_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 양의 정수만 입력해주세요");
+        if (!RegexPattern.NUMBER_REGEX.match(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
     }
     public static void winningLottoValidate(String input) {
-        if (!RegexPattern.LOTTO_REGEX.match(input)) throw new IllegalArgumentException("[ERROR] 형식에 맞지 않는 입력값 ex)1,2,3,4,5,6 형식으로 입력해주세요");
+        if (!RegexPattern.LOTTO_REGEX.match(input)) ErrorMessage.LOTTO_REGEX_PATTERN.throwIllegalArgumentException();
     }
 }
