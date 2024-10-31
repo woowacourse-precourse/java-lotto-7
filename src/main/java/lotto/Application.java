2de579704dd7b68373c693c117f278f234539902
KIMB0B <axzsd96301@gmail.com>
2024-10-31 13:39:55 +0900

    refactor(Application, User): User 클래스를 더 사용자 관리에만 적합하게 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,50 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
-        User user = new User();
+import camp.nextstep.edu.missionutils.Console;
 
-        System.out.println();
-        user.showLottoCount();
-        user.showLottos();
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Application {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    public static void main(String[] args) {
+        User user = inputNewUser();
+        printPurchaseStatus(user.getLottos());
 
         Committee committee = new Committee();
         committee.checkLottos(user);
 
-        user.printPrizes();
-        user.printReturnRate();
+        printPrizes(user.getPrizes(), user.getReturnRate());
+    }
+
+    private static User inputNewUser() {
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            return new User(Console.readLine());
+        } catch (Exception e) {
+            System.out.println(ERROR_MESSAGE + e.getMessage());
+            return inputNewUser();
+        }
+    }
+
+    public static void printPurchaseStatus(ArrayList<Lotto> lottos) {
+        System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+    }
+
+    public static void printPrizes(Map<Prize, Integer> prizes, double returnRate) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        for (int i = Prize.values().length - 1; i >= 0; i--) {
+            Prize prize = Prize.values()[i];
+            if (prize != Prize.FAIL) {
+                System.out.println(prize.getDescription() + " - " + prizes.get(prize) + "개");
+            }
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", returnRate);
     }
 }
--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,89 +8,57 @@ import java.util.Map;
 
 public class User {
 
-    private int lottoCount;
-    private ArrayList<Lotto> lottos = new ArrayList<>();
-    private Map<Prize, Integer> prizes = new LinkedHashMap<>();
-    private int illegalArgumentExceptionCount = 0;
+    private final int purchaseAmount;
+    private final ArrayList<Lotto> lottos = new ArrayList<>();
+    private final Map<Prize, Integer> prizes = new LinkedHashMap<>();
 
-    public User() {
-        this.lottoCount = inputLottoCount();
-        for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+    public User(String purchaseAmount) {
+        this.purchaseAmount = validatePurchaseAmount(purchaseAmount);
+        for (int i = 0; i < this.purchaseAmount / 1000; i++) {
+            this.lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
         for (Prize prize : Prize.values()) {
-            prizes.put(prize, 0);
+            this.prizes.put(prize, 0);
         }
     }
 
-    private int inputLottoCount() {
-        try {
-            System.out.println("구입금액을 입력해 주세요.");
-            String input = Console.readLine();
-            int purchaseAmount = validatePurchaseAmount(input);
-            return purchaseAmount / 1000;
-        } catch (Exception e) {
-            if (illegalArgumentExceptionCount++ < 5) {
-                System.out.println(e.getMessage());
-                return inputLottoCount();
-            }
-            throw e;
-        }
-    }
-
-    private int validatePurchaseAmount(String input) {
+    private static int validatePurchaseAmount(String input) {
         int purchaseAmount;
 
         try {
             purchaseAmount = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자로 입력해야 합니다.");
+            throw new IllegalArgumentException("로또 구입 금액은 숫자로 입력해야 합니다.");
         }
 
         if (purchaseAmount < 1000 || purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위로 입력해야 합니다.");
         }
 
         return purchaseAmount;
     }
 
-    public void showLottoCount() {
-        System.out.println(this.lottoCount + "개를 구매했습니다.");
-    }
-
-    public void showLottos() {
-        for (Lotto lotto : this.lottos) {
-            System.out.println(lotto.toString());
-        }
-    }
-
     public ArrayList<Lotto> getLottos() {
-        return lottos;
+        return this.lottos;
     }
 
     public void setPrize(int matchCount, int bonusMatch) {
         Prize prize = Prize.getPrize(matchCount, bonusMatch);
-        prizes.put(prize, prizes.get(prize) + 1);
+        this.prizes.put(prize, this.prizes.get(prize) + 1);
     }
 
-    public void printPrizes() {
-        System.out.println("\n당첨 통계");
-        System.out.println("---");
-        for (int i = Prize.values().length - 1; i >= 0; i--) {
-            Prize prize = Prize.values()[i];
-            if (prize != Prize.FAIL) {
-                System.out.println(prize.getDescription() + " - " + prizes.get(prize) + "개");
-            }
+    public Map<Prize, Integer> getPrizes() {
+        if (this.prizes.isEmpty()) {
+            throw new IllegalStateException("당첨 번호를 입력해야 당첨 통계를 확인할 수 있습니다.");
         }
+        return this.prizes;
     }
 
-    public void printReturnRate() {
+    public double getReturnRate() {
         int totalPrizeAmount = 0;
         for (Prize prize : Prize.values()) {
-            totalPrizeAmount += prize.getPrizeAmount() * prizes.get(prize);
+            totalPrizeAmount += prize.getPrizeAmount() * this.prizes.get(prize);
         }
-
-        double returnRate = (double) totalPrizeAmount / (lottoCount * 1000) * 100;
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", returnRate);
+        return (double) totalPrizeAmount / this.purchaseAmount * 100;
     }
 }
