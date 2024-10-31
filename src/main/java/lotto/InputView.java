19cd29bb77bf298e33f87572cd899ae7a7631286
joshinhyeok <shtree07@naver.com>
2024-10-31 22:40:52 +0900

    feat: 구매 금액을 입력받는 기능 추가

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    static String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
