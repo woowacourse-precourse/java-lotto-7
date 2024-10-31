307e3b4c3e4efceb4b9dae3311ad2b4f61e862db
choign3123 <choign3123@naver.com>
2024-10-31 23:23:41 +0900

    refactor(LottoStore, Lotto): LottoStore의 클래스 상수를 Lotto로 옮김
    변경 이유:
    - 로또값, 로또 번호 최소 최대값, 총 로또번호 개수는 LottoStore가 아니라 Lotto에서 관리하는게 적합하다고 판단
    - 요구사항 중 필드(인스턴스 변수)의 사용을 제한한다고 했지, 클래스 변수를 제한한다고 하진 않음

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+
+    public static final int MIN_LOTTO_NUM = 1;
+    public static final int MAX_LOTTO_NUM = 45;
+    public static final int TOTAL_LOTTO_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
