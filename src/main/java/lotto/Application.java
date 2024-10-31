ca626800529b04ed90c4817f70e8e3c0b518cda2
jwh946 <jwh946@gmail.com>
2024-10-31 22:13:02 +0900

    총 수익률 계산 및 출력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -32,7 +32,18 @@ public class Application {
 
         System.out.println("당첨 통계");
         System.out.println("---");
+        printStats(money, lottos, winningNumbers);
+    }
+    private static void printStats(String money, List<Lotto> lottos, List<Integer> winningNumbers) {
         checkNumber(lottos, winningNumbers);
+        System.out.println("3개 일치 (5,000원) - " + threeCnt + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourCnt + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + fiveCnt + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + bonusCnt + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + sixCnt + "개");
+        double profitPercent = 100 * profit / Integer.parseInt(money);
+        String profitRate = String.format("%.1f", profitPercent);
+        System.out.println("총 수익률은 " + profitRate +"%입니다.");
     }
     private static void checkNumber(List<Lotto> lottos, List<Integer> winningNumbers) {
         int collect = 0;
@@ -52,6 +63,7 @@ public class Application {
         }
         if (collect == 6) result = Result.Six;
         if (result != null) {
+            result.incrementCount();
             profit += result.getPrice();
         }
     }
--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -6,6 +6,11 @@ public enum Result {
     Five(1500000),
     Bonus(30000000),
     Six(2000000000);
+    public static int threeCnt = 0;
+    public static int fourCnt = 0;
+    public static int fiveCnt = 0;
+    public static int bonusCnt = 0;
+    public static int sixCnt = 0;
     private final int price;
     Result(int price) {
         this.price = price;
@@ -13,4 +18,22 @@ public enum Result {
     public int getPrice() {
         return price;
     }
+    public void incrementCount() {
+        if (this == Three) {
+            threeCnt++;
+        }
+        if (this == Four) {
+            fourCnt++;
+        }
+        if (this == Five) {
+            fiveCnt++;
+        }
+        if (this == Bonus) {
+            bonusCnt++;
+        }
+        if (this == Six) {
+            sixCnt++;
+        }
+    }
+
 }
