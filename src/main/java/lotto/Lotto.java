a324fe5b0d46a8ac641c0b3304abf82d27ac985c
theplayer1003 <theplayer1003@gmail.com>
2024-10-31 13:12:19 +0900

    feat(Lotto): 로또 용지를 구체화한 클래스

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,14 +3,32 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = makeLottos(numbers);
+    }
+
+    private List<LottoNumber> makeLottos(List<Integer> numbers) {
+        return null;
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateduplicate(numbers);
+    }
+
+    private void validateduplicate(List<Integer> numbers) {
+        Long duplicateCount = numbers.stream()
+                .distinct()
+                .count();
+        if (Long.compare(duplicateCount, numbers.size()) != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 값이 있습니다.");
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
