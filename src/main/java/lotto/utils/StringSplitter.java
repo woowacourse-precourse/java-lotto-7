837f9fec1d473753db4899a8f9c8cf8b60a5e7b5
yush <yush2202@gmail.com>
2024-10-31 20:39:31 +0900

    feat($ StringSplitter): 구분자 나누기 유틸 클래스 구현
    유저 로또 입력을 구분자로 나누기 위한 클래스 구현

---
+++ src/main/java/lotto/utils/StringSplitter.java
@@ -0,0 +1,11 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringSplitter {
+    public static List<String> splitByDelimiter(String userInput, String delimiter) {
+        String[] splittedUserInput = userInput.split(delimiter);
+        return Arrays.stream(splittedUserInput).toList();
+    }
+}
