3c4b15b8a1bdd418275930b40ba86603eb11e71d
imjunghun <dlawndgns9@naver.com>
2024-10-31 19:53:57 +0900

    feat(InputView): InputView 당첨 번호를 입력 받고 WinningNumbers 반환
    - InputView 에서 구분자 예외 검증을 한 후 WinningNumbers 반환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,22 +1,48 @@
 package lotto.view;
 
+import java.util.Arrays;
 import lotto.domain.Payment;
+import lotto.domain.WinningNumbers;
+import lotto.domain.lotto.Lotto;
 import lotto.view.console.Reader;
 import lotto.view.console.Writer;
 
 public class InputView {
 
     private static final String QUESTION_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String QUESTION_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String SEPARATOR = ",";
 
     public Payment readPrice() {
         Writer.println(QUESTION_PURCHASE_AMOUNT);
         return Payment.of(Validator.validatePrice(Reader.read()));
     }
 
+    public WinningNumbers readWinningNumbers() {
+        Writer.println(QUESTION_WINNING_NUMBERS);
+        String winningNumbers = Validator.validateSeparator(Reader.read());
+        return WinningNumbers.of(convert(winningNumbers));
+    }
+
+    private Lotto convert(String input) {
+        return Lotto.of(
+                Arrays.stream(input.split(SEPARATOR))
+                        .map(Validator::validateNumber)
+                        .toList()
+        );
+    }
+
     private static class Validator {
         private static int validatePrice(String input) {
             validateZero(input);
             return validateNumber(input);
+        }
+
+        private static String validateSeparator(String input) {
+            if (hasContinuousSeparator(input) || hasEdgeSeparator(input)) {
+                throw new IllegalArgumentException("[ERROR] 잘못된 입력 형식입니다.");
+            }
+            return input;
         }
 
         private static void validateZero(String input) {
@@ -38,6 +64,14 @@ public class InputView {
 
         private static boolean isNotNumeric(String input) {
             return !input.matches("\\d+");
+        }
+
+        private static boolean hasEdgeSeparator(String input) {
+            return input.startsWith(SEPARATOR) || input.endsWith(SEPARATOR);
+        }
+
+        private static boolean hasContinuousSeparator(String input) {
+            return input.matches(SEPARATOR.repeat(2));
         }
     }
 }
