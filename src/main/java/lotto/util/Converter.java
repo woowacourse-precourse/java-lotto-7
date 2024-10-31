2bc67284072c0a2c65f297d5d01637fb856691b4
Hyso <hsk7953@gmail.com>
2024-10-31 23:26:57 +0900

    refactor(Converter) : 불필요한 메서드 제거 - 코드 포매팅 적용

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -2,23 +2,17 @@ package lotto.util;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Converter {
-    private Converter(){
+    private Converter() {
     }
 
-    public static List<Integer> toNumberList (String input){
+    public static List<Integer> toNumberList(String input) {
         return parseNumberStream(input).toList();
     }
 
-    public static Set<Integer> toNumberSet (String input){
-        return parseNumberStream(input).collect(Collectors.toSet());
-    }
-
-    private static Stream<Integer> parseNumberStream (String input){
+    private static Stream<Integer> parseNumberStream(String input) {
         return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt)
                 .boxed();
     }
