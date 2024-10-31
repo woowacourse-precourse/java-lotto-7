ed36997ab6baa043d379999130708400df4c8cc0
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-31 09:36:57 +0900

    feat : 구매 금액에 대한 객체 생성 및 비즈니스 로직을 담당하는 컨트롤러
    - 구매 금액 모델 인스턴스를 생성자에서 발급한다.
    - 추후에 재입력 받는 부분 역시 만들어야 할 것이다.

---
+++ src/main/java/lotto/controller/BuyLottoController.java
@@ -0,0 +1,27 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.BuyPrice;
+import lotto.view.BuyPriceView;
+
+public class BuyLottoController {
+    private BuyPrice buyPrice;
+    private final BuyPriceView buyPriceView;
+
+    public BuyLottoController() {
+        this.buyPriceView = new BuyPriceView();
+        buyPriceView.printPriceInputView();
+        this.buyPrice = new BuyPrice(Console.readLine());
+    }
+
+    public BuyLottoController(String price){
+        this.buyPriceView = new BuyPriceView();
+        buyPriceView.printPriceInputView();
+        this.buyPrice = new BuyPrice(price);
+    }
+
+    public BuyPrice getBuyPrice() {
+        return buyPrice;
+    }
+
+}
