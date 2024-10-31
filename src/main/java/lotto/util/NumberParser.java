ae9869631c7e09421098895173f0fc8e7454a053
hanwool <hanwe92@gmail.com>
2024-10-31 11:36:12 +0900

    fix(NumberParser): Change comma hard coding to constant

--- src/main/java/lotto/util/NumberParser.java
+++ src/main/java/lotto/util/NumberParser.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class NumberParser {
+    private static final String COMMA = ",";
+
     public static List<Integer> parseLottoNumbers(String input) {
-        Stream<String> splitNumbers = Arrays.stream(input.split(","));
+        Stream<String> splitNumbers = Arrays.stream(input.split("COMMA"));
         Stream<String> trimNumbers = splitNumbers.map(String::trim);
         //TODO: 숫자 아닌 입력 validation 추가 필요
         return trimNumbers.map(Integer::parseInt).toList();
