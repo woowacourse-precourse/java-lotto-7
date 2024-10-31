32cad5b2d63ab6d9946c4ea6186f02ba7fd6f71c
songsunkook <songsunkook@gmail.com>
2024-10-31 18:50:16 +0900

    test: 당첨내역 및 수익률 계산 테스트 작성

--- src/main/java/lotto/domain/Wallet.java
+++ src/main/java/lotto/domain/Wallet.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import lotto.Lotto;
 import lotto.util.RandomUtil;
 
 public class Wallet {
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +22,25 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_내역을_계산한다() {
+        List<Integer> winningLottoNumbers = RandomUtil.getLottoNumbers();
+        List<Integer> numbers = winningLottoNumbers.subList(0, 5);
+        numbers.add(0);
+        Lotto lotto = new Lotto(numbers);
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumbers);
+        int rank = winningLotto.getRank(lotto);
+        assertThat(rank).isEqualTo(3);
+    }
+
+    @Test
+    void 수익률을_계산한다() {
+        Wallet wallet = new Wallet(5000);
+        wallet.buyLottoTickets();
+        Rank rank = new Rank();
+        rank.add(3);
+        assertThat(rank.getPrice()).isEqualTo(5_000 / 1_500_000);
     }
 }
