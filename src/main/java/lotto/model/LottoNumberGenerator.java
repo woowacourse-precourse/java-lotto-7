665dc92d9e64abbce08b99894af9faac9fd383f3
mininiii <didals0521@gmail.com>
2024-10-31 14:20:07 +0900

    feat: 여러개 로또 번호 리스트 생성 기능 추가

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,9 +1,19 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
 
 public class LottoNumberGenerator {
+    public List<Lotto> getLotto(long count) {
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lotto.add(new Lotto(getRandomNumbers()));
+        }
+        return lotto;
+    }
+
     public List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
