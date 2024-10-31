46c72e0bcf2e6273ee62e828f234ccc6d91c0916
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 21:50:33 +0900

    test(LottoVendingMachine) : 테스트 코드 추가

---
+++ src/test/java/lotto/model/LottoVendingMachineTests.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import static lotto.util.LottoConstants.LOTTO_TICKET_PRICE;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoVendingMachineTests {
+
+    @Test
+    @DisplayName("구입 금액에 따라 발행되는 로또의 개수는 구입 금액을 로또 1장의 가격으로 나눈 값과 동일")
+    void issueLottoTest() {
+        int totalCost = 8000;
+        int expected = totalCost/LOTTO_TICKET_PRICE;
+
+        LottoVendingMachine vendingMachine = new LottoVendingMachine(totalCost);
+        List<Lotto> purchased = vendingMachine.issueLotto();
+
+        assertEquals(purchased.size(), expected);
+    }
+}
