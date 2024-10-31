f6314f4a9b0e982711d5a9a128de184da067f496
joshinhyeok <shtree07@naver.com>
2024-11-01 00:33:32 +0900

    feat: 로또 장수에 맞게 로또를 발행하는지 테스트 추가

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -2,12 +2,23 @@ package lotto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LottoServiceTest {
+
     @Test
     void 구매할_로또_장수를_계산합니다() {
         assertEquals(3, LottoService.calculateLottoCount(3000));
         assertEquals(5, LottoService.calculateLottoCount(5000));
+    }
+
+    @Test
+    void 로또를_구매할_장수에_맞게_발행합니다() {
+        List<Lotto> lottos = LottoService.generateLottos(5);
+        assertEquals(5, lottos.size());
+        for (Lotto lotto : lottos) {
+            assertEquals(6, lotto.getNumbersSize());
+        }
     }
 }
