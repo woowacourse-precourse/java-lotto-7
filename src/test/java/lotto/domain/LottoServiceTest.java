982ea62b901274ef20e87acd2dcef6ba89114a20
OhYeHyun <ohyhohyl@naver.com>
2024-11-01 01:45:53 +0900

    test(LottoServiceTest): 수익률 계산 검증 테스트 추가

--- src/test/java/lotto/domain/LottoServiceTest.java
+++ src/test/java/lotto/domain/LottoServiceTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +14,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoServiceTest {
     private LottoService lottoService;
 
-    @Test
-    @DisplayName("당첨 금액을 올바르게 합산하는지 확인")
-    void 당첨_금액_테스트() {
+    @BeforeEach
+    void setUp() {
         Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto bonusNumber = new Lotto(Arrays.asList(7));
 
-        Lotto random1 = new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10));
-        Lotto random2 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        Lotto random3 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7));
-        Lotto random4 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10));
-        List<Lotto> lottoTickets = new ArrayList<>(Arrays.asList(random1, random2, random3, random4));
+        Lotto random1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10));    // 3등
+        Lotto random2 = new Lotto(Arrays.asList(1, 2, 3, 4, 9, 10));    // 4등
 
+        List<Lotto> lottoTickets = new ArrayList<>(Arrays.asList(random1, random2));
         lottoService = new LottoService(lottoTickets, winningNumbers, bonusNumber);
-        int result = lottoService.calculateWinningAmount();
+    }
 
-        assertThat(result).isEqualTo(2000000000 + 30000000 + 1500000);
+    @Test
+    @DisplayName("당첨 금액을 올바르게 합산하는지 확인")
+    void 당첨_금액_테스트() {
+        lottoService.calculateWinningAmount();
+        BigDecimal result = lottoService.getWinningAmount();
+        assertThat(result).isEqualTo(BigDecimal.valueOf(1500000 + 50000));
+    }
+
+    @Test
+    @DisplayName("수익률을 올바르게 계산하는지 확인")
+    void 수익률_테스트() {
+        lottoService.calculateWinningAmount();
+        lottoService.calculateRateOfReturn();
+        BigDecimal result = lottoService.getEarningsRate();
+        assertThat(result).isEqualTo(BigDecimal.valueOf(77500.0));
     }
 }
