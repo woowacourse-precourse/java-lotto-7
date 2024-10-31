2df1fe0a0ca3b8275d567f94650f82ffa60cfc81
ChoiDongJoon <adong0808@naver.com>
2024-10-31 14:48:24 +0900

    feat(lotto): Lotto의 숫자들을 가져오는 get메소드, 중복 검사, toString() 구현
    Lotto의 숫자들을 가져오는 get메소드, 중복 검사, toString() 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -22,10 +22,26 @@ public class Lotto {
         this.numbers.add(lottoNumber);
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
+
+        if (uniqueNumbers.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
