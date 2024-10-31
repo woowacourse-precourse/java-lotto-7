0cd4e3c331338125e867dd8b63521004f9d5760d
ohhyungsuh <ohhyungsuh@gmail.com>
2024-10-31 17:10:27 +0900

    feat(view, exception): 당첨 번호 입력 기능 구현 완료
    - printAndGetWinNumbers() 메소드 구현
    - 잘못된 입력인 경우 예외 처리 완료

--- src/main/java/lotto/exception/LottoErrorMessage.java
+++ src/main/java/lotto/exception/LottoErrorMessage.java
@@ -2,10 +2,16 @@ package lotto.exception;
 
 public enum LottoErrorMessage {
 
-    INVALID_AMOUNT_ERROR("공백이 아닌 값을 입력해주세요."),
+    INVALID_INPUT_ERROR("공백이 아닌 값을 입력해주세요."),
+
     NOT_NUMBER_AMOUNT_ERROR("숫자를 입력해주세요."),
     LESS_MIN_AMOUNT_ERROR("1,000원 이상을 입력해주세요."),
-    DIV_1_000_AMOUNT_ERROR("1,000원 단위로 입력해주세요.");
+    DIV_1_000_AMOUNT_ERROR("1,000원 단위로 입력해주세요."),
+
+    WIN_NUMBERS_CONTAINS_LETTER_ERROR("구분자(',')를 제외한 문자는 입력될 수 없습니다. ','와 숫자만 입력해주세요."),
+    WIN_NUMBERS_COUNT_ERROR("당첨 번호는 6개를 입력해주세요."),
+    WIN_NUMBERS_RANGE_ERROR("당첨 번호는 1 이상 45 이하의 숫자들만 입력해주세요.");
+
 
     private final String error;
 
--- src/main/java/lotto/view/UserView.java
+++ src/main/java/lotto/view/UserView.java
@@ -4,8 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.LottoErrorMessage;
 import lotto.exception.LottoArgumentException;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
     public static int printAndGetAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
@@ -24,7 +29,7 @@ public class UserView {
 
     private static int checkAmount(String inputAmount) {
         if (inputAmount == null || inputAmount.equals("\n") || inputAmount.isBlank()) {
-            throw new LottoArgumentException(LottoErrorMessage.INVALID_AMOUNT_ERROR);
+            throw new LottoArgumentException(LottoErrorMessage.INVALID_INPUT_ERROR);
         }
 
         try {
@@ -42,5 +47,50 @@ public class UserView {
         } catch (NumberFormatException e) {
             throw new LottoArgumentException(LottoErrorMessage.NOT_NUMBER_AMOUNT_ERROR);
         }
+    }
+
+    public static List<Integer> printAndGetWinNumbers() {
+        System.out.println(INPUT_WIN_NUMBERS_MESSAGE);
+        return getWinNumbers();
+    }
+
+    private static List<Integer> getWinNumbers() {
+        while (true) {
+            try {
+                return checkWinNumbers(Console.readLine());
+            } catch (LottoArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static List<Integer> checkWinNumbers(String inputWinNumbers) {
+        if (inputWinNumbers == null || inputWinNumbers.equals("\n") || inputWinNumbers.isBlank()) {
+            throw new LottoArgumentException(LottoErrorMessage.INVALID_INPUT_ERROR);
+        }
+
+        try {
+            List<Integer> winNumbers = Arrays
+                    .stream(inputWinNumbers.split(","))
+                    .map(String::strip)
+                    .map(Integer::parseInt)
+                    .toList();
+
+            if(winNumbers.size() != 6) {
+                throw new LottoArgumentException(LottoErrorMessage.WIN_NUMBERS_COUNT_ERROR);
+            }
+
+            if(!checkValidRangeNumbers(winNumbers)) {
+                throw new LottoArgumentException(LottoErrorMessage.WIN_NUMBERS_RANGE_ERROR);
+            }
+
+            return winNumbers;
+        } catch (NumberFormatException e) {
+            throw new LottoArgumentException(LottoErrorMessage.WIN_NUMBERS_CONTAINS_LETTER_ERROR);
+        }
+    }
+
+    private static boolean checkValidRangeNumbers(List<Integer> winNumbers) {
+        return winNumbers.stream().allMatch(number -> 1 <= number && number <= 45);
     }
 }
