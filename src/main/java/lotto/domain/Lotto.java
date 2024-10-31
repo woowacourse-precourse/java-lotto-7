8856ac5a6643d0fb26572e69001b57e9ac93d0da
StoneCAU <seokwoo1999@naver.com>
2024-11-01 00:07:11 +0900

    feat(Lotto): numbers getter 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,11 +10,13 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
