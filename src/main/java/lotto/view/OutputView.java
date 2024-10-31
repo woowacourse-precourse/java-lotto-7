aeac8ee3bb9f70c3bbfb26106f9f154d55561eb6
권장순 <jsoonworld@gmail.com>
2024-10-31 14:28:53 +0900

    feat(OutputView): 로또 구입 금액 입력 메시지 출력 기능 구현
    - OutputView 클래스에 구입 금액 입력 메시지를 출력하는 기능 추가
    - Application 클래스에서 OutputView 인스턴스를 생성하여 메시지 출력 확인

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void printPurchaseAmountMessage() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+    }
+}
