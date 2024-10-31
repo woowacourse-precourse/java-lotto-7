efd4a454db71d4bff69f9dc374ef78d30ca6c962
DongchannN <ldc991104@gmail.com>
2024-11-01 00:54:24 +0900

    feat: 당첨 로또 번호를 관리하는 모델 구현

---
+++ src/main/java/lotto/model/PrizeLotto.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import java.util.List;
+
+public class PrizeLotto {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public PrizeLotto(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+
+    public static PrizeLotto from(List<Integer> lottoNumbers, int bonusNumber) {
+        Lotto winningLotto = new Lotto(lottoNumbers);
+        return new PrizeLotto(winningLotto, bonusNumber);
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("로또 숫자의 범위는 1부터 45입니다.");
+        }
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("로또 숫자가 중복될 수 없습니다.");
+        }
+    }
+}
