1db8a9153be3f570665581769452cab7f55f3c3e
181108-cherry <dysy9799@naver.com>
2024-10-31 13:27:11 +0900

    feat: 로또 번호 반환 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -25,4 +25,7 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
         }
     }
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
