ac6a04efa823f155d62b9d9c950f95784b7706ca
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-31 18:38:21 +0900

    refactor($Lotto): 중복 번호와 번호 범위도 검증하도록 변경
    - 중복 번호와 범위를 검증하는 것은 로또의 역할이라 판단.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -19,6 +19,14 @@ public class Lotto {
         if (numbers.size() != SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_NUMBER, MAX_NUMBER)
+            );
+        }
     }
 
     public List<Integer> getNumbers() {
