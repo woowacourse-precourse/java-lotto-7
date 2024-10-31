7af554c65624f1c8e47bd2a30db38c5d728eac15
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 22:51:07 +0900

    feat: 당첨 번호 쉼표(,)사이 공백 제거 및 구분 기능 구현

--- src/main/java/lotto/validation/WinningNumberValidation.java
+++ src/main/java/lotto/validation/WinningNumberValidation.java
@@ -1,6 +1,18 @@
 package lotto.validation;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class WinningNumberValidation {
+    private final static String DELIMITER = ",";
+
     public static void validate(String inputWinningNumbers) {
+        List<String> winningNumbers = setTrimNumbers(inputWinningNumbers);
+    }
+
+    public static List<String> setTrimNumbers(String inputWinningNumbers) {
+        return Arrays.stream(inputWinningNumbers.split(DELIMITER))
+                .map(String::trim)
+                .toList();
     }
 }
