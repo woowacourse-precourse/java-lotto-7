4afe00f2a0db1971d8a8aea5f60adb2ddad60643
minseo <alstj778@gmail.com>
2024-10-31 18:38:47 +0900

    refactor(Purchase): 구매 가격 관리 역할을 위한 Purchase 모델 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.model.Purchase;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -7,44 +8,19 @@ public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
 
+
     public LottoController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void run() {
-        int purchasePrice = getPurchasePrice();
+        Purchase purchase = requestPurchase();
     }
 
-    private int getPurchasePrice() {
+    private Purchase requestPurchase() {
         outputView.printPurchasePriceRequest();
         int purchasePrice = inputView.getInteger();
-        validatePurchasePrice(purchasePrice);
-        return purchasePrice;
-    }
-
-    private void validatePurchasePrice(int value) {
-        validatePositive(value);
-        validatePurchaseUnit(value);
-    }
-
-    private void validatePositive(final int value) {
-        if (isNotPositive(value)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private boolean isNotPositive(final int value) {
-        return value <= 0;
-    }
-
-    private void validatePurchaseUnit(final int value) {
-        if (isNotMultipleOf1000(value)) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private boolean isNotMultipleOf1000(final int value) {
-        return value % 1000 != 0;
+        return new Purchase(purchasePrice);
     }
 }
---
+++ src/main/java/lotto/model/Purchase.java
@@ -0,0 +1,35 @@
+package lotto.model;
+
+public class Purchase {
+    private final int price;
+
+    public Purchase(int price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(int value) {
+        validatePositive(value);
+        validatePurchaseUnit(value);
+    }
+
+    private void validatePositive(final int value) {
+        if (isNotPositive(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNotPositive(final int value) {
+        return value <= 0;
+    }
+
+    private void validatePurchaseUnit(final int value) {
+        if (isNotMultipleOf1000(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNotMultipleOf1000(final int value) {
+        return value % 1000 != 0;
+    }
+}
