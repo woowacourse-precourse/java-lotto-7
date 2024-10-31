9c5c12687ee69f5dee062b18aa1a8b273fdae130
rladmstn <oz33177351@gmail.com>
2024-11-01 01:01:02 +0900

    feat(manager) : 당첨 확인하는 executeWinningProcess 메소드 추가
    - 각 로또들이 몇등 당첨됐는지 확인 후, 당첨 횟수를 UserLotto 객체에 갱신하는 메서드 작성

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -2,14 +2,16 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
+import lotto.domain.UserLotto;
 
 public class LottoManager {
-    private List<LottoRank> ranks = new ArrayList<>();
+    private final List<LottoRank> ranks = new ArrayList<>();
 
-    public LottoManager(){
+    public LottoManager() {
         ranks.add(new LottoRank(1, 2000000000, 6, false));
         ranks.add(new LottoRank(2, 30000000, 5, true));
         ranks.add(new LottoRank(3, 1500000, 5, false));
@@ -17,18 +19,47 @@ public class LottoManager {
         ranks.add(new LottoRank(5, 5000, 3, false));
     }
 
-    public List<Lotto> generateLottos(int purchaseAmount) {
+    public List<Lotto> generateLotteries(int purchaseAmount) {
         int lottoCount = purchaseAmount / 1000;
-        List<Lotto> lottos = new ArrayList<>();
+        List<Lotto> lotteries = new ArrayList<>();
 
         for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(generateLotto()));
+            lotteries.add(new Lotto(generateLotto()));
         }
-        return lottos;
+        return lotteries;
     }
 
-    private List<Integer> generateLotto(){
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public void executeWinningProcess(UserLotto userLotto, List<Integer> winningNumbers, int bonusNumber) {
+        for (Lotto lotto : userLotto.getLottos()) {
+            List<Integer> numbers = lotto.getNumbers();
+            int matchedNumbersCount = countMatchingNumbers(numbers, winningNumbers);
+            boolean isMatchedBonusNumber = checkMatchingBonusNumber(numbers, bonusNumber);
+            updateWinningCount(userLotto, matchedNumbersCount, isMatchedBonusNumber);
+        }
+    }
+    private int countMatchingNumbers(List<Integer> lotto, List<Integer> winningNumbers){
+         List<Integer> tempLotto = new ArrayList<>(lotto);
+         tempLotto.retainAll(winningNumbers);
+         return tempLotto.size();
+    }
+
+    private boolean checkMatchingBonusNumber(List<Integer> lotto, int bonusNumber){
+        return lotto.contains(bonusNumber);
+    }
+
+    private void updateWinningCount(UserLotto userLotto, int matchedNumbersCount, boolean isMatchedBonusNumber){
+        for(LottoRank rank : ranks){
+            if(rank.isWinning(matchedNumbersCount, isMatchedBonusNumber)){
+                userLotto.updateWinningCount(rank.rank());
+                break;
+            }
+        }
+    }
+
+    private List<Integer> generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
     }
 
 }
