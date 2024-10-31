c03de26f0fe8519a60e60ac1f3b8d357f9a7153e
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-31 19:55:25 +0900

    feat(LottoPurchaseAmount): 구입 금액이 양수인지 검증하는 로직 추가

--- src/main/java/lotto/domain/LottoPurchaseAmount.java
+++ src/main/java/lotto/domain/LottoPurchaseAmount.java
@@ -2,9 +2,12 @@ package lotto.domain;
 
 public class LottoPurchaseAmount {
 
+    private static final String LOTTO_PURCHASE_AMOUNT_REGEX = "^[1-9]\\d*$";
+
     private final int purchaseAmount;
 
     private LottoPurchaseAmount(String purchaseAmount) {
+        Validator.validatePurchaseAmount(purchaseAmount);
         this.purchaseAmount = Integer.parseInt(purchaseAmount);
     }
 
@@ -14,6 +17,20 @@ public class LottoPurchaseAmount {
 
     public int getPurchaseAmount() {
         return purchaseAmount;
+    }
+
+    private static class Validator {
+
+        private static void validatePurchaseAmount(String purchaseAmount) {
+            validatePurchaseAmountRegex(purchaseAmount);
+        }
+
+        private static void validatePurchaseAmountRegex(String purchaseAmount) {
+            if (!purchaseAmount.matches(LOTTO_PURCHASE_AMOUNT_REGEX)) {
+                throw new IllegalArgumentException("구입 금액은 양수만 입력 가능합니다.");
+            }
+        }
+
     }
 
 }
