fc5891f3a56e15808d1b42a7d8186d5f87a71810
EHCTH <lees90896@gmail.com>
2024-10-31 19:57:20 +0900

    style(LottoTest): ErrorMessage 변경에 의한 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,12 @@
 package lotto;
 
+import lotto.buyer.domain.Money;
+import lotto.buyer.infrastructure.Won;
+import lotto.constant.ErrorMessage;
+import lotto.lotto.domain.Lotto;
+import lotto.lotto.validator.LottoValidator;
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.winning.domain.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +29,13 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("1 ~ 45까지 범위의 숫자가 아닌경우 예외가 발생한다")
+    void withinRangeLottoNumberTest() {
+        List<List<Integer>> numbersInfo = List.of(List.of(1,2,3,4,5,46), List.of(0,1,2,3,4,5));
+        numbersInfo.forEach((numbers) -> {
+            assertThatThrownBy(() -> new Lotto(numbers)).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.WITHIN_RANGE.getMessage());
+        });
+    }
 }
