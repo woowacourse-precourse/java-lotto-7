02f7fd4a5972e9385514e046a74ad455514d4e4d
dnjswns98 <tngkrt2004@gmail.com>
2024-10-31 21:24:55 +0900

    refactor(ErrorMessage): Update error message for integer number validation

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -9,7 +9,7 @@ public enum ErrorMessage {
     DUPLICATE_VALUES("중복된 값이 입력되었습니다."),
     NOT_WINNING_SIZE("당첨 번호는 6개가 입력되어야 합니다."),
     NOT_NUMBER_RANGE("번호는 1 ~ 45 사이의 값이어야 합니다."),
-    NOT_INTEGER_NUMBER("번호는 정수만 가능합니다."),
+    NOT_INTEGER_NUMBER("정수만 입력 가능합니다."),
     NOT_BONUS_SIZE("보너스 번호는 1개만 입력되어야 합니다.");
 
     private final String message;
