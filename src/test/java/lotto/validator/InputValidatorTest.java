163c4f0659a7f4029564a89e2acf77ae7bde6471
chaerish <ccyy8432@naver.com>
2024-10-31 17:55:46 +0900

    test(InputValidator): 로또 구입 금액 입력 유효성 검사 테스트 작성
    사용자에게 로또 구입 금액을 입력받고, 금액의 유효성 검사 테스트를 작성한다.

---
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -0,0 +1,37 @@
+package lotto.validator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class InputValidatorTest {
+    private InputValidator inputValidator;
+
+    @BeforeEach
+    void init() {
+        inputValidator = new InputValidator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000히히", "120숫자", "1아2", "1?000", "1a00", "4a1b", "-1"})
+    void 구입금액이_자연수가_아닐때_에러_반환(String amount) {
+        Throwable throwable = catchThrowable(() -> {
+            inputValidator.PurchaseAmountValidate(amount);
+        });
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 정수여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"100", "1", "10", "99", "999", "0"})
+    void 구입금액이_1000보다_작을때_에러_반환(String amount) {
+        assertThatThrownBy(() -> {
+            inputValidator.PurchaseAmountValidate(amount);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+    }
+}
