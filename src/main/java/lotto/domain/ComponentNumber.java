0b2a52e44db7c6a4d159a48453705d42120135c3
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-01 03:12:09 +0900

    feat:ComponentNumber getter 매서드 추가

--- src/main/java/lotto/domain/ComponentNumber.java
+++ src/main/java/lotto/domain/ComponentNumber.java
@@ -23,6 +23,10 @@ public class ComponentNumber implements Component {
         }
     }
 
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
