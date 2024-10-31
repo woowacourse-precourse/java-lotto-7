8858fc50b8c98410f667dcc0cb49e30d0d378573
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-10-31 13:25:01 +0900

    feat(sortLottoNumberAscending): 로또 번호를 오름차순 정렬하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,22 @@
 package lotto.service;
 
+import java.util.List;
+
 public class LottoService {
+
+    public List<Integer> sortLottoNumbersAscending(List<Integer> lottoNumber) {
+        for (int i = 0; i < lottoNumber.size() - 1; i++) {
+            for (int j = 0; j <lottoNumber.size() - i - 1; j ++) {
+                if (lottoNumber.get(j) > lottoNumber.get(j + 1)) {
+                    int tmp = lottoNumber.get(j) ;
+                    lottoNumber.set(j, lottoNumber.get(j + 1));
+                    lottoNumber.set(j + 1, tmp) ;
+                }
+            }
+        }
+        return lottoNumber;
+    }
+
     public int calculateLottoQuantities(int purchaseAmount) {
         return purchaseAmount / 1000;
     }
