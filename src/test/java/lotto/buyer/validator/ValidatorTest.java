aed166f5d8e6ed3a0dc25ebfc947c0ab7e52ab76
EHCTH <lees90896@gmail.com>
2024-10-31 21:23:41 +0900

    test(ValidatorTest): 예외 추가 및 검증클래스 정적 메서드 사용

--- src/test/java/lotto/buyer/validator/ValidatorTest.java
+++ src/test/java/lotto/buyer/validator/ValidatorTest.java
@@ -1,30 +1,33 @@
 package lotto.buyer.validator;
+
 import lotto.buyer.infrastructure.Won;
 import lotto.constant.ErrorMessage;
+import lotto.util.ValidationProcess;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
 public class ValidatorTest {
     @Test
     @DisplayName("1000원으로 안나눠지는 경우 예외 발생")
     void divisibleByThousand() {
-        List<String> moneyInfo = List.of("12313","1323","1001","10", "1");
-        moneyInfo.forEach((money) -> verifyException(money, ErrorMessage.DIVISIBLE_BY_THOUSAND));
+        List<String> exceptionData = List.of("12313", "1323", "1001", "10", "1");
+        ValidationProcess.createThrownBy(exceptionData, Won::of, ErrorMessage.DIVISIBLE_BY_THOUSAND);
     }
+
     @Test
     @DisplayName("10만원 초과일 경우 예외 발생")
     void validateAmountUnderLimit() {
-        List<String> amountLimit = List.of("12313123","213213","100001", "123132123");
-        amountLimit.forEach((amount) -> verifyException(amount, ErrorMessage.AMOUNT_LIMIT));
+        List<String> exceptionData = List.of("12313123", "213213", "100001", "123132123");
+        ValidationProcess.createThrownBy(exceptionData, Won::of, ErrorMessage.AMOUNT_LIMIT);
     }
-    private void verifyException(String input, ErrorMessage errorMessage) {
-        assertThatThrownBy(
-                () -> Won.of(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(errorMessage.getMessage());
+
+    @Test
+    @DisplayName("Money 생성시 10만원 초과일 경우 예외 발생")
+    void createMoneyTest() {
+        List<String> exceptionData = List.of("9223372036854775808", "922337203685477580123", "23154192233720368");
+        ValidationProcess.createThrownBy(exceptionData, Won::of, ErrorMessage.AMOUNT_LIMIT);
     }
 
 }
