58bf0f0dca4a51e4aea77dd7e66eb58d0862672c
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-31 14:47:50 +0900

    feat(InputView): 구입 금액 유효성 검사 기능 구현 - 금액이 양의 정수이며 1,000단위일 경우에만 유효

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,9 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static int priceInput(){
-        System.out.println("구입 금액을 입력해주세요.");
-        String input = Console.readLine();
-        int price = Integer.parseInt(input);
+        while(true) {
+            try {
+                System.out.println("구입 금액을 입력해주세요.");
+                String input = Console.readLine();
+                int price = Integer.parseInt(input);
 
+                if (price <= 0 || price % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 금액은 1000단위이어야 합니다.");
+                }
+                return price;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해주세요.");
+            }
+            catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        }
     }
 }
