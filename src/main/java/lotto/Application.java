d0985266ccae7f230ac8c085df46559f296d6af0
tdddt <doto1116@ewhain.net>
2024-10-31 14:31:45 +0900

    feat : 로또 발행(로또 번호 뽑기)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+    public static List<Integer> makeLotto(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         int budget = Integer.parseInt(Console.readLine());
@@ -15,5 +21,10 @@ public class Application {
         int count = budget/1000;
         System.out.println();
         System.out.println(count+"개를 구매했습니다.");
+
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        for(int i=0;i<count;i++){
+            lottos.add(new Lotto(makeLotto()));
+        }
     }
 }
