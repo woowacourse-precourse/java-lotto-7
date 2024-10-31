2347696e332f16d5b4f0d615515edcc580e858fe
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-01 02:38:28 +0900

    feat($model) : Check lotto is won or not
    로또 하나가 당첨 됐는지 안됐는지 체크하는 기능 추가

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -24,6 +24,20 @@ public class LottoManager {
     private Lotto issueLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
+    
+
+    private void isLottoWin(Lotto lotto) {
+        int count = 0;
+        for (Integer number : lotto.getNumbers()) {
+            if (this.isNumberMatch(number)){
+                count++;
+            }
+        }
+
+        if (count>=3) {
+            this.checkFifthPrize(count, this.isBonusNumberMatch(lotto.getNumbers())); //check 함수가 연달아서 발생함.
+        }
+    }
 
     private boolean isNumberMatch(Integer lottoNumber) {
         for (Integer winNumber : this.winningNumbers) {
@@ -42,7 +56,51 @@ public class LottoManager {
         }
         return false;
     }
-    
+
+    private void checkFifthPrize(int count, boolean bonus) {
+        if (count ==3) {
+            int currentCount = winLottiesCount.get(0); // 기본값이 0으로 보장됨
+            winLottiesCount.set(0, currentCount + 1);
+            return;
+        }
+        this.checkFourthPrize(count, bonus);
+    }
+
+    private void checkFourthPrize(int count, boolean bonus) {
+        if (count ==4) {
+            int currentCount = winLottiesCount.get(1); // 기본값이 0으로 보장됨
+            winLottiesCount.set(1, currentCount + 1);
+            return;
+        }
+        this.checkThirdPrize(count, bonus);
+    }
+
+    private void checkThirdPrize(int count, boolean bonus) {
+        if (count ==5 && !bonus) {
+            int currentCount = winLottiesCount.get(2); // 기본값이 0으로 보장됨
+            winLottiesCount.set(2, currentCount + 1);
+            return;
+        }
+        this.checkSecondPrize(count, bonus);
+    }
+
+    private void checkSecondPrize(int count, boolean bonus) {
+        if (count ==5 && bonus) {
+            int currentCount = winLottiesCount.get(3); // 기본값이 0으로 보장됨
+            winLottiesCount.set(3, currentCount + 1);
+            return;
+        }
+        this.checkFirstPrize(count);
+    }
+
+    private void checkFirstPrize(int count) {
+        if (count == 6){
+            int currentCount = winLottiesCount.get(4); // 기본값이 0으로 보장됨
+            winLottiesCount.set(4, currentCount + 1);
+        }
+    }
+
+
     public List<Lotto> getLotties() {
         return lotties;
     }
