3dedee968767a5790de74933f3c7a9eb6a9ec449
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-31 15:35:52 +0900

    feat(model/Lotto): 로또 번호 유효성 검사 기능 추가
    - 중복된 번호를 검증하는 validateDuplicates 메서드 추가
    - 기존 validate 메서드를 validateSize로 이름 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,28 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validateSize(numbers);
+		validateDuplicates(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+	private void validateSize(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	private void validateDuplicates(List<Integer> numbers) {
+		Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+		if (uniqueNumbers.size() != numbers.size()) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+		}
+	}
 }
