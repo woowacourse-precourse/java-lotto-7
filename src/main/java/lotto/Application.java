c535d8b7a293bee14d75c1910226505a0f5e0371
tdddt <doto1116@ewhain.net>
2024-11-01 00:22:10 +0900

    fix : 함수의 길이가 15라인을 넘지 않도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,13 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static int[] statistics;
+
     public static int budget;
     public static int numberOfLotto;
+    public static ArrayList<Lotto> lottos;
     public static Lotto goals;
+    public static int bonusNumber;
+    public static int[] statistics;
 
     public static List<Integer> makeLotto(){
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public static void inputBudget(){
+        System.out.println("구입금액을 입력해 주세요.");
+        while(true){
+            try {
+                budget = Integer.parseInt(Console.readLine());
+                validateBudget();
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("[ERROR] 구입 금액은 숫자만 입력해야 합니다.");
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static void validateBudget(){
@@ -22,12 +40,58 @@ public class Application {
         }
     }
 
+    public static void calculateNumberOfLotto(){
+        numberOfLotto = budget/1000;
+        System.out.println();
+        System.out.println(numberOfLotto+"개를 구매했습니다.");
+    }
+
+    public static void pickNumbers(){
+        lottos = new ArrayList<>();
+        for(int i=0;i<numberOfLotto;i++){
+            lottos.add(new Lotto(makeLotto()));
+            System.out.println(lottos.get(i).toString());
+        }
+        System.out.println();
+    }
+
     public static void inputGoals(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        while(true){
+            try {
+                validateGoals();
+                break;
+            } catch(NumberFormatException e){
+                System.out.println("[ERROR] 로또 번호는 쉼표(,)로 구분된 숫자여야 합니다.");
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void validateGoals(){
         List<Integer> goalNumbers = new ArrayList<>();
         for (String number : Console.readLine().split(",")) {
             goalNumbers.add(Integer.parseInt(number.trim()));
         }
         goals = new Lotto(goalNumbers);
+    }
+
+    public static void inputBonus(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        while(true){
+            try {
+                bonusNumber = Integer.parseInt(Console.readLine());
+                validateBonusDuplicate(goals,bonusNumber);
+                validateNumberRange(bonusNumber);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 숫자여야 합니다.");
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println();
     }
 
     public static void validateBonusDuplicate(Lotto goals, int bonus){
@@ -40,6 +104,16 @@ public class Application {
         if (bonus < 1 || bonus > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    public static void printResult(){
+        statistics = new int[6];
+        for(Lotto lotto : lottos){
+            Rank rank = lotto.getRank(goals, bonusNumber);
+            statistics[rank.ordinal()]++;
+        }
+        printStatistics();
+        printProfitRate();
     }
 
     public static void printStatistics(){
@@ -68,69 +142,21 @@ public class Application {
 
     public static void main(String[] args) {
         // 1. 구매 금액 입력
-        System.out.println("구입금액을 입력해 주세요.");
-        while(true){
-            try {
-                budget = Integer.parseInt(Console.readLine());
-                validateBudget();
-                break;
-            } catch (NumberFormatException e){
-                System.out.println("[ERROR] 구입 금액은 숫자만 입력해야 합니다.");
-            } catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
+        inputBudget();
 
         // 2. 구매 개수 계산
-        numberOfLotto = budget/1000;
-        System.out.println();
-        System.out.println(numberOfLotto+"개를 구매했습니다.");
+        calculateNumberOfLotto();
 
         // 3. 로또 번호 뽑기
-        ArrayList<Lotto> lottos = new ArrayList<>();
-        for(int i=0;i<numberOfLotto;i++){
-            lottos.add(new Lotto(makeLotto()));
-            System.out.println(lottos.get(i).toString());
-        }
-        System.out.println();
+        pickNumbers();
 
         // 4. 당첨 번호 입력
-        System.out.println("당첨 번호를 입력해 주세요.");
-        while(true){
-            try {
-                inputGoals();
-                break;
-            } catch(NumberFormatException e){
-                System.out.println("[ERROR] 로또 번호는 쉼표(,)로 구분된 숫자여야 합니다.");
-            } catch(IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
+        inputGoals();
 
         // 5. 보너스 번호 입력
-        System.out.println("보너스 번호를 입력해 주세요.");
-        int bonusNumber;
-        while(true){
-            try {
-                bonusNumber = Integer.parseInt(Console.readLine());
-                validateBonusDuplicate(goals,bonusNumber);
-                validateNumberRange(bonusNumber);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 보너스 번호는 숫자여야 합니다.");
-            } catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
-        System.out.println();
+        inputBonus();
 
         // 6. 등수 계산 및 출력
-        statistics = new int[6];
-        for(Lotto lotto : lottos){
-            Rank rank = lotto.getRank(goals, bonusNumber);
-            statistics[rank.ordinal()]++;
-        }
-        printStatistics();
-        printProfitRate();
+        printResult();
     }
 }
