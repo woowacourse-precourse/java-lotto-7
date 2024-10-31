e5f59e60d54d58389c78c1b560fa4c3ae130f94a
Hyeo_Ni__ <maehyong02@gmail.com>
2024-10-31 17:05:05 +0900

    feat(input): Exception when amount is not divisible by 1,000

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,11 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
-        String buyAmountInput = Console.readLine();
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            String buyAmountInput = Console.readLine();
 
-        int buyAmount = Integer.parseInt(buyAmountInput);
+            int buyAmount = Integer.parseInt(buyAmountInput);
 
-        int lottoCount = buyAmount / 1000;
+            // 천원 단위가 아닌 경우 예외 발생
+            if (buyAmount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 천원 단위만 입력 가능합니다.");
+            }
+
+            int lottoCount = buyAmount / 1000;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
