662ca7d7b28ab983ceed03cb9f1e9e70a2e17340
whrjsgml0000 <whrjsgml4515@naver.com>
2024-10-31 23:41:34 +0900

    feat(view) : InputView와 OutputView 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public String getRequestPurchasePrice(){
+        System.out.println(REQUEST_PURCHASE_PRICE);
+        return Console.readLine();
+    }
+
+    public String getRequestWinningNumber(){
+        System.out.println(REQUEST_WINNING_NUMBER);
+        return Console.readLine();
+    }
+
+    public String getRequestBonusNumber(){
+        System.out.println(REQUEST_BONUS_NUMBER);
+        return Console.readLine();
+    }
+}
