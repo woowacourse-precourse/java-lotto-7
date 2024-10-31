a80585e38e464f42a9b13be44b3289ea7dd0f98a
rladmstn <oz33177351@gmail.com>
2024-11-01 01:47:38 +0900

    feat(output) : 당첨 결과 및 총 수익률 출력 메소드 추가
    - 당첨 결과 및 수익률을 출력하는 printLottoWinningResult 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.UserLotto;
 import lotto.enums.PrintMessage;
 
 public class OutputView {
@@ -10,5 +11,16 @@ public class OutputView {
         for(Lotto lotto : lotteries){
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printLottoWinningResult(UserLotto userLotto, float profitRate){
+        System.out.println(PrintMessage.OUTPUT_TOTAL_RESULT.getMessage());
+        System.out.println(PrintMessage.OUTPUT_FIFTH_PLACE_RESULT.format(userLotto.getWinningCount(5)));
+        System.out.println(PrintMessage.OUTPUT_FOURTH_PLACE_RESULT.format(userLotto.getWinningCount(4)));
+        System.out.println(PrintMessage.OUTPUT_THIRD_PLACE_RESULT.format(userLotto.getWinningCount(3)));
+        System.out.println(PrintMessage.OUTPUT_SECOND_PLACE_RESULT.format(userLotto.getWinningCount(2)));
+        System.out.println(PrintMessage.OUTPUT_FIRST_PLACE_RESULT.format(userLotto.getWinningCount(1)));
+
+        System.out.println(PrintMessage.OUTPUT_TOTAL_PROFIT_RATE.format(profitRate));
     }
 }
