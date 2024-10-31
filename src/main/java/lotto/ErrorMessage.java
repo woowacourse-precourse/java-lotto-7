d4f448cc3992d097d5949f500196310add5f4062
jangsubin <eltm521@naver.com>
2024-10-31 19:21:31 +0900

    feat : 당첨 번호가 쉼표와 숫자만 가지는지 검증기능 구현

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,7 +3,9 @@ package lotto;
 public enum ErrorMessage {
     ERROR_PREFIX("[Error]"),
     ERROR_PRICE_UNDER_ZERO("0이하 값은 입력할 수 없습니다."),
-    ERROR_PRICE_NOT_IN_UNITS_OF_1000("금액은 1000원 단위여야 합니다.")
+    ERROR_PRICE_NOT_IN_UNITS_OF_1000("금액은 1000원 단위여야 합니다."),
+    ERROR_DELIMETER_ONLY_HAS_COMMA("로또 번호는 숫자와 ','만 포함할 수 있습니다.")
+
     ;
 
 
--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,11 +1,29 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningLotto {
     private Lotto lotto;
     private int BonusNumber;
 
     public WinningLotto(String lottoNumber) {
+        validateInput(lottoNumber);
+        lotto = new Lotto(splitLottoNumber(lottoNumber));
     }
+
+    private List<Integer> splitLottoNumber(String lottoNumber) {
+
+        return Arrays.stream(lottoNumber.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+    private void validateInput(String lottoNumber) {
+        // 숫자와 쉼표만 허용하는 정규식 검증
+        if (!lottoNumber.matches("^[0-9,]+$")) {
+            throw new IllegalArgumentException(ErrorMessage.printError(ErrorMessage.ERROR_DELIMETER_ONLY_HAS_COMMA));
+        }
+    }
+
 }
