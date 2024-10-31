caa6f4e4adfa186eef93e0a8a0783b7d12528aef
corpi <lj845854@gmail.com>
2024-10-31 21:03:01 +0900

    refactor(InputValidator): 금액이 지정 단위로 나누어 떨어지는 경우를 검증할 수 있다.

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -3,19 +3,22 @@ package lotto.model;
 public class InputValidator {
     public void validateInputMoney(String input) {
         validateParseNumber(input);
+        validateDivideByUnit(input, 1000);
     }
 
     private void validateParseNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (Exception exception) {
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요.");
         }
     }
 
-    private void validateDivideByUnit(int input, int price) {
-        if (input % price != 0) {
-            throw new IllegalArgumentException("[ERROR] " + price + "원 단위의 값을 입력해주세요");
+    private void validateDivideByUnit(String input, int price) {
+        int parsedInput = Integer.parseInt(input);
+
+        if (parsedInput % price != 0) {
+            throw new IllegalArgumentException("[ERROR] " + price + "원 단위의 값을 입력해주세요.");
         }
     }
 }
