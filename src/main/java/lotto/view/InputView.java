e35fe38e906b7a80c8fb9bfa8dd15d1d725785a9
daxxn <dahyeonhan4529@gmail.com>
2024-10-31 17:39:18 +0900

    style(input): remove extra whitespace

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,36 +4,36 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.error.ExceptionMessage;
 
 public class InputView {
-    public String readInput(){
+    public String readInput() {
         String line = Console.readLine();
 
-        if(line.isEmpty()){
+        if (line.isEmpty()) {
             ExceptionMessage errorMessage = ExceptionMessage.ERROR_MESSAGE_INPUT_IS_EMPTY;
             throw new IllegalArgumentException(ExceptionMessage.createErrorMessage(errorMessage));
         }
         return line;
     }
 
-    public String inputValue(String inputMessage){
-        while(true){
-            try{
+    public String inputValue(String inputMessage) {
+        while (true) {
+            try {
                 System.out.println(inputMessage);
                 return readInput();
-            }catch(IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public String inputLottoCost(){
+    public String inputLottoCost() {
         return inputValue("구입금액을 입력해 주세요.");
     }
 
-    public String inputWinningNumber(){
+    public String inputWinningNumber() {
         return inputValue("당첨 번호를 입력해 주세요.");
     }
 
-    public String inputBonusNumber(){
+    public String inputBonusNumber() {
         return inputValue("보너스 번호를 입력해 주세요.");
     }
 }
