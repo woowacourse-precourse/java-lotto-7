2059b89444f26aafab42f3758df09de85e821bae
minjun <ijmtylhtf@naver.com>
2024-10-31 11:12:22 +0900

    feat : 예외 메시지 Enum 클래스에 로또 구입 금액 예외 메시지 추가

---
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package lotto.exception;
+
+public enum ExceptionMessage {
+	INVALID_PURCHASE_AMOUNT_INPUT("구입 금액은 1,000원 단위로 입력 받습니다.");
+	private final String message;
+
+	ExceptionMessage(String message) {
+		this.message = message;
+	}
+}
