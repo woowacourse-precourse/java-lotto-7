58b398545cc90ba762325a5d81fac75fefb23b66
J0YERIM <chosarah70@gmail.com>
2024-11-01 02:29:19 +0900

    refactor(view): InputValidator에서 기본 입력 유효성 검사 로직으로 단순화
    - 양수 여부와 1,000원 단위 검증은 도메인(PurchaseAmount)에서 수행하도록 변경
    - validatePurchaseAmount 메서드를 InputView에서 InputValidator로 이동하여,
    InputView가 형식 검사만 수행하도록 개선

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -2,23 +2,22 @@ package lotto.view;
 
 public class InputValidator {
 
-    public static void validateNotBlank(String input) {
+    public static void validatePurchaseAmount(String input) {
+        InputValidator.validateNotBlank(input);
+        InputValidator.validateInteger(input);
+    }
+
+    private static void validateNotBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력값은 비어 있지 않아야 합니다.");
         }
     }
 
-    public static void validateInteger(String input) {
+    private static void validateInteger(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 입력값은 정수여야 합니다.");
-        }
-    }
-
-    public static void validateThousandUnit(int input) {
-        if (input <= 0 || input % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 입력값은 1,000원 단위의 정수여야 합니다.");
         }
     }
 }
