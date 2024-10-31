73166c81f6298671ea7c64e5ba28318dfcc968f2
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-01 01:15:07 +0900

    chore(LottoUtils): 발생 오류 수정

--- src/main/java/lotto/util/LottoUtils.java
+++ src/main/java/lotto/util/LottoUtils.java
@@ -7,7 +7,7 @@ public class LottoUtils {
 
     public static void validateNumberRange(Integer bonusNumber) {
         if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
-            throw new IllegalStateException("[ERROR] 로또 번호는 당첨 번호와 중복되지 않는 1 ~ 45 사이의 정수를 입력해주세요.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 당첨 번호와 중복되지 않는 1 ~ 45 사이의 정수를 입력해주세요.");
         }
     }
 }
