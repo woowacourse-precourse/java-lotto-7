aab101808c7a54b41d24a119510907ba0ecad789
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-31 18:37:07 +0900

    feat($Lotto): 상수 정의
    - 로또 번호 수.
    - 로또 최소 번호.
    - 로또 최대 번호.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,11 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
