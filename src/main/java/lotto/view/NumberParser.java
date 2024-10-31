5f9c8e6d3b5a18459279d0895c1fe2fc4b57fff2
hanwool <hanwe92@gmail.com>
2024-10-31 14:41:16 +0900

    fix(NumberParser): Change location of NumberParser
    Change location of NumberParser
    because it is only used in inputView package

---
+++ src/main/java/lotto/view/NumberParser.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.validation.NumberValidator;
+
+public class NumberParser {
+    private static final String COMMA = ",";
+
+    public static List<Integer> parseLottoNumbers(String input) {
+        String[] splitNumbers = input.split(COMMA);
+        List<String> trimNumbers = new ArrayList<>();
+        for(String number: splitNumbers) {
+            trimNumbers.add(number.trim());
+        }
+        NumberValidator.validateNumberString(trimNumbers);
+        return trimNumbers.stream().map(Integer::parseInt).toList();
+    }
+
+    public static int parseBonusNumber(String input) {
+        String trimNumber = input.trim();
+        //TODO: 숫자 아닌 입력 validation 추가 필요
+        return Integer.parseInt(trimNumber);
+    }
+}
--- src/test/java/lotto/util/NumberParserTest.java
+++ src/test/java/lotto/util/NumberParserTest.java
@@ -3,6 +3,7 @@ package lotto.util;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import lotto.view.NumberParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
