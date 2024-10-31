744525085290e7b0a64a90b255378757ef1d6c42
parksomii <somi020605@gmail.com>
2024-11-01 02:38:59 +0900

    feat(InputValidate): 빈 값 유효성 검사 추가

--- src/main/java/lotto/util/InputValidate.java
+++ src/main/java/lotto/util/InputValidate.java
@@ -1,4 +1,9 @@
 package lotto.util;
 
 public class InputValidate {
+    public void blankValidate(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(Error.INPUT_BLANK_ERROR.message());
+        }
+    }
 }
