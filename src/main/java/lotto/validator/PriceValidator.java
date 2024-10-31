eb302fe578b83562e30422e9e18cd218dfa318ac
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-31 14:55:35 +0900

    refactor(PriceValidator): 사용자 입력을 다시 받는 기능 삭제

--- src/main/java/lotto/validator/PriceValidator.java
+++ src/main/java/lotto/validator/PriceValidator.java
@@ -4,23 +4,13 @@ import lotto.view.InputView;
 
 public class PriceValidator {
 
-    private final InputView inputView;
-
-    public PriceValidator(InputView inputView) {
-        this.inputView = inputView;
-    }
-
     public void validate(int price) {
-        try {
-            validatePositive(price);
-            validateThousandUnit(price);
-        } catch (IllegalArgumentException e) {
-            inputView.getPriceInput();
-        }
+        validatePositive(price);
+        validateThousandUnit(price);
     }
 
     private void validatePositive(int price) {
-        if (price < 0) {
+        if (price <= 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위의 양수를 입력해 주세요.");
         }
     }
