386d352bc3378095200e04556c800385e7de84e3
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-31 20:03:39 +0900

    test(LottoPurchaseAmountTest): 구입 금액이 1,000원 단위가 아니면 예외가 발생하는 테스트 작성
    해당 테스트는 실패한다.

--- src/test/java/lotto/domain/LottoPurchaseAmountTest.java
+++ src/test/java/lotto/domain/LottoPurchaseAmountTest.java
@@ -17,4 +17,13 @@ public class LottoPurchaseAmountTest {
                 .hasMessage("구입 금액은 양수만 입력 가능합니다.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1001", "100", "10", "5500"})
+    @DisplayName("로도 구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    void validatePurchaseAmountUnit(String input) {
+        assertThatThrownBy(() -> LottoPurchaseAmount.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구입 금액은 1,000원 단위로 입력할 수 있습니다.");
+    }
+
 }
