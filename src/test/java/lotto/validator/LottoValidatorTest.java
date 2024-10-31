e7f1065a7773051482eba615c0023f4e89bc7815
Jewan1120 <hawaii1468@gmail.com>
2024-11-01 00:08:29 +0900

    test(Validator): 로또 구입 금액 예외 발생 테스트 추가

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -1,4 +1,16 @@
 package lotto.validator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class LottoValidatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"-1", "jewan", "1 2", "1234"})
+    void 로또_구입_금액_예외_발생(String inputString) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> LottoValidator.validateInputMoney(inputString));
+    }
 }
