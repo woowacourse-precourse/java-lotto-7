85edb8e0f862e57eb8c678e8a05a226ae9ff2043
parksomii <somi020605@gmail.com>
2024-11-01 02:57:06 +0900

    feat(LottoGameController): 구입 금액 입력 기능 및 예외 처리 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,11 +1,13 @@
 package lotto.controller;
 
+import lotto.domain.Money;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
     private OutputView outputView = new OutputView();
     private InputView inputView = new InputView();
+    private Money money;
 
     public void run() {
         gameStart();
@@ -20,3 +22,14 @@ public class LottoGameController {
         // 기능 추가 예정
     }
 }
+    private void createMoney() {
+        while (true) {
+            try {
+                money = new Money(inputView.inputMoney());
+                break;
+            } catch (IllegalArgumentException e) {
+                outputView.println(e.getMessage());
+            }
+        }
+    }
+
