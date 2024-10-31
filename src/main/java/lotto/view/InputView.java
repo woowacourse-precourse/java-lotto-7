b621cbae09e82b7645b2011f73620fc0c8e2deb0
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-31 21:41:05 +0900

    feat(InputView, ConsoleInputView): 콘솔 인풋 뷰 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import java.util.List;
+
+public interface InputView {
+
+    int readNumber();
+
+    List<Integer> readNumbers();
+}
---
+++ src/main/java/lotto/view/console/ConsoleInputView.java
@@ -0,0 +1,27 @@
+package lotto.view.console;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.LottoInputConvertor;
+import lotto.view.InputView;
+
+public class ConsoleInputView implements InputView {
+
+    private static final String DELIMITER_COMMA = ",";
+
+    @Override
+    public int readNumber() {
+        final String input = readInput();
+        return LottoInputConvertor.parseToInt(input);
+    }
+
+    @Override
+    public List<Integer> readNumbers() {
+        final String input = readInput();
+        return LottoInputConvertor.parseToNumbers(DELIMITER_COMMA, input);
+    }
+
+    private String readInput() {
+        return Console.readLine();
+    }
+}
