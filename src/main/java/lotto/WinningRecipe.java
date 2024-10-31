edaeb9405fce72a9c308fd4dddc2f72273dfe26c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-31 22:01:06 +0900

    refactor(WinningRecipe): 정적 팩토리 메서드 파라미터 수정

--- src/main/java/lotto/WinningRecipe.java
+++ src/main/java/lotto/WinningRecipe.java
@@ -1,13 +1,16 @@
 package lotto;
 
 import java.math.BigDecimal;
+import java.util.Map.Entry;
 
 public record WinningRecipe(
         int rank,
         BigDecimal price,
         int matchCount
 ) {
-    public static WinningRecipe of(final LottoRank lottoRank, final int matchCount) {
+    public static WinningRecipe of(final Entry<LottoRank, Integer> entry) {
+        final LottoRank lottoRank = entry.getKey();
+        final Integer matchCount = entry.getValue();
         return new WinningRecipe(
                 lottoRank.getRank(),
                 lottoRank.getPrice(),
