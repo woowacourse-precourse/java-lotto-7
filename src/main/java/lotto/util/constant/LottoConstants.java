dfc996148985d31d1c807d8fc1dacc4149a32e42
qudcks8084 <qudcks8084@naver.com>
2024-10-31 18:55:07 +0900

    Fix : 공통 상수들의 접근 제어자 수정
    - 기존에 private으로 선언되어있던 상수들의 접근 제어자를 public으로 변경

--- src/main/java/lotto/util/constant/LottoConstants.java
+++ src/main/java/lotto/util/constant/LottoConstants.java
@@ -1,8 +1,8 @@
 package lotto.util.constant;
 
 public class LottoConstants {
-    private static final int LOTTO_BASE_UNIT = 1000;
-    private static final int LOTTO_MIN_NUMBER = 1;
-    private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_NUM_COUNT = 6;
+    public static final int LOTTO_BASE_UNIT = 1000;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_NUM_COUNT = 6;
 }
