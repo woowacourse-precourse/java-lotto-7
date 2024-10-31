ed228dafc057cecd6baa2eb390ad0d4b09c300fe
Hyso <hsk7953@gmail.com>
2024-10-31 15:34:12 +0900

    feat(ExceptionMessage) : 구입 금액 예외 상황에 대한 상수 추가 - 0으로 시작하는 숫자의 경우 - 숫자가 아닌 다른 문자의 경우

--- src/main/java/lotto/enums/ExceptionMessage.java
+++ src/main/java/lotto/enums/ExceptionMessage.java
@@ -3,6 +3,8 @@ package lotto.enums;
 public enum ExceptionMessage {
     NOT_BLANK("빈 칸은 입력할 수 없습니다."),
     INVALID_MONEY_UNIT("금액의 단위는 1000원이어야 합니다."),
+    INVALID_ZERO_START("0으로 시작하는 숫자는 입력할 수 없습니다."),
+    INVALID_NON_NUMERIC("숫자외에 문자는 입력할 수 없습니다."),
     DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다."),
     OUT_OF_RANGE("1부터 45 사이의 숫자만 입력 가능합니다.");
 
