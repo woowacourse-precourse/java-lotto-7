f8b8eefa86fcb56a2ab9de062032e228fde4b54b
sanghee0820 <lsh277604@gmail.com>
2024-10-31 22:34:47 +0900

    feat: 구매 금액 제약조건 검증 기능 추가

---
+++ src/main/java/lotto/model/Budget.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import lotto.exception.BudgetErrorMessage;
+
+public class Budget {
+    private static final Integer MIN_BUDGET = 0;
+    private static final Integer BUDGET_UNIT = 1000;
+
+    private Integer budget;
+
+    public Budget(Integer budget) {
+        validateBudget(budget);
+        this.budget = budget;
+    }
+
+    private void validateBudget(Integer budget) {
+        validateBudgetRange(budget);
+        validateBudgetUnit(budget);
+    }
+
+    private void validateBudgetRange(Integer budget) {
+        if (budget < MIN_BUDGET) {
+            throw new IllegalArgumentException(BudgetErrorMessage.BUDGET_RANGE_ERROR.getMessage());
+        }
+    }
+
+    private void validateBudgetUnit(Integer budget) {
+        if (budget % BUDGET_UNIT == 0) {
+            return;
+        }
+
+        throw new IllegalArgumentException(BudgetErrorMessage.BUDGET_UNIT_ERROR.getMessage());
+    }
+}
