95c23930e32dfee8449468746849e84cf5aa3df1
dltmddus <tmddus4671@gmail.com>
2024-10-31 22:19:48 +0900

    feat: Lotto 입력 검증 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import static lotto.view.input.InputError.LOTTO_NUMBER_LENGTH_INVALID;
+
 import java.util.List;
+import lotto.view.input.InputError;
+import lotto.view.input.InvalidInputException;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +15,34 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoLength(numbers);
+        validateLottoRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+
+    private void validateLottoLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new InvalidInputException(InputError.LOTTO_NUMBER_LENGTH_INVALID);
+        }
+    }
+    private void validateLottoRange(List<Integer> numbers) {
+        if (isLottoRange(numbers)) {
+            throw new InvalidInputException(InputError.LOTTO_NUMBER_RANGE_INVALID);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isLottoRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(number -> number >= 1 && number <= 45);
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        if(containsDuplicates(numbers)){
+            throw new InvalidInputException(InputError.LOTTO_NUMBER_CANNOT_DUPLICATE);
+        }
+    }
+    private boolean containsDuplicates(List<Integer> numbers){
+        return numbers.stream().distinct().count() == numbers.size();
+    }
+
 }
