a416d0a216ae50c64e473a5300013cfe8ceb242c
EHCTH <lees90896@gmail.com>
2024-10-31 20:28:27 +0900

    test(InputValidatorTest): 입력 예외 테스트

--- src/test/java/lotto/view/input/validator/InputValidatorTest.java
+++ src/test/java/lotto/view/input/validator/InputValidatorTest.java
@@ -1,4 +1,49 @@
 package lotto.view.input.validator;
 
-public class InputValidator {
+import lotto.constant.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class InputValidatorTest {
+    private static final String BLANK = "";
+    private static final String CHAR = "character";
+
+    @Test
+    @DisplayName("입력 예외 Money")
+    void inputMoneyTest() {
+        List<String> exceptionData = List.of(BLANK, CHAR);
+        verifyException(exceptionData, InputValidator::moneyValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
+    }
+
+    @Test
+    @DisplayName("입력 예외 BonusNumber")
+    void inputBonusNumberTest() {
+        List<String> exceptionData = List.of(BLANK, CHAR);
+        verifyException(exceptionData, InputValidator::bonusNumberValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
+    }
+
+    @Test
+    @DisplayName("입력 예외 WinningLotto")
+    void inputWinningLottoTest() {
+        List<String> exceptionData = List.of(
+                BLANK, CHAR, "1,,2,,3,,4,,5,,6", "1,2,3,4,5,6,",
+                ",2,3,4,5,6", "1.2.3.4.5.6", "1 ,2,3,4,5,6",
+                "1,2,3,4,5,6 "
+        );
+        verifyException(exceptionData, InputValidator::winningLottoValidate, ErrorMessage.LOTTO_REGEX_PATTERN);
+    }
+
+
+    private void verifyException(List<String> exceptionData, Consumer<String> validate, ErrorMessage errorMessage) {
+        exceptionData.forEach((data) -> {
+            assertThatThrownBy(() -> validate.accept(data))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(errorMessage.getMessage());
+        });
+    }
 }
