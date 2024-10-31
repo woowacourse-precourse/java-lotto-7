810f840ae49cb4de8f8fe4bc5682b17b97784974
ohhyungsuh <ohhyungsuh@gmail.com>
2024-11-01 01:22:59 +0900

    refactor: FrontController 추가
    - main에서 요청을 FrontController가 받는 걸로 수정
    - FrontController가 LottoController 동작

---
+++ src/main/java/lotto/controller/FrontController.java
@@ -0,0 +1,27 @@
+package lotto.controller;
+
+import lotto.repository.LottoRepository;
+import lotto.repository.LottoRepositoryImpl;
+import lotto.service.LottoService;
+import lotto.service.LottoServiceImpl;
+
+import java.util.List;
+
+import static lotto.view.UserView.*;
+
+public class FrontController {
+    private final LottoController lottoController;
+
+    public FrontController() {
+        LottoService lottoService = new LottoServiceImpl(LottoRepositoryImpl.getInstance());
+        this.lottoController = new LottoController(lottoService);
+    }
+
+    public void start() {
+        int amount = printAndGetAmount();
+        List<Integer> winNumbers = printAndGetWinNumbers();
+        int bonusNumber = printAndGetBonusNumber(winNumbers);
+
+        lottoController.start(amount, winNumbers, bonusNumber);
+    }
+}
