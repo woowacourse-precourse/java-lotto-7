0dc73ba6a13524635a7bbf36ec06bd5cce275062
Hajuuu <hpt0127@naver.com>
2024-10-31 21:42:06 +0900

    feat: 금액 입력 예외 처리 메서드 구현 및 상수 추가
    구입 금액이 1,000원 단위가 아니거나 숫자가 아닐 경우 예외를 발생시키는 메서드 구현,
    구입 금액 입력 메서드에서 입력받은 금액을 int로 반환하도록 반환 값 변경 및 안내 메시지 상수 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,9 +4,24 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
-    public void inputPurchaseAmount() {
-        String input = readLine();
-        int amount = Integer.parseInt(input);
+    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ERROR_MESSAGE_AMOUNT = "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.";
+
+    public int inputPurchaseAmount() {
+        System.out.println(INPUT_AMOUNT_MESSAGE);
+        return checkAmount(readLine());
     }
 
+    private int checkAmount(String input) {
+        int amount = 0;
+        try {
+            amount = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_AMOUNT);
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_AMOUNT);
+        }
+        return amount;
+    }
 }
