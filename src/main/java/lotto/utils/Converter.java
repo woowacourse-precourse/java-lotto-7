549e20506c6470d50e3956a38c692262c021b675
sinminseok12 <sinminseok12@naver.com>
2024-11-01 01:35:32 +0900

    feat(InputView) : 당첨 번호를 입력하는 기능 구현
    사용자로부터 당첨 번호를 입력받고 List<Integer> 타입으로 변경해 반환하는 기능을 구현했다.

--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -1,11 +1,23 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Converter {
+    public static final String DELIMITER = ",";
 
     private Converter(){
     }
 
     public static int convertToNumber(String input) {
         return Integer.parseInt(input.trim());
+    }
+
+    public static List<Integer> convertNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .map(Converter::convertToNumber)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,11 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Converter;
 
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_BUY_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     private InputView(){
     }
@@ -13,6 +16,12 @@ public class InputView {
         System.out.println(INPUT_BUY_MONEY_MESSAGE);
         String input = Console.readLine();
         return Converter.convertToNumber(input);
+    }
+
+    public static List<Integer> inputWinNumbers() {
+        System.out.println(INPUT_WIN_NUMBERS_MESSAGE);
+        String input = Console.readLine();
+        return Converter.convertNumbers(input);
     }
 
 }
