c29522b35e7b87d12b97196cfa8b62ba62ccd2f0
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 21:15:54 +0900

    refactor: 1,000원 단위 테스트 분리

--- src/test/java/lotto/validation/PurchaseValidationTest.java
+++ src/test/java/lotto/validation/PurchaseValidationTest.java
@@ -5,17 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import static lotto.constants.ErrorMessage.PURCHASE_MONEY_ONLY_CAN_NUMBER;
 import static lotto.constants.ErrorMessage.PURCHASE_MONEY_ONLY_CAN_THOUSAND_UNIT;
-import static lotto.util.PurchaseUtils.getThousandUnitCount;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseValidationTest {
-    @Test
-    @DisplayName("1,000원 단위로 로또 구매한 수")
-    void 천원_단위로_로또_구매한_수() {
-        int thousandUnitCount = getThousandUnitCount("10000");
-        assertThat(thousandUnitCount).isEqualTo(10);
-    }
 
     @Test
     @DisplayName("숫자로 로또 구매하지 않으면 예외가 발생한다.")
