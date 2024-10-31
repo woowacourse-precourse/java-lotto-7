11439dbd5fac54fb77184393ddac5349edb70d5c
world-dv <goodforx0147@naver.com>
2024-11-01 02:09:46 +0900

    feat: 구입한 로또 개수 계산
    - 지갑에서 돈을 꺼내 로또 구입 개수를 계산합니다.
    - 이때 돈은 1000원 단위입니다.

---
+++ src/main/java/lotto/service/Payment.java
@@ -0,0 +1,18 @@
+package lotto.service;
+
+import lotto.domain.Wallet;
+
+public class Payment {
+
+    private static final Integer MIN_MONEY_UNIT = 1000;
+
+    private final Wallet wallet;
+
+    public Payment(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public Integer getLottoCount() {
+        return wallet.getMoney() / MIN_MONEY_UNIT;
+    }
+}
