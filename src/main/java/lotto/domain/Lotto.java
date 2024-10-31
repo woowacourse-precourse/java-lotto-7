ee5bef7e512f71ee541e80967f0b55c285b462b3
IM-GYURI <kgulr0517@gmail.com>
2024-11-01 05:03:58 +0900

    feat : Lotto 기능 추가 및 수정
    - record로 변경
    - validateNumbers() : 검증 통합
    - validateRange() : 1 ~ 45 사이의 숫자인지 검증
    - validateDuplicated() : 숫자들 간 중복 여부 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,26 +1,42 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.exception.ErrorMessage;
 
-public class Lotto {
+public record Lotto(List<Integer> numbers) {
 
+    private static final int START_OF_RANGE = 1;
+    private static final int END_OF_RANGE = 45;
     private static final int LOTTO_COUNT = 6;
 
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Lotto {
+        validateNumbers(numbers);
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    private void validateNumbers(List<Integer> numbers) {
+        validateCount(numbers);
+        validateDuplicated(numbers);
+        numbers.forEach(this::validateRange);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateCount(List<Integer> numbers) {
         if (numbers.size() != LOTTO_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_COUNT_NOT_SIX.getMessage());
+        }
+    }
+
+    private void validateRange(int number) {
+        if (number < START_OF_RANGE || number > END_OF_RANGE) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_RANGE_NOT_MATCH.getMessage());
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        Set<Integer> duplicateCheck = new HashSet<>(numbers);
+        if (duplicateCheck.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_DUPLICATED.getMessage());
         }
     }
 }
