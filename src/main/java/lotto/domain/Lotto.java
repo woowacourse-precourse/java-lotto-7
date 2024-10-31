10608c08a2e82e07bef9aef613fae0eb8aa68b23
world-dv <goodforx0147@naver.com>
2024-10-31 11:49:57 +0900

    feat: 로또 번호 범위 검증
    - 로또 번호의 범위를 1에서 45 사이로 제한하기 위해 숫자 범위를 검증합니다.
    - 상수를 이용해 숫자 범위의 최소와 최대를 나타냅니다.
    - 로또 번호 중 숫자 범위에 벗어나는 경우가 존재한다면 예외를 발생시킵니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,11 +5,14 @@ import java.util.List;
 public class Lotto {
 
     private static final Integer LOTTO_NUMBER_COUNT = 6;
+    private static final Integer MIN_NUMBER_RANGE = 1;
+    private static final Integer MAX_NUMBER_RANGE = 45;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateNumberCount(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
@@ -17,5 +20,15 @@ public class Lotto {
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(this::IsNumberRangeInCorrect)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean IsNumberRangeInCorrect(Integer number) {
+        return number < MIN_NUMBER_RANGE && number > MAX_NUMBER_RANGE;
     }
 }
