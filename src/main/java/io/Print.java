8024c45a48dd33a2537796441697b90c05a61f01
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2024-11-01 03:29:34 +0900

    feat: 로또 구입 개수 출력
    System.out.println(lottoNumber)를 문장 print로 변경

--- src/main/java/io/Print.java
+++ src/main/java/io/Print.java
@@ -2,6 +2,7 @@ package io;
 
 public class Print {
     public static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해주세요.";
+    public static final String NUMBER_PRINT_MESSAGE = "개를 구입했습니다.";
     public final
 
     String message;
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,12 @@
 package lotto;
 
-import io.Print;
-
 public class Application {
     public static void main(String[] args) {
-        Print printMessage = new Print(io.Print.MONEY_INPUT_MESSAGE);
+        io.Print printMessage = new io.Print(io.Print.MONEY_INPUT_MESSAGE);
         printMessage.print();
         int lottoNumber = exception.Handler.getLottoNumber();
-        System.out.println(lottoNumber);
+
+        printMessage = new io.Print(lottoNumber + io.Print.NUMBER_PRINT_MESSAGE);
+        printMessage.print();
     }
 }
