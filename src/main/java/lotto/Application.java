b0a48f4655cda55fbd6037a7cfc47d68c48ec639
moon-akdong <problemy0913@gmail.com>
2024-10-31 15:33:16 +0900

    Implementing the ability to receive money
    Application과 ApplicationTest에 돈을 입력 받고 에러 테스트까지 구현
    Error Case 1000원 이하의 금액, 1000으로 떨어지지 않는 금액, 음수 입력, 금액이 아닌 다른 문자가 포함된 경우

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputMoney;
+        while (true){
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                inputMoney = Console.readLine();
+                moneyErrorCheck(inputMoney);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+    public static void moneyErrorCheck(String inputMoney){
+        int money;
+        try {
+            money = Integer.parseInt(inputMoney);
+            if (money < 1000 ) {
+                throw new IllegalArgumentException("[ERROR] 1000원 이하의 금액 입니다.");
+            } else if (money%1000 !=0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위 금액을 입력하세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식이 아닙니다.");
+        }
     }
 }
