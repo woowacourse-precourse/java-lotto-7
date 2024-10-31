1d42530530c52489e0349f49cf46b73fffebe2eb
EHCTH <lees90896@gmail.com>
2024-10-31 21:26:09 +0900

    refactor(InputValidatorTest): 검증클래스 정적 메서드 사용

--- src/test/java/lotto/view/input/validator/InputValidatorTest.java
+++ src/test/java/lotto/view/input/validator/InputValidatorTest.java
@@ -1,13 +1,11 @@
 package lotto.view.input.validator;
 
 import lotto.constant.ErrorMessage;
+import lotto.util.ValidationProcess;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.function.Consumer;
-
-import static org.assertj.core.api.Assertions.*;
 
 public class InputValidatorTest {
     private static final String BLANK = "";
@@ -17,14 +15,14 @@ public class InputValidatorTest {
     @DisplayName("입력 예외 Money")
     void inputMoneyTest() {
         List<String> exceptionData = List.of(BLANK, CHAR);
-        verifyException(exceptionData, InputValidator::moneyValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
+        ValidationProcess.inputThrownBy(exceptionData, InputValidator::moneyValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
     }
 
     @Test
     @DisplayName("입력 예외 BonusNumber")
     void inputBonusNumberTest() {
         List<String> exceptionData = List.of(BLANK, CHAR);
-        verifyException(exceptionData, InputValidator::bonusNumberValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
+        ValidationProcess.inputThrownBy(exceptionData, InputValidator::bonusNumberValidate, ErrorMessage.NUMBER_REGEX_PATTERN);
     }
 
     @Test
@@ -35,15 +33,6 @@ public class InputValidatorTest {
                 ",2,3,4,5,6", "1.2.3.4.5.6", "1 ,2,3,4,5,6",
                 "1,2,3,4,5,6 "
         );
-        verifyException(exceptionData, InputValidator::winningLottoValidate, ErrorMessage.LOTTO_REGEX_PATTERN);
-    }
-
-
-    private void verifyException(List<String> exceptionData, Consumer<String> validate, ErrorMessage errorMessage) {
-        exceptionData.forEach((data) -> {
-            assertThatThrownBy(() -> validate.accept(data))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(errorMessage.getMessage());
-        });
+        ValidationProcess.inputThrownBy(exceptionData, InputValidator::winningLottoValidate, ErrorMessage.LOTTO_REGEX_PATTERN);
     }
 }
