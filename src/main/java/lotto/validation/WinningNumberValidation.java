ab5458c5cf563065a89da47c2b194c2ed5f1a5bd
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 22:54:18 +0900

    feat: 당첨 번호 중복 제거 및 중복(6자리가 아닐)시 예외처리 구현

--- src/main/java/lotto/validation/WinningNumberValidation.java
+++ src/main/java/lotto/validation/WinningNumberValidation.java
@@ -2,17 +2,34 @@ package lotto.validation;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_ONLY_CAN_LENGTH_6;
+import static lotto.constants.LottoNumbers.LOTTO_NUMBER_COUNT;
 
 public class WinningNumberValidation {
     private final static String DELIMITER = ",";
 
     public static void validate(String inputWinningNumbers) {
         List<String> winningNumbers = setTrimNumbers(inputWinningNumbers);
+        winningNumbers = setUniqueNumbers(winningNumbers);
     }
 
     public static List<String> setTrimNumbers(String inputWinningNumbers) {
         return Arrays.stream(inputWinningNumbers.split(DELIMITER))
                 .map(String::trim)
                 .toList();
+    }
+    public static List<String> setUniqueNumbers(List<String> winningNumbers) {
+        if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ONLY_CAN_LENGTH_6.getErrorMessage());
+        }
+
+        Set<String> uniqueWinningNumbers = Set.copyOf(winningNumbers);
+        if (uniqueWinningNumbers.size() < winningNumbers.size()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ONLY_CAN_LENGTH_6.getErrorMessage());
+        }
+
+        return uniqueWinningNumbers.stream().toList();
     }
 }
