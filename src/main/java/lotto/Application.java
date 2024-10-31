8493f65e62128bb2019f4a8a567fb8600c13b4ab
tdddt <doto1116@ewhain.net>
2024-10-31 14:20:56 +0900

    feat : 구입 금액이 1000원 미만인 경우 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,5 +8,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         int budget = Integer.parseInt(Console.readLine());
+        if(budget<1000){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+        }
     }
 }
