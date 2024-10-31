e321ff7665fa9e6ad6ff67e87bf967a573977208
WARDKEY <tjrmsduq@naver.com>
2024-10-31 17:31:11 +0900

    feat(LottoMachine) : 구매한 로또 번호 출력 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,16 +1,27 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoMachine {
+
+    private final List<List<Integer>> lottoNumbers = new ArrayList<>();
+
     public void run() {
         System.out.println("구입금액을 입력해 주세요.");
 
         int cost = Integer.parseInt(Console.readLine());
 
-        System.out.println(cost/1000 + "개를 구매했습니다.");
+        int purchaseCount = cost/1000;
 
+        System.out.println(purchaseCount + "개를 구매했습니다.");
 
+        for (int i = 0; i < purchaseCount; i++) {
+            lottoNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            System.out.println(lottoNumbers.get(i));
+        }
 
     }
 }
