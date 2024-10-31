3e65b0cab576b7de725f67711e017fa769eb2592
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-31 21:55:45 +0900

    refactor(BonusNumber): getter 제거

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -15,10 +15,6 @@ public class BonusNumber {
         return numbers.contains(value);
     }
 
-    public int getValue() {
-        return value;
-    }
-
     private void validate(final int value) {
         final LottoNumberValidator lottoNumberValidator = new LottoNumberValidator();
         lottoNumberValidator.validateRange(value, 1, 45);
