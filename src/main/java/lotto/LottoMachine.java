c46c8de652860f7dd1d2f39ccaff423c4e581cf0
SeongbinPark <hyucse22@gmail.com>
2024-10-31 09:00:33 +0900

    refactor(LottoMachine): printLottos 메서드 추출

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,4 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void purchaseLottos(int purchaseAmount) {
+        int count = purchaseAmount / 1000;
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateLotto());
+        }
+    }
+
+    private Lotto generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    public void printLottos() {
+        System.out.println();
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
 }
