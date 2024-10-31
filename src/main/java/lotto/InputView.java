6e746e11aab557e5acd9628f53e59a0e7d69c579
Hajuuu <hpt0127@naver.com>
2024-10-31 22:03:55 +0900

    fix: 조건문 변경 및 접근제어자 변경
    테스트 후 1,000원 미만의 금액이 예외 처리되지 않는 것을 확인하여 조건문 변경, 접근제어자 변경

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,19 +7,20 @@ public class InputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String ERROR_MESSAGE_AMOUNT = "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.";
 
+
     public int inputPurchaseAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
         return checkAmount(readLine());
     }
 
-    private int checkAmount(String input) {
+    public int checkAmount(String input) {
         int amount = 0;
         try {
             amount = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE_AMOUNT);
         }
-        if (amount % 1000 != 0) {
+        if (amount < 1000 || amount % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE_AMOUNT);
         }
         return amount;
