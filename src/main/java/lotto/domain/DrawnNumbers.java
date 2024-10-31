5fc097415439f2e49eb46fba873eb13a4d6568c5
woo <ldw28517@naver.com>
2024-10-31 22:51:28 +0900

    feat: 보너스 번호는 당첨 번호와 중복될 수 없다
    - BonusNumber는 숫자 자체에 대한 검증만 실시
    - DrawnNumbers를 생성하는 시점에 보너스 번호가 당첨 번호에 포함되는지 확인
    - 중복될 시 보너스 번호 재입력
    - Number의 동등성 비교를 위해 equals() & hashCode() 재정의

---
+++ src/main/java/lotto/domain/DrawnNumbers.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import lotto.common.validator.DrawnNumbersValidator;
+
+public class DrawnNumbers {
+    private final WinningNumbers winningNumbers;
+    private final BonusNumber bonusNumber;
+
+    public DrawnNumbers(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        DrawnNumbersValidator.validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}
--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import lotto.common.validator.NumberValidator;
 
+import java.util.Objects;
+
 public class Number {
     private final int number;
 
@@ -12,5 +14,18 @@ public class Number {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number compare = (Number) o;
+        return number == compare.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
