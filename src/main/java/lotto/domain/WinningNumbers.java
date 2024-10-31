ecdeb49755344858f1a4bdc531b8f7b65dacfc11
woo <ldw28517@naver.com>
2024-10-31 20:15:40 +0900

    refactor(WinningNumbers): 메서드 분리
    - 생성자 내부 형변환 관련 스트림 로직 별도의 메서드로 분리

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import lotto.common.validator.LottoValidator;
-import lotto.common.validator.NumberValidator;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,8 +9,12 @@ public class WinningNumbers {
     private final List<Number> numbers;
 
     public WinningNumbers(List<String> numbers) {
-        LottoValidator.validate(numbers);
-        this.numbers = numbers.stream()
+        this.numbers = createNumbers(numbers);
+        LottoValidator.validate(this.numbers);
+    }
+
+    private List<Number> createNumbers(List<String> numbers) {
+        return numbers.stream()
                 .map(s -> new Number(s))
                 .collect(Collectors.toList());
     }
