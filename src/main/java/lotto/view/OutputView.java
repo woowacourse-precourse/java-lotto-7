9c7e6a82837f0e7627c27a36b51c6479fe2058bb
EunSeop <doosan1112@naver.com>
2024-10-31 12:29:10 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - %d개";
+    private static final String FIVE_BONUS_MATCH = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    private static final String FIVE_MATCH = "5개 일치 (1,500,000원) - %d개";
+    private static final String FOUR_MATCH = "4개 일치 (50,000원) - %d개";
+    private static final String THREE_MATCH = "3개 일치 (5,000원) - %d개";
+    private static final String LOTTO_RATE_MESSAGE = "총 수익률은 %2f%%입니다.";
+
     public void printPurchaseGuide() {
         System.out.println("로또를 구입할 금액을 입력하세요.");
     }
@@ -26,5 +33,17 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printLottoResult(List<Integer> result) {
+        System.out.println(String.format(SIX_MATCH, result.get(0)));
+        System.out.println(String.format(FIVE_BONUS_MATCH, result.get(1)));
+        System.out.println(String.format(FIVE_MATCH, result.get(2)));
+        System.out.println(String.format(FOUR_MATCH, result.get(3)));
+        System.out.println(String.format(THREE_MATCH, result.get(4)));
+    }
+
+    public void printLottoRate(double rate) {
+        System.out.println(String.format(LOTTO_RATE_MESSAGE, rate));
     }
 }
