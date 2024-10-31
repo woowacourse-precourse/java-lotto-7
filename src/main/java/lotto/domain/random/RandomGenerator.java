2af2e7e5b54a7e91633926e4d92afd83812841cb
minSsan <msp1125@hanyang.ac.kr>
2024-10-31 15:43:52 +0900

    feat(RandomGenerator): 랜덤 숫자 리스트 생성 추상화 및 구현체 추가

---
+++ src/main/java/lotto/domain/random/RandomGenerator.java
@@ -0,0 +1,7 @@
+package lotto.domain.random;
+
+import java.util.List;
+
+public interface RandomGenerator {
+    List<Integer> generate();
+}
---
+++ src/main/java/lotto/domain/random/RandomUniqueNumbersGenerator.java
@@ -0,0 +1,15 @@
+package lotto.domain.random;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.lotto.vo.LottoNumber;
+
+import java.util.List;
+
+public class LottoGenerator implements RandomGenerator {
+    private static final int COUNT = 6;
+
+    @Override
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(LottoNumber.MIN, LottoNumber.MAX, COUNT);
+    }
+}
