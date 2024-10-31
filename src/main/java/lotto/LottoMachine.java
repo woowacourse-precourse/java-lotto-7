f3aab3eb3624265306ec3296389130ecf51ef96a
WARDKEY <tjrmsduq@naver.com>
2024-10-31 17:08:13 +0900

    feat(LottoMachine) : 구입금액 입력 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoMachine {
     public void run() {
         System.out.println("구입금액을 입력해 주세요.");
+
+        int cost = Integer.parseInt(Console.readLine());
+
     }
 }
