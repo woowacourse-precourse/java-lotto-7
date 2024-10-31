212fe2fb6112646ae6dd962cb9bd7774281aa016
sanghee0820 <lsh277604@gmail.com>
2024-10-31 16:36:11 +0900

    feat: 로또 생성 시 정렬 후 저장 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 import lotto.exception.LottoErrorMessage;
 
@@ -7,10 +8,13 @@ public class Lotto {
     public static final int LOTTO_SIZE = 6;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateNumbers(numbers);
+
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
