5bb75a714c6a63f00d60629a17cf0ba8a1d58125
slay <palgalow@gmail.com>
2024-10-31 22:56:15 +0900

    feat(InputView): 구입금액 입력 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public int inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
+}
