5e9ee527577aae070b7bca83b821f0a021bc841d
DongchannN <ldc991104@gmail.com>
2024-10-31 23:50:38 +0900

    feat: 사용자의 로또를 관리하는 객체 구현

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public Lottos from(int amount) {
+        List<Lotto> lottos = new ArrayList<>(amount);
+        for (int i = 0; i < amount; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(lotto));
+        }
+        return new Lottos(lottos);
+    }
+}
