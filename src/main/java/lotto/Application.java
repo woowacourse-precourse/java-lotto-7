2dbf61ff578220f147cf89f3e43fb7feb297621b
IM-GYURI <kgulr0517@gmail.com>
2024-10-31 23:01:55 +0900

    feat : Application에서 LottoController의 startLottoDraw() 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,5 +7,6 @@ public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         LottoController lottoController = appConfig.getLottoController();
+        lottoController.startLottoDraw();
     }
 }
