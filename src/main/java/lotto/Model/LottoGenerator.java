cc61fbaedfd8e25af3b21db77fd47fc525cdb93e
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-31 15:18:30 +0900

    feat(LottoGeneartor): 로또 번호 발행 기능 구현

--- src/main/java/lotto/Model/LottoGenerator.java
+++ src/main/java/lotto/Model/LottoGenerator.java
@@ -1,10 +1,14 @@
 package lotto.Model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoGenerator {
     public List<String> genearteLotto(int price){
         int lottoCount = price / 1000;
 
+    }
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
