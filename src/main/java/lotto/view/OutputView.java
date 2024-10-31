c2c640e4db13d80765fae8ca3648257ab8a79c3e
dbdb456 <adad568@naver.com>
2024-10-31 16:27:15 +0900

    feat(outputview): 로또 번호 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,13 @@ import lotto.domain.Lotto;
 public class OutputView {
 
     public void printLottoQuantity(List<Lotto> lottos) {
-        System.out.println(lottos.size()+"개를 구매했습니다.");
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+    }
+
+    public void printLottoNumbers(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println("[" + lotto.getIndex(0) + ", " + lotto.getIndex(1) + ", " + lotto.getIndex(2) + ", "
+                    + lotto.getIndex(3) + ", " + lotto.getIndex(4) + ", " + lotto.getIndex(5) + "]");
+        }
     }
 }
