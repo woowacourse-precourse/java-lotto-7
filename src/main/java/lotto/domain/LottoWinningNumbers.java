87157317f239006f2afc294c8cbf80826ea282bc
whdmscks11 <whdmscks11@gmail.com>
2024-10-31 18:21:12 +0900

    feat(LottoWinningNumbers): 로또 당첨 번호와 보너스 번호 분리
    - 기존에는 6자리 당첨 번호와 보너스 번호를 리스트에 함께 보관했으나,
      1등과 2등 당첨 기준 판단을 위해 보너스 번호를 별도로 관리하도록 변경
    - 추가로, 주석으로 검증 메서드가 필요한 부분 메모

--- src/main/java/lotto/domain/LottoWinningNumbers.java
+++ src/main/java/lotto/domain/LottoWinningNumbers.java
@@ -10,23 +10,28 @@ public class LottoWinningNumbers {
 
     private final List<Integer> winningNumbers;
     private final String mainNumbers;
-    private final String bonusNumber;
+    private final int bonusNumber;
 
-    public LottoWinningNumbers(String secondLine, String thridLine) {
+    public LottoWinningNumbers(String secondLine, String thirdLine) {
+        // 검증 메서드 필요 : DELIMITER 사용 했는지
         this.mainNumbers = secondLine;
-        this.bonusNumber = thridLine;
+        // 검증 메서드 필요 : secondLine != thirdLine
+        Integer bonus = StringUtil.parseToPositiveInt(thirdLine);
+        this.bonusNumber = bonus;
         this.winningNumbers = new ArrayList<>();
     }
 
     public void generate() {
         String[] splitNumbers = mainNumbers.split(DELIMITER);
+        // 검증 메서드 필요 : splitNumbers의 길이가 6인지
         for (String splitNumber : splitNumbers) {
             Integer number = StringUtil.parseToPositiveInt(splitNumber);
             winningNumbers.add(number);
         }
+    }
 
-        Integer bonus = StringUtil.parseToPositiveInt(bonusNumber);
-        winningNumbers.add(bonus);
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 
     public List<Integer> getWinningNumbers() {
