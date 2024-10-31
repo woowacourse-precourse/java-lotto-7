1a8a8b3c11dddd029a7ee1dfd5a88978557529fa
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 22:47:31 +0900

    feat: 당첨 번호 입력 기능 구현

---
+++ src/main/java/lotto/validation/WinningNumberValidation.java
@@ -0,0 +1,6 @@
+package lotto.validation;
+
+public class WinningNumberValidation {
+    public static void validate(String inputWinningNumbers) {
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validation.WinningNumberValidation;
 
 import static lotto.util.PurchaseUtils.getThousandUnitCount;
 
@@ -9,5 +10,11 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         String inputPurchaseAmount = Console.readLine();
         return getThousandUnitCount(inputPurchaseAmount);
+    }
+
+    public void readWinningNumbers() {
+        System.out.println("당첨번호를 입력해 주세요.");
+        String inputWinningNumbers = Console.readLine();
+        WinningNumberValidation.validate(inputWinningNumbers);
     }
 }
