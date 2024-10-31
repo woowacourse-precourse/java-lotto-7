470e55e5fae56a3ae8d61af5443497ec12e64bea
jenna <shinysuh1992@gmail.com>
2024-10-31 11:39:38 +0900

    feat(WinningNumbers): 당첨 번호 유효성 검사
    size: 번호가 6개인지 확인
    range: 1~45 사이의 숫자인지 확인
    uniqueness: 중복되는 숫자가 있는지 확인

--- src/main/java/lotto/core/WinningNumbers.java
+++ src/main/java/lotto/core/WinningNumbers.java
@@ -3,7 +3,9 @@ package lotto.core;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
     public static final int MIN_LOTTERY_NUMBER = 1;
@@ -31,14 +33,43 @@ public class WinningNumbers {
     }
 
     private void setWinningNumbers() {
-        this.numbers = Arrays.stream(this.numberString.split(DELIMITER))
+        List<Integer> numbers = Arrays.stream(this.numberString.split(DELIMITER))
             .map(String::trim)
             .map(Integer::parseInt)
             .toList();
+
+        this.validateNumbers(numbers);
+        this.numbers = numbers;
     }
 
     private void setBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         this.bonusNumber = Integer.parseInt(Console.readLine());
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        this.validateSize(numbers);
+        this.validateRange(numbers);
+        this.validateUniqueNumbers(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTERY_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        boolean isOutOfRange = numbers.stream().anyMatch(num -> num > MAX_LOTTERY_NUMBER || num < MIN_LOTTERY_NUMBER);
+        if (isOutOfRange) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
+        }
     }
 }
