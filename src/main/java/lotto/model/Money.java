a175cc2a2fbc8d170dce6f0115859c3a969643d0
(minjae8563) <(minjae8563@naver.com)>
2024-10-31 17:25:14 +0900

    refactor: 사용자의 구매금액을 담당하는 Money클래스 구현

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,2 +1,21 @@
-package lotto.model;public class Money {
+package lotto.model;
+
+import static lotto.ErrorMessage.INVALID_PRICE;
+
+public class Money {
+    private int userInputMoney;
+
+    public Money(int userInputMoney){
+        checkAmountWithinRange(userInputMoney);
+        this.userInputMoney = userInputMoney;
+    }
+    private void checkAmountWithinRange(int userInputMoney){
+        if (userInputMoney % 1000 != 0){
+            throw new IllegalArgumentException(INVALID_PRICE.format());
+        }
+    }
+
+
+
+
 }
