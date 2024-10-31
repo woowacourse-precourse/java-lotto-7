cd31410035869f39c7b08a8578652cf3f989e48a
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-11-01 00:51:54 +0900

    feat(LottoRank): 번호 일치 갯수와 보너스 번호를 통해 로또 등수 계산

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,47 @@
+package lotto;
+
+public enum LottoRank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int matchingCount;
+    private final int prize;
+
+    LottoRank(int matchingCount, int prize) {
+        this.matchingCount = matchingCount;
+        this.prize = prize;
+    }
+
+    public static LottoRank getRank(int matchingCount, boolean matchingBonus) {
+        if (matchingCount == 6) {
+            return FIRST;
+        }
+        if (matchingCount == 5 && matchingBonus) {
+            return SECOND;
+        }
+        if (matchingCount == 5) {
+            return THIRD;
+        }
+        if (matchingCount == 4) {
+            return FOURTH;
+        }
+        if (matchingCount == 3) {
+            return FIFTH;
+        }
+
+        return MISS;
+    }
+
+    public int getMatchingCount() {
+        return matchingCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
