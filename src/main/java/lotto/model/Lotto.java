e77d57de30838b982c74ffde76e86579bd629a70
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-10-31 20:08:14 +0900

    feat: 당첨 번호 예외처리 기능 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().allMatch(num -> num < 1 && num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45사이의 숫자여야 합니다.");
+        }
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if (numSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+    }
+
+    public boolean isDuplicateWithLotto(int bonusNumber) {
+        return numbers.stream()
+                .anyMatch(num -> num.equals(bonusNumber)); //int인데 Integer와 비교 가능?
+    }
+}
