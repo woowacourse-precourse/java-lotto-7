5b2cc5a8b22b1db4c6bbd5b34c10e7b9a86e4526
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-31 14:59:04 +0900

    feat(LottoGeneartor): 로또 수량 계산 기능 구현 - 구입 금액 / 1000

---
+++ src/main/java/lotto/Model/LottoGenerator.java
@@ -0,0 +1,10 @@
+package lotto.Model;
+
+import java.util.List;
+
+public class LottoGenerator {
+    public List<String> genearteLotto(int price){
+        int lottoCount = price / 1000;
+
+    }
+}
