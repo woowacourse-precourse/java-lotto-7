6b0ee11d1eacedb3f3dc79f5f6f01f94cad455f2
Spinichi <spinichi@naver.com>
2024-11-01 02:52:24 +0900

    feat: LotteryProcess 기능 변경
    LotteryProcess 생성자의 파라미터를 객체에서 리스트, 정수형 등 필요한 정보로 변경하여 가독성 및 효율성 향상

--- src/main/java/lotto/domain/LotteryProcess.java
+++ src/main/java/lotto/domain/LotteryProcess.java
@@ -3,27 +3,24 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import lotto.input.UserBonusNumberInput;
-import lotto.service.PrintTicketCount;
 
 public class LotteryProcess {
     private final List<Integer> numbers;
     private final int bonusBallNumber;
-    private final List<List<Integer>> userTicketNumbers;
+    private final List<List<Integer>> tickets;
 
     private final int RANKINGS_NUMBER = 5;
     private final List<Integer> rankingCount = new ArrayList<>(Collections.nCopies(RANKINGS_NUMBER, 0));
 
 
-    public LotteryProcess(Lotto lotto, UserBonusNumberInput userBonusNumberInput,
-                          PrintTicketCount printTicketCount) {
-        this.numbers = lotto.getNumbers();
-        this.bonusBallNumber = userBonusNumberInput.getBonusNumber();
-        this.userTicketNumbers = printTicketCount.getTicketNumbers();
+    public LotteryProcess(List<Integer> numbers, int bonusBallNumber, List<List<Integer>> tickets) {
+        this.numbers = numbers;
+        this.bonusBallNumber = bonusBallNumber;
+        this.tickets = tickets;
     }
 
-    public void countMatchNumbers() {
-        for (List<Integer> ticket : userTicketNumbers) {
+    public List<Integer> countMatchNumbers() {
+        for (List<Integer> ticket : tickets) {
 
             int matchNumberCount = searchMatchNumbers(ticket);
             boolean bonusBallMatch = ticket.contains(bonusBallNumber);
@@ -35,9 +32,7 @@ public class LotteryProcess {
                 rankingCount.set(rankIndex, rankingCount.get(rankIndex) + 1);
             }
         }
-    }
 
-    public List<Integer> getRankingCount() {
         return rankingCount;
     }
 
