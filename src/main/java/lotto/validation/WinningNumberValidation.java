a4092ecc0700339ada653fd8ea53edd8920e5ceb
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 22:57:00 +0900

    feat: 당첨 번호 1~45 숫자 아닐 시 예외처리 구현

--- src/main/java/lotto/validation/WinningNumberValidation.java
+++ src/main/java/lotto/validation/WinningNumberValidation.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import static lotto.constants.ErrorMessage.LOTTO_NUMBER_ONLY_CAN_LENGTH_6;
-import static lotto.constants.ErrorMessage.LOTTO_NUMBER_ONLY_CAN_MONEY;
-import static lotto.constants.LottoNumbers.LOTTO_NUMBER_COUNT;
+import static lotto.constants.ErrorMessage.*;
+import static lotto.constants.LottoNumbers.*;
 
 public class WinningNumberValidation {
     private final static String DELIMITER = ",";
@@ -15,6 +14,7 @@ public class WinningNumberValidation {
         List<String> winningNumbers = setTrimNumbers(inputWinningNumbers);
         winningNumbers = setUniqueNumbers(winningNumbers);
         List<Integer> parsedWinningNumbers = parseNumbers(winningNumbers);
+        validateRange10To45(parsedWinningNumbers);
     }
 
     public static List<String> setTrimNumbers(String inputWinningNumbers) {
@@ -22,6 +22,7 @@ public class WinningNumberValidation {
                 .map(String::trim)
                 .toList();
     }
+
     public static List<String> setUniqueNumbers(List<String> winningNumbers) {
         if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(LOTTO_NUMBER_ONLY_CAN_LENGTH_6.getErrorMessage());
@@ -43,5 +44,13 @@ public class WinningNumberValidation {
                 throw new IllegalArgumentException(LOTTO_NUMBER_ONLY_CAN_MONEY.getErrorMessage());
             }
         }).toList();
+    }
+
+    public static void validateRange10To45(List<Integer> winningNumbers) {
+        boolean isLottoNumber = winningNumbers.stream()
+                .allMatch(number -> number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER);
+        if (!isLottoNumber) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ONLY_CAN_RANGE_1_TO_45.getErrorMessage());
+        }
     }
 }
