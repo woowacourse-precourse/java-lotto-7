835802009a7f2d9bfe335e434a4993aeb0109f0a
JunwooPark <wnsdn1994@gmail.com>
2024-11-01 03:36:25 +0900

    test(lotto-service): 로또 계산 및 생성 서비스 테스트 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,13 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoResults;
+
 public class LottoService {
+    public List<Lotto> generateLottos(int purchaseAmount) {
+    }
+
+    public LottoResults calculateResult(List<Lotto> userLottos, Lotto winningLotto, int bonusNumber) {
+    }
 }
