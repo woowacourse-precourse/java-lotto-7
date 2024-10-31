2406e3025d90eb6be2f84ef8146cd1dac0093e3d
dongyun <ehddbs452100@naver.com>
2024-10-31 11:51:02 +0900

    feat(Buy, Message, OutputView): calculate lotto counts
    - 지불액을 바탕으로 1000을 나누어서 총 로또 갯수 계산

--- src/main/java/lotto/global/Message.java
+++ src/main/java/lotto/global/Message.java
@@ -2,7 +2,9 @@ package lotto.global;
 
 public enum Message {
 
-    BUY_LOTTO_MESSAGE("지불액을 입력해 주세요.");
+    BUY_LOTTO_MESSAGE("지불액을 입력해 주세요."),
+    BUY_LOTTO_COUNT_MESSAGE("개를 구매했습니다.");
+
 
     private String msg;
 
