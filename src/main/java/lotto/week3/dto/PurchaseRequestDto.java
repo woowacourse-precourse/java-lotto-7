3b203fea6a8a35487668b74578b2cbedb26c2995
leejunnyeop <daffodil8620@naver.com>
2024-10-31 21:24:23 +0900

    [rename] PurchaseRequestDto(dto) - lottoCount -> money 명확하게 바꿈

--- src/main/java/lotto/week3/dto/PurchaseRequestDto.java
+++ src/main/java/lotto/week3/dto/PurchaseRequestDto.java
@@ -2,21 +2,21 @@ package lotto.week3.dto;
 
 public class PurchaseRequestDto {
 
-    private final int lottoCount;
+    private final int money;
 
-    public  PurchaseRequestDto(int lottoCount) {
-        int count = count(lottoCount);
-        this.lottoCount = count;
+    public  PurchaseRequestDto(int money) {
+        int count = count(money);
+        this.money = count;
     }
 
     private static int count(int cost) {
         if (cost % 1000 != 0) {
-            throw new IllegalArgumentException("[error] 천원  단워로 입력 해주세요. ");
+            throw new IllegalArgumentException("[ERROR] 천원  단워로 입력 해주세요. ");
         }
         return cost / 1000;
     }
 
-    public int getLottoCount() {
-        return lottoCount;
+    public int getMoney() {
+        return money;
     }
 }
