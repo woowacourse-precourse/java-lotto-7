756c44c024802f486e9d8e6db783a441890d0b45
himinqq <alsrud0966@naver.com>
2024-10-31 13:57:16 +0900

    test(LotteryMachineTest): 구입금액만큼 로또를 생성하는 기능 테스트

--- src/test/java/lotto/domain/LotteryMachineTest.java
+++ src/test/java/lotto/domain/LotteryMachineTest.java
@@ -12,20 +12,32 @@ class LotteryMachineTest {
     private LotteryMachine lotteryMachine;
     List<Integer> fixedNumber = List.of(1, 2, 3, 4, 5, 6);
 
-    @BeforeEach
-    void createFixedNumberLotto(){
-        lotteryMachine = new LotteryMachine(new FixedNumberGenerator(fixedNumber));
-    }
-
 //    @BeforeEach
-//    void createRandomNumberLotto(){
-//        lotteryMachine = new LotteryMachine(new RandomNumberGenerator());
+//    void createFixedNumberLotto(){
+//        lotteryMachine = new LotteryMachine(new FixedNumberGenerator(fixedNumber));
 //    }
+
+    @BeforeEach
+    void createRandomNumberLotto(){
+        lotteryMachine = new LotteryMachine(new RandomNumberGenerator());
+    }
 
     @Test
     void createLottoTicket() {
         Lotto lottoTicket = lotteryMachine.createLottoTicket();
         assertThat(lottoTicket.getNumbers()).isEqualTo(fixedNumber);
     }
+
+    @Test
+    void 구입금액만큼_로또를_생성한다(){
+
+        Money money = new Money(3000);
+
+        List<Lotto> lottoByPayment = lotteryMachine.createLottoByPayment(money);
+
+        assertThat(lottoByPayment.size()).isEqualTo(3);
+
+    }
+
 
 }
