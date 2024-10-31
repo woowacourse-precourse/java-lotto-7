87c1042be75f70a6ad1e84babfdfdc582ea6b8cd
minjun <ijmtylhtf@naver.com>
2024-10-31 11:02:14 +0900

    feat : 로또 구매 금액 입력값 검증 함수 구현

--- src/main/java/lotto/provider/LottoProvider.java
+++ src/main/java/lotto/provider/LottoProvider.java
@@ -8,4 +8,10 @@ public class LottoProvider {
 		this.numberOfLottos = numberOfLottos;
 		this.lottoPurchaseAmount = lottoPurchaseAmount;
 	}
+
+	private void validateLottoPurchaseAmount(int lottoPurchaseAmount) {
+		if (lottoPurchaseAmount % 1000 != 0) {
+			throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력 받습니다.");
+		}
+	}
 }
