0c48e72987179df8ed71e389737c5cc96d39b4a0
je488 <pje488@naver.com>
2024-10-31 20:31:03 +0900

    feat: 당첨 내역과 관련된 상수들을 정의한 Enum 구현

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,4 +1,29 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum Rank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000);
+
+    private final int matchCount;
+    private final boolean isBonusNumberMatched;
+    private final int prizeMoney;
+
+    Rank(int matchCount, boolean isBonusNumberMatched, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.isBonusNumberMatched = isBonusNumberMatched;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Rank valueOf (int matchCount, boolean isBonusNumberMatched) {
+        return Arrays.stream(values())
+                .filter(rank -> matchCount == rank.matchCount)
+                .filter(rank -> isBonusNumberMatched == rank.isBonusNumberMatched)
+                .findFirst()
+                .orElse(null);
+    }
 }
