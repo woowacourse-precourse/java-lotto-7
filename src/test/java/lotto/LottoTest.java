812a72e1314534c65bee19ebcf024b30c080c28a
dongyun <ehddbs452100@naver.com>
2024-10-31 12:48:38 +0900

    test(RandomLottoTest): test for validating if numbers were between 1 and 46
    - 입력한 로또의 6자리가 모두 1과 46 사이의 숫자인지 검증하여 아닐 시 해당 오류 메시지를 출력하는지 검증

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.global.Error.LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +23,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호가_1과_46_사이가_아닐시_예외가_발생() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46.getErrorMsg());
+    }
 }
