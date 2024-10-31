a93e85861c85987c9cdebb96c8d671077700ee89
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-10-31 13:06:35 +0900

    feat: 지불한 금액에 맞는 구매할 로또 매수 구하기
    지불한 금액을 1000으로 나누어 구매할 로또 매수를 구함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        int count = Integer.parseInt(Console.readLine());
+        List<Lotto> lottos = new ArrayList<>();
+
+        count /= 1000;
+
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
+
+        }
+
+
     }
 }
