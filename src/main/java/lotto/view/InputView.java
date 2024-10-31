d3d82d064d44d9e2ea4f5f892881dc7a8e98d045
IM-GYURI <kgulr0517@gmail.com>
2024-10-31 22:40:03 +0900

    feat : InputView 추가
    requestMoneyInput()
    - 안내 문구 출력 후 구입 금액 입력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public String requestMoneyInput() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+}
