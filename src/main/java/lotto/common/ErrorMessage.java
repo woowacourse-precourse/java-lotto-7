8c815ae402a7b2043217bcac1cab0e7072002456
dnwldla <leeemjw@naver.com>
2024-10-31 09:11:01 +0900

    update: 예외 메세지를 요구사항에 맞게 변경

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -1,12 +1,12 @@
 package lotto.common;
 
 public class ErrorMessage {
-    public static final String NOT_INTEGER_INPUT ="입력은 양의 정수이어야 합니다";
-    public static final String BLANK_OR_NULL_INPUT = "입력은 공백일 수 없습니다";
-    public static final String NOT_UNIT_INPUT = "구입 금액은 1000 단위이어야 합니다";
-    public static final String INVALID_CHARACTER = "입력에 숫자와 쉼표 외에 다른 문자를 입력할 수 없습니다";
-    public static final String INVALID_COUNT = "6개의 숫자를 입력해야 합니다";
-    public static final String INVALID_RANGE = "1부터 45 사이의 숫자를 입력해야 합니다";
-    public static final String DUPLICATED_NUMBER = "숫자는 중복될 수 없습니다";
-    public static final String DUPLICATED_TO_WINNING_NUMBERS ="보너스 번호는 당첨 번호와 같을 수 없습니다" ;
+    public static final String NOT_INTEGER_INPUT ="[ERROR] 입력은 양의 정수이어야 합니다";
+    public static final String BLANK_OR_NULL_INPUT = "[ERROR] 입력은 공백일 수 없습니다";
+    public static final String NOT_UNIT_INPUT = "[ERROR] 구입 금액은 1000 단위이어야 합니다";
+    public static final String INVALID_CHARACTER = "[ERROR] 입력에 숫자와 쉼표 외에 다른 문자를 입력할 수 없습니다";
+    public static final String INVALID_COUNT = "[ERROR] 로또 번호는 여섯개여야 합니다.";
+    public static final String INVALID_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다";
+    public static final String DUPLICATED_NUMBER = "[ERROR] 로또 번호는 서로 다른 숫자여야 합니다.";
+    public static final String DUPLICATED_TO_WINNING_NUMBERS ="[ERROR] 보너스 번호는 당첨 번호와 달라야 합니다" ;
 }
