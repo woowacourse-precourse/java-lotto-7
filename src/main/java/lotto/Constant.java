73bd67a3f5e3d2cff478d209d5e51ce4886ee793
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-10-31 18:05:56 +0900

    feat: Add exception handling in Constant class

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -7,5 +7,7 @@ public final class Constant {
     public static final int LOTTO_END = 45;
     public static final int LOTTO_BONUS_APPLY = 5;
 
-
+    private Constant() {
+        throw new AssertionError("Constant 클래스는 인스턴스화 할 수 없습니다.");
+    }
 }
