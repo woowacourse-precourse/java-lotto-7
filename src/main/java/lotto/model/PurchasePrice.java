3ce9c85949dd88d1cc0ad4905b43a06d2e243464
Zrfv <youngoh07015@gmail.com>
2024-10-31 13:21:47 +0900

    refactor: Lotto 클래스 패키지로 이동, 예외 처리 기능 각 클래스로 이동
    - Lotto 클래스 model 패키지로 이동
    - validatePurchasePrice 메서드는 PurchasePrice 클래스 내부로 이동

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -1,13 +1,18 @@
 package lotto.model;
 
-import lotto.util.validator.PurchasePriceValidator;
-
 public class PurchasePrice {
     private final int price;
+    private static final String INVALID_PRICE_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.";
 
     public PurchasePrice(int price) {
-        PurchasePriceValidator.validatePurchasePrice(price);
+        validatePurchasePrice(price);
         this.price = price;
+    }
+
+    public static void validatePurchasePrice(int input) {
+        if (input % 1000 != 0) {
+            throw new IllegalArgumentException(INVALID_PRICE_ERROR_MESSAGE);
+        }
     }
 
     public int get() {
---
+++ src/test/java/lotto/model/PurchasePriceTest.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class PurchasePriceTest {
+    @Test
+    void 구입_금액이_1000으로_나누어_떨이지지_않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new PurchasePrice(1001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
