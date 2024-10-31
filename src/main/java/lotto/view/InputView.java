ec8a00b70b7c86ed0e59ba89e1bd4a81accec35b
eb9862 <luclead622@gmail.com>
2024-10-31 22:02:38 +0900

    refactor: InputView 내부의 상수를 다른 클래스로 분리
    - enum 클래스인 InputMessage 로 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,19 @@
 package lotto.view;
 
+import static lotto.view.InputMessage.INPUT_BUDGET;
+import static lotto.view.InputMessage.INPUT_WINNING_NUMBERS;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String INPUT_BUDGET_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String LINE_BREAK = "\n";
-
     public static String readBudget() {
-        System.out.println(INPUT_BUDGET_MESSAGE);
+        System.out.println(INPUT_BUDGET.getMessage());
         return Console.readLine();
     }
 
     public static String readWinningNumbers() {
-        System.out.println(LINE_BREAK + INPUT_WINNING_NUMBERS_MESSAGE);
+        System.out.println(INPUT_WINNING_NUMBERS.getMessage());
         return Console.readLine();
     }
 }
