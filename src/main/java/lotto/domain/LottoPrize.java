c6f535b5d215d9bd5ed8e480949cc9ac8fff6716
qyinm <qusseun@gmail.com>
2024-10-31 13:45:11 +0900

    feat: 로또 등수 enum 각 등수별 설명 toString override 작성

---
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+public enum LottoPrize {
+
+    FIRST_PRIZE(6, 2_000_000_000),
+    SECOND_PRIZE(5, 30_000_000),
+    THIRD_PRIZE(5, 1_500_000),
+    FOURTH_PRIZE(4, 50_000),
+    FIFTH_PRIZE(3, 5_000);
+
+    private static final String describePrize = "%d개 일치%s (%s원)";
+    private static final String additionalDescribeBonusPrize = ", 보너스 볼 일치";
+    private final int matchCount;
+    private final int prizeMoney;
+
+    LottoPrize(int matchCount, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+
+    @Override
+    public String toString() {
+        String formattedPrizeMoney = String.format("%,d", prizeMoney);
+        if (SECOND_PRIZE.equals(this)) {
+            return describePrize.formatted(matchCount, additionalDescribeBonusPrize, formattedPrizeMoney);
+        }
+        return describePrize.formatted(matchCount, "", formattedPrizeMoney);
+    }
+}
