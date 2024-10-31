0544417a9e71d8a4780ebe6ce94671961e64d828
ppzepi <hsunguk1999@gmail.com>
2024-10-31 21:47:48 +0900

    feat: HitLotto entity 생성

--- src/main/java/lotto/lottoModel/HitLotto.java
+++ src/main/java/lotto/lottoModel/HitLotto.java
@@ -1,4 +1,16 @@
 package lotto.lottoModel;
 
+import java.util.List;
+
 public class HitLotto {
+    private final List<Integer> numbers;
+
+    public HitLotto(List<Integer> hitLotto) {
+        this.numbers = hitLotto;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }
