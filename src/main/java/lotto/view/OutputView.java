67ea8fa8321396819328f82ac04a83da182d2577
taek2222 <oot309@daum.net>
2024-10-31 20:17:52 +0900

    feat(OutputView): 구입 랜덤 로또 번호 출력 기능 구현
    - 구입한 로또들의 숫자 정보를 출력하는 기능을 구현했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String PURCHASE_TICKET_MESSAGE = "%d개를 구매했습니다.";
+    private static final String PURCHASE_TICKET_INFO_MESSAGE = "[%s]";
+    private static final String LINE_BREAK = "\n";
 
-    public void printTicketNumber(int number) {
-        System.out.println();
-        System.out.println(String.format(PURCHASE_TICKET_MESSAGE, number));
+    public void printTicketCount(int count) {
+        System.out.printf(LINE_BREAK + PURCHASE_TICKET_MESSAGE + LINE_BREAK, count);
+    }
+
+    public void printTicketNumbers(List<String> tickets) {
+        tickets.forEach(ticket ->
+                System.out.printf(PURCHASE_TICKET_INFO_MESSAGE + LINE_BREAK, ticket)
+        );
     }
 }
