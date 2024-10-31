29817202f426708c98a47650a66330d4ba3e5750
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-01 02:16:04 +0900

    test: 랜덤 번호 테스트코드 작성
    중복되는 값이 있을 경우를 테스트하며, 랜덤번호의 크기 또한 검증하였음

---
+++ src/test/java/utils/RandomNumberTest.java
@@ -0,0 +1,43 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumberTest {
+
+    @DisplayName("랜덤번호를 생성할 때, 6자리의 랜덤번호가 생성된다")
+    @Test
+    void create_random_number_test() {
+        //given
+        List<Integer> testRandomNumbers = RandomNumber.create();
+        //when, then
+        assertThat(testRandomNumbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("랜덤번호를 생성할 때, 중복된 값이 존재해서는 안 된다")
+    @RepeatedTest(100)
+    void random_number_duplicate_test() {
+        //given
+        List<Integer> testRandomNumbers = RandomNumber.create();
+        Set<Integer> verifyTestRandomNumbers = new HashSet<>(testRandomNumbers);
+        //when, then
+        assertThat(verifyTestRandomNumbers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("랜덤번호의 범위는 항상 1보다 크거나 같고, 45보다 작거나 같아야한다")
+    @RepeatedTest(100)
+    void random_number_range_test() {
+        //given
+        List<Integer> testRandomNumbers = RandomNumber.create();
+        for (Integer testRandomNumber : testRandomNumbers) {
+            assertThat(testRandomNumber).isBetween(1, 45);
+        }
+    }
+
+}
