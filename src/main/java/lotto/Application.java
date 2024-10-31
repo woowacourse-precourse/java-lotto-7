b7f705b7c2044b16a0f8466d9c3f2903ece3cdd9
YuyoungRhee <bbclara@naver.com>
2024-10-31 20:48:48 +0900

    feat(Application): 로또 애플리케이션 실행 구현
    - 입력 및 출력 관련 클래스, 로또 발행 및 결과 계산 클래스 인스턴스 생성
    - LottoController를 통해 사용자 입력 및 로또 결과 처리를 실행하는 run 메서드 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.InputController;
+import lotto.controller.LottoController;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoIssuer;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.WinningResultCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputController inputController = new InputController(inputView);
+
+        LottoIssuer lottoIssuer = new LottoIssuer(new LottoGenerator(new LottoNumberGenerator()));
+        WinningResultCalculator winningResultCalculator = new WinningResultCalculator();
+
+        LottoController lottoController = new LottoController(lottoIssuer, winningResultCalculator, inputController, outputView);
+
+        lottoController.run();
     }
 }
