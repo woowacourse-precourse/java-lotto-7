6ac0aeff45327cdba827c61b0251513784d13e98
GS_song98 <20003204@sju.ac.kr>
2024-11-01 00:21:48 +0900

    feat: LottoMachineController 기능 구현

---
+++ src/main/java/lotto/controller/LottoMachineController.java
@@ -0,0 +1,42 @@
+package lotto.controller;
+
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.PurchaseAmount;
+import lotto.utils.RetryUtil;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoMachineController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoMachineController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        PurchaseAmount purchaseAmount = RetryUtil.retry(this::makePurchaseAmount);
+
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount.getPurchaseAmount());
+        outputView.printBuyingLottos(lottoMachine.getLottoNumbers());
+
+        Lotto winningLotto = RetryUtil.retry(this::makeWinningLotto);
+        Bonus bonus = RetryUtil.retry(() -> makeBonus(winningLotto));
+    }
+
+    private PurchaseAmount makePurchaseAmount() {
+        return new PurchaseAmount(inputView.readPurchaseAmount());
+    }
+
+    private Lotto makeWinningLotto() {
+        return new Lotto(inputView.readWinningLotto());
+    }
+
+    private Bonus makeBonus(Lotto winningLotto) {
+        return new Bonus(inputView.readBonus(), winningLotto);
+    }
+}
