fb10dfeb8bfd4eabe7faedb6c6167dd45435512d
JunwooPark <wnsdn1994@gmail.com>
2024-10-31 20:51:16 +0900

    feat(lotto): 로또 생성 기능 구현

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,5 +1,14 @@
 package lotto.model;
 
-public class LottoMachine {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
+public class LottoMachine {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBERS_COUNT = 6;
+
+    public List<Integer> genearteLottos() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBERS_COUNT);
+    }
 }
