a0fed0a998d2401513d82b81002823f5e88b712a
Shinjongyun <tigerjong123@gmail.com>
2024-10-31 19:02:07 +0900

    feat: BuyingPrice 객체 추가

---
+++ src/main/java/lotto/domain/BuyingPrice.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class BuyingPrice {
+    private int price;
+
+    public BuyingPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
