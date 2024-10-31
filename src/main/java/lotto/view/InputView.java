346ec7bdce1010a4624744c42e4e936a5cf5d193
ghktndyd <hwasuyong99@naver.com>
2024-10-31 13:48:44 +0900

    feat(InputView): 구입 금액을 입력 받고, 빈 문자열이 아님을 검증하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,18 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public int getPurchaseAmount() {
+        String input = Console.readLine();
+        validateBlankInput(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateBlankInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 빈 문자열은 입력할 수 없습니다.");
+        }
+    }
 }
