667dbd6c514a938c2bc80fbedbb4a91ee5f81fbc
minseo <alstj778@gmail.com>
2024-10-31 17:21:50 +0900

    feat(InputView): 로또 구매 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        LottoController lottoController = new LottoController(outputView);
+        LottoController lottoController = new LottoController(inputView, outputView);
         lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,24 @@
 package lotto.controller;
 
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
+    private final InputView inputView;
     private final OutputView outputView;
 
-    public LottoController(final OutputView outputView) {
+    public LottoController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void run() {
+        int purchasePrice = getPurchasePrice();
+    }
+
+    private int getPurchasePrice() {
         outputView.printPurchasePriceRequest();
+        String purchasePrice = inputView.read();
+        return Integer.parseInt(purchasePrice);
     }
 }
