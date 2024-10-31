0954ab5e2067758c26f45e2dd62af12685bfbd68
IM-GYURI <kgulr0517@gmail.com>
2024-11-01 03:02:36 +0900

    feat : LottoController 기능 추가
    - OutputView와 LottoService 의존성 주입
    - startLottoDraw() : 로또 발행 후 출력 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,32 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
 import lotto.domain.Money;
+import lotto.service.LottoService;
 import lotto.view.ErrorView;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     private final InputView inputView;
+    private final OutputView outputView;
     private final ErrorView errorView;
+    private final LottoService lottoService;
 
-    public LottoController(InputView inputView, ErrorView errorView) {
+    public LottoController(InputView inputView, OutputView outputView,
+                           ErrorView errorView, LottoService lottoService) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.errorView = errorView;
+        this.lottoService = lottoService;
     }
 
     public void startLottoDraw() {
         Money money = getMoney();
+        Lottos lottos = lottoService.generateLottos(money.getMoney());
+
+        outputView.printLottos(lottos);
     }
 
     private Money getMoney() {
