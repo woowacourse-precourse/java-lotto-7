9c861dc71c9fb12112912baa2e38dd28f60bef3a
Hacanna42 <dtmdgus901@gmail.com>
2024-11-01 01:34:18 +0900

    refactor(MoneyTest): @CsvSource를 활용하여 테스트 코드 개선

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
@@ -9,9 +10,8 @@ import static org.assertj.core.api.Assertions.*;
 public class MoneyTest {
     @DisplayName("돈 1,000원 당 로또 티켓 1개로 교환할 수 있다")
     @ParameterizedTest
-    @ValueSource(ints = 1000)
-    void 돈_티켓_교환_테스트(int amount) {
-        int expected = 1;
+    @CsvSource({"1000, 1", "2000, 2"})
+    void 돈_티켓_교환_테스트(int amount, int expected) {
         Money money = new Money(amount);
         assertThat(money.getTicket()).isEqualTo(expected);
     }
