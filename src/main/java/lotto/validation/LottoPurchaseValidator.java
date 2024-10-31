f66c44122c5b487869ac73e2d323b231afefc880
dltmddus <tmddus4671@gmail.com>
2024-10-31 22:53:25 +0900

    feat: 로또 구매 금액 입력 검증 구현

---
+++ src/main/java/lotto/validation/LottoPurchaseValidator.java
@@ -0,0 +1,39 @@
+package lotto.validation;
+
+import lotto.view.input.InputError;
+import lotto.view.input.InvalidInputException;
+
+public class LottoPurchaseValidator {
+    public static void validate(String input) {
+        validateNotNullOrEmpty(input);
+        validateIsInteger(input);
+        validatePositiveNumber(input);
+        validatePurchaseUnit(input);
+    }
+
+    private static void validateNotNullOrEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new InvalidInputException(InputError.CANNOT_BE_NULL_OR_EMPTY);
+        }
+    }
+
+    private static void validateIsInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(InputError.POSITIVE_NUMBER_REQUIRED);
+        }
+    }
+
+    private static void validatePositiveNumber(String input) {
+        if (Integer.parseInt(input) < 0) {
+            throw new InvalidInputException(InputError.POSITIVE_NUMBER_REQUIRED);
+        }
+    }
+
+    private static void validatePurchaseUnit(String input) {
+        if (Integer.parseInt(input) % 1000 == 0) {
+            throw new InvalidInputException(InputError.LOTTO_PURCHASE_MUST_BE_MULTIPLE_OF_THOUSAND);
+        }
+    }
+}
--- src/main/java/lotto/view/input/InputError.java
+++ src/main/java/lotto/view/input/InputError.java
@@ -3,7 +3,7 @@ package lotto.view.input;
 public enum InputError {
     ERROR("[ERROR] "),
     CANNOT_BE_NULL_OR_EMPTY(ERROR + "값은 비워둘 수 없습니다."),
-    MUST_BE_NUMERIC(ERROR + "숫자만 입력 가능합니다"),
+    POSITIVE_NUMBER_REQUIRED(ERROR + "양수만 입력 가능합니다"),
     LOTTO_PURCHASE_MUST_BE_POSITIVE_INTEGER(ERROR + "로또 구입 금액은 양수로 입력해주세요."),
     LOTTO_PURCHASE_MUST_BE_MULTIPLE_OF_THOUSAND(ERROR + "로또 구입 금액은 1,000원 단위여야 합니다."),
     LOTTO_NUMBER_SEPARATOR_MUST_BE_COMMA(ERROR + "로또 번호는 쉼표로 구분해야 합니다."),
