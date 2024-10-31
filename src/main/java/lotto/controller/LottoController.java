3baa7197efeb69551cffce74af32881ba1cd2009
minseo <alstj778@gmail.com>
2024-10-31 18:09:54 +0900

    feat(LottoController): 구매 금액 입력값 검증 로직 구현
    - 입력값이 숫자로만 이루어져있는지 확인한다.
    - 입력값이 양의 정수 범위인지 확인한다.
    - 입력값이 1,000의 배수인지 확인한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -19,6 +19,48 @@ public class LottoController {
     private int getPurchasePrice() {
         outputView.printPurchasePriceRequest();
         String purchasePrice = inputView.read();
+        validatePurchasePrice(purchasePrice);
         return Integer.parseInt(purchasePrice);
+    }
+
+    private void validatePurchasePrice(final String purchasePrice) {
+        validateNumberFormat(purchasePrice);
+        validatePositiveIntegerRange(purchasePrice);
+        validatePurchaseUnit(Integer.parseInt(purchasePrice));
+    }
+
+    private void validateNumberFormat(final String input) {
+        if (isNotNumber(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNotNumber(final String input) {
+        return !input.matches("\\d+");
+    }
+
+    private void validatePositiveIntegerRange(final String input) {
+        try {
+            final int value = Integer.parseInt(input);
+            if (isZero(value)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isZero(final int value) {
+        return value == 0;
+    }
+
+    private void validatePurchaseUnit(final int value) {
+        if (isNotMultipleOf1000(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNotMultipleOf1000(final int value) {
+        return value % 1000 != 0;
     }
 }
