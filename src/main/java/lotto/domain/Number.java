32b3a03598c585cbc427f9e72facf323402b8832
Yong U Cho <yongu2000@naver.com>
2024-10-31 15:22:26 +0900

    feat: 검증을 위한 클래스 생성
    Number 래퍼 클래스 -> 기존 Integer로 쓰던 것 대체. 로또에만 쓰이는 숫자(1~45)만 있도록 검증
    Numbers 일급 콜렉션 -> 기존 List<Integer>로 쓰던 것 List<Number>로 감싸고 Numbers로 대체
    로또에 쓰이는 숫자들(6개, 중복없음) 로직 검증가능

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,13 +1,52 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Number {
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+
     private final int number;
 
     public Number(int number) {
+        validateNumberRange(number);
         this.number = number;
+    }
+
+    public Number(String number) {
+        int parsedNumber = parseStringToInteger(number);
+        validateNumberRange(parsedNumber);
+        this.number = parsedNumber;
     }
 
     public int value() {
         return number;
     }
+
+    private int parseStringToInteger(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] String 에서 변환 중 오류 발생");
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number <= MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 범위 초과");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number numberObject)) return false;
+        return number == numberObject.value();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
 }
---
+++ src/main/java/lotto/domain/Numbers.java
@@ -0,0 +1,56 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Numbers {
+    private List<Number> numbers;
+
+    public Numbers(List<Integer> numbers) {
+        List<Number> parsedNumbers = parseIntToNumber(numbers);
+        validate(parsedNumbers);
+        this.numbers = parsedNumbers;
+    }
+
+    public Numbers(String numbers) {
+        List<Number> parsedNumbers = parseStringToNumber(numbers);
+        validate(parsedNumbers);
+        this.numbers = parsedNumbers;
+    }
+
+    private void validate(List<Number> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Number> numbers) {
+        if (numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    private void validateDuplicate(List<Number> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count())
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+    }
+
+    private List<Number> parseStringToNumber(String numbers) {
+        return Stream.of(numbers.split(","))
+            .map(String::trim)
+            .map(Number::new)
+            .toList();
+    }
+
+    private List<Number> parseIntToNumber(List<Integer> numbers) {
+        return numbers.stream()
+            .map(Number::new)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return numbers.stream()
+            .map(number -> String.valueOf(number.value()))
+            .toList()
+            .toString();
+    }
+}
