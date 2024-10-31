2f5cd8b0ab5b61b6a0db6f5cb0ebb21bba47b0b7
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-31 19:47:58 +0900

    refactor($InputView): 당첨 로또에서 검증함에 따른 검증 로직 삭제
    - 번호에 대한 검증은 당첨 로또에서 수행.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -21,13 +21,9 @@ public class InputView {
         System.out.println("\n당첨 번호를 입력해 주세요.");
 
         try {
-            List<Integer> winningNumbers = Arrays.stream(Console.readLine().split(","))
+            return Arrays.stream(Console.readLine().split(","))
                     .map(Integer::parseInt)
                     .toList();
-
-            validateWinningNumbers(winningNumbers);
-
-            return winningNumbers;
         } catch (NumberFormatException e) {
             System.out.println("올바른 숫자를 입력해 주세요.");
             return inputWinningNumbers();
@@ -41,24 +37,6 @@ public class InputView {
         } catch (NumberFormatException e) {
             System.out.println("올바른 숫자를 입력해 주세요.");
             return inputBonusNumber();
-        }
-    }
-
-    private void validateWinningNumbers(List<Integer> winningNumbers) {
-        if (winningNumbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이여야 합니다.");
-        }
-        if (winningNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-        if (winningNumbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
-        }
-    }
-
-    private void validateBonusNumber(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45사이여야 합니다.");
         }
     }
 }
