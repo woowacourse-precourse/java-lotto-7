245b828a14791e879d3c2b3f7009146a03ce6353
Spinichi <spinichi@naver.com>
2024-11-01 02:50:04 +0900

    feat: PrintTicketCount 기능 변경
    기존에 배열 생성, 배열 요소 추가, 출력 기능이 다 들어있던 makeLottoNumber 메서드를 세 개의 메서드로 분리시킴으로써 기능과 역할 분리, printCountAndTickets에 출력 관련 메서드를 모음으로써 가독성 향상

--- src/main/java/lotto/service/PrintTicketCount.java
+++ src/main/java/lotto/service/PrintTicketCount.java
@@ -7,32 +7,41 @@ import java.util.List;
 
 public class PrintTicketCount {
     private final int lottoTicketCount;
-    private final List<List<Integer>> userTicketNumbers;
+    private final List<List<Integer>> tickets;
 
     public PrintTicketCount(int lottoTicketCount) {
         this.lottoTicketCount = lottoTicketCount;
-        this.userTicketNumbers = new ArrayList<>();
+        this.tickets = new ArrayList<>();
     }
 
-    public List<List<Integer>> getTicketNumbers() {
-        return userTicketNumbers;
+    public void printCountAndTickets(List<List<Integer>> tickets) {
+        printCount();
+        repeatPrintTicket(tickets);
     }
 
-    public void printCount() {
+    public List<List<Integer>> getLottoTickets() {
+        for (int i = 0; i < lottoTicketCount; i++) {
+            getLottoTicket();
+        }
+
+        return tickets;
+    }
+
+    private void getLottoTicket() {
+        List<Integer> lottoTicket = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(lottoTicket);
+        tickets.add(lottoTicket);
+    }
+
+    private void printCount() {
         System.out.println(lottoTicketCount + "개를 구매했습니다.");
     }
 
-    public void repeatPrintNumber() {
-        for (int i = 0; i < lottoTicketCount; i++) {
-            makeLottoNumber();
+    private void repeatPrintTicket(List<List<Integer>> tickets) {
+        for (List<Integer> lottoTicket : tickets) {
+            System.out.println(lottoTicket);
         }
-        System.out.println();
-    }
 
-    private void makeLottoNumber() {
-        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        Collections.sort(lottoNumbers);
-        userTicketNumbers.add(lottoNumbers);
-        System.out.println(lottoNumbers);
+        System.out.println();
     }
 }
