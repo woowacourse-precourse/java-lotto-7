8bb39087be923e0d65d539348f68b7319fd1ce34
dongjae <harold306122@gmail.com>
2024-10-31 20:34:55 +0900

    feat: LottoNumber 클래스 구현 - 개별 로또 번호의 유효성을 검증하는 로직을 포함합니다.

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,2 +1,20 @@
-package lotto.model;public class Lottonumber {
+package lotto.model;
+
+public class LottoNumber {
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
