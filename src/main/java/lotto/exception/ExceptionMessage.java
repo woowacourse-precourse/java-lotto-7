b3dc8d88ae496c75cca7db8d35488801d903d49a
Dev-LHY <dev.leehy@gmail.com>
2024-10-31 22:22:02 +0900

    refactor(exceptionMessage): 상수명 변경
    - INVALID_WINNING_NUMBER to INVALID_WINNING_NUMBER_RANGE
    - INVALID_BONUS_NUMBER to INVALID_BONUS_NUMBER_RANGE

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -6,9 +6,9 @@ public enum ExceptionMessage {
     INVALID_MONEY_INPUT("최소 구입금액은 1,000원이며 1,000원 단위로만 입력 가능합니다."),
     INVALID_LOTTO_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
     INVALID_WINNING_NUMBER_SIZE("당첨 번호는 6개여야 합니다."),
-    INVALID_WINNING_NUMBER("당첨 번호는 1~45 사이의 숫자만 입력 가능합니다."),
+    INVALID_WINNING_NUMBER_RANGE("당첨 번호는 1~45 사이의 숫자만 입력 가능합니다."),
     DUPLICATE_WINNING_NUMBER("중복 당첨 번호는 입력이 불가능합니다."),
-    INVALID_BONUS_NUMBER("보너스 번호는 1~45 사이의 숫자만 입력 가능합니다."),
+    INVALID_BONUS_NUMBER_RANGE("보너스 번호는 1~45 사이의 숫자만 입력 가능합니다."),
     DUPLICATE_BONUS_NUMBER("중복 보너스 번호는 입력이 불가능합니다.");
 
     private final String message;
