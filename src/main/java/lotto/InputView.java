15b92801a8ebb066c1c7e1708e28ed09c3934aad
sleepyhoon <pokjm99@gmail.com>
2024-10-31 17:57:43 +0900

    feat (InputView) : 기능 추가 및 코드 리팩토링

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,9 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public long getUserInputMoney() {
-        System.out.println("구입금액을 선택해주세요.");
+        return parseLongInput();
+    }
+
+    public String getWinningNumbers() {
+        return getInput("당첨 번호를 입력해 주세요.");
+    }
+
+    public String getBonusNumber() {
+        return getInput("보너스 번호를 입력해 주세요.");
+    }
+
+    private String getInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
+    private long parseLongInput() {
         try {
-            return Long.parseLong(Console.readLine());
+            return Long.parseLong(getInput("구입금액을 선택해주세요."));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
