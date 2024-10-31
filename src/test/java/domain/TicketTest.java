825d27a6a73e4c9611dd073123b63d5503e6d411
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-01 05:03:01 +0900

    test: 구입금액이 천 원 단위가 아닐 경우의 테스트코드 추가

--- src/test/java/domain/TicketTest.java
+++ src/test/java/domain/TicketTest.java
@@ -1,9 +1,13 @@
 package domain;
 
+import static exception.ErrorMessage.PURCHASE_PRICE_DIVIDE_ERROR;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TicketTest {
 
@@ -19,10 +23,19 @@ public class TicketTest {
     @DisplayName("1000이 입력되었을 경우, 1개의 티켓이 생성된다.")
     @Test
     void buy_ticket_1000_test() {
-        int purchaseAMount = 1000;
+        int purchaseAmount = 1000;
 
-        Ticket ticket = new Ticket(purchaseAMount);
+        Ticket ticket = new Ticket(purchaseAmount);
         assertThat(ticket.getQuantity()).isEqualTo(1);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 500, 900, 1300, 9999})
+    @DisplayName("천 단위로 나누어지지 않을 경우, 예외가 발생한다")
+    void buy_ticket_not_divide_1000(int purchaseAmount) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Ticket ticket = new Ticket(purchaseAmount);
+        });
+        assertThat(exception.getMessage()).isEqualTo(PURCHASE_PRICE_DIVIDE_ERROR.getMessage());
     }
 
 }
