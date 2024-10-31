4b169b0e98789a516422bb461b821d14a5d0a4f2
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-01 00:41:52 +0900

    refactor(constants) : InputError 에 메시지 구체화

--- src/main/java/lotto/constants/InputError.java
+++ src/main/java/lotto/constants/InputError.java
@@ -4,12 +4,14 @@ public enum InputError {
     // 접두사
     ERROR_PREFIX("[ERROR] "),
     // 로또 구입 금액 예외
+    PURCHASE_PRICE_SHOULD_EXIST("로또 구입 금액을 입력하셔야 합니다."),
     PURCHASE_PRICE_NOT_A_NUMBER("로또 구입 금액에는 숫자만 입력하셔야 합니다."),
     PURCHASE_PRICE_NOT_REACH_AT_LEAST("최소 한 개 이상의 로또를 구입할 수 있는 가격을 입력하셔야 합니다."
             + "(최소 금액 : "
             + LottoConstInteger.LOTTO_PRICE.getValue()
             + "원)"),
-    PURCHASE_PRICE_CANT_BE_DIVIDED_CLEARLY(LottoConstInteger.LOTTO_PRICE.getValue()
+    PURCHASE_PRICE_SHOULD_BE_DIVIDED_CLEARLY("로또 구입 금액에는 "
+            + LottoConstInteger.LOTTO_PRICE.getValue()
             + "으로 나눠지는 수를 입력하셔야 합니다."),
     // 로또 당첨 번호 예외
     LOTTO_WINNING_NUMBER_CONTAIN_BAD_INPUT("로또 당첨 번호에는 숫자와 구분자만을 입력하셔야 합니다."),
