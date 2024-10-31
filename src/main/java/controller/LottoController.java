5d1bdb4cbc8fe01a9db924f713cd204efed1aae8
cux-maks <volcano020716@gmail.com>
2024-11-01 00:07:33 +0900

    feat(LottoController): 전체적인 로직 작성
    전체적인 로직을 작성하였습니다.

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,18 +1,69 @@
 package controller;
 
+import model.*;
 import view.InputView;
+import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
 
-    public LottoController() {}
+    private Integer cost;
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
+
+    public MyLotto myLotto;
+    public Lotto winningLotto;
+    public List<Prize> resultPrize;
+    public LottoResult lottoResult;
 
     public void run() {
 
-        Integer cost = InputView.inputCost();
-        List<Integer> winningNumbers = InputView.inputWinningNumbers();
-        Integer bonusNumber = InputView.inputBonusNumber();
+        this.getInputs();
+        this.runLotto();
+        this.outputResult();
 
+    }
+
+    private void getInputs() {
+        this.cost = InputView.inputCost();
+        this.winningNumbers = InputView.inputWinningNumbers();
+        this.bonusNumber = InputView.inputBonusNumber();
+    }
+
+    private void runLotto() {
+        initializeLotto();
+        calculatePrizes();
+        createResult();
+    }
+
+    private void initializeLotto() {
+        this.myLotto = new MyLotto(this.cost / 1000);
+        this.winningLotto = new Lotto(this.winningNumbers);
+        this.resultPrize = new ArrayList<>();
+    }
+
+    private void calculatePrizes() {
+        for (Lotto lotto : this.myLotto.getMyLotto()) {
+            Prize prize = determinePrize(lotto);
+            resultPrize.add(prize);
+        }
+    }
+
+    private Prize determinePrize(Lotto lotto) {
+        int matchCount = LottoMatcher.compareNumbers(lotto, this.winningLotto);
+        boolean bonusMatch = LottoMatcher.compareBonusNumber(lotto, this.bonusNumber);
+        return Prize.prizeOf(matchCount, bonusMatch);
+    }
+
+    private void createResult() {
+        this.lottoResult = new LottoResult(this.resultPrize);
+    }
+
+    private void outputResult() {
+        OutputView.outputMyLotto(this.myLotto.getMyLottoForPrint());
+        OutputView.outputPrizeResult(this.lottoResult.getWinningResult());
+        OutputView.outputWinningRate(this.lottoResult.calculateWinningRate());
     }
 }
