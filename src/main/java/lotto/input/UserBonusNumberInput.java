c24203fb50e1ce10699dbeb9447b02b0aa4410dd
Spinichi <spinichi@naver.com>
2024-11-01 02:47:40 +0900

    feat: UserBonusNumberInput 기능 변경
    생성자 부분에 있던 검증 기능을 메서드로 분리, 외부에 있던 lottoNumbers와의 중복 예외 처리 메서드를 해당 클래스로 이동

--- src/main/java/lotto/input/UserBonusNumberInput.java
+++ src/main/java/lotto/input/UserBonusNumberInput.java
@@ -1,39 +1,42 @@
 package lotto.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.Lotto;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
 
 public class UserBonusNumberInput {
-    private final int bonusNumber;
+    private int bonusNumber;
 
-    public UserBonusNumberInput() {
-        this.bonusNumber = validateAndParseBonusNumber();
-    }
-
-    public int getBonusNumber() {
-        return bonusNumber;
-    }
-
-    public void bonusDuplicationCheck(Lotto lotto) {
-        lotto.bonusNumberCheck(bonusNumber);
-    }
-
-    private int validateAndParseBonusNumber() {
+    public int validation() {
         while (true) {
             try {
-                System.out.println("보너스 번호를 입력해 주세요.");
-                String userInput = Console.readLine();
-                int parsedBonusNumber = parseBonusNumber(userInput);
-                return rangeValidation(parsedBonusNumber);
+                bonusNumber = rangeValidation(parseInput(getInput()));
+                return bonusNumber;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage()); // 오류 메시지 출력 후 재입력
             }
         }
     }
 
-    private int parseBonusNumber(String bonusNumberInput) {
+    public void bonusDuplicationCheck(List<Integer> LottoNumbers) {
+        Set<Integer> bonusCheck = new HashSet<>(LottoNumbers);
+
+        if (!bonusCheck.add(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복될 수 없습니다.");
+        }
+
+        System.out.println();
+    }
+
+    private String getInput() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    private int parseInput(String getInput) {
         try {
-            return Integer.parseInt(bonusNumberInput);
+            return Integer.parseInt(getInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수여야 합니다.");
         }
