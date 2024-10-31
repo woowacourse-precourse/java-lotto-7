6f72afdd50ea965ce2c427c8db9390e1ec2cc7ff
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-10-31 22:06:33 +0900

    feat: 발행할 로또개수만큼 로또 발행
    LottoGenerator와 LottoStore를 통한 로또 발행
    LottoStore: 살 로또의 개수를 구하로 로또를 구매
    LottoGenerator: 랜덤한 숫자를 로또에 넣어서 발행

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,8 @@ package lotto.controller;
 
 import static lotto.view.InputView.getConsoleInput;
 
+import java.util.List;
+import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoStore;
 import lotto.view.InputView;
 
@@ -11,10 +13,12 @@ public class LottoController {
     private LottoStore lottoStore;
     private int lottoCount;
     private String purchaseAmount;
+    private List<Lotto> lottos;
 
     public void run() {
         purchaseAmount = inputView.requestPurchaseAmount(getConsoleInput());
         lottoCount = lottoStore.getLottoCount(purchaseAmount);
+        lottos = lottoStore.buyLotto(lottoCount);
 
     }
 }
--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -3,10 +3,14 @@ package lotto.domain.lotto;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
