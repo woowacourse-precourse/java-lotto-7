5a7e47e2d36f44cf9c8bc02939c2d908a5f56b6b
IM-GYURI <kgulr0517@gmail.com>
2024-11-01 03:00:46 +0900

    feat : ErrorMessage 추가
    - LOTTO_COUNT_NOT_SIX

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,8 +2,9 @@ package lotto.exception;
 
 public enum ErrorMessage {
 
-    NOT_MULTIPLE_OF_THOUSAND("구매 금액은 1,000원 단위로 입력해야 합니다."),
-    NOT_GREATER_THAN_A_THOUSAND("구매 금액은 최소 1,000원 이상이어야 합니다.");
+    MONEY_NOT_MULTIPLE_OF_THOUSAND("구매 금액은 1,000원 단위로 입력해야 합니다."),
+    MONEY_NOT_GREATER_THAN_A_THOUSAND("구매 금액은 최소 1,000원 이상이어야 합니다."),
+    LOTTO_COUNT_NOT_SIX("로또 번호는 6개여야 합니다.");
 
     private final String message;
 
