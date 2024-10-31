78a448dc7af3f8fe7c2ddfebaa95083939345638
JS <zks145@naver.com>
2024-11-01 02:04:32 +0900

    feat : 사용자가 구매한 로또 랜덤 번호 산출 기능

---
+++ src/main/java/lotto/util/Util.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Util {
+
+    public static List<Integer> randomLottoNumbers(int start , int end, int size) {
+        return Randoms.pickUniqueNumbersInRange(start, end, size);
+    }
+
+}
