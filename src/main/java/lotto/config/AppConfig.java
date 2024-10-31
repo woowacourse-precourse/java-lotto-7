61b45f58740b9900d200c38fe3a8b7bb03f40f0b
IM-GYURI <kgulr0517@gmail.com>
2024-10-31 23:02:39 +0900

    feat : AppConfig 기능 추가
    getLottoController()
    - ErrorView 추가
    
    getErrorView()
    - ErrorView 반환

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,6 +1,7 @@
 package lotto.config;
 
 import lotto.controller.LottoController;
+import lotto.view.ErrorView;
 import lotto.view.InputView;
 
 public class AppConfig {
@@ -8,10 +9,14 @@ public class AppConfig {
     private static final AppConfig appConfig = new AppConfig();
 
     public LottoController getLottoController() {
-        return new LottoController(getInputView());
+        return new LottoController(getInputView(), getErrorView());
     }
 
     private InputView getInputView() {
         return new InputView();
+    }
+
+    private ErrorView getErrorView() {
+        return new ErrorView();
     }
 }
