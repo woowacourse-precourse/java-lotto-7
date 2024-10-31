487319efa51b92f9abc16ccffe023d43f1e7e436
qudcks8084 <qudcks8084@naver.com>
2024-10-31 17:48:28 +0900

    Feat : LottoConstants 내부에 공용 상수 구현
    - 여러 클래스에 걸쳐져 사용되는 주요 상수들 선언
    	- 로또의 기본 구입 단위 : 1,000
    	- 로또의 최소 숫자 : 1
    	- 로또의 최대 숫자 : 45
    	- 로또 내부의 숫자 개수 : 6

---
+++ src/main/java/lotto/util/constant/LottoConstants.java
@@ -0,0 +1,8 @@
+package lotto.util.constant;
+
+public class LottoConstants {
+    private static final int LOTTO_BASE_UNIT = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUM_COUNT = 6;
+}
