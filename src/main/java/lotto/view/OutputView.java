9859d08278cbba19b40545beee1713ae6a512125
이창헌 <changbill@naver.com>
2024-10-31 19:03:06 +0900

    feat(view): add inputView and outputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
+
+    public void outputLotteryMachine(List<String> lotteryMachineResult,int number) {
+        System.out.printf("%d개를 구매했습니다.", number);
+        lotteryMachineResult.forEach(System.out::println);
+    }
+
+    public void outputWinningResult(String winningResult) {
+        System.out.println("당첨 통계\n" + "---");
+        System.out.println(winningResult);
+    }
+
+    public void newLine() {
+        System.out.println();
+    }
 }
