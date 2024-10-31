0a743d343d1ad7d18195cf2f408f8bfa79ac2ef9
choign3123 <choign3123@naver.com>
2024-10-31 17:04:07 +0900

    feat(LottoRank): 당첨 조건 명시

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -2,22 +2,28 @@ package lotto;
 
 public enum LottoRank {
 
-    GRADE_1TH(2000000000),
-    GRADE_2TH(30000000),
-    GRADE_3TH(1500000),
-    GRADE_4TH(50000),
-    GRADE_5TH(5000),
-    NONE(0),
+    GRADE_1TH(2000000000, "6개 일치"),
+    GRADE_2TH(30000000, "5개 일치, 보너스 볼 일치"),
+    GRADE_3TH(1500000, "5개 일치"),
+    GRADE_4TH(50000, "4개 일치"),
+    GRADE_5TH(5000, "3개 일치"),
+    NONE(0, "2개 이하 일치"),
     ;
 
     public static final String UNIT = "원";
     private final int prizeMoney;
+    private final String winCondition;
 
-    LottoRank(int prizeMoney) {
+    LottoRank(int prizeMoney, String winCondition) {
         this.prizeMoney = prizeMoney;
+        this.winCondition = winCondition;
     }
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getWinCondition() {
+        return winCondition;
     }
 }
