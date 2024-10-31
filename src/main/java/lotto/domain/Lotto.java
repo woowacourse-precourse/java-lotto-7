ad6129520546fea876d56b25c68852d8bd83a5f8
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-10-31 15:07:34 +0900

    feat(lotto) : 로또번호 검증 테스트

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,19 +6,35 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateNumber(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    private void validateNumber(List<Integer> numbers) {
+        numbers.forEach(number -> {
+                    if (number < 1 || number > 45) {
+                        throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+                    }
+                });
     }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복이 존재하면 안됩니다.");
+        }
+    }
+
+//    public List<Integer> getNumbers() {
+//        return numbers;
+//    }
 
     @Override
     public String toString() {
