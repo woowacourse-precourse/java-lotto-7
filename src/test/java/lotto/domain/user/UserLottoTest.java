005b5a20724aec971038f34ee6f088770dd523ec
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-31 16:57:00 +0900

    test : UserLotto bonus가 6자리와 중복될때 bonus부터 입력받기 테스트

--- src/test/java/lotto/domain/user/UserLottoTest.java
+++ src/test/java/lotto/domain/user/UserLottoTest.java
@@ -2,24 +2,19 @@ package lotto.domain.user;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.lottos.Lotto;
-import lotto.domain.lottos.user.BonusLotto;
 import lotto.domain.lottos.user.UserLotto;
 import org.junit.jupiter.api.Test;
 
 class UserLottoTest {
 
     @Test
-    void 보너스_메인_로또_번호_중복_예외_처리(){
-        Lotto mainLotto = new Lotto(new ArrayList<>(List.of(
-                1,2,3,4,5,6
-        )));
+    void 보너스_메인_로또_번호_중복_예외_처리() {
+        UserLotto.Builder builder = new UserLotto.Builder();
+        builder.mainLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
 
-        BonusLotto bonusLotto = new BonusLotto(1);
-
-        assertThatThrownBy(() -> new UserLotto(mainLotto, bonusLotto))
+        assertThatThrownBy(() -> builder.bonusLotto(6))
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
