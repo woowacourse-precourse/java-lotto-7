b9db3450d4cb87d988e5a05ddd3e74df304e99bd
whdmscks11 <whdmscks11@gmail.com>
2024-10-31 13:27:54 +0900

    feat(LottoWinningNumbers): 로또 당첨 번호 생성 로직 구현
    사용자의 두번째, 세번째 입력 값을 합쳐 로또 당첨 번호를 생성

--- src/main/java/lotto/domain/LottoWinningNumbers.java
+++ src/main/java/lotto/domain/LottoWinningNumbers.java
@@ -1,4 +1,34 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.util.StringUtil;
+
 public class LottoWinningNumbers {
+
+    private static final String DELIMITER = ",";
+
+    private String winningNumber;
+    private String bonusNumber;
+
+    public LottoWinningNumbers(String winningNumber, String bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> generate() {
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        String[] splitNumbers = winningNumber.split(DELIMITER);
+        for (String splitNumber : splitNumbers) {
+            Integer number = StringUtil.parseToPositiveInt(splitNumber);
+            winningNumbers.add(number);
+        }
+
+        Integer bonus = StringUtil.parseToPositiveInt(bonusNumber);
+        winningNumbers.add(bonus);
+
+        return winningNumbers;
+    }
 }
