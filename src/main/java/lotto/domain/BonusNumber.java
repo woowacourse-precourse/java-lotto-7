5e002c6972089d196c1996b4c05667481bedeba4
변우영 <likevangogh24@gmail.com>
2024-10-31 18:31:48 +0900

    feat(BonusNumber): 입력값으로부터 보너스 번호 객체를 생성하는 팩토리 메서드 추가

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import lotto.util.Convertor;
+
+public class BonusNumber {
+
+    private final int bonusNumber;
+
+    private BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumber from(String input) {
+        return new BonusNumber(Convertor.convertToInt(input));
+    }
+}
