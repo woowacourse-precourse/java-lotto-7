41b88d3f4ea87a8a1a85f123ad6a1c9700ed6984
dnjswns98 <tngkrt2004@gmail.com>
2024-10-31 16:42:08 +0900

    feat(OutputView): Output of the number of lottoes purchased

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 public class OutputView {
     private static final String QUESTION_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String PRINT_LOTTO_AMOUNT = "개를 구매했습니다.";
 
     public void printInputMoney() {
         System.out.println(QUESTION_LOTTO_AMOUNT);
+    }
+
+    public void printLottoAmount(int count) {
+        System.out.println("\n" + count + PRINT_LOTTO_AMOUNT);
     }
 }
