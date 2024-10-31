4f12b8fba65cbf99ed62bd700f02010f202f394d
JeongSH1 <jyt001532@gmail.com>
2024-10-31 18:32:32 +0900

    feat(UserInput): 당첨 번호를 입력 받는다
    UserInput에 추가된 구현 사항:
    - 로또 당첨 번호 입력 시 6개의 길이를 가진 int배열로 반환
    - 각 당첨 번호가 숫자로 변환 불가능 시 IllegalArgumentException 반환
    - 각 당첨 번호로 구분된 개수가 6개의 길이가 아닐 시 IllegalArgumentException 반환
    - 각 당첨 번호가 1~45의 범위를 가지지 않는다면 IllegalArgumentException 반환
    
    UserInput에 추가된 테스트 사항:
    - 유효한 로또 당첨 번호 입력시 int 배열 반환 테스트
    - 로또 당첨 번호 입력에서 로또 번호 개수가 6개와 다르면 예외 발생 테스트
    - 로또 당첨 번호 입력에서 각 번호가 숫자로 변환 불가능 시 예외 발생 테스트
    - 로또 당첨 번호 입력의 각 번호가 1부터 45범위 밖일 시 예외 발생 테스트

--- src/test/java/lotto/io/UserInputTest.java
+++ src/test/java/lotto/io/UserInputTest.java
@@ -30,7 +30,9 @@ class UserInputTest {
         assertThatThrownBy(() ->
                 userInput.getLottoPurchaseAmount(lottoPurchaseAmountInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 로또 구입 금액은 숫자입니다.");
+                .hasMessageContaining(
+                        "[ERROR] 로또 구입 금액은 숫자입니다."
+                );
     }
 
     @Test
@@ -42,7 +44,65 @@ class UserInputTest {
         assertThatThrownBy(() ->
                 userInput.getLottoPurchaseAmount(lottoPurchaseAmountInput))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 로또 구입 금액은 1,000원 단위입니다.");
+                .hasMessageContaining(
+                        "[ERROR] 로또 구입 금액은 " + userInput.LOTTO_AMOUNT_UNIT + "원 단위입니다."
+                );
+    }
+
+    @Test
+    void 유효한_로또_당첨_번호_입력_테스트() {
+        //given
+        String lottoWinningNumbersInput = "1,2,3,4,5,6";
+
+        //when
+        int[] lottoWinningNumbers = userInput.getLottoWinningNumbers(lottoWinningNumbersInput);
+
+        //then
+        assertThat(lottoWinningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+
+    }
+
+    @Test
+    void 로또_당첨_번호_입력의_로또_번호_개수가_요구사항과_다를_시_예외_테스트() {
+
+        //given
+        String lottoWinningNumbersInput = "1,2,3,4,5,6,7";
+
+        //then
+        assertThatThrownBy(() ->
+                userInput.getLottoWinningNumbers(lottoWinningNumbersInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR] 로또 당첨 번호의 개수는 " + userInput.NUMBER_OF_LOTTO_WINNING_NUMBERS + "개 입니다."
+                );
+    }
+
+    @Test
+    void 로또_당첨_번호_입력의_각_번호가_숫자로_변환_불가능_시_예외_테스트() {
+        //given
+        String lottoWinningNumbersInput = "one,2,3,four,5,6";
+
+        //then
+        assertThatThrownBy(() ->
+                userInput.getLottoWinningNumbers(lottoWinningNumbersInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR] 로또 당첨 번호는 숫자여야 합니다."
+                );
+    }
+
+    @Test
+    void 로또_당첨_번호_입력의_각_번호가_1부터_45_범위_밖일_시_예외_테스트() {
+        //given
+        String lottoWinningNumbersInput = "-1,0,2,46,47,99";
+
+        //then
+        assertThatThrownBy(() ->
+                userInput.getLottoWinningNumbers(lottoWinningNumbersInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "[ERROR] 로또 당첨 번호는 " + userInput.LOTTO_NUMBER_MINIMUM_VALUE + "~" + userInput.LOTTO_NUMBER_MAXIMUM_VALUE + "까지 입니다."
+                );
     }
 
 }
