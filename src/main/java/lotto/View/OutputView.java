ad854243e2abe06dac96ad8b511b7497dc799977
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-31 16:10:05 +0900

    feat(OutputView): 발행된 로또 번호 출력 기능 구현

---
+++ src/main/java/lotto/View/OutputView.java
@@ -0,0 +1,18 @@
+package lotto.View;
+
+import lotto.Model.Lotto;
+
+import java.util.Collections;
+import java.util.List;
+
+public class OutputView {
+    public static void showLottoCount(int lottoCount){
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public static void showAllLottos(List<Lotto> allLottos){
+        for (Lotto lotto : allLottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+}
