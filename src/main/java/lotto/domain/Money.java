09ef9639be2c94b92b389a134fee3d7485161fa4
parksomii <somi020605@gmail.com>
2024-11-01 02:21:51 +0900

    feat(Money): 구입 금액 유효성 검사 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,9 +1,25 @@
 package lotto.domain;
 
+import lotto.util.Error;
+
 public class Money {
     private final int amount;
 
     public Money(int amount) {
+        validateNonZero(amount);
+        validateDivisibility(amount);
         this.amount = amount;
+    }
+
+    private void validateNonZero(int amount) {
+        if (amount == 0) {
+            throw new IllegalArgumentException(Error.MONEY_ZERO_ERROR.message());
+        }
+    }
+
+    private void validateDivisibility(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(Error.MONEY_DIVIDE_ERROR.message());
+        }
     }
 }
