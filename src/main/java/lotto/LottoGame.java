3f2ee483256926b598d10328d5d51a4b633e3855
choign3123 <choign3123@naver.com>
2024-10-31 21:48:06 +0900

    comment(LottoGame): 주석 작성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -16,15 +16,19 @@ public class LottoGame {
     }
 
     public void run() {
+        // 로또 구매
         int money = lottoGameDisplay.inputMoney();
         List<Lotto> lottos = lottoStore.purchase(money);
         lottoGameDisplay.printPurchaseBreakdown(lottos);
 
+        // 당첨 번호 설정
         List<Integer> winnerNumbers = lottoGameDisplay.inputWinnerNumbers();
         int bonusNumber = lottoGameDisplay.inputBonusNumber(winnerNumbers);
 
+        // 당첨 확인
         List<LottoRank> lottoRanks = checkLottosRank(lottos, winnerNumbers, bonusNumber);
 
+        // 당첨 통계 계산
         double rateOfResult = LottoStatistics.calcRateOfReturn(money, lottoRanks);
         Map<LottoRank, Integer> rankMap = LottoStatistics.calcRankStatistics(lottoRanks);
         lottoGameDisplay.printWinStatistics(rankMap, rateOfResult);
