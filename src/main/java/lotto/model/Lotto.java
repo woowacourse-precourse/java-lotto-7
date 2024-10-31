e0a9003b2e4c338052e5ab09a2408439c3451cba
parksaegyeol <dmsgkrkt1@naver.com>
2024-10-31 17:49:12 +0900

    feat : enum타입 당첨 통계 출력 데이터 저장소 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -33,5 +33,4 @@ public class Lotto {
         return this.numbers;
     }
 
-    // TODO: 추가 기능 구현
 }
--- src/main/java/lotto/view/WinnerPrice.java
+++ src/main/java/lotto/view/WinnerPrice.java
@@ -1,4 +1,36 @@
 package lotto.view;
 
 public enum WinnerPrice {
+
+
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
+    FIFTH(3, 5000, "3개 일치 (5,000원) -"),
+    BOOM(0, 0, "");
+
+
+    private final int countMatch;
+    private final int prize;
+    private final String outputMessage;
+
+    WinnerPrice(int countMatch, int prize, String outputMessage) {
+        this.countMatch = countMatch;
+        this.prize = prize;
+        this.outputMessage = outputMessage;
+    }
+
+    public int getCountMatch() {
+        return this.countMatch;
+    }
+
+    public int getPrize() {
+        return this.prize;
+    }
+
+    public String getOutputMessage() {
+        return this.outputMessage;
+    }
+
 }
