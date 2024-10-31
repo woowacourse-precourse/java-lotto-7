c57951ef2f5ec3703d2fce2b5c6ef1516da53fab
rladmstn <oz33177351@gmail.com>
2024-10-31 19:01:46 +0900

    test(validator) : 보너스 숫자 검증 메서드에 대해 테스트 작성
    아래 4가지 케이스에 대해 테스트 작성
    1. 숫자만 입력된 형태가 아닐 경우, 검증 실패
    2. 1~45 사이 숫자가 아닐 경우, 검증 실패
    3. 당첨 숫자 목록과 중복되는 숫자일 경우, 검증 실패
    4. 보너스 번호 검증 성공

--- src/test/java/lotto/util/LottoValidatorTest.java
+++ src/test/java/lotto/util/LottoValidatorTest.java
@@ -1,45 +1,83 @@
 package lotto.util;
 
+import java.util.List;
 import lotto.enums.ErrorMessage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoValidatorTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"","0+1/3",",10,30,25"})
-    void 올바른_형태의_숫자_목록_입력_검증_실패(String input) {
-        Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_NUMBERS_INPUT.format());
+    @Nested
+    class WinningNumbers{
+        @ParameterizedTest
+        @ValueSource(strings = {"","0+1/3",",10,30,25"})
+        void 올바른_형태의_숫자_목록_입력_검증_실패(String input) {
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBERS_INPUT.format());
+        }
+
+        @Test
+        void 정확히_6개의_숫자를_입력_검증_실패(){
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers("10,20,30"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBERS_COUNT.format());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,30,2,40,5,80","31,23,42,12,0,9"})
+        void 유효_범위_내_숫자_입력_검증_실패(String input){
+            Assertions.assertThatThrownBy(()-> LottoValidator.validateNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.NOT_IN_BOUNDARY.format());
+        }
+
+        @Test
+        void 중복_숫자_입력_검증_실패(){
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers("10,1,2,3,4,10"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.DUPLICATED_NUMBERS.format());
+        }
+
+        @Test
+        void 당첨_숫자_목록_입력_검증_성공(){
+            LottoValidator.validateNumbers("3,9,15,43,2,20");
+        }
     }
 
-    @Test
-    void 정확히_6개의_숫자를_입력_검증_실패(){
-        Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers("10,20,30"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INVALID_NUMBERS_COUNT.format());
-    }
+    @Nested
+    class BonusNumber{
+        List<Integer> winningNumbers = List.of(1,3,10,23,30,9);
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1,30,2,40,5,80","31,23,42,12,0,9"})
-    void 유효_범위_내_숫자_입력_검증_실패(String input){
-        Assertions.assertThatThrownBy(()-> LottoValidator.validateNumbers(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.NOT_IN_BOUNDARY.format());
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {"","-1","+3",})
+        void 숫자_형태_입력_검증_실패(String input){
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateBonusNumber(input,winningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBER_FORM.format());
+        }
 
-    @Test
-    void 중복_숫자_입력_검증_실패(){
-        Assertions.assertThatThrownBy(() -> LottoValidator.validateNumbers("10,1,2,3,4,10"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.DUPLICATED_NUMBERS.format());
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {"0","80"})
+        void 유효_범위_내_숫자_입력_검증_실패(String input){
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateBonusNumber(input, winningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.NOT_IN_BOUNDARY.format());
+        }
 
-    @Test
-    void 당첨_숫자_목록_입력_검증_성공(){
-        LottoValidator.validateNumbers("3,9,15,43,2,20");
+        @Test
+        void 당첨_번호_목록과_중복되지_않은_숫자_검증_실패(){
+            Assertions.assertThatThrownBy(() -> LottoValidator.validateBonusNumber("23",winningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.DUPLICATED_WITH_WINNING_NUMBERS.format());
+        }
+
+        @Test
+        void 보너스_번호_검증_성공(){
+            LottoValidator.validateBonusNumber("17",winningNumbers);
+        }
     }
 
 }
