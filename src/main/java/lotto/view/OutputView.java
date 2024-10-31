ab51f77e5a909032ef83ffaff4441e0d137337ff
mininiii <didals0521@gmail.com>
2024-10-31 14:20:49 +0900

    fix: 당첨 통계 출력 방식 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     public void printPurchasedLotto(List<Lotto> purchasedLotto) {
         System.out.println(purchasedLotto.size() + "개를 구매했습니다.");
         purchasedLotto.forEach(lotto -> {
-            System.out.println(lotto.toString());
+            System.out.println(lotto.getNumbers().toString());
         });
     }
 
@@ -18,13 +18,23 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---");
         for (LottoRank rank : LottoRank.values()) {
-            if (!rank.equals(LottoRank.NONE) && statistics.containsKey(rank)) {
+            if (!rank.equals(LottoRank.NONE)) {
                 System.out.println(
-                        rank.getMatchingCount() + "개 일치 " + "(" + PrizeFormatter.formatPrize(rank.getPrize()) + "원)"
-                                + " - " + statistics.get(rank)
-                                + "개");
+                        makePrintMessage(rank, PrizeFormatter.formatPrize(rank.getPrize()),
+                                statistics.get(rank)));
             }
         }
+    }
+
+    private String makePrintMessage(LottoRank rank, String prize, int count) {
+        if (rank.equals(LottoRank.SECOND)) {
+            return String.format(rank.getMatchingCount() + "개 일치, 보너스 볼 일치 " + "(" + prize + "원)"
+                    + " - " + count
+                    + "개");
+        }
+        return String.format(rank.getMatchingCount()  + "개 일치 " + "(" + prize + "원)"
+                + " - " + count
+                + "개");
     }
 
     public void printRateOfReturn(double rateOfReturn) {
