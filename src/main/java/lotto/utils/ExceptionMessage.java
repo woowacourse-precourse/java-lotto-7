f0bf1312b10a218c94a43a8e64ab196651cb504a
kim <hwiyul_g@daum.net>
2024-10-31 16:38:53 +0900

    feat(ExceptionMessage): 예외 메시지 추가

--- src/main/java/lotto/utils/ExceptionMessage.java
+++ src/main/java/lotto/utils/ExceptionMessage.java
@@ -1,7 +1,9 @@
 package lotto.utils;
 
 public enum ExceptionMessage {
-    INVALID_INPUT_BUDGET("예산은 1000원 단위로 입력 가능합니다.");
+    INVALID_INPUT_BUDGET("예산은 1000원 단위로 입력 가능합니다."),
+    INVALID_DUPLICATED_NUMBER("중복된 숫자는 추가할 수 없습니다."),
+    INVALID_LOTTO_NUMBER_RANGE("입력 가능한 로또 숫자 범위는 1이상 45이하의 정수입니다.");
 
     private static final String BASE_MESSAGE = "[ERROR] %s";
     private final String message;
