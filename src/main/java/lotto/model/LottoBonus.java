4406628c08f6b5955e2b8f150267ee2bb4c8ed14
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-31 18:02:10 +0900

    feat(model/LottoBonus): 보너스 번호 유효성 검사 추가
    - 보너스 번호가 당첨 번호와 중복되는지 검증하는 validateDuplicate 메서드 추가

--- src/main/java/lotto/model/LottoBonus.java
+++ src/main/java/lotto/model/LottoBonus.java
@@ -1,16 +1,25 @@
 package lotto.model;
 
+import java.util.List;
+
 public class LottoBonus {
 	private final int bonusNumber;
 
-	public LottoBonus(int bonusNumber) {
-		validate(bonusNumber);
+	public LottoBonus(int bonusNumber, List<Integer> numbers) {
+		validateRange(bonusNumber);
+		validateDuplicate(bonusNumber, numbers);
 		this.bonusNumber = bonusNumber;
 	}
 
-	private void validate(int bonusNumber) {
+	private void validateRange(int bonusNumber) {
 		if (bonusNumber < 1 || bonusNumber > 45) {
 			throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+		}
+	}
+
+	private void validateDuplicate(int bonusNumber, List<Integer> numbers) {
+		if (numbers.contains(bonusNumber)) {
+			throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 		}
 	}
 }
