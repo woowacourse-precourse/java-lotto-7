35720b875d66e10b35cd8a08622f2d430d309f72
ChoiDongJoon <adong0808@naver.com>
2024-10-31 14:49:02 +0900

    feat(lotto): 오름차순 정렬과 검증 추가
    오름차순 정렬과 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,7 +3,11 @@ package lotto;
 import static lotto.constant.LottoConstant.LOTTO_LENGTH;
 import static lotto.constant.LottoConstant.LOTTO_PRICE;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.validator.LottoValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -14,8 +18,12 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateLength(numbers);
+        validateDuplicate(numbers);
+
+        ArrayList<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        this.numbers = sortedNumbers;
     }
 
     public void add(int lottoNumber) {
