631dc15b7a4e559a0ed2245f644ae58103d7b931
corpi <lj845854@gmail.com>
2024-10-31 20:55:47 +0900

    feat(InputValidator): 구입금액이 정수인지 검증할 수 있다.

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -1,7 +1,19 @@
 package lotto.model;
 
 public class InputValidator {
-    public void validateDivideByUnit(int input, int price) {
+    public void validateInputMoney(String input) {
+        validateParseNumber(input);
+    }
+
+    private void validateParseNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요");
+        }
+    }
+
+    private void validateDivideByUnit(int input, int price) {
         if (input % price != 0) {
             throw new IllegalArgumentException("[ERROR] " + price + "원 단위의 값을 입력해주세요");
         }
