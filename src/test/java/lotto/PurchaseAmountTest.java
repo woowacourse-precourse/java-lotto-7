4d44805abe9ca26cc00e479d04de26c168b26a47
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-11-01 02:29:54 +0900

    test(PurchaseAmountTest): PurchaseAmount 검증 테스트 추가

--- src/test/java/lotto/PurchaseAmountTest.java
+++ src/test/java/lotto/PurchaseAmountTest.java
@@ -3,11 +3,38 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PurchaseAmountTest {
     @Test
     void 구입_금액이_1000원_미만이면_예외가_발생한다() {
         assertThatThrownBy(() -> new PurchaseAmount(999))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new PurchaseAmount(1100))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_0원이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new PurchaseAmount(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력_받은_구입_금액이_숫자가_아닐_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new PurchaseAmount("1000원"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 입력_받은_구입_금액이_빈_값일_경우_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> new PurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
