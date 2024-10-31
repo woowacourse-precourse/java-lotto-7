6be317e1397f13576e00c0e05910dae83528db44
songsunkook <songsunkook@gmail.com>
2024-10-31 18:17:32 +0900

    test: 중복되지 않는 6개 숫자 뽑기 테스트 작성

--- src/test/java/lotto/util/RandomUtilTest.java
+++ src/test/java/lotto/util/RandomUtilTest.java
@@ -2,7 +2,9 @@ package lotto.util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,21 @@ class RandomUtilTest {
 
     @Test
     void 뽑은_로또_번호_숫자의_범위는_1부터_45_사이이다() {
-        List<Integer> number = RandomUtil.getLottoNumbers(1);
-        assertThat(number.getFirst()).isBetween(1, 45);
+        List<Integer> lottoNumbers = RandomUtil.getLottoNumbers(1);
+        assertThat(lottoNumbers.getFirst()).isBetween(1, 45);
+    }
+
+    @Test
+    void 뽑은_로또_번호_숫자는_중복되지_않는다() {
+        List<Integer> lottoNumbers = RandomUtil.getLottoNumbers();
+        Set<Integer> filteredNumbers = new HashSet<>();
+        filteredNumbers.addAll(lottoNumbers);
+        assertThat(lottoNumbers.size()).isEqualTo(filteredNumbers.size());
+    }
+
+    @Test
+    void 뽑은_번호는_총_6개이다() {
+        List<Integer> lottoNumbers = RandomUtil.getLottoNumbers();
+        assertThat(lottoNumbers.size()).isEqualTo(6);
     }
 }
