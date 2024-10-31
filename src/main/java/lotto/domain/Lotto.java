f2ee235e16bf1ee73ed628b809fec57d93b7bd96
world-dv <goodforx0147@naver.com>
2024-10-31 11:42:13 +0900

    refactor: 로또 숫자 개수 검증 메서드 재정의
    - 메서드명으로 의도를 나타내기 위해 로또 숫자 개수 검증 메서드의 이름을 변경합니다.
    - 상수를 사용해 상수 사용 의도를 드러냅니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,18 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final Integer LOTTO_NUMBER_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberCount(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
