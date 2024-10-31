043c374ea00dcc0401986162b4c452bf2ff243f7
황재웅 <wodnd0131@naver.com>
2024-10-31 14:07:03 +0900

    feat: 구입 금액 입력
    - Application.java을 통해 의존성, 생명주기를 관리
    - InputView를 AutoCloseable 인터페이스 구현으로 Console 리소스 관리
    - OutputMessage enum으로 사용자 안내 메시지 상수화
    - LottoController에서 View 객체들을 통한 IO 처리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView implements AutoCloseable {
+    public String readLine() {
+        return Console.readLine();
+    }
+
+    @Override
+    public void close() {
+        Console.close();
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+public class OutputView {
+    public void lineFeed() {
+        System.out.println();
+    }
+
+    public void print(String string) {
+        System.out.println(string);
+    }
+
+    public void print(OutputMessage message) {
+        System.out.println(message.getMessage());
+    }
+
+    public void print(OutputMessage message, Object... args) {
+        System.out.printf(message.getMessage(), args);
+    }
+}
