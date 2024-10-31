440791975cb2e52fe24a7cf862301427bc6e20ea
dudxo <father8475@naver.com>
2024-10-31 23:05:24 +0900

    feat : 보너스번호 입력 View
    - 사용자로부터 보너스번호를 입력 받는다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요.";
 
     public String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MSG);
@@ -14,6 +15,11 @@ public class InputView {
 
     public String inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBERS_MSG);
+        return Console.readLine();
+    }
+
+    public String InputBonusNumberMsg() {
+        System.out.println(INPUT_BONUS_NUMBER_MSG);
         return Console.readLine();
     }
 }
