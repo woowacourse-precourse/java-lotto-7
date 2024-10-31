554e4ec4948c06fdc7ec8a12df8b60bad9368865
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-01 01:26:26 +0900

    test(LottosTest): 로또 개수에 맞게 로또를 발행하는지 검증하는 테스트 작성
    해당 테스트는 실패한다.

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+
+    @Test
+    @DisplayName("로또 개수에 맞게 로또를 발행하는지 확인한다.")
+    void validateLottoTotalCount() {
+        int purchaseAmount = LottoPurchaseAmount.from("8000").getPurchaseAmount();
+        int lottoCount = LottoCount.from(purchaseAmount).getLottoCount();
+        int lottoListSize = Lottos.from(lottoCount).getLottoListSize();
+
+        assertThat(lottoListSize).isEqualTo(lottoCount);
+    }
+
+}
