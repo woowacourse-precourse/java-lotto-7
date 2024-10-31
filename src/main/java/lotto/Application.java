73d1a4ff44cc38db6e49b501811b6c3ac795fdac
sleepyhoon <pokjm99@gmail.com>
2024-10-31 16:54:39 +0900

    feat (Application) : controller 을 선언한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = LottoGameAppConfig.getLottoController();
+        lottoController.start();
     }
 }
