163877aebc52ebf12d83010764ec64a22dcd6b7c
EHCTH <lees90896@gmail.com>
2024-10-31 19:57:59 +0900

    style(ValidatorTest): ErrorMessage 변경에의한 수정

--- src/test/java/lotto/buyer/validator/ValidatorTest.java
+++ src/test/java/lotto/buyer/validator/ValidatorTest.java
@@ -1,4 +1,30 @@
 package lotto.buyer.validator;
+import lotto.buyer.infrastructure.Won;
+import lotto.constant.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 public class ValidatorTest {
+    @Test
+    @DisplayName("1000원으로 안나눠지는 경우 예외 발생")
+    void divisibleByThousand() {
+        List<String> moneyInfo = List.of("12313","1323","1001","10", "1");
+        moneyInfo.forEach((money) -> verifyExceptionForValidation(money, ErrorMessage.DIVISIBLE_BY_THOUSAND));
+    }
+    @Test
+    @DisplayName("10만원 초과일 경우 예외 발생")
+    void validateAmountUnderLimit() {
+        List<String> amountLimit = List.of("12313123","213213","100001", "123132123");
+        amountLimit.forEach((amount) -> verifyExceptionForValidation(amount, ErrorMessage.AMOUNT_LIMIT));
+    }
+    private void verifyExceptionForValidation(String input, ErrorMessage errorMessage) {
+        assertThatThrownBy(
+                () -> Won.of(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage.getMessage());
+    }
+
 }
