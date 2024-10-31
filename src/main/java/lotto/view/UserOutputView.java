f124806918bb2973e55077a1df849cf55dfbe0ee
icebear0111 <wltjq0901@naver.com>
2024-10-31 21:13:18 +0900

    feat: Add UserOutputView class for displaying results
    사용자에게 출력을 제공하는 클래스 추가
    로또 구매 개수와 구매한 로또 번호, 당첨 통계, 수익률을 출력한다

---
+++ src/main/java/lotto/view/UserOutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.model.Lotto;
+
+import java.util.List;
+import java.util.Map;
+
+public class UserOutputView {
+
+    private static final String STATISTICS_MESSAGE = "당첨 통계";
+    private static final String STATISTICS_DIVIDER = "---";
+
+    public static void outputLottos(List<Lotto> lottos) {
+        System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void outputWinningStatistics(Map<String, Integer> statistics) {
+        System.out.println(STATISTICS_MESSAGE);
+        System.out.println(STATISTICS_DIVIDER);
+        System.out.println("3개 일치 (5,000원) - " + statistics.getOrDefault("5등", 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + statistics.getOrDefault("4등", 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + statistics.getOrDefault("3등", 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + statistics.getOrDefault("2등", 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + statistics.getOrDefault("1등", 0) + "개");
+    }
+
+    public static void outputProfitRate(double profitRate) {
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+    }
+}
