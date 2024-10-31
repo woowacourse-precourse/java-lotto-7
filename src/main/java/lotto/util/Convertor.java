824fd994f6a15386413ab650b97ea18eda7f4baf
변우영 <likevangogh24@gmail.com>
2024-10-31 12:15:13 +0900

    feat(Convertor): 입력값을 List<Integer>로 변환하는 메서드 추가

--- src/main/java/lotto/util/Convertor.java
+++ src/main/java/lotto/util/Convertor.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Convertor {
 
     private static final String NON_NUMERIC_EXCEPTION = "입력값은 숫자여야 합니다";
@@ -18,5 +21,11 @@ public class Convertor {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NON_NUMERIC_EXCEPTION);
         }
+    }
+
+    public static List<Integer> convertToIntegerList(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Convertor::convertToInt)
+                .toList();
     }
 }
