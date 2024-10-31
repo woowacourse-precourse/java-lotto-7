9518f6912a935cda8a9709850e7aaf4144cf8c9d
seullll <tmfud@naver.com>
2024-10-31 18:50:19 +0900

    refector: 함수 분리

--- src/main/java/lotto/LottoNum.java
+++ src/main/java/lotto/LottoNum.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoNum {
+    private List<Lotto> lottos = new ArrayList<>();
+
     public LottoNum(int money){
+        checkMoney(money);
+    }
+
+    private void checkMoney(int money){
         if(money % 1000 != 0){
             throw new IllegalArgumentException("1000원으로 나누어 떨어지지 않습니다.");
         }
