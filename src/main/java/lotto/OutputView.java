433c6dcf070deddbe313ad74262375983258b92f
je488 <pje488@naver.com>
2024-10-31 23:23:26 +0900

    feat: 당첨 내역을 출력하는 함수 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Map;
+
 public class OutputView {
     public static final String LOTTO_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
     public static final String LOTTO_WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
@@ -9,7 +11,19 @@ public class OutputView {
         System.out.println(LOTTO_AMOUNT_PROMPT);
     }
 
-    public void printLottoWinningNumberPrompt() { System.out.println(LOTTO_WINNING_NUMBER_PROMPT); }
+    public void printLottoWinningNumberPrompt() {
+        System.out.println(LOTTO_WINNING_NUMBER_PROMPT);
+    }
 
-    public void printLottoBonusNumberPrompt() { System.out.println(LOTTO_BONUS_NUMBER_PROMPT); }
+    public void printLottoBonusNumberPrompt() {
+        System.out.println(LOTTO_BONUS_NUMBER_PROMPT);
+    }
+
+    public void printLottoResult(Map<Rank, Integer> lottoPrizeCount) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : lottoPrizeCount.keySet()) {
+            System.out.println(rank.getWinningMessage() + " - " + lottoPrizeCount.get(rank) + "개");
+        }
+    }
 }
