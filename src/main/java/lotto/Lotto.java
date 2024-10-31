6e294963ae83e5e50deb15696dcfe4468e2d471b
seullll <tmfud@naver.com>
2024-11-01 00:05:18 +0900

    feat: 중복 확인 함수 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -18,6 +19,10 @@ public class Lotto {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
             }
+        }
+        long count = numbers.stream().distinct().count();
+        if(count != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
