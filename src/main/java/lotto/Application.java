86817831c542b165b5536919ddc693bb6fdc042b
KimYunYeong <intmain0924@naver.com>
2024-10-31 22:31:30 +0900

    feat: Add inputPurchaseAmount method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String purchaseAmount = inputPurchaseAmount();
+    }
+
+    public static String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        return purchaseAmount;
     }
 }
