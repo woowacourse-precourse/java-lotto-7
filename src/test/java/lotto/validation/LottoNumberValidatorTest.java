363959dbe5a65cfca97aa322e2812e47724ba98a
이윤빈 <kanado20000@naver.com>
2024-11-01 02:51:23 +0900

    :art: refactor: 테스트 클래스의 포매팅 수정

--- src/test/java/lotto/validation/LottoNumberValidatorTest.java
+++ src/test/java/lotto/validation/LottoNumberValidatorTest.java
@@ -1,48 +1,46 @@
 package lotto.validation;
 
-import org.junit.jupiter.api.Test;
-
-import lotto.enums.ErrorMessage;
-
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.enums.ErrorMessage;
+import org.junit.jupiter.api.Test;
 
 class LottoNumberValidatorTest {
 
-    @Test
-    void 올바른_로또_번호가_입력되면_예외가_발생하지_않는다() {
-        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+  @Test
+  void 올바른_로또_번호가_입력되면_예외가_발생하지_않는다() {
+    List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        assertThatCode(() -> LottoNumberValidator.mainValidator(validNumbers))
-            .doesNotThrowAnyException();
-    }
+    assertThatCode(() -> LottoNumberValidator.mainValidator(validNumbers))
+        .doesNotThrowAnyException();
+  }
 
-    @Test
-    void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
-        List<Integer> invalidQuantity = Arrays.asList(1, 2, 3, 4, 5);
+  @Test
+  void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
+    List<Integer> invalidQuantity = Arrays.asList(1, 2, 3, 4, 5);
 
-        assertThatThrownBy(() -> LottoNumberValidator.mainValidator(invalidQuantity))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(ErrorMessage.INVALID_LOTTO_QUANTITY.getMessage());
-    }
+    assertThatThrownBy(() -> LottoNumberValidator.mainValidator(invalidQuantity))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(ErrorMessage.INVALID_LOTTO_QUANTITY.getMessage());
+  }
 
-    @Test
-    void 로또_번호에_범위를_벗어나는_숫자가_포함되면_예외가_발생한다() {
-        List<Integer> outOfRangeNumbers = Arrays.asList(1, 2, 3, 4, 5, 60);
+  @Test
+  void 로또_번호에_범위를_벗어나는_숫자가_포함되면_예외가_발생한다() {
+    List<Integer> outOfRangeNumbers = Arrays.asList(1, 2, 3, 4, 5, 60);
 
-        assertThatThrownBy(() -> LottoNumberValidator.mainValidator(outOfRangeNumbers))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
-    }
+    assertThatThrownBy(() -> LottoNumberValidator.mainValidator(outOfRangeNumbers))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+  }
 
-    @Test
-    void 로또_번호에_중복된_숫자가_포함되면_예외가_발생한다() {
-        List<Integer> duplicateNumbers = Arrays.asList(1, 2, 3, 4, 5, 5);
-        
-        assertThatThrownBy(() -> LottoNumberValidator.mainValidator(duplicateNumbers))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
-    }
+  @Test
+  void 로또_번호에_중복된_숫자가_포함되면_예외가_발생한다() {
+    List<Integer> duplicateNumbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+
+    assertThatThrownBy(() -> LottoNumberValidator.mainValidator(duplicateNumbers))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
+  }
 }
