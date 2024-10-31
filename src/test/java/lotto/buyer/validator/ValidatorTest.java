1e27ef7a8b27b6629d63f291846d9996c8deaea0
EHCTH <lees90896@gmail.com>
2024-10-31 20:12:07 +0900

    style(ValidatorTest): 메서드 이름 변경

--- src/test/java/lotto/buyer/validator/ValidatorTest.java
+++ src/test/java/lotto/buyer/validator/ValidatorTest.java
@@ -12,15 +12,15 @@ public class ValidatorTest {
     @DisplayName("1000원으로 안나눠지는 경우 예외 발생")
     void divisibleByThousand() {
         List<String> moneyInfo = List.of("12313","1323","1001","10", "1");
-        moneyInfo.forEach((money) -> verifyExceptionForValidation(money, ErrorMessage.DIVISIBLE_BY_THOUSAND));
+        moneyInfo.forEach((money) -> verifyException(money, ErrorMessage.DIVISIBLE_BY_THOUSAND));
     }
     @Test
     @DisplayName("10만원 초과일 경우 예외 발생")
     void validateAmountUnderLimit() {
         List<String> amountLimit = List.of("12313123","213213","100001", "123132123");
-        amountLimit.forEach((amount) -> verifyExceptionForValidation(amount, ErrorMessage.AMOUNT_LIMIT));
+        amountLimit.forEach((amount) -> verifyException(amount, ErrorMessage.AMOUNT_LIMIT));
     }
-    private void verifyExceptionForValidation(String input, ErrorMessage errorMessage) {
+    private void verifyException(String input, ErrorMessage errorMessage) {
         assertThatThrownBy(
                 () -> Won.of(input))
                 .isInstanceOf(IllegalArgumentException.class)
