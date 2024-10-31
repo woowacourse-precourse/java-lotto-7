6aeac1d63e17c01bcc6c1643a4d77f1a7bb91b77
cherryiJuice <chldlwn711@gmail.com>
2024-10-31 16:51:40 +0900

    refactor: 접근자 변경

--- src/main/java/lotto/exception/GeneralException.java
+++ src/main/java/lotto/exception/GeneralException.java
@@ -4,7 +4,7 @@ public class GeneralException extends RuntimeException {
 
     private final ErrorType errorType;
 
-    public GeneralException(ErrorType errorType) {
+    protected GeneralException(ErrorType errorType) {
         super(errorType.getMessage());
         this.errorType = errorType;
     }
