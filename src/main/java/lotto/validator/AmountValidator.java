b33bbc572a3f97680e5eead4dfdbaee129fc4598
wowseok <wowseok2774@gmail.com>
2024-10-31 16:21:23 +0900

    refactor(AmountValidator): 에러 메시지를 상수로 관리하도록 수정

--- src/main/java/lotto/validator/AmountValidator.java
+++ src/main/java/lotto/validator/AmountValidator.java
@@ -2,6 +2,8 @@ package lotto.validator;
 
 public class AmountValidator {
     // private 생성자: 클래스 인스턴스화 방지
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     private AmountValidator() {
         throw new UnsupportedOperationException("Utility class");
     }
@@ -12,19 +14,19 @@ public class AmountValidator {
             checkAmount(amount);
             return amount;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 유효한 숫자를 입력해야 합니다.");
         }
     }
 
     public static void checkAmount(int amount) {
         if (amount < 0) {
-            throw new IllegalArgumentException("[ERROR] 금액은 음수가 될 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 금액은 음수가 될 수 없습니다.");
         }
         if (amount == 0) {
-            throw new IllegalArgumentException("[ERROR] 입력 금액이 0원입니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력 금액이 0원입니다.");
         }
         if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 금액은 1,000원 단위여야 합니다.");
         }
     }
 
