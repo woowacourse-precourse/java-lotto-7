bcb717ef6beda6eb3ff0917b933a2624ee5e4ee1
Jewan1120 <hawaii1468@gmail.com>
2024-11-01 00:15:59 +0900

    feat(Validator): 로또 구입 금액 유효성 검증 로직 추가

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -3,6 +3,17 @@ package lotto.validator;
 public class LottoValidator {
 
     public static void validateInputMoney(String inputMoney) {
-
+        if (inputMoney == null || inputMoney.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < inputMoney.length(); i++) {
+            if (!Character.isDigit(inputMoney.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+        int money = Integer.parseInt(inputMoney);
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
