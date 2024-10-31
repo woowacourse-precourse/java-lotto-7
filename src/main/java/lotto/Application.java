f2b71e156b9d15423e2303eca2a50f95976f729c
je488 <pje488@naver.com>
2024-10-31 20:46:52 +0900

    feat: 사용자가 구매한 로또 번호와 당첨 번호를 비교하여 통계를 내주는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,28 +2,44 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
-    public static List<List<Integer>> generateLottoNumbers(int lottoAmount) {
-        List<List<Integer>> userLottoNumbers = new ArrayList<>();
+    static List<List<Integer>> userLottoNumbers = new ArrayList<>();
+    static Map<Rank, Integer> lottoPrizeCount = new HashMap<>() {{
+       put(Rank.FIRST, 0);
+       put(Rank.SECOND, 0);
+       put(Rank.THIRD, 0);
+       put(Rank.FOURTH, 0);
+       put(Rank.FIFTH, 0);
+    }};
+
+    public static void generateLottoNumbers(int lottoAmount) {
         for (int i = 0; i < lottoAmount; i++) {
             List<Integer> userLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             userLottoNumber.sort(Comparator.naturalOrder());
             userLottoNumbers.add(userLottoNumber);
         }
-        return userLottoNumbers;
     }
 
-    public static void checkLottoMatch(List<List<Integer>> userLottoNumbers, List<Integer> lottoWinningNumber, int lottoBonusNumber) {
+    public static void checkLottoMatch(List<Integer> lottoWinningNumber, int lottoBonusNumber) {
         boolean isBonusNumberMatched = false;
-        for (List<Integer> userLottoNumber: userLottoNumbers) {
+        for (List<Integer> userLottoNumber : userLottoNumbers) {
             int winningNumberCount = compareLottoNumbers(userLottoNumber, lottoWinningNumber);
             if (winningNumberCount == 5) {
                 isBonusNumberMatched = compareBonusNumber(userLottoNumber, lottoBonusNumber);
             }
+            countLottoRank(winningNumberCount, isBonusNumberMatched);
+        }
+    }
+
+    public static void countLottoRank(int winningNumberCount, boolean isBonusNumberMatched) {
+        Rank value = Rank.valueOf(winningNumberCount, isBonusNumberMatched);
+        if (value != null) {
+            lottoPrizeCount.put(value, lottoPrizeCount.get(value) + 1);
         }
     }
 
@@ -31,7 +47,7 @@ public class Application {
         int winningNumberCount = 0;
         for (int number : lottoWinningNumber) {
             if (userLottoNumber.contains(number)) {
-                winningNumberCount +=  1;
+                winningNumberCount += 1;
             }
         }
         return winningNumberCount;
