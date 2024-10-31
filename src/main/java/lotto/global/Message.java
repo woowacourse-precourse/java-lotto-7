9f7c8f4a158b6b4a381f2ab7548418f4251dc339
dongyun <ehddbs452100@naver.com>
2024-10-31 12:43:46 +0900

    feat(Error, InputView, Lotto, Message): input winning lotto numbers
    - 당첨 로또 번호를 입력하여 ","로 분리 후 분리 한 값들이 정수인지 검증
    - 입력한 당첨 로또 번호를 Lotto 객체를 생성함과 동시에 검증
    - 입력한 당첨 로또 번호의 갯수가 6개가 아니라면 LOTTO_NUMBER_COUNT_IS_NOT_SIX 메시지를 전달하여 당첨
    로또 번호 재입력 받기
    - 입력한 당첨 로또 번호가 서로 중복이라면 LOTTO_NUMBER_IS_DUPLICATED 메시지를 전달하여 당첨 로또 번호
    재입력 받기
    - 입력한 당첨 로또 번호가 1과 46 사이가 아니라면 LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46 메시지를
    전달하여 당첨 로또 번호 재입력 받기

--- src/main/java/lotto/global/Message.java
+++ src/main/java/lotto/global/Message.java
@@ -3,7 +3,8 @@ package lotto.global;
 public enum Message {
 
     BUY_LOTTO_MESSAGE("지불액을 입력해 주세요."),
-    BUY_LOTTO_COUNT_MESSAGE("개를 구매했습니다.");
+    BUY_LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
+    WINNING_NUMBER_MESSAGE("\n당첨 번호를 입력해 주세요.");
 
 
     private String msg;
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,17 @@ package lotto.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.global.Error.NOT_INTEGER;
 import static lotto.global.Message.BUY_LOTTO_MESSAGE;
+import static lotto.global.Message.WINNING_NUMBER_MESSAGE;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Buy;
+import lotto.domain.Lotto;
 
 public class InputView {
+
+    private static final String COMMA = ",";
 
     public Buy buyLotto() {
         System.out.println(BUY_LOTTO_MESSAGE.getMsg());
@@ -20,6 +27,28 @@ public class InputView {
             System.out.println(e.getMessage());
             return buyLotto();
         }
+    }
+
+    public Lotto inputWinningLottoNumbers() {
+        System.out.println(WINNING_NUMBER_MESSAGE.getMsg());
+        try {
+            List<Integer> lottoNumbers = convertToIntList(splitNumbersByComma(readLine()));
+            return new Lotto(lottoNumbers);
+        } catch (NumberFormatException e) {
+            System.out.println(NOT_INTEGER.getErrorMsg());
+            return inputWinningLottoNumbers();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningLottoNumbers();
+        }
+    }
+
+    private List<String> splitNumbersByComma(String numbers) {
+        return Arrays.stream(numbers.split(COMMA)).collect(Collectors.toList());
+    }
+
+    private List<Integer> convertToIntList(List<String> numbers) {
+        return numbers.stream().map(number -> convertToInt(number)).collect(Collectors.toList());
     }
 
     private int convertToInt(String money) throws NumberFormatException {
