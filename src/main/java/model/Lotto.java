b61d3a2ebde425e5eaf3a1429c382a46a179091c
cux-maks <volcano020716@gmail.com>
2024-10-31 15:46:47 +0900

    feat(Lotto): 로또 번호 랜덤 생성 기능 추가
    로또 번호를 랜덤으로 생성하는 기능을 추가하였습니다.

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,20 +1,18 @@
-package lotto;
+package model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    public Lotto() {
+        numbers = LottoGenerator.generateLotto();
+    }
+
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    // TODO: 추가 기능 구현
 }
