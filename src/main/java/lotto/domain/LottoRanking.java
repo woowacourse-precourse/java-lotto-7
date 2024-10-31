ae65ffe8f25648607cf2eb377cd15375ea8a716c
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-31 18:30:55 +0900

    fix(LottoRanking): 3개 일치 시 출력하는 문구 수정

--- src/main/java/lotto/domain/LottoRanking.java
+++ src/main/java/lotto/domain/LottoRanking.java
@@ -5,7 +5,7 @@ public enum LottoRanking {
     SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
     THIRD(5, false, "5개 일치 (1,500,000원)", 1_500_000),
     FOURTH(4, false, "4개 일치 (50,000원)", 50_000),
-    FIFTH(3, false, "3개 일치 (5,000)원", 5_000),
+    FIFTH(3, false, "3개 일치 (5,000원)", 5_000),
     NOTHING(0, false, "", 0);
 
     private final int matchCount;
