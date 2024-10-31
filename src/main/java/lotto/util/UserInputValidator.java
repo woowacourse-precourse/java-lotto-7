ac1cc82cc2fe0e8aba6996194061c0dbf3775d57
posite <su84053@gmail.com>
2024-10-31 13:58:47 +0900

    feat(UserInputValidator): 당첨 번호 검증 구현
    입력된 사용자의 당첨 번호의 검증 기능을 구현하였습니다.

--- src/main/java/lotto/util/UserInputValidator.java
+++ src/main/java/lotto/util/UserInputValidator.java
@@ -2,6 +2,7 @@ package lotto.util;
 
 import static lotto.constant.ErrorMessage.LESS_THAN_THOUSAND_PURCHASE_AMOUNT;
 import static lotto.constant.ErrorMessage.NOT_NUMBER_PURCHASE_AMOUNT;
+import static lotto.constant.ErrorMessage.NOT_NUMBER_RANGE_WINNING_NUMBER;
 import static lotto.constant.ErrorMessage.NOT_PURCHASE_AMOUNT_FORMAT;
 import static lotto.constant.ErrorMessage.TOO_BIG_PURCHASE_AMOUNT;
 
@@ -9,35 +10,67 @@ public class UserInputValidator {
     private static final int PURCHASE_AMOUNT_UNIT = 1000;
     private static final int PURCHASE_AMOUNT_LENGTH_LIMIT = 10;
     private static final int PURCHASE_AMOUNT_REMAIN = 0;
+    private static final int WINNING_NUMBERS_SIZE = 6;
+    private static final int WINNING_NUMBER_MIN = 1;
+    private static final int WINNING_NUMBER_MAX = 45;
 
     public void isValidPurchaseAmount(String purchaseAmount) {
-        isUserInputNumber(purchaseAmount);
+        isPurchaseAmountNumber(purchaseAmount);
         isPurchaseAmountTooBing(purchaseAmount);
         isPurchaseAmountLessThanThousand(purchaseAmount);
         isPurchaseAmountMultipleOfThousand(purchaseAmount);
     }
 
-    public void isUserInputNumber(String purchaseAmount) {
+    public void isValidWinningNumbers(String[] winningNumbers) {
+        isWinningNumbersSizeSix(winningNumbers);
+        isWinningNumberNumber(winningNumbers);
+        isWinningNumberInRange(winningNumbers);
+    }
+
+    private void isPurchaseAmountNumber(String purchaseAmount) {
         if (!purchaseAmount.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(NOT_NUMBER_PURCHASE_AMOUNT.getMessage());
         }
     }
 
-    public void isPurchaseAmountTooBing(String purchaseAmount) {
+    private void isPurchaseAmountTooBing(String purchaseAmount) {
         if (purchaseAmount.length() > PURCHASE_AMOUNT_LENGTH_LIMIT) {
             throw new IllegalArgumentException(TOO_BIG_PURCHASE_AMOUNT.getMessage());
         }
     }
 
-    public void isPurchaseAmountLessThanThousand(String purchaseAmount) {
+    private void isPurchaseAmountLessThanThousand(String purchaseAmount) {
         if (Integer.parseInt(purchaseAmount) < PURCHASE_AMOUNT_UNIT) {
             throw new IllegalArgumentException(LESS_THAN_THOUSAND_PURCHASE_AMOUNT.getMessage());
         }
     }
 
-    public void isPurchaseAmountMultipleOfThousand(String purchaseAmount) {
+    private void isPurchaseAmountMultipleOfThousand(String purchaseAmount) {
         if (Integer.parseInt(purchaseAmount) % PURCHASE_AMOUNT_UNIT != PURCHASE_AMOUNT_REMAIN) {
             throw new IllegalArgumentException(NOT_PURCHASE_AMOUNT_FORMAT.getMessage());
+        }
+    }
+
+    private void isWinningNumberNumber(String[] winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            if (!winningNumber.chars().allMatch(Character::isDigit)) {
+                throw new IllegalArgumentException(NOT_NUMBER_PURCHASE_AMOUNT.getMessage());
+            }
+        }
+    }
+
+    private void isWinningNumbersSizeSix(String[] winningNumbers) {
+        if (winningNumbers.length != WINNING_NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isWinningNumberInRange(String[] winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            if (Integer.parseInt(winningNumber) < WINNING_NUMBER_MIN
+                    || Integer.parseInt(winningNumber) > WINNING_NUMBER_MAX) {
+                throw new IllegalArgumentException(NOT_NUMBER_RANGE_WINNING_NUMBER.getMessage());
+            }
         }
     }
 }
