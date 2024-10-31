9b9bf12e36a61d7d4c19785ee7424c87bdf5e614
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 15:40:00 +0900

    feat(view): 당첨 번호를 입력 받는다

--- src/main/java/lotto/view/WinningNumbersInputView.java
+++ src/main/java/lotto/view/WinningNumbersInputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class WinningNumbersInputView {
     private static final String WINNING_NUMBERS_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
 
     public void printWinningNumbersInputGuide() {
         System.out.println(WINNING_NUMBERS_INPUT_GUIDE);
+    }
+    
+    public String getWinningNumbers() {
+        return Console.readLine();
     }
 }
