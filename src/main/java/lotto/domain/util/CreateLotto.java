d3176a6dd5e692360dc0dc89a1065a580d40c40a
jaeyeong <zzz26lk@gmail.com>
2024-10-31 15:25:54 +0900

    feat: 로또 숫자 생성 코드 추가
    랜덤한 숫자의 번호를 생성하기 위해서
    로또 숫자를 생성하는 코드를 추가한다
    또한, Lotto 클래스의 위치를 domain으로 옮긴다.

---
+++ src/main/java/lotto/domain/util/CreateLotto.java
@@ -0,0 +1,20 @@
+package lotto.domain.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CreateLotto {
+
+    private static final int RANGE_MIN = 1;
+    private static final int RANGE_MAX = 45;
+    private static final int LOTTO_COUNT = 6;
+
+    protected static List<Integer> lotto() {
+        List<Integer> lottoNumbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(RANGE_MIN, RANGE_MAX, LOTTO_COUNT));
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
+}
