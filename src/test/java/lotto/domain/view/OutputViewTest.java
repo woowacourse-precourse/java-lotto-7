45d9dd0edff6e2c2612743f9251e20465a95213d
LSBsity <dltmdqls51@naver.com>
2024-11-01 00:16:23 +0900

    test(OutputViewTest): 당첨 로또 출력 테스트
    당첨된 로또 정보를 출력하는 테스트

--- src/test/java/lotto/domain/view/OutputViewTest.java
+++ src/test/java/lotto/domain/view/OutputViewTest.java
@@ -1,7 +1,9 @@
 package lotto.domain.view;
 
 import lotto.common.config.Factory;
-import lotto.domain.model.Lotto;
+import lotto.domain.model.lotto.Lotto;
+import lotto.domain.model.lotto.LottoRank;
+import lotto.domain.model.lotto.LottoSummary;
 import lotto.domain.utils.TestLotto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,7 +36,7 @@ class OutputViewTest {
         System.setOut(System.out);
     }
 
-    @DisplayName("OutputView는 사용자에게 생성된 로또와 개수를 출력해야 한다.")
+    @DisplayName("printLottos메서드는 사용자에게 생성된 로또와 개수를 출력해야 한다.")
     @Test
     void printLottos() {
         String prompt = "3개를 구매했습니다.";
@@ -49,6 +52,35 @@ class OutputViewTest {
         outputView.printLottos(testLottos);
 
         assertEquals(String.format("%s\n%s\n%s\n%s\n", prompt, format1, format2, format3),
+                outputStreamCaptor.toString());
+    }
+
+    @DisplayName("printLottoSummary메서드는 로또 당첨 결과를 정리하여 보여준다.")
+    @Test
+    void printLottoSummary() {
+        //given
+        int amount = 5000;
+        LottoRank first = LottoRank.FIRST;
+        LottoRank second = LottoRank.SECOND;
+        LottoRank third = LottoRank.THIRD;
+        LottoRank fourth = LottoRank.FOURTH;
+        LottoRank fifth = LottoRank.FIFTH;
+        HashMap<LottoRank, Long> resultMap = new HashMap<>();
+        resultMap.put(first, 1L);
+        resultMap.put(second, 1L);
+        resultMap.put(third, 1L);
+        resultMap.put(fourth, 1L);
+        resultMap.put(fifth, 1L);
+        LottoSummary testSummary = LottoSummary.create(resultMap);
+
+        //when
+        outputView.printLottoSummary(testSummary);
+
+        assertEquals("3개 일치 (5,000원) - 1개\n" +
+                "4개 일치 (50,000원) - 1개\n" +
+                "5개 일치 (1,500,000원) - 1개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 1개\n",
                 outputStreamCaptor.toString());
     }
 }
