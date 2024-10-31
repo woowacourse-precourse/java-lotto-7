af887bd5ebd263378be5291f0a3748b3f67307aa
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 21:50:05 +0900

    refactor(LottoVendingMachine) : 가독성 개선을 위한 코드 수정

--- src/main/java/lotto/model/LottoVendingMachine.java
+++ src/main/java/lotto/model/LottoVendingMachine.java
@@ -3,25 +3,27 @@ package lotto.model;
 import static lotto.util.LottoConstants.LOTTO_TICKET_PRICE;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoVendingMachine {
 
-    private final int totalCost;
-    private final List<Lotto> purchased;
+    private final Integer totalCost;
 
     public LottoVendingMachine(Integer totalCost) {
         this.totalCost = totalCost;
-        this.purchased = new ArrayList<>();
     }
 
     public List<Lotto> issueLotto() {
-        for (int i = 1; i <= totalCost/LOTTO_TICKET_PRICE; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
-            purchased.add(new Lotto(numbers));
-        }
-        return Collections.unmodifiableList(purchased);
+        int lottoCount = totalCost/LOTTO_TICKET_PRICE;
+
+        return IntStream.rangeClosed(1, lottoCount)
+                .mapToObj(buyCount -> Randoms.pickUniqueNumbersInRange(1,45,6))
+                .map(this::issueLotto)
+                .toList();
+    }
+
+    private Lotto issueLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
     }
 }
