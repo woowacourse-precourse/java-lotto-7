6668adbd6f3fba2330b5c6901eaab17c3cdb962f
KEEKE132 <dksvlfdhs1@naver.com>
2024-10-31 19:58:12 +0900

    feat: 상수 관리 클래스 정의

---
+++ src/main/java/lotto/common/LottoConstant.java
@@ -0,0 +1,11 @@
+package lotto.common;
+
+public class LottoConstant {
+    public static final int NUMBERS_SIZE = 6;
+    public static final int MAX_NUMBER = 45;
+    public static final int MIN_NUMBER = 1;
+    public static final int LOTTO_PRICE = 1000;
+
+    private LottoConstant() {
+    }
+}
