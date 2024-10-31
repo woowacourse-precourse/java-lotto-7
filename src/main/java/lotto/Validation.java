004e1e786bcc11d1aa3ca1ad2add316f8e0f2449
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-01 02:04:42 +0900

    feat(Validation): 중복 여부 검사 메소드 추가

---
+++ src/main/java/lotto/Validation.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Validation {
+    public static boolean validateNoDuplication(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int number:numbers) {
+            if(!set.add(number)) return false;
+            set.add(number);
+        }
+        return true;
+    }
+}
