8b48a2fd349c9504ae73d2964ea5774d5f90104f
sleepyhoon <pokjm99@gmail.com>
2024-10-31 17:54:32 +0900

    feat (InputMoney) : 사용자의 로또 구입 금액

---
+++ src/main/java/lotto/InputMoney.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import static lotto.Constant.LOTTO_PRICE;
+import static lotto.Constant.MINIMUM_PRICE;
+
+public class InputMoney {
+    private final long amount;
+
+    public InputMoney(long amount) {
+        validateInputMoney(amount);
+        this.amount = amount;
+    }
+
+    public long getBuyAmount() {
+        return amount / LOTTO_PRICE;
+    }
+
+    private void validateInputMoney(long userInputMoney) {
+        if(userInputMoney % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 입력해야 합니다");
+        }
+        if(userInputMoney < MINIMUM_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 0원 이상 결제해야 합니다.");
+        }
+    }
+}
