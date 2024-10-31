257331b65ff56b6adac826df472e86855e8ebe32
parksomii <somi020605@gmail.com>
2024-11-01 02:46:02 +0900

    feat(LottoGameController): 기본 LottoGameController 클래스 추가

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,22 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+    private OutputView outputView = new OutputView();
+    private InputView inputView = new InputView();
+
+    public void run() {
+        gameStart();
+        gameResult();
+    }
+
+    private void gameStart() {
+        // 기능 추가 예정
+    }
+
+    private void gameResult() {
+        // 기능 추가 예정
+    }
+}
