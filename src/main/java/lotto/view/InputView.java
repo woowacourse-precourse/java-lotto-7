357c5f9134f8aaddc05a5339877c53c7d35c5eae
alpin87 <qortmdals94@naver.com>
2024-10-31 19:53:33 +0900

    style: 예시와 맞게 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -22,6 +22,7 @@ public class InputView {
     public List<Integer> inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
+        System.out.println();
         try {
             return Arrays.stream(input.split(","))
                     .map((String::trim))
