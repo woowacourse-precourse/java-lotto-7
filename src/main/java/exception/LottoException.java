d3c19415a449b85fccbbd80628e0581322637af9
cux-maks <volcano020716@gmail.com>
2024-10-31 15:34:06 +0900

    feat(LottoException): 중복값 예외 추가
    중복값에 대한 예외 문구를 추가하였습니다.

--- src/main/java/exception/LottoException.java
+++ src/main/java/exception/LottoException.java
@@ -4,7 +4,8 @@ public enum LottoException {
     NOT_A_NUMBER("숫자가 아닙니다."),
     CAN_NOT_DIVIDE_BY_1000("1000으로 나누어 떨어지지 않습니다."),
     OUT_OF_LOTTO_NUMBER_RANGE("로또 번호 범위를 벗어났습니다."),
-    LENGTH_IS_NOT_SIX("로또 번호가 6개가 아닙니다.");
+    LENGTH_IS_NOT_SIX("로또 번호가 6개가 아닙니다."),
+    DUPLICATE_NUMBER("중복된 값은 입력할 수 없습니다.");
 
     private final String message;
 
