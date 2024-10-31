07cbc83e77cfc597955091756bbc439737596769
StoneCAU <seokwoo1999@naver.com>
2024-11-01 01:42:33 +0900

    feat(Parser): 당첨 번호 input 값에 대한 파싱 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,10 +1,18 @@
 package lotto.utils;
 
+import static java.lang.String.format;
+import static lotto.exception.ErrorMessage.END_WITH_DELIMITER;
+import static lotto.exception.ErrorMessage.INPUT_EMPTY_ERROR;
 import static lotto.exception.ErrorMessage.MONEY_NOT_NUMBER;
+import static lotto.exception.ErrorMessage.WINNING_NUMBER_NOT_DIGIT;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.exception.LottoException;
 
 public class Parser {
+    private final static String DELIMITER = ",";
+
     public static int parsePrice(final String input) {
         int purchaseAmount;
 
@@ -15,5 +23,55 @@ public class Parser {
         }
 
         return purchaseAmount;
+    }
+
+    public static List<Integer> parseNumbers(final String input) {
+        inputValidate(input);
+
+        return splitByDelimiter(input).stream()
+                .map(Parser::convertToInt)
+                .toList();
+    }
+
+    private static void inputValidate(final String input) {
+        validateEndWithDelimiter(input);
+        validateEmpty(input);
+    }
+
+    private static void validateEmpty(final String input) {
+        if (isEmpty(input)) {
+            throw new LottoException(INPUT_EMPTY_ERROR);
+        }
+    }
+
+    private static void validateEndWithDelimiter(final String input) {
+        if (isEndWithDelimiter(input)) {
+            throw new LottoException(format(END_WITH_DELIMITER.getMessage(), DELIMITER));
+        }
+    }
+
+    private static List<String> splitByDelimiter(final String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .toList();
+    }
+
+    private static int convertToInt(final String str) {
+        int number;
+
+        try {
+            number = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new LottoException(WINNING_NUMBER_NOT_DIGIT);
+        }
+
+        return number;
+    }
+
+    private static boolean isEndWithDelimiter(final String input) {
+        return input.endsWith(DELIMITER);
+    }
+
+    private static boolean isEmpty(final String input) {
+        return input.isEmpty();
     }
 }
