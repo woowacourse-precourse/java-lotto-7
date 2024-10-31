0dbb1462863be19c19535366bb192470966e85d2
woongjee <jeewoong7753@naver.com>
2024-10-31 17:55:47 +0900

    feat(Selling):구매한 수량에 맞는 랜덤 번호 리스트 반환 기능 구현

--- src/main/java/lotto/Selling.java
+++ src/main/java/lotto/Selling.java
@@ -1,12 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Selling {
     private static final int PRICE = 1000;
 
-    public void validatePrice(int purchaseLotto) {
-        if(purchaseLotto%PRICE != 0){
+    public void validatePrice(int purchase) {
+        if(purchase%PRICE != 0){
             throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위여야 합니다.");
         }
     }
 
+    public List<Lotto> purchaseLottos(int purchase){
+        validatePrice(purchase);
+        int lottoCount = purchase/PRICE;
+        List<Lotto> lottos = new ArrayList<Lotto>(lottoCount);
+        for(int i =0;i<lottoCount;i++){
+            lottos.add(new Lotto(generateRandomNumbers()));
+        }
+        return lottos;
+    }
+
+    private List<Integer> generateRandomNumbers() {
+    }
 }
