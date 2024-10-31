b0a5c6c5a8bb0015af4299f022cd1340bfa0242b
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-31 19:45:21 +0900

    style: BLANK_MESSAGE 상수명 수정

--- src/main/java/lotto/exception/BlankMessageException.java
+++ src/main/java/lotto/exception/BlankMessageException.java
@@ -4,6 +4,6 @@ import lotto.message.ErrorMessage;
 
 public final class BlankMessageException extends IllegalArgumentException {
     public BlankMessageException() {
-        super(ErrorMessage.Blank_Message.getErrorMessage());
+        super(ErrorMessage.BLANK_MESSAGE.getErrorMessage());
     }
 }
