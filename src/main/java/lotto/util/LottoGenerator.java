410c1ec151891e4a2a44a160b3f246b3e2f30d6a
sanghee0820 <lsh277604@gmail.com>
2024-10-31 15:49:31 +0900

    refactor: 상수를 public으로 변경

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public final class LottoGenerator {
-    private static final int LOTTO_SIZE = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
 
     private LottoGenerator() {
     }
