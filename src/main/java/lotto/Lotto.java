aec0a742dad4aa92df01cbe6e92c3e3482dea6b9
pfalz <mega75581@gmail.com>
2024-10-31 17:40:15 +0900

    adding duplicate error handling function to Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,8 +13,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+
+        Set<Integer> set = new HashSet<>();
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        for (int number : numbers) {
+
+            if (set.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되었습니다.");
+            }
+
+            set.add(number);
         }
     }
 
