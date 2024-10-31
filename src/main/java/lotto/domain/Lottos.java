3a336e01e879346a0575015f1c12b908b1ed8cc7
lvalentine6 <ysr1227@naver.com>
2024-11-01 06:45:13 +0900

    feat: add skeleton lottos

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lottos {
+
+	private List<Lotto> lottos;
+
+	private Lottos(List<Lotto> lottos) {
+		this.lottos = lottos;
+	}
+
+	public static Lottos from(List<Lotto> lottos) {
+		return new Lottos(lottos);
+	}
 }
