753ba15c89dfcf05535493a43dbc9ba1f0a0d105
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-31 19:31:38 +0900

    feat(GradeSaver) : 로또 결과 합치는 기능
    입력받은 lotto result를 가지고 각 등수별로 몇개씩 있는지 기록해줌.

--- src/main/java/lotto/GradeSaver.java
+++ src/main/java/lotto/GradeSaver.java
@@ -9,6 +9,17 @@ public class GradeSaver {
     private Integer fifthBonus = 0;
     private Integer sixth = 0;
 
+    public GradeSaver sumUpGrades(List<MyResult> resultList){
+        for(MyResult result : resultList){
+            if(result.getMatches() == 3) this.third++;
+            else if(result.getMatches() == 4) this.fourth++;
+            else if(result.getMatches() == 5 && result.getBonusPoint()) this.fifth++;
+            else if(result.getMatches() == 5 && !result.getBonusPoint()) this.sixth++;
+            else if(result.getMatches() == 6) this.sixth++;
+        }
+        return this;
+    }
+
     public Integer getThird() {
         return third;
     }
