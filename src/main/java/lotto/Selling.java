5285f77849f848f31ae7618d6e0b6e03c8526637
woongjee <jeewoong7753@naver.com>
2024-10-31 17:51:01 +0900

    feat(Selling):가격 검증 기능 구현

--- src/main/java/lotto/Selling.java
+++ src/main/java/lotto/Selling.java
@@ -1,4 +1,12 @@
 package lotto;
 
 public class Selling {
+    private static final int PRICE = 1000;
+
+    public void validatePrice(int purchaseLotto) {
+        if(purchaseLotto%PRICE != 0){
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
 }
