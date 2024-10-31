d7491b07fc0980d56d5b2db5c523776411ac57fc
joshinhyeok <shtree07@naver.com>
2024-11-01 00:39:45 +0900

    feat: 로또 숫자들이 1에서 45이내의 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,11 +10,21 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public int getNumbersSize() {
+        return numbers.size();
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
-
-    // TODO: 추가 기능 구현
 }
