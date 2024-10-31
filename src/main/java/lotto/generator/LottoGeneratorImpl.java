79905dd8d87f24fd09cce6bd189c971dd03b0b53
Sinorang <sinholang@naver.com>
2024-10-31 23:24:43 +0900

    feat: 로또 번호 생성 시 오름차순 정렬 기능 추가

--- src/main/java/lotto/generator/LottoGeneratorImpl.java
+++ src/main/java/lotto/generator/LottoGeneratorImpl.java
@@ -9,6 +9,7 @@ public class LottoGeneratorImpl implements LottoGenerator {
     @Override
     public List<Integer> generate() {
         return Randoms.pickUniqueNumbersInRange(LottoConstants.LOTTO_MIN_NUMBER, LottoConstants.LOTTO_MAX_NUMBER, LottoConstants.LOTTO_NUMBER_COUNT).stream()
+                .sorted()
                 .toList();
     }
 }
