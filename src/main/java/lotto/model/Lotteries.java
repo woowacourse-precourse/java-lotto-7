0a60a3c1002037147b9b4ee5af33139d84cf3d36
woogym <kwj9294@gmail.com>
2024-11-01 04:08:05 +0900

    feat(model): Add lotteries mangement logic

--- src/main/java/lotto/model/Lotteries.java
+++ src/main/java/lotto/model/Lotteries.java
@@ -1,2 +1,37 @@
-package lotto.model;public class Lotteries {
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.util.LottoNumberGenerator;
+import lotto.vo.BonusNumber;
+import lotto.vo.TicketCount;
+
+public class Lotteries {
+
+    private final List<Lotto> lotteries;
+
+    private Lotteries(List<Lotto> lotteries) {
+        this.lotteries = lotteries;
+    }
+
+    public static Lotteries createLotteries(final TicketCount ticketCount,
+                                            final LottoNumberGenerator lottoNumberGenerator) {
+        List<Lotto> lotteries = IntStream.range(0, ticketCount.count())
+                .mapToObj(eachTicket -> Lotto.createLottoNumber(lottoNumberGenerator))
+                .toList();
+
+        return new Lotteries(lotteries);
+    }
+
+    public List<Integer> countMatchedNumbers(final Lotto winningLottoNumber) {
+        return lotteries.stream()
+                .map(lotto -> lotto.countMatchedNumbers(winningLottoNumber))
+                .toList();
+    }
+
+    public List<Boolean> checkBonusNumberContain(final BonusNumber bonusNumber) {
+        return lotteries.stream()
+                .map(lotto -> lotto.checkBonusNumberContain(bonusNumber))
+                .toList();
+    }
 }
