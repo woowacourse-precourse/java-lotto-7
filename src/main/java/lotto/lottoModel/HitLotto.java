b9e502a2ae86a12566648967a08d2a11282a0789
ppzepi <hsunguk1999@gmail.com>
2024-11-01 06:51:19 +0900

    refactor: 싱글톤 인스턴스로 객체 수정

--- src/main/java/lotto/lottoModel/HitLotto.java
+++ src/main/java/lotto/lottoModel/HitLotto.java
@@ -1,18 +1,30 @@
 package lotto.lottoModel;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HitLotto {
     private final List<Integer> hitNumbers;
     private final int bonusNumber;
 
-    public HitLotto(List<Integer> hitLotto, int bonusNumber) {
-        this.hitNumbers = hitLotto;
+    // 싱글톤 인스턴스
+    private static HitLotto instance;
+
+    private HitLotto(List<Integer> hitNumbers, int bonusNumber) {
+        this.hitNumbers = Collections.unmodifiableList(new ArrayList<>(hitNumbers)); // 불변 리스트
         this.bonusNumber = bonusNumber;
     }
 
-    public List<Integer> getALlHitNumbers() {
+    // 싱글톤 인스턴스를 반환하는 메서드
+    public static HitLotto getInstance(List<Integer> hitNumbers, int bonusNumber) {
+        if (instance == null) {
+            instance = new HitLotto(hitNumbers, bonusNumber);
+        }
+        return instance;
+    }
+
+    public List<Integer> getAllHitNumbers() {
         List<Integer> allNumbers = new ArrayList<>(hitNumbers);
         allNumbers.add(bonusNumber);
         return allNumbers;
@@ -20,6 +32,7 @@ public class HitLotto {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
     }
 
 }
