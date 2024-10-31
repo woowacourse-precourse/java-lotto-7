a53884bce565428835bb4b3f344090539a460098
KangGeonyoung <kky6335@gmail.com>
2024-10-31 21:19:52 +0900

    feat(InputValidator): 구입 금액이 1000원 단위인지 검사 InputValidator - isValidUnit: 구입 금액이 1000원 단위인지 검사하는 메서드

--- src/main/java/lotto/domain/InputValidator.java
+++ src/main/java/lotto/domain/InputValidator.java
@@ -14,5 +14,12 @@ public class InputValidator {
         }
     }
 
-
+    /**
+     * 구입 금액이 1000원 단위인지 검사
+     */
+    public static void isValidUnit(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위로 입력하셔야 합니다.");
+        }
+    }
 }
