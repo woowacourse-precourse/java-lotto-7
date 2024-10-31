057cf12b79f949f7cb6b5b6153dde2236b6b5089
corpi <lj845854@gmail.com>
2024-10-31 18:40:06 +0900

    feat(LottoHistory): 당첨 통계를 가질 수 있다.

--- src/main/java/lotto/model/LottoHistory.java
+++ src/main/java/lotto/model/LottoHistory.java
@@ -5,9 +5,26 @@ import java.util.List;
 public class LottoHistory {
     private final WinningLotto winningLotto;
     private final List<Lotto> boughtLottoList;
+    private final LottoStatistic lottoStatistic;
 
     public LottoHistory(WinningLotto winningLotto, List<Lotto> boughtLottoList) {
         this.winningLotto = winningLotto;
-        this.boughtLottoList = boughtLottoList;
+        this.boughtLottoList = List.copyOf(boughtLottoList);
+        List<LottoRank> rankList = boughtLottoList.stream()
+                .map(lotto -> lotto.getRankFrom(winningLotto))
+                .toList();
+        this.lottoStatistic = new LottoStatistic(rankList);
+    }
+
+    public WinningLotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public List<Lotto> getBoughtLottoList() {
+        return List.copyOf(boughtLottoList);
+    }
+
+    public LottoStatistic getLottoStatistic() {
+        return lottoStatistic;
     }
 }
