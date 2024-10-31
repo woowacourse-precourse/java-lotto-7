5b86fd9e44c47eaeffc73490d50616555b30ccde
Jewan1120 <hawaii1468@gmail.com>
2024-10-31 23:24:05 +0900

    feat(Controller): 로또 컨트롤러 추가
    흐름을 제어하기 위한 컨트롤러를 추가했습니다.

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.LottoValidator;
+
+public class LottoController {
+
+    public void run() {
+
+    }
+}
