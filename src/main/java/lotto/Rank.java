c732b071719fc83705ae30528fdd70a9e7e39588
je488 <pje488@naver.com>
2024-10-31 23:21:48 +0900

    feat: Rank에 당첨 메시지를 나타내는 필드 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,20 +3,22 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
-    FIFTH(3, false, 5000);
+    FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원)"),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, false, 1500000, "5개 일치 (1,500,000원)"),
+    FOURTH(4, false, 50000, "4개 일치 (50,000원)"),
+    FIFTH(3, false, 5000, "3개 일치 (5,000원)");
 
     private final int matchCount;
     private final boolean isBonusNumberMatched;
     private final int prizeMoney;
+    private final String winningMessage;
 
-    Rank(int matchCount, boolean isBonusNumberMatched, int prizeMoney) {
+    Rank(int matchCount, boolean isBonusNumberMatched, int prizeMoney, String winningMessage) {
         this.matchCount = matchCount;
         this.isBonusNumberMatched = isBonusNumberMatched;
         this.prizeMoney = prizeMoney;
+        this.winningMessage = winningMessage;
     }
 
     public static Rank valueOf (int matchCount, boolean isBonusNumberMatched) {
@@ -25,5 +27,9 @@ public enum Rank {
                 .filter(rank -> isBonusNumberMatched == rank.isBonusNumberMatched)
                 .findFirst()
                 .orElse(null);
+    }
+
+    public String getWinningMessage() {
+        return winningMessage;
     }
 }
