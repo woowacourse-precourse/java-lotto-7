c3509dc9bfb952b57b6d0a665fd45ba933353eba
leejunnyeop <daffodil8620@naver.com>
2024-10-31 16:02:56 +0900

    [refactor] OutputView(view) - static 으로 변경

--- src/main/java/lotto/week3/view/OutputView.java
+++ src/main/java/lotto/week3/view/OutputView.java
@@ -1,10 +1,11 @@
 package lotto.week3.view;
 
 import java.util.List;
+import lotto.week3.domain.Lotto;
 
 public class OutputView {
 
-    public void lottoOutput(List<List<Integer>> lotto){
+    public static void lottoOutput(List<Lotto> lotto){
         System.out.println(lotto.size() + " 개를 구매했습니다.");
         System.out.println(lotto);
     }
