4aee69bc6ce18e94c4c1a957302c2b81a35b662c
bmg1203 <bmg1203@naver.com>
2024-11-01 03:41:47 +0900

    [feat]: LottoNumbersGenerator 추가
    - 구입한 개수에 따라 겹치지 않는 로또를 구매한다.

---
+++ src/main/java/lotto/logic/LottoNumbersGenerator.java
@@ -0,0 +1,39 @@
+package lotto.logic;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.input.Lotto;
+
+public class LottoNumbersGenerator {
+
+    private final List<Lotto> lottos;
+
+    public LottoNumbersGenerator(int count) {
+        List<Lotto> lottos = generateLottos(count);
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    private List<Lotto> generateLottos(int count) {
+        Set<Lotto> lottos = new HashSet<>();
+
+        while (lottos.size() == count) {
+            Lotto lotto = new Lotto(generateRandomNumbers());
+            lottos.add(lotto);
+        }
+
+        return List.copyOf(lottos);
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
