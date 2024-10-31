8bc143c5febe84ed5babb16d51141051b32f10c7
MinGwan Kim <fpem3309@gmail.com>
2024-10-31 20:35:41 +0900

    refactor: 에러메시지 "[ERROR]"로 시작하게 변경

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,8 +1,8 @@
 package lotto.constants;
 
 public enum ErrorMessage {
-    PURCHASE_MONEY_ONLY_CAN_NUMBER ("구입 금액은 숫자여야 합니다."),
-    PURCHASE_MONEY_ONLY_CAN_THOUSAND_UNIT("구입 금액은 1,000원 단위여야 합니다.");
+    PURCHASE_MONEY_ONLY_CAN_NUMBER ("[ERROR] 구입 금액은 숫자여야 합니다."),
+    PURCHASE_MONEY_ONLY_CAN_THOUSAND_UNIT("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
 
     private final String errorMessage;
 
