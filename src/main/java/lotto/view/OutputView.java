52e588ed09109eb75f587be264f482d5fa791bf8
dnjswns98 <tngkrt2004@gmail.com>
2024-10-31 16:21:58 +0900

    feat(outputView): Ask how much you want to buy

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String QUESTION_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
+
+    public void printInputMoney() {
+        System.out.println(QUESTION_LOTTO_AMOUNT);
+    }
+}
