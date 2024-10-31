2fe61fe7f39ce96ca1227c582bd5402ae4b62a0d
Zrfv <youngoh07015@gmail.com>
2024-10-31 12:27:55 +0900

    feat: 보너스 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
 
     private static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String inputPurchasePrice() {
         printMessage(INPUT_PURCHASE_PRICE_MESSAGE);
@@ -22,6 +23,12 @@ public class InputView {
         String input = Console.readLine();
         InputValidator.validateContainsComma(input);
         InputValidator.validateCommaFormat(input);
+        return input;
+    }
+
+    public String inputBonusNumber() {
+        printMessage(INPUT_BONUS_NUMBER_MESSAGE);
+        String input = Console.readLine();
         return input;
     }
 
