d4aa29033ebab5e43d15bd07ec239a611186dc79
EHCTH <lees90896@gmail.com>
2024-10-31 20:01:22 +0900

    style(WinningLottoTest): ErrorMessage 변경에 의한 수정

--- src/test/java/lotto/lotto/winning/validator/WinningLottoTest.java
+++ src/test/java/lotto/lotto/winning/validator/WinningLottoTest.java
@@ -1,4 +1,41 @@
 package lotto.lotto.winning.validator;
 
+import lotto.constant.ErrorMessage;
+import lotto.lotto.validator.LottoValidator;
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.winning.domain.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class WinningLottoTest {
+    @Test
+    @DisplayName("6자리 숫자 테스트")
+    void numberCountTest() {
+        assertThatThrownBy(() -> WinningLotto.of("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NUMBER_COUNT.getMessage());
+    }
+
+    @Test
+    @DisplayName("중복 테스트")
+    void duplicateTest() {
+        verifyExceptionForValidation("1,2,3,4,5,6", "6", ErrorMessage.DUPLICATE);
+    }
+
+    @Test
+    @DisplayName("1 ~ 45까지 범위의 숫자가 아닌경우 예외가 발생한다")
+    void withinRangeLottoNumberTest() {
+        verifyExceptionForValidation("4,45,6,7,8,9", "46", ErrorMessage.WITHIN_RANGE);
+    }
+
+    private void verifyExceptionForValidation(String winningNumbers, String bonusNumber, ErrorMessage errorMessage) {
+        WinningLotto winningLotto = WinningLotto.of(winningNumbers);
+        BonusNumber bonus = BonusNumber.of(bonusNumber);
+        assertThatThrownBy(
+                () -> LottoValidator.bonusNumberValidate(bonus, winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage.getMessage());
+    }
 }
