fdfdacdd0497f366af8288949327885f41cb48db
Jewan1120 <hawaii1468@gmail.com>
2024-11-01 01:11:57 +0900

    feat(Validator): 예외 발생 시 출력할 에러 메시지 추가
    각 상황에 맞는 에러 메시지를 출력하기 위해 추가했습니다.

--- src/main/java/lotto/enums/ErrorMessages.java
+++ src/main/java/lotto/enums/ErrorMessages.java
@@ -1,4 +1,22 @@
 package lotto.enums;
 
+import java.text.MessageFormat;
+
 public enum ErrorMessages {
+
+    INPUT_NOTHING("입력 문자열은 null이거나 비어있어서는 안 됩니다."),
+    MONEY_UNIT("구입 금액의 최소 단위는 {0}원입니다."),
+    NUMBER_FORMAT("입력은 숫자만 가능합니다.")
+
+    ;
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(Object... params) {
+        return MessageFormat.format("[ERROR] " + this.message, params);
+    }
 }
--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import lotto.enums.Constants;
+import lotto.enums.ErrorMessages;
 
 public class LottoValidator {
 
@@ -9,19 +10,19 @@ public class LottoValidator {
         isDigit(inputMoney);
         int money = Integer.parseInt(inputMoney);
         if (money % Constants.MONEY_UNIT.getValue() != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessages.MONEY_UNIT.getMessage(Constants.MONEY_UNIT.getValue()));
         }
     }
 
     private static void isEmpty(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessages.INPUT_NOTHING.getMessage());
         }
     }
 
     private static void isDigit(String inputMoney) {
         if (!inputMoney.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessages.NUMBER_FORMAT.getMessage());
         }
     }
 }
