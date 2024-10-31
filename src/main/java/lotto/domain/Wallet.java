64bee8967bd917d3df40eadb848daedbd0908195
world-dv <goodforx0147@naver.com>
2024-10-31 23:31:06 +0900

    feat: 로또 구입 금액 단위 검증
    - 로또 구입 금액을 1000원 단위로 제한하기 위해 검증합니다.
    - 로또 구입 금액이 1000으로 나누어 떨어지지 않을 경우 예외를 발생시킵니다.

---
+++ src/main/java/lotto/domain/Wallet.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class Wallet {
+
+    private static final Integer MIN_MONEY_UNIT = 1000;
+    private static final Integer MIN_MONEY_UNIT_CHECK_NUMBER = 0;
+
+    private final Integer money;
+
+    public Wallet(Integer money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(Integer money) {
+        if (money % MIN_MONEY_UNIT != MIN_MONEY_UNIT_CHECK_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
