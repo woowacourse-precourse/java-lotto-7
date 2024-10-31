3dcb64fd3af8c153e9b98f9004a5141723430ae7
seullll <tmfud@naver.com>
2024-10-31 18:52:49 +0900

    feat: 로또 발행

--- src/main/java/lotto/LottoNum.java
+++ src/main/java/lotto/LottoNum.java
@@ -9,13 +9,30 @@ import java.util.List;
 public class LottoNum {
     private List<Lotto> lottos = new ArrayList<>();
 
-    public LottoNum(int money){
+    public LottoNum(int money) {
         checkMoney(money);
+        lottoNumbers(money);
+        printNumber();
     }
 
-    private void checkMoney(int money){
-        if(money % 1000 != 0){
+    private void checkMoney(int money) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("1000원으로 나누어 떨어지지 않습니다.");
+        }
+    }
+
+    private void lottoNumbers(int money) {
+        for (int i = 0; i < money / 1000; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+    }
+
+    public void printNumber() {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
         }
     }
 }
