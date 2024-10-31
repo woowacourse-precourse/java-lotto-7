bdf9fc67ff3b3f7e01789253ea6afb907cabcc43
sc@t0a14 <gusdn6671@gmail.com>
2024-11-01 06:47:04 +0900

    feat(Controller): 로또 티켓 당첨 통계 계산 로직 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.SequencedMap;
 import lotto.domain.Lotto;
 import lotto.domain.LottoTickets;
+import lotto.domain.Rank;
 import lotto.utils.Converter;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -32,6 +35,31 @@ public class Controller {
         int bonusNumber = Converter.stringToInt(inputBonus);
 
         OutputView.printWinningStatistics();
+
+        SequencedMap<Rank, Integer> results = new LinkedHashMap<>();
+
+        for (Rank value : Rank.values()) {
+            results.put(value, 0);
+        }
+
+        for (List<Integer> lottoNumber : lottoNumbers) {
+            int matchCount = 0;
+            boolean matchBonus = false;
+
+            for (Integer number : lottoNumber) {
+                if (winningNumbers.contains(number)) {
+                    matchCount++;
+                }
+                if (number == bonusNumber) {
+                    matchBonus = true;
+                }
+            }
+
+            Rank rank = Rank.matchLotto(matchCount, matchBonus);
+            results.put(rank, results.get(rank) + 1);
+        }
+
+        System.out.println(results);
 
 
     }
