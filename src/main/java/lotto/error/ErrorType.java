ed6f0506e4d3e55a078e2c7c515bc1e9be3a3859
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-01 01:33:25 +0900

    feat(ErrorType): 에러 타입 정의

---
+++ src/main/java/lotto/error/ErrorType.java
@@ -0,0 +1,21 @@
+package lotto.error;
+
+import static lotto.constant.LottoRule.MAX_NUMBER;
+import static lotto.constant.LottoRule.MIN_NUMBER;
+import static lotto.constant.LottoRule.MONEY_UNIT;
+import static lotto.constant.LottoRule.WINNING_NUMBER_SIZE;
+
+public class ErrorType {
+
+    public static final String PREFIX = "[ERROR] ";
+    public static final String EXCEEDED_NUMBER_RANGE =
+            PREFIX + String.format("번호는 %d부터 %d 사이의 자연수여야 합니다.", MIN_NUMBER, MAX_NUMBER);
+    public static final String INVALID_WINNING_NUMBER_SIZE = String.format("당첨 번호는 %d자리여야 합니다.", WINNING_NUMBER_SIZE);
+    public static final String INVALID_NUMBER_FORMAT = PREFIX + "숫자는 자연수만 가능합니다.";
+    public static final String INVALID_MONEY_FORMAT = PREFIX + String.format("구입 금액은 %,d원 단위로만 가능합니다.", MONEY_UNIT);
+    public static final String DUPLICATED_WINNING_NUMBERS = PREFIX + "당첨 번호은 서로 중복될 수 없습니다.";
+    public static final String DUPLICATED_BONUS_NUMBER = PREFIX + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+    private ErrorType() {
+    }
+}
