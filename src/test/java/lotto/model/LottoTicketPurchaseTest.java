912fbae15513671865dff2e3d976a8941c69d093
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-31 11:54:57 +0900

    fix : 로또 구매 정보 모델의 인스턴스 수 변경으로 테스트 코드 수정

--- src/test/java/lotto/model/LottoTicketPurchaseTest.java
+++ src/test/java/lotto/model/LottoTicketPurchaseTest.java
@@ -10,7 +10,7 @@ class LottoTicketPurchaseTest {
     @DisplayName("로또 리스트에 널값이 들어오면 예외를 리턴해야한다.")
     @Test
     void 로또_리스트_널값이면_예외_발생() {
-        assertThatThrownBy(() -> new LottoTicketPurchase(null, 7))
+        assertThatThrownBy(() -> new LottoTicketPurchase(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 티켓이 발급되지 않았습니다!");
     }
