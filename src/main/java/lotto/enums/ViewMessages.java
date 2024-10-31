2d39c03e5f32fa76024f8e3a93ebac77c30894e0
Jewan1120 <hawaii1468@gmail.com>
2024-10-31 23:51:47 +0900

    feat(View): 로또 구입 금액 입력받기 위한 뷰 추가

--- src/main/java/lotto/enums/ViewMessages.java
+++ src/main/java/lotto/enums/ViewMessages.java
@@ -1,4 +1,20 @@
 package lotto.enums;
 
+import java.text.MessageFormat;
+
 public enum ViewMessages {
+    // Input
+    PROMPT_MONEY("구입금액을 입력해 주세요. (단위: 1,000원)"),
+
+    // Output
+    ;
+    private final String message;
+
+    ViewMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(Object... params) {
+        return MessageFormat.format(this.message, params);
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.ViewMessages;
+
 public class InputView {
+
+    public String promptMoney() {
+        System.out.println(ViewMessages.PROMPT_MONEY.getMessage());
+        return Console.readLine();
+    }
 }
