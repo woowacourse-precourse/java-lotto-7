193af42c3c2c7525b3f71ff1c3c21e22dba1183b
hanseounggyun <hanseu9839@gmail.com>
2024-10-31 17:12:45 +0900

    test: 로또 테스트 실패 코드 작성

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,8 +1,25 @@
 package lotto;
 
-public class LottoNumber {
-    
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoNumberTest {
+
+    @Test
+    void 로또_넘버_저장_테스트() {
+        LottoNumber lottoNumber = new LottoNumber(1);
+
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(1));
+    }
+
+    @Test
+    void 로또_넘버_45_이상_에러_테스트() {
+        assertThatThrownBy(() -> new LottoNumber(46))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
 
 
 }
