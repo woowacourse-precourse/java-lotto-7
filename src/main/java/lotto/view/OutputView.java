f95aea7d39e78aa46342103163b704985f1e20cb
rladmstn <oz33177351@gmail.com>
2024-11-01 00:55:47 +0900

    refactor(output) : 메서드 파라미터 이름 수정
    - 파라미터 이름 lotteries로 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,9 +5,9 @@ import lotto.domain.Lotto;
 import lotto.enums.PrintMessage;
 
 public class OutputView {
-    public void printLottoList(List<Lotto> lottos){
-        System.out.println(PrintMessage.OUTPUT_LOTTO_LIST.format(lottos.size()));
-        for(Lotto lotto : lottos){
+    public void printLottoList(List<Lotto> lotteries){
+        System.out.println(PrintMessage.OUTPUT_LOTTO_LIST.format(lotteries.size()));
+        for(Lotto lotto : lotteries){
             System.out.println(lotto.getNumbers());
         }
     }
