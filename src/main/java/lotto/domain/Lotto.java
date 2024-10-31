b553bcd520971875af10403bdaa1bf50beaa6c8c
qyinm <qusseun@gmail.com>
2024-10-31 10:07:38 +0900

    feat: 중복 숫자 유효성 검사 기능 추가 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
-import static lotto.exception.LottoErrorCode.*;
+import static lotto.exception.lotto.LottoErrorCode.INVALID_DUPLICATE_LOTTO_NUMBERS;
+import static lotto.exception.lotto.LottoErrorCode.INVALID_LOTTO_LENGTH;
 
 import java.util.List;
 import lotto.exception.LottoException;
@@ -15,12 +16,23 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateLottoNumbersSize(numbers);
+        validateUniqueLottoNumbers(numbers);
     }
 
     private void validateLottoNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new LottoException(INVALID_LOTTO_LENGTH);
         }
+    }
+
+    private void validateUniqueLottoNumbers(List<Integer> numbers) {
+        if (!isUniqueNumbers(numbers)) {
+            throw new LottoException(INVALID_DUPLICATE_LOTTO_NUMBERS);
+        }
+    }
+
+    private boolean isUniqueNumbers(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == numbers.size();
     }
 
     @Override
--- src/main/java/lotto/exception/lotto/LottoErrorCode.java
+++ src/main/java/lotto/exception/lotto/LottoErrorCode.java
@@ -1,7 +1,10 @@
-package lotto.exception;
+package lotto.exception.lotto;
+
+import lotto.exception.ErrorCode;
 
 public enum LottoErrorCode implements ErrorCode {
-    INVALID_LOTTO_LENGTH("로또 번호는 6개여야 합니다.");
+    INVALID_LOTTO_LENGTH("로또 번호는 6개여야 합니다."),
+    INVALID_DUPLICATE_LOTTO_NUMBERS("로또 번호에 중복된 숫자가 있으면 안됩니다.");
 
     private final String message;
 
