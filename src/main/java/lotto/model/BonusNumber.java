5a1d5a71461162b94c1a23c322f9c5d3d03dc508
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 19:22:34 +0900

    feat(BonusNumber): 보너스 번호가 당첨 번호와 중복이 있다면 예외가 발생한다

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static lotto.ExceptionMessage.BONUS_NUMBER_DUPLICATE_EXCEPTION;
 import static lotto.ExceptionMessage.BONUS_NUMBER_OUT_OF_RANGE_EXCEPTION;
 
 import java.util.List;
@@ -14,11 +15,18 @@ public class BonusNumber {
 
     private void validate(int bonusNumber, List<Integer> winningNumbers) {
         validateNumbersRange(bonusNumber);
+        validateNoDuplicateNumbers(bonusNumber, winningNumbers);
     }
 
     private void validateNumbersRange(int number) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE_EXCEPTION.message());
+        }
+    }
+
+    private void validateNoDuplicateNumbers(int bonusNumber, List<Integer> winningNumbers) {
+        if(winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATE_EXCEPTION.message());
         }
     }
 }
