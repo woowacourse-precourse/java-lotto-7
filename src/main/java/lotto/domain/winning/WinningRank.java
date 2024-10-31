2eda2182eb483ea3194e777e113d9c2f4d293705
donghyunn1 <dongdong0516@naver.com>
2024-11-01 00:10:11 +0900

    fix: enum 당첨 정보 순서 수정

--- src/main/java/lotto/domain/winning/WinningRank.java
+++ src/main/java/lotto/domain/winning/WinningRank.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum WinningRank {
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - %d개\n"),
-    SECOND(5,30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    FIRST(3,5000,"3개 일치 (5,000원) - %d개\n"),
+    SECOND(4,50000, "4개 일치 (50,000원) - %d개\n"),
     THIRD(5,1500000, "5개 일치 (1,500,000원) - %d개\n"),
-    FOURTH(4,50000, "4개 일치 (50,000원) - %d개\n"),
-    FIFTH(3,5000,"3개 일치 (5,000원) - %d개\n"),
+    FOURTH(5,30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    FIFTH(6, 2000000000, "6개 일치 (2,000,000,000원) - %d개\n"),
     NONE(0,0, "0개 일치\n");
 
     private int numOfMatched;
