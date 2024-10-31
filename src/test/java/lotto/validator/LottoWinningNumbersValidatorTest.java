005cf43e0c7c757ceb709889f2a5db2a767fccb6
changuii <rhljh7410@gmail.com>
2024-10-31 11:13:30 +0900

    test : LottoWinningNumbersValidator를 테스트하라

---
+++ src/test/java/lotto/validator/LottoWinningNumbersValidatorTest.java
@@ -0,0 +1,63 @@
+package lotto.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoWinningNumbersValidatorTest {
+
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private final LottoWinningNumbersValidator lottoWinningNumbersValidator;
+
+    public LottoWinningNumbersValidatorTest() {
+        this.lottoWinningNumbersValidator = new LottoWinningNumbersValidator(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX);
+    }
+
+    @Test
+    public void 문자가_포함된_당첨번호가_입력되면_예외가_발생한다() {
+        String winningNumbers = "1,2,32,5,6.8";
+
+        assertThatThrownBy(() -> lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호_개수가_6개_미만이라면_예외가_발생한다() {
+        String winningNumbers = "1,2,3,4,5";
+
+        assertThatThrownBy(() -> lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호중_최소미만_숫자가_포함되어있다면_예외가_발생한다() {
+        String winningNumbers = "1,2,3,4,5," + (LOTTO_NUMBER_MIN - 1);
+
+        assertThatThrownBy(() -> lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호중_최대_초과_숫자가_포함되어_있다면_예외가_발생한다() {
+        String winningNumbers = "1,2,3,4,5," + (LOTTO_NUMBER_MAX + 1);
+
+        assertThatThrownBy(() -> lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 당첨_번호중_중복이_있다면_예외가_발생한다() {
+        String winningNumbers = "1,2,3,4,5,1";
+
+        assertThatThrownBy(() -> lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 올바른_당첨_번호_테스트() {
+        String winningNumbers = "1,2,3,4,5,45";
+
+        lottoWinningNumbersValidator.validateLottoWinningNumbers(winningNumbers);
+    }
+}
