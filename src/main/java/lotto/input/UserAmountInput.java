92dc15767044513de0a86f182e4420ef1526893b
Spinichi <spinichi@naver.com>
2024-11-01 02:40:06 +0900

    feat: UserAmountInput 기능 변경
    amountinput과 관련성이 떨어지는 메서드는 다른 클래스로 옮기고, 메서드명을 좀 더 직관적이고 가독성있게 변경함.

--- src/main/java/lotto/input/UserAmountInput.java
+++ src/main/java/lotto/input/UserAmountInput.java
@@ -1,61 +1,42 @@
 package lotto.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.service.PrintTicketCount;
 
 public class UserAmountInput {
     private int amount;
-    private int lottoTicketCount;
     private final int TICKET_PRICE = 1000;
 
-    public void userAmountInput() {
+    public int validation() {
         while (true) {
             try {
-                getInputAndParse();
-                validate();
-                lottoTicketCount = setLottoTicketCount();
+                amount = parseInput(getInput());
+                validate(amount);
                 System.out.println();
-                break;
+                return amount;
             } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 입력이 잘못되었습니다. 로또 구입 금액은 숫자여야 합니다.");
+                System.out.println("[ERROR] 로또 구입 금액은 숫자여야 합니다.");
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public PrintTicketCount createPrintTicket() {
-        return new PrintTicketCount(lottoTicketCount);
+    public int getLottoTicketCount() {
+        return amount / TICKET_PRICE;
     }
 
-    public void printEarningsRate(double earnings) {
-        double earningsRate = ((earnings / amount) * 100);
-        System.out.printf("총 수익률은 %.1f%%입니다.", earningsRate);
-    }
-
-    private void getInputAndParse() {
-        String amountInput = userInput();
-        amount = parseAmount(amountInput);
-    }
-
-    private String userInput() {
+    private String getInput() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    private int parseAmount(String amountInput) {
+    private int parseInput(String amountInput) {
         return Integer.parseInt(amountInput.trim().replaceAll("\\s*,\\s*", ""));
     }
 
-    private void validate() {
+    private void validate(int amount) {
         if (amount <= 0 || amount % TICKET_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 입력이 잘못되었습니다. 로또 구입 금액은 천 원단위의 양수여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 천 원단위의 양수여야 합니다.");
         }
     }
-
-    private int setLottoTicketCount() {
-        return amount / TICKET_PRICE;
-    }
 }
-
-
