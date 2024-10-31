b07c6cdb9b290d2c45e4d94815d4e469ebea2f25
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-31 18:09:04 +0900

    refactor : 수익률 계산 로직 클래스 분리

---
+++ src/main/java/lotto/domain/calculators/YieldCalculator.java
@@ -0,0 +1,11 @@
+package lotto.domain.calculators;
+
+public class YieldCalculator {
+
+    public float calculate(long totalPrizeMoney, int amount) {
+        return (float) totalPrizeMoney / amount * 100;
+    }
+
+    //얘를 어다ㅣ에?
+
+}
