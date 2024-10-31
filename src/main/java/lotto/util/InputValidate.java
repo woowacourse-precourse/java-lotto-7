134b7fc1502bcea60e20dcd781ea7a51516b9c55
parksomii <somi020605@gmail.com>
2024-11-01 02:39:45 +0900

    feat(InputValidate): 보너스 번호 범위 유효성 검사 추가

--- src/main/java/lotto/util/InputValidate.java
+++ src/main/java/lotto/util/InputValidate.java
@@ -1,6 +1,15 @@
 package lotto.util;
 
 public class InputValidate {
+    public void bonusRangeValidate(String input) {
+        blankValidate(input);
+        isDigitValidate(input);
+        int bonus = Integer.parseInt(input);
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException(Error.LOTTO_RANGE_ERROR.message());
+        }
+    }
+
     public void isDigitValidate(String input) {
         try {
             Integer.parseInt(input);
