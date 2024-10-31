17254b3a11fb09d68d9a88456847785209c03d46
slay <palgalow@gmail.com>
2024-10-31 23:05:19 +0900

    refactor(Lottos): 로또들 저장하는 Lottos 클래스 생성

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private List<Lotto> lottos;
+
+    public Lottos() {
+        this.lottos = new ArrayList<>();
+    }
+}
