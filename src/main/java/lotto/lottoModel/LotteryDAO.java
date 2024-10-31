c87606462cfabc72341cb37f19ab7c5bbc7ca548
ppzepi <hsunguk1999@gmail.com>
2024-10-31 15:42:56 +0900

    feat: DAO 메서드 추가

--- src/main/java/lotto/lottoModel/LotteryDAO.java
+++ src/main/java/lotto/lottoModel/LotteryDAO.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LotteryDAO {
+    private List<Lotto> lottoNumbers;
+
+    public LotteryDAO() {
+        this.lottoNumbers = new ArrayList<>();
+    }
+
     // 데이터베이스에 로또 번호 저장
     public void save(Lotto lotto) {
-        // 구현: 데이터베이스에 로또 객체 저장
+        lottoNumbers.add(lotto);
     }
 
-    // 데이터베이스에서 로또 번호 조회
-    public Lotto find(int id) {
-        // 구현: 주어진 ID로 데이터베이스에서 로또 객체 조회
-        return null; // 예시 반환값
-    }
 
     // 모든 로또 번호 리스트 조회
-    public List<Lotto> findAll() {
-        // 구현: 모든 로또 객체를 데이터베이스에서 조회
-        return new ArrayList<>(); // 예시 반환값
+    public List<Lotto> getAll() {
+        return new ArrayList<>(lottoNumbers); // 예시 반환값
     }
 }
