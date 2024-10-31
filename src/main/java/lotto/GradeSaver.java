a1e3ef7bb012478135044b3808a3887a9f2a7db7
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-31 19:28:31 +0900

    feat(GradeSaver) : 로또 결과 취합
    채점된 내 로또들을 가지고 각 등수가 몇개씩 있는지 기록하는 클라스.

---
+++ src/main/java/lotto/GradeSaver.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.List;
+
+public class GradeSaver {
+    private Integer third = 0;
+    private Integer fourth = 0;
+    private Integer fifth = 0;
+    private Integer fifthBonus = 0;
+    private Integer sixth = 0;
+
+    public Integer getThird() {
+        return third;
+    }
+    public Integer getFourth() {
+        return fourth;
+    }
+    public Integer getFifth() {
+        return fifth;
+    }
+    public Integer getFifthBonus() {
+        return fifthBonus;
+    }
+    public Integer getSixth() {
+        return sixth;
+    }
+}
