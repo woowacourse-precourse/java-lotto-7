62bb4203fbc02533f114d35213bdc10caf83d811
bmg1203 <bmg1203@naver.com>
2024-11-01 04:49:45 +0900

    [feat]: 등수별 당첨 개수를 구하는 기능 추가
    - 1등부터 5등까지 등수 개수를 Map 에 저장

--- src/main/java/lotto/logic/LottoResultEvaluator.java
+++ src/main/java/lotto/logic/LottoResultEvaluator.java
@@ -1,12 +1,44 @@
 package lotto.logic;
 
+import static lotto.constants.RankNumber.FIFTH;
+import static lotto.constants.RankNumber.FIFTH_PRIZE_MONEY;
+import static lotto.constants.RankNumber.FIRST;
+import static lotto.constants.RankNumber.FIRST_PRIZE_MONEY;
+import static lotto.constants.RankNumber.FIVE;
+import static lotto.constants.RankNumber.FOUR;
+import static lotto.constants.RankNumber.FOURTH;
+import static lotto.constants.RankNumber.FOURTH_PRIZE_MONEY;
+import static lotto.constants.RankNumber.SECOND;
+import static lotto.constants.RankNumber.SECOND_PRIZE_MONEY;
+import static lotto.constants.RankNumber.SIX;
+import static lotto.constants.RankNumber.THIRD;
+import static lotto.constants.RankNumber.THIRD_PRIZE_MONEY;
+import static lotto.constants.RankNumber.THREE;
+
+import java.util.List;
 import java.util.Map;
+import lotto.constants.RankNumber;
+import lotto.input.Bonus;
+import lotto.input.Lotto;
+import lotto.input.Purchase;
 
 public class LottoResultEvaluator {
 
     private Map<Integer, Integer> result;
     private int profit;
     private double returnRate;
+
+    public LottoResultEvaluator(Lotto winningNumber, LottoNumbersGenerator lottos, Bonus bonus, Purchase purchase) {
+        for (Lotto lotto : lottos.getLottos()) {
+            List<Integer> copyLotto = lotto.getNumbers();
+            copyLotto.retainAll(winningNumber.getNumbers());
+            getFirstRank(copyLotto);
+            getSecondRank(lotto, copyLotto, bonus);
+            getThirdRank(copyLotto);
+            getFourthRank(copyLotto);
+            getFifthRank(copyLotto);
+        }
+    }
 
     public Map<Integer, Integer> getResult() {
         return result;
@@ -18,5 +50,35 @@ public class LottoResultEvaluator {
 
     public double getReturnRate() {
         return returnRate;
+    }
+
+    private void getFirstRank(List<Integer> copyLotto) {
+        if (copyLotto.size() == SIX.getNumber()) {
+            result.put(FIRST.getNumber(), result.getOrDefault(FIRST.getNumber(), 0) + 1);
+        }
+    }
+
+    private void getSecondRank(Lotto lotto, List<Integer> copyLotto, Bonus bonus) {
+        if (copyLotto.size() == FIVE.getNumber() && lotto.getNumbers().contains(bonus.getBonus())) {
+            result.put(SECOND.getNumber(), result.getOrDefault(SECOND.getNumber(), 0) + 1);
+        }
+    }
+
+    private void getThirdRank(List<Integer> copyLotto) {
+        if (copyLotto.size() == FIVE.getNumber()) {
+            result.put(THIRD.getNumber(), result.getOrDefault(THIRD.getNumber(), 0) + 1);
+        }
+    }
+
+    private void getFourthRank(List<Integer> copyLotto) {
+        if (copyLotto.size() == FOUR.getNumber()) {
+            result.put(FOURTH.getNumber(), result.getOrDefault(FOURTH.getNumber(), 0) + 1);
+        }
+    }
+
+    private void getFifthRank(List<Integer> copyLotto) {
+        if (copyLotto.size() == THREE.getNumber()) {
+            result.put(FIFTH.getNumber(), result.getOrDefault(FIFTH.getNumber(), 0) + 1);
+        }
     }
 }
