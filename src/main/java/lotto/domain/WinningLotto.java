769da7ffc48e69ac26393c69f0b4ffd429e26bdc
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-31 14:05:30 +0900

    feat: WinningLotto 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public boolean isBonusNumber(int number){
+        return number==bonusNumber;
+    }
+
+}
