e5c2ce22816dfd2c90b5bfe05e3a822b825f5198
Etwashoeren <sungmin0609@nate.com>
2024-11-01 00:59:12 +0900

    refactor. UserView와 UserInput 기능 분리

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -2,6 +2,46 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInput {
 
+    public int inputAmount() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 숫자이어야 합니다.");
+        }
+    }
+
+    public int inputBonusNumber() {
+        int bonus = Integer.parseInt(Console.readLine());
+        validateBonusNumber(bonus);
+        return bonus;
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        String winningNumbersInput = Console.readLine();
+        List<Integer> winningNumbersOutput = convertInputWinningNumbersToOutput(winningNumbersInput);
+        return winningNumbersOutput;
+    }
+
+    private List<Integer> convertInputWinningNumbersToOutput(String winningNumbers) {
+        String[] inputWinningNumbers = winningNumbers.split(",");
+
+        List<Integer> winningNumbersList = new ArrayList<>();
+
+        for (String inputWinningNumber : inputWinningNumbers) {
+            winningNumbersList.add(Integer.parseInt(inputWinningNumber));
+        }
+
+        return winningNumbersList;
+    }
+
+    private void validateBonusNumber(int bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45까지의 숫자만 허용됩니다.");
+        }
+    }
 }
--- src/main/java/lotto/UserView.java
+++ src/main/java/lotto/UserView.java
@@ -1,36 +1,20 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 public class UserView {
 
-    public int inputLottoAmount() {
+    public void displayInputMessageOfLottoAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 숫자이어야 합니다.");
-        }
     }
 
-    public List<Integer> inputWinningNumbers() {
+    public void displayInputMessageOfWinningNumbers() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
-        String winningNumbersInput = Console.readLine();
-        List<Integer> winningNumbersOutput = convertInputWinningNumbersToOutput(winningNumbersInput);
-        return winningNumbersOutput;
     }
 
-    public int inputBonusNumber() {
+    public void displayInputMessageOfBonusNumber() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
-        int bonus = Integer.parseInt(Console.readLine());
-        validateBonusNumber(bonus);
-        return bonus;
     }
 
     public void displayCountOfLottos(int count) {
@@ -43,21 +27,4 @@ public class UserView {
         }
     }
 
-    private List<Integer> convertInputWinningNumbersToOutput(String winningNumbers) {
-        String[] inputWinningNumbers = winningNumbers.split(",");
-
-        List<Integer> winningNumbersList = new ArrayList<>();
-
-        for (String inputWinningNumber : inputWinningNumbers) {
-            winningNumbersList.add(Integer.parseInt(inputWinningNumber));
-        }
-
-        return winningNumbersList;
-    }
-
-    private void validateBonusNumber(int bonus) {
-        if (bonus < 1 || bonus > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45까지의 숫자만 허용됩니다.");
-        }
-    }
 }
