c3acce9011bd881a6e823a27f4b68fb6829a57c6
Heejin Kim <gmlwls768@naver.com>
2024-11-01 04:38:57 +0900

    feat: 입력기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,20 @@
 package lotto.controller;
 
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
 
     private final OutputView outputView;
+    private final InputView inputView;
 
-    public LottoController(OutputView outputView) {
+    public LottoController(OutputView outputView, InputView inputView) {
         this.outputView = outputView;
+        this.inputView = inputView;
     }
 
     public void run() {
         outputView.printInputTotalCost();
-
+        String totalCost = inputView.input();
     }
 }
