4525e23e854020941c6c510812e6246b25acf1a1
dongyun <ehddbs452100@naver.com>
2024-10-31 12:43:46 +0900

    feat(Error, InputView, Lotto, Message): input winning lotto numbers
    - 당첨 로또 번호를 입력하여 ","로 분리 후 분리 한 값들이 정수인지 검증
    - 입력한 당첨 로또 번호를 Lotto 객체를 생성함과 동시에 검증
    - 입력한 당첨 로또 번호의 갯수가 6개가 아니라면 LOTTO_NUMBER_COUNT_IS_NOT_SIX 메시지를 전달하여 당첨
    로또 번호 재입력 받기
    - 입력한 당첨 로또 번호가 서로 중복이라면 LOTTO_NUMBER_IS_DUPLICATED 메시지를 전달하여 당첨 로또 번호
    재입력 받기
    - 입력한 당첨 로또 번호가 1과 46 사이가 아니라면 LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46 메시지를
    전달하여 당첨 로또 번호 재입력 받기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.global.Error.LOTTO_NUMBER_COUNT_IS_NOT_SIX;
+import static lotto.global.Error.LOTTO_NUMBER_IS_DUPLICATED;
+import static lotto.global.Error.LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46;
+
 import java.util.List;
 
 public class Lotto {
@@ -15,10 +19,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoSizeIsNotSix(numbers);
+        validateNumbersDuplicated(numbers);
+        validateNumberNotBetween1And46(numbers);
+    }
+
+    private void validateLottoSizeIsNotSix(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LOTTO_NUMBER_COUNT_IS_NOT_SIX.getErrorMsg());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumbersDuplicated(List<Integer> numbers) {
+        if (numbers.stream().count() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_IS_DUPLICATED.getErrorMsg());
+        }
+    }
+
+    private void validateNumberNotBetween1And46(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> notBetween1And46(number))) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_BETWEEN_1_AND_46.getErrorMsg());
+        }
+    }
+
+    private boolean notBetween1And46(Integer number) {
+        if (number < 1 || number > 46) {
+            return true;
+        }
+        return false;
+    }
 }
