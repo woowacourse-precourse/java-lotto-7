7035c98da57361b6008df8295e6d457dd7a4103a
JunwooPark <wnsdn1994@gmail.com>
2024-11-01 03:24:38 +0900

    fix(Rank): rank에 description 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -4,21 +4,24 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    MISS(0, false, 0);
+    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
+    MISS(0, false, 0, "꽝");
 
     private final int matchCount;
     private final boolean requiresBonus;
     private final int prize;
+    private final String description;
 
-    Rank(int matchCount, boolean requiresBonus, int prize) {
+
+    Rank(int matchCount, boolean requiresBonus, int prize, String description) {
         this.matchCount = matchCount;
         this.requiresBonus = requiresBonus;
         this.prize = prize;
+        this.description = description;
     }
 
     public static Rank getRank(int matchCount, boolean bonusMatch) {
@@ -30,5 +33,9 @@ public enum Rank {
 
     public int getPrize() {
         return prize;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,27 @@
-package lotto.view;
+package lotto.outputview;
+
+import java.util.List;
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.Rank;
 
 public class OutputView {
+
+    public void displayLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void displayResults(Map<Rank, Integer> resultMap) {
+        System.out.println("당첨 통계");
+        for (Rank rank : Rank.values()) {
+            System.out.println(rank.getPrize() + " - " + resultMap.get(rank) + "개");
+        }
+    }
+
+    public void displayEarningsRate(double earningsRate) {
+        System.out.println("총 수익률은 " + earningsRate + "%입니다.");
+    }
 }
