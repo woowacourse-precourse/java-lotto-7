e5d588452aa9d9ea4dfd3a4befb3c9889172f572
CheChe903 <cheche903@naver.com>
2024-10-31 14:42:23 +0900

    feat(LottoGeneratorTest): 객체에 맞게 의존성 주입
    객체에 맞게 의존성 주입으로 테스트를 구현합니다.

--- src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/lotto/LottoGeneratorTest.java
@@ -2,14 +2,29 @@ package lotto.domain.lotto;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import lotto.domain.generator.RandomIntegerListGenerator;
+import lotto.domain.generator.RandomNumberListGenerator;
+import lotto.domain.sorting.AscendingSorter;
+import lotto.domain.sorting.Sorter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoGeneratorTest {
 
+    private RandomNumberListGenerator randomNumberListGenerator;
+
+    private Sorter sorter;
+
+    @BeforeEach
+    void setUp() {
+        randomNumberListGenerator = new RandomIntegerListGenerator();
+        sorter = new AscendingSorter();
+    }
+
     @Test
     void 로또_생성시_로또는_NULL이_아니다() {
         //given
-        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoGenerator lottoGenerator = new LottoGenerator(randomNumberListGenerator, sorter);
 
         //when
         Lotto lotto = lottoGenerator.generate();
