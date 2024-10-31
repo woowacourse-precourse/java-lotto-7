993878152ab5c5677da7f84c9e5c0e70365fd017
dudxo <father8475@naver.com>
2024-10-31 23:04:32 +0900

    feat : 당첨번호 입력 View
    - 사용자로부터 당첨번호를 입력 받는다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요.";
 
     public String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MSG);
+        return Console.readLine();
+    }
+
+    public String inputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MSG);
         return Console.readLine();
     }
 }
