9bbbf59cbb5701d894b963c49df377192ae943ce
Jhzlo <kjh0010703@naver.com>
2024-11-01 01:15:00 +0900

    feat(view): 로또 구입 금액을 입력받는 문구 출력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readPriceInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void closeConsole() {
+        Console.close();
+    }
+}
