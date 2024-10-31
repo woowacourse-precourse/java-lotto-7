4f8ace6a2dfa88733d0d8fbe6c9ad67e73b4f50e
kungbi <kipper12335@gmail.com>
2024-10-31 14:02:47 +0900

    feat(Purchase): Purchase 관련 entity 생성

--- src/main/java/lotto/configuration/LottoConfiguration.java
+++ src/main/java/lotto/configuration/LottoConfiguration.java
@@ -4,6 +4,9 @@ public enum LottoConfiguration {
     LOTTO_MAX_NUMBER(45),
     LOTTO_MIN_NUMBER(1),
     LOTTO_NUMBER_COUNT(6),
+
+    LOTTO_PRICE(1000),
+    PURCHASE_LIMIT(100_000),
     ;
 
     private final int value;
---
+++ src/main/java/lotto/entity/Purchase.java
@@ -0,0 +1,33 @@
+package lotto.entity;
+
+import lotto.configuration.LottoConfiguration;
+
+public class Purchase {
+    private final int amount;
+
+    public Purchase(int amount) {
+        if (amount < LottoConfiguration.LOTTO_PRICE.getValue()) {
+            throw new IllegalArgumentException("로또 가격보다 적은 금액입니다.");
+        }
+        if (amount % LottoConfiguration.LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException("로또 가격은 1000원 단위로만 가능합니다.");
+        }
+        if (amount > LottoConfiguration.PURCHASE_LIMIT.getValue()) {
+            throw new IllegalArgumentException("로또는 한번에 10만원 까지 구입 가능합니다.");
+        }
+
+        this.amount = amount;
+    }
+
+
+    public int calculateTicketCount() {
+        return amount / LottoConfiguration.LOTTO_PRICE.getValue();
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+}
