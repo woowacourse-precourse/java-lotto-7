8d95dbc1c7779a176fcdc90ada2b74c150636985
parksomii <somi020605@gmail.com>
2024-11-01 02:17:30 +0900

    feat(LottoTickets): 로또 티켓 생성 기능 추가

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,13 +1,21 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoTickets {
     private final List<List<Integer>> lottoTickets;
 
     public LottoTickets(int ticket) {
-        this.lottoTickets = new ArrayList<>();
+        List<List<Integer>> lottoTickets = new ArrayList<>();
+        for (int count = 0; count < ticket; count++) {
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(numbers);
+            lottoTickets.add(numbers);
+        }
+        this.lottoTickets = lottoTickets;
     }
 
     public List<List<Integer>> getLottoTickets() {
