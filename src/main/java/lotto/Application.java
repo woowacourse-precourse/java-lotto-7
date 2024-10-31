f90153e43794822c22ba172ca9bd399bfd8cd704
mininiii <didals0521@gmail.com>
2024-10-31 14:22:23 +0900

    feat: Application 코드 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoNumberGenerator;
+import lotto.model.LottoRankCalculator;
+import lotto.model.ProfitCalculator;
+import lotto.util.StatisticsGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+        final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        final LottoRankCalculator lottoRankCalculator = new LottoRankCalculator();
+        final StatisticsGenerator statisticsGenerator = new StatisticsGenerator(lottoRankCalculator);
+        final ProfitCalculator profitCalculator = new ProfitCalculator();
+
+        final LottoController lottoController = new LottoController(inputView, outputView, lottoNumberGenerator,
+                lottoRankCalculator, statisticsGenerator, profitCalculator);
+
+        lottoController.run();
     }
 }
