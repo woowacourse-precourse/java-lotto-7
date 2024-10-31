645f1d883b7b60ca5fc6b3a6775ed13a84a6150d
kungbi <kipper12335@gmail.com>
2024-10-31 14:11:06 +0900

    refactor(config): 설정파일 validator 생성

--- src/main/java/lotto/configuration/LottoConfiguration.java
+++ src/main/java/lotto/configuration/LottoConfiguration.java
@@ -9,6 +9,18 @@ public enum LottoConfiguration {
     PURCHASE_LIMIT(100_000),
     ;
 
+    static {
+        if (LOTTO_MAX_NUMBER.getValue() < LOTTO_MIN_NUMBER.getValue()) {
+            throw new IllegalArgumentException("LOTTO_MAX_NUMBER는 LOTTO_MIN_NUMBER보다 작을 수 없습니다.");
+        }
+        if (LOTTO_NUMBER_COUNT.getValue() < 1) {
+            throw new IllegalArgumentException("LOTTO_NUMBER_COUNT는 1보다 작을 수 없습니다.");
+        }
+        if (LOTTO_PRICE.getValue() <= 0) {
+            throw new IllegalArgumentException("LOTTO_PRICE는 0이하의 값을 가질 수 없습니다.");
+        }
+    }
+
     private final int value;
 
     LottoConfiguration(int value) {
