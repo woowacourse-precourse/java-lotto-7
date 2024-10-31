f5e74a2088d079ff8e3fb531efc33341f6abc10e
Dev-LHY <dev.leehy@gmail.com>
2024-10-31 23:09:21 +0900

    feat(winningNumbers): getter 추가
    - winningNumbers getter 추가
    - bonusNumber getter 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -61,4 +61,12 @@ public class WinningNumbers {
             throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_BONUS_NUMBER.getMessage());
         }
     }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
