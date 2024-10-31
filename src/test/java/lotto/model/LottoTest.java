2c817480bd6c7c71aa24682853dfa0d83ca9d19a
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 19:56:21 +0900

    test(Lotto) : 로또 관련 테스트 코드 추가 - 당첨 번호와 일치하는 로또 번호의 개수 산정 기능과 로또 등수 확인 기능에 관한 테스트 코드 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,11 +1,17 @@
 package lotto.model;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoTest {
     @Test
@@ -21,5 +27,30 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("2등 로또는 당첨 번호와 일치하는 로또 번호가 5개이면서 보너스 번호를 포함한다.")
+    void testIfLottoRankSecond() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Integer bonusNum = 7;
+
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertTrue(lotto.isSecondRank(winningNumbers, bonusNum));
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호와 일치하는 로또 번호의 개수를 센다.")
+    @MethodSource("provideWinningNumbersAndExpected")
+    void countSameNumberTest(List<Integer> winningNumbers, Integer expected) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertEquals(lotto.countSameNumber(winningNumbers), expected);
+    }
+
+    private static Stream<Arguments> provideWinningNumbersAndExpected() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 7, 8, 9), 3),
+                Arguments.of(List.of(1, 2, 3, 4, 7 ,8), 4),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), 5),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6)
+        );
+    }
 }
