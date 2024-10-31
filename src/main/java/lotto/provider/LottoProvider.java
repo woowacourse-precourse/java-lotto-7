bac543bb16915c7af34e0c09d15175bf78d95b54
minjun <ijmtylhtf@naver.com>
2024-10-31 11:06:57 +0900

    refactor : 1000을 상수로 선언

--- src/main/java/lotto/provider/LottoProvider.java
+++ src/main/java/lotto/provider/LottoProvider.java
@@ -1,17 +1,18 @@
 package lotto.provider;
 
 public class LottoProvider {
+	private static final int THOUSAND = 1000;
 	private final int numberOfLottos;
 	private final int lottoPurchaseAmount;
 
 	public LottoProvider(int numberOfLottos, int lottoPurchaseAmount) {
 		validateLottoPurchaseAmount(lottoPurchaseAmount);
 		this.lottoPurchaseAmount = lottoPurchaseAmount;
-		this.numberOfLottos = lottoPurchaseAmount / 1000;
+		this.numberOfLottos = lottoPurchaseAmount / THOUSAND;
 	}
 
 	private void validateLottoPurchaseAmount(int lottoPurchaseAmount) {
-		if (lottoPurchaseAmount % 1000 != 0) {
+		if (lottoPurchaseAmount % THOUSAND != 0) {
 			throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력 받습니다.");
 		}
 	}
