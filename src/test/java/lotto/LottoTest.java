22f6dff3d1a064f245f30dae7bd720bca81d3fc1
dhrgodms <dhrgodms123@gmail.com>
2024-10-31 12:17:03 +0900

    test: 당첨_로또_설정 - 당첨 로또 입력과 검증 로직 추가 test
    - LottoTest: 예외 처리 시 출력되는 message를 확인할 수 있는 코드를 추가하였다.
    - ValidatorTest: LOTTO_04(구분자, 숫자 검증) 예외 처리를 테스트하는 코드를 추가하였다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Enum.ExceptionCode;
 import lotto.Model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest extends NsTest {
+    @DisplayName("EXCEPTION_LOTTO_01: 숫자가 6개가 아니면 예외 발생")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ExceptionCode.INVALID_NUMBERS_LENGTH.getMessage());
     }
 
-    @DisplayName("LOTTO_03: 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("EXCEPTION_LOTTO_03: 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ExceptionCode.DUPLICATED_NUMBER.getMessage());
     }
 
     @DisplayName("번호_정렬: List<Integer>로 받은 번호를 정렬한다.")
--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -9,17 +9,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ValidatorTest {
     Validator validator = new Validator();
 
-    @DisplayName("PRICE_01: 1000원 단위가 아니면 예외 발생")
+    @DisplayName("EXCEPTION_PRICE_01: 1000원 단위가 아니면 예외 발생")
     @Test
     void 가격이_1000단위가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> validator.validatePrice1000(45))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 구매 금액은 1000원 단위로만 입력 가능합니다.");
     }
 
-    @DisplayName("TYPE_01: 숫자가 아닌 값이 포함되어 있으면 예외 발생")
+    @DisplayName("EXCEPTION_TYPE_01: 숫자가 아닌 값이 포함되어 있으면 예외 발생")
     @Test
     void 숫자가_아닌_값이면_예외가_발생한다() {
         assertThatThrownBy(() -> validator.validateInteger("1000;"))
                 .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 숫자만 입력 가능합니다.");
     }
+
+    @DisplayName("EXCEPTION_LOTTO_04: 숫자와 구분자 외의 것이 포함되면 예외 발생")
+    @Test
+    void 숫자와_구분자가_아닌_값이면_예외가_발생한다() {
+        assertThatThrownBy(() -> validator.validateNumbersInput("1,2,3,4, 5;6"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 로또 번호는 숫자와 구분자(,)로만 이루어져야 합니다.");
+    }
+
 }
