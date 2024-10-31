b9f4cb6dd1317d5b55b96a6d002feec8eb17b310
hyosang <billy04100410@gmail.com>
2024-10-31 12:39:25 +0900

    test(validtaion): 당첨 번호 검증에 대한 failing test 작성

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,4 +18,48 @@ public class InputValidatorTest {
             .hasMessage("구입 금액은 1,000원 단위로만 가능합니다.");
     }
 
+    @Test
+    void 쉼표가_연속적으로_입력된_경우_예외를_발생() {
+        //given
+        String inputWinnigNumber = "1,,2,3,4,5,6";
+
+        //when & then
+        Assertions.assertThatThrownBy(
+                () -> InputValidator.validateInputWinnigNumber(inputWinnigNumber))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력에 쉼표가 연속적으로 입력되었습니다.");
+    }
+
+    @Test
+    void 입력_당첨_번호가_정상_범위_밖이면_예외를_발생() {
+        //given
+        List<Integer> winningNumbers = List.of(0, 1, 2, 3, 4, 5);
+        //when & then
+        Assertions.assertThatThrownBy(
+                () -> InputValidator.validateWinningNumberRange(winningNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("로또 번호의 숫자 범위는 1~45까지입니다.");
+    }
+
+    @Test
+    void 입력_당첨_번호가_중복되면_예외를_발생() {
+        //given
+        List<Integer> winningNumbers = List.of(1, 1, 2, 3, 4, 5);
+        //when & then
+        Assertions.assertThatThrownBy(
+                () -> InputValidator.validateWinningNumberDuplicate(winningNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력된 당첨 번호가 중복됩니다.");
+    }
+
+    @Test
+    void 입력_당첨_번호가_개수가_6개가_아니면_예외를_발생() {
+        //given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4);
+        //when & then
+        Assertions.assertThatThrownBy(
+                () -> InputValidator.validateWinningNumberCount(winningNumbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("당첨 번호 개수는 6개이어야 합니다.");
+    }
 }
