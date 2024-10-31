57ede2ae2c1ea566528f95b41d5c3b78d8dd462a
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-31 22:20:53 +0900

    test: WinningNumberValidator 테스트 생성

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,11 +3,16 @@ package lotto.exception;
 public enum ErrorMessage {
 
     ERROR_INPUT_EMPTY_OR_NULL("[ERROR] 사용자 입력이 입력되지 않았습니다."),
-    ERROR_BONUS_NUMBER_NOT_NUMERIC("[ERROR] 보너스 숫자는 오직 '숫자'만 입력 가능합니다."),
+    ERROR_BONUS_NUMBER_NOT_NUMERIC("[ERROR] 보너스 번호는 오직 '숫자'만 입력 가능합니다."),
+    ERROR_WINNING_NUMBER_NOT_NUMERIC("[ERROR] 당첨 번호는 구분자(,) 외에 오직 '숫자'만 입력 가능합니다."),
     ERROR_OUT_OF_RANGE("[ERROR] 당첨번호와 보너스번호는 1~45 사이의 숫자만 사용 가능합니다."),
     ERROR_BONUS_NUMBER_DUPLICATE("[ERROR] 당첨번호와 보너스번호는 중복되는 숫자가 존재해선 안됩니다."),
+    ERROR_WINNING_NUMBER_DUPLICATE("[ERROR] 당첨번호끼리 중복되는 숫자가 존재해선 안됩니다."),
     ERROR_AMOUNT_NOT_DIVISIBLE_BY_THOUSAND("[ERROR] 로또 구매 금액은 1000원단위로 지불 가능합니다."),
-   ;
+    ERROR_INPUT_NO_EMPTY_BETWEEN_DELIMITER("[ERROR] 구분자(,) 사이에는 숫자가 있어야 합니다"),
+    ERROR_WINNING_NUMBERS_NOT_SIX("[ERROR] 당첨 번호는 총 6개의 숫자이어야 합니다"),
+    ERROR_INPUT_DELIMITER_ONLY_COMMA("[ERROR] 구분자는 쉼표(,)만 사용 가능합니다"),
+    ;
 
     private final String message;
 
---
+++ src/test/java/lotto/validation/WinningNumberValidatorTest.java
@@ -0,0 +1,89 @@
+package lotto.validation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.exception.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class WinningNumberValidatorTest {
+
+    @DisplayName("구분자와 숫자 이외의 문자 입력시 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "b", "+", "ㅋ", ":"})
+    void 구분자와_숫자_이외의_문자_예외(String input){
+        Assertions.assertThatThrownBy(()->WinningNumberValidator.validateOnlyNumeric(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_WINNING_NUMBER_NOT_NUMERIC.toString());
+    }
+
+
+    @DisplayName("구분자 사이가 비어있으면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1,,3,42,13,34,41",
+            "1,2,3,4,5,6,",
+            ",1,2,3,4,5,6",
+            "1, 2, 3, 4, , 5, 6"
+    })
+    void 구분자_사이_없음_예외(String input){
+        Assertions.assertThatThrownBy(()->WinningNumberValidator.validateBetweenDelimiter(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_INPUT_NO_EMPTY_BETWEEN_DELIMITER.toString());
+    }
+
+    @DisplayName("당첨번호가 6개가 아닌 경우 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @MethodSource("provideWinningNumbersList")
+    void 당첨번호_6개_아님_예외(List<Integer> winningNumbers){
+        Assertions.assertThatThrownBy(() -> WinningNumberValidator.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_WINNING_NUMBERS_NOT_SIX.toString());
+    }
+
+    static Stream<Arguments> provideWinningNumbersList(){
+        return Stream.of(
+                Arguments.of(Arrays.asList(1,2,3,4,5,6,7)),
+                Arguments.of(Arrays.asList(1,2,3,4,5)),
+                Arguments.of(Arrays.asList(1,4,5))
+        );
+    }
+
+    @DisplayName("구분자가 쉼표가 아니면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1:2,3:4:5:6",
+            "1,2,3:4,5,6",
+            "1/2/3/4/5/6"
+    })
+    void 구분자_쉼표_아님_예외(String input){
+        Assertions.assertThatThrownBy(()->WinningNumberValidator.validateDelimiter(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_INPUT_DELIMITER_ONLY_COMMA.toString());
+    }
+
+    @DisplayName("당첨 번호 중 중복된 숫자가 있으면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {1,31,41,29,16})
+    void 당첨_번호_숫자_중복_예외(int winningNumber){
+        List<Integer> winningNumbers = List.of(1,31,41,29,16); // 리스트에 5개의 당첨번호가 들어가있고, 마지막 당첨번호를 넣는 차례라 가정
+        Assertions.assertThatThrownBy(() -> WinningNumberValidator.validateWinningNumberrDuplicate(winningNumbers,winningNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_WINNING_NUMBER_DUPLICATE.toString());
+    }
+
+    @DisplayName("당첨 번호가 1~45 사이의 값이 아니면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, 100, 67})
+    void 숫자_범위_벗어남_예외(int winningNumber){
+        Assertions.assertThatThrownBy(() -> WinningNumberValidator.validateNumberRange(winningNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_OUT_OF_RANGE.toString());
+    }
+
+}
