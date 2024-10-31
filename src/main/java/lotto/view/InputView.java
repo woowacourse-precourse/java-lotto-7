e354223d345ca74def5081a6325516ff317a5be2
J0YERIM <chosarah70@gmail.com>
2024-11-01 02:39:12 +0900

    refactor(view): InputView의 입력 유효성 검증과 재입력 로직을 책임별로 분리
    - validatePurchaseAmount 메서드를 InputValidator로 이동하여 형식 검사를 담당하는 책임을 분리함
    - 재입력 요청 로직은 Controller로 옮겨 책임을 분리함

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,23 +6,8 @@ public class InputView {
 
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return processPurchaseAmount();
-    }
-
-    private static int processPurchaseAmount() {
-        try {
-            String amount = Console.readLine().trim();
-            validatePurchaseAmount(amount);
-            return Integer.parseInt(amount);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return inputPurchaseAmount();
-        }
-    }
-
-    private static void validatePurchaseAmount(String amount) {
-        InputValidator.validateNotBlank(amount);
-        InputValidator.validateInteger(amount);
-        InputValidator.validateThousandUnit(Integer.parseInt(amount));
+        String amount = Console.readLine().trim();
+        InputValidator.validatePurchaseAmount(amount);
+        return Integer.parseInt(amount);
     }
 }
