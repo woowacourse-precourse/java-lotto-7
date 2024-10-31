5bbb7599f9ae53fadd5ec13a550e5df64fa58f0a
dnjswns98 <tngkrt2004@gmail.com>
2024-10-31 20:32:49 +0900

    feat(InputView): Add purchase amount exception handling
    숫자 형식이 아닌 입력시 예외 처리
    
    실수를 입력했다면 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,37 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.CustomLottoException;
+import lotto.exception.ErrorMessage;
 
 public class InputView {
-    public static String inputLottoAmount(){
-        return Console.readLine();
+    public static int inputLottoAmount(){
+        String inputAmount = Console.readLine();
+        return convertInputAmountToInt(inputAmount);
+    }
+
+    private static int convertInputAmountToInt(String inputAmount) {
+        try {
+            int intAmount = Integer.parseInt(inputAmount);
+            long longAmount = convertInputAmountToLong(inputAmount);
+            validateAmountEquality(intAmount, longAmount);
+            return intAmount;
+        } catch (NumberFormatException exception) {
+            throw new CustomLottoException(ErrorMessage.NOT_NUMBER);
+        }
+    }
+
+    private static long convertInputAmountToLong(String inputAmount) {
+        try {
+            return Long.parseLong(inputAmount);
+        } catch (NumberFormatException exception) {
+            throw new CustomLottoException(ErrorMessage.NOT_NUMBER);
+        }
+    }
+
+    private static void validateAmountEquality(int intAmount, long longAmount) {
+        if (intAmount != longAmount) {
+            throw new CustomLottoException(ErrorMessage.NOT_INTEGER_NUMBER);
+        }
     }
 }
