e128d070e33b9b5faf29fe683b253b37514a30bf
IM-GYURI <kgulr0517@gmail.com>
2024-11-01 02:11:54 +0900

    feat : ErrorView 추가
    - 에러 발생 시 출력을 담당
    
    printMoneyParsingError()
    - String에서 int로 파싱 시 발생하는 오류에 대한 출력
    
    printErrorMessage()
    - 에러 메세지를 받아 출력

--- src/main/java/lotto/view/ErrorView.java
+++ src/main/java/lotto/view/ErrorView.java
@@ -3,9 +3,13 @@ package lotto.view;
 public class ErrorView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String MONEY_PARSING_ERROR_MESSAGE = "금액은 숫자로 입력해야 합니다.";
+    private static final String MONEY_PARSING_ERROR_MESSAGE = "구입 금액은 2,147,483,000원 이하의 숫자여야 합니다.";
 
-    public void printMoneyInputError() {
+    public void printMoneyParsingError() {
         System.out.println(ERROR_PREFIX + MONEY_PARSING_ERROR_MESSAGE);
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(ERROR_PREFIX + errorMessage);
     }
 }
