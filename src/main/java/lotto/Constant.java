48ab27368ec6e5ea03297fcfbe4cf3be139b5935
sleepyhoon <pokjm99@gmail.com>
2024-10-31 17:53:49 +0900

    feat (Constant) : 상수 값 한 번에 모아 둠

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -1,9 +1,10 @@
 package lotto;
 
-public class MoneyUtil {
+public class Constant {
     static final int LOTTO_PRICE = 1000;
     static final int MINIMUM_PRICE = 0;
+    static final String COMMA = ",";
 
-    private MoneyUtil() {
+    private Constant() {
     }
 }
