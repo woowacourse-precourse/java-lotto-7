76cb856bb2953fa171e55732a8a72c12d483ef2b
posite <su84053@gmail.com>
2024-10-31 12:49:09 +0900

    chore(UserInputValdiator): 매직넘버 제거
    검증에 사용한 매직 넘버를 제거하였습니다.

--- src/main/java/lotto/util/UserInputValidator.java
+++ src/main/java/lotto/util/UserInputValidator.java
@@ -6,6 +6,10 @@ import static lotto.constant.ErrorMessage.NOT_PURCHASE_AMOUNT_FORMAT;
 import static lotto.constant.ErrorMessage.TOO_BIG_PURCHASE_AMOUNT;
 
 public class UserInputValidator {
+    private static final int PURCHASE_AMOUNT_UNIT = 1000;
+    private static final int PURCHASE_AMOUNT_LENGTH_LIMIT = 10;
+    private static final int PURCHASE_AMOUNT_REMAIN = 0;
+
     public void isValidPurchaseAmount(String purchaseAmount) {
         isUserInputNumber(purchaseAmount);
         isPurchaseAmountTooBing(purchaseAmount);
@@ -20,19 +24,19 @@ public class UserInputValidator {
     }
 
     public void isPurchaseAmountTooBing(String purchaseAmount) {
-        if (purchaseAmount.length() > 10) {
+        if (purchaseAmount.length() > PURCHASE_AMOUNT_LENGTH_LIMIT) {
             throw new IllegalArgumentException(TOO_BIG_PURCHASE_AMOUNT.getMessage());
         }
     }
 
     public void isPurchaseAmountLessThanThousand(String purchaseAmount) {
-        if (Integer.parseInt(purchaseAmount) < 1000) {
+        if (Integer.parseInt(purchaseAmount) < PURCHASE_AMOUNT_UNIT) {
             throw new IllegalArgumentException(LESS_THAN_THOUSAND_PURCHASE_AMOUNT.getMessage());
         }
     }
 
     public void isPurchaseAmountMultipleOfThousand(String purchaseAmount) {
-        if (Integer.parseInt(purchaseAmount) % 1000 != 0) {
+        if (Integer.parseInt(purchaseAmount) % PURCHASE_AMOUNT_UNIT != PURCHASE_AMOUNT_REMAIN) {
             throw new IllegalArgumentException(NOT_PURCHASE_AMOUNT_FORMAT.getMessage());
         }
     }
