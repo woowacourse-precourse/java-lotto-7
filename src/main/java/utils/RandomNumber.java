f3f02b1850849cc8fe245609392879a535066564
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-01 02:04:26 +0900

    feat: 6자리의 랜덤번호 생성
    중복되는 값이 있을 경우를 검증하며, 6자리의 랜덤번호를 생성하는 정적 팩토리 메서드를 만들었음

---
+++ src/main/java/utils/RandomNumber.java
@@ -0,0 +1,34 @@
+package utils;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomNumber {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_LENGTH = 6;
+
+    private RandomNumber() {
+
+    }
+
+    public static List<Integer> create() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() != LOTTO_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            verifyDuplicateRandomNumber(randomNumbers, randomNumber);
+        }
+        return Collections.unmodifiableList(randomNumbers);
+    }
+
+    private static void verifyDuplicateRandomNumber(List<Integer> randomNumbers, int randomNumber) {
+        if (randomNumbers.contains(randomNumber)) {
+            return;
+        }
+        randomNumbers.add(randomNumber);
+    }
+}
