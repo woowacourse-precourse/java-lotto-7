31f1defa669c47fd9bc283e655a842c783c056f8
cherryiJuice <chldlwn711@gmail.com>
2024-10-31 16:51:52 +0900

    refactor: 네이밍 변경

--- src/main/java/lotto/exception/ErrorType.java
+++ src/main/java/lotto/exception/ErrorType.java
@@ -3,7 +3,7 @@ package lotto.exception;
 public enum ErrorType {
     INVALID_PURCHASE_PRICE("원 단위로 입력해주세요."),
     INVALID_BONUS_NUM("당첨 번호에 없는 보너스 번호를 입력해주세요."),
-    INVALID_INPUT_FORMAT("숫자를 입력해주세요."),
+    INVALID_NUMBER_FORMAT("숫자를 입력해주세요."),
     DUPLICATION_NUM("당첨 숫자는 중복될 수 없습니다. 다시 입력해주세요."),
     INSUFFICIENT_OR_EXCESSIVE_NUMBERS("당첨 숫자는 %d개를 입력해주세요."),
     OUT_OF_RANGE_NUMBER("당첨 숫자는 %d~%d 범위로 입력해주세요");
