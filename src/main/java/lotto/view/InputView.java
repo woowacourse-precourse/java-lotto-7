882f0e05061408730adff9b632f1b562f7fbe8a6
eb9862 <luclead622@gmail.com>
2024-10-31 16:04:46 +0900

    fix: 당첨 번호 입력 메시지 출력 시 개행 추가
    - InputView 클래스에 상수로 선언하여 사용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_BUDGET_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String LINE_BREAK = "\n";
 
     public static String readBudget() {
         System.out.println(INPUT_BUDGET_MESSAGE);
@@ -13,7 +14,7 @@ public class InputView {
     }
 
     public static String readWinningNumbers() {
-        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        System.out.println(LINE_BREAK + INPUT_WINNING_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 }
