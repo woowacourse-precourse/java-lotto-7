f1616c2d542bd7e5e625e7126e423d39c4c4e2c4
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-31 19:27:21 +0900

    feat : 당첨 번호 입력 View 1차 구현

---
+++ src/main/java/lotto/view/PrizeNumbersView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class PrizeNumbersView {
+
+    public void printPrizeNumberInputView() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printBonusNumberInputView() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
