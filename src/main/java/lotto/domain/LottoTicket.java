a5237ba2e47818ce794ab9603ceb109730c3efd2
sinminseok12 <sinminseok12@naver.com>
2024-11-01 01:24:42 +0900

    feat(LottoTicket) : 모든 로또 당첨 정보 조회 기능 구현
    로또 티켓(LottoTicket) 에 있는 모든 로또들의 당첨 결과를 조회하는 기능을 구현했다.

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
+import lotto.constants.Ranking;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
     private final List<Lotto> lottos;
 
     public LottoTicket(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public List<Ranking> checkRanking(WinningLotto winningLotto){
+        return lottos.stream()
+                .map(lotto -> lotto.checkRanking(winningLotto))
+                .collect(Collectors.toList());
     }
 
     public List<Lotto> getLottos() {
