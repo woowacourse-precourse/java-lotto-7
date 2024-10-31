d0aa8da7eb15f852303c4764ff7fe7fad28df4fe
corpi <lj845854@gmail.com>
2024-10-31 18:13:10 +0900

    feat(LottoStatistic): 당첨 통계와 수익률을 계산한다.

--- src/main/java/lotto/model/LottoStatistic.java
+++ src/main/java/lotto/model/LottoStatistic.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class LottoStatistic {
     private final static int INIT_COUNT = 0;
@@ -10,6 +11,8 @@ public class LottoStatistic {
 
     public LottoStatistic(List<LottoRank> rankList) {
         initResult();
+        calculateResult(rankList);
+        calculateProfitRatio();
     }
 
     public HashMap<LottoRank, Integer> getWinningResult() {
@@ -24,5 +27,26 @@ public class LottoStatistic {
         for (LottoRank lottoRank : LottoRank.values()) {
             winningResult.put(lottoRank, INIT_COUNT);
         }
+    }
+
+    private void calculateResult(List<LottoRank> rankList) {
+        for (LottoRank lottoRank : rankList) {
+            winningResult.put(lottoRank, winningResult.getOrDefault(lottoRank, INIT_COUNT) + 1);
+        }
+    }
+
+    private void calculateProfitRatio() {
+        double totalPrizeMoney = 0;
+        double totalLottoTimes = 0;
+
+        for (Entry<LottoRank, Integer> lottoRankIntegerEntry : winningResult.entrySet()) {
+            Integer winCount = lottoRankIntegerEntry.getValue();
+            int prizeMoney = lottoRankIntegerEntry.getKey().prizeMoney;
+
+            totalLottoTimes += winCount;
+            totalPrizeMoney += prizeMoney * winCount;
+        }
+
+        this.profitRatio = totalPrizeMoney / (totalLottoTimes * 1000) * 100;
     }
 }
