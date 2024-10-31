e3a47e716dbdde33afe015bae587d9c12810312d
StoneCAU <seokwoo1999@naver.com>
2024-11-01 01:44:39 +0900

    feat(WinningNumbers, BuyerService): WinningNumber 객체 구현
    - WinningNumbers: 로또 번호에 대한 검증 후 생성
    - BuyerService: WinningNumbers 객체 생성 로직 추가

--- src/main/java/lotto/service/BuyerService.java
+++ src/main/java/lotto/service/BuyerService.java
@@ -3,6 +3,7 @@ package lotto.service;
 import static lotto.constant.LottoGameRule.LOTTO_COST;
 
 import lotto.domain.Money;
+import lotto.domain.WinningNumbers;
 
 public class BuyerService {
     public int calculateLottoQuantity(String input) {
@@ -10,6 +11,10 @@ public class BuyerService {
         int price = money.getPrice();
 
         return price / LOTTO_COST.getValue();
+    }
+
+    public WinningNumbers createWinningNumbers(String input) {
+        return new WinningNumbers(input);
     }
 
     private Money createMoney(String input) {
