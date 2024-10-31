e238debcf99455f9784a7342ad36c05207475765
woo <ldw28517@naver.com>
2024-10-31 20:09:20 +0900

    feat: 중복된 로또 번호는 존재할 수 없다
    - 로또 검증 단계에서 중복된 번호를 확인하는 과정 추가
    - 에러 메시지도 추가

--- src/main/java/lotto/error/LottoErrorMessage.java
+++ src/main/java/lotto/error/LottoErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.error;
 
 public enum LottoErrorMessage implements ErrorMessage {
-    UNMATCHED_SIZE("로또 번호는 6개여야 합니다.");
+    UNMATCHED_SIZE("로또 번호는 6개여야 합니다."),
+    DUPLICATED_NUMBER("중복된 번호는 사용할 수 없습니다.");
 
     private final String message;
 
