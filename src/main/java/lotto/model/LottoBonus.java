20c637df9c1ce22954b0655853e9b35287365a93
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-10-31 15:40:01 +0900

    feat(model/LottoBonus): 보너스 번호 유효성 검사 추가
    - 보너스 번호가 1에서 45 사이의 숫자인지 검증하는 validateRange 메서드 추가

--- src/main/java/lotto/model/LottoBonus.java
+++ src/main/java/lotto/model/LottoBonus.java
@@ -4,6 +4,13 @@ public class LottoBonus {
 	private final int bonusNumber;
 
 	public LottoBonus(int bonusNumber) {
+		validate(bonusNumber);
 		this.bonusNumber = bonusNumber;
+	}
+
+	private void validate(int bonusNumber) {
+		if (bonusNumber < 1 || bonusNumber > 45) {
+			throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+		}
 	}
 }
