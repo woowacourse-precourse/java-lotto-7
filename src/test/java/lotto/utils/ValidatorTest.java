68768e190d98a558a5b4cc9b5cfc78fd4d0e2483
heehun <slrkqk2@naver.com>
2024-10-31 15:02:25 +0900

    test(Validator): priceValidator 메서드 테스트 작성

---
+++ src/test/java/lotto/utils/ValidatorTest.java
@@ -0,0 +1,19 @@
+package lotto.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+
+    @DisplayName("priceValidator 메서드 테스트_01")
+    @Test
+    void 천원_단위_입력이_아니면_예외가_발생한다() {
+        String inputPrice = "11100";
+
+        Assertions.assertThatThrownBy(()->Validator.priceValidator(inputPrice))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
