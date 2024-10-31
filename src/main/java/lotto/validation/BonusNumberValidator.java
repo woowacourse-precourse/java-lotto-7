51a9c6ec651d154c1ef0cd673bcb08a5520f777a
변우영 <likevangogh24@gmail.com>
2024-10-31 10:11:01 +0900

    refactor(BonusNumberValidator): 에외 메시지 접두사 추가

--- src/main/java/lotto/validation/BonusNumberValidator.java
+++ src/main/java/lotto/validation/BonusNumberValidator.java
@@ -11,7 +11,7 @@ public class BonusNumberValidator {
     public static void validateRange(String input) {
         int bonusNum = Convertor.convertToInt(input);
         if (bonusNum > MAX_NUMBER_RANGE || bonusNum < MIN_NUMBER_RANGE) {
-            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 숫자를 입력해야 합니다.");
+            throw new IllegalArgumentException(ERROR_PREFIX + "보너스 번호는 1~45 사이의 숫자를 입력해야 합니다.");
         }
     }
 }
