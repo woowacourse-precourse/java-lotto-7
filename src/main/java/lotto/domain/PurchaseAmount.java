1098a551edccb10d46973b9684a196b597407a9c
taek2222 <oot309@daum.net>
2024-10-31 20:00:05 +0900

    style(PurchaseAmount): 구입금액 객체 클래스 명 변경
    - 명칭의 모호함으로 Amount -> PurchaseAmount 변경했습니다.

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,25 +1,31 @@
 package lotto.domain;
 
-public class Amount {
+public class PurchaseAmount {
 
     public static final int LOTTO_PRICE = 1000;
     public static final int ZERO = 0;
 
-    private Integer amount;
+    private final Integer amount;
 
-    public Amount(Integer amount) {
+    public PurchaseAmount(Integer amount) {
         validateIsMultipleOfLottoPrice(amount);
         validateIsNotZero(amount);
         this.amount = amount;
     }
 
+    public Integer getAmount() {
+        return amount;
+    }
+
     private void validateIsMultipleOfLottoPrice(int amount) {
-        if (amount % LOTTO_PRICE != ZERO)
+        if (amount % LOTTO_PRICE != ZERO) {
             throw new IllegalArgumentException();
+        }
     }
 
     private void validateIsNotZero(int amount) {
-        if (amount == ZERO)
+        if (amount == ZERO) {
             throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,31 +1,31 @@
 package lotto.domain;
 
-import static lotto.domain.Amount.ZERO;
+import static lotto.domain.PurchaseAmount.ZERO;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class AmountTest {
+class PurchaseAmountTest {
 
     @ParameterizedTest(name = "테스트 금액 : [{arguments}]")
     @ValueSource(ints = {1000, 2000, 5000})
     void 유효한_금액_입력은_예외가_발생하지_않는다(int amount) {
-        assertThatCode(() -> new Amount(amount))
+        assertThatCode(() -> new PurchaseAmount(amount))
                 .doesNotThrowAnyException();
     }
 
     @Test
     void 구입금액이_0원인_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new Amount(ZERO))
+        assertThatThrownBy(() -> new PurchaseAmount(ZERO))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest(name = "테스트 금액 : [{arguments}]")
     @ValueSource(ints = {123, 1234, 12345})
     void 가격_단위가_아닌_금액은_예외가_발생한다(int amount) {
-        assertThatThrownBy(() -> new Amount(amount))
+        assertThatThrownBy(() -> new PurchaseAmount(amount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
