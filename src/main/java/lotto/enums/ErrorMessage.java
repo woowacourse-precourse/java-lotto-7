ad97aa43d60312b40148619fa931c5c9c62e5c0b
Kim Kyeongho <sgn07124@gmail.com>
2024-10-31 23:47:58 +0900

    feat(ErrorMessage): 당첨번호 입력 예외처리 관련 에러메시지 enum 클래스에 추가
    - 당첨번호 입력 예외처리 관련 에러메시지 enum 클래스에 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -6,7 +6,15 @@ public enum ErrorMessage {
     INPUT_ZERO("0을 입력하였습니다."),
 
     PRICE_UNDER_THOUSAND("1000원 이상의 금액을 입력해야 됩니다."),
-    PRICE_NOT_DIVIDE_THOUSAND("1000원 단위로 입력해야 됩니다.");
+    PRICE_NOT_DIVIDE_THOUSAND("1000원 단위로 입력해야 됩니다."),
+
+    SEPARATOR_NOT_COMMA("구분자는 쉼표만 가능합니다."),
+    NOT_ALLOW_SEPARATOR_CONTINUE("쉼표는 2개 이상 연속적으로 입력될 수 없습니다."),
+    NOT_ALLOW_NUMBER_BLANK("쉼표로 구분된 수가 공백일 수는 없습니다."),
+    NOT_INPUT_SIX_NUMBERS("6개의 숫자를 입력해주세요."),
+    NOT_ALLOW_WITHOUT_INTEGER("정수를 입력해주세요."),
+    NOT_ALLOW_DUPLICATE_NUMBER("숫자는 중복을 허용하지 않습니다."),
+    ALLOW_ONE_TO_FORTY_FIVE("입력된 수에 1~45 범위의 수가 포함되어 있습니다."),;
 
     private final String errorMessage;
 
