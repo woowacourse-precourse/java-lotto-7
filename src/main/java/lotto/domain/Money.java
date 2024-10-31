aef3722e7224d516c7247b528db3308a1ab870c8
openmpy <suhwan@kakao.com>
2024-10-31 16:35:17 +0900

    refactor: Money 클래스 null 값, 단위 validation 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 import static lotto.constants.LottoConstants.LOTTO_PRICE;
-import static lotto.exception.constants.ErrorMessage.INVALID_INPUT_TEXT;
+import static lotto.exception.constants.ErrorMessage.INVALID_NUMBER;
 import static lotto.exception.constants.ErrorMessage.LOTTO_PURCHASE_MONEY_LESS_THAN_1000;
 import static lotto.exception.constants.ErrorMessage.LOTTO_PURCHASE_MONEY_UNIT_NOT_1000;
 
@@ -14,18 +14,26 @@ public class Money {
     private final Integer amount;
 
     public Money(final Integer amount) {
+        validateNull(amount);
         validateAmount(amount);
+        validateUnit(amount);
 
         this.amount = amount;
     }
 
-    private void validateAmount(final Integer amount) {
+    private void validateNull(final Integer amount) {
         if (amount == null) {
-            throw new LottoException(INVALID_INPUT_TEXT.getMessage());
+            throw new LottoException(INVALID_NUMBER.getMessage());
         }
+    }
+
+    private void validateAmount(final Integer amount) {
         if (amount < LOTTO_PRICE.getValue()) {
             throw new LottoException(LOTTO_PURCHASE_MONEY_LESS_THAN_1000.getMessage());
         }
+    }
+
+    private void validateUnit(final Integer amount) {
         if (amount % LOTTO_PRICE.getValue() != INT_ZERO) {
             throw new LottoException(LOTTO_PURCHASE_MONEY_UNIT_NOT_1000.getMessage());
         }
