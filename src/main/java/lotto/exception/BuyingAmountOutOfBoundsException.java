15d5663dcb8905b4028cc7de065055d719a1fb7f
making-a-scene <4936joo@naver.com>
2024-10-31 15:03:50 +0900

    refactor: 구입 금액 관련 예외 처리 로직 리팩토링 로또 구입 금액 관련 예외인 경우는 BuyingAmountException이라는 커스텀 상위 클래스를 상속받도록 함 BuyingAmountException은 IllegalArgumentException의 하위 클래스"

--- src/main/java/lotto/exception/BuyingAmountOutOfBoundsException.java
+++ src/main/java/lotto/exception/BuyingAmountOutOfBoundsException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
-public class BuyingAmountOutOfBoundsException extends CustomException {
-    private static final String MESSAGE = "구입 금액이 허용 범위를 벗어납니다. 1000원 이상 10만원 이하로만 구입 가능합니다.";
+public class BuyingAmountOutOfBoundsException extends BuyingAmountException {
+    private static final String MESSAGE = "1000원 이상 10만원 이하로만 구입 가능합니다.";
 
     public BuyingAmountOutOfBoundsException() {
         super(MESSAGE);
--- src/main/java/lotto/exception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package lotto.exception;
-
-public class CustomException extends IllegalArgumentException {
-    private static final String HEADER = "[ERROR] ";
-
-    public CustomException(String message) {
-        super(HEADER + message);
-    }
-}
