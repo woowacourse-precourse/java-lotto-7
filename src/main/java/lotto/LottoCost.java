cccc8495527ff29fee761b02341a44fe6c800af8
mete0rfish <sungwon326@naver.com>
2024-10-31 15:59:28 +0900

    feat(domain): LottoCost 유효성 검사 추가
    - 양수인지, 금액 범위, 1000의 배수에 부합하는지 테스트

--- src/main/java/lotto/LottoCost.java
+++ src/main/java/lotto/LottoCost.java
@@ -4,16 +4,32 @@ public class LottoCost {
 
     private final int LOTTO_COST_MAX = 100000;
     private final int LOTTO_COST_MIN = 1000;
+    private final int LOTTO_COST_UNIT = 1000;
 
     private int cost;
 
     public LottoCost(int cost) {
-        this.cost = cost;
+        validateRange(cost);
+        validateUnit(cost);
     }
 
-    private void validateCost(int cost) {
-        if(cost < LOTTO_COST_MIN || cost > LOTTO_COST_MAX) {
+    public static LottoCost valueOf(String input) {
+        try {
+            return new LottoCost(Integer.parseInt(input));
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액은 양수이어야 합니다.");
+        }
+    }
+
+    private void validateRange(int cost) {
+        if (cost < LOTTO_COST_MIN || cost > LOTTO_COST_MAX) {
             throw new IllegalArgumentException("[ERROR] 금액은 1000원 이상, 100000 이하만 가능합니다.");
+        }
+    }
+
+    private void validateUnit(int cost) {
+        if (cost % LOTTO_COST_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000의 배수만 가능합니다.");
         }
     }
 }
