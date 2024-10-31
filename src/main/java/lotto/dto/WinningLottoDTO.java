5f29b3997c962f33c00bf0f653ce7bbcc85519bd
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-10-31 15:23:09 +0900

    style(WinningLottoDTO) : 필드 이름 수정 - 필드 winningLotto를 winningNumbers로 변경

--- src/main/java/lotto/dto/WinningLottoDTO.java
+++ src/main/java/lotto/dto/WinningLottoDTO.java
@@ -7,16 +7,16 @@ import lotto.model.Lotto;
 
 public class WinningLottoDTO {
 
-    private final Lotto winningLotto;
+    private final Lotto winningNumbers;
     private final Integer bonusNumber;
 
-    public WinningLottoDTO(Lotto winningLotto, Integer bonusNumber) {
-        this.winningLotto = winningLotto;
-        this.bonusNumber = validateDuplicated(winningLotto, bonusNumber);
+    public WinningLottoDTO(Lotto winningNumbers, Integer bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = validateDuplicated(winningNumbers, bonusNumber);
     }
 
     public List<Integer> getWinningNumber() {
-        return winningLotto.getNumbers();
+        return winningNumbers.getNumbers();
     }
 
     public Integer getBonusNumber() {
