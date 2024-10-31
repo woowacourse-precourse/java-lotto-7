022b4e8dddc75850a1f77d1bff3933883ddd885f
lejuho <zedd1324@gmail.com>
2024-10-31 09:29:40 +0900

    feat(ui) : InputManager 구현
    - 빈 입력을 검사하는 validateEmptyAndReturnInput 추가
    - 금액 입력 시 숫자가 아닌 입력을 검사하는 validateMoney 추가
    - 보너스 숫자 입력 시 숫자가 맞는지와 1 이상 45 이하인지를 검사하는
      validateBonusNUmber 추가
    - 당첨 숫자 6개 입력 시 형식에 맞는지를 검사하는
      validateAndReturnNumbers 추가
    - getPrizeTier, increaseFive, getBonusNumber, numbersIntoLotto 삭제
    - 일부 클래스 필드 삭제

--- src/main/java/lotto/ui/InputManager.java
+++ src/main/java/lotto/ui/InputManager.java
@@ -1,4 +1,63 @@
 package lotto.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputManager {
+    public String validateEmptyAndReturnInput(){
+        String input = Console.readLine();
+            if (input.isEmpty()) {
+                System.out.println("[ERROR] 빈 입력은 허용되지 않습니다.");
+                throw new IllegalArgumentException();
+            }
+
+        return input;
+    }
+
+    public String validateAndReturnInt(String input){
+            for (char c : input.toCharArray()) {
+                if ((int) c < 48 || (int) c > 57) {
+                    System.out.println("[ERROR] 숫자가 아닌 입력은 허용되지 않습니다");
+                    throw new IllegalArgumentException();
+                }
+            }
+        return input;
+    }
+
+    public void notContainComma(String input) {
+            if (!input.contains(",")) {
+                System.out.println("[ERROR] 쉼표로 구분된 6개의 숫자를 넣어주세요");
+                throw new IllegalArgumentException();
+            }
+    }
+
+    public void notSixNumbers(String input) {
+            String[] parts = input.split(",");
+            if (parts.length != 6) {
+                System.out.println("[ERROR] 쉼표로 구분된 6개의 숫자를 넣어주세요");
+                throw new IllegalArgumentException();
+            }
+    }
+
+    public void isAllNumber(String[] parts,List<Integer> numbers) {
+            for (String part : parts) {
+                part = part.trim();
+                if (!part.matches("\\d+")) {
+                    System.out.println("[ERROR] 쉼표로 구분된 6개의 숫자를 넣어주세요");
+                    throw new IllegalArgumentException();
+                }
+                numbers.add(Integer.parseInt(part));
+            }
+    }
+
+    public List<Integer> validateAndReturnNumbersInput(String input) {
+        notContainComma(input);
+        notSixNumbers(input);
+        String[] parts = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        isAllNumber(parts, numbers);
+        return numbers;
+    }
 }
