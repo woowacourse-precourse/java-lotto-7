d0151617e10acf0bb9ed379ae28cfd9f2b0215d2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-31 18:35:03 +0900

    refactor : 당첨 통계 출력 printer 생성

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,11 +1,8 @@
 package lotto.view;
 
-import java.util.Map;
-import lotto.domain.Rank;
 import lotto.domain.Wallet;
-import lotto.domain.lottos.Lotto;
 import lotto.domain.lottos.RandomLottos;
-import lotto.domain.lottos.user.WinningLottos;
+import lotto.domain.lottos.user.WinningLotto;
 
 public class Output {
     private final static String PRINT_RANK_FORMAT = "%d개 일치 (%s원) - %d개\n";
@@ -20,42 +17,17 @@ public class Output {
     public static void printPurchasedLottoList(Wallet wallet, RandomLottos randomLottos) {
         System.out.println();
         System.out.printf("%s개를 구매했습니다.\n", wallet);
-        for (Lotto lotto : randomLottos.getLottos()) {
-            System.out.println(lotto);
-        }
+        System.out.println(randomLottos);
     }
 
-    public static void printLottoWinningStatistics(WinningLottos winningLottos) {
+    public static void printLottoWinningStatistics(WinningLotto winningLottos) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        StringBuilder printout = new StringBuilder();
-
-        for (Map.Entry<Rank, Integer> entry : winningLottos.getWinningStatistics().entrySet()) {
-            Rank rank = entry.getKey();
-            if (rank.equals(Rank.NOTHING)) {
-                continue;
-            }
-
-            int matchCount = rank.getMatchCount();
-            String formatedPrizedMoney = String.format("%,d", rank.getPrizeMoney());
-            int matchResult = entry.getValue();
-
-            if (rank.equals(Rank.SECOND)) {
-                printout.append(
-                        String.format(PRINT_SECOND_RANK_FORMAT, matchCount, formatedPrizedMoney, matchResult)
-                );
-                continue; //todo 이거 꼭 써야도;ㅁ 아니면 5/true 시 2,3등 업데이트됨
-            }
-            printout.append(
-                    String.format(PRINT_RANK_FORMAT, matchCount, formatedPrizedMoney, matchResult)
-            );
-        }
-        System.out.println(printout);
+        WinningStatisticPrinter.print(winningLottos);
     }
 
     public static void printRateOfReturn(Wallet wallet) {
         System.out.printf(PRINT_RATE_OF_RETURN_FORMAT, wallet.getRateOfReturn());
-
     }
 
 
---
+++ src/main/java/lotto/view/WinningStatisticPrinter.java
@@ -0,0 +1,50 @@
+package lotto.view;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.domain.Rank;
+import lotto.domain.lottos.user.WinningLotto;
+
+public class WinningStatisticPrinter {
+    private final static String PRINT_RANK_FORMAT = "%d개 일치 (%s원) - %d개\n";
+    private final static String PRINT_SECOND_RANK_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+    private final static String PRIZE_MONEY_FORMAT = "%,d";
+
+
+    public static void print(WinningLotto winningLotto) {
+        System.out.println(getPrintout(winningLotto));
+    }
+
+
+    public static String getPrintout(WinningLotto winningLotto) {
+        StringBuilder printout = new StringBuilder();
+        EnumMap<Rank, Integer> ranks = winningLotto.getWinningStatistics();
+        loopScore(printout, ranks);
+
+        return printout.toString();
+    }
+
+    private static void loopScore(StringBuilder printout, Map<Rank, Integer> ranks) {
+        for (Map.Entry<Rank, Integer> entry : ranks.entrySet()) {
+            Rank rank = entry.getKey();
+
+            if (rank.equals(Rank.NOTHING)) {
+                continue;
+            }
+            printout.append(getFormattedScorePrintout(entry, rank));
+        }
+    }
+
+    private static String getFormattedScorePrintout(Entry<Rank, Integer> entry, Rank rank) {
+        int matchCount = rank.getMatchCount();
+        String formatedPrizedMoney = String.format(PRIZE_MONEY_FORMAT, rank.getPrizeMoney());
+        int matchResult = entry.getValue();
+
+        if (rank.equals(Rank.SECOND)) {
+            return String.format(PRINT_SECOND_RANK_FORMAT, matchCount, formatedPrizedMoney, matchResult);
+        }
+        return String.format(PRINT_RANK_FORMAT, matchCount, formatedPrizedMoney, matchResult);
+    }
+
+}
