d58b5cf39bf9f160dd7a93cefccac52bae251106
donghyunn1 <dongdong0516@naver.com>
2024-10-31 14:18:27 +0900

    feat: 당첨 번호와 보너스 번호에 대한 예외 처리 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class WinningNumber {
 
+    private static final String ERROR_INPUT = "[ERROR] 정수를 입력하세요.";
+    private static final String ERROR_RANGE = "[ERROR] 1~45 사이의 숫자를 입력해주세요.";
+    private static final String ERROR_DUPLICATE = "[ERROR] 중복된 숫자를 입력할 수 없습니다.";
+    private static final String ERROR_SIX_NUMBER = "[ERROR] 당첨 번호의 갯수는 6개의 숫자와 ,로 입력되어야 합니다.";
+
     private final List<Integer> numbers = new ArrayList<>();
     private final int bonusNumber;
 
     public WinningNumber(String numbers, String bonusNumber) {
         setWinningNumber(numbers);
+
+        validateInput(bonusNumber);
+        validateRange(Integer.parseInt(bonusNumber));
+        validateDuplicateNumber(Integer.parseInt(bonusNumber));
 
         this.bonusNumber = Integer.parseInt(bonusNumber);
     }
@@ -23,11 +32,43 @@ public class WinningNumber {
     }
 
     private void setWinningNumber(String input) {
+        validateSixNumber(input);
 
         String[] numbers = input.split(",");
+
         for (String number : numbers) {
+            validateInput(number);
+            validateRange(Integer.parseInt(number));
+            validateDuplicateNumber(Integer.parseInt(number));
             this.numbers.add(Integer.parseInt(number));
         }
     }
 
+    private void validateInput(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_INPUT);
+        }
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(ERROR_RANGE);
+        }
+    }
+
+    private void validateDuplicateNumber(int number) {
+        if (this.numbers.contains(number)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
+    }
+
+    private void validateSixNumber(String input) {
+        String[] number = input.split(",");
+
+        if (number.length != 6) {
+            throw new IllegalArgumentException(ERROR_SIX_NUMBER);
+        }
+    }
 }
