44a273d483b55beb6277f1e687f097459c54d76f
Jewan1120 <hawaii1468@gmail.com>
2024-11-01 02:02:29 +0900

    refactor(View): 불필요한 공백 제거

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,7 +5,7 @@ import lotto.enums.ViewMessages;
 public class OutputView {
 
     public void printLottoAmount(int lottoAmount) {
-        System.out.println(ViewMessages.PRINT_BLANK.getMessage());
+        System.out.println();
         System.out.println(ViewMessages.PRINT_LOTTO_AMOUNT.getMessage(lottoAmount));
     }
 }
