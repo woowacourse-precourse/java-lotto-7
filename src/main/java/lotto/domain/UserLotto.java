f3e5d46cc1d74d0399ff86628dce328bdb51feea
rladmstn <oz33177351@gmail.com>
2024-10-31 23:55:30 +0900

    refactor(user) : UserLotto 필드 수정
    - 등수 별 당첨 횟수 관련 자료구조를 Map으로 수정

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -1,46 +1,30 @@
 package lotto.domain;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserLotto {
     private List<Lotto> lottos;
-    private int firstPrizeCount;
-    private int secondPrizeCount;
-    private int thirdPrizeCount;
-    private int fourthPrizeCount;
-    private int fifthPrizeCount;
+    private Map<Integer,Integer> winningCount = new LinkedHashMap<>();
 
     public UserLotto(List<Lotto> lottos) {
         this.lottos = lottos;
-        firstPrizeCount = 0;
-        secondPrizeCount = 0;
-        thirdPrizeCount = 0;
-        fourthPrizeCount = 0;
-        fifthPrizeCount = 0;
+
+        for(int i=1; i<=5; i++){
+            winningCount.put(i,0);
+        }
     }
 
     public List<Lotto> getLottos() {
         return lottos;
     }
 
-    public int getFirstPrizeCount() {
-        return firstPrizeCount;
+    public int getWinningCount(int place){
+        return winningCount.get(place);
     }
 
-    public int getSecondPrizeCount() {
-        return secondPrizeCount;
+    public void updateWinningCount(int place){
+        winningCount.put(place, winningCount.get(place)+1);
     }
-
-    public int getThirdPrizeCount() {
-        return thirdPrizeCount;
-    }
-
-    public int getFourthPrizeCount() {
-        return fourthPrizeCount;
-    }
-
-    public int getFifthPrizeCount() {
-        return fifthPrizeCount;
-    }
-
 }
