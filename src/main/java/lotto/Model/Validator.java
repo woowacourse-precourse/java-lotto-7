f4eb6ad4026857b5e2eb5db0b9bd69d9d5ee9b84
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-10-31 17:42:01 +0900

    feat(Validator): 당첨 번호 유효성 검사 기능 구현 - 당첨 번호는 1~45 범위의 중복되지 않은 숫자 6개

--- src/main/java/lotto/Model/Validator.java
+++ src/main/java/lotto/Model/Validator.java
@@ -1,5 +1,9 @@
 package lotto.Model;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
     public static void validatePrice(int price){
         if (price <= 0){
@@ -7,6 +11,22 @@ public class Validator {
         }
         if (price % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 금액은 1000단위로 입력되어야 합니다.");
+        }
+    }
+
+    public static void validateWinNumbers(List<Integer> winNumbers) {
+        if (winNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int num : winNumbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45까지입니다.");
+            }
+            if (!uniqueNumbers.add(num)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+            }
         }
     }
 }
