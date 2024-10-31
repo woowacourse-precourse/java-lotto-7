fff4be09550c06c1c8fd3924baa3945a25133fa2
koomchang <leahpar0401@gmail.com>
2024-10-31 23:29:00 +0900

    refactor: OutputView 에서 문자열을 하드코딩하지 않고 상수로 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,26 +2,33 @@ package lotto.view;
 
 public class OutputView {
 
+    private static final String ASK_FOR_MONEY = "구입금액을 입력해 주세요.";
+    private static final String ASK_FOR_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String PURCHASED_LOTTO_COUNT = "%d개를 구매했습니다.";
+    private static final String ASK_FOR_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
     public void askForMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(ASK_FOR_MONEY);
     }
 
     public void printLottoCount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
+        System.out.printf((PURCHASED_LOTTO_COUNT) + "%n", lottoCount);
     }
 
     public void printLottoNumbers() {
+
     }
 
     private void printLottoNumber() {
+
     }
 
     public void askForWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(ASK_FOR_WINNING_NUMBERS);
     }
 
     public void askForBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(ASK_FOR_BONUS_NUMBER);
     }
 
     public void printWinningStatistics() {
