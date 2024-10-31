76f4f788aacc970b8922ff3f4434957ec2a2150a
Koo-sangwoo <luckytkddn@gmail.com>
2024-10-31 12:21:29 +0900

    refactor : 로또 생성 및 구매개수 유효성 검사 로직 모델로 이동
    - 유효성 검사 및 로또 생성 로직이 모델 생성시 동시 실행되어야한다고 판단
    - 두 메서드를 모델로 옮기고 받는 인자값을 구매개수로 변경

--- src/main/java/lotto/controller/LottoPurchaseController.java
+++ src/main/java/lotto/controller/LottoPurchaseController.java
@@ -1,32 +1,22 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 import lotto.model.LottoTicketPurchase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoPurchaseController {
     private final LottoTicketPurchase lottoTicketPurchase;
 
     public LottoPurchaseController(Integer buyCount) {
-        this.lottoTicketPurchase = new LottoTicketPurchase(getBuyLottoList(buyCount));
+        this.lottoTicketPurchase = new LottoTicketPurchase(buyCount);
     }
 
 
-    public List<Lotto> getBuyLottoList(int buyCount) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < buyCount; i++) {
-            lottoList.add(genarateLotto());
-        }
-        return lottoList;
+
+
+    public List<Lotto> getBuyLottoInfoList() {
+        return this.lottoTicketPurchase.getLottoTickets();
     }
-
-    public Lotto genarateLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-    }
-
-
 
 }
--- src/main/java/lotto/model/LottoTicketPurchase.java
+++ src/main/java/lotto/model/LottoTicketPurchase.java
@@ -1,19 +1,41 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicketPurchase {
     List<Lotto> lottoTickets;
 
-    public LottoTicketPurchase(List<Lotto> lottoTickets) {
+    public LottoTicketPurchase(Integer buyCount) {
+        buyCountValidation(buyCount);
+        this.lottoTickets = makeBuyLottoList(buyCount);
         validationTicketNullCheck(lottoTickets);
-        this.lottoTickets = lottoTickets;
+    }
+
+    private void buyCountValidation(Integer buyCount) {
+        if (buyCount == null || buyCount == 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 개수가 0개입니다.");
+        }
     }
 
     private void validationTicketNullCheck(List<Lotto> lottoTickets) {
         if (lottoTickets == null || lottoTickets.isEmpty()) {
            throw new IllegalArgumentException("[ERROR] 로또 티켓이 발급되지 않았습니다!");
         }
+    }
+
+    private Lotto genarateLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+
+    private List<Lotto> makeBuyLottoList(int buyCount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < buyCount; i++) {
+            lottoList.add(genarateLotto());
+        }
+        return lottoList;
     }
 
     public List<Lotto> getLottoTickets() {
