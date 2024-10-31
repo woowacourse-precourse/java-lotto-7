d86c99d301f026c1d270e1f2cea94a9b0a28ff8c
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 15:36:54 +0900

    feat(view): 당첨 번호 입력 안내 문구 출력

---
+++ src/main/java/lotto/view/WinningNumbersInputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class WinningNumbersInputView {
+    private static final String WINNING_NUMBERS_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
+
+    public void printWinningNumbersInputGuide() {
+        System.out.println(WINNING_NUMBERS_INPUT_GUIDE);
+    }
+}
