388b3efd2a302d6bfb5b240004e5e0401f277b5a
StoneCAU <seokwoo1999@naver.com>
2024-11-01 02:51:04 +0900

    feat(BonusNumber): BonusNumber 객체 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import static lotto.constant.LottoGameRule.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoGameRule.MIN_LOTTO_NUMBER;
+import static lotto.exception.ErrorMessage.BONUS_NUMBER_DUPLICATE;
+import static lotto.exception.ErrorMessage.BONUS_NUMBER_OUT_OF_RANGE;
+
+import java.util.List;
+import lotto.exception.LottoException;
+import lotto.utils.Parser;
+
+public class BonusNumber {
+    private final WinningNumbers winningNumbers;
+    private final int number;
+
+    public BonusNumber(WinningNumbers winningNumbers, String input) {
+        int number = Parser.parseBonusNumber(input);
+        validateNumber(winningNumbers, number);
+        this.winningNumbers = winningNumbers;
+        this.number = number;
+    }
+
+    private static void validateNumber(WinningNumbers winningNumbers, int number) {
+        validateRange(number);
+        validateDuplicate(winningNumbers.getNumbers(), number);
+    }
+
+    private static void validateRange(int number) {
+        if (isOutOfBounds(number)) {
+            throw new LottoException(BONUS_NUMBER_OUT_OF_RANGE);
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers, int number) {
+        if (isDuplicate(numbers, number)) {
+            throw new LottoException(BONUS_NUMBER_DUPLICATE);
+        }
+    }
+
+    private static boolean isOutOfBounds(int number) {
+        return number < MIN_LOTTO_NUMBER.getValue() ||
+                number > MAX_LOTTO_NUMBER.getValue();
+    }
+
+    private static boolean isDuplicate(List<Integer> numbers, int number) {
+        return numbers.contains(number);
+    }
+}
