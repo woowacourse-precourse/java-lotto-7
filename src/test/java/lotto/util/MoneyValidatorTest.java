74af7a8e3a0c2d0f12b7545c762ef18ae202d984
rladmstn <oz33177351@gmail.com>
2024-10-31 16:37:15 +0900

    test(validator) : 구매 금액 validator 테스트 작성
    아래 3가지 케이스들에 대해 테스트 작성
    1. 숫자 외의 문자 입력 시, 검증 실패하는 경우
    2. 1000으로 나눠떨어지지 않거나 1000 미만의 숫자를 입력 시, 검증 실패하는 경우
    3. 구매 금액 검증을 통과하는 경우

---
+++ src/test/java/lotto/util/MoneyValidatorTest.java
@@ -0,0 +1,32 @@
+package lotto.util;
+
+import lotto.enums.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MoneyValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"","a1","1  0","1-1"})
+    void 숫자_외의_문자_입력시_검증_실패(String input) {
+        Assertions.assertThatThrownBy(() -> MoneyValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_MONEY_INPUT.format());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0","1001"})
+    void 나눠떨어지지_않는_입력시_검증_실패(String input) {
+        Assertions.assertThatThrownBy(() -> MoneyValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NOT_DIVISIBLE_BY_THOUSAND.format());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1000","10101000"})
+    void 구매_금액_검증_통과(String input){
+        MoneyValidator.validate(input);
+    }
+
+}
