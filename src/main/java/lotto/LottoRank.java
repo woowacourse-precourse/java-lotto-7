12f6365e64dd170aa403baee0479689a480f99b6
seoyoung <si159521@gmail.com>
2024-10-31 17:42:42 +0900

    feat LottoRank : enum변수선언 및 당첨갯수 별 메세지 출력하는 코드 작성

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,8 +1,13 @@
 package lotto;
 
 public enum LottoRank {
-    three, four, five, six;
-//    public String getMessage(){
-//
-//    }
+    three, four, five, bonus, six;
+    public String getMessage(){
+        if (this == three) return "3개 일치 (5,000원) - ";
+        if (this == four) return "4개 일치 (10,000원) - ";
+        if (this == five) return "5개 일치 (1,500,000원) - ";
+        if (this == bonus) return "5개 일치, 보너스 볼 일치 (3,000,000원) - ";
+        if (this == six) return "6개 일치 (2,000,000,000원) - ";
+        return "";
+    }
 }
