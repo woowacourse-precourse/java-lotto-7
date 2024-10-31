3fe0bf2af3641d35848e58143d6f8b4329655959
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-01 00:00:08 +0900

    feat: 로또 머신구현

--- src/main/java/lotto/domain/lotto/LottoMachine.java
+++ src/main/java/lotto/domain/lotto/LottoMachine.java
@@ -4,11 +4,17 @@ import lotto.domain.lotto.factory.LottoFactory;
 
 public class LottoMachine {
 
-    private  LottoFactory lottoFactory;
-    private  Lottos lottos;
+    private LottoFactory lottoFactory;
+    private Lottos lottos;
 
-    public LottoMachine(LottoFactory lottoFactory, Lottos lottos) {
-        this.lottoFactory = lottoFactory;
-        this.lottos = lottos;
+    public LottoMachine() {
+    }
+
+    public void setLottoFactory(LottoFactory lottoFactory){
+        this.lottoFactory = lottoFactory
+    }
+
+    public void buyNumberOfLottos(int amount){
+        lottos = new Lottos(amount);
     }
 }
