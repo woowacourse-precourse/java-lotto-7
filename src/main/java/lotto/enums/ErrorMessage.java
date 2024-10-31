6a8b9b0298451da131f9434e2f7d8692bda4c04c
soohalee <suha630@naver.com>
2024-11-01 02:38:42 +0900

    refactor(Enums, InputView): 상수 에러메시지 Enums을 사용하게 수정함.

---
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -0,0 +1,21 @@
+package lotto.enums;
+
+public enum ErrorMessage {
+    NOT_NUMBER("숫자가 아닙니다."),
+    INVALID_RANGE("범위가 유효하지 않습니다."),
+    NOT_DIVISIBLE_BY_THOUSAND("천원 단위로 나누어 지지 않습니다."),
+    EMPTY_INPUT("빈 입력입니다."),
+    INVALID_WINNING_NUMBER_COUNT("당첨 번호 개수가 맞지 않습니다."),
+    LOTTO_NUMBER_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("번호는 중복될 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,18 +6,12 @@ import java.math.BigDecimal;
 import java.net.Inet4Address;
 import java.util.Arrays;
 import java.util.List;
+import lotto.enums.ErrorMessage;
 import java.util.SimpleTimeZone;
 import lotto.utils.Utils;
 import net.bytebuddy.dynamic.loading.ClassInjector.UsingInstrumentation;
 
 public class InputView {
-    private static final String NOT_NUMBER = "숫자가 아닙니다.";
-    private static final String INVALID_RANGE = "범위가 유효하지 않습니다.";
-    private static final String NOT_DIVISIBLE_BY_THOUSAND = "천원단위로 나누어 지지 않습니다.";
-    private static final String EMPTY_INPUT = "빈 입력입니다.";
-    private static final String INVALID_WINNING_NUMBER_COUNT = "당첨 번호 개수가 맞지 않습니다.";
-    private static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String DUPLICATE_LOTTO_NUMBER = "번호는 중복될 수 없습니다.";
     private static final int WINNING_NUMBER_COUNT = 6;
     private static final int MIN_LOTTO_RANGE = 1;
     private static final int MAX_LOTTO_RANGE = 45;
@@ -44,17 +38,17 @@ public class InputView {
     //금액 valiadate
     private static void validatePurchase(String userInput) {
         if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_INPUT + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage() + " : " + userInput);
         }
         if (!Utils.isDigitOnly(userInput)) {
-            throw new NumberFormatException(NOT_NUMBER + " : " + userInput);
+            throw new NumberFormatException(ErrorMessage.NOT_NUMBER.getMessage() + " : " + userInput);
         }
         BigDecimal count = Utils.stringToNumber(userInput);
         if (!Utils.isInRange(new BigDecimal("1000"), new BigDecimal("100000"), count)) {
-            throw new IllegalArgumentException(INVALID_RANGE + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE.getMessage() + " : " + userInput);
         }
         if (!Utils.isDivisibleByThousand(count.intValue())) {
-            throw new IllegalArgumentException(NOT_DIVISIBLE_BY_THOUSAND + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.NOT_DIVISIBLE_BY_THOUSAND.getMessage() + " : " + userInput);
         }
     }
 
@@ -76,21 +70,21 @@ public class InputView {
 
     private static void validateWinningNumber(String userInput) {
         if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_INPUT + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage() + " : " + userInput);
         }
         String[] numbers = userInput.split(",");
         if (!Utils.allElementsAreDigits(numbers)) {
-            throw new IllegalArgumentException(NOT_NUMBER + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage() + " : " + userInput);
         }
         if (!Utils.checkSizeEqual(numbers, WINNING_NUMBER_COUNT)) {
-            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_COUNT + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage() + " : " + userInput);
         }
         List<BigDecimal> winningNumber = Arrays.stream(numbers).map(number -> new BigDecimal(number)).toList();
         if (!Utils.areAllNumbersValidRange(new BigDecimal(MIN_LOTTO_RANGE), new BigDecimal(MAX_LOTTO_RANGE), winningNumber)) {
-            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getMessage() + " : " + userInput);
         }
         if (!Utils.isDuplicateNumber(winningNumber)) {
-            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage() + " : " + userInput);
         }
     }
 
