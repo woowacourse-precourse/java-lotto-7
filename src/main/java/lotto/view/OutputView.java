0f274e354a38918405f23452e4fe24ddadfe74c6
donghyunn1 <dongdong0516@naver.com>
2024-11-01 00:06:08 +0900

    feat: 당첨통계 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,29 @@
 package lotto.view;
 
+import lotto.domain.winning.WinningRank;
+import lotto.domain.winning.WinningStatics;
+
 import java.util.List;
 
 public class OutputView {
 
     public void printLottoStatus(List<List<Integer>> Lottos) {
-        System.out.println(Lottos.size() + "개를 구입했습니다.");
+        System.out.println("\n" + Lottos.size() + "개를 구입했습니다.");
         for (List<Integer> lotto : Lottos) {
             System.out.println(lotto);
+        }
+    }
+
+    public void printWinningStatics(WinningStatics winningStatics) {
+        System.out.println("\n" + "당첨통계");
+        System.out.println("---");
+
+        for (WinningRank winningRank : winningStatics.getWinningStatics().keySet()) {
+            if (winningRank.equals(winningRank.NONE)) {
+                continue;
+            }
+
+            System.out.printf(winningRank.getWinningString(), winningStatics.getWinningCount(winningRank));
         }
     }
 }
