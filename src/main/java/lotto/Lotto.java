e49e278dc7e45f8618b56c3fe0321f8f32bb1e0f
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-11-01 00:56:24 +0900

    feat: 로또 번호 유효성 검증 및 예외 처리 테스트 코드 추가
    - 로또 번호의 유효성을 확인하는 검증 로직 추가
      - 번호 개수가 6개인지 검증
      - 번호가 1~45 범위 내에 있는지 검증
      - 중복된 번호가 포함되지 않도록 검증
    
    - 유효성 검증에 대한 테스트 코드 작성
      - 1~45 범위를 벗어나는 번호 입력 시 IllegalArgumentException 발생 확인
      - 번호 개수 미달 및 중복 번호 입력 시 예외 처리 테스트 추가
      - 예외 발생 시 올바른 에러 메시지 출력 여부 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import utils.LottoErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +15,45 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateLottoNumCount(numbers);
+        validateLottoNumbersInRange(numbers);
+        checkNoDuplicateNumbers(numbers);
+    }
+
+
+
+
+    private void validateLottoNumCount(List<Integer> numbers) {
+        if (!isLottoCountValid(numbers)) {
+            throw new IllegalArgumentException(LottoErrorMessage.INVALID_LOTTO_COUNT.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoNumbersInRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (!isLottoNumberInRange(number)) {
+                throw new IllegalArgumentException(LottoErrorMessage.INVALID_LOTTO_NUMBER_IN_RANGE.getMessage());
+            }
+        });
+    }
+
+    private void checkNoDuplicateNumbers(List<Integer> numbers) {
+        if (!hasNoDuplicates(numbers)) {
+            throw new IllegalArgumentException(LottoErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private boolean isLottoCountValid(List<Integer> numbers) {
+        return numbers.size() == 6;
+    }
+
+
+    private boolean isLottoNumberInRange(int lottoNumber) {
+        return lottoNumber >= 1 && lottoNumber <= 45;
+    }
+
+    private boolean hasNoDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() == numbers.size();
+    }
 }
---
+++ src/main/java/utils/LottoErrorMessage.java
@@ -0,0 +1,23 @@
+package utils;
+
+public enum LottoErrorMessage {
+    INVALID_LOTTO_NUMBER_IN_RANGE("[ERROR] 로또 번호는 1~45 사이의 정수여야 합니다."),
+    INVALID_LOTTO_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호엔 중복이 있을 수 없습니다.");
+
+
+    private final String message;
+
+    LottoErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
