73b46934be6fd41210fd03549ce39b6083617226
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2024-10-31 19:20:29 +0900

    feat : lotto validator 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,19 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.dto.LottoWinResult;
+import lotto.enums.LottoCriteria;
 import lotto.enums.LottoErrorMessage;
 import lotto.exception.LottoInputException;
+import lotto.utility.NumberUtility;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
 
     private final List<Integer> numbers;
+    private static final String PRINT_NUMBERS_PREFIX = "[";
+    private static final String PRINT_NUMBERS_POSTFIX = "]" + System.lineSeparator();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -15,6 +21,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        if(NumberUtility.isDuplicateNumberExists(numbers)){
+            throw new LottoInputException(LottoErrorMessage.DUPLICATE_NUMBERS);
+        }
+        if(!LottoValidator.isInRangeLottoNumbers(numbers)){
+            throw new LottoInputException(LottoErrorMessage.INVALID_NUMBER);
+        }
         if (numbers.size() != 6) {
             throw new LottoInputException(LottoErrorMessage.LOTTO_WINNING_INVALID_SIZE);
         }
@@ -23,6 +35,9 @@ public class Lotto {
     public LottoWinResult getResult(List<Integer> winningNumbers, int bonusNumber) {
         int winCount = getWinningCount(winningNumbers);
         boolean isBonus = isBonus(bonusNumber);
+        if(winCount == LottoCriteria.BONUS_LOTTO_NUM.getCriteriaVal() && isBonus){
+            winCount = LottoCriteria.BONUS_CASE_LOTTO_NUM.getCriteriaVal();
+        }
         return new LottoWinResult(winCount,isBonus,LottoCalculator.cal(winCount,isBonus));
     }
 
@@ -41,5 +56,12 @@ public class Lotto {
             }
         }
         return winCount;
+    }
+
+    public String getLottoNumberStr(){
+        String numbersStr=  numbers.stream()
+                .map((number) -> number + "")
+                .collect(Collectors.joining(", "));
+        return PRINT_NUMBERS_PREFIX + numbersStr + PRINT_NUMBERS_POSTFIX;
     }
 }
