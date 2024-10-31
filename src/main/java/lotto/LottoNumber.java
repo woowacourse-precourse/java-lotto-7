52ac01ccfc65ab22643484a39e9f16bc2fffe0b5
hanseounggyun <hanseu9839@gmail.com>
2024-10-31 17:13:32 +0900

    feature: 로또 성공 테스트 코드 작성

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,2 +1,36 @@
-package lotto;public class LottoNumber {
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final String INVALID_NUMBER_ERROR = "[ERROR] : 로또 숫자는 1에서 45사이만 입력 가능합니다.";
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+
+    private int number;
+
+    public LottoNumber(int number) {
+        this.number = isValid(number);
+    }
+
+    private int isValid(int number) {
+        if(number > MAX_NUMBER || number < MIN_NUMBER) {
+            throw new IllegalArgumentException(INVALID_NUMBER_ERROR);
+        }
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
 }
