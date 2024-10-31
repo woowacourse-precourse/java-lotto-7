f7f7bd56f9e33bbb9792e096bdbe01c55374bf1e
181108-cherry <dysy9799@naver.com>
2024-10-31 11:51:15 +0900

    feat: 사용자 입력에 대한 유효성 검증 및 예외 처리
    - 로또 구매 금액 입력 시 유효하지 않은 금액일 경우 반복적으로 입력을 요청하도록 함
    - 금액이 0 이하이거나 1000원 단위가 아닌 경우 예외 발생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,16 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money;
+
+        while (true) {
+            try {
+                money = getMoney();
+                exception(money);
+                break;
+            } catch (IllegalArgumentException error) {
+                System.out.println("[ERROR]" + error.getMessage());
+            }
+        }
     }
 }
