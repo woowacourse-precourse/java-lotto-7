3e44496fde817470590896a69a857b7646abbf99
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-10-31 14:27:50 +0900

    feat($view) : Add printing bought lotties function

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class View {
     public String getMoneyInput() {
@@ -16,6 +17,13 @@ public class View {
     public String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public void printLotties(List<List<Integer>> lotties) {
+        System.out.println(lotties.size()+"개를 구매했습니다.");
+        for (List<Integer> lotto : lotties) {
+            System.out.println(lotto.toString());
+        }
     }
 
 }
