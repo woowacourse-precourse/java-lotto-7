f30d3c8fef5cc7b244cf4c9ddb3bff5eba1c34c7
koomchang <leahpar0401@gmail.com>
2024-10-31 23:35:08 +0900

    refactor: 입력 금액 검증 클래스에서 메소드 네이밍 변경

--- src/main/java/lotto/validator/MoneyInputValidator.java
+++ src/main/java/lotto/validator/MoneyInputValidator.java
@@ -2,40 +2,51 @@ package lotto.validator;
 
 public class MoneyInputValidator implements BasicValidator<String> {
 
+    private static final int LOTTO_PURCHASE_PRICE = 1000;
+    private static final int ZERO = 0;
+    private static final String INPUT_NOT_NUMERIC = "[ERROR] 입력값이 숫자가 아닙니다.";
+    private static final String INPUT_NOT_POSITIVE = "[ERROR] 입력값이 0보다 커야 합니다.";
+    private static final String INPUT_NOT_MULTIPLE_OF_THOUSAND = "[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다.";
+    private static final String INPUT_NOT_MORE_THAN_ONE_THOUSAND = "[ERROR] 로또 구입 금액은 최소 1000원 이상이어야 합니다.";
+
     @Override
     public void validate(String input) {
-        isNumeric(input);
-        isPositive(input);
-        isMultipleOfThousand(input);
-        isMoreThanOneThousand(input);
+        validateNumberIfNumeric(input);
+        validateNumberIfPositive(input);
+        validateNumberIfMultipleOfThousand(input);
+        validateNumberIfMoreThanOneThousand(input);
     }
 
-    private void isNumeric(String input) {
+    private void validateNumberIfNumeric(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 숫자가 아닙니다.");
+            throw new IllegalArgumentException(INPUT_NOT_NUMERIC);
         }
     }
 
-    private void isPositive(String input) {
+    private void validateNumberIfPositive(String input) {
         int money = Integer.parseInt(input);
-        if (money <= 0) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 0보다 커야 합니다.");
+        if (money <= ZERO) {
+            throw new IllegalArgumentException(INPUT_NOT_POSITIVE);
         }
     }
 
-    private void isMultipleOfThousand(String input) {
+    private void validateNumberIfMultipleOfThousand(String input) {
         int money = Integer.parseInt(input);
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다.");
+        if (isNotMultipleOfThousand(money)) {
+            throw new IllegalArgumentException(INPUT_NOT_MULTIPLE_OF_THOUSAND);
         }
     }
 
-    private void isMoreThanOneThousand(String input) {
+    private static boolean isNotMultipleOfThousand(int money) {
+        return money % LOTTO_PURCHASE_PRICE != ZERO;
+    }
+
+    private void validateNumberIfMoreThanOneThousand(String input) {
         int money = Integer.parseInt(input);
-        if (money < 1000) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최소 1000원 이상이어야 합니다.");
+        if (money < LOTTO_PURCHASE_PRICE) {
+            throw new IllegalArgumentException(INPUT_NOT_MORE_THAN_ONE_THOUSAND);
         }
     }
 }
