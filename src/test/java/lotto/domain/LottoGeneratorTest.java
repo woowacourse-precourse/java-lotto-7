d0b8ab17698c4365a5c81ce8ab984c64a192f202
OhYeHyun <ohyhohyl@naver.com>
2024-10-31 23:56:29 +0900

    refactor(LottoGenerator): 숫자를 생성만 하는 기능으로 수정

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -1,9 +1,10 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGeneratorTest {
     private LottoGenerator lotto;
@@ -11,7 +12,7 @@ public class LottoGeneratorTest {
     @Test
     @DisplayName("6개의 로또 번호를 랜덤으로 생성하는 기능 확인")
     void 로또_번호_생성_테스트() {
-        lotto = new LottoGenerator();
+        Lotto lotto = new Lotto(LottoGenerator.generateNumbers());
         assertThat(lotto.getNumbers().size()).isEqualTo(6);
     }
 }
