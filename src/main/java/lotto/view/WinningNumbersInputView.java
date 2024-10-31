72014b120c2f635066817d30170ad1ca50fe3a84
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 16:08:52 +0900

    refactor: 당첨 번호를 view에서 파싱하여 전달하도록 변경

--- src/main/java/lotto/view/WinningNumbersInputView.java
+++ src/main/java/lotto/view/WinningNumbersInputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class WinningNumbersInputView {
     private static final String WINNING_NUMBERS_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
@@ -8,8 +9,14 @@ public class WinningNumbersInputView {
     public void printWinningNumbersInputGuide() {
         System.out.println(WINNING_NUMBERS_INPUT_GUIDE);
     }
-    
-    public String getWinningNumbers() {
-        return Console.readLine();
+
+    public List<String> getWinningNumbers() {
+        String winningNumbers = Console.readLine();
+        return getParsedWinningNumbers(winningNumbers);
+    }
+
+    private List<String> getParsedWinningNumbers(String winningNumbers) {
+        String[] parsedWinningNumbers =  winningNumbers.split(",");
+        return List.of(parsedWinningNumbers);
     }
 }
