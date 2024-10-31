83a5ca20f6d233051ea8d9283357e1adcce29d44
JS <zks145@naver.com>
2024-11-01 02:06:04 +0900

    test : 사용자가 구매한 로또 랜덤 번호를 오름차순 정렬하는 기능 테스트

---
+++ src/test/java/lotto/UtilTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import lotto.util.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.util.Util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    private final static Integer LOTTO_NUMBER_START = 1;
+    private final static Integer LOTTO_NUMBER_END = 45;
+    private final static Integer LOTTO_MAX_LENGTH = 6;
+
+    @DisplayName("로또 번호 정렬")
+    @Test
+    void 로또번호_정렬() {
+        //given
+        List<Integer> lottoNumbers = randomLottoNumbers(LOTTO_NUMBER_START, LOTTO_NUMBER_END, LOTTO_MAX_LENGTH);
+
+        //when
+        List<Integer> sortLottoNumbers = sortLottoNumbers(lottoNumbers);
+
+        //then
+        for (int i = 0; i < sortLottoNumbers.size() - 1; i++) {
+            assertTrue(sortLottoNumbers.get(i) < sortLottoNumbers.get(i + 1));
+        }
+
+    }
+
+}
