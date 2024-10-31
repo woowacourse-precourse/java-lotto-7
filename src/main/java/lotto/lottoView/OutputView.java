dfa41944072b16017f7939e8a24e70681d7c145d
ppzepi <hsunguk1999@gmail.com>
2024-10-31 20:29:33 +0900

    refactor: howManyBuy 변수 전달 받도록 수정

--- src/main/java/lotto/lottoView/OutputView.java
+++ src/main/java/lotto/lottoView/OutputView.java
@@ -3,7 +3,7 @@ package lotto.lottoView;
 import java.util.List;
 
 public class OutputView {
-    private static final String HOW_MANY_BUY = "개를 구매했습니다.";
+    private static final String HOW_MANY_BUY = "%d개를 구매했습니다.";
     private static final String STATISTIC_START = "당첨 통계\n---";
     private static final String VALUE_MATCH_START = "%d개 일치";
     private static final String VALUE_MATCH_END = " (%d원) - %d개";
@@ -11,8 +11,8 @@ public class OutputView {
     private static final String PROFIT_MESSAGE = "총 수익률은 %f %입니다.";
 
 
-    public void howManyBuy() {
-        System.out.println(HOW_MANY_BUY);
+    public void howManyBuy(Long numberOfBuy) {
+        System.out.printf(HOW_MANY_BUY, numberOfBuy);
     }
 
     public void statisticStart() {
