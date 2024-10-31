779643d032d34360b592e031f1847deed8173d18
dudxo <father8475@naver.com>
2024-11-01 00:59:49 +0900

    feat : 에러 메시지
    - 양수 값이 아닐 때 출력되는 에러 메시지를 작성한다.
    - 1과 45 사이의 값이 아닐 때 출력되는 에러 메시지를 작성한다.
    - perfix 메시지인 '[ERROR] ' 문구를 상수 문자열로 선언하여 코드의 중복성을 제거한다.

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -2,16 +2,20 @@ package exception;
 
 public enum ErrorMessage {
 
-    PURCHASE_AMOUNT_INVALID_UNIT_MSG("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
-    NOT_NUMBER_FORMAT("[ERROR] 숫자로 입력해야 합니다."),
-    DUPLICATE_WINNING_NUMBERS("[ERROR] 당첨 번호는 중복되지 않는 숫자여야 합니다."),
-    NOT_SIX_WINNING_NUMBERS("[ERROR] 당첨 번호는 6자리여야 합니다."),
-    BONUS_NUMBER_IN_WINNING_NUMBERS("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
+    PURCHASE_AMOUNT_INVALID_UNIT_MSG("구입 금액은 1,000원 단위여야 합니다."),
+    NOT_NUMBER_FORMAT("숫자로 입력해야 합니다."),
+    DUPLICATE_WINNING_NUMBERS("당첨 번호는 중복되지 않는 숫자여야 합니다."),
+    NOT_SIX_WINNING_NUMBERS("당첨 번호는 6자리여야 합니다."),
+    BONUS_NUMBER_IN_WINNING_NUMBERS("보너스 번호는 당첨 번호에 포함될 수 없습니다."),
+    NOT_POSITIVE_NUMBER("양수로 입력해야 합니다."),
+    NOT_ONE_BETWEEN_FORTY_FIVE("1과 45 사이의 값이어야 합니다.");
 
     private final String message;
 
+    private static final String PREFIX_ERROR_MSG = "[ERROR] ";
+
     ErrorMessage(String message) {
-        this.message = message;
+        this.message = PREFIX_ERROR_MSG + message;
     }
 
     public String getMessage() {
