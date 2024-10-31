53ebd55deae56266838fcbf69d4283d595ecca50
sung <salamander011017@gmail.com>
2024-10-31 18:01:48 +0900

    feat : InputView 및 InputValidator 클래스 세부 메소드 구현 -InputView -readWinningNumbers() : 당첨 번호 입력 -readBonusNumber() : 보너스 번호 입력 -InputValidator -validateWinningNumbers() : String을 받아 ,로 분리하고 List 형식으로 반환하는 메소드

--- src/main/java/lotto/view/input/InputView.java
+++ src/main/java/lotto/view/input/InputView.java
@@ -1,11 +1,26 @@
 package lotto.view.input;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputView {
+
     public static int readPurchaseAmount(){
         System.out.println("\n구입금액을 입력해 주세요.");
         String input = Console.readLine();
         return InputValidator.validatePurchaseAmount(input);
     }
+    public static List<Integer> readWinningNumbers(){
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return InputValidator.validateWinningNumbers(input);
+    }
+
+    public static int readBonusNumber(){
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return InputValidator.validateNumber(input);
+    }
+
+
 }
