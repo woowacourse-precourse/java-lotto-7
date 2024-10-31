75f338f9097d59e0bdfde7079d9576c66530fae0
yeong-hwan <jghff700@naver.com>
2024-10-31 14:43:00 +0900

    refactor(enums): divide error tag with error messages

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -1,10 +1,12 @@
 package lotto.enums;
 
 public enum ErrorMessage {
-    INVALID_AMOUNT("[ERROR] 금액은 1,000원 단위로 입력해야 합니다."),
-    INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 6개여야 합니다."),
-    INVALID_BONUS_NUMBER("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_AMOUNT("금액은 1,000원 단위로 입력해야 합니다."),
+    INVALID_WINNING_NUMBERS("당첨 번호는 6개여야 합니다."),
+    INVALID_BONUS_NUMBER("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
 
+    private static final String ERROR_TAG = "[ERROR]";
+    private static final String ERROR_SEPARATOR = " ";  // 태그 뒤 공백
     private final String message;
 
     ErrorMessage(String message) {
@@ -12,10 +14,10 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return ERROR_TAG + ERROR_SEPARATOR + message;
     }
 
     public String format(Object... args) {
-        return String.format(message, args);
+        return ERROR_TAG + ERROR_SEPARATOR + String.format(message, args);
     }
 }
