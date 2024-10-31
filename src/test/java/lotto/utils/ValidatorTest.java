06972a84a15481dc29689cb6b938dfb75d2f9a24
heehun <slrkqk2@naver.com>
2024-10-31 15:24:30 +0900

    test(Validator): winningNumValidator(), 당첨 번호가 패턴에 맞지 않으면 예외처리 테스트 추가

--- src/test/java/lotto/utils/ValidatorTest.java
+++ src/test/java/lotto/utils/ValidatorTest.java
@@ -1,30 +1,40 @@
 package lotto.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorTest {
 
-    @DisplayName("priceValidator 메서드 테스트_01")
+    @DisplayName("priceValidator_메서드_테스트_01")
     @Test
     void 천원_단위_입력이_아니면_예외가_발생한다() {
-        String inputPrice = "11100";
+        String input = "11100";
 
-        Assertions.assertThatThrownBy(()->Validator.priceValidator(inputPrice))
+        assertThatThrownBy(()->Validator.priceValidator(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("priceValidator 메서드 테스트_02")
+    @DisplayName("priceValidator_메서드_테스트_02")
     @Test
     void 가격_입력이_정수가_아니면_예외가_발생한다() {
-        String inputPrice = "1000won";
+        String input = "1000won";
 
-        Assertions.assertThatThrownBy(()->Validator.priceValidator(inputPrice))
+        assertThatThrownBy(()->Validator.priceValidator(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("winningNumValidator_메서드_테스트_01")
+    @Test
+    void 당첨_번호가_숫자쉼표숫자_패턴이_아니면_예외가_발생한다() {
+        String input = "1,5,2,6,9,";
+        String input2 = "1,5,2,6.9";
 
+        assertThatThrownBy(()->Validator.winningNumValidator(input))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->Validator.winningNumValidator(input2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
