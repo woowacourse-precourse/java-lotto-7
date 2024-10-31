be3696f55fa59a242f9623a169696088e330f9c8
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-11-01 05:17:47 +0900

    test: remove unnecessary test from LottoIssuerTest
    - remove test for issuing one lotto ticket

--- src/test/java/lotto/model/LottoIssuerTest.java
+++ src/test/java/lotto/model/LottoIssuerTest.java
@@ -12,19 +12,6 @@ class LottoIssuerTest {
     private final LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
 
     @Test
-    @DisplayName("1개의 로또를 발행한다.")
-    void should_IssueOneLotto() {
-        // given
-        LottoIssuer lottoIssuer = new LottoIssuer(lottoNumbersGenerator);
-
-        // when
-        LottoTickets lottoTickets = lottoIssuer.issueLottoTicket();
-
-        // then
-        assertThat(lottoTickets.tickets()).hasSize(1);
-    }
-
-    @Test
     @DisplayName("구입 금액에 해당하는 만큼 로또를 발행한다.")
     void should_IssueLottosByPurchaseAmount() {
         // given
