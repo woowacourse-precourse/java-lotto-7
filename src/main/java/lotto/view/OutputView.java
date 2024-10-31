4232c8e17bf2ce96d699ec55dc3eba651b96cc76
EunSeop <doosan1112@naver.com>
2024-10-31 12:32:38 +0900

    feat: 상수 문자열을 enum을 활용해 관리하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,27 +6,20 @@ import java.util.List;
 
 public class OutputView {
 
-    private static final String SIX_MATCH = "6개 일치 (2,000,000,000원) - %d개";
-    private static final String FIVE_BONUS_MATCH = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
-    private static final String FIVE_MATCH = "5개 일치 (1,500,000원) - %d개";
-    private static final String FOUR_MATCH = "4개 일치 (50,000원) - %d개";
-    private static final String THREE_MATCH = "3개 일치 (5,000원) - %d개";
-    private static final String LOTTO_RATE_MESSAGE = "총 수익률은 %2f%%입니다.";
-
     public void printPurchaseGuide() {
-        System.out.println("로또를 구입할 금액을 입력하세요.");
+        System.out.println(OutputMessage.PURCHASE_GUIDE.getMessage());
     }
 
     public void printWinningNumbersGuide() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(OutputMessage.WINNING_NUMBERS_GUIDE.getMessage());
     }
 
     public void printBonusNumberGuide() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(OutputMessage.BONUS_NUMBER_GUIDE.getMessage());
     }
 
     public void printLottoCount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다.");
+        System.out.println(String.format(OutputMessage.LOTTO_COUNT_MESSAGE.getMessage(), lottoCount));
     }
 
     public void printLottos(List<Lotto> lottos) {
@@ -36,14 +29,14 @@ public class OutputView {
     }
 
     public void printLottoResult(List<Integer> result) {
-        System.out.println(String.format(SIX_MATCH, result.get(0)));
-        System.out.println(String.format(FIVE_BONUS_MATCH, result.get(1)));
-        System.out.println(String.format(FIVE_MATCH, result.get(2)));
-        System.out.println(String.format(FOUR_MATCH, result.get(3)));
-        System.out.println(String.format(THREE_MATCH, result.get(4)));
+        System.out.println(String.format(OutputMessage.SIX_MATCH.getMessage(), result.get(0)));
+        System.out.println(String.format(OutputMessage.FIVE_BONUS_MATCH.getMessage(), result.get(1)));
+        System.out.println(String.format(OutputMessage.FIVE_MATCH.getMessage(), result.get(2)));
+        System.out.println(String.format(OutputMessage.FOUR_MATCH.getMessage(), result.get(3)));
+        System.out.println(String.format(OutputMessage.THREE_MATCH.getMessage(), result.get(4)));
     }
 
     public void printLottoRate(double rate) {
-        System.out.println(String.format(LOTTO_RATE_MESSAGE, rate));
+        System.out.println(String.format(OutputMessage.LOTTO_RATE_MESSAGE.getMessage(), rate));
     }
 }
