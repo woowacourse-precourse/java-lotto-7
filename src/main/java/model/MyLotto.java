ba7d107fae02b320cc9ae212febf12823fe5cbfc
cux-maks <volcano020716@gmail.com>
2024-10-31 23:32:09 +0900

    feat(MyLotto): 구매한 로또 목록 클래스 작성
    구매한 로또 목록을 저장하는 클래스를 작성하였습니다.

---
+++ src/main/java/model/MyLotto.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MyLotto {
+    private final List<Lotto> myLotto;
+
+    public MyLotto(int lottoNum) {
+        this.myLotto = Stream.generate(Lotto::new)
+                .limit(lottoNum)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public List<Lotto> getMyLotto() {
+        return myLotto;
+    }
+}
