48784f08aa00a3a1ba9f47c38d229d6c61a963e3
ghktndyd <hwasuyong99@naver.com>
2024-10-31 13:59:29 +0900

    feat(InputView): 구입 금액 입력시 숫자 값임을 검증하는 기능 구현

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -3,7 +3,7 @@ package lotto.exception;
 public enum ExceptionMessage {
 
     BLANK_INPUT_EXCEPTION("빈 문자열은 입력할 수 없습니다."),
-    ;
+    NONE_NUMERIC_INPUT_EXCEPTION("숫자 값만 입력할 수 있습니다.");
 
     private final String message;
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,12 +9,20 @@ public class InputView {
     public int getPurchaseAmount() {
         String input = Console.readLine();
         validateBlankInput(input);
-        return Integer.parseInt(input);
+        return validateNumber(input);
     }
 
     private void validateBlankInput(String input) {
         if (input == null || input.isBlank()) {
             throw new CustomException(ExceptionMessage.BLANK_INPUT_EXCEPTION);
+        }
+    }
+
+    private int validateNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new CustomException(ExceptionMessage.NONE_NUMERIC_INPUT_EXCEPTION);
         }
     }
 }
