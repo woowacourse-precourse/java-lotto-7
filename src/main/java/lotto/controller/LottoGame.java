de4452365f9042e5c93e2b488b3f51a81c0eb893
82103 <sangjoon12345@gmail.com>
2024-10-31 23:34:10 +0900

    feat: Controller 구조 생성

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoManager;
+import lotto.view.InputView;
+
+public class LottoGame {
+
+    private final InputView inputView;
+    private final LottoManager lottoManager;
+
+    public LottoGame(InputView inputView, LottoManager lottoManager){
+        this.inputView = inputView;
+        this.lottoManager = lottoManager;
+    }
+
+    public void start(){
+
+
+
+    }
+
+
+
+}
