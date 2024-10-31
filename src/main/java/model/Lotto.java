7f2e10c12a53bb533496bd00d8229feef7e96b4d
cux-maks <volcano020716@gmail.com>
2024-11-01 00:20:44 +0900

    refactor(Lotto): 정렬 로직 추가
    로또 번호를 오름차순으로 정렬하는 로직을 추가하였습니다.

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,16 +1,21 @@
 package model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto() {
-        numbers = LottoGenerator.generateLotto();
+        numbers = LottoGenerator.generateLotto().stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());;
     }
 
     public List<Integer> getNumbers() {
