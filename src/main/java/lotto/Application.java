2a5d346d9f909d771b8b86e59fa4aad134f18e81
ComNewbie <iforking3@naver.com>
2024-10-31 14:33:59 +0900

    feat(ProgramOutput): 사용자 구입 금액 입력 요청 구현 requestPurchaseAmount() 메서드를 통해 구입 입력 요청 문구 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.io.ProgramOutput;
+
 public class Application {
+    private final ProgramOutput output;
+
+    public Application() {
+        this.output = new ProgramOutput();
+    }
+
+    public void run() {
+        output.requestPurchaseAmount();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application lottery = new Application();
+        lottery.run();
     }
 }
---
+++ src/main/java/lotto/io/ProgramOutput.java
@@ -0,0 +1,9 @@
+package lotto.io;
+
+public class ProgramOutput {
+    private static final String REQUEST_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void requestPurchaseAmount(){
+        System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
+    }
+}
