b57bad5b8386e4538e531e1730ed1064c644d535
bronzehyeon <bronzehyeon@gmail.com>
2024-10-31 15:39:19 +0900

    feat : 로또 발행 및 출력 로직 추가
    - 생성자를 통해 6개의 유니크 넘버 생성하여 로또 객체 생성
    - print 메서드를 통해 형식에 맞춰 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,25 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("구입금액을 입력해 주세요.");
         int money = Input.read(Input::money);
 
+        int N = money / 1000;
+
+        List<Lotto> lottos = new ArrayList<>();
+
+        System.out.println(N + "개를 구매했습니다.");
+        for (int i = 0; i < N; i++) {
+            Lotto lotto = new Lotto();
+            lotto.printNumbers();
+            lottos.add(lotto);
+        }
 
     }
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -10,11 +13,23 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Lotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+
+        this.numbers = numbers;
+    }
+
+    public void printNumbers() {
+        System.out.printf("[%d, %d, %d, %d, %d, %d]\n",
+                numbers.get(0), numbers.get(1), numbers.get(2),
+                numbers.get(3), numbers.get(4), numbers.get(5));
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
 }
