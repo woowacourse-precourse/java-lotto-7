7cfa31c703a0611a725e3fd83a44a40e2e01838a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-10-31 19:15:30 +0900

    refactor: 펙토리 매커니즘 수정

--- src/main/java/lotto/domain/lottofactory/LottoFactory.java
+++ src/main/java/lotto/domain/lottofactory/LottoFactory.java
@@ -4,6 +4,6 @@ import lotto.domain.lotto.Lotto;
 
 public interface LottoFactory {
 
-    Lotto create(int startInclusive, int endInclusive, int count);
+    Lotto create();
 
 }
--- src/main/java/lotto/domain/lottofactory/LottoFactoryImpl.java
+++ src/main/java/lotto/domain/lottofactory/LottoFactoryImpl.java
@@ -7,8 +7,18 @@ import java.util.List;
 
 public class LottoFactoryImpl implements LottoFactory {
 
+    private final int startInclusive;
+    private final int endInclusive;
+    private final int count;
+
+    public LottoFactoryImpl(int startInclusive, int endInclusive, int count) {
+        this.startInclusive = startInclusive;
+        this.endInclusive = endInclusive;
+        this.count = count;
+    }
+
     @Override
-    public Lotto create(int startInclusive, int endInclusive, int count) {
+    public Lotto create() {
         List<Integer> lottoCombination = Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
         return new Lotto(lottoCombination);
     }
