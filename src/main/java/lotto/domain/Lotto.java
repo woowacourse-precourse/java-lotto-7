2aa14a2424574087af8f93f5b6fb11a8b4e018fc
junghun00 <ji8657447@gmail.com>
2024-10-31 16:18:25 +0900

    feat: 로또 번호 검증 구현
    - 로또 번호는 6개이다
    - 로또 번호는 중복될 수 없다
    - 로또 번호는 1 에서 45 사이 숫자

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
@@ -13,8 +15,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+        checkSame(numbers);
+        checkRange(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkSame(List<Integer> numbers) {
+        Set<Integer> numberSize = new HashSet<>(numbers);
+        if (numbers.size() != numberSize.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (!(number >= 1 && number <= 45)) {
+                throw new IllegalArgumentException("로또 번호는 1 에서 45 사이로 작성해 주세요");
+            }
         }
     }
 
