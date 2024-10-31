4a7d628c8cbb802fcaf5a9f12fbcde9bed0f73a5
dia <dia19dev@gmail.com>
2024-10-31 17:55:24 +0900

    feature(LottoGenerator): Create LottoGenerator
    Implement functionality to generate a list of lotto tickets based on input values.
    - Calculate the number of lotto tickets to purchase from the input amount.
    - Generate lotto tickets equal to the calculated purchase count and store them in a list.
    - Call the print method for each Lotto in the lotto list.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     static final String DATANAME_AMOUNT = "구입금액을";
     static final String DATANAME_WINNINGNUM = "당첨 번호를";
@@ -8,10 +11,13 @@ public class Application {
     static InputHandler inputHandler = new InputHandler();
 
     public static void main(String[] args) {
-        inputHandler.inputData(DATANAME_AMOUNT);
-        //구매 결과 실행
+        String purchaseAmount = inputHandler.inputData(DATANAME_AMOUNT);
+
+        new LottoGenerator(Integer.parseInt(purchaseAmount));
+
         inputHandler.inputData(DATANAME_WINNINGNUM);
         inputHandler.inputData(DATANAME_BONUSNUM);
+
         //당첨 통계 실행
     }
 }
@@ -24,4 +30,37 @@ class InputHandler {
         return camp.nextstep.edu.missionutils.Console.readLine();
     }
 
+}
+
+class LottoGenerator {
+    final int LOTTO_PRICE = 1000;
+
+    List<Lotto> lottoList = new ArrayList<>();
+
+    public LottoGenerator(int price) {
+        generateLotto(calculateTickets(price));
+        printLottoList();
+    }
+
+    private int calculateTickets(int price) {
+        return price / LOTTO_PRICE;
+    }
+
+    private void generateLotto(int tickets) {
+        int count = 0;
+        while(count < tickets) {
+            lottoList.add(new Lotto());
+            count++;
+        }
+    }
+
+    private void printLottoList() {
+        for (Lotto lotto : lottoList) {
+            lotto.printLotto();
+        }
+    }
+
+    public List<Lotto> getLottoList() {
+        return this.lottoList;
+    }
 }
