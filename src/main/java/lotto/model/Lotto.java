ad2802cf697842735dbe77869868f16da968956b
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-10-31 14:06:23 +0900

    feat(lotto) : validate 메서드 수정
    입력 받은 당첨 번호에 대해 1부터 45사이의 숫자인지, 중복된 숫자가 있는지 검사

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,8 +11,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != 6 || !isValidRange(numbers) || hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자 6개여야 합니다.");
         }
     }
 
