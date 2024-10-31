4015a469c56e5059d3f720f3864a82bde66cd60c
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-10-31 21:21:36 +0900

    feat(Application): Lotto 프로그램을 시작하는 Main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.factory.ValidatorFactory;
+import lotto.model.LottoGame;
+import lotto.model.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ValidatorFactory validatorFactory = new ValidatorFactory();
+        OutputView outputView = new OutputView();
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoGame lottoGame = new LottoGame();
+        LottoController lottoController = new LottoController(inputView, validatorFactory, lottoMachine, lottoGame,
+                outputView);
+
+        lottoController.run();
     }
 }
