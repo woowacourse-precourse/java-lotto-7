8ca9bd02a1b80920f54993655dd847ea8c362fa5
IM-GYURI <kgulr0517@gmail.com>
2024-10-31 22:40:41 +0900

    feat : LottoController 추가
    - InputView를 주입받는 생성자

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,12 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+
+    public LottoController(InputView inputView) {
+        this.inputView = inputView;
+    }
+}
