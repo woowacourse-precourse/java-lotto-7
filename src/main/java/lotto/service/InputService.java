c1e0ea1b1d2940730c440ceab46212a229135792
seahwanKim <kimkimseasea@gmail.com>
2024-10-31 16:02:15 +0900

    feat: 당첨 로또 번호를 반환하는 기능을 추가한다

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.validate.BonusNumberValidate;
 import lotto.validate.LottoNumberValidate;
@@ -28,5 +29,11 @@ public class InputService {
     private int bonusNumberValidate() {
         BonusNumberValidate validate = new BonusNumberValidate(inputBonusNumber());
         return validate.getBonusNumber();
+    }
+
+    public Lotto setWinningLotto() {
+        Lotto lotto = new Lotto(lottoNumberValidate());
+        lotto.setBonusNumber(bonusNumberValidate());
+        return lotto;
     }
 }
