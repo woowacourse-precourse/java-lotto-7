361d88b133443820f951ee8d950b2193da7701e8
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-01 01:42:19 +0900

    refactor(AppException): 프리픽스 추가

--- src/main/java/lotto/error/AppException.java
+++ src/main/java/lotto/error/AppException.java
@@ -2,8 +2,10 @@ package lotto.error;
 
 public abstract class AppException extends IllegalArgumentException {
 
+    public static final String PREFIX = "[ERROR] ";
+
     protected AppException(final String errorType) {
-        super(errorType);
+        super(PREFIX + errorType);
     }
 
 }
