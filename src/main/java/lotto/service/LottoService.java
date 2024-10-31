9f22577c30022736ea12a99b94a7e23e1f5dfa05
ChoiDongJoon <adong0808@naver.com>
2024-10-31 14:46:21 +0900

    feat(lotto): Service 계층 분리
    Service 계층 분리

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,14 +1,20 @@
 package lotto.service;
 
+import static lotto.constant.LottoConstant.BONUS_NUMBER_CHECK_CRITERIA;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.Lotto;
+import lotto.dto.LottoMatchResult;
+import lotto.enumerate.Prize;
 
 public class LottoService {
 
-    public ArrayList<Lotto> generateLottoTickets(int lottoAmount) {
-        ArrayList<Lotto> lottoTickets = new ArrayList<>();
+    public List<Lotto> generateLottoTickets(int lottoAmount) {
+        List<Lotto> lottoTickets = new ArrayList<>();
         for (int i = 0; i < lottoAmount; i++) {
             List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lottoTicket = new Lotto(lottoNumbers);
@@ -17,4 +23,69 @@ public class LottoService {
         return lottoTickets;
     }
 
+    public List<Prize> calculateLottoResult(List<Lotto> lottoTickets, Lotto winningLotto, int bonusNumber) {
+        List<Prize> prizes = new ArrayList<>();
+
+        for (Lotto lottoTicket : lottoTickets) {
+            LottoMatchResult lottoMatchResult = matchLottoNums(lottoTicket, winningLotto, bonusNumber);
+
+            int matchedCount = lottoMatchResult.getMatchCount();
+            Boolean bonusMatched = lottoMatchResult.getBonusMatch();
+            Prize prize = calculatePrize(matchedCount, bonusMatched);
+
+            if(prize != null) {
+                prize.incrementCount();
+                prizes.add(prize);
+            }
+        }
+        return prizes;
+    }
+
+    public Map<Prize, Integer> totalPrizeResult(List<Prize> prizes) {
+        Map<Prize, Integer> totalPrizeCount = initPrizeCount();
+        for (Prize prize : prizes) {
+            String prizeName = prize.name();
+            totalPrizeCount.put(prize, totalPrizeCount.get(prize) + 1);
+        }
+        return totalPrizeCount;
+    }
+
+    private LottoMatchResult matchLottoNums(Lotto lottoTicket, Lotto winningLotto, int bonusNumber) {
+        List<Integer> lottoNums = lottoTicket.getNumbers();
+        List<Integer> winningNums = winningLotto.getNumbers();
+
+        int matchCount = 0;
+        for (Integer lottoNum : lottoNums) {
+            if(winningNums.contains(lottoNum)) {
+                matchCount++;
+            }
+        }
+
+        Boolean bounsMatch = false;
+        if(matchCount == BONUS_NUMBER_CHECK_CRITERIA && lottoNums.contains(bonusNumber)) {
+            bounsMatch = true;
+        }
+
+        return new LottoMatchResult(matchCount, bounsMatch);
+    }
+
+    private Prize calculatePrize(int matchCount, Boolean bonusMatch) {
+        for(Prize prize: Prize.values()) {
+            if(prize.getMatchCount() == matchCount && prize.getBonusMatch() == bonusMatch) {
+                return prize;
+            }
+        }
+        return null;
+    }
+
+    private Map<Prize, Integer> initPrizeCount() {
+        Map<Prize, Integer> prizeCount = new HashMap<Prize, Integer>();
+        prizeCount.put(Prize.FIRST, 0);
+        prizeCount.put(Prize.SECOND, 0);
+        prizeCount.put(Prize.THIRD, 0);
+        prizeCount.put(Prize.FOURTH, 0);
+        prizeCount.put(Prize.FIFTH, 0);
+
+        return prizeCount;
+    }
 }
