2e2927d3c2cefaa0019889ffb98928511032ddba
minSsan <msp1125@hanyang.ac.kr>
2024-10-31 22:02:43 +0900

    feat(RevenueRate): 테스트 코드 통과 메서드 구현

--- src/main/java/lotto/domain/rank/vo/RevenueRate.java
+++ src/main/java/lotto/domain/rank/vo/RevenueRate.java
@@ -4,4 +4,9 @@ public record RevenueRate(float value) {
     public static RevenueRate of(int revenue, int expenses) {
         return new RevenueRate(((float) revenue / (float) expenses) * 100);
     }
+
+    @Override
+    public String toString() {
+        return String.format("%.1f%%", value);
+    }
 }
