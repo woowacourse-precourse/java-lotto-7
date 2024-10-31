7287050cabf43ac02e58a13e3d1c090866e90670
lsj0822 <lsj0822@khu.ac.kr>
2024-10-31 23:59:52 +0900

    feat(OutputMessage) : OutputMessage enum 클래스 추가
    OutputMessage enum 클래스를 추가하였습니다
    -OutputMessage enum 클래스는 결과를 출력할 때 필요한 메시지들이 담겨있는 enum 클래스입니다.
    -다만 출력 방식이 일부는 포맷팅이 필요하고, 일부는 메시지만 출력하면 되는 경우가 많아 출력방식에 따라서 enum을 다르게 설정할 예정입니다.

---
+++ src/main/java/view/OutputMessage.java
@@ -0,0 +1,21 @@
+package view;
+
+public enum OutputMessage {
+    
+    INPUT_MONEY_MESSAGE("구입금액을 입력해 주세요."),
+    INPUT_LOTTO_COUNT_MESSAGE("개를 구매했습니다."), //앞에 구매 개수 붙여야함.
+    INPUT_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_WINNING_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    INPUT_WINNING_RESULT_MESSAGE("당첨 통계"),
+    INPUT_BREAK_LINE("---");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(OutputMessage messageType) {
+        return messageType.message;
+    }
+}
