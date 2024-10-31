cd740da55160dce35cffa83d0620812cbd05cd6d
J0YERIM <chosarah70@gmail.com>
2024-11-01 05:05:20 +0900

    feat(view): 당첨 번호 입력 기능 추가
    - InputView에 당첨 번호 입력 기능 추가
    - 입력된 당첨 번호에 대해 유효성 검사를 진행하고, 유효한 입력은 파싱하여 List<Integer>로 반환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.util.InputParser;
+import lotto.util.InputValidator;
 
 public class InputView {
 
@@ -9,5 +12,12 @@ public class InputView {
         String amount = Console.readLine().trim();
         InputValidator.validatePurchaseAmount(amount);
         return Integer.parseInt(amount);
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbers = Console.readLine().trim();
+        InputValidator.validateWinningNumbers(winningNumbers);
+        return InputParser.parseToNumbers(winningNumbers);
     }
 }
