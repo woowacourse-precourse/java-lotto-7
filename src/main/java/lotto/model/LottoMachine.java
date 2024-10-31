db644225932ea8940933e9f5fd8b35f1a7e202c9
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-10-31 13:46:53 +0900

    feat(lottomachine) : setBonusNumber 메서드 구현
    보너스 번호 설정

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -19,4 +19,14 @@ public class LottoMachine {
     public void setWinningLotto(List<Integer> winningNumbers) {
         this.winningLotto = new Lotto(winningNumbers);
     }
+
+    public void setBonusNumbers(int bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        if(winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] : 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
 }
