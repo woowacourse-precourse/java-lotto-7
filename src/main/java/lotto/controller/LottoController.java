684c50385c061819871dfdaf4ecdf9f8f8e98c4e
IM-GYURI <kgulr0517@gmail.com>
2024-10-31 23:01:20 +0900

    feat : LottoController 기능 추가
    startLottoDraw()
    - 로또 추첨 시작이라는 의미의 함수명
    - 외부 호출
    
    getMoney()
    - 구매 금액을 제대로 입력할 때까지 반복
    - Money 반환

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,36 @@
 package lotto.controller;
 
+import lotto.domain.Money;
+import lotto.view.ErrorView;
 import lotto.view.InputView;
 
 public class LottoController {
 
     private final InputView inputView;
+    private final ErrorView errorView;
 
-    public LottoController(InputView inputView) {
+    public LottoController(InputView inputView, ErrorView errorView) {
         this.inputView = inputView;
+        this.errorView = errorView;
+    }
+
+    public void startLottoDraw() {
+        Money money = getMoney();
+    }
+
+    private Money getMoney() {
+        Money money = null;
+
+        while (money == null) {
+            try {
+                String moneyInput = inputView.requestMoneyInput();
+                int parsedMoney = Integer.parseInt(moneyInput);
+                money = new Money(parsedMoney);
+            } catch (NumberFormatException e) {
+                errorView.printMoneyInputError();
+            }
+        }
+
+        return money;
     }
 }
