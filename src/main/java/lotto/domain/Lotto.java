6fd75f16c28888e4c53ee76c9600b4b8e709fed3
GS_song98 <20003204@sju.ac.kr>
2024-11-01 00:14:25 +0900

    feat: 로또 객체 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,57 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumbersSize(numbers);
+        validateDuplicateNumber(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public Lotto(String invalidNumbers) {
+        this(Arrays.stream(invalidNumbers.split(","))
+            .map(Integer::parseInt)
+            .toList());
+    }
+
+    public int getEqualCount(Lotto otherLotto) {
+        List<Integer> otherLottoNumbers = otherLotto.getNumbers();
+        otherLottoNumbers.retainAll(numbers);
+
+        return otherLottoNumbers.size();
+    }
+
+    public boolean isExist(int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
