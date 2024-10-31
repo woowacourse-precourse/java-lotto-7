addffbd561e817bfd45943762450b09974804cd1
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-31 18:59:45 +0900

    feat: 구입 금액 입력 받기 구현
    - 구입 금액을 입력 받는 부분 구현

---
+++ src/main/java/lotto/io/Input.java
@@ -0,0 +1,9 @@
+package lotto.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public final class Input {
+    public static String inputMessage() {
+        return Console.readLine();
+    }
+}
--- src/main/java/lotto/view/PurchaseAmount.java
+++ src/main/java/lotto/view/PurchaseAmount.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
+import lotto.io.Input;
 import lotto.io.Output;
 import lotto.message.IOMessage;
 
 public final class PurchaseAmount {
     public static void purchaseAmount() {
         Output.printlnMessage(IOMessage.INPUT_PURCHASE_AMOUNT.getMessage());
+        String purchaseAmount = Input.inputMessage();
     }
 }
