90a479586dcae70d55c0da41bee2fd6d1b7b127f
unh6unh6 <ruddbs3308@naver.com>
2024-10-31 18:42:23 +0900

    test(WinningNumbers): 당첨 번호에 중복이 있다면 예외가 발생한다

--- src/test/java/lotto/model/WinningNumbersTest.java
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static lotto.ExceptionMessage.WINNING_NUMBER_DUPLICATE_EXCEPTION;
 import static lotto.ExceptionMessage.WINNING_NUMBER_LENGTH_EXCEPTION;
 import static lotto.ExceptionMessage.WINNING_NUMBER_OUT_OF_RANGE_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -32,5 +33,12 @@ public class WinningNumbersTest {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 46, 0)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(WINNING_NUMBER_OUT_OF_RANGE_EXCEPTION.message());
+    }
+
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(WINNING_NUMBER_DUPLICATE_EXCEPTION.message());
     }
 }
