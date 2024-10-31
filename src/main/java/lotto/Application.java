35913f6ee6484c0806652cd2534d855c799cd199
ehgur062300 <ehgur062300@gmail.com>
2024-10-31 15:03:43 +0900

    refactor(Application): Update application execution flow

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.controller.UserController;
 
 public class Application {
@@ -7,7 +8,12 @@ public class Application {
 
     public static void main(String[] args) {
         final UserController userController = UserController.getInstance();
+        final LottoController lottoController = LottoController.getInstance();
 
-        userController.getPurchasePrice(ACCESS_COUNT);
+        // 구입금액 입력
+        int userId = userController.getPurchasePrice(ACCESS_COUNT);
+
+        // 로또 구매
+        lottoController.purchaseLottoTickets(userId);
     }
 }
