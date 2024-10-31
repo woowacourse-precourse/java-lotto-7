f7b11257eb6a31eeca73b584ad654da0482d06a9
kim-do-kyun <zetgi7dk5359@gmail.com>
2024-10-31 20:52:04 +0900

    remove(message) : 에러메시지 클래스 삭제

--- src/main/java/lotto/messages/ExceptionMessage.java
+++
@@ -1,8 +0,0 @@
-package lotto.messages;
-
-public class ExceptionMessage {
-    public static final String INVALID_POSITIVE_NUMBER = "[ERROR] 구입금액은 0보다 큰 숫자여야 합니다.";
-    public static final String INVALID_MULTIPLE_HUNDRED = "[ERROR] 구입금액은 1,000원 단위의 숫자여야 합니다.";
-    public static final String INVALID_TYPE_INPUT = "[ERROR] 구입금액은 숫자만 입력 가능합니다.";
-
-}
