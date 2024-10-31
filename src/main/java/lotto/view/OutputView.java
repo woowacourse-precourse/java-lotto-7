bb78b7afd0d85fd0cd0ef5c8f9b897f6e1cfdf55
alpin87 <qortmdals94@naver.com>
2024-10-31 19:10:12 +0900

    feat: 구매 개수, 로또 번호 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
 
     public void outputPurchaseAmount(int count) {
         System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 }
