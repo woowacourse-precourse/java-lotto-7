ac5493ca76d82a6aea893ec85f29105dff5e25f2
82103 <sangjoon12345@gmail.com>
2024-11-01 00:33:14 +0900

    feat: 로또 구매 요청 메세지 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+public class OutputView {
+    public static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
+    public static final String INFORM_LOTTO_COUNT = "%d개를 구매했습니다.";
+
+    public void requestLottoPrice() {
+        System.out.println(REQUEST_LOTTO_PRICE);
+    }
+
+
+
+}
