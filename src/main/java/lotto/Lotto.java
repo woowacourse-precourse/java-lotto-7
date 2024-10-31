e7a0b71245d6ed680c975c6fca6a080ba684861f
choign3123 <choign3123@naver.com>
2024-10-31 21:56:59 +0900

    feat(Lotto): numbers에 대한 getter 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -43,6 +43,10 @@ public class Lotto {
         };
     }
 
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
