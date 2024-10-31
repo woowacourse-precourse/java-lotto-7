7e8837485e7dda45a45d49aa3a0faf8eecdf89a8
김연진 <love_homebody@naver.com>
2024-10-31 21:13:43 +0900

    feat(LottoGame): 판매할 로또 개수만큼 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
+import lotto.domain.LottoGame;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.RandomLottoGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -16,7 +18,8 @@ public class LottoController {
 
     public void start() {
         PurchaseAmount amount = readPurchasePrice();
-        outputView.printPurchaseAmount(amount.getLottoCount());
+        LottoGame game = new LottoGame(amount, new RandomLottoGenerator());
+        outputView.printPurchaseAmount(game.getLottoCount());
     }
 
     private PurchaseAmount readPurchasePrice() {
---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    private final PurchaseAmount purchaseAmount;
+    private final List<Lotto> lottoes;
+
+    public LottoGame(PurchaseAmount purchaseAmount, LottoGenerator lottoGenerator) {
+        this.purchaseAmount = purchaseAmount;
+        this.lottoes = generateLottoes(lottoGenerator, purchaseAmount.getLottoCount());
+    }
+
+    private List<Lotto> generateLottoes(LottoGenerator lottoGenerator, int count) {
+        List<Lotto> randomLotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            randomLotto.add(lottoGenerator.generate());
+        }
+        return randomLotto;
+    }
+
+    public int getLottoCount() {
+        return purchaseAmount.getLottoCount();
+    }
+}
