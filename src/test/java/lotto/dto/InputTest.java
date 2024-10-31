d8beebc71d712c0daab9cb524f3dfa95c968218b
hgh1472 <hgh1472@naver.com>
2024-11-01 00:39:22 +0900

    test: 당첨 번호 입력 기능 테스트 코드 추가

--- src/test/java/lotto/dto/InputTest.java
+++ src/test/java/lotto/dto/InputTest.java
@@ -2,7 +2,6 @@ package lotto.dto;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +28,28 @@ public class InputTest {
 
         // WHEN - THEN
         assertThatThrownBy(() -> new PurchaseMoneyRequestDTO(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호를 입력받는다.")
+    public void winningNumber() {
+        // GIVEN
+        String input = "1,2,3,4,5,6";
+
+        // WHEN
+        WinningNumberRequestDTO winningNumberRequestDTO = new WinningNumberRequestDTO(input);
+
+        // THEN
+        assertThat(winningNumberRequestDTO.getNumbers()).contains(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @DisplayName("구분 후 숫자가 존재하지 않는 경우, 예외를 발생시킨다.")
+    public void notNumberWinningNumber() {
+        // GIVEN
+        String input = "1,2,3,4,5,a";
+
+        // WHEN - THEN
+        assertThatThrownBy(() -> new WinningNumberRequestDTO(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
