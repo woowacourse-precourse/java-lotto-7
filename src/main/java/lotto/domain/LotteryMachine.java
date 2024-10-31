dfd82e5eb5fc845146042fefe0b70aaa70c59eb7
himinqq <alsrud0966@naver.com>
2024-10-31 13:56:12 +0900

    feat(LotteryMachine): 구입 금액에 해당하는 만큼 로또를 발행하는 기능 구현

--- src/main/java/lotto/domain/LotteryMachine.java
+++ src/main/java/lotto/domain/LotteryMachine.java
@@ -1,8 +1,13 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.utils.LottoNumberGenerator;
 
 public class LotteryMachine {
+    public static final int LOTTO_PRICE = 1000;
+
+    private final List<Lotto> lottos = new ArrayList<>();
     private final LottoNumberGenerator lottoNumberGenerator;
 
     public LotteryMachine(LottoNumberGenerator lottoNumberGenerator) {
@@ -11,5 +16,13 @@ public class LotteryMachine {
 
     public Lotto createLottoTicket(){
         return new Lotto(lottoNumberGenerator.generate());
+    }
+
+    public List<Lotto> createLottoByPayment(Money money){
+        int amount = money.getLottoQuantity();
+        for(int i =0; i<amount; i++){
+            lottos.add(createLottoTicket());
+        }
+        return lottos;
     }
 }
