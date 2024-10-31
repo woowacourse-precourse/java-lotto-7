888aa64dc19c636517cdb9b7391560c2f0b634f4
Nestory <xofks777@naver.com>
2024-10-31 18:12:10 +0900

    feat(StringParser): split input winning numbers to list array

---
+++ src/test/java/lotto/StringParserTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringParserTest {
+
+    @Test
+    public void 쉼표를_기준으로_숫자를_분리한다() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        String inputWinningNumbers = "1,   2,3,4,5,6";
+        for (String winningNumber : inputWinningNumbers.split(",")) {
+            winningNumbers.add(Integer.parseInt(winningNumber.trim()));
+        }
+        assertThat(winningNumbers)
+                .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    public void 당첨번호가_6개가_아닐_경우() {
+
+    }
+}
