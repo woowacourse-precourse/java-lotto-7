3b280e25736a73af92832fdd7c3b0d692cf12ca2
dhrgodms <dhrgodms123@gmail.com>
2024-10-31 21:58:02 +0900

    feat: 당첨_여부_검사 - 당첨 번호와 발행된 로또를 비교하여 당첨 여부를 결정하고 Map에 추가
    - WinningPrize.java: 1등부터 5등 각각에 관련된 문자열을 모아 저장하는 Enum을 추가하였다.
    - WinningTotalController.java
        - calculateWinningTotal(): 당첨 여부를 계산하여 Map으로 반환하는 코드이다.
        - addComparingMap(): 검사 결과를  resultMap에 반영하는 코드이다.
        - makeResultMap(): WinningPrize Enum의 이름을 key로 가지는 map을 만들고 초기화하는 코드이다.

---
+++ src/main/java/lotto/Controller/WinningTotalController.java
@@ -0,0 +1,45 @@
+package lotto.Controller;
+
+import lotto.Model.Lotto;
+import lotto.Model.MyLottos;
+
+import java.util.*;
+
+public class WinningTotalController {
+    public Map<String, Integer> calculateWinningTotal(MyLottos mylottos, Lotto winningLotto, int bonusNumber) {
+        Set<Integer> comparingSet;
+        Map<String, Integer> resultMap = makeResultMap();
+
+        for (Lotto myLotto : mylottos.getLottos()) {
+            comparingSet = new HashSet<>(winningLotto.getNumbers());
+            comparingSet.addAll(myLotto.getNumbers());
+            int matchSize = comparingSet.size() - 6;
+            if (matchSize <= 3) {
+                addResultMap(comparingSet, resultMap, matchSize, bonusNumber);
+            }
+        }
+
+        return resultMap;
+    }
+
+    private void addResultMap(Set<Integer> comparingSet, Map<String, Integer> resultMap, int matchSize, int bonusNumber) {
+        List<String> prizeNames = new ArrayList<>(List.of("FIRST", "SECOND", "FOURTH", "FIFTH"));
+        if (matchSize == 1 && comparingSet.add(bonusNumber)) {
+            resultMap.put("THIRD", resultMap.get("THIRD") + 1);
+        }
+        resultMap.put(prizeNames.get(matchSize), resultMap.get(prizeNames.get(matchSize)) + 1);
+    }
+
+    private Map<String, Integer> makeResultMap() {
+        Map<String, Integer> resultMap = new HashMap<>(
+                Map.of(
+                        "FIRST", 0,
+                        "SECOND", 0,
+                        "THIRD", 0,
+                        "FOURTH", 0,
+                        "FIFTH", 0
+                )
+        );
+        return resultMap;
+    }
+}
