8d42e8eb247e53da5db9b6fe23cb01ce82b45c38
cux-maks <volcano020716@gmail.com>
2024-10-31 14:19:28 +0900

    feat(LottoValidator): 1000으로 나누어 떨어지는지 검사하는 로직 작성
    1000으로 나누어 떨어지는지 아닌지 검사하는 함수를 작성했습니다.

--- src/main/java/validator/LottoValidator.java
+++ src/main/java/validator/LottoValidator.java
@@ -8,4 +8,11 @@ public class LottoValidator {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
+
+    public static Integer isDivisibleByThousand(Integer targetInteger) {
+        if (targetInteger % 1000 != 0) {
+            throw new IllegalArgumentException("1000으로 나누어 떨어지지 않습니다.");
+        }
+        return targetInteger;
+    }
 }
