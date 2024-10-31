7d00f6bf870f4008a1fe2c246e3b6e841ad6a313
JunwooPark <wnsdn1994@gmail.com>
2024-10-31 20:50:31 +0900

    test(lotto): 로또 생성 기능 테스트

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,7 +14,18 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 6개의 번호가 필요합니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 값이어야 합니다.");
+            }
+        }
     }
+
+//    public Collection<Object> getNumbers() {
+//    }
 
     // TODO: 추가 기능 구현
 }
