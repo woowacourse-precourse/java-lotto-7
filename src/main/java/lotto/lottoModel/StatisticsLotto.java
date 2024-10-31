319b5b2300a4a86e6f4c3eacb92c47cd5dfbfebf
ppzepi <hsunguk1999@gmail.com>
2024-11-01 02:56:28 +0900

    feat: StaticsLotto 객체 구현, 맞춘 로또 빈도 수를 저장하는 객체

--- src/main/java/lotto/lottoModel/StatisticsLotto.java
+++ src/main/java/lotto/lottoModel/StatisticsLotto.java
@@ -1,4 +1,37 @@
 package lotto.lottoModel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StatisticsLotto {
+
+    private Map<Integer, Integer> hitNumberFrequency;  // 맞춘 개수 빈도 저장
+    private int bonusNumberFrequency;          // 보너스가 포함된 경우 저장
+
+    public StatisticsLotto() {
+        this.hitNumberFrequency = new HashMap<>();
+        this.bonusNumberFrequency = 0 ;
+    }
+
+    // 맞춘 개수의 빈도를 업데이트하는 메서드
+    public void updateFrequency(int size) {
+        if (size >= 3 && size <= 6) {  // 크기가 3 이상 6 이하일 때만 기록
+            hitNumberFrequency.put(size, hitNumberFrequency.getOrDefault(size, 0) + 1);
+        }
+    }
+
+    // 보너스가 포함된 경우 +1
+    public void addSpecificValue(int value) {
+        bonusNumberFrequency++;
+    }
+
+    // 빈도수 정보를 반환하는 메서드
+    public Map<Integer, Integer> getHitNumberFrequency() {
+        return hitNumberFrequency;
+    }
+
+    // 보너스 정보를 반환하는 메서드
+    public int getBonusNumberFrequency() {
+        return bonusNumberFrequency;
+    }
 }
