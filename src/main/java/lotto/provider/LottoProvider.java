b3a7d585c082daee7ea03980466563741c99c997
minjun <ijmtylhtf@naver.com>
2024-10-31 11:20:29 +0900

    feat : 로또 구입 횟수만큼 로또 번호 추출

--- src/main/java/lotto/provider/LottoProvider.java
+++ src/main/java/lotto/provider/LottoProvider.java
@@ -2,10 +2,18 @@ package lotto.provider;
 
 import static lotto.exception.ExceptionMessage.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
 public class LottoProvider {
 	private static final int THOUSAND = 1000;
 	private final int numberOfLottos;
 	private final int lottoPurchaseAmount;
+
+	private List<Lotto> pickedLottos = new ArrayList<>();
 
 	public LottoProvider(int lottoPurchaseAmount) {
 		validateLottoPurchaseAmount(lottoPurchaseAmount);
@@ -16,6 +24,12 @@ public class LottoProvider {
 	private void validateLottoPurchaseAmount(int lottoPurchaseAmount) {
 		if (lottoPurchaseAmount % THOUSAND != 0) {
 			throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_INPUT.getMessage());
+		}
+	}
+
+	public void pickLottoNumbers() {
+		for (int i = 0; i < numberOfLottos; i++) {
+			List<Integer> pickedLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 		}
 	}
 }
