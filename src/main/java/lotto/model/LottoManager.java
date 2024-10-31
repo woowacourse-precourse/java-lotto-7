a67658acf3d25b0ef73924a70c21fce08e6f5140
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-01 01:51:34 +0900

    feat($model) : make lotties(lotto list) function
    구입 금액만큼 로또를 발권하는 기능 추가

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -1,13 +1,21 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
 public class LottoManager {
-    private List<Lotto> lotties;
+    private final List<Lotto> lotties = new ArrayList<>();
     private Integer money;
     private List<Integer> winLottiesNumber; //Enum 에 대응되는 당첨 로또 개수
+
+    public LottoManager(Integer money) {
+        this.money = money;
+        for (int i = 0; i<(money/1000);i++){
+            this.lotties.add(this.issueLotto());
+        }
+    }
 
     public Lotto issueLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
