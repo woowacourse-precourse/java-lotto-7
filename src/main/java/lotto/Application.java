a69551fccfbdfe04fdb03b6579e2e4edbdda1ceb
zzzyoonnn <dwsrh03@gmail.com>
2024-10-31 14:22:39 +0900

    feat: 로또 프로그램의 Controller 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoMachineController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachineController lottoMachineController = new LottoMachineController();
+        lottoMachineController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoMachineController.java
@@ -0,0 +1,7 @@
+package lotto.controller;
+
+public class LottoMachineController {
+    public void run() {
+        // 로또 발매기 실행 구현하기
+    }
+}
