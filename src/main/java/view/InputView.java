30e2c263a52b65b29cd5ec7070eb4a4b16c6dc39
pakaheo <k70923@konkuk.ac.kr>
2024-10-31 17:21:56 +0900

    feat : Input purchase amount

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,11 @@
-package view;public class InputView {
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
 }
