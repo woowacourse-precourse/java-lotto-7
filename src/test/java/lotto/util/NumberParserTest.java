4943c96cb7559381c9391fb01fc0b9bd2e7e5f25
hanwool <hanwe92@gmail.com>
2024-10-31 15:08:43 +0900

    test(NumberParserTest): Add lotto number validate test

--- src/test/java/lotto/util/NumberParserTest.java
+++ src/test/java/lotto/util/NumberParserTest.java
@@ -1,11 +1,14 @@
 package lotto.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import lotto.view.NumberParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class NumberParserTest {
     static final String LOTTO_NUMBER = "1,2,3,4,5,6";
@@ -14,6 +17,9 @@ class NumberParserTest {
 
     static final String BONUS_NUMBER = "7";
     static final int EXPECTED_BONUS_NUMBER = 7;
+
+    static final String ALPHABET_INPUT = "1,2,3,4,5,a";
+    static final String SPECIAL_CHARACTER_INPUT = "1,2,3,4,5,/";
 
     @Test
     @DisplayName("6자리 로또 번호 파싱 테스트")
@@ -34,12 +40,19 @@ class NumberParserTest {
     }
 
     @Test
+    @DisplayName("보너스 번호 파싱 테스트")
     void testParseBonusNumber() {
         //given,when
         int result = NumberParser.parseBonusNumber(BONUS_NUMBER);
         //then
-        System.out.println(result);
         assertEquals(result, EXPECTED_BONUS_NUMBER);
+    }
+
+    @ParameterizedTest
+    @DisplayName("알파벳과 특수문자가 들어간 로또번호 예외 처리 테스트 ")
+    @ValueSource(strings = {ALPHABET_INPUT, SPECIAL_CHARACTER_INPUT})
+    void testNonNumericLottoNumber(String input) {
+        assertThrows(IllegalArgumentException.class, () -> NumberParser.parseLottoNumbers(input));
     }
 
 }
