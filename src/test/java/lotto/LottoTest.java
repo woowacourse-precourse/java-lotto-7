af95c44e259326262808630cb7647d7d09ac0ed7
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-10-31 15:06:33 +0900

    feat: LottoShop 클래스 생성 및 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 
 class LottoTest {
     @Test
