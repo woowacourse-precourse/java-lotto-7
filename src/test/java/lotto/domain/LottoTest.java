024fca7a04b55bdc3365052f9f8e9a8316e82409
GS_song98 <20003204@sju.ac.kr>
2024-11-01 01:18:49 +0900

    test: 로또 객체 예외 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
     @Test
@@ -21,5 +24,52 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("Lotto는 정렬된 리스트를 가진다.")
+    void whenCreateLottoThenReturnSortedList() {
+        //given
+        List<Integer> expected = List.of(1, 7, 8, 13, 33, 45);
+
+        //when
+        Lotto lotto = new Lotto(List.of(45, 1, 13, 7, 8, 33));
+        List<Integer> actual = lotto.getNumbers();
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Nested
+    @DisplayName("isExist(int number) 기능 테스트")
+    class whenCallIsExist {
+
+        @ParameterizedTest
+        @CsvSource({"1", "4", "8", "23", "33", "45"})
+        @DisplayName("생성된 로또에 값이 들어있는 경우 true를 반환한다.")
+        void thenReturnTrue(int number) {
+            //given
+            Lotto lotto = new Lotto(List.of(1, 4, 8, 23, 33, 45));
+            boolean expected = true;
+
+            //when
+            boolean actual = lotto.isExist(number);
+
+            //then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @ParameterizedTest
+        @CsvSource({"2", "3", "5", "13", "32", "44"})
+        @DisplayName("생성된 로또에 값이 들어있는 경우 true를 반환한다.")
+        void thenReturnFalse(int number) {
+            //given
+            Lotto lotto = new Lotto(List.of(1, 4, 8, 23, 33, 45));
+            boolean expected = false;
+
+            //when
+            boolean actual = lotto.isExist(number);
+
+            //then
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
 }
---
+++ src/test/java/lotto/domain/validator/LottoValidatorTest.java
@@ -0,0 +1,33 @@
+package lotto.domain.validator;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoValidatorTest {
+
+    @Test
+    @DisplayName("로또 숫자로 6개 이상이 들어오면 예외를 발생시킨다.")
+    void whenLengthOver6ThenThrowException() {
+        //given
+        List<Integer> invalidNumbers = List.of(4, 3, 17, 45, 33, 2, 1);
+
+        //when then
+        Assertions.assertThatThrownBy(() -> LottoValidator.validate(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("입력된 숫자 중 중복된 숫자가 있는 경우")
+    void whenDuplicateNumbersThenThrowException() {
+        //given
+        List<Integer> invalidNumbers = List.of(4, 3, 3, 45, 33, 2);
+
+        //when then
+        Assertions.assertThatThrownBy(() -> LottoValidator.validate(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+}
