922f6958283ff3a49ad450b11276ba17585621df
ppzepi <hsunguk1999@gmail.com>
2024-10-31 15:47:46 +0900

    feat: 기초 테스트용 run 구축

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import java.lang.ModuleLayer.Controller;
+import lotto.lottoController.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
+        LottoController controller = new LottoController();
+        controller.run();
     }
 }
