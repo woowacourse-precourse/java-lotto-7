90fc30d17664d494e42c8466e301a28afdf41e6c
GS_song98 <20003204@sju.ac.kr>
2024-11-01 00:19:10 +0900

    feat: 로또 번호를 생성해 가지는 객체 기능 구현

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+
+    private final List<Lotto> lottos;
+
+    public LottoMachine(int amount) {
+        RandomLottoNumberGenerator lottoNumberGenerator = new RandomLottoNumberGenerator();
+
+        this.lottos = IntStream.range(0, amount)
+            .mapToObj(i -> new Lotto(lottoNumberGenerator.generateLottoNumbers()))
+            .toList();
+    }
+
+    public List<String> getLottoNumbers() {
+        return lottos.stream()
+            .map(Lotto::toString)
+            .toList();
+    }
+}
