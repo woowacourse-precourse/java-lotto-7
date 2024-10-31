9ba3cd52c403d489677f8e0bc79b9e68e00712a3
Seonjeongg <sj030205@naver.com>
2024-10-31 21:44:17 +0900

    feat: 로또 발행 개수 출력

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,4 +1,8 @@
 package lotto.View;
 
 public class OutputView {
+    private final static String Lotto_Count_Print="개를 구매했습니다.";
+    public static void lotto_Count_print(int lotto_count){
+        System.out.println(Lotto_Count_Print+lotto_count);
+    }
 }
