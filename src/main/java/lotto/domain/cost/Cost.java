fc43c85b3612bef5086e8c329c398bef27c111a6
minSsan <msp1125@hanyang.ac.kr>
2024-10-31 23:45:20 +0900

    feat(Cost): 테스트 케이스 통과 코드 작성

--- src/main/java/lotto/domain/cost/Cost.java
+++ src/main/java/lotto/domain/cost/Cost.java
@@ -1,12 +1,31 @@
 package lotto.domain.cost;
 
+import lotto.infrastructure.constant.ExceptionMessage;
+import lotto.infrastructure.exception.CustomException;
+
 public record Cost(int value) {
+    public static final int UNIT = 1000;
+    public static final int LOTTO = 1000;
 
     public static Cost of(int cost) {
+        validateCostRange(cost);
+        validateCostUnit(cost);
         return new Cost(cost);
     }
 
     public int getPurchaseCount() {
-        return 0;
+        return value / LOTTO;
+    }
+
+    private static void validateCostRange(int cost) {
+        if (cost <= 0) {
+            throw new CustomException(ExceptionMessage.PURCHASE_LESS_THAN_ONE);
+        }
+    }
+
+    private static void validateCostUnit(int cost) {
+        if (cost % UNIT != 0) {
+            throw new CustomException(ExceptionMessage.INVALID_PURCHASE_UNIT(UNIT));
+        }
     }
 }
--- src/main/java/lotto/infrastructure/constant/ExceptionMessage.java
+++ src/main/java/lotto/infrastructure/constant/ExceptionMessage.java
@@ -3,7 +3,13 @@ package lotto.infrastructure.constant;
 public final class ExceptionMessage {
     public static final String DUPLICATE = "서로 중복된 숫자가 존재합니다.";
 
+    public static final String PURCHASE_LESS_THAN_ONE = "구입 금액은 양의 정수만 입력할 수 있습니다.";
+
     public static String INVALID_LOTTO_COUNT(int count) {
         return String.format("로또 번호는 %d개여야 합니다.", count);
+    }
+
+    public static String INVALID_PURCHASE_UNIT(int unit) {
+        return String.format("구입 금액은 %d 단위의 정수만 가능합니다.", unit);
     }
 }
