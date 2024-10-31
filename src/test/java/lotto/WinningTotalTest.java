d94e149aba68ca4e0c6a5578f029da5a7bfe10b4
dhrgodms <dhrgodms123@gmail.com>
2024-10-31 23:03:24 +0900

    test: 당첨_내역_출력 - 당첨_검사 결과를 출력하는 코드 추가 test
    - WinningTotalTest.java: 당첨 결과를 담은 map을 인자로 받아 당첨 통계를 알맞게 출력하는지 확인하는 테스트 코드를 추가하였다.

--- src/test/java/lotto/WinningTotalTest.java
+++ src/test/java/lotto/WinningTotalTest.java
@@ -5,6 +5,7 @@ import lotto.Controller.IssueTicketController;
 import lotto.Controller.WinningTotalController;
 import lotto.Model.Lotto;
 import lotto.Model.MyLottos;
+import lotto.View.OutputWinningTotalView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,8 @@ import java.util.List;
 import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningTotalTest extends NsTest {
     WinningTotalController winningTotalController = new WinningTotalController();
@@ -19,7 +22,7 @@ public class WinningTotalTest extends NsTest {
 
     @DisplayName("당첨_통계를 올바르게 내면 통과")
     @Test
-    void 당첨_통계를_숫자_리스트로_출력한다() {
+    void 당첨_통계를_숫자_맵으로_출력한다() {
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 7;
 
@@ -38,6 +41,26 @@ public class WinningTotalTest extends NsTest {
                 List.of(7, 11, 30, 40, 42, 43),
                 List.of(2, 13, 22, 32, 38, 45),
                 List.of(1, 3, 5, 14, 22, 45));
+    }
+
+    @DisplayName("당첨_통계_출력을 알맞은 순서대로 하면 통과")
+    @Test
+    void 당첨_통계_출력이_알맞은_값으로_출력된다() {
+        Map<String, Integer> testMap = Map.of("FIRST", 0, "SECOND", 0, "THIRD", 0, "FOURTH", 0, "FIFTH", 1);
+        OutputWinningTotalView outputWinningTotalView = new OutputWinningTotalView(testMap);
+
+        assertSimpleTest(
+                () -> {
+                    outputWinningTotalView.printWinningTotal();
+                    assertThat(output()).contains(
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개"
+                    );
+                }
+        );
     }
 
     @Override
