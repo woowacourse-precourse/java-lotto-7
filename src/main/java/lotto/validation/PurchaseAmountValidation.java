43b66ce8e2b3a3c1e69ecfbc16830f4c787ea391
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-11-01 01:19:43 +0900

    refactor: 함수명 수정 및 접근제어자 수정
    - PurchaseAmountValidation클래스 안의 함수명 수정 및 접근제어자 수정

--- src/main/java/lotto/validation/PurchaseAmountValidation.java
+++ src/main/java/lotto/validation/PurchaseAmountValidation.java
@@ -3,22 +3,21 @@ package lotto.validation;
 import lotto.exception.InvalidPurchaseAmountException;
 
 public final class PurchaseAmountValidation {
-
     public static int purchaseAmountValidationAndGetLottoQuantity(String purchaseAmountStr)
             throws IllegalArgumentException {
         InputValidation.isNotBlank(purchaseAmountStr);
         int purchaseAmount = InputValidation.parseNumberValidation(purchaseAmountStr);
-        purchaseAmountValidation(purchaseAmount);
+        validatePurchaseAmountBy1000(purchaseAmount);
         return convertToLottoQuantity(purchaseAmount);
     }
 
-    public static void purchaseAmountValidation(int purchaseAmount) {
+    private static void validatePurchaseAmountBy1000(int purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
             throw new InvalidPurchaseAmountException();
         }
     }
 
-    public static int convertToLottoQuantity(int purchaseAmount) {
+    private static int convertToLottoQuantity(int purchaseAmount) {
         return purchaseAmount / 1000;
     }
 }
--- src/test/java/lotto/validation/PurchaseAmountValidationTest.java
+++ src/test/java/lotto/validation/PurchaseAmountValidationTest.java
@@ -9,19 +9,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PurchaseAmountValidationTest {
-    @Test
-    @DisplayName("구입 금액은 1,000원 단위가 아닐때 오류 확인하기")
-    public void 구입_금액_1000원_단위() {
-        assertThatThrownBy(() -> PurchaseAmountValidation.purchaseAmountValidation(1200))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @ParameterizedTest
     @ValueSource(strings = {"12000", "2000"})
     @DisplayName("성공하는 구입 금액 입력 확인하기")
     public void 구입_금액_성공_테스트(String purchaseAmount) {
         int expectedAmount = Integer.parseInt(purchaseAmount) / 1000;
-        assertThat(PurchaseAmountValidation.purchaseAmountValidationAndGetLottoQuantity(purchaseAmount)).isEqualTo(expectedAmount);
+        assertThat(PurchaseAmountValidation.purchaseAmountValidationAndGetLottoQuantity(purchaseAmount)).isEqualTo(
+                expectedAmount);
     }
 
     @ParameterizedTest
