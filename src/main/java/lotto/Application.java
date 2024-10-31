df64c94ac4ea4f808f8dd06b44e9df9b2e677fe8
cm <chanmi125128@gmail.com>
2024-10-31 19:04:12 +0900

    feat: 로또 구입 금액 입력 및 처리
    - 사용자로부터 구입 금액을 입력받고 유효성을 검증한다.
      - validateIngerInput 메서드로 유효한 숫자인지 검증
      - validatePurchaseMoeny 메서드로 1,000원 단위 형식 검증
    - while문으로 사용자가 잘못된 값 입력시, 에러 메시지 출력 후 다시 입력 받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,40 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static final String INTEGER_INPUT_ERROR_MESSAGE = "[ERROR] 입력값은 숫자여야 합니다.";
+    public static final String INVALID_MONEY_FORMAT_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                var moneyInput = Console.readLine();
+                var purchaseMoney = validateIntegerInput(moneyInput);
+                validatePurchaseMoney(purchaseMoney);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // TODO: 추가 기능 구현
+    }
+
+    public static Integer validateIntegerInput(String input) {
+        try {
+            var number = Integer.parseInt(input);
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INTEGER_INPUT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validatePurchaseMoney(Integer purchaseMoney) {
+        if (purchaseMoney % 1000 != 0) {
+            throw new IllegalArgumentException(INVALID_MONEY_FORMAT_MESSAGE);
+        }
     }
 }
