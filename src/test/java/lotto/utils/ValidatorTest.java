4d4f750e32e9780adf6a60aa9cd2b58ddf14268a
heehun <slrkqk2@naver.com>
2024-10-31 15:12:40 +0900

    test(Validator): 가격 입력에 문자가 포함되는 경우 예외 추가

--- src/test/java/lotto/utils/ValidatorTest.java
+++ src/test/java/lotto/utils/ValidatorTest.java
@@ -16,4 +16,15 @@ public class ValidatorTest {
         Assertions.assertThatThrownBy(()->Validator.priceValidator(inputPrice))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("priceValidator 메서드 테스트_02")
+    @Test
+    void 가격_입력이_정수가_아니면_예외가_발생한다() {
+        String inputPrice = "1000won";
+
+        Assertions.assertThatThrownBy(()->Validator.priceValidator(inputPrice))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
