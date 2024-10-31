2e8bce070363e30ab3eb0564246f0c148f7e4fc4
EHCTH <lees90896@gmail.com>
2024-10-31 21:25:39 +0900

    test(WinningLottoTest): 예외 추가 및 검증 클래스 정적 메서드 사용

--- src/test/java/lotto/lotto/winning/validator/WinningLottoTest.java
+++ src/test/java/lotto/lotto/winning/validator/WinningLottoTest.java
@@ -1,41 +1,39 @@
 package lotto.lotto.winning.validator;
 
 import lotto.constant.ErrorMessage;
-import lotto.lotto.validator.LottoValidator;
-import lotto.lotto.winning.domain.BonusNumber;
 import lotto.lotto.winning.domain.WinningLotto;
+import lotto.util.ValidationProcess;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.List;
 
 public class WinningLottoTest {
     @Test
     @DisplayName("6자리 숫자 테스트")
     void numberCountTest() {
-        assertThatThrownBy(() -> WinningLotto.of("1,2,3,4,5,6,7"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.NUMBER_COUNT.getMessage());
+        List<String> exceptionData = List.of("1", "1,2", "1,2,3", "1,2,3,4", "1,2,3,4,5", "1,2,3,4,5,6,7");
+        ValidationProcess.createThrownBy(exceptionData, WinningLotto::of, ErrorMessage.NUMBER_COUNT);
     }
 
     @Test
     @DisplayName("중복 테스트")
     void duplicateTest() {
-        verifyExceptionForValidation("1,2,3,4,5,6", "6", ErrorMessage.DUPLICATE);
+        List<String> exceptionData = List.of("1,1,3,4,5,6", "2,2,3,4,5,6", "3,3,3,3,3,3", "11,1,1,1,1,1");
+        ValidationProcess.createThrownBy(exceptionData, WinningLotto::of, ErrorMessage.DUPLICATE);
     }
 
     @Test
     @DisplayName("1 ~ 45까지 범위의 숫자가 아닌경우 예외가 발생한다")
     void withinRangeLottoNumberTest() {
-        verifyExceptionForValidation("4,45,6,7,8,9", "46", ErrorMessage.WITHIN_RANGE);
+        List<String> exceptionData = List.of("45,46,47,48,49,50", "0,1,2,3,4,5", "1,324,113,135,53,2", "1,2,3,4,5,46");
+        ValidationProcess.createThrownBy(exceptionData, WinningLotto::of, ErrorMessage.WITHIN_RANGE);
     }
 
-    private void verifyExceptionForValidation(String winningNumbers, String bonusNumber, ErrorMessage errorMessage) {
-        WinningLotto winningLotto = WinningLotto.of(winningNumbers);
-        BonusNumber bonus = BonusNumber.of(bonusNumber);
-        assertThatThrownBy(
-                () -> LottoValidator.bonusNumberValidate(bonus, winningLotto))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(errorMessage.getMessage());
+    @Test
+    @DisplayName("WinningLotto 생성시 1 ~ 45까지 범위의 숫자가 아닌경우 예외가 발생한다")
+    void createWinningLottoTest() {
+        List<String> exceptionData = List.of("2147483648,2,3,4,5,6", "21474836471,2,3,4,5,6", "2132131232,2,3,4,5,6");
+        ValidationProcess.createThrownBy(exceptionData, WinningLotto::of, ErrorMessage.WITHIN_RANGE);
     }
 }
