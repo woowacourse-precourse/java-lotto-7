975e0ec642473f7252300b9cb1731d519ad41f9b
33jyu33 <jy04169@gmail.com>
2024-10-31 16:40:24 +0900

    feat(Consumer): validate input, get count from input
    Function getCount:
    - validate input
    - change data type of input from string to integer type
    - return total divided by 1000
    Function validatePriceOnlyInteger:
    - validate that non-integer strings are not included
    Function validatePriceScope:
    - validate price is more than 1,000, less than 100,000
    Function validatePriceUnit:
    - validate price in units of 1000

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Consumer {
     private List<Lotto> lotto;
 
-    private void setLotto(List<Integer> numbers){
+    private void setLotto(List<Integer> numbers) {
         Lotto lotto = new Lotto(numbers);
         this.lotto.add(lotto);
     }
@@ -15,4 +15,31 @@ public class Consumer {
     public List<Lotto> getLotto() {
         return this.lotto;
     }
+
+    private int getCount(String priceInput) {
+        validatePriceOnlyInteger(priceInput);
+        int price = Integer.parseInt(priceInput);
+        validatePriceScope(price);
+        validatePriceUnit(price);
+        return price / 1000;
+    }
+
+    private void validatePriceOnlyInteger(String priceInput) {
+        if (!priceInput.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("[ERROR] 금액은 정수만 입력 가능합니다.");
+        }
+    }
+
+    private void validatePriceScope(Integer price) {
+        if (price < 1000 || 100000 < price) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1,000원 이상, 100,000원 이하만 가능합니다");
+        }
+    }
+
+    private void validatePriceUnit(Integer price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위만 가능합니다.");
+        }
+    }
+
 }
