824b36a531d5475a24771256c494ea8146a82d8a
KimYunYeong <intmain0924@naver.com>
2024-10-31 23:11:27 +0900

    feat: Add parsePurchaseAmount method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,14 +3,27 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String purchaseAmount = inputPurchaseAmount();
+        int purchaseAmount = inputPurchaseAmount();
+        //System.out.println("Purchased: " + purchaseAmount);
     }
 
-    public static String inputPurchaseAmount() {
+    public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        String purchaseAmount = Console.readLine();
+        String input = Console.readLine();
+        int purchaseAmount = parsePurchaseAmount(input);
         return purchaseAmount;
+    }
+
+    public static int parsePurchaseAmount(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR_MESSAGE + " 구입금액은 정수형이어야 합니다 : " + input);
+            return -1;
+        }
     }
 }
