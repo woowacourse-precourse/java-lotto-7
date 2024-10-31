5641baa46135f74553e6a2a16d300b3fb931eef8
hyosang <billy04100410@gmail.com>
2024-10-31 14:22:28 +0900

    test(controller): 입력 문자열 분리 로직에 대한 failing test를 작성한다.

---
+++ src/test/java/lotto/WinningNumberTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("Controller 클래스 테스트")
+public class WinningNumberTest {
+
+    @Test
+    void 입력_당첨_번호를_콤마를_기준으로_분리() {
+        List<String> expectedList = Arrays.asList("1", "2", "3", "4", "5", "6");
+
+        //given
+        String input = "1,2,3,4,5,6";
+
+        //when
+        List<String> winnigNumbers = Controller.splitByComma(input);
+
+        //then
+        assertEquals(expectedList, winnigNumbers);
+    }
+}
