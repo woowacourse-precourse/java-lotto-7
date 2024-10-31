27b94da495795fe10e148b51271b014b630177b5
CheChe903 <cheche903@naver.com>
2024-10-31 13:07:34 +0900

    feat(RevenueTest): 수익 객체 단위 테스트 구현
    수익 객체를 위한 단위 테스트를 구현합니다.
    
    1. 초기 금액은 1000단위로 떨어져야 합니다.
    2. 수익률 계산이 잘 되어야합니다.

---
+++ src/test/java/lotto/domain/Person/RevenueTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.Person;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+
+class RevenueTest {
+
+    @Test
+    void 초기금액은_1000으로_나누어떨어지지_않으면_예외를_발생한다() {
+        assertThatThrownBy(() -> new Revenue(new Money(BigInteger.valueOf(1234)), new Money(BigInteger.valueOf(0))))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 수익률은_잘나와야_한다() {
+        //given
+        Revenue revenue = new Revenue(new Money(BigInteger.valueOf(8000)), new Money(BigInteger.valueOf(5000)));
+
+        //when
+        Double profit = revenue.calculateReturnRate();
+
+        //then
+        assertThat(profit).isEqualTo(62.5);
+    }
+
+
+}
