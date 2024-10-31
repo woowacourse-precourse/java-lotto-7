6dc48f757ddcc76196d4c0b63f2668ec3f5013b3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-10-31 21:35:57 +0900

    feat(LottoProfit): 당첨 이익 도메인 구현

---
+++ src/main/java/lotto/LottoProfit.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LottoProfit {
+
+    private final Map<LottoRank, Integer> lottoRanks;
+    private final Money money;
+
+    public LottoProfit(final Map<LottoRank, Integer> lottoRanks, final Money money) {
+        this.lottoRanks = lottoRanks;
+        this.money = money;
+    }
+
+    public double getProfitRatio() {
+        final BigDecimal totalPrice = getTotalPrice();
+        return money.calculateRatio(totalPrice);
+    }
+
+    private BigDecimal getTotalPrice() {
+        return lottoRanks.entrySet().stream()
+                .map(this::calculateTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal calculateTotalPrice(final Entry<LottoRank, Integer> entry) {
+        final int price = entry.getKey().getPrice().intValue();
+        final Integer count = entry.getValue();
+        return BigDecimal.valueOf((long) price * count);
+    }
+}
