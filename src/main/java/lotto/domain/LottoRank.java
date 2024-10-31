5148ce2145dd6c40829ad1a7574f1181c32614e9
rladmstn <oz33177351@gmail.com>
2024-10-31 15:35:01 +0900

    feat(rank) : 로또 랭킹 정보 저장하는 LottoRank 클래스 생성

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public record LottoRank(int rank,
+                        int prizeAmount,
+                        int winningNumberCount,
+                        boolean shouldMatchBonusNumber){
+
+    public boolean isWinning(int numberCount, boolean isMatchedBonusNumber){
+        if(numberCount != winningNumberCount)
+            return false;
+
+        if(!shouldMatchBonusNumber)
+            return true;
+
+        return isMatchedBonusNumber;
+    }
+}
