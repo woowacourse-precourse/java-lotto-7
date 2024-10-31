920c040962856e096ac2179928acac46f382432a
daxxn <dahyeonhan4529@gmail.com>
2024-10-31 17:28:52 +0900

    feat(input): add input class for user input
    - Implement InputView class to manage user input
    - Add readInput method to read input from the console and throw an IllegalArgumentException if the input is empty
    - Add inputValue, inputWinningNumber, inputBonusNumber method to display message and read input

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,40 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.error.ExceptionMessage;
+
+public class InputView {
+    public String readInput(){
+        String line = Console.readLine();
+
+        if(line.isEmpty()){
+            ExceptionMessage errorMessage = ExceptionMessage.ERROR_MESSAGE_INPUT_IS_EMPTY;
+            throw new IllegalArgumentException(ExceptionMessage.createErrorMessage(errorMessage));
+        }
+        return line;
+    }
+
+    public String inputValue(String inputMessage){
+        while(true){
+            try{
+                System.out.println(inputMessage);
+                return readInput();
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public String inputLottoCost(){
+        return inputValue("구입금액을 입력해 주세요.");
+    }
+
+    public String inputWinningNumber(){
+        return inputValue("당첨 번호를 입력해 주세요.");
+    }
+
+    public String inputBonusNumber(){
+        return inputValue("보너스 번호를 입력해 주세요.");
+    }
+}
+
