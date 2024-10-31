c0818bb25199fb4dc848a07c50673f2215fadff1
seongsoo-jeon <stome159@naver.com>
2024-10-31 12:08:45 +0900

    Validate the sum of purchase

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,4 +1,34 @@
 package lotto.View;
 
+import lotto.Model.Exception;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+
+    public int getPurchaseAmount() {
+
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+
+            int result = purNumValidate(readLine());
+            if (result != 0) {
+                return result;
+            }
+        }
+
+    }
+
+    public int purNumValidate(String purAmount) {
+        int result = 0;
+        try {
+            result = Integer.parseInt(purAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(Exception.MONEY_IS_NOT_INTEGER.getErrorMessage());
+        }
+        return result;
+
+
+    }
+
 }
