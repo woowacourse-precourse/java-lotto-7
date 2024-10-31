410765ee11256688864fd27d037ffd2edae9a7ec
dongyun <ehddbs452100@naver.com>
2024-10-31 12:15:48 +0900

    feat(Lotto, LottoRandom, OutputView): generate random lotto numbers
    - 랜덤한 로또를 구입한 로또 갯수만큼 생성하여 오름차순으로 정렬
    - Lotto 타입의 리스트에 앞선 생성한 랜덤한 숫자 6개를 Lotto 객체로 만들어서 값 추가하여 추후에 당첨 번호가 담긴
    Lotto 객체와 비교 용이

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,9 +2,16 @@ package lotto.view;
 
 import static lotto.global.Message.BUY_LOTTO_COUNT_MESSAGE;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
 
     public void printTotalLottoCounts(int lottoCounts) {
         System.out.println("\n" + lottoCounts + BUY_LOTTO_COUNT_MESSAGE.getMsg());
+    }
+
+    public void printLottoRandomNumber(List<Lotto> randomLotto) {
+        randomLotto.stream().map(Lotto::getNumbers).forEach(System.out::println);
     }
 }
