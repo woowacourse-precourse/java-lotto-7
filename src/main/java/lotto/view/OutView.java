165550c2fa6ce5d81d74cc494fd1faf764b4c2c5
supernovaMK <minki5336@naver.com>
2024-11-01 01:52:56 +0900

    refactor: 로또 구입 금액 입력 기능 출력 문구 추가

--- src/main/java/lotto/view/OutView.java
+++ src/main/java/lotto/view/OutView.java
@@ -2,8 +2,14 @@ package lotto.view;
 
 public class OutView {
     public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 
     public void printErrorMessage(String errorMessage) {
         System.out.println(ERROR_MESSAGE + " : " + errorMessage);
     }
+
+    public void printMoneyInputMessage() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+    }
+
 }
