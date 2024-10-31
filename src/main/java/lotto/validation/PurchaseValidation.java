851e4c6ae2dde46b4215e45cdb2b7f8b280d5d00
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-10-31 23:58:14 +0900

    feat: 구입 금액 1000원 단위의 검증
    - 구입 금액이 1000원 단위인지 아니면 예외 처리

--- src/main/java/lotto/validation/PurchaseValidation.java
+++ src/main/java/lotto/validation/PurchaseValidation.java
@@ -1,9 +1,18 @@
 package lotto.validation;
 
+import lotto.exception.InvalidPurchaseAmountException;
+
 public final class PurchaseValidation {
     public static int purchaseAmountValidation(String inputValue) throws IllegalArgumentException {
         InputValidation.isNotBlank(inputValue);
-        int number = InputValidation.parseValidateNumber(inputValue);
-        return number;
+        int amount = InputValidation.parseValidateNumber(inputValue);
+        invalidPurchaseAmountValidation(amount);
+        return amount;
+    }
+
+    public static void invalidPurchaseAmountValidation(int amount) {
+        if (amount % 1000 != 0) {
+            throw new InvalidPurchaseAmountException();
+        }
     }
 }
