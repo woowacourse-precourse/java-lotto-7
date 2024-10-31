df20a7d14054f48086d5eedc5004780b49947206
githublees <leeho_@naver.com>
2024-10-31 18:02:58 +0900

    test(lotto): 로또 기능 관련 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import domain.Lotto;
+import domain.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +25,30 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호_5개만_일치하는_경우() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 7);
+        int bonus = 8;
+
+        assertThat(lotto.getRank(winningNumber, bonus)).isEqualTo(Rank.THIRD_PLACE);
+    }
+
+    @Test
+    void 로또_번호와_5개와_보너스_번호가_일치하는_경우() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 7);
+        int bonus = 6;
+
+        assertThat(lotto.getRank(winningNumber, bonus)).isEqualTo(Rank.SECOND_PLACE);
+    }
+
+    @Test
+    void 로또_번호가_0개_일치하는_경우() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumber = List.of(7, 8, 9, 10, 11, 12);
+        int bonus = 13;
+
+        assertThat(lotto.getRank(winningNumber, bonus)).isEqualTo(Rank.NOTHING);
+    }
 }
