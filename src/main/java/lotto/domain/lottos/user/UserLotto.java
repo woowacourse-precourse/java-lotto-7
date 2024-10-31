221baa97fcf74096fc8ebc25ae9605a4a7654428
sotogito <108535526+sotogito@users.noreply.github.com>
2024-10-31 16:56:43 +0900

    feat : UserLotto bonus가 6자리와 중복될때 bonus부터 입력받기 빌더패턴 적용

--- src/main/java/lotto/domain/lottos/user/UserLotto.java
+++ src/main/java/lotto/domain/lottos/user/UserLotto.java
@@ -10,10 +10,10 @@ public class UserLotto {
     private final BonusLotto bonusLotto;
 
     public UserLotto(Lotto mainLotto, BonusLotto bonusLotto) {
-        validateDuplicateBonusInMainLotto(mainLotto, bonusLotto);
         this.mainLotto = mainLotto;
         this.bonusLotto = bonusLotto;
     }
+
 
     public Lotto getMainLotto() {
         return mainLotto;
@@ -23,21 +23,47 @@ public class UserLotto {
         return bonusLotto;
     }
 
+    public int getMainLottoMatchedCount(final Lotto randomLotto) {
+        return mainLotto.getNumberOfMatches(randomLotto);
+    }
 
-    //fixme 만들기 하기 전에 하는게 좋은가
-    //fixme get 해오는게 편할래나
-    private void validateDuplicateBonusInMainLotto(Lotto mainLotto, BonusLotto bonusLotto){
-        if (bonusLotto.isContainMainLotto(mainLotto)){
-            throw new IllegalArgumentException("보너스 로또 번호가 6자리 로또 번호와 중복됩니다.");
-        }
-
+    public boolean isContainBonusLotto(final Lotto randomLotto) {
+        return bonusLotto.isContainMainLotto(randomLotto);
     }
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(mainLotto);
-        builder.append(bonusLotto);
-        return builder.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append(mainLotto);
+        sb.append(bonusLotto);
+        return sb.toString();
     }
+
+
+    /**
+     * 사용 이유 : BonusLotto가 mainLotto에 중복되어있는지 확인하기위해서는 1. mainLotto을 먼저 입력받고 2. Bonus 객체를 생성시키기 전에
+     */
+    public static class Builder {
+        private Lotto mainLotto;
+        private BonusLotto bonusLotto;
+
+        public Builder() {
+        }
+
+        public void mainLotto(Lotto mainLotto) {
+            this.mainLotto = mainLotto;
+        }
+
+        public void bonusLotto(int bonusLotto) {
+            if (mainLotto.isContainNumber(bonusLotto)) {
+                throw new IllegalArgumentException("보너스 로또 번호가 6자리 로또 번호와 중복됩니다.");
+            }
+            this.bonusLotto = new BonusLotto(bonusLotto);
+        }
+
+        public UserLotto build() {
+            return new UserLotto(mainLotto, bonusLotto);
+        }
+    }
+
 }
