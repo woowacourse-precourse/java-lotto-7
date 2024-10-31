79aeea8de048817b4784a3c4494ecaecab6e6959
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-10-31 12:38:01 +0900

    refactor: LottoResponseWriter 메서드 15라인 이하 요구사항에 맞도록 변경

--- src/main/java/lotto/io/LottoResponseWriter.java
+++ src/main/java/lotto/io/LottoResponseWriter.java
@@ -5,7 +5,6 @@ import lotto.LottoPrize;
 import lotto.dto.LottoResult;
 
 import java.util.List;
-import java.util.Map;
 
 public class LottoResponseWriter {
 
@@ -33,26 +32,15 @@ public class LottoResponseWriter {
         }
 
         double profitRate = (double) totalPrize / lottoResult.userBuyMoney() * 100;
-
         System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 
     private String lottoPrizeKorean(LottoPrize lottoPrize) {
-        if (lottoPrize == LottoPrize.THREE_MATCH) {
-            return "3개 일치 (5,000원)";
-        }
-        if (lottoPrize == LottoPrize.FOUR_MATCH) {
-            return "4개 일치 (50,000원)";
-        }
-        if (lottoPrize == LottoPrize.FIVE_MATCH) {
-            return "5개 일치 (1,500,000원)";
-        }
-        if (lottoPrize == LottoPrize.FIVE_MATCH_WITH_BONUS) {
-            return "5개 일치, 보너스 볼 일치 (30,000,000원)";
-        }
-        if (lottoPrize == LottoPrize.SIX_MATCH) {
-            return "6개 일치 (2,000,000,000원)";
-        }
+        if (lottoPrize == LottoPrize.THREE_MATCH) return "3개 일치 (5,000원)";
+        if (lottoPrize == LottoPrize.FOUR_MATCH) return "4개 일치 (50,000원)";
+        if (lottoPrize == LottoPrize.FIVE_MATCH) return "5개 일치 (1,500,000원)";
+        if (lottoPrize == LottoPrize.FIVE_MATCH_WITH_BONUS) return "5개 일치, 보너스 볼 일치 (30,000,000원)";
+        if (lottoPrize == LottoPrize.SIX_MATCH) return "6개 일치 (2,000,000,000원)";
         throw new RuntimeException("[ERROR] 당첨 결과에 알맞는 메시지가 없습니다.");
     }
 }
