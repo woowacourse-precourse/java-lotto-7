d97c2e84269452c58e732ea74f7fbb2d84cdff0f
parksomii <somi020605@gmail.com>
2024-11-01 02:39:11 +0900

    feat(InputValidate): 숫자 유효성 검사 추가

--- src/main/java/lotto/util/InputValidate.java
+++ src/main/java/lotto/util/InputValidate.java
@@ -1,6 +1,14 @@
 package lotto.util;
 
 public class InputValidate {
+    public void isDigitValidate(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Error.INPUT_DIGIT_ERROR.message());
+        }
+    }
+
     public void blankValidate(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(Error.INPUT_BLANK_ERROR.message());
