ccd0e351738781f4e8ee34ed0230a68059de8263
woogym <kwj9294@gmail.com>
2024-11-01 04:07:17 +0900

    feat(model): Add lotto management logic in lotto model

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,50 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
+import lotto.util.LottoNumberGenerator;
+import lotto.vo.BonusNumber;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public static Lotto createWinningLotto(final List<Integer> winningNumbers) {
+        return new Lotto(winningNumbers);
     }
 
+    public static Lotto createLottoNumber(final LottoNumberGenerator lottoNumberGenerator) {
+        return new Lotto(createNumbersWithGenerator(lottoNumberGenerator));
+    }
 
+    private static List<Integer> createNumbersWithGenerator(final LottoNumberGenerator lottoNumberGenerator) {
+        List<Integer> randomNumbers = lottoNumberGenerator.numberGenerator();
+        return Collections.unmodifiableList(randomNumbers);
+    }
+
+    private void validateLottoNumber(List<Integer> numbers) {
+
+    }
+
+    public int countMatchedNumbers(final Lotto lotto) {
+        return (int) numbers.stream()
+                .filter(lotto::contain)
+                .count();
+    }
+
+    public boolean checkBonusNumberContain(final BonusNumber bonusNumber) {
+        return this.contain(bonusNumber.number());
+    }
+
+    private boolean contain(final Integer number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
 }
