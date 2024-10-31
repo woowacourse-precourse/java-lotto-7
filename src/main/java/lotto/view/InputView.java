605bc7e4c4723513d72789a223bc3fe4ab012985
KangGeonyoung <kky6335@gmail.com>
2024-11-01 02:08:26 +0900

    refactor(InputView): 개행 추가 InputView - getPrizeNumber, getBonusNumber: 개행 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,11 +10,13 @@ public class InputView {
     }
 
     public static String getPrizeNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public static String getBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
