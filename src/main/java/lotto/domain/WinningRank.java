856e0941b69761866394ab71e996ba2b7e75e340
donghyunn1 <dongdong0516@naver.com>
2024-10-31 18:33:48 +0900

    feat: 당첨 순위에 대한 출력 포맷 추가

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum WinningRank {
-    FIRST(6, 2000000000),
-    SECOND(5,30000000),
-    THIRD(5,1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    NONE(0,0);
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - %d개"),
+    SECOND(5,30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    THIRD(5,1500000, "5개 일치 (1,500,000원) - %d개"),
+    FOURTH(4,50000, "4개 일치 (50,000원) - %d개"),
+    FIFTH(3,5000,"3개 일치 (5,000원) - %d개"),
+    NONE(0,0, "0개 일치");
 
     private int numOfMatched;
     private int winningMoney;
+    private String winningString;
 
-    WinningRank(int numOfMatched, int winningMoney) {
+    WinningRank(int numOfMatched, int winningMoney, String winningString) {
         this.numOfMatched = numOfMatched;
         this.winningMoney = winningMoney;
+        this.winningString = winningString;
     }
 
     public static WinningRank getWinningRank(int numOfMatched, boolean isBonusNumber) {
@@ -40,5 +42,9 @@ public enum WinningRank {
 
     public int getWinningMoney() {
         return winningMoney;
+    }
+
+    public String getWinningString() {
+        return winningString;
     }
 }
