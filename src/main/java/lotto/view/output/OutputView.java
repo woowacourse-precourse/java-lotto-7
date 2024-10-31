d5eb4a9591a2dfa58eadb92c868a0a1b7b1b0c36
bowook <mbosx316@naver.com>
2024-11-01 00:39:37 +0900

    feat(OutputView) : OutputView 통계 출력 기능 추가

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -1,12 +1,15 @@
 package lotto.view.output;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 import lotto.domain.User;
 import lotto.validator.exception.LottoException;
 
 public class OutputView {
-
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
     public void displayLottos(User user) {
         int lottosCount = user.getLottos().getLottoCount();
         System.out.printf(OutputMessage.PURCHASED_COUNT_MESSAGE.getOutputMessage(), lottosCount);
@@ -14,8 +17,18 @@ public class OutputView {
             System.out.println(lotto.getNumbers());
         }
     }
-
-
+    public void displayStatistics(List<Integer> statistics) {
+        List<OutputMessage> messages = Arrays.asList(
+                OutputMessage.THREE_MATCHES,
+                OutputMessage.FOUR_MATCHES,
+                OutputMessage.FIVE_MATCHES,
+                OutputMessage.FIVE_MATCHES_PLUS_BONUS,
+                OutputMessage.SIX_MATCHES
+        );
+        for (int i = 0; i < messages.size(); i++) {
+            System.out.printf(messages.get(i).getOutputMessage(), statistics.get(i));
+        }
+    }
     public void displayErrorMessage(LottoException lottoException) {
         System.out.println(lottoException.getMessage());
     }
