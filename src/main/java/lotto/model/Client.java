e784a9712a9fd43f6794da4d5c579c4ca1adfaf8
sanghee0820 <lsh277604@gmail.com>
2024-10-31 23:03:01 +0900

    feat: 구매자 추가

---
+++ src/main/java/lotto/model/Client.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.LottoGenerator;
+
+public class Client {
+    private final Budget budget;
+    private final List<Lotto> lottos;
+
+    public Client(int budget) {
+        this.budget = new Budget(budget);
+        this.lottos = new ArrayList<>();
+    }
+
+    public void buyLotto() {
+        while (budget.buyLotto()) {
+            var randomNumbers = LottoGenerator.generateLotto();
+            Lotto lotto = new Lotto(randomNumbers);
+            lottos.add(lotto);
+        }
+    }
+}
