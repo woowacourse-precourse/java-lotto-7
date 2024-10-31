12b2b9e90b1aa4059da334c514c44c0d847ed235
bowook <mbosx316@naver.com>
2024-11-01 00:36:50 +0900

    feat(ResultService) : ResultService 기능 추가

--- src/main/java/lotto/service/result/ResultService.java
+++ src/main/java/lotto/service/result/ResultService.java
@@ -1,5 +1,10 @@
 package lotto.service.result;
 
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.User;
+
 public class ResultService {
     private final StatisticService statisticService;
     private final ProfitService profitService;
@@ -7,5 +12,9 @@ public class ResultService {
     public ResultService(StatisticService statisticService, ProfitService profitService) {
         this.statisticService = statisticService;
         this.profitService = profitService;
+    }
+
+    public List<Integer> statistics(User user, Lotto winningLotto, BonusNumber bonusNumber) {
+        return statisticService.statisticsCalculator(user, winningLotto, bonusNumber);
     }
 }
