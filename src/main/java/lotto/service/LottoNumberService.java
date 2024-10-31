4926160aa04ec792be87541bb2138150c21c8625
승일 <seoung1515@naver.com>
2024-11-01 03:35:46 +0900

    feat: 로또 번호 생성 기능(예외처리 전)

---
+++ src/main/java/lotto/service/LottoNumberService.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.dto.InputDTO;
+import lotto.dto.LottoDTO;
+import lotto.dto.WinningResultDTO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LottoNumberService {
+    private static final int LOTTO_PRICE = 1000;
+
+    public List<LottoDTO> generateLottos(int money) {
+        int lottoCount = money / LOTTO_PRICE;
+        List<LottoDTO> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new LottoDTO(drawWinningNumbers()));
+        }
+
+        return lottos;
+    }
+
+    public Set<Integer> drawWinningNumbers() {
+        return new HashSet<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+}
