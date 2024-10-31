efe8b5ee55a689a56752aac69a93ac1b0b527f5d
mininiii <didals0521@gmail.com>
2024-10-31 14:19:09 +0900

    feat: 사용자 입력에 대한 숫자 확인 로직 추가

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import java.util.List;
+
 public class InputValidator {
     public static void validateRange(int number) {
         if (number > 45) {
@@ -19,6 +21,22 @@ public class InputValidator {
     public static void isNotNegativeNumber(long purchaseAmount) {
         if (purchaseAmount < 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
+        }
+    }
+
+    public static void isNotNumber(List<String> numbers) {
+        try {
+            numbers.forEach(Integer::parseInt);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 로또 번호는 숫자여야 합니다.");
+        }
+    }
+
+    public static void isNotNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
         }
     }
 }
