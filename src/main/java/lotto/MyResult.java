d52bc17ac094e47bdcba8f5190e2e53a64925ed4
donggundg <rlaehdrjs98@g.skku.edu>
2024-10-31 19:32:26 +0900

    feat(MyResult) : 로또 결과 프린트
    취합한 로또 데이터를 프린트하는 기능.

--- src/main/java/lotto/MyResult.java
+++ src/main/java/lotto/MyResult.java
@@ -27,6 +27,16 @@ public class MyResult {
         return bonusPoint;
     }
 
+    public static void printResults(GradeSaver grades){
+        System.out.println("당첨 통계\n" + "---");
+        System.out.println("3개 일치 (5,000원) - " + grades.getThird() + "개");
+        System.out.println("4개 일치 (50,000원) - " + grades.getFourth() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + grades.getFifth() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " +grades.getFifthBonus() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + grades.getSixth() + "개");
+    }
+
+
 
 
 }
