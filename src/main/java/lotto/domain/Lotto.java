625b1d00cd81b6ed804cda197152350864064c45
이준범 <jach1206@naver.com>
2024-11-01 21:33:10 +0900

    feat(domain): 발급된 로또 번호를 검증하는 메서드 추가
    - validateCount 메서드: 발급된 로또 번호가 6개인지 검증한다.
    - validateRange 메서드: 발급된 로또 번호가 1~45 사이의 값인지 검증한다.
    - validateDuplicate 메서드: 발급된 로또 번호가 중복된 값을 갖고 있진 않은지 검증한다.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.exception.LottoErrorMessage;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateCount(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(LottoErrorMessage.IS_NOT_CORRECT_COUNT.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(LottoErrorMessage.IS_NOT_POSSIBLE_RANGE.getMessage());
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        long count = numbers.stream().distinct().count();
+        if (count != 6) {
+            throw new IllegalArgumentException(LottoErrorMessage.HAVE_DUPLICATED_NUMBER.getMessage());
+        }
+    }
+}
