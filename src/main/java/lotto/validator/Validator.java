b11ffa8174715590e5f6d15713c9d83b6bd38ec1
wgrgwg <kyoeng14697@naver.com>
2024-11-02 01:07:13 +0900

    feat(validator): Add positive number validation

--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -1,22 +1,31 @@
 package lotto.validator;
 
+import lotto.constants.ErrorMessage;
+
 public final class Validator {
     public static void validateMoneyInput(String moneyInput) {
         validateNotBlank(moneyInput);
         validateIsInteger(moneyInput);
+        validateIsPositive(moneyInput);
     }
 
-    private static void validateNotBlank(String moneyInput) {
-        if (moneyInput == null || moneyInput.isBlank()) {
-            throw new IllegalArgumentException("Input is empty");
+    private static void validateNotBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_EMPTY.getMessage());
         }
     }
 
-    private static void validateIsInteger(String moneyInput) {
+    private static void validateIsInteger(String input) {
         try {
-            Integer.parseInt(moneyInput);
+            Integer.parseInt(input);
         } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException("Total round must be integer number");
+            throw new IllegalArgumentException(ErrorMessage.NOT_INTEGER.getMessage());
+        }
+    }
+
+    private static void validateIsPositive(String input) {
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE.getMessage());
         }
     }
 }
