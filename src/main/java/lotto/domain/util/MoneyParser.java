69ef9475cabfe7db320fec94d23fbec27d507917
moonbinchoi <whsahd12@gmail.com>
2024-11-02 01:22:19 +0900

    refactor(MoneyParser): 클래스명 변경
    - 기존: TicketParser
    - 변경: MoneyParser

--- src/main/java/lotto/domain/util/MoneyParser.java
+++ src/main/java/lotto/domain/util/MoneyParser.java
@@ -2,11 +2,11 @@ package lotto.domain.util;
 
 import static lotto.domain.Lotto.TICKET_PRICE;
 
-public final class TicketParser {
+public final class MoneyParser {
 
     private final static String DECIMAL_COMMA = ",";
 
-    private TicketParser() {}
+    private MoneyParser() {}
 
     public static Integer parse(String input) {
         int money = Integer.parseInt(removeComma(input));
