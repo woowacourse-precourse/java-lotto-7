5d834cf0638a8a063a792dfa8f79c4b7c314991a
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 15:41:08 +0900

    feat: 당첨 통계 계산 기능 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.BonusBall;
 import lotto.domain.Lotto;
+import lotto.util.PrizeType;
 
 public class Application {
     public static void main(String[] args) {
@@ -86,6 +87,38 @@ public class Application {
     private static String inputBonusNumber() {
         System.out.println("\n보너스 번호를 입력해주세요.");
         return Console.readLine();
+    }
+
+    private static int[] calculateStatistics(List<Integer> winningNumbers, int bonusNumber, List<List<Integer>> lottoNumbers) {
+        int[] results = new int[PrizeType.values().length];
+
+        for (List<Integer> singleLotto : lottoNumbers) {
+            int matchCount = countMatchingNumbers(singleLotto, winningNumbers);
+            boolean hasBonus = singleLotto.contains(bonusNumber);
+
+            if (matchCount == 6) {
+                results[PrizeType.MATCHING_6.ordinal()]++;
+            } else if (matchCount == 5 && hasBonus) {
+                results[PrizeType.HAS_BONUS_WIN_MATCHING_5.ordinal()]++;
+            } else if (matchCount == 5) {
+                results[PrizeType.MATCHING_5.ordinal()]++;
+            } else if (matchCount == 4) {
+                results[PrizeType.MATCHING_4.ordinal()]++;
+            } else if (matchCount == 3) {
+                results[PrizeType.MATCHING_3.ordinal()]++;
+            }
+        }
+        return results;
+    }
+
+    private static int countMatchingNumbers(List<Integer> singleLotto, List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int number : singleLotto) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
     }
 
 }
