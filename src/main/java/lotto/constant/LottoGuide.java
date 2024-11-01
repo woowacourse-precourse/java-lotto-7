bb19e40ad3bd83c4bb2a8c25f0cc85c8d71cf0f0
33jyu33 <jy04169@gmail.com>
2024-11-01 23:37:46 +0900

    feat(constant): create LottoGuide, add a guide for lotto
    price guide
    purchase count
    winning number guide
    bonus number guide

---
+++ src/main/java/lotto/constant/LottoGuide.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public enum LottoGuide {
+    PRICE_GUIDE("구입금액을 입력해 주세요."),
+    PURCHASE_COUNT("개를 구매했습니다."),
+    WINNING_NUMBER_GUIDE("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_GUIDE("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    private LottoGuide(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
