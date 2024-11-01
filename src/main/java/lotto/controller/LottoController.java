df710da81c50bf0958e5b2f107bd8a25b835cfb4
Dohun Kim <dohun8758@naver.com>
2024-11-02 01:20:52 +0900

    feat: 입력 안내 메시지 출력 로직

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,18 +2,26 @@ package lotto.controller;
 
 import java.util.List;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public LottoController(InputView inputView) {
+    public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run(){
+        outputView.showMoneyInputComments();
         int money = inputView.getMoneyFromUser();
+
+        outputView.showLottoWinningNumberInputComments();
         List<Integer> lottoWinningNumber = inputView.getLottoWinningNumberFromUser();
+
+        outputView.showLottoBonusNumberInputComments();
         int lottoBonusNumber = inputView.getLottoBonusNumberFromUser();
     }
 
