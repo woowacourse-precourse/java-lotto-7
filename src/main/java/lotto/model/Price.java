047f49209d781d5d419dfae53020b976a24f0213
Chan531 <chan657312@gmail.com>
2024-11-01 16:33:19 +0900

    refactor(Price): 구입금액 단위 조건 상수 분리

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 public class Price {
 
+	private static final int PRICE_DIVISIBILITY_UNIT = 1000;
+
 	private final int price;
 
 	public Price(int price) {
@@ -9,7 +11,7 @@ public class Price {
 	}
 
 	private void isDivisibleByThousand(int price) {
-		if (price % 1000 != 0) {
+		if (price % PRICE_DIVISIBILITY_UNIT != 0) {
 			throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.");
 		}
 	}
