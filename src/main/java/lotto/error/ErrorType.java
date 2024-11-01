963e7a52148ffbcedfa4693f47f7c67a27a1dd8b
cherryiJuice <chldlwn711@gmail.com>
2024-11-01 19:55:16 +0900

    refactor: 예외 메세지 추가

--- src/main/java/lotto/error/ErrorType.java
+++ src/main/java/lotto/error/ErrorType.java
@@ -11,7 +11,7 @@ public enum ErrorType {
     private final String message;
 
     ErrorType(final String message) {
-        this.message = "[ERROR]" + message;
+        this.message = "[ERROR] " + message;
     }
 
     public String getMessage() {
