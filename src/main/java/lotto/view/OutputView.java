9631a5a24f10d4df30040f83713ba04b1a502485
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-01 17:01:27 +0900

    refactor($OutputView): 당첨 통계 메시지를 OutputView 가 관리하도록 변경
    - Score 에 출력 메시지를 저장하기 않도록 함에 따른 변경.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,6 @@ package lotto.view;
 import lotto.model.Lotto;
 import lotto.model.Score;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,11 +17,20 @@ public class OutputView {
     public void printScores(Map<Score, Integer> scores) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        Arrays.stream(Score.values()).forEach(score -> {
-            if (!score.equals(Score.ZERO)) {
-                printScore(score, scores.getOrDefault(score, 0));
-            }
-        });
+        System.out.printf("3개 일치 (%,d원) - %d개%n", Score.THREE.getPrize(),
+                scores.getOrDefault(Score.THREE, 0));
+
+        System.out.printf("4개 일치 (%,d원) - %d개%n", Score.FOURTH.getPrize(),
+                scores.getOrDefault(Score.FOURTH, 0));
+
+        System.out.printf("5개 일치 (%,d원) - %d개%n", Score.FIFTH.getPrize(),
+                scores.getOrDefault(Score.FIFTH, 0));
+
+        System.out.printf("5개 일치, 보너스 볼 일치 (%,d원) - %d개%n", Score.FIFTH_WITH_BONUS.getPrize(),
+                scores.getOrDefault(Score.FIFTH_WITH_BONUS, 0));
+
+        System.out.printf("6개 일치 (%,d원) - %d개%n", Score.SIX.getPrize(),
+                scores.getOrDefault(Score.SIX, 0));
     }
 
     public void printProfitRate(double profitRate) {
@@ -31,13 +39,5 @@ public class OutputView {
 
     private void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbers());
-    }
-
-    private void printScore(Score score, int amount) {
-        System.out.printf("%s (%s) - %s개\n", score.getMessage(), formatMoney(score.getPrize()), amount);
-    }
-
-    private String formatMoney(int amount) {
-        return String.format("%,d원", amount);
     }
 }
