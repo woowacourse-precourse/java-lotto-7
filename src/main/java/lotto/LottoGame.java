756ea675c6de742e867e5876b36c2a45e4b551be
haeseon lee <haeseon4816@gmail.com>
2024-11-01 22:26:25 +0900

    feat: 구매 금액 입력 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,7 +1,30 @@
 package lotto;
 
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoGame {
 
+    public int getPurchaseAmount(){
+        System.out.println("구매 금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validateNumericNumber(input);
+        Integer purchaseNumber = Integer.valueOf(input);
+        validateThousandUnit(purchaseNumber);
+        return purchaseNumber;
+    }
 
+    public void validateNumericNumber(String input){
+        for (int i = 0; i < input.length(); i++) {
+            if(!Character.isDigit(input.charAt(i))){
+                throw new IllegalArgumentException("[ERROR] 숫자 이외의 값은 입력할 수 없습니다.");
+            }
+        }
+    }
+
+    public void validateThousandUnit(int purchaseNumber){
+        if(purchaseNumber % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        }
+    }
 }
