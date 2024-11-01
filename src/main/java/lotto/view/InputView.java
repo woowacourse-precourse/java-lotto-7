08c48140e261f94fe771ce5bb4d6d144fbfe549f
jungsu <vwx1@naver.com>
2024-11-01 17:56:08 +0900

    feat(view): InputView에서 메시지 출력 기능 구현
    - 로또 구입 금액, 당첨 번호, 보너스 번호에 대한 메시지 출력 메서드 추가.
    - 각 메시지를 출력하는 printMessage 메서드를 구현하여 코드 중복을 줄임.
    - 사용자에게 각 입력 항목에 대한 프롬프트를 제공하도록 메시지 출력 메서드를 설정.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,41 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView implements InputViewInterface {
 
+  final static String PURCHASE_MESSAGE = "로또 구입 금액을 입력하세요";
+  final static String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력하세요";
+  final static String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력하세요";
+
+  // 모든 메시지를 출력하는 공통 메서드
+  private void printMessage(String message) {
+    System.out.println(message);
+  }
+
+  public void printPurchaseMessage() {
+    printMessage(PURCHASE_MESSAGE);
+  }
+
+  public void printWinningNumbersMessage() {
+    printMessage(WINNING_NUMBERS_MESSAGE);
+  }
+
+  public void printBonusNumberMessage() {
+    printMessage(BONUS_NUMBER_MESSAGE);
+  }
   @Override
   public String readPurchaseAmount() {
+    printPurchaseMessage();
     return Console.readLine();
   }
 
   @Override
   public String readWinningNumbers() {
+    printWinningNumbersMessage();
     return Console.readLine();
   }
 
   @Override
   public String readBonusNumber() {
+    printBonusNumberMessage();
     return Console.readLine();
   }
 }
