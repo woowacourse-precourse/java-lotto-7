470e8bebcb80cedce9f86e0644231cf342e221c9
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-01 18:12:25 +0900

    feat(WinningLotto): 당첨 결과 반환 기능 구현 - 당첨 번호와 발행번호 비교

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -10,6 +10,27 @@ public class WinningLotto {
         this.winNumbers = winNumbers;
         this.bonus = bonus;
     }
+    public LottoResult getMatchResult(Lotto lotto){
+        int matchCount = countMatchingNumbers(lotto);
+        boolean bonusMatched = isBonusMatched(lotto);
+
+        if(matchCount == 6){
+            return LottoResult.SIX;
+        }
+        if(matchCount == 5 && bonusMatched){
+            return LottoResult.FIVE_WITH_BONUS;
+        }
+        if(matchCount == 5){
+            return LottoResult.FIVE;
+        }
+        if(matchCount == 4){
+            return LottoResult.FOUR;
+        }
+        if(matchCount == 3){
+            return LottoResult.THREE;
+        }
+        return null;
+    }
 
     private int countMatchingNumbers(Lotto lotto){
         int count = 0;
