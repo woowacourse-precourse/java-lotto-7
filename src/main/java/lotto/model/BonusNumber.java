8ce269ecc4740e4d86a40610ff16af625bae1299
sik9252 <sik9252@naver.com>
2024-11-01 12:46:14 +0900

    refactor: 보너스 번호 입력 후 검증하는 로직을 View에서 분리, BonusNumber 클래스 내부로 이동

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,13 +1,26 @@
 package lotto.model;
 
+import lotto.exception.InvalidBonusNumberException;
+
+import static lotto.exception.ErrorMessage.INVALID_NUMBER_RANGE;
+import static lotto.model.Lotto.MAX_LOTTO_NUMBER;
+import static lotto.model.Lotto.MIN_LOTTO_NUMBER;
+
 public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
         this.bonusNumber = bonusNumber;
+        validate(bonusNumber);
     }
 
     public int get() {
         return bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new InvalidBonusNumberException(INVALID_NUMBER_RANGE.getMessage());
+        }
     }
 }
