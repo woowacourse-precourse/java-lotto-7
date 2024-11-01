26dbbb735a598e4174dc718b3a1604b95ca477d4
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-02 02:54:41 +0900

    feat(Rank): 현재 등수가 주어진 조건에 부합하는 체크 기능 구현
    일치한 번호 개수와 보너스 번호일치 여부를 받아기준에 부합하다면  True, 부합하지 않다면 False

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -2,7 +2,6 @@ package lotto.constant;
 
 import java.util.function.BiFunction;
 
-//Todo : 더 좋은 이름 찾기
 public enum Rank {
     FIRST(6, false, 2000000000, (count, bonus) -> count == 6),
     SECOND(5, true, 30000000, (count, bonus) -> count == 5 && bonus),
@@ -28,14 +27,13 @@ public enum Rank {
         return matchingNumberCount;
     }
 
-    public boolean isMatchingBonusNumber() {
-        return isMatchingBonusNumber;
-    }
-
-    public int getPrizeMoney() {
-        return prizeMoney;
-    }
-
+    /**
+     * 현재 랭크가 주어진 조건(일치한 번호 개수와 보너스 번호 일치 여부)에 부합하는지 체크
+     *
+     * @param matchedCount 일치한 번호 개수
+     * @param bonus        보너스 번호일치 여부
+     * @return 기준에 부합하다면 True, 부합하지 않다면 False
+     */
     public boolean matchesCriteria(int matchedCount, boolean bonus) {
         return criteria.apply(matchedCount, bonus);
     }
