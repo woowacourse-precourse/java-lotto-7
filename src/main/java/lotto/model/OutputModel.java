baa78f581b7209435d3541c17b8fc88d6f40bf08
mipangg <morganjeong119@gmail.com>
2024-11-02 02:06:59 +0900

    feat(OutputModel): print user lotto

--- src/main/java/lotto/model/OutputModel.java
+++ src/main/java/lotto/model/OutputModel.java
@@ -1,4 +1,14 @@
 package lotto.model;
 
+import lotto.model.UserLotto;
+
+// Output에 필요한 메서드 모음
 public class OutputModel {
+
+    // 유저 로또 리스트를 출력
+    public void showUserLotto(UserLotto userLotto) {
+        for (Lotto lotto : userLotto.getUserLotto()) {
+            System.out.println(lotto);
+        }
+    }
 }
