33557b062acb0134f4e386a04d5f4ba694612f9a
OhYeHyun <ohyhohyl@naver.com>
2024-11-01 21:28:22 +0900

    feat(NumberValidator): 당첨 번호 중복을 확인하는 기능 추가

--- src/main/java/lotto/validator/NumbersValidator.java
+++ src/main/java/lotto/validator/NumbersValidator.java
@@ -1,14 +1,17 @@
 package lotto.validator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lotto.constants.LottoConstants.*;
 
 public class NumbersValidator {
     private enum ErrorMessage {
         MUST_BE_TARGET_LENGTH("%s %s 번호는 %d개이어야 합니다."),
-        MUST_BE_NUMBER("%s 로또 번호는 숫자 형식이어야 합니다.");
+        MUST_BE_NUMBER("%s 로또 번호는 숫자 형식이어야 합니다."),
+        MUST_BE_UNIQUE("%s 로또 번호가 중복되었습니다.");
 
         private final String message;
 
@@ -38,8 +41,17 @@ public class NumbersValidator {
         }
     }
 
+    private void validateIsNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.MUST_BE_NUMBER.getMessage());
+        }
+    }
+
     public void validateWinningNumbers() {
         validateLength("당첨");
+        validateUnique();
     }
 
     public void validateBonusNumber() {
@@ -53,11 +65,16 @@ public class NumbersValidator {
         }
     }
 
-    private void validateIsNumber(String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.MUST_BE_NUMBER.getMessage());
+    private void validateUnique() {
+        int beforeSize = numbers.size();
+        int afterSize = new HashSet<>(numbers).size();
+
+        if (beforeSize != afterSize) {
+            throw new IllegalArgumentException(ErrorMessage.MUST_BE_UNIQUE.getMessage());
         }
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
     }
 }
