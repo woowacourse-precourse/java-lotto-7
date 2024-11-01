c441de2e559737e8478c6cbe1a74deac66208638
junho767 <junho767@naver.com>
2024-11-01 18:59:01 +0900

    refactor: 클래스 멤버의 순서를 상수, 멤버 변수, 생성자, 메서드 순으로 조정

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -1,52 +1,49 @@
 package lotto.model;
 
 public enum LottoRank {
-    FIRST(6,2000000000,"6개 일치 (2,000,000,000원)"),
-    SECOND(5,30000000,"5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(5,1500000,"5개 일치 (1,500,000원)"),
-    FOUR(4,50000,"4개 일치 (50,000원)"),
-    FIVE(3,5000,"3개 일치 (5,000원)"),
-    MISS(0,0,"낙첨");
-
-    LottoRank(int matchCount, int price, String message){
-        this.matchCount = matchCount;
-        this.price = price;
-        this.message = message;
-    }
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    FOUR(4, 50000, "4개 일치 (50,000원)"),
+    FIVE(3, 5000, "3개 일치 (5,000원)"),
+    MISS(0, 0, "낙첨");
 
     private final int matchCount;
     private final int price;
     private final String message;
 
+    LottoRank(int matchCount, int price, String message) {
+        this.matchCount = matchCount;
+        this.price = price;
+        this.message = message;
+    }
 
-
-    public int getMatchCount(){
+    public int getMatchCount() {
         return matchCount;
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return price;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return message;
     }
 
-    public static LottoRank getRank(int matchCount, boolean isBonusNumber){
-        if(SECOND.matchCount(matchCount) && isBonusNumber ){
+    public static LottoRank getRank(int matchCount, boolean isBonusNumber) {
+        if (SECOND.matchCount(matchCount) && isBonusNumber) {
             return SECOND;
         }
-        for(LottoRank rank : values()){
-            if(rank.matchCount(matchCount) && rank != SECOND){
+        for (LottoRank rank : values()) {
+            if (rank.matchCount(matchCount) && rank != SECOND) {
                 return rank;
             }
         }
         return MISS;
     }
 
-    public boolean matchCount(int number){
+    public boolean matchCount(int number) {
         return this.matchCount == number;
     }
 }
-
