3b341e16a8c3bcf95c9281b574e8fa1f979cc1e4
choign3123 <choign3123@naver.com>
2024-11-01 22:46:43 +0900

    feat(ErrorMessage): 중복된 당첨 번호를 입력하였을 때의 에러메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     INSERT_MONEY_NOT_DIVIDED_1000("구입 금액은 1000원 단위로 떨어져야 합니다"),
     WIN_NUMBER_SIZE_MUST_6("당첨 번호는 6개를 입력하여야 합니다"),
     LOTTO_NUMBER_BETWEEN_1_AND_45("로또 번호는 1부터 45 사이의 숫자여야 합니다"),
+    DUPLICATE_WIN_NUMBERS("중복된 당첨 번호를 입력하였습니다"),
     BONUS_NUMBER_DUPLICATE_WIN_NUMBERS("보너스 번호가 당첨 번호와 중복됩니다");
 
     private final String message;
