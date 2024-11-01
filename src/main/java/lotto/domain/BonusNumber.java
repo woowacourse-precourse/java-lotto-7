82fb104cee9b63ed869f9d91f3f9c1f0289eca48
eb9862 <luclead622@gmail.com>
2024-11-01 22:01:56 +0900

    feat: 보너스 번호 입력에 대한 검증부 구현
    - 숫자인지 여부와 범위, 당첨 번호와의 중복 여부 확인

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private static final String BONUS_NUMBER_NOT_NATURAL_NUMBER = "[ERROR] 보너스 번호가 자연수가 아닙니다.";
+    private static final String BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBERS = "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.";
+
+    private final int number;
+
+    public BonusNumber(String number, WinningNumbers numbers) {
+        validate(number, numbers);
+        this.number = Integer.parseInt(number);
+    }
+
+    private void validate(String number, WinningNumbers numbers) {
+        try {
+            int bonusNumber = Integer.parseInt(number);
+            Lotto.checkNumberRange(bonusNumber);
+            checkDuplicatedWithWinningNumbers(bonusNumber, numbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BONUS_NUMBER_NOT_NATURAL_NUMBER);
+        }
+    }
+
+    private void checkDuplicatedWithWinningNumbers(int bonusNumber, WinningNumbers numbers) {
+        Lotto winningNumbers = numbers.getNumbers();
+        if (winningNumbers.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBERS);
+        }
+    }
+}
