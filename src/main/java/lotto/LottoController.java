c537a5cd944e52cc0e651a01a19a5a663dff3540
Revi1337 <david122123@gmail.com>
2024-11-01 20:35:30 +0900

    feat: 구입 금액 입력 안내 메시지 출력 기능 구현

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.io.OutputHandler;
+
 public class LottoController {
 
+    private final OutputHandler outputHandler = new OutputHandler();
+
     public void play() {
+        outputHandler.showPriceInputNavigateMessage();
     }
 }
---
+++ src/main/java/lotto/io/OutputHandler.java
@@ -0,0 +1,10 @@
+package lotto.io;
+
+public class OutputHandler {
+
+    private static final String PRICE_INPUT_NAVIGATE = "구입금액을 입력해 주세요.";
+
+    public void showPriceInputNavigateMessage() {
+        System.out.println(PRICE_INPUT_NAVIGATE);
+    }
+}
