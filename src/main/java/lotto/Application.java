3a63dc9f0659fe611866314c50ff24149ed29152
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-02 01:13:36 +0900

    refactor(Application) : 변수 타입 변경
    기존 Wrapper Type로 정의되어있던 변수들을 메모리 절약을 위해 Primitive Type으로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,28 +1,23 @@
 package lotto;
 
-import lotto.View.Controller;
 import lotto.View.OutputView;
 
 import java.util.*;
-
-import static java.lang.Math.round;
 
 public class Application {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
-        Integer purchasePrice = controller.gainPurchaseAmount();
-        Integer lottoCount = controller.getLottoCount();
+        int purchasePrice = controller.gainPurchaseAmount();
+        int lottoCount = controller.getLottoCount();
         List<Lotto> myLottos = Lotto.sortLottoList(lottoCount);
         Lotto answer = controller.getWinningInput();
-        Integer bonus = controller.gainBonusInput(answer);
+        int bonus = controller.gainBonusInput(answer);
         List<MyResult> myResults = WinningDetails.saveMyGrades(myLottos, answer, bonus);
         WinningDetails winningDetails = new WinningDetails();
         winningDetails.sumUpGrades(myResults);
         OutputView.printResults(winningDetails);
-        Integer revenue = MyResult.getMyRevenue(winningDetails);
+        int revenue = MyResult.getMyRevenue(winningDetails);
         OutputView.printReturn(purchasePrice, revenue);
     }
-
-
 }
