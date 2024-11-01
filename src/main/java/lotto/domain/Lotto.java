d891fb0bcd041ef5d31132aed5072db2083194a8
eb9862 <luclead622@gmail.com>
2024-11-01 16:45:49 +0900

    feat: 로또 번호의 숫자 범위와 중복을 확인하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,16 @@
 package lotto.domain;
 
+import static lotto.domain.LottoInfo.END_NUMBER;
+import static lotto.domain.LottoInfo.START_NUMBER;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final String NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호의 숫자 범위를 벗어납니다.";
+    private static final String NUMBER_DUPLICATED = "[ERROR] 번호에 중복이 존재합니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,10 +22,31 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        for (int number : numbers) {
+            checkNumberRange(number);
+        }
+        checkNumberDuplicated(numbers);
     }
 
     // TODO: 추가 기능 구현
     public void showNumbers() {
         System.out.println(numbers);
+    }
+
+    public static void checkNumberRange(int number) {
+        if (number < START_NUMBER || number > END_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE);
+        }
+    }
+
+    static void checkNumberDuplicated(List<Integer> validRangeNumbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(validRangeNumbers);
+        if (distinctNumbers.size() != validRangeNumbers.size()) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATED);
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
