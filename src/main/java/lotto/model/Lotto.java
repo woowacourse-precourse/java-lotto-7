7b145cf9773719d30f030c7060481cc54f3ddc32
Beomseok Noh <prodigy0831@naver.com>
2024-11-01 21:03:45 +0900

    feat(Lotto): 중복 번호 방지 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,13 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final String ERROR_INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String ERROR_DUPLICATE_NUMBERS = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,6 +16,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+        checkDuplicates(numbers);
+
+    }
+
+    private void checkDuplicates(List<Integer> numbers) {
+        Set<?> set = new HashSet<>(numbers);
+        if (numbers.size() != set.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void checkSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
