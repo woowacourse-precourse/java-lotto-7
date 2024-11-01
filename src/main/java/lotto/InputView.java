10045847fd7c7d9e20573b476b1efc3f7e051acd
Hajuuu <hpt0127@naver.com>
2024-11-01 16:44:55 +0900

    feat: 당첨 번호 입력 예외 처리 구현
    입력받은 당첨 번호가 1~45 사이의 숫자가 아니면 예외를 발생시키는 메서드 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,11 +2,15 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class InputView {
 
     private static final int AMOUNT_UNIT = 1000;
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String ERROR_MESSAGE_AMOUNT = "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.";
+    private static final String ERROR_MESSAGE_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
 
     public int inputPurchaseAmount() {
@@ -25,5 +29,24 @@ public class InputView {
             throw new IllegalArgumentException(ERROR_MESSAGE_AMOUNT);
         }
         return amount;
+    }
+
+    public void inputLottoNumbers() {
+        Lotto lotto = new Lotto(
+                Arrays.stream(readLine().split(",")).map(num -> checkInputLottoNumbers(num))
+                        .collect(Collectors.toList()));
+    }
+
+    public int checkInputLottoNumbers(String input) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_NUMBER);
+        }
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_NUMBER);
+        }
+        return number;
     }
 }
