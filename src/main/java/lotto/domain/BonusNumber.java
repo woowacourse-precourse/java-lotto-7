28c516c1a9aa1f50d248dd6be45b91fa78a544cf
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-01 23:21:32 +0900

    refactor(BonusNumber): 검증하는 로직에서 예외 메시지를 제외한 매직 넘버를 상수화

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -18,6 +18,10 @@ public class BonusNumber {
 
     private static class Validator {
 
+        private static final String BONUS_NUMBER_NUMERIC_REGEX = "-?\\d+";
+        private static final int MINIMUM_WINNING_NUMBER = 1;
+        private static final int MAXIMUM_WINNING_NUMBER = 45;
+
         private static int validateBonusNumber(String bonusNumber) {
             validateWinningNumbersIsNotEmpty(bonusNumber);
             int numericBonusNumber = validateBonusNumberIsNumeric(bonusNumber);
@@ -32,14 +36,14 @@ public class BonusNumber {
         }
 
         private static int validateBonusNumberIsNumeric(String bonusNumber) {
-            if (!bonusNumber.matches("-?\\d+")) {
+            if (!bonusNumber.matches(BONUS_NUMBER_NUMERIC_REGEX)) {
                 throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
             }
             return Integer.parseInt(bonusNumber);
         }
 
         private static void validateBonusNumberInRange(int bonusNumber) {
-            if (bonusNumber < 1 || bonusNumber > 45) {
+            if (bonusNumber < MINIMUM_WINNING_NUMBER || bonusNumber > MAXIMUM_WINNING_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자만 입력 가능합니다.");
             }
         }
