7976d63366aac34fe3bc64b31cdb9d8a5e749431
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-01 16:26:15 +0900

    feat : 당첨번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,8 +15,7 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String inputPrice = Console.readLine();
 
-        boolean isNumeric = inputPrice.chars().allMatch(Character::isDigit);
-        if (!isNumeric) {
+        if (!isNumeric(inputPrice)) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 양수로 입력해 주세요.");
         }
 
@@ -34,5 +35,39 @@ public class Application {
             System.out.println(lotto.printLotto());
         }
         System.out.println();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbersInput = Console.readLine().trim();
+        if (!winningNumbersInput.matches("^\\d+(,\\d+)*$")) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 쉼표(,)를 기준으로, 6개 입력해 주세요.");
+        }
+        String[] inputNumbers = winningNumbersInput.split(",");
+        if (inputNumbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 쉼표(,)를 기준으로, 6개 입력해 주세요.");
+        }
+
+        Set<Integer> winningNumbers = new TreeSet<>();
+        for (String inputNumber : inputNumbers) {
+            if (!isNumeric(inputNumber)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 양수로 입력해 주세요.");
+            }
+
+            int number = Integer.parseInt(inputNumber);
+            if (number < 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 음수일 수 없습니다.");
+            }
+            if (number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            winningNumbers.add(number);
+        }
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복일 수 없습니다.");
+        }
+
+    }
+
+    private static boolean isNumeric(String inputPrice) {
+        return inputPrice.chars().allMatch(Character::isDigit);
     }
 }
