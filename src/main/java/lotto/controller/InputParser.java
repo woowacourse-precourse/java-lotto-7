690af58f1546441fbae516c4815a7502166cc8a7
kungbi <kipper12335@gmail.com>
2024-11-01 22:35:18 +0900

    refactor(InputParser): cause error 처리 및 배열이 비어있을 경우 예외 처리

--- src/main/java/lotto/controller/InputParser.java
+++ src/main/java/lotto/controller/InputParser.java
@@ -10,9 +10,13 @@ public class InputParser {
 
         List<Integer> integers = null;
         try {
-            integers = Arrays.stream(split).map(Integer::parseInt).toList();
+            integers = Arrays.stream(split).map(String::strip).map(Integer::parseInt).toList();
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.", e);
+        }
+
+        if (integers.isEmpty()) {
+            throw new IllegalArgumentException("최소 하나의 숫자는 입력해야 합니다.");
         }
 
         return integers;
@@ -24,7 +28,7 @@ public class InputParser {
         try {
             integer = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.", e);
         }
 
         return integer;
