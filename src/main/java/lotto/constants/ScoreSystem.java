4b90a6ac7bc3d794b18d96e2fcbc30283162f9a3
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-01 16:08:52 +0900

    feat: 점수 계산 로직 구현

---
+++ src/main/java/lotto/constants/ScoreSystem.java
@@ -0,0 +1,28 @@
+package lotto.constants;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public enum ScoreSystem {
+
+    DEFAULT_SCORE_SYSTEM(new HashMap<>() {{
+        put(List.of(3, 0), 0);
+        put(List.of(4, 0), 0);
+        put(List.of(5, 0), 0);
+        put(List.of(5, 1), 0);
+        put(List.of(6, 0), 0);
+    }});
+
+    private final Map<List<Integer>, Integer> scoringMap;
+
+    ScoreSystem(Map<List<Integer>, Integer> scoringMap) {
+        this.scoringMap = scoringMap;
+    }
+
+    //방어적 복사
+    public HashMap<List<Integer>, Integer> getScoringMap() {
+        return new HashMap<>(scoringMap);
+    }
+
+}
---
+++ src/main/java/lotto/domain/score/Score.java
@@ -0,0 +1,52 @@
+package lotto.domain.score;
+
+import lotto.constants.ScoreSystem;
+import lotto.domain.BonusComponent;
+import lotto.domain.Component;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Score {
+
+    private HashMap<List<Integer>, Integer> scoreSystem ;
+
+    public Score(HashMap<List<Integer>, Integer> scoreSystem) {
+        this.scoreSystem = scoreSystem;
+    }
+
+    public HashMap<List<Integer>, Integer> checkScore(Lottos lottos, Lotto winningLotto, BonusComponent bonusComponent) {
+        scoreSystem.replaceAll((key, value) -> 0);
+
+        List<Lotto> mylottos = new ArrayList<>(lottos.getLottos());
+        List<Component> winningComponent = new ArrayList<>(winningLotto.getComponents());
+
+        scoring(bonusComponent, mylottos, winningComponent);
+
+        return new HashMap<>(scoreSystem);
+    }
+
+    private void scoring(BonusComponent bonusComponent, List<Lotto> mylottos, List<Component> winningComponent) {
+        for(Lotto lotto: mylottos){
+            Integer matches = (int) lotto.getComponents()
+                    .stream()
+                    .filter(winningComponent::contains)
+                    .count();
+            Integer bonusMatch = 0;
+            if(lotto.getComponents().contains(bonusComponent)){
+                bonusMatch ++;
+            }
+            List<Integer> key = Arrays.asList(matches, bonusMatch);
+
+            if (scoreSystem.containsKey(key)) {
+                scoreSystem.put(key, scoreSystem.get(key) + 1);
+            }
+
+        }
+    }
+}
