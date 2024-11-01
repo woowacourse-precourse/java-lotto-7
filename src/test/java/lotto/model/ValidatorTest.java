648110dfc7d73e1a91d599bbd55ea8f0979e4b9d
soeun2537 <soeun2537@gmail.com>
2024-11-01 22:10:49 +0900

    test: 로또 구입 금액 유효성 검사 - 배수 아님 예외 단위 테스트 추가

--- src/test/java/lotto/model/ValidatorTest.java
+++ src/test/java/lotto/model/ValidatorTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,17 @@ class ValidatorTest {
         // when & then
         assertThatCode(() -> Validator.validateLottoPurchaseAmount(lottoPurchaseAmount))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("로또 구입 금액 유효성 검사: 배수 아님 - 예외 테스트")
+    void validateLottoPurchaseAmount_notMultiple() {
+        // given
+        Integer lottoPurchaseAmount = 2500;
+
+        // when & then
+        assertThatThrownBy(() -> Validator.validateLottoPurchaseAmount(lottoPurchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 }
