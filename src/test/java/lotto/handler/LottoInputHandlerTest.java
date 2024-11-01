26399a3b0ad534c12f0e3488b8b88518c5ebc143
Park Yu Han <pbk2312@naver.com>
2024-11-01 16:13:10 +0900

    test(handler): LottoInputHandlerTest에 DisplayName 추가 및 예외 처리 테스트 주석 추가
    - 각 테스트 메서드에 DisplayName 어노테이션을 추가하여 테스트 목적을 명확히 표시

--- src/test/java/lotto/handler/LottoInputHandlerTest.java
+++ src/test/java/lotto/handler/LottoInputHandlerTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoInputHandlerTest {
@@ -19,6 +20,7 @@ public class LottoInputHandlerTest {
     }
 
     @Test
+    @DisplayName("구매 금액 입력 테스트")
     void testGetPurchaseAmount() {
         String nextInput = "5000";
         inputView.setNextInput(nextInput);
@@ -29,6 +31,7 @@ public class LottoInputHandlerTest {
     }
 
     @Test
+    @DisplayName("당첨 번호 입력 테스트")
     void testGetWinningNumbers() {
         String nextInput = "1,2,3,4,5,6";
         inputView.setNextInput(nextInput);
@@ -39,6 +42,7 @@ public class LottoInputHandlerTest {
     }
 
     @Test
+    @DisplayName("보너스 번호 입력 테스트")
     void testGetBonusNumber() {
         String nextInput = "7";
         inputView.setNextInput(nextInput);
@@ -71,10 +75,4 @@ public class LottoInputHandlerTest {
             return nextInput;
         }
     }
-
-    // LottoInputHandlerTest 에서 예외 처리를 테스트할 수 없는 이유:
-    // getInputWithValidation 메서드가 무한 루프를 포함하고 있어서 유효하지 않은 입력이 주어질 경우
-    // 계속해서 입력을 요구하게 됩니다.이로 인해 테스트가 멈추지 않고 무한 반복될 수 있습니다.
-    // 그래서 현재로서는 이 부분을 테스트하기가 어려운 것 같습니다.
-
 }
