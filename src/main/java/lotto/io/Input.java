d4e3b192adc5aa771148f89470b1cf56c5405d8c
minjun <ijmtylhtf@naver.com>
2024-11-01 19:13:52 +0900

    feat : 로또 당청 번호 메시지 입력 받는 함수 구현

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 	private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+	private static final String ANSWER_LOTTO_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
 
 	public static String readPurchaseAmount() {
 		System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
 		return Console.readLine();
+	}
+
+	public static String readAnswerLotto() {
+		System.out.println(ANSWER_LOTTO_INPUT_MESSAGE);
+		return Console.readLine();; 
 	}
 }
