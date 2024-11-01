1ea9497d1aaaf070dce9617e8c3f9f4bdfdf7b8e
minjun <ijmtylhtf@naver.com>
2024-11-01 19:38:08 +0900

    feat : 로또 상금 기준 목록 구현

---
+++ src/main/java/lotto/answer/LottoRank.java
@@ -0,0 +1,18 @@
+package lotto.answer;
+
+public enum LottoRank {
+	FIRST(6, false, 2000000000),
+	SECOND(5, true, 30000000),
+	THIRD(5, false, 1500000),
+	FOURTH(4, false, 50000),
+	FIFTH(1, false, 5000); 
+	private final int matchCount;
+	private final boolean bonusMatch;
+	private final int prizeAmount;
+
+	LottoRank(int matchCount, boolean bonusMatch, int prizeAmount) {
+		this.matchCount = matchCount;
+		this.bonusMatch = bonusMatch;
+		this.prizeAmount = prizeAmount;
+	}
+}
