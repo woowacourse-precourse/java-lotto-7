9b07b15d95b8ec633c0cbd96b649340fa219b137
joshinhyeok <shtree07@naver.com>
2024-11-02 02:04:15 +0900

    refactor: OutputView 클래스 하드코딩된 문자열들 상수로 변환

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,43 +4,41 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+    private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String DIVIDER = "---";
+    private static final String MATCH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String MATCH_MESSAGE = "%d개 일치 (%s원) - %d개";
+    private static final String TOTAL_RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
     public static void printLottos(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매했습니다." );
+        System.out.printf((PURCHASE_MESSAGE) + "%n", lottos.size());
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
     }
 
     public static void printWinningStatistics(Map<LottoRank, Integer> statistics) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(DIVIDER);
 
         for (LottoRank rank : LottoRank.values()) {
             if (rank == LottoRank.NO_PRIZE) continue;
-            if (rank == LottoRank.SECOND_PRIZE) {
-                System.out.printf(
-                        "%d개 일치, 보너스 볼 일치 (%s원) - %d개%n",
-                        rank.getMatchCount(),
-                        formatPrize(rank.getPrizeAmount()),
-                        statistics.getOrDefault(rank, 0)
-                );
-                continue;
-            }
-
-            System.out.printf(
-                    "%d개 일치 (%s원) - %d개%n",
-                    rank.getMatchCount(),
-                    formatPrize(rank.getPrizeAmount()),
-                    statistics.getOrDefault(rank, 0)
-            );
+            int matchCount = rank.getMatchCount();
+            String prizeAmount = rank.getFormatPrize();
+            int count = statistics.getOrDefault(rank, 0);
+            System.out.println(WinningStatisticsMessage(rank,matchCount,prizeAmount,count));
         }
     }
 
-    private static String formatPrize(int prizeAmount) {
-        return String.format("%,d", prizeAmount); // 상금을 1,000 단위로 콤마 추가
+    private static String WinningStatisticsMessage(LottoRank rank, int matchCount, String prizeAmount, int count) {
+        if (rank == LottoRank.SECOND_PRIZE) {
+            return String.format(MATCH_BONUS_MESSAGE, matchCount, prizeAmount, count);
+        }
+        return String.format(MATCH_MESSAGE, matchCount, prizeAmount, count);
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", rateOfReturn);
+        System.out.printf(TOTAL_RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }
