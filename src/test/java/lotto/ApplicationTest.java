4a433c8dcc3116d667e465f2cdf4e5f63db5a36b
KF97 <aysul0@naver.com>
2024-11-01 20:24:33 +0900

    test: 보너스 번호가 숫자가 아닌 입력이 들어오면 예외 테스트 코드 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -103,6 +103,16 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @DisplayName("보너스 번호가 숫자가 아닌 입력이 들어오면 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_숫자가_아닌_입력이_들어오면_예외() {
+        assertSimpleTest(() -> {
+           runException("1000", "1,2,3,4,5,6", "abc");
+           assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
