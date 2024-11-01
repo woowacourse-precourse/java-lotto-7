9ffc530fcde22e338ef6aa43b62c3e4cebe79b3d
ChiSaeHwang <cwh730909@gmail.com>
2024-11-01 15:38:34 +0900

    feat(LottoService): LottoService 메서드 정리

--- src/main/java/lotto/domain/lotto/service/LottoService.java
+++ src/main/java/lotto/domain/lotto/service/LottoService.java
@@ -1,6 +1,11 @@
 package lotto.domain.lotto.service;
 
+import lotto.domain.lotto.dto.request.LottoGameReq;
+import lotto.domain.lotto.dto.response.LottoResultRes;
+
 public interface LottoService {
 
+    void createAndPlayGame(LottoGameReq request);
 
+    LottoResultRes getGameResult();
 }
