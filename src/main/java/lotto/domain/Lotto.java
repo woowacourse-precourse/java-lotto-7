70ccab4aec99a48d994a8ff0c82bfa67b13c3b30
yush <yush2202@gmail.com>
2024-11-01 19:06:46 +0900

    feat($ Lotto): 로또 비교 메서드 구현
    로또 간의 비교를 위해 로또 내에 비교 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.function.Predicate;
 import lotto.constants.ErrorMessages;
 
 public class Lotto {
@@ -13,6 +14,17 @@ public class Lotto {
 
     public boolean hasNumber(int number) {
         return numbers.contains(number);
+    }
+
+    public int countDuplication(Lotto lotto) {
+        return numbers.stream()
+                .filter(x -> lotto.getLotto().stream()
+                        .anyMatch(Predicate.isEqual(x)))
+                .toList().size();
+    }
+
+    public List<Integer> getLotto() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
