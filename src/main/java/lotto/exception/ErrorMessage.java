18edec4ffa139081ee620967064f283dbde423a7
wogus <ind07152@naver.com>
2024-11-01 13:05:02 +0900

    feat(ErrorMessage): 로또 번호 범위 에러 메시지 추가
    ErrorMessage애 로또 번호 범위 에러 메시지를 추가한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,10 +2,11 @@ package lotto.exception;
 
 public enum ErrorMessage {
     INVALID_LOTTO_COUNTS("로또 번호는 6개여야 합니다."),
-    INVALID_LOTTO_DUPLICATE("로또 번호는 중복될 수 없습니다.");
+    INVALID_LOTTO_DUPLICATE("로또 번호는 중복될 수 없습니다."),
+    INVALID_LOTTO_RANGE("로또 번호는 1에서 45사이의 숫자이여야합니다.");
 
     private static final String PREFIX = "[ERROR] ";
-    private String message;
+    private final String message;
 
     public String getMessage() {
         return this.message;
