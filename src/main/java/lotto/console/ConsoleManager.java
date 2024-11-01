6392319b896a3285c70950fc203a5656679af476
ufo9430 <ufo9430@naver.com>
2024-10-31 23:28:00 -0400

    feat: 콘솔 출력 함수를 관리하는 클래스 생성

--- src/main/java/lotto/console/ConsoleManager.java
+++ src/main/java/lotto/console/ConsoleManager.java
@@ -1,2 +1,10 @@
-package lotto.console;public class ConsoleManager {
+package lotto.console;
+
+public class ConsoleManager {
+    public static void printf(String input, Object... args){
+        System.out.printf(input, args);
+    }
+    public static void println(String input){
+        System.out.println(input);
+    }
 }
