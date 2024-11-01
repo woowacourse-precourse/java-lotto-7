02b8508fd9ddd46692fd4a989617895546fea966
kim <hwiyul_g@daum.net>
2024-11-01 21:13:31 +0900

    feat: 필요한 의존성 주입 및 프로그램 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        LottoService lottoService = new LottoService();
+
+        Controller controller = new Controller(inputView, outputView, lottoService);
+        controller.run();
     }
 }
