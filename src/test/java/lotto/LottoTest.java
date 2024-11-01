36c6c4d1511d4b8d479b22172ec193a6d847de22
ehgur062300 <ehgur062300@gmail.com>
2024-11-01 17:12:57 +0900

    test(LottoTest): Add test cases for exception handling

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
 import lotto.collection.Lotto;
+import lotto.collection.WinningNumbers;
+import lotto.domain.LottoResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -33,6 +36,23 @@ class LottoTest {
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호에_문자가_있으면_예외가_발생한다() {
+        String[] array = {"1", "2", "3", "a", "5", "6"};
+        assertThatThrownBy(() -> new WinningNumbers(array))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"6", "", " ", "a", "0", "46"})
+    void 보너스_번호_유효성_검사_예외테스트(String bonusNumber) {
+        String[] array = {"1", "2", "3", "4", "5", "6"};
+        WinningNumbers winningNumbers = new WinningNumbers(array);
+
+        assertThatThrownBy(() -> new LottoResult(winningNumbers,bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
