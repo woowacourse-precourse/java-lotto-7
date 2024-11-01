84c39e7d3d9a650fe9c8bee8e551552107dfbfb7
배석현 <bjh4966@naver.com>
2024-11-01 12:01:20 +0900

    당첨 번호 및 보너스 번호 입력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,9 +7,19 @@ public class Application {
         // 1. 구입 금액 입력
         int amount = InputPrice.getAmount();
         System.out.println("price: "+ amount);
+
         // 2. 로또 발행
         LottoMachine lottoMachine = new LottoMachine();
         List<Lotto> lottos = lottoMachine.buyLottos(amount);
         OutputView.displayLottos(lottos);
+
+        // 3. 당첨 번호 및 보너스 번호 입력
+        List<Integer> winningNumbers = InputPrice.getWinningNumbers();
+        int bonusNumber = InputPrice.getBonusNumber();
+
+        //확인
+        System.out.println("win num: "+ winningNumbers);
+        System.out.println("bonus num: "+ bonusNumber);
+
     }
 }
