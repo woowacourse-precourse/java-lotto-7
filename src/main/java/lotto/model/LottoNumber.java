2622184481389767885a28a936d66c5e95071c7f
Chan531 <chan657312@gmail.com>
2024-11-01 17:45:03 +0900

    feat(LottoNumber): 로또 번호 유효성 검증
    1 미만이거나 45 초과일 시, 예외 발생

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -8,6 +8,13 @@ public class LottoNumber {
 	private final int number;
 
 	public LottoNumber(int number) {
+		checkLottoNumberRange(number);
 		this.number = number;
+	}
+
+	private void checkLottoNumberRange(int number) {
+		if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 1 이상 45 이하이어야 합니다.");
+		}
 	}
 }
