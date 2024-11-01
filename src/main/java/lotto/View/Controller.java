8e57f0282ac07c4ae45f6574af5057f1833c34e8
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 21:57:51 +0900

    refactor(Controller) : 출력 함수 반영

--- src/main/java/lotto/View/Controller.java
+++ src/main/java/lotto/View/Controller.java
@@ -12,13 +12,7 @@ import java.util.stream.Collectors;
 import static lotto.View.InputView.*;
 
 public class Controller {
-
-    public static final String LOTTO_AMOUNT_PHRASE = "구입금액을 입력해 주세요.";
-    public static final String LOTTO_COUNT_PHRASE = "개를 구매했습니다.";
-    public static final String LOTTO_WINNING_INPUT = "당첨 번호를 입력해 주세요.";
-    public static final String LOTTO_BONUS_INPUT = "보너스 번호를 입력해 주세요.";
     public static final String DELIMITER = ",";
-    public static final String NUM = "개";
 
     private Integer lottoCount;
 
@@ -31,7 +25,7 @@ public class Controller {
             this.lottoCount = this.countLotto(purchasePrice);
         }
         catch(IllegalArgumentException e){
-            System.out.println(ErrorMessage.ONLY_NUMBER.getError());
+            OutputView.printError(ErrorMessage.ONLY_NUMBER.getError());
             return gainPurchaseAmount();
         }
         return purchasePrice;
@@ -42,18 +36,14 @@ public class Controller {
         if (input % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.NOT_DIV.getError());
         }
-        System.out.println(lottoCount + LOTTO_COUNT_PHRASE);
+        OutputView.printCount(lottoCount);
         return lottoCount;
     }
 
-    public static void printLotto(Lotto lotto){
-        System.out.println(lotto.getNumbers());
-    }
-
     public Lotto getWinningInput(){
-        System.out.println(LOTTO_WINNING_INPUT);
+        OutputView.printWinning();
         String rawWinningInput = Console.readLine();
-        System.out.println();
+        OutputView.printBlank();
         Lotto answer;
         try {
             answer = new Lotto(Arrays.asList(rawWinningInput.split(DELIMITER)).stream()
@@ -61,16 +51,16 @@ public class Controller {
                     .collect(Collectors.toList()));
         }
         catch (IllegalArgumentException e){
-            System.out.println(ErrorMessage.WIN_INPUT.getError());
+            OutputView.printError(ErrorMessage.WIN_INPUT.getError());
             return getWinningInput();
         }
         return answer;
     }
 
     public Integer gainBonusInput(Lotto answer){
-        System.out.println(LOTTO_BONUS_INPUT);
+        OutputView.printBonus();
         String rawBonus  = Console.readLine();
-        System.out.println();
+        OutputView.printBlank();
         Integer bonus = 0;
         try{
             bonus = parseInt(rawBonus);
@@ -78,7 +68,7 @@ public class Controller {
             checkRange(bonus);
         }
         catch (IllegalArgumentException e){
-            System.out.println(ErrorMessage.BONUS.getError());
+            OutputView.printError(ErrorMessage.BONUS.getError());
             return gainBonusInput(answer);
         }
         return bonus;
