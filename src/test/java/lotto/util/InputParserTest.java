1babcc67ba9305cb7a04bb5f2f02d2d5ac8efea5
woong2e <tlswodnd12345@naver.com>
2024-11-01 16:23:52 +0900

    test: add test for exception on non-numeric input

---
+++ src/test/java/lotto/util/InputParserTest.java
@@ -0,0 +1,20 @@
+package lotto.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputParserTest {
+
+    @Test
+    @DisplayName("로또 번호를 입력할 때 숫자를 입력하지 않으면 예외가 발생한다.")
+    void 로또_번호를_입력할_때_숫자를_입력하지_않으면_예외가_발생한다() {
+        String input = "1,2,삼,4,5,6";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> InputParser.parseNumbers(input));
+    }
+
+}
