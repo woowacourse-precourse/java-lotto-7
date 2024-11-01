3ead4d8ca2646955e4f6083bef7261f7407038b6
minSsan <msp1125@hanyang.ac.kr>
2024-11-01 16:33:43 +0900

    feat(Rank): 순위 정보 및 prize 필드 getter 추가

--- src/main/java/lotto/domain/rank/vo/Rank.java
+++ src/main/java/lotto/domain/rank/vo/Rank.java
@@ -1,19 +1,23 @@
 package lotto.domain.rank.vo;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public enum Rank {
-    FIRST(6, 0, 2_000_000_000),
-    SECOND(5, 1, 30_000_000),
-    THIRD(5, 0, 1_500_000),
-    FOURTH(4, 0, 50_000),
-    FIFTH(3, 0,5_000);
+    FIRST(1, 6, 0, 2_000_000_000),
+    SECOND(2, 5, 1, 30_000_000),
+    THIRD(3, 5, 0, 1_500_000),
+    FOURTH(4, 4, 0, 50_000),
+    FIFTH(5, 3, 0, 5_000);
 
+    private final int rank;
     private final int match;
     private final int bonus;
     private final int prize;
 
-    private Rank(int match, int bonus, int prize) {
+    private Rank(int rank, int match, int bonus, int prize) {
+        this.rank = rank;
         this.match = match;
         this.bonus = bonus;
         this.prize = prize;
@@ -24,6 +28,17 @@ public enum Rank {
                 .filter(rank -> rank.match == match && rank.bonus == bonus)
                 .findFirst()
                 .orElse(null);
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static List<Rank> getSortedRanks() {
+        return Arrays.stream(values())
+                .sorted(Comparator.comparingInt(value -> value.rank))
+                .toList()
+                .reversed();
     }
 
     @Override
