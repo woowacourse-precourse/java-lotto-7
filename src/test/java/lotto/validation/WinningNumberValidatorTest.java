ee4d7c3f1ace93284ff4807445d961b99842c1f7
Sinorang <sinholang@naver.com>
2024-11-01 11:25:47 +0900

    test: 당첨 번호 개수 검증 테스트 추가

--- src/test/java/lotto/validation/WinningNumberValidatorTest.java
+++ src/test/java/lotto/validation/WinningNumberValidatorTest.java
@@ -1,2 +1,19 @@
-package lotto.validation;public class WinningNumberValidatorTest {
+package lotto.validation;
+
+import lotto.constants.ErrorMessageConstants;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningNumberValidatorTest {
+    @Test
+    void 당첨_번호_개수_검증_테스트() {
+        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5);
+
+        assertThatThrownBy(() -> WinningNumberValidator.validateWinningNumbers(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.INVALID_WINNING_NUMBER_COUNT);
+    }
 }
