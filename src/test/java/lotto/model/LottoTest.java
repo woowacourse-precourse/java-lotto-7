3f7b8993282a7dda9aece235f256876722deb989
seophohoho <seop0937@gmail.com>
2024-11-02 01:11:46 +0900

    test(Lotto): Lotto numbers 오름차순 테스트 코드 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,11 @@
 package lotto.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoTest {
 //    @Test
 //    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
@@ -13,6 +19,13 @@ class LottoTest {
 //        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
 //                .isInstanceOf(IllegalArgumentException.class);
 //    }
+
+    @Test
+    @DisplayName("Lotto numbers 오름차순 테스트")
+    void testSortNumbers() {
+        Lotto lotto = new Lotto(List.of(43, 21, 42, 41, 23, 8));
+        assertEquals(List.of(8, 21, 23, 41, 42, 43), lotto.getNumbers());
+    }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
