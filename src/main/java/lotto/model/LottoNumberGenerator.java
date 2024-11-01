c7222c04a28055c44ec8832d83066c60d827fc0d
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-11-01 19:42:14 +0900

    refactor(LottoNumberGenerator): 가독성을 위해 줄바꿈

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,10 +1,17 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
+
+import static lotto.constants.ModelContstants.*;
 
 public class LottoNumberGenerator {
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(
+                LOTTO_STARTING_RANGE.getValue(),
+                LOTTO_END_RANGE.getValue(),
+                LOTTO_COUNT.getValue()
+        );
     }
 }
