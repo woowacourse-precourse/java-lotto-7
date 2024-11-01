7677b366ff125c1dc17c6938d12145f855293358
201912025 <michi2012@naver.com>
2024-11-01 13:37:25 +0900

    feat: 로또 범위 검증 기능 추가

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,15 +1,23 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import lotto.utils.RandomNumbersSelector;
+import lotto.utils.SortUtils;
 
-import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
 
     public List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+        List<Integer> lottoNumbers = RandomNumbersSelector.selectRandomNumbers();
+        validateLottoRange(lottoNumbers);
+        return SortUtils.sortNumbers(lottoNumbers);
+    }
+
+    private void validateLottoRange(List<Integer> lottoNumbers) {
+        for (Integer number : lottoNumbers) {
+            if (number < 1 | number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다. 잘못된 번호: " + number);
+            }
+        }
     }
 }
