8b77d5769e5e3ee51c2fabe9d7337cfa02177557
parksaegyeol <dmsgkrkt1@naver.com>
2024-11-01 19:00:01 +0900

    test : LottoProcess에 테스트 코드 작성
    로또 번호 중 당첨된 번호의 개수를 반환하는 테스트 작성
    보너스 번호가 당첨번호들 중 포함되는지 여부를 반환하는 테스트 작성
    출력할 당첨내역의 특정 당첨 개수의 수치를 1증가시키는 테스트 작성

--- src/test/java/lotto/model/LottoProcessTest.java
+++ src/test/java/lotto/model/LottoProcessTest.java
@@ -3,6 +3,7 @@ package lotto.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class LottoProcessTest {
@@ -13,6 +14,40 @@ class LottoProcessTest {
         List<Integer> lottoNumbers = lottoProcess.getRandomLottoNumbers();
         System.out.println(lottoNumbers);
         Assertions.assertThat(lottoNumbers.size()).isEqualTo(6);
+
+    }
+
+    @Test
+    void 당첨_번호_개수_통계_테스트() {
+        List<Integer> lottoNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = new ArrayList<>(List.of(7, 8, 9, 10, 1, 2));
+        LottoProcess lottoProcess = new LottoProcess();
+        int result = lottoProcess.calculateWinningCount(lottoNumbers, winningNumbers);
+        System.out.println("winningStatistics = " + lottoProcess.getWinningStatistics());
+        System.out.println(lottoProcess.getWinningStatistics());
+
+
+        Assertions.assertThat(result).isEqualTo(2);
+    }
+
+
+    @Test
+    void 보너스_번호가_당첨번호들에_포함여부() {
+        LottoProcess lottoProcess = new LottoProcess();
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        boolean result = lottoProcess.validateBonusNumInWinningNumbers(6, winningNumbers);
+        Assertions.assertThat(result).isEqualTo(true);
+
+    }
+
+    @Test
+    void addWinningStatisticsTest() {
+        LottoProcess lottoProcess = new LottoProcess();
+        lottoProcess.addWinningStatisticValue(0);
+        lottoProcess.addWinningStatisticValue(3);
+        lottoProcess.addWinningStatisticValue(4);
+
+        System.out.println("lottoProcess.getWinningStatistics() = " + lottoProcess.getWinningStatistics());
 
     }
 }
