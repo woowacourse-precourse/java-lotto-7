0d09b8978d3d9578fa74566b7a55e898189329ad
soeun2537 <soeun2537@gmail.com>
2024-11-02 00:54:23 +0900

    feat: 입력 문자열 정수로 변환 로직 추가

--- src/main/java/lotto/model/Parser.java
+++ src/main/java/lotto/model/Parser.java
@@ -1,7 +1,18 @@
 package lotto.model;
 
+import static lotto.constant.message.ErrorMessage.*;
+
 public class Parser {
 
     public static Integer parseInputToInt(String input) {
+        return convertStringToInteger(input);
+    }
+
+    private static Integer convertStringToInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE.getMessage());
+        }
     }
 }
