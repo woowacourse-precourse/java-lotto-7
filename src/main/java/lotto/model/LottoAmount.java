b7e4d7d1e1597610a463a92508e946287dfb70dd
junho767 <junho767@naver.com>
2024-11-01 18:56:18 +0900

    feat: 입력된 문자열을 정수로 변환하고 유효성 검사 수행

--- src/main/java/lotto/model/LottoAmount.java
+++ src/main/java/lotto/model/LottoAmount.java
@@ -4,12 +4,10 @@ import lotto.Message.ErrorMessage;
 
 public class LottoAmount {
     private static final int LOTTO_PRICE = 1000;
-    private static final String NUMBER_FORMAT = "^[0-9]+$";
     private final int amount;
 
     public LottoAmount(String input) {
-        validateNumber(input);
-        int amount = Integer.parseInt(input);
+        int amount = validateNumber(input);
         validateAmount(amount);
         this.amount = amount;
     }
@@ -18,9 +16,11 @@ public class LottoAmount {
         return amount / LOTTO_PRICE;
     }
 
-    private void validateNumber(String input) {
-        if (!input.matches(NUMBER_FORMAT)) {
-            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.toString());
+    private int validateNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER.toString());
         }
     }
 
