29b9aee16f64bd19d73ad25ecc8d4c8d8b9dcc83
Changsu Park <gd12091@gmail.com>
2024-11-02 00:05:51 +0900

    fix: 불변 리스트 활용

--- src/main/java/lotto/service/GenerateRandomNumbers.java
+++ src/main/java/lotto/service/GenerateRandomNumbers.java
@@ -1,13 +1,14 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class GenerateRandomNumbers {
 
     public List<Integer> generateLottoNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(numbers);
         return numbers;
     }
