284093650203c04801996797c1d38dcc76e5ab97
hanwool <hanwe92@gmail.com>
2024-11-01 22:07:06 +0900

    fix(InputView): Change return value
    Change return value from string to List<Integer> to call itself when error happened

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,9 +2,23 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
-public class InputView {
+import java.util.List;
 
-    public static String getInput(String question) {
+public class InputView {
+    private static final String LOTTO_NUMBER_INPUT_QUESTION = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_QUESTION = "보너스 번호를 입력해 주세요.";
+
+    public static List<Integer> getLottoNumber() {
+        String input = askInput(LOTTO_NUMBER_INPUT_QUESTION);
+        try {
+            return InputParser.parseLottoNumbers(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getLottoNumber();
+        }
+    }
+
+    private static String askInput(String question) {
         System.out.println(question);
         return readLine();
     }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -3,20 +3,48 @@ package lotto.view;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import camp.nextstep.edu.missionutils.Console;
 
 class InputViewTest {
+    private static final String NORMAL_LOTTO_INPUT = "1,2,3,4,5,6";
+    private static final String ZERO_LOTTO_INPUT = "1,2,3,4,5,0";
+    private static final List<Integer> NORMAL_LOTTO_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
 
+    @BeforeEach
+    void setUp() {
+        Console.close(); // 이전 Scanner 인스턴스 닫기
+    }
+
+    @DisplayName("정상적인 로또 번호 입력 테스트")
     @Test
-    void testInputNumber() {
-        //given
-        String input = "10";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        new Scanner(System.in);
-        //when
-        String result = InputView.getInput(input);
-        //then
-        assertThat(result).isEqualTo(input);
+    void testLottoNumberInput() {
+        // given
+        System.setIn(new ByteArrayInputStream((NORMAL_LOTTO_INPUT).getBytes()));
+
+        // when
+        List<Integer> result = InputView.getLottoNumber();
+
+        // then
+        assertThat(result).isEqualTo(NORMAL_LOTTO_NUMBERS);
+    }
+
+    @DisplayName("0 포함 로또 번호 입력 테스트")
+    @Test
+    void testLottoNumberInputWithZero() {
+        // given
+        // Scanner의 nextLine()은 줄바꿈까지 전까지 읽고 다시 읽을 때 그 이후 부터 읽음
+        String combinedInput = ZERO_LOTTO_INPUT + "\n" + NORMAL_LOTTO_INPUT;
+        System.setIn(new ByteArrayInputStream(combinedInput.getBytes()));
+
+        // when
+        List<Integer> result = InputView.getLottoNumber();
+
+        // then
+        assertThat(result).isEqualTo(NORMAL_LOTTO_NUMBERS);
     }
 }
