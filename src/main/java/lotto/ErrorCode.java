93afd44d454259eb26ea62c71c7b81cf80e74271
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-01 17:26:15 +0900

    feat: 에러 메세지를 저장하는 Enum 생성

---
+++ src/main/java/lotto/ErrorCode.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public enum ErrorCode {
+    MONEY_TYPE_ERROR("[ERROR] 금액은 숫자여야 합니다.");
+
+    private final String message;
+
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
