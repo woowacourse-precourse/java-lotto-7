291e4d2031e56a33a66248b19878c99ac6b319b0
soeun2537 <soeun2537@gmail.com>
2024-11-02 05:21:10 +0900

    test: 정적 팩토리 메서드 of로 Lotto 객체를 생성하는지 확인하는 단위 테스트 추가

---
+++ src/test/java/lotto/model/domain/LottoTest.java
@@ -0,0 +1,29 @@
+package lotto.model.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("정적 팩토리 메서드 of로 Lotto 객체를 생성하는지 확인")
+    void of() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = Lotto.of(numbers);
+
+        // then
+        assertThat(lotto.getNumbers().get(0)).isEqualTo(1);
+        assertThat(lotto.getNumbers().get(1)).isEqualTo(2);
+        assertThat(lotto.getNumbers().get(2)).isEqualTo(3);
+        assertThat(lotto.getNumbers().get(3)).isEqualTo(4);
+        assertThat(lotto.getNumbers().get(4)).isEqualTo(5);
+        assertThat(lotto.getNumbers().get(5)).isEqualTo(6);
+    }
+}
