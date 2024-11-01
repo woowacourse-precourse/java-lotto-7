59afa513593ecc09ca52f31077c3648bfe265a59
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 15:35:32 +0900

    feat(LottoController): PrintDetails 추가

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -8,21 +8,25 @@ import lotto.view.OutputView;
 import java.util.List;
 
 public class LottoController {
+    private static PurchaseAmount purchaseAmount;
     public LottoController(){
     }
-    private static PurchaseAmount purchaseAmount;
 
     public void run(){
         String amount_str =InputView.purchaseAmount();
         PlayerAmount(amount_str);
     }
     public void PlayerAmount(String amount_str){
-        PurchaseAmount purchaseAmount = new PurchaseAmount(amount_str);
-        LottoList(purchaseAmount.calculateLottoCount());
+        purchaseAmount = new PurchaseAmount(amount_str);
+        PrintDetails();
     }
     public static void LottoList(int ticketCount){
         for(int i=0;i<ticketCount;i++){
             OutputView.printLottoList(RandomLotto.randomLottoNumber());
         }
+    }
+    public void PrintDetails(){
+        OutputView.printCount(purchaseAmount.calculateLottoCount());
+        LottoList(purchaseAmount.calculateLottoCount());
     }
 }
