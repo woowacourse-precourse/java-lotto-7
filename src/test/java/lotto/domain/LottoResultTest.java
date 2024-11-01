dce01f9ded12b957fa459a11f3a1afbdab7cdd42
ppparkta <pppppark2@gmail.com>
2024-11-01 19:52:50 +0900

    test(LottoResultTest): 보너스번호 테스트 추가
    추가 사항
    - 보너스 번호가 당첨 번호와 중복일 때 예외 확인하는 테스트 추가
    - 보너스 번호가 1~45이내 정수가 아닐 때 예외 확인하는 테스트 추가

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoResultTest {
+    @DisplayName("보너스 번호에 당첨 번호와 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 보너스번호_중복_숫자_예외() {
+        assertThatThrownBy(() -> new LottoResult(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1~45 사이의 정수가 아니라면 예외가 발생한다.")
+    @Test
+    void 보너스번호_1_45_범위가_아니면_예외() {
+        assertThatThrownBy(() -> new LottoResult(List.of(1, 2, 3, 4, 5, 6), 46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
