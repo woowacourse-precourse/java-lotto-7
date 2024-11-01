c172612dfc18f459c9e896a2ba9a2e804a6283d2
kungbi <kipper12335@gmail.com>
2024-11-01 18:01:00 +0900

    feat(InputParser): InputParser 구조 생성

---
+++ src/main/java/lotto/controller/InputParser.java
@@ -0,0 +1,13 @@
+package lotto.controller;
+
+public class InputParser {
+
+    public static int[] parseNumbers(String input) {
+        return new int[0];
+    }
+
+    public static int parseInteger(String input) {
+        return 0;
+    }
+
+}
