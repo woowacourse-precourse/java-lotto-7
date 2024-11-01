980d010e7c5f2db33351cfec7de612d5e5611658
ouvir <kilhyunii@naver.com>
2024-11-01 22:10:28 +0900

    test(MoneyValidatorTest): 구매 금액 입력 예외처리 테스트 추가
    - 구매 금액 범위 테스트
    - 구매 금액 단위 테스트

--- src/main/java/lotto/utils/LottoRules.java
+++ src/main/java/lotto/utils/LottoRules.java
@@ -8,6 +8,8 @@ public class LottoRules {
     public static final int LOTTO_NUMBER_SIZE = 6;
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MAX_LOTTO_PURCHASE_COUNT = 100;
 
     public static List<Integer> pickRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_SIZE);
