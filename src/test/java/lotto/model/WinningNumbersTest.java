b9c7572e302e0dcc1a52f10ccba9d255c8317458
이윤빈 <kanado20000@naver.com>
2024-11-01 14:46:59 +0900

    :sparkles: feat: 당첨/보너스 번호를 관리하는 WinningNumbers 클래스의 생성자 메서드의 테스트 코드 추가

---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import lotto.enums.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumbersTest {
+  @Test
+  void 정상적인_당첨_보너스_번호가_들어오면_예외가_발생하지_않는다() {
+    assertThatCode(() -> new WinningNumbers("1,2,3,4,5,6", "7")).doesNotThrowAnyException();
+  }
+
+  @Test
+  void 비정성적인_당첨_보너스_번호가_들어오면_예외가_발생한다() {
+    assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,7", "7"))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_WINNING_NUMBER.getMessage());
+  }
+}
