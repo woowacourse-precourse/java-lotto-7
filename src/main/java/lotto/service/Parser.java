4ca5c7a34b9c9ae5d2cbe15c27c5b990c904edd3
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 17:26:14 +0900

    refactor(Parser): 형변환 클래스로 분리

--- src/main/java/lotto/service/Parser.java
+++ src/main/java/lotto/service/Parser.java
@@ -1,2 +1,16 @@
-package lotto.service;public class Parser {
+package lotto.service;
+
+import static lotto.util.ExceptionMessage.INVALID_INPUT;
+
+public class Parser {
+    public static int toInt(String stringValue) {
+        int intValue;
+        try {
+            intValue = Integer.parseInt(stringValue);
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT.format());
+        }
+        return intValue;
+    }
 }
