2758eb8675b6697d39ada1215cab4676a7c462fb
hanwool <hanwe92@gmail.com>
2024-11-01 10:58:36 +0900

    feat(NumberValidator): Add common validation for number
    Add Common validation for both lotto number and bonus number
    - not over 45
    - not negative
    - not double
    - error message for all of these

--- src/main/java/lotto/util/enums/ValidateMessage.java
+++ src/main/java/lotto/util/enums/ValidateMessage.java
@@ -2,7 +2,11 @@ package lotto.util.enums;
 
 public enum ValidateMessage {
     NON_NUMERIC_ERROR("숫자만 입력해주세요."),
-    NUMBER_SIZE_ERROR("당첨번호의 개수가 6개가 아닙니다.")
+    NUMBER_SIZE_ERROR("당첨번호의 개수가 6개가 아닙니다."),
+    NUMBER_RANGE_ERROR("1 ~ 45 사이의 숫자만 입력해주세요."),
+    POSITIVE_NUMBER_ERROR("양수만 입력해주세요."),
+    DOUBLE_ERROR("자연수만 입력해주세요."),
+    DUPLICATE_ERROR("서로 다른 숫자를 입력해주세요.")
     ;
 
     private final String message;
---
+++ src/main/java/lotto/validation/NumberValidator.java
@@ -0,0 +1,39 @@
+package lotto.validation;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lotto.util.enums.ValidateMessage;
+
+public class NumberValidator {
+    private static final int FIFTY_FIVE = 45;
+    private static final int ZERO = 0;
+
+    public static void validateNumberCondition(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (overThanFiftyFive(number)) {
+                throw new IllegalArgumentException(ValidateMessage.NUMBER_RANGE_ERROR.getMessage());
+            }
+            if (isEqualOrLessThanZero(number)) {
+                throw new IllegalArgumentException(ValidateMessage.NUMBER_RANGE_ERROR.getMessage());
+            }
+            if (isDouble(number)) {
+                throw new IllegalArgumentException(ValidateMessage.NUMBER_SIZE_ERROR.getMessage());
+            }
+        });
+    }
+
+    private static boolean overThanFiftyFive(int number) {
+        return number >= FIFTY_FIVE;
+    }
+
+    private static boolean isEqualOrLessThanZero(int number) {
+        return number <= ZERO;
+    }
+
+    private static boolean isDouble(int number) {
+        BigDecimal bigDecimal = new BigDecimal(number);
+        int intValue = bigDecimal.intValue();
+        BigDecimal decimalValue = bigDecimal.subtract(new BigDecimal(intValue));
+        return decimalValue.doubleValue() > 0;
+    }
+}
