62519d7de82c59125cfa875ee944df02444059d3
Jang <sdsd090811@gmail.com>
2024-11-01 20:47:38 +0900

    refactor: WinningNumbers가 Lotto를 상속받게 변경해 중복 제거

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,57 +1,26 @@
 package lotto;
 
-import java.util.LinkedList;
+import lotto.store.Lotto;
+
 import java.util.List;
 
-public class WinningNumbers {
+public class WinningNumbers extends Lotto {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
 
-    private final List<Integer> numbers;
     private final int bonus;
 
     public WinningNumbers(List<Integer> winningNumber, int bonus) {
-        if(isInvalidSize(winningNumber))
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        super(winningNumber);
+        if(winningNumber.contains(bonus))
+            throw new IllegalArgumentException("당첨 번호에 bonus 번호가 이미 존재합니다.");
+        if(isOutOfLottoRange(bonus))
+            throw new IllegalArgumentException("bonus는 1~45 사이의 숫자여야 합니다.");
 
-        List<Integer> numberWithBonus = combine(winningNumber, bonus);
-        if(hasDuplicatedNumbers(numberWithBonus))
-            throw new IllegalArgumentException("당첨 번호와 bonus는 유니크해야 합니다.");
-        if(hasNumbersOutOfRange(numberWithBonus))
-            throw new IllegalArgumentException("당첨 번호와 bonus는 1~45 사이의 숫자여야 합니다.");
-
-        this.numbers = winningNumber;
         this.bonus = bonus;
     }
 
-    private LinkedList<Integer> combine(List<Integer> winningNumbers, int bonus) {
-        LinkedList<Integer> numbers = new LinkedList<>(winningNumbers);
-        numbers.add(bonus);
-        return numbers;
-    }
-
-    private boolean hasNumbersOutOfRange(List<Integer> numbers) {
-        return countNumbersInRange(numbers) != numbers.size();
-    }
-
-    private long countNumbersInRange(List<Integer> numbers) {
-        return numbers.stream().filter(this::isWithinLottoRange).count();
-    }
-
-    private boolean isWithinLottoRange(Integer num) {
-        return MIN_LOTTO_NUMBER <= num && num <= MAX_LOTTO_NUMBER;
-    }
-
-    private boolean isInvalidSize(List<Integer> numbers) {
-        return numbers.size() != LOTTO_SIZE;
-    }
-
-    private boolean hasDuplicatedNumbers(List<Integer> numbers) {
-        return countUniqueNumber(numbers) != numbers.size();
-    }
-
-    private long countUniqueNumber(List<Integer> numbers) {
-        return numbers.stream().distinct().count();
+    private boolean isOutOfLottoRange(int num) {
+        return num < MIN_LOTTO_NUMBER || MAX_LOTTO_NUMBER < num ;
     }
 }
--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -8,37 +8,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningNumbersTest {
-
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
-    @Test
-    void test1() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
-        int bonus = 8;
-
-        assertThatThrownBy(() -> new WinningNumbers(numbers, bonus))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void test2() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
-        int bonus = 8;
-
-        assertThatThrownBy(() -> new WinningNumbers(numbers, bonus))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또 번호가 1~45 범위 밖에 있다면 예외가 발생한다.")
-    @Test
-    void test3() {
-        List<Integer> numbers = List.of(1, 2, 3, 999, 5, 6);
-        int bonus = 8;
-
-        assertThatThrownBy(() -> new WinningNumbers(numbers, bonus))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("보너스 번호가 로또 번호와 중복되면 예외가 발생한다.")
     @Test
     void test4() {
