5272186dfe5cbe35d210f98572e762056d4f3ef4
tjfwltn <tjfwltn1234@gmail.com>
2024-11-01 20:17:08 +0900

    feat : 로또 번호를 생성하고 정렬해주는 기능 추가
    - 로또 번호를 생성할 때 나중에 다른 방법으로 바꿀
    - 수도 있으므로, policy 패키지를 만들어서
    - LottoNumberPolicy 클래스를 enum으로 작성
    - LottoNumberPolicy 클래스 안에 생성 로직 구현

---
+++ src/main/java/lotto/policy/LottoNumberPolicy.java
@@ -0,0 +1,28 @@
+package lotto.policy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constants.LottoConstants;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static lotto.constants.LottoConstants.*;
+
+public enum LottoNumberPolicy {
+    SORTED_RANDOM_NUMBERS(() -> {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT);
+        Collections.sort(numbers);
+        return numbers;
+    });
+
+    private final Supplier<List<Integer>> policy;
+
+    LottoNumberPolicy(Supplier<List<Integer>> policy) {
+        this.policy = policy;
+    }
+
+    public List<Integer> getNumbers() {
+        return policy.get();
+    }
+}
