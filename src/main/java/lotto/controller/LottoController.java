e9ea2a9a5dafd40b4eca25d86c487cf7b858a64a
skhnpark <skhnpark125@gmail.com>
2024-11-01 21:39:26 +0900

    feat: 로또 구입 금액 안내 문구를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,13 @@
 package lotto.controller;
 
+import lotto.view.OutputView;
+
 public class LottoController {
+    public void run() {
+        insertMoney();
+    }
+
+    public void insertMoney() {
+        OutputView.printInsertMoney();
+    }
 }
