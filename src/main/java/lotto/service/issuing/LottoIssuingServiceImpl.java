6155706a30b0042f9a755d78ec4871c290c27151
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-01 23:03:43 +0900

    feat(service): 로또 발행 서비스 구현체 추가
    - LottoNumberGeneratorStrategy와 LottosFactory를 이용하여 랜덤 숫자 리스트를 기반으로 Lottos의 객체 생성

--- src/main/java/lotto/service/issuing/LottoIssuingServiceImpl.java
+++ src/main/java/lotto/service/issuing/LottoIssuingServiceImpl.java
@@ -1,2 +1,26 @@
-package lotto.service.issuing;public class LottoIssuingServiceImpl {
+package lotto.service.issuing;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.factory.LottosFactory;
+import lotto.model.Lottos;
+import lotto.strategy.number.LottoNumberGeneratorStrategy;
+
+public class LottoIssuingServiceImpl implements LottoIssuingService {
+    private final LottoNumberGeneratorStrategy lottoNumberGenerator;
+
+    public LottoIssuingServiceImpl(LottoNumberGeneratorStrategy lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    @Override
+    public Lottos issueLottos(int count) {
+        List<List<Integer>> allNumbers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            allNumbers.add(lottoNumberGenerator.generate());
+        }
+
+        return LottosFactory.createLottos(allNumbers);
+    }
 }
