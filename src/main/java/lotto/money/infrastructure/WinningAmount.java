a049a816d9446e039f0356584e2986766a836b50
EHCTH <lees90896@gmail.com>
2024-11-01 17:06:29 +0900

    feat(WinningAmount): 당첨 금액 클래스 생성

--- src/main/java/lotto/money/infrastructure/WinningAmount.java
+++ src/main/java/lotto/money/infrastructure/WinningAmount.java
@@ -1,4 +1,23 @@
 package lotto.money.infrastructure;
 
-public class WinningAmount {
+import lotto.money.domain.Money;
+import lotto.util.Convertor;
+
+public class WinningAmount implements Money {
+    private final long amount;
+    private WinningAmount(long amount) {
+        this.amount = amount;
+    }
+    public static WinningAmount of(String input) {
+        long amount = Convertor.stringToLong(input);
+        return new WinningAmount(amount);
+    }
+    @Override
+    public long getMoney() {
+        return amount;
+    }
+    @Override
+    public String toString() {
+        return String.valueOf(amount);
+    }
 }
