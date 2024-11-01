51901e27b957868fa6ca135496ce794efe139d94
ddddabi <ii887742@gmail.com>
2024-11-01 23:34:01 +0900

    feat: 02. 로또 번호 생성 기능 구현
    - 로또번호 랜덤 생성
    - 생성한 로또번호 출력

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,10 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.common.ErrorMessage;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +17,21 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER_COUNT);
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
 }
