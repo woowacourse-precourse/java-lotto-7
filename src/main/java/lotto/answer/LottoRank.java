b6471f69a6fe0d994f014b4ffba21d709f973f22
minjun <ijmtylhtf@naver.com>
2024-11-01 21:47:29 +0900

    refactor : 출력 메시지 포맷 수정

--- src/main/java/lotto/answer/LottoRank.java
+++ src/main/java/lotto/answer/LottoRank.java
@@ -4,31 +4,31 @@ public enum LottoRank {
 	FIFTH(3, false, 5000) {
 		@Override
 		public void printInfo() {
-			System.out.print(getMatchCount() + MATCH_MESSAGE + "(" + getPrizeAmount() + ")" + WON + "-");
+			System.out.print("3개 일치 (5,000원) - ");
 		}
 	},
 	FOURTH(4, false, 50000) {
 		@Override
 		public void printInfo() {
-			System.out.print(getMatchCount() +  MATCH_MESSAGE + "(" + getPrizeAmount() + ")" + WON + "-");
+			System.out.print("4개 일치 (50,000원) - ");
 		}
 	},
 	THIRD(5, false, 1500000) {
 		@Override
 		public void printInfo() {
-			System.out.print(getMatchCount() + MATCH_MESSAGE + "(" + getPrizeAmount() + ")" + WON + "-");
+			System.out.print("5개 일치 (1,500,000원) - ");
 		}
 	},
 	SECOND(5, true, 30000000) {
 		@Override
 		public void printInfo() {
-			System.out.print(getMatchCount() +  MATCH_MESSAGE + "," + BONUS_MATCH_MESSAGE + "(" + getPrizeAmount() + ")" + WON + "-");
+			System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
 		}
 	},
 	FIRST(6, false, 2000000000) {
 		@Override
 		public void printInfo() {
-			System.out.print(getMatchCount() + MATCH_MESSAGE + "(" + getPrizeAmount() + ")" + WON + "-");
+			System.out.print("6개 일치 (2,000,000,000원) - ");
 		}
 	};
 	private static final String MATCH_MESSAGE = "개 일치";
