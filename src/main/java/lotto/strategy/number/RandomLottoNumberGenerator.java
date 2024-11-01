e275b6616cbd068164b0e02ba2da38f4bae730cb
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-01 18:21:43 +0900

    feat(strategy): 로또 번호 생성기 구현체 추가
    - 최소 숫자, 최대 숫자, 숫자 개수 상수로 선언
    - RandomLottoNumberGenerator 클래스를 추가하여 1부터 45까지의 중복되지 않는 6개의 번호를 무작위로 생성하는 기능 구현

--- src/main/java/lotto/strategy/number/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/strategy/number/RandomLottoNumberGenerator.java
@@ -1,2 +1,15 @@
-package lotto.strategy.number;public class RandomLottoNumberGenerator {
+package lotto.strategy.number;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomLottoNumberGenerator implements LottoNumberGeneratorStrategy {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    @Override
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_COUNT);
+    }
 }
