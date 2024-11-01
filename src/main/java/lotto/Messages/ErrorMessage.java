1993e3053d3a54b10eb122e9daee3e7d1b515916
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 23:13:36 +0900

    refactor(ErrorMessage) : error 메세지 추가

--- src/main/java/lotto/Messages/ErrorMessage.java
+++ src/main/java/lotto/Messages/ErrorMessage.java
@@ -7,7 +7,11 @@ public enum ErrorMessage {
     WIN_INPUT("당첨 번호 조건을 다시 확인하고 입력해 주세요."),
     BONUS("보너스 번호 조건을 다시 확인하고 입력해 주세요."),
     BONUS_RANGE("보너스 정답은 1이상 45이하의 숫자이어야 합니다."),
-    BONUS_DUPLICATE("보너스 숫자는 기존 당첨 번호들과 달라야 합니다.");
+    BONUS_DUPLICATE("보너스 숫자는 기존 당첨 번호들과 달라야 합니다."),
+    LOTTO_NUM("로또 번호는 6개여야 합니다."),
+    LOTTO_RANGE("로또 번호는 1 이상 45 이하여야 합니다."),
+    LOTTO_DUPLICATE("로또 번호끼리는 중복이 되면 안 됩니다.");
+
 
 
     private String message;
