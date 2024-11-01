15624f56f96651be6182b795517079d531ccad11
최유현 <dbgus315@naver.com>
2024-11-01 20:57:16 +0900

    refactor: 구입 금액 검증 메서드 수정

--- src/main/java/lotto/view/ViewInput.java
+++ src/main/java/lotto/view/ViewInput.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class ViewInput {
     public static final String INITIAL_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.";
+    public static final String NEGATIVE_VALUE_ERROR_MESSAGE = "[ERROR] 구입 금액은 음수가 될 수 없습니다.";
     public static final String PURCHASE_AMOUNT_COUNT_MESSAGE = "개를 구매했습니다.";
 
     public int receivePurchaseAmount(){
@@ -25,6 +26,8 @@ public class ViewInput {
     private void validatorPurchaseAmount(int purchaseAmount){
         if(purchaseAmount % 1000 != 0){
             throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
+        }else if(purchaseAmount < 0){
+            throw new IllegalArgumentException(NEGATIVE_VALUE_ERROR_MESSAGE);
         }
     }
 }
