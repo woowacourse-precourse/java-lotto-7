fd4979d732ba73ff4123e8109fbbdf5cdc5778c9
soeun2537 <soeun2537@gmail.com>
2024-11-02 01:06:46 +0900

    feat: 입력 문자열 분리 로직 추가

--- src/main/java/lotto/model/Parser.java
+++ src/main/java/lotto/model/Parser.java
@@ -1,12 +1,25 @@
 package lotto.model;
 
+import static lotto.constant.core.ParserConstant.*;
 import static lotto.constant.message.ErrorMessage.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Parser {
 
     public static Integer parseInputToInt(String input) {
         String trimmedInput = trimWhitespace(input);
         return convertStringToInteger(trimmedInput);
+    }
+
+    public static List<Integer> parseInputsToIntList(String inputs) {
+        List<Integer> parsedInputs = new ArrayList<>();
+        List<String> separatedInputs = separateBySeparator(inputs);
+        for (String input : separatedInputs) {
+        }
+        return parsedInputs;
     }
 
     private static String trimWhitespace(String input) {
@@ -19,5 +32,10 @@ public class Parser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_INPUT_TYPE.getMessage());
         }
+    }
+
+    private static List<String> separateBySeparator(String inputs) {
+        String[] separatedInputs = inputs.split(PLURAL_INPUT_SEPARATOR.getStringValue());
+        return Arrays.asList(separatedInputs);
     }
 }
