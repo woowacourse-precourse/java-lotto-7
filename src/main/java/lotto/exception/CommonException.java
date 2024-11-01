fc40f03fe24d1d4b98e539ed91f3e4f60bd1d01b
Changsu Park <gd12091@gmail.com>
2024-11-01 14:27:00 +0900

    fix: super 메서드로 오류 출력

--- src/main/java/lotto/exception/CommonException.java
+++ src/main/java/lotto/exception/CommonException.java
@@ -3,15 +3,7 @@ package lotto.exception;
 import lotto.enums.ErrorCode;
 
 public class CommonException extends IllegalArgumentException {
-
-    private final ErrorCode errorCode;
-
     public CommonException(ErrorCode errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    @Override
-    public String getMessage() {
-        return this.errorCode.getMessage();
+        super("[ERROR] " + errorCode.getMessage());
     }
 }
