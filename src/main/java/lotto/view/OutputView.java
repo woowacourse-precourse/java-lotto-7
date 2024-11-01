58d231400382242c843660e0b49f1aa7009acd8a
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 15:34:33 +0900

    feat(OutputView): 구매 개수 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,4 +6,7 @@ public class OutputView {
     public static void printLottoList(List<Integer> randomLottoNumber){
         System.out.println(randomLottoNumber);
     }
+    public static void printCount(int ticketCount){
+        System.out.println(ticketCount+"개를 구매했습니다.");
+    }
 }
