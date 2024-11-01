46277beb8edd0ad1e7c7559571e9d316db974eaf
binn <anyubin0724@naver.com>
2024-11-01 12:11:46 +0900

    feat: 수익 총합 계산하는 메서드 추가

--- src/main/java/lotto/service/ProfitCalculatorService.java
+++ src/main/java/lotto/service/ProfitCalculatorService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.model.LottoResult;
 
 public class ProfitCalculatorService {
@@ -8,9 +9,15 @@ public class ProfitCalculatorService {
     private static final int FIVE_MATCH_PRIZE_AMOUNT = 1500000;
     private static final int FIVE_AND_BONUS_MATCH_PRIZE_AMOUNT = 30000000;
     private static final int SIX_MATCH_PRIZE_AMOUNT = 2000000000;
-    public double calculateProfit(LottoResult lottoResult, int purchasePrice) {
-        int prizeAmount = getPrizeAmount(lottoResult);
+    public static double calculateProfit(List<LottoResult> lottoResult, int purchasePrice) {
+        int prizeAmount = getTotalPrizeAmount(lottoResult);
         return (double) prizeAmount / purchasePrice * 100;
+    }
+
+    private static int getTotalPrizeAmount(List<LottoResult> lottoResults) {
+        return lottoResults.stream()
+                .mapToInt(ProfitCalculatorService::getPrizeAmount)
+                .sum();
     }
 
     private static int getPrizeAmount(LottoResult result) {
