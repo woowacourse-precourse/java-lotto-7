60d32ab29f1c79b5ac6654dd339599e60364f6a6
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 17:29:03 +0900

    refactor(Splitter): split 클래스 분리

--- src/main/java/lotto/service/Splitter.java
+++ src/main/java/lotto/service/Splitter.java
@@ -1,2 +1,15 @@
-package lotto.service;public class Splitter {
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Splitter {
+    public static List<Integer> splitWinningNumbers(String winningNumbers) {
+        String[] numbers = winningNumbers.split(",");
+        return Arrays.stream(numbers)
+                .filter(num -> !num.trim().isEmpty())
+                .map(num -> Parser.toInt(num.trim()))
+                .collect(Collectors.toList());
+    }
 }
