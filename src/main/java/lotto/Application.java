b2a481cacddfb0433d13ce560013ab5849dd9f24
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-11-01 22:43:22 +0900

    feat: 로또 당첨 번호 입력 및 유효성 검증 기능 추가
    - promptWinningNumbers 메서드: 사용자로부터 당첨 번호를 입력받고 유효성 검증을 통해 숫자 리스트로 반환
    - isValidWinningNumbers 메서드: 입력된 당첨 번호가 유효한지 검증
     - isBlank 메서드: 입력된 당첨 번호의 빈 값 여부, 공백 여부를 검증
     - containsComma 메서드: 입력된 당첨 번호에 구분자(,)가 포함되지 않은 경우를 검증
     - hasDuplicates 메서드: 입력된 당첨 번호의 중복 여부를 검증
     - hasSixNumbers 메서드: 입력된 당첨 번호가 6개인지 검증
     - areNumbersInRange 메서드: 입력된 당첨 번호가 1~45 범위 내에 존재하는지 검증
    - handleInvalidWinningNumbers 메서드: 유효하지 않은 당첨 번호 입력에 대해 에러 메시지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,9 +2,17 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
+
         int purchaseAmount = promptPurchaseAmount();
+
+        List<Integer> winningNumbers = promptWinningNumbers();
     }
 
     public static int promptPurchaseAmount() {
@@ -20,6 +28,22 @@ public class Application {
         }
     }
 
+    public static List<Integer> promptWinningNumbers() {
+        while (true) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String winningNumbers = Console.readLine();
+
+            if (isValidWinningNumbers(winningNumbers)) {
+                return Arrays.stream(winningNumbers.split(",", -1))
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .toList();
+            }
+
+            handleInvalidWinningNumbers();
+        }
+    }
+
     public static boolean isValidPurchaseAmount(String purchaseAmount) {
         if (purchaseAmount.isBlank()) {
             return false;
@@ -32,11 +56,68 @@ public class Application {
         }
     }
 
+    public static boolean isValidWinningNumbers(String winningNumbers) {
+        String[] splitWinningNumbers = winningNumbers.split(",", -1);
+
+        return !isBlank(winningNumbers) &&
+                containsComma(winningNumbers) &&
+                !hasDuplicates(splitWinningNumbers) &&
+                hasSixNumbers(splitWinningNumbers) &&
+                areNumbersInRange(splitWinningNumbers);
+    }
+
     public static void handleInvalidPurchaseAmount() {
         try {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위입니다. 유효한 금액을 입력해주세요.");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void handleInvalidWinningNumbers() {
+        try {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45까지의 중복되지 않는 6개의 숫자이어야 합니다. 번호는 쉼표(,)를 기준으로 구분해주세요");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static boolean isBlank(String input) {
+        return input.isBlank();
+    }
+
+    private static boolean containsComma(String input) {
+        return input.contains(",");
+    }
+
+    private static boolean hasDuplicates(String[] numbers) {
+        Set<String> uniqueNumbers = new HashSet<>(Arrays.asList(numbers));
+        return uniqueNumbers.size() != numbers.length;
+    }
+
+    private static boolean hasSixNumbers(String[] numbers) {
+        if (numbers.length != 6) {
+            return false;
+        }
+
+        for (String number : numbers) {
+
+            if (number.isBlank()) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    private static boolean areNumbersInRange(String[] numbers) {
+        for (String number : numbers) {
+
+            if (!number.trim().matches("^(?:[1-9]|[1-3][0-9]|4[0-5])$")) {
+                return false;
+            }
+
+        }
+        return true;
     }
 }
