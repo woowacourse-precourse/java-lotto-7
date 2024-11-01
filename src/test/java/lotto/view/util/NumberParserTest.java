06f3cb3a0ba4189eba27bd6322f241a633637c19
Jiihyun <cwlgusc@gmail.com>
2024-11-01 17:17:35 +0900

    refactor: numberParserTest 테스트 메서드들 이름 한글로 변경

--- src/test/java/lotto/view/util/NumberParserTest.java
+++ src/test/java/lotto/view/util/NumberParserTest.java
@@ -1,7 +1,6 @@
 package lotto.view.util;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,9 +17,8 @@ class NumberParserTest {
         numberParser = new NumberParser();
     }
 
-    @DisplayName("숫자 형식의 문자열을 숫자로 파싱할 수 있다.")
     @Test
-    void parseToInteger() {
+    void 숫자_형식의_문자열을_숫자로_파싱할_수_있다() {
         //given
         String input = "12345678";
         //when
@@ -29,10 +27,9 @@ class NumberParserTest {
         assertThat(integer).isEqualTo(12345678);
     }
 
-    @DisplayName("문자열이 숫자 형식이 아니면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"ㅇ", "1ooo", "I000"})
-    void throwExceptionWhenInputIsNotNumberFormat(String input) {
+    void 문자열이_숫자_형식이_아니면_예외가_발생한다(String input) {
         assertThatThrownBy(() -> numberParser.parseToInt(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 숫자입니다.");
