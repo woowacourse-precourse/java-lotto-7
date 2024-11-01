4f98a918ab51b52f95e08baf2b78d90f71d07d0c
qudcks8084 <qudcks8084@naver.com>
2024-11-01 23:53:17 +0900

    Feat : 예외 처리 통합 함수 구현 및 일부 수정
    - validateNewLottoNumber를 구현해 추후에 유지보수를 용이하게 구현
    - 나머지 3개의 함수의 접근 제어자 변경
    	- Private -> package

--- src/main/java/lotto/util/validator/LottoValidator.java
+++ src/main/java/lotto/util/validator/LottoValidator.java
@@ -9,13 +9,19 @@ public class LottoValidator {
 
     private static final String ERR_MSG_WRONG_WINNER_NUMBERS = "[ERROR] 로또 당첨 번호는 1부터 45 사이의 6개의 숫자여야 합니다.";
 
-    public static void validateLottoHasSixNumbers(List<Integer> numbers){
+    public static void validateNewLottoNumber(List<Integer> numbers){
+        validateLottoHasSixNumbers(numbers);
+        validateLottoNumberIsValid(numbers);
+        validateLottoNumberIsDuplicate(numbers);
+    }
+
+    static void validateLottoHasSixNumbers(List<Integer> numbers){
         if (numbers.size() != LottoConstants.LOTTO_NUM_COUNT) {
             throw new IllegalArgumentException(ERR_MSG_WRONG_WINNER_NUMBERS);
         }
     }
 
-    public static void validateLottoNumberIsValid(List<Integer> numbers) {
+    static void validateLottoNumberIsValid(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < LottoConstants.LOTTO_MIN_NUMBER || number > LottoConstants.LOTTO_MAX_NUMBER) {
                 throw new IllegalArgumentException(ERR_MSG_WRONG_WINNER_NUMBERS);
@@ -23,7 +29,7 @@ public class LottoValidator {
         }
     }
 
-    public static void validateLottoNumberIsDuplicate(List<Integer> numbers){
+    static void validateLottoNumberIsDuplicate(List<Integer> numbers){
         HashMap<Integer, Integer> hashNumber = new HashMap<>();
         for (int number : numbers) {
             if(!hashNumber.containsKey(number)){
