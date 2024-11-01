a2eaf43dd6d6734bef7533d4619ade18ae702da0
soeun2537 <soeun2537@gmail.com>
2024-11-02 01:09:45 +0900

    test: 입력 문자열 정수 리스트로 변환 - 공백 포함 예외 단위 테스트 추가

--- src/test/java/lotto/model/ParserTest.java
+++ src/test/java/lotto/model/ParserTest.java
@@ -182,4 +182,16 @@ class ParserTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
+
+    @Test
+    @DisplayName("입력 문자열 정수 리스트로 변환: 공백 포함 - 예외 테스트")
+    void parseInputsToIntList_includeWhiteSpace() {
+        // given
+        String inputs = "1, ,3";
+
+        // when & then
+        assertThatThrownBy(() -> Parser.parseInputsToIntList(inputs))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
