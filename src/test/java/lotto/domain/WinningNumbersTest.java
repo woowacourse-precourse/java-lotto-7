d2de9f795491dbd539007bf8729601b9c1e75e08
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-01 18:34:25 +0900

    test(WinningNumbersTest): 당첨 번호가 6개가 아니면 예외가 발생하는 테스트 작성
    해당 테스트는 실패한다.

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -55,4 +55,13 @@ public class WinningNumbersTest {
                 .hasMessage("[ERROR] 당첨 번호는 중복될 수 없습니다.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "1,2,3", "1,2,3,4,5,6,7"})
+    @DisplayName("당첨 번호가 6개가 아니면 예외가 발생한다.")
+    void validateWinningNumbersCount(String condition) {
+        assertThatThrownBy(() -> WinningNumbers.from(condition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
 }
