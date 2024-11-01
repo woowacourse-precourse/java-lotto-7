17e4d3457084eba9bb86ef5a65699d8fbdc42317
kungbi <kipper12335@gmail.com>
2024-11-01 18:10:14 +0900

    feat(InputParser): parseIntegers 구현 완료

--- src/main/java/lotto/controller/InputParser.java
+++ src/main/java/lotto/controller/InputParser.java
@@ -1,9 +1,21 @@
 package lotto.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
 
-    public static int[] parseIntegers(String input) {
-        return new int[0];
+    public static List<Integer> parseIntegers(String input) {
+        String[] split = input.split(",");
+
+        List<Integer> integers = null;
+        try {
+            integers = Arrays.stream(split).map(Integer::parseInt).toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+
+        return integers;
     }
 
     public static int parseInteger(String input) {
