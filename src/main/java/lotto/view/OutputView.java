fddf3c8c17cc871ae1c0847b97bf9caad384bf79
dk7648 <dk7648@korea.ac.kr>
2024-11-02 00:59:13 +0900

    feat: 발행한 로또 정보를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,27 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.LottoManager;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    public static void printLottos(List<Lotto> lottos) {
+        printNumberOfLotto(lottos);
+        for (Lotto lotto : lottos) {
+            printNumbers(lotto.getNumbers());
+        }
+    }
+
+    public static void printNumberOfLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+    }
+
+    public static void printNumbers(List<Integer> numbers) {
+        String result = "[" + numbers.stream()
+                .map(String::valueOf).
+                collect(Collectors.joining(", ")) + "]";
+        System.out.println(result);
+    }
 }
