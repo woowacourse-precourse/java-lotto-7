acc2d24624f7d38002954dc2a80294ae4e4bc0fc
woogym <kwj9294@gmail.com>
2024-11-02 05:14:25 +0900

    feat(util): 구분자를 통해서 숫자 리스트로 변환하는 유틸클래스 구현

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -1,2 +1,27 @@
-package lotto.util;public class Converter {
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.exception.LottoException.NotAllowCharacterInputException;
+
+public class Converter {
+
+    private static final String DELIMITER = ",";
+
+    private Converter() {
+    }
+
+    public static List<Integer> convertToIntgerList(final String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(number -> getNumber(number.trim()))
+                .toList();
+    }
+
+    private static Integer getNumber(final String number) {
+        try {
+            return Integer.valueOf(number);
+        } catch (NumberFormatException e) {
+            throw new NotAllowCharacterInputException();
+        }
+    }
 }
