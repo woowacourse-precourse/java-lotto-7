13a7a0dc31188f87ddc8bb8585d937d2b9ade4bb
Koo-sangwoo <luckytkddn@gmail.com>
2024-11-01 16:18:47 +0900

    feat : 당첨결과 반환 함수 추가, 수익률 계산 함수 추가
    - 당첨 결과를 Enum을 담은 리스트로 멤버 선언, 모든 ENUM 요소에 대한 당첨값을 가져간다.
    - 수익률을 계산하여 수익률 문자열 값 리턴
    - 통합 테스트 진행 예정

--- src/main/java/lotto/controller/LottoResultController.java
+++ src/main/java/lotto/controller/LottoResultController.java
@@ -4,19 +4,21 @@ import lotto.LottoWinningRank;
 import lotto.model.Lotto;
 import lotto.model.LottoResultChecker;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Type;
+import java.util.*;
 
 public class LottoResultController {
     private LottoResultChecker resultChecker;
-
-    public LottoResultController(List<Lotto> purchaseLottoList, List<Integer> prizeNumbers, Integer bonusNumber) {
+    private Map<LottoWinningRank, Integer> lottoResultMap;
+    private String profitMargin;
+    // 발급 로또 리스트, 당첨 번호, 보너스 번호, 구매 금액
+    public LottoResultController(List<Lotto> purchaseLottoList, List<Integer> prizeNumbers, Integer bonusNumber,Integer buyPrice) {
         this.resultChecker = new LottoResultChecker(purchaseLottoList, prizeNumbers, bonusNumber);
+        setLottoResult(getPurchaseEnumList());
+        setProfitMargin(buyPrice);
     }
 
-    public List<LottoWinningRank> getResultRank() {
+    public List<LottoWinningRank> getPurchaseEnumList() {
         var resultPrizeCountList = resultChecker.getPrizeResult();
         var resultIsBonusCountList = resultChecker.getPrizeBonusNumberList();
         List<LottoWinningRank> resultList = new ArrayList<>();
@@ -34,6 +36,17 @@ public class LottoResultController {
         return rankMap.getOrDefault(key, LottoWinningRank.FAIL);
     }
 
+    private void setLottoResult(List<LottoWinningRank> purchaseEnumList) {
+        Map<LottoWinningRank, Integer> resultMap = new HashMap<LottoWinningRank, Integer>();
+
+        for (LottoWinningRank resultType : LottoWinningRank.values()) {
+            int frequency = Collections.frequency(purchaseEnumList, resultType);
+            resultMap.put(resultType, frequency);
+        }
+
+        this.lottoResultMap = resultMap;
+    }
+
     // if문을 여러개 사용할 수 없어 Map으로 선언 후 getKey를 사용하여 리턴함
     private Map<String, LottoWinningRank> createPrizeRank() {
         Map<String, LottoWinningRank> rankMap = new HashMap<>();
@@ -45,5 +58,14 @@ public class LottoResultController {
         rankMap.put("3:false", LottoWinningRank.FIFTH);
 
         return rankMap;
+    }
+
+    private void setProfitMargin(Integer buyPrice) {
+        Integer allPrice = 0;
+        for (Map.Entry<LottoWinningRank, Integer> prizeEntry : lottoResultMap.entrySet()) {
+            allPrice += prizeEntry.getKey().getPrizePrice() * prizeEntry.getValue();
+        }
+
+        this.profitMargin = ((allPrice / buyPrice) * 100) + "%";
     }
 }
