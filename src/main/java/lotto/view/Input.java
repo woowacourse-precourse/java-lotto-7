5ca51915eba47599b80c571044d6257fd6b7ce78
yooo <ghddbtl@gmail.com>
2024-11-01 23:12:20 +0900

    feat : add user messages to view/Input.java
    사용자에게 출력할 메세지를 추가하였다

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,12 +2,14 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Input {
-    static String READ_PURCHASE_AMOUNT = request();
-    static String READ_WINNING_NUMBERS = request();
-    static String READ_BONUS_NUMBERS = request();
 
-    static String request() {
+public class Input {
+    final static String PURCHASE_AMOUNT_PROMPT = "로또 구매 금액을 입력해주세요";
+    final static String WINNING_NUMBERS_PROMPT = "담청 번호를 입력해주세요";
+    final static String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해주세요";
+
+    static String request(String message) {
+        System.out.println(message);
         return Console.readLine();
 
     }
