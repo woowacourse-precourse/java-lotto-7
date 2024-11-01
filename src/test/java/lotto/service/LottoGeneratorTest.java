b5f6a76230ef3570a1bba7fa6795dfc03f1a251f
GiSeok Kang <ime.rokz@gmail.com>
2024-11-01 19:37:34 +0900

    test: LottoMachine 로또 당첨 및 돈 검증 테스트 코드 추가

---
+++ src/test/java/lotto/service/LottoGeneratorTest.java
@@ -0,0 +1,15 @@
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.util.NumberGenerate;
+
+class LottoGeneratorTest implements NumberGenerate {
+
+    private List<Integer> expect = Arrays.asList(1, 20, 34, 42, 32, 6);
+
+    @Override
+    public List<Integer> randomGenerateInRange() {
+        return expect;
+    }
+}
---
+++ src/test/java/lotto/service/LottoMachineTest.java
@@ -0,0 +1,100 @@
+package lotto.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.domain.BonusBall;
+import lotto.domain.Lotto;
+import lotto.domain.PurchasedLottos;
+import lotto.domain.Rank;
+import lotto.domain.WinningLotto;
+import lotto.util.ErrorMessage;
+import lotto.util.NumberGenerate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoMachineTest {
+
+    private final int money = 1000;
+    private final NumberGenerate numberGenerate = new LottoGeneratorTest();
+    private final LottoMachine lottoMachine = new LottoMachine(numberGenerate);
+
+    @ParameterizedTest
+    @ValueSource(ints = {1100, 1200, 2600, 2040, 2003})
+    void 돈_천원_단위가_아닌_입력(int money) {
+        // given
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> lottoMachine.issueLotto(money));
+
+        // then
+        Assertions.assertThat(e.getMessage()).isEqualTo(ErrorMessage.MONEY_IS_NOT_MOD_PRICE.getMsg());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -30, -1000, -1500, -2000})
+    void 돈_0_이하_입력(int money) {
+        // given
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> lottoMachine.issueLotto(money));
+
+        // then
+        Assertions.assertThat(e.getMessage()).isEqualTo(ErrorMessage.NOT_NUMBER.getMsg());
+    }
+
+
+    @Test
+    void 랜덤_값_테스트() {
+        // given
+        List<Integer> expect = numberGenerate.randomGenerateInRange();
+
+        // when
+        PurchasedLottos purchasedLottos = lottoMachine.issueLotto(money);
+        List<Lotto> lottos = purchasedLottos.getLottos();
+
+        // then
+        for (Lotto lotto : lottos) {
+            Assertions.assertThat(lotto.lottoNums()).isEqualTo(expect);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("lottoArgsGenerate")
+    void 로또_당첨_테스트(List<Integer> numbers, int bonus, Rank win) {
+        // given
+        PurchasedLottos purchasedLottos = lottoMachine.issueLotto(money);
+        WinningLotto winningLotto = winningLottoMake(numbers, bonus);
+
+        // when
+        LottoResult lottoResult = lottoMachine.winLotto(purchasedLottos, winningLotto);
+
+        // then
+        Assertions.assertThat(lottoResult.getWinner())
+                .isEqualTo(win);
+    }
+
+    private static Stream<Arguments> lottoArgsGenerate() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 20, 34, 5, 4, 3), 5, Rank.NO5),
+                Arguments.of(Arrays.asList(1, 20, 34, 42, 4, 3), 5, Rank.NO4),
+                Arguments.of(Arrays.asList(1, 20, 34, 42, 32, 3), 5, Rank.NO3),
+                Arguments.of(Arrays.asList(1, 20, 34, 42, 32, 3), 6, Rank.NO2),
+                Arguments.of(Arrays.asList(1, 20, 34, 42, 32, 6), 5, Rank.NO1)
+        );
+    }
+
+    private WinningLotto winningLottoMake(List<Integer> numbers, int bonus) {
+        Lotto lotto = new Lotto(numbers);
+        BonusBall bonusBall = new BonusBall(bonus);
+        return new WinningLotto(lotto, bonusBall);
+    }
+
+
+}
