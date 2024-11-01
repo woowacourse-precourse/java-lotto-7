3cf6ba5994cff3add7852300577dda0cd890f993
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-01 14:15:02 +0900

    fix(lotto) : Lotto 클래스 수정
    mutable collection을 사용하여 정렬 연산이 가능하도록 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,18 +9,22 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || !isValidRange(numbers) || hasDuplicates(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자 6개여야 합니다.");
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
-
-    private boolean isValidRange(List<Integer> numbers) {
-        return numbers.stream().allMatch(num -> num >= 1 && num <= 45);
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     private boolean hasDuplicates(List<Integer> numbers) {
@@ -27,6 +32,6 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return new ArrayList<>(numbers);
     }
 }
