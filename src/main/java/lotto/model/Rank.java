c36d6245f490c05665f671129756f611adc97fd9
jina <jinahoi@naver.com>
2024-11-02 02:44:07 +0900

    feat: 로또 등수를 계산하는 기능 구현
    - `Rank`Enum 클래스로 미리 정의한 등수 값을 프로그램 전체에서 사용할 수 있도록 제한했습니다.
    - 각 등수와 관련된 데이터를 상수와 결합해서 관리하도록 구현했습니다.
    - `RankCalculator`클래스를 사용해 당첨 번호와 구매한 로또를 비교해 등수를 계산합니다.
    - 보너스 번호가 등수에 영향을 미치는지 판단하고 추가 검사를 진행합니다.

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,36 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean requiresBonus;
+    private final int prize;
+
+    Rank(int matchCount, boolean requiresBonus, int prize) {
+        this.matchCount = matchCount;
+        this.requiresBonus = requiresBonus;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean requiresBonus() {
+        return requiresBonus;
+    }
+
+    public boolean isWinningRank() {
+        return this.matchCount >= 3;
+    }
+}
