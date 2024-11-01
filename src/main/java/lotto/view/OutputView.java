d7b90f8a1cc1fd91094aa341bc76cbc5f52bad21
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-01 15:49:48 +0900

    feat(OutputView): 출력 문구 상수화
    %,d 포맷을 이용해 원화 표시를 고려하였습니다.
    %.1f 포맷을 이용해 소숫점 둘째 자리에서 수익률을 반올림하였습니다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+
+    private static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다";
+    private static final String WINNING_RESULT_PREFIX = "당첨 통계\n---\n";
+    private static final String WINNING_RESULT_MESSAGE = "%d개 일치 (%,d원) - %d개\n";
+    private static final String REVENUE_RESULT_MESSAGE = "총 수익률은 %.1f%입니다.\n";
+}
