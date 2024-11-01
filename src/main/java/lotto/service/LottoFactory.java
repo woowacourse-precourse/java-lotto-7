aac437d8bc1734e7ad4e4862e5a2f51ad96b715d
chaerish <ccyy8432@naver.com>
2024-11-01 20:15:11 +0900

    feat(LottoFactory): 1부터 45사이의 랜덤 값으로 로또 생성 클래스, 로직 구현

---
+++ src/main/java/lotto/service/LottoFactory.java
@@ -0,0 +1,15 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.Lotto;
+
+public class LottoFactory {
+    public Lotto createLotto() {
+        return new Lotto(getRandomNumber());
+    }
+
+    private List<Integer> getRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
