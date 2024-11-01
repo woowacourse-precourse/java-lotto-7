7f1eab1a44fc8ea4e0d33561bd9dd6f3e8e722ff
Chanyoo Kim <cksdb012@gmail.com>
2024-11-01 18:45:44 +0900

    refactor: 클래스 이름 변경
    클래스의 이름의 오타를 변경

--- src/main/java/lotto/exception/BonusNumberRangeException.java
+++ src/main/java/lotto/exception/BonusNumberRangeException.java
@@ -1,9 +1,9 @@
 package lotto.exception;
 
-public class BounsNumberRangeException extends IllegalArgumentException {
+public class BonusNumberRangeException extends IllegalArgumentException {
     private static final String ERROR_MESSAGE = "[ERROR] 보너스 번호는 1 ~ 45 이내로 입력하셔야 합니다";
 
-    public BounsNumberRangeException() {
+    public BonusNumberRangeException() {
         super(ERROR_MESSAGE);
     }
 }
--- src/main/java/lotto/exception/BonusNumberTypeException.java
+++ src/main/java/lotto/exception/BonusNumberTypeException.java
@@ -1,9 +1,9 @@
 package lotto.exception;
 
-public class BounsNumberTypeException extends IllegalArgumentException {
+public class BonusNumberTypeException extends IllegalArgumentException {
     private static final String ERROR_MESSAGE = "[ERROR] 보너스 번호는 숫자가 입력되어야 합니다";
 
-    public BounsNumberTypeException() {
+    public BonusNumberTypeException() {
         super(ERROR_MESSAGE);
     }
 }
