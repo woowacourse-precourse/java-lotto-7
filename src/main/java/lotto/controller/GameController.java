050a8b5ff1aaa13c519ba493a6074a69f755607c
icebear0111 <wltjq0901@naver.com>
2024-11-01 18:00:56 +0900

    feat: Add method to complete game flow
    당첨 번호와 보너스 번호 입력 및 검증 기능을 추가
    당첨 결과 계산 및 수익률을 출력하는 기능 추가

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -2,11 +2,15 @@ package lotto.controller;
 
 import lotto.model.Lotto;
 import lotto.model.LottoMachine;
+import lotto.model.ProfitCalculator;
+import lotto.model.WinningLotto;
 import lotto.util.InputValidator;
 import lotto.view.UserInputView;
 import lotto.view.UserOutputView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class GameController {
 
@@ -15,8 +19,17 @@ public class GameController {
 
         LottoMachine lottoMachine = new LottoMachine();
         List<Lotto> lottos = lottoMachine.purchaseLottos(purchaseAmount);
-
         UserOutputView.outputLottos(lottos);
+
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber(winningNumbers);
+
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        Map<String, Integer> lottoResult = winningLotto.calculateResult(lottos);
+        double profitRate = ProfitCalculator.calculateProfitRate(lottoResult, purchaseAmount);
+
+        UserOutputView.outputWinningStatistics(lottoResult);
+        UserOutputView.outputProfitRate(profitRate);
     }
 
     private int getPurchaseAmount() {
@@ -30,5 +43,44 @@ public class GameController {
         }
 
         return Integer.parseInt(inputPurchaseAmount);
+    }
+
+    private List<Integer> getWinningNumbers() {
+        String inputWinningNumbers = UserInputView.inputWinningNumbers();
+        List<Integer> winningNumbers = parseWinningNumbers(inputWinningNumbers);
+
+        try {
+            InputValidator.validateWinningNumbers(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getWinningNumbers();
+        }
+
+        return winningNumbers;
+    }
+
+    private List<Integer> parseWinningNumbers(String inputWinningNumbers) {
+        String[] numbers = inputWinningNumbers.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        for (String number : numbers) {
+            winningNumbers.add(Integer.parseInt(number));
+        }
+
+        return winningNumbers;
+    }
+
+    private int getBonusNumber(List<Integer> winningNumbers) {
+        String inputBonusNumber = UserInputView.inputBonusNumber();
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
+
+        try {
+            InputValidator.validateBonusNumber(bonusNumber, winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getBonusNumber(winningNumbers);
+        }
+
+        return bonusNumber;
     }
 }
