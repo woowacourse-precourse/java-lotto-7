34584853b49f5ef3f9a49a6441895bdae63c1054
acupoframen <choe.hwan@gmail.com>
2024-11-02 08:29:41 +0900

    feat (InputView): create InputView - add feature for buy lotto, choose winning number, choose bonus number

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+}
