3c62e092cc0d3ed33dc16534601f10771b144f46
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-01 16:12:08 +0900

    refactor: 로또 번호를 오름차순으로 저장하도록 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,6 +4,7 @@ import static lotto.common.LottoRule.LOTTO_NUMBER_COUNTS;
 import static lotto.validator.ValidationMessage.DUPLICATE_NUMBERS;
 import static lotto.validator.ValidationMessage.INVALID_NUMBER_COUNTS;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +16,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validNumberCounts(numbers);
         validDuplicate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
