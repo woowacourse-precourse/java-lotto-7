b79b06f8531b2ef5bd03b13e2f078dd37a34c431
GunsanHaribo <hth99131@gmail.com>
2024-11-02 01:51:20 +0900

    feat(WinningLotto.java): 당첨범호 자동생성

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class WinningLotto {
+    private final List<Integer> numbers;
+
+    private final int bonusNumber;
+
+    public WinningLotto() {
+        List<Integer> randoms = Randoms.pickUniqueNumbersInRange(1, 45, 7);
+        this.numbers = randoms.subList(0, 6);
+        this.bonusNumber  = randoms.get(6);
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningLottoTest {
+    @DisplayName("당첨 번호 추첨시 기본 6개와 보너스 숫자를 뽑는다.")
+    @Test
+    void 당첨_번호_추첨시_기본_6개와_보너스_숫자를_뽑는다(){
+        WinningLotto winningLotto = new WinningLotto();
+
+        assertThat(winningLotto.getNumbers()).hasSize(6);
+        assertThat(winningLotto.getBonusNumber()).isInstanceOf(Integer.class);
+    }
+}
