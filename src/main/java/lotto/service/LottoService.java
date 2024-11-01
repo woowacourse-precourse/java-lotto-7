8e4d846bef2c80efb4b2f24ea7a562ab028ea1ba
ouvir <kilhyunii@naver.com>
2024-11-01 22:19:19 +0900

    feat(LottoService): 구매 금액만큼 로또 발행 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,13 +1,32 @@
 package lotto.service;
 
+import lotto.Exception.MoneyException;
+import lotto.Exception.MoneyExceptionType;
 import lotto.model.Lotto;
 import lotto.utils.LottoRules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.Exception.MoneyExceptionType.*;
+import static lotto.utils.LottoRules.LOTTO_PRICE;
 
 public class LottoService {
 
 
     public Lotto generateLotto() {
-        Lotto newLotto = new Lotto(LottoRules.pickRandomNumbers());
-        return newLotto;
+        return new Lotto(LottoRules.pickRandomNumbers());
+    }
+
+    public List<Lotto> purchaseLottoTickets(int price) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+
+        int ticketCount = price / LOTTO_PRICE ;
+
+        for (int i = 0; i < ticketCount; i++) {
+            lottoTickets.add(generateLotto());
+        }
+
+        return lottoTickets;
     }
 }
