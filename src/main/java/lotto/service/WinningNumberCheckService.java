18f72cd5c83302c71874307c54b83d4924786cbf
binn <anyubin0724@naver.com>
2024-11-01 13:50:15 +0900

    feat: 통계값 계산 메서드 추가
    - 맞춘 숫자의 횟수를 세는 기능 구현

--- src/main/java/lotto/service/WinningNumberCheckService.java
+++ src/main/java/lotto/service/WinningNumberCheckService.java
@@ -2,14 +2,36 @@ package lotto.service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
 import lotto.model.LottoResult;
 
 public class WinningNumberCheckService {
-    public LottoResult checkWinningNumber(List<Integer> randomLotto, List<Integer> winningNumber, int bonusNumber) {
-        Set<Integer> winningNumberSet = new HashSet<>(winningNumber);
-        long matchCount = randomLotto.stream().filter(winningNumberSet::contains).count();
-        boolean hasBonus = randomLotto.contains(bonusNumber);
+    public static LottoResult checkWinningNumber(Lotto randomLotto, Lotto winningNumber, int bonusNumber) {
+        Set<Integer> winningNumberSet = new HashSet<>(winningNumber.getNumbers());
+        long matchCount = randomLotto.getNumbers().stream().filter(winningNumberSet::contains).count();
+        boolean hasBonus = randomLotto.getNumbers().contains(bonusNumber);
         return new LottoResult(matchCount, hasBonus);
+    }
+
+    public static Map<String, Integer> calculateStatistics(List<LottoResult> lottoResult) {
+        Map<String, Integer> statistics = lottoResult.stream()
+                .collect(Collectors.groupingBy(result ->{
+                    if (result.getMatchCount() == 3) return "3개";
+                    if (result.getMatchCount() == 4) return "4개";
+                    if (result.getMatchCount() == 5 && result.getHasBonus()) return "5개+보너스";
+                    if (result.getMatchCount() == 5) return "5개";
+                    if (result.getMatchCount() == 6) return "6개";
+                    return "기타";
+                }, Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
+        statistics.putIfAbsent("3개", 0);
+        statistics.putIfAbsent("4개", 0);
+        statistics.putIfAbsent("5개", 0);
+        statistics.putIfAbsent("5개+보너스", 0);
+        statistics.putIfAbsent("6개", 0);
+
+        return statistics;
     }
 }
