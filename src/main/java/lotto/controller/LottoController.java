cc78e9812590ca45bf6792a35cf37dfbe1d4aef6
Beomseok Noh <prodigy0831@naver.com>
2024-11-01 12:04:10 +0900

    feat(OutputView): 유저에게 보여지는 메시지를 전달하기 위한 OutputView 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,5 +2,10 @@ package lotto.controller;
 
 public class LottoController {
     public void run() {
+        int attemptCount = getAttemptCount();
+    }
+
+    private int getAttemptCount() {
+        return 0;
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String PURCHASE_AMOUNT_INPUT = "구입 금액을 입력해 주세요.";
+
+    public static void printPurchaseAmountInputMessage() {
+        System.out.println(PURCHASE_AMOUNT_INPUT);
+    }
+}
