023469e35370bdf0599b41eabd8a17aa0b9943e7
junho767 <junho767@naver.com>
2024-11-01 18:57:28 +0900

    style: 메서드 괄호 사이에 공백 추가

--- src/main/java/lotto/model/LottoMatcher.java
+++ src/main/java/lotto/model/LottoMatcher.java
@@ -4,12 +4,12 @@ public class LottoMatcher {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public LottoMatcher(Lotto winningLotto, int bonusNumber){
+    public LottoMatcher(Lotto winningLotto, int bonusNumber) {
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-    public LottoRank determineRank(Lotto lotto){
+    public LottoRank determineRank(Lotto lotto) {
         int matchCount = lotto.getMatchCount(winningLotto);
         boolean validateBonusNumber = lotto.isBonusMatched(bonusNumber);
         return LottoRank.getRank(matchCount, validateBonusNumber);
