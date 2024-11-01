6e2d3fa6a240b1dea8a1b06b2898f4318fa3c567
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-01 18:48:56 +0900

    feat( ValidationUtils.java ) Add Method
    Add Method List
     - validateNULL()
     - validateNumberRange()
     - validateNumberSize()
     - validateUnit()
     - validateDuplicateWinNumber()
     - validateDuplicateBonusNumber()

--- src/main/java/lotto/common/utils/ValidationUtils.java
+++ src/main/java/lotto/common/utils/ValidationUtils.java
@@ -1,29 +1,42 @@
 package lotto.common.utils;
 
-import static lotto.common.ExceptionConstants.IS_NULL;
-import static lotto.common.ExceptionConstants.NOT_INTEGER;
-import static lotto.common.ExceptionConstants.OVER_NUMBER_SIZE;
+import java.util.List;
 
+import static lotto.common.ExceptionConstants.*;
 
 public class ValidationUtils {
 
-    public static int strToInt(String str){
-        if(str.isEmpty()) throw new IllegalArgumentException(IS_NULL.getMessage());
+    public static void validateInt(String str){
         if( !str.matches("\\d+") ) throw new IllegalArgumentException(NOT_INTEGER.getMessage());
-        return Integer.parseInt(str);
-
     }
 
-    public static String[] spliteStr(String readData){  
-        //String Check
-        String[] str = readData.split(",");
-
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].length() > 6)  throw new IllegalArgumentException(OVER_NUMBER_SIZE.getMessage());
-            if (str[i].isEmpty())     throw new IllegalArgumentException(IS_NULL.getMessage());
-        }
-
-        return str;
+    public static void validateNULL(int number){
+        if(number == 0) throw new IllegalArgumentException(IS_NULL.getMessage());
     }
+
+    public static void validateNULL(String str){
+        if(str.isEmpty() || str == null) throw new IllegalArgumentException(IS_NULL.getMessage());
+    }
+
+    public static void validateNumberRange(int number){
+        if(number >= 45 ) throw new IllegalArgumentException(OVER_NUMBER_RANGE.getMessage());
+    }
+
+    public static void validateNumberSize(int arraySize){
+        if(arraySize != 6 ) throw new IllegalArgumentException(OVER_NUMBER_SIZE.getMessage());
+    }
+
+    public static void validateUnit(int money){
+        if ( (money % 1000) != 0) throw new IllegalArgumentException(NOT_BUY_UNIT.getMessage());
+    }
+
+    public static void validateDuplicateWinNumber(List<Integer> number){
+        if(number.size() != number.stream().distinct().count() ) throw new IllegalArgumentException(DUPLICATE_NUMBER.getMessage());
+    }
+
+    public static void validateDuplicateBonusNumber(List<Integer> number, int bonusNumber){
+        if (number.contains(bonusNumber)) { throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER.getMessage()); }
+    }
+
 
 }
