d69a9e56dabb4efb8495e14864c6c7c1542f8a95
wogus <ind07152@naver.com>
2024-11-01 14:06:48 +0900

    feat(BonusNumber): 보너스 숫자를 관리하는 도메인 생성
    보너스 숫자에 대해 검증하고 숫자를 보관한다.

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import static lotto.exception.ErrorMessage.INVALID_BONUS_NUMBER_RANGE;
+
+import lotto.exception.LottoException;
+
+public class BonusNumber {
+    private final int bonus;
+
+    private BonusNumber(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public static BonusNumber from(int bonus) {
+        validateRange(bonus);
+        return new BonusNumber(bonus);
+    }
+
+    private static void validateRange(int bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new LottoException(INVALID_BONUS_NUMBER_RANGE);
+        }
+    }
+}
