2277bf825e67cbed3912eaf195c9a42eb0817629
변우영 <likevangogh24@gmail.com>
2024-11-01 22:52:10 +0900

    feat(ProfitCalculator): 수익률 계산 기능 추가

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -3,9 +3,25 @@ package lotto.domain;
 import lotto.dto.MatchCondition;
 import lotto.enums.LottoRank;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
 public class ProfitCalculator {
 
-    public long calculateWinningAmount(MatchCondition matchCondition) {
+    public long calculateTotalWinningAmount(List<MatchCondition> matchConditions) {
+        return matchConditions.stream()
+                .mapToLong(this::calculateWinningAmount)
+                .sum();
+    }
+
+    public double calculateProfitRate(long totalWinningAmount, long purchaseAmount) {
+        BigDecimal profit = BigDecimal.valueOf(totalWinningAmount - purchaseAmount);
+        BigDecimal profitRate = profit.divide(BigDecimal.valueOf(purchaseAmount), 1, RoundingMode.HALF_UP);
+        return profitRate.doubleValue();
+    }
+
+    private long calculateWinningAmount(MatchCondition matchCondition) {
         LottoRank rank = LottoRank.of(matchCondition.matchCount(), matchCondition.containBonusNumber());
         return rank.getMoney();
     }
