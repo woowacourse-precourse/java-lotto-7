3082500c3f622b21d10ccd884bd4228e4ba0ce0c
soeun2537 <soeun2537@gmail.com>
2024-11-01 22:18:00 +0900

    feat: 당첨 번호 유효성 검사 - 중복된 번호 로직 추가

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -3,6 +3,7 @@ package lotto.model;
 import static lotto.constant.message.ErrorMessage.*;
 import static lotto.constant.core.ValidatorConstant.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Validator {
@@ -12,11 +13,22 @@ public class Validator {
     }
 
     public static void validateWinningTicket(List<Integer> winningTicket) {
+        List<Integer> validatedWinningTicket = new ArrayList<>();
+        for (Integer winningNumber : winningTicket) {
+            validateNoDuplicate(validatedWinningTicket, winningNumber);
+            validatedWinningTicket.add(winningNumber);
+        }
     }
 
     private static void validateMultiple(Integer number) {
         if ((number % LOTTO_PRICE.getIntegerValue()) != 0) {
             throw new IllegalArgumentException(INVALID_MULTIPLE_AMOUNT.getMessage(LOTTO_PRICE.getIntegerValue()));
+        }
+    }
+
+    private static void validateNoDuplicate(List<Integer> numbers, Integer number) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER.getMessage());
         }
     }
 }
