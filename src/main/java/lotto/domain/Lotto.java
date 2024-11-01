d62c6b8333124b0db6883603edfd0d9c7e81e529
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-02 03:40:40 +0900

    feat : 로또 클래스에 유효성 검증 메서드 추가
    - 로또 리스트 사이즈 검증
    - 로또 리스트 안 숫자 범위 검증
    - 로또 리스트 안 숫자 중복 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,11 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,12 +14,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkNumbersSize(numbers);
+        checkNumberRange(numbers);
+        checkNumberDuplicated(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+    private void checkNumbersSize(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkNumberRange(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호가 숫자 범위를 벗어납니다.");
+            }
+        }
+    }
+
+    private void checkNumberDuplicated(List<Integer> numbers){
+        Set<Integer> filterNumbers = new HashSet<>(numbers);
+        if (filterNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복이 존재합니다.");
+        }
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }
