506cfc27e4dbd95f65368581a3cb0885c8fc1c5b
seophohoho <seop0937@gmail.com>
2024-11-01 18:52:19 +0900

    chore: 불필요한 import 삭제

--- src/test/java/lotto/model/SellerTest.java
+++ src/test/java/lotto/model/SellerTest.java
@@ -3,7 +3,6 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
