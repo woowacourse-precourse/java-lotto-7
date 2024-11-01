342e0c721bf3079f645daf6c6eeb2cb097197d07
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 13:26:47 +0900

    feat(PurchaseAmount): 로또 수량 계산

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+    private static final int UNIT = 1000;
+    private final int amountNum;
+
+    public PurchaseAmount(String amountStr){
+        int amount=checkText(amountStr);
+        validate(amount);
+        this.amountNum=amount;
+    }
+    private void validate(int amount) {
+        checkNegative(amount);
+        checkUnit(amount);
+    }
+    private int checkText(String amountStr) {
+        amountStr=amountStr.trim();
+        if (amountStr.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            return Integer.parseInt(amountStr.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void checkNegative(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void checkUnit(int amount) {
+        if (amount % UNIT != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public int calculateLottoCount(){
+        return amountNum/UNIT;
+    }
+}
