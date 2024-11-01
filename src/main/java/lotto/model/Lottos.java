ed76722b7147bdf1f595ccdecc313fb151377551
gitubanana <ptyolo666@gmail.com>
2024-11-01 18:26:06 +0900

    feat(Lottos): 사용자가 입력한 금액만큼 로또 발급 기능

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(int lottoCount) {
+        lottos = new ArrayList<>(lottoCount);
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    public List<Lotto> toList() {
+        return List.copyOf(lottos);
+    }
+
+    public int count() {
+        return lottos.size();
+    }
+}
