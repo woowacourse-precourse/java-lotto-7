f45760e7e7bcf3445f6b42edb4f10cfda1db5a1d
hyuuuun <pam4522@gmail.com>
2024-11-02 01:43:48 +0900

    feat(Rank) :등수별 맞은 갯수와,보너스유무,상금 을 열거형 생성

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Rank {
+    FIRST(2_000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FOURTH(50_000, 4, false),
+    FIFTH(5_000, 3, false),
+    LOSE(0, 0, false);
+    private final int prize;
+    private final int matchLotto;
+    private final boolean matchBonus;
+
+    Rank(int prize, int matchLotto, boolean matchBonus) {
+        this.prize = prize;
+        this.matchLotto = matchLotto;
+        this.matchBonus = matchBonus;
+    }
+
+    public static Rank assign(int matchLotto, boolean matchBonus) {
+        if(matchLotto == 6){ return FIRST; }
+        if(matchLotto == 5 && matchBonus){ return SECOND; }
+        if(matchLotto == 5){ return THIRD; }
+        if(matchLotto == 4){ return FOURTH; }
+        if(matchLotto == 3){ return FIFTH; }
+        return LOSE;
+    }
+}
