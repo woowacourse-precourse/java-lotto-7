a27d7f813c4f591507e6eca00f629259f2b26d8d
seophohoho <seop0937@gmail.com>
2024-11-02 01:44:09 +0900

    feat(Buyer): Lotto 결과를 저장하는 컬렉션 추가

--- src/main/java/lotto/model/Buyer.java
+++ src/main/java/lotto/model/Buyer.java
@@ -1,15 +1,24 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Buyer {
     private List<Lotto> lottos;
+    private HashMap<Rank, Integer> resultLotto = new HashMap<>();
 
     public Buyer(List<Lotto> lottos) {
         this.lottos = lottos;
+        initResultLotto();
     }
 
     public List<Lotto> getLottos() {
         return this.lottos;
+    }
+
+    private void initResultLotto() {
+        for (Rank rank : Rank.values()) {
+            this.resultLotto.put(rank, 0);
+        }
     }
 }
