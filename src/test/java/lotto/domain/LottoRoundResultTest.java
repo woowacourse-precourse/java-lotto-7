a40f1885c8e28bab80cd8bdd86c1ad12d8df12ed
himitery <himitery@gmail.com>
2024-11-01 10:01:05 +0900

    test(lotto): 로또 회차 결과 유효성 테스트 추가
    - 로또 회차 결과의 유효성 테스트 케이스 설계

---
+++ src/test/java/lotto/domain/LottoRoundResultTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoRoundResultTest {
+
+    @Test
+    @DisplayName("로또 등수를 올바르게 관리한다.")
+    void 로또_등수를_올바르게_관리한다() {
+        // Given
+        List<LottoRank> lottoRanks = List.of(
+                LottoRank.MATCH_THREE_NUMBER,
+                LottoRank.MATCH_FOUR_NUMBER,
+                LottoRank.MATCH_THREE_NUMBER);
+
+        // When
+        LottoRoundResult result = new LottoRoundResult(lottoRanks);
+
+        // Then
+        Assertions.assertThat(result.getRankCount(LottoRank.MATCH_THREE_NUMBER))
+                .isEqualTo(2);
+        Assertions.assertThat(result.getRankCount(LottoRank.MATCH_FOUR_NUMBER))
+                .isEqualTo(1);
+    }
+}
