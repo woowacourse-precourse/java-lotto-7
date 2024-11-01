17b41380b8f947a9eb409c21eb1bed7887534c57
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-01 22:38:33 +0900

    feat(model/LottoTicket): LottoTicket 클래스 추가하여 로또 번호 생성 및 정렬 기능 구현
    - LottoTicket 클래스에서 1~45 사이의 중복 없는 6개 숫자를 랜덤으로 생성
    - 번호를 오름차순으로 정렬하여 저장

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,5 +1,24 @@
 package lotto.model;
 
-public class LottoTicket {
+import java.util.Collections;
+import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoTicket {
+	private final List<Integer> numbers;
+
+	public LottoTicket() {
+		this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		Collections.sort(numbers);
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
+
+	@Override
+	public String toString() {
+		return numbers.toString();
+	}
 }
