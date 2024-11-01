70124afff181e069b23db7b1d8c2e22c27be643f
Chanyoo Kim <cksdb012@gmail.com>
2024-11-01 18:50:09 +0900

    feat: 보너스 번호 입력 기능 구현
    보너스 번호를 입력하는 기능을 구현

---
+++ src/main/java/lotto/view/InputBonusNumberView.java
@@ -0,0 +1,40 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Set;
+import lotto.exception.BonusNumberCountException;
+import lotto.exception.BonusNumberDuplicationException;
+import lotto.exception.BonusNumberRangeException;
+import lotto.exception.BonusNumberTypeException;
+
+public class InputBonusNumberView {
+    public static int bonusNumberInput(Set<Integer> winningNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String[] input = Console.readLine().split(",| ");
+
+        if(input.length != 1){
+            throw new BonusNumberCountException();
+        }
+
+        int bonusNumber = validate(input[0]);
+
+        if(winningNumbers.contains(bonusNumber)) {
+            throw new BonusNumberDuplicationException();
+        }
+
+        return bonusNumber;
+    }
+
+    private static int validate(String input) {
+        try {
+            int bonusNumber = Integer.parseInt(input);
+            if (bonusNumber <= 0 || bonusNumber > 45) {
+                throw new BonusNumberRangeException();
+            }
+
+            return bonusNumber;
+        } catch (NumberFormatException e) {
+            throw new BonusNumberTypeException();
+        }
+    }
+}
