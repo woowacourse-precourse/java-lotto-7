a5f6cae9df4d4f71ab57a0b8935753af23e05b1b
EHCTH <lees90896@gmail.com>
2024-11-01 19:06:25 +0900

    feat(WinningLottoCreator): WinningLottoCreatorService 인터페이스 구현

--- src/main/java/lotto/lotto/winning/infrastructure/WinningLottoCreator.java
+++ src/main/java/lotto/lotto/winning/infrastructure/WinningLottoCreator.java
@@ -1,13 +1,15 @@
 package lotto.lotto.winning.infrastructure;
 
 import lotto.lotto.winning.domain.WinningLotto;
+import lotto.lotto.domain.WinningLottoCreatorService;
 import lotto.view.input.hanlder.domain.InputHandlerService;
 
-public class WinningLottoCreator {
+public class WinningLottoCreator implements WinningLottoCreatorService {
     private final InputHandlerService inputHandlerService;
     public WinningLottoCreator(InputHandlerService inputHandlerService) {
         this.inputHandlerService = inputHandlerService;
     }
+    @Override
     public WinningLotto create() {
         return inputHandlerService.retrieveReceive(WinningLotto::of);
     }
