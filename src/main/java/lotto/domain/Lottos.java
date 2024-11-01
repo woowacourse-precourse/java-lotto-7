950ddcd724e062ce89492fdd3f78945ee170bc6f
StoneCAU <seokwoo1999@naver.com>
2024-11-02 00:04:33 +0900

    refactor(Lottos): lottoQuantity 변수 추가
    - 객체 활용성을 높이기 위함

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class Lottos {
     private final List<Lotto> lottoGroup;
+    private final int lottoQuantity;
 
-    public Lottos(List<Lotto> lottoGroup) {
+    public Lottos(List<Lotto> lottoGroup, int lottoQuantity) {
         this.lottoGroup = lottoGroup;
+        this.lottoQuantity = lottoQuantity;
     }
 
     public List<Lotto> getLottoGroup() {
         return lottoGroup;
+    }
+
+    public int getLottoQuantity() {
+        return lottoQuantity;
     }
 }
