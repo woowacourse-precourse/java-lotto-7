6b93d0d5b485b77ff40e283dd559b153f97791a1
SeungminKimdev <ksm7250432@gmail.com>
2024-11-01 15:34:46 +0900

    feat: 로또 번호 범위 확인 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         checkDuplicate(numbers);
+        checkNumbersInRange(numbers);
         this.numbers = numbers;
     }
 
@@ -20,6 +21,14 @@ public class Lotto {
     private void checkDuplicate(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+        }
+    }
+
+    private void checkNumbersInRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
