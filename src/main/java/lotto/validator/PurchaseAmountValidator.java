3eac09c6a5e5071d162ad0eae65f1db923e585a1
OhYeHyun <ohyhohyl@naver.com>
2024-11-02 01:00:23 +0900

    refactor(PurchaseAmountValidator): staic 메서드로 변경

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -3,21 +3,22 @@ package lotto.validator;
 import static lotto.constants.LottoConstants.*;
 
 public class PurchaseAmountValidator {
-    private final int purchaseAmount;
-
-    public PurchaseAmountValidator(String input) {
-        this.purchaseAmount = parsePurchaseAmount(input);
-        validateOverThreshold();
-        validateUnit();
+    private PurchaseAmountValidator() {
+        // 인스턴스 생성 방지
     }
 
-    private int parsePurchaseAmount(String input) {
+    public static int validateAndParse(String input) {
         validateNoSpace(input);
         validateIsNumber(input);
-        return Integer.parseInt(input);
+        int amount = Integer.parseInt(input);
+
+        validateOverThreshold(amount);
+        validateUnit(amount);
+
+        return amount;
     }
 
-    private void validateIsNumber(String input) {
+    private static void validateIsNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -25,20 +26,20 @@ public class PurchaseAmountValidator {
         }
     }
 
-    private void validateNoSpace(String input) {
+    private static void validateNoSpace(String input) {
         if (!input.equals(input.strip())) {
             throw new IllegalArgumentException(ErrorMessage.MUST_BE_NO_SPACE.getMessage(PURCHASE_AMOUNT));
         }
     }
 
-    private void validateOverThreshold() {
-        if (purchaseAmount < PURCHASE_AMOUNT_THRESHOLD) {
+    private static void validateOverThreshold(int amount) {
+        if (amount < PURCHASE_AMOUNT_THRESHOLD) {
             throw new IllegalArgumentException(ErrorMessage.MUST_BE_OVER_THRESHOLD.getMessage());
         }
     }
 
-    private void validateUnit() {
-        if (purchaseAmount % PURCHASE_AMOUNT_UNIT != 0) {
+    private static void validateUnit(int amount) {
+        if (amount % PURCHASE_AMOUNT_UNIT != 0) {
             throw new IllegalArgumentException(ErrorMessage.MUST_BE_UNIT.getMessage());
         }
     }
--- src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
+++ src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
@@ -6,13 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PurchaseAmountValidatorTest {
-    private PurchaseAmountValidator purchaseAmountValidator;
-
     @Test
     @DisplayName("구입 금액에 공백이 포함되었는지 확인")
     void 구입_금액_공백_테스트() {
         assertThatThrownBy(() -> {
-            purchaseAmountValidator = new PurchaseAmountValidator(" 3000");
+            PurchaseAmountValidator.validateAndParse(" 3000");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입 금액에 공백을 허용하지 않습니다.");
     }
 
@@ -20,7 +18,7 @@ public class PurchaseAmountValidatorTest {
     @DisplayName("구입 금액이 숫자인지 확인")
     void 구입_금액_숫자_테스트() {
         assertThatThrownBy(() -> {
-            purchaseAmountValidator = new PurchaseAmountValidator("abc");
+            PurchaseAmountValidator.validateAndParse("abc");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입 금액은(는) 숫자 형식이어야 합니다.");
     }
 
@@ -28,7 +26,7 @@ public class PurchaseAmountValidatorTest {
     @DisplayName("구입 금액이 0원 이상인지 확인")
     void 구입_금액_0원_이상_테스트() {
         assertThatThrownBy(() -> {
-            purchaseAmountValidator = new PurchaseAmountValidator("-2000");
+            PurchaseAmountValidator.validateAndParse("-2000");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입 금액은 0원 이상이어야 합니다.");
     }
 
@@ -36,7 +34,7 @@ public class PurchaseAmountValidatorTest {
     @DisplayName("구입 금액이 1000원 단위인지 확인")
     void 구입_금액_1000원_단위_테스트() {
         assertThatThrownBy(() -> {
-            purchaseAmountValidator = new PurchaseAmountValidator("1005");
+            PurchaseAmountValidator.validateAndParse("1005");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입 금액은 1000원 단위이어야 합니다.");
     }
 }
