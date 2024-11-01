45b4b024829cec0db1bf06ce0620043d640afafc
WARDKEY <tjrmsduq@naver.com>
2024-11-01 16:01:05 +0900

    feat(LottoMachine) : 일치하는 문자의 개수와 금액 출력 기능과 총 수익률 출력 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -11,13 +11,14 @@ import java.util.stream.Collectors;
 public class LottoMachine {
 
     private Lotto lotto;
+    private  int cost;
     private int bonusNumber;
     private final List<List<Integer>> lottoNumbers = new ArrayList<>();
 
     public void run() {
         System.out.println("구입금액을 입력해 주세요.");
 
-        int cost = Integer.parseInt(Console.readLine().trim());
+        cost = Integer.parseInt(Console.readLine().trim());
 
         int purchaseCount = cost / 1000;
 
@@ -50,6 +51,57 @@ public class LottoMachine {
 
         System.out.println("당첨 통계");
         System.out.println("---");
+
+        int threeMatched = 0;
+        int fourMatched = 0;
+        int fiveMatched = 0;
+        int bonusMatched = 0;
+        int allMatched = 0;
+
+        for (List<Integer> lottoNumber : lottoNumbers) {
+            int lottoCount = 0;
+            boolean isBonusMatched = lottoNumber.contains(bonusNumber);
+
+            for (int number : lotto.getNumbers()) {
+                if (lottoNumber.contains(number)) {
+                    lottoCount++;
+                }
+            }
+
+            if (lottoCount == 6) {
+                allMatched++;
+                continue;
+            }
+            if (lottoCount == 5 && isBonusMatched) {
+                bonusMatched++;
+                continue;
+            }
+            if (lottoCount == 5) {
+                fiveMatched++;
+                continue;
+            }
+            if (lottoCount == 4) {
+                fourMatched++;
+                continue;
+            }
+            if (lottoCount == 3) {
+                threeMatched++;
+            }
+        }
+
+        System.out.println("3개 일치 (5,000원) - " + threeMatched + "개");
+        System.out.println("4개 일치 (50,000원) - " + fourMatched + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + fiveMatched + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + bonusMatched + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + allMatched + "개");
+
+        int totalWinnings = (5000 * threeMatched) + (50000 * fourMatched) +
+                (1500000 * fiveMatched) + (30000000 * bonusMatched) +
+                (2000000000 * allMatched);
+
+        double earningRatio = ((double) totalWinnings / cost) * 100;
+        String formatEarningRatio = String.format("%,.1f", earningRatio);
+        System.out.println("총 수익률은 " + formatEarningRatio + "%입니다.");
 
     }
 }
