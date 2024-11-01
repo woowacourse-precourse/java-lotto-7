a9a745c42774c8a3cb6698b5562e6f28bd3e62b3
33jyu33 <jy04169@gmail.com>
2024-11-02 00:12:06 +0900

    feat(LottoController): request input value of price and validate
    Function run:
    - create Consumer
    Function getLottoPrice:
    - InputView.readInput()
    - validate input value of price
    - return the number of lotto
    Function inputToInt:
    - validateOnlyInteger()
    - return price converted to integer form
    Function validatePrice
    - validate price

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,41 @@
+package lotto.controller;
+
+import lotto.Consumer;
+import lotto.constant.CompareInteger;
+import lotto.constant.PriceRule;
+import lotto.validator.NumberValidator;
+import lotto.validator.PriceValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import static lotto.constant.CompareInteger.PRICE_MAXIMUM;
+import static lotto.constant.CompareInteger.PRICE_MINIMUM;
+import static lotto.constant.PriceRule.SCOPE;
+
+public class LottoController {
+    public void run() {
+        OutputView.printPriceGuide();
+        Consumer consumer = new Consumer(getLottoPrice());
+    }
+
+    private int getLottoPrice() {
+        try {
+            String inputPrice = InputView.readInput();
+            int price = inputToInt(inputPrice);
+            validatePrice(price);
+            return price / CompareInteger.PRICE_LOTTO.getNumber();
+        } catch (IllegalArgumentException e) {
+            return getLottoPrice();
+        }
+    }
+
+    private int inputToInt(String input) throws IllegalArgumentException {
+        NumberValidator.validateOnlyInteger(input, PriceRule.ONLY_INTEGER.getMessage());
+        return Integer.parseInt(input);
+    }
+
+    private void validatePrice(int price) {
+        NumberValidator.validateScope(PRICE_MINIMUM.getNumber(), PRICE_MAXIMUM.getNumber(), price, SCOPE.getMessage());
+        PriceValidator.validatePriceUnit(price);
+    }
+}
