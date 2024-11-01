ccf99c6e51f5d5a99438010bfd32bb086e575e60
JS <zks145@naver.com>
2024-11-01 22:36:52 +0900

    style : 로또 중복 체크 기능 스트림에서 크기 비교 형태로 변경

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -1,6 +1,5 @@
 package lotto.validation;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -8,31 +7,22 @@ import static lotto.validation.ErrorMessage.*;
 
 public class Validation {
 
-    public void checkLottoSize(List<Integer> numbers) {
-        if (numbers.size() > 6) {
+    public static void checkLottoSize(List<Integer> lottoNumbers, Integer maxLength) {
+        if (lottoNumbers.size() > maxLength) {
             throw new IllegalArgumentException(LOTTO_ERROR_WRONG_LOTTO_SIZE.getMessage());
         }
     }
 
-    public void checkLottoDuplicate(List<Integer> numbers, Integer bonusNumber) {
-
-        HashSet<Integer> checkNumber = new HashSet<>();
-
-        numbers.stream()
-                .filter(num -> !checkNumber.add(num))
-                .findFirst()
-                .ifPresent(duplicate -> {
-                    throw new IllegalArgumentException(LOTTO_ERROR_WRONG_LOTTO_DUPLICATE.getMessage());
-                });
-
-        checkDuplicateBonusNumber(bonusNumber, checkNumber);
+    public static void checkLottoDuplicate(List<Integer> numbers) {
+        HashSet<Integer> checkNumber = new HashSet<>(numbers);
+        if (checkNumber.size() != numbers.size()) {
+            throw new IllegalArgumentException(LOTTO_ERROR_WRONG_LOTTO_DUPLICATE.getMessage());
+        }
     }
 
-    private void checkDuplicateBonusNumber(Integer bonusNumber, HashSet<Integer> checkNumber) {
-        if (bonusNumber != null) {
-            if (checkNumber.contains(bonusNumber)) {
-                throw new IllegalArgumentException(LOTTO_ERROR_WRONG_LOTTO_DUPLICATE.getMessage());
-            }
+    public static void checkDuplicateBonusNumber(List<Integer> numbers, Integer bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LOTTO_ERROR_WRONG_LOTTO_DUPLICATE.getMessage());
         }
     }
 
