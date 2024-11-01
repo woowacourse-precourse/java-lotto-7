ce6911f92a49835bbafc969c2f528ac23c3dfac6
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-01 23:30:20 +0900

    test: 당첨 번호 입력시 올바른 값인지 확인용 테스트 코드 작성

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,21 +1,13 @@
 package lotto.model;
 
-import java.util.ArrayList;
-import lotto.constants.ErrorMessage;
-import lotto.validator.AmountsValidator;
-import org.assertj.core.api.AssertionsForClassTypes;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.constants.ErrorMessage.INVALID_LOTTO_NUMBER_COUNT;
-import static lotto.constants.ErrorMessage.WINNING_NUMBER_OUT_OF_RANGE;
+import static lotto.constants.ErrorMessage.INPUT_LOTTO_NUMBER_OUT_OF_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
 
 class LottoTest {
 
@@ -47,7 +39,7 @@ class LottoTest {
     void Given_LottoNumbersAreOutOfRange_When_ValidateNumberRange_Then_Error() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(WINNING_NUMBER_OUT_OF_RANGE.getMessage());
+                .hasMessage(INPUT_LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
 
     }
 }
