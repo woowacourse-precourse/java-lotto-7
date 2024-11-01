b759b8a9e805ae02301a1b0261a7f32bb89866a6
Jang-GO <jang8195@naver.com>
2024-11-01 18:23:10 +0900

    feat(InputView): 사용자 금액입력 기능 구현

---
+++ src/main/java/lotto/util/InputValidator.java
@@ -0,0 +1,7 @@
+package lotto.util;
+
+public class InputValidator {
+
+    public void validatePrice(int price) {
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,41 @@
 package lotto.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.InputValidator;
 
-    public static int getInput() {
-        return 0;
+public class InputView {
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public int getInput() {
+        while (true) {
+            try {
+                return getPriceInput();
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    private int getPriceInput() {
+        System.out.println("구매하실 금액을 입력해 주세요.");
+        String str = Console.readLine();
+
+        int price = convertStrToInt(str);
+
+        inputValidator.validatePrice(price);
+
+        return price;
+    }
+
+    private static int convertStrToInt(String str) {
+        try{
+            return Integer.parseInt(str);
+        }catch (NumberFormatException ex){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하셔야 합니다.");
+        }
     }
 }
