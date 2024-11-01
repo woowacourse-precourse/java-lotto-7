68c680f6ab57cecfc9cdfbd7a317881fe9a9d125
plan11plan <tjtfy7028@gmail.com>
2024-11-01 13:18:34 +0900

    feat: 로또 구매 금액 입력 View 구현
    - 사용자로부터 구매 금액을 입력받는 기능 구현
    - ThousandWons 도메인 객체로 변환하는 로직 추가

--- src/main/java/lotto/ThousandWons.java
+++ src/main/java/lotto/ThousandWons.java
@@ -1,4 +1,7 @@
 package lotto;
 
 public class ThousandWons {
+    public static ThousandWons of(String money) {
+
+    }
 }
--- src/main/java/lotto/view/TicketInputView.java
+++ src/main/java/lotto/view/TicketInputView.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import lotto.ThousandWons;
 
 public class TicketInputView {
     public ThousandWons initialize() {
-        return null;
+        System.out.println("구입금액을 입력해 주세요.");
+        String money = readLine();
+
+        return ThousandWons.of(money);
     }
 }
