fa7f1baf285173ae19f2c23d4fdfa34ee039d3b6
gracepec <gracepec0621@gmail.com>
2024-11-02 02:13:06 +0900

    feat(InputView): 사용자 입력 받는 클래스 구현
    - 클래스 내에서 문자열 상수 처리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_USER_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static String readPurchaseAmount() {
+        System.out.println(REQUEST_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public static String readWinningNumber() {
+        System.out.println(REQUEST_USER_NUMBER);
+        return Console.readLine();
+    }
+
+    public static String readBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER);
+        return Console.readLine();
+    }
+}
