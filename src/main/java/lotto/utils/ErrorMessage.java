476c3c89ead3b705f26e91b9bf09d84cd4b61cb8
junghun00 <ji8657447@gmail.com>
2024-11-01 23:05:18 +0900

    feat: 에러 메세지 상수화

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -3,6 +3,15 @@ package lotto.utils;
 
 public class ErrorMessage {
     public static final String ERROR = "[ERROR] ";
+    public static final String BONUS_NUMBER_DUPLICATION = "당첨 번호에 없는 숫자를 입력해 주세요";
+    public static final String INVALID_LOTTO = "당첨 번호는 숫자로만 작성해 주세요";
+    public static final String INVALID_LOTTO_NUMBER = "로또 번호는 6개여야 합니다.";
+    public static final String LOTTO_NUMBER_DUPLICATION = "로또 번호는 중복될 수 없습니다.";
+    public static final String INVALID_RANGE = "로또 번호는 1 에서 45 사이로 작성해 주세요";
+    public static final String CAN_BUY = "로또를 구매할 수 없습니다.";
+    public static final String INVALID_MONEY = "1,000원 단위로 입력해 주세요";
+    public static final String INVALID_PRICE = "금액은 숫자로만 입력해 주세요.";
+    public static final String INVALID_BOUNS_NUMBER = "보너스 번호는 숫자만 입력해 주세요";
 
 
 }
