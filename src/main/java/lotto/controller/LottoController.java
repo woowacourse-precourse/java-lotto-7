1eae02fe14e814496899c38f58cc13791d13bae5
Beomseok Noh <prodigy0831@naver.com>
2024-11-01 18:46:29 +0900

    feat(LottoController): 발행한 로또 번호 출력 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,6 +5,7 @@ import static lotto.view.InputView.*;
 import static lotto.view.OutputView.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.model.Lotto;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -17,12 +18,15 @@ public class LottoController {
         int attemptCount = getAttemptCount();
         printPurchaseLottoCount(attemptCount);
         lottoTickets = generateLotto(attemptCount);
+
     }
 
     private List<Lotto> generateLotto(int attemptCount) {
         List<Lotto> lotto = new ArrayList<>();
         for (int count = 1; count <= attemptCount; count++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            printEachLotto(numbers);
             lotto.add(new Lotto(numbers));
         }
         return lotto;
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class OutputView {
     private static final String PURCHASE_AMOUNT_INPUT = "구입 금액을 입력해 주세요.";
     private static final String PURCHASE_Lotto_Count = "개를 구매했습니다.";
@@ -7,7 +10,12 @@ public class OutputView {
     public static void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT);
     }
+
     public static void printPurchaseLottoCount(int Count) {
-        System.out.println(System.lineSeparator()+Count+PURCHASE_Lotto_Count);
+        System.out.println(System.lineSeparator() + Count + PURCHASE_Lotto_Count);
+    }
+
+    public static void printEachLotto(List<Integer> lotto) {
+        System.out.println(lotto);
     }
 }
