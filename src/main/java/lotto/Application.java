52156422ff7f85329a68629ad4978f79c4c4ad7c
Chan531 <chan657312@gmail.com>
2024-11-01 15:43:01 +0900

    feat(Application): InputView 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         LottoController lottoController = new LottoController();
         lottoController.run();
     }
