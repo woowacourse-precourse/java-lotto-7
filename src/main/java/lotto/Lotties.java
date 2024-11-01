adee1a07084ac119a9232a2d35177a9ebe1ab7e3
jang-namu <ironhee8005@gmail.com>
2024-11-01 21:11:38 +0900

    feat: 입력 금액만큼 로또를 발행한다

---
+++ src/main/java/lotto/Lotties.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotties {
+    private final List<Lotto> lottoTickets;
+
+    private Lotties(List<Lotto> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public static Lotties of(List<Lotto> lottoTickets) {
+        return new Lotties(lottoTickets);
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return Collections.unmodifiableList(lottoTickets);
+    }
+
+    public long size() {
+        return lottoTickets.size();
+    }
+}
---
+++ src/main/java/lotto/LottiesFactory.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottiesFactory {
+    private static final long LOTTO_PRICE = 1000L;
+
+    public static Lotties purchaseLotties(Budget budget) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for (long i = 0L; i < budget.getAmount() / LOTTO_PRICE; i++) {
+            lottoTickets.add(Lotto.quickPick());
+        }
+        return Lotties.of(lottoTickets);
+    }
+}
