f2597f0d0e38571ffee5f5d4a92e1fd2715e8ac3
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 03:58:52 +0900

    refactor($LottoShop): 랜덤 Lottos 구매 시 Lottos 의 generateBy 를 사용하도록 변경

--- src/main/java/lotto/model/LottoShop.java
+++ src/main/java/lotto/model/LottoShop.java
@@ -1,27 +1,18 @@
 package lotto.model;
 
 import lotto.model.exception.PurchaseMoneyInvalidException;
-import lotto.model.number_generator.RandomLottoNumberGenerator;
-
-import java.util.stream.IntStream;
+import lotto.model.number_generator.RandomNumberGenerator;
 
 public class LottoShop {
 
     private static final int LOTTO_PRICE = 1000;
 
-    public Lottos purchaseRandomLottos(int money, RandomLottoNumberGenerator randomLottoNumberGenerator) {
+    public Lottos purchaseRandomLottos(int money, RandomNumberGenerator randomNumberGenerator) {
 
         validateMoney(money);
         int lottoCount = money / LOTTO_PRICE;
 
-        return new Lottos(IntStream.range(0, lottoCount)
-                .mapToObj(count -> makeLotto(randomLottoNumberGenerator))
-                .toList());
-    }
-
-    private Lotto makeLotto(RandomLottoNumberGenerator randomLottoNumberGenerator) {
-        LottoNumbers lottoNumbers = new LottoNumbers(randomLottoNumberGenerator.generate());
-        return new Lotto(lottoNumbers);
+        return Lottos.generateBy(randomNumberGenerator, lottoCount);
     }
 
     private void validateMoney(int money) {
