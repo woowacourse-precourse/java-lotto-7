2d24959d1d68b8ae8ba0d23041ef0dc50a5ae0ba
mipangg <morganjeong119@gmail.com>
2024-11-02 02:07:28 +0900

    feat(UserLotto): generate and store random user lotto

--- src/main/java/lotto/model/UserLotto.java
+++ src/main/java/lotto/model/UserLotto.java
@@ -1,4 +1,32 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.model.Lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+
+// User의 로또 리스트를 저장하는 클래스
 public class UserLotto {
+    private int numberOfLottos;
+    private final List<Lotto> userLotto;
+
+    public UserLotto(int numberOfLottos) {
+        this.numberOfLottos = numberOfLottos;
+        userLotto = new ArrayList<>();
+        for (int i = 0; i < numberOfLottos; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            Lotto lotto = new Lotto(numbers);
+            userLotto.add(lotto);
+        }
+    }
+
+    public int getNumberOfLotto() {
+        return numberOfLottos;
+    }
+
+    public List<Lotto> getUserLotto() {
+        return userLotto;
+    }
 }
