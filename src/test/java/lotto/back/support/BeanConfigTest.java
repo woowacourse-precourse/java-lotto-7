469ed9d8b3f84e6e9067d47b2134251088c7c92c
siwoo <k74415834@gmail.com>
2024-11-01 17:04:35 +0900

    feat(ApplicationContext, BeanConfigTest): projectScan 기능과 bean Container 동작을 총괄하는 기능 구현
    - project를 scan하고, scan된 객체들을 bean Container에 등록하는 기능 구현

--- src/test/java/lotto/back/support/BeanConfigTest.java
+++ src/test/java/lotto/back/support/BeanConfigTest.java
@@ -1,6 +1,14 @@
 package lotto.back.support;
 
+import lotto.back.global.support.ApplicationContext;
+import org.junit.jupiter.api.BeforeAll;
+
 class BeanConfigTest {
+
+    @BeforeAll
+    static void setUp() {
+        ApplicationContext.init();
+    }
 
     //TODO 빈으로 등록할 객체를 구현하면서 beanConfig 테스트
     void 빈_생성_테스트() {
