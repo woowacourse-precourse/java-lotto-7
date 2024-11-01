92f472f33146e0300a0e38443b1c142f30d9e2c7
OhYeHyun <ohyhohyl@naver.com>
2024-11-01 21:08:08 +0900

    fix(NumberValidator): 당첨/보너스 번호 개수 확인하는 기능 수정

--- src/main/java/lotto/validator/NumbersValidator.java
+++ src/main/java/lotto/validator/NumbersValidator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.constants.LottoConstants.*;
@@ -18,28 +19,31 @@ public class NumbersValidator {
             return String.format(message, ERROR_MESSAGE_BEGINNING, type, length);
         }
     }
-    private List<Integer> numbers;
 
-    public NumbersValidator(List<Integer> numbers, String type) {
-        this.numbers = numbers;
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public NumbersValidator(String numbersInput) {
+        parseNumbersToList(numbersInput);
+    }
+
+    private void parseNumbersToList(String numbersInput) {
+        for (String number : numbersInput.split(",")) {
+            numbers.add(Integer.parseInt(number));
+        }
     }
 
     public void validateWinningNumbers() {
-
+        validateLength("당첨");
     }
 
     public void validateBonusNumber() {
-
+        validateLength("보너스");
     }
 
-    private void validateLength(List<Integer> numbers, String type) {
+    private void validateLength(String type) {
         int targetLength = LENGTH_BY_NUMBER_TYPE.get(type);
         if (numbers.size() != targetLength) {
             throw new IllegalArgumentException(ErrorMessage.MUST_BE_TARGET_LENGTH.getMessage(type, targetLength));
         }
-    }
-
-    private void validateNumbers() {
-
     }
 }
