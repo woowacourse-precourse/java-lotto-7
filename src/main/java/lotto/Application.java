27b88289a200ab6bbc7b8e571e2e83b60637be52
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-01 11:16:21 +0000

    feat: 보너스 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     private static final String ASK_PURCHASE_MONEY_MESSAGE_STRING = "구입금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBERS_MESSAGE_STRING = "당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER_MESSAGE_STRING = "보너스 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
         
@@ -18,6 +19,11 @@ public class Application {
 
     private String readWinningNumbers() {
         System.out.println(ASK_WINNING_NUMBERS_MESSAGE_STRING);
+        return Console.readLine();
+    }
+
+    private String readBonusNumber() {
+        System.out.println(ASK_BONUS_NUMBER_MESSAGE_STRING);
         return Console.readLine();
     }
 }
