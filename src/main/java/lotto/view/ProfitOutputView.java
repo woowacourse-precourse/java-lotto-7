f0287ca247f155219bbe5b30beb4d1c62dcac338
unh6unh6 <ruddbs3308@naver.com>
2024-11-01 17:34:08 +0900

    fix(view): 잘못된 출력 포맷 수정

--- src/main/java/lotto/view/ProfitOutputView.java
+++ src/main/java/lotto/view/ProfitOutputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 
 public class ProfitOutputView {
 
-    private static final String PROFIT_RATE_MESSAGE_FORMAT = "총 수익률은 %.1f입니다.";
+    private static final String PROFIT_RATE_MESSAGE_FORMAT = "총 수익률은 %.1f%%입니다.";
 
     public void printProfitRate(double profitRate) {
         System.out.printf(PROFIT_RATE_MESSAGE_FORMAT, profitRate);
