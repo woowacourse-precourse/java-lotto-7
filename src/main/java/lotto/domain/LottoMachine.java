36288f644a5888a3e9a931cace3c9c9c75e3706f
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 17:21:33 +0900

    refactor(LottoMachine): 로또 번호 생성 클래스 분리

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+    public List<List<Integer>> generateLottoNumbers(int money) {
+        return IntStream.range(0, money)
+                .mapToObj(i -> {
+                    List<Integer> singleLotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+                    Collections.sort(singleLotto);
+                    return singleLotto;
+                })
+                .collect(Collectors.toList()); // List<List<Integer>>로 수집
+    }
+}
+
