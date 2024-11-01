3d160f46fa8e4f1349c1e129a57d3c2d19826719
joshinhyeok <shtree07@naver.com>
2024-11-01 23:24:05 +0900

    fix: enum 상수 순서 출력양식에 맞게 변경

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,12 +1,12 @@
 package lotto;
 
 public enum LottoRank {
-    FIRST_PRIZE(6, 2_000_000_000),
-    SECOND_PRIZE(5, 30_000_000),
-    THIRD_PRIZE(5,1_500_000),
+    NO_PRIZE(0, 0),
+    FIFTH_PRIZE(3, 5_000),
     FOURTH_PRIZE(4, 50_000),
-    FIFTH_PRIZE(3,5_000),
-    NO_PRIZE(0,0);
+    THIRD_PRIZE(5, 1_500_000),
+    SECOND_PRIZE(5, 30_000_000),
+    FIRST_PRIZE(6, 2_000_000_000);
 
     private final int matchCount;
     private final int prizeAmount;
