31d1842261243658ef73c86561e158968fbefa5d
gitubanana <ptyolo666@gmail.com>
2024-11-01 18:01:47 +0900

    feat(Converter): 로또 숫자 변환 기능
    Converter 클래스는 다음과 같은 기능을 가지고 있다.
    - 문자열 -> 로또 숫자
    - 문자열 -> 로또 숫자 리스트

---
+++ src/main/java/lotto/util/Converter.java
@@ -0,0 +1,44 @@
+package lotto.util;
+
+import static lotto.constant.ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE;
+import static lotto.constant.ErrorMessage.NOT_A_NUMBER;
+import static lotto.constant.LottoInfo.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoInfo.MIN_LOTTO_NUMBER;
+import static lotto.constant.LottoInfo.NUMBER_DELIMITER;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Converter {
+    private static final int INCLUDE_TRAILING = -1;
+
+    private Converter() {
+    }
+
+    public static int toLottoNumber(String number) {
+        int lottoNumber = parseInt(number);
+
+        if (outOfRange(lottoNumber)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+        }
+        return lottoNumber;
+    }
+
+    public static List<Integer> toLottoNumberList(String numbers) {
+        return Arrays.stream(numbers.split(NUMBER_DELIMITER, INCLUDE_TRAILING))
+                .map(Converter::toLottoNumber)
+                .toList();
+    }
+
+    public static int parseInt(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_A_NUMBER.getMessage());
+        }
+    }
+
+    private static boolean outOfRange(int number) {
+        return number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER;
+    }
+}
