3748829b372171ec6b7ddae84c9b60e7eebb7045
chaerish <ccyy8432@naver.com>
2024-11-02 05:12:06 +0900

    feat(InputView): 보너스 번호 입력 메서드 생성
    사용자에게 보너스 번호를 입력받기 위해 InputView의 메서드를 생성한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,4 +13,9 @@ public class InputView {
         return Console.readLine();
     }
 
+    public String getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
 }
