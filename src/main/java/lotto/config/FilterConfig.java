12ba9dd6996f5b5b1bba7944db8876a90c60d2b3
reindeer002 <lsj000424@gmail.com>
2024-11-01 18:29:09 +0900

    feat(story5.2): create FilterConfig to enable usage of FilterChain
    - add FilterConfig class to manage filter chain configuration
    - facilitate integration of various filters for input validation

---
+++ src/main/java/lotto/config/FilterConfig.java
@@ -0,0 +1,42 @@
+package lotto.config;
+
+import lotto.filter.FilterChain;
+import lotto.filter.impl.filter.CommaSeparatedIntegerTypeFilter;
+import lotto.filter.impl.filter.CommaSeparatedNumberInputFormatFilter;
+import lotto.filter.impl.filter.IntegerTypeFilter;
+import lotto.filter.impl.filter.PositiveIntegerFilter;
+import lotto.filter.impl.filterchain.CommaSeparatedNumberFilterChain;
+import lotto.filter.impl.filterchain.PositiveIntegerFilterChain;
+
+public class FilterConfig {
+    private static FilterConfig INSTANCE;
+
+    private final FilterChain positiveIntegerFilterChain = new PositiveIntegerFilterChain();
+    private final FilterChain commaSeparatedNumberFilterChain = new CommaSeparatedNumberFilterChain();
+
+    private FilterConfig() {
+        positiveIntegerFilterChain.addFilter(new IntegerTypeFilter());
+        positiveIntegerFilterChain.addFilter(new PositiveIntegerFilter());
+        commaSeparatedNumberFilterChain.addFilter(new CommaSeparatedNumberInputFormatFilter());
+        commaSeparatedNumberFilterChain.addFilter(new CommaSeparatedIntegerTypeFilter());
+    }
+
+    public static FilterConfig getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new FilterConfig();
+        }
+        return INSTANCE;
+    }
+
+    public static FilterConfig getINSTANCE() {
+        return INSTANCE;
+    }
+
+    public FilterChain getPositiveIntegerFilterChain() {
+        return positiveIntegerFilterChain;
+    }
+
+    public FilterChain getCommaSeparatedNumberFilterChain() {
+        return commaSeparatedNumberFilterChain;
+    }
+}
