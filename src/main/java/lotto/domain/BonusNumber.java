2c7c18d2c93181c4141c1ca2a697854bb72403a9
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-02 03:41:47 +0900

    feat : 보너스 번호 관리하는 클래스 생성
    - 보너스 객체 생성시 유효성 검증하고 반환
    - 보너스 번호가 지정된 범위를 벗어나는지 검증
    - 보너스 번호가 당첨번호 리스트와 중복되는지 검증

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private int bonus;
+
+    public BonusNumber(int bonus, Lotto winningNumber){
+        validate(bonus, winningNumber);
+        this.bonus = bonus;
+    }
+
+    private void validate(int number, Lotto winningNumber) {
+        checkNumberRange(number);
+        checkNumberDuplicated(number, winningNumber);
+    }
+
+    private void checkNumberRange(int number){
+        if(number < 1 || number > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 숫자 범위를 벗어납니다.");
+        }
+    }
+
+    private void checkNumberDuplicated(int number, Lotto winningNumber){
+        if (winningNumber.getNumbers().contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
+        }
+    }
+}
