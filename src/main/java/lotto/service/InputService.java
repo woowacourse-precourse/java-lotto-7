06881cf6eec4ecd8724cf8c4f4d1450b8e85fe38
daxxn <dahyeonhan4529@gmail.com>
2024-11-01 19:27:35 +0900

    feat(service): add new methods in input service
    - Implement validateWinningNumber method to check if each element is number, non-duplicate, and within lotto range
    - Implement extractWinningNumbers method to parse and validate a comma-separated string of winning numbers
    - Implement isLottoRange method to check if number is within lotto range(1~45)

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,11 +1,14 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.error.CustomException;
 import lotto.error.ExceptionMessage;
 
 public class InputService {
     private final String NUMBER_REGEX = "^(-[1-9][0-9]*|[1-9][0-9]*|0)$";
     private final int LOTTO_PRICE = 1000;
+    private final int LOTTO_NUMBER_COUNT = 6;
 
     public boolean isNumber(String number) {
         return number.matches(NUMBER_REGEX);
@@ -17,6 +20,10 @@ public class InputService {
 
     public boolean isValidCost(int cost) {
         return (cost % LOTTO_PRICE == 0);
+    }
+
+    public boolean isLottoRange(int number) {
+        return number >= 1 && number <= 45;
     }
 
     public int validateLottoCost(String lottoCost) {
@@ -36,4 +43,32 @@ public class InputService {
     public int calculateLottoCount(int cost) {
         return cost / LOTTO_PRICE;
     }
+
+    public int validateWinningNumber(List<Integer> winningNumbers, String element) {
+        if (!isNumber(element)) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_NUMBER);
+        }
+        int number = Integer.parseInt(element);
+        if (!isLottoRange(number)) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_IN_LOTTO_NUMBER_RANGE);
+        }
+        if (winningNumbers.contains(number)) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_DUPLICATED_LOTTO_NUMBER);
+        }
+        return number;
+    }
+
+    public List<Integer> extractWinningNumbers(String winningNumber) {
+        String[] parsedElements = winningNumber.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String element : parsedElements) {
+            int number = validateWinningNumber(winningNumbers, element);
+            winningNumbers.add(number);
+        }
+        if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_VALID_LOTTO_NUMBERS);
+        }
+        return winningNumbers;
+    }
+
 }
