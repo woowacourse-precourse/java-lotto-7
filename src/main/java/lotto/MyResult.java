504607971a24dcb8b2026dd5422b2ba9791c3ab5
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 22:36:34 +0900

    chore(MyResult) : 불필요한 라인 삭제

--- src/main/java/lotto/MyResult.java
+++ src/main/java/lotto/MyResult.java
@@ -3,31 +3,21 @@ package lotto;
 public class MyResult {
     private Integer matches;//몇개 맞췄는지
     private Boolean bonusPoint;//보너스 맞췄는지
-    private String price;//총 탄 금액
 
     public MyResult(Integer matches, Boolean bonusPoint) {
         this.matches = matches;
         this.bonusPoint = bonusPoint;
-        if(this.matches == 1) this.price = "0";
-        else if(this.matches == 2) this.price = "0";
-        else if(this.matches == 3) this.price = "5,000";
-        else if(this.matches == 4) this.price = "50,000";
-        else if(this.matches == 5 && bonusPoint) this.price = "30,000,000";
-        else if(this.matches == 5 && !bonusPoint) this.price = "1,500,000";
-        else if(this.matches == 6) this.price = "2,000,000,000";
     }
 
     public Integer getMatches() {
         return matches;
     }
-    public String getPrice() {
-        return price;
-    }
+
     public Boolean getBonusPoint() {
         return bonusPoint;
     }
 
-    public static Integer getMyRevenue(GradeSaver grades){
+    public static Integer getMyRevenue(WinningDetails grades){
         int revenue = 0;
         revenue += grades.getThird() * 5000;
         revenue += grades.getFourth() * 50000;
