93827b38349b0389103bfc9dec97bd5d6a157d16
bk <naturalbb360@gmail.com>
2024-11-01 14:52:05 +0900

    feat(Lotto): 로또 번호 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +13,37 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        isValidNumberRange(numbers);
+        isDuplicate(numbers);
+        isValidNumberSize(numbers);
+        Collections.sort(numbers);
+    }
+
+    private void isValidNumberSize(List<Integer> numbers) {
+        final int LOTTO_SIZE = 6;
+        if(numbers.size() != LOTTO_SIZE){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void isValidNumberRange(List<Integer> numbers){
+        final Integer LOTTO_MIN_NUM = 1;
+        final Integer LOTTO_MAX_NUM = 45;
+
+        for(Integer Number: numbers){
+            if((Number < LOTTO_MIN_NUM) || (Number > LOTTO_MAX_NUM)){
+                throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getErrorMessage());
+            }
+        }
+    }
+
+    private void isDuplicate(List<Integer> numbers){
+        List<Integer> duplicateNumbers = new ArrayList<>();
+        for(Integer Number: numbers){
+            if(duplicateNumbers.contains(Number)){
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getErrorMessage());
+            }
+            duplicateNumbers.add(Number);
+        }
+    }
 }
