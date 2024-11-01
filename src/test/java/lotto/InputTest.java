a907f55b109bfbbedf3602f84abae9d2619fe389
Kiki1875b <kevinheo0413@gmail.com>
2024-11-01 13:52:43 +0900

    test(InputTest) : 1000원으로 나누어 떨어지지 않는 가격 예외 테스트

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -42,9 +42,18 @@ public class InputTest {
         @DisplayName("양수가 아닌 가격이 입력되면 예외처리를 한다")
         @ParameterizedTest()
         @ValueSource(strings = {"-1", "0", "-999999999", "-100000000"})
-        void 양수가_아닌_가격이_입력되면_예외처리를_한다(){
+        void 양수가_아닌_가격이_입력되면_예외처리를_한다(String input){
             assertThrows(IllegalArgumentException.class, () -> {
-                validator.validatePrice(null);
+                validator.validatePrice(input);
+            });
+        }
+
+        @DisplayName("1000으로 나누어 떨어지지 않는 가격 입력시 예외 처리를 한다")
+        @ParameterizedTest()
+        @ValueSource(strings = {"1", "10", "100", "1001", "987654321"})
+        void 천원으로_나누어_떨어지지_않는_가격_예외(String input){
+            assertThrows(IllegalArgumentException.class, () -> {
+                validator.validatePrice(input);
             });
         }
     }
