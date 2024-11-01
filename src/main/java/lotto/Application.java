d7eb708dc3eca9e640a8f8e46d39b053a5c283d8
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 13:59:26 +0900

    feat: 로또 구매 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playLotto();
+    }
+
+    private static void playLotto() {
+        int money = inputMoney();
+    }
+
+    public static int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input) / 1000;
     }
 }
