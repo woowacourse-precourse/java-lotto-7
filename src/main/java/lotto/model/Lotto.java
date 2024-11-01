d8ba3e7748b03372ad8e15f58a511083e13bf88c
daxxn <dahyeonhan4529@gmail.com>
2024-11-02 02:44:58 +0900

    feat(lotto): add utility methods in lotto class
    - Implement isDuplicated method to check if duplicated numbers exist
    - Implement getNumber method to get numbers
    - Implement hasNumber method to check if the number included in numbers
    - Implement getCorrectNumberCount  method to calculate the count of matching numbers between two lottos

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.error.CustomException;
+import lotto.error.ExceptionMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,9 +17,31 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_VALID_LOTTO_NUMBERS);
+        }
+        if (isDuplicated(numbers)) {
+            throw new CustomException(ExceptionMessage.ERROR_MESSAGE_DUPLICATED_LOTTO_NUMBER);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isDuplicated(List<Integer> numbers) {
+        Set<Integer> numberNotDuplicated = new HashSet<>(numbers);
+        return (numberNotDuplicated.size() != numbers.size());
+    }
+
+    private List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public boolean hasNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    public int getCorrectNumberCount(Lotto winningLotto) {
+        List<Integer> winningLottoNumbers = new ArrayList<>(winningLotto.getNumbers());
+        int totalSize = winningLottoNumbers.size() + this.numbers.size();
+        winningLottoNumbers.addAll(numbers);
+        Set<Integer> mergeLottoNumbers = new HashSet<>(winningLottoNumbers);
+        return totalSize - mergeLottoNumbers.size();
+    }
 }
