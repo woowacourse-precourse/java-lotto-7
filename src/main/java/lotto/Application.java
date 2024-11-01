a96cdc2062cdf1fbb686929417e79ec22a2b5041
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-01 11:12:51 +0000

    feat: 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
+    private static final String ASK_PURCHASE_MONEY_MESSAGE_STRING = "구입금액을 입력해 주세요.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+    }
+
+    private String readPurchaseMoney() {
+        System.out.println(ASK_PURCHASE_MONEY_MESSAGE_STRING);
+        return Console.readLine();        
     }
 }
