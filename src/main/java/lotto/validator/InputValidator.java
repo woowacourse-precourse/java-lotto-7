e90864c9b23b7c47571e9765db1114ecca902e5d
chaerish <ccyy8432@naver.com>
2024-11-02 01:56:47 +0900

    feat(InputValidator): 입력 값이 1000원 단위인지 검증하는 로직 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -10,5 +10,8 @@ public class InputValidator {
         if (Integer.parseInt(amount) < LIMIT_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
         }
+        if (!(Integer.parseInt(amount) % LIMIT_AMOUNT == 0)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위어야 합니다.");
+        }
     }
 }
