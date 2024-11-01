93d6bacb306f903c1627508fba73739e76b993b1
theplayer1003 <theplayer1003@gmail.com>
2024-11-01 14:52:58 +0900

    refactor(service): 로또 타입 변경(set)으로 인한 리팩토링

--- src/main/java/lotto/service/LottoManager.java
+++ src/main/java/lotto/service/LottoManager.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.service.domain.lotto.Lotto;
@@ -13,20 +14,20 @@ public class LottoManager implements LottoValueProvider {
     private LottoBuyer lottoBuyer;
 
     @Override
-    public List<Lotto> makePurchasedLotto(int money) {
+    public Set<Lotto> makePurchasedLotto(int money) {
         Budget budget = new Budget(money);
         RandomNumberGenerator lottoGenerator = new WoowaLottoGenerator();
         this.lottoBuyer = makeLottoBuyer(budget, lottoGenerator);
-        List<Lotto> purchasedLotto = lottoBuyer.getPurchasedLotto();
+        Set<Lotto> purchasedLotto = lottoBuyer.getPurchasedLotto();
 
         return purchasedLotto;
     }
 
     private LottoBuyer makeLottoBuyer(Budget budget, RandomNumberGenerator lottoGenerator) {
         int purchaseCount = (budget.getBudget() / 1_000);
-        List<Lotto> purchasedLotto = IntStream.range(0, purchaseCount)
+        Set<Lotto> purchasedLotto = IntStream.range(0, purchaseCount)
                 .mapToObj(i -> new Lotto(lottoGenerator.makeLottoRandomNumber()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
 
         return new LottoBuyer(purchasedLotto, budget);
     }
--- src/main/java/lotto/service/LottoValueProvider.java
+++ src/main/java/lotto/service/LottoValueProvider.java
@@ -1,9 +1,10 @@
 package lotto.service;
 
 import java.util.List;
+import java.util.Set;
 import lotto.service.domain.lotto.Lotto;
 
 public interface LottoValueProvider {
-    List<Lotto> makePurchasedLotto(int money);
+    Set<Lotto> makePurchasedLotto(int money);
     void makeWinningStatistics();
 }
