d276787916300f265ef73e5ace5972d15ea33e1a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-01 22:15:40 +0900

    feat(view/InputView): InputView 클래스 작성
    - 사용자 입력을 받기 위한 InputView 클래스 작성
    - 구입 금액, 당첨 번호, 보너스 번호 입력을 위한 메시지 상수 정의
    - readInput 메서드를 통해 메시지를 출력하고 입력을 읽어오는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,27 @@
 package lotto.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+	private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+	private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+	private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+	private static String readInput(String message) {
+		System.out.println(message);
+		String input = Console.readLine();
+		return input;
+	}
+
+	public static String inputPurchasePrice() {
+		return readInput(PURCHASE_PRICE_MESSAGE);
+	}
+
+	public static String inputWinningNumbers() {
+		return readInput(WINNING_NUMBERS_MESSAGE);
+	}
+
+	public static String inputBonusNumber() {
+		return readInput(BONUS_NUMBER_MESSAGE);
+	}
 }
