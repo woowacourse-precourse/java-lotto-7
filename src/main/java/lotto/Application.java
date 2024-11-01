9ddda498cd4d2612593e41ccb394e2fdb9a12a11
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 14:12:49 +0900

    refactor: 클래스로 옮긴 메서드들 삭제 후, 클래스를 통해 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,110 +1,88 @@
 package lotto;
 
 import static lotto.ErrorMessage.DUPLICATE_BONUS_NUMBER;
-import static lotto.ErrorMessage.INVALID_PRICE;
-import static lotto.ErrorMessage.NON_NUMERIC_INPUT;
 import static lotto.ErrorMessage.NUMBER_OUT_OF_RANGE;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import lotto.model.BonusNumber;
 import lotto.model.Lotto;
 import lotto.model.Money;
+import lotto.model.PurchasedLottoNumbers;
+import lotto.model.RandomNumbers;
 import lotto.validation.Parser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     private String input;
+
+    // 컨트롤러
     public int divideByThousand(String input) {
         try{
             new Money(Parser.stringToInt(input));
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
-            System.out.println("다시 입력해 주세요.");
+            OutputView.printInputAgainPrompt();
             return divideByThousand(Console.readLine());
         }
         this.input = input;
         return Integer.parseInt(input) / 1000;
     }
 
-    public String requestPurchaseAmountInput(){
-        System.out.println("구입 금액을 입력해 주세요.");
-        return Console.readLine();
-    }
-
+    // 서비스 계층
     public ArrayList<List<Integer>> generateLottoNumbers(int times){
-        System.out.println(times + "개를 구매했습니다.");
-        ArrayList<List<Integer>> purchasedLottoNumbers = new ArrayList<>();
+        OutputView.printPurchasedTicketCount(times);
+        PurchasedLottoNumbers purchasedLottoNumbers = new PurchasedLottoNumbers();
+        // 이 과정은 RandomNumbers
         for (int i = 0; i < times; i++) {
-            List<Integer> purchasedLottoNumber = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
-            Collections.sort(purchasedLottoNumber);
-            purchasedLottoNumbers.add(purchasedLottoNumber);
+            List<Integer> purchasedLottoNumber = RandomNumbers.generateSortedLottoNumbers();
+            purchasedLottoNumbers.makePurchasedLottoNumbers(purchasedLottoNumber);
         }
-        printLottoNumbers(purchasedLottoNumbers);
-        return purchasedLottoNumbers;
-    }
-    public void printLottoNumbers(ArrayList<List<Integer>> purchasedLottoNumbers){
-        for (List<Integer> purchasedLottoNumber : purchasedLottoNumbers) {
-            System.out.println(purchasedLottoNumber);
-        }
-    }
-    public String requestLotto(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        OutputView.printPurchasedLottoNumbers(purchasedLottoNumbers.getPurchasedLottoNumbers());
+        return purchasedLottoNumbers.getPurchasedLottoNumbers();
     }
 
-    public List<Integer> splitNumbers(String input){
-        List<Integer> numbers = new ArrayList<>();
-        String [] splitInput =  input.split(",");
-        for(String i: splitInput){
-            numbers.add(Integer.parseInt(i));
-        }
-        return numbers;
-    }
+    // 컨트롤러
 
-    public Lotto generateLotto(String input) {
-        List<Integer> numbers = splitNumbers(input);
+    public Lotto generateLotto() {
+        List<Integer> numbers = InputView.requestLotto();
         try {
             return new Lotto(numbers); // 올바른 Lotto 객체 생성
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            System.out.println("다시 입력해 주세요.");
-            return generateLotto(Console.readLine());
+            OutputView.printInputAgainPrompt();
+            return generateLotto();
         }
     }
 
-    public String requestBonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
-    }
-
-    public void isNumberBetween1And45(int bonusNumber){
-        if(bonusNumber < 1 || bonusNumber > 45){
-            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.format());
-        }
-    }
+    // 서비스 계층
     public void checkForDuplicates(List<Integer> numbers, int bonusNumber){
         if(numbers.contains(bonusNumber)){
             throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER.format());
         }
     }
 
+    // 컨트롤러
     public int generateBonusNumber(Lotto lotto, String bonusNumber){
         try {
-            isNumberBetween1And45(Parser.stringToInt(bonusNumber));
+            BonusNumber bonusNumber1 = new BonusNumber(Parser.stringToInt(bonusNumber));
             checkForDuplicates(lotto.getNumbers(), Integer.parseInt(bonusNumber));
+            return bonusNumber1.getBonusNumber();
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
-            System.out.println("다시 입력해 주세요.");
+            OutputView.printInputAgainPrompt();
             return generateBonusNumber(lotto, Console.readLine());
         }
-        return Integer.parseInt(bonusNumber);
     }
 
+    // 서비스 계층
+
     public int[] calculateLottoRank(ArrayList<List<Integer>> purchasedLottoNumbers){
-        Lotto lotto = generateLotto(requestLotto());
-        int bounsNumber = generateBonusNumber(lotto, requestBonusNumber());
+        Lotto lotto = generateLotto();
+        int bounsNumber = generateBonusNumber(lotto, InputView.requestBonusNumber());
         int [] rank = {0,0,0,0,0};
         for (int i = 0; i < purchasedLottoNumbers.size(); i++) {
             int count = 0;
@@ -134,23 +112,25 @@ public class Application {
 
     }
 
-    public void printResult(int [] rank){
-        System.out.println("3개 일치 (5,000원) - " + rank[0] + "개");
-        System.out.println("4개 일치 (50,000원) - " + rank[1] + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + rank[2] + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rank[3] + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + rank[4] + "개");
-
+    // 서비스 계층으로
+    private double getTotalRate(int[] rank) {
         int total = rank[0] * 5000 + rank[1] * 50000 + rank[2] * 1500000 + rank[3] * 30000000 + rank[4] * 2000000000;
         double totalRate = (double) total / Integer.parseInt(input) * 100;
         totalRate = Math.round(totalRate * 10) / 10.0;
+        return totalRate;
+    }
 
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", totalRate);
+    // 컨트롤러
+
+    public void printResult(int [] rank){
+        double totalRate = getTotalRate(rank);
+        OutputView.printLottoResults(rank,totalRate);
     }
 
 
     public static void main(String[] args) {
         Application application = new Application();
-        application.printResult(application.calculateLottoRank(application.generateLottoNumbers(application.divideByThousand(application.requestPurchaseAmountInput()))));
+        application.printResult(application.calculateLottoRank(application.generateLottoNumbers(application.divideByThousand(
+                InputView.requestPurchaseAmountInput()))));
     }
 }
