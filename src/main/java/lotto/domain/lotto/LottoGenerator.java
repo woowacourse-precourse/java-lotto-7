5854002504813760133c06c46ff0056496d54476
donghyunn1 <dongdong0516@naver.com>
2024-11-02 02:45:34 +0900

    refactor: 로또 생성 숫자 매직넘버화

--- src/main/java/lotto/domain/lotto/LottoGenerator.java
+++ src/main/java/lotto/domain/lotto/LottoGenerator.java
@@ -7,11 +7,15 @@ import java.util.List;
 
 public class LottoGenerator {
 
+    private static final int INTEGER_UNDER_BOUND = 1;
+    private static final int INTEGER_UPPER_BOUND = 45;
+    private static final int NUMBER_COUNT = 6;
+
     private final List<List<Integer>> Lottos = new ArrayList<>();
 
     public LottoGenerator(int numOfLotto) {
         for (int i = 0; i < numOfLotto; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(INTEGER_UNDER_BOUND,INTEGER_UPPER_BOUND,NUMBER_COUNT);
             Lotto lotto = new Lotto(numbers);
             Lottos.add(lotto.getNumbers());
         }
