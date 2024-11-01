e9f1294cdb1eaaa1d2b9c54a08591d53bcdde7db
이윤빈 <kanado20000@naver.com>
2024-11-01 11:23:30 +0900

    :sparkles: feat: 발행한 모든 로또를 관리하는 Lottos 클래스 추가
    createLottos 메서드는 정적 팩토리 메서드로 각 Lotto 객체를 생성하고 마지막에 Lottos 객체를 생성한다.
    allLottosToString 메서드 사용자에게 발행한 모든 로또를 출력하기 위해 각 로또 리스트를 문자열로 번환한다.

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.utilities.Random;
+
+public class Lottos {
+  private final List<Lotto> lottos;
+
+  private Lottos(List<Lotto> lottos) {
+    this.lottos = lottos;
+  }
+
+  public static Lottos createLottos(int quantityOfLottos) {
+    List<Lotto> lottos =
+        IntStream.range(0, quantityOfLottos)
+            .mapToObj(i -> new Lotto(Random.lottoGenerator()))
+            .collect(Collectors.toList());
+    return new Lottos(lottos);
+  }
+
+  public String allLottosToString() {
+    StringBuilder sb = new StringBuilder();
+    for (Lotto lotto : lottos) {
+      sb.append(lotto.sortedNumbersToString()).append(System.lineSeparator());
+    }
+    return sb.toString();
+  }
+}
