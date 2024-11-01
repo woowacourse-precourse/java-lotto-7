1eb22fc59a43044cc0d1cb033bf29a38b102bd9d
Yooooonseo <81520018+Yooooonseo@users.noreply.github.com>
2024-11-01 23:47:44 +0900

    test: Lotto 클래스에 대한 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+    @DisplayName("로또_번호의_개수가_6개가_넘어가면_예외가_발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,6 +23,22 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또_번호가_1부터_45_사이가_아니면_예외가_발생한다.")
+    @Test
+    void 로또_번호가_1부터_45_사이가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 666)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바른_로또번호_생성_테스트")
+    @Test
+    void 올바른_로또번호_생성_테스트() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6); // List<Integer> 타입으로 로또 번호 선언
+        Lotto lotto = new Lotto(numbers); // Lotto 객체 생성 시 List<Integer> 전달
+        assertEquals(numbers, lotto.getNumbers()); // 예상한 로또 번호가 그대로 생성되는지 확인
+    }
+
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
