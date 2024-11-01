a7ec3e261300d6808d4d9843c23b3ec0e082a2ec
cherryiJuice <chldlwn711@gmail.com>
2024-11-01 20:01:38 +0900

    refactor: 출력 메세지 추가

--- src/main/java/lotto/constants/WinnerConstants.java
+++ src/main/java/lotto/constants/WinnerConstants.java
@@ -1,20 +1,22 @@
 package lotto.constants;
 
 public enum WinnerConstants {
-    FIRST_WINNER(1, 6, 2000000000),
-    SECOND_WINNER(2, 6, 30000000),
-    THIRD_WINNER(3, 5, 1500000),
-    FOURTH_WINNER(4, 4, 50000),
-    FIFTH_WINNER(5, 3, 5000);
+    FIRST_WINNER(1, 6, 2000000000, "6개 일치"),
+    SECOND_WINNER(2, 5, 30000000, "5개 일치, 보너스 볼 일치"),
+    THIRD_WINNER(3, 5, 1500000, "5개 일치"),
+    FOURTH_WINNER(4, 4, 50000, "4개 일치"),
+    FIFTH_WINNER(5, 3, 5000, "3개 일치");
 
     private final int rank;
     private final int matchCount;
     private final long prizeMoney;
+    private final String message;
 
-    WinnerConstants(final int rank, final int matchCount, final long prizeMoney) {
+    WinnerConstants(final int rank, final int matchCount, final long prizeMoney, final String message) {
         this.rank = rank;
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.message = message;
     }
 
     public int getRank() {
@@ -27,5 +29,9 @@ public enum WinnerConstants {
 
     public long getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,7 +9,7 @@ import lotto.dto.LottoResultDto;
 public class OutputView {
     private static final String PURCHASE_COUNT = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS = "당첨 통계\n---";
-    private static final String PRIZE_DETAIL = "%d개 일치 (%,d원) - %d개\n";
+    private static final String PRIZE_DETAIL = "%s (%,d원) - %d개\n";
     private static final String REVENUE_RESULT = "총 수익률은 %.1f%%입니다.";
 
 
@@ -27,7 +27,7 @@ public class OutputView {
         WINNERS.forEach(winner -> {
             System.out.printf(
                     PRIZE_DETAIL,
-                    winner.getMatchCount(),
+                    winner.getMessage(),
                     winner.getPrizeMoney(),
                     dto.prizeMatchResults().getOrDefault(winner.getRank(), 0)
             );
