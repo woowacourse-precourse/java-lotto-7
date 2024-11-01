a3f0292307334057ba44239002274eac8335245d
armcortex3445 <wlghks5966@naver.com>
2024-11-01 21:10:10 +0900

    <feat>(utils) add random numbers generations
    - define NumberList class to create integer list sent to Lotto class
    - To handle random number from user or automatic generation, define interface method

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 - this project is Korean Lotto machine service.
 - you can play simple simulation Korean Lotto.
 
+## function
+
+### Lotto relation
+
+- generate 6 numbers randomly and non-duplicated from 1 to 45 
+   - [ ] generate random number from 1 to 45
+   - [ ] generate randomly 6 numbers
+   - [ ] validate numbers **whether number is duplicated or not** 
---
+++ src/main/java/lotto/utils/NumberList.java
@@ -0,0 +1,72 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.constant.ExceptionMessage;
+
+
+public class NumberList extends ArrayList<Integer> {
+
+    static final int[] NUMBER_RANGE = {1,45};
+    static final int NUMBER_RANGE_START = 0;
+    static final int NUMBER_RANGE_END = 1;
+
+    static final int  MAX_SIZE = 6;
+
+    public NumberList(){
+        super();
+    }
+
+    static public void isListOverMaxLength(List<Integer> list, int maxLength){
+        /*TODO
+         * Exception Message를 ENUM 이나 상수로 정의하여 사용하기.
+         * */
+        if(list.size() > maxLength){
+            throw new IllegalArgumentException(ExceptionMessage.LIST_OVER_MAX_LENGTH.getMessage());
+        }
+    }
+
+    public int generateRandomNumber(){
+        return Randoms.pickNumberInRange( NUMBER_RANGE[NUMBER_RANGE_START], NUMBER_RANGE[NUMBER_RANGE_END]);
+    }
+
+
+    public NumberList addNewNumber(int newNumber){
+
+
+        if(isAddPossible()&&!isAlreadyNumberAdded(newNumber)){
+            this.add(newNumber);
+        }
+
+        return this;
+    }
+
+    public boolean isAlreadyNumberAdded(int newNumber){
+        boolean[] isNumberGenerated = new boolean[NUMBER_RANGE[NUMBER_RANGE_END]+1];
+
+        for(int num : this ){
+            isNumberGenerated[num] = true;
+        }
+
+        return isNumberGenerated[newNumber];
+
+    }
+
+    public boolean isAddPossible(){
+        return this.size() < MAX_SIZE;
+    }
+
+    public NumberList generateRandomNumberList(){
+
+        while(isAddPossible()){
+            int newNumber = generateRandomNumber();
+            addNewNumber(newNumber);
+        }
+
+        return this;
+
+    }
+
+
+}
