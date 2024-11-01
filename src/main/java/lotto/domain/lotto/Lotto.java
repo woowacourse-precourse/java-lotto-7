f1124fc473eb3136db220b083002a13c416c6274
donghyunn1 <dongdong0516@naver.com>
2024-11-02 02:40:00 +0900

    feat: 로또 중복된 숫자 예외처리 기능 구현

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
@@ -15,6 +16,14 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (numbers.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 숫자는 중복될 수 없습니다.");
+            }
         }
     }
 
