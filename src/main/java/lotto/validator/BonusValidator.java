995e7b6ef19ca8ac94c404ae7229c38cef65f3bc
Hyso <hsk7953@gmail.com>
2024-11-01 21:32:56 +0900

    refactor(BonusValidator, BonusValidatorTest) : 보너스 번호 중복 여부 검증 로직 위치 이동 - validateDuplicate 의 로직을 WinningNumbers 클래스로 이동

--- src/main/java/lotto/validator/BonusValidator.java
+++ src/main/java/lotto/validator/BonusValidator.java
@@ -12,24 +12,24 @@ public class BonusValidator implements Validator {
     }
 
     @Override
-    public void validate(String input){
+    public void validate(String input) {
         validateNull(input);
         validateNumeric(input);
         validateNonZeroStart(input);
         int number = Integer.parseInt(input);
         validateRange(number);
-        validateDuplicate(winningNumbers,number);
+        validateDuplicate(number);
     }
 
-    private void validateRange(int number){
+    private void validateRange(int number) {
 
-        if(number < 1 || number > 45){
+        if (number < 1 || number > 45) {
             throw new IllegalArgumentException(ExceptionMessage.OUT_OF_RANGE.getMessage());
         }
     }
 
-    private void validateDuplicate(List<Integer> winningNumbers, int number){
-        if(winningNumbers.contains(number)){
+    private void validateDuplicate(int number) {
+        if (winningNumbers.contains(number)) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_BONUS_NUMBER.getMessage());
         }
     }
--- src/test/java/lotto/validator/BonusValidatorTest.java
+++ src/test/java/lotto/validator/BonusValidatorTest.java
@@ -17,7 +17,7 @@ class BonusValidatorTest {
     private BonusValidator bonusValidator;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
         bonusValidator = new BonusValidator(list);
     }
@@ -33,8 +33,8 @@ class BonusValidatorTest {
 
     @DisplayName("0으로 시작하는 숫자일시, 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings ={"0","01000","03000"})
-    void validateNonZeroStart_throwsException(String input){
+    @ValueSource(strings = {"0", "01000", "03000"})
+    void validateNonZeroStart_throwsException(String input) {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> bonusValidator.validate(input));
         assertEquals(ExceptionMessage.INVALID_ZERO_START.getMessage(), exception.getMessage());
@@ -42,29 +42,20 @@ class BonusValidatorTest {
 
     @DisplayName("숫자가 아닌 다른 문자일시, 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings ={"a","aaa","bbb","10b3","100!0"})
-    void validateNumeric_throwsException(String input){
+    @ValueSource(strings = {"a", "aaa", "bbb", "10b3", "100!0"})
+    void validateNumeric_throwsException(String input) {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> bonusValidator.validate(input));
-        assertEquals(ExceptionMessage.INVALID_NON_NUMERIC.getMessage(),exception.getMessage());
+        assertEquals(ExceptionMessage.INVALID_NON_NUMERIC.getMessage(), exception.getMessage());
     }
 
     @DisplayName("범위를 초과하는 숫자 일시, 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"46","50","100"})
-    void validateRange_throwsException(String input){
+    @ValueSource(strings = {"46", "50", "100"})
+    void validateRange_throwsException(String input) {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> bonusValidator.validate(input));
         assertEquals(ExceptionMessage.OUT_OF_RANGE.getMessage(), exception.getMessage());
-    }
-
-    @DisplayName("당첨 번호와 중복되는 번호일시, 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings ={"1", "2","3","4"})
-    void validateDuplicate_throwsException(String input){
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> bonusValidator.validate(input));
-        assertEquals(ExceptionMessage.INVALID_BONUS_NUMBER.getMessage(), exception.getMessage());
     }
 
 }
