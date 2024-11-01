fe911e31944061462a45325388a6e38d0f81dcdd
EHCTH <lees90896@gmail.com>
2024-11-01 19:08:08 +0900

    test(InputViewTest): LottoMahcine 리팩토링에 의한 수정

--- src/test/java/lotto/view/input/InputViewTest.java
+++ src/test/java/lotto/view/input/InputViewTest.java
@@ -1,18 +1,13 @@
 package lotto.view.input;
 
 import lotto.buyer.domain.InsertMoneyService;
+import lotto.lotto.domain.*;
 import lotto.money.domain.Money;
 import lotto.buyer.infrastructure.InsertPurchaseMoney;
-import lotto.lotto.domain.LottoMachine;
-import lotto.lotto.domain.LottoTickets;
-import lotto.lotto.domain.NumberGenerator;
 import lotto.lotto.infrastructure.DivideThousandCalculator;
 import lotto.lotto.winning.domain.BonusNumber;
 import lotto.lotto.winning.domain.WinningLotto;
-import lotto.lotto.winning.infrastructure.BonusNumberCreator;
-import lotto.lotto.winning.infrastructure.WinningLottoCreator;
 import lotto.view.input.domain.InputService;
-import lotto.view.input.hanlder.domain.InputHandlerService;
 import lotto.view.input.hanlder.infrastructure.MoneyHandler;
 import lotto.view.output.domain.PurchaseCountViewService;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,19 +15,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class InputViewTest {
     private static Money money;
+    private static LottoMachine lottoMachine;
 
     @BeforeEach
     void init() {
         InsertMoneyService insertMoneyService = new InsertPurchaseMoney(new MoneyHandler(new MoneyInputTest()));
         money = insertMoneyService.insert();
+        lottoMachine = new LottoMachine(new DivideThousandCalculator(), new LottoCreatorTest(), new PurchaseOutputViewTest());
     }
 
     @Test
@@ -54,7 +49,6 @@ public class InputViewTest {
                 "[4, 5, 6, 7, 8, 9]",
                 "[2, 3, 42, 43, 44, 45]\n"
         );
-        LottoMachine lottoMachine = new LottoMachine(new DivideThousandCalculator(), new NumberGenerateTest(), new PurchaseOutputViewTest());
         LottoTickets lottoTickets = lottoMachine.purchaseLottoTickets(money);
         assertThat(lottoTickets.toString()).isEqualTo(excepted);
     }
@@ -62,9 +56,7 @@ public class InputViewTest {
     @Test
     @DisplayName("당첨 번호를 입력해 주세요")
     void winningLottoTest() {
-        WinningLottoCreator winningLottoCreator = createManager("1,2,3,4,5,6", WinningLottoHandlerTest::new, WinningLottoCreator::new);
-
-        WinningLotto winningLotto = winningLottoCreator.create();
+        WinningLotto winningLotto = lottoMachine.createWinningLotto();
 
         assertThat(winningLotto.toString()).isEqualTo("1,2,3,4,5,6");
     }
@@ -72,21 +64,11 @@ public class InputViewTest {
     @Test
     @DisplayName("보너스 번호를 입력해주세요")
     void BonusLottoTest() {
-        WinningLottoCreator winningLottoCreator = createManager("1,2,3,4,5,6", WinningLottoHandlerTest::new, WinningLottoCreator::new);
-        BonusNumberCreator bonusNumberCreator = createManager("7", BonusNumberHandlerTest::new, BonusNumberCreator::new);
-
-        WinningLotto winningLotto = winningLottoCreator.create();
-        BonusNumber bonusNumber = bonusNumberCreator.create(winningLotto);
+        WinningLotto winningLotto = lottoMachine.createWinningLotto();
+        BonusNumber bonusNumber = lottoMachine.createBonusNumber(winningLotto);
 
         assertThat(bonusNumber.toString()).isEqualTo("7");
     }
-
-    private <T, R> R createManager(String input, Function<String, T> transformer, Function<T, R> finalProcessor) {
-        T intermediateResult = transformer.apply(input);
-        return finalProcessor.apply(intermediateResult);
-    }
-
-
 }
 
 class MoneyInputTest implements InputService {
@@ -102,47 +84,28 @@ class PurchaseOutputViewTest implements PurchaseCountViewService {
     }
 }
 
-class NumberGenerateTest implements NumberGenerator {
-    private static int start = 0;
-    List<List<Integer>> list = List.of(
-            new ArrayList<>(List.of(45, 44, 43, 42, 41, 40)),
-            new ArrayList<>(List.of(39, 38, 37, 36, 35, 34)),
-            new ArrayList<>(List.of(33, 32, 31, 30, 29, 28)),
-            new ArrayList<>(List.of(27, 26, 25, 24, 23, 22)),
-            new ArrayList<>(List.of(21, 20, 19, 18, 17, 16)),
-            new ArrayList<>(List.of(15, 14, 13, 12, 11, 10)),
-            new ArrayList<>(List.of(9, 8, 7, 6, 5, 4)),
-            new ArrayList<>(List.of(45, 44, 43, 42, 2, 3))
-    );
-
+class LottoCreatorTest implements LottoCreatorService {
     @Override
-    public List<Integer> generate() {
-        return list.get(start++);
-    }
-}
-
-class WinningLottoHandlerTest implements InputHandlerService {
-    private final String input;
-
-    public WinningLottoHandlerTest(String input) {
-        this.input = input;
+    public LottoTickets createLottoTickets(int count) {
+        return LottoTickets.of(List.of(
+                new Lotto(List.of(45, 44, 43, 42, 41, 40)),
+                new Lotto(List.of(39, 38, 37, 36, 35, 34)),
+                new Lotto(List.of(33, 32, 31, 30, 29, 28)),
+                new Lotto(List.of(27, 26, 25, 24, 23, 22)),
+                new Lotto(List.of(21, 20, 19, 18, 17, 16)),
+                new Lotto(List.of(15, 14, 13, 12, 11, 10)),
+                new Lotto(List.of(9, 8, 7, 6, 5, 4)),
+                new Lotto(List.of(45, 44, 43, 42, 2, 3))
+        ));
     }
 
     @Override
-    public <R> R retrieveReceive(Function<String, R> function) {
-        return function.apply(input);
-    }
-}
-
-class BonusNumberHandlerTest implements InputHandlerService {
-    private final String input;
-
-    public BonusNumberHandlerTest(String input) {
-        this.input = input;
+    public BonusNumber createBonusNumber(WinningLotto winningLotto) {
+        return BonusNumber.of("7");
     }
 
     @Override
-    public <R> R retrieveReceive(Function<String, R> function) {
-        return function.apply(input);
+    public WinningLotto createWinningLotto() {
+        return WinningLotto.of("1,2,3,4,5,6");
     }
 }
