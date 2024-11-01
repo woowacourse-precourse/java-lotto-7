783f6c89be25bd027bd2c9ba3eae3133761a32c4
imjunghun <dlawndgns9@naver.com>
2024-11-01 14:36:42 +0900

    feat(Result): 당첨 결과를 Rank 별 개수를 저장한 Result 클래스 구현
    - Result는 Rank 클래스를 EnumMap을 갖고 해당 Rank 별 Lotto 당첨 개수를 센다

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+
+public class Result {
+
+    private Map<Rank, Integer> results;
+
+    private Result(Lottos lottos, AnswerNumbers answerNumbers) {
+        setUp();
+        process(lottos, answerNumbers);
+    }
+
+    public static Result of(Lottos lottos, AnswerNumbers answerNumbers) {
+        return new Result(lottos, answerNumbers);
+    }
+
+    private void setUp() {
+        this.results = new EnumMap<>(Rank.class);
+        Arrays.stream(Rank.values())
+                .forEach(rank -> results.put(rank, 0));
+    }
+
+    private void process(Lottos lottos, AnswerNumbers answerNumbers) {
+        for (Lotto lotto : lottos.getLottos()) {
+            count(answerNumbers.compare(lotto));
+        }
+    }
+
+    private void count(Rank rank) {
+        results.put(rank, results.get(rank) + 1);
+    }
+
+    public int getCount(Rank rank) {
+        return results.get(rank);
+    }
+}
