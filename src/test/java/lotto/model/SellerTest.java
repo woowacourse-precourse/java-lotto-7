6bc426b593181ea7814d0a4363dcfe748df33a23
seophohoho <seop0937@gmail.com>
2024-11-01 18:46:02 +0900

    test(Seller): Lotto 발행 시, 6개인지 각 번호가 1~45인지 검사하는 테스트 코드 추가

--- src/test/java/lotto/model/SellerTest.java
+++ src/test/java/lotto/model/SellerTest.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +30,19 @@ public class SellerTest {
 
         assertThatThrownBy(() -> seller.validate("9999"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Lotto 발행 테스트")
+    void testGetLottoTickets() {
+        seller.createLottoTickets(8000);
+
+        for (int i = 0; i < seller.getLottoTickets().size(); i++) {
+            Lotto lotto = seller.getLottoTickets().get(i);
+
+            assertThat(lotto.getNumbers().size()).isEqualTo(6);
+            assertThat(lotto.getNumbers().stream().allMatch(num -> num >= 1 && num <= 45)).isTrue();
+        }
+
     }
 }
