664b121e1087bcd3c7d2fc43b5007d6d4f667a65
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 14:44:59 +0900

    test(Lotto): 로또에 0이 포함되는 테스트를 정의하라

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -21,5 +21,9 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호에_0이_포함된다면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf((IllegalArgumentException.class));
+    }
 }
