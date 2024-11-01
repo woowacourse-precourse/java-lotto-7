40eb04faf894aedf8347f382e97eb902eede0a7c
yush <yush2202@gmail.com>
2024-11-02 05:22:54 +0900

    feat($ Prizes): 상 추가 코멘트 구현
    상에 따라 추가 코멘트를 출력하기 위해 추가 코멘트 구현

--- src/main/java/lotto/constants/Prizes.java
+++ src/main/java/lotto/constants/Prizes.java
@@ -2,20 +2,22 @@ package lotto.constants;
 
 public enum Prizes {
 
-    FIRST_PRIZE(2_000_000_000, 6, false),
-    SECOND_PRIZE(30_000_000, 5, true),
-    THIRD_PRIZE(1_500_000, 5, false),
-    FOURTH_PRIZE(50_000, 4, false),
-    FIFTH_PRIZE(5_000, 3, false);
+    FIRST_PRIZE(2_000_000_000, 6, false, ""),
+    SECOND_PRIZE(30_000_000, 5, true, ", 보너스 볼 일치"),
+    THIRD_PRIZE(1_500_000, 5, false, ""),
+    FOURTH_PRIZE(50_000, 4, false, ""),
+    FIFTH_PRIZE(5_000, 3, false, "");
 
     private final int money;
     private final int count;
     private final boolean bonus;
+    private final String bounsComment;
 
-    Prizes(int money, int count, boolean bonus) {
+    Prizes(int money, int count, boolean bonus, String bounsComment) {
         this.money = money;
         this.count = count;
         this.bonus = bonus;
+        this.bounsComment = bounsComment;
     }
 
     public int getMoney() {
@@ -28,5 +30,9 @@ public enum Prizes {
 
     public boolean getBonus() {
         return bonus;
+    }
+
+    public String getComment() {
+        return bounsComment;
     }
 }
