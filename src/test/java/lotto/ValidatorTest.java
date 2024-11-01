c44e79700390ef6052fb15010e963498022605e1
pil-go <pilbongx@naver.com>
2024-11-02 07:11:23 +0900

    test(ValidatorTest): 정수 범위 검사 기능 구현 전 테스트 추가

--- src/test/java/lotto/ValidatorTest.java
+++ src/test/java/lotto/ValidatorTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.Utils.Validator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -47,6 +48,22 @@ public class ValidatorTest {
     @ValueSource(strings = {"1원", "1.0", "", " "})
     public void 문자열이_숫자아닌_문자가_있으면_True를_반환한다(String input) {
         boolean result = Validator.isNotNumeric(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void 숫자문자열이_int타입_범위를_벗어나면_false를_반환한다() {
+        String input = String.valueOf(Integer.MAX_VALUE + 1L);
+
+        boolean result = Validator.isInteger(input);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void 숫자문자열이_int타입_범위이면_true를_반환한다() {
+        String input = String.valueOf(Integer.MAX_VALUE);
+
+        boolean result = Validator.isInteger(input);
         assertThat(result).isTrue();
     }
 
