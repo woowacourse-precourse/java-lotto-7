f4d56fc7418caf5b5ed3ebe011cae0ccb51fbe89
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-02 02:11:56 +0900

    test : UserMainLottoFactory 테스트

--- src/test/java/lotto/domain/factory/UserMainLottoFactoryTest.java
+++ src/test/java/lotto/domain/factory/UserMainLottoFactoryTest.java
@@ -4,16 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import lotto.domain.lottos.Lotto;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserMainLottoFactoryTest {
+    private final UserMainLottoFactory factory = new UserMainLottoFactory();
 
 
     @DisplayName("6자리 숫자가 같은지 확인")
     @Test
     void Lotto_객체_생성_확인() {
-        UserMainLottoFactory factory = new UserMainLottoFactory();
         List<Integer> inputNumbers = List.of(1, 2, 3, 4, 5, 6);
         String input = "1,2,3,4,5,6";
         boolean expected = true;
@@ -25,6 +26,35 @@ class UserMainLottoFactoryTest {
             }
         }
         assertTrue(expected);
-
     }
+
+    @DisplayName("콤마 이외 특수문자 입력시 예외 처리")
+    @Test
+    void 잘못된_구분자_예외_처리() {
+        String input = "1,2,3,5%,6";
+        AssertionsForClassTypes.assertThatThrownBy(() -> factory.make(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 빈_입력값_예외_처리() {
+        String input = "";
+        AssertionsForClassTypes.assertThatThrownBy(() -> factory.make(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 공백_입력값_예외_처리() {
+        String input = " ";
+        AssertionsForClassTypes.assertThatThrownBy(() -> factory.make(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 빈_입력값_포함_예외_처리() {
+        String input = "1,2,3,4,,5";
+        AssertionsForClassTypes.assertThatThrownBy(() -> factory.make(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
