be0e622c3cf53702f3e0ee3d00aef6487afad2a2
Chanyoo Kim <cksdb012@gmail.com>
2024-11-01 18:16:21 +0900

    refactor: 함수명 변경
    예외 처리 함수의 이름을 validate로 변경

--- src/main/java/lotto/view/InputPurchaseAmoutVIew.java
+++ src/main/java/lotto/view/InputPurchaseAmoutVIew.java
@@ -2,31 +2,31 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.exception.PurchaseAmountFormatException;
-import lotto.exception.PurchaseAmountNumberException;
+import lotto.exception.PurchaseAmountTypeException;
 import lotto.exception.PurchaseAmountZeroException;
 
 public class InputPurchaseAmoutVIew {
-    public static int purchaseAmountInput(){
+    public static int purchaseAmountInput() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        int purchaseAmount = purchaseAmountValidate(input);
+        int purchaseAmount = validate(input);
 
         return purchaseAmount;
     }
 
-    private static int purchaseAmountValidate(String input){
-        try{
+    private static int validate(String input) {
+        try {
             int purchaseAmount = Integer.parseInt(input);
-            if(purchaseAmount <= 0){
+            if (purchaseAmount <= 0) {
                 throw new PurchaseAmountZeroException();
             }
-            if(purchaseAmount % 1000 != 0){
+            if (purchaseAmount % 1000 != 0) {
                 throw new PurchaseAmountFormatException();
             }
 
             return purchaseAmount;
-        }catch(NumberFormatException e){
-            throw new PurchaseAmountNumberException();
+        } catch (NumberFormatException e) {
+            throw new PurchaseAmountTypeException();
         }
     }
 }
