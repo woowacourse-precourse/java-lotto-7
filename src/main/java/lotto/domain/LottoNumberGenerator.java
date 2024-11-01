64a11a4c3dd76ca5a0c5db67a48f7c4711dacdf7
GO-TE <alsrudxo9810@naver.com>
2024-11-01 17:13:23 +0900

    로또 숫자 생성 기능 구현
    - domain 패키지로 분류
    - Lotto domain 패키지로 분류

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+
+public class LottoNumberGenerator {
+    public List<Integer> generateNumbers() {
+        List<Integer> numbers = pickUniqueNumbersInRange(1, 45, 6);
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+}
