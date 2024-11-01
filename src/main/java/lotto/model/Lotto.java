a2e6e123ee328086851ea472e8896a12779a5d8b
junho767 <junho767@naver.com>
2024-11-01 20:11:21 +0900

    feat: 로또 번호의 중복 여부를 검사 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,26 +1,43 @@
 package lotto.model;
 
+import lotto.Message.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validDuplicates(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_COUNT.toString());
         }
     }
+
+    private void validDuplicates(List<Integer> numbers) {
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_DUPLICATES.toString());
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() < numbers.size();
+    }
+
 
     public List<Integer> getNumbers() {
         return numbers;
     }
 
-    public int getMatchCount(Lotto winningNumbers) {
+    public int countMatches(Lotto winningNumbers) {
         return (int) numbers.stream()
                 .filter(winningNumbers.getNumbers()::contains)
                 .count();
