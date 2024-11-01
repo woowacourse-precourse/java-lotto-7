143e473c4cd0777b6c3ea6f3f77ab16d57f1ba23
StoneCAU <seokwoo1999@naver.com>
2024-11-01 21:28:59 +0900

    refactor(Application): Controller에 StatService 매개변수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,13 +3,15 @@ package lotto;
 import lotto.controller.LottoGameController;
 import lotto.service.BuyerService;
 import lotto.service.LottoService;
+import lotto.service.StatService;
 
 public class Application {
     public static void main(String[] args) {
         final BuyerService buyerService = new BuyerService();
         final LottoService lottoService = new LottoService();
+        final StatService statService = new StatService();
 
-        LottoGameController lottoGameController = new LottoGameController(buyerService, lottoService);
+        LottoGameController lottoGameController = new LottoGameController(buyerService, lottoService, statService);
         lottoGameController.run();
     }
 }
