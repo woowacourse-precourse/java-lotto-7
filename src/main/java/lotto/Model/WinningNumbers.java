9a0ca72507a276bbc7b5f2e663a3800985e6331f
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:22:37 +0900

    feat(WinningNumbers): 당첨금액 일급컬렉션을 구현하라

---
+++ src/main/java/lotto/Model/WinningNumbers.java
@@ -0,0 +1,11 @@
+package lotto.Model;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private List<Integer> winningNumbers;
+
+    public WinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+}
