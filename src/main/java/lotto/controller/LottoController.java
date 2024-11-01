7e0f84a4b47c65e8bca0ce126ce016ab69f0414a
LeeDongHoon <likelion.leedh@gmail.com>
2024-11-01 19:30:17 +0900

    [FEAT] 구매 금액 입력, 당첨 번호 입력 로직 추가 purchase - 입력받은 값을 dto에 넣어 MyLottoInfo에 전달해 인스턴스 생성 - 구매 개수와 로또 출력
    winnings()
    - 당첨 번호 및 보너스 번호를 입력받아 dto에 각각 넣음
    - dto를 바탕으로 당첨 로또 인스턴스 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,24 +1,36 @@
 package lotto.controller;
 
-import lotto.service.LottoService;
+import lotto.model.MyLottoInfo;
+import lotto.model.WinningLotto;
+import lotto.dto.BonusNumberDto;
+import lotto.dto.PurchaseAmountDto;
+import lotto.dto.WinningLotteryDto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
-    private final LottoService lottoService;
-
-    public LottoController(LottoService lottoService) {
-        this.lottoService = lottoService;
-    }
+    private MyLottoInfo myLottoInfo;
+    private WinningLotto winningLotto;
 
     public void purchase(){
-
+        PurchaseAmountDto dto = InputView.inputPurchaseAmount();
+        myLottoInfo = MyLottoInfo.from(dto);
+        outputPurchaseInfo();
     }
 
     public void winnings(){
-
+        WinningLotteryDto winningLotteryDto = InputView.inputWinningLottery();
+        BonusNumberDto bonusNumberDto = InputView.inputBonusNumber();
+        winningLotto = WinningLotto.from(winningLotteryDto, bonusNumberDto);
     }
 
     public void result(){
 
+    }
+
+    private void outputPurchaseInfo() {
+        OutputView.outputLottoPurchaseAmount(myLottoInfo.getPurchaseLottoCount());
+        OutputView.outputPurchaseLotto(myLottoInfo.getMyLotteries());
     }
 }
