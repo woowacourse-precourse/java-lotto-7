b87af713bbd033417a24d2c854c4b86639e9236e
ruka-sh <ori8591y@gmail.com>
2024-11-01 22:46:00 +0900

    feat: 당첨 번호와 보너스 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,71 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        List<Integer> winningNumbers = inputWinningNumbers();
+        int bonusNumber = inputBonusNumber(winningNumbers);
+
+    }
+
+    static List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요");
+        String firstInput = Console.readLine();
+        System.out.println(firstInput);
+
+        List<Integer> list = Arrays.stream(firstInput.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed().collect(Collectors.toList());
+
+        try {
+            Lotto lotto = new Lotto(list);
+
+            return lotto.getNumbers();
+        } catch(IllegalArgumentException e) {
+            System.out.println(e);
+
+            return inputWinningNumbers();
+        }
+
+    }
+
+    static int inputBonusNumber(List<Integer> winningNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요");
+        String secondInput = Console.readLine();
+        System.out.println(secondInput);
+
+        try {
+            int bonusNumber = Integer.parseInt(secondInput);
+
+            validateBonusNumber(bonusNumber, winningNumbers);
+            return bonusNumber;
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+            return inputBonusNumber(winningNumbers);
+        }
+
+    }
+
+    private static void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 }
+
+
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    int bonusNumber;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +12,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        for(int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
 }
