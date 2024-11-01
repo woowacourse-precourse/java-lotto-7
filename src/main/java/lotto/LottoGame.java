5a435f959af308c02bd622d58e356002ddf0ca65
woodongle <315ehdgur@gmail.com>
2024-11-01 18:49:48 +0900

    feat: 사용자 입력 검증 추가
    1. 사용자 입력이 1,000원 단위인지 확인하는 메서드를 추가합니다.
      - checkUnitOfPurchaseAmount()

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -13,14 +13,22 @@ public class LottoGame {
     private void purchaseAmountInput() {
         System.out.println("구입금액을 입력해 주세요.");
         purchaseAmount = Integer.parseInt(checkPositiveNumber(Console.readLine()));
+        checkUnitOfPurchaseAmount(purchaseAmount);
         System.out.println(purchaseAmount);
     }
 
+    // 1,000원 단위인지 확인하는 메서드
+    private void checkUnitOfPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
     // 양수인지 확인하는 메서드
-    private String checkPositiveNumber(String number) {
-        if (!number.matches("^[1-9]\\d*$")) {
+    private String checkPositiveNumber(String input) {
+        if (!input.matches("^[1-9]\\d*$")) {
             throw new IllegalArgumentException("[ERROR] 입력한 값은 숫자(양수)여야 합니다.");
         }
-        return number;
+        return input;
     }
 }
