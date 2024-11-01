ce97ccd0cbea3a266b3ab3a054b728defc10329d
Sinorang <sinholang@naver.com>
2024-11-01 11:32:21 +0900

    feat: 당첨 번호 개수 검증 로직 구현

--- src/main/java/lotto/constants/ErrorMessageConstants.java
+++ src/main/java/lotto/constants/ErrorMessageConstants.java
@@ -6,4 +6,5 @@ public class ErrorMessageConstants {
     public static final String INVALID_NON_POSITIVE_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 양의 정수로 입력해야 합니다.";
     public static final String INVALID_LOTTO_AMOUNT_UNIT = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
 
+    public static final String INVALID_WINNING_NUMBER_COUNT = "[ERROR] 당첨 번호는 6개를 입력해야 합니다.";
 }
--- src/main/java/lotto/validation/WinningNumberValidator.java
+++ src/main/java/lotto/validation/WinningNumberValidator.java
@@ -1,2 +1,17 @@
-package lotto.validation;public class WinningNumberValidator {
+package lotto.validation;
+
+import lotto.constants.ErrorMessageConstants;
+
+import java.util.List;
+
+public class WinningNumberValidator {
+    public static void validateWinningNumbers(List<Integer> winningNumbers) {
+        validateWinningNumberCount(winningNumbers);
+    }
+
+    private static void validateWinningNumberCount(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessageConstants.INVALID_WINNING_NUMBER_COUNT);
+        }
+    }
 }
