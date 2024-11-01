cfdb6eaaa616f8ff51ac6e01dac97febc019495b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-01 15:48:45 +0900

    feat(InputView): 입력 문구 상수화

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class InputView {
+
+    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+}
