dadd76226223ad78869811ac9ac770955cb33a9f
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-02 01:56:22 +0900

    refactor : 예외 메시지 변경

--- src/main/java/lotto/enums/ExceptionMessage.java
+++ src/main/java/lotto/enums/ExceptionMessage.java
@@ -7,7 +7,7 @@ public enum ExceptionMessage {
     INVALID_MONEY_UNIT(String.format("%d원 단위로 입력해야합니다.", MONEY_UNIT.getValue())),
     ZERO_EXCEPTION("0 이상의 수를 입력해주세요."),
     INVALID_NUMBER_OF_WINNING_NUMBERS("당첨 번호의 갯수는 6개여야 합니다."),
-    INVALID_WINNING_NUM_RANGE("당첨 번호는 1이상 45이여야 합니다."),
+    INVALID_LOTTO_NUM_RANGE("로또 번호는 1이상 45이여야 합니다."),
     INVALID_WINNING_NUM_DUPLICATE("중복된 당첨번호가 있어서는 안됩니다.")
     ;
 
