7ab5b71076af2f52be43018d4489e4ae873569e6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-01 22:01:30 +0900

    refactor($WinningLotto): LottoNumberInvalidException 를 던지도록 변경

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -43,7 +43,7 @@ public class WinningLotto {
 
     private void validateBonusNumber(List<LottoNumber> numbers, LottoNumber bonusNumber) {
         if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호에 포함되서는 안됩니다.");
+            throw LottoNumberInvalidException.bonusNumberDuplicate();
         }
     }
 }
