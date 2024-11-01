368362e90129e0052055c1a5dc8b8cc311dd4f21
ppzepi <hsunguk1999@gmail.com>
2024-11-01 21:37:19 +0900

    refactor: statisticStart 메서드를 statisticEnd를 호출하도록 변경

--- src/main/java/lotto/lottoView/OutputView.java
+++ src/main/java/lotto/lottoView/OutputView.java
@@ -5,11 +5,11 @@ import lotto.lottoModel.StatisticsLottoDTO;
 
 public class OutputView {
     private static final String HOW_MANY_BUY = "%d개를 구매했습니다.";
-    private static final String STATISTIC_START = "당첨 통계\n---";
+    private static final String STATISTIC_START = "당첨 통계/n---";
     private static final String VALUE_MATCH_START = "%d개 일치";
-    private static final String VALUE_MATCH_END = " (%d원) - %d개";
+    private static final String VALUE_MATCH_END = " (%,d원) - %d개";
     private static final String BONUS_MATCH = ", 보너스 볼 일치";
-    private static final String PROFIT_MESSAGE = "총 수익률은 %f %입니다.";
+    private static final String PROFIT_MESSAGE = "총 수익률은 %f \%입니다.";
 
 
 
@@ -18,8 +18,11 @@ public class OutputView {
         System.out.println();
     }
 
-    public void statisticStart() {
+    public void statisticStart(StatisticsLottoDTO stats) {
         System.out.println(STATISTIC_START);
+        for (int i=3;i<=6;i++){
+            statisticEnd(i,stats);
+        }
     }
 
     public void statisticEnd(int i, StatisticsLottoDTO stats) {
