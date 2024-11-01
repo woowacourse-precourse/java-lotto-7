0361f1c812da8381a2eedc661d415ed42dcb9c1c
yeong-hwan <jghff700@naver.com>
2024-11-02 00:26:00 +0900

    test(view): resovle output issue caused by print stream

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import lotto.enums.ErrorMessage;
 import lotto.enums.LottoConstants;
 import org.junit.jupiter.api.AfterEach;
@@ -14,18 +16,25 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InputViewTest {
 
     private final InputStream originalIn = System.in;
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
 
     @BeforeEach
     void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
+
 
     @AfterEach
     void tearDown() {
         System.setIn(originalIn); // 테스트를 마치면 입력 버퍼를 flush
+        System.setOut(originalOut); // 테스트를 마치면 출력 버퍼를 flush
         Console.close();
     }
 
@@ -35,14 +44,23 @@ class InputViewTest {
 
 
     @Test
-    @DisplayName("구입 금액이 1000원 단위로 입력되지 않으면 예외 발생")
-    void shouldThrowExceptionWhenInvalidPurchaseAmount() {
+    @DisplayName("구입 금액이 1000원 단위로 입력되지 않으면 예외 메세지 출력")
+    void shouldDisplayErrorMessageForInvalidPurchaseAmount() {
         // Given
-        setInput("1500"); // 1000원 단위가 아님
+        String invalidAmount = "5001\n5000\n";
+        setInput(invalidAmount);
 
-        // When & Then
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, InputView::getPurchaseAmount);
-        assertEquals(ErrorMessage.INVALID_AMOUNT.getMessage(), exception.getMessage());
+        // When(doesnt throw exception. just print error message)
+        InputView.getPurchaseAmount();
+
+        String output = outputStreamCaptor.toString().trim();
+
+        // Then
+        String[] messages = output.split("\n");
+        String lastMessage = messages[messages.length - 2]; // 마지막 메시지만 검증
+
+        // Then
+        assertEquals(ErrorMessage.INVALID_AMOUNT.getMessage(), lastMessage);
     }
 
     @Test
@@ -57,4 +75,5 @@ class InputViewTest {
         // Then
         assertEquals(5000, purchaseAmount);
     }
+
 }
