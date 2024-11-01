097ec6408209c48dc00e75607ef6687ffc038513
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 14:29:40 +0900

    feat(Lotto): 중복을 검증하는 기능을 구현하라

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,15 +6,25 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLength(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static void validateDuplicate(List<Integer> numbers) {
+        long uniqueCount = numbers.stream()
+                .distinct()
+                .count();
+
+        if (uniqueCount != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호는 뽑으실 수 없습니다.");
+        }
+    }
+
 }
