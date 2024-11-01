1930d81a936960c8dcdb9e224d4b2257e31fccf3
hanseounggyun <hanseu9839@gmail.com>
2024-11-01 20:46:25 +0900

    feature: statistics 당첨 통계 클래스

--- src/main/java/lotto/statistics/Statistics.java
+++ src/main/java/lotto/statistics/Statistics.java
@@ -1,2 +1,31 @@
-package lotto.statistics;public class Statistics {
+package lotto.statistics;
+
+import lotto.Lottos;
+import lotto.Prize;
+import lotto.number.LottoNumber;
+import lotto.winner.WinnerLottoNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Statistics {
+    private static final List<Prize> ALL_PRIZES = List.of(Prize.NOTHING, Prize.FIVE, Prize.FOUR, Prize.THREE, Prize.TWO, Prize.ONE);
+    private static final int LOTTO_PRICE = 1000;
+
+
+    public double rateOfReturn(Map<Prize, Integer> prizes, int count) {
+        int allMountMoney = calculateAllMountMoney(prizes, 0);
+
+        double rate = (double) allMountMoney / (double) (count * LOTTO_PRICE) * 100;
+        return Math.round(rate * 10) / 10.0;
+    }
+
+    private static int calculateAllMountMoney(Map<Prize, Integer> prizes, int allMountMoney) {
+        for (Prize prize : ALL_PRIZES) {
+            allMountMoney += prizes.getOrDefault(prize, 0) * prize.getPrice();
+        }
+        return allMountMoney;
+    }
+
 }
--- src/test/java/lotto/statistics/StatisticsTest.java
+++ src/test/java/lotto/statistics/StatisticsTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.statistics;
+
+import lotto.Prize;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StatisticsTest {
-  
+
+    @Test
+    void 당첨금_수입률_계산_테스트() {
+        Statistics statistics = new Statistics();
+        assertThat(statistics.rateOfReturn(Map.of(Prize.FIVE,1), 8)).isEqualTo(62.5);
+    }
+
 }
