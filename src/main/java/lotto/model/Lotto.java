fc26c556a90675f35f8aa62832ca24004aad1b4d
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-01 18:41:16 +0900

    feat( Lotto.java ) Add Method
    Return validate Method
    Call static ValidationUtils.validateNumber Check

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,24 +1,25 @@
 package lotto.model;
 
+import lotto.common.utils.ValidationUtils;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        //validate(numbers);
+        validate(numbers);
+        ValidationUtils.validateDuplicateWinNumber(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() { return numbers; }
 
-
-//    private void validate(List<Integer> numbers) {
-//        if (numbers.size() != 6) {
-//            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-//        }
-//    }
-
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
 
 
     // TODO: 추가 기능 구현
