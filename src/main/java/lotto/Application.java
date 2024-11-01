8a35dd9fb50c774ac429954b6ed9fd42cad1f5a4
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 14:26:34 +0900

    feat: 무작위 로또 번호 생성하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,10 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,6 +13,8 @@ public class Application {
 
     private static void playLotto() {
         int money = inputMoney();
+        List<List<Integer>> lottoNumbers = generateLottoNumbers(money);
+        printLotto(lottoNumbers);
     }
 
     public static int inputMoney() {
@@ -16,4 +22,22 @@ public class Application {
         String input = Console.readLine();
         return Integer.parseInt(input) / 1000;
     }
+
+    private static List<List<Integer>> generateLottoNumbers(int money) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        System.out.println("\n" + money + "개를 구매했습니다.");
+        for (int i = 0; i < money; i++) {
+            List<Integer> singleLotto = Randoms.pickUniqueNumbersInRange(1,45,6);
+            Collections.sort(singleLotto);
+            lottoNumbers.add(singleLotto);
+        }
+        return lottoNumbers;
+    }
+
+    private static void printLotto(List<List<Integer>> lottoNumbers) {
+        lottoNumbers.stream()
+                .map(Object::toString)
+                .forEach(System.out::println);
+    }
+
 }
