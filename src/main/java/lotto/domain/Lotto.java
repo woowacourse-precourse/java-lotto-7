31cb08dd33459f640b6fdbeb858227717fef7c54
YoonSeungHyun2 <dbstmdgus5050@gmail.com>
2024-11-01 21:04:00 +0900

    feat. 디렉토리 구조 설계 및 기본 코드 생성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+
+
+}
