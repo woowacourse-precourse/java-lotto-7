d306d60bc092d07c1aa2dc56f28f3b583e686130
JeongHwan Kim <silverice1973@naver.com>
2024-11-01 10:43:58 +0900

    feat: 로또 발행 기능 및 번호 생성 구현

---
+++ src/main/java/lotto/machine/LottoMachine.java
@@ -0,0 +1,30 @@
+package lotto.machine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static List<Lotto> generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = generateLottoNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
+    }
+
+    public static List<Integer> generateLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
