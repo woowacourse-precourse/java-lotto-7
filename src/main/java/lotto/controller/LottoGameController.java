fc9123059d0b84a627784f66183fee79c283685b
StoneCAU <seokwoo1999@naver.com>
2024-11-01 21:28:16 +0900

    feat(LottoGameController): 통계와 수익률을 받아와 출력하는 메서드 구현
    - getWinningSummary: statService를 통해 통계와 수익률을 얻어와 출력한다

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -4,24 +4,34 @@ import static lotto.view.OutputView.printLottoExceptionMessage;
 import static lotto.view.OutputView.printLottoGroup;
 import static lotto.view.OutputView.printNewLine;
 import static lotto.view.OutputView.printPurchaseMessage;
+import static lotto.view.OutputView.printReturnRate;
+import static lotto.view.OutputView.printWinningStatsTitle;
+import static lotto.view.OutputView.printWinningSummary;
 
 import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
 import lotto.domain.Lottos;
+import lotto.domain.Revenue;
 import lotto.domain.WinningNumbers;
+import lotto.dto.WinningStat;
 import lotto.exception.LottoException;
 import lotto.service.BuyerService;
 import lotto.service.LottoService;
+import lotto.service.StatService;
+import lotto.utils.WinningSummaryGenerator;
 import lotto.view.InputView;
 
 public class LottoGameController {
     private final BuyerService buyerService;
     private final LottoService lottoService;
+    private final StatService statService;
 
-    public LottoGameController(BuyerService buyerService, LottoService lottoService) {
+    public LottoGameController(BuyerService buyerService, LottoService lottoService, StatService statService) {
         this.buyerService = buyerService;
         this.lottoService = lottoService;
+        this.statService = statService;
     }
 
     public void run() {
@@ -29,6 +39,8 @@ public class LottoGameController {
         final Lottos lottos = getLottos(lottoQuantity);
         final WinningNumbers winningNumbers = getWinningNumbers();
         final BonusNumber bonusNumber = getBonusNumber(winningNumbers);
+        final LottoGame lottoGame = getLottoGame(lottos, winningNumbers, bonusNumber);
+        getWinningSummary(lottoQuantity, lottoGame);
     }
 
     private int getLottoQuantity() {
@@ -48,7 +60,7 @@ public class LottoGameController {
 
     private Lottos getLottos(int lottoQuantity) {
         Lottos lottos = lottoService.createLottos(lottoQuantity);
-        List<Lotto> lottoGroup = lottoService.convertToLottoGroup(lottos);
+        List<Lotto> lottoGroup = lottos.getLottoGroup();
 
         printLottoGroup(lottoGroup);
 
@@ -83,5 +95,21 @@ public class LottoGameController {
 
             return getBonusNumber(winningNumbers);
         }
+    }
+
+    private LottoGame getLottoGame(Lottos lottos, WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        return lottoService.createLottoGame(lottos, winningNumbers, bonusNumber);
+    }
+
+    private void getWinningSummary(int lottoQuantity, LottoGame lottoGame) {
+        printWinningStatsTitle();
+
+        List<WinningStat> winningStats = statService.getWinningStats(lottoGame);
+
+        printWinningSummary(WinningSummaryGenerator.generate(winningStats));
+
+        Revenue revenue = statService.createRevenue(lottoQuantity, winningStats);
+
+        printReturnRate(revenue);
     }
 }
