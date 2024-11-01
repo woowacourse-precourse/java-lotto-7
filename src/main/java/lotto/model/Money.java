56811863a8ef09f033bdddf91facf9426e9b9626
Dev-LHY <dev.leehy@gmail.com>
2024-11-02 05:45:53 +0900

    refactor(moneyValidator): 검증 로직 분리
    - 검증 로직을 분리하여 MoneyValidator로 이동하였음
    - MONEY_UNIT 접근 제어자를 private to public으로 변경하여 MoneyValidator에서도 접근 가능하게 하였음

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,22 +1,14 @@
 package lotto.model;
 
-import lotto.exception.ExceptionMessage;
+import static lotto.validation.MoneyValidator.validateMoney;
 
 public class Money {
-    private static final int MONEY_UNIT = 1000;
-    private static final int VALIDATE_VALUE = 0;
-
+    public static final int MONEY_UNIT = 1000;
     private final int money;
 
     public Money(int money) {
         validateMoney(money);
         this.money = money;
-    }
-
-    private void validateMoney(int money) {
-        if (money % MONEY_UNIT != VALIDATE_VALUE){
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_MONEY_INPUT.getMessage());
-        }
     }
 
     public int getQuantity() {
