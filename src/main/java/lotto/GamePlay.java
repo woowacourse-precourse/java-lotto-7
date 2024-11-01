078c3b617a6f255bd852bef59a2dca283cad4d9a
김선우 <kgw6147@naver.com>
2024-11-01 21:12:23 +0900

    feat(GamePlay) : 로또 당첨 번호 및 보너스 번호 입력 받는 기능 - 로또 번호 입력 - 보너스 번호 입력

--- src/main/java/lotto/GamePlay.java
+++ src/main/java/lotto/GamePlay.java
@@ -2,19 +2,42 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.*;
 
 public class GamePlay {
 
     private final int LOTTO_MONEY = 1000;
     private int useMoneys;
+    private Lotto winningLotto;
+    private int bonusNumber;
 
     public GamePlay() {
-        System.out.println("로또 구입 금액을 입력해주세요. 단, 1000원 단위로 입력해주세요.");
         useMoneys = getMoney();
+        winningLotto = getWinningLotto();
+        bonusNumber = getBonusNumber();
+    }
+
+    private int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private Lotto getWinningLotto() {
+        System.out.println("당첨 번호 6자리를 입력해주세요. (,) 쉼표를 기준으로 구분됩니다.");
+
+        return new Lotto(stream(Console.readLine().split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
     }
 
     private int getMoney() {
+        System.out.println("로또 구입 금액을 입력해주세요. 단, 1000원 단위로 입력해주세요.");
         int money = Integer.parseInt(Console.readLine());
         return money / LOTTO_MONEY;
     }
