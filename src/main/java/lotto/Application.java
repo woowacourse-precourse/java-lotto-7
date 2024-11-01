e12bbcf34f7c025a00f701ac45886c78a8af5bda
ComNewbie <iforking3@naver.com>
2024-11-01 14:49:12 +0900

    feat(ProgramOutput): 보너스 번호 입력 요청 구현 requestBonusNumber() 메서드를 통해 보너스 번호 입력 요청 문구 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -24,6 +24,8 @@ public class Application {
         output.requestWinningNumbers();
         Lotto winningLotto = input.getWinningNumbers();
 
+        output.requestBonusNumber();
+
 
     }
 
--- src/main/java/lotto/io/ProgramOutput.java
+++ src/main/java/lotto/io/ProgramOutput.java
@@ -8,6 +8,7 @@ public class ProgramOutput {
     private static final String REQUEST_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_COMPLETION_MESSAGE = "개를 구매했습니다.";
     private static final String REQUEST_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void requestPurchaseAmount(){
         System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
@@ -24,6 +25,10 @@ public class ProgramOutput {
         System.out.println(REQUEST_WINNING_NUMBERS_MESSAGE);
     }
 
+    public void requestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER_MESSAGE);
+    }
+
     private void printLottoInformation(List<Lotto> purchasedLotto) {
         purchasedLotto.stream()
                 .map(lotto -> lotto.getNumbers().toString())
@@ -33,5 +38,5 @@ public class ProgramOutput {
     private void printNewLine() {
         System.out.println();
     }
-
+    
 }
