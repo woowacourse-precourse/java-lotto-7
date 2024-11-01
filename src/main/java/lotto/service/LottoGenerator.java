49a93f3661824bb815cbb064669cbac5b477d373
world-dv <goodforx0147@naver.com>
2024-11-02 02:26:44 +0900

    feat: 로또 발행
    - camp.nextstep.edu.missionutils.Randoms의 Randoms.pickUniqueNumbersInRange를 이용해 랜덤 값 6개를 생성합니다.
    - 이때 로또 수 검증을 위해 로또 객체로 생성합니다.
    - 로또 객체를 구입한 로또 개수만큼 생성해 리스트에 저장 후 반환합니다.

---
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -0,0 +1,36 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Factory.LottoFactory;
+import lotto.domain.Lotto;
+
+public class LottoGenerator {
+
+    private static final Integer MIN_NUMBER_RANGE = 1;
+    private static final Integer MAX_NUMBER_RANGE = 45;
+    private static final Integer NUMBER_COUNT = 6;
+
+    private final Integer lottoCount;
+    private final List<Lotto> lottoTicket = new ArrayList<>();
+
+    public LottoGenerator(Integer lottoCount) {
+        this.lottoCount = lottoCount;
+    }
+
+    private List<Integer> generateNumber() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE, NUMBER_COUNT);
+    }
+
+    private Lotto changeNumberToLotto(List<Integer> number) {
+        return LottoFactory.create(number);
+    }
+
+    public List<Lotto> getLottoTicket() {
+        for (int count = 0; count < lottoCount; count++) {
+            lottoTicket.add(changeNumberToLotto(generateNumber()));
+        }
+        return lottoTicket;
+    }
+}
