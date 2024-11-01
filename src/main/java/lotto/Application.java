235872af6d99d83ceeff048878020a4e8e6389c5
NamJihyun99 <namjihyun9936@gmail.com>
2024-11-01 23:04:27 +0900

    feat:> controller.run

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        GameController controller = new GameController(inputView, outputView);
+        controller.run();
     }
 }
