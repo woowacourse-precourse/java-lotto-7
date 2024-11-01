bf5f239b21190cce5b3fa7fa5d84914e0cbbba22
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-11-02 03:28:28 +0900

    feat : 중복된 값을 처리하는 기능 추가
    HashSet의 특징을 이용하여 중복된 값을 제거할 수 있도록 만듬

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//여기서 enum을 사용하여 비교하게 만들면 된다.
+//결과로 나올 수 있는 것들은 1~45까지의 숫자이기 때문에
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    //중복된 값을 처리하는 기능
+    private void checkForDuplicates(List<Integer> numbers) {
+        //HashSet은 중복된 값을 갖지 않는다는 것을 이용
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복된 값이 없어야 합니다.");
+        }
+    }
+
+}
