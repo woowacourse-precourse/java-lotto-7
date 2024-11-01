a7541c6683531c76fc3428613fbc9db073a55fc8
sinminseok12 <sinminseok12@naver.com>
2024-11-02 03:42:11 +0900

    refactor(InputView) : 입력값 검증 기능 적용
    사용자 화면(InputView)에서 입력 검증 기능 메서드를 호출한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.Converter;
+import lotto.validator.InputValidator;
 
 import java.util.List;
 
@@ -16,18 +17,21 @@ public class InputView {
     public static int inputPurchaseAmount() {
         System.out.println(INPUT_BUY_MONEY_MESSAGE);
         String input = Console.readLine();
+        InputValidator.validatePurchaseAmount(input);
         return Converter.convertToNumber(input);
     }
 
     public static List<Integer> inputWinNumbers() {
         System.out.println(INPUT_WIN_NUMBERS_MESSAGE);
         String input = Console.readLine();
+        InputValidator.validateWinNumbers(input);
         return Converter.convertNumbers(input);
     }
 
     public static int inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         String input = Console.readLine();
+        InputValidator.validateBonusNumber(input);
         return Converter.convertToNumber(input);
     }
 
