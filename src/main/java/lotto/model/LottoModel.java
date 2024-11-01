a671c39fb7802599693b12d857e4eb924d2ce76d
mipangg <morganjeong119@gmail.com>
2024-11-02 03:26:14 +0900

    feat(LottoModel): store winning numbers

--- src/main/java/lotto/model/LottoModel.java
+++ src/main/java/lotto/model/LottoModel.java
@@ -1,11 +1,23 @@
 package lotto.model;
 
+import java.util.List;
+
 // UserLotto와 Lotto를 이용하여 유저의 복권 정보를 비교하고 통계하는 메서드들 모음
 public class LottoModel {
-    UserLotto userLotto;
+    private int bonusNumber;
+    private Lotto winningNumbers;
+    private UserLotto userLotto;
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
 
     public void setUserLotto(int numberOfLotto) {
         userLotto = new UserLotto(numberOfLotto);
+    }
+
+    public void setWinningNumbers(Lotto winningNumbers) {
+        this.winningNumbers = winningNumbers;
     }
 
     public UserLotto getUserLotto() {
