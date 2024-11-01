a880793e30e0183bf2b59a966f155032c0d70213
ChoiDongJoon <adong0808@naver.com>
2024-11-02 01:06:12 +0900

    refactor(lotto): 입력을 받아 바로 리턴하도록 변경
    입력을 받아 바로 리턴하도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -15,26 +15,16 @@ public class InputView {
         return Console.readLine();
     }
 
-    public Lotto getWinningLotto() {
+    public String[] getWinningLotto() {
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        String[] winningNumberInput = Console.readLine().split(LOTTO_NUM_DELIMITER);
-        List<Integer> winningLotto = convertArrayToList(winningNumberInput);
-
-        return new Lotto(winningLotto);
+        return Console.readLine().split(LOTTO_NUM_DELIMITER);
     }
 
-    public int getBonusNumber() {
+    public String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
-    private List<Integer> convertArrayToList(String[] array) {
-        List<Integer> list = new ArrayList<>();
-        for (String number: array) {
-            number = number.trim();
-            list.add(Integer.parseInt(number));
-        }
-        return list;
-    }
+
 }
