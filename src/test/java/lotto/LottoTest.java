301a40b9e29a3f4627129a7699bead52378fc9a0
alpin87 <qortmdals94@naver.com>
2024-11-01 20:20:30 +0900

    test: Lotto Class 단위 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,15 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.domain.Lotto;
-import lotto.domain.LottoMachine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-class LottoTest extends NsTest {
+class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -26,58 +23,38 @@ class LottoTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호가 1보다 작으면 예외가 발생한다.")
     @Test
-    void 로또_구매금액_테스트() {
-        assertThatThrownBy(() -> new LottoMachine().purchase(400))
+    void 로또_번호가_1보다_작으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 로또_번호_범위_테스트() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf((IllegalArgumentException.class));
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("정상적인 로또 구매 진행")
-    void 구매_금액에따른_로또_개수_테스트() {
-        assertSimpleTest(() -> {
-            run("1000", "1,2,3,4,5,6", "7");
-            assertThat(output()).contains(
-                    "1개를 구매했습니다.");
-        });
+    void 로또_번호_개수_테스트() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 보너스_번호_입력_테스트() {
-        assertSimpleTest(() -> {
-            runAndCheckOutput("1000", "1,2,3,4,5,6", "a", "7");
-            assertThat(output()).contains(
-                    "[ERROR] 올바른 형식의 당첨 번호를 입력해주세요.",
-                    "보너스 번호를 입력해 주세요.",
-                    "당첨 통계"
-            );
-        });
+    void 로또_번호_생성_테스트() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
     @Test
-    void 당첨_번호_입력_테스트() {
-        assertSimpleTest(() -> {
-            runAndCheckOutput("1000", "1,2,3,4,5,a", "1,2,3,4,5,6", "7");
-            assertThat(output()).contains(
-                    "[ERROR] 올바른 형식의 당첨 번호를 입력해주세요.",
-                    "당첨 번호를 입력해 주세요.",
-                    "당첨 통계"
-            );
-        });
-    }
+    void 로또_번호_오름차순_테스트() {
+        List<Integer> numbers = List.of(6, 5, 4, 3, 2, 1);
+        Lotto lotto = new Lotto(numbers);
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
-
-    private void runAndCheckOutput(String... inputs) {
-        run(inputs);
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 }
