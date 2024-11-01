d6a734a1dffa167123bf567764eeaf0f8a822568
seophohoho <seop0937@gmail.com>
2024-11-01 20:27:45 +0900

    feat(Seller): Lotto 복권 생성 메소드 추가

--- src/main/java/lotto/model/Seller.java
+++ src/main/java/lotto/model/Seller.java
@@ -1,10 +1,14 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Seller {
     private final String PAY_PATTERN = "^[1-9][0-9]{3,}";
+    private List<Lotto> lottoTickets = new ArrayList<>();
 
     public void validate(String input) {
         Pattern patternPay = Pattern.compile(PAY_PATTERN);
@@ -18,6 +22,22 @@ public class Seller {
         if (money % 1000 != 0) {
             reject();
         }
+    }
+
+    public void createLottoTickets(int money) {
+        int howmany = money / 1000;
+
+        for (int i = 0; i < howmany; i++) {
+            lottoTickets.add(new Lotto(getRandomNumber()));
+        }
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return lottoTickets;
+    }
+
+    public List<Integer> getRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     public void reject() {
