f2f1af288a360b2431acb20b7d178f700e96ed88
soonhong99 <nanayah99@naver.com>
2024-11-02 02:07:04 +0900

    refactor(Test): 리팩토링한 기존 함수과 연관된 테스트 코드를 리팩토링하라
    추가한 기능:
    - 모든 테스트에 쓰이는 클래스들을 @BeforeEach로 setUp
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/test/java/lotto/LottoPurchaseAmountTest.java
+++ src/test/java/lotto/LottoPurchaseAmountTest.java
@@ -1,9 +1,10 @@
 package lotto;
 
 import convert.SingleStringToNumConverter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import validate.LottoPurchaseAmountValidator;
+import lottoPurchaseAmount.LottoPurchaseAmountValidator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,48 +13,42 @@ public class LottoPurchaseAmountTest {
     private SingleStringToNumConverter stringToIntConverter;
     private LottoPurchaseAmountValidator lottoPurchaseAmountValidator;
 
+    @BeforeEach
+    void setUp() {
+        stringToIntConverter = new SingleStringToNumConverter();
+        lottoPurchaseAmountValidator = new LottoPurchaseAmountValidator();
+    }
+
     @DisplayName("로또 구입 금액이 숫자 형식이 아니라면 예외가 발생한다.")
     @Test
     void 로또_구입_금액이_숫자_형식이_아니라면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoPurchaseAmount = "one thousand";
-        stringToIntConverter = new SingleStringToNumConverter(lottoPurchaseAmount);
-
-        // When
-        lottoPurchaseAmountValidator = new LottoPurchaseAmountValidator(lottoPurchaseAmount);
 
         // Then
-        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing(lottoPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구입 금액이 오만원을 초과한다면 예외가 발생한다.")
     @Test
     void 로또_구입_금액이_오만원을_초과한다면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoPurchaseAmount = "100000";
-        stringToIntConverter = new SingleStringToNumConverter(lottoPurchaseAmount);
-
-        // When
-        lottoPurchaseAmountValidator = new LottoPurchaseAmountValidator(lottoPurchaseAmount);
 
         // Then
-        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing(lottoPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 구입 금액이 천원단위가 아니라면 예외가 발생한다.")
     @Test
     void 로또_구입_금액이_천원단위가_아니라면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoPurchaseAmount = "5500";
-        stringToIntConverter = new SingleStringToNumConverter(lottoPurchaseAmount);
-
-        // When
-        lottoPurchaseAmountValidator = new LottoPurchaseAmountValidator(lottoPurchaseAmount);
 
         // Then
-        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoPurchaseAmountValidator.validateAllThing(lottoPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
--- src/test/java/lotto/LottoWinningNumberTest.java
+++ src/test/java/lotto/LottoWinningNumberTest.java
@@ -1,13 +1,14 @@
 package lotto;
 
 import convert.ListStringToNumConverter;
-import delimit.LottoWinningNumberDelimiter;
+import lottoWinningNumber.LottoWinningNumberDelimiter;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import validate.LottoWinningNumberValidator;
+import lottoWinningNumber.LottoWinningNumberValidator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,59 +18,57 @@ public class LottoWinningNumberTest {
     private ListStringToNumConverter listStringToNumConverter;
     private LottoWinningNumberValidator lottoWinningNumberValidator;
 
+    @BeforeEach
+    void setUp() {
+        lottoWinningNumberDelimiter = new LottoWinningNumberDelimiter();
+        lottoWinningNumberValidator = new LottoWinningNumberValidator();
+    }
+
     @DisplayName("로또 당첨 번호들의 개수가 6개가 아니라면 예외가 발생한다.")
     @Test
     void 로또_당첨_번호들의_개수가_6개가_아니라면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoWinningNumber = "1,2,3";
-
-        // When
-        lottoWinningNumberValidator = new LottoWinningNumberValidator(lottoWinningNumber);
+        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringSeperatedLottoWinningNumbers(lottoWinningNumber);
 
         // Then
-        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing(seperatedLottoWinningNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨 번호들 중 겹치는 것이 있다면 예외가 발생한다.")
     @Test
     void 로또_당첨_번호들_중_겹치는_것이_있다면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoWinningNumber = "1,1,2,3,4,5";
-
-        // When
-        lottoWinningNumberValidator = new LottoWinningNumberValidator(lottoWinningNumber);
+        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringSeperatedLottoWinningNumbers(lottoWinningNumber);
 
         // Then
-        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing(seperatedLottoWinningNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨 번호들이 숫자 형식이 아니라면 예외가 발생한다.")
     @Test
     void 로또_당첨_번호들이_숫자_형식이_아니라면_예외가_발생한다() {
-        // Given
-        lottoWinningNumber = "a,a,b,c,d,e";
-
-        // When
-        lottoWinningNumberValidator = new LottoWinningNumberValidator(lottoWinningNumber);
+        // Given&When
+        lottoWinningNumber = "1,2,3,a,5,6";
+        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringSeperatedLottoWinningNumbers(lottoWinningNumber);
 
         // Then
-        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing(seperatedLottoWinningNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 당첨 번호들의 범위가 1~45가 아니라면 예외가 발생한다.")
     @Test
     void 로또_당첨_번호들의_범위가_1이상_45이하가_아니라면_예외가_발생한다() {
-        // Given
+        // Given&When
         lottoWinningNumber = "1,24,25,66,74,21";
-
-        // When
-        lottoWinningNumberValidator = new LottoWinningNumberValidator(lottoWinningNumber);
+        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringSeperatedLottoWinningNumbers(lottoWinningNumber);
 
         // Then
-        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing())
+        assertThatThrownBy(() -> lottoWinningNumberValidator.validateAllThing(seperatedLottoWinningNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -77,13 +76,10 @@ public class LottoWinningNumberTest {
     @Test
     void 로또_당첨_번호를_쉼표_기준으로_구분한다() {
         // Given
+        listStringToNumConverter = new ListStringToNumConverter();
         lottoWinningNumber = "1,2,3,4,5";
-        lottoWinningNumberDelimiter = new LottoWinningNumberDelimiter(lottoWinningNumber);
-        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringLottoWinningNumber();
-        listStringToNumConverter = new ListStringToNumConverter(seperatedLottoWinningNumbers);
-
-        // When
-        List<Integer> seperatedLottoWinningNumbersToInt = listStringToNumConverter.convert();
+        List<String> seperatedLottoWinningNumbers = lottoWinningNumberDelimiter.runAndBringSeperatedLottoWinningNumbers(lottoWinningNumber);
+        List<Integer> seperatedLottoWinningNumbersToInt = listStringToNumConverter.convert(seperatedLottoWinningNumbers);
 
         // Then
         Assertions.assertEquals(seperatedLottoWinningNumbersToInt, Arrays.asList(1, 2, 3, 4, 5));
