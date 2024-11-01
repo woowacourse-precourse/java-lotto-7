1e9f5b35932cc19500e4dcf2ac33d267a98f2857
Kimdonghwan <kimdonghwan913@gmail.com>
2024-11-01 23:46:08 +0900

    feat(lotto): 티켓 수만큼 로또 생성 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,20 +4,29 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGenerator {
 
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 45;
     public static final int COUNT = 6;
+    public static final int ZERO = 0;
 
-    public List<Integer> lottoNumbers() {
+    public List<Lotto> generateLottos(int tickets) {
+        return IntStream.range(ZERO, tickets)
+                .mapToObj(ticketCount -> new Lotto(generateLottoNumbers()))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> generateLottoNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
-        numberSort(numbers);
+        ascending(numbers);
         return numbers;
     }
 
-    private static void numberSort(List<Integer> numbers) {
+    private static void ascending(List<Integer> numbers) {
         Collections.sort(numbers);
     }
 }
--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -14,30 +14,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LottoGeneratorTest {
 
     private LottoGenerator lottoGenerator;
+    private List<Lotto> lottos;
 
     @BeforeEach
     void setUp() {
         lottoGenerator = new LottoGenerator();
+        lottos = lottoGenerator.generateLottos(10);
     }
 
     @Test
-    void 번호_6개_생성() {
-        assertEquals(COUNT, lottoGenerator.lottoNumbers().size());
+    void 로또_번호_6개_생성() {
+        lottos.forEach(lotto ->
+                assertEquals(COUNT, lotto.getNumbers().size())
+        );
     }
 
     @Test
-    void 번호_범위_검증() {
-        assertThat(lottoGenerator.lottoNumbers())
-                .allMatch(number -> number >= START_INCLUSIVE && number <= END_INCLUSIVE);
+    void 로또_번호_범위_검증() {
+        lottos.forEach(lotto ->
+                assertThat(lotto.getNumbers()).allMatch(number -> number >= START_INCLUSIVE && number <= END_INCLUSIVE)
+        );
     }
 
     @Test
-    void 번호_중복_검증() {
-        assertThat(lottoGenerator.lottoNumbers()).doesNotHaveDuplicates();
+    void 로또_번호_오름차순_검증() {
+        lottos.forEach(lotto ->
+                assertThat(lotto.getNumbers()).isSorted()
+        );
     }
 
     @Test
-    void 번호_오름차순_검증() {
-        assertThat(lottoGenerator.lottoNumbers()).isSorted();
+    void 로또_번호_중복_검증() {
+        lottos.forEach(lotto ->
+                assertThat(lotto.getNumbers()).doesNotHaveDuplicates()
+        );
     }
 }
