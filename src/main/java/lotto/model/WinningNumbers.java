632677643ee7ee529ac76ac8b18a67565a6ec026
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-11-01 21:01:53 +0900

    feat: WinningNumbers 클래스 구현 및 테스트 추가
    - 당첨 번호와 보너스 번호 관리 기능 추가
    - 보너스 번호가 당첨 번호와 중복되지 않는 기능 추가
    - 보너스 번호가 1~45 범위를 벗어날 때 예외 발생 처리
    - 기능들의 테스트 코드 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,4 +1,30 @@
 package lotto.model;
 
 public class WinningNumbers {
+    private final Lotto winNumber;
+    private final int bonusNumber;
+
+    public WinningNumbers(Lotto winNumber, int bonusNumber) {
+        validateBonusNumber(winNumber, bonusNumber);
+        this.winNumber = winNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void validateBonusNumber(Lotto winNumber, int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이 숫자여야 합니다.");
+        }
+
+        if (winNumber.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
+        }
+    }
+
+    public Lotto getWinningNumber() {
+        return winNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
