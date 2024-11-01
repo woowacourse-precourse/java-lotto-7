78d631b8abb0b31f92027f463d51222756fd4303
김건학 <kkh10002@naver.com>
2024-11-01 14:36:04 +0900

    feat(Numbers): 숫자들을 생성하는 기능 구현
    - Numbers 객체를 생성하는 기능을 구현했습니다. (createNumbers)
    - 랜덤 숫자 6개를 생성하는 기능을 구현했습니다. (generateSixRandomNumbers)

---
+++ src/main/java/lotto/domain/Random/Numbers.java
@@ -0,0 +1,53 @@
+package lotto.domain.Random;
+
+import static lotto.resources.Constants.LOTTO_MAX_NUMBER;
+import static lotto.resources.Constants.LOTTO_MIN_NUMBER;
+import static lotto.resources.Constants.LOTTO_TOTAL_NUMBERS;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.Objects;
+
+public class Numbers {
+    private final List<Number> numbers;
+
+    private Numbers(final List<Number> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Numbers createNumbers(final List<Integer> numbers) {
+        List<Number> createdNumbers = numbers.stream()
+                .map(Number::createNumber)
+                .toList();
+
+        return new Numbers(createdNumbers);
+    }
+
+    public static Numbers generateSixRandomNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER,
+                LOTTO_TOTAL_NUMBERS);
+
+        return createNumbers(randomNumbers);
+    }
+
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Numbers otherNumbers = (Numbers) obj;
+        return Objects.equals(this.numbers, otherNumbers.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(numbers);
+    }
+}
---
+++ src/test/java/lotto/domain/Random/NumbersTest.java
@@ -0,0 +1,46 @@
+package lotto.domain.Random;
+
+import static lotto.resources.Constants.LOTTO_MAX_NUMBER;
+import static lotto.resources.Constants.LOTTO_MIN_NUMBER;
+import static lotto.resources.ErrorMessages.INVALID_RANGE_LOTTO_NUMBER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumbersTest {
+    @DisplayName("정상 범위의 숫자들을 생성해 본다.")
+    @Test
+    void 정상_범위의_숫자들을_생성해_본다() {
+        List<Integer> validNumbers = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toUnmodifiableList());
+
+        Numbers numbers = Numbers.createNumbers(validNumbers);
+
+        assertThat(numbers).isEqualTo(Numbers.createNumbers(validNumbers));
+    }
+
+    @DisplayName("정상 범위를 벗어난 숫자들로 Numbers 를 생성하면 예외가 발생한다.")
+    @Test
+    void 정상_범위를_벗어난_숫자들로_Numbers를_생성하면_예외가_발생한다() {
+        List<Integer> invalidNumbers = IntStream.rangeClosed(0, 45).boxed().collect(Collectors.toUnmodifiableList());
+
+        Assertions.assertThatThrownBy(() -> Numbers.createNumbers(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_RANGE_LOTTO_NUMBER.getMessage());
+    }
+
+    @DisplayName("난수들은 로또 번호의 범위 안에서 생성된다.")
+    @Test
+    void 난수들은_로또_번호의_범위_안에서_생성된다() {
+        Numbers numbers = Numbers.generateSixRandomNumbers();
+
+        for (Number number : numbers.getNumbers()) {
+            assertTrue(number.getNumber() >= LOTTO_MIN_NUMBER && number.getNumber() <= LOTTO_MAX_NUMBER);
+        }
+    }
+}
