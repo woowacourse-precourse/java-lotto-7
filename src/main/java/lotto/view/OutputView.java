e94b8d0cf5e0ef881fed057f2b124b1c06d14aa6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 04:23:01 +0900

    refactor($OutputView): 출력에 필요한 문자열 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,15 +9,27 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public void printPurchasedLottos(PurchaseLottoResponse response) {
-        System.out.printf("\n%d개를 구매했습니다.%n", response.getPurchasedAmount());
+    private static final String PURCHASE_MESSAGE = "\n%d개를 구매했습니다.%n";
+    private static final String LOTTO_NUMBERS_PREFIX = "[";
+    private static final String LOTTO_NUMBERS_FORMAT = ", ";
+    private static final String LOTTO_NUMBERS_SUFFIX = "]";
 
+    private static final String MATCH_RESULT_HEADER = "\n당첨 통계\n";
+    private static final String MATCH_RESULT_DIVIDER = "---\n";
+    private static final String MATCH_RESULT_FORMAT = "%d개 일치%s (%,d원) - %d개";
+    private static final String BONUS_MATCH = ", 보너스 볼 일치";
+    private static final String EMPTY = "";
+
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+
+    public void printPurchasedLottos(PurchaseLottoResponse response) {
+        System.out.printf(PURCHASE_MESSAGE, response.getPurchasedAmount());
         response.getLottoNumberResponses().forEach(this::printLotto);
     }
 
     public void printScores(LottoScoreResponses response) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.printf(MATCH_RESULT_HEADER);
+        System.out.printf(MATCH_RESULT_DIVIDER);
         response.getLottoScoreResponses().forEach(this::printScore);
     }
 
@@ -29,22 +41,22 @@ public class OutputView {
         boolean isBonusMatch = lottoScoreResponse.isBonusMatch();
         int prize = lottoScoreResponse.getPrize();
 
-        String result = String.format("%d개 일치%s (%,d원) - %d개",
+        String result = String.format(MATCH_RESULT_FORMAT,
                 matchCount,
-                isBonusMatch ? ", 보너스 볼 일치" : "",
+                isBonusMatch ? BONUS_MATCH : EMPTY,
                 prize,
                 count);
         System.out.println(result);
-
     }
 
     public void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+        System.out.printf(PROFIT_RATE_MESSAGE, profitRate);
     }
 
     private void printLotto(LottoNumberResponse response) {
         System.out.println(response.getLottoNumbers().stream()
                 .map(String::valueOf)
-                .collect(Collectors.joining(", ", "[", "]")));
+                .collect(Collectors.joining(LOTTO_NUMBERS_FORMAT, LOTTO_NUMBERS_PREFIX, LOTTO_NUMBERS_SUFFIX)));
     }
 }
+
