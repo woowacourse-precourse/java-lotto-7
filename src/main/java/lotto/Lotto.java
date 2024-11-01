6b0e02a7a5dd2a5b9c9f7e918f92f8c29873510f
speculatingwook <bwook9908@gmail.com>
2024-11-01 17:10:22 +0900

    feat: implement lotto return matching numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,17 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Integer howManyMatches(Lotto lotto) {
+        Integer count = 0;
+        for (Integer number : numbers) {
+            if (lotto.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean contains(Integer number) {
+        return numbers.contains(number);
+    }
 }
