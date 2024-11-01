f97e907d423913621bc4d5f32f89e170dad24d02
kungbi <kipper12335@gmail.com>
2024-11-01 14:06:01 +0900

    test(ProfitReport): 수익률 계산 TDD 작성

--- src/test/java/lotto/entity/ProfitReportTest.java
+++ src/test/java/lotto/entity/ProfitReportTest.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ProfitReportTest {
 
@@ -69,7 +73,7 @@ class ProfitReportTest {
     }
 
     @Test
-    void 실패__당첨_번호_가져온것_수정() {
+    void 실패__당첨_번호_가져온것_수정_불변테스트() {
         // given
         Lotto lotto = Lotto.createRandomLotto();
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
@@ -100,7 +104,7 @@ class ProfitReportTest {
     }
 
     @Test
-    void 실패__구매한_로또_가져온것_수정() {
+    void 실패__구매한_로또_가져온것_수정_불변테스트() {
         // given
         Lotto lotto = Lotto.createRandomLotto();
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
@@ -130,15 +134,44 @@ class ProfitReportTest {
         assertEquals(1000, actual);
     }
 
-    /**
-     * Feature TODO
-     * - [ ] 수익률 계산
-     * - [ ] 수익 계산
-     * - [ ] 각 등수 별 당첨 횟수 계산
-     * - [ ] 각 로또별 당첨 여부 계산
-     * <p>
-     * - [x] 투자 금액 (gettter)
-     * - [x] 당첨 번호 (getter)
-     * - [x] 구매한 로또 (getter)
-     */
+    // 계산 테스트들
+
+    static Stream<Arguments> 성공__수익률_계산_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6), 7, 2_000_000_000),   // 1등
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 5, 6), 7, 30_000_000),      // 2등
+                Arguments.of(List.of(1, 2, 3, 4, 5, 11), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),      // 3등
+                Arguments.of(List.of(1, 2, 3, 4, 7, 11), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),      // 3등 + 보너스 번호
+                Arguments.of(List.of(1, 2, 3, 4, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),        // 4등
+                Arguments.of(List.of(1, 2, 3, 7, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),        // 4등 + 보너스 번호
+                Arguments.of(List.of(1, 2, 3, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 5_000),        // 5등
+                Arguments.of(List.of(1, 2, 7, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 5_000)         // 5등 + 보너스 번호
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("성공__수익률_계산_테스트_케이스")
+    void 성공__수익률_계산(List<Integer> lottoInput, List<Integer> winningNumbersInput, int bonusNumber, long expected) {
+        // given
+        Lotto lotto = new Lotto(lottoInput);
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersInput, bonusNumber);
+        ProfitReport profitReport = new ProfitReport(List.of(lotto), winningNumbers);
+
+        // when
+        long actual = profitReport.calculateProfitRate();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void 성공__수익_계산() {
+        // calculateProfit
+    }
+
+    @Test
+    void 성공__각_등수_별_당첨_횟수_계산() {
+        // calculateWinningCountsByRank
+    }
+
 }
