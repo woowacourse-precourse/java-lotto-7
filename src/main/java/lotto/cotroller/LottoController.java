22f28d5a23501e97c0af913793590ce7ed1e3c53
seahwanKim <kimkimseasea@gmail.com>
2024-11-01 12:55:16 +0900

    feat: 로또 실행 기능을 추가한다

--- src/main/java/lotto/cotroller/LottoController.java
+++ src/main/java/lotto/cotroller/LottoController.java
@@ -1,4 +1,36 @@
 package lotto.cotroller;
 
+import lotto.domain.Lotto;
+import lotto.domain.User;
+import lotto.service.InputService;
+import lotto.service.LottoCalculatorService;
+
 public class LottoController {
+
+    private final InputService inputService = new InputService();
+    private final LottoCalculatorService lottoCalculatorService = new LottoCalculatorService();
+
+    public void run() {
+        User user = setUser();
+        Lotto lotto = setWinningLotto();
+
+        getCalculateResult(user, lotto);
+        getProfit(user);
+    }
+
+    private void getProfit(User user) {
+        lottoCalculatorService.profitCalculate(user);
+    }
+
+    private void getCalculateResult(User user, Lotto lotto) {
+        lottoCalculatorService.calculateWinningResult(user, lotto);
+    }
+
+    private User setUser() {
+        return inputService.inputUser();
+    }
+
+    private Lotto setWinningLotto() {
+        return inputService.setWinningLotto();
+    }
 }
