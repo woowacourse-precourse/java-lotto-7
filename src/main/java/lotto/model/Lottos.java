027b2021251a19b1091d86c084bb7e0dc5439c05
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-01 22:41:14 +0900

    feat(service, model): 상금 계산 서비스 구현체 추가, Lottos 클래스에 forEachLotto 메서드 추가
    - PrizeCalculatorServiceImpl 클래스를 추가하여 상금 계산 로직을 구현
    - PrizeFacade를 이용해 Lottos 객체의 각 로또 번호에 대한 당첨 전략을 확인하고 총 상금을 계산합니다.
    - Lottos 클래스에 forEachLotto 메서드를 추가하여 Consumer를 사용해 각 Lotto 객체에 작업을 수행할 수 있도록 구현
    - PrizeCalculatorServiceImpl에서는 람다 표현식과 AtomicInteger를 활용해 총 상금을 계산하는 메소드를 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,11 +1,16 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Lottos {
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public void forEachLotto(Consumer<Lotto> action) {
+        lottos.forEach(action);
     }
 }
--- src/main/java/lotto/service/prize/PrizeCalculatorServiceImpl.java
+++ src/main/java/lotto/service/prize/PrizeCalculatorServiceImpl.java
@@ -1,2 +1,27 @@
-package lotto.service.prize;public class PrizeCalculatorServiceImpl {
+package lotto.service.prize;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import lotto.model.Lottos;
+import lotto.model.WinningNumbers;
+import lotto.strategy.prize.PrizeFacade;
+
+public class PrizeCalculatorServiceImpl implements PrizeCalculatorService {
+    private final PrizeFacade prizeFacade;
+
+    public PrizeCalculatorServiceImpl(PrizeFacade prizeFacade) {
+        this.prizeFacade = prizeFacade;
+    }
+
+    @Override
+    public int calculateTotalPrize(Lottos lottos) {
+        AtomicInteger totalPrize = new AtomicInteger(0);
+        lottos.forEachLotto(lotto -> {
+            var winningStrategy = prizeFacade.getWinningStrategy(lotto, WinningNumbers.getInstance());
+            if (winningStrategy != null) {
+                totalPrize.addAndGet(winningStrategy.getPrizeAmount());
+            }
+        });
+        return totalPrize.get();
+    }
+
 }
