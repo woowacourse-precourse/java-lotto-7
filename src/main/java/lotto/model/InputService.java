fe35ecf671c7bf74e55d6d502206146dee330b0a
immyeong <wpfls6670@naver.com>
2024-11-01 14:12:33 +0900

    feat: 생성된 로또 번호 출력 로직 구현

--- src/main/java/lotto/model/InputService.java
+++ src/main/java/lotto/model/InputService.java
@@ -2,14 +2,21 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.exception.InvalidBonusNumberException;
+import lotto.exception.InvalidDuplicateBonusNumberException;
+import lotto.exception.InvalidDuplicateNumberException;
 import lotto.exception.InvalidPurchaseAmountException;
 
 import java.util.*;
 
 public class InputService {
     private static List<Lotto> lottos = new ArrayList<>();
+    private static List<Integer> winningNumbers = new ArrayList<>();
+    private static Set<Integer> winningNumberSet = new HashSet<>();
     private static final String CONVERT_ERROR_MESSAGE = "[ERROR] 잘못된 입력값입니다. 입력값은 숫자여야합니다.";
     private static final String AMOUNT_ERROR_MESSAGE = "[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호가 중복될 수 없습니다.";
+    private static final String BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 0이상 45이하여야 합니다.";
 
     public int purchaseValue() {
         String value = Console.readLine();
@@ -42,6 +49,13 @@ public class InputService {
             lottos.add(lotto);
         }
 
+        printLottoNumber(lottos);
         return lottos;
+    }
+
+    private void printLottoNumber(List<Lotto> lottoNumbers) {
+        for (Lotto lottoNumber : lottoNumbers) {
+            System.out.println(lottoNumber.getNumbers().toString());
+        }
     }
 }
