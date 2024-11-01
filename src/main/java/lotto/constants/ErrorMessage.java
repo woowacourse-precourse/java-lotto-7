d541d7e78ea19c63c2a3474b29dafeab05c78502
fpem3309 <fpem3309@gmail.com>
2024-11-01 10:13:42 +0900

    feat: 보너스 번호 숫자 아닐 시 예외처리 구현

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     PURCHASE_MONEY_ONLY_CAN_THOUSAND_UNIT("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
     LOTTO_NUMBER_ONLY_CAN_MONEY("[ERROR] 로또 번호는 숫자여야 합니다."),
     LOTTO_NUMBER_ONLY_CAN_LENGTH_6("[ERROR] 로또 번호는 중복되지 않은 6개의 숫자여야 합니다."),
-    LOTTO_NUMBER_ONLY_CAN_RANGE_1_TO_45("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    LOTTO_NUMBER_ONLY_CAN_RANGE_1_TO_45("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+
+    BONUS_NUMBER_ONLY_CAN_NUMBER("[ERROR] 보너스 번호는 숫자여야 합니다.");
 
     private final String errorMessage;
 
--- src/main/java/lotto/validation/BonusNumberValidation.java
+++ src/main/java/lotto/validation/BonusNumberValidation.java
@@ -1,7 +1,17 @@
 package lotto.validation;
 
+import static lotto.constants.ErrorMessage.BONUS_NUMBER_ONLY_CAN_NUMBER;
+
 public class BonusNumberValidation {
     public static void validate(String inputBonusNumber){
+        Integer bonusNumber = parseNumber(inputBonusNumber);
+    }
 
+    public static Integer parseNumber(String inputBonusNumber) {
+        try {
+            return Integer.parseInt(inputBonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(BONUS_NUMBER_ONLY_CAN_NUMBER.getErrorMessage());
+        }
     }
 }
