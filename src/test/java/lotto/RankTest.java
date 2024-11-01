74d29fd40689cc828ec55ce9b4745e0006566990
daeuun <leeda51925@gmail.com>
2024-11-01 16:08:29 +0900

    test: 당첨 기준 유효성 테스트 추가
    보너스 번호 예외 처리 추가 및 모든 기준에 대한 유효성 테스트를 추가한다.

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -7,23 +7,50 @@ import org.junit.jupiter.api.Test;
 class RankTest {
 
     @Test
-    void 일치하는_숫자의_개수와_보너스_여부를_입력_받아서_등수를_반환한다() {
+    void 당첨번호가_6개_일치하면_FIRST_등급으로_분류한다() {
+        Rank rank = Rank.findRank(6, false);
+        assertThat(Rank.FIRST).isEqualTo(rank);
+    }
+
+    @Test
+    void 당첨번호가_5개_일치하고_보너스_일치하면_SECOND_등급으로_분류한다() {
+        Rank second = Rank.findRank(5, true);
+        assertThat(Rank.SECOND).isEqualTo(second);
+    }
+
+    @Test
+    void 당첨번호가_5개_일치하고_보너스_불일치하면_THIRD_등급으로_분류한다() {
+        Rank third = Rank.findRank(5, false);
+        assertThat(Rank.THIRD).isEqualTo(third);
+    }
+
+    @Test
+    void 당첨번호가_4개_일치하면_FOURTH_등급으로_분류한다() {
+        Rank rank = Rank.findRank(4, false);
+        assertThat(Rank.FOURTH).isEqualTo(rank);
+    }
+
+    @Test
+    void 당첨번호가_3개_일치하면_FIFTH_등급으로_분류한다() {
         Rank rank = Rank.findRank(3, false);
         assertThat(Rank.FIFTH).isEqualTo(rank);
     }
 
     @Test
-    void 일치하는_숫자의_개수가_5개면_보너스_여부로_등수를_결정한다() {
-        Rank second = Rank.findRank(5, true);
-        Rank third = Rank.findRank(5, false);
-        assertThat(Rank.SECOND).isEqualTo(second);
-        assertThat(Rank.THIRD).isEqualTo(third);
+    void 예외_테스트_당첨번호가_2개_일치하면_NONE으로_분류한다() {
+        Rank rank = Rank.findRank(2, false);
+        assertThat(Rank.NONE).isEqualTo(rank);
     }
 
     @Test
-    void 예외_테스트_일치하는_숫자의_개수가_없으면_NONE_등수를_반환한다() {
+    void 예외_테스트_일치하는_숫자의_개수가_정상범위를_벗어나면_NONE_등수를_반환한다() {
         Rank rank = Rank.findRank(100, false);
         assertThat(Rank.NONE).isEqualTo(rank);
     }
 
+    @Test
+    void 예외_테스트_보너스_번호가_있어도_일치하는_개수가_5개가_아니면_NONE으로_분류한다() {
+        Rank rank = Rank.findRank(2, true);
+        assertThat(Rank.NONE).isEqualTo(rank);
+    }
 }
