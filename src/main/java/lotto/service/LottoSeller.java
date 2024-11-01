8e8848b7e2352f836780ab56777348fc4c79bdf6
chaerish <ccyy8432@naver.com>
2024-11-02 01:36:40 +0900

    feat(LottoSeller): 로또 구매 개수 만큼 로또를 지급해주는 메소드 구현
    로또 구매 개수 만큼 로또를 지급하는 provideLotto 메소드를 구현한다.

---
+++ src/main/java/lotto/service/LottoSeller.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class LottoSeller {
+    private final LottoFactory lottoFactory = new LottoFactory();
+
+    public int calculatePurchaseCount(int amount) {
+        return amount / 1000;
+    }
+
+    public List<Lotto> provideLotto(int count) {
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lotto.add(lottoFactory.createLotto());
+        }
+        return lotto;
+    }
+}
