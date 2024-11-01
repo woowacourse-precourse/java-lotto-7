f6538d7c710063dd571a330567704cf8d2ee9896
sanghee0820 <lsh277604@gmail.com>
2024-11-01 11:48:45 +0900

    refactor: bonus 여부를 boolean으로 변경

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -1,27 +1,26 @@
 package lotto.model;
 
 public enum WinningResult {
-    FIRST(6, 0, 2_000_000_000),
-    SECOND(5, 1, 30_000_000),
-    THIRD(5, 0, 1_500_000),
-    FOURTH(4, 0, 50_000),
-    FIFTH(3, 0, 5_000),
-    NONE(0, 0, 0);
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
 
     private final int normalCount;
-    private final int bonusCount;
+    private final boolean bonusMatch;
     private final int reward;
 
-
-    WinningResult(int normalCount, int bonusCount, int reward) {
+    WinningResult(int normalCount, boolean bonusCount, int reward) {
         this.normalCount = normalCount;
-        this.bonusCount = bonusCount;
+        this.bonusMatch = bonusCount;
         this.reward = reward;
     }
 
-    public static WinningResult of(int normalCount, int bonusCount) {
+    public static WinningResult of(int matchCount, boolean isBonusMatch) {
         for (WinningResult winningResult : values()) {
-            if (winningResult.normalCount == normalCount && winningResult.bonusCount == bonusCount) {
+            if (winningResult.normalCount == matchCount && winningResult.bonusMatch == isBonusMatch) {
                 return winningResult;
             }
         }
