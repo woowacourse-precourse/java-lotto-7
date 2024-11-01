d04ca4334a5286cc4a80e38acaa2df3a4ba2566d
NamJihyun99 <namjihyun9936@gmail.com>
2024-11-01 12:11:03 +0900

    feat:> 보너스 번호 유효성 검사

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -5,15 +5,24 @@ import java.util.List;
 public class WinningNumber {
 
     private final List<Integer> numbers;
+    private final int bonusNumber;
 
-    public WinningNumber(List<Integer> numbers) {
+    public WinningNumber(List<Integer> numbers, int bonusNumber) {
         validate(numbers);
+        validate(bonusNumber);
         this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 7) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호의 개수가 맞지 않습니다.");
+        }
+    }
+
+    private void validate(int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
         }
     }
 
