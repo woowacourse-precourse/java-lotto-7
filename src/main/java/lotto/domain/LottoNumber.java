ba96d1f2373f85bd0f83db3a03accc74a1a4d9c4
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 21:56:18 +0900

    feat(LottoNumber): 예외처리 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -12,9 +12,14 @@ public class LottoNumber {
     public static List<Integer> winLottoNumber(String number_str) {
         List<Integer> lottoNum = new ArrayList<>();
         String[] tokens = number_str.trim().split(",");
-        for (String token : tokens) {
-            int number = Integer.parseInt(token);
-            lottoNum.add(number);
+        try{
+            for (String token : tokens) {
+                int number = Integer.parseInt(token);
+                lottoNum.add(number);
+            }
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("");//입력값이 문자일때,비었을때
         }
         Collections.sort(lottoNum);
         return lottoNum;
