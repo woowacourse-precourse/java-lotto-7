257432b53e777f356e17f2d1b9f010b25ca26a54
pokm321 <pokm90@gmail.com>
2024-11-01 15:42:01 +0900

    style: divide a long code line into a few for readability

--- src/main/java/lotto/view/Errors.java
+++ src/main/java/lotto/view/Errors.java
@@ -8,7 +8,11 @@ public enum Errors {
     NULL_OR_EMPTY_INPUT("값이 비어있습니다."),
     NOT_A_NATURAL_NUMBER("자연수가 아닙니다."),
     NOT_A_LONG("long 범위를 벗어났습니다."),
-    NOT_IN_RANGE("범위를 벗어났습니다 (" + Constants.MIN_MONEY.getNumber() + "~" + Constants.MAX_MONEY.getNumber() + ")."),
+    NOT_IN_RANGE("범위를 벗어났습니다 ("
+            + Constants.MIN_MONEY.getNumber()
+            + "~"
+            + Constants.MAX_MONEY.getNumber()
+            + ")."),
     REMAINDER_EXISTENT("값이 나누어 떨어지지 않습니다.");
 
     private final String message;
