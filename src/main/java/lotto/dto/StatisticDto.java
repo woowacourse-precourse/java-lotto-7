4fee04c080eec0a7bd26959c3560edf0230eb1a4
woogym <kwj9294@gmail.com>
2024-11-02 05:12:25 +0900

    feat(dto): 당첨 통계 출력을 위한 dto 구현

--- src/main/java/lotto/dto/StatisticDto.java
+++ src/main/java/lotto/dto/StatisticDto.java
@@ -1,2 +1,51 @@
-package lotto.dto;public class StatisticDto {
+package lotto.dto;
+
+import static lotto.config.LottoRank.FIFTH_RANK;
+import static lotto.config.LottoRank.FIRST_RANK;
+import static lotto.config.LottoRank.FOURTH_RANK;
+import static lotto.config.LottoRank.SECOND_RANK;
+import static lotto.config.LottoRank.THIRD_RANK;
+
+import java.util.Map;
+import lotto.config.LottoRank;
+import lotto.util.StatisticCalculate;
+
+public class StatisticDto {
+    private static final int INITIAL_COUNT = 0;
+
+    private final Map<LottoRank, Integer> statisticResult;
+
+    private StatisticDto(Map<LottoRank, Integer> statisticResult) {
+        this.statisticResult = statisticResult;
+    }
+
+    public static StatisticDto from(final StatisticCalculate statisticCalculate) {
+        Map<LottoRank, Integer> statisticResult = statisticCalculate.getStatisticResult();
+
+        return new StatisticDto(Map.copyOf(statisticResult));
+    }
+
+    private Map<LottoRank, Integer> getStatisticResult() {
+        return Map.copyOf(statisticResult);
+    }
+
+    public Integer getFifthRankCount() {
+        return getStatisticResult().getOrDefault(FIFTH_RANK, INITIAL_COUNT);
+    }
+
+    public Integer getFourthRankCount() {
+        return getStatisticResult().getOrDefault(FOURTH_RANK, INITIAL_COUNT);
+    }
+
+    public Integer getThirdRankCount() {
+        return getStatisticResult().getOrDefault(THIRD_RANK, INITIAL_COUNT);
+    }
+
+    public Integer getSecondRankCount() {
+        return getStatisticResult().getOrDefault(SECOND_RANK, INITIAL_COUNT);
+    }
+
+    public Integer getFirstRankCount() {
+        return getStatisticResult().getOrDefault(FIRST_RANK, INITIAL_COUNT);
+    }
 }
