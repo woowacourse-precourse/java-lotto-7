5b7cbade3b4b2074c16f792d93fa621029ad7976
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-01 18:22:06 +0900

    test(lotto/logic): 로또 번호 발행 기능 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,26 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+
+    @Test
+    void 로또_번호_발행() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    Lotto lotto = new Lotto(Lotto.generate());
+                    assertThat(lotto).isEqualTo(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+                },
+                List.of(8, 21, 23, 41, 42, 43)
+        );
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
