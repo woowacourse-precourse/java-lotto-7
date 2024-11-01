878383eaf7f6bd230ab4ca4fb967c600e36fb4f0
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-01 17:03:19 +0900

    refactor: 중복 테스트 코드 축약
    -   @ParameterizedTest
        @ValueSource를 사용하여 테스트 코드 축약

--- src/test/java/lotto/InputValidator/InputValidatorTest.java
+++ src/test/java/lotto/InputValidator/InputValidatorTest.java
@@ -2,34 +2,34 @@ package lotto.InputValidator;
 
 import lotto.exception.InputErrorMessage;
 import lotto.util.InputValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
 
-    @Test
-    void 입력금액이_숫자가_아니면_예외발생_문자열 () {
-        String inValidAmount = "YOON";
-        InputValidator.validateAmount(inValidAmount);
+    @ParameterizedTest
+    @ValueSource(strings = {"YOON", "*&!"})
+    void 입력금액이_숫자가_아니면_예외발생(String inValidAmount) {
+        assertThatThrownBy(() -> InputValidator.validateAmount(inValidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputErrorMessage.INVALID_NUMERIC_AMOUNT.getMessage()); // 예외 메시지는 실제 메시지로 수정 필요
     }
 
-    @Test
-    void 입력금액이_숫자가_아니면_예외발생_특수문자 () {
-        String inValidAmount = "*&!";
-        InputValidator.validateAmount(inValidAmount);
-    }
-
-    @Test
-    void 입력금액이_1000원_단위일때_통과한다() {
-        String validAmount = "1000";
+    @ParameterizedTest
+    @ValueSource(strings = {"1000"})
+    void 입력금액이_1000원_단위일때_통과한다(String validAmount) {
         InputValidator.validateAmount(validAmount);
     }
 
-    @Test
-    void 입력금액이_1000원_단위가_아닐때_예외발생() {
-        String inValidAmount = "2500";
+    @ParameterizedTest
+    @ValueSource(strings = {"2500"})
+    void 입력금액이_1000원_단위가_아닐때_예외발생(String inValidAmount) {
         assertThatThrownBy(() -> InputValidator.validateAmount(inValidAmount))
-            .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(InputErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
     }
 }
