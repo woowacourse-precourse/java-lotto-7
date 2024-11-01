d84592c30b77d9a8c07889cbed66d15f69c87e46
hanwool <hanwe92@gmail.com>
2024-11-01 11:29:50 +0900

    feat(LottoTest): Add test creating Lotto

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,25 +1,56 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.util.enums.ValidateMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+    static final List<Integer> MORE_THAN_SIX_SIZE_NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7);
+    static final List<Integer> DUPLICATE_NUMBERS = List.of(1, 2, 3, 4, 5, 5);
+    static final List<Integer> MORE_THAN_FIFTY_FIVE_NUMBERS = List.of(1, 2, 3, 4, 5, 67);
+    static final List<Integer> EQUAL_ZERO_NUMBERS = List.of(1, 2, 3, 4, 5, 0);
+    static final List<Integer> LESS_THAN_ZERO_NUMBERS = List.of(1, 2, 3, 4, 5, -1);
+
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
+    void lottoNumberSizeTest() {
+        assertThatThrownBy(() -> new Lotto(MORE_THAN_SIX_SIZE_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidateMessage.NUMBER_SIZE_ERROR.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+    void lottoNumberDuplicateTest() {
+        assertThatThrownBy(() -> new Lotto(DUPLICATE_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidateMessage.DUPLICATE_ERROR.getMessage());
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호 숫자가 45가 넘으면 예외가 발생한다.")
+    @Test
+    void lottoNumberOverFiftyFiveTest() {
+        assertThatThrownBy(() -> new Lotto(MORE_THAN_FIFTY_FIVE_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidateMessage.NUMBER_RANGE_ERROR.getMessage());
+    }
+
+    @DisplayName("로또 번호가 0이면 예외가 발생한다.")
+    @Test
+    void lottoNumberEqualZeroTest() {
+        assertThatThrownBy(() -> new Lotto(EQUAL_ZERO_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidateMessage.POSITIVE_NUMBER_ERROR.getMessage());
+    }
+
+    @DisplayName("로또 번호가 음수이면 예외가 발생한다.")
+    @Test
+    void lottoNumberLessThanZeroTest() {
+        assertThatThrownBy(() -> new Lotto(LESS_THAN_ZERO_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidateMessage.POSITIVE_NUMBER_ERROR.getMessage());
+    }
 }
