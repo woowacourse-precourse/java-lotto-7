99aa2a1925b47c37203bece2e2f9065366336015
MinJae00 <exceedmin@gmail.com>
2024-11-02 00:17:36 +0900

    feat: 랜덤 로또 번호를 발행하는 Service 기능 구현

--- src/main/java/lotto/service/GenerateLottoNumberManager.java
+++ src/main/java/lotto/service/GenerateLottoNumberManager.java
@@ -1,2 +1,27 @@
-package lotto.service;public class GenerateLottoNumberManager {
+package lotto.service;
+
+import java.util.ArrayList;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumberGenerater;
+import java.util.List;
+import lotto.repository.LottoRepository;
+
+public class GenerateLottoNumberManager {
+    private List<List<Integer>> lottoNumber;
+
+    public List<List<Integer>> generate(int num){
+        LottoNumberGenerater generater = new LottoNumberGenerater();
+        for(int i = 0; i < num; i++){
+            List<Integer> list = generater.numberGenerate();
+            new Lotto(list);
+            lottoNumber.add(list);
+        }
+        new LottoRepository(lottoNumber);
+        return lottoNumber;
+    }
+
+    public GenerateLottoNumberManager(){
+        lottoNumber = new ArrayList<>();
+    }
+
 }
