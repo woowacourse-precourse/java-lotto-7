ed5e7481ca576d32642d352425f256720c54a367
sinminseok12 <sinminseok12@naver.com>
2024-11-01 17:40:40 +0900

    test(InputValidatorTest) : 구입금액, 보너스 번호 입력시 양의 정수가 아닐경우 예외처리 테스트 구현

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -16,6 +16,13 @@ public class InputValidatorTest {
     }
 
     @ParameterizedTest
+    @ValueSource(strings = {"0", "-1000"})
+    void 구입금액이_양의_정수가_아닐경우_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
     @ValueSource(strings = {" ", ""})
     void 당첨번호에_공백이_있으면_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputValidator.validateWinNumbers(input))
@@ -25,6 +32,13 @@ public class InputValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {" ", ""})
     void 보너스_번호에_공백이_있으면_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1000"})
+    void 보너스_번호가_양의_정수가_아닐경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputValidator.validateBonusNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
