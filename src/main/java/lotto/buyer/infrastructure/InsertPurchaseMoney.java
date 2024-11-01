5cfcdd45ae741098d15f2a105906324757a01911
EHCTH <lees90896@gmail.com>
2024-11-01 17:08:58 +0900

    style(InsertPurchaseMoney): 클래스 이름 변경

--- src/main/java/lotto/buyer/infrastructure/InsertPurchaseMoney.java
+++ src/main/java/lotto/buyer/infrastructure/InsertPurchaseMoney.java
@@ -1,16 +1,17 @@
 package lotto.buyer.infrastructure;
 
 import lotto.buyer.domain.InsertMoneyService;
-import lotto.buyer.domain.Money;
+import lotto.money.domain.Money;
+import lotto.money.infrastructure.PurchaseAmount;
 import lotto.view.input.hanlder.domain.InputHandlerService;
 
-public class InsertWon implements InsertMoneyService {
+public class InsertPurchaseMoney implements InsertMoneyService {
     private final InputHandlerService inputHandlerService;
-    public InsertWon(InputHandlerService inputHandlerService) {
+    public InsertPurchaseMoney(InputHandlerService inputHandlerService) {
         this.inputHandlerService = inputHandlerService;
     }
     @Override
     public Money insert() {
-        return inputHandlerService.retrieveReceive(Won::of);
+        return inputHandlerService.retrieveReceive(PurchaseAmount::of);
     }
 }
