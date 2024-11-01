59f0e57a7862a1de36d61e3c55dd318348e00e59
ouvir <kilhyunii@naver.com>
2024-11-01 22:18:39 +0900

    test(LottoServiceTest): 구매 금액만큼 로또 발행 테스트 작성

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,10 +1,13 @@
 package lotto.service;
 
+import lotto.Exception.MoneyException;
 import lotto.model.Lotto;
 import lotto.utils.LottoRules;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -19,4 +22,11 @@ class LottoServiceTest {
         Assertions.assertThat(lottoService.generateLotto()).isInstanceOf(Lotto.class);
     }
 
+    @ParameterizedTest
+    @DisplayName("구매 금액 만큼 로또 발행하기")
+    @ValueSource(ints = {1, 30, 20, 15, 4})
+    void purchaseLottoTickets(int count) {
+        List<Lotto> lottoTickets = lottoService.purchaseLottoTickets(LottoRules.LOTTO_PRICE * count);
+        assertEquals(lottoTickets.size(), count);
+    }
 }
