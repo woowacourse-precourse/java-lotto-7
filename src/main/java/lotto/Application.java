c6883225709fa48c5443dea5a9c9257519a4734b
junho <wnsgh732@gmail.com>
2024-11-02 01:48:04 +0900

    feat: 생성된 Lotto의 번호를 출력하도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int buyingAmount = InputHandler.getBuyingAmount();
+        List<Lotto> lottos = LottoGenerator.getLotto(buyingAmount);
+        System.out.println(buyingAmount + "개를 구매했습니다.");
+        lottos.forEach(System.out::println);
+
+        System.out.println("당첨 번호를 입력해주세요.");
+        List<Integer> winningNumber = InputHandler.getWinningNumber();
     }
 }
