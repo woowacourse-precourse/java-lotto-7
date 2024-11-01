a0a8c100195ebe6478166c267cb49cff664e9b6d
SeongbinPark <hyucse22@gmail.com>
2024-11-01 20:57:42 +0900

    refactor(LottoMachine): 상수 추출

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,22 +1,36 @@
 package lotto;
 
+import static lotto.LottoConstants.LOTTO_MAX_NUMBER;
+import static lotto.LottoConstants.LOTTO_MIN_NUMBER;
+import static lotto.LottoConstants.LOTTO_NUMBER_COUNT;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
+    private static final int LOTTO_PRICE = LottoMachineConstants.LOTTO_PRICE;
+
     private final List<Lotto> lottos = new ArrayList<>();
 
     public void purchaseLottos(int purchaseAmount) {
-        int count = purchaseAmount / 1000;
+        int count = purchaseAmount / LOTTO_PRICE;
         for (int i = 0; i < count; i++) {
             lottos.add(generateLotto());
         }
     }
 
     private Lotto generateLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_NUMBER,
+                LOTTO_MAX_NUMBER,
+                LOTTO_NUMBER_COUNT
+        );
         return new Lotto(numbers);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 
     public void printLottos() {
@@ -25,9 +39,5 @@ public class LottoMachine {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos;
     }
 }
