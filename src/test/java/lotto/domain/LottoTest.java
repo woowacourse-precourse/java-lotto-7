96d41b1ebd6afe84e9447f1fca376a7f053e59cf
eb9862 <luclead622@gmail.com>
2024-11-01 16:42:40 +0900

    test: LottoTest 클래스에 로또 번호의 숫자 범위를 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.domain.LottoInfo.PICK_COUNT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +23,10 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외처리() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, PICK_COUNT + 1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
