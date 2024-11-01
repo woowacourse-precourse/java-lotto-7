3ceeadc091ea0db84d3dd336938babb56890df47
wingyou <winwing.you@gmail.com>
2024-11-02 03:00:47 +0900

    feat: InputHandler handles user input and throws exceptions

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,9 +1,70 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * InputHandler
  */
 public class InputHandler {
+
+    final int LOTTO_PRICE = 1000;
+    final int RANGE_LOW = 1;
+    final int RANGE_HIGH = 45;
+
+    public int parsePurchaseAmount(String input) {
+        try {
+            int purchaseAmount = Integer.parseInt(input);
+
+            if (purchaseAmount % LOTTO_PRICE != 0)
+                throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위여야 합니다.");
+
+            return purchaseAmount;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 금액 형식입니다.");
+        }
+    }
+
+    public List<Integer> parseWinningNumbers(String input) {
+       try {
+            List<Integer> winningNumbers = new ArrayList<>(6);
+            String[] segments = input.split(",");
+
+            if (segments.length != 6)
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 정확히 6개여야 합니다. ");
+
+            boolean[] check = new boolean[RANGE_HIGH];
+
+            for (String segment : segments) {
+                int number = Integer.parseInt(segment);
+
+                if (number < RANGE_LOW || number > RANGE_HIGH)
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + RANGE_LOW + " 부터 " + RANGE_HIGH + " 사이여야 합니다.");
+
+                if (check[number])
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복이 있으면 안됩니다.");
+
+                check[number] = true;
+                winningNumbers.add(number);
+            }
+
+            return winningNumbers;
+       } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 번호 형식입니다.");
+       } 
+    }
+
+    public int parseBonusNumber(String input) {
+        try {
+            int bonusNumber = Integer.parseInt(input);
+            if (bonusNumber < RANGE_LOW || bonusNumber > RANGE_HIGH)
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + RANGE_LOW + " 부터 " + RANGE_HIGH + " 사이여야 합니다.");
+
+            return bonusNumber;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 보너스 번호 형식입니다.");
+        }
+    }
 
     
 }
