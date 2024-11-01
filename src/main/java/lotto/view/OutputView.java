86226f047025e4d1b14bda19c52923238c79bd47
sangjun <sangjoon12345@gmail.com>
2024-11-01 13:57:04 +0900

    feat: 보너스 번호 입력을 요구하는 메세지 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,10 +5,11 @@ import lotto.model.Lotto;
 import java.util.List;
 
 public class OutputView {
-    public static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
-    public static final String INFORM_LOTTO_COUNT = "%d개를 구매했습니다.";
-    public static final String REQUEST_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
-    public static final String NEW_LINE = System.lineSeparator();
+    private static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
+    private static final String INFORM_LOTTO_COUNT = "%d개를 구매했습니다.";
+    private static final String REQUEST_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_LOTTO = "보너스 번호를 입력해 주세요.";
+    private static final String NEW_LINE = System.lineSeparator();
 
     public void requestLottoPrice() {
         System.out.println(REQUEST_LOTTO_PRICE);
@@ -20,9 +21,15 @@ public class OutputView {
         for (Lotto lotto : lottoSets) {
             System.out.println(lotto.getLotto());
         }
+        System.out.print(NEW_LINE);
     }
 
     public void requestWinningLottoNumbers(){
         System.out.println(REQUEST_WINNING_LOTTO);
+    }
+
+    public void requestBonusLottoNumbers(){
+        System.out.print(NEW_LINE);
+        System.out.println(REQUEST_BONUS_LOTTO);
     }
 }
