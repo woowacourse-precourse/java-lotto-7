09f5e9214911454690f0a474522414db5e606d15
hanwool <hanwe92@gmail.com>
2024-11-01 10:12:07 +0900

    feat(NumberParser, NumberValidator): Change logic of NumberValidator
    Changed logic to reuse one method in another method

--- src/main/java/lotto/validation/NumberValidator.java
+++ src/main/java/lotto/validation/NumberValidator.java
@@ -1,25 +1,20 @@
 package lotto.validation;
 
 import java.util.List;
-import lotto.util.enums.ValidateMessage;
+import lotto.exception.InputException;
 
 public class NumberValidator {
     private static final String NUMBER_REGX = "[0-9]";
 
-    public static void validateLottoNumberString(List<String> numbers) {
+    public static void validateMultiNumberInput(List<String> numbers) {
         boolean isAllNumeric = numbers.stream()
-                .allMatch(number -> number.matches(NUMBER_REGX));
-        throwNotNumericException(isAllNumeric);
+                .allMatch(NumberValidator::isInputNumber);
+        InputException.notNumericException(isAllNumeric);
     }
 
-    public static void validateBonusNumberString(String number) {
+    public static boolean isInputNumber(String number) {
         boolean isNumeric = number.matches(NUMBER_REGX);
-        throwNotNumericException(isNumeric);
-    }
-    
-    private static void throwNotNumericException(boolean isNumeric) {
-        if (!isNumeric) {
-            throw new IllegalArgumentException(ValidateMessage.NON_NUMERIC_ERROR.getMessage());
-        }
+        InputException.notNumericException(isNumeric);
+        return true;
     }
 }
--- src/main/java/lotto/view/NumberParser.java
+++ src/main/java/lotto/view/NumberParser.java
@@ -17,13 +17,15 @@ public class NumberParser {
         // 2. validation은 진짜 validation만 수행하는 것 중 무엇이 나은지 고민됨
         // 1번으로 가면 Integer.parseInt()를 통해 별도 로직 없이 숫자 체크를 할 수 있음
         // 다만, validation의 책임이 늘어나는 것처럼 보이기도 함
-        NumberValidator.validateLottoNumberString(trimNumbers);
+        // 이 클래스 내부의 private 메소드로 validation을 둘까?
+        // 객체에서 따로 validation을 하는 것과 동일한 맥락일 수 있을 것 같음
+        NumberValidator.validateMultiNumberInput(trimNumbers);
         return trimNumbers.stream().map(Integer::parseInt).toList();
     }
 
     public static int parseBonusNumber(String input) {
         String trimNumber = input.trim();
-        NumberValidator.validateBonusNumberString(trimNumber);
+        NumberValidator.isInputNumber(trimNumber);
         return Integer.parseInt(trimNumber);
     }
 }
