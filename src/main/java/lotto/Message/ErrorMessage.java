493f160862401a3609af6f9ea522d2d6c3f9a2b7
junho767 <junho767@naver.com>
2024-11-01 18:50:12 +0900

    fix: 메시지에 [ERROR] 추가
    NUMBER_OUT_OF_RANGE, INVALID_NUMBER에 추가

--- src/main/java/lotto/Message/ErrorMessage.java
+++ src/main/java/lotto/Message/ErrorMessage.java
@@ -1,9 +1,9 @@
 package lotto.Message;
 
 public enum ErrorMessage {
-    INVALID_NUMBER("숫자가 아닙니다."),
+    INVALID_NUMBER("[ERROR] 숫자가 아닙니다."),
     NOT_THOUSAND_UNIT("[ERROR] 1,000원 단위로 입력해야 합니다."),
-    NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
 
     public final String message;
