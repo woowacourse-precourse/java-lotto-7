0997840f992398c5733a97f9dad4629c00b32edc
woodongle <315ehdgur@gmail.com>
2024-11-01 18:37:58 +0900

    feat: 사용자 입력 검증 추가
    1. 사용자 입력이 양수인지 확인하는 메서드를 추가합니다.
      - checkPositiveNumber()

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -12,7 +12,15 @@ public class LottoGame {
 
     private void purchaseAmountInput() {
         System.out.println("구입금액을 입력해 주세요.");
-        purchaseAmount = Integer.parseInt(Console.readLine());
+        purchaseAmount = Integer.parseInt(checkPositiveNumber(Console.readLine()));
         System.out.println(purchaseAmount);
+    }
+
+    // 양수인지 확인하는 메서드
+    private String checkPositiveNumber(String number) {
+        if (!number.matches("^[1-9]\\d*$")) {
+            throw new IllegalArgumentException("[ERROR] 입력한 값은 숫자(양수)여야 합니다.");
+        }
+        return number;
     }
 }
