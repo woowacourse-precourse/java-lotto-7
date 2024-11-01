ca4520208a8fa12fb56aec145769e98ad0b736af
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-01 17:24:53 +0900

    feat: 당첨 번호를 검증하는 기능 추가

--- src/main/java/lotto/parser/InputParser.java
+++ src/main/java/lotto/parser/InputParser.java
@@ -1,7 +1,19 @@
 package lotto.parser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputParser {
+    private static final String DELIMITER = ",";
+
     public static int parseInteger(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> parseNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/lotto/validator/WinningNumbersValidator.java
@@ -0,0 +1,35 @@
+package lotto.validator;
+
+import static lotto.parser.InputParser.parseNumbers;
+import static lotto.validator.ValidationMessage.INVALID_WINNING_NUMBERS_FORMAT;
+
+import lotto.exception.InvalidInputException;
+
+public class WinningNumbersValidator {
+
+    public void validate(String inputNumbers) {
+        validateNotNull(inputNumbers);
+        validateNotEmpty(inputNumbers);
+        validateDelimiter(inputNumbers);
+    }
+
+    public void validateNotNull(String inputNumbers) {
+        if (inputNumbers == null) {
+            throw new InvalidInputException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
+        }
+    }
+
+    private void validateNotEmpty(String inputNumbers) {
+        if (inputNumbers.isEmpty() || inputNumbers.isBlank()) {
+            throw new InvalidInputException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
+        }
+    }
+
+    private void validateDelimiter(String inputNumbers) {
+        try {
+            parseNumbers(inputNumbers);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
+        }
+    }
+}
