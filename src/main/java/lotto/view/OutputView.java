a349c6ab72b9ade55e2a232d9ef25b34d3d5165a
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-01 14:57:53 +0900

    feat: 로또 구매 개수를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import static lotto.view.ViewMessage.INPUT_PURCHASE_PRICE;
+import static lotto.view.ViewMessage.PRINT_TICKET_COUNT;
 
 public class OutputView {
 
@@ -11,6 +12,11 @@ public class OutputView {
         printMessage(INPUT_PURCHASE_PRICE.getMessage());
     }
 
+    public static void printLottoTicket(int ticketCount) {
+        printMessage();
+        printMessage(PRINT_TICKET_COUNT.getMessage(), ticketCount);
+    }
+
     private static void printMessage() {
         System.out.println();
     }
@@ -19,4 +25,7 @@ public class OutputView {
         System.out.println(message);
     }
 
+    private static void printMessage(String message, Object... args) {
+        System.out.println(String.format(message, args));
+    }
 }
--- src/main/java/lotto/view/ViewMessage.java
+++ src/main/java/lotto/view/ViewMessage.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
 public enum ViewMessage {
-    INPUT_PURCHASE_PRICE("구입금액을 입력해 주세요.");
+    INPUT_PURCHASE_PRICE("구입금액을 입력해 주세요."),
+    PRINT_TICKET_COUNT("%d개를 구매했습니다.");
+
     private final String message;
 
     ViewMessage(String message) {
