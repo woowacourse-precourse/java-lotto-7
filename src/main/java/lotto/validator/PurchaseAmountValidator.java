d2bb502dd0285b13d64b6f318b917334a39dbc1c
OhYeHyun <ohyhohyl@naver.com>
2024-11-02 00:12:55 +0900

    feat(PurchaseAmountValidator): 구입 금액 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -1,4 +1,17 @@
 package lotto.validator;
 
+import static lotto.constants.LottoConstants.*;
+
 public class PurchaseAmountValidator {
+    public PurchaseAmountValidator(String input) {
+        validateIsNumber(input);
+    }
+
+    private void validateIsNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.MUST_BE_NUMBER.getMessage(PURCHASE_AMOUNT));
+        }
+    }
 }
