76fa2eee1fb428ce64b9a0b56ff301d03c47a833
qudcks8084 <qudcks8084@naver.com>
2024-11-02 00:09:40 +0900

    Fix : CommonValidator을 호출하는 방식으로 변경
    - 기존 4개의 함수를 CommonValidator를 상속받아 호출하도록 수정
    - 호출하면서 각 항목에 맞는 예외 메시지 출력

--- src/main/java/lotto/util/validator/PurchaseMoneyValidator.java
+++ src/main/java/lotto/util/validator/PurchaseMoneyValidator.java
@@ -2,14 +2,9 @@ package lotto.util.validator;
 
 import lotto.util.constant.LottoConstants;
 
-public class PurchaseMoneyValidator {
-    private static final char ZERO_ASCII = '0';
-    private static final int ZERO = 0;
+public class PurchaseMoneyValidator extends CommonValidator {
 
-    // CONSTANTS FOR EXCEPTION MESSAGE
-    private static final String ERR_MSG_WRONG_PURCHASE_INPUT ="[ERROR] 로또 구입 금액은 1,000원 단위의 양의 정수이여야합니다.";
-    private static final String ERR_MSG_WRONG_WINNER_NUMBERS = "[ERROR] 로또 당첨 번호는 1부터 45 사이의 6개의 숫자여야 합니다.";
-    private static final String ERR_MSG_WRONG_BONUS_NUMBER = "[ERROR] 로또 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERR_MSG_WRONG_PURCHASE_INPUT ="[ERROR] 로또 구입 금액은 1,000원 단위의 양의 정수만 가능합니다.";
 
     /**
      * 사용자에게 입력받은 로또 구입 금액이 유효한 입력값인지 검사
@@ -26,43 +21,16 @@ public class PurchaseMoneyValidator {
      * @param inputPurchaseMoney
      */
     public static void validatePurchaseMoney(String inputPurchaseMoney) throws IllegalArgumentException {
-        validateEmptyValue(inputPurchaseMoney);
-        validateWhitespaceAtHeadOrTail(inputPurchaseMoney);
-        validateEachCharacterIsDigit(inputPurchaseMoney);
-        validateStartWithZero(inputPurchaseMoney);
+        validateEmptyValue(inputPurchaseMoney, ERR_MSG_WRONG_PURCHASE_INPUT);
+        validateWhitespaceAtHeadOrTail(inputPurchaseMoney, ERR_MSG_WRONG_PURCHASE_INPUT);
+        validateEachCharacterIsDigit(inputPurchaseMoney, ERR_MSG_WRONG_PURCHASE_INPUT);
+        validateStartWithZero(inputPurchaseMoney, ERR_MSG_WRONG_PURCHASE_INPUT);
         validateDivisibleByBaseUnit(inputPurchaseMoney);
-    }
-
-    static void validateEmptyValue(String validateTarget) throws IllegalArgumentException {
-        if (validateTarget.isEmpty()) {
-            throw new IllegalArgumentException(ERR_MSG_WRONG_PURCHASE_INPUT);
-        }
-    }
-
-    static void validateWhitespaceAtHeadOrTail(String validateTarget) throws IllegalArgumentException {
-        if (!validateTarget.equals(validateTarget.trim())) {
-            throw new IllegalArgumentException(ERR_MSG_WRONG_PURCHASE_INPUT);
-        }
-    }
-
-    static void validateEachCharacterIsDigit(String validateTarget) throws IllegalArgumentException {
-        char[] targets = validateTarget.toCharArray();
-        for (char target : targets) {
-            if (!Character.isDigit(target)) {
-                throw new IllegalArgumentException(ERR_MSG_WRONG_PURCHASE_INPUT);
-            }
-        }
-    }
-
-    static void validateStartWithZero(String validateTarget) throws IllegalArgumentException {
-        if (validateTarget.charAt(ZERO) == ZERO_ASCII) { // '0' 문자로 확인
-            throw new IllegalArgumentException(ERR_MSG_WRONG_PURCHASE_INPUT);
-        }
     }
 
     static void validateDivisibleByBaseUnit(String validateTarget) throws IllegalArgumentException {
         long target = Long.parseLong(validateTarget);
-        if (target % LottoConstants.LOTTO_BASE_UNIT != ZERO) {
+        if (target % LottoConstants.LOTTO_BASE_UNIT != LottoConstants.ZERO) {
             throw new IllegalArgumentException(ERR_MSG_WRONG_PURCHASE_INPUT);
         }
     }
