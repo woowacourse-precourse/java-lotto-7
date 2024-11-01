13f68f1b5486f23ccd4ce78f4a0bdb0c18785d71
srcho01 <seorim5306@gmail.com>
2024-11-01 15:35:14 +0900

    feat: 로또 발행
    - 로또를 발행해야 하는 수 계산
    - 로또 발행
    - 로또 번호에 중복된 숫자가 없는지 검사
    - 발행한 로또 번호 출력

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import lotto.util.ErrorCode;
+
+import java.util.*;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+        Collections.sort(this.numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw ErrorCode.INVALID_LOTTO_NUMBER_COUNT.exception();
+        }
+
+        Set<Integer> integerSet = new HashSet<>(numbers);
+        if (numbers.size() > integerSet.size()) {
+            throw ErrorCode.DUPLICATE_LOTTO_NUMBER.exception();
+        }
+    }
+
+    @Override
+    public String toString() {
+        List<String> formattedNumbers = numbers.stream()
+                .map(String::valueOf)
+                .toList();
+
+        return "[" + String.join(", ", formattedNumbers) + "]";
+    }
+}
--- src/main/java/lotto/util/ErrorCode.java
+++ src/main/java/lotto/util/ErrorCode.java
@@ -2,8 +2,9 @@ package lotto.util;
 
 public enum ErrorCode {
 
-    INVALD_PURCHASE_AMOUNT("구매 금액은 1,000원 이상 100,000원 이하이고, 1,000으로 나누어 떨어져야 합니다.");
-
+    INVALD_PURCHASE_AMOUNT("구매 금액은 1,000원 이상 100,000원 이하이고, 1,000으로 나누어 떨어져야 합니다."),
+    INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다.");
 
     private final String message;
 
