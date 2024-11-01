d116a5f61d650a72a90cb3c9ee7e207603073b4b
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-01 11:52:14 +0900

    feat(app) : 로또 진행
    - LottoController 를 생성해 로또 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.concurrent.ConcurrentSkipListMap;
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.tryLotto();
     }
 }
