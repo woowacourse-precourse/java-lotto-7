28ca8006cab8d605dba4cd19d0db5536be590ea9
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 17:23:41 +0900

    refactor(CalculateResult): 당첨 내역 계산 클래스 분리

--- src/main/java/lotto/domain/CalculateResult.java
+++ src/main/java/lotto/domain/CalculateResult.java
@@ -1,2 +1,38 @@
-package lotto.domain;public class CalculateLottoResult {
+package lotto.domain;
+
+import java.util.List;
+import lotto.util.PrizeType;
+
+public class CalculateResult {
+    public int[] calculateStatistics(List<Integer> winningNumbers, int bonusNumber, List<List<Integer>> lottoNumbers) {
+        int[] results = new int[PrizeType.values().length];
+
+        for (List<Integer> singleLotto : lottoNumbers) {
+            int matchCount = countMatchingNumbers(singleLotto, winningNumbers);
+            boolean hasBonus = singleLotto.contains(bonusNumber);
+
+            if (matchCount == 6) {
+                results[PrizeType.MATCHING_6.ordinal()]++;
+            } else if (matchCount == 5 && hasBonus) {
+                results[PrizeType.HAS_BONUS_WIN_MATCHING_5.ordinal()]++;
+            } else if (matchCount == 5) {
+                results[PrizeType.MATCHING_5.ordinal()]++;
+            } else if (matchCount == 4) {
+                results[PrizeType.MATCHING_4.ordinal()]++;
+            } else if (matchCount == 3) {
+                results[PrizeType.MATCHING_3.ordinal()]++;
+            }
+        }
+        return results;
+    }
+
+    private int countMatchingNumbers(List<Integer> singleLotto, List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int number : singleLotto) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
 }
