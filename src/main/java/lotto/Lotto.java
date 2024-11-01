59ca23ff3851666effdb9dc72e6c19de7b13029b
seullll <tmfud@naver.com>
2024-11-01 16:02:15 +0900

    refactor: 코드 리팩토링

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -20,8 +20,7 @@ public class Lotto {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
             }
         }
-        long count = numbers.stream().distinct().count();
-        if(count != 6){
+        if(numbers.stream().distinct().count() != 6){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
