f1b72484b3916a2400e95156e7dc927feb0ff1a9
pil-go <pilbongx@naver.com>
2024-11-02 06:00:53 +0900

    feat(ErroeMessage): 에러메시지 관리 위한 enum 추가

---
+++ src/main/java/lotto/Messages/ErrorMessage.java
@@ -0,0 +1,17 @@
+package lotto.Messages;
+
+public enum ErrorMessage {
+    EMPTY_PURCHASE_AMOUNT("구입 금액은 빈 문자열로 입력할 수 없습니다."),
+    BLANK_PURCHASE_AMOUNT("구입 금액을  공백 문자열로 입력할 수 없습니다."),
+    NOT_NUMERIC_PURCHASE_AMOUNT("구입 금액은 숫자만 입력할 수 있습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
