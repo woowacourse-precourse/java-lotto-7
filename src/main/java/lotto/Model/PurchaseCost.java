032a976778decf5f5ffa5db26d55eeea3232fd85
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:13:48 +0900

    feat(PurchaseCost): 1,000으로 나누어 떨어지지 않는 구입금액은 예외를 던져라

--- src/main/java/lotto/Model/PurchaseCost.java
+++ src/main/java/lotto/Model/PurchaseCost.java
@@ -5,12 +5,19 @@ public class PurchaseCost {
 
     public PurchaseCost(int inputtedCost) {
         validateUnderZero(inputtedCost);
+        validateCanDivideBy1000(inputtedCost);
         this.purchaseCost = inputtedCost;
     }
 
     private void validateUnderZero(int inputtedCost) {
         if (inputtedCost <= 0) {
             throw new IllegalArgumentException("[ERROR] 0 이하의 수는 입력하실 수 없습니다.");
+        }
+    }
+
+    private void validateCanDivideBy1000(int inputtedCost) {
+        if (inputtedCost % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR]: 1,000으로 나누어 떨어지지 않는 수는 입력하실 수 없습니다.");
         }
     }
 }
