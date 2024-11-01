a980c8936783fcacebdb6211506ddfcf156f3a6e
woongjee <jeewoong7753@naver.com>
2024-11-01 14:04:55 +0900

    fix(README.md):중복된 번호 개수 세는 기능 winning 클래스로 이동

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -9,5 +9,10 @@ public class Winning {
         if(winningNumbers.size() != 6){
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개입니다.");
         }
+        for(int i = 0; i < winningNumbers.size(); i++){
+            if(winningNumbers.get(i) < 1||winningNumbers.get(i) > 45||countOcurrences(winningNumbers, winningNumbers.get(i)){
+
+            }
+        }
     }
 }
