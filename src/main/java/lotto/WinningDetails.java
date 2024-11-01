7702c8a268d4dd21dae196889e179382da3be3e5
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 23:21:17 +0900

    refactor(WinningDetails) : sumUpGrades 함수 리팩토링
    WinningDetails 클래스를 반환하던 함수를 그냥 void를 반환시키는 것으로 바꿈.

--- src/main/java/lotto/WinningDetails.java
+++ src/main/java/lotto/WinningDetails.java
@@ -10,15 +10,14 @@ public class WinningDetails {
     private Integer fifthBonus = 0;
     private Integer sixth = 0;
 
-    public WinningDetails sumUpGrades(List<MyResult> resultList){
+    public void sumUpGrades(List<MyResult> resultList){
         for(MyResult result : resultList){
             if(result.getMatches() == 3) this.third++;
             else if(result.getMatches() == 4) this.fourth++;
             else if(result.getMatches() == 5 && result.getBonusPoint()) this.fifth++;
-            else if(result.getMatches() == 5 && !result.getBonusPoint()) this.sixth++;
+            else if(result.getMatches() == 5 && !result.getBonusPoint()) this.fifthBonus++;
             else if(result.getMatches() == 6) this.sixth++;
         }
-        return this;
     }
 
     public static List<MyResult> saveMyGrades(List<Lotto> lottoList, Lotto answer, Integer bonus){
