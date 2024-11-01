822e005e091a74f384b87d17ff6eabf0a0787405
Hacanna42 <dtmdgus901@gmail.com>
2024-11-02 00:52:23 +0900

    refactor(MoneyTest): DisplayName 문구 수정

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -3,12 +3,11 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class MoneyTest {
-    @DisplayName("돈 1,000원 당 로또 티켓 1개로 교환할 수 있다")
+    @DisplayName("돈 1,000원 당 로또 티켓 1개로 교환할 수 있다.")
     @ParameterizedTest
     @CsvSource({"1000, 1", "2000, 2"})
     void 돈_티켓_교환_테스트(int amount, int expected) {
