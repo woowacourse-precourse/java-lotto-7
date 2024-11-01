94274ab71b5e6b66b6aee460403a78a54705053d
Chanyoo Kim <cksdb012@gmail.com>
2024-11-01 17:27:29 +0900

    refactor: 에러 메시지 수정
    에러 메시지 수정

--- src/main/java/lotto/exception/PurchaseAmountZeroException.java
+++ src/main/java/lotto/exception/PurchaseAmountZeroException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class PurchaseAmountZeroException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "[ERROR] 구입 금액은 0원 이상이어야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 구입 금액은 0원 이상이어야 합니다";
 
     public PurchaseAmountZeroException() {
         super(ERROR_MESSAGE);
