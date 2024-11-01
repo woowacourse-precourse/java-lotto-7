2295f30f68c36aea4a0f8470dacbac79627a188d
seophohoho <seop0937@gmail.com>
2024-11-01 22:12:21 +0900

    test(WinningNumber): 당첨 번호 입력 유효성 검사 테스트 코드 추가

---
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -0,0 +1,59 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumberTest {
+    private final WinningNumber winningNumber = new WinningNumber();
+
+    @Test
+    @DisplayName("당첨 번호 입력 유효성 검사")
+    void testValidate() {
+        assertThatThrownBy(() -> winningNumber.validate("-1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,a,3,c,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("  1,  2,3,4, 5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2 ,3,4, 5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate(" 1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2,3,4,5,6,"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2,3,4"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate(""))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,2,3,4,5,,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("1,99,98,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> winningNumber.validate("46,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
