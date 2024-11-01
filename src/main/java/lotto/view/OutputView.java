0972c182abd2d4f3d3dbd3e314725667294c069c
ouvir <kilhyunii@naver.com>
2024-11-01 23:04:24 +0900

    feat(OutputView): 구매한 로또 번호 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,8 +4,25 @@ import java.util.List;
 
 public class OutputView {
     private static final String DISPLAY_LOTTO_COUNT_MESSAGE_FORMAT = "%d개를 구매했습니다.";
+    private static final String DISPLAY_LOTTO_NUMBER_MESSAGE_FORMAT = "[%s]";
 
     public void displayPurchasedLottoCount(int count) {
         System.out.println(String.format(DISPLAY_LOTTO_COUNT_MESSAGE_FORMAT, count));
+    }
+
+    public void displayPurchasedLottoTickets(List<List<Integer>> lottoTickets) {
+        for (List<Integer> lottoTicket : lottoTickets) {
+            String lottoView = formattingLottoNumbers(lottoTicket);
+            System.out.println(lottoView);
+        }
+    }
+
+    private String formattingLottoNumbers(List<Integer> lottoTicket) {
+        List<String> numbers = lottoTicket.stream()
+                .map(String::valueOf)
+                .toList();
+
+        String joinedNumbers = String.join(", ", numbers);
+        return String.format(DISPLAY_LOTTO_NUMBER_MESSAGE_FORMAT, joinedNumbers);
     }
 }
