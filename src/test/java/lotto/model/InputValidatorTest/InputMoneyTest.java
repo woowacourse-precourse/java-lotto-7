8ddadec1d57c219818e0d038f32123acc4d3009b
corpi <lj845854@gmail.com>
2024-11-01 14:47:45 +0900

    refactor(InputMoneyTest): 클래스 명 변경

--- src/test/java/lotto/model/InputValidatorTest/InputMoneyTest.java
+++ src/test/java/lotto/model/InputValidatorTest/InputMoneyTest.java
@@ -1,14 +1,15 @@
-package lotto.model;
+package lotto.model.InputValidatorTest;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.model.InputValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class InputValidatorTest {
+class InputMoneyTest {
     private InputValidator inputValidator;
 
     @BeforeEach
