28561ac13dad7605b47a1cfe7dc40f90f3e2be7c
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-01 18:42:33 +0900

    feat( LottoService.java ) Add Method
    Add Method List
     - lotResult()
     - createPapers()
     - createRandomNumber()

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,32 +1,75 @@
 package lotto.service;
 
+import lotto.common.utils.ValidationUtils;
 import lotto.model.Lotto;
 import lotto.model.UserData;
 import lotto.view.UserInput;
 import lotto.view.PrintMessages;
 
-//import lotto.common.utils.ValidationUtils;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 
-//단일 데이터에 대한 데이터 처리
-public class LottoService {
+public class LottoService{
 
     UserData userData = new UserData();
+    UserInput userInput = new UserInput();
+    InputParser inputParser = new InputParser();
+    MatchCheck matchCheck = new MatchCheck();
     Lotto[] lotto;
 
-    UserInput userInput = new UserInput();
-
     public void userInput() {
+        int integerData;
+        List<Integer> arrayData;
 
+        //Money Input
         PrintMessages.printInputMoneyMsg();
-        userInput.getInput();
+        integerData = inputParser.strToInt( userInput.getInput() );
+        userData.setMoney(integerData);
 
-        userData.setLottoPapes( userData.getMoney() );
+        //Calculate Lotto Papers
+        integerData = inputParser.calculatePapers( integerData );
+        userData.setLottoPapes(integerData);
+        PrintMessages.printBuyMsg(integerData);
 
-        lotto = new Lotto[ userData.getLottoPapes() ];
+        //WinNumber Input
+        PrintMessages.printInputWinNumberMsg();
+        arrayData = inputParser.splitToArray( userInput.getInput() );
+        userData.setWinNumbers(arrayData);
 
+        //BonusNumber Input
+        PrintMessages.printInputBonusNumberMsg();
+        integerData = inputParser.strToInt( userInput.getInput() );
+        ValidationUtils.validateDuplicateBonusNumber(arrayData, integerData);
+        userData.setBonusNumber(integerData);
 
     }
+
+    public void lotResult() {
+        int matchCount = 0;
+        List<Integer> matchs;
+        PrintMessages.printResultMsg();
+        PrintMessages.printDash();
+        createPapers();  // `createPapes` 메서드로 로또 용지 생성
+
+        matchs = matchCheck.matchCountPapers(lotto, userData.getWinNumbers(), userData.getBonusNumber());
+        System.out.println(matchs);
+
+    }
+
+
+    public void createPapers(){
+        lotto = new Lotto[ userData.getLottoPapes() ];
+        for ( int i = 0; i < lotto.length; i++ ) {
+            lotto[i] = new Lotto( inputParser.sortArray(createRandomNumber()) );
+        }
+    }
+
+    public List<Integer> createRandomNumber(){
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
 
 
 }
