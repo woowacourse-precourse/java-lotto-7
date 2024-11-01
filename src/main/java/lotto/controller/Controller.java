265e44d9b217006a49104c30fcde16e53c763261
fpem3309 <fpem3309@gmail.com>
2024-11-01 11:51:43 +0900

    feat: 발행한 로또 수량 출력 기능 구현

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,15 +1,19 @@
 package lotto.controller;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Controller {
     private final InputView inputView;
+    private final OutputView outputView;
 
     public Controller() {
+        this.outputView = new OutputView();
         this.inputView = new InputView();
     }
 
     public void run() {
-        inputView.readPurchaseAmount();
+        int thousandUnitCount = inputView.readPurchaseAmount();
+        outputView.printUnitCount(thousandUnitCount);
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    public void printUnitCount(int thousandUnitCount) {
+        System.out.println(thousandUnitCount + "개를 구매했습니다.");
+    }
+}
