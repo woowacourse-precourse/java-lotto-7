91ca12c3238eefa22ec775da73b1ebbb271d3d70
jonghwanlee <kavae550@gmail.com>
2024-11-01 20:20:22 +0900

    feat(domain): 로또숫자 생성 기능 구현
    -임의의 숫자를 만들어내는 기능이 있는 인터페이스 NumberGenerator 구현
    -이후 이 인터페이싀 구현체인 LottoNumbersGenerator 클래스를 통해 임의의 로또 번호를 생성하는 기능 구현

--- src/main/java/lotto/domain/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoNumbersGenerator.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
-public class LottoNumbersGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+import static lotto.constants.LottoConstants.LOWER_BOUND;
+import static lotto.constants.LottoConstants.UPPER_BOUND;
+import static lotto.constants.LottoConstants.NUMBERS_PER_LOTTO;
+
+
+public class LottoNumbersGenerator implements NumberGenerator {
+
+    public List<Integer> generateNumbers() {
+        return  Randoms.pickUniqueNumbersInRange(
+                LOWER_BOUND.getValue()
+                ,UPPER_BOUND.getValue()
+                ,NUMBERS_PER_LOTTO.getValue());
+    }
 }
