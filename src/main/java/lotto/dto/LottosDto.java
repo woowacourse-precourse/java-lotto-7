9c811348f0dcf94bcbb7438a4a91b4e3b27177f3
jonghwanlee <kavae550@gmail.com>
2024-11-02 00:10:29 +0900

    refactor: 클래스명 변경

---
+++ src/main/java/lotto/dto/LottosDto.java
@@ -0,0 +1,7 @@
+package lotto.dto;
+
+import java.util.List;
+
+public record LottosDto(List<LottoDto> lottoDtos)  {
+    
+}
---
+++ src/main/java/lotto/service/LottoPurchaseService.java
@@ -0,0 +1,5 @@
+package lotto.service;
+
+public interface LottoPurchaseService {
+    void buyLottos(int purchaseAmount);
+}
