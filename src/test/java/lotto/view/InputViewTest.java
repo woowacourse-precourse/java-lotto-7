b50feebba0f8786cfd13d535b880f27c2a506d36
JeongHwan Kim <silverice1973@naver.com>
2024-11-01 10:49:47 +0900

    test: 당첨 번호, 보너스 번호 유효성 검사

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -4,6 +4,8 @@ import lotto.validator.InputValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,5 +32,47 @@ public class InputViewTest {
     void 유효한_구입_금액을_입력하면_해당_금액을_반환한다() {
         int amount = InputValidator.validatePurchaseAmount("3000");
         assertThat(amount).isEqualTo(3000);
+    }
+
+    @DisplayName("당첨 번호가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void 당첨_번호가_6개가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumber(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호가 1미만 45초과 라면 예외가 발생한다.")
+    @Test
+    void 당첨_번호가_1미만_45초과_라면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumber(List.of(-1, 0, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 번호는 1부터 45 사이어야 합니다.");
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumber(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 번호가 있습니다.");
+    }
+
+    @DisplayName("보너스 번호가 1미만 45초과 라면 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_1미만_45초과_라면_예외가_발생한다() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(0, winningNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1부터 45 사이어야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> InputValidator.validateBonusNumber(1, winningNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
 }
