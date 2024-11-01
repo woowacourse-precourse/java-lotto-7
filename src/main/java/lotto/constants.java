6d280a71695d6e30e8badd73b4223404d79418c9
Layla Oh <crispylemon7120@gmail.com>
2024-11-01 23:19:21 +0900

    fix(warnings): 코드 경고 해결을 통한 안정성 향상
    불필요한 변수 및 import 경고 처리

--- src/main/java/lotto/constants.java
+++ src/main/java/lotto/constants.java
@@ -1,14 +1,14 @@
 package lotto;
 
 public class constants {
-    public class Request_Messages{
+    public static class Request_Messages{
         public static final String INPUT_TOTAL_AMOUNT = "구입금액을 입력해 주세요.";
         public static final String OUTPUT_TOTAL_COUNT = "개를 구매했습니다.";
         public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
         public static final String INPUT_BONUS_WINNING_NUMBER = "보너스 번호를 입력해 주세요.";
         public static final String SUMMARY = "당첨 통계\n---\n";
     }
-    public class Error_Messages{
+    public static class Error_Messages{
         public static final String INPUT_NOT_POSITIVE_INT = "[ERROR] 값은 양수여야 합니다.";
         public static final String INPUT_TOTAL_AMOUNT_NOT_LARGER_THAN_1000 = "[ERROR] 구입 최소 금액은 1000원입니다.";
         public static final String INPUT_ERROR = "[ERROR] 입력값에 오류가 있습니다.";
