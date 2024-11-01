0e18bf7dd399978d72e652418aad5004da1327bd
kjyyjk <rlawnsdud920@gmail.com>
2024-11-01 16:05:06 +0900

    feat: 발행된 로또의 수량을 안내한다
    - run()은 흐름을 제어하는 책임을 가지도록 출력 로직을 메서드로 분리한다.

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class LottoManager {
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-
+    private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final int LOTTO_PRICE = 1000;
 
     public void run() {
         int purchaseAmount = inputPurchaseAmount();
-        List<Lotto> lottoes = buyLottoes(purchaseAmount);
+        List<Lotto> lottoes = purchaseLottoes(purchaseAmount);
+        printPurchaseMessage(lottoes.size());
     }
 
     public int inputPurchaseAmount() {
@@ -18,8 +19,12 @@ public class LottoManager {
         return Integer.parseInt(Console.readLine());
     }
 
-    private List<Lotto> buyLottoes(final int purchaseAmount) {
+    private List<Lotto> purchaseLottoes(final int purchaseAmount) {
         int totalLottoCount = purchaseAmount / LOTTO_PRICE;
-        return LottoMachine.issueLottoes(purchaseAmount / LOTTO_PRICE);
+        return LottoMachine.issueLottoes(totalLottoCount);
+    }
+
+    private static void printPurchaseMessage(final int purchasedLottoesCount) {
+        System.out.println(purchasedLottoesCount + LOTTO_PURCHASE_MESSAGE);
     }
 }
