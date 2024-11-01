6273518298055ceaa0c3ef8100c4c119c1d2d44b
jungsu <vwx1@naver.com>
2024-11-01 17:26:58 +0900

    feat: 맵핑과 의존성 주입
    LottoController에 각 클래스들을 맵핑하고 생성자를 통한 의존성 주입을 구현했습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,20 @@
 package lotto.controller;
 
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+      this.inputView = inputView;
+      this.outputView = outputView;
+      this.lottoService = lottoService;
+    }
+
 
 
 }
