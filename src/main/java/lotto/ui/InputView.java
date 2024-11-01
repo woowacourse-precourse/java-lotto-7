4fefc8dd3f8bf9ba28cfb0480dbb9435d73fdc69
Mini <mini@MiniMac.local>
2024-11-01 18:16:55 +0900

    feat: add validate method for winningNumbers in InputView

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String ERROR_NON_POSITIVE_AMOUNT = "[ERROR] 0초과 값을 입력해주세요.";
     private static final String ERROR_INVALID_AMOUNT_UNIT = "[ERROR] 1,000원 단위로 입력해주세요.";
     private static final String ERROR_INVALID_NUMBER_FORMAT = "[ERROR] 정수값을 입력해주세요.";
+    private static final String ERROR_INVALID_NUMBER_SIZE = "[ERROR] 각 번호는 1 이상 45 이하여야 합니다.";
 
 
     public static int getPurchaseAmount() {
@@ -26,10 +27,17 @@ public class InputView {
 
     public static List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해주세요. (쉼표로 구분)");
-        return Arrays.stream(Console.readLine().split(","))
-                     .map(Integer::parseInt)
-                     .sorted()
-                     .collect(Collectors.toList());
+        try {
+            List<Integer> numbers = Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .sorted()
+                    .collect(Collectors.toList());
+            validateWinningNumbers(numbers);
+            return numbers;
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_INVALID_NUMBER_FORMAT);
+        }
     }
 
     public static int getBonusNumber() {
@@ -45,5 +53,14 @@ public class InputView {
             if (amount % 1000 != 0) {
                 throw new IllegalArgumentException(ERROR_INVALID_AMOUNT_UNIT);
             }
+    }
+
+    private static void validateWinningNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ERROR_INVALID_NUMBER_SIZE);
+            }
+        }
+
     }
 }
