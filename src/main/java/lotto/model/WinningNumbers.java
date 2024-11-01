563a36e770b69a8716b9d2b436351fad74386d94
이윤빈 <kanado20000@naver.com>
2024-11-01 14:39:10 +0900

    :sparkles: feat: 당첨/보너스 번호를 관리하는 WinningNumbers 클래스 추가

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import java.util.List;
+import lotto.utilities.Parser;
+import lotto.utilities.Splitter;
+import lotto.validation.WinningNumbersValidator;
+
+public class WinningNumbers {
+  private final List<Integer> winningNumbers;
+  private final Integer bonusNumber;
+
+  public WinningNumbers(String winningNumbers, String bonusNumber) {
+    List<String> splittedWinningNumbers = splitWinningNumbers(winningNumbers);
+    List<Integer> parseredWinningNumbers = paserWinningNumbers(splittedWinningNumbers);
+    Integer parseredBonusNumber = paserBonusNumber(bonusNumber);
+    parseredWinningNumbers.add(0, parseredBonusNumber);
+    validate(parseredWinningNumbers);
+    parseredBonusNumber = parseredWinningNumbers.remove(0);
+    this.winningNumbers = parseredWinningNumbers;
+    this.bonusNumber = parseredBonusNumber;
+  }
+
+  private List<String> splitWinningNumbers(String winningNumbers) {
+    return Splitter.splitWinningNumbers(winningNumbers);
+  }
+
+  private List<Integer> paserWinningNumbers(List<String> winningNumbers) {
+    return Parser.parseNumbersToInt(winningNumbers);
+  }
+
+  private Integer paserBonusNumber(String bonusNumber) {
+    return Parser.parseNumberToInt(bonusNumber);
+  }
+
+  private void validate(List<Integer> winningNumbers) {
+    WinningNumbersValidator.mainValidator(winningNumbers);
+  }
+}
