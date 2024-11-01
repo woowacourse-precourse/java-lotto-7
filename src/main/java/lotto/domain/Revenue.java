a29cd60910de389b1c407100ff50bfe1e7c063a6
StoneCAU <seokwoo1999@naver.com>
2024-11-01 21:03:08 +0900

    feat(Revenue): 수익률 계산을 위한 Revenue 객체 구현

---
+++ src/main/java/lotto/domain/Revenue.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public class Revenue {
+    private final float returnRate;
+
+    public Revenue(int purchaseAmount, int totalAmount) {
+        this.returnRate = calculateReturnRate(purchaseAmount, totalAmount);
+    }
+
+    public float getReturnRate() {
+        return returnRate;
+    }
+
+    private static float calculateReturnRate(int purchaseAmount, int totalAmount) {
+        if (isZero(totalAmount)) return 0;
+        return (float) totalAmount / purchaseAmount * 100;
+    }
+
+    private static boolean isZero(int value) {
+        return value == 0;
+    }
+}
