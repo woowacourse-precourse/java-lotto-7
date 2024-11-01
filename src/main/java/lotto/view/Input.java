f1d532d2aece7173c1b67ca4c75040a9a80e4e1c
yooo <ghddbtl@gmail.com>
2024-11-01 22:22:49 +0900

    feat : create Input.java
    콘솔을 입력받을 메서드와, 구매금액, 당첨번호, 보너스번호를 입력 받는다.

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+  static String READ_PURCHASE_AMOUNT = request();
+  static String READ_WINNING_NUMBERS = request();
+  static String READ_BONUS_NUMBERS = request();
+
+  static String request() {
+    return Console.readLine();
+  }
+
+}
