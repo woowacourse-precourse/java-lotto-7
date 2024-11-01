e44c212c43cad74da1843d73f0e2c668a8b95c85
hanwool <hanwe92@gmail.com>
2024-11-01 17:29:04 +0900

    fix(LottoTest): Shorten test by using MethodSource

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,54 +3,43 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 import lotto.util.enums.ValidateMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
     static final List<Integer> MORE_THAN_SIX_SIZE_NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7);
     static final List<Integer> DUPLICATE_NUMBERS = List.of(1, 2, 3, 4, 5, 5);
     static final List<Integer> MORE_THAN_FIFTY_FIVE_NUMBERS = List.of(1, 2, 3, 4, 5, 67);
-    static final List<Integer> EQUAL_ZERO_NUMBERS = List.of(1, 2, 3, 4, 5, 0);
-    static final List<Integer> LESS_THAN_ZERO_NUMBERS = List.of(1, 2, 3, 4, 5, -1);
 
-    @Test
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
-    void lottoNumberSizeTest() {
-        assertThatThrownBy(() -> new Lotto(MORE_THAN_SIX_SIZE_NUMBERS))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ValidateMessage.NUMBER_SIZE_ERROR.getMessage());
+    @DisplayName("""
+            "로또 번호의 개수가 6개가 넘어가면 예외가 발생한다"
+            "로또 번호에 중복된 숫자가 있으면 예외가 발생한다."
+            "로또 번호 숫자가 45가 넘으면 예외가 발생한다."
+            "로또 번호가 0이면 예외가 발생한다."
+            "로또 번호가 음수이면 예외가 발생한다."
+        """)
+    @ParameterizedTest
+    @MethodSource("provideFewConditionedList")
+    void lottoNumberConditionsTest(List<Integer> input, IllegalArgumentException expected) {
+        assertThatThrownBy(() -> new Lotto(input))
+                .isInstanceOf(expected.getClass())
+                .hasMessage(expected.getMessage());
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void lottoNumberDuplicateTest() {
-        assertThatThrownBy(() -> new Lotto(DUPLICATE_NUMBERS))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ValidateMessage.DUPLICATE_ERROR.getMessage());
-    }
-
-    @DisplayName("로또 번호 숫자가 45가 넘으면 예외가 발생한다.")
-    @Test
-    void lottoNumberOverFiftyFiveTest() {
-        assertThatThrownBy(() -> new Lotto(MORE_THAN_FIFTY_FIVE_NUMBERS))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ValidateMessage.NUMBER_RANGE_ERROR.getMessage());
-    }
-
-    @DisplayName("로또 번호가 0이면 예외가 발생한다.")
-    @Test
-    void lottoNumberEqualZeroTest() {
-        assertThatThrownBy(() -> new Lotto(EQUAL_ZERO_NUMBERS))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ValidateMessage.POSITIVE_NUMBER_ERROR.getMessage());
-    }
-
-    @DisplayName("로또 번호가 음수이면 예외가 발생한다.")
-    @Test
-    void lottoNumberLessThanZeroTest() {
-        assertThatThrownBy(() -> new Lotto(LESS_THAN_ZERO_NUMBERS))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ValidateMessage.POSITIVE_NUMBER_ERROR.getMessage());
+    private static Stream<Arguments> provideFewConditionedList() {
+        return Stream.of(
+                Arguments.of(MORE_THAN_SIX_SIZE_NUMBERS,
+                        new IllegalArgumentException(ValidateMessage.NUMBER_SIZE_ERROR.getMessage())),
+                Arguments.of(DUPLICATE_NUMBERS,
+                        new IllegalArgumentException(ValidateMessage.DUPLICATE_ERROR.getMessage())),
+                Arguments.of(MORE_THAN_FIFTY_FIVE_NUMBERS,
+                        new IllegalArgumentException(ValidateMessage.NUMBER_RANGE_ERROR.getMessage()))
+        );
     }
 }
+
