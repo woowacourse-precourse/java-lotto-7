54b9b33b07b66bb4b3b1dbf32ba7b8c5f5315935
mipangg <morganjeong119@gmail.com>
2024-11-02 04:18:50 +0900

    feat(InputModel): get and validate bonus number

--- src/main/java/lotto/model/InputModel.java
+++ src/main/java/lotto/model/InputModel.java
@@ -36,6 +36,47 @@ public class InputModel {
         return winningNumbers;
     }
 
+    // 보너스 번호 입력
+    public int getBonusNumber(List<Integer> winningNumbers) {
+        int bonusNumber;
+        try {
+            bonusNumber = validateBonusNumber(Console.readLine(), winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println("보너스 번호를 다시 입력해 주세요.");
+            bonusNumber = getBonusNumber(winningNumbers);
+        }
+        return bonusNumber;
+    }
+
+    // 보너스 번호의 유효성 체크
+    static int validateBonusNumber(String inputNumberText, List<Integer> winningNumbers) {
+        int bonusNumber;
+        isEmptyOrNull(inputNumberText);
+        bonusNumber = isNumber(inputNumberText);
+        isInRange(bonusNumber);
+        isDuplicate(bonusNumber, winningNumbers);
+        return bonusNumber;
+    }
+
+    static void isInRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    static void isDuplicate(int bonusNumber, List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            checkNumbers(bonusNumber, number);
+        }
+    }
+
+    static void checkNumbers(int bonusNumber, int number) {
+        if (number == bonusNumber) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
     // 당첨번호 문자열을 체크하고 List<Integer>로 반환
     static List<Integer> convertText(String text) {
         List<Integer> inputNumbers;
