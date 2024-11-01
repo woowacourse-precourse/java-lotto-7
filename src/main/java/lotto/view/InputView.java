e4cf77e61b4ab1d2ee31ec5ae178b50e015a3a10
Chan531 <chan657312@gmail.com>
2024-11-01 16:09:03 +0900

    feat(InputView): 구입금액 입력 예외 처리
    NumberFormatException 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 	public int getPurchasePriceInput() {
-		String purchasePrice = Console.readLine();
+		try {
+			String purchasePrice = Console.readLine();
+			return Integer.parseInt(purchasePrice);
+		} catch (NumberFormatException exception) {
+			throw new IllegalArgumentException("[ERROR] 구입금액은 숫자이어야 합니다.");
+		}
 	}
 }
