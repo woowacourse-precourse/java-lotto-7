83c11d6786081ceb23aea8b38a533a9df2d5362b
himitery <himitery@gmail.com>
2024-11-01 18:00:48 +0900

    fix(validation): 프록시 기반 유효성 검증 누락 수정
    - 프록시에서 유효성 검증 누락 부분 수정

--- src/main/java/lotto/config/context/ContextProxy.java
+++ src/main/java/lotto/config/context/ContextProxy.java
@@ -1,8 +1,14 @@
 package lotto.config.context;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+import lotto.config.validation.Validation;
+import lotto.config.validation.annotation.Valid;
 
 /**
  * 메서드를 실행하기 이전 특정 작업을 수행하는 프록시 클래스
@@ -25,6 +31,31 @@ public class ContextProxy implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        validation(method, args);
+
         return method.invoke(target, args);
+    }
+
+    private void validation(Method method, Object[] arguments) {
+        List<Parameter> parameters = List.of(method.getParameters());
+
+        for (int index = 0; index < parameters.size(); index++) {
+            Parameter parameter = parameters.get(index);
+            Object argument = arguments[index];
+
+            if (parameter.isAnnotationPresent(Valid.class)) {
+                validParameter(parameter, argument);
+            }
+        }
+    }
+
+    private void validParameter(Parameter parameter, Object argument) {
+        List<Annotation> annotations = Arrays.stream(parameter.getAnnotations())
+                .filter(it -> !it.annotationType().equals(Valid.class))
+                .toList();
+
+        for (final Annotation annotation : annotations) {
+            Validation.valid(annotation, argument);
+        }
     }
 }
