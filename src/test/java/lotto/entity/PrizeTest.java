5048c22eb9f4589a2fbaaaf69ad4f188ac777c84
kungbi <kipper12335@gmail.com>
2024-11-01 15:13:01 +0900

    test(Prize): 등수 찾기 테스트 추가

---
+++ src/test/java/lotto/entity/PrizeTest.java
@@ -0,0 +1,153 @@
+package lotto.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class PrizeTest {
+
+    @Test
+    void 등수_찾기_테스트_1등() {
+        // given
+        int matchCount = 6;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.FIRST, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_2등() {
+        // given
+        int matchCount = 5;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.SECOND, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_3등_메인번호() {
+        // given
+        int matchCount = 5;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.THIRD, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_3등_보너스포함() {
+        // given
+        int matchCount = 4;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.THIRD, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_4등_메인번호() {
+        // given
+        int matchCount = 4;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.FOURTH, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_4등_보너스포함() {
+        // given
+        int matchCount = 3;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.FOURTH, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_5등_메인번호() {
+        // given
+        int matchCount = 3;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.FIFTH, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_5등_보너스포함() {
+        // given
+        int matchCount = 2;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.FIFTH, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_미당첨_2개() {
+        // given
+        int matchCount = 2;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.NONE, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_미당첨_1개_보너스포함() {
+        // given
+        int matchCount = 1;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.NONE, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_미당첨_0개() {
+        // given
+        int matchCount = 0;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, false);
+
+        // then
+        assertEquals(Prize.NONE, prize);
+    }
+
+    @Test
+    void 등수_찾기_테스트_미당첨_0개_보너스포함() {
+        // given
+        int matchCount = 0;
+
+        // when
+        Prize prize = Prize.findPrize(matchCount, true);
+
+        // then
+        assertEquals(Prize.NONE, prize);
+    }
+
+}
