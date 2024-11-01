f4609437fa222b1b230181d007c569fdcabc4978
201912025 <michi2012@naver.com>
2024-11-01 13:16:37 +0900

    feat: 랜덤 정수 값 생성기 별도 구현

---
+++ src/main/java/lotto/util/RandomNumbersSelector.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomNumbersSelector {
+
+    public static List<Integer> selectRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
