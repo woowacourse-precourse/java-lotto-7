e47232cd7b862407769251edb7ec1e37f2e1a723
NamJihyun99 <namjihyun9936@gmail.com>
2024-11-01 12:52:59 +0900

    feat:> 등수enum

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public enum Rank {
+
+    FIRST(1, 2_000_000_000L),
+    SECOND(2, 30_000_000L),
+    THIRD(3, 1_500_000L),
+    FOURTH(4, 50_000L),
+    FIFTH(5, 5_000L),
+    BLANK(6, 0L);
+
+    final Integer rank;
+    final Long prize;
+
+    Rank(Integer rank, Long prize) {
+        this.rank = rank;
+        this.prize = prize;
+    }
+
+}
