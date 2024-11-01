f5e671e1b1f2be92392c8ab6a1e302267c6f7e17
Changsu Park <gd12091@gmail.com>
2024-11-01 21:56:36 +0900

    feat: Lotto 도메인 검증 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,49 @@
 package lotto.domain;
 
+import static lotto.constants.CommonConstants.LOTTO_NUMBER_COUNT;
+import static lotto.constants.CommonConstants.LOTTO_NUMBER_MAX;
+import static lotto.constants.CommonConstants.LOTTO_NUMBER_MIN;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.enums.ErrorCode;
+import lotto.exception.CommonException;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    public List<Integer> getLotto() {
+        return this.numbers;
+    }
+
+    public void validateLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new CommonException(ErrorCode.INVALID_LOTTO_NUMBER_COUNT);
+        }
+        validateNumberRange(numbers);
+        validateNoDuplicates(numbers);
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        boolean hasInvalidNumber = numbers.stream()
+                .anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX);
+
+        if (hasInvalidNumber) {
+            throw new CommonException(ErrorCode.INVALID_NUMBER_RANGE);
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    private void validateNoDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new CommonException(ErrorCode.DUPLICATE_NUMBER);
+        }
+    }
 }
