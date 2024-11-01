c7ee4461ff3f5766317e2169a8be1f4d649f8c20
FhRh <yiyop@naver.com>
2024-11-01 23:13:32 +0900

    refactor(Lotto) : validate는 하위 메서드로 구체적인 검증 메서드를 갖는다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,15 +11,25 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        validateNumberCount(numbers);
         validateDuplicate(numbers);
     }
 
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
     private void validateDuplicate(List<Integer> numbers) {
-        if (numbers.size() != numbers.stream().distinct().count()) {
+        if (numbers.size() != getDistinctSize()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
+    }
+
+    private int getDistinctSize() {
+        return (int) numbers.stream()
+            .distinct()
+            .count();
     }
 }
