593515a46861a147851e14faedf7deebfd295101
201912025 <michi2012@naver.com>
2024-11-01 16:21:55 +0900

    feat: 로또 번호 중복 검증 메서드 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import lotto.utils.SortUtils;
 import lotto.validator.LottoValidator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoGenerator {
     private final LottoValidator lottoValidator;
@@ -14,6 +16,19 @@ public class LottoGenerator {
 
     public List<Integer> generateLottoNumbers(List<Integer> randomNumbers) {
         lottoValidator.validateLottoRange(randomNumbers);
+        validateDuplicationLottoNumbers(randomNumbers);
+
         return SortUtils.sortNumbers(randomNumbers);
     }
+
+    private void validateDuplicationLottoNumbers(List<Integer> randomNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : randomNumbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다. 중복된 번호: " + number);
+            }
+        }
+    }
+
 }
