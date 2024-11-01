afa692499efe58020042038e33611b765036f671
yereumi <lovelyyeji01@gmail.com>
2024-11-01 18:06:34 +0900

    refactor(OutputView): 메서드명 숫자 영어로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -15,23 +15,23 @@ public class OutputView {
         System.out.println(OutputMessage.SECTION);
     }
 
-    public void print3Matches(int count) {
+    public void printThreeMatches(int count) {
         System.out.println(OutputMessage.THREE_MATCHES.getMessage(count));
     }
 
-    public void print4Matches(int count) {
+    public void printFourMatches(int count) {
         System.out.println(OutputMessage.FOUR_MATCHES.getMessage(count));
     }
 
-    public void print5Matches(int count) {
+    public void printFiveMatches(int count) {
         System.out.println(OutputMessage.FIVE_MATCHES.getMessage(count));
     }
 
-    public void print5BonusMatches(int count) {
+    public void printFiveBonusMatches(int count) {
         System.out.println(OutputMessage.FIVE_BONUS_MATCHES.getMessage(count));
     }
 
-    public void print6Matches(int count) {
+    public void printSixMatches(int count) {
         System.out.println(OutputMessage.SIX_MATCHES.getMessage(count));
     }
 }
