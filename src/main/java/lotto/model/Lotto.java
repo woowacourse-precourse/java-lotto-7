85e8104c91b7161608d5a7293d28fb5f4874cc37
woogym <kwj9294@gmail.com>
2024-11-02 06:02:48 +0900

    feat(model): lotto 모델 검증 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,14 @@
 package lotto.model;
 
+import static lotto.config.LottoInfo.MAXIMUM_LOTTO_NUMBER;
+import static lotto.config.LottoInfo.MINIMUM_LOTTO_NUMBER;
+
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.exception.LottoException.DuplicateLottoNumberException;
+import lotto.exception.LottoException.LottoNumberOutOfRangeException;
 import lotto.util.generator.LottoNumberGenerator;
 import lotto.vo.BonusNumber;
 
@@ -26,10 +33,6 @@ public class Lotto {
         return Collections.unmodifiableList(randomNumbers);
     }
 
-    private void validateLottoNumber(List<Integer> numbers) {
-
-    }
-
     public int countMatchedNumbers(final Lotto lotto) {
         return (int) numbers.stream()
                 .filter(lotto::contain)
@@ -46,5 +49,38 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    private void validateLottoNumber(List<Integer> numbers) {
+        hasDuplicateNumber(numbers);
+        hasOverThenMaxNumber(numbers);
+        hasLessThenMinNumber(numbers);
+    }
+
+    private void hasDuplicateNumber(final List<Integer> numbers) {
+        Set<Integer> numberGroup = new HashSet<>();
+        for (Integer number : numberGroup) {
+            if (!numberGroup.add(number)) {
+                throw new DuplicateLottoNumberException();
+            }
+        }
+    }
+
+    private void hasOverThenMaxNumber(final List<Integer> numbers) {
+        boolean hasOverMaxNumber = numbers.stream()
+                .anyMatch(number -> (number > MAXIMUM_LOTTO_NUMBER.getValue()));
+
+        if (hasOverMaxNumber) {
+            throw new LottoNumberOutOfRangeException();
+        }
+    }
+
+    private void hasLessThenMinNumber(final List<Integer> numbers) {
+        boolean hasLessMinNumber = numbers.stream()
+                .anyMatch(number -> (number < MINIMUM_LOTTO_NUMBER.getValue()));
+
+        if (hasLessMinNumber) {
+            throw new LottoNumberOutOfRangeException();
+        }
     }
 }
