7090f009c13c2c599e326c631f243d042c3e74f2
Changsu Park <gd12091@gmail.com>
2024-11-01 21:57:30 +0900

    refactor: 기존 검증 로직들 도메인으로 이동

--- src/main/java/lotto/service/ValidatorService.java
+++ src/main/java/lotto/service/ValidatorService.java
@@ -1,55 +1,15 @@
 package lotto.service;
 
-import static lotto.constants.CommonConstants.LOTTO_NUMBER_COUNT;
-import static lotto.constants.CommonConstants.LOTTO_NUMBER_MAX;
-import static lotto.constants.CommonConstants.LOTTO_NUMBER_MIN;
 import static lotto.constants.CommonConstants.LOTTO_PRICE;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lotto.enums.ErrorCode;
 import lotto.exception.CommonException;
 
 public class ValidatorService {
 
     public void validatePurchaseAmount(int amount) {
-        if(amount <= 0 || amount % LOTTO_PRICE != 0) {
+        if (amount <= 0 || amount % LOTTO_PRICE != 0) {
             throw new CommonException(ErrorCode.INVALID_PURCHASE_AMOUNT);
-        }
-    }
-
-    public void validateLottoNumbers(List<Integer> numbers){
-        if(numbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new CommonException(ErrorCode.INVALID_LOTTO_NUMBER_COUNT);
-        }
-        validateNumberRange(numbers);
-        validateNoDuplicates(numbers);
-    }
-
-    public void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
-        if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
-            throw new CommonException(ErrorCode.INVALID_NUMBER_RANGE);
-        }
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new CommonException(ErrorCode.DUPLICATE_BONUS_NUMBER);
-        }
-    }
-
-    private void validateNumberRange(List<Integer> numbers) {
-        boolean hasInvalidNumber = numbers.stream()
-                .anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX);
-
-        if (hasInvalidNumber) {
-            throw new CommonException(ErrorCode.INVALID_NUMBER_RANGE);
-        }
-    }
-
-
-    private void validateNoDuplicates(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size()) {
-            throw new CommonException(ErrorCode.DUPLICATE_NUMBER);
         }
     }
 }
