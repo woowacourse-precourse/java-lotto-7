5f9dc9b1708af862f44c217d3d5b7c78993abdeb
StoneCAU <seokwoo1999@naver.com>
2024-11-01 21:26:07 +0900

    feat(OutputView): 통계와 수익률을 추출하는 메서드 구현
    - printWinningSummary
    - printReturnRate

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import static java.lang.String.format;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.Revenue;
 import lotto.exception.LottoException;
 
 public class OutputView {
@@ -11,6 +12,12 @@ public class OutputView {
     private final static String INFORM_PURCHASE_LOTTO_MESSAGE = "%d개를 구매했습니다.";
     private final static String WINNING_STATISTICS_TITLE = "당첨 통계";
     private final static String SEPARATOR = "---";
+    private final static String RETURN_RATE_STAT = "총 수익률은 %.1f%%입니다.";
+
+
+    public static void printNewLine() {
+        System.out.print(NEW_LINE);
+    }
 
     public static void printPurchaseMessage(int lottoQuantity) {
         System.out.println(format(INFORM_PURCHASE_LOTTO_MESSAGE, lottoQuantity));
@@ -27,7 +34,16 @@ public class OutputView {
         printNewLine();
     }
 
-    public static void printNewLine() {
-        System.out.print(NEW_LINE);
+    public static void printWinningStatsTitle() {
+        System.out.println(WINNING_STATISTICS_TITLE);
+        System.out.println(SEPARATOR);
+    }
+
+    public static void printWinningSummary(List<String> winningSummary) {
+        winningSummary.forEach(System.out::println);
+    }
+
+    public static void printReturnRate(Revenue revenue) {
+        System.out.println(format(RETURN_RATE_STAT, revenue.getReturnRate()));
     }
 }
