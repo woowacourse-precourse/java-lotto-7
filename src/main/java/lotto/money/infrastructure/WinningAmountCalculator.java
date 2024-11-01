82dd9d15d8ca228f8923175c5c82a25c58594ecd
EHCTH <lees90896@gmail.com>
2024-11-01 19:30:41 +0900

    refactor(WinningAmountCalculator): benefit 생성 메서드 분리

--- src/main/java/lotto/money/infrastructure/WinningAmountCalculator.java
+++ src/main/java/lotto/money/infrastructure/WinningAmountCalculator.java
@@ -1,16 +1,13 @@
-package lotto.lotto.winning.domain;
+package lotto.money.infrastructure;
 
-import lotto.money.domain.Benefit;
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.winning.domain.WinningLotto;
+import lotto.lotto.winning.domain.WinningPlace;
 import lotto.money.domain.Money;
 import lotto.lotto.domain.LottoTickets;
-import lotto.money.infrastructure.WinningAmount;
 
 public class WinningAmountCalculator {
-    public Benefit updateBenefit(LottoTickets lottoTickets, WinningLotto winningLotto, BonusNumber bonusNumber) {
-        Money winningAmount = winningCalculate(lottoTickets, winningLotto, bonusNumber);
-        return new Benefit(winningAmount);
-    }
-    private Money winningCalculate(LottoTickets lottoTickets, WinningLotto winningLotto, BonusNumber bonusNumber) {
+    public Money calculate(LottoTickets lottoTickets, WinningLotto winningLotto, BonusNumber bonusNumber) {
         return new WinningAmount(lottoTickets.info().stream()
                 .map((lotto) -> WinningPlace.calculate(winningLotto, bonusNumber, lotto))
                 .mapToLong(Money::getMoney)
