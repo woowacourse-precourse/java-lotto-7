ee3557af847e85ea669aab9a44805c003fff8084
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 20:22:07 +0900

    refactor(View) : printResults StateMessage에 따라 보완

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -13,13 +13,7 @@ public class View {
     public static final String LOTTO_COUNT_PHRASE = "개를 구매했습니다.";
     public static final String LOTTO_WINNING_INPUT = "당첨 번호를 입력해 주세요.";
     public static final String LOTTO_BONUS_INPUT = "보너스 번호를 입력해 주세요.";
-    public static final String WIN_STATICS = "당첨 통계\n---";
-    public static final String COMMA = ",";
-    public static final String THREE = "3개 일치 (5,000원) - ";
-    public static final String FOUR = "4개 일치 (50,000원) - ";
-    public static final String FIVE = "5개 일치 (1,500,000원) - ";
-    public static final String FIVE_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    public static final String SIX = "6개 일치 (2,000,000,000원) - ";
+    public static final String DELIMITER = ",";
     public static final String NUM = "개";
 
     private Integer lottoCount;
@@ -59,7 +53,7 @@ public class View {
         System.out.println();
         Lotto answer;
         try {
-            answer = new Lotto(Arrays.asList(rawWinningInput.split(COMMA)).stream()
+            answer = new Lotto(Arrays.asList(rawWinningInput.split(DELIMITER)).stream()
                     .map(Application::parseInt)
                     .collect(Collectors.toList()));
         }
@@ -88,12 +82,12 @@ public class View {
     }
 
     public void printResults(WinningDetails grades){
-        System.out.println(WIN_STATICS);
-        System.out.println(THREE + grades.getThird() + NUM);
-        System.out.println(FOUR + grades.getFourth() + NUM);
-        System.out.println(FIVE + grades.getFifth() + NUM);
-        System.out.println(FIVE_BONUS +grades.getFifthBonus() + NUM);
-        System.out.println(SIX + grades.getSixth() + NUM);
+        System.out.println(StateMessage.WIN_STATICS.getMessage());
+        System.out.println(StateMessage.THREE.getMessage() + grades.getThird() + NUM);
+        System.out.println(StateMessage.FOUR.getMessage() + grades.getFourth() + NUM);
+        System.out.println(StateMessage.FIVE.getMessage() + grades.getFifth() + NUM);
+        System.out.println(StateMessage.FIVE_BONUS.getMessage() +grades.getFifthBonus() + NUM);
+        System.out.println(StateMessage.SIX.getMessage() + grades.getSixth() + NUM);
     }
 
     public Integer getLottoCount(){
