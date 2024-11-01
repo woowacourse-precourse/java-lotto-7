bf07a6df99db376b4d8e7ab952ac70c9f5863fa0
sangjun <sangjoon12345@gmail.com>
2024-11-01 09:46:46 +0900

    refactor: generateLottoNumbers() 리턴 타입 변경

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -1,15 +1,9 @@
 package lotto.model;
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
-// 로또 서비스 구현
-// 로또 번호 추출
 public class LottoManager {
-
-    public List<Integer>generateLottoNumbers(){
-        return pickUniqueNumbersInRange(1,45,6);
+    public Lotto generateLottoNumbers(){
+        return new Lotto(pickUniqueNumbersInRange(1,45,6));
     }
-
 
 }
