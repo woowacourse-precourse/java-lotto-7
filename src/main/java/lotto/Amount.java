d75b18b2b9f54253a177dc5391caa1a83a9ec22a
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-01 19:43:09 +0900

    feat(lotto/validate): 로또 구입 금액 타입 예외 기능 추가

--- src/main/java/lotto/Amount.java
+++ src/main/java/lotto/Amount.java
@@ -8,9 +8,21 @@ public class Amount {
         this.number = number;
     }
 
+    public static Amount of(String number) {
+        return new Amount(validateType(number));
+    }
+
     private void validateUnit(int number) {
         if (number % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구매는 1000원 단위로 가능합니다.");
+        }
+    }
+
+    private static int validateType(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액을 숫자로 입력해주세요.");
         }
     }
 }
