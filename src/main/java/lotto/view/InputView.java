cd0f90b259ad84bbbaf0783c954cf9e61a78efb4
corpi <lj845854@gmail.com>
2024-11-01 11:47:02 +0900

    refactor(ViewConstant): 입력 안내 문구 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,22 +1,26 @@
 package lotto.view;
 
+import static lotto.view.ViewConstant.INPUT_BONUS_NUMBER_PROMPT;
+import static lotto.view.ViewConstant.INPUT_MONEY_PROMPT;
+import static lotto.view.ViewConstant.INPUT_WINNING_NUMBER_PROMPT;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(INPUT_MONEY_PROMPT);
 
         return Console.readLine();
     }
 
     public String inputWinningNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBER_PROMPT);
 
         return Console.readLine();
     }
 
     public String inputWinningBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER_PROMPT);
 
         return Console.readLine();
     }
---
+++ src/main/java/lotto/view/ViewConstant.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class ViewConstant {
+    public static final String INPUT_MONEY_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+}
