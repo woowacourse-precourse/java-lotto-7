ecb0c5623dc2219c75a44362777167f292f80c56
hyosang <billy04100410@gmail.com>
2024-11-01 15:36:12 +0900

    refactor(test): InputvalidatorTest를 LottoControllerTest에서 처리

---
+++ src/test/java/lotto/LottoControllerTest.java
@@ -0,0 +1,63 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.controller.LottoController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("Controller 클래스 테스트")
+public class LottoControllerTest {
+
+    private LottoController lottoController;
+
+    @BeforeEach
+    void setUp() {
+        lottoController = new LottoController();
+    }
+
+    @Test
+    void 구입금액이_1000원_단위가_아닐_때_예외가_발생한다() {
+        int purchaseAmount = 10500;
+        assertThatThrownBy(() -> lottoController.validatePurchaseAmount(purchaseAmount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("구입 금액은 1,000원 단위로만 가능합니다.");
+    }
+
+    @Test
+    void 연속된_쉼표가_입력된_경우_예외가_발생한다() {
+        String inputWinningNumber = "1,,2,3,4,5,6";
+        assertThatThrownBy(() -> lottoController.validateInputWinnigNumber(inputWinningNumber))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력에 쉼표가 연속적으로 입력되었습니다.");
+    }
+
+    @Test
+    void 입력_당첨_번호를_콤마로_분리한다() {
+        List<String> expectedList = Arrays.asList("1", "2", "3", "4", "5", "6");
+        String input = "1,2,3,4,5,6";
+        List<String> winningNumbers = lottoController.splitByComma(input);
+        assertEquals(expectedList, winningNumbers);
+    }
+
+    @Test
+    void 문자열_리스트를_Integer_리스트로_변환한다() {
+        List<Integer> expectedList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> stringList = Arrays.asList("1", "2", "3", "4", "5", "6");
+        List<Integer> integerList = lottoController.stringListToIntList(stringList);
+        assertEquals(expectedList, integerList);
+    }
+
+    @Test
+    void 잘못된_입력_형식으로_예외가_발생한다() {
+        String input = "1@2";
+        assertThatThrownBy(() -> lottoController.parseInt(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력 형식이 올바르지 않습니다.");
+    }
+}
