7c04fc897cdb7f55b131b6bd416091cc4741507b
201912025 <michi2012@naver.com>
2024-11-01 13:25:12 +0900

    feat: 숫자 정렬 기능 구현

---
+++ src/main/java/lotto/utils/SortUtils.java
@@ -0,0 +1,12 @@
+package lotto.utils;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortUtils {
+
+    public static List<Integer> sortNumbers(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
+}
