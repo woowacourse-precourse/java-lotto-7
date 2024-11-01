a0c71d4667ab803308851ee716c1b77f7a40fbb2
김호연 <yun1094@naver.com>
2024-11-01 09:35:50 +0900

    잘못된 접근 제어자 수정

--- src/main/java/Util/Validator.java
+++ src/main/java/Util/Validator.java
@@ -3,13 +3,13 @@ package Util;
 import Constant.Constant;
 
 public class Validator {
-    private static void validateThousandUnit(Integer purchaseAmount) {
+    public static void validateThousandUnit(Integer purchaseAmount) {
         if (purchaseAmount % 1000 != 0) {
             throw new IllegalArgumentException(Constant.ErrorConstant.PURCHASE_THOUSAND_ERROR);
         }
     }
 
-    private static void validateIsNumber(Integer purchaseAmount) {
+    public static void validateIsNumber(Integer purchaseAmount) {
         try {
             Integer.parseInt(String.valueOf(purchaseAmount));
         } catch (NumberFormatException e) {
