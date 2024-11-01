a75116746904fd3d253ab07cf2c1d2abe9e84afb
Changsu Park <gd12091@gmail.com>
2024-11-01 21:56:16 +0900

    refactor: IDEA 자동 포맷팅

--- src/main/java/lotto/exception/CommonException.java
+++ src/main/java/lotto/exception/CommonException.java
@@ -3,6 +3,7 @@ package lotto.exception;
 import lotto.enums.ErrorCode;
 
 public class CommonException extends IllegalArgumentException {
+
     public CommonException(ErrorCode errorCode) {
         super("[ERROR] " + errorCode.getMessage());
     }
