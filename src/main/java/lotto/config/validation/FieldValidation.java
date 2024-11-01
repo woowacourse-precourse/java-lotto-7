62f142508660e6cbad2813de2ee30d2de4b7f616
himitery <himitery@gmail.com>
2024-11-01 16:43:00 +0900

    fix(validation): 필드 기반 유효성 검사 오류 수정
    - 상속 받는 경우 부모 클래스의 필드를 검사하지 못하는 오류 수정
    - ValidationException은 그대로 throw 하도록 수정

--- src/main/java/lotto/config/validation/FieldValidation.java
+++ src/main/java/lotto/config/validation/FieldValidation.java
@@ -3,10 +3,11 @@ package lotto.config.validation;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.config.validation.exception.NotFoundClassFieldException;
+import lotto.config.validation.exception.ValidationException;
 
 /**
  * 어노테이션 기반으로 필드의 데이터 유효성 검사를 진행하는 클래스
@@ -19,33 +20,40 @@ public abstract class FieldValidation {
         cls = this.getClass();
     }
 
-    public void valid() {
-        List<Field> fields = extractFields();
+    protected void valid() {
+        List<Field> fields = extractFields(cls);
 
         for (final Field field : fields) {
             isValid(field);
         }
     }
 
-    private List<Field> extractFields() {
-        return Arrays.stream(cls.getDeclaredFields())
+    private List<Field> extractFields(Class<?> target) {
+        List<Field> fields = List.of(target.getDeclaredFields());
+        if (target.getSuperclass() != null) {
+            Stream<Field> classFields = fields.stream();
+            Stream<Field> superClassFields = extractFields(target.getSuperclass()).stream();
+
+            fields = Stream.concat(classFields, superClassFields)
+                    .toList();
+        }
+
+        return fields.stream()
                 .peek(it -> it.setAccessible(true))
                 .collect(Collectors.toList());
     }
 
     private void isValid(Field field) {
-        List<Annotation> annotations = extractAnnotations(field);
+        List<Annotation> annotations = List.of(field.getAnnotations());
 
         for (Annotation annotation : annotations) {
             try {
                 Validation.valid(annotation, field.get(this));
+            } catch (ValidationException e) {
+                throw e;
             } catch (IllegalArgumentException | IllegalAccessException e) {
                 throw new NotFoundClassFieldException();
             }
         }
-    }
-
-    private List<Annotation> extractAnnotations(Field field) {
-        return List.of(field.getAnnotations());
     }
 }
