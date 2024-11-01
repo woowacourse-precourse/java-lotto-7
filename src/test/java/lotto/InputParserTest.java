43389ef102d8244bd0fe1aa74923b4929a8d4669
kjyyjk <rlawnsdud920@gmail.com>
2024-11-01 21:49:49 +0900

    test: InputParser의 단위 테스트를 수행한다
    - parseWinningNumber() 테스트 시 containsExactly() 대신 contains()를
    사용한다.containsExactly()는 순서까지 정확하게 일치되어야 하지만,
    parseWinningNumber()는 순서를 보장할 책임은 가지고 있지 않기 때문이다.

---
+++ src/test/java/lotto/InputParserTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputParserTest {
+
+    @DisplayName("InputParser는_문자열을_정수로_변환할_수_있다")
+    @Test
+    public void parseInt() {
+        //given
+        String input = "23";
+
+        //when
+        int result = InputParser.parseInt(input);
+
+        //then
+        assertThat(result).isEqualTo(23);
+    }
+
+    @DisplayName("InputParser는_당첨번호_문자열을_분리하여_정수_리스트로_변환할_수_있다")
+    @Test
+    public void parseWinningNumber() {
+        //given
+        String input = "1,2,3,4,5";
+
+        //when
+        List<Integer> result = InputParser.parseWinningNumber(input);
+
+        //then
+        assertThat(result).contains(1, 2, 3, 4, 5);
+    }
+}
