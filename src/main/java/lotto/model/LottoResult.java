10d79f6da31fcb16fcb2b0dc99df108992e5c1c0
kgy1008 <gayeon1008@g.skku.edu>
2024-11-01 23:29:22 +0900

    feat: compare winning lotto and buying lottos

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<Rank, Integer> result = new EnumMap<>(Rank.class);
+
+    public LottoResult() {
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+    }
+
+    public void addResult(final Rank rank) {
+        result.put(rank, result.get(rank) + 1);
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return Collections.unmodifiableMap(result);
+    }
+}
