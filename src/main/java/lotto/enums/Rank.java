76f7bdeaa476899d05f7698db5e62d2ba2b18c4f
Changsu Park <gd12091@gmail.com>
2024-11-01 23:16:55 +0900

    style: 순위 순서 변경

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -3,12 +3,12 @@ package lotto.enums;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
+    NONE(0, false, 0),
     FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
 
     Rank(int correctCount, boolean bonusCheck, int amount) {
         this.correctCount = correctCount;
@@ -36,7 +36,7 @@ public enum Rank {
         return Arrays.stream(Rank.values())
                 .filter(rank -> rank != NONE)
                 .filter(rank -> rank.correctCount == matchCount)
-                .filter(rank -> !rank.bonusCheck || rank.bonusCheck == matchBonus)
+                .filter(rank -> !rank.bonusCheck || (rank.bonusCheck && matchBonus))
                 .findFirst()
                 .orElse(NONE);
     }
