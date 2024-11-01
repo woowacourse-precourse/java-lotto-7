001bac096e02eafa8eb24d3e203561b2b666fcbd
dbdb456 <adad568@naver.com>
2024-11-01 15:16:51 +0900

    feat(validator): 숫자가 아닌 문자 예외 메서드 추가

--- src/main/java/lotto/validator/Validators.java
+++ src/main/java/lotto/validator/Validators.java
@@ -1,10 +1,20 @@
 package lotto.validator;
 
+import java.util.InputMismatchException;
+
 public class Validators {
 
     public void validatePurchaseAmountUnit(int price) {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.");
+        }
+    }
+
+    public void validateNumericInput(String price) {
+        try {
+            Integer.parseInt(price);
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
         }
     }
 }
