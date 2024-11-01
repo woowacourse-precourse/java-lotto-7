b500e914a2cca7bad75d81a94090ac53b1efdcd1
Yooooonseo <81520018+Yooooonseo@users.noreply.github.com>
2024-11-01 14:54:27 +0900

    refactor(model): Rank 리팩토링
    - enum 타입 수정
    - displayName 통해 출력되도록 수정

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,21 +1,23 @@
 package lotto.model;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FIRST(6, false, 2_000_000_000, "6개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    FIFTH(3, false, 5_000, "3개 일치"),
+    NONE(0, false, 0, "0개 일치");
 
     private final int matchCount;
     private final boolean matchBonus;
     private final int prizeMoney;
+    private final String displayName;
 
-    Rank(int matchCount, boolean matchBonus, int prizeMoney) {
+    Rank(int matchCount, boolean matchBonus, int prizeMoney, String displayName) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prizeMoney = prizeMoney;
+        this.displayName = displayName;
     }
 
     public int getMatchCount() {
@@ -28,6 +30,20 @@ public enum Rank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public static Rank of(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) return FIRST;
+        if (matchCount == 5 && bonusMatch) return SECOND;
+        if (matchCount == 5) return THIRD;
+        if (matchCount == 4) return FOURTH;
+        if (matchCount == 3) return FIFTH;
+        return NONE;
+        //return null;
     }
 }
 
