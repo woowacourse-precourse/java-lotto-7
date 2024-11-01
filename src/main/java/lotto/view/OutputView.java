332121e2007b313599edddf9a9c4c42be8e3944d
dnjswns98 <tngkrt2004@gmail.com>
2024-11-01 17:44:50 +0900

    feat(OutputView): Request to input the winning number

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,12 +3,19 @@ package lotto.view;
 public class OutputView {
     private static final String QUESTION_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String PRINT_LOTTO_AMOUNT = "개를 구매했습니다.";
+    private static final String QUESTION_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public void printInputMoney() {
         System.out.println(QUESTION_LOTTO_AMOUNT);
     }
 
     public void printLottoAmount(int count) {
-        System.out.println("\n" + count + PRINT_LOTTO_AMOUNT);
+        System.out.println();
+        System.out.println(count + PRINT_LOTTO_AMOUNT);
+    }
+
+    public void printWinningNumbers() {
+        System.out.println();
+        System.out.println(QUESTION_WINNING_NUMBERS);
     }
 }
