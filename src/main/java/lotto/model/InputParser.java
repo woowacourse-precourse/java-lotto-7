dfaada6d9f11e03ceee8614a65d94cfd4548a8ef
steamedEggMaster <cksdud5014@naver.com>
2024-11-01 19:31:43 +0900

    fix(InputParser): 구입 금액을 1000으로 나누는 작업 추가
    1. 구입 금액을 1000으로 나누지 않은 채 진행되는 것을 확인하여 수정

--- src/main/java/lotto/model/InputParser.java
+++ src/main/java/lotto/model/InputParser.java
@@ -9,9 +9,13 @@ public class InputParser {
             int purchaseAmount = Integer.parseInt(input);
             InputValidator.validateNonNegativeAmount(purchaseAmount);
             InputValidator.validateAmountUnit(purchaseAmount);
-            return purchaseAmount;
+            return divideByThousand(purchaseAmount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_FORMAT_ERROR);
         }
+    }
+
+    private int divideByThousand(int purchaseAmount) {
+        return purchaseAmount / 1000;
     }
 }
