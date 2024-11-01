fc944d010cffc318f6f7fc057a851f8c49e0d288
donghyunn1 <dongdong0516@naver.com>
2024-11-02 03:17:49 +0900

    fix: 로또 중복넘버 생성 로직 수정

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,14 +1,15 @@
 package lotto.domain.lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        validateDuplicate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
@@ -20,10 +21,9 @@ public class Lotto {
     }
 
     private void validateDuplicate(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (numbers.contains(number)) {
-                throw new IllegalArgumentException("[ERROR] 숫자는 중복될 수 없습니다.");
-            }
+        Set<Integer> lottoNumbers = new HashSet<>(numbers);
+        if (lottoNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 숫자는 중복될 수 없습니다.");
         }
     }
 
