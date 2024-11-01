293b6e67d8c9df0d70e4027ef27d6fd12fc99b7d
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-01 21:55:07 +0900

    feat(LottoConstant): 로또 관련 상수 정의

---
+++ src/main/java/lotto/global/constant/LottoConstant.java
@@ -0,0 +1,9 @@
+package lotto.global.constant;
+
+public class LottoConstant {
+
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_PRICE = 1_000;
+}
