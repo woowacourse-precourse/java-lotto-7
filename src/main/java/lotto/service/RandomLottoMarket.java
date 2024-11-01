3bd27b70a74bed23df95f2401b3e7c11118b8d7e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-02 01:03:14 +0900

    refactor : 랜덤 로또 생성 티켓 get해와서 생성

--- src/main/java/lotto/service/RandomLottoMarket.java
+++ src/main/java/lotto/service/RandomLottoMarket.java
@@ -3,32 +3,34 @@ package lotto.service;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Wallet;
+import lotto.domain.calculators.TicketCalculator;
 import lotto.domain.lottos.Lotto;
 import lotto.domain.lottos.RandomLottos;
 import lotto.domain.number.NumbersMaker;
 
 public class RandomLottoMarket {
+    private final TicketCalculator ticketCalculator;
     private final NumbersMaker numbersMaker;
     private final Wallet wallet;
 
-    public RandomLottoMarket(NumbersMaker randomNumberMaker, Wallet wallet) {
+    public RandomLottoMarket(TicketCalculator ticketCalculator, NumbersMaker randomNumberMaker, Wallet wallet) {
+        this.ticketCalculator = ticketCalculator;
         this.numbersMaker = randomNumberMaker;
         this.wallet = wallet;
     }
 
     public RandomLottos createRandomLottos() {
-        wallet.buyTicket();
-        List<Lotto> lottos = makeLottos();
-        
-        return new RandomLottos(lottos);
+        buyTicket();
+        int ticket = wallet.getTicket();
+
+        return new RandomLottos(makeLottos(ticket));
     }
 
-    public List<Lotto> makeLottos() {
+    public List<Lotto> makeLottos(int ticket) {
         List<Lotto> resultRandomLottos = new ArrayList<>();
 
-        while (!wallet.isRunOutTicket()) {
+        for (int i = 0; i < ticket; i++) {
             resultRandomLottos.add(makeLotto());
-            wallet.decreaseTicket();
         }
         return resultRandomLottos;
     }
@@ -36,6 +38,10 @@ public class RandomLottoMarket {
     private Lotto makeLotto() {
         List<Integer> randomNumbers = numbersMaker.make();
         return new Lotto(randomNumbers);
+    }
+
+    private void buyTicket() {
+        wallet.calculateNumberOfTicket(ticketCalculator);
     }
 
 }
