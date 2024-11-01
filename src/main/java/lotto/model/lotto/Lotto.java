1121979a8209c286f207593cb0ca940470209d19
kgy1008 <gayeon1008@g.skku.edu>
2024-11-01 16:26:21 +0900

    feat: validate winning lotto number

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private static final int LOTTO_COUNT = 6;
     private final List<Integer> numbers;
 
@@ -15,6 +17,7 @@ public class Lotto {
 
     private void validate(final List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
         validateDuplicated(numbers);
     }
 
@@ -28,6 +31,14 @@ public class Lotto {
         Set<Integer> lottoNumbers = new HashSet<>(numbers);
         if (lottoNumbers.size() != LOTTO_COUNT) {
             throw new IllegalArgumentException("[ERROR] 중복된 로또 번호가 존재합니다.");
+        }
+    }
+
+    private void validateRange(final List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
