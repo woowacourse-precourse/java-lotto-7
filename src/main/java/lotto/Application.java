3d36452f852ebcf405663d75d7761e6abed1880e
ddddabi <ii887742@gmail.com>
2024-11-01 23:34:49 +0900

    feat: Controller 구현
    - 1번과 2번 기능 controller 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,6 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController controller = new LottoController();
-        controller.start();
+        controller.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,19 @@
 package lotto.controller;
+import lotto.model.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
-    public void start() {
+    public void run() {
+        int purchaseAmount = InputView.inputPurchaseAmount(); // 로또 구입 금액 입력
+        int lottoCount = purchaseAmount / 1000; // 구매할 로또 수량 계산
+        List<Lotto> lottos = Lotto.generateLottos(lottoCount); // 로또 번호 생성
+
+        OutputView.printLottos(lottoCount, lottos); // 로또 번호 출력
+
+        List<Integer> winningNumbers = InputView.inputWinningNumbers(); // 당첨 번호 입력
+        int bonusNumber = InputView.inputBonusNumber(winningNumbers); // 보너스 번호 입력
     }
 }
