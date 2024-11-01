5c6b131dfadec99eae41d31bcc96f04f45fc607c
joshinhyeok <shtree07@naver.com>
2024-11-01 20:54:35 +0900

    feat: 당첨 등수를 확인하는 테스트 검증 케이스 추가

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -8,5 +8,12 @@ public class LottoRankTest {
     @Test
     void 로또_번호와_당첨_번호와_보너스_번호를_종합해_등수를_확인합니다() {
         assertEquals(LottoRank.FIRST_PRIZE, LottoRank.findRank(6, false));
+        assertEquals(LottoRank.SECOND_PRIZE, LottoRank.findRank(5, true));
+        assertEquals(LottoRank.THIRD_PRIZE, LottoRank.findRank(5, false));
+        assertEquals(LottoRank.FOURTH_PRIZE, LottoRank.findRank(4, true));
+        assertEquals(LottoRank.FOURTH_PRIZE, LottoRank.findRank(4, false));
+        assertEquals(LottoRank.FIFTH_PRIZE, LottoRank.findRank(3, false));
+        assertEquals(LottoRank.NO_PRIZE, LottoRank.findRank(2, false));
+        assertEquals(LottoRank.NO_PRIZE, LottoRank.findRank(1, false));
     }
 }
