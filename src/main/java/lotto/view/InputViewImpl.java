a195755c5e0a925faeb471a5c2e713e1d82f742a
Sinorang <sinholang@naver.com>
2024-11-01 20:04:13 +0900

    feat: 구입 금액, 당첨 번호 입력 메서드 구현

---
+++ src/main/java/lotto/view/InputViewImpl.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputViewImpl implements InputView {
+    @Override
+    public String getPurchaseAmountInput() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    @Override
+    public String getWinningNumbersInput() {
+        System.out.println("당첨 번호를 입력해 주세요. (쉼표로 구분)");
+        return Console.readLine();
+    }
+
+    @Override
+    public String getBonusNumberInput() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+}
