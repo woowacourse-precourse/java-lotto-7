1d50fd325bf46b9ef38d0e9d13bb6a26f5105f4b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-01 16:15:09 +0900

    feat(model): Lotto의 목록을 관리하는 Lottos 모델 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,2 +1,11 @@
-package lotto.model;public class Lottos {
+package lotto.model;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
 }
