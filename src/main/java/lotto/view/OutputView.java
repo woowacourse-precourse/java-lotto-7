9d9e421381fb3e09d8b71078c743e206d9647bae
soeun2537 <soeun2537@gmail.com>
2024-11-02 04:00:19 +0900

    feat: 당첨 통계 출력 로직 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
+import static lotto.constant.category.Rank.*;
 import static lotto.constant.message.OutputMessage.*;
 
 import java.util.List;
+import lotto.model.RankCounter;
 
 public class OutputView {
 
@@ -14,5 +16,19 @@ public class OutputView {
         for (String lottoTicket : lottoTickets) {
             System.out.println(lottoTicket);
         }
+    }
+
+    public static void printWinningStatistics(RankCounter rankCounter) {
+        System.out.println(WINNING_STATISTICS_OUTPUT_START_MESSAGE.getMessage());
+        System.out.println(FIFTH_PLACE_OUTPUT_MESSAGE.getMessage(
+                FIFTH_PLACE.getMatchCount(), FIFTH_PLACE.getPrizeAmount(), rankCounter.getRankCount(FIFTH_PLACE)));
+        System.out.println(FOURTH_PLACE_OUTPUT_MESSAGE.getMessage(
+                FOURTH_PLACE.getMatchCount(), FOURTH_PLACE.getPrizeAmount(), rankCounter.getRankCount(FOURTH_PLACE)));
+        System.out.println(THIRD_PLACE_OUTPUT_MESSAGE.getMessage(
+                THIRD_PLACE.getMatchCount(), THIRD_PLACE.getPrizeAmount(), rankCounter.getRankCount(THIRD_PLACE)));
+        System.out.println(SECOND_PLACE_OUTPUT_MESSAGE.getMessage(
+                SECOND_PLACE.getMatchCount(), SECOND_PLACE.getPrizeAmount(), rankCounter.getRankCount(SECOND_PLACE)));
+        System.out.println(FIRST_PLACE_OUTPUT_MESSAGE.getMessage(
+                FIRST_PLACE.getMatchCount(), FIRST_PLACE.getPrizeAmount(), rankCounter.getRankCount(FIRST_PLACE)));
     }
 }
