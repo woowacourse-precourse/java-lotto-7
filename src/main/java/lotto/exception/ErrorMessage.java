ab80859da8c220a1fb17ad9dba69ddc224c5e9b7
wogus <ind07152@naver.com>
2024-11-01 13:59:11 +0900

    feat(ErrorMessage): 보너스 숫자 관련 에러 메시지 추가
    보너스 숫자 범위 관련 에러 메시지를 추가한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,10 +3,11 @@ package lotto.exception;
 public enum ErrorMessage {
     INVALID_LOTTO_COUNTS("로또 번호는 6개여야 합니다."),
     INVALID_LOTTO_DUPLICATE("로또 번호는 중복될 수 없습니다."),
-    INVALID_LOTTO_RANGE("로또 번호는 1에서 45사이의 숫자이여야합니다."),
+    INVALID_LOTTO_RANGE("로또 번호는 1에서 45사이의 숫자여야합니다."),
     INVALID_PURCHASE_TYPE("구매 금액은 숫자로만 입력해주세요."),
     INVALID_PURCHASE_AMOUNT_UNIT("구매 금액은 1000원 단위만 가능합니다."),
-    INVALID_PURCHASE_MIN("구매 금액은 1000원 이상이여야합니다.");
+    INVALID_PURCHASE_MIN("구매 금액은 1000원 이상이여야합니다."),
+    INVALID_BONUS_NUMBER_RANGE("보너스 숫자는 1에서 45사이의 숫자여야합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
