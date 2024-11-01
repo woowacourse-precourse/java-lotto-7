0ea95930f787fa23b3c83769b13d9db7627cce16
EHCTH <lees90896@gmail.com>
2024-11-01 19:00:48 +0900

    faet(LottoCreatorService): interface 구현 * feat: +createLottoTickets(int) : LottoTickets * feat: +createBonusNumber(WinningLotto): BonusNumber * feat: +createWinningLotto(): WinningLotto

--- src/main/java/lotto/lotto/domain/LottoCreatorService.java
+++ src/main/java/lotto/lotto/domain/LottoCreatorService.java
@@ -1,4 +1,10 @@
 package lotto.lotto.domain;
 
+import lotto.lotto.winning.domain.BonusNumber;
+import lotto.lotto.winning.domain.WinningLotto;
+
 public interface LottoCreatorService {
+    LottoTickets createLottoTickets(int count);
+    BonusNumber createBonusNumber(WinningLotto winningLotto);
+    WinningLotto createWinningLotto();
 }
