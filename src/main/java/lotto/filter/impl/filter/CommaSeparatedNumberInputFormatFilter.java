41768b386d14090019ef380d3d2ed1b02f64dc33
reindeer002 <lsj000424@gmail.com>
2024-11-01 18:47:56 +0900

    fix: correct implementation to prevent unnecessary exceptions
    - fix code that always triggered exceptions

--- src/main/java/lotto/filter/impl/filter/CommaSeparatedNumberInputFormatFilter.java
+++ src/main/java/lotto/filter/impl/filter/CommaSeparatedNumberInputFormatFilter.java
@@ -8,9 +8,9 @@ public class CommaSeparatedNumberInputFormatFilter implements Filter {
 
     @Override
     public void doFilter(String input, FilterChain filterChain) {
-        if (input.matches(regex)) {
-            filterChain.doFilter(input);
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 숫자(양의 정수)와 구분자(,)로 이루어진 문자열을 입력해주십시오.");
         }
-        throw new IllegalArgumentException("[ERROR] 숫자(양의 정수)와 구분자(,)로 이루어진 문자열을 입력해주십시오.");
+        filterChain.doFilter(input);
     }
 }
