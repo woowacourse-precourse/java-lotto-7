7e9c9c2ebc5313bf5235f8a9c445f5c6bcca1dee
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-01 16:17:53 +0900

    refactor(OutputView): 수익률 변수명 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,5 +5,5 @@ public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다";
     private static final String WINNING_RESULT_PREFIX = "당첨 통계\n---\n";
     private static final String WINNING_RESULT_MESSAGE = "%d개 일치 (%,d원) - %d개\n";
-    private static final String REVENUE_RESULT_MESSAGE = "총 수익률은 %.1f%입니다.\n";
+    private static final String PROFIT_RATE_RESULT_MESSAGE = "총 수익률은 %.1f%입니다.\n";
 }
