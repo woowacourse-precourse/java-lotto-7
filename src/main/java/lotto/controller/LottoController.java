6229ce77fac715359fc1bc21dfbfc0e16a689ced
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-11-01 15:53:44 +0900

    feat(controller): LottoController process logic design
    - 구매자와 관리자 모델을 통해 상품 구매 로직 디자인
    - 구매자는 가격을 통해 상품을 요청
    - 관리자는 로또를 판매 로직
    - 해당되는 가격만큼 로또의 랜덤 번호를 출력시켜줌
    - 구매자가 수익률을 요청하면 이에 응답해주는 역할

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,20 +1,57 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.LottoManager;
+import lotto.model.LottoMember;
+import lotto.view.ManagerInputView;
 import lotto.view.MemberInputView;
 import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
 
     private final MemberInputView memberInputView;
+    private final ManagerInputView managerInputView;
     private final OutputView outputView;
 
     public LottoController() {
         this.memberInputView = new MemberInputView();
+        this.managerInputView = new ManagerInputView();
         this.outputView = new OutputView();
     }
 
     public void run() {
-        int price = memberInputView.getPrice();
+        //구매자가 상품을 주문
+        outputView.printGetPrice();
+        Integer price = memberInputView.getPrice();
+        LottoManager lottoManager = new LottoManager(price);
+
+        //구매자가 상품을 사고 관리자가 상품을 구매자에게 넘겨줌
+        Integer purchasedLottoTotal = lottoManager.purchaseLotto();
+
+        LottoMember lottoMember = new LottoMember();
+        for (Lotto lotto : lottoManager.getPurchasedLotto()) {
+            lottoMember.setPurchasedLotto(lotto); // Lotto 객체를 추가
+        }
+
+        outputView.printPurchasedLottoCount(purchasedLottoTotal);
+        outputView.printLottoPurchaseList(lottoMember.getPurchasedLotto());
+
+        //관리자는 로또 번호를 생성함(로또 당첨 정기일이라 생각)
+        outputView.printLottoNumber();
+        List<Integer> lottoNumbers = managerInputView.getLottoNumbers();
+        outputView.printLottoBonusNumber();
+        Integer bonusNumber = managerInputView.getLottoBonusNumber();
+
+        //구매자가 구매한 상품을 관리자에게 넘겨 당첨 여부와 수익률에 대해서 출력을 요청
+        //관리자는 구매자가 구매한 상품과 생성된 로또 번호를 통해 당첨 수익률을 계산하고 당첨 통계를 구매자에게 응답.
+        lottoManager.isLottoResult(lottoNumbers, bonusNumber);
+
+        // 로또 몇등 당첨되었는지의 정보를 저장
+        lottoMember.setLottoResult();
+
+        //구매자에서 요청 내역 출력
 
     }
 }
