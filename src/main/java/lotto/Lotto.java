93c193161b0d5591c952205d8ce5eb5b4e4458ea
Jang <sdsd090811@gmail.com>
2024-11-01 17:32:01 +0900

    feat: 로또 생성 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,54 @@
 package lotto;
 
+import lotto.basic.NumbersGenerator;
+
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    protected Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    public static Lotto generate(NumbersGenerator numbersGenerator) {
+        List<Integer> randomNumbers = numbersGenerator.random(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
+
+        if(isInvalidSize(randomNumbers))
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        if(hasDuplicatedNumbers(randomNumbers))
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개는 유니크해야 합니다.");
+        if(hasNumbersOutOfRange(randomNumbers))
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.");
+
+        return new Lotto(randomNumbers);
     }
 
-    // TODO: 추가 기능 구현
+    private static boolean hasNumbersOutOfRange(List<Integer> numbers) {
+        return countNumbersInRange(numbers) != numbers.size();
+    }
+
+    private static long countNumbersInRange(List<Integer> numbers) {
+        return numbers.stream().filter(Lotto::isWithinLottoRange).count();
+    }
+
+    private static boolean isWithinLottoRange(Integer num) {
+        return MIN_LOTTO_NUMBER <= num && num <= MAX_LOTTO_NUMBER;
+    }
+
+    private static boolean isInvalidSize(List<Integer> numbers) {
+        return numbers.size() != LOTTO_SIZE;
+    }
+
+    private static boolean hasDuplicatedNumbers(List<Integer> numbers) {
+        return countUniqueNumber(numbers) != numbers.size();
+    }
+
+    private static long countUniqueNumber(List<Integer> numbers) {
+        return numbers.stream().distinct().count();
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.basic.NumbersGeneratorStub;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,18 +10,39 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    private NumbersGeneratorStub generatorStub;
+
+    @BeforeEach
+    void beforeEach() {
+        generatorStub = new NumbersGeneratorStub();
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+    void test1() {
+        generatorStub.setTestRandomNumbers(List.of(1, 2, 3, 4, 5, 6, 7));
+
+        assertThatThrownBy(() -> Lotto.generate(generatorStub))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+    void test2() {
+        generatorStub.setTestRandomNumbers(List.of(1, 2, 3, 4, 5, 5));
+
+        assertThatThrownBy(() -> Lotto.generate(generatorStub))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1~45 범위 밖에 있다면 예외가 발생한다.")
+    @Test
+    void test3() {
+        generatorStub.setTestRandomNumbers(List.of(1, 2, 3, 999, 5, 6));
+
+        assertThatThrownBy(() -> Lotto.generate(generatorStub))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
