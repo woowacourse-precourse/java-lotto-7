432735f8e016583a855a0d4a2b47c121b18c56b5
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-01 13:36:27 +0900

    refactor(rank): Rank Enum 수정
    Map을 사용하여 Rank 값을 가져오도록 리팩토링.

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public enum Rank {
     FIRST(6, 2_000_000_000, "6개 일치"),
     SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
@@ -7,6 +10,16 @@ public enum Rank {
     FOURTH(4, 50_000, "4개 일치"),
     FIFTH(3, 5_000, "3개 일치"),
     NONE(0, 0, "당첨 없음");
+
+    private static final Map<String, Rank> RANK_MAP = new HashMap<>();
+
+    static {
+        RANK_MAP.put("6,false", FIRST);
+        RANK_MAP.put("5,true", SECOND);
+        RANK_MAP.put("5,false", THIRD);
+        RANK_MAP.put("4,false", FOURTH);
+        RANK_MAP.put("3,false", FIFTH);
+    }
 
     private final int matchCount;
     private final int prize;
@@ -18,15 +31,19 @@ public enum Rank {
         this.description = description;
     }
 
-    public int getPrize() {
-        return prize;
-    }
-
     public int getMatchCount() {
         return matchCount;
     }
 
-   public String getDescription() {
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getDescription() {
         return description;
-   }
+    }
+
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        return RANK_MAP.getOrDefault(matchCount + "," + matchBonus, NONE);
+    }
 }
