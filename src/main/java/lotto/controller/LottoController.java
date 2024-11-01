09107759df24454bde107d5c12848576fbd99d68
shin-chaeeun <chaeeun0217@naver.com>
2024-11-01 18:42:47 +0900

    Feat(ValidationManager):빈문자열 처리 메서드와 1000원단위 에러 처리 메서드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,19 +2,40 @@ package lotto.controller;
 
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import model.ValidationManager;
 
 public class LottoController {
 
     private final OutputView outputView;
     private final InputView inputView;
+    private final ValidationManager validationManager;
 
-    public LottoController(InputView inputView, OutputView outputView) {
+    public LottoController(InputView inputView, OutputView outputView, ValidationManager validationManager) {
         this.outputView = outputView;
         this.inputView = inputView;
+        this.validationManager = validationManager;
     }
 
-    public void paly(){
-        outputView.printRequest(OutputView.REQUEST_AMOUNT_MESSAGE);
-        String amount= inputView.readInput();
+    public void play(){
+        handleUserInputError();
+    }
+
+    public void handleUserInputError(){
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                outputView.printRequest(OutputView.REQUEST_AMOUNT_MESSAGE);
+                String amount= inputView.readInput();
+
+                validInput=validationManager.isEmptyInput(amount, validationManager.NON_NUMERIC_ERROR);
+                validInput=validationManager.isNumber(amount,validationManager.EMPTY_INPUT_ERROR);
+                validInput=validationManager.isDivideByThousand(amount,validationManager.NOT_DIVISIBLE_BY_THOUSAND_ERROR);
+
+            } catch (IllegalArgumentException e) {
+                // 잘못된 입력에 대한 예외를 처리하고 다시 입력을 요청
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
