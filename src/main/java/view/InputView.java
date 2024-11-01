3234d9c69b2856450fa7cc133e920c0eb31b823a
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-01 10:42:48 +0900

    feat: 당첨 번호 입력 구현
    요구한 메세지를 출력하고, 입력을 받아 Int 배열로 변환을 해주는 메서드를 생성하였고,
    양의 정수 입력 메세지를 공통으로 사용하기 위해 에러 이름을 변경하였음

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,17 +1,18 @@
 package view;
 
-import static exception.ErrorMessage.PURCHASE_PRICE_ONLY_NUMBER;
+import static exception.ErrorMessage.INPUT_ONLY_NUMBER;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputView {
 
+    private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해주세요.";
     private static final String CHECK_NUMBER_REGEX = "\\d+";
 
     private InputView() {
     }
-
-    private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해주세요.";
 
     public static void purchasePriceInput() {
         System.out.println(PURCHASE_PRICE_MESSAGE);
@@ -19,11 +20,25 @@ public class InputView {
         validatePurchasePrice(userInput);
     }
 
+    public static List<Integer> winningNumberInput() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
+        String userInput = Console.readLine();
+        return splitAndConvertUserInput(userInput);
+    }
+
     private static void validatePurchasePrice(String purchasePrice) {
         if (!purchasePrice.matches(CHECK_NUMBER_REGEX)) {
-            throw new IllegalArgumentException(PURCHASE_PRICE_ONLY_NUMBER.getMessage());
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBER.getMessage());
         }
     }
 
-
+    private static List<Integer> splitAndConvertUserInput(String userInput) {
+        try {
+            return List.of(userInput.split(",")).stream()
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBER.getMessage());
+        }
+    }
 }
