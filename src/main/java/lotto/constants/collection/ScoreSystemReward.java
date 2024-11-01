fde1bf77a6242736f580d0482e32dd3b8061a80a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-02 01:25:57 +0900

    chore: ScoreSystem 관련 주석 추가

--- src/main/java/lotto/constants/collection/ScoreSystemReward.java
+++ src/main/java/lotto/constants/collection/ScoreSystemReward.java
@@ -5,6 +5,9 @@ import lotto.constants.Constants;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ScoreSystem의 조합 순서에 맞게 Reward가 List에 들어가 있습니다.
+ */
 public enum ScoreSystemReward implements Constants<List> {
     DEFAULT(List.of(5000,
             50000,
