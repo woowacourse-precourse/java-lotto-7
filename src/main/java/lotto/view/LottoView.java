51ca9802d0ca792847b0c39323b808239a599319
woogym <kwj9294@gmail.com>
2024-11-02 05:24:46 +0900

    feat(view): 로또 뷰 구현

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,2 +1,80 @@
-package lotto.view;public class LottoView {
+package lotto.view;
+
+import java.util.List;
+import lotto.dto.LottoDto;
+import lotto.dto.StatisticDto;
+import lotto.exception.LottoException.BonusNumberAlreadyIncludedWinningNumberException;
+import lotto.model.Lotto;
+import lotto.util.Converter;
+import lotto.util.InputValidator;
+import lotto.vo.BonusNumber;
+import lotto.vo.BuyLottoAmount;
+import lotto.vo.TicketCount;
+
+public class LottoView {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoView(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public BuyLottoAmount promptBuyAmount() {
+        while (true) {
+            try {
+                String amount = inputView.inputBuyAmount();
+                InputValidator.validateBuyLotteriesAmount(amount);
+                return new BuyLottoAmount(Integer.parseInt(amount));
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    public Lotto promptWinningLotto() {
+        while (true) {
+            try {
+                String numbers = inputView.inputWinningNumber();
+                InputValidator.validateWinningNumbers(numbers);
+                List<Integer> winningNumbers = Converter.convertToIntgerList(numbers);
+                return Lotto.createWinningLotto(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    public BonusNumber promptBonusNumber(List<Integer> winningNumbers) {
+        while (true) {
+            try {
+                String number = inputView.inputBonusNumber();
+                InputValidator.validateBonusNumber(number);
+                int bonusNumber = Integer.parseInt(number);
+                if (winningNumbers.contains(bonusNumber)) {
+                    throw new BonusNumberAlreadyIncludedWinningNumberException();
+                }
+                return new BonusNumber(bonusNumber);
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    public void displayTicketCount(TicketCount ticketCount) {
+        outputView.printTicketCountResult(ticketCount.count());
+    }
+
+    public void displayLotteries(List<LottoDto> lotteriesDto) {
+        outputView.printLotteriesNumber(lotteriesDto);
+    }
+
+    public void displayStatistics(StatisticDto statisticDto) {
+        outputView.printStatistic(statisticDto);
+    }
+
+    public void displayProfitRate(double profitRate) {
+        outputView.printYeildRate(profitRate);
+    }
 }
