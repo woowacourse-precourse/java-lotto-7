a77e296d5fba4b1ff295bdff18d018d8fb372ef2
iceprins <junghankyul@gmail.com>
2024-11-02 00:26:33 +0900

    refactor: Lotto의 패키지 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 
@@ -11,9 +11,9 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6)
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+
     }
 
     // TODO: 추가 기능 구현
