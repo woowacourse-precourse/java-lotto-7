81e7bf4fff151ee23f44e3ee4a72acc9b86fad85
plan11plan <tjtfy7028@gmail.com>
2024-11-02 01:55:00 +0900

    feat: 로또 도메인 생성 기능 구현

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -10,6 +10,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
