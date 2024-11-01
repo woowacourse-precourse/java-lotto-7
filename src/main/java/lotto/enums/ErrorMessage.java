e45e402a3e25b3897f8a16012a60ea24381ba566
yereumi <lovelyyeji01@gmail.com>
2024-11-02 03:08:41 +0900

    feat(ErrorMessage): 로또 번호 중복 메시지 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
     // 에러 메시지
     INVALID_LOTTO_COUNT("[ERROR] 로또 구입 금액은 1000으로 나누어 떨어지는 숫자여야 합니다."),
     INVALID_NUMBERS("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_DELIMITER("[ERROR] 구분자는 쉼표(,)여야 합니다.");
+    DUPLICATE_NUMBERS("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
 
     private final String message;
 
