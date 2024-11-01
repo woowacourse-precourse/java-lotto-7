d14d3ce8115f8a9c18574f10946e646fbf314eb3
imjunghun <dlawndgns9@naver.com>
2024-11-01 14:34:51 +0900

    feat(Rank): 로또 당첨 등수 정보를 저장한 Rank Enum 클래스 구현

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,54 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    NONE(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
+
+    private final int correctCount;
+    private final int prize;
+    private final boolean hasBonus;
+
+    Rank(int correctCount, int prize, boolean hasBonus) {
+        this.correctCount = correctCount;
+        this.prize = prize;
+        this.hasBonus = hasBonus;
+    }
+
+    public static Rank find(int count, boolean isBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.match(count, isBonus))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean match(int count, boolean isBonus) {
+        if (this == FIRST) {
+            return count == this.correctCount;
+        }
+
+        if (this == SECOND || this == THIRD) {
+            return count == this.correctCount && isBonus == this.hasBonus;
+        }
+
+        return count == this.correctCount;
+    }
+
+    public int getCorrectCount() {
+        return correctCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public boolean getBonus() {
+        return hasBonus;
+    }
+}
