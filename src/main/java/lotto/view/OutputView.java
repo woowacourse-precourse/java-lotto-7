11baa10d91589f67790857142e4084a386361140
soeun2537 <soeun2537@gmail.com>
2024-11-02 03:59:46 +0900

    feat: 구입한 로또 번호 출력 로직 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,9 +2,17 @@ package lotto.view;
 
 import static lotto.constant.message.OutputMessage.*;
 
+import java.util.List;
+
 public class OutputView {
 
     public static void printLottoCount(Integer lottoCount) {
         System.out.println(PURCHASE_NUMBER_OUTPUT_MESSAGE.getMessage(lottoCount));
+    }
+
+    public static void printLottoTicketsDetails(List<String> lottoTickets) {
+        for (String lottoTicket : lottoTickets) {
+            System.out.println(lottoTicket);
+        }
     }
 }
