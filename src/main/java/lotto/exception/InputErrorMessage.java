5f88e31304b3a34741f3865337385461784ab0a7
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-01 16:28:10 +0900

    feat(exception): 예외메시지를 담은 enum클래스 추가
    - 사용자 입력을 검증하는 InputErrorMessage
    
    - 로또 번호를 검증하는 LottoErrorMessage

---
+++ src/main/java/lotto/exception/InputErrorMessage.java
@@ -0,0 +1,29 @@
+package lotto.exception;
+
+public enum InputErrorMessage {
+    INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
+    EMPTY_PURCHASE_AMOUNT("[ERROR] 구입 금액은 비어있을 수 없습니다."),
+    NEGATIVE_OR_ZERO_AMOUNT("[ERROR] 구입 금액은 0원 이상이어야 합니다."),
+    EXCEEDS_LIMIT_AMOUNT("[ERROR] 구입 금액은 1,000,000원을 초과할 수 없습니다."),
+
+    EMPTY_WINNING_NUMBERS("[ERROR] 당첨 번호는 비어있을 수 없습니다."),
+    INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 숫자여야 합니다."),
+    NEGATIVE_OR_ZERO_WINNING_NUMBER("[ERROR] 당첨 번호는 1~45 범위의 양의 정수여야 합니다."),
+    DUPLICATED_WINNING_NUMBERS("[ERROR] 당첨 번호는 중복될 수 없습니다."),
+
+    EMPTY_BONUS_NUMBER("[ERROR] 보너스 번호는 비어있을 수 없습니다."),
+    INVALID_BONUS_NUMBER("[ERROR] 보너스 번호는 숫자여야 합니다."),
+    NEGATIVE_OR_ZERO_BONUS_NUMBER("[ERROR] 보너스 번호는 1~45 범위의 양의 정수여야 합니다."),
+    DUPLICATED_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+
+
+    private final String message;
+
+    InputErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/exception/LottoErrorMessage.java
@@ -0,0 +1,19 @@
+package lotto.exception;
+
+public enum LottoErrorMessage {
+    INVALID_LOTTO_COUNT("[ERROR] 발행할 로또 수가 유효하지 않습니다."),
+    LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호는 1~45 범위의 숫자여야 합니다."),
+    DUPLICATED_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    INVALID_WINNING_RANK("[ERROR] 잘못된 당첨 등수입니다."),
+    INVALID_YIELD("[ERROR] 잘못된 수익률 값입니다.");
+
+    private final String message;
+
+    LottoErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
