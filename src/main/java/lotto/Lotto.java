8504a5fc6aa9567f2f0d72df8228bc293a29f74c
EP45 <5510816+EP45@users.noreply.github.com>
2024-11-01 21:27:15 +0900

    feat(Lotto): implement validation method
    Currently the method checks:
    - if only 6 numbers are in the list
    - if numbers are in range
    - if numbers are distinct

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -14,7 +16,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        for (int i : numbers) {
+            if (!inRange(i)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1 이상 45 이하의 정수여야 합니다.");
+            }
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 각 번호는 서로 달라야 합니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean inRange(int number) {
+        return (number <= 45 && number >= 1);
+    }
 }
