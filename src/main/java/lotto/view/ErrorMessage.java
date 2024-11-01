655a99783583c85317e28a9ee9b19f51708515a9
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 19:41:45 +0900

    refactor: 새로운 상수 취급하는 문자열 추가

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     INVALID_PRICE("1000원 단위의 금액을 입력해 주세요."),
     DUPLICATE_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복되어선 안됩니다."),
-    NON_NUMERIC_INPUT("숫자만 입력해 주세요.");
+    NON_NUMERIC_INPUT("숫자만 입력해 주세요."),
+    DUPLICATE_LOTTO_NUMBER("중복된 숫자를 입력하지 마시오.");
 
     public static final String BASE_MESSAGE = "[ERROR] %s";
     private final String message;
