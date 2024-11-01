7bf22eefa88767581a5a190b8884b72e532c1e00
JinHyung-Park <trupjh@gmail.com>
2024-11-01 22:46:23 +0900

    refactor(LottoController) : 메소드명 동사가 앞으로 오게 정정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,16 +9,17 @@ public class LottoController {
     private final OutputView outputView = new OutputView();
     private final PurchaseService purchaseService = new PurchaseService();
 
-    public void lottoPurchase() {
+    public void purchaseLotto() {
         int price = inputView.getPrice();
 
         int lottoCount = purchaseService.buyLotto(price);
         outputView.printPurchaseResult(lottoCount);
 
-        lottoMachineRun(lottoCount);
+        //수량만큼 로또 발부
+        runLottoMachine(lottoCount);
     }
 
-    public void lottoMachineRun(int lottoCount) {
+    public void runLottoMachine(int lottoCount) {
 
     }
 }
