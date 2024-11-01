fe0611279e7919ca34ef904a484614be99680ca9
ddddabi <ii887742@gmail.com>
2024-11-01 22:41:59 +0900

    style: ErrorMessage 변수명 수정
    - INVALID_WINNING_NUMBERS -> INVALID_WINNING_NUMBER_COUNT

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -2,7 +2,7 @@ package lotto.common;
 
 public class ErrorMessage {
     public static final String INVALID_PURCHASE_AMOUNT = "[ERROR] 로또 구입금액은 1,000원 단위여야 합니다.";
-    public static final String INVALID_WINNING_NUMBERS = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String INVALID_WINNING_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
     public static final String INVALID_BONUS_NUMBER = "[ERROR] 보너스 번호는 1개여야 합니다.";
     public static final String NUMBER_OUT_OF_RANGE = "[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.";
 
