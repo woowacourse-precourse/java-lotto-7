350d4dfdf7d35fd9a649bd6aacfc71698a2e13e4
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-01 19:02:28 +0900

    feat($inputValidation) : Check unit of money
    1000원 단위인지 확인하는 기능 추가

--- src/main/java/lotto/controller/InputValidation.java
+++ src/main/java/lotto/controller/InputValidation.java
@@ -9,14 +9,26 @@ public class InputValidation {
         view = new View();
     }
 
+
+
     public Long moneyCheck() {
+        while (true) {
+            try {
+                return getValidatedMoney(); // 올바른 입력일 때 반환
+            } catch (Exception e) {
+                System.out.println(e.getMessage()); // 모든 예외를 한 번에 처리
+            }
+        }
+    }
+
+    private Long getValidatedMoney() {
         String moneyInput = view.getMoneyInput();
         checkInputNull(moneyInput);
-
         Long money = Long.parseLong(moneyInput);
+        checkUnitOfMoney(money);
         return money;
-
     }
+
 
     private void checkInputNull(String input) {
         if (input.isEmpty()){
@@ -24,6 +36,12 @@ public class InputValidation {
         }
     }
 
-    
+    private void checkUnitOfMoney(Long input) {
+        if (input == 0 || !(input%1000==0)){
+            throw new IllegalArgumentException("[ERROR] 금액은 0원이 될 수 없고, 1000원 단위로만 입력 가능합니다.");
+        }
+    }
+
+
 
 }
