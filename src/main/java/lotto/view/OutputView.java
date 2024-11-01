7ef922765ce4180d16b79a5a69702b19f67db0c2
SungJun <kmymy00@naver.com>
2024-11-01 11:06:32 +0900

    fix(OutputView): 당첨 갯수 출력 오류 수정
    기존 printf문에 %d가 아닌 0으로 입력되어 오류 발생

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -20,9 +20,9 @@ public class OutputView {
         System.out.println("---");
         System.out.printf("3개 일치 (5,000원) - %d개\n", winnings.getOrDefault(Winning.THREE, 0));
         System.out.printf("4개 일치 (50,000원) - %d개\n", winnings.getOrDefault(Winning.FOUR, 0));
-        System.out.printf("5개 일치 (1,500,000원) - 0개\n", winnings.getOrDefault(Winning.FIVE, 0));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n", winnings.getOrDefault(Winning.FIVE_BONUS, 0));
-        System.out.printf("6개 일치 (2,000,000,000원) - 0개\n", winnings.getOrDefault(Winning.SIX, 0));
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", winnings.getOrDefault(Winning.FIVE, 0));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", winnings.getOrDefault(Winning.FIVE_BONUS, 0));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", winnings.getOrDefault(Winning.SIX, 0));
         System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 }
