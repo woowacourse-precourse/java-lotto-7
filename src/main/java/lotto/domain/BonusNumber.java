67213dd93d09bd2fb2f5a2fb280847ca55d07472
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-11-01 15:48:07 +0900

    test: 보너스 번호 당첨 번호 포함 예외

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -6,19 +6,24 @@ public class BonusNumber {
 
     private final Integer bonusNumber;
 
-    public BonusNumber(String input) {
-        validate(input);
+    public BonusNumber(String input, WinningNumbers winningNumbers) {
+        validate(input, winningNumbers);
         this.bonusNumber = Integer.parseInt(input);
     }
 
-    private void validate(String input) {
+    private void validate(String input, WinningNumbers winningNumbers) {
+
+        int n = 0;
         try{
-            int n = Integer.parseInt(input);
+            n = Integer.parseInt(input);
             if(n < 1 || n > 45)
                 throw new IllegalArgumentException();
         } catch (Exception e) {
             throw new IllegalArgumentException(LottoErrorMessage.LOTTO_RANGE_EXCEPTION.getMsg());
         }
+
+        if(winningNumbers.getNumbers().contains(n))
+            throw new IllegalArgumentException(LottoErrorMessage.BONUS_WINNING_CONFLICT_EXCEPTION.getMsg());
     }
 
     public int getBonusNumber() {
