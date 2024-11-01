99c918580c6e92c75ea164667ca97a2248007b1c
JS <zks145@naver.com>
2024-11-01 22:34:23 +0900

    style : 로또 도메인 디렉토리 위치 변경

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.List;
+
+import static lotto.util.Utils.sortLottoNumbers;
+import static lotto.validation.Validation.*;
+
+public class Lotto {
+
+    private final static Integer LOTTO_START_NUMBER = 1;
+    private final static Integer LOTTO_END_NUMBER = 45;
+    private final static Integer LOTTO_MAX_LENGTH = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = sortLottoNumbers(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        checkLottoSize(numbers, LOTTO_MAX_LENGTH);
+        checkLottoDuplicate(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
