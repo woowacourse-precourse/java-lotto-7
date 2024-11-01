b4251ee10d3bf4bf58cd30bc014028947f9bf618
Chanyoo Kim <cksdb012@gmail.com>
2024-11-01 18:15:43 +0900

    refactor: 클래스 이름 변경
    구입 금액 타입이 문자열로 입력된 경우의 예외처리 클래스의 이름을 변경

--- src/main/java/lotto/exception/PurchaseAmountTypeException.java
+++ src/main/java/lotto/exception/PurchaseAmountTypeException.java
@@ -1,9 +1,9 @@
 package lotto.exception;
 
-public class PurchaseAmountNumberException extends IllegalArgumentException {
+public class PurchaseAmountTypeException extends IllegalArgumentException {
     private static final String ERROR_MESSAGE = "[ERROR] 구입 금액은 숫자로 입력해주셔야 합니다";
 
-    public PurchaseAmountNumberException() {
+    public PurchaseAmountTypeException() {
         super(ERROR_MESSAGE);
     }
 }
