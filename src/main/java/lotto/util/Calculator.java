08c234193d542bfadb7ed0fb2ac9f5dbbb8d92b5
menzzi <minmin3927@naver.com>
2024-11-01 15:59:55 +0900

    feat(util) : Calculator - calculateRateOfReturn 메서드 구현

--- src/main/java/lotto/util/Calculator.java
+++ src/main/java/lotto/util/Calculator.java
@@ -1,7 +1,19 @@
 package lotto.util;
 
+import java.util.Map;
+import lotto.model.Result;
+
 public class Calculator {
     public static int calculateLottoTicketNumber(int input){
         return input / 1000;
+    }
+
+    public static double calculateRateOfReturn(int lottoAmount, Map<Result,Integer> results){
+        double totalProfit = 0;
+        for(Result result : results.keySet()){
+            totalProfit += result.getPrizeMoney() * results.get(result);
+        }
+        totalProfit /= lottoAmount;
+        return Math.round(totalProfit*1000);
     }
 }
