8a0f6bbd70daace39084ed46cb46bc4f78142a3a
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 15:13:49 +0900

    feat(OutputView): 랜덤로또번호 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printLottoList(List<Integer> randomLottoNumber){
+        System.out.println(randomLottoNumber);
+    }
+}
