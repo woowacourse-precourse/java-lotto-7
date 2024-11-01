ac62b10bb63d1c2c23f10d97a3830bf0d576f1ce
Yun-tellije <dbswjddms517@naver.com>
2024-11-02 00:49:05 +0900

    feat: 당첨 번호 타입 검증 기능 구현

---
+++ src/main/java/lotto/exception/LottoNumberTypeValidator.java
@@ -0,0 +1,20 @@
+package lotto.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberTypeValidator {
+
+    public List<Integer> validateWinningNumbersType(String userInput) {
+        try {
+            String[] numbers = userInput.split(",");
+            List<Integer> winningNumbers = new ArrayList<>();
+            for (String number : numbers) {
+                winningNumbers.add(Integer.parseInt(number.trim()));
+            }
+            return winningNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자로 입력해주세요.\n");
+        }
+    }
+}
