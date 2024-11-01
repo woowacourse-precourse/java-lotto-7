bd34bcc20e14bd0ee7765ab4fd114dd4ac836516
hunko <rhwlgns4386@naver.com>
2024-11-01 14:06:05 +0900

    feat : 로또 검증 기능 완성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,33 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
+    private final Set<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        this.numbers = toLottoNumbers(numbers);
+        validateSize();
+        validDuplicate(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateSize() {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validDuplicate(List<Integer> inputNumbers) {
+        if (inputNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복되어서는 안됩니다.");
+        }
+    }
+
+    private Set<LottoNumber> toLottoNumbers(List<Integer> numbers) {
+        return numbers.stream().map(LottoNumber::new).collect(Collectors.toSet());
     }
 
     // TODO: 추가 기능 구현
