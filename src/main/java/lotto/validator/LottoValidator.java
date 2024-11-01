95628f42ecf09e32e16ed186a643621c41a8ea68
jaehwannnnnn <seanjung0915@gmail.com>
2024-11-01 18:30:20 +0900

    feat(LottoValidator): 당첨번호와 보너스 번호 중복 예외처리 구현

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,9 +1,27 @@
 package lotto.validator;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class LottoValidator {
-    public static void validatePurchaseAmount(int price) {
+    public static void validatePrice(int price) {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위이어야 합니다.");
+        }
+    }
+    public static void validateWinningNumbers(List<Integer> numbers, int bonusNumber){
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        if (uniqueNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
         }
     }
 }
