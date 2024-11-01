496986d7e7fcac6957cb94289a2e54dc102be78d
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2024-11-01 19:57:33 +0900

    fix: 로또 번호 40을 패턴에서 누락함

--- src/main/java/lotto/io/validator/regex/RegexPattern.java
+++ src/main/java/lotto/io/validator/regex/RegexPattern.java
@@ -6,5 +6,5 @@ public class RegexPattern {
     private RegexPattern() {
     }
 
-    public static final Pattern NUMBER_RANGE = Pattern.compile("^([1-9]|[1-3][0-9]|4[1-5])$");
+    public static final Pattern NUMBER_RANGE = Pattern.compile("^([1-9]|[1-3][0-9]|4[0-5])$");
 }
