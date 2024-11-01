2259a5fdb62eaaadfda53acb8b52a1438e2e1362
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-01 20:33:41 +0900

    feat($RandomLottoNumberGenerator): 랜덤 LottoNumber 생성기 인터페이스 추가
    - 랜덤한 LottoNumber 를 생성하는 역할.
    - LottoShop 에서 사용할 예정.

---
+++ src/main/java/lotto/model/number_generator/RandomLottoNumberGenerator.java
@@ -0,0 +1,10 @@
+package lotto.model.number_generator;
+
+import lotto.model.LottoNumber;
+
+import java.util.List;
+
+public interface RandomLottoNumberGenerator {
+
+    List<LottoNumber> generate();
+}
