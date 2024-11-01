8670adaeb3bb4d7329e57480a199e525cad171e3
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-01 23:40:26 +0900

    feat: ErrorCatcher 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,37 @@
 package lotto.controller;
 
+import lotto.constants.value.LottoRule;
 import lotto.domain.LottoMachine;
 import lotto.view.OutputView;
+
+import java.util.function.Supplier;
 
 public class LottoController {
 
     private final LottoInputManger inputManger;
-    private final LottoMachine;
-    private final OutputView;
+    private final LottoMachine lottoMachine;
+    private final OutputView outputView;
 
-    public LottoController(LottoInputManger inputManger) {
+    public LottoController(LottoInputManger inputManger, LottoMachine lottoMachine, OutputView outputView) {
         this.inputManger = inputManger;
+        this.lottoMachine = lottoMachine;
+        this.outputView = outputView;
+    }
+
+    public void play(){
+        int inputPrice = ErrorCatcher(inputManger::getInputPrice);
+        int lottoBuyAmount = inputPrice/ LottoRule.LOTTO_PRICE.getInstance();
+
     }
 
 
+    private <T> T ErrorCatcher(Supplier<T> function) {
+        while (true) {
+            try {
+                return function.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
