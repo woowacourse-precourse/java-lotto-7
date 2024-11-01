4b9855530d7ff3728d20bfae5d1d6c940e3017b0
skhnpark <skhnpark125@gmail.com>
2024-11-01 22:07:57 +0900

    feat: 구입금액이 0 이상인지 확인하는 기능 구현

--- src/main/java/lotto/model/Balance.java
+++ src/main/java/lotto/model/Balance.java
@@ -4,6 +4,13 @@ public class Balance {
     private int money;
 
     public Balance(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 0 이상이어야 합니다.");
+        }
     }
 }
