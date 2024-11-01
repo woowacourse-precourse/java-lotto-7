a9c496448993dfce2c11a675c313780de946e0f6
wogus <ind07152@naver.com>
2024-11-01 12:53:42 +0900

    feat(ErrorMessage): 에러 메시지 추가
    로또 번호는 6개여야한다는 메시지를 추가한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public enum ErrorMessage {
-    ;
+    INVALID_LOTTO_COUNTS("로또 번호는 6개여야 합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private String message;
