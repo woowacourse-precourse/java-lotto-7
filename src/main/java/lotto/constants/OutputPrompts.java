2d4311a5943765c9474c685f8e9393f7c9fcf86b
bmg1203 <bmg1203@naver.com>
2024-11-01 23:42:43 +0900

    [fix]: 출력 형식 수정
    - 수익관련 메시지에 \n를 추가한다.
    - 수익률 %지정자를 수정합니다.

--- src/main/java/lotto/constants/OutputPrompts.java
+++ src/main/java/lotto/constants/OutputPrompts.java
@@ -1,14 +1,14 @@
 package lotto.constants;
 
 public enum OutputPrompts {
-    PURCHASE_RESULT("구매 결과: %d개를 구매했습니다."),
+    PURCHASE_RESULT("\n구매 결과: %d개를 구매했습니다."),
     WINNING_STATISTICS_HEADER("당첨 통계\n---"),
-    MATCHED_THREE("3개 일치 (5,000원) - %d개"),
-    MATCHED_FOUR("4개 일치 (50,000원) - %d개"),
-    MATCHED_FIVE("5개 일치 (1,500,000원) - %d개"),
-    MATCHED_FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    MATCHED_SIX("6개 일치 (2,000,000,000원) - %d개"),
-    TOTAL_PROFIT_RATE("총 수익률은 %.1f%%입니다.");
+    MATCHED_THREE("3개 일치 (5,000원) - %d개\n"),
+    MATCHED_FOUR("4개 일치 (50,000원) - %d개\n"),
+    MATCHED_FIVE("5개 일치 (1,500,000원) - %d개\n"),
+    MATCHED_FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    MATCHED_SIX("6개 일치 (2,000,000,000원) - %d개\n"),
+    TOTAL_PROFIT_RATE("총 수익률은 %s%%입니다.\n");
 
     private final String message;
 
