b1c64de1c4fe21e055448db852def58b10cba301
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:01:20 +0900

    feat(PurchaseCost): 구입금액 객체를 구현하라

---
+++ src/main/java/lotto/Model/PurchaseCost.java
@@ -0,0 +1,9 @@
+package lotto.Model;
+
+public class PurchaseCost {
+    private int purchaseCost;
+
+    public PurchaseCost(int purchaseCost) {
+        this.purchaseCost = purchaseCost;
+    }
+}
