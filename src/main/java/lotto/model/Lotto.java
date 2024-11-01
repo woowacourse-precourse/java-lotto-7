061d9acd33845da03aeec8f728774cd1bd5fdb09
Smith <125195487+sigmaith@users.noreply.github.com>
2024-11-01 17:45:02 +0900

    feat: 발행된 로또 검증

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,12 @@
 package lotto.model;
 
+import static lotto.utils.Error.DUPLICATED_LOTTO_NUMBERS;
+import static lotto.utils.Error.LOTTO_NUMBERS_OUT_OF_RANGE;
+import static lotto.utils.Error.NOT_SIX_LOTTO_NUMBERS;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +17,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSixLottoNumbers(numbers);
+        validateNoDuplicate(numbers);
+        validateNumbersInRange(numbers);
+    }
+
+    private void validateSixLottoNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(NOT_SIX_LOTTO_NUMBERS.getDescription());
         }
+    }
+
+    private void validateNoDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (numbers.size() != set.size()) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS.getDescription());
+        }
+    }
+
+    private void validateNumbersInRange(List<Integer> numbers) {
+        numbers.forEach(number -> {
+            if (number < 1 || 45 < number) {
+                throw new IllegalArgumentException(LOTTO_NUMBERS_OUT_OF_RANGE.getDescription());
+            }
+        });
     }
 
     @Override
