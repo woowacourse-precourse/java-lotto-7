f0941b8aec3fd223f91fe7d76f77683efb7c8083
SeongbinPark <hyucse22@gmail.com>
2024-11-01 14:57:47 +0900

    refactor(LottoGame): LottoGame 에서 게임의 흐름을 제어하도록 변경

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,31 @@
+package lotto;
+
+public class LottoGame {
+    private final InputView inputView;
+    private final LottoMachine lottoMachine;
+    private Result result;
+
+    public LottoGame() {
+        this.inputView = new InputView();
+        this.lottoMachine = new LottoMachine();
+    }
+
+    public void start() {
+        inputView.inputPurchaseAmount();
+
+        lottoMachine.purchaseLottos(inputView.getPurchaseAmount());
+        lottoMachine.printLottos();
+
+        inputView.inputWinningNumbers();
+        inputView.inputBonusNumber();
+
+        result = new Result(
+                lottoMachine.getLottos(),
+                inputView.getWinningNumbers(),
+                inputView.getBonusNumber()
+        );
+
+        result.calculate();
+        result.printResult(inputView.getPurchaseAmount());
+    }
+}
