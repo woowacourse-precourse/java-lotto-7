5b1e1b7ccd3739a7e73b765bcc63fa2329d09738
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 15:11:52 +0900

    feat(Lotto): 검증 항목 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateWinningNumberRange(numbers);
+        validateNoDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,24 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateWinningNumberRange(List<Integer> numbers) {
+        boolean isValid = numbers.stream()
+                .allMatch(number -> number >= 1
+                        && number <= 45);
+        if (!isValid) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("당첨 번호는 6개의 서로 다른 숫자여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
