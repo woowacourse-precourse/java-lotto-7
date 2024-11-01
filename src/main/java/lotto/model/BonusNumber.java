dc9acec982793f54e801d4b56c2c76ee50dfb412
junho767 <junho767@naver.com>
2024-11-01 18:52:59 +0900

    feat: 입력 받은 보너스 번호의 숫자 범위 검증 기능 구현
    1. MAX_NUMBER와 MIN_NUMBER를 범위로 지정

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -3,11 +3,15 @@ package lotto.model;
 import lotto.Message.ErrorMessage;
 
 public class BonusNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     private final int bonusNumber;
 
-    public BonusNumber(String input){
-        this.bonusNumber = validateNumber(input);
+    public BonusNumber(String input) {
+        int bonusNumber = validateNumber(input);
+        validNumberRange(bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
 
     private int validateNumber(String input) {
@@ -18,7 +22,13 @@ public class BonusNumber {
         }
     }
 
-    public int getNumber(){
+    private void validNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.toString());
+        }
+    }
+
+    public int getNumber() {
         return bonusNumber;
     }
 }
