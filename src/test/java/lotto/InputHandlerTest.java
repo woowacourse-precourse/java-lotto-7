15204f4ae9f05ed84f3385dd6cee9c509f225f4d
Jungwoo Chu <cjw980930@gmail.com>
2024-11-01 17:46:17 +0900

    test: 로또 구입금액이 1000단위가 아닐 경우 예외 테스트

---
+++ src/test/java/lotto/InputHandlerTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InputHandlerTest {
+    @Test
+    void 로또_구입금액이_1000단위가_아닐경우_예외_발생(){
+        // Given
+        int price = 5500;
+
+        // When
+        InputHandler inputHandler = new InputHandler();
+        // Then
+        Assertions.assertThatThrownBy(() -> InputHandler.priceInputValidator(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입금액은 1000단위만 가능합니다.");
+    }
+
+}
