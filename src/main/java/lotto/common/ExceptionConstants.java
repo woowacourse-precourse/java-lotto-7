50a23eea4220e9ae60e0a4cc086eac5e96525be8
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-01 18:37:54 +0900

    feat( ExceptionConstants.java ) Add DUPLICATE_BONUS_NUMBER Enum Constant

--- src/main/java/lotto/common/ExceptionConstants.java
+++ src/main/java/lotto/common/ExceptionConstants.java
@@ -2,7 +2,8 @@ package lotto.common;
 
 public enum ExceptionConstants {
     OVER_NUMBER_RANGE("[ERROR] ","당첨번호 범위 초과"),
-    OVER_NUMBER_SIZE("[ERROR] ","당첨번호 갯수 초과"),
+    OVER_NUMBER_SIZE("[ERROR] ","당첨번호 갯수 오류"),
+    DUPLICATE_BONUS_NUMBER("[ERROR] ","보너스 번호 당첨번호 중복 발생"),
     DUPLICATE_NUMBER("[ERROR] ","당첨번호 중복 발생"),
     NOT_BUY_UNIT("[ERROR] ","구매 단위 오류"),
     NOT_INTEGER("[ERROR] ","정수가 아님"),
