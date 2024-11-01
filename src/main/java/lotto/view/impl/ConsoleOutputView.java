aa6bc0715cd6f4f6b11991d25654f8d34c5b4193
reindeer002 <lsj000424@gmail.com>
2024-11-01 15:43:38 +0900

    feat(story4.3): add print function in right format
    - none prize statistics is not print in this function

--- src/main/java/lotto/view/impl/ConsoleOutputView.java
+++ src/main/java/lotto/view/impl/ConsoleOutputView.java
@@ -1,5 +1,7 @@
 package lotto.view.impl;
 
+import java.util.Map;
+import lotto.common.LottoRank;
 import lotto.domain.Lotto;
 import lotto.domain.LottoStatistics;
 import lotto.domain.LottoTickets;
@@ -8,6 +10,8 @@ import lotto.view.OutputView;
 public class ConsoleOutputView implements OutputView {
     private static final String NUMBER_OF_PURCHASE_LOTTO_NOTICE = "\n%d개를 구매했습니다.\n";
     private static final String LOTTO_STATISTICS_NOTICE = "\n당첨 통계\n---";
+    private static final String NONE_BONUS_MATCHES_NOTICE = "%d개 일치 (%,d원) - %d개\n";
+    private static final String BONUS_MATCHES_NOTICE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
     private static final String RETURN_RATE_NOTICE = "총 수익률은 %.1f%%입니다.";
     private static final String INPUT_AMOUNT_NOTICE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_NOTICE = "\n당첨 번호를 입력해 주세요.";
@@ -22,8 +26,20 @@ public class ConsoleOutputView implements OutputView {
     @Override
     public void printLottoStatistics(LottoStatistics lottoStatistics, double returnRate) {
         System.out.println(LOTTO_STATISTICS_NOTICE);
-        System.out.println(lottoStatistics.getResults());
+        Map<LottoRank, Integer> results = lottoStatistics.getResults();
+        results.forEach(this::printLottoPrize);
         System.out.printf(RETURN_RATE_NOTICE, returnRate);
+    }
+
+    private void printLottoPrize(LottoRank lottoRank, int count) {
+        if (lottoRank.getMatchCount() == 0) {
+            return;
+        }
+        if (lottoRank.isMatchBonus()) {
+            System.out.printf(BONUS_MATCHES_NOTICE, lottoRank.getMatchCount(), lottoRank.getPrize(), count);
+            return;
+        }
+        System.out.printf(NONE_BONUS_MATCHES_NOTICE, lottoRank.getMatchCount(), lottoRank.getPrize(), count);
     }
 
     @Override
