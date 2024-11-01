fd3bbdd0179f630a5f9f02e248dfa49ff86ccbdd
Hyun0828 <spdlqj4818@naver.com>
2024-11-01 13:38:19 +0900

    feat(OutputView): 당첨 내역 및 수익률을 출력하고 게임을 종료한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,26 @@
 package lotto.view;
 
+import lotto.domain.LottoResult;
 import lotto.dto.LottoOutputDto;
 
 public class OutputView {
 
-    public void showResult(LottoOutputDto lottoOutputDto) {
+    private static final String WINNING_DETAIL_FORMAT = "%d개 일치 (%d원) - %d개";
+    private static final String RATE_MESSAGE_FORMAT = "총 수익률은 %f%%입니다";
 
+    public void showResult(LottoOutputDto lottoOutputDto) {
+        showWinningDetails(lottoOutputDto.lottoResult());
+        showRateOfReturn(lottoOutputDto.rateOfReturn());
+    }
+
+    private void showWinningDetails(LottoResult lottoResult) {
+        lottoResult.getLottoResult().forEach((ranking, numOfWinner) -> {
+            System.out.println(String.format(WINNING_DETAIL_FORMAT, ranking.getMatchCount(), ranking.getWinningPrize(),
+                    numOfWinner));
+        });
+    }
+
+    private void showRateOfReturn(double rateOfReturn) {
+        System.out.println(String.format(RATE_MESSAGE_FORMAT, rateOfReturn));
     }
 }
