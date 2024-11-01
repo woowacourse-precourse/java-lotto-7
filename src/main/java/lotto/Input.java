1505815e528ea09b427e37624ece429d17c832c5
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-11-01 19:32:27 +0900

    feat: Input  입력 받는 클래스 생성 및 테스트

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+    public static int purchase() {
+        System.out.println("구입금액을 입력해 주세요.\n");
+        String text = Console.readLine();
+        return getNumber(text);
+    }
+
+    public static List<Integer> inputNumber(){
+        System.out.println("당첨 번호를 입력해 주세요,\n");
+        String text = Console.readLine();
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : text.split(",")) {
+            int number = getNumber(s);
+            numbers.add(number);
+        }
+        return numbers;
+    }
+
+    public static int inputBonus(){
+        System.out.println("보너스 번호를 입력해 주세요.\n");
+        String text = Console.readLine();
+        return getNumber(text);
+    }
+
+    private static int getNumber(String s) {
+        int number;
+        try {
+            number = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 제대로 입력해주세요.");
+        }
+        return number;
+    }
+
+
+
+}
