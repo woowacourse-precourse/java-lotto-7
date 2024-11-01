c15d7ddfcd5e1503f1385189329df19974326021
eunho <eunho1032@gmail.com>
2024-11-02 03:32:08 +0900

    feat: add new class

---
+++ src/main/java/lotto/View/Input.java
@@ -0,0 +1,18 @@
+package lotto.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String price() {
+        return (Console.readLine());
+    }
+
+    public String winNumbers() {
+        return (Console.readLine());
+    }
+
+    public String bonusNumber() {
+        return (Console.readLine());
+    }
+}
