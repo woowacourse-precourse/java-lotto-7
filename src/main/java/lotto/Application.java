638eda5bc3e44b7274f6bf18420ac6e17fd5cbc2
exena <exena01@gmail.com>
2024-11-01 18:33:03 +0900

    fix: 형변환 에러 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,26 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         String input_pay = readLine();
-        Integer number_of_times = Integer.getInteger(input_pay) / 1000;
+        Integer number_of_times = Integer.valueOf(input_pay) / 1000;
         System.out.println("\n" + number_of_times + "개를 구매했습니다.");
-        List<Lotto> lottos = new ArrayList<>();
+        List<Lotto> all_lottos = new ArrayList<>();
         for (int i = 0; i < number_of_times; i++) {
             List<Integer> marked_numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             marked_numbers.sort(Comparator.naturalOrder());
-            lottos.add(new Lotto(marked_numbers));
+            all_lottos.add(new Lotto(marked_numbers));
             String output_one = marked_numbers.stream()
                                               .map(String::valueOf)
                                               .collect(Collectors.joining(","));
@@ -36,5 +34,6 @@ public class Application {
         String input_bonus = readLine();
         Integer bonus_number = Integer.getInteger(input_bonus);
         System.out.println("\n당첨 통계\n---");
+
     }
 }
