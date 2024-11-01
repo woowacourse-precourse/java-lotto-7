ccb2cc1c4edaf26d1de088a0382cab48bc3be9c2
이준범 <jach1206@naver.com>
2024-11-01 21:38:16 +0900

    feat(service): 입력받은 당첨 번호를 파싱하기 위한 LottoParser 클래스 생성 및 예외 처리

---
+++ src/main/java/lotto/service/LottoParser.java
@@ -0,0 +1,26 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.exception.LottoErrorMessage;
+
+public class LottoParser {
+
+    public List<Integer> comma(String userInput) {
+        String[] splitInput = userInput.split(",");
+        List<Integer> result = new ArrayList<>();
+
+        for (String eachInput : splitInput) {
+            result.add(validate(eachInput));
+        }
+        return result;
+    }
+
+    private int validate(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoErrorMessage.SEPARATE_ONLY_COMMA.getMessage());
+        }
+    }
+}
