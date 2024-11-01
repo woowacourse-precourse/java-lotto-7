2dc448af98a37feb7d0023a8160a7969a5a1abc1
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 15:46:51 +0900

    feat(Utility): 당첨 번호를 파싱하는 기능을 구현하라

---
+++ src/main/java/lotto/Utility/WinningNumberParser.java
@@ -0,0 +1,33 @@
+package lotto.Utility;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WinningNumberParser {
+
+    public static List<Integer> parseWinningNumbers(String inputtedNumbers) {
+        return Stream.of(inputtedNumbers.split(","))
+                .map(String::trim)
+                .map(rawNumber -> {
+                    return stringToInt(rawNumber);
+                })
+                .collect(Collectors.toList());
+    }
+
+    private static int stringToInt(String rawNumber) {
+        try {
+            int number = Integer.parseInt(rawNumber);
+            validateNumber(number);
+            return number;
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 숫자를 입력하셨습니다.");
+        }
+    }
+
+    private static void validateNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("[ERROR] 0 이하의 수는 입력하실 수 없습니다.");
+        }
+    }
+}
--- src/test/java/lotto/UtilityTest.java
+++ src/test/java/lotto/UtilityTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import lotto.Utility.RandomNumberCreator;
+import lotto.Utility.WinningNumberParser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
