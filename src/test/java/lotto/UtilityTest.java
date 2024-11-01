6c911b37c334a7a1c98b33b2824c1ee6de3a8b93
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 14:16:54 +0900

    test(Utility): 6개의 무작위 수를 뽑는 테스트를 작성하라

---
+++ src/test/java/lotto/UtilityTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class UtilityTest {
+    @Test
+    public void 랜덤한_6개의_수를_뽑아서_List로_반환한다() {
+        List<Integer> randomNumbers = RandomNumberCreator.generateRandomNumbers();
+
+        assertThat(randomNumbers).hasSize(6);
+        assertThat(randomNumbers).allMatch(number -> 1 <= number && number <= 45);
+    }
+}
