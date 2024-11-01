14273127deff87691a57054dacc75c7bb87ea27a
yyysolhhh <solbaram37@gmail.com>
2024-11-01 21:39:56 +0900

    feat(Output): 메시지 출력 기능 추가
    printMessage
    - 문자열을 출력
    
    printLottoNumbers
    - 당첨 번호 출력
    
    printWinningStatistics
    - 당첨 통계 출력

---
+++ src/main/java/lotto/io/Output.java
@@ -0,0 +1,35 @@
+package lotto.io;
+
+import static lotto.io.InstructionMessages.NUMBER_OF_PURCHASE;
+import static lotto.io.InstructionMessages.WINNING_STATISTICS;
+
+import java.util.List;
+import java.util.Set;
+
+public class Output {
+    private static final String DIVIDER = "---";
+    private static final String AMOUNT_OF_MATCH = "개 일치";
+    private static final String MATCH_BONUS = "보너스 볼 일치";
+    private static final String UNIT = "개";
+    private static final String CURRENCY = "원";
+
+    public static void printLottoNumbers(List<Set<Integer>> lottoNumbers, int amount) {
+        printMessage(amount + NUMBER_OF_PURCHASE.getMessage());
+        for (Set<Integer> lottoNumber : lottoNumbers) {
+//            printMessage(String.valueOf(lottoNumber));
+            printMessage(lottoNumber.toString());
+        }
+    }
+
+    public static void printWinningStatistics(List<Integer> amountOfMatches) { // TODO 컬렉션 종류
+        printMessage(WINNING_STATISTICS.getMessage());
+        for (Integer amountOfMatch : amountOfMatches) {
+
+        }
+
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
