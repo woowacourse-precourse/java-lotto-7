d5ee71c79729f88891812ee730625335cfb3caca
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-02 01:31:52 +0900

    style: 코드 컨밴션 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -26,9 +26,9 @@ public class LottoController {
      * Error는 사용자의 입력이 동반 된 작업에만 발생할 수 있습니다.
      * 따라서, ErrorCatcher는 사용자의 입력을 받는 경우에만 적용됩니다.
      */
-    public void play(){
+    public void play() {
         int inputPrice = ErrorCatcher(inputManger::getInputPrice);
-        int lottoBuyAmount = inputPrice/ LottoRule.LOTTO_PRICE.getInstance();
+        int lottoBuyAmount = inputPrice / LottoRule.LOTTO_PRICE.getInstance();
 
         lottoMachine.buyNumberOfLottos(lottoBuyAmount);
         LottosDto lottosDto = lottoMachine.getLottos();
@@ -37,7 +37,7 @@ public class LottoController {
         Lotto winningComponents = ErrorCatcher(inputManger::getInputWinningComponent);
         BonusComponent bonusComponent = ErrorCatcher(() -> inputManger.getInputBonusComponent(winningComponents));
 
-        ScoreDto scoreDto = lottoMachine.getScore(winningComponents,bonusComponent);
+        ScoreDto scoreDto = lottoMachine.getScore(winningComponents, bonusComponent);
         outputView.printScoreResult(scoreDto);
     }
 
