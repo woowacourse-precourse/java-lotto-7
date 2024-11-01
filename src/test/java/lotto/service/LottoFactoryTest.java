d67109f16260e48ed8781fee31d94609b95a0cab
chaerish <ccyy8432@naver.com>
2024-11-01 20:14:42 +0900

    test(LottoFactoryTest): 1부터 45사이의 랜덤 값으로 로또 생성 로직 테스트 추가

---
+++ src/test/java/lotto/service/LottoFactoryTest.java
@@ -0,0 +1,24 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.Lotto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoFactoryTest {
+    private LottoFactory lottoFactory;
+
+    @BeforeEach
+    void setUp() {
+        lottoFactory = new LottoFactory();
+    }
+
+    @Test
+    void 새로운_로또를_생성한다() {
+        Lotto lotto = lottoFactory.createLotto();
+        assertThat(lotto).isInstanceOf(Lotto.class);
+    }
+
+
+}
