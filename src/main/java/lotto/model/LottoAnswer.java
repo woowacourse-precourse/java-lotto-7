ed695ccd71f8711786ff2b1ec05211aa50803b82
sanghee0820 <lsh277604@gmail.com>
2024-11-01 17:03:34 +0900

    feat: 로또 당첨 번호 생성 기능 추가

---
+++ src/main/java/lotto/model/LottoAnswer.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import java.util.List;
+import lotto.exception.LottoErrorMessage;
+
+public class LottoAnswer extends Lotto {
+    private final int bonusNumber;
+
+    public LottoAnswer(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+        validateBonusNumberDuplicated(bonusNumber);
+    }
+
+    private void validateBonusNumberDuplicated(int bonusNumber) {
+        var numbers = super.getNumbers();
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoErrorMessage.LOTTO_BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
+        }
+    }
+
+    private void validateBonusNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(LottoErrorMessage.LOTTO_BONUS_NUMBER_RANGE_ERROR.getMessage());
+        }
+    }
+}
