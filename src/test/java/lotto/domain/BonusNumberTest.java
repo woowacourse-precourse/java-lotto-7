9adf981b4045ad6439b3843dcfb98d882271542e
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-01 23:41:48 +0900

    refactor(BonusNumberTest): 테스트 코드에서 불필요한 인자를 빈 리스트로 대체
    from 정적 팩터리 메서드에서 of 정적 팩터라 메서드로 변경

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,7 +18,7 @@ public class BonusNumberTest {
     @ValueSource(strings = {"\n", "", "\t", "\r\n"})
     @DisplayName("보너스 번호가 비어있으면 예외가 발생한다.")
     void validateWinningNumbersIsNotEmpty(String condition) {
-        assertThatThrownBy(() -> BonusNumber.from(condition))
+        assertThatThrownBy(() -> BonusNumber.of(Collections.emptyList(), condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 비어있을 수 없습니다.");
     }
@@ -26,7 +27,7 @@ public class BonusNumberTest {
     @ValueSource(strings = {"a", "-", ";"})
     @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다.")
     void validateBonusNumberIsNumeric(String condition) {
-        assertThatThrownBy(() -> BonusNumber.from(condition))
+        assertThatThrownBy(() -> BonusNumber.of(Collections.emptyList(), condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
     }
@@ -35,7 +36,7 @@ public class BonusNumberTest {
     @ValueSource(strings = {"-1", "0", "46"})
     @DisplayName("보너스 번호가 1~45 사이가 아니면 예외가 발생한다.")
     void validateBonusNumberInRange(String condition) {
-        assertThatThrownBy(() -> BonusNumber.from(condition))
+        assertThatThrownBy(() -> BonusNumber.of(Collections.emptyList(), condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 보너스 번호는 1~45 사이의 숫자만 입력 가능합니다.");
     }
