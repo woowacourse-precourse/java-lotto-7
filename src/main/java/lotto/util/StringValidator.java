30fc7fd613c67164ca628ea883780135e46dbf4c
plan11plan <tjtfy7028@gmail.com>
2024-11-01 19:51:11 +0900

    refactor: StringValidator 생성자 접근 방식 수정

--- src/main/java/lotto/util/StringValidator.java
+++ src/main/java/lotto/util/StringValidator.java
@@ -2,6 +2,9 @@ package lotto.util;
 
 public class StringValidator {
 
+    private StringValidator() {
+    }
+
     public static void validateNotBlank(String value) {
         if (value == null || value.trim().isEmpty()) {
             throw new IllegalArgumentException("문자열은 null이거나 공백일 수 없습니다.");
