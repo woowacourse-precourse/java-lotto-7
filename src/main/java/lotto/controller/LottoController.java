83ed3f1f818056173a63caaa97362e86cb8e2e3c
Beomseok Noh <prodigy0831@naver.com>
2024-11-01 12:14:54 +0900

    feat(LottoController): 구매금액 Input로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,17 @@
 package lotto.controller;
 
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
     public void run() {
         int attemptCount = getAttemptCount();
     }
 
     private int getAttemptCount() {
+        OutputView.printPurchaseAmountInputMessage();
+        String purchaseAmount = InputView.UserInput();
         return 0;
     }
 }
