9c5c969ceed251203c14e48cf3185c4044720f50
MinJae00 <exceedmin@gmail.com>
2024-11-02 00:05:33 +0900

    feat: 랜덤 로또 번호 출력 OutputView 기능 완성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,13 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import java.util.List;
+
+public class OutputView {
+    public void result(List<List<Integer>> list){
+        System.out.println();
+        System.out.println(list.size() + "개를 구매했습니다.");
+        for(List<Integer> l : list){
+            System.out.println(l);
+        }
+    }
 }
