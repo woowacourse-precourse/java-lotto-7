18bbadc37fded6d2834dde7922a9090ec2bd0abe
MinJae00 <exceedmin@gmail.com>
2024-11-02 00:19:20 +0900

    feat: View와 Service 사이에서 데이터를 전달하는 LottoController 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,26 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.service.DecideQuantityOfLotto;
+import lotto.service.GenerateLottoNumberManager;
+import lotto.service.WinningManager;
+import lotto.view.OutputView;
+import java.util.List;
+public class LottoController {
+    OutputView outputView = new OutputView();
+    WinningManager winning = new WinningManager();
+
+    public void createLottoNumber(int price){
+        int num = new DecideQuantityOfLotto().purchaseLottoTickets(price);
+        List<List<Integer>> list = new GenerateLottoNumberManager().generate(num);
+        outputView.result(list);
+    }
+
+    public void createWinningNumber(String s){
+        winning.generateWinningNumber(s);
+    }
+
+    public void calculateCorrect(){
+
+    }
+
 }
