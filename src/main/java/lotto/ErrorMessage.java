0bdd83ba7d920613577dafee4668d63e5f200e62
leesh7807 <leesh7807@gmail.com>
2024-11-01 14:33:45 +0900

    test(Lotto): add test about out of range

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto;
 public enum ErrorMessage {
     NOT_A_NUMBER("숫자를 입력해야 합니다."),
     OUT_OF_RANGE("%d ~ %d 사이의 숫자만 입력할 수 있습니다."),
-    INVALID_DELIMITER("'%s'를 사용하여 숫자를 구분해야 합니다.");
+    INVALID_DELIMITER("'%s'를 사용하여 숫자를 구분해야 합니다."),
+
+    INVALID_SIZE_LOTTO("로또 번호는 6개여야 합니다."),
+    DUPLICATED_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다.");
 
     private final String message;
 
