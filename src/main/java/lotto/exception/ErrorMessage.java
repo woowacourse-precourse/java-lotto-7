58f7a7d22e5ad90fb4a572766a79335f287d5ea6
wogus <ind07152@naver.com>
2024-11-01 13:00:44 +0900

    feat(ErrorMessage): 로또 번호 중복 메시지 추가
    ErrorMessage에 로또 번호 중복 메시지를 추가한다.

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.exception;
 
 public enum ErrorMessage {
-    INVALID_LOTTO_COUNTS("로또 번호는 6개여야 합니다.");
+    INVALID_LOTTO_COUNTS("로또 번호는 6개여야 합니다."),
+    INVALID_LOTTO_DUPLICATE("로또 번호는 중복될 수 없습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private String message;
