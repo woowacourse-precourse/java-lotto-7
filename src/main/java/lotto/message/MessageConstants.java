7c7a7c181bc400412fb9b57b67a8ce6778e4abff
giwoong <dev.choi0409@gmail.com>
2024-11-01 16:40:14 +0900

    refactor: 출력 메시지 상수 분리

--- src/main/java/lotto/message/MessageConstants.java
+++ src/main/java/lotto/message/MessageConstants.java
@@ -5,4 +5,6 @@ public class MessageConstants {
     public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
+    public static final String OUTPUT_PURCHASE_COUNT_MESSAGE = "\n%d개를 구매했습니다.\n";
+
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,17 @@
 package lotto.view;
 
+import static lotto.message.MessageConstants.OUTPUT_PURCHASE_COUNT_MESSAGE;
+
 import java.util.List;
 
 public class OutputView {
 
     public void printLottoPurchaseCountMessage(int lottoCount) {
-        System.out.printf("\n%d개를 구매했습니다.\n", lottoCount);
+        System.out.printf(OUTPUT_PURCHASE_COUNT_MESSAGE, lottoCount);
     }
 
     public void printLottoNumbers(List<Integer> numbers) {
         System.out.println(numbers);
     }
-    
+
 }
