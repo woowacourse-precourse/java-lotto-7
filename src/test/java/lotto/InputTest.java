5ba9e8647d13d08b3bdfb8c0645324f2ccb982ba
bronzehyeon <bronzehyeon@gmail.com>
2024-11-01 17:25:53 +0900

    refactor : inputTest 메서드 명 변경
    한글로 변경하여 이해 쉽도록 변경

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -12,23 +12,21 @@ public class InputTest {
 
     @Test
     @DisplayName("1000의 배수가 아닌 금액이 입력되면 예외가 발생한다.")
-    void invalid_money_test1() {
+    void 천의_배수가_아닌_금액_입력_시_예외_발생() {
         assertThatThrownBy(() -> Input.parseMoney("1001"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("0이하의 금액이 입력되면 예외가 발생한다.")
-    void invalid_money_test2() {
-        assertThatThrownBy(() -> Input.parseMoney("0"))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("0이하의 금액이 입력되면 예외가 발생한다.(음수 포함)")
+    void 숫자가_아닌_문자_입력_시_예외_발생() {
         assertThatThrownBy(() -> Input.parseMoney("-1000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("제대로된 입력 검증 테스트")
-    void valid_input_test() {
+    void 제대로된_금액_입력_시_int_반환() {
         assertThat(Input.parseMoney("1000")).isEqualTo(1000);
     }
 }
