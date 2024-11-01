ef9a82ef4b91ce5b6a94bd6e59f5a355184fda54
mipangg <morganjeong119@gmail.com>
2024-11-02 01:22:52 +0900

    feat(Application): create LottoMedel, LottoView, LottoController instances and call run() method in controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoModel;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+        LottoModel model = new LottoModel();
+        LottoController controller = new LottoController(view, model);
+
+        controller.run();
     }
 }
