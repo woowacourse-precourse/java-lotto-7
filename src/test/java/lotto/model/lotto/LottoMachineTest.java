b170fcc5da766b62eee348e21be88510b23c9da4
kgy1008 <gayeon1008@g.skku.edu>
2024-11-01 12:55:18 +0900

    test: test for generation of lotto number

---
+++ src/test/java/lotto/model/lotto/LottoMachineTest.java
@@ -0,0 +1,20 @@
+package lotto.model.lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoMachineTest {
+    private final LottoMachine lottoMachine = new LottoMachine();
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 5})
+    @DisplayName("입력된 개수만큼 로또 생성 테스트")
+    void executeLottoMachine(final int input) {
+        Lottos lottos = lottoMachine.execute(input);
+        
+        assertThat(lottos.getLottos().size()).isEqualTo(input);
+    }
+}
