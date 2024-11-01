24534c39ecfc2f6599092b21df48a49d4d738ad4
Sinorang <sinholang@naver.com>
2024-11-01 22:59:38 +0900

    test: 당첨 번호 입력값 파싱 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -2,6 +2,8 @@ package lotto.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoServiceTest {
@@ -15,5 +17,15 @@ class LottoServiceTest {
         int lottoCount = lottoService.calculateLottoCount(purchaseAmount);
 
         assertThat(lottoCount).isEqualTo(expectedLottoCount);
+    }
+
+    @Test
+    void 당첨_번호_입력값_파싱_테스트() {
+        String winningNumbersInput = "1, 2, 3, 4, 5, 6";
+        List<Integer> expectedWinningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        List<Integer> parsedWinningNumbers = lottoService.parseWinningNumbers(winningNumbersInput);
+
+        assertThat(parsedWinningNumbers).isEqualTo(expectedWinningNumbers);
     }
 }
