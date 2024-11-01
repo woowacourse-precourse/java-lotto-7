9c473485e9ec97cb1c36966af2d7daf21764d72f
seophohoho <seop0937@gmail.com>
2024-11-01 23:07:38 +0900

    feat(BonusNumber): 보너스 입력 유효성 검사 메소드 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,2 +1,25 @@
-package lotto.model;public class BonusNumber {
+package lotto.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BonusNumber {
+    private final String BONUS_NUMBER_PATTERN = "[1-9][0-9]{0,1}";
+
+    public void validate(String input) {
+        Pattern patternBonusNumber = Pattern.compile(BONUS_NUMBER_PATTERN);
+        Matcher matcher = patternBonusNumber.matcher(input);
+
+        if (!matcher.matches()) {
+            reject();
+        }
+
+        if (!(Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 45)) {
+            reject();
+        }
+    }
+
+    public void reject() {
+        throw new IllegalArgumentException();
+    }
 }
