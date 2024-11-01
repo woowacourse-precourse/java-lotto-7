c2a7ac2f126a04f659d21b391a31840331210002
menzzi <minmin3927@naver.com>
2024-11-01 16:13:41 +0900

    feat(view) : OutputView - printProfit 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,4 +16,8 @@ public class OutputView {
         DecimalFormat df = new DecimalFormat("###,###");
         System.out.println(matchCount + "개 일치 (" + df.format(prizeMoney) + "원) -" + winningCount + "개");
     }
+
+    public void printProfit(String profit){
+        System.out.println("총 수익률은 " + profit + "%입니다.");
+    }
 }
