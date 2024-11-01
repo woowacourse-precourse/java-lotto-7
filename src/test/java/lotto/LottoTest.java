cad0a264ca66f47c90a5af1d19ee5877007d62cc
wingyou <winwing.you@gmail.com>
2024-11-02 01:03:17 +0900

    test: Lotto calculates winning prize

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,15 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +23,34 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void create_a_valid_lotto() {
+        new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    void calculate_winning_prize() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonus = 7;
+        Lotto first = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto second = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto third = new Lotto(List.of(1, 2, 3, 4, 5, 10));
+        Lotto forth = new Lotto(List.of(1, 2, 3, 4, 10, 11));
+        Lotto fifth = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+        Lotto none = new Lotto(List.of(1, 2, 10, 11, 12, 13));
+
+        int firstPrize = first.getPrize(winningNumbers, bonus);
+        int secondPrize = second.getPrize(winningNumbers, bonus);
+        int thirdPrize = third.getPrize(winningNumbers, bonus);
+        int forthPrize = forth.getPrize(winningNumbers, bonus);
+        int fifthPrize = fifth.getPrize(winningNumbers, bonus);
+        int nonePrize = none.getPrize(winningNumbers, bonus);
+
+        assertThat(firstPrize).isEqualTo(2_000_000_000);
+        assertThat(secondPrize).isEqualTo(30_000_000);
+        assertThat(thirdPrize).isEqualTo(1_500_000);
+        assertThat(forthPrize).isEqualTo(50_000);
+        assertThat(fifthPrize).isEqualTo(5_000);
+        assertThat(nonePrize).isEqualTo(0);
+    }
 }
