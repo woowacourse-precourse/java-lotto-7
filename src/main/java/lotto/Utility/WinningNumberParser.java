e7dee297d2553d0c907d20bbcae15f65f2ba17b4
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 15:52:33 +0900

    feat(Utility): 당첨 번호가 6개가 아닐 경우 예외를 던져라

--- src/main/java/lotto/Utility/WinningNumberParser.java
+++ src/main/java/lotto/Utility/WinningNumberParser.java
@@ -7,12 +7,15 @@ import java.util.stream.Stream;
 public class WinningNumberParser {
 
     public static List<Integer> parseWinningNumbers(String inputtedNumbers) {
-        return Stream.of(inputtedNumbers.split(","))
+        List<Integer> parsedNumbers = Stream.of(inputtedNumbers.split(","))
                 .map(String::trim)
                 .map(rawNumber -> {
                     return stringToInt(rawNumber);
                 })
                 .collect(Collectors.toList());
+
+        validateLength(parsedNumbers);
+        return parsedNumbers;
     }
 
     private static int stringToInt(String rawNumber) {
@@ -28,6 +31,12 @@ public class WinningNumberParser {
     private static void validateNumber(int number) {
         if (number <= 0) {
             throw new IllegalArgumentException("[ERROR] 0 이하의 수는 입력하실 수 없습니다.");
+        }
+    }
+
+    private static void validateLength(List<Integer> parsedNumbers) {
+        if (parsedNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
         }
     }
 }
