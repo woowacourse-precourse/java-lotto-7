2fdcc4eab0d0df393ab16aad3708dcb097592b33
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 20:27:36 +0900

    feat(OutputView): 구입한 개수를 출력하는 기능을 구현하라

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,22 @@
 package lotto.controller;
 
+import lotto.model.PurchaseCost;
+import lotto.utility.PurchaseCostParser;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
     public void purchaseLotto() {
+        int inputtedCost = inputPurchaseCost();
+        PurchaseCost purchaseCost = new PurchaseCost(inputtedCost);
+
+        int purchasedLottoCount = purchaseCost.calculateBuyableLottoCount();
+        OutputView.outputPurchasedLottoCount(purchasedLottoCount);
+    }
+
+    public int inputPurchaseCost() {
+        String rawPurchaseCost = InputView.inputPurchaseCost();
+        return PurchaseCostParser.parseToInteger(rawPurchaseCost);
     }
 }
