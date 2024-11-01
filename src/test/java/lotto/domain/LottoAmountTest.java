460e27da919aa8ef91f3bad85bd20aa62da92469
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 19:00:52 +0900

    test(LottoAmountTest):구입 금액 입력 테스트

--- src/test/java/lotto/domain/LottoAmountTest.java
+++ src/test/java/lotto/domain/LottoAmountTest.java
@@ -1,2 +1,42 @@
-package lotto.domain;public class LottoAmountTest {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import org.junit.jupiter.api.Test;
+
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoAmountTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 예외_테스트_잘못된_입력() {
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_1000원_단위() {
+        assertSimpleTest(() -> {
+            runException("999");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_음수() {
+        assertSimpleTest(() -> {
+            runException("-1000");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
