eb7bc87dad18f11b696d7b13105fd24ee572b625
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-02 03:37:00 +0900

    feat : 랜덤 라이브러리 사용하여 로또 넘버 생성
    - 범위와 갯수를 지정하여 6개의 수를 오름차순으로 정렬하여 로또 숫자 리스트 반환

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int PICK_COUNT = 6;
+
+    public static List<Integer> generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT);
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
+    }
+}
