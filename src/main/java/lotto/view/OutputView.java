699ca6607b196a443abfb5c7ee8b1e44bf261f24
Yehyeok Bang <qkddpgur318@gmail.com>
2024-11-01 21:17:22 +0900

    feat: 당첨 통계 일치 메시지 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
 
     private static final String PURCHASED_QUANTITY_MESSAGE = "\n%d개를 구매했습니다.\n";
     private static final String WINNING_RESULT_MESSAGE = "\n당첨 통계\n---\n%s";
+    private static final String TOTAL_YIELD_MESSAGE = "총 수익률은 %.1f%%입니다.%n";
 
     public void printPurchasedQuantity(int purchasedQuantity) {
         System.out.printf(PURCHASED_QUANTITY_MESSAGE, purchasedQuantity);
@@ -26,11 +27,11 @@ public class OutputView {
     }
 
     private String getWinningStatistics(WinningResult winningResult) {
-        return String.format("3개 일치 (5,000원) - %d개%n", winningResult.threeMatchesCount())
-                + String.format("4개 일치 (50,000원) - %d개%n", winningResult.fourMatchesCount())
-                + String.format("5개 일치 (1,500,000원) - %d개%n", winningResult.fiveMatchesCount())
-                + String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", winningResult.fiveWithBonusCount())
-                + String.format("6개 일치 (2,000,000,000원) - %d개%n", winningResult.sixMatchesCount())
-                + String.format("총 수익률은 %.1f%%입니다.%n", winningResult.totalYield());
+        return WinningMatchMessage.FIFTH.getMessage(winningResult.threeMatchesCount())
+                + WinningMatchMessage.FOURTH.getMessage(winningResult.fourMatchesCount())
+                + WinningMatchMessage.THIRD.getMessage(winningResult.fiveMatchesCount())
+                + WinningMatchMessage.SECOND.getMessage(winningResult.fiveWithBonusCount())
+                + WinningMatchMessage.FIRST.getMessage(winningResult.sixMatchesCount())
+                + String.format(TOTAL_YIELD_MESSAGE, winningResult.totalYield());
     }
 }
