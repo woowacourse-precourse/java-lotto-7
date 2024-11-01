14a85fe68cc9ee5328fccbb3ffc588417d8cbd05
Dohun Kim <dohun8758@naver.com>
2024-11-02 01:18:00 +0900

    feat: 출력을 담당하는 OutputView 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class OutputView {
+
+    public void showMoneyInputComments() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void showLottoWinningNumberInputComments() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void showLottoBonusNumberInputComments() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
