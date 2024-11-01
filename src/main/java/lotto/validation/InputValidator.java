c2c45be44a91383c86758bbdeb41030e07939300
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-01 17:30:16 +0900

    refactor(InputValidator):입력값 검증 클래스 분리

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -3,13 +3,19 @@ package lotto.validation;
 import static lotto.util.ExceptionMessage.INVALID_AMOUNT_UNIT;
 
 public class InputValidator {
-    public static void isZero(int money) {
+    public int validate(int value) {
+        isZero(value);
+        idValidateAmountUnit(value);
+        return value;
+    }
+
+    private void isZero(int money) {
         if (money <= 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_UNIT.format());
         }
     }
 
-    public static void idValidateAmountUnit(int money) {
+    private void idValidateAmountUnit(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_UNIT.format());
         }
