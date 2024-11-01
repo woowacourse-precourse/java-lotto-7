5fdf5bdeb1c20ce759b6c0ddcbe08ce03b4a8c77
minSsan <msp1125@hanyang.ac.kr>
2024-11-01 21:34:32 +0900

    fix(ManageLottoRequest): WinningLotto 구현체 오타 - 추상화에 의존하도록 수정

--- src/main/java/lotto/application/dto/request/ManageLottoRequest.java
+++ src/main/java/lotto/application/dto/request/ManageLottoRequest.java
@@ -2,9 +2,9 @@ package lotto.application.dto.request;
 
 import lotto.domain.cost.Cost;
 import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.WinningLottoImpl;
+import lotto.domain.lotto.WinningLotto;
 
 import java.util.List;
 
-public record ManageLottoRequest(Cost cost, List<Lotto> lottos, WinningLottoImpl winningLotto) implements Request {
+public record ManageLottoRequest(Cost cost, List<Lotto> lottos, WinningLotto winningLotto) implements Request {
 }
