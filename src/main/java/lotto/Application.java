865b7727c92646e46363587f3736a1e3ca5f0d08
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-01 23:02:36 +0900

    feat: Application 의존성 주입 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,28 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+import lotto.constants.collection.ScoreSystem;
+import lotto.constants.collection.ScoreSystemReward;
+import lotto.constants.value.LottoRule;
+import lotto.controller.LottoController;
+import lotto.controller.LottoInputManger;
+import lotto.controller.NumberLottoInputManger;
+import lotto.domain.LottoMachine;
+import lotto.domain.factory.NumberLottoFactory;
+import lotto.domain.score.Score;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        List<Integer> test = new ArrayList<>(List.of(1));
+        LottoController lottoController = new LottoController(
+                new NumberLottoInputManger(InputView.getInstance()),
+                new LottoMachine(
+                        new NumberLottoFactory(LottoRule.MINIMUM_NUMBER_RANGE.getInstance(),
+                                               LottoRule.MAXIMUM_NUMBER_RANGE.getInstance(),
+                                               LottoRule.COMBINATION_LENGTH.getInstance()),
+                        new Score(ScoreSystem.DEFAULT.getInstance(),
+                                  ScoreSystemReward.DEFAULT.getInstance())),
+                OutputView.getInstance()
+        );
     }
 }
