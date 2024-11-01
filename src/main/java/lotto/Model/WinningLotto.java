bbfc16460dbfbfe782301610bba0f8e7ae613641
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-01 17:55:53 +0900

    feat(WinningLotto): 당첨 번호와 발행 번호 일치 개수 계산 기능 구현

---
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -0,0 +1,24 @@
+package lotto.Model;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final List<Integer> winNumbers;
+    private final int bonus;
+
+    public WinningLotto(List<Integer> winNumbers, int bonus){
+        this.winNumbers = winNumbers;
+        this.bonus = bonus;
+    }
+
+    private int countMatchingNumbers(Lotto lotto){
+        int count = 0;
+        for(int number : lotto.getNumbers()){
+            if(winNumbers.contains(number)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
