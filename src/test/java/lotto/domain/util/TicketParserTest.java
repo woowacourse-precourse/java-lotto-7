423c84e5adca49c6a7b04fd37d39f3354b359d37
moonbinchoi <whsahd12@gmail.com>
2024-11-01 17:51:06 +0900

    test(TicketParser): TICKET_PRICE가 Lotto에서 관리되도록 수정

--- src/test/java/lotto/domain/util/TicketParserTest.java
+++ src/test/java/lotto/domain/util/TicketParserTest.java
@@ -2,6 +2,7 @@ package lotto.domain.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ class TicketParserTest {
     void 변환_성공() {
         //given
         String input = "30000";
-        int expected = Integer.parseInt(input) / TicketParser.TICKET_PRICE;
+        int expected = Integer.parseInt(input) / Lotto.TICKET_PRICE;
 
         //when
         Integer parsed = TicketParser.parse(input);
