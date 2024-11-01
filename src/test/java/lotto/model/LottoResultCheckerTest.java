3bbbc73577d4fa19aefbb8db228c6d0be6651750
LeeJiWon <dlwldnjs1009@gmail.com>
2024-11-01 11:20:25 +0900

    test: 당첨 번호와 발행된 로또 번호 2등~등수없음까지 테스트 작성

--- src/test/java/lotto/model/LottoResultCheckerTest.java
+++ src/test/java/lotto/model/LottoResultCheckerTest.java
@@ -33,4 +33,64 @@ public class LottoResultCheckerTest {
 
         assertThat(matchCount).isEqualTo(Ranking.FIRST);
     }
+
+    @Test
+    @DisplayName("당첨 번호와 발행된 로또 번호 일치하는지 비교, 2등, 보너스 번호 포함")
+    void lottoNumberMatchTest2() {
+        Set<Integer> winningLotto = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> issuedLotto = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+
+        Ranking matchCount = LottoResultChecker.matchCount(winningLotto, issuedLotto, bonusNumber);
+
+        assertThat(matchCount).isEqualTo(Ranking.SECOND);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 발행된 로또 번호 일치하는지 비교, 3등, 보너스 번호 미포함")
+    void lottoNumberMatchTest3() {
+        Set<Integer> winningLotto = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> issuedLotto = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+
+        Ranking matchCount = LottoResultChecker.matchCount(winningLotto, issuedLotto, bonusNumber);
+
+        assertThat(matchCount).isEqualTo(Ranking.THIRD);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 발행된 로또 번호 일치하는지 비교, 4등")
+    void lottoNumberMatchTest4() {
+        Set<Integer> winningLotto = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> issuedLotto = List.of(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
+
+        Ranking matchCount = LottoResultChecker.matchCount(winningLotto, issuedLotto, bonusNumber);
+
+        assertThat(matchCount).isEqualTo(Ranking.FOURTH);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 발행된 로또 번호 일치하는지 비교, 5등")
+    void lottoNumberMatchTest5() {
+        Set<Integer> winningLotto = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> issuedLotto = List.of(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+
+        Ranking matchCount = LottoResultChecker.matchCount(winningLotto, issuedLotto, bonusNumber);
+
+        assertThat(matchCount).isEqualTo(Ranking.FIFTH);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 발행된 로또 번호 일치하는지 비교, 등수없음")
+    void lottoNumberMatchTest6() {
+        Set<Integer> winningLotto = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> issuedLotto = List.of(new Lotto(List.of(1, 2, 8, 9, 10, 11)));
+
+        Ranking matchCount = LottoResultChecker.matchCount(winningLotto, issuedLotto, bonusNumber);
+
+        assertThat(matchCount).isEqualTo(Ranking.NONE);
+    }
 }
