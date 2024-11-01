7a1a014853d1d4a98e2a5990f4c89d00cd9b7b17
Chaeyoung714 <202102760@hufs.ac.kr>
2024-11-01 14:05:12 +0900

    feat : 1.1 로또 구입 금액 입력 기능 구현
    test : 예외 흐름에 관한 테스트 진행, 통과
    Console.Input, System.out에 관한 테스트는 하지 않았음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        validateInputInteger(purchaseAmount);
+    }
 
+    public static void validateInputInteger(String inputInteger) {
+        if (inputInteger == null
+                || inputInteger.isBlank()
+                || !inputInteger.matches("^-?\\d+$")) {
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요.");
+        }
     }
 }
--- src/test/java/lotto/AdditionalApplicationTest.java
+++ src/test/java/lotto/AdditionalApplicationTest.java
@@ -1,5 +1,41 @@
 package lotto;
 
-public class AdditionalApplicationTest {
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AdditionalApplicationTest {
+    @Test
+    void shouldValidateInputInteger() {
+        String testString = "50";
+
+        assertThatCode(() -> Application.validateInputInteger(testString))
+                .doesNotThrowAnyException();
+
+    }
+
+    @Test
+    void failIfInputIntegerIsNull() {
+        String testString = null;
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+                .withMessage("[ERROR] 정수를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void failIfInputIntegerIsBlank(String testString) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+                .withMessage("[ERROR] 정수를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-20.2, -1.0, 0.5, 2.0, 2.5, 3.24321"})
+    void failIfInputIntegerIsNotInteger(String testString) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+                .withMessage("[ERROR] 정수를 입력해주세요.");
+    }
 }
