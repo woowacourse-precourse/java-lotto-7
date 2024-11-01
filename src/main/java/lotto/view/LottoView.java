34fc2d0a6352bc83effceb920f4dce00c63aecf4
mipangg <morganjeong119@gmail.com>
2024-11-02 02:07:13 +0900

    feat(LottoView): print lotto count and call method to print lotto

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,16 +1,25 @@
 package lotto.view;
 
 import lotto.model.InputModel;
+import lotto.model.OutputModel;
+import lotto.model.UserLotto;
 
 public class LottoView {
     InputModel inputModel;
+    OutputModel outputModel;
 
     public LottoView() {
         inputModel = new InputModel();
+        outputModel = new OutputModel();
     }
 
     public int inputPriceView() {
         System.out.println("구입금액을 입력해 주세요.");
         return (inputModel.getPrice())/1000;
+    }
+
+    public void outputUserLottoView(UserLotto userLotto) {
+        System.out.println(userLotto.getNumberOfLotto()+"개를 구매했습니다.");
+        outputModel.showUserLotto(userLotto);
     }
 }
