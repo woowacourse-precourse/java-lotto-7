c34fc413d17bee21dafb08d9875283a9e8beacd9
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:05:28 +0900

    feat(PurchaseCost): 0 이하의 구입금액을 입력받으면 예외를 던져라

--- src/main/java/lotto/Model/PurchaseCost.java
+++ src/main/java/lotto/Model/PurchaseCost.java
@@ -3,7 +3,14 @@ package lotto.Model;
 public class PurchaseCost {
     private int purchaseCost;
 
-    public PurchaseCost(int purchaseCost) {
-        this.purchaseCost = purchaseCost;
+    public PurchaseCost(int inputtedCost) {
+        validateUnderZero(inputtedCost);
+        this.purchaseCost = inputtedCost;
+    }
+
+    private void validateUnderZero(int inputtedCost) {
+        if (inputtedCost <= 0) {
+            throw new IllegalArgumentException("[ERROR] 0 이하의 수는 입력하실 수 없습니다.");
+        }
     }
 }
