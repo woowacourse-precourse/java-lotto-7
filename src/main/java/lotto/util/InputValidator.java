0f146dfb442b3dcf8d441afca52215f0d838cb58
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-01 16:57:25 +0900

    feat: 구입 금액 검증 기능 추가
    - 숫자가 아닌 입력이 들어올 경우 예외를 발생하는 기능 추가

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,8 +1,17 @@
 package lotto.util;
 
+import lotto.exception.InputErrorMessage;
+
 public class InputValidator {
 
-    public static void validateAmount(String validAmount) {
-        
+    public static void validateAmount(String input) {
+        validateNumericInput(input);
+    }
+
+
+    private static void validateNumericInput(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(InputErrorMessage.INVALID_NUMERIC_AMOUNT.getMessage());
+        }
     }
 }
