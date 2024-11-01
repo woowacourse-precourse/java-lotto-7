08db365a41c3f3e16b35bd1eb1783a327b672010
yooo <ghddbtl@gmail.com>
2024-11-01 22:39:34 +0900

    feat : create RegexUtils.java
    자주 사용하는 정규식을 만들어 문자열을 확인할 수 있도록 하였다.
    양수인지 확인, 스페이스 모두 제거

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -3,12 +3,13 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-  static String READ_PURCHASE_AMOUNT = request();
-  static String READ_WINNING_NUMBERS = request();
-  static String READ_BONUS_NUMBERS = request();
+    static String READ_PURCHASE_AMOUNT = request();
+    static String READ_WINNING_NUMBERS = request();
+    static String READ_BONUS_NUMBERS = request();
 
-  static String request() {
-    return Console.readLine();
-  }
+    static String request() {
+        return Console.readLine();
+
+    }
 
 }
