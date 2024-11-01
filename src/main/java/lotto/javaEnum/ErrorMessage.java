76c9ac5feb06cd4f5f9c5e7bea4f5f2e06f8b4d5
JunBe <wnsqowid0901@gmail.com>
2024-11-01 19:23:47 +0900

    feat: 에러 메시지 추가
    보너스 번호가 당첨 번호와 중복 될 경우 출력할 메시지를 추가했다.

--- src/main/java/lotto/javaEnum/ErrorMessage.java
+++ src/main/java/lotto/javaEnum/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     NEGATIVE_OR_ZERO("[ERROR] 0 이하의 수는 입력할 수 없습니다."),
     INVALID_INPUT("[ERROR] 문자열은 입력 불가합니다."),
     DUPLICATE_NUMBER("[ERROR] 숫자가 중복되면 안됩니다."),
+    DUPLICATE_WITH_WIN_NUMBER("[ERROR] 당첨 번호와 보너스 번호가 중복되면 안됩니다."),
     OUT_OF_RANGE("[ERROR] 1 ~ 45 중에서 입력할 수 있습니다."),
     INVALID_COUNT("[ERROR] 6개를 입력해야 합니다.");
 
