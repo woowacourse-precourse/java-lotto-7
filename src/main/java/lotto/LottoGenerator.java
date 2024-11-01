8740d5865b329fc77878ceb9377a38e47b8a9fc1
joshinhyeok <shtree07@naver.com>
2024-11-02 00:56:06 +0900

    refactor: LottoGenerator 클래스로 Generator 메서드 분리, 이동

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoGenerator {
+    public static Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                Lotto.LOTTO_NUMBER_MIN,
+                Lotto.LOTTO_NUMBER_MAX,
+                Lotto.LOTTO_SIZE
+        );
+        return new Lotto(numbers);
+    }
+}
