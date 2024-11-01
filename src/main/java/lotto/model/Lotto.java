080228896bead6a8b5835a27c36fb8e654240f0e
menzzi <minmin3927@naver.com>
2024-11-01 17:04:20 +0900

    fix : Lotto 클래스 예외 적용

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,7 @@
 package lotto.model;
 
 import java.util.List;
-import java.util.function.Predicate;
+import lotto.validator.Validator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,6 +15,8 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Validator.validateLottoDuplicate(numbers);
+        Validator.validateNumberCount(numbers);
     }
 
     // TODO: 추가 기능 구현
