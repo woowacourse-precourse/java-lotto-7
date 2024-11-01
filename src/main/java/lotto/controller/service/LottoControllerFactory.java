365afba94f303e0f6450fa8e619ba1a4236edbed
EHCTH <lees90896@gmail.com>
2024-11-01 19:39:07 +0900

    style(LottoContollerFactory): 메서드 정리

--- src/main/java/lotto/controller/service/LottoControllerFactory.java
+++ src/main/java/lotto/controller/service/LottoControllerFactory.java
@@ -4,10 +4,7 @@ import lotto.buyer.domain.Buyer;
 import lotto.buyer.domain.InsertMoneyService;
 import lotto.buyer.infrastructure.InsertPurchaseMoney;
 import lotto.controller.domain.LottoController;
-import lotto.lotto.domain.BonusNumberCreatorService;
-import lotto.lotto.domain.LottoMachine;
-import lotto.lotto.domain.PurchaseLottoTicketsService;
-import lotto.lotto.domain.WinningLottoCreatorService;
+import lotto.lotto.domain.*;
 import lotto.lotto.infrastructure.LottoCreator;
 import lotto.lotto.infrastructure.PurchaseLottoTickets;
 import lotto.money.domain.BenefitCreatorService;
@@ -28,40 +25,24 @@ import lotto.view.output.infrastructure.*;
 
 public class LottoControllerFactory {
     public static LottoController create() {
-        InsertMoneyService insertMoneyService = createInsertMoneyService();
-        Buyer buyer = createBuyer(insertMoneyService);
+        Buyer buyer = createBuyer();
         LottoMachine lottoMachine = createLottoMachine();
         BenefitCreatorService benefitCreator = createBenefitCreator();
         ResultViewService resultViewService = createResultViewService();
         return new LottoController(buyer, lottoMachine, benefitCreator, resultViewService);
     }
 
-    private static InsertMoneyService createInsertMoneyService() {
-        return new InsertPurchaseMoney(new MoneyHandler(new MoneyInput(new MoneyOutput())));
-    }
-
-    private static Buyer createBuyer(InsertMoneyService insertMoneyService) {
+    private static Buyer createBuyer() {
+        InsertMoneyService insertMoneyService = new InsertPurchaseMoney(new MoneyHandler(new MoneyInput(new MoneyOutput())));
         return new Buyer(insertMoneyService);
     }
 
-    private static WinningLottoCreator createWinningLottoCreator() {
-        return new WinningLottoCreator(new WinningLottoHandler(new WinningLottoInput(new WinningLottoOutput())));
-    }
-    private static BonusNumberCreator createBonusNumberCreator() {
-        return new BonusNumberCreator(new BonusNumberHandler(new BonusNumberInput(new BonusNumberOutput())));
-    }
-    private static PurchaseLottoTickets createLottoTicketsCreator() {
-        return new PurchaseLottoTickets(new RandomNumberGenerate());
-    }
-    private static LottoCreator createLottoCreator() {
-        PurchaseLottoTicketsService lottoTicketsCreator = createLottoTicketsCreator();
-        WinningLottoCreatorService winningLottoCreator = createWinningLottoCreator();
-        BonusNumberCreatorService bonusNumberCreator = createBonusNumberCreator();
-        return new LottoCreator(lottoTicketsCreator, winningLottoCreator, bonusNumberCreator);
-    }
     private static LottoMachine createLottoMachine() {
-        LottoCreator lottoCreator = createLottoCreator();
-        return new LottoMachine(new DivideThousandCalculator(), lottoCreator, new PurchaseOutput());
+        PurchaseLottoTicketsService lottoTicketsCreator = new PurchaseLottoTickets(new RandomNumberGenerate());
+        WinningLottoCreatorService winningLottoCreator = new WinningLottoCreator(new WinningLottoHandler(new WinningLottoInput(new WinningLottoOutput())));
+        BonusNumberCreatorService bonusNumberCreator = new BonusNumberCreator(new BonusNumberHandler(new BonusNumberInput(new BonusNumberOutput())));
+        LottoCreatorService lottoCreatorService = new LottoCreator(lottoTicketsCreator, winningLottoCreator, bonusNumberCreator);
+        return new LottoMachine(new DivideThousandCalculator(), lottoCreatorService, new PurchaseOutput());
     }
 
     private static ResultViewService createResultViewService() {
