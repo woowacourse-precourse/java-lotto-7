f460573caaab0c30ac275a0581b3dabfc3752f7a
jiffyin7 <jiffyin7@gmail.com>
2024-11-01 17:47:40 +0900

    feat(LottoCommand): `Lotto` 당첨 번호 유효성 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.command.validate.LottoCommand;
+import lotto.command.validate.PurchaseAmountCommand;
+import lotto.dto.PurchaseAmountUserInput;
+import lotto.dto.WinningLottoUserInput;
+import lotto.service.amount.AmountService;
+import lotto.service.lotto.LottoService;
+import lotto.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PurchaseAmountCommand purchaseAmountCommand = new PurchaseAmountCommand();
+        AmountService amountService = new AmountService(purchaseAmountCommand);
+        ConsoleView view = new ConsoleView();
+        PurchaseAmountUserInput userInput = (PurchaseAmountUserInput) view.promptInput(amountService.getValidateCommand());
+
+        LottoCommand lottoCommand = new LottoCommand();
+        LottoService lottoService = new LottoService(lottoCommand);
+        WinningLottoUserInput winningLottoUserInput = (WinningLottoUserInput) view.promptInput(lottoService.getLottoCommand());
+
+        int a = 0;
+
     }
 }
