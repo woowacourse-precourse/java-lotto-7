2cbd9f6c37fba7bb97dba6438764e2d49f3a9157
Changsu Park <gd12091@gmail.com>
2024-11-01 21:58:09 +0900

    feat: 보너스 번호를 포함하는 로또 도메인 구현

---
+++ src/main/java/lotto/domain/Guess.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import static lotto.constants.CommonConstants.LOTTO_NUMBER_MAX;
+import static lotto.constants.CommonConstants.LOTTO_NUMBER_MIN;
+
+import java.util.List;
+import lotto.enums.ErrorCode;
+import lotto.exception.CommonException;
+
+public class Guess extends Lotto{
+    private final int bonusNumber;
+
+    public Guess(List<Integer> numbers, int bonusNumber){
+        super(numbers);
+        validateBonusNumber(bonusNumber, numbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return this.bonusNumber;
+    }
+
+    public void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
+            throw new CommonException(ErrorCode.INVALID_NUMBER_RANGE);
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new CommonException(ErrorCode.DUPLICATE_BONUS_NUMBER);
+        }
+    }
+
+}
