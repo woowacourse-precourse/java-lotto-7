a8c0b7c930d1864df933da14f7f2958bdf016663
Changsu Park <gd12091@gmail.com>
2024-11-02 00:05:17 +0900

    fix: MoneyFormat 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,6 +10,8 @@ import lotto.domain.Lotto;
 import lotto.enums.Rank;
 
 public class OutputView {
+
+    private final NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
 
     public void printPurchasedLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
@@ -25,14 +29,14 @@ public class OutputView {
 
         Stream.of(Rank.values())
                 .filter(rank -> rank != Rank.NONE)
-                .forEach(rank -> System.out.printf("%s (%d원) - %d개\n",
+                .forEach(rank -> System.out.printf("%s (%s원) - %d개\n",
                         getMatchDescription(rank),
-                        rank.getAmount(),
+                        numberFormat.format(rank.getAmount()),
                         rankCount.getOrDefault(rank, 0L)));
     }
 
     public void printProfitRate(double profit) {
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", profit * 100);
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profit);
     }
 
     private String getMatchDescription(Rank rank) {
