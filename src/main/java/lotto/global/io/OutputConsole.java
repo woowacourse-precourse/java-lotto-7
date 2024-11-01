c759b628a78cc6ffe9b68f94e4dd9df28e40d221
moonbinchoi <whsahd12@gmail.com>
2024-11-01 18:43:06 +0900

    feat(OutputConsole): 문자열 출력 기능 추가

---
+++ src/main/java/lotto/global/io/OutputConsole.java
@@ -0,0 +1,41 @@
+package lotto.global.io;
+
+import lotto.domain.Lotteries;
+import lotto.domain.util.Statistics;
+
+public class OutputConsole {
+
+    private static OutputConsole instance;
+
+    private OutputConsole() {}
+
+    public static OutputConsole getInstance() {
+        if (instance == null) {
+            instance = new OutputConsole();
+        }
+        return instance;
+    }
+
+    public void printPurchaseMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printPurchaseResult(Lotteries lotteries) {
+        System.out.println(lotteries.size() + "개를 구입했습니다.");
+        System.out.println(lotteries);
+    }
+
+    public void printWinningNumberMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printBonusNumberMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void printProfitStatistics(Statistics statistics) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(statistics);
+    }
+}
