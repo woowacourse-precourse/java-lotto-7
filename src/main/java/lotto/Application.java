0c72b748824d6a267f9512649c23408852663a8b
Hyungjung Kim <gudwnd1113@gmail.com>
2024-11-01 23:35:58 +0900

    feat(Application): implement Lotto Machine feature
    요구사항 및 기능을 충족하는 로또 발매기 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import static lotto.IOProcessor.printIssueAmount;
+import static lotto.IOProcessor.printIssuedLottos;
+import static lotto.IOProcessor.printProfitRate;
+import static lotto.IOProcessor.printWinningDetails;
+import static lotto.Lotto.getIssueAmount;
+import static lotto.Lotto.readBonusNumber;
+import static lotto.Lotto.readPurchaseAmount;
+import static lotto.Lotto.readWinningNumbers;
+import static lotto.Utils.issueLottos;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = readPurchaseAmount();
+        int issueAmount = getIssueAmount(purchaseAmount);
+
+        List<Lotto> issuedLottos = issueLottos(issueAmount);
+        printIssueAmount(issueAmount);
+        printIssuedLottos(issuedLottos);
+
+        List<Integer> winningNumbers = readWinningNumbers();
+        int bonusNumber = readBonusNumber(winningNumbers);
+
+        for (Lotto lotto : issuedLottos) {
+            lotto.checkLottoWin(winningNumbers, bonusNumber);
+        }
+
+        printWinningDetails();
+        printProfitRate(purchaseAmount);
     }
 }
