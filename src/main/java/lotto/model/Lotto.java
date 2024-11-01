2c8cc435bb7aedfba3b96282a867ffa4ee632eca
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-01 14:23:56 +0900

    feat(Lotto): 로또 번호 중복값 검증 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,12 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
+        validateNotDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validateLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNotDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
