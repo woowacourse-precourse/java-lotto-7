5b5b31c6051dd282060a79c227cc6a40edc67232
minSsan <msp1125@hanyang.ac.kr>
2024-11-01 19:56:23 +0900

    feat(LottoInputValidator): 빈 문자열 검증 추가

--- src/main/java/lotto/application/validator/LottoInputValidator.java
+++ src/main/java/lotto/application/validator/LottoInputValidator.java
@@ -12,17 +12,26 @@ public class LottoInputValidator {
     private static final String DELIMITER = ",";
 
     public Cost validateCost(final String input) {
+        validateEmpty(input);
         final int number = validateNumberFormat(input);
         return Cost.of(number);
     }
 
     public WinningLottoImpl validateWinningLotto(String numbers, String bonusInput) {
+        validateEmpty(numbers);
+        validateEmpty(bonusInput);
         List<String> splits = Arrays.stream(numbers.split(DELIMITER))
                 .map(String::trim)
                 .toList();
         List<Integer> basicNumbers = splits.stream().map(this::validateNumberFormat).toList();
         int bonusNumber = validateNumberFormat(bonusInput);
         return WinningLottoImpl.of(basicNumbers, bonusNumber);
+    }
+
+    private void validateEmpty(final String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new CustomException(ExceptionMessage.EMPTY_INPUT);
+        }
     }
 
     private int validateNumberFormat(final String input) {
