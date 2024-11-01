a3f32f4cacd5f1104eb6290e7886f3e3f07cf63c
yooo <ghddbtl@gmail.com>
2024-11-02 00:02:40 +0900

    chore : revise printing message to Input.java
    예외를 터트리고 보여줄 메세지들을 작성하였다.

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,14 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class Input {
-    final static String PURCHASE_AMOUNT_PROMPT = "로또 구매 금액을 입력해주세요";
-    final static String WINNING_NUMBERS_PROMPT = "담청 번호를 입력해주세요";
-    final static String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해주세요";
+    final static String PURCHASE_AMOUNT_PROMPT = "로또 구매 금액을 입력해주세요. (장당 1,000원)";
+    final static String WINNING_NUMBERS_PROMPT = "담청 번호를 입력해주세요.(숫자는 ,(콤마)로 구분합니다.)";
+    final static String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해주세요.";
 
     static String request(String message) {
         System.out.println(message);
         return Console.readLine();
-
     }
 
 }
