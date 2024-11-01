3890f81fc4d0b197699bb16b7484a6a4fe5a7577
corpi <lj845854@gmail.com>
2024-11-01 14:49:50 +0900

    test(InputWinningNumberTest): 당첨 번호가 6개가 아닐 경우 에러가 발생한다.

---
+++ src/test/java/lotto/model/InputValidatorTest/InputWinningNumberTest.java
@@ -0,0 +1,26 @@
+package lotto.model.InputValidatorTest;
+
+import lotto.model.InputValidator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class InputWinningNumberTest {
+    private InputValidator inputValidator;
+
+    @BeforeEach
+    public void setup() {
+        inputValidator = new InputValidator();
+    }
+
+    @DisplayName("당첨 번호가 6개가 아닐 경우 에러가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"'1'", "'1,2'", "'1,2,3'", "'1,2,3,4'", "'1,2,3,4,5'"})
+    public void 당첨_번호가_6개가_아닐_경우_에러가_발생한다(String input) {
+        Assertions.assertThatThrownBy(() -> {
+            inputValidator.validateInputWinningNumber(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
