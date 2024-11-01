c9a02a5292ae49f984b71e740aa9b8c97b4c83d8
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-01 20:45:32 +0900

    fix : Convert constants to local variables
    - lottoPurchaseAmount 변수는 입력에 해당하므로 고정값인 상수에 어울리지 않아서 변경함.

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,9 +3,10 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private final int lottoPurchaseAmount = Integer.parseInt(Console.readLine());
 
     public int getLottoPurchaseAmount() {
+        int lottoPurchaseAmount = Integer.parseInt(Console.readLine());
+
         return lottoPurchaseAmount;
     }
 }
