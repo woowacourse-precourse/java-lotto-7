20fa181c0e0531209764d662334a36267f646df2
LeeDongHoon <likelion.leedh@gmail.com>
2024-11-01 18:59:33 +0900

    [FEAT] 예외 상태 추가

--- src/main/java/lotto/exception/LottoExceptionStatus.java
+++ src/main/java/lotto/exception/LottoExceptionStatus.java
@@ -5,6 +5,8 @@ public enum LottoExceptionStatus {
 
     INVALID_LOTTO_PURCHASE_AMOUNT("[ERROR] 구입 금액이 올바르지 않습니다. 로또 하나당 금액은 1000원입니다."),
     INVALID_WINNING_NUMBER_RANGE("[ERROR] 당첨 로또 번호가 올바르지 않습니다. 번호는 1 ~ 45 사이 숫자만 가능합니다."),
+    INVALID_WINNING_NUMBER_DUPLICATE("[ERROR] 중복된 당첨 로또 번호가 존재합니다."),
+    INVALID_WINNING_NUMBER_SIZE("[ERROR] 당첨 로또 번호는 6개여야 합니다."),
     INVALID_BONUS_NUMBER_RANGE("[ERROR] 보너스 번호가 올바르지 않습니다. 번호는 1 ~ 45 사이 숫자만 가능합니다."),
     INVALID_GENERATED_LOTTO_NUMBERS_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
 
