0dd16940bebe054c246f0ba39a49bd33dbaa0343
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-01 16:48:11 +0900

    refactor(Lotto): LottoValidator 적용
    - 유용성 메서드 삭제
    - LottoValidator 적용

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,7 @@
 package lotto.model;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
   private final List<Integer> numbers;
@@ -13,19 +12,6 @@ public class Lotto {
   }
 
   private void validate(List<Integer> numbers) {
-    // TODO 다른 validate가 모여서 사용되는 함수로 구성 할 것
-
-    final int NUMBERS_LENGTH = 6;
-    Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-
-    if (numbers.size() != NUMBERS_LENGTH) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
-
-    if (uniqueNumbers.size() < NUMBERS_LENGTH) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-    }
+    LottoValidator.validate(numbers);
   }
-  // TODO: 추가 기능 구현
-  // TODO 로또 생성에 대한 validate 추가 예정
 }
