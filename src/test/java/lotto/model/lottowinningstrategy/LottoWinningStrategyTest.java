3136ae858ca57faff23127684a3717fb0f0ec816
DongYeop <s01054830455@gmail.com>
2024-11-02 03:10:24 +0900

    test: 로또 당첨 전략 테스트 추가

---
+++ src/test/java/lotto/model/lottowinningstrategy/LottoWinningStrategyTest.java
@@ -0,0 +1,32 @@
+package lotto.model.lottowinningstrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.model.WinningNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoWinningStrategyTest {
+
+    @DisplayName("로또 당첨 순위를 계산할 수 있다.")
+    @Test
+    void calculateRank() {
+        //given
+        Lotto lotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLottoNumbers = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+
+        WinningNumbers winningNumbers = WinningNumbers.of(winningLottoNumbers, 7);
+
+        LottoWinningStrategy lottoWinningStrategy = new LottoWinningStrategy();
+
+        //when
+        LottoRank lottoRank = lottoWinningStrategy.calculateRank(lotto, winningNumbers);
+
+        //then
+        assertThat(lottoRank).isEqualTo(LottoRank.FIRST);
+    }
+
+}
