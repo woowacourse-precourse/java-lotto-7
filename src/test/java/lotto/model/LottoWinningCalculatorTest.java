019b456d26e52a25ace8b2087ffe1ce4ddd0407d
DongYeop <s01054830455@gmail.com>
2024-11-02 03:32:14 +0900

    test: 로또 당첨 계산 기능 테스트

---
+++ src/test/java/lotto/model/LottoWinningCalculatorTest.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import lotto.model.lottowinningstrategy.LottoWinningStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoWinningCalculatorTest {
+
+    @DisplayName("당첨 결과를 계산할 수 있다.")
+    @Test
+    void calculateWinningResult() {
+        //given
+        Lottos lottos = Lottos.from(List.of(
+                Lotto.of(List.of(1, 2, 3, 4, 5, 6)),
+                Lotto.of(List.of(7, 8, 9, 10, 11, 12))
+        ));
+
+        WinningNumbers winningNumbers = WinningNumbers.of(
+                Lotto.of(List.of(1, 2, 3, 4, 5, 6)), 7
+        );
+
+        LottoWinningCalculator winningCalculator = LottoWinningCalculator.from(new LottoWinningStrategy());
+
+        //when
+        LottoWinningResult lottoWinningResult = winningCalculator.calculateWinningResult(lottos, winningNumbers);
+
+        //then
+        Map<LottoRank, Integer> compareResult = Map.of(
+                LottoRank.FIRST, 1,
+                LottoRank.NO_LUCK, 1
+        );
+
+        assertThat(lottoWinningResult.getLottoWinningResult()).isEqualTo(compareResult);
+
+    }
+}
