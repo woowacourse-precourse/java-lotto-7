dc68132380e3ef03a6541a6703f17d48cab3125d
soeun2537 <soeun2537@gmail.com>
2024-11-02 00:55:54 +0900

    test: 입력 문자열 정수로 변환 - 양쪽 공백 제거 단위 테스트 추가

--- src/test/java/lotto/model/ParserTest.java
+++ src/test/java/lotto/model/ParserTest.java
@@ -47,6 +47,19 @@ class ParserTest {
     }
 
     @Test
+    @DisplayName("입력 문자열 정수로 변환: 양쪽 공백 제거 - 성공 테스트")
+    void parseInputToInt_trimBothWhiteSpace() {
+        // given
+        String input = " 1 ";
+
+        // when
+        Integer parsedInput = Parser.parseInputToInt(input);
+
+        // then
+        assertThat(parsedInput).isEqualTo(1);
+    }
+
+    @Test
     @DisplayName("입력 문자열 정수로 변환: 문자 - 예외 테스트")
     void parseInputToInt_string() {
         // given
