7981349b4bb3126b105afe1fe382d72618075a7f
speculatingwook <bwook9908@gmail.com>
2024-11-01 16:48:02 +0900

    feat: implement feature getting input again when got error
    - there might be another exception, will implement later

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -2,12 +2,15 @@ package lotto.io;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class Input {
+    private static final Pattern isWinningNumberPattern = Pattern.compile("^(0?[1-9]|[1-3][0-9]|4[0-5])(,(0?[1-9]|[1-3][0-9]|4[0-5])){5}$");
 
     public static String inputPrice() {
         String price = Console.readLine();
         try {
-            validatePrice();
+            validatePrice(price);
         } catch (IllegalArgumentException e) {
             return inputPrice();
         }
@@ -17,7 +20,7 @@ public class Input {
     public static String inputWinningNumber() {
         String winningNumber = Console.readLine();
         try{
-            validateWinningNumber();
+            validateWinningNumber(winningNumber);
         } catch (IllegalArgumentException e) {
             return inputWinningNumber();
         }
@@ -27,23 +30,53 @@ public class Input {
     public static String inputBonusNumber() {
         String bonusNumber = Console.readLine();
         try{
-            validateBonusNumber();
+            validateBonusNumber(bonusNumber);
         } catch (IllegalArgumentException e) {
             return inputBonusNumber();
         }
         return bonusNumber;
     }
 
-    private static void validatePrice() {
-
+    private static void validatePrice(String input) throws IllegalArgumentException {
+        validateInputNumeric(input);
+        Integer price = Integer.parseInt(input);
+        validatePriceDivisible(price);
     }
 
-    private static void validateWinningNumber() {
-
+    private static void validateWinningNumber(String input) throws IllegalArgumentException {
+        isWinningNumberFormat(input);
     }
 
-    private static void validateBonusNumber() {
+    private static void validateBonusNumber(String input) throws IllegalArgumentException {
+        validateInputNumeric(input);
+        Integer bonusNumber = Integer.parseInt(input);
+        validateBonusNumberInRange(bonusNumber);
+    }
 
+    private static void validateInputNumeric(String price) {
+        try{
+            Integer.parseInt(price);
+        }catch (NumberFormatException e) {
+            throw new NumberFormatException("[ERROR] Price must be an integer");
+        }
+    }
+
+    private static void validatePriceDivisible(Integer price) {
+        if(price % 1000 != 0){
+            throw new NumberFormatException("[ERROR] Price must be divisible by 1000");
+        }
+    }
+
+    private static void isWinningNumberFormat(String input){
+        if(!isWinningNumberPattern.matcher(input).matches()){
+            throw new IllegalArgumentException("[ERROR] Winning number must be a right format");
+        }
+    }
+
+    private static void validateBonusNumberInRange(Integer bonusNumber){
+        if(!(bonusNumber > 0 && bonusNumber < 46)){
+            throw new IllegalArgumentException("[ERROR] Bonus number must be between 0 and 46");
+        }
     }
 
 }
