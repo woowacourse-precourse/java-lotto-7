5db3390254e2b401fa56d0e7bd9a244653710bd0
33jyu33 <jy04169@gmail.com>
2024-11-01 23:40:52 +0900

    style(constant): add number to d

--- src/main/java/lotto/constant/LottoGuide.java
+++ src/main/java/lotto/constant/LottoGuide.java
@@ -2,7 +2,7 @@ package lotto.constant;
 
 public enum LottoGuide {
     PRICE_GUIDE("구입금액을 입력해 주세요."),
-    PURCHASE_COUNT("개를 구매했습니다."),
+    PURCHASE_COUNT("%d개를 구매했습니다."),
     WINNING_NUMBER_GUIDE("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_GUIDE("보너스 번호를 입력해 주세요.");
 
