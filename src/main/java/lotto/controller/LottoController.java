7f10fd495097e91e50a92ec0630353629aa85e4c
jungsu <vwx1@naver.com>
2024-11-01 17:57:05 +0900

    feat: 기본 입력 처리 기능을 갖춘 LottoController 구현
    - 사용자의 구입 금액, 당첨 번호, 보너스 번호를 InputView를 통해 받아옴.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,20 +1,30 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-    private final InputView inputView;
-    private final OutputView outputView;
-    private final LottoService lottoService;
 
-    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
-      this.inputView = inputView;
-      this.outputView = outputView;
-      this.lottoService = lottoService;
-    }
+  private final InputView inputView;
+  private final OutputView outputView;
+  private final LottoService lottoService;
 
+  public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+    this.inputView = inputView;
+    this.outputView = outputView;
+    this.lottoService = lottoService;
+  }
 
+  public void run() {
+
+    inputView.printPurchaseMessage();
+    //validator 따로 구현
+    String purchaseAmount = inputView.readPurchaseAmount();
+    String winningNumbers = inputView.readWinningNumbers();
+    String bonusNumber = inputView.readBonusNumber();
+
+  }
 
 }
