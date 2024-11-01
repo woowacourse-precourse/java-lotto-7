aaf548b328b6e033a97cd3d18d3e9f94bbde6b19
sese2204 <sese2204@naver.com>
2024-11-01 23:21:10 +0900

    feat(view): 사용자 입력 InputView 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,36 @@
+package lotto.view;
+
+import static java.lang.Integer.parseInt;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class InputView {
+    public static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String COMMA_DELIMITER = ",";
+
+    public int RequestPurchaseAmount() {
+        System.out.println(REQUEST_PURCHASE_AMOUNT);
+        String input = Console.readLine();
+        return parseInt(input); // TODO: 검증 로직 추가
+    }
+
+    public List<Integer> RequestWinningNumbers() {
+        System.out.println(REQUEST_WINNING_NUMBERS);
+        String input = Console.readLine();
+        return Arrays.stream(input.split(COMMA_DELIMITER))
+                .map(number -> Integer.parseInt(number.trim()))
+                .collect(Collectors.toList()); // TODO: 검증 로직 추가
+    }
+
+    public int RequestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER);
+        String input = Console.readLine();
+        return parseInt(input); // TODO: 검증 로직 추가
+    }
+}
