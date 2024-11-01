ac15f8d9608cc75f93354132243d0c4296b989f5
jinukee <jinukee333@gmail.com>
2024-11-01 11:35:28 +0900

    feat(controller.LottoController) : 잘못된 값 입력을 처리하는 method 구현
    - getValidMoney() : 사용자가 잘못된 금액을 입력하면 에러 메세지를 출력하고 다시 입력을 받게 합니다.
    - getValidWinningLotto() : 사용자가 당첨번호나 보너스번호를 잘못 입력하면 에러 메세지를 출력하고 다시 입력을 받게 합니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -11,21 +11,46 @@ public class LottoController {
     private static final int LOTTO_PRICE = 1_000;
 
     public void run() {
-        int money = InputView.getMoney();
+        int money = getValidMoney();
         int numberOfLottos = calculateNumberOfLottos(money);
         List<Lotto> purchasedLottos = LottoGenerator.generateLottos(numberOfLottos);
         OutputView.printPurchasedLottos(purchasedLottos);
 
-        WinningLotto winningLotto = new WinningLotto(InputView.getWinningNumbers(), InputView.getBonusNumber());
+        WinningLotto winningLotto = getValidWinningLotto();
         LottoResult result = new LottoResult(winningLotto, purchasedLottos);
         OutputView.printLottoResult(result.getResultCountMap());
         OutputView.printRateOfReturn(calculateRateOfReturn(result, money));
     }
 
-    private int calculateNumberOfLottos(int money) {
+    private int getValidMoney() {
+        while (true) {
+            try {
+                int money = InputView.getMoney();
+                validateMoney(money);
+                return money;
+            } catch (IllegalArgumentException e) {
+                //todo 에러 메세지 출력 (OutputView class 이용)
+            }
+        }
+    }
+
+    private WinningLotto getValidWinningLotto() {
+        while (true) {
+            try {
+                return new WinningLotto(InputView.getWinningNumbers(), InputView.getBonusNumber());
+            } catch (IllegalArgumentException e) {
+                //todo 에러 메세지 출력 (OutputView class 이용)
+            }
+        }
+    }
+
+    private void validateMoney(int money) {
         if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
         }
+    }
+
+    private int calculateNumberOfLottos(int money) {
         return money / LOTTO_PRICE;
     }
 
