d25e578ff6b46e613c923d741ed602b9ced92c51
kjyyjk <rlawnsdud920@gmail.com>
2024-11-01 19:48:55 +0900

    feat: 당첨 번호를 입력 받는다
    - inputWinningNumber()은 안내 문구를 출력하고 입력을 그대로 반환한다.
    구분자로 분리하고, 정수로 변환하고 반환할 수 있지만,
    input 관련 메서드의 책임은 딱 입력 받는 것까지라고 판단하여 그대로 반환한다.
    
    - 입력 받은 값을 분리하고, 정수로 변환하는 parseWinningNumber 메서드를 사용한다.

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,22 +1,47 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoManager {
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBER_DELIMITER = ",";
     private static final int LOTTO_PRICE = 1000;
 
     public void run() {
-        int purchaseAmount = inputPurchaseAmount();
+        int purchaseAmount = parsePurchaseAmount(inputPurchaseAmount());
         List<Lotto> lottoes = purchaseLottoes(purchaseAmount);
         Output.printPurchaseMessage(lottoes.size());
         lottoes.forEach(Output::printLotto);
+        List<Integer> winningNumber = parseWinningNumber(inputWinningNumber());
     }
 
-    public int inputPurchaseAmount() {
+    private int parsePurchaseAmount(String inputPurchaseAmount) {
+        return Integer.parseInt(inputPurchaseAmount);
+    }
+
+    private List<Integer> parseWinningNumber(String inputWinningNumber) {
+        String[] winningNumbers = inputWinningNumber.split(WINNING_NUMBER_DELIMITER);
+        return Arrays.stream(winningNumbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        return input();
+    }
+
+    public String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+        return input();
+    }
+
+    private static String input() {
+        return Console.readLine();
     }
 
     private List<Lotto> purchaseLottoes(final int purchaseAmount) {
