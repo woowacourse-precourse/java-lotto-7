c938fdefb3515b617d04a9ce945408ce335c78a0
inha0319@naver.com <inha031913@gmail.com>
2024-11-01 10:57:47 +0900

    [refactor] RottoMachine 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,7 @@
 package lotto;
 
 import lotto.model.Lotto;
-import lotto.model.LottoMachine;
+import lotto.controller.LottoMachine;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean requiresBonus;
+    private final int prize;
+
+    Rank(int matchCount, boolean requiresBonus, int prize) {
+        this.matchCount = matchCount;
+        this.requiresBonus = requiresBonus;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        for (Rank rank : values()) {
+            if (rank.matchCount == matchCount && rank.requiresBonus == matchBonus) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+
+}
