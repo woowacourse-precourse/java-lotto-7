daadbc5a7338920c0937aa13ba8801fb44d3bdfd
김동진 <dongjin403@naver.com>
2024-11-01 20:48:36 +0900

    refactor : lottoCount 필요없는 변수 삭제
    LottoBuyer 생성 시
    lottoCount에 값을 주고 그 값에 따라 lottos를 구성하였는데
    lottoCount를 거치지 않고 바로 lottos를 구성하도록 변경

--- src/main/java/lotto/LottoBuyer.java
+++ src/main/java/lotto/LottoBuyer.java
@@ -4,19 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
 public class LottoBuyer {
-
-    private long lottoCount;
-    private ArrayList<Lotto> lottos = new ArrayList<>();
+    private final ArrayList<Lotto> lottos = new ArrayList<>();
 
     private Lotto winningLotto;
     private int bonusLottoNumber;
 
     public LottoBuyer(long lottoCount) {
-        this.lottoCount = lottoCount;
-        addLotto();
+        buyLotto(lottoCount);
     }
 
-    private void addLotto() {
+    private void buyLotto(long lottoCount) {
         for (long i = 0; i < lottoCount; i++) {
             Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             lottos.add(lotto);
