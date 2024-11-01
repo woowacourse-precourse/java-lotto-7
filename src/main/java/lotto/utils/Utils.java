2e534b97a169c69a0af9ea6dcaf7df72caaa4fa1
dbdb456 <adad568@naver.com>
2024-11-01 16:07:02 +0900

    feat(utils): 1~45 사이의 랜덤으로 중복되지 않은 6개 번호 생성 메서드 추가

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -1,4 +1,17 @@
 package lotto.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class Utils {
+
+    public List<Lotto> generateRandomLottoNumbers(int price) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < price / 1000; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottos;
+    }
 }
