313c77f60da21cebfca4468af13764a11e3fa040
wanbinchoi <cwb6278@gmail.com>
2024-11-01 22:21:14 +0900

    add purchaseticket method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int tickets = PurchaseTickets();
+    }
+    //로또 구매
+    private static int PurchaseTickets(){
+        System.out.println("구입 금액을 입력해 주세요.");
+        int tickets = Integer.parseInt(Console.readLine());
+        if(tickets % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 로또는 1,000원 단위로 구매해야 합니다.");
+        }
+        return tickets/1000;
     }
 }
