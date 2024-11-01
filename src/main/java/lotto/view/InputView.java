be29b3a1891fe86a13be26d1dc37061046b4b9b9
woogym <kwj9294@gmail.com>
2024-11-02 05:10:47 +0900

    feat(view): 인풋 뷰 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,23 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+public class InputView implements ViewManager{
+
+    private static final String INPUT_BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호 입력해 주세요.";
+
+    protected String inputBuyAmount() {
+        printMessage(INPUT_BUY_AMOUNT_MESSAGE);
+        return System.console().readLine();
+    }
+
+    protected String inputWinningNumber() {
+        printMessage(INPUT_WINNING_NUMBER_MESSAGE);
+        return System.console().readLine();
+    }
+
+    protected String inputBonusNumber() {
+        printMessage(INPUT_BONUS_NUMBER_MESSAGE);
+        return System.console().readLine();
+    }
 }
