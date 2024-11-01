95fc0325ba88317a1898a185ccb8d80c2e3a1218
Changsu Park <gd12091@gmail.com>
2024-11-02 00:06:01 +0900

    fix: 반올림 방법 변경

--- src/main/java/lotto/service/ProfitCalculatorService.java
+++ src/main/java/lotto/service/ProfitCalculatorService.java
@@ -1,7 +1,5 @@
 package lotto.service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Map;
 import lotto.enums.Rank;
 
@@ -13,12 +11,9 @@ public class ProfitCalculatorService {
                 .sum();
 
         if (totalCost == 0) {
-            return 0.0; // 구입 금액이 0일 경우 수익률은 0으로 반환
+            return 0.0;
         }
-
-        BigDecimal profitRate = BigDecimal.valueOf((double) totalProfit / totalCost)
-                .setScale(4, RoundingMode.HALF_UP);
-
-        return profitRate.doubleValue();
+        double result = (double) totalProfit / (double) totalCost;
+        return Math.round(result * 1000) / 10.0;
     }
 }
