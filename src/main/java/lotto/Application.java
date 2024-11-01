66e753675806e3dd36a35e10ce4b00a11b8c8a1b
YoungGaLee <rkduddl1717@gmail.com>
2024-11-01 13:55:27 +0900

    feat: 로또 발행
    로또 1장 가격이 1000원일 때, 로또 수량을 계산하고 로도를 발행한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,6 @@ import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView input = new InputView();
-        int money = input.getMoney();
-
+        new Controller().start();
     }
 }
