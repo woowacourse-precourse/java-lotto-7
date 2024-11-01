d1aad6f7aa359c9bc0ce6ddf33bf9cdad71df151
kdh1004 <50905795+kdh1004@users.noreply.github.com>
2024-11-01 17:36:15 +0900

    로또 번호 발행

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,10 +11,29 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    /**
+     * 로또 번호 생성기
+     * 1~45 사이의 숫자 중에서 중복 없이 6개의 번호를 랜덤으로 선택하여 반환
+     * @return 중복 없는 6개의 로또 번호 리스트
+     */
+    private List<Integer> generateLottoNumbers() {
+        // Randoms.pickUniqueNumbersInRange()를 사용해 로또 번호 생성
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    /**
+     * 로또 번호 유효성 검사
+     * 번호의 개수가 6개인지 확인하고, 중복 여부를 검사
+     * @param numbers 검사할 로또 번호 리스트
+     */
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers; // 생성된 로또 번호 반환
     }
 
     // TODO: 추가 기능 구현
