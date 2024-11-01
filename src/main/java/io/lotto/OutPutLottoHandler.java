63b22168b68849a206fdb541fd0cf8d3b618ea21
zoaseo <huijaeseo18@google.com>
2024-11-01 21:54:26 +0900

    feat: 출력 부분 구현

--- src/main/java/io/lotto/OutPutLottoHandler.java
+++ src/main/java/io/lotto/OutPutLottoHandler.java
@@ -1,14 +1,20 @@
 package io.lotto;
 
 import lotto.Lotto;
+import user.User;
+import lotto.type.WinType;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutPutLottoHandler {
 
     private static final String OUTPUT_BUY_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String OUTPUT_RESULT_MESSAGE = "당첨 통계";
 
     public void showOutputBuyCountMessage(int count) {
         System.out.println();
@@ -25,5 +31,39 @@ public class OutPutLottoHandler {
             showLottos.add(lottoNumbers);
         }
         return showLottos;
+    }
+
+    public void showTotalResult(User user) {
+        List<WinType> values = Arrays.stream(WinType.values())
+                .sorted(Comparator.reverseOrder()).toList();
+        System.out.println();
+        System.out.println(OUTPUT_RESULT_MESSAGE);
+        System.out.println("---");
+        for (WinType value : values) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(value.getDescription())
+                    .append(" (").append(convertLottoWinningsToStringBy(value)).append("원) - ")
+                    .append(user.getRecordWin().get(value)).append("개");
+            System.out.println(stringBuilder);
+        }
+    }
+
+    public String convertLottoWinningsToStringBy(WinType winType) {
+        long lottoWinnings = winType.getLottoWinnings();
+        return NumberFormat.getInstance().format(lottoWinnings);
+    }
+
+    public void showProfitRate(int price, long profit) {
+        double profitRate = getProfitRate(price, profit);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("총 수익률은 ")
+                .append(profitRate).append("%")
+                .append("입니다.");
+        System.out.println(stringBuilder);
+    }
+
+    public double getProfitRate(int price, long profit) {
+        double num = (double) profit / price * 100.0;
+        return Math.round(num * 100) / 100.0;
     }
 }
