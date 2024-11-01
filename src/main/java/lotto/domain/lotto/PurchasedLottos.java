d710d2db21809e0455a7b8fc79bb36d4825b4589
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-01 22:00:30 +0900

    feat(PurchasedLottos): 구매된 로또 도메인 추가

---
+++ src/main/java/lotto/domain/lotto/PurchasedLottos.java
@@ -0,0 +1,32 @@
+package lotto.domain.lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.global.constant.LottoConstant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchasedLottos {
+
+    private final List<Lotto> lottos;
+
+    private PurchasedLottos(final int count) {
+        lottos = new ArrayList<>();
+
+    }
+
+    public static PurchasedLottos from(final int count) {
+        return new PurchasedLottos(count);
+    }
+
+    private void generateRandomNumber(final int count) {
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                    LottoConstant.MIN_LOTTO_NUMBER,
+                    LottoConstant.MAX_LOTTO_NUMBER,
+                    LottoConstant.LOTTO_SIZE
+            );
+            lottos.add(Lotto.from(numbers));
+        }
+    }
+}
