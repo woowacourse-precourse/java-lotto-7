5c98b44afdb8d3b8cc39f102f5d0bb798cfb1354
cherryiJuice <chldlwn711@gmail.com>
2024-11-01 19:52:56 +0900

    refactor: 상속받는 예외 클래스 변경

--- src/main/java/lotto/error/exception/GeneralException.java
+++ src/main/java/lotto/error/exception/GeneralException.java
@@ -2,7 +2,7 @@ package lotto.error.exception;
 
 import lotto.error.ErrorType;
 
-public class GeneralException extends RuntimeException {
+public class GeneralException extends IllegalArgumentException {
     private final ErrorType errorType;
 
     protected GeneralException(ErrorType errorType) {
