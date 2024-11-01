33c09a261684aa414f7a9443e2c6595d24d94603
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-01 20:35:21 +0900

    refactor($LottoShop): RandomLottoNumberGenerator 를 주입받아 Lottos 를 구매하도록 변경

--- src/main/java/lotto/model/LottoShop.java
+++ src/main/java/lotto/model/LottoShop.java
@@ -1,37 +1,26 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.exception.PurchaseMoneyInvalidException;
+import lotto.model.number_generator.RandomLottoNumberGenerator;
 
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoShop {
 
     private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_NUMBER_SIZE = 6;
-    private static final int LOTTO_MIN = 1;
-    private static final int LOTTO_MAX = 45;
 
-    public Lottos purchaseLottos(int money) {
+    public Lottos purchaseRandomLottos(int money, RandomLottoNumberGenerator randomLottoNumberGenerator) {
 
         validateMoney(money);
         int lottoCount = money / LOTTO_PRICE;
 
         return new Lottos(IntStream.range(0, lottoCount)
-                .mapToObj(i -> makeLotto())
+                .mapToObj(count -> makeLotto(randomLottoNumberGenerator))
                 .toList());
     }
 
-    private Lotto makeLotto() {
-        return new Lotto(makeLottoNumbers());
-    }
-
-    private List<LottoNumber> makeLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN, LOTTO_MAX, LOTTO_NUMBER_SIZE).stream()
-                .sorted()
-                .map(LottoNumber::from)
-                .toList();
+    private Lotto makeLotto(RandomLottoNumberGenerator randomLottoNumberGenerator) {
+        return new Lotto(randomLottoNumberGenerator.generate());
     }
 
     private void validateMoney(int money) {
