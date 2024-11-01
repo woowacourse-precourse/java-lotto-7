d293dcedb41b74b4ddf01f6de0d5bf5fdb22b1f5
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 14:07:30 +0900

    refactor: 보너스 번호를 담당하는 BonusNumber 클래스 구현

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,2 +1,19 @@
-package lotto.model;public class BonusNumber {
+package lotto.model;
+
+import java.util.List;
+
+public class BonusNumber {
+    private final int bonusNumber;
+    public BonusNumber(int bonusNumber){
+        isNumberBetween1And45(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+    private void isNumberBetween1And45(int bonusNumber){
+        if(bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+    public int getBonusNumber(){
+        return bonusNumber;
+    }
 }
