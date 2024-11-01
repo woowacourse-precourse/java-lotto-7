32898a886fe66f28cb5fff4213012b7c10021dde
bill291104 <bill29123@gmail.com>
2024-11-01 21:02:23 +0900

    feat(LottoShop): implement LottoShop class to pass tests
    - Add buy method to create specified number of Lotto instances based on input money
    - Add setWinningLotto method to initialize WinningLotto with selected numbers and bonus number

---
+++ src/main/java/lotto/LottoShop.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+public class LottoShop {
+
+    public static final int PRICE = 1000;
+    public static final int START = 1;
+    public static final int END = 45;
+    public static final int COUNT = 6;
+
+    public List<Lotto> buy(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        int count = calNumOfLotto(money);
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = makeLotto();
+            System.out.println(lotto);
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
+
+    private int calNumOfLotto(int money) {
+        //ignore the change(money % PRICE)
+        return money / PRICE;
+    }
+
+    public WinningLotto setWinningLotto(List<Integer> numbers, int bonusNum) {
+        return new WinningLotto(numbers, bonusNum);
+    }
+
+    private Lotto makeLotto() {
+        return new Lotto(pickUniqueNumbersInRange(START, END, COUNT));
+    }
+
+}
