bfc669b2f8084d3e4805f0022c0c0f3d82afeeea
reindeer002 <lsj000424@gmail.com>
2024-11-02 03:02:15 +0900

    refactor(story5.2): remove unused packages and update exception handling messages

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,14 +4,13 @@ import static lotto.common.ErrorConstants.ERROR_HEADER;
 import static lotto.common.LottoConstants.LOTTO_NUMBER_MAX;
 import static lotto.common.LottoConstants.LOTTO_NUMBER_MIN;
 
-import java.util.List;
 import lotto.common.LottoRank;
 
 public class WinningLotto {
     private static final int BONUS_NUMBER_MAX = LOTTO_NUMBER_MAX;
     private static final int BONUS_NUMBER_MIN = LOTTO_NUMBER_MIN;
     private static final String ERROR_MESSAGE_BONUS_NUMBER_RANGE = ERROR_HEADER + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String ERROR_MESSAGE_BONUS_NUMBER_DUPLICATE = ERROR_HEADER + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_BONUS_NUMBER_DUPLICATE = ERROR_HEADER + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 
     private final Lotto winningLotto;
     private final int bonusNumber;
