0c10ecadcf7285e0d1165aa7522332bbb30f9f72
woongjee <jeewoong7753@naver.com>
2024-11-01 14:16:12 +0900

    feat(Benefit):수익률 계산 기능 구현

--- src/main/java/lotto/Benefit.java
+++ src/main/java/lotto/Benefit.java
@@ -1,4 +1,13 @@
 package lotto;
 
 public class Benefit {
+    private static final int[] PRIZES = {0, 2000000000, 30000000, 1500000, 50000, 5000};
+
+    public double calculateBenefit(int purchaseAmount, int[] ranks){
+        int totalPrize = 0;
+        for(int i = 1;i<ranks.length;i++){
+            totalPrize += ranks[i]*PRIZES[i];
+        }
+        return (double) totalPrize/purchaseAmount*100;
+    }
 }
