0f5e37384e289aebe4cc36deb7d664962be949e1
dongkyunKim <kyun010713@gmail.com>
2024-11-02 03:29:19 +0900

    feat(model): 보너스 번호 관리하는 클래스 추가 - 보너스 번호가 1부터 45 사이에 없으면 예외 발생 - 생성 시 제공된 당첨 번호와 보너스 번호가 중복되면 예외 발생

---
+++ src/main/java/lotto/model/BonusNumber.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import java.util.List;
+
+public class BonusNumber {
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        validateBonusNumber(bonusNumber);
+        validateBonusNumberUniqueness(bonusNumber, winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (isNumberOutOfRange(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateBonusNumberUniqueness(int bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 겹칠 수 없습니다.");
+        }
+    }
+
+    private boolean isNumberOutOfRange(int number) {
+        return number < 1 || number > 45;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
