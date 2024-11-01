6e5dfb591073991a4b38e18cc7868c477eec26f5
jang-namu <ironhee8005@gmail.com>
2024-11-01 18:18:19 +0900

    style: 세미콜론 삭제

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,7 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_BUDGET_INFO= "구입금액을 입력해 주세요.";
+    private static final String INPUT_BUDGET_INFO = "구입금액을 입력해 주세요.";
     private static final String ERROR_BUDGET_NUMBER_FORMAT = "[ERROR] 금액은 숫자로 입력해주세요.";
 
     public Budget readBudget() {
@@ -18,7 +18,7 @@ public class InputView {
 
     private Budget makeBudget(String input) {
         try {
-            Long amount = Long.parseLong(input);;
+            Long amount = Long.parseLong(input);
             return Budget.of(amount);
         } catch (NumberFormatException e) {
             System.out.println(ERROR_BUDGET_NUMBER_FORMAT);
