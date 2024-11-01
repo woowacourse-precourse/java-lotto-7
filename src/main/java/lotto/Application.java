178a39b54df8c1621cd73d1b5768162d30eb6c9b
seullll <tmfud@naver.com>
2024-11-01 15:16:47 +0900

    refactor: 테스트 실패 문제 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Input input = new Input();
-        int money = input.moneyInput();
-
         try {
+            Input input = new Input();
+            int money = input.moneyInput();
+
             LottoNum lottoNum = new LottoNum(money);
             List<Integer> numbers = input.numberInput();
             int bonus = input.bonusInput();
             lottoNum.printResult(numbers, bonus, money);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 }
