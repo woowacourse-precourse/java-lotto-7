f63c3458405a445effb54ddbe0b91c6c83c2804a
Seonjeongg <sj030205@naver.com>
2024-11-01 21:46:09 +0900

    fix: 오름차순 정렬 Lotto에 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,23 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers); // 가변 리스트로 복사
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        Set<Integer> lotto_numbers = new HashSet<>();
+        for (int number : numbers) {
+            if (!lotto_numbers.add(number)) throw new IllegalArgumentException("[ERROR] 로또에 중복된 숫자가 없어야 합니다.");
         }
     }
 
@@ -24,4 +29,5 @@ public class Lotto {
         if(numbers.contains(bonus_number)) return true;
         return false;
     }
+
 }
