564dcba11ba347e5b2e6ac7124f2e3102bdc074c
Chan531 <chan657312@gmail.com>
2024-11-01 18:57:55 +0900

    feat(LottoCreator): 랜덤 숫자 리스트 생성
    1 이상 45 이하의 6가지 중복되지 않는 랜덤 숫자 생성

--- src/main/java/lotto/model/LottoCreator.java
+++ src/main/java/lotto/model/LottoCreator.java
@@ -1,8 +1,16 @@
 package lotto.model;
 
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoCreator {
 
 	private static final int MIN_LOTTO_NUMBER = 1;
 	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int LOTTO_NUMBER_COUNT = 6;
+
+	private List<Integer> getRandomNumbers() {
+		return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+	}
 }
