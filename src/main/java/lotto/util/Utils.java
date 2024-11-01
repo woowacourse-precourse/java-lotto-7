8445043d221131e8452f46ddf2e1c3eb001e40c0
JS <zks145@naver.com>
2024-11-01 21:59:55 +0900

    feat : 문자를 숫자로 바꾸는 기능 추가

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -1,12 +1,17 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.validation.Validation;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Util {
+public class Utils {
+
+    public static int changeStringToInt(String number) {
+        Validation.checkStringToInteger(number);
+        return Integer.parseInt(number);
+    }
 
     public static List<Integer> randomLottoNumbers(int start , int end, int size) {
         return Randoms.pickUniqueNumbersInRange(start, end, size);
