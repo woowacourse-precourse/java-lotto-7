323721d799a9bd7c0d470e5adb4082b4dda38362
siwoo <k74415834@gmail.com>
2024-11-01 22:33:44 +0900

    feat(LottoRequestParser, LottoRequestParserTest): 당첨 번호 파싱 기능 구현

--- src/main/java/lotto/front/util/LottoRequestParser.java
+++ src/main/java/lotto/front/util/LottoRequestParser.java
@@ -1,5 +1,7 @@
 package lotto.front.util;
 
+import java.util.List;
+import java.util.stream.Stream;
 import lotto.global.exception.CustomIllegalArgumentException;
 
 public class LottoRequestParser {
@@ -9,6 +11,17 @@ public class LottoRequestParser {
             return Integer.parseInt(price);
         } catch (NumberFormatException e) {
             throw new CustomIllegalArgumentException("금액은 유효한 숫자로 입력해주세요.");
+        }
+    }
+
+    public static List<Integer> parsePrizeNumber(String prizeNumber) {
+        try {
+            return Stream.of(prizeNumber.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new CustomIllegalArgumentException("당첨 번호는 유효한 숫자로 입력해주세요.");
         }
     }
 }
