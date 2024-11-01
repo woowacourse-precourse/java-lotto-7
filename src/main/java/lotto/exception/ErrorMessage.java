a1abcaa041f9c52f7ac4d3bc674c20458a7ac6d8
StoneCAU <seokwoo1999@naver.com>
2024-11-02 00:46:15 +0900

    chore(ErrorMessage): 범위 에러 메세지 수정
    - WINNING_NUMBER_OUT_OF_RANGE
    - BONUS_NUMBER_OUT_OF_RANGE
    - LottoGameRule의 상수를 반영하도록 포맷팅

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,5 +1,9 @@
 package lotto.exception;
 
+import static java.lang.String.format;
+import static lotto.constant.LottoGameRule.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoGameRule.MIN_LOTTO_NUMBER;
+
 public enum ErrorMessage {
     // 입력
     INPUT_EMPTY_ERROR("빈 입력입니다."),
@@ -12,14 +16,14 @@ public enum ErrorMessage {
 
     // 당첨 번호
     WINNING_NUMBER_NOT_DIGIT("당첨 번호는 숫자여야 합니다."),
-    WINNING_NUMBER_OUT_OF_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    WINNING_NUMBER_OUT_OF_RANGE(format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_LOTTO_NUMBER.getValue(), MAX_LOTTO_NUMBER.getValue())),
     DUPLICATED_NUMBER("중복된 번호가 있습니다."),
     NUMBERS_SIZE_ERROR("로또 번호는 %d개여야 합니다."),
 
     // 보너스 번호
     BONUS_NUMBER_NOT_DIGIT("보너스 번호는 숫자여야 합니다."),
     BONUS_NUMBER_DUPLICATE("당첨 번호와 중복된 번호입니다."),
-    BONUS_NUMBER_OUT_OF_RANGE("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    BONUS_NUMBER_OUT_OF_RANGE(format("보너스 번호는 %d부터 %d 사이의 숫자여야 합니다.", MIN_LOTTO_NUMBER.getValue(), MAX_LOTTO_NUMBER.getValue()));
 
     private final String message;
 
