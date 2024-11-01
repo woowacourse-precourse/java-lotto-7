fc270dd62277596ddd1023ee67302065215821a1
EP45 <5510816+EP45@users.noreply.github.com>
2024-11-01 22:25:01 +0900

    test(Lotto): add tests for lotto numbers

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +22,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 유효하지 않다면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_유효하지_않다면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(null))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("로또 숫자가 범위 밖이라면 예외가 발생한다.")
+    @Test
+    void 로또_숫자가_범위_밖이라면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(6, 14, 26, 27, 38, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
