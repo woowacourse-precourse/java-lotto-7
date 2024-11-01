a28aef6c926a7dee7a0c6ccd2da489d29688f61e
seophohoho <seop0937@gmail.com>
2024-11-01 22:34:59 +0900

    feat(WinningNumber): 당첨 번호 입력 저장 메소드 추가

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WinningNumber {
     private final String WINNING_NUMBER_PATTERN = "([1-9][0-9]{0,1},){5}[1-9][0-9]{0,1}";
+    private List<Integer> numbers = new ArrayList<>();
 
     public void validate(String input) {
         Pattern patternWinningNumber = Pattern.compile(WINNING_NUMBER_PATTERN);
@@ -20,6 +23,18 @@ public class WinningNumber {
                 .allMatch(num -> Integer.parseInt(num) >= 1 && Integer.parseInt(num) <= 45)) {
             reject();
         }
+    }
+
+    public void setNumbers(String input) {
+        String[] separateWinningNumber = input.split(",");
+
+        for (int i = 0; i < separateWinningNumber.length; i++) {
+            this.numbers.add(Integer.parseInt(separateWinningNumber[i]));
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
     }
 
     public void reject() {
