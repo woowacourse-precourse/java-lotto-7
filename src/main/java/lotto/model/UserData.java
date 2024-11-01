2342a043c82cd6dfb91728d6e12f59019ac0fc88
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-01 18:47:16 +0900

    feat( UserData.java ) Add Method
    Add Method List
     - getLottoPapers()
     - getWinNumbers()
     - getBonusNumber()
     - setBonusNumber()

--- src/main/java/lotto/model/UserData.java
+++ src/main/java/lotto/model/UserData.java
@@ -1,24 +1,43 @@
 package lotto.model;
 
+import java.util.List;
+
 public class UserData {
     private int money = 0;
-    private int lottoPapes = 0;
-
-
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
-    public void setLottoPapes(int lottoPapes) {
-        this.lottoPapes = lottoPapes;
-    }
+    private int lottoPapers = 0;
+    private List<Integer> winNumbers;
+    private int bonusNumber;
 
     public int getMoney() {
         return money;
     }
 
-    public int getLottoPapes() {
-        return lottoPapes;
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getLottoPapers() {
+        return lottoPapers;
+    }
+
+    public void setLottoPapers(int lottoPapes) {
+        this.lottoPapers = lottoPapes;
+    }
+
+    public List<Integer> getWinNumbers() {
+        return winNumbers;
+    }
+
+    public void setWinNumbers(List<Integer> winNumbers) {
+        this.winNumbers = winNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
     }
 
 }
