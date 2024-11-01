d1ad394b99550aa1e8a265aa8316365c61f73f54
lucku <lsw6731@naver.com>
2024-11-01 18:18:48 +0900

    feat: LottoConstants 추가

---
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -0,0 +1,17 @@
+package lotto.constants;
+
+public enum LottoConstants {
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    LOTTO_SIZE(6);
+
+    private final int value;
+
+    LottoConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
