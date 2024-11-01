5763f7d3c64b26d116ede78a8e20322d01e6f00b
Chan531 <chan657312@gmail.com>
2024-11-01 18:55:00 +0900

    feat(LottoCreator): 로또 번호 최대값 상수 추가

--- src/main/java/lotto/model/LottoCreator.java
+++ src/main/java/lotto/model/LottoCreator.java
@@ -3,5 +3,6 @@ package lotto.model;
 public class LottoCreator {
 
 	private static final int MIN_LOTTO_NUMBER = 1;
+	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int LOTTO_NUMBER_COUNT = 6;
 }
