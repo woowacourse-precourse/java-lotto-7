067e4d3c064a254fefcd24e598d2201cf43e169d
33jyu33 <jy04169@gmail.com>
2024-11-02 00:22:45 +0900

    refactor(Consumer): move validating price and getting count to controller
    Change the parameter of Consumer to an integer count

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -2,19 +2,13 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Console.readLine;
-import static lotto.CompareInteger.*;
-import static lotto.PriceRule.*;
-import static lotto.validator.NumberValidator.validateOnlyInteger;
-import static lotto.validator.PriceValidator.validatePriceUnit;
-
 public class Consumer {
-    private List<Lotto> lottoTicket;
+    private List<Lotto> lottoTicket = new ArrayList<>();
 
-    public Consumer(String priceInput) {
-        int count = getCount(priceInput);
+    public Consumer(int count) {
         for (int i = 0; i < count; i++) {
             setLotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         }
@@ -28,19 +22,4 @@ public class Consumer {
     public List<Lotto> getLotto() {
         return this.lottoTicket;
     }
-
-    private int getCount(String priceInput) {
-        try {
-            validateOnlyInteger(priceInput, ONLY_INTEGER.getMessage());
-            int price = Integer.parseInt(priceInput);
-            lotto.validator.NumberValidator.validateScope(PRICE_MINIMUM.getNumber(), PRICE_MAXIMUM.getNumber(), price, SCOPE.getMessage());
-            validatePriceUnit(price);
-            return price / CompareInteger.PRICE_LOTTO.getNumber();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            // 사용자로부터 priceInput을 다시 입력 받는다.
-            return getCount(readLine());
-        }
-    }
-
 }
