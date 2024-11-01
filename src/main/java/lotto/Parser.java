e43c33d07a4955a35063a14f0173fad9e4a36d15
chyo1 <hyowon3856@gmail.com>
2024-11-01 19:55:10 +0900

    feat : 입력된 당첨 번호 파싱 기능 추가

--- src/main/java/lotto/Parser.java
+++ src/main/java/lotto/Parser.java
@@ -3,8 +3,15 @@ package lotto;
 import java.util.Arrays;
 
 public class Parser {
+    String WINNING_NUMBER_OPERATOR = ",";
 
     int purchaseAmount(String purchaseInput) {
         return Integer.parseInt(purchaseInput);
+    }
+
+    int[] winningNumbers(String winningNumbers) {
+        return Arrays
+            .stream(winningNumbers.split(WINNING_NUMBER_OPERATOR))
+            .mapToInt(Integer::parseInt).toArray();
     }
 }
