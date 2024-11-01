82483ce54fd3ade589b9494616db45189e637759
moonbinchoi <whsahd12@gmail.com>
2024-11-01 15:25:56 +0900

    feat(Statistics): 수익률 계산 기능 추가

---
+++ src/main/java/lotto/domain/Statistics.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public final class Statistics {
+
+    private final Map<Rank, Integer> rankMap;
+
+    private Statistics(List<Rank> ranks) {
+        this.rankMap = countRanks(ranks);
+    }
+
+    public static Statistics of(List<Rank> ticketResults) {
+        return new Statistics(ticketResults);
+    }
+
+    public double calculateProfitRate() {
+        final double seed = TicketParser.TICKET_PRICE * rankMap.size();
+        int profit = 0;
+
+        for (Entry<Rank, Integer> entry : rankMap.entrySet()){
+            profit += entry.getKey().price * entry.getValue();
+        }
+
+        return profit / seed * 100;
+    }
+
+    private Map<Rank, Integer> countRanks(List<Rank> ranks) {
+        Map<Rank, Integer> statistics = new HashMap<>();
+
+        for (Rank rank : ranks) {
+            statistics.put(rank, statistics.getOrDefault(rank, 0) + 1);
+        }
+
+        return statistics;
+    }
+}
