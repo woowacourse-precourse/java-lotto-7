9e1ff9a4535d7958067868c6a4232d271cbb534e
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-01 19:43:22 +0900

    test(lotto/validate): 로또 구입 금액 타입 예외 기능 테스트 추가

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -10,7 +10,16 @@ class AmountTest {
     @DisplayName("로또_구입금액의_단위가_1000이_아니라면_예외가_발생한다")
     void validateUnit() {
         assertThatThrownBy(() -> new Amount(2500))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구매는 1000원 단위로 가능합니다.");
     }
-    
+
+    @Test
+    @DisplayName("로또_구입금액의_타입이_숫자가_아니라면_예외가_발생한다")
+    void validateType() {
+        assertThatThrownBy(() -> Amount.of("2000a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 구입 금액을 숫자로 입력해주세요.");
+    }
+
 }
