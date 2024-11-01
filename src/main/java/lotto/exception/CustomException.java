501208cabfe614beeed5e0e6520bf4e244ad204c
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 04:42:00 +0900

    feat($CustomException): 디테일 메시지를 덧붙이는 함수 추가

--- src/main/java/lotto/exception/CustomException.java
+++ src/main/java/lotto/exception/CustomException.java
@@ -1,4 +1,12 @@
 package lotto.exception;
 
 public interface CustomException {
+
+    static String appendDetails(String message, String... details) {
+        StringBuilder stringBuilder = new StringBuilder(message);
+        for (String detail : details) {
+            stringBuilder.append(" ").append(detail);
+        }
+        return stringBuilder.toString();
+    }
 }
