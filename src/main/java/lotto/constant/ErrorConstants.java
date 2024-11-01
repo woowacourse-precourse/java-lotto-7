ff5886c868005026773b884f4c884b6670a89efc
Kiki1875b <kevinheo0413@gmail.com>
2024-11-01 13:58:07 +0900

    feat(Validator) : 1000원으로 나누어 떨어지지 않는 가격 예외처리

--- src/main/java/lotto/constant/ErrorConstants.java
+++ src/main/java/lotto/constant/ErrorConstants.java
@@ -3,9 +3,10 @@ package lotto.constant;
 public enum ErrorConstants {
 
     ERROR_MESSAGE_START("[ERROR] "),
-    INVALID_PRICE_FORMAT("구입 금액은 숫자여야 합니다."),
+    INVALID_PRICE_FORMAT("구입 금액은 숫자 혹은 1000원 단위여야 합니다."),
     NULL_NOT_ALLOWED("NULL 은 허용되지 않습니다."),
     NEGATIVE_PRICE_NOT_ALLOWED("구입 금액은 양수여야 합니다.");
+
 
     private final String message;
 
--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -4,11 +4,16 @@ import lotto.constant.ErrorConstants;
 
 public class Validator {
     private final static int ZERO = 0;
+    private final static int MINIMUM_PRICE_UNIT = 1000;
+    private int price;
     public void validatePrice(String input){
         checkInputIsNotNull(input);
         checkInputIsNumber(input);
 
-        checkInputIsNotNegative(input);
+        parseInputToNumber(input);
+
+        checkInputIsNotNegative();
+        checkInputIsDivisableByThousand();
     }
 
     private void checkInputIsNumber(String input){
@@ -25,9 +30,19 @@ public class Validator {
         }
     }
 
-    private void checkInputIsNotNegative(String input){
-        if(Integer.parseInt(input) <= ZERO){
+    private void parseInputToNumber(String input){
+        price = Integer.parseInt(input);
+    }
+
+    private void checkInputIsNotNegative(){
+        if(price <= ZERO){
             throw new IllegalArgumentException(ErrorConstants.NEGATIVE_PRICE_NOT_ALLOWED.getMessage());
+        }
+    }
+
+    private void checkInputIsDivisableByThousand(){
+        if(price % MINIMUM_PRICE_UNIT != ZERO){
+            throw new IllegalArgumentException(ErrorConstants.INVALID_PRICE_FORMAT.getMessage());
         }
     }
 
