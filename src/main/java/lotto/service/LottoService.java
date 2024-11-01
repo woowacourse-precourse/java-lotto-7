03fe675beca49cc5572d1a19d584b63ec0ffef81
ufo9430 <ufo9430@naver.com>
2024-10-31 23:03:21 -0400

    feat: 로또 갯수에 따라 로또를 발행하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,14 +1,24 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
-    public List<Lotto> issue(int count){
-        List<Lotto> lottos = new ArrayList<>();
+    private List<Lotto> lottos = new ArrayList<>();
 
+    public void issue(int count){
+        for(int i=0;i<count;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange
+                    (1,45,6);
+            lottos.add(new Lotto(numbers));
+        }
+    }
+
+    public List<Lotto> getLottos() {
         return lottos;
     }
 }
