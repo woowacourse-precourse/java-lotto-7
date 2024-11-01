4621897f183a848262f6dfe8b160e734f59bc9f4
Chaeyoung714 <202102760@hufs.ac.kr>
2024-11-01 15:07:25 +0900

    feat : 1.2 로또 구입 개수 확인 기능 구현
    test : 로또 구입 개수 반환 & 예외 처리 테스트 추가, 통과
    feat : 예외가 발생하면 1.1로 돌아갈 수 있도록 전체적으로 코드 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,9 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        int purchaseAmount = purchaseLottos();
+    }
+
+    private static int purchaseLottos() {
+        try {
+            String purchaseAmountInput = inputPurchaseAmount();
+            int purchaseAmount = countLottoAmount(purchaseAmountInput);
+            return purchaseAmount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return purchaseLottos();
+        }
+    }
+
+    private static String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         validateInputInteger(purchaseAmount);
+        return purchaseAmount;
     }
 
     public static void validateInputInteger(String inputInteger) {
@@ -20,5 +36,13 @@ public class Application {
         if (inputInteger.length() > 10) {
             throw new IllegalArgumentException("[ERROR] 10자리 이하의 금액을 입력해주세요.");
         }
+    }
+
+    public static int countLottoAmount(String purchaseAmountInput) {
+        int purchaseAmount = Integer.parseInt(purchaseAmountInput);
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 단위의 금액을 입력해주세요.");
+        }
+        return purchaseAmount / 1000;
     }
 }
--- src/test/java/lotto/AdditionalApplicationTest.java
+++ src/test/java/lotto/AdditionalApplicationTest.java
@@ -1,48 +1,49 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class AdditionalApplicationTest {
     @Test
-    void shouldValidateInputInteger() {
+    void 양의정수_10자리_이하_금액을_입력한다() {
         String testString = "50";
 
         assertThatCode(() -> Application.validateInputInteger(testString))
                 .doesNotThrowAnyException();
-
     }
 
     @Test
-    void failIfInputIntegerIsNull() {
-        String testString = null;
+    void 금액_null일시_예외() {
+        String testNumber = null;
 
-        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
                 .withMessage("[ERROR] 금액을 입력해주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
-    void failIfInputIntegerIsBlank(String testString) {
-        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+    void 금액_빈_문자열일시_예외(String testNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
                 .withMessage("[ERROR] 금액을 입력해주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-20.2, -1.0, 0.5, 2.0, 2.5, 3.24321"})
-    void 금액이_정수가_아니면_예외(String testString) {
-        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+    void 금액이_정수가_아니면_예외(String testNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
                 .withMessage("[ERROR] 양의 정수를 입력해주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-20, -1, 0"})
-    void 금액이_양수가_아니면_예외(String testString) {
-        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+    void 금액이_양수가_아니면_예외(String testNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
                 .withMessage("[ERROR] 양의 정수를 입력해주세요.");
     }
 
@@ -51,6 +52,21 @@ public class AdditionalApplicationTest {
     void 금액이_10자리를_넘어가면_예외(String testNumber) {
         assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
                 .withMessage("[ERROR] 10자리 이하의 금액을 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000:1", "5000:5", "11000:11", "25000:25", "303000:303", "1474134000:1474134", "1000000000:1000000"}, delimiter = ':')
+    void 금액을_1000으로_나누어_개수를_구한다(String testNumber, int expected) {
+        int lottoAmount = Application.countLottoAmount(testNumber);
+
+        assertThat(lottoAmount).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10", "1002", "2524", "100001", "100020"})
+    void 금액이_1000으로_나누어떨어지지_않으면_예외(String testNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.countLottoAmount(testNumber))
+                .withMessage("[ERROR] 1000 단위의 금액을 입력해주세요.");
     }
 
 
