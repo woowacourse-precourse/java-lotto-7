c026598316573ab6c213aadee5a8dd28b275cf7b
junho767 <junho767@naver.com>
2024-11-01 20:10:37 +0900

    refactor: 로또 번호 갯수와 번호 중복에 대한 에러 메세지 추가

--- src/main/java/lotto/Message/ErrorMessage.java
+++ src/main/java/lotto/Message/ErrorMessage.java
@@ -1,9 +1,11 @@
 package lotto.Message;
 
 public enum ErrorMessage {
-    INVALID_NUMBER("[ERROR] 숫자가 아닙니다."),
-    NOT_THOUSAND_UNIT("[ERROR] 1,000원 단위로 입력해야 합니다."),
-    NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    ERROR_INVALID_NUMBER("[ERROR] 숫자가 아닙니다."),
+    ERROR_NOT_THOUSAND_UNIT("[ERROR] 1,000원 단위로 입력해야 합니다."),
+    ERROR_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    ERROR_LOTTO_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    ERROR_DUPLICATES("[ERROR] 로또 번호는 중복될 수 없습니다.");
 
 
     public final String message;
