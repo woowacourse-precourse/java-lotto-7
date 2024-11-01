43ab177f324a811ff52564e236b0ae8c5afa70c9
rnrnr <rkdqudtjs6@naver.com>
2024-11-02 01:04:53 +0900

    test: 입력 및 예외 처리 테스트

--- src/main/java/lotto/controller/InputValidator.java
+++ src/main/java/lotto/controller/InputValidator.java
@@ -39,7 +39,7 @@ public class InputValidator {
 
     private void validateNumbersRange(int number) {
         if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private final InputValidator validator = new InputValidator();
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -24,4 +25,39 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("구입 금액에 음수가 들어오면 예외가 발생한다.")
+    void 구입_금액에_음수가_들어오면_예외가_발생한다() {
+        assertThatThrownBy(() -> validator.validatePurchaseAmount(-5000))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액이 음수이면 안됩니다.");
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아니라면 예외가 발생한다.")
+    void 구입_금액이_1000원_단위가_아니라면_예외가_발생한다() {
+        assertThatThrownBy(() -> validator.validatePurchaseAmount(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 번호의 숫자의 범위가 넘어가면 예외가 발생한다.")
+    void 로또_번호의_숫자_범위가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 보너스 번호의 숫자의 범위가 넘어가면 예외가 발생한다.")
+    void 로또_보너스_번호의_숫자_범위가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    /*
+     *
+     */
 }
