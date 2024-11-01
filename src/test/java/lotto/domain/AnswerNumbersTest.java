72d228ec36faef6564d5b4b968bd00d8c50f0bc0
imjunghun <dlawndgns9@naver.com>
2024-11-01 16:28:50 +0900

    test(AnswerNumbers): 로또 번호와 당첨 번호 비교 테스트

---
+++ src/test/java/lotto/domain/AnswerNumbersTest.java
@@ -0,0 +1,75 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.domain.lotto.Lotto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AnswerNumbersTest {
+
+    AnswerNumbers answerNumbers;
+
+    @BeforeEach
+    void setUp() {
+        WinningNumbers winningNumbers = WinningNumbers.of(
+                Lotto.of(
+                        List.of(1, 2, 3, 4, 5, 6)
+                )
+        );
+
+        BonusNumber bonusNumber = BonusNumber.valueOf(
+                winningNumbers, Number.of(7)
+        );
+
+        answerNumbers = AnswerNumbers.from(
+                winningNumbers, bonusNumber);
+    }
+
+    @DisplayName("객체 생성 테스트")
+    @Test
+    void 객체생성_테스트() {
+        assertThat(answerNumbers).isNotNull();
+    }
+
+    @DisplayName("당첨 번호와 로또 번호 비교 테스트")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provide")
+    void 비교하기(String title, List<Integer> numbers, Rank expectedRank) {
+        Lotto lotto = generateLotto(numbers);
+        Rank rank = answerNumbers.compare(lotto);
+
+        assertThat(rank).isNotNull();
+        assertThat(rank).isEqualTo(expectedRank);
+    }
+
+    static Stream<Arguments> provide() {
+        return Stream.of(
+                Arguments.of("1등",
+                        List.of(1, 2, 3, 4, 5, 6),
+                        Rank.FIRST),
+                Arguments.of("2등",
+                        List.of(1, 2, 3, 4, 5, 7),
+                        Rank.SECOND),
+                Arguments.of("3등",
+                        List.of(1, 2, 3, 4, 5, 8),
+                        Rank.THIRD),
+                Arguments.of("4등 보너스 포함",
+                        List.of(1, 2, 3, 4, 7, 8),
+                        Rank.FOURTH),
+                Arguments.of("4등 보너스 미포함",
+                        List.of(1, 2, 3, 4, 8, 9),
+                        Rank.FOURTH)
+        );
+    }
+
+    private Lotto generateLotto(List<Integer> numbers) {
+        return Lotto.of(numbers);
+    }
+}
