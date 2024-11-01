15d2555b1d587a2cab573ab74cd4bf1a051ddc18
DongchannN <ldc991104@gmail.com>
2024-11-01 20:09:35 +0900

    feat: 사용자 입력 구매 금액에 대한 유효성 검사 구현

---
+++ src/main/java/lotto/util/BudgetValidator.java
@@ -0,0 +1,32 @@
+package lotto.util;
+
+public class BudgetValidator {
+    private BudgetValidator() {
+    }
+
+    public static void validateInputBudget(String inputBudget) {
+        validateBudgetEmpty(inputBudget);
+        validateNumberFormat(inputBudget);
+        validateIntegerFormat(inputBudget);
+    }
+
+    private static void validateBudgetEmpty(String inputBudget) {
+        if (inputBudget == null || inputBudget.isEmpty()) {
+            throw new IllegalArgumentException("사용자의 입력이 비었습니다.");
+        }
+    }
+
+    private static void validateNumberFormat(String inputBudget) {
+        if (!inputBudget.matches("\\d+")) {
+            throw new IllegalArgumentException("구매 금액은 숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private static void validateIntegerFormat(String inputBudget) {
+        try {
+            Integer.parseInt(inputBudget);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구입 금액은 정수 형태여야 합니다.");
+        }
+    }
+}
