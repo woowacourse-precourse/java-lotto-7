5f85d95c467d087df0acd5017a9d0722e34c5f8f
mipangg <morganjeong119@gmail.com>
2024-11-02 01:23:59 +0900

    feat(LottoView): display purchase price guide message and call the method for input

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import lotto.model.InputModel;
+
 public class LottoView {
+    InputModel inputModel;
+
+    public LottoView() {
+        inputModel = new InputModel();
+    }
+
+    public int inputPriceView() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return (inputModel.getPrice())/1000;
+    }
 }
