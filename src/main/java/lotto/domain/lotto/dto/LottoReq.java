3389d36ccdc5e76637c6f62f62ffdb7b9698613d
ChiSaeHwang <cwh730909@gmail.com>
2024-11-01 15:13:57 +0900

    feat(LottoReq): 로또 클래스 입력 DTO 생성

--- src/main/java/lotto/domain/lotto/dto/LottoReq.java
+++ src/main/java/lotto/domain/lotto/dto/LottoReq.java
@@ -1,7 +1,47 @@
 package lotto.domain.lotto.dto;
 
+import java.util.List;
+
 public class LottoReq {
+    private final List<List<Integer>> purchasedNumbers;
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+    private final int cost;
 
+    private LottoReq(
+            List<List<Integer>> purchasedNumbers,
+            List<Integer> winningNumbers,
+            int bonusNumber,
+            int cost
+    ) {
+        this.purchasedNumbers = purchasedNumbers;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        this.cost = cost;
+    }
 
+    public static LottoReq of(
+            List<List<Integer>> purchasedNumbers,
+            List<Integer> winningNumbers,
+            int bonusNumber,
+            int cost
+    ) {
+        return new LottoReq(purchasedNumbers, winningNumbers, bonusNumber, cost);
+    }
 
+    public List<List<Integer>> getPurchasedNumbers() {
+        return purchasedNumbers;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public int getCost() {
+        return cost;
+    }
 }
