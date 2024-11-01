ef1bbcd69f81fa9e18b8cc0892b6154afd7f3160
kungbi <kipper12335@gmail.com>
2024-11-01 17:40:35 +0900

    test(ProfitReport): 각 로또 등수 별 로또 개수 구하는 기능 테스트

--- src/test/java/lotto/entity/ProfitReportTest.java
+++ src/test/java/lotto/entity/ProfitReportTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -41,36 +42,74 @@ class ProfitReportTest {
                 Arguments.of(List.of(30, 31, 32, 33, 34, 35), List.of(1, 2, 3, 4, 5, 6), 7, 0),
 
                 // 보너스 번호만 일치 (당첨되지 않음)
-                Arguments.of(List.of(7, 8, 9, 10, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 0)
-        );
+                Arguments.of(List.of(7, 8, 9, 10, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 0));
     }
 
 
     static Stream<Arguments> 수익률_계산_테스트_케이스() {
+        return Stream.of(Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))), // 구매한 로또 리스트
+                List.of(1, 2, 3, 4, 5, 6), // 당첨 번호 리스트
+                7, // 보너스 번호
+                200000000.0 // 예상 수익률 (2,000,000,000 / 1,000 * 100)
+        ), Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(10, 11, 12, 13, 14, 15))),
+                List.of(1, 2, 3, 4, 5, 6), 7, 100000000.0 // 예상 수익률 ((2,000,000,000 / 2,000) * 100)
+        ), Arguments.of(List.of(new Lotto(List.of(8, 9, 10, 11, 12, 13))), List.of(1, 2, 3, 4, 5, 6), 7, 0.0
+                // 예상 수익률 (0 / 1,000 * 100)
+        ));
+    }
+
+    static Stream<Arguments> 각_등수_별_당첨_횟수_계산_테스트_케이스() {
         return Stream.of(
+                // 1등 당첨 케이스
                 Arguments.of(
-                        List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))), // 구매한 로또 리스트
-                        List.of(1, 2, 3, 4, 5, 6), // 당첨 번호 리스트
-                        7, // 보너스 번호
-                        200000000.0 // 예상 수익률 (2,000,000,000 / 1,000 * 100)
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        Map.of(Prize.FIRST, 1)
                 ),
+                // 2등 당첨 (5개 + 보너스 번호 일치)
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        Map.of(Prize.SECOND, 1)
+                ),
+                // 3등 당첨 (5개 일치)
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        Map.of(Prize.THIRD, 1)
+                ),
+                // 4등 당첨 (4개 일치)
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 10, 11))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        Map.of(Prize.FOURTH, 1)
+                ),
+                // 5등 당첨 (3개 일치)
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 10, 11, 12))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        Map.of(Prize.FIFTH, 1)
+                ),
+                // 여러 등수 복합 테스트
                 Arguments.of(
                         List.of(
-                                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                                new Lotto(List.of(10, 11, 12, 13, 14, 15))
+                                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
+                                new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 3등
+                                new Lotto(List.of(1, 2, 3, 4, 10, 11)) // 4등
                         ),
                         List.of(1, 2, 3, 4, 5, 6),
                         7,
-                        100000000.0 // 예상 수익률 ((2,000,000,000 / 2,000) * 100)
-                ),
-                Arguments.of(
-                        List.of(new Lotto(List.of(8, 9, 10, 11, 12, 13))),
-                        List.of(1, 2, 3, 4, 5, 6),
-                        7,
-                        0.0 // 예상 수익률 (0 / 1,000 * 100)
+                        Map.of(Prize.FIRST, 1, Prize.SECOND, 1, Prize.THIRD, 1, Prize.FOURTH, 1)
                 )
         );
     }
+
 
     // 생성 테스트
 
@@ -225,9 +264,19 @@ class ProfitReportTest {
 
     }
 
-    @Test
-    void 성공__각_등수_별_당첨_횟수_계산() {
-        // calculateWinningCountsByRank
+    @ParameterizedTest
+    @MethodSource("각_등수_별_당첨_횟수_계산_테스트_케이스")
+    void 성공__각_등수_별_당첨_횟수_계산(List<Lotto> lottoInputs, List<Integer> winningNumbersInput, int bonusNumber,
+                             Map<Prize, Integer> expected) {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersInput, bonusNumber);
+        ProfitReport profitReport = new ProfitReport(lottoInputs, winningNumbers);
+
+        // when
+        Map<Prize, Integer> actual = profitReport.calculateWinningCountsByRank();
+
+        // then
+        assertEquals(expected, actual);
     }
 
 }
