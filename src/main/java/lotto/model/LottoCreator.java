fcfc259cf147cbd56a0b603dde1ea8ec91d5821f
Chan531 <chan657312@gmail.com>
2024-11-01 19:07:28 +0900

    feat(LottoCreator): LottoNumber 리스트 생성

--- src/main/java/lotto/model/LottoCreator.java
+++ src/main/java/lotto/model/LottoCreator.java
@@ -10,6 +10,13 @@ public class LottoCreator {
 	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int LOTTO_NUMBER_COUNT = 6;
 
+	public List<LottoNumber> createPurchasedLotto() {
+		List<Integer> randomNumbers = getRandomNumbers();
+		List<LottoNumber> purchasedLotto = randomNumbers.stream()
+				.map(LottoNumber::from)
+				.toList();
+	}
+
 	private List<Integer> getRandomNumbers() {
 		return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
 	}
