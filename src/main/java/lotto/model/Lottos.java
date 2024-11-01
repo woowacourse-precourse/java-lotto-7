dc19c50fb99572aee5c1733a7c1559b36aaf988a
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 03:58:05 +0900

    refactor($Lottos): Lotto 의 generateBy 를 사용하는 정적 팩토리 메서드 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
 
+import lotto.model.number_generator.RandomNumberGenerator;
+
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lottos {
 
@@ -10,6 +13,12 @@ public class Lottos {
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public static Lottos generateBy(RandomNumberGenerator randomNumberGenerator, int size) {
+        return new Lottos(IntStream.range(0, size)
+                .mapToObj(count -> Lotto.generateBy(randomNumberGenerator))
+                .toList());
     }
 
     public List<Score> calculateScore(WinningLotto winningLotto) {
