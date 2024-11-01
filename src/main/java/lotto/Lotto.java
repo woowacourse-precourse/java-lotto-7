c738f7e3a04c556ce74331163d2159085d544ea7
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-01 15:25:59 +0900

    feat : Lotto 예외처리 구현, 정적 팩토리 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,16 +5,32 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    private Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateEachNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public static Lotto createLotto(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateEachNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 음수일 수 없습니다.");
+            }
+            if (number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+
 }
