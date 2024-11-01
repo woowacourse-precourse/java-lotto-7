0c6c745bc31ca1eaa767a977dddedfe44fc7231c
exena <exena01@gmail.com>
2024-11-01 18:41:18 +0900

    chore: 결과 출력 약간 수정

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum Prize {
-    fifth(5_000), forth(50_000), third(1_500_500), second(30_000_000), first(2_000_000_000);
+    fifth(5_000), forth(50_000), third(1_500_000), second(30_000_000), first(2_000_000_000);
 
     Integer prize;
 
