ea69fe7a85e07661fc3414a7305ea71032da4b27
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 20:10:00 +0900

    test(LottoResult): 숫자를 맞힌 개수를 증가시키는 테스트를 정의하라

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -2,6 +2,9 @@ package lotto;
 
 import lotto.Model.LottoResult;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class LottoResultTest {
     private LottoResult lottoResult;
@@ -9,5 +12,45 @@ public class LottoResultTest {
     @BeforeEach
     void 로또_결과_객체를_생성한다() {
         lottoResult = new LottoResult();
+    }
+
+    @Test
+    void 숫자가_3개가_일치하는_경우의_개수를_1씩_증가시킨다() {
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(3, false);
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(3, true);
+
+        assertThat(lottoResult.getMatchThreeNumberCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 숫자가_4개가_일치하는_경우의_개수를_1씩_증가시킨다() {
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(4, false);
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(4, true);
+
+        assertThat(lottoResult.getMatchFourNumberCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 숫자가_5개가_일치하고_보너스_숫자가_다를_경우의_개수를_1씩_증가시킨다() {
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(5, false);
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(5, false);
+
+        assertThat(lottoResult.getMatchFiveNumberCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 숫자가_5개가_일치하고_보너스_숫자가_일치하는_경우의_개수를_1씩_증가시킨다() {
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(5, true);
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(5, true);
+
+        assertThat(lottoResult.getMatchFiveNumberAndBonusCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 숫자가_6개가_일치하는_경우의_개수를_1씩_증가시킨다() {
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(6, false);
+        lottoResult.increaseCountByNumberMatchedAndBonusMatched(6, true);
+
+        assertThat(lottoResult.getMatchSixNumberCount()).isEqualTo(2);
     }
 }
