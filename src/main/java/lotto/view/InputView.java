237859502739e5569159326f696ce4afae7c4c74
Su-daa <subin.kk019@gmail.com>
2024-11-02 01:43:22 +0900

    feat: 구입 금액 사용자 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,14 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
     public static void printPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
+    }
+
+    public static String inputPurchaseAmount() {
+        printPurchaseAmount();
+        return readLine();
     }
 }
