7d825146bd1a42afb3c321395d5222b4756a5bc1
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 13:28:06 +0900

    feat(InputView): 정수형 변환 삭제

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE="구입금액을 입력해 주세요.";
 
-    public int purchaseAmount(){
+    public String purchaseAmount(){
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
