d5a5ebb352947ca15cd72d31584ae0c8bca5c9cb
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-11-01 22:25:11 +0900

    feat: 발행한 로또를 저장하는 Lottos 클래스 구현

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Lottos {
+    public final List<Lotto> lottos;
+
+    public Lottos(int purchaseQuantity) {
+        lottos = new ArrayList<Lotto>();
+    }
+}
