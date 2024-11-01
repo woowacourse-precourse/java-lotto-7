f17b55fe47c1d213e887fe0cf8467589c1610792
daxxn <dahyeonhan4529@gmail.com>
2024-11-01 19:29:32 +0900

    test(service): update unit tests
    - Add unit tests for isLottoNumber, validateWinningNumber and extractWinningNumbers method in inputService

--- src/test/java/lotto/InputServiceTest.java
+++ src/test/java/lotto/InputServiceTest.java
@@ -3,6 +3,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lotto.error.ExceptionMessage;
 import lotto.service.InputService;
 import org.junit.jupiter.api.BeforeEach;
@@ -100,6 +103,67 @@ public class InputServiceTest {
     @Test
     void 로또_구입_금액으로_로또_수량_계산하기() {
         assertThat(inputService.calculateLottoCount(3000)).isEqualTo(3);
+    }
+
+    @DisplayName("번호가 로또 번호 범위 안의 문자이면 True를 반환한다.")
+    @Test
+    void 번호가_로또_번호_범위_안의_문자이면_True를_반환한다() {
+        assertThat(inputService.isLottoRange(30)).isTrue();
+    }
+
+    @DisplayName("번호가 로또 번호 범위 안의 문가 아니면 False를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 46})
+    void 번호가_로또_번호_범위_안의_문자가_아니면_False를_반환한다(int number) {
+        assertThat(inputService.isLottoRange(number)).isFalse();
+    }
+
+    @DisplayName("당첨 번호 요소가 유효하면 숫자로 반환된다. ")
+    @ParameterizedTest
+    @ValueSource(strings = {"4", "30", "43"})
+    void 당첨_번호_요소가_유효하면_숫자로_반환된다(String element) {
+        List<Integer> testWinningNumbers = new ArrayList<>(Arrays.asList(1, 10, 20));
+        int expected = Integer.parseInt(element);
+        assertThat(inputService.validateWinningNumber(testWinningNumbers, element)).isEqualTo(expected);
+    }
+
+    @DisplayName("당첨 번호 요소가 수가 아니면 예외가 발생한다. ")
+    @ParameterizedTest
+    @ValueSource(strings = {"~", "1번", ""})
+    void 당첨_번호_요소가_수가_아니면_예외가_발생한다(String element) {
+        List<Integer> testWinningNumbers = new ArrayList<>(Arrays.asList(1, 10, 20));
+        assertThatThrownBy(() -> inputService.validateWinningNumber(testWinningNumbers, element))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_MESSAGE_IS_NOT_NUMBER.toString());
+    }
+
+    @DisplayName("당첨 번호 요소가 로또 번호 범위의 수가 아니면 예외가 발생한다. ")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1", "46"})
+    void 당첨_번호_요소가_로또_번호_범위의_수가_아니면_예외가_발생한다(String element) {
+        List<Integer> testWinningNumbers = new ArrayList<>(Arrays.asList(1, 10, 20));
+        assertThatThrownBy(() -> inputService.validateWinningNumber(testWinningNumbers, element))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_MESSAGE_IS_NOT_IN_LOTTO_NUMBER_RANGE.toString());
+    }
+
+    @DisplayName("당첨 번호 요소가 중복이면 예외가 발생한다. ")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "10", "20"})
+    void 당첨_번호_요소가_중복이면_예외가_발생한다(String element) {
+        List<Integer> testWinningNumbers = new ArrayList<>(Arrays.asList(1, 10, 20));
+        assertThatThrownBy(() -> inputService.validateWinningNumber(testWinningNumbers, element))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.ERROR_MESSAGE_DUPLICATED_LOTTO_NUMBER.toString());
+    }
+
+    @DisplayName("유효한 당첨 번호 문자열이 들어온 경우 정상적으로 당첨 번호들을 추출한다.")
+    @Test
+    void 유효한_당첨번호_문자열이_들어온_경우_정상적으로_번호_추출한다() {
+        String testValue = "1,3,5,10,34,40";
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 3, 5, 10, 34, 40));
+        assertThat(inputService.extractWinningNumbers(testValue))
+                .isEqualTo(expected);
     }
 
 }
