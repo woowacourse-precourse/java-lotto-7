3ec3598e0d5cf4ec5611f3f9cfffb56e8e372020
jonghwanlee <kavae550@gmail.com>
2024-11-01 20:33:57 +0900

    refactor(utils):클래스명, 매개변수 명 및 메서드 명 변경
    통일성을 위하여 구매금액을 뜻하는 부분을 purchaseAmount로 통일

--- src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/utils/validator/PurchaseAmountValidator.java
@@ -4,31 +4,31 @@ import static lotto.exception.ErrorMessages.BEYOND_LIMIT;
 import static lotto.constants.LottoConstants.LOTTO_PRICE;
 import static lotto.constants.LottoConstants.PURCHASE_LIMIT;
 
-public class PurchasePriceValidator implements Validator<String> {
+public class PurchaseAmountValidator implements Validator<String> {
 
     private final Validator<String> positiveIntValidator;
 
-    public PurchasePriceValidator(Validator<String> positiveIntValidator) {
+    public PurchaseAmountValidator(Validator<String> positiveIntValidator) {
         this.positiveIntValidator = positiveIntValidator;
     }
 
     @Override
-    public void validate(String rawPurchasePrice) {
-        positiveIntValidator.validate(rawPurchasePrice);
+    public void validate(String rawPurchaseAmount) {
+        positiveIntValidator.validate(rawPurchaseAmount);
 
-        int purchasePrice = Integer.parseInt(rawPurchasePrice);
+        int purchasePrice = Integer.parseInt(rawPurchaseAmount);
         validateDividedByLottoPrice(purchasePrice);
         validateNotBeyondPurchaseLimit(purchasePrice);
     }
 
-    private void validateDividedByLottoPrice (int purchasePrice) {
-        if (purchasePrice % LOTTO_PRICE.getValue() != 0){
+    private void validateDividedByLottoPrice (int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE.getValue() != 0){
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateNotBeyondPurchaseLimit (int purchasePrice) {
-        if (purchasePrice > PURCHASE_LIMIT.getValue()){
+    private void validateNotBeyondPurchaseLimit (int purchaseAmount) {
+        if (purchaseAmount > PURCHASE_LIMIT.getValue()){
             throw new IllegalArgumentException(String.format(BEYOND_LIMIT.getMessage(), PURCHASE_LIMIT));
         }
     }
