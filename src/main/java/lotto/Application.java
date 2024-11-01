2fbe84483e584a97979701bcf004565c8f10ed36
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-01 20:55:40 +0900

    feat : Purchase in increments of 1000 won

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,11 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        inputView.getLottoPurchaseAmount();
+
+        try {
+            inputView.getLottoPurchaseAmount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
