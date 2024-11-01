b9069bd6f6af0800f218ffc3e102d2e95b4907df
kjyyjk <rlawnsdud920@gmail.com>
2024-11-01 19:20:12 +0900

    test: Lotto의 readNumberAscending 테스트
    - Lotto가 readNumberAscending 파라미터로 주어진 구분자를 기준으로
    자신의 번호를 오름차순으로 읽어 문자열로 정상 반환하는 지를 테스트한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +22,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또는_주어진_구분자로_자신의_번호를_오름차순으로_읽어_문자열로_반환할_수_있다")
+    @Test
+    public void readNumberAscending() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 6, 4, 3, 2, 5));
+        String delimiter = ", ";
+
+        //when
+        String result = lotto.readNumberAscending(delimiter);
+
+        //then
+        assertThat(result).isEqualTo("1, 2, 3, 4, 5, 6");
+    }
 }
