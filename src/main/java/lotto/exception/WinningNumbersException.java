eab82a1067adb2fbb0353c9fa388a9da2eb929d8
making-a-scene <4936joo@naver.com>
2024-11-01 15:12:36 +0900

    refactor: 로또 당첨 번호 및 보너스 번호 입력에 대한 예외 처리 구조 수정
    당첨 번호와 보너스 번호 입력 예외 각각에 대해 클래스를 구현한 후 이들을 상속받는 하위 예외 클래스를 작성하려고 하였으나
    당첨 번호 입력에만 관련된 예외는 당첨 번호가 6개가 입력되지 않은 경우 뿐임
    나머지 모든 경우는 로또 번호 각각에 대한 예외로, 두 예외 상황에 대한 처리가 비슷함
    따라서 기존에 당첨 번호 관련 예외를 처리하던 WinningNumbersException을 삭제하고, IllegalLottoNumberException으로 두 예외를 함께 처리

--- src/main/java/lotto/exception/WinningNumbersException.java
+++
@@ -1,9 +0,0 @@
-package lotto.exception;
-
-public class WinningNumbersException extends IllegalArgumentException {
-    private static final String HEADER = "[ERROR] 당첨 번호 입력이 올바르지 않습니다. ";
-
-    public WinningNumbersException(String message) {
-        super(HEADER + message);
-    }
-}
--- src/main/java/lotto/exception/WinningNumbersQuantityException.java
+++ src/main/java/lotto/exception/WinningNumbersQuantityException.java
@@ -1,6 +1,6 @@
 package lotto.exception;
 
-public class WinningNumbersQuantityException extends WinningNumbersException {
+public class WinningNumbersQuantityException extends IllegalLottoNumberException {
     private static final String MESSAGE = "로또 번호는 6개여야 합니다.";
 
     public WinningNumbersQuantityException() {
