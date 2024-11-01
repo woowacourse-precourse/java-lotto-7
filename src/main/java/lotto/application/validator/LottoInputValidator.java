b45260d6c4b96f4774b23552977b253d8c80b629
minSsan <msp1125@hanyang.ac.kr>
2024-11-01 18:38:52 +0900

    feat(LottoInputValidator): 사용자 입력 검증 클래스 추가

---
+++ src/main/java/lotto/application/validator/LottoInputValidator.java
@@ -0,0 +1,14 @@
+package lotto.application.validator;
+
+import lotto.domain.cost.Cost;
+import lotto.domain.lotto.WinningLottoImpl;
+
+public class LottoInputValidator {
+    public Cost validateCost(final String input) {
+        return null;
+    }
+
+    public WinningLottoImpl validateWinningLotto(String numbers, String bonusInput) {
+        return null;
+    }
+}
