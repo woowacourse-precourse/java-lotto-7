6f1e5674922b2e6da76d6a03197fcc2cead43c61
making-a-scene <4936joo@naver.com>
2024-11-01 21:16:12 +0900

    fix(ResultController.makeWinningStatistic()): 당첨이 한 번도 나오지 않은 등수의 정보는 저장되지 않던 문제 수정
    prizeStatus에 있던 값이 Map의 key 값으로 이미 존재하면 이전 값에 1 더해서 갱신,
    존재하지 않으면 0으로 초기화하는 방식으로 구현하였으나
    prizeStatus에 값이 하나도 없는 등수에 대해서는 정보가 저장되지 않아
    initMap()이라는 메소드를 추가 정의해 모든 등수에 대해 value 값을 0으로 초기화한 후 기능 수행

--- src/main/java/lotto/controller/ResultController.java
+++ src/main/java/lotto/controller/ResultController.java
@@ -9,12 +9,23 @@ import java.util.Map;
 public class ResultController {
 
     public Map<Prize, Integer> makeWinningStatistic(List<Prize> prizeStatus) {
-        Map<Prize, Integer> result = new HashMap<>();
+        Map<Prize, Integer> result = initMap();
         for (Prize prize : prizeStatus) {
             if (prize == Prize.LOSE) {
                 continue;
             }
-            result.put(prize, result.getOrDefault(prize, 0) + 1);
+            result.put(prize, result.get(prize) + 1);
+        }
+        return result;
+    }
+
+    private Map<Prize, Integer> initMap() {
+        Map<Prize, Integer> result = new HashMap<>();
+        for (Prize prize : Prize.values()) {
+            if (prize == Prize.LOSE) {
+                continue;
+            }
+            result.put(prize, 0);
         }
         return result;
     }
---
+++ src/test/java/lotto/WinningStatisticTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+
+import lotto.controller.ResultController;
+import lotto.domain.Prize;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningStatisticTest {
+    private final ResultController resultController = new ResultController();
+
+    @Test
+    void 등수별_당첨_횟수_합산() {
+        List<Prize> prizeStatus = Stream.of(
+                Prize.FIRST, Prize.FIRST, Prize.FIRST,
+                Prize.SECOND,
+                Prize.THIRD, Prize.THIRD, Prize.THIRD, Prize.THIRD, Prize.THIRD,
+                Prize.FOURTH, Prize.FOURTH)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        Map<Prize, Integer> statistic = resultController.makeWinningStatistic(prizeStatus);
+        assertThat(statistic.get(Prize.FIRST)).isEqualTo(3);
+        assertThat(statistic.get(Prize.SECOND)).isEqualTo(1);
+        assertThat(statistic.get(Prize.THIRD)).isEqualTo(5);
+        assertThat(statistic.get(Prize.FOURTH)).isEqualTo(2);
+        assertThat(statistic.get(Prize.FIFTH)).isEqualTo(0);
+    }
+
+}
