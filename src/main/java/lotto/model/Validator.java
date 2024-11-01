da83fdc90319f50be66db3f377d4e9dccae6d1d3
soeun2537 <soeun2537@gmail.com>
2024-11-01 22:12:08 +0900

    feat: 로또 구입 금액 유효성 검사 - 배수 아님 로직 추가

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -1,7 +1,17 @@
 package lotto.model;
 
+import static lotto.constant.message.ErrorMessage.*;
+import static lotto.constant.core.ValidatorConstant.*;
+
 public class Validator {
 
-    public static void validateLottoPurchaseAmount(Integer lottoAmount) {;
+    public static void validateLottoPurchaseAmount(Integer lottoAmount) {
+        validateMultiple(lottoAmount);
+    }
+
+    private static void validateMultiple(Integer number) {
+        if ((number % LOTTO_PRICE.getIntegerValue()) != 0) {
+            throw new IllegalArgumentException(INVALID_MULTIPLE_AMOUNT.getMessage(LOTTO_PRICE.getIntegerValue()));
+        }
     }
 }
