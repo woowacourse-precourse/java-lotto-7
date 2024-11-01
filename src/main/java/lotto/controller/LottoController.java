25c7e31a95ebeb434c723aa79f0094d70b27366c
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 20:37:25 +0900

    feat(LottoController, OutputView): 로또를 구입하고 출력하는 기능을 구현하라

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,14 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
 import lotto.model.PurchaseCost;
 import lotto.utility.PurchaseCostParser;
+import lotto.utility.RandomNumberCreator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
 
@@ -13,10 +18,23 @@ public class LottoController {
 
         int purchasedLottoCount = purchaseCost.calculateBuyableLottoCount();
         OutputView.outputPurchasedLottoCount(purchasedLottoCount);
+
+        purchaseLotto(purchasedLottoCount);
     }
 
     public int inputPurchaseCost() {
         String rawPurchaseCost = InputView.inputPurchaseCost();
         return PurchaseCostParser.parseToInteger(rawPurchaseCost);
+    }
+
+    public List<Lotto> purchaseLotto(int purchaseCount) {
+        List<Lotto> purchasedLottos = new ArrayList<>();
+        for (int i=0; i<purchaseCount; i++) {
+            List<Integer> randomlyGeneratedNumbers = RandomNumberCreator.generateRandomNumbers();
+            OutputView.outputPurchaseOneLottoResult(randomlyGeneratedNumbers);
+            purchasedLottos.add(new Lotto(randomlyGeneratedNumbers));
+        }
+
+        return purchasedLottos;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,16 @@
 package lotto.view;
 
+import java.util.Collections;
+import java.util.List;
+
 public class OutputView {
 
     public static void outputPurchasedLottoCount(int purchasedLottoCount) {
         System.out.println(purchasedLottoCount + "개를 구매했습니다.");
+    }
+
+    public static void outputPurchaseOneLottoResult(List<Integer> purchasedLottoNumbers) {
+        Collections.sort(purchasedLottoNumbers);
+        System.out.println(purchasedLottoNumbers);
     }
 }
