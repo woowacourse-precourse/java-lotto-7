9d9631edeb26066f9f1d27b951d662e0594c1702
Beomseok Noh <prodigy0831@naver.com>
2024-11-01 20:47:03 +0900

    test(WinningNumberValidationTest): 보너스 번호 입력 및 유효성 검증 로직 테스트

--- src/test/java/validation/WinningNumberValidationTest.java
+++ src/test/java/validation/WinningNumberValidationTest.java
@@ -16,7 +16,7 @@ public class WinningNumberValidationTest {
     @ValueSource(strings = {"a,1", "a,b,c,d,e,f"})
     @NullAndEmptySource
     @DisplayName("구매 금액이 null이거나 빈 값, 또는 문자가 입력될 때 예외 처리")
-    void validateNullOrEmptyOrStringTest(String input) {
+    void NullOrEmptyOrStringWinningNumberTest(String input) {
         assertThatThrownBy(() -> {
             WinningNumberValidation.parseValidatedWinningNumber(input);
         })
@@ -26,7 +26,7 @@ public class WinningNumberValidationTest {
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3,4,5,6,7", "1,2,3,4,5"})
     @DisplayName("입력된 숫자가 6개가 아닐 때 예외 처리")
-    void validateInvalidSizeTest(String input) {
+    void InvalidWinningNumberSizeTest(String input) {
         assertThatThrownBy(() -> {
             WinningNumberValidation.parseValidatedWinningNumber(input);
         })
@@ -36,7 +36,7 @@ public class WinningNumberValidationTest {
     @ParameterizedTest
     @ValueSource(strings = {"-1,2,3,4,5,6", "1,2,3,4,5,66"})
     @DisplayName("입력된 숫자가 1~45사이의 숫자가 아닐 때 예외 처리")
-    void validateRangeTest(String input) {
+    void InvalidWinningNumberRangeTest(String input) {
         assertThatThrownBy(() -> {
             WinningNumberValidation.parseValidatedWinningNumber(input);
         })
@@ -46,10 +46,21 @@ public class WinningNumberValidationTest {
     @ParameterizedTest
     @ValueSource(strings = {"1,  2, 3,,4,5,6"})
     @DisplayName("공백이 있어도 유효한 숫자면 예외를 발생시키지 않는다.")
-    void testValidNameLength(String input) {
+    void ValidWinningNumberLengthTest(String input) {
         assertThatNoException().isThrownBy(() -> {
             WinningNumberValidation.parseValidatedWinningNumber(input);
         });
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"0", "46", "abc"})
+    @DisplayName("보너스 번호가 null이나 빈 값이거나, 범위를 넘거나, 숫자가 아닌 경우 예외 발생")
+    void InvalidBonusNumberTest(String input) {
+        assertThatThrownBy(() -> {
+            WinningNumberValidation.parseValidatedBonusNumber(input);
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
