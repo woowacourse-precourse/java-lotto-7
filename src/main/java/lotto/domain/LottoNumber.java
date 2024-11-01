0df858f37737dfa6624868da2a34a8bbb4465b9d
kssk3 <kss8014@daum.net>
2024-11-01 20:31:49 +0900

    Build LottoNumber

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -20,6 +20,13 @@ public class LottoNumber implements Comparable<LottoNumber> {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
 
     @Override
     public int hashCode() {
