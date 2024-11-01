ead7983174c21e4c1b4a7dd1bc845c6971fd2ffb
soeun2537 <soeun2537@gmail.com>
2024-11-02 04:42:59 +0900

    feat: RankCounter 클래스 생성 및 초기화 로직 추가

---
+++ src/main/java/lotto/model/RankCounter.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import lotto.constant.category.Rank;
+
+public class RankCounter {
+    private final Map<Rank, Integer> rankCounter;
+
+    private RankCounter(Map<Rank, Integer> rankCounter) {
+        this.rankCounter = rankCounter;
+    }
+
+    public static RankCounter create() {
+        return new RankCounter(initializeCounter());
+    }
+
+    private static Map<Rank, Integer> initializeCounter() {
+        Map<Rank, Integer> counter = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            counter.put(rank, 0);
+        }
+        return counter;
+    }
+
+    public Integer getRankCount(Rank rank) {
+        return rankCounter.get(rank);
+    }
+}
