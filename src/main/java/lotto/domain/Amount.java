70db070233872666186b3a808658f44e7b62d55a
JooYoung <beachu23@naver.com>
2024-11-01 20:46:33 +0900

    refactor(Amount): 구입 금액 기능을 도메인으로 리팩토링하라
    LottoMachine의 int 타입 필드 amount를 별도의 도메인 클래스로 정의한다.
    Amount는 value 유효성 검증을 수행한다.
    
    BREACKING CHANGE:
    	변경 사항:
    	- Amount 클래스를 구현한다.
    
    	예외 처리:
    	- 구입 금액이 0이면 IllegalArgumentException이 발생한다.
    	- 구입 금액이 음수면 IllegalArgumentException이 발생한다.
    	- 구입 금액이 1000으로 나누어 떨어지지 않으면 IllegalArgumentException이 발생한다.

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public class Amount {
+
+    public static final String AMOUNT_ERROR_MSG = "[ERROR] 구입 금액은 1000(원) 단위의 숫자입니다. 예: 14000";
+    public static final int LOTTO_PRICE = 1000;
+
+    private final int value;
+
+    public Amount(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+
+        if (value <= 0 || value % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(AMOUNT_ERROR_MSG);
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
