93aea51fca10a7df3951c6887edb9f1bcc3e6887
Changsu Park <gd12091@gmail.com>
2024-11-01 14:27:13 +0900

    feat: 상수 클래스 정의

---
+++ src/main/java/lotto/constants/CommonConstants.java
@@ -0,0 +1,14 @@
+package lotto.constants;
+
+public class CommonConstants {
+
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static final int LOTTO_PRICE = 1000;
+
+    public static final String INPUT_PROMPT_PURCHASE_AMOUNT = "구매 금액을 입력해 주세요.";
+    public static final String INPUT_PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+}
