ecf383a22d2ac1779ca0c54c7ca1dcd29791dfd4
DongYeop <s01054830455@gmail.com>
2024-11-02 03:09:26 +0900

    test: LottoRankTest 로또 순위 반환 테스트 추가

---
+++ src/test/java/lotto/model/LottoRankTest.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoRankTest {
+
+    @DisplayName("당첨 번호 일치 개수와 보너스 번호의 일치 여부로 로또 순위를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "6, false, FIRST",
+            "5, true, SECOND",
+            "5, false, THIRD",
+            "4, false, FOURTH",
+            "3, false, FIFTH",
+            "2, false, NO_LUCK"
+    })
+    void matchRank(int matchLottoCount, boolean bonusNumber, LottoRank lank) {
+        //when
+        LottoRank lottoRank = LottoRank.matchRank(matchLottoCount, bonusNumber);
+
+        //then
+        assertThat(lottoRank).isEqualTo(lank);
+    }
+}
