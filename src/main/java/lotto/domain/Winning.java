66ba8a5dd10f784e16b742f5d035eb0688d765d0
taek2222 <oot309@daum.net>
2024-11-01 15:34:25 +0900

    feat(Winning): 당첨 번호 쉼표 구분 기능 구현
    - 당첨 번호 쉼표 기준으로 구분후, List 로 Lotto 번호 구현했습니다.

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Winning {
+
+    private static final String DELIMITER = ",";
+    private final Lotto lotto;
+
+    public Winning(String input) {
+        List<Integer> numbers = convertToIntegerList(input);
+        this.lotto = new Lotto(numbers);
+    }
+
+    private List<Integer> convertToIntegerList(String input) {
+        String[] inputSplit = input.split(DELIMITER);
+        return Arrays.stream(inputSplit)
+                .map(Integer::parseInt)
+                .toList();
+    }
+}
