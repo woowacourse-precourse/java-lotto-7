408d805a70c06b22a88406e8c8531432a56ce2f3
yush <yush2202@gmail.com>
2024-11-01 20:04:35 +0900

    feat($ Prizes): prizes의 보너스 기능 추가
    어떤 상이 bouns 볼을 맞춰야 수상 가능한지 구현

--- src/main/java/lotto/constants/Prizes.java
+++ src/main/java/lotto/constants/Prizes.java
@@ -2,18 +2,20 @@ package lotto.constants;
 
 public enum Prizes {
 
-    FIRST_PRIZE(2_000_000_000, 6),
-    SECOND_PRIZE(30_000_000, 5),
-    THIRD_PRIZE(1_500_000, 5),
-    FOURTH_PRIZE(50_000, 4),
-    FIFTH_PRIZE(5_000, 3);
+    FIRST_PRIZE(2_000_000_000, 6, false),
+    SECOND_PRIZE(30_000_000, 5, true),
+    THIRD_PRIZE(1_500_000, 5, false),
+    FOURTH_PRIZE(50_000, 4, false),
+    FIFTH_PRIZE(5_000, 3, false);
 
     private final int money;
     private final int count;
+    private final boolean bonus;
 
-    Prizes(int money, int count) {
+    Prizes(int money, int count, boolean bonus) {
         this.money = money;
         this.count = count;
+        this.bonus = bonus;
     }
 
     public int getMoney() {
@@ -22,5 +24,9 @@ public enum Prizes {
 
     public int getCount() {
         return this.count;
+    }
+
+    public boolean getBonus() {
+        return bonus;
     }
 }
