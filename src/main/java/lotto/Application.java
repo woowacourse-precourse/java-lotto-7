0f0f45f32d75785cdf8f5c1af3f50e000c700cae
Chaeyoung714 <202102760@hufs.ac.kr>
2024-11-01 14:31:24 +0900

    feat : 1.1 로또 구입 금액 입력 시 예외사항 추가, 변경
    양수 검증하는 로직도 1.1에서 하기로 다시 변경, 금액 10자리 초과 시 예외 처리 추가
    변경 사항 README, README_CHANGELOG 반영
    변경 사항에 맞는 테스트 추가, 통과

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,9 +11,14 @@ public class Application {
 
     public static void validateInputInteger(String inputInteger) {
         if (inputInteger == null
-                || inputInteger.isBlank()
-                || !inputInteger.matches("^-?\\d+$")) {
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해주세요.");
+                || inputInteger.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 금액을 입력해주세요.");
+        }
+        if (!inputInteger.matches("^[1-9]\\d*$")) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해주세요.");
+        }
+        if (inputInteger.length() > 10) {
+            throw new IllegalArgumentException("[ERROR] 10자리 이하의 금액을 입력해주세요.");
         }
     }
 }
--- src/test/java/lotto/AdditionalApplicationTest.java
+++ src/test/java/lotto/AdditionalApplicationTest.java
@@ -22,20 +22,37 @@ public class AdditionalApplicationTest {
         String testString = null;
 
         assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
-                .withMessage("[ERROR] 정수를 입력해주세요.");
+                .withMessage("[ERROR] 금액을 입력해주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     void failIfInputIntegerIsBlank(String testString) {
         assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
-                .withMessage("[ERROR] 정수를 입력해주세요.");
+                .withMessage("[ERROR] 금액을 입력해주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-20.2, -1.0, 0.5, 2.0, 2.5, 3.24321"})
-    void failIfInputIntegerIsNotInteger(String testString) {
+    void 금액이_정수가_아니면_예외(String testString) {
         assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
-                .withMessage("[ERROR] 정수를 입력해주세요.");
+                .withMessage("[ERROR] 양의 정수를 입력해주세요.");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-20, -1, 0"})
+    void 금액이_양수가_아니면_예외(String testString) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testString))
+                .withMessage("[ERROR] 양의 정수를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10000000000", "123456789123456789"})
+    void 금액이_10자리를_넘어가면_예외(String testNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Application.validateInputInteger(testNumber))
+                .withMessage("[ERROR] 10자리 이하의 금액을 입력해주세요.");
+    }
+
+
+
 }
