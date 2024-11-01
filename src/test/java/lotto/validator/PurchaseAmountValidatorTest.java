e6e11857f9e85e9d412f3d8ce8ee72ccdff11a66
OhYeHyun <ohyhohyl@naver.com>
2024-11-02 00:21:11 +0900

    test(PurchaseAmountValidatorTest): 구입 금액 공백 포함하는 검증 테스트 추가

--- src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
+++ src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
@@ -9,6 +9,14 @@ public class PurchaseAmountValidatorTest {
     private PurchaseAmountValidator purchaseAmountValidator;
 
     @Test
+    @DisplayName("구입 금액에 공백이 포함되었는지 확인")
+    void 구입_금액_공백_테스트() {
+        assertThatThrownBy(() -> {
+            purchaseAmountValidator = new PurchaseAmountValidator(" 3000");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("[ERROR] 구입 금액에 공백을 허용하지 않습니다.");
+    }
+
+    @Test
     @DisplayName("구입 금액이 숫자인지 확인")
     void 구입_금액_숫자_테스트() {
         assertThatThrownBy(() -> {
