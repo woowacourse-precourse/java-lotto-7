537aa11c06a851323ca065f3316bd931d2a80504
plan11plan <tjtfy7028@gmail.com>
2024-11-01 19:47:46 +0900

    feat: 로또 가격 도메인 구현
    - 기본 로또 가격(1000원) 정의
    - 구매 가능 여부 검증 기능 추가
    - 구매 가능 로또 수량 계산 기능 구현

---
+++ src/main/java/lotto/domain/LottoPrice.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+
+import lotto.ThousandWons;
+
+public class LottoPrice {
+    private final static int BASIC_PRICE = 1000;
+    private final int amount;
+
+    private LottoPrice(int amount) {
+        this.amount = amount;
+    }
+
+    public static LottoPrice basic() {
+        return new LottoPrice(BASIC_PRICE);
+    }
+
+    public boolean isAffordable(ThousandWons money) {
+        return money.isMuchThanOrEqual(amount);
+    }
+
+    public int calculateLottoCount(ThousandWons money) {
+        return money.divide(amount);
+    }
+}
