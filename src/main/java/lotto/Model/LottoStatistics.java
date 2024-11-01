1ae78ceee7fbda78ba82de162b69580f3d90b95c
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-01 18:40:49 +0900

    feat(LottoStatistics): 당첨 결과별 카운트 증가 기능 구현

---
+++ src/main/java/lotto/Model/LottoStatistics.java
@@ -0,0 +1,27 @@
+package lotto.Model;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoStatistics {
+    private final Map<LottoResult, Integer> resultCounts = new EnumMap<>(LottoResult.class);
+    private final int purchaseAmount;
+
+    public LottoStatistics(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        initializeResultCounts();
+    }
+
+    private void initializeResultCounts() {
+        for(LottoResult result : LottoResult.values()) {
+            resultCounts.put(result, 0);
+        }
+    }
+
+    private void updateResultCount(LottoResult result) {
+        if(result != null){
+            resultCounts.put(result, resultCounts.get(result) + 1);
+        }
+    }
+}
