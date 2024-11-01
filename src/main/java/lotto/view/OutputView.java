7a010d98c4387c4bf6be164ca33843275fbd8d42
sangjun <sangjoon12345@gmail.com>
2024-11-01 10:10:31 +0900

    feat: 로또 구매결과 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,25 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class OutputView {
     public static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
     public static final String INFORM_LOTTO_COUNT = "%d개를 구매했습니다.";
+    public static final String NEW_LINE = System.lineSeparator();
 
     public void requestLottoPrice() {
         System.out.println(REQUEST_LOTTO_PRICE);
     }
 
+    public void displayLottoPurchaseResult(List<Lotto> lottoSets) {
+        System.out.print(NEW_LINE);
+        System.out.printf(INFORM_LOTTO_COUNT + NEW_LINE, lottoSets.size());
+        for (Lotto lotto : lottoSets) {
+            System.out.println(lotto.getLotto());
+        }
+    }
 
 
 }
