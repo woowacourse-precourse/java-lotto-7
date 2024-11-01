1bdaff3df1c4e9e2f355f28f8feb33fc1d604351
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:26:05 +0900

    feat(PurchasedLottos): 구입한 로또를 모은 일급컬렉션을 구현하라

---
+++ src/main/java/lotto/Model/PurchasedLottos.java
@@ -0,0 +1,11 @@
+package lotto.Model;
+
+import java.util.List;
+
+public class PurchasedLottos {
+    private List<Lotto> purchasedLottos;
+
+    public PurchasedLottos(List<Lotto> purchasedLottos) {
+        this.purchasedLottos = purchasedLottos;
+    }
+}
