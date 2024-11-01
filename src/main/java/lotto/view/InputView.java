2ecd3948cb18a94b4ab596b625603e1f035f9463
wogus <ind07152@naver.com>
2024-11-01 12:39:30 +0900

    fix(InputView): 출력 버그 수정
    입력을 받은 후 개행 문자를 추가한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public final class InputView {
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static String getPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
-        return Console.readLine();
+        String amount = Console.readLine();
+        System.out.println();
+        return amount;
     }
 }
