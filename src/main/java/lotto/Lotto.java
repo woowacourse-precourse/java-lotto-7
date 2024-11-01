014e395db11bcd09eb5a49aed6b24c008bf98f79
KF97 <aysul0@naver.com>
2024-11-01 18:43:16 +0900

    feat: 로또 검증 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -13,6 +14,22 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자 6개여야 합니다.");
+            }
+        }
+
+        int number = numbers.getFirst();
+
+        for (int i = 1; i < numbers.size(); i++) {
+            if (number == numbers.get(i)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않은 숫자 6개여야 합니다.");
+            }
+
+            number = numbers.get(i);
         }
     }
 
