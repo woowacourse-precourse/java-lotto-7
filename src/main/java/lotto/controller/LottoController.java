b5e01ba36e914193009f8061f36f6a172255819d
jaehwannnnnn <seanjung0915@gmail.com>
2024-11-01 21:00:57 +0900

    feat(LottoController): Controller 클래스 작성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,44 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.utility.LottoComparison;
+import lotto.utility.LottoGenerator;
+import lotto.validator.LottoValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+    public void run() {
+        int price = InputView.getPrice();
+        LottoValidator.validatePrice(price);
+        int amount = price / 1000;
+
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            lottos.add(LottoGenerator.generateLottoNumbers());
+        }
+        OutputView.printLottoNumbers(amount, lottos.stream()
+                .map(Lotto::getNumbers)
+                .toList());
+
+        Lotto winningLotto = InputView.getWinningNumbers();
+        int bonusNumber = InputView.getBonusNumber();
+
+        LottoValidator.validateWinningNumbers(winningLotto.getNumbers(), bonusNumber);
+
+        Map<LottoRank, Integer> rankCounts = new HashMap<>();
+        for (Lotto userLotto : lottos) {
+            LottoRank rank = LottoComparison.compareLottoNumbers(winningLotto.getNumbers(), bonusNumber, userLotto.getNumbers());
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        }
+        double profitRate = LottoComparison.calculateProfitRate(rankCounts, price);
+        OutputView.printWinningStatistics(rankCounts, profitRate);
+    }
+}
