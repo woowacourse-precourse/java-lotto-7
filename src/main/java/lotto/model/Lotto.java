8344c816e8f97c02d32442e3cebacbc84e5ea42a
sangjun <sangjoon12345@gmail.com>
2024-11-01 10:40:57 +0900

    feat: 로또 구매시 생성자 방식으로 오름차순 정렬 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +17,9 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+    public List<Integer> getLotto(){
+        return numbers;
+    }
 
-    // TODO: 추가 기능 구현
+
 }
