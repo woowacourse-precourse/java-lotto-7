629640cc758e10757ad711ba9431c79fb8d3865c
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-11-01 20:26:52 +0900

    refactor(LottoNumberGenerator): 상수를 이용해 가독성 증가

--- src/test/java/lotto/model/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/model/LottoNumberGeneratorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.constants.ModelContstants.*;
 import static org.assertj.core.api.Assertions.*;
 
 class LottoNumberGeneratorTest {
@@ -13,14 +14,17 @@ class LottoNumberGeneratorTest {
     @Test
     void 로또_번호_개수_테스트() {
         List<Integer> lottoNumbers = LottoNumberGenerator.generate();
-        assertThat(lottoNumbers).hasSize(6);
+        assertThat(lottoNumbers).hasSize(LOTTO_COUNT.getValue());
     }
 
     @DisplayName("로또 번호가 1부터 45 사이의 숫자인지 테스트")
     @Test
     void 로또_번호_범위_테스트() {
         List<Integer> lottoNumbers = LottoNumberGenerator.generate();
-        boolean allInRange = lottoNumbers.stream().allMatch(number -> number >= 1 && number <= 45);
+        boolean allInRange = lottoNumbers.stream()
+                .allMatch(number ->
+                        number >= LOTTO_STARTING_RANGE.getValue() && number <= LOTTO_END_RANGE.getValue());
         assertThat(allInRange).isTrue();
     }
+
 }
