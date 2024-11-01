899e919dd3f78f5e3344690ae0a4c3ad193d30e3
qudcks8084 <qudcks8084@naver.com>
2024-11-01 23:44:36 +0900

    Fix : 기존에 구현되어있던 validate 내부 수정
    - 기존의 로직을 제외하고, LottoValidator에서 함수를 호출하도록 변경
    - 입력값에 대한 총 2개의 예외 처리 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.util.validator.LottoValidator;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -13,9 +15,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        LottoValidator.validateLottoNumberIsValid(numbers);
+        LottoValidator.validateLottoHasSixNumbers(numbers);
     }
 
     public String toString(){
