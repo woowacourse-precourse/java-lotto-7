c9657838f049dd892be9d70bc9c483b04ea511e2
slay <palgalow@gmail.com>
2024-11-01 18:24:01 +0900

    feat(Lottos): 로또 생성 기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -2,12 +2,22 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.util.RandomNumbers;
 
 public class Lottos {
 
     private List<Lotto> lottos;
+    private final int lottoCount;
 
-    public Lottos() {
+    public Lottos(int purchaseAmount) {
         this.lottos = new ArrayList<>();
+        this.lottoCount = purchaseAmount/1000;
+    }
+
+    public Lotto createLotto() {
+        List<Integer> numbers = RandomNumbers.generateRandomNumbers();
+        Lotto lotto = new Lotto(numbers);
+        lotto.sortNumbers(lotto.getNumbers());
+        return lotto;
     }
 }
