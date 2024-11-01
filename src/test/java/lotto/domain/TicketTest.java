9b3d27095145b9baa5f9b52dbd6f8c0d0bce3ec6
yush <yush2202@gmail.com>
2024-11-01 18:32:34 +0900

    test($ TicketTest): 티켓 클래스 테스트
    입력받은 돈에 따라 티켓을 정확하게 발급하는지 테스트

---
+++ src/test/java/lotto/domain/TicketTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.vo.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TicketTest {
+
+    @ParameterizedTest
+    @DisplayName("올바른 티켓 갯수를 반환한다.")
+    @ValueSource(ints = {1000, 3000, 5000, 10000, 99000})
+    void 올바른_티켓_갯수를_반환한다(int target) {
+        Money money = new Money(target);
+        Ticket ticket = Ticket.from(money);
+        assertThat(ticket.getTicket()).isEqualTo(target / 1_000);
+    }
+}
