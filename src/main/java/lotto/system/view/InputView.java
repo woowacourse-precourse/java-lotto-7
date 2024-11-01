a700d983590227372ed418f2c706f2e5d8c54260
chosunghyun18 <krismon@naver.com>
2024-11-01 19:17:55 +0900

    refcat : change sout into systeIO show message

--- src/main/java/lotto/system/view/InputView.java
+++ src/main/java/lotto/system/view/InputView.java
@@ -3,19 +3,19 @@ package lotto.system.view;
 public class InputView {
 
     public String  showInputMoneyMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        SystemIO.showMessageToConsole("구입금액을 입력해 주세요.");
         return SystemIO.readUserInput();
     }
 
     public String showInputLottoNumberMessage() {
-        System.out.println("");
-        System.out.println("당첨 번호를 입력해 주세요.");
+        SystemIO.showMessageToConsole("");
+        SystemIO.showMessageToConsole("당첨 번호를 입력해 주세요.");
         return SystemIO.readUserInput();
     }
 
     public String showInputBonusNumberMessage() {
-        System.out.println("");
-        System.out.println("보너스 번호를 입력해 주세요.");
+        SystemIO.showMessageToConsole("");
+        SystemIO.showMessageToConsole("보너스 번호를 입력해 주세요.");
         return SystemIO.readUserInput();
     }
 }
