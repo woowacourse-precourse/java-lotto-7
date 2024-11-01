af8c4cebcaf186009e5280b5624e26bbaf896ae6
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-02 01:05:38 +0900

    test : Wallet 의존성 주입 변경으로 인한 테스트 코드 수정

--- src/test/java/lotto/domain/WalletTest.java
+++ src/test/java/lotto/domain/WalletTest.java
@@ -2,18 +2,13 @@ package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import lotto.domain.calculators.TicketCalculator;
-import lotto.domain.calculators.TicketCalculatorImpl;
-import lotto.domain.calculators.YieldCalculator;
 import org.junit.jupiter.api.Test;
 
 class WalletTest {
-    private final TicketCalculator ticketCalculator = new TicketCalculatorImpl();
-    private final YieldCalculator yieldCalculator = new YieldCalculator();
 
     @Test
     void 최소_구입_금액_미달시_예외_처리() {
-        assertThatThrownBy(() -> new Wallet(100, ticketCalculator, yieldCalculator))
+        assertThatThrownBy(() -> new Wallet(100))
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
@@ -21,7 +16,7 @@ class WalletTest {
 
     @Test
     void 금액_단위가_맞지_않을시_예외_처리() {
-        assertThatThrownBy(() -> new Wallet(1001, ticketCalculator, yieldCalculator))
+        assertThatThrownBy(() -> new Wallet(1001))
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
