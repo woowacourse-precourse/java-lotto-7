beb7a2ddc10d505d57be3a70148c3031a3fa3301
yybmion <yyb400@naver.com>
2024-11-01 17:43:20 +0900

    test: 로또 입력 검증 테스트(정상적인 입력값)
    정상적인 구매 입력값이 입력되면 정상 처리되는 테스트코드 작성.

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import lotto.Input.InputValidator;
@@ -11,7 +12,7 @@ public class InputValidatorTest {
 
     @Test
     @DisplayName("로또 구매 금액이 숫자가 아니라면 예외가 발생한다.")
-    void 실패_로또구입_숫자아님(){
+    void 실패_로또구입_숫자아님() {
         // given
         InputValidator validator = new InputValidator();
 
@@ -23,7 +24,7 @@ public class InputValidatorTest {
 
     @Test
     @DisplayName("구매 금액이 0 이하이면 예외가 발생한다.")
-    void 실패_로또구입_0원이하(){
+    void 실패_로또구입_0원이하() {
         // given
         InputValidator validator = new InputValidator();
 
@@ -43,5 +44,16 @@ public class InputValidatorTest {
         assertThatThrownBy(() -> validator.validateInput("2500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구입 금액은 1,000원 단위여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("정상적인 구매입력값이 입력되면 정상 처리된다.")
+    void 성공_로또구입_알맞은_입력값() {
+        // given
+        InputValidator validator = new InputValidator();
+
+        // when & then
+        assertThatCode(() -> validator.validateInput("3000"))
+                .doesNotThrowAnyException();
     }
 }
