e828108cbef5aaa59d184bc3bac15605735ea020
Park Yu Han <pbk2312@naver.com>
2024-11-01 14:30:03 +0900

    refactor(model): Lotto 클래스의 상수 참조 방식 개선
    - LottoConstants의 상수들을 static import 방식으로 변경하여 가독성 향상
    - 코드 일관성을 높이기 위해 상수 참조 방식을 통일

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,12 @@
 package lotto.model;
 
+import static lotto.constants.LottoConstants.LOTTO_MAX_NUMBER;
+import static lotto.constants.LottoConstants.LOTTO_MIN_NUMBER;
+import static lotto.constants.LottoConstants.LOTTO_NUMBER_COUNT;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
-import lotto.constants.LottoConstants;
 import lotto.validator.LottoValidator;
 
 public class Lotto {
@@ -36,8 +39,7 @@ public class Lotto {
     }
 
     public static List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(LottoConstants.MIN_NUMBER, LottoConstants.MAX_NUMBER,
-                LottoConstants.LOTTO_NUMBER_COUNT);
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
     }
 
     public String formatNumbers() {
@@ -45,4 +47,5 @@ public class Lotto {
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ", "[", "]"));
     }
+
 }
