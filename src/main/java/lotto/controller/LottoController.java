aa203d7fc64a9866a2a8ccceaee608bbf83b904f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-02 00:04:43 +0900

    feat: LottoController 구현 완료

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
 import lotto.constants.value.LottoRule;
+import lotto.domain.BonusComponent;
+import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
+import lotto.dto.LottosDto;
+import lotto.dto.ScoreDto;
 import lotto.view.OutputView;
 
 import java.util.function.Supplier;
@@ -18,10 +22,23 @@ public class LottoController {
         this.outputView = outputView;
     }
 
+    /**
+     * Error는 사용자의 입력이 동반 된 작업에만 발생할 수 있습니다.
+     * 따라서, ErrorCatcher는 사용자의 입력을 받는 경우에만 적용됩니다.
+     */
     public void play(){
         int inputPrice = ErrorCatcher(inputManger::getInputPrice);
         int lottoBuyAmount = inputPrice/ LottoRule.LOTTO_PRICE.getInstance();
 
+        lottoMachine.buyNumberOfLottos(lottoBuyAmount);
+        LottosDto lottosDto = lottoMachine.getLottos();
+        outputView.printBoughtLottos(lottosDto);
+
+        Lotto winningComponents = ErrorCatcher(inputManger::getInputWinningComponent);
+        BonusComponent bonusComponent = ErrorCatcher(() -> inputManger.getInputBonusComponent(winningComponents));
+
+        ScoreDto scoreDto = lottoMachine.getScore(winningComponents,bonusComponent);
+        outputView.printScoreResult(scoreDto);
     }
 
 
