1f111f126f634128730c95ff5fd98fa15c6b1c25
plan11plan <tjtfy7028@gmail.com>
2024-11-01 14:03:16 +0900

    feat: 천원 단위 금액 도메인 객체 구현
    - 천원 단위 검증 로직 추가
    - 문자열을 천원 단위 객체로 변환하는 기능 구현

--- src/main/java/lotto/ThousandWons.java
+++ src/main/java/lotto/ThousandWons.java
@@ -1,7 +1,28 @@
 package lotto;
 
-public class ThousandWons {
-    public static ThousandWons of(String money) {
+import static lotto.util.StringConverter.toInt;
 
+public class ThousandWons {
+
+    private final int value;
+
+    public ThousandWons(int value) {
+        this.value = value;
+    }
+
+    public static ThousandWons of(String money) {
+        validate(toInt(money));
+        return ThousandWons.of(money);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private static void validate(int value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력 가능 합니다.");
+        }
     }
 }
+
