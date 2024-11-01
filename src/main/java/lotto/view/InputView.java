b4c789da3c724067d23f23c7e89f237bd79c1312
soeun2537 <soeun2537@gmail.com>
2024-11-02 03:36:07 +0900

    feat: 로또 구입 금액 입력 로직 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import static lotto.constant.message.InputMessage.*;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String getLottoPurchaseAmountInput() {
+        System.out.println(LOTTO_PURCHASE_AMOUNT_INPUT_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+}
