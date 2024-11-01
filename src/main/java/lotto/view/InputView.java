6ec1ba7e4223e316112f04374a778f285b997a7a
menzzi <minmin3927@naver.com>
2024-11-01 17:20:56 +0900

    refactor : 출력형식 맞춤 - 개행추가, 오름차순 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,18 +8,21 @@ public class InputView {
     public int inputLottoAmount(){
         System.out.println("구입금액을 입력해 주세요.");
         String amount = Console.readLine();
+        System.out.println();
         return Validator.validateLottoAmount(amount);
     }
 
     public List<Integer> inputWinningNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
         String winningNumber = Console.readLine();
+        System.out.println();
         return Validator.validateWinningLotto(winningNumber);
     }
 
     public int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
+        System.out.println();
         return Validator.validateBonusNumber(bonusNumber);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import java.text.DecimalFormat;
+import java.util.Collections;
 import java.util.List;
 import lotto.model.Lotto;
 
@@ -8,8 +9,10 @@ public class OutputView {
     public void printLottoTicket(List<Lotto> lottoTickets, int lottoTicketNumber){
         System.out.println(lottoTicketNumber + "개를 구매했습니다.");
         for(Lotto lotto : lottoTickets){
+            Collections.sort(lotto.getNumbers());
             System.out.println(lotto.getNumbers());
         }
+        System.out.println();
     }
 
     public void printResult(int matchCount, int prizeMoney, boolean isBonusMatch ,int winningCount){
