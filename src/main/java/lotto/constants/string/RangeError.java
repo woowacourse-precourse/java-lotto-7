8aef03913d5bb7fa07505963c8637ecc950c1689
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-02 05:26:09 +0900

    style: 코드 컨벤션에 맞게 정리

--- src/main/java/lotto/constants/string/RangeError.java
+++ src/main/java/lotto/constants/string/RangeError.java
@@ -4,8 +4,8 @@ import lotto.constants.Constants;
 import lotto.constants.value.LottoRule;
 
 public enum RangeError implements Constants<String> {
-    NUMBER("[ERROR] 로또 번호는 "+ LottoRule.MINIMUM_NUMBER_RANGE.getInstance() +"부터 "+ LottoRule.MAXIMUM_NUMBER_RANGE.getInstance() +"사이여야 합니다."),
-    LOTTO("[ERROR] 로또의 길이가 "+LottoRule.COMBINATION_LENGTH.getInstance()+"과 일치하지 않습니다");
+    NUMBER("[ERROR] 로또 번호는 " + LottoRule.MINIMUM_NUMBER_RANGE.getInstance() + "부터 " + LottoRule.MAXIMUM_NUMBER_RANGE.getInstance() + "사이여야 합니다."),
+    LOTTO("[ERROR] 로또의 길이가 " + LottoRule.COMBINATION_LENGTH.getInstance() + "과 일치하지 않습니다");
 
     private String message;
 
