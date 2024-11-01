1cfc7e13730e9ceae6f20305115de3422e1c44e8
sinminseok12 <sinminseok12@naver.com>
2024-11-01 23:53:16 +0900

    test(LottoTicketTest) : 로또 수익률 계산 테스트 구현
    로또 티켓과 당첨 로또 정보를 비교해 수익률을 계산하는 테스트를 구현했다.

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 
+import static lotto.constants.LottoValue.LOTTO_PRICE;
+
 public class LottoTicketTest {
 
     @Test
     void 로또_티켓에_있는_모든_로또의_당첨_결과를_조회한다() {
         LottoTicket lottoTicket = new LottoTicket(providerLottos());
-        WinningLotto winningLotto = WinningLotto.of(List.of(1,2,3,4,5,6), 7);
+        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 7);
 
         EnumMap<Ranking, Integer> rankings = lottoTicket.checkRanking(winningLotto);
 
@@ -21,6 +23,17 @@ public class LottoTicketTest {
                 .containsEntry(Ranking.FIRST, 1)
                 .containsEntry(Ranking.SECOND, 1)
                 .containsEntry(Ranking.FOURTH, 1);
+    }
+
+    @Test
+    void 로또_티켓의_수익률을_계산한다() {
+        LottoTicket lottoTicket = new LottoTicket(providerLottos());
+        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        double profitRate = lottoTicket.calculateProfitRate(winningLotto);
+
+        double expectedProfitRate = calculateExpectedProfitRate();
+        Assertions.assertThat(profitRate).isEqualTo(expectedProfitRate);
     }
 
     private List<Lotto> providerLottos() {
@@ -31,5 +44,10 @@ public class LottoTicketTest {
         return lottos;
     }
 
+    private double calculateExpectedProfitRate() {
+        long totalReward = Ranking.FIRST.getReward() + Ranking.SECOND.getReward() + Ranking.FOURTH.getReward();
+        int totalCost = 3 * LOTTO_PRICE.getValue();
+        return (double) totalReward / totalCost;
+    }
 
 }
