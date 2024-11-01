d0863104f4d5da43924bf8472d2b80987d62ba9d
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-01 15:37:50 +0900

    feat : 사용자로부터 구입 금액을 입력받는다.
    구입 금액이 양수가 아니거나, 로또 금액으로 나누어 떨어지지 않는 경우 에러를 출력한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int lottoPrice = 1000;
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputPrice = Console.readLine();
+
+        boolean isNumeric = inputPrice.chars().allMatch(Character::isDigit);
+        if (!isNumeric) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 양수로 입력해 주세요.");
+        }
+
+        int userPrice = Integer.parseInt(inputPrice);
+        if (userPrice % lottoPrice != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 " + lottoPrice + "단위로 입력해 주세요.");
+        }
     }
 }
