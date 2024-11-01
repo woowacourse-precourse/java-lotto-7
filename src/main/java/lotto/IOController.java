140c5f45eef378e0326b89146221e9e2d998ff0f
chyo1 <hyowon3856@gmail.com>
2024-11-01 19:38:15 +0900

    feat : 금액, 당첨 번호, 보너스 번호 입력 기능 추가

---
+++ src/main/java/lotto/IOController.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class IOController {
+    public static String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    String inputPurchase() {
+        System.out.println(INPUT_PURCHASE_MESSAGE);
+        return Console.readLine();
+    }
+
+    String inputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    String inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+}
