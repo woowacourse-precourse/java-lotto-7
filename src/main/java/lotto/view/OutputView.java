4269500374c73ff111bf44f76344c6773dea3ae3
kgy1008 <gayeon1008@g.skku.edu>
2024-11-01 23:31:09 +0900

    feat: print lotto result

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,13 @@
 package lotto.view;
 
 import lotto.dto.LottoNumbers;
+import lotto.dto.WinningStatistics;
 
 public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String RESULT_MESSAGE = "당첨 통계";
     private static final String DIVIDER = "---";
+    private static final String PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public void printLottoCount(final int count) {
         System.out.println(System.lineSeparator() + count + PURCHASE_MESSAGE);
@@ -19,8 +21,14 @@ public class OutputView {
         System.out.println(message);
     }
 
-    public void printResult() {
-        System.out.println(System.lineSeparator() + RESULT_MESSAGE);
-        System.out.println(DIVIDER + System.lineSeparator());
+    public void printResult(final WinningStatistics winningStatistics) {
+        System.out.println(RESULT_MESSAGE + System.lineSeparator() + DIVIDER);
+        winningStatistics.getStatistics().forEach((rank, count) -> {
+            System.out.printf("%s - %d개%s", rank.getMessage(), count, System.lineSeparator());
+        });
+    }
+
+    public void printProfit(final double profit) {
+        System.out.printf(PROFIT_MESSAGE, profit);
     }
 }
