ccc9f484ab9e437474b615d6e0fe065f943dc25d
Yehyeok Bang <qkddpgur318@gmail.com>
2024-11-01 20:42:11 +0900

    test: 번호 매칭 기능 테스트 작성

---
+++ src/test/java/lotto/model/match/SetBasedLottoNumbersMatchCounterTest.java
@@ -0,0 +1,88 @@
+package lotto.model.match;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("로또 번호 매치 카운터 객체 테스트")
+class SetBasedLottoNumbersMatchCounterTest {
+
+    @DisplayName("6개가 동일한 경우")
+    @Test
+    void shouldReturn6_WhenAllMatch() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 6);
+    }
+
+    @DisplayName("5개가 동일한 경우")
+    @Test
+    void shouldReturn5_When5Match() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 7);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 5);
+    }
+
+    @DisplayName("4개가 동일한 경우")
+    @Test
+    void shouldReturn4_When4Match() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 7, 8);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 4);
+    }
+
+    @DisplayName("3개가 동일한 경우")
+    @Test
+    void shouldReturn3_When3Match() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 3);
+    }
+
+    @DisplayName("2개가 동일한 경우")
+    @Test
+    void shouldReturn2_When2Match() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 2, 7, 8, 9, 10);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 2);
+    }
+
+    @DisplayName("1개가 동일한 경우")
+    @Test
+    void shouldReturn1_When1Match() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(1, 7, 8, 9, 10, 11);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 1);
+    }
+
+    @DisplayName("동일한 숫자가 없는 경우")
+    @Test
+    void shouldReturn0_WhenNotMatch() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = List.of(7, 8, 9, 10, 11, 12);
+        MatchCounter<Integer> matchCounter = new SetBasedLottoNumbersMatchCounter<>();
+
+        int matchesCount = matchCounter.countMatches(lottoNumbers, winningNumbers);
+        assertEquals(matchesCount, 0);
+    }
+}
