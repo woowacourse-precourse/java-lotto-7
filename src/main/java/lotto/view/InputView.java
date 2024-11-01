4dd2bbd42a200d3df2f717c9fc63516f7884ae02
OhYeHyun <ohyhohyl@naver.com>
2024-11-02 01:39:37 +0900

    feat(InputView): 구입 금액 입력 받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,27 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.NumbersValidator;
+import lotto.validator.PurchaseAmountValidator;
+
+import static lotto.view.OutputView.*;
+
 public class InputView {
+    private final NumbersValidator numbersValidator = new NumbersValidator();
+
+    public String getInput() {
+        return Console.readLine();
+    }
+
+    private int getInputPurchaseAmount() {
+        while (true) {
+            promptPurchaseAmount();
+            String purchaseAmountInput = getInput();
+            try {
+                return PurchaseAmountValidator.validateAndParse(purchaseAmountInput);
+            } catch (IllegalArgumentException e) {
+                print(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,12 @@
 package lotto.view;
 
 public class OutputView {
+
+    public static void print(String string) {
+        System.out.println(string);
+    }
+
+    public static void promptPurchaseAmount() {
+        print("구입금액을 입력해 주세요.");
+    }
 }
