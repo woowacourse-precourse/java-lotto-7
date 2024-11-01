166832ba860c88a14617e8b587852f2e7f07c918
mipangg <morganjeong119@gmail.com>
2024-11-02 02:06:42 +0900

    feat(LottoModel): create userLotto instance to store user lotto information

--- src/main/java/lotto/model/LottoModel.java
+++ src/main/java/lotto/model/LottoModel.java
@@ -1,4 +1,14 @@
 package lotto.model;
 
+// UserLotto와 Lotto를 이용하여 유저의 복권 정보를 비교하고 통계하는 메서드들 모음
 public class LottoModel {
+    UserLotto userLotto;
+
+    public void setUserLotto(int numberOfLotto) {
+        userLotto = new UserLotto(numberOfLotto);
+    }
+
+    public UserLotto getUserLotto() {
+        return userLotto;
+    }
 }
