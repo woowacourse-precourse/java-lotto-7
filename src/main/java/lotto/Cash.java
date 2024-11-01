de908ea4cb885ec8bc502076149f21f9c9fcdebc
GaBaljaintheroom <junsu1222@naver.com>
2024-11-01 13:30:48 +0900

    feat(Cash): 로또 구매 기본 단위 검증 기능 구현

--- src/main/java/lotto/Cash.java
+++ src/main/java/lotto/Cash.java
@@ -2,10 +2,19 @@ package lotto;
 
 public class Cash {
 
+    private static final int DEFAULT_UNIT = 1000;
+
     private final int amount;
 
     public Cash(int amount) {
+        validateDefaultUnit(amount);
         this.amount = amount;
+    }
+
+    private void validateDefaultUnit(int amount) {
+        if(amount % DEFAULT_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 1장의 가격은 1,000원 입니다.");
+        }
     }
 
 }
