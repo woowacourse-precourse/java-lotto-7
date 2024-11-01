2e670f9dce7e61c0ae5ca5ce1f26af78eb767bcd
Chan531 <chan657312@gmail.com>
2024-11-01 15:58:36 +0900

    feat(InputView): 구입금액 입력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	public int getPurchasePriceInput() {
+		String purchasePrice = Console.readLine();
+	}
+}
