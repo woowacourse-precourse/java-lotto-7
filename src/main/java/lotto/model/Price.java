3e356a1eeb05f211a4af571f26a38d1d81fab511
Chan531 <chan657312@gmail.com>
2024-11-01 16:35:36 +0900

    feat(Price): 구입금액 조건 검증
    구입금액이 0 이하라면 예외 처리

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -15,4 +15,10 @@ public class Price {
 			throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.");
 		}
 	}
+
+	private void isPositive(int price) {
+		if (price <= 0) {
+			throw new IllegalArgumentException("[ERROR] 구입금액은 0보다 커야 합니다.");
+		}
+	}
 }
