58a7ab52f238e0e43f3c931485b81c3a7555b167
sinminseok12 <sinminseok12@naver.com>
2024-11-01 17:41:40 +0900

    feat(InputValidator) : 구입금액, 보너스 번호 입력시 양의 정수임을 검증하는 기능 구현
    구입 금액, 보너스 번호를 입력할때 양의 정수가 아니면 예외를 발생시킨다.

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -4,13 +4,14 @@ import lotto.utils.ExceptionUtils;
 
 public class InputValidator {
     private static final String ERROR_MESSAGE_EMPTY_INPUT = "입력값에 공백을 허용하지 않습니다.";
-
+    private static final String ERROR_MESSAGE_NOT_POSITIVE_NUMBER = "양의 정수가 아닌 값은 허용하지 않습니다.";
 
     private InputValidator(){
     }
 
     public static void validatePurchaseAmount(String input){
         checkEmptyInput(input);
+        checkPositiveNumber(input);
     }
 
     public static void validateWinNumbers(String input){
@@ -19,11 +20,18 @@ public class InputValidator {
 
     public static void validateBonusNumber(String input){
         checkEmptyInput(input);
+        checkPositiveNumber(input);
     }
 
     private static void checkEmptyInput(String input) {
         if (input.isBlank()) {
             ExceptionUtils.throwIllegalArgument(ERROR_MESSAGE_EMPTY_INPUT);
+        }
+    }
+
+    private static void checkPositiveNumber(String input){
+        if (!input.matches("\\d+") || Integer.parseInt(input) <= 0) {
+            ExceptionUtils.throwIllegalArgument(ERROR_MESSAGE_NOT_POSITIVE_NUMBER);
         }
     }
 
