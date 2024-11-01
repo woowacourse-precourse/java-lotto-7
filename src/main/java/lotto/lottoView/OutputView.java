0e1c1279932a44190634d3ed1c24dd4b7cef1f46
ppzepi <hsunguk1999@gmail.com>
2024-11-01 23:39:18 +0900

    feat: 수익 결과를 출력하는 profitMessage 메서드 생성

--- src/main/java/lotto/lottoView/OutputView.java
+++ src/main/java/lotto/lottoView/OutputView.java
@@ -5,11 +5,11 @@ import lotto.lottoModel.StatisticsLottoDTO;
 
 public class OutputView {
     private static final String HOW_MANY_BUY = "%d개를 구매했습니다.";
-    private static final String STATISTIC_START = "당첨 통계/n---";
+    private static final String STATISTIC_START = "당첨 통계\n---";
     private static final String VALUE_MATCH_START = "%d개 일치";
     private static final String VALUE_MATCH_END = " (%,d원) - %d개";
     private static final String BONUS_MATCH = ", 보너스 볼 일치";
-    private static final String PROFIT_MESSAGE = "총 수익률은 %f \%입니다.";
+    private static final String PROFIT_MESSAGE = "총 수익률은 %.1f %%입니다.";
 
 
 
@@ -36,4 +36,10 @@ public class OutputView {
         System.out.printf(VALUE_MATCH_START+VALUE_MATCH_END,i,LottoPrize.getPrize(i,false),stats.getHitNumberValue(i));
         System.out.println();
     }
+
+    public void profitMessage(double profit) {
+        System.out.printf(PROFIT_MESSAGE, profit);
+    }
+
+
 }
