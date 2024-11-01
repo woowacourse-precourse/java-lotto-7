513ac5a03909d7d5a2aba1fb8fdd0c5b9e48656a
leesh7807 <leesh7807@gmail.com>
2024-11-01 17:57:02 +0900

    refactor(LottoIssuer): move logic from constructor to method
    Since LottoIssuer save the states about Lotto list, it must use a getter.
    So, I refactor it to be a method that directly return the value.

--- src/main/java/lotto/LottoIssuer.java
+++ src/main/java/lotto/LottoIssuer.java
@@ -5,27 +5,18 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoIssuer {
-    private final List<Lotto> purchasedLottos;
-    private final int money;
-
-    public LottoIssuer(int money) {
+    public List<Lotto> issueLottos(int money) {
         checkMoney(money);
-        this.money = money;
-        purchasedLottos = purchaseLottos();
+        int count = money / 1000;
+        return IntStream.range(0, count)
+                .mapToObj(it -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
+                .map(Lotto::new)
+                .toList();
     }
 
     private void checkMoney(int money) {
         if (money <= 0 || money % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY.format());
         }
-    }
-
-    private List<Lotto> purchaseLottos() {
-        int count = money / 1000;
-        return IntStream.range(0, count)
-                .mapToObj(it -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
-                .map(Lotto::new)
-                .toList();
-
     }
 }
--- src/test/java/lotto/LottoIssuerTest.java
+++ src/test/java/lotto/LottoIssuerTest.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test;
 public class LottoIssuerTest {
     @Test
     void 구입_금액이_음수인_예외() {
-        assertThatThrownBy(() -> new LottoIssuer(-1000))
+        assertThatThrownBy(() -> new LottoIssuer().issueLottos(-1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
 
     @Test
     void 구입_금액이_0인_예외() {
-        assertThatThrownBy(() -> new LottoIssuer(0))
+        assertThatThrownBy(() -> new LottoIssuer().issueLottos(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
 
     @Test
     void 구입_금액이_1000원_단위_양수가_아닌_예외() {
-        assertThatThrownBy(() -> new LottoIssuer(3500))
+        assertThatThrownBy(() -> new LottoIssuer().issueLottos(3500))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
