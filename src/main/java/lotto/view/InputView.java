26e427fa487e2203c5aaeba954b35cb16245be2e
신관규 <soundbar91@gmail.com>
2024-11-01 16:00:28 +0900

    feat: 입력 View 클래스 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public String InputLottoPurchaseAmount() {
+        return readLine();
+    }
+
+    public String InputLottoWinningNumbers() {
+        return readLine();
+    }
+
+    public String InputLottoBonusNumber() {
+        return readLine();
+    }
+}
