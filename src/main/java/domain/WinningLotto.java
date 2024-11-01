a4038ff766148c2ea3e2c172df94eb260fef48d6
dudxo <father8475@naver.com>
2024-11-02 02:00:00 +0900

    feat(WinningLotto) : 당첨 등수
    - 당첨 등수에 따른 Enum을 선언한다.
    - rank와 일치하는 WinningLotto를 생성하는 정적 팩토리를 제공한다.

---
+++ src/main/java/domain/WinningLotto.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum WinningLotto {
+
+    FIRST(1, 6, 2_000_000_000),
+    SECOND(2, 5, 30_000_000),
+    THIRD(3, 5, 1_500_000),
+    FOURTH(4, 4, 50_000),
+    FIFTH(5, 3, 5_000),
+    NO_RANK(6, 0, 0);
+
+    private final int rank;
+    private final int correctCount;
+    private final int prizeMoney;
+
+    WinningLotto(int rank, int correctCount, int prizeMoney) {
+        this.rank = rank;
+        this.correctCount = correctCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static WinningLotto from(long input) {
+        return Arrays.stream(values())
+                .filter(correctCount -> correctCount.isEqual(input))
+                .findAny()
+                .orElse(NO_RANK);
+    }
+
+    private boolean isEqual(long input) {
+        return input == this.correctCount;
+    }
+}
