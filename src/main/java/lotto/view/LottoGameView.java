d0a4839f9840ef43ae4e2471593e44da4cd4e4bb
wgrgwg <kyoeng14697@naver.com>
2024-11-02 00:38:35 +0900

    fix(view): Add message to get input from user

--- src/main/java/lotto/view/LottoGameView.java
+++ src/main/java/lotto/view/LottoGameView.java
@@ -6,6 +6,8 @@ public final class LottoGameView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
 
     public String getMoneyInput() {
+        System.out.println(INPUT_MONEY);
+        
         return Console.readLine().trim();
     }
 }
