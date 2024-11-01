73783595468ca1231a3bf89fbccd58066cf7c5ec
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-01 11:15:01 +0000

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final String ASK_PURCHASE_MONEY_MESSAGE_STRING = "구입금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS_MESSAGE_STRING = "당첨 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
         
@@ -13,5 +14,10 @@ public class Application {
     private String readPurchaseMoney() {
         System.out.println(ASK_PURCHASE_MONEY_MESSAGE_STRING);
         return Console.readLine();        
+    }
+
+    private String readWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBERS_MESSAGE_STRING);
+        return Console.readLine();
     }
 }
