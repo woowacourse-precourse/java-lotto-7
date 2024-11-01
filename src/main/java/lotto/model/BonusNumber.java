ae96c8ca5085cb75cb0cc97a506e0c2b10d4cf30
seophohoho <seop0937@gmail.com>
2024-11-01 23:12:50 +0900

    feat(BonusNumber): 보너스 번호 저장 메소드 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class BonusNumber {
     private final String BONUS_NUMBER_PATTERN = "[1-9][0-9]{0,1}";
+    private int number;
 
     public void validate(String input) {
         Pattern patternBonusNumber = Pattern.compile(BONUS_NUMBER_PATTERN);
@@ -17,6 +18,14 @@ public class BonusNumber {
         if (!(Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 45)) {
             reject();
         }
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
     }
 
     public void reject() {
