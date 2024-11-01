fdddc8f6b79175792ade350c9b93f54ff1435a0b
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-01 11:51:44 +0000

    feat: 구입 금액만큼 금액이 있는 지갑 생성 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.Wallet;
 
 
 public class Application {
@@ -9,10 +10,21 @@ public class Application {
     private static final String ASK_BONUS_NUMBER_MESSAGE_STRING = "보너스 번호를 입력해 주세요.";
 
     public static void main(String[] args) {
-        
+        Wallet myWallet = makeWallet();
     }
 
-    private String readPurchaseMoney() {
+    public static Wallet makeWallet() {
+        while (true) {
+            try {
+                String purchaseMoney = readPurchaseMoney();
+                return new Wallet(purchaseMoney);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static String readPurchaseMoney() {
         System.out.println(ASK_PURCHASE_MONEY_MESSAGE_STRING);
         return Console.readLine();        
     }
