0fd21f96a274d5f2adae5bdf6ef7c8ec95370873
Hajuuu <hpt0127@naver.com>
2024-11-01 17:08:13 +0900

    feat: 보너스 번호 입력 및 예외 처리 구현
    보너스 번호를 입력받는 메서드와 보너스 번호가 1~45 사이의 숫자 한 개가 아닐 경우 예외를 발생시키는 메서드 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String ERROR_MESSAGE_AMOUNT = "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.";
     private static final String ERROR_MESSAGE_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ERROR_MESSAGE_BONUS_NUMBER = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자 중 하나를 입력해야 합니다.";
 
 
     public int inputPurchaseAmount() {
@@ -49,6 +50,24 @@ public class InputView {
         }
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException(ERROR_MESSAGE_LOTTO_NUMBER);
+        }
+        return number;
+    }
+
+    public int inputBonus() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return checkInputBonusNumber(readLine());
+    }
+
+    public int checkInputBonusNumber(String input) {
+        int number = 0;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_NUMBER);
+        }
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BONUS_NUMBER);
         }
         return number;
     }
