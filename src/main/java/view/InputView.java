7ac03cb681ce298d41e418112cc58fcb7e967121
dudxo <father8475@naver.com>
2024-11-01 16:40:28 +0900

    feat(InputView) : 구입금액 입력 View
    - 사용자로부터 입력 받은 구입 금액을 검증하고 정수형으로 변환한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,24 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import convert.InputConvertor;
 
 public class InputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요.";
+    private final InputConvertor inputConvertor;
+
+    public InputView(InputConvertor inputConvertor) {
+        this.inputConvertor = inputConvertor;
+    }
 
     public String inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MSG);
-        return Console.readLine();
+
+        String inputPurchaseAmount = Console.readLine();
+        return inputConvertor.convertPurchaseAmount(inputPurchaseAmount);
     }
 
     public String inputWinningNumbers() {
