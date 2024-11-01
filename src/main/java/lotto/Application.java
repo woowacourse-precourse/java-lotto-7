07f0c5feea4e0b42bb00c90ce7ea2aab0e50cf0e
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-01 23:24:12 +0900

    refactor(Application) : 변수 네이밍
    뒤에 list라고 붙어있던 것들 다른 변수로 지어줌.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,18 +11,14 @@ public class Application {
 
     public static void main(String[] args) {
         Controller controller = new Controller();
-        //구입금액과 몇 개 샀는지 처리
         Integer purchasePrice = controller.gainPurchaseAmount();
         Integer lottoCount = controller.getLottoCount();
-        //로또 발급 후 정렬 및 프린트
         List<Lotto> myLottos = Lotto.sortLottoList(lottoCount);
-        //당첨 번호 및 보너스 번호 입력
         Lotto answer = controller.getWinningInput();
         Integer bonus = controller.gainBonusInput(answer);
-        //각 로또 별 내 결과 저장
-        List<MyResult> resultList = WinningDetails.saveMyGrades(myLottos, answer, bonus);
+        List<MyResult> myResults = WinningDetails.saveMyGrades(myLottos, answer, bonus);
         WinningDetails winningDetails = new WinningDetails();
-        winningDetails = winningDetails.sumUpGrades(resultList);
+        winningDetails.sumUpGrades(myResults);
         OutputView.printResults(winningDetails);
         Integer revenue = MyResult.getMyRevenue(winningDetails);
         OutputView.printReturn(purchasePrice, revenue);
