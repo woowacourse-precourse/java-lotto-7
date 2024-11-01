4b779adaaeaaf81101e69343f0e39295a945aea0
dnjswns98 <tngkrt2004@gmail.com>
2024-11-01 17:48:07 +0900

    feat(OutputView): Request to input the bonus number

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     private static final String QUESTION_LOTTO_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String PRINT_LOTTO_AMOUNT = "개를 구매했습니다.";
     private static final String QUESTION_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String QUESTION_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public void printInputMoney() {
         System.out.println(QUESTION_LOTTO_AMOUNT);
@@ -17,5 +18,10 @@ public class OutputView {
     public void printWinningNumbers() {
         System.out.println();
         System.out.println(QUESTION_WINNING_NUMBERS);
+    }
+
+    public void printBonusNumber() {
+        System.out.println();
+        System.out.println(QUESTION_BONUS_NUMBER);
     }
 }
