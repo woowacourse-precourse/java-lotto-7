d6f80e883086594202651ca95c389b5105b38942
EHCTH <lees90896@gmail.com>
2024-11-01 19:05:20 +0900

    feat(PurchaseLottoTickets): PurchaseLottoTicketsService 인터페이스 구현

--- src/main/java/lotto/lotto/infrastructure/PurchaseLottoTickets.java
+++ src/main/java/lotto/lotto/infrastructure/PurchaseLottoTickets.java
@@ -2,20 +2,20 @@ package lotto.lotto.infrastructure;
 
 import lotto.lotto.domain.Lotto;
 import lotto.lotto.domain.LottoTickets;
-import lotto.lotto.domain.LottoTicketsCreatorService;
+import lotto.lotto.domain.PurchaseLottoTicketsService;
 import lotto.lotto.domain.NumberGenerator;
 
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
-public class LottoTicketsCreator implements LottoTicketsCreatorService {
+public class PurchaseLottoTickets implements PurchaseLottoTicketsService {
     private final NumberGenerator numberGenerator;
-    public LottoTicketsCreator(NumberGenerator numberGenerator) {
+    public PurchaseLottoTickets(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
     @Override
-    public LottoTickets purchaseLottoTickets(int count) {
+    public LottoTickets purchase(int count) {
         return LottoTickets.of(Stream.generate(this::createLotto)
                 .limit(count)
                 .collect(toList()));
