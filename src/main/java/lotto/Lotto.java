bf83b90a0020c06aadf4a31854e9fc0c3656a2ab
junhyunlim73 <limjh0703@gmail.com>
2024-11-01 21:24:08 +0900

    Feat: Lotto 클래스에 중복 확인 로직 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -22,4 +25,10 @@ public class Lotto {
         return numbers;
     }
 
+    private void checkDuplicate(List<Integer> nums) {
+        Set<Integer> uniqueNumbers = new HashSet<>(nums);
+        if (uniqueNumbers.size() != nums.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }
