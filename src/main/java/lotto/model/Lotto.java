721cb5be89ad0561890353ef20276fe98c4ad002
JeongHyeon Jo <cisxo@naver.com>
2024-11-01 09:20:12 +0900

    feat(model): all model redesign

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,22 +1,22 @@
 package lotto.model;
 
+import lotto.utils.LottoValidator;
 import lotto.utils.Validator;
 
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    Validator<Integer> validator;
+    Validator<List<Integer>> validator;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validator = new LottoValidator();
+        validator.validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public List<Integer> getLottoNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
--- src/main/java/lotto/model/LottoMember.java
+++ src/main/java/lotto/model/LottoMember.java
@@ -1,4 +1,10 @@
 package lotto.model;
 
 public class LottoMember {
+    /**
+     * 로또 구매자라 생각
+     * 로또 랜덤 넘버들을 받아와야 함
+     * 로또 당첨 여부를 받아와야 함
+     * 로또 수익률을 받아와야 함
+     */
 }
