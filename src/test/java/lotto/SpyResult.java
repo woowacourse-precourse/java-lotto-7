b1ff2dfa0b2dbcae6bf3111505d847567597d2b4
hongkikii <hyangmeeh@icloud.com>
2024-11-02 06:29:44 +0900

    test(ReturnRateTest): 수익률 계산 테스트 코드 작성

--- src/test/java/lotto/SpyResult.java
+++ src/test/java/lotto/SpyResult.java
@@ -1,2 +1,18 @@
-package lotto;public class SpyResult {
+package lotto;
+
+public class SpyResult extends Result {
+    private long totalPrize;
+
+    public SpyResult() {
+        this.totalPrize = 0L;
+    }
+
+    public void setTotalPrize(long totalPrize) {
+        this.totalPrize = totalPrize;
+    }
+
+    @Override
+    public long calculateTotalPrize() {
+        return this.totalPrize;
+    }
 }
