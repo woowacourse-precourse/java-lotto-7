28f90db67e0ad822da4775c4e9f4bd64a926e42e
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-01 22:13:51 +0900

    feat(LottoController): 당첨번호 컨트롤러 구현

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,10 +1,13 @@
 package lotto.Controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.PurchaseAmount;
 import lotto.view.InputView;
 import lotto.domain.RandomLotto;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
@@ -13,8 +16,13 @@ public class LottoController {
     }
 
     public void run(){
+        PrintInput();
+    }
+    public void PrintInput(){
         String amount_str =InputView.purchaseAmount();
         PlayerAmount(amount_str);
+        String number_str =InputView.sixLottoNumber();
+        lottoNum(number_str);
     }
     public void PlayerAmount(String amount_str){
         purchaseAmount = new PurchaseAmount(amount_str);
@@ -28,5 +36,9 @@ public class LottoController {
     public void PrintDetails(){
         OutputView.printCount(purchaseAmount.calculateLottoCount());
         LottoList(purchaseAmount.calculateLottoCount());
+    }
+    public void lottoNum(String number_str){
+        List<Integer> lottoNumber =new ArrayList<>(LottoNumber.winLottoNumber(number_str));
+        Lotto lotto =new Lotto(lottoNumber);
     }
 }
