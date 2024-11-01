c608d187d5849637d10d0cf84ec3a1cf49114d8b
woong2e <tlswodnd12345@naver.com>
2024-11-01 16:49:21 +0900

    feat: implement input validation function

---
+++ src/main/java/lotto/util/InputValidator.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+import java.util.List;
+
+public class InputValidator {
+
+    public static void validatePurchaseAmount(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    public static void validateBonusNumberRange(int bonus) {
+        if (bonus < 0 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public static void checkBonusNumberDuplicates(List<Integer> winningNumbers ,int bonus) {
+        for (Integer number : winningNumbers) {
+            if (number == bonus) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호와 중복되면 안됩니다.");
+            }
+        }
+    }
+}
