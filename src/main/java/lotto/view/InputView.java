fd8ed1166c79cec68f557a4530aa078f5a17bfe1
33jyu33 <jy04169@gmail.com>
2024-11-02 00:17:08 +0900

    feat(InputView): read and return input value
    Function readInput:
    return readLine()

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.NumberValidator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public static String readInput() {
+        return readLine();
+    }
+}
