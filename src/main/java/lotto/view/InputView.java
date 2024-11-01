b16054008b665b36afdc4facab8f9cb594d71eb4
(minjae8563) <(minjae8563@naver.com)>
2024-11-01 19:35:33 +0900

    refactor: 문자열 출력 메서드, Split 하는 메서드, List에 담는 메서드로 메서드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,25 +8,34 @@ public class InputView {
     public static final String INPUT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String COMMA = ",";
 
-    public static String requestPurchaseAmountInput(){
+    public static void  printRequestPurchaseAmountInput(){
         System.out.println(INPUT_PURCHASE_AMOUNT);
+    }
+
+    public static void printRequestLotto(){
+        System.out.println(INPUT_WINNING_NUMBERS);
+    }
+
+    public static void printRequestBonusNumber(){
+        System.out.println(INPUT_BONUS_NUMBER);
+    }
+
+    public static String getUserInput() {
         return Console.readLine();
     }
 
-    public static List<Integer> requestLotto(){
-        System.out.println(INPUT_WINNING_NUMBERS);
+    public static String[] getUserInputSplitByComma(){
+        return Console.readLine().split(COMMA);
+    }
+
+    public static List<Integer> getLotto(String[] splitInput) {
         List<Integer> numbers = new ArrayList<>();
-        String [] splitInput =  Console.readLine().split(",");
         for(String i: splitInput){
             numbers.add(Integer.parseInt(i));
         }
         return numbers;
-    }
-
-    public static String requestBonusNumber(){
-        System.out.println(INPUT_BONUS_NUMBER);
-        return Console.readLine();
     }
 
 }
