f7248afc98c02c94c8905d7a912a7cf76c24556f
soeun2537 <soeun2537@gmail.com>
2024-11-01 22:31:24 +0900

    feat: 당첨 번호 유효성 검사 - 개수 로직 추가

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -19,6 +19,7 @@ public class Validator {
             validateRange(winningNumber);
             validatedWinningTicket.add(winningNumber);
         }
+        validateSize(validatedWinningTicket);
     }
 
     private static void validateMultiple(Integer number) {
@@ -37,6 +38,12 @@ public class Validator {
         if ((number < MIN_LOTTO_NUMBER.getIntegerValue()) || (number > MAX_LOTTO_NUMBER.getIntegerValue())) {
             throw new IllegalArgumentException(
                     OUT_OF_RANGE.getMessage(MIN_LOTTO_NUMBER.getIntegerValue(), MAX_LOTTO_NUMBER.getIntegerValue()));
+        }
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT.getIntegerValue()) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_COUNT.getMessage(LOTTO_NUMBER_COUNT.getIntegerValue()));
         }
     }
 }
