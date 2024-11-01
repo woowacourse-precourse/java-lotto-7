231434f4ab8d0a9ccda0d197cf37a7efa5b568eb
minSsan <msp1125@hanyang.ac.kr>
2024-11-01 18:40:45 +0900

    fix(ExceptionMessage): 예외처리 상수 커밋 생략

--- src/main/java/lotto/infrastructure/constant/ExceptionMessage.java
+++ src/main/java/lotto/infrastructure/constant/ExceptionMessage.java
@@ -5,6 +5,8 @@ public final class ExceptionMessage {
 
     public static final String PURCHASE_LESS_THAN_ONE = "구입 금액은 양의 정수만 입력할 수 있습니다.";
 
+    public static final String INVALID_INTEGER = "올바른 정수 값이 아닙니다.";
+
     public static String INVALID_LOTTO_COUNT(int count) {
         return String.format("로또 번호는 %d개여야 합니다.", count);
     }
