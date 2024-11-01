59a6413d3eff2e1775f8bbdbe41d3b76f651ba26
jang-namu <ironhee8005@gmail.com>
2024-11-01 19:12:38 +0900

    feat: 로또 하나를 생성한다

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,8 +1,12 @@
 package lotto;
 
 public enum ErrorMessage {
+    // Budget 관련
     BUDGET_NEGATIVE_NUMBER("[ERROR] 금액은 음수일 수 없습니다."),
-    BUDGET_INVALID_UNIT("[ERROR] 금액은 1000원 단위여야 합니다.");
+    BUDGET_INVALID_UNIT("[ERROR] 금액은 1000원 단위여야 합니다."),
+
+    // Lotto 관련
+    LOTTO_HAVE_A_SIX_UMBER("[ERROR] 로또 번호는 6개여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBER_OF_NUMBERS = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +15,15 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto quickPick() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_OF_NUMBERS));
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != NUMBER_OF_NUMBERS) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_HAVE_A_SIX_UMBER.message());
         }
     }
 
-    // TODO: 추가 기능 구현
+
 }
