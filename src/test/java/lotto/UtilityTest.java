a41adec666640842d0c8baa1db54da2d408160e4
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 15:38:11 +0900

    test(Utility): 당첨 번호를 파싱하는 테스트를 정의하라

--- src/test/java/lotto/UtilityTest.java
+++ src/test/java/lotto/UtilityTest.java
@@ -4,14 +4,24 @@ import static org.assertj.core.api.Assertions.*;
 
 import lotto.Utility.RandomNumberCreator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.List;
 
 public class UtilityTest {
     @Test
-    public void 랜덤한_6개의_수를_뽑아서_List로_반환한다() {
+    void 랜덤한_6개의_수를_뽑아서_List로_반환한다() {
         List<Integer> randomNumbers = RandomNumberCreator.generateRandomNumbers();
 
         assertThat(randomNumbers).hasSize(6);
         assertThat(randomNumbers).allMatch(number -> 1 <= number && number <= 45);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "1, 2, 3, 4, 5, 6", "1 , 2 , 3 , 4 , 5 , 6 "})
+    void 당첨_번호를_콤마로_구분한다(String inputtedNumbers) {
+        List<Integer> result = WinningNumberParser.parseWinningNumbers(inputtedNumbers);
+        assertThat(result).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 }
