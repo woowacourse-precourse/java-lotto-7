770b0da8396c9cda6452fe7c325de86e278ae3e4
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:03:47 +0900

    test(PurchaseCost): 0 이하의 구입금액 테스트를 정의하라

--- src/test/java/lotto/PurchaseCostTest.java
+++ src/test/java/lotto/PurchaseCostTest.java
@@ -2,6 +2,10 @@ package lotto;
 
 import lotto.Model.PurchaseCost;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PurchaseCostTest {
     static PurchaseCost purchaseCost;
@@ -9,5 +13,12 @@ public class PurchaseCostTest {
     @BeforeEach
     void 구입금액_객체_생성() {
         purchaseCost = new PurchaseCost(8000);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1000, 0})
+    void 제로_이하의_구입금액은_예외를_발생시킨다(int inputtedCost) {
+        assertThatThrownBy(() -> new PurchaseCost(inputtedCost))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
