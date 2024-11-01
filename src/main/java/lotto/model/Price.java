26875cbdedf346d1046bba39cefff6a3b7d3a613
Chan531 <chan657312@gmail.com>
2024-11-01 16:29:35 +0900

    feat(Price): 구입금액 조건 검증
    구입금액이 1000으로 나누어 떨어지지 않으면 예외 처리

--- src/main/java/lotto/model/Price.java
+++ src/main/java/lotto/model/Price.java
@@ -7,4 +7,10 @@ public class Price {
 	public Price(int price) {
 		this.price = price;
 	}
+
+	private void isDivisibleByThousand(int price) {
+		if (price % 1000 != 0) {
+			throw new IllegalArgumentException("[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.");
+		}
+	}
 }
