fb045b9fd4b4ca4bd332286d2b3c4d9f574b4ad3
plan11plan <tjtfy7028@gmail.com>
2024-11-01 19:48:48 +0900

    feat: 결제 결과 도메인 구현
    - 완료된 결제 정보와 구매 로또 수량을 포함한 결과 객체 구현

---
+++ src/main/java/lotto/domain/PaymentResult.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import lotto.LottoCount;
+
+public class PaymentResult {
+    private final Payment completedPayment;
+    private final LottoCount lottoCount;
+
+    public PaymentResult(Payment completedPayment, LottoCount lottoCount) {
+        this.completedPayment = completedPayment;
+        this.lottoCount = lottoCount;
+    }
+
+    public Payment getCompletedPayment() {
+        return completedPayment;
+    }
+
+    public LottoCount getLottoCount() {
+        return lottoCount;
+    }
+}
