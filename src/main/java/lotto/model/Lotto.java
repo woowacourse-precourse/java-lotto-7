9bec5f1fcba9f4444b1dccd10c6376048f50b585
acupoframen <choe.hwan@gmail.com>
2024-11-01 13:15:38 +0900

    fix (LottoResult): fix validate - check lotto numbers are unique

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-    public class Lotto {
+public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,11 +18,17 @@ import java.util.List;
         final int MIN_NUMBER = 1;
         final int MAX_NUMBER = 45;
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
-        if (numbers.get(0)<MIN_NUMBER || numbers.get(5)>=MAX_NUMBER){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            if (numbers.indexOf(number) != i) {
+                throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+                }
+            }
 
     }
     private List<Integer> sortLotto(List <Integer> numbers){
