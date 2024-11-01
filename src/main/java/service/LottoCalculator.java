26d848faa515e75d633e454e5d9b0be2e7f54982
Mini <mini@MiniMac.local>
2024-11-02 00:33:55 +0900

    feat: add calculateRank method to LottoCalculator

--- src/main/java/service/LottoCalculator.java
+++ src/main/java/service/LottoCalculator.java
@@ -1,4 +1,26 @@
 package service;
 
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+
+import java.util.List;
+
 public class LottoCalculator {
+
+    public static LottoRank calculateRank(Lotto userLotto, List<Integer> winningNumbers, int bonusNumber) {
+        int matchCount = 0;
+        boolean bonusMatch = false;
+
+        for (int number : userLotto.getNumbers()) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+            if (number == bonusNumber) {
+                bonusMatch = true;
+            }
+        }
+        return LottoRank.findRank(matchCount, bonusMatch);
+
+
+    }
 }
