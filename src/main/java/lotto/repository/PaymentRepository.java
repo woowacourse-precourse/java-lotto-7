a90564c16acaa093f1d62f3a3610250cca71453d
plan11plan <tjtfy7028@gmail.com>
2024-11-01 16:48:39 +0900

    feat: 로또 결제 레파지토리 저장 구현

--- src/main/java/lotto/repository/PaymentRepository.java
+++ src/main/java/lotto/repository/PaymentRepository.java
@@ -1,9 +1,16 @@
 package lotto.repository;
 
+import java.util.concurrent.ConcurrentHashMap;
 import lotto.domain.Payment;
 
 public class PaymentRepository {
+    private final ConcurrentHashMap repository = new ConcurrentHashMap<Long, Payment>();
+
+    private PaymentRepository() {
+    }
+
     public Long save(Payment payment) {
-        return null;
+        repository.put(payment.getId(), payment);
+        return payment.getId();
     }
 }
