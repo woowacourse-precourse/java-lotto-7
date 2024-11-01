ebc7c7c6767ef6727e6c0fd67e8e90b04f8f09b3
seophohoho <seop0937@gmail.com>
2024-11-01 15:04:59 +0900

    feat(Seller): 올바른 Lotto 구매 금액임을 검증하는 validate 메소드 추가

--- src/main/java/lotto/model/Seller.java
+++ src/main/java/lotto/model/Seller.java
@@ -1,2 +1,26 @@
-package lotto.model;public class Seller {
+package lotto.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Seller {
+    private final String PAY_PATTERN = "^[1-9][0-9]{3,}";
+
+    public void validate(String input) {
+        Pattern patternPay = Pattern.compile(PAY_PATTERN);
+        Matcher matcherPay = patternPay.matcher(input);
+
+        if (!matcherPay.matches()) {
+            reject();
+        }
+
+        int money = Integer.parseInt(input);
+        if (money % 1000 != 0) {
+            reject();
+        }
+    }
+
+    public void reject() {
+        throw new IllegalArgumentException();
+    }
 }
