4043042517373e4ae5b0ad7a98d025e278428ff5
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-01 15:20:30 +0900

    refactor(Lotto): 중복값 검증 메서드 명 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,7 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
-        validateNotDuplicate(numbers);
+        validateUnique(numbers);
         this.numbers = numbers;
     }
 
@@ -17,7 +17,7 @@ public class Lotto {
         }
     }
 
-    private static void validateNotDuplicate(List<Integer> numbers) {
+    private static void validateUnique(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
