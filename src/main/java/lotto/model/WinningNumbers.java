cba5f8b5acaac858e8d227693e6bf94377a03596
sik9252 <sik9252@naver.com>
2024-11-01 12:45:23 +0900

    refactor: 당첨 번호 문자열을 입력 받아 가공한 후 로또 형식으로 만드는 메서드들을 View에서 분리, WinningNumbers 클래스 내부로 이동

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,15 +1,56 @@
 package lotto.model;
 
+import lotto.exception.InvalidWinningNumbersException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
-public class WinningNumbers {
-    private final List<Integer> winningNumbers;
+import static lotto.exception.ErrorMessage.DUPLICATE_NUMBER_IS_NOT_ALLOWED;
+import static lotto.exception.ErrorMessage.INVALID_NUMBER_RANGE;
+import static lotto.model.Lotto.MAX_LOTTO_NUMBER;
+import static lotto.model.Lotto.MIN_LOTTO_NUMBER;
 
-    public WinningNumbers(List<Integer> winningNumbers) {
-        this.winningNumbers = winningNumbers;
+public class WinningNumbers {
+    private List<Integer> winningNumbers;
+
+    public WinningNumbers(String winningString) {
+        createLottoByWinningString(winningString);
     }
 
     public List<Integer> get() {
         return winningNumbers;
+    }
+
+    private void createLottoByWinningString(String winningString) {
+        List<Integer> createdLotto = processingWinningNumbers(winningString);
+        checkEachLottoNumberHasValidate(createdLotto);
+
+        winningNumbers = createdLotto;
+    }
+
+    private List<Integer> processingWinningNumbers(String winningString) {
+        List<String> splitStringByComma = List.of(winningString.split(","));
+        List<Integer> processedWinningNumbers = new ArrayList<>();
+
+        for (String string : splitStringByComma) {
+            processedWinningNumbers.add(Integer.parseInt(string.trim()));
+        }
+
+        return processedWinningNumbers;
+    }
+
+    private void checkEachLottoNumberHasValidate(List<Integer> winningNumbers) {
+        HashSet<Integer> numbers = new HashSet<>();
+
+        for (int number : winningNumbers) {
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+                throw new InvalidWinningNumbersException(INVALID_NUMBER_RANGE.getMessage());
+            }
+
+            if (!numbers.add(number)) {
+                throw new InvalidWinningNumbersException(DUPLICATE_NUMBER_IS_NOT_ALLOWED.getMessage());
+            }
+        }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,81 +5,47 @@ import lotto.exception.InvalidBonusNumberException;
 import lotto.exception.InvalidPurchaseAmountException;
 import lotto.exception.InvalidWinningNumbersException;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
-import static lotto.exception.ErrorMessage.DUPLICATE_NUMBER_IS_NOT_ALLOWED;
 import static lotto.exception.ErrorMessage.EMPTY_INPUT_IS_NOT_POSSIBLE;
 import static lotto.exception.ErrorMessage.INVALID_NUMBER_FORMAT;
-import static lotto.exception.ErrorMessage.INVALID_NUMBER_RANGE;
 import static lotto.exception.ErrorMessage.INVALID_WINNING_NUMBERS_FORMAT;
 
 public class InputView {
     public int getPurchaseAmount() {
-        int purchaseAmount;
         System.out.println("구입금액을 입력해 주세요.");
 
         try {
-            purchaseAmount = Integer.parseInt(Console.readLine());
-            return purchaseAmount;
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new InvalidPurchaseAmountException(INVALID_NUMBER_FORMAT.getMessage());
         }
     }
 
-    public List<Integer> getWinningNumbers() {
-        String winningNumbers;
+    public String getWinningLotto() {
         System.out.println("\n" + "당첨 번호를 입력해 주세요.");
-        winningNumbers = Console.readLine();
 
-        if (winningNumbers.isEmpty()) {
-            throw new InvalidWinningNumbersException(EMPTY_INPUT_IS_NOT_POSSIBLE.getMessage());
-        }
+        String winningString = Console.readLine();
+        checkEnteredWinningStringIsValid(winningString);
 
-        if (!winningNumbers.matches("^\\d+(,\\d+){5}")) {
-            throw new InvalidWinningNumbersException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
-        }
-
-        return createLottoWithWinningNumbers(winningNumbers);
+        return winningString;
     }
 
     public int getBonusNumber() {
-        int bonusNumber;
         System.out.println("\n" + "보너스 번호를 입력해 주세요.");
 
         try {
-            bonusNumber = Integer.parseInt(Console.readLine());
-
-            if (bonusNumber < 1 || bonusNumber > 45) {
-                throw new InvalidBonusNumberException(INVALID_NUMBER_RANGE.getMessage());
-            }
-
-            return bonusNumber;
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new InvalidBonusNumberException(INVALID_NUMBER_FORMAT.getMessage());
         }
     }
 
-    private List<Integer> createLottoWithWinningNumbers(String winningString) {
-        List<String> splitWinningString = List.of(winningString.split(","));
-        List<Integer> winningNumbers = new ArrayList<>();
-        HashSet<Integer> uniqueNumbers = new HashSet<>();
-
-        for (String string : splitWinningString) {
-            int number = Integer.parseInt(string);
-
-            if (number < 1 || number > 45) {
-                throw new InvalidWinningNumbersException(INVALID_NUMBER_RANGE.getMessage());
-            }
-
-            if (!uniqueNumbers.add(number)) {
-                throw new InvalidWinningNumbersException(DUPLICATE_NUMBER_IS_NOT_ALLOWED.getMessage());
-            }
-
-            winningNumbers.add(number);
+    private void checkEnteredWinningStringIsValid(String winningString) {
+        if (winningString.isEmpty()) {
+            throw new InvalidWinningNumbersException(EMPTY_INPUT_IS_NOT_POSSIBLE.getMessage());
         }
 
-        return winningNumbers;
+        if (!winningString.matches("^\\d+(,\\d+){5}")) {
+            throw new InvalidWinningNumbersException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
+        }
     }
 }
