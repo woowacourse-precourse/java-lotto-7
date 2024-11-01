5d120c317437a4549a31f062ba66a38768428ee7
kungbi <kipper12335@gmail.com>
2024-11-01 17:16:13 +0900

    test(ProfitReport): 수익률 계산 테스트 및 테스트케이스 추가

--- src/test/java/lotto/entity/ProfitReportTest.java
+++ src/test/java/lotto/entity/ProfitReportTest.java
@@ -14,6 +14,64 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class ProfitReportTest {
 
+
+    static Stream<Arguments> 수익_계산_테스트_케이스() {
+        return Stream.of(
+                // 1등
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6), 7, 2_000_000_000),
+
+                // 2등 (5개 일치 + 보너스 번호 일치)
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 5, 6), 7, 30_000_000),
+
+                // 3등 (5개 일치)
+                Arguments.of(List.of(1, 2, 3, 4, 5, 11), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 10), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),
+                Arguments.of(List.of(1, 2, 3, 4, 7, 10), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),
+
+                // 4등 (4개 일치)
+                Arguments.of(List.of(1, 2, 3, 4, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),
+                Arguments.of(List.of(1, 2, 3, 7, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),
+
+                // 5등 (3개 일치)
+                Arguments.of(List.of(1, 2, 3, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 5_000),
+                Arguments.of(List.of(1, 2, 3, 14, 15, 16), List.of(1, 2, 3, 4, 5, 6), 7, 5_000),
+
+                // 당첨되지 않음 (0~2개 일치)
+                Arguments.of(List.of(8, 9, 10, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 0),
+                Arguments.of(List.of(30, 31, 32, 33, 34, 35), List.of(1, 2, 3, 4, 5, 6), 7, 0),
+
+                // 보너스 번호만 일치 (당첨되지 않음)
+                Arguments.of(List.of(7, 8, 9, 10, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 0)
+        );
+    }
+
+
+    static Stream<Arguments> 수익률_계산_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))), // 구매한 로또 리스트
+                        List.of(1, 2, 3, 4, 5, 6), // 당첨 번호 리스트
+                        7, // 보너스 번호
+                        200000000.0 // 예상 수익률 (2,000,000,000 / 1,000 * 100)
+                ),
+                Arguments.of(
+                        List.of(
+                                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                                new Lotto(List.of(10, 11, 12, 13, 14, 15))
+                        ),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        100000000.0 // 예상 수익률 ((2,000,000,000 / 2,000) * 100)
+                ),
+                Arguments.of(
+                        List.of(new Lotto(List.of(8, 9, 10, 11, 12, 13))),
+                        List.of(1, 2, 3, 4, 5, 6),
+                        7,
+                        0.0 // 예상 수익률 (0 / 1,000 * 100)
+                )
+        );
+    }
+
     // 생성 테스트
 
     @Test
@@ -42,6 +100,8 @@ class ProfitReportTest {
         Assertions.assertEquals("구매한 로또는 null 또는 비어있을 수 없습니다.", exception.getMessage());
     }
 
+    // getter 테스트들
+
     @Test
     void 실패__로또_EMPTY() {
         // given
@@ -54,8 +114,6 @@ class ProfitReportTest {
         // then
         Assertions.assertEquals("구매한 로또는 null 또는 비어있을 수 없습니다.", exception.getMessage());
     }
-
-    // getter 테스트들
 
     @Test
     void 성공__당첨_번호_가져오기() {
@@ -120,6 +178,8 @@ class ProfitReportTest {
         assertEquals(lotto, actual.get(0));
     }
 
+    // 계산 테스트들
+
     @Test
     void 성공__투자한_금액_조회() {
         // given
@@ -132,21 +192,6 @@ class ProfitReportTest {
 
         // then
         assertEquals(1000, actual);
-    }
-
-    // 계산 테스트들
-
-    static Stream<Arguments> 수익_계산_테스트_케이스() {
-        return Stream.of(
-                Arguments.of(List.of(1, 2, 3, 4, 5, 6), List.of(1, 2, 3, 4, 5, 6), 7, 2_000_000_000),   // 1등
-                Arguments.of(List.of(1, 2, 3, 4, 5, 7), List.of(1, 2, 3, 4, 5, 6), 7, 30_000_000),      // 2등
-                Arguments.of(List.of(1, 2, 3, 4, 5, 11), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),      // 3등
-                Arguments.of(List.of(1, 2, 3, 4, 7, 11), List.of(1, 2, 3, 4, 5, 6), 7, 1_500_000),      // 3등 + 보너스 번호
-                Arguments.of(List.of(1, 2, 3, 4, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),        // 4등
-                Arguments.of(List.of(1, 2, 3, 7, 11, 12), List.of(1, 2, 3, 4, 5, 6), 7, 50_000),        // 4등 + 보너스 번호
-                Arguments.of(List.of(1, 2, 3, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 5_000),        // 5등
-                Arguments.of(List.of(1, 2, 7, 11, 12, 13), List.of(1, 2, 3, 4, 5, 6), 7, 5_000)         // 5등 + 보너스 번호
-        );
     }
 
     @ParameterizedTest
@@ -164,9 +209,20 @@ class ProfitReportTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void 성공__수익률_계산_테스트_케이스() {
-        // calculateProfit
+    @ParameterizedTest
+    @MethodSource("수익률_계산_테스트_케이스")
+    void 성공__수익률_계산_테스트_케이스(List<Lotto> lottoInputs, List<Integer> winningNumbersInput, int bonusNumber,
+                            double expected) {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersInput, bonusNumber);
+        ProfitReport profitReport = new ProfitReport(lottoInputs, winningNumbers);
+
+        // when
+        double actual = profitReport.calculateProfitRate();
+
+        // then
+        assertEquals(expected, actual);
+
     }
 
     @Test
