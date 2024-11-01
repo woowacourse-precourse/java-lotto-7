3b68fba6758b7d3549b196824f449c9ce521fb29
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-01 18:19:35 +0900

    test: 문자열을  정수로 변환하는 테스트 추가

---
+++ src/test/java/lotto/InputValidator/ParserTest.java
@@ -0,0 +1,27 @@
+package lotto.InputValidator;
+
+import lotto.exception.InputErrorMessage;
+import lotto.util.ParserUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ParserTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"YOON", "*&!"})
+    void 입력값이_숫자가_아니면_예외발생(String inValidAmount) {
+        assertThatThrownBy(() -> ParserUtil.parseInt(inValidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputErrorMessage.INVALID_NUMERIC_FORMAT.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {""," "})
+    void 입력값이_없다면_예외발생(String inValidAmount) {
+        assertThatThrownBy(() -> ParserUtil.parseInt(inValidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputErrorMessage.EMPTY_INPUT.getMessage());
+    }
+}
