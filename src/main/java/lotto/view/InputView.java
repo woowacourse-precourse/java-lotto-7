023a0ca18ac89547ccf5a138bda44586c46ba6f8
MinJae00 <exceedmin@gmail.com>
2024-11-02 00:05:07 +0900

    feat: 구입 금액을 입력받는 Input 기능 구현, 당첨 번호 입력 Input은 아직 미완성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,29 @@
-package lotto.view;public class InputView {
+package lotto.view;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import lotto.controller.LottoController;
+import java.util.List;
+public class InputView {
+    private LottoController controller;
+
+    public void inputPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int price = Integer.parseInt(Console.readLine());
+        controller.createLottoNumber(price);
+    }
+
+    public void inputNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String s;
+        s = Console.readLine();
+    }
+
+
+    public InputView(){
+        controller = new LottoController();
+    }
 }
+
+
+
+
