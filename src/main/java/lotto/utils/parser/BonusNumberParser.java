d78074dc529555654b6813ac05ecef951c8e5d8f
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-01 22:19:13 +0900

    feat: 보너스 숫자 파싱 코드 작성

---
+++ src/main/java/lotto/utils/parser/BonusNumberParser.java
@@ -0,0 +1,38 @@
+package lotto.utils.parser;
+
+import static lotto.constants.ErrorMessage.CANT_DUPLICATED_BONUS_NUMBER_WITH_WINNING_NUMBER;
+import static lotto.constants.ErrorMessage.INPUT_BONUS_NUMBER_OUT_OF_RANGE;
+import static lotto.constants.ErrorMessage.ONLY_INTEGER_ALLOWED;
+
+import java.util.List;
+import lotto.dto.LottoNumbers;
+import lotto.model.Lotto;
+
+public class BonusNumberParser {
+    public static int getBonusNumber(Lotto winningNumbers, String userBonusNumber) {
+        int bonusNumber = 0;
+        try {
+            bonusNumber = Integer.parseInt(userBonusNumber);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ONLY_INTEGER_ALLOWED.getMessage());
+        }
+        checkBonusNumberRange(bonusNumber);
+        checkDuplicatedCheck(winningNumbers, bonusNumber);
+        return bonusNumber;
+    }
+
+    private static void checkBonusNumberRange(int bonusNumber) {
+        if (!(bonusNumber >= 1 && bonusNumber <= 45)){
+            throw new IllegalArgumentException(INPUT_BONUS_NUMBER_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private static void checkDuplicatedCheck(Lotto winningNumbers, int bonusNumber) {
+        LottoNumbers lottoNumbers = winningNumbers.getLottoNumbers();
+        List<Integer> winningLottoNumbers = lottoNumbers.getLottoNumbers();
+        if (winningLottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(CANT_DUPLICATED_BONUS_NUMBER_WITH_WINNING_NUMBER.getMessage());
+        }
+    }
+
+}
