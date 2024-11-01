6f2c01e31c0dc5ba463d478fd4babc635aa40653
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 16:36:00 +0900

    test(LottoResult): 로또 결과 객체 생성 테스트를 정의하라

---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class LottoResultTest {
+    private LottoResult lottoResult;
+
+    @BeforeEach
+    void 로또_결과_객체를_생성한다() {
+        lottoResult = new LottoResult();
+    }
+}
