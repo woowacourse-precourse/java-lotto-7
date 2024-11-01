2481a9bc785c8c96188197fb11aaba6a1c37c0c8
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-01 18:27:41 +0900

    feat: 문자열을 정수로 반환하는 PaserUtil 클래스 추가
    - 공백 , 아무것도 입력 하지 않을시 예외 발생
    - 숫자 형식이 아니면 예외 발생

---
+++ src/main/java/lotto/util/ParserUtil.java
@@ -0,0 +1,28 @@
+package lotto.util;
+
+import lotto.exception.InputErrorMessage;
+
+public class ParserUtil {
+
+    private ParserUtil() {
+    }
+
+    public static int parseInt(String input) {
+        validateNotEmpty(input);
+        validateNumericFormat(input);
+
+        return Integer.parseInt(input);
+    }
+
+    private static void validateNotEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(InputErrorMessage.EMPTY_INPUT.getMessage());
+        }
+    }
+
+    private static void validateNumericFormat(String input) {
+        if (!input.matches("\\d+")) {  // 숫자 형식이 아니면 예외 발생
+            throw new IllegalArgumentException(InputErrorMessage.INVALID_NUMERIC_FORMAT.getMessage());
+        }
+    }
+}
