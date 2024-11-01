005bfc82283ecf379fd587ea88d5372eb3a70e90
신관규 <soundbar91@gmail.com>
2024-11-01 16:39:00 +0900

    feat: 로또 구입 금액이 숫자만으로 구성됐는 지 체크

--- src/main/java/lotto/validator/LottoPurchaseAmountValidator.java
+++ src/main/java/lotto/validator/LottoPurchaseAmountValidator.java
@@ -1,6 +1,18 @@
 package lotto.validator;
 
 public class LottoPurchaseAmountValidator {
+    private static String LOTTO_PURCHASE_AMOUNT_REGEX = "^[0-9]+$";
+
     private LottoPurchaseAmountValidator() {
+    }
+
+    public static void validateLottoPurchaseAmount(String lottoPurchaseAmount) {
+        checkIncludeSpecialCharacters(lottoPurchaseAmount);
+    }
+
+    private static void checkIncludeSpecialCharacters(String lottoPurchaseAmount) {
+        if (!lottoPurchaseAmount.matches(LOTTO_PURCHASE_AMOUNT_REGEX)) {
+            throw new IllegalArgumentException("로또 구입 금액은 숫자로 입력해야 합니다.");
+        }
     }
 }
