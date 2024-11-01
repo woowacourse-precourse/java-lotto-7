40c6d5383a15da1394a7f79e32dc54cf72dcfe76
imscow11253 <seaweed8759@naver.com>
2024-11-02 01:54:53 +0900

    add(ErrorMessage) : Enum 클래스로 에러 메시지 상수화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 
+import static lotto.common.ErrorMessage.*;
+
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -12,7 +14,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LOTTO_SHOULD_BE_SIX.getMessage());
         }
     }
 
