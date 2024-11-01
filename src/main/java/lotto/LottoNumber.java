2e319a5e340748b7c36ca4034b958139b680fb82
hyuuuun <pam4522@gmail.com>
2024-11-01 18:50:33 +0900

    feat(LottoNumber) : 1-45 숫자 유효성 검증

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class LottoNumber {
+    private final int number;
+    public LottoNumber(int number){
+        vaildateNumberRange(number);
+        this.number=number;
+    }
+    private void vaildateNumberRange(int number){
+        if(number <1 || number>45){
+            throw new IllegalArgumentException("[ERROR] 번호는 1-45의 숫자여야 합니다.");
+        }
+    }
+
+}
