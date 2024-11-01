45293dbbd08c3acb896bb107264996a760d49755
kimdev <kim__dev@hufs.ac.kr>
2024-11-01 15:10:25 +0900

    test(Lotto): 당첨 숫자 개수 확인 테스트를 정의하라

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+    private Lotto lotto;
+
+    @BeforeEach
+    void 로또_객체_생성() {
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -31,5 +39,11 @@ class LottoTest {
     void 로또_번호에_음수가_포함된다면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(-1, -2, 3, 4, 5, 6)))
                 .isInstanceOf((IllegalArgumentException.class));
+    }
+
+    @Test
+    void 로또_당첨_숫자의_개수를_반환한다() {
+        long matchedCount = lotto.getMatchedNumbersCount(List.of(6, 5, 4, 3, 2, 1));
+        assertThat(matchedCount).isEqualTo(6);
     }
 }
