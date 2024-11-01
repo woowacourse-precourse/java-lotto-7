49535c23987fdbe52599b65aaa6398acc7c03ab0
201912025 <michi2012@naver.com>
2024-11-01 14:59:20 +0900

    feat: 로또번호 범위 검증 메서드 구현

---
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -0,0 +1,14 @@
+package lotto.validator;
+
+import java.util.List;
+
+public class LottoValidator {
+
+    public void validateLottoRange(List<Integer> lottoNumbers) {
+        for (Integer number : lottoNumbers) {
+            if (number < 1 | number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 정수여야 합니다. 잘못된 번호: " + number);
+            }
+        }
+    }
+}
