82a514418ab09e44e468ded1619d8a5e320888f2
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-02 19:19:19 +0900

    feat: Lotto클래스 작성
    - validate 메서드 작성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import lotto.error.Error;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateCount(numbers);
+        validateWithinRange(numbers);
+        validateUnique(numbers);
+    }
+
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(Error.INVALID_LOTTO_COUNT.getMessage());
+        }
+    }
+
+    private void validateWithinRange(List<Integer> numbers) {
+        for (int num: numbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException(Error.OUT_OF_RANGE.getMessage());
+            }
+        }
+    }
+
+    private void validateUnique(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException(Error.DUPLICATE_LOTTO_NUMBERS.getMessage());
+        }
+    }
+}
