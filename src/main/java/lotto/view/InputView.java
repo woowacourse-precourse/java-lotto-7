239f3f46666099ea90382f976c655e7c04084699
acupoframen <choe.hwan@gmail.com>
2024-11-02 16:52:55 +0900

    fix (InputView): Change return parameter into string - change parameter to String, to check non-integer value error

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static int inputPurchaseAmount() {
+    public static String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
     public static String inputWinningNumbers() {
@@ -13,8 +13,8 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static int inputBonusNumber() {
+    public static String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 }
