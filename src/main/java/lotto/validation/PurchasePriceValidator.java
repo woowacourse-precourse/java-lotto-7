63f124abcca96f64e28dc5ee1a9c72e20157150d
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-02 23:48:12 +0900

    refactor : 에러문구 생성

--- src/main/java/lotto/validation/PurchasePriceValidator.java
+++ src/main/java/lotto/validation/PurchasePriceValidator.java
@@ -1,7 +1,7 @@
 package lotto.validation;
 
 public class PurchasePriceValidator {
-    //구매금액 유효성 검증
+
     public static void validator(String price) {
         checkNaturalNumber(price);
         checkDivideIntoThousand(price);
@@ -11,17 +11,17 @@ public class PurchasePriceValidator {
         try {
             int amount = convertToInteger(price);
             if (amount < 1) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 구입 금액이 자연수가 아닙니다.");
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 구입 금액이 자연수가 아닙니다.");
         }
     }
 
     private static void checkDivideIntoThousand(String price) {
         int amount = convertToInteger(price);
         if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("1000원으로 나누어떨어지지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 1000원으로 나누어떨어지지 않습니다.");
         }
     }
 
