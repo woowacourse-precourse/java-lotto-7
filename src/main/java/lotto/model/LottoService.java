7a4833384906cfb935965049b1a08ddd8f03c2cf
soeun2537 <soeun2537@gmail.com>
2024-11-02 16:44:21 +0900

    feat: 당첨 티켓 생성 확인하는 로직 추가

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import static lotto.constant.core.LottoServiceConstant.*;
 
+import java.util.List;
+import lotto.model.domain.WinningLotto;
 import lotto.model.util.RandomUtil;
 
 public class LottoService {
@@ -14,5 +16,9 @@ public class LottoService {
 
     public Integer calculateLottoCount(Integer lottoPurchaseAmount) {
         return lottoPurchaseAmount / LOTTO_PRICE.getIntegerValue();
+    }
+
+    public WinningLotto createWinningTicket(List<Integer> numbers, Integer bonusNumber) {
+        return WinningLotto.of(numbers, bonusNumber);
     }
 }
