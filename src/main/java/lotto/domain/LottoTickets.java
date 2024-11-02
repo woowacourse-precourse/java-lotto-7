c5d09f3844512d1a24f3ca1ed43308eff8f60aeb
dltmddus <tmddus4671@gmail.com>
2024-11-02 18:26:48 +0900

    fix: lottos -> lotteries 변수 네이밍 수정

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -5,17 +5,21 @@ import java.util.List;
 import java.util.stream.LongStream;
 
 public class LottoTickets {
-    private final List<Lotto> lottos;
+    private final List<Lotto> lotteries;
 
-    public LottoTickets(List<Lotto> lottos) {
-        this.lottos = lottos;
+    public LottoTickets(List<Lotto> lotteries) {
+        this.lotteries = lotteries;
+    }
+
+    public List<Lotto> getLotteries() {
+        return lotteries;
     }
 
     public static LottoTickets generateLottoTickets(long count) {
-        List<Lotto> lottos = LongStream.range(0, count)
+        List<Lotto> lotteries = LongStream.range(0, count)
                 .mapToObj(i -> new Lotto(getRandomNumber()))
                 .toList();
-        return new LottoTickets(lottos);
+        return new LottoTickets(lotteries);
     }
 
     private static List<Integer> getRandomNumber() {
