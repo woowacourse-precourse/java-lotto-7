83cc036c8c25389ad1dd758ff3d070234030a8a0
dbdb456 <adad568@naver.com>
2024-11-02 15:27:21 +0900

    feat(domain): 로또 당첨 번호 비교 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,6 +10,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public boolean matchNumber(int number) {
+        return numbers.contains(number);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
