f1af64afd04e754abc22a1c0fc82339a9c11d39b
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 02:41:18 +0900

    feat(LottoNumberSupplierImpl): 난수로 구성된 로또 번호를 제공하는 메소드 구현

---
+++ src/main/java/lotto/util/LottoNumberSupplierImpl.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+public class LottoNumberSupplierImpl implements LottoNumberSupplier {
+
+    @Override
+    public List<Integer> getLottoNumbers() {
+        Set<Integer> nonOverlappingNumbers = new HashSet<>();
+
+        while (nonOverlappingNumbers.size() < LOTTO_SIZE) {
+            nonOverlappingNumbers.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        }
+
+       return new ArrayList<>(nonOverlappingNumbers);
+    }
+}
