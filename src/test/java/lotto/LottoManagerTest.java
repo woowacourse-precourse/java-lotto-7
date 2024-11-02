50bd803b6284c5b639023e515dbe55d536be661c
yaongmeow <minnie_00@naver.com>
2024-11-03 00:04:43 +0900

    test(LottoManager): 테스트코드 작성
    - 로또 당첨 결과 산출 테스트 코드 추가
    - 로또 수익률 산출 테스트 코드 추가

---
+++ src/test/java/lotto/LottoManagerTest.java
@@ -0,0 +1,56 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LottoManagerTest {
+
+    @Test
+    void 당첨_결과를_산출한다() {
+        // given
+        List<Lotto> myLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
+                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+        );
+
+        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Integer bonusNumber = 8;
+
+        LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
+
+        // when
+        List<LottoResult> lottoResult = lottoManager.processResult();
+
+        // then
+        assertThat(lottoResult).isEqualTo(List.of(
+                LottoResult.THREE,
+                LottoResult.NONE,
+                LottoResult.FOUR
+        ));
+    }
+
+    @Test
+    void 수익률을_산출한다() {
+        // given
+        List<Lotto> myLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
+                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+        );
+
+        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Integer bonusNumber = 8;
+        LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
+
+        // when
+        lottoManager.processResult();
+        Double profitRate = lottoManager.calculateProfitRate();
+
+        // then
+        assertThat(profitRate).isEqualTo(50166.7);
+    }
+}
