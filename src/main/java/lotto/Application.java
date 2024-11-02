d804d965ebac0882279e0d4877647e6bab568064
exena <exena01@gmail.com>
2024-11-02 23:08:40 +0900

    feat: 총 수익률 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -14,7 +14,8 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         String input_pay = readLine();
-        Integer number_of_times = Integer.valueOf(input_pay) / 1000;
+        Integer pay = Integer.valueOf(input_pay);
+        Integer number_of_times = pay / 1000;
         System.out.println("\n" + number_of_times + "개를 구매했습니다.");
         List<Lotto> all_lottos = new ArrayList<>();
         for (int i = 0; i < number_of_times; i++) {
@@ -52,5 +53,11 @@ public class Application {
         System.out.println("5개 일치 (" + third.prize + "원) - " + (match_count.get(5) - bonus_match) +"개");
         System.out.println("5개 일치, 보너스 볼 일치 (" + second.prize + "원) - " + bonus_match +"개");
         System.out.println("6개 일치 (" + first.prize + "원) - " + match_count.get(6) +"개");
+        Double sum_prize = (double)(fifth.prize * match_count.get(3)
+                        + forth.prize * match_count.get(4)
+                        + third.prize * (match_count.get(5) - bonus_match)
+                        + second.prize * bonus_match
+                        + first.prize * match_count.get(6));
+        System.out.println("총 수익률은 "+ String.format("%.1f", sum_prize / pay * 100) +"%입니다.");
     }
 }
