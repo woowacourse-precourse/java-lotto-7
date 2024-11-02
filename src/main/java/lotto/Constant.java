8da74dbd35adce41fcbacefb8283ad6226dd13ad
jintakkim <mine407603@gmail.com>
2024-11-02 21:35:00 +0900

    chore(Constant): 검증에 필요한 상수 값 추가
    검증 로직에서 사용되는 상수 값을 추가한다

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -2,4 +2,7 @@ package lotto;
 
 public class Constant {
     public static final int LOTTO_PRICE = 1000;
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
 }
