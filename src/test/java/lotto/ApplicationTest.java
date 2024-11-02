4a44a39439a5c33787dd184064bb29f0199373c5
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-03 03:04:33 +0900

    test(Validator): 당첨 번호 입력 유효성 검사 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -34,6 +34,20 @@ class ApplicationTest extends NsTest {
                 .hasMessageContaining("금액은 1000단위로 입력되어야 합니다.");
     }
 
+    @Test
+    void 당첨_번호_입력_유효성_검사_테스트() {
+        assertThatThrownBy(() -> Validator.validateWinNumbers(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 6개여야 합니다.");
+
+        assertThatThrownBy(() -> Validator.validateWinNumbers(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호의 숫자 범위는 1~45까지입니다.");
+
+        assertThatThrownBy(() -> Validator.validateWinNumbers(List.of(1, 2, 3, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 중복되지 않아야 합니다.");
+    }
 
     @Test
     void 기능_테스트() {
