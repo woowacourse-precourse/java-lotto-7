cebf57e35b200a54d51ab3484dc5a8863411f2ca
Chan531 <chan657312@gmail.com>
2024-11-02 20:40:59 +0900

    refactor(PurchaseLottoResultGenerator): 로또 리스트 변환 로직 수정
    반복문을 stream으로 변환

--- src/main/java/lotto/model/PurchaseLottoResultGenerator.java
+++ src/main/java/lotto/model/PurchaseLottoResultGenerator.java
@@ -1,18 +1,14 @@
 package lotto.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PurchaseLottoResultGenerator {
 
 	public List<List<Integer>> generatePurchaseLottoResult(LottoBundle lottoBundle) {
 		List<Lotto> lottos = lottoBundle.getLottos();
-		List<List<Integer>> result = new ArrayList<>();
-		for (Lotto lotto : lottos) {
-			List<LottoNumber> numbers = lotto.getNumbers();
-			result.add(getLottoNumbers(numbers));
-		}
-		return result;
+		return lottos.stream()
+				.map(lotto -> getLottoNumbers(lotto.getNumbers()))
+				.toList();
 	}
 
 	private List<Integer> getLottoNumbers(List<LottoNumber> numbers) {
