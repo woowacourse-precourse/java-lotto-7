42fd217e4e98260feb14df3ad31c344552ab0467
문고은 <>
2024-11-02 17:16:30 +0900

    feat(output) : 구입 금액 입력 요청 메시지 추가

---
+++ src/main/java/lotto/LottoOutput.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class LottoOutput {
+    private final static String PURCHASE_AMOUNT_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public void displayPurchaseAmountPrompt() {
+        System.out.println(PURCHASE_AMOUNT_PROMPT_MESSAGE);
+    }
+}
