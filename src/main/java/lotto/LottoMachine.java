8596b136174e30198ca588ef9dda909d362a1b57
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-11-02 15:17:47 +0900

    refactor: LottoMachine  클래스 파일 분리
    LottoMachine 구조 리팩토링,  PurchaseValidator로 클래스 분리

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -3,17 +3,6 @@ package lotto;
 import java.util.List;
 import java.util.Scanner;
 
-public class PurchaseValidator {
-    private static final int LOTTO_PRICE = 1000;
-
-    public static void validate(int amount) {
-        if (amount <= 0 || amount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
-        }
-    }
-}
-
-
 public class LottoMachine {
     private final LottoTicketGenerator ticketGenerator = new LottoTicketGenerator();
 
