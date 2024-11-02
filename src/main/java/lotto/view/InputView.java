daa8624968524bf3f0057e0977e86db672ae9723
HyeonBin2379 <hyeonBin2379@gmail.com>
2024-11-02 21:43:41 +0900

    refactor(InputView) : inputWinningNumbers 메서드의 당첨 번호 입력 로직 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,5 @@
 package lotto.view;
 
-import static lotto.util.constants.LottoConstants.INPUT_DELIMITER;
 import static lotto.util.message.InputMessage.INPUT_BONUS_NUMBER;
 import static lotto.util.message.InputMessage.INPUT_TOTAL_COST;
 import static lotto.util.message.InputMessage.INPUT_WINNING_LOTTO_NUMBER;
@@ -10,7 +9,6 @@ import static lotto.util.validator.LottoNumberValidator.validateNumberInRange;
 import static lotto.util.validator.TotalCostValidator.validateDividedByLottoPrice;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
@@ -18,13 +16,15 @@ public class InputView {
     public Long inputTotalCost() {
         System.out.println(INPUT_TOTAL_COST);
         String input = validateBlank(Console.readLine());
+
         return validateDividedByLottoPrice(input);
     }
 
     public List<Integer> inputWinningNumbers() {
         System.out.println(INPUT_WINNING_LOTTO_NUMBER);
-        String input = validateInputString(Console.readLine());
-        return Arrays.stream(input.split(INPUT_DELIMITER))
+        List<String> input = validateInputString(Console.readLine());
+
+        return input.stream()
                 .map(s -> validateNumberInRange(s.trim()))
                 .map(Long::intValue)
                 .toList();
@@ -33,6 +33,7 @@ public class InputView {
     public Long inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
         String input = validateBlank(Console.readLine());
+
         return validateNumberInRange(input);
     }
 }
