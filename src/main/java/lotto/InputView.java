94c094c666ccdad3d848b01e6be455e3a6b541db
ahpicl64 <ahpicl64@gmail.com>
2024-11-02 23:22:21 +0900

    feat(view): 당첨 및 보너스 번호 입력 구현
    각 요구사항에 맞는 안내 메세지 출력
    Console.readLine으로 입력된 당첨 및 보너스번호를 반환

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -8,12 +8,27 @@ public class InputView {
     private static final String PURCHASE_CONFIRM = "개를 구매했습니다.";
     private static final String REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_NUM = "보너스 번호를 입력해 주세요.";
+    private int purchaseAmount;
 
     public int purchaseLotto() {
         System.out.println(REQUEST_PURCHASE_MOUNT);
-        int purchaseAmount = Integer.parseInt(Console.readLine());
+        purchaseAmount = Integer.parseInt(Console.readLine());
 
         return purchaseAmount;
+    }
+
+    private void showDrawAmount(int drawAmount) {
+        System.out.println(drawAmount + PURCHASE_CONFIRM);
+    }
+
+    private String inputWinningNumber() {
+        System.out.println(REQUEST_WINNING_NUM);
+        return Console.readLine();
+    }
+
+    private String inputBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUM);
+        return Console.readLine();
     }
 
 
