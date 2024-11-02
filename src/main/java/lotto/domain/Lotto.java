b53cd0e53abfaced5e49ff401eb5bdb1e97848b0
gongPyeon <pyeonkang@naver.com>
2024-11-02 16:20:24 +0900

    feat(Lotto): 1개의 로또를 발행할 때 6개의 숫자가 중복되어있는지 검증하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,9 +18,15 @@ public class Lotto {
         }
     }
 
+    private void validateDuplicate(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+
     public List<Integer> getNumbers() {
         return numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
