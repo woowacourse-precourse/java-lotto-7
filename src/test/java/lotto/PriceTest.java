b53578eb3fe67ad6cd4f45404f282f16a85d6558
hongkikii <hyangmeeh@icloud.com>
2024-11-02 17:25:11 +0900

    test(PriceTest): 예외 상황 [ERROR] 테스트 코드 추가

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -22,42 +22,51 @@ public class PriceTest {
     @Test
     void 로또_구입_금액이_null일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> Price.from(null))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("로또 구입 금액에 문자가 포함될 경우 예외가 발생한다.")
     @Test
     void 로또_구입_금액에_문자가_포함될_경우_예외가_발생한다() {
         assertThatThrownBy(() -> Price.from("3000.14"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> Price.from("300 0"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> Price.from(" "))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> Price.from("*"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> Price.from("-1"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("로또 구입 금액이 1,000원으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
     @Test
     void 로또_구입_금액이_1000원으로_나누어_떨어지지_않는_경우_예외가_발생한다() {
         assertThatThrownBy(() -> Price.from("0"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> Price.from("1500"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("로또 구입 금액이 2,000,000,000원을 초과할 경우 예외가 발생한다.")
     @Test
     void 로또_구입_금액이_2000000000원을_초과할_경우_예외가_발생한다() {
         assertThatThrownBy(() -> Price.from("2000001000"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 }
