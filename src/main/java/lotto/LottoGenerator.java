b317478e5b1f688318500a1a0498d6ad33ec56fd
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 20:12:55 +0900

    style: 줄 바꿈 및 불필요한 import 제거

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 public class LottoGenerator {
     static final int PRICE_PER_LOTTO = 1000;
+
     public int getLottoCount(int budget) {
         if (budget < PRICE_PER_LOTTO) {
             throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO + "원 이상의 금액을 입력하세요.");
