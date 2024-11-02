f961eae3c73508dd720df8c6930a8e0cdff7b5fe
choiminu <alsdn4590@gmail.com>
2024-11-02 14:53:05 +0900

    feat(model): 로또 구입 금액이 천 원 단위인지 확인하는 유효성 검사 기능을 추가하였음
    - 로또 구입 금액이 1,000원 단위인지 확인하는 `validateThousandUnitAmount` 메서드를 구현하였음
    - `LottoShopTest`에 잘못된 금액에 대해 예외가 발생하는지 확인하는 파라미터화 테스트 `로또구입_금액이_천원단위가_아니면_예외발생`을 추가하였음

---
+++ src/main/java/lotto/domain/LottoShop.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+public class LottoShop {
+    private final int LOTTO_UNIT_PRICE = 1_000;
+    private final int NO_REMAINDER = 0;
+
+    public void buyLotto(int money) {
+        validateThousandUnitAmount(money);
+    }
+
+    private void validateThousandUnitAmount(int money) {
+        if (money % LOTTO_UNIT_PRICE != NO_REMAINDER) {
+            throw new IllegalArgumentException("로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoShopTest {
+
+    private final LottoShop lottoShop = new LottoShop();
+
+    @ParameterizedTest
+    @ValueSource(ints = {1500, 2500, 3500, 4200, 10001, Integer.MAX_VALUE})
+    void 로또구입_금액이_천원단위가_아니면_예외발생(int money) {
+        assertThatThrownBy(() -> lottoShop.buyLotto(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    }
+
+}
