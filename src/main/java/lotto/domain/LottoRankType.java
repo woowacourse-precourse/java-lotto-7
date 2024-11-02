8871602d3246c8b8337ab3540bca53495f183592
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-02 17:46:55 +0900

    feat(LottoRankType): 로또 순위를 정의하고 판별 로직 추가

---
+++ src/main/java/lotto/domain/LottoRankType.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRankType {
+
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean hasBonusNumber;
+    private final int price;
+
+    LottoRankType(int matchCount, boolean hasBonusNumber, int price) {
+        this.matchCount = matchCount;
+        this.hasBonusNumber = hasBonusNumber;
+        this.price = price;
+    }
+
+    public static LottoRankType of(int matchCount, boolean matchBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount && rank.hasBonusNumber == matchBonus)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isHasBonusNumber() {
+        return hasBonusNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
