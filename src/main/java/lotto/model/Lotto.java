8b94443ff848fee360301f67e65c4f55b832e17a
acupoframen <choe.hwan@gmail.com>
2024-11-02 17:27:04 +0900

    fix (Lotto): create [ERROR] message in Controller - only use [ERROR] in controller for better visibility

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -15,15 +15,15 @@ public class Lotto {
         final int MIN_NUMBER = 1;
         final int MAX_NUMBER = 45;
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(" 로또 번호는 6개여야 합니다.");
         }
         for (int i = 0; i < numbers.size(); i++) {
             int number = numbers.get(i);
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                throw new IllegalArgumentException(" 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
             if (numbers.indexOf(number) != i) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+                throw new IllegalArgumentException(" 로또 번호는 중복될 수 없습니다.");
                 }
             }
 
