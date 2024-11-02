630507459aee39e4d765ff9fb6fe30ed2a6b3dd6
문고은 <>
2024-11-02 23:50:49 +0900

    feat(output) : 당첨 번호 입력 요청 메시지 추가

--- src/main/java/lotto/LottoOutput.java
+++ src/main/java/lotto/LottoOutput.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class LottoOutput {
     private final static String PURCHASE_AMOUNT_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String PURCHASED_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    private final static String WINNING_NUMBERS_PROMPT_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public void displayPurchaseAmountPrompt() {
         System.out.println(PURCHASE_AMOUNT_PROMPT_MESSAGE);
@@ -18,5 +19,9 @@ public class LottoOutput {
         for (Lotto lotto : lottoTickets) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void displayWinningNumbersPrompt() {
+        System.out.println(WINNING_NUMBERS_PROMPT_MESSAGE);
     }
 }
