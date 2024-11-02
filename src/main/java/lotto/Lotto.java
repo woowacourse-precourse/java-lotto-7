98bed5a825ce9f46200db6f567522be3457e28d1
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-03 01:12:48 +0900

    feat: lotto 클래스 구현
    - validate 메서드에 리스트 요소의 중복값을 확인하는 코드를 추가하였습니다.
    - setPrizeTier 메서드를 구현하여 당첨 등수를 받고, 유효한 형태일 경우 당첨 등수 필드에 저장하는 기능을 추가하였습니다.
    - getPrizeTier 메서드를 구현하여 외부 클래스에서 lotto 객체의 당첨 등수에 접근할 수 있게 하였습니다.
    - 당첨 등수를 저장하는 prizeTier 멤버 변수를 선언하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int prizeTier = 0;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,7 +16,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Set<Integer> set = Set.copyOf(numbers);
+        if(set.size()<numbers.size()){
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
+    public void setPrizeTier(int prizeTier){
+        if(prizeTier < 0 || prizeTier> 5) {
+            throw new IllegalArgumentException("당첨 등수의 범위는 0~5등 입니다.");
+        }
+        this.prizeTier = prizeTier;
+    }
+
+    public int getPrizeTier(){
+        return prizeTier;
+    }
 }
