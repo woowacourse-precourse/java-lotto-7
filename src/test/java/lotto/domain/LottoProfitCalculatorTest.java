ce65376d1bfd003526df43dc5737e2e80e7a1c6b
ppparkta <pppppark2@gmail.com>
2024-11-02 18:48:04 +0900

    test(LottoProfitCalculator): 로또 결과 관련 테스트 추가
    추가사항
    - 로또가 일치하는 갯수 제대로 반환하는지 확인하는 테스트 추가
    - 로또 순위를 LottoRank enum으로 관리

---
+++ src/test/java/lotto/domain/LottoProfitCalculatorTest.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.common.RandomNumberGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoProfitCalculatorTest {
+    @DisplayName("로또가 일치하는 갯수를 반환한다.")
+    @Test
+    void 로또가_일치하는_갯수를_반환한다() {
+        // given
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));    // 1등
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));    // 2등
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 8, 9));    // 4등
+        Lotto lotto4 = new Lotto(List.of(8, 9, 10, 11, 12, 13));// 꽝
+        List<Lotto> lottos = List.of(lotto1, lotto2, lotto3, lotto4);
+        IssuedRandomLotto issuedLotto = new IssuedRandomLotto(generator, lottos, 4000);
+
+        LottoResult lottoResult = new LottoResult(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        LottoProfitCalculator lottoProfitCalculator = new LottoProfitCalculator(lottoResult, issuedLotto);
+
+        // when
+        List<LottoRank> lottoRanks = lottoProfitCalculator.calculateLottoStatistics();
+
+        // then
+        Assertions.assertThat(lottoRanks.size()).isEqualTo(3);
+        Assertions.assertThat(lottoRanks.get(0).getHitCount()).isEqualTo(LottoRank.FIRST.getHitCount());
+        Assertions.assertThat(lottoRanks.get(0).getPrize()).isEqualTo(LottoRank.FIRST.getPrize());
+        Assertions.assertThat(lottoRanks.get(0).isBonusHit()).isEqualTo(LottoRank.FIRST.isBonusHit());
+
+        Assertions.assertThat(lottoRanks.get(1).getHitCount()).isEqualTo(LottoRank.SECOND.getHitCount());
+        Assertions.assertThat(lottoRanks.get(1).getPrize()).isEqualTo(LottoRank.SECOND.getPrize());
+        Assertions.assertThat(lottoRanks.get(1).isBonusHit()).isEqualTo(LottoRank.SECOND.isBonusHit());
+
+        Assertions.assertThat(lottoRanks.get(2).getHitCount()).isEqualTo(LottoRank.FOURTH.getHitCount());
+        Assertions.assertThat(lottoRanks.get(2).getPrize()).isEqualTo(LottoRank.FOURTH.getPrize());
+        Assertions.assertThat(lottoRanks.get(2).isBonusHit()).isEqualTo(LottoRank.FOURTH.isBonusHit());
+    }
+
+    @DisplayName("로또 수익률을 계산한다.")
+    @Test
+    void 수익률을_계산한다() {
+        // given
+
+        // when
+
+        // then
+    }
+}
