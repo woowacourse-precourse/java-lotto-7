1c15de524425f5a4a442e662b610caa8ecea88ff
sejinezy <psj8880@naver.com>
2024-11-02 14:42:41 +0900

    refactor(Input): 메서드를 static으로 변경하여 유틸리티성 강화

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-    public String getPurchaseAmount() {
+    public static String getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public void validateNotEmpty(String input) {
+    public static void validateNotEmpty(String input) {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 입력값이 비어 있습니다.");
         }
     }
 
-    public void validateIsNumber(String input) {
+    public static void validateIsNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -23,17 +23,13 @@ public class Input {
         }
     }
 
-    public void validateDivisibleByThousand(int amount) {
+    public static void validateDivisibleByThousand(int amount) {
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
     }
 
-    public int toInt(String input) {
+    public static int toInt(String input) {
         return Integer.parseInt(input);
     }
-
-
-
-
 }
