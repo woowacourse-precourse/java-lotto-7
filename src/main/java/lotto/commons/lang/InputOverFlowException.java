a090f9757068cbfda188216189349e5b9844f928
hseungho <hsh47607@gmail.com>
2024-11-03 04:54:57 +0900

    chore: InputOverFlowException default message 변경
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- src/main/java/lotto/commons/lang/InputOverFlowException.java
+++ src/main/java/lotto/commons/lang/InputOverFlowException.java
@@ -3,7 +3,7 @@ package lotto.commons.lang;
 public class InputOverFlowException extends RuntimeException {
 
     public InputOverFlowException() {
-        super("입력 기회를 초과하였습니다.");
+        super("입력 기회를 초과하여 프로그램을 종료합니다.");
     }
 
     public InputOverFlowException(String message) {
