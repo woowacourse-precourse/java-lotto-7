863ae8f26acfe3fee911856eec438d5d5fc415d1
hmlssghm <kwonse326@gmail.com>
2024-11-02 23:55:34 +0900

    feat(ErrorMessage): 에러 메시지 추가

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.common;
 
 public enum ErrorMessage {
-    EMPTY_INPUT("[ERROR] 입력이 비어있습니다.");
+    EMPTY_INPUT("[ERROR] 입력이 비어있습니다."),
+    WINNING_NUMBER_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
     private final String message;
 
     ErrorMessage(String message) {
