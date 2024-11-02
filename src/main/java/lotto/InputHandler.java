1962055c35fa72ad27e3478b50c6d0b62fbda402
Jungwoo Chu <cjw980930@gmail.com>
2024-11-02 21:30:06 +0900

    feat: 당첨번호 입력 받기
    중복된 숫자가 있을 경우 예외메세지 출력 후 다시 입력 받기.

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputHandler {
@@ -18,6 +20,32 @@ public class InputHandler {
     public void priceInputValidator(int price){
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000단위만 가능합니다.");
+        }
+    }
+
+    public List<Integer> winningNumbersInput(){
+        while(true){
+            try{
+                List<Integer> winningNumbers = new ArrayList<>();
+                List<String> numbersInString = Arrays.asList(Console.readLine().split(,));
+                for (String number : numbersInString){
+                    winningNumbers.add(Integer.parseInt(number));
+                }
+                winningNumbersInputValidator(winningNumbers);
+                return winningNumbers;
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void winningNumbersInputValidator(List<Integer> winningNumbers){
+        List<Integer> check = new ArrayList<>();
+        for (int number : winningNumbers){
+            if (check.contains(number)){
+                throw new IllegalArgumentException("[ERROR] 당첨번호에 중복된 숫자가 존재합니다.")
+            }
+            check.add(number);
         }
     }
 
