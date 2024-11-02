e98a602da7761f7d39c9a52773cce3e8d8cbc643
rdyjun <rdyjun00@gmail.com>
2024-11-02 19:14:37 +0900

    feat: Rank enum 클래스 구현

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import java.util.function.Function;
+
+public enum Rank {
+    FIFTH(3, 5_000, Rank::getNormalMessage),
+    FOURTH(4, 50_000, Rank::getNormalMessage),
+    THIRD(5, 1_500_000, Rank::getNormalMessage),
+    SECOND(5, 30_000_000, Rank::getBonusMessage),
+    FIRST(6, 2_000_000_000, Rank::getNormalMessage);
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final Function<Rank, String> messageFunction;
+
+    Rank(int matchCount, int prizeMoney, Function<Rank, String> messageFunction) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.messageFunction = messageFunction;
+    }
+
+    public String getMessage() {
+        return this.messageFunction.apply(this);
+    }
+
+    private String getFormattedPrizeMoney() {
+        return String.format("%,d", this.prizeMoney);
+    }
+
+    private String getNormalMessage() {
+        return this.matchCount + "개 일치 (" + getFormattedPrizeMoney() + ")";
+    }
+
+    private String getBonusMessage() {
+        return this.matchCount + "개 일치, 보너스 볼 일치 (" + getFormattedPrizeMoney() + ")";
+    }
+}
