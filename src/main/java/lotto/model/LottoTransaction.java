24be11b2814f8d32782f2564bdb72c22d9db6927
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-02 14:51:52 +0900

    feat(LottoTransaction): 필드값 set & get 기능 추가

--- src/main/java/lotto/model/LottoTransaction.java
+++ src/main/java/lotto/model/LottoTransaction.java
@@ -7,4 +7,21 @@ public class LottoTransaction {
   private int amount = 0;
   private List<Lotto> purchasedLottos = new ArrayList<>();
 
+  public void setPurchasedLottos(List<Lotto> lottos) {
+    this.purchasedLottos = lottos;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public int getAmount() {
+
+    return this.amount;
+  }
+
+  public List<Lotto> getPurchasedLottos() {
+
+    return this.purchasedLottos;
+  }
 }
