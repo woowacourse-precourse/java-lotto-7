cb5ced606211292ea20f1227f4e2052e219d7ddf
joonwan <joonwan1024@naver.com>
2024-11-02 20:47:40 +0900

    refactor: 랜덤 번호 생성 로직 util 로 분리

--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -3,18 +3,14 @@ package lotto.model;
 import static lotto.message.ErrorMessage.INVALID_UNIT_ERROR_MESSAGE;
 import static lotto.message.ErrorMessage.UPPER_LIMIT_EXCEEDED_ERROR_MESSAGE;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.util.random.RandomNumberUtils;
 
 public class LottoStore {
 
     private static final int UNIT = 1000;
     private static final int MAX_PURCHASE_AMOUNT = 100_000;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_MAX_COUNT = 6;
-
 
     public static LottoTicket purchaseLottoTicket(int purchaseAmount) {
 
@@ -23,12 +19,7 @@ public class LottoStore {
 
         int lottoCount = getLottoCount(purchaseAmount);
         return createLottoTicket(lottoCount);
-    }
 
-    private static void validateThousandWonUnit(int purchaseAmount) {
-        if (purchaseAmount % UNIT != 0) {
-            throw new IllegalArgumentException(INVALID_UNIT_ERROR_MESSAGE.getContent());
-        }
     }
 
     private static void validateMaxPurchaseAmount(int purchaseAmount) {
@@ -37,15 +28,21 @@ public class LottoStore {
         }
     }
 
+    private static void validateThousandWonUnit(int purchaseAmount) {
+        if (purchaseAmount % UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_UNIT_ERROR_MESSAGE.getContent());
+        }
+    }
+
     private static int getLottoCount(int purchaseAmount) {
         return purchaseAmount / UNIT;
     }
 
     private static LottoTicket createLottoTicket(int lottoCount) {
-       List<Lotto> lottos = new ArrayList<>();
+        List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < lottoCount; i++) {
-            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_MAX_COUNT);
+            List<Integer> randomNumbers = RandomNumberUtils.getLottoNumbers();
             Lotto lotto = new Lotto(randomNumbers);
             lottos.add(lotto);
         }
