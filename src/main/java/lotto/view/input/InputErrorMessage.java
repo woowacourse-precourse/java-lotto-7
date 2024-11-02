f1785e9150fd003c8520aacb1b6affca1645d71e
dltmddus <tmddus4671@gmail.com>
2024-11-02 18:25:42 +0900

    fix: message 필드 직접 참조하도록 수정

--- src/main/java/lotto/view/input/InputErrorMessage.java
+++ src/main/java/lotto/view/input/InputErrorMessage.java
@@ -2,15 +2,15 @@ package lotto.view.input;
 
 public enum InputErrorMessage {
     ERROR("[ERROR] "),
-    CANNOT_BE_NULL_OR_EMPTY(ERROR + "값은 비워둘 수 없습니다."),
-    INTEGER_REQUIRED(ERROR + "숫자만 입력 가능합니다."),
-    POSITIVE_NUMBER_REQUIRED(ERROR + "양수만 입력 가능합니다"),
-    LOTTO_PURCHASE_MUST_BE_POSITIVE_INTEGER(ERROR + "로또 구입 금액은 양수로 입력해주세요."),
-    LOTTO_PURCHASE_MUST_BE_MULTIPLE_OF_THOUSAND(ERROR + "로또 구입 금액은 1,000원 단위여야 합니다."),
-    LOTTO_NUMBER_SEPARATOR_MUST_BE_COMMA(ERROR + "쉼표로 구분하여 유효한 로또 번호를 입력해주세요."),
-    LOTTO_NUMBER_LENGTH_INVALID(ERROR + "로또 번호는 6개여야 합니다."),
-    LOTTO_NUMBER_RANGE_INVALID(ERROR + "로또 번호는 1-45 사이의 숫자로 입력해주세요."),
-    LOTTO_NUMBER_CANNOT_DUPLICATE(ERROR + "중복된 로또 번호가 있습니다.");
+    CANNOT_BE_NULL_OR_EMPTY(ERROR.message + "값은 비워둘 수 없습니다."),
+    INTEGER_REQUIRED(ERROR.message + "숫자만 입력 가능합니다."),
+    POSITIVE_NUMBER_REQUIRED(ERROR.message + "양수만 입력 가능합니다"),
+    LOTTO_PURCHASE_MUST_BE_POSITIVE_INTEGER(ERROR.message + "로또 구입 금액은 양수로 입력해주세요."),
+    LOTTO_PURCHASE_MUST_BE_MULTIPLE_OF_THOUSAND(ERROR.message + "로또 구입 금액은 1,000원 단위여야 합니다."),
+    LOTTO_NUMBER_SEPARATOR_MUST_BE_COMMA(ERROR.message + "쉼표로 구분하여 유효한 로또 번호를 입력해주세요."),
+    LOTTO_NUMBER_LENGTH_INVALID(ERROR.message + "로또 번호는 6개여야 합니다."),
+    LOTTO_NUMBER_RANGE_INVALID(ERROR.message + "로또 번호는 1-45 사이의 숫자로 입력해주세요."),
+    LOTTO_NUMBER_CANNOT_DUPLICATE(ERROR.message + "중복된 로또 번호가 있습니다.");
 
     final String message;
 
--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -1,15 +1,26 @@
 package lotto.view.output;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoShop;
+
 public class OutputView {
     public static void printPurchasePrice() {
-        System.out.println(OutputMessage.INPUT_PURCHASE_PRICE);
+        System.out.println(OutputMessage.INPUT_PURCHASE_PRICE.message);
+    }
+
+    public static void printPurchasedLottoery(LottoShop lottoShop) {
+        List<Lotto> lotteries = lottoShop.getLottoTickets().getLotteries();
+
+        lotteries.stream().map(Lotto::getNumbers)
+                .forEach(System.out::println);
     }
 
     public static void printWinningNumbers() {
-        System.out.println(OutputMessage.INPUT_WINNING_NUMBERS);
+        System.out.println(OutputMessage.INPUT_WINNING_NUMBERS.message);
     }
 
     public static void printBonusNumber() {
-        System.out.println(OutputMessage.INPUT_BONUS_NUMBER);
+        System.out.println(OutputMessage.INPUT_BONUS_NUMBER.message);
     }
 }
