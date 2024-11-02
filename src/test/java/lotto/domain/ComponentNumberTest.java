a2668f6b7b5c3ce4ac67df6209d559381d9239b2
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-02 21:18:49 +0900

    feat(ComponentNumberTest): 테스트 코드 작성 완료

---
+++ src/test/java/lotto/domain/ComponentNumberTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import lotto.constants.string.RangeError;
+import lotto.constants.value.LottoRule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+class ComponentNumberTest {
+
+    @Test
+    @DisplayName("ComponentNumber는 지정된 범위를 벗어나면 에러가 난다")
+    void testNumberRangerValidation() {
+
+        assertThatThrownBy(() -> {
+                    new ComponentNumber(LottoRule.MAXIMUM_NUMBER_RANGE.getInstance() + 1);
+                }
+        ).hasMessageContaining(RangeError.NUMBER.getInstance());
+
+        assertThatThrownBy(() -> {
+                    new ComponentNumber(LottoRule.MINIMUM_NUMBER_RANGE.getInstance() - 1);
+                }
+        ).hasMessageContaining(RangeError.NUMBER.getInstance());
+
+    }
+
+    @Test
+    @DisplayName("ComponentNumber는 내부의 값에따라 비교가 가능하다")
+    void testEqualsMethod() {
+        ComponentNumber num1 = new ComponentNumber(2);
+        ComponentNumber num2 = new ComponentNumber(2);
+
+        assertThat(num1).isEqualTo(num2);
+    }
+}
