78a467ecee685ac3d4ba36297991143e35972c61
kaswhy <sxshx818@naver.com>
2024-11-02 23:50:39 +0900

    test(InputAmountTest): 구입 금액에 문자가 포함되면 예외 발생하는지에 대한 테스트 구현
    변경 후
    - 숫자 이외의 문자를 찾으면 예외가 발생하도록 구현

--- src/test/java/lotto/model/InputAmountTest.java
+++ src/test/java/lotto/model/InputAmountTest.java
@@ -26,5 +26,17 @@ public class InputAmountTest {
                 .hasMessageContaining(ErrorMessage.INPUT_CAN_NOT_BE_BLANK.get());
     }
 
+    @DisplayName("숫자 이외의 문자를 포함하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "1,000", "abc", "한글"})
+    public void ThrowExceptionIfInputAmountHasCharacter(String inputAmount) {
+        Throwable thrown = catchThrowable(() -> {
+            this.inputAmount = new InputAmount(inputAmount);
+        });
+
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.AMOUNT_CAN_NOT_HAVE_CHARACTER.get());
+    }
+
 
 }
