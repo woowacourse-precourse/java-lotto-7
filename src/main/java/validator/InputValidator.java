574006eda333909e5450cc68e25ce0df3e48fd37
dawit0905 <dawit0905@gmail.com>
2024-11-02 23:39:14 +0900

    feat: InputValidator 상수값 enum으로 처리

--- src/main/java/validator/InputValidator.java
+++ src/main/java/validator/InputValidator.java
@@ -1,11 +1,12 @@
 package validator;
 
 import common.ErrorMessage;
+import common.LottoConstants;
 
 public class InputValidator {
 
     public static void isDivisibleByThousand(int lottoPurchaseAmount) {
-        if (lottoPurchaseAmount % 1000 != 0) {
+        if (lottoPurchaseAmount % LottoConstants.TICKET_PRICE.getValue() != 0) {
             throw new IllegalArgumentException(ErrorMessage.DIVISIBILITY_BY_THOUSAND_ERROR.getMessage());
         }
     }
