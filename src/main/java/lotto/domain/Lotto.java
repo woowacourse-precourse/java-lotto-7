90201a01a7e1bec8558b3999805113ee20c5c867
OSEUK <tpdnrzmzm3@naver.com>
2024-11-02 16:23:10 +0900

    refactor(controller) : Lotto 리팩토링
    로또의 크기인 6 상수화
    toString을 통해 출력을 쉽게 함.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ")) + "]";
+    }
+
+}
