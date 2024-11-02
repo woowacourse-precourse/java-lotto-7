34d96af2f20bd6d878fe5485d881f1d297d02980
yngbao97 <yukyj0418@gmail.com>
2024-11-02 23:47:20 +0900

    feat: 구입금액을 입력받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("구입금액을 입력해 주세요.");
 
+        String account = Console.readLine();
 
     }
 }
