e1e75f4cac10cb60749923b00c2702e695def989
Park, minsu <cmwmss59@gmail.com>
2024-11-02 22:19:58 +0900

    feat : 구입 금액을 입력받는 기능 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,4 +1,45 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoGame {
+    private static final int LOTTO_PRICE = 1000;
+    private LottoTickets purchasedTickets;
+    private WinningLotto winningLotto;
+
+    public void start() {
+        try {
+            int purchaseAmount = inputPurchaseAmount();
+            generateLottoTickets(purchaseAmount);
+            inputWinningNumbers();
+            displayResult(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            start();
+        }
+
+    }
+
+    private int inputPurchaseAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validateNumericInput(input);
+        int amount = Integer.parseInt(input);
+        validatePurChaseAmount(amount);
+        return amount;
+    }
+
+    private void validatePurChaseAmount(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다");
+        }
+    }
+
+    private void validateNumericInput(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다");
+        }
+    }
+
+
 }
