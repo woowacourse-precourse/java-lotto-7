a0dd7d72a58b0211a64eb17c9db90df1fe21a75e
kellyihyeon <kellykang13@gmail.com>
2024-11-03 00:36:01 +0900

    feat: 로또 번호의 범위를 검증하는 기능

--- src/main/java/lotto/LottoNumberValidator.java
+++ src/main/java/lotto/LottoNumberValidator.java
@@ -6,12 +6,24 @@ import java.util.Set;
 
 public class LottoNumberValidator {
 
+    private static final int MINIMUM = 1;
+    private static final int MAXIMUM = 45;
+
     public static void validateDuplicateNumber(List<Integer> numbers) {
         Set<Integer> checker = new HashSet<>();
         numbers
                 .forEach(number -> {
                     if (!checker.add(number)) {
                         throw new IllegalArgumentException("[ERROR] 중복된 번호가 있어요.");
+                    }
+                });
+    }
+
+    public static void validateRange(List<Integer> numbers) {
+        numbers
+                .forEach(number -> {
+                    if (number < MINIMUM || number > MAXIMUM) {
+                        throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45까지의 숫자로만 이루어져야해요.");
                     }
                 });
     }
--- src/test/java/lotto/LottoNumberValidatorTest.java
+++ src/test/java/lotto/LottoNumberValidatorTest.java
@@ -3,9 +3,28 @@ package lotto;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoNumberValidatorTest {
+
+    private static Stream<Arguments> generateInvalidateLotto() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 45, 46)),
+                Arguments.of(List.of(0, 2, 3, 4, 5, 6)),
+                Arguments.of(List.of(-1, 2, 3, 4, 5, 6))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "generateInvalidateLotto")
+    void 로또_번호가_1이상_45이하가_아니면_IllegalArgumentException_예외가_발생한다(List<Integer> numbers) {
+        assertThrows(IllegalArgumentException.class,
+                () -> LottoNumberValidator.validateRange(numbers));
+    }
 
     @Test
     void 중복된_숫자가_있으면_IllegalArgumentException_예외가_발생한다() {
