005ef2d6799f5c14a0c7efa5ac87d07f8ef61a78
jinukee <jinukee333@gmail.com>
2024-11-02 12:53:01 +0900

    feat(model.Lotto) : validate() 에 로또 번호의 범위를 확인하는 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -19,6 +19,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         for (int number : numbers) {
+            if(number<0||number>45) throw new IllegalArgumentException("[ERROR] 로또 번호로 1 ~ 45 사이의 정수를 입력하세요.");
             if (!uniqueNumber.add(number)) throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
