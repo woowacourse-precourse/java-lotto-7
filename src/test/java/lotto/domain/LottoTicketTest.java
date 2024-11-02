698a6b2b5eee90277411914e3487128f19714a8e
sinminseok12 <sinminseok12@naver.com>
2024-11-02 19:07:53 +0900

    refactor(LottoTicketTest) : 수익률 계산 기능 테스트 변경

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,25 +1,25 @@
 package lotto.domain;
 
 import lotto.constants.Ranking;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 
-import static lotto.constants.LottoValue.LOTTO_PRICE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class LottoTicketTest {
 
     @Test
     void 로또_티켓에_있는_모든_로또의_당첨_결과를_조회한다() {
         LottoTicket lottoTicket = new LottoTicket(providerLottos());
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningLotto winningLotto = WinningLotto.of(Lotto.from(List.of(1,2,3,4,5,6)), 7);
 
         EnumMap<Ranking, Integer> rankings = lottoTicket.checkRanking(winningLotto);
 
-        Assertions.assertThat(rankings)
+        assertThat(rankings)
                 .containsEntry(Ranking.FIRST, 1)
                 .containsEntry(Ranking.SECOND, 1)
                 .containsEntry(Ranking.FOURTH, 1);
@@ -27,13 +27,12 @@ public class LottoTicketTest {
 
     @Test
     void 로또_티켓의_수익률을_계산한다() {
-        LottoTicket lottoTicket = new LottoTicket(providerLottos());
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 2, 3, 4, 5, 6), 7);
+        LottoTicket lottoTicket = new LottoTicket(List.of(Lotto.from(List.of(1,2,3,43,44,45))));
+        WinningLotto winningLotto = WinningLotto.of(Lotto.from(List.of(1,2,3,4,5,6)), 7);
 
         double profitRate = lottoTicket.calculateProfitRate(winningLotto);
 
-        double expectedProfitRate = calculateExpectedProfitRate();
-        Assertions.assertThat(profitRate).isEqualTo(expectedProfitRate);
+        assertThat(profitRate).isEqualTo(500.0);
     }
 
     private List<Lotto> providerLottos() {
@@ -42,12 +41,6 @@ public class LottoTicketTest {
             lottos.add(Lotto.from(List.of(i + 1, i + 2, i + 3, i + 4, i + 5, i + 6)));
         }
         return lottos;
-    }
-
-    private double calculateExpectedProfitRate() {
-        long totalReward = Ranking.FIRST.getReward() + Ranking.SECOND.getReward() + Ranking.FOURTH.getReward();
-        int totalCost = 3 * LOTTO_PRICE.getValue();
-        return (double) totalReward / totalCost;
     }
 
 }
