f28c8d0641323664c2966a9ab5f652088ff7db44
soobin <a01056239778@gmail.com>
2024-11-02 22:14:23 +0900

    feat: 로또 구입 금액 검증 로직 추가
    - 로또 구입 금액이 로또 개별 가격 미만인 경우 예외 처리
    - 로또 구입 금액이 로또 개별 가격 단위가 아닌 경우 예외 처리

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -12,14 +12,24 @@ public class LottoStore {
     }
 
     public int calculateLottoCount(int money) {
+        validateMoney(money);
         return money / LOTTO_PRICE;
     }
 
     public List<Lotto> sell(int lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
-        for(int i = 0; i < lottoCount; i++) {
+        for (int i = 0; i < lottoCount; i++) {
             lottos.add(lottoGenerator.generate());
         }
         return lottos;
+    }
+
+    private void validateMoney(int money) {
+        if (money < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 " + LOTTO_PRICE + "원 이상이어야 합니다.");
+        }
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.");
+        }
     }
 }
