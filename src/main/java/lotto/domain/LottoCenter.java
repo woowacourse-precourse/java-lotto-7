9b579a148fb510981fffdd4ce545d9372ca8db87
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-11-03 00:01:41 +0900

    Feat : Add LottoCenter function
    로또 추첨국 기능 추가
    추첨국에서 하나의 기준 로또를 생성한다

---
+++ src/main/java/lotto/domain/LottoCenter.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import lotto.domain.lottoGeneratir.LottoGenerator;
+import lotto.dto.LottoDto;
+import lotto.dto.WinningLotto;
+
+public class LottoCenter {
+    private WinningLotto winningLotto;
+    private LottoGenerator lottoGenerator;
+    public LottoCenter(LottoGenerator lottoGenerator){
+        this.lottoGenerator = lottoGenerator;
+    }
+    public void drawWinningNumbers(LottoDto lottoDto){
+        winningLotto = lottoGenerator.generateWinningLotto(lottoDto.getNumbers(), lottoDto.getBonus());
+    }
+}
