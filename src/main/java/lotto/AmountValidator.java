581e9bb93d8ab1c8c9f45f0c0e065929fecb73b2
Hujin0322 <hongyuj0322@naver.com>
2024-11-03 03:28:30 +0900

    refactor(AmountValidator): 쉼표 검증 로직 추가
    - validateInput()에 쉼표만 입력한 경우 추가
    - validateInput()에 쉼표가 천 단위 이외에 사용된 경우 추가
    - parseInputToInt()에서 쉼표를 제거한 cleanedInput 사용
    - validateAmount()에서 1000원인 경우 제외

--- src/main/java/lotto/AmountValidator.java
+++ src/main/java/lotto/AmountValidator.java
@@ -3,19 +3,30 @@ package lotto;
 public class AmountValidator {
     private static final String EMPTY_INPUT_ERROR = "[ERROR] 구매 금액을 입력해주세요.";
     private static final String NOT_NUMBER_ERROR = "[ERROR] 금액은 숫자로만 입력해주세요.";
+    private static final String COMMA_FORMAT_ERROR = "[ERROR] 천 단위에만 쉼표를 사용해야 합니다.";
     private static final String MINIMUM_AMOUNT_ERROR = "[ERROR] 로또 구입 최소 금액은 1,000원입니다.";
     private static final String AMOUNT_UNIT_ERROR = "[ERROR] 1,000원 단위로 입력해주세요.";
     private static final String NEGATIVE_AMOUNT_ERROR = "[ERROR] 금액은 0 이상의 숫자여야 합니다.";
+
 
     public void validateInput(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
         }
+
+        if (input.replace(",", "").isEmpty()) {
+            throw new IllegalArgumentException(NOT_NUMBER_ERROR);
+        }
+
+        if (!(input.matches("\\d{1,3}(,\\d{3})*") || input.matches("\\d+"))) {
+            throw new IllegalArgumentException(COMMA_FORMAT_ERROR);
+        }
     }
 
     public int parseInputToInt(String input) {
+        String cleanedInput = input.replaceAll(",", "");
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(cleanedInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_NUMBER_ERROR);
         }
@@ -25,7 +36,7 @@ public class AmountValidator {
         if (lottoAmount < 0) {
             throw new IllegalArgumentException(NEGATIVE_AMOUNT_ERROR);
         }
-        if (lottoAmount <= 1000) {
+        if (lottoAmount < 1000) {
             throw new IllegalArgumentException(MINIMUM_AMOUNT_ERROR);
         }
         if (lottoAmount % 1000 != 0) {
