fe4c9308bae2f31885ab2027d0d9fb1cf8ca8444
TalkingPotato90 <star901210@hanmail.net>
2024-11-02 18:19:20 +0900

    feat: 구입 금액 입력 기능 구현

--- src/main/java/lotto/temp/LottoTemp.java
+++ src/main/java/lotto/temp/LottoTemp.java
@@ -1,5 +1,20 @@
 package lotto.temp;
 
+import lotto.util.CommonIo;
+
 public class LottoTemp {
+    private final CommonIo io;
+
+    public LottoTemp(CommonIo io) {
+        this.io = io;
+    }
+
+    public void printRequestPurchase(){
+        io.printMessage("구입금액을 입력해 주세요.");
+    }
+
+    public String inputPurchaseAmount(){
+        return io.receiveInput();
+    }
 
 }
---
+++ src/test/java/lotto/temp/LottoTempTest.java
@@ -0,0 +1,7 @@
+package lotto.temp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTempTest {
+
+}
