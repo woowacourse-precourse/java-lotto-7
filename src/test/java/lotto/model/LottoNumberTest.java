0beda99194bac8b06d6054f7a2f841262a101ea2
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 02:07:54 +0900

    test(lotto/validate): 로또 번호 범위 예외 기능 테스트 추가

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -13,4 +13,14 @@ class LottoNumberTest {
         assertThatThrownBy(() -> LottoNumber.from("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("로또 번호가 1에서 45사이의 숫자가 아니라면 예외가 발생한다.")
+    void validateRange() {
+        assertThatThrownBy(() -> LottoNumber.from("46"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> LottoNumber.from("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
