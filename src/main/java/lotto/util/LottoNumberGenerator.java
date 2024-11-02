e3e22a3522c10d70ce3a6b2a16d15e01c6a571f4
tentenball <lth8905@gmail.com>
2024-11-02 18:21:57 +0900

    feat: 로또번호 생성 구현
    * LottoNumberGenerator 통해 로또번호를 생성합니다.

---
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domains.lotto.constant.LottoNumberConstant;
+
+public class LottoNumberGenerator {
+
+	public static List<Integer> generateLottoNumbers() {
+		return Randoms.pickUniqueNumbersInRange(LottoNumberConstant.LOTTO_MINIMUM_NUMBER,
+			LottoNumberConstant.LOTTO_MAXIMUM_NUMBER, LottoNumberConstant.LOTTO_NUMBER_SIZE);
+	}
+}
