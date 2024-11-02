cfde227ba3a5dd270c28d5c6bee5fd9f5bb69d47
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-02 20:58:29 +0900

    test(StringTest): String 클래스의 charAt() 메서드의 예외처리 테스트
    - 인덱스 예외 테스트 추가
    - assertThatThrownBy보다 의도가 명확한 assertThatExceptionOfType 사용

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -47,5 +48,25 @@ public class StringTest {
         String str = "abc";
         char ch = str.charAt(str.length() - 1);
         assertThat(ch).isEqualTo('c');
+    }
+
+//    @Test
+//    @DisplayName("String의 잘못된 인덱스에 접근하면 StringIndexOutOfBoundsException 예외가 발생한다.")
+//    void charAt_인덱스_예외_테스트() {
+//        assertThatThrownBy(() -> {
+//            String str = "abc";
+//            char ch = str.charAt(str.length());
+//        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+//    }
+
+    // assertThatExceptionOfType는 특정 예외가 발생하는지 확인하는지 판단하고자 하는 의도를 더 명확히 드러낸다.
+    @DisplayName("String의 잘못된 인덱스에 접근하면 StringIndexOutOfBoundsException 예외가 발생한다.")
+    void charAt_인덱스_예외_테스트() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                            String str = "abc";
+                            char ch = str.charAt(str.length());
+                        }
+                );
     }
 }
