b8c575541ebeeb8258ba45a6a29430459d1cebbb
Park, Sojeong <sojeong2101@gmail.com>
2024-11-02 19:22:18 +0900

    feat: 구입 금액 추출 기능 구현
    1. null, 빈 문자열, 공백 예외 처리
    2. 숫자 이외의 문자 입력된 경우 예외 처리
    3. 나머지가 있는 경우 예외 처리
    4. 0일 경우 예외 처리
    5. 검증 후 문자열에서 숫자 추출

---
+++ src/main/java/lotto/purchase/PaymentExtractor.java
@@ -0,0 +1,42 @@
+package lotto.purchase;
+
+import lotto.util.Extractor;
+
+public class PaymentExtractor implements Extractor<Integer> {
+
+    private static final String NUMBER_REGULAR_EXPRESSION = "\\d+";
+    private static final String PAYMENT_RULE_REGULAR_EXPRESSION = "000$";
+
+    @Override
+    public Integer extract(String input) {
+        validateInput(input);
+        return Integer.parseInt(input);
+    }
+
+    @Override
+    public void validateInput(String input) {
+        checkNull(input);
+        checkBlank(input);
+        checkNotNumber(input);
+        checkRemainder(input);
+        checkZero(input);
+    }
+
+    private void checkNotNumber(String input) {
+        if (!input.matches(NUMBER_REGULAR_EXPRESSION)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 가능합니다.");
+        }
+    }
+
+    private void checkRemainder(String input) {
+        if (!input.matches(PAYMENT_RULE_REGULAR_EXPRESSION)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    private void checkZero(String input) {
+        if (Integer.parseInt(input) == 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 0원이 될 수 없습니다.");
+        }
+    }
+}
