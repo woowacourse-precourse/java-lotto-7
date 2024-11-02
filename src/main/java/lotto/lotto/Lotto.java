d01cd6a6e9c7474f8f9ea64b9c0e9b5a64df9b35
leeedohyun <ldh19738@gmail.com>
2024-11-02 13:51:35 +0900

    feat: 로또 번호에 중복된 숫자가 있는지 검증

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -1,22 +1,37 @@
 package lotto.lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int REQUIRED_LOTTO_NUMBER_COUNT = 6;
+
     private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumberCount(numbers);
+        validateLottoNumberDuplication(numbers);
         this.numbers = numbers.stream()
                 .map(LottoNumber::new)
                 .toList();
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateLottoNumberCount(List<Integer> numbers) {
+        if (numbers.size() != REQUIRED_LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + REQUIRED_LOTTO_NUMBER_COUNT + "개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateLottoNumberDuplication(List<Integer> numbers) {
+        if (hasDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private boolean hasDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
+    }
 }
