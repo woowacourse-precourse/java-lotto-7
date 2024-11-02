44cca7c4045168b2e9766988602682097e11db5e
soeun2537 <soeun2537@gmail.com>
2024-11-02 14:46:27 +0900

    test: getNumbers 호출 시 WinningLotto의 번호 리스트를 반환하는지 확인하는 단위 테스트 추가

--- src/test/java/lotto/model/domain/WinningLottoTest.java
+++ src/test/java/lotto/model/domain/WinningLottoTest.java
@@ -24,4 +24,20 @@ class WinningLottoTest {
         assertThat(winningLotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
         assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
     }
+
+    @Test
+    @DisplayName("getNumbers 호출 시 WinningLotto의 번호 리스트를 반환하는지 확인")
+    void getNumbers() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Integer bonusNumber = 7;
+        WinningLotto winningLotto = WinningLotto.of(numbers, bonusNumber);
+
+        // when
+        List<Integer> retrievedNumbers = winningLotto.getNumbers();
+
+        // then
+        assertThat(retrievedNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+
+    }
 }
