b10fc98dc61ebc3674bc2170bdaa966c1850f5c3
giwoong <dev.choi0409@gmail.com>
2024-11-02 22:13:55 +0900

    refactor: PurchaseAmountValidator 수정
    중복된 예외처리가 있어 제거합니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -6,7 +6,6 @@ public class ErrorMessage {
     public static final String ERROR_NON_NUMERIC_INPUT = "[ERROR] 숫자를 입력해주세요.";
     public static final String ERROR_NEGATIVE_PRICE = "[ERROR] 음수는 입력할 수 없습니다.";
     public static final String ERROR_INVALID_UNIT = "[ERROR] 1000원 단위로 입력해주세요.";
-    public static final String ERROR_BELOW_MINIMUM = "[ERROR] 1000원 이상 입력해주세요.";
 
     public static final String ERROR_NOT_SEPARATED = "[ERROR] 입력값에 쉼표(,) 구분자가 포함되어 있지 않습니다.";
     public static final String ERROR_EMPTY_WINNING_NUMBERS = "[ERROR] 당첨 번호를 입력하셔야 합니다.";
@@ -18,5 +17,5 @@ public class ErrorMessage {
     public static final String ERROR_EMPTY_BONUS_NUMBER = "[ERROR] 보너스 번호를 입력해주세요.";
     public static final String ERROR_BONUS_NUMBER_RANGE = "[ERROR] 보너스 번호는 1부터 45까지의 숫자만 가능합니다.";
     public static final String ERROR_DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
-    
+
 }
--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -2,7 +2,6 @@ package lotto.validator;
 
 import static lotto.message.CommonConstants.MINIMUM_AMOUNT;
 import static lotto.message.CommonConstants.ZERO;
-import static lotto.message.ErrorMessage.ERROR_BELOW_MINIMUM;
 import static lotto.message.ErrorMessage.ERROR_INPUT_PURCHASE_AMOUNT;
 import static lotto.message.ErrorMessage.ERROR_INVALID_UNIT;
 import static lotto.message.ErrorMessage.ERROR_NEGATIVE_PRICE;
@@ -18,7 +17,6 @@ public class PurchaseAmountValidator {
     public static void validatePurchaseAmount(int purchaseAmount) {
         validateNegativeAmount(purchaseAmount);
         validateInvalidUnit(purchaseAmount);
-        validateBelowMinimumAmount(purchaseAmount);
     }
 
     private static void validateBlankInput(String input) {
@@ -36,12 +34,6 @@ public class PurchaseAmountValidator {
     private static void validateInvalidUnit(int amount) {
         if (amount % MINIMUM_AMOUNT != ZERO) {
             throw new IllegalArgumentException(ERROR_INVALID_UNIT);
-        }
-    }
-
-    private static void validateBelowMinimumAmount(int amount) {
-        if (amount < MINIMUM_AMOUNT) {
-            throw new IllegalArgumentException(ERROR_BELOW_MINIMUM);
         }
     }
 
