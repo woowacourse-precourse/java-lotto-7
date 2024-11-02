0ac328b3b9122051dc095a5f8c96b22193bf8b02
lsj0822 <lsj0822@khu.ac.kr>
2024-11-02 17:00:04 +0900

    refactor(Validator) : Validator 수정
    Validator의 각 메서드가 모듈의 기능을 더 담당하도록 수정하였습니다
    - 기존의 로직은 하나의 객체에 여러 검증 메서드가 호출되는 방식이었습니다.
    - 이를 대분류와 소분류로 나누어서 validator 바깥에 호출할 메서드와 내부에서 호출할 메서드로 분리하였습니다.
    - 후에 검증 메서드가 너무 많아진다면 별도의 클래스로 분리할 예정입니다.

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -1,11 +1,28 @@
 package validator;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import utils.Pair;
 
 public class Validator {
-    public final Pair<Integer, Integer> LOTTO_NUMBER_SCOPE = new Pair<>(1, 45);
+    public static final Integer LOTTO_MIN = 1;
+    public static final Integer LOTTO_MAX = 45;
+    public static final Integer LOTTO_COST = 1000;
+    public static final Pair<Integer, Integer> LOTTO_NUMBER_SCOPE = new Pair<>(LOTTO_MIN, LOTTO_MAX);
 
-    public static void validateLong(String inputContent) {
+    public static void validateMoney(String money) {
+        validateNumber(money);
+        validatePositiveNumber(money);
+        validatePurchaseable(Long.parseLong(money), LOTTO_COST);
+    }
+
+    public static void validateLottoNumbers(List<Integer> elements) {
+        validateUniqueElements(elements);
+        validateLottoNumbersInRange(elements);
+    }
+
+    public static void validateNumber(String inputContent) {
         try {
             Long.parseLong(inputContent);
         } catch (IllegalArgumentException e) {
@@ -13,10 +30,26 @@ public class Validator {
         }
     }
 
+    public static void validatePurchaseable(long criteriaMoney, int cost) {
+        if (!isPurchaseAble(criteriaMoney, cost)) {
+            throw new IllegalArgumentException("[ERROR] 잔액이 남습니다.");
+        }
+    }
+
+    private static boolean isPurchaseAble(long criteriaMoney, int cost) {
+        return criteriaMoney % cost == 0;
+    }
+
     public static void validatePositiveNumber(String inputContent) {
         long convertedContent = Long.parseLong(inputContent);
         if (convertedContent <= 0) {
             throw new IllegalArgumentException("[ERROR] 입력한 값이 0 이하의 음수입니다.");
+        }
+    }
+
+    public static void validateLottoNumbersInRange(List<Integer> groups) {
+        for (Integer element : groups) {
+            validateNumberInRange(element, LOTTO_NUMBER_SCOPE);
         }
     }
 
@@ -36,13 +69,11 @@ public class Validator {
         return true;
     }
 
-    public static void validatePurchasing(long criteriaMoney, int cost) {
-        if (!isPurchaseAble(criteriaMoney, cost)) {
-            throw new IllegalArgumentException("[ERROR] 잔액이 남습니다.");
+    public static void validateUniqueElements(List<?> groups) {
+        Set<Object> uniqueGroup = new HashSet<>(groups);
+        if (uniqueGroup.size() < groups.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 값이 있습니다.");
         }
     }
 
-    private static boolean isPurchaseAble(long criteriaMoney, int cost) {
-        return criteriaMoney % cost == 0;
-    }
 }
