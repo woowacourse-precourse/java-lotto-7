7519d17a4b3b983b2057050516c9d5586ac9f0fb
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-02 16:39:08 +0900

    feat(WinningResult): 로또 결과 도메인 클래스 추가
    로또 순위, 갯수 형태로 로또 결과를 저장하였습니다.

---
+++ src/main/java/lotto/domain/result/WinningResult.java
@@ -0,0 +1,64 @@
+package lotto.domain.result;
+
+import lotto.domain.lotto.LottoRank;
+import lotto.domain.lotto.WinningLotto;
+import lotto.domain.lotto.WinningLottos;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningResult {
+
+    private static final int INCREASE_COUNT = 1;
+
+    private final Map<LottoRank, Integer> lottoResult;
+    private final Revenue revenue;
+
+    private WinningResult(final WinningLottos winningLottos, final int purchaseCount) {
+        this.lottoResult = calculateRankCount(winningLottos.getWinningLottos());
+        this.revenue = createRevenue(purchaseCount);
+    }
+
+    public static WinningResult of(final WinningLottos winningLottos, final int purchaseCount) {
+        return new WinningResult(winningLottos, purchaseCount);
+    }
+
+    private Map<LottoRank, Integer> calculateRankCount(final List<WinningLotto> winningLottos) {
+        Map<LottoRank, Integer> result = initializeRankCount();
+        for (WinningLotto winningLotto : winningLottos) {
+            LottoRank rank = winningLotto.getRank();
+            result.put(rank, result.get(rank) + INCREASE_COUNT);
+        }
+        return result;
+    }
+
+    private Map<LottoRank, Integer> initializeRankCount() {
+        Map<LottoRank, Integer> result = new EnumMap<>(LottoRank.class);
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank != LottoRank.NONE) {
+                result.put(rank, 0);
+            }
+        }
+        return result;
+    }
+
+    private Revenue createRevenue(final int purchaseCount) {
+        return Revenue.of(calculateTotalPrizeAmount(), purchaseCount);
+    }
+
+    private int calculateTotalPrizeAmount() {
+        return lottoResult.entrySet()
+                .stream()
+                .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
+    }
+
+    public Map<LottoRank, Integer> getLottoResult() {
+        return Map.copyOf(lottoResult);
+    }
+
+    public Revenue getRevenue() {
+        return revenue;
+    }
+}
