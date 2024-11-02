d9a540e7068d5c5fee1d16aa8782d4cee1c27576
wogus <ind07152@naver.com>
2024-11-02 17:26:01 +0900

    feat(OutputView): 로또 구매 결과 출력 기능 추가
    구매한 로또들의 결과를 오름차순으로 출력한다.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+
+public final class OutputView {
+
+    public static void printLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.print("[");
+            String result = lotto.getNumbers().stream().map(String::valueOf).collect(Collectors.joining(","));
+            System.out.print(result);
+            System.out.println("]");
+        }
+        System.out.println();
+    }
+}
