37b714343e5ed239546b97f6eb6a207072bc9319
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 22:02:19 +0900

    feat(draw numbers validator): 추첨 번호의 유효성 검사 기능 추가

---
+++ src/main/java/lotto/utils/DrawNumbersValidator.java
@@ -0,0 +1,95 @@
+package lotto.utils;
+
+import lotto.model.lotto.LottoPrintFormat;
+import lotto.model.lotto.LottoNumber;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DrawNumbersValidator {
+
+    public List<Integer> validateWinningNumbers(String winningNumbers) {
+        validateEmpty(winningNumbers);
+        validateWinningNumbersFormat(winningNumbers);
+
+        List<String> splitWinningNumbers = List.of(winningNumbers.split(LottoPrintFormat.DELIMITER));
+        validateCountWinningNumbers(splitWinningNumbers);
+        validateAllNoLetters(splitWinningNumbers);
+        validateDuplicateWiningNumbers(splitWinningNumbers);
+        validateAllRange(splitWinningNumbers);
+        return splitWinningNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private void validateAllRange(List<String> splitWinningNumbers) {
+        for (String number : splitWinningNumbers) {
+            validateRange(number);
+        }
+    }
+
+    private void validateAllNoLetters(List<String> splitWinningNumbers) {
+        for (String number : splitWinningNumbers) {
+            validateNoLetters(number);
+        }
+    }
+
+    public int validateBonusNumber(String bonusNumber) {
+
+        validateEmpty(bonusNumber);
+        validateNoLetters(bonusNumber);
+        int parseBonusNumber = Integer.parseInt(bonusNumber);
+        validateRange(bonusNumber);
+
+        return parseBonusNumber;
+    }
+
+    public void validateAssociateWinningAndBonusNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호와 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+    private void validateRange(String number) {
+        if (Integer.parseInt(number) < LottoNumber.MIN_VALUE || LottoNumber.MAX_VALUE < Integer.parseInt(number)) {
+            throw new IllegalArgumentException("당첨 번호는 1 ~ 45 범위로 입력해주세요.");
+        }
+    }
+
+    private void validateDuplicateWiningNumbers(List<String> splitWinningNumbers) {
+        if (isDuplicateWinningNumbers(splitWinningNumbers)) {
+            throw new IllegalArgumentException("당첨 번호는 중복을 제거하고 입력해주세요");
+        }
+    }
+
+    private void validateNoLetters(String number) {
+        if (!number.matches(LottoNumber.FORMAT)) {
+            throw new IllegalArgumentException("당첨 번호로는 문자를 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateCountWinningNumbers(List<String> splitWinningNumbers) {
+        if (splitWinningNumbers.size() < LottoPrintFormat.NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호 개수가 부족합니다.");
+        }
+
+        if (splitWinningNumbers.size() > LottoPrintFormat.NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호 개수가 초과되었습니다.");
+        }
+    }
+
+    private void validateWinningNumbersFormat(String winningNumbers) {
+        if (winningNumbers.startsWith(LottoPrintFormat.DELIMITER) || winningNumbers.endsWith(LottoPrintFormat.DELIMITER)) {
+            throw new IllegalArgumentException("잘못된 당첨 번호 형식입니다.");
+        }
+    }
+
+    private void validateEmpty(String number) {
+        if (number.isEmpty()) {
+            throw new IllegalArgumentException("번호를 입력해주세요.");
+        }
+    }
+
+    private boolean isDuplicateWinningNumbers(List<String> splitWinningNumbers) {
+        return splitWinningNumbers.stream().distinct().count() != splitWinningNumbers.size();
+    }
+}
