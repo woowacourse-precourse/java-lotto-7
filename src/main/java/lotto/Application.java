cc0994859d780fa705d7550bbfdea48599d68148
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-11-03 05:41:04 +0900

    feat(Application.java) 가격 입력 예외 처리 기능 추가
    가격을 이상하게 입력하거나 0 이하의 정수를 입력했을 때 예외 처리를
    하도록 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,11 +5,38 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("구입 금액을 입력해주세요.");
-        String priceInput=Console.readLine();
-        int price=Integer.parseInt(priceInput);
+        int price=0;
+        while(price<=0) {
+            System.out.println("구입 금액을 입력해주세요.");
+            String priceInput = Console.readLine();
+
+            try {
+                price=getPrice(priceInput);
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+
+        }
 
         //가격 정상 입력 테스트용
         System.out.println("price: "+price);
+
+    }
+
+    public static int getPrice(String priceInput) throws IllegalArgumentException{
+        int price=0;
+        String ERRORMESSAGE="[ERROR] 가격은 양의 정수여야 합니다.";
+
+        try{
+            price=Integer.parseInt(priceInput);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(ERRORMESSAGE);
+        }
+
+        if(price<=0) {
+            throw new IllegalArgumentException(ERRORMESSAGE);
+        }
+
+        return price;
     }
 }
