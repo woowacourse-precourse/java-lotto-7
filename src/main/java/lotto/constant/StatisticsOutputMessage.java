5d6a90cc01927b1730feb95bb9d3c73dd3875546
yunhacandy <yunhacandy@gmail.com>
2024-11-03 01:36:23 +0900

    feat (StatisticsOutputMessage): 통계 관련 출력 메시지 enum으로 분리

---
+++ src/main/java/lotto/constant/StatisticsOutputMessage.java
@@ -0,0 +1,19 @@
+package lotto.constant;
+
+public enum StatisticsOutputMessage {
+    START_COMMENT("\n당첨 통계"),
+    START_LINE("---"),
+    CONTENT_OF_LOTTO_RESULT("%d개 일치 (%s원) - %d개"),
+    CONTENT_OF_RESULT_FOR_BONUS_NUMBER("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    PROFIT_RATE("총 수익률은 %.1f%%입니다.");
+
+    private final String message;
+
+    StatisticsOutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
