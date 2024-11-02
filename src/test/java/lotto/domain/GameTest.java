bd7ff1f5c94988021d2363b23c5c7950bf8ea96b
yushinc <cys74351@gmail.com>
2024-11-02 23:47:55 +0900

    test(GameTest): 당첨 결과 확인 테스트 코드 추가

--- src/test/java/lotto/domain/GameTest.java
+++ src/test/java/lotto/domain/GameTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -43,14 +44,42 @@ public class GameTest {
     }
 
     @Test
-    void 당첨_결과_확인_3개_일치() {
+    void 당첨_결과_확인_() {
+        // given & when
+        List<Lotto> lottos = new ArrayList<>();
 
+        for (int i = 1; i < 6; i++) {
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5+i);
+            lottos.add(new Lotto(numbers));
+        }
+
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Game game = new Game(lottos);
+        game.compareNumbers(winningNumbers);
     }
 
     @Test
-    void 당첨_결과_확인_4개_일치() {
+    void 당첨_결과_확인() {
+        // given
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 7, 8, 9)),  // correct 3
+                new Lotto(List.of(1, 2, 3, 4, 7, 8)),  // correct 4
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)),  // correct 5
+                new Lotto(List.of(1, 2, 3, 4, 5, 10)), // correct 5 + bonus
+                new Lotto(List.of(1, 2, 3, 4, 5, 6))   // correct 6
+        );
 
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 10;
+        Game game = new Game(lottos, winningNumbers, bonusNumber);
+
+        // when
+        int[] results = game.compareNumbers(winningNumbers);
+
+        // then
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, results);
     }
+
 
     @Test
     void 당첨_결과_확인_5개_일치() {
