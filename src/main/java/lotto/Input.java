99200df8731d5b57bff5400483a734f302bfb917
sejinezy <psj8880@naver.com>
2024-11-02 14:17:21 +0900

    feat(Input):  1. [입력] 로또 구입 금액을 입력받는 기능
    - 입력 받은 문자열을 정수로 바꾸는 기능 구현
    - [예외처리] 1,000원으로 나누어 떨어지지 않는 경우
    - [예외처리] 숫자가 아닌 경우
    - [예외처리] 빈 값일 경우

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public void validateNotEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어 있습니다.");
+        }
+    }
+
+    public void validateIsNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
+    }
+
+    public void validateDivisibleByThousand(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    public int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+
+
+
+}
