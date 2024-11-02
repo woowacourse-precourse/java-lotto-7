64e8bd26d1f66160d467092678546910cd134bbe
lsj0822 <lsj0822@khu.ac.kr>
2024-11-03 00:11:16 +0900

    feat(OutputView) : 결과 출력 기능 추가
    로직에 따른 결과문을 출력하기 위한 메서드를 추가하였습니다.
    -현재는 로또를 구매하고 보너스 번호까지 입력하였을 때를 구현했기에 이에 맞는 메소드만 구현하였습니다.
    -후에 LottoResult와 해당 enum 객체의 카운트를 출력하는 메서드와 수익률을 분석하는 메소드를 구현할 예정입니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.List;
+import model.Lotto;
+
 public class OutputView {
 
     public static void printInputMessage(OutputMessage message) {
@@ -8,5 +11,21 @@ public class OutputView {
 
     public static void printErrorMessage(IllegalArgumentException e) {
         System.out.println(e.getMessage());
+    }
+
+    public static void printLottoPurchasing(Integer count, List<Lotto> lottos) {
+        String outputMessage = OutputMessage.INPUT_LOTTO_COUNT_MESSAGE.getMessage();
+        System.out.println(String.format(outputMessage, count));
+        printLottos(lottos);
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void printBlankLine() {
+        System.out.println();
     }
 }
