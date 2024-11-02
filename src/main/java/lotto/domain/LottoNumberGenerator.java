c0266b52bf641a534bd1c831c2d779247fd697d9
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-02 23:50:05 +0900

    refactor : 랜덤 난수생성하는 리스트를 변형가능한 리스트로 수정

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public class LottoNumberGenerator {
     private static final int PICK_COUNT = 6;
 
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT));
         numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
