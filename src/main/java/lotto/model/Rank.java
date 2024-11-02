84b0be065edc5b8ead6be395e6ea2481658e1b1e
MinJun Choi <mj043000@naver.com>
2024-11-02 21:55:00 +0900

    feat: 당첨 수에 맞는 Rank 가져오는 기능 구현

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
     FIRST(6, 2000000000),
     SECOND(5, 30000000),
@@ -13,5 +15,16 @@ public enum Rank {
     Rank(int sameCount, int reward) {
         this.sameCount = sameCount;
         this.reward = reward;
+    }
+
+    public static Rank getRank(int sameCount, boolean isBonusNumberWinning) {
+        Rank rank = Arrays.stream(values())
+                .filter(value -> value.sameCount == sameCount)
+                .findAny()
+                .orElse(null);
+        if (rank == SECOND && !isBonusNumberWinning) {
+            rank = THIRD;
+        }
+        return rank;
     }
 }
