739850fe7784882f5b41d97e14b144a1f7006319
eunji <kuji5256@gmail.com>
2024-11-02 20:16:03 +0900

    refactor: 로또 구입 금액 입력 기능 수정 - ValidationValues 클래스를 생성 - 구입 금액 입력값을 검증하는 함수를 해당 클래스로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,14 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.exception.ValidationValues;
 import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
-        LottoService lottoService = new LottoService();
 
-        LottoController lottoController = new LottoController(lottoService);
+        LottoController lottoController = new LottoController();
         lottoController.startLotto();
+
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,20 +5,6 @@ import lotto.view.InputView;
 
 public class LottoService {
 
-    public int validatePurchaseAmount(String inputPurchaseAmount) {
-        if(inputPurchaseAmount == null || inputPurchaseAmount.isEmpty()) {
-            throw new IllegalArgumentException(UserErrorMessage.ERROR_NOT_ALLOWED_BLANK);
-        }
 
-        try {
-            int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
-            if(purchaseAmount % 1000 != 0) {
-                throw new IllegalArgumentException(UserErrorMessage.ERROR_NOT_ALLOWED_VALUE);
-            }
-            return purchaseAmount;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(UserErrorMessage.ERROR_NON_NUMERIC_VALUE);
-        }
-    }
 
 }
