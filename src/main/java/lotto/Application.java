f9994854c1ea9d01191e7ceeede5f5efdf20a937
yumi <yumkoori@gmail.com>
2024-11-02 18:53:49 +0900

    Refactor: 잘못된 입력값 작성시 다시 입력받을 수 있도록 Apllication 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,18 +7,34 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         LottoMachine lottoMachine = new LottoMachineImpl();
-        
-        System.out.println("구입금액을 입력해 주세요.");
-        String purchaseMoney = Console.readLine();
-        List<Lotto> lottoTickets = lottoMachine.createLottoTickets(purchaseMoney);
+        String purchaseMoney;
+        List<Lotto> lottoTickets;
+        HashMap<LottoRank, Integer> winningResult;
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String winningNumbers = Console.readLine();
-        System.out.println();
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String bonusNumber = Console.readLine();
-        HashMap<LottoRank, Integer> winningResult =
-                lottoMachine.getWinningResult(lottoTickets, winningNumbers, bonusNumber);
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                purchaseMoney = Console.readLine();
+                lottoTickets = lottoMachine.createLottoTickets(purchaseMoney);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e. getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String winningNumbers = Console.readLine();
+                System.out.println();
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String bonusNumber = Console.readLine();
+                winningResult = lottoMachine.getWinningResult(lottoTickets, winningNumbers, bonusNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         Double profitRate = lottoMachine.calculateProfitRate(winningResult, purchaseMoney);
         System.out.println("총 수익률은 "+ profitRate + "%입니다.");
