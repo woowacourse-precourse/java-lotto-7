cd8fa8611b93250ab411f381bb01eabdd4df3483
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-02 17:41:58 +0900

    test: 1 ~ 45 사이의 보너스 번호 입력 및 검증 기능 테스트 작성

--- src/test/java/lotto/view/ParserTest.java
+++ src/test/java/lotto/view/ParserTest.java
@@ -13,6 +13,9 @@ class ParserTest {
             "[ERROR] 당첨 번호는 1에서 45 사이의 숫자로 이루어져 있어야 합니다.";
     private final static String ERROR_DUPLICATE_WINNING_NUMBER_MESSAGE =
             "[ERROR] 당첨 번호는 중복되지 않은 서로 다른 6개의 숫자가 입력 되어야 합니다.";
+    private final static String ERROR_NON_NUMERIC_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 번호는 숫자로 이루어져 있어야 합니다.";
+    private static final String ERROR_OUT_OF_RANGE_BONUS_NUMBER_MESSAGE =
+            "[ERROR] 보너스 번호는 1에서 45 사이의 숫자로 이루어져 있어야 합니다.";
 
     @Test
     public void 구입_금액이_1000으로_나누어_떨어지지_않으면_예외_발생() {
@@ -95,4 +98,25 @@ class ParserTest {
                 .hasMessage(ERROR_DUPLICATE_WINNING_NUMBER_MESSAGE);
     }
 
+    @Test
+    public void 보너스_번호가_숫자가_아니면_예외_발생() {
+        // given
+        String input = "문자";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> Parser.parseBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_NON_NUMERIC_BONUS_NUMBER_MESSAGE);
+    }
+
+    @Test
+    public void 보너스_번호가_1에서_45_사이의_숫자가_아니면_예외_발생() {
+        // given
+        String input = "46";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> Parser.parseBonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_OUT_OF_RANGE_BONUS_NUMBER_MESSAGE);
+    }
 }
