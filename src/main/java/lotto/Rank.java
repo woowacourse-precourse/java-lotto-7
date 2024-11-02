fdca70b9f2a11d59303c46422d8dcd5deea467a0
soobin <a01056239778@gmail.com>
2024-11-03 01:49:16 +0900

    refactor: 당첨 등수 순서 기준 추가
    - 등수별 출력 순서 기준 설정 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,21 +2,27 @@ package lotto;
 
 public enum Rank {
 
-    FIRST("1등"),
-    SECOND("2등"),
-    THIRD("3등"),
-    FOURTH("4등"),
-    FIFTH("5등"),
-    NONE("꽝");
+    FIRST("1등", 5),
+    SECOND("2등", 4),
+    THIRD("3등", 3),
+    FOURTH("4등", 2),
+    FIFTH("5등", 1),
+    NONE("꽝",null);
 
     private final String name;
+    private final Integer printOrder;
 
-    Rank(String name) {
+    Rank(String name, Integer printOrder) {
         this.name = name;
+        this.printOrder = printOrder;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getPrintOrder() {
+        return printOrder;
     }
 
 }
