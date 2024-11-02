dd8ae4c3225c7e2218e2f28d8d60fc890f718638
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-03 01:33:31 +0900

    feat(WinningNumberInputHandler): 당첨번호를 입력받을 수 있는 생성자 생성 및 유효하지 않을 경우 에러를 발생시키는 메소드 추가

--- src/main/java/lotto/WinningNumberInputHandler.java
+++ src/main/java/lotto/WinningNumberInputHandler.java
@@ -6,9 +6,25 @@ import java.util.List;
 public class WinningNumberInputHandler extends InputHandler{
     private String winningNumber;
 
+    public WinningNumberInputHandler() {
+        boolean isValid = false;
+
+        while(!isValid){
+            System.out.println("당첨 번호를 입력해 주세요.");
+            this.winningNumber = camp.nextstep.edu.missionutils.Console.readLine();
+            try{
+                validateInput();
+                isValid = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+    }
+
     @Override
     public void validateInput() {
-
+        throwExceptionWhenInputIsInvalid();
+        throwExceptionWhenInputIsNotInRange1To45();
     }
 
     public String[] splitNumbers(String input) {
@@ -22,5 +38,17 @@ public class WinningNumberInputHandler extends InputHandler{
             lottoWinningNumbers.add(Integer.parseInt(number));
         }
         return lottoWinningNumbers;
+    }
+
+    private void throwExceptionWhenInputIsInvalid() {
+        if(!Validation.isValidWinningNumber(winningNumber)){
+            throwIllegalArgumentException("입력값이 유효하지 않습니다.");
+        }
+    }
+
+    private void throwExceptionWhenInputIsNotInRange1To45() {
+        if(!Validation.isInRange1To45(stringInputParseInteger(winningNumber))){
+            throwIllegalArgumentException("당첨 번호의 숫자는 1부터 45까지의 범위안에서 입력해 주세요.");
+        }
     }
 }
