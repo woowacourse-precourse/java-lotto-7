0e1663b11bd1e84c41195eef9afa1d857f38ebba
dawit0905 <dawit0905@gmail.com>
2024-11-02 21:20:19 +0900

    feat: LottoService 생성

---
+++ src/main/java/service/LottoService.java
@@ -0,0 +1,59 @@
+package service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+import validator.InputValidator;
+import view.InputView;
+import view.OutputView;
+
+public class LottoService {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public int getLottoPurchaseAmount() {
+        return inputView.inputLottoPurchaseAmount();
+    }
+
+    public int getLottoDrawCount(int lottoPurchaseAmount) {
+        InputValidator.isDivisibleByThousand(lottoPurchaseAmount);
+        return lottoPurchaseAmount / 1000;
+    }
+
+
+    public Lotto getWinnerLottoNumbers() {
+
+        List<Integer> lottoList = inputView.inputWinnerLottoNumbers();
+        Lotto LottoWinnerNumber = new Lotto(lottoList);
+
+        return LottoWinnerNumber;
+    }
+
+    public List<Lotto> generateRandomLottoNumbers(int lottoDrawCount) {
+        List<Lotto> lottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < lottoDrawCount; i++) {
+            lottoNumbers.add(makeLotto());
+        }
+
+        return lottoNumbers;
+    }
+
+    public Lotto makeLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            numbers.add(Randoms.pickNumberInRange(1, 45));
+        }
+        return new Lotto(numbers);
+    }
+
+    public void printLottoNumbers(List<Lotto> lottoList) {
+        outputView.outputLottoNumbers(lottoList);
+    }
+
+    public int getBonusNumber() {
+        return inputView.inputBonusNumber();
+    }
+}
