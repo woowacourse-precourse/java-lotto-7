8af76c55ff97cab34d7002b78293eb52aa2e9ec2
leeedohyun <ldh19738@gmail.com>
2024-11-02 18:39:23 +0900

    feat: 오류가 발생하면 그 부분부터 다시 입력

--- src/main/java/lotto/io/ConsoleInputHandler.java
+++ src/main/java/lotto/io/ConsoleInputHandler.java
@@ -13,19 +13,37 @@ public class ConsoleInputHandler implements InputHandler {
 
     @Override
     public LottoAmount inputPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        return inputConverter.convertToLottoAmount(Console.readLine());
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                return inputConverter.convertToLottoAmount(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     @Override
     public WiningNumbers inputWiningNumbers() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
-        return inputConverter.convertToWiningNumbers(Console.readLine().split(NUMBER_DELIMITER));
+        while (true) {
+            try {
+                System.out.println("\n당첨 번호를 입력해 주세요.");
+                return inputConverter.convertToWiningNumbers(Console.readLine().split(NUMBER_DELIMITER));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     @Override
     public LottoNumber inputBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        return inputConverter.convertToBonusNumber(Console.readLine());
+        while (true) {
+            try {
+                System.out.println("\n보너스 번호를 입력해 주세요.");
+                return inputConverter.convertToBonusNumber(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
