0e02476f9f97dd52719a6840c403be226718bb68
GaBaljaintheroom <junsu1222@naver.com>
2024-11-02 23:52:47 +0900

    feat(LottoMachine): 로또 기계에서 현금으로 로또를 구매 기능 구현

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int LAST_LOTTO_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
+    private LottoTickets lottoTickets;
+
+    public void purchase(Cash cash) {
+        int ticketCount = cash.getTicketCount();
+        List<Lotto> tickets = createTickets(ticketCount);
+
+        lottoTickets = new LottoTickets(tickets);
+    }
+
+    private List<Lotto> createTickets(int ticketCount) {
+        return IntStream.range(0, ticketCount)
+            .mapToObj(i -> new Lotto(
+                RandomNumber.getUniqueNumbers(START_LOTTO_NUMBER, LAST_LOTTO_NUMBER, LOTTO_COUNT)
+            ))
+            .toList();
+    }
+
+}
