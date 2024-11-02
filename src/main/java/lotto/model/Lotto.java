d8cf83f8aa75bd9aa979e1c3301ffacc8684f8ae
suyeong jang <jangsuyeong923@naver.com>
2024-11-03 02:50:06 +0900

    feat(Lotto): 로또 번호 일치하는 개수 계산 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -16,10 +16,22 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-
+        if(numbers.stream().distinct().count()!=6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
     public List<Integer> getNumbers(){
         return numbers;
+    }
+
+    public int getMatchCount(List<Integer>winningNumbers){
+        return (int)numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public boolean containsBonusNumber(int bonusNumber){
+        return numbers.contains(bonusNumber);
     }
     // TODO: 추가 기능 구현
 }
