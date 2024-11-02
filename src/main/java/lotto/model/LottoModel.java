6104d02e851540b86df06249a0d71e7df7373ad8
mipangg <morganjeong119@gmail.com>
2024-11-02 18:13:09 +0900

    feat(LottoModel): add implement to compare user lotto to winning numbers, bonus number

--- src/main/java/lotto/model/LottoModel.java
+++ src/main/java/lotto/model/LottoModel.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-// UserLotto와 Lotto를 이용하여 유저의 복권 정보를 비교하고 통계하는 메서드들 모음
+// UserLotto와 Lotto를 이용하여 유저의 로또 정보를 비교하고 통계하는 메서드들 모음
 public class LottoModel {
     private int bonusNumber;
     private Lotto winningNumbers;
     private UserLotto userLotto;
+    private List<Integer> winnerCount;
 
     public void setBonusNumber(int bonusNumber) {
         this.bonusNumber = bonusNumber;
@@ -26,5 +28,19 @@ public class LottoModel {
 
     public Lotto getWinningNumbers() {
         return winningNumbers;
+    }
+
+    // 당첨번호와 유저의 로또 정보 비교
+    public void compareLotto() {
+        winnerCount = new ArrayList<>();
+        for (int i = 0; i < userLotto.getNumberOfLotto(); i++ ) {
+            List<Integer> winner = userLotto.getAtLotto(i);
+            winner.retainAll(winningNumbers.getNumbers());
+            winnerCount.add(winner.size());
+        }
+    }
+
+    public boolean matchBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
     }
 }
