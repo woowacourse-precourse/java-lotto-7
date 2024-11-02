d9746425898cf3185fe2bc00e1eec2a752e7ee19
33jyu33 <jy04169@gmail.com>
2024-11-02 13:50:02 +0900

    feat(OutputView): add changeLine
    Function changeLine

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import lotto.constant.LottoGuide;
 
+import java.util.List;
+
 public class OutputView {
     public static void printPriceGuide(){
         System.out.println(LottoGuide.PRICE_GUIDE.getMessage());
@@ -9,6 +11,11 @@ public class OutputView {
 
     public static void printPurchaseCount(int count){
         System.out.printf(LottoGuide.PURCHASE_COUNT.getMessage(), count);
+        System.out.println();
+    }
+
+    public static void printLottoTicket(List<Integer> lottoTicket){
+        System.out.println(lottoTicket);
     }
 
     public static void printWinningNumberGuide(){
@@ -17,5 +24,9 @@ public class OutputView {
 
     public static void printBonusNumberGuide(){
         System.out.println(LottoGuide.BONUS_NUMBER_GUIDE.getMessage());
+    }
+
+    public static void changeLine(){
+        System.out.println();
     }
 }
