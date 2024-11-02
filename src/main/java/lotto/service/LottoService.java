e3bae326dd7acfe780cc6cfdc2e64f79c82d1716
jungsu <vwx1@naver.com>
2024-11-02 22:57:32 +0900

    feat: 로또 생성 로직 구현
    - LottosServiceInterface의 generateLottosByAmount 메서드를 구현했습니다.
    - 로또 구매 금액에 따라 생성할 로또의 수를 계산합니다 (1,000원당 1장).
    - 각 로또를 생성하기 위해 1부터 45 사이의 고유한 숫자 6개를 랜덤으로 선택하여 Lotto 객체에 추가합니다.
    - 생성된 로또 목록을 반환합니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,33 @@
 package lotto.service;
 
-public class LottoService implements LottosServiceInterface{
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
 
+public class LottoService implements LottosServiceInterface {
+
+  @Override
+  public List<Lotto> generateLottosByAmount(int amount) {
+    int numOfLottos = amount / 1000;
+    List<Lotto> lottos = new ArrayList<>();
+
+    for (int i = 0; i < numOfLottos; i++) {
+      lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+    }
+    return lottos;
+  }
+
+
+  @Override
+  public List<Integer> checkWinningNumbers(List<Lotto> lottos, List<Integer> winningNumbers,
+      int bonusNumber) {
+    return null;
+  }
+
+
+  @Override
+  public float calculateYield(List<Integer> winningCounts, int purchaseAmount) {
+    return 1;
+  }
 }
