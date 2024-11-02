f437279e6d60f6105fce0e2a2acacb8fce73d9e2
Zepelown <dbswjdqhd12@gmail.com>
2024-11-03 03:40:36 +0900

    feat: Add input feature for lotto purchase amount

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,13 @@
 package lotto.controller;
 
+import lotto.view.LottoInputView;
 import lotto.view.LottoOutputView;
 
 public class LottoController {
     private final LottoOutputView lottoOutputView = new LottoOutputView();
+    private final LottoInputView lottoInputView = new LottoInputView();
     public void start(){
         lottoOutputView.printLottoAmountNotification();
+        String lottoAmount = lottoInputView.getUserInput();
     }
 }
