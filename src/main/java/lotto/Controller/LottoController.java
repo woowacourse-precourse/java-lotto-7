3a115abe478b5769829b9c87e18643dfcf1e81a2
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-02 21:27:26 +0900

    feat(LottoController): 로또번호비교 컨트롤러

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -5,28 +5,33 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoController {
     private static PurchaseAmount purchaseAmount;
+    private static BonusNumber bonusNumber;
+    private static List<Integer> lotto = new ArrayList<>();
+    private static List<Integer> lottoNumber =new ArrayList<>();
+    private static List<Integer> lottoList=new ArrayList<>();
     public LottoController(){
     }
 
     public void run(){
-        PrintInput();
-        PrintOutput();
-    }
-    public void PrintInput(){
-        try{
-            String amount_str =InputView.purchaseAmount();
-            PlayerAmount(amount_str);
-            String number_str =InputView.sixLottoNumber();
-            lottoNum(number_str);
-            String bonusnum_str=InputView.bonusLottoNumber();
-            bonusNum(bonusnum_str);
+        try {
+            PrintInput();
+            PrintOutput();
         }catch (IllegalArgumentException e){
             System.out.println("[ERROR]"+ e.getMessage());
         }
+    }
+    public void PrintInput(){
+        String amount_str =InputView.purchaseAmount();
+        PlayerAmount(amount_str);
+        String number_str =InputView.sixLottoNumber();
+        lottoNum(number_str);
+        String bonusnum_str=InputView.bonusLottoNumber();
+        bonusNum(bonusnum_str);
     }
     public void PlayerAmount(String amount_str){
         purchaseAmount = new PurchaseAmount(amount_str);
@@ -34,7 +39,10 @@ public class LottoController {
     }
     public static void LottoList(int ticketCount){
         for(int i=0;i<ticketCount;i++){
-            OutputView.printLottoList(RandomLotto.randomLottoNumber());
+            lotto=RandomLotto.randomLottoNumber();
+            lottoList.addAll(lotto);
+            OutputView.printLottoList(lotto);
+            lotto.clear();
         }
     }
     public void PrintDetails(){
@@ -42,13 +50,20 @@ public class LottoController {
         LottoList(purchaseAmount.calculateLottoCount());
     }
     public void lottoNum(String number_str){
-        List<Integer> lottoNumber =new ArrayList<>(LottoNumber.winLottoNumber(number_str));
+        lottoNumber =LottoNumber.winLottoNumber(number_str);
         Lotto lotto =new Lotto(lottoNumber);
     }
     public void bonusNum(String bonusNum_str){
-        BonusNumber bonusNumber=new BonusNumber(bonusNum_str);
+        bonusNumber=new BonusNumber(bonusNum_str);
     }
     public void PrintOutput(){
         OutputView.printTotal();
+        CompareLotto();
+    }
+    public void CompareLotto(){
+        int[] count=LottoTotal.winningLotto(lottoList,lottoNumber);
+        int bonus_num=bonusNumber.getBonusNum();
+        count=LottoTotal.bonusMatches(bonus_num,lottoList,count);
+        OutputView.printCorrect(count);
     }
 }
