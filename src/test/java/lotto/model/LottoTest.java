dfe4946b04fd48574ff99258603756dcb84d0c61
Hyso <hsk7953@gmail.com>
2024-11-02 23:49:43 +0900

    test : 로또 중복 번호 테스트 통과 및 getter 메서드 확인

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+class LottoTest {
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("가지고 있는 로또 번호를 반환하는 함수 테스트")
+    @Test
+    void getNumbersTest(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        assertThat(lotto.getNumbers()).isInstanceOf(List.class)
+                .containsExactly(1,2,3,4,5,6)
+                .doesNotContain(7);
+    }
+}
