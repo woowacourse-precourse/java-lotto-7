bf89dea6024ceb4d7e3c2232ef3427a9110ab8ea
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-03 01:49:38 +0900

    test : 당첨 로또의 보너스 번호가 로또의 번호와 중복되는 케이스 검사

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -11,7 +11,7 @@ class LottoTest {
 
     @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
     @Test
-    void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다(){
+    void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -34,6 +34,13 @@ class LottoTest {
     @Test
     void 로또_번호에_허동되는_범위_이외의_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 3, 6, 8, 13, 53)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또의 보너스 번호가 로또의 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_당첨_로또의_보너스_번호가_포함되어있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningLotto(List.of(1, 3, 6, 9, 13, 15), 15))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
