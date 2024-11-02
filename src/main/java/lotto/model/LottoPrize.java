e4b30d15f228f7e153e3fba92eb6167f3ffc44b7
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 02:09:19 +0900

    feat: 로또 상금 담는 enum 작성

---
+++ src/main/java/lotto/model/LottoPrize.java
@@ -0,0 +1,39 @@
+package lotto.model;
+
+public enum LottoPrize {
+    FAIL(0, 0, "", 0),
+    FIFTH_PRIZE(3, 0, "3개 일치 (5,000원)", 5000),
+    FORTH_PRIZE(4, 0, "4개 일치 (50,000원)", 50000),
+    THIRD_PRIZE(5, 0, "5개 일치 (1,500,000원)", 1500000),
+    SECOND_PRIZE(5, 1, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
+    FIRST_PRIZE(6, 0, "6개 일치 (2,000,000,000원)", 2000000000)
+    ;
+
+    private final int matchingWinningCount;
+    private final int matchingBonusCount;
+    private final String prizeDetails;
+    private final long prizeAmounts;
+
+    LottoPrize(int matchingWinningCount, int matchingBonusCount, String prizeDetails, long prizeAmounts) {
+        this.matchingWinningCount = matchingWinningCount;
+        this.matchingBonusCount = matchingBonusCount;
+        this.prizeDetails = prizeDetails;
+        this.prizeAmounts = prizeAmounts;
+    }
+
+    public int getMatchingWinningCount() {
+        return matchingWinningCount;
+    }
+
+    public int getMatchingBonusCount() {
+        return matchingBonusCount;
+    }
+
+    public String getPrizeDetails() {
+        return prizeDetails;
+    }
+
+    public long getPrizeAmounts() {
+        return prizeAmounts;
+    }
+}
