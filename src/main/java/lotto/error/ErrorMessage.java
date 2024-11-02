5dfa3234336bc57aa555e6edd1c948c53a16a5ed
Fletacode <bassykd@gmail.com>
2024-11-02 19:02:38 +0900

    fix: 에러 메세지 문구 추가
    에러 메세지 문구 추가
    전체 에러 메세지 [ERROR] 문구 추가

--- src/main/java/lotto/error/ErrorMessage.java
+++ src/main/java/lotto/error/ErrorMessage.java
@@ -1,12 +1,12 @@
 package lotto.error;
 
 public enum ErrorMessage {
-    NOTNUMBERBUYAMOUNT("구매금액이 정수가 아닙니다."),
-    NOTPER1000BUYAMOUNT("구매금액이 1000원 단위가 아닙니다."),
-    NOTDELIMITERWINNUMBERS("입력된 당첨번호의 구분자가 ,가 아닙니다."),
-    NOTNUMBERWINNUMBERS("당첨번호가 숫자가 아닙니다"),
-    NOTNUMBERBONUSNUMBER("보너스 번호가 정수가 아닙니다."),
-    OUTOFRANGEBONUSNUMBER("보너스숫자가 1~45 범위안에 존재하는 숫자가 아닙니다.");
+    NOTNUMBERBUYAMOUNT("[ERROR] 구매금액이 정수가 아닙니다."),
+    NOTPER1000BUYAMOUNT("[ERROR] 구매금액이 1000원 단위가 아닙니다."),
+    NOTDELIMITERWINNUMBERS("[ERROR] 입력된 당첨번호의 구분자가 ,가 아닙니다."),
+    NOTNUMBERWINNUMBERS("[ERROR] 당첨번호가 숫자가 아닙니다"),
+    NOTNUMBERBONUSNUMBER("[ERROR] 보너스 번호가 정수가 아닙니다."),
+    OUTOFRANGEBONUSNUMBER("[ERROR] 보너스숫자가 1~45 범위안에 존재하는 숫자가 아닙니다.");
 
     private final String message;
 
