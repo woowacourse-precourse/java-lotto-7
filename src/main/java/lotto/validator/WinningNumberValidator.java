26676dc3833fb577a559346a9c0f15567067596b
33jyu33 <jy04169@gmail.com>
2024-11-02 16:02:52 +0900

    feat(WinningNumberValidator): validate number is in scope
    Create winningNumber list to return
    Function validateNumberScope

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -1,7 +1,9 @@
 package lotto.validator;
 
+import lotto.constant.CompareInteger;
 import lotto.constant.WinningNumberRule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumberValidator {
@@ -12,10 +14,15 @@ public class WinningNumberValidator {
         validateLastComma(input);
     }
 
-    public static void validateInputNumber(List<String> input)throws IllegalArgumentException{
-        for (String separatedInput : input){
+    public static List<Integer> validateInputNumber(List<String> input) throws IllegalArgumentException {
+        List<Integer> winningNumber = new ArrayList<>();
+        for (String separatedInput : input) {
             validateOnlyInteger(separatedInput);
-        };
+            Integer inputNumber = Integer.parseInt(separatedInput);
+            validateNumberScope(inputNumber);
+            winningNumber.add(inputNumber);
+        }
+        return winningNumber;
     }
 
     private static void validateDoubletComma(String input) throws IllegalArgumentException {
@@ -24,21 +31,25 @@ public class WinningNumberValidator {
         }
     }
 
-    private static void validateFirstComma(String input) throws IllegalArgumentException{
-        if(input.startsWith(WinningNumberRule.COMMA.getMessage())){
+    private static void validateFirstComma(String input) throws IllegalArgumentException {
+        if (input.startsWith(WinningNumberRule.COMMA.getMessage())) {
             throw new IllegalArgumentException(WinningNumberRule.COMMA_FIRST.getMessage());
         }
     }
 
-    private static void validateLastComma(String input) throws IllegalArgumentException{
-        if(input.endsWith(WinningNumberRule.COMMA.getMessage())){
+    private static void validateLastComma(String input) throws IllegalArgumentException {
+        if (input.endsWith(WinningNumberRule.COMMA.getMessage())) {
             throw new IllegalArgumentException(WinningNumberRule.COMMA_LAST.getMessage());
         }
     }
 
-    private static void validateOnlyInteger(String input){
-        if(!input.matches(WinningNumberRule.MATCH_NUMBER.getMessage())){
+    private static void validateOnlyInteger(String input) {
+        if (!input.matches(WinningNumberRule.MATCH_NUMBER.getMessage())) {
             throw new IllegalArgumentException(WinningNumberRule.ONLY_COMMA_INTEGER.getMessage());
         }
+    }
+
+    private static void validateNumberScope(Integer number) {
+        NumberValidator.validateScope(CompareInteger.LOTTO_NUMBER_MINIMUM.getNumber(), CompareInteger.LOTTO_NUMBER_MAXIMUM.getNumber(), number, WinningNumberRule.SCOPE.getMessage());
     }
 }
