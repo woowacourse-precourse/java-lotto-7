fe044ef984892c104fbd097dc8de4a96599c3b1e
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-11-02 22:28:13 +0900

    feat: LottoController 클래스 구현
    - 당첨 결과 계산 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
 import lotto.model.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -21,6 +22,18 @@ public class LottoController {
         List<Integer> winNumbers = InputView.WinNumbers();
         int bonusNumber = InputView.BonusNumber();
         WinningNumbers winningNumbers = new WinningNumbers(new Lotto(winNumbers), bonusNumber);
-        
+
+        int[] rankCount = new int[LottoRank.ranks.size()];
+        for (Lotto lotto : purchaseLottos) {
+            int matchCount = 0;
+            for (int number : lotto.getNumbers()) {
+                if (winningNumbers.getWinningNumber().getNumbers().contains(number)) {
+                    matchCount++;
+                }
+            }
+            boolean matchBonus = lotto.getNumbers().contains(winningNumbers.getBonusNumber());
+            LottoRank rank = LottoRank.getRank(matchCount, matchBonus);
+            rankCount[LottoRank.ranks.indexOf(rank)]++;
+        }
     }
 }
