c2d1f3a7ca6d7aa9ed2182c4a516d0b4582238ed
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 21:34:51 +0900

    refactor(LottoGenerator): enum 사용해서 코드 리팩토링

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,21 +2,25 @@ package lotto;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
-public class LottoGenerator {
-    static final int PRICE_PER_LOTTO = 1000;
 
+import static lotto.LottoConstants.*;
+public class LottoGenerator {
     public int getLottoCount(int budget) {
-        if (budget < PRICE_PER_LOTTO) {
-            throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO + "원 이상의 금액을 입력하세요.");
+        if (budget < PRICE_PER_LOTTO.getValue()) {
+            throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO.getValue() + "원 이상의 금액을 입력하세요.");
         }
-        if (budget % PRICE_PER_LOTTO != 0) {
+        if (budget % PRICE_PER_LOTTO.getValue() != 0) {
             throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO + "원 단위로만 구매 가능합니다.");
         }
-        return budget / PRICE_PER_LOTTO;
+        return budget / PRICE_PER_LOTTO.getValue();
     }
 
     public Lotto generateLotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                MIN_NUMBER.getValue(),
+                MAX_NUMBER.getValue(),
+                SELECTED_NUMBER_COUNT.getValue()
+        );
         return new Lotto(numbers);
     }
 
