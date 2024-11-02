43484f02257ba45c5da07c483c7b84237d1de73f
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-02 18:16:14 +0900

    feat: 당첨 번호와 보너스 번호가 중복 될 경우에 대한 검증 기능 구현

--- src/main/java/lotto/view/Validator.java
+++ src/main/java/lotto/view/Validator.java
@@ -13,6 +13,8 @@ public class Validator {
             "[ERROR] 당첨 번호는 중복되지 않은 서로 다른 6개의 숫자가 입력 되어야 합니다.";
     private static final String ERROR_OUT_OF_RANGE_BONUS_NUMBER_MESSAGE =
             "[ERROR] 보너스 번호는 1에서 45 사이의 숫자로 이루어져 있어야 합니다.";
+    private static final String ERROR_DUPLICATE_WINNING_NUMBER_AND_BONUS_NUMBER_MESSAGE =
+            "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않은 숫자가 입력 되어야 합니다.";
 
     private final static int PURCHASE_AMOUNT_UNITS = 1000;
     private final static int ZERO = 0;
@@ -43,6 +45,12 @@ public class Validator {
     public static void validateBonusNumber(int bonusNumber) {
         if (bonusNumber < MINIMUM_LOTTO_NUMBER || bonusNumber > MAXIMUM_LOTTO_NUMBER) {
             throw new IllegalArgumentException(ERROR_OUT_OF_RANGE_BONUS_NUMBER_MESSAGE);
+        }
+    }
+
+    public static void validateDuplicateWith(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE_WINNING_NUMBER_AND_BONUS_NUMBER_MESSAGE);
         }
     }
 }
