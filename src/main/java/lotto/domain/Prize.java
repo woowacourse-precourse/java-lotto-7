b96416ade19dae17e847aef8b4d13c501ed90524
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 02:28:03 +0900

    feat(Prize): 로또 게임의 등수 정보를 가지는 클래스 작성

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum Prize {
+
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    FAIL(-1, false, 0);
+
+    private int matchCount;
+    private boolean isBonus;
+    private int money;
+
+    Prize(int matchCount, boolean isBonus, int money) {
+        this.matchCount = matchCount;
+        this.isBonus = isBonus;
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
