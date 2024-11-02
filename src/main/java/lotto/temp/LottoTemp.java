e66534e4d06218335a83d56a9f55175c4310c757
TalkingPotato90 <star901210@hanmail.net>
2024-11-03 00:42:16 +0900

    feat: 등수별 당첨 개수 확인하기

--- src/main/java/lotto/temp/LottoTemp.java
+++ src/main/java/lotto/temp/LottoTemp.java
@@ -6,7 +6,9 @@ import lotto.util.CommonIo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class LottoTemp {
@@ -126,6 +128,47 @@ public class LottoTemp {
         }
 
         return rank;
+    }
+
+    // TODO : 심각!!!
+    public Map<String,Integer> finalRank(List<String> ranking){
+        Map<String,Integer> ranks = new HashMap<>();
+        int noRank = 0;
+        int rank5 = 0;
+        int rank4 = 0;
+        int rank3 = 0;
+        int rank2 = 0;
+        int rank1 = 0;
+
+        for (String s : ranking) {
+            if (s.equals("꽝")) {
+                noRank++;
+            }
+            if (s.equals("5등")) {
+                rank5++;
+            }
+            if (s.equals("4등")) {
+                rank4++;
+            }
+            if (s.equals("3등")) {
+                rank3++;
+            }
+            if (s.equals("2등")) {
+                rank2++;
+            }
+            if (s.equals("1등")) {
+                rank1++;
+            }
+        }
+
+        ranks.put("꽝",noRank);
+        ranks.put("5등",rank5);
+        ranks.put("4등",rank4);
+        ranks.put("3등",rank3);
+        ranks.put("2등",rank2);
+        ranks.put("1등",rank1);
+
+        return ranks;
     }
 
     public void printProfit(int profit) {
--- src/test/java/lotto/temp/LottoTempTest.java
+++ src/test/java/lotto/temp/LottoTempTest.java
@@ -80,4 +80,17 @@ class LottoTempTest {
         assertThat(lottoTemp.determineRank(5,true)).isEqualTo("2등");
         assertThat(lottoTemp.determineRank(6,true)).isEqualTo("1등");
     }
+
+    @Test
+    @DisplayName("등수 리스트를 받아서 당첨 개수를 제대로 나타내는지 확인하는 테스트")
+    void checkRankingCount(){
+        assertThat(lottoTemp.finalRank(List.of("5등","5등","5등","5등","꽝")))
+                .containsEntry("5등",4)
+                .containsEntry("꽝",1);
+        assertThat(lottoTemp.finalRank(List.of("5등","4등","3등","5등","꽝")))
+                .containsEntry("5등",2)
+                .containsEntry("4등",1)
+                .containsEntry("3등",1)
+                .containsEntry("꽝",1);
+    }
 }
