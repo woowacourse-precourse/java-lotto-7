657ff85a8c49020a10f83475dff46c756c3ee61d
ssggii <haan7525@gmail.com>
2024-11-03 00:42:24 +0900

    feat: implement the feature for creating a Lotto instance
    로또 객체 생성 기능을 구현했습니다. Lotto 인스턴스 생성 시 로또 숫자의
    개수와 중복 여부를 검사합니다. 로또 숫자가 6개가 아니거나 중복된 숫자가
    있으면 예외가 발생합니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,28 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberCount(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers == null || numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복되었습니다.");
+        }
+    }
 }
