5e215486200eb2c2155c3bd112e59e72010e2022
Dohun Kim <dohun8758@naver.com>
2024-11-02 17:13:53 +0900

    feat: 로또 구매내역 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+
 public class OutputView {
 
     public void showMoneyInputComments() {
@@ -12,5 +16,14 @@ public class OutputView {
 
     public void showLottoBonusNumberInputComments() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void showPurchasedLottos(int purchasedCount, Lottos purchasedLottos) {
+        System.out.println();
+        System.out.printf("%d개를 구매했습니다\n", purchasedCount);
+        List<Lotto> lottos = purchasedLottos.getLottos();
+
+        lottos.forEach(System.out::println);
+        System.out.println();
     }
 }
