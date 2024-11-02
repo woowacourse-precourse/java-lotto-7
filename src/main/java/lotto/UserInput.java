45185ff49e5d10ee5e8f880fbe7c98f4fa444d7f
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-11-02 19:56:19 +0900

    feat: 구매 금액 입력 메서드 및 검증 메서드 구현

---
+++ src/main/java/lotto/UserInput.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+public class UserInput {
+
+    private int numberOfLotto;
+
+    public int purchaseAmountInput() {
+
+        System.out.println("구입 금액을 입력해주세요.");
+
+        String amount = readLine();
+
+        validPurchaseAmountInput(amount);
+
+        return Integer.parseInt(amount);
+    }
+
+    public void validPurchaseAmountInput(String input) {
+
+        int amount;
+
+        try {
+            amount = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자여야 합니다.");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 0 이상이어야 합니다.");
+        }
+
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+    public UserInput() {
+        this.numberOfLotto = purchaseAmountInput();
+    }
+}
