e05bb502bbf27277cfc30b4226f288fe5c28ccee
김영록 <praoo800@konkuk.ac.kr>
2024-11-02 19:22:25 +0900

    <test>(registerWinnerNumberTest) : 당첨 번호 입력 테스트
    테스트 코드를 보니 번호가 6개가 넘어가거나 중복된 숫자가 있는 경우에는 이미 테스트 코드가 쓰여져 있고,
    로또 번호 범위에 포함되지 않는 숫자를 입력했을 경우 그리고
    로또 번호가 6개 미만인 경우는 테스트 코드로 쓰여있지 않아서 이를 반영해 구현하였다

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -22,4 +25,18 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 범위(1~45)에 들어가지 않는 숫자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 47})
+    void 로또_번호_범위에_들어가지_않는_숫자가_있으면_예외가_발생한다(Integer overScopeNum) {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, overScopeNum, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 6개 미만인 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_6개가_아닌_경우_예외_발생() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
