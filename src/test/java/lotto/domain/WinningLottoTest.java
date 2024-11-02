8cf96854268c82173447a4e96792e4eb8ca02a5a
changuii <rhljh7410@gmail.com>
2024-11-02 13:54:51 +0900

    test : 로또 당첨 번호들의 예외 테스트를 진행하라
    - README : 검증을 더 세분화하여 작성하였다.

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.enums.LottoConfig;
+import lotto.enums.LottoError;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+    private static final LottoConfig CONFIG = LottoConfig.WOOWA_CONFIG;
+    private static final String ERROR_PREFIX = "[ERROR]";
+
+    @Test
+    void 로또_당첨_번호의_수가_로또_번호의_수와_다르다면_예외가_발생한다(){
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        assertThatThrownBy(() -> WinningLotto.ofNumbersAndConfig(numbers, CONFIG))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoError.LOTTO_WINNING_NUMBERS_COUNT.getMessage())
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+
+    @Test
+    void 로또_당첨_번호들_중_로또_번호의_숫자_범위_초과이면_예외가_발생한다(){
+        List<Integer> numbers = List.of(1,2,3,4,5,46);
+
+        assertThatThrownBy(() -> WinningLotto.ofNumbersAndConfig(numbers, CONFIG))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoError.LOTTO_NUMBER_MORE_THAN_MAX.getMessage())
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+
+    @Test
+    void 로또_당첨_번호들_중_로또_번호의_숫자_범위_미만이면_예외가_발생한다(){
+        List<Integer> numbers = List.of(0,1,2,3,4,5);
+
+        assertThatThrownBy(() -> WinningLotto.ofNumbersAndConfig(numbers, CONFIG))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoError.LOTTO_NUMBER_LESS_THAN_MIN.getMessage())
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+
+    @Test
+    void 로또_당첨_번호들_중_중복된_숫자가_존재한다면_예외가_발생한다(){
+        List<Integer> numbers = List.of(1,2,3,4,5,5);
+
+        assertThatThrownBy(() -> WinningLotto.ofNumbersAndConfig(numbers, CONFIG))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoError.LOTTO_WINNING_NUMBERS_DUPLICATION.getMessage())
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+}
