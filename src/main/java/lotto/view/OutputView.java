5519ced115755685d5faa692116db8bcd5257c20
차왕준 <ckdhdkwns@gmail.com>
2024-11-02 17:47:43 +0900

    feat(view): 발행된 로또 목록 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,17 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
     private static final String PURCHASE_COUNT_FORMAT = "%d개를 구매했습니다.";
 
     public void printPurchaseCount(int count) {
         System.out.printf((PURCHASE_COUNT_FORMAT) + "%n", count);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        lottos.forEach(System.out::println);
     }
 }
