60e3e9cb8a5748656fe43caf5cc6c47b1b55d476
이희수 <hslee9234@naver.com>
2024-11-02 20:28:34 +0900

    feat: LottoWinningNumbers dto 구현
    해당 객체는 dto로써 당첨 번호와 보너스 번호 데이터 전송의 역할을 함

---
+++ src/main/java/lotto/dto/LottoWinningNumbers.java
@@ -0,0 +1,21 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoWinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public LottoWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
