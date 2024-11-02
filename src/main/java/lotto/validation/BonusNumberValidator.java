e7a59ad19d40fa6703f3d6a31941aeb6fa6ac944
dltmddus <tmddus4671@gmail.com>
2024-11-02 14:51:44 +0900

    feat: 보너스 로또 번호 입력 검증

--- src/main/java/lotto/validation/BonusNumberValidator.java
+++ src/main/java/lotto/validation/BonusNumberValidator.java
@@ -1,2 +1,25 @@
-package lotto.validation;public class BonusNumberValidator {
+package lotto.validation;
+
+import static lotto.validation.LottoNumberValidator.validateBonusNumber;
+
+import java.util.List;
+import lotto.view.input.InputError;
+import lotto.view.input.InvalidInputException;
+
+public class BonusNumberValidator {
+    public static void validate(String input) {
+        validateBonusNumber(input);
+    }
+
+    private static void validateLottoRange(Integer input) {
+        if (input >= 1 && input <= 45) {
+            throw new InvalidInputException(InputError.LOTTO_NUMBER_RANGE_INVALID);
+        }
+    }
+
+    private static void validateDuplicate(Integer input, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(input)) {
+            throw new InvalidInputException(InputError.LOTTO_NUMBER_CANNOT_DUPLICATE);
+        }
+    }
 }
