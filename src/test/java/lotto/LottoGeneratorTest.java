e50156573b5d8401352936bdf84e05c09242f00b
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 19:59:21 +0900

    test(LottoGenerator): 테스트 코드에 사용한 테스트 값 수정

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -15,7 +15,7 @@ public class LottoGeneratorTest {
 
     @Test
     void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> lottoGenerator.getLottoCount(-1000))
+        assertThatThrownBy(() -> lottoGenerator.getLottoCount(1500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
