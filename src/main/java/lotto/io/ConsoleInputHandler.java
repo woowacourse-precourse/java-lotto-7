32df4c92bb800a85574bdbc17691bccd5aabee68
leeedohyun <ldh19738@gmail.com>
2024-11-02 13:30:26 +0900

    feat: 당첨 번호 입력

---
+++ src/main/java/lotto/io/ConsoleInputHandler.java
@@ -0,0 +1,20 @@
+package lotto.io;
+
+import java.util.Arrays;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInputHandler implements InputHandler {
+
+    private static final String NUMBER_DELIMITER = ",";
+
+    @Override
+    public List<Integer> inputLottoNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] splitNumbers = Console.readLine().split(NUMBER_DELIMITER);
+        return Arrays.stream(splitNumbers)
+                .map(Integer::parseInt)
+                .toList();
+    }
+}
