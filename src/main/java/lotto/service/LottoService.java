b9468f2105747b3fbc61dd943fb543d3466dcf63
Jewan1120 <hawaii1468@gmail.com>
2024-11-03 01:18:43 +0900

    refactor(Service): 하드 코딩되어 있는 값을 열거형으로 분리
    하드 코딩되어 있는 값들을 관리하기 위해 상수, 열거형으로 분리하였습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,14 +3,16 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.enums.Constants;
 
 public class LottoService {
 
     public List<Integer> getLottoNumbers() {
         List<Integer> numbers = new ArrayList<>();
-        while(numbers.size() < 6) {
-            int number = Randoms.pickNumberInRange(1, 45);
-            if(!numbers.contains(number)){
+        while (numbers.size() < Constants.LOTTO_NUMBER_SIZE.getValue()) {
+            int number = Randoms.pickNumberInRange(Constants.LOTTO_LOWER_BOUND.getValue(),
+                    Constants.LOTTO_UPPER_BOUND.getValue());
+            if (!numbers.contains(number)) {
                 numbers.add(number);
             }
         }
