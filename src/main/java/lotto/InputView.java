ec2c4a51f9fe9d423a80052fb3f0a9ed96d1bc0e
jinwook <wjswlsdnr333@gmail.com>
2024-11-02 17:51:56 +0900

    feat(InputView): 로또 구매 개수 구하는 기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,37 +1,44 @@
 package lotto;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final int lottoPurchase;
     private final int bonusNumber;
     private final List<Integer> lottoNumbers;
+    private final int lottoCount;
+    final int LOTTO_PURCHASE_COUNT = 1000;
 
-    public InputView(){
+    public InputView() {
         this.lottoPurchase = lottoPurchase();
         this.lottoNumbers = lottoNumbers();
         this.bonusNumber = bonusNumber();
+        this.lottoCount = lottoPurchase/LOTTO_PURCHASE_COUNT;
     }
 
-    public int lottoPurchase(){
+    public int lottoPurchase() {
         System.out.println("구입금액을 입력해 주세요.");
         String lottoPurchase = Console.readLine();
         return Integer.parseInt(lottoPurchase);
     }
-    public int bonusNumber(){
+
+    public int bonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String bonusNumber = Console.readLine();
         return Integer.parseInt(bonusNumber);
     }
-    public List<Integer> lottoNumbers(){
+
+    public List<Integer> lottoNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String rawNumbers = Console.readLine();
         String[] rawNumberSplit = rawNumbers.split(",");
 
         List<Integer> lottoNumbers = new ArrayList<>();
 
-        for(String numberInput : rawNumberSplit) {
+        for (String numberInput : rawNumberSplit) {
             int number = Integer.parseInt(numberInput);
             lottoNumbers.add(number);
         }
@@ -41,10 +48,16 @@ public class InputView {
     public int getBonusNumber() {
         return bonusNumber;
     }
+
     public int getLottoPurchase() {
         return lottoPurchase;
     }
+
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
+    }
+
+    public int getLottoCount(){
+        return lottoCount;
     }
 }
