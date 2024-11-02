8311dda5ccc92f39238ae336e25656d79be97ad0
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 03:54:01 +0900

    refactor(ViewMessage): 보너스 번호 입력을 요구하는 메시지에 개행 추가

--- src/main/java/lotto/constants/ViewMessage.java
+++ src/main/java/lotto/constants/ViewMessage.java
@@ -4,7 +4,7 @@ public enum ViewMessage {
 
     PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     LOTTO_PURCHASE_COUNT("\n%d개를 구매했습니다.");
 
     private final String text;
