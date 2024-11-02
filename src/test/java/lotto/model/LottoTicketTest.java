907055aa1594c5d3912c6e7c3f29019b9aa5f88c
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 02:44:14 +0900

    test: test코드 불필요한리스트  제거

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -3,9 +3,7 @@ package lotto.model;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.Arrays;
-import java.util.List;
 import lotto.dto.LottoBundle;
-import lotto.dto.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,18 +15,12 @@ class LottoTicketTest {
         // Given
         Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto lotto2 = new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12));
-        Lotto lotto3 = new Lotto(Arrays.asList(13, 14, 15, 16, 17, 18));
-        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(lotto1, lotto2, lotto3));
+        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(lotto1, lotto2));
 
         // When
         LottoBundle lottoBundle = lottoTicket.getLottoTicketStatus();
 
         // Then
-        List<LottoNumbers> expectedLottoNumbers = Arrays.asList(
-                lotto1.getLottoNumbers(),
-                lotto2.getLottoNumbers(),
-                lotto3.getLottoNumbers()
-        );
-        assertThat(lottoBundle.getLottoNumbers().size()).isEqualTo(expectedLottoNumbers.size());
+        assertThat(lottoBundle.getLottoNumbers().size()).isEqualTo(2);
     }
 }
