1d33624d28a7c7d4b085cf79ffab17939be8943e
KangGeonyoung <kky6335@gmail.com>
2024-11-02 17:36:18 +0900

    feat(OutputView): 당첨 통계 출력 OutputView - printPrizeResult: 당첨 통계 출력하는 메서드

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.PrizeCount;
 
 import java.util.List;
 
@@ -15,5 +16,19 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    /**
+     * 당첨 통계 출력
+     */
+    public static void printPrizeResult(PrizeCount prizeCount) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + prizeCount.getFifthPrizeCount() + "개");
+        System.out.println("4개 일치 (50,000원) - " + prizeCount.getFourthPrizeCount() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizeCount.getThirdPrizeCount() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeCount.getSecondPrizeCount() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizeCount.getFirstPrizeCount() + "개");
     }
 }
