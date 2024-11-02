c3f6855600deb6e39f3f084e116d073ddd23d7e4
wvssm <neoramiregi@gmail.com>
2024-11-03 03:01:12 +0900

    feat(Lotto): 로또 번호는 6개여야 한다

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
+
+import static lotto.exception.ErrorMessages.LOTTO_NUMBER_LENGTH_ERROR;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +13,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (isSizeSix(numbers.size())) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_LENGTH_ERROR);
         }
+    }
+
+    private boolean isSizeSix(int size){
+        return numbers.size() == 6;
     }
 
     // TODO: 추가 기능 구현
