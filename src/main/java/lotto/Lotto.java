b1025a25c1a5185bf8428d85c17ebf2e2587d101
Lkk7380 <dlrlrhkd@naver.com>
2024-11-02 14:32:49 +0900

    feat:로또 번호 범위 검증 기능 추가(테스트 코드 포함)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,7 +9,6 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
-
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -18,5 +17,9 @@ public class Lotto {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
-    }// TODO: 추가 기능 구현
+
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
