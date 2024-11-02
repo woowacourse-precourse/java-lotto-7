86b7f7bd826c64a6fd5d70f6db04338ebe30f39d
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-02 22:40:30 +0900

    feat(LottoService): 당첨자 결과 계산 및 수익율 계산
    LottoService에서 당첨 번호에 따른 당첨 결과를 반환하고 당첨금에 대한 수익률을 계산한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,14 @@
 package lotto.controller;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LottoController {
 
@@ -24,17 +26,15 @@ public class LottoController {
         try{
             // todo : Lotto Main Logic 작성
             int purchaseAmount = inputView.inputPurchaseAmount();
-            
+
             List<Lotto> purchasedLottos = lottoService.purchaseLotto(purchaseAmount);
             outputView.printPurchasedLottos(purchasedLottos);
 
             List<Integer> winningLottoNumbers = inputView.inputWinningNumber();
             int bonusNumber = inputView.inputBonusNumber();
 
-            // todo : 당첨 번호에 맞춰서 당첨 결과 계산
-            List<Integer> result = new ArrayList<>();
-            // todo : 투자 금액과 당첨 결과로 수익률 계산
-            double profitRate = 0.0;
+            Map<Rank,Integer> result = lottoService.calculateResult(purchasedLottos, winningLottoNumbers, bonusNumber);
+            double profitRate = lottoService.calculateProfitRate(result ,purchaseAmount);
             outputView.printResult(result, profitRate);
 
         } catch (IllegalArgumentException e){
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,9 +2,12 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoService {
 
@@ -17,6 +20,35 @@ public class LottoService {
         int purchaseLottoCount = purchaseAmount / LOTTO_PRICE;
         return issueLotto(purchaseLottoCount);
     }
+
+    public Map<Rank, Integer> calculateResult(List<Lotto> lottos, List<Integer> winningNumber, int bonusNumber) {
+        Map<Rank, Integer> result = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+
+        for (Lotto lotto : lottos) {
+            int matchCount = lotto.countMatches(winningNumber);
+            boolean isBonusMatched = lotto.getNumbers().contains(bonusNumber);
+            Rank rank = Rank.getRank(matchCount, isBonusMatched);
+            result.put(rank, result.get(rank) + 1);
+        }
+
+        return result;
+    }
+
+
+    public double calculateProfitRate(Map<Rank, Integer> result, int investmentMoney) {
+        int totalProfit = 0;
+
+        for (Map.Entry<Rank, Integer> entry : result.entrySet()) {
+            Rank rank = entry.getKey();
+            int count = entry.getValue();
+            totalProfit += rank.getPrize() * count;
+        }
+        return ((double) totalProfit / investmentMoney) * 100;
+    }
+
 
     private List<Lotto> issueLotto(int purchaseLottoCount){
         List<Lotto> lottos = new ArrayList<>();
