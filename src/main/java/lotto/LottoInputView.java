d5e0d35f3c3af140ac4d7638abba5c8fe1d10798
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-11-02 17:28:36 +0900

    feat(view) : 입력 금액을 잘못입력받았을 때, 예외 처리한다.

--- src/main/java/lotto/LottoInputView.java
+++ src/main/java/lotto/LottoInputView.java
@@ -5,8 +5,21 @@ import camp.nextstep.edu.missionutils.Console;
 public class LottoInputView
 {
     public static int LottoPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
-        int amount = Integer.parseInt(Console.readLine());
-        return amount;
+        try {
+            System.out.println("구입 금액을 입력해 주세요.");
+            int amount = Integer.parseInt(Console.readLine());
+            validatePurchaseLotto(amount);
+            return amount;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            // 다시 입력 받는다.
+            return LottoPurchaseAmount();
+        }
+    }
+
+    private static void validatePurchaseLotto(int amount) {
+        if (amount <= 0 || amount % 1000 != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해주세요.");
+        }
     }
 }
