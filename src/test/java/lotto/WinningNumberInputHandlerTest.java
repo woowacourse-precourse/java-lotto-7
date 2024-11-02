3c96c1141d36e0358128f185ec5e08fbe7b61e54
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-02 23:11:10 +0900

    test(WinningNumberInputHandlerTest): 입력된 값의 타입을 문자열에서 숫자로 변경하는 메소드 테스트

--- src/test/java/lotto/WinningNumberInputHandlerTest.java
+++ src/test/java/lotto/WinningNumberInputHandlerTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,18 @@ public class WinningNumberInputHandlerTest {
                 .contains("4")
                 .contains("5")
                 .contains("6");
+    }
+
+    @Test
+    void shouldReturnIntegerListWhenParseArray() {
+        WinningNumberInputHandler winningNumberInputHandler = new WinningNumberInputHandler();
+        assertThat(winningNumberInputHandler.stringInputParseInteger("1,2,3,4,5,6"))
+                .hasSize(6)
+                .contains(1)
+                .contains(2)
+                .contains(3)
+                .contains(4)
+                .contains(5)
+                .contains(6);
     }
 }
