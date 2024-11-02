2c920b69b866c2736cebbb2cf017a40e1c7d67f1
yuntasha <jaljayo85@gmail.com>
2024-11-02 17:53:14 +0900

    feat(LottoException): feat LottoException
    Feat constructor, makeErrorMessage method

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,4 +1,15 @@
 package lotto.exception;
 
-public class LottoException {
+public class LottoException extends IllegalArgumentException {
+    private static final String ERROR = "[ERROR] ";
+    private final ErrorMessage errorMessage;
+
+    public LottoException (ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+        this.errorMessage = errorMessage;
+    }
+
+    public String makeErrorMessage() {
+        return ERROR+errorMessage.getMessage();
+    }
 }
