e1d62df060a799fc0f4763aff382ef217698de7d
chaerish <ccyy8432@naver.com>
2024-11-03 02:46:01 +0900

    refactor(MatchingCountResult): MatchingCountResult 를 불변 레코드에서 객체로 변경

--- src/main/java/lotto/dto/MatchingCountResult.java
+++ src/main/java/lotto/dto/MatchingCountResult.java
@@ -2,5 +2,24 @@ package lotto.dto;
 
 import lotto.constant.WinningCondition;
 
-public record MatchingCountResult(WinningCondition winningCondition, int conditionCount) {
+public class MatchingCountResult {
+    private WinningCondition winningCondition;
+    private int conditionCount;
+
+    public MatchingCountResult(WinningCondition winningCondition) {
+        this.winningCondition = winningCondition;
+        this.conditionCount = 0;
+    }
+
+    public WinningCondition getWinningCondition() {
+        return winningCondition;
+    }
+
+    public int getConditionCount() {
+        return conditionCount;
+    }
+
+    public void addCount() {
+        this.conditionCount++;
+    }
 }
