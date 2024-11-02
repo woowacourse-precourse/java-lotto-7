47f23985f9adc50b02a577f3c3fd0d4fc94becae
chaerish <ccyy8432@naver.com>
2024-11-02 20:28:03 +0900

    test(LottoRateCalculatorTest): 로또 수익률을 계산하는 검증 테스트 생성

---
+++ src/test/java/lotto/service/LottoRateCalculatorTest.java
@@ -0,0 +1,23 @@
+package lotto.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lotto.MatchingCountResult;
+import lotto.constant.WinningCondition;
+import org.junit.jupiter.api.Test;
+
+class LottoRateCalculatorTest {
+    /*
+    수익률=(총수익 / 총 투자금액) x 100
+ )
+     */
+    @Test
+    void 수익률을_계산한다() {
+        MatchingCountResult matchingCountResult1 = new MatchingCountResult(WinningCondition.MATCH_3, 1);
+        List<MatchingCountResult> matchingCountResults = List.of(matchingCountResult1);
+        int amount = 8000;
+        double rate = LottoRateCalculator.calculateReturnOfRate(8000, matchingCountResults);
+        assertEquals(62.5, rate);
+    }
+}
