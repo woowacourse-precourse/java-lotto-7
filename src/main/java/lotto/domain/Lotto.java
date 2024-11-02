f9507f1d7fc1872e2edc5e99a871974f3875efd7
yybmion <yyb400@naver.com>
2024-11-02 20:43:02 +0900

    feat: 로또 생성 오름차순 정렬
    로또 생성하고 오름차순 정렬로 출력한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,12 +9,17 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        ascendingOrder();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void ascendingOrder() {
+        Collections.sort(numbers);
     }
 
     public List<Integer> getNumbers() {
