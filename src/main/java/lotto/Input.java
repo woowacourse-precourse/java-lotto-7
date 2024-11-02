0f42d899bed46462fff5f2f0a85388f4472089ce
KoYunHyuk <aes2036@daum.net>
2024-11-02 19:16:48 +0900

    refactor: Divide exception handling method - getWinNumbers, getBonusNumber

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -15,22 +15,29 @@ public class Input {
     }
 
     public static List<Integer> getWinNumbers() {
-        List<Integer> numbers;
-        while(true) {
-            try {
-                System.out.println("당첨 번호를 입력해 주세요.");
-                numbers = Arrays.stream(Console.readLine().split(","))
-                        .map(Integer::parseInt)
-                        .toList();
+        List<Integer> numbers = new ArrayList<>();
+        while (true) {
+            numbers = inputWinners();
+            break;
+        }
+        return numbers;
+    }
 
-                if(numbers.size() != 6) {
-                    throw new IllegalArgumentException();
-                }
+    public static List<Integer> inputWinners() {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            numbers = Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .toList();
 
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 당첨 번호는 6개를 입력해야 합니다. 다시 입력해주세요");
+            if (numbers.size() != 6) {
+                throw new IllegalArgumentException();
             }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 당첨 번호에는 숫자만 들어올 수 있습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
         }
         return numbers;
     }
@@ -38,17 +45,28 @@ public class Input {
     public static int getBonusNumber(List<Integer> winningNumbers) {
         int num = 0;
         while (true) {
-            try {
-                System.out.println("보너스 번호를 입력해 주세요");
-                num = Integer.parseInt(Console.readLine());
-                if (winningNumbers.contains(num)) {
-                    throw new IllegalStateException();
-                }
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 중복된 값이 입력되었습니다.");
-            }
+            num = inputBonusNumber(winningNumbers);
+            break;
         }
         return num;
     }
+
+    public static int inputBonusNumber(List<Integer> winningNumbers) {
+        int num = 0;
+
+        try {
+            System.out.println("보너스 번호를 입력해 주세요");
+            num = Integer.parseInt(Console.readLine());
+            if (winningNumbers.contains(num)) {
+                throw new IllegalStateException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 보너스 번호는 숫자만 입력할 수 있습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 중복된 값이 입력되었습니다.");
+        }
+
+        return num;
+    }
+
 }
