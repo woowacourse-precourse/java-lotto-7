2250e67b740152db107b6a4edc4631f9310b7d01
Park, minsu <cmwmss59@gmail.com>
2024-11-02 23:27:54 +0900

    feat: Lotto의 번호 중복 검증과 , 제한 범위에 값이 존재하는지 체크 하는 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -14,6 +16,22 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if(hasInvalidNumber(numbers))
+        {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다");
+        }
+        if(hasDuplicate(numbers))
+        {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.stream().distinct().count()!=numbers.size();
+    }
+
+    private boolean hasInvalidNumber(List<Integer> numbers) {
+        return numbers.stream().anyMatch(num->num<1 ||num>45);
     }
 
     public List<Integer> getNumbers() {
