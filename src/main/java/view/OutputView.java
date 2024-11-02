5900cd0aeb0c8081c7eaf97af67f4750622a8342
lsj0822 <lsj0822@khu.ac.kr>
2024-11-02 16:55:24 +0900

    refactor(OutputView) : OutputView 로직 수정
    OutputView의 로직을 parameter를 통한 결과를 출력하도록 수정하였습니다
    -기존의 로직은 parameter를 받지 않았지만, enum 객체에 따라서 다른 메서드를 만들어야하는 단점이 있습니다.
    -그렇기에 parameter를 통해서 같은 타입의 메시지를 출력해야한다면 객체를 전달하는 방식으로 수정하였습니다.
    -Controller를 통한 결과의 출력은 포맷팅이 필요한 경우가 있기 때문에 경우에 따라 parameter를 1개(enum객체), 2개(enum객체, Object객체)로 나누어 전달할 예정입니다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,8 +1,12 @@
 package view;
 
 public class OutputView {
-    
-    public static void printInputMoneyMessage() {
-        System.out.println(OutputMessage.INPUT_MONEY_MESSAGE);
+
+    public static void printInputMessage(OutputMessage message) {
+        System.out.println(message.getMessage());
+    }
+
+    public static void printErrorMessage(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
     }
 }
