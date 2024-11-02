9736baf3e575ab07a5b6166ac963b6296e2026a7
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 03:49:14 +0900

    feat(LottoController): 구입 금액에 따라 로또를 발행하는 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,13 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoCount;
 import lotto.domain.LottoPurchaseAmount;
+import lotto.domain.Lottos;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 import static lotto.view.InputView.readInput;
 
@@ -11,6 +17,12 @@ public class LottoController {
     public void run() {
         InputView.printPurchaseAmountPrompt();
         int purchaseAmount = LottoPurchaseAmount.from(readInput()).getPurchaseAmount();
+
+        int lottoCount = LottoCount.from(purchaseAmount).getLottoCount();
+        List<Lotto> lottos = Lottos.from(lottoCount).getLottos();
+        OutputView.printLottoPurchaseCount(lottoCount);
+        OutputView.printLottoListPrompt(lottos);
+
     }
 
 }
