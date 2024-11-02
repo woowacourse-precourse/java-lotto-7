146e595a15ff5ab282f2815bd621047b97c9488b
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 22:00:49 +0900

    feat(lotto number): 로또 번호의 정보를 저장하는 상수 클래스 추가

---
+++ src/main/java/lotto/model/lotto/LottoNumber.java
@@ -0,0 +1,11 @@
+package lotto.model.lotto;
+
+public final class LottoNumber {
+
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    public static final String FORMAT = "-?\\d+";
+    
+    private LottoNumber() {
+    }
+}
