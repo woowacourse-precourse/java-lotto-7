17cb160c141029699ac893dcffe6fe6c286ba0b5
Hyso <hsk7953@gmail.com>
2024-11-03 00:45:33 +0900

    feat : 6개의 번호를 가지고 있는 로또를 생성하는 LottoMachine 클래스 추가

---
+++ src/main/java/lotto/model/LottoMachine.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoMachine {
+    public Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+}
