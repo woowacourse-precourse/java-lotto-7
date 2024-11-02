2054c578a75f11c2cb961d1c3b14f728204e14d5
bee9827 <ehfrhfo9494@naver.com>
2024-11-02 20:57:24 +0900

    feat(LottoRankGroups): Get prize money, Get count
    enum타입인 List<LottoRank>를 관리하기 위한 일급 컬렉션.
    
    모든 당첨 금액을 가져온다.
    등수 해당하는 개수를 리턴한다.

---
+++ src/main/java/lotto/LottoRankGroups.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LottoRankGroups {
+    private final List<LottoRank> ranks;
+
+    public LottoRankGroups(List<LottoRank> ranks) {
+        this.ranks = ranks;
+    }
+
+    public Long getAllPrize() {
+        return ranks.stream()
+                .mapToLong(LottoRank::getPrice)
+                .sum();
+    }
+
+    public Long getCount(LottoRank targetRank) {
+        return filterCount(rank -> rank.equals(targetRank));
+    }
+
+    public Long getPrize(LottoRank targetRank) {
+        return filterPrize(rank -> rank.equals(targetRank));
+    }
+
+    private Long filterCount(Predicate<LottoRank> predicate) {
+        return ranks.stream()
+                .filter(predicate)
+                .count();
+    }
+
+    private Long filterPrize(Predicate<LottoRank> predicate) {
+        return ranks.stream()
+                .filter(predicate)
+                .mapToLong(LottoRank::getPrice)
+                .sum();
+    }
+}
