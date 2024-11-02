e4c42a7565fb44e929567b255f28b9e8900b5317
LeeDongHoon <likelion.leedh@gmail.com>
2024-11-02 16:09:44 +0900

    [FEAT] 로또 결과에 맞는 순위 반환 기능 추가 - 당첨되지 않는 NONE 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,25 +1,28 @@
 package lotto.model;
 
+import java.util.Arrays;
+
 public enum Rank {
 
+    NONE(0, false, 0),
     FIFTH_PLACE(3, false, 5_000),
     FOURTH_PLACE(4, false, 50_000),
     THIRD_PLACE(5, false, 1_500_000),
     SECOND_PLACE(5, true, 30_000_000),
     FIRST_PLACE(6, false, 2_000_000_000);
 
-    private final int matchedNumberCount;
+    private final int matchingCount;
     private final boolean containsBonusNumber;
     private final int winningPrice;
 
     Rank(int matchingCount, boolean containsBonusNumber, int winningPrice) {
-        this.matchedNumberCount = matchingCount;
+        this.matchingCount = matchingCount;
         this.containsBonusNumber = containsBonusNumber;
         this.winningPrice = winningPrice;
     }
 
     public int getMatchedNumberCount() {
-        return matchedNumberCount;
+        return matchingCount;
     }
 
     public boolean isContainsBonusNumber() {
@@ -28,5 +31,12 @@ public enum Rank {
 
     public int getWinningPrice() {
         return winningPrice;
+    }
+
+    public static Rank findRank(int matchingCount, boolean isBonusNumberMatches){
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.matchingCount == matchingCount && rank.containsBonusNumber == isBonusNumberMatches)
+                .findFirst()
+                .orElse(Rank.NONE);
     }
 }
