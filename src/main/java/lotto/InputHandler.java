ef01d1c9f8ada89e04746daa2d7bfcd9b9b3e92e
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-02 15:39:17 +0900

    feat(InputHandler): 입력값이 유효하지 않을 때 에러를 발생시키는 공통메소드 추가

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -2,4 +2,12 @@ package lotto;
 
 public abstract class InputHandler {
     public abstract void validateInput();
+
+    public void throwIllegalArgumentException(String message) {
+        try{
+            throw new IllegalArgumentException(message);
+        } catch(IllegalArgumentException e) {
+            System.out.println("[ERROR] "+e.getMessage());
+        }
+    }
 }
