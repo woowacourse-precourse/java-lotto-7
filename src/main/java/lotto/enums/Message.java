2193c574384ebb8389d3930e02bc832c33186171
변우영 <likevangogh24@gmail.com>
2024-11-02 18:02:43 +0900

    type(Message): 메시지마다 끝에 dot(.) 추가

--- src/main/java/lotto/enums/Message.java
+++ src/main/java/lotto/enums/Message.java
@@ -1,9 +1,9 @@
 package lotto.enums;
 
 public enum Message {
-    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요"),
-    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요"),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요"),
+    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     PURCHASE_SUFFIX("%s개를 구매했습니다."),
     TOTAL_PROFIT_RATE("총 수익률은 %s%%입니다.");
 
