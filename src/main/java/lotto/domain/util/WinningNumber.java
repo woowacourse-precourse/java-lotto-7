632e4352dbeba3097f859934091fc31515d83923
jaeyeong <zzz26lk@gmail.com>
2024-11-02 16:27:25 +0900

    feat: 당첨 번호 검증추가
    당첨 번호를 검증하기 위해서
    검증 로직을 사용하는 코드를 추가한다.

--- src/main/java/lotto/domain/util/WinningNumber.java
+++ src/main/java/lotto/domain/util/WinningNumber.java
@@ -2,12 +2,16 @@ package lotto.domain.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.validator.NumberValidator;
+import lotto.validator.Validator;
 
 public final class WinningNumber {
 
     private final static String DELIMITER = ",";
 
     public static List<Integer> create(String input) {
+        validate(input);
+
         List<Integer> winningNumbers = new ArrayList<>();
         String[] inputNumbers = input.split(DELIMITER);
         for (String inputNumber : inputNumbers) {
@@ -15,6 +19,11 @@ public final class WinningNumber {
             winningNumbers.add(winningNumber);
         }
         return winningNumbers;
+    }
+
+    private static void validate(String input) {
+        Validator<String> validator = new NumberValidator();
+        validator.validate(input);
     }
 
 }
