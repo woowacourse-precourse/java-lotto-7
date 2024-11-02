a90b5b9dc85f21c49ca6ec9c915b32b7a74992f0
BaSak0630 <babamba125@gmail.com>
2024-11-03 01:02:25 +0900

    test : Add WinningLottoTest
    당첨 로또 테스드 추가
    당첨 번호,보너스 번호 중복 검증 테스트
    로또와 당첨번호 비교 후 일치되는 숫자 개수 테스트 추가

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningLottoTest {
+    @Test
+    @DisplayName("당첨 번호, 보너스 중복 검증")
+    public void winningLottoTest() {
+        //given
+        WinningNumber winningNumber = new WinningNumber("1,2,3,4,5,6");
+        Bonus bonus = new Bonus("6");
+
+         //expect
+        assertThrows(IllegalArgumentException.class, () -> new WinningLotto(winningNumber, bonus));
+    }
+    @Test
+    @DisplayName("로또와 당첨번호 비교 후 일치되는 숫자 개수 테스트")
+    public void getMatchingCountTest() {
+        //given
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        WinningNumber winningNumber = new WinningNumber("1,2,3,4,5,6");
+        Bonus bonus = new Bonus("7");
+
+        WinningLotto lotto = new WinningLotto(winningNumber, bonus);
+        //when
+        int matchingCount = lotto.getMatchingCount(list);
+        //then
+        assertEquals(6, matchingCount);
+    }
+}
