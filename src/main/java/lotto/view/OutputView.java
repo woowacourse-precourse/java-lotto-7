97b36c655c67fd1ff503acd9ca10e020517fbd5d
Yun-tellije <dbswjddms517@naver.com>
2024-11-02 21:53:29 +0900

    feat: 당첨 통계 출력 메시지 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,5 @@ public class OutputView {
     public static final String NOTICE_PURCHASE_COUNT = "개를 구매했습니다.";
     public static final String INPUT_WINNING_NUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---";
 }
