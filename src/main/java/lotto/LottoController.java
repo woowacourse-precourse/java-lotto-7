e20893849072f384b0d30e190c7650f7defda588
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-11-02 17:50:10 +0900

    feat(view) : 구입한 로또의 리스트들을 출력한다.

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -7,6 +7,7 @@ public class LottoController {
     public void startGame() {
         int lottoPurchase = LottoInputView.lottoPurchaseAmount();
         List<Lotto> makePurchasedLottos = LottoFactory.createLottos(count(lottoPurchase));
+        LottoOutputView.printPurchasedLottoCount(makePurchasedLottos);
     }
 
     public int count (int lottoPurchase) {
