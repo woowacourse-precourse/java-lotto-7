d5872900ce7366a17ca0e7d7a9a2d9a442af3937
bk <naturalbb360@gmail.com>
2024-11-02 18:02:34 +0900

    fix(Message): 로또 게임 진행메세지 수정

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -4,15 +4,15 @@ public enum Message {
     GET_INPUT_MESSAGE("구입금액을 입력해 주세요."),
     GET_LOTTO_MESSAGE("개를 구매했습니다."),
     GET_WINNIG_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
-    GET_BONUS_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
-    RESULT_AVERAGE_MESSAGE("당첨 번호를 입력해 주세요.");
+    GET_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    RESULT_AVERAGE_MESSAGE("당첨 통계");
 
     private String message;
 
-    Message(String message) {this.message = message;}
+    Message(String message) { this.message = message;}
 
     public String getMessage() {
         return message;
-
     }
 }
+
