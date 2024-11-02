434ff045fb821331ef140ffc9b4e83154623fe49
배성연 <bsy309@ajou.ac.kr>
2024-11-02 16:15:45 +0900

    feat(PurchasingInput): 구입금액을 입력받고 형변환 및 예외처리 후 반환

---
+++ src/main/java/lotto/controller/PurchasingInput.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class PurchasingInput {
+
+    private PurchasingInput() {}
+
+    public static Integer getCost() {
+        System.out.println("구입 금액을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        Integer cost = makeNumber(input);
+
+        if (cost % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 올바른 숫자를 입력하세요");
+        }
+
+        return cost;
+    }
+
+    public static Integer makeNumber(String input){
+        try {
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 변환할 수 없는 값입니다.");
+            return 1;
+        }
+    }
+
+}
