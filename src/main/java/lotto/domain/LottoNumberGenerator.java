ae867df54e4f0625933a1575e4cd6093b71f2129
GO-TE <alsrudxo9810@naver.com>
2024-11-02 20:38:34 +0900

    refactor(LottoNumberGenerator): generateNumbers static으로 수정
    - 상태가 없는 클래스이므로 정적 메소드가 더 자연스러워 static으로 변경

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 import java.util.List;
 
 public class LottoNumberGenerator {
-    public List<Integer> generateNumbers() {
+    public static List<Integer> generateNumbers() {
         List<Integer> numbers = pickUniqueNumbersInRange(1, 45, 6);
         return numbers.stream()
                 .sorted()
