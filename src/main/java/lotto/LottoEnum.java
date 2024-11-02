f8ed9b7c125464427937eec2ec71d3af623d1bf0
KoYunHyuk <aes2036@daum.net>
2024-11-02 09:51:52 +0900

    feat: Implement Enum Class to print winningNumbers List

---
+++ src/main/java/lotto/LottoEnum.java
@@ -0,0 +1,55 @@
+package lotto;
+
+public enum LottoEnum {
+    FIRST(7, 2000000000, 0),
+    SECOND(6, 30000000, 0),
+    THIRD(5, 1500000, 0),
+    FOURTH(4, 50000, 0),
+    FIFTH(3, 5000, 0),
+    NONE(0, 0,0);
+
+
+    private final int matchCount;
+    private final int prizeAmount;
+    private int winnerCount;
+
+    LottoEnum(int matchCount, int prizeAmount, int winnerCount) {
+        this.matchCount = matchCount;
+        this.prizeAmount = prizeAmount;
+        this.winnerCount = winnerCount;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+
+    public int getWinnerCount() {
+        return winnerCount;
+    }
+
+    public static int getPrizeByMatchCount(int count) {
+        for(LottoEnum lotto : LottoEnum.values()) {
+            if(lotto.matchCount == count) {
+                return lotto.getPrizeAmount();
+            }
+        }
+        return 0;
+    }
+
+    public static void increaseWinnerCount(int count) {
+        for(LottoEnum lotto : LottoEnum.values()) {
+            if(lotto.matchCount == count) {
+                lotto.winnerCount++;
+            }
+        }
+    }
+
+    public static int getWinnerCount(int count) {
+        for(LottoEnum lotto : LottoEnum.values()) {
+            if(lotto.matchCount == count) {
+                return lotto.getWinnerCount();
+            }
+        }
+        return 0;
+    }
+}
