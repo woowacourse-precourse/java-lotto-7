3e9788d81e9bdfe6bc8d4380afa6186290db7032
Youngchan <wdc4512@naver.com>
2024-11-02 18:43:52 +0900

    test: 당첨번호 입력 및 예외처리 테스트 추가
    - 정상적인 구입 금액 입력에 대한 테스트 추가
    - 당첨 번호 입력 시 유효성 검사를 위한 예외 테스트 추가
      1. 숫자가 아닌 문자열이 포함된 경우
      2. 번호 개수가 6개가 아닌 경우
      3. 중복된 번호가 있는 경우
      4. 번호가 1~45 범위를 벗어난 경우

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -3,9 +3,6 @@ package lotto;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,9 +10,9 @@ class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
-    void 구매금액_정상입력_테스트() {
+    void 정상입력_테스트() {
         assertSimpleTest(()->{
-            run("8000");
+            run("8000", "1,2,3,4,5,6");
             assertThat(output()).contains(
                     "구입금액을 입력해주세요.",
                     "8개를 구매했습니다."
@@ -44,6 +41,51 @@ class ApplicationTest extends NsTest {
             );
         });
     }
+
+    @Test
+    void 당첨번호_예외_테스트_1() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,5,six");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ERROR] 유효한 숫자를 입력하세요."
+            );
+        });
+    }
+
+    @Test
+    void 당첨번호_예외_테스트_2() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,5,6,7");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ERROR] 로또 번호는 6개여야 합니다."
+            );
+        });
+    }
+
+    @Test
+    void 당첨번호_예외_테스트_3() {
+        assertSimpleTest(() -> {
+            run("8000", "1,2,3,4,4,5");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ERROR] 중복되지 않는 번호를 입력하세요."
+            );
+        });
+    }
+
+    @Test
+    void 당첨번호_예외_테스트_4() {
+        assertSimpleTest(() -> {
+            run("8000", "0,1,2,3,4,5");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE,
+                    "[ERROR] 1부터 45 사이의 숫자를 입력하세요."
+            );
+        });
+    }
+
 
 //    @Test
 //    void 기능_테스트() {
