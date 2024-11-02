d3121138b96f6bcb78816c7c00f55efb3c3b22b7
soeun2537 <soeun2537@gmail.com>
2024-11-02 14:12:56 +0900

    feat: LottoRandomUtil에 싱글톤 패턴을 적용하여 인스턴스 생성 제어 로직 추가

--- src/main/java/lotto/model/util/LottoRandomUtil.java
+++ src/main/java/lotto/model/util/LottoRandomUtil.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class LottoRandomUtil implements RandomUtil {
 
+    private static LottoRandomUtil lottoRandomUtil;
+
+    private LottoRandomUtil() {
+    }
+
+    public static LottoRandomUtil getLottoRandomUtil() {
+        if (lottoRandomUtil == null) {
+            lottoRandomUtil = new LottoRandomUtil();
+        }
+        return lottoRandomUtil;
+    }
+
     @Override
     public List<Integer> issueLottoTicket(Integer minNumber, Integer maxNumber, Integer lottoCount) {
         return Randoms.pickUniqueNumbersInRange(minNumber, maxNumber, lottoCount);
