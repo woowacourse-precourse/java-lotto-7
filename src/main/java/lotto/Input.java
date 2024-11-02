1876d6bcf96d8a5cf01ab2cc6529d28872e4059e
KoYunHyuk <aes2036@daum.net>
2024-11-02 18:26:15 +0900

    fix: Add handling exception in Input Class

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,6 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Input {
@@ -12,25 +14,40 @@ public class Input {
         return Integer.parseInt(Console.readLine());
     }
 
-    public static String getWinNumbers() {
-//        System.out.println("당첨 번호를 입력해 주세요.");
-        System.out.println("Input Win Numbers");
-        return Console.readLine();
+    public static List<Integer> getWinNumbers() {
+        List<Integer> numbers;
+        while(true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                numbers = Arrays.stream(Console.readLine().split(","))
+                        .map(Integer::parseInt)
+                        .toList();
+
+                if(numbers.size() != 6) {
+                    throw new IllegalArgumentException();
+                }
+
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 당첨 번호는 6개를 입력해야 합니다. 다시 입력해주세요");
+            }
+        }
+        return numbers;
     }
 
     public static int getBonusNumber(List<Integer> winningNumbers) {
-//        System.out.println("보너스 번호를 입력해 주세요");
-        System.out.println("Input Bonus Number");
         int num = 0;
-        boolean check = true;
-
-        while (check) {
-            num = Integer.parseInt(Console.readLine());
-            if (winningNumbers.contains(num)) {
-                System.out.println("Duplication Generate : " + num);
-                continue;
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요");
+                num = Integer.parseInt(Console.readLine());
+                if (winningNumbers.contains(num)) {
+                    throw new IllegalStateException();
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 중복된 값이 입력되었습니다.");
             }
-            check = false;
         }
         return num;
     }
