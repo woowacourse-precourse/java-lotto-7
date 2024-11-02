88736b3a31af796fe7d790b291555f73fa69d2af
DongYeop <s01054830455@gmail.com>
2024-11-02 18:48:57 +0900

    test: 총 수익률을 계산하는 기능 테스트

---
+++ src/test/java/lotto/model/LottoWinningResultTest.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.EnumMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class
+LottoWinningResultTest {
+
+    @DisplayName("총 수익률을 계산할 수 있다.")
+    @Test
+    void calculateRateOfReturn() {
+        //given
+        Map<LottoRank, Integer> lottoRank = new EnumMap<>(LottoRank.class);
+        lottoRank.put(LottoRank.FIFTH, 2);
+        lottoRank.put(LottoRank.FOURTH, 1);
+
+        LottoWinningResult lottoWinningResult = LottoWinningResult.from(lottoRank);
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(8000);
+
+        //when
+        double rateOfReturn = lottoWinningResult.calculateRateOfReturn(purchaseAmount);
+
+        //then
+        assertThat(rateOfReturn).isEqualTo(750.0);
+    }
+}
