8c64354f2cddae648fd42afbbe30d0fbebfacbdd
peanut990 <peanut1998@naver.com>
2024-11-03 01:23:02 +0900

    feat(Parser): 보너스 번호가 당첨 번호와 중복되는지 검증하는 기능

--- src/main/java/lotto/service/LottoWinningChecker.java
+++ src/main/java/lotto/service/LottoWinningChecker.java
@@ -2,9 +2,11 @@ package lotto.service;
 
 import java.util.HashSet;
 import java.util.List;
+import lotto.utils.Utils;
 
 public class LottoWinningChecker {
     private HashSet<Integer> winningNumbs;
+    private int bonusNumber;
 
     LottoWinningChecker() {
     }
@@ -13,8 +15,18 @@ public class LottoWinningChecker {
         winningNumbs = new HashSet<>(validatedWinningNumbs);
     }
 
+    public void saveBonusNumber(int bonusNumber) {
+        if (winningNumbs.contains(bonusNumber)) {
+            throw new IllegalArgumentException(Utils.makeErrorMessage("보너스 번호가 당첨번호와 중복됩니다."));
+        }
+        this.bonusNumber = bonusNumber;
+    }
+
     public HashSet<Integer> getWinningNumbs() {
         return winningNumbs;
     }
 
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
--- src/test/java/lotto/service/LottoWinningCheckerTest.java
+++ src/test/java/lotto/service/LottoWinningCheckerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoWinningCheckerTest {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
     public LottoWinningChecker lottoWinningChecker;
     @BeforeEach
     public void init() {
@@ -35,6 +36,31 @@ class LottoWinningCheckerTest {
         lottoWinningChecker.saveWinningNumbs(winningNumbs);
         //then
         Assertions.assertThat(lottoWinningChecker.getWinningNumbs()).contains(input);
+    }
+
+    @Test
+    public void 보너스번호_저장_테스트() throws Exception {
+        //given
+        List<Integer> winningNumbs = Parser.parseWinningNumbs("1,2, 3,4,5,6");
+        lottoWinningChecker.saveWinningNumbs(winningNumbs);
+        int bonusNumber = Parser.parseBonusNum("7");
+        //when
+        lottoWinningChecker.saveBonusNumber(bonusNumber);
+        //then
+        Assertions.assertThat(lottoWinningChecker.getBonusNumber()).isEqualTo(7);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2","3","4","5","6"})
+    public void 보너스번호_중복된_번호_예외테스트(String input) throws Exception {
+        //given
+        List<Integer> winningNumbs = Parser.parseWinningNumbs("1,2, 3,4,5,6");
+        lottoWinningChecker.saveWinningNumbs(winningNumbs);
+        int bonusNumber = Parser.parseBonusNum(input);
+        //when
+        Assertions.assertThatThrownBy(() -> lottoWinningChecker.saveBonusNumber(bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE+"보너스 번호가 당첨번호와 중복됩니다.");
     }
 
 }
