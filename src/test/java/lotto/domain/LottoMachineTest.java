b14fa5a4b35fe6361c668cb7b13f3a06259961c4
JiEung2 <181757@jnu.ac.kr>
2024-11-02 22:05:18 +0900

    test(lottoMachineTest): 생성된 로또 숫자가 오름차순으로 정렬되어 발행되는지 확인하는 테자스트

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +20,17 @@ class LottoMachineTest {
 
         List<Lotto> lottos = lottoMachine.buyLottos();
         assertThat(lottos).hasSize(5000 / LOTTO_PRICE);
+    }
+
+    @DisplayName("생성된 로또 숫자가 오름차순으로 정렬되어 발행되는지 확인하는 테스트")
+    @Test
+    void buyLottos_GeneratesLottosWithSortedNumbers() {
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+        List<Integer> lottoNumbers = lottoNumberGenerator.generate();
+        List<Integer> sortedNumbers = new ArrayList<>(lottoNumbers);
+        Collections.sort(sortedNumbers);
+
+        assertThat(lottoNumbers).isEqualTo(sortedNumbers);
     }
 }
