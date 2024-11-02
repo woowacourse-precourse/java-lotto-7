3bd57ecc0bd3539506ae18b8b8e5a9047784ece8
sinminseok12 <sinminseok12@naver.com>
2024-11-03 01:48:13 +0900

    refactor(InputValidatorTest) : 입력값 검증 테스트 코드 리팩토링
    입력값을 검증하는 테스트 코드에서 통합이 가능한 테스트를 통합했다.

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -9,28 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class InputValidatorTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {" ", ""})
-    void 구입금액에_공백이_있으면_예외가_발생한다(String input) {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"0", "-1000"})
+    @ValueSource(strings = {" ", "", "0", "-1000"})
     void 구입금액이_양의_정수가_아닐경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputValidator.validatePurchaseAmount(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {" ", ""})
-    void 당첨번호에_공백이_있으면_예외가_발생한다(String input) {
-        assertThatThrownBy(() -> InputValidator.validateWinNumbers(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1.2.3.4.5.6", "1,2,3,4,5,6,"})
+    @ValueSource(strings = {" ", "", "1.2.3.4.5.6", "1,2,3,4,5,6,"})
     void 당첨번호를_구분자로_구분할수_없으면_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputValidator.validateWinNumbers(input))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -44,14 +30,7 @@ public class InputValidatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {" ", ""})
-    void 보너스_번호에_공백이_있으면_예외가_발생한다(String input) {
-        assertThatThrownBy(() -> InputValidator.validateBonusNumber(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"0", "-1000"})
+    @ValueSource(strings = {" ", "", "0", "-1000"})
     void 보너스_번호가_양의_정수가_아닐경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputValidator.validateBonusNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
