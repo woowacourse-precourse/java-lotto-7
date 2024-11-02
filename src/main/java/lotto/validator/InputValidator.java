97e48c1c8e0655d069d74a7b406c3b92ce127284
gongPyeon <pyeonkang@naver.com>
2024-11-02 13:23:26 +0900

    feat(InputValidator): 로또 구입 금액이 숫자인지 검증하는 기능 추가

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,16 +1,24 @@
 package lotto.validator;
 
 public class InputValidator {
-    public int validate(String lottoPrice) {
-        validateEmpty(lottoPrice);
-        validateForm(lottoPrice);
-        int lottoPrice = validateNumber(lottoPrice);
+    public int validate(String lottoPriceString) {
+        validateEmpty(lottoPriceString);
+        int lottoPrice = validateNumber(lottoPriceString);
         
         return lottoPrice;
     }
 
-    private void validateEmpty(String lottoPrice) {
-        if(lottoPrice == null || lottoPrice.isEmpty())
+    private int validateNumber(String lottoPriceString) {
+        try {
+            int lottoPrice = Integer.parseInt(lottoPriceString);
+            return lottoPrice;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력받은 문자열을 숫자로 변환할 수 없습니다.");
+        }
+    }
+
+    private void validateEmpty(String lottoPriceString) {
+        if(lottoPriceString == null || lottoPriceString.isEmpty())
             throw new IllegalArgumentException("[ERROR] 입력받은 문자열이 빈문자열 또는 null입니다.");
     }
 
