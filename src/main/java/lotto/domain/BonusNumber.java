d948cc37313fec6252ecb7f24a6bff1a8b4c86ae
gladhee <gladhee@icloud.com>
2024-11-02 18:32:05 +0900

    feat(BonusNumber): 입력 보너스 넘버 도메인 작성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private final int number;
+
+    private BonusNumber(String inputNumber) {
+        int number = validateType(inputNumber);
+        validateRange(number);
+        this.number = number;
+    }
+
+    public static BonusNumber of(String inputNumber) {
+        return new BonusNumber(inputNumber);
+    }
+
+    private int validateType(String inputNumber) {
+        try {
+            return Integer.parseInt(inputNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수여야 합니다.");
+        }
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+}
