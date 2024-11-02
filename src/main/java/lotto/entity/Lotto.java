54d5d87f8c26fe5f23de1b305d48b7d937405226
yeong-hwan <jghff700@naver.com>
2024-11-03 00:15:52 +0900

    refactor(entity): append duplication validation

---
+++ src/main/java/lotto/entity/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.entity;
+
+import lotto.enums.ErrorMessage;
+import lotto.enums.LottoConstants;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    /**
+     * 입력받은 번호 리스트의 유효성을 검사합니다.
+     * - 번호는 6개여야 하며, 중복이 없어야 합니다.
+     * - 각 번호는 1에서 45 사이여야 합니다.
+     *
+     * @param numbers 로또 번호 리스트
+     */
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LottoConstants.LOTTO_NUMBERS_COUNT.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.getMessage());
+        }
+
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
+    }
+}
