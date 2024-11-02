0059b2b31d2fa99789b590ea6099e8afcecdce20
corpi <lj845854@gmail.com>
2024-11-02 17:13:54 +0900

    refactor(InputWinningNumberTest): 불필요한 테스트 코드 제거

--- src/test/java/lotto/model/InputValidatorTest/InputWinningNumberTest.java
+++ src/test/java/lotto/model/InputValidatorTest/InputWinningNumberTest.java
@@ -15,24 +15,6 @@ public class InputWinningNumberTest {
         inputValidator = new InputValidator();
     }
 
-    @DisplayName("당첨 번호가 6개가 아닐 경우 에러가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"'1'", "'1,2'", "'1,2,3'", "'1,2,3,4'", "'1,2,3,4,5'"})
-    public void 당첨_번호가_6개가_아닐_경우_에러가_발생한다(String input) {
-        Assertions.assertThatThrownBy(() -> {
-            inputValidator.validateInputWinningNumber(input);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("당첨 번호가 중복될 경우 에러가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"'1,2,2,3,4,5'", "'1,1,1,1,1,1'", "'29,30,31,32,33,30'"})
-    public void 당첨_번호가_중복될_경우_에러가_발생한다(String input) {
-        Assertions.assertThatThrownBy(() -> {
-            inputValidator.validateInputWinningNumber(input);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("당첨 번호의 범위가 1에서 45가 아닐 경우 에러가 발생한다.")
     @ParameterizedTest
     @CsvSource(value = {"'0,2,3,5,46,99'", "'99,999,9999,99999,999999,9999999'"})
