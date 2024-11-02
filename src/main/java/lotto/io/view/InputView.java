cdbaff02a7e068f526baaabe9375902b6d7d18f5
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-02 17:06:50 +0900

    refactor(input): 불필요한 import문 제거, 공백 라인 수정

--- src/main/java/lotto/io/view/InputView.java
+++ src/main/java/lotto/io/view/InputView.java
@@ -1,9 +1,6 @@
 package lotto.io.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import lotto.io.InputValidator;
 import lotto.domain.Lotto;
 import lotto.domain.BonusNumber;
@@ -14,13 +11,13 @@ public class InputView {
     private static final String AMOUNT_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String INPUT_DELIM = ",";
 
     private final InputValidator validator = new InputValidator();
 
     public Money getAmountOfMoney() {
         System.out.println(AMOUNT_INPUT_GUIDE_MESSAGE);
         Money money;
+
         while (true) {
             try {
                 String input = Console.readLine();
@@ -30,12 +27,14 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
+
         return money;
     }
 
     public Lotto getWinningNumbers() {
         System.out.println(WINNING_NUMBERS_INPUT_GUIDE_MESSAGE);
         Lotto winningNumbers;
+
         while (true) {
             try {
                 String input = Console.readLine();
@@ -45,12 +44,14 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
+
         return winningNumbers;
     }
 
     public BonusNumber getBonusNumber() {
         System.out.println(BONUS_NUMBER_INPUT_GUIDE_MESSAGE);
         BonusNumber bonusNumber;
+
         while (true) {
             try {
                 String input = Console.readLine();
@@ -60,6 +61,7 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
+
         return bonusNumber;
     }
 }
