f887a2f452faeea6341b739ae63adfc566fb27f4
bill291104 <bill29123@gmail.com>
2024-11-02 18:12:49 +0900

    test(PrizeTest): add test for Prize.getPrize
    - Add getPrize functionality test to check prize evaluation based on matching numbers and bonus

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class PrizeTest {
+class PrizeTest extends NsTest {
     @ParameterizedTest
     @EnumSource(Prize.class)
     @DisplayName("당첨_기준과_금액_출력_테스트")
@@ -15,23 +18,46 @@ class PrizeTest {
     }
 
     private String expected(Prize prize) {
-        //why can't I use switch/case?
-        //how to handle "no return statement" without "else"
+        //why can't I use switch/case or else?
+        //how to handle "no return statement"
         if (prize == Prize.FIRST) {
-            return "1등: 6개 번호 일치 / 2,000,000,000원";
+            return "6개 일치 (2,000,000,000원)";
         }
         if (prize == Prize.SECOND) {
-            return "2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원";
+            return "5개 일치, 보너스 볼 일치 (30,000,000원)";
         }
         if (prize == Prize.THIRD) {
-            return "3등: 5개 번호 일치 / 1,500,000원";
+            return "5개 일치 (1,500,000원)";
         }
         if (prize == Prize.FOURTH) {
-            return "4등: 4개 번호 일치 / 50,000원";
+            return "4개 일치 (50,000원)";
         }
         if (prize == Prize.FIFTH) {
-            return "5등: 3개 번호 일치 / 5,000원";
+            return "3개 일치 (5,000원)";
         }
         return "";
+    }
+
+    @DisplayName("기능_테스트_getPrize")
+    @Test
+    void 기능_테스트_getPrize() {
+        Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto2 = new Lotto(List.of(1,2,3,4,5,7));
+        Lotto lotto3 = new Lotto(List.of(1,2,3,4,5,8));
+        Lotto lotto4 = new Lotto(List.of(1,2,3,4,7,8));
+        Lotto lotto5 = new Lotto(List.of(1,2,3,7,8,9));
+        Lotto lottoNo = new Lotto(List.of(1,2,7,8,9,10));
+        WinningLotto winningLotto = new WinningLotto(List.of(1,2,3,4,5,6),7);
+        assertThat(Prize.getPrize(lotto1, winningLotto)).isEqualTo(Prize.FIRST);
+        assertThat(Prize.getPrize(lotto2, winningLotto)).isEqualTo(Prize.SECOND);
+        assertThat(Prize.getPrize(lotto3, winningLotto)).isEqualTo(Prize.THIRD);
+        assertThat(Prize.getPrize(lotto4, winningLotto)).isEqualTo(Prize.FOURTH);
+        assertThat(Prize.getPrize(lotto5, winningLotto)).isEqualTo(Prize.FIFTH);
+        assertThat(Prize.getPrize(lottoNo, winningLotto)).isNull();
+    }
+
+    @Override
+    protected void runMain() {
+
     }
 }
