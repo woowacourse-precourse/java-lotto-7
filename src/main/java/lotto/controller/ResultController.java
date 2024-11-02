ece7be53e647c87fe688d1a6eccb3764b03f562b
making-a-scene <4936joo@naver.com>
2024-11-02 13:02:27 +0900

    feat(ResultController.calculateEarningRatio()): 수익률 연산 기능 추가

--- src/main/java/lotto/controller/ResultController.java
+++ src/main/java/lotto/controller/ResultController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultController {
+    private final int PERCENTAGE = 100;
+    private final String ROUNDING_OFF_FORMAT = "%.1f";
 
     public Map<Prize, Integer> makeWinningStatistic(List<Prize> prizeStatus) {
         Map<Prize, Integer> result = initMap();
@@ -28,5 +30,14 @@ public class ResultController {
             result.put(prize, 0);
         }
         return result;
+    }
+
+    public String calculateEarningRatio(Map<Prize, Integer> statistic, int buyingAmount) {
+        float sum = 0f;
+        for (Prize prize : statistic.keySet()) {
+            sum += statistic.get(prize) * prize.getAmount();
+        }
+        float ratio = sum / buyingAmount * PERCENTAGE;
+        return String.format(ROUNDING_OFF_FORMAT, ratio);
     }
 }
