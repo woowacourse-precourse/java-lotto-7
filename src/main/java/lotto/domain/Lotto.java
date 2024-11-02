2a77325216409bbbdae8d2c2862eed6b21c7fc0f
KIM MINSEOK <mskim.glenn@gmail.com>
2024-11-03 01:22:30 +0900

    refactor: 검증 로직을 Validator 클래스로 이동하여 책임 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,7 @@
 package lotto.domain;
 
-import lotto.constants.LottoConstants;
+import lotto.util.Validator;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -10,23 +9,8 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validator.validateLottoNumbers(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LottoConstants.NUMBER_OF_LOTTO) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + LottoConstants.NUMBER_OF_LOTTO + "개여야 합니다.");
-        }
-        for (Integer number : numbers) {
-            if (number < LottoConstants.RANDOM_LOWER_BOUND || number > LottoConstants.RANDOM_UPPER_BOUND) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 "
-                        + LottoConstants.RANDOM_LOWER_BOUND + "부터 " + LottoConstants.RANDOM_UPPER_BOUND + " 사이의 숫자여야 합니다.");
-            }
-        }
-        if (numbers.size() != new HashSet<>(numbers).size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        }
     }
 
     @Override
--- src/main/java/lotto/util/LottoConverter.java
+++ src/main/java/lotto/util/LottoConverter.java
@@ -4,20 +4,8 @@ import lotto.constants.LottoConstants;
 
 public class LottoConverter {
 
-    private static final String UNDER_PRICE_ERROR = "[ERROR] 최소 구입 금액은 " + LottoConstants.BASE_PRICE + "원 이상이어야 합니다.";
-    private static final String UNIT_ERROR = "[ERROR] 금액은 " + LottoConstants.BASE_PRICE + "원 단위여야 합니다.";
-
     public int convertPriceToLotto(int price) {
-        validatePrice(price);
+        Validator.validatePrice(price);
         return price / LottoConstants.BASE_PRICE;
-    }
-
-    private void validatePrice(int price) {
-        if (price < LottoConstants.BASE_PRICE) {
-            throw new IllegalArgumentException(UNDER_PRICE_ERROR);
-        }
-        if (price % LottoConstants.BASE_PRICE != 0) {
-            throw new IllegalArgumentException(UNIT_ERROR);
-        }
     }
 }
