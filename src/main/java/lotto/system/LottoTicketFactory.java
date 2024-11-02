cc44f01d3189851c87694367cbf02a28d7fb4642
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-02 11:08:43 +0900

    feat: 로또 티켓 발행 기능 구현

---
+++ src/main/java/lotto/system/LottoTicketFactory.java
@@ -0,0 +1,30 @@
+package lotto.system;
+
+import static lotto.user.LottoNumber.LOTTO_NUMBER_LOWER_BOUND;
+import static lotto.user.LottoNumber.LOTTO_NUMBER_UPPER_BOUND;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoTicketFactory { // 로또를 사면 로또 번호를 생성하는 객체
+
+    private static final int VALID_SIZE = 6;
+
+    public static List<List<Integer>> generateTickets(int quantity) {
+        return IntStream
+                .range(0, quantity)
+                .mapToObj(i -> generateSingleTicket())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(quantity)));
+    }
+
+    public static List<Integer> generateTicket() {
+        return generateSingleTicket();
+    }
+
+    static List<Integer> generateSingleTicket() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_LOWER_BOUND, LOTTO_NUMBER_UPPER_BOUND, VALID_SIZE);
+    }
+}
---
+++ src/test/java/lotto/system/LottoTicketFactoryTest.java
@@ -0,0 +1,46 @@
+package lotto.system;
+
+import static lotto.user.LottoNumber.LOTTO_NUMBER_LOWER_BOUND;
+import static lotto.user.LottoNumber.LOTTO_NUMBER_UPPER_BOUND;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketFactoryTest {
+
+    @Test
+    @DisplayName("로또 티켓의 개수가 주어진 수량과 일치하는지 테스트")
+    void generate_createsCorrectNumberOfTickets() {
+
+        // given
+        int quantity = 5;
+
+        // when
+        List<List<Integer>> lottoTickets = LottoTicketFactory.generateTickets(quantity);
+
+        // then
+        assertEquals(quantity, lottoTickets.size(), "생성된 로또 티켓의 수가 일치하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("생성된 로또 번호가 중복이 없고 유효 범위 내에 있는지 테스트")
+    void generateLotto_createsUniqueNumbersWithinRange() {
+
+        //when
+        List<Integer> lottoNumbers = LottoTicketFactory.generateSingleTicket();
+
+        // then1 : 로또 번호의 개수가 6개인지 확인
+        assertEquals(6, lottoNumbers.size(), "로또 번호의 개수가 6개가 아닙니다.");
+
+        // then2 : 중복이 없는지 확인
+        assertEquals(new HashSet<>(lottoNumbers).size(), lottoNumbers.size(), "로또 번호에 중복이 있습니다.");
+
+        // then3 : 각 번호가 지정된 범위 내에 있는지 확인
+        assertTrue(lottoNumbers.stream().allMatch(
+                        num -> num >= LOTTO_NUMBER_LOWER_BOUND && num <= LOTTO_NUMBER_UPPER_BOUND),
+                "로또 번호가 유효한 범위를 벗어났습니다.");
+    }
+}
