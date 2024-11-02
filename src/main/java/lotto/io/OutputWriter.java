234d255e7f66b4a909cfd1c0beaccd51f2381003
woong2e <tlswodnd12345@naver.com>
2024-11-02 20:15:35 +0900

    fix: correct output form

--- src/main/java/lotto/io/OutputWriter.java
+++ src/main/java/lotto/io/OutputWriter.java
@@ -6,13 +6,18 @@ import java.util.List;
 
 public class OutputWriter {
 
-    public void write(List<Lotto> lottes) {
+    public void write(List<Lotto> lottos) {
         StringBuilder sb = new StringBuilder();
 
-        for (Lotto lotte : lottes) {
+        sb.append("\n").append(lottos.size()).append("개를 구매했습니다.").append("\n");
+        for (Lotto lotte : lottos) {
             sb.append(lotte.toString()).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    public void printStatistics() {
+
     }
 }
