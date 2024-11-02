81907e9329fba3eda16bc254fa799cb039816da1
woong2e <tlswodnd12345@naver.com>
2024-11-02 17:58:50 +0900

    test: add test for store lotto

--- src/test/java/lotto/vendingmachine/VendingMachineServiceImplTest.java
+++ src/test/java/lotto/vendingmachine/VendingMachineServiceImplTest.java
@@ -1,10 +1,12 @@
 package lotto.vendingmachine;
 
 import lotto.AppConfig;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,8 +31,8 @@ class VendingMachineServiceImplTest {
                 () -> {
                     List<Lotto> lottos = vendingMachineService.generate(3);
                     Set<CharSequence> output = new HashSet<>();
-                    for (int i = 0; i < 3; i++) {
-                        output.add(lottos.get(i).toString());
+                    for (Lotto lotto : lottos) {
+                        output.add(lotto.toString());
                     }
 
                     assertThat(output).contains(
@@ -42,6 +44,25 @@ class VendingMachineServiceImplTest {
                 List.of(8, 21, 23, 41, 42, 43),
                 List.of(3, 5, 11, 16, 32, 38),
                 List.of(7, 11, 16, 35, 36, 44)
+        );
+    }
+
+    @Test
+    @DisplayName("로또 저장 테스트")
+    void 로또_저장_테스트() {
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1,2,3,4,5,6)));
+        lottos.add(new Lotto(List.of(7,8,9,10,11,12)));
+
+        Set<CharSequence> output = new HashSet<>();
+        vendingMachineService.recordLottos(lottos);
+        for (Lotto lotto : vendingMachineService.getIssuedLottoTickets()) {
+            output.add(lotto.toString());
+        }
+
+        assertThat(output).contains(
+                "[1, 2, 3, 4, 5, 6]",
+                "[7, 8, 9, 10, 11, 12]"
         );
     }
 }
