b58b3610bce5cd02f6e84684375da56b28b9c937
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 03:56:33 +0900

    refactor(ViewMessage): 당첨 번호 입력을 요구하는 메시지에 개행 추가

--- src/main/java/lotto/constants/ViewMessage.java
+++ src/main/java/lotto/constants/ViewMessage.java
@@ -3,7 +3,7 @@ package lotto.constants;
 public enum ViewMessage {
 
     PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
     BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     LOTTO_PURCHASE_COUNT("\n%d개를 구매했습니다.");
 
