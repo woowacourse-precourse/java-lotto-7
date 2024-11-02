58047d61429269ce8fe40067e4a5a7099223a38d
SeongbinPark <hyucse22@gmail.com>
2024-11-02 18:51:44 +0900

    refactor(BonusNumber): 입력받은 보너스번호 전처리 과정을 별도 클래스에서 관리하도록 변경

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
+import lotto.constants.ErrorMessages;
+import lotto.constants.LottoConstants;
+
 public class BonusNumber {
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber, WinningNumbers winningNumbers) {
+        validateBonusNumber(bonusNumber, winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber, WinningNumbers winningNumbers) {
+        if (bonusNumber < LottoConstants.LOTTO_MIN_NUMBER || bonusNumber > LottoConstants.LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE);
+        }
+        if (winningNumbers.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessages.BONUS_NUMBER_DUPLICATE);
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
