d41b0e24ad09a80d5b970c7e4fb6a5025ee91c37
JoonKyo <joonkyolee@gmail.com>
2024-11-03 00:15:13 +0900

    feat(Lotto): 중복된 수가 있는지 확인하는 메서드 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,11 +15,24 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateLottoSize(numbers);
+        validateDuplicatedNumber(numbers);
     }
 
     private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw InputException.from(ErrorMessage.LOTTO_SIZE_IS_NOT_VALID);
         }
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        if (findLottoSizeWithoutDuplicated(numbers) != LOTTO_SIZE) {
+            throw InputException.from(ErrorMessage.LOTTO_NUMBER_HAS_DUPLICATED_NUMBER);
+        }
+    }
+
+    private int findLottoSizeWithoutDuplicated(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .distinct()
+                .count();
     }
 }
