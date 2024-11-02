5e415d2cebdbb00d7802ce0c04df393235a4fbfd
sik9252 <sik9252@naver.com>
2024-11-02 17:37:10 +0900

    refactor: 메서드, 변수명 변경

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -3,60 +3,65 @@ package lotto.model;
 import lotto.exception.InvalidWinningNumbersException;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import static lotto.exception.ErrorMessage.DUPLICATE_NUMBER_IS_NOT_ALLOWED;
 import static lotto.exception.ErrorMessage.INVALID_NUMBER_RANGE;
+import static lotto.exception.ErrorMessage.INVALID_WINNING_NUMBERS_FORMAT;
 import static lotto.model.Lotto.MAX_LOTTO_NUMBER;
 import static lotto.model.Lotto.MIN_LOTTO_NUMBER;
 
 public class WinningNumber {
-    private List<Integer> winningNumbers;
+    private List<Integer> winningNumber;
 
-    public WinningNumber(String winningString) {
-        createLottoByWinningString(winningString);
+    public WinningNumber(String input) {
+        convertInputToWinningNumber(input);
     }
 
     public List<Integer> get() {
-        return winningNumbers;
+        return winningNumber;
     }
 
-    private void createLottoByWinningString(String winningString) {
-        List<Integer> createdLotto = processingWinningNumbers(winningString);
-        checkEachLottoNumberHasValidate(createdLotto);
+    private void convertInputToWinningNumber(String input) {
+        checkHasValidInputFormat(input);
+        List<Integer> createdLotto = processingWinningNumber(input);
+        checkEachNumberHasValidate(createdLotto);
 
-        winningNumbers = createdLotto;
+        winningNumber = createdLotto;
     }
 
-    private List<Integer> processingWinningNumbers(String winningString) {
-        List<String> splitStringByComma = List.of(winningString.split(","));
-        List<Integer> processedWinningNumbers = new ArrayList<>();
-
-        for (String string : splitStringByComma) {
-            processedWinningNumbers.add(Integer.parseInt(string.trim()));
-        }
-
-        return processedWinningNumbers;
-    }
-
-    private void checkEachLottoNumberHasValidate(List<Integer> winningNumbers) {
-        HashSet<Integer> numbers = new HashSet<>();
-
-        for (int number : winningNumbers) {
-            checkNumberHasValidRange(number);
-            checkNumberIsDuplicate(numbers, number);
+    private void checkHasValidInputFormat(String input) {
+        if (!input.matches("^-?\\d+(,-?\\d+){5}")) {
+            throw new InvalidWinningNumbersException(INVALID_WINNING_NUMBERS_FORMAT.getMessage());
         }
     }
 
-    private void checkNumberHasValidRange(int number) {
-        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-            throw new InvalidWinningNumbersException(INVALID_NUMBER_RANGE.getMessage());
+    private List<Integer> processingWinningNumber(String input) {
+        List<String> splitInputByComma = List.of(input.split(","));
+        List<Integer> processedWinningNumber = new ArrayList<>();
+
+        for (String string : splitInputByComma) {
+            processedWinningNumber.add(Integer.parseInt(string.trim()));
+        }
+
+        return processedWinningNumber;
+    }
+
+    private void checkEachNumberHasValidate(List<Integer> winningNumber) {
+        checkNumberHasDuplicate(winningNumber);
+        checkEachNumberHasValidRange(winningNumber);
+    }
+
+    private void checkEachNumberHasValidRange(List<Integer> winningNumber) {
+        for (int number : winningNumber) {
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+                throw new InvalidWinningNumbersException(INVALID_NUMBER_RANGE.getMessage());
+            }
         }
     }
 
-    private void checkNumberIsDuplicate(HashSet<Integer> numbers, int number) {
-        if (!numbers.add(number)) {
+    private void checkNumberHasDuplicate(List<Integer> winningNumber) {
+        if (winningNumber.size() != winningNumber.stream().distinct().count()) {
             throw new InvalidWinningNumbersException(DUPLICATE_NUMBER_IS_NOT_ALLOWED.getMessage());
         }
     }
