c0e4b95c16adc06881fef4e56b4499a97a46c509
kungbi <kipper12335@gmail.com>
2024-11-02 12:31:57 +0900

    test(ConsoleInputView): 테스트 코드 수정 (MockInputProvider 적용)

--- src/test/java/lotto/view/ConsoleInputTest.java
+++ src/test/java/lotto/view/ConsoleInputTest.java
@@ -1,74 +1,72 @@
 package lotto.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
+import lotto.view.InputProvider.MockInputProvider;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ConsoleInputTest {
 
-    private final InputStream originalIn = System.in;
-    private ConsoleInput consoleInput;
-
-    @BeforeEach
-    void setUp() {
-        consoleInput = new ConsoleInput();
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setIn(originalIn);
-        consoleInput.close();
-    }
-
     @Nested
     class 구입금액_입력_테스트 {
 
         @Test
-        void 구입금액_정상_입력() {
-            String input = "1000\n";
-            System.setIn(new ByteArrayInputStream(input.getBytes()));
+        void 정상__구입금액_입력() {
+            // given
+            List<String> inputs = List.of("1000");
+            String expect = "1000";
 
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
             String result = consoleInput.getPurchasedAmount();
 
-            assertThat(result).isEqualTo("1000");
+            // then
+            assertThat(result).isEqualTo(expect);
         }
 
         @Test
-        void 구입금액_입력_실패() {
-            System.setIn(new ByteArrayInputStream("".getBytes()));
+        void 정상__구입금액_빈문자열() {
+            List<String> inputs = List.of("");
+            String expect = "";
 
-            assertThatThrownBy(() -> consoleInput.getPurchasedAmount())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("입력을 받는데 실패했습니다.");
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
+            String result = consoleInput.getPurchasedAmount();
+
+            assertThat(result).isEqualTo(expect);
         }
+
     }
 
     @Nested
     class 당첨번호_입력_테스트 {
 
         @Test
-        void 당첨번호_정상_입력() {
-            String input = "1, 2, 3, 4, 5, 6\n";
-            System.setIn(new ByteArrayInputStream(input.getBytes()));
+        void 정상__당첨번호_입력() {
+            List<String> inputs = List.of("1, 2, 3, 4, 5, 6");
+            String expect = "1, 2, 3, 4, 5, 6";
 
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
             String result = consoleInput.getWinnerNumbers();
 
-            assertThat(result).isEqualTo("1, 2, 3, 4, 5, 6");
+            // then
+            assertThat(result).isEqualTo(expect);
         }
 
         @Test
-        void 당첨번호_입력_실패() {
-            System.setIn(new ByteArrayInputStream("".getBytes()));
+        void 정상__당첨번호_빈문자열() {
+            List<String> inputs = List.of("");
+            String expect = "";
 
-            assertThatThrownBy(() -> consoleInput.getWinnerNumbers())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("입력을 받는데 실패했습니다.");
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
+            String result = consoleInput.getWinnerNumbers();
+
+            // then
+            assertThat(result).isEqualTo(expect);
         }
     }
 
@@ -76,22 +74,29 @@ public class ConsoleInputTest {
     class 보너스번호_입력_테스트 {
 
         @Test
-        void 보너스번호_정상_입력() {
-            String input = "7\n";
-            System.setIn(new ByteArrayInputStream(input.getBytes()));
+        void 정상__보너스번호_입력() {
+            List<String> inputs = List.of("1");
+            String expect = "1";
 
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
             String result = consoleInput.getBonusNumber();
 
-            assertThat(result).isEqualTo("7");
+            // then
+            assertThat(result).isEqualTo(expect);
         }
 
         @Test
-        void 보너스번호_입력_실패() {
-            System.setIn(new ByteArrayInputStream("".getBytes()));
+        void 정상__보너스번호_입력_빈문자열() {
+            List<String> inputs = List.of("1");
+            String expect = "1";
 
-            assertThatThrownBy(() -> consoleInput.getBonusNumber())
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("입력을 받는데 실패했습니다.");
+            // when
+            ConsoleInput consoleInput = new ConsoleInput(new MockInputProvider(inputs));
+            String result = consoleInput.getBonusNumber();
+
+            // then
+            assertThat(result).isEqualTo(expect);
         }
     }
 }
