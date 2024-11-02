901c2ed6ee56b6f53f9c42a7c6866b0926f40ffa
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-11-02 15:18:25 +0900

    feat: PurchaseValidator
    구입금액 유효성 검사하는 클래스 추가

--- src/main/java/lotto/PurchaseValidator.java
+++ src/main/java/lotto/PurchaseValidator.java
@@ -1,5 +1,11 @@
 package lotto;
 
 public class PurchaseValidator {
+    private static final int LOTTO_PRICE = 1000;
 
+    public static void validate(int amount) {
+        if (amount <= 0 || amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해 주세요.");
+        }
+    }
 }
