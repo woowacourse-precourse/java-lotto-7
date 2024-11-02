7f66b5f4fd89a8cd07e6849dc1e1d58e27344749
sejinezy <psj8880@naver.com>
2024-11-02 16:07:27 +0900

    refactor(Input): 메서드 반환형 수정
    메서드 호출 부분의 코드 가독성을 위해 검증 메서드의 반환형을 수정함

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -9,24 +9,27 @@ public class Input {
         return Console.readLine();
     }
 
-    public static void validateNotEmpty(String input) {
+    public static String validateNotEmpty(String input) {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 입력값이 비어 있습니다.");
         }
+        return input;
     }
 
-    public static void validateIsNumber(String input) {
+    public static String validateIsNumber(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
         }
+        return input;
     }
 
-    public static void validateDivisibleByThousand(int amount) {
+    public static int validateDivisibleByThousand(int amount) {
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
+        return amount;
     }
 
     public static int toInt(String input) {
