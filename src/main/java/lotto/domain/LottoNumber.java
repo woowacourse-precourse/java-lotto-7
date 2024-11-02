2737d827b0c0f6a4cff117a85b3430b905496147
jae woo Jung <awfjol2008@gmail.com>
2024-11-02 19:23:41 +0900

    feat: 로또 번호가 유요한 범위인지 검증하는 기능 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,6 +7,17 @@ public class LottoNumber {
 	private final int number;
 
 	public LottoNumber(int number) {
+		validateNumber(number);
 		this.number = number;
+	}
+
+	private void validateNumber(int number) {
+		if (isNumberOutOfRange(number)) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위의 정수여야 합니다.");
+		}
+	}
+
+	private static boolean isNumberOutOfRange(int number) {
+		return number < MIN_NUMBER || number > MAX_NUMBER;
 	}
 }
