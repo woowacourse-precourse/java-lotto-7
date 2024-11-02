6c7dd4512a06668031f6d50c9dadc0ee734a3160
Ahnsukjoo <bos98@naver.com>
2024-11-03 06:45:28 +0900

    feat: LottoNumberStatistics 클래스 구현
    LottoNumberStatistics 클래스를 추가하여 로또 번호 통계 기능을 구현했습니다.
    
    - 로또 티켓과 당첨 번호를 비교하여 당첨 결과를 확인하는 checkWinner 메서드 추가했습니다.
    - 로또 등급을 관리하는 LottoRank enum을 사용하여 당첨 통계를 처리했습니다.
    - 총 반환율을 계산하고 결과를 반환하는 기능 구현했습니다.

--- src/main/java/lotto/model/LottoNumberStatistics.java
+++ src/main/java/lotto/model/LottoNumberStatistics.java
@@ -1,8 +1,82 @@
 package lotto.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class LottoNumberStatistics {
 
-    public void checkWinner() {
+    private static final String TOTAL_RETURN_RATE = "TOTAL_RETURN_RATE";
+    private static final int LOTTO_PRICE = 1000;
 
+    public HashMap<String, String> checkWinner(List<Lotto> lotteryTickets,
+        HashMap<Integer, String> winnerNumbers) {
+        HashMap<LottoRank, Integer> winningCount = initializeWinningCount();
+        long totalPrize = 0;
+        for (Lotto ticket : lotteryTickets) {
+            Map<String, Integer> matchResult = matchNumber(ticket, winnerNumbers);
+            LottoRank rank = LottoRank.getRank(matchResult.get("matchCount"),
+                matchResult.get("matchBonus") == 1);
+            if (rank != LottoRank.NONE) {
+                winningCount.put(rank, winningCount.get(rank) + 1);
+                totalPrize += rank.getPrize();
+            }
+        }
+        return calculateResults(winningCount, totalPrize, lotteryTickets.size());
+    }
+
+    private HashMap<LottoRank, Integer> initializeWinningCount() {
+        HashMap<LottoRank, Integer> winningCount = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank != LottoRank.NONE) {
+                winningCount.put(rank, 0);
+            }
+        }
+        return winningCount;
+    }
+
+    private Map<String, Integer> matchNumber(Lotto ticket, HashMap<Integer, String> winnerNumbers) {
+        List<Integer> winningNumbers = getWinningNumbers(winnerNumbers);
+        int bonusNumber = getBonusNumber(winnerNumbers);
+        int matchCount = (int) ticket.getNumbers().stream()
+            .filter(winningNumbers::contains)
+            .count();
+        int matchBonus = 0;
+        if (ticket.getNumbers().contains(bonusNumber)) {
+            matchBonus = 1;
+        }
+        Map<String, Integer> result = new HashMap<>();
+        result.put("matchCount", matchCount);
+        result.put("matchBonus", matchBonus);
+        return result;
+    }
+
+    private List<Integer> getWinningNumbers(HashMap<Integer, String> winnerNumbers) {
+        return winnerNumbers.entrySet().stream()
+            .filter(entry -> "winningNumber".equals(entry.getValue()))
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toList());
+    }
+
+    private int getBonusNumber(HashMap<Integer, String> winnerNumbers) {
+        return winnerNumbers.entrySet().stream()
+            .filter(entry -> "bonusNumber".equals(entry.getValue()))
+            .map(Map.Entry::getKey)
+            .findFirst()
+            .orElse(0);
+    }
+
+    private HashMap<String, String> calculateResults(HashMap<LottoRank, Integer> winningCount,
+        long totalPrize, int totalTickets) {
+        HashMap<String, String> results = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank != LottoRank.NONE) {
+                results.put(rank.name(), winningCount.get(rank).toString());
+            }
+        }
+        double returnRate = (double) totalPrize / (totalTickets * LOTTO_PRICE) * 100;
+        results.put(TOTAL_RETURN_RATE, String.format("%.1f", returnRate));
+        return results;
     }
 }
