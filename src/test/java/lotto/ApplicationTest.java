d24d5a6771aa138676fa2dc344c8fe4b4b40f1a2
gitubanana <ptyolo666@gmail.com>
2024-11-02 23:56:25 +0900

    test(Application): 로또 프로그램 테스트 추가
    다른 단위 테스트를 많이 했기 때문에, 필요 없겠지만 그래도 있으면 좋을 거 같아 간단한 테스트를 추가했다.

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,57 +1,103 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.constant.ErrorMessage.DUPLICATED_BONUS;
+import static lotto.constant.ErrorMessage.NOT_A_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ApplicationTest extends NsTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-    @Test
-    void 기능_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
+@DisplayName("== 로또 프로그램 테스트 ==")
+class ApplicationTest extends NsTest {
+    @Nested
+    @DisplayName("-- 기능 테스트 --")
+    class FeatureTest {
+        @Test
+        @DisplayName("기본 테스트")
+        void 기본_테스트() {
+            assertRandomUniqueNumbersInRangeTest(
+                    () -> {
+                        run("8000", "1,2,3,4,5,6", "7");
+                        assertThat(output()).contains(
+                                "8개를 구매했습니다.",
+                                "[8, 21, 23, 41, 42, 43]",
+                                "[3, 5, 11, 16, 32, 38]",
+                                "[7, 11, 16, 35, 36, 44]",
+                                "[1, 8, 11, 31, 41, 42]",
+                                "[13, 14, 16, 38, 42, 45]",
+                                "[7, 11, 30, 40, 42, 43]",
+                                "[2, 13, 22, 32, 38, 45]",
+                                "[1, 3, 5, 14, 22, 45]",
+                                "3개 일치 (5,000원) - 1개",
+                                "4개 일치 (50,000원) - 0개",
+                                "5개 일치 (1,500,000원) - 0개",
+                                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                                "6개 일치 (2,000,000,000원) - 0개",
+                                "총 수익률은 62.5%입니다."
+                        );
+                    },
+                    List.of(8, 21, 23, 41, 42, 43),
+                    List.of(3, 5, 11, 16, 32, 38),
+                    List.of(7, 11, 16, 35, 36, 44),
+                    List.of(1, 8, 11, 31, 41, 42),
+                    List.of(13, 14, 16, 38, 42, 45),
+                    List.of(7, 11, 30, 40, 42, 43),
+                    List.of(2, 13, 22, 32, 38, 45),
+                    List.of(1, 3, 5, 14, 22, 45)
+            );
+        }
+
+        @Test
+        @DisplayName("과정 테스트")
+        void 과정_테스트() {
+            assertRandomUniqueNumbersInRangeTest(
+                    () -> {
+                        run("3000", "1,2,3,4,5,6", "7");
+                        assertThat(output()).contains(
+                                "3개를 구매했습니다.",
+                                "[1, 2, 3, 7, 8, 9]",
+                                "[1, 2, 3, 10, 11, 12]",
+                                "[1, 2, 3, 13, 14, 15]",
+                                "3개 일치 (5,000원) - 3개",
+                                "4개 일치 (50,000원) - 0개",
+                                "5개 일치 (1,500,000원) - 0개",
+                                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                                "6개 일치 (2,000,000,000원) - 0개",
+                                "총 수익률은 500.0%입니다."
+                        );
+                    },
+                    List.of(9, 2, 3, 7, 8, 1),
+                    List.of(10, 2, 3, 1, 12, 11),
+                    List.of(3, 1, 2, 14, 15, 13)
+            );
+        }
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() -> {
-            runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+    @Nested
+    @DisplayName("-- 예외 테스트 --")
+    class ExceptionTest {
+        @Test
+        @DisplayName("기본 테스트")
+        void 기본_테스트() {
+            assertSimpleTest(() -> {
+                runException("1000j");
+                assertThat(output()).contains(NOT_A_NUMBER.getMessage());
+            });
+        }
+
+        @Test
+        @DisplayName("당첨 번호와 보너스 번호 중복")
+        void 당첨_번호와_보너스_번호_중복() {
+            assertSimpleTest(() -> {
+                runException("1000", "1,2,3,4,5,6", "6");
+                assertThat(output()).contains(DUPLICATED_BONUS.getMessage());
+            });
+        }
     }
 
     @Override
