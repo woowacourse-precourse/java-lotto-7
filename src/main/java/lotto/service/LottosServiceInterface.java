e355a17549152b14ef63164ca98db8b3955834cc
jungsu <vwx1@naver.com>
2024-11-02 22:54:46 +0900

    feat: LottosServiceInterface 설계
    - LottosServiceInterface를 설계하였습니다.
    - 로또 생성, 당첨 번호 체크 및 수익률 계산 기능을 포함합니다.

--- src/main/java/lotto/service/LottosServiceInterface.java
+++ src/main/java/lotto/service/LottosServiceInterface.java
@@ -1,5 +1,12 @@
 package lotto.service;
 
-public interface LottosServiceInterface {
+import java.util.List;
+import lotto.model.Lotto;
 
+public interface LottosServiceInterface {
+  List<Lotto> generateLottosByAmount(int amount);
+
+  List<Integer> checkWinningNumbers(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber);
+
+  float calculateYield(List<Integer> winningCounts, int purchaseAmount);
 }
