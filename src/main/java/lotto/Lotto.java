2fbdb82532a2b865ad215bb297637014c6dc4d33
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 21:04:59 +0900

    feat(Lotto): 검증 로직을 두 가지로 구분
    validate 메서드는 validateSize와 validateUnique 메서드를 호출하고 각각 로또 번호의 개수와 중복 여부를 검증하도록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,8 +14,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateUnique(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateUnique(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
