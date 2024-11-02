ba48c1e5d9d295baefd51c3a7c6bf7ae1068dfb5
pokm321 <pokm90@gmail.com>
2024-11-02 20:11:35 +0900

    test: add test cases for the business logic validations for Lotto

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,18 +1,55 @@
 package lotto.model;
 
-class LottoTest {
-//    @Test
-//    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-//        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-//    @Test
-//    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-//        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-//                .isInstanceOf(IllegalArgumentException.class);
-//    }
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+import java.util.List;
+import lotto.util.InputParser;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoTest {
+    private final List<Integer> NORMAL_INPUT = List.of(1, 2, 3, 4, 5, 6);
+    Lotto lotto = new Lotto(NORMAL_INPUT);
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6,7", "10,2"})
+    void 숫자_개수가_틀릴경우_예외발생(String numbers) {
+        assertThatThrownBy(() -> {
+            lotto.validateNumberCount(InputParser.parseListOfInteger(numbers));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "999,1,0,-1,4,2"})
+    void 숫자_개수가_맞을경우_통과(String numbers) {
+        lotto.validateNumberCount(InputParser.parseListOfInteger(numbers));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,1,2,3,4,5", "1,3,10,5,4,10"})
+    void 중복된_값이_있을경우_예외발생(String numbers) {
+        assertThatThrownBy(() -> {
+            lotto.validateDuplicates(InputParser.parseListOfInteger(numbers));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "999,998,997,6,5,4"})
+    void 중복된_값이_없을경우_통과(String numbers) {
+        lotto.validateDuplicates(InputParser.parseListOfInteger(numbers));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"999,998,997,6,5,4", "0,1,2,3,4,5"})
+    void 범위를_벗어날_경우_예외발생(String numbers) {
+        assertThatThrownBy(() -> {
+            lotto.validateRange(InputParser.parseListOfInteger(numbers));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "1,45,3,2,6,7"})
+    void 범위내에_있는_경우_통과(String numbers) {
+        lotto.validateRange(InputParser.parseListOfInteger(numbers));
+    }
 }
