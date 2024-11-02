f47c5907d8a1b20f2b05e99b5f7ce5ae60b4ffe7
world-dv <goodforx0147@naver.com>
2024-11-02 15:57:35 +0900

    feat: 보너스 번호 범위 검증
    - 보너스 번호를 1에서 45 사이의 수로 제한하기 위해 예외 처리를 합니다.
    - 보너스 번호가 범위를 벗어날 경우 예외를 발생시킵니다.

---
+++ src/main/java/lotto/domain/Bonus.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+public class Bonus {
+
+    private static final Integer MIN_NUMBER_RANGE = 1;
+    private static final Integer MAX_NUMBER_RANGE = 45;
+
+    private final Integer bonus;
+
+    public Bonus(Integer bonus) {
+        this.bonus = bonus;
+    }
+
+    private boolean IsNumberRangeIncorrect(Integer bonus) {
+        return bonus < MIN_NUMBER_RANGE && bonus > MAX_NUMBER_RANGE;
+    }
+
+    private void validateRange(Integer bonus) {
+        if (IsNumberRangeIncorrect(bonus)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Integer getBonus() {
+        return bonus;
+    }
+}
