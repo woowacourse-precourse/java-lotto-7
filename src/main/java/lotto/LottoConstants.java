9f3b4092c2ac8c22a0a436395cb85c05c836b912
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 21:30:14 +0900

    refactor(LottoConstant): 변수명 더 명확하게 변경

--- src/main/java/lotto/LottoConstants.java
+++ src/main/java/lotto/LottoConstants.java
@@ -4,7 +4,7 @@ public enum LottoConstants {
     MIN_NUMBER(1),
     MAX_NUMBER(45),
     SELECTED_NUMBER_COUNT(6),
-    TICKET_PRICE(1000);
+    PRICE_PER_LOTTO(1000);
 
     private final int value;
 
