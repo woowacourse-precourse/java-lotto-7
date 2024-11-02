556ae25fa7f0a9bc07c29b8cfcb712461f419d86
KJungW <sagakkb0707@naver.com>
2024-11-02 22:11:29 +0900

    test(input): 당첨 번호 입력 기능 테스트
    테스트 내용
    - 당첨 번호 입력값의 검증
    - 당첨 번호 입력값의 파싱

--- src/test/java/lotto/service/InputParsingServiceTest.java
+++ src/test/java/lotto/service/InputParsingServiceTest.java
@@ -2,7 +2,10 @@ package lotto.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import lotto.constant.LottoConfiguration;
+import lotto.generator.RawInputGenerator;
+import lotto.model.Lotto;
 import lotto.model.PurchasePrice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,5 +31,19 @@ class InputParsingServiceTest {
         assertThat(actualPrice)
                 .usingRecursiveComparison()
                 .isEqualTo(new PurchasePrice(givenPrice));
+    }
+
+    @DisplayName("당첨 번호에 대한 입력값 파싱")
+    @Test
+    void 당첨_번호에_대한_입력값_파싱() {
+        List<Integer> givenNumbers = List.of(1, 2, 3, 4, 5, 6);
+        String rawWinningNumbers = RawInputGenerator.makeRawWinningNumbers(givenNumbers);
+
+        Lotto actualWinningLotto = inputParsingService.parseWinningLotto(rawWinningNumbers);
+
+        Lotto expectedWinningLotto = new Lotto(givenNumbers);
+        assertThat(actualWinningLotto)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedWinningLotto);
     }
 }
--- src/test/java/lotto/service/InputValidationServiceTest.java
+++ src/test/java/lotto/service/InputValidationServiceTest.java
@@ -3,9 +3,11 @@ package lotto.service;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.List;
 import java.util.stream.Stream;
 import lotto.constant.LottoConfiguration;
 import lotto.constant.ValidationFailMessage;
+import lotto.generator.RawInputGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -47,4 +49,34 @@ class InputValidationServiceTest {
                 Arguments.of("-99999999999999", ValidationFailMessage.OUT_OF_PARSE_RANGE.getMessage())
         );
     }
+
+    @DisplayName("올바른 당첨 번호의 입력을 검증")
+    @Test
+    void 올바른_당첨_번호의_입력을_검증() {
+        List<Integer> givenNumbers = List.of(1, 2, 3, 4, 5, 6);
+        String rawWinningNumbers = RawInputGenerator.makeRawWinningNumbers(givenNumbers);
+        assertThatCode(() -> inputValidationService.validateWinningNumber(rawWinningNumbers))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("잘못된 당첨 번호의 입력을 검증")
+    @ParameterizedTest(name = "\"{0}\"가 입력된 경우 : {1}")
+    @MethodSource
+    void 잘못된_당첨_번호의_입력을_검증(String inputPurchasePrice, String expectedExceptionMessage) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> inputValidationService.validateWinningNumber(inputPurchasePrice))
+                .withMessage(expectedExceptionMessage);
+    }
+
+    static Stream<Arguments> 잘못된_당첨_번호의_입력을_검증() {
+        return Stream.of(
+                Arguments.of(" ", ValidationFailMessage.EMPTY_INPUT.getMessage()),
+                Arguments.of(",1,2,3,4,5", ValidationFailMessage.EMPTY_INPUT.getMessage()),
+                Arguments.of("1,2,3,4,5,", ValidationFailMessage.EMPTY_INPUT.getMessage()),
+                Arguments.of("일,2,3,4,5,6", ValidationFailMessage.NON_NUMERIC_INPUT.getMessage()),
+                Arguments.of("99999999999999,2,3,4,5,6", ValidationFailMessage.OUT_OF_PARSE_RANGE.getMessage()),
+                Arguments.of("-99999999999999,2,3,4,5,6", ValidationFailMessage.OUT_OF_PARSE_RANGE.getMessage())
+        );
+    }
+
 }
