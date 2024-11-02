41500dfef75adf192366b7bcfc5ba8c00ed59898
dongjae <harold306122@gmail.com>
2024-11-03 00:48:17 +0900

    refactor: LottoStatistics 클래스 리팩토링으로 코드 구조 개선 및 단일 책임 원칙 준수
    - 결과 계산 및 출력 로직을 개별 메서드로 분리하여 단일 책임 원칙을 준수하도록 리팩토링
    - 수익률 계산 메서드에서 복잡한 로직을 단순화하고 코드 가독성을 향상시킴
    - 통계 결과 출력 메서드를 분리하여 LottoStatistics 클래스가 수행하는 각 작업의 명확성 확보

--- src/main/java/lotto/model/LottoStatistics.java
+++ src/main/java/lotto/model/LottoStatistics.java
@@ -6,28 +6,40 @@ public class LottoStatistics {
     private int totalPrize = 0;
 
     public void calculateResults(List<Lotto> purchasedLottos, WinningLotto winningLotto) {
-        int[] results = new int[LottoRank.values().length];
-
-        for (Lotto lotto : purchasedLottos) {
-            int matchCount = winningLotto.countMatchingNumbers(lotto);
-            boolean isBonusMatched = winningLotto.isBonusNumberMatched(lotto);
-            LottoRank rank = LottoRank.findRank(matchCount, isBonusMatched);
-
-            results[rank.ordinal()]++;
-            totalPrize += rank.getPrize();
-        }
-
+        int[] results = initializeResultsArray();
+        calculateLottoResults(purchasedLottos, winningLotto, results);
         printResults(results);
     }
 
     public double calculateYield(int totalSpent) {
-        if (totalSpent == 0) return 0;
-        return Math.round(((double) totalPrize / totalSpent) * 1000) / 10.0;
+        return totalSpent == 0 ? 0 : Math.round(((double) totalPrize / totalSpent) * 1000) / 10.0;
+    }
+
+    private int[] initializeResultsArray() {
+        return new int[LottoRank.values().length];
+    }
+
+    private void calculateLottoResults(List<Lotto> purchasedLottos, WinningLotto winningLotto, int[] results) {
+        for (Lotto lotto : purchasedLottos) {
+            LottoRank rank = determineRank(winningLotto, lotto);
+            results[rank.ordinal()]++;
+            totalPrize += rank.getPrize();
+        }
+    }
+
+    private LottoRank determineRank(WinningLotto winningLotto, Lotto lotto) {
+        int matchCount = winningLotto.countMatchingNumbers(lotto);
+        boolean isBonusMatched = winningLotto.isBonusNumberMatched(lotto);
+        return LottoRank.findRank(matchCount, isBonusMatched);
     }
 
     private void printResults(int[] results) {
         System.out.println("당첨 통계");
         System.out.println("---");
+        printRankResults(results);
+    }
+
+    private void printRankResults(int[] results) {
         System.out.printf("3개 일치 (5,000원) - %d개\n", results[LottoRank.FIFTH.ordinal()]);
         System.out.printf("4개 일치 (50,000원) - %d개\n", results[LottoRank.FOURTH.ordinal()]);
         System.out.printf("5개 일치 (1,500,000원) - %d개\n", results[LottoRank.THIRD.ordinal()]);
