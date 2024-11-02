e27017b51c4523c29b53ea4f3b45c82b253dad7e
posite <su84053@gmail.com>
2024-11-02 19:29:25 +0900

    fix(CustomException): 커스텀 Exception 오류 수정
    RuntimeException이 아닌 IllegalArgumentException이 발생하게 수정하였습니다.

--- src/main/java/lotto/util/DuplicateBonusNumberException.java
+++ src/main/java/lotto/util/DuplicateBonusNumberException.java
@@ -2,7 +2,7 @@ package lotto.util;
 
 import static lotto.constant.ErrorMessage.DUPLICATE_BONUS_NUMBER;
 
-public class DuplicateBonusNumberException extends RuntimeException {
+public class DuplicateBonusNumberException extends IllegalArgumentException {
     public DuplicateBonusNumberException() {
         super(DUPLICATE_BONUS_NUMBER.getMessage());
     }
