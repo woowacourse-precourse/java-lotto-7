36fdd031afac93d2710ad31b09b5c8bef37e5aad
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 02:13:56 +0900

    feat(BonusNumber): 보너스 번호를 가질 클래스 작성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class BonusNumber {
+
+    private int number;
+
+    public BonusNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
