75048a119adb28daff34a92c15bb7cef099c242d
porori1224 <iminseo031224@gmail.com>
2024-11-02 14:35:57 +0900

    fix(LottoGame.java): 예외 처리 개선
    로또 구입 금액 입력 시 숫자 형식 오류 처리 및 단위 처리 예외 추가
    
    - NumberFormatExcetion을 catch하여 숫자가 아닌 입력에 대한 예외 처리 추가
    - 유효하지 않은 입력에 대한 안내 메시지 출력 후 재입력 요청에 대해 개선
    - amount % 1000 != 0 을 통해 입력 단위가 1000원 단위가 되도록 조건 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -6,17 +6,17 @@ public class LottoGame {
     private final Scanner scanner = new Scanner(System.in);
 
     private int inputAmount() {
-        System.out.println("구입금액을 입력해주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
         String input = scanner.nextLine();
 
         try {
             int amount = Integer.parseInt(input);
-            if (amount <= 0) {
-                throw new NumberFormatException();
+            if (amount <= 0 || amount % 1000 != 0) {
+                throw new NumberFormatException("[ERROR] 구입 금액은 1,000원 단위의 양수로 입력해야 합니다.");
             }
             return amount;
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 구입 금액은 1,000원 단위의 양수로 입력해야 합니다.");
+            System.out.println("[ERROR] 유효한 숫자를 입력하세요.");
             return inputAmount();
         }
     }
