a30460ee305bf04ff14a1f0c1f028b7e18877630
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 04:31:47 +0900

    feat : int[] 데이터를 전달 받아 List<int[]>에 추가하는 기능 구현

--- src/main/java/lotto/model/TempLottos.java
+++ src/main/java/lotto/model/TempLottos.java
@@ -1,4 +1,16 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TempLottos {
+    private final List<int[]> lottoNumbers = new ArrayList<int[]>();
+
+    public TempLottos(int[] lottoNumber) {
+        this.lottoNumbers.add(lottoNumber);
+    }
+
+    public List<int[]> getLottoNumbers() {
+        return lottoNumbers;
+    }
 }
