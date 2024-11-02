a2c2f999a846c58a897863e686239c11811b42c5
hwangdaesun <hds650588@gmail.com>
2024-11-02 16:26:35 +0900

    refactor : 테스트 메서드 네이밍 수정

--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class PrizeTest {
     @Test
-    void 등수에_맞는_돈을_반환한다() {
+    void 당첨되었을_경우_정상_작동한다() {
         Lotto myLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 6, 8));
         BonusNumber bonusNumber = new BonusNumber(12);
