aa1a2305848268ae2fbeef81932e417b0c58ec19
rdyjun <rdyjun00@gmail.com>
2024-11-02 13:32:43 +0900

    feat: 입력받은 구입 금액 유효성 검사 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,5 +6,18 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해 주세요.");
         String amountInput = Console.readLine().trim();
+        validateAmountInput(amountInput);
+
+        int amount = Integer.parseInt(amountInput);
+    }
+
+    private static void validateAmountInput(String amountInput) {
+        if (!amountInput.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+
+        if (Integer.parseInt(amountInput) % 1000 > 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액 단위는 1000원 단위 이상이어야 합니다.");
+        }
     }
 }
