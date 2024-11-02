e22ba1ea2a625e6e1c70625693983460d85343f1
장혜민 <gpals5987@gmail.com>
2024-11-03 07:00:15 +0900

    [feat] LottoConstants 작성
    - MIN_RANGE_NUMBER
    - MAX_RANGE_NUMBER
    - LOTTO_NUMBERS_COUNT

---
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -0,0 +1,8 @@
+package lotto.constant;
+
+public class LottoConstants {
+    public static final int MIN_RANGE_NUMBER = 1;
+    public static final int MAX_RANGE_NUMBER = 45;
+
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+}
