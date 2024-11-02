c3c94470bc055260f0418061123004c45584589d
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-02 18:57:18 +0900

    feat(LottoResult): 로또 번호와 당첨 번호가 일치하는 개수를 구하는 로직 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -10,6 +10,7 @@ public class LottoResult {
 
     private LottoResult(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
         lottoRankTypes = new ArrayList<>();
+        calculateLottoRanks(lottos, winningNumbers, bonusNumber);
     }
 
     public static LottoResult of(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
@@ -18,6 +19,27 @@ public class LottoResult {
 
     public List<LottoRankType> getLottoRankTypes() {
         return Collections.unmodifiableList(lottoRankTypes);
+    }
+
+    private void calculateLottoRanks(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        for (Lotto lotto : lottos) {
+            calculateRankForLotto(lotto.getNumbers(), winningNumbers, bonusNumber);
+        }
+    }
+
+    private void calculateRankForLotto(List<Integer> lottoNumbers, List<Integer> winningNumbers, int bonusNumber) {
+        int matchCount = 0;
+        for (Integer lottoNumber : lottoNumbers) {
+            if (winningNumbers.contains(lottoNumber)) {
+                matchCount++;
+            }
+        }
+        boolean hasBonusNumber = containsBonusNumber(lottoNumbers, bonusNumber);
+        lottoRankTypes.add(LottoRankType.of(matchCount, hasBonusNumber));
+    }
+
+    private boolean containsBonusNumber(List<Integer> lottoNumbers, int bonusNumber) {
+        return lottoNumbers.contains(bonusNumber);
     }
 
 }
