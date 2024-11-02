401f0ff8d55185277fb7a5fcc6fcb3d3209a6616
kungbi <kipper12335@gmail.com>
2024-11-02 14:14:20 +0900

    fix(LottoValidator): 잘못된 에러 메시지 출력 수정

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -3,6 +3,7 @@ package lotto.validator;
 import static lotto.configuration.LottoConfiguration.LOTTO_MAX_NUMBER;
 import static lotto.configuration.LottoConfiguration.LOTTO_MIN_NUMBER;
 import static lotto.configuration.LottoConfiguration.LOTTO_NUMBER_COUNT;
+import static lotto.exception.LottoExceptionMessage.DUPLICATE_NUMBERS;
 import static lotto.exception.LottoExceptionMessage.INVALID_NUMBER_COUNT;
 import static lotto.exception.LottoExceptionMessage.NULL_OR_EMPTY_NUMBERS;
 import static lotto.exception.LottoExceptionMessage.NUMBER_OUT_OF_RANGE;
@@ -17,14 +18,14 @@ public class LottoValidator {
             throw ExceptionUtils.IllegalArgument(NULL_OR_EMPTY_NUMBERS);
         }
         if (numbers.size() != LOTTO_NUMBER_COUNT.getValue()) {
-            throw ExceptionUtils.IllegalArgument(NUMBER_OUT_OF_RANGE);
+            throw ExceptionUtils.IllegalArgument(INVALID_NUMBER_COUNT);
         }
         if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT.getValue()) {
-            throw ExceptionUtils.IllegalArgument(NUMBER_OUT_OF_RANGE);
+            throw ExceptionUtils.IllegalArgument(DUPLICATE_NUMBERS);
         }
         if (numbers.stream().anyMatch(
                 number -> !(LOTTO_MIN_NUMBER.getValue() <= number && number <= LOTTO_MAX_NUMBER.getValue()))) {
-            throw ExceptionUtils.IllegalArgument(INVALID_NUMBER_COUNT);
+            throw ExceptionUtils.IllegalArgument(NUMBER_OUT_OF_RANGE);
         }
     }
 }
