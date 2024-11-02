64c84003f7086ab29071d6e5293934c7286c84d4
jaeyeong <zzz26lk@gmail.com>
2024-11-02 16:29:29 +0900

    test: 당첨 번호 테스트 코드 작성
    당첨 번호를 생성하는 코드를 검증하기 위해서
    생성하는 테스트와 예외 상황을 테스트하는 코드를 작성한다.

---
+++ src/test/java/lotto/domain/util/WinningNumberTest.java
@@ -0,0 +1,63 @@
+package lotto.domain.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import lotto.validator.NumberValidator;
+import lotto.validator.Validator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumberTest {
+
+    @Test
+    @DisplayName("당첨 번호를 입력받으면 List로 만드는지 테스트한다.")
+    void createWinningNumberTest() {
+        String inputWinningNumber = "1, 2, 3, 4, 5, 6";
+
+        List<Integer> winningNumber = WinningNumber.create(inputWinningNumber);
+
+        assertThat(winningNumber).hasSize(6);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 6자리가 아니면 예외를 발생한다.")
+    void winningNumbersTest1() {
+        Validator<String> numberValidator = new NumberValidator();
+        String winningNumbers = "1,2,3,4,5";
+
+        assertThatThrownBy(() -> numberValidator.validate(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 중복되면 예외를 발생한다.")
+    void winningNumbersTest2() {
+        Validator<String> numberValidator = new NumberValidator();
+        String winningNumbers = "1,2,3,4,5,5";
+
+        assertThatThrownBy(() -> numberValidator.validate(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 숫자가 아니면 예외를 발생한다.")
+    void winningNumbersTest3() {
+        Validator<String> numberValidator = new NumberValidator();
+        String winningNumbers = "1,2,3,a,5,6";
+
+        assertThatThrownBy(() -> numberValidator.validate(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호가 1 ~ 45 사이의 숫자가 아니면 예외를 발생한다.")
+    void winningNumbersTest4() {
+        Validator<String> numberValidator = new NumberValidator();
+        String winningNumbers = "0,2,3,4,5,6";
+
+        assertThatThrownBy(() -> numberValidator.validate(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
