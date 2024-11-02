eaa7c77378d429bcf51db971b1caa2cce49e233c
sejinezy <psj8880@naver.com>
2024-11-02 17:41:39 +0900

    feat: 5. [입력] 당첨 번호를 입력 받는 기능
    - 번호는 쉼표(,)를 기준으로 구분
    - [예외처리] 번호가 6개가 아닌 경우
    - [예외처리] 숫자가 아닌 값이 있는 경우
    -
    [예외처리] 중복된 값이 있는 경우

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,12 +1,59 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Input {
 
     public static String getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public static String getWinningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String[] split(String input) {
+        return input.split(",");
+    }
+
+    public static String[] validateIsNumber(String[] strings) {
+        try {
+            for (String string : strings) {
+                Integer.parseInt(string);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+        return strings;
+    }
+
+    public static List<Integer> toInt(String[] strings) {
+        List<Integer> integers = new ArrayList<>();
+        for (String string : strings) {
+            integers.add(Integer.parseInt(string));
+        }
+        return integers;
+    }
+
+    public static List<Integer> validateCount(List<Integer> integers) {
+        if (integers.size() != 6) {
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
+        return integers;
+    }
+
+    public static List<Integer> validateNotDuplicate(List<Integer> integers) {
+        Set<Integer> set = new HashSet<>(integers);
+        if (set.size() != integers.size()) {
+            throw new IllegalArgumentException("당첨 번호는 중복되면 안됩니다.");
+        }
+        return integers;
     }
 
     public static String validateNotEmpty(String input) {
