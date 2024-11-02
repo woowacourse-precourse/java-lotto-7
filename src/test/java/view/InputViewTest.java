33934af58c0b44b6ed3aa1fb472a60afdb02e774
JiEung2 <181757@jnu.ac.kr>
2024-11-03 00:46:12 +0900

    test(inputViewTest): 당첨번호 입력 1~45가 아닐 때 예외 테스트 추가

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package view;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.exception.Exception.LOTTO_NUMBER_OUT_OF_RANGE;
 import static lotto.exception.Exception.LOTTO_NUMBER_SIZE_MUST_BE_SIX;
 import static lotto.validator.LottoNumbersValidator.validate;
 import static org.assertj.core.api.Assertions.*;
@@ -64,6 +65,17 @@ class InputViewTest {
                 assertThatThrownBy(() -> runException("1,2,3,4,5,6,7"))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage(LOTTO_NUMBER_SIZE_MUST_BE_SIX.getMessage());
+            });
+
+        }
+
+        @DisplayName("당첨번호 입력 1~45가 아닐 때 예외 테스트")
+        @Test
+        void inputWinningNumbers_fail_outOfRange() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1,2,3,4,5,46"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
             });
 
         }
