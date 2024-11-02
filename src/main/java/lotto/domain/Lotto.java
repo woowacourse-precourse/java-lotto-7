b229eaa70acb157e03da1916d4af83e6b3ca90d0
sihyun10 <hyhy200170@gmail.com>
2024-11-03 01:55:21 +0900

    feat(Lotto) : 오름차순 정렬, 숫자 범위 유효성 검사 추가
    로또 번호는 오름차순으로 정렬하여 보여줘야하기 때문에 Lotto클래스에서
    설정해주었습니다. 이제 로또 번호를 생성할때면 정렬이 되어 반환이 될것입니다.
    또 숫자 범위가 1~45까지인지 검증하는 코드도 추가하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,27 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        Collections.sort(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
-        this.numbers = numbers;
+        validateRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -26,6 +35,19 @@ public class Lotto {
         if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (isInValidNumber(number)) {
+                throw new IllegalArgumentException(
+                        "[ERROR] 로또 번호는 " + LOTTO_NUMBER_MIN + " 이상 " + LOTTO_NUMBER_MAX + " 이하여야 합니다.");
+            }
+        }
+    }
+
+    private boolean isInValidNumber(Integer number) {
+        return number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX;
     }
 
     public List<Integer> getNumbers() {
