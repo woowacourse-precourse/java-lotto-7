abdfd7df783fcd8fad589d58bac90792e35d3b04
JiEung2 <181757@jnu.ac.kr>
2024-11-03 01:55:12 +0900

    feat(exception): 보너스 넘버가 당첨 번호와 중복일 때의 예외 메시지 추가

--- src/main/java/lotto/exception/Exception.java
+++ src/main/java/lotto/exception/Exception.java
@@ -11,6 +11,7 @@ public enum Exception {
     LOTTO_NUMBER_DUPLICATED("로또 번호는 중복될 수 없습니다."),
 
     BONUS_NUMBER_OUT_OF_RANGE("보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
+    BONUS_NUMBER_DUPLICATED("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     ;
 
     private static final String PREFIX = "[ERROR] ";
