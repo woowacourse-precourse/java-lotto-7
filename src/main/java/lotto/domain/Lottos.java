874889e450319c4c30d4cb02e4085b6a3eefdb0f
slay <palgalow@gmail.com>
2024-11-02 18:41:54 +0900

    feat(Lottos): 로또 등수 결정 기능 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,17 +1,24 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import lotto.util.RandomNumbers;
 
 public class Lottos {
 
     private List<Lotto> lottos;
     private final int lottoCount;
+    private final List<Integer> winningNumbers;
+    private Map<LottoRank, Integer> statistics = new EnumMap<>(LottoRank.class);
+    private final int bonusNumber;
 
-    public Lottos(int purchaseAmount) {
+    public Lottos(int purchaseAmount,List<Integer> winningNumbers, int bonusNumber) {
         this.lottos = new ArrayList<>();
-        this.lottoCount = purchaseAmount/1000;
+        this.lottoCount = purchaseAmount / 1000;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public Lotto createLotto() {
@@ -23,5 +30,27 @@ public class Lottos {
 
     public int getLottoCount() {
         return this.lottoCount;
+    }
+
+    public void calculateWinningStatistics(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+
+        }
+    }
+
+    private LottoRank calculateRank(Lotto lotto) {
+        int matchCount = 0;
+        boolean matchBonus = false;
+        int prize = 0;
+        List<Integer> numbers = lotto.getNumbers();
+        for (int number : numbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+            if (number == bonusNumber) {
+                matchBonus = true;
+            }
+        }
+        return LottoRank.valueOf(matchCount, matchBonus);
     }
 }
