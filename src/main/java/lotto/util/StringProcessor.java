23a6c87ce64bdb6689ca47f2ea3b318bd439731b
minzzun99 <skg06123@naver.com>
2024-11-02 16:52:43 +0900

    feat(StringProcessor): 입력받은 문자열 검증, 정수형으로 형변환하는 기능 추가
    - validateInput() 메서드를 통해 null과 빈 값 입력 시에 예외 발생
    - integerConverter() 메서드를 통해 정수 범위를 벗어나거나 음수가 입력될 경우에는 예외 발생하고 정수 반환

--- src/main/java/lotto/util/StringProcessor.java
+++ src/main/java/lotto/util/StringProcessor.java
@@ -1,7 +1,36 @@
 package lotto.util;
 
+import static lotto.constant.ErrorMessage.EMPTY_INPUT_ERROR;
+import static lotto.constant.ErrorMessage.NULL_INPUT_ERROR;
+import static lotto.constant.ErrorMessage.NUMBER_FORMAT_ERROR;
+import static lotto.constant.ErrorMessage.NUMBER_RANGE_ERROR;
+
 public class StringProcessor {
+    public static void validateInput(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(NULL_INPUT_ERROR.getMessage());
+        }
+
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR.getMessage());
+        }
+    }
+
     public static String removeCommas(String input) {
         return input.replaceAll(",", "");
+    }
+
+    public static int integerConverter(String input) {
+        try {
+            int number = Integer.parseInt(input);
+
+            if (number < 1) {
+                throw new IllegalArgumentException(NUMBER_RANGE_ERROR.getMessage());
+            }
+
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR.getMessage());
+        }
     }
 }
