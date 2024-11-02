cd109675c6a405dcf10b817eef7cdf561e671a61
lsj0822 <lsj0822@khu.ac.kr>
2024-11-03 00:26:15 +0900

    feat(LottoController) : 메서드 추가 및 리팩터링
    - 기존의 로직은 각각의 값을 void 형태로 입력하는 것이었지만, Status라는 모델을 생성하였기에 이의 생성자와 getter로 객체를 넘겨주는 로직으로 수정하였습니다.
    - 보너스 당첨번호를 입력받는 메서드를 추가하였습니다.

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.List;
+import model.Status;
 import utils.Converter;
+import utils.LottoFactory;
 import utils.Parser;
 import validator.Validator;
 import view.InputView;
@@ -9,16 +11,19 @@ import view.OutputView;
 
 public class LottoController {
 
-    public void startProgram() {
-        inputMoneyController();
-        winningNumbersController();
+    public void setProgram() {
+        Status status = new Status(inputMoneyController());
+        status.setLottos(LottoFactory.generateLotto(status.getLottoCount()));
+        OutputView.printLottoPurchasing(status.getLottoCount(), status.getLottos());
+        status.setWinningNumbers(winningNumbersController());
+        status.setBonusNumber(bonusWinningNumberController(status));
     }
 
-    private Long inputMoneyController() {
+    private Integer inputMoneyController() {
         try {
             String originalInput = InputView.inputPurchaseMoney();
             Validator.validateMoney(originalInput);
-            return Long.parseLong(originalInput);
+            return Integer.parseInt(originalInput);
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e);
         }
@@ -29,6 +34,7 @@ public class LottoController {
         try {
             String originalInput = InputView.inputWinningNumbers();
             List<String> parsedInput = Parser.parsing(originalInput);
+            Validator.validateWinningGroup(parsedInput);
             List<Integer> numbers = Converter.convertStringToNumber(parsedInput);
             Validator.validateLottoNumbers(numbers);
             return numbers;
@@ -36,5 +42,17 @@ public class LottoController {
             OutputView.printErrorMessage(e);
         }
         return winningNumbersController();
+    }
+
+
+    private Integer bonusWinningNumberController(Status status) {
+        try {
+            String originalInput = InputView.inputBonusWinningNumber();
+            Validator.validateBonusNumber(originalInput, status);
+            return Integer.parseInt(originalInput);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+        }
+        return bonusWinningNumberController(status);
     }
 }
