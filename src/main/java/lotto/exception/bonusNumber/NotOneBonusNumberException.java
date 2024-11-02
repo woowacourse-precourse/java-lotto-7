75a85efc12c65e1d84ffa16271c8af16ba1730b7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-11-02 22:58:30 +0900

    refactor: 사용하지 않는 Exception class 삭제

--- src/main/java/lotto/exception/bonusNumber/NotOneBonusNumberException.java
+++
@@ -1,9 +0,0 @@
-package lotto.exception.bonusNumber;
-
-public class NotOneBonusNumberException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "보너스 번호는 한자리만 입력해야 합니다";
-
-    public NotOneBonusNumberException() {
-        super(ERROR_MESSAGE);
-    }
-}
