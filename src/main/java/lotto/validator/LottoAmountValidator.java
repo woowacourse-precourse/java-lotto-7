334991ca49ec2c363a806fe884627720e23a5ef8
JiEung2 <181757@jnu.ac.kr>
2024-11-02 20:36:57 +0900

    feat(lottoAmountValidator): 구입 금액이 로또 금액 단위로 나누어 떨어지는지 검증하는 기능 추가

--- src/main/java/lotto/validator/LottoAmountValidator.java
+++ src/main/java/lotto/validator/LottoAmountValidator.java
@@ -5,6 +5,7 @@ import static lotto.exception.Exception.*;
 public class LottoAmountValidator {
     public static void validate(int purchaseAmount, int lottoPrice) {
         validatePositiveAmount(purchaseAmount);
+        validateDivisibleByLottoPrice(purchaseAmount, lottoPrice);
     }
 
     private static void validatePositiveAmount(int purchaseAmount) {
@@ -13,4 +14,9 @@ public class LottoAmountValidator {
         }
     }
 
+    private static void validateDivisibleByLottoPrice(int purchaseAmount, int lottoPrice) {
+        if (purchaseAmount % lottoPrice != 0) {
+            throw new IllegalArgumentException(ONLY_DIVISIBLE_BY_LOTTO_PRICE.getMessage());
+        }
+    }
 }
