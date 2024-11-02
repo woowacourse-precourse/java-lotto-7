7b160e9b1021794f48620b6c33c760953ee5a494
SukJin Kim <rkathrwks@gmail.com>
2024-11-02 21:30:19 +0900

    feat(view): 입력된 구매 금액에 대한 유효성 검증 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,14 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.PurchaseAmountValidator;
 
 public class InputView {
 
     public long readPurchaseAmount() {
-        return Long.parseLong(readInput());
+        String input = readInput();
+        PurchaseAmountValidator.validate(input);
+        return Long.parseLong(input);
     }
 
     private String readInput() {
