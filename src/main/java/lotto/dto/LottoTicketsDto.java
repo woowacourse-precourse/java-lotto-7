75cf0b07565302bd22a65f1c002eb6968202e66d
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 18:30:36 +0900

    feat(dto): 로또 티켓 정보를 위한 LottoTicketsDto 클래스와 정적 팩토리 메서드 추가

---
+++ src/main/java/lotto/dto/LottoTicketsDto.java
@@ -0,0 +1,16 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoTicketsDto {
+    private final List<List<Integer>> tickets;
+
+    private LottoTicketsDto(List<List<Integer>> tickets) {
+        this.tickets = tickets;
+    }
+
+    public static LottoTicketsDto from(List<List<Integer>> tickets) {
+        return new LottoTicketsDto(tickets);
+    }
+
+}
