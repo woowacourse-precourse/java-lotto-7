aa3005e4c166e86bb3a70220977450c3c595116a
howareyoo <newworld121@naver.com>
2024-11-03 01:39:32 +0900

    test: unit test for validateNumbersRange method

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -59,6 +59,12 @@ class LottoTest {
                 .hasMessage("[ERROR] 보너스 번호는 입력하신 로또 번호와 중복되지 않아야 합니다.");
     }
 
+    @Test
+    void 로또_번호의_입력_값_범위에_벗어나면_예외가_발생한다(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1에서 45 사이의 숫자를 입력해야 합니다.");
+    }
 
 
 }
