57e1d4c9689d7b6eed995a0c219a927371073e74
yybmion <yyb400@naver.com>
2024-11-02 21:06:35 +0900

    test: 로또 생성 테스트코드 작성
    1. 생성된 로또 번호가 오름차순으로 정렬되어 있는가?
    2. 생성된 로또 번호가 1부터 45 사이의 숫자인가?
    3. 생성된 로또 번호가 중복되지 않는가?

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -29,4 +29,52 @@ public class LottoGeneratorTest {
         // then
         assertThat(lottos).hasSize(5);
     }
+
+    @Test
+    @DisplayName("생성된 로또 번호는 오름차순으로 정렬되어 있어야 한다.")
+    void 성공_로또생성_오름차순() {
+        // given
+        int amount = 1000;
+
+        // when
+        List<Lotto> lottos = lottoNumberGenerator.generate(amount);
+
+        // then
+        assertThat(lottos).allSatisfy(lotto -> {
+            List<Integer> numbers = lotto.getNumbers();
+            assertThat(numbers).isSorted();
+        });
+    }
+
+    @Test
+    @DisplayName("생성된 로또 번호는 1부터 45 사이의 숫자여야 한다.")
+    void 성공_로또생성_알맞은범위() {
+        // given
+        int amount = 1000;
+
+        // when
+        List<Lotto> lottos = lottoNumberGenerator.generate(amount);
+
+        // then
+        assertThat(lottos).allSatisfy(lotto -> {
+            List<Integer> numbers = lotto.getNumbers();
+            assertThat(numbers).allMatch(number -> number >= 1 && number <= 45);
+        });
+    }
+
+    @Test
+    @DisplayName("생성된 로또 번호는 중복되지 않아야 한다.")
+    void 성공_로또생성_중복되지않는번호() {
+        // given
+        int amount = 1000;
+
+        // when
+        List<Lotto> lottos = lottoNumberGenerator.generate(amount);
+
+        // then
+        assertThat(lottos).allSatisfy(lotto -> {
+            List<Integer> numbers = lotto.getNumbers();
+            assertThat(numbers).doesNotHaveDuplicates();
+        });
+    }
 }
