b94ac7dcec648ccd65f3a271ba1bc328b7fd6bbb
jaeyeong <zzz26lk@gmail.com>
2024-11-02 18:31:21 +0900

    test: LottoList 생성 테스트
    구입 금액에 맞는 Lotto 클래스를 생성하는지
    테스트 하기 위해서 테스트 코드를 작성한다.

---
+++ src/test/java/lotto/domain/util/CreateLottoListTest.java
@@ -0,0 +1,22 @@
+package lotto.domain.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreateLottoListTest {
+
+    @Test
+    @DisplayName("로또 구입 금액에 맞는 로또를 생성하는지 테스트한다.")
+    void createLottoListTest() {
+        int amount = 8000;
+        int count = amount / 1000;
+
+        List<Lotto> lottos = CreateLottoList.create(count);
+
+        assertThat(lottos.size()).isEqualTo(count);
+    }
+}
