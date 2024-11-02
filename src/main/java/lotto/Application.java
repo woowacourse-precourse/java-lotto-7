4f01a5a45a32c193e309e575919632d216fe282e
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-11-02 15:31:53 +0900

    feat(Application): 현재 개발된 도메인을 사용하여 앱의 기능 로직을 간단히 구현
    - 실제로 작동하지 않는 로직이지만 로직을 정리하기 위해 작성되었다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = 8000;
+        // TODO: 여기도 연산
+        int lottoCount = price / 1000; // TODO: 나누어 떨어지지 않으면 예외
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto lotto = new Lotto(randomNumbers);
+            lottos.add(lotto);
+        }
+
+        List<Integer> winningLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        List<Integer> machingNumCount = new ArrayList<>();
+        for(int i=0;i<lottoCount;i++) {
+            machingNumCount.add(countMatchingNumbers(lottos.get(i), winningLottoNumbers));
+        }
+        // [3, 0, 0, 0, 0]
+        int revenue = 5000;
+        double profit = revenue / price; // TODO: 연산
+    }
+
+    public static Integer countMatchingNumbers(Lotto lotto, List<Integer> winningNums) {
+        return 1;
     }
 }
