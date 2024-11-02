29d8380f40ad1be0ce74d91102be7f8ece5961f6
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-02 17:52:22 +0900

    refactor(InputView, OutputView): 올바른 출력이 나오도록 개행문자 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,14 +6,14 @@ import lotto.Result;
 public class OutputView {
 
     public void outputPurchaseLottoNumber(int buyCount, List<List<Integer>> buyLottoList) {
-        System.out.println(buyCount + "개를 구매했습니다.");
+        System.out.println("\n" + buyCount + "개를 구매했습니다.");
         for (List<Integer> lottoNumber : buyLottoList) {
             System.out.println(lottoNumber);
         }
     }
 
     public void outputLottoWinningStatistics() {
-        System.out.println("당첨 통계");
+        System.out.println("\n당첨 통계");
         System.out.println("---");
 
         for (Result result : Result.values()) {
