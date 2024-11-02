55bb71a5071effa7d7ffadf80b1bec39b0085a0b
sonsuchang <son336208@naver.com>
2024-11-02 23:04:53 +0900

    feat: 구입금액 입력 기능 구현 및 테스트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(readLine());
     }
 }
