de2ede665f5668038ed9db5f50a0b927d211905b
KF97 <aysul0@naver.com>
2024-11-02 15:05:26 +0900

    refactor: 메서드 추출 리팩토링

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -20,18 +20,24 @@ public class Validator {
     static int validateBonusNumber(String bonusNumberInput, Lotto winningNumbersLotto) {
         try {
             int bonusNumber = Integer.parseInt(bonusNumberInput);
-
-            if (bonusNumber < 1 || bonusNumber > 45) {
-                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
-            }
-
-            if (winningNumbersLotto.contains(bonusNumber)) {
-                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-            }
+            validateBonusNumberInRange(bonusNumber);
+            validateBonusNumberDuplicate(bonusNumber, winningNumbersLotto);
 
             return bonusNumber;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    static void validateBonusNumberInRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    static void validateBonusNumberDuplicate(int bonusNumber, Lotto winningNumbersLotto) {
+        if (winningNumbersLotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
     }
 }
