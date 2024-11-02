7f79e53a1425f349797fa3fa8e72ea223af773cf
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 01:06:48 +0900

    feat(InputViewTest) : checkBonus 테스트 함수

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -7,14 +7,19 @@ import lotto.Model.Lotto;
 import lotto.View.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class InputViewTest extends NsTest {
+    InputView inputView = new InputView();
+
     @Test
     @DisplayName("parseInt 정상 작동 테스트")
     void parseIntTest(){
-        InputView inputView = new InputView();
         int actual = inputView.parseInt("123");
         assertThat(actual).isEqualTo(123);
     }
@@ -22,11 +27,29 @@ public class InputViewTest extends NsTest {
     @Test
     @DisplayName("parseInt 숫자가 아닌 것 테스트")
     void parseIntErrorTest(){
-        InputView inputView = new InputView();
         assertThatThrownBy(() -> inputView.parseInt("123a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.ONLY_NUMBER.getError());
     }
+
+    @Test
+    @DisplayName("checkBonus 정상 작동 테스트")
+    void checkBonusTest(){
+        int bonusNumber = 7;
+        Lotto answer = new Lotto(List.of(1,2,3,4,5,6));
+        assertThatNoException().isThrownBy(()-> inputView.checkBonus(bonusNumber, answer));
+    }
+
+    @Test
+    @DisplayName("checkBonus 당첨 번호와 보너스 번호가 중복될 때")
+    void checkBonusDuplicateTest(){
+        int bonusNumber = 6;
+        Lotto answer = new Lotto(List.of(1,2,3,4,5,6));
+        assertThatThrownBy(() -> inputView.checkBonus(bonusNumber, answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_DUPLICATE.getError());
+    }
+
 
     @Override
     public void runMain() {
