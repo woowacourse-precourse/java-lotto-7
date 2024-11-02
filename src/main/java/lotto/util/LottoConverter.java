fe53efb05bb426cc7d44a9a26714cd274475a2dd
KIM MINSEOK <mskim.glenn@gmail.com>
2024-11-02 16:01:44 +0900

    feat: LottoConverter 클래스의 로또 개수 변환 및 금액 검증 기능 추가

---
+++ src/main/java/lotto/util/LottoConverter.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+public class LottoConverter {
+
+    private static final int BASE_PRICE = 1000;
+
+    public static final String UNDER_PRICE_ERROR = "[ERROR] 최소 구입 금액은" + BASE_PRICE + "원 이상이어야 합니다.";
+    public static final String UNIT_ERROR = "[ERROR] 금액은 " + BASE_PRICE + "원 단위여야 합니다.";
+
+    public int convertPriceToLotto(String input) {
+        int price = Integer.parseInt(input);
+        validatePrice(price);
+
+        return price / BASE_PRICE;
+    }
+
+    private void validatePrice(int price) {
+        if (price < BASE_PRICE) {
+            throw new IllegalArgumentException(UNDER_PRICE_ERROR);
+        }
+        if (price % BASE_PRICE != 0) {
+            throw new IllegalArgumentException(UNIT_ERROR);
+        }
+    }
+
+}
