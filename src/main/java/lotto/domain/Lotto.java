8c0aa0d72ced80e6021fd409c3648b25d33159ce
eb9862 <luclead622@gmail.com>
2024-11-02 22:57:39 +0900

    feat: 로또의 당첨 등수를 반환하는 메서드 구현
    Lotto 클래스 내부에 findPlace() 메서드 형태로 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import static lotto.domain.LottoInfo.END_NUMBER;
 import static lotto.domain.LottoInfo.PICK_COUNT;
 import static lotto.domain.LottoInfo.START_NUMBER;
+import static lotto.domain.WinningInfo.SECOND;
+import static lotto.domain.WinningInfo.THIRD;
 
 import java.util.HashSet;
 import java.util.List;
@@ -39,7 +41,7 @@ public class Lotto {
         }
     }
 
-    public static void checkNumberRange(int number) {
+    static void checkNumberRange(int number) {
         if (number < START_NUMBER || number > END_NUMBER) {
             throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE);
         }
@@ -50,6 +52,41 @@ public class Lotto {
         if (distinctNumbers.size() != validRangeNumbers.size()) {
             throw new IllegalArgumentException(NUMBER_DUPLICATED);
         }
+    }
+
+    public int findPlace(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        List<Integer> winningNumber = winningNumbers.getNumbersList();
+        int matchingNumberCount = findMatchingNumberCount(winningNumber);
+        if (matchingNumberCount != SECOND.getMatchingNumberCount()) {
+            return findPlaceExceptSecondAndThird(matchingNumberCount);
+        }
+        return findPlaceSecondOrThird(bonusNumber);
+    }
+
+    private int findMatchingNumberCount(List<Integer> winningNumbers) {
+        int matchingNumberCount = 0;
+        for (int number : winningNumbers) {
+            if (numbers.contains(number)) {
+                matchingNumberCount++;
+            }
+        }
+        return matchingNumberCount;
+    }
+
+    private int findPlaceExceptSecondAndThird(int matchingNumberCount) {
+        for (WinningInfo info : WinningInfo.values()) {
+            if (matchingNumberCount == info.getMatchingNumberCount()) {
+                return info.getPlace();
+            }
+        }
+        return 0;
+    }
+
+    private int findPlaceSecondOrThird(BonusNumber bonusNumber) {
+        if (numbers.contains(bonusNumber.getNumber())) {
+            return SECOND.getPlace();
+        }
+        return THIRD.getPlace();
     }
 
     public List<Integer> getNumbers() {
