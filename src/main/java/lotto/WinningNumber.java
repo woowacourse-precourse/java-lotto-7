c9ebb9545f9b9cd1f92e2bba9bfc51fab4078675
33jyu33 <jy04169@gmail.com>
2024-11-02 14:21:20 +0900

    feat(WinningNumber): create class, return list of winning numbers

---
+++ src/main/java/lotto/WinningNumber.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers;
+
+    public WinningNumber(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
