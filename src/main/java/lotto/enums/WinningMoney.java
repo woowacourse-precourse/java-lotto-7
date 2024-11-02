722b746715b709cfd3aaffa8ebb78aa7f7db578f
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 07:01:31 +0900

    feat : 순위에 따른 금액 enum 구현

--- src/main/java/lotto/enums/WinningMoney.java
+++ src/main/java/lotto/enums/WinningMoney.java
@@ -1,4 +1,15 @@
 package lotto.enums;
 
 public enum WinningMoney {
+    FIRST_PRIZE_MONEY("2,000,000,000원"),
+    SECOND_PRIZE_MONEY("30,000,000원"),
+    THIRD_PRIZE_MONEY("1,500,000원"),
+    FOURTH_PRIZE_MONEY("50,000원"),
+    FIFTH_PRIZE_MONEY("5,000원");
+
+    private final String prizeMoney;
+
+    WinningMoney(String prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
 }
