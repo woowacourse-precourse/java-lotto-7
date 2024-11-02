ac3177bda9d430b515a337a3c8ef9b5a954682ad
howareyoo <newworld121@naver.com>
2024-11-03 02:09:06 +0900

    feat: add user purcharse price method

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -15,11 +15,24 @@ public class LottoController {
         return userNumbers;
     }
 
+    public Integer getUserPurcharsePrice(String userInput) {
+        userInput = userInput.trim();
+        return parseMoney(userInput);
+    }
+
     private int splitNumber(String userInput) {
         try {
             return Integer.parseInt(userInput);
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
+        }
+    }
+
+    private int parseMoney(String userInput) {
+        try {
+            return Integer.parseInt(userInput);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액 입력은 숫자여야 합니다.");
         }
     }
 
