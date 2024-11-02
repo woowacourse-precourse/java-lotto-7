8e97a4cbb10c4736c957482be85780367c0d00ca
soeun2537 <soeun2537@gmail.com>
2024-11-02 14:44:59 +0900

    test: 정적 팩토리 메서드 of로 WinningLotto 객체를 생성하는지 확인하는 단위 테스트 추가

---
+++ src/test/java/lotto/model/domain/WinningLottoTest.java
@@ -0,0 +1,27 @@
+package lotto.model.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @Test
+    @DisplayName("정적 팩토리 메서드 of로 WinningLotto 객체를 생성하는지 확인")
+    void of() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Integer bonusNumber = 7;
+
+        // when
+        WinningLotto winningLotto = WinningLotto.of(numbers, bonusNumber);
+
+        // then
+        assertThat(winningLotto).isNotNull();
+        assertThat(winningLotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
+    }
+}
