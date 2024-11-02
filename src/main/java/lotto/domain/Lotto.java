3554f6cfe62183987602a07c5d6e16ef477bf077
bowook <mbosx316@naver.com>
2024-11-02 17:19:38 +0900

    refactor(Lotto) : Lotto 도메인 리팩토링
    - Lotto 도메인 내에서 검증하도록 기능 개선

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,14 +1,48 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
-import lotto.validator.LottoValidator;
+import java.util.Set;
+import lotto.constants.Constants;
+import lotto.validator.exception.ErrorMessage;
+import lotto.validator.exception.LottoException;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        LottoValidator.validateProcess(numbers);
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        int numbersLength = numbers.size();
+        if (numbersLength != Constants.LOTTO_LENGTH.getValue()) {
+            throw LottoException.from(ErrorMessage.WINNING_LOTTO_IS_NOT_SIX_LENGTH);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < Constants.LOTTO_MIN_NUMBER.getValue() || number > Constants.LOTTO_MAX_NUMBER.getValue()) {
+                throw LottoException.from(ErrorMessage.WINNING_LOTTO_RANGE_ERROR);
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>();
+        for (int number : numbers) {
+            if (!numberSet.add(number)) {
+                throw LottoException.from(ErrorMessage.WINNING_LOTTO_DUPLICATE_ERROR);
+            }
+        }
     }
 
     public List<Integer> getNumbers() {
