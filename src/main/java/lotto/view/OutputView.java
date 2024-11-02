6e25a8ded924231e111d10d9f7bb7f0673b7c65f
chaerish <ccyy8432@naver.com>
2024-11-02 22:55:53 +0900

    feat(OutputView): 로또 구매 내역과, 당첨 통계를 출력하는 로직 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,29 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.Lotto;
+import lotto.LottoResult;
+import lotto.MatchingCountResult;
+
 public class OutputView {
-    public void displayPurchaseCount(int count) {
+    public static void displayPurchaseCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void displayPurchaseLotto(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+    }
+
+    public static void displayResult(LottoResult lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        List<MatchingCountResult> matchingCountResults = lottoResult.matchingCountResults();
+        for (MatchingCountResult matchingCountResult : matchingCountResults) {
+            System.out.print(matchingCountResult.winningCondition().toString());
+            System.out.print(matchingCountResult.conditionCount() + "개");
+        }
+        System.out.println("총 수익률은 " + lottoResult.rate() + "%" + "입니다.");
     }
 }
