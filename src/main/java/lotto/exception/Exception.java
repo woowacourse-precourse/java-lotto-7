29044fb91fef37d6120ca0213d678f3300fb6dcb
JiEung2 <181757@jnu.ac.kr>
2024-11-02 23:45:45 +0900

    feat(exception): 로또 번호가 6개가 아닐 때의 예외 메시지 추가

--- src/main/java/lotto/exception/Exception.java
+++ src/main/java/lotto/exception/Exception.java
@@ -6,6 +6,7 @@ public enum Exception {
     ONLY_DIVISIBLE_BY_LOTTO_PRICE("구입 금액은 1000원 단위로 나누어떨어져야 합니다."),
 
     ONLY_NUMERIC_INPUT_FOR_WINNING_NUMBERS("당첨 번호는 숫자만 입력할 수 있습니다."),
+    LOTTO_NUMBER_SIZE_MUST_BE_SIX("로또 번호는 6개여야 합니다."),
     ;
 
     private static final String PREFIX = "[ERROR] ";
