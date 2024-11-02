bad97b6217f5c885c9697430d888ed22d5165959
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-02 21:39:34 +0900

    feat : 로또 번호 비교 로직 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,18 +1,47 @@
 package lotto.service;
 
-import lotto.validator.BonusNumValidator;
-import lotto.validator.MoneyValidator;
-import lotto.validator.WinningNumValidator;
-import lotto.view.InputView;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+import lotto.utils.InputHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
-    private final InputView inputView;
+    private final InputHandler inputHandler;
 
-    public LottoService(InputView inputView) {
-        this.inputView = inputView;
+    public LottoService(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public List<Lotto> lottoGenerate(int gameTrial) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i<gameTrial; i++){
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottos;
+    }
+
+    public List<Integer> compareLottosWithWinning(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNum) {
+        List<Integer> result = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            result.add(compareLottoWithWinning(winningNumbers, lotto,bonusNum));
+        }
+        return result;
+    }
+
+    private int compareLottoWithWinning(List<Integer> winningNumbers, Lotto lotto, int bonusNum) {
+        List<Integer> numbers = lotto.getNumbers();
+        int cnt =0;
+        for (Integer number : numbers) {
+            if (winningNumbers.contains(number)) {
+                cnt+=1;
+            }
+        }
+        if (cnt==5 && numbers.contains(bonusNum)){
+            cnt+=2;
+        }
+        return cnt;
     }
 
 
