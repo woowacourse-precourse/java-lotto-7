c87a277c802a9c181d51f05923c3c1fcca11bcaa
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-02 19:22:56 +0900

    fix : Remove bad implementation of main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,17 +2,6 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        LottoCount lottoCount = new LottoCount();
-
-        try {
-            inputView.getLottoPurchaseAmount();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        inputView.getWinningNumber();
-        inputView.getBonusNumber();
-        lottoCount.getLottoCount();
+        // TODO: 모든 클래스 작성 후 구현
     }
 }
