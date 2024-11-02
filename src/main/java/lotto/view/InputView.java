43cb1d82005574517fa2377a1bceac6fac4d7131
taek2222 <oot309@daum.net>
2024-11-02 21:50:17 +0900

    refactor(InputView): 줄내림 추가 및 상수화
    - 화면 출력 결과와 동일하도록 줄내림을 추가했습니다.
    
    - 메시지를 상수화해 관리하도록 변경했습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public int readPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -18,6 +19,7 @@ public class InputView {
     }
 
     public String readWinningNumber() {
+        System.out.println();
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         String inputWinning = Console.readLine();
 
@@ -27,7 +29,8 @@ public class InputView {
     }
 
     public String readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println();
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         String inputBonus = Console.readLine();
 
         validateNotNullOrBlank(inputBonus);
