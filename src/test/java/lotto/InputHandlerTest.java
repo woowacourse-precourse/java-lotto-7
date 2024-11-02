1aac8a54cfa3ff7430a085551b4dc56f0b329cad
zionhann <hanzion1108@gmail.com>
2024-11-02 17:43:27 +0900

    test: 로또 구입 금액이 정수형 최댓값을 초과하는 경우 예외 처리

--- src/test/java/lotto/InputHandlerTest.java
+++ src/test/java/lotto/InputHandlerTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,9 +13,16 @@ public class InputHandlerTest {
     @DisplayName("로또 구입 금액의 단위는 천 원이다.")
     @ParameterizedTest
     @ValueSource(strings = {"1000", "2000", "5000", "10000"})
-    void lottoUnitBudgetShouldBe1000Won(String input) {
-        int budget = InputHandler.toInt(input);
+    void purchaseUnit_shouldBe_1000Won(String input) {
+        int purchaseAmount = InputHandler.toInt(input);
 
-        assertThat(budget % 1000).isZero();
+        assertThat(purchaseAmount % 1000).isZero();
+    }
+
+    @DisplayName("로또 구입 금액이 정수형 최댓값을 초과하는 경우 예외처리")
+    @Test
+    void throwException_when_purchaseAmountExceedsMaxInt() {
+        assertThatThrownBy(() -> InputHandler.toInt("22000000000"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
