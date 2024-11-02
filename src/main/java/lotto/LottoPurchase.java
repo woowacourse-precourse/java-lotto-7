f53472c1d92524e1a83b15d473d10d6a7299d6a4
Hujin0322 <hongyuj0322@naver.com>
2024-11-03 02:57:57 +0900

    refactor(LottoPurchase): 입력 검증 로직 및 재입력 요청 기능 추가
    - 사용자로부터 입력 받은 뒤, AmountValidator 클래스를 사용하여 검증 수행 추가
    - 검증 불만족 시, 오류 메시지 출력 후에 재입력 요청 추가
    - 검증이 만족될 때까지 반복

--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -3,9 +3,26 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoPurchase {
+    private final AmountValidator amountValidator = new AmountValidator();
+
     public int inputPurchaseAmount() {
-        System.out.print("구입금액을 입력해 주세요. : ");
-        String input = Console.readLine().trim();
-        return Integer.parseInt(input) / 1000;
+        int lottoCount = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("구입금액을 입력해 주세요. : ");
+            String input = Console.readLine().trim();
+
+            try {
+                amountValidator.validateInput(input);
+                int lottoAmount = amountValidator.parseInputToInt(input);
+                amountValidator.validateAmount(lottoAmount);
+                lottoCount = lottoAmount / 1000;
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return lottoCount;
     }
 }
