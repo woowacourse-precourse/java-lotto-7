2cd2b3b23c47439a91e1233f478df5e0db2b073c
softmoca <acyc0909@gmail.com>
2024-11-02 14:05:11 +0900

    feat : 숫자 형식 검증 기능 구현

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -14,14 +14,24 @@ public class View {
         return Integer.parseInt(amountInput);
     }
 
+    private static void validatePurchaseAmount(String inputValue) {
+        validateNumeric(inputValue);
+        validateEmpty(inputValue);
+
+    }
+
     private static void validateEmpty(String inputValue) {
         if (inputValue == null || inputValue.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
         }
     }
 
-    private static void validatePurchaseAmount(String inputValue) {
-        validateEmpty(inputValue);
+    private static void validateNumeric(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식이 아닙니다.");
+        }
     }
 
 
