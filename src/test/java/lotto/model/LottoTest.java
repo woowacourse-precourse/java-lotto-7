5455407f8e85e545e73b41894a93afcc0e62f92c
Hanjjong <tim4974@naver.com>
2024-11-03 02:09:40 +0900

    test: 로또 객체 생성시 예외 테스트 구현

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
 
 public class LottoTest {
 
@@ -22,6 +22,21 @@ public class LottoTest {
                 {List.of(1,2,3,4,5,6)},
                 {List.of(2,3,4,5,6,7)},
                 {List.of(12,23,34,45,11,25)}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("inValidLottoNumberProvider")
+    @DisplayName("로또 객체 생성시 예외 테스트")
+    public void createLottoExceptionTest(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers));
+    }
+
+    private static Object[][] inValidLottoNumberProvider() {
+        return new Object[][] {
+                {List.of(1,2,3,4,5,46)},
+                {List.of(2,3,4,5,6,7,8)},
+                {List.of(12,23,34,48,11,25)}
         };
     }
 }
