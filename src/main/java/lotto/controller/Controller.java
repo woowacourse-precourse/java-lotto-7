03ab66a31669b3279ee2955913fced19b2ce39b5
wooqqq <sge0225@naver.com>
2024-11-03 05:21:00 +0900

    feat(Lottos): 구입한 로또 관리 클래스 생성

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,9 +1,9 @@
 package lotto.controller;
 
-import lotto.model.Lotto;
-import lotto.model.LottoGenerator;
-import lotto.model.LottoService;
-import lotto.model.Money;
+import lotto.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -12,14 +12,21 @@ public class Controller {
     public void run() {
         Money money = lottoService.getMoney();
 
-        // 테스트용
-        System.out.println("받은 돈: " + money);
-        Lotto testLotto = LottoGenerator.createLotto();
-
         // 구입 금액에 따른 로또 개수 테스트
         System.out.println("로또 개수: " + money.getLottoTicket());
 
-        // 로또 생성 테스트
-        System.out.println(testLotto.getLottoNumbers());
+        Lottos lottos = purchaseLottos(money);
+        System.out.println(lottos.getSize());
+    }
+
+    private Lottos purchaseLottos(Money money) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < money.getLottoTicket(); i++) {
+            Lotto lotto = LottoGenerator.createLotto();
+            lottos.add(lotto);
+        }
+
+        return new Lottos(lottos);
     }
 }
---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public int getSize() {
+        return lottos.size();
+    }
+}
