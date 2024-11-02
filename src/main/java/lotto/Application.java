fa1b7b06aa4d472b90c1406160ba939e6d962499
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-02 18:46:16 +0900

    fix(app) : delete unused import
    불필요한 import 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,7 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.concurrent.ConcurrentSkipListMap;
 import lotto.controller.LottoController;
-import lotto.domain.Lotto;
+
 
 public class Application {
     public static void main(String[] args) {
