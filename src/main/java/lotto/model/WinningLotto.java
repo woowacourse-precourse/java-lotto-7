541ac067f6c9f73f71485210dba41654442ef150
mingking2 <mingee641@gmail.com>
2024-11-02 16:33:47 +0900

    feat: 당첨로또 클래스 추가

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public class WinningLotto {
+    private Lotto winningLotto;
+    private int bonusNumber;
+
+    public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public boolean isBonusMatch(Lotto lotto) {
+        return lotto.hasNumber(bonusNumber);
+    }
+
+    public int countMatchingNumbers(Lotto lotto) {
+        return winningLotto.countingMatchNumbers(lotto);
+    }
+}
