d50048b156f7c4126a901cc6fe2ebcf25ec0b73f
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-02 17:08:22 +0900

    feat(InputView): 입력을 위한 메서드 정의

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,25 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
+    public int readPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public String readWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBERS);
+        return Console.readLine();
+    }
+
+    public int readBonusNumber() {
+        System.out.println(ASK_BONUS_NUMBER);
+        return Integer.parseInt(Console.readLine());
+    }
 }
