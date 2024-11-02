cbaa466af54b89505f28885500098dab1f105b8f
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-03 02:46:40 +0900

    feat(displayLottoStatics): 당첨 통계를 출력하는 기능 구현

--- src/main/java/lotto/view/LottoStaticsHandler.java
+++ src/main/java/lotto/view/LottoStaticsHandler.java
@@ -3,10 +3,12 @@ package lotto.view;
 import lotto.domain.Rank;
 import lotto.service.LottoService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LottoStaticsHandler {
-     private LottoService lottoService;
+    private LottoService lottoService;
 
     public LottoStaticsHandler(LottoService lottoService) {
         this.lottoService = lottoService;
@@ -15,5 +17,24 @@ public class LottoStaticsHandler {
     public void displayLottoStatics(List<Rank> ranks) {
         System.out.println("당첨 통계");
         System.out.println("---");
+        Map<Rank, Integer> rankCounts = lottoService.matchingWinningNumbers(ranks);
+        List<Rank> rankOrder = new ArrayList<>(List.of(Rank.FIRST, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST));
+        for (Rank rank : rankOrder) {
+            String text = displayLottoStaticsText(rank, rankCounts.get(rank));
+            System.out.println(text);
+        }
+    }
+
+    public String displayLottoStaticsText(Rank rank, int rankCount) {
+        if (rank.isRequiresBonus()) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%.d원) - %d개", rank.getMatchCount(), rank.getPrize(), rankCount);
+        }
+        return String.format("%d개 일치 (%.d원) - %d개", rank.getMatchCount(), rank.getPrize(), rankCount);
+    }
+
+    public void printReturn(List<Rank> ranks, int purchaseAmount) {
+        double returnRate = lottoService.calculateReturn(ranks, purchaseAmount);
+        String text = String.format("총 수익률은 %.2f%입니다.", returnRate);
+        System.out.println(text);
     }
 }
