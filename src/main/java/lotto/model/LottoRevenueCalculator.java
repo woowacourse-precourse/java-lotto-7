d7a9865a45425ed472f861afad51fea1874aff80
Dohun Kim <dohun8758@naver.com>
2024-11-02 22:33:04 +0900

    feat: 로또 수익률 계산을 담당하는 LottoRevenueCalculator 구현

---
+++ src/main/java/lotto/model/LottoRevenueCalculator.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.Map;
+
+public class LottoRevenueCalculator {
+    private final Map<LottoRank, Integer> lottoResult;
+    private final int moneySpent;
+
+    private LottoRevenueCalculator(Map<LottoRank, Integer> lottoResult, int moneySpent) {
+        this.lottoResult = lottoResult;
+        this.moneySpent = moneySpent;
+    }
+
+    public static LottoRevenueCalculator of(Map<LottoRank, Integer> lottoResult, int moneySpent) {
+        return new LottoRevenueCalculator(lottoResult, moneySpent);
+    }
+
+    public double calculateRevenue() {
+        int moneyEarned = lottoResult.entrySet()
+                .stream()
+                .mapToInt(entry -> entry.getKey().getPrizeAmount() * entry.getValue())
+                .sum();
+        return (double) moneyEarned / moneySpent;
+    }
+
+}
