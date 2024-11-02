33484ce1bd0eab1c5727d4ec90d0b65636418c87
dhrgodms <dhrgodms123@gmail.com>
2024-11-02 11:24:19 +0900

    refactor: IssueTicketController.java 내 함수 분리

--- src/main/java/lotto/Controller/IssueTicketController.java
+++ src/main/java/lotto/Controller/IssueTicketController.java
@@ -8,18 +8,20 @@ import lotto.View.OutputIssuedTicketView;
 import java.util.List;
 
 public class IssueTicketController {
-    MyLottos myLottos = new MyLottos();
-    OutputIssuedTicketView outputLottoView = new OutputIssuedTicketView();
-
     public MyLottos issueTickets(int numberOfTicket) {
-        myLottos = new MyLottos();
-        for (int i = 0; i < numberOfTicket; i++) {
-            myLottos.addLotto(new Lotto(makeRandomNumbers()));
-        }
+        OutputIssuedTicketView outputLottoView = new OutputIssuedTicketView();
+        MyLottos myLottos = makeIssuedTickets(numberOfTicket);
         outputLottoView.printMylottos(myLottos);
         return myLottos;
     }
 
+    private MyLottos makeIssuedTickets(int numberOfTicket) {
+        MyLottos myLottos = new MyLottos();
+        for (int i = 0; i < numberOfTicket; i++) {
+            myLottos.addLotto(new Lotto(makeRandomNumbers()));
+        }
+        return myLottos;
+    }
 
     private List<Integer> makeRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
