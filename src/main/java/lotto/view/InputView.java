6b3f7f3a8bd12503797efa528bb42c531897b77b
dongjae <harold306122@gmail.com>
2024-11-03 00:48:23 +0900

    refactor: InputView 클래스의 중복 메서드 통합 및 코드 간결화
    - 구입 금액 입력 메서드를 하나로 통합하여 중복 기능 제거
    - 입력 관련 메서드들이 단일 책임 원칙에 따라 하나의 작업만 수행하도록 리팩토링
    - InputView 클래스에서 입력 로직을 간결하게 개선하여 코드 가독성 및 유지보수성 향상

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public int inputPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
-    }
 
-    public String inputPurchaseAmountString() {
+    public String inputPurchaseAmount() {
         System.out.println("구입 금액을 입력해 주세요.");
         return Console.readLine();
     }
