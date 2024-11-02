13dd475fdb647f69144c441eff4bd0fef8208aae
Mirim <marinrim23@gmail.com>
2024-11-02 23:02:22 +0900

    feat(PaymentValidator): Validate for input payment

--- src/main/java/lotto/controller/PurchasingController.java
+++ src/main/java/lotto/controller/PurchasingController.java
@@ -1,18 +1,21 @@
 package lotto.controller;
 
 import lotto.service.PurchasingService;
+import lotto.validator.PaymentValidator;
 import lotto.view.InputPaymentView;
 
 public class PurchasingController {
     private final InputPaymentView inputPaymentView;
+    private final PaymentValidator validator;
 
-    public PurchasingController(InputPaymentView inputPaymentView) {
+    public PurchasingController(InputPaymentView inputPaymentView, PaymentValidator validator) {
         this.inputPaymentView = inputPaymentView;
+        this.validator = validator;
     }
 
     public void purchaseLottoTickets() {
         String inputPayment = inputPaymentView.getPayment();
-        int payment = Integer.parseInt(inputPayment);
+        int payment = validator.validPayment(inputPayment);
         PurchasingService purchasingService = new PurchasingService();
         purchasingService.purchaseTickets(payment);
     }
---
+++ src/main/java/lotto/validator/PaymentValidator.java
@@ -0,0 +1,23 @@
+package lotto.validator;
+
+public class PaymentValidator {
+
+    public boolean validPayment(String inputPayment) {
+        try {
+            Integer.parseInt(inputPayment);
+            dividedBy1000(inputPayment);
+            return true;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+    }
+
+    private boolean dividedBy1000(String inputPayment) {
+        int payment = Integer.parseInt(inputPayment);
+        return payment % 1000 == 0;
+        // 유효하지 않은 숫자라고 예외 추가하기
+    }
+
+
+
+}
