e1a0449042a8c72a59d1460fa07cbd77df769ef9
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-11-02 22:44:08 +0900

    feat: Implement LottoGame class with purchase amount input
    LottoGame 클래스에서 구입 금액 입력과 기본적인 유효성 검사를 수행하는 기능을 구현하였습니다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,2 +1,25 @@
-package lotto;public class LottoGame {
+package lotto;
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoGame {
+
+    public static void main(String[] args) {
+        try {
+            int purchaseAmount = getPurchaseAmount();
+            LottoValidator.validateAmount(purchaseAmount);
+            int lottoCount = purchaseAmount / 1000;
+            System.out.println(lottoCount + "개를 구매했습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static int getPurchaseAmount() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력해 주세요.");
+        }
+    }
 }
