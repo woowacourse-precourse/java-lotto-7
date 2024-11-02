54e2fe974c78a6ada571608b4757bea1aea2a28a
J0YERIM <chosarah70@gmail.com>
2024-11-03 02:04:12 +0900

    feat(controller): 보너스 번호 입력 처리 로직 추가
    - inputBonusNumber 메서드 추가하여 보너스 번호 입력 및 유효성 검증 로직 구현
    - 잘못된 입력 시 예외 메시지를 출력하고 재입력 받도록 예외 처리 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,17 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.Lotto;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 
 public class LottoController {
 
     public void run() {
         PurchaseAmount purchaseAmount = inputPurchaseAmount();
+        Lotto winningNumbers = inputWinningNumbers();
+        WinningLotto winningLotto = inputBonusNumber(winningNumbers);
     }
 
     private PurchaseAmount inputPurchaseAmount() {
@@ -16,6 +21,26 @@ public class LottoController {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputPurchaseAmount();
+        }
+    }
+
+    private Lotto inputWinningNumbers() {
+        try {
+            List<Integer> numbers = InputView.inputWinningNumbers();
+            return new Lotto(numbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningNumbers();
+        }
+    }
+
+    private WinningLotto inputBonusNumber(Lotto winningNumbers) {
+        try {
+            int bonusNumber = InputView.inputBonusNumber();
+            return new WinningLotto(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputBonusNumber(winningNumbers);
         }
     }
 }
