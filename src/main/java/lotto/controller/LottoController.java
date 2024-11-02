671b080f3df3d3537994a72b9ef8d37fd3aecd87
donghyunn1 <dongdong0516@naver.com>
2024-11-02 23:35:28 +0900

    refactor: controller 로직 생성자 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -19,35 +19,56 @@ public class LottoController {
     }
 
     public void run() {
-        Purchase purchase = null;
-        while (purchase == null) {
+        Purchase purchase = createPurchase();
+
+        LottoGenerator lottos = showPrintLottos(outputView, purchase);
+
+        WinningNumber winningNumber = createWinningNumber();
+
+        WinningStatics winningStatics = showWinningStatics(outputView, lottos, winningNumber);
+
+        showEarning(outputView, purchase, winningStatics);
+    }
+
+    private Purchase createPurchase() {
+        while (true) {
             try {
-                purchase = new Purchase(inputView.purchaseInput());
+                return new Purchase(inputView.purchaseInput());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
-        LottoGenerator lottos = new LottoGenerator(purchase.numberOfLotto());
-        outputView.printLottoStatus(lottos.getLottos());
-
-        WinningNumber winningNumber = null;
-        while (winningNumber == null) {
+    private WinningNumber createWinningNumber() {
+        while (true) {
             try {
                 String winningNum = inputView.winningNumberInput();
                 String bonusNumber = inputView.bonusInput();
-                winningNumber = new WinningNumber(winningNum, bonusNumber);
+                return new WinningNumber(winningNum, bonusNumber);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
+    private LottoGenerator showPrintLottos(OutputView outputView, Purchase purchase) {
+        LottoGenerator lottos = new LottoGenerator(purchase.numberOfLotto());
+        outputView.printLottoStatus(lottos.getLottos());
+
+        return lottos;
+    }
+
+    private WinningStatics showWinningStatics(OutputView outputView, LottoGenerator lottos, WinningNumber winningNumber) {
         WinningStatics winningStatics = new WinningStatics();
         winningStatics.numOfWinnings(lottos.getLottos(), winningNumber);
         outputView.printWinningStatics(winningStatics);
 
-        int purchaseAmount = purchase.getAmount();
-        Earning earning = new Earning(purchaseAmount, winningStatics.getWinningStatics());
+        return winningStatics;
+    }
+
+    private void showEarning(OutputView outputView, Purchase purchase, WinningStatics winningStatics) {
+        Earning earning = new Earning(purchase.getAmount(), winningStatics.getWinningStatics());
         outputView.printEarning(earning);
     }
 }
