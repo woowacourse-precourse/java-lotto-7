00a2dd5c8cd2d00e4ee6b7ce19f2f6d6043127bc
GunsanHaribo <hth99131@gmail.com>
2024-11-02 20:22:59 +0900

    feat(Lotto.java): 구매한 로또 하나와 당첨 로또를 비교

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,6 +13,22 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public Rank calculateLotto(List<Integer> winningLotto, int bonusNumber) {
+        int basicCount = 0;
+        int bonusCount = 0;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i).equals(winningLotto.get(i))) {
+                basicCount++;
+            }
+        }
+        if (numbers.contains(bonusNumber)) {
+            bonusCount++;
+        }
+
+        return Rank.calculateRank(basicCount, bonusCount);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,9 +2,14 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -20,5 +25,27 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매한 로또 하나와 당첨 로또를 비교한다.")
+    @ParameterizedTest
+    @MethodSource("provideWiningLottoAndBonusAndRank")
+    void 구매한_로또_하나와_당첨_로또를_비교한다(List<Integer> winningLotto, int bonusNumber, Rank expectedRank) {
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Rank rank = userLotto.calculateLotto(winningLotto, bonusNumber);
+
+        assertThat(rank).isEqualTo(expectedRank);
+    }
+
+    static Stream<Arguments> provideWiningLottoAndBonusAndRank() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 8, Rank.FIRST),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), 6, Rank.SECOND),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 7), 8, Rank.THIRD),
+                Arguments.of(List.of(1, 2, 3, 4, 7, 8), 9, Rank.FOURTH),
+                Arguments.of(List.of(1, 2, 3, 7, 8, 9), 10, Rank.FIFTH),
+                Arguments.of(List.of(7, 8, 9, 10, 5, 6), 8, Rank.NONE)
+        );
     }
 }
