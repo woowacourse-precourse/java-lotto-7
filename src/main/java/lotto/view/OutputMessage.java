8da36d95f1623585ce70c82b779cc6b0446dae88
eb9862 <luclead622@gmail.com>
2024-11-02 23:57:19 +0900

    feat: 출력 메시지 템플릿 추가
    - 각 등수에 대한 당첨 내역 메시지 추가
    - 수익률에 대한 메시지 추가

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -2,7 +2,10 @@ package lotto.view;
 
 public enum OutputMessage {
 
-    OUTPUT_PURCHASE_QUANTITY("\n%d개를 구매했습니다.");
+    OUTPUT_PURCHASE_QUANTITY("\n%d개를 구매했습니다."),
+    OUTPUT_EXCEPT_SECOND_PLACE_RESULT("%d개 일치 (%,d원) - %d개"),
+    OUTPUT_SECOND_PLACE_RESULT("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
+    OUTPUT_EARNING_RATE("총 수익률은 %.1f%%입니다");
 
     private final String messageTemplate;
 
