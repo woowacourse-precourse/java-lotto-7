b2d52d19da3714dca39304128f6ff8aeb1e4e5c5
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 17:45:08 +0900

    feat(service): LottoResults 클래스에서 Rank별 결과 저장을 위한 lottoResults 맵 추가

---
+++ src/main/java/lotto/service/LottoResults.java
@@ -0,0 +1,11 @@
+package lotto.service;
+
+import java.util.LinkedHashMap;
+import java.util.SequencedMap;
+import lotto.domain.Rank;
+
+public class LottoResults {
+    private final SequencedMap<Rank,Integer> lottoResults = new LinkedHashMap<>();
+
+
+}
