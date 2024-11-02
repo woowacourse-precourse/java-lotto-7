2364cecb189ba03c68e18f593c2ed4e577b4643f
DongYeop <s01054830455@gmail.com>
2024-11-02 17:45:42 +0900

    refactor(LottoRank): Enum 상수 순서 변경
    - 출력 순서를 바꾸기 위해서 변경하였음

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -2,12 +2,12 @@ package lotto.model;
 
 public enum LottoRank {
 
-    FIRST("6개 일치 (2,000,000,000원)", 2000000000L),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000L),
-    THIRD("5개 일치 (1,500,000원)", 1500000L),
-    FOURTH("4개 일치 (50,000원)", 50000L),
+    NO_LUCK("3개 미만 일치 (0원)", 0L),
     FIFTH("3개 일치 (5,000원)", 5000L),
-    NO_LUCK("3개 미만 일치 (0원)", 0L);
+    FOURTH("4개 일치 (50,000원)", 50000L),
+    THIRD("5개 일치 (1,500,000원)", 1500000L),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000L),
+    FIRST("6개 일치 (2,000,000,000원)", 2000000000L);
 
     private final String description;
     private final long winningAmount;
