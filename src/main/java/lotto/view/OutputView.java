e348b1f55228ad2c5b8f9aca0b4df147727dacc8
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 03:45:00 +0900

    feat(OutputView): 로또 구매 개수를 알려주는 출력 메시지 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,11 +5,16 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
+import static lotto.constants.ViewMessage.LOTTO_PURCHASE_COUNT;
 
 public class OutputView {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printLottoPurchaseCount(int lottoCount) {
+        printMessage(String.format(LOTTO_PURCHASE_COUNT.getText(), lottoCount));
     }
 
     public static void printLottoListPrompt(List<Lotto> lottos) {
