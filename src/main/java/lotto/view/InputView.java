5024f92c61f0d94f8ccee628b7a0586d6f9b8a0e
JoonKyo <joonkyolee@gmail.com>
2024-11-02 18:24:43 +0900

    feat(InputView): 로또 당첨 번호를 입력받기 위한 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String ASK_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    public static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public static String askPurchasePrice() {
         System.out.println(ASK_PURCHASE_PRICE);
+        return Console.readLine();
+    }
+
+    public static String askWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBER);
         return Console.readLine();
     }
 }
