884c130dbb26ceba911c8b9477611107e4c9ba61
Ahnsukjoo <bos98@naver.com>
2024-11-03 07:54:44 +0900

    feat: PurchaseAmount 클래스에 예외 처리 추가
    PurchaseAmount 클래스의 input 메서드에 예외 처리 로직을 추가했습니다.
    
    - 사용자 입력 시 발생할 수 있는 예외를 처리하기 위해 handleInputException 메서드를 구현했습니다.
    - 구입 금액이 양수이며 1,000원 단위인지 검증하는 로직을 추가했습니다.
    - 잘못된 입력에 대해 명확한 오류 메시지를 출력하도록 개선했습니다.
    - 에러 메시지를 Enum클래스에 추가했습니다.

--- src/main/java/lotto/view/input/InputMessageEnum.java
+++ src/main/java/lotto/view/input/InputMessageEnum.java
@@ -5,10 +5,11 @@ public enum InputMessageEnum {
     WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     ERROR_PREFIX("[ERROR] "),
-    INVALID_NUMBER_ERROR(ERROR_PREFIX.getMessage() + "로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATE_NUMBER_ERROR(ERROR_PREFIX.getMessage() + "로또 번호에 중복된 숫자가 있습니다."),
-    INVALID_COUNT_ERROR(ERROR_PREFIX.getMessage() + "로또 번호는 6개여야 합니다."),
-    DUPLICATE_BONUS_NUMBER_ERROR(ERROR_PREFIX.getMessage() + "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    INVALID_NUMBER_ERROR(ERROR_PREFIX.message + "로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_NUMBER_ERROR(ERROR_PREFIX.message + "로또 번호에 중복된 숫자가 있습니다."),
+    INVALID_COUNT_ERROR(ERROR_PREFIX.message + "로또 번호는 6개여야 합니다."),
+    DUPLICATE_BONUS_NUMBER_ERROR(ERROR_PREFIX.message + "보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    INVALID_AMOUNT_ERROR(ERROR_PREFIX.message + "구입금액은 1,000원 단위의 양수여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/view/input/PurchaseAmount.java
+++ src/main/java/lotto/view/input/PurchaseAmount.java
@@ -5,8 +5,31 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class PurchaseAmount {
 
     public int input() {
-        InputMessageEnum.PURCHASE_AMOUNT.printMessage();
-        String purchaseAmount = readLine();
-        return Integer.parseInt(purchaseAmount);
+        while (true) {
+            try {
+                InputMessageEnum.PURCHASE_AMOUNT.printMessage();
+                String purchaseAmount = readLine();
+                int amount = Integer.parseInt(purchaseAmount);
+                validatePurchaseAmount(amount);
+                return amount;
+            } catch (Exception e) {
+                handleInputException(e);
+            }
+        }
+    }
+
+    private void validatePurchaseAmount(int amount) {
+        if (amount <= 0 || amount % 1000 != 0) {
+            throw new IllegalArgumentException(InputMessageEnum.INVALID_AMOUNT_ERROR.getMessage());
+        }
+    }
+
+    private void handleInputException(Exception e) {
+        if (e instanceof NumberFormatException) {
+            System.out.println(InputMessageEnum.INVALID_AMOUNT_ERROR.getMessage());
+        }
+        if (e instanceof IllegalArgumentException) {
+            System.out.println(e.getMessage());
+        }
     }
 }
