b71dfb7705d37cecd62d486a5c38e937e52c2edf
tentenball <lth8905@gmail.com>
2024-11-02 21:29:15 +0900

    test: 정적 팩토리 메서드 사용으로 인한 테스트 수정

--- src/test/java/lotto/domain/CustomerTest.java
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -12,13 +12,13 @@ public class CustomerTest {
 	@DisplayName("Customer 클래스가 올바르게 생성된다.")
 	@Test
 	void Customer_클래스가_올바르게_생성된다() {
-		assertThat(new Customer(3000)).isInstanceOf(Customer.class);
+		assertThat(Customer.from(3000)).isInstanceOf(Customer.class);
 	}
 
 	@DisplayName("올바른 로또 개수를 계산한다.")
 	@Test
 	void 올바른_로또_개수를_계산한다() {
-		Customer customer = new Customer(3000);
+		Customer customer = Customer.from(3000);
 		assertThat(customer.calculateAmount()).isEqualTo(3);
 	}
 }
