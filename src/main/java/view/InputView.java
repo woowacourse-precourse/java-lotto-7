48d5059a3d0b878a84c72ae5506014c6f9864c15
dawit0905 <dawit0905@gmail.com>
2024-11-02 21:19:56 +0900

    feat: inputview에 당첨 번호 입력, 보너스 번호 입력 작성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,9 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -11,4 +14,18 @@ public class InputView {
         return LottoPurchaseAmount;
     }
 
+    public List<Integer> inputWinnerLottoNumbers() {
+        System.out.println("\n당첨 번호를 입력해주세요.");
+        String input = Console.readLine();
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
 }
