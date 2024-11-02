b3ad19369b62d6b62c7e6e7c9f8e3a23af19cddb
chahyunsoo <chahs1012@naver.com>
2024-11-03 04:00:49 +0900

    feat(GameMessage) : GameMessage enum을 활용한 출력 메시지 추가
    * printGameMessage 메서드 오버로딩을 통해 인자가 있는 메시지와 없는 메시지를 모두 지원합니다.
    
    * BOUGHT_LOTTO_MESSAGE에 줄바꿈 추가하여 출력 시 한 줄 공백 적용(추후 개선 가능)

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -1,7 +1,8 @@
 package lotto.constant;
 
 public enum GameMessage {
-    GAME_PRINT_MESSAGE("구입금액을 입력해 주세요.");
+    GAME_PRINT_MESSAGE("구입금액을 입력해 주세요."),
+    BOUGHT_LOTTO_MESSAGE("\n%d개를 구매했습니다.\n");
 
     private final String message;
 
@@ -11,5 +12,9 @@ public enum GameMessage {
 
     public void printGameMessage() {
         System.out.println(this.message);
+    }
+
+    public void printGameMessage(Object... args) {
+        System.out.printf(this.message, args);
     }
 }
