d9012ecb38a77f0786a6179229a1b2dc074d6e4e
navangs <43688529+insertend23@users.noreply.github.com>
2024-11-02 15:16:32 +0900

    Test(Application): write test code first for TDD

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,49 +1,29 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
     void 기능_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
+        assertRandomUniqueNumbersInRangeTest(() -> {
+                run("8000", "1,2,3,4,5,6", "7");
+                assertThat(output()).contains("8개를 구매했습니다.", "[8, 21, 23, 41, 42, 43]", "[3, 5, 11, 16, 32, 38]",
+                    "[7, 11, 16, 35, 36, 44]", "[1, 8, 11, 31, 41, 42]", "[13, 14, 16, 38, 42, 45]",
+                    "[7, 11, 30, 40, 42, 43]", "[2, 13, 22, 32, 38, 45]", "[1, 3, 5, 14, 22, 45]", "3개 일치 (5,000원) - 1개",
+                    "4개 일치 (50,000원) - 0개", "5개 일치 (1,500,000원) - 0개", "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개", "총 수익률은 62.5%입니다.");
+            }, List.of(8, 21, 23, 41, 42, 43), List.of(3, 5, 11, 16, 32, 38), List.of(7, 11, 16, 35, 36, 44),
+            List.of(1, 8, 11, 31, 41, 42), List.of(13, 14, 16, 38, 42, 45), List.of(7, 11, 30, 40, 42, 43),
+            List.of(2, 13, 22, 32, 38, 45), List.of(1, 3, 5, 14, 22, 45));
     }
 
     @Test
@@ -52,6 +32,83 @@ class ApplicationTest extends NsTest {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @Test
+    void purchaseAmountBlankTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void purchaseAmountIntegerTest() {
+        assertSimpleTest(
+            () -> assertThatThrownBy(() -> runException("12j")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void purchaseAmountMoreThanLimitTest() {
+        assertSimpleTest(
+            () -> assertThatThrownBy(() -> runException("4611687000")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void purchaseAmount1000WonUnitsTest() {
+        assertSimpleTest(
+            () -> assertThatThrownBy(() -> runException("1300")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void winningNumbersBlankTest() {
+        assertSimpleTest(
+            () -> assertThatThrownBy(() -> runException("1000", "")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void winningNumbersIntegerTest() {
+        assertSimpleTest(
+            () -> assertThatThrownBy(() -> runException("1000", "12j")).isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void winningNumbersMoreThanSixTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,6,7")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void winningNumbersInRangeTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "100,2,3,4,5,6,7")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void winningNumbersDuplicateTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,1,2,3,4,5,6")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void bonusNumberBlankTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,6", "")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void bonusNumberIntegerTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,6", "12j")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void bonusNumberInRangeTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,6", "100")).isInstanceOf(
+            IllegalArgumentException.class));
+    }
+
+    @Test
+    void bonusNumberDuplicateTest() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1000", "1,2,3,4,5,6", "1")).isInstanceOf(
+            IllegalArgumentException.class));
     }
 
     @Override
