b544407c50060da0db97eefacd5c6e432cfa7859
delena0702 <delena0702@gmail.com>
2024-11-03 01:16:57 +0900

    refactor: Lotto 내 validate 함수 Validator로 분리
    로또 확인 로직을 추후 다른 클래스에서 이용하기 위해 분리함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,14 +9,8 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        Validator.checkLottoNumbers(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
     }
 
     @Override
--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,10 +1,21 @@
 package lotto;
 
+import java.util.List;
+
 public class Validator {
     public static void checkValidPurchaseCount(long amount) {
         if (!(amount > 0))
             throw new IllegalArgumentException("[ERROR] 양수가 아닙니다.");
         if (amount % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 입력이 1000으로 나누어 떨어지지 않습니다.");
+    }
+
+    public static void checkLottoNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().distinct().toList().size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 숫자가 있습니다.");
+        }
     }
 }
