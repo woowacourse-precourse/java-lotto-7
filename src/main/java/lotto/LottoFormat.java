91be818b6b0d53a793883f7439f03738d1983bd0
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 16:33:32 +0900

    feat(lotto format): lotto format 상수 클래스 추가

---
+++ src/main/java/lotto/LottoFormat.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public final class LottoFormat {
+
+    public static final int NUMBER_COUNT = 6;
+    public static final String DELIMITER = ",";
+    public static final String PREFIX = "[";
+    public static final String SUFFIX = "]";
+}
