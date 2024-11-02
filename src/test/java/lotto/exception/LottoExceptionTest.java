40e3df109928690427b9f5fbf9aca4bebde654e3
yuntasha <jaljayo85@gmail.com>
2024-11-02 17:40:27 +0900

    test(LottoExceptionTest): add test
    Add test for write LottoException

---
+++ src/test/java/lotto/exception/LottoExceptionTest.java
@@ -0,0 +1,37 @@
+package lotto.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoExceptionTest {
+    String ERROR = "[ERROR] ";
+
+    @Test
+    @DisplayName("IllegalArgumentException으로 잘 작동하는지")
+    void isSuccess() {
+        // given
+        LottoException lottoException = new LottoException(ErrorMessage.NOT_SIX_NUM);
+
+        // when
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
+            throw lottoException;
+        });
+        // then
+        assertEquals(ErrorMessage.NOT_SIX_NUM, illegalArgumentException.getMessage());
+    }
+
+    @Test
+    @DisplayName("에러메세지 리턴")
+    void getErrorMessage() {
+        // given
+        LottoException lottoException = new LottoException(ErrorMessage.NOT_SIX_NUM);
+
+        // when
+        String message = lottoException.makeErrorMessage();
+
+        // then
+        assertEquals(ERROR+ErrorMessage.NOT_SIX_NUM.getMessage(), message);
+    }
+}
