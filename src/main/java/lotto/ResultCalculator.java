f878c2ec22ce267abd02bd45e45f56a56221eab4
Etwashoeren <sungmin0609@nate.com>
2024-11-02 18:45:33 +0900

    feat. 당첨 번호와 로또 번호 비교하기 기능 구현

--- src/main/java/lotto/ResultCalculator.java
+++ src/main/java/lotto/ResultCalculator.java
@@ -1,5 +1,33 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ResultCalculator {
 
+    public Map<Rank, Integer> calculateResult(List<Lotto> lottos, Lotto winningNumber, int bonus) {
+        Map<Rank, Integer> result = new HashMap<>();
+
+        for (Lotto lotto : lottos) {
+            int matchCount = countMatches(lotto.getNumbers(), winningNumber.getNumbers());
+            boolean bonusMatch = lotto.getNumbers().contains(bonus);
+            Rank rank = Rank.getRank(matchCount, bonusMatch);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+
+        return result;
+    }
+
+    private int countMatches(List<Integer> lottos, List<Integer> winningNumber) {
+        int count = 0;
+
+        for (int number : lottos) {
+            if (winningNumber.contains(number)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
