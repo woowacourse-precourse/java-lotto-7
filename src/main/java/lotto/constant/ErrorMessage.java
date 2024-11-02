000b3549b294a37b95b964d1a2638a0cb8fa39f8
Jewoo Lee <jaewoo0254@gmail.com>
2024-11-02 11:28:50 +0900

    feat(InputView): 사용자 입력이 null인 경우 예외 처리 기능 추가

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,5 @@
+package lotto.constant;
+
+public class ErrorMessage {
+    public static String NULL_INPUT_ERROR = "[ERROR]: 입력값은 null이 될 수 없습니다.";
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.ErrorMessage;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -8,7 +9,11 @@ public class InputView {
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     private String readLine() {
-        return Console.readLine();
+        String input = Console.readLine();
+        if (input == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_INPUT_ERROR);
+        }
+        return input.trim();
     }
 
     public String getPurchaseAmount() {
