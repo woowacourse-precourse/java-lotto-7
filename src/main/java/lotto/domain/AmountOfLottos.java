5fdee4fcaec1a4af4bb3dcd55b886c6ccbb67b78
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-11-02 12:34:46 +0900

    refactor: 로또 금액 동적 처리

--- src/main/java/lotto/domain/AmountOfLottos.java
+++ src/main/java/lotto/domain/AmountOfLottos.java
@@ -7,14 +7,14 @@ public class AmountOfLottos {
     private final int amount;
 
     public AmountOfLottos(String inputMoney, int lottoPrize) {
-        validate(inputMoney);
+        validate(inputMoney, lottoPrize);
         this.amount = Integer.parseInt(inputMoney) / lottoPrize;
     }
 
-    private void validate(String input) {
+    private void validate(String input, int lottoPrize) {
         try {
             int money = Integer.parseInt(input);
-            if(money % 1000 != 0)
+            if(money % lottoPrize != 0)
                 throw new Exception();
         } catch (Exception e){
             throw new IllegalArgumentException(LottoErrorMessage.MONEY_EXCEPTION.getMsg());
