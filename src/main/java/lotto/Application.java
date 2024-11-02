6c7b95abbd4a1b0948af79bf4aa6d42521f0a9c6
okod <okodee@naver.com>
2024-11-02 16:48:19 +0900

    feat: 로또 구입 금액 입력 받기 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.*;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 로또 구입 금액 입력 받기
+        System.out.println("구입금액을 입력해 주세요.");
+        int inputCash = Integer.parseInt(Console.readLine());
+        // 1000 단위, 숫자 여부 검증 필요
+        int lottoCount = inputCash / 1000;
+
     }
 }
