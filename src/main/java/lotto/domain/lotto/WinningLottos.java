71f648ca4dedf53d3346608d7232bb0756033e64
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-02 12:08:58 +0900

    feat(WinningLottos): 당첨된 로또 리스트 도메인 추가

---
+++ src/main/java/lotto/domain/lotto/WinningLottos.java
@@ -0,0 +1,34 @@
+package lotto.domain.lotto;
+
+import lotto.domain.number.WinningNumbers;
+
+import java.util.List;
+
+public class WinningLottos {
+
+    private final List<WinningLotto> winningLottos;
+
+    private WinningLottos(final PurchasedLottos purchasedLottos, final WinningNumbers winningNumbers, final int bonusNumber) {
+        this.winningLottos = createWinningLottos(purchasedLottos, winningNumbers, bonusNumber);
+    }
+
+    public static WinningLottos of(final PurchasedLottos purchasedLottos, final WinningNumbers winningNumbers, final int bonusNumber) {
+        return new WinningLottos(purchasedLottos, winningNumbers, bonusNumber);
+    }
+
+    private List<WinningLotto> createWinningLottos(
+            final PurchasedLottos purchasedLottos,
+            final WinningNumbers winningNumbers,
+            final int bonusNumber
+    ) {
+        return purchasedLottos.getPurchasedLottos()
+                .stream()
+                .map(lotto -> WinningLotto.createWinningLotto(lotto, winningNumbers, bonusNumber))
+                .filter(winningLotto -> winningLotto.rank() != LottoRank.NONE)
+                .toList();
+    }
+
+    public List<WinningLotto> getWinningLottos() {
+        return winningLottos;
+    }
+}
