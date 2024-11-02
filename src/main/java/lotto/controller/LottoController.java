1fc98fa6f1419136c8b219768275a26999187379
Mini <mini@MiniMac.local>
2024-11-02 18:48:36 +0900

    feat: add game flow with ticket generation and result calculation

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,20 +1,24 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
 import lotto.model.LottoGameInfo;
+import lotto.model.LottoResult;
+import lotto.service.LottoCalculator;
 import lotto.service.LottoGenerator;
 import lotto.ui.InputView;
+import lotto.ui.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static lotto.ui.InputView.getPurchaseAmount;
 
 public class LottoController {
 
     public void startGame() {
         // 구매금액
-        int purchaseAmount = getPurchaseAmount();
+        int purchaseAmount = InputView.getPurchaseAmount();
         LottoGameInfo gameInfo = new LottoGameInfo();
         gameInfo.setPurchaseAmount(purchaseAmount);
+        gameInfo.setLottotickets(purchaseAmount);
 
         //당첨 번호
         List<Integer> winningNumbers = InputView.getWinningNumbers();
@@ -23,6 +27,24 @@ public class LottoController {
         //보너스번호
         int bonusNumber = InputView.getBonusNumber(winningNumbers);
         gameInfo.setBonusNumber(bonusNumber);
+
+        //로또 생성
+        int lottoTicketsCount = gameInfo.getLottotickets();
+        OutputView.countLotto(lottoTicketsCount);
+        List<Lotto> lottoTickets = LottoGenerator.generateLottoTickets(lottoTicketsCount);
+        for (Lotto lotto : lottoTickets) {
+            OutputView.lottoPrint(lotto);
+        }
+
+        //당첨계산
+        LottoResult lottoResult = new LottoResult();
+        for (int i = 0; i < lottoTicketsCount; i++) {
+            LottoCalculator.calculateRank(lottoTickets.get(i), gameInfo.getWinningNumber(), gameInfo.getBonusNumber());
+        }
+
+        //결과 출력
+        OutputView.lottoResult(lottoResult, lottoResult.getTotalPrizeAmount(), purchaseAmount);
+
 
 
 
