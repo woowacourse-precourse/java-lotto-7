9ab0f5624ef24c96efef61a09a612a95cd125991
hynseoj <wjdgustj2877@naver.com>
2024-11-02 22:39:48 +0900

    feat: 당첨 번호, 보너스 번호 예외 처리 구현

--- src/main/java/lotto/common/exception/ErrorMessage.java
+++ src/main/java/lotto/common/exception/ErrorMessage.java
@@ -5,13 +5,17 @@ import static lotto.common.constant.LottoPrice.LOTTO_PRICE;
 public enum ErrorMessage {
 
     INPUT_NULL_OR_EMPTY_ERROR("값이 입력되지 않았습니다."),
+    INPUT_NOT_INTEGER_ERROR("정수를 입력해주세요."),
 
     LOTTO_NUMBERS_COUNT_ERROR("로또 번호는 6개여야 합니다."),
     LOTTO_NUMBERS_DUPLICATION_ERROR("로또 번호는 중복되지 않아야 합니다."),
 
     LOTTO_NUMBER_OUT_OF_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
 
-    PURCHASE_AMOUNT_VALUE_ERROR("로또 구입 금액은 " + LOTTO_PRICE.price() + "으로 나누어떨어져야 합니다.");
+    PURCHASE_AMOUNT_VALUE_ERROR("로또 구입 금액은 " + LOTTO_PRICE.price() + "으로 나누어떨어져야 합니다."),
+
+    WINNING_NUMBERS_COUNT_ERROR("당첨 번호는 6개여야 합니다."),
+    WINNING_NUMBERS_DUPLICATION_ERROR("당첨 번호는 중복되지 않아야 합니다.");
 
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
     private final String message;
