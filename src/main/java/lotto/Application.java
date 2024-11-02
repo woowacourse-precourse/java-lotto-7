59cb1acc9a242ff438ecf89b3827704d570ec381
wkqkel <wkqkel@naver.com>
2024-11-03 02:23:38 +0900

    feat: 어플리케이션 구동 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.WinningResult;
+
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+    static LottoService lottoService = new LottoService();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int price = InputView.getPrice();
+        List<Lotto> lottos = lottoService.buy(price);
+        OutputView.printLotto(lottos);
+
+        List<Integer> winners = InputView.getWinnerNumbers();
+        int bonus = InputView.getBonusNumber();
+        WinningResult result = lottoService.getResult(winners, bonus);
+        OutputView.printWinning(result);
     }
 }
