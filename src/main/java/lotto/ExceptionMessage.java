6ef5dcb92866282b9b7fd95e0ae859f2886a1c64
문혜교 <3hilney@gmail.com>
2024-11-03 03:18:37 +0900

    feat: 예외 처리 메세지 추가
    로또 번호 범위 이탈했을 때, 문자를 입력했을 때, 구입 금액을 1000원 단위로 입력하지 않았을 때

--- src/main/java/lotto/ExceptionMessage.java
+++ src/main/java/lotto/ExceptionMessage.java
@@ -2,7 +2,11 @@ package lotto;
 
 public enum ExceptionMessage {
     INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
-    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    OUT_OF_RANGE_LOTTO_NUMBER("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다."),
+    INVALID_INPUT_FORMAT("[ERROR] 로또 번호를 숫자로 입력해야 합니다."),
+    INVALID_PURCHASE_AMOUNT("[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.");
+
 
     private final String message;
 
