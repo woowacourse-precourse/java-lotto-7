f5c3641ab8277e510f869b5ff02b554ccdb6cb20
jaeyeong <zzz26lk@gmail.com>
2024-11-02 16:35:17 +0900

    feat: 보너스 번호 검증
    사용자 입력으로 들어온 보너스 번호를 검증하기 위해서
    검증 로직을 추가한다.

--- src/main/java/lotto/domain/util/AddBonusNumber.java
+++ src/main/java/lotto/domain/util/AddBonusNumber.java
@@ -1,12 +1,21 @@
 package lotto.domain.util;
 
 import java.util.List;
+import lotto.validator.NumberValidator;
+import lotto.validator.Validator;
 
 public final class AddBonusNumber {
 
     public static List<Integer> create(List<Integer> winningNumbers, String input) {
+        validate(input);
+
         int bonusNumber = Integer.parseInt(input);
         winningNumbers.add(bonusNumber);
         return winningNumbers;
+    }
+
+    private static void validate(String input) {
+        Validator<String> validator = new NumberValidator();
+        validator.validate(input);
     }
 }
