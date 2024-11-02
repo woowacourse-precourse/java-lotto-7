eccacb9d074365884f9a46a6f61d1bb0e2fca406
김건학 <kkh10002@naver.com>
2024-11-02 17:31:55 +0900

    test(lotto, number): Lottos 객체 생성 및 equals 테스트 추가
    - Lottos 객체를 생성할 때, Lotto의 순서가 동일하면 두 객체가 equals 로 판별됩니다.
    - 순서가 다르면 equals 하지 않도록 판별합니다.

--- src/test/java/lotto/domain/lotto/LottosTest.java
+++ src/test/java/lotto/domain/lotto/LottosTest.java
@@ -11,32 +11,37 @@ class LottosTest {
     Lotto customLotto1;
     Lotto customLotto2;
     Lotto customLotto3;
-    Lotto customLotto4;
 
     @BeforeEach
     void setUp() {
         customLotto1 = LottoFactory.createCustomLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
         customLotto2 = LottoFactory.createCustomLotto(Arrays.asList(11, 12, 13, 14, 15, 16));
         customLotto3 = LottoFactory.createCustomLotto(Arrays.asList(21, 22, 23, 24, 25, 26));
-        customLotto4 = LottoFactory.createCustomLotto(Arrays.asList(31, 32, 33, 34, 35, 36));
     }
 
-    @DisplayName("Lotto 여러개를 입력받아 Lottos 객체를 생성한다.")
+    @DisplayName("Lottos 에 쌓은 순서가 동일하면 두 Lottos 객체는 equals 하다.")
     @Test
-    void Lotto_여러개를_입력받아_Lottos_객체를_생성한다() {
+    void Lottos_에_쌓은_순서가_동일하면_두_Lottos_객체는_동일하다() {
+        Lotto cloneCustomLotto1 = LottoFactory.createCustomLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto cloneCustomLotto2 = LottoFactory.createCustomLotto(Arrays.asList(11, 12, 13, 14, 15, 16));
+        Lotto cloneCustomLotto3 = LottoFactory.createCustomLotto(Arrays.asList(21, 22, 23, 24, 25, 26));
+
         Lottos lottos = Lottos.of(Arrays.asList(customLotto1, customLotto2, customLotto3));
+        Lottos cloneLottos = Lottos.of(Arrays.asList(cloneCustomLotto1, cloneCustomLotto2, cloneCustomLotto3));
 
-        assertThat(lottos).isEqualTo(Lottos.of(Arrays.asList(customLotto1, customLotto2, customLotto3)));
+        assertThat(lottos).isEqualTo(cloneLottos);
     }
 
-    @DisplayName("Lottos내 Lotto가 다르면 두 객체가 equals 하지 않다.")
+    @DisplayName("Lottos 내 쌓은 Lotto 순서가 다르면 두 객체는 equals 하지 않다.")
     @Test
-    void Lottos내_Lotto가_다르면_두_객체가_equals_하지_않다() {
-        Lottos lottos1 = Lottos.of(Arrays.asList(customLotto1, customLotto2, customLotto3));
-        Lottos lottos2 = Lottos.of(Arrays.asList(customLotto2, customLotto3, customLotto4));
-        Lottos lottos3 = Lottos.of(Arrays.asList(customLotto2, customLotto1, customLotto3));
+    void Lottos내_쌓은_Lotto_순서가_다르면_두_객체는_equals_하지_않다() {
+        Lotto cloneCustomLotto1 = LottoFactory.createCustomLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto cloneCustomLotto2 = LottoFactory.createCustomLotto(Arrays.asList(11, 12, 13, 14, 15, 16));
+        Lotto cloneCustomLotto3 = LottoFactory.createCustomLotto(Arrays.asList(21, 22, 23, 24, 25, 26));
 
-        assertThat(lottos1).isNotEqualTo(lottos2);
-        assertThat(lottos1).isNotEqualTo(lottos3);
+        Lottos lottos = Lottos.of(Arrays.asList(customLotto1, customLotto2, customLotto3));
+        Lottos cloneLottos = Lottos.of(Arrays.asList(cloneCustomLotto3, cloneCustomLotto2, cloneCustomLotto1));
+
+        assertThat(lottos).isNotEqualTo(cloneLottos);
     }
 }
