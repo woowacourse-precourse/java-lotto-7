ed06f6aa5c0528a2a9084c868670f3a8b4c17a0c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-02 22:41:42 +0900

    fix(OutputView): 당첨 결과 OutputView 수정
    당첨 결과 Format이 변경되었으므로 OutputView 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,14 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
-    // todo: 발행된 로또의 수량과 번호를 오름차순으로 정리하여 출력
+    //발행된 로또의 수량과 번호를 오름차순으로 정리하여 출력
     public void printPurchasedLottos(List<Lotto> lottos){
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for(Lotto lotto : lottos){
@@ -14,19 +16,18 @@ public class OutputView {
         }
     }
 
-    // todo : 당첨 통계와 수익률을 포함한 전체 결과 출력
-    public void printResult( List<Integer> result, double profitRate){
+    // 당첨 통계와 수익률을 포함한 전체 결과 출력
+    public void printResult(Map<Rank,Integer> result, double profitRate){
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        // todo : 개수에 맞춰서 출력하기
-        System.out.println("3개 일치 (5,000원) - 1개");
-        System.out.println("4개 일치 (50,000원) - 0개");
-        System.out.println("5개 일치 (1,500,000원) - 0개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개");
-        System.out.println("6개 일치 (2,000,000,000원) - 0개");
+        System.out.println("3개 일치 (5,000원) - "+ result.get(Rank.FIFTH) +"개");
+        System.out.println("4개 일치 (50,000원) - "+result.get(Rank.FOURTH)+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+result.get(Rank.THIRD)+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+result.get(Rank.SECOND)+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+result.get(Rank.FIRST)+"개");
 
-        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
 
     }
 
