789b0ebe2b8df44efeffeac66dd35a86424d7a4d
hanwool <hanwe92@gmail.com>
2024-11-03 00:16:56 +0900

    feat(ValidateMessage): Add bonus number message

--- src/main/java/lotto/util/enums/ValidateMessage.java
+++ src/main/java/lotto/util/enums/ValidateMessage.java
@@ -5,7 +5,9 @@ public enum ValidateMessage {
 
     NUMBER_SIZE_ERROR("당첨번호의 개수가 6개가 아닙니다."),
     NUMBER_RANGE_ERROR("1 ~ 45 사이의 숫자만 입력해주세요."),
-    DUPLICATE_ERROR("서로 다른 숫자를 입력해주세요.")
+    DUPLICATE_ERROR("서로 다른 숫자를 입력해주세요."),
+
+    SAME_WITH_LOTTO_NUMBER_ERROR("당첨 번호와 다른 번호를 입력해주세요.")
     ;
 
     private static final String PREFIX = "[ERROR] ";
