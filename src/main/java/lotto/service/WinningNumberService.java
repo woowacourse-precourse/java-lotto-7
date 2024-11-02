a349f746bc1f96a5dbbb401e06e2fdfd637c09ef
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-03 04:35:50 +0900

    feat(WinningNumber): 당첨 번호 생성 기능 추가
    당첨 번호 생성 기능을 구현했습니다.

--- src/main/java/lotto/service/WinningNumberService.java
+++ src/main/java/lotto/service/WinningNumberService.java
@@ -1,4 +1,12 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.domain.WinningNumber;
+
 public class WinningNumberService {
+    public WinningNumber createWinningNumber(List<Integer> winningNumber, int bonusNumber) {
+        return new WinningNumber(winningNumber, bonusNumber);
+    }
+
+
 }
