d17bbf5856ea455e62b1b0c5e750eb6c349c6fed
chyo1 <hyowon3856@gmail.com>
2024-11-03 02:30:21 +0900

    chore : 구입 금액 입력 메서드 public 명시화

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -7,8 +7,8 @@ public class IOController {
     private static String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    String inputPurchase() {
-        System.out.println(INPUT_PURCHASE_MESSAGE);
+    public String inputPurchase() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
     }
 
