4320124bd82ac0ab73e9ed70c7ada3b4a4a45081
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-02 23:57:37 +0900

    feat : Lotto 클래스 추가
    validate메소드 validateSize와 validateDuplicated로 분할하여 로또의 크기와 중복을 체크함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,8 +11,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicated(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복 될 수 없습니다.");
         }
     }
 
