8c6379ef3d05d69cff679ca37d184f0437c50158
skadjs <skadjs11@gmail.com>
2024-11-03 03:06:14 +0900

    feat(PayForLotto): 구입금액 입력 관련 예외처리 기능 추가
    다음 상황에 대한 예외처리 코드를 추가함.
    - 구입금액에 숫자 이외의 문자가 포함된 경우
    - 구입금액이 1,000원 단위가 아닌 경우
    - 구입금액이 빈 문자열인 경우

--- src/main/java/lotto/PayForLotto.java
+++ src/main/java/lotto/PayForLotto.java
@@ -22,7 +22,10 @@ public class PayForLotto {
     public void getUserPay() {
         String userInput = readLine();
         userInput = removeBlank(userInput);
+        checkIsBlank(userInput);
+        checkIsOnlyNumber(userInput);
         lottoPayout = castInputType(userInput);
+        checkMoneyUnit(lottoPayout);
     }
 
     public String removeBlank(String input) {
@@ -31,5 +34,29 @@ public class PayForLotto {
 
     public int castInputType(String input) {
         return Integer.parseInt(input);
+    }
+
+    public void checkIsOnlyNumber(String input) {
+        String ERROR_MESSAGE = "[ERROR] 구입금액은 숫자뿐이어야 합니다.";
+        try {
+            castInputType(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    public void checkMoneyUnit(int input) {
+        String ERROR_MESSAGE = "[ERROR] 구입금액은 1,000원 단위여야 합니다.";
+        int REMAINDER = input % 1000;
+        if (REMAINDER != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
+    public void checkIsBlank(String input) {
+        String ERROR_MESSAGE = "[ERROR] 금액을 입력하지 않았습니다.";
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
