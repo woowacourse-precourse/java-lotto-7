fa410c278c9ccefea1c7c7287dc5dd609d43ed4b
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 05:53:10 +0900

    feat : 보너스 번호 생성 및 검증 기능 구현

--- src/main/java/lotto/model/LottoBonusNumber.java
+++ src/main/java/lotto/model/LottoBonusNumber.java
@@ -1,4 +1,20 @@
 package lotto.model;
 
 public class LottoBonusNumber {
+    int START_LOTTO_NUMBER = 1;
+    int FINISH_LOTTO_NUMBER = 45;
+    private final int bonusNumber;
+
+    public LottoBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+        validateBonusNumber();
+    }
+
+    private void validateBonusNumber() {
+        if (START_LOTTO_NUMBER <= bonusNumber && bonusNumber <= FINISH_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(String.format("보너스 번호는 %d부터 %d 사이의 숫자여야 합니다.", START_LOTTO_NUMBER, FINISH_LOTTO_NUMBER));
+        }
+    }
+
+
 }
