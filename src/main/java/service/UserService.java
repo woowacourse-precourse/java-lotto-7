0abf195e54fe98e20352fd4775e50abb0b252049
dudxo <father8475@naver.com>
2024-11-02 18:08:32 +0900

    refactor(issueLotto): 로또 발행
    - 로또 발행 로직을 클래스 책임에 따라 이동한다.(LottoPlay.class -> UserService)

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,13 +1,27 @@
 package service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import domain.Lotto;
 import domain.User;
 import domain.WinningLotto;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
 
     public User init(int amount) {
         return new User(amount);
+    }
+
+    public void issueLotto(User user) {
+        int purchaseCount = user.getPurchaseCount();
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < purchaseCount; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lotto.ascNumber();
+            lottos.add(lotto);
+        }
+        user.updateLottos(lottos);
     }
 
     public void updateRateOfReturn(User user) {
