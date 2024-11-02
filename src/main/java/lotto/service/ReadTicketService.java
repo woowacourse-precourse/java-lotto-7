45edf2c980b42e897892e6ae02d3ccd36823d07a
plan11plan <tjtfy7028@gmail.com>
2024-11-02 13:51:45 +0900

    feat: 읽기 전용 로또 티켓 서비스 개발

---
+++ src/main/java/lotto/service/ReadTicketService.java
@@ -0,0 +1,17 @@
+package lotto.service;
+
+import lotto.domain.ticket.Ticket;
+import lotto.repository.ReadTicketRepository;
+
+public class ReadTicketService {
+    private final ReadTicketRepository repository;
+
+    public ReadTicketService(ReadTicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public Ticket getById(Long ticketId) {
+        return repository.findById(ticketId).orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 티켓 ID 입니다."));
+    }
+
+}
