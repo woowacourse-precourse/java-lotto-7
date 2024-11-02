e611857d6f2eaf8e32a692df991b95466ad2bad2
woodongle <315ehdgur@gmail.com>
2024-11-02 11:35:48 +0900

    feat: 로또 번호(당첨 번호) 입력 검증 추가
    1. 로또 번호의 최솟값과 최댓값을 검증하는 메서드를 추가합니다.
      - checkLottoNumberRange()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,10 +3,14 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkLottoNumberRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,10 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 로또 번호의 최솟값과 최댓값을 검증하는 메서드
+    private void checkLottoNumberRange(List<Integer> numbers) {
+        if (numbers.getFirst() < MIN_LOTTO_NUMBER || numbers.getLast() > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+        }
+    }
 }
