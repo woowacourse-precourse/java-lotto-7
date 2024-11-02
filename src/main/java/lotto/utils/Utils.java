daefea1a23d389e172b359e462c506312bcb7f92
dbdb456 <adad568@naver.com>
2024-11-02 15:24:58 +0900

    feat(utils): 로또 등수 메서드 추가

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -3,6 +3,7 @@ package lotto.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lotto.domain.Lotto;
@@ -20,6 +21,46 @@ public class Utils {
 
     public List<Integer> convertToIntegerList(String numbers) {
         return Arrays.stream(numbers.split(",")).map(Integer::parseInt).toList();
+    }
+
+    public Map<Rank, Integer> utils(Lotto winningNumber, int bonusNumber, List<Lotto> lottos) {
+        Map<Rank, Integer> map = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            matchLottoNumber(map, winningNumber, bonusNumber, lotto);
+        }
+        return map;
+    }
+
+    private void matchLottoNumber(Map<Rank, Integer> map, Lotto winningNumber, int bonusNumber, Lotto lotto) {
+        int count = 0, bonusCount = 0;
+        for (int i = 0; i < 6; i++) {
+            if (lotto.matchNumber(winningNumber.getIndex(i))) {
+                count++;
+            }
+        }
+        if (lotto.matchNumber(bonusNumber)) {
+            bonusCount++;
+        }
+        if (count >= 3) {
+            Rank rank = rankingLotto(count, bonusCount);
+            map.put(rank, map.getOrDefault(rank, 0) + 1);
+        }
+    }
+
+    private Rank rankingLotto(int count, int bonusCount) {
+        if (count == 6) {
+            return Rank.ONE;
+        }
+        if (count == 5) {
+            if (bonusCount == 1) {
+                return Rank.TWO;
+            }
+            return Rank.THREE;
+        }
+        if (count == 4) {
+            return Rank.FOUR;
+        }
+        return Rank.FIVE;
     }
 
     public int totalPrize(Map<Rank, Integer> map) {
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,17 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String lottoPrice(){
+    public String lottoPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String winLottoNumber(){
+    public String winLottoNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String bonusLottoNumber(){
+    public String bonusLottoNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
