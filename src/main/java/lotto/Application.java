c36dc12373cdbb9a2ff2817bf985163da4e3a27e
Jungwoo Chu <cjw980930@gmail.com>
2024-11-02 21:09:49 +0900

    refactor: apllication 클래스 자바 컨벤션에 맞게 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,22 +6,24 @@ public class Application {
     private InputHandler inputHandler;
     private LottoGenerator lottoGenerator;
     private List<Lotto> lottoTickets;
-    public Application(){
+
+    public Application() {
         inputHandler = new InputHandler();
         lottoGenerator = new LottoGenerator();
     }
+
     public static void main(String[] args) {
         Application app = new Application();
         app.run();
     }
 
-    public void run(){
+    public void run() {
         int price = inputHandler.priceInput();
-        int purchasedLottoCount= price/1000;
-        System.out.println(purchasedLottoCount+"개를 구매했습니다.");
+        int purchasedLottoCount = price / 1000;
+        System.out.println(purchasedLottoCount + "개를 구매했습니다.");
 
         lottoTickets = lottoGenerator.generate(purchasedLottoCount);
-        for (Lotto lotto : lottoTickets){
+        for (Lotto lotto : lottoTickets) {
             System.out.println(lotto.getNumbers());
         }
     }
