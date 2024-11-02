97cbed718a99c285493e09f68f2b7af8836c9dc7
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-02 21:06:44 +0900

    test(SetTest): Set 컬렉션의 contains() 메서드 기능 false 테스트 추가
    - Set에 에 존재하지 않는 값은 false로 나오는지 확인
    - @CsvSource 어노테이션 사용해 여러 인풋값에 대응되는 여러 아웃풋값 한번에 테스트 하도록 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -40,5 +42,15 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void Set에_값이_들어갔는지_확인(int input) {
         assertTrue(numbers.contains(input));
+    }
+
+    /**
+     * 요구사항 3
+     */
+    @ParameterizedTest
+    @DisplayName("Set에 값이 들어갔는지 확인 - false 포함")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void Set에_값이_들어갔는지_확인_false포함(Integer input, Boolean expected) {
+        assertEquals(numbers.contains(input), expected);
     }
 }
