e4faf66f3be7a31016100d4c0a33aa79d248b3d1
JinGoo <zzgg3040@naver.com>
2024-11-02 15:26:59 +0900

    feat: 사용자 입력 역할 분리

---
+++ src/main/java/lotto/util/ConsoleInput.java
@@ -0,0 +1,28 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.BusinessException;
+
+public class ConsoleInput {
+
+    public static String getStringWithQuestion(String question) {
+        System.out.println(question);
+        return Console.readLine();
+    }
+
+    public static long getLongWithPrompt(String prompt) {
+        try {
+            return Long.parseLong(getStringWithQuestion(prompt));
+        } catch (NumberFormatException e) {
+            throw new BusinessException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static int getIntWithPrompt(String prompt) {
+        try {
+            return Integer.parseInt(getStringWithQuestion(prompt));
+        } catch (NumberFormatException e) {
+            throw new BusinessException("숫자만 입력할 수 있습니다.");
+        }
+    }
+}
