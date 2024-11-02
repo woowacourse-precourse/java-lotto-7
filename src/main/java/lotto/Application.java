364ecb93b2826923c94a6dfa781f937503de4182
mintos00 <kimhm2170@gmail.com>
2024-11-02 22:41:53 +0900

    feat(validMoney): add a feature to validate the Lotto purchase amount

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        inputMoney();
+
+    }
+
+    public static void inputMoney() {
+        System.out.println("구입금액을 입력해주세요.");
+        Long money = Long.parseLong(Console.readLine());
+        if (!validMoney(money)) {
+            inputMoney();
+        }
+    }
+
+    public static boolean validMoney(Long money) {
+        return money % 1000 == 0;
     }
 }
