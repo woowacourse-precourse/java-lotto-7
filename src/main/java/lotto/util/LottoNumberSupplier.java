ee960a9850b577bf5a78545f691b67b312b9e0b7
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 02:39:23 +0900

    feat(LottoNumberSupplier): 로또 번호를 제공하는 함수형 인터페이스 정의

---
+++ src/main/java/lotto/util/LottoNumberSupplier.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface LottoNumberSupplier {
+
+    int LOTTO_SIZE = 6;
+    int MIN_NUMBER = 1;
+    int MAX_NUMBER = 45;
+
+    List<Integer> getLottoNumbers();
+}
