098a3ae0116529633fc4010d0936138bd7163709
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-02 21:03:16 +0900

    test(SetTest): Set 컬렉션의 size() 메서드 기능 테스트 추가
    - Set에 값 추가 후 사이즈가 올바른지 확인하는 기능 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,8 +1,12 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -16,4 +20,12 @@ public class SetTest {
         numbers.add(3);
     }
 
+    /**
+     * 요구사항 1
+     */
+    @Test
+    @DisplayName("Set의 크기 확인")
+    void Set의_크기_확인() {
+        assertThat(numbers).size().isEqualTo(3);
+    }
 }
