a8c2520b65c336805df24a5a99dbbce94f07b4fc
dudxo <father8475@naver.com>
2024-11-02 15:59:38 +0900

    feat(UserService): 총 당첨 금액과 수익률 계산
    - 총 당첨 금액을 계산한다.
    - 총 당첨 금액을 통해 구입 금액 대비 수익률을 계산한다.
    - 계산된 수익률을 유저(User)에 저장한다.:

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,10 +1,28 @@
 package service;
 
 import domain.User;
+import domain.WinningLotto;
+import java.util.List;
 
 public class UserService {
 
     public User init(int amount) {
         return new User(amount);
+    }
+
+    public void updateRateOfReturn(User user) {
+        double rateOfReturn = calculateRateOfReturn(user);
+        user.updateRateOfReturn(rateOfReturn);
+    }
+
+    private double calculateRateOfReturn(User user) {
+        int totalPrizeMoney = calculateTotalPrizeMoney(user);
+        return (double) totalPrizeMoney / user.getAmount() * 100;
+    }
+
+    private int calculateTotalPrizeMoney(User user){
+        return user.getWinningLottos().stream()
+                .mapToInt(WinningLotto::getPrizeMoney)
+                .sum();
     }
 }
