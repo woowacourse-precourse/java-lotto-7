b0fe8ab95f5f8b07bd155dafd3a7681f93a37dc4
Dohun Kim <dohun8758@naver.com>
2024-11-02 14:28:05 +0900

    refactor: 매직 스트링 상수 추출

--- src/main/java/lotto/validator/CommonInputValidator.java
+++ src/main/java/lotto/validator/CommonInputValidator.java
@@ -2,6 +2,8 @@ package lotto.validator;
 
 public class CommonInputValidator {
 
+    public static final String WHITE_SPACE = " ";
+
     public static void validateCommonInput(String input) {
         validateIsNotEmptyInput(input);
         validateHasNoWhiteSpace(input);
@@ -14,7 +16,7 @@ public class CommonInputValidator {
     }
 
     private static void validateHasNoWhiteSpace(String input) {
-        if (input.contains(" ")) {
+        if (input.contains(WHITE_SPACE)) {
             throw new IllegalArgumentException("[ERROR] 입력 값에 공백이 포함되어 있습니다.");
         }
     }
