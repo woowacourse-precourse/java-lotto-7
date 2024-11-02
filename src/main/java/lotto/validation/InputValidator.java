c25abd57f0b6021123b6d67b81fcf462175967f4
변우영 <likevangogh24@gmail.com>
2024-11-02 19:16:47 +0900

    feat(InputValidator): 입력 받은 당첨 번호가 6개가 아닌 경우 예외 처리 추가

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -1,18 +1,28 @@
 package lotto.validation;
 
+import lotto.enums.Delimiter;
+import lotto.enums.Message;
+
 public class InputValidator {
 
-    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final int MAX_LOTTO_NUMBER_COUNT = 6;
 
     public static void validateNonBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException(ERROR_PREFIX + "입력값이 비어 있거나 공백으로만 구성되었습니다.");
+            throw new IllegalArgumentException(Message.ERROR_PREFIX.getMessage() + "입력값이 비어 있거나 공백으로만 구성되었습니다.");
         }
     }
 
     public static void validateDelimitedByComma(String input) {
         if (!input.matches("^\\d+(,\\d+)*$")) {
-            throw new IllegalArgumentException(ERROR_PREFIX + "당첨 번호 입력 시, 쉼표(,)로 구분해야 합니다.");
+            throw new IllegalArgumentException(Message.ERROR_PREFIX.getMessage() + "당첨 번호 입력 시, 쉼표(,)로 구분해야 합니다.");
+        }
+    }
+
+    public static void validateInputNumberSix(String input) {
+        String[] splitInput = input.split(Delimiter.COMMA.getDelimiter());
+        if (splitInput.length != MAX_LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(Message.ERROR_PREFIX.getMessage() + "당첨 번호는 6개까지 입력 가능합니다.");
         }
     }
 }
