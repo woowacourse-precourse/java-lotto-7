71f923ee3b30e9b136206e6f1db04995e037dbf9
Jinwoo Choi <jk62362@mensakorea.org>
2024-11-03 02:58:08 +0900

    style: 입력 메시지 양식에 맞춰 줄 바꿈 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 
 public class InputView {
+    private static final String NEW_LINE = System.lineSeparator();
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
@@ -14,11 +15,13 @@ public class InputView {
     }
 
     public static String readWinningLotto() {
+        System.out.print(NEW_LINE);
         System.out.println(WINNING_LOTTO_INPUT_MESSAGE);
         return Console.readLine();
     }
 
     public static String readBonusNumber() {
+        System.out.print(NEW_LINE);
         System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
         return Console.readLine();
     }
