68a87bbd2e37c652e0f2168f4d8cd8ca6ed0a9fb
srcho01 <seorim5306@gmail.com>
2024-11-03 02:50:06 +0900

    fix: 지정 에러 문구 대소문자 수정
    Error -> ERROR

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -2,7 +2,7 @@ package lotto.util;
 
 public class Constant {
 
-    public static final String ERROR_PREFIX = "[Error] ";
+    public static final String ERROR_PREFIX = "[ERROR] ";
 
     public static final String PURCHASE_AMOUNT_CHECK_REGEX = "^[0-9]+$";
     public static final String BALL_NUMBER_RANGE_CHECK_REGEX = "^(?:[1-9]|[1-3][0-9]|4[0-5])$";
