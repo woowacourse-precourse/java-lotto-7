eeac35c63477b4970e79ce1748eeca2e643e1854
gladhee <gladhee@icloud.com>
2024-11-02 18:19:05 +0900

    feat(Amount): 입력 금액 도메인 작성

---
+++ src/main/java/lotto/domain/Amount.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+public class Amount {
+
+    private final int amount;
+
+    private static final int MIN_LOTTO_PRICE = 1000;
+
+    private Amount(String inputAmount) {
+        int amount = validateType(inputAmount);
+        validateRange(amount);
+        validateDivisible(amount);
+        this.amount = amount;
+    }
+
+    public static Amount of(String inputAmount) {
+        return new Amount(inputAmount);
+    }
+
+    private static int validateType(String inputAmount) {
+        int amount;
+
+        try {
+            amount = Integer.parseInt(inputAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액은 자연수여야 합니다.");
+        }
+
+        return amount;
+    }
+
+    private void validateRange(int amount) {
+        if (amount < MIN_LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다.");
+        }
+    }
+
+    private void validateDivisible(int amount) {
+        if ((amount / MIN_LOTTO_PRICE) * MIN_LOTTO_PRICE != amount) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.");
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+}
