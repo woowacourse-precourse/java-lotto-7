9fb7a992c3a81daa5261b60db9408d932871756f
Dohun Kim <dohun8758@naver.com>
2024-11-02 17:14:41 +0900

    feat: 로또 구매 및 출력 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,11 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 import lotto.model.Lotto;
+import lotto.model.Lottos;
+import lotto.model.RandomLottoGenerator;
 import lotto.model.WinningLottoNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -17,11 +22,33 @@ public class LottoController {
 
     public void run() {
         int money = getMoney();
+        int purchaseCount = money / 1000;
+
+        Lottos purchasedLottos = purchaseLottos(purchaseCount);
+
+        outputView.showPurchasedLottos(purchaseCount, purchasedLottos);
+
         Lotto lottoWinningNumbers = getLottoWinningNumbers();
         int lottoBonusNumber = getLottoBonusNumber();
 
         WinningLottoNumbers winningLottoNumbers = createWinningLottoNumbers(lottoWinningNumbers, lottoBonusNumber);
 
+    }
+
+    private Lottos purchaseLottos(int purchaseCount) {
+        List<Lotto> purchasedLottos = getPurchasedLottos(purchaseCount);
+        return Lottos.of(purchasedLottos);
+    }
+
+    private List<Lotto> getPurchasedLottos(int purchaseCount) {
+        return IntStream.range(0, purchaseCount)
+                .mapToObj(lotto -> purchaseLotto())
+                .toList();
+    }
+
+    private Lotto purchaseLotto() {
+        RandomLottoGenerator lottoGenerator = new RandomLottoGenerator();
+        return lottoGenerator.generateLotto();
     }
 
     private int getMoney() {
