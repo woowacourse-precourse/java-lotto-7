a681cf4cd31f290e7630c01c1d2971afc8480f44
howareyoo <newworld121@naver.com>
2024-11-03 00:05:46 +0900

    feat: add duplication validation for numbers list

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,11 +1,13 @@
-package lotto;
+package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateDuplication(numbers);
         validate(numbers);
         this.numbers = numbers;
     }
@@ -13,6 +15,13 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers){
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if(uniqueNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자를 입력해야 합니다.");
         }
     }
 
