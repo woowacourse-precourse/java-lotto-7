d4c06863d597720e7e0fec8222a44fa177cd0a01
leeseongsil <leegwichan@naver.com>
2024-11-02 21:26:01 +0900

    feat(InputView): 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,26 +1,41 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
+
+    private static final String NUMBER_DELIMITER = ",";
 
     private InputView() {
     }
 
     public static int inputTotalPrice() {
         System.out.println("\n구입금액을 입력해 주세요.");
-        return inputInt();
+        return inputNumber();
     }
 
-    private static int inputInt() {
+    private static List<Integer> inputWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        return inputNumbers();
+    }
+
+    private static int inputNumber() {
         return parseInt(Console.readLine());
+    }
+
+    private static List<Integer> inputNumbers() {
+        return Arrays.stream(Console.readLine().split(NUMBER_DELIMITER))
+                .map(InputView::parseInt)
+                .toList();
     }
 
     private static int parseInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("범위에 맞는 숫자를 입력해야 합니다");
         }
     }
 }
