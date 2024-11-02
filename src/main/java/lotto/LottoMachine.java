f660bb2c3cd035e100a12b6ed76a121857957cea
sejinezy <psj8880@naver.com>
2024-11-02 16:03:58 +0900

    feat(LottoMachine): 로또티켓을 발행하는 메서드 구현
    금액에 맞는 로또 수 만큼 로또를 생성해서 하나의 로또 티켓을 만든다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import static lotto.Input.*;
+
+import java.util.List;
+
 public class LottoMachine {
     public static final int PRICE_OF_ONE_LOTTERY_TICKET = 1000;
+
+    public LottoTicket generateLottoTicket(int amount) {
+        int lottoCount = calculateLottoCount(amount);
+        List<Lotto> lottos = LottoGenerator.generateLottos(lottoCount);
+        return new LottoTicket(lottos);
+    }
 
     private int calculateLottoCount(int amount) {
         return amount / PRICE_OF_ONE_LOTTERY_TICKET;
