5c281038bbb7f2f1214546d1dc9dac1d38a19f90
yunhacandy <yunhacandy@gmail.com>
2024-11-03 01:37:32 +0900

    feat (Constants): 로또 게임 관련 사용할 값 분리

---
+++ src/main/java/lotto/constant/Constants.java
@@ -0,0 +1,8 @@
+package lotto.constant;
+
+public class Constants {
+    public static final int LOTTO_NUMBER_LENGTH = 6;
+    public static final int LOTTO_NUMBER_RANGE_START = 1;
+    public static final int LOTTO_NUMBER_RANGE_END = 45;
+    public static final int LOTTO_PURCHASE_PRICE = 1000;
+}
