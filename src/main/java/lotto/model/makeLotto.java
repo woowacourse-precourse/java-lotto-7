d48c40898252aec04bb853f45da4c1e3357ffeee
khyunse0 <rjdls2549@donga.ac.kr>
2024-11-02 20:42:48 +0900

    정해진 구매 수대로 1~45 범위의 6개 숫자 배열 생성 구현

--- src/main/java/lotto/model/makeLotto.java
+++ src/main/java/lotto/model/makeLotto.java
@@ -1,13 +1,29 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class makeLotto {
-    private final int amount;
 
     public int splitAmount(int amount){
-        if (amount%1000 != 0){
+        validate(amount);
+        return amount / 1000;
+    }
+    private void validate(int amount){
+        if (amount % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위입니다.");
         }
-        return amount / 1000;
+    }
+    private List<Lotto> makeLottoList(int quantity){
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < quantity; i++){
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(new Lotto(lotto));
+        }
+        return lottoList;
     }
 
 }
