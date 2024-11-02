cd5c60ab586bfbe3961cb1de500e28156d991604
lsj0822 <lsj0822@khu.ac.kr>
2024-11-03 00:19:04 +0900

    feat(Validator) : 사용자 입력 당첨 번호, 보너스 번호 관련 검증 추가
    사용자 입력 당첨 번호의 경우, 파싱한 멤버들을 검사할 로직이 필요합니다.
    - 입력받은 원소의 개수가 6개인지 확인
    - 각각의 입력받은 원소가 1-45 사이의 정수 범위인지 확인
    - 입력받은 원소에 중복이 없는지 확인
    
    보너스 번호의 경우 다음과 같은 검사가 필요합니다
    - 입력받은 번호가 정수인지 확인
    - 입력받은 번호가 1-45 사이의 정수인지 확인
    - 기존에 입력한 WinningNumber와 중복되는지 확인

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -3,11 +3,14 @@ package validator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import model.Status;
 import utils.Pair;
 
 public class Validator {
     public static final Integer LOTTO_MIN = 1;
     public static final Integer LOTTO_MAX = 45;
+    public static final Integer LOTTO_SIZE = 6;
+    ;
     public static final Integer LOTTO_COST = 1000;
     public static final Pair<Integer, Integer> LOTTO_NUMBER_SCOPE = new Pair<>(LOTTO_MIN, LOTTO_MAX);
 
@@ -33,6 +36,23 @@ public class Validator {
     public static void validatePurchaseable(long criteriaMoney, int cost) {
         if (!isPurchaseAble(criteriaMoney, cost)) {
             throw new IllegalArgumentException("[ERROR] 잔액이 남습니다.");
+        }
+    }
+
+    public static void validateWinningGroup(List<String> elements) {
+        validateWinningSize(elements.size());
+        validateWinningElement(elements);
+    }
+
+    private static void validateWinningSize(Integer size) {
+        if (size != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 입력한 수의 개수가 6개가 아닙니다.");
+        }
+    }
+
+    private static void validateWinningElement(List<String> elements) {
+        for (String element : elements) {
+            validateNumber(element);
         }
     }
 
@@ -73,6 +93,18 @@ public class Validator {
         Set<Object> uniqueGroup = new HashSet<>(groups);
         if (uniqueGroup.size() < groups.size()) {
             throw new IllegalArgumentException("[ERROR] 중복되는 값이 있습니다.");
+        }
+    }
+
+    public static void validateBonusNumber(String inputContent, Status status) {
+        validateNumber(inputContent);
+        validateNumberInRange(Integer.parseInt(inputContent), LOTTO_NUMBER_SCOPE);
+        validateDuplicateToWinningGroup(inputContent, status);
+    }
+
+    private static void validateDuplicateToWinningGroup(String inputContent, Status status) {
+        if (!status.compareToWinningNumber(Integer.parseInt(inputContent))) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호와 보너스 번호가 중복하는 값이 있습니다.");
         }
     }
 
