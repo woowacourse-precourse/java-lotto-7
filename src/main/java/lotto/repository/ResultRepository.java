ef0d97f86158a0c0aef32ea91f3cfae78bc00ceb
OSEUK <tpdnrzmzm3@naver.com>
2024-11-02 16:29:42 +0900

    feat(Repository) : 당첨 결과 저장소
    당첨 번호와 보너스 번호를 기준으로 당첨된 결과를 저장하는 저장소 인터페이스

---
+++ src/main/java/lotto/repository/ResultRepository.java
@@ -0,0 +1,18 @@
+package lotto.repository;
+
+import lotto.domain.Money;
+import lotto.domain.Rank;
+
+import java.util.Map;
+import java.util.Optional;
+
+public interface ResultRepository {
+
+    void addResult(Rank rank);
+
+    Map<Rank, Integer> getResult();
+
+    int getCountByRank(Rank rank);
+
+    double calculate(Money money);
+}
