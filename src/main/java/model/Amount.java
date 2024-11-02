3a46a4ad0972ea7efd327c2d536a4ab9a249a8a5
juhyeongKim <zxcvbm804@gmail.com>
2024-11-02 16:48:23 +0900

    feat : 입력 가격이 로또 가격으로 나누어 떨어지지 않을 경우 에러 발생

--- src/main/java/model/Amount.java
+++ src/main/java/model/Amount.java
@@ -4,12 +4,21 @@ package model;
 public class Amount {
 
     private final int purchaseAmount;
+    private static final int lottoValue = 1000;
+    private static final String DIVIDE_LOTTO_VALUE_ERROR = "[ERROR] 로또 가격으로 나누어 떨어지지 않습니다.";
 
     public Amount(int purchaseAmount) {
+        divideByLottoValue(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
     public int getPurchaseAmount() {
         return purchaseAmount;
+    }
+
+    private void divideByLottoValue(int purchaseAmount) {
+        if (purchaseAmount % lottoValue != 0) {
+            throw new IllegalArgumentException(DIVIDE_LOTTO_VALUE_ERROR);
+        }
     }
 }
--- src/test/java/model/AmountTest.java
+++ src/test/java/model/AmountTest.java
@@ -1,17 +1,26 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 public class AmountTest {
 
     @Test
-    void amount_생성확인() {
+    void amount_생성확인_테스트() {
         Amount amount1 = new Amount(8000);
 
         int value = amount1.getPurchaseAmount();
 
         assertThat(value).isEqualTo(8000);
+    }
+
+    @Test
+    void amount_생성실패_테스트() {
+        int purchaseAmount = 8234;
+
+        assertThatThrownBy(() -> new Amount(purchaseAmount))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
