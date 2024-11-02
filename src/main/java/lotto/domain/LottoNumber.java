5cf7e9cee2922973b8648a2e5f70e04e636ed1c1
jae woo Jung <awfjol2008@gmail.com>
2024-11-02 19:37:10 +0900

    refactor: LottoNumber 클래스 내부의 검증 메서드명 변경

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,11 +7,11 @@ public class LottoNumber {
 	private final int number;
 
 	protected LottoNumber(int number) {
-		validateNumber(number);
+		validate(number);
 		this.number = number;
 	}
 
-	private void validateNumber(int number) {
+	private void validate(int number) {
 		if (isNumberOutOfRange(number)) {
 			throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위의 정수여야 합니다.");
 		}
