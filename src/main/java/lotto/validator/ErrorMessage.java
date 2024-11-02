25b1cb577b636a769a6af31283599fcdcdfa8f7b
dd-jiyun <dlwldbs8015@gmail.com>
2024-11-02 14:17:04 +0900

    feat: 에러 메세지 관리
    - Enum으로 에러 메세지를 한 곳에서 관리할 수 있게 하였습니다.

---
+++ src/main/java/lotto/validator/ErrorMessage.java
@@ -0,0 +1,22 @@
+package lotto.validator;
+
+public enum ErrorMessage {
+
+    EMPTY_INPUT_ERROR("값을 입력해주세요."),
+    NUMBER_FORMAT_ERROR("양의 정수를 입력해주세요."),
+    INVALID_UNIT_ERROR("금액은 1000원 단위로 입력해주세요."),
+    NUMBER_SIZE_ERROR("6개의 숫자를 입력해주세요."),
+    OUT_OF_RANGE_ERROR("1에서 45 사이의 숫자로 입력해주세요."),
+    DUPLICATE_NUMBER_ERROR("중복되지 않는 숫자로 입력해주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
