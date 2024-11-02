cafc04e6a6e04f9f9e9fb53bab8f5442e99d85ec
JunBe <wnsqowid0901@gmail.com>
2024-11-02 23:25:43 +0900

    style: 상수의 에러 메시지를 변경
    상수의 에러메시지를 변경했고, 보기 좋게 상수 이름 순으로 정렬했다.

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -2,12 +2,12 @@ package lotto.validation;
 
 public enum ErrorMessage {
     INVALID_AMOUNT("[ERROR] 가격은 1000원 단위로 입력 가능합니다."),
-    NEGATIVE_OR_ZERO("[ERROR] 0 이하의 수는 입력할 수 없습니다."),
     INVALID_INPUT("[ERROR] 문자열은 입력 불가합니다."),
+    INVALID_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
     DUPLICATE_NUMBER("[ERROR] 숫자가 중복되면 안됩니다."),
     DUPLICATE_WITH_WIN_NUMBER("[ERROR] 당첨 번호와 보너스 번호가 중복되면 안됩니다."),
-    OUT_OF_RANGE("[ERROR] 1 ~ 45 중에서 입력할 수 있습니다."),
-    INVALID_COUNT("[ERROR] 6개를 입력해야 합니다.");
+    NEGATIVE_OR_ZERO("[ERROR] 0 이하의 수는 입력할 수 없습니다."),
+    OUT_OF_RANGE("[ERROR] 1 ~ 45 중에서 입력할 수 있습니다.");
 
     private final String message;
 
