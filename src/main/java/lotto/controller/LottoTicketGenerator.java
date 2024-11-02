5174c5fb364008fd616102be0bdf0ff875037833
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-03 01:37:14 +0900

    refactor(controller/LottoTicketGenerator): 중복된 구매 금액 검증 로직 제거
    - 입력 검증은 LottoController에서만 수행하도록 책임 분리
    - LottoTicketGenerator는 검증된 데이터를 받아 티켓 인스턴스를 생성하는 역할에 집중

--- src/main/java/lotto/controller/LottoTicketGenerator.java
+++ src/main/java/lotto/controller/LottoTicketGenerator.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lotto.model.LottoTicket;
-import lotto.validator.LottoTicketValidator;
 
 public class LottoTicketGenerator {
 	public static final int LOTTO_PRICE = 1000;
 
     public static List<LottoTicket> generateTickets(int purchasePrice) {
-    	LottoTicketValidator.validatePurchasePrice(purchasePrice);
-    	
         int ticketCount = purchasePrice / LOTTO_PRICE;
         List<LottoTicket> tickets = new ArrayList<>();
 
