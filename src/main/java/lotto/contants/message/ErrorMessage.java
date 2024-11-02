731755aae1c51711f281aa32b96897587e673226
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-11-02 12:31:48 +0900

    fix: enum관리로 변경

--- src/main/java/lotto/contants/message/ErrorMessage.java
+++ src/main/java/lotto/contants/message/ErrorMessage.java
@@ -1,8 +1,17 @@
 package lotto.contants.message;
 
-public class ErrorMessage {
-    static final String ERROR = "[ERROR] ";
-    public static final String PURCHASE_PRICE = ERROR + "구입 금액 단위는 1,000원입니다.";
-    public static final String SPILT_EMPTY = ERROR + "쉼표(,) 사이에 숫자가 존재해야합니다.";
-    public static final String LOTTO_NUMBER_RANGE = ERROR + "로또 번호 범위는 1~45입니다.";
+public enum ErrorMessage {
+    PURCHASE_PRICE("구입 금액 단위는 1,000원입니다."),
+    SPILT_EMPTY("쉼표(,) 사이에 숫자가 존재해야합니다."),
+    LOTTO_NUMBER_RANGE("로또 번호 범위는 1~45입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return "[ERROR]" + message;
+    }
 }
--- src/main/java/lotto/contants/value/LottoValue.java
+++ src/main/java/lotto/contants/value/LottoValue.java
@@ -1,17 +1,22 @@
 package lotto.contants.value;
 
-public class LottoValue {
-    public static final int AMOUNT_UNIT = 1000;
-    public static final int MIN_LOTTO_NUM = 1;
-    public static final int MAX_LOTTO_NUM = 45;
-    public static final int RANK_FIRST = 1;
-    public static final int RANK_FIRST_MONEY = 2000000000;
-    public static final int RANK_SECOND = 2;
-    public static final int RANK_SECOND_MONEY = 30000000;
-    public static final int RANK_THRID = 3;
-    public static final int RANK_THRID_MONEY = 1500000;
-    public static final int RANK_FOURTH = 4;
-    public static final int RANK_FOURTH_MONEY = 50000;
-    public static final int RANK_FIFTH = 5;
-    public static final int RANK_FIFTH_MONEY = 5000;
+public enum LottoValue {
+    PRICE_UNIT(1000),
+    MIN_LOTTO_NUM(1),
+    MAX_LOTTO_NUM(45),
+    RANK_FIRST_PRIZE(2000000000),
+    RANK_SECOND_PRIZE(30000000),
+    RANK_THRID_PRIZE(1500000),
+    RANK_FOURTH_PRIZE(50000),
+    RANK_FIFTH_PRIZE(5000);
+
+    private final int value;
+
+    LottoValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
