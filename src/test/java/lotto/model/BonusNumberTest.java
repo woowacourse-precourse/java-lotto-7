6de90d7b53741e7ca86c6061045430a267efc85d
KJungW <sagakkb0707@naver.com>
2024-11-02 22:31:41 +0900

    test(model): BonusNumber 모델 테스트 추가
    테스트 내용
    - 정상흐름의 객체 생성 테스트
    - 음수를 번호로 입력해서 객체 생성할 경우
      예외 처리 테스트
    - 금지된 번호 리스트에 포함된 번호를 입력해서
      객체를 생성할 경우 예외 처리 테스트

---
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -0,0 +1,44 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BonusNumberTest {
+
+    @DisplayName("올바른 입력값을 통해 객체 생성을 시도")
+    @Test
+    void 올바른_입력값을_통해_객체_생성을_시도() {
+        List<Integer> givenBannedNumbers = List.of(10, 11, 12, 13, 14, 15);
+        int givenBonusNumber = 1;
+
+        BonusNumber actualBonusNumber = new BonusNumber(givenBonusNumber, givenBannedNumbers);
+
+        assertThat(actualBonusNumber.getNumber()).isEqualTo(givenBonusNumber);
+    }
+
+    @DisplayName("잘못된 입력값을 통해 객체 생성을 시도")
+    @ParameterizedTest(name = "금액 {0}을 입력할 경우 : {1}")
+    @MethodSource
+    void 잘못된_금액을_통해_객체_생성을_시도(int inputNumber, List<Integer> bannedNumbers, String expectedExceptionMessage) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new BonusNumber(inputNumber, bannedNumbers))
+                .withMessage(expectedExceptionMessage);
+    }
+
+    static Stream<Arguments> 잘못된_금액을_통해_객체_생성을_시도() {
+        List<Integer> bannedNumbers = List.of(10, 11, 12, 13, 14, 15);
+        return Stream.of(
+                Arguments.of(0, bannedNumbers, BonusNumber.NOT_POSITIVE_NUMBER_EXCEPTION_MESSAGE),
+                Arguments.of(-1, bannedNumbers, BonusNumber.NOT_POSITIVE_NUMBER_EXCEPTION_MESSAGE),
+                Arguments.of(10, bannedNumbers, BonusNumber.BANNED_NUMBER_MESSAGE)
+        );
+    }
+}
