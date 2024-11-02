c822a66a0e5a83f1fcc7c634ef4ed5412041beeb
fbehddn <tom0611@naver.com>
2024-11-02 17:45:17 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,9 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+}
