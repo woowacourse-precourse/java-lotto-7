6516123c579766802ceb335d045edcb669aa1333
pil-go <pilbongx@naver.com>
2024-11-02 19:47:05 +0900

    feat(LottoMachine): 구입 금액이 너무 클 경우 예외처리 추가

--- src/main/java/lotto/Domain/LottoMachine.java
+++ src/main/java/lotto/Domain/LottoMachine.java
@@ -4,6 +4,7 @@ import lotto.Messages.ErrorMessage;
 
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MAX_STOCK = 10000;
     private PurchaseAmount purchaseAmount;
 
     private LottoMachine(PurchaseAmount purchaseAmount) {
@@ -20,6 +21,7 @@ public class LottoMachine {
         int amount = purchaseAmount.getValue();
         checkMinimum(amount);
         checkDivisible(amount);
+        checkLimit(amount);
     }
 
     private static void checkMinimum(int amount) {
@@ -31,6 +33,12 @@ public class LottoMachine {
     private static void checkDivisible(int amount) {
         if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ErrorMessage.NOT_DIVISIBLE_PURCHASE_AMOUNT.getMessage());
+        }
+    }
+
+    private static void checkLimit(int amount) {
+        if (amount / LOTTO_PRICE > MAX_STOCK) {
+            throw new IllegalArgumentException(ErrorMessage.MAX_OUT_PURCHASE_AMOUNT.getMessage());
         }
     }
 
--- src/main/java/lotto/Messages/ErrorMessage.java
+++ src/main/java/lotto/Messages/ErrorMessage.java
@@ -7,8 +7,8 @@ public enum ErrorMessage {
     INTEGER_OUT_PURCHASE_AMOUNT("구입 금액은 정수 범위를 초과할 수 없습니다."),
     NEGATIVE_PURCHASE_AMOUNT("구입 금액은 음수로 입력할 수 없습니다."),
     INSUFFICIENT_PURCHASE_AMOUNT("구입 금액은 로또 1장의 가격보다 많아야 합니다."),
-    NOT_DIVISIBLE_PURCHASE_AMOUNT("구입 금액은 로또 판매 가격으로 나누어 떨어져야 합니다.");
-
+    NOT_DIVISIBLE_PURCHASE_AMOUNT("구입 금액은 로또 판매 가격으로 나누어 떨어져야 합니다."),
+    MAX_OUT_PURCHASE_AMOUNT("구입 금액이 최대 허용 금액을 초과했습니다.");
 
     private final String message;
 
