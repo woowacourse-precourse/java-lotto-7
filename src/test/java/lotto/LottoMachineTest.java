af7121532728829e34d9c3c45bcdd13dee1145c4
mingking2 <mingee641@gmail.com>
2024-11-02 16:24:47 +0900

    test: 구매한 로또 개수에 대한 테스트코드 추가

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lotto.model.LottoMachine;
+import lotto.model.LottoTicket;
+import org.junit.jupiter.api.Test;
+
+public class LottoMachineTest {
+
+    @Test
+    void 입력한_금액만큼_구매한_로또티켓을_생성한다() {
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoTicket lottoTicket = lottoMachine.purchaseTicket(5000);
+
+        assertEquals(5, lottoTicket.getLottos().size());
+    }
+
+}
