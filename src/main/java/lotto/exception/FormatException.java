774ff791b9600e33d209a9cfd85808a8506d7396
Changsu Park <gd12091@gmail.com>
2024-11-02 16:47:38 +0900

    feat: NumberFormat 예외 처리를 위한 클래스 구현

---
+++ src/main/java/lotto/exception/FormatException.java
@@ -0,0 +1,10 @@
+package lotto.exception;
+
+import lotto.enums.ErrorCode;
+
+public class FormatException extends NumberFormatException{
+
+    public FormatException(ErrorCode errorCode) {
+        super("[ERROR] " + errorCode.getMessage());
+    }
+}
