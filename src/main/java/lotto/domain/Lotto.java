a8d0ae0dad66057111953fb98b370b360de45256
minjae k <rkdalswoals@gmail.com>
2024-11-03 01:01:13 +0900

    refactor(domain): extract validation logic from Lotto to LottoValidator

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,17 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public List<Integer> getNumbers() {
+        return numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
