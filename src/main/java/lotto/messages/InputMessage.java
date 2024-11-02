e7018c8f32a4fc808bdc73ab4b0454e2e63738b2
GrO0vy <lmc2819@naver.com>
2024-11-02 22:26:20 +0900

    feat: 입력 메세지 열거 클래스 추가

---
+++ src/main/java/lotto/messages/InputMessage.java
@@ -0,0 +1,17 @@
+package lotto.messages;
+
+public enum InputMessage {
+    PRICE_INPUT_MESSAGE("구입금액을 입력해 주세요"),
+    WINNING_NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요"),
+    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요");
+
+    private final String message;
+
+    private InputMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
