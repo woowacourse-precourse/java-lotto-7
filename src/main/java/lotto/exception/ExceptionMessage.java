1e90b00646ace20be269132c47eef5f105b0517f
SukJin Kim <rkathrwks@gmail.com>
2024-11-02 20:59:12 +0900

    feat(validator): 구입 금액 유효성 검증(공백, 단위금액으로 나누어 떨어짐) 기능 구현

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,7 +1,11 @@
 package lotto.exception;
 
+import static lotto.validator.PurchaseAmountValidator.UNIT_PURCHASE_AMOUNT;
+
 public enum ExceptionMessage {
-    NON_POSITIVE_PURCHASE_AMOUNT("구입 금액은 양수이어야 합니다.");
+    BLANK_PURCHASE_AMOUNT("공백이 아닌 구입 금액을 입력해주세요."),
+    NON_POSITIVE_PURCHASE_AMOUNT("구입 금액은 양수이어야 합니다."),
+    NOT_DIVIDED_BY_UNIT_AMOUNT("구입 금액은 단위 금액인 " + UNIT_PURCHASE_AMOUNT + "으로 나누어 떨어져야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -1,15 +1,26 @@
 package lotto.validator;
 
+import static lotto.exception.ExceptionMessage.BLANK_PURCHASE_AMOUNT;
 import static lotto.exception.ExceptionMessage.NON_POSITIVE_PURCHASE_AMOUNT;
+import static lotto.exception.ExceptionMessage.NOT_DIVIDED_BY_UNIT_AMOUNT;
 
 import java.util.regex.Pattern;
+import org.junit.platform.commons.util.StringUtils;
 
 public class PurchaseAmountValidator {
     private static final String POSITIVE_NUMBER_REGEX = "^[1-9][0-9]*$";
-    private static final long UNIT_PURCHASE_AMOUNT = 1000L;
+    public static final long UNIT_PURCHASE_AMOUNT = 1000L;
 
     public void validate(String purchaseAmount) {
+        validateBlank(purchaseAmount);
         validatePositiveNumber(purchaseAmount);
+        validateDivisibleByUnitAmount(purchaseAmount);
+    }
+
+    private void validateBlank(String purchaseAmount) {
+        if (StringUtils.isBlank(purchaseAmount)) {
+            throw new IllegalArgumentException(BLANK_PURCHASE_AMOUNT.getMessage());
+        }
     }
 
     private void validatePositiveNumber(String purchaseAmount) {
@@ -22,5 +33,13 @@ public class PurchaseAmountValidator {
         return !Pattern.matches(POSITIVE_NUMBER_REGEX, purchaseAmount);
     }
 
-    private void
+    private void validateDivisibleByUnitAmount(String purchaseAmount) {
+        if (isNotDividedByUnitAmount(purchaseAmount)) {
+            throw new IllegalArgumentException(NOT_DIVIDED_BY_UNIT_AMOUNT.getMessage());
+        }
+    }
+
+    private boolean isNotDividedByUnitAmount(String purchaseAmount) {
+        return !(Long.parseLong(purchaseAmount) % UNIT_PURCHASE_AMOUNT == 0);
+    }
 }
