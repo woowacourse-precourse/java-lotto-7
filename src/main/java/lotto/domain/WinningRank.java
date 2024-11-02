22c32a24db646120c0288c3664a7d08b3cef31e3
giwoong <dev.choi0409@gmail.com>
2024-11-02 13:23:22 +0900

    refactor: WinningRank 클래스 수정, 보너스 번호 일치 로직 수정
    보너스 번호 일치 여부를 확인하는 requiresBonusMatch 필드를 추가합니다. 그리고 isMatch 메소드에서 보너스 번호 일치 여부를 확인하는 로직을 수정합니다.

--- src/main/java/lotto/domain/WinningRank.java
+++ src/main/java/lotto/domain/WinningRank.java
@@ -10,25 +10,27 @@ public enum WinningRank {
     FIFTH(3, MESSAGE_FIFTH, 5_000),
     FOURTH(4, MESSAGE_FOURTH, 50_000),
     THIRD(5, MESSAGE_THIRD, 1_500_000),
-    SECOND(5, MESSAGE_SECOND, 30_000_000),
+    SECOND(5, MESSAGE_SECOND, 30_000_000, true),
     FIRST(6, MESSAGE_FIRST, 2_000_000_000);
 
     private final int matchCount;
     private final String message;
     private final int prizeMoney;
+    private final boolean requiresBonusMatch;
 
     WinningRank(int matchCount, String message, int prizeMoney) {
+        this(matchCount, message, prizeMoney, false);
+    }
+
+    WinningRank(int matchCount, String message, int prizeMoney, boolean requiresBonusMatch) {
         this.matchCount = matchCount;
         this.message = message;
         this.prizeMoney = prizeMoney;
+        this.requiresBonusMatch = requiresBonusMatch;
     }
 
     public boolean isMatch(long matchCount, boolean bonusMatch) {
-        if (this == THIRD) {
-            return matchCount == this.matchCount && !bonusMatch;
-        }
-
-        return matchCount == this.matchCount;
+        return this.matchCount == matchCount && (!requiresBonusMatch || bonusMatch);
     }
 
     public String getMessage() {
@@ -38,5 +40,4 @@ public enum WinningRank {
     public int getPrizeMoney() {
         return prizeMoney;
     }
-
 }
