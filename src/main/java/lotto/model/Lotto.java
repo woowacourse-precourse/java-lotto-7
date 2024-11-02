b8f4f0589b9cc0d5b81318ead89b9cbe7177a142
Yooooonseo <81520018+Yooooonseo@users.noreply.github.com>
2024-11-02 14:10:05 +0900

    refactor(model): Lotto 클래스 리팩토링
    - 로또 번호 유효성 검증 기능 메서드 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,30 +11,11 @@ public class Lotto {  //개수, 범위, 중복 유효성 검사
     }
 
     private void validate(List<Integer> numbers) {
-        validateNumberCount(numbers);
-        validateNumberRange(numbers);
-        validateUniqueNumbers(numbers);
-    }
-
-    // 로또 번호 개수 검증
-    private void validateNumberCount(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    // 로또 번호 범위 검증
-    private void validateNumberRange(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    // 로또 번호 중복 검증
-    private void validateUniqueNumbers(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
-        }
+        LottoValidator.validateNumberCount(numbers);
+        LottoValidator.validateNumberRange(numbers);
+        LottoValidator.validateUniqueNumbers(numbers);
+        // 숫자 형식 검증
+        //LottoValidator.validateNumberIsNumeric(numbers);
     }
 
     public List<Integer> getNumbers() {
