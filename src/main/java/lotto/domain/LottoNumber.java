0621e9a1adc1046096a4c24dd07a96b1e455f154
jae woo Jung <awfjol2008@gmail.com>
2024-11-02 19:21:31 +0900

    feat: 로또 번호 낱개에 관한 LottoNumber 클래스 구현

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class LottoNumber {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+
+	private final int number;
+
+	public LottoNumber(int number) {
+		this.number = number;
+	}
+}
