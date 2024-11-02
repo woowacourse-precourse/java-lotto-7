4e45b76320c93d23dfb27d480fcf5e32e94cf0dc
DK <laancer4@gmail.com>
2024-11-02 19:34:53 +0900

    style : 클래스 시작 전 공백 추가

--- src/main/java/lotto/RandomLottoStrategy.java
+++ src/main/java/lotto/RandomLottoStrategy.java
@@ -3,7 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class RandomLottoStrategy implements LottoGenerateStrategy{
+public class RandomLottoStrategy implements LottoGenerateStrategy {
     @Override
     public List<Integer> pickNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
