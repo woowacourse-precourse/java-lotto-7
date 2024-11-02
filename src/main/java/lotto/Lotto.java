595910e2d615a36fd11c75552a3802bde0aeff5f
GrO0vy <lmc2819@naver.com>
2024-11-02 16:26:44 +0900

    refactor: 중복 숫자 여부 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -12,18 +13,28 @@ public class Lotto {
         sort();
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
+        isSixCountOfList(numbers);
+        isDuplicatedNumber(numbers);
+    }
+
+    private void isDuplicatedNumber(List<Integer> numbers){
+        if(numbers.size() > new HashSet<>(numbers).size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호 6개는 각각 모두 중복 될 수 없습니다.");
+        }
+    }
+
+    private void isSixCountOfList(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
     private void sort(){
         Collections.sort(numbers);
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 }
