d26f8e4c89cbc0dda29b9264516550e51ff99e61
sese2204 <sese2204@naver.com>
2024-11-02 20:04:21 +0900

    feat(domain): 당첨 결과를 계산하는 WinningResult 클래스 구현
    - 등수와 당첨 내역 HashMapt 형태로 반환하는 caculateResult 메소드 구현
    - 수익률 계산 및 소수점 둘째 자리에서 반올림 하여 반환하는 calculateReturnRate 구현

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,89 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningResult {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningResult(Lotto winningLotto, int bonusNumber) {
+        validateBonusNumber(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Map<Prize, Integer> calculateResult(Lottos lottos) {
+        Map<Prize, Integer> result = initializeResultMap();
+        for(Lotto lotto : lottos.getLottos()) {
+            result.merge(calculatePrize(lotto), 1, Integer::sum);
+        }
+        return result;
+    }
+
+    public Prize calculatePrize(Lotto lotto) {
+        int matchCount = countMatchLotto(lotto.getNumbers());
+        boolean isMatchBonus = false;
+        if (matchCount == 5) {
+            isMatchBonus = matchBonusNumber(lotto);
+        }
+        return Prize.from(matchCount, isMatchBonus);
+    }
+
+    public double calculateReturnRate(Lottos lottos) {
+        long totalPrize = calculateTotalPrize(calculateResult(lottos));
+        long totalPurchaseAmount = calculateTotalPurchaseAmount(lottos);
+        return formatReturnRate((double) totalPrize / totalPurchaseAmount * 100);
+    }
+
+    private void validateBonusNumber(Lotto winningLotto, int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR} 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+
+    }
+
+    private Map<Prize, Integer> initializeResultMap() {
+        Map<Prize, Integer> result = new HashMap<>();
+        for(Prize prize : Prize.values()) {
+            result.put(prize, 0);
+        }
+        return result;
+    }
+
+    private int countMatchLotto(List<Integer> lottoNumbers) {
+        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+        int matchCount = 0;
+        for (Integer lottoNumber : lottoNumbers) {
+            if (winningLottoNumbers.contains(lottoNumber)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    private boolean matchBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+
+    private long calculateTotalPrize(Map<Prize, Integer> result) {
+        long totalPrize = 0;
+        for (Prize prize : result.keySet()) {
+            totalPrize += prize.getPrizeAmount() * result.get(prize);
+        }
+        return totalPrize;
+    }
+
+    private long calculateTotalPurchaseAmount(Lottos lottos) {
+        return lottos.size()* 1_000L;
+    }
+
+    private double formatReturnRate(double rate) {
+        return Math.round(rate * 10) / 10.0;
+    }
+}
