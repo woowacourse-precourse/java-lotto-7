8860b9a8d36929c660fafb5bc5b04dc45a52cc75
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 22:59:15 +0900

    refactor(WinningNumberParser): 예외 처리를 구체적으로 변경
    split 메서드에서 발생한 NumberFormatException을 catch 하여 IllegalArgumentException을 발생시키도록 수정

--- src/main/java/lotto/WinningNumberParser.java
+++ src/main/java/lotto/WinningNumberParser.java
@@ -6,8 +6,13 @@ import java.util.stream.Collectors;
 
 public class WinningNumberParser {
     public static List<Integer> parse(String input) {
-        return Arrays.stream(input.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.", e);
+        }
     }
 }
