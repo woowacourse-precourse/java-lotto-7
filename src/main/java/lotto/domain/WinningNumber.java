59a3e70f32d07822992d764712583aa9305e58d9
hwan2-99 <thh1233123@gmail.com>
2024-11-02 10:15:49 +0900

    refactor: 당첨번호 도메인 클래스의 에러메시지 상수화

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningNumber {
+    private static final String CONTAIN_BONUS_NUMBER_ERROR = "[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.";
+
     private final Lotto winningNumber;
     private final LottoNumber bonusNumber;
 
@@ -18,7 +20,7 @@ public class WinningNumber {
 
     private void validateContainBonusNumber(Lotto winningNumber, LottoNumber bonusNumber) {
         if (winningNumber.hasNumber(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
+            throw new IllegalArgumentException(CONTAIN_BONUS_NUMBER_ERROR);
         }
     }
 
