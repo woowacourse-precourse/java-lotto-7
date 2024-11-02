0ce31981ffc402cb2fb264de16d0bcd72ec06bef
Jungwoo Chu <cjw980930@gmail.com>
2024-11-02 20:57:26 +0900

    fix: for문 증감자 오류 수정
    for문 증감자에 count++을 하여 무한루프가 발생함. i++로 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class LottoGenerator {
     public List<Lotto> generate(int count) {
         List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < count; count++) {
+        for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottoTickets.add(new Lotto(numbers));
         }
