7465e727535a49b5fd0e039c2aee4a9a0be74192
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-02 20:19:33 +0900

    feat(LottoController, OuputView): 티켓 개수 출력을 위한 LottoController 수정 및 OutputView 작성
    - LottoController에서 생성된 로또 티켓 개수를 계산하고 OutputView에 전달
    - OutputView에서 티켓 개수를 출력하는 displayPurchaseCount 메서드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,12 +6,16 @@ import lotto.model.LottoTicket;
 import lotto.validator.InputValidator;
 import lotto.validator.LottoTicketValidator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
 
 	public void run() {
 		int purchasePrice = getValidatedPurchasePrice();
+		
 		List<LottoTicket> tickets = LottoTicketGenerator.generateTickets(purchasePrice);
+		int ticketsCount = tickets.size();
+		OutputView.displayPurchaseCount(ticketsCount);
 	}
 
 	private int getValidatedPurchasePrice() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
 public class OutputView {
-
+	private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+	
+	public static void displayPurchaseCount(int ticketCount) {
+		System.out.println(ticketCount + TICKET_COUNT_MESSAGE);
+	}
 }
