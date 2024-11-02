beda2f586257a9242f3ade0c4fd6c12381b00efa
SukJin Kim <rkathrwks@gmail.com>
2024-11-02 20:20:26 +0900

    refactor(validator): 양수 여부 검증 메서드명 수정

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -6,18 +6,21 @@ import java.util.regex.Pattern;
 
 public class PurchaseAmountValidator {
     private static final String POSITIVE_NUMBER_REGEX = "^[1-9][0-9]*$";
+    private static final long UNIT_PURCHASE_AMOUNT = 1000L;
 
     public void validate(String purchaseAmount) {
         validatePositiveNumber(purchaseAmount);
     }
 
     private void validatePositiveNumber(String purchaseAmount) {
-        if (isNotPositiveNumber(purchaseAmount)) {
+        if (isNonPositiveNumber(purchaseAmount)) {
             throw new IllegalArgumentException(NON_POSITIVE_PURCHASE_AMOUNT.getMessage());
         }
     }
 
-    private boolean isNotPositiveNumber(String purchaseAmount) {
+    private boolean isNonPositiveNumber(String purchaseAmount) {
         return !Pattern.matches(POSITIVE_NUMBER_REGEX, purchaseAmount);
     }
+
+    private void
 }
