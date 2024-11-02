2c22b7e9e992351dc5eac8cb4331aeb0d728a381
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-02 14:53:52 +0900

    feat : 로또 객체 검증로직 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,19 +1,47 @@
 package lotto.model;
 
+import lotto.enums.Constants;
+import lotto.enums.ExceptionMessage;
+
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.enums.Constants.MAX_LOTTO_NUM;
+import static lotto.enums.Constants.MIN_LOTTO_NUM;
+import static lotto.enums.ExceptionMessage.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_WINNING_NUMBERS.getMessage());
         }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> lottoNums= new HashSet<>(numbers);
+        if (lottoNums.size()!= 6){
+            throw new IllegalArgumentException(INVALID_WINNING_NUM_DUPLICATE.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_LOTTO_NUM.getValue() || number > MAX_LOTTO_NUM.getValue()) {
+                throw new IllegalArgumentException(INVALID_LOTTO_NUM_RANGE.getMessage());
+            }
+        }
+
     }
 
     // TODO: 추가 기능 구현
