19b332d9c3152f6525dbd9b7c4da722be69d8f8b
song hyeongyu <lit3207@ajou.ac.kr>
2024-11-02 19:34:06 +0900

    feat: add printResult in OutputView Class

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -10,4 +10,15 @@ public class OutputView {
             System.out.println(lotto);
         }
     }
+
+    public static void printResult(Map<Prize, Integer> prizeCountMap, double returnRate) {
+        System.out.println("당첨 결과:");
+        System.out.println("3개 일치 (5,000원) - " + prizeCountMap.getOrDefault(Prize.FIFTH, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + prizeCountMap.getOrDefault(Prize.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizeCountMap.getOrDefault(Prize.THIRD, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeCountMap.getOrDefault(Prize.SECOND, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizeCountMap.getOrDefault(Prize.FIRST, 0) + "개");
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", returnRate);
+    }
+
 }
