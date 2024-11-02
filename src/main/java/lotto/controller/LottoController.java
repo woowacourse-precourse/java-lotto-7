de9574f9ce61a5542b1317b010d44bf37981f567
jungsu <vwx1@naver.com>
2024-11-02 20:39:34 +0900

    feat: LottoController 클래스 생성 및 입력 처리 로직 추가
    - InputValidator를 활용하여 유효성을 체크하여 코드의 재사용성과 가독성을 높임.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import java.util.List;
 import lotto.service.LottoService;
+import lotto.validator.InputValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -10,21 +11,24 @@ public class LottoController {
   private final InputView inputView;
   private final OutputView outputView;
   private final LottoService lottoService;
-
-  public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+  private final InputValidator inputValidator;
+  public LottoController(InputView inputView, OutputView outputView, LottoService lottoService, InputValidator inputValidator) {
     this.inputView = inputView;
     this.outputView = outputView;
     this.lottoService = lottoService;
+    this.inputValidator = inputValidator;
   }
 
   public void run() {
 
-    inputView.printPurchaseMessage();
-    //validator 따로 구현
-    String purchaseAmount = inputView.readPurchaseAmount();
-    String winningNumbers = inputView.readWinningNumbers();
-    String bonusNumber = inputView.readBonusNumber();
+    String purchaseAmountInput = inputView.readPurchaseAmount();
+    int validatedPurchaseAmount = InputValidator.validatePurchaseAmount(purchaseAmountInput);
 
+    String winningNumbersInput = inputView.readWinningNumbers();
+    List<Integer> validatedWinningNumbers = InputValidator.validateWinningNumbers(winningNumbersInput);
+
+    String bonusNumberInput = inputView.readBonusNumber();
+    int validatedBonusNumber = InputValidator.validateBonusNumber(bonusNumberInput, validatedWinningNumbers);
   }
 
 }
