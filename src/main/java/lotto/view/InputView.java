7bcbf7d2dec3d4374c66645497c7fc3103d15a54
jae woo Jung <awfjol2008@gmail.com>
2024-11-02 14:07:45 +0900

    feat: 구입 금액이 정수인지 검증하는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,4 +12,14 @@ public class InputView {
 			System.out.println("[ERROR] 입력값은 비어있지 않아야 합니다.");
 		}
 	}
+
+	private void validateDigit(String input) {
+		if (!isDigit(input)) {
+			System.out.println("[ERROR] 입력값은 숫자여야 합니다.");
+		}
+	}
+
+	private boolean isDigit(String input) {
+		return input.chars().allMatch(Character::isDigit);
+	}
 }
