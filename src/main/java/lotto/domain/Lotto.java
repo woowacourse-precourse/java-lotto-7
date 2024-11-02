5651ae01801ff6d4a9db07d13066aa2dfb349ff8
OSEUK <tpdnrzmzm3@naver.com>
2024-11-02 18:19:31 +0900

    refactor(domain) : Lotto 리팩토링
    중복 검사 로직을 추가함

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,12 +10,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        if (numbers.stream().distinct().count() != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
