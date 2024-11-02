c7451ab3a94bda847ea3189e91c0e65e2e3f0b52
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 02:15:18 +0900

    feat: input메시지 상수 enum 작성

---
+++ src/main/java/lotto/constants/InputMessage.java
@@ -0,0 +1,17 @@
+package lotto.constants;
+
+public enum InputMessage {
+    GET_LOTTO_AMOUNTS_MESSAGE("구입 금액을 입력해 주세요."),
+    GET_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
+    GET_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
+    ;
+    private final String message;
+
+    InputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
