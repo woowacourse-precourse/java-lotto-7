9946bb8c37da436c9accfe51b4532cf92d7cf5a7
Park, Sojeong <sojeong2101@gmail.com>
2024-11-03 00:52:18 +0900

    test($LottoGeneratorTest): 정렬 메서드와 로또 발행 메서드 테스트 구현

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.purchase.LottoGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    @DisplayName("숫자 배열이 주어지면 정렬한다.")
+    @Test
+    void shouldSortNumbersWhenGiveRawNumbers() {
+        // give
+        List<Integer> actualNumbers = new ArrayList<>(List.of(1, 4, 5, 2, 10, 17));
+        List<Integer> expectedNumbers = List.of(1, 2, 4, 5, 10, 17);
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        // when
+        lottoGenerator.manufactureRawNumbers(actualNumbers);
+
+        // then
+        Assertions.assertThat(actualNumbers).isEqualTo(expectedNumbers);
+    }
+
+    @DisplayName("원하는 숫자만큼 로또를 발행하여 반환한다.")
+    @Test
+    void shouldMakeLotteriesWhenRequestCreate() {
+        // give
+        int expectedTotalLotteryCount = 3;
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        // when
+        int actualTotalLotteryCount = lottoGenerator.createLotteries(expectedTotalLotteryCount).size();
+
+        // then
+        Assertions.assertThat(actualTotalLotteryCount).isEqualTo(expectedTotalLotteryCount);
+    }
+}
