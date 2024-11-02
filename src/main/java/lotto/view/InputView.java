14b3e888999e7bce8d6532901a72da3dfb63a52f
MinGwan Kim <fpem3309@gmail.com>
2024-11-02 12:02:33 +0900

    feat: 잘못된 값 입력 시 예외 발생/출력과 다시 입력 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,20 +8,41 @@ import static lotto.util.PurchaseUtils.getThousandUnitCount;
 
 public class InputView {
     public int readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String inputPurchaseAmount = Console.readLine();
-        return getThousandUnitCount(inputPurchaseAmount);
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String inputPurchaseAmount = Console.readLine();
+                return getThousandUnitCount(inputPurchaseAmount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
     public void readWinningNumbers() {
-        System.out.println("당첨번호를 입력해 주세요.");
-        String inputWinningNumbers = Console.readLine();
-        WinningNumberValidation.validate(inputWinningNumbers);
+        while (true) {
+            try {
+                System.out.println("당첨번호를 입력해 주세요.");
+                String inputWinningNumbers = Console.readLine();
+                WinningNumberValidation.validate(inputWinningNumbers);
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public void readBonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String inputBonusNumber = Console.readLine();
-        BonusNumberValidation.validate(inputBonusNumber);
+    public void readBonusNumber() {
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String inputBonusNumber = Console.readLine();
+                BonusNumberValidation.validate(inputBonusNumber);
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
