f313ef810ca38ebbaeb996e7115a364eb6b591bf
eb9862 <luclead622@gmail.com>
2024-11-02 22:33:04 +0900

    feat: 당첨에 대한 정보를 가진 enum 클래스인 WinningInfo 구현
    당첨 등수, 일치하는 번호의 개수, 상금, 당첨 티켓의 개수를 포함

---
+++ src/main/java/lotto/domain/WinningInfo.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import static lotto.domain.LottoInfo.PICK_COUNT;
+
+public enum WinningInfo {
+
+    FIFTH(5, 3, 5_000),
+    FOURTH(4, 4, 50_000),
+    THIRD(3, 5, 1_500_000),
+    SECOND(2, 5, 30_000_000),
+    FIRST(1, 6, 2_000_000_000);
+
+    private final int place;
+    private final int matchingNumberCount;
+    private final int prizeMoney;
+    private int winningTicketCount;
+
+    WinningInfo(int place, int matchingNumberCount, int prizeMoney) {
+        this.place = place;
+        this.matchingNumberCount = matchingNumberCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public void win() {
+        winningTicketCount++;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public int getMatchingNumberCount() {
+        return matchingNumberCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public int getWinningTicketCount() {
+        return winningTicketCount;
+    }
+}
