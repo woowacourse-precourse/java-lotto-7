f7b8454949a112cf64cdcbcd4a65b699c8e95d2b
Jo Myeong-geun <happy63590@gmail.com>
2024-11-03 05:05:43 +0900

    feat: 발행한 로또의 등수 저장

---
+++ src/main/java/lotto/enums/Rank.java
@@ -0,0 +1,33 @@
+package lotto.enums;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean requiresBonus;
+    private final int winningAmount;
+
+    Rank(int matchCount, boolean requiresBonus, int winningAmount) {
+        this.matchCount = matchCount;
+        this.requiresBonus = requiresBonus;
+        this.winningAmount = winningAmount;
+    }
+
+    public static Rank getRank(int matchCount, boolean containsBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount && rank.requiresBonus == containsBonus)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+}
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,16 +3,28 @@ package lotto.service;
 import static lotto.util.RandomUtil.generateLottoNumbers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 import lotto.domain.Lotto;
+import lotto.enums.Rank;
 
 public class LottoService {
 
     private final List<Lotto> lottos = new ArrayList<>();
+    private final Map<Rank, Integer> results = new HashMap<>();
 
     public void generateLottos(int lottoCount) {
         IntStream.range(0, lottoCount)
                 .forEach(i -> lottos.add(new Lotto(generateLottoNumbers())));
+    }
+
+    public void saveLottoRanks(List<Integer> winningNumbers, int bonusNumber) {
+        lottos.forEach(lotto -> {
+            int count = lotto.countMatchingNumbers(winningNumbers);
+            Rank rank = Rank.getRank(count, lotto.containsBonusNumber(bonusNumber));
+            results.put(rank, results.getOrDefault(rank, 0) + 1);
+        });
     }
 }
