09d12e50b0c598df62dff32e4eb0c8c55761dc2d
bowook <mbosx316@naver.com>
2024-11-02 17:20:12 +0900

    refactor(BonusNumber) : BonusNumber 리팩토링
    - BonusNumber도메인 내에서 검증하도록 기능 개선

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,13 +1,26 @@
 package lotto.domain;
 
+import lotto.constants.Constants;
+import lotto.validator.exception.ErrorMessage;
+import lotto.validator.exception.LottoException;
+
 public class BonusNumber {
     private final int number;
 
     public BonusNumber(int number) {
+        validateRange(number);
         this.number = number;
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < Constants.LOTTO_MIN_NUMBER.getValue()
+                || bonusNumber > Constants.LOTTO_MAX_NUMBER.getValue()) {
+            throw LottoException.from(ErrorMessage.BONUS_NUMBER_RANGE_ERROR);
+        }
     }
 
     public int getNumber() {
         return number;
     }
+
 }
