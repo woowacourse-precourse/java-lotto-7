9f18d266390c781a1615de44f99f9c98d7f77f05
tentenball <lth8905@gmail.com>
2024-11-02 18:20:30 +0900

    feat: 로또 생성시 검증 로직 추가
    * 로또 숫자의 재사용성을 위해 LottoNumberConstant를 만들었습니다. 로또 번호 중복, 로또번호 범위에 대한 검증을 작성하였습니다.

--- src/main/java/lotto/domains/lotto/Lotto.java
+++ src/main/java/lotto/domains/lotto/Lotto.java
@@ -1,20 +1,46 @@
 package lotto.domains.lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import lotto.domains.lotto.constant.LottoNumberConstant;
+import lotto.exception.ExceptionMessage;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+	private static void validate(List<Integer> numbers) {
+		validateLottoSize(numbers);
+		validateLottoNumberIsDuplicated(numbers);
+		validateLottoNumberBoundary(numbers);
+	}
 
-    // TODO: 추가 기능 구현
+	private static void validateLottoSize(List<Integer> numbers) {
+		if (numbers.size() != LottoNumberConstant.LOTTO_NUMBER_SIZE) {
+			throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_SIZE.toString());
+		}
+	}
+
+	private static void validateLottoNumberIsDuplicated(List<Integer> numbers) {
+		Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+		if (nonDuplicatedNumbers.size() != numbers.size()) {
+			throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUMBER_IS_DUPLICATED.toString());
+		}
+	}
+
+	private static void validateLottoNumberBoundary(List<Integer> numbers) {
+		numbers.forEach(number -> {
+			if (number < LottoNumberConstant.LOTTO_MINIMUM_NUMBER
+				|| number > LottoNumberConstant.LOTTO_MAXIMUM_NUMBER) {
+				throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUMBER_IS_OUT_OF_BOUND.toString());
+			}
+		});
+	}
+
 }
