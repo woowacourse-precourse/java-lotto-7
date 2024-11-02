085fd94d7772c552e1e5ef80f8476a383426d9ed
gladhee <gladhee@icloud.com>
2024-11-02 22:30:14 +0900

    feat(Lottos): Lotto 일급컬렉션 도메인 작성
    countMatchesWith(Lotto winningLotto, BonusNumber bonusNumber): List<Rank>
    로또들을 순회하며 당첨번호와 비교해 해당 등수를 모은 리스트 반환

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Rank> countMatchesWith(Lotto winningLotto, BonusNumber bonusNumber) {
+        List<Rank> ranks = new ArrayList<>();
+
+        lottos.forEach(lotto -> {
+            int matchCount = matchCount(lotto, winningLotto);
+            boolean bonusMatch = lotto.contains(bonusNumber.getNumber());
+            ranks.add(Rank.valueOf(matchCount, bonusMatch));
+        });
+
+        return ranks;
+    }
+
+    private int matchCount(Lotto lotto, Lotto winningLotto) {
+        return (int) lotto.getNumbers().stream()
+                .filter(winningLotto::contains)
+                .count();
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+}
