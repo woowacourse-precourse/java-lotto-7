fec3634b114e3e3887fc84303e5643b67d32290b
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-02 21:14:37 +0900

    feat : 로또 번호 범위 유효성 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final Integer lowerBound = 1;
+    private static final Integer upperBound = 45;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -18,13 +21,22 @@ public class Lotto {
     }
 
     private void validateDuplicate(List<Integer> numbers) {
-        if(numbers.size() != getDistinctSize(numbers)){
+        if (numbers.size() != getDistinctSize(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
     private static int getDistinctSize(List<Integer> numbers) {
         return numbers.stream().distinct().toList().size();
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(num ->
+        {
+            if (num < lowerBound || num > upperBound) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 숫자만 가능합니다.");
+            }
+        });
     }
 
     // TODO: 추가 기능 구현
