ed4e7a09fcaa0e4f20d3975e80c628d2e457be01
신관규 <soundbar91@gmail.com>
2024-11-02 11:13:38 +0900

    feat: LottoService 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,19 @@
 package lotto.controller;
 
 import lotto.dto.LottoPurchaseAmount;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoService lottoService;
 
-    public LottoController(InputView inputView, OutputView outputView) {
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoService = lottoService;
     }
 
     public void setUp() {
