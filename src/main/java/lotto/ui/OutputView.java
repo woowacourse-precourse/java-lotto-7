22cf18bffce9185fec56daa09d4ef574578a958e
Mini <mini@MiniMac.local>
2024-11-02 22:45:04 +0900

    fix: correct prize amount for 4 matches in OutputView

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -21,7 +21,7 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.println("3개 일치 (5,000원) - " + lottoResult.getRankCount(LottoRank.FIFTH) + "개");
-        System.out.println("4개 일치 (5,0000원) - " + lottoResult.getRankCount(LottoRank.FOURTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResult.getRankCount(LottoRank.FOURTH) + "개");
         System.out.println("5개 일치 (1,500,000원) - " + lottoResult.getRankCount(LottoRank.THIRD) + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.getRankCount(LottoRank.SECOND) + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.getRankCount(LottoRank.FIRST) + "개");
