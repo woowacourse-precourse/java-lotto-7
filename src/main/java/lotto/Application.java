dde83825112dd08272e53e598c78846cf55f29e3
Etwashoeren <sungmin0609@nate.com>
2024-11-02 20:43:40 +0900

    feat. Application에 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,18 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        UserView userView = new UserView();
+        UserInput userInput = new UserInput();
+        LottoMachine lottoMachine = new LottoMachine();
+        ResultCalculator resultCalculator = new ResultCalculator();
+
+        LottoController lottoController = new LottoController(userView, userInput ,lottoMachine, resultCalculator);
+
+        try {
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
