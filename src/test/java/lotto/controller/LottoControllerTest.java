f5f6f81f16b2891f9b1a678e22b2549c0dd47128
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-03 01:38:57 +0900

    test(controller/LottoControllerTest): LottoController 단위 테스트 추가
    - 유효한 구매 금액 입력 시 예외 없이 구매 금액이 반환되는지 테스트
    - 유효한 구매 금액 입력 시 해당 금액에 맞는 개수의 티켓이 생성되는지 확인
    - 정수가 아닌 입력, 1000원 단위가 아닌 입력, 음수 및 0 입력 시 예외 테스트

---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,33 @@
+package lotto.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoControllerTest {
+	private final LottoController lottoController = new LottoController();
+
+	@Test
+	@DisplayName("유효한 구매 금액이 입력된 경우 예외가 발생하지 않고 구매 금액이 반환된다.")
+	void should_ReturnPurchasePrice_When_ValidPriceInput() {
+		int purchasePrice = lottoController.getValidatedPurchasePrice("5000");
+
+		assertThat(purchasePrice).isEqualTo(5000);
+	}
+
+	@Test
+	@DisplayName("구매 금액이 정수가 아닌 경우 예외가 발생한다.")
+	void should_ThrowException_When_PurchasePriceIsNotInteger() {
+		assertThatThrownBy(() -> lottoController.getValidatedPurchasePrice("abc"))
+				.isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 입력한 값이 정수가 아닙니다.");
+	}
+
+	@Test
+	@DisplayName("구매 금액이 1000원 단위가 아닌 경우 예외가 발생한다.")
+	void should_ThrowException_When_PurchasePriceNotDivisibleByThousand() {
+		assertThatThrownBy(() -> lottoController.getValidatedPurchasePrice("5500"))
+				.isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+	}
+}
