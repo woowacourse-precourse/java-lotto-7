b1158dfdb2beb39089e3345f9a650071a779650a
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-11-02 20:09:35 +0900

    feat: Lotto 모델 구현 및 오름차순 정렬 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,35 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+// Lotto에 numbers 이외의 필드를 추가할 수 없다.
+// numbers의 접근제어자 변경 불가
+// Lotto의 패키지 변경 가능
+
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void sortNumbers() {
+        Collections.sort(numbers);
+    }
+}
