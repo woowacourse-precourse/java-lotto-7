4a751b3ca3fdeb2d33efc620ed3fa4a8de5cf6a0
yeong-hwan <jghff700@naver.com>
2024-11-02 23:47:52 +0900

    refactor(enums): append error message for duplication

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -1,9 +1,14 @@
 package lotto.enums;
 
 public enum ErrorMessage {
-    INVALID_AMOUNT("금액은 1,000원 단위로 입력해야 합니다."),
-    INVALID_WINNING_NUMBERS("당첨 번호는 6개여야 합니다."),
-    INVALID_BONUS_NUMBER("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_AMOUNT("금액은 " + LottoConstants.LOTTO_PRICE.getValue() + "원 단위로 입력해야 합니다."),
+    INVALID_WINNING_NUMBERS_COUNT("로또 번호는 " + LottoConstants.LOTTO_NUMBERS_COUNT.getValue() + "개여야 합니다."),
+    INVALID_BONUS_NUMBER("보너스 번호는 " + LottoConstants.MIN_LOTTO_NUMBER.getValue() + "부터 "
+            + LottoConstants.MAX_LOTTO_NUMBER.getValue() + " 사이의 숫자여야 합니다."),
+    DUPLICATED_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
+    INVALID_WINNING_NUMBES_RANGE("로또 번호는 " + LottoConstants.MIN_LOTTO_NUMBER.getValue() + "부터 "
+            + LottoConstants.MAX_LOTTO_NUMBER.getValue() + " 사이의 숫자여야 합니다.");
+
 
     private static final String ERROR_TAG = "[ERROR]";
     private static final String ERROR_SEPARATOR = " ";  // 태그 뒤 공백
