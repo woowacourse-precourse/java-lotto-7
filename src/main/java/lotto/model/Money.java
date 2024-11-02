a9bfc0fa3d8090ecd0edaaeb1b3c201a43476cc8
wooqqq <sge0225@naver.com>
2024-11-03 05:03:03 +0900

    feat(Money): 구입 금액에 대한 예외 처리(1,000원 이하의 값이 입력된 경우)

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -8,11 +8,18 @@ public record Money (int amount) {
 
     private void validate(int input) {
         validateIsDivisible(input);
+        validateBelowMinimum(input);
     }
 
     private void validateIsDivisible(int input) {
         if (input % 1000 != 0) {
             throw new IllegalArgumentException("1,000 원 단위의 값이어야 합니다.");
+        }
+    }
+
+    private void validateBelowMinimum(int input) {
+        if (input < 1000) {
+            throw new IllegalArgumentException("1,000 원 이상의 값이어야 합니다.");
         }
     }
 }
