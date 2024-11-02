45fa376af61fb87304f9f1805d9a1dabccb445d9
dawit0905 <dawit0905@gmail.com>
2024-11-02 21:20:49 +0900

    feat: LottoController run 함수 추가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,8 +1,28 @@
 package controller;
 
+import java.util.List;
+import lotto.Lotto;
+import service.LottoService;
+
 public class LottoController {
 
+    private List<Lotto> lottoList;
+    private Lotto WinnerLottoNumber;
+    private int lottoPurchaseAmount;
+    private int lottoDrawCount;
+    private LottoService lottoservice = new LottoService();
+
     public void run() {
+        lottoPurchaseAmount = lottoservice.getLottoPurchaseAmount();
+        lottoDrawCount = lottoservice.getLottoDrawCount(lottoPurchaseAmount);
+        lottoList = lottoservice.generateRandomLottoNumbers(lottoDrawCount);
+
+        lottoservice.printLottoNumbers(lottoList);
+        WinnerLottoNumber = lottoservice.getWinnerLottoNumbers();
+        int bonusNumber = lottoservice.getBonusNumber();
+
+
 
     }
+
 }
