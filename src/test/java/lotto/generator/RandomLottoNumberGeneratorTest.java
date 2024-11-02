969e4129ea82cff8fe36fb4361595bc47dee3158
Sinorang <sinholang@naver.com>
2024-11-02 16:51:11 +0900

    test: 로또 티켓 생성 테스트 추가
    - LottoTicketTest 클래스 생성
    - 로또 티켓이 올바르게 생성되는지 확인하기 위한 테스트 추가
    - RandomLottoNubmerGeneratorTest로 클래스명을 수정하여 어떤 클래스를
      테스트하는지 명확히 함

--- src/test/java/lotto/generator/RandomLottoNumberGeneratorTest.java
+++ src/test/java/lotto/generator/RandomLottoNumberGeneratorTest.java
@@ -8,35 +8,35 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LottoGeneratorTest {
-    private RandomValueGenerator lottoGenerator;
+class RandomLottoNumberGeneratorTest {
+    private RandomValueGenerator randomLottoNumberGenerator;
 
     @BeforeEach
     void setUp() {
-        lottoGenerator = new RandomLottoNumberGenerator();
+        randomLottoNumberGenerator = new RandomLottoNumberGenerator();
     }
     @Test
     void 로또_번호_6개_생성_테스트() {
-        List<Integer> lottoNumbers = lottoGenerator.generate();
+        List<Integer> lottoNumbers = randomLottoNumberGenerator.generate();
         assertThat(lottoNumbers).hasSize(LottoConstants.LOTTO_NUMBER_COUNT);
     }
 
     @Test
     void 로또_번호_범위_테스트() {
-        List<Integer> lottoNumbers = lottoGenerator.generate();
+        List<Integer> lottoNumbers = randomLottoNumberGenerator.generate();
         assertThat(lottoNumbers).allMatch(number -> number >= LottoConstants.LOTTO_MIN_NUMBER && number <= LottoConstants.LOTTO_MAX_NUMBER);
     }
 
     @Test
     void 로또_번호_중복_테스트() {
-        List<Integer> lottoNumbers = lottoGenerator.generate();
+        List<Integer> lottoNumbers = randomLottoNumberGenerator.generate();
         Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
         assertThat(uniqueNumbers).hasSize(LottoConstants.LOTTO_NUMBER_COUNT);
     }
 
     @Test
     void 로또_번호_오름차순_정렬_테스트() {
-        List<Integer> lottoNumbers = lottoGenerator.generate();
+        List<Integer> lottoNumbers = randomLottoNumberGenerator.generate();
         List<Integer> sortedNumbers = new ArrayList<>(lottoNumbers);
         Collections.sort(sortedNumbers);
         assertThat(lottoNumbers).isEqualTo(sortedNumbers);
