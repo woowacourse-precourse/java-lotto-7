d9d4f2091ed02f59389113f5a64555324a6009ef
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 18:09:45 +0900

    feat(service): calculateMatchCount 메서드를 추가하여 일치하는 번호 개수 계산

--- src/main/java/lotto/service/LottoResults.java
+++ src/main/java/lotto/service/LottoResults.java
@@ -1,8 +1,13 @@
 package lotto.service;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.SequencedMap;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.LottoTickets;
 import lotto.domain.Rank;
+import lotto.domain.WinningNumbers;
 
 public class LottoResults {
     private final SequencedMap<Rank,Integer> lottoResults = new LinkedHashMap<>();
@@ -15,5 +20,17 @@ public class LottoResults {
         for (Rank rank : Rank.values()) {
             lottoResults.put(rank,0);
         }
+    }
+
+    private int calculateMatchCount(WinningNumbers winningNumbers, Lotto lotto) {
+        int matchCount = 0;
+        List<Integer> winningNumbersList = winningNumbers.getWinningNumbers();
+
+        for (Integer number : lotto.getNumbers()) {
+            if (winningNumbersList.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
     }
 }
