dc44e97fbcb4059cccf150a05c0f0c6d72321492
minseo <alstj778@gmail.com>
2024-11-03 01:23:27 +0900

    refactor(Prize): 가독성 개선을 위해 메세지 상수로 추출

--- src/main/java/lotto/constant/Prize.java
+++ src/main/java/lotto/constant/Prize.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum Prize {
-    FIFTH((matchedCount, hasBonus) -> matchedCount == 3, 5_000, "3개 일치 (5,000원) - %d개"),
-    FOURTH((matchedCount, hasBonus) -> matchedCount == 4, 50_000, "4개 일치 (50,000원) - %d개"),
-    THIRD((matchedCount, hasBonus) -> matchedCount == 5 && !hasBonus, 1_500_000, "5개 일치 (1,500,000원) - %d개"),
-    SECOND((matchedCount, hasBonus) -> matchedCount == 5 && hasBonus, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    FIRST((matchedCount, hasBonus) -> matchedCount == 6, 2_000_000_000, "6개 일치 (2,000,000,000원) - %d개"),
+    FIFTH((matchedCount, hasBonus) -> matchedCount == 3, 5_000, PrizeMessage.FIFTH_PRIZE_MESSAGE),
+    FOURTH((matchedCount, hasBonus) -> matchedCount == 4, 50_000, PrizeMessage.FOURTH_PRIZE_MESSAGE),
+    THIRD(
+            (matchedCount, hasBonus) -> matchedCount == 5 && !hasBonus,
+            1_500_000,
+            PrizeMessage.THIRD_PRIZE_MESSAGE
+    ),
+    SECOND(
+            (matchedCount, hasBonus) -> matchedCount == 5 && hasBonus,
+            30_000_000,
+            PrizeMessage.SECOND_PRIZE_MESSAGE
+    ),
+    FIRST((matchedCount, hasBonus) -> matchedCount == 6, 2_000_000_000, PrizeMessage.FIRST_PRIZE_MESSAGE),
     NON((matchedCount, hasBonus) -> true, 0, "");
 
     private final BiPredicate<Integer, Boolean> condition;
@@ -28,15 +36,15 @@ public enum Prize {
                 .orElse(NON);
     }
 
+    private boolean matches(final int count, final boolean hasBonus) {
+        return condition.test(count, hasBonus);
+    }
+
     public int getPrizeMoney() {
         return prizeMoney;
     }
 
     public String getResultMessage() {
         return resultMessage;
-    }
-
-    private boolean matches(final int count, final boolean hasBonus) {
-        return condition.test(count, hasBonus);
     }
 }
