6a60c1b522d6d4be80920d0d3825257f96e2b2f6
kungbi <kipper12335@gmail.com>
2024-11-02 12:33:07 +0900

    feat(Application): MVC 연동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.ConsoleInput;
+import lotto.view.ConsoleOutput;
+import lotto.view.InputProvider.ConsoleInputProvider;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleInput input = new ConsoleInput(new ConsoleInputProvider());
+        ConsoleOutput output = new ConsoleOutput();
+        LottoController controller = new LottoController(input, output);
+
+        try {
+            controller.run();
+        } finally {
+            input.close();
+        }
     }
 }
