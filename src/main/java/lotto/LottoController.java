1a5179f0cbde41acee52d666d722e1f8c2b12390
lsj0822 <lsj0822@khu.ac.kr>
2024-11-02 17:06:46 +0900

    feat(LottoController) : 로또 관리 기능 추가 및 수정
    로또 관리의 기능을 수정하였습니다.
    -기존의 경우 변수를 담고 이를 반환할 예정이었으나, 여기에서 변수까지 관리하는 것 보다는 상태를 관리할 모델을 만드는 것이 옳다고 판단하였기에 해당 클래스에서는 변수를 가공하는 과정만 포함하였습니다.
    - try-catch 구문과 재귀를 사용하여 IllegalArgumentException이 발생했을 시 다시 값을 입력받도록 변경하였습니다.
    - 다만 재귀를 호출하는 방법이 메모리 스택 관리 측면에서는 좋지 못한 방법이기 때문에 다른 방법을 고려하고 있습니다.

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,16 +1,40 @@
 package lotto;
 
+import java.util.List;
+import utils.Converter;
+import utils.Parser;
 import validator.Validator;
 import view.InputView;
+import view.OutputView;
 
 public class LottoController {
-    public final int LOTTO_COST = 1000;
-    long purchaseMoney, winningMoney = 0;
-    Lotto[] lottos;
-
 
     public void startProgram() {
-        purchaseMoney = InputView.inputPurchaseMoney();
-        Validator.validatePurchasing(purchaseMoney, LOTTO_COST);
+        inputMoneyController();
+        winningNumbersController();
+    }
+
+    private Long inputMoneyController() {
+        try {
+            String originalInput = InputView.inputPurchaseMoney();
+            Validator.validateMoney(originalInput);
+            return Long.parseLong(originalInput);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+        }
+        return inputMoneyController();
+    }
+
+    private List<Integer> winningNumbersController() {
+        try {
+            String originalInput = InputView.inputWinningNumbers();
+            List<String> parsedInput = Parser.parsing(originalInput);
+            List<Integer> numbers = Converter.convertStringToNumber(parsedInput);
+            Validator.validateLottoNumbers(numbers);
+            return numbers;
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+        }
+        return winningNumbersController();
     }
 }
--- src/main/java/view/OutputMessage.java
+++ src/main/java/view/OutputMessage.java
@@ -1,9 +1,9 @@
 package view;
 
 public enum OutputMessage {
-    
+
     INPUT_MONEY_MESSAGE("구입금액을 입력해 주세요."),
-    INPUT_LOTTO_COUNT_MESSAGE("개를 구매했습니다."), //앞에 구매 개수 붙여야함.
+    INPUT_LOTTO_COUNT_MESSAGE("%d개를 구매했습니다."),
     INPUT_WINNING_NUMBERS_MESSAGE("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_WINNING_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
     INPUT_WINNING_RESULT_MESSAGE("당첨 통계"),
@@ -15,7 +15,7 @@ public enum OutputMessage {
         this.message = message;
     }
 
-    public String getMessage(OutputMessage messageType) {
-        return messageType.message;
+    public String getMessage() {
+        return message;
     }
 }
