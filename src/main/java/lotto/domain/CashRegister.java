f85e54a24c48d7b5263c08feb69c1057ce451c07
rootTiket <l20078829@gmail.com>
2024-11-02 16:43:30 +0900

    feat: 금액이 1000원 단위가 아닐경우 예외처리

--- src/main/java/lotto/domain/CashRegister.java
+++ src/main/java/lotto/domain/CashRegister.java
@@ -1,16 +1,25 @@
 package lotto.domain;
 
+import static lotto.domain.constant.ErrorMessage.PRICE_UNMATCHED;
+
 public class CashRegister {
-    private final int money;
+    private int money;
     private final int SINGLE_LOTTO_PRICE = 1000;
 
     public CashRegister(int money) {
+        validate(money);
         this.money = money;
     }
 
 
     private int calculateLottoCount() {
         return this.money / SINGLE_LOTTO_PRICE;
+    }
+
+    private void validate(int money) {
+        if (money % SINGLE_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(PRICE_UNMATCHED.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/lotto/domain/constant/ErrorMessage.java
@@ -0,0 +1,15 @@
+package lotto.domain.constant;
+
+public enum ErrorMessage {
+    PRICE_UNMATCHED("돈은 1000원 단위로만 입력할 수 있습니다");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
