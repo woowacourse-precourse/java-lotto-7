c2ac1a749a8a01355e44e2a77dc4b31598bb9e9c
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-02 21:38:42 +0900

    feat: 당첨등수 enum으로 구현

---
+++ src/main/java/lotto/enums/Ranking.java
@@ -0,0 +1,39 @@
+package lotto.enums;
+
+public enum Ranking {
+    FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - "), // 1등
+    SECOND(7, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "), // 2등
+    THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - "), // 3등
+    FOURTH(4, 50_000, "4개 일치 (50,000원) - "), // 4등
+    FIFTH(3, 5_000, "3개 일치 (5,000원) - "), // 5등
+    MISS(0, 0, "");
+    Ranking(int countOfMatch, int winningAmount, String message) {
+        this.countOfMatch = countOfMatch;
+        this.winningAmount = winningAmount;
+        this.message = message;
+    }
+    private int countOfMatch;
+    private int winningAmount;
+    private String message;
+
+    public static Ranking valueOf(int countOfMatch){
+        for (Ranking rank : values()){
+            if (rank.getCountOfMatch() == countOfMatch) {
+                return rank;
+            }
+        }
+        return MISS;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
