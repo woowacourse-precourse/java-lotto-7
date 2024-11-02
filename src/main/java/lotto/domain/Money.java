65b883c91e3441060a1f1ba476f27ae1b1e04fad
softmoca <acyc0909@gmail.com>
2024-11-03 00:31:38 +0900

    feat : Money 객체에 [Error] 접미사 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -18,13 +18,13 @@ public class Money {
 
     private void validateMinimumAmount(long money) {
         if (money < MINIMUM_LOTTO_PRICE) {
-            throw new IllegalArgumentException("로또 구매는 1000원부터 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 구매는 1000원부터 가능합니다.");
         }
     }
 
     private void validateDivisibleByLottoPrice(long money) {
         if (money % LOTTO_PRICE_UNIT != 0) {
-            throw new IllegalArgumentException("로또 구매는 1000원 단위로 가능합니다");
+            throw new IllegalArgumentException("[ERROR] 로또 구매는 1000원 단위로 가능합니다");
         }
     }
 
