2be526d7405b7ace48dc3bbeee376bfdcf60717d
ufo9430 <ufo9430@naver.com>
2024-11-01 23:48:23 -0400

    refactor: InputView의 내부 검증 클래스 추출

---
+++ src/main/java/lotto/validate/Validator.java
@@ -0,0 +1,41 @@
+package lotto.validate;
+
+import lotto.exception.ExceptionMessage;
+import lotto.exception.InputException;
+
+import java.util.regex.Pattern;
+
+public class Validator{
+    public static String validateWinning(String input){
+        String[] splitNumber = input.split(",");
+        for (String number : splitNumber) {
+            isNotNumeric(number);
+        }
+        return input;
+    }
+    public static String validateAmount(String input){
+        isNotNumeric(input);
+        isMinus(input);
+        isBlank(input);
+        return input;
+    }
+
+    private static void isBlank(String input){
+        if(input.isEmpty()){
+            throw new InputException(ExceptionMessage.BLANK_INPUT_ERROR);
+        }
+    }
+
+    private static void isNotNumeric(String input) {
+        String regex = "^[0-9]*$";
+        if(!Pattern.matches(regex, input)){
+            throw new InputException(ExceptionMessage.INVALID_INPUT_ERROR);
+        }
+    }
+    private static void isMinus(String input){
+        int num = Integer.parseInt(input);
+        if(num < 0){
+            throw new InputException(ExceptionMessage.MINUS_PRICE_ERROR);
+        }
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,22 +5,24 @@ import lotto.exception.ExceptionMessage;
 import lotto.exception.InputException;
 import lotto.model.Amount;
 import lotto.model.Lotto;
+import lotto.validate.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import static lotto.console.ConsoleManager.*;
+import static lotto.validate.Validator.*;
 
 public class InputView {
     public Amount getAmount(){
         println("구입금액을 입력해 주세요.");
-        String amount = Validator.validateAmount(Console.readLine());
+        String amount = validateAmount(Console.readLine());
         return new Amount(Integer.parseInt(amount));
     }
     public Lotto getWinning(){
         println("당첨 번호를 입력해 주세요.");
-        String line = Validator.validateWinning(Console.readLine());
+        String line = validateWinning(Console.readLine());
         List<Integer> numbers = lineToNumbers(line);
         return new Lotto(numbers);
     }
@@ -34,38 +36,4 @@ public class InputView {
         return numbers;
     }
 
-    private static class Validator{
-        static String validateWinning(String input){
-            String[] splitNumber = input.split(",");
-            for (String number : splitNumber) {
-                isNotNumeric(number);
-            }
-            return input;
-        }
-        static String validateAmount(String input){
-            isNotNumeric(input);
-            isMinus(input);
-            isBlank(input);
-            return input;
-        }
-
-        private static void isBlank(String input){
-            if(input.isEmpty()){
-                throw new InputException(ExceptionMessage.BLANK_INPUT_ERROR);
-            }
-        }
-
-        private static void isNotNumeric(String input) {
-            String regex = "^[0-9]*$";
-            if(!Pattern.matches(regex, input)){
-                throw new InputException(ExceptionMessage.INVALID_INPUT_ERROR);
-            }
-        }
-        private static void isMinus(String input){
-            int num = Integer.parseInt(input);
-            if(num < 0){
-                throw new InputException(ExceptionMessage.MINUS_PRICE_ERROR);
-            }
-        }
-    }
 }
