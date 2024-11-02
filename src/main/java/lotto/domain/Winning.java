811abbd93cf85c8e3b824af18078e720cbaff8c7
jaeyeong <zzz26lk@gmail.com>
2024-11-02 17:54:32 +0900

    feat: 당첨 내역 추가
    당첨 내역에 따른 의미와 당첨금을 알기 위해서
    enum으로 당첨 내역을 작성하여 추가한다.

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+public enum Winning {
+    THREE("3개 일치 (5,000원)", 5_000L, 3),
+    FOUR("4개 일치 (50,000원)", 50_000L, 4),
+    FIVE("5개 일치 (1,500,000원)", 1_500_000L, 5),
+    FIVE_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000L, 10),
+    SIX("6개 일치 (2,000,000,000원)", 2_000_000_000L, 6);
+
+
+    private String mean;
+    private long price;
+    private int count;
+
+    Winning(String mean, long price, int count) {
+        this.mean = mean;
+        this.price = price;
+        this.count = count;
+    }
+
+    public String getMean() {
+        return mean;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
