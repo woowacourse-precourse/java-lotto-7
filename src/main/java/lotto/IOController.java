30bd3f58f85a28948f99f901ad03b9ffbaf3400a
chyo1 <hyowon3856@gmail.com>
2024-11-02 09:55:33 +0900

    refactor : 내부 상수 범위 private으로 변경

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -3,9 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class IOController {
-    public static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    public static String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    public static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     String inputPurchase() {
         System.out.println(INPUT_PURCHASE_MESSAGE);
