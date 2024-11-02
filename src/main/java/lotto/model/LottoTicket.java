36a171723f91cabc9eed00410cd2482aec5bc0e7
mingking2 <mingee641@gmail.com>
2024-11-02 16:13:52 +0900

    feat: 로또티켓 클래스 추가

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+import java.util.List;
+
+
+public class LottoTicket {
+    private List<Lotto> lottos;
+
+    public LottoTicket(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+}
