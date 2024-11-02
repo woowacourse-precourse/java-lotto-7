246dd2bef90496ec40ebedac8b39bec31bc07fd4
daxxn <dahyeonhan4529@gmail.com>
2024-11-03 02:25:20 +0900

    fix(service): correct rate of return calculation
    - Correct rate of return calculation formula in LottoService
    - Change methods from static to instance methods

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -11,28 +11,21 @@ import lotto.model.LottoResult;
 
 public class LottoService {
 
-    public static List<Integer> makeLottoNumber() {
+    public List<Integer> makeLottoNumber() {
         int LOTTO_NUMBER_COUNT = 6;
         int LOTTO_NUMBER_START = 1;
         int LOTTO_NUMBER_END = 45;
 
-        List<Integer> lottoNumbers = new ArrayList<Integer>();
-        while (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
-            int number = Randoms.pickNumberInRange(LOTTO_NUMBER_START, LOTTO_NUMBER_END);
-            if (!lottoNumbers.contains(number)) {
-                lottoNumbers.add(number);
-            }
-        }
-        return lottoNumbers;
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_START, LOTTO_NUMBER_END, LOTTO_NUMBER_COUNT);
     }
 
-    public static LottoGrade checkLottoGrade(Lotto lotto, Lotto winningLotto, int bonusNumber) {
+    public LottoGrade checkLottoGrade(Lotto lotto, Lotto winningLotto, int bonusNumber) {
         int correctCount = getCorrectNumberCount(lotto, winningLotto);
         boolean hasBonusNumber = lotto.hasNumber(bonusNumber);
         return LottoGrade.getGrade(correctCount, hasBonusNumber);
     }
 
-    public static int getCorrectNumberCount(Lotto lotto, Lotto winningLotto) {
+    public int getCorrectNumberCount(Lotto lotto, Lotto winningLotto) {
         List<Integer> winningLottoNumbers = new ArrayList<>(winningLotto.getNumbers());
         List<Integer> lottoNumbers = new ArrayList<>(lotto.getNumbers());
         int totalSize = winningLottoNumbers.size() + lottoNumbers.size();
@@ -41,15 +34,18 @@ public class LottoService {
         return totalSize - mergeLottoNumbers.size();
     }
 
-    public static double calculateRateOfReturn(int lottoValue, int lottoCost) {
-        double rateOfReturn = (double) (lottoValue - lottoCost) / lottoCost * 100;
+    public double calculateRateOfReturn(int lottoValue, int lottoCost) {
+        if (lottoCost == 0) {
+            return 0;
+        }
+        double rateOfReturn = ((double) lottoValue / lottoCost) * 100;
         return Math.round(rateOfReturn * 100.0) / 100.0;
     }
 
-    public static int sumLottoPrize(LottoResult lottoResult) {
+    public int sumLottoPrize(LottoResult lottoResult) {
         int lottoValue = 0;
         for (LottoGrade lottoGrade : LottoGrade.values()) {
-            if (lottoGrade == LottoGrade.SIXTH_GRADE || lottoGrade == LottoGrade.SEVENTH_GRADE) {
+            if (lottoGrade.getRanking() >= 6) {
                 continue;
             }
             lottoValue += (lottoGrade.getPrize() * lottoResult.getGradeCount(lottoGrade));
--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -16,18 +16,20 @@ public class LottoServiceTest {
     Lotto winningLotto;
     int bonusNumber;
     LottoResult lottoResult;
+    LottoService lottoService;
 
     @BeforeEach
     public void setUp() {
         winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         bonusNumber = 7;
         lottoResult = new LottoResult();
+        lottoService = new LottoService();
     }
 
     @DisplayName("1~45사이의 중복되지 않은 랜덤한 수 6개를 생성합니다.")
     @Test
     public void 로또_번호_생성기_테스트_입니다() {
-        assertDoesNotThrow(() -> new Lotto(LottoService.makeLottoNumber()));
+        assertDoesNotThrow(() -> new Lotto(lottoService.makeLottoNumber()));
     }
 
     @DisplayName("1등 로또의 당첨 결과를 반환하는 테스트입니다.")
@@ -35,7 +37,7 @@ public class LottoServiceTest {
     public void 로또_번호_당첨_1등_결과_테스트_입니다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
-        assertThat(LottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
+        assertThat(lottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
                 .isEqualTo(LottoGrade.FIRST_GRADE);
     }
 
@@ -44,7 +46,7 @@ public class LottoServiceTest {
     public void 로또_번호_당첨_2등_결과_테스트_입니다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
 
-        assertThat(LottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
+        assertThat(lottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
                 .isEqualTo(LottoGrade.SECOND_GRADE);
     }
 
@@ -53,7 +55,7 @@ public class LottoServiceTest {
     public void 로또_번호_당첨_3등_결과_테스트_입니다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 10));
 
-        assertThat(LottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
+        assertThat(lottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
                 .isEqualTo(LottoGrade.THIRD_GRADE);
     }
 
@@ -62,7 +64,7 @@ public class LottoServiceTest {
     public void 로또_번호_당첨_4등_결과_테스트_입니다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 10, 20));
 
-        assertThat(LottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
+        assertThat(lottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
                 .isEqualTo(LottoGrade.FOURTH_GRADE);
     }
 
@@ -71,7 +73,7 @@ public class LottoServiceTest {
     public void 로또_번호_당첨_5등_결과_테스트_입니다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 30, 10, 20));
 
-        assertThat(LottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
+        assertThat(lottoService.checkLottoGrade(lotto, winningLotto, bonusNumber))
                 .isEqualTo(LottoGrade.FIFTH_GRADE);
     }
 
@@ -81,17 +83,17 @@ public class LottoServiceTest {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 10));
         int expected = 5;
-        assertThat(LottoService.getCorrectNumberCount(lotto, winningLotto)).isEqualTo(expected);
+        assertThat(lottoService.getCorrectNumberCount(lotto, winningLotto)).isEqualTo(expected);
     }
 
     @DisplayName("수익률 계산이 올바르게 수행되는지 테스트합니다.")
     @Test
     public void 수익률_계산_테스트_입니다() {
-        double rateOfReturn = LottoService.calculateRateOfReturn(200000, 100000);
-        assertThat(rateOfReturn).isEqualTo(100.0);
+        double rateOfReturn = lottoService.calculateRateOfReturn(200000, 100000);
+        assertThat(rateOfReturn).isEqualTo(200.0);
 
-        rateOfReturn = LottoService.calculateRateOfReturn(150000, 100000);
-        assertThat(rateOfReturn).isEqualTo(50.0);
+        rateOfReturn = lottoService.calculateRateOfReturn(150000, 100000);
+        assertThat(rateOfReturn).isEqualTo(150.0);
     }
 
     @DisplayName("로또 당첨금 총합 계산이 올바르게 수행되는지 테스트합니다.")
@@ -103,7 +105,7 @@ public class LottoServiceTest {
         lottoResult.addGrade(LottoGrade.FOURTH_GRADE);
         lottoResult.addGrade(LottoGrade.FIFTH_GRADE);
 
-        int totalPrize = LottoService.sumLottoPrize(lottoResult);
+        int totalPrize = lottoService.sumLottoPrize(lottoResult);
         assertThat(totalPrize).isEqualTo(
                 LottoGrade.FIRST_GRADE.getPrize() +
                         LottoGrade.SECOND_GRADE.getPrize() +
