0da7ea8a53f6243b7458b118bdbafd081d275797
이윤빈 <kanado20000@naver.com>
2024-11-02 17:05:05 +0900

    :art: refactor: 출력을 위해 불변 리스트를 변경가능한 리스트로 복사하는 로직 추가

--- src/main/java/lotto/utilities/Sorter.java
+++ src/main/java/lotto/utilities/Sorter.java
@@ -1,11 +1,13 @@
 package lotto.utilities;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Sorter {
   public static List<Integer> inAscendingOrder(List<Integer> listOfLottoNumbers) {
-    Collections.sort(listOfLottoNumbers);
-    return listOfLottoNumbers;
+    List<Integer> mutableList = new ArrayList<>(listOfLottoNumbers);
+    Collections.sort(mutableList);
+    return mutableList;
   }
 }
