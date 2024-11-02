0e4d66d1d62a9ded63f02a6f624bcb985e829673
bk <naturalbb360@gmail.com>
2024-11-03 04:12:01 +0900

    refactor(LottoSeller): LottoSeller 클래스 리팩토링 - 숫자 검증 메소드 삭제 - 클래스변수 lottoNumber 삭제 - 로또의 개수를 계산하는 메소드 추가 - 로또를 발행하는 메소드명 수정

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -1,69 +1,31 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoSeller {
+
     private int cash;
-    private int lottoNumber = 0;
-    private List<Lotto> customerLottos = new ArrayList<Lotto>();
+    private int lottoPrice;
 
-    LottoSeller
-    public void initLottoNumber(){
-        System.out.println(GameMessage.GET_INPUT_MESSAGE.getMessage());
-        cashValidate(getInput());
-        buyLotto();
-        System.out.println(printLotto());
+    public LottoSeller(int cash, int lottoPrice) {
+        this.cash = cash;
+        this.lottoPrice = lottoPrice;
     }
 
-    private static String getInput(){
-        return Console.readLine();
-    }
-
-    protected void cashValidate(String input){
-        isNumber(input);
-        isDivisibleByThousand();
-    }
-
-    protected void isNumber(String input){
-        if(input == null || input.isBlank() || input.isEmpty()){
-            throw new IllegalArgumentException(ErrorMessage.INPUT_IS_EMPTY.getMessage());
+    protected List<Lotto> sell() {
+        int lottoNumbers = getLottoCount();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoNumbers; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(randomNumbers));
         }
-        if(!input.matches("[\\d]+")){
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_POSITIVE_NUMBER.getMessage());
-        }
-        cash = Integer.parseInt(input);
+        return lottos;
     }
 
-    protected void isDivisibleByThousand(){
-        final int LOTTO_PRICE = 1000;
-        if(cash == 0 || cash % LOTTO_PRICE > 0 ){
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_DIVISIBLE_BY_THOUSAND.getMessage());
-        }
-        lottoNumber = (cash / LOTTO_PRICE);
-    }
 
-    protected void buyLotto(){
-        List<Integer> customerLotto;
-        for(int i=0; i< lottoNumber; i++){
-            customerLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            customerLottos.add(new Lotto(customerLotto));
-        }
+    public int getLottoCount() {
+        return cash / Lotto.PRICE;
     }
-
-    protected String printLotto(){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(lottoNumber + GameMessage.GET_LOTTO_MESSAGE.getMessage());
-        for(Lotto curlotto: customerLottos){
-            stringBuilder.append(curlotto.getLottoNumber());
-        }
-        return stringBuilder.toString();
-    }
-
-    protected int getLottoNumber(){
-        return lottoNumber;
-    }
-
 }
