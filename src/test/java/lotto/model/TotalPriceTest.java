2cd08fe540aa4a3764623590351dcb5a38ec1622
이윤빈 <kanado20000@naver.com>
2024-11-02 13:46:00 +0900

    :sparkles: feat: 총상금을 관리하는 클래스 TotalPrice 클래스를 sumAllPrice calculateReturnRate 메서드를 위한 테스트 코드 추가

---
+++ src/test/java/lotto/model/TotalPriceTest.java
@@ -0,0 +1,30 @@
+package lotto.model;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import lotto.enums.WinningType;
+
+public class TotalPriceTest {
+
+    @Test
+    void 투자_금액과_총_상금에_따라_수익률을_계산한다() {
+        List<WinningType> winningStatistic = Arrays.asList(
+            WinningType.FIFTH_PLACE 
+        );
+        TotalPrice totalPrice = TotalPrice.sumAllPrice(winningStatistic);
+        Money investmentMoney = new Money("8000");
+
+
+        double expectedReturnRate = 62.5;
+        double actualReturnRate = totalPrice.calculateReturnRate(investmentMoney);
+
+
+        assertThat(actualReturnRate).isEqualTo(expectedReturnRate);
+        System.out.println("결과:" + expectedReturnRate);
+    }
+   }
+
