bc45e0d997f61bc4201c318e2758a0f98a93e8ae
GaBaljaintheroom <junsu1222@naver.com>
2024-11-03 00:17:22 +0900

    feat(WinningNumbers): 당첨 번호 일급 컬렉션 생성

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+
+    private final List<Integer> values;
+
+    public WinningNumbers(List<Integer> values) {
+        this.values = values;
+    }
+}
