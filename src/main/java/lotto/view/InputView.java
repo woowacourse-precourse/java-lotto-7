22191bede682fe48c3ef6575e8deaa27e5ada79a
MinJun Choi <mj043000@naver.com>
2024-11-02 23:56:39 +0900

    feat: 사용자가 잘못된 값을 입력 시 재입력 요구 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,29 +11,41 @@ public class InputView {
     private final static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
 
     public int getLottoAmount() {
-        System.out.println(INPUT_MONEY_AMOUNT);
-        String money = Console.readLine();
-
-        InputValidator.validateMoney(money);
-
-        return Integer.parseInt(money);
+        while (true) {
+            try {
+                System.out.println(INPUT_MONEY_AMOUNT);
+                String money = Console.readLine();
+                InputValidator.validateMoney(money);
+                return Integer.parseInt(money);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public List<Integer> getWinningLottoNumber() {
-        System.out.println(NEW_LINE+ INPUT_WINNING_LOTTO_NUMBER);
-        String lottoNumber = Console.readLine();
-
-        InputValidator.validateLottoNumber(lottoNumber);
-
-        return InputValidator.makeLottoNumber(lottoNumber);
+        while (true) {
+            try {
+                System.out.println(NEW_LINE+ INPUT_WINNING_LOTTO_NUMBER);
+                String lottoNumber = Console.readLine();
+                InputValidator.validateLottoNumber(lottoNumber);
+                return InputValidator.makeLottoNumber(lottoNumber);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public int getBonusNumber() {
-        System.out.println(NEW_LINE+ INPUT_BONUS_NUMBER);
-        String bonusNumber = Console.readLine();
-
-        InputValidator.validateBonusNumber(bonusNumber);
-
-        return Integer.parseInt(bonusNumber);
+        while (true) {
+            try {
+                System.out.println(NEW_LINE+ INPUT_BONUS_NUMBER);
+                String bonusNumber = Console.readLine();
+                InputValidator.validateBonusNumber(bonusNumber);
+                return Integer.parseInt(bonusNumber);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
