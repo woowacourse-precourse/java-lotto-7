42e06cbae38edb91f26019d96b12142893e2603b
633jinn <judyg00dy68@gmail.com>
2024-11-02 13:03:39 +0900

    feat(Game): 로도 번호 범위 검증 기능
    로또 번호가 1이상 45이하인지 체크, 범위에 해당하지 않는다면 에러 발생

--- src/main/java/lotto/model/Game.java
+++ src/main/java/lotto/model/Game.java
@@ -21,15 +21,25 @@ public class Game {
         if (winningNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다");
         }
+
+        boolean flag = winningNumbers.stream().allMatch(this::validateRange);
+        if (!flag || !validateRange(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 번호는 1이상 45 이하의 숫자로 이루어져야 합니다.");
+        }
+
         HashSet<Integer> set = new HashSet<>(winningNumbers);
         if (set.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
         }
+
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
         }
     }
 
+    private boolean validateRange(Integer num) {
+        return 1 <= num && num <= 45;
+    }
 
     public Lottos getLottos() {
         return lottos;
