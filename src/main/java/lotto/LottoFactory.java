67a25c4acc44d6edd2cff05c435835e8009ca284
daeyeon <kdy4410121@naver.com>
2024-11-02 19:24:13 +0900

    feat: 의존성 주입 및 lotto 여러 개 만드는 함수 작성

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -1,14 +1,32 @@
 package lotto;
 
+import NumberGenerator.RandomNumber;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoFactory {
 
-    private LottoRandomNumber lottoRandomNumber = new LottoRandomNumber();
+    private final RandomNumber randomNumber;
+
+    public LottoFactory(RandomNumber randomNumber){
+        this.randomNumber = randomNumber;
+    }
 
     public Lotto createRandomLotto(){
-        List<Integer> numbers = lottoRandomNumber.randomLottoNumbers();
+        List<Integer> numbers = randomNumber.randomLottoNumbers();
         return new Lotto(numbers);
     }
+
+    public List<Lotto> createRandomLottos(int number){
+        List<Lotto> lottos = new ArrayList<>();
+
+        for(int i = 0 ; i < number; i++){
+            lottos.add(createRandomLotto());
+        }
+
+        return lottos;
+    }
+
 
 }
