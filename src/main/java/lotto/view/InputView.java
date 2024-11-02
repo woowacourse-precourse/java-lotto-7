ee12603bb4ef2f9cf906c10e9734cb09e6af1f0d
soohalee <suha630@naver.com>
2024-11-03 02:10:37 +0900

    refactor(InputView): 에러 출력양식 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -41,23 +41,23 @@ public class InputView {
 
     private static void validateWinningNumber(String userInput) {
         if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
         }
         String[] numbers = userInput.split(",");
         if (!Utils.allElementsAreDigits(numbers)) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
         }
         if (!Utils.checkSizeEqual(numbers, LottoEnum.WINNING_NUMBER_COUNT.getNumber())) {
             throw new IllegalArgumentException(
-                    ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage() + " : " + userInput);
+                    ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage());
         }
         List<BigDecimal> winningNumber = Arrays.stream(numbers).map(number -> new BigDecimal(number)).toList();
         if (!Utils.areAllNumbersValidRange(new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
                 new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()), winningNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getMessage());
         }
         if (!Utils.isDuplicateNumber(winningNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
@@ -71,14 +71,14 @@ public class InputView {
 
     private static void validateBonusNumberInput(String userInput, List<Integer> winningNumberData) {
         if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
         }
         if (!Utils.isDigitOnly(userInput)) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
         }
         BigDecimal count = Utils.stringToNumber(userInput);
         if (Utils.isNumberInList(winningNumberData, count.intValue())) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage() + " : " + userInput);
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
         }
         if (!Utils.isInRange(new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
                 new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()), count)) {
