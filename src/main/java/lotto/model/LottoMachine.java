e22823c816123aada19b472c4b62ce8db0c9229a
mingking2 <mingee641@gmail.com>
2024-11-02 16:18:29 +0900

    feat: 로또 구매를 위한 로또기계 클래스 추가

---
+++ src/main/java/lotto/model/LottoMachine.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.RandomUtil;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    public LottoTicket purchaseTicket(int price) {
+        int count = price / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i<count; i++) {
+            lottos.add(generateLotto());
+        }
+        return new LottoTicket(lottos);
+    }
+
+    private Lotto generateLotto() {
+        List<Integer> numbers = RandomUtil.generateRandomNumbers();
+        return new Lotto(numbers);
+    }
+}
