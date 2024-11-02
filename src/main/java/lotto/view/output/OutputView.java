cc054e2bb5568b960e688491cd3b79e840b6042a
dltmddus <tmddus4671@gmail.com>
2024-11-02 17:18:51 +0900

    feat: 입력 메세지 출력 메서드 view 구현

---
+++ src/main/java/lotto/view/output/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view.output;
+
+public class OutputView {
+    public static void printPurchasePrice() {
+        System.out.println(OutputMessage.INPUT_PURCHASE_PRICE);
+    }
+
+    public static void printWinningNumbers() {
+        System.out.println(OutputMessage.INPUT_WINNING_NUMBERS);
+    }
+
+    public static void printBonusNumber() {
+        System.out.println(OutputMessage.INPUT_BONUS_NUMBER);
+    }
+}
