bd43eb3960314bdec13aabcb90cb34d0b2369bf8
corpi <lj845854@gmail.com>
2024-11-02 17:14:34 +0900

    test(LottoTest): 당첨 번호가 6개가 아닐 경우 에러가 발생한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto;
 
+import java.util.stream.Stream;
 import lotto.model.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -27,14 +31,34 @@ class LottoTest {
     @Test
     void 로또를_생성할_때_번호는_오름차순으로_저장된다() {
         /* given */
-        Lotto lotto = new Lotto(List.of(10,9,8,7,6,5));
+        Lotto lotto = new Lotto(List.of(10, 9, 8, 7, 6, 5));
 
         /* when */
         List<Integer> numbers = lotto.getNumbers();
 
         /* then */
         for (int i = 1; i < numbers.size(); i++) {
-            assertThat(numbers.get(i-1) < numbers.get(i)).isTrue();
+            assertThat(numbers.get(i - 1) < numbers.get(i)).isTrue();
         }
+    }
+
+    @DisplayName("당첨 번호가 6개가 아닐 경우 에러가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidSizeWinningNumberList")
+    void 당첨_번호가_6개가_아닐_경우_에러가_발생한다(List<Integer> input) {
+        Assertions.assertThatThrownBy(() -> {
+            new Lotto(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<Integer>> invalidSizeWinningNumberList() {
+        return Stream.of(
+                List.of(),
+                List.of(1),
+                List.of(1, 2),
+                List.of(1, 2, 3),
+                List.of(1, 2, 3, 4),
+                List.of(1, 2, 3, 4, 5)
+        );
     }
 }
