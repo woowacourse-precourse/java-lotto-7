42b85ad686edf6c2ee834cf212350defbc569e84
DESKTOP-TMDOSVL\user <tlsekdn505@naver.com>
2024-11-02 13:24:10 +0900

    당첨번호 입력 메서드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,11 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         LottoGame game = new LottoGame();
@@ -9,5 +14,15 @@ public class Application {
         System.out.println("로또 구입 금액을 입력해주세요.");
         int amount = Integer.parseInt(Console.readLine());
         game.BuyLotto(amount);
+
+        System.out.println("당첨 번호를 입력해주세요.");
+        List<Integer> winningNumbers = Arrays.stream(Console.readLine().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println("보너스 번호를 입력해주세요.");
+        int bonusNumbers = Integer.parseInt(Console.readLine());
+
+        game.UpdateWinnings(winningNumbers, bonusNumbers);
     }
 }
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,10 +3,13 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
     private final List<Lotto> lottos = new ArrayList<>();
+    private Map<String, Integer> winnings = new HashMap<>();
 
     public void BuyLotto (int amount) {
         if (amount % 1000 != 0){
@@ -20,5 +23,26 @@ public class LottoGame {
         }
         System.out.println(quantity+ "개를 구매했습니다.");
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void UpdateWinnings (List<Integer> winningNumbers, int bonusNumber) {
+        for (Lotto lotto : lottos) {
+            int matchCount = (int) lotto.getNumbers().stream()
+                    .filter(winningNumbers::contains)
+                    .count();
+
+            if (matchCount == 6) {
+                winnings.put("6개 일치", winnings.getOrDefault("6개 일치", 0) + 1);
+            }
+            if (matchCount == 5 && lotto.getNumbers().contains(bonusNumber)) {
+                winnings.put("5개 일치, 보너스 볼 일치", winnings.getOrDefault("5개 일치, 보너스볼 일치", 0) + 1);
+            }
+            if (matchCount == 4) {
+                winnings.put("4개 일치", winnings.getOrDefault("4개 일치", 0) + 1);
+            }
+            if (matchCount == 3) {
+                winnings.put("3개 일치", winnings.getOrDefault("3개 일치", 0) + 1);
+            }
+        }
     }
 }
