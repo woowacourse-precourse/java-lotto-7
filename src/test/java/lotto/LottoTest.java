535580497b403b3869df466a16035e5193ac5f0b
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-02 19:54:17 +0900

    test : 로또 번호가 정해진 숫자 범위 내에 있는지 검증하는 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -18,6 +20,13 @@ class LottoTest {
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1~45 이외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_허동되는_범위_이외의_숫자가_있으면_예외가_발생한다(){
+        assertThatThrownBy(() -> new Lotto(List.of(1,3,6,8,13,53)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
