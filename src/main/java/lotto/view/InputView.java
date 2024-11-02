95483172a46a43c549a87cb33ff05438295a57eb
kingrangE <yuvw123@gmail.com>
2024-11-02 18:50:05 +0900

    feat(InputView): add input winningNumber method
    당첨 번호 입력 메서드를 추가하였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,24 +2,35 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.model.CustomException;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
     private final String INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final String INPUT_BONUS_NUMBER_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final InputView instance = new InputView();
     private final CustomException customException;
-    private InputView(){
+
+    private InputView() {
         customException = CustomException.getInstance();
     }
 
-    public static InputView getInstance(){
+    public static InputView getInstance() {
         return instance;
     }
 
-    public int inputPurchaseAmount(){
+    public int inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE);
         String price = Console.readLine();
         customException.purchaseAmountInputCheck(price);
         return Integer.parseInt(price);
+    }
+
+    public int[] inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_GUIDE_MESSAGE);
+        String winningNumber = Console.readLine();
+        customException.winningNumberInputCheck(winningNumber);
+        return Arrays.stream(winningNumber.split(",")).mapToInt(Integer::parseInt).toArray();
     }
 }
