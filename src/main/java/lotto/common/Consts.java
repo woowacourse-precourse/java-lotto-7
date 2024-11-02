47b4f8188ed3aa9f6f80d01e7d21ae91c2c850a1
bellmin <whdlas011203@naver.com>
2024-11-03 02:07:37 +0900

    feat(Consts): 에러 메세지 전역 상수화
    - INPUT_MUST_BE_NUMBER : 입력은 무조건 숫자
    - PURCHASE_AMOUNT_1000_UNIT : 입력 금액은 로또 가격으로 나누어 떨어져야 함

--- src/main/java/lotto/common/Consts.java
+++ src/main/java/lotto/common/Consts.java
@@ -4,6 +4,8 @@ public class Consts {
 
     public static final int LOTTO_PRICE = 1_000;
     public static final String ERROR_PREFIX = "[ERROR]";
-    
+
+    public static final String INPUT_MUST_BE_NUMBER = "금액은 숫자로 입력해야 합니다.";
+    public static final String PURCHASE_AMOUNT_1000_UNIT = String.format("금액은 %d원 단위로 입력해 주세요.", LOTTO_PRICE);
 
 }
