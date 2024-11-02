f42755ad0da0755fc9ccbeb4eb7133f785166cde
kaswhy <sxshx818@naver.com>
2024-11-02 21:42:33 +0900

    test(InputAmountTest): 구매 금액이 빈 값이면 예외 발생 테스트
    변경 후
    - @ParametrizedTest와 @NullAndEmptySource를 활용하여 빈 값으로 생성자 호출 시도했을 때 예외가 발생하면 테스트 성공

---
+++ src/test/java/lotto/model/InputAmountTest.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import lotto.constants.ErrorMessage;
+
+public class InputAmountTest {
+
+    private InputAmount inputAmount;
+
+    @DisplayName("입력값이 빈 값이면 예외를 발생시킨다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void throwExceptionIfInputAmountEmpty(String inputAmount) {
+        Throwable thrown = catchThrowable(() -> {
+            this.inputAmount = new InputAmount(inputAmount);
+        });
+
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INPUT_CAN_NOT_BE_BLANK.get());
+    }
+}
