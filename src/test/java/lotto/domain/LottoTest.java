44883b00e9fd53a4337959554fa7490476fa94b7
Hyun0828 <spdlqj4818@naver.com>
2024-11-02 21:12:55 +0900

    feat(LottoTest): 1개의 로또를 발행할 때, 중복된 숫자가 없고 숫자가 6개여야 한다

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoTest {
+
+    @DisplayName("1개의 로또를 발행할 때 중복된 숫자가 없고 번호가 6개가 여야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,", "1,1,2,3,4,5"})
+    void validateLotto(String numbers) {
+        // given
+        String delimiter = ",";
+        List<Integer> lottoNumbers = Arrays.stream(numbers.split(delimiter))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        // when
+
+        // then
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(lottoNumbers);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
