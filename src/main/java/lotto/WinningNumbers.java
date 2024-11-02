5599524eec6d864f0050ae6511a992324b1af0a2
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 03:20:56 +0900

    change(WinningNumbers): 당첨번호 랜덤생성 기능 제거
    - 유저가 입력하는 형식임

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,18 +1,10 @@
 package lotto;
 
-import static lotto.LottoConstants.NUMBER_COUNT;
-
 import java.util.List;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
-
-    public WinningNumbers() {
-        winningNumbers = RandomMaker.getRandomNumbers(NUMBER_COUNT + 1);
-        bonusNumber = winningNumbers.getLast();
-        winningNumbers.removeLast();
-    }
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
