4c3b7c5c8090c75880b0a877da175793fe215367
차왕준 <ckdhdkwns@gmail.com>
2024-11-02 10:13:58 +0900

    feat(domain): 로또 결과 관리 클래스 생성

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import lotto.util.LottoConstant;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class LottoResult {
+    private final Map<Rank, Integer> rankCount;
+    private final int purchaseAmount;
+    private final WinningLotto winningLotto;
+
+    public LottoResult(List<Lotto> lottos, WinningLotto winningLotto) {
+        this.rankCount = new HashMap<>();
+        this.purchaseAmount = lottos.size() * LottoConstant.LOTTO_PURCHASE_AMOUNT.getIntValue();
+        this.winningLotto = winningLotto;
+    }
+}
