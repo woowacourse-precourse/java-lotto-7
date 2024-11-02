c04ee8acfc03455c5de2973c91f1876c5a9dec9b
unluckyboy214 <janggoon0214@naver.com>
2024-11-03 01:56:42 +0900

    refactor: 로또 수량 로직 개선

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,22 +10,28 @@ import lotto.view.OutputView;
 public class LottoController {
 
     private static List<Lotto> lottoList;
+    private static int lottoPrice;
     private static int lottoCount;
     private static List<Integer> lotto = new ArrayList<>();
 
     public void start(){
-        lottoCount = InputView.inputPrice();
-        OutputView.printLottoCount(lottoCount);
+        lottoPrice = InputView.inputPrice();
+        OutputView.printLottoCount(lottoPrice);
 
+        lottoCount = makeLottoCount(lottoPrice);
         lottoList = makeLottoList(lottoCount);
     }
 
     public static List<Lotto> makeLottoList(int lottoCount){
         lottoList = new ArrayList<>();
-        for(int i=0;i<lottoCount/1000;i++){
+        for(int i=0;i<lottoCount;i++){
             lottoList.add(makeLotto());
         }
         return lottoList;
+    }
+
+    public static int makeLottoCount(int lottoPrice){
+        return lottoPrice /1000;
     }
 
     public static Lotto makeLotto(){
