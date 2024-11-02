7f16c373861ca43b8a772df7db8615427ff8dd8b
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 02:30:48 +0900

    fix(RandomMakerTest): 리터럴 대신 NUMBER_COUNT 사용
    - 셍성된 중복되지 않는 숫자들이 NUMBER_COUNT개인지 검증하는 로직에서 수정

--- src/test/java/lotto/RandomMakerTest.java
+++ src/test/java/lotto/RandomMakerTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static lotto.LottoConstants.NUMBER_COUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
@@ -12,10 +13,10 @@ public class RandomMakerTest {
     @DisplayName("RandomNumber 생성 테스트")
     @RepeatedTest(1000)
     void RandomNumber_생성_테스트() {
-        List<Integer> randomNumbers = RandomMaker.getRandomNumbers(6);
+        List<Integer> randomNumbers = RandomMaker.getRandomNumbers(NUMBER_COUNT);
         Set<Integer> randomUniqueNumbers = new HashSet<>(randomNumbers);
         assertThat(randomUniqueNumbers)
-                .hasSize(6)
+                .hasSize(NUMBER_COUNT)
                 .allMatch(num -> 1 <= num && num <= 45);
     }
 }
