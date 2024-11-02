5efb8d7eeb6bbd45560777cada787c3e2d293865
yaongmeow <minnie_00@naver.com>
2024-11-03 01:04:21 +0900

    feat(I/O): 구입 금액 입력
    - 금액 입력
    - 금액 검증 후 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String lottoPriceString = Console.readLine();
+        Integer lottoPrice;
+
+        try {
+            lottoPrice = Integer.parseInt(lottoPriceString);
+            if(lottoPrice <= 0 || lottoPrice%1000 != 0){
+                throw new IllegalArgumentException("로또 구입 금액은 1000원 단위로 입력해야 합니다.");
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("입력 형식이 잘못되었습니다.");
+        }
     }
 }
