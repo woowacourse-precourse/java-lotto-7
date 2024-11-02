85969c7c644b71e6d51b8c338cd86fec66f338c7
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-03 00:01:22 +0900

    feat(ValidateTest): 당첨번호 입력값 유효성 검사 메소드 테스트

--- src/test/java/lotto/ValidateTest.java
+++ src/test/java/lotto/ValidateTest.java
@@ -50,4 +50,19 @@ public class ValidateTest {
         assertFalse(Validation.isInRange1To45(List.of(0,2,3,4,5,6)));
         assertFalse(Validation.isInRange1To45(List.of(1,2,3,4,5,46)));
     }
+
+    @Test
+    void shouldReturnTrueWhenWinningNumberIsValid() {
+        assertTrue(Validation.isValidWinningNumber("1,2,3,4,5,6"));
+        assertTrue(Validation.isValidWinningNumber("41,42,43,64,85,126"));
+    }
+
+    @Test
+    void shouldReturnFalseWhenWinningNumberIsInvalid() {
+        assertFalse(Validation.isValidWinningNumber("1,2,a,4,5,6"));
+        assertFalse(Validation.isValidWinningNumber("41,42,43,64,85,"));
+        assertFalse(Validation.isValidWinningNumber(",42,43,64,85,1"));
+        assertFalse(Validation.isValidWinningNumber("41,42,,64,85,5"));
+        assertFalse(Validation.isValidWinningNumber("41,42,4,64,5"));
+    }
 }
