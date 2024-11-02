638c04971bde1d76b06c4183e444ab580e48d73f
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 02:50:16 +0900

    feat(OutputView): 로또 번호를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,24 @@
 package lotto.view;
 
+import lotto.domain.Lottos;
+
+import static java.util.stream.Collectors.joining;
+
 public class OutputView {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printLottoListPrompt(Lottos lottos) {
+        lottos.getLottos()
+                .forEach(lotto -> printMessage(formatLottoNumbers(lottos)));
+    }
+
+    private static String formatLottoNumbers(Lottos lotto) {
+        return lotto.getLottos().stream()
+                .map(String::valueOf)
+                .collect(joining(", ", "[", "]"));
     }
 
 }
