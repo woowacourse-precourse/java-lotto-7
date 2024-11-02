f3b625c6e33cf81aea5349c7a5f159043a17ecde
차왕준 <ckdhdkwns@gmail.com>
2024-11-03 01:52:19 +0900

    test(domain): 당첨 번호와 사용자 로또 번호 일치 개수 반환 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class WinningLottoTest {
     @DisplayName("보너스 번호가 범위 내에 있지 않으면 예외가 발생한다.")
@@ -14,4 +14,16 @@ public class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(Arrays.asList(1,2,3,4,5,6), 48))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호와 사용자 로또 번호를 비교하여 일치하는 개수를 반환한다.")
+    @Test
+    void 당첨_번호와_사용자_로또_번호를_비교하여_일치하는_개수를_반환한다() {
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+        Lotto userLotto = new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9));
+
+        int matchCount = winningLotto.countMatchNumbers(userLotto);
+
+        assertThat(matchCount).isEqualTo(3);
+    }
+
 }
