3c82a7af41372d09a515c2203de3275437e1f13a
DoomchitYJ <poj1004@inha.edu>
2024-11-02 21:20:38 +0900

    feat(NumbersComparator): 당첨 횟수를 확인하는 기능
    - int[] rank: 몇 등에 몇 번 당첨되었는지 저장하는 변수
    - updateRank(): matchCount와 bonusHit 변수를 이용하여 몇 등에 당첨되었는지 확인하고, rank 업데이트

--- src/main/java/lotto/domain/NumbersComparator.java
+++ src/main/java/lotto/domain/NumbersComparator.java
@@ -7,6 +7,17 @@ public class NumbersComparator {
 
     private static final int TOTAL_RANK_TYPES = 5;
 
+    private static final int FIRST_PLACE_MATCH_COUNT = 6;
+    private static final int SECOND_AND_THIRD_PLACE_MATCH_COUNT = 5;
+    private static final int FOURTH_PLACE_MATCH_COUNT = 4;
+    private static final int FIFTH_PLACE_MATCH_COUNT = 3;
+
+    private static final int FIRST_PLACE_INDEX = 0;
+    private static final int SECOND_PLACE_INDEX = 1;
+    private static final int THIRD_PLACE_INDEX = 2;
+    private static final int FOURTH_PLACE_INDEX = 3;
+    private static final int FIFTH_PLACE_INDEX = 4;
+
     private final List<Lotto> myLottos;
 
     private final WinningNumbers winningNumbers;
@@ -22,6 +33,8 @@ public class NumbersComparator {
             List<Integer> myNumbers = myLottos.get(i).getNumbers();
             int matchCount = countMatchedNumbers(myNumbers);
             boolean bonusHit = isBonusMatched(myNumbers);
+
+            updateRank(rank, matchCount, bonusHit);
         }
 
         return rank;
@@ -35,5 +48,19 @@ public class NumbersComparator {
 
     private boolean isBonusMatched(List<Integer> myNumbers) {
         return myNumbers.contains(winningNumbers.getBonusNumber());
+    }
+
+    private void updateRank(int[] rank, int matchCount, boolean bonusHit) {
+        if (matchCount == FIRST_PLACE_MATCH_COUNT) {
+            rank[FIRST_PLACE_INDEX]++;
+        } else if (matchCount == SECOND_AND_THIRD_PLACE_MATCH_COUNT && bonusHit) {
+            rank[SECOND_PLACE_INDEX]++;
+        } else if (matchCount == SECOND_AND_THIRD_PLACE_MATCH_COUNT) {
+            rank[THIRD_PLACE_INDEX]++;
+        } else if (matchCount == FOURTH_PLACE_MATCH_COUNT) {
+            rank[FOURTH_PLACE_INDEX]++;
+        } else if (matchCount == FIFTH_PLACE_MATCH_COUNT) {
+            rank[FIFTH_PLACE_INDEX]++;
+        }
     }
 }
