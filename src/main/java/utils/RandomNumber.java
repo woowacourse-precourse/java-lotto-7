93d9f162a7ac375160aab8bd808b45e5f44cc313
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-03 01:33:13 +0900

    refactor: 요구사항에 맞게 RandomNumber 생성을 pickNumbersInRange를 사용하도록 변경하였음

--- src/main/java/utils/RandomNumber.java
+++ src/main/java/utils/RandomNumber.java
@@ -2,8 +2,6 @@ package utils;
 
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class RandomNumber {
@@ -13,23 +11,12 @@ public class RandomNumber {
     private static final int LOTTO_LENGTH = 6;
 
     private RandomNumber() {
-
     }
 
     public static List<Integer> create() {
-        List<Integer> randomNumbers = new ArrayList<>();
-        while (randomNumbers.size() != LOTTO_LENGTH) {
-            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-            verifyDuplicateRandomNumber(randomNumbers, randomNumber);
-            Collections.sort(randomNumbers);
-        }
-        return Collections.unmodifiableList(randomNumbers);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER,
+                LOTTO_LENGTH);
+        return randomNumbers;
     }
 
-    private static void verifyDuplicateRandomNumber(List<Integer> randomNumbers, int randomNumber) {
-        if (randomNumbers.contains(randomNumber)) {
-            return;
-        }
-        randomNumbers.add(randomNumber);
-    }
 }
