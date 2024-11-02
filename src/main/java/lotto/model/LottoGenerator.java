4f5687a10d730c02564215e5666cba81c0caba52
Su-daa <subin.kk019@gmail.com>
2024-11-02 17:55:47 +0900

    feat: 로또 번호 발행 기능 구현

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+    private static Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
+}
