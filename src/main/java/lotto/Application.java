abbc34d43e1f8096dd9e25d1620b5941b45cba9b
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-11-02 20:04:46 +0900

    feat(Application): 구입 금액 입력 메서드 추가
    - 올바르지 않은 값을 입력하면 입력을 다시 받게 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,16 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        UserInput number = new UserInput();
+
+        while (true) {
+            try {
+                number.purchaseAmountInput();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
