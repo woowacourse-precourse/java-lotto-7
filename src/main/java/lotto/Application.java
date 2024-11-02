60384cd429c0db5de4643f149c845dfff87f2b1e
mintos00 <kimhm2170@gmail.com>
2024-11-02 22:55:27 +0900

    fix(inputMoney): change inputMoney method to return Long
    Change inputMoney method to return Long
    - modify inputMoney to return money(Lotto purchase amount) when validMoney is true
    - modify inputMoney to return inputMoney when validMoney is false

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,16 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        inputMoney();
+        Long money = inputMoney();
 
     }
 
-    public static void inputMoney() {
+    public static Long inputMoney() {
         System.out.println("구입금액을 입력해주세요.");
         Long money = Long.parseLong(Console.readLine());
-        if (!validMoney(money)) {
-            inputMoney();
+        if (validMoney(money)) {
+            return money;
         }
+        return inputMoney();
     }
 
     public static boolean validMoney(Long money) {
