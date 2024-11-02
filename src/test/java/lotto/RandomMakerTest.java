63ae6d0b9907f0b0db585886672edc7bae17c5a5
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 00:58:10 +0900

    fix(RandomMakerTest): RandomNumbers 생성 테스트 수정
    - RandomNumber -> RandomNumbers 이름 수정
    - 생성된 값을 Set에 담아서 값이 중복되지 않는지 확인
    - 테스트 1000회 반복해서 확인

--- src/test/java/lotto/RandomMakerTest.java
+++ src/test/java/lotto/RandomMakerTest.java
@@ -2,17 +2,19 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 public class RandomMakerTest {
     @DisplayName("RandomNumber 생성 테스트")
-    @Test
+    @RepeatedTest(1000)
     void RandomNumber_생성_테스트() {
         List<Integer> randomNumbers = RandomMaker.getRandomNumbers(6);
-
-        assertThat(randomNumbers)
+        Set<Integer> randomUniqueNumbers = new HashSet<>(randomNumbers);
+        assertThat(randomUniqueNumbers)
                 .hasSize(6)
                 .allMatch(num -> 1 <= num && num <= 45);
     }
