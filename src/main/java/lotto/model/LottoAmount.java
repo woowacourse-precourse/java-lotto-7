6d0dc5a434165cf56a38a280ba7e526f1e225834
Chaeyoung714 <202102760@hufs.ac.kr>
2024-11-02 10:34:29 +0900

    refactor(LottoAmount) : 값 변환과 값 검증 로직 분리
    before : validate() 안에서 검증과 값 변환 모두 진행
    after : transferToInteger에서 값 변환 후 validate에서 비즈니스로직 검증

--- src/main/java/lotto/model/LottoAmount.java
+++ src/main/java/lotto/model/LottoAmount.java
@@ -4,21 +4,24 @@ public class LottoAmount {
     private int lottoAmount;
 
     public LottoAmount(String purchaseAmountInput) {
-        int purchaseAmount = validate(purchaseAmountInput);
+        int purchaseAmount = transferToInteger(purchaseAmountInput);
+        validate(purchaseAmount);
         this.lottoAmount = calculateLottoAmount(purchaseAmount);
     }
 
-    private int validate(String purchaseAmountInput) {
-        //앞서 양의정수를 점검했는데, 또 해야할까?
+    private void validate(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000 단위의 금액을 입력해주세요.");
+        }
+    }
+
+    private int transferToInteger(String purchaseAmountInput) {
         try {
-            if (!purchaseAmountInput.endsWith("000")) {
-                throw new IllegalArgumentException("[ERROR] 1000 단위의 금액을 입력해주세요.");
-            }
+            //앞서 양의정수를 점검했는데, 또 해야할까? -> 보험용. 그러니 validate 안에 없어도 된다
             return Integer.parseInt(purchaseAmountInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 1000 단위의 금액을 입력해주세요.");
         }
-
     }
 
     private int calculateLottoAmount(int purchaseAmount) {
