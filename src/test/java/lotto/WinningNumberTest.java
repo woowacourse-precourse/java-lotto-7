cb1a2749cfb3542a537318dfb708ba5955d0a8ad
hongkikii <hyangmeeh@icloud.com>
2024-11-02 17:25:28 +0900

    test(WinningNumberTest): 예외 상황 [ERROR] 테스트 코드 추가

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -18,21 +18,24 @@ class WinningNumberTest {
     @Test
     void 당첨_번호가_6개_미만일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,5"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호가 6개를 초과할 경우 예외가 발생한다.")
     @Test
     void 당첨_번호가_6개를_초과할_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,5,6,7"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호에 중복된 숫자가 있을 경우 예외가 발생한다.")
     @Test
     void 당첨_번호에_중복된_숫자가_있을_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,6,6"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호의 숫자 범위는 1~45이다.")
@@ -45,11 +48,13 @@ class WinningNumberTest {
     @Test
     void 당첨_번호의_숫자가_1_미만일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from("-1,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
 
         assertThatThrownBy(() -> WinningNumber.from("0,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호의 숫자가 45를 초과할 경우 예외가 발생한다.")
@@ -59,7 +64,8 @@ class WinningNumberTest {
                 .isInstanceOf(IllegalArgumentException.class);
 
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,5,200000000000"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호는 쉼표(,)를 기준으로 구분한다.")
@@ -76,26 +82,36 @@ class WinningNumberTest {
     @Test
     void 쉼표_사이에_아무_숫자가_없을_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from(",1,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+
         assertThatThrownBy(() -> WinningNumber.from("1,,2,3,4,5,6"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,5,6,"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+
         assertThatThrownBy(() -> WinningNumber.from("1,2,3,4,5,6,,"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
     @DisplayName("당첨 번호에 숫자와 쉼표를 제외한 문자가 입력될 경우 예외가 발생한다.")
     @Test
     void 당첨_번호에_숫자와_쉼표를_제외한_문자가_입력될_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from("1, 2, 3, 4, 5, 45"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> WinningNumber.from("*,2,3,4,5,45"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> WinningNumber.from("1*2*3*4*5*45"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
 
     }
 
@@ -103,6 +119,7 @@ class WinningNumberTest {
     @Test
     void 당첨_번호의_null일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> WinningNumber.from(null))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 }
