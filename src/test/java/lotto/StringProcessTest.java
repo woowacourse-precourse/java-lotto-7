b738346fbf43511a1e6a04ae193c4b5b5b011e79
minzzun99 <skg06123@naver.com>
2024-11-02 16:56:54 +0900

    test(StringProcessor): null 값과 빈 값 입력 테스트, 문자열 정수 변환 테스트 추가
    - null 값과 빈 값이 입력될 경우 예외 발생을 확인하는 테스트 추가
    - 문자열 정수 변환 시에 양수가 아닌 숫자, int 범위 초과, 숫자가 아닌 문자가 입력될 경우 예외 발생 테스트 추가
    - 문자열 정수 변환 테스트 추가

--- src/test/java/lotto/StringProcessTest.java
+++ src/test/java/lotto/StringProcessTest.java
@@ -1,15 +1,53 @@
 package lotto;
 
+import static lotto.util.StringProcessor.integerConverter;
 import static lotto.util.StringProcessor.removeCommas;
+import static lotto.util.StringProcessor.validateInput;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringProcessTest {
     @Test
     @DisplayName("콤마_제거를_확인하는_테스트")
     void 콤마_제거를_확인하는_테스트() {
-        assertThat(removeCommas("1,000,000")).contains("1000000");
+        assertThat(removeCommas("1,000,000"))
+                .contains("1000000");
+    }
+
+    @ParameterizedTest
+    @DisplayName("Null값_빈_값_입력_예외테스트")
+    @MethodSource("inputs")
+    void Null값_빈값_입력_예외테스트(String input) {
+        assertThatThrownBy(() -> validateInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    static Stream<String> inputs() {
+        return Stream.of(null, "");
+    }
+
+    @ParameterizedTest
+    @DisplayName("문자열_정수_변환_예외테스트")
+    @ValueSource(strings = {"0", "2200000000", "test", "1000k"})
+    void 문자열_정수_변환_예외테스트(String input) {
+        assertThatThrownBy(() -> integerConverter(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("문자열_정수_변환_테스트")
+    void 문자열_정수_변환_테스트() {
+        assertThat(integerConverter("200000"))
+                .isEqualTo(200000);
     }
 }
