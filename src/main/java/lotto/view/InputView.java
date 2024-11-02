224ad34807f8456d05531642526696581419bef2
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-11-02 16:16:52 +0900

    feat : view.InputView 구입금액 입력로직 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int buyPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 유효한 숫자를 입력해 주세요.");
+            return buyPrice();
+        }
+    }
+}
