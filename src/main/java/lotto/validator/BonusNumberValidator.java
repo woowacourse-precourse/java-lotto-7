e7046299ec1cd46f0d4197deb97386493ebf4325
JiEung2 <181757@jnu.ac.kr>
2024-11-03 02:12:54 +0900

    feat(bonusNumberValidator): 보너스 넘버가 당첨 번호와 중복인지 검증하는 메서드 추가

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -7,11 +7,18 @@ import java.util.List;
 public class BonusNumberValidator {
     public static void validate(int bonusNumber, List<Integer> winningNumbers) {
         validateBonusNumberRange(bonusNumber);
+        validateBonusNumberDuplicate(bonusNumber, winningNumbers);
     }
 
     private static void validateBonusNumberRange(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException(BONUS_NUMBER_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private static void validateBonusNumberDuplicate(int bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATED.getMessage());
         }
     }
 }
