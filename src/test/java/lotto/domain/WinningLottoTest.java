8eb6e5551ddd067e0e417ae39778c5636e110909
FhRh <yiyop@naver.com>
2024-11-02 21:50:13 +0900

    test(WinningLotto) : 당첨번호와 보너스 번호가 중복되면 예외가 발생한다

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +14,25 @@ public class WinningLottoTest {
     @Test
     void 당첨번호와_보너스번호로_당첨로또를_생성한다() {
         //given
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 7;
 
         //when
-        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
 
         //then
         assertThat(winningLotto.getWinningLottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
         assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
+    }
+
+    @Test
+    void 당첨번호와_보너스번호가_중복되면_예외가_발생한다() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 6;
+
+        //when & then
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
