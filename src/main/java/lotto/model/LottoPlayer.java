8fcb0cd58774b9961b1d031f90ac6d799fdc3156
Ahnsukjoo <bos98@naver.com>
2024-11-03 01:13:43 +0900

    feat: 로또 구매자(LottoPlayer) 클래스 구현
    - 로또 번호 생성 기능 추가 (1-45 범위, 중복 없는 6개 숫자)
    - 지정된 금액만큼 로또 구매 기능 구현

--- src/main/java/lotto/model/LottoPlayer.java
+++ src/main/java/lotto/model/LottoPlayer.java
@@ -1,18 +1,26 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import lotto.Lotto;
 
 public class LottoPlayer {
 
-    private Lotto lotto;
-    private List<Lotto> AutoNumbers;
+    private List<Lotto> autoNumbers;
 
-    public List<Lotto> purchase(int number) {
-        return AutoNumbers;
+    public List<Lotto> purchase(int money) {
+        int number = money / 1000;
+        for (int i = 0; i < number; i++) {
+            autoNumbers.add(chooseRandomNumbers());
+        }
+        return autoNumbers;
     }
 
-    private Lotto chooseRandomNumbers(int number) {
-        return lotto;
+    private Lotto chooseRandomNumbers() {
+        List<Integer> numbers = randomNumbers();
+        return new Lotto(numbers);
+    }
+
+    private List<Integer> randomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
