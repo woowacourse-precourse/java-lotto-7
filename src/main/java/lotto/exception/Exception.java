7837b8cee67cd524518f6e0ba44599ebd7be7109
JiEung2 <181757@jnu.ac.kr>
2024-11-02 21:03:57 +0900

    feat(lottoAmountValidator): 구입 금액이 로또 금액 단위로 나누어 떨어지지 않을 때 예외 메시지 추가

--- src/main/java/lotto/exception/Exception.java
+++ src/main/java/lotto/exception/Exception.java
@@ -3,6 +3,7 @@ package lotto.exception;
 public enum Exception {
     ONLY_NUMERIC_INPUT_FOR_PURCHASE_AMOUNT("구입 금액은 숫자만 입력할 수 있습니다."),
     PURCHASE_AMOUNT_MUST_BE_POSITIVE("구입 금액은 0보다 커야 합니다."),
+    ONLY_DIVISIBLE_BY_LOTTO_PRICE("구입 금액은 1000원 단위로 나누어떨어져야 합니다.");
     ;
 
     private static final String PREFIX = "[ERROR] ";
