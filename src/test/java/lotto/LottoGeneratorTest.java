56de34e715c1fb60829756f21eaa7e214edd524a
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 19:58:47 +0900

    test(LottoGenerator): 테스트 함수 2개 추가
    1000원 이하 입력 불가, 1000원 단위로만 입력 두 가지 입력을 테스트하는 코드 추가

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,4 +1,21 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class LottoGeneratorTest {
+    LottoGenerator lottoGenerator = new LottoGenerator();
+
+    @Test
+    void 구입_금액이_1000원_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> lottoGenerator.getLottoCount(-1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> lottoGenerator.getLottoCount(-1000))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
