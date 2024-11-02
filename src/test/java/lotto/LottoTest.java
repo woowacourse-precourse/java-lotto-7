888942ad2f6b47788a1b919c8141f3f79726f0f2
howareyoo <newworld121@naver.com>
2024-11-02 23:28:54 +0900

    test: unit tests for LottoController methods
    getUserNumbers and splitNumber

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +24,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호의_입력_값_숫자_입력후_리스트_반환(){
+        String userInput = "1, 2, 3, 4, 5, 6";
+        LottoController lottoController = new LottoController();
+        List<Integer> userNumbers = lottoController.getUserNumbers(userInput);
+        assertThat(userNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 로또_번호의_입력_값_숫자가_아닌_입력값이_있으면_예외가_발생한다(){
+
+        String userInput = "1,e,f";
+        LottoController lottoController = new LottoController();
+
+        assertThatThrownBy(() -> lottoController.getUserNumbers(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 숫자여야 합니다.");
+    }
+
 }
