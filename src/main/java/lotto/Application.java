a106f42a4e2913edb7d7fa37edb7f8baa2108b5d
jin66 <dbf6609@naver.com>
2024-11-03 02:34:26 +0900

    feat : 금액 입력받기, 당첨번호 + 보너스번호 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 금액 입력받기
+        System.out.print("금액을 입력하세요: ");
+        int price = Integer.parseInt(Console.readLine());
+        System.out.println("입력한 금액: " + price);
+
+        // 당첨번호 6개 입력받기
+        System.out.print("당첨번호 6개를 입력하세요(,키로 구분됩니다)");
+        String inputNumbers = Console.readLine();
+        // 입력된 문자열을 공백으로 나누어 List<Integer>로 변환
+        List<Integer> winningNumbers = Arrays.stream(inputNumbers.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        System.out.printf("입력한 당첨번호: %s%n", winningNumbers);
+
+        // 보너스번호 입력받기
+        System.out.print("보너스 번호 1개를 입력하세요: ");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        System.out.println("입력한 보너스 번호: " + bonusNumber);
+
     }
 }
