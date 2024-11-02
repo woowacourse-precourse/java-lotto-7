a6b6619af93a68960a834c61838fb86a68fc0d1c
rootTiket <l20078829@gmail.com>
2024-11-03 00:06:38 +0900

    feat(Parser): 쉼표를 기준으로 입력값을 분할하는 기능 추가

--- src/main/java/lotto/domain/Parser.java
+++ src/main/java/lotto/domain/Parser.java
@@ -1,8 +1,17 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
     public int parseToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private List<String> parseComma(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toList();
     }
 
 }
