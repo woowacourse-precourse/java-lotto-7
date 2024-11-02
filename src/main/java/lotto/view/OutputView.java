deb641e6cf6c74fd41c75023210e3cf7a30c2989
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-02 18:44:40 +0900

    fix(view) : replace output to constant
    출력 메시지 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,29 +2,23 @@ package lotto.view;
 
 import java.util.List;
 import java.util.StringJoiner;
-import lotto.domain.Lotto;
+import lotto.util.LottoPrint;
 
 public class OutputView {
-    private static final String ASK_PAYMENT = "구입금액을 입력해 주세요.";
-    private static final String ALERT_BUYING_NUMBER = "개를 구매했습니다.";
-    private static final String ASK_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String ASK_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-
-
     public void askPayment() {
-        System.out.println(ASK_PAYMENT);
+        System.out.println(LottoPrint.ASK_PAYMENT.getMessage());
     }
 
     public void alertBuyingNumber(int buyingNumber) {
-        System.out.println(buyingNumber + ALERT_BUYING_NUMBER);
+        System.out.println(buyingNumber + LottoPrint.ALERT_BUYING_NUMBER.getMessage());
     }
 
     public void alertEarningRate(double earningRate) {
         System.out.println("총 수익률은 " + earningRate + "%입니다.");
     }
 
-    public void printSortedNumbers(List<List<Integer>> sortedNumbers){
-        for(List<Integer> numbers : sortedNumbers){
+    public void printSortedNumbers(List<List<Integer>> sortedNumbers) {
+        for (List<Integer> numbers : sortedNumbers) {
             printNumbers(numbers);
         }
     }
@@ -34,31 +28,29 @@ public class OutputView {
         for (Integer number : numbers) {
             lottoJoiner.add(String.valueOf(number));
         }
-        System.out.println(lottoJoiner) ;
+        System.out.println(lottoJoiner);
     }
 
 
     public void askWinningNumbers() {
-        System.out.println(ASK_WINNING_NUMBERS);
+        System.out.println(LottoPrint.ASK_WINNING_NUMBERS.getMessage());
     }
 
     public void askBonusNumber() {
-        System.out.println(ASK_BONUS_NUMBER);
+        System.out.println(LottoPrint.ASK_BONUS_NUMBER.getMessage());
     }
 
     public void alertStartStat() {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(LottoPrint.ALERT_START_STAT.getMessage());
     }
 
     // 등수 결과로 출력하는 함수
     public void printLottoResults(int[] countRank) {
-        // 출력
-        System.out.println("3개 일치 (5,000원) - " + countRank[5] + "개");
-        System.out.println("4개 일치 (50,000원) - " + countRank[4] + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + countRank[3] + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countRank[2] + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + countRank[1] + "개");
+        System.out.println(LottoPrint.STAT_PREFIX_5th.getMessage()+ countRank[5] + LottoPrint.STAT_SUFFIX.getMessage());
+        System.out.println(LottoPrint.STAT_PREFIX_4th.getMessage()+ countRank[4] + LottoPrint.STAT_SUFFIX.getMessage());
+        System.out.println(LottoPrint.STAT_PREFIX_3rd.getMessage()+ countRank[3] + LottoPrint.STAT_SUFFIX.getMessage());
+        System.out.println(LottoPrint.STAT_PREFIX_2nd.getMessage()+ countRank[2] + LottoPrint.STAT_SUFFIX.getMessage());
+        System.out.println(LottoPrint.STAT_PREFIX_1st.getMessage()+ countRank[1] + LottoPrint.STAT_SUFFIX.getMessage());
     }
 
 }
