fd7735eee3577dae866d3598509367e062ea26ba
soeunnPark <carol0724@naver.com>
2024-11-02 23:17:37 +0900

    feat(WinningLotto): make a winning lotto
    6개의 로또 번호와 1개의 보너스 번호를 포함한 당첨 로또를 만들었습니다.

---
+++ src/main/java/lotto/domain/lotto/WinningLotto.java
@@ -0,0 +1,20 @@
+package lotto.domain.lotto;
+
+public class WinningLotto {
+
+    private final Lotto winningLotto;
+    private final LottoNumber bonusNumber;
+
+    public WinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+}
