08c7dca1453ffe659598559ec6802229702df9dc
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 05:32:26 +0900

    feat : 입력 받은 개수 만큼의 로또를 생성, 검증 및 반환하는 기능 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,4 +1,32 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lottos {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(List<List<Integer>> numbers, int numberOfLottos) {
+        saveLottos(numbers);
+        validateLottoNumber(numberOfLottos);
+    }
+
+    private void saveLottos(List<List<Integer>> numbers) {
+        for (List<Integer> number : numbers) {
+            lottos.add(new Lotto(number));
+        }
+    }
+
+    public void validateLottoNumber(int numberOfLottos) {
+        if(lottos.size() != numberOfLottos) {
+            throw new IllegalArgumentException(String.format("[Error] 로또의 개수는 %d개여야 합니다.", numberOfLottos));
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+
 }
