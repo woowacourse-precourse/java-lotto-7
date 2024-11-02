41bc2c477eb6dcacdb0ea9bef1983505981d1685
minseo <alstj778@gmail.com>
2024-11-03 01:08:46 +0900

    refactor(Lotto): 메서드 파라미터를 불변(final)로 정의

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(final List<Integer> numbers) {
         validate(numbers);
         sort(numbers);
         this.numbers = numbers;
@@ -19,36 +19,36 @@ public class Lotto {
         return new Lotto((numbers));
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(final List<Integer> numbers) {
         validateSize(numbers);
         validateIsDuplicated(numbers);
         validateRange(numbers);
     }
 
-    private void validateSize(List<Integer> numbers) {
+    private void validateSize(final List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    private void validateIsDuplicated(List<Integer> numbers) {
+    private void validateIsDuplicated(final List<Integer> numbers) {
         HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (numbers.size() != uniqueNumbers.size()) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateRange(List<Integer> numbers) {
+    private void validateRange(final List<Integer> numbers) {
         if (numbers.stream().anyMatch(this::isInvalidRange)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private boolean isInvalidRange(int number) {
+    private boolean isInvalidRange(final int number) {
         return number < 1 || number > 45;
     }
 
-    private void sort(List<Integer> numbers) {
+    private void sort(final List<Integer> numbers) {
         numbers.sort(Comparator.naturalOrder());
     }
 
