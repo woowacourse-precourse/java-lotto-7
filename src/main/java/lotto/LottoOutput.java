314ccc265d18e1197e9dca16d6e85691c6422eeb
문고은 <>
2024-11-02 23:39:33 +0900

    feat(output) : 발행된 로또 번호를 출력하는 기능 추가

--- src/main/java/lotto/LottoOutput.java
+++ src/main/java/lotto/LottoOutput.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoOutput {
     private final static String PURCHASE_AMOUNT_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String PURCHASED_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
@@ -10,5 +12,11 @@ public class LottoOutput {
 
     public void displayPurchasedLottoCount(long purchasedLottoCount) {
         System.out.println(purchasedLottoCount + PURCHASED_LOTTO_COUNT_MESSAGE);
+    }
+
+    public void displayLottoNumbers(List<Lotto> lottoTickets) {
+        for (Lotto lotto : lottoTickets) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
