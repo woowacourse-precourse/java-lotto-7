2ae10509755d8d988212792c975941e7d815b35b
Jewan1120 <hawaii1468@gmail.com>
2024-11-03 01:47:34 +0900

    feat(domain): Lotto 객체 출력 시 번호가 오름차순으로 정렬되도록 toString() 오버라이드
    Lotto 객체의 번호를 오름차순으로 출력하기 위해 toString() 메서드를 오버라이드했습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -14,6 +14,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.stream().sorted().toList().toString();
     }
 
     // TODO: 추가 기능 구현
