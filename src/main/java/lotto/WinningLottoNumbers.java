4b27c0e2daac290c13a12fba46e60fbc674cc55c
yybmion <yyb400@naver.com>
2024-11-02 22:35:41 +0900

    test: 당첨 번호 검증 테스트(당첨 번호 중복)

--- src/main/java/lotto/WinningLottoNumbers.java
+++ src/main/java/lotto/WinningLottoNumbers.java
@@ -17,6 +17,7 @@ public class WinningLottoNumbers {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateNumbersRange(numbers);
+        validateDuplicate(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -29,6 +30,9 @@ public class WinningLottoNumbers {
         if (numbers.stream().anyMatch(this::isInvalidRange)) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
     }
 
     private boolean isInvalidRange(int number) {
--- src/test/java/lotto/WinningLottoNumbersTest.java
+++ src/test/java/lotto/WinningLottoNumbersTest.java
@@ -32,4 +32,16 @@ public class WinningLottoNumbersTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
+
+    @Test
+    @DisplayName("당첨 번호는 중복될 수 없다.")
+    void 실패_당첨번호_중복() {
+        // given
+        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5, 5);
+
+        // when & then
+        assertThatThrownBy(() -> new WinningLottoNumbers(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨 번호는 중복될 수 없습니다.");
+    }
 }
