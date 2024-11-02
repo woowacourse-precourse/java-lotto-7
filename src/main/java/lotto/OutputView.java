bd93c34fc5ea31cc2358b6f2fe322eadbf0972d7
je488 <pje488@naver.com>
2024-11-02 18:41:46 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 함수 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
     public static final String LOTTO_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
     public static final String LOTTO_WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
     public static final String LOTTO_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    public static final String CONFIRM_PURCHASE_TEXT = "개를 구매했습니다.";
 
     public void printLottoAmountPrompt() {
         System.out.println(LOTTO_AMOUNT_PROMPT);
@@ -17,6 +19,17 @@ public class OutputView {
 
     public void printLottoBonusNumberPrompt() {
         System.out.println(LOTTO_BONUS_NUMBER_PROMPT);
+    }
+
+    public void printLottoAmount(int lottoAmount) {
+        System.out.println();
+        System.out.println(lottoAmount + CONFIRM_PURCHASE_TEXT);
+    }
+
+    public void printUserLottoNumber(List<List<Integer>> userLottoNumbers) {
+        for (List<Integer> userLottoNumber : userLottoNumbers) {
+            System.out.println(userLottoNumber);
+        }
     }
 
     public void printLottoResult(Map<Rank, Integer> lottoPrizeCount) {
