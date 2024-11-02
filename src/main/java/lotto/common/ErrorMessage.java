2064f883649c2b7ddfc328ff499d00c7b552aca1
hmlssghm <kwonse326@gmail.com>
2024-11-02 17:37:01 +0900

    feat(ErrorMessage): 에러 출력 enum 생성

---
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -0,0 +1,14 @@
+package lotto.common;
+
+public enum ErrorMessage {
+    EMPTY_INPUT("[ERROR] 입력이 비어있습니다.");
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+//    public String getMessage() {
+//        return message;
+//    }
+}
