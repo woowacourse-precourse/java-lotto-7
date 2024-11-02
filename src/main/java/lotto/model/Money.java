a466d24c4c787c754e4b1b1c1e20866f059fd9d8
Su-daa <subin.kk019@gmail.com>
2024-11-02 16:49:16 +0900

    feat: 생성자 및 금액 조건 검증 구현

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -2,4 +2,18 @@ package lotto.model;
 
 public class Money {
     private long money;
+
+    public Money(long money) {
+        validateAmount(money);
+        this.money = money;
+    }
+
+    private void validateAmount(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 0보다 커야 합니다.");
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
 }
