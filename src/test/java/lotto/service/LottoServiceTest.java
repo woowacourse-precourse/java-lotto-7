9351084f2ea9ce0b287ac180d3f3f003ae4b514f
201912025 <michi2012@naver.com>
2024-11-02 17:00:39 +0900

    test: 로또 생성 테스트 기능 구현

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,12 +1,16 @@
 package lotto.service;
 
+import lotto.domain.Lotto.Lotto;
 import lotto.domain.Lotto.LottoGenerator;
 import lotto.domain.Lotto.LottoManager;
 import lotto.domain.LottoFormatter;
 import lotto.domain.WinningLotto.WinningLottoManager;
+import lotto.dto.WinningLottoResultDTO;
 import lotto.validator.LottoValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,5 +37,42 @@ public class LottoServiceTest {
         int lottoCount = lottoService.calculateBuyLottoCount(5000);
 
         assertThat(lottoCount).isEqualTo(5);
+    }
+
+    @Test
+    void 로또_생성_및_저장_확인() {
+        int buyLottoCount = 3;
+        lottoService.createLottos(buyLottoCount);
+
+        List<Lotto> createdLottos = lottoManager.getLottos();
+        assertThat(createdLottos.size()).isEqualTo(buyLottoCount);
+
+        for (Lotto lotto : createdLottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            assertThat(numbers.size()).isEqualTo(6);
+            assertThat(numbers).doesNotHaveDuplicates();
+            for (int number : numbers) {
+                assertThat(number).isBetween(1, 45);
+            }
+        }
+    }
+
+    @Test
+    void 당첨_로또_결과_포맷_확인() {
+        winningLottoManager.recordWinningLotto(6, false);
+        winningLottoManager.recordWinningLotto(6, false);
+
+        List<WinningLottoResultDTO> resultDTOs = lottoService.formatWinningLottoResults();
+
+        assertThat(resultDTOs).isNotEmpty();
+
+        for (WinningLottoResultDTO dto : resultDTOs) {
+            if (dto.getMatchedCount() == 6) {
+                assertThat(dto.getCount()).isEqualTo(2);
+                assertThat(dto.getPrize()).isEqualTo(2000000000);
+            } else {
+                assertThat(dto.getCount()).isEqualTo(0);
+            }
+        }
     }
 }
