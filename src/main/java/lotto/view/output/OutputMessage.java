ebb1da26a7438adaba7b501840c936eae0d6d3a2
dltmddus <tmddus4671@gmail.com>
2024-11-02 16:15:45 +0900

    feat: 출력 메세지 enum 클래스 구현

---
+++ src/main/java/lotto/view/output/OutputMessage.java
@@ -0,0 +1,16 @@
+package lotto.view.output;
+
+public enum OutputMessage {
+    INPUT_PURCHASE_PRICE("구입금액을 입력해 주세요."),
+    PURCHASED_NUMBER("%d개를 구매했습니다."),
+    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("당첨 통계"),
+    MATCH_COUNT("%d개 일치");
+    final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+}
