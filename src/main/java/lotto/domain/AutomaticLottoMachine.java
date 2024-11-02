7618e598f24caf5e640b69080e4c9acf5df82719
J0YERIM <chosarah70@gmail.com>
2024-11-03 03:32:52 +0900

    feat(domain): AutomaticLottoMachine 구현체 추가
    자동으로 로또 번호를 생성하는 AutomaticLottoMachine 클래스를 구현
    로또 번호는 1~45 사이의 중복되지 않는 6개의 숫자로 생성되며,
    LottoTickets 객체를 반환하도록 구성됨

---
+++ src/main/java/lotto/domain/AutomaticLottoMachine.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.Lotto;
+
+public class AutomaticLottoMachine implements LottoMachine {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    @Override
+    public LottoTickets generateLottoTickets(int ticketCount) {
+        List<Lotto> lottoTickets = IntStream.range(0, ticketCount)
+                .mapToObj(i -> createLotto())
+                .toList();
+        return new LottoTickets(lottoTickets);
+    }
+
+    private Lotto createLotto() {
+        return new Lotto(generateLottoNumbers());
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE);
+    }
+}
