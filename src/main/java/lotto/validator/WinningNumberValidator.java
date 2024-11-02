d3dca05ba98c4863585676f724950cc59f2b5b3e
33jyu33 <jy04169@gmail.com>
2024-11-02 16:12:12 +0900

    feat(WinningNumberValidator): check the duplication in the list
    Function validateDuplication:
    - Create object of set
    - Compare the sizes of set and list

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -4,7 +4,9 @@ import lotto.constant.CompareInteger;
 import lotto.constant.WinningNumberRule;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumberValidator {
 
@@ -15,6 +17,7 @@ public class WinningNumberValidator {
     }
 
     public static List<Integer> validateInputNumber(List<String> input) throws IllegalArgumentException {
+        validateDuplication(input);
         List<Integer> winningNumber = new ArrayList<>();
         for (String separatedInput : input) {
             validateOnlyInteger(separatedInput);
@@ -43,13 +46,20 @@ public class WinningNumberValidator {
         }
     }
 
-    private static void validateOnlyInteger(String input) {
+    private static void validateDuplication(List<String> input) throws IllegalArgumentException {
+        Set<String> deduplication = new HashSet<>(input);
+        if (deduplication.size() != input.size()) {
+            throw new IllegalArgumentException(WinningNumberRule.DUPLICATION.getMessage());
+        }
+    }
+
+    private static void validateOnlyInteger(String input) throws IllegalArgumentException {
         if (!input.matches(WinningNumberRule.MATCH_NUMBER.getMessage())) {
             throw new IllegalArgumentException(WinningNumberRule.ONLY_COMMA_INTEGER.getMessage());
         }
     }
 
-    private static void validateNumberScope(Integer number) {
+    private static void validateNumberScope(Integer number) throws IllegalArgumentException {
         NumberValidator.validateScope(CompareInteger.LOTTO_NUMBER_MINIMUM.getNumber(), CompareInteger.LOTTO_NUMBER_MAXIMUM.getNumber(), number, WinningNumberRule.SCOPE.getMessage());
     }
 }
