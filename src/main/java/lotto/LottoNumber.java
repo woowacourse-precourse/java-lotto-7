68af90ffe7a36261e7fe9f0816b1e75b28a44124
pakaheo <k70923@konkuk.ac.kr>
2024-11-02 11:09:30 +0900

    refactor : Make numbers dont't go out of range
    Lotto numbers should be in range 1~45
    Create LottoNumber object
    LottoNumber check this range itself

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,2 +1,39 @@
-package lotto;public class LottoNumber {
+package lotto;
+
+import constants.ErrorMessage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+
+    private static final int LOWER_BOUND = 1;
+    private static final int UPPER_BOUND = 45;
+    private static final List<LottoNumber> LOTTO_NUMBERS = new ArrayList<>();
+
+    static {
+        IntStream.range(LOWER_BOUND, UPPER_BOUND + 1)
+                .forEach(number -> LOTTO_NUMBERS.add(new LottoNumber(number)));
+    }
+
+    private final int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber from(final int number) {
+        validateRange(number);
+        return LOTTO_NUMBERS.stream()
+                .filter(lottoNumber -> Objects.equals(lottoNumber, LOTTO_NUMBERS.get(number - 1)))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private static void validateRange(int number) {
+        if (number < LOWER_BOUND || number > UPPER_BOUND) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE);
+        }
+    }
 }
--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,2 +1,27 @@
-package lotto;public class LottoNumberTest {
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import constants.ErrorMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottoNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+            27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45})
+    void LottoNumber_생성(int number) {
+        LottoNumber lottoNumber = LottoNumber.from(number);
+        assertThat(lottoNumber).isEqualTo(LottoNumber.from(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 숫자가_1에서_45_범위를_벗어나면_예외(int number) {
+        assertThatThrownBy(() -> LottoNumber.from(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_NUMBER_RANGE);
+    }
 }
