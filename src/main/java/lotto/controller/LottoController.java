53988e224820b9c8a9baf4163baca3bf5e7f95e7
Ahnsukjoo <bos98@naver.com>
2024-11-03 00:32:04 +0900

    feat: LottoController 기능 구현
    - 프로그램의 절차에 따라 진행되도록 구성하였습니다.
    - run() 메서드의 주요 기능들을 구현하였습니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,35 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoNumberStatistics;
+import lotto.model.LottoPlayer;
+import lotto.view.input.LottoWinningNumber;
+import lotto.view.input.PurchaseAmount;
+import lotto.view.output.LottoPurchaseListPrinter;
+import lotto.view.output.LottoWinningStatsPrinter;
+
 public class LottoController {
 
-    public void run() {
+    private final PurchaseAmount purchaseAmount = new PurchaseAmount();
+    private final LottoWinningNumber winningNumber = new LottoWinningNumber();
+    private final LottoPurchaseListPrinter purchaseListPrinter = new LottoPurchaseListPrinter();
+    private final LottoWinningStatsPrinter winningStatsPrinter = new LottoWinningStatsPrinter();
+    private final LottoNumberStatistics lottoNumberStatistics = new LottoNumberStatistics();
+    private final LottoPlayer lottoPlayer = new LottoPlayer();
+    private HashMap<Integer, String> winnerNumbers = new HashMap<>();
+    private HashMap<Integer, Integer> winningResults = new HashMap<>();
 
+    List<Lotto> lotteryTickets = new ArrayList<>();
+
+    public void run() {
+        int money = purchaseAmount.input();
+        lotteryTickets = lottoPlayer.purchase(money);
+        purchaseListPrinter.output(lotteryTickets);
+        winnerNumbers = winningNumber.input();
+        winningResults = lottoNumberStatistics.checkWinner(lotteryTickets, winnerNumbers);
+        winningStatsPrinter.output(winningResults);
     }
 }
