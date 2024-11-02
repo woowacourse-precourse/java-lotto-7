91c85eadb13291320d2b99a4d6cfc2e0024223bf
Jungwoo Chu <cjw980930@gmail.com>
2024-11-02 21:55:37 +0900

    feat: 보너스 번호 입력 받기
    당첨번호에 같은 숫자가 존재할 경우 오류 발생 후 재입력 받기

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -7,45 +7,64 @@ import java.util.List;
 
 public class InputHandler {
     public int priceInput() {
-        while(true) {
+        while (true) {
             try {
                 int price = Integer.parseInt(Console.readLine());
                 priceInputValidator(price);
                 return price;
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
-    public void priceInputValidator(int price){
+
+    public void priceInputValidator(int price) {
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000단위만 가능합니다.");
         }
     }
 
-    public List<Integer> winningNumbersInput(){
-        while(true){
-            try{
+    public List<Integer> winningNumbersInput() {
+        while (true) {
+            try {
                 List<Integer> winningNumbers = new ArrayList<>();
                 List<String> numbersInString = Arrays.asList(Console.readLine().split(","));
-                for (String number : numbersInString){
+                for (String number : numbersInString) {
                     winningNumbers.add(Integer.parseInt(number));
                 }
                 winningNumbersInputValidator(winningNumbers);
                 return winningNumbers;
-            } catch(IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public void winningNumbersInputValidator(List<Integer> winningNumbers){
+    public void winningNumbersInputValidator(List<Integer> winningNumbers) {
         List<Integer> check = new ArrayList<>();
-        for (int number : winningNumbers){
-            if (check.contains(number)){
+        for (int number : winningNumbers) {
+            if (check.contains(number)) {
                 throw new IllegalArgumentException("[ERROR] 당첨번호에 중복된 숫자가 존재합니다.");
             }
             check.add(number);
+        }
+    }
+
+    public int bonusNumberInput(List<Integer> winningNumbers) {
+        while (true) {
+            try {
+                int bonusNumber = Integer.parseInt(Console.readLine());
+                bonusNumberValidator(winningNumbers, bonusNumber);
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void bonusNumberValidator(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호에 보너스번호와 중복된 숫자가 존재합니다.")
         }
     }
 
