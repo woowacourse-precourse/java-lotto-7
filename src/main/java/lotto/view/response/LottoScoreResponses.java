7352e81ad6e61858bdaa0df5e9690b3dbe71462c
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 22:42:36 +0900

    refactor($LottoScoreResponses): Map<Score, Integer> 를 매개변수로 받아 초기화되도록 변경

--- src/main/java/lotto/view/response/LottoScoreResponses.java
+++ src/main/java/lotto/view/response/LottoScoreResponses.java
@@ -1,17 +1,21 @@
 package lotto.view.response;
 
+import lotto.model.Score;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class LottoScoreResponses {
 
     private final Map<LottoScoreResponse, Integer> lottoScoreResponses;
 
-    private LottoScoreResponses(Map<LottoScoreResponse, Integer> lottoScoreResponses) {
-        this.lottoScoreResponses = lottoScoreResponses;
+    private LottoScoreResponses(Map<Score, Integer> scores) {
+        this.lottoScoreResponses = new HashMap<>();
+        scores.forEach((score, count) -> lottoScoreResponses.put(LottoScoreResponse.from(score), count));
     }
 
-    public static LottoScoreResponses from(Map<LottoScoreResponse, Integer> lottoScoreResponses) {
-        return new LottoScoreResponses(lottoScoreResponses);
+    public static LottoScoreResponses from(Map<Score, Integer> scores) {
+        return new LottoScoreResponses(scores);
     }
 
     public Map<LottoScoreResponse, Integer> getLottoScoreResponses() {
