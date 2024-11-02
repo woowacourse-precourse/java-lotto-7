3af52aa20fea2bf9fea6374527ef44d8c7dc815c
jiffyin7 <jiffyin7@gmail.com>
2024-11-02 16:46:06 +0900

    feat(MatchResult): 당첨 금액 출력 순서 보장하기 위해 LinkedHashMap으로 수정

--- src/main/java/lotto/dto/MatchResults.java
+++ src/main/java/lotto/dto/MatchResults.java
@@ -2,8 +2,7 @@ package lotto.dto;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EnumMap;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import lotto.service.lotto.constant.LottoConstant;
@@ -18,7 +17,7 @@ public class MatchResults {
   private final Map<MatchResult, Integer> prize;
   private Map<MatchResult, Integer> dashboard;
 
-  private static final MatchResult[] SORTED_MATCH_RESULTS = {
+  public static final MatchResult[] SORTED_MATCH_RESULTS = {
       MatchResult.from(3, MatchBonusEnum.DONT_CARE),
       MatchResult.from(4, MatchBonusEnum.DONT_CARE),
       MatchResult.from(5, MatchBonusEnum.NOT_MATCHED),
@@ -32,21 +31,18 @@ public class MatchResults {
     this.dashboard = initializeDashboardMap();
   }
 
-  public static MatchResults from(MatchResults matchResults) {
-  }
-
   private Map<MatchResult, Integer> initializePrizeMap() {
-    Map<MatchResult, Integer> prizeMap = new EnumMap<>(MatchResult.class);
+    var prizeMap = new LinkedHashMap<MatchResult, Integer>();
     prizeMap.put(MatchResult.from(3, MatchBonusEnum.DONT_CARE), LottoConstant.MATCH_THREE_PRIZE);
     prizeMap.put(MatchResult.from(4, MatchBonusEnum.DONT_CARE), LottoConstant.MATCH_FOUR_PRIZE);
     prizeMap.put(MatchResult.from(5, MatchBonusEnum.NOT_MATCHED), LottoConstant.MATCH_FIVE_PRIZE);
-    prizeMap.put(MatchResult.from(5, MatchBonusEnum.MATCHED), LottoConstant.MATCH_FIVE_PRIZE);
+    prizeMap.put(MatchResult.from(5, MatchBonusEnum.MATCHED), LottoConstant.MATCH_FIVE_BONUS_PRIZE);
     prizeMap.put(MatchResult.from(6, MatchBonusEnum.DONT_CARE), LottoConstant.MATCH_SIX_PRIZE);
     return Collections.unmodifiableMap(prizeMap);
   }
 
   private Map<MatchResult, Integer> initializeDashboardMap() {
-    Map<MatchResult, Integer> dashboard = new EnumMap<>(MatchResult.class);
+    var dashboard = new LinkedHashMap<MatchResult, Integer>();
     for (MatchResult result : SORTED_MATCH_RESULTS) {
       dashboard.put(result, 0);
     }
