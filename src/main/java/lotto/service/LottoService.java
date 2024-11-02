4c95228696c26065118c18945a3b7903efbb31b6
daxxn <dahyeonhan4529@gmail.com>
2024-11-02 15:26:26 +0900

    feat(service): update methods in lotto service
    - Implement calculateRateOfReturn method to calculate rate of return
    - Implement sumLottoPrize method to sum of prize

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,9 +2,12 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.model.Lotto;
 import lotto.model.LottoGrade;
+import lotto.model.LottoResult;
 
 public class LottoService {
 
@@ -24,9 +27,31 @@ public class LottoService {
     }
 
     public static LottoGrade checkLottoGrade(Lotto lotto, Lotto winningLotto, int bonusNumber) {
-        int correctCount = lotto.getCorrectNumberCount(winningLotto);
+        int correctCount = getCorrectNumberCount(lotto, winningLotto);
         boolean hasBonusNumber = lotto.hasNumber(bonusNumber);
         return LottoGrade.getGrade(correctCount, hasBonusNumber);
+    }
+
+    public static int getCorrectNumberCount(Lotto lotto, Lotto winningLotto) {
+        List<Integer> winningLottoNumbers = new ArrayList<>(winningLotto.getNumbers());
+        List<Integer> lottoNumbers = new ArrayList<>(lotto.getNumbers());
+        int totalSize = winningLottoNumbers.size() + lottoNumbers.size();
+        winningLottoNumbers.addAll(lottoNumbers);
+        Set<Integer> mergeLottoNumbers = new HashSet<>(winningLottoNumbers);
+        return totalSize - mergeLottoNumbers.size();
+    }
+
+    public static double calculateRateOfReturn(int lottoValue, int lottoCost) {
+        double rateOfReturn = (double) (lottoValue - lottoCost) / lottoCost * 100;
+        return Math.round(rateOfReturn * 100.0) / 100.0;
+    }
+
+    public static int sumLottoPrize(LottoResult lottoResult) {
+        int lottoValue = 0;
+        for (LottoGrade lottoGrade : LottoGrade.values()) {
+            lottoValue += lottoGrade.getPrize() * lottoResult.getGradeCount(lottoGrade);
+        }
+        return lottoValue;
     }
 
 }
