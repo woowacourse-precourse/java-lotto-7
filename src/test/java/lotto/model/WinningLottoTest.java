1355779789fd165f8b0c446830fe1629fa0778d1
Hanjjong <tim4974@naver.com>
2024-11-03 02:38:28 +0900

    test: WinningLotto 객체 생성 시 예외 테스트 추가

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -3,10 +3,13 @@ package lotto.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningLottoTest {
 
@@ -22,4 +25,13 @@ public class WinningLottoTest {
     void createWinningLottoTest() {
         assertThat(winningLotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "1,2,3", "1,2,3,4,5,abc", "1,1,2,3,4,5"})
+    @DisplayName("WinningLotto 생성 시 예외 테스트")
+    void createWinningLotto_invalidInput(String input) {
+        assertThatThrownBy(() -> WinningLotto.create(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
