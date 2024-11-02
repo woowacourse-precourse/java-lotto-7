3662d72a2151728d2759348acbf713139ebd94fb
mintos00 <kimhm2170@gmail.com>
2024-11-02 23:08:03 +0900

    feat(availableLotto): add method to output the number of lotto available for purchase

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         Long money = inputMoney();
+        Long countLotto = availableLotto(money);
 
     }
 
@@ -19,5 +20,11 @@ public class Application {
 
     public static boolean validMoney(Long money) {
         return money % 1000 == 0;
+    }
+
+    public static Long availableLotto(Long money) {
+        Long countLotto = money / 1000;
+        System.out.println(countLotto + "개를 구매했습니다.");
+        return countLotto;
     }
 }
