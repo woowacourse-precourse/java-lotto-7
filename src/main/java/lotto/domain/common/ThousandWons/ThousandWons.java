409899aaee72e2b8abb37132d6ffcefd227cae2c
plan11plan <tjtfy7028@gmail.com>
2024-11-02 15:12:05 +0900

    fix: ThousnadWons 생성 시 재귀 호출 버그 수정

--- src/main/java/lotto/domain/common/ThousandWons/ThousandWons.java
+++ src/main/java/lotto/domain/common/ThousandWons/ThousandWons.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain.common.ThousandWons;
 
 import static lotto.util.StringConverter.toInt;
 
@@ -11,8 +11,10 @@ public class ThousandWons {
     }
 
     public static ThousandWons of(String money) {
-        validate(toInt(money));
-        return ThousandWons.of(money);
+        int value = toInt(money);
+        validate(value);
+
+        return new ThousandWons(value);
     }
 
     public boolean isMuchThanOrEqual(int target) {
@@ -28,7 +30,7 @@ public class ThousandWons {
     }
 
     private static void validate(int value) {
-        if (value % 1000 != 0) {
+        if (value == 0 || value % 1000 != 0) {
             throw new IllegalArgumentException("1000원 단위로 입력 가능 합니다.");
         }
     }
