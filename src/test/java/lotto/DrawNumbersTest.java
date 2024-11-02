4da76cf64787f8835e4ca4d13bda6d615a513fcc
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 22:05:28 +0900

    test(draw number): 추첨 번호 테스트 추가

---
+++ src/test/java/lotto/DrawNumbersTest.java
@@ -0,0 +1,123 @@
+package lotto;
+
+import lotto.model.lotto_result.DrawNumbersBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class DrawNumbersTest {
+
+    @ParameterizedTest
+    @MethodSource("provideDuplicate")
+    void duplicateNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideExceedRange")
+    void ExceedRangeNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNonNumber")
+    void NonNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInsufficient")
+    void InsufficientNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideExcess")
+    void ExcessNumberCountTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWrongFormat")
+    void WrongFormatNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideEmpty")
+    void EmptyNumberTest(String winningNumbers, String bonusNumber) {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                buildLottoResult(winningNumbers, bonusNumber));
+    }
+
+    static Stream<Arguments> provideDuplicate() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,5",     "6"),   // 당첨 번호 중복
+                makeArguments("1,2,3,4,5,6",     "6")    // 보너스 번호 중복
+        );
+    }
+
+    static Stream<Arguments> provideExceedRange() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,46",    "6"),   // 당첨 번호 범위 초과
+                makeArguments("-1,2,3,4,5,6",    "7"),   // 당첨 번호 범위 초과
+                makeArguments("1,2,3,4,5,6",     "-1"),  // 보너스 번호 범위 초과
+                makeArguments("1,2,3,4,5,6",     "46")   // 보너스 번호 범위 초과
+        );
+    }
+
+    static Stream<Arguments> provideNonNumber() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,n",     "7"),   // 당첨 번호 문자 입력
+                makeArguments("1,2,3,4,5,6",     "n")    // 보너스 번호 문자 입력
+        );
+    }
+
+    static Stream<Arguments> provideInsufficient() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,",      "6")    // 당첨 번호 개수 부족
+        );
+    }
+
+    static Stream<Arguments> provideExcess() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,6,7", "8")    // 당첨 번호 개수 초과
+        );
+    }
+
+    static Stream<Arguments> provideWrongFormat() {
+        return Stream.of(
+                makeArguments("1,2,3,4,5,6,",    "7"),     // 당첨 번호 잘못된 형식
+                makeArguments("1,2,3,4,5,  6",    "7"),    // 당첨 번호 잘못된 형식
+                makeArguments("1,2,3,4,5,6",     "7,"),    // 보너스 번호 잘못된 형식
+                makeArguments("1,2,3,4,5,6",     "7   ")   // 보너스 번호 잘못된 형식
+        );
+    }
+
+    static Stream<Arguments> provideEmpty() {
+        return Stream.of(
+                makeArguments("",                "7"),   // 당첨 번호 빈 형식
+                makeArguments("1,2,3,4,5,6",     "")     // 보너스 번호 빈 형식
+        );
+    }
+
+    private static void buildLottoResult(String winningNumbers, String bonusNumber) {
+        new DrawNumbersBuilder()
+                .winningNumbers(winningNumbers)
+                .bonusNumber(bonusNumber)
+                .build();
+    }
+
+    private static Arguments makeArguments(String winningNumber, String bonusNumber) {
+        return Arguments.of(winningNumber, bonusNumber);
+    }
+}
