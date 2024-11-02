e7793fc8d87a98f593ce8b4e42d9eda631fe09d6
kaswhy <sxshx818@naver.com>
2024-11-02 23:54:08 +0900

    feat(lotto): 로또 구입 금액이 문자를 포함하면 예외 처리하는 기능 구현
    변경 후
    - InputAmount에서 해당 기능 구현
    - ErrorMessage에 관련 오류 메시지 등록
    - README 업데이트

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,7 +1,9 @@
 package lotto.constants;
 
 public enum ErrorMessage {
-    INPUT_CAN_NOT_BE_BLANK("빈 값은 입력할 수 없습니다.");
+    INPUT_CAN_NOT_BE_BLANK("빈 값은 입력할 수 없습니다."),
+
+    AMOUNT_CAN_NOT_HAVE_CHARACTER("숫자 이외의 문자를 포함할 수 없습니다.");
 
     private static final String header = "[ERROR] ";
     private final String errorMessage;
--- src/main/java/lotto/model/InputAmount.java
+++ src/main/java/lotto/model/InputAmount.java
@@ -5,12 +5,13 @@ import lotto.constants.ErrorMessage;
 
 public class InputAmount {
 
-    private static final Pattern ONLY_NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
+    private static final Pattern HAS_CHARACTER_PATTERN = Pattern.compile("[^0-9]");
 
     private int inputAmount;
 
     public InputAmount(String inputAmount) {
         validateIsBlank(inputAmount);
+        validateHasCharacter(inputAmount);
     }
 
     private void validateIsBlank(String inputAmount) {
@@ -19,4 +20,9 @@ public class InputAmount {
         }
     }
 
+    private void validateHasCharacter(String inputAmount) {
+        if (HAS_CHARACTER_PATTERN.matcher(inputAmount).find()) {
+            throw new IllegalArgumentException(ErrorMessage.AMOUNT_CAN_NOT_HAVE_CHARACTER.get());
+        }
+    }
 }
