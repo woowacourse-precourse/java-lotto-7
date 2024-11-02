05220c1112d2712edd41aae006ea0b4745132b80
FhRh <yiyop@naver.com>
2024-11-02 22:13:36 +0900

    feat(LottoCreateService) : 랜덤로또 n개를 생성한다

---
+++ src/main/java/lotto/service/LottoCreateService.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
+public class LottoCreateService {
+
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
+    public Lottos createLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = createRandomNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+        return new Lottos(lottos);
+    }
+
+    public List<Integer> createRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX,
+            LOTTO_NUMBER_COUNT);
+    }
+
+}
---
+++ src/test/java/lotto/service/LottoCreateServiceTest.java
@@ -0,0 +1,41 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.domain.Lottos;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("LottoCreateService 테스트")
+public class LottoCreateServiceTest {
+
+    private LottoCreateService lottoCreateService;
+
+    @BeforeEach
+    void setUp() {
+        lottoCreateService = new LottoCreateService();
+    }
+
+    @Test
+    void 랜덤번호_6개를_생성한다() {
+        //when
+        List<Integer> randomNumbers = lottoCreateService.createRandomNumbers();
+
+        //then
+        assertThat(randomNumbers).hasSize(6);
+    }
+
+    @Test
+    void 랜덤로또_n개를_생성한다() {
+        //given
+        int lottoCount = 5;
+
+        //when
+        Lottos myLottos = lottoCreateService.createLottos(lottoCount);
+
+        //then
+        assertThat(myLottos.getLottos()).hasSize(lottoCount);
+    }
+}
