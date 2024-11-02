e223cf11860c886470e4a5ede25688219cd40f07
chaerish <ccyy8432@naver.com>
2024-11-02 19:31:20 +0900

    feat(WinningCondition): 당첨 통계를 출력하기 위한 toString 메서드 생성 및 message 필드 추가

--- src/main/java/lotto/constant/WinningCondition.java
+++ src/main/java/lotto/constant/WinningCondition.java
@@ -1,11 +1,23 @@
 package lotto.constant;
 
 public enum WinningCondition {
-    MATCH_3(3, false, 5_000),
-    MATCH_4(4, false, 50_000),
-    MATCH_5(5, false, 1_500_000),
-    MATCH_5_BONUS(5, true, 30_000_000),
-    MATCH_6(6, false, 2_000_000_000);
+    NO_MATCH(0, false, 0, ""),
+    MATCH_3(3, false, 5_000, "3개 일치"),
+    MATCH_4(4, false, 50_000, "4개 일치"),
+    MATCH_5(5, false, 1_500_000, "5개 일치"),
+    MATCH_5_BONUS(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    MATCH_6(6, false, 2_000_000_000, "6개 일치");
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prizeAmount;
+    private final String message;
+
+    WinningCondition(int matchCount, boolean bonusMatch, int prizeAmount, String message) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prizeAmount = prizeAmount;
+        this.message = message;
+    }
 
     public int getMatchCount() {
         return matchCount;
@@ -19,19 +31,17 @@ public enum WinningCondition {
         return prizeAmount;
     }
 
-    public String PrizeAmountToString() {
-        String amount = String.valueOf(prizeAmount);
-        amount.replace("_", ",");
-        return amount;
+    @Override
+    public String toString() {
+        return message + " " + prizeAmountToString() + "-" + " ";
     }
 
-    private final int matchCount;
-    private final boolean bonusMatch;
-    private final int prizeAmount;
-
-    WinningCondition(int matchCount, boolean bonusMatch, int prizeAmount) {
-        this.matchCount = matchCount;
-        this.bonusMatch = bonusMatch;
-        this.prizeAmount = prizeAmount;
+    public String prizeAmountToString() {
+        StringBuilder printAmount = new StringBuilder();
+        printAmount.append("(");
+        String amount = String.valueOf(prizeAmount).replace("_", ",");
+        printAmount.append(amount).append("원").append(")").append(" ");
+        return printAmount.toString();
     }
+
 }
