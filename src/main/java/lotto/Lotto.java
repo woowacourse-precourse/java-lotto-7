a2153b5535305c8a92ac6507b202ff364c915aa0
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 15:37:34 +0900

    feat(lotto): 중복 번호 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import lotto.message.LottoErrorMessage;
+
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> numbers; // numbers 이외의 필드 추가 불가능, private 변경 불가능
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,9 +14,12 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LottoErrorMessage.INVALID_NUMBERS_COUNT.getMessage());
+        }
+
+        if (numbers.stream().distinct().count() < numbers.size()) {
+            throw new IllegalArgumentException(LottoErrorMessage.DUPLICATE_NUMBERS.getMessage());
         }
     }
-
     // TODO: 추가 기능 구현
 }
