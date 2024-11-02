583dcf9bd93b1e8240bc40e65f1b2a4d27d17a24
JiEung2 <181757@jnu.ac.kr>
2024-11-03 00:04:45 +0900

    feat(lottoNumberValidator): 로또 번호가 1~45 사이인지 검증하는 메서드 추가

--- src/main/java/lotto/validator/LottoNumbersValidator.java
+++ src/main/java/lotto/validator/LottoNumbersValidator.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import static lotto.exception.Exception.LOTTO_NUMBER_OUT_OF_RANGE;
 import static lotto.exception.Exception.LOTTO_NUMBER_SIZE_MUST_BE_SIX;
 
 import java.util.List;
@@ -13,6 +14,13 @@ public class LottoNumbersValidator {
     private static void validateLottoNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(LOTTO_NUMBER_SIZE_MUST_BE_SIX.getMessage());
+        }
+    }
+
+    private static void validateLottoNumberRange(List<Integer> numbers) {
+        boolean isValid = numbers.stream().allMatch(number -> number >= 1 && number <= 45);
+        if (!isValid) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
         }
     }
 }
