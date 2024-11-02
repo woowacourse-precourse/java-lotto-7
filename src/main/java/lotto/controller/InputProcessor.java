8bf2d538adf4b77fa26153af05cd50c9c11fd535
Jiihyun <cwlgusc@gmail.com>
2024-11-03 00:57:12 +0900

    refactor: 컨트롤러 추상화 - 파싱 및 분리를 담당하는 InputProcessor 생성

---
+++ src/main/java/lotto/controller/InputProcessor.java
@@ -0,0 +1,39 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.util.NumberParser;
+import lotto.view.util.WinningNumberSplitter;
+
+import java.util.List;
+
+public class InputProcessor {
+
+    private final NumberParser numberParser;
+    private final WinningNumberSplitter winningNumberSplitter;
+    private final InputView inputView;
+
+    public InputProcessor(NumberParser numberParser, WinningNumberSplitter winningNumberSplitter, InputView inputView) {
+        this.numberParser = numberParser;
+        this.winningNumberSplitter = winningNumberSplitter;
+        this.inputView = inputView;
+    }
+
+    public Integer processPurchaseLottos() {
+        String input = inputView.readPurchaseAmount();
+        return numberParser.parseToInt(input);
+    }
+
+    public Lotto processWinningNumber() {
+        String input = inputView.readWinningNumber();
+        List<String> winningNumber = winningNumberSplitter.splitWinningNumber(input);
+        return new Lotto(winningNumber.stream()
+                .map(numberParser::parseToInt)
+                .toList());
+    }
+
+    public Integer processBonusNumber() {
+        String input = inputView.readBonusNumber();
+        return numberParser.parseToInt(input);
+    }
+}
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,67 +5,48 @@ import lotto.domain.PurchasedLottos;
 import lotto.domain.WinningNumbers;
 import lotto.domain.constant.Ranking;
 import lotto.service.LottoService;
-import lotto.view.InputView;
 import lotto.view.OutputView;
-import lotto.view.util.NumberParser;
-import lotto.view.util.WinningNumberSplitter;
 
 import java.util.EnumMap;
-import java.util.List;
 import java.util.function.Supplier;
 
 public class LottoController {
 
-    private final NumberParser numberParser;
-    private final WinningNumberSplitter winningNumberSplitter;
     private final LottoService lottoService;
-    private final InputView inputView;
+    private final InputProcessor inputProcessor;
     private final OutputView outputView;
 
-    public LottoController(NumberParser numberParser, WinningNumberSplitter winningNumberSplitter, LottoService lottoService, InputView inputView, OutputView outputView) {
-        this.numberParser = numberParser;
-        this.winningNumberSplitter = winningNumberSplitter;
+    public LottoController(LottoService lottoService, InputProcessor inputProcessor, OutputView outputView) {
         this.lottoService = lottoService;
-        this.inputView = inputView;
+        this.inputProcessor = inputProcessor;
         this.outputView = outputView;
     }
 
     public void run() {
         PurchasedLottos purchasedLottos = retryOnInvalidInput(this::getPurchasedLottos);
+        drawResult(purchasedLottos);
+    }
+
+    private PurchasedLottos getPurchasedLottos() {
+        Integer purchaseAmount = inputProcessor.processPurchaseLottos();
+        PurchasedLottos purchasedLottos = lottoService.purchaseLottos(purchaseAmount);
         outputView.printPurchasedLottos(purchasedLottos);
+        return purchasedLottos;
+    }
+
+    private void drawResult(PurchasedLottos purchasedLottos) {
         EnumMap<Ranking, Integer> statistics = getStatistics(purchasedLottos);
         double earningRate = lottoService.calculateEarningRate(purchasedLottos, statistics);
         outputView.printDrawResult(statistics, earningRate);
     }
 
-    private PurchasedLottos getPurchasedLottos() {
-        String input = inputView.readPurchaseAmount();
-        Integer purchaseAmount = numberParser.parseToInt(input);
-        return lottoService.purchaseLottos(purchaseAmount);
-    }
-
     private EnumMap<Ranking, Integer> getStatistics(PurchasedLottos purchasedLottos) {
-        Lotto winningNumber = retryOnInvalidInput(this::getWinningNumber);
-        WinningNumbers winningNumbers = retryOnInvalidInput(() -> getWinningNumbers(winningNumber));
+        Lotto winningNumber = retryOnInvalidInput(inputProcessor::processWinningNumber);
+        WinningNumbers winningNumbers = retryOnInvalidInput(() -> {
+            Integer bonusNumber = inputProcessor.processBonusNumber();
+            return WinningNumbers.from(winningNumber, bonusNumber);
+        });
         return lottoService.drawResult(purchasedLottos, winningNumbers);
-    }
-
-    private WinningNumbers getWinningNumbers(Lotto winningNumber) {
-        Integer bonusNumber = retryOnInvalidInput(this::getBonusNumber);
-        return WinningNumbers.from(winningNumber, bonusNumber);
-    }
-
-    private Lotto getWinningNumber() {
-        String input = inputView.readWinningNumber();
-        List<String> winningNumber = winningNumberSplitter.splitWinningNumber(input);
-        return new Lotto(winningNumber.stream()
-                .map(numberParser::parseToInt)
-                .toList());
-    }
-
-    private Integer getBonusNumber() {
-        String input = inputView.readBonusNumber();
-        return numberParser.parseToInt(input);
     }
 
     private <T> T retryOnInvalidInput(Supplier<T> input) {
