1d73c5ac519ac0b3013c0dc6be703b396aed9c47
hanmh319 <hanmh319@naver.com>
2024-11-02 17:05:39 +0900

    feat: ã구매 금액 및 시도 횟수 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int attempts= validateBetAmount()/1000;
+
+    }
+
+    private static int validateBetAmount(){
+        String input = Console.readLine();
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+        int betAmount = Integer.parseInt(input);
+        if(betAmount<1000){
+            throw new IllegalArgumentException("베팅 금액이 1000원 미만입니다");
+        }
+        if(betAmount%1000!=0) {
+            throw new IllegalArgumentException("베팅 금액이 1000원 단위가 아닙니다");
+        }
+        return betAmount;
     }
 }
