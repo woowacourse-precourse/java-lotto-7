4e0900c24079f8f9259d798579391d929dcff8ae
juhyeongKim <zxcvbm804@gmail.com>
2024-11-03 01:29:47 +0900

    refactor : Prize 리팩토링

--- src/main/java/model/Prize.java
+++ src/main/java/model/Prize.java
@@ -6,16 +6,16 @@ import java.util.function.BiPredicate;
 public enum Prize {
     ZERO(0, 0,
         (matchCount, containBonusNumber) -> matchCount < 3),
-    FIRST(2_000_000_000, 6,
-        (matchCount, containBonusNumber) -> matchCount == 6),
-    SECOND(30_000_000, 5,
-        (matchCount, containBonusNumber) -> matchCount == 5 && containBonusNumber),
-    THIRD(1_500_000, 5,
-        (matchCount, containBonusNumber) -> matchCount == 5 && !containBonusNumber),
+    FIFTH(5_000, 3,
+        (matchCount, containBonusNumber) -> matchCount == 3),
     FOURTH(50_000, 4,
         (matchCount, containBonusNumber) -> matchCount == 4),
-    FIFTH(5_000, 3,
-        (matchCount, containBonusNumber) -> matchCount == 3);
+    THIRD(1_500_000, 5,
+        (matchCount, containBonusNumber) -> matchCount == 5 && !containBonusNumber),
+    SECOND(30_000_000, 5,
+        (matchCount, containBonusNumber) -> matchCount == 5 && containBonusNumber),
+    FIRST(2_000_000_000, 6,
+        (matchCount, containBonusNumber) -> matchCount == 6);
     private final int money;
     private final int matchCount;
     private final BiPredicate<Integer, Boolean> isContain;
