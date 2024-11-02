6a4caf8f11d3a8e1fc0f0c173a9fd29a3cdcf1fb
Su-daa <subin.kk019@gmail.com>
2024-11-02 17:58:28 +0900

    feat: 로또 번호 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,4 +9,14 @@ public class OutputView {
     public static void printResultBuyCount(int purchaseCount) {
         System.out.printf("%d개를 구매했습니다.\n", purchaseCount);
     }
+
+    public static void printLottos(Lottos lottos) {
+        List<Lotto> lottoGroup = lottos.getLottos();
+        printResultBuyCount(lottoGroup.size());
+        lottoGroup.forEach(OutputView::printLotto);
+    }
+
+    private static void printLotto(Lotto lotto) {
+        System.out.println(lotto);
+    }
 }
