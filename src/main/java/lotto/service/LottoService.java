b341c56167c21bd640133623ba54ce2d0f1922f2
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-03 02:51:04 +0900

    feat(lotto): 로또 수량 구하기 기능 추가
    로또 수량 구하는 기능을 구현했습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,12 +1,20 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
-import lotto.domain.Lotto;
 
 public class LottoService {
+    private final int LOTTO_PRICE = 1000;
+    private final int START_INCLUSIVE = 1;
+    private final int END_INCLUSIVE = 45;
+    private final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
+
     public List<Integer> createLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, NUMBER_OF_LOTTO_NUMBERS);
+    }
+
+    public int getLottoQuantity(int amount) {
+        return amount / LOTTO_PRICE;
     }
 }
