2853dd8782b8ebb43c8692b8f43ee26e3659dee9
bowook <mbosx316@naver.com>
2024-11-02 17:12:53 +0900

    feat(WinningNumber) : WinningNumber 기능 추가
    - 당첨 번호와 보너스 번호를 모아놓기 위한 객체

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class WinningNumber {
+package lotto.domain;
+
+import lotto.validator.exception.ErrorMessage;
+import lotto.validator.exception.LottoException;
+
+public class WinningNumber {
+    private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
+
+    public WinningNumber(Lotto winningLotto, BonusNumber bonusNumber) {
+        validateDuplicate(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicate(Lotto winningLotto, BonusNumber bonusNumber) {
+        if (winningLotto.getNumbers().contains(bonusNumber.getNumber())) {
+            throw LottoException.from(ErrorMessage.BONUS_NUMBER_DUPLICATE_WINNING_LOTTO);
+        }
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public BonusNumber getBonusNumber() {
+        return bonusNumber;
+    }
 }
