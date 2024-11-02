295f2375cedd7ffad6a260f97f210ac56a656946
pokm321 <pokm90@gmail.com>
2024-11-02 17:59:47 +0900

    test: add test cases for LottoValidator

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class MoneyValidatorTest {
+public class LottoValidatorTest {
 
-    MoneyValidator validator;
+    LottoValidator validator;
 
     @Test
     void 값이_null이면_예외발생() {
         assertThatThrownBy(() -> {
-            validator = new MoneyValidator(null);
+            validator = new LottoValidator(null);
             validator.validateNotNull();
         }).isInstanceOf(IllegalArgumentException.class);
     }
@@ -22,40 +22,40 @@ public class MoneyValidatorTest {
     @ValueSource(strings = {"", " ", "   "})
     void 텍스트가_비어있으면_예외발생(String input) {
         assertThatThrownBy(() -> {
-            validator = new MoneyValidator(input);
+            validator = new LottoValidator(input);
             validator.validateNotEmpty();
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"null", "asdf", "32.22", "0.0", "-37.0"})
+    @ValueSource(strings = {"asd,123,qwe1", "asdf", "1,2.0,2", "0.0,33", "-37.0,11,999"})
     void 정수가_아니면_예외발생(String input) {
         assertThatThrownBy(() -> {
-            validator = new MoneyValidator(input);
+            validator = new LottoValidator(input);
             validator.validateWholeNumber();
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"0", "123", "01", "-0", "-49183921", "99999999999999999999999999999999999"})
+    @ValueSource(strings = {"0,0,0", "1,3,-2", "-0,1,2,3", "-49183921,493021", "99999999999999999999999999999999999"})
     void 정수면_통과(String input) {
-        validator = new MoneyValidator(input);
+        validator = new LottoValidator(input);
         validator.validateWholeNumber();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"99999999999999999999999999999999999", "-99999999999999999999999999999999999"})
-    void long_변환_불가능할경우_예외발생(String input) {
+    @ValueSource(strings = {"99999999999999999999999999999999999,-99999999999999999999999999999999999"})
+    void 정수_리스트_변환_불가능할경우_예외발생(String input) {
         assertThatThrownBy(() -> {
-            validator = new MoneyValidator(input);
-            validator.validateLong();
+            validator = new LottoValidator(input);
+            validator.validateListOfInteger();
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"999999999", "-9999999999999", "0000000000000000000000000000000000000000000001"})
-    void long_변환_가능한경우_통과(String input) {
-        validator = new MoneyValidator(input);
-        validator.validateLong();
+    @ValueSource(strings = {"999999,-999999999,0,0,1,1", "0000000000000000000000000000000000000000000001,2,3"})
+    void 정수_리스트_변환_가능한경우_통과(String input) {
+        validator = new LottoValidator(input);
+        validator.validateListOfInteger();
     }
 }
