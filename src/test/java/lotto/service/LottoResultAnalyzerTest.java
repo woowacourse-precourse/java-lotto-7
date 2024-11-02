9f76bf84b0379d75f031f26ed941284273f2b5c2
whdmscks11 <whdmscks11@gmail.com>
2024-11-02 18:37:01 +0900

    test(LottoResultAnalyzerTest): 수익률 계산 테스트 삭제
    - computeYield 메서드의 파라미터가 LottoGenerator로 변경됨에 따라, 사용자 로또를 직접 생성할 수 없어 기존 테스트를 삭제
    - 랜덤으로 생성되는 LottoGenerator를 테스트에 활용할 수 없으므로 수익률 계산 검증이 불가능함

--- src/test/java/lotto/service/LottoResultAnalyzerTest.java
+++ src/test/java/lotto/service/LottoResultAnalyzerTest.java
@@ -8,40 +8,25 @@ import java.util.Map;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPrize;
 import lotto.domain.LottoWinningNumbers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoResultAnalyzerTest {
 
     private final LottoResultAnalyzer lottoResultAnalyzer = new LottoResultAnalyzer();
-    private final Lotto[] userLotto = new Lotto[3];
-    LottoWinningNumbers winningLotto = new LottoWinningNumbers("1,2,3,4,5,6", "7");
 
-    @BeforeEach
-    void init(){
+    @Test
+    void 사용자_로또_추첨_결과_확인_테스트(){
         // 사용자 로또 발행
+        Lotto[] userLotto = new Lotto[3];
         userLotto[0] = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         userLotto[1] = new Lotto(List.of(1, 2, 3, 4, 5, 7));
         userLotto[2] = new Lotto(List.of(1, 2, 3, 4, 5, 10));
 
         // 당첨 번호 생성
+        LottoWinningNumbers winningLotto = new LottoWinningNumbers("1,2,3,4,5,6", "7");
         winningLotto.generate();
-    }
 
-    @Test
-    void 사용자_로또_추첨_결과_확인_테스트(){
         Map<LottoPrize, Integer> analyzedResult = lottoResultAnalyzer.analyze(userLotto, winningLotto);
         assertThat(analyzedResult).containsValues(1, 1, 1, 0, 0); // 1등:1, 2등:1, 3등:1, 4등:0, 5등:0
     }
-
-    @Test
-    void 수익률_계산_테스트(){
-        // 추첨 결과 확인 후
-        lottoResultAnalyzer.analyze(userLotto, winningLotto);
-
-        // 수익률 계산
-        int totalYield = lottoResultAnalyzer.computeYield();
-        assertEquals(2_031_500_000, totalYield);
-    }
-
 }
