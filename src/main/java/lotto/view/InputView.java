a5aae92764ffc3a0aef0285ef5ca5a03b83c9467
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-11-02 16:24:52 +0900

    feat: InputView 클래스 및 테스트 구현
    - 로또 구입 금액, 당첨 번호, 보너스 번호 입력 기능
    - 구입 금액 1000원 단위 아닐 경우 예외 처리
    - 당첨 번호 입력 시 숫자, 쉼표(,)형식 아닐 경우 예외 처리
    - 보너스 번호 입력 시 숫자형식 아닐 경우 예외 처리
    - 각 입력 테스트 메스드를 통한 테스트 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,54 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
+    public int inputPurchase() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return validatePurchase(input);
+    }
+
+    public List<Integer> WinNumbers() {
+        System.out.println("당첨 번호를 입력해주세요.");
+        String input = Console.readLine();
+        return validateWinNumber(input);
+    }
+
+    public int BonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        String input = Console.readLine();
+        return 0;
+    }
+
+    static int validatePurchase(String input) {
+        int lottoAmount = Integer.parseInt(input);
+        if (lottoAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 살 수 있습니다");
+        }
+        return lottoAmount / 1000;
+    }
+
+    static List<Integer> validateWinNumber(String input) {
+        String[] LottoNumber = input.split(",");
+        List<Integer> winNumbers = new ArrayList<>();
+        try {
+            for (String number : LottoNumber) {
+                winNumbers.add(Integer.parseInt(number));
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 하고, 쉼표(,)로 구분되어야 합니다.");
+        }
+        return winNumbers;
+    }
+
+    static int validateBonusNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+    }
 }
---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,52 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    @Test
+    void 구입_금액_1000원_단위일_경우() {
+        int result = InputView.validatePurchase("7000");
+        assertThat(result).isEqualTo(7);
+    }
+
+    @Test
+    void 구입_금액_1000원_단위가_아닐_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> InputView.validatePurchase("7700"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호가_올바른_형식일_경우() {
+        List<Integer> winNumber = InputView.validateWinNumber("1,2,3,4,5,6");
+        assertThat(winNumber).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 당첨_번호에_문자가_포함된_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> InputView.validateWinNumber("1,2,3,4,5,a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호에_쉼표로_구분하지_않은_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> InputView.validateWinNumber("1,2,3,4,5.6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_숫자인_경우() {
+        int BonusNumber = InputView.validateBonusNumber("7");
+        assertThat(BonusNumber).isEqualTo(7);
+    }
+
+    @Test
+    void 보너스_번호가_숫자가_아닌_경우() {
+        assertThatThrownBy(() -> InputView.validateBonusNumber("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
