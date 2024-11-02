fffb96ddd80240e9c52c59001ac37b9c7a414ac2
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-03 05:54:39 +0900

    feat: findMatchNumberCount 메서드 추가
    - theNumberOfLotto : 로또 발행 개수
    - convertStringToInt : String 타입을 Int타입으로 변환
    - totalLotto : 발행된 로또를 리스트에 저장
    - findMatchNumber : 발행 번호와 일치하는 번호를 찾아서 리스트에 저장
    - findMatchNumberCount : 일치된 번호의 로또의 개수를 리스트에 저장
    
    - 객체 지향보다도 급선무는 기능을 어떻게든 구현하는 것이라고 생각해서
      절차지향적으로 작업했습니다. 준비도 없이 java라는 언어로 프로그래밍을
      하는 것이 쉽지 않았습니다.
    
    - 일치하는 번호의 개수를 구한 뒤 어떻게 할지 고민하다가 리스트에
      저장하고 리스트를 순회해서 일치된 번호랑 동일한 인덱스에 저장하여
      나중에 상금을 구할 수 있도록 했습니다.

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,8 +1,13 @@
 package lotto.Model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.View.InputView;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -24,9 +29,66 @@ public class Lotto {
         return numbers;
     }
 
-    public List<Integer> publicationNumbersArray() {
+    public List<Integer> getSortedLottoNumbers() {
         List<Integer> numbers = lottoNumberPublication();
         Collections.sort(numbers);
         return numbers;
+    }
+
+    public int theNumberOfLotto(int lottoPurchase, int price) {
+        return lottoPurchase / price;
+    }
+
+    public int convertStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public List<List<Integer>> totalLotto(int count) {
+        List<List<Integer>> publicationNumbers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            publicationNumbers.add(getSortedLottoNumbers());
+        }
+
+        return publicationNumbers;
+    }
+
+    public List<Integer> findMatchNumber() {
+        int PRICE = 1000;
+        int lottoPurchase = convertStringToInt(InputView.lottoPurchase());
+        int count = theNumberOfLotto(lottoPurchase, PRICE);
+        List<List<Integer>> lotto = totalLotto(count);
+        List<Integer> duplicateCountList = new ArrayList<>();
+
+        Set<Integer> set = new HashSet<>(numbers);
+
+        for (List<Integer> numbers : lotto) {
+            long duplicateCount = numbers.stream()
+                    .filter(set::contains)
+                    .count();
+
+            duplicateCountList.add((int) duplicateCount);
+        }
+
+        return duplicateCountList;
+    }
+
+    public List<Integer> findMatchNumberCount(List<Integer> duplicateCountList) {
+        // 0부터 5까지의 인덱스를 가진 리스트를 초기화 (모든 값은 0)
+        List<Integer> result = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0));
+
+        // 각 숫자의 개수를 계산하여 해당 인덱스에 저장
+        for (int number : duplicateCountList) {
+            result.set(number, result.get(number) + 1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+
+        lotto.findMatchNumberCount(lotto.findMatchNumber());
     }
 }
