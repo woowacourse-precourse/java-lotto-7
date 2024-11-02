a834e3e18273a56c775fcc56d2b3f16a406cef4b
soeun2537 <soeun2537@gmail.com>
2024-11-02 16:49:06 +0900

    feat: 여러 장의 로또 티켓 생성 확인하는 로직 추가

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -2,7 +2,9 @@ package lotto.model;
 
 import static lotto.constant.core.LottoServiceConstant.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.model.domain.Lotto;
 import lotto.model.domain.WinningLotto;
 import lotto.model.util.RandomUtil;
 
@@ -20,5 +22,22 @@ public class LottoService {
 
     public WinningLotto createWinningTicket(List<Integer> numbers, Integer bonusNumber) {
         return WinningLotto.of(numbers, bonusNumber);
+    }
+
+    public List<Lotto> createLottoTickets(Integer lottoCount) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lottoTicket = createLottoTicket();
+            lottoTickets.add(lottoTicket);
+        }
+        return lottoTickets;
+    }
+
+    private Lotto createLottoTicket() {
+        List<Integer> numbers = randomUtil.issueLottoTicket(
+                MIN_LOTTO_NUMBER.getIntegerValue(),
+                MAX_LOTTO_NUMBER.getIntegerValue(),
+                LOTTO_NUMBER_COUNT.getIntegerValue());
+        return Lotto.of(numbers);
     }
 }
