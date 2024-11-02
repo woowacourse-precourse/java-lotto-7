598531d36a07a01aa29cec11e9d3d288fdf224fb
jinwook <wjswlsdnr333@gmail.com>
2024-11-02 18:10:57 +0900

    feat(OutputView): 발행한 로또 출력기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        Lotto lotto = new Lotto(inputView.getLottoNumbers());
+        List<List<Integer>> boughtLottoNumbers = lotto.generateLotto(inputView.getLottoCount());
+        OutputView outputView = new OutputView(boughtLottoNumbers);
+        outputView.outputLottoNumbers();
     }
 }
--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,4 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class OutputView {
+    private final List<List<Integer>> boughtLottoNumbers;
+
+    public OutputView(List<List<Integer>> boughtLottoNumbers) {
+        this.boughtLottoNumbers = boughtLottoNumbers;
+    }
+
+    public void outputLottoNumbers() {
+        System.out.println(boughtLottoNumbers.size() + "개를 구매했습니다.");
+        for (List<Integer> boughtLottoNumber : boughtLottoNumbers) {
+            System.out.println(boughtLottoNumber);
+        }
+    }
+
 }
