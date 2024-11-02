c3b8710f6377fe0c2a92e9060731fca582f435bc
JiEung2 <181757@jnu.ac.kr>
2024-11-03 00:39:03 +0900

    test(lottoNumbersValidatorTest): 로또 숫자가 6개가 아닐 때 예외 테스트 추가

--- src/test/java/lotto/validator/LottoNumbersValidatorTest.java
+++ src/test/java/lotto/validator/LottoNumbersValidatorTest.java
@@ -1,6 +1,8 @@
 package lotto.validator;
 
+import static lotto.exception.Exception.*;
 import static lotto.validator.LottoNumbersValidator.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -14,5 +16,15 @@ class LottoNumbersValidatorTest {
     void validateLottoNumber_success() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
         assertDoesNotThrow(() -> validate(numbers));
+    }
+
+    @DisplayName("로또 숫자가 6개가 아닐 때 예외 테스트")
+    @Test
+    void validateLottoNumberSize_fail() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        assertThatThrownBy(() -> validate(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBER_SIZE_MUST_BE_SIX.getMessage());
     }
 }
