7b3f7bbce192c60b83701421af49ee0a6bdf73e4
OSEUK <tpdnrzmzm3@naver.com>
2024-11-02 18:20:13 +0900

    refactor(domain) : Money refactor
    processor 정형화를 통한 검증 로직 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,22 +2,15 @@ package lotto.domain;
 
 public class Money {
     private static final int DIVISABLE_PRICE = 1000;
-    private static final String NOT_DIVISABLE = "[ERROR] 구입 금액은 1000원으로 나누어 떨어져야 합니다.";
 
     int price;
     int amount;
 
     public Money(int price) {
-        validate(price);
         this.price = price;
         this.amount = price / DIVISABLE_PRICE;
     }
 
-    private void validate(int price){
-        if (price % DIVISABLE_PRICE != 0){
-            throw new IllegalArgumentException(NOT_DIVISABLE);
-        }
-    }
 
     public int getAmount() {
         return amount;
