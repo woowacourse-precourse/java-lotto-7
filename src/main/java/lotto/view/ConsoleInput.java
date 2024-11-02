1754a44b439fd132e8010de2831afd9e4198e667
kungbi <kipper12335@gmail.com>
2024-11-02 12:29:09 +0900

    refactor(ConsoleInputView): InputProvider 주입 및 출력 형식 변경

--- src/main/java/lotto/view/ConsoleInput.java
+++ src/main/java/lotto/view/ConsoleInput.java
@@ -1,33 +1,38 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.NoSuchElementException;
+import lotto.view.InputProvider.InputProvider;
 
 public class ConsoleInput {
+    final private InputProvider inputProvider;
+
+    public ConsoleInput(InputProvider inputProvider) {
+        this.inputProvider = inputProvider;
+    }
+
     public String getPurchasedAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return readline();
+        String input = inputProvider.readLine();
+        System.out.println();
+        return input;
     }
 
     public String getWinnerNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return readline();
+        String input = inputProvider.readLine();
+        System.out.println();
+        return input;
     }
 
     public String getBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return readline();
+        String input = inputProvider.readLine();
+        System.out.println();
+        return input;
     }
 
     public void close() {
         Console.close();
     }
 
-    private String readline() {
-        try {
-            return Console.readLine();
-        } catch (NoSuchElementException | IllegalStateException error) {
-            throw new IllegalArgumentException("입력을 받는데 실패했습니다.", error);
-        }
-    }
 }
