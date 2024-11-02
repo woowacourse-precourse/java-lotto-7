330c9568dfeb9ad3c08b24ea0b325077d8b33884
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-02 15:51:25 +0900

    feat: 당첨 숫자를 확인하기 위해 WinningCalculator 도입

---
+++ src/main/java/utils/WinningCalculator.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.List;
+
+public class WinningCalculator {
+
+    private WinningCalculator() {
+    }
+
+    public static int countMatchingNumber(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        return (int) lottoNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public static boolean isBonusNumberMatched(List<Integer> winningNumbers, int bonusNumber) {
+        return winningNumbers.contains(bonusNumber);
+    }
+}
