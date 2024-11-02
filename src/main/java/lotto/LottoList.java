dc9f859e6b1d0698e71ab6d09ea35104f3b73d17
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-11-02 22:24:21 +0900

    feat: 구매한 로또 리스트 출력 메서드 구현

--- src/main/java/lotto/LottoList.java
+++ src/main/java/lotto/LottoList.java
@@ -3,6 +3,7 @@ package lotto;
 import static camp.nextstep.edu.missionutils.Randoms.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoList {
@@ -11,6 +12,17 @@ public class LottoList {
     private static final int lottoNumberMin = 1;
     private static final int lottoNumberMax = 45;
     private final List<Lotto> lottoList = new ArrayList<>();
+
+    public void printLottoList() {
+
+        for (Lotto lotto : lottoList) {
+            List<Integer> numbers = lotto.getNumbers();
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
+
+        System.out.println();
+    }
 
     public LottoList(int numberOfLotto) {
         for (int i = 0; i < numberOfLotto; i++) {
