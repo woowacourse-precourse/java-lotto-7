db31e7e600ba6387615438d91760ca10f9384916
speculatingwook <bwook9908@gmail.com>
2024-11-02 20:34:38 +0900

    refactor: detach inputs to constructor in Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,32 +5,57 @@ import lotto.io.Input;
 import lotto.io.View;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
 public class Application {
+    private final Integer price;
+    private final Lotto winningNumber;
+    private final Integer bonusNumber;
+
+    public Application() {
+        this.price = Integer.parseInt(Input.inputPrice());
+        this.winningNumber = new Lotto(toLotto(Input.inputWinningNumber()));
+        this.bonusNumber = Integer.parseInt(Input.inputBonusNumber());
+    }
+
     public static void main(String[] args) {
-        Integer price = Integer.parseInt(Input.inputPrice());
-        Lotto winningNumber = new Lotto(Arrays.stream(Input.inputWinningNumber().split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList()));
-        Integer bonusNumber = Integer.parseInt(Input.inputBonusNumber());
+        Application app = new Application();
 
         LottoGenerator lottoGenerator = new LottoGenerator();
-        Lottos lottos = new Lottos(lottoGenerator.generateLottos(price));
+        Lottos lottos = new Lottos(lottoGenerator.generateLottos(app.getPrice()));
 
         LottoResult lottoResult = new LottoResult();
-        WinningChecker winningChecker = new WinningChecker(winningNumber, bonusNumber, lottoResult);
+        WinningChecker winningChecker = new WinningChecker(app.getWinningNumber(), app.getBonusNumber(), lottoResult);
         winningChecker.calculate(lottos);
         lottoResult.calculateTotalBenefit();
 
         printResults(lottos, lottoResult);
     }
 
-    private static void printResults(Lottos lottos,LottoResult lottoResult) {
+    private static void printResults(Lottos lottos, LottoResult lottoResult) {
         View.printLotto(lottos.getLottoCount(), lottos.toString());
         View.printWinningResult(lottoResult.toString());
         View.printProfit(lottoResult.getProfitRate(lottos));
+    }
+
+    private Integer getPrice() {
+        return price;
+    }
+
+    private Lotto getWinningNumber() {
+        return winningNumber;
+    }
+
+    private Integer getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private List<Integer> toLotto(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
