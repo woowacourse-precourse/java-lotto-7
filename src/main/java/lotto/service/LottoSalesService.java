cc25640fc98ea1a978a044a3d0c9bb2c500eab04
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-11-02 23:03:46 +0900

    feat: 구매할 수 있는 로또의 수량을 확인하는 기능 추가

--- src/main/java/lotto/service/LottoSalesService.java
+++ src/main/java/lotto/service/LottoSalesService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
@@ -13,7 +14,7 @@ public class LottoSalesService {
         validateQuantity(quantity);
         List<Lotto> lottos = new ArrayList<>();
 
-        for(int i=0;i<quantity;i++) {
+        for (int i = 0; i < quantity; i++) {
             Lotto lotto = new Lotto(randomNumberGenerator.generate(1, 45, 6));
             lottos.add(lotto);
         }
@@ -21,12 +22,25 @@ public class LottoSalesService {
         return lottos;
     }
 
-    private void validateQuantity(int quantity) {
-        if(quantity < 0) {
-            throw new IllegalArgumentException("[ERROR] 수량은 0보다 작을 수 없습니다.");
+    public int getAvailableLottoQuantity(int payment) throws IllegalArgumentException {
+        validatePayment(payment);
+        if (payment % 1000 == 0) {
+            return payment / 1000;
         }
-        else if(quantity == Integer.MAX_VALUE) {
+        throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구입할 수 있습니다.");
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("[ERROR] 수량은 0보다 작을 수 없습니다.");
+        } else if (quantity == Integer.MAX_VALUE) {
             throw new IllegalArgumentException("[ERROR] 수량은 Integer.MAX_VALUE 보다 클 수 없습니다.");
+        }
+    }
+
+    private void validatePayment(int payment) {
+        if (payment < 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 0보다 커야 합니다.");
         }
     }
 }
--- src/test/java/lotto/service/LottoSalesServiceTest.java
+++ src/test/java/lotto/service/LottoSalesServiceTest.java
@@ -6,7 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoSalesServiceTest {
 
@@ -45,5 +49,38 @@ class LottoSalesServiceTest {
         //when & then
         assertThrows(IllegalArgumentException.class,
             () -> lottoSalesService.createLottos(epicQuantity));
+    }
+
+    @DisplayName("구매 금액이 주어지면 구매가능한 로또 수량을 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1", "8000,8", "100_000_000,100_000"})
+    public void givenPayment_whenCalculateAvailableQuantity_thenReturnQuantity(int payment, int expected) {
+        //when
+        int quantity = lottoSalesService.getAvailableLottoQuantity(payment);
+
+        //then
+        assertThat(quantity).isEqualTo(expected);
+    }
+
+    @DisplayName("구매금액이 음수이면 예외를 발생시킨다.")
+    @Test
+    public void givenInvalidPayment_thenThrowException() {
+        //given
+        int payment = -1000;
+
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+            () -> lottoSalesService.getAvailableLottoQuantity(payment));
+    }
+
+    @DisplayName("구매금액이 1000원 단위가 아니라면 예외를 발생시킨다.")
+    @RepeatedTest(value = 4, name = "구매 금액 : 1{currentRepetition}00원")
+    public void shouldThrowException_whenPaymentIsNotInThousands(RepetitionInfo repetition) {
+        //given
+        int payment = 1000 + repetition.getCurrentRepetition();
+
+        //when & then
+        assertThrows(IllegalArgumentException.class,
+            () -> lottoSalesService.getAvailableLottoQuantity(payment));
     }
 }
