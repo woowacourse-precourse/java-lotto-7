26ce42673d6961edae768c6e4616c4ceea1bd440
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 15:34:59 +0900

    feat(message): LottoErrorMessage enum 구현

---
+++ src/main/java/lotto/message/LottoErrorMessage.java
@@ -0,0 +1,18 @@
+package lotto.message;
+
+public enum LottoErrorMessage {
+
+    INVALID_NUMBERS_COUNT("로또 번호는 6개여야 합니다."),
+    DUPLICATE_NUMBERS("로또 번호는 중복될 수 없습니다.");
+
+    private final String message;
+    private final String prefix = ErrorMessage.PREFIX.getMessage();
+
+    LottoErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return prefix + message;
+    }
+}
