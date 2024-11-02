3107bc500ab7efa4f0be91be9b8ada60ce08a694
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 01:59:58 +0900

    feat: 로또 관련 상수 클래스 작성

---
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -0,0 +1,14 @@
+package lotto.constants;
+
+public final class LottoConstant {
+    public final static int WON_1000 = 1000;
+    public static final int PURCHASABLE_MAX_AMOUNTS = 100000;
+
+    public final static int LOTTO_COUNT = 6;
+    public final static int MIN_LOTTO_NUMBER = 1;
+    public final static int MAX_LOTTO_NUMBER = 45;
+    public final static int COUNT = 6;
+
+    private LottoConstant() {
+    }
+}
