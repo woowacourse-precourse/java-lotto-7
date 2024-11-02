56f25cc696e903858bb024c410d36b2262572b74
fbehddn <tom0611@naver.com>
2024-11-02 17:43:40 +0900

    feat: 게임시작 안내문구 출력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+import lotto.parsers.Parser;
+
+public class InputView {
+    public int inputMoney() {
+        System.out.println("구입 금액을 입력해주세요.");
+        return Integer.parseInt(readLine());
+    }
+
+    public List<Integer> inputWinningNumber() {
+        System.out.println("당첨 번호를 입력해주세요.");
+        String numbers = readLine();
+        return Parser.parseStringToList(numbers);
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        return Integer.parseInt(readLine());
+    }
+}
