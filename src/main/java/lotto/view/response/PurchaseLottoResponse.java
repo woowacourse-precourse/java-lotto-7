8a946df8daebe527d90211fa4cab731c7e40bbe9
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-02 22:22:14 +0900

    refactor($PurchaseLottoResponse): Lottos 를 통해 초기화되도록 변경

--- src/main/java/lotto/view/response/PurchaseLottoResponse.java
+++ src/main/java/lotto/view/response/PurchaseLottoResponse.java
@@ -1,26 +1,28 @@
 package lotto.view.response;
 
+import lotto.model.Lottos;
+
 import java.util.List;
 
 public class PurchaseLottoResponse {
 
-    private final int purchasedAmount;
     private final List<LottoNumberResponse> lottoNumberResponses;
+    private final int purchasedAmount;
 
-    private PurchaseLottoResponse(List<LottoNumberResponse> lottoNumberResponses) {
+    private PurchaseLottoResponse(Lottos lottos) {
+        this.lottoNumberResponses = lottos.getAllLottoNumbers().stream().map(LottoNumberResponse::from).toList();
         this.purchasedAmount = lottoNumberResponses.size();
-        this.lottoNumberResponses = lottoNumberResponses;
     }
 
-    public static PurchaseLottoResponse from(List<LottoNumberResponse> lottoNumberResponses) {
-        return new PurchaseLottoResponse(lottoNumberResponses);
-    }
-
-    public int getPurchasedAmount() {
-        return purchasedAmount;
+    public static PurchaseLottoResponse from(Lottos lottos) {
+        return new PurchaseLottoResponse(lottos);
     }
 
     public List<LottoNumberResponse> getLottoNumberResponses() {
         return lottoNumberResponses;
+    }
+
+    public int getPurchasedAmount() {
+        return purchasedAmount;
     }
 }
