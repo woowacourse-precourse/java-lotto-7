644d14ce8864fe50ced95601367cb9e63219f78e
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-02 14:21:04 +0900

    refactor(InputView): 구입 금액 입력 기능 리팩토링
    정수로 변환해서 보내는 방식 대신 일단 먼저 문자열로 반환하도록 리팩토링했습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,8 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public int inputAmount() {
+    public String inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
+
 }
