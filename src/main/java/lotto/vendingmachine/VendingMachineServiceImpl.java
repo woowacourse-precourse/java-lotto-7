306429c54dd96c1afc9d8bab3af8c2010a1054e8
woong2e <tlswodnd12345@naver.com>
2024-11-02 17:19:51 +0900

    feat: implement generate method to create Lotto tickets

---
+++ src/main/java/lotto/vendingmachine/VendingMachineServiceImpl.java
@@ -0,0 +1,43 @@
+package lotto.vendingmachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMachineServiceImpl implements VendingMachineService {
+
+    private final VendingMachineRepository vendingMachineRepository;
+
+    public VendingMachineServiceImpl(VendingMachineRepository vendingMachineRepository) {
+        this.vendingMachineRepository = vendingMachineRepository;
+    }
+
+    @Override
+    public List<Lotto> generate(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            List<Integer> numbers = pickRandomNumbers();
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private List<Integer> pickRandomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return numbers;
+    }
+
+    @Override
+    public void recordLottos(List<Lotto> lottos) {
+
+    }
+
+    @Override
+    public List<Lotto> getIssuedLottoTickets() {
+        return List.of();
+    }
+
+}
