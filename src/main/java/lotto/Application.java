760f80212388744215250544bb4ebb4bb127c0c8
kymin <park48664866@gmail.com>
2024-11-02 15:04:04 +0900

    feat: LottoHandler 의존성 주입 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,13 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.handler.InputHandler;
+import lotto.handler.LottoHandler;
 import lotto.handler.OutputHandler;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController controller = new LottoController(new InputHandler(),
-                new OutputHandler());
+        LottoController controller = new LottoController(
+                new InputHandler(), new OutputHandler(), new LottoHandler());
 
         controller.lottoStart();
     }
