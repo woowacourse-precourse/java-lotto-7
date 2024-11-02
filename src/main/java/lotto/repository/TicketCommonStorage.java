eb26871688bd06616ed74dfef791f46ee1cdad47
plan11plan <tjtfy7028@gmail.com>
2024-11-02 13:50:01 +0900

    feat: 공용 로또 티켓 저장소 개발
    쓰기 전용 레파지토리, 읽기 전용 레파지토리에서 사용할 저장소 개발
    최대한 하나의 클래스에 하나의 기능만 할 수 있게 노력함

---
+++ src/main/java/lotto/repository/TicketCommonStorage.java
@@ -0,0 +1,16 @@
+package lotto.repository;
+
+import java.util.concurrent.ConcurrentHashMap;
+import lotto.domain.ticket.Ticket;
+
+class TicketCommonStorage {
+    private static final ConcurrentHashMap<Long, Ticket> repository = new ConcurrentHashMap<>();
+
+    private TicketCommonStorage() {
+    }
+
+    public static ConcurrentHashMap<Long, Ticket> getRepository() {
+        return repository;
+    }
+
+}
