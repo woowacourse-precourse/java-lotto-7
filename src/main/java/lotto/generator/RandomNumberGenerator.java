de64b70c8cf3270564dd6a3a34a95f378107e48b
xeimun <hinote444@naver.com>
2024-11-02 19:31:04 +0900

    feat(NumberGenerator, RandomNumberGenerator): 설정된 범위와 개수만큼 번호 생성

---
+++ src/main/java/lotto/generator/RandomNumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.generator;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int PICK_COUNT = 6;
+
+    @Override
+    public List<Integer> generateNumber() {
+        return pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, PICK_COUNT);
+    }
+}
