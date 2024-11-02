ef4a230078cf8fc134e52e892e3a0750c9fdf087
plan11plan <tjtfy7028@gmail.com>
2024-11-02 16:49:09 +0900

    feat: 로또 수량 범위 검증 기능 추가

--- src/main/java/lotto/domain/payment/LottoQuantity.java
+++ src/main/java/lotto/domain/payment/LottoQuantity.java
@@ -1,6 +1,8 @@
 package lotto.domain.payment;
 
 public class LottoQuantity {
+    private static final int MINIMUM_QUANTITY = 1;
+    private static final int MAXIMUM_QUANTITY = 100;
 
     private final int value;
 
@@ -9,7 +11,30 @@ public class LottoQuantity {
     }
 
     public static LottoQuantity of(int count) {
+        validateInValidRange(count);
+
         return new LottoQuantity(count);
+    }
+
+    private void validate(int count) {
+        validateInValidRange(count);
+    }
+
+    private static void validateInValidRange(int count) {
+        validateMinimumQuantity(count);
+        validateMaximumQuantity(count);
+    }
+
+    private static void validateMaximumQuantity(int count) {
+        if (count < MINIMUM_QUANTITY) {
+            throw new IllegalArgumentException("[ERROR] 로또 수량은 1개 이상이어야 합니다.");
+        }
+    }
+
+    private static void validateMinimumQuantity(int count) {
+        if (count > MAXIMUM_QUANTITY) {
+            throw new IllegalArgumentException("[ERROR] 로또 수량은 100개를 초과할 수 없습니다.");
+        }
     }
 
     public int getValue() {
