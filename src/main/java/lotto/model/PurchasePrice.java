8e53dc582f30fec34826a9f68d999221418e5c6d
minseo <alstj778@gmail.com>
2024-11-03 00:10:24 +0900

    refactor(PurchasePrice): 컴팩트 생성자 사용

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -3,23 +3,22 @@ package lotto.model;
 public record PurchasePrice(int value) {
     private static final int LOTTO_PRICE = 1000;
 
-    public static PurchasePrice from(final int value) {
+    public PurchasePrice {
         validate(value);
-        return new PurchasePrice(value);
     }
 
-    private static void validate(final int value) {
+    private void validate(final int value) {
         validateIsPositive(value);
         validateIsPurchaseUnit(value);
     }
 
-    private static void validateIsPositive(final int value) {
+    private void validateIsPositive(final int value) {
         if (value <= 0) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void validateIsPurchaseUnit(final int value) {
+    private void validateIsPurchaseUnit(final int value) {
         if (value % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException();
         }
