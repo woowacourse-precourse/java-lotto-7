6e45a41c9baa5100fefd85fb3b7d34ea1325c1f5
yaongmeow <minnie_00@naver.com>
2024-11-03 00:34:09 +0900

    test(LottoManager): 테스트코드 추가
    - 보너스 번호 검증 테스트
    - 빈 로또 리스트 검증 테스트

--- src/test/java/lotto/LottoManagerTest.java
+++ src/test/java/lotto/LottoManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoManagerTest {
@@ -16,10 +17,8 @@ public class LottoManagerTest {
                 new Lotto(List.of(1, 2, 4, 6, 10, 11)),
                 new Lotto(List.of(2, 3, 7, 10, 20, 30))
         );
-
         Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
         Integer bonusNumber = 8;
-
         LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
 
         // when
@@ -41,7 +40,6 @@ public class LottoManagerTest {
                 new Lotto(List.of(1, 2, 4, 6, 10, 11)),
                 new Lotto(List.of(2, 3, 7, 10, 20, 30))
         );
-
         Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
         Integer bonusNumber = 8;
         LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
@@ -52,5 +50,29 @@ public class LottoManagerTest {
 
         // then
         assertThat(profitRate).isEqualTo(50166.7);
+    }
+
+    @Test
+    void 빈_로또_리스트를_제공한_경우_예외가_발생한다() {
+        List<Lotto> myLotto = List.of();
+        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Integer bonusNumber = 8;
+
+        assertThatThrownBy(() -> new LottoManager(myLotto, winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호의_범위가_올바르지_않은_경우_예외가_발생한다() {
+        List<Lotto> myLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
+                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+        );
+        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Integer bonusNumber = 50;
+
+        assertThatThrownBy(() -> new LottoManager(myLotto, winningLotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
