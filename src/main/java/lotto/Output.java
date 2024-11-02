4fcc80e93971c9ef187b8db1ffb4311bda703be5
KoYunHyuk <aes2036@daum.net>
2024-11-02 18:38:19 +0900

    fix: Change printing Message English to Korean

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class Output {
     public static void printLotto(List<Lotto> values) {
-//        System.out.println(values.size() + "개를 구매했습니다.");
-        System.out.println(values.size() + " Purchase Lotto.");
+        System.out.println(values.size() + "개를 구매했습니다.");
         for (int i = 0; i < values.size(); i++) {
             System.out.println(Arrays.toString(values.get(i).getNumbers().toArray()));
         }
@@ -15,26 +14,20 @@ public class Output {
     }
 
     public static void printWinner(int cost) {
-//        System.out.println("당첨 통계\n---");
-        System.out.println("Winner Statistics\n---");
+        System.out.println("당첨 통계\n---");
 
         for (LottoEnum lotto : LottoEnum.values()) {
             if (lotto.getPrizeAmount() == 0) {
                 break;
             }
-
             if (lotto.getMatchCount() == Constant.LOTTO_BONUS_CORRECT) {
-                //System.out.println("5개 일치, 보너스 볼 일치 (" + getPrizeToString(lotto.getPrizeAmount()) + "원) - " + lotto.getWinnerCount() + "개");
-                System.out.println("5 correct, bonus (" + getPrizeToString(lotto.getPrizeAmount()) + ") - " + lotto.getWinnerCount());
+                System.out.println("5개 일치, 보너스 볼 일치 (" + getPrizeToString(lotto.getPrizeAmount()) + "원) - " + lotto.getWinnerCount() + "개");
                 continue;
             }
-
-            //System.out.println(lotto.getMatchCount() + "개 일치 (" + getPrizeToString(lotto.getPrizeAmount()) + "원) - " + lotto.getWinnerCount() + "개");
-            System.out.println(lotto.getMatchCount() + " correct (" + getPrizeToString(lotto.getPrizeAmount()) + ") - " + lotto.getWinnerCount());
+            System.out.println(lotto.getMatchCount() + "개 일치 (" + getPrizeToString(lotto.getPrizeAmount()) + "원) - " + lotto.getWinnerCount() + "개");
         }
 
-        //System.out.println("총 수익률은 %입니다.");
-        System.out.println("Total Percent is " + getRevenue(cost) + "%");
+        System.out.println("총 수익률은 " + getRevenue(cost) + "% 입니다.");
     }
 
     public static double getRevenue(int cost) {
