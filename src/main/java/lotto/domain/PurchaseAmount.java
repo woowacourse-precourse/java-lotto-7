409c05f147b9515ba08079332d47bc548f2b6b9d
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-11-02 16:12:57 +0900

    feat: 구매 금액 도메인 구현

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+
+    private int value;
+
+    public PurchaseAmount(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
