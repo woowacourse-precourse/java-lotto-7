4ebe808bcf99208d98ad3dd498c5a96da5962477
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 02:38:21 +0900

    style: 가독성 위해 공백 추가

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,17 +1,20 @@
 package lotto.constants;
 
 public enum ErrorMessage {
+    ERROR_MESSAGE("[ERROR]"),
     EMPTY_AMOUNT("로또 금액이 입력되지 않았습니다. 다시 입력해주세요."),
     NON_NUMERIC("숫자가 아닙니다. 다시 입력해주세요."),
     UNDER_MIN_AMOUNTS("로또는 1000원이상 구매가능합니다. 1000원 이상의 금액을 입력해주세요."),
     OVER_MAX_AMOUNTS("로또는 최대 10만원까지 구매가능합니다. 10만원 이하의 금액을 입력해주세요."),
     IS_NOT_DIVISIBLE_BY_THOUSAND_WON("1000원 단위로 구매가능합니다. 다시 입력해주세요."),
+
     EMPTY_LOTTO_WINNING_NUMBERS("당첨 로또가 입력되지 않았습니다. 다시 입력해주세요."),
     HAS_CONSECUTIVE_COMMA("숫자 + , 의 형태로 입력해 주세요."),
     ONLY_DIGITS_AND_COMMAS_ALLOWED("숫자 쉼표 이외의 값이 들어가 있습니다. 다시 입력해주세요."),
     INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
     INPUT_LOTTO_NUMBER_OUT_OF_RANGE("당첨번호는 1부터 45사이의 번호여야 합니다."),
     DUPLICATE_NUMBER_MESSAGE("중복된 숫자가 있습니다. 다시 입력해주세요."),
+
     EMPTY_LOTTO_BONUS_NUMBERS("보너스 숫자가 입력되지 않았습니다. 다시 입력해주세요."),
     ONLY_DIGITS_ALLOWED("보너스 숫자는 오직 숫자만 입력할 수 있습니다. 다시 입력해주세요."),
     ONLY_INTEGER_ALLOWED("보너스 번호는 정수값만 입력할 수 있습니다."),
@@ -26,6 +29,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage(){
-        return "[ERROR]" + message;
+        return ERROR_MESSAGE + message;
     }
 }
