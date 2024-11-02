0ff068339908c706a24374f9b82e893ab4f05c20
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-11-02 23:42:03 +0900

    style(InitMessages): InputMessages에서 이름 변경

--- src/main/java/lotto/util/InitMessages.java
+++ src/main/java/lotto/util/InitMessages.java
@@ -1,18 +1,22 @@
 package lotto.util;
 
-public enum InputMessages {
-    INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    PURCHASE_RESULT("%d개를 구매했습니다."),
+public enum InitMessages {
+    INPUT_PURCHASE_AMOUNTS("구입금액을 입력해 주세요."),
+    PURCHASE_AMOUNTS("%d개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     private final String message;
 
-    InputMessages(String message) {
+    InitMessages(String message) {
         this.message = message;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public String getMessage(int amounts) {
+        return String.format(message, amounts);
     }
 }
