092814bc78d2c0d0c775ef723828a7098347d05d
howareyoo <newworld121@naver.com>
2024-11-03 04:42:54 +0900

    feat: add winning value enum class

---
+++ src/main/java/lotto/WinningRank.java
@@ -0,0 +1,33 @@
+package lotto;
+
+public enum WinningRank {
+    FIRST(6, false,"2,000,000,000"),
+    SECOND(5, true,"30,000,000원"),
+    THIRD(5, false,"1,500,000원"),
+    FOURTH(4, false,"50,000원"),
+    FIFTH(3, false, "5,000원"),
+    ;
+
+    private final Integer correct;
+    private final Boolean isBonus;
+    private final String price;
+
+    WinningRank(int correct, boolean isBonus, String price) {
+        this.correct = correct;
+        this.isBonus = isBonus;
+        this.price = price;
+    }
+
+    public int getCorrectCount() {
+        return correct;
+    }
+
+    public boolean isBonus() {
+        return isBonus;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+}
