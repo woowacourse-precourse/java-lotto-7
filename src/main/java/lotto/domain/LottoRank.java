b9824a0bee3c9ea7831480170dbb1938985e6cde
yyysolhhh <solbaram37@gmail.com>
2024-11-02 22:48:28 +0900

    feat(LottoRank): 당첨 기준과 금액을 enum으로 추가
    matchCount
    - 일치 개수
    
    bonuxMatch
    - 보너스 일치 여부
    
    prize
    - 상금

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+public enum LottoRank {
+    MATCH_6(6, false, 2_000_000_000),
+    MATCH_5_BONUS(5, true, 30_000_000),
+    MATCH_5(5, false, 1_500_000),
+    MATCH_4(4, false, 50_000),
+    MATCH_3(3, false, 5_000);
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
+
+    LottoRank(int matchCount, boolean bonusMatch, int prize) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
