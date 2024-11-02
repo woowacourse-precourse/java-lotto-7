8376598ca5a318ff24a5dbf80c64e95ee721a4e9
Sinorang <sinholang@naver.com>
2024-11-02 15:50:03 +0900

    test: LottoTest 로또 번호 중복 검증 테스트 추가

--- src/main/java/lotto/constants/ErrorMessageConstants.java
+++ src/main/java/lotto/constants/ErrorMessageConstants.java
@@ -10,11 +10,11 @@ public class ErrorMessageConstants {
     public static final String INVALID_NON_POSITIVE_PURCHASE_AMOUNT = ERROR_PREFIX + "구입 금액은 양의 정수로 입력해야 합니다.";
     public static final String INVALID_LOTTO_AMOUNT_UNIT = ERROR_PREFIX + "구입 금액은 1,000원 단위로 입력해야 합니다.";
 
-    public static final String EMPTY_WINNING_NUMBERS = ERROR_PREFIX + "당첨 번호는 공백일 수 없습니다.";
-    public static final String INVALID_WINNING_NUMBER_FORMAT = ERROR_PREFIX + "당첨 번호는 숫자로 입력해야 합니다.";
-    public static final String INVALID_WINNING_NUMBER_COUNT = ERROR_PREFIX + "당첨 번호는 6개를 입력해야 합니다.";
-    public static final String INVALID_WINNING_NUMBER_RANGE = ERROR_PREFIX + "당첨 번호는 1부터 45 사이의 숫자를 입력해야 합니다.";
-    public static final String INVALID_WINNING_NUMBER_DUPLICATE = ERROR_PREFIX + "당첨 번호는 중복될 수 없습니다.";
+    public static final String EMPTY_WINNING_NUMBERS = ERROR_PREFIX + "로또 번호는 공백일 수 없습니다.";
+    public static final String INVALID_WINNING_NUMBER_FORMAT = ERROR_PREFIX + "로또 번호는 숫자로 입력해야 합니다.";
+    public static final String INVALID_WINNING_NUMBER_COUNT = ERROR_PREFIX + "로또 번호는 6개여야 합니다.";
+    public static final String INVALID_WINNING_NUMBER_RANGE = ERROR_PREFIX + "로또 번호는 1부터 45 사이의 숫자를 입력해야 합니다.";
+    public static final String INVALID_WINNING_NUMBER_DUPLICATE = ERROR_PREFIX + "로또 번호는 중복될 수 없습니다.";
     public static final String INVALID_BONUS_NUMBER_RANGE = ERROR_PREFIX + "보너스 번호는 1부터 45 사이의 숫자를 입력해야 합니다.";
     public static final String INVALID_BONUS_NUMBER_DUPLICATE = ERROR_PREFIX + "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
 
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.constants.ErrorMessageConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,18 +9,29 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private static final List<Integer> INVALID_LOTTO_SIZE_EXCEEDS = List.of(1, 2, 3, 4, 5, 6, 7);
+    private static final List<Integer> DUPLICATE_NUMBERS = List.of(1, 2, 3, 4, 5, 5);
+    private static final List<Integer> OUT_OF_RANGE_LOW = List.of(0, 2, 3, 4, 5, 6);
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(INVALID_LOTTO_SIZE_EXCEEDS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.INVALID_WINNING_NUMBER_COUNT);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(DUPLICATE_NUMBERS))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.INVALID_WINNING_NUMBER_DUPLICATE);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(OUT_OF_RANGE_LOW))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessageConstants.INVALID_WINNING_NUMBER_RANGE);
+    }
 }
