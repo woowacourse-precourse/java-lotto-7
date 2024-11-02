eda0f3024d0986c7ed00e4e9dcff3772453f40e3
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-02 22:40:13 +0900

    feat(OutputView): 개수일치 문장 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import java.util.List;
 
 public class OutputView {
-    private static final int TREEE_CORRECT = 3;
+    private static final int THREE_CORRECT = 3;
     private static final int FOUR_CORRECT = 4;
     private static final int FIVE_CORRECT = 5;
     private static final int FIVE_AND_BONUS_CORRECT = 7;
@@ -20,11 +20,11 @@ public class OutputView {
         System.out.println("---");
     }
     public static void printCorrect(int[] count){
-        System.out.println("3개 일치 (5,000원) - "+count[TREEE_CORRECT]);
-        System.out.println("4개 일치 (50,000원) - "+count[FOUR_CORRECT]);
-        System.out.println("5개 일치 (1,500,000원) - "+count[FIVE_CORRECT]);
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+count[FIVE_AND_BONUS_CORRECT]);
-        System.out.println("6개 일치 (2,000,000,000원) - "+count[SIX_CORRECT]);
+        System.out.println("3개 일치 (5,000원) - "+count[THREE_CORRECT]+"개");
+        System.out.println("4개 일치 (50,000원) - "+count[FOUR_CORRECT]+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+count[FIVE_CORRECT]+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+count[FIVE_AND_BONUS_CORRECT]+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+count[SIX_CORRECT]+"개");
     }
     public static void printRate(double EarnRate){
         System.out.println("총 수익률은 "+String.format("%.1f", EarnRate) + "%입니다.");
