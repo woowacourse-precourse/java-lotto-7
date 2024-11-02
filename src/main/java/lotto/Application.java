4133d49056b8556769b38073733214c71c05dd71
yujin-zero <poly30@naver.com>
2024-11-02 21:54:15 +0900

    fix(output) : 당첨 번호 출력 누락 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,4 +1,5 @@
 package lotto;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -14,9 +15,12 @@ public class Application {
 
         int purchaseAmountInt = validatePurchaseAmount(purchaseAmount);
 
+        System.out.println();
+        System.out.println(purchaseAmountInt / 1000 + "개를 구매했습니다.");
         for (int i = 0; i < purchaseAmountInt / 1000; i++) {
             List<Integer> lotto = pickLottoNumber();
             lotto = sortAscendingInteger(lotto);
+            System.out.println(lotto);
         }
 
         String inputWinningNumbers;
@@ -30,7 +34,7 @@ public class Application {
 
     public static List<Integer> stringListToIntegerList(List<String> stringList) {
         List<Integer> intList = new ArrayList<>();
-        for(String s : stringList) {
+        for (String s : stringList) {
             intList.add(Integer.parseInt(s));
         }
         return intList;
@@ -50,10 +54,10 @@ public class Application {
         int purchaseAmountInt;
         try {
             purchaseAmountInt = Integer.parseInt(purchaseAmount);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 정수여야합니다");
         }
-        if (purchaseAmountInt <= 0 || purchaseAmountInt %1000 > 0 ) {
+        if (purchaseAmountInt <= 0 || purchaseAmountInt % 1000 > 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위여야합니다.");
         }
 
