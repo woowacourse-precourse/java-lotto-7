37bcba5cbc19b3e141cd232f1bb3da41546508f1
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-02 21:13:58 +0900

    feat(LottoController): 구입 금액 입력 기능 추가

---
+++ src/main/java/lotto/Controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.Controller;
+
+import lotto.View.InputView;
+
+public class LottoController {
+
+    public void lottoController() {
+        int price = InputView.priceInput();
+    }
+}
+
