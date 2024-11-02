a685fa4a8115c326080ebbbbbd8c98e8f9f4764e
bbandm <hjerachel@gmail.com>
2024-11-03 01:16:34 +0900

    feat(Input): 입력받은 금액 정수로 파싱 기능 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,9 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private final String SYSTEM_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-
+    int parsedPrice;
     public void getPriceInput() {
         System.out.println(SYSTEM_PRICE_INPUT_MESSAGE);
         String priceInput = Console.readLine();
+        makePriceInputInteger(priceInput);
+    }
+
+    private void makePriceInputInteger(String priceInput) {
+        parsedPrice = Integer.parseInt(priceInput);
     }
 }
