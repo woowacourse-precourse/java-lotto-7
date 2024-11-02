ccae7b1849318207dadad1beb9718c5fc9d0f02a
10 <dinophy@nate.com>
2024-11-02 13:56:41 +0900

    refactor: 출력 테스트 보완

--- src/test/java/lotto/format/IssueLottoMessageFormatterTest.java
+++ src/test/java/lotto/format/IssueLottoMessageFormatterTest.java
@@ -1,22 +1,43 @@
 package lotto.format;
 
 import lotto.domain.IssueLotto;
-import lotto.strategy.AutoIssueStrategy;
 import lotto.strategy.IssueStrategy;
+import lotto.support.ManualIssueStrategy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class IssueLottoMessageFormatterTest {
 
     @Test
     void 발행한_로또_번호_출력() {
         //given
-        IssueStrategy issueStrategy = AutoIssueStrategy.getInstance();
+        IssueStrategy issueStrategy = ManualIssueStrategy.ofList(
+                List.of(10, 11, 12, 13, 14, 15),
+                List.of(20, 21, 22, 23, 24, 25),
+                List.of(30, 31, 32, 33, 34, 35),
+                List.of(40, 41, 42, 43, 44, 45)
+        );
         IssueLotto issueLotto = new IssueLotto(8, issueStrategy);
 
         //when
         String format = new IssueLottoMessageFormatter().format(issueLotto);
 
         //then
-        System.out.println(format);
+        assertThat(format).isEqualTo(answer());
+    }
+
+    static String answer() {
+        return "[10, 11, 12, 13, 14, 15]\n" +
+                "[20, 21, 22, 23, 24, 25]\n" +
+                "[30, 31, 32, 33, 34, 35]\n" +
+                "[40, 41, 42, 43, 44, 45]\n" +
+                "[10, 11, 12, 13, 14, 15]\n" +
+                "[20, 21, 22, 23, 24, 25]\n" +
+                "[30, 31, 32, 33, 34, 35]\n" +
+                "[40, 41, 42, 43, 44, 45]";
     }
 }
--- src/test/java/lotto/format/LottoRankMessageFormatterTest.java
+++ src/test/java/lotto/format/LottoRankMessageFormatterTest.java
@@ -1,11 +1,14 @@
 package lotto.format;
 
 import lotto.domain.LottoRank;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.WritableAssertionInfo;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static lotto.domain.LottoRank.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoRankMessageFormatterTest {
 
@@ -18,6 +21,14 @@ class LottoRankMessageFormatterTest {
         String format = new LottoRankMessageFormatter().format(lottoRanks);
 
         //then
-        System.out.println(format);
+        assertThat(format).isEqualTo(answer());
+    }
+
+    static String answer() {
+        return "3개 일치 (5,000원) - 2개\n" +
+                "4개 일치 (50,000원) - 1개\n" +
+                "5개 일치 (1,500,000원) - 1개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 0개";
     }
 }
