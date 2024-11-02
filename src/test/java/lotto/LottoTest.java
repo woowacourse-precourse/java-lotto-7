32c6897043d6b826d16d11a0d85784735d2fe8cb
tentenball <lth8905@gmail.com>
2024-11-02 18:39:33 +0900

    test: 로또 클래스에 대한 테스트 작성
    * 클래스 생성, 범위를 벗어난 숫자에 대한 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,8 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import lotto.domains.lotto.Lotto;
 
 class LottoTest {
+    @DisplayName("로또 클래스가 올바르게 생성된다.")
+    @Test
+    void 로또_클래스가_올바르게_생성된다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto).isInstanceOf(Lotto.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +32,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 로또 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_로또_범위를_벗어난_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
