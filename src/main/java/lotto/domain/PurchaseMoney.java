500c4522e6d431203a012cd4cd2e4107272e4b58
RedJunKi <bing8821@naver.com>
2024-11-02 19:32:46 +0900

    feat: 구매 금액의 예외를 처리하라
    PurchaseMoney.java: 나누어 떨어지지 않는 경우, 양수가 아닌 경우, 구매가능 금액을 초과하는 경우 예외처리 기능을 구현했다.
    README.md: 해당 요구사항에 체크했다.

--- src/main/java/lotto/domain/PurchaseMoney.java
+++ src/main/java/lotto/domain/PurchaseMoney.java
@@ -14,8 +14,26 @@ public class PurchaseMoney {
     }
 
     private void validateMoney(int money) {
+        validateDivideLottoPrice(money);
+        validatePositiveNumber(money);
+        validateTotalAmount(money);
+    }
+
+    private void validateDivideLottoPrice(int money) {
         if (money % NumberConstant.LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(CAN_NOT_DIVIDE_MONEY.getMessage());
+        }
+    }
+
+    private void validatePositiveNumber(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("양수만 입력 가능합니다.");
+        }
+    }
+
+    private void validateTotalAmount(int money) {
+        if (money > 100_000) {
+            throw new IllegalArgumentException("사장님 재미로만 하세요");
         }
     }
 
