bafcd821c1ea04a64059518208859e4351a86f52
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-02 14:38:44 +0900

    refactor(MainController): 이전된 메서드 삭제
    - LottoTransactionController로 이전한 메서드를 삭제
    - 사용 안하는 코드 삭제

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,42 +1,17 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import lotto.model.Lotto;
 
 public class MainController {
-  final int LOTTO_PRICE = 1000; // TODO MainController 말고 다른 곳에 정의할지 생각
+  final private LottoTransactionController lottoTransactionController;
 
   public MainController() {
+
+    this.lottoTransactionController = new LottoTransactionController();
   }
 
-  // TODO amount 대한 vaildate는 sellLotto 또는 inputView에서 할지 생각
-  public List<Lotto> sellLotto(int amount) {
-    List<Lotto> lottos = new ArrayList<>();
-    int count = amount / LOTTO_PRICE;
+  public void startLottoBusiness() {
+    // TODO 새롭게 코드 구현
 
-    while (lottos.size() < count) {
-      Lotto lotto = produceLotto();
-      lottos.add(lotto);
-    }
-    System.out.println(count + "개를 구매했습니다."); // TODO 출력 처리 이동
-
-    return lottos;
-  }
-
-  private Lotto produceLotto() {
-    Set<Integer> lottoNumbers = new HashSet<>();
-
-    while (lottoNumbers.size() < 6) {
-      int number = Randoms.pickNumberInRange(1, 45);
-      lottoNumbers.add(number);
-    }
-    List<Integer> numbers = new ArrayList<>(lottoNumbers);
-
-    return new Lotto(numbers);
   }
 
 }
