6b3f06d46974cc84feb6e28ed2778b6b092fded9
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-11-02 13:00:43 +0900

    fix: 수정 가능한 List로 반환되게 변경

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -2,11 +2,12 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BuyLotto {
-    List<List<Integer>> lottoList; // 여러 개의 로또 번호를 저장할 리스트
+    private List<List<Integer>> lottoList; // 여러 개의 로또 번호를 저장할 리스트
 
     public BuyLotto(int numberOfLotto) {
         lottoList = new ArrayList<>();
@@ -14,15 +15,17 @@ public class BuyLotto {
     }
 
     private void buyLotto(int numberOfLotto) {
-        for (int i = 0; i < numberOfLotto; i++) {
-            List<Integer> singleLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6); // 중복되지 않는 6개의 번호 생성
-            lottoList.add(singleLotto);
-        }
+        lottoList = IntStream.range(0, numberOfLotto)
+                .mapToObj(i -> Randoms.pickUniqueNumbersInRange(1, 45, 6) // 중복되지 않는 6개의 번호 생성
+                        .stream()
+                        .sorted()
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
     }
 
     public void printLottoList() {
+        System.out.println("발행한 로또 번호:");
         for (List<Integer> integers : lottoList) {
-            Collections.sort(integers);
             System.out.println(integers);
         }
     }
@@ -30,4 +33,5 @@ public class BuyLotto {
     public List<List<Integer>> getLottoList() {
         return lottoList;
     }
+
 }
