fafeb1a42f1ae599d4c45bc7a558a1371c5a0ae9
chahyunsoo <chahs1012@naver.com>
2024-11-02 23:15:14 +0900

    feat(LottoController) : 로또 게임 시작을 담당하는 클래스 생성 및 Application 정의
    * 로또 게임 실행을 담당하는 LottoController 클래스를 생성하고, 이를 주어진 Application 클래스에 의존성을 주입하여 메인 클래스인Application 클 정의하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.application.LottoController;
+import lotto.view.impl.InputViewImpl;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new InputViewImpl());
+        lottoController.run();
+
     }
 }
