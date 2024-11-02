82de37c590ec7b99252a7726607717e06d2a1651
leeedohyun <ldh19738@gmail.com>
2024-11-02 14:33:42 +0900

    feat: 입력된 당첨 번호 및 보너스 번호가 숫자가 아닌 경우 예외 처리

---
+++ src/main/java/lotto/io/InputConverter.java
@@ -0,0 +1,29 @@
+package lotto.io;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lotto.lotto.LottoNumber;
+import lotto.lotto.WiningNumbers;
+
+public class InputConverter {
+
+    public WiningNumbers convertToWiningNumbers(String[] splitNumbers) {
+        List<Integer> numbers = Arrays.stream(splitNumbers)
+                .map(this::convertToInteger)
+                .toList();
+        return new WiningNumbers(numbers);
+    }
+
+    public LottoNumber convertToBonusNumber(String number) {
+        return new LottoNumber(convertToInteger(number));
+    }
+
+    private int convertToInteger(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
+}
