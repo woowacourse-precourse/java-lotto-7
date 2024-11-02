6451a258aa4fecf9bd2b57af5731ad3184b87d69
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-02 14:32:14 +0900

    feat(LottoTransactionController): 새로운 컨트롤러 추가
    - MainController 에서 사용 중인 메서드 동일하게 사용
    - MainController에 책임을 줄이기 위해 사용
    
    기능 구현 중 많은 기능이 MainController에 몰릴 것으로 예상되기에 분활을 하고자 만들었습니다

---
+++ src/main/java/lotto/controller/LottoTransactionController.java
@@ -0,0 +1,49 @@
+package lotto.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.model.Lotto;
+import lotto.model.LottoTransaction;
+
+public class LottoTransactionController {
+  final int LOTTO_PRICE = 1000; // TODO MainController 말고 다른 곳에 정의할지 생각
+
+  final LottoTransaction lottoTransaction;
+
+  public LottoTransactionController() {
+    this.lottoTransaction = new LottoTransaction();
+  }
+
+  // TODO amount 대한 vaildate는 sellLotto 또는 inputView에서 할지 생각
+  public List<Lotto> sellLotto(int amount) {
+    List<Lotto> lottos = new ArrayList<>();
+    int count = amount / LOTTO_PRICE;
+
+    while (lottos.size() < count) {
+      Lotto lotto = produceLotto();
+      lottos.add(lotto);
+    }
+    System.out.println(count + "개를 구매했습니다."); // TODO 출력 처리 이동
+
+    return lottos;
+  }
+
+  public void compareWinningNumbers() {
+
+  }
+
+  private Lotto produceLotto() {
+    Set<Integer> lottoNumbers = new HashSet<>();
+
+    while (lottoNumbers.size() < 6) {
+      int number = Randoms.pickNumberInRange(1, 45);
+      lottoNumbers.add(number);
+    }
+    List<Integer> numbers = new ArrayList<>(lottoNumbers);
+
+    return new Lotto(numbers);
+  }
+}
