1b8a31dc1c57e5860f95e92e02f36bef4b3b77f5
RedJunKi <bing8821@naver.com>
2024-11-02 15:42:25 +0900

    feat: 보너스 번호를 입력받아라
    WinningNumber.java: 보너스 번호 필드 생성, 보너스 번호 입력시 검증 로직을 구현했다.
    README.md: 해당 요구사항에 체크했다.

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -10,6 +10,7 @@ import static lotto.constant.NumberConstant.*;
 public class WinningNumber {
 
     SequencedSet<Integer> winningNumber;
+    private Integer bonusNumber;
 
     public WinningNumber(SequencedSet<Integer> winningNumber) {
         validateSize(winningNumber);
@@ -19,6 +20,18 @@ public class WinningNumber {
     private void validateSize(SequencedSet<Integer> winningNumber) {
         if (winningNumber.size() != LOTTO_NUMBER_PICK_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.NOT_MATCHED_WINNING_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    public void addBonusNumber(final int number) {
+        validateDuplicateNumber(number);
+
+        this.bonusNumber = number;
+    }
+
+    private void validateDuplicateNumber(int number) {
+        if (winningNumber.contains(number)) {
+            throw new IllegalArgumentException("보너스 숫자는 당첨 번호와 다르게 입력해주세요.");
         }
     }
 }
