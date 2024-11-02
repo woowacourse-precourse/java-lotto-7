06b2d87ac747ee3fad4e1eae52e66dd54900298f
rootTiket <l20078829@gmail.com>
2024-11-02 16:44:57 +0900

    feat: 입력된 매수만큼 로또를 생성하는 기능 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,9 +1,21 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.Lotto;
 
 public class LottoMachine {
     private final NumberGenerater numberGenerater = new NumberGenerater(6);
+
+    public List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateLotto());
+        }
+
+        return lottos;
+    }
 
     private Lotto generateLotto() {
         return new Lotto(numberGenerater.createRandomNumbers());
