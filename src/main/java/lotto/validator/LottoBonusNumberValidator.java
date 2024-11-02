6915b60729c0ebcb46f173b0aef2edb30b4c63ac
신관규 <soundbar91@gmail.com>
2024-11-02 13:32:01 +0900

    feat: 로또 보너스 번호 유효성 검증 클래스 추가

---
+++ src/main/java/lotto/validator/LottoBonusNumberValidator.java
@@ -0,0 +1,40 @@
+package lotto.validator;
+
+import lotto.dto.WinningLottoNumbers;
+
+public class LottoBonusNumberValidator {
+    private static final String LOTTO_BONUS_NUMBER_REGEX = "^[0-9]+$";
+
+    private LottoBonusNumberValidator() {
+    }
+
+    public static void validateLottoBonusNumber(String inputLottoBonusNumber, WinningLottoNumbers winningLottoNumbers) {
+        try {
+            int lottoBonusNumber = Integer.parseInt(inputLottoBonusNumber);
+            checkLottoBonusNumberRange(lottoBonusNumber);
+            checkDuplicateInWinningLottoNumber(lottoBonusNumber, winningLottoNumbers);
+        } catch (NumberFormatException e) {
+            checkIncludeSpecialCharacters(inputLottoBonusNumber);
+            throw new IllegalArgumentException("로또 당첨 번호는 0 이상 45 이하입니다");
+        }
+    }
+
+    private static void checkLottoBonusNumberRange(int lottoBonusNumber) {
+        if (0 >= lottoBonusNumber || lottoBonusNumber > 45) {
+            throw new IllegalArgumentException("로또 보너스 번호는 0 이상 45 이하입니다");
+        }
+    }
+
+    private static void checkIncludeSpecialCharacters(String lottoBonusNumber) {
+        if (!lottoBonusNumber.matches(LOTTO_BONUS_NUMBER_REGEX)) {
+            throw new IllegalArgumentException("로또 보너스 번호는 숫자로 입력해야 합니다.");
+        }
+    }
+
+    private static void checkDuplicateInWinningLottoNumber(int lottoBonusNumber,
+                                                           WinningLottoNumbers winningLottoNumbers) {
+        if (winningLottoNumbers.winningLottoNumber().contains(lottoBonusNumber)) {
+            throw new IllegalArgumentException("로또 보너스 번호와 로또 당첨 번호가 중복됩니다.");
+        }
+    }
+}
