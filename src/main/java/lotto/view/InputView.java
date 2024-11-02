a6ec90abc007b10ce7474a269881db0f8a053f87
fbehddn <tom0611@naver.com>
2024-11-02 18:57:02 +0900

    feat: 입력 금액이 숫자가 아닐시 예외 반환 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,8 +8,14 @@ import lotto.parsers.Parser;
 public class InputView {
     public int inputMoney() {
         System.out.println("구입 금액을 입력해주세요.");
-        return Integer.parseInt(readLine());
+        String input = readLine();
+        while (!Parser.isInteger(input)) {
+            System.out.println("[ERROR] 숫자를 입력해주세요.");
+            input = readLine();
+        }
+        return Integer.parseInt(input);
     }
+
 
     public List<Integer> inputWinningNumber() {
         System.out.println("당첨 번호를 입력해주세요.");
