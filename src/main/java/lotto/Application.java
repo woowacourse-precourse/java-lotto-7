36b63a0497a1cbe679eaaa90e5a5aad5d882f787
SukJin Kim <rkathrwks@gmail.com>
2024-11-03 01:11:36 +0900

    feat(application): application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoMachineController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachineController lottoMachineController = new LottoMachineController(new InputView(), new OutputView());
+        lottoMachineController.run();
     }
 }
