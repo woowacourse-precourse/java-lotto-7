7f6181d245f9112ee1bb84b019f677ce62dfde20
fbehddn <tom0611@naver.com>
2024-11-02 18:00:45 +0900

    feat: WinningLotto 생성할 때 적합한 랜덤로또번호와 보너스번호인지 검증 로직 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningLotto {
     private final Lotto winningLotto;
@@ -8,6 +10,22 @@ public class WinningLotto {
 
     public WinningLotto(List<Integer> winningNumber, Integer bonusNumber) {
         this.winningLotto = new Lotto(winningNumber);
+        validateDuplicate(winningNumber, bonusNumber);
+        validateBonusNumberInRange(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumberInRange(Integer bonusNumber) {
+        if (bonusNumber <= 0 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 정수여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> winningNumber, Integer bonusNumber) {
+        Set<Integer> lottoNumbers = new HashSet<>(winningNumber);
+        lottoNumbers.add(bonusNumber);
+        if (lottoNumbers.size() != 7) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다");
+        }
     }
 }
