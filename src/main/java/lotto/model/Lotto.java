7063cb208ee61f498a137a2e89e7aa7d414bbf7f
junho767 <junho767@naver.com>
2024-11-02 20:08:58 +0900

    feat: 로또 번호 리스트에서 숫자의 범위 검증 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,10 +7,14 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
         validDuplicates(numbers);
         this.numbers = numbers;
     }
@@ -27,11 +31,21 @@ public class Lotto {
         }
     }
 
+    private void validateRange(List<Integer> numbers) {
+        if (isOutOfRange(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_OUT_OF_RANGE.toString());
+        }
+    }
+
     private boolean hasDuplicates(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         return uniqueNumbers.size() < numbers.size();
     }
 
+    private boolean isOutOfRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
+    }
 
     public List<Integer> getNumbers() {
         return numbers;
