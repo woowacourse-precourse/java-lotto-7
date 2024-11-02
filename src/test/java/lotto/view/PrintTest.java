7b2a9a110a26c3942f3ab869b097dfca48e429f4
fortune-man <wnguddl96@knou.ac.kr>
2024-11-02 23:10:00 +0900

    test(PrintTest) : 출력 테스트 시나리오 작성을 위해 실패하는 테스트 추가

--- src/test/java/lotto/view/PrintTest.java
+++ src/test/java/lotto/view/PrintTest.java
@@ -42,17 +42,43 @@ class PrintTest {
   @DisplayName("사용자 입력을 요청받은만큼 로또를 발행하여 응답하면 응답 결과를 출력한다")
   @Test
   public void printGeneratedTest() throws Exception{
-      //given
-    List<Integer> generated = handler.generateLotto(request);
-    print.printGenerated(request);
-    //when
-
-    //then
+    // 출력값 예시
+    // 사용자 입력을 읽으면
+    // 입력 값에 기반하여 요청을 전달하고
+    // 요청받은만큼 로또를 발행한다
+    // 로또 발행 결과를 전달함으로써 요청값에 응답한다
+    // 응답 내용을 출력한다
+    // 실제 출력값
+    // 예시와 실제값 검증
   }
 
+  // 이 테스트는 쪼개야 하는건가 아니면 함께 출력하기 위해 통합해야 하는건가..???
   @DisplayName("당첨 번호 / 보너스 번호 비교 결과를 조회하여 각각 당첨 통계와 수익률을 출력하고 애플리케이션 종료")
   @Test
   void printRevenueTest() throws Exception{
+    // 일치성 결과 예시
+    String givenResult = "3개 일치 (5,000원) - 1개\n" +
+        "4개 일치 (50,000원) - 0개\n" +
+        "5개 일치 (1,500,000원) - 0개\n" +
+        "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n" +
+        "6개 일치 (2,000,000,000원) - 0개";
 
+    // 수익률 결과 예시
+    String givenRevenue = "총 수익률은 62.5%입니다.";
+
+    // 실제 일치성 결과 조회
+    String actualResult = handler.compareNumbersResult(actualLotto, winning, bonus);
+    // 실제 수익률 결과 조회
+    String actualRevenue = handler.getResult(handler.calculateRevenue());
+
+    // 실제 일치성 결과 출력
+    print.printResult(actualResult);
+    // 실제 수익률 결과 출력
+    print.printRevenue(actualRevenue);
+
+    assertEquals(getOutput(), actualResult);
+    assertEquals(getOutput(), actualRevenue);
   }
+
+
 }
