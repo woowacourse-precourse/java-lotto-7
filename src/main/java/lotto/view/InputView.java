ee5278cb2a32d28e0730ebf9a98ae5242f005b2d
eunseobb <seobseob0810@naver.com>
2024-11-03 00:49:12 +0900

    feat(view): 로또 번호 입력 메서드 추가
    InputView에 당첨 번호와 보너스 번호를 입력하는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,10 +6,24 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public String readAmount(){
+    public String readAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String amount = readLine();
         return amount;
+    }
+
+    public List<String> readNumbers() {
+        List<String> numbers = new ArrayList<>();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String rawNumbers = readLine();
+        numbers.add(rawNumbers);
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = readLine();
+        numbers.add(bonusNumber);
+
+        return numbers;
     }
 
 }
