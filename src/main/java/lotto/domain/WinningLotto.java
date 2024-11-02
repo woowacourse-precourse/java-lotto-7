113d76e0d17cba5664e71fccafea8bafe2e09182
J0YERIM <chosarah70@gmail.com>
2024-11-03 01:42:10 +0900

    feat(domain): 당첨 번호와 보너스 번호를 관리하는 WinningLotto 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+public class WinningLotto {
+
+    private static final int MIN_BONUS_NUMBER = 1;
+    private static final int MAX_BONUS_NUMBER = 45;
+
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validateBonusNumber(bonusNumber, winningNumbers);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber, Lotto winningNumbers) {
+        validateRange(bonusNumber);
+        validateNotDuplicate(bonusNumber, winningNumbers);
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < MIN_BONUS_NUMBER || bonusNumber > MAX_BONUS_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateNotDuplicate(int bonusNumber, Lotto winningNumbers) {
+        if (winningNumbers.get().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        }
+    }
+}
