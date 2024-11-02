5f42a46b648b44f71f180bdf40227dab2f863d71
alpin87 <qortmdals94@naver.com>
2024-11-02 19:57:43 +0900

    refactor: WinningLotto 클래스 연동 및 개선

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -2,52 +2,53 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
-    private final Map<LottoRank, Integer> rankCnt;
+    private static final int PERCENTAGE_MULTIPLIER = 100;
+    private final Map<LottoRank, Integer> rankCounts;
     private final int purchaseAmount;
 
     public LottoResult(int purchaseAmount) {
-        this.rankCnt = new EnumMap<>(LottoRank.class);
+        this.rankCounts = new EnumMap<>(LottoRank.class);
         this.purchaseAmount = purchaseAmount;
         initializeResult();
     }
 
     private void initializeResult() {
         for (LottoRank rank : LottoRank.values()) {
-            rankCnt.put(rank, 0);
+            rankCounts.put(rank, 0);
         }
     }
 
-    private int countMatches(List<Integer> userNumbers, List<Integer> winningNumbers) {
-        return (int) userNumbers.stream()
-                .filter(winningNumbers::contains)
-                .count();
-    }
-
-    public void addWinningResult(Lotto userLotto, Lotto winningLotto, int bonusNumber) {
-        int matchCount = countMatches(userLotto.getNumbers(), winningLotto.getNumbers());
-        boolean hasBonus = matchCount == 5 && userLotto.getNumbers().contains(bonusNumber);
+    public void addWinningResult(Lotto userLotto, WinningLotto winningLotto) {
+        int matchCount = winningLotto.matchNumbers(userLotto);
+        boolean hasBonus = matchCount == 5 && winningLotto.matchBonus(userLotto);
 
         LottoRank rank = LottoRank.valueOf(matchCount, hasBonus);
-        rankCnt.put(rank, rankCnt.get(rank) + 1);
+        updateRankCount(rank);
+    }
+
+    private void updateRankCount(LottoRank rank) {
+        rankCounts.put(rank, rankCounts.get(rank) + 1);
     }
 
     private long calculateTotalPrize() {
-        return rankCnt.entrySet().stream()
-                .mapToLong(entry -> (long) entry.getKey().getAmount() * entry.getValue())
+        return rankCounts.entrySet().stream()
+                .mapToLong(this::calculatePrizeForRank)
                 .sum();
+    }
+
+    private long calculatePrizeForRank(Map.Entry<LottoRank, Integer> entry) {
+        return (long) entry.getKey().getAmount() * entry.getValue();
     }
 
     public double calculateProfitRate() {
         long totalPrize = calculateTotalPrize();
-        return (totalPrize * 100.0) / purchaseAmount;
+        return (totalPrize * PERCENTAGE_MULTIPLIER) / (double) purchaseAmount;
     }
 
     public Map<LottoRank, Integer> getRankCounts() {
-        return Collections.unmodifiableMap(rankCnt);
+        return Collections.unmodifiableMap(rankCounts);
     }
-
 }
