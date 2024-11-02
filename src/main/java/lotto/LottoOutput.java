5e17ea3ece7d6a0a9662ab76248389ec9f664947
문고은 <>
2024-11-03 00:59:33 +0900

    feat(output) : 보너스 번호 입력 요청 메시지 추가

--- src/main/java/lotto/LottoOutput.java
+++ src/main/java/lotto/LottoOutput.java
@@ -6,6 +6,7 @@ public class LottoOutput {
     private final static String PURCHASE_AMOUNT_PROMPT_MESSAGE = "구입금액을 입력해 주세요.";
     private final static String PURCHASED_LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
     private final static String WINNING_NUMBERS_PROMPT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final static String BONUS_NUMBER_PROMPT_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public void displayPurchaseAmountPrompt() {
         System.out.println(PURCHASE_AMOUNT_PROMPT_MESSAGE);
@@ -23,5 +24,9 @@ public class LottoOutput {
 
     public void displayWinningNumbersPrompt() {
         System.out.println(WINNING_NUMBERS_PROMPT_MESSAGE);
+    }
+
+    public void displayBonusNumberPrompt() {
+        System.out.println(BONUS_NUMBER_PROMPT_MESSAGE);
     }
 }
