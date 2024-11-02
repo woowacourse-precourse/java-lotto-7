c9627a2488e41cfd2c9ee13dfcb7aed7ddf26915
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-02 22:44:24 +0900

    fix(Lotto): 불변 리스트에 따른 Lotto 번호 복사
    불변리스트로 Lotto를 생성할시 List를 변경하려고 하면(정렬) 에러가 발생한다.
    따라서 번호를 복사하는 것으로 수정한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
     }
 
