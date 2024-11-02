29f6406ddb63ca862ecd8959247c544bee11d7fa
mirinae700 <mirinae219@gmail.com>
2024-11-02 11:08:30 +0900

    feat(lottoutility) : 당첨번호 문자열 숫자 배열로 반환 기능 추가
    사용자가 입력한 당첨번호를 숫자 배열로 반환합니다.

--- src/main/java/lotto/LottoUtility.java
+++ src/main/java/lotto/LottoUtility.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoUtility {
     public static void validatePurchaseAmount(String input) {
         try {
@@ -10,5 +14,19 @@ public class LottoUtility {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 숫자만 입력하셔야 합니다.");
         }
+    }
+
+    public static List<Integer> stringToWinningNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            numbers = Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력하셔야 합니다.");
+        }
+        return numbers;
     }
 }
