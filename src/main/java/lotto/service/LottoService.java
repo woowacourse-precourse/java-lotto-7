67d303f79820e30ecf00c1cace5af8f40fd8af21
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-03 03:03:46 +0900

    feat(lotto): 로또 생성 기능 추가
    구매한 로또 개수만큼 로또를 생성하는 기능을 구현했습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
 
 public class LottoService {
     private final int LOTTO_PRICE = 1000;
@@ -9,8 +11,17 @@ public class LottoService {
     private final int END_INCLUSIVE = 45;
     private final int NUMBER_OF_LOTTO_NUMBERS = 6;
 
+    public List<Lotto> createLottos(int lottoQuantity) {
+        List<Lotto> lottos = new ArrayList<>();
 
-    public List<Integer> createLottoNumbers() {
+        for (int i = 0; i < lottoQuantity; i++) {
+            lottos.add(new Lotto(createLottoNumbers()));
+        }
+
+        return lottos;
+    }
+
+    private List<Integer> createLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, NUMBER_OF_LOTTO_NUMBERS);
     }
 
