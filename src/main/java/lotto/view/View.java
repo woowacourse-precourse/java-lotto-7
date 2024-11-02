160ed187745da51cd72f535a6cbf4e35aa96bc60
softmoca <acyc0909@gmail.com>
2024-11-02 14:35:33 +0900

    feat : 구입금액 입력값 음수 검증 로직 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -17,6 +17,7 @@ public class View {
     private static void validatePurchaseAmount(String inputValue) {
         validateNumeric(inputValue);
         validateEmpty(inputValue);
+        validateNegative(inputValue);
     }
 
     private static void validateEmpty(String inputValue) {
@@ -32,4 +33,11 @@ public class View {
             throw new IllegalArgumentException("[ERROR] 유효한 숫자 형식이 아닙니다.");
         }
     }
+
+    private static void validateNegative(String inputValue) {
+        if (Long.parseLong(inputValue) < 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 음수가 될 수 없습니다.");
+        }
+    }
+
 }
