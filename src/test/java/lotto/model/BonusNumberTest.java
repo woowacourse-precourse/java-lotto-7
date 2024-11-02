691ac51c8f1ea9c42bc9146086dec9fdb9b69d00
hwangdaesun <hds650588@gmail.com>
2024-11-02 20:28:20 +0900

    refactor : static으로 import 하도록 수정

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -1,7 +1,8 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import lotto.common.ErrorMessage;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,7 +10,7 @@ class BonusNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
     void 보너스_번호는_1에서_45_사이_입니다(int number) {
-        Assertions.assertThatThrownBy(() -> new BonusNumber(number))
+        assertThatThrownBy(() -> new BonusNumber(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBER_RANGE.getMessage());
     }
--- src/test/java/lotto/model/LottoSellerTest.java
+++ src/test/java/lotto/model/LottoSellerTest.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
 import lotto.common.ErrorMessage;
 import lotto.dto.MoneyDTO;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,20 +16,21 @@ class LottoSellerTest {
     private LottoSeller lottoSeller;
 
     @BeforeEach
-    void init(){
+    void init() {
         lottoSeller = new LottoSeller();
     }
+
     @Test
-    void 로또_구입_금액은_1000원_단위이다(){
+    void 로또_구입_금액은_1000원_단위이다() {
         long money = 10000;
         List<Lotto> lottos = lottoSeller.sell(MoneyDTO.from(money));
-        Assertions.assertThat(lottos.size()).isEqualTo(money / LottoSeller.SELL_UNIT);
+        assertThat(lottos.size()).isEqualTo(money / LottoSeller.SELL_UNIT);
     }
 
     @ParameterizedTest
     @ValueSource(longs = {1200, 800, 1004})
-    void 로또_구입_금액이_아닐_경우_예외가_발생한다(long money){
-        Assertions.assertThatThrownBy(() -> lottoSeller.sell(MoneyDTO.from(money)))
+    void 로또_구입_금액이_아닐_경우_예외가_발생한다(long money) {
+        assertThatThrownBy(() -> lottoSeller.sell(MoneyDTO.from(money)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INPUT_MONEY_IS_MULTIPLE_1000.getMessage());
     }
