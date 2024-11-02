ca4c76e39ba73c970d53d6f828395b5b96aae603
kgy1008 <gayeon1008@g.skku.edu>
2024-11-03 00:05:24 +0900

    test: test for lotto seller

---
+++ src/test/java/lotto/model/LottoSellerTest.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoSellerTest {
+    private final LottoSeller lottoSeller = new LottoSeller();
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1500, 300})
+    @DisplayName("금액 오류 테스트")
+    void invalidInputAmount(final int input) {
+        assertThatThrownBy(() -> lottoSeller.sellTo(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
