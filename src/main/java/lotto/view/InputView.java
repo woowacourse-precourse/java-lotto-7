1e55fdca3b3cc731e1c68ff186d503499b5da39a
hyeblee <hyeb1224@naver.com>
2024-11-02 15:29:07 +0900

    feat: 로또 구입금액 입력 구현
    - promptPurchase 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    Scanner sc = new Scanner(System.in);
+
+    public String promptPurchaseAmount(){
+        String purchaseAmount = sc.next();
+        return purchaseAmount;
+    }
+
 }
