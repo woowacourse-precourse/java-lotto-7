523c5107dec74549f80dd35ff7ac0762d87db5c5
sese2204 <sese2204@naver.com>
2024-11-02 20:38:44 +0900

    feat(view): 발행한 로또 수량과 번호를 출력하고 당첨 내역을 출력하는 OutputView 클래스 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lottos;
+import lotto.domain.Prize;
+
+public class OutputView {
+    private static final String WINNING_STATISTICS_HEADER = """
+            당첨 통계
+            ---""";
+    private static final String WINNING_STATISTICS_FORMAT = "%s - %d개";
+
+    public void printPurchaseResult(Lottos lottos) {
+        System.out.println();
+        System.out.println(lottos.toString());
+    }
+
+    public void printWinningStatistics(Map<Prize, Integer> result) {
+        System.out.println();
+        System.out.println(WINNING_STATISTICS_HEADER);
+
+        List<Prize> sortedPrizes = Arrays.asList(
+                Prize.FIFTH,    // 3개 일치
+                Prize.FOURTH,   // 4개 일치
+                Prize.THIRD,    // 5개 일치
+                Prize.SECOND,   // 5개 일치 + 보너스
+                Prize.FIRST     // 6개 일치
+        );
+
+        sortedPrizes.forEach(prize ->
+                System.out.printf(WINNING_STATISTICS_FORMAT + "%n",
+                        prize,
+                        result.getOrDefault(prize, 0))
+        );
+    }
+}
