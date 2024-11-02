77b82444d23418bfd0da5eeb480d86955edec1b7
porori1224 <iminseo031224@gmail.com>
2024-11-02 23:52:25 +0900

    feat(LottoTicket.java): LottoTicket class 생성
    - 입력 받은 금액에 따라 티켓 수를 계산하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,25 +1,49 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class LottoGame {
     private final LottoMachine lottoMachine = new LottoMachine();
-    private final Scanner scanner = new Scanner(System.in);
+    private List<LottoTicket> userTickets;
+
+    public void start() {
+        int amount = inputAmount();
+        int ticketCount = lottoMachine.calculateTicketCount(amount);
+
+        userTickets = generateTickets(ticketCount);
+        printTickets(usterTickets);
+    }
 
     private int inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
 
         try {
             int amount = Integer.parseInt(input);
-            if (amount <= 0 || amount % 1000 != 0) {
-                throw new IllegalArgumentException();
-            }
+            lottoMachine.vaildateAmount(amount);
             return amount;
         } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 구입금액은 숫자로 입력하세요.");
+            System.out.println("[ERROR] 구입 금액은 숫자로 입력 하세요.");
+            return inputAmount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
             return inputAmount();
         }
+    }
+
+    private List<LottoTicket> getUserTickets(int count) {
+        List<LottoTicket> tickets = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            tickets.add(lottoMachine.generateTicket());
+        }
+        return tickets;
+    }
+
+    private void printTickets(List<LottoTicket> tickets) {
+        System.out.println(tickets.size() + "개를 구매했습니다.");
+        tickets.forEach(ticket -> System.out.println(ticket.getSortedNumbers()));
     }
 }
--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,5 +1,28 @@
 package lotto;
 
-public class LottoTicket {
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+public class LottoTicket {
+    private final Set<Integer> numbers;
+
+    public LottoTicket(Set<Integer> numbers) {
+        if (numbers.size() != 6 || numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 6개 숫자여야 합니다.");
+        }
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getSortedNumbers() {
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
+
+    public int matchCount(LottoTicket winningTicket) {
+        return (int) winningTicket.numbers.stream().filter(numbers :: contains).count();
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
 }
