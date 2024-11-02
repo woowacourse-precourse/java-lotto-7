631874d0c7633d1be4e0d413947799b65ba0b4a0
RedJunKi <bing8821@naver.com>
2024-11-02 15:51:24 +0900

    feat: 당첨 조건을 작성하라
    Prize.java: 각 당첨 정보를 구현했다.
    README.md: 해당 요구사항에 체크했다.

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public enum Prize {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000);
+
+    private int matchCount;
+    private boolean matchBonusNumber;
+    private int prizeMoney;
+
+    Prize(int matchCount, boolean matchBonusNumber, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.matchBonusNumber = matchBonusNumber;
+        this.prizeMoney = prizeMoney;
+    }
+}
