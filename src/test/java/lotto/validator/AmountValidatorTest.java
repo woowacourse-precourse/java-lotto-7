3d03e063762913fbb3d338600fbb5aff08d4b713
jaeyeong <zzz26lk@gmail.com>
2024-11-02 18:29:25 +0900

    test: 검증 로직 테스트
    구입 금액 검증 로직이 정상적으로 동작하는지
    테스트하기 위해서 텍스트 코드를 작성한다.

---
+++ src/test/java/lotto/validator/AmountValidatorTest.java
@@ -0,0 +1,31 @@
+package lotto.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AmountValidatorTest {
+
+    @ParameterizedTest
+    @DisplayName("구입 금액이 정수가 아니면 예외를 발생한다.")
+    @ValueSource(strings = {"a", "?", "1.3", "가", "1a"})
+    void amountTest1(String number) {
+        Validator<String> validator = new AmountValidator();
+
+        assertThatThrownBy(() -> validator.validate(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외를 발생한다.")
+    void amountTest2() {
+        Validator<String> validator = new AmountValidator();
+        String amount = "8600";
+
+        assertThatThrownBy(() -> validator.validate(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
