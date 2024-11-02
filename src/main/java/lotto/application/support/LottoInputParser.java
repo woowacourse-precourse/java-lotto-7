e6e18d2dc1a82e02f9c146f48cd3203296ab946d
bellmin <whdlas011203@naver.com>
2024-11-03 02:11:37 +0900

    feat(LottoInputParser): validate 메소드 추가
    - validate : 입력 값에 대한 validate 메소드 추가

--- src/main/java/lotto/application/support/LottoInputParser.java
+++ src/main/java/lotto/application/support/LottoInputParser.java
@@ -1,15 +1,33 @@
 package lotto.application.support;
 
+import static lotto.common.Consts.INPUT_MUST_BE_NUMBER;
+import static lotto.common.Consts.INPUT_MUST_BE_POSITIVE_NUMBER;
+import static lotto.common.Consts.LOTTO_PRICE;
+import static lotto.common.Consts.PURCHASE_AMOUNT_1000_UNIT;
+
 import lotto.common.Errors;
 
 public class LottoInputParser {
 
     public int parseAmount(String amount){
         try{
-            return Integer.parseInt(amount);
+            int parsed = Integer.parseInt(amount);
+            validate(parsed);
+
+            return parsed;
         } catch (NumberFormatException e) {
-            Errors.IllegalArgumentException("금액은 숫자로 입력해야 합니다.");
+            Errors.IllegalArgumentException(INPUT_MUST_BE_NUMBER);
         }
         return -1;
+    }
+
+    private void validate(int amount){
+        if((amount % LOTTO_PRICE) != 0){
+            Errors.IllegalArgumentException(PURCHASE_AMOUNT_1000_UNIT);
+        }
+
+        if(amount < 0){
+            Errors.IllegalArgumentException(INPUT_MUST_BE_POSITIVE_NUMBER);
+        }
     }
 }
