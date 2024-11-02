0ae5600e09fada0d230d9674e064156c773b006a
softmoca <acyc0909@gmail.com>
2024-11-02 13:45:38 +0900

    feat : 사용자 구입금액 입력 프롬프트 메시지 출력 기능 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,22 @@
+package lotto.controller;
+
+import lotto.view.View;
+
+public class LottoController {
+    public void run(){
+        initLotts();
+
+
+
+
+    }
+
+    private void initLotts(){
+        View.promptForPurchaseAmount();
+
+    }
+
+
+
+
+}
---
+++ src/main/java/lotto/view/View.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class View {
+
+    public static void promptForPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+
+
+}
