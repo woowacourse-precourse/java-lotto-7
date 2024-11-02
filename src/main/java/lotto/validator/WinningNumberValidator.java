6529f90d5415772e93d44346c44419dee55ce51d
33jyu33 <jy04169@gmail.com>
2024-11-02 15:20:50 +0900

    fix(WinningNumberValidator): all methods be static
    To use validator without creating objects in other classes

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -4,25 +4,25 @@ import lotto.constant.WinningNumberRule;
 
 public class WinningNumberValidator {
 
-    public void validateInputComma(String input) throws IllegalArgumentException {
+    public static void validateInputComma(String input) throws IllegalArgumentException {
         validateDoubletComma(input);
         validateFirstComma(input);
         validateLastComma(input);
     }
 
-    private void validateDoubletComma(String input) throws IllegalArgumentException {
+    private static void validateDoubletComma(String input) throws IllegalArgumentException {
         if (input.contains(WinningNumberRule.COMMA.getMessage() + WinningNumberRule.COMMA.getMessage())) {
             throw new IllegalArgumentException(WinningNumberRule.DOUBLE_COMMA.getMessage());
         }
     }
 
-    private void validateFirstComma(String input) throws IllegalArgumentException{
+    private static void validateFirstComma(String input) throws IllegalArgumentException{
         if(input.startsWith(WinningNumberRule.COMMA.getMessage())){
             throw new IllegalArgumentException(WinningNumberRule.COMMA_FIRST.getMessage());
         }
     }
 
-    private void validateLastComma(String input) throws IllegalArgumentException{
+    private static void validateLastComma(String input) throws IllegalArgumentException{
         if(input.endsWith(WinningNumberRule.COMMA.getMessage())){
             throw new IllegalArgumentException(WinningNumberRule.COMMA_LAST.getMessage());
         }
