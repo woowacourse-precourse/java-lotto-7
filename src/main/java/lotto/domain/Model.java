aa8feed78195e6349ebc16bc4324c372fc2c6e32
jaeyeong <zzz26lk@gmail.com>
2024-11-02 18:25:01 +0900

    feat: Model 클래스 추가
    로또 생성과 당첨 내역, 수익률을 계산하는
    코드들을 실행하기 위해서
    Model 클래스를 추가한다.

---
+++ src/main/java/lotto/domain/Model.java
@@ -0,0 +1,69 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+import lotto.domain.util.AddBonusNumber;
+import lotto.domain.util.Calculate;
+import lotto.domain.util.CreateLottoList;
+import lotto.domain.util.CreateWinningMap;
+import lotto.domain.util.WinningNumber;
+import lotto.validator.AmountValidator;
+import lotto.validator.Validator;
+
+public class Model {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    private final int amount;
+    private final int count;
+    private final List<Lotto> lottos;
+    private List<Integer> winningNumbers;
+    private final Map<String, Integer> winningMap;
+    private long winningAmount;
+
+    public Model(String amount) {
+        validate(amount);
+        this.amount = Integer.parseInt(amount);
+        count = this.amount / LOTTO_PRICE;
+        lottos = CreateLottoList.create(count);
+        winningMap = CreateWinningMap.create();
+    }
+
+    private void totalWinningAmount() {
+        for (Lotto lotto : lottos) {
+            winningAmount += lotto.winningCount(winningNumbers, winningMap);
+
+        }
+    }
+
+    private void validate(String amount) {
+        Validator<String> validator = new AmountValidator();
+        validator.validate(amount);
+    }
+
+    public String calculate() {
+        return Calculate.calculate(winningAmount, amount);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Map<String, Integer> getWinningMap() {
+        totalWinningAmount();
+        return winningMap;
+    }
+
+    public void setWinningNumbers(String winningNumbers) {
+        this.winningNumbers = WinningNumber.create(winningNumbers);
+    }
+
+    public void setBonusNumber(String bonusNumber) {
+        AddBonusNumber.create(winningNumbers, bonusNumber);
+    }
+
+}
