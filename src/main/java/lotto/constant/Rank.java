7eca0f5f705ef2ad370a7ee42943df2668262ce1
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-02 15:08:22 +0900

    fix(Rank): 상금 long 타입으로 변경
    상품출력시 정수형으로 표시되는 문제가 있어 long 타입으로 변경

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -3,21 +3,21 @@ package lotto.constant;
 import java.util.function.BiFunction;
 
 public enum Rank {
-    FIRST(6, false, 2000000000, (count, bonus) -> count == 6),
-    SECOND(5, true, 30000000, (count, bonus) -> count == 5 && bonus),
-    THIRD(5, false, 1500000, (count, bonus) -> count == 5 && !bonus),
-    FOURTH(4, false, 50000, (count, bonus) -> count == 4),
-    FIFTH(3, false, 5000, (count, bonus) -> count == 3),
+    FIRST(6, false, 2_000_000_000, (count, bonus) -> count == 6),
+    SECOND(5, true, 30_000_000, (count, bonus) -> count == 5 && bonus),
+    THIRD(5, false, 1_500_000, (count, bonus) -> count == 5 && !bonus),
+    FOURTH(4, false, 50_000, (count, bonus) -> count == 4),
+    FIFTH(3, false, 5_000, (count, bonus) -> count == 3),
     NONE(0, false, 0, (count, bonus) -> count <= 2);
 
-    private final int matchingNumberCount;
+    private final int matchingNumber;
     private final boolean isMatchingBonusNumber;
-    private final int prizeMoney;
+    private final long prizeMoney;
     private final BiFunction<Integer, Boolean, Boolean> criteria;
 
-    Rank(int matchingNumberCount, boolean isMatchingBonusNumber, int prizeMoney,
+    Rank(int matchingNumber, boolean isMatchingBonusNumber, long prizeMoney,
          BiFunction<Integer, Boolean, Boolean> criteria) {
-        this.matchingNumberCount = matchingNumberCount;
+        this.matchingNumber = matchingNumber;
         this.isMatchingBonusNumber = isMatchingBonusNumber;
         this.prizeMoney = prizeMoney;
         this.criteria = criteria;
@@ -35,15 +35,15 @@ public enum Rank {
         return criteria.apply(matchedCount, bonus);
     }
 
-    public int getMatchingNumberCount() {
-        return matchingNumberCount;
+    public int getMatchingNumber() {
+        return matchingNumber;
     }
 
     public boolean isMatchingBonusNumber() {
         return isMatchingBonusNumber;
     }
 
-    public int getPrizeMoney() {
+    public long getPrizeMoney() {
         return prizeMoney;
     }
 }
