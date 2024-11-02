7450f8b64624e6f6c045442a43a0a2b1215f37e8
kkonii <119216472+kkonii@users.noreply.github.com>
2024-11-02 16:21:28 +0900

    feat : 무작위 로또 숫자 생성 기능 구현

---
+++ src/main/java/lotto/util/LottoMaker.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoMaker {
+
+    private static final int MINIMUM = 1;
+    private static final int MAXIMUM = 45;
+    private static final int COUNT = 6;
+
+    public static List<Integer> make() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM, MAXIMUM, COUNT);
+    }
+}
---
+++ src/test/java/lotto/util/LottoMakerTest.java
@@ -0,0 +1,31 @@
+package lotto.util;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoMakerTest {
+
+    @Test
+    @DisplayName("무작위로 뽑은 숫자는 서로 중복되지 않는다")
+    void pick_unique_numbers_without_duplication() {
+        List<Integer> numbers = LottoMaker.make();
+
+        Set<Integer> deleted_duplication = new HashSet<>(numbers);
+
+        Assertions.assertEquals(numbers.size(), deleted_duplication.size());
+    }
+
+    @Test
+    @DisplayName("숫자는 지정된 갯수만큼 뽑는다")
+    void pick_numbers_as_fix_size() {
+        int fixedSize = 6;
+
+        List<Integer> numbers = LottoMaker.make();
+
+        Assertions.assertEquals(numbers.size(), fixedSize);
+    }
+}
