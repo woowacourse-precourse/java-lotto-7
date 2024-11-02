08f1bc1172edc09b01e2cb16af80de6f0f983966
JS <zks145@naver.com>
2024-11-02 10:51:41 +0900

    feat : 로또 당첨 번호 입력 문자 -> 숫자 전처리 기능 추가

--- src/main/java/lotto/util/Utils.java
+++ src/main/java/lotto/util/Utils.java
@@ -3,14 +3,26 @@ package lotto.util;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.validation.Validation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.validation.Validation.*;
 
 public class Utils {
 
     public static int changeStringToInt(String number) {
-        Validation.checkInputTypeNumber(number);
+        checkInputTypeNumber(number);
         return Integer.parseInt(number);
+    }
+
+    public static List<Integer> changeStringToIntegerList(String number) {
+        checkInputTypeNumbers(number);
+        return Arrays.stream(number.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> randomLottoNumbers(int start , int end, int size) {
