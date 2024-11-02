595d9b8b5f6c3f21dc0254d316d6223439392c96
chaerish <ccyy8432@naver.com>
2024-11-02 23:29:38 +0900

    feat(Converter): 사용자 입력 값을 정수, 리스트로 변환하는 로직 추가

--- src/main/java/lotto/service/Converter.java
+++ src/main/java/lotto/service/Converter.java
@@ -1,9 +1,26 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.constant.WinningCondition;
 import lotto.dto.MatchingCountResult;
 
 public class Converter {
+    private static String SEPARATOR = ",";
+
+    public static int integerConvert(String number) {
+        return Integer.parseInt(number);
+    }
+
+    public static List<Integer> winningNumbersConvert(String winningNumbers) {
+        String[] splitNumbers = winningNumbers.split(SEPARATOR);
+        List<Integer> convertNumbers = new ArrayList<>();
+        for (String number : splitNumbers) {
+            convertNumbers.add(Integer.parseInt(number));
+        }
+        return convertNumbers;
+    }
+
     public static MatchingCountResult matchingCounterResultConvert(int winningCount, int bonusCount) {
         if (winningCount < 3) {
             return new MatchingCountResult(WinningCondition.NO_MATCH, 0);
@@ -14,12 +31,16 @@ public class Converter {
         if (winningCount == 4) {
             return new MatchingCountResult(WinningCondition.MATCH_4, winningCount);
         }
-        if (winningCount == 5 && bonusCount == 0) {
-            return new MatchingCountResult(WinningCondition.MATCH_5, winningCount);
-        }
-        if (winningCount == 5 && bonusCount == 1) {
-            return new MatchingCountResult(WinningCondition.MATCH_5_BONUS, winningCount);
+        if (winningCount == 5) {
+            return checkBonusNumber(winningCount, bonusCount);
         }
         return new MatchingCountResult(WinningCondition.MATCH_6, winningCount);
+    }
+
+    private static MatchingCountResult checkBonusNumber(int winningCount, int bonusCount) {
+        if (bonusCount == 1) {
+            return new MatchingCountResult(WinningCondition.MATCH_5_BONUS, winningCount);
+        }
+        return new MatchingCountResult(WinningCondition.MATCH_5, winningCount);
     }
 }
