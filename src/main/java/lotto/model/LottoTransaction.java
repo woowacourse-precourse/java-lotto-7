0c2114ef5e921cacb038494a302ddce50627a2a5
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-02 14:25:30 +0900

    feat(LottoTransaction): 로또 거래 model 추가
    - 구입 금액, 구매한 로또 정보 저장에 필요
    
    MainController와 Lotto에서만으로 코드를 짤때 가독성이 떨어지고 한쪽에 기능이 몰릴것으로 예상되어서 책임을 분산 시키기 위해서 사용

---
+++ src/main/java/lotto/model/LottoTransaction.java
@@ -0,0 +1,10 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTransaction {
+  private int amount = 0;
+  private List<Lotto> purchasedLottos = new ArrayList<>();
+
+}
