adedd18f5d41769075a453166b71ed5e77c872d3
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 17:44:36 +0900

    feat(BonusNumber): BonusNumber 클래스에서 보너스 번호 생성 및 접근 기능 추가

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public class BonusNumber {
+    private final int bonusNumber;
+
+    private BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumber from(int bonusNumber) {
+        return new BonusNumber(bonusNumber);
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+}
