e7d4c54074b5246b193124750b34dcefb9236881
= <hyeonhc328@gmail.com>
2024-11-02 20:15:14 +0900

    refactor(LottoTest): 파라미터화 테스트 적용
    테스트 케이스에 대해 비슷한 테스트가 반복되어 ParameterizeTest를 적용함

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,44 +6,51 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
-
-    @Test
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
-    void lottoSizeValidateTest() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다")
-    void lottoNumbersDuplicatedValidateTest() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("로또 번호는 1-45 범위 안에 있지 않으면 예외가 발생한다")
-    void lottoNumberRangeValidateTest() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(List.of(-11, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("로또 번호가 범위 안에서 중복되지 않고 오름차순 정렬되어 저장된다")
-    void lottoTest() throws NoSuchFieldException, IllegalAccessException {
+    @ParameterizedTest
+    @CsvSource({
+            "'6,7,1,2,40,30', '1,2,6,7,30,40'",
+            "'6,5,4,3,2,1', '1,2,3,4,5,6'",
+            "'10,9,8,30,44,21','8,9,10,21,30,44'"
+    })
+    @DisplayName("로또가 성공적으로 생성되면 번호가 오름차순 정렬된다")
+    void lottoTest(String numbersInput, String expected) throws NoSuchFieldException, IllegalAccessException {
         //given
-        ArrayList<Integer> numberInput = new ArrayList<>(List.of(6, 7, 1, 2, 40, 30));
-        ArrayList<Integer> numberExpected = new ArrayList<>(List.of(1, 2, 6, 7, 30, 40));
+        ArrayList<Integer> numbers = convertStringToIntegerList(numbersInput);
+        ArrayList<Integer> expectResult = convertStringToIntegerList(expected);
         //when
-        Lotto lotto = new Lotto(numberInput);
+        Lotto lotto = new Lotto(numbers);
         //then
-        assertThatLottoValidated(lotto, numberExpected);
+        assertThatLottoValidated(lotto, expectResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2,3,4,5,6,7', '[ERROR] 로또 번호는 6개여야 합니다.'",
+            "'1,2,3,4,5,5', '[ERROR] 로또 번호는 중복될 수 없습니다.'",
+            "'1,2,3,4,5,60', '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.'",
+            "'-1,2,3,4,5,6','[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.'"
+    })
+    @DisplayName("로또 번호가 잘못된 경우 예외가 발생하며 메세지가 출력된다.")
+    void validateLottoNumbers(String numbers, String expectedMessage) {
+        //given, when
+        List<Integer> numberList = convertStringToIntegerList(numbers);
+        //then
+        assertThatThrownBy(() -> new Lotto(numberList))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(expectedMessage);
+    }
+
+    private static ArrayList<Integer> convertStringToIntegerList(String input) {
+        return Stream.of(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     private static void assertThatLottoValidated(Lotto lotto, ArrayList<Integer> numbers)
