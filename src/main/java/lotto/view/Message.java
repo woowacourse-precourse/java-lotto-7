a91f625baa89665492041d0cc67fd1123d3dc35f
jjjjjoon <mjj7343@gmail.com>
2024-11-03 02:46:38 +0900

    feat(OutputView): outputview 구현

---
+++ src/main/java/lotto/view/Message.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+public enum Message {
+    PURCHASED_NUMBERS("8개를 구매했습니다."),
+    WINNING_STATISTICS("당첨 통계"),
+    PIECES("개"),
+    WON("원"),
+    LINE_DIVIDE("---"),
+    MATCH_3("3개 일치 (5,000원) - "),
+    MATCH_4("4개 일치 (50,000원) - "),
+    MATCH_5("5개 일치 (1,500,000원) - "),
+    MATCH_5_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    MATCH_6("6개 일치 (2,000,000,000원) - "),
+    TOTAL_PRIZE("총 당첨금: "),
+    PROFIT_RATE("총 수익률은 %.2f%%입니다.");
+
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.dto.LottoDTO;
+import lotto.dto.LottoResultDTO;
+import lotto.dto.LottoStatisticsDTO;
+import java.util.List;
+
+public class OutputView {
+
+    public static void printLottoNumbers(List<LottoDTO> lottoDTOs) {
+        System.out.println(Message.PURCHASED_NUMBERS.getMessage());
+        for (LottoDTO lottoDTO : lottoDTOs) {
+            System.out.println(lottoDTO.getNumbers());
+        }
+    }
+
+    public static void printWinningResults(LottoResultDTO resultDTO) {
+        System.out.println(Message.WINNING_STATISTICS.getMessage());
+        List<Integer> winningCounts = resultDTO.getWinningCounts();
+
+        System.out.println(Message.MATCH_3.getMessage() + winningCounts.get(4) + Message.PIECES.getMessage());
+        System.out.println(Message.MATCH_4.getMessage() + winningCounts.get(3) + Message.PIECES.getMessage());
+        System.out.println(Message.MATCH_5.getMessage() + winningCounts.get(2) + Message.PIECES.getMessage());
+        System.out.println(Message.MATCH_5_BONUS.getMessage() + winningCounts.get(1) + Message.PIECES.getMessage());
+        System.out.println(Message.MATCH_6.getMessage() + winningCounts.get(0) + Message.PIECES.getMessage());
+
+        System.out.println(Message.TOTAL_PRIZE.getMessage() + resultDTO.getTotalPrize() + Message.WON.getMessage());
+    }
+
+    public static void printProfitRate(LottoStatisticsDTO statisticsDTO) {
+        System.out.printf(Message.PROFIT_RATE.getMessage(), statisticsDTO.getProfitRate());
+    }
+}
