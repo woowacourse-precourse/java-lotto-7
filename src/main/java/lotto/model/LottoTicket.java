4e66033bf3acde76b52c7256d14338c59eeb01fa
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 01:58:39 +0900

    feat: 로또 티켓들을 담아둘 수 있는 클래스 작성

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.dto.LottoNumbers;
+import lotto.dto.LottoBundle;
+
+public class LottoTicket {
+    private List<Lotto> lottoTicket;
+
+    public LottoTicket(List<Lotto> lottoTicket) {
+        this.lottoTicket = lottoTicket;
+    }
+
+    public LottoBundle getLottoTicketStatus(){
+        List<LottoNumbers> lottoNumbers = new ArrayList<>();
+        for(Lotto lotto : lottoTicket) {
+            lottoNumbers.add(lotto.getLottoNumbers());
+        }
+        return new LottoBundle(lottoNumbers);
+    }
+}
