5e74220c508e59c7da7d3ff16012b4ae5012a20b
KangGeonyoung <kky6335@gmail.com>
2024-11-02 23:39:02 +0900

    test(InputValidatorTest): InputValidator 테스트 코드 작성

---
+++ src/test/java/lotto/domain/input/InputValidatorTest.java
@@ -0,0 +1,77 @@
+package lotto.domain.input;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputValidatorTest {
+
+    @Test
+    @DisplayName("문자열을 정수형 숫자로 변환")
+    void 문자열_숫자로_변환() {
+        String number = "8000";
+        int convertedNumber = InputValidator.convertInteger(number);
+        assertThat(convertedNumber).isEqualTo(8000);
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아닐 경우")
+    void 구입금액_1000원단위_아닐경우() {
+        int money = 8080;
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.isValidUnit(money);
+        });
+    }
+
+    @Test
+    @DisplayName("문자열을 쉼표 기준으로 분리")
+    void 문자열_쉼표기준_분리() {
+        String input = "1,2,3,4,5,6";
+        String[] splitedString = InputValidator.splitString(input);
+        assertThat(splitedString.length).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("당첨 번호들을 정수형 숫자로 변환하여 리스트로 변환")
+    void 문자열배열_숫자리스트_변환() {
+        String input = "1,2,3,4,5,6";
+        String[] splitedString = InputValidator.splitString(input);
+        List<Integer> numbers = InputValidator.convertIntegers(splitedString);
+
+        assertThat(numbers.size()).isEqualTo(6);
+        assertThat(numbers.get(0)).isInstanceOf(Integer.class);
+    }
+
+    @Test
+    @DisplayName("입력된 당첨 번호가 6개인지 검사")
+    void 입력번호_6개_아닐때() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.isValidLength(numbers);
+        });
+    }
+
+    @Test
+    @DisplayName("입력된 당첨 번호가 중복인지 검사")
+    void 입력번호_중복일때() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 3, 6);
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.isDuplicate(numbers);
+        });
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되지 않는지 검사")
+    void 보너스번호_당첨번호와_중복일때() {
+        List<Integer> prizeNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 3;
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputValidator.isDuplicateWithPrizeNumber(prizeNumbers, bonusNumber);
+        });
+    }
+}
