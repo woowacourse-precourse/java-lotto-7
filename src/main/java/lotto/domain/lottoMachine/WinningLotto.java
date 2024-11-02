34a98bbe142dd0fa380f69c32b71ffbd373425ac
Son JunHyung <kingjh1125@gmail.com>
2024-11-03 02:23:14 +0900

    feat(BonusNumber): create bonusNumber from userInput

--- src/main/java/lotto/domain/lottoMachine/WinningLotto.java
+++ src/main/java/lotto/domain/lottoMachine/WinningLotto.java
@@ -1,6 +1,8 @@
 package lotto.domain.lottoMachine;
 
 import lotto.domain.lotto.Lotto;
+import lotto.global.exception.Exception;
+import lotto.global.exception.ValidatorBuilder;
 
 public class WinningLotto {
 
@@ -9,10 +11,16 @@ public class WinningLotto {
 
     private WinningLotto(final Lotto winningLotto, final BonusNumber bonusNumber) {
         this.winningLotto = winningLotto;
-        this.bonusNumber = bonusNumber;
+        this.bonusNumber = validateDuplicate(bonusNumber);
     }
 
     public static WinningLotto of(final Lotto winningLotto, final BonusNumber bonusNumber) {
         return new WinningLotto(winningLotto, bonusNumber);
+    }
+
+    private BonusNumber validateDuplicate(BonusNumber bonusNumber) {
+        return ValidatorBuilder.from(bonusNumber)
+                .validate(number -> winningLotto.isContains(bonusNumber), Exception.DUPLICATE_BONUS_NUMBER)
+                .get();
     }
 }
