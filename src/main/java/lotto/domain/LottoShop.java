004851f9a1deca8eef94044044eca15faa53e339
dltmddus <tmddus4671@gmail.com>
2024-11-02 17:19:41 +0900

    feat: 로또 발행하는 LottoShop 도메인 구현

---
+++ src/main/java/lotto/domain/LottoShop.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class LottoShop {
+    private final LottoTickets lottoTickets;
+
+    public LottoShop(LottoTickets lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+//    public LottoTickets getLottoTickets() {
+//        return lottoTickets;
+//    }
+
+    public static LottoShop buyLotteries(LottoCount lottoCount) {
+        long purchasedLotteriesCount = lottoCount.getCount();
+        LottoTickets lottoTickets = LottoTickets.generateLottoTickets(purchasedLotteriesCount);
+        return new LottoShop(lottoTickets);
+    }
+}
