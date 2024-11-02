41bf63eb6c7f87303461e35608129a4d93f5c552
koomchang <leahpar0401@gmail.com>
2024-11-02 22:07:55 +0900

    fix: 보너스 번호 입력에 대한 검증을 통해 보너스 번호 검증이 필요 없다 생각하여 지움

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,7 +1,6 @@
 package lotto.model;
 
 import java.util.List;
-import lotto.validator.BonusNumberValidator;
 
 public class WinningLotto {
 
@@ -10,7 +9,6 @@ public class WinningLotto {
 
     public WinningLotto(List<Integer> winningLotto, Integer bonusNumber) {
         this.winningLotto = new Lotto(winningLotto);
-        BonusNumberValidator.validate(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
