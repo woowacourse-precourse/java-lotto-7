2b2c5f39cbe097b418d07d596537acb534b3098c
Dev-LHY <dev.leehy@gmail.com>
2024-11-02 21:17:07 +0900

    feat(moneyValidator): 돈 검증 추가
    - 양수가 아닌 숫자가 들어왔을 경우 검증 추가
    - validataMoney를 상위 메서드로 사용하기 위해 validateMoneyUnit을 따로 분리

--- src/main/java/lotto/validation/MoneyValidator.java
+++ src/main/java/lotto/validation/MoneyValidator.java
@@ -6,9 +6,21 @@ import lotto.exception.ExceptionMessage;
 
 public class MoneyValidator {
     private static final int VALIDATE_VALUE = 0;
+    private static final int POSITIVE_NUMBER = 1;
 
     public static void validateMoney(int money) {
-        if (money % MONEY_UNIT != VALIDATE_VALUE){
+        validatePositiveNumber(money);
+        validateMoneyUnit(money);
+    }
+
+    private static void validatePositiveNumber(int money) {
+        if (money < POSITIVE_NUMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_MONEY_INPUT.getMessage());
+        }
+    }
+
+    private static void validateMoneyUnit(int money) {
+        if (money % MONEY_UNIT != VALIDATE_VALUE) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_MONEY_INPUT.getMessage());
         }
     }
