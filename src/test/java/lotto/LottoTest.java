b305eea364c6c273ccaedb39af75fcffda7ced8f
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-02 19:47:31 +0900

    test: Lotto클래스 newInstance 테스트코드작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호가 6개가 아니면 예외를 발생 한다.")
@@ -28,6 +29,17 @@ class LottoTest {
     void validateUniqueExceptionTest() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Lotto클래스 생성 테스트")
+    @Test
+    void newInstanceTest() {
+        Lotto lotto1 = Lotto.newInstance();
+        Lotto lotto2 = Lotto.newInstance();
+
+        // 두 객체가 서로 다른 객체임을 테스트한다.
+        assertThat(lotto1).isNotSameAs(lotto2);
+
     }
 
 }
