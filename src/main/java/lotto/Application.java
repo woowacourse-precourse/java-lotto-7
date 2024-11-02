1ee1fbd912eea8edaf1bdb463bb686c7d4c83989
world-dv <goodforx0147@naver.com>
2024-11-03 00:33:24 +0900

    feat: 로또 구입 금액 문구 출력 및 입력
    - 로또 구입 금액 문구를 출력합니다.
    - 사용자로부터 로또 구입 금액을 입력받습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.PaymentController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+
+        PaymentController paymentController = new PaymentController(inputView, outputView);
     }
 }
