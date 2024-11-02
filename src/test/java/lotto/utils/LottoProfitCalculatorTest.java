477fc1d8c5b9d7a6b1a906c5e5f65ee2f5873c60
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 01:56:02 +0900

    test(LottoProfitCalculatorTest): 각 로또 당첨 조건에 따른 총 수익률을 검증하는 테스트 작성

--- src/test/java/lotto/utils/LottoProfitCalculatorTest.java
+++ src/test/java/lotto/utils/LottoProfitCalculatorTest.java
@@ -1,8 +1,14 @@
 package lotto.utils;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoRankType;
+import lotto.domain.LottoResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +29,36 @@ public class LottoProfitCalculatorTest {
         String formattedProfitRate = String.format("%.1f", profitRate);
 
         assertThat(formattedProfitRate).isEqualTo(expectedRate);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'8,9,10,11,12,13', 0.0",
+            "'1,2,3,8,9,10', 500.0",
+            "'1,2,3,4,8,9', 5000.0",
+            "'1,2,3,4,5,8', 150000.0",
+            "'1,2,3,4,5,7', 3000000.0",
+            "'1,2,3,4,5,6', 200000000.0"
+    })
+    @DisplayName("각 로또 당첨 조건에 따른 총 수익률을 검증한다.")
+    void profitRateCalculationForEachRank(String lottoStr, String expectedRate) {
+        List<Integer> lotto = Arrays.stream(lottoStr.split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        // given
+        List<Lotto> lottos = List.of(Lotto.from(lotto));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        final int bonusNumber = 7;
+        final int purchaseAmount = 1000;
+
+        // then
+        LottoResult lottoResult = LottoResult.of(lottos, winningNumbers, bonusNumber);
+        List<LottoRankType> ranks = lottoResult.getLottoRankTypes();
+        String result = LottoProfitCalculator.calculateProfitRate(ranks, purchaseAmount);
+
+        // then
+        assertThat(result).isEqualTo(expectedRate);
     }
 
 }
