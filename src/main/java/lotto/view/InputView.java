9ed2f5ef99fdc8b67fab910273a8e84a124d68eb
jaeyeong <zzz26lk@gmail.com>
2024-11-02 23:51:42 +0900

    refactor: 공백 검증 로직 삭제
    공백이 들어올 경우 다른 검증로직에서도
    검증이 가능하기 때문에
    불필요한 공백 검증 로직을 삭제하여
    InputView의 코드를 간결하게 유지한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,31 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.validator.InputValidator;
-import lotto.validator.Validator;
 
 public class InputView {
 
     public String readAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return getInput();
+        return Console.readLine();
     }
 
     public String readWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return getInput();
+        return Console.readLine();
     }
 
     public String readBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return getInput();
-    }
-
-    private static String getInput() {
-        Validator validator = new InputValidator();
-
-        String input = Console.readLine();
-        validator.validate(input);
-        return input;
+        return Console.readLine();
     }
 }
