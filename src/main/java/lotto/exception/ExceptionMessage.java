62a002a4b68f4d6a444cbf633a2fe22f6a8df10d
jintakkim <mine407603@gmail.com>
2024-11-03 00:31:27 +0900

    chore(ExceptionMessage): 예외 메세지 추가
    Lotto 검증 로직과 관련된 예외 메세지를 추가한다.

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -6,7 +6,9 @@ public enum ExceptionMessage {
     INVALID_NUMBER("[ERROR] 입력이 숫자형식이 아니거나 입력 가능값을 초과했습니다."),
     NO_SUCH_ELEMENT("[ERROR] 입력값이 없습니다."),
     INVALID_LOTTO_NUMBER("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
+    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 6개의 로또 번호는 중복되면 안됩니다.");
+
 
     ExceptionMessage(String message) {
         this.message = message;
