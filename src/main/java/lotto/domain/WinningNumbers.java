681163e70250c637087dda03f6f6ae867cf9e083
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 17:45:34 +0900

    feat(utils): Converter 클래스에서 문자열 리스트를 정수 리스트로 변환하는 parseToIntegerList 메서드 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,26 +1,18 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import lotto.utils.Converter;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
 
     public WinningNumbers(String winningNumbers) {
         List<String> winningNumberStrings = splitInput(winningNumbers);
-        this.winningNumbers = parseToIntegerList(winningNumberStrings);
+        this.winningNumbers = Converter.parseToIntegerList(winningNumberStrings);
     }
 
     private List<String> splitInput(String input) {
         return List.of(input.split(","));
-    }
-
-    private List<Integer> parseToIntegerList(List<String> winningNumberStrings) {
-        List<Integer> numbers = new ArrayList<>();
-        for (String numStr : winningNumberStrings) {
-            numbers.add(Integer.parseInt(numStr.trim()));
-        }
-        return numbers;
     }
 
     public List<Integer> getWinningNumbers() {
--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -1,8 +1,19 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
 
     public static int stringToInt(String string){
         return Integer.parseInt(string);
+    }
+
+    public static List<Integer> parseToIntegerList(List<String> stringNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : stringNumbers) {
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers;
     }
 }
