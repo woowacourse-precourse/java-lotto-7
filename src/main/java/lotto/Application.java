e360a698e441698f04ea6af5400d290e887f9f8e
chair00 <pk1029@yu.ac.kr>
2024-11-03 07:37:25 +0900

    feat: 당첨 번호와 보너스 번호 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 당첨 번호와 보너스 번호 입력받기
+        System.out.println("당첨 번호를 입력하세요.(숫자는 쉼표(,) 기준으로 구분)");
+        String input = readLine();
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println("보너스 번호를 입력하세요.");
+        input = readLine();
+        int bonusNum = Integer.parseInt(input);
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNum);
     }
 }
