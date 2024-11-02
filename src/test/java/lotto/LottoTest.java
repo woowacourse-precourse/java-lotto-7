d2c0d6db823a4b192cf5cfe936ccffd9517e6ed4
supernovaMK <minki5336@naver.com>
2024-11-02 15:53:06 +0900

    test: 로또 번호를 저장하는 기능 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @Test
@@ -19,6 +22,30 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 중 하나가 범위를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다(int invalidNumber) {
+        assertThatThrownBy(() -> new Lotto(List.of(invalidNumber, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 숫자는 1 이상 45 이하입니다.");
+    }
+
+    @Test
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    void 로또_번호의_개수가_6개_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 6개 입니다.");
+    }
+
+    @Test
+    @DisplayName("유효한 로또 번호 리스트가 주어지면 Lotto 객체가 정상적으로 생성된다.")
+    void 유효한_로또_번호_리스트가_주어지면_Lotto_객체가_정상적으로_생성된다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
 }
