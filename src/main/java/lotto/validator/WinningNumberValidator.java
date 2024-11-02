57bc8e126c208293473abf543f82ca7e6053aed8
33jyu33 <jy04169@gmail.com>
2024-11-02 15:41:13 +0900

    feat(WinningNumberValidator): validate separated string consist of only numbers
    Function validateOnlyInteger

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -2,12 +2,20 @@ package lotto.validator;
 
 import lotto.constant.WinningNumberRule;
 
+import java.util.List;
+
 public class WinningNumberValidator {
 
     public static void validateInputComma(String input) throws IllegalArgumentException {
         validateDoubletComma(input);
         validateFirstComma(input);
         validateLastComma(input);
+    }
+
+    public static void validateInputNumber(List<String> input)throws IllegalArgumentException{
+        for (String separatedInput : input){
+            validateOnlyInteger(separatedInput);
+        };
     }
 
     private static void validateDoubletComma(String input) throws IllegalArgumentException {
@@ -25,6 +33,12 @@ public class WinningNumberValidator {
     private static void validateLastComma(String input) throws IllegalArgumentException{
         if(input.endsWith(WinningNumberRule.COMMA.getMessage())){
             throw new IllegalArgumentException(WinningNumberRule.COMMA_LAST.getMessage());
+        }
+    }
+
+    private static void validateOnlyInteger(String input){
+        if(!input.matches(WinningNumberRule.MATCH_NUMBER.getMessage())){
+            throw new IllegalArgumentException(WinningNumberRule.ONLY_COMMA_INTEGER.getMessage());
         }
     }
 }
