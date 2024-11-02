d931839dd21f3b682506dee30b5f2b24c18aebda
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 16:32:58 +0900

    feat(lotto): numbersToString 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,9 +3,11 @@ package lotto;
 import lotto.message.LottoErrorMessage;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers; // numbers 이외의 필드 추가 불가능, private 변경 불가능
+
+    private final List<Integer> numbers; // numbers 이외의 필드(인스턴스 변수) 추가 불가능, private 변경 불가능
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -13,13 +15,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoFormat.NUMBER_COUNT) {
             throw new IllegalArgumentException(LottoErrorMessage.INVALID_NUMBERS_COUNT.getMessage());
         }
 
         if (numbers.stream().distinct().count() < numbers.size()) {
             throw new IllegalArgumentException(LottoErrorMessage.DUPLICATE_NUMBERS.getMessage());
         }
+    }
+
+    public String numbersToString() {
+        String joinedNumbers = numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(LottoFormat.DELIMITER));
+        return LottoFormat.PREFIX + joinedNumbers + LottoFormat.SUFFIX;
     }
     // TODO: 추가 기능 구현
 }
