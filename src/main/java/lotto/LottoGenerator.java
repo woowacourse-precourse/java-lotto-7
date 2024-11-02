332e77c07fd0a1d1b27abe2f3f4b5885ee416702
Seol-Munhyeok <abc1234568@naver.com>
2024-11-02 19:51:08 +0900

    feat(LottoGenerator): getLottoCount 메서드 추가
    1000으로 나누어떨어지는 금액만 받을 수 있도록 하는 메서드를 추가하였다.

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -3,8 +3,20 @@ package lotto;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 public class LottoGenerator {
+    static final int PRICE_PER_LOTTO = 1000;
+    public int getLottoCount(int budget) {
+        if (budget < PRICE_PER_LOTTO) {
+            throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO + "원 이상의 금액을 입력하세요.");
+        }
+        if (budget % PRICE_PER_LOTTO != 0) {
+            throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO + "원 단위로만 구매 가능합니다.");
+        }
+        return budget % PRICE_PER_LOTTO;
+    }
+
     public Lotto generateLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
     }
+
 }
