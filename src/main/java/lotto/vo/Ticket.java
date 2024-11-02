f3c3da700ee97cf20e5cbfa8366a2c7a1c65d0a9
hmlssghm <kwonse326@gmail.com>
2024-11-02 23:56:26 +0900

    feat(Ticket): ticket 객체 생성

---
+++ src/main/java/lotto/vo/Ticket.java
@@ -0,0 +1,13 @@
+package lotto.vo;
+
+import java.util.List;
+
+public class Ticket {
+    private final List<Integer> ticket;
+
+    public Ticket(List<Integer> ticket) {
+        this.ticket = ticket;
+    }
+
+    // [8, 21, 23, 41, 42, 43]
+}
