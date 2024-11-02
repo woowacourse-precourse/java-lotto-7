0215db261067feae4d94d4a61188812a5d946e17
SeongbinPark <hyucse22@gmail.com>
2024-11-02 18:38:03 +0900

    refactor(WinningNumbers): 입력받은 당첨번호 전처리 과정을 별도 클래스에서 관리하도록 변경

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+import lotto.constants.ErrorMessages;
+
 public class WinningNumbers {
+    private final Set<Integer> numbers;
+
+    public WinningNumbers(String input) {
+        this.numbers = parseWinningNumbers(input);
+        validateWinningNumbers();
+    }
+
+    private Set<Integer> parseWinningNumbers(String input) {
+        String[] splitNumbers = input.split(",");
+        Set<Integer> numbers = new HashSet<>();
+        for (String numStr : splitNumbers) {
+            int number = Integer.parseInt(numStr.trim());
+            validateNumberRange(number);
+            if (!numbers.add(number)) {
+                throw new IllegalArgumentException(ErrorMessages.DUPLICATE_WINNING_NUMBER);
+            }
+        }
+        return numbers;
+    }
 }
