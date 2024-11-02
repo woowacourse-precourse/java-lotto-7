f85cd84dde9988cf16d2e6770e67de27b45e22f2
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 15:34:10 +0900

    feat(ErrorMessage): ErrorMessage Enum 정의

---
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -0,0 +1,16 @@
+package lotto.message;
+
+public enum ErrorMessage {
+
+    PREFIX("[ERROR] ");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
