04dec4662193a164e926076136f84a039e1c39c5
soeun2537 <soeun2537@gmail.com>
2024-11-02 17:33:35 +0900

    refactor: Application 클래스 구조 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoService;
+import lotto.model.util.LottoRandomUtil;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoRandomUtil lottoRandomUtil = LottoRandomUtil.getLottoRandomUtil();
+        LottoService lottoService = new LottoService(lottoRandomUtil);
+        LottoController lottoController = new LottoController(lottoService);
+
+        lottoController.run();
     }
 }
