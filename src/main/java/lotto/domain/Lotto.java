eeb70f5c6aecb77e66e55c8f63610202f46de043
jaeyeong <zzz26lk@gmail.com>
2024-11-02 23:25:36 +0900

    feat: 당첨 확인 로직 추가
    Lotto 번호와 당첨 번호를 확인하기 위해서
    당첨 확인 로직을 추가하고
    당첨 내역에 따라 당첨금을 반환하는 로직을 추가한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
 import lotto.validator.LottoValidator;
 import lotto.validator.Validator;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,5 +19,27 @@ public class Lotto {
         validator.validate(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    public long winningCount(List<Integer> winningNumbers, Map<String, Integer> map) {
+        int count = 0;
+        for (int i = 0; i < winningNumbers.size() - 1; i++) {
+            if (numbers.contains(winningNumbers.get(i))) {
+                count++;
+            }
+        }
+        if (count == 5 && numbers.contains(winningNumbers.getLast())) {
+            count *= 2;
+        }
+        Winning[] winnings = Winning.values();
+        for (Winning winning : winnings) {
+            if (winning.getCount() == count) {
+                map.put(winning.getMean(), map.get(winning.getMean()) + 1);
+                return winning.getPrice();
+            }
+        }
+        return 0;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
