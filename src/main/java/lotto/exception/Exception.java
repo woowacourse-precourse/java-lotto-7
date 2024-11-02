11b7e2bb209524ae9200ed8f835cecf6196afb41
JiEung2 <181757@jnu.ac.kr>
2024-11-02 23:30:20 +0900

    feat(exception): 당첨 번호가 숫자가 아닐 때의 예외 메시지 추가

--- src/main/java/lotto/exception/Exception.java
+++ src/main/java/lotto/exception/Exception.java
@@ -3,7 +3,9 @@ package lotto.exception;
 public enum Exception {
     ONLY_NUMERIC_INPUT_FOR_PURCHASE_AMOUNT("구입 금액은 숫자만 입력할 수 있습니다."),
     PURCHASE_AMOUNT_MUST_BE_POSITIVE("구입 금액은 0보다 커야 합니다."),
-    ONLY_DIVISIBLE_BY_LOTTO_PRICE("구입 금액은 1000원 단위로 나누어떨어져야 합니다.");
+    ONLY_DIVISIBLE_BY_LOTTO_PRICE("구입 금액은 1000원 단위로 나누어떨어져야 합니다."),
+
+    ONLY_NUMERIC_INPUT_FOR_WINNING_NUMBERS("당첨 번호는 숫자만 입력할 수 있습니다."),
     ;
 
     private static final String PREFIX = "[ERROR] ";
