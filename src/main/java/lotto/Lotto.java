8bec6bb24a044d18b1cfeb3aa3f62869d13d3218
KoYunHyuk <aes2036@daum.net>
2024-11-02 18:57:03 +0900

    fix: Add Exception Handing of testing Duplication

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,22 +1,33 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for(Integer item: numbers) {
+            if(!set.add(item)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+            }
+        }
+    }
+
     public List<Integer> getNumbers() {
         return this.numbers;
     }
