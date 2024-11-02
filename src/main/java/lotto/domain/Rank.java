f54de001a8920f73dd8b3d3dd305e803d3edc96f
dnwldla <leeemjw@naver.com>
2024-11-02 14:59:19 +0900

    feat: Rank에 당첨내역을 반환하는 람다식 추가 enum에서 상태와 행위를 함께 관리하기 위하여 출력문을 반환하는 람다식을 추가하였다.

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,23 +1,36 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 public enum Rank {
-    UNDER_TWO_HIT(0, BonusExistence.IRRELEVANT, 0),
-    THREE_HIT(3, BonusExistence.IRRELEVANT, 5000),
-    FOUR_HIT(4, BonusExistence.IRRELEVANT, 50000),
-    FIVE_HIT_WITHOUT_BONUS(5, BonusExistence.NO_BONUS, 1500000),
-    FIVE_HIT_WITH_BONUS(5, BonusExistence.HAVE_BONUS, 30000000),
-    SIX_HIT(6, BonusExistence.IRRELEVANT, 2000000000);
+    UNDER_TWO_HIT(0, BonusExistence.IRRELEVANT, 0,
+            (cnt)->"") ,
+    THREE_HIT(3, BonusExistence.IRRELEVANT, 5000L,
+            (cnt)-> String.join(cnt.toString(),"3개 일치 (5,000원) - ","개")),
+    FOUR_HIT(4, BonusExistence.IRRELEVANT, 50000L,
+            (cnt)-> String.join(cnt.toString(),"4개 일치 (50,000원) - ","개")),
+    FIVE_HIT_WITHOUT_BONUS(5, BonusExistence.NO_BONUS, 1500000L,
+            (cnt)-> String.join(cnt.toString(),"5개 일치 (1,500,000원) - ","개")),
+    FIVE_HIT_WITH_BONUS(5, BonusExistence.HAVE_BONUS, 30000000L,
+            (cnt)-> String.join(cnt.toString(),"5개 일치, 보너스 볼 일치 (30,000,000원) - ","개")),
+    SIX_HIT(6, BonusExistence.IRRELEVANT, 2000000000L,
+            (cnt)-> String.join(cnt.toString(),"6개 일치 (2,000,000,000원) - ","개"));
 
     private int hits;
     private BonusExistence bonusExistence;
     private long prizeAmount;
+    Function<String,String> showHitState;
 
-    Rank(int hits, BonusExistence haveBonus, long prizeAmount) {
+    Rank(int hits, BonusExistence haveBonus, long prizeAmount,Function sp) {
         this.hits = hits;
         this.bonusExistence = haveBonus;
         this.prizeAmount = prizeAmount;
+        this.showHitState =sp;
+    }
+
+    public long getPrizeAmount() {
+        return prizeAmount;
     }
 
     public static Rank of(int hits, BonusExistence bonusExistence) {
@@ -31,4 +44,7 @@ public enum Rank {
         return this.hits == hits && this.bonusExistence == bonusExistence;
     }
 
+    public  String getInfo(int cnt) {
+        return showHitState.apply(String.valueOf(cnt));
+    }
 }
