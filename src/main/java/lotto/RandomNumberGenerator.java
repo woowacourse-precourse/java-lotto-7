9cf93e0b949e731502a88822f591770996bf3591
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-11-02 17:56:09 +0900

    feat: Randoms 유틸리티에서 생성되는 예외를 처리하는 기능 추가

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class RandomNumberGenerator {
 
     public List<Integer> generate(int startInclusive, int endInclusive, int count) {
-        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+        try{
+            return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] " + e.getMessage());
+        }
     }
 }
--- src/test/java/lotto/RandomNumberGeneratorTest.java
+++ src/test/java/lotto/RandomNumberGeneratorTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -13,7 +15,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class RandomNumberGeneratorTest {
 
-    @DisplayName("주어진 범위 내의 숫자만 생성한다")
+    @DisplayName("주어진 범위 내의 숫자만 생성한다.")
     @ParameterizedTest
     @MethodSource("provideRangeAndCount")
     public void givenRange_thenGenerateNumbersInRange(int start, int end, int count) {
@@ -27,6 +29,19 @@ class RandomNumberGeneratorTest {
         assertThat(numbers).filteredOn(num -> num >= start && num <= end);
     }
 
+    @DisplayName("잘못된 범위를 제공할 시 형식에 맞는 예외 메시지를 던진다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidRangeAndCount")
+    public void givenInvalidRange_thenThrowFormattedExceptionMessage(int start, int end, int count) {
+        //given
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        //when & then
+        assertThatThrownBy(() -> randomNumberGenerator.generate(start, end, count))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageStartingWith("[ERROR]");
+    }
+
 
 
     private static Stream<Arguments> provideRangeAndCount() {
@@ -35,6 +50,16 @@ class RandomNumberGeneratorTest {
             Arguments.of(-45, -1, 6),
             Arguments.of(-100_000, 0, 5),
             Arguments.of(0, 100_000, 5)
+        );
+    }
+
+    private static Stream<Arguments> provideInvalidRangeAndCount() {
+        return Stream.of(
+            Arguments.of(45, 1, 6),
+            Arguments.of(Integer.MIN_VALUE, 0, 6),
+            Arguments.of(0, Integer.MAX_VALUE, 6),
+            Arguments.of(Integer.MIN_VALUE, Integer.MAX_VALUE, 6),
+            Arguments.of(1, 5, 100)
         );
     }
 }
