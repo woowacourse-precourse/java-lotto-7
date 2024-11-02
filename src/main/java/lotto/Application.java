255c0e206487cc5c417c941566d0e553d93d4c0c
hyunwon <khw73850@gmail.com>
2024-11-02 13:02:11 +0900

    feat: read winning numbers input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,12 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     private static int amountToPurchase;
     private static int numOfLotto;
     private static List<Lotto> lottos;
+    private static List<Integer> winningNumbers;
     public static void main(String[] args) {
         readUserInput();
     }
@@ -23,6 +25,10 @@ public class Application {
 
         lottos = createLottos();
         lottos.forEach(System.out::println);
+
+        System.out.println("당첨 번호를 입력해 주새요.");
+        winningNumbers = readWinningNumbers();
+
 
     }
 
@@ -43,5 +49,13 @@ public class Application {
         }
 
         return lottos;
+    }
+
+    private static List<Integer> readWinningNumbers(){
+        String input = Console.readLine();
+
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
     }
 }
