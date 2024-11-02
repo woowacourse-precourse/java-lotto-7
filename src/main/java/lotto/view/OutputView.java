832bfb8031edc3c0b044c4da66914d6e863f9572
변우영 <likevangogh24@gmail.com>
2024-11-02 17:12:24 +0900

    feat(OutputView): 구매 로또 번호를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import lotto.enums.Message;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OutputView {
 
     public void printPurchaseAmountInputMessage() {
@@ -19,5 +23,16 @@ public class OutputView {
     public void printPurchaseCount(int count) {
         String message = String.format(Message.PURCHASE_SUFFIX.getMessage(), count);
         System.out.println(message);
+    }
+
+    public void printPurchasedLottoNumbers(List<Integer> lottoNumbers) {
+        List<Integer> numbers = sortAscending(lottoNumbers);
+        System.out.println(numbers);
+    }
+
+    public List<Integer> sortAscending(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return numbers;
     }
 }
