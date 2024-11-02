831f13a9ae88ec3f8bf22ec48e4e3ddd3199901d
Byuntil <gugugu2014@jbnu.ac.kr>
2024-11-02 15:14:57 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,24 @@
 package lotto.view;
 
+import java.util.List;
 import lotto.dto.GeneratedTickets;
+import lotto.dto.LottoResult;
 
 public class OutputView {
 
     public static final String PURCHASED_TICKET_MESSAGE = "%d개를 구매했습니다.";
+    public static final String WINNING_RESULT_MESSAGE = "당첨 통계\n---";
 
     public void printGeneratedTickets(GeneratedTickets generatedTicket) {
         System.out.println(String.format(PURCHASED_TICKET_MESSAGE, generatedTicket.ticketCount()));
         generatedTicket.getTickets().forEach(System.out::println);
+    }
+
+    public void printResult(List<LottoResult> results) {
+        System.out.println(WINNING_RESULT_MESSAGE);
+        for (LottoResult result : results) {
+            System.out.print(result.getRank().toString());
+            System.out.printf(" - %d개%n", result.getNumberOfMatched());
+        }
     }
 }
