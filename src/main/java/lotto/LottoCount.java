1a3f9a068a74e2bf0aba28bf19b9513c69718b7d
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-02 19:36:03 +0900

    refactor : Integrate created classes
    - 클래스 분리가 어려워서 한 클래스에 구현 후 분리할 예정임.

--- src/main/java/lotto/LottoCount.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-public class LottoCount {
-    public int getLottoCount() {
-        InputView inputView = new InputView();
-
-        return inputView.getLottoPurchaseAmount() / 1000;
-    }
-}
