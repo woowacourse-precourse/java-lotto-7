d0a1a6d00ab146696a86bcb474c5e2d6b073e20f
hanmh319 <hanmh319@naver.com>
2024-11-02 18:33:24 +0900

    feat: 응모 내역 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.sql.Array;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
-        int attempts= validateBetAmount()/1000;
+        int attempts= validateBetAmount();
+        printEntries(attempts);
 
-    }
+  }
 
     private static int validateBetAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         if (!input.matches("\\d+")) {
             throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
@@ -19,6 +26,20 @@ public class Application {
         if(betAmount%1000!=0) {
             throw new IllegalArgumentException("베팅 금액이 1000원 단위가 아닙니다");
         }
-        return betAmount;
+        int attempts = betAmount/1000;
+        System.out.println(attempts + "개를 구매했습니다.");
+        return attempts;
+    }
+    private static void printEntries(int attempts) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for(int i=0; i<attempts; i++){
+            List<Integer> entries = new ArrayList<>();
+            entries = Randoms.pickUniqueNumbersInRange(1,45,6);
+            entries.sort(Comparator.naturalOrder());
+            lists.add(entries);
+        }
+        for(int j=0; j<attempts; j++){
+            System.out.println(lists.get(j));
+        }
     }
 }
