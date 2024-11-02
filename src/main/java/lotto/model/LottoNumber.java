de64e672136c55da22af20a42f3b75209239ee94
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 02:07:28 +0900

    feat(lotto/validate): 로또 번호 범위 예외 기능 추가

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -7,11 +7,18 @@ public class LottoNumber {
     private final int value;
 
     public LottoNumber(int value) {
+        validateRange(value);
         this.value = value;
     }
 
-    public static LottoNumber of(String value) {
+    public static LottoNumber from(String value) {
         return new LottoNumber(NumberUtil.parseInt(value));
+    }
+
+    private void validateRange(int value) {
+        if (value < 1 || 45 < value) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45사이의 숫자만 가능합니다.");
+        }
     }
 
     public int getValue() {
--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -10,7 +10,7 @@ class LottoNumberTest {
     @Test
     @DisplayName("로또 번호가 숫자가 아니라면 예외가 발생한다.")
     void validateType() {
-        assertThatThrownBy(() -> LottoNumber.of("a"))
+        assertThatThrownBy(() -> LottoNumber.from("a"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
