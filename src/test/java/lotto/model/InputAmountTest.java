1c5b2b3cec430874d32920107bfb8b87e7ae222a
kaswhy <sxshx818@naver.com>
2024-11-02 23:31:37 +0900

    refactor(InputAmountTest): 메소드명 앞글자를 대문자로, 필요없는 import문 삭제
    변경 전
    - throwExceptionIfInputAmountEmpty
    
    변경 후
    - ThrowExceptionIfInputAmountBlank
    - Feature to be tested 방식으로 작성

--- src/test/java/lotto/model/InputAmountTest.java
+++ src/test/java/lotto/model/InputAmountTest.java
@@ -3,22 +3,21 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
-import net.bytebuddy.implementation.bytecode.Throw;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import lotto.constants.ErrorMessage;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputAmountTest {
 
     private InputAmount inputAmount;
 
-    @DisplayName("입력값이 빈 값이면 예외를 발생시킨다")
+    @DisplayName("입력값이 빈 값이면 예외를 발생시킨다.")
     @ParameterizedTest
     @NullAndEmptySource
-    public void throwExceptionIfInputAmountEmpty(String inputAmount) {
+    public void ThrowExceptionIfInputAmountBlank(String inputAmount) {
         Throwable thrown = catchThrowable(() -> {
             this.inputAmount = new InputAmount(inputAmount);
         });
@@ -26,4 +25,6 @@ public class InputAmountTest {
         assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INPUT_CAN_NOT_BE_BLANK.get());
     }
+
+
 }
