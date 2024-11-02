916cb39097a3e8bc72ac1881623d83432869410a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-02 21:06:24 +0900

    feat(validator/LottoTicketValidator): 구매 금액 검증을 위한 validatePositive 및 validateUnit 메서드 추가
    - validatePositive 메서드를 통해 구매 금액이 양수인지 확인
    - validateUnit 메서드를 통해 구매 금액이 1000원 단위인지 확인
    - 두 검증 메서드를 validatePurchasePrice에서 호출하여 통합된 검증 수행

--- src/main/java/lotto/validator/LottoTicketValidator.java
+++ src/main/java/lotto/validator/LottoTicketValidator.java
@@ -1,10 +1,21 @@
 package lotto.validator;
 
-import lotto.controller.LottoTicketGenerator;
-
 public class LottoTicketValidator {
+	public static final int LOTTO_PRICE = 1000;
+
 	public static void validatePurchasePrice(int purchasePrice) {
-		if (purchasePrice % LottoTicketGenerator.LOTTO_PRICE != 0) {
+		validatePositive(purchasePrice);
+		validateUnit(purchasePrice);
+	}
+
+	public static void validatePositive(int purchasePrice) {
+		if (purchasePrice <= 0) {
+			throw new IllegalArgumentException("[ERROR] 금액은 양수여야 합니다.");
+		}
+	}
+
+	public static void validateUnit(int purchasePrice) {
+		if (purchasePrice % LOTTO_PRICE != 0) {
 			throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
 		}
 	}
