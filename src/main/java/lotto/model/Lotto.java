ae14579cff1b6de56c78b1bf9cb24b4f96675d34
sik9252 <sik9252@naver.com>
2024-11-02 14:44:51 +0900

    feat: 로또 번호 중복 검사 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,8 @@ package lotto.model;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.exception.ErrorMessage.DUPLICATE_NUMBER_IS_NOT_ALLOWED;
+
 public class Lotto {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
@@ -21,8 +23,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkLottoNumberSize(numbers);
+        checkLottoNumberHasDuplicate(numbers);
+    }
+
+    private void checkLottoNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkLottoNumberHasDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_IS_NOT_ALLOWED.getMessage());
         }
     }
 }
