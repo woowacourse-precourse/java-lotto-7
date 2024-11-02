99bfcf28348538fc4e874c0484d3a584bcfc73ac
softmoca <acyc0909@gmail.com>
2024-11-02 19:47:22 +0900

    feat: 보너스 번호 입력 및 검증 기능 추가

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -23,12 +23,29 @@ public class View {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
+    public static void promptForWinningBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+
+
+
+
     public static List<Integer> inputWinningLotto() {
         String winningLottoInput = Console.readLine();
         validateEmpty(winningLottoInput);
         validateCommaFormat(winningLottoInput);
         return convertToNumbers(winningLottoInput);
     }
+
+    public static int inputWinningBonusNumber() {
+        String winningBonusNumberInput = Console.readLine();
+        validateEmpty(winningBonusNumberInput);
+        validateNumeric(winningBonusNumberInput);
+
+        return Integer.parseInt(winningBonusNumberInput);
+    }
+
 
 
     private static void validateCommaFormat(String input) {
