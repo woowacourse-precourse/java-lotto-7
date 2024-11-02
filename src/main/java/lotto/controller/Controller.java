a3e2f4db4e5d3b6fdfe2965d3299a2ea2f4b0d0c
rootTiket <l20078829@gmail.com>
2024-11-02 23:46:25 +0900

    feat(Controller): 사용자에게 금액을 입력받아 로또를 구매하는 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -5,15 +5,38 @@ import static lotto.view.constant.Message.START;
 import static lotto.view.constant.Message.STATISTICS;
 import static lotto.view.constant.Message.WINNING_START;
 
+import java.util.List;
+import lotto.Lotto;
+import lotto.domain.CashRegister;
+import lotto.domain.LottoMachine;
+import lotto.domain.Parser;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Controller {
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final Parser parser = new Parser();
+    private final LottoMachine lottoMachine = new LottoMachine();
 
     public void start() {
         outputView.printResult(START.getMessage());
+        List<Lotto> lottos = pickLotto();
+        outputView.printLottos(lottos);
+
         outputView.printResult(WINNING_START.getMessage());
         outputView.printResult(BONUS_START.getMessage());
         outputView.printResult(STATISTICS.getMessage());
+    }
+
+    private List<Lotto> pickLotto() {
+        int money = parser.parseMoney(inputView.inputString());
+        int count = getCount(money);
+        return lottoMachine.generateLottos(count);
+    }
+
+    private int getCount(int money) {
+        CashRegister cashRegister = new CashRegister(money);
+        return cashRegister.calculateLottoCount();
     }
 }
