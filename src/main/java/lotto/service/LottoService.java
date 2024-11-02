5969d9d5343f7556a4a0d5865c7df2e32cd66981
qudcks8084 <qudcks8084@naver.com>
2024-11-02 21:02:13 +0900

    Feat : LottoService에서 당첨 로또 생성을 위한 변환 함수 구현
    - generateNewLotto
    	- 1. 입력받은 String 문자열을 콤마를 기준으로 분리
    	- 2. 각각의 값을 정수로 바꿔서 ArrayList에 삽입
    	- 3. 생성된 ArrayList를 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,8 +1,13 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoCollection;
+import lotto.domain.LottoPrize;
+import lotto.domain.LottoResult;
+import lotto.util.constant.LottoConstants;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 import static lotto.util.constant.LottoConstants.*;
@@ -17,7 +22,16 @@ public class LottoService {
         return newLotto;
     }
 
-    public static Lotto genereteNewLotto(){
+    private static Lotto genereteNewLotto(){
         return new Lotto(pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUM_COUNT));
     }
+
+    public static ArrayList<Integer> generateWinnerLotto(String winnerLottoInput){
+        ArrayList<Integer> winnerLotto = new ArrayList<>();
+        for (String s : winnerLottoInput.split(",")) {
+            winnerLotto.add(Integer.parseInt(s));
+        }
+        return winnerLotto;
+    }
+
 }
