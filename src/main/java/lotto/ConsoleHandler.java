a43d74ded62f96a7d290bcd47aca696eca96e4ad
soobin <a01056239778@gmail.com>
2024-11-02 22:16:49 +0900

    feat: 숫자 입력 검증 로직 구현
    - 입력 값이 숫자여야 하는 경우 에러 문구 출력
    - 로또 구입금액 입력 검증 추가

--- src/main/java/lotto/ConsoleHandler.java
+++ src/main/java/lotto/ConsoleHandler.java
@@ -7,7 +7,15 @@ public class ConsoleHandler {
     public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String money = Console.readLine();
-        return Integer.parseInt(money);
+        return invertNumber(money);
     }
 
+
+    private int invertNumber(String number) {
+        try{
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
 }
