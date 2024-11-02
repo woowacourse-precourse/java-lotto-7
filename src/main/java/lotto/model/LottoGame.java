e6ee2235655e1a0afcab7bb044f8ea10871327eb
suyeong jang <jangsuyeong923@naver.com>
2024-11-03 03:15:55 +0900

    feat(LottoGame): 당첨된 등수를 계산하는 기능 구현

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -2,15 +2,21 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
     private final List<Lotto>lottoTickets=new ArrayList<>();
+    private final Map<PrizeType, Integer> prizeStates = new EnumMap<>(PrizeType.class);
 
     public LottoGame(int purchaseAmount){
         for(int i=0;i<purchaseAmount/1000; i++){
             List<Integer> lottoNumbers=new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
             lottoTickets.add(new Lotto(lottoNumbers));
+        }
+        for (PrizeType prize : PrizeType.values()) {
+            prizeStates.put(prize, 0);
         }
     }
 
@@ -19,5 +25,21 @@ public class LottoGame {
     }
     public int getLottoCount(){
         return lottoTickets.size();
+    }
+    public void calculateLotto(List<Integer> winningNumbers, int bonusNumber) {
+        for (Lotto lotto : lottoTickets) {
+            int matchCount = lotto.getMatchCount(winningNumbers);
+            boolean hasBonusNumber = lotto.containsBonusNumber(bonusNumber);
+            updatePrizeStates(matchCount, hasBonusNumber);
+        }
+    }
+
+    private void updatePrizeStates(int matchCount, boolean hasBonusNumber) {
+        for (PrizeType prize : PrizeType.values()) {
+            if (prize.isWinner(matchCount, hasBonusNumber)) {
+                prizeStates.put(prize, prizeStates.getOrDefault(prize, 0) + 1);
+                break; // 첫 번째 일치하는 상금 유형을 찾으면 루프 종료
+            }
+        }
     }
 }
