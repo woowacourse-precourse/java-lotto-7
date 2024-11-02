1bdf2a1635493fd82882181d4e6cb4b5b9b1ab77
ufo9430 <ufo9430@naver.com>
2024-11-02 15:59:42 -0400

    feat: 당첨 번호를 저장하는 클래스 생성

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,9 @@
+package lotto.model;
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    public WinningNumbers(Lotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,27 +1,20 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.exception.ExceptionMessage;
-import lotto.exception.InputException;
 import lotto.model.Amount;
 import lotto.model.Lotto;
-import lotto.validate.Validator;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+import lotto.model.WinningNumbers;
 
 import static lotto.console.ConsoleManager.*;
-import static lotto.validate.Validator.*;
 
 public class InputView {
     public Amount getAmount(){
         println("구입금액을 입력해 주세요.");
         return new Amount(Console.readLine());
     }
-    public Lotto getWinning(){
+    public WinningNumbers getWinning(){
         println("당첨 번호를 입력해 주세요.");
-        return new Lotto(Console.readLine());
+        return new WinningNumbers(new Lotto(Console.readLine()));
     }
 
 }
