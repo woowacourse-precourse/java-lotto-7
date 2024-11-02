7cb5bd78069e8b18498b83e5cfaa3af15e1964e2
dudxo <father8475@naver.com>
2024-11-02 18:18:10 +0900

    feat(LottoGenerator): 로또 생성기
    - 로또를 발해하고 오름차순으로 정렬한다.

---
+++ src/main/java/util/LottoGenerator.java
@@ -0,0 +1,13 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import domain.Lotto;
+
+public class LottoGenerator {
+
+    public Lotto generateLotto() {
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        lotto.ascNumber();
+        return lotto;
+    }
+}
