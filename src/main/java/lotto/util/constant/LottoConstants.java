5b9b509a10a19fff589250386e874d7326574323
qudcks8084 <qudcks8084@naver.com>
2024-11-02 21:28:00 +0900

    Feat : 자주 사용하는 0 상수 추가

--- src/main/java/lotto/util/constant/LottoConstants.java
+++ src/main/java/lotto/util/constant/LottoConstants.java
@@ -5,4 +5,6 @@ public class LottoConstants {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
     public static final int LOTTO_NUM_COUNT = 6;
+
+    public static final int ZERO = 0;
 }
