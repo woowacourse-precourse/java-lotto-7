4d8e4755c76c98a3cac2adc197fa2f8bb8c9730c
백재우 <9712jw@gmail.com>
2024-11-02 19:01:11 +0900

    feat : 당첨 개수를 관리하는 enum 구현

---
+++ src/main/java/domain/rank/MatchCount.java
@@ -0,0 +1,32 @@
+package domain.rank;
+
+public enum MatchCount {
+    THREE_MATCH(3, "3개 일치 (5,000원)", 5000L),
+    FOUR_MATCH(4, "4개 일치 (50,000원)", 50000L),
+    FIVE_MATCH(5, "5개 일치 (1,500,000원)", 1500000L),
+    SIX_MATCH(6, "6개 일치 (2,000,000,000원)", 2000000000L),
+    BONUS_FIVE_MATCH(7, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000L);
+
+    private final int matchCount;
+    private final String outputMessage;
+    private final Long priceMoney;
+
+    private MatchCount(int matchCount, String outputMessage, Long priceMoney) {
+        this.matchCount = matchCount;
+        this.outputMessage = outputMessage;
+        this.priceMoney = priceMoney;
+    }
+
+    private int getMatchCount() {
+        return matchCount;
+    }
+
+    public static MatchCount from(int count) {
+        for (MatchCount matchCount : MatchCount.values()) {
+            if (matchCount.getMatchCount() == count) {
+                return matchCount;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
--- src/main/java/domain/rank/Rank.java
+++
@@ -1,7 +0,0 @@
-package domain.rank;
-
-public enum Rank {
-    /*
-    * 1~6등 까지 관리하는 Enum 클래스
-    * */
-}
