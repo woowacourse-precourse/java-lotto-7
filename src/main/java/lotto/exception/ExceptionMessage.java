dd8c4be0573df2c2ce3445e4bf41680a61d0826b
SukJin Kim <rkathrwks@gmail.com>
2024-11-02 21:23:16 +0900

    feat(validator): 구입 금액 유효성 검증(최대 구매가능 금액 초과) 기능 구현

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,11 +1,13 @@
 package lotto.exception;
 
+import static lotto.validator.PurchaseAmountValidator.MAX_PURCHASE_AMOUNT;
 import static lotto.validator.PurchaseAmountValidator.UNIT_PURCHASE_AMOUNT;
 
 public enum ExceptionMessage {
     BLANK_PURCHASE_AMOUNT("공백이 아닌 구입 금액을 입력해주세요."),
     NON_POSITIVE_PURCHASE_AMOUNT("구입 금액은 양수이어야 합니다."),
-    NOT_DIVIDED_BY_UNIT_AMOUNT("구입 금액은 단위 금액인 " + UNIT_PURCHASE_AMOUNT + "으로 나누어 떨어져야 합니다.");
+    NOT_DIVIDED_BY_UNIT_AMOUNT("구입 금액은 단위 금액인 " + UNIT_PURCHASE_AMOUNT + "으로 나누어 떨어져야 합니다."),
+    EXCEEDS_MAX_PURCHASE_AMOUNT("구입 금액은 최대 구매가능 금액인 " + MAX_PURCHASE_AMOUNT + "을 넘지 못합니다.");
 
     private final String message;
 
--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import static lotto.exception.ExceptionMessage.BLANK_PURCHASE_AMOUNT;
+import static lotto.exception.ExceptionMessage.EXCEEDS_MAX_PURCHASE_AMOUNT;
 import static lotto.exception.ExceptionMessage.NON_POSITIVE_PURCHASE_AMOUNT;
 import static lotto.exception.ExceptionMessage.NOT_DIVIDED_BY_UNIT_AMOUNT;
 
@@ -10,11 +11,13 @@ import org.junit.platform.commons.util.StringUtils;
 public class PurchaseAmountValidator {
     private static final String POSITIVE_NUMBER_REGEX = "^[1-9][0-9]*$";
     public static final long UNIT_PURCHASE_AMOUNT = 1000L;
+    public static final long MAX_PURCHASE_AMOUNT = 1_000_000L;
 
     public void validate(String purchaseAmount) {
         validateBlank(purchaseAmount);
         validatePositiveNumber(purchaseAmount);
         validateDivisibleByUnitAmount(purchaseAmount);
+        validateExceedsMaxPurchaseAmount(purchaseAmount);
     }
 
     private void validateBlank(String purchaseAmount) {
@@ -41,5 +44,15 @@ public class PurchaseAmountValidator {
 
     private boolean isNotDividedByUnitAmount(String purchaseAmount) {
         return !(Long.parseLong(purchaseAmount) % UNIT_PURCHASE_AMOUNT == 0);
+    }
+
+    private void validateExceedsMaxPurchaseAmount(String purchaseAmount) {
+        if (exceedsMaxPurchaseAmount(purchaseAmount)) {
+            throw new IllegalArgumentException(EXCEEDS_MAX_PURCHASE_AMOUNT.getMessage());
+        }
+    }
+
+    private boolean exceedsMaxPurchaseAmount(String purchaseAmount) {
+        return Long.parseLong(purchaseAmount) > MAX_PURCHASE_AMOUNT;
     }
 }
