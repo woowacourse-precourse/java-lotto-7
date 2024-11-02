716a6472e7f9693c6484d164f43ca5b414624de9
DoomchitYJ <poj1004@inha.edu>
2024-11-02 21:50:19 +0900

    feat(OutputView): 당첨 통계를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,6 +12,13 @@ public class OutputView {
     private static final String EACH_LOTTO_NUMBERS_DELIMITER = ", ";
     private static final String EACH_LOTTO_NUMBERS_SUFFIX = "]";
 
+    private static final String WINNING_RESULT_START_VIEW = "당첨 통계\n---";
+    private static final String RESULT_VIEW = "3개 일치 (5,000원) - %d개\n"
+            + "4개 일치 (50,000원) - %d개\n"
+            + "5개 일치 (1,500,000원) - %d개\n"
+            + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
+            + "6개 일치 (2,000,000,000원) - %d개\n";
+
     public static void showLottoNumbers(List<Lotto> myLottos) {
         System.out.println(BUYING_LOTTO_VIEW);
 
@@ -24,5 +31,16 @@ public class OutputView {
                     EACH_LOTTO_NUMBERS_SUFFIX));
             System.out.println();
         }
+    }
+
+    public static void showWinningResult(int[] lottoResult) {
+        System.out.println(WINNING_RESULT_START_VIEW);
+        System.out.printf(RESULT_VIEW,
+                lottoResult[4],
+                lottoResult[3],
+                lottoResult[2],
+                lottoResult[1],
+                lottoResult[0]);
+        System.out.println();
     }
 }
