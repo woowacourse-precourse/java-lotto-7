15979ca177b7691d9e42406b9cfa3771461fbba3
daeyeon <kdy4410121@naver.com>
2024-11-02 19:24:45 +0900

    feat: 오름차순으로 로또 번호 저장하는 함수 작성

--- src/main/java/lotto/LottoRandomNumber.java
+++ src/main/java/lotto/LottoRandomNumber.java
@@ -2,6 +2,7 @@ package lotto;
 
 import NumberGenerator.RandomNumber;
 
+import java.util.Collections;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -11,13 +12,15 @@ public class LottoRandomNumber implements RandomNumber {
     private static final int endNumber = 45;
     private static final int count = 6;
 
-    public LottoRandomNumber(){
-
-    }
-
     @Override
     public List<Integer> randomLottoNumbers(){
-        return Randoms.pickUniqueNumbersInRange(startNumber, endNumber, count);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(startNumber, endNumber, count);
+        return orderedByAsc(lotto);
+    }
+
+    private List<Integer> orderedByAsc(List<Integer> lotto){
+        Collections.sort(lotto);
+        return lotto;
     }
 
 }
