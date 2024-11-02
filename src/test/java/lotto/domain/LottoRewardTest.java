b87c589480340ed803a1350924ff7b3033a93e1a
hwan2-99 <thh1233123@gmail.com>
2024-11-02 10:09:59 +0900

    style: 코드컨벤션 수정

--- src/test/java/lotto/domain/LottoRewardTest.java
+++ src/test/java/lotto/domain/LottoRewardTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 class LottoRewardTest {
     @Test
     void 로또보상_구하기() {
-        assertThat(LottoReward.findByMatchCount(6,true)).isEqualTo(LottoReward.FIRST);
-        assertThat(LottoReward.findByMatchCount(5,true)).isEqualTo(LottoReward.SECOND);
-        assertThat(LottoReward.findByMatchCount(5,false)).isEqualTo(LottoReward.THIRD);
-        assertThat(LottoReward.findByMatchCount(4,true)).isEqualTo(LottoReward.FOURTH);
-        assertThat(LottoReward.findByMatchCount(3,true)).isEqualTo(LottoReward.FIFTH);
-        assertThat(LottoReward.findByMatchCount(0,true)).isEqualTo(LottoReward.NONE);
+        assertThat(LottoReward.findByMatchCount(6, true)).isEqualTo(LottoReward.FIRST);
+        assertThat(LottoReward.findByMatchCount(5, true)).isEqualTo(LottoReward.SECOND);
+        assertThat(LottoReward.findByMatchCount(5, false)).isEqualTo(LottoReward.THIRD);
+        assertThat(LottoReward.findByMatchCount(4, true)).isEqualTo(LottoReward.FOURTH);
+        assertThat(LottoReward.findByMatchCount(3, true)).isEqualTo(LottoReward.FIFTH);
+        assertThat(LottoReward.findByMatchCount(0, true)).isEqualTo(LottoReward.NONE);
     }
 }
