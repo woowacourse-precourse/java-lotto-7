1c33473d231094223226f39f1e30fbde62928a20
mirinae700 <mirinae219@gmail.com>
2024-11-02 12:47:44 +0900

    refactor(lottoutility) : 구매금액 유효성 확인 기능 별도의 클래스로 분리위해 삭제

--- src/main/java/lotto/LottoUtility.java
+++ src/main/java/lotto/LottoUtility.java
@@ -5,17 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LottoUtility {
-    public static void validatePurchaseAmount(String input) {
-        try {
-            int amount = Integer.parseInt(input);
-            if(amount % 1000 > 0) {
-                throw new IllegalArgumentException("[ERROR] 구입금액은 1,000 단위로 입력하셔야 합니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자만 입력하셔야 합니다.");
-        }
-    }
-
     public static List<Integer> stringToWinningNumbers(String input) {
         List<Integer> numbers = new ArrayList<>();
         try {
--- src/test/java/lotto/LottoUtilityTest.java
+++ src/test/java/lotto/LottoUtilityTest.java
@@ -6,18 +6,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoUtilityTest {
     @Test
-    void 구입금액이_숫자가_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> LottoUtility.validatePurchaseAmount("오천원"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 구입금액이_천단위가_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> LottoUtility.validatePurchaseAmount("10500"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
     void 입력한_당첨번호가_숫자가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> LottoUtility.stringToWinningNumbers("이,삼,사,오,육"))
                 .isInstanceOf(IllegalArgumentException.class);
