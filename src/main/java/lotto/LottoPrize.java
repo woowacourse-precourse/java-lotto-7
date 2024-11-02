30b5db511c1036bb6df971b8fa486f86c096e9cc
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-02 17:16:03 +0900

    feat: 수익률 계산 기능

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -19,22 +19,37 @@ public enum LottoPrize {
         this.prize = prize;
     }
 
-    public static void getIndividualResult(int matchCount, boolean matchBonusNumber, List<Integer> prizeCount) {
+    public static void getIndividualResult(int matchCount, boolean matchBonusNumber,
+                                           List<Integer> prizeCount) {
         for (LottoPrize rank : LottoPrize.values()) {
-            if (rank.numberMatchCount == matchCount && rank.bonusNumberRequired == matchBonusNumber) {
+            if (rank.numberMatchCount == matchCount
+                    && rank.bonusNumberRequired == matchBonusNumber) {
                 prizeCount.set(rank.ordinal(), prizeCount.get(rank.ordinal()) + 1);
             }
         }
     }
 
     public static void printResult(List<Integer> prizeCount) {
+        System.out.println("\n당첨 통계\n---");
+
         for (LottoPrize rank : LottoPrize.values()) {
             System.out.print(rank.numberMatchCount + "개 일치");
             if (rank.bonusNumberRequired) {
                 System.out.print(", 보너스 볼 일치");
             }
-            System.out.printf(String.format(" (%,d원) - %,d개", rank.prize, prizeCount.get(rank.ordinal())));
-            System.out.println();
+            System.out.printf(String.format(" (%,d원) - %,d개\n", rank.prize,
+                    prizeCount.get(rank.ordinal())));
         }
+    }
+
+    public static void calculateProfitRate(List<Integer> prizeCount, int paymentAmount) {
+        double totalProfit = 0;
+
+        for (LottoPrize rank : LottoPrize.values()) {
+            totalProfit += prizeCount.get(rank.ordinal()) * rank.prize;
+        }
+
+        String profitRate = String.format("%.1f", totalProfit / paymentAmount * 100);
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
     }
 }
