37fb3808f1911b9fb34c6cb21f414a4b8be0981a
yumi <yumkoori@gmail.com>
2024-11-02 19:29:34 +0900

    Refactor: 중복값의 로또 번호는 생성조차 안되도록 변경
    - 변경전에는 로또 번호의 중복값 검사는 로또가 생성이 된 후에 중복값이 있는지 검사
    - 중복값이 있는 로또 번호는 있을 수 없으므로, 로또 번호의 생성자에서 중복값 검사를 하여 생성부터 안되도록 변경
    - 변경 후 로또 생성의 안전성이 더 높아졌고, InputValidater에서 오로지 중복값 검사만을 위해서 쓰였던 HashSet을 제거하여 복잡도 개선

--- src/main/java/lotto/InputValidater.java
+++ src/main/java/lotto/InputValidater.java
@@ -9,16 +9,11 @@ public class InputValidater {
         List<Integer> winningNumbers = Arrays.stream(inputWinningNumbers
                 .split(","))
                 .map(Integer::parseInt)
-                .toList();                              //당첨 번호를 HashSet으로 관리하는것 고려
+                .toList();                              
 
-        HashSet<Integer> set = new HashSet<>();
-        for (int number : winningNumbers) {
-            //중복값 검사
-            if (!set.add(number)) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복 되는 값이 없어야 합니다.");
-            }
-            //당첨 번호가 1 ~ 45 사이의 값이 아닐 때 (공통)
-            validateNumberRange(number);
+        //당첨 번호가 1 ~ 45 사이의 값이 아닐 때 (공통)
+        for (int winningNumber: winningNumbers) {
+            validateNumberRange(winningNumber);
         }
         //당첨 번호가 총 개수가 6개가 아닐 때
         if (winningNumbers.size() != 6) {
