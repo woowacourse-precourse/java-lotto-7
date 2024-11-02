56ab61eb37d32d14b0443d14d4134b54db221bbc
RaZel713 <razel7130@gmail.com>
2024-11-03 01:04:01 +0900

    refactor: 수익률 계산 주요 로직을 서비스에서 모델로 이동

--- src/main/java/lotto/custom/constants/NumberConstants.java
+++ src/main/java/lotto/custom/constants/NumberConstants.java
@@ -6,5 +6,6 @@ public class NumberConstants {
     public static final int LOTTO_NUMBERS_PER_TICKET = 6;
 
     public static final int LOTTO_NUMBER_RANGE_START = 1;
+    
     public static final int LOTTO_NUMBER_RANGE_END = 45;
 }
--- src/main/java/lotto/custom/service/CalculateYieldService.java
+++ src/main/java/lotto/custom/service/CalculateYieldService.java
@@ -3,20 +3,18 @@ package lotto.custom.service;
 import static lotto.custom.constants.NumberConstants.LOTTO_PRICE;
 
 import java.util.List;
-import lotto.custom.model.LottoPrize;
+import lotto.custom.model.LottoYieldCalculator;
 import lotto.custom.model.Lottos;
 
 public class CalculateYieldService {
+    private final LottoYieldCalculator lottoYieldCalculator;
+
+    public CalculateYieldService() {
+        this.lottoYieldCalculator = new LottoYieldCalculator();
+    }
 
     public double run(List<Integer> result, Lottos myLottoTickets) {
-        int index = 0;
-        double sum = 0;
-
-        for (LottoPrize prize : LottoPrize.values()) {
-            sum += prize.getPrizeMoney() * result.get(index++);
-        }
-
         int purchaseAmount = myLottoTickets.size() * LOTTO_PRICE;
-        return sum / purchaseAmount * 100;
+        return lottoYieldCalculator.run(result, purchaseAmount);
     }
 }
