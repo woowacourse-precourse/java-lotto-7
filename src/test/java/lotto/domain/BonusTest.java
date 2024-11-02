02e2d88d22cc21c4668c4c86d284542fa8e0d1b4
BaSak0630 <babamba125@gmail.com>
2024-11-02 19:00:48 +0900

    test : Add BonusTest
    범위, 값, 내부 로직 테스트 추가

---
+++ src/test/java/lotto/domain/BonusTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusTest {
+    @Test
+    @DisplayName("보너스 범위 검증")
+    public void bonusRangeTest() {
+        //given
+        String bonus1 = "-1";
+        String bonus2 = "46";
+        //expect
+        assertThrows(IllegalArgumentException.class, () -> {new Bonus(bonus1);});
+        assertThrows(IllegalArgumentException.class, () -> {new Bonus(bonus2);});
+    }
+
+    @Test
+    @DisplayName("파라미터 변환 검증")
+    public void bonusParseTest() {
+        //given
+        String bonus = "일";
+        //expect
+        assertThrows(IllegalArgumentException.class, () -> {new Bonus(bonus);});
+    }
+
+    @Test
+    @DisplayName("보너스 내부 값 확인 테스트")
+    public void getBonusTest() {
+        //given
+        String bonusNumber = "1";
+        Bonus bonus = new Bonus(bonusNumber);
+        //when
+        int value = bonus.getBonus();
+        //then
+        assertEquals(value, 1);
+    }
+}
