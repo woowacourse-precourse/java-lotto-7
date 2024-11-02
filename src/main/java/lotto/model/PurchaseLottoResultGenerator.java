09db33bd2ce3b685b1e7aa101d7f15e8712fdfe9
Chan531 <chan657312@gmail.com>
2024-11-02 20:34:02 +0900

    feat(PurchaseLottoResultGenerator): 로또 묶음 정보 2차원 정수 리스트로 변환

---
+++ src/main/java/lotto/model/PurchaseLottoResultGenerator.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchaseLottoResultGenerator {
+
+	public List<List<Integer>> generatePurchaseLottoResult(LottoBundle lottoBundle) {
+		List<Lotto> lottos = lottoBundle.getLottos();
+		List<List<Integer>> result = new ArrayList<>();
+		for (Lotto lotto : lottos) {
+			List<Integer> row = new ArrayList<>();
+			List<LottoNumber> numbers = lotto.getNumbers();
+			for (LottoNumber number : numbers) {
+				row.add(number.getNumber());
+			}
+			result.add(row);
+		}
+		return result;
+	}
+}
