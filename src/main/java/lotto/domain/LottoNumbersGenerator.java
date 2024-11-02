c42cc2404bdb207d71adfb953e2b5122b7f39142
eb9862 <luclead622@gmail.com>
2024-11-03 00:19:52 +0900

    fix: 테스트 코드에 대한 오류 수정
    - 출력 메시지와 다른 부분인 '.' 추가
    - 테스트 케이스가 불변 List 형태이므로 mutable 형태로 전환 후 sort

--- src/main/java/lotto/domain/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoNumbersGenerator.java
@@ -5,14 +5,15 @@ import static lotto.domain.LottoInfo.PICK_COUNT;
 import static lotto.domain.LottoInfo.START_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumbersGenerator {
 
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT);
-        numbers.sort(Comparator.naturalOrder());
-        return numbers;
+        List<Integer> mutableNumbers = new ArrayList<>( Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT));
+        mutableNumbers.sort(Comparator.naturalOrder());
+        return mutableNumbers;
     }
 }
--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -5,7 +5,7 @@ public enum OutputMessage {
     OUTPUT_PURCHASE_QUANTITY("\n%d개를 구매했습니다."),
     OUTPUT_EXCEPT_SECOND_PLACE_RESULT("%d개 일치 (%,d원) - %d개"),
     OUTPUT_SECOND_PLACE_RESULT("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
-    OUTPUT_EARNING_RATE("총 수익률은 %.1f%%입니다");
+    OUTPUT_EARNING_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String messageTemplate;
 
