341d3d1c1f1d024301f139187449db0c5d07888a
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 02:37:32 +0900

    feat: Output메시지 상수enum 작성

---
+++ src/main/java/lotto/constants/OutputMessage.java
@@ -0,0 +1,20 @@
+package lotto.constants;
+
+public enum OutputMessage {
+    GET_TOTAL_LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
+    GET_WINNING_STATISTICS_MESSAGE("\n당첨 통계"),
+    LINE_SEPARATOR("---"),
+    PRIZE_SEPARATOR(" - "),
+    RETURN_ON_INVESTMENT_IS("총 수익률은 "),
+    PERCENT_IS("%입니다."),
+    ;
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
