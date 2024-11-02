900eaf59f0b381800b540104bac684eb91b942dd
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-11-02 22:44:13 +0900

    feat: Add LottoValidator class for validating purchase amount
    LottoValidator 클래스에서 구입 금액이 1,000원 단위로 입력되었는지 확인하는 기능을 추가하였습니다.

--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -1,2 +1,12 @@
-package lotto;public class LottoValidator {
+package lotto;
+
+public class LottoValidator {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public static void validateAmount(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
 }
