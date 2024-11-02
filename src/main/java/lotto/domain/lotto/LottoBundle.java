1f360e85bdd9a519103eccfa126c480fd2c285ea
CheChe903 <cheche903@naver.com>
2024-11-02 21:04:42 +0900

    feat(LottoBundle): 로또 모음 객체 생성
    로또 모음이라는 이름을 가진 객체를 생성합니다.
    LottoBundle에 getter를 둔 이유는, 상세한 format까지 알면 의존성이 커질 가능성이 있어 getter를 둡니다.
    추후에 Lotto, LottoNumber도 고민해볼 필요가 있습니다.

---
+++ src/main/java/lotto/domain/lotto/LottoBundle.java
@@ -0,0 +1,29 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+import lotto.domain.generator.RandomNumberListGenerator;
+import lotto.domain.sorting.Sorter;
+
+public class LottoBundle {
+
+    private final List<Lotto> lottoBundle;
+    private final RandomNumberListGenerator randomNumberListGenerator;
+    private final Sorter sorter;
+
+    public LottoBundle(List<Lotto> lottoBundle, RandomNumberListGenerator randomNumberListGenerator, Sorter sorter) {
+        this.lottoBundle = lottoBundle;
+        this.randomNumberListGenerator = randomNumberListGenerator;
+        this.sorter = sorter;
+    }
+
+    public void generate(int lottoQuantity) {
+        LottoGenerator lottoGenerator = new LottoGenerator(randomNumberListGenerator, sorter);
+        for (int count = 0; count < lottoQuantity; count++) {
+            lottoBundle.add(lottoGenerator.generate());
+        }
+    }
+
+    public List<Lotto> getLottoBundle() {
+        return lottoBundle;
+    }
+}
