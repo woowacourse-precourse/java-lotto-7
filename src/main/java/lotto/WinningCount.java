3bc9d0e072a6fb75494a37bfe99cf10f7db49cdd
hyunwon <khw73850@gmail.com>
2024-11-02 15:21:36 +0900

    fix: resolve syntax error in enum WinningCount SIX

--- src/main/java/lotto/WinningCount.java
+++ src/main/java/lotto/WinningCount.java
@@ -5,7 +5,7 @@ public enum WinningCount {
     FOUR(4, false, 50000),
     FIVE(5, false, 1500000),
     FIVE_AND_BONUS(5,true, 30000000),
-    SIX(,6, false, 2000000000);
+    SIX(6, false, 2000000000);
 
     private int countOfWinningNumber;
     private boolean isBonusMatched;
