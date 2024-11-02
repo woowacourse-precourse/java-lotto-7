e529593f924e323594e0a25a0d85f739a7f7b694
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 03:04:45 +0900

    refactor: 기존 Controller에 있던 금액 파싱 클래스 분리

---
+++ src/main/java/lotto/utils/parser/AmountsParser.java
@@ -0,0 +1,38 @@
+package lotto.utils.parser;
+
+import static lotto.constants.ErrorMessage.IS_NOT_DIVISIBLE_BY_THOUSAND_WON;
+import static lotto.constants.ErrorMessage.OVER_MAX_AMOUNTS;
+import static lotto.constants.ErrorMessage.UNDER_MIN_AMOUNTS;
+import static lotto.constants.LottoConstant.PURCHASABLE_MAX_AMOUNTS;
+import static lotto.constants.LottoConstant.WON_1000;
+
+public class AmountsParser {
+
+    public static int getAmounts(String amounts) {
+        int lottoAmounts = Integer.parseInt(amounts);
+
+        checkMinAmounts(lottoAmounts);
+        checkMaxAmounts(lottoAmounts);
+        checkDivisibilityByThousand(lottoAmounts);
+
+        return lottoAmounts;
+    }
+
+    private static void checkMinAmounts(int lottoAmounts) {
+        if (lottoAmounts < WON_1000){
+            throw new IllegalArgumentException(UNDER_MIN_AMOUNTS.getMessage());
+        }
+    }
+
+    private static void checkMaxAmounts(int lottoAmounts) {
+        if (lottoAmounts > PURCHASABLE_MAX_AMOUNTS){
+            throw new IllegalArgumentException(OVER_MAX_AMOUNTS.getMessage());
+        }
+    }
+
+    private static void checkDivisibilityByThousand(int lottoAmounts) {
+        if ((lottoAmounts % WON_1000) != 0) {
+            throw new IllegalArgumentException(IS_NOT_DIVISIBLE_BY_THOUSAND_WON.getMessage());
+        }
+    }
+}
