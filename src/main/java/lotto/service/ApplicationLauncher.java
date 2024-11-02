4be81b3bfb0828fce949bad2e6b01530bd278957
whdmscks11 <whdmscks11@gmail.com>
2024-11-02 17:56:51 +0900

    feat(ApplicationLauncher): 프로그램의 전반적인 기능을 순차적으로 실행시켜주는 클래스

--- src/main/java/lotto/service/ApplicationLauncher.java
+++ src/main/java/lotto/service/ApplicationLauncher.java
@@ -1,4 +1,36 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoWinningNumbers;
+import lotto.io.InputHandler;
+import lotto.io.OutputHandler;
+
 public class ApplicationLauncher {
+    public static void run(){
+        try(InputHandler inputHandler = new InputHandler()){
+            OutputHandler.print(OutputHandler.ENTER_PURCHASE_AMOUNT);
+            String firstLine = inputHandler.readline();
+
+            LottoGenerator lottoGenerator = new LottoGenerator();
+            Lotto[] userLotto = lottoGenerator.generate(firstLine);
+
+            OutputHandler.printPurchasedQuantity(lottoGenerator);
+            OutputHandler.printPurchasedLotto(lottoGenerator);
+
+            OutputHandler.print(OutputHandler.ENTER_WINNING_NUMBERS);
+            String secondLine = inputHandler.readline();
+
+            OutputHandler.print(OutputHandler.ENTER_BONUS_NUMBER);
+            String thirdLine = inputHandler.readline();
+
+            LottoWinningNumbers winningLotto = new LottoWinningNumbers(secondLine, thirdLine);
+            winningLotto.generate();
+
+            LottoResultAnalyzer lottoResultAnalyzer = new LottoResultAnalyzer();
+            lottoResultAnalyzer.analyze(userLotto, winningLotto);
+
+            OutputHandler.printCompareResult(lottoResultAnalyzer);
+            OutputHandler.printTotalYield(lottoResultAnalyzer, lottoGenerator);
+        }
+    }
 }
