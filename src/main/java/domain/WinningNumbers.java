baddbe4eb4ff9238b9408190b63e60b2ed296795
sinji <tobeabestns@naver.com>
2024-11-03 01:42:05 +0900

    feat(WinningNumbers): 당첨 번호(6자)를 저장하는 리스트 생성

---
+++ src/main/java/domain/WinningNumbers.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningNumbers {
+
+    public static List<String> generateWinningNumbers(String winningNumber) {
+        List<String> winningNumberCollection = new ArrayList<>();
+        winningNumberCollection.add(winningNumber);
+        return winningNumberCollection;
+    }
+}
