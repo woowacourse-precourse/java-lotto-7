13491ff547b03cacfb3ac108f101d6c6ec592ccd
peanut990 <peanut1998@naver.com>
2024-11-03 00:14:03 +0900

    refactor(Parser): parseWinningNumbs()의 가독성 증가를 위한 리팩토링

--- src/main/java/lotto/service/Parser.java
+++ src/main/java/lotto/service/Parser.java
@@ -7,20 +7,22 @@ import lotto.validation.LottoValidator;
 
 public class Parser {
     public static List<Integer> parseWinningNumbs(String inputWinningNumbs) {
-        List<Integer> winningNumbs = new ArrayList<>();
-
         String[] winningNumbsStrs = inputWinningNumbs.split(",");
         for (String winningNumbsStr : winningNumbsStrs) {
             LottoValidator.validateEmptyString(winningNumbsStr);
         }
-
         LottoValidator.validateWinningNumbsSize(winningNumbsStrs);
+
+        return  createWinningnumbs(winningNumbsStrs);
+    }
+
+    private static List<Integer> createWinningnumbs(String[] winningNumbsStrs) {
+        List<Integer> winningNumbs = new ArrayList<>();
         for (String winningNumStr : winningNumbsStrs) {
             int winningNum = Utils.changeStringToNum(winningNumStr);
             LottoValidator.isLottoNumInRange(winningNum);
             winningNumbs.add(winningNum);
         }
-
         return winningNumbs;
     }
 }
