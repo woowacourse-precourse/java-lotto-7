8a39fb8dbc9fa40431bc4b2d3ee53f9d64c729cc
Hun0914 <bkh010914@naver.com>
2024-11-02 20:39:56 +0900

    feat : 등수 계산 기능 구현

--- src/main/java/lotto/PrizeService.java
+++ src/main/java/lotto/PrizeService.java
@@ -1,4 +1,31 @@
 package lotto;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PrizeService {
+
+    public Map<LottoRate, Integer> prizeService(List<Lotto> lottos, List<Integer> lottoNumbers, int bonusNumber) {
+        Map<LottoRate, Integer> prize = new HashMap<>();
+
+        prizeCalculate(lottos, lottoNumbers, bonusNumber, prize);
+
+        return prize;
+    }
+
+    private static void prizeCalculate(List<Lotto> lottos, List<Integer> lottoNumbers, int bonusNumber, Map<LottoRate, Integer> prize) {
+        for (Lotto lotto : lottos) {
+            int matchCount = (int) lotto.getNumbers().stream()
+                    .filter(lottoNumbers::contains)
+                    .count();
+
+            boolean bonusMatched = lotto.getNumbers().contains(bonusNumber);
+
+            LottoRate rate = LottoRate.match(matchCount, bonusMatched);
+
+            prize.put(rate, prize.getOrDefault(rate, 0) + 1);
+        }
+    }
+
+
 }
