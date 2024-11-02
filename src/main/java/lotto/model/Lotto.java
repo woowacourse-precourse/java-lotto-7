146164982a3b65dea8f4524a806966b9d4596424
Chan531 <chan657312@gmail.com>
2024-11-02 20:22:59 +0900

    refactor(Lotto): 로또 번호 개수 상수 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,7 +3,7 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    
+
     private static final int LOTTO_NUMBER_COUNT = 6;
     
     private final List<LottoNumber> numbers;
@@ -14,7 +14,7 @@ public class Lotto {
     }
 
     private void validate(List<LottoNumber> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
