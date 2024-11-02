254ae77d791c039b3eb8c41a00f6ca57fa1089ad
soohalee <suha630@naver.com>
2024-11-02 20:58:49 +0900

    feat(OutputView, Rank): 구매한 모든 로또 출력하는 기능 추가

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(2000000000, "6개 일치 ", 1),
+    SECOND(30000000, "5개 일치, 보너스 볼 일치 ", 2),
+    THIRD(1500000, "5개 일치 ", 3),
+    FOURTH(50000, "4개 일치 ", 4),
+    FIFTH(5000, "3개 일치 ", 5);
+
+    private int prize;
+    private String matchMessage;
+    private int rank;
+
+    Rank(int prize, String matchMessage, int rank) {
+        this.prize = prize;
+        this.matchMessage = matchMessage;
+        this.rank = rank;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getMatchMessage() {
+        return matchMessage;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+}
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.enums.OutputViewEnum;
+import lotto.model.Lotto;
+
 public class OutputView {
     private static final String ERROR = "[ERROR] ";
 
@@ -10,6 +14,13 @@ public class OutputView {
     public static void printPrompt(String string) {
         System.out.println(string);
     }
+
+    public static void printLotto(Lotto lotto) {
+        String lottoText = String.join(", ", lotto.getNumbers().stream().map(String::valueOf).toList());
+        lottoText = "[" + lottoText + "]";
+        System.out.println(lottoText);
+    }
+
     public static void printTickets(List<Lotto> tickets) {
         System.out.println(tickets.size() + OutputViewEnum.PURCHASE_COUNT_MESSAGE.getMessage());
         for (Lotto lotto : tickets) {
