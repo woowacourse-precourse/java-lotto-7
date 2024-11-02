99b742a6b4209f561e8d5d0d1da9fcf0d969c365
badgelatte <saucy5939@gmail.com>
2024-11-03 03:19:17 +0900

    LottoChecker class 추가
    로또 당첨 번호 입력 받고 변환하는 class 추가

---
+++ src/main/java/lotto/LottoChecker.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoChecker {
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public LottoChecker() {
+        this.winningNumbers = new ArrayList<>();
+    }
+
+}
