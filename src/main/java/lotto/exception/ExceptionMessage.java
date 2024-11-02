abed788f969c2dcfce0454a83f9955a785e752b0
Heejin Kim <gmlwls768@naver.com>
2024-11-03 02:27:52 +0900

    feat: customExcpetion 구현

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -5,13 +5,10 @@ public enum ExceptionMessage {
     ERROR_NOT_INTEGER("[ERROR] 숫자를 입력해주세요."),
     ERROR_NEGATIVE_NUMBER("[ERROR] 자연수를 입력해주세요."),
     ERROR_NOT_IN_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    ERROR_NULL("[ERROR] 값을 입력해주세요."),
-    ERROR_LOTTO_SIZE_NOT_MATCHED("[ERROR] 로또 번호는 6개여야 합니다.")
-    ;
-
+    ERROR_LOTTO_SIZE_NOT_MATCHED("[ERROR] 로또 번호는 6개여야 합니다."),
+    ERROR_DUPLICATE_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다.");
 
     private final String message;
-
 
     ExceptionMessage(String message) {
         this.message = message;
