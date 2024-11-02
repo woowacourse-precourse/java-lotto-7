75a7a11af6cd49b9cb8515e7630f05e36e92778d
KIM MINSEOK <mskim.glenn@gmail.com>
2024-11-02 17:13:25 +0900

    test: LottoGenerator 클래스에 로또 번호 생성 기능 테스트 추가

---
+++ src/test/java/lotto/util/LottoGeneratorTest.java
@@ -0,0 +1,41 @@
+package lotto.util;
+
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LottoGeneratorTest {
+
+    LottoGenerator lottoGenerator = new LottoGenerator();
+
+    @Test
+    @DisplayName("로또가 정상적으로 뽑히는지 테스트")
+    void generateLottoNumbersTest() {
+        // given
+        List<Integer> numbers = lottoGenerator.generateLottoNumbers();
+
+        // when
+        int numberOfLotto = Lotto.NUMBER_OF_LOTTO;
+
+        // then
+        assertEquals(numberOfLotto, numbers.size());
+    }
+
+    @Test
+    @DisplayName("로또 번호가 오름차순으로 정렬되었는지 테스트")
+    void generateLottoNumbersSortedTest() {
+        // given
+        List<Integer> numbers = lottoGenerator.generateLottoNumbers();
+
+        // when
+        List<Integer> sortedNumbers = numbers.stream().sorted().toList();
+
+        // then
+        assertEquals(sortedNumbers, numbers);
+    }
+
+}
