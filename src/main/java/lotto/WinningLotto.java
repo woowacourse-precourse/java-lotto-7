6b90f9bb7637784b7837117bf4ebc9d05cacc803
soobin <a01056239778@gmail.com>
2024-11-03 02:25:24 +0900

    feat: 당첨 번호와 보너스 번호 검증

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -10,6 +10,7 @@ public class WinningLotto {
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
+        validate();
     }
 
     public List<Integer> getNumbers() {
@@ -18,6 +19,38 @@ public class WinningLotto {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+
+    public void validate() {
+        checkTotalCount();
+        checkRange();
+        checkDuplicate();
+    }
+
+    private void checkTotalCount() {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkRange() {
+        numbers.stream().forEach(numbers -> validateRange(numbers));
+        validateRange(bonusNumber);
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 번호는 1부터 45사이여야 합니다.");
+        }
+    }
+
+    private void checkDuplicate() {
+        List<Integer> allNumbers = numbers;
+        allNumbers.add(bonusNumber);
+        if (allNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
     }
 
 }
