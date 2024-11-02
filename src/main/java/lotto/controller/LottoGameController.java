b3af0d6daa43d8535d19c79e4630de49021dc159
chaerish <ccyy8432@naver.com>
2024-11-03 02:44:47 +0900

    feat(LottoGameController): 로또 게임 흐름 관리하는 controller 구현
    사용자에게 로또 관련 입력을 받고, 로또를 발급해 통계를 내고, 결과를 출력하는 흐름을 통제하는 LottoGameController를 구현한다.

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,67 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.dto.LottoResult;
+import lotto.model.Lotto;
+import lotto.service.Converter;
+import lotto.service.LottoResultService;
+import lotto.service.LottoSeller;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.PurchaseAmountValidator;
+import lotto.validator.WinningNumberValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+    private final PurchaseAmountValidator purchaseAmountValidator;
+    private final WinningNumberValidator winningNumberValidator;
+    private final BonusNumberValidator bonusNumberValidator;
+    private final LottoSeller lottoSeller;
+    private LottoResultService lottoResultService;
+
+    public LottoGameController() {
+        this.purchaseAmountValidator = new PurchaseAmountValidator();
+        this.winningNumberValidator = new WinningNumberValidator();
+        this.bonusNumberValidator = new BonusNumberValidator();
+        this.lottoSeller = new LottoSeller();
+    }
+
+    public void start() {
+        int purchaseAmount = getPurchaseAmountAndValidate();
+        OutputView.displayPurchaseCount(lottoSeller.calculatePurchaseCount(purchaseAmount));
+
+        List<Lotto> lotto = lottoSeller.buyLotto(purchaseAmount); //로또를 삼
+        OutputView.displayPurchaseLotto(lotto);
+
+        List<Integer> winningNumbers = getWinningNumbersAndValidate();
+        int bonusNumber = getBonusNumberAndValidate(winningNumbers);
+
+        LottoResult lottoResult = getLottoResult(winningNumbers, bonusNumber, lotto);
+        OutputView.displayResult(lottoResult);
+
+    }
+
+    private LottoResult getLottoResult(List<Integer> winningNumbers, int bonusNumber, List<Lotto> lotto) {
+        lottoResultService = new LottoResultService(lotto, winningNumbers, bonusNumber);
+        return lottoResultService.getLottoResult();
+    }
+
+    private int getPurchaseAmountAndValidate() {
+        String purchaseAmount = InputView.getLottoPurchaseAmount();
+        purchaseAmountValidator.validate(purchaseAmount);
+        return Converter.integerConvert(purchaseAmount);
+    }
+
+    private List<Integer> getWinningNumbersAndValidate() {
+        String winningNumbers = InputView.getWinningNumbers();
+        winningNumberValidator.validate(winningNumbers);
+        return Converter.winningNumbersConvert(winningNumbers);
+    }
+
+    private int getBonusNumberAndValidate(List<Integer> winningNumbers) {
+        String bonusNumber = InputView.getBonusNumber();
+        bonusNumberValidator.validate(winningNumbers, bonusNumber);
+        return Converter.integerConvert(bonusNumber);
+    }
+
+}
