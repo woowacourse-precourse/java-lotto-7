465ac0d62fd8cffeb26e43af18a6665e4231a3b3
seaniiio <cnc4750@naver.com>
2024-11-02 23:23:51 +0900

    style(LottoRank): 상수 선언 순서 변경

--- src/main/java/lotto/constant/LottoRank.java
+++ src/main/java/lotto/constant/LottoRank.java
@@ -3,11 +3,11 @@ package lotto.constant;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(2_000_000_000, 6, "6개 일치 (2,000,000,000원)"),
-    SECOND(30_000_000, 5, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(1_500_000, 5, "5개 일치 (1,500,000원)"),
+    FIFTH(5_000, 3, "3개 일치 (5,000원)"),
     FORTH(50_000, 4,"4개 일치 (50,000원)"),
-    FIFTH(5_000, 3, "3개 일치 (5,000원)");
+    THIRD(1_500_000, 5, "5개 일치 (1,500,000원)"),
+    SECOND(30_000_000, 5, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(2_000_000_000, 6, "6개 일치 (2,000,000,000원)");
 
     private static final String HYPHEN = " - ";
     private static final String LOTTO_UNIT = "개";
