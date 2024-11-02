7be4c48f16801da38eb6b7c121b9b2d648538f66
sc@t0a14 <gusdn6671@gmail.com>
2024-11-02 18:33:15 +0900

    refactor(LottoPurchase): 로또 구입 금액 입력 정적 팩토리 메서드 변환

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -4,9 +4,13 @@ public class LottoPurchase {
     private final int amount;
     private final int ticketCount;
 
-    public LottoPurchase(int amount) {
+    private LottoPurchase(int amount) {
         this.amount = amount;
         this.ticketCount = amount / 1000;
+    }
+
+    public static LottoPurchase of(int amount) {
+        return new LottoPurchase(amount);
     }
 
     public int getAmount() {
