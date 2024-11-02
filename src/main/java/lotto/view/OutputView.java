48c99957ecfa10b95f040c249f1b11e110b3acd6
chae-ring <149919388+chae-ring@users.noreply.github.com>
2024-11-02 15:36:05 +0900

    feat(OutputView): 결과문구 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,4 +9,8 @@ public class OutputView {
     public static void printCount(int ticketCount){
         System.out.println(ticketCount+"개를 구매했습니다.");
     }
+    public static void printTotal(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
 }
