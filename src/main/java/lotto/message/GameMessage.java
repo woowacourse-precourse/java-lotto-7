c84ba8ff2959bd6174c0182c49b85cff0bbd3f63
hyeblee <hyeb1224@naver.com>
2024-11-02 15:14:37 +0900

    feat: GameMessage에 게임 진행 관련 메시지 추가
    ENTER_PURCHASE_AMOUNT: 구입금액 입력 요청 메시지
    ENTER_WINNING_NUMBER: 당첨 번호 입력 요청 메시지
    ENTER_BONUS_NUMBER: 보너스 번호 입력 요청 메시지
    WINNING_STATISTICS: 당첨 결과 안내 메시지

--- src/main/java/lotto/message/GameMessage.java
+++ src/main/java/lotto/message/GameMessage.java
@@ -1,4 +1,19 @@
 package lotto.message;
 
 public enum GameMessage {
+    ENTER_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    ENTER_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("당첨 통계\n" + "---")
+    ;
+
+    private String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
