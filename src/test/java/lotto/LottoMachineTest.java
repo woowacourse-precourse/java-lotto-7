c109165712e50f8c823938f0a1ac1eb36a3cdd53
sejinezy <psj8880@naver.com>
2024-11-02 16:58:50 +0900

    test(LottoMachineTest): 금액에 맞게 로또 티켓이 생성되는 지 테스트

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,17 @@
+package lotto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoMachineTest {
+
+    @Test
+    void 금액에_맞게_로또_티켓이_생성된다(){
+        LottoMachine lottoMachine = new LottoMachine();
+        int amount = 5000;
+
+        LottoTicket lottoTicket = lottoMachine.generateLottoTicket(amount);
+
+        assertThat(lottoTicket.getLottos()).hasSize(5);
+    }
+}
