6f1236bdb3fe13be803d12af14e795ea92d94a7b
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-11-02 21:17:09 +0900

    feat(LottoController): 로또 발행 및 출력 기능 추가

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,11 +1,24 @@
 package lotto.Controller;
 
+import lotto.Model.Lotto;
+import lotto.Model.LottoGenerator;
 import lotto.View.InputView;
+import lotto.View.OutputView;
+
+import java.util.List;
 
 public class LottoController {
 
     public void lottoController() {
         int price = InputView.priceInput();
+
+        LottoGenerator generator = new LottoGenerator();
+        List<Lotto> allLottos = generator.generateLottos(price);
+        OutputView.showLottoCount(allLottos.size());
+        OutputView.showAllLottos(allLottos);
     }
+
+
+
 }
 
