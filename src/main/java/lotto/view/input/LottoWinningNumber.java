65c4fe98b8b68878025600337d932be4b65ec7f3
Ahnsukjoo <bos98@naver.com>
2024-11-03 06:24:28 +0900

    feat: 로또 당첨 번호 입력 기능 구현
    LottoWinningNumber 클래스를 추가하여 로또 당첨 번호 입력 기능을 구현했습니다.
    
    - 사용자로부터 당첨 번호와 보너스 번호를 입력받는 기능 추가했습니다.
    - 입력받은 번호를 파싱하여 정수 리스트로 변환하는 기능 구현했습니다.
    - 당첨 번호와 보너스 번호를 하나의 HashMap으로 결합하는 기능 추가했습니다.
    
    이 구현은 기본적인 입력 처리만을 포함하고 있으며,
    향후 입력값 유효성 검사 및 예외 처리 기능을 추가할 예정입니다.

--- src/main/java/lotto/view/input/LottoWinningNumber.java
+++ src/main/java/lotto/view/input/LottoWinningNumber.java
@@ -1,9 +1,37 @@
 package lotto.view.input;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
+
 public class LottoWinningNumber {
 
-    public void input() {
+    private HashMap<Integer, String> winningNumbers = new HashMap<>();
 
+    public HashMap<Integer, String> input() {
+        InputMessageEnum.WINNING_NUMBERS.printMessage();
+        String winningNumber = readLine();
+        InputMessageEnum.BONUS_NUMBER.printMessage();
+        String bonusNumber = readLine();
+
+        return combineWinningNumbersAndBonus(winningNumber, bonusNumber);
     }
-    // 로또 당첨번호에 대한 예외처리 구현
+
+    private HashMap<Integer, String> combineWinningNumbersAndBonus(String winningNumber, String bonusNumber) {
+        Lotto numbers = new Lotto(parseNumbers(winningNumber));
+        numbers.getNumbers().forEach(n -> winningNumbers.put(n, "winningNumber"));
+        winningNumbers.put(Integer.parseInt(bonusNumber), "bonusNumber");
+        return winningNumbers;
+    }
+
+    private List<Integer> parseNumbers(String numbers) {
+        return Arrays.stream(numbers.split(","))
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
 }
