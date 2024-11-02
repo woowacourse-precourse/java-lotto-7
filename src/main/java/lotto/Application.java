e16312efa1c985c7b07c15178c3726a1997dc5be
jieun <ju3391@naver.com>
2024-11-02 19:20:39 +0900

    feat(Application): 메인 애플리케이션 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.presentation.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController controller = new LottoController();
+        controller.run();
     }
 }
