e9106cad6a9a73b563993fa28f279c5a8b7efa0b
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-11-02 14:29:43 +0900

    feat: OutputView, 더미 실행문구 작성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class OutputView {
+
+    // 메서드 명 길이에 대해 고민해보기
+    // 메서드 명
+    public void purchaseLottoAmountMesssage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    public void purchaseLottoCountMessage(List<Integer> numbers) {
+        System.out.println(numbers.size() + "개를 구매했습니다.");
+    }
+
+    public void enterWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void enterBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public void WinningStatistics() {
+        System.out.println("당첨 통계");
+    }
+
+    public void matchWinningNumber(int matchCount) {
+        System.out.print(matchCount + "개 일치" + "(---원)");
+    }
+    public void matchWinningCount(int count) {
+        System.out.println(" - " + count + "개");
+    }
+
+    public void promptTotalReturnRate(String totalReturnRate) {
+        System.out.println("총 수익률은 " + "totalReturnRate" + "%입니다.");
+    }
+
+    public void showLottoNumber(List<Lotto> lottoNumbers) { }
+
+
+}
