b2a1711e95e19dee9ff68cf5047f4cc9348dd5e2
mipangg <morganjeong119@gmail.com>
2024-11-02 18:13:41 +0900

    feat(Prize): store match count, bonus match and prize money in enum

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,4 +1,31 @@
 package lotto.model;
 
 public enum Prize {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000);
+
+    private int count;
+    private boolean bonusNumber;
+    private int prizeMoney;
+
+    Prize(int count, boolean bonusNumber, int prizeMoney) {
+        this.count = count;
+        this.bonusNumber = bonusNumber;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isBonusNumber() {
+        return bonusNumber;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
 }
