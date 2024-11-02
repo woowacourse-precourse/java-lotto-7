d23b01be5275c5431ae5e1d8a73c45ac1945d209
Hyso <hsk7953@gmail.com>
2024-11-03 00:06:50 +0900

    refactor(LottoTest) : 테스트코드 패키지 이동

---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningNumbersTest {
+
+//    @DisplayName("이미 가지고 있는 로또 번호를 보너스 번호로 입력할 시 예외가 발생한다.")
+//    @Test
+//    void
+
+}
