cba60ac2cf53768470af7f186d518569db753d53
Park, Sojeong <sojeong2101@gmail.com>
2024-11-03 02:04:27 +0900

    fix($MyLotto): MyLotto 클래스를 record로 변경

--- src/main/java/lotto/purchase/MyLotto.java
+++ src/main/java/lotto/purchase/MyLotto.java
@@ -3,13 +3,7 @@ package lotto.purchase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyLotto {
-
-    private final List<Integer> numbers;
-
-    public MyLotto(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
+public record MyLotto(List<Integer> numbers) {
 
     public int countMatches(List<Integer> winningLotto) {
         List<Integer> myLottoNumbers = new ArrayList<>(numbers);
