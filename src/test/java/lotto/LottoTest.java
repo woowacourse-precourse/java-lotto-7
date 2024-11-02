2038e3b3c9dc53721db3377fba6d396fcb3f87a3
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-03 01:08:40 +0900

    test: lotto 클래스 테스트 케이스 작성
    - 로또 번호의 개수가 6개가 넘을 경우 예외가 발생하는지 테스트합니다
    - 로또 번호에 중복된 숫자가 있을 경우 예외가 발생하는지 테스트합니다
    - 당첨 등수의 형태가 잘못되었을 때 예외가 발생하는지 테스트합니다
    - 당첨 등수의 형태가 올바를 때 당첨 등수가 필드에 올바르게 저장되는지 테스트합니다

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
@@ -22,4 +26,26 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("당첨 등수가 0~5 범위에 해당하는 숫자가 아니면 예외가 발생한다")
+    void failSetPrizeTierTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(()-> lotto.setPrizeTier(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> lotto.setPrizeTier(6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 등수가 0~5 범위의 정수일 경우 등수 필드에 저장된다")
+    void passSetPrizeTierTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lotto.setPrizeTier(0);
+
+        assertThat(lotto.getPrizeTier()).isEqualTo(0);
+    }
+
+
+
 }
