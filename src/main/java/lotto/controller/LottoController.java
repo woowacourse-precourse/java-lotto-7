9cae89c3b794389abf70220459f3695b92087b7b
sese2204 <sese2204@naver.com>
2024-11-02 20:54:52 +0900

    feat(controller): 로또 게임 컨트롤러 구현
    - 로또 구매부터 결과 출력까지의 전체 게임 흐름 제어
    - InputView를 통한 구매 금액, 당첨 번호, 보너스 번호 입력 처리
    - LottoStore를 통한 로또 구매 프로세스 관리
    - WinningResult를 통한 당첨 결과 계산
    - OutputView를 통한 구매 내역, 당첨 통계, 수익률 출력
    - 잘못된 입력에 대한 예외 처리 및 재시도 로직 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,52 @@
+package lotto.controller;
+
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoStore;
+import lotto.domain.Lottos;
+import lotto.domain.Prize;
+import lotto.domain.WinningResult;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoStore store;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.store = new LottoStore();
+    }
+
+    public void run(){
+        int purchaseAmount = inputView.RequestPurchaseAmount();
+        Lottos lottos = purchaseLotto(purchaseAmount);
+        WinningResult winningResult = createWinningResult();
+        printResults(lottos, winningResult);
+    }
+
+    private Lottos purchaseLotto(int purchaseAmount) {
+        Lottos lottos = store.sell(purchaseAmount);
+        outputView.printPurchaseResult(lottos);
+        return lottos;
+    }
+
+    private WinningResult createWinningResult() {
+            try {
+                Lotto winningLotto = new Lotto(inputView.RequestWinningNumbers());
+                int bonusNumber = inputView.RequestBonusNumber();
+                return new WinningResult(winningLotto, bonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return createWinningResult();
+            }
+    }
+
+    private void printResults(Lottos lottos, WinningResult winningResult) {
+        Map<Prize, Integer> prizeResult = winningResult.calculateResult(lottos);
+        outputView.printWinningStatistics(prizeResult);
+        outputView.printReturnRate(winningResult.calculateReturnRate(lottos));
+    }
+}
