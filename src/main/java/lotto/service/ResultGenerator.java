f6dc46a2c8cf83af58202be516097483d923338c
world-dv <goodforx0147@naver.com>
2024-11-02 19:55:16 +0900

    feat: 로또 당첨 판단 로직 추가
    - 로또 당첨 판단을 위해 생성된 로또들과 당첨 번호의 일치 개수를 계산합니다.
    - 로또 당첨 판단을 위해 생성된 로또들과 보너스 번호의 일치 개수를 계산합니다.

---
+++ src/main/java/lotto/service/ResultGenerator.java
@@ -0,0 +1,47 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
+
+public class ResultGenerator {
+
+    private final List<Long> winningResult = new ArrayList<>();
+    private final List<Integer> bonusResult = new ArrayList<>();
+
+    public ResultGenerator(List<Lotto> lottoTicket, Lotto winning, Bonus bonus) {
+        compareTicketToWinning(lottoTicket, winning);
+        compareTicketToBonus(lottoTicket, bonus);
+    }
+
+    private void compareTicketToWinning(List<Lotto> lottoTicket, Lotto winning) {
+        lottoTicket.forEach(lotto -> countWinning(lotto, winning));
+    }
+
+    private void countWinning(Lotto lotto, Lotto winning) {
+        winningResult.add(lotto.getNumbers().stream()
+                .filter(number -> winning.getNumbers().contains(number))
+                .count());
+    }
+
+    private void compareTicketToBonus(List<Lotto> lottoTicket, Bonus bonus) {
+        lottoTicket.forEach(lotto -> countBonus(lotto, bonus));
+    }
+
+    private void countBonus(Lotto lotto, Bonus bonus) {
+        if (lotto.getNumbers().contains(bonus.getNumber())) {
+            bonusResult.add(1);
+            return;
+        }
+        bonusResult.add(0);
+    }
+
+    public List<Long> getWinningResult() {
+        return winningResult;
+    }
+
+    public List<Integer> getBonusResult() {
+        return bonusResult;
+    }
+}
