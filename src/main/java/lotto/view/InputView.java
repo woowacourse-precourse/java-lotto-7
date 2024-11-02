cf43a380a5667c41d752593542bf82cc92a17636
장혜민 <gpals5987@gmail.com>
2024-11-03 06:22:52 +0900

    [feat] inputView 작성
    - readPurchaseAmount
    - readWinningNumber
    - readBonusNumber

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String readPurchaseAmount(){
+        return Console.readLine();
+    }
+    public static String readWinningNumber(){
+        return Console.readLine();
+    }
+    public static String readBonusNumber(){
+        return Console.readLine();
+    }
+
+}
