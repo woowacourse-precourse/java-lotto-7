d1175aac8b00851646df5fc0b9349bcde0cfe156
daxxn <dahyeonhan4529@gmail.com>
2024-11-03 02:06:15 +0900

    test(enum): add lotto grade enum test for validation
    - Implement LottoGradeTest to verify correctness of getGrade method

--- src/main/java/lotto/model/LottoGrade.java
+++ src/main/java/lotto/model/LottoGrade.java
@@ -4,14 +4,14 @@ import lotto.error.CustomException;
 import lotto.error.ExceptionMessage;
 
 public enum LottoGrade {
-    FIRST_GRADE(1, 6, 2000000000),
-    SECOND_GRADE(2, 5, 30000000),
-    THIRD_GRADE(3, 5, 1500000),
-    FOURTH_GRADE(4, 4, 50000),
-    FIFTH_GRADE(5, 3, 5000),
-    SIXTH_GRADE(6, 2, 0),
+    EIGHTH_GRADE(8, 0, 0),
     SEVENTH_GRADE(7, 1, 0),
-    ;
+    SIXTH_GRADE(6, 2, 0),
+    FIFTH_GRADE(5, 3, 5000),
+    FOURTH_GRADE(4, 4, 50000),
+    THIRD_GRADE(3, 5, 1500000),
+    SECOND_GRADE(2, 5, 30000000),
+    FIRST_GRADE(1, 6, 2000000000);
 
     private final int ranking;
     private final int correctCount;
@@ -36,15 +36,15 @@ public enum LottoGrade {
     }
 
     public static LottoGrade getGrade(int correctCount, boolean isCorrectBonus) {
-        return switch (correctCount) {
-            case 1 -> SEVENTH_GRADE;
-            case 2 -> SIXTH_GRADE;
-            case 3 -> FIFTH_GRADE;
-            case 4 -> FOURTH_GRADE;
-            case 5 -> isCorrectBonus ? SECOND_GRADE : THIRD_GRADE;
-            case 6 -> FIRST_GRADE;
-            default -> throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_VALID_CORRECT_COUNT);
-        };
+        if (isCorrectBonus && correctCount == SECOND_GRADE.getCorrectCount()) {
+            return SECOND_GRADE;
+        }
+        for (LottoGrade grade : LottoGrade.values()) {
+            if (grade.getCorrectCount() == correctCount) {
+                return grade;
+            }
+        }
+        throw new CustomException(ExceptionMessage.ERROR_MESSAGE_IS_NOT_VALID_CORRECT_COUNT);
     }
 
 }
