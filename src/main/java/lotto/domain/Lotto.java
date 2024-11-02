6e337d93ef83bd3aca6943b2abc47bd38fc59188
minzzun99 <skg06123@naver.com>
2024-11-02 15:57:18 +0900

    feat(Lotto): 중복 제거 및 검증 로직 추가
    - validateDuplication() 메서드로 중복을 제거하고 중복이 있을 시에 예외를 발생

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
+import static lotto.constant.ErrorMessage.DUPLICATION_ERROR;
 import static lotto.constant.ErrorMessage.NUMBER_COUNT_ERROR;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private static final int NUMBER_COUNT = 6;
@@ -12,6 +15,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
         sortLottoNumbers(numbers);
         this.numbers = numbers;
     }
@@ -19,6 +23,13 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != NUMBER_COUNT) {
             throw new IllegalArgumentException(NUMBER_COUNT_ERROR.getMessage());
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        HashSet<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() != NUMBER_COUNT) {
+            throw new IllegalArgumentException(DUPLICATION_ERROR.getMessage());
         }
     }
 
