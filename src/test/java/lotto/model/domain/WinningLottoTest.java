3446a96feb2ae4e3bcb60c4f149c38141e106fd8
soeun2537 <soeun2537@gmail.com>
2024-11-02 14:47:37 +0900

    test: getBonusNumber 호출 시 WinningLotto의 보너스 번호를 반환하는지 확인하는 단위 테스트 추가

--- src/test/java/lotto/model/domain/WinningLottoTest.java
+++ src/test/java/lotto/model/domain/WinningLottoTest.java
@@ -40,4 +40,20 @@ class WinningLottoTest {
         assertThat(retrievedNumbers).containsExactly(1, 2, 3, 4, 5, 6);
 
     }
+
+    @Test
+    @DisplayName("getBonusNumber 호출 시 WinningLotto의 보너스 번호를 반환하는지 확인")
+    void getBonusNumber() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Integer bonusNumber = 7;
+        WinningLotto winningLotto = WinningLotto.of(numbers, bonusNumber);
+
+        // when
+        Integer retrievedBonusNumber = winningLotto.getBonusNumber();
+
+        // then
+        assertThat(retrievedBonusNumber).isEqualTo(7);
+
+    }
 }
