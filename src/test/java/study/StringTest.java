1083db630285070f0d6c95a76899d8809e281184
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-02 20:57:12 +0900

    test(StringTest): String 클래스의 charAt() 메서드의 기능 확인 테스트 추가
    - 첫번째 문자 가져오는 테스트 추가
    - 마지막 문자 가져오는 테스트 추가
    - @DisplayName 어노테이션 적용

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -30,5 +31,21 @@ public class StringTest {
         String str = "(1,2)";
         str = str.substring(1, str.length() - 1);
         assertThat(str).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String의 첫번째 문자를 가져온다.")
+    void 첫번째_위치의_문자_가져오기() {
+        String str = "abc";
+        char ch = str.charAt(0);
+        assertThat(ch).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("String의 마지막 문자를 가져온다.")
+    void 마지막_위치의_문자_가져오기() {
+        String str = "abc";
+        char ch = str.charAt(str.length() - 1);
+        assertThat(ch).isEqualTo('c');
     }
 }
