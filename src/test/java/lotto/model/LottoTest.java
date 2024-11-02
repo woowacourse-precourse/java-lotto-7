bf8f5364a56d1f681a59db30b88e146a19017061
Hanjjong <tim4974@naver.com>
2024-11-03 02:03:57 +0900

    test: 로또 객체 생성 테스트 추가

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+public class LottoTest {
+
+    @ParameterizedTest
+    @MethodSource("validLottoNumberProvider")
+    @DisplayName("로또 객체 생성 테스트")
+    public void createLottoTest(List<Integer> numbers) {
+        assertThatNoException().isThrownBy(() -> new Lotto(numbers));
+    }
+
+    private static Object[][] validLottoNumberProvider() {
+        return new Object[][] {
+                {List.of(1,2,3,4,5,6)},
+                {List.of(2,3,4,5,6,7)},
+                {List.of(12,23,34,45,11,25)}
+        };
+    }
+}
