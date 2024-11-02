90b3c41044738721b39d2905628955af806c54fa
leeedohyun <ldh19738@gmail.com>
2024-11-02 18:28:06 +0900

    feat: 로또 게임

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.io.ConsoleInputHandler;
+import lotto.io.ConsoleOutputHandler;
+import lotto.lotto.LottoGame;
+import lotto.lotto.LottoNumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame(new ConsoleInputHandler(), new ConsoleOutputHandler(), new LottoNumberGenerator());
+        lottoGame.run();
     }
 }
