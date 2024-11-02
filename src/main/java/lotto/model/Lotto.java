9ebff4ca0ed7ca63de1639b720226ddc319f89e2
WARDKEY <tjrmsduq@naver.com>
2024-11-02 14:43:27 +0900

    refactor(Lotto) : Validate관련 메서드 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,29 +11,48 @@ public class Lotto {
     }
 
     private void validateLotto(List<Integer> numbers) {
+        isLottoSizeSix(numbers);
+
+        isLottoNotDuplicated(numbers);
+
+        for (Integer number : numbers) {
+            isLottoBlank(number);
+
+            isLottoNegative(number);
+
+            isLottoInRange(number);
+        }
+
+    }
+
+    private void isLottoSizeSix(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
 
+    private void isLottoNotDuplicated(List<Integer> numbers) {
         if (numbers.stream().distinct().toList().size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
         }
+    }
 
-        for (Integer number : numbers) {
-            if (number == null || String.valueOf(number).isBlank()) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호를 입력해주세요.");
-            }
-
-            if (number < 0) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 음수를 입력할 수 없습니다.");
-            }
-
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.");
-            }
-
+    private void isLottoBlank(Integer number) {
+        if (number == null || String.valueOf(number).isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호를 입력해주세요.");
         }
+    }
 
+    private void isLottoNegative(Integer number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 음수를 입력할 수 없습니다.");
+        }
+    }
+
+    private void isLottoInRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자를 입력해주세요.");
+        }
     }
 
     public List<Integer> getNumbers() {
