2d7ba6558473ffdadeb2b9afaea373d55c101349
JiEung2 <181757@jnu.ac.kr>
2024-11-03 00:44:11 +0900

    test(inputViewTest): 당첨번호 입력 개수 예외 테스트 추가

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,6 +1,8 @@
 package view;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.exception.Exception.LOTTO_NUMBER_SIZE_MUST_BE_SIX;
+import static lotto.validator.LottoNumbersValidator.validate;
 import static org.assertj.core.api.Assertions.*;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -45,12 +47,23 @@ class InputViewTest {
 
     @Nested
     class 당첨번호_입력_테스트 extends NsTest {
-        @DisplayName("구입금액 입력 성공 테스트")
+        @DisplayName("당첨번호 입력 성공 테스트")
         @Test
         void inputWinningNumbers_success() {
             assertSimpleTest(() -> {
                 run("1,2,3,4,5,6");
                 assertThat(output()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+            });
+
+        }
+
+        @DisplayName("당첨번호 입력 개수 예외 테스트")
+        @Test
+        void inputWinningNumbers_fail_notSix() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1,2,3,4,5,6,7"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(LOTTO_NUMBER_SIZE_MUST_BE_SIX.getMessage());
             });
 
         }
