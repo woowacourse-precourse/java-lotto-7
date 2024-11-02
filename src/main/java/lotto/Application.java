2658127ca3b3cdd3c8f2d4f986f8e05cb09dd789
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 02:20:07 +0900

    feat(Application): 유저에게서 값 입력받는 로직 추가
    - 구입금액 관련 예외처리 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.LottoConstants.LOTTO_PRICE;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money;
+        int lottoCount;
+        List<Lotto> lottos = new ArrayList<>();
+
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            money = Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+        }
+
+        if (money % LOTTO_PRICE == 0) {
+            lottoCount = money / LOTTO_PRICE;
+        } else {
+            throw new IllegalArgumentException("[ERROR] 1000원으로 나눠 떨어지는 값을 입력해야 합니다.");
+        }
     }
 }
