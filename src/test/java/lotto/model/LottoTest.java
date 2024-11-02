ad669252193526db8d4a0b2affd5da7e65c2904c
KJungW <sagakkb0707@naver.com>
2024-11-02 19:43:54 +0900

    test(model): Lotto 모델 테스트 추가
    추가된 테스트
    - 올바른 로또번호를 입력해 Lotto모델을 생성하는
      정상흐름 테스트
    - Lotto모델에 저장된 로또번호가 오름차순으로
      정렬되는지 테스트
    - Lotto모델에 저장된 로또번호가 문자열로 올바르게
      출력되는지 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,6 +18,16 @@ class LottoTest {
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바른 로또 번호를 입력해 객체 생성을 시도")
+    @Test
+    void 올바른_로또_번호를_입력해_객체_생성을_시도() {
+        List<Integer> inputNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        Lotto actualNumber = new Lotto(inputNumbers);
+
+        assertThat(actualNumber.getNumbers()).containsExactlyElementsOf(inputNumbers);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -43,5 +54,26 @@ class LottoTest {
                 Arguments.of(List.of(1, 2, 3, 4, 5, 46), Lotto.OUT_OF_RANGE_NUMBER_EXCEPTION_MESSAGE),
                 Arguments.of(List.of(0, 2, 3, 4, 5, 6), Lotto.OUT_OF_RANGE_NUMBER_EXCEPTION_MESSAGE)
         );
+    }
+
+    @DisplayName("객체에 저장된 로또번호들은 오름차순으로 정렬됨")
+    @Test
+    void 객체에_저장된_로또번호들은_오름차순으로_정렬됨() {
+        List<Integer> inputNumbers = List.of(6, 5, 4, 3, 2, 1);
+        List<Integer> expectedSortedNumbers = inputNumbers.reversed();
+
+        Lotto actualNumber = new Lotto(inputNumbers);
+
+        assertThat(actualNumber.getNumbers()).containsExactlyElementsOf(expectedSortedNumbers);
+    }
+
+    @DisplayName("객체에 저장된 로또번호를 출력")
+    @Test
+    void 객체에_저장된_로또번호를_출력() {
+        Lotto givenLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        String actualPrintResult = givenLotto.printLottoNumbers();
+
+        assertThat(actualPrintResult).isEqualTo("1, 2, 3, 4, 5, 6");
     }
 }
