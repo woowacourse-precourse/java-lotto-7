21d76f0db9c13b8ab26024930431c3991198ad77
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-03 04:41:16 +0900

    feat(WinningNumber): 당첨 번호 검증 기능 추가
    당첨 번호가 6개인지, 6개의 숫자가 중복되지 않은 숫자인지 검증하는 기능을 구현했습니다.

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,14 +1,36 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.message.ErrorMessage;
 
 public class WinningNumber {
     private final List<Integer> numbers;
     private final int bonusNumber;
 
     public WinningNumber(List<Integer> numbers, int bonusNumber) {
+        validate(numbers);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_EXCEED);
+        }
+
+        if (isDuplication(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBERS);
+        }
+    }
+
+    private boolean isDuplication(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (numbers.size() != distinctNumbers.size()) {
+            return true;
+        }
+        return false;
     }
 
     public List<Integer> getNumbers() {
