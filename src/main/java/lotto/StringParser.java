86e78103a73619fa18682b3b68e679908c5e1844
delena0702 <delena0702@gmail.com>
2024-11-03 02:12:48 +0900

    fix: parseInt 함수를 모두 StringParser 내의 함수로 수정
    - 오류 처리를 한 곳에 모아서 하기 위함.

--- src/main/java/lotto/StringParser.java
+++ src/main/java/lotto/StringParser.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringParser {
     public static int parseInt(String input) {
         try {
@@ -7,5 +10,9 @@ public class StringParser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
         }
+    }
+
+    public static List<Integer> parseIntListByComma(String input) {
+        return Arrays.stream(input.split(",", -1)).map(StringParser::parseInt).toList();
     }
 }
