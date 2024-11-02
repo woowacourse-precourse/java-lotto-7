b983277bbe9013950fffa3af7322bd3b360f8196
hyojun <hjlim45678@gmail.com>
2024-11-02 09:36:23 +0900

    feat: "로또 구입 금액 입력" 기능 구현

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.InputView;
+
+public class LottoController {
+    public void run() {
+        InputView.readInputMoney();
+
+    }
+
+
+}
