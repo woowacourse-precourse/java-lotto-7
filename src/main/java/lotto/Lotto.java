5dc918b8419b141080fecbd8a313d51e01d925c3
jieun <ju3391@naver.com>
2024-11-02 19:23:28 +0900

    refactor: 파일 패키지 이동 및 기존 파일 삭제

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,22 @@
-package lotto;
-
-import java.util.List;
-
-public class Lotto {
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    // TODO: 추가 기능 구현
-}
+//package lotto;
+//
+//import java.util.List;
+//
+//public class Lotto {
+//    private final List<Integer> numbers;
+//
+//    public Lotto(List<Integer> numbers) {
+//        validate(numbers);
+//        this.numbers = numbers;
+//    }
+//
+//    private void validate(List<Integer> numbers) {
+//        if (numbers.size() != 6) {
+//            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+//        }
+//    }
+//
+//    public List<Integer> getNumbers() {
+//        return numbers; // 외부에서 읽기만 가능하도록 설정
+//    }
+//}
