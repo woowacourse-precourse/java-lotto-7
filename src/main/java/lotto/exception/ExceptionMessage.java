6afd7846921a4afc1a398be5071de343828c56c3
jintakkim <mine407603@gmail.com>
2024-11-02 19:32:29 +0900

    feat(ExceptionMessage): 예외 메세지 추가
    필요한 예외 메세지를 추가한다.

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,12 +1,14 @@
 package lotto.exception;
 
 public enum ExceptionMessage {
-    NEGATIVE_INPUT("[ERROR] 음수 입력은 허용되지 않습니다"),
-    THOUSAND_UNIT_ONLY("[ERROR] 천 단위 입력만 허용됩니다"),
+    NEGATIVE_INPUT("[ERROR] 음수 입력은 허용되지 않습니다."),
+    THOUSAND_UNIT_ONLY("[ERROR] 천 단위 입력만 허용됩니다."),
     INVALID_NUMBER("[ERROR] 입력이 숫자형식이 아니거나 입력 가능값을 초과했습니다."),
-    NO_SUCH_ELEMENT("[ERROR] 입력값이 없습니다.");
+    NO_SUCH_ELEMENT("[ERROR] 입력값이 없습니다."),
+    INVALID_LOTTO_NUMBER("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
 
-    private ExceptionMessage(String message) {
+    ExceptionMessage(String message) {
         this.message = message;
     }
 
