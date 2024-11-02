ec4dd74e5da6f3464a753607af83014a7030caf4
Dohun Kim <dohun8758@naver.com>
2024-11-02 14:40:56 +0900

    refactor: Lotto 클래스에서 검증 로직 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,13 @@
 package lotto.model;
 
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validateLotto(numbers);
         this.numbers = numbers;
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    // TODO: 추가 기능 구현
 }
