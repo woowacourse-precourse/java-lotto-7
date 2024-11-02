d6117b2e7380400de2b7bdc580dd35f2972fc523
ahpicl64 <ahpicl64@gmail.com>
2024-11-03 06:08:08 +0900

    feat(model): 입력된 금액에 맞는 로또를 발행하는 기능 구현
    - drawLottos 메서드에 의해 금액만큼 pickRandomNumbers 메서드가 반복실행
    - pickRandomNumbers 메서드에 의한 1~45 이내의 중복되지 않는 6개 정수를 List로 반환
    - 반환된 List(로또)는 Lotto클래스의 lottos 리스트로 저장

--- src/main/java/lotto/LotteryMachine.java
+++ src/main/java/lotto/LotteryMachine.java
@@ -1,11 +1,39 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LotteryMachine {
 
-    private int draw(int purchaseAmount) {
+    private final List<Lotto> lottos;
+
+    public LotteryMachine() {
+        this.lottos = new ArrayList<>();
+    }
+
+
+    private int drawCount(int purchaseAmount) {
         return purchaseAmount / 1000;
         // 추후 1,000원 단위 조건 체크 필요.
     }
 
+    public void drawLottos(int purchaseAmount) {
+        int count = drawCount(purchaseAmount);
+
+        for (int i = 0; i <count; i++) {
+            Lotto lotto = new Lotto(pickRandomNumbers());
+            lottos.add(lotto);
+        }
+    }
+
+    private List<Integer> pickRandomNumbers() {
+
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 
 }
