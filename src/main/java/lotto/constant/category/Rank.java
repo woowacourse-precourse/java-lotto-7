54146a23273e3a672051866ba90bd276e152f918
soeun2537 <soeun2537@gmail.com>
2024-11-02 16:31:21 +0900

    fix: Rank Enum 로또 순위 기준 수정

--- src/main/java/lotto/constant/category/Rank.java
+++ src/main/java/lotto/constant/category/Rank.java
@@ -1,11 +1,11 @@
 package lotto.constant.category;
 
 public enum Rank {
-    FIRST_PLACE(3, false, 5000),
-    SECOND_PLACE(4, false, 50000),
-    THIRD_PLACE(5, true, 1500000),
-    FOURTH_PLACE(5, false, 30000000),
-    FIFTH_PLACE(6, false, 2000000000),
+    FIRST_PLACE(6, false, 2000000000),
+    SECOND_PLACE(5, true, 30000000),
+    THIRD_PLACE(5, false, 1500000),
+    FOURTH_PLACE(4, false, 50000),
+    FIFTH_PLACE(3, false, 5000),
     LAST_PLACE(0, false, 0);
 
 
