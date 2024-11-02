c0bab800625f9ccddfcf92c87190feb0e26133b7
dudxo <father8475@naver.com>
2024-11-02 20:47:31 +0900

    refactor(View): Input/OutputView 리팩토링
    - Input/OutputView의 Message를 필드 상수가 아닌 Enum을 통해 관리한다.
    - Enum에 재정의된 출력 함수를 통해 Message를 출력한다.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,10 +5,6 @@ import convert.InputConvertor;
 import java.util.List;
 
 public class InputView {
-
-    private static final String INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBERS_MSG = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MSG = "보너스 번호를 입력해 주세요.";
     private final InputConvertor inputConvertor;
 
     public InputView(InputConvertor inputConvertor) {
@@ -16,19 +12,19 @@ public class InputView {
     }
 
     public int inputPurchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT_MSG);
+        ViewMessage.INPUT_PURCHASE_AMOUNT.print();
         String inputPurchaseAmount = Console.readLine();
         return inputConvertor.convertPurchaseAmount(inputPurchaseAmount);
     }
 
     public List<Integer> inputWinningNumbers() {
-        System.out.println(INPUT_WINNING_NUMBERS_MSG);
+        ViewMessage.INPUT_WINNING_NUMBERS.print();
         String inputWinningNumbers = Console.readLine();
         return inputConvertor.convertInputWinningNumbers(inputWinningNumbers);
     }
 
     public int inputBonusNumber(List<Integer> winningNumbers) {
-        System.out.println(INPUT_BONUS_NUMBER_MSG);
+        ViewMessage.INPUT_BONUS_NUMBER.print();
         String inputBonusNumber = Console.readLine();
         return inputConvertor.convertInputBonusNumber(inputBonusNumber, winningNumbers);
     }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,34 +6,21 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-
-    private static final String PURCHASE_LOTTO_AMOUNT_MSG = "개를 구매했습니다.";
-    private static final String WINNING_STATISTIC_MSG = "당첨 통계\n---\n";
-    private static final String FIFTH_STATISTIC_MSG = "3개 일치 (5,000원) - ";
-    private static final String FOURTH_STATISTIC_MSG = "4개 일치 (50,000원) - ";
-    private static final String THIRD_STATISTIC_MSG = "5개 일치 (1,500,000원) - ";
-    private static final String SECOND_STATISTIC_MSG = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private static final String FIRST_STATISTIC_MSG = "6개 일치 (2,000,000,000원) - ";
-
     public void outputPurchaseLottoAmount(List<Lotto> lottos) {
-        System.out.printf("%d" + PURCHASE_LOTTO_AMOUNT_MSG + "\n", lottos.size());
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto.getNumbers());
-        }
-        System.out.println();
+        ViewMessage.OUTPUT_PURCHASE_LOTTO_AMOUNT.print(lottos.size());
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 
     public void outputWinningStatistics(Map<Rank, Long> rankResult) {
-        System.out.printf(WINNING_STATISTIC_MSG);
-        System.out.printf(FIFTH_STATISTIC_MSG + "%d개%n", rankResult.getOrDefault(Rank.FIFTH, 0L));
-        System.out.printf(FOURTH_STATISTIC_MSG + "%d개%n", rankResult.getOrDefault(Rank.FOURTH, 0L));
-        System.out.printf(THIRD_STATISTIC_MSG + "%d개%n", rankResult.getOrDefault(Rank.THIRD, 0L));
-        System.out.printf(SECOND_STATISTIC_MSG + "%d개%n", rankResult.getOrDefault(Rank.SECOND, 0L));
-        System.out.printf(FIRST_STATISTIC_MSG + "%d개%n", rankResult.getOrDefault(Rank.FIRST, 0L));
+        ViewMessage.OUTPUT_WINNING_STATISTIC.print();
+        ViewMessage.OUTPUT_FIFTH_STATISTIC.print(rankResult.getOrDefault(Rank.FIFTH, 0L));
+        ViewMessage.OUTPUT_FOURTH_STATISTIC.print(rankResult.getOrDefault(Rank.FOURTH, 0L));
+        ViewMessage.OUTPUT_THIRD_STATISTIC.print(rankResult.getOrDefault(Rank.THIRD, 0L));
+        ViewMessage.OUTPUT_SECOND_STATISTIC.print(rankResult.getOrDefault(Rank.SECOND, 0L));
+        ViewMessage.OUTPUT_FIRST_STATISTIC.print(rankResult.getOrDefault(Rank.FIRST, 0L));
     }
 
     public void outputRateOfReturn(double rateOfReturn) {
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", rateOfReturn);
+        ViewMessage.OUTPUT_RATE_OF_RETURN.print(rateOfReturn);
     }
-
 }
