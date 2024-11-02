c809d6da6f891ed2ee7891b6a5283bbf7a4a41d1
chaerish <ccyy8432@naver.com>
2024-11-02 20:28:20 +0900

    feat(LottoRateCalculator): 로또 수익률을 계산하는 로직 구현

---
+++ src/main/java/lotto/service/LottoRateCalculator.java
@@ -0,0 +1,17 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.MatchingCountResult;
+
+public class LottoRateCalculator {
+    public static double calculateReturnOfRate(int purchaseAmount, List<MatchingCountResult> countResults) {
+        double profit = 0;
+        for (MatchingCountResult matchingCountResult : countResults) {
+            int count = matchingCountResult.conditionCount();
+            int amount = matchingCountResult.winningCondition().getPrizeAmount();
+            profit += count * amount;
+        }
+        return (profit / purchaseAmount) * 100;
+    }
+
+}
