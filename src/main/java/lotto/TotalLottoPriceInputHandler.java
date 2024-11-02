915944370df27e793aefaf6bdb2207c61f074e4e
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-02 17:11:58 +0900

    feat(TotalLottoPriceInputHandler): 생성자 생성
    생성자를 생성하고 발생한 오류 수정

--- src/main/java/lotto/TotalLottoPriceInputHandler.java
+++ src/main/java/lotto/TotalLottoPriceInputHandler.java
@@ -1,15 +1,31 @@
 package lotto;
 
 public class TotalLottoPriceInputHandler extends InputHandler{
-    private final String totalLottoPrice;
+    private String totalLottoPrice;
 
-    public TotalLottoPriceInputHandler(String price) {
-        this.totalLottoPrice = price;
+    public TotalLottoPriceInputHandler() {
+        boolean isValid = false;
+
+        while(!isValid){
+            System.out.println("구입금액을 입력해 주세요.");
+            this.totalLottoPrice = camp.nextstep.edu.missionutils.Console.readLine();
+            try{
+                validateInput();
+                isValid = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+    }
+
+    public int getTotalLottoPrice() {
+        return Integer.parseInt(totalLottoPrice);
     }
 
     @Override
     public void validateInput() {
-
+        throwExceptionWhenInputIsInvalid();
+        throwExceptionWhenInputIsInvalidNumber();
     }
 
     public boolean isValidPrice() {
@@ -17,7 +33,7 @@ public class TotalLottoPriceInputHandler extends InputHandler{
     }
 
     private void throwExceptionWhenInputIsInvalid() {
-        if(!Validation.isNumeric(totalLottoPrice) || !Validation.isEmptyInput(totalLottoPrice)) {
+        if(!Validation.isNumeric(totalLottoPrice) || Validation.isEmptyInput(totalLottoPrice)) {
             throwIllegalArgumentException("입력이 유효하지 않습니다.");
         }
     }
@@ -25,6 +41,9 @@ public class TotalLottoPriceInputHandler extends InputHandler{
     private void throwExceptionWhenInputIsInvalidNumber() {
         if(!isValidPrice()) {
             throwIllegalArgumentException("구입금액은 1000원 단위로만 입력 가능합니다.");
+        }
+        if(totalLottoPrice.equals("0")) {
+            throwIllegalArgumentException("구입금액은 1원 이상부터 입력 가능합니다.");
         }
     }
 
