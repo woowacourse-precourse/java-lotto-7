d8e894c361742575368f1a33fe8678652b944496
hyunwon <khw73850@gmail.com>
2024-11-02 12:25:00 +0900

    feat: calculate and print  number of Lottos

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,19 +7,29 @@ import java.util.List;
 
 public class Application {
     private static int amountToPurchase;
-
+    private static int numOfLotto;
+    private static List<Lotto> lottos = new ArrayList<>();
     public static void main(String[] args) {
         readUserInput();
     }
 
     public static void readUserInput(){
         readAmountToPurchase();
+
     }
 
     public static void readAmountToPurchase(){
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         amountToPurchase = (Integer.parseInt(input) / 1000) * 1000;
+    }
+
+    public static void createLottos(){
+        numOfLotto = amountToPurchase / 1000;
+
+        System.out.println(numOfLotto + "개를 구매했습니다.");
+
+
     }
 
 }
