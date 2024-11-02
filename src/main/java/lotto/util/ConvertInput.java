47940e65c3fdf07031b5ca4270fe4a78459e3c19
JoonKyo <joonkyolee@gmail.com>
2024-11-03 00:04:49 +0900

    feat(ConvertInput): 입력한 로또 당첨 번호를 숫자로 변환시키고 문자가 포함되어 있는지 확인하는 메서드 구현

--- src/main/java/lotto/util/ConvertInput.java
+++ src/main/java/lotto/util/ConvertInput.java
@@ -1,14 +1,28 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.exception.ErrorMessage;
 import lotto.exception.InputException;
 
 public class ConvertInput {
+    private static final String LOTTO_NUMBER_DELIMITER = ",";
+
     public static int makeMoneyToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
             throw InputException.from(ErrorMessage.MONEY_HAS_CHARACTER);
+        }
+    }
+
+    public static List<Integer> makeWinningNumberToList(String input) {
+        try {
+            return Arrays.stream(input.split(LOTTO_NUMBER_DELIMITER))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException exception) {
+            throw InputException.from(ErrorMessage.LOTTO_NUMBER_HAS_CHARACTER);
         }
     }
 }
