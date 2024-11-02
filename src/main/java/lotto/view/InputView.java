db1dc2893dbdbcbe842027d66c6819a2b66423a6
SeongbinPark <hyucse22@gmail.com>
2024-11-02 21:41:35 +0900

    refactor(InputView): 예외 처리 로직 다시 추가
    입력과 동시에 예외 처리를 위함. 별로 도메인을 만들기엔 기준이 모호하다고 생각

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,29 +1,62 @@
 package lotto.view;
 
+import static lotto.constants.LottoMachineConstants.LOTTO_PRICE;
+
 import camp.nextstep.edu.missionutils.Console;
+import lotto.constants.ErrorMessages;
+import lotto.constants.InputMessages;
 import lotto.domain.BonusNumber;
 import lotto.domain.WinningNumbers;
 
 public class InputView {
-    private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
-
     public int inputPurchaseAmount() {
-        System.out.println(PURCHASE_AMOUNT_PROMPT);
-        String inputAmount = Console.readLine();
-        return Integer.parseInt(inputAmount);
+        while (true) {
+            try {
+                System.out.println(InputMessages.PURCHASE_AMOUNT_PROMPT);
+                String inputAmount = Console.readLine();
+                int purchaseAmount = Integer.parseInt(inputAmount);
+                validatePurchaseAmount(purchaseAmount);
+                return purchaseAmount;
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMessages.PURCHASE_AMOUNT_NOT_NUMBER);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validatePurchaseAmount(int amount) {
+        if (amount < LOTTO_PRICE || amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_PURCHASE_AMOUNT);
+        }
     }
 
     public WinningNumbers inputWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_PROMPT);
-        String input = Console.readLine();
-        return new WinningNumbers(input);
+        while (true) {
+            try {
+                System.out.println(InputMessages.WINNING_NUMBERS_PROMPT);
+                String input = Console.readLine();
+                return new WinningNumbers(input);
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMessages.INVALID_WINNING_NUMBER_COUNT);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public BonusNumber inputBonusNumber(WinningNumbers winningNumbers) {
-        System.out.println(BONUS_NUMBER_PROMPT);
-        String input = Console.readLine();
-        return new BonusNumber(Integer.parseInt(input.trim()), winningNumbers);
+        while (true) {
+            try {
+                System.out.println(InputMessages.BONUS_NUMBER_PROMPT);
+                String input = Console.readLine();
+                int bonusNumber = Integer.parseInt(input.trim());
+                return new BonusNumber(bonusNumber, winningNumbers);
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorMessages.BONUS_NUMBER_NOT_NUMBER);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
