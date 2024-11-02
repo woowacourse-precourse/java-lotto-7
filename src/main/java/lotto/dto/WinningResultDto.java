14e73bf6d978b844cabc756d2f56559e9c9be8dc
howareyoo <newworld121@naver.com>
2024-11-03 05:34:00 +0900

    feat: add method to compare user number and winning numbers
    각 결과 dto 생성 후, 결과 리스트 dto에 add 후 사용

---
+++ src/main/java/lotto/dto/WinningResultDto.java
@@ -0,0 +1,22 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class WinningResultDto {
+
+    private final Integer winningScore;
+    private final boolean isBonusMatched;
+
+    public WinningResultDto(Integer winningScore, boolean isBonusMatched) {
+        this.winningScore = winningScore;
+        this.isBonusMatched = isBonusMatched;
+    }
+
+    public Integer getWinningScore() {
+        return winningScore;
+    }
+
+    public boolean isBonusMatched() {
+        return isBonusMatched;
+    }
+}
--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lotto.dto.WinningNumbersDto;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.dto.WinningResultDto;
+import lotto.dto.WinningResultsDto;
 
 public class Winning {
     private final Map<Integer, WinningNumbersDto> winningTickets = new HashMap<>();
@@ -33,11 +36,42 @@ public class Winning {
     }
 
     private Integer getWinningBonusNumber(List<Integer> winningNumbers){
-        int winningBonusNumber = 0;
+        int winningBonusNumber;
         do {
             winningBonusNumber = Randoms.pickNumberInRange(1, 45);
         } while (winningNumbers.contains(winningBonusNumber));
 
         return winningBonusNumber;
+    }
+
+    private WinningResultsDto compareNumbers(List<Integer> userNumbers, Integer userBonusNumber){
+        List<WinningResultDto> winningResultDtos = new ArrayList<>();
+
+        for(WinningNumbersDto winningNumbers : winningTickets.values()){
+            List<Integer> winningNumber = winningNumbers.getWinningNumber();
+
+            Integer correctCount = compareEachNumbers(winningNumber, userNumbers);
+            boolean isCorrectBonus = compareBonusNumber(winningNumbers.getWinningBonusNumber(), userBonusNumber);
+
+            WinningResultDto winningResult = new WinningResultDto(correctCount, isCorrectBonus);
+            winningResultDtos.add(winningResult);
+        }
+
+        return new WinningResultsDto(winningResultDtos);
+    }
+
+    private Integer compareEachNumbers(List<Integer> winningTicketNumbers, List<Integer> userNumbers){
+        Integer correctValue = 0;
+        for(Integer userNumber : userNumbers){
+            boolean contains = winningTicketNumbers.contains(userNumber);
+            if(contains){
+                ++correctValue;
+            }
+        }
+        return correctValue;
+    }
+
+    private boolean compareBonusNumber(Integer winningBonusNumber, Integer userBonusNumber){
+        return winningBonusNumber.equals(userBonusNumber);
     }
 }
