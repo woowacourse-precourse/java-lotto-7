3fe66a9913909c72fc997d8eba16f6dd3df3387a
yuntasha <jaljayo85@gmail.com>
2024-11-02 22:30:51 +0900

    feat(ErrorMessage): add ErrorType and refactor
    Add NOT_NATURAL_NUM
    
    Before:
    NOT_NUM("숫자가 아닙니다."),
    NOT_IN_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
    ILLEGAL_GAMBLING("복권 100장 초과 구매는 불법입니다."),
    UNDIVIDED_THOUSAND("복권은 한장당 1000원입니다."),
    NOT_INPUT_FORMAT("형식에 맞지 않습니다."),
    EXIST_NUM("중복된 숫자가 있습니다."),
    NOT_SIX_NUM("복권은 6개의 숫자로 이루어져있습니다."),
    
    After:
    // InputHelper Error
    NOT_NUM("숫자가 아닙니다."),
    NOT_INPUT_FORMAT("형식에 맞지 않습니다."),
    
    // LottoMachine Error
    ILLEGAL_GAMBLING("복권 100장 초과 구매는 불법입니다."),
    UNDIVIDED_THOUSAND("복권은 한장당 1000원입니다."),
    NOT_NATURAL_NUM("자연수로 입력해주세요."),
    
    // Lotto Error
    NOT_SIX_NUM("복권은 6개의 숫자로 이루어져있습니다."),
    NOT_IN_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
    EXIST_NUM("중복된 숫자가 있습니다."),

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,13 +1,19 @@
 package lotto.exception;
 
 public enum ErrorMessage {
+    // InputHelper Error
     NOT_NUM("숫자가 아닙니다."),
-    NOT_IN_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NOT_INPUT_FORMAT("형식에 맞지 않습니다."),
+
+    // LottoMachine Error
     ILLEGAL_GAMBLING("복권 100장 초과 구매는 불법입니다."),
     UNDIVIDED_THOUSAND("복권은 한장당 1000원입니다."),
-    NOT_INPUT_FORMAT("형식에 맞지 않습니다."),
-    EXIST_NUM("중복된 숫자가 있습니다."),
+    NOT_NATURAL_NUM("자연수로 입력해주세요."),
+
+    // Lotto Error
     NOT_SIX_NUM("복권은 6개의 숫자로 이루어져있습니다."),
+    NOT_IN_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    EXIST_NUM("중복된 숫자가 있습니다."),
     ;
 
     private String message;
