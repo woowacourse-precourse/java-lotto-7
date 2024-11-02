aca4c84020b6fd4500d2ba7e90aa126eef50672a
Dohun Kim <dohun8758@naver.com>
2024-11-02 15:45:50 +0900

    feat: 보너스 번호에 대한 검증 로직 추가

--- src/main/java/lotto/model/WinningLottoNumbers.java
+++ src/main/java/lotto/model/WinningLottoNumbers.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import lotto.validator.BonusNumberValidator;
+
 public class WinningLottoNumbers {
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLottoNumbers(Lotto winningLotto, int bonusNumber) {
         this.lotto = winningLotto;
+        BonusNumberValidator.validateBonusNumber(lotto, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 }
