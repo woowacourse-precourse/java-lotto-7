369da75201de2a571a9d685274a0d4409adcb531
Youngchan <wdc4512@naver.com>
2024-11-02 18:28:11 +0900

    feat: 당첨번호 입력 및 유효성 검증 기능 추가
    - Application 클래스에서 당첨 번호를 입력받고 리스트로 변환하는 메서드 추가
    - Lotto 클래스에서 입력된 번호의 개수, 중복 여부, 범위 검증 기능 추가
    - 구입 금액 입력 및 예외 처리 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,18 +2,45 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("구입금액을 입력해주세요.");
         String inputPurchase = Console.readLine();
 
-        // 구입금액 입력에 대한 예외 처리 및 구매 갯수 출력
+        // 구입금액 입력에 대한 예외 처리, 구매 갯수 출력
         try {
             PurchaseCalculator pCalc = new PurchaseCalculator(inputPurchase);
             int lottoCount = pCalc.calculateLottoCount();
             System.out.println(lottoCount + "개를 구매했습니다.");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            return;
+        }
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String inputNumber = Console.readLine();
+
+        // 당첨번호 입력에 대한 예외 처리 후 List에 저장
+        try {
+            List<Integer> numbers = parseInputToList(inputNumber);
+            Lotto lotto = new Lotto(numbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static List<Integer> parseInputToList(String input) {
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(String::trim) // 공백 제거
+                    .map(Integer::parseInt) // 문자열->Integer 변환
+                    .collect(Collectors.toList()); // 리스트로 변환
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요.");
         }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,9 +12,19 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    // 입력 검증
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 번호를 입력하세요.");
+        }
+
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력하세요.");
         }
     }
 
