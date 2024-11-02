d081ef24deb3ca49a6ae07953112f86a5a4b539b
hanwool <hanwe92@gmail.com>
2024-11-02 23:33:27 +0900

    fix(InputView): Change logic not using recursive function

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,23 +9,30 @@ public class InputView {
     private static final String BONUS_NUMBER_INPUT_QUESTION = "보너스 번호를 입력해 주세요.";
 
     public static List<Integer> getLottoNumber() {
-        String input = askInput(LOTTO_NUMBER_INPUT_QUESTION);
-        try {
-            return InputParser.parseLottoNumbers(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getLottoNumber();
+        List<Integer> lottoNumber = null;
+        while (lottoNumber == null) {
+            try {
+                String input = askInput(LOTTO_NUMBER_INPUT_QUESTION);
+                lottoNumber = InputParser.parseLottoNumbers(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        return lottoNumber;
     }
 
     public static int getBonusNumber() {
-        String input = askInput(BONUS_NUMBER_INPUT_QUESTION);
-        try {
-            return InputParser.parseBonusNumber(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getBonusNumber();
+        int bonusNumber = 0;
+        while (bonusNumber == 0) {
+            try {
+                String input = askInput(BONUS_NUMBER_INPUT_QUESTION);
+                bonusNumber = InputParser.parseBonusNumber(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return getBonusNumber();
+            }
         }
+        return bonusNumber;
     }
 
     private static String askInput(String question) {
