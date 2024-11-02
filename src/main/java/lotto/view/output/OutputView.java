a7b86767093d6812db3f90383c38eaf512331411
mingking2 <mingee641@gmail.com>
2024-11-02 20:24:58 +0900

    feat: 출력화면 추가

---
+++ src/main/java/lotto/view/output/OutputView.java
@@ -0,0 +1,37 @@
+package lotto.view.output;
+
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.model.LottoTicket;
+
+public class OutputView {
+
+    public static void outputLottoNumbers(LottoTicket lottoTicket) {
+        System.out.println(lottoTicket.getLottos().size() + OutputViewMessage.PURCHASE_COUNT_VIEW);
+        for (Lotto lotto : lottoTicket.getLottos()) {
+            String numbers = lotto.getNumbers().stream()
+                    .sorted()
+                    .map(String::valueOf)
+                    .reduce((num1, num2) -> num1 + ", " + num2)
+                    .orElse("");
+
+            System.out.println("[" + numbers + "]");
+        }
+    }
+
+    public static void outputRankSummary(Map<LottoRank, Integer> resultRank, double profitRate) {
+        System.out.println(OutputViewMessage.SUMMARY_VIEW);
+
+        for (LottoRank rank : LottoRank.values()) {
+            int count = resultRank.getOrDefault(rank, 0);
+            String bonusText = rank.getRequireBonus() ? OutputViewMessage.BONUS_MATCH : "";
+            System.out.printf(OutputViewMessage.MATCH_COUNT + bonusText + OutputViewMessage.PRIZE_AMOUNT + "\n",
+                    rank.getMatchingNumberCount(),
+                    String.format("%,d", rank.getMoney()),
+                    count);
+        }
+
+        System.out.printf(OutputViewMessage.PROFIT_RATE + "\n", profitRate);
+    }
+}
