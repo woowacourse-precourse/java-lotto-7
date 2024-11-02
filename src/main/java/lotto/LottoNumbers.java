36097a6c5fad937b03bd1fbd426a8c23ef321ff8
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-03 02:11:18 +0900

    feat(LottoNumbers): 발급받은 로또 번호들을 담을 수 있는 클래스 생성 및 로또번호를 생성하는 메소드 추가

---
+++ src/main/java/lotto/LottoNumbers.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoNumbers {
+    private List<Lotto> lottos;
+
+    public LottoNumbers() {
+
+    }
+
+    private List<Integer> createLotto() {
+        return camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1,45,6);
+    }
+}
