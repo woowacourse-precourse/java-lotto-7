8f284f853241ea21e344259127ff819cc78ac430
soobin <a01056239778@gmail.com>
2024-11-02 21:59:59 +0900

    fix: 로또 생성기 구조 수정
    - 로또 생성기가 Lotto를 가질 필요가 없으므로 관련 로직 삭제
    - 로또 번호 생성 메소드 public으로 접근제어자 변경

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -6,19 +6,10 @@ import java.util.List;
 
 public class LottoGenerator {
 
-    public final Lotto lotto;
-
-    private LottoGenerator() {
-        this.lotto = generate();
-    }
-
     //로또 번호 생성
-    private Lotto generate() {
+    public Lotto generate() {
         List<Integer> generatedNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(generatedNumbers);
     }
 
-    public Lotto getLotto() {
-        return lotto;
-    }
 }
