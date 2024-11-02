bcff8cac266d3d6e4f15e1de0fe1185808c9ca93
hyosang <billy04100410@gmail.com>
2024-11-02 13:45:25 +0900

    feat(PublishLotto): PublishLotto 오름차순 정렬 기능 추가

--- src/main/java/lotto/domain/PublishLotto.java
+++ src/main/java/lotto/domain/PublishLotto.java
@@ -5,6 +5,7 @@ import static lotto.constant.LottoConstant.MIN_LOTTO_NUMBER;
 import static lotto.constant.LottoConstant.VALID_LOTTO_NUMBER_COUNT;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 import lotto.validator.LottoValidator;
 
@@ -14,6 +15,7 @@ public class PublishLotto {
 
     private PublishLotto(LottoValidator validator) {
         numbers = getRandomNumbers();
+        sortNumbers();
         validate(numbers, validator);
     }
 
@@ -27,6 +29,10 @@ public class PublishLotto {
 
     private List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, VALID_LOTTO_NUMBER_COUNT);
+    }
+
+    private void sortNumbers() {
+        Collections.sort(numbers);
     }
 
     public List<Integer> getPublishLottoNumbers() {
