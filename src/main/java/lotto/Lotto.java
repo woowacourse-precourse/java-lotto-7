67c9ca222b35dbc03f874f9f391befc40d6505de
Leegeonwoo7 <workgeon07@naver.com>
2024-11-03 06:54:45 +0900

    feat(Lotto): 로또 생성시 중복된 번호 입력 예외처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.constant.NumberType;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +13,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        boolean isDuplicate = numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+
+        if (isDuplicate) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 당첨번호를 입력하셨습니다");
+        }
+    }
 }
