4ac779b624d3f34f2bd950e565edfbb7504a6f33
soobin <a01056239778@gmail.com>
2024-11-02 22:04:38 +0900

    feat: 고객이 로또가게에서 로또 구매 로직 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,11 @@ public class Application {
     public static void main(String[] args) {
         ConsoleHandler consoleHandler = new ConsoleHandler();
         int money = consoleHandler.inputMoney();
+
+        Customer customer = new Customer(money);
+
+        LottoStore lottoStore = new LottoStore();
+        customer.buyLottoFrom(lottoStore);
     }
 
 }
