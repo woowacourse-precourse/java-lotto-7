3b5682758144ee743fdd1d4fe06e7ba8828ec9d7
whdmscks11 <whdmscks11@gmail.com>
2024-11-02 18:38:22 +0900

    feat(Application): ApplicationLauncher의 run 메서드 호출로 프로그램 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.service.ApplicationLauncher;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ApplicationLauncher.run();
     }
 }
