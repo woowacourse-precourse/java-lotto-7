8c7db993a8330cfc8f274c87193af63093ec16a4
JoonKyo <joonkyolee@gmail.com>
2024-11-02 23:35:58 +0900

    feat(Money): 금액이 기준 금액으로 나눠지지 않는 경우를 판단하는 메서드 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -13,6 +13,7 @@ public class Money {
 
     public static Money from(String input) {
         int money = stringToInt(input);
+        validate(money);
         return new Money(money);
     }
 
@@ -24,6 +25,12 @@ public class Money {
         }
     }
 
+    private static void validate(int money) {
+        validateNegativeNumber(money);
+        validateLessThanUnitPrice(money);
+        validateNotDividedUnitPrice(money);
+    }
+
     private static void validateNegativeNumber(int money) {
         if (money < 0) {
             throw InputException.from(ErrorMessage.MONEY_IS_NEGATIVE_NUMBER);
@@ -33,6 +40,12 @@ public class Money {
     private static void validateLessThanUnitPrice(int money) {
         if (money < UNIT_PRICE) {
             throw InputException.from(ErrorMessage.MONEY_IS_LESS_THAN_UNIT_PRICE);
+        }
+    }
+
+    private static void validateNotDividedUnitPrice(int money) {
+        if (money % UNIT_PRICE != 0) {
+            throw InputException.from(ErrorMessage.MONEY_IS_NOT_DIVIDED_BY_UNIT_PRICE);
         }
     }
 }
