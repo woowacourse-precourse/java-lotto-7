0b9311dd6f0c12d947ba0820229d645a14cb8fa1
fbehddn <tom0611@naver.com>
2024-11-02 17:49:23 +0900

    feat: 로또 총금액 입력받기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,26 @@
 package lotto.controller;
 
+import lotto.domain.Money;
+
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+
+    public void start() {
+        Money money = getMoney();
+    }
+
+    private Money getMoney() {
+        while (true) {
+            try {
+                int moneyInput = inputView.inputMoney();
+                return new Money(moneyInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
