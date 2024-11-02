a64f2b16bbd8dea5d9ccb78c5f5b2d79a3ba8b90
alpin87 <qortmdals94@naver.com>
2024-11-02 20:02:26 +0900

    test: ErrorCode 메시지 일치 여부 검증 로직 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,10 @@
 package lotto;
 
 import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
+import lotto.exception.ErrorCode;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -13,48 +13,47 @@ class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_SIZE.getMessage());
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.DUPLICATE_LOTTO_NUMBER.getMessage());
     }
 
-    @DisplayName("로또 번호가 1보다 작으면 예외가 발생한다.")
     @Test
     void 로또_번호가_1보다_작으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_RANGE.getMessage());
     }
 
     @Test
     void 로또_번호_범위_테스트() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_RANGE.getMessage());
     }
 
     @Test
     void 로또_번호_개수_테스트() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_SIZE.getMessage());
     }
 
     @Test
     void 로또_번호_생성_테스트() {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-        Lotto lotto = new Lotto(numbers);
-
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
     @Test
     void 로또_번호_오름차순_테스트() {
-        List<Integer> numbers = List.of(6, 5, 4, 3, 2, 1);
-        Lotto lotto = new Lotto(numbers);
-
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
         assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 }
