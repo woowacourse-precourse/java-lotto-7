a034b3efae4d06a8cf9bf1eecb2c636035ac4114
je488 <pje488@naver.com>
2024-11-03 01:42:37 +0900

    feat: 보너스 번호 입력 시 예외 처리에 대한 테스트 코드 작성

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,7 @@ public class InputViewTest {
     @DisplayName("로또 구입 금액이 숫자가 아닌 경우 예외 발생")
     @Test
     void isPurchasePriceNumber() {
-        assertThatThrownBy(() -> inputView.validatePurchasePriceIsNumber("5000j"))
+        assertThatThrownBy(() -> inputView.validateIsNumeric("5000j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -39,6 +40,20 @@ public class InputViewTest {
     @Test
     void isNonCommaDelimiter() {
         assertThatThrownBy(() -> inputView.validateNonCommaDelimiter("4-15-30-35-41-43"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1부터 45 사이의 숫자를 입력하지 않은 경우 예외 발생")
+    @Test
+    void isBonusNumberOutOfRange() {
+        assertThatThrownBy(() -> inputView.validateLottoRange(47))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호를 당첨 번호와 중복해서 입력한 경우 예외 발생")
+    @Test
+    void isBonusNumberDuplicate() {
+        assertThatThrownBy(() -> inputView.validateBonusNumberDuplicate(Arrays.asList(1,2,3,4,5,6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
