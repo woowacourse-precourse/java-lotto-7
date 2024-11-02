7ae0769944aa0e6616ec6e5916b031462e22854b
Youngchan <wdc4512@naver.com>
2024-11-02 17:38:36 +0900

    feat: 구입금액 입력 검증, 로또 개수 계산 기능
    - Console.readLine()을 통해 구입 금액을 입력받는 기능 추가
    - 입력값을 정수로 변환하는 메서드 구현 및 유효성 검증 추가
    - 입력 금액이 1,000원 단위인지 확인하는 검증 메서드 추가
    - 입력된 금액에 따라 구매 가능한 로또 개수 계산 기능 구현
    - 잘못된 입력에 대한 예외 처리 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해주세요.");
+        String inputPurchase = Console.readLine();
+
+        // 구입금액 입력에 대한 예외 처리 및 구매 갯수 출력
+        try {
+            PurchaseCalculator pCalc = new PurchaseCalculator(inputPurchase);
+            int lottoCount = pCalc.calculateLottoCount();
+            System.out.println(lottoCount + "개를 구매했습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/lotto/PurchaseCalculator.java
@@ -0,0 +1,31 @@
+package lotto;
+
+public class PurchaseCalculator {
+    private final int purchaseAmount;
+
+    public PurchaseCalculator(String input) {
+        this.purchaseAmount = convertInputToAmount(input);
+        validateAmount(purchaseAmount);
+    }
+
+    // 문자열->정수 변환
+    private int convertInputToAmount(String input) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요.");
+        }
+    }
+
+    // 변환 금액이 1,000원 단위인지 확인
+    private void validateAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력하세요.");
+        }
+    }
+
+    // 로또의 개수 반환
+    public int calculateLottoCount() {
+        return purchaseAmount / 1000;
+    }
+}
