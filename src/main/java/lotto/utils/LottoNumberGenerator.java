bdc6db618c8c6db2e4fd2a419cd43ece8f05f4d7
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-02 18:44:38 +0900

    feat: 1~45 사이 6개의 중복되지 않는 랜덤 숫자를 반환하는 기능 구현

---
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -0,0 +1,15 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, LOTTO_NUMBER_SIZE);
+    }
+}
