c43782030541de844344174ecf4c7b82fffe3e0e
likerhythm <wjdalsdl0412@gmail.com>
2024-11-02 16:34:12 +0900

    fix(lotto generator): 역할 간략화 및 number generator 주입 받도록 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,26 +1,20 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
 
-    private static final int NUMBERS_PER_LOTTO = 6;
-    private static final int START_NUMBER = 1;
-    private static final int END_NUMBER = 45;
-    private static final int PRICE_OF_LOTTO = 1000;
+    private final NumberGenerator numberGenerator;
 
-    public List<Lotto> generate(long payment) {
-        long lottoCount = payment / PRICE_OF_LOTTO;
-        List<Lotto> lottos = new ArrayList<>();
+    public LottoGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
 
-        for (int i = 0; i < lottoCount; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBERS_PER_LOTTO);
-            lottos.add(new Lotto(numbers));
-        }
+    public List<Integer> createNumbers() {
+        return numberGenerator.generate();
+    }
 
-        return lottos;
+    public Lotto generate(List<Integer> numbers) {
+        return new Lotto(numbers);
     }
 }
