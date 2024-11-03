be95543a9d46bda53e4f9e83652d0fd1d23f87a6
wooqqq <sge0225@naver.com>
2024-11-03 23:35:41 +0900

    refactor: 로또 번호 관련 상수 Constants 클래스로 분리

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,18 +1,15 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.util.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
 
-    private static int MIN_NUMBER = 1;
-    private static int MAX_NUMBER = 45;
-    private static int LOTTO_NUMBER_SIZE = 6;
-
     public static Lotto createLotto() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_SIZE);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(Constants.MIN_NUMBER, Constants.MAX_NUMBER, Constants.LOTTO_NUMBER_SIZE);
         return createLotto(lottoNumbers);
     }
 
---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,8 @@
+package lotto.util;
+
+public class Constants {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_NUMBER_SIZE = 6;
+}
