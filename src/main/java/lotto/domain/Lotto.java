b3fba8a217cb39a53249fd4d9fb42e501cc44f22
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 05:08:21 +0900

    refactor(domain): Lotto 클래스 수정 -validator함수는 validator 패키지의 클래스에서 담당하므로 메소드를 삭제했습니다. -countMatchingNumber 메소드는 두 로또 객체간에 일치하는 번호 개수를 계산합니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,23 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    // TODO: 추가 기능 구현
+    public int countMatchingNumbers(Lotto otherLotto) {
+        return (int) numbers.stream()
+            .filter(otherLotto.getNumbers()::contains)
+            .count();
+    }
 }
