700c92af3c13469e8460020e5f846bde436dcb78
wgrgwg <kyoeng14697@naver.com>
2024-11-04 02:02:03 +0900

    feat: Implement method to return lotto game result

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import java.util.Map;
+import lotto.constants.PrizeRank;
+
 public class LottoGame {
 
     private final LottoTickets lottoTickets;
@@ -10,5 +13,16 @@ public class LottoGame {
         this.lottoTickets = lottoTickets;
         this.winningLotto = winningLotto;
         this.money = money;
+    }
+
+    public LottoGameResult calculateLottoResults() {
+        Map<PrizeRank, Integer> prizeResultMap = lottoTickets.getPrizeRankCountMap(winningLotto);
+
+        long prizeMoney = prizeResultMap.entrySet().stream()
+                .mapToLong(entry -> (long) entry.getKey().getPrizeMoney() * entry.getValue())
+                .sum();
+        double profitRate = Math.round((double) prizeMoney / money);
+
+        return new LottoGameResult(prizeResultMap, profitRate);
     }
 }
