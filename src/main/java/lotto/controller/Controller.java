228b7523da07a7d8e2bd744efe3ebe58da9e6278
hyebin <shin378378@naver.com>
2024-11-04 00:19:35 +0900

    feat : 보너스볼 등록 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -2,11 +2,15 @@ package lotto.controller;
 
 import lotto.model.LottoCreator;
 import lotto.model.Lottos;
+import lotto.model.WinningLotto;
 import lotto.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
     InputView inputView = new InputView();
-
 
     public void purchaseLottos() {
         LottoCreator lottoCreator = new LottoCreator();
@@ -17,5 +21,20 @@ public class Controller {
             if (numberOfLotto != LottoCreator.INITIAL_NUMBER_OF_LOTTO) break;
         }
         Lottos lottos = new Lottos(numberOfLotto);
+    }
+
+    private static List<Integer> convertToIntegerList(String winningNumber) {
+        return Arrays.stream(winningNumber.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public void createWinningLotto(){
+        String winningNumbersBeforeConvert = inputView.inputWinningNumbers();
+        List<Integer> winningNumbers = convertToIntegerList(winningNumbersBeforeConvert);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers);
+        int bonusNumber = inputView.inputBonusNumber();
+        winningLotto.settingBonusNumber(bonusNumber);
     }
 }
--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public class WinningLotto extends Lotto{
     private final List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public WinningLotto(List<Integer> numbers) {
         super(numbers);
         this.winningNumbers = numbers;
+    }
+
+    public void settingBonusNumber(int bonusNumber){
+        this.bonusNumber = bonusNumber;
     }
 
     public List<Integer> getWinningNumbers() {
