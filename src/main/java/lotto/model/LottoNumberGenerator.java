aefd8dae45f0d82c5eb4fdfde4ee2f804d27145e
wowseok <wowseok2774@gmail.com>
2024-11-03 23:44:16 +0900

    feat: 로또번호를 오름차순으로 정렬한다.✔️

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,13 +1,15 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
     private List<Integer> lottoNumbers;
-    
+
     public List<Integer> generateNumbers() {
         this.lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
         return List.copyOf(lottoNumbers);
     }
 }
