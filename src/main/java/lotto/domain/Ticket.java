b3ecd43384180c5c3e252c233646bb75e6da47df
chanwon <clw8679@naver.com>
2024-11-03 21:08:06 +0900

    feat: 총 수익률 계산 기능 구현

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class Ticket {
     private final int price;
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
+    private final EnumMap<Prize, Integer> result;
 
     public Ticket(List<Lotto> lottos, int price, List<Integer> winningNumbers, int bonusNumber) {
         this.lottos = lottos;
@@ -17,20 +19,17 @@ public class Ticket {
         validatePrice(price);
         validateDuplication(winningNumbers);
         validateBonusNumber(bonusNumber);
+
+        this.result = getResult();
     }
 
-    public EnumMap<Prize, Integer> getLottoResults() {
-        EnumMap<Prize, Integer> lottoResults = new EnumMap<>(Prize.class);
-        for (Prize prize : Prize.values()) {
-            lottoResults.put(prize, 0);
-        }
+    public double getEarningRate() {
+        int totalPrize = result.entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
+                .sum();
 
-        lottos.forEach(lotto -> {
-            Prize prize = Prize.getPrize(lotto, winningNumbers, bonusNumber);
-            lottoResults.put(prize, lottoResults.get(prize) + 1);
-        });
-
-        return lottoResults;
+        double earningRate = (double) totalPrize / price;
+        return Math.round(earningRate * 100) / 100.0;
     }
 
     private void validatePrice(int price) {
@@ -49,5 +48,17 @@ public class Ticket {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
+    }
+
+    private EnumMap<Prize, Integer> getResult() {
+        EnumMap<Prize, Integer> result = new EnumMap<>(Prize.class);
+        Arrays.stream(Prize.values()).forEach(prize -> result.put(prize, 0));
+
+        lottos.forEach(lotto -> {
+            Prize prize = Prize.getPrize(lotto, winningNumbers, bonusNumber);
+            result.put(prize, result.getOrDefault(prize, 0) + 1);
+        });
+
+        return result;
     }
 }
