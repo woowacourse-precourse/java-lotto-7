b3cefdb04c10314d2dac7d506883b450135026b2
leesh7807 <leesh7807@gmail.com>
2024-11-03 18:09:06 +0900

    feat(LottoMessages): add PROFIT_RATE message

--- src/main/java/lotto/LottoMessages.java
+++ src/main/java/lotto/LottoMessages.java
@@ -5,7 +5,8 @@ public enum LottoMessages {
     PURCHASE_COUNT("%d개를 구매했습니다."),
     INPUT_WIN("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    WIN_STATISTICS("당첨 통계\n---");
+    WIN_STATISTICS("당첨 통계\n---"),
+    PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 
