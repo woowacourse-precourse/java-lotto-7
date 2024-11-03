5471e9a35ff60338783f9f75bef7c59c4aa6a612
mjk <mjk3220@ajou.ac.kr>
2024-11-03 21:32:47 +0900

    feat: make inputAmount function
    구입금액 입력을 위한 함수 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,24 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
+    public static List<Lotto> lottos = new ArrayList<>();
+
+    public static int inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int lotto_cost = Integer.parseInt(readLine());
+        return lotto_cost;
+    }
+
     public static void buyLotto() {
-        int n = inputAmount();
-        makeLotto(n);
+        int lotto_cost = inputAmount();
+        makeLotto(lotto_cost);
         outputLottoNumbers();
     }
 
