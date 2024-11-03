b6e8f06e0f08d00d6aeced4221561c79426ff19d
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 02:54:59 +0900

    refactor: formatStatistics 메서드 책임 분리 적용

--- src/main/java/lotto/system/formater/winning/WinningStatisticsFormatter.java
+++ src/main/java/lotto/system/formater/winning/WinningStatisticsFormatter.java
@@ -11,16 +11,29 @@ public class WinningStatisticsFormatter {
 
     public static String formatStatistics(Map<PrizeType, Integer> statistics, int totalPurchaseAmount) {
         StringBuilder sb = new StringBuilder();
-        sb.append(HEADER.getMessage());
 
+        appendHeader(sb);
+        appendWinningEntries(sb, statistics);
+        appendProfitRate(sb, statistics, totalPurchaseAmount);
+
+        return sb.toString();
+    }
+
+    private static void appendHeader(StringBuilder sb) {
+        sb.append(HEADER.getMessage()).append("\n");
+    }
+
+    private static void appendWinningEntries(StringBuilder sb, Map<PrizeType, Integer> statistics) {
         for (PrizeType prizeType : PrizeType.values()) {
             int count = statistics.getOrDefault(prizeType, 0);
             String formattedWinning = WinningEntryFormatter.formatWinnings(prizeType.getCode(), count);
             sb.append(formattedWinning).append("\n");
         }
+    }
+
+    private static void appendProfitRate(StringBuilder sb, Map<PrizeType, Integer> statistics, int totalPurchaseAmount) {
         ProfitRateCalculator profitRateCalculator = new ProfitRateCalculator(statistics, totalPurchaseAmount);
         double profitRate = profitRateCalculator.calculator();
         sb.append(ProfitRateFormatter.formatAsMessage(profitRate));
-        return sb.toString();
     }
 }
