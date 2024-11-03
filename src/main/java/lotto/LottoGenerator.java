051cc32ff115c66db850c17e287efae303ddcabf
MinseokGo <rhalstjr1999@naver.com>
2024-11-03 17:05:55 +0900

    style: 불필요한 import문 제거

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,9 +1,6 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public final class LottoGenerator {
 
