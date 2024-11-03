643c77762acdeab9a42e750425028db88df548b7
Cheoroo <195822@jnu.ac.kr>
2024-11-03 18:35:58 +0900

    feat : 로또 번호의 범위가 1~45가 아닐경우 예외를 발생시킨다
    - LottoTest 클래스에 로또 번호가 1부터 45 사이의 숫자인지 검증하는 테스트를 추가
    - validate 메서드에 로또 번호의 범위를 검증하는 로직을 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,6 +18,12 @@ public class Lotto {
         if (new HashSet<>(numbers).size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자 6개여야 합니다.");
         }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+
     }
 
     // TODO: 추가 기능 구현
