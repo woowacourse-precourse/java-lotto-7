dec29090daf8bdba2ffae69029c99226fb2cfea6
eunho <eunho1032@gmail.com>
2024-11-03 20:40:21 +0900

    feat: add new methods -generateLottos: need refactor -calculateStatistics: need refactor

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,43 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.Lotto;
+import lotto.model.Statistics;
+import lotto.model.WinningLotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
 
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
+
+    public List<Lotto> generateLottos(int numberOfTickets) {
+        List<Lotto> lottos = new ArrayList<>();
+        List<Integer> numbers = null;
+        Lotto lotto = null;
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            numbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBERS_COUNT);
+            numbers.sort(Integer::compareTo); //오름차순 정렬
+            lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    public Statistics calculateStatistics(List<Lotto> purchasedLottos, WinningLotto winningLotto) {
+        int matchCount;
+        boolean bonusMatch;
+        Statistics statistics = new Statistics();
+        
+        for (Lotto lotto : purchasedLottos) {
+            matchCount = lotto.getMatchCount(winningLotto.getWinningNumbers());
+            bonusMatch = lotto.getNumbers().contains(winningLotto.getBonusNumber());
+            statistics.addResult(matchCount, bonusMatch);
+        }
+        return statistics;
+    }
 }
