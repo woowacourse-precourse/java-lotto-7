218ed88f6cdc2a5fb1b4479663c2c738c884ec7c
장영재 <urinaner@naver.com>
2024-11-03 16:01:49 +0900

    feat(InputView): 구입 금액 입력 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+    private static final String INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String INVALID_NUMBER = "[ERROR] 숫자를 입력해주세요";
+    public int getValue(){
+        System.out.println(INPUT_MESSAGE);
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INVALID_NUMBER);
+        }
+    }
 }
