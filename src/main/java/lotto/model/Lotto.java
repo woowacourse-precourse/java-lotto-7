a0ace705a40a7dac71bf9dac10c3d94df3cd970c
yunhacandy <yunhacandy@gmail.com>
2024-11-03 17:46:45 +0900

    feat (Lotto): 당첨 번호가 범위 안에 없으면 예외 처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import static lotto.constant.Constants.ERROR_PREFIX;
 import static lotto.constant.Constants.LOTTO_NUMBER_LENGTH;
+import static lotto.constant.Constants.LOTTO_NUMBER_RANGE_END;
+import static lotto.constant.Constants.LOTTO_NUMBER_RANGE_START;
 
 import java.util.List;
 
@@ -16,6 +18,12 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_LENGTH) {
             throw new IllegalArgumentException(ERROR_PREFIX + " 로또 번호는 6개여야 합니다.");
+        }
+
+        for (int number : numbers) {
+            if (number < LOTTO_NUMBER_RANGE_START || number > LOTTO_NUMBER_RANGE_END) {
+                throw new IllegalArgumentException(ERROR_PREFIX + " 로또 번호는 1부터 45 사이여야 합니다.");
+            }
         }
     }
 
