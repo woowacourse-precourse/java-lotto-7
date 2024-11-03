6a4366c098c059d775d9e2b76e3769a97c05ee8b
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-04 00:31:09 +0900

    feat: 결과를 저장할 도메인 개발

---
+++ src/main/java/lotto/data/Result.java
@@ -0,0 +1,37 @@
+package lotto.data;
+
+public class Result {
+    private Integer threeNumberMatch;
+    private Integer fourNumberMatch;
+    private Integer fiveNumberMatch;
+    private Integer bonusNumberMatch;
+    private Integer sixNumberMatch;
+
+    public Result() {
+        this.threeNumberMatch = 0;
+        this.fourNumberMatch = 0;
+        this.fiveNumberMatch = 0;
+        this.bonusNumberMatch = 0;
+        this.sixNumberMatch = 0;
+    }
+
+    public void addThreeNumberMatch() {
+        this.threeNumberMatch++;
+    }
+
+    public void addFourNumberMatch() {
+        this.fourNumberMatch++;
+    }
+
+    public void addFiveNumberMatch() {
+        this.fiveNumberMatch++;
+    }
+
+    public void addBonusNumberMatch() {
+        this.bonusNumberMatch++;
+    }
+
+    public void addSixNumberMatch() {
+        this.sixNumberMatch++;
+    }
+}
