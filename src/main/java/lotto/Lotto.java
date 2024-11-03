dd3de89de20123e06a62290d3bc22f8f018f7add
GrO0vy <lmc2819@naver.com>
2024-11-03 19:29:58 +0900

    refactor: 로또 번호 검증 기능 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,37 +1,28 @@
 package lotto;
 
+import lotto.validator.LottoNumberValidator;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final LottoNumberValidator validator;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        this.validator = new LottoNumberValidator(numbers);
         this.numbers = numbers;
         sort();
+        validate();
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        isSixCountOfList(numbers);
-        isDuplicatedNumber(numbers);
-    }
-
-    private void isDuplicatedNumber(List<Integer> numbers){
-        if(numbers.size() > new HashSet<>(numbers).size()){
-            throw new IllegalArgumentException("[ERROR] 로또 번호 6개는 각각 모두 중복 될 수 없습니다.");
-        }
-    }
-
-    private void isSixCountOfList(List<Integer> numbers){
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    private void validate() {
+        validator.validate();
     }
 
     private void sort(){
---
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -0,0 +1,44 @@
+package lotto.validator;
+
+import java.util.HashSet;
+import java.util.List;
+
+// 로또 번호 검증 클래스
+public class LottoNumberValidator implements Validator{
+    private final List<Integer> numbers;
+
+    public LottoNumberValidator(List<Integer> numbers){
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void validate(){
+        isDuplicated();
+        isValidCount();
+        isValidNumbers();
+    }
+
+    private void isDuplicated(){
+        if(numbers.size() > new HashSet<>(numbers).size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호 6개는 중복 될 수 없습니다.");
+        }
+    }
+
+    private void isValidCount(){
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void isValidNumbers(){
+        for(int number: numbers){
+            isInRange(number);
+        }
+    }
+
+    private void isInRange(int number){
+        if(number < 1 || number > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자이어야 합니다.");
+        }
+    }
+}
