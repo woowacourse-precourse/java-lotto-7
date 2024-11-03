a7110fdf9b33d431447789dd4b9cc9ad9e1caf2e
장시영 <sysmile77@gmail.com>
2024-11-04 03:14:02 +0900

    feat(LottoDomain) : DataObject

--- src/main/java/lotto/domain/LottoDomain.java
+++ src/main/java/lotto/domain/LottoDomain.java
@@ -1,2 +1,40 @@
-package lotto.domain;public class LottoDomain {
+package lotto.domain;
+
+import java.util.List;
+
+
+public  class LottoDomain {
+
+    public static int price;
+    public static int playLumber;
+    public static List<Integer> lottoNumbers;
+
+
+    public LottoDomain(int price) {
+        LottoDomain.price = price ;
+
+    }
+    public static int getPrice() {
+        return price;
+    }
+
+    public static void setPrice(int price) {
+        LottoDomain.price = price;
+    }
+
+    public static int getPlayLumber() {
+        return playLumber;
+    }
+
+    public static void setPlayLumber(int playLumber) {
+        LottoDomain.playLumber = playLumber;
+    }
+
+    public static List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public static void setLottoNumbers(List<Integer> lottoNumbers) {
+        LottoDomain.lottoNumbers = lottoNumbers;
+    }
 }
