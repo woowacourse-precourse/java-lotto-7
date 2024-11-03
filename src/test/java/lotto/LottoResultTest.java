cf03551a9d2677d4c0de749de92ac4daaeaa6ffb
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-03 21:42:55 +0900

    feat(LottoResultTest) : 로또 출력 형식과 반환율 확인 테스트 기능 추가

---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class LottoResultTest {
+
+    @Test
+    void 로또_결과_출력_형식과_반환율_확인() {
+        //Given
+        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+        List<Lotto> purchasedLotto = Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), //6개 일치(1등)
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)), //5개 + 보너스 일치(2등)
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8)), //5개 일치(3등)
+                new Lotto(Arrays.asList(1, 2, 3, 4, 10, 11)), //4개 일치(4등)
+                new Lotto(Arrays.asList(1, 2, 3, 9, 10, 11)) //3개 일치(5등)
+        );
+        Calculator calculator = new Calculator(winningNumbers);
+
+        //When
+        LottoResult lottoResult = new LottoResult(purchasedLotto, winningNumbers, calculator);
+        Map<Ranking, Long> results = lottoResult.getResults();
+        double returnRate = lottoResult.getReturnRate();
+
+        //Then
+        assertThat(returnRate).isGreaterThan(0);
+        String expectOutput = "3개 일치 (5,000원) - 1개\n"
+                + "4개 일치 (50,000원) - 1개\n"
+                + "5개 일치 (1,500,000원) - 1개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n"
+                + "6개 일치 (2,000,000,000원) - 1개";
+
+        assertThat(lottoResult.formatResults()).isEqualTo(expectOutput);
+    }
+}
