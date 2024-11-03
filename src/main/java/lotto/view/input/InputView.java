637a0824b34a445461f31e260656cd3032410f16
mingking2 <mingee641@gmail.com>
2024-11-03 14:33:05 +0900

    refactor: 검증 및 파싱 기능 분리

--- src/main/java/lotto/view/input/InputView.java
+++ src/main/java/lotto/view/input/InputView.java
@@ -1,32 +1,20 @@
 package lotto.view.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import lotto.util.InputUtil;
-import lotto.valid.ValidInput;
 
 public class InputView {
-    public static int inputMoney() {
+    public static String inputMoney() {
         System.out.println(InputViewMessage.INPUT_MONEY_VIEW);
-        String inputMoney = Console.readLine();
-
-        ValidInput.checkInputMoney(inputMoney);
-        return Integer.parseInt(inputMoney);
+        return Console.readLine();
     }
 
-    public static List<Integer> inputLottoNumbers() {
+    public static String inputLottoNumbers() {
         System.out.println(InputViewMessage.INPUT_WINNING_NUMBER_VIEW);
-        String inputLottoNumbers = Console.readLine();
-
-        ValidInput.checkInputLottoNumbers(inputLottoNumbers);
-        return InputUtil.splitNumbers(inputLottoNumbers);
+        return Console.readLine();
     }
 
-    public static int inputBonusNumber() {
+    public static String inputBonusNumber() {
         System.out.println(InputViewMessage.INPUT_BONUS_NUMBER_VIEW);
-        String inputBonusNumber = Console.readLine();
-
-        ValidInput.checkInputBonusNumber(inputBonusNumber);
-        return Integer.parseInt(inputBonusNumber);
+        return Console.readLine();
     }
 }
