9a237da3e9b26b4e38c86b73d1c5efe4b2564eb2
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 04:07:11 +0900

    feat(WinningNumberDTO): 당첨 번호를 전달하는 DTO 추가

--- src/main/java/lotto/DTO/WinningNumberDTO.java
+++ src/main/java/lotto/DTO/WinningNumberDTO.java
@@ -1,4 +1,15 @@
 package lotto.DTO;
 
+import java.util.List;
+
 public class WinningNumberDTO {
+    List<Integer> winningNumber;
+
+    public WinningNumberDTO(List<Integer> winningNumber) {
+        this.winningNumber = winningNumber;
+    }
+
+    public List<Integer> getWinningNumber() {
+        return winningNumber;
+    }
 }
