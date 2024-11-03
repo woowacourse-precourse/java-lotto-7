8b5c88bc4d68aa45d8b7c29f3a4dece4607ab87a
plan11plan <tjtfy7028@gmail.com>
2024-11-03 13:48:18 +0900

    feat(PrizeInputView): 콘솔에 당첨 번호 입력 안내 메시지 출력

---
+++ src/main/java/lotto/view/input/PrizeInputView.java
@@ -0,0 +1,13 @@
+package lotto.view.input;
+
+public class PrizeInputView {
+
+    private final String INPUT_PRIZE_NUMBER = "당첨 번호를 입력해 주세요.";
+    private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
+
+    public void initialize() {
+        System.out.println(INPUT_PRIZE_NUMBER);
+
+    }
+
+}
