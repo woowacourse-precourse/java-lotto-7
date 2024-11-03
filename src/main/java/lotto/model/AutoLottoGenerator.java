01ebc71cf3ab16a16569e4d9c013e68962c41e46
Yunsu4 <ysa020428@gmail.com>
2024-11-03 20:35:20 +0900

    refactor(AutoLottoGenerator): 값을 상수로 변환하여 입력하도록 수정

--- src/main/java/lotto/model/AutoLottoGenerator.java
+++ src/main/java/lotto/model/AutoLottoGenerator.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public class AutoLottoGenerator {
 
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int COUNT_OF_NUMBERS_INCLUDED_IN_ONE_LOTTO = 6;
+
+
     public AutoLottoGenerator() {
     }
 
@@ -15,7 +20,8 @@ public class AutoLottoGenerator {
         List<Lotto> lottos = new ArrayList<>();
 
         for(int i = 0; i<lottoCount;i++){
-            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
+                    COUNT_OF_NUMBERS_INCLUDED_IN_ONE_LOTTO);
             Lotto lotto = new Lotto(lottoNumbers);
             lottos.add(lotto);
         }
