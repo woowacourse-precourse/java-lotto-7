227234425c1323bde25619f0c2985edcb1427ded
201912025 <michi2012@naver.com>
2024-11-03 17:15:24 +0900

    feat: 도메인 분리 및 테스트 코드 작성

--- src/main/java/lotto/domain/Lotto/LottoManager.java
+++ src/main/java/lotto/domain/Lotto/LottoManager.java
@@ -1,12 +1,17 @@
 package lotto.domain.Lotto;
 
+import lotto.utils.RandomNumbersSelector;
+import lotto.utils.SortUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoManager {
+    private final LottoNumbersGenerator lottoNumbersGenerator;
     private List<Lotto> lottos;
 
-    public LottoManager() {
+    public LottoManager(LottoNumbersGenerator lottoNumbersGenerator) {
+        this.lottoNumbersGenerator = lottoNumbersGenerator;
         this.lottos = new ArrayList<>();
     }
 
@@ -14,7 +19,16 @@ public class LottoManager {
         return lottos;
     }
 
-    public void createLottosByRandomNumbers(List<Integer> randomNumbers) {
+    public void createLottos(int buyLottoCount) {
+        for (int i = 0; i < buyLottoCount; i++) {
+            List<Integer> randomNumbers = RandomNumbersSelector.selectRandomNumbers();
+            List<Integer> lottoNumbers = lottoNumbersGenerator.generateLottoNumbers(randomNumbers);
+            List<Integer> sortedLottoNumbers = SortUtils.sortNumbers(lottoNumbers);
+            createLottosByRandomNumbers(sortedLottoNumbers);
+        }
+    }
+
+    private void createLottosByRandomNumbers(List<Integer> randomNumbers) {
         Lotto lotto = new Lotto(randomNumbers);
         lottos.add(lotto);
     }
---
+++ src/test/java/lotto/domain/LottoManagerTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import lotto.domain.Lotto.LottoManager;
+import lotto.domain.Lotto.LottoNumbersGenerator;
+import lotto.validator.LottoValidator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LottoManagerTest {
+    private LottoValidator lottoValidator;
+    private LottoNumbersGenerator lottoNumbersGenerator;
+    private LottoManager lottoManager;
+
+    @BeforeEach
+    void setup() {
+        lottoValidator = new LottoValidator();
+        lottoNumbersGenerator = new LottoNumbersGenerator(lottoValidator);
+        lottoManager = new LottoManager(lottoNumbersGenerator);
+    }
+
+    @Test
+    void 로또_구매_개수_테스트() {
+        int buyLottoCount = 8;
+
+        lottoManager.createLottos(buyLottoCount);
+
+        int size = lottoManager.getLottos().size();
+
+        Assertions.assertThat(8).isEqualTo(8);
+    }
+}
