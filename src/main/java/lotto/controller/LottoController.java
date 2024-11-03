b37f1c564e77cdaaf5baf9a017762668ca83f29f
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 23:08:04 +0900

    feat(lottoController): 돈 입력 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 
 import java.util.function.Supplier;
+import lotto.model.Money;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -15,6 +16,11 @@ public class LottoController {
         this.lottoService = new LottoService();
         this.outputView = new OutputView();
         this.inputView = new InputView();
+    }
+
+    private Money setMoney() {
+        outputView.inputMoney();
+        return new Money(inputView.inputNumber());
     }
 
     private <T> T retryIfErrorOccur(Supplier<T> supplier) {
