0fedc3a925ab44000eefc058aaaab693d8cb2fc6
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 02:03:11 +0900

    fix: validate(int) 메서드 이름 다르게 분리

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -8,7 +8,7 @@ public enum ErrorMessage {      // 예외별 메시지 관리
     INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1,000원 단위의 양수 입니다.") {
         // 구입 금액은 양수인 1000원 단위
         @Override
-        public void validate(int amount) {
+        public void validateAmount(int amount) {
             if (amount > 0 || amount % 1000 != 0) {
                 throw new IllegalArgumentException(getMessage());
             }
@@ -74,7 +74,7 @@ public enum ErrorMessage {      // 예외별 메시지 관리
         this.message = message;
     }
 
-    public void validate(int amount) {}
+    public void validateAmount(int amount) {}
     public void validate(List<Integer> numbers) {}
     public void validate(int number) {}
     public void validate(String numbers) {}
