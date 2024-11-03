94afc6a44467796aa3b5f2f861f372c4e5229d42
zzzyoonnn <dwsrh03@gmail.com>
2024-11-03 16:02:28 +0900

    feat: "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -44,4 +44,19 @@ public class StringTest {
 
         assertEquals(substring,"1,2");
     }
+
+    // 요구사항 3
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트")
+    public void testCharAtTest() {
+        String word = "abc";
+
+        char firstChar = word.charAt(0);    // 'a'
+        char secondChar = word.charAt(1);   // 'b'
+        char lastChar = word.charAt(2);     // 'c'
+
+        assertEquals(firstChar,'a');
+        assertEquals(secondChar, 'b');
+        assertEquals(lastChar, 'c');
+    }
 }
