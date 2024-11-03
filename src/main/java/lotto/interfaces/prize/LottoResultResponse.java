daa0233ace9b3be63726d53b9ddb4427684c80de
soeunnPark <carol0724@naver.com>
2024-11-04 02:18:47 +0900

    fix(LottoResultResponse): exclude LottoPrize.NONE in LottoResponse
    로또 결과 응답 객체에서 NONE 은 제외한다.

--- src/main/java/lotto/interfaces/prize/LottoResultResponse.java
+++ src/main/java/lotto/interfaces/prize/LottoResultResponse.java
@@ -1,18 +1,20 @@
-package lotto.interfaces;
+package lotto.interfaces.prize;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.prize.LottoPrize;
 import lotto.domain.prize.LottoResult;
 import lotto.domain.money.Revenue;
 
-public record LottoResponse(
+public record LottoResultResponse(
         List<LottoPrizeResponse> lottoPrizes,
         double returns
 ) {
-    public static LottoResponse of(LottoResult lottoResult, Revenue revenue) {
+    public static LottoResultResponse of(LottoResult lottoResult, Revenue revenue) {
         List<LottoPrizeResponse> prizeResponses = lottoResult.getLottoResult().entrySet().stream()
+                .filter(entry -> entry.getKey() != LottoPrize.NONE)
                 .map(entry -> LottoPrizeResponse.of(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList());
-        return new LottoResponse(prizeResponses, revenue.getReturns());
+        return new LottoResultResponse(prizeResponses, revenue.getReturns());
     }
 }
