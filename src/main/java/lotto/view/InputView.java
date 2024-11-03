8b6329a7fafbe72541dfe7d1383c2da78369aca3
smyeongL <sangmyeonglee123@gmail.com>
2024-11-03 21:16:51 +0900

    feat(view): add input view for user input

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.InputMessage;
+
+
+public class InputView {
+    public static String requestLottoPurchase() {
+        System.out.println(InputMessage.USER_PURCHASE_AMOUNT_REQUEST);
+        return Console.readLine();
+    }
+
+    public static String requestWinningNumbers() {
+        System.out.println(InputMessage.ADMIN_WINNING_NUMBERS_REQUEST);
+        return Console.readLine();
+    }
+
+    public static String requestBonusNumber() {
+        System.out.println(InputMessage.ADMIN_BONUS_NUMBER_REQUEST);
+        return Console.readLine();
+    }
+
+}
