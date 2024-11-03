19a0e98166a2cd91467d572bd3a72164bb56794f
181108-cherry <dysy9799@naver.com>
2024-11-03 17:37:43 +0900

    test: 보너스 번호 일치 여부 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -30,4 +31,13 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1에서 45 사이의 숫자여야 합니다.");
     }
+
+    @DisplayName("보너스 번호가 일치하는지에 대한 여부를 확인한다.")
+    @Test
+    void 보너스_번호가_일치하는지에_대한_여부를_확인한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        boolean containsBonus = lotto.getNumbers().contains(6);
+        assertThat(containsBonus).isTrue();
+    }
+
 }
