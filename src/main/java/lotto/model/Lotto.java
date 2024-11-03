76ee67d5d5460be5a7d5e4eaf8ac89e20cd48bd9
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-03 15:44:23 +0900

    feat(model): 로또 클래스 검증 기능 추가
    - 로또 번호는 6개여야 한다.
    - 로또 번호는 1~45 사이여야 한다.
    - 로또 번호는 중복될 수 없다.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,40 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        lottoCountShouldBeSixValidator(numbers);
+        lottoNumberRangeValidator(numbers);
+        lottoDuplicateValidator(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void lottoCountShouldBeSixValidator(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void lottoNumberRangeValidator(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number<1 || number>45){
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+    private void lottoDuplicateValidator(List<Integer> numbers) {
+        Set<Integer> lottoNumbersSet = Set.copyOf(numbers);
+        if (lottoNumbersSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }
