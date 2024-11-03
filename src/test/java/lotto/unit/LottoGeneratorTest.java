dc1ca8939618c6bf5acee82c157db4e5ce88da6a
SeongbinPark <hyucse22@gmail.com>
2024-11-03 15:09:50 +0900

    test(LottoGeneratorTest): 구입 금액이 1000원 미만인 경우 예외가 발생한다.

---
+++ src/test/java/lotto/unit/LottoGeneratorTest.java
@@ -0,0 +1,31 @@
+package lotto.unit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.constants.ErrorMessages;
+import lotto.domain.LottoGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위가 아닌 경우 예외가 발생한다.")
+    void purchaseAmountNotMultipleOfThousand() {
+        LottoGenerator generator = new LottoGenerator();
+        int purchaseAmount = 8500;
+        assertThatThrownBy(() -> generator.purchaseLottos(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_PURCHASE_AMOUNT);
+    }
+    
+    @Test
+    @DisplayName("구입 금액이 1000원 미만인 경우 예외가 발생한다.")
+    void purchaseAmountLessThanThousand() {
+        LottoGenerator generator = new LottoGenerator();
+        int purchaseAmount = 500;
+        assertThatThrownBy(() -> generator.purchaseLottos(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_PURCHASE_AMOUNT);
+    }
+}
