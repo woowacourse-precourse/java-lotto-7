79e256984da05b05626dfd42891527d691c0e837
Leegeonwoo7 <workgeon07@naver.com>
2024-11-03 21:44:20 +0900

    feat(Lotto): Lotto 번호조회 기능추가
    getNumber() - 테스트에서 사용 중

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateDuplication(numbers);
