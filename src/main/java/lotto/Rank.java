2638bbea0e3ec110d5f63ad4ef775fc5743de64e
jungsehui <jungsehui@naver.com>
2024-11-03 23:30:42 +0900

    feat(Rank.java): 로또 번호 당첨 순위를 위한 통계 랭크 Enum 작성

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,21 @@
+package lotto;
+
+public enum Rank {
+
+    FIRST(6, 2_000_000_000, "6개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    FOURTH(4, 50_000, "4개 일치"),
+    FIFTH(3, 5_000, "3개 일치"),
+    NONE(0, 0, "미당첨");
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final String description;
+
+    Rank(final int matchCount, final int prizeMoney, final String description) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.description = description;
+    }
+}
