808e61590fb0606b9dad9251388a65cf6bb09b38
jinno Yun <jinno321@naver.com>
2024-11-03 16:09:55 +0900

    feat: 내가 산 로또용지를 의미하는 MyWallet 클래스 생성 및 getter 생성

---
+++ src/main/java/lotto/model/MyWallet.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+public class MyWallet {
+
+    private final Lotto lotto;
+
+    public MyWallet(Lotto lotto) {
+        this.lotto = lotto;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+}
