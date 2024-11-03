77dd4f1e53eb9f9848b4057d5a4b22b1c1436e21
문혜교 <3hilney@gmail.com>
2024-11-03 16:13:53 +0900

    feat: 입력값이 비어 있을 때 예외 처리 기능 구현
    예외 처리 메세지 추가

--- src/main/java/lotto/LottoInputValidator.java
+++ src/main/java/lotto/LottoInputValidator.java
@@ -4,6 +4,7 @@ public class LottoInputValidator {
     private final String string;
 
     public LottoInputValidator(String string) {
+        validateLottoNumberExists(string);
         validateLottoNumberNumeric(string);
         this.string = string;
     }
@@ -11,6 +12,12 @@ public class LottoInputValidator {
     private void validateLottoNumberNumeric(String string) {
         if (!string.matches("\\d+")) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT_FORMAT.getErrorMessage());
+        }
+    }
+
+    private void validateLottoNumberExists(String string) {
+        if (string == null || string.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.EMPTY_INPUT.getErrorMessage());
         }
     }
 }
