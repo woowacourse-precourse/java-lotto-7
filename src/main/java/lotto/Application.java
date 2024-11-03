a50fcf0d702687664aedb590eb3f0a17ee4d8a77
mintos00 <kimhm2170@gmail.com>
2024-11-04 03:36:56 +0900

    feat(validateNumber): add a feature to validate the prize numbers and bonus number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,8 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import com.oracle.svm.core.annotate.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class Application {
         Long countLotto = availableLotto(money);
         List<List<Integer>> lottoList = lottoRandom(countLotto);
         haveLotto(lottoList);
+        Lotto lotto = new Lotto(prizeNumbers());
+        int bonusNumber = inputBonus();
+        lotto.addNumber(bonusNumber);
     }
 
     public static Long inputMoney() {
@@ -24,6 +29,64 @@ public class Application {
 
     public static boolean validateMoney(Long money) {
         return money % 1000 == 0;
+    }
+
+    public static List prizeNumbers() {
+        List<Integer> finalList = new ArrayList<>();
+
+        while (true) {
+            List<Integer> numberList = new ArrayList<>();
+            boolean validate = inputNumber(numberList);
+            if (validate) {
+                finalList = numberList;
+                break;
+            }
+        }
+
+        return finalList;
+    }
+
+    public static boolean inputNumber(List<Integer> NumberList) {
+        System.out.println("당첨번호를 입력해주세요.");
+        String winNumbers = Console.readLine();
+
+        for (String num : winNumbers.split(",", -1)) {
+            if (!validateNumber(num)) {
+                return false;
+            }
+            NumberList.add(Integer.parseInt(num));
+        }
+        return true;
+    }
+
+    public static int inputBonus() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        String number = Console.readLine();
+
+        if (!validateNumber(number)) {
+            return inputBonus();
+        }
+        return Integer.parseInt(number);
+    }
+
+    public static boolean validateNumber(String num) {
+        if (num.isBlank()) {
+            return false;
+        }
+
+        if (!Pattern.matches("^[0-9]*$", num)) {
+            return false;
+        }
+
+        if (Integer.parseInt(num) < 1) {
+            return false;
+        }
+
+        if (45 < Integer.parseInt(num)) {
+            return false;
+        }
+
+        return true;
     }
 
     public static Long availableLotto(Long money) {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void addNumber(int number) {
+        this.numbers.add(number);
+    }
 }
