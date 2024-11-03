b60dba078f9c9bcb449a53611a75e72c665660a2
GaBaljaintheroom <junsu1222@naver.com>
2024-11-03 23:41:18 +0900

    feat(LottoStore): 로또 상점 오픈 기능 구현

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class LottoStore {
+
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputView outputView = new OutputView();
+    private final LottoMachine lottoMachine = new LottoMachine();
+
+    public void open() {
+        Cash cash = inputHandler.inputCash();
+        lottoMachine.purchase(cash);
+        outputView.printLottoTickets(lottoMachine);
+
+
+    }
+
+}
