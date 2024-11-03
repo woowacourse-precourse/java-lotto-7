62280ace61ac681076a5b383dc07514fa03b96ee
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 04:15:10 +0900

    refactor(InputViewTest): 검증 로직 병합 및 메서드 분리에 의한 테스트 코드 수정
    - 호출 대상 클래스가 달라졌기 때문에 이를 수정 (기존 inputView -> 현재 inputNumberValidator)

--- src/test/java/lotto/View/InputViewTest.java
+++ src/test/java/lotto/View/InputViewTest.java
@@ -6,17 +6,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.io.ByteArrayInputStream;
 import lotto.DTO.PaymentPriceDTO;
 import lotto.Util.Error.ErrorMessage;
+import lotto.Validation.InputNumberValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
     private final InputView inputView = new InputView();
+    private final InputNumberValidator inputNumberValidator = new InputNumberValidator();
 
     @DisplayName("구입 금액이 문자일 경우에는 NumberFormatException 예외를 발생시킨다.")
     @Test
     void 구입_금액이_문자열일_경우_예외가_발생한다() {
         String invalidInput = "예? 한장에 얼마라구요?";
-        assertThatThrownBy(() -> inputView.validatePaymentPriceType(invalidInput))
+        assertThatThrownBy(() -> inputNumberValidator.validatePaymentPriceType(invalidInput))
                 .isInstanceOf(NumberFormatException.class)
                 .hasMessage(ErrorMessage.INVALID_PURCHASE_UNIT.getMessage());
     }
@@ -25,7 +27,7 @@ class InputViewTest {
     @Test
     void 구입_금액이_Integer_범위를_벗어날_경우_예외가_발생한다() {
         String invalidInput = "9999999999999999";
-        assertThatThrownBy(() -> inputView.validatePaymentPriceValue(invalidInput))
+        assertThatThrownBy(() -> inputNumberValidator.validatePaymentPriceValue(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.PURCHASE_LIMIT_REACHED.getMessage());
     }
@@ -34,7 +36,7 @@ class InputViewTest {
     @Test
     void 구입_금액이_0이거나_음수일_경우_예외가_발생한다() {
         String invalidInput = "-1000";
-        assertThatThrownBy(() -> inputView.validatePaymentPriceValue(invalidInput))
+        assertThatThrownBy(() -> inputNumberValidator.validatePaymentPriceValue(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.MINIMUM_PURCHASE_AMOUNT.getMessage());
     }
@@ -43,7 +45,7 @@ class InputViewTest {
     @Test
     void 구입_금액이_1000원_단위가_아닐_경우_예외가_발생한다() {
         String invalidInput = "1500";
-        assertThatThrownBy(() -> inputView.validatePaymentPriceValue(invalidInput))
+        assertThatThrownBy(() -> inputNumberValidator.validatePaymentPriceValue(invalidInput))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage(ErrorMessage.INVALID_PURCHASE_UNIT.getMessage());
     }
