ea345347d2ef5360c457b330ba084827767bf561
smyeongL <sangmyeonglee123@gmail.com>
2024-11-03 21:17:34 +0900

    feat(validator): add purchase amount input validator

---
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -0,0 +1,32 @@
+package lotto.validator;
+
+import lotto.controller.LottoController;
+import lotto.controller.PurchaseAmountController;
+import lotto.util.ErrorMessage;
+
+public class PurchaseAmountValidator {
+    public static void validateBlank(String purchaseAmount) {
+        try {
+            if (purchaseAmount.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException err) {
+            System.out.println(ErrorMessage.EMPTY_STRING_ERROR);
+            PurchaseAmountController.restart();
+        }
+    }
+    public static void validateDivisibleBy1000(String purchaseAmount) {
+        try {
+            int parsedAmount = Integer.parseInt(purchaseAmount);
+            if (parsedAmount % 1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(ErrorMessage.NOT_INTEGER_STRING_ERROR);
+            PurchaseAmountController.restart();
+        } catch (IllegalArgumentException err) {
+            System.out.println(ErrorMessage.NOT_DIVISIBLE_BY_1000_ERROR);
+            PurchaseAmountController.restart();
+        }
+    }
+}
