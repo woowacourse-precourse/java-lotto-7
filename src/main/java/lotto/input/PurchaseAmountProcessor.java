e8574a3e36571009b16d9fdfbe94e8f7049f0c82
Gahyeon <best11gh@naver.com>
2024-11-03 12:50:37 +0900

    refactor(InputUtil): 양의 정수 판단하는 함수 추가

--- src/main/java/lotto/input/PurchaseAmountProcessor.java
+++ src/main/java/lotto/input/PurchaseAmountProcessor.java
@@ -8,10 +8,7 @@ public class PurchaseAmountProcessor {
 
     private static final int PURCHASE_UNIT = 1000;
     private static final String POSITIVE_SIGN = "+";
-    private static final String NEGATIVE_SIGN = "-";
     private static final String NUMERIC_REGEX = "\\d+";
-    private static final String DECIMAL_POINT = ".";
-    private static final String ZERO = "0";
 
     private PurchaseAmountProcessor() {
     }
@@ -28,14 +25,10 @@ public class PurchaseAmountProcessor {
 
     private static int validateAndParse(String input) {
         InputUtil.validateEmptyInput(input);
-
         String trimmedInput = input.trim();
-
+        InputUtil.validatePositiveInteger(trimmedInput);
         if (trimmedInput.startsWith(POSITIVE_SIGN)) {
             throw new IllegalArgumentException(POSITIVE_SIGN_INPUT.getMessage());
-        }
-        if (trimmedInput.startsWith(NEGATIVE_SIGN) || ZERO.equals(trimmedInput) || trimmedInput.contains(DECIMAL_POINT)) {
-            throw new IllegalArgumentException(ONLY_POSITIVE_INPUT.getMessage());
         }
         if (!trimmedInput.matches(NUMERIC_REGEX)) {
             throw new IllegalArgumentException(NON_NUMERIC_INPUT.getMessage());
@@ -43,6 +36,4 @@ public class PurchaseAmountProcessor {
 
         return Integer.parseInt(trimmedInput);
     }
-
-
 }
--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,11 +1,24 @@
 package lotto.util;
 
 import static lotto.exception.ExceptionMessage.EMPTY_INPUT;
+import static lotto.exception.ExceptionMessage.ONLY_POSITIVE_INPUT;
 
 public class InputUtil {
+
+    private static final String DECIMAL_POINT = ".";
+    private static final String ZERO = "0";
+    private static final String NEGATIVE_SIGN = "-";
+
     public static void validateEmptyInput(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(EMPTY_INPUT.getMessage());
         }
     }
+
+    public static void validatePositiveInteger(String input){
+        if (input.startsWith(NEGATIVE_SIGN) || ZERO.equals(input) || input.contains(DECIMAL_POINT)) {
+            throw new IllegalArgumentException(ONLY_POSITIVE_INPUT.getMessage());
+        }
+    }
+
 }
