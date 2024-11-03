4c13c2ab4597a2a1b0011100815c4458d1306387
sejinezy <psj8880@naver.com>
2024-11-03 15:25:25 +0900

    refactor(LottoGeneratorTest): 불필요한 import문 삭제

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,9 +1,7 @@
 package lotto;
 
 import org.junit.jupiter.api.Test;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
