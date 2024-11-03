0cae905bf30c538e25a55cafe04299280f391306
Chan531 <chan657312@gmail.com>
2024-11-03 17:34:34 +0900

    chore(LottoTest): 패키지 이동
    model 내로 이동

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,13 +1,11 @@
-package lotto;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.model.Lotto;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
