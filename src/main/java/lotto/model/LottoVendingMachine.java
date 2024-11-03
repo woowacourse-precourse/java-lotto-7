76083500667971c907bba8c64e658d4d83cac73c
jinno Yun <jinno321@naver.com>
2024-11-04 00:18:43 +0900

    feat: 여러개의 로또를 돈에 맞춰 발급해주는 메서드 생성 - issueNewLottos:LottoVendingMachine

--- src/main/java/lotto/model/LottoVendingMachine.java
+++ src/main/java/lotto/model/LottoVendingMachine.java
@@ -2,10 +2,22 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoVendingMachine {
 
+    public List<Lotto> issueNewLottos(int money) {
+        checkMoney(money);
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < money / 1000; i++) {
+            Lotto lotto = issueNewLotto();
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
 
     private Lotto issueNewLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
