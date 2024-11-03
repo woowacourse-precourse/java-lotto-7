6d2b980c9bef79f49351cb0669cc82f92ef1a78b
pakaheo <k70923@konkuk.ac.kr>
2024-11-03 10:41:52 +0900

    refactor : Manage LOTTO_SIZE constant other class
    manage LOTTO_SIZE constant in LottoValidator instead of Constants

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -3,7 +3,6 @@ package constants;
 import java.text.DecimalFormat;
 
 public class Constants {
-
-    public static final int LOTTO_SIZE = 6;
+    
     public static final DecimalFormat AMOUNT_NOTATION = new DecimalFormat("#,###");
 }
