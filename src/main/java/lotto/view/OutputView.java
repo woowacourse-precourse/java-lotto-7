72aea466219649954f3c560584bc86baa0021414
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 02:08:33 +0900

    feat(OutputView): 구매한 로또 개수를 출력하는 메서드 추가
    - 구매한 로또 개수를 출력하기 위한 printNumberOfPurchasedLottos 메서드 추가
    - 상수 PURCHASED_LOTTO_COUNT_FORMAT 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,13 @@ package lotto.view;
 
 public class OutputView {
 
+    private static final String PURCHASED_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
+
     public void printErrorMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printNumberOfPurchasedLottos(int count) {
+        System.out.printf(PURCHASED_LOTTO_COUNT_FORMAT, count);
     }
 }
