9a24fcfedcde2cb439784dc8f9d4c785cd1c6ebd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 02:52:45 +0900

    feat(OutputView): 보고서 출력 형식 상수 추가 및 로또 결과 보고서 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,14 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
+import lotto.model.LottoPrize;
 
 public class OutputView {
 
     private static final String PURCHASED_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
+    private static final String REPORT_FORMAT = "%d개 일치 (%d원) - %d개\n";
 
     public void printErrorMessage(String message) {
         System.out.println(message);
@@ -18,6 +21,12 @@ public class OutputView {
     public void printLottoList(List<Lotto> lottoList) {
         for (Lotto lotto : lottoList) {
             System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void printReport(Map<LottoPrize, Integer> matchCountMap) {
+        for (LottoPrize prize : LottoPrize.values()) {
+            System.out.printf(REPORT_FORMAT, prize.getMatchCount(), prize.getPrize(), matchCountMap.get(prize));
         }
     }
 }
