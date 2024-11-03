44b5768a6e5ed5496bb6ccbd4316989cfc32a21a
byeongdoohan <quden04@gmail.com>
2024-11-04 03:19:40 +0900

    refactor: 출력문 출력 이상 수정

--- src/main/java/lotto/view/LottoInfoMessages.java
+++ src/main/java/lotto/view/LottoInfoMessages.java
@@ -7,8 +7,8 @@ public enum LottoInfoMessages {
     INSERT_MANUAL_AMOUNT_START("수동 번호로 구매할 갯수를 입력해주세요. (최대 "),
     INSERT_MANUAL_AMOUNT_END("개 입력 가능)"),
     INSERT_MANUAL_NUMBERS_START("""
-            수동으로 구매할 1~45 사이의 겹치지 않는 6개의 번호를 쉼표로 구분해 입력해주세요. (수동 구매 갯수가 [ 
-            """),
+            수동으로 구매할 1~45 사이의 겹치지 않는 6개의 번호를 쉼표로 구분해 입력해주세요.
+            (수동 구매 갯수가 [\s"""),
     INSERT_MANUAL_NUMBERS_END(" ]개 남았습니다. Enter를 누르면 자동으로 번호를 입력합니다.)"),
     TOTAL_BUY_AMOUNT("[구입 번호 목록]"),
     INSERT_WINNING_NUMBER("""
