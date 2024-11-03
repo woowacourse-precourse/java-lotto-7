a299477e3c619ceea95d0a7ec193eff774dfbbe7
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:57:30 +0900

    chore(LottoTest) : 커맨드라인 자동 정렬

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -28,7 +28,7 @@ class LottoTest {
 
     @Test
     @DisplayName("validateTest 로또 number들이 6개가 아닌 경우")
-    void validateTest(){
+    void validateTest() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUM.getError());
@@ -36,7 +36,7 @@ class LottoTest {
 
     @Test
     @DisplayName("validateTest 로또 number 사이에 중복이 있을 때")
-    void validateDuplicateTest(){
+    void validateDuplicateTest() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_DUPLICATE.getError());
@@ -44,31 +44,31 @@ class LottoTest {
 
     @Test
     @DisplayName("validateTest 로또 number 정상 작동")
-    void validateCorrectTest(){
-        assertThatNoException().isThrownBy(() -> new Lotto(List.of(1,2,3,4,5,6)));
+    void validateCorrectTest() {
+        assertThatNoException().isThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)));
     }
 
     @Test
     @DisplayName("sortLotto 정상 작동")
-    void sortLottoTest(){
-        Lotto actualLotto = Lotto.sortLotto(new Lotto(List.of(6,4,3,2,1,7)));
-        Lotto expectedLotto = new Lotto(List.of(1,2,3,4,6,7));
+    void sortLottoTest() {
+        Lotto actualLotto = Lotto.sortLotto(new Lotto(List.of(6, 4, 3, 2, 1, 7)));
+        Lotto expectedLotto = new Lotto(List.of(1, 2, 3, 4, 6, 7));
         assertThat(actualLotto.getNumbers()).isEqualTo(expectedLotto.getNumbers());
     }
 
     @Test
     @DisplayName("sortLottoList 정상 작동")
-    void sortLottoListTest(){
+    void sortLottoListTest() {
         int lottoCount = 4;
         List<Lotto> expectedLottos = new ArrayList<>();
-        expectedLottos.add(new Lotto(List.of(1,2,3,4,6,7)));
-        expectedLottos.add(new Lotto(List.of(2,3,4,5,6,7)));
-        expectedLottos.add(new Lotto(List.of(3,4,5,6,7,8)));
-        expectedLottos.add(new Lotto(List.of(1,2,3,4,6,7)));
+        expectedLottos.add(new Lotto(List.of(1, 2, 3, 4, 6, 7)));
+        expectedLottos.add(new Lotto(List.of(2, 3, 4, 5, 6, 7)));
+        expectedLottos.add(new Lotto(List.of(3, 4, 5, 6, 7, 8)));
+        expectedLottos.add(new Lotto(List.of(1, 2, 3, 4, 6, 7)));
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     List<Lotto> actualLottos = Lotto.sortLottoList(lottoCount);
-                    for(int i = 0; i < lottoCount; i++){
+                    for (int i = 0; i < lottoCount; i++) {
                         assertThat(actualLottos.get(i)
                                 .getNumbers())
                                 .isEqualTo(expectedLottos
@@ -76,18 +76,18 @@ class LottoTest {
                                         .getNumbers());
                     }
                 },
-                List.of(6,4,3,2,1,7),
-                List.of(2,3,4,5,6,7),
-                List.of(8,7,6,5,4,3),
-                List.of(1,2,3,4,6,7)
+                List.of(6, 4, 3, 2, 1, 7),
+                List.of(2, 3, 4, 5, 6, 7),
+                List.of(8, 7, 6, 5, 4, 3),
+                List.of(1, 2, 3, 4, 6, 7)
         );
     }
 
     @Test
     @DisplayName("gradeLotto 정상 작동 테스트")
-    void gradeLottoTest(){
-        Lotto answerLotto = new Lotto(List.of(1,2,3,4,5,6));
-        Lotto targetLotto = new Lotto(List.of(1,2,3,4,5,8));
+    void gradeLottoTest() {
+        Lotto answerLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto targetLotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
         int bonus = 8;
         MyResults actualMyResult = Lotto.gradeLotto(answerLotto, targetLotto, bonus);
         MyResults expectedMyResult = new MyResults(5, true);
