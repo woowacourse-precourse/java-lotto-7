ac1f3d632e94f402923f683017e3b0183f31dcf3
JEONGHUNJEON <jjhoon06@gmail.com>
2024-11-03 22:54:54 +0900

    refactor: 출력 방식 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,42 @@
 package lotto.view;
 
+import lotto.domain.LottoWinningTierManager;
+
 public class OutputView {
     public void printMessage (String message) {
         System.out.println(message);
+    }
+    public void printNewLine() {
+        printMessage("");
+    }
+    public void printRequestPurchaseAmount () {
+        printMessage(RequestMessage.ENTER_PURCHASE_AMOUNT.getMessage());
+    }
+    public void printRequestWinningNumbers () {
+        printNewLine();
+        printMessage(RequestMessage.ENTER_WINNING_NUMBERS.getMessage());
+    }
+    public void printRequestBonusNumber () {
+        printNewLine();
+        printMessage(RequestMessage.ENTER_BONUS_NUMBER.getMessage());
+    }
+
+    public void printPurchasedLottoCount (int lottoCount) {
+        printNewLine();
+        printMessage(ResultMessage.PURCHASED_LOTTO.getIntMessage(lottoCount));
+    }
+    public void printStartWinResult () {
+        printNewLine();
+        printMessage(ResultMessage.WIN_RESULT.getMessage());
+    }
+    public void printTotalProfitRate (double profitRate) {
+        printMessage(ResultMessage.TOTAL_PROFIT_RATE.getProfitRateMessage(profitRate));
+    }
+    public void printLottoPlace (LottoWinningTierManager lottoWinningTierManager) {
+        lottoWinningTierManager.getLottoWinningTiers().forEach(((tier, value) -> {
+            if (tier.getMatchCount() != 0 && tier.getCountMessage(lottoWinningTierManager.getWinningTierCount(tier)) != null) {
+                printMessage(tier.getCountMessage(lottoWinningTierManager.getLottoWinningTiers().get(tier)));
+            }
+        }));
     }
 }
--- src/main/java/lotto/view/ResultMessage.java
+++ src/main/java/lotto/view/ResultMessage.java
@@ -3,11 +3,6 @@ package lotto.view;
 public enum ResultMessage {
     PURCHASED_LOTTO("%d개를 구매했습니다."),
     WIN_RESULT("당첨 통계\n---"),
-    FIFTH_PLACE("3개 일치 (5,000) - %d개"),
-    FOURTH_PLACE("4개 일치 (50,000) - %d개"),
-    THIRD_PLACE("5개 일치 (1,500,000원) - %d개"),
-    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000) - %d개"),
-    FIRST_PLACE("6개 일치 (2,000,000,000원) - %d개"),
     TOTAL_PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
@@ -18,7 +13,7 @@ public enum ResultMessage {
     public String getMessage() {
         return message;
     }
-    public String getWinMessage(int place) {
+    public String getIntMessage(int place) {
         return String.format(message, place);
     }
     public String getProfitRateMessage(double rate) {
