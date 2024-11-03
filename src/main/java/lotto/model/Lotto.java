987cafcd18a4d6874228483175ca31cae07f3a38
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-03 19:06:49 +0900

    refactor: "[ERROR] 로또 번호는 6개여야 합니다." 문자열 상수로 추출

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,7 +3,10 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private static final String ERROR_LOTTO_NUMBERS_BE_SIX_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -12,7 +15,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ERROR_LOTTO_NUMBERS_BE_SIX_MESSAGE);
         }
     }
 
