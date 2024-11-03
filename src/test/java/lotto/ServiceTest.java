cef49568a1e68661b4a5ccb9af2af6289395e264
jungsu <vwx1@naver.com>
2024-11-03 18:08:01 +0900

    test: 생성된 복권 리스트 당첨 결과 체크 로직
    checkWinningNumbers를 검증하는 Test를 생성했습니다.
    - 당첨 결과가 예상과 같은 지 체크하기 위해 테스트를 생성했습니다.
    - 2등은 로직이 달라서 추가로 체크하기 위해 테스트를 생성했습니다.

--- src/test/java/lotto/ServiceTest.java
+++ src/test/java/lotto/ServiceTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import lotto.model.Lotto;
 import lotto.service.LottoService;
 import lotto.service.LottosServiceInterface;
@@ -14,19 +15,52 @@ import java.util.List;
 class ServiceTest {
 
   private LottosServiceInterface lottosService;
+  private List<Lotto> lottos;
 
   @BeforeEach
   void setUp() {
     lottosService = new LottoService();
+    // 8개 로또 리스트
+    lottos = new ArrayList<>();
+    lottos.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+    lottos.add(new Lotto(List.of(3, 5, 11, 16, 32, 38)));
+    lottos.add(new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+    lottos.add(new Lotto(List.of(1, 8, 11, 31, 41, 42)));
+    lottos.add(new Lotto(List.of(13, 14, 16, 38, 42, 45)));
+    lottos.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+    lottos.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+    lottos.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
   }
 
   @Test
   @DisplayName("로또를 생성하는 테스트")
-  void generateLottosTest() {
+  void 로또를_생성하는_테스트() {
     int purchaseAmount = 10000;
-    List<Lotto> lottos = lottosService.generateLottosByAmount(purchaseAmount);
-    assertThat(lottos).isNotEmpty(); // 생성된 로또가 비어 있지 않음을 확인합니다.
-    assertThat(lottos.size()).isEqualTo(10); // 10개의 로또가 생성되었는지 확인합니다.
+    List<Lotto> testLotto = lottosService.generateLottosByAmount(purchaseAmount);
+    assertThat(testLotto).isNotEmpty(); // 생성된 로또가 비어 있지 않음을 확인합니다.
+    assertThat(testLotto.size()).isEqualTo(10); // 10개의 로또가 생성되었는지 확인합니다.
+  }
+
+  @Test
+  @DisplayName("당첨 결과 리스트 테스트")
+  void 당첨_결과_리스트_테스트() {
+    List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+    int bonusNumber = 7;
+
+    List<Integer> result = lottosService.checkWinningNumbers(lottos, winningNumbers, bonusNumber);
+
+    assertThat(result).containsExactly(1, 0, 0, 0, 0);
+  }
+
+  @Test
+  @DisplayName("당첨 결과 리스트 테스트 - 2등 로또 당첨")
+  void 당첨_결과_리스트_테스트_2등() {
+    List<Integer> winningNumbers = List.of(9, 21, 23, 41, 42, 43);
+    int bonusNumber = 8;
+
+    List<Integer> result = lottosService.checkWinningNumbers(lottos, winningNumbers, bonusNumber);
+
+    assertThat(result).containsExactly(0, 0, 0, 1, 0);
   }
 
 }
