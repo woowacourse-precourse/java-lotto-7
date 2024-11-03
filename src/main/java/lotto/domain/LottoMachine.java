21b733d5ce205aab38fdd4567517bb36cb96c104
SeongJunChun <csj991210@naver.com>
2024-11-04 01:51:13 +0900

    feat: 일정 갯수 만큼 로또를 생성하고 관리하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.service.Generator;
+
+public class LottoMachine {
+
+    private final List<Lotto> lottos;
+
+    private LottoMachine(int amount, Generator<Lotto> lottoGenerator) {
+        lottos = IntStream.range(0, amount)
+                .mapToObj(i -> lottoGenerator.generate())
+                .collect(Collectors.toList());
+    }
+
+    public static LottoMachine buyLotto(int amount, Generator<Lotto> lottoGenerator) {
+        return new LottoMachine(amount, lottoGenerator);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+}
