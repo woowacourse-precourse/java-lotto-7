3c5c9ab6ea53c0bafa0f0cc481143efe3f0bef3e
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-03 21:15:01 +0900

    feat(WinningNumbers) : 사용자가 입력한 로또 번호와, 보너스 번호를 저장하는 기능 추가

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+}
