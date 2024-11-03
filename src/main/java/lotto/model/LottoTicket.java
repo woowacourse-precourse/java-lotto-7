1fa4c104f88b4e76a96e50c43e2dd9ec5251c55f
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 22:50:30 +0900

    refactor(lottoTicket): 로또 구매 기능 분리
    - lottoTicket이 lotto에 의존성을 가지고 있기 때문에 purchase기능을 service로 분리

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -6,16 +6,12 @@ import java.util.List;
 public class LottoTicket {
     private final List<Lotto> lottoTicket;
 
-    public LottoTicket(List<Lotto> lottoTicket) {
-        this.lottoTicket = lottoTicket;
+    public LottoTicket() {
+        this.lottoTicket = new ArrayList<>();
     }
 
-    public static LottoTicket purchase(int quantity) {
-        List<Lotto> lottoTicket = new ArrayList<>();
-        for (int i = 0; i < quantity; i++) {
-            lottoTicket.add(new Lotto(Lotto.lottoGenerator()));
-        }
-        return new LottoTicket(lottoTicket);
+    public void lottoAdd(Lotto lotto) {
+        lottoTicket.add(lotto);
     }
 
     public List<Lotto> getLottoTicket() {
