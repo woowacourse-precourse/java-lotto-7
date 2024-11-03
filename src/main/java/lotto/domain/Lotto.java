4114fc2469decbaf796f2abae5b1646dce863d8c
윤민수 <koelysa5696@naver.com>
2024-11-03 10:34:06 +0900

    Feat: Lotto 클래스 보완
    - 패키지 위치 변경
    - 추가 검증사항 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,13 @@
 package lotto.domain;
 
+import static lotto.constants.LottoBuyException.NEGATIVE_NUMBER_NOT_ALLOWED;
+import static lotto.constants.LottoRule.LOTTO_NUMBER_SIX;
+import static lotto.constants.LottoRule.Lotto_Number_Max;
+import static lotto.constants.LottoRule.Lotto_Number_Min;
+import static lotto.constants.LottoRule.SAME_NUMBER_COUNT;
+import static lotto.constants.LottoWinNumberException.NUMBER_ONLY_SIX;
+import static lotto.constants.LottoWinNumberException.NUMBER_RANGE;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -14,23 +22,24 @@ public class Lotto {
     }
 
     private void checkNumberCount(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_NUMBER_SIX.getValue()) {
+            throw new IllegalArgumentException(NUMBER_ONLY_SIX.getMessage());
         }
     }
 
     private void checkSameNumber(List<Integer> numbers) {
         for (int number : numbers) {
-            if (Collections.frequency(numbers, number) > 1) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다");
+            if (Collections.frequency(numbers, number) > SAME_NUMBER_COUNT.getValue()) {
+                throw new IllegalArgumentException(NEGATIVE_NUMBER_NOT_ALLOWED.getMessage());
             }
         }
     }
 
     private void checkNumberRange(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 1~45의 정수만 입력하세요.");
+            if (number < Lotto_Number_Min.getValue() ||
+                    number > Lotto_Number_Max.getValue()) {
+                throw new IllegalArgumentException(NUMBER_RANGE.getMessage());
             }
         }
     }
