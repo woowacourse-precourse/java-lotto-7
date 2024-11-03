85cc5a2baa33bb2b506cdaebbe90cdb3c87d484c
mikekks <thdalsrb79@naver.com>
2024-11-03 14:26:51 +0900

    feat(Money): 로또 구입 금액 클래스 구현
    1. 로또 구입 금액 저장
    2. 로또 구입 금액 검증

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static lotto.global.ErrorMessage.*;
+
+public class Money {
+	private static final int ZERO_MONEY = 0;
+
+	private final int amount;
+
+	public Money(int amount) {
+		validateAmount(amount);
+		this.amount = amount;
+	}
+
+	private void validateAmount(int amount) {
+		if (amount <= ZERO_MONEY) {
+			throw new IllegalArgumentException(MONEY_AMOUNT_ERROR.getMessage());
+		}
+	}
+
+	public int getAmount(){
+		return amount;
+	}
+}
