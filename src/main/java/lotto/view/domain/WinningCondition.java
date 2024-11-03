28843d6bf51c85896d9ee54ca8c89db1a43a6b8f
TaegeunYou <dbxorms2310@naver.com>
2024-11-04 01:31:15 +0900

    refactor(WinningCondition): rank 필드 삭제

--- src/main/java/lotto/view/domain/WinningCondition.java
+++ src/main/java/lotto/view/domain/WinningCondition.java
@@ -4,19 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum WinningCondition {
-    FIRST(1, 6, false, 2000000000),
-    SECOND(2, 5, true, 30000000),
-    THIRD(3, 5, false, 1500000),
-    FOURTH(4, 4, false, 50000),
-    FIFTH(5, 3, false, 5000);
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000);
 
-    private final int rank;
     private final int winningNumberCount;
     private final boolean mustIncludeBonusNumber;
     private final int rewardAmount;
 
-    WinningCondition(int rank, int winningNumberCount, boolean mustIncludeBonusNumber, int rewardAmount) {
-        this.rank = rank;
+    WinningCondition(int winningNumberCount, boolean mustIncludeBonusNumber, int rewardAmount) {
         this.winningNumberCount = winningNumberCount;
         this.mustIncludeBonusNumber = mustIncludeBonusNumber;
         this.rewardAmount = rewardAmount;
