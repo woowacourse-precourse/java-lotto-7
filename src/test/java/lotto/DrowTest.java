f4b8fe77d1a7fd428993aaed42350cc0e737075f
taehyun <xogus5070318@gmail.com>
2024-11-03 23:05:07 +0900

    test(DrowTest): 예외처리 관련 테스트 구현
    당첨번호와 보너스번호 입력 과정에서 발생하는 예외처리가 올바르게 작동하는지 다음과 같은 테스트를 구현하였습니다.
    1. 당첨 번호와 보너스 번호는 1보다 작을 수 없다.
    2. 당첨 번호와 보너스 번호는 45보다 클 수 없다.
    3. 숫자는 중복되면 안된다.
    4. 보너스 번호는 당첨 번호와 중복되면 안된다.
    5. 당첨 번호는 숫자로 되어있어야 한다.
    6. 보너스 번호는 숫자로 되어있어야 한다.
    
    BREAKING CHANGE: None

--- src/test/java/lotto/DrowTest.java
+++ src/test/java/lotto/DrowTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DisplayName("추첨 테스트")
@@ -18,43 +20,87 @@ public class DrowTest {
         @Test
         void 당첨_번호는_숫자가_반드시_6개로_구분되어야_한다() {
 
+            Drow drow = new Drow();
+
+            String fiveNumbers = "1,2,3,4,5"; //5자리 숫자
+            String sixNumbers = "1,2,3,4,5,6"; //6자리 숫자
+            String sevenNumbers = "1,2,3,4,5,6,7"; //6자리 숫자
+
+            assertThat(drow.validateDrowNumbers(fiveNumbers))
+                    .isEqualTo(false);
+            assertThat(drow.validateDrowNumbers(sixNumbers)) // 6자리 숫자만 true 반환
+                    .isEqualTo(true);
+            assertThat(drow.validateDrowNumbers(sevenNumbers))
+                    .isEqualTo(false);
 
         }
 
         @Test
         void 당첨_번호는_숫자로_되어있어야_한다() {
 
+            Drow drow = new Drow();
+
+            String numbersIncludeAlphabet = "1,2,3,root,5,6";
+
+            assertThat(drow.validateDrowNumbers(numbersIncludeAlphabet))
+                    .isEqualTo(false);
         }
 
         @Test
-        void 당첨_번호는_1보다_작을_수_없다(){
+        void 보너스_번호는_숫자로_되어있어야_한다() {
+
+            Drow drow = new Drow();
+
+            String bonusNumberAlphabet = "root";
+
+            assertThat(drow.validateBonusNumber(bonusNumberAlphabet))
+                    .isEqualTo(false);
+        }
+
+
+        @Test
+        void 당첨_번호와_보너스_번호는_1보다_작을_수_없다(){
+
+            Drow drow = new Drow();
+
+            int smallNumber = 0;
+
+            assertThatThrownBy(() -> drow.validateInputNumber(smallNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
 
         }
 
         @Test
-        void 당첨_번호는_45보다_클_수_없다(){
+        void 당첨_번호와_보너스_번호는_45보다_클_수_없다(){
 
-        }
+            Drow drow = new Drow();
 
-        @Test
-        void 보너스_번호는_1보다_작을_수_없다(){
+            int bigNumber = 46;
 
-        }
-
-        @Test
-        void 보너스_번호는_45보다_클_수_없다(){
-
+            assertThatThrownBy(() -> drow.validateInputNumber(bigNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
 
         @Test
         void 숫자는_중복되면_안된다(){
+            Drow drow = new Drow();
 
+            String duplicateNumbers = "41,42,43,44,45,45";
+
+            assertThat(drow.validateDrowNumbers(duplicateNumbers))
+                    .isEqualTo(false);
         }
 
         @Test
-        void 보너스_번호_또한_당첨번호_숫자와_중복되면_안된다(){
+        void 보너스_번호는_당첨번호_숫자와_중복되면_안된다(){
+            Drow drow = new Drow();
 
+            List<Integer> drawNumbers = List.of(1, 2, 3, 4, 5, 6);
+            int duplicateBonusNumber = 6;
+
+            assertThatThrownBy(() -> drow.validateDuplicationNumber(drawNumbers,duplicateBonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
 
