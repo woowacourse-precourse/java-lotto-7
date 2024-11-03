336711a6aa4d0e3d1a33f2861df659e8d1831f97
HyeongSeok <hyeongseok1995@gmail.com>
2024-11-03 23:59:56 +0900

    refator : 돌아가는 쓰레기 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,18 +3,37 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import lotto.policy.LottoPricePolicy;
 
 public class InputView {
 
     public static int budget(){
-        int initialCapital = Integer.parseInt(Console.readLine());
-        validateThousandUnit(initialCapital);
-        return initialCapital;
+        int budget;
+        while(true){
+            try{
+                String input = Console.readLine();
+                validateNumber(input);
+                validateThousandUnit(input);
+
+                budget = Integer.parseInt(input);
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return budget;
     }
 
-    private static void validateThousandUnit(long initialCapital) {
-        if(initialCapital % 1000 != 0){
-            throw new IllegalArgumentException("[ERROR] 천원단위의 구입금액을 입력해야 합니다. ");
+    private static void validateNumber(String input){
+        String number = "^[0-9]*$";
+        if(!(input.matches(number))){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private static void validateThousandUnit(String input) {
+        if(Long.parseLong(input) % LottoPricePolicy.LOTTO_TICKET_PRICE.price() != 0){
+            throw new IllegalArgumentException("[ERROR] 천원단위의 구입금액을 입력해야 합니다.");
         }
     }
 
