81ff4611ff969a0afe1b34b34dd9d4fe24ebfb06
SeongbinPark <hyucse22@gmail.com>
2024-11-03 14:43:21 +0900

    test(BonusNumberTest): 보너스 번호가 정상적으로 생성된다.

--- src/test/java/lotto/unit/BonusNumberTest.java
+++ src/test/java/lotto/unit/BonusNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.constants.ErrorMessages;
@@ -20,7 +21,7 @@ class BonusNumberTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE);
     }
-    
+
     @Test
     @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우 예외가 발생한다.")
     void bonusNumberDuplicateWithWinningNumbers() {
@@ -30,5 +31,15 @@ class BonusNumberTest {
         assertThatThrownBy(() -> new BonusNumber(bonusNumber, winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.BONUS_NUMBER_DUPLICATE);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 정상적으로 생성된다.")
+    void createBonusNumberSuccessfully() {
+        String input = "1,2,3,4,5,6";
+        WinningNumbers winningNumbers = new WinningNumbers(input);
+        int bonusNumber = 7;
+        BonusNumber bonus = new BonusNumber(bonusNumber, winningNumbers);
+        assertThat(bonus.getBonusNumber()).isEqualTo(7);
     }
 }
