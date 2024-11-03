a81e162973c58e74d77facbee0c05d1c88a700c0
kjyyjk <rlawnsdud920@gmail.com>
2024-11-04 02:02:43 +0900

    refactor: LottoManager의 run() 메서드를 분리한다
    - 가독성을 위해 관련 로직들을 하나의 메서드로 묶는다.
    ex) purchaseLottoes() 안에 로또 구매, 출력(Output 책임)로직을 포함한다.

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -5,6 +5,9 @@ import static lotto.view.Input.inputPurchaseAmount;
 import static lotto.view.Input.inputWinningNumbers;
 import static lotto.InputParser.parseInt;
 import static lotto.InputParser.parseWinningNumbers;
+import static lotto.view.Output.printPurchaseMessage;
+import static lotto.view.Output.printReturnRate;
+import static lotto.view.Output.printWinningStatistics;
 
 import java.util.List;
 import lotto.view.Output;
@@ -14,17 +17,10 @@ public class LottoManager {
     public void run() {
         PurchaseAmount purchaseAmount = getPurchaseAmount();
         List<Lotto> lottoes = purchaseLottoes(purchaseAmount);
-        Output.printPurchaseMessage(lottoes.size());
-        lottoes.forEach(Output::printLotto);
         WinningNumbers winningNumbers = getWinningNumbers();
         BonusNumber bonusNumber = getBonusNumber(winningNumbers);
-        WinningRecord winningRecord = new WinningRecord();
-        lottoes.stream()
-                .map(lotto -> lotto.getRank(winningNumbers, bonusNumber))
-                .forEach(winningRecord::put);
-        Output.printWinningStatistics(winningRecord);
-        int totalWinningAmount = winningRecord.calculateTotalWinningAmount();
-        Output.printReturnRate(purchaseAmount.calculateReturnRate(totalWinningAmount));
+        WinningRecord winningRecord = getWinningRecord(lottoes, winningNumbers, bonusNumber);
+        printResult(purchaseAmount, winningRecord);
     }
 
     private static PurchaseAmount getPurchaseAmount() {
@@ -59,6 +55,24 @@ public class LottoManager {
     }
 
     private List<Lotto> purchaseLottoes(final PurchaseAmount purchaseAmount) {
-        return LottoMachine.purchaseLottoes(purchaseAmount);
+        List<Lotto> lottoes = LottoMachine.purchaseLottoes(purchaseAmount);
+        printPurchaseMessage(lottoes.size());
+        lottoes.forEach(Output::printLotto);
+        return lottoes;
+    }
+
+    private WinningRecord getWinningRecord(List<Lotto> lottoes, WinningNumbers winningNumbers,
+                                           BonusNumber bonusNumber) {
+        WinningRecord winningRecord = new WinningRecord();
+        lottoes.stream()
+                .map(lotto -> lotto.getRank(winningNumbers, bonusNumber))
+                .forEach(winningRecord::put);
+        return winningRecord;
+    }
+
+    private static void printResult(PurchaseAmount purchaseAmount, WinningRecord winningRecord) {
+        printWinningStatistics(winningRecord);
+        int totalWinningAmount = winningRecord.calculateTotalWinningAmount();
+        printReturnRate(purchaseAmount.calculateReturnRate(totalWinningAmount));
     }
 }
