2ec34c66f447546bff65f52165b0c4ff8708b985
yush <yush2202@gmail.com>
2024-11-04 03:29:28 +0900

    test($ UnitConverter): 단위 표기 기능 테스트

--- src/test/java/lotto/utils/UnitConverterTest.java
+++ src/test/java/lotto/utils/UnitConverterTest.java
@@ -3,6 +3,7 @@ package lotto.utils;
 import static lotto.utils.UnitConverter.convertUnit;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,8 +21,17 @@ public class UnitConverterTest {
     @Test
     @DisplayName("주어진 실수를 ,를 붙여 반환하는지 확인합니다.")
     void 실수_단위_나누기_확인() {
-        double profit = 10000.2;
+        BigDecimal profit = new BigDecimal("10000.2");
         String answer = "10,000.2";
+
+        assertThat(convertUnit(profit)).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("주어진 실수 마지막에 .0을 붙여 반환하는지 확인합니다.")
+    void 실수_단위_나누기_확인2() {
+        BigDecimal profit = new BigDecimal("10000.0");
+        String answer = "10,000.0";
 
         assertThat(convertUnit(profit)).isEqualTo(answer);
     }
