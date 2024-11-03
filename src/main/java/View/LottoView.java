b1e1a8716456cd465f110b4882d1b03adc5dbba5
kyunghoon01 <sports0124@naver.com>
2024-11-03 21:49:01 +0900

    feat: 구입한 로또 개수 출력 기능 구현

--- src/main/java/View/LottoView.java
+++ src/main/java/View/LottoView.java
@@ -1,11 +1,19 @@
 package View;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.PurchaseCount;
 
 public class LottoView {
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
         return purchaseAmount;
+    }
+
+    public String printPurchasedLottoCountFromView(PurchaseCount purchaseCount) {
+        int count = purchaseCount.getPurchaseCount();
+        String result = count + "개를 구매했습니다.";
+        System.out.println(result);
+        return result;
     }
 }
