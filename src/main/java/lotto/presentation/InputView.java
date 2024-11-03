173e692fe180a8297793781f28eaeb5c93eddbd9
DK <laancer4@gmail.com>
2024-11-03 21:58:56 +0900

    fix : 출력 형식이 예시와 완전히 동일하도록 변경
    - System.lineSeparator() 사용해서 개행 추가

--- src/main/java/lotto/presentation/InputView.java
+++ src/main/java/lotto/presentation/InputView.java
@@ -7,9 +7,10 @@ import java.util.function.Consumer;
 import lotto.global.ErrorMessage;
 
 public class InputView {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String PROMPT_MONEY = "구입금액을 입력해 주세요.";
-    private static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String PROMPT_WINNING_NUMBERS = LINE_SEPARATOR + "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_BONUS_NUMBER = LINE_SEPARATOR + "보너스 번호를 입력해 주세요.";
 
     private InputView() {
         throw new UnsupportedOperationException();
--- src/main/java/lotto/presentation/OutputView.java
+++ src/main/java/lotto/presentation/OutputView.java
@@ -1,19 +1,21 @@
 package lotto.presentation;
 
 public class OutputView {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String PURCHASE_AMOUNT_IS = "개를 구매했습니다.";
     private static final String EARNING_RATE = "총 수익률은 %.1f%%입니다."; // 소수 첫째 자리까지 표시
+    private static final String STATISTICS = LINE_SEPARATOR + "당첨 통계" + LINE_SEPARATOR + "---";
 
     private OutputView() {
         throw new UnsupportedOperationException();
     }
 
     public static void printPurchaseAmount(int quantity) {
-        System.out.println(quantity + PURCHASE_AMOUNT_IS);
+        System.out.println(LINE_SEPARATOR + quantity + PURCHASE_AMOUNT_IS);
     }
 
     public static void printPurchasedLottos(String purchasedLottos) {
-        System.out.println(purchasedLottos);
+        System.out.println(purchasedLottos + LINE_SEPARATOR);
     }
 
     public static void printEarningRate(float earningRate) {
@@ -22,6 +24,7 @@ public class OutputView {
     }
 
     public static void printResult(String summary) {
+        System.out.println(STATISTICS);
         System.out.println(summary);
     }
 }
