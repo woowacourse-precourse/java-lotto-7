93df7318edbc8c256466e53dbff36fe33caf5479
peanut990 <peanut1998@naver.com>
2024-11-03 23:57:22 +0900

    feat(view): 당첨 번호와 보너스 번호 입력 기능

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -7,4 +7,14 @@ public class LottoInputView {
         System.out.println("구입금액을 입력해 주세요.");
         return readLine();
     }
+
+    public String inputWinningNums() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return readLine();
+    }
+
+    public String inputBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return readLine();
+    }
 }
