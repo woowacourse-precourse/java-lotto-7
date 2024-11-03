066faddcd620de8d636f5ff2436cd04c3c71ff23
SolfE <love23869318@gmail.com>
2024-11-04 02:39:04 +0900

    fix: change lotto exception handling to throws

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,12 +5,12 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
