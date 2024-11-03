fe695d37fcde5a4f3051a9fb3aa5b2031b09eee9
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-11-03 17:34:33 +0900

    refactor: Lotto validate 기능 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,16 +14,28 @@ public class Lotto {
 
 
     private void validate(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>();
+        validateLottoSize(numbers);
+        validateLottoRange(numbers);
+        validateUniqueNumbers(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
         }
+    }
 
+    private void validateLottoRange(List<Integer> numbers) {
         for (int number : numbers) {
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException(ErrorMessage.LOTTO_OUT_OF_RANGE.getMessage());
             }
+        }
+    }
 
+    private void validateUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int number : numbers) {
             if (!uniqueNumbers.add(number)) {
                 throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
             }
