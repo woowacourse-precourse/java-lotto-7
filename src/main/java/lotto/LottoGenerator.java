8b8077dbc0670d77f7cd1e0ee8eed0ab47031365
sejinezy <psj8880@naver.com>
2024-11-03 17:05:26 +0900

    fix(LottoGenerator): 로또 번호 생성을 ArrayList로 변환하여 정렬 용이성 개선
    - `Randoms.pickUniqueNumbersInRange`의 반환 값을 ArrayList로 변환하여 로또 번호 정렬이 쉽게
    이루어지도록 수정

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class LottoGenerator {
 
     private static List<Integer> generateNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(numbers);
         return numbers;
     }
