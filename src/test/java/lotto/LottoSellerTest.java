cab8687a13ce90198bf4417fba2f4906f7d76aa0
SolfE <love23869318@gmail.com>
2024-11-04 02:31:21 +0900

    test: add winner number read test

--- src/test/java/lotto/LottoSellerTest.java
+++ src/test/java/lotto/LottoSellerTest.java
@@ -15,7 +15,7 @@ class LottoSellerTest extends NsTest {
     void 로또_금액_정상동작() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
-                    run("8000");
+                    runException("8000");
                     assertThat(output()).contains(
                             "8개를 구매했습니다.",
                             "[8, 21, 23, 41, 42, 43]",
@@ -42,7 +42,7 @@ class LottoSellerTest extends NsTest {
     @Test
     void 로또_금액이_숫자가_아니면_에러_출력_후_재입력받는다() {
         assertSimpleTest(() -> {
-            run("8000a", "8000");
+            runException("8000a", "8000");
             assertThat(output())
                     .contains(ERROR_MESSAGE + "로또 금액은 숫자여야 합니다.");
         });
@@ -51,7 +51,7 @@ class LottoSellerTest extends NsTest {
     @Test
     void 로또_금액이_양수가_아니면_에러_출력_후_재입력받는다() {
         assertSimpleTest(() -> {
-            run("-8000", "8000");
+            runException("-8000", "8000");
             assertThat(output())
                     .contains(ERROR_MESSAGE + "로또 금액은 양수여야 합니다.");
         });
@@ -60,9 +60,27 @@ class LottoSellerTest extends NsTest {
     @Test
     void 로또_금액이_1000원으로_나누어_떨어지지_않으면_에러_출력_후_재입력받는다() {
         assertSimpleTest(() -> {
-            run("8500", "8000");
+            runException("8500", "8000");
             assertThat(output())
                     .contains(ERROR_MESSAGE + "로또 금액은 1000원을 단위로 입력해야 합니다.");
+        });
+    }
+
+    @Test
+    void 로또_당첨_번호_정상동작() {
+        assertSimpleTest(() -> {
+            runException("8000", "1,2,3,4,5,6");
+            assertThat(output())
+                    .doesNotContain(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 로또_당첨_번호_입력에_다른_문자가_포함되면_에러_출력_후_재입력받는다() {
+        assertSimpleTest(() -> {
+            runException("8000", "1,2, ,4,5,6", "1,2,3,4,5,6");
+            assertThat(output())
+                    .contains(ERROR_MESSAGE + "숫자와 콤마(,)를 제외한 문자는 입력할 수 없습니다.");
         });
     }
 
