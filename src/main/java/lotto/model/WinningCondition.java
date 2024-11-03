3ff569018b973fc312478c5b54341c5ecf4b156f
KJungW <sagakkb0707@naver.com>
2024-11-03 14:45:33 +0900

    feat(model): WinningCondition 모델 추가
    추가 내용
    - 당첨 조건을 추상화한 모델 추가
    - 모델 관련된 검증로직을 모델 내에 추가

---
+++ src/main/java/lotto/model/WinningCondition.java
@@ -0,0 +1,48 @@
+package lotto.model;
+
+public class WinningCondition {
+
+    public static final String NOT_ZERO_OR_POSITIVE_PRICE_EXCEPTION_MESSAGE =
+            "우승조건에서 일치하는 번호가 음수인 경우는 허용하지 않습니다.";
+
+    private final int matchedNumberCount;
+    private final boolean isBonusNumberRequired;
+
+    public WinningCondition(int matchedNumberCount, boolean isBonusNumberRequired) {
+        validateZeroOrPositiveNumberCount(matchedNumberCount);
+        this.matchedNumberCount = matchedNumberCount;
+        this.isBonusNumberRequired = isBonusNumberRequired;
+    }
+
+    private void validateZeroOrPositiveNumberCount(int matchedNumberCount) {
+        if (matchedNumberCount < 0) {
+            throw new IllegalArgumentException(NOT_ZERO_OR_POSITIVE_PRICE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public boolean checkWinning(int matchedNumberCount, boolean isBonusNumberMatched) {
+        if (this.matchedNumberCount != matchedNumberCount) {
+            return false;
+        }
+        if (this.isBonusNumberRequired) {
+            return isBonusNumberMatched;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "WinningCondition{" +
+                "matchedNumberCount=" + matchedNumberCount +
+                ", isBonusNumberRequired=" + isBonusNumberRequired +
+                '}';
+    }
+
+    public int getMatchedNumberCount() {
+        return matchedNumberCount;
+    }
+
+    public boolean isBonusNumberRequired() {
+        return isBonusNumberRequired;
+    }
+}
