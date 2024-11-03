142e3fb499387b7504e1615fab03b33d948e25fd
MinseokGo <rhalstjr1999@naver.com>
2024-11-03 18:43:22 +0900

    feat: 당첨 번호, 보너스 번호와 비교하는 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,17 @@ public class Lotto {
         System.out.println(this.numbers);
     }
 
+    public int countMatchedNumber(final List<Integer> winningLottoNumbers) {
+        return numbers.stream()
+                .filter(winningLottoNumbers::contains)
+                .toList()
+                .size();
+    }
+
+    public boolean isMatchedBonusNumber(final int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
