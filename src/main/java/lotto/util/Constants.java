86b32e590be1763a1e0fa7180e2528ac728e3c87
wooqqq <sge0225@naver.com>
2024-11-03 23:40:30 +0900

    refactor: 당첨 번호 구분자 상수 클래스로 분리

--- src/main/java/lotto/util/Constants.java
+++ src/main/java/lotto/util/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_NUMBER_SIZE = 6;
+
+    public static final String DELIMITER = ",";
 }
--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Converter {
 
     public static List<Integer> convertLottoNumber(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(Constants.DELIMITER))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .toList();
