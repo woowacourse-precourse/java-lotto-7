b4a48add85ff78f199907e00275802c0d0b8187e
seha9631 <gktkddms9631@gmail.com>
2024-11-04 00:12:52 +0900

    feat(Lotto): 로또 번호 유효성 검증 기능 추가
    - 로또 번호에 중복이 없는지 확인하는 기능 추가
    - 로또 번호가 1부터 45 사이의 숫자인지 범위 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
 import java.util.List;
+import java.util.HashSet;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateCountOfNumbers(numbers);
+        validateDuplicateNumber(numbers);
+        validateRangeOfNumbers(numbers);
+    }
+
+    private static void validateCountOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRangeOfNumbers(List<Integer> numbers) {
+        boolean isOutOfRange = numbers.stream().anyMatch(num -> num < LOTTO_NUMBER_MIN || num > LOTTO_NUMBER_MAX);
+        if (isOutOfRange) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        boolean isDuplicated = new HashSet<>(numbers).size() != 6;
+        if(isDuplicated) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야만 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
