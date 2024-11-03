3621ab3788f6208d5e591427710995661767c5f0
soohalee <suha630@naver.com>
2024-11-03 12:04:17 +0900

    feat(Validator): InputView의 유효성검사 분리를 위해 Validator 클래스 추가

---
+++ src/main/java/lotto/validation/Validator.java
@@ -0,0 +1,58 @@
+package lotto.validation;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lotto.enums.ErrorMessage;
+import lotto.enums.LottoEnum;
+import lotto.utils.Utils;
+
+public class Validator {
+    public static void isEmpty(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
+        }
+    }
+
+    public static void isDigitOnly(String userInput) {
+        if (!Utils.isDigitOnly(userInput)) {
+            throw new NumberFormatException(ErrorMessage.NOT_NUMBER.getMessage());
+        }
+    }
+
+    public static void allDigits(String[] numbers) {
+        if (!Utils.allElementsAreDigits(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
+        }
+    }
+
+    public static void sizeEqual(String[] numbers, int number) {
+        if (!Utils.checkSizeEqual(numbers, LottoEnum.WINNING_NUMBER_COUNT.getNumber())) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    public static void allNumberRange(BigDecimal min, BigDecimal max, List<BigDecimal> winningNumber) {
+        if (!Utils.areAllNumbersValidRange(min, max, winningNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getMessage());
+        }
+    }
+
+    public static void DuplicateNumber(List<BigDecimal> winningNumber) {
+        if (!Utils.isDuplicateNumber(winningNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    public static void oneNumberRange(BigDecimal min, BigDecimal max, BigDecimal bonusNumber) {
+        if (!Utils.isInRange(min, max, bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE.getMessage());
+        }
+    }
+
+    public static void isDuplicateBonusNumber(List<Integer> winningNumberData, int bonusNumber) {
+        if (Utils.isNumberInList(winningNumberData, bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
+        }
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import lotto.enums.ErrorMessage;
 import lotto.enums.LottoEnum;
 import lotto.enums.OutputViewEnum;
 import lotto.utils.Utils;
+import lotto.validation.Validator;
 
 public class InputView {
 
@@ -15,50 +16,37 @@ public class InputView {
     public static BigDecimal inputPurchaseAmount() {
         OutputView.printPrompt(OutputViewEnum.PURCHASE_AMOUNT_INPUT.getMessage());
         String userInput = Console.readLine();
-        validateInput(userInput);
+        validatePurchaseAmountInput(userInput);
         return new BigDecimal(userInput);
     }
 
     //금액 valiadate
-    private static void validateInput(String userInput) {
-        if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
-        }
-        if (!Utils.isDigitOnly(userInput)) {
-            throw new NumberFormatException(ErrorMessage.NOT_NUMBER.getMessage());
-        }
+    private static void validatePurchaseAmountInput(String userInput) {
+        Validator.isEmpty(userInput);
+        Validator.isDigitOnly(userInput);
     }
 
     //inputWinningNumber 구현
     public static List<Integer> inputWinningNumber() {
         OutputView.printPrompt(OutputViewEnum.WINNING_NUMBER_INPUT.getMessage());
         String userInput = Console.readLine();
-        validateWinningNumber(userInput);
         String[] numbers = userInput.split(",");
-        List<Integer> winningNumber = Arrays.stream(numbers).map(number -> Integer.parseInt(number)).toList();
-        return winningNumber;
+        validateWinningNumber(userInput);
+        return Arrays.stream(numbers).map(number -> Integer.parseInt(number)).toList();
     }
 
     private static void validateWinningNumber(String userInput) {
-        if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
-        }
         String[] numbers = userInput.split(",");
-        if (!Utils.allElementsAreDigits(numbers)) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
-        }
-        if (!Utils.checkSizeEqual(numbers, LottoEnum.WINNING_NUMBER_COUNT.getNumber())) {
-            throw new IllegalArgumentException(
-                    ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage());
-        }
         List<BigDecimal> winningNumber = Arrays.stream(numbers).map(number -> new BigDecimal(number)).toList();
-        if (!Utils.areAllNumbersValidRange(new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
-                new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()), winningNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE_ERROR.getMessage());
-        }
-        if (!Utils.isDuplicateNumber(winningNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
-        }
+
+        Validator.isEmpty(userInput);
+        Validator.allDigits(numbers);
+        Validator.sizeEqual(numbers, LottoEnum.WINNING_NUMBER_COUNT.getNumber());
+        Validator.allNumberRange(
+                new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
+                new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()),
+                winningNumber);
+        Validator.DuplicateNumber(winningNumber);
     }
 
     //inputBonusNumber 구현
@@ -70,19 +58,14 @@ public class InputView {
     }
 
     private static void validateBonusNumberInput(String userInput, List<Integer> winningNumberData) {
-        if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTY_INPUT.getMessage());
-        }
-        if (!Utils.isDigitOnly(userInput)) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER.getMessage());
-        }
-        BigDecimal count = Utils.stringToNumber(userInput);
-        if (Utils.isNumberInList(winningNumberData, count.intValue())) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
-        }
-        if (!Utils.isInRange(new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
-                new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()), count)) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE.getMessage());
-        }
+        BigDecimal bonusNumber = Utils.stringToNumber(userInput);
+
+        Validator.isEmpty(userInput);
+        Validator.isDigitOnly(userInput);
+        Validator.oneNumberRange(
+                new BigDecimal(LottoEnum.MIN_LOTTO_RANGE.getNumber()),
+                new BigDecimal(LottoEnum.MAX_LOTTO_RANGE.getNumber()),
+                bonusNumber);
+        Validator.isDuplicateBonusNumber(winningNumberData,bonusNumber.intValue());
     }
 }
