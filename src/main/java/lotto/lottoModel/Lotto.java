b877f9805be9ec8f4d11fea02385703710150ebf
ppzepi <hsunguk1999@gmail.com>
2024-11-04 05:23:40 +0900

    refactor: 중복 체크 검사 생성

--- src/main/java/lotto/lottoModel/Lotto.java
+++ src/main/java/lotto/lottoModel/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
     }
 
     public List<Integer> getNumbers() {
