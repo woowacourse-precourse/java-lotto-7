e1b18bfe18a1e4da103e3f9807f0fe782e03856b
jinno Yun <jinno321@naver.com>
2024-11-04 00:14:27 +0900

    feat: 하나의 로또를 발급해주는 메서드 생성

--- src/main/java/lotto/model/LottoVendingMachine.java
+++ src/main/java/lotto/model/LottoVendingMachine.java
@@ -1,10 +1,19 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class LottoVendingMachine {
 
 
-    private void checkMoney(int money){
-        if(money % 1000 != 0){
+    private Lotto issueNewLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(lottoNumbers);
+    }
+
+    private void checkMoney(int money) {
+        if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
