222699f454d7d93ebed08678681e6b9615393e48
plan11plan <tjtfy7028@gmail.com>
2024-11-04 02:37:31 +0900

    test(StatisticsResult): 수익률 소수점 둘째자리 반올림

--- src/test/java/lotto/application/statistics/domain/StatisticsResultTest.java
+++ src/test/java/lotto/application/statistics/domain/StatisticsResultTest.java
@@ -1,6 +1,9 @@
 package lotto.application.statistics.domain;
 
 
+import static lotto.application.statistics.domain.Rank.FIFTH;
+import static lotto.application.statistics.domain.Rank.FIRST;
+import static lotto.application.statistics.domain.Rank.THIRD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,14 +17,14 @@ class StatisticsResultTest {
     void 당첨_등수별_개수_조회() {
         // given
         RankCounter counter = new RankCounter();
-        counter.add(Rank.FIRST);
-        counter.add(Rank.THIRD);
-        counter.add(Rank.THIRD);
+        counter.add(FIRST);
+        counter.add(THIRD);
+        counter.add(THIRD);
         StatisticsResult result = StatisticsResult.from(counter);
 
         // expect
-        assertThat(result.getCount(Rank.FIRST)).isEqualTo(1);
-        assertThat(result.getCount(Rank.THIRD)).isEqualTo(2);
+        assertThat(result.getCount(FIRST)).isEqualTo(1);
+        assertThat(result.getCount(THIRD)).isEqualTo(2);
     }
 
     @Test
@@ -29,7 +32,7 @@ class StatisticsResultTest {
     void 수익률_계산() {
         // given
         RankCounter counter = new RankCounter();
-        counter.add(Rank.FIFTH);  // 5000원
+        counter.add(FIFTH);  // 5000원
         StatisticsResult result = StatisticsResult.from(counter);
 
         // when
@@ -37,6 +40,21 @@ class StatisticsResultTest {
 
         // then
         assertThat(profitRate).isEqualTo(62.5);  // (5,000 / 8,000)* 100
+    }
+
+    @Test
+    @DisplayName("수익률 소수점 둘째자리 반올림")
+    void 수익률_소수점_둘째자리_반올림() {
+        // given
+        RankCounter counter = new RankCounter();
+        counter.add(FIFTH);
+        StatisticsResult result = StatisticsResult.from(counter);
+
+        // when
+        double profitRate = result.calculateProfitRate(7000);
+
+        // then
+        assertThat(profitRate).isEqualTo(71.43);
     }
 
 }
