8f952e35982194ad504231ca2860846b3a04ce17
JINSU LEE <bluesea9519@gmail.com>
2024-11-03 23:04:57 +0900

    feat: 당첨번호, 보너스 번호 일치 및 포함 비교 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,13 +12,34 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public int countMatchingNumbers(List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (Integer number : numbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
 
+    public boolean contains(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
 
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            }
+        }
+    }
 
 }
