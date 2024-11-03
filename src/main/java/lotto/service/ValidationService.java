ce9ffbd5c90872fdab0a62989ed562488013bc7b
byeongdoohan <quden04@gmail.com>
2024-11-04 01:46:07 +0900

    fix: 구입 금액, 구매 갯수 제대로 출력되지 않던 문제 해결

--- src/main/java/lotto/service/ValidationService.java
+++ src/main/java/lotto/service/ValidationService.java
@@ -14,36 +14,35 @@ public class ValidationService {
     }
 
     public int validatePayInput() {
-        int pay = 0;
         try {
             System.out.println(LottoInfoMessages.INSERT_PAY.text());
-            pay = Integer.parseInt(Console.readLine());
+            return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.addErrorText());
-            validatePayInput();
+            return validatePayInput();
         }
-        return pay;
     }
 
     public int validateManualAmountIsInteger(int enableAmount) {
-        int amount = 0;
         try {
-            System.out.println(LottoInfoMessages.INSERT_MANUAL_AMOUNT_START.text()+enableAmount
-                    +LottoInfoMessages.INSERT_MANUAL_AMOUNT_END.text());
-            amount = Integer.parseInt(Console.readLine());
-            validateOverManualAmount(amount, enableAmount);
+            System.out.println(LottoInfoMessages.INSERT_MANUAL_AMOUNT_START.text() + enableAmount
+                    + LottoInfoMessages.INSERT_MANUAL_AMOUNT_END.text());
+            int amount = Integer.parseInt(Console.readLine());
+            amount = validateOverManualAmount(amount,enableAmount);
+            return amount;
         } catch (NumberFormatException e) {
             System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.addErrorText());
-            validateManualAmountIsInteger(enableAmount);
+            return validateManualAmountIsInteger(enableAmount);
         }
-        return amount;
     }
 
-    private void validateOverManualAmount(int amount, int enableAmount) {
+    public int validateOverManualAmount(int amount, int enableAmount) {
         if (amount > enableAmount || amount < 0) {
             System.out.println(LottoErrorMessages.NOT_ENABLE_AMOUNT_START.addErrorText()
                     + enableAmount + LottoErrorMessages.NOT_ENABLE_AMOUNT_END.text());
-            validateManualAmountIsInteger(enableAmount);
+            return validateManualAmountIsInteger(enableAmount);
         }
+        return amount;
     }
 }
+
