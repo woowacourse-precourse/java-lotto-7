c63f1299359e0695b69e141779c2ace775413584
hongkikii <hyangmeeh@icloud.com>
2024-11-04 05:15:05 +0900

    refactor(WinningCondition): 클래스 이름 변경, 불필요 필드 제거

--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -1,18 +1,16 @@
 package lotto;
 
-public enum WinningCondition {
-    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
-    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
+public enum Winning {
+    FIFTH( 5000, "3개 일치 (5,000원) - "),
+    FOURTH( 50000, "4개 일치 (50,000원) - "),
+    THIRD( 1500000, "5개 일치 (1,500,000원) - "),
+    SECOND( 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST( 2000000000, "6개 일치 (2,000,000,000원) - ");
 
-    private final int matchingCondition;
     private final int prize;
     private final String printFormat;
 
-    WinningCondition(int matchingCondition, int prize, String printFormat) {
-        this.matchingCondition = matchingCondition;
+    Winning(int prize, String printFormat) {
         this.prize = prize;
         this.printFormat = printFormat;
     }
