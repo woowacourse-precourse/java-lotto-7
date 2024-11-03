de569db94d304c374686b218cc37182c37e0b86f
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-04 00:05:52 +0900

    Test : LottosTest 추가
    TargetLotto와 비교하여 일등부터 낙첨까지 매치되는지 테스트
    테스트 시 숫자들이 뽑힌 순서는 중요하지 않음을 확인
    숫자들이 뽑혔을 때 보너스번호는 2등과 3등을 나누는데에만 사용됨을 확인

---
+++ src/test/java/lotto/domain/RandomNumberGeneratorTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberGeneratorTest {
+
+    @RepeatedTest(1000)
+    void 숫자_생성_범위_중복성_테스트() {
+        List<Integer> generateNumbers = RandomNumberGenerator.generate();
+
+        long count = generateNumbers.stream()
+                .distinct()
+                .filter(num -> num <= 45 && num >= 1)
+                .count();
+
+        Assertions.assertThat(count).isEqualTo(6);
+    }
+
+    @Test
+    void 숫자_생성_독립성_테스트() {
+        List<Integer> generateNumbers = RandomNumberGenerator.generate();
+        int testRuns = 100;
+        int allowedCount = 5;
+        int count = 0;
+
+        for (int i = 0; i < testRuns; i++) {
+            List<Integer> temp = new ArrayList<>(generateNumbers);
+            List<Integer> newGenerated = RandomNumberGenerator.generate();
+            temp.retainAll(newGenerated);
+
+            if (temp.size() == 6) {
+                count++;
+            }
+        }
+        Assertions.assertThat(count).isLessThan(allowedCount);
+    }
+}
