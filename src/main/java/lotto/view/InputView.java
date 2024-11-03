0ef5c5889763a0ee765fa8409b34722e41be8b2e
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-04 02:05:04 +0900

    refactor: 입력에 공백이 포함되어 있을 경우 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,7 +13,6 @@ public class InputView {
     private static final String PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     private static final String CHECK_NUMBER_REGEX = "\\d+";
 
     private InputView() {
@@ -72,6 +71,7 @@ public class InputView {
     private static List<Integer> splitAndConvertUserInput(String userInput) {
         try {
             return List.of(userInput.split(",")).stream()
+                    .map(String::strip)
                     .map(Integer::parseInt)
                     .toList();
         } catch (NumberFormatException e) {
