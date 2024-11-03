ed2de2188eab2cc200af07b058c968e56cc76bd5
Yunsu4 <ysa020428@gmail.com>
2024-11-03 14:10:28 +0900

    feat(Lotto): 검증 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,13 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    public static final String WHITE_SPACE_REGEX = "\\s";
+    public static final String EMPTY_STRING = "";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +15,36 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getLottoNumbers(){
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
+        checkIfEmpty(numbers);
+        checkLottoNumberCount(numbers);
+        hasDuplicate(numbers);
+    }
+
+    private void checkLottoNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void hasDuplicate(List<Integer> numbers) {
+
+        Set<Integer> distinctLottoNumbers = new HashSet<>();
+
+        for (Integer lottoNumber : numbers) {
+            if (!distinctLottoNumbers.add(lottoNumber)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void checkIfEmpty(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
