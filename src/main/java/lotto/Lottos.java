c8bb30516280ed3bc7d16c8e804b03cf8b2b873e
xeimun <hinote444@naver.com>
2024-11-03 17:39:29 +0900

    refactor(Lottos, LottoService): 메소드 위치 수정하여 책임 분리

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,13 +1,24 @@
 package lotto;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.generator.NumberGenerator;
 
 public class Lottos {
 
     private final Set<Lotto> lottos;
 
-    public Lottos(Set<Lotto> lottos) {
+    private Lottos(Set<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public static Lottos of(int purchaseQuantity, NumberGenerator generator) {
+        return new Lottos(
+                IntStream.range(0, purchaseQuantity)
+                         .mapToObj(i -> new Lotto(generator.generateNumber()))
+                         .collect(Collectors.toSet())
+        );
     }
 
     public Set<Lotto> getLottos() {
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,19 +2,7 @@ package lotto.service;
 
 import static java.lang.Integer.parseInt;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import lotto.Lotto;
-import lotto.generator.NumberGenerator;
-
 public class LottoService {
-
-    public Set<Lotto> generateLottosByPurchaseQuantity(int purchaseQuantity, NumberGenerator generator) {
-        return IntStream.range(0, purchaseQuantity)
-                        .mapToObj(i -> new Lotto(generator.generateNumber()))
-                        .collect(Collectors.toSet());
-    }
 
     public int calculatePurchaseQuantity(String purchaseAmount) {
         return parseInt(purchaseAmount) / 1000;
