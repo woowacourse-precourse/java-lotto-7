9b547dc2d90f5404fea0a70cd0e007b793fa90c0
jinwook <wjswlsdnr333@gmail.com>
2024-11-03 19:03:34 +0900

    feat(Lotto): 로또 당첨 번호 비교 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,13 +2,15 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numbers.sort(Integer::compareTo); // 오름차순 정렬
         this.numbers = numbers;
     }
 
@@ -18,6 +20,54 @@ public class Lotto {
         }
     }
 
+    public void start(OutputView outputView,int bonusNumber){
+        lottoNumberCheck(outputView.getBoughtLottoNumbers(),bonusNumber);
+    }
+
+    public List<Integer> pushNumberTable(List<Integer> equalNumberTable,int equalNumberCount, boolean bonusNumber){
+        while(true){
+            if(equalNumberCount == 3){
+                equalNumberTable.set(0, equalNumberTable.get(0)+1);
+                break;
+            }
+            if(equalNumberCount == 4){
+                equalNumberTable.set(1, equalNumberTable.get(1)+1);
+                break;
+            }
+            if(equalNumberCount == 5 && bonusNumber){
+                equalNumberTable.set(3, equalNumberTable.get(3)+1);
+                break;
+            }
+            if(equalNumberCount == 5){
+                equalNumberTable.set(2, equalNumberTable.get(2)+1);
+                break;
+            }
+            if(equalNumberCount == 6){
+                equalNumberTable.set(4, equalNumberTable.get(4)+1);
+                break;
+            }
+        }
+        return equalNumberTable;
+    }
+
+    public List<Integer> lottoNumberCheck(List<List<Integer>> boughtLottoNumbers,int bonusNumber){
+        List<Integer> equalNumberTable = new ArrayList<>(Arrays.asList(0,0,0,0,0));
+
+        for(List<Integer> boughtLottoNumber : boughtLottoNumbers){
+            int equalNumberCount = 0;
+            boolean equalBonusNumber = false;
+            for(int i = 0 ; i < numbers.size() ; i++){
+                if(numbers.get(i).equals(boughtLottoNumber.get(i))){
+                    equalNumberCount++;
+                }
+                if(boughtLottoNumber.get(i).equals(bonusNumber)){
+                    equalBonusNumber = true;
+                }
+            }
+            equalNumberTable = pushNumberTable(equalNumberTable,equalNumberCount,equalBonusNumber);
+        }
+        return equalNumberTable;
+    }
 }
 
 
