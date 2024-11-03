2e7d09c8a6cf80b082dbf20327ff53ebe53b52a7
Seol-Munhyeok <abc1234568@naver.com>
2024-11-03 21:30:47 +0900

    refactor(LottoGenerator): 검증 로직의 조건문을 별도의 메서드로 분리

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -5,11 +5,19 @@ import camp.nextstep.edu.missionutils.Randoms;
 import static lotto.LottoConstants.*;
 
 public class LottoGenerator {
+    private boolean isValidMinimumPrice(int budget) {
+        return budget >= PRICE_PER_LOTTO.getValue();
+    }
+
+    private boolean isValidPriceUnit(int budget) {
+        return budget % PRICE_PER_LOTTO.getValue() == 0;
+    }
+
     public int getLottoCount(int budget) {
-        if (budget < PRICE_PER_LOTTO.getValue()) {
+        if (!isValidMinimumPrice(budget)) {
             throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO.getValue() + "원 이상의 금액을 입력하세요.");
         }
-        if (budget % PRICE_PER_LOTTO.getValue() != 0) {
+        if (!isValidPriceUnit(budget)) {
             throw new IllegalArgumentException("[ERROR] " + PRICE_PER_LOTTO.getValue() + "원 단위로만 구매 가능합니다.");
         }
         return budget / PRICE_PER_LOTTO.getValue();
