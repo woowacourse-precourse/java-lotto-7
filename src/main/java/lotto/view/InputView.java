b38446a0d3a38ce3bc20dc13e03c1c574c5b48d9
장영재 <urinaner@naver.com>
2024-11-03 16:04:47 +0900

    style(InputView): 줄맞춤

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,7 +11,8 @@ public class InputView {
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String INVALID_NUMBER = "[ERROR] 숫자를 입력해주세요";
-    public int getValue(){
+
+    public int getValue() {
         System.out.println(INPUT_MESSAGE);
         try {
             return Integer.parseInt(Console.readLine());
@@ -19,4 +20,25 @@ public class InputView {
             throw new IllegalArgumentException(INVALID_NUMBER);
         }
     }
+
+    public static List<Integer> getLottoNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        try {
+            return Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INVALID_NUMBER);
+        }
+    }
+
+    public int getBonusLottoNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INVALID_NUMBER);
+        }
+    }
+
 }
