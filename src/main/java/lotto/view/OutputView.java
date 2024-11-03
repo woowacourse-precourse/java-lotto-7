2b98b00a292d853b113e3a134599d590327326d2
slay <palgalow@gmail.com>
2024-11-03 16:41:13 +0900

    refactor(OutputView): 코드 리팩토링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,18 +1,28 @@
 package lotto.view;
 
-import lotto.domain.Lottos;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
 public class OutputView {
-
-    private static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
-
-    private Lottos lottos;
-
-    public OutputView(Lottos lottos) {
-        this.lottos = lottos;
+    public static void printPurchasedLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 
-    public void outputLottoCount() {
-        System.out.println(lottos.getLottoCount() + LOTTO_COUNT_MESSAGE);
+    public static void printResults(Map<LottoRank, Integer> results, double yield) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank == LottoRank.NONE) {
+                continue;
+            }
+            int count = results.getOrDefault(rank, 0);
+            System.out.println(rank.getMessage() + " - " + count + "개");
+        }
+        System.out.println("총 수익률은 " + String.format("%.1f", yield) + "%입니다.");
     }
 }
