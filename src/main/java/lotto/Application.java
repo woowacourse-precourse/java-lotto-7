b82234ac698591931bd043a20d7eeb91d4eec8a9
sinji <tobeabestns@naver.com>
2024-11-04 01:46:46 +0900

    refactor(Application): 다른 클래스들과 개행 구조 일치

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import controller.LottoController;
 
 public class Application {
+
     public static void main(String[] args) {
         LottoController controller = new LottoController();
         controller.run();
