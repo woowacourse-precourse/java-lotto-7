e9f51fa31029feea8665db3ff602329695dec9c3
kkonii <119216472+kkonii@users.noreply.github.com>
2024-11-03 23:57:32 +0900

    test(lottoFixture) : 반복해서 사용하는 테스트 객체 분리

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -3,58 +3,42 @@ package lotto.domain.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import java.util.stream.Stream;
 import lotto.exception.message.Error;
+import lotto.fixture.LottoFixture;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
-    private static Stream<List<LottoNumber>> createOverSizeLotto() {
-        return Stream.of(
-                List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6), new LottoNumber(7))
-        );
-    }
+    private List<LottoNumber> duplicatedLotto = LottoFixture.createDuplicatedLotto();
+    private List<LottoNumber> overSizeLotto = LottoFixture.createOverSizeLotto();
 
-    private static Stream<List<LottoNumber>> createDuplicatedLotto() {
-        return Stream.of(
-                List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(5))
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("createOverSizeLotto")
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다(List<LottoNumber> numbers) {
-        assertThatThrownBy(() -> new Lotto(numbers))
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(overSizeLotto))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest
-    @MethodSource("createOverSizeLotto")
+    @Test
     @DisplayName("로또 번호의 갯수가 맞지 않으면, 해당 문구를 출력한다")
-    void throw_when_lotto_size_is_incorrect(List<LottoNumber> numbers) {
-        assertThatThrownBy(() -> new Lotto(numbers))
+    void throw_when_lotto_size_is_incorrect() {
+        assertThatThrownBy(() -> new Lotto(overSizeLotto))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Error.NOT_FIX_SIZE.formatMessageOf(6));
     }
 
-    @ParameterizedTest
-    @MethodSource("createOverSizeLotto")
+    @Test
     @DisplayName("발생하는 예외 문구에는 캡션이 포함되어 있다")
-    void message_has_caption(List<LottoNumber> numbers) {
-        assertThatThrownBy(() -> new Lotto(numbers))
+    void message_has_caption() {
+        assertThatThrownBy(() -> new Lotto(overSizeLotto))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Error.CAPTION.getMessage());
     }
 
-    @ParameterizedTest
-    @MethodSource("createDuplicatedLotto")
+    @Test
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다(List<LottoNumber> numbers) {
-        assertThatThrownBy(() -> new Lotto(numbers))
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(duplicatedLotto))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -3,25 +3,17 @@ package lotto.service;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 import lotto.domain.model.Lotto;
 import lotto.domain.model.LottoNumber;
+import lotto.fixture.LottoFixture;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoServiceTest {
 
+    private List<LottoNumber> testLotto = LottoFixture.createTestLotto();
     private LottoService lottoService = new LottoService();
-
-    private static Stream<List<LottoNumber>> createTestLotto() {
-        return Stream.of(
-                List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))
-        );
-    }
 
     @Test
     @DisplayName("구매한 갯수만큼의 랜덤 로또를 생성한다")
@@ -32,12 +24,11 @@ public class LottoServiceTest {
         Assertions.assertEquals(testLottos.size(), ticket);
     }
 
-    @ParameterizedTest
-    @MethodSource("createTestLotto")
+    @Test
     @DisplayName("입력한 당첨 번호로 로또를 생성한다")
-    void make_lotto_with_winning_input(List<LottoNumber> numbers) {
+    void make_lotto_with_winning_input() {
         String winningInputs = "1,2,3,4,5,6";
-        Lotto comparsionWinningLotto = new Lotto(numbers);
+        Lotto comparsionWinningLotto = new Lotto(testLotto);
         Iterator<Integer> iterator = comparsionWinningLotto.getNumbers().iterator();
 
         Lotto winningLotto = lottoService.createWinningLottoNumbers(winningInputs);
