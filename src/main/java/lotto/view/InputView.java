27f3817b2047c956a1ee102b0212e219ea2a04cd
dongkyunKim <kyun010713@gmail.com>
2024-11-03 15:28:24 +0900

    feat(view): 입력을 관리하는 클래스 추가 - 구입 급액 입력 메서드 - 당첨 번호 입력 메서드 - 보너스 번호 입력 메서드

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+
+        return Console.readLine().trim();
+    }
+
+    public String readWinningNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        return Console.readLine().trim();
+    }
+
+    public String readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        return Console.readLine().trim();
+    }
+}
