01d865e8844498f49b073641ddc66db281735e53
Yeongjae Kim <sees77770@gmail.com>
2024-11-04 00:29:31 +0900

    feat: 당첨 로또를 생성
    당첨 로또 도메인 로직을 책임지는 WinningLotto 객체를 생성한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoTicket;
+import lotto.domain.WinningLotto;
 import lotto.external.RandomNumbersGenerator;
 
 public class Application {
@@ -8,6 +11,10 @@ public class Application {
     public static void main(String[] args) {
 
         LottoTicket lottoTicket = new LottoTicket(3, new RandomNumbersGenerator());
+
+        WinningLotto winningLotto = new WinningLotto(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7
+        );
         
     }
 
---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class WinningLotto {
+
+    private final Lotto winninglotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winninglotto, int bonusNumber) {
+        this.winninglotto = winninglotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    
+}
