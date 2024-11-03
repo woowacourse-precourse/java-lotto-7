5dfc6b09f74273f3d560f2c5119a2b7811031dc8
sanikani <sanikani@naver.com>
2024-11-03 17:19:29 +0900

    feat: 입력 문자열 파싱
    - "," 로 구분되는 당첨번호를 List<Integer>로 파싱하기 위한 메서드 정의

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,9 +1,24 @@
 package lotto.util;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Parser {
+    public static final String WINNING_NUMBER_DELIMITER = ",";
+
     public static Integer parseStringToInt(String purchaseAmount) {
         try {
             return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
+
+    public static List<Integer> parseStringToList(String winningNumbers) {
+        try {
+            return Stream.of(winningNumbers.split(WINNING_NUMBER_DELIMITER))
+                .map(Integer::parseInt)
+                .toList();
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
