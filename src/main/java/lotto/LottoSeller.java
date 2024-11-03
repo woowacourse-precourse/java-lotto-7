4b0658cd18f49fa493973bc01e77e038c6573fd6
SolfE <love23869318@gmail.com>
2024-11-04 02:10:47 +0900

    feat: add buy lottos

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class LottoSeller {
+    private static int lottoPrice = 1000;
 
     public void run() {
         int pay = readPay();
-        System.out.println();
+        List<Lotto> lottos = buyLottos(pay);
     }
 
     private int readPay() {
@@ -34,8 +36,18 @@ public class LottoSeller {
         if (pay < 0) {
             throw new IllegalArgumentException("[ERROR] 로또 금액은 양수여야 합니다.");
         }
-        if (pay % 1000 != 0) {
+        if (pay % lottoPrice != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 금액은 1000원을 단위로 입력해야 합니다.");
         }
+    }
+
+    private List<Lotto> buyLottos(int pay) {
+        int lottoCount = pay / lottoPrice;
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        List<Lotto> lottos = LottoRandomGenerator.generate(lottoCount);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+        return lottos;
     }
 }
