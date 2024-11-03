32b604081a5d74f2ef79ebfe46b3e44446e8ae35
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-03 21:59:48 +0900

    refactor: 사용자 로또 번호 정렬하는 함수 수정

--- src/main/java/lotto/model/UserNumber.java
+++ src/main/java/lotto/model/UserNumber.java
@@ -1,12 +1,14 @@
 package lotto.model;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class UserNumber {
     private final List<Integer> userNumber;
 
     public UserNumber(List<Integer> userNumber) {
-        this.userNumber = userNumber;
+        List<Integer>sortUserNumber = userNumber.stream().sorted().toList();
+        this.userNumber = sortUserNumber;
     }
 
     public List<Integer> getUserNumber() {
