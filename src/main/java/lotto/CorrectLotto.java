0ed9fc16b7b6f0e2ce590cdb04b695d40687db66
mete0rfish <sungwon326@naver.com>
2024-11-03 18:43:04 +0900

    feat(domain): 등수 체크 메서드 추가
    CorrectLotto 내에서 사용자의 Lotto와 번호를 비교하여 Rank를 반환

--- src/main/java/lotto/CorrectLotto.java
+++ src/main/java/lotto/CorrectLotto.java
@@ -12,9 +12,18 @@ public class CorrectLotto {
         this.bonusNumber = bonusNumber;
     }
 
-    void validateDuplicate(Lotto lotto, int bonusNumber) {
+    private void validateDuplicate(Lotto lotto, int bonusNumber) {
         if(lotto.numbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호와 정답 번호는 중복될 수 없습니다.");
         }
+    }
+
+    public Rank match(Lotto other) {
+        int matchCount = (int) lotto.numbers().stream()
+                .filter(other.numbers()::contains)
+                .count();
+        boolean isContainBonusNumber = other.numbers().contains(bonusNumber);
+
+        return Rank.of(matchCount, isContainBonusNumber);
     }
 }
