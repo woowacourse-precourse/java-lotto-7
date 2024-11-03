6512db299ec5a3e4d072e3853c8aaceeb9b76733
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 21:12:00 +0900

    :necktie: feat : LottoRank enum클래스 추가
    - 등수 별 일치 갯수, 보너스 포함 여부, 상금을 갖도록 추가
    - 두 로또 번호와 보너스 번호를 통해 등수에 맞게 리턴하는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,21 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
 
+    public static int inputPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validateAmountInput(input);
+        return Integer.parseInt(input);
+    }
+
     public static void validateAmountInput(String money){
-        if (money == null || money.isEmpty()){
+        if (isEmptyString(money)){
             throw new IllegalArgumentException("[ERROR] 구입 금액을 입력해 주세요.");
         }
         if (!isNumeric(money)){
@@ -22,5 +34,31 @@ public class InputView {
 
     public static boolean isModZero(int money){
         return money % 1000 == 0;
+    }
+
+
+    public static void validateWinningLottoInput(String input){
+        if (isEmptyString(input)){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호를 입력해 주세요.");
+        }
+        if (!input.matches("^(\\d+,){5}\\d+$")){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표로 구분된 6개의 숫자여야 합니다.");
+        }
+
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                                        .map(String::trim)
+                                        .map(Integer::parseInt)
+                                        .collect(Collectors.toList());
+        for (int number : numbers){
+            validateNumberRange(number);
+        }
+    }
+
+    public static boolean isEmptyString(String input){
+        return input == null || input.isEmpty();
+    }
+
+    public static void validateNumberRange(int number){
+        if (number < 1 || number > 45) throw new IllegalArgumentException("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }
