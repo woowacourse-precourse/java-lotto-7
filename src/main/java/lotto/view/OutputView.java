e25af6fdf77c6f6dd1898bc2c4384d3d80ff0f68
JoonKyo <joonkyolee@gmail.com>
2024-11-04 01:49:59 +0900

    feat(OutputView): 로또의 결과 및 수익률을 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
 import static lotto.view.OutputMessage.SHOW_PURCHASE_COUNT;
+import static lotto.view.OutputMessage.SHOW_SEPARATOR_LINE;
+import static lotto.view.OutputMessage.SHOW_STATISTICS_INTRO;
+import static lotto.view.OutputMessage.SHOW_TOTAL_PROFIT;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 import lotto.domain.Money;
+import lotto.domain.Profit;
 
 public class OutputView {
     public static void printPurchasedResult(Money money, List<Lotto> lottos) {
@@ -19,6 +24,26 @@ public class OutputView {
 
     private static void printLottoNumbers(Lotto lotto) {
         System.out.println(lotto);
+    }
+
+    public static void printLottoResult(LottoResult lottoResult, Profit profit) {
+        printNewLine();
+        printLottoHead();
+        printWinningResult(lottoResult);
+        printProfit(profit);
+    }
+
+    private static void printLottoHead() {
+        System.out.println(SHOW_STATISTICS_INTRO.getMessage());
+        System.out.println(SHOW_SEPARATOR_LINE.getMessage());
+    }
+
+    private static void printWinningResult(LottoResult lottoResult) {
+        System.out.println(OutputMessage.makeResultFormat(lottoResult.calculateRankDistribution()));
+    }
+
+    private static void printProfit(Profit profit) {
+        System.out.printf(String.format(SHOW_TOTAL_PROFIT.getMessage(), profit.calculateProfit()));
     }
 
     private static void printNewLine() {
