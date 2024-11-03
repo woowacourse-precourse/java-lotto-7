93b216f6d4ecc8b0a854e5d6130bb963747cc8be
youngseo3 <wys4475@gmail.com>
2024-11-03 16:54:26 +0900

    feat(Lottos): 로또 일급 컬렉션 생성

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public int getCount() {
+        return lottos.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (Lotto lotto : lottos) {
+            result.append(lotto.toString())
+                    .append("\n");
+        }
+
+        return result.toString();
+    }
+}
