6c0655942611ea674ce6dbda151e3dd9c9c0e5f5
이희수 <hslee9234@naver.com>
2024-11-03 16:41:02 +0900

    refactor: DefaultLottoGenerator
    Randoms.pickUniqueNumbersInRange 메서드에 포함되는 값 상수 처리

--- src/main/java/lotto/domain/DefaultLottoGenerator.java
+++ src/main/java/lotto/domain/DefaultLottoGenerator.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultLottoGenerator implements LottoGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     @Override
     public List<Lotto> generateLottos(int count) {
         List<Lotto> lottos = new ArrayList<>();
@@ -15,6 +19,6 @@ public class DefaultLottoGenerator implements LottoGenerator {
     }
 
     private List<Integer> generateRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_SIZE);
     }
 }
