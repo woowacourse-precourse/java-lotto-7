1400f76e3b8c55ffa43294bc93f55ba5eae3dbde
JunHakLee03 <junhak111701@gmail.com>
2024-11-03 15:23:05 +0900

    feat: 예외 발생시 해당 단계부터 이어서 재시작하는 기능 추가

--- src/main/java/lotto/controller/LottoManager.java
+++ src/main/java/lotto/controller/LottoManager.java
@@ -19,30 +19,59 @@ public class LottoManager {
 
     public void startLotto() {
         InputView inputView = new InputView();
+        Price money = null;
 
-        int price = NumberParser.stringToInt(inputView.price()); // 구입금액 입력 받기
-        Price money = new Price(price); // 검사 후 저장
+        // 구입 금액 입력 받기
+        while (money == null) {
+            try {
+                int price = NumberParser.stringToInt(inputView.price());
+                money = new Price(price);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
-        randomLotto(money.getPrice()); // 금액에 맞게 로또 발행
+        randomLotto(money.getPrice());
 
         List<Integer> lottoNumbers = new ArrayList<>();
-        String[] winningNum = inputView.winningNumber().split(","); // 당첨번호 입력 받기
-        for (String num : winningNum) {
-            lottoNumbers.add(NumberParser.stringToInt(num)); // 정수로 변환 후 List로 전달
-        }
-        Lotto lotto = new Lotto(lottoNumbers); // 당첨번호 검사 후 저장
+        boolean validWinningNumbers = false;
 
-        int bonusNum = NumberParser.stringToInt(inputView.bonusNumber()); // 보너스 번호 입력 받기
-        BonusLotto bonusLotto = new BonusLotto(bonusNum); // 보너스번호 숫자 범위 검사 후 저장
-        bonusLotto.bonusDuplicate(bonusNum, lotto.getLottoNumbers()); // 중복 검사
+        // 당첨 번호 입력 받기
+        while (!validWinningNumbers) {
+            try {
+                String[] winningNum = inputView.winningNumber().split(",");
+                for (String num : winningNum) {
+                    lottoNumbers.add(NumberParser.stringToInt(num));
+                }
+                new Lotto(lottoNumbers);
+                validWinningNumbers = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                lottoNumbers.clear();
+            }
+        }
+
+        int bonusNum = 0;
+        boolean validBonusNumber = false;
+
+        // 보너스 번호 입력 받기
+        while (!validBonusNumber) {
+            try {
+                bonusNum = NumberParser.stringToInt(inputView.bonusNumber());
+                new BonusLotto(bonusNum).bonusDuplicate(bonusNum, lottoNumbers);
+                validBonusNumber = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
         ResultJudging resultJudging = new ResultJudging();
         PurchasedLotto purchasedLotto = PurchasedLotto.getInstance();
 
         Result result = resultJudging.lottoResult(
                 purchasedLotto.getPurchasedLotto(),
-                lotto.getLottoNumbers(),
-                bonusLotto.getBonusNumber()
+                lottoNumbers,
+                bonusNum
         );
 
         OutputView outputView = new OutputView();
