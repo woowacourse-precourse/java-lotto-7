1cbe44eced2d4c44eaeb3ba738b86f3366be983d
kyunghoon01 <sports0124@naver.com>
2024-11-04 00:19:09 +0900

    refactor: 패키지명 수정

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,4 +1,4 @@
-package Service;
+package service;
 
 import static java.util.stream.Stream.generate;
 
--- src/test/java/service/LottoServiceTest.java
+++ src/test/java/service/LottoServiceTest.java
@@ -1,4 +1,4 @@
-package Service;
+package service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
