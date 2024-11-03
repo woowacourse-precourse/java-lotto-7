f2a079475720570d421c5d733ff2643a7e71be8d
JeongHyeon Jo <cisxo@naver.com>
2024-11-03 20:09:28 +0900

    feat(controller): LottoController Check WinLottoNum && Result Calculate Prize logic
    - 구매자가 구매한 상품을 관리자에 넘겨 당첨 여부를 확인하고 유저에게 넘김
    - 유저는 관리자에게 당첨 정보를 통해 수익률과 통계를 출력 받도록 함

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,9 +3,10 @@ package lotto.controller;
 import lotto.model.Lotto;
 import lotto.model.LottoManager;
 import lotto.model.LottoMember;
-import lotto.view.ManagerInputView;
-import lotto.view.MemberInputView;
-import lotto.view.OutputView;
+import lotto.utils.constants.LottoPrize;
+import lotto.view.input.ManagerInputView;
+import lotto.view.input.MemberInputView;
+import lotto.view.output.OutputView;
 
 import java.util.List;
 
@@ -22,36 +23,56 @@ public class LottoController {
     }
 
     public void run() {
-        //구매자가 상품을 주문
-        outputView.printGetPrice();
-        Integer price = memberInputView.getPrice();
+        Integer price = getPriceFromUser();
         LottoManager lottoManager = new LottoManager(price);
-
-        //구매자가 상품을 사고 관리자가 상품을 구매자에게 넘겨줌
         Integer purchasedLottoTotal = lottoManager.purchaseLotto();
+        LottoMember lottoMember = createLottoMember(lottoManager);
 
+        displayPurchaseInformation(purchasedLottoTotal, lottoMember);
+
+        List<Integer> lottoNumbers = getWinningNumbersFromManager();
+        Integer bonusNumber = getBonusNumberFromManager();
+
+        List<LottoPrize> resultPrize = calculateLottoResults(lottoManager, lottoMember, lottoNumbers, bonusNumber);
+        displayResults(resultPrize, lottoManager, lottoMember);
+    }
+
+    private Integer getPriceFromUser() {
+        outputView.printGetPrice();
+        return memberInputView.getPrice();
+    }
+
+    private LottoMember createLottoMember(LottoManager lottoManager) {
         LottoMember lottoMember = new LottoMember();
         for (Lotto lotto : lottoManager.getPurchasedLotto()) {
-            lottoMember.setPurchasedLotto(lotto); // Lotto 객체를 추가
+            lottoMember.setPurchasedLotto(lotto);
         }
+        return lottoMember;
+    }
 
+    private void displayPurchaseInformation(Integer purchasedLottoTotal, LottoMember lottoMember) {
         outputView.printPurchasedLottoCount(purchasedLottoTotal);
         outputView.printLottoPurchaseList(lottoMember.getPurchasedLotto());
-
-        //관리자는 로또 번호를 생성함(로또 당첨 정기일이라 생각)
-        outputView.printLottoNumber();
-        List<Integer> lottoNumbers = managerInputView.getLottoNumbers();
-        outputView.printLottoBonusNumber();
-        Integer bonusNumber = managerInputView.getLottoBonusNumber();
-
-        //구매자가 구매한 상품을 관리자에게 넘겨 당첨 여부와 수익률에 대해서 출력을 요청
-        //관리자는 구매자가 구매한 상품과 생성된 로또 번호를 통해 당첨 수익률을 계산하고 당첨 통계를 구매자에게 응답.
-        lottoManager.isLottoResult(lottoNumbers, bonusNumber);
-
-        // 로또 몇등 당첨되었는지의 정보를 저장
-        lottoMember.setLottoResult();
-
-        //구매자에서 요청 내역 출력
-
     }
+
+    private List<Integer> getWinningNumbersFromManager() {
+        outputView.printLottoNumber();
+        return managerInputView.getLottoNumbers();
+    }
+
+    private Integer getBonusNumberFromManager() {
+        outputView.printLottoBonusNumber();
+        return managerInputView.getLottoBonusNumber();
+    }
+
+    private List<LottoPrize> calculateLottoResults(LottoManager lottoManager, LottoMember lottoMember, List<Integer> lottoNumbers, Integer bonusNumber) {
+        return lottoManager.isLottoResult(lottoNumbers, bonusNumber, lottoMember.getPurchasedLotto());
+    }
+
+    private void displayResults(List<LottoPrize> resultPrize, LottoManager lottoManager, LottoMember lottoMember) {
+        lottoMember.setLottoResult(resultPrize);
+        double resultProfitRate = lottoManager.resultProfitRate(lottoMember.getLottoResult());
+        outputView.resultStatistics(resultPrize, resultProfitRate);
+    }
+
 }
