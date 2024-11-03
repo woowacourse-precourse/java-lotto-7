94c091442bedda16fefee4b0d179f04b873a01b5
ppparkta <pppppark2@gmail.com>
2024-11-03 18:25:50 +0900

    feat(InputView): 사용자 입력 메서드 추가

--- src/main/java/lotto/presentation/view/InputView.java
+++ src/main/java/lotto/presentation/view/InputView.java
@@ -1,34 +1,64 @@
 package lotto.presentation.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-    // todo: input 메서드 반복입력 처리 받기
-//        boolean validInput = false;
-//        while (!validInput) {
-//            try {
-//                inputMethod();
-//            } catch (NumberFormatException e) {
-//                System.out.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력하세요.");
-//            } catch (IllegalArgumentException e) {
-//                System.err.println(e.getMessage());
-//            }
-//        }
     public int getValidPurchaseAmount() {
-        // todo: 구입금액 입력
-        // todo: 구입금액 유효성검사
-        return 0;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String inputPurchaseAmount = Console.readLine().replace(" ", "");
+                validatePurchaseAmount(inputPurchaseAmount);
+                return Integer.parseInt(inputPurchaseAmount);
+            } catch (NumberFormatException e) {
+                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validatePurchaseAmount(String inputPurchaseAmount) {
     }
 
     public List<Integer> getValidWinningNumbers() {
-        // todo: 당첨번호 입력
-        // todo: 당첨번호 유효성 검사
-        return null;
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String inputWinningNumbers = Console.readLine().replace(" ", "");
+                String[] splitInputWinningNumbers = inputWinningNumbers.split(",");
+                validateWinningNumbers(splitInputWinningNumbers);
+                return Arrays.stream(splitInputWinningNumbers).map(winningNumber ->
+                        Integer.parseInt(winningNumber)
+                ).toList();
+            } catch (NumberFormatException e) {
+                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateWinningNumbers(String[] splitInputWinningNumbers) {
     }
 
     public int getValidBonusNumber() {
-        // todo: 보너스번호 입력
-        // todo: 보너스번호 유효성 검사
-        return 0;
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String inputBonusNumber = Console.readLine().replace(" ", "");
+                validateBonusNumber(inputBonusNumber);
+                return Integer.parseInt(inputBonusNumber);
+            } catch (NumberFormatException e) {
+                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateBonusNumber(String inputBonusNumber) {
     }
 }
