12ac573306dee2f3ca01813e9a8e447e3602eafe
minzzun99 <skg06123@naver.com>
2024-11-03 19:32:16 +0900

    test(StringProcessorTest): 테스트 클래스 이름 수정 및 기능 추가에 따른 테스트 코드 추가
    - StringProcessTest -> StringProcessorTest 로 클래스명 수정
    - 문자열 배열 정수형 List 변환 테스트 추가
    - 문자열 구분 테스트 추가

--- src/test/java/lotto/StringProcessorTest.java
+++ src/test/java/lotto/StringProcessorTest.java
@@ -1,20 +1,23 @@
 package lotto;
 
 import static lotto.util.StringProcessor.integerConverter;
+import static lotto.util.StringProcessor.integerListConverter;
 import static lotto.util.StringProcessor.removeCommas;
+import static lotto.util.StringProcessor.splitInputString;
 import static lotto.util.StringProcessor.validateInput;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class StringProcessTest {
+public class StringProcessorTest {
     @Test
     @DisplayName("콤마_제거를_확인하는_테스트")
     void 콤마_제거를_확인하는_테스트() {
@@ -49,5 +52,24 @@ public class StringProcessTest {
     void 문자열_정수_변환_테스트() {
         assertThat(integerConverter("200000"))
                 .isEqualTo(200000);
+    }
+
+    @Test
+    @DisplayName("문자열 쉼표 구분 테스트")
+    void 문자열_쉼표_구분_테스트() {
+        assertThat(splitInputString("1,2,3")).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    @DisplayName("문자열 한 개 쉼표 구분 테스트")
+    void 문자열_한_개_쉼표_구분_테스트() {
+        assertThat(splitInputString("1")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("문자열 배열 정수형 List 변환 테스트")
+    void 문자열_배열_정수형_List_변환_테스트() {
+        String[] values = {"1", "2", "3"};
+        assertThat(integerListConverter(values)).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3)));
     }
 }
