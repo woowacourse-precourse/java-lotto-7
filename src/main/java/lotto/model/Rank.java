29c1213dd8f2fd1df2744ba9f7bb638c03320a81
yuntasha <jaljayo85@gmail.com>
2024-11-03 13:10:05 +0900

    fix(Rank): add value
    Add String description

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,21 +1,27 @@
 package lotto.model;
 
 public enum Rank {
-    FIRST(2_000_000_000L),
-    SECOND(30_000_000L),
-    THIRD(1_500_000L),
-    FOURTH(50_000L),
-    FIFTH(5_000L),
-    NO_LUCK(0L),
+    FIRST(2_000_000_000L, "6개 일치 (2,000,000,000원)"),
+    SECOND(30_000_000L, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(1_500_000L, "5개 일치 (1,500,000원)"),
+    FOURTH(50_000L, "4개 일치 (50,000원)"),
+    FIFTH(5_000L, "3개 일치 (5,000원)"),
+    NO_LUCK(0L, ""),
     ;
 
     private long price;
+    private String description;
 
-    Rank(long price) {
+    Rank(long price, String description) {
         this.price = price;
+        this.description = description;
     }
 
     public long getPrice() {
         return price;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
