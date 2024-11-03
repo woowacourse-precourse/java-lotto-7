616081357274d8459042d7060c17ede34c285c4e
srcho01 <seorim5306@gmail.com>
2024-11-04 00:50:34 +0900

    feat: 당첨 번호 및 보너스 번호 예외 추가
    - 당첨 번호 중 1 ~ 45 범위를 초과하는 번호가 있는 경우 에러
    - 보너스 번호가 당첨 번호와 중복되면 에러

--- src/main/java/lotto/domain/BallNumber.java
+++ src/main/java/lotto/domain/BallNumber.java
@@ -10,25 +10,51 @@ public record BallNumber(List<Integer> winningNumbers, int bonusNumber) {
 
     public BallNumber {
         validateWinningNumbers(winningNumbers);
-        validateBonusNumbers((bonusNumber));
+        validateBonusNumber(bonusNumber, winningNumbers);
     }
 
     private void validateWinningNumbers(List<Integer> winningNumbers) {
-        // 당첨 번호 숫자가 6개인지 검사
+        winningNumbersCountCheck(winningNumbers);
+        winningNumbersRangeCheck(winningNumbers);
+        winningNumbersDuplicateCheck(winningNumbers);
+    }
+
+    private void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        numberRangeCheck(bonusNumber, ErrorCode.INVALID_BONUS_NUMBER);
+        bonusNumberDuplicateCheck(bonusNumber, winningNumbers);
+    }
+
+    private void winningNumbersCountCheck(List<Integer> winningNumbers) {
         if (winningNumbers.size() != 6) {
             throw ErrorCode.INVALID_WINNING_NUMBER_COUNT.exception();
         }
+    }
 
-        // 당첨 번호 중 중복이 없는지 검사
+    private void winningNumbersDuplicateCheck(List<Integer> winningNumbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(winningNumbers);
         if (winningNumbers.size() > nonDuplicateNumbers.size()) {
             throw ErrorCode.DUPLICATE_WINNING_NUMBER.exception();
         }
     }
 
-    private void validateBonusNumbers(int bonusNumber) {
-        if (bonusNumber <= 0 || bonusNumber > 45) {
-            throw ErrorCode.INVALID_BONUS_NUMBER.exception();
+    private void winningNumbersRangeCheck(List<Integer> winningNumbers) {
+        for (Integer number : winningNumbers) {
+            numberRangeCheck(number, ErrorCode.INVALID_WINNING_NUMBER);
         }
     }
+
+    private void numberRangeCheck(int number, ErrorCode errorCode) {
+        if (number <= 0 || number > 45) {
+            throw errorCode.exception();
+        }
+    }
+
+    private void bonusNumberDuplicateCheck(int bonusNumber, List<Integer> winningNumbers) {
+        for (Integer number : winningNumbers) {
+            if (number == bonusNumber) {
+                throw ErrorCode.DUPLICATE_BONUS_NUMBER.exception();
+            }
+        }
+    }
+
 }
