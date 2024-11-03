5a6b75c8b6dc1458238066463db3d7371404bc5e
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 19:14:51 +0900

    feat(GameManager): 구입 금액을 입력 받아 로또를 판매하는 기능

---
+++ src/main/java/lotto/manager/GameManager.java
@@ -0,0 +1,41 @@
+package lotto.manager;
+
+import lotto.domain.Player;
+import lotto.domain.Result;
+import lotto.io.Reader;
+import lotto.io.Writer;
+import lotto.util.LottoNumberSupplier;
+
+import java.util.List;
+
+/*
+* 게임 흐름을 알고 게임을 진행하는 클래스
+* */
+public class GameManager {
+    private final LottoNumberSupplier lottoNumberSupplier;
+    private final LottoSeller lottoSeller;
+    private final GameResultManager gameResultManager;
+
+    public GameManager(LottoNumberSupplier lottoNumberSupplier) {
+        this.lottoNumberSupplier = lottoNumberSupplier;
+        this.lottoSeller = new LottoSeller(lottoNumberSupplier);
+        this.gameResultManager = new GameResultManager();
+    }
+
+    private Player sellLottoToPlayer() {
+        Player player;
+
+        while (true) {
+            try {
+                int money = Reader.readMoney();
+                player = lottoSeller.sell(money);
+                break;
+            }
+            catch (IllegalArgumentException e) {
+                Writer.writeMessage(e.getMessage());
+            }
+        }
+
+        return player;
+    }
+}
