705a42544ab3b9ee372dcd95f7f37fcf6a9c18bd
JS <zks145@naver.com>
2024-11-03 14:47:25 +0900

    feat : 수익율 및 당첨 통계 값 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,13 @@ package lotto.view;
 
 import lotto.domain.Consumer;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+
+import java.util.HashMap;
 
 public class OutputView {
+
+    private final static String LOTTO_RESULT = "당첨 통계\n ---";
 
     public static void ErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
@@ -17,6 +22,36 @@ public class OutputView {
         for (Lotto lotto : lottoes.getLottoes()) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void printLottoMessage() {
+        System.out.println(LOTTO_RESULT);
+    }
+
+    public static void printLottoRank(HashMap<LottoRank, Integer> lottoRank) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.getCount() >= 3) {
+                printRank(lottoRank, rank);
+            }
+        }
+    }
+
+    private static void printRank(HashMap<LottoRank, Integer> lottoRank, LottoRank rank) {
+        String bonusMessage = "";
+
+        if (rank.getBonus()) {
+            bonusMessage = ", 보너스 볼 일치";
+        }
+
+        System.out.printf("%d개 일치%s (%s원) - %d개%n",
+                rank.getCount(),
+                bonusMessage,
+                rank.getStringPrice(),
+                lottoRank.get(rank));
+    }
+
+    public static void printTotalYield(double yield) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
     }
 
 }
