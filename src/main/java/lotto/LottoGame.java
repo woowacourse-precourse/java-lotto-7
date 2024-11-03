8b03aae99812b16254714e25daa1aea89e03a54f
mikekks <thdalsrb79@naver.com>
2024-11-03 14:27:33 +0900

    feat(LottoGame): 로또 게임 시작하는 기능 구현

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class LottoGame {
+
+	public void start(){
+		// money 입력
+
+		// 로또 구매 -> List<Lotto> 반환
+
+		// 당첨 로또 입력 및 생성
+
+		// 보너스 번호 입력 및 생성
+
+		// 결과 계산 및 출력
+
+		// 수익률 계산 및 출력
+	}
+}
