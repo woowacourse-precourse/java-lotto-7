ed0b8bde7c0f0d3d3463abdf1f48c4169566f05b
Lee-KwanBeom <dmtmqhdkk@naver.com>
2024-11-03 17:57:08 +0900

    feat : 구매 금액 입력 및 로또 수량 계산
    - 숫자 변환 유효성 검사
    - 구매금액 유효성 검사
    - 잘못된 값 입력시 재입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1 . 구매 금액 입력 및 로또 수량 계산
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchasePrice = getValidPurchasePrice();
+        int lottoQuantity = purchasePrice / 1000;
+
+
+    }
+
+    private static int getValidPurchasePrice() {
+        while (true) {
+            try {
+                int purchasePrice = getInt(Console.readLine());
+                if (purchasePrice < 1000 || purchasePrice % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구매 금액은 1000이상, 1000원 단위여야 합니다.");
+                }
+                return purchasePrice;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("구입금액을 입력해 주세요.");
+            }
+        }
+    }
+    
+    private static int getInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요");
+        }
     }
 }
