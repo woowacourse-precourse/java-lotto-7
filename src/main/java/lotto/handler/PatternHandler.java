0684c7322368c922d9a8eeeddb4a6558b766e40a
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:35:17 +0900

    :sparkles: feat(PatternHandler): 입력 검증을 위한 정규식 패턴 제공

--- src/main/java/lotto/handler/PatternHandler.java
+++ src/main/java/lotto/handler/PatternHandler.java
@@ -1,4 +1,9 @@
 package lotto.handler;
 
+import java.util.regex.Pattern;
+
 public class PatternHandler {
+    public static final Pattern MONEY_PATTERN = Pattern.compile("\\d+");
+    public static final Pattern WINNING_NUMBERS_PATTERN = Pattern.compile("^\\s*\\d+\\s*(,\\s*\\d+\\s*){5}$");
+    public static final Pattern BONUS_NUMBER_PATTERN = Pattern.compile("\\d+");
 }
