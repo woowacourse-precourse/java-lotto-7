9eefa8ebeaf3536432fa6e3b08fdf7f949f2b405
kaswhy <sxshx818@naver.com>
2024-11-04 01:57:23 +0900

    feat(InputView): 당첨 번호 입력받는 메서드 구현
    변경 후
    - Console.readline()을 반환하여 해당 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String getAmount(){
+    public String getAmount() {
+        return Console.readLine();
+    }
+
+    public String getWinningNumbers() {
         return Console.readLine();
     }
 }
