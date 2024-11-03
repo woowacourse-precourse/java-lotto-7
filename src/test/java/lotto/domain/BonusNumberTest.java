28f0fc312790de20a67e4497b9c52cf5ea8d6228
변우영 <likevangogh24@gmail.com>
2024-11-03 11:00:04 +0900

    test(BonusNumber): BonusNumber 객체 생성 테스트 추가

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusNumberTest {
+
+    @DisplayName("BonusNumber 객체 생성 테스트")
+    @Test
+    void createBonusNumber() {
+        BonusNumber bonusNumber = BonusNumber.from("7");
+
+        Assertions.assertThat(bonusNumber.isEqualTo(7)).isTrue();
+    }
+}
