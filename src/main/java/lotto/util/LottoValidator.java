ef4c863db6b10a4889da98550ae4f7915af91d9a
BusanGukbap <dong2857210@gmail.com>
2024-11-04 01:40:33 +0900

    feat(LottoValidator) : 메소드 추가
    validatePurchaseAmount : 구입 금액 1000원 단위인지 확인
    parsePositiveInteger : 숫자인지, 양수인지 확인

--- src/main/java/lotto/util/LottoValidator.java
+++ src/main/java/lotto/util/LottoValidator.java
@@ -1,4 +1,33 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.LottoNumber;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoValidator {
+
+    public static void validatePurchaseAmount(String input) {
+        int amount = parsePositiveInteger(input);
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+
+    private static int parsePositiveInteger(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number <= 0) {
+                throw new IllegalArgumentException("[ERROR] 양수를 입력해야 합니다.");
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+        }
+
+    }
+
 }
