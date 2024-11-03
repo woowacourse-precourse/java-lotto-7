ad003084994ee239039d968841f917bdb2ef2091
dltjdnjs <scw.dltjdnjs@gmail.com>
2024-11-03 20:33:19 +0900

    feat: 당첨 번호 및 보너스 번호 입력 기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
@@ -19,6 +20,38 @@ public class InputView {
                 return inputPrice;
             } catch (NumberFormatException e) {
                 System.out.println("[ERROR] 입력값이 유효한 숫자가 아닙니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        while (true) {
+            String input = Console.readLine();
+            String[] numbers = input.split(",");
+            try {
+                winningNumbers = validator.parseNumbers(numbers);
+                return winningNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static int getBonusNumber(List<Integer> winningNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber;
+
+        while (true) {
+            String input = Console.readLine();
+            try {
+                bonusNumber = Integer.parseInt(input);
+                validator.validateBonusNumber(bonusNumber, winningNumbers);
+                return bonusNumber;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
