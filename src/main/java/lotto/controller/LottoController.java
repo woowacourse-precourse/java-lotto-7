73bcff0f2f60864a0c00dfe4b86436aa8d6124ed
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 23:09:07 +0900

    feat(lottoController): 당첨 번호 입력 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,10 @@
 package lotto.controller;
 
 
+import java.util.List;
 import java.util.function.Supplier;
 import lotto.model.Money;
+import lotto.model.WinningNumbers;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -21,6 +23,16 @@ public class LottoController {
     private Money setMoney() {
         outputView.inputMoney();
         return new Money(inputView.inputNumber());
+    }
+
+    private WinningNumbers setWinningNumbers() {
+        outputView.inputWinningNumbers();
+        List<Integer> winningNumber = inputView.inputWinningNumbers();
+
+        outputView.inputBonusNumber();
+        int bonusNumber = inputView.inputNumber();
+
+        return new WinningNumbers(winningNumber, bonusNumber);
     }
 
     private <T> T retryIfErrorOccur(Supplier<T> supplier) {
