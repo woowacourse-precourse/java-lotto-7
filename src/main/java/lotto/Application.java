4d10633c6c4178717d5256d8757ee0e346c75ceb
smyeongL <sangmyeonglee123@gmail.com>
2024-11-03 22:14:25 +0900

    feat(application): call controllers in main method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.controller.PurchaseAmountController;
+import lotto.dto.PurchaseAmountRequestDto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PurchaseAmountRequestDto purchaseAmountRequestDto =  PurchaseAmountController.run();
+        LottoController.run(purchaseAmountRequestDto);
     }
 }
