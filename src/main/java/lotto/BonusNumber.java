243e98ac4b3a011579454b9df1d73089b57d547b
yybmion <yyb400@naver.com>
2024-11-03 19:26:04 +0900

    feat: 보너스 숫자 기능 추가(숫자 파싱)
    숫자가 아닌 문자를 파싱할 경우 예외가 발생한다.

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -8,6 +8,11 @@ public class BonusNumber {
     }
 
     public static BonusNumber from(String input) {
-        return null;
+        try {
+            int number = Integer.parseInt(input);
+            return new BonusNumber(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
     }
 }
