3ffe4c91ed5e0b36ebe2b225875054a627d03d12
LSBsity <dltmdqls51@naver.com>
2024-11-03 14:29:09 +0900

    refactor(LottoConst): 오류 메시지 수정

--- src/main/java/lotto/common/constant/LottoConst.java
+++ src/main/java/lotto/common/constant/LottoConst.java
@@ -20,7 +20,7 @@ public class LottoConst {
     public static final String LOTTO_BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요.";
 
 
-    public static final String NOT_NUMBER_MSG = "구매하고 싶은 정확한 금액을 입력해주세요.";
+    public static final String NOT_NUMBER_MSG = "올바른 숫자를 입력해주세요.";
     public static final String CANT_DIVIDE_MSG = String.format("로또의 한 장당 가격은 %d원 입니다. %d원 단위로 입력해주세요.",
             LottoConst.LOTTO_PRICE, LottoConst.LOTTO_PRICE);
 
