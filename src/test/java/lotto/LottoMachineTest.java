a64bf4573c519c5456859c0b46c7f9a6f37ef160
johyewon <johyewon4601@gmail.com>
2024-11-04 00:58:43 +0900

    test(LottoMachineTest): 천 단위로 나누어떨어지지 않는 구매 금액 예외 발생 테스트 추가

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LottoMachineTest {
@@ -11,5 +14,12 @@ public class LottoMachineTest {
         List<Lotto> lottos = lottoMachine.issueLottos(5000);
 
         assertEquals(5, lottos.size());
+    }
+
+    @DisplayName("로또 구매 금액이 천단위로 나누어떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void 로또_구매_금액이_천단위로_나누어떨어지지_않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new LottoMachine().issueLottos(2500))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
