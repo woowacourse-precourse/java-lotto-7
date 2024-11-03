e6e57d5f67e434bd5787de6d87a76469eea86894
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:29:43 +0900

    feat (Lotto) : 중복 체크 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,14 @@
 package lotto.domain;
 
+import static lotto.utils.Constant.LOTTO_NUMBER_MAX;
+import static lotto.utils.Constant.LOTTO_NUMBER_MIN;
+import static lotto.utils.Constant.LOTTO_SIZE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringJoiner;
 
 /**
@@ -17,6 +25,16 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto create() {
+        List<Integer> randoms = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_SIZE);
+        Collections.sort(randoms);
+        return new Lotto(randoms);
+    }
+
+    public boolean containBonusNumber(BonusNumber bonusNumber) {
+        return numbers.contains(bonusNumber.getBonusNumber());
+    }
+
     public String result() {
         StringJoiner stringJoiner = new StringJoiner(", ","[","]");
         numbers.forEach(
@@ -25,9 +43,37 @@ public class Lotto {
         return stringJoiner.toString();
     }
 
+    public Integer getElement(int index) {
+        if(index < 0 || index >= LOTTO_SIZE) {
+            throw new IndexOutOfBoundsException();
+        }
+        return numbers.get(index);
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateNumbersInRange(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumbersInRange(List<Integer> numbers) {
+        if(numbers.stream().anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1 이상 45 이하 입니다");
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for(Integer number : numbers) {
+            if(!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 중복 되지 않은 로또 번호를 입력하세요");
+            }
         }
     }
 }
