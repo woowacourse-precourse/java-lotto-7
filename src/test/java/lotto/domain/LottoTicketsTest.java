9423e452a3e958ab86112eecdb8e7df1914059ae
qyinm <qusseun@gmail.com>
2024-11-03 14:41:11 +0900

    test: 로또 당첨 결과 반환 Map 결과 확인 테스트 작성

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -3,11 +3,14 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTicketsTest {
-
 
     @Test
     @DisplayName("발행한 로또 번호 값 출력 포맷에 맞게 반환 테스트")
@@ -30,4 +33,63 @@ class LottoTicketsTest {
                 [13, 14, 15, 16, 17, 18]""";
         assertThat(result).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @MethodSource("lottoPrizeDataProvider")
+    @DisplayName("로또 당첨 결과를 확인 테스트")
+    void testCheckLottoPrizesMap(WinningLotto winningLotto,
+                               LottoTickets lottoTickets,
+                               Map<LottoPrize, Integer> expectedResults) {
+        // Given & When
+        Map<LottoPrize, Integer> lottoPrizesMap = lottoTickets.getLottoPrizesMap(winningLotto);
+
+        // Then
+        assertThat(lottoPrizesMap).containsExactlyInAnyOrderEntriesOf(expectedResults);
+    }
+
+    static Stream<Object[]> lottoPrizeDataProvider() {
+        return Stream.of(
+                new Object[]{
+                    new WinningLotto(
+                            new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 당첨 번호
+                            10)
+                    ,
+                    new LottoTickets(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                            new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 3등
+                            new Lotto(List.of(1, 2, 3, 4, 8, 9)), // 4등
+                            new Lotto(List.of(1, 2, 3, 10, 11, 12)), // 5등
+                            new Lotto(List.of(1, 2, 13, 14, 15, 16)), // 꽝
+                            new Lotto(List.of(17, 18, 19, 20, 21, 22)) // 꽝
+                    )),
+                    Map.of(LottoPrize.FIRST_PRIZE, 1,
+                            LottoPrize.SECOND_PRIZE, 0,
+                            LottoPrize.THIRD_PRIZE, 1,
+                            LottoPrize.FOURTH_PRIZE, 1,
+                            LottoPrize.FIFTH_PRIZE, 1,
+                            LottoPrize.NO_PRIZE, 2)
+                },
+                new Object[]{
+                        new WinningLotto(
+                                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                                10),
+                        new LottoTickets(List.of(
+                                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등 (보너스 포함)
+                                new Lotto(List.of(1, 2, 3, 4, 8, 9)), // 3등
+                                new Lotto(List.of(1, 2, 3, 10, 11, 12)), // 4등
+                                new Lotto(List.of(1, 2, 13, 14, 15, 16)), // 5등
+                                new Lotto(List.of(17, 18, 19, 20, 21, 22)) // 꽝
+                        )),
+                        Map.of(
+                                LottoPrize.FIRST_PRIZE, 1,
+                                LottoPrize.SECOND_PRIZE, 0,
+                                LottoPrize.THIRD_PRIZE, 1,
+                                LottoPrize.FOURTH_PRIZE, 1,
+                                LottoPrize.FIFTH_PRIZE, 1,
+                                LottoPrize.NO_PRIZE, 2
+                        )
+                }
+        );
+    }
+
 }
