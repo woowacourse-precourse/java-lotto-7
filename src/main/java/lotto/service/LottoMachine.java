c9161e4f6c1af50eea135d779041fb75b2dd91a7
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-03 23:29:23 +0900

    refactor: 중복 로직 제거

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -12,21 +12,11 @@ import static camp.nextstep.edu.missionutils.Randoms.*;
 public class LottoMachine {
 
     public void buyLotto(Long money) {
-        validMoney(money);
         Long lottoCount = money / Value.lottoPrice;
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> lottoNumber = pickUniqueNumbersInRange(Value.lottoStartNumber, Value.lottoEndNumber, Value.lottoNumberCount);
             Database.purchaseLottoList.add(new Lotto(lottoNumber));
             System.out.println(lottoNumber);
-        }
-    }
-
-    private void validMoney(Long money) {
-        if (money < Value.lottoPrice) {
-            throw new IllegalArgumentException(ErrorCode.LOTTO_MIN_PRICE_ERROR.getMessage());
-        }
-        if (money % Value.lottoPrice != 0) {
-            throw new IllegalArgumentException(ErrorCode.LOTTO_PRICE_ERROR.getMessage());
         }
     }
 
