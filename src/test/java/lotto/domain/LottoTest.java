a5a007b50f11aec8ffdcae370c267ac2e46083b2
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 23:34:41 +0900

    test : Lotto 객체 생성 시 중복된 숫자가 있는 경우 예외 발생 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -13,4 +13,13 @@ class LottoTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(invalidNumbers));
         assertEquals("[ERROR] 로또 번호는 6개여야 합니다.", exception.getMessage());
     }
+
+    @Test
+    @DisplayName("Lotto 객체 생성 시 중복된 숫자가 있는 경우 예외 발생")
+    void createLottoWithDuplicateNumbers() {
+        List<Integer> duplicateNumbers = List.of(1, 2, 3, 4, 5, 5);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Lotto(duplicateNumbers));
+        assertEquals("[ERROR] 로또 번호에 중복된 값이 있습니다.", exception.getMessage());
+    }
+
 }
