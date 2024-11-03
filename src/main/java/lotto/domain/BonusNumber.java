e59f72fa0af1086a8125d27102316c95c8ff6a43
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-04 04:15:58 +0900

    refactor(BonusNumber): 매직 넘버 상수화

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import lotto.enums.ErrorMessage;
 
 public class BonusNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int ZERO = 0;
+
     private final int number;
 
     private BonusNumber(int number) {
@@ -16,13 +20,13 @@ public class BonusNumber {
     }
 
     private void validatePositiveInteger(int number) {
-        if (number <= 0) {
+        if (number <= ZERO) {
             throw new IllegalArgumentException(ErrorMessage.NEGATIVE_BONUS_NUMBER_NOT_ALLOWED.getMessage());
         }
     }
 
     private void validateRange(int number) {
-        boolean isValid = (number >= 1 && number <= 45);
+        boolean isValid = (number >= MIN_NUMBER && number <= MAX_NUMBER);
 
         if (!isValid) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER_RANGE.getMessage());
