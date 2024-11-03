8f8117bf4900fcc8a6584f4a677685db7fae33c5
koosco <rnxogud123@naver.com>
2024-11-04 04:05:33 +0900

    fix: 잘못된 검증 로직 삭제
    PositiveNumber에 포함된 Lotto 검증 로직 삭제

--- src/main/java/lotto/domain/PositiveNumber.java
+++ src/main/java/lotto/domain/PositiveNumber.java
@@ -5,18 +5,11 @@ public class PositiveNumber {
     private final Long number;
 
     public PositiveNumber(Long number) {
-        validate(number);
         this.number = number;
     }
 
     public PositiveNumber(String number) {
         this(Long.parseLong(number));
-    }
-
-    private static void validate(Long number) {
-        if (number > 45 || number < 1) {
-            throw new IllegalArgumentException("숫자는 1 ~ 45 사이여야 합니다");
-        }
     }
 
     public PositiveNumber divide(Long number) {
