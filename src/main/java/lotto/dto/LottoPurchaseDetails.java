ead54fe576c2f8652c15338b4a1ce48426b88638
SukJin Kim <rkathrwks@gmail.com>
2024-11-03 23:04:36 +0900

    refactor(view): dto를 통해 출력에 필요한 정보만 받도록 수정

---
+++ src/main/java/lotto/dto/LottoPurchaseDetails.java
@@ -0,0 +1,29 @@
+package lotto.dto;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoGroup;
+
+public class LottoPurchaseDetails {
+    private static final String PURCHASED_NUM_LOTTO_MESSAGE = "%d개를 구매했습니다.";
+    private static final String NEW_LINE = "\n";
+
+    private final List<Lotto> lottos;
+    private final int numLotto;
+
+    public LottoPurchaseDetails(LottoGroup lottoGroup) {
+        lottos = lottoGroup.getLottos();
+        numLotto = lottoGroup.getLottos().size();
+    }
+
+    public String getPurchaseDetailsMessage() {
+        StringBuilder purchaseDetailsMessage = new StringBuilder();
+
+        purchaseDetailsMessage.append(String.format(PURCHASED_NUM_LOTTO_MESSAGE, numLotto)).append(NEW_LINE);
+        for (Lotto lotto : lottos) {
+            purchaseDetailsMessage.append(lotto.getNumbers()).append(NEW_LINE);
+        }
+
+        return purchaseDetailsMessage.toString();
+    }
+}
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.dto.LottoPurchaseDetails;
+
 public class OutputView {
     private static final String PURCHASE_AMOUNT_REQUEST = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
@@ -9,9 +11,9 @@ public class OutputView {
         printMessage(PURCHASE_AMOUNT_REQUEST);
     }
 
-    public void printPurchaseDetailsMessage(String purchaseDetailsMessage) {
+    public void printPurchaseDetailsMessage(LottoPurchaseDetails lottoPurchaseDetails) {
         printNewLine();
-        printMessage(purchaseDetailsMessage);
+        printMessage(lottoPurchaseDetails.getPurchaseDetailsMessage());
     }
 
     public void printExitMessage(String message) {
