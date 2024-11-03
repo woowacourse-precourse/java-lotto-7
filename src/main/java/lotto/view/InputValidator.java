97551f00989bb34fbef2d73cf8334d371aa690e9
taek2222 <oot309@daum.net>
2024-11-04 04:37:57 +0900

    feat(InputValidator): 입력 검증 분리
    - 가독성 향상을 위해, 입력 검증 로직을 분리했습니다.
    
    - 입력 클래스에서 사용할 수 있도록 protected 접근 제어자를 사용했습니다.

---
+++ src/main/java/lotto/view/InputValidator.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import static lotto.constant.ErrorMessage.INVALID_NON_NUMERIC_INPUT;
+import static lotto.constant.ErrorMessage.INVALID_NULL_OR_BLANK;
+
+import java.util.Arrays;
+
+public class InputValidator {
+
+    private static final String COMMA = ",";
+
+    protected static void validateNotNullOrBlank(String input) {
+        if (input == null || input.isBlank())
+            throw new IllegalArgumentException(INVALID_NULL_OR_BLANK.getMessage());
+    }
+
+    protected static void validateIsNumeric(String input) {
+        String[] numbers = splitInput(input);
+        Arrays.stream(numbers)
+                .forEach(InputValidator::validateNumberIsNumeric);
+    }
+
+    private static String[] splitInput(String input) {
+        return input.split(COMMA);
+    }
+
+    private static void validateNumberIsNumeric(String number) {
+        if (!number.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(INVALID_NON_NUMERIC_INPUT.getMessage());
+        }
+    }
+}
