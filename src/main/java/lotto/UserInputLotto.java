f71f54f114849b895bf0377e91b18675c9998870
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 04:28:34 +0900

    refactor: purchasAmount() 예외시 입력값 다시 받기 수정

--- src/main/java/lotto/UserInputLotto.java
+++ src/main/java/lotto/UserInputLotto.java
@@ -7,10 +7,17 @@ import java.util.List;
 
 public class UserInputLotto {   // 사용자가 입력하는 값에 대한 클래스
     public int purchaseAmount() {   // 사용자가 얼마어치 사는지 입력
-        System.out.println("얼마어치를 구매하시겠습니까? (1,000원 단위)");
-        int amount = Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                int amount = purchaseAmountRead();
+                validatePurchaseAmount(amount);
 
-        return amount;
+                return amount;
+            } catch
+                (IllegalArgumentException e) {
+                printErrorMessage(e.getMessage());
+            }
+        }
     }
 
     public List<Integer> inputPrizeNumbers() {      // 사용자가 당첨 번호를 입력
