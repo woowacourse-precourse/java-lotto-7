a81ae222d04add4e99e080c9a9b9062de6e271c6
YongLeeCode <yonghwan311@gmail.com>
2024-11-03 23:14:03 +0900

    feat: 출력 순서 변경

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,17 +1,16 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Output {
+
     private final Map<MatchCondition, Integer> prizeCounts = new LinkedHashMap<>();
 
     public Output() {
-        // 상금 조건별 초기 카운트 설정
-        for (MatchCondition condition : TicketPrizeMatcher.matchConditions) {
-            prizeCounts.put(condition, 0);
-        }
+        initializePrizeCounts();
     }
 
     public static void outputTicketNumbers(List<LottoTicket> lottoTicketBundle) {
@@ -23,6 +22,7 @@ public class Output {
 
     public void printMatchResult(MatchResult matchResult) {
         List<MatchCondition> matchConditions = matchResult.getMatchedConditions();
+
         for (MatchCondition matchCondition : matchConditions) {
             // 조건에 맞는 카운트 증가
             prizeCounts.put(matchCondition, prizeCounts.getOrDefault(matchCondition, 0) + 1);
@@ -37,5 +37,20 @@ public class Output {
             System.out.printf("%s - %d개\n", condition.prizeDescription(), count);
         }
         System.out.printf("총 수익률은 %.2f%%입니다.\n", matchResult.getProfitRatio());
+    }
+
+    private List<MatchCondition> getSortedMatchConditions(List<MatchCondition> matchConditions) {
+        return matchConditions.stream()
+                .sorted(Comparator.comparing(MatchCondition::getPrizedAmount))
+                .toList();
+    }
+
+    private void initializePrizeCounts() {
+        List<MatchCondition> sortedMatchConditions = getSortedMatchConditions(TicketPrizeMatcher.matchConditions);
+
+        // 상금 조건별 초기 카운트 설정
+        for (MatchCondition condition : sortedMatchConditions) {
+            prizeCounts.put(condition, 0);
+        }
     }
 }
