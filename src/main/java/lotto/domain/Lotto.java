8e04cd99cf86a2f16052ec0c981d8be7c56692d3
chanwon <clw8679@naver.com>
2024-11-03 21:24:37 +0900

    test: lotto 테스트 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,6 +14,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        for (int number : numbers) {
+            if (number < 0 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45숫자만 가능합니다.");
+            }
+        }
+
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
     }
 
     public int matchNumbers(List<Integer> winningNumber) {
