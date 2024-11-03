8c620cb363a72166ca861d2df0b374e880dfb373
jiyun <tomatoziiilll@gmail.com>
2024-11-03 23:42:59 +0900

    feat: 로또 구입금액 입력 받기
    -로또 금액이 올바른지 검사
    -연산 후 로또 갯수 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseInput = Console.readLine();
+
+        LottoMachine lottoMachine = new LottoMachine(purchaseInput);
     }
 }
