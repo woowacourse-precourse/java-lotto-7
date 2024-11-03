ad2a528874a281461f199b835fc9d1138178db5c
윤진완 <yunjin-wan@yunjin-wan-ui-MacBookAir.local>
2024-11-03 11:12:38 +0900

    feat: LottoService 모든 객체들 종합해서 프로그램 돌리는 클래스

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,22 +5,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            int price = Input.purchasePrice();
-            LottoShop lottoShop = new LottoShop(price);
-            lottoShop.issueLotto();
-            Output.printLotto(lottoShop.getCount(), lottoShop.getLottos());
+        LottoService lottoService = new LottoService();
+        lottoService.run();
 
-            List<Integer> winNumber = Input.winNumber();
-            int bonus = Input.inputBonus(winNumber);
-
-            LottoChecker lottoChecker = new LottoChecker();
-            lottoChecker.checkLotto(winNumber, lottoShop.getLottos(), bonus);
-
-            Output.printResult(lottoChecker.getRankCount(), lottoChecker.calculateProfit(price));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
 
