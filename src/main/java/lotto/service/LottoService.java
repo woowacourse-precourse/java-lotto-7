bf82545609c132f9e33ff43d6e2efc1ac37f08f3
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 19:30:09 +0900

    feat(LottoService): 로또 결과 계산 및 DTO 변환 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import lotto.domain.BonusNumber;
 import lotto.domain.LottoPurchase;
@@ -12,13 +13,20 @@ import lotto.dto.LottoTicketsDto;
 
 public class LottoService {
     private final LottoStore lottoStore;
+    private final LottoResults lottoResults;
 
     public LottoService() {
         this.lottoStore = new LottoStore();
+        this.lottoResults = new LottoResults();
     }
 
     public LottoTickets purchaseLottoTickets(int amount){
         LottoPurchase purchase = LottoPurchase.of(amount);
         return lottoStore.generateLottoTickets(purchase.getTicketCount());
+    }
+
+    public List<LottoResultDto> calculateResults(LottoTickets tickets , WinningNumbers winningNumbers, BonusNumber bonusNumber){
+        lottoResults.calculateResults(tickets,winningNumbers,bonusNumber);
+        return lottoResults.generateResultDtos();
     }
 }
