639a69984c5476d463ae1e5ffc704bc00100086e
seoys0910 <seoys0910@naver.com>
2024-11-03 18:15:44 +0900

    feat : 구입 금액을 입력받는 기능 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -6,5 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 public class InputView {
-
+    public static int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String value = Console.readLine();
+        return Integer.parseInt(value);
+    }
 }
