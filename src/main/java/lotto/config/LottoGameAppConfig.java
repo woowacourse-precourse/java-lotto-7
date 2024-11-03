3c00642c9ea3429d2f2cd19d35b1009a45e50df4
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:30:40 +0900

    refactor (LottoGameAppConfig) : 단순 코드 수정

--- src/main/java/lotto/config/LottoGameAppConfig.java
+++ src/main/java/lotto/config/LottoGameAppConfig.java
@@ -9,7 +9,7 @@ public class LottoGameAppConfig {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
     private static final LottoService lottoService = new LottoService();
-    private static final LottoController lottoController = new LottoController(lottoService,outputView,inputView);
+    private static final LottoController lottoController = new LottoController(lottoService,inputView,outputView);
 
     private LottoGameAppConfig() {
     }
