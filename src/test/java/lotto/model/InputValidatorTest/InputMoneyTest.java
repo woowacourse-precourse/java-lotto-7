db269e8515d6f13faa3b884a0f44743b23d354ae
corpi <lj845854@gmail.com>
2024-11-03 14:21:39 +0900

    refactor(InputMoneyTest): 불필요한 테스트 제거

--- src/test/java/lotto/model/InputValidatorTest/InputMoneyTest.java
+++ src/test/java/lotto/model/InputValidatorTest/InputMoneyTest.java
@@ -3,7 +3,6 @@ package lotto.model.InputValidatorTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.model.InputValidator;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -30,15 +29,6 @@ class InputMoneyTest {
     @ParameterizedTest
     @CsvSource(value = {"-1", "-13", "-999"})
     public void 입력된_값이_음수일_경우_에러가_발생한다(String input) {
-        assertThatThrownBy(() -> {
-            inputValidator.validateInputMoney(input);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("입력된 값이 양수이지만 단위 가격 미만일 경우 에러가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"10", "100", "999", "500"})
-    public void 입력된_값이_양수이지만_단위_가격_미만일_경우_에러가_발생한다(String input) {
         assertThatThrownBy(() -> {
             inputValidator.validateInputMoney(input);
         }).isInstanceOf(IllegalArgumentException.class);
