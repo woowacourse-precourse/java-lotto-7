2804102d67e53c758fee5766a7446bdc18983356
encoreJeong <zzaibean@gmail.com>
2024-11-03 21:47:22 +0900

    feat: 당첨번호 입력 기능 추가
    1. 당첨번호 입력 힌트 출력
    2. 입력으로 부터 당첨 번호 값 List로 파싱
    3. 당첨 번호 입력 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -10,5 +13,19 @@ public class Application {
         if(totalPrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000의 배수인 정수를 입력해야 합니다.");
         }
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = readLine();
+        String[] inputNumbers = input.split(",");
+
+        Set<Integer> numbersInput = Arrays.stream(inputNumbers)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+
+        if(numbersInput.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로 중복되지 않는 1~45 사이의 6가지 수여야 합니다.");
+        }
+
     }
 }
