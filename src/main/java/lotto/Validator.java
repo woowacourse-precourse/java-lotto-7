da5c94f863bb94003531fa259c910449dfac18dd
AwesomeMinsik <beaut3579@gmail.com>
2024-11-03 21:13:11 +0900

    refactor: 로또 구매 금액에 대한 검증 메서드를 추가하였습니다
    - 금액의 입력값에 대한 검증 메서드를 추가하고, 1000원단위가 아닐 경우 예외를 발생하도록 로직을 구성하였습니다

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,30 +1,34 @@
 package lotto;
 
+import lotto.IO.InputHandler;
+import lotto.messages.ErrorMessage;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public abstract class Validator {
-    public static void validateInput(List<Integer> winningNumber){
+    public static void validateInput(List<Integer> winningNumber) {
         Set<Integer> uniqueNumbers = new HashSet<>(winningNumber);
-        if (uniqueNumbers.size() != winningNumber.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
-        }
+        if (uniqueNumbers.size() != winningNumber.size())
+            throw new IllegalArgumentException();
     }
-    public static void validateInput(String bonusNumber,List<Integer> winningNumber){
+
+    public static void validateInput(String bonusNumber) {
         try {
             Integer.parseInt(bonusNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.", e);
+            throw new IllegalArgumentException(ErrorMessage.BONUS_NUMBER_NOT_NUMBER.getMessage());
         }
-
-        int bonus = Integer.parseInt(bonusNumber);
-        checkBonusNumberDuplication(bonus,winningNumber);
     }
 
-    private static void checkBonusNumberDuplication(int bonus,List<Integer> winningNumber) {
-        if (winningNumber.contains(bonus)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다");
-        }
+    public static int checkBonusNumberDuplication(int bonus, List<Integer> winningNumber) {
+        if (winningNumber.contains(bonus))
+            throw new IllegalArgumentException();
+        return bonus;
+    }
+    public static void validateLottoPrice(String input){
+        if ( InputHandler.convertToInt(input) % 1000 != 0)
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_PRICE.getMessage());
     }
 }
