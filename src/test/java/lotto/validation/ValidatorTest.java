821af6e57cf22f8da5e27f64dc58db4caa58b249
soohalee <suha630@naver.com>
2024-11-04 03:09:06 +0900

    Test(ValidatorTest): 문자열이 모두 숫자로 구성되있지 않으면 예외 발생 테스트 추가

--- src/test/java/lotto/validation/ValidatorTest.java
+++ src/test/java/lotto/validation/ValidatorTest.java
@@ -19,5 +19,15 @@ public class ValidatorTest {
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("문자열이 모두 숫자로 구성되있지 않으면 예외 발생 테스트")
+    void isDigitOnly() {
+        String digit = "F123";
+
+        assertThatThrownBy(() -> {
+            Validator.isDigitOnly(digit);
+        }).isInstanceOf(NumberFormatException.class);
+    }
+
     }
 }
