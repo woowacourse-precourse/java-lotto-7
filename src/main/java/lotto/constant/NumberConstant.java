db79e342482ff801a9576d9f38dcb5442c91d71e
daxxn <dahyeonhan4529@gmail.com>
2024-11-03 13:13:08 +0900

    feat(constant): add constant class
    - Implement LottoConstant class to handle common lotto constant variable
    - Implement NumberConstant class to handle common number constant variable

---
+++ src/main/java/lotto/constant/NumberConstant.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class NumberConstant {
+    public static final int ZERO = 0;
+    public static final int HUNDRED = 100;
+    public static final double HUNDRED_ONE_DECIMAL = 100.0;
+}
