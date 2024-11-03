173be1d07c7cc7a3e97679dc86449c15e88b87fd
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-04 01:13:03 +0900

    feat(Application): 수익률 출력 로직 추가
    - 수익률 출력 로직을 추가하여, 로또 구매 후 당첨 결과와 함께 수익률을 확인할 수 있도록 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,13 @@
 package lotto;
 
 import java.util.List;
-import java.util.Map;
 
 import lotto.controller.LottoNumberController;
 import lotto.controller.LottoPurchaseController;
 import lotto.controller.LottoResultChecker;
 import lotto.model.Lotto;
 import lotto.model.LottoBonus;
-import lotto.model.LottoRank;
 import lotto.model.LottoTicket;
-import lotto.view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
@@ -22,10 +19,6 @@ public class Application {
 		LottoBonus bonusNumber = lottoNumberController.generateBonusNumber(winningLotto.getNumbers());
 
 		LottoResultChecker resultChecker = new LottoResultChecker();
-		Map<LottoRank, Integer> resultCount = resultChecker.checkResults(tickets, winningLotto, bonusNumber);
-		OutputView.displayResult(resultCount);
-
-		int totalPrize = resultChecker.calculateTotalPrize(resultCount);
-		OutputView.displayTotalPrize(totalPrize);
+		resultChecker.displayResults(tickets, winningLotto, bonusNumber);
 	}
 }
