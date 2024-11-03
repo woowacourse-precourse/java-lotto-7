3f01dd8b79756c6fe1d0d56bfd532dc926b29163
GrO0vy <lmc2819@naver.com>
2024-11-03 22:05:52 +0900

    test: 테스트 이름 출력 형식 변경

--- src/test/java/lotto/PriceValidatorTest.java
+++ src/test/java/lotto/PriceValidatorTest.java
@@ -9,7 +9,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PriceValidatorTest {
     // 구입 금액 검증 기능 ( PriceValidator ) 테스트
-    @DisplayName("구입 금액에 숫자 외의 문자가 있으면 예외가 발생한다")
+
+    @DisplayName("[PriceValidatorTest] 구입 금액에 숫자 외의 문자가 있으면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"asd", ",123", "12 12432", "@$%#!", "123@$12,./"})
     void 구입_금액에_숫자_외의_문자가_있으면_예외가_발생한다(String input){
@@ -18,7 +19,7 @@ public class PriceValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("음수를 입력하면 예외가 발생한다.")
+    @DisplayName("[PriceValidatorTest] 음수를 입력하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"-1", "-1000000", "-2147483648", "-3482739"})
     void 음수를_입력하면_예외가_발생한다(String input){
@@ -26,7 +27,7 @@ public class PriceValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("정수 범위를 벗어나면 예외가 발생한다.")
+    @DisplayName("[PriceValidatorTest] 정수 범위를 벗어나면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1231241242141", "999999999999999", "2147483648"})
     void 정수_범위를_벗어나면_예외가_발생한다(String input){
@@ -34,7 +35,7 @@ public class PriceValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("천 단위 숫자가 아니면 예외가 발생한다")
+    @DisplayName("[PriceValidatorTest] 천 단위 숫자가 아니면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"10001", "25001", "2147483647", "0"})
     void 천_단위_숫자가_아니면_예외가_발생한다(String input){
