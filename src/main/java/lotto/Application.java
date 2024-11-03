d0e63dd77afbf147f756d10eee3e10b933e28448
YooSejeong <nived3@naver.com>
2024-11-04 02:51:45 +0900

    feat(InputView): 당첨 번호와 보너스 번호 입력 받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.util.List;
 import lotto.controller.LottoController;
 import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         int lottoMoney = InputView.getLottoMoney();
+        List<Integer> numbers = InputView.getWinningNumbers();
+        int bonusNumber = InputView.getBousNumber();
         LottoController controller = new LottoController(lottoMoney);
         controller.startLotto();
     }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,28 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     public static int getLottoMoney() {
         System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> getWinningNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbers = Console.readLine();
+        //TODO: 메서드 분리하기
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : winningNumbers.split(",")) {
+            numbers.add(Integer.parseInt(number.trim()));
+        }
+        return numbers;
+    }
+
+    public static int getBousNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 }
