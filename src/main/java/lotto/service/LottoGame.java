f3cc4c86cfe5a8ef94c40aa5bed4b3757529931f
joon <khjoon372@naver.com>
2024-11-04 01:07:09 +0900

    feat(LottoGame): 당첨 번호와 발행한 로또 번호 비교하는 기능 구현

--- src/main/java/lotto/service/LottoGame.java
+++ src/main/java/lotto/service/LottoGame.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
+import lotto.domain.WinningResult;
 import lotto.utils.Utils;
+import lotto.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,7 @@ public class LottoGame {
     private static final int LOTTO_PRICE = 1000;
     private final int lottoAmount;
     private final List<Lotto> userLottoList = new ArrayList<>();
+    private final List<WinningResult> winningResultList = new ArrayList<>();
 
     public LottoGame(int lottoCost){
 
@@ -30,4 +33,15 @@ public class LottoGame {
     public List<Lotto> getUserLottoList() {
         return userLottoList;
     }
+
+    private int checkMatchCount(Lotto lotto, List<Integer> winningNumbers) {
+
+        return (int) lotto.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+
+
+
 }
