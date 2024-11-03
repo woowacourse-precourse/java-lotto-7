4da317ce724c2fc37016ef5c9a4b7ac6dcaf7dad
Jin409 <sualng123@gmail.com>
2024-11-03 23:49:52 +0900

    refactor: 에러 메시지를 더 자세히 해 의도를 명확히 해라

--- src/main/java/lotto/controller/ErrorMessages.java
+++ src/main/java/lotto/controller/ErrorMessages.java
@@ -5,7 +5,7 @@ public class ErrorMessages {
     public static final String INVALID_PAID_AMOUNT_FORMAT =
             "구입금액은 0보다 큰 정수 형태여야 합니다.";
     public static final String INVALID_WINNING_NUMBERS_FORMAT =
-            "당첨 번호는 1,2,3 과 같이 쉼표(,)와 0 보다 큰 정수로 이루어져야 합니다.";
+            "당첨 번호는 띄어쓰기 없이 쉼표(,)와 0 보다 큰 정수로 이루어져야 합니다.";
 
     public static final String INVALID_BONUS_NUMBER_FORMAT = "보너스 번호는 0보다 큰 정수 형태여야 합니다.";
 }
