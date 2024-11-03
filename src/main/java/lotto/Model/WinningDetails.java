0e39d7b210b4acda1e1e08aef8bd47aaf3fb7bdb
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:45:56 +0900

    chore(WinningDetails) : 커맨드라인 자동 정렬

--- src/main/java/lotto/Model/WinningDetails.java
+++ src/main/java/lotto/Model/WinningDetails.java
@@ -10,19 +10,19 @@ public class WinningDetails {
     private Integer fifthBonus = 0;
     private Integer sixth = 0;
 
-    public void sumUpGrades(List<MyResults> myResults){
-        for(MyResults result : myResults){
-            if(result.getMatches() == 3) this.third++;
-            else if(result.getMatches() == 4) this.fourth++;
-            else if(result.getMatches() == 5 && !result.getIsBonus()) this.fifth++;
-            else if(result.getMatches() == 5 && result.getIsBonus()) this.fifthBonus++;
-            else if(result.getMatches() == 6) this.sixth++;
+    public void sumUpGrades(List<MyResults> myResults) {
+        for (MyResults result : myResults) {
+            if (result.getMatches() == 3) this.third++;
+            else if (result.getMatches() == 4) this.fourth++;
+            else if (result.getMatches() == 5 && !result.getIsBonus()) this.fifth++;
+            else if (result.getMatches() == 5 && result.getIsBonus()) this.fifthBonus++;
+            else if (result.getMatches() == 6) this.sixth++;
         }
     }
 
-    public static List<MyResults> saveMyGrades(List<Lotto> lottos, Lotto answer, int bonus){
+    public static List<MyResults> saveMyGrades(List<Lotto> lottos, Lotto answer, int bonus) {
         List<MyResults> myResults = new ArrayList<>();
-        for(int i = 0; i < lottos.size(); i++){
+        for (int i = 0; i < lottos.size(); i++) {
             MyResults gradedLotto = Lotto.gradeLotto(answer, lottos.get(i), bonus);
             myResults.add(gradedLotto);
         }
@@ -32,30 +32,39 @@ public class WinningDetails {
     public Integer getThird() {
         return third;
     }
+
     public Integer getFourth() {
         return fourth;
     }
+
     public Integer getFifth() {
         return fifth;
     }
+
     public Integer getFifthBonus() {
         return fifthBonus;
     }
+
     public Integer getSixth() {
         return sixth;
     }
+
     public void setThird(Integer third) {
         this.third = third;
     }
+
     public void setFourth(Integer fourth) {
         this.fourth = fourth;
     }
+
     public void setFifth(Integer fifth) {
         this.fifth = fifth;
     }
+
     public void setFifthBonus(Integer fifthBonus) {
         this.fifthBonus = fifthBonus;
     }
+
     public void setSixth(Integer sixth) {
         this.sixth = sixth;
     }
