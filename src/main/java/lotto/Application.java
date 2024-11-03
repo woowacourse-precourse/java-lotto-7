5a004a1ba85c93fe7373d544d86107cec8c4ad97
kwonhee <lkh8033@gmail.com>
2024-11-03 18:03:40 +0900

    fix(README.md, Application) : fix README.md and Application 구현 구조 재설립
    구현 구조 재 설립

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import controller.LottoController;
+import view.InputView;
+import view.OutputView;
+
+import java.util.ArrayList;
+
 public class Application {
+    private static LottoController controller = new LottoController();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputPurchaseStr = InputView.inputLottoPurchase();
+        int lottoPurchase = controller.getLottoPurchaseInt(inputPurchaseStr);
+
+        int lottoCount = controller.getLottoCount();
+        ArrayList<Lotto> lottos = controller.getLottos();
+
+        OutputView.printLottoNumbers(lottoCount, lottos);
+
+        String inputNumbersStr = InputView.inputLottoNumber();
+        String inputBonusNumberStr = InputView.inputBunusNumber();
+        LottoResult lottoResult = controller.getResultLotto(inputNumbersStr, inputBonusNumberStr, lottos);
+
+        String totalWinnings = controller.getTotalWinnings(lottoPurchase, lottoResult);
+
+        OutputView.printLottoResult(lottoResult.toString(), totalWinnings);
     }
 }
