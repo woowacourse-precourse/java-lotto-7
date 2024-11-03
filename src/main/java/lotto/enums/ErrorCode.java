c6f5a6694f6bc6c3941cebf097be1b9382b3bc8c
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-03 21:14:15 +0900

    refactor: ErrorCode enum에 [ERROR] 접두사를 포함한 toString() 메서드 추가

--- src/main/java/lotto/enums/ErrorCode.java
+++ src/main/java/lotto/enums/ErrorCode.java
@@ -1,14 +1,14 @@
 package lotto.enums;
 
 public enum ErrorCode {
-    MONEY_TYPE_ERROR("[ERROR] 금액은 숫자여야 합니다."),
-    LOTTO_MIN_PRICE_ERROR("[ERROR] 로또 구입 금액은 최소 1000원입니다."),
-    LOTTO_PRICE_ERROR("[ERROR] 로또 구입 금액은 1000원 단위로 가능합니다."),
-    LOTTO_NUMBER_COUNT_ERROR("[ERROR] 로또 번호는 6개여야 합니다."),
-    LOTTO_NUMBER_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45사이여야 합니다."),
-    LOTTO_NUMBER_DUPLICATE_ERROR("[ERROR] 로또 번호에 중복된 숫자가 있습니다."),
-    WINNING_NUMBER_TYPE_ERROR("[ERROR] 당첨 번호는 숫자여야 합니다."),
-    BONUS_NUMBER_TYPE_ERROR("[ERROR] 보너스 번호는 숫자여야 합니다.");
+    MONEY_TYPE_ERROR("금액은 숫자여야 합니다."),
+    LOTTO_MIN_PRICE_ERROR("로또 구입 금액은 최소 1000원입니다."),
+    LOTTO_PRICE_ERROR("로또 구입 금액은 1000원 단위로 가능합니다."),
+    LOTTO_NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다."),
+    LOTTO_NUMBER_RANGE_ERROR("로또 번호는 1부터 45사이여야 합니다."),
+    LOTTO_NUMBER_DUPLICATE_ERROR("로또 번호에 중복된 숫자가 있습니다."),
+    WINNING_NUMBER_TYPE_ERROR("당첨 번호는 숫자여야 합니다."),
+    BONUS_NUMBER_TYPE_ERROR("보너스 번호는 숫자여야 합니다.");
 
     private final String message;
 
@@ -18,5 +18,9 @@ public enum ErrorCode {
 
     public String getMessage() {
         return message;
+    }
+
+    public String toString() {
+        return "[ERROR] " + message;
     }
 }
