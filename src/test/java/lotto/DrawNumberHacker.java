b9e107892253a02be495bdc01665bf994734e99f
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:42:52 +0900

    refactor(args): DrawNumbers의 파라미터 형태가 달라진 것 반영

--- src/test/java/lotto/DrawNumberHacker.java
+++ src/test/java/lotto/DrawNumberHacker.java
@@ -2,17 +2,19 @@ package lotto;
 
 import lotto.model.lotto_result.DrawNumbers;
 
+import java.util.List;
+
 public enum DrawNumberHacker {
 
-    HACK_FIRST_PLACE(new DrawNumbers( "1,2,3,4,5,6",  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
-    HACK_SECOND_PLACE(new DrawNumbers("1,2,3,4,5,7",  hackedBonusNumber())),
-    HACK_THIRD_PLACE(new DrawNumbers( "1,2,3,4,5,7",  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
-    HACK_FOURTH_PLACE(new DrawNumbers("1,2,3,4,7,8",  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
-    HACK_FIFTH_PLACE(new DrawNumbers( "1,2,3,7,8,9",  DrawNumberHacker.UNKNOWN_BONUS_NUMBER));
+    HACK_FIRST_PLACE(new DrawNumbers( List.of(1,2,3,4,5,6),  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
+    HACK_SECOND_PLACE(new DrawNumbers(List.of(1,2,3,4,5,7),  hackedBonusNumber())),
+    HACK_THIRD_PLACE(new DrawNumbers( List.of(1,2,3,4,5,7),  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
+    HACK_FOURTH_PLACE(new DrawNumbers(List.of(1,2,3,4,7,8),  DrawNumberHacker.UNKNOWN_BONUS_NUMBER)),
+    HACK_FIFTH_PLACE(new DrawNumbers( List.of(1,2,3,7,8,9),  DrawNumberHacker.UNKNOWN_BONUS_NUMBER));
 
-    public static final String MIN_WINNING_NUMBER   = "1";
-    public static final String MAX_WINNING_NUMBER   = "6";
-    public static final String UNKNOWN_BONUS_NUMBER = "45";
+    public static final int MIN_WINNING_NUMBER   = 1;
+    public static final int MAX_WINNING_NUMBER   = 6;
+    public static final int UNKNOWN_BONUS_NUMBER = 45;
 
     private final DrawNumbers drawNumbers;
 
@@ -24,7 +26,7 @@ public enum DrawNumberHacker {
         return drawNumbers;
     }
 
-    private static String hackedBonusNumber() {
+    private static int hackedBonusNumber() {
         return DrawNumberHacker.MAX_WINNING_NUMBER;
     }
 }
