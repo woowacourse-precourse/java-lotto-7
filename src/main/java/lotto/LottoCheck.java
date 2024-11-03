13074a5c8f87507fb5b1cdb799fe61e571f769b7
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-03 19:23:03 +0900

    refactor: List<Lotto>에서 List<Integer>로 변경 및 iter문 int로 변경

--- src/main/java/lotto/LottoCheck.java
+++ src/main/java/lotto/LottoCheck.java
@@ -22,10 +22,10 @@ public class LottoCheck {   // 로또 당첨 및 등수확인
         return 0;
     }
 
-    public int countMatches(List<Lotto> userPurchasedLotto, List<Integer> winningNumber) {
+    public int countMatches(List<Integer> userPurchasedLotto, List<Integer> winningNumber) {
         // 보너스번호 제외하고 유저가 구매한번호와 당첨번호가 몇개 맞는지 확인
         int match = 0;
-        for (Lotto number : userPurchasedLotto) {
+        for (int number : userPurchasedLotto) {
             if (winningNumber.contains(number)) {
                 match++;
             }
