9e2d9adf7e2bece5d3bbb7d8b2aeafe3a250ac8d
btongtong <leeym2615@naver.com>
2024-11-04 02:06:04 +0900

    feat(UserLottos): Validate purchaseAmount

--- src/main/java/lotto/UserLottos.java
+++ src/main/java/lotto/UserLottos.java
@@ -4,6 +4,13 @@ public class UserLottos {
     private final int purchaseAmount;
 
     public UserLottos(int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount == 0 || purchaseAmount % LottoInfo.PRICE.getInfo() != 0) {
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_THOUSAND_UNIT_ERROR.getMessage());
+        }
     }
 }
