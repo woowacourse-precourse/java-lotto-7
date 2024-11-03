034a337df5efb838afc13bb72ef888be6da7b09c
etd93 <etd93@naver.com>
2024-11-04 00:24:48 +0900

    feat: 예외 발생할 경우 IllegalArgumentException을 발생, "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 받기 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,29 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.constants.IOMessageConstants;
+import lotto.exception.InputViewException;
 
 public class InputView {
 
+    private final InputViewException inputViewException;
+
+    public InputView(){
+        inputViewException = new InputViewException();
+    }
+
     public String inputMoney() {
-        String inputMoney = Console.readLine();
+        String inputMoney;
+        while(true){
+            try{
+                System.out.println(IOMessageConstants.INPUT_PURCHASE_AMOUNT);
+                inputMoney = Console.readLine();
+                inputViewException.validateInputMoney(inputMoney);
+                break;
+            }catch(IllegalArgumentException | ArithmeticException e){
+                System.out.println(e.getMessage());
+            }
+        }
         return inputMoney;
     }
 }
