7817a0fa5f724d1a85ed3f8813924c6217c76bb6
차왕준 <ckdhdkwns@gmail.com>
2024-11-04 03:29:20 +0900

    test(domain): 로또 번호 범위 검증 테스트

--- src/test/java/lotto/util/LottoNumberValidatorTest.java
+++ src/test/java/lotto/util/LottoNumberValidatorTest.java
@@ -56,4 +56,27 @@ public class LottoNumberValidatorTest {
                     .hasMessageContaining("중복");
         }
     }
+
+    @Nested
+    @DisplayName("로또 번호들의 범위 검증")
+    class ValidateNumbersInRange {
+        @Test
+        @DisplayName("모든 번호가 범위 내에 있으면 정상 처리한다.")
+        void 모든_번호가_범위_내에_있으면_정상_처리한다() {
+            List<Integer> numbers = Arrays.asList(1, 20, 30, 40, 44, 45);
+
+            assertThatNoException()
+                    .isThrownBy(() -> LottoNumberValidator.validateNumbersInRange(numbers));
+        }
+
+        @Test
+        @DisplayName("범위를 벗어난 번호가 있으면 예외가 발생한다.")
+        void 범위를_벗어난_번호가_있으면_예외가_발생한다() {
+            List<Integer> numbers = Arrays.asList(1, 20, 30, 40, 45, 46);
+
+            assertThatThrownBy(() -> LottoNumberValidator.validateNumbersInRange(numbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이하");
+        }
+    }
 }
