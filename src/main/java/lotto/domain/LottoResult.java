02783a0175412b2918f70db955a171963dabf1e3
dltmddus <tmddus4671@gmail.com>
2024-11-03 15:59:33 +0900

    feat: 당첨 로또 계산 getMatchingNumbers함수 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,31 +1,39 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoResult {
-    private List<Integer> matchNumber;
+    private final Map<Integer, Integer> matchNumber;
+    private final LottoShop lottoShop;
+    private final Lotto winningLotto;
 
-    public LottoResult(List<Integer> matchNumber) {
-        this.matchNumber = matchNumber;
+    public LottoResult(LottoShop lottoShop, Lotto winningLotto) {
+        this.lottoShop = lottoShop;
+        this.winningLotto = winningLotto;
+        matchNumber = new HashMap<>();
+        calculateMatchLotto();
     }
 
-    public static LottoResult calculateResult(LottoShop lottoShop, Lotto winningLotto, int bonusNumber) {
-        List<Integer> matchedCounts = new ArrayList<>();
-
-        List<Lotto> matchingLotteries = lottoShop.getLottoTickets().getLotteries().stream()
-                .filter(lotto -> !getMatchingNumbers(lotto, winningLotto).isEmpty()).toList();
-
-        matchingLotteries.forEach(lotto -> {
-            List<Integer> matchedNumbers = getMatchingNumbers(lotto, winningLotto);
-            int matchedCount = matchedNumbers.size();
-            boolean hasBonus = lotto.getNumbers().contains(bonusNumber);
-            matchedCounts.add(matchedCount);
-        });
-        return new LottoResult(matchedCounts);
+    public Map<Integer, Integer> getMatchNumber() {
+        return matchNumber;
     }
 
-    private static List<Integer> getMatchingNumbers(Lotto lotto, Lotto winningLotto) {
+    private void calculateMatchLotto() {
+        lottoShop.getLottoTickets().getLotteries().stream()
+                .filter(lotto -> !getMatchingNumbers(lotto).isEmpty())
+                .forEach(this::addMatchingLotto);
+    }
+
+    private void addMatchingLotto(Lotto lotto) {
+        int matchedCount = getMatchingNumbers(lotto).size();
+        if (matchedCount >= 3) {
+            matchNumber.put(matchedCount, matchNumber.getOrDefault(matchedCount, 0) + 1);
+        }
+    }
+
+    private List<Integer> getMatchingNumbers(Lotto lotto) {
         return lotto.getNumbers().stream()
                 .filter(winningLotto.getNumbers()::contains)
                 .toList();
