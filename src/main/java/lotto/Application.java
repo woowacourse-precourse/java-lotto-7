71e41bc782cb5b41797a6ba5caac55b9e78f4db3
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-03 19:45:36 +0900

    feat: LottoController 생성 및 의존성 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.io.Input;
+import lotto.io.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Output output = new Output();
+        Input input = new Input();
+
+        LottoController lottoController = new LottoController(output, input);
+        lottoController.playLotto();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,18 @@
+package lotto.controller;
+
+import lotto.io.Input;
+import lotto.io.Output;
+
+public class LottoController {
+    private final Output output;
+    private final Input input;
+
+    public LottoController(Output output, Input input) {
+        this.output = output;
+        this.input = input;
+    }
+
+    public void playLotto() {
+
+    }
+}
