ada4a418ce14cb60d99fd673f4c03eee0b943c4e
sodychoe <sodychoe@icloud.com>
2024-11-03 22:27:30 +0900

    feat(InputConsoleHandler) : 로또 구입 금액 입력 받기 기능 구현

--- src/main/java/lotto/io/InputConsoleHandler.java
+++ src/main/java/lotto/io/InputConsoleHandler.java
@@ -1,7 +1,15 @@
 package lotto.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputConsoleHandler {
     public void showBuyingAmountGuideMessage() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public int askBuyingAmount() {
+        String rawBuyingAmount = Console.readLine();
+
+        return Integer.parseInt(rawBuyingAmount);
     }
 }
