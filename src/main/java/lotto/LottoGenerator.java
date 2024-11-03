fd6753e7ea43dc710ded1c67208ccfd228bda7de
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-03 16:38:21 +0900

    feat: LottoGenerator 클래스 구현

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LottoGenerator {
+    int lottoCount;
+    int money;
+    List<Lotto> lottoList = new ArrayList<>();
+
+    void getTicketCount(String input) throws Exception{
+        if (input == null) throw new IllegalStateException("[ERROR] 입금액이 입력되지 않았습니다.");
+        if (input.length()<4) throw new IllegalArgumentException("[ERROR] 입금액이 올바르지 않습니다. 1000 단위로 입력해주세요.");
+        try {
+            money = Integer.parseInt((input));
+            this.lottoCount = money/1000;
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 입금액이 올바르지 않습니다. 1000 단위로 입력해주세요.");
+        }
+    }
+
+    List<Lotto> generateLotto(int totalNum){
+        for (int i = 0; i<totalNum; i++){
+            List<Integer> numList = new ArrayList<>();
+            numList = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lotto a = new Lotto(numList);
+            lottoList.add(a);
+        }
+        return lottoList;
+    }
+
+    List<Lotto> getLottoList(String input) throws Exception {
+        this.getTicketCount(input);
+
+        return this.generateLotto(this.lottoCount);
+
+
+    }
+}
