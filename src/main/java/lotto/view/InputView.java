b9cef496691bc5f6278a009adde9e2f1bad3b0c8
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:41:44 +0900

    feat(class): 입력 뷰 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,40 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.lotto.Lotto;
+import lotto.model.lotto_result.DrawNumbers;
+import lotto.model.lotto_result.DrawNumbersBuilder;
+import lotto.utils.DrawNumbersValidator;
+import lotto.utils.PaymentValidator;
+
+import java.util.List;
+
 public class InputView {
+
+    private final PaymentValidator paymentValidator = new PaymentValidator();
+    private final DrawNumbersValidator drawNumbersValidator = new DrawNumbersValidator();
+
+    public long getPayment() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String payment = readLine();
+        paymentValidator.validate(payment);
+        return Long.parseLong(payment);
+    }
+
+    public DrawNumbersBuilder getWinningNumbers(DrawNumbersBuilder builder) {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumbers = readLine();
+        return builder.winningNumbers(winningNumbers);
+    }
+
+    public DrawNumbersBuilder getBonusNumber(DrawNumbersBuilder builder) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = readLine();
+        return builder.bonusNumber(bonusNumber);
+    }
+
+    private String readLine() {
+        return Console.readLine();
+    }
 
 }
