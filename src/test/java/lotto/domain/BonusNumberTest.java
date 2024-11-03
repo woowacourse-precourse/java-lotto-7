f8caf08ff2274f532461ecd64ffb4918e472fdc4
변우영 <likevangogh24@gmail.com>
2024-11-03 20:02:05 +0900

    test(BonusNumber): 보너스 번호 범위 예외 테스트 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,5 +16,14 @@ class BonusNumberTest {
         BonusNumber bonusNumber = BonusNumber.from("7", WinningNumber.from("1,2,3,4,5,6"));
 
         Assertions.assertThat(bonusNumber.isEqualTo(7)).isTrue();
+    }
+
+    @DisplayName("보너스 번호에 1~45 범위 바깥의 숫자를 입력하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-3", "0", "46"})
+    void checkBonusNumberInRange(String inputBonusNumber) {
+        Assertions.assertThatThrownBy(() -> BonusNumber.from(inputBonusNumber, WinningNumber.from("1,2,3,4,5,6")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호는 1~45 사이의 숫자를 입력해야 합니다.");
     }
 }
