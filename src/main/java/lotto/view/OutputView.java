b1e0e635eede76beaba93f539ae5a0417f8b9ce2
Seungwan Yoo <ysw99@kakao.com>
2024-11-03 19:34:12 +0900

    feat: 생성 된 로또 번호 출력 구현 - 로또 갯수도 같이 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,29 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+
 public class OutputView {
-    public void renderErrorMessage(String message) {
+    private static final String PURCHASED_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+
+    public void renderErrorMessage(final String message) {
         System.out.println(message);
         System.out.println();
     }
+
+    public void renderNewLine() {
+        System.out.println();
+    }
+
+    public void renderGeneratedLottos(final LottoGame lottoGame) {
+        renderNewLine();
+        System.out.printf(PURCHASED_LOTTO_COUNT_MESSAGE, lottoGame.getLottoCount());
+        renderNewLine();
+        lottoGame.getGeneratedLottos().forEach(this::renderLotto);
+    }
+
+    public void renderLotto(final Lotto lotto) {
+        System.out.println(lotto.getNumbers());
+    }
+
 }
