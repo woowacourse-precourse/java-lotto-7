4d3bfd3a34a6038565ad5240ec80b0a1668e23a5
OhYeHyun <ohyhohyl@naver.com>
2024-11-04 01:33:15 +0900

    feat(Application): 프로그램실행하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final InputView inputView = new InputView();
+        LottoController lottoController = new LottoController(inputView);
+
+        lottoController.start();
     }
 }
