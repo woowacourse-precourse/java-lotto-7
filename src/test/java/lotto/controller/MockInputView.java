1ec2b8465d8831707a32f843987f5b40f12a6cab
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-03 15:45:20 +0900

    refactor(MockInputView): 제네릭 클래스로 변경

--- src/test/java/lotto/controller/MockInputView.java
+++ src/test/java/lotto/controller/MockInputView.java
@@ -2,29 +2,29 @@ package lotto.controller;
 
 import lotto.view.InputView;
 
-public class MockInputView implements InputView {
+public class MockInputView<T> implements InputView {
 
-    private String input;
+    private T input;
 
     public MockInputView() {
     }
 
-    public void setInput(String input) {
+    public void setInput(T input) {
         this.input = input;
     }
 
     @Override
     public String getInputPrice() {
-        return input;
+        return input.toString();
     }
 
     @Override
     public String getWinningComponent() {
-        return input;
+        return input.toString();
     }
 
     @Override
     public String getBonusComponent() {
-        return input;
+        return input.toString();
     }
 }
