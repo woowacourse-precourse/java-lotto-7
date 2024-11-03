9a680bd7ecaa7cae2ab800394a83b5c93c7da113
kimchanho97 <nh0903@pusan.ac.kr>
2024-11-04 08:14:39 +0900

    feat(LottoRank): "로또 등수에 대한 정보를 안다"

---
+++ src/main/java/lotto/model/LottoRank.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+public enum LottoRank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    FAIL(0, false, 0),
+    ;
+
+    private final int matchCount;
+    private final boolean bonus;
+    private final int price;
+
+    LottoRank(int matchCount, boolean bonus, int prize) {
+        this.matchCount = matchCount;
+        this.bonus = bonus;
+        this.price = prize;
+    }
+}
