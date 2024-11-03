641482188f94d9995f4228a14f6d84098c250eb3
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:35:41 +0900

    refactor(constructor): 로또 발행 기능을 추출하였으므로 번호 생성기 제거 및 추첨 번호필드 추가

--- src/main/java/lotto/model/lotto/LottoMachine.java
+++ src/main/java/lotto/model/lotto/LottoMachine.java
@@ -1,15 +1,17 @@
 package lotto.model.lotto;
 
-import lotto.number_generator.NumberGenerator;
+import lotto.model.Winning;
+import lotto.model.lotto_result.DrawNumbers;
 
 import java.util.List;
+import java.util.Optional;
 
 public class LottoMachine {
 
-    private final NumberGenerator numberGenerator;
+    private final DrawNumbers drawNumbers;
 
-    public LottoMachine(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
+    public LottoMachine(DrawNumbers drawNumbers) {
+        this.drawNumbers = drawNumbers;
     }
 
     public List<Integer> createLottoNumbers() {
