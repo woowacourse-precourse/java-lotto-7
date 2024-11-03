77c0f5c96ee96118672bb749070d65c9db6092c6
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:58:32 +0900

    feat: ReturnRate 클래스 구현

--- src/main/java/lotto/model/ReturnRate.java
+++ src/main/java/lotto/model/ReturnRate.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
 public class ReturnRate {
+
+    private final WinningMoney winningMoney;
+    private final Money money;
+    private double returnRate;
+
+    public ReturnRate(WinningMoney winningMoney, Money money) {
+        this.winningMoney = winningMoney;
+        this.money = money;
+        calculate();
+    }
+
+    private void calculate() {
+        returnRate = (double)winningMoney.getTotalReward() / money.getMoney() * 100;
+    }
+
+    public double getReturnRate() {
+        return returnRate;
+    }
 }
