d1f014d8e58b4bab80e4f2d74c868556c64e6530
kaswhy <sxshx818@naver.com>
2024-11-04 01:56:46 +0900

    feat(OutputView): 당첨 번호 입력 가이드 출력 메서드 구현
    변경 후
    - 가이드 메시지를 상수화 후, 이를 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
 
     private static final String GUIDE_INPUT_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String GUIDE_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public void printInputAmount() {
         System.out.println(GUIDE_INPUT_AMOUNT);
@@ -26,5 +27,9 @@ public class OutputView {
         for (Lotto nowTicket : purchasedLottos.get()) {
             System.out.println(nowTicket.get());
         }
+    }
+
+    public void printInputWinningNumbers() {
+        System.out.println(GUIDE_INPUT_WINNING_NUMBERS);
     }
 }
