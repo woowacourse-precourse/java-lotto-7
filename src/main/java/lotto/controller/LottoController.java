d5d5e568a78698a9ac2ef73fe6f1afff5b49f274
SinnoLn <sintmtn69@gmail.com>
2024-11-03 22:54:39 +0900

    refactor: LottoController에 LoggerUtils 적용하여 로깅 기능 추가
    - 프로그램 시작, 로또 티켓 발행, 당첨 결과 계산 등 주요 프로세스에 대한 로그 추가
    - 예외 상황 시 에러 로그 기록으로 디버깅 용이성 향상
    - 프로그램 흐름을 로깅하여 추적 가능성 강화

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import lotto.Lotto;
 import lotto.model.LottoRank;
 import lotto.service.LottoService;
+import lotto.utils.LoggerUtils;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
@@ -28,14 +29,26 @@ public class LottoController {
      * 로또 프로그램 실행 메서드
      */
     public void run() {
-        int purchaseAmount = inputView.inputPurchaseAmount();
-        List<Lotto> tickets = lottoService.purchaseLottoTickets(purchaseAmount);
-        resultView.printLottoTickets(tickets);
+        try {
+            LoggerUtils.logInfo("로또 프로그램 시작");
 
-        List<Integer> winningNumbers = inputView.inputWinningNumbers();
-        int bonusNumber = inputView.inputBonusNumber();
+            int purchaseAmount = inputView.inputPurchaseAmount();
+            List<Lotto> tickets = lottoService.purchaseLottoTickets(purchaseAmount);
+            LoggerUtils.logInfo("로또 티켓 발행 완료");
 
-        Map<LottoRank, Integer> results = lottoService.calculateResults(tickets, winningNumbers, bonusNumber);
-        resultView.printResults(results);
+            resultView.printLottoTickets(tickets);
+
+            List<Integer> winningNumbers = inputView.inputWinningNumbers();
+            int bonusNumber = inputView.inputBonusNumber();
+
+            Map<LottoRank, Integer> results = lottoService.calculateResults(tickets, winningNumbers, bonusNumber);
+            LoggerUtils.logInfo("당첨 결과 계산 완료");
+
+            resultView.printResults(results);
+
+        } catch (Exception e) {
+            LoggerUtils.logError("로또 프로그램 실행 중 오류 발생: " + e.getMessage());
+            throw e;
+        }
     }
 }
