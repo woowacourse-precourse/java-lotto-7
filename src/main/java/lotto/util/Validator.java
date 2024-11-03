5fc45eed9c2271c8ad8705afb01cf10ae875cb38
wooqqq <sge0225@naver.com>
2024-11-03 23:37:09 +0900

    feat(Validator): 로또 번호에 대한 예외 처리(번호가 1과 45 사이가 아닌 경우)

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -7,4 +7,10 @@ public class Validator {
             throw new IllegalArgumentException("로또 번호는 숫자 형식이어야 합니다.");
         }
     }
+
+    public static void validateLottoRange(int number) {
+        if (number < Constants.MIN_NUMBER || number > Constants.MAX_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 1과 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
