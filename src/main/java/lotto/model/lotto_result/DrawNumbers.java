ca88d053248ddcbcee371a07a82ccc1ac6821859
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 15:10:57 +0900

    feat(DrawNumbers): 제거한 DrawNumbersBuilder의 역할 위임

--- src/main/java/lotto/model/lotto_result/DrawNumbers.java
+++ src/main/java/lotto/model/lotto_result/DrawNumbers.java
@@ -1,5 +1,7 @@
 package lotto.model.lotto_result;
 
+import lotto.utils.DrawNumbersValidator;
+
 import java.util.List;
 
 public class DrawNumbers {
@@ -7,8 +9,23 @@ public class DrawNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
-    DrawNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+    public DrawNumbers(String winningNumbers, String bonusNumber) {
+        DrawNumbersValidator drawNumbersValidator = new DrawNumbersValidator();
+        this.winningNumbers = drawNumbersValidator.validateWinningNumbers(winningNumbers);
+        this.bonusNumber = drawNumbersValidator.validateBonusNumber(bonusNumber);
+        drawNumbersValidator.validateAssociateWinningAndBonusNumbers(this.winningNumbers, this.bonusNumber);
+    }
+
+    public int countMatch(List<Integer> numbers) {
+        return (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    public int countBonusNumber(List<Integer> numbers) {
+        if (numbers.contains(bonusNumber)) {
+            return 1;
+        }
+        return 0;
     }
 }
