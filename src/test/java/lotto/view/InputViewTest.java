a9c152d602a756b75e5487c6eca0df8e3e22fdf5
SinnoLn <sintmtn69@gmail.com>
2024-11-03 22:41:20 +0900

    test: InputView에서 Validator로 이동한 유효성 검증 로직에 맞춰 InputViewTest 수정
    - InputViewTest에서 Validator로 옮겨진 검증 로직에 대한 테스트를 삭제
    - InputView의 입력 함수의 테스트를 위한 로직으로 변경
    - 테스트 명확성을 위해 리팩토링된 부분 반영

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -9,30 +9,10 @@ public class InputViewTest {
     private final InputView inputView = new InputView();
 
     @Test
-    @DisplayName("당첨 번호가 6개가 아니거나, 중복되거나, 범위를 벗어나면 예외 발생")
-    void 당첨_번호_유효성_검증() {
-        assertThatThrownBy(() -> inputView.parseAndValidateWinningNumbers("1, 2, 3, 3, 5, 6"))
+    @DisplayName("구입 금액이 숫자가 아닐 경우 예외 발생")
+    void 구입_금액_숫자_아님_예외() {
+        assertThatThrownBy(inputView::inputPurchaseAmount)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
-
-        assertThatThrownBy(() -> inputView.parseAndValidateWinningNumbers("1, 2, 3, 4, 5, 46"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 1부터 45 사이여야 합니다.");
-
-        assertThatThrownBy(() -> inputView.parseAndValidateWinningNumbers("1, 2, 3, 4, 5"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 6개의 숫자로 입력해야 합니다.");
-    }
-
-    @Test
-    @DisplayName("보너스 번호가 1~45 범위를 벗어나면 예외 발생")
-    void 보너스_번호_유효성_검증() {
-        assertThatThrownBy(() -> inputView.validateBonusNumber(0))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
-
-        assertThatThrownBy(() -> inputView.validateBonusNumber(46))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
+                .hasMessageContaining("[ERROR] 구입 금액은 숫자로 입력해 주세요.");
     }
 }
