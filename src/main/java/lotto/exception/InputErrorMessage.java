ebbe75dc043be977a85e19255cfe07db6c442dcb
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-03 13:44:06 +0900

    refactor: Error메시지 수정
    - 에러 메시지 내용 및 이름 변경

--- src/main/java/lotto/exception/InputErrorMessage.java
+++ src/main/java/lotto/exception/InputErrorMessage.java
@@ -4,6 +4,7 @@ public enum InputErrorMessage {
 
     //문자열 -> 숫자 파싱 관련 메시지
     INVALID_NUMERIC_FORMAT("[ERROR] 입력값은 숫자여야 합니다."),  // 구입 금액, 당첨 번호, 보너스 번호에 공통 사용 가능,
+    OVER_NUMERIC_FORTMAT("[ERROR] 정수의 최대값을 입력할 수 없습니다."),
     EMPTY_INPUT("[ERROR] 입력은 비어있을 수 없습니다."),
 
     // 구입 금액 관련 메시지,
--- src/main/java/lotto/exception/LottoErrorMessage.java
+++ src/main/java/lotto/exception/LottoErrorMessage.java
@@ -1,9 +1,12 @@
 package lotto.exception;
 
 public enum LottoErrorMessage {
+    EMPTY_LOTTO_VALUE("[ERROR] 비어있는 로또가 존재합니다."),
+    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호가 6자리가 아닙니다."),
     INVALID_LOTTO_COUNT("[ERROR] 발행할 로또 수가 유효하지 않습니다."),
     LOTTO_NUMBER_OUT_OF_BOUNDS("[ERROR] 로또 번호는 1~45 범위의 숫자여야 합니다."),
     DUPLICATED_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복될 수 없습니다."),
+
     INVALID_WINNING_RANK("[ERROR] 잘못된 당첨 등수입니다."),
     INVALID_YIELD("[ERROR] 잘못된 수익률 값입니다.");
 
