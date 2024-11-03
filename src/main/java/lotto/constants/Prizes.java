458630dfcd459c34968800ac6041f288a9e5cf37
yush <yush2202@gmail.com>
2024-11-04 03:07:30 +0900

    refactor($ Prizes): 상 순서 변경
    출력 사양에 맞게 상의 순서 변경

--- src/main/java/lotto/constants/Prizes.java
+++ src/main/java/lotto/constants/Prizes.java
@@ -1,23 +1,22 @@
 package lotto.constants;
 
 public enum Prizes {
-
-    FIRST_PRIZE(2_000_000_000, 6, false, ""),
-    SECOND_PRIZE(30_000_000, 5, true, ", 보너스 볼 일치"),
-    THIRD_PRIZE(1_500_000, 5, false, ""),
+    FIFTH_PRIZE(5_000, 3, false, ""),
     FOURTH_PRIZE(50_000, 4, false, ""),
-    FIFTH_PRIZE(5_000, 3, false, "");
+    THIRD_PRIZE(1_500_000, 5, false, ""),
+    SECOND_PRIZE(30_000_000, 5, true, ", 보너스 볼 일치"),
+    FIRST_PRIZE(2_000_000_000, 6, false, "");
 
     private final int money;
     private final int count;
     private final boolean bonus;
-    private final String bounsComment;
+    private final String bonusComment;
 
-    Prizes(int money, int count, boolean bonus, String bounsComment) {
+    Prizes(int money, int count, boolean bonus, String bonusComment) {
         this.money = money;
         this.count = count;
         this.bonus = bonus;
-        this.bounsComment = bounsComment;
+        this.bonusComment = bonusComment;
     }
 
     public int getMoney() {
@@ -33,6 +32,6 @@ public enum Prizes {
     }
 
     public String getComment() {
-        return bounsComment;
+        return bonusComment;
     }
 }
