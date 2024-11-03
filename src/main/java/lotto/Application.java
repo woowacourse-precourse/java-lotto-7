52eae84853d50783bc29d64096a01f633434baf2
minseojeong <jgoldstone7@gmail.com>
2024-11-03 20:14:30 +0900

    feat(application): 구매한 금액만큼의 로또 번호 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int amount = getAmount();
-        //int[] numbers = getNumbers();
+        int lottoCount = amount /1000;
+
+        System.out.println(lottoCount + " 개를 구매했습니다.");
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lotto = new Lotto(generateRandomLottoNumbers());
+            System.out.println(lotto.getNumbers());
+        }
+
+        Lotto winningLotto = new Lotto();
+
+        //구매한 로또 번호와 당첨 번호 비교, 당첨 결과 확인
     }
 
     public static int getAmount() {
@@ -29,5 +40,9 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 타입입니다.");
         }
 
+    }
+
+    private static List<Integer> generateRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1,45,6);
     }
 }
