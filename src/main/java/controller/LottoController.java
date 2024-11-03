c9e4bec89830f0c57aecea7d64e19ff9433e7be9
aaahyunseo <hs030608@naver.com>
2024-11-03 19:06:42 +0900

    refactor(LottoController) : 주석 삭제

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -16,26 +16,20 @@ public class LottoController {
 
 
     public void run(){
-        // 로또 구입 금액을 입력 받아서 로또 발행 횟수를 반환
         int purchaseAmount = inputView.inputPurchaseAmount();
         int count = lottoService.lottoCount(purchaseAmount);
 
-        // 구입한 금액만큼 로또 발행하기
         List<Lotto> lottoList = lottoService.publishLotto(count);
         outputView.publishLotto(count, lottoList);
 
-        // 당첨 번호 입력 받기
         String winningNumber = inputView.inputWinningNumber();
         Lotto lotto = lottoService.inputWinningNumber(winningNumber);
 
-        // 보너스 번호 입력 받기
         int bonusNumber = lottoService.bonusNumber(inputView.inputBonusNumber(), lotto);
 
-        // 당첨 내역 출력하기
         Map<Rank, Integer> rankCount = lottoService.calculateStatistics(lottoList, lotto, bonusNumber);
         outputView.printStatistics(rankCount);
 
-        // 총 수익률 출력하기
         double rate = lottoService.totalReturn(rankCount, purchaseAmount);
         outputView.printTotalReturn(rate);
     }
