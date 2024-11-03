2a9cf258c572f2e90606f52a0d03ddd49df85e9e
soeunnPark <carol0724@naver.com>
2024-11-03 21:29:33 +0900

    feat(Revenue): calculate revenue and returns
    수익과 수익률을 계산하는 기능을 구현했습니다.

---
+++ src/main/java/lotto/domain/money/Revenue.java
@@ -0,0 +1,32 @@
+package lotto.domain.money;
+
+import lotto.domain.matcher.LottoResult;
+
+public class Revenue {
+
+    private final Money revenue;
+    private final double returns;
+
+    public Revenue(LottoResult lottoResult, Money cost) {
+        revenue = new Money(calculateRevenue(lottoResult));
+        returns = calculateReturns(revenue, cost);
+    }
+
+    public Money getRevenue() {
+        return revenue;
+    }
+
+    public double getReturns() {
+        return returns;
+    }
+
+    private long calculateRevenue(LottoResult lottoResult) {
+        return lottoResult.getLottoResult().entrySet().stream()
+                .mapToLong(entry -> (long) entry.getKey().getPrize() * entry.getValue())
+                .sum();
+    }
+
+    private double calculateReturns(Money revenue, Money money) {
+        return (double) revenue.getMoney() / money.getMoney();
+    }
+}
---
+++ src/test/java/lotto/domain/money/RevenueTest.java
@@ -0,0 +1,25 @@
+package lotto.domain.money;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lotto.domain.matcher.LottoPrize;
+import lotto.domain.matcher.LottoResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RevenueTest {
+
+    @DisplayName("로또 결과를 통해 수익률을 반환하는 테스트")
+    @Test
+    void revenueTest() {
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.addLottoResult(LottoPrize.SECOND_PRIZE);
+        lottoResult.addLottoResult(LottoPrize.THIRD_PRIZE);
+        lottoResult.addLottoResult(LottoPrize.FOURTH_PRIZE);
+        Revenue revenue = new Revenue(lottoResult, new Money(5000));
+        assertAll(() -> assertThat(revenue.getReturns()).isEqualTo(6310.0),
+                () -> assertThat(revenue.getRevenue().getMoney()).isEqualTo(31550000)
+        );
+    }
+}
