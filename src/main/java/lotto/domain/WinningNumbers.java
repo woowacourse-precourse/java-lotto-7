2bf34416dec6e2c2cab2bc429085f2c80804c092
minjae k <rkdalswoals@gmail.com>
2024-11-04 01:40:53 +0900

    feat(domain): change WinningNumbers to use Set for unique lotto numbers

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,34 +1,27 @@
 package lotto.domain;
 
+import static lotto.validator.LottoValidator.validateBonusNumber;
+
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.validator.LottoValidator;
 
 public class WinningNumbers {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
 
-    private final List<Integer> numbers;
+    private final Set<Integer> numbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> numbers, int bonusNumber) {
-        LottoValidator.validate(numbers);
+        LottoValidator.validateLottoNumbers(numbers);
         validateBonusNumber(bonusNumber, numbers);
-
-        this.numbers = numbers;
+        this.numbers = new HashSet<>(numbers);
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateBonusNumber(int bonusNumber, List<Integer> numbers) {
-        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-        }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
+    public Set<Integer> getNumbers() {
+        return Collections.unmodifiableSet(numbers);
     }
 
     public int getBonusNumber() {
