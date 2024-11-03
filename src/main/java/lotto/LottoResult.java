46ba547bf83ba2af1dcbc91b9fc6a4cc3ad8c013
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-03 21:41:07 +0900

    feat(LottoResult): 로또 결과 반환 기능 추가

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoResult {
+    private final Map<Ranking, Long> results;
+    private final double returnRate;
+    private final int purchasedCount;
+
+    public LottoResult(List<Lotto> purchasedLotto, WinningNumbers winningNumbers, Calculator rankingCalculator) {
+        this.purchasedCount = purchasedLotto.size();
+        this.results = rankingCalculator.lottoResults(purchasedLotto);
+        this.returnRate = rankingCalculator.calculateReturnRate(results, purchasedCount);
+    }
+
+    public Map<Ranking, Long> getResults() {
+        return this.results;
+    }
+
+    public int getPurchasedCount() {
+        return this.purchasedCount;
+    }
+
+    public double getReturnRate() {
+        return this.returnRate;
+    }
+
+    public String formatResults() {
+        return results.entrySet().stream()
+                .filter(entry -> entry.getKey() != Ranking.NONE) // NONE인 경우 제외
+                .map(entry -> entry.getKey().getPrintMessage() + " - " + entry.getValue() + "개")
+                .collect(Collectors.joining("\n"));
+    }
+}
