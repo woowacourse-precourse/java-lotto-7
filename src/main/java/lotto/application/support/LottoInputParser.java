eb7cc971759de5863080c0ad516a27e073c9f91e
bellmin <whdlas011203@naver.com>
2024-11-04 01:14:25 +0900

    feat(LottoInputParser): 당첨 번호 파싱 기능 추가
    - 사용자로부터 입력받은 String 타입의 당첨 번호를 리스트 형태로 바꾼다
    - 중복된 번호가 있는지 유효성 검증을 수행한다

--- src/main/java/lotto/application/support/LottoInputParser.java
+++ src/main/java/lotto/application/support/LottoInputParser.java
@@ -1,10 +1,13 @@
 package lotto.application.support;
 
+import static lotto.common.Consts.WINNING_NUMBER_NOT_DUPLICATED_ERROR;
 import static lotto.common.Consts.INPUT_MUST_BE_NUMBER;
 import static lotto.common.Consts.INPUT_MUST_BE_POSITIVE_NUMBER;
 import static lotto.common.Consts.LOTTO_PRICE;
 import static lotto.common.Consts.PURCHASE_AMOUNT_1000_UNIT;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.common.Errors;
 
 public class LottoInputParser {
@@ -28,6 +31,28 @@ public class LottoInputParser {
 
         if(amount < 0){
             Errors.IllegalArgumentException(INPUT_MUST_BE_POSITIVE_NUMBER);
+        }
+    }
+
+    public List<Integer> parseWinningNumber(String winningNumber){
+        List<Integer> winningNumbers = winningNumbertoList(winningNumber);
+        validateWinningNumber(winningNumbers);
+        return winningNumbers;
+    }
+
+    private List<Integer> winningNumbertoList(String winningNumber){
+        String[] winningNumbers = winningNumber.split(",");
+
+        return Arrays.asList(
+                Arrays.stream(winningNumbers)
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .toArray(Integer[]::new));
+    }
+
+    private void validateWinningNumber(List<Integer> winningNumbers){
+        if(winningNumbers.stream().distinct().count() != winningNumbers.size()){
+            Errors.IllegalArgumentException(WINNING_NUMBER_NOT_DUPLICATED_ERROR);
         }
     }
 }
