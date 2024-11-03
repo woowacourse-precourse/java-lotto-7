73b98c783cc4a78f3609d0859149fa716aced6b6
nns503 <73779581+nns503@users.noreply.github.com>
2024-11-04 00:56:19 +0900

    test: MoneyManager 테스트로 이름 변경

--- src/test/java/lotto/domain/MoneyManagerTest.java
+++ src/test/java/lotto/domain/MoneyManagerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoManagerTest {
+class MoneyManagerTest {
 
     @Test
     void 수익률_반환() {
