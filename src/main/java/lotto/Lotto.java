5ae3341ea61baa95e9876006de646f301f13a154
dev-Rhea <dev.rhea4444@gmail.com>
2024-11-03 19:06:29 +0900

    :art: refactor : 오름차순 정렬 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -39,6 +40,7 @@ public class Lotto {
 
         for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers); // 오름차순 정렬
             lottoList.add(new Lotto(numbers));
         }
 
