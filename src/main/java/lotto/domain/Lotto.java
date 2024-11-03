16ea7f334cacb103f16453850dc7d789459a5407
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:57:00 +0900

    feat(Lotto): 로또 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int MAX_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,6 +18,13 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        numbers.forEach((number) -> {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                String errorMessage = String.format("[ERROR] 로또 번호는 %d 이상, %d 이하 정수여야 합니다.", MIN_NUMBER, MAX_NUMBER);
+                throw new IllegalArgumentException(errorMessage);
+            }
+        });
     }
 
     public List<Integer> getImmutableNumbers() {
