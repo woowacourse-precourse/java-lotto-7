d794e09f09600bfbbaf2fca2681184ff5ddf8d3b
youjeans <dbyj2810@naver.com>
2024-11-03 22:50:50 +0900

    feat: 입력된 문자열에서 번호를 추출하는 기능

---
+++ src/main/java/lotto/LottoDraw.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoDraw {
+    private static List<Integer> splitAndTrimInput(String input) {
+        List<Integer> numberList = new ArrayList<>();
+        String[] parts = input.split(",");
+        for (int i = 0; i < parts.length; i++) {
+            parts[i] = parts[i].trim();
+        }
+        parseStringToInteger(parts, numberList);
+        return numberList;
+    }
+
+    private static void parseStringToInteger(String[] parts, List<Integer> numberList) {
+        for (String part : parts) {
+            try {
+                numberList.add(Integer.parseInt(part));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 정수이어야 합니다.");
+            }
+        }
+    }
+}
