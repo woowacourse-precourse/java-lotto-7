c0fcbdd148c9395cad27160e7f5c7c7e430827b9
haebyun <bhk4392@gmail.com>
2024-11-03 17:11:14 +0900

    feat(LottoNumber): 로또 번호 범위 검증 로직 추가
    - 로또 번호가 1에서 45 사이에 속하는지 확인하는 validate 메서드 추가
    - 유효하지 않은 번호 입력 시 예외를 발생시키도록 validateLottoNumberRange 메서드 구현
    - 로또 번호 유효성 검증을 위한 상수 MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER 정의

--- src/main/java/lotto/domain/vo/LottoNumber.java
+++ src/main/java/lotto/domain/vo/LottoNumber.java
@@ -1,17 +1,34 @@
 package lotto.domain.vo;
 
+import lotto.exception.ErrorMessage;
+
 public class LottoNumber {
+    private static final Integer MIN_LOTTO_NUMBER = 1;
+    private static final Integer MAX_LOTTO_NUMBER = 45;
+
     private final Integer number;
 
     private LottoNumber(Integer number) {
+        validate(number);
         this.number = number;
     }
 
     public static LottoNumber of(Integer number) {
+        validate(number);
         return new LottoNumber(number);
     }
 
     public Integer getValue() {
         return number;
+    }
+
+    private static void validate(Integer value) {
+        validateLottoNumberRange(value);
+    }
+
+    private static void validateLottoNumberRange(Integer value) {
+        if (value < MIN_LOTTO_NUMBER || value > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+        }
     }
 }
