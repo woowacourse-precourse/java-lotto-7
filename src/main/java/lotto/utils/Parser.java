730df6133cda8a930ac0b4329aa0e5acd670c7ec
kkmdevel <kkm.devel@gmail.com>
2024-11-03 14:17:01 +0900

    feat(Parser): 문자열을 숫자로 만들어주는 기능 생성
    ParseStringToInt 메소드 생성

---
+++ src/main/java/lotto/utils/Parser.java
@@ -0,0 +1,12 @@
+package lotto.utils;
+
+public final class Parser {
+
+    private Parser() {
+    }
+
+    public static int parseStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+}
