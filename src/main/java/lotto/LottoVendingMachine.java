6cc72da37b7062f53e5b341f4924ecac030b5292
taehyun <xogus5070318@gmail.com>
2024-11-03 16:03:04 +0900

    feat(LottoVendingMachine): 로또 구입금액 입력기능 추가
    로또 구입금액을 입력하면, 금액에 맞춰 로또를 발행해주는 기능을 구현하였습니다.
    사용자 입장에서로또 자판기의 역할은 사용자가 지불한 금액만큼의 로또를 생성해주는 역할 하나만 수행하면 될 것이라 생각합니다.
    그래서 로또 자판기는 멤버 변수 없이, 로또 구매, 밸리디에이션 메소드로만 구성하였습니다.
    
    BREAKING CHANGE: None

--- src/main/java/lotto/LottoVendingMachine.java
+++ src/main/java/lotto/LottoVendingMachine.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoVendingMachine {
+
+    public List<Lotto> purchaseLottos(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = Console.readLine();
+        validateMoney(userInput);
+        int money = Integer.parseInt(userInput);
+        List<Lotto> lottos = new ArrayList<>();
+
+        for(int i=1;i<=(money/1000);i++){
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+
+        return lottos;
+    }
+
+    public void validateMoney(String userInput){
+
+    }
 }
