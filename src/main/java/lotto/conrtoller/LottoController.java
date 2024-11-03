d489392ea3deb0d2d11fab00c7473d7659ef7cc1
dradnats1012 <herjebi1012@gmail.com>
2024-11-04 01:03:37 +0900

    refactor: LottoController 재입력 로직 수정

--- src/main/java/lotto/conrtoller/LottoController.java
+++ src/main/java/lotto/conrtoller/LottoController.java
@@ -17,25 +17,40 @@ public class LottoController {
     private static final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
     public void run() {
+        try {
+            Money money = inputMoneyWithRetry();
+            Trial trial = new Trial(money.getMoney());
+            Lottos lottos = new Lottos(numberGenerator, trial);
+            OutputView.printLotties(lottos.getLottoNums());
+
+            WinningNumber winningNumber = inputWinningNumberWithRetry();
+            LottoChecker lottoChecker = new LottoChecker(lottos, winningNumber);
+            WinningMoney winningMoney = new WinningMoney(lottoChecker.checkLottos());
+            ReturnRate returnRate = new ReturnRate(winningMoney, money);
+
+            OutputView.printWinningDetails(lottoChecker.getResultMap());
+            OutputView.printReturnRate(returnRate.getReturnRate());
+        } catch (IllegalStateException e) {
+            OutputView.printErrorMessage(e.getMessage());
+        }
+    }
+
+    private Money inputMoneyWithRetry() {
         while (true) {
             try {
-                Money money = new Money(InputView.inputMoney());
-                Trial trial = new Trial(money.getMoney());
-                Lottos lottos = new Lottos(numberGenerator, trial);
-                OutputView.printLotties(lottos.getLottoNums());
+                return new Money(InputView.inputMoney());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 
-                WinningNumber winningNumber = new WinningNumber(InputView.inputWinningNumber(),
-                    InputView.inputBonusNumber());
-                LottoChecker lottoChecker = new LottoChecker(lottos, winningNumber);
-                WinningMoney winningMoney = new WinningMoney(lottoChecker.checkLottos());
-                ReturnRate returnRate = new ReturnRate(winningMoney, money);
-
-                OutputView.printWinningDetails(lottoChecker.getResultMap());
-                OutputView.printReturnRate(returnRate.getReturnRate());
-                break;
-
-            } catch (IllegalArgumentException | IllegalStateException e) {
-                System.out.println(e.getMessage());
+    private WinningNumber inputWinningNumberWithRetry() {
+        while (true) {
+            try {
+                return new WinningNumber(InputView.inputWinningNumber(), InputView.inputBonusNumber());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
             }
         }
     }
