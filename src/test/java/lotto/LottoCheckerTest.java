d3ec98e38ce66b76338c8fa82a7963745d7d2021
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 15:35:24 +0900

    fix(LottoCheckerTest): check메서드의 시그니처에 맞게 테스트코드 수정
    - List<Integer> 대신 Lotto를 받게 수정
    - LottoChecker 인스턴스 없이 static 메서드인 check를 바로 활용하도록 변경

--- src/test/java/lotto/LottoCheckerTest.java
+++ src/test/java/lotto/LottoCheckerTest.java
@@ -11,10 +11,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("6개 번호가 모두 일치 -> 1등")
     void check_1등_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 3, 4, 5, 6), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.FIRST);
     }
@@ -22,10 +22,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("5개 번호와 보너스 번호가 일치 -> 2등")
     void check_2등_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 3, 4, 5, 7), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.SECOND);
     }
@@ -33,10 +33,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("5개 번호만 일치 -> 3등")
     void check_3등_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 3, 4, 5, 8), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.THIRD);
     }
@@ -44,10 +44,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("4개 번호가 일치 -> 4등")
     void check_4등_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 3, 4, 8, 9), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.FOURTH);
     }
@@ -55,10 +55,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("3개 번호가 일치 -> 5등")
     void check_5등_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 3, 8, 9, 10), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.FIFTH);
     }
@@ -66,10 +66,10 @@ class LottoCheckerTest {
     @Test
     @DisplayName("2개 이하의 번호가 일치 -> 등수 NONE")
     void check_등수없음_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 8, 9, 10, 11));
         WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
-        LottoChecker checker = new LottoChecker();
 
-        Rank result = checker.check(List.of(1, 2, 8, 9, 10, 11), winningNumbers);
+        Rank result = LottoChecker.check(lotto, winningNumbers);
 
         assertThat(result).isEqualTo(Rank.NONE);
     }
