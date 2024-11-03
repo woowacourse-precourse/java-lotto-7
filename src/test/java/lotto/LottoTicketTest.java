4e3cead194171e4a80ad4516a1662f7b53eff7f2
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 02:53:13 +0900

    test(LottoTicketTest): 로또 티켓 테스트 추가 -로또번호 개수가 6개가 아니면 예외를 발생시키는지 테스트합니다. -로또 번호에 중복된 숫자가 있으면 예외를 발생시키는지 테스트합니다.

---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTicketTest {
+
+  @DisplayName("로또 번호 리스트가 6개가 아니면 예외가 발생한다.")
+  @Test
+  void 로또번호_개수가_6개가_아니면_예외_발생() {
+    assertThatThrownBy(() -> new LottoTicket(List.of(1, 2, 3, 4, 5)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("[ERROR] 로또 번호는 6개여야 합니다.");
+  }
+
+  @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+  @Test
+  void 로또번호에_중복이_있으면_예외_발생() {
+    assertThatThrownBy(() -> new LottoTicket(List.of(1, 2, 3, 4, 5, 5)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+  }
+}
