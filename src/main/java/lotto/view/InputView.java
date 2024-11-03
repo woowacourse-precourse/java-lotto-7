09b0d0a9c92c8ba8034ed2a986ef008c68ac650f
joon <khjoon372@naver.com>
2024-11-03 17:03:10 +0900

    feat(InputView): 구매할 로또 금액 입력 받는 기능

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static Integer inputLottoAmount(){
+
+        return Integer.parseInt(Console.readLine());
+    }
+
+}
