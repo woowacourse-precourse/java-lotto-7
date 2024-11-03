c0b7e7ae0f194a1b0d719fb54b5ce345a6b3f0dd
HanJun Lee <137124830+LeeHanJun00@users.noreply.github.com>
2024-11-03 17:42:06 +0900

    test: 로또번호가 1~45 사이 숫자가 아닐때 예외처리 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -13,6 +13,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 로또_번호가_1부터_45사이의_숫자가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 47, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
