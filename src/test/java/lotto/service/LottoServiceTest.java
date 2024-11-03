905eb3061e9adcac2b1b191b04a486b047093bf9
Hanjjong <tim4974@naver.com>
2024-11-03 23:41:15 +0900

    test: 당첨 번호 생성 및 보너스 번호 추가 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import lotto.model.LottoTicket;
+import lotto.model.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,18 @@ public class LottoServiceTest {
         LottoTicket lottoTicket = lottoService.createLottoTicket(purchaseMoney);
 
         assertThat(lottoTicket.getLottosCount()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 생성 및 보너스 번호 추가 테스트")
+    void createWinningLottoAndAddBonusTest() {
+        String winningNumber = "1, 2, 3, 4, 5, 6";
+        String bonusNumber = "7";
+
+        WinningLotto winningLotto = lottoService.createWinningLotto(winningNumber);
+        lottoService.addBonusNumber(winningLotto, bonusNumber);
+
+        assertThat(winningLotto.getNumbers()).containsExactlyInAnyOrder(1, 2, 3, 4, 5, 6);
+        assertThat(winningLotto.getBonusNum()).isEqualTo(7);
     }
 }
