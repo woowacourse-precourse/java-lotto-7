46a58148f7533d5126d616a12b9b1d12d7211a91
badgelatte <saucy5939@gmail.com>
2024-11-04 02:20:46 +0900

    1000원 단위의 금액이 아닐 시 예외처리 구현
    1000원 단위가 아닌 금액 입력으로 IllegalArgumentException을 띄움

--- src/main/java/lotto/LottoTicketFactory.java
+++ src/main/java/lotto/LottoTicketFactory.java
@@ -3,7 +3,11 @@ package lotto;
 public class LottoTicketFactory {
 
     public static int getLottoTicketCount(String money) {
-        int moneyInt= Integer.parseInt(money);
+        int moneyInt = Integer.parseInt(money);
+
+        if (moneyInt % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액이 아닙니다.");
+        }
         return moneyInt / 1000;
     }
 }
