039e2ef5807b16809da070ba047401b6749f4dd3
송지웅 <thdwldnd7@dgu.ac.kr>
2024-11-03 19:03:09 +0900

    feat: 로또 당첨 객체 (enum class) 추가 - 조건 (일치 개수, 보너스 매칭 여부) - 금액

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,23 @@
+﻿package lotto;
+
+public enum LottoResult {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000);
+
+    private int match;
+    private boolean bonusMatch;
+    private int winningMoney;
+
+    LottoResult(int match, boolean bonusMatch, int winningMoney) {
+        this.match = match;
+        this.bonusMatch = bonusMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getWinningMoney() {
+        return this.winningMoney;
+    }
+};
