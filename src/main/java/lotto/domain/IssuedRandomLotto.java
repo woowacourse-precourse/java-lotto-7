f2caded2b25336b02c74be11df55487e28cdfb27
ppparkta <pppppark2@gmail.com>
2024-11-03 23:42:20 +0900

    feat(InputView): input, validator 추가
    추가사항
    - 구입금액을 입력받고 처리하는 getValidPurchaseAmount 추가
    - 당첨번호를 입력받고 처리하는 getValidWinningNumbers 추가
    - 보너스번호를 입력받고 처리하는 getValidBonusNumber 추가
    - 각 사용자입력 메서드에 validator 연결

--- src/main/java/lotto/domain/IssuedRandomLotto.java
+++ src/main/java/lotto/domain/IssuedRandomLotto.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import lotto.common.LottoConfig;
 import lotto.common.RandomNumberGenerator;
+import lotto.common.validator.IssuedLottoValidator;
 
 public class IssuedRandomLotto implements IssuedLotto {
     private final RandomNumberGenerator randomNumberGenerator;
@@ -12,14 +13,14 @@ public class IssuedRandomLotto implements IssuedLotto {
     private List<Lotto> issuedLottos = new ArrayList<>();
 
     public IssuedRandomLotto(RandomNumberGenerator randomNumberGenerator, int lottoPurchaseAmount) {
-        validate(lottoPurchaseAmount);
+        IssuedLottoValidator.validate(lottoPurchaseAmount);
         this.randomNumberGenerator = randomNumberGenerator;
         this.lottoPurchaseAmount = lottoPurchaseAmount;
     }
 
     public IssuedRandomLotto(RandomNumberGenerator randomNumberGenerator, List<Lotto> issuedLottos,
                              int lottoPurchaseAmount) {
-        validate(lottoPurchaseAmount);
+        IssuedLottoValidator.validate(lottoPurchaseAmount);
         this.randomNumberGenerator = randomNumberGenerator;
         this.issuedLottos = new ArrayList<>(issuedLottos);
         this.lottoPurchaseAmount = lottoPurchaseAmount;
@@ -50,14 +51,5 @@ public class IssuedRandomLotto implements IssuedLotto {
 
     private List<Integer> generateRandomLottoNumbers() {
         return randomNumberGenerator.generateUniqueRandomNumber();
-    }
-
-    private void validate(int lottoPurchaseAmount) {
-        if (lottoPurchaseAmount % LottoConfig.LOTTO_PRICE.getValue() != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해주세요.");
-        }
-        if (lottoPurchaseAmount > LottoConfig.LOTTO_PURCHASE_LIMIT.getValue()) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 10,000원 이하로 입력해주세요.");
-        }
     }
 }
--- src/main/java/lotto/presentation/view/InputView.java
+++ src/main/java/lotto/presentation/view/InputView.java
@@ -3,27 +3,30 @@ package lotto.presentation.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import lotto.common.validator.IssuedLottoValidator;
+import lotto.common.validator.LottoResultValidator;
+import lotto.common.validator.LottoValidator;
 
 public class InputView {
     public int getValidPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         while (true) {
             try {
-                System.out.println("구입금액을 입력해 주세요.");
                 String inputPurchaseAmount = Console.readLine().replace(" ", "");
                 validatePurchaseAmount(inputPurchaseAmount);
                 return Integer.parseInt(inputPurchaseAmount);
             } catch (NumberFormatException e) {
-                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+                System.out.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
             } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
 
     public List<Integer> getValidWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         while (true) {
             try {
-                System.out.println("당첨 번호를 입력해 주세요.");
                 String inputWinningNumbers = Console.readLine().replace(" ", "");
                 String[] splitInputWinningNumbers = inputWinningNumbers.split(",");
                 validateWinningNumbers(splitInputWinningNumbers);
@@ -31,34 +34,52 @@ public class InputView {
                         Integer.parseInt(winningNumber)
                 ).toList();
             } catch (NumberFormatException e) {
-                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+                System.out.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
             } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
 
-    public int getValidBonusNumber() {
+    public int getValidBonusNumber(List<Integer> winningNumbers) {
+        System.out.println("보너스 번호를 입력해 주세요.");
         while (true) {
             try {
-                System.out.println("보너스 번호를 입력해 주세요.");
                 String inputBonusNumber = Console.readLine().replace(" ", "");
-                validateBonusNumber(inputBonusNumber);
+                validateBonusNumber(inputBonusNumber, winningNumbers);
                 return Integer.parseInt(inputBonusNumber);
             } catch (NumberFormatException e) {
-                System.err.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
+                System.out.println("[ERROR] 숫자 형식이 잘못되었습니다. 다시 입력해주세요.");
             } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
 
     private void validatePurchaseAmount(String inputPurchaseAmount) {
+        isDigit(inputPurchaseAmount);
+        int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
+        IssuedLottoValidator.validate(purchaseAmount);
     }
 
     private void validateWinningNumbers(String[] splitInputWinningNumbers) {
+        for (String winningNumber : splitInputWinningNumbers) {
+            isDigit(winningNumber);
+        }
+        List<Integer> winningNumbers = Arrays.stream(splitInputWinningNumbers)
+                .map(winningNumber -> Integer.parseInt(winningNumber)).toList();
+        LottoValidator.validate(winningNumbers);
     }
 
-    private void validateBonusNumber(String inputBonusNumber) {
+    private void validateBonusNumber(String inputBonusNumber, List<Integer> winningNumbers) {
+        isDigit(inputBonusNumber);
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
+        LottoResultValidator.bonusNumberValidate(bonusNumber, winningNumbers);
+    }
+
+    private void isDigit(String inputPurchaseAmount) {
+        if (!inputPurchaseAmount.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("[ERROR] 값은 숫자로 입력해주세요.");
+        }
     }
 }
