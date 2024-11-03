682c659dccd1d49329e9810a8f1a237c18794845
superbigwave <zlzhznzk@gmail.com>
2024-11-03 14:12:29 +0900

    <feat> 티켓 구매  1) "구입금액을 입력해 주세요." 메세지 출력  2) 콘솔로 구매금액 입력 기능 추가  3) 입력 값 정수 확인 기능 추가  4) 금액 개수로 변환 및 "N개를 구매했습니다." 메세지 출력  5) 구매 갯수만큼 로또번호 랜덤 생성 및 출력
    <feat> 당첨, 보너스번호
     1) 당첨 번호 입력 기능 추가
     2) 예외점검(6자리, 중복번호,1~45숫자)
     3) 보너스 번호 입력 기능 추가
     4) 예외점검(당첨번호 중복)
    
     <feat> 당첨 통계, 수익률
     1) 당첨번호(보너스번호) 확인 가능한 메소드 작성
     2) 당첨숫자별 통계 변환
     3) 금액 환산 및 수익률 계산

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,150 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // #1 티켓 구매
+        int ticketCount = promptTicketCount("구입금액을 입력해 주세요.");
+        System.out.println("\n" + ticketCount + "개를 구매했습니다.");
+        // #2 구매 로또 번호 생성 및 당첨,보너스 번호 입력
+        List<List<Integer>> allLottoNumbers = printLottoNumbers(ticketCount);
+        Lotto winnerNumbers = Lotto.inputWinnerLotto("\n당첨 번호를 입력해 주세요.");
+        int bonusNumber = promptBonusNumber("보너스 번호를 입력해 주세요.",winnerNumbers.getNumbers());
+        // #3 당첨통계
+        int[] prizeCount = calculatePrizeCount(allLottoNumbers, winnerNumbers.getNumbers(), bonusNumber);
+        printPrizeStatistics(prizeCount);
+        // #4 수익률
+        double rateOfReturn = calculateRateOfReturn(prizeCount, ticketCount);
+        System.out.println("총 수익률은 " + roundToTwoDecimalPlaces(rateOfReturn) + "%입니다.");
+    }
+
+    // 티켓 금액 필터링 메소드
+    private static int promptTicketCount(String message) {
+        String cashInput;
+        while (true) {
+            try {
+                System.out.println(message);
+                cashInput = Console.readLine(); // 구매입력 콘솔로 입력받음
+                cashInputCheck(cashInput); // 정수 유효성 검사 메소드
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // 예외 메시지 출력 후 재입력
+            }
+        }
+        return Integer.parseInt(cashInput) / 1000; // cashInput을 정수로 변환 후 장수 변환
+    }
+
+    // 티켓 금액 정수 유효성 검사
+    private static void cashInputCheck(String cahsInput) {
+        for (char cI : cahsInput.toCharArray()) {
+            if (!Character.isDigit(cI)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 다른 숫자여야 합니다.");
+            }
+        }
+    }
+
+    // 로또 번호 생성 및 출력 메소드
+    private static List<List<Integer>> printLottoNumbers(int ticketCount) {
+        List<List<Integer>> allLottoNumbers = new ArrayList<>(); // 모든 로또 번호 저장
+        for (int i = 0; i < ticketCount; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6); // 랜덤으로 로또 넘버 생성
+            allLottoNumbers.add(lottoNumbers); // 각 티켓을 저장
+            System.out.println(lottoNumbers); // 로또 번호 출력
+        }
+        return allLottoNumbers;
+    }
+
+    // 보너스 번호 입력 메소드
+    private static int promptBonusNumber(String message, List<Integer> winnerNumbers) {
+        int bonusNumber;
+
+        while (true) {
+            try {
+                System.out.println(message);
+                String lottoBonusNumberInput = Console.readLine(); // 보너스 번호 입력
+                bonusNumber = Integer.parseInt(lottoBonusNumberInput);
+                bonusNumberCheck(bonusNumber, winnerNumbers); // 유효성 검사 메소드
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // 예외 메시지 출력 후 재입력
+            }
+        }
+        return bonusNumber;
+    }
+
+    // 보너스 번호 유효성 검사
+    private static void bonusNumberCheck(int bonusNumber, List<Integer> winnerNumbers) {
+        if (winnerNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 다른 숫자여야 합니다.");
+        }
+    }
+
+
+    // 당첨 여부 확인 가능한 메소드
+    private static int[] countWinnerNumber(List<Integer> winnerNumbers, List<Integer> lottoNumbers, int bonusNumber) {
+        int count = 0;
+        int bonusCheck = 0;
+
+        for (Integer winnerNumber : winnerNumbers) { // winnerNumbers의 각 숫자를 순회
+            if (lottoNumbers.contains(winnerNumber)) { // lottoNumbers에 해당 숫자가 포함되어 있는지 확인
+                count++; // 포함되어 있으면 카운트 증가
+            }
+        }
+        if (count == 5 && lottoNumbers.contains(bonusNumber)) { // 보너스넘버가 있는지도 확인
+            bonusCheck = 1;
+        }
+        return new int[]{count, bonusCheck}; // 일치하는 숫자 및 보너스의 개수 반환
+    }
+
+    // 당첨 통계 저장을 위한 메소드
+    private static int[] calculatePrizeCount(List<List<Integer>> allLottoNumbers, List<Integer> winnerNumbers, int bonusNumber) {
+        int[] prizeCount = new int[8]; // 0~6개 일치 카운트 / 7번 인덱스는 보너스카운트
+        int bonusCount = 0;
+
+        for (List<Integer> lottoNumbers : allLottoNumbers) {
+            int[] matchingCountArray = countWinnerNumber(winnerNumbers, lottoNumbers, bonusNumber);
+            int matchingCount = matchingCountArray[0]; // count
+            prizeCount[matchingCount]++; // 일치하는 개수에 따라 카운트 증가
+            if (matchingCount == 5 && matchingCountArray[1] == 1) {
+                bonusCount++;
+                prizeCount[5]--; // 5개+보너스 일치할 시 5개 카운트 감소
+            }
+        }
+        prizeCount[7] = bonusCount;
+        return prizeCount; // 최종 통계 배열 반환
+    }
+
+    // 통계 출력부분 메소드
+    private static void printPrizeStatistics(int[] prizeCount) {
+        System.out.println("\n당첨 통계\n" + "---" );  // #5 당첨 통계
+        System.out.println("3개 일치 (5,000원) - " + prizeCount[3] + "개");  // 통계 출력
+        System.out.println("4개 일치 (50,000원) - " + prizeCount[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + prizeCount[5] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + prizeCount[7] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + prizeCount[6] + "개");
+    }
+
+    // 소수점 둘째 자리 반올림 메소드
+    private static double roundToTwoDecimalPlaces(double value) {
+        return new BigDecimal(value).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    // 금액 계산 메소드
+    private static double calculateRateOfReturn(int[] prizeCount, int ticketCount) {
+        double prizeSum = (
+                5000 * prizeCount[3]
+                        + 50000 * prizeCount[4]
+                        + 1500000 * prizeCount[5]
+                        + 30000000 * prizeCount[7]
+                        + 2000000000 * prizeCount[6]);
+        double rateOfReturn = (prizeSum / ticketCount) / 10;
+        return rateOfReturn;
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,74 @@
 package lotto;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkEachNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private static void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 1~45 사이이 숫자인지 유효성 검사
+    private static void validateNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    // 6개 번호가 모두 다른지 유효성 검사
+    private static void checkEachNumbers(List<Integer> numbers) {
+        HashSet<Integer> eachNumbers = new HashSet<Integer>(numbers);
+        if (eachNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 각각 다른 숫자여야 합니다.");
+        }
+    }
+
+    // 당첨 로또 번호 입력 및 유효 판단
+    public static Lotto inputWinnerLotto(String message) {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 6) {
+            try {
+                System.out.println(message);
+                String numberInput = Console.readLine();
+                numbers = parseNumbers(numberInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return new Lotto(numbers);
+    }
+
+    // 입력된 번호 문자열을 정수열로 변환
+    private static List<Integer> parseNumbers(String input) {
+        String[] lottoWinnerNumberStringArray = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String numberString : lottoWinnerNumberStringArray) {
+            int number = Integer.parseInt(numberString.trim());
+            validateNumber(number);
+            numbers.add(number);
+        }
+
+        validate(numbers);
+        checkEachNumbers(numbers);
+
+        return numbers;
+    }
+
+    // 메인에서 당첨번호를 불러와 사용하기위해 getNumbers 추가
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
