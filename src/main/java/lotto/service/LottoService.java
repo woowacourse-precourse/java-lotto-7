18ac47ab9f3f883d2c8a2be8c342230b07f0e0e5
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 22:15:14 +0900

    feat: 사용자가 입력한 당첨 번호와 얼마나 일치하는지 확인 후, 당첨 등수를 반환하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,8 +2,12 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.Prize;
+import lotto.dto.LottoResponse;
+import lotto.dto.PrizeResponse;
 import lotto.repository.LottoRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
@@ -11,6 +15,20 @@ public class LottoService {
 
     public LottoService(LottoRepository lottoRepository) {
         this.lottoRepository = lottoRepository;
+    }
+
+    private PrizeResponse calculatePrize(Lotto winningLotto, int bonusNumber, List<Integer> numbers) {
+        int matchingNumberCount = (int) winningLotto.getNumbers().stream()
+                .filter(numbers::contains)
+                .count();
+        boolean containsBonusNumber = numbers.contains(bonusNumber);
+
+        if (containsBonusNumber) {
+            matchingNumberCount++;
+        }
+        Prize prize = Prize.findBy(matchingNumberCount, containsBonusNumber);
+
+        return new PrizeResponse(prize, containsBonusNumber);
     }
 
     public void saveLottos(List<Lotto> lottos) {
