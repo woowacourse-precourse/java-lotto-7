3e83bc0006d180329fba2634444f52c824ba59ea
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 06:02:18 +0900

    feat: 수익률 계산하는 기능 추가

--- src/main/java/lotto/util/EarningCalculator.java
+++ src/main/java/lotto/util/EarningCalculator.java
@@ -1,2 +1,13 @@
-package lotto.util;public class EarningCalculator {
+package lotto.util;
+
+public abstract class EarningCalculator {
+
+    private EarningCalculator() {
+    }
+
+    public static String calculateEarningsRate(int winningAmount, int purchaseAmount) {
+        double earningsRate = (double) winningAmount * 100 / purchaseAmount;
+        double roundedEarningsRate = Math.round(earningsRate * 10) / 10.0;
+        return String.format("%,.1f", roundedEarningsRate);
+    }
 }
