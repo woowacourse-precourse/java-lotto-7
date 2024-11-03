00131b9923d0214013ec455e8b3c86eb9740012d
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-11-03 12:53:07 +0900

    feat: enum 클래스 적용, 금액에 따른 로또 개수 판단 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Constants.LottoRule;
 
 public class Application {
     public static List<Integer> selectRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange( Constants.MIN_NUMBER, Constants.MAX_NUMBER, Constants.NUM_COUNT );
+        return Randoms.pickUniqueNumbersInRange( LottoRule.MIN_NUMBER.getNumber(), LottoRule.MAX_NUMBER.getNumber(), LottoRule.NUM_AMOUNT.getNumber() );
     }
     public static Lotto publishLotto(){
         return new Lotto( selectRandomNumbers() );
+    }
+
+    public static int checkAmount( int price ){
+        if( price < 1000 || price % 1000 != 0 ) throw new IllegalArgumentException();
+        return price / 1000;
     }
 
     public static void main(String[] args) {
--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -2,7 +2,16 @@ package lotto;
 
 public class Constants {
 
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
-    public static final int NUM_COUNT = 6;
+    public enum LottoRule {
+        MIN_NUMBER(1),
+        MAX_NUMBER(45),
+        NUM_AMOUNT(6);
+
+        private final int number;
+        LottoRule( int number ){
+            this.number = number;
+        }
+        public int getNumber(){ return this.number; }
+
+    }
 }
