63321d3f5b93b1cbdcde368a1d0de7ef7d99494e
mins-git <m2insapp@gmail.com>
2024-11-03 15:05:11 +0900

    feat(LottoGameController): 로또 게임 컨트롤러 구현
      - 게임 진행 play() 로직 구현

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,17 +1,18 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoGameController {
     private final LottoService lottoService;
-    private final LottoInputView LottoInputView;
-    private final LottoOutputView LottoOutputView;
+    private final LottoInputView lottoInputView;
+    private final LottoOutputView lottoOutputView;
 
 
-    public LottoGameController(lotto.LottoInputView lottoInputView, LottoService lottoService, lotto.LottoOutputView lottoOutputView) {
-        LottoInputView = lottoInputView;
+    public LottoGameController(LottoInputView lottoInputView, LottoService lottoService, LottoOutputView lottoOutputView) {
+        this.lottoInputView = lottoInputView;
         this.lottoService = lottoService;
-        LottoOutputView = lottoOutputView;
+        this.lottoOutputView = lottoOutputView;
     }
 
     public void play(){
@@ -23,12 +24,40 @@ public class LottoGameController {
 
     private List<Lotto> purchaseLottos(){
         try {
-            int amount = LottoInputView.readPurchaseAmount();
+            int amount = lottoInputView.readPurchaseAmount();
             List<Lotto> lottos = lottoService.purchaseLotto(amount);
-            LottoOutputView.printPurchaseResult(lottos); // 출력필요.
+            lottoOutputView.printPurchaseResult(lottos);
             return lottos;
         } catch (IllegalArgumentException e){
-            LottoOutputView.print // 에러 표시 필요.
+            lottoOutputView.printError(e.getMessage());
+            return purchaseLottos();
         }
     }
+
+    private Lotto getWinningLotto() {
+        try {
+            return lottoInputView.readWinningNumbers();
+        } catch (IllegalArgumentException e) {
+            lottoOutputView.printError(e.getMessage());
+            return getWinningLotto();
+        }
+    }
+
+    private int getBonusNumber(Lotto winningLotto) {
+        try {
+            return lottoInputView.readBonusNumber(winningLotto);
+        } catch (IllegalArgumentException e) {
+            lottoOutputView.printError(e.getMessage());
+            return getBonusNumber(winningLotto);
+        }
+    }
+
+    private void showResult(List<Lotto> lottos, Lotto winningLotto, int bonusNumber) {
+        Map<WinningRank, Integer> statistics =
+                lottoService.calculateWinningStatistics(lottos, winningLotto, bonusNumber);
+        double returnRate =
+                lottoService.calculateReturnRate(statistics, lottos.size() * 1000);
+        lottoOutputView.printWinningStatistics(statistics, returnRate);
+    }
+
 }
