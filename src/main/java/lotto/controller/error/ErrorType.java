c3b1d4a326bc06dac9607facbb2194b60bc1ef79
jeyong <118044367+joon6093@users.noreply.github.com>
2024-11-03 22:13:10 +0900

    feat(controller): 로또 게임의 오류 메시지 타입 정의
    - 다양한 입력 검증을 위한 오류 메시지 타입 정의
    - 모든 오류 메시지에 일관된 [ERROR] 접두사를 적용

---
+++ src/main/java/lotto/controller/error/ErrorType.java
@@ -0,0 +1,27 @@
+package lotto.controller.error;
+
+public enum ErrorType {
+
+    NULL_INPUT("입력이 null일 수 없습니다."),
+    INVALID_DELIMITER("로또 번호 구분자 형식이 잘못되었습니다."),
+    NULL_OR_BLANK_INPUT("입력이 null이거나 빈 문자열일 수 없습니다."),
+    INVALID_NUMBER_FORMAT("입력이 int 범위를 벗어났거나 숫자가 아닙니다."),
+    NEGATIVE_AMOUNT("구입 금액이 음수가 될 수 없습니다."),
+    INVALID_UNIT_AMOUNT("구입 금액은 1,000원 단위로 입력해야 합니다."),
+    INVALID_LOTTO_COUNT("로또 번호는 6개여야 합니다."),
+    DUPLICATE_NUMBER("중복된 번호가 존재합니다."),
+    OUT_OF_RANGE("로또 번호는 1부터 45 사이의 값이어야 합니다."),
+    ;
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    ErrorType(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX + message;
+    }
+}
