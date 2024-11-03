786caf5767243ab68742556a343e5d93c3d7821b
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-03 20:57:53 +0900

    feat: Lotto validate 메서드 수정 시 사용하는 ErrorMessage

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
     EMPTY_INPUT("입력값이 비어 있습니다."),
     NON_NUMBER("int 범위의 정수값으로 입력해야 합니다."),
     NON_POSITIVE_NUMBER("양수값으로 입력해야 합니다."),
-    NON_DIVISIBLE_BY_THOUSAND("구입 금액은 1,000 단위여야 합니다.");
+    NON_DIVISIBLE_BY_THOUSAND("구입 금액은 1,000 단위여야 합니다."),
+    LOTTO_NUMBERS_COUNT("로또 번호는 6개여야 합니다."),
+    DUPLICATE_WINNING_NUMBERS("로또 번호는 중복될 수 없습니다.");
 
     private static final String ERROR_START = "[ERROR] ";
     private final String message;
