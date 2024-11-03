b3506b1efec956758ee15253462a1ef4511beaf3
kkmdevel <kkm.devel@gmail.com>
2024-11-03 15:20:19 +0900

    feat(PaymentController): 결제 처리 담당 컨트롤러 구현
    - 결제 금액 입력 및 유효성 검증을 위한 PaymentController 생성
    - 사용자가 입력한 금액을 기반으로 Payment 객체 생성
    - 잘못된 금액 입력 시 예외 처리 및 에러 메시지 출력

---
+++ src/main/java/lotto/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package lotto.controller;
+
+import lotto.domain.Payment;
+import lotto.exception.PaymentException;
+import lotto.utils.Parser;
+import lotto.view.InputReader;
+import lotto.view.OutputView;
+
+public class PaymentController {
+
+    public PaymentController() {
+    }
+
+    public Payment processPayment() {
+        while (true) {
+            try {
+                int money = getPaymentAmount();
+                return Payment.of(money);
+            } catch (PaymentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private int getPaymentAmount() {
+        OutputView.printPaymentMessage();
+        String moneyInput = InputReader.readLine();
+        return Parser.parseStringToInt(moneyInput);
+    }
+}
