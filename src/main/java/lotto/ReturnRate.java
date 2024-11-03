5c665186bdd7c0bb602f5887eebf886b734f9a0b
hongkikii <hyangmeeh@icloud.com>
2024-11-04 05:17:41 +0900

    refactor(ReturnRate): 상수 분리

--- src/main/java/lotto/ReturnRate.java
+++ src/main/java/lotto/ReturnRate.java
@@ -3,16 +3,18 @@ package lotto;
 import java.text.DecimalFormat;
 
 public class ReturnRate {
+    private static final int PERCENTAGE_MULTIPLIER = 100;
+    private static final String RETURN_RATE_FORMAT = "#,##0.0";
+    private static final String RETURN_RATE_UNIT = "%";
 
     public String calculate(Result result, Payment payment) {
         long totalPrize = result.calculateTotalPrize();
-        double returnRate = ((double) totalPrize / payment.get()) * 100;
-
+        double returnRate = ((double) totalPrize / payment.get()) * PERCENTAGE_MULTIPLIER;
         return printFormatted(returnRate);
     }
 
     private String printFormatted(double returnRate) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
-        return decimalFormat.format(returnRate) + "%";
+        DecimalFormat decimalFormat = new DecimalFormat(RETURN_RATE_FORMAT);
+        return decimalFormat.format(returnRate) + RETURN_RATE_UNIT;
     }
 }
