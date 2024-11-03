43710236623ae2b74552630167820a9eb7bed6fb
hojae <ghwo336@naver.com>
2024-11-04 02:04:01 +0900

    feat: ParsingService 구현

--- src/main/java/lotto/Service/ParsingService.java
+++ src/main/java/lotto/Service/ParsingService.java
@@ -1,5 +1,13 @@
 package lotto.Service;
 
 public class ParsingService {
+    private final int money;
 
+    public ParsingService(String money) {
+        this.money=Integer.parseInt(money);
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
