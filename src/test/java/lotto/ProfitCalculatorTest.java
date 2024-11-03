f1944c0741ff3bf431eccba18b4845a753458116
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 03:39:06 +0900

    test(ProfitCalculatorTest): 수익률 계산 테스트 추가 -총 투자 금액과 당첨 금액을 통해 수익률 계산을 테스트합니다.

---
+++ src/test/java/lotto/ProfitCalculatorTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfitCalculatorTest {
+
+  @DisplayName("총 투자 금액과 당첨 금액을 통해 수익률을 계산한다.")
+  @Test
+  void 수익률_계산() {
+    int totalInvestment = 5000;
+    int totalEarnings = 2500;
+
+    double profitRate = ProfitCalculator.calculateProfitRate(totalInvestment, totalEarnings);
+    assertThat(profitRate).isEqualTo(50.0);
+  }
+}
