f99a207c25a52e6e74496faf2215557ea584e060
cherry031 <20203093@kookmin.ac.kr>
2024-11-04 07:56:06 +0900

    style: 구입 최소, 최대 금액과 로또 금액 상수화

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,6 +3,10 @@ package lotto.domain;
 import lotto.exception.MoneyExceptionType;
 
 public class Money {
+    private static final int MINIMUM_MONEY = 1000;
+    private static final int MAXIMUM_MONEY = 100000;
+    private static final int LOTTO_PRIZE = 1000;
+
     private final int money;
 
     public Money(int money) {
@@ -11,8 +15,12 @@ public class Money {
     }
 
     private void validate(int money) throws IllegalArgumentException {
-        if (money < 1000 || money > 100000 || money % 1000 != 0) {
+        if (money < MINIMUM_MONEY || money > MAXIMUM_MONEY || money % LOTTO_PRIZE != 0) {
             throw new IllegalArgumentException(MoneyExceptionType.OUT_OF_RANGE_MONEY.getMessage());
         }
+    }
+
+    public int getMoney() {
+        return money;
     }
 }
