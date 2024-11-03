49491903fc821099f18e102c796f3fbc3931980b
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-03 15:07:56 +0900

    feat : WinningStatus enum 생성

---
+++ src/main/java/lotto/WinningStatus.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum WinningStatus {
+
+    first("6개 일치", 2000000000),
+    second("5개 일치, 보너스 볼 일치", 30000000),
+    third("5개 일치", 1500000),
+    fourth("4개 일치", 50000),
+    fifth("3개 일치", 5000);
+
+    private String numberMatchInfo;
+    private int winningMoney;
+
+    WinningStatus(String numberMatchInfo, int winningMoney) {
+        this.numberMatchInfo = numberMatchInfo;
+        this.winningMoney = winningMoney;
+    }
+
+}
