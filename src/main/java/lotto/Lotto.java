5eadc8463754f224e15e72c578539a0708502be1
sinyebin <dpqls0625@naver.com>
2024-11-03 22:55:55 +0900

    feat: 발급된 번호가 6개인지

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,16 +3,22 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
+    private static final String IS_NOT_LOTTO_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLotto(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateLotto(List<Integer> numbers) {
+        validateLottoSetSize(numbers);
+    }
+
+    private void validateLottoSetSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
         }
     }
 
