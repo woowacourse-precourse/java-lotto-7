8e262ec87914da3ef3436c7b31ce258f1bcf2516
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 00:28:46 +0900

    feat(WinnerLottoValidator): 로또 당첨 번호 검증 로직 추가
    - 로또 번호의 개수, 범위, 중복성 검증 메서드 추가
    - 보너스 번호가 당첨 번호에 중복되지 않도록 확인하는 로직 추가
    - 예외 발생 시 사용자 친화적인 메시지 제공

---
+++ src/main/java/lotto/validator/WinnerLottoValidator.java
@@ -0,0 +1,37 @@
+package lotto.validator;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+
+import java.util.List;
+
+public class WinnerLottoValidator {
+
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public void bonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw LottoException.from(ErrorMessage.BONUS_NUMBER_DUPLICATES_WITH_WINNING_NUMBERS);
+        }
+    }
+
+    public void numberCount(String[] numberStrings) {
+        if (numberStrings.length != LOTTO_NUMBER_COUNT) {
+            throw LottoException.from(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX);
+        }
+    }
+
+    public void numberRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw LottoException.from(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45);
+        }
+    }
+
+    public void checkForDuplicate(List<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
+            throw LottoException.from(ErrorMessage.LOTTO_NUMBERS_MUST_NOT_DUPLICATE);
+        }
+    }
+}
