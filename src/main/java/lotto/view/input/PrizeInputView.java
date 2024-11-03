bd8cb4143a6da520c2335171378018049a2c3907
plan11plan <tjtfy7028@gmail.com>
2024-11-03 13:55:13 +0900

    feat(PrizeInputView): 콘솔에 당첨 번호 입력 받기 기능 구현
    - 쉼표로 구분된 당첨 번호 입력 받기
    - 입력값 공백 제거
    - 숫자 리스트로 변환

--- src/main/java/lotto/view/input/PrizeInputView.java
+++ src/main/java/lotto/view/input/PrizeInputView.java
@@ -1,5 +1,12 @@
 package lotto.view.input;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.util.StringConverter.split;
+import static lotto.util.StringConverter.toInts;
+import static lotto.util.StringConverter.trim;
+
+import java.util.List;
+
 public class PrizeInputView {
 
     private final String INPUT_PRIZE_NUMBER = "당첨 번호를 입력해 주세요.";
@@ -7,6 +14,7 @@ public class PrizeInputView {
 
     public void initialize() {
         System.out.println(INPUT_PRIZE_NUMBER);
+        List<Integer> prizeNumber = toInts(split(trim(readLine())));
 
     }
 
