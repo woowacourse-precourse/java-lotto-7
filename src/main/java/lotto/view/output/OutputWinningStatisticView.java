f1dfbdde49e2260248fd671516de1c05d791c111
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 23:03:18 +0900

    :sparkles: feat(OutputWinningStatisticView): 당첨 통계를 출력하는 기능 제공

--- src/main/java/lotto/view/output/OutputWinningStatisticView.java
+++ src/main/java/lotto/view/output/OutputWinningStatisticView.java
@@ -1,4 +1,34 @@
 package lotto.view.output;
 
+import static lotto.handler.OutputHandler.OUTPUT_LOTTO_PROFIT;
+import static lotto.handler.OutputHandler.OUTPUT_LOTTO_RANK;
+import static lotto.handler.OutputHandler.OUTPUT_LOTTO_RANK_BONUS;
+import static lotto.handler.OutputHandler.OUTPUT_WINNING_STATISTIC;
+
+import java.util.Map;
+import lotto.domain.LottoProfit;
+import lotto.domain.LottoRank;
+import lotto.domain.Rank;
+
 public class OutputWinningStatisticView {
+    public void showWinningStatistic(LottoRank lottoRank, LottoProfit lottoProfit) {
+        System.out.println(OUTPUT_WINNING_STATISTIC);
+        showLottoRank(lottoRank);
+        showLottoProfit(lottoProfit);
+    }
+
+    private void showLottoRank(LottoRank lottoRank) {
+        Map<Rank, Integer> rankCounts = lottoRank.getLottoRank();
+
+        for (Rank rank : rankCounts.keySet()) {
+            if (rank == Rank.SECOND) {
+                System.out.printf(OUTPUT_LOTTO_RANK_BONUS, rank.getMatchCount(), rank.getReward(), rankCounts.getOrDefault(rank, 0));
+            }
+            System.out.printf(OUTPUT_LOTTO_RANK, rank.getMatchCount(), rank.getReward(), rankCounts.getOrDefault(rank, 0));
+        }
+    }
+
+    private void showLottoProfit(LottoProfit lottoProfit) {
+        System.out.printf(OUTPUT_LOTTO_PROFIT, lottoProfit.getLottoProfit());
+    }
 }
