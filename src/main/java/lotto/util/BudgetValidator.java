e410c9791d36e93fb1cf548376c4dc5157048fbc
yunjeooong <yunjeongssu@gmail.com>
2024-11-03 14:08:41 +0900

    feat(BudgetValidator): 입력값 공백 제거 추가

--- src/main/java/lotto/util/BudgetValidator.java
+++ src/main/java/lotto/util/BudgetValidator.java
@@ -1,5 +1,13 @@
 package lotto.util;
 
-public class BudgetValidator {
+public class BudgetValidator implements Validator {
+
+    @Override
+    public void validate(String input) throws IllegalArgumentException {
+        String budget = removeSpace(input);
+    }
+    private static String removeSpace(String input) {
+        return input.replaceAll(" ", "");
+    }
 
 }
