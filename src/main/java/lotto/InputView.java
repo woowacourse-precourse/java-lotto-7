0e313453502d8cb780e0b504680f93ac844117a7
seoys0910 <seoys0910@naver.com>
2024-11-03 18:22:59 +0900

    feat : 입력받은 값이 숫자인지 검증하는 로직 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -10,12 +10,19 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         String value = Console.readLine();
         validateEmpty(value);
+        validateNumber(value);
         return Integer.parseInt(value);
     }
 
     private static void validateEmpty(String input) {
         if (input.isEmpty() || input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 빈 값이 입력되었습니다.");
+        }
+    }
+
+    private static void validateNumber(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 양수를 입력해주세요.");
         }
     }
 }
