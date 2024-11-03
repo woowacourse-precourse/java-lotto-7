5b9cca92a637e9baec341c7db378ae9244d863ad
SeongbinPark <hyucse22@gmail.com>
2024-11-03 14:20:21 +0900

    test(BonusNumberTest): 보너스 번호가 1~45 범위를 벗어나는 경우 예외가 발생한다.

---
+++ src/test/java/lotto/unit/BonusNumberTest.java
@@ -0,0 +1,23 @@
+package lotto.unit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.constants.ErrorMessages;
+import lotto.domain.BonusNumber;
+import lotto.domain.WinningNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @Test
+    @DisplayName("보너스 번호가 1~45 범위를 벗어나는 경우 예외가 발생한다.")
+    void bonusNumberOutOfRange() {
+        String input = "1,2,3,4,5,6";
+        WinningNumbers winningNumbers = new WinningNumbers(input);
+        int bonusNumber = 0;
+        assertThatThrownBy(() -> new BonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE);
+    }
+}
