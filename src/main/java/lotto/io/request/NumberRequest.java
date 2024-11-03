a2ba8dcce0ace9e4f2b9ffa0a54e5621570ee4c0
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-11-04 01:39:41 +0900

    refactor(NumberRequest): 검증 메서드를 기능별로 분리

--- src/main/java/lotto/io/request/NumberRequest.java
+++ src/main/java/lotto/io/request/NumberRequest.java
@@ -7,13 +7,17 @@ public record NumberRequest(String number) {
     private static final String NUMBER_PATTERN = "\\d+";
 
     public NumberRequest {
+        validateEmpty(number);
         validateNumber(number);
     }
 
-    private void validateNumber(String number) {
+    private void validateEmpty(String number) {
         if (number == null || number.isBlank()) {
             throw new IllegalArgumentException(ExceptionMessages.EMPTY_INPUT.getMessage());
         }
+    }
+
+    private void validateNumber(String number) {
         if (!number.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException(ExceptionMessages.INVALID_CHARACTER.getMessage());
         }
