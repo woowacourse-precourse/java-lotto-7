19d5cb502dd3d04bf2b7ddb7297f58eccb7b940a
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-03 23:40:55 +0900

    refactor: 컨트롤러 사용하여 main문 동작하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        LottoSystem lottoSystem = new LottoSystem();
-        lottoSystem.run();
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
