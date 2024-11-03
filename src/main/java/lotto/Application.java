7c5a7e648f511008ff83f6f50714a007dc92db16
wkdlrn <kkjjgg97@gmail.com>
2024-11-04 01:33:31 +0900

    feat: 로또 번호 생성 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_RANGE_START = 1;
+    private static final int LOTTO_NUMBER_RANGE_END = 45;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
 
     public static void main(String[] args) {
         int purchaseAmount = getPurchaseAmount();
         System.out.println("구입 금액: " + purchaseAmount);
+
     }
 
     private static int getPurchaseAmount() {
@@ -17,5 +24,11 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
         return purchaseAmount;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                LOTTO_NUMBER_RANGE_START, LOTTO_NUMBER_RANGE_END, LOTTO_NUMBERS_COUNT
+        );
     }
 }
