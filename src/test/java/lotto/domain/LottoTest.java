0deaba93b6dcf17c4d851b3fed77ccecba00f461
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-04 03:31:02 +0900

    test(LottoTest): 로또 번호 검증을 한 class로 통합

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 class LottoTest {
 
     @Nested
-    class 로또_번호_개수_검증 {
+    class 로또_번호_검증 {
 
         @Test
         void 로또_번호가_정확히_6개일_경우_Lotto_객체를_생성할_수_있다() {
@@ -35,10 +35,6 @@ class LottoTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
-
-    @Nested
-    class 로또_번호_중복_검증 {
 
         @Test
         void 로또_번호에_중복된_숫자가_있으면_Lotto_객체_생성에_실패한다() {
@@ -50,18 +46,16 @@ class LottoTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorMessage.DUPLICATE_LOTTO_NUMBER_NOT_ALLOWED.getMessage());
         }
-    }
-
-    @Nested
-    class 로또_번호_범위_검증 {
 
         @Test
         void 로또_번호가_1에서_45_사이가_아니면_Lotto_객체_생성에_실패한다() {
+            // given
             List<Integer> outOfRangeNumbers = List.of(0, 2, 3, 4, 5, 46);
+
+            // when & then
             assertThatThrownBy(() -> new Lotto(outOfRangeNumbers))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());
         }
     }
-
 }
