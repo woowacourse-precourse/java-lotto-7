645789bf467ed424594c32adfe9d016f88e0831c
world-dv <goodforx0147@naver.com>
2024-11-03 21:00:19 +0900

    test: 로또 예외 테스트 코드 추가
    - 로또 생성 시 발생하는 예외 처리를 테스트하는 코드를 추가합니다.
    - 로또 번호의 개수가 6개가 아닐 경우 예외를 발생하는지 검증합니다.
    - 로또 번호가 1에서 45 사이의 숫자가 아닐 경우 예외를 발생하는지 검증합니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -8,6 +8,15 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    @DisplayName("로또 번호의 개수가 6개가 되지 않으면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개가_되지않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +30,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 45 초과일 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_45_초과일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 46, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1 미만일 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1_미만일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
