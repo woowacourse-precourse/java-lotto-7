9e24be4e61a899552c3e49dd0cd388c22a095982
jijongkwon <help3451@naver.com>
2024-11-03 22:10:42 +0900

    test(service): 수익률 계산 테스트 코드 작성

---
+++ src/test/java/lotto/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package lotto.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    private LottoMachine lottoMachine;
+    private Member member;
+    private LottoMachineService lottoMachineService;
+    private MemberService memberService;
+
+    @BeforeEach
+    public void setup() {
+        lottoMachine = LottoMachine.getInstance();
+        member = Member.getInstance();
+        lottoMachineService = new LottoMachineService();
+        memberService = new MemberService();
+    }
+
+    @Test
+    void 수익률_계산() {
+        // given
+        String amount = "8000";
+        String winningNumbers = "1,2,3,4,5,6";
+        String bonusNumber = "7";
+        List<Lotto> lottos = new ArrayList<>();
+
+        // when
+        lottoMachineService.inputLottoPurchaseAmount(amount);
+        lottoMachineService.inputWinningNumbers(winningNumbers);
+        lottoMachineService.inputBonusNumber(bonusNumber);
+        lottos.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+        lottoMachineService.giveCorrectCountAndMoney(lottos);
+        memberService.calculateReturnOfRate();
+
+        // then
+        assertEquals(62.5, member.getReturnOfRate());
+    }
+}
