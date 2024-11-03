b3f95c71821acdfc00c8b58ca52dd1699f8cd3b7
나상연 <reddynor@naver.com>
2024-11-03 14:35:04 +0900

    test: add unit tests for LottoValidator validation logic
    - Test correct and incorrect formats for purchase amount validation
    - Verify correct integer conversion for numeric string inputs and handle invalid formats
    - Ensure exceptions are thrown with invalid data inputs

---
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -0,0 +1,40 @@
+package lotto.validator;
+
+import org.junit.jupiter.api.Test;
+
+import static lotto.validator.LottoValidator.stringToInt;
+import static lotto.validator.LottoValidator.unitValidator;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoValidatorTest {
+
+    @Test
+    void 올바른_유효_형식_테스트(){
+        boolean result = unitValidator(10000);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 잘못된_유효_형식_테스트(){
+        int result = 10300;
+        assertThatThrownBy(() -> unitValidator(result)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_형식_테스트(){
+        String number = "13000";
+        int convertedNumber = stringToInt(number);
+        assertThat(convertedNumber).isEqualTo(13000);
+    }
+
+    @Test
+    void 잘못된_숫자_형식_테스트(){
+        String number = "wrong format";
+        assertThatThrownBy(() -> stringToInt(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+}
