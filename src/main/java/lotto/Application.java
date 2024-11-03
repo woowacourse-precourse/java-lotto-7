f7613357d5827364f097709423c71b31c5d52e09
Silbet <65993038+Silbet@users.noreply.github.com>
2024-11-03 16:11:42 +0900

    Feat: 로또 구입 구현
    금액을 입력하면 해당 금액만큼의 로또를 구매하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static int lottoPrice = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputMoney = Console.readLine();
+        int lottoPieces = buyLotto(inputMoney);
+    }
+
+    public static int buyLotto(final String input) {
+        int payment = Integer.parseInt(input);
+        if (payment % lottoPrice != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return payment / lottoPrice;
     }
 }
