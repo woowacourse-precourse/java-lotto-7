02878a946020d86eb2373d8a758f240fcf73dc4e
dbdb456 <adad568@naver.com>
2024-11-03 16:05:10 +0900

    feat(application): 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.utils.Utils;
+import lotto.validator.Validators;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller lotto = new Controller(new InputView(), new OutputView(), new Validators(), new Utils());
+        lotto.start();
     }
 }
