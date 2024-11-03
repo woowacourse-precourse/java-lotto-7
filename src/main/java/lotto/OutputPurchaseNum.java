a678c5cbb8ca4321c10e2ac65bb04442f20f9b4d
seungheejang <justinholla908@gmail.com>
2024-11-03 18:34:56 +0900

    feat(OutputPurchaseNum): Add output feature to print the number of purchased lotto
    - Add method to print the purchaseNumber.

---
+++ src/main/java/lotto/OutputPurchaseNum.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class OutputPurchaseNum {
+    public void printPurchaseNumber(int purchaseNumber) {
+        System.out.println();
+        System.out.println(purchaseNumber + "개를 구매했습니다.");
+    }
+}
