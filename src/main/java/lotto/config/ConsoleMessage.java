0ea02238fcc62c04712f64d0f1bd01c2b4268c87
rlaehd62 <rlaehd62@gmail.com>
2024-11-03 19:57:01 +0900

    feat(config): 사용자 출력 메세지 설정 추가 - 사용자가 보게되는 출력 메세지를 관리 - 출력의 형식을 유연하게 변경하기 위해서 사용

---
+++ src/main/java/lotto/config/ConsoleMessage.java
@@ -0,0 +1,14 @@
+package lotto.config;
+
+public interface ConsoleMessage {
+    String JOINT = ", ";
+    String OPEN_WRAPPER = "[";
+    String CLOSE_WRAPPER = "]";
+    String DIGIT_INPUT = "\n당첨 번호를 입력해 주세요.";
+    String BONUS_INPUT = "\n보너스 번호를 입력해 주세요.";
+    String BUDGET_INPUT = "\n구입금액을 입력해주세요.";
+    String PURCHASE = "%d개를 구매했습니다.";
+    String STATISTICS_HEAD = "당첨 통계";
+    String STATISTICS_LINE = "---";
+    String RATE = "총 수익률은 %.1f%%입니다.";
+}
