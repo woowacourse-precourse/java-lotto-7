7040f3bcb9557240b8f92b7eb0b1840b28e5f3e0
wgrgwg <kyoeng14697@naver.com>
2024-11-03 19:02:20 +0900

    feat: Add more errorMessages in enum class

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -7,7 +7,10 @@ public enum ErrorMessage {
     NOT_POSITIVE("입력은 양수여야 합니다."),
 
     // 도메인 관련
-    NOT_VALID_MONEY("구입 금액은 1,000원 단위로 입력해야 합니다.");
+    NOT_VALID_MONEY("구입 금액은 1,000원 단위로 입력해야 합니다."),
+    NOT_VALID_SIZE("로또 번호는 6개여야 합니다."),
+    NOT_IN_RANGE("숫자가 유효 범위(1-45)를 벗어났습니다."),
+    DUPLICATE("로또 번호는 중복되지 않아야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
 
