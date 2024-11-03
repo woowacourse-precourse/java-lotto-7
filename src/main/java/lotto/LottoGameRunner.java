45d3a3afa54511252f2740b029375ff25d22fec4
encoreJeong <zzaibean@gmail.com>
2024-11-04 00:53:48 +0900

    refactor: LottoGameRunner 내의 메서드 책임 분산

--- src/main/java/lotto/LottoGameRunner.java
+++ src/main/java/lotto/LottoGameRunner.java
@@ -16,78 +16,14 @@ public class LottoGameRunner {
 
     public void run() {
 
-        List<Integer> winningHistoty = lottoGame.getLottos().stream()
-                .map(lotto -> {
-                    int correctCount = 0;
-                    for(Integer value : lotto.getNumbers()) {
-                        if(lottoGame.getWinningNumbers().contains(value)) {
-                            correctCount++;
-                        }
-                    }
-                    if (correctCount == 5) {
-                        if (lotto.contains(lottoGame.getBonusNumber())) {
-                            correctCount = 7;
-                        }
-                    }
-                    return  correctCount;
-                })
-                .toList();
+        List<Integer> wins = calculateRankFromMatchResult(matchLottoNumbers());
 
-        List<Integer> winningRanks = winningHistoty.stream()
-                .map(wimCount -> {
-                    if(wimCount == 6) {
-                        return 1;
-                    }
+        Double earningRate = calculateEarningRate(wins);
 
-                    if(wimCount == 7) {
-                        return 2;
-                    }
+        printLottoGameResult(wins, earningRate);
+    }
 
-                    if(wimCount == 5) {
-                        return 3;
-                    }
-
-                    if(wimCount == 4) {
-                        return 4;
-                    }
-
-                    if(wimCount == 3) {
-                        return 5;
-                    }
-
-                    return 0;
-                })
-                .toList();
-
-        Integer totalEarned = winningRanks.stream()
-                .mapToInt(rank -> {
-                    if(rank == 1) {
-                        return 2000000000;
-                    }
-
-                    if(rank == 2) {
-                        return 30000000;
-                    }
-
-                    if(rank == 3) {
-                        return 1500000;
-                    }
-
-                    if(rank == 4) {
-                        return 50000;
-                    }
-
-                    if(rank == 5) {
-                        return 5000;
-                    }
-
-                    return 0;
-                })
-                .sum();
-
-        double earnRate = ((double) totalEarned / lottoGame.getTotalPrice()) * 100;
-        Double roundedEarnRate = Math.round(earnRate * 100) / 100.0;
-
+    private void printLottoGameResult(List<Integer> winningRanks, Double earningRate) {
         StringBuilder winStatsResult = new StringBuilder();
         int[] countPerWinningRank = new int[6];
         winningRanks
@@ -102,8 +38,98 @@ public class LottoGameRunner {
                 .append("5개 일치 (1,500,000원) - ").append(countPerWinningRank[3]).append("\n")
                 .append("5개 일치, 보너스 볼 일치 (30,000,000원) - ").append(countPerWinningRank[2]).append("\n")
                 .append("6개 일치 (2,000,000,000원) - ").append(countPerWinningRank[1]).append("\n")
-                .append("총 수익률은 ").append(roundedEarnRate).append("%입니다.");
+                .append("총 수익률은 ").append(earningRate).append("%입니다.");
 
         System.out.println(winStatsResult);
+    }
+
+    private Double calculateEarningRate(List<Integer> winningRanks) {
+
+        double earnRate = ((double) calculateTotalEarning(winningRanks) / lottoGame.getTotalPrice()) * 100;
+
+        return roundToSecondDecimalPlace(earnRate);
+    }
+
+    private double roundToSecondDecimalPlace(double earnRate) {
+        return Math.round(earnRate * 100) / 100.0;
+    }
+
+    private Integer calculateTotalEarning(List<Integer> winningRanks) {
+        return winningRanks.stream()
+                .mapToInt(LottoGameRunner::winPriceFromRank)
+                .sum();
+    }
+
+    private static int winPriceFromRank(Integer rank) {
+        if(rank == 1) {
+            return 2000000000;
+        }
+
+        if(rank == 2) {
+            return 30000000;
+        }
+
+        if(rank == 3) {
+            return 1500000;
+        }
+
+        if(rank == 4) {
+            return 50000;
+        }
+
+        if(rank == 5) {
+            return 5000;
+        }
+
+        return 0;
+    }
+
+    private List<Integer> calculateRankFromMatchResult(List<Integer> winningHistoty) {
+        return winningHistoty.stream()
+                .map(LottoGameRunner::gradeFromWinCount)
+                .toList();
+    }
+
+    private static int gradeFromWinCount(Integer winCount) {
+        if(winCount == 6) {
+            return 1;
+        }
+        if(winCount == 7) {
+            return 2;
+        }
+        if(winCount == 5) {
+            return 3;
+        }
+        if(winCount == 4) {
+            return 4;
+        }
+        if(winCount == 3) {
+            return 5;
+        }
+        return 0;
+    }
+
+    private List<Integer> matchLottoNumbers() {
+        List<Integer> winningHistoty = lottoGame.getLottos().stream()
+                .map(this::countMatchedNumbers)
+                .toList();
+        return winningHistoty;
+    }
+
+    private int countMatchedNumbers(Lotto lotto) {
+        int correctCount = 0;
+
+        for(Integer value : lotto.getNumbers()) {
+            if(lottoGame.getWinningNumbers().contains(value)) {
+                correctCount++;
+            }
+        }
+
+        if (correctCount == 5) {
+            if (lotto.contains(lottoGame.getBonusNumber())) {
+                correctCount = 7;
+            }
+        }
+        return correctCount;
     }
 }
