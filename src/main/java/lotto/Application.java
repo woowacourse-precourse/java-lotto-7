e6ad6005e192de1174171105f769cd173e207e55
LaiTial <asna90@naver.com>
2024-11-04 05:57:18 +0900

    make lotto tickets

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // 입력 받기
+
         int buy = Input.buyMoney(); // 구입 금액 입력 받기
+        LottoPurchase purchase = new LottoPurchase(buy/1000); // 로또 번호 생성
+        purchase.Print(); // 생성한 로또 번호 print
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String lotto = Console.readLine(); // 당첨 번호
--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -1,8 +1,47 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoPurchase {
+
+    int buy;
+    List<List<Integer>> lottoTickets = new ArrayList<>(); // 구입한 로또 티켓을 저장할 리스트
+
+    // 구매 개수를 받아 로또 리켓 생성
+    public LottoPurchase(int buy) {
+
+        this.buy = buy; // 구입한 개수 저장
+        
+        for (int i=0; i<buy; i++) {
+            List<Integer> lotto = Lotto(); // 로또 티켓 생성
+            Collections.sort(lotto); // 정렬
+            lottoTickets.add(lotto); // 티켓 추가
+        }
+    }
+
+    // 랜덤으로 로또 생성
+    private List<Integer> Lotto() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    // 로또 티켓 print
+    public void Print() {
+
+        System.out.println(String.valueOf(buy)+"개를 구매했습니다.");
+
+        for (List<Integer> ticket : lottoTickets) {
+            String result = ticket.stream()
+                    .map(String::valueOf) // Integer를 String으로 변환
+                    .collect(Collectors.joining(", ")); // Join
+
+            System.out.println("["+result+"]"); // 출력: [1, 2, 3, 4, 5, 6]
+        }
+    }
 
 
 }
