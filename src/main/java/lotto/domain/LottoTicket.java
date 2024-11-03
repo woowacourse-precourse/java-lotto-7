21043d88fe42d0b7561ef0f9fc4565c048c05a61
hyuuuun <pam4522@gmail.com>
2024-11-03 20:13:26 +0900

    style (LottoTicket) : 상수 PRICE_MAX 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
 public class LottoTicket {
-    private static final int TICKET_PRICE = 1_000;
+    private static final int PRICE_TICKET = 1_000;
+    private static final int PRICE_MAX = 100_000;
     private final int price;
     private int count;
 
-    LottoTicket(int price) {
+    public LottoTicket(int price) {
         validatePrice(price);
         this.price = price;
         count = priceChangeToTicket(price);
@@ -14,7 +15,7 @@ public class LottoTicket {
         return count;
     }
     private void validatePrice(int price){
-        if(price < 0 || price > 100_000){
+        if(price < PRICE_TICKET || price > PRICE_MAX){
             throw new IllegalArgumentException("[ERROR] 로또 티켓은 10먼원까지 구입 가능합니다.");
         }
         if(checkMoneyChange(price) != 0 ){
@@ -22,10 +23,10 @@ public class LottoTicket {
         }
     }
     private int priceChangeToTicket(int price){
-        count = price /TICKET_PRICE;
+        count = price / PRICE_TICKET;
         return count;
     }
     private int checkMoneyChange(int price){
-        return price % TICKET_PRICE;
+        return price % PRICE_TICKET;
     }
 }
