6f90e801722e6dd7d135d6c2673f98d53b9921ca
EHCTH <lees90896@gmail.com>
2024-11-03 13:55:23 +0900

    feat(ResultMessage): 문자열 포맷팅 출력 메세지 enum 클래스 로 변경

--- src/main/java/lotto/view/output/domain/ResultMessage.java
+++ src/main/java/lotto/view/output/domain/ResultMessage.java
@@ -1,4 +1,28 @@
 package lotto.view.output.domain;
 
 public enum ResultMessage {
+    ERROR_MESSAGE("%s"),
+    INSERT_MONEY("%s"),
+    TICKETS_PURCHASED_COUNT("%d개를 구매했습니다."),
+    PURCHASE_TICKETS_INFO("%s"),
+    WINNING_LOTTO("%s"),
+    BONUS_NUMBER("%s"),
+    WINNING_PLACE_INFO("%s") {
+        @Override
+        public void print(Object... obj) {
+            System.out.printf(getFormatMessage(), obj);
+        }
+    },
+    TOTAL_PROFIT_RATE("총 수익률은 %s%%입니다.");
+    private final String formatMessage;
+    ResultMessage(String formatMessage) {
+        this.formatMessage = formatMessage;
+    }
+    public String getFormatMessage() {
+        return formatMessage;
+    }
+    public void print(Object... obj) {
+        String format = String.format(formatMessage, obj);
+        System.out.println(format);
+    }
 }
