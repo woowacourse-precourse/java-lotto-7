754ce7b8fdce3ee0ebe91caadff12918290f79cf
kimdev <kim__dev@hufs.ac.kr>
2024-11-03 22:47:33 +0900

    fix(BonusNumberValidator): 보너스 번호는 45까지는 허용하게 하라

--- src/main/java/lotto/utility/BonusNumberValidator.java
+++ src/main/java/lotto/utility/BonusNumberValidator.java
@@ -4,7 +4,7 @@ public class BonusNumberValidator {
     private static final int MAX_LOTTO_NUMBER = 45;
 
     public static void validateUnderFourtySix(int bonusNumber) {
-        if (bonusNumber >= MAX_LOTTO_NUMBER) {
+        if (bonusNumber > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(ExceptionEnum.CANNOT_OVER_FOURTY_SIX.getMessage());
         }
     }
