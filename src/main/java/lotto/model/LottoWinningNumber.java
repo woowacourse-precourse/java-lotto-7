0464587bcdaa46e24194398098e1f3d8a61d9368
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 21:26:51 +0900

    feat : 로또 당첨 번호 길이 및 중복 검증 로직 구현

--- src/main/java/lotto/model/LottoWinningNumber.java
+++ src/main/java/lotto/model/LottoWinningNumber.java
@@ -1,4 +1,21 @@
 package lotto.model;
 
+import lotto.validation.LottoWinningNumberValidator;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class LottoWinningNumber {
+
+    private final List<Integer> lottoWinningNumbers;
+    private final LottoWinningNumberValidator validator = new LottoWinningNumberValidator();
+
+    public LottoWinningNumber(List<Integer> lottoWinningNumbers) {
+        this.lottoWinningNumbers = lottoWinningNumbers;
+        validator.validateLengthWinningNumber(lottoWinningNumbers);
+        validator.validateDuplicationWinningNumber(lottoWinningNumbers);
+    }
+
+
 }
