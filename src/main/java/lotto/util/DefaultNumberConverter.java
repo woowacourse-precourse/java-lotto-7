604df68220e94cb016549a0d8b1654623c4d5aa1
양효빈 <hyobin3026@naver.com>
2024-11-03 20:21:41 +0900

    feat(DefaultNumberConverter): Add class to convert String to Integer

---
+++ src/main/java/lotto/util/DefaultNumberConverter.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import lotto.exception.GeneralExceptionMessages;
+
+public class DefaultNumberConverter{
+
+    public int convertNumber(String input) {
+        String trimmedInput = input.trim();
+        if (trimmedInput.isEmpty() || !trimmedInput.matches("\\d+")) {
+            throw new IllegalArgumentException(GeneralExceptionMessages.INVALID_NUMBER);
+        }
+
+        try {
+            return Integer.parseInt(trimmedInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(GeneralExceptionMessages.INVALID_NUMBER);
+        }
+    }
+}
