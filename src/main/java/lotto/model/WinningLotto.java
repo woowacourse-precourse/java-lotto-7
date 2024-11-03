b55392d8b16015bb8eab22a1255b5eb32c47e53f
Chan531 <chan657312@gmail.com>
2024-11-03 18:47:36 +0900

    feat(WinningLotto): 당첨 번호 로또 생성

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.List;
+
 public class WinningLotto {
 
 	private final Lotto winningNumbers;
@@ -8,5 +10,9 @@ public class WinningLotto {
 	public WinningLotto(Lotto winningNumbers, LottoNumber bonusNumber) {
 		this.winningNumbers = winningNumbers;
 		this.bonusNumber = bonusNumber;
+	}
+
+	private Lotto createWinningNumbers(List<Integer> winningNumbers, LottoCreator lottoCreator) {
+		return lottoCreator.createWinningLotto(winningNumbers);
 	}
 }
