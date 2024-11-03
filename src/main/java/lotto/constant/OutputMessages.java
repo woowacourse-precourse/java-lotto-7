6c3fb30ea84170fca0af55350dbb42de0ffcaca8
lucku <lsw6731@naver.com>
2024-11-03 17:22:15 +0900

    feat: 출력 기능 구현
    - 로또 개수 및 번호 출력 기능
    - 당첨 내역 출력 기능
    - 수익률 출력 기능
    - 에러 메시지 출력 기능

---
+++ src/main/java/lotto/constant/OutputMessages.java
@@ -0,0 +1,16 @@
+package lotto.constant;
+
+public class OutputMessages {
+    public static final String LOTTO_BUY_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String OUTPUT_DELIMITER = ", ";
+    public static final String PREFIX = "[";
+    public static final String SUFFIX = "]";
+    public static final String RESULT_MESSAGE = "당첨 통계%n---";
+    public static final String MATCH_COUNT_MESSAGE = "%d개 일치 (%s원) - %d개";
+    public static final String MATCH_COUNT_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    public static final String WINNINGS_FORMAT = "%,d";
+    public static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.%n";
+
+    private OutputMessages() {
+    }
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,78 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.constant.Rank;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+
+import static lotto.constant.OutputMessages.LOTTO_BUY_COUNT_MESSAGE;
+import static lotto.constant.OutputMessages.OUTPUT_DELIMITER;
+import static lotto.constant.OutputMessages.PREFIX;
+import static lotto.constant.OutputMessages.SUFFIX;
+import static lotto.constant.OutputMessages.RESULT_MESSAGE;
+import static lotto.constant.OutputMessages.MATCH_COUNT_MESSAGE;
+import static lotto.constant.OutputMessages.MATCH_COUNT_BONUS_MESSAGE;
+import static lotto.constant.OutputMessages.WINNINGS_FORMAT;
+import static lotto.constant.OutputMessages.PROFIT_RATE_MESSAGE;
+
+public class OutputView {
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        printLottoBuyCount(lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(convertLottoToString(lotto));
+        }
+    }
+
+    public static void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public static void printMatchCounts(LottoResult result) {
+        String message;
+
+        for (Rank rank : Rank.values()) {
+            message = createRankMessage(rank, result.getWinningCount().get(rank));
+            System.out.println(message);
+        }
+    }
+
+    public static void printProfitRate(double profitRate) {
+        System.out.printf(PROFIT_RATE_MESSAGE, profitRate);
+    }
+
+    private static String convertLottoToString(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(OUTPUT_DELIMITER, PREFIX, SUFFIX));
+    }
+
+    private static void printLottoBuyCount(int count) {
+        String message = String.format(LOTTO_BUY_COUNT_MESSAGE, count);
+        System.out.println(message);
+    }
+
+    private static String formatWinnings(int winnings) {
+        return String.format(WINNINGS_FORMAT, winnings);
+    }
+
+    private static String createRankMessage(Rank rank, int matchCount) {
+        if (rank == Rank.FIVE_MATCH_BONUS) {
+            return String.format(
+                    MATCH_COUNT_BONUS_MESSAGE,
+                    rank.getMatchCount(),
+                    formatWinnings(rank.getWinnings()),
+                    matchCount);
+        }
+        return String.format(MATCH_COUNT_MESSAGE,
+                rank.getMatchCount(),
+                formatWinnings(rank.getWinnings()),
+                matchCount);
+    }
+}
