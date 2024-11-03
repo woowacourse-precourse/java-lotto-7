b35532c4d10ddc15abeb6aae7841359cef6a578a
Dohun Kim <dohun8758@naver.com>
2024-11-03 17:48:17 +0900

    refactor: purchase 메서드에 NumbersGenerator를 받아서 처리하도록 수정

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -5,17 +5,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
+import lotto.util.NumbersGenerator;
 
 public class Lottos {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
     private final List<Lotto> lottos;
 
     private Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
-    public static Lottos purchase(int purchaseCount) {
+    public static Lottos purchase(int purchaseCount, NumbersGenerator numbersGenerator) {
         List<Lotto> purchasedLottos = IntStream.range(0, purchaseCount)
-                .mapToObj(lotto -> new RandomLottoGenerator().generateLotto())
+                .mapToObj(lotto -> Lotto.of(
+                        numbersGenerator.generateNumbers(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT)))
                 .toList();
         return new Lottos(purchasedLottos);
     }
@@ -26,7 +31,7 @@ public class Lottos {
             long matchingCount = lotto.matchingCountWith(winningLotto.getLotto());
             boolean isBonusNumberMatch = lotto.contains(winningLotto.getBonusNumber());
             LottoRank lottoRank = LottoRank.rankFrom(matchingCount, isBonusNumberMatch);
-            lottoResult.put(lottoRank, lottoResult.getOrDefault(lottoRank, 0) + 1);
+            lottoResult.put(lottoRank, lottoResult.getOrDefault(lottoRank, 0) + MIN_LOTTO_NUMBER);
         }
         return lottoResult;
     }
