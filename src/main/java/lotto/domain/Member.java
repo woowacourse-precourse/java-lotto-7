1aa32606218ceae2262e074077f0389688b117c2
jijongkwon <help3451@naver.com>
2024-11-03 19:32:03 +0900

    feat(domain): member 싱글톤 패턴, 필드, 메서드 생성
    - saveIssuedLotto: 발행된 로또 저장
    - getIssuedLottos: 발행된 로또 반환

--- src/main/java/lotto/domain/Member.java
+++ src/main/java/lotto/domain/Member.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+
+    private static final Member instance = new Member();
+    private final List<Lotto> issuedLottos;
+
+    private Member() {
+        issuedLottos = new ArrayList<>();
+    }
+
+    public static Member getInstance() {
+        return instance;
+    }
+
+    public void saveIssuedLotto(Lotto lotto) {
+        issuedLottos.add(lotto);
+    }
+
+    public List<Lotto> getIssuedLottos() {
+        return issuedLottos;
+    }
 }
