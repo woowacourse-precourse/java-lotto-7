4fd931d3ce9a66270255969b8f2ff6f31e7ebe3b
donghan378 <ehdgksqorgh@naver.com>
2024-11-04 03:02:43 +0900

    feat(Application): add getNum feature
    로또 구매 개수를 구하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseNum;
+
+        purchaseNum = getNum();
+
+    }
+
+    public static int getNum() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int sum = Integer.parseInt(Console.readLine());
+
+        if(sum % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액이 1000원으로 나누어 떨어지지 않습니다.");
+        }
+        return sum / 1000;
+    }
+
+    public static int calculateLottoNum() {
+
     }
 }
