cbb5f36b4011dfe006ecd88dc2bc7d30afea6362
Donghee <hongricky73@naver.com>
2024-11-03 16:02:49 +0900

    feat: Implement inputAmount method to get the amount from user input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
     public static void main(String[] args) {
+        int purchaseAmount = inputAmount();
+
         // TODO: 프로그램 구현
+
+    }
+    private static int inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+
+        if (!isNum(input)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+
+        int amount = Integer.parseInt(input);
+
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+
+        return amount;
+    }
+
+    private static boolean isNum(String str) {
+        return str.matches("\\d+");
     }
 }
