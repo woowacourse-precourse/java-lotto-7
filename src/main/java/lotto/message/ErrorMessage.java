83558994dc75f3081e2a364532efa71b43d6b42e
KSJ0128 <ksj01128@naver.com>
2024-11-04 01:32:51 +0900

    feat: ErrorMessage 구현
    기본적인 ErrorMessage를 세팅하고 생성자, getter, print 메서드를 구현했습니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -1,4 +1,20 @@
 package lotto.message;
 
 public enum ErrorMessage {
+    PREFIX("[ERROR]"),
+    NONE_NUMBER("수를 입력해주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
+    }
 }
