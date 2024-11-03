4565950335e6ae05ca34e87b2c9c5f809f459dd4
김성민 <klg1425@gmail.com>
2024-11-04 00:10:09 +0900

    test: LottoResult 단위테스트
    LottoResultTest 클래스 생성

---
+++ src/test/java/model/LottoResultTest.java
@@ -0,0 +1,35 @@
+package model;
+
+import lotto.model.LottoResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoResultTest {
+    @DisplayName("매칭 개수와 상금 확인")
+    @Test
+    void 매칭_개수와_상금_확인() {
+        assertThat(LottoResult.THREE_NUMBER_MATCH.lottoMatchCount()).isEqualTo(3);
+        assertThat(LottoResult.THREE_NUMBER_MATCH.lottoPrize()).isEqualTo(5000);
+
+        assertThat(LottoResult.FOUR_NUMBER_MATCH.lottoMatchCount()).isEqualTo(4);
+        assertThat(LottoResult.FOUR_NUMBER_MATCH.lottoPrize()).isEqualTo(50000);
+
+        assertThat(LottoResult.FIVE_NUMBER_MATCH.lottoMatchCount()).isEqualTo(5);
+        assertThat(LottoResult.FIVE_NUMBER_MATCH.lottoPrize()).isEqualTo(1500000);
+
+        assertThat(LottoResult.FIVE_NUMBER_AND_BONUS_NUMBER_MATCH.lottoMatchCount()).isEqualTo(5);
+        assertThat(LottoResult.FIVE_NUMBER_AND_BONUS_NUMBER_MATCH.lottoPrize()).isEqualTo(30000000);
+
+        assertThat(LottoResult.SIX_NUMBER_MATCH.lottoMatchCount()).isEqualTo(6);
+        assertThat(LottoResult.SIX_NUMBER_MATCH.lottoPrize()).isEqualTo(2000000000);
+    }
+
+    @DisplayName("보너스 번호 일치 여부 확인")
+    @Test
+    void 보너스_매치_확인() {
+        assertThat(LottoResult.isBonusMatch(5, true)).isTrue();
+        assertThat(LottoResult.isBonusMatch(5, false)).isFalse();
+        assertThat(LottoResult.isBonusMatch(4, true)).isFalse();
+    }
+}
