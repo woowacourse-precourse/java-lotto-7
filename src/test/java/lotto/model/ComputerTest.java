f98779cfe72625285a599119d283c7e06a6209e9
SunHee <76764643+sunni546@users.noreply.github.com>
2024-11-03 23:30:09 +0900

    test: 로또 비교 후 등수 계산 테스트

--- src/test/java/lotto/model/ComputerTest.java
+++ src/test/java/lotto/model/ComputerTest.java
@@ -3,26 +3,71 @@ package lotto.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import lotto.config.RankType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
+    private static final List<Integer> WINNING_NUMBERS = List.of(1, 2, 3, 4, 5, 6);
+    private static final int BONUS_NUMBER = 7;
+
+    private Computer computer;
+
+    @BeforeEach
+    void setUp() {
+        computer = new Computer(WINNING_NUMBERS);
+        computer.setBonusNumber(BONUS_NUMBER);
+    }
+
     @Test
     void 당첨_로또_발행() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
-
-        Computer computer = new Computer(winningNumbers);
         Lotto winningLotto = computer.getWinningLotto();
 
-        assertEquals(winningNumbers, winningLotto.getNumbers());
+        assertEquals(WINNING_NUMBERS, winningLotto.getNumbers());
     }
 
     @Test
     void 보너스_번호_설정() {
-        int bonusNumber = 7;
+        assertEquals(BONUS_NUMBER, computer.getBonusNumber());
+    }
 
-        Computer computer = new Computer(List.of(1, 2, 3, 4, 5, 6));
-        computer.setBonusNumber(bonusNumber);
+    @Test
+    void 로또_비교_1등() {
+        Lotto lotto = new Lotto(WINNING_NUMBERS);
+        computer.compareLotto(List.of(lotto));
 
-        assertEquals(bonusNumber, computer.getBonusNumber());
+        assertEquals(1, computer.getResults().get(RankType.FIRST.getRank()));
+    }
+
+    @Test
+    void 로또_비교_2등_보너스_포함() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, BONUS_NUMBER));
+        computer.compareLotto(List.of(lotto));
+
+        assertEquals(1, computer.getResults().get(RankType.SECOND.getRank()));
+    }
+
+    @Test
+    void 로또_비교_3등_보너스_미포함() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        computer.compareLotto(List.of(lotto));
+
+        assertEquals(1, computer.getResults().get(RankType.THIRD.getRank()));
+    }
+
+    @Test
+    void 로또_비교_4등() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
+        computer.compareLotto(List.of(lotto));
+
+        assertEquals(1, computer.getResults().get(RankType.FOURTH.getRank()));
+    }
+
+    @Test
+    void 로또_비교_5등() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
+        computer.compareLotto(List.of(lotto));
+
+        assertEquals(1, computer.getResults().get(RankType.FIFTH.getRank()));
     }
 }
