caf61f8c93f47fc458542308ac637f37c986629d
Chan531 <chan657312@gmail.com>
2024-11-03 17:35:16 +0900

    test(LottoTest): 로또 번호 개수 검증 테스트
    로또 번호 개수가 6개를 초과한다면 예외 발생

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -3,16 +3,26 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+
+	@Test
+	@DisplayName("로또 번호의 개수가 6개를 초과한다면 에러를 발생시킨다.")
+	void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+		// given
+		List<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, 7)
+				.mapToObj(LottoNumber::from)
+				.toList();
+
+		// when, then
+		assertThatThrownBy(() -> new Lotto(lottoNumbers))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
+	}
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
