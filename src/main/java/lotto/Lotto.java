28f502bb5132107bbaed3520faaf50c4d3e343ad
윤찬 <vsvx13@naver.com>
2024-11-03 21:35:06 +0900

    [FEAT] 기본 기능 구현하기 요구사항 기능에 맞게 기능 구현을 Application 파일에 단순 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,4 +18,18 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    int getWinningNumber(List<Integer> winnings, int bonus) {
+        int count = (int) numbers.stream().filter(winnings::contains).count();
+
+        if(count == 6 || (count == 5 && numbers.contains(bonus))) {
+            return 7 - count;
+        }
+
+        return 7 - (count - 1);
+    }
+
+
+    void printNumber() {
+        System.out.println("[" + numbers.stream().sorted().map(String::valueOf).collect(Collectors.joining(", ")) + "]");
+    }
 }
