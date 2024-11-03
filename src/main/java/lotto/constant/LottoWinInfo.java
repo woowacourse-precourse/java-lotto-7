badb25e1f6b58b5afad11698417185f6b491a392
yesjuhee <juhee0924@g.skku.edu>
2024-11-04 04:50:48 +0900

    refactor: 사용하지 않는 라인 정리

--- src/main/java/lotto/constant/LottoWinInfo.java
+++ src/main/java/lotto/constant/LottoWinInfo.java
@@ -4,19 +4,17 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum LottoWinInfo {
-    FIFTH(5, 3, false, 5000),
-    FOURTH(4, 4, false, 50000),
-    THIRD(3, 5, false, 1500000),
-    SECOND(2, 5, true, 30000000),
-    FIRST(1, 6, false, 2000000000);
+    FIFTH(3, false, 5000),
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000);
 
-    private final int rank;
     private final int numberMatchCount;
     private final boolean hasBonusNumber;
     private final int prize;
 
-    LottoWinInfo(final int rank, final int numberMatchCount, final boolean hasBonusNumber, final int prize) {
-        this.rank = rank;
+    LottoWinInfo(final int numberMatchCount, final boolean hasBonusNumber, final int prize) {
         this.numberMatchCount = numberMatchCount;
         this.hasBonusNumber = hasBonusNumber;
         this.prize = prize;
@@ -27,10 +25,6 @@ public enum LottoWinInfo {
                 .filter(lottoWinInfo -> lottoWinInfo.numberMatchCount == numberMatchCount)
                 .filter(lottoWinInfo -> lottoWinInfo.hasBonusNumber == hasBonusNumber)
                 .findAny();
-    }
-
-    public int getRank() {
-        return rank;
     }
 
     public int getNumberMatchCount() {
