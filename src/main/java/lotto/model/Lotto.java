f625f6f0d643ca1ee7d5db448baf7de696490eaa
Parkjyun <jaeyun0507@naver.com>
2024-11-03 18:52:11 +0900

    refactor(Lotto): 중복 생성된 클래스 삭제

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -12,7 +12,9 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+
+    // TODO: 추가 기능 구현
 }
