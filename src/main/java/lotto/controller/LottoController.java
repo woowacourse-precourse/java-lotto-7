0fbcb050428884e5de8474d76ef6af77c799d2c0
Seungwan Yoo <ysw99@kakao.com>
2024-11-03 18:36:19 +0900

    feat: 로또 구입 금액 입력 구현 - 관련 예외 처리 및 메시지 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,27 @@
 package lotto.controller;
 
-public class LottoController {
-    public void run() {
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        Integer purchaseAmount = getPurchaseAmount();
+    }
+
+    private Integer getPurchaseAmount() {
+        try {
+            return inputView.getPurchaseAmountInput();
+        } catch (IllegalArgumentException e) {
+            outputView.renderErrorMessage(e.getMessage());
+            return getPurchaseAmount();
+        }
     }
 }
