59b825848c7ca06fd14ec79dac7f9b29d0f3e54f
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-11-03 23:21:48 +0900

    feat(domain) : LottoRank Enum 구현하기

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,35 @@
+package lotto;
+
+public enum LottoRank{
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0);
+
+    private final int  match;
+    private final boolean matchBonus;
+    private final int prize;
+
+
+    LottoRank(int match, boolean matchBonus, int prize) {
+        this.match = match;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static LottoRank valueof(int match, boolean matchBonus) {
+        for (LottoRank rank : values()) {
+            if (rank.match == match && rank.matchBonus == matchBonus) {
+                return rank;
+            }
+        }
+        return MISS;
+    }
+
+}
