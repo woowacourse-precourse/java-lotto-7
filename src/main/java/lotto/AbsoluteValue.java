7542de5227b4949ec2ae6895be46e652bcf75505
YongLeeCode <yonghwan311@gmail.com>
2024-11-03 22:16:48 +0900

    feat: 티켓 장 수 생성

--- src/main/java/lotto/AbsoluteValue.java
+++ src/main/java/lotto/AbsoluteValue.java
@@ -4,7 +4,5 @@ public class AbsoluteValue {
     public static final Integer MIN_NUMBER = 1;
     public static final Integer MAX_NUMBER = 45;
     public static final Integer LOTTO_NUMBER_COUNT = 6;
-    public static Integer TICKET_COUNT (Integer pay) {
-        return pay / 1000;
-    }
+    public static final Integer TICKET_PRICE = 1000;
 }
