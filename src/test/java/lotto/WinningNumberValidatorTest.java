58c913f41b730f6ff29aeb3c2cc952b9397980c8
GrO0vy <lmc2819@naver.com>
2024-11-03 22:06:28 +0900

    test: 테스트 이름 출력 형식 변경

--- src/test/java/lotto/WinningNumberValidatorTest.java
+++ src/test/java/lotto/WinningNumberValidatorTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningNumberValidatorTest {
     // 당첨 번호 검증 클래스 ( WinningNumberValidator ) 테스트
-    @DisplayName("당첨 번호에 숫자 외의 값이 입력되면 예외가 발생한다")
+    @DisplayName("[WinningNumberValidatorTest] 당첨 번호에 숫자 외의 값이 입력되면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"asd", ",123", "12 12432", "@$%#!", "123@$12,./"})
     void 당첨_번호에_숫자_외의_값이_입력되면_예외가_발생한다(String input){
@@ -17,7 +17,7 @@ public class WinningNumberValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("당첨 번호에 정수 범위를 벗어난 값을 입력하면 예외가 발생한다")
+    @DisplayName("[WinningNumberValidatorTest] 당첨 번호에 정수 범위를 벗어난 값을 입력하면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"990909999999", "2147483648", "1111111111111111111", "9999999999999999"})
     void 당첨_번호에_정수_범위를_벗어난_값을_입력하면_예외가_발생한다(String input){
