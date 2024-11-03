1d39eccb56fda8fb0e5a5f08042b7716bb5822eb
yuntasha <jaljayo85@gmail.com>
2024-11-03 13:32:10 +0900

    feat(OutputHelper): write OuputHelper
    Add value list:
    - String PURCHASE_MESSAGE
    - String LINE_BREAK
    - String STATS_TITLE
    - String LINE
    - String RESULT_FORMAT
    - String RATE_FORMAT
    - List<Rank> rankList
    
    Add method list:
    - printLotto
    - makeLottoList
    - printResult
    - makeResult
    - makeRankResult
    - makeRateMessage

--- src/main/java/lotto/view/OutputHelper.java
+++ src/main/java/lotto/view/OutputHelper.java
@@ -1,4 +1,59 @@
 package lotto.view;
 
+import lotto.model.Rank;
+import lotto.model.Result;
+
+import java.util.List;
+import java.util.StringJoiner;
+
 public class OutputHelper {
+    private final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private final String LINE_BREAK = "\n";
+    private final String STATS_TITLE = "당첨 통계";
+    private final String LINE = "---";
+    private final String RESULT_FORMAT = "%s - %d개";
+    private final String RATE_FORMAT = "총 수익률은 %s입니다.";
+    private final List<Rank> rankList = List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST);
+
+    public void printLotto(List<List<Integer>> numlists) {
+        System.out.println(makeLottoList(numlists));
+    }
+
+    private String makeLottoList(List<List<Integer>> numlists) {
+        StringJoiner message = new StringJoiner(LINE_BREAK);
+        message.add(numlists.size()+PURCHASE_MESSAGE);
+
+        for (List<Integer> numlist: numlists) {
+            message.add(numlist.toString());
+        }
+
+        return message.toString();
+    }
+
+    public void printResult(Result result) {
+        System.out.println(makeResult(result));
+    }
+
+    private String makeResult(Result result) {
+        StringJoiner message = new StringJoiner(LINE_BREAK);
+
+        message.add(STATS_TITLE);
+        message.add(LINE);
+
+        for (Rank rank : rankList) {
+            message.add(makeRankResult(result, rank));
+        }
+
+        message.add(makeRateMessage(result));
+
+        return message.toString();
+    }
+
+    private String makeRankResult(Result result, Rank rank) {
+        return String.format(RESULT_FORMAT, rank.getDescription(), result.getCount(rank));
+    }
+
+    private String makeRateMessage(Result result) {
+        return String.format(RATE_FORMAT, result.getRate());
+    }
 }
