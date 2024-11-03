333304a39f2af09261b6d8b10ff2fe52a238c15d
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-03 15:20:32 +0900

    feat(LottoController): 예외 발생 시 해당 부분부터 재입력 로직 적용

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,9 +5,11 @@ import lotto.domain.lotto.WinningLottos;
 import lotto.domain.number.BonusNumber;
 import lotto.domain.number.WinningNumbers;
 import lotto.domain.result.WinningResult;
-import lotto.global.constant.LottoConstant;
+import lotto.global.exception.LottoException;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.function.Supplier;
 
 public class LottoController {
 
@@ -20,26 +22,40 @@ public class LottoController {
     }
 
     public void run() {
-        PurchasedLottos purchasedLottos = purchaseLottos();
-        WinningResult winningResult = checkWinningResult(purchasedLottos);
+        WinningResult winningResult = generateWinningResult();
         displayResults(winningResult);
     }
 
+    private WinningResult generateWinningResult() {
+        PurchasedLottos purchasedLottos = purchaseLottos();
+        WinningNumbers winningNumbers = inputWinningNumbers();
+        BonusNumber bonusNumber = inputBonusNumber(winningNumbers);
+
+        return createWinningResult(purchasedLottos, winningNumbers, bonusNumber);
+    }
+
     private PurchasedLottos purchaseLottos() {
-        int purchaseAmount = inputView.readPurchaseAmount();
-        PurchasedLottos purchasedLottos = PurchasedLottos.from(calculatePurchaseCount(purchaseAmount));
+        PurchasedLottos purchasedLottos = inputPurchaseLottos();
         outputView.printPurchasedLottos(purchasedLottos);
         return purchasedLottos;
     }
 
-    private WinningResult checkWinningResult(PurchasedLottos purchasedLottos) {
-        WinningNumbers winningNumbers = getWinningNumbers();
-        BonusNumber bonusNumber = BonusNumber.from(inputView.readBonusNumber());
-        return createWinningResult(purchasedLottos, winningNumbers, bonusNumber);
+    private PurchasedLottos inputPurchaseLottos() {
+        return retryOnException(() ->
+                PurchasedLottos.from(inputView.readPurchaseAmount())
+        );
     }
 
-    private WinningNumbers getWinningNumbers() {
-        return WinningNumbers.from(inputView.readWinningNumbers());
+    private WinningNumbers inputWinningNumbers() {
+        return retryOnException(() ->
+                WinningNumbers.from(inputView.readWinningNumbers())
+        );
+    }
+
+    private BonusNumber inputBonusNumber(WinningNumbers winningNumbers) {
+        return retryOnException(() ->
+                BonusNumber.from(inputView.readBonusNumber(), winningNumbers)
+        );
     }
 
     private WinningResult createWinningResult(
@@ -47,15 +63,22 @@ public class LottoController {
             WinningNumbers winningNumbers,
             BonusNumber bonusNumber
     ) {
-        WinningLottos winningLottos = WinningLottos.of(purchasedLottos, winningNumbers, bonusNumber);
-        return WinningResult.of(winningLottos, purchasedLottos.getPurchasedLottos().size());
+        return retryOnException(() -> {
+            WinningLottos winningLottos = WinningLottos.of(purchasedLottos, winningNumbers, bonusNumber);
+            return WinningResult.of(winningLottos, purchasedLottos.getPurchasedLottos().size());
+        });
     }
 
     private void displayResults(WinningResult winningResult) {
         outputView.printWinningStatistics(winningResult);
     }
 
-    private int calculatePurchaseCount(int purchaseAmount) {
-        return purchaseAmount / LottoConstant.LOTTO_PRICE;
+    private <T> T retryOnException(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (LottoException e) {
+            System.out.println(e.getMessage());
+            return retryOnException(supplier);
+        }
     }
 }
