ec0a712aaf3e8fd2a1b56a36df80f3a19530ba0a
dev-Rhea <dev.rhea4444@gmail.com>
2024-11-03 18:39:58 +0900

    :art: refactor : 출력 형식 변경

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,7 +4,7 @@ public enum Prize {
     FIFTH(5, "3개 일치", 5_000, false),
     FOURTH(4, "4개 일치", 50_000, false),
     THIRD(3, "5개 일치", 1_500_000, false),
-    SECOND(2, "5개 일치 + 보너스 번호", 30_000_000, true),
+    SECOND(2, "5개 일치, 보너스 번호", 30_000_000, true),
     FIRST(1, "6개 일치", 2_000_000_000, false);
 
     private final int rank;
