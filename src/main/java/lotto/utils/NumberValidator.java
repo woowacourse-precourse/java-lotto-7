a42b6cb03d875c09c1376c87d978f76b483a4cd9
yush <yush2202@gmail.com>
2024-11-04 03:48:10 +0900

    refactor($ NumberValidator): 숫자 검증 추가
    자연수 검증 메서드 분리

--- src/main/java/lotto/utils/NumberValidator.java
+++ src/main/java/lotto/utils/NumberValidator.java
@@ -14,4 +14,19 @@ public class NumberValidator {
     public static boolean exceedRange(Integer number, int max, int min) {
         return number > max || number < min;
     }
+
+    public static void validateInt(String input) {
+        if (!isInt(input)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_DIGIT.getMessage());
+        }
+    }
+
+    private static boolean isInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
