42a2491f43341c96002773699ac3f3725be580d5
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-03 13:51:42 +0900

    feat: 로또의 당첨 내역을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,10 +3,18 @@ package lotto.view;
 import static lotto.view.ViewMessage.INPUT_BONUS_NUMBER;
 import static lotto.view.ViewMessage.INPUT_PURCHASE_PRICE;
 import static lotto.view.ViewMessage.INPUT_WINNING_NUMBERS;
+import static lotto.view.ViewMessage.PRINT_DIVIDER;
+import static lotto.view.ViewMessage.PRINT_MATCH_3;
+import static lotto.view.ViewMessage.PRINT_MATCH_4;
+import static lotto.view.ViewMessage.PRINT_MATCH_5;
+import static lotto.view.ViewMessage.PRINT_MATCH_5_AND_BONUS;
+import static lotto.view.ViewMessage.PRINT_MATCH_6;
 import static lotto.view.ViewMessage.PRINT_TICKET_COUNT;
+import static lotto.view.ViewMessage.PRINT_WINNING_STATISTICS;
 
-import java.util.List;
+import lotto.common.LottoRank;
 import lotto.model.Lotto;
+import lotto.model.LottoResult;
 
 public class OutputView {
 
@@ -22,10 +30,8 @@ public class OutputView {
         printMessage(PRINT_TICKET_COUNT.getMessage(), ticketCount);
     }
 
-    public static void printLottos(List<Lotto> lottos) {
-        for (Lotto lotto : lottos) {
-            printMessage(lotto.toString());
-        }
+    public static void printLotto(Lotto lotto) {
+        printMessage(lotto.toString());
     }
 
     public static void printInputWinningNumbers() {
@@ -36,6 +42,17 @@ public class OutputView {
     public static void printInputBonusNumber() {
         printMessage();
         printMessage(INPUT_BONUS_NUMBER.getMessage());
+    }
+
+    public static void printStatistics(LottoResult lottoResult) {
+        printMessage();
+        printMessage(PRINT_WINNING_STATISTICS.getMessage());
+        printMessage(PRINT_DIVIDER.getMessage());
+        printMessage(PRINT_MATCH_3.getMessage(), lottoResult.getWinningCount(LottoRank.FIFTH));
+        printMessage(PRINT_MATCH_4.getMessage(), lottoResult.getWinningCount(LottoRank.FOURTH));
+        printMessage(PRINT_MATCH_5.getMessage(), lottoResult.getWinningCount(LottoRank.THIRD));
+        printMessage(PRINT_MATCH_5_AND_BONUS.getMessage(), lottoResult.getWinningCount(LottoRank.SECOND));
+        printMessage(PRINT_MATCH_6.getMessage(), lottoResult.getWinningCount(LottoRank.FIRST));
     }
 
     private static void printMessage() {
--- src/main/java/lotto/view/ViewMessage.java
+++ src/main/java/lotto/view/ViewMessage.java
@@ -4,7 +4,14 @@ public enum ViewMessage {
     INPUT_PURCHASE_PRICE("구입금액을 입력해 주세요."),
     PRINT_TICKET_COUNT("%d개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    PRINT_WINNING_STATISTICS("당첨 통계"),
+    PRINT_DIVIDER("---"),
+    PRINT_MATCH_3("3개 일치 (5,000원) - %d개"),
+    PRINT_MATCH_4("4개 일치 (50,000원) - %d개"),
+    PRINT_MATCH_5("5개 일치 (1,500,000원) - %d개"),
+    PRINT_MATCH_5_AND_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    PRINT_MATCH_6("6개 일치 (2,000,000,000원) - %d개");
 
     private final String message;
 
