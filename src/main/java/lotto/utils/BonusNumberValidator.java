b9ca165c667a52e6e58171c207ed53cd0445e5dc
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 23:18:55 +0900

    feat(BonusNumberValidator): 보너스 번호가 당첨번호와 중복될 경우

--- src/main/java/lotto/utils/BonusNumberValidator.java
+++ src/main/java/lotto/utils/BonusNumberValidator.java
@@ -1,20 +1,27 @@
 package lotto.utils;
 
+import java.util.List;
+
 public class BonusNumberValidator {
     private static void validateIsNumeric(String number) {
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+            throw new IllegalArgumentException("보너스 번호는 숫자여야 합니다.");
         }
     }
 
     private static void validateInRange(String number) {
         int num = Integer.parseInt(number);
         if (num < 1 || num > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이여야 합니다.");
+            throw new IllegalArgumentException("보너스 번호는 1에서 45 사이여야 합니다.");
         }
     }
 
-
+    private static void validateNotDuplicateWithWinningNumbers(String number, List<Integer> winningNumbers) {
+        int bonusNum = Integer.parseInt(number);
+        if (winningNumbers.contains(bonusNum)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
 }
