4c4a4ce491989b61bca4e220cac83238209921d5
gikhoon <ghikhoon@naver.com>
2024-11-04 02:53:00 +0900

    feat: 당첨 번호 final화

--- src/main/java/lotto/model/lottoInfo/WinningNumber.java
+++ src/main/java/lotto/model/lottoInfo/WinningNumber.java
@@ -3,7 +3,7 @@ package lotto.model.lottoInfo;
 import java.util.List;
 
 public class WinningNumber {
-    private List<Integer> winningNumbers;
+    private final List<Integer> winningNumbers;
 
     public WinningNumber(List<Integer> numbers) {
         this.winningNumbers = numbers;
