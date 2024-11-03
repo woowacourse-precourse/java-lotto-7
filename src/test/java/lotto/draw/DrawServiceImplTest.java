30d74142d3c0832bebecdfca60e42fa7179e372e
woong2e <tlswodnd12345@naver.com>
2024-11-03 19:13:47 +0900

    test: add test for check winning result

--- src/test/java/lotto/draw/DrawServiceImplTest.java
+++ src/test/java/lotto/draw/DrawServiceImplTest.java
@@ -27,7 +27,7 @@ class DrawServiceImplTest {
     }
 
     @Test
-    @DisplayName("당첨 확인 테스트1")
+    @DisplayName("당첨 확인 테스트 - 5등 잘 되는지")
     void 당첨_확인_테스트1() {
         List<Lotto> lottos = new ArrayList<>(List.of(new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(1,2,3,4,5,7))));
         vendingMachineRepository.save(lottos);
@@ -42,7 +42,7 @@ class DrawServiceImplTest {
     }
 
     @Test
-    @DisplayName("당첨 확인 테스트2")
+    @DisplayName("당첨 확인 테스트 - 미당첨 결과 잘 나오는지")
     void 당첨_확인_테스트2() {
         List<Lotto> lottos = new ArrayList<>(List.of(new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(4,5,6,7,8,9))));
         vendingMachineRepository.save(lottos);
@@ -50,6 +50,21 @@ class DrawServiceImplTest {
         Map<Rank, Integer> result = drawService.checkLotto(new WinningLotto(new Lotto(List.of(1,2,3,43,44,45)), 27));
 
         assertEquals(Integer.valueOf(0), result.get(Rank.FIRST));
+        assertEquals(Integer.valueOf(0), result.get(Rank.SECOND));
+        assertEquals(Integer.valueOf(0), result.get(Rank.THIRD));
+        assertEquals(Integer.valueOf(0), result.get(Rank.FOURTH));
+        assertEquals(Integer.valueOf(1), result.get(Rank.FIFTH));
+    }
+
+    @Test
+    @DisplayName("당첨 확인 테스트 - 5개가 아닐 때 보너스가 맞을 경우 2등이 안되도록 되는지")
+    void 당첨_확인_테스트3() {
+        List<Lotto> lottos = new ArrayList<>(List.of(new Lotto(List.of(1,2,3,4,5,27)), new Lotto(List.of(1,2,3,43,44,45))));
+        vendingMachineRepository.save(lottos);
+
+        Map<Rank, Integer> result = drawService.checkLotto(new WinningLotto(new Lotto(List.of(1,2,3,43,44,45)), 27));
+
+        assertEquals(Integer.valueOf(1), result.get(Rank.FIRST));
         assertEquals(Integer.valueOf(0), result.get(Rank.SECOND));
         assertEquals(Integer.valueOf(0), result.get(Rank.THIRD));
         assertEquals(Integer.valueOf(0), result.get(Rank.FOURTH));
