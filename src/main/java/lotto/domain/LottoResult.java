b8b1471df7ef5d0aac5b4e6ff2b7817eae477132
JoonKyo <joonkyolee@gmail.com>
2024-11-03 23:25:55 +0900

    feat(LottoResult): 각 로또에 대해 일치한 개수를 판단하고 등수를 저장하는 메서드 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,2 +1,30 @@
-package lotto.domain;public class LottoResult {
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoResult {
+    private final Lottos lottos;
+    private final WinNumber winNumber;
+
+    private LottoResult(Lottos lottos, WinNumber winNumber) {
+        this.lottos = lottos;
+        this.winNumber = winNumber;
+    }
+
+    public static LottoResult of(Lottos lottos, WinNumber winNumber) {
+        return new LottoResult(lottos, winNumber);
+    }
+
+    private List<Rank> collectMatchResults() {
+        return lottos.getLottos().stream()
+                .map(this::determineRank)
+                .collect(Collectors.toList());
+    }
+
+    private Rank determineRank(Lotto lotto) {
+        int matchCount = winNumber.matchWithLotto(lotto);
+        boolean bonusMatch = (matchCount == 5) && winNumber.matchWithBonusNumber(lotto);
+        return Rank.getRank(matchCount, bonusMatch);
+    }
 }
