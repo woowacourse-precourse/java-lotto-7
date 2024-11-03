2952e4fc1e8d52d6951be57e8ec6a6f2574ed1f0
Yunsu4 <ysa020428@gmail.com>
2024-11-03 21:28:26 +0900

    refactor(Application): LottoConfig로 객체 생성과 의존성 주입을 관리하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,27 +1,14 @@
 package lotto;
 
+import lotto.config.LottoConfig;
 import lotto.controller.LottoController;
-import lotto.model.AutoLottoGenerator;
-import lotto.model.LottoMatcher;
-import lotto.model.YieldCalculator;
-import lotto.util.LottoNumberParser;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+
 
 public class Application {
 
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        LottoNumberParser lottoNumberParser = new LottoNumberParser();
-        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
-        LottoMatcher lottoMatcher = new LottoMatcher();
-        YieldCalculator yieldCalculator = new YieldCalculator();
-
-        LottoController lottoController = new LottoController(inputView, outputView,
-                lottoNumberParser, autoLottoGenerator,
-                lottoMatcher, yieldCalculator);
+        LottoController lottoController = LottoConfig.lottoController();
 
         lottoController.run();
     }
