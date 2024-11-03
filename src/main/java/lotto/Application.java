0f373d423dd4fabd544f2d5944af3f216bc4f15f
jermany17 <leejm5050@naver.com>
2024-11-03 20:41:08 +0900

    Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,43 @@
 package lotto;
 
+import lotto.model.Lotto;
+import lotto.view.Input;
+import lotto.view.Output;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            // 모든 로또들을 넣을 리스트
+            List<Lotto> lottoList = new ArrayList<>();
+
+            // 구입 금액 입력
+            int total = Input.moneyInput();
+
+            // 구매 개수를 출력
+            Output.lottoTotalOutput(total);
+
+            // 자동 로또 번호를 생성 및 출력
+            for (int i = 0; i < total; i++) {
+                List<Integer> autoLotto = Input.autoLottoInput();
+                Collections.sort(autoLotto);
+                Lotto lotto = new Lotto(autoLotto);
+                lottoList.add(lotto);
+                Output.lottoNumOutput(autoLotto);
+            }
+
+            // 당첨 번호를 입력
+            List<Integer> winnerLotto = Input.winnerLottoNum();
+
+            // 보너스 볼을 입력
+            int bonusNum = Input.bonusNum();
+
+            // 당첨 통계를 출력
+            Output.resultOfLotto(lottoList, winnerLotto, bonusNum, total * 1000);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
