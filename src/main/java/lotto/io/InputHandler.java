4aa7537de2cd532cc75031b5d53f4a21a6efda24
whdmscks11 <whdmscks11@gmail.com>
2024-11-03 18:29:06 +0900

    fix(InputHandler): AutoCloseable 인터페이스 구현 해제
    프로그램 실행 중 사용자가 잘못된 입력을 할 경우, 예외가 발생하고 에러 메시지가 출력된 후 다시 입력을 받아야 한다. 이 과정에서 자원이 자동으로 닫히게 되어 두 번째 루프부터 NoSuchElementException이 발생하게 된다. 이를 해결하기 위해 AutoCloseable을 구현하지 않도록 변경하고, 필요한 경우 수동으로 close 메서드를 호출하도록 하였다.

--- src/main/java/lotto/io/InputHandler.java
+++ src/main/java/lotto/io/InputHandler.java
@@ -1,19 +1,9 @@
 package lotto.io;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.NoSuchElementException;
 
-public class InputHandler implements AutoCloseable {
+public class InputHandler {
     public String readline(){
-        try{
             return Console.readLine();
-        } catch(NoSuchElementException|IllegalStateException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void close() {
-        Console.close();
     }
 }
