1eb711c43724a1dd5aaef14b9a122602c02ef94e
이윤빈 <kanado20000@naver.com>
2024-11-03 22:47:32 +0900

    :art: refactor: 코드 테스트 파일 원본과 맞추기

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,89 +1,61 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.List;
-import org.junit.jupiter.api.Test;
-
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @SuppressWarnings("unchecked")
     @Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
-            () -> {
-                run("8000", "1,2,3,4,5,6", "7");
-                assertThat(output())
-                    .contains(
-                        "8개를 구매했습니다.",
-                        "[8, 21, 23, 41, 42, 43]",
-                        "[3, 5, 11, 16, 32, 38]",
-                        "[7, 11, 16, 35, 36, 44]",
-                        "[1, 8, 11, 31, 41, 42]",
-                        "[13, 14, 16, 38, 42, 45]",
-                        "[7, 11, 30, 40, 42, 43]",
-                        "[2, 13, 22, 32, 38, 45]",
-                        "[1, 3, 5, 14, 22, 45]",
-                        "3개 일치 (5,000원) - 1개",
-                        "4개 일치 (50,000원) - 0개",
-                        "5개 일치 (1,500,000원) - 0개",
-                        "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                        "6개 일치 (2,000,000,000원) - 0개",
-                        "총 수익률은 62.5%입니다.");
-            },
-            List.of(8, 21, 23, 41, 42, 43),
-            List.of(3, 5, 11, 16, 32, 38),
-            List.of(7, 11, 16, 35, 36, 44),
-            List.of(1, 8, 11, 31, 41, 42),
-            List.of(13, 14, 16, 38, 42, 45),
-            List.of(7, 11, 30, 40, 42, 43),
-            List.of(2, 13, 22, 32, 38, 45),
-            List.of(1, 3, 5, 14, 22, 45));
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    void 당첨번호가_없는_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-            () -> {
-                run("5000", "1,2,3,4,5,6", "7");
-                assertThat(output())
-                    .contains(
-                        "5개를 구매했습니다.",
-                        "[8, 9, 10, 11, 12, 13]",
-                        "[14, 15, 16, 17, 18, 19]",
-                        "[20, 21, 22, 23, 24, 25]",
-                        "[26, 27, 28, 29, 30, 31]",
-                        "[32, 33, 34, 35, 36, 37]",
-                        "3개 일치 (5,000원) - 0개",
-                        "4개 일치 (50,000원) - 0개",
-                        "5개 일치 (1,500,000원) - 0개",
-                        "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                        "6개 일치 (2,000,000,000원) - 0개",
-                        "아쉽게도 수익이 발생하지 않았습니다. 다음 기회를 노려보세요!");
-            },
-            List.of(8, 9, 10, 11, 12, 13),
-            List.of(14, 15, 16, 17, 18, 19),
-            List.of(20, 21, 22, 23, 24, 25),
-            List.of(26, 27, 28, 29, 30, 31),
-            List.of(32, 33, 34, 35, 36, 37));
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 62.5%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
     }
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(
-            () -> {
-                runException("1000j");
-                assertThat(output()).contains(ERROR_MESSAGE);
-            });
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Override
     public void runMain() {
-        Application.main(new String[] {});
+        Application.main(new String[]{});
     }
 }
