7af5692173177032ab1269a05a9f85a1ddec8d8e
yeong-hwan <jghff700@naver.com>
2024-11-03 19:31:20 +0900

    feat(service): generate lottos using random numbers

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.entity.Lotto;
+import lotto.entity.Lottos;
+import lotto.entity.WinningNumbers;
+import lotto.enums.LottoConstants;
+import lotto.enums.Prize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoService {
+
+    public Lottos generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / LottoConstants.LOTTO_PRICE.getValue();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                    LottoConstants.MIN_LOTTO_NUMBER.getValue(),
+                    LottoConstants.MAX_LOTTO_NUMBER.getValue(),
+                    LottoConstants.LOTTO_NUMBERS_COUNT.getValue()
+            );
+            lottos.add(new Lotto(numbers));
+        }
+        return new Lottos(lottos);
+    }
+}
