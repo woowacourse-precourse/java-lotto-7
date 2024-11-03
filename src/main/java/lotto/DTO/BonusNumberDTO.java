0290ebb1c7508d73858da19b477693f0ae5fffa6
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 04:07:21 +0900

    feat(BonusNumberDTO): 보너스 번호를 전달하는 DTO 추가

--- src/main/java/lotto/DTO/BonusNumberDTO.java
+++ src/main/java/lotto/DTO/BonusNumberDTO.java
@@ -1,4 +1,13 @@
 package lotto.DTO;
 
 public class BonusNumberDTO {
+    private final Integer bonusNumber;
+
+    public BonusNumberDTO(Integer bonusNumberInput) {
+        this.bonusNumber = bonusNumberInput;
+    }
+
+    public Integer getBonusNumber() {
+        return bonusNumber;
+    }
 }
