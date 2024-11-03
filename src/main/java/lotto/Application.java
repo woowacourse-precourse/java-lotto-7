7a04475090d06bebae5d66e2c3376f30a7c8fe9b
howareyoo <newworld121@naver.com>
2024-11-03 11:02:57 +0900

    feat: add main start method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+        LottoController lottoController = new LottoController(view);
+        lottoController.startLotto();
     }
 }
