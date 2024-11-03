41ebd0c147187dfa94478d6f657e8d8e1ff38990
고희준 <kohuijun@gohuijun-ui-MacBookAir.local>
2024-11-03 15:58:03 +0900

    refactor: LottoRank를 요구사항에 맞게 enum으로 변경

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -2,25 +2,25 @@ package lotto.model;
 
 import java.util.List;
 
-public class LottoRank {
+public enum LottoRank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    MISS(0, false, 0);
+
     private final int matchCount;
     private final boolean matchBonus;
     private final int prize;
 
-    public static final LottoRank FIRST = new LottoRank(6, false, 2000000000);
-    public static final LottoRank SECOND = new LottoRank(5, true, 30000000);
-    public static final LottoRank THIRD = new LottoRank(5, false, 1500000);
-    public static final LottoRank FOURTH = new LottoRank(4, false, 50000);
-    public static final LottoRank FIFTH = new LottoRank(3, false, 5000);
-    public static final LottoRank MISS = new LottoRank(0, false, 0);
-
-    public static final List<LottoRank> ranks = List.of(FIRST, SECOND, THIRD, FOURTH, FIFTH, MISS);
-
-    private LottoRank(int matchCount, boolean matchBonus, int prize) {
+    LottoRank(int matchCount, boolean matchBonus, int prize) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prize = prize;
     }
+
+    public static final List<LottoRank> ranks = List.of(FIRST, SECOND, THIRD, FOURTH, FIFTH, MISS);
 
     public static LottoRank getRank(int matchCount, boolean matchBonus) {
         if (matchCount < 3) {
