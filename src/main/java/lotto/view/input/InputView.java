b290e20b3b47565a9804d01cc61a77599c6b2b56
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-03 17:25:58 +0900

    refactor($InputView): InputView 인터페이스로 변경

--- src/main/java/lotto/view/input/InputView.java
+++ src/main/java/lotto/view/input/InputView.java
@@ -1,46 +1,12 @@
 package lotto.view.input;
 
-import camp.nextstep.edu.missionutils.Console;
-import lotto.view.exception.InvalidInputException;
-
-import java.util.Arrays;
 import java.util.List;
 
-public class InputView {
+public interface InputView {
 
-    private static final String PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    Integer inputPurchaseMoney();
 
-    public Integer inputPurchaseMoney() {
-        System.out.println(PURCHASE_MONEY_MESSAGE);
+    List<Integer> inputWinningNumbers();
 
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw InvalidInputException.invalidNumber();
-        }
-    }
-
-    public List<Integer> inputWinningNumbers() {
-        System.out.println(WINNING_NUMBER_MESSAGE);
-
-        try {
-            return Arrays.stream(Console.readLine().split(","))
-                    .map(Integer::parseInt)
-                    .toList();
-        } catch (NumberFormatException e) {
-            throw InvalidInputException.invalidNumber();
-        }
-    }
-
-    public int inputBonusNumber() {
-        System.out.println(BONUS_NUMBER_MESSAGE);
-
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (NumberFormatException e) {
-            throw InvalidInputException.invalidNumber();
-        }
-    }
+    int inputBonusNumber();
 }
