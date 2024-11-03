b1f0c7746ac8eab7ff4164f993da8266ef63afa1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-03 16:58:49 +0900

    test(LottoProfitTest): 로또 수익 테스트
    테스트한 내용은 다음과 같습니다:
    - 로또 수익률을 성공적으로 반환 한다

---
+++ src/test/java/lotto/domain/LottoProfitTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoProfitTest {
+
+    @DisplayName("로또 수익률을 성공적으로 반환 한다.")
+    @Test
+    void getProfitRatioTest() {
+        //given
+        final LottoRank rank4 = LottoRank.RANK_4;
+        final int count1 = 2;
+        final LottoRank rank5 = LottoRank.RANK_5;
+        final int count2 = 3;
+        final Map<LottoRank, Integer> lottoRanks = Map.of(rank4, count1, rank5, count2);
+        final Money money = new Money(5000);
+        final BigDecimal totalPrice = rank4.getPrice().multiply(BigDecimal.valueOf(count1))
+                .add(rank5.getPrice().multiply(BigDecimal.valueOf(count2)));
+        final double ratio = money.calculateRatio(totalPrice);
+        final LottoProfit lottoProfit = new LottoProfit(lottoRanks, money);
+        //when
+        final double profitRatio = lottoProfit.getProfitRatio();
+        //then
+        assertThat(profitRatio).isEqualTo(ratio);
+
+    }
+}
