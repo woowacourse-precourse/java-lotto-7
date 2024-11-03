f76120aa37c0659fd4693ac45221014fcf42881d
dawit0905 <dawit0905@gmail.com>
2024-11-03 21:42:42 +0900

    feat: 로또에 중복된 숫자가 입력되면 에러 발생

--- src/main/java/common/ErrorMessage.java
+++ src/main/java/common/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     ERROR_INVALID_WINNER_NUMBER_COUNT("[ERROR] 당첨 번호는 6개여야 합니다."),
     ERROR_NUMBER_OUT_OF_RANGE("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다."),
     ERROR_BONUS_NUMBER_OUT_OF_RANGE("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
-    ERROR_INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다.");
+    ERROR_INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    ERROR_DUPLICATE_LOTTO_NUMBER("[ERROR] 중복된 번호가 있습니다.");
 
     private final String message;
 
