72e9dc648f0c6c7bc462dc732de5ec98e086d839
choign3123 <choign3123@naver.com>
2024-11-03 14:40:07 +0900

    feat(ErrorMessage): 구입 금액이 음수일 경우의 에러메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
 
     INPUT_MUST_NUMERIC("숫자를 입력해야 합니다"),
     INSERT_MONEY_NOT_DIVIDED_1000("구입 금액은 1000원 단위로 떨어져야 합니다"),
+    MONEY_CAN_NOT_MINUS("구입 금액이 음수일 수는 없습니다"),
     WIN_NUMBER_SIZE_MUST_6("당첨 번호는 6개를 입력하여야 합니다"),
     LOTTO_NUMBER_BETWEEN_1_AND_45("로또 번호는 1부터 45 사이의 숫자여야 합니다"),
     DUPLICATE_WIN_NUMBERS("중복된 당첨 번호를 입력하였습니다"),
