28878d4d6ecb854c0237a5884d740b31a27a9fe5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-03 22:38:04 +0900

    refactor(LottoPurchasedAmountInput): 오탈자 수정

--- src/main/java/lotto/dto/LottoPurchasedAmountInput.java
+++ src/main/java/lotto/dto/LottoPurchasedAmountInput.java
@@ -5,21 +5,21 @@ import lotto.exception.LottoException;
 
 public record LottoPurchasedAmountInput(String rawAmount) {
 
-    private static void validateLottoPurchasedAmountNotNull(String RawAmount) {
-        if (RawAmount == null) {
+    private static void validateLottoPurchasedAmountNotNull(String rawAmount) {
+        if (rawAmount == null) {
             throw LottoException.from(ErrorMessage.NULL_INPUT_ERROR);
         }
     }
 
-    private static void validateLottoPurchasedAmountEmpty(String RawAmount) {
-        if (RawAmount.isEmpty()) {
+    private static void validateLottoPurchasedAmountEmpty(String rawAmount) {
+        if (rawAmount.isEmpty()) {
             throw LottoException.from(ErrorMessage.EMPTY_INPUT_ERROR);
         }
     }
 
-    public static LottoPurchasedAmountInput from(String RawAmount) {
-        validateLottoPurchasedAmountNotNull(RawAmount);
-        validateLottoPurchasedAmountEmpty(RawAmount);
-        return new LottoPurchasedAmountInput(RawAmount);
+    public static LottoPurchasedAmountInput from(String rawAmount) {
+        validateLottoPurchasedAmountNotNull(rawAmount);
+        validateLottoPurchasedAmountEmpty(rawAmount);
+        return new LottoPurchasedAmountInput(rawAmount);
     }
 }
