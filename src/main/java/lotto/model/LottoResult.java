8ef1f5fcbdee7add54bb6e7ccfa3b5faccfd2484
acupoframen <choe.hwan@gmail.com>
2024-11-03 10:26:06 +0900

    fix (LottoResult): change two depth - divide method to check if each input is okay

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -13,26 +13,30 @@ public class LottoResult {
         this.bonusNumber = bonusNumber;
     }
 
-
     public Map<Score, Integer> calculateResults(LottoList lottoList) {
         Map<Score, Integer> resultCount = new HashMap<>();
         List<Integer> winningNumbers = winningLotto.getNumbers();
 
         for (Lotto lotto : lottoList.getLottoList()) {
-            int matchCount = 0;
-            boolean bonusMatch = false;
-
-            for (int number : lotto.getNumbers()) {
-                if (winningNumbers.contains(number)) {
-                    matchCount++;
-                }
-                if (number == bonusNumber) {
-                    bonusMatch = true;
-                }
-            }
-            Score score = Score.getScore(matchCount, bonusMatch);
+            Score score = getScoreLotto(lotto, winningNumbers);
             resultCount.put(score, resultCount.getOrDefault(score, 0) + 1);
         }
         return resultCount;
+    }
+
+    private Score getScoreLotto(Lotto lotto, List<Integer> winningNumbers) {
+        int matchCount = countMatchingNumbers(lotto.getNumbers(), winningNumbers);
+        boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
+        return Score.getScore(matchCount, bonusMatch);
+    }
+
+    private int countMatchingNumbers(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int number : lottoNumbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
     }
 }
