d2e5d0097dc96355346fa4a8cc987b6e6b7b96db
heehun <slrkqk2@naver.com>
2024-11-03 17:53:57 +0900

    feat(Controller): 인터페이스 구조로 controller 작성

---
+++ src/main/java/lotto/controller/woowahanLotto.java
@@ -0,0 +1,77 @@
+package lotto.controller;
+
+import static lotto.utils.Converter.StringToPrice;
+import static lotto.utils.Converter.priceToLottoCount;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLottos;
+import lotto.global.LottoRank;
+import lotto.utils.Converter;
+import lotto.utils.Generator;
+import lotto.utils.Validator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class woowahanLotto implements LottoController {
+    @Override
+    public void run() {
+        String input = getLottoCount();
+        int lottoCount = priceToLottoCount(input);
+        int price = StringToPrice(input);
+
+        Lottos lottos = getLottos(lottoCount);
+        OutputView.printLottos(lottos);
+
+        List<Integer> winningNumbers = getWinningNum();
+        int bonusNumber = getBonusNumber();
+
+        WinningLottos winningLottos = getWinningLottos(lottos.getWinningLottos(winningNumbers,bonusNumber),price);
+        OutputView.printResult(winningLottos);
+
+    }
+
+    private String getLottoCount() {
+        try {
+            String input = InputView.getPrice();
+            Validator.priceValidator(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            return getLottoCount();
+        }
+    }
+
+    private Lottos getLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(Generator.lottoNumberGenerate()));
+        }
+        return new Lottos(lottos);
+    }
+
+    private List<Integer> getWinningNum() {
+        try {
+            String input = InputView.getWinningNumber();
+            Validator.lottoNumValidator(input);
+            return Converter.StringToLottoNumbers(input);
+        } catch (IllegalArgumentException e) {
+            return getWinningNum();
+        }
+    }
+
+    private int getBonusNumber() {
+        try {
+            String input = InputView.getBonusNumber();
+            Validator.lottoNumValidator(input);
+            return Converter.StringToBonusNumber(input);
+        } catch (IllegalArgumentException e) {
+            return getBonusNumber();
+        }
+    }
+
+    private WinningLottos getWinningLottos(List<LottoRank> winningLottos, int price) {
+        return new WinningLottos(winningLottos,price);
+    }
+}
--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -10,13 +10,20 @@ public class Converter {
 
     public static int priceToLottoCount(String input) {
         int count = Integer.parseInt(input);
-
         return count/1000;
+    }
+
+    public static int StringToPrice(String input) {
+        return Integer.parseInt(input);
     }
 
     public static List<Integer> StringToLottoNumbers(String input) {
         return Arrays.stream(input.split(LOTTO_NUMBER_DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public static int StringToBonusNumber(String input) {
+        return Integer.parseInt(input);
     }
 }
