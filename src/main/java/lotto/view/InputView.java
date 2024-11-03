c429201b0e3a260fcd189a2c0c6f1a8bffecdcdc
Seol_JY <wlsdud5654@gmail.com>
2024-11-04 00:26:48 +0900

    feat(InputView): 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,14 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String GET_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String GET_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public String getPurchaseAmount() {
         return readLineWithPrompt(GET_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public String getWinningNumbers() {
+        return readLineWithPrompt(GET_WINNING_NUMBERS_MESSAGE);
     }
 
     private String readLineWithPrompt(String prompt) {
