c4a8d87960a14a95c1589cbc0d4344545ebeab9d
Hanjjong <tim4974@naver.com>
2024-11-03 16:48:01 +0900

    test: 5등 당첨 테스트 추가

---
+++ src/test/java/lotto/model/LottoResultTest.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import lotto.enums.LottoRank;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoResultTest {
+
+    private LottoResult lottoResult;
+
+    private WinningLotto winningLotto;
+
+    @BeforeEach
+    void setUp() {
+        lottoResult = new LottoResult();
+        winningLotto = WinningLotto.create("1,2,3,4,5,6");
+        winningLotto.addBonusNumber("7");
+    }
+
+    @Test
+    @DisplayName("5등 당첨 테스트")
+    public void winningLottoCompareTest_5th() {
+        LottoTicket lottoTicket = new LottoTicket(List.of(
+                new Lotto(List.of(1, 2, 3, 8, 9, 10))
+        ));
+
+        lottoResult.compare(lottoTicket, winningLotto);
+        Map<LottoRank, Integer> results = lottoResult.getRankResults();
+
+        assertThat(results.get(LottoRank.FIFTH)).isEqualTo(1);
+    }
+}
