f3198fef6953f893aa90d3da2805b3b0537733e6
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 22:46:06 +0900

    feat(AmountValidator): 금액이 숫자가 아닐 경우 예외 발생 기능 추가

--- src/main/java/lotto/utils/AmountValidator.java
+++ src/main/java/lotto/utils/AmountValidator.java
@@ -7,5 +7,13 @@ public class AmountValidator {
         }
     }
 
+    private static void validateIsNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
+        }
+    }
+
 
 }
