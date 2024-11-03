500233723192668e7a886e52cc28bcffe03c52c5
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-03 14:58:05 +0900

    feat: 파라미터에 초기화 할 객체 넘겨주기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Service.LottoService;
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.ViewFacade;
+
+import javax.swing.text.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ViewFacade viewFacade = new ViewFacade(inputView, outputView);
+        LottoService service = new LottoService();
+
+        LottoController lottoController = new LottoController(viewFacade, service);
+        lottoController.run();
     }
 }
