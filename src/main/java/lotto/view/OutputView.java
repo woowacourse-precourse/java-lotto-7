593fcf87c9572cbab192718eeac96a93dad3f6aa
hyebin <shin378378@naver.com>
2024-11-04 02:12:06 +0900

    feat : 티켓 일치 횟수 계산 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public void outputNumberOfLotto(int numberOfLotto){
@@ -16,7 +18,27 @@ public class OutputView {
         }
     }
 
-    public void outputStatistics(){
+    private void outputMatchCount(Map<LottoRank, Integer> rankCounts){
+        for (Map.Entry<LottoRank, Integer> rankCount : rankCounts.entrySet()) {
+            LottoRank lottoRank = rankCount.getKey();
+            int count = rankCount.getValue();
+            System.out.println(count+"개 일치");
+            if(lottoRank == LottoRank.SECOND){
+                System.out.println(", 보너스 볼 일치");
+            }
+            System.out.printf("("+String.format("%,d",lottoRank.getPrize())+") - "+count+"개");
+        }
+    }
 
+    private void outputProfitRate(double profitRate){
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", profitRate);
+    }
+
+    public void outputStatistics(Map<LottoRank, Integer> rankCounts, double profitRate){
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        outputMatchCount(rankCounts);
+        System.out.println();
+        outputProfitRate(profitRate);
     }
 }
