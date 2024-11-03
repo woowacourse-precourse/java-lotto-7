f6c93abf63be1ab1a232f4add010de95e3f465b8
Minuring <mw9505@gmail.com>
2024-11-03 15:33:19 +0900

    feat(service): 구매 서비스
    LottoPurchase purchase(int, NumbersSelector): 구입금액과 숫자 선택전략을 받아 로또를 구매하는 메서드

---
+++ src/main/java/lotto/service/purchase/PurchaseServiceImpl.java
@@ -0,0 +1,41 @@
+package lotto.service.purchase;
+
+import static lotto.constant.Error.INVALID_UNIT_PURCHASE;
+import static lotto.constant.Error.MAX_AMOUNT_PURCHASE;
+import static lotto.constant.Error.MIN_AMOUNT_PURCHASE;
+import static lotto.constant.LottoConstant.PRICE;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.domain.Lotto;
+import lotto.domain.LottoPurchase;
+import lotto.service.NumbersSelector;
+
+public class PurchaseServiceImpl implements PurchaseService {
+
+    @Override
+    public LottoPurchase purchase(int payAmount, NumbersSelector selector) {
+        validatePayAmount(payAmount);
+        int lottoAmount = payAmount / PRICE;
+
+        List<Lotto> lottos = IntStream.range(0, lottoAmount)
+            .mapToObj(sequence -> new Lotto(selector.select()))
+            .toList();
+
+        return LottoPurchase.purchase(lottos);
+    }
+
+    private static void validatePayAmount(int payAmount) {
+        if (payAmount == 0) {
+            throw new IllegalArgumentException(MIN_AMOUNT_PURCHASE);
+        }
+
+        if (payAmount % PRICE != 0) {
+            throw new IllegalArgumentException(INVALID_UNIT_PURCHASE);
+        }
+
+        if (payAmount / PRICE > 1000) {
+            throw new IllegalArgumentException(MAX_AMOUNT_PURCHASE);
+        }
+    }
+}
