04eaac6505a6821226c44a6af6c80f13492c1b6b
KNU-K <knukang334@gmail.com>
2024-11-03 16:23:41 +0900

    feat(ProfitRate): 수익률 도메인 구성
    BigDecimal 형식으로 수익률을 계산하고 처리하는 기능을 담당한다.

---
+++ src/main/java/lotto/model/ProfitRate.java
@@ -0,0 +1,56 @@
+package lotto.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import lotto.exception.ErrorMessages;
+import lotto.exception.LottoException;
+
+/**
+ * ProfitRate는 수익률을 나타내는 도메인 클래스입니다.
+ */
+public class ProfitRate {
+    private final BigDecimal rate;
+
+    private ProfitRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public static ProfitRate from(int totalProfits, Money originMoney) {
+        if (originMoney == null) {
+            throw new LottoException(ErrorMessages.ORIGIN_MONEY_NULL);
+        }
+        if (originMoney.isZero()) {
+            return new ProfitRate(BigDecimal.ZERO);
+        }
+        return new ProfitRate(calculateRate(totalProfits, originMoney));
+    }
+
+    private static BigDecimal calculateRate(int totalProfits, Money originMoney) {
+        return BigDecimal.valueOf(totalProfits)
+                .divide(originMoney.toBigDecimal(), 3, BigDecimal.ROUND_HALF_UP)
+                .multiply(BigDecimal.valueOf(100))
+                .max(BigDecimal.ZERO);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ProfitRate that = (ProfitRate) o;
+        return Objects.equals(rate, that.rate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rate);
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+}
