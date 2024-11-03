5fe6fd93fbc9a3edf8faa986a8d6f71820694320
yush <yush2202@gmail.com>
2024-11-04 04:15:42 +0900

    refactor($ ErrorMessages): 에러 문구 수정
    사용 위치를 고려해서 문구 수정

--- src/main/java/lotto/constants/ErrorMessages.java
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -3,10 +3,9 @@ package lotto.constants;
 public enum ErrorMessages {
     ERROR_PREFIX("[ERROR] "),
     INVALID_USER_INPUT("입력은 공백이거나 없을 수 없습니다."),
-    INVALID_DIGIT("숫자가 아닙니다."),
+    INVALID_DIGIT("자연수가 아닙니다."),
     INVALID_LOTTO_NUMBER_RANGE("로또 숫자는 1이상, 45이하의 자연수만 가능합니다."),
     INVALID_LOTTO_COUNT("로또 번호는 6개여야 합니다."),
-    INVALID_BONUS_COUNT("보너스 번호는 1개여야 합니다."),
     INVALID_LOTTO_DUPLICATED("로또 숫자는 중복될 수 없습니다."),
     INVALID_MONEY_RANGE("구입 금액은 0원 초과, 10만원 이하만 가능합니다."),
     INVALID_MONEY_UNIT("구입 금액은 천원 단위만 가능합니다.");
