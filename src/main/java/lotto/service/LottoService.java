079ef26ba098d8d2e2e305b318ccd070bad913eb
scc9811 <wjsrudtjr98@naver.com>
2024-11-03 19:11:27 +0900

    fix : 로또 번호 오름차순 정렬 로직 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,6 +4,7 @@ import lotto.Lotto;
 import lotto.util.ParseUtil;
 import lotto.util.RandomUtil;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +26,11 @@ public class LottoService {
 
     public List<Lotto> generateLottos(int size) {
         List<Lotto> lottos = IntStream.range(0, size)
-                .mapToObj(i -> new Lotto(RandomUtil.getSixRandomNumbers(1, 45)))
+                .mapToObj(i -> {
+                    Lotto lotto = new Lotto(RandomUtil.getSixRandomNumbers(1, 45));
+                    Collections.sort(lotto.getNumbers()); // Lotto의 numbers를 정렬
+                    return lotto;
+                })
                 .collect(Collectors.toList());
         return lottos;
     }
