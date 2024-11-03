af3dc90e25b8e1d8fd4674d7393651265d577b6f
heehun <slrkqk2@naver.com>
2024-11-03 16:04:10 +0900

    feat(InputView): 사용자 입력 메시지 추가 및 입력 메서드 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public static final String PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static String getPrice() {
+        System.out.println(PRICE_INPUT_MESSAGE);
+        return readLine();
+    }
+
+    public static String getWinningNumber() {
+        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        return readLine();
+    }
+
+    public static String getBonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        return readLine();
+    }
+}
