238d1ce19e8166f990f48808ffd9b4707643cc12
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:11:15 +0900

    feat (Output) : OutputView 로 통합하고 삭제

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,24 @@
 package lotto.view;
-
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
 
 public class OutputView {
-    public void displayBuyResult(Lottos lottos) {
+
+    private static final String START_SHOW_RESULT = "당첨 통계";
+    private static final String DELIMITER = "---";
+
+    public void displayBuyLottos(Lottos lottos) {
         long lottoAmount = lottos.getSize();
         System.out.println(lottoAmount + "개를 구매했습니다.");
-        String result = lottos.result();
-        System.out.println(result);
+        System.out.println(lottos.result());
+    }
+
+    public void displayWinningResult(LottoResult lottoResult) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(START_SHOW_RESULT).append("\n");
+        sb.append(DELIMITER).append("\n");
+        sb = lottoResult.result(sb);
+        sb.append("총 수익률은 ").append(lottoResult.computeProfitRate()).append("% 입니다.");
+        System.out.println(sb);
     }
 }
