86077f0daf3fbc7013a28212efc84fad84a588b4
choiyoung69 <choiyoung609@gmail.com>
2024-11-04 05:50:00 +0900

    feat(inputView): 입력 메서드 정의

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,23 @@
 package lotto.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.view.ViewMessages.REQUIRE_BONUS_NUMBER;
+import static lotto.view.ViewMessages.REQUIRE_PURCHASE_AMOUNT;
+import static lotto.view.ViewMessages.REQUIRE_WINNING_NUMBERS;
+
 public class InputView {
-    
+    public static void inputLottoPurchaseAmount() {
+        System.out.println(REQUIRE_PURCHASE_AMOUNT);
+        readLine();
+    }
+
+    public static void inputWinningNumbers() {
+        System.out.println(REQUIRE_WINNING_NUMBERS);
+        readLine();
+    }
+
+    public static void inputBonusNumber() {
+        System.out.println(REQUIRE_BONUS_NUMBER);
+        readLine();
+    }
 }
--- src/main/java/lotto/view/ViewMessages.java
+++ src/main/java/lotto/view/ViewMessages.java
@@ -1,10 +1,10 @@
 package lotto.view;
 
 public class ViewMessages {
-    private static final String REQUIRE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String ENTER_NUMBER_OR_PURCHASES = "개를 구매했습니다.";
-    private static final String REQUIRE_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String REQUIRE_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private static final String ENTER_STATISTICS = "당첨 통계";
-    private static final String ENTER_PROFIT_MESSAGES = "총 수익률은 %.2f%%입니다.";
+    public static final String REQUIRE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String ENTER_NUMBER_OR_PURCHASES = "개를 구매했습니다.";
+    public static final String REQUIRE_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public static final String REQUIRE_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String ENTER_STATISTICS = "당첨 통계";
+    public static final String ENTER_PROFIT_MESSAGES = "총 수익률은 %.2f%%입니다.";
 }
