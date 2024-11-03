004be0f382ad0da964cc7d753337ffa5d5da3ebc
changuii <rhljh7410@gmail.com>
2024-11-03 14:03:38 +0900

    test : LottoBonusNumberTest의 이름을 BonusNumberTest로 변경하라
    - 테스트하려는 클래스와 다른 이름을 일치하게 만들었다.

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class LottoBonusNumberTest {
+public class BonusNumberTest {
     private static final LottoConfig CONFIG = LottoConfig.WOOWA_CONFIG;
     private WinningLotto winningLotto;
 
