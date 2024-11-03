6724b47d0b208c406a4db1373c6df8efb7263f01
KSJ0128 <ksj01128@naver.com>
2024-11-04 00:54:29 +0900

    feat: Purchase DTO 구현
    유저 로또 구매 금액을 담을 DTO Purchase를 구현했습니다.

---
+++ src/main/java/lotto/dto/Purchase.java
@@ -0,0 +1,11 @@
+package lotto.dto;
+
+import java.math.BigInteger;
+
+public class Purchase {
+    BigInteger money;
+
+    public Purchase(BigInteger money) {
+        this.money = money;
+    }
+}
