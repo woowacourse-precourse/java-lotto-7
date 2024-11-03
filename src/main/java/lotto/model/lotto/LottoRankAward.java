3b8ec1aba2f90e6f4d88beac9fa04c8e7979eb43
양효빈 <hyobin3026@naver.com>
2024-11-04 00:26:21 +0900

    feat(LottoRankAward): Add enum to represent lotto ranks and prizes - Define each rank with matching criteria and prize information - Implement findRank method to determine rank based on matched numbers

---
+++ src/main/java/lotto/model/lotto/LottoRankAward.java
@@ -0,0 +1,46 @@
+package lotto.model.lotto;
+
+public enum LottoRankAward {
+
+    FIRST_RANK(6, 2_000_000_000, false),
+    SECOND_RANK(5, 30_000_000, true),
+    THIRD_RANK(5,  1_500_000,false),
+    FORTH_RANK(4,  50_000, false),
+    FIFTH_RANK(3, 5_000, false);
+
+    private final int matchedNumberCount;
+    private final int prizeAmount;
+    private final boolean isBonusNumberMatched;
+
+    LottoRankAward(int matchedNumberCount, int prizeAmount, boolean isBonusNumberMatched) {
+        this.matchedNumberCount = matchedNumberCount;
+        this.prizeAmount = prizeAmount;
+        this.isBonusNumberMatched = isBonusNumberMatched;
+    }
+
+    public int getMatchLottoNumberCount() {
+        return matchedNumberCount;
+    }
+
+    public int getWinningMoneyPrize() {
+        return prizeAmount;
+    }
+
+    public boolean getIsBonusMatched() {
+        return isBonusNumberMatched;
+    }
+
+    public static LottoRankAward findRank(int matchedCount, boolean isBonusNumberMatched) {
+        for (LottoRankAward rank : values()) {
+            if (rank.matchedNumberCount == matchedCount && rank.isBonusNumberMatched == isBonusNumberMatched) {
+                return rank;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+}
