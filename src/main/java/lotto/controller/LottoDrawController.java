b2f0ed45766b6d6a0bd29fb28e233b8ae89cfa0f
Chan531 <chan657312@gmail.com>
2024-11-03 18:53:46 +0900

    feat(LottoDrawController): 인스턴스 변수 추가
    LottoCreator 추가

--- src/main/java/lotto/controller/LottoDrawController.java
+++ src/main/java/lotto/controller/LottoDrawController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.model.LottoCreator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -7,6 +8,7 @@ public class LottoDrawController {
 
 	private final InputView inputView;
 	private final OutputView outputView;
+	private final LottoCreator lottoCreator;
 
 	public LottoDrawController(InputView inputView, OutputView outputView) {
 		this.inputView = inputView;
