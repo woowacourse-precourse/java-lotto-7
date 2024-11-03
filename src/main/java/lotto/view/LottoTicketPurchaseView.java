46f278408a217bbf47b7045437a6e6ef65d6c54e
koo-sangwoo <luckytkddn@gmail.com>
2024-11-03 15:34:56 +0900

    fix : 구매 개수 멘트 변경
    - 구매한 개수를 나타내는 멘트를 요구사항대로 변경했습니다.

--- src/main/java/lotto/view/LottoTicketPurchaseView.java
+++ src/main/java/lotto/view/LottoTicketPurchaseView.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class LottoTicketPurchaseView {
 
     public void printPurchaseCount(int purchaseCount) {
-        System.out.println( purchaseCount+ "개를 구입하였습니다.");
+        System.out.println( purchaseCount+ "개를 구매했습니다.");
     }
 
     public void printLottoPurchase(List<Lotto> lottoList) {
