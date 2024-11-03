7ff34f46e4abb7d78c26fbf054ea99cb308ea46d
swhyeon98 <smreo75@gmail.com>
2024-11-03 22:49:19 +0900

    feat(Lotto): 로또 번호 숫자 범위 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,6 +15,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        validateLottoNumberRange(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -27,6 +28,14 @@ public class Lotto {
         Set<Integer> uniqueNumber = new HashSet<>(numbers);
         if (uniqueNumber.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.서로 다른 숫자 6개를 입력해주세요.");
+        }
+    }
+
+    private void validateLottoNumberRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+            }
         }
     }
 }
