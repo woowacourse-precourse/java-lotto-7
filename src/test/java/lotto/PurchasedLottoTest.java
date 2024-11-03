5f55a581d6b7f9bae22a747d1fd8cc67653d70c4
GoGradually <chkun3109@gmail.com>
2024-11-03 21:27:36 +0900

    refactor(PurchasedLottoTest.java): Map의 Null 처리 방식 변경
    - null 처리를 get() 메소드 직접 사용에서 getOrDefault()으로 변경

--- src/test/java/lotto/PurchasedLottoTest.java
+++ src/test/java/lotto/PurchasedLottoTest.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,10 +18,47 @@ class PurchasedLottoTest {
         int generateCount = 10;
 
         //when
-        PurchasedLotto purchasedLotto = PurchasedLotto.generateLottos(generateCount);
+        PurchasedLotto purchasedLotto = PurchasedLotto
+                .generateLottos(
+                        generateCount,
+                        () -> Randoms.pickUniqueNumbersInRange(1,45,6)
+                );
 
         //then
         assertThat(purchasedLotto.getSize()).isEqualTo(generateCount);
+    }
+
+    @Test
+    void 로또_당첨_검사_1등(){
+        //given
+        int generateCount = 3;
+        PurchasedLotto purchasedLotto = PurchasedLotto.generateLottos(generateCount, () -> List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        Map<Prize, Integer> result = purchasedLotto.checkWin(List.of(1, 2, 3, 4, 5, 6), 7);
+
+        //then
+        assertThat(result.getOrDefault(Prize.FIRST_PLACE, 0)).isEqualTo(generateCount);
+        assertThat(result.getOrDefault(Prize.SECOND_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.THIRD_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.FOURTH_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.FIFTH_PLACE, 0)).isZero();
+    }
+    @Test
+    void 로또_당첨_검사_2등(){
+        //given
+        int generateCount = 3;
+        PurchasedLotto purchasedLotto = PurchasedLotto.generateLottos(generateCount, () -> List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        Map<Prize, Integer> result = purchasedLotto.checkWin(List.of(1, 2, 3, 4, 5, 7), 6);
+
+        //then
+        assertThat(result.getOrDefault(Prize.FIRST_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.SECOND_PLACE, 0)).isEqualTo(3);
+        assertThat(result.getOrDefault(Prize.THIRD_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.FOURTH_PLACE, 0)).isZero();
+        assertThat(result.getOrDefault(Prize.FIFTH_PLACE, 0)).isZero();
     }
 
 }
