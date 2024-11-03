6c8f79321e6e97032a47253c1682601910a101fa
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 04:04:25 +0900

    refactor(LottoPrize): 당첨상금 enum 정렬 변경
    - MATCH_COUNT_5_WITH_BONUS의 위치를 변경하여 코드 가독성 개선

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -4,8 +4,8 @@ public enum LottoPrize {
     MATCH_COUNT_3(3, 5000),
     MATCH_COUNT_4(4, 50000),
     MATCH_COUNT_5(5, 1500000),
-    MATCH_COUNT_6(6, 2000000000),
-    MATCH_COUNT_5_WITH_BONUS(7, 30000000);
+    MATCH_COUNT_5_WITH_BONUS(7, 30000000),
+    MATCH_COUNT_6(6, 2000000000);
 
     private final int matchCount;
     private final int prize;
