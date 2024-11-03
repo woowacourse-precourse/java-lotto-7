96455ad3c201f28be003c1c0625c865ad7e45333
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-03 12:45:00 +0900

    refactor(LottoPublication): 책임 분리
    - 로또 번호를 발행하는 목적을 가지고 있는 메서드를 분리하였습니다.

--- src/main/java/lotto/Model/LottoPublication.java
+++ src/main/java/lotto/Model/LottoPublication.java
@@ -1,22 +1,30 @@
 package lotto.Model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoPublication {
-    private List<Integer> publicationNumbers;
 
-    public void lottoNumberPublication() {
-        publicationNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public List<Integer> lottoNumberPublication() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
     }
 
-    public void publicationNumbersArray() {
-        lottoNumberPublication();
-        Collections.sort(publicationNumbers);
+    public List<Integer> getSortedLottoNumbers() {
+        List<Integer> numbers = lottoNumberPublication();
+        Collections.sort(numbers);
+        return numbers;
     }
 
-    public List<Integer> getPublicationNumbers() {
+    public List<List<Integer>> totalLotto(int count) {
+        List<List<Integer>> publicationNumbers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            publicationNumbers.add(getSortedLottoNumbers());
+        }
+
         return publicationNumbers;
     }
 }
