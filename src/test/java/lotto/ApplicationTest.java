973344c28abb5a89998d47fe08571830749b4564
yeong-hwan <jghff700@naver.com>
2024-11-03 21:22:17 +0900

    test(integration): display winning results based on matching numbers

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -78,6 +78,31 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    @DisplayName("당첨 결과를 계산하고 출력")
+    void displayWinningResultsBasedOnMatchingNumbers() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("3000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "3개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[1, 2, 3, 35, 36, 44]",  // 3개 일치
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 166.7%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(1, 2, 3, 35, 36, 44)
+        );
+    }
+
 
     @Override
     public void runMain() {
