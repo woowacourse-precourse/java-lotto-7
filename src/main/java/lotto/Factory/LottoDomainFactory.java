ef3185d307a227e807cdc12df74ca04b4ad11a90
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:37:55 +0900

    feat(LottoDomainFactory): 도메인 객체를 생성하는 로직 추가
    - createPurchasePriceCalculator() 메서드로 PurchasePriceCalculator 객체 생성
    - createRandomLottoNumbersGenerator() 메서드로 RandomLottoNumbersGenerator 객체 생성

--- src/main/java/lotto/Factory/LottoDomainFactory.java
+++ src/main/java/lotto/Factory/LottoDomainFactory.java
@@ -1,4 +1,19 @@
 package lotto.Factory;
 
+import lotto.Domain.PrizeMoneyCalculator;
+import lotto.Domain.PurchasePriceCalculator;
+import lotto.Domain.RandomLottoNumbersGenerator;
+
 public class LottoDomainFactory {
+    public PrizeMoneyCalculator createPrizeMoneyCalculator() {
+        return new PrizeMoneyCalculator();
+    }
+
+    public PurchasePriceCalculator createPurchasePriceCalculator() {
+        return new PurchasePriceCalculator();
+    }
+
+    public RandomLottoNumbersGenerator createRandomLottoNumbersGenerator() {
+        return new RandomLottoNumbersGenerator();
+    }
 }
