5ecf9ec21ed46d8aa2cf53055e593f8e017e7aa0
Zepelown <dbswjdqhd12@gmail.com>
2024-11-03 19:51:18 +0900

    feat: Add Lotto number validation in Lotto class
    - Add Lotto number duplicate validation
    - Add Lotto number ranged validation

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,8 +17,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != 6 || hasNotNumberRanged(numbers) || hasDuplicationNumber(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 중복되지 않은 자연수 6개여야 합니다.");
         }
+    }
+    private boolean hasNotNumberRanged(List<Integer> numbers){
+        for (int number : numbers){
+            if (number < 0 || number > 45){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean hasDuplicationNumber(List<Integer> numbers){
+        Set<Integer> uniqueNames = new HashSet<>(numbers);
+        if (uniqueNames.size() < numbers.size()) {
+            return true;
+        }
+        return false;
     }
 }
