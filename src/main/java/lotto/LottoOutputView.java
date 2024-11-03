d60d54c1e6ffc02943ded95b7c6d9afc649245a7
mins-git <m2insapp@gmail.com>
2024-11-03 15:01:26 +0900

    feat(LottoOutputView): 결과 출력 기능 구현   - 구매 결과 출력 기능   - 당첨 통계 출력 기능   - 수익률 출력 기능   - 에러 메시지(컨트롤러) 출력 기능

--- src/main/java/lotto/LottoOutputView.java
+++ src/main/java/lotto/LottoOutputView.java
@@ -1,4 +1,34 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class LottoOutputView {
+    public void printPurchaseResult(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void printWinningStatistics(Map<WinningRank, Integer> statistics, double returnRate) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        Arrays.stream(WinningRank.values())
+                .filter(rank -> rank != WinningRank.NONE)
+                .forEach(rank -> printWinningRank(rank, statistics.getOrDefault(rank, 0)));
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", returnRate);
+    }
+
+    private void printWinningRank(WinningRank rank, int count) {
+        System.out.printf("%s (%,d원) - %d개\n",
+                rank.getDescription(),
+                rank.getPrize(),
+                count);
+    }
+
+    public void printError(String message) {
+        System.out.println(message);
+    }
+
+
 }
