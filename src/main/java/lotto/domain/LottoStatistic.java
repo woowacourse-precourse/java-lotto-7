ea1b711b73e34fd7a8a456047a21d583f49efc3e
장영재 <urinaner@naver.com>
2024-11-04 02:46:59 +0900

    feat(lottoStatistic): 당첨 통계 계산 메서드 구현

--- src/main/java/lotto/domain/LottoStatistic.java
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -1,5 +1,34 @@
 package lotto.domain;
 
-public class LottoStatistic {
+import java.util.HashMap;
+import java.util.Map;
 
+public class LottoStatistic {
+    private final Map<WinningRank, Integer> rankCount;
+    private final int totalPrice;
+
+    public LottoStatistic(Lottos lottos, WinningLotto winningLotto, int totalPrice) {
+        this.rankCount = initializeRankCount();
+        calculateRank(lottos, winningLotto);
+        this.totalPrice = totalPrice;
+    }
+
+    private Map<WinningRank, Integer> initializeRankCount() {
+        Map<WinningRank, Integer> rankCount = new HashMap<>();
+        for (WinningRank rank : WinningRank.values()) {
+            rankCount.put(rank, 0);
+        }
+        return rankCount;
+    }
+
+    private void calculateRank(Lottos lottos, WinningLotto winningLotto) {
+        for (Lotto lotto : lottos.getLottos()) {
+            WinningRank rank = winningLotto.match(lotto);
+            rankCount.put(rank, rankCount.get(rank) + 1);
+        }
+    }
+
+    public Map<WinningRank, Integer> getRankCount() {
+        return new HashMap<>(rankCount);
+    }
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,10 +1,15 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
     private List<Lotto> lottos;
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return new ArrayList<>(lottos);
     }
 }
