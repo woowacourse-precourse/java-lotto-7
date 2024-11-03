38560206c32cf5a309952233088aa49647366c0a
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:19:50 +0900

    refactor (InputMoney) : record class 로 변경

--- src/main/java/lotto/domain/InputMoney.java
+++ src/main/java/lotto/domain/InputMoney.java
@@ -3,23 +3,20 @@ package lotto.domain;
 import static lotto.utils.Constant.LOTTO_PRICE;
 import static lotto.utils.Constant.MINIMUM_PRICE;
 
-public class InputMoney {
-    private final long amount;
-
-    public InputMoney(long amount) {
+public record InputMoney(long amount) {
+    public InputMoney {
         validateInputMoney(amount);
-        this.amount = amount;
     }
 
     public long getBuyAmount() {
+        if (this.amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 입력해야 합니다");
+        }
         return amount / LOTTO_PRICE;
     }
 
     private void validateInputMoney(long userInputMoney) {
-        if(userInputMoney % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 입력해야 합니다");
-        }
-        if(userInputMoney < MINIMUM_PRICE) {
+        if (userInputMoney < MINIMUM_PRICE) {
             throw new IllegalArgumentException("[ERROR] 0원 이상 결제해야 합니다.");
         }
     }
