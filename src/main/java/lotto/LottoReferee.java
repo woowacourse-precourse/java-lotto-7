fde01e37d62a0b6be40cfd89db9c16b694e072c2
MinseokGo <rhalstjr1999@naver.com>
2024-11-03 18:42:15 +0900

    feat: 로또 심판 기능

---
+++ src/main/java/lotto/LottoReferee.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoReferee {
+
+    private final LottoManager lottoManager;
+
+    public LottoReferee(final LottoManager lottoManager) {
+        this.lottoManager = lottoManager;
+    }
+
+    public void judgeWinning() {
+        List<WinningStatus> winningStatuses = lottoManager.compareWinningLotto();
+    }
+}
