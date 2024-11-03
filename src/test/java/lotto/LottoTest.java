7a2e445ec6a76bfe8156c18c1245b0e716e9aedf
sjsk3232 <sjsk3232@gmail.com>
2024-11-04 03:13:51 +0900

    test: Lotto 클래스 테스트 코드 작성
    로또 번호 개수 확인, 로또 번호 중복 여부 확인, 로또 번호 범위 확인, 로또
    번호 정렬 상태 확인 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,23 +4,41 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 6개여야 합니다.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 각각의 로또 번호는 중복되지 않아야 합니다.");
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호 중 1에서 45를 벗어난 숫자가 있다면 예외가 발생한다.")
+    void lottoNumberBoundaryTest() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 생성시 로또 번호가 정렬되어 있어야 한다.")
+    void lottoNumbersOrderTest() {
+        List<Integer> numbers = Arrays.asList(5, 2, 8, 44, 1, 32);
+        Lotto lotto = new Lotto(numbers);
+        assertThat(lotto.getNumbers()).contains("[1, 2, 5, 8, 32, 44]");
+    }
 }
