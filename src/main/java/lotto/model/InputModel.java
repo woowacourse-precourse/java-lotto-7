d1cb6a08d6315f0a9ef66a9b618c1c1069287bee
mipangg <morganjeong119@gmail.com>
2024-11-03 17:14:03 +0900

    refactor(InputModel): rename method convertText to validateNumbers

--- src/main/java/lotto/model/InputModel.java
+++ src/main/java/lotto/model/InputModel.java
@@ -3,6 +3,7 @@ package lotto.model;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.model.Lotto;
 
 // Input에 필요한 메서드 모음
 public class InputModel {
@@ -10,6 +11,7 @@ public class InputModel {
     // 구입금액 입력 받은 후 반환
     public int getPrice() {
         int price;
+
         try {
             price = validatePrice(Console.readLine());
         } catch (IllegalArgumentException e) {
@@ -22,11 +24,11 @@ public class InputModel {
 
     // 당첨번호 입력 받은 후 Lotto 객체로 반환
     public Lotto getWinningNumbers() {
-        Lotto winningNumbers;
         List<Integer> numbers;
+        Lotto winningNumbers;
 
         try {
-            numbers = convertText(Console.readLine());
+            numbers = validateNumbers(Console.readLine());
             winningNumbers = new Lotto(numbers);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -36,9 +38,10 @@ public class InputModel {
         return winningNumbers;
     }
 
-    // 보너스 번호 입력
+    // 보너스 번호 입력 받은 후 반환
     public int getBonusNumber(List<Integer> winningNumbers) {
         int bonusNumber;
+
         try {
             bonusNumber = validateBonusNumber(Console.readLine(), winningNumbers);
         } catch (IllegalArgumentException e) {
@@ -52,6 +55,7 @@ public class InputModel {
     // 보너스 번호의 유효성 체크
     static int validateBonusNumber(String inputNumberText, List<Integer> winningNumbers) {
         int bonusNumber;
+
         isEmptyOrNull(inputNumberText);
         bonusNumber = isNumber(inputNumberText);
         isInRange(bonusNumber);
@@ -78,12 +82,13 @@ public class InputModel {
     }
 
     // 당첨번호 문자열을 체크하고 List<Integer>로 반환
-    static List<Integer> convertText(String text) {
+    static List<Integer> validateNumbers(String inputNumbersText) {
         List<Integer> inputNumbers;
-        isEmptyOrNull(text);
-        text = text.replaceAll(" ", "");
+
+        isEmptyOrNull(inputNumbersText);
+        inputNumbersText = inputNumbersText.replaceAll(" ", "");
         try {
-            inputNumbers = addInputNumbers(text.split(","));
+            inputNumbers = addInputNumbers(inputNumbersText.split(","));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 각 로또 번호는 쉼표(,)로 구분된 숫자여야 합니다.");
         }
@@ -92,6 +97,7 @@ public class InputModel {
 
     private static List<Integer> addInputNumbers(String[] numbers) {
         List<Integer> inputNumbers = new ArrayList<>();
+
         for (String number : numbers) {
             inputNumbers.add(Integer.parseInt(number));
         }
@@ -101,6 +107,7 @@ public class InputModel {
     // 구입금액 입력 유효성 검사
     static int validatePrice(String inputPriceText) {
         int price;
+
         isEmptyOrNull(inputPriceText);
         price = isNumber(inputPriceText);
         isOverThousand(price);
@@ -108,7 +115,6 @@ public class InputModel {
         return price;
     }
 
-    // 구입금액, 당첨 번호 입력 시 공유
     private static void isEmptyOrNull(String inputText) {
         if (inputText == null || inputText.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 빈 입력입니다.");
@@ -117,6 +123,7 @@ public class InputModel {
 
     private static int isNumber(String priceText) {
         int price;
+
         try {
             price = Integer.parseInt(priceText);
         } catch (NumberFormatException e) {
