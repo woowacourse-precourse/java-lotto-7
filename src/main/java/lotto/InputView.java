ded47a08e1d53f80493f8b346690dae35f0fddd4
capDoYeonLee <ghtyu21@naver.com>
2024-11-04 04:28:46 +0900

    feat: 사용자의 입력을 담당하는 객체 개발
    - inputMoney() : 사용자의 돈을 입력받는 기능
    - inputWinningLottoNumbers() : 로또 당첨 번호를 입력받는 기능
    - inputBonusLottoNumber() : 로또 보너스 번호를 입력 받는 기능

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,27 +1,55 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
 
+    Validator validator = new Validator();
+
     public Integer inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        return getValidMoney();
     }
+
+    private Integer getValidMoney() {
+        try {
+            String money = Console.readLine();
+            Integer parsedMoney = validator.validateMoney(money);
+            return parsedMoney;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getValidMoney();
+        }
+    }
+
 
     public List<Integer> inputWinningLottoNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String input = Console.readLine();
-        return Arrays.stream(input.split(","))
+        String[] input = Console.readLine().replace(" ", "").split(",");
+
+        List<Integer> winningNumbers = Arrays.stream(input)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+
+        if (!validator.isValidWinningNumbers(winningNumbers)) {
+            System.out.println("[ERROR] 당첨 번호는 1에서 45 사이의 중복되지 않는 6개의 숫자여야 합니다. 다시 입력해 주세요.");
+            return inputWinningLottoNumbers();
+        }
+        return winningNumbers;
     }
 
-    public int inputBonusLottoNumber() {
+    public int inputBonusLottoNumber(List<Integer> winningNumbers) {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        int bonusNumber = Integer.parseInt(Console.readLine());
+
+        if (!validator.isValidBonusNumber(bonusNumber, winningNumbers)) {
+            System.out.println("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 하며, 당첨 번호와 중복될 수 없습니다. 다시 입력해 주세요.");
+            return inputBonusLottoNumber(winningNumbers);
+        }
+        return bonusNumber;
     }
 }
