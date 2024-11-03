9f6e49b7549167fdf73a519716fe7f35e8ebc053
sseen2 <kse10023@naver.com>
2024-11-03 16:19:59 +0900

    feat: 당첨된 로또 선별 기능 구현

---
+++ src/main/java/lotto/LottoWinning.java
@@ -0,0 +1,66 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LottoWinning {
+    private int bonusNumber;
+    private Lotto winningNumbers;
+    private LottoDraw lottoDraw;
+    private Map<String, Integer> winningLotto;
+
+    public LottoWinning(Lotto winningNumbers, int bonusNumber, LottoDraw lottoDraw) {
+        this.bonusNumber = bonusNumber;
+        this.winningNumbers = winningNumbers;
+        this.lottoDraw = lottoDraw;
+        this.winningLotto = setWinningLotto();
+        calculateWinningResult();
+    }
+
+    public Map<String, Integer> getWinningLotto() {
+        return winningLotto;
+    }
+
+    private Map<String, Integer> setWinningLotto() {
+        Map<String, Integer> setWinningLotto = new LinkedHashMap<>();
+        setWinningLotto.put("3개 일치", 0);
+        setWinningLotto.put("4개 일치", 0);
+        setWinningLotto.put("5개 일치", 0);
+        setWinningLotto.put("5개 일치, 보너스 볼 일치", 0);
+        setWinningLotto.put("6개 일치", 0);
+        return setWinningLotto;
+    }
+
+    private void calculateWinningResult() {
+        for (Lotto lotto : lottoDraw.getLottoDrawNumbers()) {
+            int matchCount = calculateMatchCount(lotto);
+            boolean hasBonus = hasBonusNumber(lotto);
+            String mapKeyString = updateWinningLotto(matchCount, hasBonus);
+            winningLotto.computeIfPresent(mapKeyString, (k, v) -> v + 1);
+        }
+    }
+
+    private int calculateMatchCount(Lotto lotto) {
+        int matchCount = 0;
+        for (int numbers : lotto.getNumbers()) {
+            if (winningNumbers.getNumbers().contains(numbers)) {
+                matchCount++;
+            }
+        }
+
+        return matchCount;
+    }
+
+    private boolean hasBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+
+    private String updateWinningLotto(int matchCount, boolean hasBonus) {
+        if (matchCount == 5 && hasBonus) {
+            return "5개 일치, 보너스 볼 일치";
+        }
+
+        return matchCount + "개 일치";
+    }
+}
