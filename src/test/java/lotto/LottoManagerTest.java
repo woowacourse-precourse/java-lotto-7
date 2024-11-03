ea1a5999a2710be4ee0ba765f1d2d6868fdf3fd1
yaongmeow <minnie_00@naver.com>
2024-11-03 12:54:13 +0900

    test(LottoManager): 테스트코드 오류 수정
    - 불변리스트를 가변리스트로 변경

--- src/test/java/lotto/LottoManagerTest.java
+++ src/test/java/lotto/LottoManagerTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,11 +14,11 @@ public class LottoManagerTest {
     void 당첨_결과를_산출한다() {
         // given
         List<Lotto> myLotto = List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
-                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+                new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
+                new Lotto(new ArrayList<>(List.of(1, 2, 4, 6, 10, 11))),
+                new Lotto(new ArrayList<>(List.of(2, 3, 7, 10, 20, 30)))
         );
-        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Lotto winningLotto = new Lotto(new ArrayList<>(List.of(1, 3, 4, 7, 10, 30)));
         Integer bonusNumber = 8;
         LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
 
@@ -36,11 +37,11 @@ public class LottoManagerTest {
     void 수익률을_산출한다() {
         // given
         List<Lotto> myLotto = List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
-                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+                new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
+                new Lotto(new ArrayList<>(List.of(1, 2, 4, 6, 10, 11))),
+                new Lotto(new ArrayList<>(List.of(2, 3, 7, 10, 20, 30)))
         );
-        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Lotto winningLotto = new Lotto(new ArrayList<>(List.of(1, 3, 4, 7, 10, 30)));
         Integer bonusNumber = 8;
         LottoManager lottoManager = new LottoManager(myLotto, winningLotto, bonusNumber);
 
@@ -55,7 +56,7 @@ public class LottoManagerTest {
     @Test
     void 빈_로또_리스트를_제공한_경우_예외가_발생한다() {
         List<Lotto> myLotto = List.of();
-        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Lotto winningLotto = new Lotto(new ArrayList<>(List.of(1, 3, 4, 7, 10, 30)));
         Integer bonusNumber = 8;
 
         assertThatThrownBy(() -> new LottoManager(myLotto, winningLotto, bonusNumber))
@@ -65,11 +66,11 @@ public class LottoManagerTest {
     @Test
     void 보너스_번호의_범위가_올바르지_않은_경우_예외가_발생한다() {
         List<Lotto> myLotto = List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                new Lotto(List.of(1, 2, 4, 6, 10, 11)),
-                new Lotto(List.of(2, 3, 7, 10, 20, 30))
+                new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
+                new Lotto(new ArrayList<>(List.of(1, 2, 4, 6, 10, 11))),
+                new Lotto(new ArrayList<>(List.of(2, 3, 7, 10, 20, 30)))
         );
-        Lotto winningLotto = new Lotto(List.of(1, 3, 4, 7, 10, 30));
+        Lotto winningLotto = new Lotto(new ArrayList<>(List.of(1, 3, 4, 7, 10, 30)));
         Integer bonusNumber = 50;
 
         assertThatThrownBy(() -> new LottoManager(myLotto, winningLotto, bonusNumber))
