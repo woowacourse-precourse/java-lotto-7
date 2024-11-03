18ca6168338c26cb600684eeb727ab525c8b79f4
윤민수 <koelysa5696@naver.com>
2024-11-03 10:41:16 +0900

    Feat: 로또 구매 가격 검증 클래스
    - 문자나 소수, 오버플로우 예외 확인
    - 음수 예외 확인
    - 천의 배수 입력 예외 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.Controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.run();
         // TODO: 프로그램 구현
     }
 }
