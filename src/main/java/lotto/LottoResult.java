2404381e8a67f117b9c6c3101918cec2efc1fd91
Jungwoo Chu <cjw980930@gmail.com>
2024-11-03 22:30:44 +0900

    fix: 로또 결과 초기값 설정
    null값 안나오도록 모든 rank에 0으로 초기값 설정

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -7,9 +7,12 @@ import java.util.Map;
 public class LottoResult {
     public Map<Rank,Integer> getFinalResult(List<Lotto> lottoTickets, List<Integer> winningNumbers, int bonus){
         Map<Rank,Integer> result = new HashMap<>();
+        for(Rank rank : Rank.values()){
+            result.put(rank,0);
+        }
         for (Lotto lotto : lottoTickets){
             Rank rank = lotto.getResult(winningNumbers,bonus);
-            result.put(rank,result.getOrDefault(rank,0)+1);
+            result.put(rank,result.get(rank)+1);
         }
         return result;
     }
