0440438c41f13884a4e76f1431535d47f2599d9e
byeongdoohan <quden04@gmail.com>
2024-11-03 23:38:18 +0900

    refactor: [ERROR] 추가된 메세지 따로 분리 적용, 에러 문구 수정

--- src/main/java/lotto/exception/LottoErrorMessages.java
+++ src/main/java/lotto/exception/LottoErrorMessages.java
@@ -4,7 +4,7 @@ public enum LottoErrorMessages {
     ERROR_UNIT("[ERROR] "),
     PAY_INPUT_ERROR("숫자만 입력 가능합니다."),
     NOT_ENABLE_AMOUNT_START("수동 구매 갯수는 0부터 "),
-    NOT_ENABLE_AMOUNT_END("까지만 입력할 수 있습니다."),
+    NOT_ENABLE_AMOUNT_END("사이의 숫자만 입력할 수 있습니다."),
     NUMBER_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     BONUS_NUMBER_ERROR("로또 번호로 입력한 번호는 보너스 번호로 등록할 수 없습니다."),
     DUPLICATED_NUMBER_ERROR("중복된 번호가 있습니다."),
@@ -17,7 +17,10 @@ public enum LottoErrorMessages {
         this.text = text;
     }
 
-    public String text() {
+    public String addErrorText() {
         return ERROR_UNIT.text + this.text;
+    }
+    public String text(){
+        return this.text;
     }
 }
--- src/main/java/lotto/service/ValidationService.java
+++ src/main/java/lotto/service/ValidationService.java
@@ -19,7 +19,7 @@ public class ValidationService {
             System.out.println(LottoInfoMessages.INSERT_PAY.text());
             pay = Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
-            System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.text());
+            System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.addErrorText());
             validatePayInput();
         }
         return pay;
@@ -28,11 +28,12 @@ public class ValidationService {
     public int validateManualAmountIsInteger(int enableAmount) {
         int amount = 0;
         try {
-            System.out.println(LottoInfoMessages.INSERT_MANUAL_AMOUNT_START.text());
+            System.out.println(LottoInfoMessages.INSERT_MANUAL_AMOUNT_START.text()+enableAmount
+                    +LottoInfoMessages.INSERT_MANUAL_AMOUNT_END.text());
             amount = Integer.parseInt(Console.readLine());
             validateOverManualAmount(amount, enableAmount);
         } catch (NumberFormatException e) {
-            System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.text());
+            System.out.println(LottoErrorMessages.PAY_INPUT_ERROR.addErrorText());
             validateManualAmountIsInteger(enableAmount);
         }
         return amount;
@@ -40,7 +41,7 @@ public class ValidationService {
 
     private void validateOverManualAmount(int amount, int enableAmount) {
         if (amount > enableAmount || amount < 0) {
-            System.out.println(LottoErrorMessages.NOT_ENABLE_AMOUNT_START.text()
+            System.out.println(LottoErrorMessages.NOT_ENABLE_AMOUNT_START.addErrorText()
                     + enableAmount + LottoErrorMessages.NOT_ENABLE_AMOUNT_END.text());
             validateManualAmountIsInteger(enableAmount);
         }
