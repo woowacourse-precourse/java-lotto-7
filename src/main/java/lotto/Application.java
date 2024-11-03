726e5abad998a7172daa92beb7b6a87925990e5a
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-04 03:10:55 +0900

    feat(Application): 전체 실행 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,15 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        TotalLottoPriceInputHandler totalLotto = new TotalLottoPriceInputHandler();
+        LottoNumbers lottoNumbers = new LottoNumbers(totalLotto.getTotalLottoPrice());
+        lottoNumbers.printLottos();
+        WinningNumberInputHandler winningNumber = new WinningNumberInputHandler();
+        BonusNumberInputHandler bonusNumber = new BonusNumberInputHandler(winningNumber.getWinningNumber());
+        MatchLotto match = new MatchLotto(totalLotto.getTotalLottoPrice(),
+                winningNumber.getWinningNumber(),
+                bonusNumber.getBonusNumber(),
+                lottoNumbers);
+        match.printTotalResult();
     }
 }
