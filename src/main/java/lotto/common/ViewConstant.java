3660d490c01db90a36f205f324d9ec66958a843d
Kyuwon-Choi <kyuwon30@naver.com>
2024-11-03 16:44:14 +0900

    feat: inputview 추가

---
+++ src/main/java/lotto/common/ViewConstant.java
@@ -0,0 +1,19 @@
+package lotto.common;
+
+public class ViewConstant {
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static final String OUTPUT_LOTTO_COUNT_MESSAGE = "%d 개를 구매했습니다.";
+
+    public static final String OUTPUT_WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    public static final String OUTPUT_MATCHING_NUMBERS_MESSAGE = "%d 개 일치";
+    public static final String OUTPUT_MATCHING_BONUS_MESSAGE = "보너스 볼 일치";
+    public static final String OUTPUT_MATCHING_MONEY_MESSAGE = "(%d원)- %d개";
+    public static final String OUTPUT_PROFIT_RATE_MESSAGE = "총 수익률은 %.2f%입니다.";
+
+    public static final String DASH = "---";
+    public static final String DELIMITER = ", ";
+
+}
