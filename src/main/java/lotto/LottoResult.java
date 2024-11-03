32f9414f3dbaf2428e0deadbbc67b6a903d3b394
youjeans <dbyj2810@naver.com>
2024-11-03 23:08:52 +0900

    feat: 당첨 통계 출력 기능

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -9,6 +9,12 @@ public class LottoResult {
     private static int fourthRankCount;
     private static int fifthRankCount;
 
+    private static final LottoRank FIRST_RANK = LottoRank.FIRST;
+    private static final LottoRank SECOND_RANK = LottoRank.SECOND;
+    private static final LottoRank THIRD_RANK = LottoRank.THIRD;
+    private static final LottoRank FOURTH_RANK = LottoRank.FOURTH;
+    private static final LottoRank FIFTH_RANK = LottoRank.FIFTH;
+
     public static void countFirstRank(List<Lotto> lottoSets, Lotto winningNumbers) {
         firstRankCount = 0;
         for (Lotto lotto : lottoSets) {
@@ -59,5 +65,15 @@ public class LottoResult {
                 fifthRankCount++;
             }
         }
+    }
+
+    public static void printResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(FIRST_RANK.getDescription() + firstRankCount + "개");
+        System.out.println(SECOND_RANK.getDescription() + secondRankCount + "개");
+        System.out.println(THIRD_RANK.getDescription() + thirdRankCount + "개");
+        System.out.println(FOURTH_RANK.getDescription() + fourthRankCount + "개");
+        System.out.println(FIFTH_RANK.getDescription() + fifthRankCount + "개");
     }
 }
