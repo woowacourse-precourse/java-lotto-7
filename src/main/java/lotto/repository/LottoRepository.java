b42ae10a0d6113c80c7019b53047135845be2bce
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 15:10:24 +0900

    feat(LottoRepository): 발행된 로또를 한꺼번에 불러오는 기능 구현

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -3,6 +3,7 @@ package lotto.repository;
 import lotto.domain.Lotto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,7 +11,15 @@ public class LottoRepository {
     private static final AtomicInteger ID_GENERATOR = new AtomicInteger(1);
     private Map<Integer, Lotto> lottos = new HashMap<>();
 
+    public void saveAll(List<Lotto> lottos) {
+        lottos.forEach(this::save);
+    }
+
     public void save(Lotto lotto) {
         lottos.put(ID_GENERATOR.incrementAndGet(), lotto);
+    }
+
+    public List<Lotto> findAll() {
+        return List.copyOf(lottos.values());
     }
 }
