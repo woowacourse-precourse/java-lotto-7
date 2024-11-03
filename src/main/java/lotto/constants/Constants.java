2d0ea7e667e7ae09754d93895db75b112c5a4527
yush <yush2202@gmail.com>
2024-11-04 03:48:38 +0900

    feat($ Constants): 상수 추가
    코드에서 사용될 상수 구현

---
+++ src/main/java/lotto/constants/Constants.java
@@ -0,0 +1,20 @@
+package lotto.constants;
+
+public class Constants {
+    private Constants() {
+
+    }
+
+    public static final int LOTTO_MAX = 45;
+    public static final int LOTTO_MIN = 1;
+    public static final int INITIAL_PROFIT = 0;
+    public static final int PERCENT_UNIT = 100;
+    public static final int INITIAL_COUNT = 0;
+    public static final int LOTTO_RESULT_SIZE = 5;
+    public static final int MONEY_UNIT = 1_000;
+    public static final int LOTTO_SIZE = 6;
+    public static final int ONE = 1;
+
+    public static final String DELIMITER = ",";
+    
+}
