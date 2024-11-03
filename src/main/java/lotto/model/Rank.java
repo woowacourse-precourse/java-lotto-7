7b13f96c912ed4c5ea0241ee76798d297c6b359d
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 17:32:47 +0900

    feat(lotto/logic): 등수 반환 기능 추가

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,39 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    FIFTH(3, 5000, "3개 일치 (5,000원)");
+
+
+    private final int matchCount;
+    private final int prize;
+    private final String description;
+
+    Rank(int matchCount, int prize, String description) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.description = description;
+    }
+
+    public static Rank of(int matchCount, boolean hasBonus) {
+        if (FIRST.matchCount == matchCount) {
+            return FIRST;
+        }
+        if (SECOND.matchCount == matchCount && hasBonus) {
+            return SECOND;
+        }
+        if (THIRD.matchCount == matchCount) {
+            return THIRD;
+        }
+        if (FOURTH.matchCount == matchCount) {
+            return FOURTH;
+        }
+        if (FIFTH.matchCount == matchCount) {
+            return FIFTH;
+        }
+        return null;
+    }
+}
