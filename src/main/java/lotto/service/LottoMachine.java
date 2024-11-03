101e33abcd70bdfec39987539ba4f917b7dd3d12
yunsseong <aster20@hanyang.ac.kr>
2024-11-03 23:17:24 +0900

    feat: 로또 구매 금액 입력 기능 구현

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -2,11 +2,20 @@ package lotto.service;
 
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.view.InputView;
+import lotto.view.message.SystemMessage;
 
 public class LottoMachine {
+    private final InputView inputView = new InputView();
+    private final Validator validator = new Validator();
+    private final OutputView outputView = new OutputView();
 
     public List<Lotto> getLotteries() {
         return
+        String input = "";
+        do {
+            input = inputView.input(SystemMessage.INPUT_PURCHASE_MONEY_AMOUNT.getMessage());
+        } while (!validateMoneyAmount(input));
     }
 
     public boolean validateMoneyAmount(String moneyAmount) {
