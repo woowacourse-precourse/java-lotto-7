d43fd9847e800fca76f9bb4201fc9782e5e65656
haebyun <bhk4392@gmail.com>
2024-11-03 22:12:46 +0900

    feat(PurchaseAmount): 로또 구입 금액 검증 기능을 갖춘 값 객체 추가
    - PurchaseAmount 값 객체 생성 및 of 팩토리 메서드 추가
    - 최소 구입 금액(LOTTO_PRICE) 미만일 경우 예외 발생하도록 검증
    - 금액이 로또 가격의 배수가 아닐 경우 예외 발생
    - 에러 메시지 형식화를 위해 ErrorMessage의 getFormattedMessage 메서드 사용

---
+++ src/main/java/lotto/domain/vo/PurchaseAmount.java
@@ -0,0 +1,35 @@
+package lotto.domain.vo;
+
+import lotto.exception.ErrorMessage;
+
+public class PurchaseAmount {
+    private static final Integer LOTTO_PRICE = 1000;
+
+    private final Integer amount;
+
+    private PurchaseAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public static PurchaseAmount of(Integer amount) {
+        validate(amount);
+        return new PurchaseAmount(amount);
+    }
+
+    private static void validate(Integer amount) {
+        if(amount < LOTTO_PRICE) {
+            throw new IllegalArgumentException(ErrorMessage.MONEY_INSUFFICIENT.getFormattedMessage(LOTTO_PRICE));
+        }
+        if (isMoneyLeft(amount)) {
+            throw new IllegalArgumentException(ErrorMessage.MONEY_LEFT.getFormattedMessage(LOTTO_PRICE));
+        }
+    }
+
+    private static boolean isMoneyLeft(Integer amount) {
+        return (amount % LOTTO_PRICE) != 0;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+}
