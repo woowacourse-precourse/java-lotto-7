dd84c20c07204304577d7474e1a7b9ee785ae4bf
dbdb456 <adad568@naver.com>
2024-11-03 20:44:34 +0900

    test(utils): 숫자가 아닌 문자를 넣으면 예외 발생 테스트 추가

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -1,4 +1,21 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.utils.Utils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UtilsTest {
+
+    private final Utils utils = new Utils();
+
+    @DisplayName("숫자가 아닌 다른 문자를 넣으면 예외가 발생한다.")
+    @Test
+    void 숫자가_아니면_예외가_발생한다() {
+        String inputTest = "가나다";
+
+        assertThatThrownBy(() -> utils.parseStringToInt(inputTest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
