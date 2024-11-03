a08e7a8a2e68bf3ee1317f6b7590a290bc482bcd
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:38:28 +0900

    refactor(constant): 로또의 번호 개수 상수 Lotto 클래스로 이동

--- src/main/java/lotto/model/lotto/LottoPrintFormat.java
+++ src/main/java/lotto/model/lotto/LottoPrintFormat.java
@@ -2,7 +2,6 @@ package lotto.model.lotto;
 
 public final class LottoPrintFormat {
 
-    public static final int NUMBER_COUNT = 6;
     public static final String DELIMITER = ", ";
     public static final String PREFIX = "[";
     public static final String SUFFIX = "]";
