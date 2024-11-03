aae34ad925ba7dd03b948e52b83ce198c1b7462a
이윤빈 <kanado20000@naver.com>
2024-11-04 00:50:33 +0900

    :bug: fix: 에상치 못한 오류로 인해 Apllication 파일 초기화하고 다시 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,5 @@
 package lotto;
-
 import lotto.controller.LottoMachineController;
-
 public class Application {
     public static void main(String[] args) {
         lotto.controller.LottoMachineFactory factory = new lotto.controller.LottoMachineFactory();
