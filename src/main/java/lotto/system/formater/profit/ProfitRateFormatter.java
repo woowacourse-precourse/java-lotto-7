e19cae3480d6e556bb6c1d4a0a7ec15981ccb58b
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-03 18:24:35 +0900

    refactor: 수익률 출력 메시지 형식 수정

--- src/main/java/lotto/system/formater/profit/ProfitRateFormatter.java
+++ src/main/java/lotto/system/formater/profit/ProfitRateFormatter.java
@@ -2,9 +2,9 @@ package lotto.system.formater.profit;
 
 public class ProfitRateFormatter { // 수익률 환산
 
-    private final static String PROFIT_RATE_MESSAGE = "총 수익률은 %d입니다.";
+    private final static String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
-    public static String formatAsMessage (int profitRate) {
+    public static String formatAsMessage (double profitRate) {
         return String.format(PROFIT_RATE_MESSAGE, profitRate);
     }
 }
