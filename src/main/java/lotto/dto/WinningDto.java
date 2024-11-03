34770418250ea67a9b3db10daf77582516f30011
CHISANW <keuye06380618@gmail.com>
2024-11-04 06:53:01 +0900

    refactor(WinningDto): of() 메서드 리팩토링
    - 기존 int[] 배열을 통해 값을 저장했으나, List<Integer>을 통해 값을 저장하도록 수정

--- src/main/java/lotto/dto/WinningDto.java
+++ src/main/java/lotto/dto/WinningDto.java
@@ -3,6 +3,7 @@ package lotto.dto;
 import static lotto.eunm.WinningResult.*;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import lotto.eunm.WinningResult;
 
@@ -16,13 +17,13 @@ public class WinningDto {
         this.price = price;
     }
 
-    public static WinningDto of(int[] numbers) {
+    public static WinningDto of(List<Integer> matchStatistics) {
         Map<WinningResult, Integer> winning = new LinkedHashMap<>();
-        winning.put(THREE, numbers[THREE.winningCount]);
-        winning.put(FOUR, numbers[FOUR.winningCount]);
-        winning.put(FIVE, numbers[FIVE.winningCount]);
-        winning.put(FIVE_AND_BONUS, numbers[FIVE_AND_BONUS.winningCount]);
-        winning.put(SIX, numbers[SIX.winningCount]);
+        winning.put(THREE, matchStatistics.get(THREE.index));
+        winning.put(FOUR, matchStatistics.get(FOUR.index));
+        winning.put(FIVE, matchStatistics.get(FIVE.index));
+        winning.put(FIVE_AND_BONUS, matchStatistics.get(FIVE_AND_BONUS.index));
+        winning.put(SIX, matchStatistics.get(SIX.index));
 
         return new WinningDto(winning, 0);
     }
