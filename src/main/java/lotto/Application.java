1edb137b939a50a5cd7b26904448b50f519ab394
Yunsu4 <ysa020428@gmail.com>
2024-11-03 19:19:53 +0900

    feat(Application): 로또를 실행하는 주요 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.AutoLottoGenerator;
+import lotto.model.LottoMatcher;
+import lotto.model.YieldCalculator;
+import lotto.util.LottoNumberParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoNumberParser lottoNumberParser = new LottoNumberParser();
+        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
+        LottoMatcher lottoMatcher = new LottoMatcher();
+        YieldCalculator yieldCalculator = new YieldCalculator();
+
+        LottoController lottoController = new LottoController(inputView, outputView,
+                lottoNumberParser, autoLottoGenerator,
+                lottoMatcher, yieldCalculator);
+
+        lottoController.run();
     }
 }
