2ded0feec8751f159bd87f17d7b5f21be1dd9698
kkmdevel <kkm.devel@gmail.com>
2024-11-03 14:37:28 +0900

    feat(Parser): 예외 처리 및 에러 메시지 관리 기능 추가
    - 파싱 오류 시 사용할 ParserExceptionMessage enum 추가
    - 숫자 입력 및 빈 문자열 입력에 대한 예외 메시지 관리
    - ParserException을 통해 에러 메시지에 "[ERROR]" 접두사를 적용하여 출력

---
+++ src/main/java/lotto/exception/message/ParserExceptionMessage.java
@@ -0,0 +1,15 @@
+package lotto.exception.message;
+
+public enum ParserExceptionMessage{
+    EMPTY_INPUT("아무것도 입력되지 않았습니다."),
+    NOT_NUMBER("숫자를 입력해주세요");
+    private final String message;
+
+    ParserExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
