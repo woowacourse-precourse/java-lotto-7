4ae0f795ffab9fcf2b83c13d73024dee70b84749
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:37:05 +0900

    feat(LottoService): 사용자가 입력한 금액만큼의 랜덤한 로또 생성 기능 추가
    - LottoDomainFactory를 통해서 각 도메인 객체의 의존성을 주입받게끔 로직 추가
    - 사용자가 입력한 금액만큼의 장수를 계산하는 로직 호출부 추가
    - 계산된 로또의 장수만큼 랜덤한 로또를 생성하는 로직 호출부 추가
    - 생성된 로또들을 일급 컬렉션으로 묶어서 DTO를 통해서 반환하는 로직 추가

--- src/main/java/lotto/Service/LottoService.java
+++ src/main/java/lotto/Service/LottoService.java
@@ -1,4 +1,35 @@
 package lotto.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.DTO.PaymentPriceDTO;
+import lotto.DTO.RandomLottoNumberDTO;
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoTickets;
+import lotto.Domain.PurchasePriceCalculator;
+import lotto.Domain.RandomLottoNumbersGenerator;
+import lotto.Factory.LottoDomainFactory;
+
 public class LottoService {
+    private final LottoDomainFactory lottoDomainFactory;
+
+    public LottoService(LottoDomainFactory lottoDomainFactory) {
+        this.lottoDomainFactory = lottoDomainFactory;
+    }
+
+    public RandomLottoNumberDTO purchaseLotto(PaymentPriceDTO paymentPriceDTO) {
+        PurchasePriceCalculator purchasePriceCalculator = lottoDomainFactory.createPurchasePriceCalculator();
+        RandomLottoNumbersGenerator randomLottoNumberGenerator = lottoDomainFactory.createRandomLottoNumbersGenerator();
+
+        Integer lottoPurchaseCount = purchasePriceCalculator.calculateLottoPrice(paymentPriceDTO.getPaymentPrice());
+
+        List<Lotto> generatedLottos = new ArrayList<>();
+        for (int i = 0; i < lottoPurchaseCount; i++) {
+            List<Integer> numbers = randomLottoNumberGenerator.createRandomLotto();
+            generatedLottos.add(new Lotto(numbers));
+        }
+        LottoTickets lottoTickets = new LottoTickets(generatedLottos);
+
+        return new RandomLottoNumberDTO(lottoTickets, lottoPurchaseCount);
+    }
 }
