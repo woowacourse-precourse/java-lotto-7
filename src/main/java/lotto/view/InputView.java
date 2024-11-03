401d9a35c25fa7d0eab95054d1c2b57edd830826
KSJ0128 <ksj01128@naver.com>
2024-11-04 00:56:26 +0900

    feat: 유저 로또 구매 금액 입력 메서드 InputPurchaseMoney() 구현
    유저 로또 구매 금액을 입력 받는 메서드인 InputPurchaseMoney를 구현했습니다.
    구입 금액을 입력해 주세요.를 출력하고, readLine을 통해 유저 입력을 받습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,11 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.message.Message;
+
 public class InputView {
+    public String InputPurchaseMoney() {
+        Message.INPUT_PURCHASE_MONEY.println();
+        return Console.readLine();
+    }
 }
