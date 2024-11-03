fdc4da3128c80654a769af0a7e4b0e14316a04eb
song hyeongyu <lit3207@ajou.ac.kr>
2024-11-03 21:12:18 +0900

    feat: add Validator class

---
+++ src/main/java/lotto/domain/Validator.java
@@ -0,0 +1,72 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Validator {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String DELIMITER = ",";
+
+
+    public static void validateSize(List<Integer> inputLottoNumbers) {
+        if (inputLottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static void tokenizeInput(String input, List<Integer> inputLottoNumbers) {
+        validateDelimiter(input);
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        StringTokenizer tokenizer = new StringTokenizer(input, DELIMITER);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            validateUnique(uniqueNumbers, token);
+            inputLottoNumbers.add(Integer.parseInt(token));
+        }
+    }
+
+    public static void validateUnique(Set<Integer> uniqueNumbers, String token) {
+        int number = Integer.parseInt(token);
+        if (!uniqueNumbers.add(number)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 중복된 숫자를 입력했습니다.");
+        }
+    }
+
+    public static void validateDelimiter(String input) {
+        if (!input.contains(DELIMITER)) {
+            System.out.println();
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 잘못된 구분자를 포함하고 있습니다.");
+        }
+    }
+
+    public static void validateInput(String money) {
+        validateNumber(money);
+        validateMoney(money);
+    }
+
+    public static void validateNumber(String number) throws IllegalArgumentException {
+        if (number == null || number.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력이 비어 있습니다.");
+        }
+        if(!checkInteger(number)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 문자가 포함 됐습니다.");
+        }
+    }
+
+    public static boolean checkInteger(String input) {
+        for(char c : input.toCharArray()) {
+            if(!Character.isDigit(c))
+                return false;
+        }
+        return true;
+    }
+
+    public static void validateMoney(String money) {
+        int amount = Integer.parseInt(money);
+        if (amount < 0 || amount % 1000 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 금액은 0 이상의 1000 단위여야 합니다.");
+        }
+    }
+}
