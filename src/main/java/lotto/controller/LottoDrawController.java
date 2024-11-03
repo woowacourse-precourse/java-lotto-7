e9783ef587c19bcf695a67d8cceb7cba02a4b1b4
Chan531 <chan657312@gmail.com>
2024-11-03 18:18:22 +0900

    feat(LottoDrawController): 인스턴스 변수 선언

---
+++ src/main/java/lotto/controller/LottoDrawController.java
@@ -0,0 +1,10 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoDrawController {
+
+	private final InputView inputView;
+	private final OutputView outputView;
+}
