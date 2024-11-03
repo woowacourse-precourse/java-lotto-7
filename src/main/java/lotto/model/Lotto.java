50b133f700f836d1b669d8b08bd442800d911b46
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:48:08 +0900

    feat: Lotto 클래스 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,50 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    private static final String WRONG_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String WRONG_RANGE = "[ERROR] 숫자 범위가 벗어났습니다.";
+    private static final String DUPLICATE_NUMBER = "[ERROR] 중복되는 숫자가 있습니다.";
+
+    public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        checkRange(numbers);
+        checkDuplicate(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        sortNumbers();
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(WRONG_SIZE);
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (!(number > 0 && number <= 45)) {
+                throw new IllegalArgumentException(WRONG_RANGE);
+            }
+        }
+    }
+
+    private void checkDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER);
+        }
+    }
+
+    private void sortNumbers() {
+        Collections.sort(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
