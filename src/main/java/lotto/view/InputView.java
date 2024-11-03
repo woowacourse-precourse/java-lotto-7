5ae65b8f5d12a2b01e1ff7c81a235114dbc8a7f0
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 04:09:48 +0900

    feat: 보너스 번호 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,12 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Bonus;
 import lotto.domain.Lotto;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public abstract class InputView {
-
-    private static final int LOTTO_PRICE = 1000;
 
     private InputView() {
     }
@@ -31,11 +27,21 @@ public abstract class InputView {
                 .toList());
     }
 
+    public static Bonus readBonus() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        checkIfBonusIsValid(input);
+        return new Bonus(Integer.parseInt(input));
+    }
+
+    private static void checkIfBonusIsValid(String input) {
+        isInputEmpty(input);
+        isNumeric(input);
+    }
+
     private static void checkIfPurchaseAmountIsValid(String input) {
         isInputEmpty(input);
         isNumeric(input);
-        int purchaseAmount = Integer.parseInt(input);
-        isDividedByLottoPrice(purchaseAmount);
     }
 
     private static void checkIfWinningLottoIsValid(String input) {
@@ -58,12 +64,6 @@ public abstract class InputView {
         int inputLength = input.length();
         if (input.charAt(0) == ',' || input.charAt(inputLength - 1) == ',') {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력 형식입니다.");
-        }
-    }
-
-    private static void isDividedByLottoPrice(int money) {
-        if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 로또 가격 단위로 이루어져야 합니다.");
         }
     }
 
