4ca1e10a3a8bec327aa7fd54c122d1d6882252fe
kjyyjk <rlawnsdud920@gmail.com>
2024-11-03 19:25:49 +0900

    feat: 잘못된 당첨 번호를 입력하면 예외를 발생한다
    - validateUniqueWinningNumber()
    당첨 번호들의 중복을 제거한 크기가 6인지를 검증한다.
    
    - validateWinningNumbersRange()
    당첨 번호의 범위가 1~45인지를 검증한다.
    
    - validateWinningNumbersCount()
    당첨 번호의 개수가 6개인지를 검증한다.

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,9 +2,13 @@ package lotto;
 
 public enum ErrorMessage {
 
-    PURCHASE_AMOUNT_NOT_ENOUGH("[ERROR] 구입 금액은 최소 1000원 이상입니다.\n"),
-    PURCHASE_AMOUNT_NOT_MULTIPLE_LOTT_PRICE("[ERROR] 구입 금액은 1000원으로 나누어떨어져야합니다.\n"),
-    PURCHASE_AMOUNT_EXCEED_LIMIT("[ERROR] 구입 금액은 100000원 이하여야합니다.\n");
+    NOT_ENOUGH_PURCHASE_AMOUNT("[ERROR] 구입 금액은 최소 1000원 이상입니다.\n"),
+    PURCHASE_AMOUNT_NOT_MULTIPLE_LOTTO_PRICE("[ERROR] 구입 금액은 1000원으로 나누어떨어져야합니다.\n"),
+    PURCHASE_AMOUNT_EXCEED_LIMIT("[ERROR] 구입 금액은 100000원 이하여야합니다.\n"),
+
+    INVALID_WINNING_NUMBERS_COUNT("[ERROR] 당첨 번호는 6개여야합니다.\n"),
+    INVALID_WINNING_NUMBER_RANGE("[ERROR] 당첨 번호의 범위는 1~45여야 합니다.\n"),
+    NOT_UNIQUE_WINNING_NUMBER("[ERROR] 당첨 번호는 중복되지 않아야 합니다.\n");
 
     private final String message;
 
--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,13 +1,16 @@
 package lotto;
 
+import static lotto.ErrorMessage.INVALID_WINNING_NUMBERS_COUNT;
+import static lotto.ErrorMessage.INVALID_WINNING_NUMBER_RANGE;
+import static lotto.ErrorMessage.NOT_UNIQUE_WINNING_NUMBER;
 import static lotto.ErrorMessage.PURCHASE_AMOUNT_EXCEED_LIMIT;
-import static lotto.ErrorMessage.PURCHASE_AMOUNT_NOT_ENOUGH;
-import static lotto.ErrorMessage.PURCHASE_AMOUNT_NOT_MULTIPLE_LOTT_PRICE;
+import static lotto.ErrorMessage.NOT_ENOUGH_PURCHASE_AMOUNT;
+import static lotto.ErrorMessage.PURCHASE_AMOUNT_NOT_MULTIPLE_LOTTO_PRICE;
 import static lotto.Input.inputBonusNumber;
 import static lotto.Input.inputPurchaseAmount;
-import static lotto.Input.inputWinningNumber;
+import static lotto.Input.inputWinningNumbers;
 import static lotto.InputParser.parseInt;
-import static lotto.InputParser.parseWinningNumber;
+import static lotto.InputParser.parseWinningNumbers;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +20,9 @@ public class LottoManager {
     private static final int LOTTO_PRICE = 1000;
     private static final int MAX_PURCHASE_AMOUNT = 100000;
     private static final int PERCENTAGE_FACTOR = 100;
+    private static final int MIN_WINNING_NUMBERS_RANGE = 1;
+    private static final int MAX_WINNING_NUMBERS_RANGE = 45;
+    private static final int WINNING_NUMBERS_COUNT = 6;
     private final Map<Rank, Integer> winningRecord = new HashMap<>();
 
     public void run() {
@@ -24,10 +30,10 @@ public class LottoManager {
         List<Lotto> lottoes = purchaseLottoes(purchaseAmount);
         Output.printPurchaseMessage(lottoes.size());
         lottoes.forEach(Output::printLotto);
-        List<Integer> winningNumber = getWinningNumber();
+        List<Integer> winningNumbers = getWinningNumbers();
         int bonusNumber = getBonusNumber();
         lottoes.stream()
-                .map(lotto -> lotto.getRank(winningNumber, bonusNumber))
+                .map(lotto -> lotto.getRank(winningNumbers, bonusNumber))
                 .forEach(this::saveRankOnRecord);
         Output.printWinningStatistics(winningRecord);
         double returnRate = (double) calculateTotalWinningAmount() * PERCENTAGE_FACTOR / purchaseAmount;
@@ -46,32 +52,15 @@ public class LottoManager {
         }
     }
 
-    private static void validatePurchaseAmountEnough(final int purchaseAmount) {
-        if (purchaseAmount < LOTTO_PRICE) {
-            System.out.println(PURCHASE_AMOUNT_NOT_ENOUGH.getMessage());
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validatePurchaseAmountMultipleLottoPrice(final int purchaseAmount) {
-        if (purchaseAmount % LOTTO_PRICE != 0) {
-            System.out.println(PURCHASE_AMOUNT_NOT_MULTIPLE_LOTT_PRICE.getMessage());
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validatePurchaseAmountExceedLimit(final int purchaseAmount) {
-        if (purchaseAmount > MAX_PURCHASE_AMOUNT) {
-            System.out.println(PURCHASE_AMOUNT_EXCEED_LIMIT.getMessage());
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static List<Integer> getWinningNumber() {
+    private static List<Integer> getWinningNumbers() {
         try {
-            return parseWinningNumber(inputWinningNumber());
+            List<Integer> winningNumbers = parseWinningNumbers(inputWinningNumbers());
+            validateWinningNumbersCount(winningNumbers);
+            validateWinningNumbersRange(winningNumbers);
+            validateUniqueWinningNumber(winningNumbers);
+            return winningNumbers;
         } catch (IllegalArgumentException e) {
-            return getWinningNumber();
+            return getWinningNumbers();
         }
     }
 
@@ -82,7 +71,6 @@ public class LottoManager {
             return getBonusNumber();
         }
     }
-
 
     private void saveRankOnRecord(final Rank rank) {
         int rankCount = winningRecord.getOrDefault(rank, 0);
@@ -101,5 +89,57 @@ public class LottoManager {
             totalWinningAmount += winningRankCount * rank.getWinningAmount();
         }
         return totalWinningAmount;
+    }
+
+    private static void validatePurchaseAmountEnough(final int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE) {
+            System.out.println(NOT_ENOUGH_PURCHASE_AMOUNT.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validatePurchaseAmountMultipleLottoPrice(final int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            System.out.println(PURCHASE_AMOUNT_NOT_MULTIPLE_LOTTO_PRICE.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validatePurchaseAmountExceedLimit(final int purchaseAmount) {
+        if (purchaseAmount > MAX_PURCHASE_AMOUNT) {
+            System.out.println(PURCHASE_AMOUNT_EXCEED_LIMIT.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateUniqueWinningNumber(List<Integer> winningNumbers) {
+        long distinctCount = winningNumbers.stream()
+                .distinct()
+                .count();
+
+        if (distinctCount != WINNING_NUMBERS_COUNT) {
+            System.out.println(NOT_UNIQUE_WINNING_NUMBER.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateWinningNumbersRange(List<Integer> winningNumbers) {
+        for (int number : winningNumbers) {
+            validateWinningNumberRange(number);
+        }
+    }
+
+    private static void validateWinningNumberRange(int number) {
+        if (number < MIN_WINNING_NUMBERS_RANGE || number > MAX_WINNING_NUMBERS_RANGE) {
+            System.out.println(INVALID_WINNING_NUMBER_RANGE.getMessage());
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateWinningNumbersCount(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != WINNING_NUMBERS_COUNT) {
+            System.out.println(INVALID_WINNING_NUMBERS_COUNT.getMessage());
+            throw new IllegalArgumentException();
+        }
     }
 }
