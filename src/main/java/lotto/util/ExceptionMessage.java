38dd1a8d0d8d99e75ffd0277c27bf8d3e74c7a2a
yunjeooong <yunjeongssu@gmail.com>
2024-11-03 18:33:59 +0900

    test(BudgetValidator): 정상, 비정상 입력 테스트 코드 작성

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -4,8 +4,8 @@ import static lotto.util.Constants.LOTTO_PRICE;
 
 public enum ExceptionMessage {
 
-    NOT_NUMERIC("숫자로 입력하세요."),
-    NOT_IN_RANNGE_OF_BUDGET("구매 금액은 1000원부터 10만원까지 입력 가능합니다."),
+    INVALID_NOT_NUMERIC("정수의 형태로 입력하세요."),
+    INVALID_OUT_OF_BUDGET_RANGE("구매 금액은 1000원부터 10만원까지 입력 가능합니다."),
     INVALID_RANGE("1부터 45까지의 숫자만 입력 가능합니다."),
     INVALID_UNIT_OF_BUDGET(String.format("%d원 단위로 구매 가능합니다.", LOTTO_PRICE));
 
--- src/main/java/lotto/util/validator/BudgetValidator.java
+++ src/main/java/lotto/util/validator/BudgetValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 import lotto.util.ExceptionMessage;
 
 public class BudgetValidator implements Validator {
-    private static final Pattern NUMBER_REGEX = Pattern.compile("^[0-9]*$");
+    private static final Pattern NUMBER_REGEX = Pattern.compile("^[0-9]+$");
     private static final int MIN_BUDGET = 1_000;
     private static final int MAX_BUDGET = 100_000;
 
@@ -14,7 +14,9 @@ public class BudgetValidator implements Validator {
     @Override
     public void validate(String input) throws IllegalArgumentException {
         String budget = removeSpace(input);
-        validate(budget);
+        validateNumber(budget);       // 숫자 검증
+        validateInputRange(budget);   // 범위 검증
+        validateInputUnit(budget);    // 로또 가격 단위 검증
 
     }
 
@@ -26,13 +28,13 @@ public class BudgetValidator implements Validator {
         if (NUMBER_REGEX.matcher(budget).matches()) {
             return;
         }
-        throw new IllegalArgumentException(ExceptionMessage.NOT_NUMERIC.getMessage());
+        throw new IllegalArgumentException(ExceptionMessage.INVALID_NOT_NUMERIC.getMessage());
     }
 
     private void validateInputRange(String budget) {
         int budgetValue = Integer.parseInt(budget);
         if (budgetValue < MIN_BUDGET || budgetValue > MAX_BUDGET) {
-            throw new IllegalArgumentException(ExceptionMessage.NOT_IN_RANNGE_OF_BUDGET.getMessage());
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_OUT_OF_BUDGET_RANGE.getMessage());
         }
     }
     private void validateInputUnit(String budget) {
