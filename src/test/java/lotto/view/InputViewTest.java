a587821426951598ade275e4bdcde4cd5362c6bd
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 18:55:19 +0900

    :white_check_mark: test : validateAmountInput 메서드 테스트
    - null인 경우
    - 빈 문자열인 경우
    - 숫자가 아닌 경우
    - 숫자인 경우

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,35 @@
+package lotto.view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    void 구매_금액이_null일_때_예외(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.validateAmountInput(null);
+        }, "[ERROR] 구입 금액을 입력해 주세요.");
+    }
+
+    @Test
+    void 구매_금액이_빈_문자열일_떄_예외_발생(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.validateAmountInput("");
+        }, "[ERROR] 구입 금액을 입력해 주세요.");
+    }
+
+    @Test
+    void 구매_금액이_숫자가_아닐_때_예와_발생(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.validateAmountInput("invalid123");
+        }, "[ERROR] 구입 금액은 숫자여야 합니다.");
+    }
+
+    @Test
+    void 구매_금액이_올바른_입력일_때(){
+        assertDoesNotThrow(() -> InputView.validateAmountInput("1234"));
+    }
+
+}
