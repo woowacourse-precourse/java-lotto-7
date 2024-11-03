d755c66dd8f10c2a5f01724425a7e6dbc3727f34
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-03 20:02:27 +0900

    feat(LottoGameController): 의존성 주입을 통한 객체 생성 및 게임 시작 메서드 추가
    - LottoService, InputView, OutputView를 주입받아 LottoGameController 객체를 생성하도록 구현
    - gameStart 메서드를 추가하여 사용자의 입력을 받는 메서드 구현

--- src/main/java/lotto/Controller/LottoGameController.java
+++ src/main/java/lotto/Controller/LottoGameController.java
@@ -1,4 +1,21 @@
 package lotto.Controller;
 
+import lotto.Service.LottoService;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
 public class LottoGameController {
+    private LottoService lottoService;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public LottoGameController(LottoService lottoService, InputView inputView, OutputView outputView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void gameStart() {
+        inputView.showPaymentPriceMessage();
+    }
 }
