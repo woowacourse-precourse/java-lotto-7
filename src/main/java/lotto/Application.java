84d3807a12edcadcc48380dc8a717c21e84896c1
Silbet <65993038+Silbet@users.noreply.github.com>
2024-11-03 21:26:53 +0900

    Feat: 로또 번호 생성 기능 구현
    파라미터의 pieces 만큼 로또를 생성하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static int lottoPrice = 1000;
@@ -16,5 +19,18 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
         return payment / lottoPrice;
+    }
+
+    public static List<Lotto> createLotto(final int pieces) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < pieces; i++) {
+            List<Integer> randomLottoNum = createLottoNum();
+            lottos.add(new Lotto(randomLottoNum));
+        }
+        return lottos;
+    }
+
+    public static List<Integer> createLottoNum() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
