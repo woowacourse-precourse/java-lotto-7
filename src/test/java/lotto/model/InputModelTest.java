804496df4c2eefcb2cb3c54feffcd939e8c720d0
mipangg <morganjeong119@gmail.com>
2024-11-03 17:16:22 +0900

    test(InputModelTest): rename method from convertText to validateNumbers

--- src/test/java/lotto/model/InputModelTest.java
+++ src/test/java/lotto/model/InputModelTest.java
@@ -20,7 +20,7 @@ public class InputModelTest {
     @Test
     void 조건에_맞는_당첨번호_문자열을_입력하면_숫자_리스트를_반환한다() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-        assertThat(InputModel.convertText("1,2,3,4,5,6")).isEqualTo(numbers);
+        assertThat(InputModel.validateNumbers("1,2,3,4,5,6")).isEqualTo(numbers);
     }
 
     @Test
@@ -52,7 +52,7 @@ public class InputModelTest {
 
     @Test
     void 당첨번호에_쉼표를_제외한_알_수_없는_문자를_입력할_경우_예외_테스트() {
-        assertThatThrownBy(() -> InputModel.convertText("1/2/3/4/5/6")
+        assertThatThrownBy(() -> InputModel.validateNumbers("1/2/3/4/5/6")
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 각 로또 번호는 쉼표(,)로 구분된 숫자여야 합니다.");
     }
@@ -61,7 +61,7 @@ public class InputModelTest {
     @EmptySource
     @NullSource
     void 당첨번호에_빈_값을_입력했을_경우_예외_테스트(String input) {
-        assertThatThrownBy(() -> InputModel.convertText(input)
+        assertThatThrownBy(() -> InputModel.validateNumbers(input)
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 빈 입력입니다.");
     }
@@ -69,7 +69,7 @@ public class InputModelTest {
     @ParameterizedTest
     @CsvSource(value = {
             "6:[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.",
-            "47:[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.",
+            "47:[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.",
             "n:[ERROR] 숫자를 입력해야 합니다."
     }, delimiter = ':')
     void 보너스번호의_입력조건을_맞추지_않은_경우_예외_테스트(String input, String errorMessage) {
