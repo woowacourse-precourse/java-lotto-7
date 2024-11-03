758278cb1e84640785378f8639591123a71b6723
jijongkwon <help3451@naver.com>
2024-11-03 21:45:22 +0900

    feat(domain): 필드, 로또 결과 추가, 로또 결과 반환 메서드 구현

--- src/main/java/lotto/domain/Member.java
+++ src/main/java/lotto/domain/Member.java
@@ -1,15 +1,27 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import lotto.enums.lotto.LottoRank;
 
 public class Member {
 
     private static final Member instance = new Member();
     private final List<Lotto> issuedLottos;
+    private final Map<LottoRank, Integer> lottoResults;
 
     private Member() {
         issuedLottos = new ArrayList<>();
+        lottoResults = new EnumMap<>(LottoRank.class);
+        initializeLottoResults();
+    }
+
+    private void initializeLottoResults() {
+        for (LottoRank rank : LottoRank.values()) {
+            lottoResults.put(rank, 0);
+        }
     }
 
     public static Member getInstance() {
@@ -22,5 +34,13 @@ public class Member {
 
     public List<Lotto> getIssuedLottos() {
         return issuedLottos;
+    }
+
+    public void addLottoResult(LottoRank lottoRank) {
+        lottoResults.put(lottoRank, lottoResults.get(lottoRank) + 1);
+    }
+
+    public Map<LottoRank, Integer> getLottoResults() {
+        return lottoResults;
     }
 }
