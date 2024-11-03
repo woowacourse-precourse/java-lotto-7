bce2d2fc16761047de52241f6272b9669f3fad94
hojae <ghwo336@naver.com>
2024-11-04 01:58:44 +0900

    feat: Controller의 ValidService 적용

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,11 +1,26 @@
 package lotto.controller;
 
+import lotto.Service.ValidService;
+import lotto.view.Input;
 import lotto.view.Output;
 
 public class Controller {
+    ValidService validService=new ValidService();
 
     public void start(){
         Output.requestPurchaseAmount();
+        while (true){
+            String money= Input.getInput();
+            try{
+                validService.validMoney(money);
+                break;
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+
 
     }
 
