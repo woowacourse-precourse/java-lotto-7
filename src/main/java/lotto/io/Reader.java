4a01a3a4dd689025a0672b0eb11b9d5e066d9688
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 23:21:15 +0900

    feat(Reader): 당첨 번호 입력 기능

--- src/main/java/lotto/io/Reader.java
+++ src/main/java/lotto/io/Reader.java
@@ -2,11 +2,19 @@ package lotto.io;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Reader {
 
     public static int readMoney() {
         String input = Console.readLine();
 
         return Parser.parseInputToMoney(input);
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        String input = Console.readLine();
+
+        return Parser.parseInputToNumbers(input);
     }
 }
