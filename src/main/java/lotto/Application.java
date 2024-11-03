b000b114873225ce956daa77a217e7590a25345d
GeonHui Lee <spongging@gmail.com>
2024-11-03 17:23:08 +0900

    [fix] 메인 클래스 예외 발생시 프로그램이 종료되게 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import controller.LottoController;
+import exception.InvalidInputException;
 import purchase.PurchaseAmount;
 import view.InputView;
 import view.OutputView;
@@ -8,17 +9,15 @@ import view.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        boolean isRunning = true;
-        while (isRunning) {
-            try {
-                PurchaseAmount purchaseAmount = InputView.readPurchaseAmount();
-                LottoController controller = new LottoController(purchaseAmount);
-                controller.run();
-                isRunning = false; // 정상 종료
-            } catch (IllegalArgumentException e) {
-                OutputView.printErrorMessage(e.getMessage());
-                // 루프를 계속해서 재입력 요구
-            }
+        try {
+            PurchaseAmount purchaseAmount = InputView.readPurchaseAmount();
+            LottoController controller = new LottoController(purchaseAmount);
+            controller.run();
+        } catch (InvalidInputException e) {
+            OutputView.printErrorMessage(e.getMessage());
+        } catch (Exception e) {
+            // 예상치 못한 예외를 처리하기 위한 일반적인 예외 처리
+            OutputView.printErrorMessage("알 수 없는 오류가 발생했습니다.");
         }
     }
 }
