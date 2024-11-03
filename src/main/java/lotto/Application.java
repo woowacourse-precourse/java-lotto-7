41dd1fbd81fd795f71053bf6fcc9bba893eee4ba
장혜민 <gpals5987@gmail.com>
2024-11-03 16:03:58 +0900

    [refactor] lottoService 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,13 +3,11 @@ package lotto;
 import lotto.contoller.LottoController;
 import lotto.service.ProfitCalculatorService;
 import lotto.service.WinningStatisticsService;
-import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         LottoController lottoController = new LottoController(
-                LottoService.getInstance(),
                 WinningStatisticsService.getInstance(),
                 ProfitCalculatorService.getInstance());
         lottoController.start();
