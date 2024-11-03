4bf2f32624f2108fb87ba364d23667820b1b8e30
kimdev <kim__dev@hufs.ac.kr>
2024-11-03 14:01:01 +0900

    feat(WinningNumbers): 중복된 당첨번호 입력 시 예외를 던져라

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,12 +1,22 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
     private List<Integer> winningNumbers;
 
     public WinningNumbers(List<Integer> winningNumbers) {
+        validateDuplicates(winningNumbers);
         this.winningNumbers = winningNumbers;
+    }
+
+    private void validateDuplicates(List<Integer> winningNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningNumbers);
+        if (uniqueNumbers.size() != winningNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 수는 입력하실 수 없습니다.");
+        }
     }
 
     public List<Integer> getWinningNumbers() {
