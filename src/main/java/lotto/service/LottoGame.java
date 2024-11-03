4d23ab085378c78da0249dc0c3366849bb4c2a56
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 22:24:05 +0900

    :necktie: feat : run 메서드 추가
    - 전체 로또 기능을 수행하는 run 메서드 추가

--- src/main/java/lotto/service/LottoGame.java
+++ src/main/java/lotto/service/LottoGame.java
@@ -8,11 +8,28 @@ import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 import lotto.domain.LottoResult;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGame {
 
     public static final int LOTTO_PRICE = 1000;
 
+    public void run() {
+        try {
+            int purchaseAmount = InputView.inputPurchaseAmount();
+            List<Lotto> purchasedLottos = purchaseLottos(purchaseAmount);
+            OutputView.printPurchasedLotto(purchasedLottos);
+
+            Lotto winningLotto = InputView.inputWinningLotto();
+            int bonusNumber = InputView.inputBonusNumber(winningLotto.getNumbers());
+
+            LottoResult result = calculateResult(purchasedLottos, winningLotto, bonusNumber);
+            OutputView.printResult(result, purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     private List<Lotto> purchaseLottos(int amount){
         return IntStream.rangeClosed(0, amount / LOTTO_PRICE)
