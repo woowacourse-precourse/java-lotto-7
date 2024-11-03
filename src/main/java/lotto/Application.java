e5320a13425a7b26ee792a824af5cfd4e89b7860
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-04 01:17:03 +0900

    feat(Application) : 3.1 당첨 번호를 입력 받는 기능
    - camp.nextstep.edu.missionutils.Console의 readLine()을 통해 문자열을 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -14,13 +14,15 @@ public class Application {
 
         // 2.4 로또 번호를 통해 구매할 로또의 개수만큼 로또 객체를 생성하는 기능
         ArrayList<Lotto> lottoTickets = generateLottoTickets(purchaseAmount);
-
         // 2.5 로또의 번호를 출력하는 기능
         printLottoTickets(lottoTickets);
+
+        // 3.1 당첨 번호를 입력 받는 기능
+        String winningNumbersInput = getInputString("당첨 번호를 입력해 주세요.");
     }
 
     private static String getInputString(String message) {
-        System.out.println(message);
+        System.out.println("\n" + message);
         return Console.readLine();
     }
 
@@ -80,7 +82,7 @@ public class Application {
     }
 
     private static void printLottoTickets(ArrayList<Lotto> lottoTickets) {
-        System.out.println(lottoTickets.size() + "개를 구매했습니다.");
+        System.out.println("\n" + lottoTickets.size() + "개를 구매했습니다.");
         for (Lotto lottoTicket : lottoTickets) {
             System.out.println(lottoTicket);
         }
