978e48886126bd35b130970e613528eea57f1911
LeeJU <thirdspirit@naver.com>
2024-11-03 15:34:53 +0900

    fix(LottoControllerFactory): erase I/O view from LottoController
    fix I/O view to static methods and load directly

--- src/main/java/lotto/controller/LottoControllerFactory.java
+++ src/main/java/lotto/controller/LottoControllerFactory.java
@@ -1,12 +1,10 @@
 package lotto.controller;
 
 import lotto.service.LottoService;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoControllerFactory {
 
     public static LottoController getLottoController() {
-        return new LottoController(InputView.getInstance(), OutputView.getInstance(), new LottoService());
+        return new LottoController(new LottoService());
     }
 }
