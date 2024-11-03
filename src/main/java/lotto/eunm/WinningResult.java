8b58725de6da16eef51ef4e1d458ccf237b2825d
CHISANW <keuye06380618@gmail.com>
2024-11-04 05:31:15 +0900

    fix(WinningResult): 보너스 및 전체 일치 조건의 변수 설정 오류 수정
    - 보너스 당첨 시 배열 인덱스가 6, 전체 번호 일치 시 인덱스가 7이 되도록 수정
    - 잘못 설정된 변수로 인해 발생한 당첨 결과 오류를 올바르게 수정

--- src/main/java/lotto/eunm/WinningResult.java
+++ src/main/java/lotto/eunm/WinningResult.java
@@ -7,8 +7,8 @@ public enum WinningResult {
     THREE(3, 5_000),
     FOUR(4, 50_000),
     FIVE(5, 1_500_000),
-    FIVE_AND_BONUS(5, 30_000_000),
-    SIX(6, 2_000_000_000);
+    FIVE_AND_BONUS(6, 30_000_000),
+    SIX(7, 2_000_000_000);
 
     public final int winningCount;
     public final int prizeMoney;
