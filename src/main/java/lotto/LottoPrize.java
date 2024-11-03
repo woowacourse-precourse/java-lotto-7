b24297abd45478713a27eba1f40afeadd6f56d55
koosco <rnxogud123@naver.com>
2024-11-03 21:37:20 +0900

    feat: Prize에 보너스 넘버 유무 추가

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -2,17 +2,19 @@ package lotto;
 
 public enum LottoPrize {
 
-    FIRST_PRIZE(6, 2_000_000_000),
-    SECOND_PRIZE(5, 30_000_000),
-    THIRD_PRIZE(5, 1_500_000),
-    FOURTH_PRIZE(4, 50_000),
-    FIFTH_PRIZE(3, 5_000);
+    FIRST_PRIZE(6, false, 2_000_000_000),
+    SECOND_PRIZE(5, true, 30_000_000),
+    THIRD_PRIZE(5, false, 1_500_000),
+    FOURTH_PRIZE(4, false, 50_000),
+    FIFTH_PRIZE(3, false, 5_000);
 
-    private final int count;
+    private final int winningCount;
+    private final boolean bonusExists;
     private final int prize;
 
-    LottoPrize(int count, int prize) {
-        this.count = count;
+    LottoPrize(int winningCount, boolean bonusExists, int prize) {
+        this.winningCount = winningCount;
+        this.bonusExists = bonusExists;
         this.prize = prize;
     }
 }
