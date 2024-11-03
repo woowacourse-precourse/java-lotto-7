7727d9b470153dd5e4aed707833e0b44b552070a
psihyeong <qkrtlgud3286@naver.com>
2024-11-04 04:08:17 +0900

    style : 에러 메시지 상수 클래스 제거

--- src/main/java/lotto/global/ExceptionConstants.java
+++
@@ -1,9 +0,0 @@
-package lotto.global;
-
-public class ExceptionConstants {
-
-    public static final String PURCHASE_AMOUNT_NOT_CHARACTER_MESSAGE = "[ERROR] 구입금액은 숫자여야 합니다.";
-
-    private ExceptionConstants() {
-    }
-}
