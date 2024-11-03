ff455d17f737cbcfe7c901ebbbf5598b385f753d
YongLeeCode <yonghwan311@gmail.com>
2024-11-03 20:48:49 +0900

    feat: 출력 클래스 생성

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Output {
+    private final Map<MatchCondition, Integer> prizeCounts = new LinkedHashMap<>();
+
+    public Output() {
+        // 상금 조건별 초기 카운트 설정
+        for (MatchCondition condition : TicketPrizeMatcher.matchConditions) {
+            prizeCounts.put(condition, 0);
+        }
+    }
+
+    public static void outputTicketNumbers(List<LottoTicket> lottoTicketBundle) {
+        System.out.println(lottoTicketBundle.size() + "개를 구매했습니다.");
+        for (LottoTicket lottoTicket : lottoTicketBundle) {
+            System.out.println(lottoTicket.getNumbers());
+        }
+    }
+
+    public void printMatchResult(MatchResult matchResult) {
+        List<MatchCondition> matchConditions = matchResult.getMatchedConditions();
+        for (MatchCondition matchCondition : matchConditions) {
+            // 조건에 맞는 카운트 증가
+            prizeCounts.put(matchCondition, prizeCounts.getOrDefault(matchCondition, 0) + 1);
+        }
+
+        // 당첨 통계 출력
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        for (Map.Entry<MatchCondition, Integer> entry : prizeCounts.entrySet()) {
+            MatchCondition condition = entry.getKey();
+            Integer count = entry.getValue();
+            System.out.printf("%s - %d개\n", condition.prizeDescription(), count);
+        }
+        System.out.printf("총 수익률은 %.2f%%입니다.\n", matchResult.getProfitRatio());
+    }
+}
