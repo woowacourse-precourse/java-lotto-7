e38caeed95b823de46d01acaa162a5f79ea72f78
maedachaeva <parkchaeyeon.b@gmail.com>
2024-11-03 16:08:28 +0900

    feat(Lotto): 당첨 번호 추첨 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Lotto {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,5 +23,19 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto generateWinningNumbers() {
+        List<Integer> winningNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
+        return new Lotto(winningNumbers);
+    }
+
+    public static int generateBonusNumber(Lotto winningNumbers) {
+        Set<Integer> uniqueWinningNumbers = new HashSet<>(winningNumbers.numbers);
+        int bonusNumber;
+
+        do {
+            bonusNumber = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, 1).get(0);
+        } while (uniqueWinningNumbers.contains(bonusNumber));
+
+        return bonusNumber;
+    }
 }
