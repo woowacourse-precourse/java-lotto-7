7e0ada113b7f1dd67a09d6c8e567b0665ad3d439
jijongkwon <help3451@naver.com>
2024-11-03 20:01:14 +0900

    feat(domain): lotto 생성시 정렬 후 생성하기

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,27 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.enums.lotto.LottoMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortedNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LottoMessage.EXCEPTION_LOTTO_SIZE_NOT_EQUALS_6.getMessage());
         }
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public List<Integer> sortedNumbers(List<Integer> numbers) {
+        return numbers.stream().sorted().toList();
     }
 }
