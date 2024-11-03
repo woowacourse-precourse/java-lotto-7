4fe3ce4e120ffdcfca43ff2cc59f2db60b893394
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-03 19:26:50 +0900

    refactor : 에러 프린트 방식 변경

--- src/main/java/lotto/view/ErrorPrinter.java
+++ src/main/java/lotto/view/ErrorPrinter.java
@@ -4,11 +4,9 @@ import lotto.constants.InputError;
 
 public class ErrorPrinter {
     public static void errorPrint(InputError message) {
-//        System.out.println(message.getMessage());
         try {
             throw new IllegalArgumentException(message.getMessage());
         } catch (IllegalArgumentException e) {
-//            e.printStackTrace();
             System.out.println(e.getMessage());
         }
     }
