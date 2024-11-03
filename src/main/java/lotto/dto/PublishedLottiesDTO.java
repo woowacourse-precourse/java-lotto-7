38207e861f2c96ce95c2e50500bf1a207bdb71f8
yush <yush2202@gmail.com>
2024-11-04 05:41:11 +0900

    feat($ PublishedLottoDTO): 티켓과 랜덤로또 전달 객체 구현

---
+++ src/main/java/lotto/dto/PublishedLottiesDTO.java
@@ -0,0 +1,28 @@
+package lotto.dto;
+
+import java.util.List;
+import lotto.domain.Lotties;
+import lotto.domain.Lotto;
+import lotto.domain.Ticket;
+
+public class PublishedLottiesDTO {
+    private Lotties randomLotties;
+    private Ticket publishedTicket;
+
+    private PublishedLottiesDTO(Lotties randomLotties, Ticket publishedTicket) {
+        this.publishedTicket = publishedTicket;
+        this.randomLotties = randomLotties;
+    }
+
+    public static PublishedLottiesDTO from(Lotties randomLotties, Ticket publishedTicket) {
+        return new PublishedLottiesDTO(randomLotties, publishedTicket);
+    }
+
+    public int getPublishedTicket() {
+        return publishedTicket.getTicket();
+    }
+
+    public List<Lotto> getRandomLotties() {
+        return randomLotties.getLotties();
+    }
+}
