3c75637fc98199d7e1ea32d415beba624386fe1f
zzzyoonnn <dwsrh03@gmail.com>
2024-11-04 02:55:27 +0900

    feat: 쉼표를 기준으로 잘려진 문자열의 갯수가 6개인지 확인하는 isSixNumber test 구현

--- src/test/java/lotto/winning/WinningTest.java
+++ src/test/java/lotto/winning/WinningTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningTest {
+
     @Test
     @DisplayName("숫자와 쉼표로 이루어진 문자열은 올바른 당첨 번호 문자열이다.")
     public void testValidWinningString() {
@@ -22,5 +23,21 @@ public class WinningTest {
         String winningString = "1,2, 3,4, ab,c!";
 
         assertFalse(WinningValidate.isValidString(winningString));
+    }
+
+    @Test
+    @DisplayName("쉼표를 기준으로 잘려진 문자열의 크기가 6인 문자열은 올바른 당첨 번호 문자열이다.")
+    public void testValidStringSize() {
+        String winningString = "11,22,33,44,55,66";
+
+        assertTrue(WinningValidate.isSixNumber(winningString));
+    }
+
+    @Test
+    @DisplayName("쉼표를 기준으로 잘려진 문자열의 크기가 6이 아닌 문자열은 올바르지 않은 당첨 번호 문자열이다.")
+    public void testInValidStringSize() {
+        String winningString = "1,2,3,4,5";
+
+        assertFalse(WinningValidate.isSixNumber(winningString));
     }
 }
