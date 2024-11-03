1b2044ed0eb4945e06591a193ece6b8dd6e6c47f
mjk <mjk3220@ajou.ac.kr>
2024-11-03 22:32:26 +0900

    refactor: inputAmount
    try catch 사용, 에러체크 위해  리펙토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
@@ -10,16 +10,31 @@ public class Application {
 
     public static List<Lotto> lottos = new ArrayList<>();
 
+    public static int stringToNum(String input) {
+        try {
+            int lotto_cost = Integer.parseInt(input);
+            return lotto_cost;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.");
+        }
+    }
+
     public static int inputAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int lotto_cost = Integer.parseInt(readLine());
-        return lotto_cost;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int lotto_cost = stringToNum(readLine());
+                return lotto_cost;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
     public static void buyLotto() {
         int lotto_cost = inputAmount();
-        makeLotto(lotto_cost);
-        outputLottoNumbers();
+        // makeLotto(lotto_cost);
+        // outputLottoNumbers();
     }
 
     public static void main(String[] args) {
