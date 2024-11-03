306a1d391ec565f7b8633b3b1d4b7231e9d31ca9
chxghee <francis001021@gmail.com>
2024-11-03 15:53:23 +0900

    Fix: generateRandomLottoNumbers메서드에서 불변 리스트를 sorting했던 오류 수정.
    생성한 로또 번호의 오름차순 정렬이 가능하도록 생성한 로또번호를 ArrayList로 복사하여 정렬 가능하도록 수정.

--- src/main/java/lotto/domain/RandomLottoNumbers.java
+++ src/main/java/lotto/domain/RandomLottoNumbers.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,12 +10,10 @@ public class RandomLottoNumbers {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int NUMBER_COUNT = 6;
-    private List<Integer> numbers;
 
     public List<Integer> generateRandomLottoNumbers(){
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT);
+        List<Integer> lottoNumbers =  new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT));
         Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
-
 }
