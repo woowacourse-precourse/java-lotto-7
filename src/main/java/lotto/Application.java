d93408040b545c125e07084ab4db1428ebd6189f
Youngchan <wdc4512@naver.com>
2024-11-03 19:56:29 +0900

    feat: 보너스번호 입력 및 유효성 검증 기능 추가
    - Application 클래스에서 보너스 번호 입력받기 기능 추가
    - 입력된 보너스 번호에 대한 유효성 검증 로직을 Lotto 클래스에 구현
    - 보너스 번호가 1부터 45 사이의 범위에 속하는지 및 당첨 번호와 중복되지 않는지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -24,21 +24,46 @@ public class Application {
         System.out.println("당첨 번호를 입력해 주세요.");
         String inputNumber = Console.readLine();
 
+        Lotto lotto;
+
         // 당첨번호 입력에 대한 예외 처리 후 List에 저장
         try {
             List<Integer> numbers = parseInputToList(inputNumber);
-            Lotto lotto = new Lotto(numbers);
+            lotto = new Lotto(numbers);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            return;
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String inputBonusNum = Console.readLine();
+
+        // 보너스 번호 입력 정수 변환 후 예외 처리
+        try {
+            int bonus = parseBonusToInt(inputBonusNum);
+            lotto.setBonusNum(bonus);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
         }
     }
 
+    // 당첨 번호 문자열->정수 변환
     private static List<Integer> parseInputToList(String input) {
         try {
             return Arrays.stream(input.split(","))
                     .map(String::trim) // 공백 제거
                     .map(Integer::parseInt) // 문자열->Integer 변환
                     .collect(Collectors.toList()); // 리스트로 변환
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요.");
+        }
+    }
+
+    // 보너스 번호 문자열->숫자 변환
+    private static int parseBonusToInt(String input) {
+        try {
+            return Integer.parseInt(input.trim());
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력하세요.");
         }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,13 +6,19 @@ import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int bonusNum;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    // 입력 검증
+    public void setBonusNum(int bonusNum) {
+        validateBonusNum(bonusNum);
+        this.bonusNum = bonusNum;
+    }
+
+    // 당첨번호 입력 검증
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -28,5 +34,22 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // 보너스 입력 검증
+    private void validateBonusNum(int bonusNum) {
+        if (bonusNum < 1 || bonusNum > 45) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자를 입력하세요.");
+        }
+
+        if (numbers.contains(bonusNum)) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호와 중복되지 않는 번호를 입력하세요.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNum() {
+        return bonusNum;
+    }
 }
