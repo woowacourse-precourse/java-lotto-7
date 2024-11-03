a043f65a262dc0ebbb4f04c05e21fb63d7d832da
slay <palgalow@gmail.com>
2024-11-03 15:56:51 +0900

    feat(WinningNumbers): 당첨 번호 및 보너스 번호 객체 클래스 생성, 유효성 검증 기능 추가

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningLotto = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber() {
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+        if (bonusNumber < LottoConstants.LOTTO_MIN_NUMBER || bonusNumber > LottoConstants.LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
+        }
+    }
+}
