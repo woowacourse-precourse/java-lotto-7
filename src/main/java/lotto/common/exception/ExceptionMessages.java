02ff29935fa6b379f6712abb6f31ac4fcbfa6a0d
yyysolhhh <solbaram37@gmail.com>
2024-11-03 20:48:03 +0900

    refactor(ExceptionMessages): 예외 메시지 넓은 범위로 수정
    - NONE_NUMERIC_INPUT을 다른 곳에서도 쓰기위해 넓은 의미의 단어로 수정

--- src/main/java/lotto/common/exception/ExceptionMessages.java
+++ src/main/java/lotto/common/exception/ExceptionMessages.java
@@ -2,7 +2,7 @@ package lotto.common.exception;
 
 public enum ExceptionMessages {
     EMPTY_INPUT("입력값이 없습니다. 다시 입력해주세요."),
-    NONE_NUMERIC_INPUT("구입 금액은 숫자여야 합니다."),
+    NONE_NUMERIC_INPUT("입력값은 숫자여야 합니다."),
     OUT_OF_INTEGER_RANGE("구입 금액은 정수 범위여야 합니다."),
     NOT_MULTIPLE_OF_UNIT_PRICE("구입 금액은 개당 금액의 배수여야 합니다."),
     INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
