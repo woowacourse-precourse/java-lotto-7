03a38926d9d9168a27692a5fce5f182b4e764c5b
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 02:16:53 +0900

    refact: 에러 메시지 수정

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -45,7 +45,7 @@ public class Input {
         try {
             return Integer.parseInt(readLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력해주세요");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력해 주세요");
         }
     }
 }
