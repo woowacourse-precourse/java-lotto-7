b6c1facebb9a7280ba283c269a3e536afdf76e3f
CheChe903 <cheche903@naver.com>
2024-11-03 16:55:11 +0900

    test(InvestmentTest): 금액 초과 단위 테스트 추가

--- src/test/java/lotto/domain/InvestmentTest.java
+++ src/test/java/lotto/domain/InvestmentTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class InvestmentTest {
 
     @Test
-    void 초기금액은_1000으로_나누어떨어지지_않으면_예외를_발생한다() {
+    void 초기_금액은_1000으로_나누어떨어지지_않으면_예외를_발생한다() {
         assertThatThrownBy(() -> new Investment(BigInteger.valueOf(1234)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -17,5 +17,11 @@ class InvestmentTest {
     @Test
     void 금액은_음수면_예외를_발생한다() {
         assertThatThrownBy(() -> new Investment(BigInteger.valueOf(-1))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 초기_금액은_100000_초과가_될수없다() {
+        assertThatThrownBy(() -> new Investment(BigInteger.valueOf(200000)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
