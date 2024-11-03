f70ea5e3b7477df74b1241ea4efd3dccaf873f28
seophohoho <seop0937@gmail.com>
2024-11-03 23:45:56 +0900

    test(Buyer): 당첨 번호와 몇개 동일한지 체크하는 테스트 코드 추가

--- src/test/java/lotto/model/BuyerTest.java
+++ src/test/java/lotto/model/BuyerTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,17 @@ public class BuyerTest {
         Buyer buyer = new Buyer(seller.createLottoTickets(8000));
 
         assertEquals(buyer.getLottos().size(), 8);
+    }
+
+    @Test
+    @DisplayName("당첨 복권과 비교해서 몇개가 동일한지 알아보는 테스트")
+    void testHowManyCorrectMyLotto() {
+        Buyer buyer = new Buyer();
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(40, 39, 38, 37, 36, 1))), 1);
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(40, 39, 38, 37, 2, 1))), 2);
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(40, 39, 38, 3, 2, 1))), 3);
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(40, 39, 4, 3, 2, 1))), 4);
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(40, 5, 4, 3, 2, 1))), 5);
+        assertEquals(buyer.getMatchCount(List.of(1, 2, 3, 4, 5, 6), new Lotto(List.of(6, 5, 4, 3, 2, 1))), 6);
     }
 }
