6dae73505df53167aa9159bd4a5c76058c4503fa
youngseo3 <wys4475@gmail.com>
2024-11-03 18:28:13 +0900

    refactor(LottoNumberTest): 생성한 로또 번호 크기 확인 테스트 삭제 LottoTest.java의 검증 테스트 코드와 일치하다고 생각하여 삭제합니다

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -9,18 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoNumberTest {
 
     @Test
-    void 생성한_로또_번호의_크기가_맞는지_확인한다() {
-        //given
-        int size = 6;
-
-        //when
-        List<Integer> lottoNumbers = LottoNumber.generate();
-
-        //then
-        assertThat(lottoNumbers).hasSize(size);
-    }
-
-    @Test
     void 범위_내의_랜덤_숫자를_반환한다() {
         //given
         int start = 1;
@@ -33,4 +21,5 @@ class LottoNumberTest {
         assertThat(lottoNumbers)
                 .allMatch(number -> number >= start && number <= end);
     }
+
 }
