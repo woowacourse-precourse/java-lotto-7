90451f2146519bdce7c6c78d5418b4e738282969
Parkjyun <jaeyun0507@naver.com>
2024-11-03 20:54:31 +0900

    feat(LottoNumber): 같은 번호를 가지면 같은 객체이다

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -7,4 +7,20 @@ public class LottoNumber {
     public LottoNumber(final String number) {
         this.number = Integer.parseInt(number);
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof LottoNumber lottoNumber)) {
+            return false;
+        }
+        return this.number == lottoNumber.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(number);
+    }
 }
--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -18,4 +18,16 @@ class LottoNumberTest {
         //then
         Assertions.assertThat(lottoNumber).isNotNull();
     }
+
+    @Test
+    @DisplayName("같은 값을 가지면 같은 객체이다")
+    void should_BeEqualInstance_When_GivenSameValue() {
+        //given
+        String number = "1";
+        //when
+        LottoNumber lottoNumber1 = new LottoNumber(number);
+        LottoNumber lottoNumber2 = new LottoNumber(number);
+        //then
+        Assertions.assertThat(lottoNumber1).isEqualTo(lottoNumber2);
+    }
 }
