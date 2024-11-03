82e1010c8578bf16fa3d3dbfc78036ec54a3be8c
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 17:48:13 +0900

    feat: 구입 금액이 1,000원 이상인지 검증하는 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -8,4 +8,10 @@ public class PurchaseAmount {
     public PurchaseAmount(int amount) {
         this.amount = amount;
     }
+
+    private void validateMinimumPurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("구매 금액은 0 이상이어야 합니다.");
+        }
+    }
 }
