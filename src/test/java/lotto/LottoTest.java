0d1f1f8011b7cb14bb332c1b4739626da86866c9
변우영 <likevangogh24@gmail.com>
2024-11-03 09:47:52 +0900

    test(Lotto): 당첨 번호 일치 개수 확인 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.WinningNumber;
+import lotto.dto.MatchCondition;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +27,18 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호와 당첨 번호를 비교해 일치 개수를 확인한다.")
+    @Test
+    void countMatchingNumber() {
+        String enteredWinningNumbers = "1,2,3,4,5,6";
+        List<Integer> lottoNumbers = List.of(1,2,3,4,7,9);
+
+        WinningNumber winningNumber = WinningNumber.from(enteredWinningNumbers);
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        MatchCondition matchCondition = lotto.compareWithWinningNumbers(winningNumber, BonusNumber.from("10"));
+        int actualMatchCount = matchCondition.matchCount();
+
+        Assertions.assertThat(actualMatchCount).isEqualTo(4);
+    }
 }
