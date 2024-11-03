69edf6014555dfaf48d60ae90e34a8b9f6e29068
badgelatte <saucy5939@gmail.com>
2024-11-03 22:40:50 +0900

    printLottoInfo 메서드 구현
    구입 갯수 알리는 문구와 로또 번호 내역을 출력하는 메서드 구현

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -13,11 +13,23 @@ public class Customer {
 
     public void buyLotto(String money) {
         int count = LottoTicketFactory.getLottoTicketCount(money);
+
         while(count > 0) {
             List<Integer> lottoNums = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Lotto lotto = new Lotto(lottoNums);
             lottos.add(lotto);
             count--;
         }
+
+        printLottoInfo(lottos.size());
+    }
+
+    public void printLottoInfo(int count) {
+        System.out.println(count + "를 구매했습니다.");
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+        System.out.println();
     }
 }
