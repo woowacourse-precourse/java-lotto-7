331e272ff0641f01ad254326f4f92b4e6d97acbd
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 08:52:42 +0900

    refactor(Lotto): 로또 개수 및 중복 숫자 예외 처리 로직 분리
    - validate 메서드에서 로또 개수 예외를 던지는 부분을 validateLottoNumberSize 메서드로 분리
    - validateLottoNumberSize 메서드에서 사용되는 조건을 isLottoNumberSize 메서드로 분리
    - validate 메서드에서 중복 숫자 예외를 던지는 부분을 validateDuplication 메서드로 분리
    - validateDuplication 메서드에서 사용되는 조건을 isDuplication 메서드로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,6 @@ package lotto;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,12 +12,27 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateLottoNumberSize(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateLottoNumberSize(List<Integer> numbers) {
+        if (isLottoNumberSize(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-        Set<Integer> set = new HashSet<>(numbers);
-        if (set.size() != numbers.size()) {
+    }
+
+    private void validateDuplication(List<Integer> numbers){
+        if (isDuplication(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.");
         }
+    }
+
+    private boolean isLottoNumberSize (List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private boolean isDuplication (List<Integer> numbers) {
+        return new HashSet<>(numbers).size() != numbers.size();
     }
 }
