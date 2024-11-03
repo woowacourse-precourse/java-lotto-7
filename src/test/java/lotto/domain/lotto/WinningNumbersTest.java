41ebe7ae7038132a6679c6d7f6aff8c1584bf298
CheChe903 <cheche903@naver.com>
2024-11-03 13:45:54 +0900

    feat(WinningNumbersTest): 단위 테스트 구현
    주어진 값에 따라 랭크가 정확히 나오는 단위 테스트를 진행합니다.

---
+++ src/test/java/lotto/domain/lotto/WinningNumbersTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lotto.domain.Rank;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class WinningNumbersTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, false, FIRST",
+            "5, true, SECOND",
+            "5, false, THIRD",
+            "4, false, FOURTH",
+            "3, false, FIFTH",
+            "2, false, MISS",
+            "1, false, MISS",
+            "0, false, MISS"
+    })
+    void 랭크는_올바르게_나와야한다(int matchCount, boolean hasBonusNumber, Rank expectedRank) {
+        // when
+        Rank lottoResult = Rank.of(matchCount, hasBonusNumber);
+
+        // then
+        assertEquals(expectedRank, lottoResult);
+    }
+
+}
