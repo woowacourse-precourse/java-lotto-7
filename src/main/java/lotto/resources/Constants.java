fc9d16ec02fdd0211eb00ffa67192363b53530cf
김건학 <kkh10002@naver.com>
2024-11-03 19:02:31 +0900

    feat(winning): 맞춘 번호 개수 및 보너스 번호 개수 상수 추가

--- src/main/java/lotto/resources/Constants.java
+++ src/main/java/lotto/resources/Constants.java
@@ -6,4 +6,11 @@ public class Constants {
     public static final int LOTTO_TOTAL_NUMBERS = 6;
     public static final int THOUSAND_UNIT = 1000;
     public static final int ZERO = 0;
+    public static final int MATCH_THREE = 3;
+    public static final int MATCH_FOUR = 4;
+    public static final int MATCH_FIVE = 5;
+    public static final int MATCH_SIX = 6;
+    public static final boolean MATCH_BONUS = true;
+    public static final boolean FAIL_MATCH_BONUS = false;
+
 }
