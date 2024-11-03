69c3b70c91a4f7af3b5f1acf36fb19fa2bf6cf21
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-03 23:36:06 +0900

    refactor(InputView): 사용자 입력 메서드 자료형 변환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,42 +4,31 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public int getPriceInput() {
+    public String inputPrice() {
         printInputPrice();
-        return inputPrice();
+        return Console.readLine();
     }
 
-    public String getWinningNumberInput() {
+    public String inputWinningNumber() {
         printInputWinningNumbers();
-        return inputWinningNumbers();
+        return Console.readLine();
     }
 
-    public int getBonusNumberInput() {
+    public String inputBonusNumber() {
         printInputBonusNumber();
-        return inputBonusNumber();
+        return Console.readLine();
     }
 
     private void printInputPrice() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
 
-    private int inputPrice() {
-        return Integer.parseInt(Console.readLine());
-    }
-
     private void printInputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-    }
-
-    private String inputWinningNumbers() {
-        return Console.readLine();
     }
 
     private void printInputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
-    private int inputBonusNumber() {
-        return Integer.parseInt(Console.readLine());
-    }
 }
