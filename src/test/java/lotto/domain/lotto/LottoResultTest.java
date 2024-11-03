8a777aa89bc38e559ee711dced096fa3769a8e53
CheChe903 <cheche903@naver.com>
2024-11-03 14:23:59 +0900

    test(LottoResultTest): Rank 정보가 있는 Map 단위 테스트 구현
    Map에 올바르게 적용되는지 확인하는 테스트를 진행합니다.

--- src/test/java/lotto/domain/lotto/LottoResultTest.java
+++ src/test/java/lotto/domain/lotto/LottoResultTest.java
@@ -3,11 +3,37 @@ package lotto.domain.lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lotto.domain.Investment;
+import lotto.domain.Rank;
+import lotto.domain.generator.RandomIntegerListGenerator;
+import lotto.domain.sorting.AscendingSorter;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
+
+    @Test
+    void 랭크의_개수가_올바르게_올라야한다() {
+
+        List<LottoNumber> numbers = new ArrayList<>(List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)));
+
+        Lotto lotto = new Lotto(numbers);
+        List<Lotto> testLottoBundle = new ArrayList<>(List.of(lotto));
+        WinningNumbers winningNumbers = new WinningNumbers(numbers, new LottoNumber(7));
+        LottoBundle lottoBundle = new LottoBundle(testLottoBundle, new RandomIntegerListGenerator(),
+                new AscendingSorter());
+        LottoResult lottoResult = new LottoResult(new EnumMap<>(Rank.class), BigInteger.ZERO);
+
+        lottoResult.calculate(lottoBundle, winningNumbers);
+        Map<Rank, Integer> rankCount = lottoResult.getRankCount();
+
+        assertThat(rankCount.get(Rank.FIRST)).isEqualTo(1);
+    }
 
     @Test
     void 수익률은_잘나와야_한다() {
