7265f23d52aebfc0e6a3171461600c05abebc97d
Jo Myeong-geun <happy63590@gmail.com>
2024-11-04 03:54:42 +0900

    test: 당첨 등수를 구하는 기능 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -30,4 +30,114 @@ class LottoServiceTest {
         List<Lotto> lottos = lottoService.getLottos();
         assertEquals(8, lottos.size());
     }
+
+    @Test
+    void 당첨_번호와_3개_번호가_일치하면_5등이다() {
+        //given
+        List<Lotto> lottos = lottoService.getLottos();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 43, 44, 45);
+        int bonusNumber = 7;
+
+        //when
+        lottoService.saveLottoRanks(winningNumbers, bonusNumber);
+
+        //then
+        Map<Rank, Integer> results = lottoService.getResults();
+        assertEquals(1, results.getOrDefault(Rank.FIFTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.FOURTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.THIRD, 0));
+        assertEquals(0, results.getOrDefault(Rank.SECOND, 0));
+        assertEquals(0, results.getOrDefault(Rank.FIRST, 0));
+    }
+
+    @Test
+    void 당첨_번호와_4개_번호가_일치하면_4등이다() {
+        //given
+        List<Lotto> lottos = lottoService.getLottos();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 44, 45);
+        int bonusNumber = 7;
+
+        //when
+        lottoService.saveLottoRanks(winningNumbers, bonusNumber);
+
+        //then
+        Map<Rank, Integer> results = lottoService.getResults();
+        assertEquals(0, results.getOrDefault(Rank.FIFTH, 0));
+        assertEquals(1, results.getOrDefault(Rank.FOURTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.THIRD, 0));
+        assertEquals(0, results.getOrDefault(Rank.SECOND, 0));
+        assertEquals(0, results.getOrDefault(Rank.FIRST, 0));
+    }
+
+    @Test
+    void 당첨_번호와_5개_번호가_일치하면_3등이다() {
+        //given
+        List<Lotto> lottos = lottoService.getLottos();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 45);
+        int bonusNumber = 7;
+
+        //when
+        lottoService.saveLottoRanks(winningNumbers, bonusNumber);
+
+        //then
+        Map<Rank, Integer> results = lottoService.getResults();
+        assertEquals(0, results.getOrDefault(Rank.FIFTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.FOURTH, 0));
+        assertEquals(1, results.getOrDefault(Rank.THIRD, 0));
+        assertEquals(0, results.getOrDefault(Rank.SECOND, 0));
+        assertEquals(0, results.getOrDefault(Rank.FIRST, 0));
+    }
+
+    @Test
+    void 당첨_번호와_5개_번호가_일치하고_보너스_번호가_일치하면_2등이다() {
+        //given
+        List<Lotto> lottos = lottoService.getLottos();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 45);
+        int bonusNumber = 6;
+
+        //when
+        lottoService.saveLottoRanks(winningNumbers, bonusNumber);
+
+        //then
+        Map<Rank, Integer> results = lottoService.getResults();
+        assertEquals(0, results.getOrDefault(Rank.FIFTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.FOURTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.THIRD, 0));
+        assertEquals(1, results.getOrDefault(Rank.SECOND, 0));
+        assertEquals(0, results.getOrDefault(Rank.FIRST, 0));
+    }
+
+    @Test
+    void 당첨_번호와_6개_번호가_일치하면_1등이다() {
+        //given
+        List<Lotto> lottos = lottoService.getLottos();
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        lottos.add(new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        //when
+        lottoService.saveLottoRanks(winningNumbers, bonusNumber);
+
+        //then
+        Map<Rank, Integer> results = lottoService.getResults();
+        assertEquals(0, results.getOrDefault(Rank.FIFTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.FOURTH, 0));
+        assertEquals(0, results.getOrDefault(Rank.THIRD, 0));
+        assertEquals(0, results.getOrDefault(Rank.SECOND, 0));
+        assertEquals(1, results.getOrDefault(Rank.FIRST, 0));
+    }
 }
