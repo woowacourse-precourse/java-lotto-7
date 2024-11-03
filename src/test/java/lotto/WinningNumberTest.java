af9347b1223398b1b3523d04c90ba31d397b01ae
min-0 <dhe08133@naver.com>
2024-11-04 02:07:39 +0900

    test: 각 테스트 별 예외 메세지 검증 추가

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -3,7 +3,6 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WinningNumberTest {
     @DisplayName("당첨 번호가 숫자가 아닐 때 예외가 발생한다.")
@@ -11,9 +10,10 @@ class WinningNumberTest {
     void 당첨_번호가_숫자가_아닐_떄_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
         String input = "1ㅏ";
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThatThrownBy(() -> {
             winningNumber.parseWinningNumbers(input);
-        });
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호 입력이 잘못되었습니다.");
     }
 
     @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -21,9 +21,10 @@ class WinningNumberTest {
     void 당첨_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
         String input = "1,2,3,4,5,6,7";
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThatThrownBy(() -> {
             winningNumber.parseWinningNumbers(input);
-        });
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 6개여야 합니다.");
     }
 
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -31,9 +32,10 @@ class WinningNumberTest {
     void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
         String input = "1,2,3,3,4,5";
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThatThrownBy(() -> {
             winningNumber.parseWinningNumbers(input);
-        });
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 중복될 수 없습니다.");
     }
 
     @DisplayName("당첨 번호가 범위를 벗어나면 예외가 발생한다.")
@@ -41,29 +43,32 @@ class WinningNumberTest {
     void 당첨_번호가_범위를_벗어나면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
         String input = "1,2,3,3,4,46";
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThatThrownBy(() -> {
             winningNumber.parseWinningNumbers(input);
-        });
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
     @DisplayName("보너스 번호가 숫자가 아닐 때 예외가 발생한다.")
     @Test
     void 보너스_번호가_숫자가_아닐_떄_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
-        String input = "아";
-        assertThrows(IllegalArgumentException.class, () -> {
-            winningNumber.parseBonusNumber(input);
-        });
+        String bonusNumber = "아";
+        assertThatThrownBy(() -> {
+            winningNumber.parseBonusNumber(bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호 입력이 잘못되었습니다.");
     }
 
     @DisplayName("보너스 번호가 1개를 넘어가면 예외가 발생한다.")
     @Test
     void 보너스_번호가_1개를_넘어가면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
-        String input = "1,6";
-        assertThrows(IllegalArgumentException.class, () -> {
-            winningNumber.parseBonusNumber(input);
-        });
+        String bonusNumber = "1,6";
+        assertThatThrownBy(() -> {
+            winningNumber.parseBonusNumber(bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호 입력이 잘못되었습니다.");
     }
 
     @DisplayName("보너스 번호에 당첨 번호와 중복된 숫자가 있으면 예외가 발생한다.")
@@ -71,9 +76,9 @@ class WinningNumberTest {
     void 보너스_번호에_당첨_번호와_중복된_숫자가_있으면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
         winningNumber.parseWinningNumbers("1,2,3,4,5,6");
-        int duplicateBonusNumber = 3;
+        String duplicateBonusNumber = "3";
         assertThatThrownBy(() -> {
-            winningNumber.validateBonusNumberDuplication(duplicateBonusNumber);
+            winningNumber.parseBonusNumber(duplicateBonusNumber);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
@@ -82,9 +87,10 @@ class WinningNumberTest {
     @Test
     void 보너스_번호가_범위를_벗어나면_예외가_발생한다() {
         WinningNumber winningNumber = new WinningNumber();
-        String input = "66";
-        assertThrows(IllegalArgumentException.class, () -> {
-            winningNumber.parseBonusNumber(input);
-        });
+        String bonusNumber = "66";
+        assertThatThrownBy(() -> {
+            winningNumber.parseBonusNumber(bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 }
