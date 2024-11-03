3ad5963f03c122fe7232db772f123e9f9d256ff9
smyeongL <sangmyeonglee123@gmail.com>
2024-11-04 02:16:08 +0900

    feat(application): call winning numbers controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,11 +2,14 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.controller.PurchaseAmountController;
+import lotto.controller.WinningNumbersController;
 import lotto.dto.PurchaseAmountRequestDto;
+import lotto.dto.WinningNumbersRequestDto;
 
 public class Application {
     public static void main(String[] args) {
-        PurchaseAmountRequestDto purchaseAmountRequestDto =  PurchaseAmountController.run();
+        PurchaseAmountRequestDto purchaseAmountRequestDto = PurchaseAmountController.run();
         LottoController.run(purchaseAmountRequestDto);
+        WinningNumbersRequestDto winningNumbersRequestDto = WinningNumbersController.run();
     }
 }
