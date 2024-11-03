44b56b20dd4f7d694bc3ee9a62deecbf33325474
jbilee <julishahn@gmail.com>
2024-11-03 21:18:56 +0900

    test(utils): 소수점 반올림 기능 테스트 코드 버그 수정
    - `assertEquals()` 함수의 인자가 반대로 기입돼있던 부분 수정

--- src/test/java/lotto/utils/DecimalsTest.java
+++ src/test/java/lotto/utils/DecimalsTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class DecimalsTest {
     @Test
     void 반올림_기능_테스트() {
-        assertEquals(Decimals.round(10.15, 1), 10.2);
-        assertEquals(Decimals.round(10.151, 2), 10.15);
-        assertEquals(Decimals.round(10.0007, 3), 10.001);
+        assertEquals(10.2, Decimals.round(10.15, 1));
+        assertEquals(10.15, Decimals.round(10.151, 2));
+        assertEquals(10.001, Decimals.round(10.0007, 3));
     }
 
     @DisplayName("유효하지 않은 소수점 자리 값을 입력하면 예외가 발생한다.")
