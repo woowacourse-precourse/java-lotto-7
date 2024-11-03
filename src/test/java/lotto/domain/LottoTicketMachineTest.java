68f16ee8580997dabf28b46131a019eec73f7992
tentenball <lth8905@gmail.com>
2024-11-04 03:26:12 +0900

    style: 코드 공백 추가

--- src/test/java/lotto/domain/LottoTicketMachineTest.java
+++ src/test/java/lotto/domain/LottoTicketMachineTest.java
@@ -46,12 +46,14 @@ public class LottoTicketMachineTest {
 			Map.of(5, true),
 			Map.of(5, false)
 		);
+
 		String expectedOutput = """
 			3개 일치 (5,000원) - 1개
 			4개 일치 (50,000원) - 0개
 			5개 일치 (1,500,000원) - 1개
 			5개 일치, 보너스 볼 일치 (30,000,000원) - 1개
 			6개 일치 (2,000,000,000원) - 0개""";
+
 		String lottoResult = lottoTicketMachine.registerLottoResult(winningStatus, lottoResults);
 		assertThat(lottoResult).isEqualTo(expectedOutput);
 	}
