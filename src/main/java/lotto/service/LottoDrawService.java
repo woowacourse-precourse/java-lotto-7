c5a943e52e2ce7fed368eaa66dac068ed6d2d86b
MinJae00 <exceedmin@gmail.com>
2024-11-04 02:28:11 +0900

    refact: 수익률 계산 로직과 통계 로직을 한번에 당첨 서비스에서 처리

--- src/main/java/lotto/service/LottoDrawService.java
+++ src/main/java/lotto/service/LottoDrawService.java
@@ -1,23 +1,24 @@
 package lotto.service;
 
+import java.util.List;
 import java.util.Map;
-import lotto.domain.LottoRank;
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
 import lotto.domain.WinningChecker;
 import lotto.repository.LottoRepository;
 
-public class WinningManager {
+public class LottoDrawService {
     public Map<String, Integer> checkWinning(LottoRepository lottoRepository) {
-        Map<String, Integer> matchingResult = new WinningChecker(lottoRepository).checkWinning();
-        calculateRate(matchingResult, lottoRepository);
+        Bonus bonus = lottoRepository.getBonusNumber();
+        Lotto lotto = lottoRepository.getWinningNumbers();
+        List<List<Integer>> userLotto = lottoRepository.getLottoNumbers();
+
+        WinningChecker winningChecker = new WinningChecker();
+        Map<String, Integer> matchingResult = winningChecker.checkWinning(bonus, lotto, userLotto);
         return matchingResult;
     }
 
-    public double calculateRate(Map<String, Integer> matchingResult, LottoRepository lottoRepository) {
-        double sum = 0;
-        for (LottoRank rank : LottoRank.values()) {
-            sum += rank.getPrize() * matchingResult.get(rank.name());
-        }
-        double result = sum / (lottoRepository.getLottoNumbers().size() * 1000) * 100;
-        return result;
+    public double calculateRate(Map<String, Integer> matchingResult, int numberOfLotto) {
+        return WinningChecker.calculateReturn(matchingResult, numberOfLotto);
     }
 }
