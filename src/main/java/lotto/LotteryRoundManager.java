264b22d17c055dd2d619ca802e060c03eab3e0a6
GoGradually <chkun3109@gmail.com>
2024-11-03 22:25:31 +0900

    feat(LotteryRoundManager.java): 당첨번호 등록 기능 구현
    - 보너스 번호와 당첨 번호 간에는 겹치는 수가 없어야 한다.
    - 보너스 번호와 당첨 번호는 모두 1 이상 45 이하여야 한다.
    - 로또 당첨 번호는 6개여야 한다.

--- src/main/java/lotto/LotteryRoundManager.java
+++ src/main/java/lotto/LotteryRoundManager.java
@@ -22,10 +22,25 @@ public class LotteryRoundManager {
         if (cost % UNIT_COST != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE + "금액은 1000원 단위로 입력되어야 합니다.");
         }
-        return PurchasedLotto.generateLottos(
-                cost / UNIT_COST,
-                () -> Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        );
+        return PurchasedLotto.generateLottos(cost / UNIT_COST, () -> Randoms.pickUniqueNumbersInRange(1, 45, 6));
     }
 
+    public void setWinningNumber(List<Integer> winningNumber, int bonusNumber) {
+        validateWinningNumber(winningNumber, bonusNumber);
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateWinningNumber(List<Integer> winningNumber, int bonusNumber) {
+        if (!winningNumber.stream().filter(n -> (n < 1 || n > 45)).toList().isEmpty())
+            throw new IllegalArgumentException(ERROR_MESSAGE + "로또 당첨 번호는 1 이상 45 이하여야 합니다.");
+        if (winningNumber.stream().distinct().count() != winningNumber.size())
+            throw new IllegalArgumentException(ERROR_MESSAGE + "로또 당첨 번호는 겹치지 않아야 합니다.");
+        if (winningNumber.size() != 6)
+            throw new IllegalArgumentException(ERROR_MESSAGE + "로또 당첨 번호는 6개여야 합니다.");
+        if (bonusNumber < 1 || bonusNumber > 45)
+            throw new IllegalArgumentException(ERROR_MESSAGE + "보너스 번호는 1 이상 45 이하여야 합니다.");
+        if (winningNumber.contains(bonusNumber))
+            throw new IllegalArgumentException(ERROR_MESSAGE + "보너스 번호는 당첨 번호와 겹치지 않아야 합니다.");
+    }
 }
