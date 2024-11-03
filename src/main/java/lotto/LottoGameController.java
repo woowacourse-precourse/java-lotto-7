5ef1947ee38197f72406cbd872847a438f3962a8
mins-git <m2insapp@gmail.com>
2024-11-03 14:28:15 +0900

    feat(LottoGameController): 로또 서비스 기능 구현 - view 먼저 구현 필요 - view 구현 이후 controller 구현진행

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,4 +1,30 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoGameController {
+    private final LottoService lottoService;
+    private final LottoInputView LottoInputView;
+    private final LottoOutputView LottoOutputView;
+
+
+    public LottoGameController(lotto.LottoInputView lottoInputView, LottoService lottoService, lotto.LottoOutputView lottoOutputView) {
+        LottoInputView = lottoInputView;
+        this.lottoService = lottoService;
+        LottoOutputView = lottoOutputView;
+    }
+
+    public void play(){
+        List<Lotto> purchaseLottos = purchaseLottos(); // 로또를 구입한 후
+        Lotto winningLotto = getWinningLotto(); // 이긴 로또 가져오고
+        int bonusNumber = getBonusNumber(winningLotto); // 보너스 로또를 체크하고
+        showResult(purchaseLottos, winningLotto,bonusNumber); // 결과 보여주기
+    }
+
+    private List<Lotto> purchaseLottos(){
+        try {
+            int amount = LottoInputView.();
+
+        }
+    }
 }
