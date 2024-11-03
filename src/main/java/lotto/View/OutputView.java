8d61a55038c19af6d719531ad51b5b1d4ed3ed7e
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:51:42 +0900

    feat(OutputView): 사용자가 입력한 금액만큼 생성된 랜덤한 로또를 출력하는 로직 추가
    - outputRandomLottoNumber() 메서드를 통해 구매 수량과 랜덤하게 생성된 로또 번호 출력 기능 구현

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,4 +1,26 @@
 package lotto.View;
 
+import static lotto.Util.Constant.IOMessage.OUTPUT_LOTTO_COUNT;
+
+import java.util.List;
+import lotto.DTO.RandomLottoNumberDTO;
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoTickets;
+
 public class OutputView {
+    private LottoTickets lottoTickets;
+    private List<Lotto> lottos;
+    private Integer lottoPurchaseCount;
+
+    public void outputRandomLottoNumber(RandomLottoNumberDTO randomLottoNumberDTO) {
+        this.lottoTickets = randomLottoNumberDTO.getLottoTickets();
+        this.lottos = lottoTickets.getTickets();
+        this.lottoPurchaseCount = lottoTickets.getTicketCount();
+
+        System.out.println(OUTPUT_LOTTO_COUNT.format(lottoPurchaseCount));
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
 }
