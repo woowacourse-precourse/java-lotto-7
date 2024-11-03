84ca5c05284d8aee97202b09ab78e1ea33f15174
yunhacandy <yunhacandy@gmail.com>
2024-11-03 21:30:13 +0900

    refactor (LottoValidator): 당첨 번호 검증 코드 클래스로 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,37 +1,14 @@
 package lotto.model;
 
-import static lotto.constant.Constants.LOTTO_NUMBER_LENGTH;
-import static lotto.constant.Constants.LOTTO_NUMBER_RANGE_END;
-import static lotto.constant.Constants.LOTTO_NUMBER_RANGE_START;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.exception.InputNumberException;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBER_LENGTH) {
-            throw new IllegalArgumentException(InputNumberException.INVALID_LENGTH.getMessage());
-        }
-
-        for (int number : numbers) {
-            if (number < LOTTO_NUMBER_RANGE_START || number > LOTTO_NUMBER_RANGE_END) {
-                throw new IllegalArgumentException(InputNumberException.INVALID_RANGE.getMessage());
-            }
-        }
-
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException(InputNumberException.DUPLICATE_NUMBERS.getMessage());
-        }
     }
 
     // TODO: 추가 기능 구현
