28e215db78379283acca761b9b47686dfe3fc850
dnjswls <dnjswls6670@naver.com>
2024-11-04 03:42:13 +0900

    feat: 로또 구입 금액 입력 및 유효성 검사 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmount();
+        System.out.println("구입 금액: " + purchaseAmount);
+    }
+
+    private static int getPurchaseAmount() {
+        int amount = 0;
+
+        while (true) {
+            try {
+                System.out.println("구입 금액을 입력해 주세요.");
+                amount = Integer.parseInt(Console.readLine());
+
+                // 1000원 단위 확인하기
+                if (amount <= 0 || amount % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+                }
+                break; // 유효한 금액이면 반복문 탈출
+
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해야 합니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return amount;
     }
 }
