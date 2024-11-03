a989e59ebc4d6de878e89add517b41425ee350a8
sihyun10 <hyhy200170@gmail.com>
2024-11-04 00:42:28 +0900

    feat(WinningStatistics) : 당첨 통계 및 수익률 구하는 기능
    WinningPrize 클래스를 활용해 상금을 탔다면 당첨 횟수를 증가시킵니다.
    increase()메서드를 통해 특정 상금 카테고리에 대한 횟수를 1
    증가시켜줍니다. 그다음 display()를 통해 당첨 통계를 출력하고, 총 수익률
    계산기능도 구현하였습니다.

---
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningStatistics {
+    private final Map<LottoPrize, Integer> prizeCount;
+
+    public WinningStatistics() {
+        this.prizeCount = new HashMap<>();
+        initializePrizeCount();
+    }
+
+    private void initializePrizeCount() {
+        for (LottoPrize prize : LottoPrize.values()) {
+            prizeCount.put(prize, 0);
+        }
+    }
+
+    public void calculateCount(List<Lotto> purchasedLotto, WinningPrize winningPrize) {
+        for (Lotto lotto : purchasedLotto) {
+            LottoPrize prize = winningPrize.getPrize(lotto);
+            if (prize != null) {
+                increase(prize);
+            }
+        }
+    }
+
+    private void increase(LottoPrize prize) {
+        prizeCount.put(prize, prizeCount.get(prize) + 1);
+    }
+
+    public void display() {
+        System.out.println("당첨 통계\n---");
+        for (LottoPrize prize : LottoPrize.values()) {
+            int count = prizeCount.getOrDefault(prize, 0);
+            System.out.printf("%s - %d개\n", prize, count);
+        }
+    }
+
+    public double calculateTotalProfit(int purchaseAmount) {
+        int totalPrizeAmount = 0;
+        for (LottoPrize prize : LottoPrize.values()) {
+            int count = prizeCount.getOrDefault(prize, 0);
+            totalPrizeAmount += count * prize.getPrizeAmount();
+        }
+
+        double yield = (double) totalPrizeAmount / purchaseAmount * 100;
+
+        return Math.round(yield * 100) / 100.0;
+    }
+}
