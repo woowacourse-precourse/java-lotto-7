d00f28a5cc6abc2a71f2159c9f9acfed9e172573
EunSeop <doosan1112@naver.com>
2024-11-03 23:00:55 +0900

    refactor: enum을 활용해 상금 상수들 관리

--- src/main/java/lotto/calculator/LottoRateCalculator.java
+++ src/main/java/lotto/calculator/LottoRateCalculator.java
@@ -1,27 +1,20 @@
 package lotto.calculator;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 public class LottoRateCalculator {
-
-    private static final long FIRST_PRIZE = 2000000000;
-    private static final long SECOND_PRIZE = 30000000;
-    private static final long THIRD_PRIZE = 1500000;
-    private static final long FOURTH_PRIZE = 50000;
-    private static final long FIFTH_PRIZE = 5000;
 
     private double rate;
 
     public void calculate(int price, List<Integer> lottoResult) {
         long totalPrize = 0;
 
-        totalPrize += lottoResult.get(0) * FIRST_PRIZE;
-        totalPrize += lottoResult.get(1) * SECOND_PRIZE;
-        totalPrize += lottoResult.get(2) * THIRD_PRIZE;
-        totalPrize += lottoResult.get(3) * FOURTH_PRIZE;
-        totalPrize += lottoResult.get(4) * FIFTH_PRIZE;
+        totalPrize += lottoResult.get(0) * Prize.FIRST.get();
+        totalPrize += lottoResult.get(1) * Prize.SECOND.get();
+        totalPrize += lottoResult.get(2) * Prize.THIRD.get();
+        totalPrize += lottoResult.get(3) * Prize.FOURTH.get();
+        totalPrize += lottoResult.get(4) * Prize.FIFTH.get();
+
         rate = ((double) totalPrize / price) * 100;
 
         halfUp();
