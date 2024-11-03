7c1ae45e4072354bf5092718e684eccf3352eff4
조민규 <ariettys2@naver.com>
2024-11-03 22:15:01 +0900

    fix : 형식에 맞게 println문 추가

--- src/main/java/lotto/view/InputHandler.java
+++ src/main/java/lotto/view/InputHandler.java
@@ -3,16 +3,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputHandler {
     public String getBuyMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         InputValidate.validateBuyMoney(input);
         return input;
     }
     public String[] getWinningNum(){
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         InputValidate.validateWinningNumInput(input);
         return input.split(",");
     }
     public String getBonusNum(){
+        System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         InputValidate.validateBonusNum(input);
         return input;
