891cdd0890f8084436577384e9f94bc9a541da7e
jijongkwon <help3451@naver.com>
2024-11-03 16:40:31 +0900

    feat(view): input, output view 클래스 생성
    TODO
    - 로또 구입 금액 입력화면을 출력한다.
    - 당첨 번호 입력화면을 출력한다.
    - 보너스 번호 입력화면을 출력한다.
    - 발행한 로또 수량 및 번호를 출력한다.
    - 당첨내역을 출력한다.
    - 수익률을 출력한다.

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class InputView {
+    // TODO: 로또 구입 금액 입력화면을 출력한다.
+
+    // TODO: 당첨 번호 입력화면을 출력한다.
+
+    // TODO: 보너스 번호 입력화면을 출력한다.
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputView {
+
+    // TODO: 발행한 로또 수량 및 번호를 출력한다.
+
+    // TODO: 당첨내역을 출력한다.
+
+    // TODO: 수익률을 출력한다.
+}
