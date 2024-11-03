4f55d99f0953f2be8d5c2bc9852f590aa3ea8d2d
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-03 16:41:18 +0900

    feat: 출력 형식 처리 기능 구현

---
+++ src/main/java/lotto/system/formater/PrintFormatter.java
@@ -0,0 +1,38 @@
+package lotto.system.formater;
+
+import lotto.system.lottoGetter.LottoTicketIssuer;
+import lotto.system.utils.Parser;
+import lotto.user.Bonus;
+import lotto.user.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutView;
+
+public class PrintFormatter {
+
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static LottoTicketIssuer formatPurchaseInfo() {
+        OutView.printMessage(PURCHASE_AMOUNT_MESSAGE);
+        return new LottoTicketIssuer(InputView.inputPurchaseAmount());
+    }
+
+    public static void formatLottoTickets(LottoTicketIssuer ticketIssuer) {
+        ticketIssuer.issueLottoTickets().forEach(System.out::println);
+    }
+
+    public static void formatWinningNumbers() {
+        OutView.printMessage(WINNING_NUMBER_MESSAGE);
+        new Lotto(Parser.parseWithDelimiter(InputView.inputWinningNumbers()));
+    }
+
+    public static void formatBonusNumber() {
+        OutView.printMessage(BONUS_NUMBER_MESSAGE);
+        new Bonus(InputView.inputBonusNumber());
+    }
+
+    public static void formatResult(String result) {
+        OutView.printMessageWithNewLine(result);
+    }
+}
