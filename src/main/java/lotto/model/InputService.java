cd89aa1764b4defe5db2cedfe7e2931c9da943c8
immyeong <wpfls6670@naver.com>
2024-11-03 19:02:35 +0900

    refactor: 예외처리 메서드 분리

--- src/main/java/lotto/model/InputService.java
+++ src/main/java/lotto/model/InputService.java
@@ -12,7 +12,8 @@ public class InputService {
     private static Set<Integer> winningNumberSet = new HashSet<>();
     private static final String CONVERT_ERROR_MESSAGE = "[ERROR] 잘못된 입력값입니다. 입력값은 숫자여야합니다.";
     private static final String AMOUNT_ERROR_MESSAGE = "[ERROR] 구입금액은 1000으로 나누어 떨어져야 합니다.";
-    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호가 중복될 수 없습니다.";
+    private static final String DUPLICATE_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨 번호가 중복될 수 없습니다.";
+    private static final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호가 당첨 번호와 중복될 수 없습니다.";
     private static final String BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1이상 45이하여야 합니다.";
     private static final String BONUS_INVALID_VALUE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 반드시 1개여야 합니다.";
 
@@ -24,9 +25,7 @@ public class InputService {
     public int validatePurchaseValue(String value) {
         int convertValue = convertToInt(value);
 
-        if (convertValue % 1000 != 0) {
-            throw new InvalidPurchaseAmountException(AMOUNT_ERROR_MESSAGE);
-        }
+        checkDivisibleByThousand(convertValue);
         return convertValue;
     }
 
@@ -63,9 +62,7 @@ public class InputService {
         String[] winningNumber = value.split(",");
         for (String number : winningNumber) {
             int convertNumber = convertToInt(number);
-            if (checkDuplicateNumber(convertNumber)) {
-                throw new InvalidDuplicateNumberException(DUPLICATE_ERROR_MESSAGE);
-            }
+            checkDuplicateWinningNumber(convertNumber);
             winningNumbers.add(convertNumber);
         }
 
@@ -75,29 +72,47 @@ public class InputService {
     public int bonusNumberValue() {
         String value = Console.readLine();
 
-        if(checkInvalidValue(value)) throw new InvalidBonusValueException(BONUS_INVALID_VALUE_ERROR_MESSAGE);
+        checkInvalidValue(value);
 
         int convertValue = convertToInt(value);
-        if(convertValue < 1 || convertValue > 45){
-            throw new InvalidBonusNumberException(BONUS_NUMBER_ERROR_MESSAGE);
-        }
-        if (checkDuplicateNumber(convertValue)) {
-            throw new InvalidDuplicateBonusNumberException(DUPLICATE_ERROR_MESSAGE);
-        }
+
+        checkInvalidBonusNumber(convertValue);
+        checkDuplicateBonusNumber(convertValue);
         return convertValue;
     }
-    public boolean checkInvalidValue(String value){
-        if(!value.matches("^\\s*\\d+\\s*$")){
-            return true;
+
+    public boolean checkDivisibleByThousand(int value){
+        if (value % 1000 != 0) {
+            throw new InvalidPurchaseAmountException(AMOUNT_ERROR_MESSAGE);
         }
         return false;
     }
 
-    public boolean checkDuplicateNumber(int number) {
+    public boolean checkDuplicateWinningNumber(int number) {
         if (!winningNumberSet.add(number)) {
-            return true;
+            throw new InvalidDuplicateNumberException(DUPLICATE_WINNING_NUMBER_ERROR_MESSAGE);
         }
+        return false;
+    }
 
+    public boolean checkInvalidValue(String value){
+        if(!value.matches("^\\s*\\d+\\s*$")){
+            throw new InvalidBonusValueException(BONUS_INVALID_VALUE_ERROR_MESSAGE);
+        }
+        return false;
+    }
+
+    public boolean checkInvalidBonusNumber(int convertValue){
+        if(convertValue < 1 || convertValue > 45){
+            throw new InvalidBonusNumberException(BONUS_NUMBER_ERROR_MESSAGE);
+        }
+        return false;
+    }
+
+    public boolean checkDuplicateBonusNumber(int number) {
+        if (!winningNumberSet.add(number)) {
+            throw new InvalidDuplicateBonusNumberException(DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE);
+        }
         return false;
     }
 }
