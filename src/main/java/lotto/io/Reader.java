5b2a491c7d8b2fddf2b06ecaf143d2f8d458d082
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:08:00 +0900

    refactor(Reader): 입력 기능에 안내 메시지 출력 추가

--- src/main/java/lotto/io/Reader.java
+++ src/main/java/lotto/io/Reader.java
@@ -7,18 +7,21 @@ import java.util.List;
 public class Reader {
 
     public static int readMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
 
         return Parser.parseInputToMoney(input);
     }
 
     public static List<Integer> readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
 
         return Parser.parseInputToNumbers(input);
     }
 
     public static int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
 
         return Parser.parseInputToNumber(input);
