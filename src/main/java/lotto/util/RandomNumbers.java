a22f1a266afe9b5e7332f00a72e7597824bc7443
slay <palgalow@gmail.com>
2024-11-03 16:53:30 +0900

    refactor(RandomNumbers): 인자 상수 처리

--- src/main/java/lotto/util/RandomNumbers.java
+++ src/main/java/lotto/util/RandomNumbers.java
@@ -2,10 +2,11 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.domain.LottoConstants;
 
 public class RandomNumbers {
 
     public static List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(LottoConstants.LOTTO_MIN_NUMBER,LottoConstants.LOTTO_MAX_NUMBER,LottoConstants.LOTTO_NUMBERS_COUNT);
     }
 }
