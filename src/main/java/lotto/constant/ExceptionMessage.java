db320edc010011c9c640b8b126063b389b0371f4
songsunkook <songsunkook@gmail.com>
2024-11-03 15:00:06 +0900

    refactor: 매직넘버 상수화

--- src/main/java/lotto/constant/ExceptionMessage.java
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -3,22 +3,20 @@ package lotto.constant;
 import java.text.DecimalFormat;
 
 public enum ExceptionMessage {
-    /* IllegalArgumentException */
-    INVALID_LOTTO_NUMBER_COUNT(
-        String.format("로또 번호는 %d개여야 합니다.",
-            LottoConfig.DEFAULT_NUMBER_COUNT)),
     DUPLICATED_LOTTO_NUMBERS("로또 번호는 중복될 수 없습니다."),
     NOT_NUMBER_MONEY("구입금액은 숫자여야 합니다."),
     NOT_NUMBER_BONUS_NUMBER("보너스 번호는 숫자여야 합니다."),
-    INVALID_MONEY_UNIT("로또 구입 금액은 %s원 단위로 입력해야 합니다."),
     BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    INVALID_LOTTO_NUMBER_COUNT(
+        String.format("로또 번호는 %d개여야 합니다.",
+            LottoConfig.DEFAULT_NUMBER_COUNT)),
+    INVALID_MONEY_UNIT(
+        String.format("로또 구입 금액은 %s원 단위로 입력해야 합니다.",
+            LottoConfig.LOTTO_COST)),
     INVALID_LOTTO_NUMBER(
         String.format("로또 번호는 %d에서 %d 이내의 숫자여야 합니다.",
             LottoConfig.LOTTO_NUMBER_START_INCLUSIVE,
             LottoConfig.LOTTO_NUMBER_END_INCLUSIVE)),
-
-    /* IllegalStateException */
-    NOT_EXIST_RANK("존재하지 않는 등수입니다."),
     ;
 
     private final String message;
@@ -36,5 +34,9 @@ public enum ExceptionMessage {
             return String.format(message, new DecimalFormat("###,###").format(content));
         }
         return String.format(message, content);
+    }
+
+    public String getMessage(Object... args) {
+        return String.format(message, args);
     }
 }
--- src/main/java/lotto/domain/Wallet.java
+++ src/main/java/lotto/domain/Wallet.java
@@ -1,9 +1,10 @@
 package lotto.domain;
 
 import static lotto.constant.ExceptionMessage.INVALID_MONEY_UNIT;
+import static lotto.constant.LottoConfig.LOTTO_COST;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,28 +13,29 @@ import lotto.random.LottoRandom;
 
 public class Wallet {
 
-    private long money;
-    private long originalMoney;
-    private Map<Rank, Integer> ranks = new HashMap<>();
     private final List<Lotto> lottos = new ArrayList<>();
+    private final Map<Rank, Integer> rankCounts = new EnumMap<>(Rank.class);
+
+    private long money;
+    private long initalMoney;
 
     public Wallet(long money) {
         validateMoney(money);
         this.money = money;
-        originalMoney = money;
+        initalMoney = money;
     }
 
     public void buyLottoTickets(LottoRandom strategy) {
-        long ticketCount = money / 1000;
-        money -= ticketCount * 1000;
+        long ticketCount = money / LOTTO_COST;
+        money -= ticketCount * LOTTO_COST;
         while (ticketCount-- > 0) {
             lottos.add(new Lotto(strategy.getLottoNumbers()));
         }
     }
 
     private void validateMoney(long money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException(INVALID_MONEY_UNIT.getMessage(1000));
+        if (money % LOTTO_COST != 0) {
+            throw new IllegalArgumentException(INVALID_MONEY_UNIT.getMessage());
         }
     }
 
@@ -46,17 +48,21 @@ public class Wallet {
     }
 
     public void addRank(Rank rank) {
-        ranks.merge(rank, 1, Integer::sum);
+        rankCounts.merge(rank, 1, Integer::sum);
     }
 
     public double gain() {
-        long totalPrice = ranks.entrySet().stream()
-            .mapToLong(set -> set.getKey().getPrice() * set.getValue())
+        long totalPrice = rankCounts.entrySet().stream()
+            .mapToLong(set -> {
+                Rank rank = set.getKey();
+                int count = set.getValue();
+                return rank.getPrice() * count;
+            })
             .sum();
-        return (double)totalPrice / originalMoney;
+        return (double)totalPrice / initalMoney;
     }
 
     public Map<Rank, Integer> getRanks() {
-        return ranks;
+        return rankCounts;
     }
 }
