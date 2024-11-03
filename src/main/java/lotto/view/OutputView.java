e818175b5958c424545b349c0c6fbd56685679b4
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 22:36:59 +0900

    fix: 출력 형식 수정으로 인한 테스트 실패 문제 해결

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,17 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
 import lotto.dto.LottoResultDto;
 import lotto.dto.LottoTicketsDto;
 import lotto.utils.RankMessages;
 
 public class OutputView {
 
-    private static final String PURCHASE_MESSAGE = "\n%d개를 구매했습니다.";
+    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_HEADER = "\n당첨 통계\n---";
-    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %s입니다.";
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
 
     public static void printLottoTickets(List<LottoTicketsDto> lottoTickets) {
@@ -19,23 +20,31 @@ public class OutputView {
     }
 
     public static void printLottoResults(LottoResultDto result) {
-        System.out.println("당첨 통계\n---");
+        System.out.println(WINNING_STATISTICS_HEADER);
         printRankResults(result);
         printProfitRate(result.getProfitRate());
     }
 
     private static void printRankResults(LottoResultDto result) {
         result.getRankCounts().forEach((rank, count) -> {
-            String message = RankMessages.getMessage(rank.getMatchCount(), rank.isMatchBonus());
-            System.out.printf("%s (%d원) - %d개\n", message, rank.getPrize(), count);
+            if (rank.getPrize() > 0) {
+                String message = RankMessages.getMessage(rank.getMatchCount(), rank.isMatchBonus());
+                String formattedPrize = formatPrize(rank.getPrize());
+                System.out.printf("%s (%s원) - %d개\n", message, formattedPrize, count);
+            }
         });
     }
 
     private static void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+        System.out.printf(PROFIT_RATE_MESSAGE+"\n", profitRate);
+    }
+
+    private static String formatPrize(long prize) {
+        return String.format("%,d", prize);
     }
 
     public static void printErrorMessage(String errorMessage) {
         System.out.printf(ERROR_MESSAGE_FORMAT, errorMessage);
     }
+
 }
