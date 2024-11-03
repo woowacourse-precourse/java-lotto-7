dc77825a07bef7e9f411f6ecfb76a503dd2c5382
dltmddus <tmddus4671@gmail.com>
2024-11-03 21:07:19 +0900

    refactor: 로또 관련 상수들 util 클래스 생성

---
+++ src/main/java/lotto/util/LottoConstants.java
@@ -0,0 +1,17 @@
+package lotto.util;
+
+public enum LottoConstants {
+    MIN_LOTTO_NUMBER(1),
+    MAX_LOTTO_NUMBER(45),
+    LOTTO_LENGTH(6),
+    LOTTO_PRICE(1000);
+    private final int value;
+
+    LottoConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
