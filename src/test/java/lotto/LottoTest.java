63087e1cb73f840719285d898fcf7dd5e2c7acfd
hongkikii <hyangmeeh@icloud.com>
2024-11-03 19:30:38 +0900

    test(LottoTest): 테스트 수정 및 추가
    리드미 기능 목록에 맞춰 테스트 시나리오 수정 및 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,61 +9,69 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-    @DisplayName("로또 번호가 null인 경우 예외가 발생한다.")
-    @Test
-    void 로또_번호가_null인_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
 
     @DisplayName("1장의 로또는 6개의 번호를 가진다.")
     @Test
-    void 한장의_로또는_6개의_번호를_가진다() {
+    void 한장의_로또는_여섯개의_번호를_가진다() {
         assertDoesNotThrow(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)));
     }
 
-    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
-    @Test
-    void 로또_번호의_개수가_6개_미만이면_예외가_발생한다() {
+    @DisplayName("로또 번호가 6개 미만의 개수일 경우 예외가 발생한다.")
+    void 로또_번호가_여섯개_미만의_개수일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
 
-    @DisplayName("로또 번호는 1에서 45사이의 숫자이다.")
+    @DisplayName("로또 번호가 6개 초과의 개수일 경우 예외가 발생한다.")
+    void 로또_번호가_여섯개_초과의_개수일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("로또 번호의 범위는 1~45이다.")
     @Test
-    void 로또_번호는_1에서_45사이의_숫자이다() {
+    void 로또_번호의_범위는_일에서_사십오_사이이다() {
         assertDoesNotThrow(() -> new Lotto(List.of(1, 5, 15, 25, 35, 45)));
     }
 
-    @DisplayName("로또 번호가 1에서 45 사이가 아닌 경우 예외가 발생한다.")
+    @DisplayName("로또 번호가 1 미만일 경우 예외가 발생한다.")
     @Test
-    void 로또_번호가_1에서_45_사이가_아닌_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("[ERROR]");
-
+    void 로또_번호가_일_미만일_경우_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
 
         assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("로또 번호가 45를 초과할 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_사십오를_초과할_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있는 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("로또 번호가 null일 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_null일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
