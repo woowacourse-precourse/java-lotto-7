b9105b9f5e1dd60cd75a7bae5eab89ab0bcf1360
TaegeunYou <dbxorms2310@naver.com>
2024-11-03 13:59:39 +0900

    refactor(AmountTest): 테스트 @DisplayName 어노테이션 추가

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -1,3 +1,5 @@
+package lotto;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.view.domain.Amount;
@@ -5,13 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AmountTest {
+    @DisplayName("금액이 1000으로 나누어떨어지지 않으면 예외가 발생한다.")
     @Test
     void 금액이_1000으로_나누어떨어지지_않으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Amount(1001))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("금액이 음수이면 예외가 발생한다.")
     @Test
     void 금액이_음수이면_예외가_발생한다() {
         assertThatThrownBy(() -> new Amount(-1000))
