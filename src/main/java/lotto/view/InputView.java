cd71596701524bdf12be63b787426b996846bdde
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 00:27:38 +0900

    feat(InputView에): 당첨 로또 번호 입력 메서드 추가
    - `InputView` 클래스에 당첨 로또 번호를 읽는 `readWinnerLottoNumbers` 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,11 +2,17 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.dto.LottoPurchasedAmountInput;
+import lotto.dto.WinnerLottoNumbersInput;
 
 public class InputView {
 
     public LottoPurchasedAmountInput readLottoPurchasedAmount() {
         String RawAmount = Console.readLine();
         return LottoPurchasedAmountInput.from(RawAmount.trim());
+    }
+
+    public WinnerLottoNumbersInput readWinnerLottoNumbers() {
+        String rawNumbers = Console.readLine();
+        return WinnerLottoNumbersInput.from(rawNumbers.trim());
     }
 }
