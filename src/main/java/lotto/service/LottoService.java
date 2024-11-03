ded3dd0b341de77364e166a63c323334152e55f9
skhnpark <skhnpark125@gmail.com>
2024-11-03 13:28:34 +0900

    feat: 로또 번호를 오름차순으로 정렬하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,13 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoService {
     public static List<Integer> createLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumbers =  Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
 }
