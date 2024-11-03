670de6eec4e380f104665f2c47463f527e145d05
kssk3 <kss8014@daum.net>
2024-11-03 20:52:01 +0900

    Fear Util 패키지

---
+++ src/main/java/lotto/utils/RandomNumbersGenerator.java
@@ -0,0 +1,17 @@
+package lotto.utils;
+
+import java.util.Set;
+import java.util.TreeSet;
+import lotto.domain.LottoNumber;
+
+public class RandomNumbersGenerator implements LottoNumbersGenerator {
+
+    @Override
+    public Set<LottoNumber> generate(int size) {
+        Set<LottoNumber> lottoNumbers = new TreeSet<>();
+        while (lottoNumbers.size() != size) {
+            lottoNumbers.add(RandomNumberGenerator.generate());
+        }
+        return lottoNumbers;
+    }
+}
