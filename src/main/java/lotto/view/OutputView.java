a15b615bf80321d59d51b04aff2c0f669b8509c6
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 23:15:13 +0900

    refactor(outputView): 줄바꿈 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,22 +9,22 @@ public class OutputView {
     }
 
     public void purchaseHistory(int quantity, LottoTicket lottoTicket) {
-        System.out.println(OutputMessage.PURCHASED_AMOUNT.getFormattedMessage(quantity));
+        System.out.println("\n" + OutputMessage.PURCHASED_AMOUNT.getFormattedMessage(quantity));
         for (Lotto lotto : lottoTicket.getLottoTicket()) {
             System.out.println(lotto.getNumbers());
         }
     }
 
     public void inputWinningNumbers() {
-        System.out.println(OutputMessage.INPUT_WINNING_NUMBERS.getMessage());
+        System.out.println("\n" + OutputMessage.INPUT_WINNING_NUMBERS.getMessage());
     }
 
     public void inputBonusNumber() {
-        System.out.println(OutputMessage.INPUT_BONUS_NUMBER.getMessage());
+        System.out.println("\n" + OutputMessage.INPUT_BONUS_NUMBER.getMessage());
     }
 
     public void winningStatistics(Integer[] winningCount) {
-        System.out.println(OutputMessage.WINNING_STATISTICS.getFormattedMessage((Object[]) winningCount));
+        System.out.println("\n" + OutputMessage.WINNING_STATISTICS.getFormattedMessage((Object[]) winningCount));
     }
 
     public void rateOfReturn(double rateOfReturn) {
