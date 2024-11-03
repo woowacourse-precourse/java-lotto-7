42b1e3f23459029869d5f603426b3c5a487458e3
chanwon <clw8679@naver.com>
2024-11-03 20:11:53 +0900

    feat: Ticket 구현

---
+++ src/main/java/lotto/domain/Ticket.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Ticket {
+    private final List<Lotto> lottos;
+    private final int price;
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public Ticket(List<Lotto> lottos, int price, List<Integer> winningNumbers, int bonusNumber) {
+        this.lottos = lottos;
+        this.price = price;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+        validatePrice(price);
+        validateDuplication(winningNumbers);
+        validateBonusNumber(bonusNumber);
+    }
+
+    private void validatePrice(int price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입은 1,000원 단위로 구입해야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+}
