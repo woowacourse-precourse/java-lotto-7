0db58a27cca4ad356dde5961513c87efaa49f7d9
JS <zks145@naver.com>
2024-11-03 14:41:57 +0900

    feat : 로또 당첨 번호 및 보너스 번호 매칭 개수 카운트 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,8 +2,13 @@ package lotto.service;
 
 import lotto.domain.Consumer;
 import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
+import lotto.domain.LottoRank;
 import lotto.util.Utils;
+import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LottoService {
@@ -11,6 +16,8 @@ public class LottoService {
     private final static Integer LOTTO_START_NUMBER = 1;
     private final static Integer LOTTO_END_NUMBER = 45;
     private final static Integer LOTTO_MAX_LENGTH = 6;
+    private final static Integer MATCH_ZERO = 0;
+    private final static Integer MATCH_NUMBER = 1;
 
     public void buyLottoes(Consumer consumer, int numberLimit) {
         int count = consumer.getMoney() / numberLimit;
@@ -28,5 +35,44 @@ public class LottoService {
         return new Lotto(lottoNumbers);
     }
 
+    public void calculationLottoRank(Consumer consumer, LottoPrize lottoPrize) {
+        ArrayList<Lotto> lottoes = consumer.getLottoes();
+        List<Integer> lottoPrizeNumbers = lottoPrize.getLottoPrizeNumbers();
+        Integer bonusNumber = lottoPrize.getBonusNumber();
+
+        findMatchingNumbers(consumer, lottoes, lottoPrizeNumbers, bonusNumber);
+    }
+
+    public void findMatchingNumbers(Consumer consumer, ArrayList<Lotto> lottoes, List<Integer> lottoPrizeNumbers, Integer bonusNumber) {
+        int matchNumbers;
+        boolean matchBonusNumber;
+
+        for (Lotto lotto : lottoes) {
+
+            matchNumbers = MATCH_ZERO;
+
+            matchNumbers = countMatchNumbers(matchNumbers, lotto.getNumbers(), lottoPrizeNumbers);
+            matchBonusNumber = countBonusMatches(lotto.getNumbers(), bonusNumber);
+
+            if (matchBonusNumber) {
+                matchNumbers += MATCH_NUMBER;
+            }
+
+            consumer.updateLottoRank(matchNumbers, matchBonusNumber);
+        }
+    }
+
+    public int countMatchNumbers(int matchNumbers, List<Integer> lottoNumbers, List<Integer> prizeNumbers) {
+        for (Integer number : lottoNumbers) {
+            if (prizeNumbers.contains(number)) {
+                matchNumbers += MATCH_NUMBER;
+            }
+        }
+        return matchNumbers;
+    }
+
+    public boolean countBonusMatches(List<Integer> lottoNumbers, int bonusNumber) {
+        return lottoNumbers.contains(bonusNumber);
+    }
 
 }
