290115b8fbbd006d744c0ae67365a39924841969
변우영 <likevangogh24@gmail.com>
2024-11-03 13:46:28 +0900

    test(LottoTicket): 당첨 결과 개수 확인 테스트 추가

---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import lotto.dto.MatchResult;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LottoTicketTest {
+    LottoNumberGenerator lottoNumberGenerator;
+    LottoTicket lottoTicket;
+
+    @BeforeEach
+    void init() {
+        lottoNumberGenerator = new LottoNumberGenerator();
+        PurchaseAmount purchaseAmount = PurchaseAmount.from("5000");
+        lottoTicket = LottoTicket.of(lottoNumberGenerator, purchaseAmount);
+    }
+
+    @DisplayName("5000원어치 로또 구매 시, 당첨 결과가 5개인지 확인한다.")
+    @Test
+    void matchResultCountTest() {
+        WinningNumber winningNumber = WinningNumber.from("1,2,3,4,5,7");
+        BonusNumber bonusNumber = BonusNumber.from("7");
+
+        List<MatchResult> matchResults = lottoTicket.gatherMatchResult(winningNumber, bonusNumber);
+
+        Assertions.assertThat(matchResults.size()).isEqualTo(5);
+    }
+}
