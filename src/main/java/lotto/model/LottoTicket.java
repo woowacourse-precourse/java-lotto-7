35d97f4e8c8462de810a788ccaf12c5d0425efb4
KNU-K <knukang334@gmail.com>
2024-11-03 16:16:13 +0900

    feat(LottoTicket): LottoTicket 도메인 구성
    Lotto와 Money로 구성된 도메인이다.
    - 생성시 Lotto가 null이면 안된다.
    - 생성시 Money가 null이면 안된다.

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+import lotto.exception.ErrorMessages;
+import lotto.exception.LottoException;
+
+public record LottoTicket(Lotto lotto, Money price) {
+    public LottoTicket {
+      validate(lotto, price);
+    }
+    private void validate(Lotto lotto, Money price){
+        if (lotto == null) {
+            throw new LottoException(ErrorMessages.LOTTO_NULL);
+        }
+        if (price == null) {
+            throw new LottoException(ErrorMessages.PRICE_NULL);
+        }
+    }
+    @Override
+    public String toString() {
+        return lotto.toString();
+    }
+}
