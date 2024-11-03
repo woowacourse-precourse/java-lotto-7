e89ba487ec8f4bf8b7612375748dbe715c8462be
JeongSH1 <jyt001532@gmail.com>
2024-11-03 19:04:30 +0900

    feat(WinningInfo): 로또 번호 일치 개수와 상금에 대한 정보를 저장한다
    WinningInfo에 대한 구현 사항:
    - 일치하는 번호의 개수와 상금에 대한 값이 저장
    - 일치하는 번호의 개수를 입력하면 WinningInfo 상수 정보 반환

--- src/main/java/lotto/constant/ExceptionMessageConstant.java
+++ src/main/java/lotto/constant/ExceptionMessageConstant.java
@@ -3,6 +3,7 @@ package lotto.constant;
 public class ExceptionMessageConstant {
 
     private static final String PREFACE = "[ERROR] ";
+
     public static final String INVALID_LOTTO_PURCHASE_AMOUNT_FORMAT = PREFACE + "로또 구입 금액은 숫자입니다.";
     public static final String INVALID_LOTTO_NUMBER_FORMAT = PREFACE + "로또 번호는 숫자입니다.";
     public static final String INVALID_LOTTO_PURCHASE_AMOUNT_UNIT =
@@ -11,5 +12,7 @@ public class ExceptionMessageConstant {
             PREFACE + "로또 당첨 번호의 개수는 " + LottoNumberConstant.REQUIRED_COUNT + "개 입니다.";
     public static final String INVALID_LOTTO_NUMBER_RANGE =
             PREFACE + "로또 번호는 " + LottoNumberConstant.MIN_NUMBER + "~" + LottoNumberConstant.MAX_NUMBER + "까지 입니다.";
+
+    public static final String UNKNOWN_RANK = PREFACE + "일치하는 순위를 찾을 수 없습니다.";
 
 }
