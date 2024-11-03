40193d97a6b9b09981bdc66f39a1352617637645
yybmion <yyb400@naver.com>
2024-11-03 19:30:32 +0900

    feat: 보너스 번호 기능 추가(범위 검사)

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -1,9 +1,13 @@
 package lotto;
 
 public class BonusNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final int number;
 
     public BonusNumber(int number) {
+        validate(number);
         this.number = number;
     }
 
@@ -14,5 +18,15 @@ public class BonusNumber {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
+    }
+
+    private void validate(int number) {
+        if (isInvalidRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private boolean isInvalidRange(int number) {
+        return number < MIN_NUMBER || number > MAX_NUMBER;
     }
 }
