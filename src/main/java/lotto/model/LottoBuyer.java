c4f209f2436640873cd66bf20fe3e58c55ef5dfe
KNU-K <knukang334@gmail.com>
2024-11-03 16:28:50 +0900

    feat(LottoBuyer): 로또 티켓 구매 기능 추가
    - 사용자가 로또 머신을 통해 로또 티켓을 구매하는 buyLottoTickets() 메서드 구현
    - 입력 금액에 따라 구매 가능한 티켓 수를 계산하여 generateLottoTickets() 메서드로 티켓 생성
    - money가 null일 경우에 대한 유효성 검사 추가
    
    이번 업데이트로 사용자가 지정한 금액에 따라 로또 티켓을 자동으로 생성할 수 있는 기능을 추가하여 로또 구매 과정을 간소화했습니다.

---
+++ src/main/java/lotto/model/LottoBuyer.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.exception.ErrorMessages;
+import lotto.exception.LottoException;
+import lotto.factory.LottoMachine;
+
+public class LottoBuyer {
+
+    public LottoTickets buyLottoTickets(LottoMachine lottoMachine, Money money) {
+        if (money == null) {
+            throw new LottoException(ErrorMessages.MONEY_NULL);
+        }
+
+        int buyCount = money.divide(LottoMachine.PRICE_PER_ONE);
+        return generateLottoTickets(lottoMachine, buyCount);
+    }
+
+    private LottoTickets generateLottoTickets(LottoMachine lottoMachine, int ticketCount) {
+        List<LottoTicket> tickets = IntStream.range(0, ticketCount)
+                .mapToObj(i -> lottoMachine.generateLottoTicket())
+                .toList();
+        return new LottoTickets(tickets);
+    }
+}
