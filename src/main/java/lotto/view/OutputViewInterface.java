9177f2417641befc0382ff53b1ce9df19af4f415
jungsu <vwx1@naver.com>
2024-11-03 19:12:32 +0900

    feat: OutputViewInterface 설계

--- src/main/java/lotto/view/OutputViewInterface.java
+++ src/main/java/lotto/view/OutputViewInterface.java
@@ -1,5 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public interface OutputViewInterface {
 
+  void printPurchaseCount(int count);
+
+  void printGeneratedLottos(List<Lotto> lottos);
+
+  void printWinningResults(List<Integer> result);
+
+  void printYield(double yield);
 }
