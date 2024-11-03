16ae01de400a2ab31b7ac9e8ead7002e2d7f258f
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-11-03 14:30:39 +0900

    feat(Result): 로또 결과 저장 enum 클래스 구현
    - 결과 출력 여부, 출력 메시지, 매칭 결과, 매칭 로직 구현

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import lotto.message.InfoMessage;
+
+import java.util.function.BiPredicate;
+
+import static lotto.message.InfoMessage.*;
+
+public enum Result {
+
+    ZERO(false, null, 0,
+            (match, bonus) -> match == 0),
+
+    ONE(false, null, 0,
+            (match, bonus) -> match == 1),
+
+    TWO(false, null, 0,
+            (match, bonus) -> match == 2),
+
+    THREE(true, THREE_NUMBERS_MATCH, 0,
+            (match, bonus) -> match == 3),
+
+    FOUR(true, FOUR_NUMBERS_MATCH, 0,
+            (match, bonus) -> match == 4),
+
+    FIVE(true, FIVE_NUMBERS_MATCH, 0,
+            (match, bonus) -> match == 5 && !bonus),
+
+    FIVE_WITH_BONUS(true, FIVE_NUMBERS_MATCH_WITH_BONUS, 0,
+            (match, bonus) -> match == 5 && bonus),
+
+    SIX(true, FIVE_NUMBERS_MATCH_WITH_BONUS, 0,
+            (match, bonus) -> match == 6);
+
+    private boolean print;
+    private InfoMessage infoMessage;
+    private int result;
+    private BiPredicate<Integer, Boolean> predicate;
+
+    Result(boolean print, InfoMessage infoMessage, int result, BiPredicate<Integer, Boolean> predicate) {
+        this.print = print;
+        this.infoMessage = infoMessage;
+        this.result = 0;
+        this.predicate = predicate;
+    }
+
+    private void increment() {
+        result++;
+    }
+
+}
