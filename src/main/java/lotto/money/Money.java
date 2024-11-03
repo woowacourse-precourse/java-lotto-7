3ab6380d38f12cab8605d09dece7b55c5e2f5c28
Jang <sdsd090811@gmail.com>
2024-11-03 15:13:16 +0900

    feat: 돈 역할에 따른 기능 추가
    돈 더하기 기능 추가
    돈 뭉치 세기 기능 추가
    비율 계산하기 기능 추가

--- src/main/java/lotto/money/Money.java
+++ src/main/java/lotto/money/Money.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Money {
     private static final int EMPTY_MONEY = 0;
+    public static final Money EMPTY = new Money(EMPTY_MONEY);
 
     public final int amount;
 
@@ -11,6 +12,20 @@ public class Money {
         if(isMinusMoney(amount))
             throw new IllegalArgumentException("돈은 음수가 불가능합니다.");
         this.amount = amount;
+    }
+
+    public Money plus(Money money) {
+        return new Money(amount + money.amount);
+    }
+
+    public int countBundle(Money bundleUnit) {
+        if(bundleUnit.equals(EMPTY) || amount % bundleUnit.amount != 0)
+            throw new IllegalArgumentException("묶을 수 없는 단위입니다.");
+        return amount / bundleUnit.amount;
+    }
+
+    public double rateAs(Money money) {
+        return (double) amount / money.amount;
     }
 
     @Override
--- src/test/java/lotto/money/MoneyTest.java
+++ src/test/java/lotto/money/MoneyTest.java
@@ -2,15 +2,53 @@ package lotto.money;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
+    private final Money ONE = new Money(1);
+    private Money TWO = new Money(2);
+    private final Money FOUR = new Money(4);
+    private final Money FIVE = new Money(5);
+    private final Money SIX = new Money(6);
+
     @DisplayName("돈은 마이너스가 불가능함")
     @Test
-    void test() {
+    void test1() {
         assertThrows(IllegalArgumentException.class, () -> new Money(-1));
+    }
+
+    @DisplayName("돈을 더할 수 있음")
+    @Test
+    void test2() {
+        assertEquals(ONE.plus(FOUR), FIVE);
+        assertEquals(ONE.plus(FIVE), SIX);
+    }
+
+    @DisplayName("0 단위로 돈뭉치를 세려고 하면 예외가 발생함")
+    @Test
+    void test3() {
+        assertThrows(IllegalArgumentException.class, () -> FIVE.countBundle(Money.EMPTY));
+    }
+
+    @DisplayName("나누어 떨어지지 단위로 돈뭉치를 세려고 하면 예외가 발생함")
+    @Test
+    void test4() {
+        assertThrows(IllegalArgumentException.class, () -> FIVE.countBundle(FOUR));
+    }
+
+    @DisplayName("특정 단위로 돈뭉치를 셀 수 있음")
+    @Test
+    void test5() {
+        assertEquals(FOUR.countBundle(ONE), 4);
+        assertEquals(FOUR.countBundle(new Money(2)), 2);
+    }
+
+    @DisplayName("돈의 비율을 구할 수 있음")
+    @Test
+    void test6() {
+        assertEquals(FOUR.rateAs(ONE), 4);
+        assertEquals(FIVE.rateAs(ONE), 5);
+        assertEquals(SIX.rateAs(TWO), 3);
     }
 }
