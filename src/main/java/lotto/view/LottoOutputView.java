fb2017ee558a9702fd90a22ff6208c86d301de5a
peanut990 <peanut1998@naver.com>
2024-11-04 00:18:59 +0900

    feat(view): 당첨 통계 출력 기능

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -2,12 +2,24 @@ package lotto.view;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
 public class LottoOutputView {
+    private static final int FOR_PRINT_FROM_SECOND = 2;
     public void printLottos(List<Lotto> lottos) {
-        System.out.println(lottos.size()+"개를 구매했습니다.");
+        System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printResult(int[] rankCounts, float rateOfRevenue) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (int i = rankCounts.length - FOR_PRINT_FROM_SECOND; i >= 0 ; i--) {
+            LottoRank lottoRank = LottoRank.values()[i];
+            System.out.printf("%d개 일치 (%,d원) - %d개\n",lottoRank.getWinningCount(),lottoRank.getPrize(),rankCounts[i]);
+        }
+        System.out.printf("총 수익률은 %f%%입니다.",rateOfRevenue);
     }
 }
