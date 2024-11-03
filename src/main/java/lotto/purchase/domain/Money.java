e08b23cba3b40c598bcbdce0893127792b704093
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-11-03 22:02:50 +0900

    feat(domain): domain에 rule을 도입하고 input value의 유효성 검사 로직을 구현한다
    domain 내부에서 input value의 유효성을 검사할 수 있는 로직을 구현하고 rule을 통해 조건을 관리한다. static factoty method를 통해 생성하며 유효성검사를 할 수 있도록 구현한다.

--- src/main/java/lotto/purchase/domain/Money.java
+++ src/main/java/lotto/purchase/domain/Money.java
@@ -1,8 +1,8 @@
 package lotto.purchase.domain;
 
-public class Money {
+import lotto.common.rule.Rule;
 
-    private static final int MINIMUM_MONEY_VALUE = 1000;
+public class Money {
 
     private final long moneyValue;
 
@@ -15,23 +15,23 @@ public class Money {
         return new Money(money);
     }
 
-    public long getQuantitiesCanBuy() {
-        return moneyValue / 1000;
-    }
-
-    public double calculateRateOfIncome(long amount) {
-        return ((double) amount / moneyValue) * 100;
-    }
-
     private static void validateMoney(long moneyValue) {
         if (moneyValue <= 0) {
             throw new IllegalArgumentException("[ERROR] 금액은 음수일 수 없습니다.");
         }
-        if (moneyValue < MINIMUM_MONEY_VALUE) {
+        if (moneyValue < Rule.MONEY_MINIMUM_VALUE) {
             throw new IllegalArgumentException("[ERROR] 금액은 1000원 미만일 수 없습니다.");
         }
-        if (moneyValue % MINIMUM_MONEY_VALUE != 0) {
-            throw new IllegalArgumentException("[ERROR] 금액은 1000월 단위로 나눠 떨어져야 합니다");
+        if (moneyValue % Rule.MONEY_MINIMUM_VALUE != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위여야 합니다");
         }
+    }
+
+    public long getQuantitiesCanBuy() {
+        return moneyValue / Rule.LOTTO_PRICE;
+    }
+
+    public double calculateRateOfIncome(long amount) {
+        return ((double) amount / moneyValue) * 100;
     }
 }
