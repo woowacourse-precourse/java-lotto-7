f351c099ee6fcc85935c344a63c3b498287501b0
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-03 17:26:22 +0900

    feat($ConsoleInputView): InputView 의 Console 구현체 추가

---
+++ src/main/java/lotto/view/input/ConsoleInputView.java
@@ -0,0 +1,49 @@
+package lotto.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.exception.InvalidInputException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsoleInputView implements InputView {
+
+    private static final String PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+
+    @Override
+    public Integer inputPurchaseMoney() {
+        System.out.println(PURCHASE_MONEY_MESSAGE);
+
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw InvalidInputException.invalidNumber();
+        }
+    }
+
+    @Override
+    public List<Integer> inputWinningNumbers() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
+
+        try {
+            return Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw InvalidInputException.invalidNumber();
+        }
+    }
+
+    @Override
+    public int inputBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw InvalidInputException.invalidNumber();
+        }
+    }
+}
