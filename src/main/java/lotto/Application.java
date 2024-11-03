4939a667581ed8bc7ca4e86d89a62a190830f724
HwangSgHn <dolphin33@jbnu.ac.kr>
2024-11-04 05:02:16 +0900

    feat: main 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,29 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        output.purchaseAmount();
+        input.purchaseAmount();
+        while (!InputValidator.purchaseAmountValidator(input.getPurchaseAmount())) {
+            input.purchaseAmount();
+        }
+        LottoGenerator lottoGenerator = new LottoGenerator(InputValidator.getPurchaseAmount() / 1000);
+        output.lottoCountAndNumbers(lottoGenerator);
+        output.winningNumbers();
+        input.winningNumber();
+        while (!InputValidator.winningNumberValidator(input.getWinningNumber())) {
+            input.winningNumber();
+        }
+        output.bonusNumber();
+        input.bonusNumber();
+        while (!InputValidator.bonusNumberValidator(input.getBonusNumber())) {
+            input.bonusNumber();
+        }
+        WinningCounter winningCounter = new WinningCounter();
+        for (Lotto lotto : lottoGenerator.getLottos()) {
+            winningCounter.addWinning(lotto.confirmWinning());
+        }
+        output.winningStatistics(winningCounter);
     }
 }
