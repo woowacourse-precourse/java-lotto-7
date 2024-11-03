6ca5b76c5a09d81e6ef73b63ad021f73c26c6b26
Gahyeon <best11gh@naver.com>
2024-11-03 15:56:58 +0900

    feat(InputView): InputView 클래스 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+
+
+    private static String getInput(String prompt) {
+        System.out.println(prompt);
+        try {
+            return Console.readLine().trim();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
+    }
+
+    public static String getPurchaseAmount() {
+        return getInput(PURCHASE_AMOUNT_PROMPT);
+    }
+
+    public static String getWinningNumberInput() {
+        return getInput(WINNING_NUMBER_PROMPT);
+    }
+
+    public static String getBonusNumberInput() {
+        return getInput(BONUS_NUMBER_PROMPT);
+    }
+}
