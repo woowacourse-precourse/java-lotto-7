54029b6adf7a7c9db8bc7d806e2f9b0377aab8f9
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-03 23:15:02 +0900

    feat: 여러 클래스에서 공유되어야 하는 데이터를 싱글톤으로 저장하는 클래스 생성

---
+++ src/main/java/lotto/data/Database.java
@@ -0,0 +1,10 @@
+package lotto.data;
+
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static List<Lotto> purchaseLottoList = new ArrayList<>();
+}
