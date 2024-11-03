e2f8585fbae0e7c9ea59946273c1c83ada4a72d7
Jewan1120 <hawaii1468@gmail.com>
2024-11-03 14:26:04 +0900

    feat(Validator): 로또 당첨 번호 입력의 유효성 검증 로직 추가

--- src/main/java/lotto/enums/ErrorMessages.java
+++ src/main/java/lotto/enums/ErrorMessages.java
@@ -6,10 +6,12 @@ public enum ErrorMessages {
 
     INPUT_NOTHING("입력 문자열은 null이거나 비어있어서는 안 됩니다."),
     MONEY_UNIT("구입 금액의 최소 단위는 {0}원입니다."),
-    NUMBER_FORMAT("입력은 숫자만 가능합니다.")
+    NUMBER_FORMAT("입력은 숫자만 가능합니다."),
 
+    LOTTO_SIZE("로또 숫자의 개수는 {0}개이어야 합니다."),
+    LOTTO_BOUND("로또 숫자의 범위는 {0}부터 {1}까지입니다."),
+    LOTTO_UNIQUE_NUMBER("로또 숫자에 중복된 숫자가 존재합니다.")
     ;
-
     private final String message;
 
     ErrorMessages(String message) {
--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,9 +1,13 @@
 package lotto.validator;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.enums.Constants;
 import lotto.enums.ErrorMessages;
 
 public class LottoValidator {
+
+    static final String DELIMITER = ",";
 
     public static void validateInputMoney(String inputMoney) {
         isEmpty(inputMoney);
@@ -14,15 +18,48 @@ public class LottoValidator {
         }
     }
 
+    public static void validateWinningNumbers(String inputWinningNumbers) {
+        isEmpty(inputWinningNumbers);
+        List<String> winningNumbers = Arrays.stream(inputWinningNumbers.split(DELIMITER)).toList();
+        isLottoSize(winningNumbers);
+        isUnique(winningNumbers);
+        for (String winningNumber : winningNumbers) {
+            isDigit(winningNumber);
+            isInBound(winningNumber);
+        }
+    }
+
     private static void isEmpty(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessages.INPUT_NOTHING.getMessage());
         }
     }
 
-    private static void isDigit(String inputMoney) {
-        if (!inputMoney.chars().allMatch(Character::isDigit)) {
+    private static void isDigit(String inputString) {
+        if (!inputString.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException(ErrorMessages.NUMBER_FORMAT.getMessage());
+        }
+    }
+
+    private static void isInBound(String inputNumber) {
+        int number = Integer.parseInt(inputNumber);
+        if (number < Constants.LOTTO_LOWER_BOUND.getValue() || number > Constants.LOTTO_UPPER_BOUND.getValue()) {
+            throw new IllegalArgumentException(
+                    ErrorMessages.LOTTO_BOUND.getMessage(Constants.LOTTO_LOWER_BOUND.getValue(),
+                            Constants.LOTTO_UPPER_BOUND.getValue()));
+        }
+    }
+
+    private static void isLottoSize(List<String> WinningNumbers) {
+        if (WinningNumbers.size() != Constants.LOTTO_NUMBER_SIZE.getValue()) {
+            throw new IllegalArgumentException(
+                    ErrorMessages.LOTTO_SIZE.getMessage(Constants.LOTTO_NUMBER_SIZE.getValue()));
+        }
+    }
+
+    private static void isUnique(List<String> winningNumbers) {
+        if (winningNumbers.stream().distinct().count() != Constants.LOTTO_NUMBER_SIZE.getValue()) {
+            throw new IllegalArgumentException(ErrorMessages.LOTTO_UNIQUE_NUMBER.getMessage());
         }
     }
 }
