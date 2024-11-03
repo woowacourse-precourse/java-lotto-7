c39a47f9e9289b4d3f5154480e07632bb9b1f382
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-03 20:00:06 +0900

    feat(ErrorMessage): 예외 처리에 사용될 열거형 타입의 에러 메시지 추가

--- src/main/java/lotto/Util/Error/ErrorMessage.java
+++ src/main/java/lotto/Util/Error/ErrorMessage.java
@@ -1,4 +1,25 @@
 package lotto.Util.Error;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+    INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    INVALID_WINNING_NUMBER_COUNT("[ERROR] 로또의 당첨 번호는 6개 입니다."),
+    INVALID_BONUS_NUMBER_COUNT("[ERROR] 로또의 보너스 번호는 1개 입니다."),
+    INVALID_PURCHASE_UNIT("[ERROR] 로또 구입 금액은 1,000원 단위 정수로 입력해야 합니다."),
+    PURCHASE_LIMIT_REACHED("[ERROR] 로또의 구매 한도에 도달했습니다."),
+    MINIMUM_PURCHASE_AMOUNT("[ERROR] 로또의 최소 구입 금액은 1,000원 입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String format(Object... args) {
+        return String.format(message, args);
+    }
 }
