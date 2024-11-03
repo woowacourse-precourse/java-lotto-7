14e87fac96f1b1076ac134d27f6bf4fa15ca65c4
lvalentine6 <ysr1227@naver.com>
2024-11-03 22:39:09 +0900

    refactor: split enum according to domain logic

--- src/main/java/lotto/validator/LottoValidationMessage.java
+++ src/main/java/lotto/validator/LottoValidationMessage.java
@@ -1,16 +1,9 @@
 package lotto.validator;
 
 public enum LottoValidationMessage {
-	INVALID_PURCHASE_AMOUNT_FORMAT("[ERROR] 구매 금액은 숫자여야 합니다."),
-	INVALID_PURCHASE_AMOUNT_OVER_FLOW("[ERROR] 구매 금액의 최대 범위를 초과했습니다."),
-	INVALID_PURCHASE_AMOUNT_NEGATIVE("[ERROR] 구매 금액은 양수여야 합니다."),
-	INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구매 금액은 1000원 단위여야 합니다."),
-	MISSING_WINNING_NUMBERS_DELIMITER("[ERROR] 당첨 번호에 쉼표(,)가 없습니다."),
-	INVALID_WINNING_NUMBERS_FORMAT("[ERROR] 당첨 번호는 숫자여야 합니다."),
-	INVALID_WINNING_NUMBERS_OVER_FLOW("[ERROR] 당첨 번호의 최대 범위를 초과했습니다."),
-	INVALID_BONUS_NUMBER_FORMAT("[ERROR] 보너스 번호는 숫자여야 합니다."),
-	INVALID_BONUS_NUMBER_NEGATIVE("[ERROR] 보너스 번호는 양수여야 합니다."),
-	INVALID_BONUS_NUMBER_OVER_FLOW("[ERROR] 보너스 번호의 최대 범위를 초과했습니다."),
+	INVALID_LOTTO_NUMBERS_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+	INVALID_LOTTO_NUMBERS_RANGE("[ERROR] 로또 번호는 1~45 사이여야 합니다."),
+	INVALID_LOTTO_NUMBERS_DUPLICATION("[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다."),
 	;
 
 	private final String message;
