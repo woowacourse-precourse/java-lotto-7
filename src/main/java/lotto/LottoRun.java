752721ba98fcb10e1d7ac98cefbcf517697b0d28
sunjong0214 <rlatjswhd8200@gmail.com>
2024-11-03 16:25:51 +0900

    feat: 입력 에러 시 다시 입력 받는 기능 추가

--- src/main/java/lotto/LottoRun.java
+++ src/main/java/lotto/LottoRun.java
@@ -7,10 +7,31 @@ import lotto.view.LottoView;
 
 public class LottoRun {
     public static void start() {
-        LottoController lottoController = configure();
-        lottoController.setAndPrintLottos();
-        lottoController.inputAndConfirmWinningNumber();
-        lottoController.printAndCalculateWinningRate();
+        LottoController lottoController = null;
+        try {
+            lottoController = configure();
+        } catch (IllegalAccessError e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            lottoController = configure();
+        }
+        try {
+            lottoController.setAndPrintLottos();
+        } catch (IllegalAccessError e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            lottoController.setAndPrintLottos();
+        }
+        try {
+            lottoController.inputAndConfirmWinningNumber();
+        } catch (IllegalAccessError e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            lottoController.inputAndConfirmWinningNumber();
+        }
+        try {
+            lottoController.printAndCalculateWinningRate();
+        } catch (IllegalAccessError e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            lottoController.printAndCalculateWinningRate();
+        }
     }
 
     private static LottoController configure() {
