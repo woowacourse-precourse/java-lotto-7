07944af2a1ad97e8d8fd63bf1d8d65123f389c6b
장혜민 <gpals5987@gmail.com>
2024-11-03 16:03:28 +0900

    [refactor] MessageConstants -> GameMessage 변경

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
-public class MessageConstants {
+public class GameMessage {
     // Input Messages
     public static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String PURCHASED_LOTTO_COUNT = "%d개를 구매했습니다.";
