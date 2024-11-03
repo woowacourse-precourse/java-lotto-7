bb35d8262c3baa38bdb632a8a82a29f150985fbb
youngseo3 <wys4475@gmail.com>
2024-11-04 03:08:02 +0900

    test(InputValidatorTest): 당첨 번호 검증 테스트 작성

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -45,4 +45,35 @@ class InputValidatorTest {
         }
     }
 
+    @Nested
+    class 당첨_번호_검증 {
+        @Test
+        void 올바른_형식의_당첨_번호가_입력되면_성공한다() {
+            assertThatCode(() -> inputValidator.validateWinningNumber("1,2,3,4,5,6"))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 중복된_번호가_있으면_예외가_발생한다() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber("1,1,2,3,4,5"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 당첨 번호가 중복되지 않게 입력해주세요.");
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1, 2,3,4,5,6", "1,2 ,3,4,5,6", "1,2,3, 4,5,6"})
+        void 공백이_포함된_번호가_있으면_예외가_발생한다(String numbers) {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber(numbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 오직 ,로만 당첨 번호를 구분해서 입력하시고 공백을 포함하지 말아주세요.");
+        }
+
+        @Test
+        void 숫자가_아닌_문자가_포함되어_있으면_예외가_발생한다() {
+            assertThatThrownBy(() -> inputValidator.validateWinningNumber("1,2,a,4,5,6"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("[ERROR] 문자가 아닌 숫자를 입력해주세요.");
+        }
+    }
+
 }
