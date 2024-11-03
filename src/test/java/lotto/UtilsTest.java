e68f2d877131efa666146e144dccaae223dea22e
dbdb456 <adad568@naver.com>
2024-11-03 21:32:15 +0900

    test(utils): 로또 번호 5개 맞으면 Rank.THREE 카운트 증가 테스트 추가

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -80,4 +80,19 @@ public class UtilsTest {
 
         assertThat(resultCounts.get(Rank.TWO)).isEqualTo(1);
     }
+
+    @DisplayName("로또 번호 5개 맞으면 Map에 Rank.THREE부분이 +1이 된다.")
+    @Test
+    void 로또번호_5개일치_보너스번호_일치_Map_THREE갯수추가_로직() {
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumber = new Lotto(numbers);
+        int bonusNumber = 7;
+        List<Integer> myNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 8));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(myNumbers));
+
+        Map<Rank, Integer> resultCounts = utils.evaluateLottoRanks(winningNumber, bonusNumber, lottos);
+
+        assertThat(resultCounts.get(Rank.THREE)).isEqualTo(1);
+    }
 }
