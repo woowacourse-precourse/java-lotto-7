8ef8768854a679c4c257d5dc4f1b80af63eae6ee
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-04 00:52:11 +0900

    test: 구매 가격 테스트 코드 이름 수정

--- src/test/java/lotto/domain/PurchasePriceTest.java
+++ src/test/java/lotto/domain/PurchasePriceTest.java
@@ -1,14 +1,13 @@
-package lotto.validation;
+package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import lotto.domain.PurchasePrice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PurchasePriceValidatorTest {
+class PurchasePriceTest {
     @DisplayName("구입 금액이 자연수가 아닌 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"0", "-1000", "abc"})
