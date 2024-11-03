921d1d3d7a67400302ef5157c670ef73d01f1138
이희수 <hslee9234@naver.com>
2024-11-04 06:04:29 +0900

    refacotr: LottoResultCalculator
    DTO를 이용하여 데이터를 주고받도록 리팩토링

--- src/main/java/lotto/domain/LottoResultCalculator.java
+++ src/main/java/lotto/domain/LottoResultCalculator.java
@@ -2,28 +2,63 @@ package lotto.domain;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
-import lotto.dto.LottoWinningNumbers;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lotto.dto.LottoResult;
+import lotto.dto.RankCount;
+import lotto.dto.RankCounts;
+import lotto.dto.WinningLottoNumbers;
+import lotto.util.enums.Rank;
 
 public class LottoResultCalculator {
 
-    public BigDecimal calculateProfitRate(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers,
-                                          int purchaseAmount) {
-        long totalPrize = calculateTotalPrize(lottos, lottoWinningNumbers);
+    public LottoResult calculateResult(Lottos lottos, WinningLottoNumbers winningLottoNumbers, int purchaseAmount) {
+        Map<Rank, Integer> rankCountMap = initializeRankCountMap();
+        List<Integer> winningNumbers = winningLottoNumbers.getWinningNumbers();
+        int bonusNumber = winningLottoNumbers.getBonusNumber();
+        updateRankCountMap(rankCountMap, lottos, winningNumbers, bonusNumber);
 
-        BigDecimal profitRate = new BigDecimal(totalPrize)
-                .divide(new BigDecimal(purchaseAmount), 10, RoundingMode.HALF_UP)
-                .multiply(new BigDecimal(100));
+        RankCounts rankCounts = convertToRankCounts(rankCountMap, purchaseAmount);
 
-        return profitRate.setScale(1, RoundingMode.HALF_UP);
+        return new LottoResult(rankCounts, purchaseAmount);
     }
 
-    private long calculateTotalPrize(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers) {
-        return lottos.stream()
-                .mapToLong(lotto -> lotto.calculateRank(
-                        lottoWinningNumbers.getWinningNumbers(),
-                        lottoWinningNumbers.getBonusNumber()
-                ).getPrize())
-                .sum();
+    private Map<Rank, Integer> initializeRankCountMap() {
+        return Arrays.stream(Rank.values())
+                .collect(Collectors.toMap(rank -> rank, rank -> 0, (a, b) -> b, () -> new EnumMap<>(Rank.class)));
+    }
+
+    private void updateRankCountMap(Map<Rank, Integer> rankCountMap, Lottos lottos, List<Integer> winningNumbers,
+                                    int bonusNumber) {
+        lottos.getLottos().stream()
+                .map(lotto -> lotto.calculateRank(winningNumbers, bonusNumber))
+                .forEach(rank -> rankCountMap.put(rank, rankCountMap.get(rank) + 1));
+    }
+
+    private RankCounts convertToRankCounts(Map<Rank, Integer> rankCountMap, int purchaseAmount) {
+        List<RankCount> rankCountList = rankCountMap.entrySet().stream()
+                .map(entry -> new RankCount(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+        return new RankCounts(rankCountList);
+    }
+
+    public BigDecimal calculateProfitRate(LottoResult lottoResult) {
+        BigDecimal totalPrize = calculateTotalPrize(lottoResult.getRankCounts());
+        BigDecimal purchaseAmount = BigDecimal.valueOf(lottoResult.getPurchaseAmount());
+
+        return totalPrize
+                .divide(purchaseAmount, 10, RoundingMode.HALF_UP)
+                .multiply(BigDecimal.valueOf(100))
+                .setScale(1, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateTotalPrize(RankCounts rankCounts) {
+        return rankCounts.getRankCounts().stream()
+                .map(rankCount -> BigDecimal.valueOf(rankCount.getRank().getPrize())
+                        .multiply(BigDecimal.valueOf(rankCount.getCount())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
