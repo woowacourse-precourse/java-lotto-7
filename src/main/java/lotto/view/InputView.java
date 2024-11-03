f4e37014f96c0b3cf481bc4db9577f301524f797
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 03:53:31 +0900

    feat: 당첨 번호 입력 받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class InputView {
 
@@ -12,30 +18,56 @@ public abstract class InputView {
     public static int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        checkIfPurchaseAmountValid(input);
+        checkIfPurchaseAmountIsValid(input);
         return Integer.parseInt(input);
     }
 
-    public static void checkIfPurchaseAmountValid(String input) {
+    public static Lotto readWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        checkIfWinningLottoIsValid(input);
+        return new Lotto(Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList());
+    }
+
+    private static void checkIfPurchaseAmountIsValid(String input) {
         isInputEmpty(input);
         isNumeric(input);
         int purchaseAmount = Integer.parseInt(input);
         isDividedByLottoPrice(purchaseAmount);
     }
 
-    public static void isInputEmpty(String input) {
+    private static void checkIfWinningLottoIsValid(String input) {
+        isInputEmpty(input);
+        isInputFormatValid(input);
+        Arrays.stream(input.split(","))
+                .forEach(value -> {
+                    isInputEmpty(value);
+                    isNumeric(value);
+                });
+    }
+
+    private static void isInputEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 값을 입력하세요.");
         }
     }
 
-    public static void isDividedByLottoPrice(int money) {
+    private static void isInputFormatValid(String input) {
+        int inputLength = input.length();
+        if (input.charAt(0) == ',' || input.charAt(inputLength - 1) == ',') {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 입력 형식입니다.");
+        }
+    }
+
+    private static void isDividedByLottoPrice(int money) {
         if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 로또 가격 단위로 이루어져야 합니다.");
         }
     }
 
-    public static void isNumeric(String input) {
+    private static void isNumeric(String input) {
         if (!input.matches("^[1-9][0-9]*$")) {
             throw new IllegalArgumentException("[ERROR] 잘못된 형식의 입력입니다.");
         }
