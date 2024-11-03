cd55d95d462e34dde15caca1a15b44a1753855e0
JS <zks145@naver.com>
2024-11-03 14:39:26 +0900

    feat : 로또 랭킹 기록 기능 추가

--- src/main/java/lotto/domain/Consumer.java
+++ src/main/java/lotto/domain/Consumer.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static lotto.validation.Validation.*;
 
@@ -12,6 +10,8 @@ public class Consumer {
     private final static int MAX_COST = 100000;
     private final static int MIN_COST = 1000;
     private final static int ZERO = 0;
+    private final static int ONE = 1;
+    private final static int MATCH_NUMBERS_FIVE = 5;
     private final ArrayList<Lotto> lottoes = new ArrayList<>();
     private final HashMap<LottoRank, Integer> lottoRankResult = new HashMap<>();
     private final int money;
@@ -46,6 +46,31 @@ public class Consumer {
         }
     }
 
+    public void updateLottoRank(int matchNumbers, boolean bonusMatch) {
+        for (LottoRank rank : LottoRank.values()) {
+            updateRanking(matchNumbers, bonusMatch, rank);
+        }
+    }
 
+    private void updateRanking(int matchNumbers, boolean bonusMatch, LottoRank rank) {
+
+        bonusInclude(matchNumbers, bonusMatch, rank);
+
+        if (rank.getCount() == matchNumbers) {
+            lottoRankResult.put(rank, lottoRankResult.get(rank) + ONE);
+        }
+    }
+
+    private void bonusInclude(int matchNumbers, boolean bonusMatch, LottoRank rank) {
+        if (matchNumbers == MATCH_NUMBERS_FIVE) {
+            if (rank.getCount() == matchNumbers && rank.getBonus() == bonusMatch) {
+                lottoRankResult.put(rank, lottoRankResult.get(rank) + ONE);
+            }
+        }
+    }
+
+    public HashMap<LottoRank, Integer> getLottoRankResult() {
+        return lottoRankResult;
+    }
 
 }
