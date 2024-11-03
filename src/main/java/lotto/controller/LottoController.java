db45d0cb450932dfefd6df2a16f88e38020cdb34
dgh06175 <dgh06175@gmail.com>
2024-11-03 15:05:39 +0900

    feat: 로또 발행 및 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
+import lotto.model.Lotto;
 import lotto.view.LottoInputView;
 import lotto.view.LottoOutputView;
 
@@ -22,11 +26,13 @@ public class LottoController {
             validateAmount(amount);
             return Integer.parseInt(amount);
         });
-        System.out.println(money);
         // 구입한 개수만큼 로또 발행
-
+        List<Lotto> lottos = issueLottos(money);
         // 발행한 로또 수량 및 번호 출력
-
+        System.out.printf("\n%d개를 구매했습니다.\n", lottos.size());
+        for (Lotto lotto : lottos) {
+            lotto.print();
+        }
         // 당첨 번호 입력 받기
 
         // 보너스 번호 입력 받기
@@ -56,5 +62,15 @@ public class LottoController {
         if (intAmount % 1000 != 0 || intAmount == 0) {
             throw new IllegalArgumentException("로또 금액은 1,000 단위로 입력해주세요.");
         }
+    }
+
+    private List<Lotto> issueLottos(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+        int lottoCount = money / 1000;
+        while (lottoCount-- > 0) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
