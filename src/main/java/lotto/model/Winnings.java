853deb30f2362bc2bb25f849dbc9755e7eacb73b
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-03 21:35:40 +0900

    feat: 상금,일치갯수를 가지는 Winnings enum 구현

---
+++ src/main/java/lotto/model/Winnings.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+public enum Winnings {
+    FIFTH(5000, 3),
+    FOURTH(50000, 4),
+    THIRD(1500000, 5),
+    SECOND(30000000, 5),
+    FIRST(2000000000,6);
+
+    private final double prize;
+    private final int matchCount;
+
+    Winnings(double prize, int matchCount) {
+        this.prize = prize;
+        this.matchCount = matchCount;
+    }
+
+    public double getPrize() {
+        return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}
