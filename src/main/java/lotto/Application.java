bb94884041b68b1af1aa0d25b6cf4f69a1c27962
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-03 17:18:13 +0900

    feat(Application): 등수 계산 및 결과 출력 기능 추가
    - 맞는 개수 확인 및 수익률 계산 및 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,22 +6,25 @@ import static lotto.LottoConstants.NUMBER_COUNT;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         int money;
         int lottoCount;
         List<Lotto> lottos = new ArrayList<>();
+        int totalPrice = 0;
 
         // 로또 구매
         System.out.println("구입금액을 입력해 주세요.");
         try {
             money = Integer.parseInt(readLine());
+            System.out.println();
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
         }
-
         if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원으로 나눠 떨어지는 값을 입력해야 합니다.");
         }
@@ -36,6 +39,7 @@ public class Application {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+        System.out.println();
 
         // 당첨번호 입력
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -47,10 +51,39 @@ public class Application {
                 throw new IllegalArgumentException("[ERROR] 쉼표로 구분된 숫자만 입력해야 합니다.");
             }
         }).toList();
+        System.out.println();
 
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(readLine());
+        System.out.println();
 
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumbersNoBonus, bonusNumber);
 
+        // EnumMap으로 등수 갯수 계산
+        Map<Rank, Integer> rankCountMap = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            rankCountMap.put(rank, 0);
+        }
+        for (Lotto lotto : lottos) {
+            Rank rank = LottoChecker.check(lotto, winningNumbers);
+            rankCountMap.put(rank, rankCountMap.get(rank) + 1);
+            totalPrice += rank.getPrize();
+        }
+
+        // 결과 출력
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (Rank rank : Rank.values()) {
+            System.out.print(rank.getMatchCount());
+            System.out.print("개 일치");
+            if (rank.isRequireBonus()) {
+                System.out.print(", 보너스 볼 일치");
+            }
+            System.out.print(" (" + String.format("%,d", rank.getPrize()) + "원)");
+            System.out.println(" - " + rankCountMap.get(rank) + "개");
+        }
+
+        System.out.println("총 수익률은 " + String.format("%.1f", (double) totalPrice / money * 100) + "%입니다.");
     }
 }
