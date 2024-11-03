6bccc002abce30fbf7c65a32998cee51eaf58666
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-03 13:46:10 +0900

    refactor: 로또를 출력시에만 정렬하도록 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,6 +4,7 @@ import static lotto.common.LottoRule.LOTTO_NUMBER_COUNTS;
 import static lotto.validator.ValidationMessage.DUPLICATE_NUMBERS;
 import static lotto.validator.ValidationMessage.INVALID_NUMBER_COUNTS;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -16,7 +17,6 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validateNumberCount(numbers);
         validateNoDuplicate(numbers);
-        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -35,6 +35,8 @@ public class Lotto {
 
     @Override
     public String toString() {
-        return numbers.toString();
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers.toString();
     }
 }
