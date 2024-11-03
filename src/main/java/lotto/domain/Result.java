ad5d6a597bb6ce712799c66158d68be815d11913
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-11-03 15:54:06 +0900

    test(ResultTest): 올바른 Result 객체를 반환하는지 검증하는 단위테스트 구현

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -9,28 +9,28 @@ import static lotto.message.InfoMessage.*;
 
 public enum Result {
 
-    ZERO(false, null, 0,
+    ZERO(false, null,
             (match, bonus) -> match == 0),
 
-    ONE(false, null, 0,
+    ONE(false, null,
             (match, bonus) -> match == 1),
 
-    TWO(false, null, 0,
+    TWO(false, null,
             (match, bonus) -> match == 2),
 
-    THREE(true, THREE_NUMBERS_MATCH, 0,
+    THREE(true, THREE_NUMBERS_MATCH,
             (match, bonus) -> match == 3),
 
-    FOUR(true, FOUR_NUMBERS_MATCH, 0,
+    FOUR(true, FOUR_NUMBERS_MATCH,
             (match, bonus) -> match == 4),
 
-    FIVE(true, FIVE_NUMBERS_MATCH, 0,
+    FIVE(true, FIVE_NUMBERS_MATCH,
             (match, bonus) -> match == 5 && !bonus),
 
-    FIVE_WITH_BONUS(true, FIVE_NUMBERS_MATCH_WITH_BONUS, 0,
+    FIVE_WITH_BONUS(true, FIVE_NUMBERS_MATCH_WITH_BONUS,
             (match, bonus) -> match == 5 && bonus),
 
-    SIX(true, FIVE_NUMBERS_MATCH_WITH_BONUS, 0,
+    SIX(true, FIVE_NUMBERS_MATCH_WITH_BONUS,
             (match, bonus) -> match == 6);
 
     private boolean print;
@@ -38,7 +38,7 @@ public enum Result {
     private int result;
     private BiPredicate<Integer, Boolean> predicate;
 
-    Result(boolean print, InfoMessage infoMessage, int result, BiPredicate<Integer, Boolean> predicate) {
+    Result(boolean print, InfoMessage infoMessage, BiPredicate<Integer, Boolean> predicate) {
         this.print = print;
         this.infoMessage = infoMessage;
         this.result = 0;
@@ -49,14 +49,22 @@ public enum Result {
         result++;
     }
 
-    private boolean matches(int matchingNumber, boolean bonus) {
+    private boolean equals(int matchingNumber, boolean bonus){
         return predicate.test(matchingNumber, bonus);
     }
 
-    public static void update(int matchingNumber, boolean bonus) {
-        Arrays.stream(Result.values())
-                .filter(result -> result.matches(matchingNumber, bonus))
+    public static Result matches(int matchingNumber, boolean bonus) {
+        return Arrays.stream(Result.values())
+                .filter(result -> result.equals(matchingNumber, bonus))
                 .findAny()
-                .ifPresent(Result::increment);
+                .orElse(ZERO);
+    }
+
+    public static void update(int matchingNumber, boolean bonus) {
+        matches(matchingNumber, bonus).increment();
+    }
+
+    public static int getResult(int matchingNumber, boolean bonus) {
+        return matches(matchingNumber, bonus).result;
     }
 }
---
+++ src/test/java/lotto/domain/ResultTest.java
@@ -0,0 +1,80 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static lotto.domain.Result.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResultTest {
+
+    @DisplayName("당첨 번호 5개와 보너스 번호가 일치할 때 결과가 잘 반영되는지 확인")
+    @Test
+    void fiveWithBonusSuccessTest() {
+        // given
+        int matchingNumber = 5;
+        boolean bonus = true;
+        int before = getResult(matchingNumber, bonus);
+
+        // when
+        update(matchingNumber, bonus);
+
+        // then
+        int after = getResult(matchingNumber, bonus);
+        assertThat(after).isEqualTo(before + 1);
+    }
+
+    @DisplayName("당첨 번호 6개가 일치하는 경우")
+    @Test
+    void sixSuccessTest() {
+        // given
+        int matchingNumber = 6;
+        boolean bonus = false;
+        int before = getResult(matchingNumber, bonus);
+
+        // when
+        update(matchingNumber, bonus);
+
+        // then
+        int after = getResult(matchingNumber, bonus);
+        assertThat(after).isEqualTo(before + 1);
+    }
+
+    @DisplayName("매핑되어 있는 Result 객체를 올바르게 반환하는지 테스트")
+    @ParameterizedTest
+    @CsvSource({
+            "0, false, ZERO",
+            "1, false, ONE",
+            "2, false, TWO",
+            "3, false, THREE",
+            "4, false, FOUR",
+            "5, false, FIVE",
+            "5, true, FIVE_WITH_BONUS",
+            "6, false, SIX",
+    })
+    void matchingSuccessTest(int matchingNumber, boolean bonus, Result expected) {
+        // when
+        Result result = matches(matchingNumber, bonus);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("잘못된 값을 입력하는 경우 ZERO를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "-1, false",
+            "7, true",
+            "12, false"
+    })
+    void invalidInputTest(int matchingNumber, boolean bonus) {
+        // when
+        Result result = matches(matchingNumber, bonus);
+
+        // then
+        assertThat(result).isEqualTo(ZERO);
+    }
+
+}
