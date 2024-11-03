ef118e6f5fa4b9be2ae14ed05567044df029b521
DongYeop <s01054830455@gmail.com>
2024-11-03 19:03:12 +0900

    test: 입력 형 변환 테스트
    - 입력한 문자열이 원하는 자료형으로 변환되는지 테스트함.

---
+++ src/test/java/lotto/util/InputConverterTest.java
@@ -0,0 +1,37 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.view.InputConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputConverterTest {
+
+    @DisplayName("문자열을 정수형 리스트로 변환할 수 있다.")
+    @Test
+    void convertToNumbers() {
+        //given
+        String input = "1,2,3,4,5,6";
+
+        //when
+        List<Integer> numbers = InputConverter.convertToNumbers(input);
+
+        //then
+        assertThat(numbers).isInstanceOf(List.class).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("문자열을 정수형으로 변환할 수 있다.")
+    @Test
+    void parseToNumber() {
+        //given
+        String input = "1000";
+
+        //when
+        int number = InputConverter.parseToNumber(input);
+
+        //then
+        assertThat(number).isInstanceOf(Integer.class).isEqualTo(1000);
+    }
+}
