6a445450d7001414a35d4142d0bc16affe934624
chahyunsoo <chahs1012@naver.com>
2024-11-03 18:58:36 +0900

    feat(OutPutView) : 당첨통계 출력 및 수익률 출력 메소드 출력 인터페이스에 선언

--- src/main/java/lotto/view/OutPutView.java
+++ src/main/java/lotto/view/OutPutView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.Lotto;
+import lotto.constant.WinningRank;
 
 public interface OutPutView {
     void printBoughtLotto(List<Lotto> boughtLotto);
+
+    void printWinningStatistic(Map<WinningRank, Integer> results);
+
+    void printProfitRate(double profitRate);
 }
