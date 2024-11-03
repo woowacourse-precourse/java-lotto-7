1de9fb094a7684cd7264db6aa1b45644ba6c3449
yxoni <psy337250@gmail.com>
2024-11-03 21:39:42 +0900

    feat: 당첨 번호 리스트 생성 기능 구현

--- src/main/java/lotto/OperationManager.java
+++ src/main/java/lotto/OperationManager.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -13,7 +14,7 @@ public class OperationManager {
     public void buy() {
         // 입력 받기
         String payment = io.readPayment();
-        String winningNumber = io.readWinningNumber();
+        String winningNumberInput = io.readWinningNumber();
         String bonusNumber = io.readBonusNumber();
 
         // 로또 생성
@@ -22,6 +23,13 @@ public class OperationManager {
             List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(lotto);
             lottos.add(new Lotto(lotto));
+        }
+
+        // 당첨 번호 리스트 생성
+        List<Integer> winningNumbers = new ArrayList<>();
+        String[] winningNumber = winningNumberInput.split(",");
+        for (int i = 0; i < 6; i++) {
+            winningNumbers.add(Integer.parseInt(winningNumber[i]));
         }
 
 
