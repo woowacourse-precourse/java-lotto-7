2e5ef5ee6e80a882432a286de74ac590016ca1b9
xeimun <hinote444@naver.com>
2024-11-03 19:37:07 +0900

    feat(LottoService): 당첨 등수 판별 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,7 +2,16 @@ package lotto.service;
 
 import static java.lang.Integer.parseInt;
 
+import lotto.Lotto;
+import lotto.LottoRank;
+
 public class LottoService {
+
+    private LottoRank calculateLottoRank(Lotto lotto, Lotto userLotto, int bonusNumber) {
+        int matchCount = lotto.countMatchingNumbers(userLotto);
+        boolean bonusMatch = lotto.containsNumber(bonusNumber);
+        return LottoRank.findRankByMatches(matchCount, bonusMatch);
+    }
 
     public int calculatePurchaseQuantity(String purchaseAmount) {
         return parseInt(purchaseAmount) / 1000;
