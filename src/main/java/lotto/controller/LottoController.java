2949b277e984cedee8db4f67cbc4d6b2a7de91a9
JS <zks145@naver.com>
2024-11-03 14:40:43 +0900

    feat : 로또 게임 시작 기능 추가 (로또 당첨 번호, 로또 보너스 번호) 입력 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,15 @@
 package lotto.controller;
 
 import lotto.domain.Consumer;
+import lotto.domain.LottoPrize;
 import lotto.service.LottoService;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 import static lotto.util.Utils.changeStringToInt;
-import static lotto.view.InputView.inputPrice;
+import static lotto.util.Utils.changeStringToIntegerList;
+import static lotto.view.InputView.*;
 import static lotto.view.OutputView.*;
 
 public class LottoController {
@@ -12,11 +17,12 @@ public class LottoController {
     private static final int NUMBER_LIMIT = 1000;
     private final LottoService lottoService = new LottoService();
     private Consumer consumer;
+    private LottoPrize lottoPrize;
 
     public void start() {
         setting();
-//        startLotto();
-//        resultLotto();
+        startLotto();
+        resultLotto();
     }
 
     public void setting() {
@@ -49,5 +55,47 @@ public class LottoController {
         }
     }
 
+    public void startLotto() {
+        lottoPrize = lottoPrizeNumber();
+        lottoPrize = lottoBonusNumber();
+    }
+
+    private LottoPrize lottoPrizeNumber() {
+        try {
+            return LottoPrize.createLottoPrize(inputLottoPrizeNumber());
+        } catch (IllegalArgumentException e) {
+            ErrorMessage(e.getMessage());
+            return lottoPrizeNumber();
+        }
+    }
+
+    private List<Integer> inputLottoPrizeNumber() {
+        try {
+            String inputPrizeNumber = inputLottoNumbers();
+            return changeStringToIntegerList(inputPrizeNumber);
+        } catch (IllegalArgumentException e) {
+            ErrorMessage(e.getMessage());
+            return inputLottoPrizeNumber();
+        }
+    }
+
+    private LottoPrize lottoBonusNumber() {
+        try {
+            return LottoPrize.createLottoBonus(lottoPrize.getLottoPrizeNumbers(), inputLottoBonusNumber());
+        } catch (IllegalArgumentException e) {
+            ErrorMessage(e.getMessage());
+            return lottoBonusNumber();
+        }
+    }
+
+    private Integer inputLottoBonusNumber() {
+        try {
+            String inputBonusNumber = inputLottoBonus();
+            return changeStringToInt(inputBonusNumber);
+        } catch (IllegalArgumentException e) {
+            ErrorMessage(e.getMessage());
+            return inputLottoBonusNumber();
+        }
+    }
 
 }
