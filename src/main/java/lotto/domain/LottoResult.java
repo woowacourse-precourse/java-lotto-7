3006705edd418896c7586ac33d2506cb650c36dc
dltmddus <tmddus4671@gmail.com>
2024-11-03 17:14:52 +0900

    fix: Rank 판별 로직의 matchNumber 키를 Rank로 수정

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -5,18 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
-    private final Map<Integer, Integer> matchNumber;
+    private final Map<Rank, Integer> matchNumber;
     private final LottoShop lottoShop;
     private final Lotto winningLotto;
+    private final int bonusNumber;
 
-    public LottoResult(LottoShop lottoShop, Lotto winningLotto) {
+    public LottoResult(LottoShop lottoShop, Lotto winningLotto, int bonusNumber) {
         this.lottoShop = lottoShop;
         this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
         matchNumber = new HashMap<>();
         calculateMatchLotto();
     }
 
-    public Map<Integer, Integer> getMatchNumber() {
+    public Map<Rank, Integer> getMatchNumber() {
         return matchNumber;
     }
 
@@ -28,8 +30,10 @@ public class LottoResult {
 
     private void addMatchingLotto(Lotto lotto) {
         int matchedCount = getMatchingNumbers(lotto).size();
-        if (matchedCount >= 3) {
-            matchNumber.put(matchedCount, matchNumber.getOrDefault(matchedCount, 0) + 1);
+        Rank rank = getCorrectRank(lotto, matchedCount);
+
+        if (rank != Rank.NOT_MATCH) {
+            matchNumber.put(rank, matchNumber.getOrDefault(rank, 0) + 1);
         }
     }
 
@@ -38,4 +42,21 @@ public class LottoResult {
                 .filter(winningLotto.getNumbers()::contains)
                 .toList();
     }
+
+    private Rank getCorrectRank(Lotto lotto, int matchedCount) {
+        if (matchedCount == 6) {
+            return Rank.FIRST_PLACE;
+        }
+        if (matchedCount == 5) {
+            return lotto.getNumbers().contains(bonusNumber) ? Rank.SECOND_PLACE : Rank.THIRD_PLACE;
+        }
+        if (matchedCount == 4) {
+            return Rank.FOURTH_PLACE;
+        }
+        if (matchedCount == 3) {
+            return Rank.FIFTH_PLACE;
+        }
+        return Rank.NOT_MATCH;
+    }
+
 }
