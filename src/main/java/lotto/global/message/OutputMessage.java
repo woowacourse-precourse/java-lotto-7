2610ccffd4cddf02128569955e32b87048c103f6
softmoca <acyc0909@gmail.com>
2024-11-03 20:31:49 +0900

    fix : 출력 형식 수정

--- src/main/java/lotto/global/message/OutputMessage.java
+++ src/main/java/lotto/global/message/OutputMessage.java
@@ -1,7 +1,7 @@
 package lotto.global.message;
 
 public class OutputMessage {
-    public static final String PURCHASE_COUNT_FORMAT = "\n%d개를 구매했습니다.";
+    public static final String PURCHASE_COUNT_FORMAT = "\n%d개를 구매했습니다.\n";
     public static final String STATISTICS_HEADER = "\n당첨 통계";
     public static final String STATISTICS_DELIMITER = "---";
     public static final String PRIZE_RESULT_FORMAT = "%s (%,d원) - %d개%n";
