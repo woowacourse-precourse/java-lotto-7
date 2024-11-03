20b5c5e5261365affabe4c07683459446616385a
yunsseong <aster20@hanyang.ac.kr>
2024-11-03 23:21:44 +0900

    feat: 로또 생성 기능 구현

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,6 +1,9 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.model.Lotto;
 import lotto.validator.Validator;
 import lotto.view.InputView;
@@ -17,6 +20,13 @@ public class LottoMachine {
         do {
             input = inputView.input(SystemMessage.INPUT_PURCHASE_MONEY_AMOUNT.getMessage());
         } while (!validateMoneyAmount(input));
+
+        int moneyAmount = Integer.parseInt(input);
+        return IntStream.range(0, calculateNumberLotto(moneyAmount))
+                .mapToObj(num -> new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+                .toList();
+    }
+
     public int calculateNumberLotto(int moneyAmount) {
         return moneyAmount / 1000;
     }
