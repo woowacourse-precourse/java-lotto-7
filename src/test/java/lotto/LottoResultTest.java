b20f812437a19ea62abb1d59214025f4e66be0eb
johyewon <johyewon4601@gmail.com>
2024-11-04 01:12:04 +0900

    test(LottoResultTest): 정확한 수익률 계산 테스트 추가

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LottoResultTest {
+    @Test
+    void 수익률_계산이_제대로_계산된_경우() {
+        LottoResult lottoResult = new LottoResult();
+        double profitRate = lottoResult.calculateProfitRate(10000, 5000);
+
+        assertEquals(50.0, profitRate);
+    }
 }
