4f307d8eaee92568befa88827096f1543aa94a27
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 22:32:51 +0900

    feat : 로또 구입 금액을 검증 및 구매 횟수 반환 기능 구현 - 입력 금액이 1000원 단위인지 확인 - 유효한 금액에 따라 구매 가능한 로또 장 수 계산

--- src/main/java/lotto/model/LottoPurchasePrice.java
+++ src/main/java/lotto/model/LottoPurchasePrice.java
@@ -1,4 +1,30 @@
 package lotto.model;
 
+import java.util.List;
+
 public class LottoPurchasePrice {
+    int LOTTO_PRICE = 1000;
+    private final String lottoPurchasePrice;
+    private final int lottoPurchaseCount;
+
+    public LottoPurchasePrice(String lottoPurchasePrice) {
+        this.lottoPurchasePrice = lottoPurchasePrice;
+        validateLottoPurchasePrice(lottoPurchasePrice);
+        this.lottoPurchaseCount = convertPriceToCount(lottoPurchasePrice);
+
+    }
+
+    private int convertPriceToCount(String lottoPurchasePrice) {
+        return Integer.parseInt(lottoPurchasePrice)/LOTTO_PRICE;
+    }
+
+    private void validateLottoPurchasePrice(String lottoPurchasePrice) {
+        if (Integer.parseInt(lottoPurchasePrice) % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    public int getLottoPurchaseCount() {
+        return lottoPurchaseCount;
+    }
 }
