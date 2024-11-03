984fe50865f02766fcfbe4ec0bef449728d639cf
yuntasha <jaljayo85@gmail.com>
2024-11-03 18:26:31 +0900

    feat(InputHelper): write LottoController
    Write field list:
    - InputHelper inputHelper
    - OutputHelper outputHelper
    - LottoMachine lottoMachine
    
    Write method list:
    - run
    - buyLotto
    - setWinningAndBonus
    - showResult

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,60 @@
 package lotto.controller;
 
+import lotto.exception.LottoException;
+import lotto.model.LottoMachine;
+import lotto.model.Result;
+import lotto.view.InputHelper;
+import lotto.view.OutputHelper;
+
+import java.util.List;
+
 public class LottoController {
+    private final InputHelper inputHelper;
+    private final OutputHelper outputHelper;
+    private final LottoMachine lottoMachine;
+
+    public LottoController(InputHelper inputHelper, OutputHelper outputHelper, LottoMachine lottoMachine) {
+        this.inputHelper = inputHelper;
+        this.outputHelper = outputHelper;
+        this.lottoMachine = lottoMachine;
+    }
+
+    public void run() {
+        buyLotto();
+        setWinningAndBonus();
+        showResult();
+    }
+
+    private void buyLotto() {
+        try {
+            int money = inputHelper.getPurchase();
+            lottoMachine.buyLotto(money);
+            outputHelper.printLotto(lottoMachine.getLottoNums());
+        } catch (LottoException e) {
+            System.out.println(e.makeErrorMessage());
+            buyLotto();
+        }
+    }
+
+    private void setWinningAndBonus() {
+        try {
+            List<Integer> winningNum = inputHelper.getWinner();
+            int bonus = inputHelper.getBonus();
+
+            lottoMachine.init(winningNum, bonus);
+        } catch (LottoException e) {
+            System.out.println(e.makeErrorMessage());
+            buyLotto();
+        }
+    }
+    
+    private void showResult() {
+        try {
+            Result result = lottoMachine.getResult();
+            outputHelper.printResult(result);
+        } catch (LottoException e) {
+            System.out.println(e.makeErrorMessage());
+            buyLotto();
+        }
+    }
 }
