dbc2a5cf78de3de1a551c4e95891707b0b76dbf0
Parkjyun <jaeyun0507@naver.com>
2024-11-03 19:07:54 +0900

    feat(Answer): 보너스 번호를 갖는다

--- src/main/java/lotto/model/Answer.java
+++ src/main/java/lotto/model/Answer.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class Answer {
 
     private final List<Integer> answerNumbers;
+    private final BonusNumber bonusNumber;
 
-    public Answer(List<Integer> answerNumbers) {
+
+    public Answer(List<Integer> answerNumbers, BonusNumber bonusNumber) {
         this.answerNumbers = answerNumbers;
+        this.bonusNumber = bonusNumber;
     }
 }
--- src/test/java/lotto/model/exception/AnswerTest.java
+++ src/test/java/lotto/model/exception/AnswerTest.java
@@ -3,6 +3,7 @@ package lotto.model.exception;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.Answer;
+import lotto.model.BonusNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,8 +15,9 @@ class AnswerTest {
     void should_CreateAnswer_WhenGivenNumbers() {
         // given
         List<Integer> answerNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber("7");
         // when
-        Answer answer = new Answer(answerNumbers);
+        Answer answer = new Answer(answerNumbers, bonusNumber);
         // then
         Assertions.assertThat(answer).isNotNull();
     }
