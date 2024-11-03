87d3a25693b18211664c5d7f0b00cb04cb583f8e
imscow11253 <seaweed8759@naver.com>
2024-11-04 04:15:45 +0900

    feat : 가격 숫자 앞뒤 공백은 제거한다

--- src/main/java/lotto/model/PriceToBuyLotto.java
+++ src/main/java/lotto/model/PriceToBuyLotto.java
@@ -7,15 +7,13 @@ import lotto.validation.PriceToBuyLottoValidation;
 public record PriceToBuyLotto (Integer price){
     public static PriceToBuyLotto of (String input) {
         Integer price;
-
         try{
-            price = Integer.parseInt(input);
+            price = Integer.parseInt(input.trim());
         }catch(NumberFormatException e){
             throw new PriceToBuyLottoException(ErrorMessage.PRICE_SHOULD_BE_INTEGER);
         }
 
         PriceToBuyLottoValidation.validatePriceToBuyLotto(price);
-
         return new PriceToBuyLotto(price);
     }
 }
