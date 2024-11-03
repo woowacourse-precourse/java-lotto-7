ef4d3718eabbd9f2051ea39e59d4a65e9a21886c
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 18:12:20 +0900

    test(lotto/logic): 로또 티켓 등수 목록 반환 기능 테스트 추가

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTicketTest {
@@ -16,4 +19,25 @@ class LottoTicketTest {
         );
     }
 
+    @Test
+    @DisplayName("로또 등수 번호 반환")
+    void getRanks() {
+        assertRandomUniqueNumbersInRangeTest(() -> {
+                    LottoTicket lottoTicket = LottoTicket.of(6);
+                    WinningTicket winningTicket = WinningTicket.of("1,2,3,4,5,6", "7");
+                    List<Rank> ranks = lottoTicket.getRanks(winningTicket);
+                    assertThat(ranks.get(0)).isEqualTo(null);
+                    assertThat(ranks.get(1)).isEqualTo(Rank.FIFTH);
+                    assertThat(ranks.get(2)).isEqualTo(Rank.FOURTH);
+                    assertThat(ranks.get(3)).isEqualTo(Rank.THIRD);
+                    assertThat(ranks.get(4)).isEqualTo(Rank.SECOND);
+                    assertThat(ranks.get(5)).isEqualTo(Rank.FIRST);
+                },
+                List.of(11, 12, 13, 14, 15, 16),
+                List.of(1, 2, 3, 14, 15, 16),
+                List.of(1, 2, 3, 4, 15, 16),
+                List.of(1, 2, 3, 4, 5, 16),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 6));
+    }
 }
