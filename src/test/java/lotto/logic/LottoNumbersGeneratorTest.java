fe9e7dbec1bc6da670cc9335e1e40e887bb6e579
bmg1203 <bmg1203@naver.com>
2024-11-03 17:03:28 +0900

    [test]: 단위테스트 - 로또 자동추첨 테스트 추가
    - 로또를 많이 구매했을 때 자동추첨이 잘 되는지 테스트합니다.

--- src/test/java/lotto/logic/LottoNumbersGeneratorTest.java
+++ src/test/java/lotto/logic/LottoNumbersGeneratorTest.java
@@ -1,9 +1,7 @@
 package lotto.logic;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LottoNumbersGeneratorTest {
@@ -12,6 +10,19 @@ class LottoNumbersGeneratorTest {
     void 로또_자동_추첨_테스트() {
         //given
         int count = 5;
+
+        //when
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator(count);
+
+        //then
+        assertThat(count)
+                .isEqualTo(lottoNumbersGenerator.getLottos().size());
+    }
+
+    @Test
+    void 로또_자동_추첨_많이_테스트() {
+        //given
+        int count = 100000;
 
         //when
         LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator(count);
