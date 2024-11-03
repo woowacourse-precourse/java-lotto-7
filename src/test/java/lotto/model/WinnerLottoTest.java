535b5b947a67f83a133a5015b14d3aa2ce5de06f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 00:39:20 +0900

    test(WinnerLottoTest): WinnerLotto 클래스의 예외 처리 테스트 코드 추가
    - 입력된 당첨 번호가 6이 아니거나 범위가 1~45가 아닐 경우 예외 발생 테스트
    - 입력된 보너스 번호가 범위 외 또는 당첨 번호와 중복되는 경우 예외 발생 테스트

---
+++ src/test/java/lotto/model/WinnerLottoTest.java
@@ -0,0 +1,51 @@
+package lotto.model;
+
+import lotto.exception.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinnerLottoTest {
+
+    @Test
+    void 입력된_당첨번호_문자열_배열의_크기가_6이_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX.getMessage());
+
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX.getMessage());
+    }
+
+    @DisplayName("숫자 문자열 배열에 숫자 문자열이 1~45사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 숫자_문자열_배열에_숫자_문자열이_범위_내_숫자가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage());
+
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage());
+    }
+
+    @DisplayName("입력된 보너스 번호가 1~45사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 입력된_보너스_번호가_범위_내_숫자가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6").setBonusNumber("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage());
+    }
+
+    @Test
+    void 입력된_보너스_번호가_당첨번호와_중복되면_예외가_발생한다() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6").setBonusNumber("6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_NUMBER_DUPLICATES_WITH_WINNING_NUMBERS.getMessage());
+    }
+}
