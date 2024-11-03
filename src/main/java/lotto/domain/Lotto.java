585cc524cf9f75367f41e78cb3d95d722d10287f
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-03 16:45:39 +0900

    refactor(Lotto): 로또 번호 값 객체로 분리
    - 로또 번호 wrapper 클래스 LottoNumber 구현
    - List<Integer> numbers를 List<LottoNumber>로 변경
    - 로또 번호 검증을 LottoNumber로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,36 +3,26 @@ package lotto.domain;
 import java.util.List;
 import lotto.exception.LottoException;
 
-public record Lotto(List<Integer> numbers) {
+public record Lotto(List<LottoNumber> numbers) {
 
-    private final static int MIN_NUMBER = 1;
-    private final static int MAX_NUMBER = 45;
     private final static int LOTTO_SIZE = 6;
 
     public Lotto {
         validate(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<LottoNumber> numbers) {
         validateLottoSize(numbers);
-        validateNumberRange(numbers);
         validateDuplicate(numbers);
     }
 
-    private void validateLottoSize(final List<Integer> numbers) {
+    private void validateLottoSize(final List<LottoNumber> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new LottoException("로또 번호는 " + LOTTO_SIZE + "개여야 합니다.");
         }
     }
 
-    private void validateNumberRange(final List<Integer> numbers) {
-        if (numbers.stream()
-                .anyMatch(num -> num < MIN_NUMBER || num > MAX_NUMBER)) {
-            throw new LottoException("로또 번호는 " + MIN_NUMBER + "에서 " + MAX_NUMBER + " 사이의 값이어야 합니다.");
-        }
-    }
-
-    private void validateDuplicate(final List<Integer> numbers) {
+    private void validateDuplicate(final List<LottoNumber> numbers) {
         if (numbers.stream().distinct().count() != LOTTO_SIZE) {
             throw new LottoException("로또 번호는 중복될 수 없습니다.");
         }
---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import lotto.exception.LottoException;
+
+public record LottoNumber(int number) {
+
+    private final static int MIN_NUMBER = 1;
+    private final static int MAX_NUMBER = 45;
+
+    public LottoNumber {
+        validate(number);
+    }
+
+    private void validate(final int number) {
+        validateNumberRange(number);
+    }
+
+    private void validateNumberRange(final int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new LottoException("로또 번호는 " + MIN_NUMBER + "에서 " + MAX_NUMBER + " 사이의 값이어야 합니다.");
+        }
+    }
+}
