c54b93f29ae7ae8008c5342ae047b8944bfd3785
SeungminKimdev <ksm7250432@gmail.com>
2024-11-03 21:30:36 +0900

    feat: 등수에 필요한 enum 생성

---
+++ src/main/java/lotto/LottoReward.java
@@ -0,0 +1,33 @@
+package lotto;
+
+public enum LottoReward {
+    FIRST(6, false, 2000000000),    // 1등
+    SECOND(5, true, 30000000),      // 2등
+    THIRD(5, false, 1500000),       // 3등
+    FOURTH(4, false, 50000),        // 4등
+    FIFTH(3, false, 5000),          // 5등
+    BOOM(0, false, 0);              // 꽝
+
+    private final int match;
+    private final boolean bonus;
+    private final int prize;
+
+    LottoReward(int match, boolean bonus, int prize) {
+        this.match = match;
+        this.bonus = bonus;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static LottoReward valueOf(int match, boolean bonus) {
+        for (LottoReward reward : LottoReward.values()) {
+            if (reward.match == match && (!reward.bonus || (bonus == reward.bonus))) {
+                return reward;
+            }
+        }
+        return BOOM;
+    }
+}
