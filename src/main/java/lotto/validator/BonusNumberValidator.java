884bef22c12d74dc225caf0b53dc1cda62b892cb
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 04:42:58 +0900

    refactor(validator): BonusNumberValidator 클래스 수정 -exception클래스를 선언하여 예외를 던지는 책임을 exception클래스로 분리하였습니다.

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -1,18 +1,19 @@
 package lotto.validator;
 
 import java.util.List;
+import lotto.exception.LottoException;
 
 public class BonusNumberValidator {
 
-  private static final String ERROR_OUT_OF_RANGE_BONUS = "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.";
-  private static final String ERROR_DUPLICATE_BONUS = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+  private static final int MIN_LOTTO_NUMBER = 1;
+  private static final int MAX_LOTTO_NUMBER = 45;
 
   public static void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
-    if (bonusNumber < 1 || bonusNumber > 45) {
-      throw new IllegalArgumentException(ERROR_OUT_OF_RANGE_BONUS);
+    if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+      LottoException.throwOutOfRangeBonusException();
     }
     if (winningNumbers.contains(bonusNumber)) {
-      throw new IllegalArgumentException(ERROR_DUPLICATE_BONUS);
+      LottoException.throwDuplicateBonusException();
     }
   }
 }
