60188fb8f7039e4422cd893201a42da3b57cc3f9
dltmddus <tmddus4671@gmail.com>
2024-11-03 16:00:13 +0900

    feat: 로또 등수 정보를 담는 Rank enum클래스 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public enum Rank {
+    NOT_MATCH(0, "", 0),
+    FIFTH_PLACE(3, "3개 일치 (5,000원) - %d개", 5_000),
+    FOURTH_PLACE(4, "4개 일치 (50,000원) - %d개", 50_000),
+    THIRD_PLACE(5, "5개 일치 (1,500,000원) - %d개", 1_500_000),
+    SECOND_PLACE(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", 30_000_000),
+    FIRST_PLACE(6, "6개 일치 (2,000,000,000원) - %d개", 2_000_000_000);
+
+    final int matchNumber;
+    final String message;
+    final int prize;
+
+    Rank(int matchNumber, String message, int prize) {
+
+        this.matchNumber = matchNumber;
+        this.message = message;
+        this.prize = prize;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
