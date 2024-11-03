c8d2350b6139fe42f67f02a5f92cf3a37a8549b8
wkdlrn <kkjjgg97@gmail.com>
2024-11-04 01:08:51 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmount();
+        System.out.println("구입 금액: " + purchaseAmount);
+    }
+
+    private static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine().trim());
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return purchaseAmount;
     }
 }
