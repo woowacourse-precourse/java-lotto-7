5833e814d93d78e492fa84a8500e416f7a32b74d
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 19:01:57 +0900

    feat(GameResultManager): 당첨 로또 설정 기능

---
+++ src/main/java/lotto/manager/GameResultManager.java
@@ -0,0 +1,14 @@
+package lotto.manager;
+
+import lotto.domain.*;
+import java.util.List;
+
+
+public class GameResultManager {
+    private WinningLotto winningLotto;
+
+    public void changeWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+
+        this.winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    }
+}
