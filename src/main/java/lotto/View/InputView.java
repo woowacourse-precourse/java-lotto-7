daca793d6dbca4e28d70e75365264c66eaa858b5
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:46:29 +0900

    chore(InputView) : 커맨드라인 자동 정렬

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -11,58 +11,56 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String DELIMITER = ",";
 
-    public static Integer readPurchaseAmount(){
+    public static Integer readPurchaseAmount() {
         int purchasePrice = 0;
         try {
             String rawPurchasePrice = Console.readLine();
             purchasePrice = parseInt(rawPurchasePrice);
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER.getError());
         }
         return purchasePrice;
     }
 
-    public static Lotto readWinningNum(){
+    public static Lotto readWinningNum() {
         String rawWinningInput = Console.readLine();
         return new Lotto(Arrays.asList(rawWinningInput.split(DELIMITER)).stream()
                 .map(InputView::parseInt)
                 .collect(Collectors.toList()));
     }
 
-    public static int readBonus(Lotto answer){
+    public static int readBonus(Lotto answer) {
         String rawBonus = Console.readLine();
         int bonus = parseInt(rawBonus);
         checkBonus(bonus, answer);
-        checkRange(bonus);
+        checkBonusRange(bonus);
         return bonus;
     }
 
     public static int parseInt(String strNum) {
         int result = 0;
-        try{
+        try {
             result = Integer.parseInt(strNum);
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER.getError());
         }
         return result;
     }
 
-    public static void checkBonus(int bonusNumber, Lotto answer){
-        if(!(answer.getNumbers().stream()
+    public static void checkBonus(int bonusNumber, Lotto answer) {
+        if (!(answer.getNumbers().stream()
                 .allMatch(number -> number != bonusNumber))) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_DUPLICATE.getError());
         }
     }
 
-    public static void checkRange(int lottoNumber){
-        if(!(lottoNumber >= 1 && lottoNumber <= 45)) {
+    public static void checkBonusRange(int lottoNumber) {
+        if (!(lottoNumber >= 1 && lottoNumber <= 45)) {
             throw new IllegalArgumentException(ErrorMessage.BONUS_RANGE.getError());
         }
     }
 
-    public static boolean checkRangeList(List<Integer> numbers){
+    public static boolean checkRangeList(List<Integer> numbers) {
         return numbers.stream()
                 .allMatch(number -> number >= 1 && number <= 45);
     }
