8e62c7da300f582ea1c48110caf4433ca2039368
minjae k <rkdalswoals@gmail.com>
2024-11-04 01:41:59 +0900

    refactor(config): implement object factory

---
+++ src/main/java/lotto/config/ObjectFactory.java
@@ -0,0 +1,31 @@
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.domain.ProfitCalculator;
+import lotto.domain.ResultAnalyzer;
+import lotto.domain.TicketGenerator;
+import lotto.service.LottoService;
+import lotto.validator.BasicNumberValidator;
+import lotto.validator.NumberValidator;
+import lotto.validator.PurchaseAmountValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class ObjectFactory {
+
+    public static LottoController createLottoController() {
+        NumberValidator purchaseValidator = new PurchaseAmountValidator();
+        NumberValidator basicNumberValidator = new BasicNumberValidator();
+
+        InputView inputView = new InputView(purchaseValidator, basicNumberValidator);
+        OutputView outputView = new OutputView();
+
+        TicketGenerator ticketGenerator = new TicketGenerator();
+        ResultAnalyzer resultAnalyzer = new ResultAnalyzer();
+        ProfitCalculator profitCalculator = new ProfitCalculator();
+
+        LottoService lottoService = new LottoService(ticketGenerator, resultAnalyzer, profitCalculator);
+
+        return new LottoController(inputView, outputView, lottoService);
+    }
+}
