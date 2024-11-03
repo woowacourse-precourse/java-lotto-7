c32842047dbe0c8a8a5da579a93da10214fb6ef2
chaeyeon0130 <k84292330@gmail.com>
2024-11-03 16:51:09 +0900

    feat(InputValidator): 구입 검증 로직 구현
    - 구입 금액이 숫자 형식이 아닌 경우 예외 처리
    - 구입 금액이 1000원 단위가 아닌 경우 예외 처리

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -1,4 +1,9 @@
 package lotto.util;
 
 public class InputValidator {
+    public void checkDividedBy1000(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위이어야 합니다.");
+        }
+    }
 }
