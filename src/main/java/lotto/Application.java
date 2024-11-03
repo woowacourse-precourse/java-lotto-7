1a6ae78a1fa5c492e62491ea346b180d8ed3c462
kim-su-mi <ksmm9607@naver.com>
2024-11-03 18:36:02 +0900

    Feat : 3. 당첨 번호 입력 및 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,26 +7,46 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-    	try {
-    		System.out.println("구입금액을 입력해 주세요.");
-            String input = Console.readLine();
-            validatePurchaseAmount(Integer.parseInt(input));
-            
-            int amount = Integer.parseInt(input);
-            List<Lotto> lottos = LottoMachine.issue(amount);
-            
-            System.out.printf("%d개를 구매했습니다.\n", lottos.size());
-            lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
-    	}catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+    	List<Lotto> lottos = purchaseLottos();
+        WinningNumber winningNumber = inputWinningNumber();
+        
+    	
     	
     }
-
+    private static List<Lotto> purchaseLottos() {
+    	 while (true) {
+    		 try {
+    	    		System.out.println("구입금액을 입력해 주세요.");
+    	            String input = Console.readLine();
+    	            validatePurchaseAmount(Integer.parseInt(input));
+    	            
+    	            int amount = Integer.parseInt(input);
+    	            List<Lotto> lottos = LottoMachine.issue(amount);
+    	            
+    	            System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+    	            lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    	            return lottos;
+    	    	}catch (IllegalArgumentException e) {
+    	            System.out.println(e.getMessage());
+    	        }
+    	 }
+    	
+    }
     private static void validatePurchaseAmount(int amount) {
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }
     
+    }
+    private static WinningNumber inputWinningNumber() {
+        while (true) {
+            try {
+                System.out.println("\n당첨 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                return new WinningNumber(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
