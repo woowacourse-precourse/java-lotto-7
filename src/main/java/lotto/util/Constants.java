7989f3bc7ccf1b231ee148d81a3ea04cea29f71c
dongri <l0o0lv@github.com>
2024-11-03 19:38:02 +0900

    refactor(Constants): 상수 클래스 삭제 후 모든 상수 OutputView 로 이동

--- src/main/java/lotto/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package lotto.util;
-
-public class Constants {
-    private Constants() {}
-
-    public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String OUTPUT_LOTTO_COUNT = "개를 구매했습니다.";
-}
