7c4040b687ec1e8cb61e98cdbf7b8a9393753506
장영재 <urinaner@naver.com>
2024-11-04 02:34:09 +0900

    feat(OutputView): 로또 구매 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,4 +12,8 @@ public class OutputView {
     private static final String WINNING_STATISTIC_FORMAT = "%d개 일치 (%,d원) - %d개";
     private static final String WINNING_STATISTIC_SECOND_FORMAT = "5개 일치, 보너스 볼 일치 (%,d원) - %d개";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
+
+    public void printBuyLotto(Money money) {
+        System.out.printf((OUTPUT_MESSAGE) + "%n", money.getTicket());
+    }
 }
