2348a65b8699701075f2087799a0e269926ef671
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-03 20:09:35 +0900

    feat(PaymentPriceDTO): 사용자에게 입력받은 값을 전달하는 DTO 추가
    - 사용자에게 입력 받은 paymentPrice를 서비스 계층에게 전달하는 DTO 추가

--- src/main/java/lotto/DTO/PaymentPriceDTO.java
+++ src/main/java/lotto/DTO/PaymentPriceDTO.java
@@ -1,4 +1,13 @@
 package lotto.DTO;
 
 public class PaymentPriceDTO {
+    private Integer paymentPrice;
+
+    public PaymentPriceDTO(String paymentPrice) {
+        this.paymentPrice = Integer.parseInt(paymentPrice);
+    }
+
+    public Integer getPaymentPrice() {
+        return paymentPrice;
+    }
 }
