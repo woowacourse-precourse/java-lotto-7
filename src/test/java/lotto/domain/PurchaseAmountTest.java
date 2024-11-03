61277c41093c84171dd6f58f229d204fbb966bfc
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-11-04 06:59:30 +0900

    refactor : PurchaseAmountTest 하드 코딩 수정

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -12,42 +12,50 @@ class PurchaseAmountTest {
     @Test
     @DisplayName("5,000원을 입력 했을 경우")
     public void test1(){
-        PurchaseAmount purchaseAmount = new PurchaseAmount("5000");
-        assertThat(purchaseAmount.getLottoTickets()).isEqualTo(5);
+        String money = "5000";
+        int result = 5;
+        PurchaseAmount purchaseAmount = new PurchaseAmount(money);
+        assertThat(purchaseAmount.getLottoTickets()).isEqualTo(result);
     }
 
     @Test
     @DisplayName("1,000,000원을 입력 했을 경우")
     public void test2(){
-        PurchaseAmount purchaseAmount = new PurchaseAmount("1000000");
-        assertThat(purchaseAmount.getLottoTickets()).isEqualTo(1000);
+        String money = "1000000";
+        int result = 1000;
+        PurchaseAmount purchaseAmount = new PurchaseAmount(money);
+        assertThat(purchaseAmount.getLottoTickets()).isEqualTo(result);
     }
 
     @Test
     @DisplayName("정확한 숫자를 입력하지 않은 경우")
     public void test3(){
-        assertThatThrownBy(() -> new PurchaseAmount("test"))
+        String test = "test";
+        assertThatThrownBy(() -> new PurchaseAmount(test))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_NUMBER.getMessage());
     }
 
     @Test
     @DisplayName("빈 값을 입력한 경우")
     public void test4(){
-        assertThatThrownBy(() -> new PurchaseAmount(""))
+        String test = "t";
+        assertThatThrownBy(() -> new PurchaseAmount(test))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_NUMBER.getMessage());
     }
 
     @Test
     @DisplayName("입력한 숫자가 1,000의 단위가 아닌 경우")
     public void test5(){
-        assertThatThrownBy(() -> new PurchaseAmount("9999"))
+        String test = "9999";
+        assertThatThrownBy(() -> new PurchaseAmount(test))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_THOUSAND.getMessage());
     }
 
     @Test
     @DisplayName("입력한 숫자가 1,000의 단위 이지만 음수인 경우")
     public void test6(){
-        assertThatThrownBy(() -> new PurchaseAmount("-1000"))
+        String test = "-1000";
+        assertThatThrownBy(() -> new PurchaseAmount(test))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.NEGATIVE_NUMBER.getMessage());
     }
 
