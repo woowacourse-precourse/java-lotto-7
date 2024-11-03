6b193f2813ba861bf6fb1889bdb8d1f5f27e1f8e
AwesomeMinsik <beaut3579@gmail.com>
2024-11-03 21:17:34 +0900

    feat : 로또 번호의 중복 검증 메서드를 추가하였습니다
    - Lotto 클래스의 로또 번호 중복 여부에 대해 검증하는 메서드를 추가하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,23 +1,29 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicationCheck(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException();
         }
     }
-
-    // TODO: 추가 기능 구현
-
+    private void duplicationCheck(List<Integer> numbers){
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호에 중복이 존재합니다.");
+        }
+    }
     public List<Integer> getNumbers() {
         return numbers;
     }
