7c6325d0a385d32eeb9f58ab9586ed224df75b0d
BusanGukbap <dong2857210@gmail.com>
2024-11-04 01:17:47 +0900

    feat(InputView) : 메소드 추가
    inputWinningNumbers () : 당첨 번호 입력시
    
    inputBonusNumber () : 보너스 번호 입력시

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String inputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
 }
