43e1e430e6c39c50cceab753d462b8493d50cd38
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-03 21:20:36 +0900

    feat: Lotto 객체 생성자 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+}
