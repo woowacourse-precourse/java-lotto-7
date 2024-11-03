8e2837cf4f8615eed52b603e53dbd859c475f037
kcs19 <changsung19@gmail.com>
2024-11-03 18:39:45 +0900

    feat: 구입 금액 입력 기능 추가
    입력한 금액이 유효한지 체크하는 기능 추가

--- src/main/java/lottoController/GenerateLottoController.java
+++ src/main/java/lottoController/GenerateLottoController.java
@@ -1,4 +1,22 @@
 package lottoController;
 
+import view.InputView;
+
 public class GenerateLottoController {
+    private final int LOTTO_PRICE = 1000;
+
+    private boolean validateMoney(String inputMoney) {
+        try{
+            int validMoney = Integer.parseInt(inputMoney);
+
+            if(validMoney % LOTTO_PRICE != 0){
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력 가능합니다.");
+            }
+
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+
+        return true;
+    }
 }
