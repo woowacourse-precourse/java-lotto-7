aaf5e0b050a21577a92b14607883724b565dd563
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-03 15:19:51 +0900

    feat(BonusNumber): 로또 검증 로직 적용

--- src/main/java/lotto/domain/number/BonusNumber.java
+++ src/main/java/lotto/domain/number/BonusNumber.java
@@ -1,20 +1,44 @@
 package lotto.domain.number;
 
+import lotto.global.constant.LottoConstant;
+import lotto.global.exception.ErrorMessage;
+import lotto.global.exception.LottoException;
+
 import java.util.List;
 
 public class BonusNumber {
 
     private final int bonusNumber;
 
-    private BonusNumber(final int input) {
+    private BonusNumber(final int input, final WinningNumbers winningNumbers) {
+        Validator.validate(input, winningNumbers);
         this.bonusNumber = input;
     }
 
-    public static BonusNumber from(final int input) {
-        return new BonusNumber(input);
+    public static BonusNumber from(final int input, final WinningNumbers winningNumbers) {
+        return new BonusNumber(input, winningNumbers);
     }
 
     public boolean contains(final List<Integer> numbers) {
         return numbers.contains(bonusNumber);
+    }
+
+    private static class Validator {
+        private static void validate(final int number, final WinningNumbers winningNumbers) {
+            validateRange(number);
+            validateDuplication(number, winningNumbers);
+        }
+
+        private static void validateRange(final int number) {
+            if (number < LottoConstant.MIN_LOTTO_NUMBER || number > LottoConstant.MAX_LOTTO_NUMBER) {
+                throw LottoException.from(ErrorMessage.INVALID_LOTTO_RANGE);
+            }
+        }
+
+        private static void validateDuplication(final int number, final WinningNumbers winningNumbers) {
+            if (winningNumbers.contains(number)) {
+                throw LottoException.from(ErrorMessage.DUPLICATE_BONUS_NUMBER);
+            }
+        }
     }
 }
