3a6ead37fe1537dca2d491eaa8227f26e673a813
이희수 <hslee9234@naver.com>
2024-11-03 16:41:25 +0900

    feat: LottoGeneratorTest

---
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoGeneratorTest {
+
+    @Test
+    @DisplayName("로또 생성 테스트")
+    public void 로또_생성기는_요청한_갯수와_동일한_로또_갯수를_반환해야한다() {
+        // given
+        LottoGenerator generator = new DefaultLottoGenerator();
+        int count = 5;
+
+        // when
+        List<Lotto> lottos = generator.generateLottos(count);
+
+        // then
+        assertEquals(count, lottos.size());
+    }
+
+}
