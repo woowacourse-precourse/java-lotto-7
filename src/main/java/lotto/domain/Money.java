91fe4ec8ef2778f13202e011f3557499e9f77eca
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-03 15:55:19 +0900

    refactor(Money): 매직 넘버 상수화

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,12 +4,15 @@ import lotto.exception.LottoException;
 
 public record Money(int amount) {
 
+    private static final int MIN_AMOUNT = 0;
+    private static final int UNIT_AMOUNT = 1000;
+
     public Money {
         validate(amount);
     }
 
     private void validate(final int amount) {
-        if (amount < 0 || amount % 1000 != 0) {
+        if (amount < MIN_AMOUNT || amount % UNIT_AMOUNT != 0) {
             throw new LottoException("금액은 1000원으로 나누어 떨어지는 자연수여야 합니다.");
         }
     }
