a87902cca45a600e73d84e998ecb915e9aae8a81
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-11-03 23:54:14 +0900

    test: 로또 보너스  번호 유효성 검증을 위한 테스트 코드 추가
    - 보너스 번호가 당첨 번호 6개와 중복하는 경우에 대한 검증 테스트 추가
    - 보너스 번호가 범위를 벗어난 경우에 대한 테스트 추가
    - 보너스 번호가 숫자가 아닌 다른 문자들이 포함된 경우에 대한 테스트 추가
    - 보너스 번호가 빈 값이거나 엔터인 경우에 대한 테스트 추가
    - 보너스 번호 앞 뒤에 공백이 있는 경우에 대한 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -50,7 +53,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Nested
-    class IsValidPurchaseAmountTest {
+    class PurchaseAmountValidTest {
 
         @ParameterizedTest
         @ValueSource(strings = {"\n", "  "})
@@ -120,58 +123,98 @@ class ApplicationTest extends NsTest {
         }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5,6,7", "1,2,3,4", "1", "1,2,3,4,5,6,7,8,9"})
-    void 당첨_번호_6개_아닌_경우(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
+    @Nested
+    class WinningNumberValidTest {
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3,4,5,6,7", "1,2,3,4", "1", "1,2,3,4,5,6,7,8,9"})
+        void 당첨_번호_6개_아닌_경우(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3,3,3,6", "11,12,11,13,41,45"})
+        void 당첨_번호_내_중복된_값_존재(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"0,2,3,11,31,45", "1,12,37,25,41,99", "1,12,1000,25,41,99"})
+        void 당첨_번호_허용_범위_외_값이_존재(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,3.11.31$45", "1*12*37*25**41*45"})
+        void 당첨_번호_쉼표_외_구분자_사용(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1,2,,,3,4,5,6", "11,12,13,18,41,,45"})
+        void 당첨_번호_내_쉼표_연속_존재(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {",11,12,13,18,41,45", "1,2,3,4,5,6,"})
+        void 당첨_번호_쉼표로_시작하거나_끝나는_경우(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"\n", "   "})
+        void 당첨_번호가_빈_값_혹은_공백(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"11,1   2,13,18,4   1,45", " 1, 2  2 ,3, 4  2,5,6"})
+        void 당첨_번호_내_공백_존재(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {" 11,12 , 13 ,18,41 , 45", "   1,2,3,4   ,5,6   "})
+        void 당첨_번호_앞뒤_공백_허용_검증(String winningNumbers) {
+            assertThat(Application.isValidWinningNumbers(winningNumbers)).isTrue();
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,3,3,6", "11,12,11,13,41,45"})
-    void 당첨_번호_내_중복된_값_존재(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
+    @Nested
+    class BonusNumberValidTest {
+        @ParameterizedTest
+        @MethodSource("provideBonusNumberAndWinningNumbers")
+        void 보너스_번호_유효성검사_테스트(String bonusNumberStr, List<Integer> winningNumbers, boolean expected) {
+            // 실제 결과 호출
+            boolean result = Application.isValidBonusNumber(bonusNumberStr, winningNumbers);
 
-    @ParameterizedTest
-    @ValueSource(strings = {"0,2,3,11,31,45", "1,12,37,25,41,99", "1,12,1000,25,41,99"})
-    void 당첨_번호_허용_범위_외_값이_존재(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
+            // 예상 결과와 비교
+            assertThat(result).isEqualTo(expected);
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3.11.31$45", "1*12*37*25**41*45"})
-    void 당첨_번호_쉼표_외_구분자_사용(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,,,3,4,5,6", "11,12,13,18,41,,45"})
-    void 당첨_번호_내_쉼표_연속_존재(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {",11,12,13,18,41,45", "1,2,3,4,5,6,"})
-    void 당첨_번호_쉼표로_시작하거나_끝나는_경우(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"\n", "   "})
-    void 당첨_번호가_빈_값_혹은_공백(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"11,1   2,13,18,4   1,45", " 1, 2  2 ,3, 4  2,5,6"})
-    void 당첨_번호_내_공백_존재(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isFalse();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {" 11,12 , 13 ,18,41 , 45", "   1,2,3,4   ,5,6   "})
-    void 당첨_번호_앞뒤_공백_허용_검증(String winningNumbers) {
-        assertThat(Application.isValidwinningNumbers(winningNumbers)).isTrue();
+        // 테스트 케이스를 제공하는 메소드
+        static Stream<Arguments> provideBonusNumberAndWinningNumbers() {
+            return Stream.of(
+                    Arguments.of("7", List.of(1, 2, 3, 4, 5, 6), true),  // 정상케이스
+                    Arguments.of("10", List.of(1, 3, 5, 7, 9, 45), true), // 정상케이스
+                    Arguments.of("15", List.of(1, 3, 5, 7, 9, 11), true), // 정상케이스
+                    Arguments.of(" 6", List.of(1, 3, 5, 7, 9, 11), true), // 정상케이스 -> 앞 공백 포함
+                    Arguments.of("6 ", List.of(1, 3, 5, 7, 9, 11), true), // 정상케이스 -> 뒤 공백 포함
+                    Arguments.of(" 6 ", List.of(1, 3, 5, 7, 9, 11), true), // 정상케이스 -> 앞뒤 공백 포함
+                    Arguments.of("1", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 당첨 번호에 있는 케이스
+                    Arguments.of("0", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 범위를 벗어난 케이스
+                    Arguments.of("46", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 범위를 벗어난 케이스
+                    Arguments.of("100", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 범위를 벗어난 케이스
+                    Arguments.of("0,2,3,11,31,45", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("31$45", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("1*12*37*25**41*45", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("1,2,,,3,4,5,6", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("\n", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("   ", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("11,1", List.of(1, 3, 5, 7, 9, 11), false), // 보너스 번호가 잘못 들어온 케이스
+                    Arguments.of("1,45", List.of(1, 3, 5, 7, 9, 11), false) // 보너스 번호가 잘못 들어온 케이스
+            );
+        }
     }
 
     @Override
