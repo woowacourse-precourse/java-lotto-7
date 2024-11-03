b3773b4175eb270b24212b3b458652a838737d43
KJungW <sagakkb0707@naver.com>
2024-11-03 16:21:26 +0900

    test(model): WinningResult 테스트 추가
    테스트 내용
    - 당첨 타입 리스트를 받아 객체가
      제대로 생성되는지 테스트

---
+++ src/test/java/lotto/model/WinningResultTest.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.EnumMap;
+import java.util.List;
+import lotto.constant.WinningType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningResultTest {
+
+    @DisplayName("올바른 입력을 통한 객체 생성 시도")
+    @Test
+    void 올바른_입력을_통한_객체_생성_시도() {
+        List<WinningType> inputWinningTypes = List.of(
+                WinningType.FIRST,
+                WinningType.SECOND, WinningType.SECOND,
+                WinningType.THIRD, WinningType.THIRD, WinningType.THIRD,
+                WinningType.FORT, WinningType.FORT, WinningType.FORT, WinningType.FORT,
+                WinningType.FIFTH, WinningType.FIFTH, WinningType.FIFTH, WinningType.FIFTH, WinningType.FIFTH
+        );
+
+        WinningResult winningResult = new WinningResult(inputWinningTypes);
+
+        EnumMap<WinningType, Integer> countPerWinningType = winningResult.getCountPerWinningType();
+        assertThat(countPerWinningType.get(WinningType.FIRST)).isEqualTo(1);
+        assertThat(countPerWinningType.get(WinningType.SECOND)).isEqualTo(2);
+        assertThat(countPerWinningType.get(WinningType.THIRD)).isEqualTo(3);
+        assertThat(countPerWinningType.get(WinningType.FORT)).isEqualTo(4);
+        assertThat(countPerWinningType.get(WinningType.FIFTH)).isEqualTo(5);
+        assertThat(countPerWinningType.get(WinningType.NONE)).isEqualTo(0);
+    }
+}
