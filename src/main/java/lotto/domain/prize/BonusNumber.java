cc0691610dcd3bf64ed4a116737d6e40ba97099d
plan11plan <tjtfy7028@gmail.com>
2024-11-03 15:02:51 +0900

    feat(Prize): 보너스 번호 범위 검증(1~45)

--- src/main/java/lotto/domain/prize/BonusNumber.java
+++ src/main/java/lotto/domain/prize/BonusNumber.java
@@ -1,5 +1,10 @@
 package lotto.domain.prize;
 
+import static lotto.domain.ticket.LottoNumberRule.END_INCLUSIVE;
+import static lotto.domain.ticket.LottoNumberRule.START_INCLUSIVE;
+
+import lotto.domain.ticket.Lotto;
+
 public class BonusNumber {
     private final int value;
 
@@ -7,11 +12,23 @@ public class BonusNumber {
         this.value = value;
     }
 
-    public static BonusNumber of(int value) {
+    public static BonusNumber of(int value, Lotto lotto) {
+        validate(value, lotto);
         return new BonusNumber(value);
     }
 
     public int getValue() {
         return 0;
     }
+
+    private static void validate(int value, Lotto lotto) {
+        validateNumberRange(value);
+    }
+
+    private static void validateNumberRange(int value) {
+        if (value < END_INCLUSIVE.getValue() || value > START_INCLUSIVE.getValue()) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
 }
