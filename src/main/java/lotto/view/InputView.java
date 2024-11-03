eb0113b29e0a67b6ebea0bd59a558b3a636aab8c
이준범 <jach1206@naver.com>
2024-11-03 23:19:52 +0900

    refactor(view): 하드 코딩된 값을 모두 상수로 바꿈

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String GUIDE_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String GUIDE_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String GUIDE_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String NEWLINE = "\n";
+
+
     public String inputPurchase() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(GUIDE_PURCHASE_MESSAGE);
         return Console.readLine();
     }
 
     public String inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(GUIDE_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(NEWLINE + GUIDE_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
