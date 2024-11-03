7b4df31d55f17844c7917c358c278081970a874f
Seyoung Goo <0917sy@gmail.com>
2024-11-04 06:03:54 +0900

    feat: 로또 비교 로직 추가
    - 각 로또 번호를 다른 로또와 비교하여 일치하는 숫자 개수 확인
    - 로또가 특정 숫자를 포함하는지 확인

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -44,4 +44,14 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    public boolean contains(int value) {
+        return numbers.contains(value);
+    }
+
+    public int getMatchingCount(Lotto other) {
+        return (int) numbers.stream()
+                .filter(other::contains)
+                .count();
+    }
 }
