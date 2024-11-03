378b9e8acf350c123247a93dfc0d4c83d8399905
youjeans <dbyj2810@naver.com>
2024-11-03 23:07:56 +0900

    feat: 등수별 당첨 횟수 반환 기능

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,4 +1,63 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoResult {
+    private static int firstRankCount;
+    private static int secondRankCount;
+    private static int thirdRankCount;
+    private static int fourthRankCount;
+    private static int fifthRankCount;
+
+    public static void countFirstRank(List<Lotto> lottoSets, Lotto winningNumbers) {
+        firstRankCount = 0;
+        for (Lotto lotto : lottoSets) {
+            int matchingCount = lotto.countMatchingNumbersWithSameObject(winningNumbers);
+            if (matchingCount == 6) {
+                firstRankCount++;
+            }
+        }
+    }
+
+    public static void countSecondRank(List<Lotto> lottoSets, Lotto winningNumbers, List<Integer> bonusNumber) {
+        secondRankCount = 0;
+        for (Lotto lotto : lottoSets) {
+            int matchingCount = lotto.countMatchingNumbersWithSameObject(winningNumbers);
+            int matchingCountWithBonusNumber = lotto.countMatchingNumbersWithIntegerList(bonusNumber);
+            if (matchingCount == 5 && matchingCountWithBonusNumber == 1) {
+                secondRankCount++;
+            }
+        }
+    }
+
+    public static void countThirdRank(List<Lotto> lottoSets, Lotto winningNumbers, List<Integer> bonusNumber) {
+        thirdRankCount = 0;
+        for (Lotto lotto : lottoSets) {
+            int matchingCount = lotto.countMatchingNumbersWithSameObject(winningNumbers);
+            int matchingCountWithBonusNumber = lotto.countMatchingNumbersWithIntegerList(bonusNumber);
+            if (matchingCount == 5 && matchingCountWithBonusNumber == 0) {
+                thirdRankCount++;
+            }
+        }
+    }
+
+    public static void countFourthRank(List<Lotto> lottoSets, Lotto winningNumbers) {
+        fourthRankCount = 0;
+        for (Lotto lotto : lottoSets) {
+            int matchingCount = lotto.countMatchingNumbersWithSameObject(winningNumbers);
+            if (matchingCount == 4) {
+                fourthRankCount++;
+            }
+        }
+    }
+
+    public static void countFifthRank(List<Lotto> lottoSets, Lotto winningNumbers) {
+        fifthRankCount = 0;
+        for (Lotto lotto : lottoSets) {
+            int matchingCount = lotto.countMatchingNumbersWithSameObject(winningNumbers);
+            if (matchingCount == 3) {
+                fifthRankCount++;
+            }
+        }
+    }
 }
