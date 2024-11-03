955c2210c08315231e2468fc91dc7d1558615e43
seophohoho <seop0937@gmail.com>
2024-11-04 00:12:50 +0900

    refactor(Rank): enum을 배열처럼 쓰기 위해서 goal이 0인 값 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,14 +1,15 @@
 package lotto.model;
 
 public enum Rank {
-    EMPTY_0("0", 0, 1),
-    EMPTY_1("0", 0, 2),
+    EMPTY_0("0", 0, 0),
+    EMPTY_1("0", 0, 1),
+    EMPTY_2("0", 0, 2),
     FIFTH("5,000", 5000, 3),
     FOURTH("50,000", 50000, 4),
     THIRD("1,500,000", 1500000, 5),
     FIRST("2,000,000,000", 2000000000, 6),
     SECOND("30,000,000", 30000000, 5);
-    
+
     private final String strPrize;
     private final long prize;
     private final int goal;
