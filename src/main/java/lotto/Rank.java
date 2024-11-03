363515eab9aea3c3c23dbe2b662359f863c58725
Jungwoo Chu <cjw980930@gmail.com>
2024-11-03 18:04:17 +0900

    feat: 당첨 등수에 관한 enum 작성

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,32 @@
+package lotto;
+
+public enum Rank {
+    FIRST(6,false,2000000000),
+    SECOND(5,true,30000000),
+    THIRD(5,false,1500000),
+    FOURTH(4,false,50000),
+    FIFTH(3,false,5000);
+
+    private final int numberMatch;
+    private final boolean bonus;
+    private final int prize;
+
+    Rank(int numberMatch, boolean bonus, int prize){
+        this.numberMatch = numberMatch;
+        this.bonus = bonus;
+        this.prize = prize;
+    }
+
+    public int getNumberMatch(){
+        return numberMatch;
+    }
+    public boolean getBonus(){
+        return bonus;
+    }
+    public int getPrize(){
+        return prize;
+    }
+
+
+
+}
