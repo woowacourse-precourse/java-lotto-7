edf6af94360d7f08ea0f3568df232f177dc75598
hwangdaesun <hds650588@gmail.com>
2024-11-04 00:27:25 +0900

    test : Lotto 클래스로부터 LottoPrice를 조회하도록 수정

--- src/test/java/lotto/model/LottoSellerTest.java
+++ src/test/java/lotto/model/LottoSellerTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static lotto.model.Lotto.getLottoPrice;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,7 +25,7 @@ class LottoSellerTest {
     void 로또_구입_금액은_1000원_단위이다() {
         long money = 10000;
         List<Lotto> lottos = lottoSeller.sell(MoneyDTO.from(money));
-        assertThat(lottos.size()).isEqualTo(money / 1000);
+        assertThat(lottos.size()).isEqualTo(money / getLottoPrice());
     }
 
     @ParameterizedTest
