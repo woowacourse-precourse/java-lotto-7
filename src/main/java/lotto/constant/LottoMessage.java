5e010747c7b16ca97dd7338e2a68e96a911a35d2
minzzun99 <skg06123@naver.com>
2024-11-03 18:30:11 +0900

    feat(constant): 출력 메시지 상수 추가
    - 로또 범위 초과 에러 메시지 추가
    - 당첨 통계 관련 출력 메시지 추가

--- src/main/java/lotto/constant/LottoMessage.java
+++ src/main/java/lotto/constant/LottoMessage.java
@@ -5,7 +5,10 @@ public enum LottoMessage {
     PURCHASE_COUNT_MESSAGE("개를 구매했습니다."),
     INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS_MESSAGE("당첨 통계"),
+    WINNING_STATISTICS_MESSAGE("\n당첨 통계"),
+    COUNT_MESSAGE("개\n"),
+    TOTAL_RETURN_RATE_MESSAGE("총 수익률은 "),
+    END_MESSAGE("%입니다."),
     HORIZON("---"),
     NEW_LINE("\n");
 
