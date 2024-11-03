20d118c53f58c828b3cd19be072ca31dab10fa02
JiEung2 <181757@jnu.ac.kr>
2024-11-03 22:35:50 +0900

    feat(lottoRanking): 로또 기준표를 enum으로 선언해 통계와 비교하여 몇등인지 반환하는 기능 추가

---
+++ src/main/java/lotto/domain/LottoRanking.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRanking {
+    FIRST(6, false, "6개 일치 (2,000,000,000원)", 2_000_000_000),
+    SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    THIRD(5, false, "5개 일치 (1,500,000원)", 1_500_000),
+    FOURTH(4, false, "4개 일치 (50,000원)", 50_000),
+    FIFTH(3, false, "3개 일치 (5,000원)", 5_000);
+
+    private final int matchCount;
+    private final boolean matchBonus;
+    private final String message;
+    private final int prize;
+
+    LottoRanking(int matchCount, boolean matchBonus, String message, int prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.message = message;
+        this.prize = prize;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static LottoRanking of(int matchCount, boolean matchBonus) {
+        return Arrays.stream(values())
+                .filter(ranking -> ranking.matchCount == matchCount && ranking.matchBonus == matchBonus)
+                .findFirst()
+                .orElse(null);
+    }
+}
