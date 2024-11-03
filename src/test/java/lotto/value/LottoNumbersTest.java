6810c722c8cb5cfe99a2b1296c5d1066929e7339
JeongSu Park <bechalrang@gmail.com>
2024-11-03 20:58:02 +0900

    test(LottoNumbersTest): 동일 숫자 동등비교 테스트 케이스 추가

--- src/test/java/lotto/value/LottoNumbersTest.java
+++ src/test/java/lotto/value/LottoNumbersTest.java
@@ -12,7 +12,7 @@ class LottoNumbersTest {
     public static final String ERROR_HEADER = "[ERROR]";
 
     @Test
-    void 한줄의_로또번호를_생성한다() {
+    void 한줄의_로또숫자들을_생성한다() {
         assertThatCode(() -> LottoNumbers.of(List.of(1, 2, 3, 4, 5, 6)))
                 .doesNotThrowAnyException();
         assertThat(LottoNumbers.of(List.of(6, 2, 3, 4, 5, 1)))
@@ -27,6 +27,14 @@ class LottoNumbersTest {
         assertThatCode(() -> LottoNumbers.of(List.of(1, 1, 3, 4, 5, 6)))
                 .isInstanceOf(COMMON_EXCEPTION)
                 .hasMessageContaining(ERROR_HEADER);
+    }
+
+    @Test
+    void 같은_로또숫자들의_동등비교가_가능하다() {
+        LottoNumbers number1 = LottoNumbers.of(List.of(1, 2, 3, 4, 5, 6));
+        LottoNumbers number2 = LottoNumbers.of(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(number1).isEqualTo(number2);
+        assertThat(number1.equals(number2)).isTrue();
     }
 
 }
