28a6b9bc686453b65ee1c265489c614c4fe12b00
Parkjyun <jaeyun0507@naver.com>
2024-11-04 02:01:44 +0900

    refactor(LottoGenerator): 사용하지 않는 상수 제거

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    private static final int LOTTO_SIZE = 6;
     private final NumberGenerator numberGenerator;
 
     public LottoGenerator(NumberGenerator numberGenerator) {
