6b18a33a79b287385ffbde60a2d05966abd69bc2
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-03 22:42:02 +0900

    Refactor : LottoFactory 메소드 유효성 검사 추가
    지불액이 1000원 단위인지 검사

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -14,11 +14,19 @@ public class LottoFactory {
     }
 
     public Lottos generateLottos(long payment) {
+        validate(payment);
+        
         int count = (int) (payment / LOTTO_PRICE);
         List<Lotto> lottos = IntStream.range(0, count)
                 .mapToObj(i -> new Lotto(numberMachine.get()))
                 .toList();
 
         return new Lottos(lottos);
+    }
+
+    private void validate(long payment) {
+        if (payment % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] " + payment + "는 " + LOTTO_PRICE + "단위의 숫자가 아님");
+        }
     }
 }
