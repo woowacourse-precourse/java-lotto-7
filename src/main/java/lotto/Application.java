12e5874ceb754c46d1e2bedd2d20bbe7480e18a6
youngkwanglim <richard08@naver.com>
2024-11-03 19:36:27 +0900

    feat: 구매한 수량만큼 로또 발행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int purchaseAmount = getPurchaseAmount();
         int ticketCount = calculateTicketCount(purchaseAmount);
         printTicketCount(ticketCount);
+
+        List<Lotto> tickets = generateTickets(ticketCount);
     }
 
     private static int getPurchaseAmount() {
@@ -22,6 +27,14 @@ public class Application {
 
     private static void printTicketCount(int ticketCount) {
         System.out.println(ticketCount + "개를 구매했습니다.");
+    }
+
+    private static List<Lotto> generateTickets(int ticketCount) {
+        List<Lotto> tickets = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            tickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return tickets;
     }
 
 }
