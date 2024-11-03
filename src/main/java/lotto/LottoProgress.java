933da30d8d93c572e8265519307d210941942297
Taekgil <taekgil273@gmail.com>
2024-11-03 20:25:01 +0900

    feat: 로또 발급 로직 구현

---
+++ src/main/java/lotto/LottoProgress.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.List;
+import lotto.console.InputConsole;
+import lotto.console.OutputConsole;
+
+public class LottoProgress {
+
+    public static void lottoProgress() {
+        int inputMoney = InputConsole.intputMoney();
+        List<Lotto> lottoList = buyLotto(inputMoney);
+
+        Lotto winningLottoNumbers = InputConsole.inputWinningNumbers();
+        int bonusNum = InputConsole.inputBonusNumbers(winningLottoNumbers);
+
+        OutputConsole.outputWinningStatics(winningLottoNumbers, lottoList, bonusNum);
+        OutputConsole.outputProfit(inputMoney);
+    }
+
+    private static List<Lotto> buyLotto(int inputMoney) {
+        int lottoNum = LottoList.lottoNumber(inputMoney);
+        System.out.println("\n" + lottoNum + "개를 구매했습니다.");
+
+        List<Lotto> lottoList = LottoList.drawingLotto(lottoNum);
+        OutputConsole.outputLottoList(lottoList);
+
+        return lottoList;
+    }
+}
