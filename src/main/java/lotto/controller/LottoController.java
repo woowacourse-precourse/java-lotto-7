d08a83b3d6f2ffd614fbb3bae8821c731710cb86
sanikani <sanikani@naver.com>
2024-11-03 17:20:13 +0900

    feat: 당첨 로또 생성
    - 입력 값을 바탕으로 WinningLotto 인스턴스 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,11 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.model.BonusNumber;
+import lotto.model.Lotto;
 import lotto.model.PurchaseAmount;
+import lotto.model.WinningLotto;
+import lotto.util.Parser;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -17,5 +22,9 @@ public class LottoController {
     public void run() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(inputView.requestPurchaseAmount());
         outputView.printPurchaseAmount(purchaseAmount.getLottoQuantity());
+
+        List<Integer> winningNumbers = Parser.parseStringToList(inputView.requestWinningNumbers());
+        int bonusNumber = Parser.parseStringToInt(inputView.requestBonusNumber());
+        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), new BonusNumber(bonusNumber));
     }
 }
