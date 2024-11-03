dc9cf5334eb913438b2c02e64b2a4b7379758ec3
jinno Yun <jinno321@naver.com>
2024-11-03 16:00:16 +0900

    feat: 당첨번호 입력받고 그것을 반환해주는 메서드 생성

--- src/main/java/lotto/view/lottoView.java
+++ src/main/java/lotto/view/lottoView.java
@@ -4,10 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class lottoView {
 
-    public static void purchaseInput() {
+    public static String purchaseInput() {
         System.out.println("구입금액을 입력해 주세요.");
-        String s = Console.readLine();
+        String input = Console.readLine();
+        System.out.println();
+
+        return input;
     }
 
+    public static String winningInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println();
 
+        return input;
+    }
 }
