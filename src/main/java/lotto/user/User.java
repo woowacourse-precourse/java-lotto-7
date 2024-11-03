1413ab8249d62f587743162eab65d19f3ab96ff1
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-04 01:47:39 +0900

    feat : 유저 클래스 getter 함수 추가

--- src/main/java/lotto/user/User.java
+++ src/main/java/lotto/user/User.java
@@ -8,16 +8,24 @@ public class User {
     private final Integer purchaseCost;
     private final List<UserLottoInfo> lottoInfos;
 
-    public User(List<UserLottoInfo> lottoInfos , Integer purchaseCost) {
+    public User(List<UserLottoInfo> lottoInfos, Integer purchaseCost) {
         this.lottoInfos = lottoInfos;
         this.purchaseCost = purchaseCost;
+    }
+
+    public List<UserLottoInfo> getLottoInfos() {
+        return lottoInfos;
+    }
+
+    public Integer getPurchaseCost() {
+        return purchaseCost;
     }
 
     public static class UserLottoInfo {
         private final Lotto lotto;
         private final WinningStatus winningStatus;
 
-        public UserLottoInfo(Lotto lotto,WinningStatus winningStatus) {
+        public UserLottoInfo(Lotto lotto, WinningStatus winningStatus) {
             this.lotto = lotto;
             this.winningStatus = winningStatus;
         }
