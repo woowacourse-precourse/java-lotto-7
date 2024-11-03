2e48e6da15c9916d59e329d6188ecb63e910ab51
moonbinchoi <whsahd12@gmail.com>
2024-11-04 02:13:07 +0900

    refactor(Application): main 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.controller.LottoController;
+import lotto.domain.service.LottoService;
+import lotto.domain.util.parser.DelimitedNumberParser;
+import lotto.domain.util.parser.MoneyParser;
+import lotto.domain.util.parser.StringParser;
+import lotto.global.io.InputConsole;
+import lotto.global.io.OutputConsole;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringParser<List<Integer>> numberParser = DelimitedNumberParser.getInstance();
+        StringParser<Integer> moneyParser = MoneyParser.getInstance();
+        OutputConsole.init(System.out);
+        OutputConsole output = OutputConsole.getInstance();
+
+        LottoService.init(numberParser, moneyParser);
+        LottoService service = LottoService.getInstance();
+
+        try (InputConsole input = new InputConsole()) {
+            LottoController.init(service, input, output);
+            LottoController controller = LottoController.getInstance();
+
+            controller.play();
+        }
     }
 }
