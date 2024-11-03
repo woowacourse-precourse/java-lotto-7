ec476c39ab84a16de15a05e0ea3bef5285d7fadb
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:09:02 +0900

    feat(Application): 클래스 초기화 및 게임 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.manager.GameManager;
+import lotto.util.LottoNumberSupplier;
+import lotto.util.LottoNumberSupplierImpl;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        LottoNumberSupplier lottoNumberSupplier = new LottoNumberSupplierImpl();
+        GameManager gameManager = new GameManager(lottoNumberSupplier);
+
+        gameManager.run();
     }
 }
