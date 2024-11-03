11169eec00b98bb4c5b948b4229acf41935bb581
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 04:36:30 +0900

    style: 가독성위해 return문 수정

--- src/main/java/lotto/utils/parser/WinningNumbersParser.java
+++ src/main/java/lotto/utils/parser/WinningNumbersParser.java
@@ -11,15 +11,14 @@ public class WinningNumbersParser {
     public final static String COMMA_REGEX = ",";
 
     public static Lotto getWinningNumbers(String userWinningNumbers) {
-        List<Integer> winningNumbers;
         try {
-            winningNumbers = Arrays.stream(userWinningNumbers.split(COMMA_REGEX))
+            List<Integer> winningNumbers = Arrays.stream(userWinningNumbers.split(COMMA_REGEX))
                     .map(String::trim)
                     .map(Integer::valueOf)
                     .collect(Collectors.toList());
+            return new Lotto(winningNumbers);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(WINNING_NUMBER_IS_ONLY_INTEGER_ALLOWED.getMessage());
         }
-        return new Lotto(winningNumbers);
     }
 }
