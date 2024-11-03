facb853dec9dd0bec2c9305ee51a376339460a31
leeseongsil <leegwichan@naver.com>
2024-11-03 15:51:41 +0900

    feat(Application): 입력 중 예외 발생 시, 재입력 받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import lotto.domain.Lotties;
-import lotto.domain.Numbers;
 import lotto.domain.Number;
+import lotto.domain.Numbers;
 import lotto.domain.WinningNumbers;
 import lotto.domain.WinningRank;
 import lotto.view.InputView;
@@ -12,17 +14,52 @@ import lotto.view.OutputView;
 public class Application {
 
     public static void main(String[] args) {
-        int totalPrice = InputView.inputTotalPrice();
-        Lotties lotties = Lotties.from(totalPrice);
-
+        Lotties lotties = retryWhenThrowException(Application::inputLotties);
         OutputView.printLotties(lotties.getLottiesNumbers());
 
-        Numbers numbers = Numbers.from(InputView.inputWinningNumbers());
-        Number bonusNumber = new Number(InputView.inputBonusNumber());
-        WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
+        Numbers numbers = retryWhenThrowException(Application::inputWinningNumbers);
+        WinningNumbers winningNumbers = retryWhenThrowException(Application::inputBonusNumber, numbers);
 
+        printTotalResult(winningNumbers, lotties);
+    }
+
+    private static Lotties inputLotties() {
+        int totalPrice = InputView.inputTotalPrice();
+        return Lotties.from(totalPrice);
+    }
+
+    private static Numbers inputWinningNumbers() {
+        return Numbers.from(InputView.inputWinningNumbers());
+    }
+
+    private static WinningNumbers inputBonusNumber(Numbers winningNumbers) {
+        Number bonusNumber = new Number(InputView.inputBonusNumber());
+        return new WinningNumbers(winningNumbers, bonusNumber);
+    }
+
+    private static void printTotalResult(WinningNumbers winningNumbers, Lotties lotties) {
         Map<WinningRank, Long> result = winningNumbers.findResult(lotties);
         double profit = winningNumbers.calculateProfit(lotties);
         OutputView.printResult(result, profit);
+    }
+
+    private static <T> T retryWhenThrowException(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (RuntimeException exception) {
+                OutputView.printExceptionMessage(exception);
+            }
+        }
+    }
+
+    private static <T, R> R retryWhenThrowException(Function<T, R> function, T argument) {
+        while (true) {
+            try {
+                return function.apply(argument);
+            } catch (RuntimeException exception) {
+                OutputView.printExceptionMessage(exception);
+            }
+        }
     }
 }
