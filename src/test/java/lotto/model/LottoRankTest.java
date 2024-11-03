ba31f03055d30e4e665589b9e38b177016ef5640
Dohun Kim <dohun8758@naver.com>
2024-11-04 02:07:44 +0900

    test: ParameterizedTest 각 이름 표시

--- src/test/java/lotto/model/LottoRankTest.java
+++ src/test/java/lotto/model/LottoRankTest.java
@@ -9,29 +9,30 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoRankTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}: {3}")
     @MethodSource("lottoRankTestData")
     @DisplayName("로또 번호 매칭 횟수에 따른 등수 반환 테스트")
-    void 로또_번호_매칭_횟수에_따른_등수_반환_테스트(int matchingCount, boolean isBonusNumberMatched, LottoRank expectedRank) {
+    void 로또_번호_매칭_횟수에_따른_등수_반환_테스트(int matchingCount, boolean isBonusNumberMatched, LottoRank expectedRank,
+                                   String name) {
         Assertions.assertEquals(expectedRank, LottoRank.rankFrom(matchingCount, isBonusNumberMatched));
     }
 
     private static Stream<Arguments> lottoRankTestData() {
         return Stream.of(
-                Arguments.of(6, true, LottoRank.FIRST),
-                Arguments.of(6, false, LottoRank.FIRST),
-                Arguments.of(5, true, LottoRank.SECOND),
-                Arguments.of(5, false, LottoRank.THIRD),
-                Arguments.of(4, true, LottoRank.FOURTH),
-                Arguments.of(4, false, LottoRank.FOURTH),
-                Arguments.of(3, true, LottoRank.FIFTH),
-                Arguments.of(3, false, LottoRank.FIFTH),
-                Arguments.of(2, true, LottoRank.NONE),
-                Arguments.of(2, false, LottoRank.NONE),
-                Arguments.of(1, true, LottoRank.NONE),
-                Arguments.of(1, false, LottoRank.NONE),
-                Arguments.of(0, true, LottoRank.NONE),
-                Arguments.of(0, false, LottoRank.NONE)
+                Arguments.of(6, true, LottoRank.FIRST, "1등 테스트1"),
+                Arguments.of(6, false, LottoRank.FIRST, "1등 테스트2"),
+                Arguments.of(5, true, LottoRank.SECOND, "2등 테스트"),
+                Arguments.of(5, false, LottoRank.THIRD, "3등 테스트"),
+                Arguments.of(4, true, LottoRank.FOURTH, "4등 테스트1"),
+                Arguments.of(4, false, LottoRank.FOURTH, "4등 테스트2"),
+                Arguments.of(3, true, LottoRank.FIFTH, "5등 테스트1"),
+                Arguments.of(3, false, LottoRank.FIFTH, "5등 테스트2"),
+                Arguments.of(2, true, LottoRank.NONE, "순위권 이외 테스트1"),
+                Arguments.of(2, false, LottoRank.NONE, "순위권 이외 테스트2"),
+                Arguments.of(1, true, LottoRank.NONE, "순위권 이외 테스트3"),
+                Arguments.of(1, false, LottoRank.NONE, "순위권 이외 테스트4"),
+                Arguments.of(0, true, LottoRank.NONE, "순위권 이외 테스트5"),
+                Arguments.of(0, false, LottoRank.NONE, "순위권 이외 테스트6")
         );
     }
 
