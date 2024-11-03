5e32f263a1e9a73cd58acf6d4def50b42d3307f9
mikekks <thdalsrb79@naver.com>
2024-11-03 14:54:06 +0900

    feat(LottoMachine): 임의의 로또 목록 생성 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -2,21 +2,36 @@ package lotto;
 
 import static lotto.global.ErrorMessage.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoMachine {
 
-	private static final int MONEY_UNIT = 1000;
+	private static final int LOTTO_PRICE = 1000;
 
 	public List<Lotto> purchaseLottos(Money money) {
 		validatePurchaseMoneyUnit(money);
+		int lottoCount = money.getAmount() / LOTTO_PRICE;
 
+		List<Lotto> lottos = generateLottos(lottoCount);
 
+		return lottos;
 	}
 
 	private void validatePurchaseMoneyUnit(Money money) {
-		if (money.getAmount() % MONEY_UNIT != 0) {
+		if (money.getAmount() % LOTTO_PRICE != 0) {
 			throw new IllegalArgumentException(MONEY_UNIT_ERROR.getMessage());
 		}
+	}
+
+	private List<Lotto> generateLottos(int lottoCount) {
+		List<Lotto> lottos = new ArrayList<>();
+		for(int i = 0; i< lottoCount; i++) {
+			List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+			lottos.add(new Lotto(lottoNumbers));
+		}
+		return lottos;
 	}
 }
