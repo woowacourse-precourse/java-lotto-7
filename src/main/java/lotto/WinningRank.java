5bdc8f4ca439dee3bdb5c9ea47e3741dc6ba48f1
kim-su-mi <ksmm9607@naver.com>
2024-11-03 19:08:33 +0900

    refactor : 덩첨통계 순서  수정

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,11 +1,11 @@
 package lotto;
 
 public enum WinningRank {
-	 	FIRST(6, 2_000_000_000, "6개 일치"),
-	    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+	 	FIRST(3, 5_000, "3개 일치"),
+	    SECOND(4, 50_000, "4개 일치"),
 	    THIRD(5, 1_500_000, "5개 일치"),
-	    FOURTH(4, 50_000, "4개 일치"),
-	    FIFTH(3, 5_000, "3개 일치"),
+	    FOURTH(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+	    FIFTH(6, 2_000_000_000, "6개 일치"),
 	    NONE(0, 0, "미당첨");
 
 	    private final int matchCount;
@@ -19,19 +19,19 @@ public enum WinningRank {
 	    }
 
 	    public static WinningRank calculate(int matchCount, boolean matchBonus) {
-	        if (matchCount == 6) {
+	        if (matchCount == 3) {
 	            return FIRST;
 	        }
-	        if (matchCount == 5 && matchBonus) {
+	        if (matchCount == 4) {
 	            return SECOND;
 	        }
 	        if (matchCount == 5) {
 	            return THIRD;
 	        }
-	        if (matchCount == 4) {
+	        if (matchCount == 5 && matchBonus) {
 	            return FOURTH;
 	        }
-	        if (matchCount == 3) {
+	        if (matchCount == 6) {
 	            return FIFTH;
 	        }
 	        return NONE;
