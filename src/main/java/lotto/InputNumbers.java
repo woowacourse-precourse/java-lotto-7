dbd288d20b16d987021bb96f65a5922466b33552
Hujin0322 <hongyuj0322@naver.com>
2024-11-03 22:01:46 +0900

    feat(InputNumbers): 로또 당첨 번호 입력 기능 구현
    - InputNumbers 클래스: 로또 당첨 번호 입력 및 유효성 검사 기능 구현
    - inputWinningNumbers 메서드: 당첨 번호 6개 입력 기능 구현
    - inputBonusNumber 메서드: 보너스 번호 1개 입력 기능 구현

--- src/main/java/lotto/InputNumbers.java
+++ src/main/java/lotto/InputNumbers.java
@@ -5,43 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputNumbers {
-    public Lotto inputWinningNumbers(){
-        while (true){
-            try {
-                System.out.println("당첨 번호를 입력해 주세요. : ");
-                String input = Console.readLine().trim();
-                List<Integer> winningNumbers = parseInputToList(input);
-                return new Lotto(winningNumbers);
-            }catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            }
-        }
+    public Lotto inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요. : ");
+        String input = Console.readLine().trim();
+        List<Integer> winningNumbers = parseInputToList(input);
+        return new Lotto(winningNumbers);
     }
 
     public int inputBonusNumber() {
-        while (true) {
-            try {
-                System.out.println("보너스 번호를 입력해 주세요. : ");
-                String input = Console.readLine().trim();
-                int bonusNumber = parseBonusNumber(input);
-                return bonusNumber; // 보너스 번호 반환
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        System.out.println("보너스 번호를 입력해 주세요. : ");
+        String input = Console.readLine().trim();
+        int bonusNumber = Integer.parseInt(input);
+        return bonusNumber;
     }
-
 
     private List<Integer> parseInputToList(String input) {
-    List<Integer> numbers = new ArrayList<>();
-
-    if (!input.trim().isEmpty()) {
-        String[] splitNumbers = input.split(",");
-
-        for (String number : splitNumbers) {
-            numbers.add(Integer.parseInt(number.trim()));
-        }
+        // winningNumbers를 리스트로 변환 기능 구현
+        return new ArrayList<>();
     }
-    return numbers;
-    }
+
 }
