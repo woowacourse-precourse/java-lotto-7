3d0757c466f80fa9b5b8b67aa8fdeb368554b43a
soohalee <suha630@naver.com>
2024-11-03 17:45:11 +0900

    Test(UtilsTest): isInRange BigDecimal 버전 메소드 테스트 추가

--- src/test/java/lotto/utils/UtilsTest.java
+++ src/test/java/lotto/utils/UtilsTest.java
@@ -1,5 +1,6 @@
 package lotto.utils;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,11 +15,21 @@ public class UtilsTest {
         assertTrue(Utils.isDigitOnly("2147483647"));
     }
 
-    @DisplayName("숫자가 min ~ max 범위에 들어 오는지 확인한다")
+    @DisplayName("숫자가 min ~ max 범위에 들어 오는지 확인한다. [int 버전]")
     @Test
     void isInRange() {
         assertTrue(Utils.isInRange(1, 45, 1));
         assertTrue(Utils.isInRange(1, 45, 45));
         assertTrue(Utils.isInRange(1, 45, 10));
+    }
+
+    @DisplayName("숫자가 min ~ max 범위에 들어 오는지 확인한다. [BigDecimal 버전]")
+    @Test
+    void isInRangeBigDecimalType() {
+        BigDecimal min = new BigDecimal(1);
+        BigDecimal max = new BigDecimal(45);
+        BigDecimal num = new BigDecimal(10);
+
+        assertTrue(Utils.isInRange(min, max, num));
     }
 }
