9639597d8d76e03fc693c2a10db492546cb250ad
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 00:44:03 +0900

    refactor: 구매 이력 출력 포멧 수정

--- src/main/java/lotto/system/formater/PrintFormatter.java
+++ src/main/java/lotto/system/formater/PrintFormatter.java
@@ -14,7 +14,7 @@ import lotto.view.OutPutViewInterface;
 public class PrintFormatter {
 
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String LOTTO_TICKET_QUANTITY_MESSAGE = "%d개를 구매하셨습니다.";
+    private static final String LOTTO_TICKET_QUANTITY_MESSAGE = "%d개를 구매했습니다.";
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     
