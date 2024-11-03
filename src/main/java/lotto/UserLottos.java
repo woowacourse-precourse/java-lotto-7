bd9f638139d424f4313977d2895faecb431e61fd
btongtong <leeym2615@naver.com>
2024-11-04 02:20:29 +0900

    feat(UserLottos): Add method to calculate quantity

--- src/main/java/lotto/UserLottos.java
+++ src/main/java/lotto/UserLottos.java
@@ -2,15 +2,25 @@ package lotto;
 
 public class UserLottos {
     private final int purchaseAmount;
+    private final int quantity;
 
     public UserLottos(int purchaseAmount) {
         validatePurchaseAmount(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
+        this.quantity = calculateQuantity();
     }
 
     private void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount == 0 || purchaseAmount % LottoInfo.PRICE.getInfo() != 0) {
             throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_THOUSAND_UNIT_ERROR.getMessage());
         }
+    }
+
+    private int calculateQuantity() {
+        return this.purchaseAmount / LottoInfo.PRICE.getInfo();
+    }
+
+    public int getQuantity() {
+        return this.quantity;
     }
 }
