2c4b272b686ff41ef5e288207086a856532de09f
Parkjyun <jaeyun0507@naver.com>
2024-11-03 22:07:50 +0900

    feat(LottoGenerator): 로또를 생성하기 위한 전략을 가진다

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    NumberGenerator numberGenerator;
+
+    public LottoGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+}
---
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+
+    @Test
+    @DisplayName("로또 생성기를 생성할 수 있다.")
+    void should_CreateLottoGenerator_When_GivenGenerator() {
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        // when
+        LottoGenerator lottoGenerator = new LottoGenerator(numberGenerator);
+        // then
+        Assertions.assertThat(lottoGenerator).isNotNull();
+    }
+}
