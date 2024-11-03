3e38836d615c83d25b7d2b00b4b8bd7b9201f4ae
sanikani <sanikani@naver.com>
2024-11-03 17:17:50 +0900

    feat: 당첨 번호 입력
    - 당첨 번호와 보너스 번호 콘솔 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,9 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String requestPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
     }
+
+    public String requestWinningNumbers() {
+        System.out.println(INPUT_WINNING_LOTTO_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String requestBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
+
 }
