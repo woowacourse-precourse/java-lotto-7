d47903756a456019ed46d47b4747cbe12f292264
SeoMoonk <sunghoon1216@naver.com>
2024-11-03 10:58:23 +0900

    refactor: 구매 금액 유효성 검사 리팩토링 및 상수 관리
    구매 금액 유효성 검사 기능의 리팩토링 진행 및 2차 구현, 상수 별도 관리

---
+++ src/main/java/lotto/constant/LottoStatic.java
@@ -0,0 +1,6 @@
+package lotto.constant;
+
+public class LottoStatic {
+    public static final Long PURCHASE_AMOUNT_UNIT = 1000L;
+    public static final String ERROR_MSG_PREFIX = "[ERROR] ";   //TODO: 위치 이동?
+}
--- src/main/java/lotto/utils/LottoValidator.java
+++ src/main/java/lotto/utils/LottoValidator.java
@@ -1,40 +1,63 @@
 package lotto.utils;
 
+import static lotto.constant.LottoStatic.ERROR_MSG_PREFIX;
+import static lotto.constant.LottoStatic.PURCHASE_AMOUNT_UNIT;
+
 import java.math.BigInteger;
 
 public class LottoValidator {
 
-    private final String ERROR_MSG_PREFIX = "[ERROR]";
-
     public void validatePurchaseAmount(String inputPurchaseAmount) {
-
         BigInteger purchaseAmount;
 
-        if(inputPurchaseAmount.contains("+")) {
-            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "+ 기호가 포함된 입력은 불가합니다.");
-        }
+        plusSignValidate(inputPurchaseAmount);
+        blankValidate(inputPurchaseAmount);
+        decimalValidate(inputPurchaseAmount);
+        purchaseAmount = parsingValidate(inputPurchaseAmount);
+        greaterThanZeroValidate(purchaseAmount);
+        purchaseAmountUnitValidate(purchaseAmount);
+    }
 
-        if(inputPurchaseAmount.contains(" ") || inputPurchaseAmount.isBlank()) {
-            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "공백이 포함된 입력은 불가합니다.");
+    private void plusSignValidate(String input) {
+        if (input.contains("+")) {
+            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "+ 기호가 포함된 입력은 불가합니다." + input);
         }
+    }
 
-        if(inputPurchaseAmount.contains(".")) {
-            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "소수의 입력은 불가합니다.");
+    private void blankValidate(String input) {
+        if (input.contains(" ") || input.isBlank()) {
+            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "공백이 포함된 입력은 불가합니다." + input);
         }
+    }
 
-        try{
-            purchaseAmount = new BigInteger(inputPurchaseAmount);
+    private void decimalValidate(String input) {
+        if (input.contains(".")) {
+            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "소수의 입력은 불가합니다." + input);
+        }
+    }
+
+    //TODO: parsing과 Validate의 간극, 있어야 할 위치 고민
+    private BigInteger parsingValidate(String input) {
+        BigInteger parsedValue;
+
+        try {
+            parsedValue = new BigInteger(input);
         } catch (NumberFormatException e) {
             throw new NumberFormatException(ERROR_MSG_PREFIX + "숫자가 아닌 값은 입력할 수 없습니다.");
         }
 
-        if(purchaseAmount.compareTo(BigInteger.ZERO) <= 0) {
+        return parsedValue;
+    }
+
+    private void greaterThanZeroValidate(BigInteger value) {
+        if (value.compareTo(BigInteger.ZERO) <= 0) {
             throw new IllegalArgumentException(ERROR_MSG_PREFIX + "0 이하의 값은 입력할 수 없습니다.");
         }
+    }
 
-        //FIXME: 1000원 하드코딩 X
-        if(!purchaseAmount.remainder(BigInteger.valueOf(1000L)).equals(BigInteger.ZERO)) {
-            throw new IllegalArgumentException(ERROR_MSG_PREFIX + "1000원 단위의 입력만 가능합니다.");
+    private void purchaseAmountUnitValidate(BigInteger purchaseAmount) {
+        if (!purchaseAmount.remainder(BigInteger.valueOf(PURCHASE_AMOUNT_UNIT)).equals(BigInteger.ZERO)) {
+            throw new IllegalArgumentException(ERROR_MSG_PREFIX + PURCHASE_AMOUNT_UNIT + "원 단위의 입력만 가능합니다.");
         }
     }
 }
