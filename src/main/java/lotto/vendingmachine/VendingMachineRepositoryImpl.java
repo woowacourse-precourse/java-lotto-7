88be3f5cccf568882556871da159b42a6dab4745
woong2e <tlswodnd12345@naver.com>
2024-11-03 15:41:04 +0900

    feat: implement store statistics and get functionality

--- src/main/java/lotto/vendingmachine/VendingMachineRepositoryImpl.java
+++ src/main/java/lotto/vendingmachine/VendingMachineRepositoryImpl.java
@@ -1,11 +1,16 @@
 package lotto.vendingmachine;
 
+import lotto.draw.Rank;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VendingMachineRepositoryImpl implements VendingMachineRepository {
 
     private static List<Lotto> lottoStorage = new ArrayList<>();
+    private static Map<Rank, Integer> lottoStatistics = new HashMap<>();
 
     @Override
     public void save(List<Lotto> lottos) {
@@ -13,7 +18,16 @@ public class VendingMachineRepositoryImpl implements VendingMachineRepository {
     }
 
     @Override
+    public void saveWinningStatistics(Map<Rank, Integer> statistics) {
+        lottoStatistics = statistics;
+    }
+
+    @Override
     public List<Lotto> getStoredLottoTickets() {
         return lottoStorage;
+    }
+
+    public Map<Rank, Integer> getWinningStatistics() {
+        return lottoStatistics;
     }
 }
