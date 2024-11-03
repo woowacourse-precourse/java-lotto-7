7013f81a3ed5d21f1adb83c40a2778546f571f20
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-04 00:43:11 +0900

    feat(service): 보너스 검증 기능, 보상 계산 기능 구현
    - 보너스 번호는 숫자만 가능
    - 당첨 번호와 구매한 로또의 숫자를 비교하여 보상을 계산

--- src/main/java/lotto/model/service/LottoService.java
+++ src/main/java/lotto/model/service/LottoService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import lotto.model.domain.BonusNumber;
 import lotto.model.domain.Lotto;
 import lotto.model.domain.LottoMachine;
+import lotto.model.domain.LottoPrize;
+import lotto.model.domain.LottoWinningNumbers;
+import lotto.model.domain.Pocket;
 
 public class LottoService {
 
@@ -19,24 +22,55 @@ public class LottoService {
         return lottoMachine.multipleLottoGenerator();
     }
 
-    public List<Integer> winningNumbersGenerator(String inputWinningNumbers) {
+    public Lotto winningNumbersGenerator(String inputWinningNumbers) {
         winningNumbersNotNullValidator(inputWinningNumbers);
-        return winningNumbersParser(inputWinningNumbers);
+        return new Lotto(winningNumbersParser(inputWinningNumbers));
     }
-
 
     public BonusNumber bonusNumberGenerator(String bonusNumber) {
         bonusNumbersNotNullValidator(bonusNumber);
-        return bonusNumberGenerator(bonusNumber);
+        int bonusNumericNumber = bonusNumberNumericValidator(bonusNumber);
+        return new BonusNumber(bonusNumericNumber);
     }
+
+    private int bonusNumberNumericValidator(String inputBonusNumber) {
+        try {
+            return Integer.parseInt(inputBonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야합니다.");
+        }
+    }
+
+    public void calculateReward(LottoWinningNumbers lottoWinningNumbers, Pocket pocket, LottoPrize lottoPrize) {
+        for (Lotto lotto : pocket.getLottos()) {
+            int matchNumberCount = 0;
+            boolean isMatchBonus = false;
+            for (int number : lotto.getNumbers()) {
+                matchNumberCount = checkMatchNumber(lottoWinningNumbers, number, matchNumberCount);
+                isMatchBonus = checkMatchBonus(lottoWinningNumbers, number, isMatchBonus);
+            }
+            lottoPrize.updateReward(matchNumberCount, isMatchBonus);
+        }
+    }
+
+    private int checkMatchNumber(LottoWinningNumbers lottoWinningNumbers, int number, int matchNumberCount) {
+        if (lottoWinningNumbers.isContainsWinningNumbers(number)) {
+            matchNumberCount++;
+        }
+        return matchNumberCount;
+    }
+
+    private boolean checkMatchBonus(LottoWinningNumbers lottoWinningNumbers, int number, boolean isMatchBonus) {
+        if (!isMatchBonus && lottoWinningNumbers.isContainsBonusNumber(number)) {
+            isMatchBonus = true;
+        }
+        return isMatchBonus;
+    }
+
     private void bonusNumbersNotNullValidator(String bonusNumber) {
         if (bonusNumber.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호가 입력되지 않았습니다.");
         }
-    }
-
-    public void setWinningNumbers(List<Integer> winningNumbers, BonusNumber bonusNumber) {
-
     }
 
     private void moneyNotNullValidator(String inputMoney) {
@@ -60,12 +94,10 @@ public class LottoService {
     }
 
     private List<Integer> winningNumbersParser(String inputWinningNumbers) {
-        List<String> splitLottoMainNumbers = Arrays.stream(inputWinningNumbers.split(",", -1)).toList();
-        List<Integer> winningNumbers = new ArrayList<>();
-        for (String splitNumber : splitLottoMainNumbers) {
-            winningNumbers.add(winningNumbersNumericValidator(splitNumber));
-        }
-        return winningNumbers;
+        return Arrays.stream(inputWinningNumbers.split(",",-1))
+                .map(String::trim)
+                .map(this::winningNumbersNumericValidator)
+                .toList();
     }
 
     private int winningNumbersNumericValidator(String inputWinningNumbers) {
