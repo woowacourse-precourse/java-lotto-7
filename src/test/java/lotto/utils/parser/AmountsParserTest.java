e9deb907075732f6680b14ef6823f59c06f77a30
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-03 22:57:57 +0900

    fix: 예외 테스트 시 올바른 클래스명호출 하도록 수정

--- src/test/java/lotto/utils/parser/AmountsParserTest.java
+++ src/test/java/lotto/utils/parser/AmountsParserTest.java
@@ -11,28 +11,28 @@ import org.junit.jupiter.params.provider.ValueSource;
 class AmountsParserTest {
 
     @ParameterizedTest
-    @DisplayName("1000원보다 적은 금액이 입력되면, 예외 처리된다.")
+    @DisplayName("1000원보다 적은 금액이 입력되면, 예외가 발생한다.")
     @ValueSource(strings = "999")
     void Given_AmountsAreOverMinAmounts_When_CheckMinAmounts_Then_Error(String input) {
-        assertThatThrownBy(() -> AmountsValidator.validateLottoAmount(input))
+        assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.UNDER_MIN_AMOUNTS.getMessage());
     }
 
     @ParameterizedTest
-    @DisplayName("10만원이 넘는 금액이 입력되면, 예외 처리된다.")
+    @DisplayName("10만원이 넘는 금액이 입력되면, 예외가 발생한다.")
     @ValueSource(strings = "100001")
     void Given_AmountsAreOverMaxAmounts_When_CheckMaxAmounts_Then_Error(String input) {
-        assertThatThrownBy(() -> AmountsValidator.validateLottoAmount(input))
+        assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.OVER_MAX_AMOUNTS.getMessage());
     }
 
     @ParameterizedTest
-    @DisplayName("1000원 단위가 아닌 금액이 입력되면, 예외 처리된다.")
+    @DisplayName("1000원 단위가 아닌 금액이 입력되면, 예외가 발생한다.")
     @ValueSource(strings = "1100")
     void Given_AmountsCanNotDivideByThousand_When_CheckDivisibilityByThousand_Then_Error(String input) {
-        assertThatThrownBy(() -> AmountsValidator.validateLottoAmount(input))
+        assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.IS_NOT_DIVISIBLE_BY_THOUSAND_WON.getMessage());
     }
