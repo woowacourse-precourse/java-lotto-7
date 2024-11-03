d11eaaae7ec8aa9f2f5f8bf1700230e8e383b394
tjdus <dusml0277@gmail.com>
2024-11-04 03:35:57 +0900

    feat(lotto): add validation

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -53,6 +55,55 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
+    @Test
+    void 금액이_1000원_단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> Application.validateAmount(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+    }
+
+    @Test
+    void 올바른_금액을_입력했을_때_예외가_발생하지_않는다() {
+        assertDoesNotThrow(() -> Application.validateAmount(3000));
+    }
+    @Test
+    void 문자열을_정수로_변환할_때_숫자가_아닌_값을_입력하면_예외가_발생한다() {
+        assertThatThrownBy(() -> Application.convertToInteger("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력해야 합니다.");
+    }
+
+    @Test
+    void 중복된_로또_번호가_있는_경우_예외가_발생한다() {
+        Integer[] duplicateNumbers = {1, 2, 3, 4, 5, 5};
+        assertThatThrownBy(() -> Application.validateLottoNumbers(duplicateNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 중복된 번호가 있습니다.");
+    }
+
+    @Test
+    void 로또_번호가_6개가_아닐_경우_예외가_발생한다() {
+        String[] input = {"1", "2", "3", "4", "5"};
+        assertThatThrownBy(() -> Application.validateStringLength(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 번호 6개를 입력해야 합니다.");
+    }
+
+    @Test
+    void 유효_범위_밖의_로또_번호가_있는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> Application.validateLottoNumber(50))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @Test
+    void 랜덤_로또_번호_생성_시_6개의_정렬된_숫자가_생성된다() {
+        List<Integer> randomNumbers = Application.generateRandomNumbers();
+        assertThat(randomNumbers).hasSize(6);
+        assertThat(randomNumbers).isSorted();
+    }
+
 
     @Override
     public void runMain() {
