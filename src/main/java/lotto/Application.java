c5f37ff136ac48e2455a16df907f967c4bbfe171
sejinezy <psj8880@naver.com>
2024-11-04 01:56:06 +0900

    refactor(Application): import문 추가
    - LottoMachine 클래스의 패키지 변동으로 인한 import문 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.service.LottoMachine;
+
 public class Application {
     public static void main(String[] args) {
         LottoMachine lottoMachine = new LottoMachine();
