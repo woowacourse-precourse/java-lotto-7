7ae9e3f695bc0afa29f956689643aa2a1338aed7
chyo1 <hyowon3856@gmail.com>
2024-11-04 02:37:33 +0900

    feat : 당첨 정보(일치 갯수, 상금 등)를 가진 Enum Rank 추가

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public enum Rank {
+    THREE(3, false, 5_000, "3개 일치 (5,000원)"),
+    FOUR(4, false, 50_000, "4개 일치 (50,000원)"),
+    FIVE(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    FIVE_AND_BONUS(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)"),
+    ETC(-1, false, 0, "당첨권 밖");
+    ;
+
+    private int matchingCount;
+    private boolean isBonusMatched;
+    private int prizeAmount;
+    private String printMessage;
+
+    Rank(int matchingCount, boolean isBonusMatched, int prizeAmount, String printMessage) {
+        this.matchingCount = matchingCount;
+        this.isBonusMatched = isBonusMatched;
+        this.prizeAmount = prizeAmount;
+        this.printMessage = printMessage;
+    }
+}
