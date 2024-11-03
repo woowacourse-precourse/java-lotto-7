13f81e71a34d68b5938661aace4e1048aceaf9ae
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:46:36 +0900

    chore(OutputView) : 커맨드라인 자동 정렬

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -11,44 +11,44 @@ public class OutputView {
     public static final String LOTTO_BONUS_INPUT = "보너스 번호를 입력해 주세요.";
     public static final String NUM = "개";
 
-    public static void printPurchaseAmount(){
+    public static void printPurchaseAmount() {
         System.out.println(LOTTO_AMOUNT_PHRASE);
     }
 
-    public static void printCount(Integer lottoCount){
+    public static void printCount(Integer lottoCount) {
         System.out.println(lottoCount + LOTTO_COUNT_PHRASE);
     }
 
-    public static void printWinning(){
+    public static void printWinning() {
         System.out.println(LOTTO_WINNING_INPUT);
     }
 
-    public static void printBonus(){
+    public static void printBonus() {
         System.out.println(LOTTO_BONUS_INPUT);
     }
 
-    public static void printBlank(){
+    public static void printBlank() {
         System.out.println();
     }
 
-    public static void printReturn(double myReturn){
+    public static void printReturn(double myReturn) {
         System.out.println("총 수익률은 " + myReturn + "%입니다.");
     }
 
-    public static void printError(String error){
+    public static void printError(String error) {
         System.out.println(error);
     }
 
-    public static void printLotto(Lotto lotto){
+    public static void printLotto(Lotto lotto) {
         System.out.println(lotto.getNumbers());
     }
 
-    public static void printResults(WinningDetails grades){
+    public static void printResults(WinningDetails grades) {
         System.out.println(StateMessage.WIN_STATICS.getMessage());
         System.out.println(StateMessage.THREE.getMessage() + grades.getThird() + NUM);
         System.out.println(StateMessage.FOUR.getMessage() + grades.getFourth() + NUM);
         System.out.println(StateMessage.FIVE.getMessage() + grades.getFifth() + NUM);
-        System.out.println(StateMessage.FIVE_BONUS.getMessage() +grades.getFifthBonus() + NUM);
+        System.out.println(StateMessage.FIVE_BONUS.getMessage() + grades.getFifthBonus() + NUM);
         System.out.println(StateMessage.SIX.getMessage() + grades.getSixth() + NUM);
     }
 
