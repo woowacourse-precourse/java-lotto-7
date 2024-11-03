f0ab5b4142f92f2dd81b3a9deec209347d1ad55c
haebyun <bhk4392@gmail.com>
2024-11-03 22:54:01 +0900

    feat(LottoConstants): 로또 관련 상수 모음 클래스 추가
    - MIN_LOTTO_NUMBER: 로또 번호의 최소값 상수 추가
    - MAX_LOTTO_NUMBER: 로또 번호의 최대값 상수 추가
    - LOTTO_PRICE: 로또 한 장의 가격 상수 추가
    - LOTTO_DEFAULT_COUNT: 기본 로또 번호 개수 상수 추가
    - 클래스 인스턴스화 방지를 위한 private 생성자 추가

---
+++ src/main/java/lotto/domain/constants/LottoConstants.java
@@ -0,0 +1,10 @@
+package lotto.domain.constants;
+
+public final class LottoConstants {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_DEFAULT_COUNT = 6;
+
+    private LottoConstants() {}
+}
