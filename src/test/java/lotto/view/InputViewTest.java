5a33f13b2e00177dd2565c803c6d9351a6cc6a80
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 00:40:32 +0900

    test(InputViewTest): 입력 문자열에 대한 예외 처리 테스트 코드 확장
    - WinnerLottoNumbersInput`, `BonusLottoNumBerInput`에 대해 `null` 및 빈 문자열 입력 시 예외 발생 테스트 추가

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -3,7 +3,9 @@ package lotto.view;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import lotto.dto.BonusLottoNumBerInput;
 import lotto.dto.LottoPurchasedAmountInput;
+import lotto.dto.WinnerLottoNumbersInput;
 import lotto.exception.ErrorMessage;
 import lotto.exception.LottoException;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,8 @@ public class InputViewTest {
     void 입력된_문자열이_null_이면_예외가_발생한다() {
         LottoException exception = assertThrows(LottoException.class, () -> {
             LottoPurchasedAmountInput.from(null);
+            WinnerLottoNumbersInput.from(null);
+            BonusLottoNumBerInput.from(null);
         });
         assertEquals(ErrorMessage.NULL_INPUT_ERROR.getMessage(), exception.getMessage());
     }
@@ -22,6 +26,8 @@ public class InputViewTest {
     void 입력된_문자열이_빈_문자열이면_예외가_발생한다() {
         LottoException exception = assertThrows(LottoException.class, () -> {
             LottoPurchasedAmountInput.from("");
+            WinnerLottoNumbersInput.from("");
+            BonusLottoNumBerInput.from("");
         });
         assertEquals(ErrorMessage.EMPTY_INPUT_ERROR.getMessage(), exception.getMessage());
     }
