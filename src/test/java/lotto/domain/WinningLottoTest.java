98f421dcb68cfbbbe8aa48f4b3dc5dca1a4414fc
ratatuwee <pkqwaszx@gmail.com>
2024-11-03 19:13:49 +0900

    test(winning lotto) : 당첨 번호 도메인 테스트 추가
    당첨 번호를 보관하는 도메인 테스트를 추가하였습니다.
    최초 객체를 생성할 때에만 당첨 번호와 보너스 번호를 입력하여 수정하지 않고 하나의 객체를 이용하도록하여 리소스를 절약하며 데이터를 일관성 있게 유지하기 위하여 싱글톤으로 설계하였습니다.
    도메인에 값이 들어올 때 유효한지 검증을 최종적으로 진행하며 뷰, 컨트롤러와 책임을 분리하였습니다.

---
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,90 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class WinningLottoTest {
+    @BeforeEach
+    void setupTestEnvironment() {
+        System.setProperty("IS_TEST_ENV", "true");
+    }
+
+    @AfterEach
+    void resetTestEnvironment() {
+        System.clearProperty("IS_TEST_ENV");
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호 중복 예외 확인")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void checkDuplicateWinningNumbersError(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = Arrays.asList(1, 2, 3, 4, 5);
+        test.add(testNum)
+        String errorMessage = "[ERROR] 당첨 번호에 중복된 번호가 있습니다.";
+
+        assertThatThrownBy(() -> WinningLotto.getPurchaseAmount(test, 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 중복 예외 확인")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void checkDuplicateBonusNumbersError(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = Arrays.asList(1, 6, 2, 3, 4, 5);
+        String errorMessage = "[ERROR] 당첨 번호에 중복된 번호가 있습니다.";
+
+        assertThatThrownBy(() -> WinningLotto.getPurchaseAmount(test, testNum))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨 번호 범위 예외 확인")
+    @ValueSource(ints = {0, 46, 100, -1})
+    void checkBoundaryWinningNumbersError(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = Arrays.asList(1, 2, 3, 4, 5);
+        test.add(testNum)
+        String errorMessage = "[ERROR] 당첨 번호가 범위를 벗어나는 번호가 있습니다.";
+
+        assertThatThrownBy(() -> WinningLotto.getPurchaseAmount(test, 7))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 범위 예외 확인")
+    @ValueSource(ints = {0, 46, 100, -1})
+    void checkBoundaryBonusNumbersError(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = Arrays.asList(1, 6, 2, 3, 4, 5);
+        String errorMessage = "[ERROR] 당첨 번호가 범위를 벗어나는 번호가 있습니다.";
+
+        assertThatThrownBy(() -> WinningLotto.getPurchaseAmount(test, testNum))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("정상 동작 확인")
+    @ValueSource(ints = {1, 45, 10, 30})
+    void checkWinningLotto(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = Arrays.asList(3, 20, 33, 13, 44);
+        test.add(testNum);
+
+        assertDoesNotThrow(() -> WinningLotto.getPurchaseAmount(test, 9));
+    }
+}
