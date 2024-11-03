1ece9deafed0b54b114a383edaa0b81820cf7f2b
tentenball <lth8905@gmail.com>
2024-11-03 19:04:40 +0900

    refactor: 패키지 이동

--- src/main/java/lotto/domains/lotto/domain/Lotto.java
+++ src/main/java/lotto/domains/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto.domains.lotto;
+package lotto.domains.lotto.domain;
 
 import java.util.HashSet;
 import java.util.List;
--- src/main/java/lotto/domains/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domains/lotto/domain/LottoTicket.java
@@ -1,4 +1,4 @@
-package lotto.domains.lotto;
+package lotto.domains.lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
