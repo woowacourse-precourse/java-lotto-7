effd69e0245f25336df6b7cfe0fe497c47c1b438
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:57:24 +0900

    feat: WinningMoney 클래스 구현

--- src/main/java/lotto/model/WinningMoney.java
+++ src/main/java/lotto/model/WinningMoney.java
@@ -1,4 +1,26 @@
 package lotto.model;
 
+import java.util.List;
+
+import lotto.Rank;
+
 public class WinningMoney {
+
+    private final List<Rank> result;
+    private int totalReward = 0;
+
+    public WinningMoney(List<Rank> result) {
+        this.result = result;
+        calculate();
+    }
+
+    private void calculate() {
+        for (Rank rank : result) {
+            totalReward += rank.getReward();
+        }
+    }
+
+    public int getTotalReward() {
+        return totalReward;
+    }
 }
