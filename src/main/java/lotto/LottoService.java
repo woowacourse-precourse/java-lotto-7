db63e9f5907fa343db5c8b91d4757ed0895431d7
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-03 21:04:08 +0900

    feat: 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또를 발행해야 한다.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -8,19 +8,16 @@ public class LottoService {
     private final static int LOTTO_MIN_NUMBER = 1;
     private final static int LOTTO_MAX_NUMBER = 45;
 
-    public List<Lotto> buyLottos(int lottoCount){
+    public List<Lotto> buyLottos(int lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
-        while(lottoCount-->0){
+        while (lottoCount-- > 0) {
             lottos.add(new Lotto(makeLandomNumber()));
         }
         return lottos;
     }
 
-    private List<Integer> makeLandomNumber(){
+    private List<Integer> makeLandomNumber() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, 6);
     }
 
-    public double getYield(int useMoney, int winningMoney){
-        return  Math.round(useMoney/winningMoney * 10) / 10.0;
-    }
 }
