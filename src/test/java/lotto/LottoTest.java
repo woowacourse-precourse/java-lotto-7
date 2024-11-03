210dd94c2359726eea8cc98397605b745946f424
song hyeongyu <lit3207@ajou.ac.kr>
2024-11-04 01:18:20 +0900

    test: revise LottoTest

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,13 +21,5 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-
-    @DisplayName("로또 번호에 범위 외 숫자가 존재하면 예와가 발생한다.")
-    @Test
-    void 로또_번호에_1이하_45이상_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 70)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 }
