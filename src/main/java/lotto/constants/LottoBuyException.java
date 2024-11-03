3ad4a239945cb799ba30221282f358629a349566
윤민수 <koelysa5696@naver.com>
2024-11-03 17:47:22 +0900

    Fix: [ERROR] 누락된거 추가

--- src/main/java/lotto/constants/LottoBuyException.java
+++ src/main/java/lotto/constants/LottoBuyException.java
@@ -1,9 +1,9 @@
 package lotto.constants;
 
 public enum LottoBuyException {
-    INVALID_POSITIVE_INPUT("양의 정수 입력이 아닙니다."),
-    NEGATIVE_NUMBER_NOT_ALLOWED("음수는 입력할 수 없습니다."),
-    INVALID_AMOUNT_MULTIPLE("금액은 천의 배수 단위로 입력하세요.");
+    INVALID_POSITIVE_INPUT("[ERROR] 문자, 소수, 2^31-1 이상의 정수는 입력 불가합니다."),
+    NEGATIVE_NUMBER_NOT_ALLOWED("[ERROR] 음수는 입력할 수 없습니다."),
+    INVALID_AMOUNT_MULTIPLE("[ERROR] 금액은 천의 배수 단위로 입력하세요.");
 
     private final String message;
 
