6c25b0096aa22e6486a09f5775ae95c7e45aa9a4
xeimun <hinote444@naver.com>
2024-11-03 19:49:52 +0900

    feat(LottoService): 당첨 결과 저장 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,8 +4,21 @@ import static java.lang.Integer.parseInt;
 
 import lotto.Lotto;
 import lotto.LottoRank;
+import lotto.LottoResult;
+import lotto.Lottos;
 
 public class LottoService {
+
+    public LottoResult calculateLottoResult(Lottos lottos, Lotto userLotto, int bonusNumber) {
+        LottoResult lottoResult = new LottoResult();
+
+        lottos.getLottos()
+              .stream()
+              .map(lotto -> calculateLottoRank(lotto, userLotto, bonusNumber))
+              .forEach(lottoResult::addRankCount);
+
+        return lottoResult;
+    }
 
     private LottoRank calculateLottoRank(Lotto lotto, Lotto userLotto, int bonusNumber) {
         int matchCount = lotto.countMatchingNumbers(userLotto);
