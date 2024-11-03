4a65588a91efc33fdcfd08a3df2811a5e890f1b0
plan11plan <tjtfy7028@gmail.com>
2024-11-04 03:56:53 +0900

    feat(StatisticsOutputView): 당첨 통계 시작 메시지 출력
    - 당첨 통계 시작 구분선 출력 ("---")
    - 당첨 통계 제목 출력

--- src/main/java/lotto/application/statistics/view/StatisticsOutputView.java
+++ src/main/java/lotto/application/statistics/view/StatisticsOutputView.java
@@ -1,9 +1,27 @@
 package lotto.application.statistics.view;
 
-import lotto.usecase.StatisticsResponse;
+import lotto.application.statistics.dto.StatisticsResponse;
+import lotto.application.ticket.view.output.OutputPrinter;
 
 public class StatisticsOutputView {
-    public void show(StatisticsResponse response) {
+    private static final String DIVISION_LINE = "---";
+    private static final String TITLE = "당첨 통계";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
 
+    private final OutputPrinter printer;
+
+    public StatisticsOutputView() {
+        this.printer = new OutputPrinter();
     }
+
+    public void show(StatisticsResponse response) {
+        appendHeader();
+        printer.execute();
+    }
+
+    private void appendHeader() {
+        printer.appendLine(DIVISION_LINE);
+        printer.appendLine(TITLE);
+    }
+
 }
