7ef7a6ead3796647d57819ecee5286b07495030e
Chan531 <chan657312@gmail.com>
2024-11-03 18:58:12 +0900

    refactor(Application): LottoCreator 객체 생성 및 전달

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.model.LottoCreator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,7 +9,8 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        LottoController lottoController = new LottoController(inputView, outputView);
+        LottoCreator lottoCreator = new LottoCreator();
+        LottoController lottoController = new LottoController(inputView, outputView, lottoCreator);
         lottoController.run();
     }
 }
