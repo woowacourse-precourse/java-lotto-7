2155215ccf922995d446ac1728ec0d8a7cd50fd8
TalkingPotato90 <star901210@hanmail.net>
2024-11-04 01:57:08 +0900

    feat: 정수형 변환 과정에서 발생하는 NumberFormatException 일괄 처리

--- src/main/java/lotto/model/WinningNumberGenerator.java
+++ src/main/java/lotto/model/WinningNumberGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class WinningNumberGenerator {
     public Lotto createWinningNumbers(String rawWinningNumbers) {
+        validate(rawWinningNumbers);
         List<String> winningNumbers = splitWinningNumbers(rawWinningNumbers);
         List<Integer> numbers = convertWinningNumbers(winningNumbers);
         return Lotto.createWinningLotto(numbers);
@@ -15,6 +16,20 @@ public class WinningNumberGenerator {
     }
 
     private List<Integer> convertWinningNumbers(List<String> rawWinningNumbers) {
-        return rawWinningNumbers.stream().map(Integer::parseInt).toList();
+        try {
+            return rawWinningNumbers.stream().map(Integer::parseInt).toList();
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 부정확한 번호가 입력되었습니다.");
+        }
+    }
+
+    private void validate(String rawWinningNumbers){
+        validateFormat(rawWinningNumbers);
+    }
+
+    private void validateFormat(String rawWinningNumbers){
+        if(!rawWinningNumbers.matches("^[0-9,]*$")){
+            throw new IllegalArgumentException("[ERROR] 당첨 로또 번호는 숫자와 콤마만 입력가능합니다.");
+        }
     }
 }
