65bb0e786f45133ecd76fc22e10b122937cf4be0
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:42:12 +0900

    feat(Lotto): 로또 번호들을 감싸는 일급 컬렉션 Lotto 추가
    - List<Integer> 형식으로 된 numbers를 감싸는 일급 컬렉션으로 Lotto를 변경

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.Domain;
 
+import static lotto.Util.Error.ErrorMessage.INVALID_LOTTO_NUMBER_COUNT;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -12,9 +15,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNT.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
