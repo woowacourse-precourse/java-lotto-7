672b13a599de8611639c5490f866f43fb807b60e
조민규 <ariettys2@naver.com>
2024-11-03 21:26:52 +0900

    feat : 검증 부분 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -8,21 +9,36 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    public List<Integer> getNumbers() {
-        return new ArrayList<>(numbers);  // 불변성을 위해 새로운 리스트 반환
-    }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);  // 불변성을 위해 새로운 리스트 반환
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
     public int countMatchNumbers(Lotto winningLotto) {
         int matchCount = 0;
         List<Integer> winningNumbers = winningLotto.getNumbers();
