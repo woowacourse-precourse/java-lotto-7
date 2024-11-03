50497b6a4903983bd64c93696ed42b73cec4300b
hanwool <hanwe92@gmail.com>
2024-11-03 09:58:45 +0900

    feat(LottoController): Add purchaseService instance to call domain

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,22 +3,23 @@ package lotto.controller;
 import java.util.List;
 import lotto.domain.Bonus;
 import lotto.domain.Lotto;
-import lotto.domain.PurchaseAmount;
+import lotto.domain.Purchase;
 import lotto.service.DrawService;
-import lotto.service.PurchaseAmountService;
+import lotto.service.PurchaseService;
 import lotto.view.InputView;
 
 public class LottoController {
     DrawService drawService ;
-    PurchaseAmountService purchaseAmountService ;
+    PurchaseService purchaseService;
 
     public LottoController() {
         this.drawService = new DrawService();
+        this.purchaseService = new PurchaseService();
     }
 
     public void draw() {
         int purchaseAmount = InputView.getPurchaseAmount();
-        PurchaseAmount purchaseAmountService.getPurchaseAmount(purchaseAmount);
+        Purchase purchase = purchaseService.purchaseLotto(purchaseAmount);
 
         List<Integer> lottoNumbers = InputView.getLottoNumber();
         //inputView를 서비스에서도 호출하고 컨틀롤러에서도 호출하는게 이상함 리팩토링 필요
