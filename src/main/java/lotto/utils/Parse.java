114604c4ab22db0ab92863ef5b6e8f6f641d5d7d
nahyunKoo <angelcindy09@gmail.com>
2024-11-04 02:06:08 +0900

    refactor : Parse.winningNumbers 메소드 분리

--- src/main/java/lotto/utils/Parse.java
+++ src/main/java/lotto/utils/Parse.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class Parse {
     public static List<Integer> winningNumbers(String buffer) {
-        List<Integer> winningNumbers = new ArrayList<>();
+        String[] numbers = splitNumbers(buffer);
+        return convertToIntegerList(numbers);
+    }
+
+    public static String[] splitNumbers(String buffer) {
         String[] numbers = buffer.split(",");
+        return numbers;
+    }
+
+    public static List<Integer> convertToIntegerList(String[] numbers) {
+        List<Integer> numberList = new ArrayList<>();
         for (String number : numbers) {
-            winningNumbers.add(Integer.parseInt(number));
+            numberList.add(Integer.parseInt(number));
         }
-        return winningNumbers;
+        return numberList;
     }
 }
