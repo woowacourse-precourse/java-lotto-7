48619ac736bc4164fdbf10a66380acfeea6e9fdf
양효빈 <hyobin3026@naver.com>
2024-11-04 00:45:00 +0900

    feat(GeneralExceptionMessages): Add constants for general exception messages

---
+++ src/main/java/lotto/exception/GeneralExceptionMessages.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class GeneralExceptionMessages {
+
+    public static final String INVALID_NUMBER = "[ERROR] 올바른 숫자 형식이 아닙니다.";
+    public static final String DUPLICATE_NUMBER = "[ERROR] 중복된 숫자가 포함되어 있습니다.";
+}
