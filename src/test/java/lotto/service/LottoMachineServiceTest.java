fb5af96b101c5581ca63374fe874f701eb73053c
jijongkwon <help3451@naver.com>
2024-11-03 17:35:17 +0900

    test(util): 1000원 단위인지 확인하는 테스트 코드 구현

---
+++ src/test/java/lotto/service/LottoMachineServiceTest.java
@@ -0,0 +1,23 @@
+package lotto.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineServiceTest {
+
+    private LottoMachineService lottoMachineService;
+
+    @BeforeEach
+    public void setup() {
+        lottoMachineService = new LottoMachineService();
+    }
+
+    @Test
+    void inputLottoPurchaseAmount() {
+        // given
+
+        // when
+
+        // then
+    }
+}
