a9c7dedd3fa32097d92c6bf813376af9bcecc46a
tentenball <lth8905@gmail.com>
2024-11-04 00:33:02 +0900

    test: LottoTest 테스트 이름 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -16,9 +16,9 @@ class LottoTest {
         assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))).isInstanceOf(Lotto.class);
     }
 
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다")
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외가 발생한다")
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+    void 로또_번호의_개수가_6개가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
