c195e8637401ee82f622d27ee6d05c2932c0318d
Chanyoo Kim <cksdb012@gmail.com>
2024-11-03 15:18:23 +0900

    feat: 로또 번호 생성 기능 구현
    로또 번호를 생성하는 기능을 구현

---
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberGenerator {
+    public List<Integer> generate() {
+
+        return new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+}
