cca21d921df1179bd0d4ccf45f991643f0c61a31
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-03 12:42:56 +0900

    refactor(Convert): 타입 변환 메서드 이동
    - convertStringToInt메서드와 convertStringToList메서드의 목적은 타입을
      변경하는 것이기 때문에 해당 클래스로 옮겼습니다.

--- src/main/java/lotto/Utils/Convert.java
+++ src/main/java/lotto/Utils/Convert.java
@@ -1,4 +1,17 @@
 package lotto.Utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Convert {
+    public static int convertStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public static List<Integer> convertStringToList(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
 }
