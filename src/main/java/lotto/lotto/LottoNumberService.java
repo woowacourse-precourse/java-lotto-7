a52f8f63b7c1657a0bf1fb1f10bbd5ebe4cf55db
wooyoungKwon <rnjsdndud00@naver.com>
2024-11-03 18:09:42 +0900

    feat(lotto): 로또 생성 service 추가

---
+++ src/main/java/lotto/lotto/LottoNumberService.java
@@ -0,0 +1,22 @@
+package lotto.lotto;
+
+import static lotto.constant.LottoConstants.LOTTO_SIZE;
+import static lotto.constant.LottoConstants.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoConstants.MIN_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumberService {
+
+    public List<Integer> createRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
+    }
+
+    public int createBonusNumber() {
+        /**
+         * 기존 로또 번호에 있는 수는 아닌지 중복 검사하는 로직 추가 필요
+         */
+        return Randoms.pickNumberInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+    }
+}
