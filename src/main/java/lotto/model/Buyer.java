dd25b37e5c4d17ca0913d621e346371689914e85
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-03 22:22:37 +0900

    feat(Buyer): Buyer 클래스 추가
    - 구매 금액을 처리하고 로또 개수를 계산하는 `Buyer` 클래스 구현
    - 문자열 입력을 정수로 변환하고 유효성을 검사하는 로직 포함
    - 구매 금액이 최소 금액 및 1000의 배수인지 확인하는 예외 처리 추가

---
+++ src/main/java/lotto/model/Buyer.java
@@ -0,0 +1,44 @@
+package lotto.model;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+
+public class Buyer {
+
+    private static final int LOTTO_PRICE = 1000;
+    private final int purchaseAmount;
+    private final int numberOfLottos;
+
+    private Buyer(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        this.numberOfLottos = calculateNumberOfLottos(purchaseAmount);
+    }
+
+    public static Buyer from(String rawAmount) {
+        NumberStringConverter numberStringConverter = new NumberStringConverter();
+
+        int purchaseAmount = numberStringConverter.convert(rawAmount);
+
+        validatePurchaseAmount(purchaseAmount);
+
+        return new Buyer(purchaseAmount);
+    }
+
+    private static void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE) {
+            throw LottoException.from(ErrorMessage.PURCHASE_AMOUNT_MUST_BE_AT_LEAST_1000);
+        }
+
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw LottoException.from(ErrorMessage.PURCHASE_AMOUNT_MUST_BE_MULTIPLE_OF_1000);
+        }
+    }
+
+    private int calculateNumberOfLottos(int purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public int getNumberOfLottos() {
+        return numberOfLottos;
+    }
+}
