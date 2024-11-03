7f750e269c785a8564768a2d1a0858e8a68c4b9a
rootTiket <l20078829@gmail.com>
2024-11-03 17:28:19 +0900

    refactor(Winning): Winning이 Lotto를 상속받게 변경

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -2,17 +2,16 @@ package lotto.domain;
 
 import java.util.List;
 
-public class Winning {
-    private final List<Integer> numbers;
+public class Winning extends Lotto {
     private final Integer bonusNumber;
 
     public Winning(List<Integer> numbers, Integer bonusNumber) {
-        this.numbers = numbers;
+        super(numbers);
         this.bonusNumber = bonusNumber;
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return super.getNumbers();
     }
 
     public Integer getBonusNumber() {
