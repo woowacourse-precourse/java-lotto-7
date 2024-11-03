fd6bd77ed35fd47302cf4d5fd48a6189e799fd53
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-04 03:11:39 +0900

    refactor(ErrorMessage): 예외 메세지에 [ERROR] 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -1,10 +1,10 @@
 package lotto.enums;
 
 public enum ErrorMessage {
-    BLANK_INPUT_NOT_ALLOWED("사용자 입력은 null, 빈문자열, 공백으로만 이루어진 문자열일 수 없습니다."),
-    INVALID_INTEGER_INPUT("숫자는 정수 값만 입력해야 합니다."),
-    INVALID_PURCHASE_AMOUNT_UNIT("구입 금액은 1,000원 단위로 입력해야 합니다."),
-    NEGATIVE_AMOUNT_NOT_ALLOWED("구입 금액은 1,000원 단위의 양의 정수여야 합니다.");
+    BLANK_INPUT_NOT_ALLOWED("[ERROR] 사용자 입력은 null, 빈문자열, 공백으로만 이루어진 문자열일 수 없습니다."),
+    INVALID_INTEGER_INPUT("[ERROR] 숫자는 정수 값만 입력해야 합니다."),
+    INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다."),
+    NEGATIVE_AMOUNT_NOT_ALLOWED("[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.");
 
     private final String message;
 
