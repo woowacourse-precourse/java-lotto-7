cfec0311f70b1f7b180052997fb872a74fb226c3
qyinm <qusseun@gmail.com>
2024-11-03 17:49:34 +0900

    refactor: LottoController 메서드 분리 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -14,15 +14,26 @@ import lotto.view.OutputView;
 public class LottoController {
 
     public void runLotto() {
-        LottoPrice lottoPurchaseAmount = LottoExceptionUtils.runUntilNoneLottoException(InputView::inputLottoPrice);
-        LottoTickets lottoTickets = LottoGenerator.generateLottoTickets(lottoPurchaseAmount.getCanPurchaseLottoCount());
+        LottoPrice lottoPurchaseAmount = getLottoPurchaseAmount();
+        LottoTickets lottoTickets = generateLottoTickets(lottoPurchaseAmount);
         OutputView.printLottoTickets(lottoTickets);
 
-        Lotto wonLotto = LottoExceptionUtils.runUntilNoneLottoException(InputView::wonLottoNumbers);
-        Number bonusNumber = LottoExceptionUtils.runUntilNoneLottoException(InputView::inputBonusNumber);
-        WinningLotto winningLotto = new WinningLotto(wonLotto, bonusNumber);
-
+        WinningLotto winningLotto = inputWinningLotto();
         LottoPrizesRecord lottoPrizesRecord = new LottoPrizesRecord(lottoTickets.getLottoPrizesMap(winningLotto));
         OutputView.printLottoPrizes(lottoPrizesRecord, lottoPurchaseAmount);
+    }
+
+    private LottoPrice getLottoPurchaseAmount() {
+        return LottoExceptionUtils.runUntilNoneLottoException(InputView::inputLottoPrice);
+    }
+
+    private LottoTickets generateLottoTickets(LottoPrice lottoPurchaseAmount) {
+        return LottoGenerator.generateLottoTickets(lottoPurchaseAmount.getCanPurchaseLottoCount());
+    }
+
+    private WinningLotto inputWinningLotto() {
+        Lotto wonLotto = LottoExceptionUtils.runUntilNoneLottoException(InputView::wonLottoNumbers);
+        Number bonusNumber = LottoExceptionUtils.runUntilNoneLottoException(InputView::inputBonusNumber);
+        return new WinningLotto(wonLotto, bonusNumber);
     }
 }
