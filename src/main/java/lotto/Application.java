16191ec5e749c1097e364ec1ffa91d5e3256b947
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-03 17:22:30 +0900

    chore($multiple): OutputView 패키지 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,8 @@ package lotto;
 import lotto.controller.LottoController;
 import lotto.model.number_generator.DefaultRandomNumberGenerator;
 import lotto.model.shop.LottoShop;
-import lotto.view.OutputView;
 import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
 
 public class Application {
     public static void main(String[] args) {
