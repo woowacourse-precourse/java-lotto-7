7298292eca439de9f93b7bc85d537888dba32d9f
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-11-04 04:07:41 +0900

    feat(User): 로또 구매 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,9 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.info.LottoInfo;
+import lotto.model.Lotto;
+import lotto.model.User;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,7 +11,17 @@ public class LottoController {
     public void start() {
         int money = InputView.getMoneyToBuy();
         int amount = money / LottoInfo.PRICE.getNumber();
+
+        User user = new User(amount);
+
         OutputView.printBlankLine();
         OutputView.notifyAmount(amount);
+        printLottoNumbers(user.getLotto());
+    }
+
+    private void printLottoNumbers(List<Lotto> lotto) {
+        for (Lotto oneLotto : lotto) {
+            OutputView.printLottoNumbers(oneLotto.getNumbers());
+        }
     }
 }
