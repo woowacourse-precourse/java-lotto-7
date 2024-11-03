86bbefb16d676d6cfa56d2752b56f4c17b7575ce
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 17:47:50 +0900

    feat: 로또 구입 금액을 저장하는 PurchaseAmount 클래스 생성

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+    private static final int LOTTO_PRICE = 1000;
+
+    private final int amount;
+
+    public PurchaseAmount(int amount) {
+        this.amount = amount;
+    }
+}
