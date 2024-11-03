cb37e80a6143c0e18b6febd828ca73069c9609ba
Leegeonwoo7 <workgeon07@naver.com>
2024-11-03 21:37:44 +0900

    test(LottoMachineTest): LottoMachine 테스트
    LottoMachine이 6개의 숫자를 발급하는지 테스트

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,8 +1,25 @@
+package lotto;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+
 class LottoMachineTest {
-  
+
+    LottoMachine lottoMachine = new LottoMachine();
+
+    @Test
+    @DisplayName("로또를 발행한다")
+    void publishLotto() {
+        //given //when
+        Lotto lotto = lottoMachine.publishLotto();
+        List<Integer> numbers = lotto.getNumbers();
+
+        //then
+        assertThat(numbers).hasSize(6);
+    }
 }
