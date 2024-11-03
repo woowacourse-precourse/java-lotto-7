0a9907fe7132bce8993b3396d89189b7a3e773dc
sc@t0a14 <gusdn6671@gmail.com>
2024-11-04 00:07:13 +0900

    test(Rank): 각 Rank의 상금이 정확히 계산되는지 확인 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -9,12 +9,23 @@ class RankTest {
     @Test
     @DisplayName("매치 카운트와 보너스 여부에 따라 Rank가 올바르게 반환되는지 확인")
     void matchLottoRank() {
-        assertEquals(Rank.SIX, Rank.matchLotto(6, false));  // 6개 일치
-        assertEquals(Rank.FIVE_AND_BONUS, Rank.matchLotto(5, true)); // 5개 일치 + 보너스
-        assertEquals(Rank.FIVE, Rank.matchLotto(5, false)); // 5개 일치
-        assertEquals(Rank.FOUR, Rank.matchLotto(4, false)); // 4개 일치
-        assertEquals(Rank.THREE, Rank.matchLotto(3, false)); // 3개 일치
-        assertEquals(Rank.NONE, Rank.matchLotto(2, false)); // 2개 이하 일치
+        assertEquals(Rank.SIX, Rank.matchLotto(6, false));
+        assertEquals(Rank.FIVE_AND_BONUS, Rank.matchLotto(5, true));
+        assertEquals(Rank.FIVE, Rank.matchLotto(5, false));
+        assertEquals(Rank.FOUR, Rank.matchLotto(4, false));
+        assertEquals(Rank.THREE, Rank.matchLotto(3, false));
+        assertEquals(Rank.NONE, Rank.matchLotto(2, false));
+    }
+
+    @Test
+    @DisplayName("각 Rank의 상금이 정확히 계산되는지 확인")
+    void calculateTotalEarnings() {
+        assertEquals(2_000_000_000, Rank.SIX.calculateTotalEarnings(1));
+        assertEquals(60_000_000, Rank.FIVE_AND_BONUS.calculateTotalEarnings(2));
+        assertEquals(3_000_000, Rank.FIVE.calculateTotalEarnings(2));
+        assertEquals(150_000, Rank.FOUR.calculateTotalEarnings(3));
+        assertEquals(10_000, Rank.THREE.calculateTotalEarnings(2));
+        assertEquals(0, Rank.NONE.calculateTotalEarnings(5));
     }
 
 }
