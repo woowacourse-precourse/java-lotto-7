09414538f537c08b74dc89a84829167b4c4de001
chungjeongsu <wjdwlgh2000@gmail.com>
2024-11-03 23:38:05 +0900

    feat: 로또 핸들러 빌더와 의존성 설정 클래스 구현
    - LottoHandlerBuilder: LottoHandler들을 빌드할 수 있고, REAME에 적혀있는 순서에 맞춰서
      각 핸들러들을 생성해야함.
    - LottoHandlerDependencyConfig: 각 핸들러들의 의존성을 추가.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.config.LottoHandlerBuilder;
+import lotto.config.LottoHandlerDependencyConfig;
+import lotto.handler.LottoHandler;
+import lotto.handler.token.HandlerToken;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoHandlerDependencyConfig lottoHandlerDependencyConfig = new LottoHandlerDependencyConfig(
+                new LottoHandlerBuilder());
+        LottoHandler lottoHandler = lottoHandlerDependencyConfig.lottoHandler();
+
+        lottoHandler.handle(new HandlerToken());
+        Console.close();
     }
 }
