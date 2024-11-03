c60767116203a3745408ae02875c2f4117ede537
dbdb456 <adad568@naver.com>
2024-11-03 13:45:34 +0900

    feat(controller): 필드, 생성자 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,4 +1,21 @@
 package lotto.controller;
 
+import lotto.utils.Utils;
+import lotto.validator.Validators;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Controller {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Validators validators;
+    private final Utils utils;
+
+
+    public Controller(InputView inputView, OutputView outputView, Validators validators, Utils utils) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.validators = validators;
+        this.utils = utils;
+    }
 }
