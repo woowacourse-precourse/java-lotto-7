0be73cf38e162a83eb0dc9e55a71920e4faa2f26
Parkjyun <jaeyun0507@naver.com>
2024-11-03 21:49:43 +0900

    feat(AnswerNumbers): 당첨번호 문자열을 파싱한다

--- src/main/java/lotto/model/AnswerNumbers.java
+++ src/main/java/lotto/model/AnswerNumbers.java
@@ -1,20 +1,50 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import lotto.model.exception.DomainExceptionMessage;
 
 public class AnswerNumbers {
-    private static final Pattern numberPattern = Pattern.compile("^[0-9]+$");
-    private static final int LOWER_LIMIT = 1;
-    private static final int UPPER_LIMIT = 45;
+    private static final String seperator = ",";
+    private static final int ANSWER_SIZE = 6;
 
-    private final List<Integer> answerNumbers;
+    private final List<LottoNumber> answerNumbers;
 
-    public AnswerNumbers(List<Integer> answerNumbers) {
+    public AnswerNumbers(List<LottoNumber> answerNumbers) {
         this.answerNumbers = answerNumbers;
     }
 
-    public List<Integer> getAnswerNumbers() {
+    public List<LottoNumber> getAnswerNumbers() {
         return List.copyOf(answerNumbers);
+    }
+
+    public static AnswerNumbers from(String input) {
+        List<LottoNumber> answers = Arrays.stream(input.split(seperator))
+                .map(LottoNumber::new)
+                .toList();
+        validate(answers);
+        return new AnswerNumbers(answers);
+    }
+
+    private static void validate(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+    }
+
+    private static void validateSize(List<LottoNumber> numbers) {
+        if (numbers.size() != ANSWER_SIZE) {
+            throw new IllegalArgumentException(
+                    DomainExceptionMessage.INVALID_ANSWER_SIZE.getMessage()
+            );
+        }
+    }
+
+    private static void validateDuplication(List<LottoNumber> numbers) {
+        long distinctNumberCount = numbers.stream().distinct().count();
+        if (distinctNumberCount != numbers.size()) {
+            throw new IllegalArgumentException(
+                    DomainExceptionMessage.DUPLICATED_ANSWER_NUMBER.getMessage()
+            );
+        }
     }
 }
--- src/test/java/lotto/model/AnswerNumbersTest.java
+++ src/test/java/lotto/model/AnswerNumbersTest.java
@@ -3,19 +3,21 @@ package lotto.model;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.AnswerNumbers;
+import lotto.model.exception.DomainExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AnswerNumbersTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Answer 객체를 생성할 수 있다.")
-    void should_CreateAnswer_WhenGivenNumbers() {
-        // given
-        List<Integer> answerNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    @ValueSource(strings = {"1,2,3,,4,5,6", "1, 2, 3, 4, 5, 6"})
+    void should_CreateAnswer_WhenGivenNumbers(String input) {
         // when
-        AnswerNumbers answer = new AnswerNumbers(answerNumbers);
+        AnswerNumbers answer = AnswerNumbers.from(input);
         // then
         Assertions.assertThat(answer).isNotNull();
     }
@@ -24,34 +26,52 @@ class AnswerNumbersTest {
     @DisplayName("정답 번호를 반환 받을 수 있다.")
     void should_GetAnswerNumbers() {
         // given
-        List<Integer> answerNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        AnswerNumbers answer = new AnswerNumbers(answerNumbers);
+        String input = "1,2,3,4,5,6";
         // when
-        List<Integer> actual = answer.getAnswerNumbers();
+        AnswerNumbers answer = AnswerNumbers.from(input);
+        List<LottoNumber> answerNumbers = answer.getAnswerNumbers();
         // then
-        Assertions.assertThat(actual).isEqualTo(answerNumbers);
-    }
-
-    @Test
-    @DisplayName("반환 받은 정답 번호는 원본 번호와 동일성을 보장 받지 않는다.")
-    void should_NotBeSameInstance_When_GetAnswerNumbers() {
-        // given
-        List<Integer> answerNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        AnswerNumbers answer = new AnswerNumbers(answerNumbers);
-        // when
-        List<Integer> actual = answer.getAnswerNumbers();
-        // then
-        Assertions.assertThat(actual).isNotSameAs(answerNumbers);
+        Assertions.assertThat(answerNumbers).contains(
+                new LottoNumber("1"),
+                new LottoNumber("2"),
+                new LottoNumber("3"),
+                new LottoNumber("4"),
+                new LottoNumber("5"),
+                new LottoNumber("6")
+        );
     }
 
     @Test
     @DisplayName("반환 받은 정답 번호를 변경하면 예외가 발생한다.")
     void should_ThrowException_When_ModifyAnswerNumbers() {
         // given
-        List<Integer> answerNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        AnswerNumbers answer = new AnswerNumbers(answerNumbers);
+        String input = "1,2,3,4,5,6";
+        // when
+        AnswerNumbers answer = AnswerNumbers.from(input);
         // when, then
-        Assertions.assertThatThrownBy(() -> answer.getAnswerNumbers().add(7))
+        Assertions.assertThatThrownBy(() -> answer.getAnswerNumbers().add(new LottoNumber("7")))
                 .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("정답 번호가 6개가 아닌 경우 예외를 발생한다.")
+    void should_ThrowException_When_AnswerNumbersSizeIsNotSix() {
+        // given
+        String input = "1,2,3,4,5";
+        // when, then
+        Assertions.assertThatThrownBy(() -> AnswerNumbers.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DomainExceptionMessage.INVALID_ANSWER_SIZE.getMessage());
+    }
+
+    @Test
+    @DisplayName("정답 번호가 중복된 경우 예외를 발생한다.")
+    void should_ThrowException_When_AnswerNumbersHasDuplicatedNumbers() {
+        // given
+        String input = "1,2,3,4,5,5";
+        // when, then
+        Assertions.assertThatThrownBy(() -> AnswerNumbers.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DomainExceptionMessage.DUPLICATED_ANSWER_NUMBER.getMessage());
     }
 }
