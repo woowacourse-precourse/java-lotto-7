127c4b1eb01d9626aa0b54fb743830d886a3c40a
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 06:14:16 +0900

    feat: 수익을 계산하는 기능 추가

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -9,19 +9,39 @@ public class LottoManager {
     private final Map<LottoResult, Integer> lottoResultMap = new HashMap<>();
     private final List<Lotto> lottos;
 
-    public LottoManager(List<Lotto> lottos) {
+    public LottoManager(List<Lotto> lottos, Lotto winningLotto, Bonus bonus) {
         this.lottos = lottos;
+        getLottoResultFrom(winningLotto, bonus);
     }
 
-    public void getLottoResultFrom(Lotto winningLotto, Bonus bonus) {
+    public int calculateWinningAmount() {
+        return lottoResultMap.entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getWinningAmount() * entry.getValue())
+                .sum();
+    }
+
+    public Map<LottoResult, Integer> getLottoResultMap() {
+        return lottoResultMap;
+    }
+
+    private void getLottoResultFrom(Lotto winningLotto, Bonus bonus) {
+        setLottoResultMap();
         lottos.forEach(lotto -> compareWith(winningLotto, bonus, lotto));
+    }
+
+    private void setLottoResultMap() {
+        for (LottoResult lottoResult : LottoResult.values()) {
+            lottoResultMap.put(lottoResult, 0);
+        }
     }
 
     private void compareWith(Lotto winningLotto, Bonus bonus, Lotto lotto) {
         int matchingCount = findMatchingCount(winningLotto, lotto);
         boolean doesBonusMatch = lotto.hasBonus(bonus);
         LottoResult lottoResult = LottoResult.getLottoResult(matchingCount, doesBonusMatch);
-        lottoResultMap.put(lottoResult, lottoResultMap.getOrDefault(lottoResult, 0) + 1);
+        if (lottoResult != null) {
+            lottoResultMap.put(lottoResult, lottoResultMap.get(lottoResult) + 1);
+        }
     }
 
     private int findMatchingCount(Lotto winningLotto, Lotto lotto) {
