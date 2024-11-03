9d399736f350f9547036674295091f32d189e74b
MinJae00 <exceedmin@gmail.com>
2024-11-04 02:31:48 +0900

    refact: output 출력 형식 오류 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,13 +14,26 @@ public class OutputView {
     }
 
     public void printresult(Map<String, Integer> result) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
         for (LottoRank rank : LottoRank.values()) {
             int num = result.get(rank.name());
-            System.out.println(rank.getMatchCount() + "개 일치 " + rank.getOutputPrize() + "원" + " - " + num + "개");
+            System.out.println(printRank(rank, num));
         }
     }
 
+    private String printRank(LottoRank rank, int num) {
+        if (rank.name() == "SECOND") {
+            String message = String.format("%d개 일치, 보너스 볼 일치 (%,d원) - %d개", rank.getMatchCount(), rank.getPrize(),
+                    num);
+            return message;
+        }
+        String message = String.format("%d개 일치 (%,d원) - %d개", rank.getMatchCount(), rank.getPrize(), num);
+        return message;
+    }
+
+
     public void printRate(double num) {
-        System.out.println("총 수익률은 " + String.format("%.2f", num) + "%입니다.");
+        System.out.println("총 수익률은 " + String.format("%.1f", num) + "%입니다.");
     }
 }
