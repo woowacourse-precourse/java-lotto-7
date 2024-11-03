4ac6ee16ffb4371d504f60c86b086d68238ba00f
dbdb456 <adad568@naver.com>
2024-11-03 20:48:25 +0900

    test(utils): 숫자를 넣었을때 정상 로직 테스트 추가

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import lotto.utils.Utils;
 import org.junit.jupiter.api.DisplayName;
@@ -17,5 +18,13 @@ public class UtilsTest {
 
         assertThatThrownBy(() -> utils.parseStringToInt(inputTest))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자를 넣으면 정상 로직")
+    @Test
+    void 숫자면_정상로직이다() {
+        String inputTest = "123";
+
+        assertDoesNotThrow(() -> utils.parseStringToInt(inputTest));
     }
 }
