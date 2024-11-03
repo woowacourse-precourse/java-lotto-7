cb103c5bfd32ec548f0e02cc254c01d86221618a
Taekgil <taekgil273@gmail.com>
2024-11-03 20:23:23 +0900

    feat: 입력 유효성 검사 구현

--- src/main/java/lotto/exception/CheckInput.java
+++ src/main/java/lotto/exception/CheckInput.java
@@ -1,5 +1,9 @@
 package lotto.exception;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.Lotto;
 import lotto.enums.ErrorType;
 
 public class CheckInput {
@@ -12,6 +16,51 @@ public class CheckInput {
 
         if (inputMoney % 1000 != 0) {
             System.out.println(ErrorType.INVALID_PURCHASE_UNIT.getErrorMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public static Lotto checkLottoNumbers(String inputLottoNumbers) {
+        String[] splitInputLottoNumbers = inputLottoNumbers.split(",");
+
+        List<Integer> tmpLottoNumber = new ArrayList<>();
+        for (String splitInputLottoNumber : splitInputLottoNumbers) {
+            try {
+                int tmpNumber = Integer.parseInt(splitInputLottoNumber.trim());
+
+                if (tmpNumber < 1 || tmpNumber > 45) {
+                    throw new IllegalArgumentException();
+                }
+
+                tmpLottoNumber.add(tmpNumber);
+            } catch (NumberFormatException e) {
+                System.out.println(ErrorType.INVALID_LOTTO_NUMBER_FORMAT.getErrorMessage());
+                return null;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ErrorType.INVALID_LOTTO_NUMBER_RANGE.getErrorMessage());
+                return null;
+            } catch (Exception e) {
+                System.out.println(ErrorType.UNEXPECTED_ERROR.getErrorMessage());
+                return null;
+            }
+        }
+
+        Collections.sort(tmpLottoNumber);
+        Lotto tmpLotto = new Lotto(tmpLottoNumber);
+
+        return tmpLotto;
+    }
+
+    public static boolean checkBonusNumber(int bonusNum, Lotto winningLottoNumbers) {
+        if (bonusNum < 1 || bonusNum > 45) {
+            System.out.println(ErrorType.INVALID_LOTTO_NUMBER_RANGE.getErrorMessage());
+            return false;
+        }
+
+        if (winningLottoNumbers.checkDuplicateWithBonusNumber(bonusNum)) {
+            System.out.println(ErrorType.INVALID_LOTTO_BONUS_NUMBER_DUPLICATE.getErrorMessage());
             return false;
         }
 
