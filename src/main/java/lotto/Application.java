00c93e7865199f01b35db260ebe438f6965d7db2
mskim <msung98@naver.com>
2024-11-03 15:00:27 +0900

    docs(readme): 기능구현 목록 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,60 @@
 package lotto;
 
+interface Predator{
+    String getFood();
+}
+
+interface Barkable{
+    void bark();
+}
+
+class Bouncer {
+    void barkAnimal(Barkable animal) {  // Animal 대신 Barkable을 사용
+        animal.bark();
+    }
+}
+
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Tiger extends Animal implements Predator, Barkable {
+    public String getFood() {
+        return "apple";
+    }
+    public void bark() {
+        System.out.println("어흥");
+    }
+
+}
+
+class Lion extends Animal implements Predator, Barkable {
+    public String getFood() {
+        return "banana";
+    }
+    public void bark() {
+        System.out.println("으르렁");
+    }
+
+}
+
+class ZooKeeper {
+    void feed(Predator predator) {
+        System.out.println("feed " + predator.getFood());
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Tiger tiger = new Tiger();
+        Lion lion = new Lion();
+
+        Bouncer bouncer= new Bouncer();
+        bouncer.barkAnimal(tiger);
+        bouncer.barkAnimal(lion);
     }
 }
