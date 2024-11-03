900e056c5a73c274dfe513ba00b4f04566c0e296
soohalee <suha630@naver.com>
2024-11-04 03:07:55 +0900

    Test(ValidatorTest): isEmpty 출력메시지 수정

--- src/test/java/lotto/validation/ValidatorTest.java
+++ src/test/java/lotto/validation/ValidatorTest.java
@@ -1,16 +1,24 @@
 package lotto.validation;
 
 import static org.assertj.core.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorTest {
 
     @Test
-    @DisplayName("문자열이 비었는지 확인하는 테스트")
+    @DisplayName("문자열이 비어있으면 예외 발생 테스트")
     void isEmpty() {
         String empty = "";
 
         assertThatThrownBy(() -> {Validator.isEmpty(empty);}).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {
+            Validator.isEmpty(empty);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
     }
 }
