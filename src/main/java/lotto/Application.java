cdeda3c5e3a9c25d08b4f0b5ecbe7aef8cb337cd
steamedEggMaster <cksdud5014@naver.com>
2024-11-03 20:30:06 +0900

    refactor: Application 코드 최적화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,9 +5,6 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
-
         lottoController.executeLottoWorkflow();
-
-        // TODO: 프로그램 구현
     }
 }
