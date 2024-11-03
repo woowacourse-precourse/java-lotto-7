bb437d8496b1c07fbc7e2c7ddd7ccf358e5da333
koo-sangwoo <luckytkddn@gmail.com>
2024-11-04 00:19:07 +0900

    refactor : 천원 이하의 금액을 걸러내는 로직 삭제
    - 400원등 1000원으로 나누어 떨어지지 않는 금액에 대한 검사 로직이 있다.
    - 또한 0원을 입력했을 때 걸러내는 로직도 있기 때문에 1000원에 대한 로직은 불필요하다 판정.

--- src/main/java/lotto/model/BuyPrice.java
+++ src/main/java/lotto/model/BuyPrice.java
@@ -5,7 +5,7 @@ package lotto.model;
  * 구매 금액에 대한 유효성 검사를 가진다.
  */
 public class BuyPrice {
-    private Integer price;
+    private final Integer price;
 
     public BuyPrice(String inputPrice) {
         String trimPrice = inputPrice.replaceAll("\\s+", "");
@@ -22,23 +22,16 @@ public class BuyPrice {
     }
 
     private void devideThousandValidation(String inputPrice) {
-        Integer parsePrice = Integer.parseInt(inputPrice);
+        int parsePrice = Integer.parseInt(inputPrice);
         if (parsePrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 잔돈이 없습니다. 1000원 단위에 맞춰 입력해주세요.");
         }
     }
 
     private void checkZeroTest(String inputPrice) {
-        Integer parsePrice = Integer.parseInt(inputPrice);
+        int parsePrice = Integer.parseInt(inputPrice);
         if (parsePrice == 0) {
             throw new IllegalArgumentException("[ERROR] 금액을 내셔야 구매가 가능합니다.");
-        }
-    }
-
-    private void biggerThanThousand(String inpuString) {
-        Integer parsePrice = Integer.parseInt(inpuString);
-        if (parsePrice < 1000) {
-            throw new IllegalArgumentException("[ERROR] 천원 이상을 입금해주셔야합니다~!");
         }
     }
 
