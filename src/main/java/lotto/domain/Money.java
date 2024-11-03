f87c828aee70ce261b85825778700b45122e82fd
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-04 04:13:45 +0900

    refactor(Money): 0을 상수화

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.enums.ErrorMessage;
 
 public class Money {
+    private final int ZERO = 0;
     private final int UNIT_OF_PURCHASE_AMOUNT = 1_000;
 
     private final int amount;
@@ -16,20 +17,20 @@ public class Money {
         return new Money(amount);
     }
 
-    private void validate(int amount) {
+    private void validate(final int amount) {
         validatePositiveInteger(amount);
         validateUnit(amount);
     }
 
-    private void validatePositiveInteger(int amount) {
-        boolean isValid = (0 < amount);
+    private void validatePositiveInteger(final int amount) {
+        boolean isValid = (ZERO < amount);
         if (!isValid) {
             throw new IllegalArgumentException(ErrorMessage.NEGATIVE_AMOUNT_NOT_ALLOWED.getMessage());
         }
     }
 
-    private void validateUnit(int amount) {
-        boolean isValid = (amount % UNIT_OF_PURCHASE_AMOUNT == 0);
+    private void validateUnit(final int amount) {
+        boolean isValid = (amount % UNIT_OF_PURCHASE_AMOUNT == ZERO);
         if (!isValid) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
         }
