c8340abe0f299e2bb6e634b968fc12f7d9905626
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 02:08:49 +0900

    feat: 로또 당첨 여부 확인 메소드 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,14 +1,17 @@
-package lotto;
+package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
 public class LottoService {
     private final static int LOTTO_MIN_NUMBER = 1;
     private final static int LOTTO_MAX_NUMBER = 45;
 
-    public List<Lotto> buyLottos(int lottoCount) {
+    public List<Lotto> buyLottos(int buyMoney) {
+        int lottoCount = buyMoney / 1000;
         List<Lotto> lottos = new ArrayList<>();
         while (lottoCount-- > 0) {
             lottos.add(new Lotto(makeLandomNumber()));
@@ -21,6 +24,14 @@ public class LottoService {
     }
 
     public double getYield(int useMoney, int winningMoney) {
-        return Math.round(useMoney / winningMoney * 10) / 10.0;
+        return ((double) winningMoney / (double) useMoney) * 100;
+    }
+
+    public LottoResult matchLotto(List<Lotto> lottos, Lotto winningLotto) {
+        LottoResult lottoResult = new LottoResult();
+        for (Lotto lotto : lottos) {
+            lottoResult.countMatchingNumbers(lotto, winningLotto);
+        }
+        return lottoResult;
     }
 }
