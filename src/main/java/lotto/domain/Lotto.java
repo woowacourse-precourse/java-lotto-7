534ac9d2c6f4df03826adefcfc822082cdb902b6
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 01:34:56 +0900

    refactor(Lotto): 로또 검증 메소드 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
@@ -15,17 +17,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateNumberRange(numbers);
+        validateOverlapping(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != MAX_COUNT) {
             String errorMessage = String.format("[ERROR] 로또 번호는 %d개여야 합니다.", MAX_COUNT);
             throw new IllegalArgumentException(errorMessage);
         }
+    }
 
+    private void validateNumberRange(List<Integer> numbers) {
         numbers.forEach((number) -> {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
                 String errorMessage = String.format("[ERROR] 로또 번호는 %d 이상, %d 이하 정수여야 합니다.", MIN_NUMBER, MAX_NUMBER);
                 throw new IllegalArgumentException(errorMessage);
             }
         });
+    }
+
+    private void validateOverlapping(List<Integer> numbers) {
+        Set<Integer> nonOverlappingNumbers = new HashSet<>(numbers);
+
+        if (nonOverlappingNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 중복될 수 없습니다.");
+        }
     }
 
     public List<Integer> getImmutableNumbers() {
