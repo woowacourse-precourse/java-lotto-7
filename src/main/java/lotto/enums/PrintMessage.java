e7864d561675919e62e53200132d3bad5fe9f8ba
rladmstn <oz33177351@gmail.com>
2024-11-03 16:40:54 +0900

    fix(enum) : 에러 메세지 포매팅 오류 수정

--- src/main/java/lotto/enums/PrintMessage.java
+++ src/main/java/lotto/enums/PrintMessage.java
@@ -9,9 +9,9 @@ public enum PrintMessage {
     OUTPUT_TOTAL_RESULT("당첨 통계\n---"),
     OUTPUT_FIFTH_PLACE_RESULT("3개 일치 (5,000원) - %d개"),
     OUTPUT_FOURTH_PLACE_RESULT("4개 일치 (50,000원) - %d개"),
-    OUTPUT_THIRD_PLACE_RESULT("5개 일치 (1,500,000원) - 0개"),
-    OUTPUT_SECOND_PLACE_RESULT("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개"),
-    OUTPUT_FIRST_PLACE_RESULT("6개 일치 (2,000,000,000원) - 0개"),
+    OUTPUT_THIRD_PLACE_RESULT("5개 일치 (1,500,000원) - %d개"),
+    OUTPUT_SECOND_PLACE_RESULT("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    OUTPUT_FIRST_PLACE_RESULT("6개 일치 (2,000,000,000원) - %d개"),
     OUTPUT_TOTAL_PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
