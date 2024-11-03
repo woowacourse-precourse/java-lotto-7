62b6dbb8b378e0645d91e667550b1833669eb824
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:48:04 +0900

    :sparkles: feat(Money): 구입 금액 정보 저장 및 검증

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,4 +1,41 @@
 package lotto.domain;
 
+import static lotto.handler.ConstantHandler.LOTTO_PRICE;
+import static lotto.handler.ConstantHandler.MIN_MONEY_AMOUNT;
+import static lotto.handler.ConstantHandler.MONEY_REMAINDER;
+import static lotto.handler.ErrorHandler.INVALID_DIVISION;
+import static lotto.handler.ErrorHandler.INVALID_POSITIVE;
+
 public class Money {
+    private final int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getTicketCount() {
+        return money / LOTTO_PRICE;
+    }
+
+    private void validate(int money) {
+        validatePositive(money);
+        validateDivision(money);
+    }
+
+    private void validatePositive(int money) {
+        if (money <= MIN_MONEY_AMOUNT) {
+            throw INVALID_POSITIVE.getException();
+        }
+    }
+
+    private void validateDivision(int money) {
+        if (money % LOTTO_PRICE != MONEY_REMAINDER) {
+            throw INVALID_DIVISION.getException();
+        }
+    }
 }
