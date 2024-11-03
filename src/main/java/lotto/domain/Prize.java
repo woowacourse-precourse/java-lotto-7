bef9f140fa98f61bff7afd5becd04ad0160f795b
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:07:27 +0900

    style(Prize): 일치 개수가 적은 순으로 작성

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -2,12 +2,12 @@ package lotto.domain;
 
 public enum Prize {
 
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
+    FAIL(-1, false, 0),
     FIFTH(3, false, 5000),
-    FAIL(-1, false, 0);
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000);
 
     private int matchCount;
     private boolean isBonus;
