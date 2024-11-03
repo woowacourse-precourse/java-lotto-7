4ad0c64c17357d87638f6d82af6808e7c84c0038
seungchan60 <157793981+seungchan60@users.noreply.github.com>
2024-11-03 20:13:41 +0900

    feat: 로또 구입 금액 입력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int purchaseAmount = getPurchaseAmount();
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+
+        try {
+            int amount = Integer.parseInt(input);
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위로 입력해야 합니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력이 가능합니다.");
+        }
     }
 }
