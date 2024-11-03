b9c0bcb01958a39e144fb5f9f3cb8bdd19b1ad1d
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 01:52:45 +0900

    refactor: 개별 당첨 내역 출력 포멧 수정

--- src/main/java/lotto/system/utils/constants/WinningMessage.java
+++ src/main/java/lotto/system/utils/constants/WinningMessage.java
@@ -3,7 +3,7 @@ package lotto.system.utils.constants;
 public enum WinningMessage {
 
     HEADER("당첨 통계\n---\n"),
-    CONTENT("%s개 일치 (%s원)- %s개\n총 수익률은 %.1f%%입니다.");
+    CONTENT("%s - %d개");
 
     private final String message;
 
