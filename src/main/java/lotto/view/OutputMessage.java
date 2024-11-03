d2641b9b0766050b3f8706f5635dd8f813888c85
kkmdevel <kkm.devel@gmail.com>
2024-11-03 20:25:51 +0900

    feat(OutputView): 당첨 번호 및 보너스 번호 입력 메시지 출력 기능 추가
    - OutputMessage에 "당첨 번호 입력" 및 "보너스 번호 입력" 메시지 추가
    - OutputView에 당첨 번호 및 보너스 번호 입력 메시지를 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,12 +1,14 @@
 package lotto.view;
 
-public enum OutputMessages {
+public enum OutputMessage {
     PAYMENT("구입금액을 입력해 주세요."),
-    PURCHASED_LOTTO_COUNT("%d개를 구매했습니다.");
+    PURCHASED_LOTTO_COUNT("%d개를 구매했습니다."),
+    WINNING_LOTTO("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     private final String message;
 
-    OutputMessages(String message) {
+    OutputMessage(String message) {
         this.message = message;
     }
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,7 +5,7 @@ import lotto.domain.LottoPaper;
 public class OutputView {
 
     public static void printPaymentMessage() {
-        System.out.println(OutputMessages.PAYMENT.getMessage());
+        System.out.println(OutputMessage.PAYMENT.getMessage());
     }
 
     public static void printErrorMessage(String errorMessage) {
@@ -15,8 +15,17 @@ public class OutputView {
     public static void printLottoPaperMessage(LottoPaper lottoPaper){
         int lottoCount = lottoPaper.getLottoCount();
         System.out.println();
-        System.out.println(OutputMessages.PURCHASED_LOTTO_COUNT.getFormattedMessage(lottoCount));
+        System.out.println(OutputMessage.PURCHASED_LOTTO_COUNT.getFormattedMessage(lottoCount));
         lottoPaper.getLottoNumbers().forEach(System.out::println);
     }
 
+    public static void printWinningLottoMessage() {
+        System.out.println();
+        System.out.println(OutputMessage.WINNING_LOTTO.getFormattedMessage());
+    }
+
+    public static void printBonusNumberMessage() {
+        System.out.println();
+        System.out.println(OutputMessage.BONUS_NUMBER.getFormattedMessage());
+    }
 }
