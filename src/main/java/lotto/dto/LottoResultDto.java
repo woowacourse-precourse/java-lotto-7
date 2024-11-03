b7262d8e1fba42ae05a45f3358dffe310c830eca
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 22:03:03 +0900

    refactor(LottoResultsDto) : dto 객체 변경

--- src/main/java/lotto/dto/LottoResultDto.java
+++ src/main/java/lotto/dto/LottoResultDto.java
@@ -1,25 +1,22 @@
 package lotto.dto;
 
+import java.util.Map;
+import lotto.model.Rank;
+
 public class LottoResultDto {
-    private final String matchDescription;
-    private final long prize;
-    private final int count;
+    private final Map<Rank, Integer> rankCounts;
+    private final double profitRate;
 
-    public LottoResultDto(String matchDescription, long prize, int count) {
-        this.matchDescription = matchDescription;
-        this.prize = prize;
-        this.count = count;
+    public LottoResultDto(Map<Rank, Integer> rankCounts, double profitRate) {
+        this.rankCounts = rankCounts;
+        this.profitRate = profitRate;
     }
 
-    public String getMatchDescription() {
-        return matchDescription;
+    public Map<Rank, Integer> getRankCounts() {
+        return rankCounts; // 랭크 카운트를 반환
     }
 
-    public long getPrize() {
-        return prize;
-    }
-
-    public int getCount() {
-        return count;
+    public double getProfitRate() {
+        return profitRate; // 수익률 반환
     }
 }
