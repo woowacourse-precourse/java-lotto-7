50cd72132a93ce964f3ab04eab4006d3596cf9e5
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-03 14:35:27 +0900

    feat: 당첨 결과 및 수익률 포메팅 기능 구현

---
+++ src/main/java/lotto/system/formater/WinningFormatter.java
@@ -0,0 +1,18 @@
+package lotto.system.formater;
+
+import lotto.system.utils.PrizeType;
+
+public class WinningFormatter { // 당첨 결과를 출력하는 클래스
+
+    private final static String WINNINGS_MESSAGE = "%d개 일치 (%d원)- %d개";
+    private final static String BONUS_WINNINGS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원)- %d개";
+
+    public static String formatWinnings(int code, int count) {
+        PrizeType typeByCode = PrizeType.getTypeByCode(code);
+        if (typeByCode == PrizeType.SECOND_PRIZE) {
+            return String.format(BONUS_WINNINGS_MESSAGE, typeByCode.getCode(), typeByCode.getPrizeMoney(), count);
+        }
+        return String.format(WINNINGS_MESSAGE, typeByCode.getCode(), typeByCode.getPrizeMoney(), count);
+    }
+
+}
