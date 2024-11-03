6b51517c20cb56b9cf4d7ebb15a612f8aa679452
33jyu33 <jy04169@gmail.com>
2024-11-03 17:35:40 +0900

    feat(Lotto): find the number of lotto that match the winning number
    Function setLottoResult:
    - save and update matching count in lottoResult
    - If the number of matching numbers in a lottery is five, the bonus numbers are compared to see if they are second place
    Function getLottoResult
    Function getSecondPlace

--- src/main/java/lotto/Consumer.java
+++ src/main/java/lotto/Consumer.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.CompareInteger;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -8,6 +9,8 @@ import java.util.List;
 
 public class Consumer {
     private List<Lotto> lottoTicket = new ArrayList<>();
+    private int[] lottoResult = new int[CompareInteger.LOTTO_NUMBER_COUNT.getNumber() + 1];
+    private int secondPlace = CompareInteger.ZERO.getNumber();
 
     public Consumer(int count) {
         for (int i = 0; i < count; i++) {
@@ -28,5 +31,23 @@ public class Consumer {
 
     public List<Lotto> getLottoTicket() {
         return this.lottoTicket;
+    }
+
+    public void setLottoResult(WinningNumber winningNumber) {
+        for (Lotto lotto : lottoTicket) {
+            int count = lotto.getMatchingCount(winningNumber.getNumbers());
+            lottoResult[count]++;
+            if (count == CompareInteger.LOTTO_MATCHING_COUNT_SECOND_PLACE.getNumber() && lotto.isMatchBonus(winningNumber.getBonus())) {
+                secondPlace++;
+            }
+        }
+    }
+
+    public int[] getLottoResult() {
+        return this.lottoResult;
+    }
+
+    public int getSecondPlace() {
+        return this.secondPlace;
     }
 }
