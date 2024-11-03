1146df78386be686389c684196c40fe06a416665
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-03 18:50:16 +0900

    refactor : 로또 어플 실행 로직 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,31 +3,18 @@ package lotto;
 import lotto.controller.LottoPurchaseController;
 import lotto.controller.LottoStatisticsController;
 import lotto.controller.LottoWinningNumbersController;
-import lotto.domain.LottoResult;
-import lotto.domain.LottoWinningNumbers;
-import lotto.domain.Lottos;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
-import java.util.Set;
 
 public class Application {
 
-    private static final LottoPurchaseController lottoPurchaseController = new LottoPurchaseController();
-    private static final LottoWinningNumbersController lottoWinningNumbersController = new LottoWinningNumbersController();
-    private static final LottoStatisticsController lottoStatisticsController = new LottoStatisticsController();
 
     public static void main(String[] args) {
 
-        long userPurchaseMoney = InputView.inputPurchaseMoney();
-        Lottos lottos = lottoPurchaseController.purchaseLottos(userPurchaseMoney);
-        OutputView.showPurchasedLottos(lottos);
+        LottoApplication app = new LottoApplication(
+            new LottoPurchaseController(),
+            new LottoWinningNumbersController(),
+            new LottoStatisticsController()
+        );
 
-        Set<Integer> winningNumbers = InputView.inputWinningNumbers();
-        int bonusNumber = InputView.inputBonusWinningNumber();
-        LottoWinningNumbers lottoWinningNumbers = lottoWinningNumbersController.determineLottoWinningNumbers(winningNumbers, bonusNumber);
-
-        LottoResult lottoResult = lottoStatisticsController.calculateLottoPrizes(lottos, lottoWinningNumbers, userPurchaseMoney);
-        OutputView.showPurchasedLottosStatus(lottoResult);
+        app.run();
     }
 }
