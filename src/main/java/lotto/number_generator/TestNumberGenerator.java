8c05b3de80a5575d4d2ca6d227d996727b32d196
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 15:12:22 +0900

    test(NumberGenerator): 테스트 번호를 유연하게 생성하도록 수정

--- src/main/java/lotto/number_generator/TestNumberGenerator.java
+++ src/main/java/lotto/number_generator/TestNumberGenerator.java
@@ -1,10 +1,21 @@
 package lotto.number_generator;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TestNumberGenerator implements NumberGenerator{
+
+    private final List<Integer> numbers;
+
+    public TestNumberGenerator(int start, int end) {
+        this.numbers = IntStream.rangeClosed(start, end)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
     @Override
     public List<Integer> generate() {
-        return List.of(6, 5, 4, 3, 2, 1);
+        return numbers;
     }
 }
