7e3a8e12d2d0c04c2eec95aec50ec9951ebb9bf7
yxoni <psy337250@gmail.com>
2024-11-03 20:45:32 +0900

    feat: 사용자가 구매한 로또 생성 기능 구현

--- src/main/java/lotto/OperationManager.java
+++ src/main/java/lotto/OperationManager.java
@@ -1,12 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class OperationManager {
     private final IO io = new IO();
+    List<Lotto> lottos;
+
 
     public void buy() {
+        // 입력 받기
         String payment = io.readPayment();
         String winningNumber = io.readWinningNumber();
         String bonusNumber = io.readBonusNumber();
+
+        // 로또 생성
+        long purchaseAmount = Long.parseLong(payment)/1000;
+        for (int i = 0; i < purchaseAmount; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(lotto);
+            lottos.add(new Lotto(lotto));
+        }
+
+
+
+
     }
 
     public void result() {
