20831a3da5809ab8c92ed7858e1ffdd79df7b162
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:50:56 +0900

    feat: 보너스 넘버 생성 매니저

--- src/main/java/lotto/service/BonusNumberManager.java
+++ src/main/java/lotto/service/BonusNumberManager.java
@@ -1,2 +1,14 @@
-package lotto.service;public class BonusNumberManager {
+package lotto.service;
+
+import lotto.domain.Bonus;
+import lotto.repository.LottoRepository;
+import lotto.valuate.Valuate;
+
+public class BonusNumberManager {
+    public void createBonusNumber(String s, LottoRepository lottoRepository) {
+        Valuate.isNum(s);
+        int num = Integer.parseInt(s);
+        Bonus bonus = new Bonus(num, lottoRepository.getWinningNumbers());
+        lottoRepository.saveBonusNumbers(bonus);
+    }
 }
