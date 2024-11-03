d5363e62d758f1e27dc949aa4c445feb0ada96d1
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-11-04 00:37:28 +0900

    fix : 출력 오류 수정

--- src/main/java/lotto/LottoOutputView.java
+++ src/main/java/lotto/LottoOutputView.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -7,21 +8,22 @@ public class LottoOutputView {
     public static void printPurchasedLottoCount(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
-            System.out.println(lotto.getNumbers());
+            List<Integer> numbers = lotto.getNumbers();
+            Collections.sort(numbers);
+            System.out.println(numbers);
         }
     }
 
     public static void printResult(Map<LottoRank, Integer> result) {
         System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println("---------");
         for (LottoRank rank : LottoRank.values()) {
-            if (rank != LottoRank.MISS) {
-                System.out.printf("%d개 일치 (%d원) - %d개%n", rank.getMatchCount(), rank.getPrize(), result.getOrDefault(rank, 0));
-            }
+            int count = result.getOrDefault(rank, 0);
+            System.out.printf("%s (%d원) - %d개%n", rank.getRank(), rank.getPrize(), count);
         }
     }
 
     public static void printrateOfReturn(double rate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", rate);
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", rate);
     }
 }
--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,39 +1,47 @@
 package lotto;
 
 public enum LottoRank{
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    MISS(0, false, 0);
+    FIRST(6, 2_000_000_000, "6개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    FOURTH(4, 50_000, "4개 일치"),
+    FIFTH(3, 5_000, "3개 일치");
 
-    private final int  match;
-    private final boolean matchBonus;
+
+    private final int matchCount;
     private final int prize;
+    private final String rank;
 
 
-    LottoRank(int match, boolean matchBonus, int prize) {
-        this.match = match;
-        this.matchBonus = matchBonus;
+    LottoRank(int matchCount, int prize, String rank) {
+        this.matchCount = matchCount;
         this.prize = prize;
+        this.rank = rank;
     }
 
     public int getPrize() {
         return prize;
     }
 
-    public int getMatchCount() {
-        return match;
+    public String getRank() {
+        return rank;
     }
 
-    public static LottoRank valueOf(int match, boolean matchBonus) {
-        for (LottoRank rank : values()) {
-            if (rank.match == match && rank.matchBonus == matchBonus) {
-                return rank;
-            }
+    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == FIRST.matchCount) {
+            return FIRST;
         }
-        return MISS;
+        if (matchCount == SECOND.matchCount && matchBonus) {
+            return SECOND;
+        }
+        if (matchCount == THIRD.matchCount) {
+            return THIRD;
+        }
+        if (matchCount == FOURTH.matchCount) {
+            return FOURTH;
+        }
+            return FIFTH;
+
     }
 
 }
