631a481b0554e611520c6ef0e8f7cb9817ff38ce
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 03:23:16 +0900

    feat(inputhandler): 당첨 번호 입력 기능 추가
    - 사용자로부터 당첨 번호를 입력받는 메서드를 추가하였습니다.
    - 입력된 값을 문자열로 받아 그대로 반환합니다.

--- src/main/java/lotto/input/InputHandler.java
+++ src/main/java/lotto/input/InputHandler.java
@@ -1,10 +1,16 @@
 package lotto.input;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputHandler {
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(readLine());
+    }
+    public String inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return readLine();
     }
 }
