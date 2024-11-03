bfbe3aa64f4b33398ba685e89f933e2d8d6e3261
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 01:19:59 +0900

    refactor: 당첨번호가 정수 범위의 값인지 확인하는 로직 추가

--- src/main/java/lotto/utils/parser/WinningNumbersParser.java
+++ src/main/java/lotto/utils/parser/WinningNumbersParser.java
@@ -1,5 +1,7 @@
 package lotto.utils.parser;
 
+import static lotto.constants.ErrorMessage.WINNING_NUMBER_IS_ONLY_INTEGER_ALLOWED;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +11,15 @@ public class WinningNumbersParser {
     public final static String COMMA_REGEX = ",";
 
     public static Lotto getWinningNumbers(String userWinningNumbers) {
-        List<Integer> winningNumbers = Arrays.stream(userWinningNumbers.split(COMMA_REGEX))
-                .map(String::trim)
-                .map(Integer::valueOf)
-                .collect(Collectors.toList());
+        List<Integer> winningNumbers;
+        try {
+            winningNumbers = Arrays.stream(userWinningNumbers.split(COMMA_REGEX))
+                    .map(String::trim)
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(WINNING_NUMBER_IS_ONLY_INTEGER_ALLOWED.getMessage());
+        }
         return new Lotto(winningNumbers);
     }
 }
