e0926baf9c89179531d6bfe3c3e014874a937251
jijongkwon <help3451@naver.com>
2024-11-03 16:42:19 +0900

    feat(service): LottoMachineService 클래스 생성
    TODO
    - LOTTO 구입 금액을 입력받는다.
    - 보너스 번호를 입력받는다.
    - 로또 번호들을 발급한다.
    - 입력된 당첨 번호를 저장한다.
    - 입력된 보너스 번호를 저장한다.
    - 사용자의 로또 번호와 비교한다.
    - 맞춘 개수와 금액을 사용자에게 전달한다.
    - 1000원 단위인지 확인한다.

---
+++ src/main/java/lotto/service/LottoMachineService.java
@@ -0,0 +1,20 @@
+package lotto.service;
+
+public class LottoMachineService {
+
+    // TODO: LOTTO 구입 금액을 입력받는다.
+
+    // TODO: 보너스 번호를 입력받는다.
+
+    // TODO: 로또 번호들을 발급한다.
+
+    // TODO: 입력된 당첨 번호를 저장한다.
+
+    // TODO: 입력된 보너스 번호를 저장한다.
+
+    // TODO: 사용자의 로또 번호와 비교한다.
+
+    // TODO: 맞춘 개수와 금액을 사용자에게 전달한다.
+
+    // TODO: 1000원 단위인지 확인한다.
+}
