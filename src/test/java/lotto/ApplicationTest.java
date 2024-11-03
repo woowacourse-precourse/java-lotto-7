86954742fc5cdb19963f7cb734eeb256a87a14ff
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 03:07:49 +0900

    test: application 테스트 코드 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -52,6 +53,32 @@ class ApplicationTest extends NsTest {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @DisplayName("올바른 값이 들어왔을 경우, 정상작동 한다.")
+    @Test
+    void Given_CorrectInput_When_ControllerRun_Then_Success() {
+        // Given
+        assertRandomUniqueNumbersInRangeTest (
+            () -> {
+                run("3000", "1,2,3,4,5,6", "7");
+                assertThat(output()).contains(
+                        "3개를 구매했습니다.",
+                        "[1, 2, 3, 4, 5, 6]",
+                        "[40, 41, 42, 43, 44, 45]",
+                        "[1, 2, 3, 4, 5, 7]",
+                        "3개 일치 (5,000원) - 0개",
+                        "4개 일치 (50,000원) - 0개",
+                        "5개 일치 (1,500,000원) - 0개",
+                        "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                        "6개 일치 (2,000,000,000원) - 1개",
+                        "총 수익률은 67666666.7%입니다."
+                );
+            },
+                    List.of(1, 2, 3, 4, 5, 6),
+                    List.of(40, 41, 42, 43, 44, 45),
+                    List.of(1, 2, 3, 4, 5, 7)
+        );
     }
 
     @Override
