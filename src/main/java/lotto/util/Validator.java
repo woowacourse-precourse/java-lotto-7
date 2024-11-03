d3e8383fe543d5acbe6221bf06b1ea15a24272bb
beginerer <newstart1379@naver.com>
2024-11-04 00:52:02 +0900

    test: add unit tests for LotteryMachine functionality

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -7,7 +7,7 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 입력이 비어있습니다.");
     }
     public static void validatePositiveNumber(int num) {
-        if(num <=0) {
+        if(num <= 0) {
             throw new IllegalArgumentException("[ERROR] 양수가 아닙니다.");
         }
     }
--- src/test/java/lotto/domain/LotteryMachineTest.java
+++ src/test/java/lotto/domain/LotteryMachineTest.java
@@ -4,6 +4,7 @@ import lotto.dto.Result;
 import lotto.util.LottoRank;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,19 +14,32 @@ class LotteryMachineTest {
     User user;
     LotteryMachine lotteryMachine;
 
-
     @BeforeEach
     void before() {
         purchaseAmount = 10000;
         user = new User(purchaseAmount);
         lotteryMachine = new LotteryMachine();
-
     }
 
     @Test
     void purchaseLottoTickets() {
         lotteryMachine.purchaseLottoTickets(user);
         Assertions.assertThat(user.getPurchasedLotto().size()).isEqualTo(purchaseAmount/LotteryMachine.LOTTO_TICKET_PRICE);
+    }
+    @DisplayName("구매 금액이 로또 가격단위가 아닐경우 예외가 발생한다.")
+    @Test
+    void 구매금액이_로또_가격단위가_아닐경우_예외가_발생한다() {
+        User user1 = new User(7900);
+        Assertions.assertThatThrownBy(()-> lotteryMachine.purchaseLottoTickets(user1)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+    @DisplayName("구매 금액이 양수가 아닐 경우 예외가 발생한다.")
+    @Test
+    void 구매금액이_양수가_아닐경우_예외가_발생한다() {
+        Assertions.assertThatThrownBy(()->new User(-1)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+        Assertions.assertThatThrownBy(()->new User(0)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
     }
 
     @Test
