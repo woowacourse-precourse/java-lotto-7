d83f28465c7c72a2bf2e5ab86045c371680497cb
youjeans <dbyj2810@naver.com>
2024-11-03 22:33:49 +0900

    feat: 정수 변환 가능 여부 검증 함수

---
+++ src/main/java/lotto/Util.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Util {
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
