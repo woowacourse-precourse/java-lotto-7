568f3d2ec072ef875f991ae487183b9385ac1e9b
taek2222 <oot309@daum.net>
2024-11-04 03:44:06 +0900

    feat(Tickets): 당첨 번호 로또 가져오는 메서드 추가
    - 당첨 번호 로또로 결과 집계를 위해 메서드를 추가했습니다.

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -6,11 +6,16 @@ import java.util.List;
 public class Winning {
 
     private static final String DELIMITER = ",";
+
     private final Lotto lotto;
 
     public Winning(String input) {
         List<Integer> numbers = convertToIntegerList(input);
         this.lotto = new Lotto(numbers);
+    }
+
+    public Lotto getLotto() {
+        return lotto;
     }
 
     private List<Integer> convertToIntegerList(String input) {
