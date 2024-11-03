453e1093752ffd9c573257e4740e1e87ea8db120
Parkjyun <jaeyun0507@naver.com>
2024-11-04 00:55:37 +0900

    feat(Customer): 로또를 구매할 수 있다

--- src/main/java/lotto/model/Customer.java
+++ src/main/java/lotto/model/Customer.java
@@ -1,11 +1,16 @@
 package lotto.model;
 
+import java.util.List;
+
 public class Customer {
-    private Money budget;
+    private final Money budget;
     private final MyLotto myLotto;
     public Customer(Money budget, MyLotto myLotto) {
         this.budget = budget;
         this.myLotto = myLotto;
     }
 
+    public void buyLotto(List<Lotto> lottos) {
+        lottos.forEach(myLotto::addLotto);
+    }
 }
--- src/test/java/lotto/model/CustomerTest.java
+++ src/test/java/lotto/model/CustomerTest.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,5 +19,19 @@ class CustomerTest {
         Customer customer = new Customer(budget, myLotto);
         //then
         Assertions.assertThat(customer).isNotNull();
+    }
+
+    @Test
+    @DisplayName("고객은 로또를 구매할 수 있다.")
+    void should_BuyLotto() {
+        // given
+        Money budget = new Money("8000");
+        MyLotto myLotto = new MyLotto(new ArrayList<>());
+        Customer customer = new Customer(budget, myLotto);
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        // when
+        customer.buyLotto(lottos);
+        // then
+        Assertions.assertThat(myLotto.getLottos()).hasSize(1);
     }
 }
