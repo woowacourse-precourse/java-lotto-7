b022d8df3eac6bfae04d4cf8c4259b7ae0413e92
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:30:38 +0900

    test(WinningDetailsTest) : saveMyGrades 테스트

--- src/test/java/lotto/WinningDetailsTest.java
+++ src/test/java/lotto/WinningDetailsTest.java
@@ -1,14 +1,16 @@
 package lotto;
 
+import lotto.Model.Lotto;
 import lotto.Model.MyResults;
 import lotto.Model.WinningDetails;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningDetailsTest {
@@ -25,5 +27,28 @@ public class WinningDetailsTest {
         assertThat(actualWinningDetails.getFifth()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("saveMyGrades 정상 작동 테스트")
+    void saveMyGradesTest() {
+        int lottoCount = 6;
+        int bonus = 7;
+        MyResults expectedResult1 = new MyResults(1, false);
+        MyResults expectedResult2 = new MyResults(1, false);
+        MyResults expectedResult3 = new MyResults(2, true);
+        AtomicReference<List<Lotto>> myLottos = new AtomicReference<>(new ArrayList<>());
+        Lotto answer = new Lotto(List.of(1,2,3,4,5,6));
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    myLottos.set(Lotto.sortLottoList(lottoCount));
+                    List<MyResults> actualMyResults = WinningDetails.saveMyGrades(myLottos.get(), answer, bonus);
+                    assertThat(actualMyResults.get(0).getMatches()).isEqualTo(expectedResult1.getMatches());
+                    assertThat(actualMyResults.get(1).getMatches()).isEqualTo(expectedResult2.getMatches());
+                    assertThat(actualMyResults.get(2).getMatches()).isEqualTo(expectedResult3.getMatches());
+                },
+                List.of(5, 8, 10, 12, 26, 40),
+                List.of(3, 17, 25, 34, 38, 44),
+                List.of(1, 4, 7, 16, 24, 26)
+        );
+    }
 
 }
