77bff7d3d80d5e323baf78cb5b345aa696ff8212
minzzun99 <skg06123@naver.com>
2024-11-03 18:33:00 +0900

    feat(StringProcessor): 입력값 처리 기능 추가
    - 입력된 당첨번호를 (,)를 기준으로 나누는 기능 추가
    - 문자열 배열을 정수형 List로 변환하는 기능 추가

--- src/main/java/lotto/util/StringProcessor.java
+++ src/main/java/lotto/util/StringProcessor.java
@@ -4,7 +4,14 @@ import static lotto.constant.ErrorMessage.EMPTY_INPUT_ERROR;
 import static lotto.constant.ErrorMessage.NULL_INPUT_ERROR;
 import static lotto.constant.ErrorMessage.NUMBER_RANGE_ERROR;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringProcessor {
+    private static final String COMMA = ",";
+    private static final String EMPTY = "";
+    private static final int MIN_POSITIVE_NUMBER = 1;
+
     public static void validateInput(String input) {
         if (input == null) {
             throw new IllegalArgumentException(NULL_INPUT_ERROR.getMessage());
@@ -16,14 +23,14 @@ public class StringProcessor {
     }
 
     public static String removeCommas(String input) {
-        return input.replaceAll(",", "");
+        return input.replaceAll(COMMA, EMPTY);
     }
 
     public static int integerConverter(String input) {
         try {
-            int number = Integer.parseInt(input);
+            int number = Integer.parseInt(input.trim());
 
-            if (number < 1) {
+            if (number < MIN_POSITIVE_NUMBER) {
                 throw new IllegalArgumentException(NUMBER_RANGE_ERROR.getMessage());
             }
 
@@ -31,5 +38,19 @@ public class StringProcessor {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_RANGE_ERROR.getMessage());
         }
+    }
+
+    public static String[] splitInputString(String input) {
+        return input.split(COMMA);
+    }
+
+    public static List<Integer> integerListConverter(String[] strings) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String element : strings) {
+            int number = integerConverter(element);
+            numbers.add(number);
+        }
+
+        return numbers;
     }
 }
