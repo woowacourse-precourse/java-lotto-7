4231246b095d52e8cc899961b0767df1040228ba
Dohun Kim <dohun8758@naver.com>
2024-11-03 19:17:19 +0900

    feat: 로또에 관련된 상수를 가지는 Enum 구현

---
+++ src/main/java/lotto/LottoConstants.java
@@ -0,0 +1,19 @@
+package lotto;
+
+public enum LottoConstants {
+    MIN_LOTTO_NUMBER(1),
+    MAX_LOTTO_NUMBER(45),
+    LOTTO_COUNT(6),
+    LOTTO_PRICE(1000),
+    ;
+
+    private final int value;
+
+    LottoConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
