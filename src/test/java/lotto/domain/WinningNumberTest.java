fd836d113b441bced5564730ee4c35a2e918ba24
slay <palgalow@gmail.com>
2024-11-03 20:09:59 +0900

    refactor(WinningNumberTest): 보너스번호 당첨번호와 중복 예외, 보너스번호 범위 벗어남 예외 테스트 기능 추가

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,5 +20,27 @@ public class WinningNumberTest {
 
         assertThat(winningNums.getWinningLotto().getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
         assertThat(winningNums.getBonusNumber()).isEqualTo(7);
+    }
+
+    @DisplayName("보너스번호 당첨번호와 중복 예외")
+    @Test
+    void duplicateBonusNumberAndWinningNumbersTest() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 6;
+
+        assertThatThrownBy(() -> new WinningNumbers(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
+    @DisplayName("보너스번호 범위 벗어남 예외")
+    @Test
+    void bonusNumberNotInRangeTest() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 46;
+
+        assertThatThrownBy(() -> new WinningNumbers(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
     }
 }
