0a11a4ba11366f05c9b2191806a2a681f4d709c8
world-dv <goodforx0147@naver.com>
2024-11-03 23:28:36 +0900

    refactor: 매직 넘버 정의

--- src/main/java/lotto/util/ProfitCalculator.java
+++ src/main/java/lotto/util/ProfitCalculator.java
@@ -4,7 +4,9 @@ import lotto.service.Payment;
 
 public class ProfitCalculator {
 
+    private static final Integer PERCENT = 100;
+
     public static float calculate(Payment payment, Integer prizeMoney) {
-        return (float) prizeMoney / payment.getWalletMoney() * 100;
+        return (float) prizeMoney / payment.getWalletMoney() * PERCENT;
     }
 }
