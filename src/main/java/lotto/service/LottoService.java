74614e43e7aca29a2a9516b584fc07f948473741
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 15:12:56 +0900

    feat(LottoService): 발행된 로또를 한꺼번에 저장하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,19 @@
 package lotto.service;
 
-public class LottoService {
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+import lotto.repository.LottoRepository;
 
+import java.util.List;
+
+public class LottoService {
+    private final LottoRepository lottoRepository;
+
+    public LottoService(LottoRepository lottoRepository) {
+        this.lottoRepository = lottoRepository;
+    }
+
+    public void saveLottos(List<Lotto> lottos) {
+        lottoRepository.saveAll(lottos);
+    }
 }
