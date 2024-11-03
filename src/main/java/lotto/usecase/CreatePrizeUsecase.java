9f96279d0f489501571a7d58ee92d4c6673b3e98
plan11plan <tjtfy7028@gmail.com>
2024-11-03 20:24:14 +0900

    feat(CreatePrizeUsecase): 당첨 생성 유스케이스 구현
    - 당첨번호 입력 뷰에서 요청 정보 받기
    - 컨트롤러를 통한 당첨번호 저장
    - 저장된 당첨번호 정보 조회

---
+++ src/main/java/lotto/usecase/CreatePrizeUsecase.java
@@ -0,0 +1,24 @@
+package lotto.usecase;
+
+import lotto.PrizeResponse;
+import lotto.controller.PrizeController;
+import lotto.view.input.PrizeInputView;
+import lotto.view.input.PrizeViewRequest;
+
+public class CreatePrizeUsecase {
+    private final PrizeInputView inputView;
+    private final PrizeController controller;
+
+    public CreatePrizeUsecase(PrizeInputView inputView, PrizeController controller) {
+        this.inputView = inputView;
+        this.controller = controller;
+    }
+
+    public PrizeResponse execute() {
+        PrizeViewRequest request = inputView.initialize();
+        Long savedId = controller.save(request.winnerNumbers(), request.bonusNumber());
+
+        return controller.getPrize(savedId);
+    }
+
+}
