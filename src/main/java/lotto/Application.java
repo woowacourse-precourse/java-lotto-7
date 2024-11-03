475762adfc137444186221fa4af450afa5780bdf
LaiTial <asna90@naver.com>
2024-11-04 06:37:36 +0900

    ìmake winNumber LottoClass

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,19 +2,30 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
 
         int buy = Input.buyMoney(); // 구입 금액 입력 받기
-        LottoPurchase purchase = new LottoPurchase(buy/1000); // 로또 번호 생성
+        LottoBox purchase = new LottoBox(buy/1000); // 로또 번호 생성
         purchase.Print(); // 생성한 로또 번호 print
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        String lotto = Console.readLine(); // 당첨 번호
-        //Lotto win = new Lotto();
-        //getLotto
+        String numbers = Console.readLine(); // 당첨 번호
+        Lotto winNumber = new Lotto(changeType(numbers)); // 당첨 번호 Lotto 클래스 생성
 
         System.out.println("보너스 번호를 입력해 주세요.");
-        String bonus = Console.readLine(); // 보너스 번호
+        String bonus = Console.readLine(); // 보너스 번호 입력받기
+
+
+    }
+
+    public static List<Integer> changeType(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
