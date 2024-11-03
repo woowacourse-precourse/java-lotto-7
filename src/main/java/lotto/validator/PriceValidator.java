35286a5fc04c9e5a5d8e31ff8d9b2ce0193d15cc
GrO0vy <lmc2819@naver.com>
2024-11-03 19:36:42 +0900

    refactor: 구입 금액 검증 기능 분리

---
+++ src/main/java/lotto/validator/PriceValidator.java
@@ -0,0 +1,41 @@
+package lotto.validator;
+
+// 구입 금액 검증 클래스
+public class PriceValidator implements Validator {
+    private final String price;
+
+    public PriceValidator(String price){
+        this.price = price;
+    }
+
+    @Override
+    public void validate() {
+        isValidFormat();
+        isValidPrice();
+    }
+
+    private void isValidFormat() {
+        if (!price.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 금액은 0 이상의 숫자 하나만 입력 할 수 있습니다.");
+        }
+    }
+
+    private void isValidPrice(){
+        int inputPrice = isValueInRange();
+        isMultipleOfAThousand(inputPrice);
+    }
+
+    private int isValueInRange() {
+        try{
+            return Integer.parseInt(price);
+        }catch (Exception e){
+            throw new IllegalArgumentException(String.format("[ERROR] 금액은 %d 범위를 벗어날 수 없습니다.", Integer.MAX_VALUE));
+        }
+    }
+
+    private void isMultipleOfAThousand(int inputValue){
+        if(inputValue == 0 || inputValue % 1000 > 0){
+            throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해주세요.");
+        }
+    }
+}
--- src/main/java/lotto/view/PriceInputView.java
+++ src/main/java/lotto/view/PriceInputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.messages.InputMessage;
+import lotto.validator.PriceValidator;
 
 public class PriceInputView extends InputView{
     @Override
@@ -9,7 +10,12 @@ public class PriceInputView extends InputView{
     }
 
     @Override
-    protected void validate(String input) {
+    public void initializeValidator(){
+        validator = new PriceValidator(inputValue);
+    }
 
+    @Override
+    protected void validate() {
+        validator.validate();
     }
 }
