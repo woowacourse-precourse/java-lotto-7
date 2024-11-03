f84fb5febefbe165b27a0d12b0fddc0ea6953314
yeong-hwan <jghff700@naver.com>
2024-11-03 20:22:17 +0900

    test(entity): throw exception wehn main numbers count is invalid

--- src/test/java/lotto/entity/WinningNumbersTest.java
+++ src/test/java/lotto/entity/WinningNumbersTest.java
@@ -22,4 +22,15 @@ class WinningNumbersTest {
         assertEquals(mainNumbers, winningNumbers.getMainNumbers());
         assertEquals(bonusNumber, winningNumbers.getBonusNumber());
     }
+
+    @Test
+    @DisplayName("메인 숫자가 6개가 아닐 때 예외 발생")
+    void throwExceptionWhenMainNumbersCountIsInvalid() {
+        List<Integer> mainNumbers = List.of(1, 2, 3, 4, 5); // 숫자 개수가 5개
+        int bonusNumber = 7;
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumbers(mainNumbers, bonusNumber));
+        assertEquals(ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.getMessage(), exception.getMessage());
+    }
 }
