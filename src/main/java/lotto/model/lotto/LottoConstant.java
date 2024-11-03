7e30499e9a7c0c5d6e3620f9845d0cada2cb6555
양효빈 <hyobin3026@naver.com>
2024-11-04 00:22:28 +0900

    feat(LottoConstant): Add class to save constants about rule of lotto

---
+++ src/main/java/lotto/model/lotto/LottoConstant.java
@@ -0,0 +1,11 @@
+package lotto.model.lotto;
+
+public class LottoConstant {
+
+    public static final int NUMBER_START_INCLUSIVE = 1;
+    public static final int NUMBER_END_INCLUSIVE = 45;
+    public static final int NUMBER_COUNT = 6;
+    public static final int PRICE = 1000;
+    public static final int MAX_PURCHASE_AMOUNT = 100_000;
+
+}
