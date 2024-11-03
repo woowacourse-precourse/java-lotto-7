c106e30717c5553d20c4a5dad6bfded82fb7f7af
awaykim <wonj010101@gmail.com>
2024-11-04 06:02:58 +0900

    refac: 상수, 메세지, 에러메세지 util에 저장

---
+++ src/main/java/lotto/utils/ErrorMessages.java
@@ -0,0 +1,15 @@
+package lotto.utils;
+
+public class ErrorMessages {
+    public static final String ERROR = "[ERROR] ";
+    public static final String AMOUNT_SHOULD = "구입 금액은 ";
+    public static final String LOTTO_SHOULD = "로또 번호는 ";
+    public static final String BONUS_SHOULD = "보너스 번호는 ";
+    public static final String BE_INTEGER = "정수여야 합니다.";
+    public static final String MINIMUM_AMOUNT = "로또 하나의 가격은 %d원입니다.";
+    public static final String BE_POSITIVE = "양수여야 합니다.";
+    public static final String BE_N_SIZE = "%d개여야 합니다.";
+    public static final String NOT_BE_REPEATED = "중복될 수 없습니다.";
+    public static final String BE_IN_RANGE = "%d에서 %d 사이의 숫자여야 합니다. ";
+    public static final String OBJECT_NOT_INIT = "객체가 초기화되지 않았습니다.";
+}
