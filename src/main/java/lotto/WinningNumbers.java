979bf1f591288c3c1ba40fefad64a0b44576f6bd
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 00:27:46 +0900

    feat(WinningNumbers): 당첨 번호 숫자 리스트 개수 검증 기능 구현

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 public class WinningNumbers {
 
-    private final List<Integer> values;
+    private static final int NUMBER_COUNT = 6;
+    private final List<Integer> numbers;
 
-    public WinningNumbers(List<Integer> values) {
-        this.values = values;
+    public WinningNumbers(List<Integer> numbers) {
+        validateNumberCount(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
     }
 }
