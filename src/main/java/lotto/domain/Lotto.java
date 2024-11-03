033ba0846bf664e8b5a005fbf6884bb51eb13d01
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 02:20:19 +0900

    feat, docs: 맞춘 개수 확인 기능 추가
    - 기능 구현에 따른 README 업데이트

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.exception.ExceptionMessage;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(ExceptionMessage.TOO_MANY_NUMBERS.getMessage());
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+    public Long checkMatchCount(List<Integer> checkingNumbers) {
+        return numbers.stream()
+                .filter(checkingNumbers::contains)
+                .count();
+    }
+}
