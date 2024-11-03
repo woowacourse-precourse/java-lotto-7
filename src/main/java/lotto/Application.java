436fe2285b3c431cb68539a7a5c575d38a2134ce
encoreJeong <zzaibean@gmail.com>
2024-11-03 21:29:31 +0900

    feat: 구입금액 입력 기능 추가
    1. 안내문구 출력
    2. 입력을 Integer 형으로 변환
    2. 입력값 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer totalPrice = Integer.parseInt(readLine());
+        if(totalPrice % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000의 배수인 정수를 입력해야 합니다.");
+        }
     }
 }
