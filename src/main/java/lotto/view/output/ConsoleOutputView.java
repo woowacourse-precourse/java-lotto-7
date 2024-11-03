6be7f8d1ae8e6210f5794ee6138b83bfa3c1aae8
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-03 17:39:18 +0900

    refactor($ConsoleOutputView): response 의 로또 번호를 정렬해서 출력하도록 변경
    - 로또 번호를 정렬해서 보여 주어야 하는 것은 View 의 책임이라 판단.

--- src/main/java/lotto/view/output/ConsoleOutputView.java
+++ src/main/java/lotto/view/output/ConsoleOutputView.java
@@ -55,6 +55,7 @@ public class ConsoleOutputView implements OutputView {
 
     private void printLotto(LottoNumberResponse response) {
         System.out.println(response.getLottoNumbers().stream()
+                .sorted()
                 .map(String::valueOf)
                 .collect(Collectors.joining(LOTTO_NUMBERS_FORMAT, LOTTO_NUMBERS_PREFIX, LOTTO_NUMBERS_SUFFIX)));
     }
