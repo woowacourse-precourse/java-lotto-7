8135485da3ea017331935d726a5393ba4e550a0a
이준범 <jach1206@naver.com>
2024-11-03 22:29:37 +0900

    feat(domain): 당첨 정보와 상금을 반환받는 메서드 생성
    - getFormatterPrize: 추가적으로, 출력 요구 형식에 맞춰 반환 하는 메서드를 만들었다.

--- src/main/java/lotto/domain/WinningPrize.java
+++ src/main/java/lotto/domain/WinningPrize.java
@@ -1,20 +1,35 @@
 package lotto.domain;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public enum WinningPrize {
 
-    FAILURE(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
+    FAILURE("낙첨", 0),
+    FIFTH("3개 일치", 5000),
+    FOURTH("4개 일치", 50000),
+    THIRD("5개 일치", 1500000),
+    SECOND("5개 일치, 보너스 볼 일치", 30000000),
+    FIRST("6개 일치", 2000000000);
 
-    private final int matchCount;
+    private final String matchCount;
     private final int prize;
 
-    WinningPrize(int matchCount, int prize) {
+    WinningPrize(String matchCount, int prize) {
         this.matchCount = matchCount;
         this.prize = prize;
     }
 
+    public String getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getFormattedPrize() {
+        NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+        return formatter.format(prize);
+    }
 }
