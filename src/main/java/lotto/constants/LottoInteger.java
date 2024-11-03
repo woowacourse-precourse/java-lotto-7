f241733dc4f8f55e73ac893d51deb415c64452ab
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-03 19:16:25 +0900

    fix : 에러 메시지 출력 방식 변경

--- src/main/java/lotto/constants/LottoInteger.java
+++ src/main/java/lotto/constants/LottoInteger.java
@@ -5,8 +5,6 @@ public enum LottoInteger {
     LOTTO_START_NUMBER(1),
     LOTTO_END_NUMBER(45),
     LOTTO_NUMBER_COUNT(6),
-    BONUS_NUMBER_COUNT(1),
-
     ;
     private final int value;
 
--- src/main/java/lotto/view/ErrorPrinter.java
+++ src/main/java/lotto/view/ErrorPrinter.java
@@ -4,11 +4,12 @@ import lotto.constants.InputError;
 
 public class ErrorPrinter {
     public static void errorPrint(InputError message) {
-        System.out.println(message.getMessage());
-//        try {
-//            throw new IllegalArgumentException(message.getMessage());
-//        } catch (IllegalArgumentException e) {
+//        System.out.println(message.getMessage());
+        try {
+            throw new IllegalArgumentException(message.getMessage());
+        } catch (IllegalArgumentException e) {
 //            e.printStackTrace();
-//        }
+            System.out.println(e.getMessage());
+        }
     }
 }
