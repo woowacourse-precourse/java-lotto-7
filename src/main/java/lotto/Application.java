5e5b768bfb14d5c1dc9a2ed96f89ad453504a453
chanwon <clw8679@naver.com>
2024-11-03 23:00:07 +0900

    feat: Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Ticket;
+import lotto.io.Input;
+import lotto.io.Output;
+import lotto.service.TicketService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = Input.price();
+        List<Integer> winningNumbers = Input.winningNumbers();
+        int bonusNumber = Input.bonusNumber();
+
+        Ticket ticket = TicketService.generateTicket(price, winningNumbers, bonusNumber);
+        Output.lottos(ticket);
+        Output.result(ticket);
     }
 }
