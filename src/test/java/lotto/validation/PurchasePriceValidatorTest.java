748173796436231c87fbe337014d8f80bbb4380e
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-03 17:45:46 +0900

    test: 구매금액 관련 클래스 통합으로 인한 로직 수정

--- src/test/java/lotto/validation/PurchasePriceValidatorTest.java
+++ src/test/java/lotto/validation/PurchasePriceValidatorTest.java
@@ -2,6 +2,7 @@ package lotto.validation;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import lotto.domain.PurchasePrice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,7 +13,7 @@ class PurchasePriceValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"0", "-1000", "abc"})
     void 구입_금액이_자연수가_아닌_경우_예외가_발생한다(String price) {
-        assertThatThrownBy(() -> PurchasePriceValidator.validator(price))
+        assertThatThrownBy(() -> new PurchasePrice(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액이 자연수가 아닙니다.");
     }
@@ -21,7 +22,7 @@ class PurchasePriceValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"1500", "250"})
     void 구입_금액이_1000으로_나누어떨어지지_않는_경우_예외가_발생한다(String price) {
-        assertThatThrownBy(() -> PurchasePriceValidator.validator(price))
+        assertThatThrownBy(() -> new PurchasePrice(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1000원으로 나누어떨어지지 않습니다.");
     }
@@ -29,8 +30,8 @@ class PurchasePriceValidatorTest {
     @DisplayName("구입 금액이 자연수이면서 1000으로 나누어떨어지는 경우 예외가 발생하지 않는다.")
     @Test
     void 구입_금액이_자연수_이면서_1000으로_나누어떨어지는_경우_예외가_발생하지_않는다() {
-        PurchasePriceValidator.validator("1000");
-        PurchasePriceValidator.validator("5000");
-        PurchasePriceValidator.validator("100000");
+        new PurchasePrice("1000");
+        new PurchasePrice("5000");
+        new PurchasePrice("100000");
     }
 }
