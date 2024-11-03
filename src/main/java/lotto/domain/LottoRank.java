acd7b694e27334c6bbf2c19fe34c2c1f5b69e2ec
minjae k <rkdalswoals@gmail.com>
2024-11-04 01:36:55 +0900

    feat(domain): implement enum lotto rank

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.text.NumberFormat;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoRank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getFormattedPrize() {
+        return NumberFormat.getInstance().format(prize);  // 출력 시 콤마 추가
+    }
+}
