843d67a14b92526520bfa594b54ddfade09cdd88
TroyKang <kdu3840@gmail.com>
2024-11-04 03:01:26 +0900

    fix(LottoInput): Adjust output formatting to match expected result

--- src/main/java/lotto/model/LottoInput.java
+++ src/main/java/lotto/model/LottoInput.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.errors.BonusNumberErrors;
 import lotto.errors.PurchaseAmountErrors;
 import lotto.errors.WinningNumbersErrors;
@@ -13,17 +14,17 @@ import java.util.stream.Collectors;
 
 public class LottoInput {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public Integer getLottoPurchaseAmount() {
         // 로또 구입 금액 입력 받기
         System.out.println("구입금액을 입력해 주세요.");
-        String purchaseAmount = scanner.nextLine();
+        String purchaseAmount = Console.readLine();
 
         PurchaseAmountErrors errorHandler = new PurchaseAmountErrors();
         errorHandler.errorCheck(purchaseAmount);
 
         Integer lottoAmount = Integer.parseInt(purchaseAmount) / 1000;
+
+        System.out.println();
         System.out.println(lottoAmount + "개를 구매했습니다.");
 
         return lottoAmount;
@@ -31,7 +32,7 @@ public class LottoInput {
 
     public List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        String winningNumbersInput = scanner.nextLine();
+        String winningNumbersInput = Console.readLine();
 
         // 숫자 이외의 값을 입력했을 경우
         if (!winningNumbersInput.matches("^(\\d+\\s*,\\s*)*\\d+$")) {
@@ -57,7 +58,7 @@ public class LottoInput {
     public Integer inputBonusNumber(List<Integer> winningNumbers) {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
-        String bonusNumber = scanner.nextLine();
+        String bonusNumber = Console.readLine();
 
         BonusNumberErrors errorHandler = new BonusNumberErrors();
         errorHandler.errorCheck(bonusNumber, winningNumbers);
