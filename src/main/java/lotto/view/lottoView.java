7a3af74d5cf981b09816fa4a2924f74f5f5481ba
jinno Yun <jinno321@naver.com>
2024-11-03 15:53:57 +0900

    feat: lottoView 생성 및 구입금액 입력받는 메서드 생성

---
+++ src/main/java/lotto/view/lottoView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class lottoView {
+
+    public static void purchaseInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String s = Console.readLine();
+    }
+
+
+}
