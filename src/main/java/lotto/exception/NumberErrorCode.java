0762d74a2e0684074bda5df82c3696e02757a06d
gikhoon <ghikhoon@naver.com>
2024-11-04 02:43:43 +0900

    refactor: 숫자가 아닐 때 에러 메세지 수정

--- src/main/java/lotto/exception/NumberErrorCode.java
+++ src/main/java/lotto/exception/NumberErrorCode.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public enum NumberErrorCode {
-    NUMBER_PARSE_ERROR("당첨번호는 숫자여야 합니다"),
+    NUMBER_PARSE_ERROR("숫자가 아닙니다."),
     NUMBER_DUPLICATE_ERROR("중복되는 숫자가 있습니다."),
     NUMBER_COUNT_ERROR("숫자 갯수가 맞지 않습니다"),
     NUMBER_RANGE_ERROR("숫자 범위가 맞지 않습니다");
