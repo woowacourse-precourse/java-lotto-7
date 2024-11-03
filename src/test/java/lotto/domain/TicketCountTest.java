7995f3d6ebff53540b21050c8d69441f30e6541b
taek2222 <oot309@daum.net>
2024-11-04 08:49:52 +0900

    test(TicketCountTest): 티켓 갯수 객체 테스트
    - 기존 테스트에 추가된 메서드로 새로 테스트를 진행했습니다.

--- src/test/java/lotto/domain/TicketCountTest.java
+++ src/test/java/lotto/domain/TicketCountTest.java
@@ -1,22 +1,33 @@
 package lotto.domain;
 
-import static lotto.domain.PurchaseAmount.LOTTO_PRICE;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TicketCountTest {
 
     @Test
-    void 금액에_알맞는_로또_티켓을_구매한다() {
-        // given
-        int purchaseAmount = 5000;
+    void 유효한_금액으로_티켓_개수를_계산한다() {
+        TicketCount ticketCount = new TicketCount("5000");
+        assertThat(ticketCount.getCount()).isEqualTo(5);
+    }
 
-        // when
-        TicketCount ticketCount = new TicketCount(purchaseAmount);
+    @Test
+    void 유효하지_않은_금액으로_티켓_개수를_계산할_때_예외가_발생한다() {
+        assertThatThrownBy(() -> new TicketCount("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        // then
-        Assertions.assertThat(ticketCount.getCount())
-                .isEqualTo(purchaseAmount / LOTTO_PRICE);
+    @Test
+    void 금액이_0일_때_예외가_발생한다() {
+        assertThatThrownBy(() -> new TicketCount("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 금액이_숫자가_아닐_때_예외가_발생한다() {
+        assertThatThrownBy(() -> new TicketCount("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
