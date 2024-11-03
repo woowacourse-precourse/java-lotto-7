0e123bcbc7cc1ba2c1b9aff5867b503e6c836186
JiEung2 <181757@jnu.ac.kr>
2024-11-04 01:26:02 +0900

    feat(outputView): 당첨 통계를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,17 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoRanking;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
     private static final String SEPARATOR_LINE = "---";
+    private static final String COUNT_UNIT = "개";
+    private static final String HYPHEN = " - ";
 
     public static void println(String message) {
         System.out.println(message);
@@ -20,5 +24,11 @@ public class OutputView {
 
     private static void printLotto(Lotto lotto) {
         println(lotto.getNumbers().toString());
+    }
+
+    private static void printWinningDetails(Map<LottoRanking, Integer> rankingCount) {
+        for (LottoRanking lottoRanking : LottoRanking.values()) {
+            println(lottoRanking.getMessage() + HYPHEN + rankingCount.getOrDefault(lottoRanking, 0) + COUNT_UNIT);
+        }
     }
 }
