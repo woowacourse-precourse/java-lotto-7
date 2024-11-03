dde5c79f5a1a8b64464767e507641f738c8fe3a7
이희수 <hslee9234@naver.com>
2024-11-04 07:15:33 +0900

    feat: Applicaion

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.DefaultLottoGenerator;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoResultCalculator;
+import lotto.service.LottoService;
+import lotto.util.validator.InputValidator;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGenerator lottoGenerator = new DefaultLottoGenerator();
+        LottoResultCalculator calculator = new LottoResultCalculator();
+        InputValidator inputValidator = new InputValidator();
+
+        LottoService lottoService = new LottoService(lottoGenerator, calculator);
+
+        LottoController lottoController = new LottoController(lottoService, inputValidator);
+
+        lottoController.startLottoGame();
     }
 }
