280017ba8197745ef4df5e4dbc4c66188343f2f4
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 03:21:54 +0900

    refactor: 구매 안내 문구 및 사용자에게 입력받고 정수로 반환

--- src/main/java/lotto/UserInputLotto.java
+++ src/main/java/lotto/UserInputLotto.java
@@ -32,4 +32,9 @@ public class UserInputLotto {   // 사용자가 입력하는 값에 대한 클�
 
         return bonus;
     }
+
+    private int purchaseAmountRead() {
+        System.out.println("얼마어치를 구매하시겠습니까? (1,000원 단위)");
+        int amount = Integer.parseInt(Console.readLine());
+    }
 }
