2120cb4cbbda1704d14696bc9ee3caf82d03fac7
장혜민 <gpals5987@gmail.com>
2024-11-03 10:38:15 +0900

    [feat] 의존성 주입 방식으로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,17 @@
 package lotto;
 
 import lotto.contoller.LottoController;
+import lotto.service.ProfitCalculatorService;
+import lotto.service.WinningStatisticsService;
+import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController.getInstance().start();
+        LottoController lottoController = new LottoController(
+                LottoService.getInstance(),
+                WinningStatisticsService.getInstance(),
+                ProfitCalculatorService.getInstance());
+        lottoController.start();
     }
 }
