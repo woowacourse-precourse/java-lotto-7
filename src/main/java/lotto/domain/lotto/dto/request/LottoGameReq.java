d5bf674192442d67812f00dd158ab31cb95440fa
ChiSaeHwang <cwh730909@gmail.com>
2024-11-03 21:28:19 +0900

    refactor(LottoGameReq): LottoGameReq 불필요한 필드 삭제

--- src/main/java/lotto/domain/lotto/dto/request/LottoGameReq.java
+++ src/main/java/lotto/domain/lotto/dto/request/LottoGameReq.java
@@ -3,34 +3,26 @@ package lotto.domain.lotto.dto.request;
 import java.util.List;
 
 public class LottoGameReq {
-    private final List<List<Integer>> purchasedNumbers;
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
     private final int cost;
 
     private LottoGameReq(
-            List<List<Integer>> purchasedNumbers,
+            int cost,
             List<Integer> winningNumbers,
-            int bonusNumber,
-            int cost
+            int bonusNumber
     ) {
-        this.purchasedNumbers = purchasedNumbers;
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
         this.cost = cost;
     }
 
     public static LottoGameReq of(
-            List<List<Integer>> purchasedNumbers,
-            List<Integer> winningNumbers,
-            int bonusNumber,
-            int cost
+            final int cost,
+            final List<Integer> winningNumbers,
+            final int bonusNumber
     ) {
-        return new LottoGameReq(purchasedNumbers, winningNumbers, bonusNumber, cost);
-    }
-
-    public List<List<Integer>> getPurchasedNumbers() {
-        return purchasedNumbers;
+        return new LottoGameReq(cost, winningNumbers, bonusNumber);
     }
 
     public List<Integer> getWinningNumbers() {
