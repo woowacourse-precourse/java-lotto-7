25b8d708832a1367707b908f12593c3c03483c78
junhaa <2171326@hansung.ac.kr>
2024-11-04 01:58:19 +0900

    feat(LottoController): 구입 금액 단위 검증 기능
    - 1000원 단위가 아닌 경우 예외를 반환하도록 기능 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,34 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private static final String PURCHASE_AMOUNT_ERROR_MESSAGE = "[ERROR] 구입 금액은 %d원 단위여야 합니다.";
+    private static final Integer LOTTO_PURCHASE_COST = 1000;
+
+    public void run() {
+        Integer lottoCount = buyLotto();
+    }
+
+    private Integer buyLotto() {
+        Integer totalCost = getUserPurchaseCost();
+        return getLottoAmount(totalCost);
+    }
+
+    private Integer getLottoAmount(Integer cost) {
+        if (cost % LOTTO_PURCHASE_COST != 0) {
+            throw new IllegalArgumentException(String.format(PURCHASE_AMOUNT_ERROR_MESSAGE, LOTTO_PURCHASE_COST));
+        }
+
+        return cost / LOTTO_PURCHASE_COST;
+    }
+
+    private Integer getUserPurchaseCost() {
+        OutputView.getInstance().printPurchaseInput();
+        return Integer.parseInt(InputView.getInstance().readUserInput());
+    }
+
+
+}
---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,26 @@
+package lotto.controller;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoControllerTest {
+
+    @DisplayName("구입금액이 1000원 단위가 아니면 예외를 반환한다.")
+    @Test
+    void 구입금액이_1000원_단위가_아니면_예외를_반환한다(){
+        LottoController lottoController = new LottoController();
+        String input = "6500";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> lottoController.run())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+}
