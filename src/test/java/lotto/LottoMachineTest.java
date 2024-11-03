55ce6f5ab8687a8a7d16837d40e25430680b554e
johyewon <johyewon4601@gmail.com>
2024-11-04 00:55:20 +0900

    test(LottoMachineTest): 유효한 로또 금액 테스트 추가

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,4 +1,15 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LottoMachineTest {
+    @Test
+    void 로또_구매_금액이_유효한_경우() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = lottoMachine.issueLottos(5000);
+
+        assertEquals(5, lottos.size());
+    }
 }
