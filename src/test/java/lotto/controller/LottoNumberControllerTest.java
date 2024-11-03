39dd0dd9e207c5a5a43e7cd7d5ef21df0f466b9b
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-03 22:55:27 +0900

    refactor(controller, model): LottoBonusTest 파일 삭제 및 LottoNumberControllerTest에 내용 추가

--- src/test/java/lotto/controller/LottoNumberControllerTest.java
+++ src/test/java/lotto/controller/LottoNumberControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.model.Lotto;
+import lotto.model.LottoBonus;
 
 public class LottoNumberControllerTest {
 
@@ -60,5 +61,39 @@ public class LottoNumberControllerTest {
         assertThatThrownBy(() -> lottoNumberController.generateWinningLotto(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 당첨 번호는 쉼표로 구분된 6개의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("유효한 보너스 번호와 당첨 번호가 입력된 경우 LottoBonus 객체가 생성된다.")
+    void should_CreateLottoBonusInstance_When_BonusNumberIsValid() {
+        Lotto winningLotto = lottoNumberController.generateWinningLotto("1,2,3,4,5,6");
+        LottoBonus bonus = lottoNumberController.generateBonusNumber("7", winningLotto.getNumbers());
+
+        assertThat(bonus).isNotNull();
+        assertThat(bonus.getBonusNumber()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 1 미만이거나 45 초과일 경우 예외가 발생한다.")
+    void should_ThrowException_When_BonusNumberOutOfRange() {
+        Lotto winningLotto = lottoNumberController.generateWinningLotto("1,2,3,4,5,6");
+
+        assertThatThrownBy(() -> lottoNumberController.generateBonusNumber("0", winningLotto.getNumbers()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+
+        assertThatThrownBy(() -> lottoNumberController.generateBonusNumber("46", winningLotto.getNumbers()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    void should_ThrowException_When_BonusNumberIsDuplicateWithWinningNumbers() {
+        Lotto winningLotto = lottoNumberController.generateWinningLotto("1,2,3,4,5,6");
+
+        assertThatThrownBy(() -> lottoNumberController.generateBonusNumber("5", winningLotto.getNumbers()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
     }
 }
