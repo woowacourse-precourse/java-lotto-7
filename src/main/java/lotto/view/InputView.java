579b0e70ab289e2ffbb034fcef1693be1fc36b0c
DongYeop <s01054830455@gmail.com>
2024-11-03 18:55:48 +0900

    feat(InputConverter): 입력 문자열의 형 변환을 하는 클래스를 추가
    - 입력 문자열을 받아서 유효성 검사를 하고, 형 변환을 한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,50 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.List;
-import lotto.exception.io.InputValidation;
 
 public class InputView {
 
-    private static final String WINNING_LOTTO_NUMBERS_DELIMITER = ",";
-
     public int inputPurchaseAmount() {
         String purchaseAmount = Console.readLine();
-
-        InputValidation.validateNullOrEmpty(purchaseAmount);
-        InputValidation.validateContainBlank(purchaseAmount);
-        InputValidation.validateNumeric(purchaseAmount);
-
-        return Integer.parseInt(purchaseAmount);
+        return InputConverter.parseToNumber(purchaseAmount);
     }
 
     public List<Integer> inputWinningLottoNumbers() {
         String winningNumbers = Console.readLine();
-
-        InputValidation.validateNullOrEmpty(winningNumbers);
-        InputValidation.validateContainBlank(winningNumbers);
-        InputValidation.validateDelimiter(winningNumbers);
-
-        String[] split = winningNumbers.split(WINNING_LOTTO_NUMBERS_DELIMITER);
-
-        List<Integer> winningLottoNumbers = new ArrayList<>();
-
-        for (String number : split) {
-            int parseNumber = Integer.parseInt(number);
-            winningLottoNumbers.add(parseNumber);
-        }
-
-        return winningLottoNumbers;
+        return InputConverter.convertToNumbers(winningNumbers);
     }
 
     public int inputBonusNumber() {
         String bonusNumber = Console.readLine();
-
-        InputValidation.validateNullOrEmpty(bonusNumber);
-        InputValidation.validateContainBlank(bonusNumber);
-        InputValidation.validateNumeric(bonusNumber);
-
-        return Integer.parseInt(bonusNumber);
+        return InputConverter.parseToNumber(bonusNumber);
     }
 }
