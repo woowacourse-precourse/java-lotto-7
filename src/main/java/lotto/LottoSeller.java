16745771227a29aee48c3c7166ad3df5e1c2de86
SolfE <love23869318@gmail.com>
2024-11-04 02:32:02 +0900

    feat: add winner number read

--- src/main/java/lotto/LottoSeller.java
+++ src/main/java/lotto/LottoSeller.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoSeller {
@@ -9,6 +10,7 @@ public class LottoSeller {
     public void run() {
         int pay = readPay();
         List<Lotto> lottos = buyLottos(pay);
+        Lotto winnerLotto = readWinnerLotto();
     }
 
     private int readPay() {
@@ -49,5 +51,32 @@ public class LottoSeller {
             System.out.println(lotto);
         }
         return lottos;
+    }
+
+    private Lotto readWinnerLotto() {
+        Lotto winner;
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                List<Integer> numbers = parseWinnerLotto(Console.readLine());
+                winner = new Lotto(numbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return winner;
+    }
+
+    private List<Integer> parseWinnerLotto(String input) throws IllegalArgumentException {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            for (String token : input.split(",")) {
+                numbers.add(Integer.parseInt(token));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 콤마(,)를 제외한 문자는 입력할 수 없습니다.");
+        }
+        return numbers;
     }
 }
