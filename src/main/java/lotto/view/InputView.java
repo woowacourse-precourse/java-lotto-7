51398947b190b1a3b6e64d5dfc729cf8b09021de
CHISANW <keuye06380618@gmail.com>
2024-11-03 16:45:14 +0900

    feat(InputView): 당첨 번호 입력 기능 추가
    - winningNumberInput() 메서드를 통해 당첨 번호 입력 기능 구현
    - readWinningNumbers() 메서드에서 유효성 검사를 통해 입력된 당첨 번호가 올바른 형식인지 확인후 올바르지 않다면 재귀를 통해 재입력 구현
    - getWinningNumbers() 입력값을 콤마로 구분하여 숫자로 변환하고, 유효하지 않은 경우 예외 메시지를 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,16 +3,26 @@ package lotto.view;
 import static lotto.vaildate.Validate.*;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import lotto.vaildate.Validate;
 
 public class InputView {
 
     private static final String START_PRICE_INPUT = "구입금액을 입력해 주세요.";
+    private static final String START_WINNING_NUMBERS_INPUT = "당첨 번호를 입력해 주세요.";
+    private static final String PREFIX = ",";
 
     public int buyLottoPriceInput() {
         System.out.println(START_PRICE_INPUT);
         return readLottoPrice();
     }
 
+    public List<Integer> winningNumberInput() {
+        System.out.println(START_WINNING_NUMBERS_INPUT);
+        return readWinningNumbers();
+    }
+    
     private int readLottoPrice() {
         try {
             return purchasePriceValidate(consoleReadLine());
@@ -20,6 +30,25 @@ public class InputView {
             printErrorMessage(e);
             return readLottoPrice();
         }
+    }
+
+    private List<Integer> readWinningNumbers() {
+        try {
+            return readAndValidateWinningNumbers();
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e);
+            return readWinningNumbers();
+        }
+    }
+
+    private List<Integer> readAndValidateWinningNumbers() {
+        return winningNumbersValidate(getWinningNumbers());
+    }
+
+    private List<Integer> getWinningNumbers() {
+        return Arrays.stream(consoleReadLine().split(PREFIX))
+                .map(Validate::parseIntegerValidate)
+                .toList();
     }
 
     private static String consoleReadLine() {
