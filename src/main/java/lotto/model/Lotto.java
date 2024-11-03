0734c33475c02e3ab59da2c1f58ebbf10e200e33
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-03 20:57:29 +0900

    feat: Lotto validate 메서드 수정 - validateNumbersCount와 validateDuplicateNumbers로 분리 - 로또 번호는 6개여야 한다. - 로또 번호는 중복될 수 없다.

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import lotto.util.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -15,8 +18,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumbersCount(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private void validateNumbersCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_COUNT.getMessage());
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBERS.getMessage());
         }
     }
 }
