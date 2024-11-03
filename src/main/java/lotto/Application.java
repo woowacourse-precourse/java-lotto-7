94a1774bb58f18adef5acd371c13c774821e19c0
wodus4145@naver.com <wodus4145@naver.com>
2024-11-03 21:45:36 +0900

    feat: Write the main() function
    프로그램의 진입점과 흐름을 제어하는 main() 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler handler = new InputHandler();
+        LotteryMachine machine = new LotteryMachine();
+        LottoResultCalculator calculator = new LottoResultCalculator();
+
+        int price = handler.getPrice();
+        Lotto winningLotto = handler.getWinningNumbers();
+        int bonusNumber = handler.getBonusNumber(winningLotto);
+
+        ArrayList<Lotto> myLotto = machine.generateLottoList(price);
+
+        calculator.calculateWinnings(myLotto, winningLotto, bonusNumber, price);
     }
 }
