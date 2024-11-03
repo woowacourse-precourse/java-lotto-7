37a39d8c82427affe4b62dbbefb0ff7a4757cdd7
heehunjung <slrkqk2@naver.com>
2024-11-04 00:57:35 +0900

    feat(InputErrorMessage): 보너스 번호 중복 체크

--- src/main/java/lotto/global/error/InputErrorMessages.java
+++ src/main/java/lotto/global/error/InputErrorMessages.java
@@ -12,7 +12,8 @@ public enum InputErrorMessages {
 
     INVALID_LOTTO_NUMBER_FORMAT("[ERROR] ", "로또 번호에는 문자가 포함되면 안됩니다."),
     INVALID_LOTTO_NUMBER_RANGE("[ERROR] ", "로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_LOTTO_NUMBER_DUPLICATE("[ERROR] ", "당첨 번호에 중복된 숫자가 있습니다.");
+    INVALID_LOTTO_NUMBER_DUPLICATE("[ERROR] ", "당첨 번호에 중복된 숫자가 있습니다."),
+    INVALID_BONUS_NUMBER_DUPLICATE("[ERROR] ", "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private final String message;
     private final String code;
