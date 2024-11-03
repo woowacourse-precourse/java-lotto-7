5fb7754634961f761f77ac919907c7ad8fddb61a
corpi <lj845854@gmail.com>
2024-11-03 14:12:32 +0900

    test(LottoTest): 당첨로또에 해당하는 등수를 반환할 수 있다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,17 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.stream.Stream;
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.model.WinningLotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -51,6 +55,17 @@ class LottoTest {
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("당첨로또에 해당하는 등수를 반환할 수 있다.")
+    @ParameterizedTest
+    @MethodSource("generateLottoListAndWinningLotto")
+    void 당첨로또에_해당하는_등수를_반환할_수_있다(Lotto lotto, WinningLotto winningLotto, LottoRank expectedRank) {
+        // when
+        LottoRank actualRank = lotto.getRankFrom(winningLotto);
+
+        // then
+        Assertions.assertThat(actualRank).isEqualTo(expectedRank);
+    }
+
     private static Stream<List<Integer>> invalidSizeWinningNumberList() {
         return Stream.of(
                 List.of(),
@@ -60,5 +75,25 @@ class LottoTest {
                 List.of(1, 2, 3, 4),
                 List.of(1, 2, 3, 4, 5)
         );
+    }
+
+    private static Stream<Arguments> generateLottoListAndWinningLotto() {
+        WinningLotto winningNumber = new WinningLotto(
+                new Lotto(List.of(7, 9, 13, 26, 29, 43)), 11
+        );
+
+        HashMap<Lotto, LottoRank> lottoResult = new HashMap<>();
+        lottoResult.put(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoRank.LOSE);
+        lottoResult.put(new Lotto(List.of(7, 8, 11, 14, 15, 16)), LottoRank.LOSE);
+        lottoResult.put(new Lotto(List.of(7, 9, 11, 14, 15, 16)), LottoRank.LOSE);
+        lottoResult.put(new Lotto(List.of(7, 9, 13, 14, 15, 16)), LottoRank.RANK_5);
+        lottoResult.put(new Lotto(List.of(7, 9, 13, 26, 27, 28)), LottoRank.RANK_4);
+        lottoResult.put(new Lotto(List.of(7, 9, 13, 26, 29, 36)), LottoRank.RANK_3);
+        lottoResult.put(new Lotto(List.of(7, 9, 11, 13, 26, 29)), LottoRank.RANK_2);
+        lottoResult.put(new Lotto(List.of(7, 9, 11, 26, 29, 43)), LottoRank.RANK_2);
+        lottoResult.put(new Lotto(List.of(7, 9, 13, 26, 29, 43)), LottoRank.RANK_1);
+
+        return lottoResult.entrySet().stream()
+                .map(lotto -> Arguments.of(lotto.getKey(), winningNumber, lotto.getValue()));
     }
 }
