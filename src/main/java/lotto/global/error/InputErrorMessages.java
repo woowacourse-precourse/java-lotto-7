ab8b64011d8de461cda4a51ef2d8c0f2aff788f0
heehun <slrkqk2@naver.com>
2024-11-03 15:31:52 +0900

    refactor(ErroMessage): toString() 오버라이딩 삭제, getMessage() 생성

--- src/main/java/lotto/global/error/InputErrorMessages.java
+++ src/main/java/lotto/global/error/InputErrorMessages.java
@@ -1,26 +1,27 @@
 package lotto.global.error;
 
 public enum InputErrorMessages {
-    INVALID_INPUT_FORMAT_NULL("구입 금액 입력값이 비어 있습니다."),
+    INVALID_INPUT_FORMAT_NULL("[ERROR] ", "구입 금액 입력값이 비어 있습니다."),
 
-    INVALID_PRICE_FORMAT("로또 구입 금액에는 문자가 포함되면 안됩니다."),
-    INVALID_PRICE_FORMAT_UNIT("로또 구입 금액은 1000원 단위의 금액만 가능합니다."),
-    INVALID_PRICE_FORMAT_NEGATIVE("로또 구입 금액은 양수 값만 가능합니다."),
+    INVALID_PRICE_FORMAT("[ERROR] ","로또 구입 금액에는 문자가 포함되면 안됩니다."),
+    INVALID_PRICE_FORMAT_UNIT("[ERROR] ","로또 구입 금액은 1000원 단위의 금액만 가능합니다."),
+    INVALID_PRICE_FORMAT_NEGATIVE("[ERROR] ","로또 구입 금액은 양수 값만 가능합니다."),
 
-    INVALID_WINNING_NUMBER_PATTERN("당첨 번호는 쉼표로 구분된 숫자 형식이어야 합니다."),
-    INVALID_WINNING_NUM_COUNT("당첨 번호는 6개의 숫자를 입력해야 합니다."),
+    INVALID_WINNING_NUMBER_PATTERN("[ERROR] ","당첨 번호는 쉼표로 구분된 숫자 형식이어야 합니다."),
+    INVALID_WINNING_NUM_COUNT("[ERROR] ","당첨 번호는 6개의 숫자를 입력해야 합니다."),
 
-    INVALID_LOTTO_NUMBER_FORMAT("로또 번호에는 문자가 포함되면 안됩니다."),
-    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    INVALID_LOTTO_NUMBER_FORMAT("[ERROR] ","로또 번호에는 문자가 포함되면 안됩니다."),
+    INVALID_LOTTO_NUMBER_RANGE("[ERROR] ","로또 번호는 1부터 45 사이의 숫자여야 합니다.");
 
     private final String message;
+    private final String code;
 
-    InputErrorMessages(String message) {
+    InputErrorMessages(String code, String message) {
         this.message = message;
+        this.code = code;
     }
 
-    @Override
-    public String toString() {
-        return "[ERROR] " + message;
+    public String getMessage() {
+        return code + message;
     }
 }
--- src/main/java/lotto/global/error/LottoErrorMessages.java
+++ src/main/java/lotto/global/error/LottoErrorMessages.java
@@ -2,16 +2,19 @@ package lotto.global.error;
 
 public enum LottoErrorMessages {
 
-    INVALID_MATCH_COUNT("유효하지 않은 매치 카운트입니다.");
+    INVALID_MATCH_COUNT("[ERROR] ","유효하지 않은 매치 카운트입니다."),
+    INVALID_LOTTO_NUMBER("[ERROR] ","로또 번호는 6개여야 합니다."),
+    INVALID_LOTTO_RANGE("[ERROR] ","로또 번호에는 중복된 숫자가 없어야 합니다.");
 
     private final String message;
+    private final String code;
 
-    LottoErrorMessages(String message) {
+    LottoErrorMessages(String code, String message) {
         this.message = message;
+        this.code = code;
     }
 
-    @Override
-    public String toString() {
-        return "[ERROR] " + message;
+    public String getMessage() {
+        return code + message;
     }
 }
