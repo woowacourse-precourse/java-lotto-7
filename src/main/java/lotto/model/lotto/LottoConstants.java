e2cf6726ea35741e522649160b79b75305447177
choi <nippert@naver.com>
2024-11-03 23:58:29 +0900

    feat: 로또에 필요한 상수 정의

---
+++ src/main/java/lotto/model/lotto/LottoConstants.java
@@ -0,0 +1,8 @@
+package lotto.model.lotto;
+
+public class LottoConstants {
+    public static final String LOTTO_PRINT_DELIMITER = ", ";
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_NUM_MAX = 45;
+    public static final int LOTTO_NUM_MIN = 1;
+}
