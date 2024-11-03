20ac50c23b90df75634e4887814d724dab921e86
dd-jiyun <dlwldbs8015@gmail.com>
2024-11-03 19:13:26 +0900

    feat: 로또 게임 관리 기능
    - 로또 게임을 전반적으로 관리합니다.

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,60 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.model.calculator.PrizeCalculator;
+import lotto.model.calculator.RateOfReturnCalculator;
+import lotto.model.handler.PurchaseRequestHandler;
+import lotto.model.handler.WinningLottoRequestHandler;
+import lotto.model.lotto.LottoResult;
+import lotto.model.lotto.LottoResultChecker;
+import lotto.model.lotto.Lottos;
+import lotto.model.winning.WinningNumbersAndBonusNumber;
+import lotto.model.rank.RankChecker;
+import lotto.model.rank.RankResult;
+
+public class LottoService {
+    private final PurchaseRequestHandler purchaseRequestHandler;
+    private final WinningLottoRequestHandler winningLottoRequestHandler;
+
+    public LottoService(PurchaseRequestHandler purchaseRequestHandler,
+                        WinningLottoRequestHandler winningLottoRequestHandler) {
+        this.purchaseRequestHandler = purchaseRequestHandler;
+        this.winningLottoRequestHandler = winningLottoRequestHandler;
+    }
+
+    public int getPurchaseCount(String amount) {
+        return purchaseRequestHandler.getPurchaseAmount(amount);
+    }
+
+    public List<Integer> getWinningNumbers(List<String> lottoInput) {
+        return winningLottoRequestHandler.getWinningNumbers(lottoInput);
+    }
+
+    public int getBonusNumber(String bonusInput, List<Integer> winningNumbers) {
+        return winningLottoRequestHandler.getBonusNumber(bonusInput, winningNumbers);
+    }
+
+    public Lottos generateLottos(int count) {
+        return Lottos.generate(count);
+    }
+
+    public LottoResultChecker createResultChecker(WinningNumbersAndBonusNumber winningNumbersAndBonusNumber) {
+        return new LottoResultChecker(winningNumbersAndBonusNumber);
+    }
+
+    public RankResult checkRanks(List<LottoResult> lottoResults) {
+        RankChecker rankChecker = new RankChecker();
+        return rankChecker.check(lottoResults);
+    }
+
+    public double calculateTotalPrize(RankResult rankResult) {
+        PrizeCalculator prizeCalculator = new PrizeCalculator();
+        return prizeCalculator.calculate(rankResult);
+    }
+
+    public double calculateRateOfReturn(int count, double totalPrize) {
+        RateOfReturnCalculator rateOfReturnCalculator = new RateOfReturnCalculator();
+        return rateOfReturnCalculator.calculate(count, totalPrize);
+    }
+
+}
