3c8003975caabe07df9535a05c993cd15ebdfd2a
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-04 00:41:01 +0900

    feat(domain): 당첨 번호 검증 기능
    - 필드 getter 생성
    - 당첨 번호와 보너스 번호 중복 검증 기능 수정

--- src/main/java/lotto/model/domain/LottoWinningNumbers.java
+++ src/main/java/lotto/model/domain/LottoWinningNumbers.java
@@ -1,21 +1,31 @@
 package lotto.model.domain;
 
-import java.util.List;
-
-public class LottoWinningNumbers extends Lotto{
-
+public class LottoWinningNumbers {
+    private final Lotto winningLottoNumbers;
     private final BonusNumber bonusNumber;
 
-    public LottoWinningNumbers(List<Integer> numbers, BonusNumber bonusNumber) {
-        super(numbers);
-        bonusNumberDuplicateValidator(numbers, bonusNumber);
+    public LottoWinningNumbers(Lotto winningLottoNumbers, BonusNumber bonusNumber) {
+        this.winningLottoNumbers = winningLottoNumbers;
+        bonusNumberDuplicateValidator(winningLottoNumbers, bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
-    private void bonusNumberDuplicateValidator(List<Integer> numbers, BonusNumber bonusNumber) {
-        if (numbers.contains(bonusNumber)) {
+    private void bonusNumberDuplicateValidator(Lotto winningLottoNumbers, BonusNumber bonusNumber) {
+        if (winningLottoNumbers.getNumbers().contains(bonusNumber.getBonusNumber())) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
         }
+    }
+
+    public boolean isContainsWinningNumbers(int number) {
+        return winningLottoNumbers.getNumbers().contains(number);
+    }
+
+    public boolean isContainsBonusNumber(int number) {
+        return bonusNumber.getBonusNumber() == number;
+    }
+
+    public Lotto getWinningLottoNumbers() {
+        return winningLottoNumbers;
     }
 
     public BonusNumber getWinningBonusNumber() {
