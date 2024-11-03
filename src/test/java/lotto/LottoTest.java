5f1ede4b6f5610473bd33bbf361632ff8a74561f
qyinm <qusseun@gmail.com>
2024-11-03 16:38:18 +0900

    test: 두 로또 번호 동일한 번호 개수 반환 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.domain.Number;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -44,5 +46,30 @@ class LottoTest {
 
         // Then
         assertThat(isContain).isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTwoLottoAndExpectedResult")
+    void 두_로또_번호_동일한_번호_개수_반환한다(Lotto firstLotto, Lotto secondLotto, long expectedResult) {
+        // Given & When
+        long countSameNumbers = firstLotto.countSameNumbers(secondLotto);
+
+        // Then
+        assertThat(countSameNumbers).isEqualTo(expectedResult);
+    }
+
+    static Stream<Object[]> provideTwoLottoAndExpectedResult() {
+        return Stream.of(
+                new Object[] {
+                      new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                      new Lotto(List.of(7, 8, 9, 10, 11, 12)),
+                      0
+                },
+                new Object[] {
+                        new Lotto(List.of(1, 2, 3, 5, 8, 10)),
+                        new Lotto(List.of(1, 2, 9, 10, 11, 12)),
+                        3
+                }
+        );
     }
 }
