bc8d7fbcb00506d5c9d7521588dac70935d804a8
LaiTial <asna90@naver.com>
2024-11-04 01:25:50 +0900

    validate buy money

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,8 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // 입력 받기
-        System.out.println("구입금액을 입력해 주세요.");
-        String buy = Console.readLine();  // 구입 금액
+        int buy = Input.buyMoney(); // 구입 금액 입력 받기
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String lotto = Console.readLine(); // 당첨 번호
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,4 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+
+    public static int buyMoney() {
+
+        String buy;
+
+        while(true) {
+
+            System.out.println("구입금액을 입력해 주세요.");
+            buy = Console.readLine();  // 구입 금액
+
+            try {
+                validateAmount(buy); // 구입 금액 검증
+                break; // 유효한 입력일 경우 while 종료
+            } 
+            
+            catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // 예외 메시지만 출력
+            }}
+
+        return Integer.parseInt(buy);
+    }
+
+    // 구입 금액이 유효한지 검증
+    private static void validateAmount(String input) {
+        int amount = Integer.parseInt(input); // 숫자로 변환
+        if (amount % 1000 != 0) { // 1,000원 단위가 아닐 시 예외 처리
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
 }
