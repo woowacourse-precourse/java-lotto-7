7ae1027fa119beef8555745008b57fbf1e967fe8
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-11-03 19:37:35 +0900

    refactor(LottoController): Application의 로직을 LottoController로 이동
    - main 메서드가 너무 많은 일을 처리해서 controller로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,34 +1,10 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.domain.LottoMachine;
-import lotto.domain.Lottos;
-import lotto.domain.Result;
-import lotto.domain.lottoForm.WinningNumbers;
-import lotto.handler.MoneyInputHandler;
-import lotto.handler.WinningNumbersInputHandler;
-import lotto.view.OutputView;
-
-import static lotto.view.OutputView.showLottosInfo;
 
 public class Application {
     public static void main(String[] args) {
-        MoneyInputHandler moneyInputHandler = new MoneyInputHandler();
-        long lottoCount = moneyInputHandler.getLottoCount();
-
-        LottoMachine lottoMachine = new LottoMachine();
-        Lottos lottos = lottoMachine.issue(lottoCount);
-        showLottosInfo(lottos);
-
-        WinningNumbersInputHandler NumbersInputHandler = new WinningNumbersInputHandler();
-        WinningNumbers winningNumbers = NumbersInputHandler.getWinningNumbers();
-        int bonusNumber = NumbersInputHandler.getBonusNumber(winningNumbers);
-
-        LottoController lottoController = new LottoController(lottos, winningNumbers, bonusNumber);
-        lottoController.evaluate();
-
-        OutputView.showResult();
-        long profitSum = Result.getProfitSum();
-        OutputView.showProfitRate(lottoCount, profitSum);
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,35 @@
 package lotto.controller;
 
+import lotto.domain.LottoMachine;
 import lotto.domain.Lottos;
+import lotto.domain.Result;
 import lotto.domain.lottoForm.WinningNumbers;
+import lotto.handler.MoneyInputHandler;
+import lotto.handler.WinningNumbersInputHandler;
+
+import static lotto.view.OutputView.*;
 
 public class LottoController {
+    private final MoneyInputHandler moneyInputHandler;
+    private final LottoMachine lottoMachine;
+    private final WinningNumbersInputHandler numbersInputHandler;
 
-    private final Lottos lottos;
-    private final WinningNumbers winningNumbers;
-    private final int bonusNumber;
-
-    public LottoController(Lottos lottos, WinningNumbers winningNumbers, int bonusNumber) {
-        this.lottos = lottos;
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+    public LottoController() {
+        moneyInputHandler = new MoneyInputHandler();
+        lottoMachine = new LottoMachine();
+        numbersInputHandler = new WinningNumbersInputHandler();
     }
 
-    public void evaluate() {
+    public void run() {
+        long lottoCount = moneyInputHandler.getLottoCount();
+        Lottos lottos = lottoMachine.issue(lottoCount);
+        showLottosInfo(lottos);
+
+        WinningNumbers winningNumbers = numbersInputHandler.getWinningNumbers();
+        final int bonusNumber = numbersInputHandler.getBonusNumber(winningNumbers);
         lottos.compare(winningNumbers, bonusNumber);
+
+        showResult();
+        showProfitRate(lottoCount, Result.getProfitSum());
     }
 }
