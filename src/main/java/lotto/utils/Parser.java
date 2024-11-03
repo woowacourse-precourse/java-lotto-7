a8a20e98db3c518c310ed08a7baac4c8baa27f7e
kkmdevel <kkm.devel@gmail.com>
2024-11-03 14:40:59 +0900

    feat(Parser): 문자열 입력 검증 기능 추가
    - 비어있는 문자열 입력 시 예외 처리 기능 구현
    - 숫자가 아닌 문자열 입력 시 예외 처리 기능 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,12 +1,25 @@
 package lotto.utils;
 
+import lotto.exception.ParserException;
+import lotto.exception.message.ParserExceptionMessage;
+
 public final class Parser {
 
     private Parser() {
     }
 
     public static int parseStringToInt(String input) {
-        return Integer.parseInt(input);
+        validateIsEmpty(input);
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new ParserException(ParserExceptionMessage.NOT_NUMBER);
+        }
     }
 
+    private static void validateIsEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new ParserException(ParserExceptionMessage.EMPTY_INPUT);
+        }
+    }
 }
