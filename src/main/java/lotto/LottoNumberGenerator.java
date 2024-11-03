8655c0bfd10e8d7d596d357f1ca06d061a926154
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-03 21:10:27 +0900

    feat(LottoNumberGenerator) : 1과 45사이의 6개의 랜덤 숫자를 생성하는 로또 생성 기능 추가

---
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    static final int SIX_DIGITS = 6;
+    private final List<Lotto> lottos;
+
+    public LottoNumberGenerator(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static List<Integer> createRandomLotto() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER,SIX_DIGITS);
+    }
+
+}
--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -7,17 +7,31 @@ import java.util.List;
 public class LottoStore {
 
     private final int purchasePrice;
+    private final List<Lotto> purchasedLotto = new LinkedList<>();
     private final int LOTTO_PRICE = 1000;
 
     public LottoStore(int purchaseAmount) {
-        this.purchasePrice = purchaseAmount;
+        this.purchasePrice = purchaseAmount;// 입력된 구매 금액 유효성 확인
+        this.purchasedLotto.addAll(purchaseLotto());
     }
 
+    public List<Lotto> purchaseLotto() {
+        int lottoCount = getNumberOfLotto();
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            purchasedLotto.add(new Lotto(LottoNumberGenerator.createRandomLotto()));
+        }
+        return lottoList;
+    }
 
     public int getNumberOfLotto() {
         return purchasePrice / LOTTO_PRICE;
     }
 
+    //아래 테스트 필요
+    public List<Lotto> getPurchasedLotto() {
+        return new ArrayList<>(purchasedLotto);
+    }
 
-    
+
 }
