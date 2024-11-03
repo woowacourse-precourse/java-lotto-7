2381caa6d115dbac7a7914d9d8939c721fcf8f7a
HJ <gmlwnchlrh@gmail.com>
2024-11-04 00:37:54 +0900

    feat: InputView 클래스에 구입 금액 입력 예외 처리 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,12 +8,21 @@ import java.util.stream.Stream;
 
 public class InputView {
     public static int requestPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
-        int amount = Integer.parseInt(Console.readLine());
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        while (true) {
+            System.out.println("구입 금액을 입력해 주세요.");
+            String input = Console.readLine();
+            try {
+                int amount = Integer.parseInt(input);
+                if (amount % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+                }
+                return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효하지 않은 입력입니다. 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        return amount;
     }
 
     public static List<Integer> requestWinningNumbers() {
