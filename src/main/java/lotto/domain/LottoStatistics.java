4ed1490e71bc92beac25f0fa7bbc6b80a8dc2dc3
JiEung2 <181757@jnu.ac.kr>
2024-11-03 22:38:19 +0900

    feat(lottoStatistics): 로또 번호와 당첨 번호를 비교하여 일치하는 숫자의 개수를 계산하는 메서드 추가

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,6 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoStatistics {
+    private int calculateMatchCount(Lotto lotto, List<Integer> numbers) {
+        return (int) lotto.getNumbers().stream()
+                .filter(numbers::contains)
+                .count();
+    }
+
     private boolean isBonusNumberMatched(Lotto lotto, int bonusNumber) {
         return lotto.getNumbers().contains(bonusNumber);
     }
