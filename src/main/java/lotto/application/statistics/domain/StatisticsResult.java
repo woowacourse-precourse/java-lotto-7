f352c26c92de97cd1b88be189643d99e9ce36a96
plan11plan <tjtfy7028@gmail.com>
2024-11-04 02:30:06 +0900

    feat(StatisticsResult): 수익률 계산

---
+++ src/main/java/lotto/application/statistics/domain/StatisticsResult.java
@@ -0,0 +1,32 @@
+package lotto.application.statistics.domain;
+
+public class StatisticsResult {
+    private static final double PERCENTAGE = 100.0;
+    private final RankCounter rankCounter;
+
+    private StatisticsResult(RankCounter rankCounter) {
+        this.rankCounter = rankCounter;
+    }
+
+    public static StatisticsResult from(RankCounter rankCounter) {
+        return new StatisticsResult(rankCounter);
+    }
+
+    public double calculateProfitRate(int purchaseAmount) {
+        int totalPrize = rankCounter.calculateTotalPrize();
+        return roundToSecondDecimal(calculateRate(totalPrize, purchaseAmount));
+    }
+
+    public long getCount(Rank rank) {
+        return rankCounter.getCount(rank);
+    }
+
+    private double calculateRate(int totalPrize, int purchaseAmount) {
+        return (totalPrize / (double) purchaseAmount) * PERCENTAGE;
+    }
+
+    private double roundToSecondDecimal(double value) {
+        return Math.round(value * PERCENTAGE) / PERCENTAGE;
+    }
+
+}
