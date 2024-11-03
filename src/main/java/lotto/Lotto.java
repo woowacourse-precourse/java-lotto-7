9fa071b1a6132f528d8686b5429ee3d5f71c7765
yongminKwon1 <ym.kwon@lipis.kr>
2024-11-04 03:04:31 +0900

    fix(Lotto): 중복숫자 시 발생하는 예외 IllegalStateException -> IllegalArgumentException 로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -21,7 +21,7 @@ public class Lotto {
 
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalStateException("[ERROR] 로또 번호에는 중복된 숫자가 있을 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복된 숫자가 있을 수 없습니다.");
         }
     }
 
