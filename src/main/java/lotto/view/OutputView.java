4a3967049edad1a91d9ef03128dce66009799c91
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-03 17:33:17 +0900

    feat(view): 출력 뷰 기능 추가
    - 로또 구매 개수 출력
    - 오류 문자열 출력
    - 상품 통계 결과 출력
    - 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import java.text.NumberFormat;
+import java.util.Map;
+import lotto.model.LottoRank;
+
 public class OutputView {
     private OutputView() {
     }
@@ -7,6 +11,7 @@ public class OutputView {
     public static void printRequestMoney() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
     public static void printRequestLottoWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
@@ -14,4 +19,65 @@ public class OutputView {
     public static void printRequestLottoBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+
+    public static void printPurchasedLottoCount(int numberOfLottos) {
+        System.out.println(numberOfLottos + "개를 구매했습니다.");
+    }
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public static void printRewardStatistic(Map<Integer, Integer> rewardRankResult) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        rewardRankResult.forEach((rank, count) -> {
+            LottoRank lottoRank = LottoRank.findByRank(rank);
+
+            if (lottoRank == LottoRank.DEFAULT) {
+                return;
+            } else if (lottoRank == LottoRank.SECOND) {
+                printSecondRewardStatistic(
+                        lottoRank.getMatchCount(),
+                        numberFormat.format(lottoRank.getPrize()),
+                        count
+                );
+                return;
+            }
+            printDefaultRewardStatistic(
+                    lottoRank.getMatchCount(),
+                    numberFormat.format(lottoRank.getPrize()),
+                    count
+            );
+        });
+    }
+
+    public static void printProfitAtSecondDecimals(long reward, int money) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+
+        float profitRatio = getProfitRatio((float) reward, money);
+        float profitRateAtSecondDecimals = getProfitRateAtSecondDecimals(profitRatio);
+
+        System.out.println("총 수익률은 " + numberFormat.format(profitRateAtSecondDecimals) + "%입니다.");
+    }
+
+    private static void printSecondRewardStatistic(int matchNumberCount, String prize, int count) {
+        System.out.println(matchNumberCount + "개 일치, 보너스 볼 일치 (" +
+                prize + "원) - " +
+                count + "개");
+    }
+
+    private static void printDefaultRewardStatistic(int matchNumberCount, String prize, int count) {
+        System.out.println(matchNumberCount + "개 일치, 보너스 볼 일치 (" +
+                prize + "원) - " +
+                count + "개");
+    }
+
+    private static float getProfitRateAtSecondDecimals(float profitRatio) {
+        return Math.round(profitRatio * 100) / 100.0f;
+    }
+
+    private static float getProfitRatio(float reward, int money) {
+        return reward / money * 100;
+    }
+
 }
