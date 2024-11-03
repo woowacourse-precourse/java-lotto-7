bb5383bc64e04ab812888425b5835d27ac3c6495
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-03 20:04:01 +0900

    chore(LottoRankTest): 디렉토리 구조 변경

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,10 +1,9 @@
-package lotto;
+package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import lotto.domain.LottoRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
