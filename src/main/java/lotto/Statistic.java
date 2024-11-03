41a92404b36cfc79f3733ae249254b336f78f2c6
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-04 01:45:44 +0900

    feat : 당첨 통계 출력 기능 추가

--- src/main/java/lotto/Statistic.java
+++ src/main/java/lotto/Statistic.java
@@ -1,16 +1,63 @@
 package lotto;
 
 import java.util.List;
+import lotto.prize.WinningStatus;
 import lotto.user.User.UserLottoInfo;
 
 public class Statistic {
 
+    private int fifth = 0;
+    private int fourth = 0;
+    private int third = 0;
+    private int second = 0;
+    private int first = 0;
+
     public double getInterestRate(List<UserLottoInfo> userLottoInfos, Integer purchaseCost) {
-        int sum = userLottoInfos.stream()
+        double sum = userLottoInfos.stream()
                 .mapToInt(userLottoInfo -> userLottoInfo.getWinningStatus()
                         .getWinningMoney())
                 .sum();
 
-        return sum / purchaseCost;
+        return (sum / purchaseCost) * 100;
+    }
+
+    public void setWinningStatistics(List<UserLottoInfo> lottoInfos) {
+        for (UserLottoInfo userlottoInfo : lottoInfos) {
+            if (userlottoInfo.getWinningStatus() == WinningStatus.first) {
+                first++;
+            }
+            if (userlottoInfo.getWinningStatus() == WinningStatus.second) {
+                second++;
+            }
+            if (userlottoInfo.getWinningStatus() == WinningStatus.third) {
+                third++;
+            }
+            if (userlottoInfo.getWinningStatus() == WinningStatus.fourth) {
+                fourth++;
+            }
+            if (userlottoInfo.getWinningStatus() == WinningStatus.fifth) {
+                fifth++;
+            }
+        }
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getThird() {
+        return third;
+    }
+
+    public int getFourth() {
+        return fourth;
+    }
+
+    public int getFifth() {
+        return fifth;
     }
 }
--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -1,6 +1,7 @@
 package lotto.io;
 
 import lotto.Lotto;
+import lotto.Statistic;
 
 public class OutputHandler {
 
@@ -12,21 +13,35 @@ public class OutputHandler {
         System.out.println("당첨 번호를 입력해주세요");
     }
 
-    public void showWinningLottoBonusNumberInputComment(){
+    public void showWinningLottoBonusNumberInputComment() {
         System.out.println("보너스 번호를 입력해주세요");
     }
 
     public void showErrorMessage(String message) {
-        System.err.println(message);
+        System.out.println(message);
     }
 
     public void showPurchaseLottoCount(Integer purchaseCost) {
-        System.out.println(purchaseCost / 1000 + "개를 구매했습니다.");
+        System.out.println(purchaseCost + "개를 구매했습니다.");
+    }
+
+    public void showWinningStatistics(Statistic statistic) {
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + statistic.getFifth() + "개");
+        System.out.println("4개 일치 (50,000원) - " + statistic.getFourth() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + statistic.getThird() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + statistic.getSecond() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + statistic.getFirst() + "개");
+
     }
 
     public void showNumber(Lotto lotto) {
         System.out.println(lotto.getNumbers());
     }
 
-
+    public void showInterestRate(double interestRate) {
+        System.out.println("총 수익률은 " + interestRate + "%입니다.");
+    }
 }
