d4ffd96aa99b024cc75e33b19d54fb2e1f38cb05
dongkyunKim <kyun010713@gmail.com>
2024-11-03 21:59:52 +0900

    refactor(model): 중복된 숫자 검증 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,21 +1,44 @@
 package lotto.model;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        validateNumbers(numbers);
+        this.numbers = sortInAscendingOrder(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumbers(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateHasDuplicateNumber(numbers);
+    }
+
+    private List<Integer> sortInAscendingOrder(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateHasDuplicateNumber(List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복된 숫자가 있을 수 없습니다.");
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        return numberSet.size() != numbers.size();
     }
 
     public List<Integer> getNumbers() {
