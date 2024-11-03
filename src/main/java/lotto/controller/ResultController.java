98c602be9a75324c09675e4a92af1a732c1061cf
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-04 00:32:21 +0900

    feat: 로또 결과를 다루는 컨트롤러 개발

---
+++ src/main/java/lotto/controller/ResultController.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.service.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class ResultController {
+    private final InputView inputView;
+    private final LottoMachine lottoMachine;
+    private final OutputView outputView;
+
+    public ResultController(InputView inputView, LottoMachine lottoMachine, OutputView outputView) {
+        this.inputView = inputView;
+        this.lottoMachine = lottoMachine;
+        this.outputView = outputView;
+    }
+
+    public void result() {
+        List<Integer> winningNumbers = inputView.inputWinningNumbers();
+        Integer bonusNumber = inputView.inputBonusNumber();
+        lottoMachine.calculateResult(winningNumbers, bonusNumber);
+    }
+}
--- src/main/java/lotto/data/Database.java
+++ src/main/java/lotto/data/Database.java
@@ -5,4 +5,13 @@ import java.util.List;
 
 public class Database {
     public static List<Lotto> purchaseLottoList = new ArrayList<>();
+    public static Result result = new Result();
+
+    public static List<Lotto> getPurchaseLottoList() {
+        return purchaseLottoList;
+    }
+
+    public static Result getResult() {
+        return result;
+    }
 }
