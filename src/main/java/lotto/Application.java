0ba33b427bca7ef87219575678de6f9fee02fa08
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-11-04 01:03:19 +0900

    feat: 구매 로또 번호 수량 및 번호 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -28,6 +29,15 @@ public class Application {
         for (int i = 0; i < count; i++) {
             List<Integer> purchaseNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             purchaseLottos.add(purchaseNumbers);
+        }
+
+        // 3. 구매 로또 번호 수량 및 번호 출력
+        String checkCount = "%d개를 구매했습니다.".formatted(count);
+        System.out.println(checkCount);
+
+        for (List<Integer> purchaseNumbers : purchaseLottos) {
+            Collections.sort(purchaseNumbers);
+            System.out.println(purchaseNumbers);
         }
     }
 }
