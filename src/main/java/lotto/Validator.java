3cc6c0dbac4252f2becf59779c04161b77bf0e32
sejinezy <psj8880@naver.com>
2024-11-04 01:15:26 +0900

    refactor(Validator): 입력값 유효성 검사를 담당하는 Validator 클래스 분리

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,32 @@
+package lotto;
+
+public class Validator {
+
+    public static void notEmpty(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어 있습니다.");
+        }
+    }
+
+    private static boolean isEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static void isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
+    }
+
+    public static void isNumber(String[] strings) {
+        try {
+            for (String string : strings) {
+                isNumber(string);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+    }
+}
