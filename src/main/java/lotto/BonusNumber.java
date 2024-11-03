e4ca2b61755af9d19a1bfc9b1dc003c21188c6d1
Parkjyun <jaeyun0507@naver.com>
2024-11-03 18:50:45 +0900

    feat(BonusNumber): 보너스 번호를 갖는다

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class BonusNumber {
+    private int number;
+
+    public BonusNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @Test
+    @DisplayName("보너스 번호를 가지고 BonusNumber 객체를 생성할 수 있다.")
+    void should_CreateBonusNumber_When_GivenNumber() {
+        // given
+        int number = 1;
+        // when
+        BonusNumber bonusNumber = new BonusNumber(number);
+        // then
+        Assertions.assertThat(bonusNumber).isNotNull();
+    }
+
+}
