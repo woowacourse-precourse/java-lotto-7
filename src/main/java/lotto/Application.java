f698229301238a37e837f952cd3846948f33a328
Lee Jeong Woo <sysliar@gmail.com>
2024-11-04 05:16:12 +0900

    feat: 구입금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            int money = InputView.inputMoney();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
