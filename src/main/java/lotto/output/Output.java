de9836ca0a38e9a8f23c47f0ca39c7a45055afb9
jintakkim <mine407603@gmail.com>
2024-11-03 16:11:55 +0900

    feat(Output): 출력 기능 구현
    기본적인 출력은 출력 format을 매개변수로 받아 출력한다.
    복잡한 결과 출력은 메서드를 별로로 분리해 구현한다.

---
+++ src/main/java/lotto/output/Output.java
@@ -0,0 +1,49 @@
+package lotto.output;
+
+import lotto.LottoPrize;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Output {
+
+    public void write(String string) {
+        System.out.println(string);
+    }
+
+    public void write(String format, Double number) {
+        String output = String.format(format, number);
+        write(output);
+    }
+
+    public void write(String format, Object... objects) {
+        String output = String.format(format, objects);
+        write(output);
+    }
+
+    public void write(String format, Integer number) {
+        String output = String.format(format, number);
+        write(output);
+    }
+
+    public void writeLottoPrize(Map<LottoPrize, Integer> map, Double roi) {
+        write("당첨 통계");
+        write("---");
+        map.forEach(this::writeDetailResult);
+        write("총 수익률 %.1f%%입니다.", roi);
+    }
+
+    private void writeDetailResult(LottoPrize lottoPrize, Integer result) {
+        if(lottoPrize.equals(LottoPrize.FIVE_MATCH_BONUS)) {
+            write("%d개 일치, 보너스 볼 일치 (%,d원) - %d개",
+                    lottoPrize.getMatchCount(),
+                    lottoPrize.getPrize(),
+                    result);
+            return;
+        }
+        write("%d개 일치 (%,d원) - %d개",
+                lottoPrize.getMatchCount(),
+                lottoPrize.getPrize(),
+                result);
+    }
+}
