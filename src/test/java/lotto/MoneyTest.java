d54db503692afd57e0413d4bbb4a3815c1e40f49
Leegeonwoo7 <workgeon07@naver.com>
2024-11-03 22:51:01 +0900

    test(MoneyTest): Money Test 작성
    - 로또 개당가격이 1,000이고 12,500으로 구매할 경우 500원이 남아 예외

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,8 +1,23 @@
+package lotto;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import static lotto.constant.ErrorMessage.PURCHASE_PRICE_ERROR;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+
 class MoneyTest {
-  
+
+    @Test
+    @DisplayName("로또 금액과 나머지 연산한 결과가 0이 아니면 예외가 발생한다.")
+    void noneFitPurchase() {
+        //given
+        long amount = 12500;
+
+        //when //then
+        assertThatThrownBy(() -> new Money(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(PURCHASE_PRICE_ERROR.getMessage());
+    }
 }
