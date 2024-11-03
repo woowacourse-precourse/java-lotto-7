4a8be7c5390444f79b5bb45815f609b42a12baa0
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-03 20:09:38 +0900

    refactor: 반복문 시작숫자 상수로 추출

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -7,13 +7,14 @@ import lotto.utils.LottoNumberGenerator;
 public class LottoMachine {
 
     private static final int PURCHASE_AMOUNT_UNITS = 1000;
+    private static final int LOOP_START_INDEX = 0;
 
     private final List<Lotto> lottos = new ArrayList<>();
 
     public List<Lotto> purchaseLottos(int purchaseAmount) {
         int count = purchaseAmount / PURCHASE_AMOUNT_UNITS;
 
-        for (int i = 0; i < count; i++) {
+        for (int i = LOOP_START_INDEX; i < count; i++) {
             Lotto lotto = new Lotto(LottoNumberGenerator.generate());
             lottos.add(lotto);
         }
