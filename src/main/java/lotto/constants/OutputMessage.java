4ce74e4ba9b356ee0c103fd1aa274abcbba8edf4
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 04:40:54 +0900

    refactor: 사용하지 않는 상수 삭제

--- src/main/java/lotto/constants/OutputMessage.java
+++ src/main/java/lotto/constants/OutputMessage.java
@@ -4,8 +4,6 @@ public enum OutputMessage {
     GET_TOTAL_LOTTO_COUNT_MESSAGE("개를 구매했습니다."),
     GET_WINNING_STATISTICS_MESSAGE("\n당첨 통계"),
     LINE_SEPARATOR("---"),
-    PRIZE_SEPARATOR(" - "),
-    UNIT_COUNT("개"),
     RETURN_ON_INVESTMENT_IS("총 수익률은 "),
     PERCENT_IS("%입니다."),
     ;
