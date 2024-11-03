d1a819fbfc175c7036215c18816c942d534038f6
Gahyeon <best11gh@naver.com>
2024-11-03 16:15:11 +0900

    feat(OutputView): OutputView 생성

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,19 @@
+package lotto.view;
+
+public class OutputView {
+    public static void printLottoCount(int lottoCount) {
+        System.out.printf("%d개를 구매했습니다.%n", lottoCount);
+    }
+
+    public static void printLottoNumbers() {
+
+    }
+
+    public static void printWinningDetails() {
+
+    }
+
+    public static void printYield(double yield) {
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", yield);
+    }
+}
