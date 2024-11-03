3f5eab23c0bb18a8fb2e4bb38b90dc02f1199179
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-03 16:01:39 +0900

    feat: 출력에 필요한 상수 선언

--- src/main/java/lotto/constant/ExceptionMessageConstants.java
+++ src/main/java/lotto/constant/ExceptionMessageConstants.java
@@ -5,7 +5,9 @@ public enum ExceptionMessageConstants {
     DUPLICATE_LOTTO_NUMBER_ERROR("로또 번호는 중복될 수 없습니다."),
     LOTTO_NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다."),
     LOTTO_NUMBER_OUT_OF_RANGE_ERROR("로또 번호는 1~45사이의 숫자 입니다."),
-    EXACT_CHANGE_NOT_POSSIBLE_ERROR("금액은 1,000원으로 나누어 떨어져아 합니다.")
+    EXACT_CHANGE_NOT_POSSIBLE_ERROR("금액은 1,000원으로 나누어 떨어져아 합니다."),
+    PRICE_NUMBER_FORMAT_ERROR("금액은 숫자로 입력해주세요."),
+    LOTTO_NUMBER_FORMAT_ERROR("당첨번호는 숫자로 입력해주세요")
     ;
 
     private final String message;
