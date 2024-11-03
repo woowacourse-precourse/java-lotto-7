692970dca1af37cfef6bb9bac57b5de7e58429a0
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-03 21:42:03 +0900

    feat: 당첨은 1등부터 5등까지 있다. 당첨 기준과 금액은 아래와 같다.

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,81 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoResult {
+    private static final int THREE_MATCHES_PRIZE = 5000;
+    private static final int FOUR_MATCHES_PRIZE = 50000;
+    private static final int FIVE_MATCHES_PRIZE = 1500000;
+    private static final int FIVE_MATCHES_WITH_BONUS_PRIZE = 30000000;
+    private static final int SIX_MATCHES_PRIZE = 2000000000;
+
+    private int threeMatchesCount;
+    private int fourMatchesCount;
+    private int fiveMatchesCount;
+    private int fiveMatchesWithBonusCount;
+    private int sixMatchesCount;
+
+    public LottoResult() {
+        this.threeMatchesCount = 0;
+        this.fourMatchesCount = 0;
+        this.fiveMatchesCount = 0;
+        this.fiveMatchesWithBonusCount = 0;
+        this.sixMatchesCount = 0;
+    }
+
+    public void countMatchingNumbers(Lotto lotto, Lotto winningLotto) {
+        int count = 0;
+        boolean isMachtedBonusNumber = BonusNumber.isMatchedBonusNumber(lotto);
+        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
+        for (Integer lottoNumber : lotto.getNumbers()) {
+            if (winningLottoNumbers.contains(lottoNumber)) {
+                count++;
+            }
+        }
+        updateLottoResult(count, isMachtedBonusNumber);
+    }
+
+    public StringBuilder getResultMessage() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("3개 일치 (5,000원) - " + threeMatchesCount + "\n");
+        stringBuilder.append("4개 일치 (50,000원) - " + fourMatchesCount + "\n");
+        stringBuilder.append("5개 일치 (1,500,000원) - " + fiveMatchesCount + "\n");
+        stringBuilder.append("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveMatchesWithBonusCount + "\n");
+        stringBuilder.append("6개 일치 (2,000,000,000원) - " + sixMatchesCount + "\n");
+        return stringBuilder;
+    }
+
+    public int getTotalPrizeAmount() {
+        int totalPrizeAmount = 0;
+        totalPrizeAmount += threeMatchesCount * THREE_MATCHES_PRIZE;
+        totalPrizeAmount += fourMatchesCount * FOUR_MATCHES_PRIZE;
+        totalPrizeAmount += fiveMatchesCount * FIVE_MATCHES_PRIZE;
+        totalPrizeAmount += fiveMatchesWithBonusCount * FIVE_MATCHES_WITH_BONUS_PRIZE;
+        totalPrizeAmount += sixMatchesCount * SIX_MATCHES_PRIZE;
+
+        return totalPrizeAmount;
+    }
+
+    private void updateLottoResult(int count, boolean isMachtedBonusNumber) {
+        if (count == 3) {
+            threeMatchesCount++;
+            return;
+        }
+        if (count == 4) {
+            fourMatchesCount++;
+            return;
+        }
+        if (count == 5 && isMachtedBonusNumber) {
+            fiveMatchesWithBonusCount++;
+            return;
+        }
+        if (count == 5) {
+            fiveMatchesCount++;
+            return;
+        }
+        if (count == 6) {
+            sixMatchesCount++;
+        }
+    }
+
+}
