cc0f8af6878179666a8471faaf3d3f29927731d3
JiEung2 <181757@jnu.ac.kr>
2024-11-04 01:30:29 +0900

    feat(outputView): 총 수익률을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,8 @@ import java.util.Map;
 public class OutputView {
     private static final String LOTTO_PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    private static final String PROFIT_MESSAGE_PREFIX= "총 수익률은 ";
+    private static final String PROFIT_MESSAGE_SUFFIX = "입니다.";
     private static final String SEPARATOR_LINE = "---";
     private static final String COUNT_UNIT = "개";
     private static final String HYPHEN = " - ";
@@ -22,6 +24,14 @@ public class OutputView {
         lottos.forEach(OutputView::printLotto);
     }
 
+    public static void printWinningStatistics(Map<LottoRanking, Integer> rankingCount, int profitRate) {
+        println(WINNING_STATISTICS_MESSAGE);
+        println(SEPARATOR_LINE);
+
+        printWinningDetails(rankingCount);
+        printProfitRate(profitRate);
+    }
+
     private static void printLotto(Lotto lotto) {
         println(lotto.getNumbers().toString());
     }
@@ -30,5 +40,9 @@ public class OutputView {
         for (LottoRanking lottoRanking : LottoRanking.values()) {
             println(lottoRanking.getMessage() + HYPHEN + rankingCount.getOrDefault(lottoRanking, 0) + COUNT_UNIT);
         }
+    }
+
+    private static void printProfitRate(int profitRate) {
+        println(PROFIT_MESSAGE_PREFIX + profitRate + PROFIT_MESSAGE_SUFFIX);
     }
 }
