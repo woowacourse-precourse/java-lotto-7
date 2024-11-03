8aeba6f634dd924556569a55501c5ac0df47327e
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-03 15:42:20 +0900

    test(LottoControllerTest) : gainReturn 테스트

--- src/test/java/lotto/LottoControllerTest.java
+++ src/test/java/lotto/LottoControllerTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoControllerTest {
+    LottoController lottoController = new LottoController();
+
     @Test
     @DisplayName("gainMyRevenue 정상 작동 테스트")
     void gainMyRevenueTest() {
         int actualRevenue = 0;
-        LottoController lottoController = new LottoController();
         int expectedRevenue = 3 * 5000 + 1 * 50000 + 2 * 1500000;
         WinningDetails winningDetails = new WinningDetails();
         winningDetails.setFifth(2);
@@ -20,5 +21,16 @@ public class LottoControllerTest {
         winningDetails.setThird(3);
         actualRevenue = lottoController.gainMyRevenue(winningDetails);
         assertThat(actualRevenue).isEqualTo(expectedRevenue);
+    }
+
+    @Test
+    @DisplayName("gainReturn 정상 작동 테스트")
+    void gainReturnTest() {
+        double actualReturn = 0;
+        int purchasePrice = 8000;
+        int revenue = 5000;
+        double expectedReturn = 62.5;
+        actualReturn = lottoController.gainReturn(purchasePrice, revenue);
+        assertThat(actualReturn).isEqualTo(expectedReturn);
     }
 }
