86ba20277b62e01246049bb4bb884648f2af30c6
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:36:56 +0900

    :sparkles: feat(ConstantHandler): 로또 관련 상수 제공

--- src/main/java/lotto/handler/ConstantHandler.java
+++ src/main/java/lotto/handler/ConstantHandler.java
@@ -1,4 +1,18 @@
 package lotto.handler;
 
 public class ConstantHandler {
+    public static final String WINNING_NUMBERS_DELIMITER = ",";
+
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
+    public static final int TOTAL_PERCENTAGE = 100;
+
+    public static final int RANK_DEFAULT_VALUE = 0;
+    public static final int RANK_ADD_VALUE = 1;
+
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MIN_MONEY_AMOUNT = 0;
+    public static final int MONEY_REMAINDER = 0;
 }
