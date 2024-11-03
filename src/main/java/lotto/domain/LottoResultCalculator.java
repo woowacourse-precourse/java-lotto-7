9bef9c8cdfc79f69de512e3e6734968a1472386c
이희수 <hslee9234@naver.com>
2024-11-03 17:27:13 +0900

    refactor: LottoResultCalculator
    계산 정확도를 위해 BigDecimal 자료형 사용

--- src/main/java/lotto/domain/LottoResultCalculator.java
+++ src/main/java/lotto/domain/LottoResultCalculator.java
@@ -1,18 +1,29 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import lotto.dto.LottoWinningNumbers;
 
 public class LottoResultCalculator {
 
-    public double calculateProfitRate(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers, int purchaseAmount) {
-        int totalPrize = lottos.stream()
-                .mapToInt(lotto -> lotto.calculateRank(
+    public BigDecimal calculateProfitRate(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers,
+                                          int purchaseAmount) {
+        long totalPrize = calculateTotalPrize(lottos, lottoWinningNumbers);
+
+        BigDecimal profitRate = new BigDecimal(totalPrize)
+                .divide(new BigDecimal(purchaseAmount), 10, RoundingMode.HALF_UP)
+                .multiply(new BigDecimal(100));
+
+        return profitRate.setScale(1, RoundingMode.HALF_UP);
+    }
+
+    private long calculateTotalPrize(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers) {
+        return lottos.stream()
+                .mapToLong(lotto -> lotto.calculateRank(
                         lottoWinningNumbers.getWinningNumbers(),
                         lottoWinningNumbers.getBonusNumber()
                 ).getPrize())
                 .sum();
-
-        return (double) totalPrize / purchaseAmount * 100;
     }
 }
