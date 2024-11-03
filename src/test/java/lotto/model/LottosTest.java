be2b08c4be2380cc55495904f46202b55666ccba
CinaDor <cinador@github.com>
2024-11-03 23:36:59 +0900

    refactor: Refactor LottosTest class
    LottosTest 클래스의 단일 책임을 위해 당첨 금액을 테스트에서 결과 객체 테스트로 변경

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,14 +1,11 @@
 package lotto.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class LottosTest {
     static Lottos sampleLottos;
@@ -20,23 +17,22 @@ class LottosTest {
         sampleLottos.insertLotto(new Lotto(List.of(1, 2, 3, 11, 12, 13)));
         sampleLottos.insertLotto(new Lotto(List.of(1, 2, 3, 4, 12, 13)));
     }
-
-    @ParameterizedTest(name = "{index}: {2}")
-    @MethodSource("lottosTest")
+    
+    @Test
     @DisplayName("선택한 수에 따른 결과 테스트")
-    void prize_findByHitAndBonusTest(Lotto targetLotto, int targetBonus, String message, int totalMoney) {
+    void Lottos_getResultTest() {
+        Lotto sampleLotto = new Lotto(List.of(1, 2, 3, 12, 13, 45));
+        int bonusNumber = 4;
 
-        Result sampleResult = sampleLottos.getResult(targetLotto, targetBonus);
+        Result sampleResult = sampleLottos.getResult(sampleLotto, bonusNumber);
 
-        assertEquals(sampleResult.totalMoney(), totalMoney);
-    }
-
-    static Stream<Arguments> lottosTest() {
-        return Stream.of(
-                Arguments.of(new Lotto(List.of(1, 2, 3, 12, 13, 45)), 4, "2등+3등+5등", 31_505_000),
-                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7, "1등+4등+5등", 2_000_055_000),
-                Arguments.of(new Lotto(List.of(1, 2, 3, 20, 21, 22)), 4, "5등 3개", 15_000),
-                Arguments.of(new Lotto(List.of(30, 31, 32, 33, 34, 35)), 1, "x", 0)
-        );
+        assertThat(sampleResult.toString())
+                .contains(
+                        "3개 일치 (5,000원) - 1개",
+                        "4개 일치 (50,000원) - 0개",
+                        "5개 일치 (1,500,000원) - 1개",
+                        "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                        "6개 일치 (2,000,000,000원) - 0개"
+                );
     }
 }
