05f5eea044ffd59f763058ad2e524f89ced0271b
jinukee <jinukee333@gmail.com>
2024-11-03 14:14:29 +0900

    refactor : 누락된 parameter final 선언 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -63,7 +63,7 @@ public class LottoController {
         }
     }
 
-    private void validateBonusNumber(final int bonusNumber, List<Integer> winningNumbers) {
+    private void validateBonusNumber(final int bonusNumber, final List<Integer> winningNumbers) {
         if (bonusNumber < 1 || bonusNumber > 45 || winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45사이의 당첨번호와 중복되지 않는 번호여야 합니다.");
         }
