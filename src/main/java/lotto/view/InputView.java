3123c9d90dbbbda246a59366229f3d745e929792
ruudska6 <feels19@naver.com>
2024-11-03 16:58:00 +0900

    <feat> InputView 로또 당첨번호를 사용자로부터 입력 받는 lottoNumberInput() 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static final String ASK_MONEY_INPUT = "구입금액을 입력해 주세요.";
+    public static final String ASK_LOTTO_NUM_INPUT = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_MONEY_NOT_NUMBER = "[ERROR] 입력하신 금액이 양의 정수가 아닙니다.";
+
 
 
     public int lottoMoneyInput() {
@@ -13,6 +15,13 @@ public class InputView {
         return Integer.parseInt(rawMoney);
     }
 
+    public String[] lottoNumberInput() {
+        System.out.printf(ASK_LOTTO_NUM_INPUT);
+        String rawNumbers = Console.readLine();
+        return rawNumbers.split(",");
+    }
+
+
     private void validateIsNumber(String rawValue) {
         try {
             Integer.parseInt(rawValue);
@@ -20,8 +29,5 @@ public class InputView {
             throw new IllegalArgumentException(INPUT_MONEY_NOT_NUMBER);
         }
     }
-
-
-
 
 }
