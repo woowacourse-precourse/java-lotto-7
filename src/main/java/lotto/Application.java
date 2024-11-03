202764aa401b7e0db19234a029dbd7acb91e6492
youngkwanglim <richard08@naver.com>
2024-11-03 19:13:05 +0900

    feat: 로또 구입 금액 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = getPurchaseAmount();
+    }
+
+    private static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+        System.out.println();
+        return purchaseAmount;
     }
 }
