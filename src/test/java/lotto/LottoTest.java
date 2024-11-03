c640a2cb38051aa0890e39b89537805bbd7fe514
jhpark1227 <junhyeok0246@naver.com>
2024-11-03 20:00:38 +0900

    test: Lotto 단위 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,15 @@
 package lotto;
 
+import lotto.error.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static lotto.LottoConstant.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +27,32 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 범위안의_로또_번호_6개가_중복되지_않으면_예외가_발생하지_않는다(){
+        //given
+        //when
+        //then
+        assertThatCode(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 로또_번호가_6개보다_적으면_예외가_발생한다() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NUMBER_COUNT_ERROR.format(NUMBER_COUNT));
+    }
+
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.NUMBER_RANGE_ERROR.format(MIN_VALUE, MAX_VALUE));
+    }
 }
