8a20dd076d76dafb46c58527fa4aa0852339b62f
hjg1473 <hjg1473@naver.com>
2024-11-03 16:55:19 +0900

    feat : Add winning number
    The winning number was added and the exception was processed.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,11 +11,20 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         while (true) {
             String purchase = Console.readLine();
-            if (validatePurchase(Integer.parseInt(purchase))) {
+            Integer purchaseInt = Integer.parseInt(purchase);
+            if (validatePurchase(purchaseInt)) {
                 break;
             }
         }
 
+        System.out.println("당첨번호를 입력해 주세요.");
+        while (true) {
+            String winningNum = Console.readLine();
+            String winningNumber[] = winningNum.split(",");
+            if (validateWinningNumber(winningNumber)) {
+                break;
+            }
+        }
 
 
     }
@@ -30,5 +39,29 @@ public class Application {
             System.out.println(e.getMessage());
         }
         return false;
+    }
+
+    private static boolean validateWinningNumber(String[] numbers) {
+        try {
+            if (numbers.length != 6) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+            }
+            if (!checkNumbersRange(numbers)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이 숫자여야 합니다.");
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private static boolean checkNumbersRange(String[] numbers) {
+        for (String number : numbers) {
+            if(!(Integer.parseInt(number) >= 1 && Integer.parseInt(number) <= 45)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
