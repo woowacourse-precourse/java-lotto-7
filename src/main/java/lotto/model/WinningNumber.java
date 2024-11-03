1628a5b31dd68785f217886d170943515414e58c
Chaeyoung714 <202102760@hufs.ac.kr>
2024-11-03 10:45:50 +0900

    refactor(Lotto, WinningNumber) : 조건문에 부정연산자 사용하지 않도록 메서드 분리
    youtoo

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -23,13 +23,17 @@ public class WinningNumber {
     private void validateEachNumber(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>();
         for (int number : numbers) {
-            if (!uniqueNumbers.add(number)) {
+            if (addedDuplicated(number, uniqueNumbers)) {
                 throw new IllegalArgumentException("[ERROR] 중복되지 않은 숫자를 입력해주세요.");
             }
             if (number < 1 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 1 이상 45 이하의 정수를 입력해주세요.");
             }
         }
+    }
+
+    private boolean addedDuplicated(int number, Set<Integer> uniqueNumbers) {
+        return !uniqueNumbers.add(number);
     }
 
     public boolean contains(int number) {
