47fbd749a835a4f107beebaeeab16ac493e73a19
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-03 17:57:26 +0900

    feat(Application): 의존성 주입을 위한 컴포넌트 생성
    - 의존성 주입을 위해서 개별 컴포넌트 생성
    - 사용자의 입력과 출력을 위한 InputView와 OutputView 생성
    - 도메인 객체 생성을 위한 LottoDomainFactory 생성
    - 비즈니스 로직 처리를 위한 LottoService 생성
    - 로또 프로그램을 주도하는 LottoGameController에 필요한 의존성 주입 및 생성
    - gameStart() 메서드 호출을 통해 로또 프로그램 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.Controller.LottoGameController;
+import lotto.Factory.LottoDomainFactory;
+import lotto.Service.LottoService;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        OutputView outputView = new OutputView();
+
+        LottoDomainFactory lottoDomainFactory = new LottoDomainFactory();
+
+        LottoService lottoService = new LottoService();
+
+        LottoGameController lottoGameController = new LottoGameController(lottoService, inputView, outputView);
+
+        lottoGameController.gameStart();
     }
 }
