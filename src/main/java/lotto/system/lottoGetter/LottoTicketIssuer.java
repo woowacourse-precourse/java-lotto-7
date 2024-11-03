b4cdcf36c853867397694c43f7088a431b9b3c16
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-03 18:14:55 +0900

    feat: LottoTicketIssuer에 getPurchaseAmount 메서드 추가

--- src/main/java/lotto/system/lottoGetter/LottoTicketIssuer.java
+++ src/main/java/lotto/system/lottoGetter/LottoTicketIssuer.java
@@ -6,10 +6,14 @@ import lotto.system.unit.LottoTicket;
 public class LottoTicketIssuer { // 로또 구매 금액을 입력 받아 로또 티켓을 발급하는 객체
 
     private static final int TICKET_PRICE = 1000;
+
+    private final int purchaseAmount;
     private final int quantity;
+
 
     public LottoTicketIssuer(int totalPayment) {
         LottoPaymentValidator.validate(totalPayment);
+        this.purchaseAmount = totalPayment;
         this.quantity = totalPayment / TICKET_PRICE;
     }
 
@@ -19,5 +23,9 @@ public class LottoTicketIssuer { // 로또 구매 금액을 입력 받아 로또
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
     }
 }
