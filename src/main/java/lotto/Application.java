4ebba9ef4acbc26f97d97ae3d4cb06ccc1789760
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-04 00:52:39 +0900

    refactor: LottoController내용 Application으로 이동
    - Controller로 나눴을때의 이점이 없어보여 Application으로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,28 @@
 package lotto;
 
-import lotto.controller.LottoController;
+import java.util.HashMap;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoCommittee;
+import lotto.model.LottoMachine;
+import lotto.model.Ranking;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        LottoMachine lottoMachine = new LottoMachine();
+        int purchaseAmount = InputView.inputPurchaseAmount();
+        List<Lotto> lottos = lottoMachine.purchaseLottos(purchaseAmount);
+        OutputView.printPurchasedLottoCountAndNumber(lottos);
+
+        List<Integer> winningNumber = InputView.inputWinningNumber();
+        Integer bonusNumber = InputView.inputBonusNumber(winningNumber);
+        LottoCommittee committee = new LottoCommittee(winningNumber, bonusNumber);
+
+        HashMap<Ranking, Integer> rankingCountMap = committee.calculateRanking(lottos);
+        OutputView.printWinningHistory(rankingCountMap);
+        OutputView.printRateOfReturn(purchaseAmount, rankingCountMap);
     }
 }
