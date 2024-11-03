e137c4ff9b4d33ef5ef179db60a062e714bc66c8
차왕준 <ckdhdkwns@gmail.com>
2024-11-04 03:18:19 +0900

    test(domain): 로또 번호 중복 검증 테스트

--- src/test/java/lotto/util/LottoNumberValidatorTest.java
+++ src/test/java/lotto/util/LottoNumberValidatorTest.java
@@ -33,4 +33,27 @@ public class LottoNumberValidatorTest {
                     .hasMessageContaining("6개");
         }
     }
+
+    @Nested
+    @DisplayName("로또 번호 중복 검증")
+    class ValidateDuplicates {
+        @Test
+        @DisplayName("중복되지 않은 번호들은 정상 처리한다.")
+        void 중복되지_않은_번호들은_정상_처리한다() {
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+            assertThatNoException()
+                    .isThrownBy(() -> LottoNumberValidator.validateDuplicates(numbers));
+        }
+
+        @Test
+        @DisplayName("중복된 번호가 있으면 예외가 발생한다.")
+        void 중복된_번호가_있으면_예외가_발생한다() {
+            List<Integer> numbers = Arrays.asList(1, 2, 3, 3, 4, 5);
+
+            assertThatThrownBy(() -> LottoNumberValidator.validateDuplicates(numbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("중복");
+        }
+    }
 }
