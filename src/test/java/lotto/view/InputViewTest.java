003e9cac637874e33e96357a5bddb3abfa6bb900
navangs <43688529+insertend23@users.noreply.github.com>
2024-11-03 19:36:54 +0900

    Test: write input functions test code

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    private static final String BLANK = " ";
+    private final InputView inputView = new InputView();
+
+    @Test
+    void getPurchaseAmount() {
+        run(BLANK);
+
+        assertThatThrownBy(inputView::getPurchaseAmount)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void getWinningNumbers() {
+        run(BLANK);
+
+        assertThatThrownBy(inputView::getWinningNumbers)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void getBonusNumber() {
+        run(BLANK);
+
+        assertThatThrownBy(inputView::getBonusNumber)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Override
+    protected void runMain() {}
+}
