b4721bd3dad9bb7ee39cbaa16a74cf42ef7a1176
rootTiket <l20078829@gmail.com>
2024-11-03 18:28:46 +0900

    refactor(ErrorMessage): 에러메시지 규격을 통일

--- src/main/java/lotto/domain/constant/ErrorMessage.java
+++ src/main/java/lotto/domain/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package lotto.domain.constant;
 
 public enum ErrorMessage {
-    PRICE_UNMATCHED("돈은 1000원 단위로만 입력할 수 있습니다");
+    PRICE_UNMATCHED("[error] 돈은 1000원 단위로만 입력할 수 있습니다");
 
     private final String message;
 
