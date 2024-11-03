1d329d83d17e4cd0334629b25a5702fb3412df39
sanikani <sanikani@naver.com>
2024-11-03 23:54:28 +0900

    feat: 로또 당첨 결과 계산
    - WinningStatus Enum을 사용하여 당첨 결과 정의
    - WinningLotto와 구매한 로또들을 비교하여 당첨 결과 계산
    - LottoResult 에서 결과를 EnumMap으로 담아서 return

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LottoResult {
+
+    private final Map<WinningStatus, Integer> result = new EnumMap<>(WinningStatus.class);
+
+    public LottoResult() {
+        for (WinningStatus status : WinningStatus.values()) {
+            result.put(status, 0);
+        }
+    }
+
+    public void add(WinningStatus winningStatus) {
+        result.put(winningStatus, result.get(winningStatus) + 1);
+    }
+
+    public Map<WinningStatus, Integer> getResult() {
+        return result;
+    }
+}
---
+++ src/main/java/lotto/model/WinningStatus.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import java.util.Arrays;
+
+public enum WinningStatus {
+
+    THREE_MATCHES(3, 5_000, "3개 일치 (5,000원)"),
+    FOUR_MATCHES(4, 50_000, "4개 일치 (50,000원)"),
+    FIVE_MATCHES(5, 1_500_000, "5개 일치 (1,500,000원)"),
+    FIVE_MATCHES_WITH_BONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIX_MATCHES(6, 2_000_000_000, "6개 일치 (2,000,000,000원)"),
+    NO_WIN(0, 0, "");
+
+    private final int matchCount;
+    private final int prize;
+    private final String description;
+
+    WinningStatus(int matchCount, int prize, String description) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.description = description;
+    }
+
+    public static WinningStatus getWinningStatus(int matchCount, boolean isBonusMatched) {
+        if(FIVE_MATCHES.getMatchCount()==matchCount && isBonusMatched) {
+            return FIVE_MATCHES_WITH_BONUS;
+        }
+        return Arrays.stream(WinningStatus.values())
+                .filter(winningStatus -> winningStatus.getMatchCount() == matchCount)
+                .findFirst()
+                .orElse(NO_WIN);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}
+
