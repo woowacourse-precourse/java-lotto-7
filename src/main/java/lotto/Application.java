f9b676c76db7da80e5600595c0f8c1f38ace6e4f
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-03 15:43:57 +0900

    feat(Application): 생성한 클래스의 객체를 생성한다
    돈을 입력받고, Customer객체를 생성한다. 그리고 계산된 복권의 갯수를 토대로 복권을 생성하고 출력한다.
    출력문을 변수로 선언하여 출력하고 스페이스를 해주는 함수를 생성한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
+    static String introduction = "구입 금액을 입력해 주세요.";
+
+    public static void spaceString() {
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(introduction);
+        String money = Console.readLine();
+        Customer customer = new Customer(money);
+        spaceString();
+        System.out.println(customer.getLottoCount() + "개를 구매했습니다");
+        LottoNumberProducer producer = new LottoNumberProducer(customer.getLottoCount());
+        producer.createRandomNumbers();
+        producer.displayLottoNumbers();
     }
 }
