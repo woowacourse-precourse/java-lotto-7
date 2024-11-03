9af1ca21d1abd40dceea586c446abd83d4e7a7a9
leesh7807 <leesh7807@gmail.com>
2024-11-03 19:50:43 +0900

    refactor(LottoMoney): money DTO
    Initially, LottoIssuer manage validation on money.
    But, since money is reused for statistics calculations beyond just issuing tickets,
    it is necessary to manage money as an independent class.
    So, I separate money variable into an class and refactor all affected classes.

--- src/main/java/lotto/LottoIssuer.java
+++ src/main/java/lotto/LottoIssuer.java
@@ -6,17 +6,10 @@ import java.util.stream.IntStream;
 
 public class LottoIssuer {
     public static List<Lotto> issueLottos(int money) {
-        checkMoney(money);
         int count = money / 1000;
         return IntStream.range(0, count)
                 .mapToObj(it -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
                 .map(Lotto::new)
                 .toList();
-    }
-
-    private static void checkMoney(int money) {
-        if (money <= 0 || money % 1000 != 0) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY.format());
-        }
     }
 }
--- src/test/java/lotto/LottoMoneyTest.java
+++ src/test/java/lotto/LottoMoneyTest.java
@@ -4,24 +4,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-public class LottoIssuerTest {
+public class LottoMoneyTest {
     @Test
     void 구입_금액이_음수인_예외() {
-        assertThatThrownBy(() -> new LottoIssuer().issueLottos(-1000))
+        assertThatThrownBy(() -> new LottoMoney(-1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
 
     @Test
     void 구입_금액이_0인_예외() {
-        assertThatThrownBy(() -> new LottoIssuer().issueLottos(0))
+        assertThatThrownBy(() -> new LottoMoney(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
 
     @Test
     void 구입_금액이_1000원_단위_양수가_아닌_예외() {
-        assertThatThrownBy(() -> new LottoIssuer().issueLottos(3500))
+        assertThatThrownBy(() -> new LottoMoney(3500))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY.format());
     }
