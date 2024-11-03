18103730c6f23035d9e4ac1577711efb039437e8
jijongkwon <help3451@naver.com>
2024-11-03 17:35:06 +0900

    feat(util): 1000원 단위인지 확인하는 메서드 구현

--- src/main/java/lotto/util/LottoValidator.java
+++ src/main/java/lotto/util/LottoValidator.java
@@ -4,6 +4,18 @@ import lotto.enums.lotto.LottoMessage;
 
 public class LottoValidator {
 
+    private static final int PRICE_UNIT = 1000;
+
+    public static void validatePriceUnit(int price) {
+        if (price % PRICE_UNIT != 0) {
+            throw new IllegalArgumentException(LottoMessage.EXCEPTION_PRICE_UNIT.getMessage());
+        }
+
+        if (price <= 0) {
+            throw new IllegalArgumentException(LottoMessage.EXCEPTION_PRICE_UNIT.getMessage());
+        }
+    }
+
     public static int validNumber(String number) {
         try {
             return Integer.parseInt(number);
