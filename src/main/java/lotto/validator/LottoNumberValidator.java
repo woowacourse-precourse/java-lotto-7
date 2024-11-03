9d812543cac5907724eb0c290c5e98dc356764b2
scc9811 <wjsrudtjr98@naver.com>
2024-11-04 03:50:48 +0900

    style : 불필요한 주석 삭제

--- src/main/java/lotto/validator/LottoNumberValidator.java
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -9,21 +9,6 @@ public class LottoNumberValidator {
     private LottoNumberValidator() {
     }
 
-//    public static void validateNumberCount(List<String> tokens) {
-//        if (tokens.size() != 6) {
-//            throw new IllegalArgumentException("[ERROR] 당첨 번호가 6개가 아닙니다.");
-//        }
-//    }
-
-//    public static void validateDuplicatedNumber(List<Integer> numbers) {
-//        boolean hasDuplicates = numbers.stream()
-//                .anyMatch(number -> Collections.frequency(numbers, number) > 1);
-//
-//        if (hasDuplicates) {
-//            throw new IllegalArgumentException("[ERROR] 중복되는 번호가 존재합니다.");
-//        }
-//    }
-
     public static void validateIntegers(List<String> tokens) {
         for (String token : tokens) {
             validateInteger(token);
@@ -38,12 +23,6 @@ public class LottoNumberValidator {
             throw new IllegalArgumentException("[ERROR] 정수 형태의 번호가 아닙니다.");
         }
     }
-
-//    public static void validateRanges(List<Integer> numbers) {
-//        for (Integer number : numbers) {
-//            validateNumberRange(number);
-//        }
-//    }
 
     public static void validateNumberRange(Integer number) {
         if (number < MIN || number > MAX) {
