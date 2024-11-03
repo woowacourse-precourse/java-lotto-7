d1ce465f4a857a030d59c015187dc79a36804efd
wgrgwg <kyoeng14697@naver.com>
2024-11-03 17:56:45 +0900

    fix: Fix LottoPurchaseResult to store amount

--- src/main/java/lotto/model/LottoPurchaseResult.java
+++ src/main/java/lotto/model/LottoPurchaseResult.java
@@ -2,5 +2,5 @@ package lotto.model;
 
 import java.util.List;
 
-public record LottoPurchaseResult(List<List<Integer>> lottoTicketsNumbers, int money) {
+public record LottoPurchaseResult(List<List<Integer>> lottoTicketsNumbers, int amount) {
 }
