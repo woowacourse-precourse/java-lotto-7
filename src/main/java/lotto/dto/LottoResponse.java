e60c7c448864e775056b23a226bc8116ae76e095
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 20:47:34 +0900

    feat: 로또 목록 Response DTO 제거

--- src/main/java/lotto/dto/LottoResponse.java
+++ src/main/java/lotto/dto/LottoResponse.java
@@ -1,6 +1,10 @@
 package lotto.dto;
 
+import lotto.domain.Lotto;
 import java.util.List;
 
 public record LottoResponse(List<Integer> numbers) {
+    public LottoResponse(Lotto lotto) {
+        this(lotto.getNumbers());
+    }
 }
