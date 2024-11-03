53fa726dcfd7d92cc0bfcefa72a21a01193122c1
pbh914 <52387364+ArkBB@users.noreply.github.com>
2024-11-04 04:51:57 +0900

    feat : LottoIOHandler 구현

---
+++ src/main/java/lotto/io/LottoIOHandler.java
@@ -0,0 +1,70 @@
+package lotto.io;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+import lotto.domain.statistic.Statistic;
+
+public class LottoIOHandler {
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+
+    public Integer askPurchaseCost (){
+        outputHandler.showPurchaseCostInputComments();
+
+        Integer purchaseCost = null;
+
+        while (purchaseCost == null) {
+            try {
+                purchaseCost = inputHandler.getPurchaseCost();
+
+            } catch (IllegalArgumentException e) {
+                outputHandler.showErrorMessage(e.getMessage());
+            }
+        }
+        return purchaseCost;
+    }
+
+    public WinningLotto askWinningLotto() {
+
+        outputHandler.showWinningLottoInputComment();
+        List<Integer> winningLottoNumber = new ArrayList<>();
+
+        while (winningLottoNumber.isEmpty()) {
+            try {
+                winningLottoNumber = inputHandler.getWinningLottoInput();
+
+                WinningLotto.Builder builder = new WinningLotto.Builder().numbers(winningLottoNumber);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                winningLottoNumber.clear();
+            }
+        }
+        outputHandler.showWinningLottoBonusNumberInputComment();
+        Integer bonusNum = inputHandler.getWinningLottoBonusNumberInput();
+
+        WinningLotto winningLotto = new WinningLotto.Builder()
+                .numbers(winningLottoNumber)
+                .bonusNumber(bonusNum)
+                .build();
+        return winningLotto;
+    }
+
+    public void showPurchaseLottoCount(Integer lottoCount) {
+        outputHandler.showPurchaseLottoCount(lottoCount);
+    }
+
+    public void showNumber(Lotto lotto) {
+        outputHandler.showNumber(lotto);
+    }
+
+    public void showWinningStatistics(Statistic statistic) {
+        outputHandler.showWinningStatistics(statistic);
+    }
+
+    public void showInterestRate(double interestRate) {
+        outputHandler.showInterestRate(interestRate);
+    }
+}
