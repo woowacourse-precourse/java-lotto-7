c444d3edb76e1c6cd87983a2471021cc4f088cd6
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-11-03 18:12:05 +0900

    refactor: 메서드 이름 변경
    Dto에서 Entity를 만들기 때문에 fromEntity보다 toEntity가 더 적합하다고 생각한다.

--- src/main/java/lotto/service/BonusNumberService.java
+++ src/main/java/lotto/service/BonusNumberService.java
@@ -1,6 +1,5 @@
 package lotto.service;
 
-import lotto.domain.BonusNumber;
 import lotto.dto.BonusNumberDto;
 import lotto.repository.BonusNumberRepository;
 
@@ -8,11 +7,6 @@ public class BonusNumberService {
     private final BonusNumberRepository bonusNumberRepository = BonusNumberRepository.getInstance();
 
     public void saveBonusNumber(final BonusNumberDto dto) {
-        bonusNumberRepository.save(dto.fromEntity());
-    }
-
-    public BonusNumberDto findBonusNumber() {
-        BonusNumber bonusNumber = bonusNumberRepository.find();
-        return bonusNumber.toDto();
+        bonusNumberRepository.save(dto.toEntity());
     }
 }
