1704394f2772959043c308cd9065cdc32fcca43e
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 00:42:34 +0900

    refactor: 상금 단위 쉼표 포멧 수정

--- src/main/java/lotto/system/formater/winning/WinningEntryFormatter.java
+++ src/main/java/lotto/system/formater/winning/WinningEntryFormatter.java
@@ -4,8 +4,8 @@ import lotto.system.utils.PrizeType;
 
 public class WinningEntryFormatter { // 각 개별 당첨 항목 포맷
 
-    private final static String WINNINGS_MESSAGE = "%d개 일치 (%d원) - %d개";
-    private final static String BONUS_WINNINGS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
+    private final static String WINNINGS_MESSAGE = "%d개 일치 (%,d원) - %d개";
+    private final static String BONUS_WINNINGS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
 
     public static String formatWinnings(int code, int count) {
         PrizeType typeByCode = PrizeType.getTypeByCode(code);
