09f2a13ed45c76003a70139b822e73292d80f6a1
imscow11253 <seaweed8759@naver.com>
2024-11-04 04:29:32 +0900

    test : PriceToBuyLotto model 클래스 test code 작성

---
+++ src/test/java/lotto/model/PriceToBuyLottoTest.java
@@ -0,0 +1,81 @@
+package lotto.model;
+
+import lotto.exception.PriceToBuyLottoException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PriceToBuyLottoTest {
+
+    @DisplayName("로또 가격 객체가 성공적으로 생성된다")
+    @Test
+    void 로또_가격_객체_생성_성공(){
+        //given
+        String price = "5000";
+        //when
+        PriceToBuyLotto priceToBuyLotto = PriceToBuyLotto.of(price);
+        //then
+        assertEquals(priceToBuyLotto.price(), 5000);
+    }
+
+    @DisplayName("로또 가격 객체가 성공적으로 생성된다. 가격 문자열에 공백이 있어도 성공")
+    @Test
+    void 로또_가격_객체_생성_성공_문자열_공백(){
+        //given
+        String price = " 5000 ";
+        //when
+        PriceToBuyLotto priceToBuyLotto = PriceToBuyLotto.of(price);
+        //then
+        assertEquals(priceToBuyLotto.price(), 5000);
+    }
+
+    @DisplayName("로또 가격이 음수일 때 실패")
+    @Test
+    void 로또_가격_객체_생성_실패_음수(){
+        //given
+        String price = "-5000";
+        //when
+        //then
+        assertThrows(PriceToBuyLottoException.class, ()->{
+           PriceToBuyLotto.of(price);
+        });
+    }
+
+    @DisplayName("로또 가격이 실수일 때 실패")
+    @Test
+    void 로또_가격_객체_생성_실패_실수(){
+        //given
+        String price = "5000.1";
+        //when
+        //then
+        assertThrows(PriceToBuyLottoException.class, ()->{
+            PriceToBuyLotto.of(price);
+        });
+    }
+
+    @DisplayName("로또 가격이 1000단위가 아닐 때 실패")
+    @Test
+    void 로또_가격_객체_생성_실패_단위(){
+        //given
+        String price = "5200";
+        //when
+        //then
+        assertThrows(PriceToBuyLottoException.class, ()->{
+            PriceToBuyLotto.of(price);
+        });
+    }
+
+    @DisplayName("로또 가격이 2,000,000,000 넘을 때 실패")
+    @Test
+    void 로또_가격_객체_생성_실패_범위_초과(){
+        //given
+        String price = "2000001000";
+        //when
+        //then
+        assertThrows(PriceToBuyLottoException.class, ()->{
+            PriceToBuyLotto.of(price);
+        });
+    }
+}
