b007c9f2d470c7eeaf0385d66dd3e6451801bd9c
조민규 <ariettys2@naver.com>
2024-11-03 21:40:07 +0900

    feat : 보너스 번호 validate 메서드 구현

--- src/main/java/lotto/model/LottoList.java
+++ src/main/java/lotto/model/LottoList.java
@@ -12,12 +12,21 @@ public class LottoList {
     }
     // 당첨 번호와 보너스 번호를 설정하는 메서드
     public void setWinningNumber(Lotto winningLotto, int bonusNum) {
+        validateBonusNumber(winningLotto, bonusNum);
         this.winningLotto = winningLotto;
         this.bonusNum = bonusNum;
     }
     // 발행 로또 추가 메서드
     public void add(Lotto lotto){
         lottoList.add(lotto);
+    }
+    private void validateBonusNumber(Lotto winningLotto, int bonusNum) {
+        if (bonusNum < 1 || bonusNum > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (winningLotto.checkBonus(bonusNum)) {  // checkBonus 메서드 사용
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 
 }
