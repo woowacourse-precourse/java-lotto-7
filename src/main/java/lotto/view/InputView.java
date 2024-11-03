de2c3ac83451156be8763082456ec1e2e0daf84d
kssk3 <kss8014@daum.net>
2024-11-03 18:17:37 +0900

    Refactor: 불필요한 메서드 삭제

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,32 @@
 package lotto.view;
 
 import java.util.Scanner;
+import java.util.Set;
 
 public class InputView {
     private static final String REQUEST_BONUS_NUMBER = "보너스 볼을 입력해 주새요.";
     private static final String REQUEST_AMOUNT = "구입금액을 입력해주세요.";
     private static final String REQUEST_NUMBER = "숫자를 입력해주세요.";
-    private static final String REQUEST_INPUT_NUMBER = "0이상의 수를 입력해주세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
 
+    public static int requestAmount() {
+        try {
+            System.out.println(REQUEST_AMOUNT);
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println(REQUEST_NUMBER);
+            return requestAmount();
+        }
+    }
 
-
-    private static void validateNotNegative(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException(REQUEST_INPUT_NUMBER);
+    public static int requestBonusNumber() {
+        try {
+            System.out.println(REQUEST_BONUS_NUMBER);
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println(REQUEST_NUMBER);
+            return requestAmount();
         }
     }
 
