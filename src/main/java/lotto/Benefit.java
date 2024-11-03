de01ba30a08f42441ee7d14f4fd064196a662dd0
woongjee <jeewoong7753@naver.com>
2024-11-03 13:34:08 +0900

    fix(Winning) : 숫자 검증 기능 메소드로 분리

--- src/main/java/lotto/Benefit.java
+++ src/main/java/lotto/Benefit.java
@@ -10,4 +10,5 @@ public class Benefit {
         }
         return (double) totalPrize/purchaseAmount*100;
     }
+
 }
--- src/main/java/lotto/Winning.java
+++ src/main/java/lotto/Winning.java
@@ -10,16 +10,18 @@ public class Winning {
         if(winningNumbers.size() != 6){
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개입니다.");
         }
-        for(int i = 0; i < winningNumbers.size(); i++){
-            if(winningNumbers.get(i) < 1||winningNumbers.get(i) > 45||countOccurrences(winningNumbers, winningNumbers.get(i))>1){
-                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없는 1~45 사이의 숫자입니다.");
-            }
-        }
         if(bonusNumber<1||bonusNumber>45||winningNumbers.contains(bonusNumber)){
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 숫자입니다.");
         }
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+    private void validateWinningNumber(int winningNumber){
+        for(int i = 0; i < winningNumbers.size(); i++){
+            if(winningNumbers.get(i) < 1||winningNumbers.get(i) > 45||countOccurrences(winningNumbers, winningNumbers.get(i))>1){
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없는 1~45 사이의 숫자입니다.");
+            }
+        }
     }
     private int countOccurrences(List<Integer> numbers, int target) {
         int count = 0;
