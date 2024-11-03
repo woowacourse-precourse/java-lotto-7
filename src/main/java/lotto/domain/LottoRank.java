6b25fb4a1190bd7717547ab1167047036ae5ed9a
MinJae00 <exceedmin@gmail.com>
2024-11-04 02:26:14 +0900

    refact: 로또 최종 결과 카운트를 위한 MAP 초기화 로직 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,32 +1,37 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum LottoRank {
-    FIFTH(3, false, 5000, "5,000"),
-    FOURTH(4, false, 50000, "50,000"),
-    THIRD(5, false, 1500000, "1,500,000"),
-    SECOND(5, true, 30000000, "30,000,000"),
-    FIRST(6, false, 2000000000, "2,000,000,000"),
-    NONE(0, false, 0, "0");
+    FIFTH(3, false, 5000),
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000);
 
     private final int matchCount;
     private final boolean matchBonus;
     private final int prize;
-    private final String outputPrize;
 
-    LottoRank(int matchCount, boolean matchBonus, int prize, String standardOutputPrize) {
+    LottoRank(int matchCount, boolean matchBonus, int prize) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prize = prize;
-        this.outputPrize = standardOutputPrize;
+    }
+
+    static Map<String, Integer> LottoRankCollector() {
+        Map<String, Integer> rankCounts = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            rankCounts.put(rank.name(), 0); // 초기값 설정
+        }
+        return rankCounts;
     }
 
     public int getPrize() {
         return prize;
     }
 
-    public String getOutputPrize() {
-        return outputPrize;
-    }
 
     public int getMatchCount() {
         return matchCount;
@@ -34,9 +39,16 @@ public enum LottoRank {
 
     public static String valueOf(int matchCount, boolean matchBonus) {
         for (LottoRank rank : values()) {
-            if (rank.matchCount == matchCount && rank.matchBonus == matchBonus) {
+            if (rank.matchCount == matchCount) {
+                if (matchCount == 5 && matchBonus == false) {
+                    return "THIRD";
+                }
+                if (matchCount == 5 && matchBonus == true) {
+                    return "SECOND";
+                }
                 return rank.name();
             }
+
         }
         return "NONE";
     }
