0ecf78a2e196dcc2d58d37a8e3b2d714a2979a1d
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:04:32 +0900

    feat(Writer): Prize에 따른 출력 메시지 편집

--- src/main/java/lotto/io/Writer.java
+++ src/main/java/lotto/io/Writer.java
@@ -5,4 +5,12 @@ public class Writer {
     public static void writeMessage(String message) {
         System.out.println(message);
     }
+
+    private static String constructMessageFrom(Prize prize) {
+        if (prize.isBonusMatch()) {
+            return String.format("%d개 일치, 보너스볼 일치 (%,d원)", prize.getMatchCount(), prize.getMoney());
+        }
+
+        return String.format("%d개 일치 (%,d원)", prize.getMatchCount(), prize.getMoney());
+    }
 }
