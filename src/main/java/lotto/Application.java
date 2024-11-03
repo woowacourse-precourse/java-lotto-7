2dd6ab2021d8b3f8e685c9b268ed1dd74c3e9f22
Jaehwi Song <wotns0319@gmail.com>
2024-11-04 01:31:51 +0900

    feat(lotto): implement first feature - handling input
    1. handling input for whole budget
    2. handling input for winning lottery number
    3. handling input for bonus number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,49 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
+    public static int budget_input() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String userInput = Console.readLine().trim();
+
+        return Integer.parseInt(userInput);
+    }
+
+    public static List<Integer> lotto_input() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String userInput = Console.readLine().trim();
+        String[] splittedNumber = userInput.split(",");
+        List<Integer> winningNumber = new ArrayList<>();
+        for (String number : splittedNumber) {
+            winningNumber.add(Integer.parseInt(number));
+        }
+
+        return winningNumber;
+    }
+
+    public static int bonus_input() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String userInput = Console.readLine().trim();
+
+        return Integer.parseInt(userInput);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int budget = budget_input();
+
+        List<Integer> winningNumber = new ArrayList<>();
+        winningNumber = lotto_input();
+
+        int bonusNumber = bonus_input();
+
+
+
+
+
     }
 }
