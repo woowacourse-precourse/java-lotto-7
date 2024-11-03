0897ccd483dab1466a2049cd6718f8d21ddec3dc
Jin409 <sualng123@gmail.com>
2024-11-04 00:32:20 +0900

    test: 테스트 내부에서 사용되는 상수를 추가해 코드의 중복을 줄여라

---
+++ src/test/java/lotto/common/ConstantsForTest.java
@@ -0,0 +1,8 @@
+package lotto.common;
+
+public class ConstantsForTest {
+    public static final int VALID_SIZE = 6;
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 45;
+    public static final int PRICE_OF_LOTTO_TICKET = 1000;
+}
