044c6682a9c93a1b8fac2fa5cac715ecfb8f4694
kkmdevel <kkm.devel@gmail.com>
2024-11-03 15:18:45 +0900

    feat(OutputView): 에러 메시지 출력 기능 추가
    - 에러 메시지를 출력하는 printErrorMessage 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,9 @@ public class OutputView {
     public static void printPaymentMessage() {
         System.out.println(OutputMessages.PAYMENT.getMessage());
     }
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
 }
