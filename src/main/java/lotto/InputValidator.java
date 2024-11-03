a72a1dae60b73fc5bbbdacc2fbeb4fba4fb771be
youngseo3 <wys4475@gmail.com>
2024-11-04 02:55:03 +0900

    refactor(InputValidator): 로또 구입 금액 검증 수정 1. String을 int로 변환 시 에러 발생 2. 1000원 단위 검증 시 amount < 1000 인 경우도 추가

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -2,9 +2,25 @@ package lotto;
 
 public class InputValidator {
 
-    public void validateBuyLotto(int amount) {
-        if (amount % 1000 != 0) {
+    public void validateBuyLotto(String amount) {
+        int lottoAmount = parseInt(amount);
+
+        if (isValidThousandUnit(lottoAmount)) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해주세요.");
         }
     }
+
+    private boolean isValidThousandUnit(int amount) {
+        return (amount < 1000) || (amount % 1000 != 0);
+    }
+
+
+    private int parseInt(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 문자가 아닌 숫자를 입력해주세요.");
+        }
+    }
+
 }
