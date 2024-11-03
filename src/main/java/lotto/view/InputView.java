3f0feb409fda57f2f3af8b9a47575041c9db3c85
권장순 <jsoonworld@gmail.com>
2024-11-03 14:42:52 +0900

    feat(InputView) : 당첨 번호 입력 기능 추가
    - readLottoNumbers 메서드를 추가하여 사용자로부터 당첨 번호 입력을 받는 기능 구현
    - 입력 값을 LottoNumbersInput DTO로 변환하여 전달

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.dto.LottoNumbersInput;
 import lotto.dto.PurchaseTotalPriceInput;
 
 public class InputView {
@@ -8,5 +9,10 @@ public class InputView {
     public PurchaseTotalPriceInput readPurchaseTotalPrice() {
         String input = Console.readLine();
         return new PurchaseTotalPriceInput(input.trim());
+    }
+
+    public LottoNumbersInput readLottoNumbers() {
+        String input = Console.readLine();
+        return new LottoNumbersInput(input.trim());
     }
 }
