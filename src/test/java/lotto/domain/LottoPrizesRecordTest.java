2dc72a48a4ce28d4e6d7022f25a30d0301e1199e
qyinm <qusseun@gmail.com>
2024-11-03 17:18:08 +0900

    test: 수익률 계산 테스트 작성

--- src/test/java/lotto/domain/LottoPrizesRecordTest.java
+++ src/test/java/lotto/domain/LottoPrizesRecordTest.java
@@ -53,4 +53,48 @@ class LottoPrizesRecordTest {
                 }
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("provideRateOfReturnTestCases")
+    void 수익률_계산_테스트(Map<LottoPrize, Integer> lottoPrizesMap, LottoPrice lottoPrice, BigDecimal expectedRateOfReturn) {
+        // Given
+        LottoPrizesRecord lottoPrizesRecord = new LottoPrizesRecord(lottoPrizesMap);
+
+        // When
+        BigDecimal rateOfReturn = lottoPrizesRecord.getRateOfReturn(lottoPrice);
+        System.out.println(rateOfReturn);
+        // Then
+        assertThat(rateOfReturn).isEqualByComparingTo(expectedRateOfReturn);
+    }
+
+    static Stream<Object[]> provideRateOfReturnTestCases() {
+        return Stream.of(
+                new Object[]{
+                        Map.of(
+                                LottoPrize.FIRST_PRIZE, 1,
+                                LottoPrize.SECOND_PRIZE, 0,
+                                LottoPrize.THIRD_PRIZE, 1,
+                                LottoPrize.FOURTH_PRIZE, 1,
+                                LottoPrize.FIFTH_PRIZE, 1
+                        ),
+                        new LottoPrice(4000), // Assume lotto price is 1000
+                        BigDecimal.valueOf(50038875) // 2_001_555_000 / 4000 * 100
+                },
+                new Object[]{
+                        Map.of(
+                                LottoPrize.FIFTH_PRIZE, 2
+                        ),
+                        new LottoPrice(2000),
+                        BigDecimal.valueOf(500.0) // 10_000 / 2_000 * 100
+                },
+                new Object[]{
+                        Map.of(
+                                LottoPrize.NO_PRIZE, 5
+                        ),
+                        new LottoPrice(5000),
+                        BigDecimal.valueOf(0.0) // No prize case
+                }
+        );
+    }
+
 }
