a08bc15befc633c11ee7edefedaf42be4974971c
dongri <l0o0lv@github.com>
2024-11-03 22:11:17 +0900

    refactor(LottoNumberGenerator): 로또 번호 생성 Stream 활용 로직으로 변경

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -2,14 +2,14 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoNumberGenerator {
     public static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lottoNumbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
