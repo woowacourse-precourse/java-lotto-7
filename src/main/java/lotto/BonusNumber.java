55241953f0a8ed39193ea4d1e6bb75bcba56009d
yybmion <yyb400@naver.com>
2024-11-03 19:17:23 +0900

    test: 보너스 번호 입력 검증(숫자 검증)
    숫자가 아닌 입력값이 들어오면 예외가 발생하는 테스트코드

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class BonusNumber {
+    private final int number;
+
+    public BonusNumber(int number) {
+        this.number = number;
+    }
+
+    public static BonusNumber from(String input) {
+        return null;
+    }
+}
---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("보너스 번호 테스트")
+class BonusNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"십", "abc", "a", " "})
+    @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다.")
+    void 실패_보너스번호입력_숫자가_아님(String input) {
+        assertThatThrownBy(() -> BonusNumber.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자만 입력 가능합니다.");
+    }
+}
