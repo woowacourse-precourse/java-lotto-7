2f432219679c1ff11f105d310b7f2ab8687034e9
장영재 <urinaner@naver.com>
2024-11-04 02:36:37 +0900

    feat(lottoController): 로또 시작 메서드 초기 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,34 @@
 package lotto.controller;
 
-public class LottoController {
-    void getStartLotto(){
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoGenerator lottoGenerator;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.lottoGenerator = new LottoGenerator();
     }
+
+    public void getStartLotto() {
+        try {
+            Money money = new Money(inputView.getValue());
+            Lottos lottos = lottoGenerator.createLottos(money.getTicket());
+
+            outputView.printBuyLotto(money);
+            outputView.printLottos(lottos);
+
+
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
 }
