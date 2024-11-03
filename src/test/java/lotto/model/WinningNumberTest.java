d565c44add83da58dab4c37aafe232cb5567a216
soohalee <suha630@naver.com>
2024-11-04 04:37:37 +0900

    Test(WinningNumberTest): 테스트메소드 실행전마다 실행될 setUP메소드 추가

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -4,23 +4,40 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumberTest {
+    List<Integer> purchaseNumber;
+    WinningNumber winningNumber;
+
+    @BeforeEach
+    void setUp() {
+        purchaseNumber = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            purchaseNumber.add(i);
+        }
+
+        List<Integer> winningNumData = new ArrayList<>();
+        winningNumData.add(1);
+        winningNumData.add(2);
+        winningNumData.add(3);
+        winningNumData.add(10);
+        winningNumData.add(11);
+        winningNumData.add(12);
+        int bonusNumber = 8;
+        winningNumber = new WinningNumber(winningNumData, bonusNumber);
+    }
 
     @Test
     @DisplayName("당첨번호 생성시 보너스 번호 범위가 1~45가 아니면 예외 발생하는 테스트")
     void checkBonusNumberRange() {
         int bonusNumber = 100;
-        List<Integer> lottoNumber = new ArrayList<>();
-        for (int i = 1; i <= 6; i++) {
-            lottoNumber.add(i);
-        }
 
         assertThatThrownBy(() -> {
-            final WinningNumber winningNumber = new WinningNumber(lottoNumber, bonusNumber);
+            final WinningNumber InvalidWinningNumber = new WinningNumber(winningNumber.getNumbers(), bonusNumber);
         }).isInstanceOf(IllegalArgumentException.class);
-
     }
 }
