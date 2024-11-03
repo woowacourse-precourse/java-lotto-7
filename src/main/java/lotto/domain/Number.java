fe3fe84b025ddbe43cc7e1dd6118c67331d2376e
qyinm <qusseun@gmail.com>
2024-11-03 15:40:56 +0900

    feat: Number 숫자 범위 유효성 검사

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,10 +1,15 @@
 package lotto.domain;
 
+import static lotto.exception.number.NumberErrorCode.INVALID_NUMBER_RANGE;
+
+import lotto.exception.LottoException;
+
 public class Number {
 
     private final Integer number;
 
     public Number(Integer number) {
+        validate(number);
         this.number = number;
     }
 
@@ -15,5 +20,15 @@ public class Number {
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    private void validate(Integer number) {
+        validateNumberRange(number);
+    }
+
+    private void validateNumberRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new LottoException(INVALID_NUMBER_RANGE);
+        }
     }
 }
---
+++ src/main/java/lotto/exception/number/NumberErrorCode.java
@@ -0,0 +1,18 @@
+package lotto.exception.number;
+
+import lotto.exception.ErrorCode;
+
+public enum NumberErrorCode implements ErrorCode {
+    INVALID_NUMBER_RANGE("숫자는 1부터 45사이여야 합니다.");
+
+    private final String message;
+
+    NumberErrorCode(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+}
