457d8a81b557b87ef2c6c786362e7c1de20437f9
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 19:50:38 +0900

    feat(inputView): 당첨 번호 입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,14 +2,32 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.exception.ExceptionMessage;
 
 public class InputView {
+    private static final String COMMA = ",";
     public int inputNumber() {
         while (true) {
             String input = readLine().trim();
             try {
                 return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println(ExceptionMessage.INVALID_NUMBER_FORMAT.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        while (true) {
+            String[] winningNumbers = readLine().split(COMMA);
+            try {
+                return Stream.of(winningNumbers)
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
             } catch (NumberFormatException e) {
                 System.out.println(ExceptionMessage.INVALID_NUMBER_FORMAT.getMessage());
             }
