90b2aaa3201e311a89ddf001b473c28a8ea1b661
garden14 <ci667@dgu.ac.kr>
2024-11-03 20:18:54 +0900

    feat: 로또 구입 금액 입력 기능 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class Money {
+    private static final int ONE_THOUSAND = 1000;
+    private final int money;
+
+    public Money(final int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int value) {
+        validateAboveMinimum(value);
+        validateAmountInThousands(value);
+    }
+
+    private void validateAboveMinimum(int value) {
+        if (value < ONE_THOUSAND) {
+            throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력해야합니다.");
+        }
+    }
+
+    private void validateAmountInThousands(int value) {
+        if (value % ONE_THOUSAND != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원으로 나누어 떨어져야합니다.");
+        }
+    }
+}
