e23d1a2984d709aad4c5d51e2bc1ecb4a354bb54
FhRh <yiyop@naver.com>
2024-11-03 23:24:00 +0900

    feat(LottoInputConverter) : 로또 번호를 문자열로 입력받아 숫자 리스트를 만든다

---
+++ src/main/java/lotto/service/LottoInputConverter.java
@@ -0,0 +1,31 @@
+package lotto.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoInputConverter {
+
+    public int convertMoney(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public List<Integer> convertNumbers(String input) {
+        String[] split = input.split(",");
+
+        List<Integer> list = new ArrayList<>();
+        for (String s : split) {
+            validateNumber(s);
+            list.add(Integer.parseInt(s));
+        }
+
+        return list;
+    }
+
+    private void validateNumber(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또번호 및 보너스번호는 숫자만 입력 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/service/LottoInputConverterTest.java
@@ -0,0 +1,48 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("LottoInputConverter 테스트")
+public class LottoInputConverterTest {
+
+    private LottoInputConverter lottoInputConverter = new LottoInputConverter();
+
+    @Test
+    void 로또_구입금액을_입력받는다() {
+        // given
+        String input = "14000";
+
+        // when
+        int money = lottoInputConverter.convertMoney(input);
+
+        // then
+        assertThat(money).isEqualTo(14000);
+    }
+
+    @Test
+    void 로또_번호를_문자열로_입력받아서_숫자_리스트를_만든다() {
+        // given
+        String input = "1,2,3,4,5,6";
+
+        // when
+        List<Integer> numbers = lottoInputConverter.convertNumbers(input);
+
+        // then
+        assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"일,이,삼,사,오,육", "1,2,3,4,5,6!"})
+    void 로또_번호는_숫자만_입력_가능하다(String input) {
+        // when & then
+        assertThatThrownBy(() -> lottoInputConverter.convertNumbers(input))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
