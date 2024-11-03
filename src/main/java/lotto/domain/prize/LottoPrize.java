ef6030ca7a5719866101bcb136cf54075d2bce9a
soeunnPark <carol0724@naver.com>
2024-11-04 00:56:29 +0900

    fix(LottoPrize): fix to return Second Prize
    2등 출력에 발생하는 문제를 해결했습니다.

--- src/main/java/lotto/domain/prize/LottoPrize.java
+++ src/main/java/lotto/domain/prize/LottoPrize.java
@@ -2,11 +2,11 @@ package lotto.domain.prize;
 
 public enum LottoPrize {
 
-    FIRST_PRIZE(6, false, 2000000000),
-    SECOND_PRIZE(5,false, 30000000),
-    THIRD_PRIZE(5, false, 1500000),
-    FOURTH_PRIZE(4, true, 50000),
     FIFTH_PRIZE(3, false, 5000),
+    FOURTH_PRIZE(4, false, 50000),
+    THIRD_PRIZE(5, false, 1500000),
+    SECOND_PRIZE(5,true, 30000000),
+    FIRST_PRIZE(6, false, 2000000000),
     NONE(0, false, 0)
     ;
 
@@ -21,10 +21,12 @@ public enum LottoPrize {
     }
 
     public static LottoPrize getLottoPrize(int matchCount, boolean isBonusNumberMatch) {
+        if(matchCount == LottoPrize.SECOND_PRIZE.matchCount && isBonusNumberMatch) {
+            return LottoPrize.SECOND_PRIZE;
+        }
         for (LottoPrize lottoPrize : LottoPrize.values()) {
-            if(lottoPrize.matchCount == matchCount && lottoPrize.isBonusNumberMatchRequired == isBonusNumberMatch) {
+            if(lottoPrize.matchCount == matchCount)
                 return lottoPrize;
-            }
         }
         return LottoPrize.NONE;
     }
