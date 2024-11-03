b30d16e5eb2d8c9c234abe3e845a21969606b32a
장혜민 <gpals5987@gmail.com>
2024-11-03 16:03:40 +0900

    [refactor] LottoConstants -> GameConfig 변경

--- src/main/java/lotto/constant/GameConfig.java
+++ src/main/java/lotto/constant/GameConfig.java
@@ -1,12 +1,11 @@
 package lotto.constant;
 
-public class LottoConstants {
+public class GameConfig {
     public static final int MIN_RANGE_NUMBER = 1;
     public static final int MAX_RANGE_NUMBER = 45;
 
     public static final int LOTTO_NUMBERS_COUNT = 6;
     public static final int WINNING_LOTTO_NUMBERS_COUNT = 7;
-
 
     public static final int LOTTO_PRICE = 1000;
 }
