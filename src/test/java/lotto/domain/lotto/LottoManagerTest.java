ce80d99a0fd354f02e61be4427769ca7b2ba12f7
201912025 <michi2012@naver.com>
2024-11-03 20:56:08 +0900

    test: LottoManagerTest 기능 추가

--- src/test/java/lotto/domain/lotto/LottoManagerTest.java
+++ src/test/java/lotto/domain/lotto/LottoManagerTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class LottoManagerTest {
     private LottoValidator lottoValidator;
     private LottoNumbersGenerator lottoNumbersGenerator;
@@ -26,5 +28,41 @@ public class LottoManagerTest {
         int size = lottoManager.getLottos().size();
 
         Assertions.assertThat(size).isEqualTo(8);
+    }
+
+    @Test
+    void 로또_생성_후_모든_로또_번호_개수_테스트() {
+        int buyLottoCount = 5;
+        lottoManager.createLottos(buyLottoCount);
+
+        List<Lotto> lottos = lottoManager.getLottos();
+        for (Lotto lotto : lottos) {
+            Assertions.assertThat(lotto.getNumbers().size()).isEqualTo(6);
+        }
+    }
+
+    @Test
+    void 로또_번호_중복_테스트() {
+        int buyLottoCount = 5;
+        lottoManager.createLottos(buyLottoCount);
+
+        List<Lotto> lottos = lottoManager.getLottos();
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = lotto.getNumbers();
+            Assertions.assertThat(numbers).doesNotHaveDuplicates();
+        }
+    }
+
+    @Test
+    void 로또_번호_유효범위_테스트() {
+        int buyLottoCount = 5;
+        lottoManager.createLottos(buyLottoCount);
+
+        List<Lotto> lottos = lottoManager.getLottos();
+        for (Lotto lotto : lottos) {
+            for (Integer number : lotto.getNumbers()) {
+                Assertions.assertThat(number).isBetween(1, 45);
+            }
+        }
     }
 }
