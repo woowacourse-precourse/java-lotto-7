1b2be904c8d2324d46278b96d0bf9cbcd4820071
WhitePiano <startoflateness@gmail.com>
2024-11-04 00:30:45 +0900

    feat(application): complete 1st MVP

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import java.util.List;
+import lotto.business.LottoShop;
+import lotto.lotto.Lotto;
+import lotto.lotto.LottoResult;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoShop lottoShop = IoCContainer.getLottoShop();
+        List<Lotto> lotteriesBuy = lottoShop.sellLotto();
+        LottoResult lottoResult = lottoShop.draw();
+        lottoShop.printResult(lotteriesBuy, lottoResult);
     }
 }
