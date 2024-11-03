d4b03a9df8b7f23d8c1d8e08699ef6791215f9b2
jintakkim <mine407603@gmail.com>
2024-11-03 16:09:04 +0900

    feat(LottoManager): 로또 진행 핵심 로직 구현
    로또를 개수만큼 랜덤으로 생성한다.
    랜덤 생성된 로또와 사용자 입력 로또를 비교하여 결과를 도출한다.
    결과를 이용하여 수익률을 계산한다.

---
+++ src/main/java/lotto/LottoManager.java
@@ -0,0 +1,99 @@
+package lotto;
+
+import lotto.domain.Attempt;
+import lotto.domain.BonusLotto;
+import lotto.domain.Lotto;
+
+import java.util.*;
+
+public class LottoManager {
+    private final Attempt attempt;
+    private final Lotto lotto;
+    private final BonusLotto bonusLotto;
+    private final List<Lotto> randomLottoList;
+
+    public LottoManager(Attempt attempt, Lotto lotto, BonusLotto bonusLotto) {
+        this.attempt = attempt;
+        this.lotto = lotto;
+        this.bonusLotto = bonusLotto;
+        this.randomLottoList = makeRandomLottoList(attempt.getLottoAmount());
+    }
+
+    private List<Lotto> makeRandomLottoList(Integer amount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            lottoList.add(Lotto.makeRandomLotto());
+        }
+        return lottoList;
+    }
+
+    public Map<LottoPrize, Integer> doLotto() {
+        //기본 로또 당첨 비교
+        Map<LottoPrize, Integer> lottoPrizeMap = initializePrizeMap();
+
+        for(Lotto randomLotto: randomLottoList) {
+            int result = lotto.compareLottoNumber(randomLotto.getNumbers());
+            updatePrizeMap(lottoPrizeMap, result);
+        }
+        return lottoPrizeMap;
+    }
+
+    private void updatePrizeMap(Map<LottoPrize, Integer> prizeMap, int result) {
+        if (result == 6) {
+            incrementPrizeCount(prizeMap, LottoPrize.SIX_MATCH);
+            return;
+        }
+        if (result == 5) {
+            if (isBonusMatched()) {
+                incrementPrizeCount(prizeMap, LottoPrize.FIVE_MATCH_BONUS);
+                return;
+            }
+            incrementPrizeCount(prizeMap, LottoPrize.FIVE_MATCH);
+            return;
+        }
+        if (result == 4) {
+            incrementPrizeCount(prizeMap, LottoPrize.FOUR_MATCH);
+            return;
+        }
+        if (result == 3) {
+            incrementPrizeCount(prizeMap, LottoPrize.THREE_MATCH);
+        }
+    }
+
+    private boolean isBonusMatched() {
+        return lotto.compareLottoNumber(List.of(bonusLotto.getNumber())) == 1;
+    }
+
+    private void incrementPrizeCount(Map<LottoPrize, Integer> prizeMap, LottoPrize prize) {
+        prizeMap.put(prize, prizeMap.get(prize) + 1);
+    }
+
+    private Map<LottoPrize, Integer> initializePrizeMap() {
+        //출력 순서 보장을 위해 HashMap이 아닌 LinkedHashMap을 사용
+        Map<LottoPrize, Integer> prizeMap = new LinkedHashMap<>();
+        Arrays.stream(LottoPrize.values())
+                .forEach(prize -> prizeMap.put(prize, 0));
+        return prizeMap;
+    }
+
+    /**
+     * 수익룰 = (총 수익 - 투자 비용) / 투자 비용 * 100
+     */
+    public double getROI(Map<LottoPrize, Integer> prizeMap) {
+        int usedCash = attempt.getCashAmount();
+        int totalPrize = prizeMap.entrySet()
+                .stream()
+                .map(entry -> entry.getKey().getPrize() * entry.getValue())
+                .reduce(0, Integer::sum);
+
+        return (double) (totalPrize - usedCash) / usedCash * 100.0;
+    }
+
+    public int getLottoAmount() {
+        return attempt.getLottoAmount();
+    }
+
+    public List<Lotto> getRandomLottoList() {
+        return randomLottoList;
+    }
+}
