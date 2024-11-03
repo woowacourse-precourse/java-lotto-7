e7ab59c0061ad1988a471b79911b40bbc7c05a85
HwangBBang <h1009218@hufs.ac.kr>
2024-11-03 19:18:58 +0900

    feat(구현 목록#9) : 추첨 도메인 유닛 테스트 구현
    - 보너스 번호가 당첨 번호와 중복될 때 예외 발생
    - 보너스 번호가 범위를 벗어날 때 예외 발생
    - 유효한 당첨 번호와 보너스 번호로 Lottery 객체 생성
    - 기능 구현 목록 수정

---
+++ src/test/java/lotto/LotteryUnitTest.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import lotto.common.error.BonusNumErrorMessage;
+import lotto.domain.Lottery;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LotteryUnitTest {
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void 보너스_번호_중복_예외() {
+        // given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int invalidBonusNumber = 6;
+
+        // when & then
+        assertThatThrownBy(() -> new Lottery(winningNumbers, invalidBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BonusNumErrorMessage.CONFLICT_BONUS_NUMBER.getInfoMessage());
+    }
+
+    @DisplayName("보너스 번호가 1에서 45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 보너스_번호_범위_예외() {
+        // given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int invalidBonusNumber = 50;
+
+        // when & then
+        assertThatThrownBy(() -> new Lottery(winningNumbers, invalidBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BonusNumErrorMessage.INVALID_BONUS_NUMBER.getInfoMessage());
+    }
+
+    @DisplayName("유효한 당첨 번호와 보너스 번호로 Lottery 객체가 생성된다.")
+    @Test
+    void 유효한_당첨번호와_보너스번호_객체_생성() {
+        // given
+        List<Integer> validWinningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int validBonusNumber = 7;
+
+        // when
+        Lottery lottery = new Lottery(validWinningNumbers, validBonusNumber);
+
+        // then
+        assertThat(lottery.getWinningNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(lottery.getBonusNumber()).isEqualTo(validBonusNumber);
+    }
+}
