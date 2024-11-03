3091bebf621cf7be1a5c019383bdd66a2d55fbaf
taek2222 <oot309@daum.net>
2024-11-04 08:16:41 +0900

    test(RandomUtilTest): 로또 랜덤 번호 테스트 코드
    - 리팩토링으로 인해 변경된 점을 적용해 다시 테스트 코드를 작성했습니다.

--- src/test/java/lotto/util/RandomUtilTest.java
+++ src/test/java/lotto/util/RandomUtilTest.java
@@ -1,8 +1,8 @@
 package lotto.util;
 
-import static lotto.util.RandomUtil.LOTTO_NUMBER_COUNT;
-import static lotto.util.RandomUtil.LOTTO_NUMBER_RANGE_MAX;
-import static lotto.util.RandomUtil.LOTTO_NUMBER_RANGE_MIN;
+import static lotto.constant.LottoConstant.NUMBER_LENGTH;
+import static lotto.constant.LottoConstant.NUMBER_RANGE_MAX;
+import static lotto.constant.LottoConstant.NUMBER_RANGE_MIN;
 import static lotto.util.RandomUtil.generateLottoNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,23 +19,29 @@ class RandomUtilTest {
 
         // when & then
         assertThat(lottoNumbers)
-                .hasSize(LOTTO_NUMBER_COUNT);
+                .hasSize(NUMBER_LENGTH.getValue());
     }
 
     @Test
     void 로또_번호는_설정한_범위내_값이다() {
+        // given
         List<Integer> lottoNumbers = generateLottoNumbers();
+
+        // when & then
         assertThat(lottoNumbers)
                 .allMatch(number ->
-                        number >= LOTTO_NUMBER_RANGE_MIN &&
-                        number <= LOTTO_NUMBER_RANGE_MAX
+                        number >= NUMBER_RANGE_MIN.getValue() &&
+                                number <= NUMBER_RANGE_MAX.getValue()
                 );
     }
 
     @Test
     void 로또_번호는_중복되지_않는다() {
+        // given
         List<Integer> lottoNumbers = generateLottoNumbers();
+
+        // when & then
         assertThat(new HashSet<>(lottoNumbers))
-                .hasSize(LOTTO_NUMBER_COUNT);
+                .hasSize(NUMBER_LENGTH.getValue());
     }
 }
