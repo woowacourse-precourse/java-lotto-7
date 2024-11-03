89bddaff320dbdb607518488715c31320565a733
(minjae8563) <(minjae8563@naver.com)>
2024-11-03 14:53:37 +0900

    refactor: 많이 쓰이는 int, String을 상수화 한 클래스 구현

---
+++ src/main/java/lotto/common/Constant.java
@@ -0,0 +1,13 @@
+package lotto.common;
+
+public class Constant {
+    public static final int THOUSAND = 1000;
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int LOTTO_LENGTH = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final double TEN_DOUBLE = 10.0;
+    public static String NUMERIC_REGEX = "^[0-9]*$";
+
+}
