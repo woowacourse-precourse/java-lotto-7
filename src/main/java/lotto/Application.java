1a5dbcca18dd8dbc12ec6323c1b25e2edd7cd961
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-03 17:21:01 +0900

    refactor($Application): LottoController 에 의존성을 주입하도록 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,19 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.model.number_generator.DefaultRandomNumberGenerator;
+import lotto.model.shop.LottoShop;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
+        LottoController lottoController = new LottoController(
+                new InputView(),
+                new OutputView(),
+                new DefaultRandomNumberGenerator(),
+                new LottoShop()
+        );
 
         lottoController.run();
     }
