c2009baf453f6096c97de5398b8480975943a5c5
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 23:29:53 +0900

    feat(lotto/logic): 로또 등수 반환 기능 낙첨 추가
    null을 반환하게 되면 NullPointerException 의 위험이 있으므로 낙첨 enum을 추가한다

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -5,7 +5,8 @@ public enum Rank {
     SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
     THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
     FOURTH(4, 50000, "4개 일치 (50,000원)"),
-    FIFTH(3, 5000, "3개 일치 (5,000원)");
+    FIFTH(3, 5000, "3개 일치 (5,000원)"),
+    MISS(-1, 0, "낙첨");
 
 
     private final int matchCount;
@@ -34,6 +35,6 @@ public enum Rank {
         if (FIFTH.matchCount == matchCount) {
             return FIFTH;
         }
-        return null;
+        return MISS;
     }
 }
