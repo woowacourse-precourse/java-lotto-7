99d79f71981c9420123104697afeefe38482e1d8
sinminseok12 <sinminseok12@naver.com>
2024-11-03 19:20:31 +0900

    tes(LottoResultTest) : 로또 결과 기능 테스트 코드 구현
    수익률 계산, 당첨 정보 조회 기능을 테스트 했다.

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import lotto.constants.Ranking;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LottoResultTest {
+
+    @Test
+    void 수익률_계산_기능_테스트() {
+        List<Ranking> rankings = Arrays.asList(Ranking.FIFTH);
+        LottoResult lottoResult = LottoResult.from(rankings);
+
+        double profit = lottoResult.calculateProfit(8000);
+
+        assertThat(profit).isEqualTo(62.5);
+    }
+
+    @Test
+    void 로또_당첨_결과_생성_테스트() {
+        List<Ranking> rankings = Arrays.asList(Ranking.FIRST, Ranking.SECOND, Ranking.SECOND, Ranking.THIRD);
+
+        LottoResult lottoResult = LottoResult.from(rankings);
+
+        EnumMap<Ranking, Integer> expectedRankingMap = new EnumMap<>(Ranking.class);
+        expectedRankingMap.put(Ranking.FIRST, 1);
+        expectedRankingMap.put(Ranking.SECOND, 2);
+        expectedRankingMap.put(Ranking.THIRD, 1);
+
+        assertEquals(expectedRankingMap, lottoResult.getRankingMap());
+    }
+}
