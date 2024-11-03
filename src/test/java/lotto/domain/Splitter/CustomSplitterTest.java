7473ad6d9d65d184684a1e4ff30c30bf5004ebd6
CheChe903 <cheche903@naver.com>
2024-11-03 16:09:18 +0900

    feat(CustomSplitterTest): 단위 테스트 진행
    해당 객체에 맞는 단위 테스트를 진행합니다.

--- src/test/java/lotto/domain/Splitter/CustomSplitterTest.java
+++ src/test/java/lotto/domain/Splitter/CustomSplitterTest.java
@@ -1,18 +1,23 @@
 package lotto.domain.Splitter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class CustomSplitterTest {
 
-    private final CustomSplitter customSplitter;
-
-    CustomSplitterTest(CustomSplitter customSplitter) {
-        this.customSplitter = customSplitter;
-    }
+    private final CustomSplitter customSplitter = new CustomSplitter(",");
 
     @Test
     void 구분자로_쪼개져야_한다() {
         //given
         String input = "1,2,3,4,5,6,7";
+
+        //when
+        String[] splitInput = customSplitter.splitFrom(input);
+
+        //then
+        assertThat(splitInput.length).isEqualTo(7);
+        assertThat(splitInput[splitInput.length - 1]).isEqualTo("7");
     }
 }
