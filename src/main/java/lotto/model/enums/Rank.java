4ea29889ca479411c278fd3f15504ac9fdac1d74
gikhoon <ghikhoon@naver.com>
2024-11-04 03:26:08 +0900

    feat: 로또의 랭킹 체크 로직 구현

--- src/main/java/lotto/model/enums/Rank.java
+++ src/main/java/lotto/model/enums/Rank.java
@@ -1,20 +1,42 @@
 package lotto.model.enums;
 
 public enum Rank {
-    FIRST("6개 일치"),
-    SECOND("5개 일치, 보너스 볼 일치"),
-    THIRD("5개 일치"),
-    FOURTH("4개 일치"),
-    FIFTH("3개 일치"),
+    FIRST("6개 일치", 6),
+    SECOND("5개 일치, 보너스 볼 일치", 5),
+    THIRD("5개 일치", 5),
+    FOURTH("4개 일치", 4),
+    FIFTH("3개 일치", 3),
+    NONE("꽝", 0)
     ;
 
     private final String message;
+    private final Integer match;
 
-    Rank(String message) {
+    Rank(String message, int match) {
         this.message = message;
+        this.match = match;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public static Rank checkRank(long match, boolean bonus) {
+        if (match == FIRST.match) {
+            return FIRST;
+        }
+        if (match == SECOND.match) {
+            if (bonus) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+        if (match == FOURTH.match) {
+            return FOURTH;
+        }
+        if (match == FIFTH.match) {
+            return FIFTH;
+        }
+        return NONE;
     }
 }
