2b57fe2aaf9b6a0916099a540fff640dfac8d316
mingking2 <mingee641@gmail.com>
2024-11-03 20:56:20 +0900

    refactor: 랜덤숫자생성 메서드 파라미터 주입으로 변경

--- src/main/java/lotto/helper/util/RandomUtil.java
+++ src/main/java/lotto/helper/util/RandomUtil.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomUtil {
-    public static List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public static List<Integer> generateRandomNumbers(int startNumber, int endNumber, int countNumber) {
+        return Randoms.pickUniqueNumbersInRange(startNumber, endNumber, countNumber);
     }
 }
