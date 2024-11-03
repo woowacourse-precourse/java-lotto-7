6069e2d6add7a3817ca971444842a311da52e463
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-11-03 13:08:27 +0900

    feat: 등수 관련 enum 내용 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -1,17 +1,27 @@
 package lotto;
 
 public class Constants {
-
     public enum LottoRule {
         MIN_NUMBER(1),
         MAX_NUMBER(45),
         NUM_AMOUNT(6);
-
         private final int number;
-        LottoRule( int number ){
-            this.number = number;
-        }
+        LottoRule( int number ){ this.number = number; }
         public int getNumber(){ return this.number; }
-
+    }
+    public enum LottoGrade {
+        FIRST_PRIZE( "6개 일치", 2000000000 ),
+        SECOND_PRIZE( "5개 일치, 보너스 볼 일치", 30000000 ),
+        THIRD_PRIZE( "5개 일치", 1500000 ),
+        FOURTH_PRIZE( "4개 일치", 50000 ),
+        FIFTH_PRIZE( "3개 일치", 5000 );
+        private final String condition;
+        private final int prize;
+        LottoGrade( String condition, int prize ){
+            this.condition = condition;
+            this.prize = prize;
+        }
+        public String getCondition() { return this.condition; }
+        public int getPrize() { return this.prize; }
     }
 }
