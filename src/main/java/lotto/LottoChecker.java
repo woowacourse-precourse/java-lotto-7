84e2514c928c2341e415cea8d752d503c55245da
badgelatte <saucy5939@gmail.com>
2024-11-04 05:12:45 +0900

    수익률과 당첨 내역 출력하는 메서드 구현
    List 대신 int 배열로 등수를 받아 해당하는 번째의 배열에 +1을 총 등수 계산을 함
    이후 showLottoResult 메서드에서 당첨 내역을 알려주고 수익률을 표기함

--- src/main/java/lotto/LottoChecker.java
+++ src/main/java/lotto/LottoChecker.java
@@ -12,14 +12,34 @@ public class LottoChecker {
         this.bonusNumber = bonusNumber;
     }
 
-    public List<Integer> lottoCheck(Customer customer) {
-        List<Integer> totalRanks = new ArrayList<>();
+    public void showLottoResult(int[] totalRanks) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + totalRanks[5] + "개");
+        System.out.println("4개 일치 (50,000원) - " + totalRanks[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + totalRanks[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + totalRanks[2] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + totalRanks[1] + "개");
+
+        int prizeAmount =
+                5000 * totalRanks[5] + 50000 * totalRanks[4] + 1500000 * totalRanks[3] + 30000000 * totalRanks[2]
+                        + 2000000000 * totalRanks[1];
+        int ticketNums = totalRanks[0] + totalRanks[1] + totalRanks[2] + totalRanks[3] + totalRanks[4] + totalRanks[5];
+        double rateOfReturn = (double) prizeAmount / (ticketNums * 1000) * 100;
+
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
+    }
+
+    public int[] lottoCheck(Customer customer) {
+        int[] totalRanks = new int[6];
 
         for (Lotto lotto : customer.getLottos()) {
             int count = determineRank(lotto);
             int rank = calculateRankFromCount(count);
-            totalRanks.add(rank);
+            totalRanks[rank]++;
         }
+
+        showLottoResult(totalRanks);
         return totalRanks;
     }
 
