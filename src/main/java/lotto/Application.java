4b5ae376f920135a28c7cd575f1d2948b007dd42
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 04:03:59 +0900

    refactor(Application): YieldAnalyst 의존성 주입 추가
    - LottoController에 YieldAnalyst를 주입하여 코드의 의존성 관리 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,17 +2,15 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.model.LottoNumberGenerator;
+import lotto.model.YieldAnalyst;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(
-                new InputView(),
-                new OutputView(),
-                new LottoNumberGenerator()
-        );
+        LottoController lottoController = new LottoController(new InputView(), new OutputView(),
+                new LottoNumberGenerator(), new YieldAnalyst());
 
         lottoController.start();
     }
