4ba44eb86d9f906699531f59336ec52385a5f9fe
BusanGukbap <dong2857210@gmail.com>
2024-11-04 01:39:26 +0900

    feat(InputView) : Add getInputPurchaseAmount
    구입 금액을 입력받는 메소드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,21 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.LottoValidator;
 
 public class InputView {
+
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static String getInputPurchaseAmount() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+        String input = Console.readLine();
+        LottoValidator.validatePurchaseAmount(input);
+        return input;
+    }
+
     public static String inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
         return Console.readLine();
