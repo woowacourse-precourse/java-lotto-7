2ea52d5cbece40780fa65c1e3bee12aeb4be6cce
CHISANW <keuye06380618@gmail.com>
2024-11-03 17:14:00 +0900

    feat(LottoView): 로또 게임 시작 메서드 구현
    - 로또 번호 구매를 위한 가격 입력 처리
    - 가격에 따라 랜덤으로 로또 번호 생성
    - 생성된 로또 번호 출력
    - 당첨 번호 및 보너스 번호 입력 처리
    - LottoDto 객체 생성하여 초기화 정보 반환

---
+++ src/main/java/lotto/view/LottoView.java
@@ -0,0 +1,35 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.Lotto;
+import lotto.dto.LottoDto;
+
+public class LottoView {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoView() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public LottoDto initLottoGame() {
+        int priceInput = inputView.buyLottoPriceInput();
+        List<Lotto> lottos = createRandomLottoNumbers(priceInput);
+        outputView.buyOutput(priceInput, lottos);
+
+        List<Integer> winningNumbers = inputView.winningNumberInput();
+        int bonusNumber = inputView.bonusNumber(winningNumbers);
+
+        return LottoDto.of(lottos, winningNumbers, bonusNumber);
+    }
+
+    private List<Lotto> createRandomLottoNumbers(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> Lotto.createNumber())
+                .toList();
+    }
+
+}
