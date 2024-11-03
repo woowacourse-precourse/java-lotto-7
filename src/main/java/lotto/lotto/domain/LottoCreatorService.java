e1e85cde2ae15890bdd18c6af42ff21d47783b15
EHCTH <lees90896@gmail.com>
2024-11-03 12:23:49 +0900

    feat(LottoGenerator): 클래스 명 및 메서드 명 변경

--- src/main/java/lotto/lotto/domain/LottoCreatorService.java
+++
@@ -1,7 +0,0 @@
-package lotto.lotto.domain;
-
-public interface LottoCreatorService {
-    LottoTickets createLottoTickets(int count);
-    BonusNumber createBonusNumber(WinningLotto winningLotto);
-    WinningLotto createWinningLotto();
-}
