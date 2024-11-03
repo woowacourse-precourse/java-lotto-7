99c74b1df8fa6ddcc6a56ba3164af7621c26765b
kkmdevel <kkm.devel@gmail.com>
2024-11-03 16:56:43 +0900

    feat(Lotto): 로또 번호 검증 로직 추가
    - 로또 번호 개수 유효성 검사 로직 추가
    - 로또 번호 범위 유효성 검사 추가
    - 중복된 로또 번호 검증 로직 추가
    - 유효성 검증 실패 시 LottoException을 통해 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.LottoException;
+import lotto.exception.message.LottoExceptionMessage;
+
+import static lotto.constant.LottoConstant.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,10 +14,40 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateNumbersSizeIsLottoCount(numbers);
+        validateNumbersInRange(numbers);
+        validateDuplicated(numbers);
+    }
+
+    private void validateNumbersSizeIsLottoCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_TO_DRAW) {
+            throw new LottoException(LottoExceptionMessage.INVALID_LOTTO_COUNT);
         }
+    }
+
+    private void validateNumbersInRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+                throw new LottoException(LottoExceptionMessage.INVALID_LOTTO_NUMBER);
+            }
+        }
+    }
+
+    private void validateDuplicated(List<Integer> numbers) {
+        if (hasDuplicate(numbers)) {
+            throw new LottoException(LottoExceptionMessage.DUPLICATE_LOTTO_NUMBER);
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.stream()
+                .distinct()
+                .count() != numbers.size();
     }
 
 
