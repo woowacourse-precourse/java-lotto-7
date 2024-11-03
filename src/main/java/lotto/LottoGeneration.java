3abbcb1443b39f3bde5857ac9614a4a03aa698ff
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 04:31:24 +0900

    refactor: 변수명 수정

--- src/main/java/lotto/LottoGeneration.java
+++ src/main/java/lotto/LottoGeneration.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class LottoGeneration {  // 번호 열거형
     public enum LottoOption {
         NUMBER_MAX(45),
-        NUMBER_COUNT(6);
+        NUMBER_COUNT(6),
+        NUMBER_START(1);
 
         private final int value;
 
@@ -22,7 +23,7 @@ public class LottoGeneration {  // 번호 열거형
     }
 
     public List<Integer> pickUniqueNumbers () {     // 1부터 45 중복되지 않는 숫자 뽑기
-        return Randoms.pickUniqueNumbersInRange(1, LottoOption.NUMBER_MAX.getValue(), LottoOption.NUMBER_COUNT.getValue());
+        return Randoms.pickUniqueNumbersInRange(LottoOption.NUMBER_START.getValue(), LottoOption.NUMBER_MAX.getValue(), LottoOption.NUMBER_COUNT.getValue());
     }
 
     public List<Lotto> generateLottoLines (int count) {     // 횟수를 받아서 몇줄 생성하는지
