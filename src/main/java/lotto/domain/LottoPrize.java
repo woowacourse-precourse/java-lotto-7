aed7392898d2a4c3f9279c66a857af764fce0fef
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:58:34 +0900

    :sparkles: feat(LottoPrize): 로또 당첨 결과에 따른 당첨금 계산

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
 public class LottoPrize {
+    private final int lottoPrize;
+
+    public LottoPrize(LottoRank lottoRank) {
+        this.lottoPrize = calculateLottoPrize(lottoRank);
+    }
+
+    public int getLottoPrize() {
+        return lottoPrize;
+    }
+
+    private int calculateLottoPrize(LottoRank lottoRank) {
+        return lottoRank.getLottoRank().entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getReward() * entry.getValue())
+                .sum();
+    }
 }
