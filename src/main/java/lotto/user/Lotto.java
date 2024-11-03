3d0637a309652c2b707f044b1cad19475f35875f
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 00:26:00 +0900

    refactor: 로또 번호 유효성 검사 메서드 분리

--- src/main/java/lotto/user/Lotto.java
+++ src/main/java/lotto/user/Lotto.java
@@ -13,15 +13,26 @@ public class Lotto { // 사용자 입력 로또 번호 검증 후 객체 생성
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateNumbersRange(numbers);
+    }
+
+    private static void validateNumbersRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateNumberRange(number);
+        }
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < LottoNumber.LOTTO_NUMBER_LOWER_BOUND || LottoNumber.LOTTO_NUMBER_UPPER_BOUND < number) {
+            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d사이의 숫자여야 합니다.",
+                    LottoNumber.LOTTO_NUMBER_LOWER_BOUND, LottoNumber.LOTTO_NUMBER_UPPER_BOUND));
+        }
+    }
+
+    private static void validateNumbersSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-
-        for (int number : numbers) {
-            if (number < LottoNumber.LOTTO_NUMBER_LOWER_BOUND || LottoNumber.LOTTO_NUMBER_UPPER_BOUND < number) {
-                throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d사이의 숫자여야 합니다.",
-                        LottoNumber.LOTTO_NUMBER_LOWER_BOUND, LottoNumber.LOTTO_NUMBER_UPPER_BOUND));
-            }
         }
     }
 
