5d6f413722a7973c77fb7a14a581c5fc649af6bf
johyewon <johyewon4601@gmail.com>
2024-11-04 01:44:20 +0900

    feat(LottoMachine): 금액에 따른 로또 발행 메소드 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -3,8 +3,19 @@ package lotto;
 import java.util.*;
 
 public class LottoMachine {
+    static final int AMOUNT_UNIT = 1000;
+
     public List<Lotto> issueLottos(int amount) {
-        return null;
+        validateAmount(amount);
+
+        int lottoCnt = amount / AMOUNT_UNIT;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCnt; i++) {
+            lottos.add(generateLotto());
+        }
+
+        return lottos;
     }
 
     private void validateAmount(int amount) {
