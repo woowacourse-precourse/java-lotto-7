433f0a65560f1771422093787985a6426c257d1e
이윤빈 <kanado20000@naver.com>
2024-11-03 19:47:49 +0900

    :art: refactor: 불필요한 상수 제거

--- src/main/java/lotto/utilities/Random.java
+++ src/main/java/lotto/utilities/Random.java
@@ -2,16 +2,17 @@ package lotto.utilities;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.constants.RandomNumberConstants;
 
 public class Random {
-    private static final int MINIMUM_RANDOM_NUMBER = 1;
-    private static final int MAXIMUM_RANDOM_NUMBER = 45;
-    private static final int QUANTITY_OF_NUMBERS = 6;
+  private static final int QUANTITY_OF_NUMBERS = 6;
 
-    public static List<Integer> lottoGenerator() {
-        List<Integer> randomNumber =
-            Randoms.pickUniqueNumbersInRange(
-                MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER, QUANTITY_OF_NUMBERS);
-        return randomNumber;
-    }
+  public static List<Integer> lottoGenerator() {
+    List<Integer> randomNumber =
+        Randoms.pickUniqueNumbersInRange(
+            RandomNumberConstants.MINIMUM_RANDOM_NUMBER,
+            RandomNumberConstants.MAXIMUM_RANDOM_NUMBER,
+            QUANTITY_OF_NUMBERS);
+    return randomNumber;
+  }
 }
