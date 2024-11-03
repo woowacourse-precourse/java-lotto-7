36198c6e25bd41da35055b40d2c75a153b663061
koosco <rnxogud123@naver.com>
2024-11-03 21:21:02 +0900

    fix: 출력 문구가 나오지 않는 에러 수정
    InputView에서 출력 문구가 나오지 않는 에러를 수정했습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public int getPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
         int price = Integer.parseInt(Console.readLine());
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("금액은 1,000원 단위로 입력해야 합니다");
