f083145de20c5a30a3349fd07f6a20bdda408cf9
hlmg <devhlmg@gmail.com>
2024-11-04 01:49:27 +0900

    feat: 구입금액 제한 추가

--- src/main/java/lotto/domain/money/PurchaseAmount.java
+++ src/main/java/lotto/domain/money/PurchaseAmount.java
@@ -5,7 +5,8 @@ import lotto.exception.LottoApplicationException;
 public class PurchaseAmount {
 
     private static final int UNIT = 1000;
-    private static final int MIN_AMOUNT = 0;
+    private static final int MIN_AMOUNT = 1000;
+    private static final int MAX_AMOUNT = 100_000;
 
     private final int amount;
 
@@ -19,12 +20,21 @@ public class PurchaseAmount {
     }
 
     private void validate(int amount) {
-        if (amount < MIN_AMOUNT) {
-            throw new LottoApplicationException(String.format("구입 금액은 %,d보다 작을 수 없습니다.", MIN_AMOUNT));
+        if (isOutOfRange(amount)) {
+            String message = String.format("구입 금액은 %,d원 이상, %,d원 이하여야 합니다.", MIN_AMOUNT, MAX_AMOUNT);
+            throw new LottoApplicationException(message);
         }
-        if (amount % UNIT != 0) {
+        if (isInvalidUnit(amount)) {
             throw new LottoApplicationException(String.format("구입 금액은 %,d원 단위여야 합니다.", UNIT));
         }
+    }
+
+    private boolean isOutOfRange(int amount) {
+        return amount < MIN_AMOUNT || amount > MAX_AMOUNT;
+    }
+
+    private boolean isInvalidUnit(int amount) {
+        return amount % UNIT != 0;
     }
 
     public Money toMoney() {
--- src/test/java/lotto/domain/money/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/money/PurchaseAmountTest.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.params.provider.CsvSource;
 class PurchaseAmountTest {
 
     @CsvSource(textBlock = """
-            -1,[ERROR] 구입 금액은 0보다 작을 수 없습니다.
-            900,'[ERROR] 구입 금액은 1,000원 단위여야 합니다.'
+            -1,'[ERROR] 구입 금액은 1,000원 이상, 100,000원 이하여야 합니다.'
+            999,'[ERROR] 구입 금액은 1,000원 이상, 100,000원 이하여야 합니다.'
+            100001,'[ERROR] 구입 금액은 1,000원 이상, 100,000원 이하여야 합니다.'
+            1500,'[ERROR] 구입 금액은 1,000원 단위여야 합니다.'
             """)
     @ParameterizedTest
     void 유효한_구입_금액이_아니면_예외가_발생한다(int amount, String message) {
