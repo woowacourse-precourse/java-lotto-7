df88b52902032be1fc4c857453ec60c64ad20825
SeongUk52 <rkfka7849@gmail.com>
2024-11-03 17:23:11 +0900

    feat: 로또 구입 금액을 입력받는 기능 구현

---
+++ src/main/java/lotto/controller/LottoContoller.java
@@ -0,0 +1,39 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.service.LottoService;
+import lotto.service.LottoServiceImpl;
+import lotto.view.InputView;
+
+public class LottoContoller {
+    private LottoService lottoService;
+    private InputView inputView;
+
+    private LottoContoller(LottoService lottoService, InputView inputView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+    }
+
+    private static class Holder {
+        private static final LottoContoller INSTANCE = new LottoContoller(
+                LottoServiceImpl.getInstance(),
+                InputView.getInstance()
+        );
+    }
+
+    public static LottoContoller getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public void run() {
+        lottoService.computeProfitRate(
+                parseNumeric(inputView.inputPurchaseAmount()),
+                null,
+                0
+        );
+    }
+
+    private int parseNumeric(String stringInput) {
+        return Integer.parseInt(stringInput);
+    }
+}
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private InputView() {}
+
+    private static class Holder {
+        private static final InputView INSTANCE = new InputView();
+    }
+
+    public static InputView getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
