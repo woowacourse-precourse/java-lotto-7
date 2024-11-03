6188200a7d7646ce1baa31ae4ced20d22ed991c0
na0th <105775683+na0th@users.noreply.github.com>
2024-11-03 13:46:18 +0900

    feat : Parser 클래스 추가
    추가한 내용 :
    1. Validate 후 데이터를 원하는 형태로 변경해주는 Parser 클래스 추가
    -> 왜? 이를 다른 클래스에 책임을 주기에는 너무 맥락이 없는 기능이라서

---
+++ src/main/java/lotto/parser/Parser.java
@@ -0,0 +1,11 @@
+package lotto.parser;
+
+public class Parser {
+
+    public void parseInputToNumbers(String input) {
+
+    }
+    public int parseStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
