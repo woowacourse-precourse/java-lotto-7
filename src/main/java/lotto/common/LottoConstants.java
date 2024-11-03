e2e0d7f8e7134b620ceef5562f870a78e984ff09
minjae k <rkdalswoals@gmail.com>
2024-11-04 01:38:29 +0900

    refactor(common): implement enum lotto constants

---
+++ src/main/java/lotto/common/LottoConstants.java
@@ -0,0 +1,9 @@
+package lotto.common;
+
+public class LottoConstants {
+    public static final int TICKET_PRICE = 1000;
+    public static final int ZERO = 0;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+}
