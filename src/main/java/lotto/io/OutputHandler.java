1226583d21873a988cb3af2ec90db7cc080f7552
lvalentine6 <ysr1227@naver.com>
2024-11-03 22:36:52 +0900

    refactor: add constant, change show formatting

--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -10,7 +10,12 @@ public class OutputHandler {
 	private final static String PROMPT_PURCHASE_COUNT_SUFFIX = "개를 구매했습니다.";
 	private final static String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 	private final static String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-	private final static String PROMPT_WINNING_REPORT_HEADER = "당첨 통계 \n ---";
+	private final static String PROMPT_WINNING_REPORT_HEADER = "당첨 통계\n---";
+	private final static String PROMPT_WINNING_REPORT_DELIMITER = " - ";
+	private final static String PROMPT_WINNING_REPORT_COUNT = "개";
+	private final static String PROMPT_WINNING_REPORT_PROFIT_RATE_PREFIX = "총 수익률은 ";
+	private final static String PROMPT_WINNING_REPORT_PROFIT_RATE_SUFFIX = "%입니다.";
+	private final static String PROMPT_NEW_LINE = "\n";
 
 	private OutputHandler() {
 	}
@@ -27,24 +32,33 @@ public class OutputHandler {
 		System.out.println(PROMPT_PURCHASE_AMOUNT);
 	}
 
-	public void showPurchaseLottos(int lottoSize, List<Lotto> lottos) {
-		System.out.println(lottoSize + PROMPT_PURCHASE_COUNT_SUFFIX);
-		lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+	public void showPurchaseLottos(int lottoSize, List<Lotto> purchasedLottos) {
+		System.out.println(PROMPT_NEW_LINE + lottoSize + PROMPT_PURCHASE_COUNT_SUFFIX);
+		purchasedLottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
 	}
 
 	public void showWinningNumbersPrompt() {
-		System.out.println(PROMPT_WINNING_NUMBERS);
+		System.out.println(PROMPT_NEW_LINE + PROMPT_WINNING_NUMBERS);
 	}
 
 	public void showBonusNumberPrompt() {
-		System.out.println(PROMPT_BONUS_NUMBER);
+		System.out.println(PROMPT_NEW_LINE + PROMPT_BONUS_NUMBER);
 	}
 
-	public void showWinningReport() {
-		System.out.println(PROMPT_WINNING_REPORT_HEADER);
+	public void showWinningReportHeader() {
+		System.out.println(PROMPT_NEW_LINE + PROMPT_WINNING_REPORT_HEADER);
+	}
+
+	public void showWinningReportBody(String message, int matchCount) {
+		System.out.println(message + PROMPT_WINNING_REPORT_DELIMITER + matchCount + PROMPT_WINNING_REPORT_COUNT);
+	}
+
+	public void showWinningReportProfitRate(String profitRate) {
+		System.out.println(
+			PROMPT_WINNING_REPORT_PROFIT_RATE_PREFIX + profitRate + PROMPT_WINNING_REPORT_PROFIT_RATE_SUFFIX);
 	}
 
 	public void showExceptionMessage(String message) {
-		System.out.println(message);
+		System.out.println(message + PROMPT_NEW_LINE);
 	}
 }
