7afc4821322f2e9210b6e9d54c8b36962ac09080
venus01 <geumsung7769@naver.com>
2024-11-03 22:00:28 +0900

    Feat : 로또 번호를 생성하는 기능 개발
    로또 번호를 생성하는 LottoNumberGenerator를 개발하였음

---
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -0,0 +1,44 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.BonusNumber;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.constant.Amount.LOTTO_NUMBERS_SIZE;
+import static lotto.constant.Amount.MAXIMUM_LOTTO_NUMBER;
+import static lotto.constant.Amount.MINIMUM_LOTTO_NUMBER;
+import static lotto.constant.Amount.THOUSAND;
+
+public class LottoNumberGenerator {
+
+    private final List<List<Integer>> lottoNumbersList = new ArrayList<>();
+
+    public List<List<Integer>> generateLottoNumbers(int quantity) {
+        for (int i = 0; i < quantity / THOUSAND.getValue(); i++) {
+            ArrayList<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER.getValue(), MAXIMUM_LOTTO_NUMBER.getValue(), LOTTO_NUMBERS_SIZE.getValue()));
+            sortLottoNumbers(lottoNumbers);
+            lottoNumbersList.add(lottoNumbers);
+        }
+        return lottoNumbersList;
+    }
+
+    public Lotto createWinningNumbers(String winningNumbers) {
+        String[] separatedWinningNumbers = WinningNumberSeparator.separatedLottoNumbers(winningNumbers);
+        List<Integer> parseWinningNumbers = WinningNumberParser.parseWinningNumber(separatedWinningNumbers);
+        return new Lotto(parseWinningNumbers);
+    }
+
+
+    private void sortLottoNumbers(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
+    }
+
+    public BonusNumber createBonusNumber(String bonusNumber, List<Integer> winningNumbers) {
+        int parsedBonusNumber = WinningNumberParser.parseBonusNumber(bonusNumber);
+        return new BonusNumber(parsedBonusNumber, winningNumbers);
+    }
+}
