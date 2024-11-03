276e8619c823e105c33b4c51ca4ff2bcadf79810
Lee Ye Jin <jene0028@gmail.com>
2024-11-04 07:48:06 +0900

    feat: 입력 처리 - 로또 개수 계산, 당첨 번호 구분자 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,28 @@
 package lotto.controller;
 
+import lotto.model.LottoMachine;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.Set;
+
 public class LottoController {
+    private final LottoMachine lottoMachine = new LottoMachine();
+
+    public void run() {
+        try {
+            int purchaseAmount = InputView.requestPurchaseAmount();
+            lottoMachine.purchaseLottos(purchaseAmount);
+
+            OutputView.printPurchasedLottos(lottoMachine.getPurchasedLottos());
+
+            // 당첨 번호 및 보너스 번호 입력 받기
+            Set<Integer> winningNumbers = InputView.requestWinningNumbers();
+            int bonusNumber = InputView.requestBonusNumber(winningNumbers);
+
+            // TODO: 당첨 결과 계산 및 출력 로직 추가
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,4 +1,37 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.exception.InvalidPurchaseAmountException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+    private final List<Lotto> purchasedLottos = new ArrayList<>();
+
+    // 구입 금액을 기준으로 로또 개수를 계산하고 발행
+    public void purchaseLottos(int amount) {
+        int count = calculateLottoCount(amount);
+        for (int i = 0; i < count; i++) {
+            purchasedLottos.add(generateLotto());
+        }
+    }
+
+    private int calculateLottoCount(int amount) {
+        if (amount < LOTTO_PRICE) {
+            throw new InvalidPurchaseAmountException("구입 금액이 부족하여 로또를 구매할 수 없습니다.");
+        }
+        return amount / LOTTO_PRICE;
+    }
+
+    private Lotto generateLotto() {
+        // List<Integer>로 받기
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    public List<Lotto> getPurchasedLottos() {
+        return purchasedLottos;
+    }
 }
