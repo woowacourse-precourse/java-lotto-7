03de3e39d869058d99f894a327274632c8f5091f
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2024-11-04 08:22:17 +0900

    feat: 발행된 로또 티켓 수량 및 번호 출력 기능 추가
    사용자가 구매한 로또 티켓 수량과 각 로또의 번호를 오름차순으로 출력하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 
 public class Application {
     private static final int LOTTO_PRICE = 1000;
@@ -17,6 +18,9 @@ public class Application {
     public void run() {
         int purchaseAmount = getPurchaseAmount();
         int ticketCount = purchaseAmount / LOTTO_PRICE;
+
+        List<Lotto> purchasedLottos = generateLottos(ticketCount);
+        displayLottos(purchasedLottos);
     }
 
     private int getPurchaseAmount() {
@@ -27,5 +31,20 @@ public class Application {
             throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
         return amount;
+    }
+
+    private List<Lotto> generateLottos(int ticketCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            lottos.add(Lotto.generateRandomLotto());
+        }
+        return lottos;
+    }
+
+    private void displayLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
