84b74f6b42a68f3d8bd5e3b1ec92cde5ec9d706d
rootTiket <l20078829@gmail.com>
2024-11-03 18:28:20 +0900

    feat(CashRegister): 금액이 0인경우 예외처리 추가

--- src/main/java/lotto/domain/CashRegister.java
+++ src/main/java/lotto/domain/CashRegister.java
@@ -10,13 +10,17 @@ public class CashRegister {
         validate(money);
         this.money = money;
     }
-    
+
     public int calculateLottoCount() {
         return this.money / SINGLE_LOTTO_PRICE;
     }
 
     private void validate(int money) {
         if (money % SINGLE_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(PRICE_UNMATCHED.getMessage());
+        }
+
+        if (money == 0) {
             throw new IllegalArgumentException(PRICE_UNMATCHED.getMessage());
         }
     }
