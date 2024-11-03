5ad96cf0d298f51505c89e178be8ff550a68b169
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:23:27 +0900

    fix(LottoPrintFormat): 구분자 수정

--- src/main/java/lotto/model/lotto/LottoPrintFormat.java
+++ src/main/java/lotto/model/lotto/LottoPrintFormat.java
@@ -3,7 +3,9 @@ package lotto.model.lotto;
 public final class LottoPrintFormat {
 
     public static final int NUMBER_COUNT = 6;
-    public static final String DELIMITER = ",";
+    public static final String DELIMITER = ", ";
     public static final String PREFIX = "[";
     public static final String SUFFIX = "]";
+
+    private LottoPrintFormat() {}
 }
