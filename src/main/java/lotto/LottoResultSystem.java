71ea882f4c0cc13164818a2fd7cb2be07364d26c
capDoYeonLee <ghtyu21@naver.com>
2024-11-04 04:37:39 +0900

    feat: 로또 당첨 통계하는 객체 개발
    - displayResults() : 사용자가 구매한 번호와 당첨 번호를 비교하여 동일한 번호 카운트 기능

---
+++ src/main/java/lotto/LottoResultSystem.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResultSystem {
+    Map<Integer, Integer> results = new HashMap<>();
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public LottoResultSystem(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void displayResults(List<Lotto> purchasedLottos, Integer money) {
+        Map<Integer, Integer> matchResults = calculateMatchResults(purchasedLottos);
+        int bonusMatchCount = calculateBonusMatches(purchasedLottos);
+        LottoSweepstakesTvShow lottoTV = new LottoSweepstakesTvShow(matchResults, bonusMatchCount);
+        lottoTV.printResults(money);
+    }
+
+    private Map<Integer, Integer> calculateMatchResults(List<Lotto> purchasedLottos) {
+        for (Lotto lotto : purchasedLottos) {
+            updateMatchResults(lotto);
+        }
+        return results;
+    }
+
+    private void updateMatchResults(Lotto lotto) {
+        int matchCount = lotto.countMatches(winningNumbers);
+        if (isBonusMatch(matchCount, lotto)) {
+            return;
+        }
+        results.put(matchCount, results.getOrDefault(matchCount, 0) + 1);
+    }
+
+    private boolean isBonusMatch(int matchCount, Lotto lotto) {
+        return ((matchCount == 5) && (lotto.containsBonusNumber(bonusNumber)));
+    }
+
+    private int calculateBonusMatches(List<Lotto> purchasedLottos) {
+        int bonusMatchCount = 0;
+        for (Lotto lotto : purchasedLottos) {
+            if (isBonusMatch(lotto.countMatches(winningNumbers), lotto)) {
+                bonusMatchCount++;
+            }
+        }
+        return bonusMatchCount;
+    }
+}
