00d3b9719b2912b317be9f170354828938a7abc0
장영재 <urinaner@naver.com>
2024-11-03 14:16:53 +0900

    feat(lottoNumber): 로또 번호 검증 기능 구현

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,9 +1,18 @@
 package lotto;
 
 public class LottoNumber {
+    private final static int MIN_LOTTO_NUMBER = 1;
+    private final static int MAX_LOTTO_NUMBER = 45;
+    private final static String OUT_OF_RANGE = "로또번호는 1~45의 범위입니다.";
     private int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
+        validateLottoNumber(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+    private void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < MIN_LOTTO_NUMBER | lottoNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_RANGE);
+        }
     }
 }
