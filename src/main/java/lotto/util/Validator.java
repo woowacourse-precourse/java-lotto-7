bdea8a7d88ec91370bcc5797f624f1a3411d02a5
KSJ0128 <ksj01128@naver.com>
2024-11-04 01:40:39 +0900

    feat: Number 검증 로직 구현
    Validator 내에 숫자가 입력된 것이 맞는지 검증하는 로직을 구현했습니다.

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,9 +1,18 @@
 package lotto.util;
 
+import lotto.message.ErrorMessage;
+
 import java.math.BigInteger;
 
 public class Validator {
     public BigInteger validPurchaseMoney(String number) {
+        if (!isNumber(number))
+            throw new IllegalArgumentException(ErrorMessage.PREFIX.getMessage() + ErrorMessage.NONE_NUMBER);
+
         return new BigInteger(number);
+    }
+
+    public boolean isNumber(String number) {
+        return number.matches("\\d+");
     }
 }
