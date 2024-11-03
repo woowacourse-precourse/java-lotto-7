9b1ab9d15eabb0f4514411331f6a563c21265c4a
sejinezy <psj8880@naver.com>
2024-11-03 23:53:35 +0900

    refactor(PurchaseAmount): 구매 금액에 대한 클래스 생성
    - 구매 금액을 관리하는 클래스를 별도로 생성하여 책임 분리
    - Input에 있던 구매 금액 관련 검증 메서드를 PurchaseAmount로 이동

---
+++ src/main/java/lotto/PurchaseAmount.java
@@ -0,0 +1,41 @@
+package lotto;
+
+public class PurchaseAmount {
+
+    private final int amount;
+
+    public PurchaseAmount(int amount) {
+        this.amount = amount;
+        validatePurchaseAmount();
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    private void validatePurchaseAmount() {
+        validatePositiveAmount();
+        validateDivisibleByThousand();
+    }
+
+    private void validateDivisibleByThousand() {
+        if (isNotDivisibleByThousand()) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+    private boolean isNotDivisibleByThousand() {
+        return amount % 1000 != 0;
+    }
+
+    private void validatePositiveAmount() {
+        if (isNegativeOrZero()) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 0원보다 커야 합니다.");
+        }
+    }
+
+    private boolean isNegativeOrZero() {
+        return amount <= 0;
+    }
+
+}
