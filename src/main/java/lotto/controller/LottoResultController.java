8ab8a058d2a9c44778f1e4ffedb226c07a549c5c
koo-sangwoo <luckytkddn@gmail.com>
2024-11-03 15:28:59 +0900

    fix : 비즈니스 로직 모델로 이동, 생성자에 우선 필요기능 정리
    - 기존 등수 리스트 및 결과 Map을 구하는 공식들을 resultChecker 클래스로 옮김

--- src/main/java/lotto/controller/LottoResultController.java
+++ src/main/java/lotto/controller/LottoResultController.java
@@ -3,69 +3,35 @@ package lotto.controller;
 import lotto.LottoWinningRank;
 import lotto.model.Lotto;
 import lotto.model.LottoResultChecker;
+import lotto.model.PrizeNumbers;
+import lotto.view.LottoResultView;
+import lotto.view.PrizeNumbersView;
 
 import java.lang.reflect.Type;
 import java.util.*;
 
 public class LottoResultController {
     private LottoResultChecker resultChecker;
-    private Map<LottoWinningRank, Integer> lottoResultMap;
-    private String profitMargin;
+    private PrizeNumbers prizeNumbers;
+    private PrizeNumbersView prizeNumbersView = new PrizeNumbersView();
+    private LottoResultView resultView = new LottoResultView();
+
     // 발급 로또 리스트, 당첨 번호, 보너스 번호, 구매 금액
-    public LottoResultController(List<Lotto> purchaseLottoList, List<Integer> prizeNumbers, Integer bonusNumber,Integer buyPrice) {
-        this.resultChecker = new LottoResultChecker(purchaseLottoList, prizeNumbers, bonusNumber);
-        setLottoResult(getPurchaseEnumList());
-        setProfitMargin(buyPrice);
+    public LottoResultController(List<Lotto> purchaseLottoList, Integer buyPrice) {
+
+        this.prizeNumbers = new PrizeNumbers(prizeNumbersView.getPrizeNumbers(), prizeNumbersView.getBonusNumber());
+
+        List<Integer> prizeNumbers = this.prizeNumbers.getPrizeNumberList();
+        Integer bonusNumber = this.prizeNumbers.getBonusPrizeNumber();
+
+        this.resultChecker = new LottoResultChecker(purchaseLottoList, prizeNumbers, bonusNumber,buyPrice);
+        resultView.outputLottoResult(resultChecker.getLottoResultMap());
+        resultView.outputProfitMargin(resultChecker.getProfitMargin());
     }
 
-    public List<LottoWinningRank> getPurchaseEnumList() {
-        var resultPrizeCountList = resultChecker.getPrizeResult();
-        var resultIsBonusCountList = resultChecker.getPrizeBonusNumberList();
-        List<LottoWinningRank> resultList = new ArrayList<>();
 
-        for (int i = 0; i < resultPrizeCountList.size(); i++) {
-            resultList.add(checkRankEnum(resultPrizeCountList.get(i),resultIsBonusCountList.get(i)));
-        }
 
-        return resultList;
-    }
 
-    private LottoWinningRank checkRankEnum(Integer prizeCount, Boolean isPrizeBonus) {
-        var rankMap = createPrizeRank();
-        String key = prizeCount + ":" + isPrizeBonus;
-        return rankMap.getOrDefault(key, LottoWinningRank.FAIL);
-    }
 
-    private void setLottoResult(List<LottoWinningRank> purchaseEnumList) {
-        Map<LottoWinningRank, Integer> resultMap = new HashMap<LottoWinningRank, Integer>();
 
-        for (LottoWinningRank resultType : LottoWinningRank.values()) {
-            int frequency = Collections.frequency(purchaseEnumList, resultType);
-            resultMap.put(resultType, frequency);
-        }
-
-        this.lottoResultMap = resultMap;
-    }
-
-    // if문을 여러개 사용할 수 없어 Map으로 선언 후 getKey를 사용하여 리턴함
-    private Map<String, LottoWinningRank> createPrizeRank() {
-        Map<String, LottoWinningRank> rankMap = new HashMap<>();
-
-        rankMap.put("6:false", LottoWinningRank.FIRST);
-        rankMap.put("5:true", LottoWinningRank.SECOND);
-        rankMap.put("5:false", LottoWinningRank.THIRD);
-        rankMap.put("4:false", LottoWinningRank.FOURTH);
-        rankMap.put("3:false", LottoWinningRank.FIFTH);
-
-        return rankMap;
-    }
-
-    private void setProfitMargin(Integer buyPrice) {
-        Integer allPrice = 0;
-        for (Map.Entry<LottoWinningRank, Integer> prizeEntry : lottoResultMap.entrySet()) {
-            allPrice += prizeEntry.getKey().getPrizePrice() * prizeEntry.getValue();
-        }
-
-        this.profitMargin = ((allPrice / buyPrice) * 100) + "%";
-    }
 }
