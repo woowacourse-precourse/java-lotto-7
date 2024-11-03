205b45317efb830cd76bea087caa1ac08b27ea54
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-03 22:00:06 +0900

    fear(Application): 구매금액 입력받기 기능 추가
    사용자로부터 구매 금액을 입력받아 저장하는 기능을 Apllication에 추가.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 구매 금액 입력
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = inputPurchaseAmount();
+
+
+    }
+
+    private static int inputPurchaseAmount() {
+        return Integer.parseInt(Console.readLine());
     }
 }
