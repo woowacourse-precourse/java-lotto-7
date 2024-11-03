9a4bb4bb1b33412fe1e9965893018463c39e458a
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-03 23:53:15 +0900

    refactor: LottoNumberGenerator 클래스 모델로 패키지로 이동

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import lotto.utils.LottoNumberGenerator;
 
 public class LottoMachine {
 
--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -1,4 +1,4 @@
-package lotto.utils;
+package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
