edb7f4ec61543c64c71b2051540e93cbae89b550
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 04:03:02 +0900

    feat(OutputView): 사용자 입력 프롬프트 및 통계 헤더 추가
    - 구입금액 및 당첨 번호 입력 프롬프트 추가
    - 당첨 통계 출력 시 헤더 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,10 +7,18 @@ import lotto.model.LottoPrize;
 
 public class OutputView {
 
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    private static final String BONUS_REPORT_FORMAT = "5개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
     private static final String PURCHASED_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
     private static final String REPORT_FORMAT = "%d개 일치 (%,d원) - %d개\n";
-    private static final String BONUS_REPORT_FORMAT = "5개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
     private static final String YIELD_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계\n---";
+
+    public void printPurchaseAmountPrompt() {
+        System.out.println(PURCHASE_AMOUNT_PROMPT);
+    }
 
     public void printErrorMessage(String message) {
         System.out.println(message);
@@ -26,7 +34,17 @@ public class OutputView {
         }
     }
 
+    public void printWinningNumbersPrompt() {
+        System.out.println(WINNING_NUMBERS_PROMPT);
+    }
+
+    public void printBonusNumberPrompt() {
+        System.out.println(BONUS_NUMBER_PROMPT);
+    }
+
     public void printReport(Map<LottoPrize, Integer> matchCountMap) {
+        System.out.println(WINNING_STATISTICS_HEADER);
+
         for (LottoPrize prize : LottoPrize.values()) {
             if (prize == LottoPrize.MATCH_COUNT_5_WITH_BONUS) {
                 System.out.printf(BONUS_REPORT_FORMAT, prize.getPrize(), matchCountMap.get(prize));
