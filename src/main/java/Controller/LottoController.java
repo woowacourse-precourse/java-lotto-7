40340a6162137d444f54885c57694b106b9c8283
kyunghoon01 <sports0124@naver.com>
2024-11-03 21:28:11 +0900

    feat: 입력 받는 함수를 호출해 구매 가능한 개수 구하는 기능 구현

---
+++ src/main/java/Controller/LottoController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import Service.LottoService;
+import View.LottoView;
+import lotto.PurchaseCount;
+
+public class LottoController {
+    private final LottoView lottoView;
+    private final LottoService lottoService;
+
+    public LottoController(LottoView lottoView, LottoService lottoService) {
+        this.lottoView = lottoView;
+        this.lottoService = lottoService;
+    }
+
+    private PurchaseCount getPurchasedLottoCount() {
+        String purchaseAmount = lottoView.inputPurchaseAmount();
+        return lottoService.getCount(purchaseAmount);
+    }
+}
