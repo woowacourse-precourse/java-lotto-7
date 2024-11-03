5848d4efb8946b73400dd4e78b757b3cc26e7409
wogus <ind07152@naver.com>
2024-11-03 16:45:40 +0900

    feat(OutputView): 로또 최종 결과 출력 기능 추가
    로또 최종 결과를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,12 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
 public final class OutputView {
 
@@ -17,5 +21,27 @@ public final class OutputView {
             System.out.println("]");
         }
         System.out.println();
+    }
+
+    public static void printResult(Map<LottoResult, Integer> result, int money) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        List<LottoResult> list = Arrays.stream(LottoResult.values())
+                .filter(lottoResult -> !lottoResult.equals(LottoResult.NONE))
+                .toList();
+        double sum = 0;
+        for (LottoResult lottoResult : list) {
+            int matchedCount = lottoResult.getMatchedCount();
+            int rewardAmount = lottoResult.getRewardAmount();
+            int winningCount = result.getOrDefault(lottoResult, 0);
+            sum += winningCount * rewardAmount;
+            System.out.print(matchedCount + "개 일치");
+            if (lottoResult.isBonusMatched()) {
+                System.out.print(", 보너스 볼 일치");
+            }
+            System.out.printf(" (%,d원) - %d개", rewardAmount, winningCount);
+            System.out.println();
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.", (double) sum * 100 / money);
     }
 }
