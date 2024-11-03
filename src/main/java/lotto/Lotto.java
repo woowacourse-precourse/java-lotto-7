6ffe64f83cfe13983a2b5fa77ef4efde5bb3cf75
CHISANW <keuye06380618@gmail.com>
2024-11-03 17:02:39 +0900

    feat(Lotto): Lotto 추가 메서드 구현
    - getSortNumbers(): 구매한 로또 번호를 오름차순으로 정렬하여 조회할 수 있도록 구현
    - createNumber(): 1~45 사이의 중복되지 않은 숫자 6개를 생성하여 로또 번호를 반환하도록 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import static lotto.eunm.LottoConstants.*;
 import static lotto.vaildate.ErrorMessage.INVALID_NUMBER_COUNT;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.vaildate.ErrorMessage;
 
@@ -25,6 +28,21 @@ public class Lotto {
         if (numbers.stream().distinct().count() != LOTTO_NUMBERS_COUNT.value) {
             throw new IllegalArgumentException(ErrorMessage.UNIQUE_NUMBER);
         }
+    }
+
+    public List<Integer> getSortNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>(this.numbers);
+        Collections.sort(numbers);
+        return numbers;
+    }
+
+    public static Lotto createNumber() {
+        return new Lotto(createRandomNumber());
+    }
+
+    private static List<Integer> createRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_NUMBER.value, MAX_LOTTO_NUMBER.value,
+                LOTTO_NUMBERS_COUNT.value);
     }
 
 }
