a461c27950d04f5a7ffc0346672a68d3ab6bbbda
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 03:15:21 +0900

    fix(LottoNumGenerator): generateNum 메소드의 중첩 리스트 반환 문제 수정
    - generateNum 메소드에서 Randoms.pickUniqueNumbersInRange(1, 45, 6)을 리스트에 추가하여 중첩 리스트가 반환되는 문제를 수정하였습니다.
    - Randoms.pickUniqueNumbersInRange의 반환 값을 직접 numbers 변수에 할당하고 정렬하도록 변경하였습니다.

--- src/main/java/lotto/LottoNumGenerator.java
+++ src/main/java/lotto/LottoNumGenerator.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public class LottoNumGenerator {
     public List<Integer> generateNum(){
-        List numbers = new ArrayList<Integer>();
-        numbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        List numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         numbers.sort(null);
         return numbers;
     }
