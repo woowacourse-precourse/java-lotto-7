51a2a030cd877bc772574c4617d1c5448e42f93b
yybmion <yyb400@naver.com>
2024-11-03 19:06:00 +0900

    refactor: 로또 오름차순 로직 수정

--- src/main/java/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/RandomLottoGenerator.java
@@ -7,6 +7,9 @@ import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 
 public class RandomLottoGenerator implements LottoNumberGenerator {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
     private static final int LOTTO_PRICE = 1000;
 
     @Override
@@ -22,6 +25,9 @@ public class RandomLottoGenerator implements LottoNumberGenerator {
     }
 
     private List<Integer> createNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE)
+                .stream()
+                .sorted()
+                .toList();
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,17 +9,12 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
-        ascendingOrder();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
-
-    private void ascendingOrder() {
-        Collections.sort(numbers);
     }
 
     public List<Integer> getNumbers() {
