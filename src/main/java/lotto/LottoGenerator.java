6a2384cbd88b6205dffcb43428f85696551a8224
MinseokGo <rhalstjr1999@naver.com>
2024-11-03 18:42:47 +0900

    refactor: 로또 생성 책임 분리

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,13 +1,22 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public final class LottoGenerator {
 
     private LottoGenerator() {
     }
 
-    public static Lotto generate() {
+    public static List<Lotto> createLottos(final int lottoAmount) {
+        System.out.println(lottoAmount + "개를 구매했습니다.");
+        return IntStream.range(0, lottoAmount)
+                .mapToObj(lotto -> LottoGenerator.generate())
+                .toList();
+    }
+
+    private static Lotto generate() {
         return new Lotto(
                 Randoms.pickUniqueNumbersInRange(1, 45, 6)
         );
