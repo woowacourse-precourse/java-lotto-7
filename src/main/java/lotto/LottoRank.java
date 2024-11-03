42cbbb913f369650546fd9b4b969867bea8d9f91
johyewon <johyewon4601@gmail.com>
2024-11-04 03:27:12 +0900

    feat(LottoRank): 로또 순위 지정 enum 클래스 추가

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum LottoRank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoRank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
