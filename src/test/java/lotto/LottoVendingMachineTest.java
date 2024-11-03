8730ba207346048b2c69e6ffeca68cd6a775d6fc
taehyun <xogus5070318@gmail.com>
2024-11-03 17:04:50 +0900

    test(LottoVendingMachineTest): 사용자 입력 테스트 메소드 설계
    로또 구입금액 입력기능 관련하여 테스트 메소드를 설계하였습니다.
    사용자가 로또 자판기에 돈을 지불할 때, 2가지를 테스트해야한다고 생각합니다.
    첫번째 이유는 사용자가 돈을 잘못 지불하였을때, 적절히 예외처리를 해주는지 확인하는 것이며,
    두번째 이유는 사용자가 지불한 값 만큼의 로또가 생성되는지 확인하는 것입니다.
    
    BREAKING CHANGE: None

---
+++ src/test/java/lotto/LottoVendingMachineTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoVendingMachineTest {
+
+    @Test
+    void 로또_구입_금액의_범위는_1000원_이상_50000원_이하이다() {
+
+
+
+    }
+
+    @Test
+    void 로또_구입_금액은_1000원으로_나누어_떨어져야_한다() {
+
+
+
+    }
+
+    @Test
+    void 로또_구입_금액은_숫자로_입력받아야_한다(){
+
+
+
+    }
+
+    @Test
+    void 사용자가_구입한_금액_1000원_당_하나의_복권이_생성되어야_한다(){
+
+
+    }
+
+}
