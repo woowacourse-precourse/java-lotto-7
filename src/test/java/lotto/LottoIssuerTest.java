cbe5457d51885901b54a0bbf17c6c4a9b35492bc
hongkikii <hyangmeeh@icloud.com>
2024-11-03 19:30:21 +0900

    test(LottoIssuerTest): 테스트 수정 및 추가
    리드미 기능 목록에 맞춰 테스트 시나리오 수정 및 테스트 케이스 추가

--- src/test/java/lotto/LottoIssuerTest.java
+++ src/test/java/lotto/LottoIssuerTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 
 class LottoIssuerTest {
 
-    @DisplayName("1,000원당 로또 한 장을 발행한다.")
+    @DisplayName("1000원당 로또 1장을 발행한다.")
     @Test
-    void 천원당_로또_한_장을_발행한다() {
-        int price = 1000;
+    void 천원당_로또_한장을_발행한다() {
+        Price price = Price.from("1000");
         LottoIssuer issuer = new LottoIssuer(price);
         assertEquals(issuer.getLottoCount(), 1);
     }
