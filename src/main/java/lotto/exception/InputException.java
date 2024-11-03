cba2cf65054e1abfb19fe667c75e2d07060acd35
yujin-zero <poly30@naver.com>
2024-11-03 23:39:34 +0900

    refactor(InputException) : 입력 관련 예외 처리를 위한 InputException 클래스 추가

---
+++ src/main/java/lotto/exception/InputException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class InputException extends IllegalArgumentException {
+    public InputException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import java.util.List;
+import lotto.exception.InputException;
 
 public class InputValidator {
     public static int validatePurchaseAmount(String purchaseAmount) {
@@ -8,30 +9,30 @@ public class InputValidator {
         try {
             purchaseAmountInt = Integer.parseInt(purchaseAmount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 정수여야 합니다.");
+            throw new InputException("[ERROR] 구입금액은 정수여야 합니다.");
         }
         if (purchaseAmountInt <= 0 || purchaseAmountInt % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위여야 합니다.");
+            throw new InputException("[ERROR] 구입금액은 1,000원 단위여야 합니다.");
         }
         return purchaseAmountInt;
     }
 
     public static List<Integer> validateWinningNumbers(List<Integer> winningNumbers) {
         if (winningNumbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자여야 합니다.");
+            throw new InputException("[ERROR] 당첨 번호는 6개의 숫자여야 합니다.");
         }
         if (winningNumbers.stream().anyMatch(num -> num < 1 || num > 45)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new InputException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
         return winningNumbers;
     }
 
     public static int validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new InputException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+            throw new InputException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
         }
         return bonusNumber;
     }
