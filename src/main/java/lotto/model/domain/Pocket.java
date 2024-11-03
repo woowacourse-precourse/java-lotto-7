c30728df4236b24a8195d1101dfc6f5056d0e16c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-04 00:36:57 +0900

    feat(domain): money 필드 추가

--- src/main/java/lotto/model/domain/Pocket.java
+++ src/main/java/lotto/model/domain/Pocket.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 public class Pocket {
     List<Lotto> lottos;
+    int money;
 
-    public Pocket(List<Lotto> lottos) {
+    public Pocket(List<Lotto> lottos,int money) {
         this.lottos = lottos;
+        this.money = money;
     }
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+    public int getMoney() {
+        return money;
     }
 }
