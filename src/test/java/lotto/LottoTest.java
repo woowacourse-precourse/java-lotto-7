4ea590dbafe79eddb8592bed234f668f78d04c77
문혜교 <3hilney@gmail.com>
2024-11-03 14:46:27 +0900

    test: 에러 메세지 확인 기능 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,22 +12,28 @@ class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INVALID_LOTTO_NUMBER_COUNT.getErrorMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.DUPLICATE_LOTTO_NUMBER.getErrorMessage());
     }
 
     @DisplayName("로또 번호가 1에서 45 사이를 벗어나면 예외가 발생한다.")
     @Test
     void 로또_번호가_1에서_45_사이를_벗어나면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 47, 3, 23, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.OUT_OF_RANGE_LOTTO_NUMBER.getErrorMessage());
+
     }
+
+
 
 
 
