2b56bd69b31d9522d4765c12324968efcd498bad
kimchanho97 <nh0903@pusan.ac.kr>
2024-11-04 06:21:43 +0900

    feat(NumberGenerator): 로또 번호 생성기 인터페이스 구현

---
+++ src/main/java/lotto/model/FixedNumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import java.util.List;
+
+public class FixedNumberGenerator implements NumberGenerator {
+
+    private final List<Integer> numbers;
+
+    public FixedNumberGenerator(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public List<Integer> generateNumbersInRange() {
+        return numbers;
+    }
+}
