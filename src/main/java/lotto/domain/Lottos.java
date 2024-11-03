92582533e31f4cf649e3ce50f2773a02b93a1f89
heehun <slrkqk2@naver.com>
2024-11-03 16:17:15 +0900

    feat(Lottos): getter 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -7,10 +8,10 @@ import lotto.global.LottoRank;
 
 public class Lottos {
 
-    private List<Lotto> lottos;
+    private final List<Lotto> lottos;
 
-    public Lottos(List<Lotto> lotto) {
-        this.lottos = lotto;
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
     }
 
     public List<LottoRank> getWinningLottos(List<Integer> winningNumbers, int bonusNum) {
@@ -18,5 +19,9 @@ public class Lottos {
                 .map(lotto -> lotto.checkWinningStatus(winningNumbers, bonusNum))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
     }
 }
