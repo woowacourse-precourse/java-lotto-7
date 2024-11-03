c5e7a8c358718f470d5895891d82d622c2055a27
tentenball <lth8905@gmail.com>
2024-11-04 00:01:45 +0900

    refactor, feat: 수익률 출력 format 작성 및 당첨내역 출력 format 리팩토링

--- src/main/java/lotto/view/OutputInterface.java
+++ src/main/java/lotto/view/OutputInterface.java
@@ -8,31 +8,34 @@ public enum OutputInterface {
 	PURCHASE_AMOUNT("개를 구매했습니다."),
 
 	WINNING_STATISTICS_INFORMATION("당첨 통계\n---"),
-	WINNING_STATISTICS("""
-		3개 일치 (5,000원) - %d개
-		4개 일치 (50,000원) - %d개
-		5개 일치 (1,500,000원) - %d개
-		5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
-		6개 일치 (2,000,000,000원) - %d개
-		""");
+	WINNING_STATISTICS(String.join("\n",
+		"3개 일치 (5,000원) - %d개",
+		"4개 일치 (50,000원) - %d개",
+		"5개 일치 (1,500,000원) - %d개",
+		"5개 일치, 보너스 볼 일치 (30,000,000원) - %d개",
+		"6개 일치 (2,000,000,000원) - %d개"
+	)),
+	PROFIT_INFORMATION("총 수익률은 %.1f%%입니다.");
 	private final String message;
 
 	OutputInterface(String message) {
 		this.message = message;
 	}
 
-	@Override
-	public String toString() {
-		return message;
-	}
-
 	public static void printMessage(OutputInterface message) {
 		System.out.println(message);
 	}
+
 	public static void printMessage(String message) {
 		System.out.println(message);
 	}
-	public static void printNewLine(){
+
+	public static void printNewLine() {
 		System.out.println();
+	}
+
+	@Override
+	public String toString() {
+		return message;
 	}
 }
