9d0d14a2fb1483cb2a9499f025aa40c708c95150
chyo1 <hyowon3856@gmail.com>
2024-11-04 03:09:29 +0900

    feat : 당첨 통계 출력 기능 추가

--- src/main/java/lotto/IOController.java
+++ src/main/java/lotto/IOController.java
@@ -1,6 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class IOController {
     private static String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
@@ -8,6 +11,7 @@ public class IOController {
     private static String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     private static String OUTPUT_PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static String OUTPUT_WINNING_STATISTICS_MESSAGE = "당첨 통계\n---";
 
 
     public String inputPurchaseAmount() {
@@ -53,4 +57,15 @@ public class IOController {
     public void printWinningStatistics(double statistic) {
         System.out.println("총 수익률은 " + statistic + "%입니다.");
     }
+
+    public void printWinningResults(Map<Rank, Integer> winningStatistics) {
+        System.out.println(OUTPUT_WINNING_STATISTICS_MESSAGE);
+        winningStatistics.entrySet().stream()
+            .filter(entry -> entry.getKey() != Rank.ETC)
+            .sorted(Comparator.comparingInt(entry -> entry.getKey().ordinal()))
+            .forEach(entry -> {
+                System.out.println(entry.getKey().getPrintMessage() + " - " + entry.getValue() + "개");
+        });
+    }
 }
+
