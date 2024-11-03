3d5d929acf2791bb3d2b0da66e40605cd2bde0f6
KSJ0128 <ksj01128@naver.com>
2024-11-04 00:57:27 +0900

    feat: InputPurchaseMoney()를 통한 입력 로직
    InputPurchaseMoney()를 통해 유저 로또 구매 금액을 입력 받고, purchaseMoney에 담아둡니다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,14 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+
 public class LottoController {
+    public void play() {
+        input();
+    }
+
+    public void input() {
+        InputView inputView = new InputView();
+        String purchaseMoney = inputView.InputPurchaseMoney();
+    }
 }
