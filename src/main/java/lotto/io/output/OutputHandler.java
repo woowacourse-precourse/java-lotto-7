2a53c9fecfb667cb25cb6c815b5d8f388474ed75
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-04 02:07:06 +0900

    feat: 구매한 로또 목록 출력 기능 구현

--- src/main/java/lotto/io/output/OutputHandler.java
+++ src/main/java/lotto/io/output/OutputHandler.java
@@ -1,10 +1,43 @@
 package lotto.io.output;
 
+import java.util.List;
+import java.util.StringJoiner;
+
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
 public class OutputHandler {
 
-	public static final String PURCHASE_AMOUNT_COMMENT = "구입금액을 입력해 주세요.";
+	private static final String LOTTO_SIGN_DELIMITER = ", ";
+	private static final String LOTTO_SIGN_PREFIX = "[";
+	private static final String LOTTO_SIGN_SUFFIX = "]";
+	private static final String PURCHASE_AMOUNT_COMMENT = "구입금액을 입력해 주세요.";
+	private static final String PURCHASED_LOTTO_COUNT_COMMENT = "개를 구매했습니다.";
 
 	public void showPurchaseComment() {
 		System.out.println(PURCHASE_AMOUNT_COMMENT);
 	}
+
+	public void showPurchasedLottos(Lottos lottos) {
+		List<Lotto> lottosToShow = lottos.getLottos();
+		System.out.println(lottosToShow.size() + PURCHASED_LOTTO_COUNT_COMMENT);
+
+		for (Lotto lotto : lottosToShow) {
+			String lottoSign = makeLottoSign(lotto);
+			System.out.println(lottoSign);
+		}
+	}
+
+	private String makeLottoSign(Lotto lotto) {
+		StringJoiner stringJoiner = new StringJoiner(LOTTO_SIGN_DELIMITER, LOTTO_SIGN_PREFIX, LOTTO_SIGN_SUFFIX);
+
+		List<Integer> lottoNumbers = lotto.getNumbers();
+
+		for (Integer lottoNumber : lottoNumbers) {
+			stringJoiner.add(String.valueOf(lottoNumber));
+		}
+
+		return stringJoiner.toString();
+	}
+
 }
