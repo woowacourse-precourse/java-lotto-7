ea3d4b5262c4e0a8d4db91adb62b18c6c4bd2f7e
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:53:38 +0900

    feat(ErrorMessage): 일급 컬렉션 Lotto의 출력 메시지 상수화

--- src/main/java/lotto/Util/Error/ErrorMessage.java
+++ src/main/java/lotto/Util/Error/ErrorMessage.java
@@ -3,6 +3,7 @@ package lotto.Util.Error;
 public enum ErrorMessage {
     INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
     INVALID_WINNING_NUMBER_COUNT("[ERROR] 로또의 당첨 번호는 6개 입니다."),
     INVALID_BONUS_NUMBER_COUNT("[ERROR] 로또의 보너스 번호는 1개 입니다."),
     INVALID_PURCHASE_UNIT("[ERROR] 로또 구입 금액은 1,000원 단위 정수로 입력해야 합니다."),
