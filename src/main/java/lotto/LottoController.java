43a2011176f6fe2d392b43d6a9ec5fd3e71d579a
westzeroright <kt8267@naver.com>
2024-11-04 00:38:48 +0900

    feat: 사용자에게 몇 개의 로또를 구매했는지 알려줌

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import static lotto.Consumer.enterPurchaseAmount;
+import static lotto.Seller.getHowManyLottoMessage;
 import static lotto.Seller.setInputPurchaseAmount;
 
 public class LottoController {
     public void start() {
         setInputPurchaseAmount();
         int purchaseAmount = enterPurchaseAmount();
+        System.out.println(getHowManyLottoMessage(purchaseAmount));
     }
 }
--- src/main/java/lotto/Seller.java
+++ src/main/java/lotto/Seller.java
@@ -2,6 +2,7 @@ package lotto;
 
 public class Seller {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요";
+    private static final String HOW_MANY_LOTTO_TEMPLATE = "%d개를 구매했습니다.";
 
     public static void setInputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
@@ -10,5 +11,9 @@ public class Seller {
     public static int countNumberOfLotto(int purchaseAmount) {
         int countNumberOfLotto = purchaseAmount / 1000;
         return countNumberOfLotto;
+    }
+
+    public static String getHowManyLottoMessage(int numberOfLotto) {
+        return String.format(HOW_MANY_LOTTO_TEMPLATE, numberOfLotto);
     }
 }
