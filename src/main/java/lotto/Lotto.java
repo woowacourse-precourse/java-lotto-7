58fb1b2258b9b69a87baba3db94749b360739bc4
bee9827 <ehfrhfo9494@naver.com>
2024-11-03 22:24:13 +0900

    feat: Validate lotto numbers
    중복 체크
    값의 범위 체크
    번호 개수 체크

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
+    private static final int NUMBER_SIZE = 6;
+    private static final int NUMBER_MIN = 1;
+    private static final int NUMBER_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,34 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 중복 될 수 없습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < NUMBER_MIN || number > NUMBER_MAX) {
+                throw new IllegalArgumentException(String.format("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", NUMBER_MIN, NUMBER_MAX));
+            }
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_SIZE) {
+            throw new IllegalArgumentException(String.format("로또 번호는 %d개여야 합니다.", NUMBER_SIZE));
+        }
+    }
+
+    //  > 크기, 값의 범위, 중복 여부
+    //- [ ] 번호를 정렬한다.
+    //- [ ] 우승 로또와 맞는 개수를 전달한다.
+    //- [ ] 보너스볼을 체크한다.
+    //- [ ] 로또 번호를 전달한다.
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,12 +2,17 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -18,6 +23,14 @@ class LottoTest {
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_범위_예외(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
