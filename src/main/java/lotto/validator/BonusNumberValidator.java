d5a7db1bd68ae82bc5c9e1a3ac0e7854d276cc65
smyeongL <sangmyeonglee123@gmail.com>
2024-11-04 04:18:52 +0900

    fix(validator): add validator class for bonus number input

---
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -0,0 +1,39 @@
+package lotto.validator;
+
+import lotto.util.ErrorMessage;
+import lotto.util.LottoNumber;
+
+public class BonusNumberValidator {
+    public static boolean validateBlank(String bonusNUmber) {
+        try {
+            if (bonusNUmber.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        } catch (IllegalArgumentException err) {
+            System.out.println(ErrorMessage.EMPTY_BONUS_NUMBER_STRING_ERROR);
+            return false;
+        }
+    }
+    public static boolean validateDataType(String bonusNUmber) {
+        try {
+            Integer.parseInt(bonusNUmber);
+            return true;
+        } catch (NumberFormatException err) {
+            System.out.println(ErrorMessage.INVALID_BONUS_NUMBER_FORMAT_ERROR);
+            return false;
+        }
+    }
+    public static boolean validateRange(String bonusNUmber) {
+        try {
+            int parsedNumber = Integer.parseInt(bonusNUmber);
+            if (parsedNumber < LottoNumber.MIN_LOTTO_NUMBER || parsedNumber > LottoNumber.MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        } catch (IllegalArgumentException err) {
+            System.out.println(ErrorMessage.INVALID_BONUS_NUMBER_RANGE_ERROR);
+            return false;
+        }
+    }
+}
