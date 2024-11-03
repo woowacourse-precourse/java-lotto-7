28e128f5fa47193fe7640175a43ea16cf4eef60a
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 03:08:51 +0900

    test: 정수 범위가 아닐 경우 예외 테스트 추가

--- src/test/java/lotto/utils/parser/WinningNumbersParserTest.java
+++ src/test/java/lotto/utils/parser/WinningNumbersParserTest.java
@@ -1,7 +1,8 @@
 package lotto.utils.parser;
 
+import static lotto.constants.ErrorMessage.WINNING_NUMBER_IS_ONLY_INTEGER_ALLOWED;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,16 +12,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersParserTest {
+
     @DisplayName("사용자 입력을 올바르게 파싱하여 Lotto 객체로 반환하는지 확인한다.")
     @Test
     void Given_UserWinningNumbersString_When_GetWinningNumbers_Then_ReturnLottoObjectWithCorrectNumbers() {
         // Given
-        String userWinningNumbers = "1, 2, 3, 4, 5, 6";
+        String userWinningNumbers = "1,2,3,4,5,6";
         List<Integer> expectedNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         // When
         Lotto lotto = WinningNumbersParser.getWinningNumbers(userWinningNumbers);
         LottoNumbers lottoNumbers = lotto.getLottoNumbers();
         // Then
         assertThat(lottoNumbers.getLottoNumbers()).isEqualTo(expectedNumbers);
+    }
+
+    @DisplayName("정수 범위가 아닌 숫자를 입력한 경우, 예외가 발생한다.")
+    @Test
+    void Given_UserWinningNumbersString_When_GetWinningNumbers_Then_Error() {
+        String userWinningNumbers = "1,2147483648,3,4,5,6";
+
+        assertThatThrownBy(() -> WinningNumbersParser.getWinningNumbers(userWinningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(WINNING_NUMBER_IS_ONLY_INTEGER_ALLOWED.getMessage());
     }
 }
