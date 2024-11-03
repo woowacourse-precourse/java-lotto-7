01df1756a63f61fbf1ec2e31b39620076892b1f4
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-03 23:41:41 +0900

    feat(Application) : 1.2 입력된 구입 금액을 정수로 변환할 수 없는 경우 예외 처리하는 기능
    - 입력된 문자열을 정수로 변환할 수 없으면 IllegalArgumentException 발생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,5 +9,21 @@ public class Application {
         System.out.println("구입 금액을 입력해 주세요.");
         String purchaseAmountInput = Console.readLine();
 
+        try {
+            // 1.2 입력된 구입 금액을 정수로 변환할 수 없는 경우 예외 처리하는 기능
+            validateNumericString(purchaseAmountInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    private static void validateNumericString(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자로 변환할 수 없는 문자열입니다.");
+        }
     }
 }
