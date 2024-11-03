4e1532132c27b7d428f0a468563dd7d315251b64
sodychoe <sodychoe@icloud.com>
2024-11-04 01:33:40 +0900

    feat(BonusNumberInputConsoleHandler) : 보너스 번호 입력받기 기능 구현

--- src/main/java/lotto/io/BonusNumberInputConsoleHandler.java
+++ src/main/java/lotto/io/BonusNumberInputConsoleHandler.java
@@ -1,7 +1,15 @@
 package lotto.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BonusNumberInputConsoleHandler {
     public void showBonusNumberGuideMessage() {
         System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public int askBonusNumber() {
+        String rawBonusNumber = Console.readLine();
+
+        return Integer.parseInt(rawBonusNumber);
     }
 }
