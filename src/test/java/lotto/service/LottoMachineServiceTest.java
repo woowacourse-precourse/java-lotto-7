0da891d46407e28bb54223b61a0205e2c60e146c
jijongkwon <help3451@naver.com>
2024-11-03 17:56:08 +0900

    test(service): 로또 구입 금액 입력받는 테스트 코드 구현

--- src/test/java/lotto/service/LottoMachineServiceTest.java
+++ src/test/java/lotto/service/LottoMachineServiceTest.java
@@ -1,23 +1,31 @@
 package lotto.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lotto.domain.LottoMachine;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineServiceTest {
 
+    private LottoMachine lottoMachine;
     private LottoMachineService lottoMachineService;
 
     @BeforeEach
     public void setup() {
+        lottoMachine = LottoMachine.getInstance();
         lottoMachineService = new LottoMachineService();
     }
 
     @Test
-    void inputLottoPurchaseAmount() {
+    void 로또_구입금액_입력받는다() {
         // given
+        String purchaseAmount = "1000";
 
         // when
+        lottoMachineService.inputLottoPurchaseAmount(purchaseAmount);
 
         // then
+        assertEquals(1000, lottoMachine.getPurchaseAmount());
     }
 }
