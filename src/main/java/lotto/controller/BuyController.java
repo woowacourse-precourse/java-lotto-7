cf3b351fedd295f2af4764849a65671823d84f27
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-03 23:53:28 +0900

    refactor: 클래스 이름 변경

--- src/main/java/lotto/controller/BuyController.java
+++ src/main/java/lotto/controller/BuyController.java
@@ -4,18 +4,18 @@ import lotto.service.LottoMachine;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-public class LottoController {
+public class BuyController {
     private final InputView inputView;
     private final LottoMachine lottoMachine;
     private final OutputView outputView;
 
-    public LottoController(final InputView inputView, final LottoMachine lottoMachine, OutputView outputView) {
+    public BuyController(final InputView inputView, final LottoMachine lottoMachine, OutputView outputView) {
         this.inputView = inputView;
         this.lottoMachine = lottoMachine;
         this.outputView = outputView;
     }
 
-    public void run() {
+    public void buy() {
         Long money = inputView.inputMoney();
         lottoMachine.buyLotto(money);
         outputView.printPurchaseLotto(lottoMachine.getPurchasedLotto());
