d1f161b68056b80872663c2aa8d11aff61d00c14
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-04 01:58:58 +0900

    feat(lotto/input): 잘못된 입력인 경우 다시 입력을 받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,47 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.Amount;
+import lotto.model.Lotto;
+import lotto.model.LottoNumber;
 
 public class InputView {
     public static String readLottoPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String input = Console.readLine();
+                Amount.from(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static String readWinningLotto() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                Lotto.from(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static String readWinningBonus() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                LottoNumber.from(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
