ea86d277cba6bd82e7dd2624ae4d8da53d8f6cfd
KNU-K <knukang334@gmail.com>
2024-11-03 16:31:19 +0900

    feat(LottoInput): 사용자 입력 기능 추가
    - 구매 금액 및 당첨 번호 입력을 위한 메서드 구현
    - getBuyAmount 메서드: 사용자로부터 구매 금액을 입력받음
    - getWinningNumbers 메서드: 당첨 번호와 보너스 번호를 입력받음
    - 유효성 검사를 통해 잘못된 입력을 처리하고 최대 5회까지 재입력 기회를 제공
    - 잘못된 입력에 대해 예외 처리 및 오류 메시지 출력
    
    이번 업데이트로 로또 애플리케이션에서 사용자가 구매 금액 및 당첨 번호를 쉽게 입력할 수 있는 기능이 추가되었습니다.

---
+++ src/main/java/lotto/view/LottoInput.java
@@ -0,0 +1,70 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.ErrorMessages;
+import lotto.exception.LottoException;
+import lotto.exception.MaxRetryExceededException;
+import lotto.factory.WinningNumberPicker;
+import lotto.model.Money;
+import lotto.model.WinningNumbers;
+
+public class LottoInput {
+    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MAIN_NUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    private static final String NUMBER_REGEX = "^(?:[1-9]|[1-3][0-9]|4[0-5])(,(?:[1-9]|[1-3][0-9]|4[0-5])){5}$";
+
+    public Money getBuyAmount() {
+        return retryOnError(() -> {
+            System.out.println(INPUT_AMOUNT_MESSAGE);
+            return Money.of(Integer.parseInt(Console.readLine()));
+        });
+    }
+
+    public WinningNumbers getWinningNumbers() {
+        return retryOnError(() -> {
+            String mainNumbers = getValidatedMainNumber();
+            int bonusNumber = getValidatedBonusNumber();
+            return WinningNumberPicker.createWinningNumbers(mainNumbers, bonusNumber);
+        });
+    }
+
+    private String getValidatedMainNumber() {
+        return retryOnError(() -> {
+            System.out.println(INPUT_MAIN_NUMBER_MESSAGE);
+            String mainNumber = Console.readLine();
+            if (!mainNumber.matches(NUMBER_REGEX)) {
+                throw new LottoException(ErrorMessages.INVALID_FORMAT);
+            }
+            return mainNumber;
+        });
+    }
+
+    private int getValidatedBonusNumber() {
+        return retryOnError(() -> {
+            System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+            return Integer.parseInt(Console.readLine());
+        });
+    }
+
+    private <T> T retryOnError(SupplierWithException<T> supplier) {
+        final int maxRetries = 5;
+        int attempts = 0;
+
+        while (attempts < maxRetries) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                attempts++;
+            }
+        }
+
+        throw new MaxRetryExceededException();
+    }
+
+    @FunctionalInterface
+    private interface SupplierWithException<T> {
+        T get() throws IllegalArgumentException;
+    }
+}
