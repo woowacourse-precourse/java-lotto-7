5c51e24027a249713dcb656fa33d6be20b5e4c71
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-03 14:38:49 +0900

    feat: InputView에 당첨 번호 중복 검증 로직 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,10 +1,13 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 6개여야 합니다.";
-    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호와 보너스 번호는 중복되지 않아야 합니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 중복될 수 없습니다.";
+    private static final String BONUS_DUPLICATE_ERROR_MESSAGE = "[ERROR] 당첨 번호와 보너스 번호는 중복되지 않아야 합니다.";
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
@@ -17,7 +20,8 @@ public class WinningNumbers {
 
     private void validate(List<Integer> winningNumbers, int bonusNumber) {
         validateSize(winningNumbers);
-        validateNoDuplicates(winningNumbers, bonusNumber);
+        validateNoDuplicateNumbers(winningNumbers);
+        validateNoBonusDuplicate(winningNumbers, bonusNumber);
     }
 
     private void validateSize(List<Integer> winningNumbers) {
@@ -26,9 +30,16 @@ public class WinningNumbers {
         }
     }
 
-    private void validateNoDuplicates(List<Integer> winningNumbers, int bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
+    private void validateNoDuplicateNumbers(List<Integer> winningNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningNumbers);
+        if (uniqueNumbers.size() != winningNumbers.size()) {
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateNoBonusDuplicate(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
