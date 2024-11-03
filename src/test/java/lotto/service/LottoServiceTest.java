0f682db189d23f3e3b95fe14f0ee9227696b128e
lchyeong <lchyeong@gmail.com>
2024-11-03 23:36:27 +0900

    test(LottoServiceTest): 테스트 독립성을 위해 당첨카운트 초기화 메서드 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,8 +1,11 @@
 package lotto.service;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.PrizeTable;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,24 +13,33 @@ import org.junit.jupiter.api.Test;
 class LottoServiceTest {
     private final LottoService lottoService = new LottoService();
 
-    List<Lotto> lottos = List.of(
-            new Lotto(Arrays.asList(8, 21, 23, 41, 42, 43)),
-            new Lotto(Arrays.asList(7, 11, 16, 35, 36, 44)),
-            new Lotto(Arrays.asList(1, 8, 11, 31, 41, 42)),
-            new Lotto(Arrays.asList(3, 5, 11, 16, 32, 38)),
-            new Lotto(Arrays.asList(13, 14, 16, 38, 42, 45)),
-            new Lotto(Arrays.asList(7, 11, 30, 40, 42, 43)),
-            new Lotto(Arrays.asList(2, 13, 22, 32, 38, 45)),
-            new Lotto(Arrays.asList(1, 3, 5, 14, 22, 45))
-    );
-    List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-    int bonusNumber = 7;
+    @DisplayName("당첨 카운트 초기화")
+    @AfterEach
+    void resetWinningCounts() throws Exception {
+        for (PrizeTable prizeTable : PrizeTable.values()) {
+            Field field = prizeTable.getClass().getDeclaredField("winningCount");
+            field.setAccessible(true);
+            field.set(prizeTable, 0);
+        }
+    }
 
     @DisplayName("총 상금을 계산하여 반환")
     @Test
     void getTotalPrizeMoney() {
-
+        List<Lotto> lottos = List.of(
+                new Lotto(Arrays.asList(8, 21, 23, 41, 42, 43)),
+                new Lotto(Arrays.asList(7, 11, 16, 35, 36, 44)),
+                new Lotto(Arrays.asList(1, 8, 11, 31, 41, 42)),
+                new Lotto(Arrays.asList(3, 5, 11, 16, 32, 38)),
+                new Lotto(Arrays.asList(13, 14, 16, 38, 42, 45)),
+                new Lotto(Arrays.asList(7, 11, 30, 40, 42, 43)),
+                new Lotto(Arrays.asList(2, 13, 22, 32, 38, 45)),
+                new Lotto(Arrays.asList(1, 3, 5, 14, 22, 45))
+        );
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
         int expect = 5000;
+
         int result = lottoService.getTotalPrizeMoney(lottos, winningNumbers, bonusNumber);
 
         Assertions.assertEquals(expect, result);
