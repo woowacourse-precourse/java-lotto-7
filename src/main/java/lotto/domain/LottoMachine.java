814b86ae960cfe2472d2e1252238868cb15b16c1
jijongkwon <help3451@naver.com>
2024-11-03 18:12:05 +0900

    feat(domain): 보너스 번호 저장 및 반환 메서드 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,6 +3,7 @@ package lotto.domain;
 public class LottoMachine {
     private static final LottoMachine instance = new LottoMachine();
     private int purchaseAmount;
+    private int bonusNumber;
 
     private LottoMachine() {
 
@@ -14,6 +15,14 @@ public class LottoMachine {
 
     public void savePurchaseAmount(int purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
+    }
+
+    public void saveBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 
     public int getPurchaseAmount() {
