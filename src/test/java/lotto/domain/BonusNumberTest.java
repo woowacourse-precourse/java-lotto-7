68234e63d80ceb958fc318e94231374ef3aff3ab
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-03 19:00:24 +0900

    test(BonusNumberTest): 보너스 번호 도메인 테스트
    테스트한 내용은 다음과 같습니다:
    - 로또 번호에 보너스번호가 포함 되어 true를 반환 한다
    - 로또 번호에 보너스번호가 포함 되어 있지 않아 false를 반환 한다

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @DisplayName("로또 번호에 보너스번호가 포함 되어 true를 반환 한다.")
+    @Test
+    void isMatchNumberTest_true() {
+        //given
+        final BonusNumber bonusNumber = new BonusNumber(2);
+        final Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        final boolean matchNumber = bonusNumber.isMatchNumber(lotto);
+
+        //then
+        assertThat(matchNumber).isTrue();
+    }
+
+    @Test
+    @DisplayName("로또 번호에 보너스번호가 포함 되어 있지 않아 false를 반환 한다.")
+    void isMatchNumberTest_false() {
+        //given
+        final BonusNumber bonusNumber = new BonusNumber(10);
+        final Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        final boolean matchNumber = bonusNumber.isMatchNumber(lotto);
+
+        //then
+        assertThat(matchNumber).isFalse();
+    }
+}
