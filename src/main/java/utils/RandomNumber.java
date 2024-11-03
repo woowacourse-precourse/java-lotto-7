75aa03b01a7b1f454bd556beae4f55d9c4361a74
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-03 18:02:06 +0900

    refactor: 테스트 코드 통과를 위해, RandomNumber의 값을 새로운 배열에 값 복사

--- src/main/java/utils/RandomNumber.java
+++ src/main/java/utils/RandomNumber.java
@@ -2,6 +2,7 @@ package utils;
 
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,8 +16,7 @@ public class RandomNumber {
     }
 
     public static List<Integer> create() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER,
-                LOTTO_LENGTH);
+        List<Integer> randomNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_LENGTH));
         Collections.sort(randomNumbers);
         return randomNumbers;
     }
