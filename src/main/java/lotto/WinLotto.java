5e6482adf0acd575743328276d65c8d2809b3cb3
임채연 <dlacodus0407@gmail.com>
2024-11-03 20:45:10 +0900

    refactor(ExceptionMessages) : 예외명을 enum화 한다
    - 예외를 한곳에서 관리할 수 있도록, enum화 한다.

--- src/main/java/lotto/WinLotto.java
+++ src/main/java/lotto/WinLotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import static lotto.ExceptionMessages.ERROR_BONUS_NUMBER_SAME;
+import static lotto.ExceptionMessages.ERROR_LOTTO_NUMBER_RANGE;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -17,14 +20,14 @@ public class WinLotto {
     public void validate(Lotto lotto, Integer bonus) {
         for (Integer number : lotto.getNumbers()) {
             if (Objects.equals(number, bonus)) {
-                throw new IllegalArgumentException("[ERROR] 보너스 번호와 로또 번호가 같습니다.");
+                throw new IllegalArgumentException(ERROR_BONUS_NUMBER_SAME.getMessage());
             }
         }
     }
 
     public void validate(Integer bonus) {
         if (bonus < 1 || bonus > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 1~45 사이입니다.");
+            throw new IllegalArgumentException(ERROR_LOTTO_NUMBER_RANGE.getMessage());
         }
     }
 
