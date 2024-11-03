6857fa8f372f0b9ac025176ba798ea76ff0bc7f7
lchyeong <lchyeong@gmail.com>
2024-11-03 18:17:59 +0900

    refactor(NumberParser): 로또 번호 파싱 기능 분리를 위한 클래스 추가

---
+++ src/main/java/lotto/validator/NumberParser.java
@@ -0,0 +1,28 @@
+package lotto.validator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberParser {
+
+    public static final String delimiter = ",";
+
+    public static List<Integer> toNumbers(String input) {
+        List<String> numbers = splitByDelimiter(input);
+        List<Integer> toNumbers = new ArrayList<>();
+
+        for (String number : numbers) {
+            try {
+                toNumbers.add(Integer.parseInt(number));
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("[ERROR] 숫자를 입력해 주세요.");
+            }
+        }
+        return toNumbers;
+    }
+
+    private static List<String> splitByDelimiter(String winningNumbers) {
+        return Arrays.asList(winningNumbers.split(delimiter));
+    }
+}
