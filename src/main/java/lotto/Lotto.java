3459c85053774152fd7584df07bb42168595667a
YongLeeCode <yonghwan311@gmail.com>
2024-11-03 20:49:49 +0900

    feat: 로또 클래스 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,50 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import validator.BonusValidator;
+import validator.PaymentValidator;
+import validator.PrizeNumberValidator;
+
 public class Lotto {
     private final PrizeNumber prizeNumber;
+    private final Integer pay;
+    private final Integer bonusNumber;
 
-    public Lotto(PrizeNumber prizeNumber) {
-        validate(prizeNumber);
-        this.prizeNumber = prizeNumber;
+    public Lotto(
+            List<Integer> inputPrizeNumber,
+            Integer pay,
+            Integer getBonus
+    ) {
+        PrizeNumberValidator.validate(inputPrizeNumber);
+        PaymentValidator.validate(pay);
+        BonusValidator.validate(getBonus, inputPrizeNumber);
+
+        this.prizeNumber = new PrizeNumber(inputPrizeNumber);
+        this.pay = pay;
+        this.bonusNumber = getBonus;
     }
 
-    private void validate(PrizeNumber prizeNumber) {
-        if (prizeNumber.getNumbers().size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    public void play() {
+        List<LottoTicket> lottoTicketBundle = getLottoTicketBundle();
+        TicketPrizeMatcher ticketPrizeMatcher = new TicketPrizeMatcher(
+                prizeNumber,
+                lottoTicketBundle,
+                bonusNumber
+        );
+        MatchResult matchResult = ticketPrizeMatcher.matchAll();
+        Output output = new Output();
+        output.printMatchResult(matchResult);
+    }
+
+    private List<LottoTicket> getLottoTicketBundle() {
+        LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator();
+        List<LottoTicket> lottoTicketBundle = new ArrayList<>();
+
+        for (int i = 0; i < AbsoluteValue.TICKET_COUNT(pay); i += 1) {
+            lottoTicketBundle.add(lottoTicketGenerator.generate());
         }
+        Output.outputTicketNumbers(lottoTicketBundle);
+        return lottoTicketBundle;
     }
-
-    // TODO: 추가 기능 구현
 }
