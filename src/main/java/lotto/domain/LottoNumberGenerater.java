f9b854e6f17e50f91fe00c137e1924aa066dd12c
MinJae00 <exceedmin@gmail.com>
2024-11-04 02:25:37 +0900

    refact: service 레이어에 비즈니스 로직이 종속되어 있는걸 방지하기 위해 티켓 장 수 만큼 랜덤 번호를 생성

--- src/main/java/lotto/domain/LottoNumberGenerater.java
+++ src/main/java/lotto/domain/LottoNumberGenerater.java
@@ -1,12 +1,26 @@
 package lotto.domain;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 public class LottoNumberGenerater {
-    List<Integer> lotto;
-    public List<Integer> numberGenerate(){
-        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    List<List<Integer>> lotto = new ArrayList<>();
+
+    public List<List<Integer>> generateLottoTickets(int numberOfLotto) {
+        for (int i = 0; i < numberOfLotto; i++) {
+            List<Integer> generatedNumbers = new ArrayList<>(numberGenerate());
+            Collections.sort(generatedNumbers);
+            lotto.add(generatedNumbers);
+        }
         return lotto;
     }
+
+    private List<Integer> numberGenerate() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
 }
 
 
