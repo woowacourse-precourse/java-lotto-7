1e742ddf0d753d18bfae58e85e011fe09280340b
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-03 21:32:42 +0900

    refactor: parser로직 분리

--- src/main/java/lotto/domain/WinningLottoNumber.java
+++ src/main/java/lotto/domain/WinningLottoNumber.java
@@ -1,12 +1,7 @@
 package lotto.domain;
 
-import static lotto.constants.ErrorMessage.PUT_VALID_BONUS_NUMBER;
-import static lotto.constants.ErrorMessage.PUT_VALID_LOTTO_NUMBERS;
-import static lotto.constants.LottoConstant.REGEX;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import lotto.utils.Parser;
 
 public class WinningLottoNumber {
 
@@ -15,31 +10,7 @@ public class WinningLottoNumber {
 
     public WinningLottoNumber(Lotto winningNumber, String bonusNumber){
         this.winningNumber = winningNumber;
-        this.bonusNumber = parseBonusNumber(bonusNumber, this.winningNumber);
-    }
-
-    public static Lotto parseWinningNumber(String winningNumber) {
-        List<String> list = new ArrayList<>(Arrays.asList(winningNumber.split(REGEX, -1)));
-        List<Integer> winningNumbers = new ArrayList<>();
-
-        for (String lottoNumber : list) {
-            try {
-                winningNumbers.add(Integer.parseInt(lottoNumber));
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException(PUT_VALID_LOTTO_NUMBERS.getMessage());
-            }
-        }
-        return new Lotto(winningNumbers);
-    }
-
-    public static BonusNumber parseBonusNumber(String bonusNumber, Lotto winningNumber) {
-        int bonus;
-        try {
-            bonus = Integer.parseInt(bonusNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(PUT_VALID_BONUS_NUMBER.getMessage());
-        }
-        return new BonusNumber(bonus, winningNumber);
+        this.bonusNumber = Parser.parseBonusNumber(bonusNumber, this.winningNumber);
     }
 
     public List<Integer> getWinningNumber() {
