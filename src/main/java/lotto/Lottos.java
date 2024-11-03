1461acfcf8ababa8dfa045ead203d2283ca50fcd
Leegeonwoo7 <workgeon07@naver.com>
2024-11-04 03:13:52 +0900

    feat(Lottos): Lottos 구현
    Lotto의 일급 컬렉션

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,2 +1,15 @@
-package lotto;public class Lottos {
+package lotto;
+
+import java.util.List;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public long getQuantity() {
+        return lottos.size();
+    }
 }
