aa7c8d59262a9479f5abfd5c5e1a886dfbc6f0e5
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-03 21:26:35 +0900

    refactor : inputView 리팩토링
    mvc구조를 잘못이해하고있어 이해한뒤 수정했음

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,28 +2,54 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.enums.ExceptionMessage.NOT_INT;
+
 public class InputView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public String readMoney(){
+    public int readMoney(){
         System.out.println(INPUT_MONEY);
         String money = Console.readLine();
-        return money;
+        validateInt(money);
+        return Integer.parseInt(money);
     }
 
-    public String readLottoNum(){
+    public List<Integer> readWinningNumber(){
         System.out.println();
         System.out.println(INPUT_LOTTO_NUMBER);
-        String lottoNum = Console.readLine();
-        return lottoNum;
+        String input = Console.readLine();
+
+        String[] winningNumberInput = input.split(",");
+        List<Integer> winningNumber = new ArrayList<>();
+
+        for (String winningNum : winningNumberInput) {
+            validateInt(winningNum);
+            winningNumber.add(Integer.parseInt(winningNum));
+        }
+        return winningNumber;
     }
 
-    public String readBonusNum() {
+    public int readBonusNum() {
         System.out.println();
         System.out.println(INPUT_BONUS_NUMBER);
+
         String bonusNum = Console.readLine();
-        return bonusNum;
+
+        validateInt(bonusNum);
+
+        return Integer.parseInt(bonusNum);
     }
+    private void validateInt(String input) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(input);
+        }catch (Exception e){
+            throw new IllegalArgumentException(NOT_INT.getMessage());
+        }
+    }
+
 }
