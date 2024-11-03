643eb385f3bce6c8616b608389aa9a52c25178aa
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-03 18:25:53 +0900

    test: 수익률 계산 테스트 임시 주석

--- src/test/java/lotto/system/formater/profit/ProfitRateCalculatorTest.java
+++ src/test/java/lotto/system/formater/profit/ProfitRateCalculatorTest.java
@@ -1,4 +1,25 @@
+package lotto.system.formater.profit;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class ProfitRateCalculatorTest {
-  
+
+//    @Test
+//    @DisplayName("수익률 계산 테스트")
+//    void calculate_ShouldCalculateProfitRate() {
+//        // given
+//        int totalPurchaseAmount = 10000;
+//        int totalPrizeMoney = 20000;
+//        int expected = 200;
+//
+//        // when
+//        int result = ProfitRateCalculator.calculate(totalPurchaseAmount, totalPurchaseAmount);
+//
+//        // then
+//        assertEquals(expected, result, "수익률 계산 결과가 예상과 일치하지 않습니다.");
+//    }
+
 }
