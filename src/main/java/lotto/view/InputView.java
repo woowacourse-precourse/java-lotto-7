276b40d228c6599b3cdcbbe9c83777e334b7e53d
Seungwan Yoo <ysw99@kakao.com>
2024-11-03 19:30:37 +0900

    refactor: 매개 변수 및 지역 변수 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,19 +14,19 @@ public class InputView {
         return validateAndParsePurchaseAmount(readLine());
     }
 
-    public Integer validateAndParsePurchaseAmount(String input) {
+    public Integer validateAndParsePurchaseAmount(final String input) {
         validateNumber(input);
-        int purchaseAmount = Integer.parseInt(input);
+        final int purchaseAmount = Integer.parseInt(input);
         validateAmount(purchaseAmount);
         return purchaseAmount;
     }
 
-    public void validateNumber(String input) {
+    public void validateNumber(final String input) {
         if (!input.matches(NUMBER_PATTERN)) // 정수로 이루어진 문자열인지 검증 [0-9]+
             throw new IllegalArgumentException(LottoException.NUMBER_FORMAT_NOT_VALID.getMessage());
     }
 
-    public void validateAmount(Integer purchaseAmount) {
+    public void validateAmount(final Integer purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) // 1,000원 단위로 나누어 떨어지지 않을 경우
             throw new IllegalArgumentException(LottoException.PURCHASE_AMOUNT_NOT_VALID.getMessage());
     }
