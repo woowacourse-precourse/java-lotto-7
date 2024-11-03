84fd1d027b4d94c2484f789cf1492dad3a9f0189
sanikani <sanikani@naver.com>
2024-11-03 18:08:11 +0900

    refactor: LottoController 메서드 분리
    - 하나의 역할로 메서드를 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import java.util.List;
 import lotto.model.BonusNumber;
 import lotto.model.Lotto;
+import lotto.model.LottoGenerator;
 import lotto.model.PurchaseAmount;
 import lotto.model.WinningLotto;
 import lotto.util.Parser;
@@ -20,11 +21,35 @@ public class LottoController {
     }
 
     public void run() {
+        PurchaseAmount purchaseAmount = getPurchaseAmount();
+
+        List<Lotto> lottos = purchaseLottos(purchaseAmount);
+        printPurchaseLottos(lottos);
+
+        WinningLotto winningLotto = getWinningLotto();
+    }
+
+    private PurchaseAmount getPurchaseAmount() {
         PurchaseAmount purchaseAmount = new PurchaseAmount(inputView.requestPurchaseAmount());
         outputView.printPurchaseAmount(purchaseAmount.getLottoQuantity());
+        return purchaseAmount;
+    }
 
-        List<Integer> winningNumbers = Parser.parseStringToList(inputView.requestWinningNumbers());
-        int bonusNumber = Parser.parseStringToInt(inputView.requestBonusNumber());
-        WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), new BonusNumber(bonusNumber));
+    private List<Lotto> purchaseLottos(PurchaseAmount purchaseAmount) {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        return lottoGenerator.generateLottos(purchaseAmount.getLottoQuantity());
+    }
+
+    private void printPurchaseLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            outputView.printLottoNumbers(lotto.getNumbers());
+        }
+        outputView.printEnter();
+    }
+
+    private WinningLotto getWinningLotto() {
+        Lotto lotto = new Lotto(Parser.parseStringToList(inputView.requestWinningNumbers()));
+        BonusNumber bonusNumber = new BonusNumber(Parser.parseStringToInt(inputView.requestBonusNumber()));
+        return new WinningLotto(lotto, bonusNumber);
     }
 }
