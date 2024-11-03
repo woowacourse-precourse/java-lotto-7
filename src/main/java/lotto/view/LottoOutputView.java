ef10a76fff29032c457fea82fb1e7a56918f4b63
Lkk7380 <dlrlrhkd@naver.com>
2024-11-04 01:23:23 +0900

    feat: 로또 번호 출력 기능 구현

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -1,7 +1,13 @@
 package lotto.view;
 
+import java.util.List;
+
 public class LottoOutputView {
     public String getLottoCountMessage(int count) {
         return count + "개를 구매했습니다.";
+    }
+
+    public String getLottoNumbersMessage(List<Integer> numbers) {
+        return numbers.toString();
     }
 }
--- src/test/java/lotto/view/LottoOutputViewTest.java
+++ src/test/java/lotto/view/LottoOutputViewTest.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoOutputViewTest {
@@ -11,5 +12,13 @@ class LottoOutputViewTest {
     void printLottoCount() {
         LottoOutputView outputView = new LottoOutputView();
         assertThat(outputView.getLottoCountMessage(3)).isEqualTo("3개를 구매했습니다.");
+    }
+
+    @DisplayName("로또 번호를 출력한다")
+    @Test
+    void printLottoNumbers() {
+        LottoOutputView outputView = new LottoOutputView();
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThat(outputView.getLottoNumbersMessage(numbers)).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 }
