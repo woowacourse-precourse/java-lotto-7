9cfd0b7207f716084e379acb30928526afcb7d7b
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 19:18:32 +0900

    :necktie: feat : Lotto 클래스 수정
    - validate 메서드 1 ~ 45 사이의 수인지 검증하는 로직 추가
    - getter 추가
    - 당첨 번호와 생성한 번호의 일치 갯수 리턴하는 함수 추가
    - 보너스 번호가 로또 번호에 존재하는지 리턴하는 함수 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream()
+                .anyMatch(number -> number < 1 || number > 45)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers(){
+        return this.numbers;
+    }
+
+    public int countMatchedNumbers(Lotto other){
+        return (int) numbers.stream()
+                .filter(other.getNumbers()::contains)
+                .count();
+    }
+
+    public boolean containsNumber(int bonusNumber){
+        return numbers.contains(bonusNumber);
+    }
+
+}
