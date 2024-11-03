a7a834edea2c7e87460735710e890591ef8afca6
Chan531 <chan657312@gmail.com>
2024-11-03 20:45:12 +0900

    feat(Winning): 생성자 선언

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -5,4 +5,10 @@ public enum Winning {
 	private final int count;
 	private final int prize;
 	private final boolean hasBonusNumber;
+
+	Winning(int count, int prize, boolean hasBonusNumber) {
+		this.count = count;
+		this.prize = prize;
+		this.hasBonusNumber = hasBonusNumber;
+	}
 }
