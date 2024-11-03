5a5b6ca18b607fc6b933751569d54a1293dd323c
ppparkta <pppppark2@gmail.com>
2024-11-03 15:55:27 +0900

    refactor(LottoProfitCalculator): 수익률 테스트 코드 분리

--- src/test/java/lotto/domain/LottoProfitCalculatorTest.java
+++ src/test/java/lotto/domain/LottoProfitCalculatorTest.java
@@ -18,7 +18,8 @@ public class LottoProfitCalculatorTest {
 
     @DisplayName("로또가 일치하는 갯수를 반환한다.")
     @Test
-    void shouldReturnCorrectMatchCountForLottos() {
+    void 로또가_일치하는_갯수_반환() {
+        // given
         IssuedRandomLotto issuedLotto = createIssuedLotto(3000, List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 7)),       // 2등
                 new Lotto(List.of(1, 2, 3, 4, 8, 9)),       // 4등
@@ -26,24 +27,42 @@ public class LottoProfitCalculatorTest {
         ));
 
         LottoProfitCalculator calculator = new LottoProfitCalculator(lottoResult, issuedLotto);
+
+        // when
         List<LottoRank> lottoRanks = calculator.calculateLottoStatistics();
 
+        // then
         Assertions.assertThat(lottoRanks.size()).isEqualTo(2);
         validateLottoRank(lottoRanks.get(0), LottoRank.SECOND);
         validateLottoRank(lottoRanks.get(1), LottoRank.FOURTH);
     }
 
-    @DisplayName("로또 수익률을 계산한다.")
+    @DisplayName("로또를 3개 구매해서 2등, 4등에 당첨됐을 때 수익률을 반환한다.")
     @Test
-    void shouldCalculateProfitRate() {
+    void 로또3개구매_2등_4등_당첨됐을때의_수익률_반환() {
+        // given
         IssuedRandomLotto issuedLotto = createIssuedLotto(3000, List.of(
                 new Lotto(List.of(1, 2, 3, 4, 5, 7)),       // 2등
                 new Lotto(List.of(1, 2, 3, 4, 8, 9)),       // 4등
                 new Lotto(List.of(8, 9, 10, 11, 12, 13))    // 꽝
         ));
+        LottoProfitCalculator calculator = new LottoProfitCalculator(lottoResult, issuedLotto);
+        calculator.calculateLottoStatistics();
 
-        IssuedRandomLotto issuedLotto2 = createIssuedLotto(8000, List.of(
-                new Lotto(List.of(1, 2, 3, 7, 8, 9)),       // 4등
+        // when
+        double rateOfProfit = calculator.calculateRateOfProfit();
+
+        // then
+        Assertions.assertThat(rateOfProfit).isEqualTo(1001666.7);
+    }
+
+
+    @DisplayName("로또를 8개 구매해서 5등에 당첨됐을 때 수익률을 반환한다.")
+    @Test
+    void 로또8개_구매해서_5등_한번_당첨됐을때의_수익률_반환() {
+        // given
+        IssuedRandomLotto issuedLotto = createIssuedLotto(8000, List.of(
+                new Lotto(List.of(1, 2, 3, 7, 8, 9)),       // 5등
                 new Lotto(List.of(8, 9, 10, 11, 12, 13)),   // 꽝
                 new Lotto(List.of(8, 9, 10, 11, 12, 13)),   // 꽝
                 new Lotto(List.of(8, 9, 10, 11, 12, 13)),   // 꽝
@@ -52,18 +71,14 @@ public class LottoProfitCalculatorTest {
                 new Lotto(List.of(8, 9, 10, 11, 12, 13)),   // 꽝
                 new Lotto(List.of(8, 9, 10, 11, 12, 13))    // 꽝
         ));
-
         LottoProfitCalculator calculator = new LottoProfitCalculator(lottoResult, issuedLotto);
-        LottoProfitCalculator calculator2 = new LottoProfitCalculator(lottoResult, issuedLotto2);
-
         calculator.calculateLottoStatistics();
-        calculator2.calculateLottoStatistics();
 
+        // when
         double rateOfProfit = calculator.calculateRateOfProfit();
-        double rateOfProfit2 = calculator2.calculateRateOfProfit();
 
-        Assertions.assertThat(rateOfProfit).isEqualTo(1001666.7);
-        Assertions.assertThat(rateOfProfit2).isEqualTo(62.5);
+        // then
+        Assertions.assertThat(rateOfProfit).isEqualTo(62.5);
     }
 
     private IssuedRandomLotto createIssuedLotto(int purchaseAmount, List<Lotto> lottos) {
