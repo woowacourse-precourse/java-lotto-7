34a275a69181c999e519949f756c1d1448392aff
sejinezy <psj8880@naver.com>
2024-11-04 01:12:52 +0900

    refactor(Input): 사용자 입력과 관련된 역할만 담당하도록 수정

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
@@ -9,76 +8,34 @@ public class Input {
     public int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = input();
-        validate(input);
-        return toInt(input);
+        Validator.notEmpty(input);
+        Validator.isNumber(input);
+        return Parser.toInt(input); // 수정
     }
 
     public List<Integer> getWinningNumbers() {
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String input = input();
-        validateNotEmpty(input);
+        Validator.notEmpty(input);
         String[] splitNumbers = split(input);
-        validateIsNumber(splitNumbers);
-        return toInts(splitNumbers);
+        Validator.isNumber(splitNumbers);
+        return Parser.toInts(splitNumbers);
     }
 
     public int getBonusNumber() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
         String input = input();
-        validate(input);
-        return toInt(input);
-    }
-
-    public void validate(String input) {
-        validateNotEmpty(input);
-        validateIsNumber(input);
+        Validator.notEmpty(input);
+        Validator.isNumber(input);
+        return Parser.toInt(input);
     }
 
     private String input() {
         return Console.readLine();
     }
 
-    private void validateNotEmpty(String input) {
-        if (isEmpty(input)) {
-            throw new IllegalArgumentException("[ERROR] 입력값이 비어 있습니다.");
-        }
-    }
-
-    private boolean isEmpty(String input) {
-        return input == null || input.trim().isEmpty();
-    }
-
     private String[] split(String input) {
         return input.split(",");
     }
 
-    private void validateIsNumber(String input) {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
-        }
-    }
-
-    private void validateIsNumber(String[] strings) {
-        try {
-            for (String string : strings) {
-                validateIsNumber(string);
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
-        }
-    }
-
-    private List<Integer> toInts(String[] strings) {
-        List<Integer> integers = new ArrayList<>();
-        for (String string : strings) {
-            integers.add(Integer.parseInt(string));
-        }
-        return integers;
-    }
-
-    private int toInt(String input) {
-        return Integer.parseInt(input);
-    }
 }
