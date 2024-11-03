6ae0fd6bf1050990218d890c4b3bc5649c86ec0f
김연진 <love_homebody@naver.com>
2024-11-03 17:16:19 +0900

    feat(BonusNumber): 1~45 범위의 숫자가 아닌 경우 예외 발생

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,11 +1,22 @@
 package lotto.domain;
 
+import lotto.constant.ExceptionMessage;
+import lotto.constant.LottoConstant;
+
 public class BonusNumber {
 
     private final int value;
 
     public BonusNumber(int value) {
+        validateValue(value);
         this.value = value;
+    }
+
+    private void validateValue(int value) {
+        if (!(LottoConstant.MIN_NUMBER.getValue() <= value && value <= LottoConstant.MAX_NUMBER.getValue())) {
+            ExceptionMessage message = ExceptionMessage.INVALID_RANGE;
+            throw new IllegalArgumentException(message.getMessage());
+        }
     }
 
     public boolean isSame(int number) {
