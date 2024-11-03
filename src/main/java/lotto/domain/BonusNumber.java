fb742e0c9f0cb210014965d30792677d6863e0d8
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 00:56:01 +0900

    feat(BonusNumber): 보너스 번호 예외 처리

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -2,13 +2,24 @@ package lotto.domain;
 
 public class BonusNumber {
 
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final int number;
 
     public BonusNumber(int number) {
+        validate(number);
         this.number = number;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            String errorMessage = String.format("[ERROR] 보너스 번호는 %d 이상, %d 이하 정수여야 합니다.", MIN_NUMBER, MAX_NUMBER);
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
