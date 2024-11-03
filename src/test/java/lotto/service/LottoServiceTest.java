31032cfe3818239f760563a7fa3d13bb11c18b4a
Hanjjong <tim4974@naver.com>
2024-11-03 23:57:11 +0900

    test: LottoResult 생성 확인 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,10 +1,14 @@
 package lotto.service;
 
+import lotto.enums.LottoRank;
+import lotto.model.LottoResult;
 import lotto.model.LottoTicket;
 import lotto.model.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,7 +33,7 @@ public class LottoServiceTest {
     @Test
     @DisplayName("당첨 번호 생성 및 보너스 번호 추가 테스트")
     void createWinningLottoAndAddBonusTest() {
-        String winningNumber = "1, 2, 3, 4, 5, 6";
+        String winningNumber = "1,2,3,4,5,6";
         String bonusNumber = "7";
 
         WinningLotto winningLotto = lottoService.createWinningLotto(winningNumber);
@@ -37,5 +41,19 @@ public class LottoServiceTest {
 
         assertThat(winningLotto.getNumbers()).containsExactlyInAnyOrder(1, 2, 3, 4, 5, 6);
         assertThat(winningLotto.getBonusNum()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("로또 결과 생성 테스트")
+    void compareLottoTest() {
+        String purchaseMoney = "100000";
+        LottoTicket lottoTicket = lottoService.createLottoTicket(purchaseMoney);
+        WinningLotto winningLotto = lottoService.createWinningLotto("1,2,3,4,5,6");
+        lottoService.addBonusNumber(winningLotto, "7");
+
+        LottoResult lottoResult = lottoService.compareLotto(lottoTicket, winningLotto);
+
+        Map<LottoRank, Integer> rankResults = lottoResult.getRankResults();
+        assertThat(rankResults).isNotNull();
     }
 }
