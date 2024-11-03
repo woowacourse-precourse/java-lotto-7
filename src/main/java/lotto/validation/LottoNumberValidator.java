6406ad9a4cbc815838575ede74cfb622fe892b3c
변우영 <likevangogh24@gmail.com>
2024-11-03 14:42:30 +0900

    feat(LottoNumberValidator): 로또 숫자 중복 예외 처리 추가

--- src/main/java/lotto/validation/LottoNumberValidator.java
+++ src/main/java/lotto/validation/LottoNumberValidator.java
@@ -2,6 +2,7 @@ package lotto.validation;
 
 import lotto.enums.Message;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class LottoNumberValidator {
@@ -10,11 +11,20 @@ public class LottoNumberValidator {
 
     public static void validate(List<Integer> numbers) {
         validateSize(numbers);
+        validateDuplication(numbers);
     }
 
     private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(Message.ERROR_PREFIX.getMessage() + "로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static void validateDuplication(List<Integer> numbers) {
+
+        int deduplicatedNumbersCount = new HashSet<>(numbers).size();
+        if (deduplicatedNumbersCount != numbers.size()) {
+            throw new IllegalArgumentException(Message.ERROR_PREFIX.getMessage() + "로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 }
