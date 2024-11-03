59026e8290055e22e7eb2754030e1afb4bda7d4f
bellmin <whdlas011203@naver.com>
2024-11-04 01:53:58 +0900

    feat(LottoInputParser): 보너스 숫자 파싱, 유효성 검증 로직 추가
    - 보너스 숫자를 파싱하고, 로또 번호 범위에 맞는지 유효성 검증

--- src/main/java/lotto/application/support/LottoInputParser.java
+++ src/main/java/lotto/application/support/LottoInputParser.java
@@ -1,5 +1,6 @@
 package lotto.application.support;
 
+import static lotto.common.Consts.LOTTO_NUMBER_MUST_BE_BETWEEN_ONE_FORTY_FIVE_ERROR;
 import static lotto.common.Consts.WINNING_NUMBER_NOT_DUPLICATED_ERROR;
 import static lotto.common.Consts.INPUT_MUST_BE_NUMBER_ERROR;
 import static lotto.common.Consts.INPUT_MUST_BE_POSITIVE_NUMBER_ERROR;
@@ -58,6 +59,26 @@ public class LottoInputParser {
     private void validateWinningNumber(List<Integer> winningNumbers){
         if(winningNumbers.stream().distinct().count() != winningNumbers.size()){
             Errors.IllegalArgumentException(WINNING_NUMBER_NOT_DUPLICATED_ERROR);
+        }
+    }
+
+    public int parseBonusNumber(String bonusNumber){
+        int parsed;
+        try{
+            parsed = Integer.parseInt(bonusNumber);
+
+            validateBonusNumber(parsed);
+
+            return parsed;
+        } catch (NumberFormatException e) {
+            Errors.IllegalArgumentException(INPUT_MUST_BE_NUMBER_ERROR);
+        }
+        return -1;
+    }
+
+    private void validateBonusNumber(int bonusNumber){
+        if(bonusNumber < 0 || bonusNumber > 45){
+            Errors.IllegalArgumentException(LOTTO_NUMBER_MUST_BE_BETWEEN_ONE_FORTY_FIVE_ERROR);
         }
     }
 }
