54b38c346307b7d4e336b3215e5f3ff42b8f8b0e
chaeyeon0130 <k84292330@gmail.com>
2024-11-03 17:22:04 +0900

    feat(Lotto): 로또 번호에 중복된 번호가 포함된 경우 예외 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,18 +1,29 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> duplicates = new HashSet<>(numbers);
+
+        if (duplicates.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있으면 안 됩니다.");
         }
     }
 
