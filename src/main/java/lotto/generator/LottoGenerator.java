0f2783641a1a54fd0e271da265b9847a84822bde
JeongSH1 <jyt001532@gmail.com>
2024-11-03 18:17:28 +0900

    feat(LottoGenerator): 구입한 개수만큼 로또를 생성한다
    LottoGenerator의 변경점:
    - generate 함수를 overload 하여 매개변수로 개수가 주어지면 해당 개수 만큼 로또 반환

--- src/main/java/lotto/generator/LottoGenerator.java
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -5,6 +5,7 @@ import static lotto.constant.LottoNumberConstant.MIN_NUMBER;
 import static lotto.constant.LottoNumberConstant.REQUIRED_COUNT;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
@@ -13,5 +14,14 @@ public class LottoGenerator {
     public Lotto generate() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, REQUIRED_COUNT);
         return new Lotto(lottoNumbers);
+    }
+
+    public List<Lotto> generate(int numberOfPurchases) {
+        List<Lotto> lottoes = new ArrayList<>();
+        for (int i = 0; i < numberOfPurchases; i++) {
+            lottoes.add(generate());
+        }
+
+        return lottoes;
     }
 }
