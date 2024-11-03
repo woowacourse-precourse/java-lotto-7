ace71370a162d3d560eae4ff60c9aa6ba31731cc
jinhyeon <tiemo0708@naver.com>
2024-11-03 20:52:31 +0900

    feat: 로또 구매 금액, 당첨 및 보너스번호를 입력 받는 기능 구현

---
+++ src/main/java/lotto/presentation/view/InputView.java
@@ -0,0 +1,25 @@
+package lotto.presentation.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요 (쉼표로 구분).";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+
+    public String inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_PROMPT);
+        return Console.readLine().trim();
+    }
+
+    public String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_PROMPT);
+        return Console.readLine().trim();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER_PROMPT);
+        return Console.readLine().trim();
+    }
+}
