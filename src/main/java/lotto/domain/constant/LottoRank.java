3e73787ac5def3febe8fa47c6fe97506210ffa93
rootTiket <l20078829@gmail.com>
2024-11-03 21:00:42 +0900

    refactor(LottoRank): 사용하지 않는 메서드 삭제

--- src/main/java/lotto/domain/constant/LottoRank.java
+++ src/main/java/lotto/domain/constant/LottoRank.java
@@ -39,14 +39,6 @@ public enum LottoRank {
         return NONE;
     }
 
-    public int getMatchCount() {
-        return matchCount;
-    }
-
-    public boolean isMatchBonus() {
-        return matchBonus;
-    }
-
     public int getPrize() {
         return prize;
     }
