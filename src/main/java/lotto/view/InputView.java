8e7d98a28ab2dbfa2be510e463d3da1fb9eb2917
yushinc <cys74351@gmail.com>
2024-11-03 18:53:55 +0900

    feat(InputView, OutputView): 입출력 메서드 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String getPriceStr() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String priceStr = Console.readLine();
+        return priceStr;
+    }
+
+    public String getWinningNumStr() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumStr = Console.readLine();
+        return winningNumStr;
+    }
+
+    public String getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumStr = Console.readLine();
+        return bonusNumStr;
+    }
+}
