de412366f9442a81552c9785ecdb72105ad6d193
gahyeon <1000ji1@naver.com>
2024-11-03 15:04:38 +0900

    feat: 1000 단위로 입력했는지 확인

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -3,7 +3,8 @@ package lotto.utils;
 public class Validator {
     private static final int DIVIDED_AMOUNT = 1000;
     public static final String INVALID_NUMBER_MESSAGE = "[ERROR] 숫자 입력하셔야 합니다.";
-    
+    public static final String INVALID_AMOUNT_MESSAGE = "[ERROR] 1000원 단위로 입력하셔야 합니다.";
+
     private Validator() {
     }
 
@@ -12,6 +13,12 @@ public class Validator {
             Integer.parseInt(amount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
+    }
+
+    public static void amountIsMultipleOf1000(int amount) {
+        if (amount % DIVIDED_AMOUNT != 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
         }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,26 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.Validator;
 
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
 
-    public static String purchaseAmount() {
+    public static int purchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
-        return read();
+        String amount = read();
+        int parseAmount = parseAndValidateAmount(amount);
+        return parseAmount;
     }
 
-    public static String read() {
+    private static int parseAndValidateAmount(String amount) {
+        Validator.isNumber(amount);
+        int parseAmount = Integer.parseInt(amount);
+        Validator.amountIsMultipleOf1000(parseAmount);
+        return parseAmount;
+    }
+
+    private static String read() {
         return Console.readLine();
     }
 }
