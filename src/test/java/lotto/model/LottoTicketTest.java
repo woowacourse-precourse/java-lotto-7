bf26bd569a7c539f5f911a010a4166ca8818057d
Dev-LHY <dev.leehy@gmail.com>
2024-11-03 23:27:50 +0900

    refactor(lottoTicketTest): 구조 변경에 따른 변경사항 적용
    - LottoTicket의 purchase 기능을 LottoService로 분리하였기 때문에 해당 변경사항 적용하였음

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.*;
 
+import lotto.service.LottoService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,7 +11,8 @@ class LottoTicketTest {
     @ParameterizedTest
     @CsvSource({"1, 1", "100, 100", "123, 123"})
     void quantity_수량만큼_구매가_되어야한다(int quantity, int expected) {
-        LottoTicket testLottoTicket = LottoTicket.purchase(quantity);
+        LottoService lottoService = new LottoService();
+        LottoTicket testLottoTicket = lottoService.purchase(quantity);
         assertThat(testLottoTicket.getLottoTicket().size()).isEqualTo(expected);
     }
 }
