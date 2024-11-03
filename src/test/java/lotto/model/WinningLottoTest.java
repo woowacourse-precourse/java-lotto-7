ed1ee608e6951713eac7e6a18d5e231c87747951
kimchanho97 <nh0903@pusan.ac.kr>
2024-11-04 08:02:51 +0900

    feat(WinningLotto): "당첨 번호와 보너스 번호가 중복되지 않는다"

---
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복이면 예외를 던진다.")
+    void testDuplicateWinningLottoThrowsException() {
+        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6");
+        BonusNumber bonusNumber = new BonusNumber("1");
+
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
+
