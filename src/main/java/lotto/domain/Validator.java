27e1aa66456ae9e08478fa897b946de44da005b5
song hyeongyu <lit3207@ajou.ac.kr>
2024-11-04 01:09:09 +0900

    refactor: revise all  structure

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -69,4 +69,22 @@ public class Validator {
             throw new IllegalArgumentException(ERROR_MESSAGE + " 금액은 0 이상의 1000 단위여야 합니다.");
         }
     }
+
+    public static void validateBonusNumber(int bonusNumber, List<Integer> inputLottoNumbers) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 보너스 번호는 1부터 45 사이여야 합니다.");
+        }
+        if (inputLottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public static void validateUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(ERROR_MESSAGE + "당첨 번호에 중복된 숫자가 포함될 수 없습니다.");
+            }
+        }
+    }
 }
