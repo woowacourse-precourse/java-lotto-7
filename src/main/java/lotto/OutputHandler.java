7f373274b7b80cf2857b23a8803b3945030f7bb6
hongkikii <hyangmeeh@icloud.com>
2024-11-04 07:14:24 +0900

    refactor(OutputHandler): 출력을 위한 클래스 생성

--- src/main/java/lotto/OutputHandler.java
+++ src/main/java/lotto/OutputHandler.java
@@ -1,12 +1,12 @@
 package lotto;
 
-public class DisplayHandler {
+public class OutputHandler {
     private static final String PURCHASE_MESSAGE_FORMAT = "%d개를 구매했습니다.\n";
     private static final String WINNING_STATISTICS_HEADER = "당첨 통계\n---";
     private static final String RETURN_RATE_MESSAGE_FORMAT = "총 수익률은 %s입니다.";
 
     public void show(PurchasedLotto purchasedLotto) {
-        System.out.printf((PURCHASE_MESSAGE_FORMAT), purchasedLotto.getCount());
+        System.out.printf(PURCHASE_MESSAGE_FORMAT, purchasedLotto.getCount());
         System.out.println(purchasedLotto.getFormatted());
     }
 
