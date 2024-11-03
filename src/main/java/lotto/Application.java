f59e2eb15665f5637fd77bd32f80ccb3b1807f19
youjeans <dbyj2810@naver.com>
2024-11-03 23:11:13 +0900

    feat: 프로그램 작동 메인 함수 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.print("구매금액을 입력해 주세요: ");
+        int amount = LottoGeneration.inputPurchaseAmount();
+        int quantity = LottoGeneration.extractQuantity(amount);
+        List<Lotto> lottoSets = LottoGeneration.issueLotto(quantity);
+
+        LottoGeneration.printLotto(lottoSets, quantity);
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Lotto winningNumbers = LottoDraw.inputWinnigNumbers();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        List<Integer> bonusNumber = LottoDraw.inputBonusNumber(winningNumbers);
+
+        LottoResult.countFirstRank(lottoSets, winningNumbers);
+        LottoResult.countSecondRank(lottoSets, winningNumbers, bonusNumber);
+        LottoResult.countThirdRank(lottoSets, winningNumbers, bonusNumber);
+        LottoResult.countFourthRank(lottoSets, winningNumbers);
+        LottoResult.countFifthRank(lottoSets, winningNumbers);
+
+        LottoResult.printResult();
+        LottoResult.printRateOfReturn(amount);
     }
 }
