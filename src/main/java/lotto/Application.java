e171a10923f1469bad25d440d53f32c4c89832f9
CheChe903 <cheche903@naver.com>
2024-11-03 14:54:22 +0900

    feat(LottoController): 객체들의 움직임을 관리하는 Controller 구현
    Model인 domain, 그리고 view인 View의 중간 다리 역할을 하는 LottoController를 작성합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,13 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.input.ConsoleInputView;
+import lotto.view.output.ConsoleOutputView;
 
 public class Application {
     public static void main(String[] args) {
 
-        LottoController lottoController = new LottoController();
+        LottoController lottoController = new LottoController(new ConsoleInputView(), new ConsoleOutputView());
 
         lottoController.run();
     }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,5 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.EnumMap;
@@ -8,70 +7,69 @@ import java.util.List;
 import java.util.Map;
 import lotto.domain.generator.RandomIntegerListGenerator;
 import lotto.domain.lotto.Investment;
-import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoBundle;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.LottoResult;
 import lotto.domain.lotto.Rank;
 import lotto.domain.lotto.WinningNumbers;
 import lotto.domain.sorting.AscendingSorter;
+import lotto.view.input.InputView;
+import lotto.view.output.OutputView;
 
 public class LottoController {
 
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+
     public void run() {
 
-        System.out.println("구입 금액을 입력해 주세요.");
+        outputView.printPurchaseGuide();
 
-        int cost = Integer.parseInt(Console.readLine());
+        int cost = Integer.parseInt(inputView.readLine());
+
+        outputView.printNewLine();
 
         Investment investment = new Investment(BigInteger.valueOf(cost));
 
         int lottoQuantity = investment.getQuantity();
-        System.out.println();
-        System.out.println(lottoQuantity + "개를 구매했습니다.");
+
+        outputView.printPurchasedAmount(lottoQuantity);
 
         LottoBundle lottoBundle = new LottoBundle(new ArrayList<>(), new RandomIntegerListGenerator(),
                 new AscendingSorter());
 
         lottoBundle.generate(lottoQuantity);
 
-        for (Lotto lotto : lottoBundle.getLottoBundle()) {
-            System.out.print("[");
-            System.out.print(lotto);
-            System.out.println("]");
-        }
+        outputView.printLottoTicket(lottoBundle.getLottoBundle());
 
-        System.out.println();
+        outputView.printWinningNumbersGuide();
 
-        System.out.println("당첨번호를 입력해 주세요.");
-        String input = Console.readLine();
+        String input = inputView.readLine();
+        outputView.printNewLine();
         String[] splitInput = input.split(",");
         List<LottoNumber> preWinningNumbers = new ArrayList<>();
         for (String splitNumber : splitInput) {
             preWinningNumbers.add(new LottoNumber(Integer.parseInt(splitNumber)));
         }
 
-        System.out.println();
-
-        System.out.println("보너스 번호를 입력해 주세요.");
-        LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(Console.readLine()));
+        outputView.printBonusNumberGuide();
+        LottoNumber bonusNumber = new LottoNumber(Integer.parseInt(inputView.readLine()));
+        outputView.printNewLine();
         WinningNumbers winningNumbers = new WinningNumbers(preWinningNumbers, bonusNumber);
-        System.out.println();
 
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        outputView.printWinningStatistics();
         LottoResult lottoResult = new LottoResult(new EnumMap<>(Rank.class), BigInteger.ZERO);
 
         lottoResult.calculate(lottoBundle, winningNumbers);
         Map<Rank, Integer> rankCount = lottoResult.getRankCount();
+        outputView.printWinningResult(rankCount);
 
-        System.out.printf("3개 일치 (5,000원) - %d개%n", rankCount.get(Rank.FIFTH));
-        System.out.printf("4개 일치 (50,000원) - %d개%n", rankCount.get(Rank.FOURTH));
-        System.out.printf("5개 일치 (1,500,000원) - %d개%n", rankCount.get(Rank.THIRD));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", rankCount.get(Rank.SECOND));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", rankCount.get(Rank.FIRST));
-
-        System.out.printf("총 수익률은 %.1f%%입니다.", lottoResult.calculateReturnRate(investment));
-
+        outputView.printProfitRate(lottoResult.calculateReturnRate(investment));
     }
 }
