ae2ba28cb42245e2aa74c6410bde4cc610c7a7f4
Hun0914 <bkh010914@naver.com>
2024-11-03 10:41:16 +0900

    feat : lottostart Controller 구현

--- src/main/java/lotto/PrizeController.java
+++ src/main/java/lotto/PrizeController.java
@@ -1,4 +1,44 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.Validate;
+
 public class PrizeController {
+
+    private final InputView inputView = new InputView(new Validate());
+    private final PrizeService prizeService = new PrizeService();
+    private final OutputView outputView = new OutputView();
+
+    public void lottoStart(){
+        int lottoMoney=inputView.getMoneyInput()/1000;
+        int count=lottoMoney/1000;
+
+        List<Lotto> lotto=generateLotto(lottoMoney);
+        outputView.printLotto(lotto);
+
+        Map<LottoRate, Integer> prizeRepository = prizeService.servicePrize(lotto,
+                inputView.getLottoInput(),
+                inputView.getBonusInput());
+
+        double retunaValue=prizeService.calculateReturnValue(prizeRepository, lottoMoney);
+
+        outputView.producePrize(prizeRepository, retunaValue);
+    }
+
+
+    public List<Lotto> generateLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for(int i=0; i<count; i++) {
+            lottos.add(new LottoGenerator().getLottoGenerator());
+        }
+        return lottos;
+    }
+
+
 }
