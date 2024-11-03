3a103724750ec59a34636178cec710c311875b23
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:57:47 +0900

    feat: BonusNumber 클래스 구현

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,4 +1,47 @@
 package lotto.model;
 
 public class BonusNumber {
+
+    private final int bonusNumber;
+
+    private static final String ONLY_NUMBER = "[ERROR] 숫자만 입력해 주세요!";
+    private static final String NEGATIVE_NUMBER = "[ERROR] 양수를 입력해주세요!";
+    private static final String WRONG_RANGE = "[ERROR] 숫자 범위가 벗어났습니다.";
+    private static final String DUPLICATE_NUMBER = "[ERROR] 중복되는 숫자가 있습니다.";
+
+    public BonusNumber(String bonusNumber) {
+        this.bonusNumber = validateNumber(bonusNumber);
+    }
+
+    private int validateNumber(String bonusNumber) {
+        int uncheckedMoney = checkNumber(bonusNumber);
+        checkPositiveNumber(uncheckedMoney);
+        checkRange(uncheckedMoney);
+
+        return uncheckedMoney;
+    }
+
+    private int checkNumber(String bonusNumber) {
+        try {
+            return Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_NUMBER);
+        }
+    }
+
+    private void checkPositiveNumber(int bonusNumber) {
+        if (!(bonusNumber >= 0)) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER);
+        }
+    }
+
+    private void checkRange(int bonusNumber) {
+        if (!(bonusNumber > 0 && bonusNumber <= 45)) {
+            throw new IllegalArgumentException(WRONG_RANGE);
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
