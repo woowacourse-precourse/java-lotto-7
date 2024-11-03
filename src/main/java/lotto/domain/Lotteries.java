472442ae498cf418e1ba843122854ab5c6029c9a
moonbinchoi <whsahd12@gmail.com>
2024-11-04 01:05:50 +0900

    refactor(Lotteries): 불필요한 import 제거

--- src/main/java/lotto/domain/Lotteries.java
+++ src/main/java/lotto/domain/Lotteries.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.util.LottoNumberGenerator;
-import lotto.global.common.Rank;
 
 public class Lotteries {
 
