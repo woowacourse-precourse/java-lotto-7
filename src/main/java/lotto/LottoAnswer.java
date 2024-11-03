8862b8dbb2862edf351c8498b33c04a21461cd70
송지웅 <thdwldnd7@dgu.ac.kr>
2024-11-03 19:00:27 +0900

    feat: 당첨 번호, 보너스 번호 포함하는 객체 추가

---
+++ src/main/java/lotto/LottoAnswer.java
@@ -0,0 +1,13 @@
+﻿package lotto;
+
+import java.util.List;
+
+public class LottoAnswer {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public LottoAnswer(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+}
