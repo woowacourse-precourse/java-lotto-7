fde950f8db5c1e2a221f613103883d829c7616c5
koosco <rnxogud123@naver.com>
2024-11-04 04:01:37 +0900

    feat: 로또 구매 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,6 @@ public class Application {
 
     public static void main(String[] args) {
         AppConfig config = new AppConfig();
-        LottoApp lottoApp = new LottoApp(
-            config.lottoGenerateController(),
-            config.lottoCheckController(),
-            config.lottoResultController());
-
-        lottoApp.run();
+        new LottoApp(config.lottoController()).run();
     }
 }
--- src/main/java/lotto/LottoApp.java
+++ src/main/java/lotto/LottoApp.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.ui.LottoController;
+
 public class LottoApp {
 
+    private final LottoController lottoController;
+
+    public LottoApp(LottoController lottoController) {
+        this.lottoController = lottoController;
+    }
+
     public void run() {
+        lottoController.purchaseAll();
     }
 }
