f9f5c34d36ea1b80e0fb12c2d655afbff19a59e0
김아린 <dkfls921@gmail.com>
2024-11-04 08:07:40 +0900

    feat(controller): sort lotto numbers in ascending order upon generation

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -2,7 +2,9 @@ package lotto.Controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Model.Lotto;
+import lotto.Model.LottoResult;
 import lotto.View.InputView;
+import lotto.View.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +16,15 @@ public class LottoController {
             int purchaseAmount = InputView.inputPurchaseAmount();
             int numberOfLottos = purchaseAmount / 1000;
 
+            List<Lotto> purchasedLottos = purchaseLottos(numberOfLottos);
+
             List<Integer> winningNumbers = InputView.inputWinningNumbers();
             int bonusNumber = InputView.inputBonusNumber();
 
-            List<Lotto> purchasedLottos = purchaseLottos(numberOfLottos);
+            LottoResult lottoResult = calculateLottoResult(purchasedLottos, winningNumbers, bonusNumber);
+
+            OutputView.printPurchasedLottos(numberOfLottos, extractLottoNumbers(purchasedLottos));
+            OutputView.printWinningStatistics(lottoResult.getMatchCounts(), lottoResult.calculateYield(purchaseAmount));
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -28,8 +35,25 @@ public class LottoController {
         List<Lotto> purchasedLottos = new ArrayList<>();
         for (int i = 0; i < numberOfLottos; i++) {
             List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.sort(Integer::compareTo);
             purchasedLottos.add(new Lotto(lottoNumbers));
         }
         return purchasedLottos;
+    }
+
+    private List<List<Integer>> extractLottoNumbers(List<Lotto> lottos) {
+        List<List<Integer>> lottoNumbers = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            lottoNumbers.add(lotto.getNumbers());
+        }
+        return lottoNumbers;
+    }
+
+    private LottoResult calculateLottoResult(List<Lotto> purchasedLottos, List<Integer> winningNumbers, int bonusNumber) {
+        LottoResult result = new LottoResult();
+        for (Lotto lotto : purchasedLottos) {
+            result.updateStatistics(lotto, winningNumbers, bonusNumber);
+        }
+        return result;
     }
 }
