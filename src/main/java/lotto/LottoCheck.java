0dc854db85dea4a20bbaec2a2a7f39a825f1d72a
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-03 12:43:49 +0900

    feat: 사용자가 구매한 번호와 당첨번호가 몇개 맞는지 맞는횟수 확인

--- src/main/java/lotto/LottoCheck.java
+++ src/main/java/lotto/LottoCheck.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoCheck {   // 로또 당첨 및 등수확인
     public int lottoRankResult (int match, int bonus) {     // 1~5등 조건
         if (match == 6) {
@@ -18,5 +20,17 @@ public class LottoCheck {   // 로또 당첨 및 등수확인
             return 5;
         }
         return 0;
+    }
+
+    public int countMatches(List<Lotto> userPurchasedLotto, List<Integer> winningNumber) {
+        // 보너스번호 제외하고 유저가 구매한번호와 당첨번호가 몇개 맞는지 확인
+        int match = 0;
+        for (Lotto number : userPurchasedLotto) {
+            if (winningNumber.contains(number)) {
+                match++;
+            }
+        }
+
+        return match;
     }
 }
