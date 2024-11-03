7000b687d0524d5faa2b4b92a9a4f09068faf68c
GrO0vy <lmc2819@naver.com>
2024-11-03 20:31:36 +0900

    refactor: 구매 금액 검증 테스트 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.validator.PriceValidator;
+import lotto.view.PriceInputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,52 +29,41 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-    @DisplayName("구입금액에 숫자 외의 문자가 있으면 예외가 발생한다")
+    // 구매 금액 검증 기능 ( PriceValidator ) 테스트
+    @DisplayName("구입 금액에 숫자 외의 문자가 있으면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"asd", ",123", "12 12432", "@$%#!", "123@$12,./"})
     void 구입_금액에_숫자_외의_문자가_있으면_예외가_발생한다(String input){
-        System.setIn(userInput(input));
-        assertThatThrownBy(() -> new LottoGenerator())
+        System.out.println(input);
+        assertThatThrownBy(() -> new PriceValidator(input).validate())
                 .isInstanceOf(IllegalArgumentException.class);
-
-        Console.close();
     }
 
     @DisplayName("음수를 입력하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"-1", "-1000000", "-2147483648", "-3482739"})
     void 음수를_입력하면_예외가_발생한다(String input){
-        System.setIn(userInput(input));
-        assertThatThrownBy(() -> new LottoGenerator())
+        assertThatThrownBy(() -> new PriceValidator(input).validate())
                 .isInstanceOf(IllegalArgumentException.class);
-
-        Console.close();
     }
 
     @DisplayName("정수 범위를 벗어나면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1231241242141", "999999999999999", "2147483648"})
     void 정수_범위를_벗어나면_예외가_발생한다(String input){
-        System.setIn(userInput(input));
-        assertThatThrownBy(() -> new LottoGenerator())
+        assertThatThrownBy(() -> new PriceValidator(input).validate())
                 .isInstanceOf(IllegalArgumentException.class);
-
-        Console.close();
     }
 
     @DisplayName("천 단위 숫자가 아니면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"10001", "25001", "2147483647", "0"})
     void 천_단위_숫자가_아니면_예외가_발생한다(String input){
-        System.setIn(userInput(input));
-        assertThatThrownBy(() -> new LottoGenerator())
+        assertThatThrownBy(() -> new PriceValidator(input).validate())
                 .isInstanceOf(IllegalArgumentException.class);
-
-        Console.close();
     }
 
-    private InputStream userInput(String input){
-        return new ByteArrayInputStream(input.getBytes());
-    }
+//    private InputStream userInput(String input){
+//        return new ByteArrayInputStream(input.getBytes());
+//    }
 }
