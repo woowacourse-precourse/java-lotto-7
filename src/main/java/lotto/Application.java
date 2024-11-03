c0149701595c5a7559b440f574960417fe9ffa0b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 02:42:21 +0900

    refactor(Application): LottoNumberGenerator를 LottoController에 주입
    - `LottoController` 생성자에 `LottoNumberGenerator` 추가
    - 로또 번호 생성기를 외부에서 관리하도록 구조 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.model.LottoNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -9,7 +10,8 @@ public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController(
                 new InputView(),
-                new OutputView()
+                new OutputView(),
+                new LottoNumberGenerator()
         );
 
         lottoController.start();
