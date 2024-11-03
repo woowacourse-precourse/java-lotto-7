d3c7e0256e881064ef881fb5c4c1070f1acfe111
wgrgwg <kyoeng14697@naver.com>
2024-11-04 03:54:05 +0900

    fix: Fix NOT_INTEGER errorMessage

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.constants;
 public enum ErrorMessage {
     // 입력 관련
     INPUT_EMPTY("입력이 없거나 공백이어선 안됩니다."),
-    NOT_INTEGER("입력은 정수여야 합니다."),
+    NOT_INTEGER("입력은 Integer 범위 내의 정수여야 합니다."),
     NOT_POSITIVE("입력은 양수여야 합니다."),
 
     // 도메인 관련
