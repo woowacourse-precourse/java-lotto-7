80b1721332d7ca7e6ec9951caf7eda80bb5c0dba
jongsic17 <qkrwhdtlr178@naver.com>
2024-11-03 13:34:07 +0900

    feat: 구입금액을 입력 받고 구매 가능한 갯수 출력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,46 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchasePrice = inputLottoPurchaseAmount();
+        int lottoCount = calculateLottoCount(purchasePrice);
+
+        System.out.println(lottoCount + "개를 구매했습니다.");
     }
+    public static int inputLottoPurchaseAmount() {
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            int purchasePrice = Integer.parseInt(readLine());
+            if (!validateCount(purchasePrice)) {
+                throw new IllegalArgumentException();
+            }
+            return purchasePrice;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자를 입력해주세요. ");
+            return inputLottoPurchaseAmount();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputLottoPurchaseAmount();
+        }
+    }
+
+    public static int calculateLottoCount(int price) {
+        int count = 0;
+        count = price / 1000;
+        return count;
+    }
+
+    public static boolean validateCount(int price) {
+        if (price == 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 1장의 가격은 1000원 입니다. 금액을 다시 입력해 주세요.");
+        }
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원으로 나누어 떨어지지 않습니다. 금액을 다시 입력해 주세요.");
+        }
+        return true;
+    }
+
 }
