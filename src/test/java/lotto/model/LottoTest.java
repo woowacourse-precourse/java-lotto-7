f2915ea0188cc3e1be19d9b0b3aa66b780380cd2
Dohun Kim <dohun8758@naver.com>
2024-11-03 18:05:21 +0900

    test: Lotto 클래스 테스트 코드 작성

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -3,6 +3,7 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,33 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 객체 생성 테스트")
+    void 객체_생성_테스트() {
+        List<Integer> numbers = List.of(6, 3, 4, 2, 1, 5);
+        Lotto lotto = Lotto.of(numbers);
+
+        Assertions.assertEquals("[1, 2, 3, 4, 5, 6]", lotto.toString());
+    }
+
+    @Test
+    @DisplayName("contains 메서드 테스트")
+    void contains_테스트() {
+        List<Integer> numbers = List.of(6, 3, 4, 2, 1, 5);
+        Lotto lotto = Lotto.of(numbers);
+
+        Assertions.assertTrue(lotto.contains(6));
+        Assertions.assertFalse(lotto.contains(7));
+    }
+
+    @Test
+    @DisplayName("matchingCountWith 메서드 테스트")
+    void matchingCountWith_테스트() {
+        Lotto lotto1 = Lotto.of(List.of(6, 3, 4, 2, 1, 5));
+        Lotto lotto2 = Lotto.of(List.of(1, 8, 4, 35, 21, 7));
+        Lotto lotto3 = Lotto.of(List.of(9, 8, 12, 35, 21, 7));
+
+        Assertions.assertEquals(2, lotto1.matchingCountWith(lotto2));
+        Assertions.assertEquals(0, lotto1.matchingCountWith(lotto3));
+    }
 }
