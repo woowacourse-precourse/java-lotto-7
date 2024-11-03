51eb2c6c032183f89f30996eaa418e0c510fabde
YooSejeong <nived3@naver.com>
2024-11-03 23:47:20 +0900

    feat(InputView): 로또 구입금액 입력받는 기능

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int getLottoMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+}
