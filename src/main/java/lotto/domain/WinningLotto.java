044748a1b936cfb162e09b82c43539aa08a33927
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:49:07 +0900

    :sparkles: feat(WinningLotto): 당첨 로또 정보 저장 및 검증

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,36 @@
 package lotto.domain;
 
-public class WinningLotto {
+import static lotto.handler.ErrorHandler.INVALID_UNIQUE;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        super(winningNumbers);
+        validate(winningNumbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers, int bonusNumber) {
+        validateUnique(winningNumbers, bonusNumber);
+        validateBonusNumber(bonusNumber);
+    }
+
+    private void validateUnique(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw INVALID_UNIQUE.getException();
+        }
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        validateNumberRange(bonusNumber);
+    }
 }
+
