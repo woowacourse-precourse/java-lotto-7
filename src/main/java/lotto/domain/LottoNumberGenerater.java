af59033dd7003cef468d2c3233f8b2ee6ea01c0a
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:42:27 +0900

    feat: 랜덤 번호 생성

--- src/main/java/lotto/domain/LottoNumberGenerater.java
+++ src/main/java/lotto/domain/LottoNumberGenerater.java
@@ -1,2 +1,13 @@
-package lotto.domain;public class LottoNumberGenerate {
+package lotto.domain;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+public class LottoNumberGenerater {
+    List<Integer> lotto;
+    public List<Integer> numberGenerate(){
+        lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return lotto;
+    }
 }
+
+
+
