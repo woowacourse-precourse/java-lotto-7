0932274782354ea0c784827f13f7fedb39294aa4
DaeyeonCho <daecho8@gmail.com>
2024-11-03 15:34:09 +0900

    refactor: 로또 클래스 리펙토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -18,11 +18,15 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return numbers.stream().sorted().toList();
+        return numbers.stream()
+                .sorted()
+                .toList();
     }
 
     private void checkDuplicate(List<Integer> numbers) {
-        if (numbers.size() != numbers.stream().distinct().count()) {
+        if (numbers.size() != numbers.stream()
+                .distinct()
+                .count()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 6개여야 합니다.");
         }
     }
