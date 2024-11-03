7f1db1ee9595e14fbcdfed598c14b1a1de1c8b26
yunsseong <aster20@hanyang.ac.kr>
2024-11-03 22:02:00 +0900

    feat: LottoMachine 구현을 위한 뼈대 코드 작성

---
+++ src/main/java/lotto/service/LottoMachine.java
@@ -0,0 +1,20 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class LottoMachine {
+
+    public List<Lotto> getLotteries() {
+        return
+    }
+
+    public boolean validateMoneyAmount(String moneyAmount) {
+        return
+    }
+
+    public Lotto generateLotto() {
+        return
+    }
+
+}
---
+++ src/main/java/lotto/view/message/ErrorMessage.java
@@ -0,0 +1,6 @@
+package lotto.view.message;
+
+public enum ErrorMessage {
+
+
+}
