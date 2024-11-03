6d84a1864a87a2ec6df4dd8c8e30b5e8e0fbff9f
Yeongjae Kim <sees77770@gmail.com>
2024-11-04 01:39:53 +0900

    feat: 순위와 당첨금을 관리하는 Rank 생성
    순위와 당첨금을 관리하는 Enum 클래스 Rank를 생성한다.
    - WinningLotto의 책임을 Rank로 분리한다.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    Rank(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    
+}
