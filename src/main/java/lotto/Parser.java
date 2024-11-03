8057a437c863e46d470e8bd2d46532ac61c22cf3
sejinezy <psj8880@naver.com>
2024-11-04 01:13:42 +0900

    refactor(Parser): 입력값 파싱을 처리하는 Parser 클래스 분리

---
+++ src/main/java/lotto/Parser.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    public static List<Integer> toInts(String[] inputs) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String input : inputs) {
+            numbers.add(toInt(input));
+        }
+        return numbers;
+    }
+
+    public static int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+
+}
