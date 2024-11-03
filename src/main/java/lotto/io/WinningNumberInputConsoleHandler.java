5ae060bb4c617656b9ee0fa43f59732f3e661004
sodychoe <sodychoe@icloud.com>
2024-11-04 00:58:16 +0900

    feat(WinningNumberInputConsoleHandler) : 당첨 번호 입력 받기 기능 구현

--- src/main/java/lotto/io/WinningNumberInputConsoleHandler.java
+++ src/main/java/lotto/io/WinningNumberInputConsoleHandler.java
@@ -1,7 +1,18 @@
 package lotto.io;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class WinningNumberInputConsoleHandler {
+
+    public static final String RAW_NUMBER_SEPARATOR = ",";
+
     public void showWinningNumberGuideMessage() {
         System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public String[] askWinningNumber() {
+        String rawWinningNumber = Console.readLine();
+
+        return rawWinningNumber.split(RAW_NUMBER_SEPARATOR);
     }
 }
