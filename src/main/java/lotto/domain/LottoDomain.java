5b141e6a324c36a2264bec081fb2f5d84077e300
장시영 <sysmile77@gmail.com>
2024-11-04 03:15:50 +0900

    refactor(LotteDomain) : 미사용 vo 제거

--- src/main/java/lotto/domain/LottoDomain.java
+++ src/main/java/lotto/domain/LottoDomain.java
@@ -6,35 +6,9 @@ import java.util.List;
 public  class LottoDomain {
 
     public static int price;
-    public static int playLumber;
-    public static List<Integer> lottoNumbers;
-
-
-    public LottoDomain(int price) {
-        LottoDomain.price = price ;
-
-    }
-    public static int getPrice() {
-        return price;
-    }
 
     public static void setPrice(int price) {
         LottoDomain.price = price;
     }
 
-    public static int getPlayLumber() {
-        return playLumber;
-    }
-
-    public static void setPlayLumber(int playLumber) {
-        LottoDomain.playLumber = playLumber;
-    }
-
-    public static List<Integer> getLottoNumbers() {
-        return lottoNumbers;
-    }
-
-    public static void setLottoNumbers(List<Integer> lottoNumbers) {
-        LottoDomain.lottoNumbers = lottoNumbers;
-    }
 }
