d679384a4a693d83518de71e618b5b1ee8a5a10b
GoGradually <chkun3109@gmail.com>
2024-11-03 22:23:49 +0900

    test(LotteryRoundManager.java): 당첨번호 등록 테스트 코드 작성
    - 보너스 번호와 당첨 번호 간에는 겹치는 수가 없어야 한다.
    - 보너스 번호와 당첨 번호는 모두 1 이상 45 이하여야 한다.
    - 로또 당첨 번호는 6개여야 한다.

--- src/test/java/lotto/LotteryRoundManagerTest.java
+++ src/test/java/lotto/LotteryRoundManagerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -46,6 +48,89 @@ class LotteryRoundManagerTest {
         //when, then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> lotteryRoundManager.purchaseLotto(cost))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+
+    @DisplayName("로또 당첨번호는 겹치는 수가 없어야 한다.")
+    @Test
+    void 로또_당첨_번호는_겹치는_수가_없어야_한다(){
+        //given
+        List<Integer> wrongList = List.of(1, 1, 2, 3, 4, 5);
+        int bonusNumber = 7;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+        
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+
+    @DisplayName("로또 보너스번호는 당첨 번호와 겹치지 말아야 한다.")
+    @Test
+    void 로또_당첨_번호는_당첨_번호와_겹치지_말아야_한다(){
+        //given
+        List<Integer> wrongList = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 2;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+
+    @DisplayName("로또 당첨번호는 1 이상 45 이하여야 한다.")
+    @Test
+    void 로또_당첨_번호는_1이상_45이하여야_한다(){
+        //given
+        List<Integer> wrongList = List.of(1, 2, 3, 4, 5, 46);
+        int bonusNumber = 7;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+
+    @DisplayName("로또 보너스번호는 1 이상 45 이하여야 한다.")
+    @Test
+    void 로또_보너스_번호는_1이상_45이하여야_한다(){
+        //given
+        List<Integer> wrongList = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 47;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+    @DisplayName("로또 당첨번호는 6개를 초과하여 존재하면 안된다.")
+    @Test
+    void 로또_당첨_번호는_6개를_초과하면_안된다(){
+        //given
+        List<Integer> wrongList = List.of(1, 2, 3, 4, 5, 6, 7);
+        int bonusNumber = 8;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
+                .withMessageStartingWith(ERROR_MESSAGE);
+    }
+
+    @DisplayName("로또 당첨번호는 6개 미만으로 존재하면 안된다.")
+    @Test
+    void 로또_당첨_번호는_6개_미만으로_존재하면_안된다(){
+        //given
+        List<Integer> wrongList = List.of(1, 2, 3, 4, 5);
+        int bonusNumber = 8;
+        LotteryRoundManager lotteryRoundManager = new LotteryRoundManager();
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lotteryRoundManager.setWinningNumber(wrongList, bonusNumber))
                 .withMessageStartingWith(ERROR_MESSAGE);
     }
 }
