786a29297c9b3996d2d88929b04a5f6f6efb5f31
Jungwoo Chu <cjw980930@gmail.com>
2024-11-03 19:42:33 +0900

    test: 맞춘숫자가 5개일 경우 보너스 여부에 따른 등수 반환 테스트

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -19,4 +19,17 @@ class RankTest {
         //Then
         Assertions.assertThat(rank).isEqualTo(Rank.FIRST);
     }
+
+    @Test
+    void 맞춘숫자가_5개_일_경우_보너스_여부에_따른_등수반환_테스트() {
+        //Given
+        int numberMatch = 5;
+        boolean bonus = true;
+
+        //When
+        Rank rank = Rank.getRank(numberMatch,bonus);
+
+        //Then
+        Assertions.assertThat(rank).isEqualTo(Rank.SECOND);
+    }
 }
