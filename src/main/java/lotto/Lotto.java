0a41b387c20c9307b76f48553a7046960b82a240
githyj-jang <hoo9268@gmail.com>
2024-11-04 00:31:32 +0900

    feat: 로또 발행 기능 추가 - 로또 번호를 무작위로 생성하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,15 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_PRICE = 1000;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +18,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + LOTTO_SIZE + "개여야 합니다.");
         }
+
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto generateRandomLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE);
+        Collections.sort(randomNumbers);
+        return new Lotto(randomNumbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
