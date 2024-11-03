daacd710ee4d0fb34ca10939e657eca2f9eefbcf
Lee-KwanBeom <dmtmqhdkk@naver.com>
2024-11-03 21:40:49 +0900

    refactor : LottoInput 리팩토링
    - 기능, 유효성 검사, 입력 부분 세부적인 리팩토링
    - 자바 컨벤션에 맞춰 코드 수정

--- src/test/java/lotto/LottoInputParserTest.java
+++ src/test/java/lotto/LottoInputParserTest.java
@@ -3,8 +3,6 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,7 +10,7 @@ class LottoInputTest {
     @DisplayName("숫자가 아닌 입력을 받을 경우 예외가 발생한다")
     @Test
     void 숫자가_아닌_입력_예외_테스트() {
-        assertThatThrownBy(() -> LottoInput.parsePrice("abc"))
+        assertThatThrownBy(() -> InputParser.parsePrice("abc"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -21,14 +19,14 @@ class LottoInputTest {
     void 정상_입력_변환_테스트() {
         String[] input = {"1", "2", "3", "4", "5", "6"};
         int[] expected = {1, 2, 3, 4, 5, 6};
-        assertThat(LottoInput.getIntArray(input)).containsExactly(expected);
+        assertThat(InputParser.getIntArray(input)).containsExactly(expected);
     }
 
     @DisplayName("입력 값에 유효하지 않은 값이 있으면 예외가 발생한다")
     @Test
     void 유효하지_않은_값_테스트() {
         String[] input = {"1", "2", "abc", "4", "5", "6"};
-        assertThatThrownBy(() -> LottoInput.getIntArray(input))
+        assertThatThrownBy(() -> InputParser.getIntArray(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -36,25 +34,8 @@ class LottoInputTest {
     @Test
     void 빈_문자열_테스트() {
         String[] input = {"1", "2", "", "4", "5", "6"};
-        assertThatThrownBy(() -> LottoInput.getIntArray(input))
+        assertThatThrownBy(() -> InputParser.getIntArray(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("45이상의 숫자가 입력되면 예외가 발생한다")
-    @Test
-    void 유효하지_않은_숫자_테스트() {
-        List<Integer> input = List.of(1, 2, 3, 4, 5, 60);
-        assertThatThrownBy(() -> LottoInput.validLotto(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("1000원 미만 || 1000원 단원 단위 아닌")
-    @Test
-    void 유효하지_않은_숫자_테스트2() {
-        assertThatThrownBy(() -> LottoInput.validatePrice(900))
-                .isInstanceOf(IllegalArgumentException.class);
-
-        assertThatThrownBy(() -> LottoInput.validatePrice(1500))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 }
