ebd8461c172db952e93e8c3a27e5e06a9188bea2
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-03 09:36:21 +0900

    feat(LottoPrizeMoney): 로또 등수 추가
    - 로또 등수에 따라 가격을 출력하기 위해서 추가하였습니다.

--- src/main/java/lotto/Model/LottoPrizeMoney.java
+++ src/main/java/lotto/Model/LottoPrizeMoney.java
@@ -1,18 +1,24 @@
 package lotto.Model;
 
 public enum LottoPrizeMoney {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    FIRST(1, 6, 2000000000),
+    SECOND(2, 5, 30000000),
+    THIRD(3, 5, 1500000),
+    FOURTH(4, 4, 50000),
+    FIFTH(5, 3, 5000);
 
+    private final int rank;
     private final int prizeMoney;
     private final int matchNumber;
 
-    LottoPrizeMoney(int matchNumber, int prizeMoney) {
+    LottoPrizeMoney(int rank, int matchNumber, int prizeMoney) {
+        this.rank = rank;
         this.matchNumber = matchNumber;
         this.prizeMoney = prizeMoney;
+    }
+
+    public int getRank() {
+        return rank;
     }
 
     public int getPrizeMoney() {
