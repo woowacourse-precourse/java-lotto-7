ef0da225118d5bac430fa7ccd853e2dc3d3beb51
Yeongjae Kim <sees77770@gmail.com>
2024-11-03 17:14:22 +0900

    feat: 로또 번호가 중복될 때 예외 처리
    번호가 중복된 로또 생성을 방지하기 위해
    - 예외 처리 기능을 구현한다.
    - 주어진 로또 테스트를 성공시킨다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -11,8 +12,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (Collections.frequency(numbers, number) >= 2) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+            }
         }
     }
 }
