3e26a0d79a19afd2c3bc14e169f263b70fd398ff
Shinjongyun <tigerjong123@gmail.com>
2024-11-03 16:55:22 +0900

    feat: 당첨번호를 저장하는 객체 추가

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final List<Integer> numbers;
+
+    public WinningLotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+}
