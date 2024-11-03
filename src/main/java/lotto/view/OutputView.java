bbd4404b6855c7b2061011bb7135e70645679c90
sseen2 <kse10023@naver.com>
2024-11-03 21:15:57 +0900

    refactor: OutputView 당첨 내역 출력 메소드 기능 분리
    출력할 String을 리턴하는 메소드를 만들어 output 메소드에서는 출력만 하도록 작업을 분리함.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,8 @@
 package lotto.view;
 
-import lotto.Lotto;
-import lotto.LottoDraw;
-import lotto.enums.InputMessage;
-import lotto.enums.LottoPrize;
+import lotto.*;
 import lotto.enums.OutputMessage;
-import lotto.LottoWinning;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class OutputView {
@@ -19,30 +14,25 @@ public class OutputView {
         }
     }
 
-    public static void outputWinningResult(LottoWinning lottoWinning) {
+    public static void outputWinningResult(LottoWinning lottoWinning, LottoPrizeMap lottoPrizeMap) {
         System.out.println(OutputMessage.STRING_WINNING_STATISTIC_OUTPUT.getMessage());
         Map<String, Integer> winningResult = lottoWinning.getWinningLotto();
-        Map<String, Integer> prizes = setPrizes();
-        for (Map.Entry<String, Integer> entry : winningResult.entrySet()) {
-            Integer prize = prizes.get(entry.getKey());
-            String outputString = String.format(
-                    OutputMessage.WINNING_STATISTICS_OUTPUT.getMessage(),
-                    entry.getKey(),
-                    prize,
-                    entry.getValue()
-            );
+        Map<String, Integer> prizes = lottoPrizeMap.getPrizes();
 
-            System.out.println(outputString);
+        for (Map.Entry<String, Integer> entry : winningResult.entrySet()) {
+            System.out.println(winningResultOutputString(entry, prizes));
         }
     }
 
-    private static Map<String, Integer> setPrizes() {
-        Map<String, Integer> setPrizes = new LinkedHashMap<>();
-        setPrizes.put("3개 일치", LottoPrize.THREE_WINNING_PRIZE.getPrize());
-        setPrizes.put("4개 일치", LottoPrize.FOUR_WINNING_PRIZE.getPrize());
-        setPrizes.put("5개 일치", LottoPrize.FIVE_WINNING_PRIZE.getPrize());
-        setPrizes.put("5개 일치, 보너스 볼 일치", LottoPrize.FIVE_WINNING_WITH_BONUS_PRIZE.getPrize());
-        setPrizes.put("6개 일치", LottoPrize.SIX_WINNING_PRIZE.getPrize());
-        return setPrizes;
+    private static String winningResultOutputString(
+            Map.Entry<String, Integer> winningResult,
+            Map<String, Integer> prizes
+    ) {
+        return String.format(
+                OutputMessage.WINNING_STATISTICS_OUTPUT.getMessage(),
+                winningResult.getKey(),
+                prizes.get(winningResult.getKey()),
+                winningResult.getValue()
+        );
     }
 }
