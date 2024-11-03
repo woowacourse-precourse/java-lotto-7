5e3f4d082a5c17ae1386a79d06b5aa5fb04bc763
sinyebin <dpqls0625@naver.com>
2024-11-03 22:57:30 +0900

    feat: 중복번호가 있는지

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
     private static final String IS_NOT_LOTTO_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String IS_DUPLICATE_NUMBER = "[ERROR] 중복된 번호가 있습니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,11 +17,19 @@ public class Lotto {
 
     private void validateLotto(List<Integer> numbers) {
         validateLottoSetSize(numbers);
+        valdiateDuplicate(numbers);
+    }
+
+    private void valdiateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(IS_DUPLICATE_NUMBER);
+        }
     }
 
     private void validateLottoSetSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(IS_NOT_LOTTO_SIZE);
         }
     }
 
