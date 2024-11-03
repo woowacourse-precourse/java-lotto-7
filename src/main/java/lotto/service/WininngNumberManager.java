ea64757e43093e7fa3c44b6900af7156bf045558
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:53:16 +0900

    feat: 당첨 번호를 일급 컬렉션으로 생성하는 매니저

--- src/main/java/lotto/service/WininngNumberManager.java
+++ src/main/java/lotto/service/WininngNumberManager.java
@@ -1,2 +1,40 @@
-package lotto.service;public class WininngNumberManager {
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.repository.LottoRepository;
+import lotto.valuate.Valuate;
+
+public class WininngNumberManager {
+    private List<String> list;
+    private List<Integer> list_num;
+    Valuate valuate = new Valuate();
+    LottoRepository lottoRepository;
+    private Lotto lotto;
+
+    public void createWinningNumber() {
+        lotto = new Lotto(list_num);
+        lottoRepository.saveWinningNumbers(lotto);
+    }
+
+    private void parsingWinningInput() {
+        list_num = list.stream()
+                .map(String::trim)
+                .map(this::evaluateWinningNumber)
+                .collect(Collectors.toList());
+    }
+
+    private int evaluateWinningNumber(String s) {
+        valuate.isNum(s);
+        return Integer.parseInt(s);
+    }
+
+    public WininngNumberManager(String s, LottoRepository lottoRepository) {
+        this.lottoRepository = lottoRepository;
+        list = Arrays.asList(s.split(","));
+        parsingWinningInput();
+    }
+
 }
