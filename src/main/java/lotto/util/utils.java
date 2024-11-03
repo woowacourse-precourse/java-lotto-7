bc452fe1782934668a7f6dcec982946a40012cee
Gahyeon <best11gh@naver.com>
2024-11-03 20:06:14 +0900

    refactor: 상수화

--- src/main/java/lotto/util/utils.java
+++ src/main/java/lotto/util/utils.java
@@ -3,8 +3,12 @@ package lotto.util;
 import static lotto.exception.ExceptionMessage.*;
 
 public class utils {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     public static void validateRange(int number) {
-        if (number < 1 || number > 45) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER.getMessage());
         }
     }
