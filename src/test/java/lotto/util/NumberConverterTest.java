c2172824ade0992128031cdc3bbcf1d8cfe14a67
양효빈 <hyobin3026@naver.com>
2024-11-03 20:19:51 +0900

    test(NumberConverterTest): Add test class for NumberConverter

---
+++ src/test/java/lotto/util/NumberConverterTest.java
@@ -0,0 +1,64 @@
+package lotto.util;
+
+import lotto.exception.GeneralExceptionMessages;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class NumberConverterTest {
+
+    private final DefaultNumberConverter converter = new DefaultNumberConverter();
+
+    @Test
+    @DisplayName("유효한 숫자 문자열 입력 시 해당 숫자로 변환된다")
+    void shouldConvertValidNumberString() {
+        // given
+        String input = "1";
+
+        // when
+        int result = converter.convertNumber(input);
+
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자 앞뒤로 공백이 포함되어 있다면 공백을 제거하고 숫자를 반환한다")
+    void shouldConvertValidNumberStringIncludingTrim() {
+        // given
+        String input = "1 ";
+
+        // when
+        int result = converter.convertNumber(input);
+
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("빈 문자열 입력 시 예외 발생")
+    void shouldThrowExceptionForEmptyString() {
+        // given
+        String input = "";
+
+        // when & then
+        assertThatThrownBy(() -> converter.convertNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(GeneralExceptionMessages.INVALID_NUMBER);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자열 입력 시 예외 발생")
+    void shouldThrowExceptionForNonNumericString() {
+        // given
+        String input = "hi";
+
+        // when & then
+        assertThatThrownBy(() -> converter.convertNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(GeneralExceptionMessages.INVALID_NUMBER);
+    }
+
+}
