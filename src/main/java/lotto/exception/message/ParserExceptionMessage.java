cd08ebe32ac3f73395625cd8b4a30a3ba3501970
kkmdevel <kkm.devel@gmail.com>
2024-11-03 20:18:55 +0900

    refactor(ParserExceptionMessage): 예외 메시지 간소화
    - "아무것도 입력되지 않았습니다."에서 "입력되지 않았습니다."로 메시지 간소화

--- src/main/java/lotto/exception/message/ParserExceptionMessage.java
+++ src/main/java/lotto/exception/message/ParserExceptionMessage.java
@@ -1,7 +1,7 @@
 package lotto.exception.message;
 
 public enum ParserExceptionMessage{
-    EMPTY_INPUT("아무것도 입력되지 않았습니다."),
+    EMPTY_INPUT("입력되지 않았습니다."),
     NOT_NUMBER("숫자를 입력해주세요");
     private final String message;
 
