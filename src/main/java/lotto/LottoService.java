56654dc545336ef12c8b14c314eac68e11a710bf
dltjdnjs <scw.dltjdnjs@gmail.com>
2024-11-03 20:51:08 +0900

    add: 로또 번호 생성기 분리 및 일치 개수 계산 기능 추가

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,26 +1,47 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoService {
     public static List<Lotto> createLottos(int inputPrice) {
         int totalLottoCount = inputPrice / 1000;
+        LottoNumberGenerator generator = new LottoNumberGenerator();
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < totalLottoCount; i++) {
-            List<Integer> lottoNumbers = generateLottoNumbers();
+            List<Integer> lottoNumbers = generator.generateLottoNumbers();
             lottos.add(new Lotto(lottoNumbers));
         }
         return lottos;
     }
 
-    private static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+    public static int[] calculateMatchCounts(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        int[] matchCounts = new int[7];
+
+        for (int i = 0; i < lottos.size(); i++) {
+            Lotto lotto = lottos.get(i);
+            int matchCount = lotto.matchCount(winningNumbers);
+            updateMatchCounts(matchCounts, matchCount, lotto.hasBonusNumber(bonusNumber));
+        }
+        return matchCounts;
+    }
+
+    private static void updateMatchCounts(int[] matchCounts, int matchCount, boolean hasBonus) {
+        if (matchCount == 6) {
+            matchCounts[6]++;
+            return;
+        }
+        if (matchCount == 5) {
+            if (hasBonus) {
+                matchCounts[5]++;
+            } else {
+                matchCounts[4]++;
+            }
+            return;
+        }
+        if (matchCount >= 3) {
+            matchCounts[matchCount - 1]++;
+        }
     }
 }
