da1a43c7a71fc7ba14a3c3674ba83dd6c001568d
GrO0vy <lmc2819@naver.com>
2024-11-03 19:38:39 +0900

    refactor: 입력 검증 과정 추가

--- src/main/java/lotto/view/WinningNumberInputView.java
+++ src/main/java/lotto/view/WinningNumberInputView.java
@@ -1,15 +1,23 @@
 package lotto.view;
 
 import lotto.messages.InputMessage;
+import lotto.validator.WinningNumberValidator;
 
 public class WinningNumberInputView extends InputView{
+
+
     @Override
     protected void printInputMessage() {
         System.out.println(InputMessage.WINNING_NUMBER_INPUT_MESSAGE.getMessage());
     }
 
     @Override
-    protected void validate(String input) {
+    protected void initializeValidator() {
+        validator = new WinningNumberValidator(inputValue);
+    }
 
+    @Override
+    protected void validate() {
+        validator.validate();
     }
 }
