fc3f1d10f878eaed6fd292eb94004f75b60615fa
gahyeon <1000ji1@naver.com>
2024-11-03 19:31:28 +0900

    feat: 1~45 사이의 숫자를 입력했는지 확인

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -2,8 +2,12 @@ package lotto.utils;
 
 public class Validator {
     private static final int DIVIDED_AMOUNT = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     public static final String INVALID_NUMBER_MESSAGE = "[ERROR] 숫자 입력하셔야 합니다.";
     public static final String INVALID_AMOUNT_MESSAGE = "[ERROR] 1000원 단위로 입력하셔야 합니다.";
+    public static final String INVALID_NUMBER_RANGE_MESSAGE = "[ERROR] 1~45 사이의 숫자여야합니다.";
 
     private Validator() {
     }
@@ -19,6 +23,12 @@ public class Validator {
     public static void amountIsMultipleOf1000(int amount) {
         if (amount % DIVIDED_AMOUNT != 0) {
             throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+
+    public static void numberInRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE_MESSAGE);
         }
     }
 }
