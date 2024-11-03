956e496a20b0d09c49e97ec0286232434a79d06a
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:56:40 +0900

    :sparkles: feat(LottoRank): 구입한 로또 티켓의 당첨 결과 계산

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,4 +1,43 @@
 package lotto.domain;
 
+import static lotto.handler.ConstantHandler.RANK_ADD_VALUE;
+import static lotto.handler.ConstantHandler.RANK_DEFAULT_VALUE;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 public class LottoRank {
+
+    private final Map<Rank, Integer> lottoRank = new EnumMap<>(Rank.class);
+
+    public LottoRank(LottoTickets lottoTickets, WinningLotto winningLotto) {
+        for (Rank rank : Rank.values()) {
+            if (rank != Rank.MISS) {
+                lottoRank.put(rank, RANK_DEFAULT_VALUE);
+            }
+        }
+
+        for (Lotto lotto : lottoTickets.getLottoTickets()) {
+            Rank rank = getRank(lotto, winningLotto);
+            if (rank != Rank.MISS) {
+                lottoRank.put(rank, lottoRank.get(rank) + RANK_ADD_VALUE);
+            }
+        }
+    }
+
+    public Map<Rank, Integer> getLottoRank() {
+        return lottoRank;
+    }
+
+    private Rank getRank(Lotto lotto, WinningLotto winningLotto) {
+        int matchCount = getMatchCount(lotto, winningLotto);
+        boolean containBonusNumber = lotto.getNumbers().contains(winningLotto.getBonusNumber());
+        return Rank.getRank(matchCount, containBonusNumber);
+    }
+
+    private int getMatchCount(Lotto lotto, WinningLotto winningLotto) {
+        return (int) lotto.getNumbers().stream()
+                .filter(winningLotto.getNumbers()::contains)
+                .count();
+    }
 }
