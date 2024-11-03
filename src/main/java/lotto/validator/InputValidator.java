f3ca9968061b8a4844098fd05f7c699f4fe19255
navangs <43688529+insertend23@users.noreply.github.com>
2024-11-04 01:50:19 +0900

    Fix: input validator not use interface

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,9 +1,19 @@
 package lotto.validator;
 
-public class InputValidator {
+public class InputValidator extends Validator {
     private static final String INPUT_IS_BLANK_ERROR = "[ERROR] 입력이 공백입니다.";
+    String input;
 
-    public void validateInputIsBlank(String input) {
+    public InputValidator(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public void validate() throws IllegalArgumentException {
+        validateInputIsBlank();
+    }
+
+    public void validateInputIsBlank() {
         if(input.isBlank()){
             throw new IllegalArgumentException(INPUT_IS_BLANK_ERROR);
         }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,23 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.validator.InputValidator;
 
 public class InputView {
-    private final InputValidator validator = new InputValidator();
+    private InputValidator validator;
 
     public String getPurchaseAmount() {
         String input = Console.readLine();
-        validator.validateInputIsBlank(input);
+        validator = new InputValidator(input);
+        validator.validate();
         return input;
     }
 
     public String getWinningNumbers() {
         String input = Console.readLine();
-        validator.validateInputIsBlank(input);
+        validator = new InputValidator(input);
+        validator.validate();
         return input;
     }
 
     public String getBonusNumber() {
         String input = Console.readLine();
-        validator.validateInputIsBlank(input);
+        validator = new InputValidator(input);
+        validator.validate();
         return input;
     }
 }
