a54a24bf53e8c05eea88283b95978f35d36b51cd
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 00:55:52 +0900

    refactor: 당첨 내역 출력 포멧 수정

--- src/main/java/lotto/system/formater/winning/WinningEntryFormatter.java
+++ src/main/java/lotto/system/formater/winning/WinningEntryFormatter.java
@@ -4,15 +4,10 @@ import lotto.system.utils.PrizeType;
 
 public class WinningEntryFormatter { // 각 개별 당첨 항목 포맷
 
-    private final static String WINNINGS_MESSAGE = "%d개 일치 (%,d원) - %d개";
-    private final static String BONUS_WINNINGS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private static final String FORMAT_MESSAGE = "%s - %d개";
 
     public static String formatWinnings(int code, int count) {
         PrizeType typeByCode = PrizeType.getTypeByCode(code);
-        if (typeByCode == PrizeType.SECOND_PRIZE) {
-            return String.format(BONUS_WINNINGS_MESSAGE, typeByCode.getCode(), typeByCode.getPrizeMoney(), count);
-        }
-        return String.format(WINNINGS_MESSAGE, typeByCode.getCode(), typeByCode.getPrizeMoney(), count);
+        return String.format(FORMAT_MESSAGE, typeByCode.getDescription(), count);
     }
-
 }
