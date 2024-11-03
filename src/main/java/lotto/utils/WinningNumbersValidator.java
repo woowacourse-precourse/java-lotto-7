9dcdb6c8e502b7fd65f3f566795d474f5a30aaf7
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 23:00:08 +0900

    feat(WinningNumbersValidator): 입력 값에 중복된 숫자가 있을 경우 예외 발생 기능 추가

--- src/main/java/lotto/utils/WinningNumbersValidator.java
+++ src/main/java/lotto/utils/WinningNumbersValidator.java
@@ -1,6 +1,8 @@
 package lotto.utils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbersValidator {
 
@@ -9,7 +11,7 @@ public class WinningNumbersValidator {
             try {
                 Integer.parseInt(number);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 입력된 값 중 숫자가 아닌 값이 있습니다.");
+                throw new IllegalArgumentException(" 입력된 값 중 숫자가 아닌 값이 있습니다.");
             }
         }
     }
@@ -18,8 +20,15 @@ public class WinningNumbersValidator {
         for (String number : inputNumbers) {
             int num = Integer.parseInt(number);
             if (num < 1 || num > 45) {
-                throw new IllegalArgumentException("[ERROR] 숫자는 1에서 45 사이여야 합니다.");
+                throw new IllegalArgumentException(" 숫자는 1에서 45 사이여야 합니다.");
             }
+        }
+    }
+
+    private static void validateNoDuplicates(List<String> inputNumbers) {
+        Set<String> uniqueNumbers = new HashSet<>(inputNumbers);
+        if (uniqueNumbers.size() != inputNumbers.size()) {
+            throw new IllegalArgumentException(" 중복된 숫자가 포함되어 있습니다.");
         }
     }
 
