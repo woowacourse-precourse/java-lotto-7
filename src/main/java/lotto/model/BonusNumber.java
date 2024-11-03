add12d57ec873ae302d19ca8efd171db092c9f51
Parkjyun <jaeyun0507@naver.com>
2024-11-03 18:58:01 +0900

    feat(BonusNumber): 숫자가 아닌 값이 들어오면 예외처리한다

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,9 +1,22 @@
 package lotto.model;
 
+import java.util.regex.Pattern;
+import lotto.model.exception.DomainExceptionMessage;
+
 public class BonusNumber {
+    private static final Pattern numberPattern = Pattern.compile("^[0-9]+$");
     private int number;
 
-    public BonusNumber(int number) {
-        this.number = number;
+    public BonusNumber(String number) {
+        validateNumberPattern(number);
+        this.number = Integer.parseInt(number);
+    }
+
+    private void validateNumberPattern(final String number) {
+        if (!numberPattern.matcher(number).matches()) {
+            throw new IllegalArgumentException(
+                    DomainExceptionMessage.INVALID_BONUS_NUMBER_FORMAT.getMessage()
+            );
+        }
     }
 }
--- src/main/java/lotto/model/exception/DomainExceptionMessage.java
+++ src/main/java/lotto/model/exception/DomainExceptionMessage.java
@@ -3,7 +3,8 @@ package lotto.model.exception;
 public enum DomainExceptionMessage {
     INVALID_MONEY_FORMAT("[ERROR] 금액은 숫자만 입력 가능합니다."),
     INVALID_MONEY_UNIT("[ERROR] 금액은 1000원 단위여야 합니다."),
-    INVALID_MONEY_VALUE("[ERROR] 금액은 0원 이상이어야 합니다.");
+    INVALID_MONEY_VALUE("[ERROR] 금액은 0원 이상이어야 합니다."),
+    INVALID_BONUS_NUMBER_FORMAT("[ERROR] 보너스 숫자는 숫자만 입력 가능합니다.");
     private final String message;
 
     DomainExceptionMessage(final String message) {
