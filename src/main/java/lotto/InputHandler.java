a371967061645a6c5e46d18940e8a9e6d29a8de5
GaBaljaintheroom <junsu1222@naver.com>
2024-11-03 23:29:56 +0900

    feat(InputHandler): 로또 구입할 금액 입력 처리 구현

---
+++ src/main/java/lotto/InputHandler.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.function.Supplier;
+
+public class InputHandler {
+
+    private final InputView inputView = new InputView();
+
+    public Cash inputCash() {
+        return handleError(inputView::inputPurchasePrice);
+    }
+
+    private <T> T handleError(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
