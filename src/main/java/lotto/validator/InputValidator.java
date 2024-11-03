30cebc1e7eed0a626ea98072430f42386104ee77
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-04 02:50:05 +0900

    feat(InputValidator): 숫자 입력 시 정수 값이 아닌 경우 예외 처리 추가

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -9,11 +9,20 @@ public class InputValidator {
 
     public static void validateInteger(String input) {
         validateBlank(input);
+        validateInt(input);
     }
 
     private static void validateBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT_NOT_ALLOWED.getMessage());
+        }
+    }
+
+    private static void validateInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INTEGER_INPUT.getMessage());
         }
     }
 }
--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -3,19 +3,33 @@ package lotto.validator;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.enums.ErrorMessage;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputValidatorTest {
 
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = {" "})
-    void 숫자_입력은_비거나_공백으로_이루어진_문자열일_수_없다(String input) {
-        // when & then
-        assertThatThrownBy(() -> InputValidator.validateInteger(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.BLANK_INPUT_NOT_ALLOWED.getMessage());
+    @Nested
+    class 숫자_입력_검증 {
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {" "})
+        void 숫자_입력은_비거나_공백으로_이루어진_문자열일_수_없다(String input) {
+            // when & then
+            assertThatThrownBy(() -> InputValidator.validateInteger(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.BLANK_INPUT_NOT_ALLOWED.getMessage());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"abc", "123abc", "!@#", "12.34"})
+        void 숫자_입력은_정수_형식만_허용한다(String input) {
+            // when & then
+            assertThatThrownBy(() -> InputValidator.validateInteger(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INTEGER_INPUT.getMessage());
+        }
     }
 }
