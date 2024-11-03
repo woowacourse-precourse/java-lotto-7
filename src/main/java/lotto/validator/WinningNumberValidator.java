be05f869ad12a2fc44c573842acd27edd2e25e24
GrO0vy <lmc2819@naver.com>
2024-11-03 22:46:09 +0900

    refactor: 당첨 번호 검증 과정 수정

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -18,8 +18,26 @@ public class WinningNumberValidator implements Validator {
     }
 
     private void isValidatedForm(){
+        isConsistOfNumberAndComma();
+        isStartOrEndWithComma();
+        isContainsContinuousComma();
+    }
+
+    private void isConsistOfNumberAndComma(){
         if(!winningNumbers.matches("[0-9|,]+")){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자와 쉼표만 입력 할 수 있습니다. (공백 없이 쉼표로만 숫자 구분)");
+        }
+    }
+
+    private void isStartOrEndWithComma(){
+        if(winningNumbers.startsWith(",") || winningNumbers.endsWith(",")){
+            throw new IllegalArgumentException("[ERROR] 쉼표로 시작하거나 끝날 수 없습니다.");
+        }
+    }
+
+    private void isContainsContinuousComma(){
+        if(winningNumbers.contains(",,")){
+            throw new IllegalArgumentException("[ERROR] 쉼표 사이에는 숫자를 입력해 주세요.");
         }
     }
 
