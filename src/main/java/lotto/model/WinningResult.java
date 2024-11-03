1a63301b3ce3d37527eabde292fac06ac075e8d5
ufo9430 <ufo9430@naver.com>
2024-11-03 17:17:31 -0500

    refactor: 당첨 내역을 입력하는 인자를 Reward로 변경

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -5,8 +5,7 @@ import java.util.HashMap;
 public class WinningResult {
     private final HashMap<Reward, Integer> rankCount = new HashMap<>();
 
-    public void put(int rank) {
-        Reward reward = Reward.getReward(rank);
+    public void put(Reward reward) {
         rankCount.put(reward, rankCount.getOrDefault(reward,0)+1);
     }
 
