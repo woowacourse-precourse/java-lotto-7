a0dffd0ce4b72b67cd702381eead018392cc2ece
Gyuri <haha0888@naver.com>
2024-11-03 18:40:58 +0900

    feat: 구입 금액 입력 안내 문구 출력 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,13 @@
 package lotto.controller;
 
+import lotto.view.OutputView;
+
 public class LottoController {
+
+    public void lotto(){
+        readUserBudget();
+    }
+    public void readUserBudget(){
+        OutputView.printBudgetInputDescription();
+    }
 }
