1f531c282ed8e8ca6d0e1cd24c811658280dbf57
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-03 15:23:14 +0900

    refactor(LottoController): 메서드 길이가 15라인을 넘지 않도록 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -7,32 +7,47 @@ import lotto.view.OutputView;
 
 import java.util.List;
 
-import static lotto.constants.ViewMessage.PROFIT_RATE;
 import static lotto.constants.ViewMessage.WINNING_STATISTICS;
 import static lotto.view.InputView.readInput;
-
 
 public class LottoController {
 
     public void run() {
-        InputView.printPurchaseAmountPrompt();
-        int purchaseAmount = LottoPurchaseAmount.from(readInput()).getPurchaseAmount();
+        int purchaseAmount = getPurchaseAmount();
+        List<Lotto> lottos = getLottos(purchaseAmount);
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber(winningNumbers);
+        getWinningStatistics(lottos, winningNumbers, bonusNumber, purchaseAmount);
+    }
 
-        int lottoCount = LottoCount.from(purchaseAmount).getLottoCount();
-        List<Lotto> lottos = Lottos.from(lottoCount).getLottos();
-        OutputView.printLottoPurchaseCount(lottoCount);
-        OutputView.printLottoListPrompt(lottos);
-
-        InputView.printWinningNumbersPrompt();
-        List<Integer> winningNumbers = WinningNumbers.from(readInput()).getNumbers();
-
-        InputView.printBonusNumbersPrompt();
-        int bonusNumber = BonusNumber.of(winningNumbers, readInput()).getBonusNumber();
-
+    private void getWinningStatistics(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber, int purchaseAmount) {
         OutputView.printMessage(WINNING_STATISTICS.getText());
         List<LottoRankType> lottoRankTypes = LottoResult.of(lottos, winningNumbers, bonusNumber).getLottoRankTypes();
         String profitRate = LottoProfitCalculator.calculateProfitRate(lottoRankTypes, purchaseAmount);
         OutputView.printLottoProfitRate(profitRate);
+    }
+
+    private int getBonusNumber(List<Integer> winningNumbers) {
+        InputView.printBonusNumbersPrompt();
+        return BonusNumber.of(winningNumbers, readInput()).getBonusNumber();
+    }
+
+    private List<Integer> getWinningNumbers() {
+        InputView.printWinningNumbersPrompt();
+        return WinningNumbers.from(readInput()).getNumbers();
+    }
+
+    private List<Lotto> getLottos(int purchaseAmount) {
+        int lottoCount = LottoCount.from(purchaseAmount).getLottoCount();
+        List<Lotto> lottos = Lottos.from(lottoCount).getLottos();
+        OutputView.printLottoPurchaseCount(lottoCount);
+        OutputView.printLottoListPrompt(lottos);
+        return lottos;
+    }
+
+    private int getPurchaseAmount() {
+        InputView.printPurchaseAmountPrompt();
+        return LottoPurchaseAmount.from(readInput()).getPurchaseAmount();
     }
 
 }
