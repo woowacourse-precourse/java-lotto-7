6e033344f439f47d14d6053e7ab266b12afb4b69
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 23:29:45 +0900

    feat(LottoValidator) : 로또에 대한 검증 메서드 추가

---
+++ src/main/java/lotto/utils/LottoValidator.java
@@ -0,0 +1,36 @@
+package lotto.utils;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoValidator {
+
+    public static void validateLotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateNoDuplicates(numbers);
+        validateInRange(numbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private static void validateNoDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 값이 있습니다.");
+        }
+    }
+
+    private static void validateInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이여야 합니다.");
+            }
+        }
+    }
+
+}
