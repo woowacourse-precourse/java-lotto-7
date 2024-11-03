ed6fd0aaf55dc161e52a9ed5fd83713d66c90609
장시영 <sysmile77@gmail.com>
2024-11-04 03:18:01 +0900

    feat(Generate) : 랜덤숫자 발생

--- src/main/java/lotto/logic/Generate.java
+++ src/main/java/lotto/logic/Generate.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.logic;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
@@ -8,22 +9,24 @@ import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 public class Generate {
     final static String MESSAGE_GENERATE_AMOUNT = "개를 구매했습니다.";
 
-    public static void run(int n){
+    public static List<List<Integer>> run(int n){
 
         System.out.println(n+MESSAGE_GENERATE_AMOUNT);
-        List<Integer> randomNumbers = new ArrayList<>();
+        List<List<Integer>> lottoNumbersList = new ArrayList<>();
 
         for(int i=0; i<n; i++) {
-            randomNumbers=getRandom();
+            List<Integer> randomNumbers = getRandom();
+            Collections.sort(randomNumbers);
+            lottoNumbersList.add(randomNumbers);
             System.out.println(randomNumbers);
         }
 
-
+     return lottoNumbersList;
     }
 
     private static List<Integer> getRandom(){
 
-        return pickUniqueNumbersInRange(1,45,6);
+        return new ArrayList<>(pickUniqueNumbersInRange(1,45,6));
 
     }
 
