d583e44b0373a1407be679bc991947b548380e35
MinJae00 <exceedmin@gmail.com>
2024-11-04 02:29:30 +0900

    refact: 보너스 숫자 생성 로직을 한번에 처리

--- src/main/java/lotto/service/WininngNumberManager.java
+++ src/main/java/lotto/service/WininngNumberManager.java
@@ -3,38 +3,33 @@ package lotto.service;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.Bonus;
 import lotto.domain.Lotto;
 import lotto.repository.LottoRepository;
-import lotto.valuate.Valuate;
+import lotto.valuate.WinnerNumberValuate;
 
 public class WininngNumberManager {
-    private List<String> list;
-    private List<Integer> list_num;
-    Valuate valuate = new Valuate();
-    LottoRepository lottoRepository;
-    private Lotto lotto;
-
-    public void createWinningNumber() {
-        lotto = new Lotto(list_num);
-        lottoRepository.saveWinningNumbers(lotto);
+    public void createWinningNumber(String[] inputWinningNumber, LottoRepository lottoRepository) {
+        List<Integer> WinningNumber = parsingWinningInput(inputWinningNumber);
+        lottoRepository.saveWinningNumbers(new Lotto(WinningNumber));
     }
 
-    private void parsingWinningInput() {
-        list_num = list.stream()
+    private List<Integer> parsingWinningInput(String[] inputWinningNumbers) {
+        List<Integer> list_num = Arrays.asList(inputWinningNumbers).stream()
                 .map(String::trim)
                 .map(this::evaluateWinningNumber)
                 .collect(Collectors.toList());
+        return list_num;
     }
 
-    private int evaluateWinningNumber(String s) {
-        valuate.isNum(s);
-        return Integer.parseInt(s);
+    private int evaluateWinningNumber(String inputWinningNumber) {
+        WinnerNumberValuate.isValidNumber(inputWinningNumber);
+        return Integer.parseInt(inputWinningNumber);
     }
 
-    public WininngNumberManager(String s, LottoRepository lottoRepository) {
-        this.lottoRepository = lottoRepository;
-        list = Arrays.asList(s.split(","));
-        parsingWinningInput();
+    public void createBonusNumber(int bonusNumber, LottoRepository lottoRepository) {
+        Bonus bonus = new Bonus(bonusNumber, lottoRepository.getWinningNumbers());
+        lottoRepository.saveBonusNumbers(bonus);
     }
 
 }
