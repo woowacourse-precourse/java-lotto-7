c9088f8cee4cba670d90cb42c7b0362c4c130e49
Park, Sojeong <sojeong2101@gmail.com>
2024-11-03 17:08:54 +0900

    feat($Lotto): 당첨 숫자의 중복과 범위 부합 검사 기능 구현
    1. 당첨 숫자 중복 시 예외 처리
    2. 1~45의 숫자가 아닐 경우 예외 처리

---
+++ src/main/java/lotto/statistics/Lotto.java
@@ -0,0 +1,35 @@
+package lotto.statistics;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        checkDuplicateNumber(numbers);
+        checkLottoRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void checkDuplicateNumber(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+        if (distinctCount < numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void checkLottoRange(List<Integer> numbers) {
+        for (Integer rawNumber : numbers) {
+            if (!LottoBall.isInRange(rawNumber)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45만 가능합니다.");
+            }
+        }
+    }
+}
