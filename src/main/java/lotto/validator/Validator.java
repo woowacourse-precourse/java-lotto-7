dce44c927a6034e3f5a7270ffcc6e73873b92036
joon <khjoon372@naver.com>
2024-11-03 22:47:17 +0900

    feat(Validator, ValidatorTest): 당첨 번호가 1 ~ 45가 아닌 경우 예외 처리

--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -1,5 +1,7 @@
 package lotto.validator;
 
+import java.util.List;
+
 public class Validator {
 
     public static void validateLottoAmountIsPositiveAndDivisibleByThousand(int lottoAmount){
@@ -8,5 +10,12 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 로또 금액은 1000원 단위로 입력해 주세요.");
         }
 
+    }
+
+    public static void validateLottoNumberInRangeOneToFortyFive(List<Integer> lottoWinningNumbers){
+
+        if (!lottoWinningNumbers.stream().allMatch(number -> number >= 1 && number <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호를 1~45 사이로 입력해 주세요.");
+        }
     }
 }
--- src/test/java/lotto/validator/ValidatorTest.java
+++ src/test/java/lotto/validator/ValidatorTest.java
@@ -3,6 +3,7 @@ package lotto.validator;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,5 +19,16 @@ class ValidatorTest {
         // when & then
         assertThatThrownBy(() -> Validator.validateLottoAmountIsPositiveAndDivisibleByThousand(invalidAmount))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호가_1에서_45_사이가_아닌_꼉우_예외_테스트(){
+        //given
+        List<Integer> lottoWinningNumbers = Arrays.asList(8, 21, 23, 41, 42, 46);
+
+        //when & then
+        assertThatThrownBy(() -> Validator.validateLottoNumberInRangeOneToFortyFive(lottoWinningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
