aa68dd7ca631c575697da1f7972e2ff08e1b435a
ruudska6 <feels19@naver.com>
2024-11-03 16:54:51 +0900

    <feat> InputView 로또 구입 금액을 사용자로부터 입력 받는 lottoMoneyInput() 작성과 예외처리

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,27 @@
+package lotto.view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static final String ASK_MONEY_INPUT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_MONEY_NOT_NUMBER = "[ERROR] 입력하신 금액이 양의 정수가 아닙니다.";
+
+
+    public int lottoMoneyInput() {
+        System.out.printf(ASK_MONEY_INPUT);
+        String rawMoney = Console.readLine();
+        validateIsNumber(rawMoney);
+        return Integer.parseInt(rawMoney);
+    }
+
+    private void validateIsNumber(String rawValue) {
+        try {
+            Integer.parseInt(rawValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_MONEY_NOT_NUMBER);
+        }
+    }
+
+
+
+
+}
