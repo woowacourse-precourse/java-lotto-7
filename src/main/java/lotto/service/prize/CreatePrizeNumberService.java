0083bce04a67ad79464f62d2129fb379edaf8758
plan11plan <tjtfy7028@gmail.com>
2024-11-03 15:53:41 +0900

    refactor(PrizeWriteService): 내부 생성 조립 메서드 클래스 분리
    -  CreatePrizeNumberService로 PrizeNumber 생성 책임 분리
    
    메서드 길이가 8줄이가 넘어갔기 때문

---
+++ src/main/java/lotto/service/prize/CreatePrizeNumberService.java
@@ -0,0 +1,18 @@
+package lotto.service.prize;
+
+import lotto.domain.prize.BonusNumber;
+import lotto.domain.prize.PrizeNumber;
+import lotto.domain.prize.WinnerNumbers;
+import lotto.domain.ticket.Lotto;
+
+public class CreatePrizeNumberService {
+
+    public PrizeNumber execute(int bonusNum, Lotto winLotto) {
+        BonusNumber createdBonusNum = BonusNumber.of(bonusNum, winLotto);
+        WinnerNumbers createdWinNums = WinnerNumbers.of(winLotto);
+
+        PrizeNumber prizeNumber = PrizeNumber.of(createdWinNums, createdBonusNum);
+
+        return prizeNumber;
+    }
+}
--- src/main/java/lotto/service/prize/PrizeWriteService.java
+++ src/main/java/lotto/service/prize/PrizeWriteService.java
@@ -1,32 +1,31 @@
 package lotto.service.prize;
 
 import java.util.List;
-import lotto.domain.prize.BonusNumber;
 import lotto.domain.prize.Prize;
 import lotto.domain.prize.PrizeNumber;
-import lotto.domain.prize.WinnerNumbers;
 import lotto.domain.ticket.Lotto;
 import lotto.repository.prize.PrizeWriteRepository;
 import lotto.service.IdGenerator;
 
 public class PrizeWriteService {
+
+    private final CreatePrizeNumberService createPrizeNumberService;
     private final PrizeWriteRepository prizeWriteRepository;
     private final IdGenerator idGenerator;
 
-    public PrizeWriteService(PrizeWriteRepository prizeWriteRepository, IdGenerator idGenerator) {
+    public PrizeWriteService(CreatePrizeNumberService createPrizeNumberService,
+                             PrizeWriteRepository prizeWriteRepository,
+                             IdGenerator idGenerator) {
+        this.createPrizeNumberService = createPrizeNumberService;
         this.prizeWriteRepository = prizeWriteRepository;
         this.idGenerator = idGenerator;
     }
 
     public Long create(int bonusNum, List<Integer> winNums) {
         Lotto winLotto = Lotto.of(winNums);
+        PrizeNumber prizeNumber = createPrizeNumberService.execute(bonusNum, winLotto);
 
-        BonusNumber createdBonusNum = BonusNumber.of(bonusNum, winLotto);
-        WinnerNumbers createdWinNums = WinnerNumbers.of(winLotto);
-        PrizeNumber createdPrizeNum = PrizeNumber.of(createdWinNums, createdBonusNum);
-
-        Prize createdPrize = Prize.of(idGenerator.generate(), createdPrizeNum);
-
+        Prize createdPrize = Prize.of(idGenerator.generate(), prizeNumber);
         Long savedId = prizeWriteRepository.save(createdPrize);
 
         return savedId;
