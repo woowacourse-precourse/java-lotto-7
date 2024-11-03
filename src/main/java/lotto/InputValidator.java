a270d8c4278331f7be06f27a85c4fd2534fbfc44
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 00:23:46 +0900

    feat(InputValidator): 쉼표로 구분된 숫자 리스트 검증 기능 구현

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -9,4 +9,16 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
     }
+
+    public void validateNumbersByComma(String numbers) {
+        for(char ch : numbers.toCharArray()) {
+            if(isNotNumbersCondition(ch)) {
+                throw new IllegalArgumentException("[ERROR] 쉼표로 구분해서 숫자를 입력해주세요");
+            }
+        }
+    }
+
+    private boolean isNotNumbersCondition(char ch) {
+        return !Character.isDigit(ch) && ch != ',';
+    }
 }
