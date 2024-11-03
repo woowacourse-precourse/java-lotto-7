af22d0f291391a1c5b516be355c7a9207c2d4c0d
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:57:02 +0900

    feat: Lottos 클래스 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,4 +1,37 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lotto.model.numbergenerator.NumberGenerator;
+
 public class Lottos {
+
+    private List<Lotto> lottos = new ArrayList<>();
+    private final NumberGenerator numberGenerator;
+    private final Trial trial;
+
+    public Lottos(NumberGenerator numberGenerator, Trial trial) {
+        this.numberGenerator = numberGenerator;
+        this.trial = trial;
+        makeLottos();
+    }
+
+    public void makeLottos() {
+        for (int i = 0; i < trial.getTrial(); i++) {
+            lottos.add(new Lotto(numberGenerator.generateNumber()));
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public List<List<Integer>> getLottoNums() {
+        List<List<Integer>> lottoNums = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            lottoNums.add(lotto.getNumbers());
+        }
+        return lottoNums;
+    }
 }
