c8a1b399e5a0471c782aa51065908e5b6b79c869
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 04:12:50 +0900

    refactor(Lotto): 로또에 중복되는 번호가 없는지 검증하는 메서드 추가
    - 일급 컬렉션 Lotto에서 사용하는 검증 메서드는 공통적으로 사용할 메서드이기 때문에 따로 검증 클래스로 분리하지 않고 구현

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,9 +1,12 @@
 package lotto.Domain;
 
+import static lotto.Util.Error.ErrorMessage.DUPLICATE_LOTTO_NUMBER;
 import static lotto.Util.Error.ErrorMessage.INVALID_LOTTO_NUMBER_COUNT;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,6 +19,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNT.getMessage());
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
