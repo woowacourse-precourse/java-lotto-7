9a337b4bcbb2f5e66c6851d1b52a05143616e2aa
capDoYeonLee <ghtyu21@naver.com>
2024-11-04 04:35:03 +0900

    feat: 사용자의 수익률을 계산하는 객체 개발

---
+++ src/main/java/lotto/CalculateRevenue.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.Map;
+
+public class CalculateRevenue {
+    private final Map<Float, Integer> rateOfReturn;
+    private final int totalSpent;
+    private final int percentage = 100;
+
+    public CalculateRevenue(Map<Float, Integer> rateOfReturn, int totalSpent) {
+        this.rateOfReturn = rateOfReturn;
+        this.totalSpent = totalSpent;
+        printRevenue();
+    }
+
+    private void printRevenue() {
+        float totalWinnings = calculateTotalWinnings();
+        float revenue = calculateRevenue(totalWinnings);
+
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", revenue);
+    }
+
+    private float calculateTotalWinnings() {
+        float totalWinnings = 0;
+        for (Map.Entry<Float, Integer> entry : rateOfReturn.entrySet()) {
+            float prize = entry.getKey();
+            int count = entry.getValue();
+            totalWinnings += prize * count;
+        }
+        return totalWinnings;
+    }
+
+    private float calculateRevenue(float totalWinnings) {
+        return (totalWinnings / totalSpent) * percentage;
+    }
+}
