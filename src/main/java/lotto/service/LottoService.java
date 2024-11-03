d77681d018b9edc9a2d871ea4a5ed5a94daf9bf3
lucku <lsw6731@naver.com>
2024-11-03 17:20:16 +0900

    feat: LottoService 클래스 추가 및 로또 핵심 로직 구현
    - 로또 구매 기능
    - 로또 생성 기능
    - 당첨 결과 계산 및 저장 기능
    - 수익률 계산 기능

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,61 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.constant.Rank;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.LottoBuyAmount;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningLotto;
+
+import static lotto.constant.LottoConstants.LOTTO_SIZE;
+import static lotto.constant.LottoConstants.MIN_NUMBER;
+import static lotto.constant.LottoConstants.MAX_NUMBER;
+
+public class LottoService {
+    private final List<Lotto> lottos;
+
+    public LottoService() {
+        this.lottos = new ArrayList<>();
+    }
+
+    public List<Lotto> buyLotto(LottoBuyAmount amount) {
+        for (int i = 0; i < amount.getLottoBuyCount(); i++) {
+            lottos.add(new Lotto(pickRandomNumbers()));
+        }
+        return lottos;
+    }
+
+    public LottoResult calculateResult(WinningLotto winningLotto, BonusNumber bonusNumber) {
+        LottoResult result = new LottoResult();
+
+        for (Lotto lotto : lottos) {
+            int matchCount = winningLotto.countMatchingNumbers(lotto);
+            boolean hasBonus = bonusNumber.containsBonusNumber(lotto);
+            record(result, matchCount, hasBonus);
+        }
+        return result;
+    }
+
+    public double calculateProfitRate(LottoResult lottoResult) {
+        return (double) lottoResult.getTotalWinnings() / (lottos.size() * 1000) * 100;
+    }
+
+    private List<Integer> pickRandomNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                MIN_NUMBER,
+                MAX_NUMBER,
+                LOTTO_SIZE
+        );
+    }
+
+    private void record(LottoResult result, int matchCount, boolean hasBonus) {
+        Rank rank = Rank.getRank(matchCount, hasBonus);
+
+        if (rank.getMatchCount() != 0) {
+            result.recordMatch(rank);
+        }
+    }
+}
