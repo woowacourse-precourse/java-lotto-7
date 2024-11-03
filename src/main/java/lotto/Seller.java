7fcbc15f7295610aeaa2bae25bf7e248e1f3cf22
Leegeonwoo7 <workgeon07@naver.com>
2024-11-04 03:11:22 +0900

    feat(Seller): Seller 구현
    로또를 판매하는 역할을 수행

--- src/main/java/lotto/Seller.java
+++ src/main/java/lotto/Seller.java
@@ -1,2 +1,21 @@
-package lotto;public class Seller {
+package lotto;
+
+import lotto.constant.NumberType;
+
+public class Seller {
+    private LottoMachine lottoMachine;
+
+    public Seller(LottoMachine lottoMachine) {
+        this.lottoMachine = lottoMachine;
+    }
+
+    public void sellLottoTo(Person person) {
+        long buyQuantity = getBuyQuantity(person.pay());
+        Lottos lottos = lottoMachine.publishLottos(buyQuantity);
+        person.setLottos(lottos);
+    }
+
+    private long getBuyQuantity(Money money) {
+        return money.getAmount() / NumberType.LOTTO_PRICE.getPrice();
+    }
 }
