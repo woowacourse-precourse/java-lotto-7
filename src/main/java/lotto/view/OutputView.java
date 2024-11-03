ad99ff3a11155e9893c4b816b7b77a47ccadd44f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-04 00:46:18 +0900

    feat(view/OutputView): 로또 결과 및 통계를 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,14 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 
+import lotto.model.LottoRank;
 import lotto.model.LottoTicket;
 
 public class OutputView {
 	private static final String TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+	private static final String STATISTICS_HEADER = "당첨 통계\n---";
 
 	public static void displayPurchaseCount(int ticketCount) {
 		System.out.println(ticketCount + TICKET_COUNT_MESSAGE);
@@ -15,5 +18,17 @@ public class OutputView {
 		for (LottoTicket ticket : tickets) {
 			System.out.println(ticket.getNumbers());
 		}
+	}
+
+	public static void displayResult(Map<LottoRank, Integer> resultCount) {
+		System.out.println(STATISTICS_HEADER);
+		for (LottoRank rank : LottoRank.values()) {
+			int count = resultCount.getOrDefault(rank, 0);
+			System.out.printf("%s - %d개%n", rank.getDescription(), count);
+		}
+	}
+
+	public static void displayTotalPrize(int totalPrize) {
+		System.out.printf("총 상금: %,d원%n", totalPrize);
 	}
 }
