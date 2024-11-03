1a7489cdfdb14429170fb2b8145ac99e0730cd25
taek2222 <oot309@daum.net>
2024-11-04 02:18:18 +0900

    feat(OutputView): 당첨 결과 및 수익률 출력 추가
    - 당첨 결과 리스트 및 수익률 출력 부분을 추가했습니다.
    
    - 수익률 출력은 소수점 둘째 자리에서 반올림하고 천 단위에 표시할 수 있도록 포맷했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,9 +2,12 @@ package lotto.view;
 
 import static lotto.constant.MessageConstant.*;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
+
+    private static final String PROFIT_RATE_FORMAT = "#,##0.0";
 
     public void printTicketCount(int count) {
         System.out.println();
@@ -17,5 +20,18 @@ public class OutputView {
             System.out.printf(OUTPUT_PURCHASE_TICKET_INFO.getMessage(), ticket);
             System.out.println();
         });
+    }
+
+    public void printWinningStatistics(List<String> results) {
+        System.out.printf(NEWLINE.getMessage());
+        System.out.println(OUTPUT_WINNING_STATISTICS_HEADER.getMessage());
+        System.out.println(OUTPUT_WINNING_STATISTICS_SEPARATOR.getMessage());
+
+        results.forEach(System.out::println);
+    }
+
+    public void printProfitRate(double profitRate) {
+        DecimalFormat decimalFormat = new DecimalFormat(PROFIT_RATE_FORMAT);
+        System.out.printf(OUTPUT_PROFIT_RATE.getMessage(), decimalFormat.format(profitRate));
     }
 }
