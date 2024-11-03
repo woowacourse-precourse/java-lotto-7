21cfa9bf94d00ca46e90d615f2db529d36189fa1
mikekks <thdalsrb79@naver.com>
2024-11-03 16:46:31 +0900

    feat(BonusNumber): 보너스 번호 관련 로직 구현
    1. 보너스 번호 저장
    2. 보너스 번호 범위 검증

---
+++ src/main/java/lotto/BonusNumber.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static lotto.global.ErrorMessage.*;
+import static lotto.global.LottoConstant.*;
+
+public class BonusNumber {
+	private final int number;
+
+	public BonusNumber(int number) {
+		validateLottoNumber(number);
+		this.number = number;
+	}
+
+	private void validateLottoNumber(int number) {
+		if (number < LOTTO_MINIMUM_NUMBER || number > LOTTO_MAXIMUM_NUMBER) {
+			throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE.getMessage());
+		}
+	}
+
+	public int getNumber(){
+		return number;
+	}
+}
--- src/main/java/lotto/global/ErrorMessage.java
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.global;
 public enum ErrorMessage {
 
 	MONEY_AMOUNT_ERROR("[ERROR] 0원 이하는 입력할 수 없습니다."),
-	MONEY_UNIT_ERROR("[ERROR] 1000원 단위로 입력할 수 있습니다.");
+	MONEY_UNIT_ERROR("[ERROR] 1000원 단위로 입력할 수 있습니다."),
+	INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1~45 까지만 가능합니다.");
 
 	ErrorMessage(String message) {
 		this.message = message;
