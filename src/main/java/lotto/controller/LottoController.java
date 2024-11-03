8c1e90764f48c17b4eab24b0604709ccac0ba815
Seungwan Yoo <ysw99@kakao.com>
2024-11-03 20:56:05 +0900

    feat: 당첨 번호, 보너스 번호 입력 및 MyLotto 객체 생성 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,11 @@
 package lotto.controller;
 
 import lotto.domain.LottoGame;
+import lotto.domain.MyLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
     private final InputView inputView;
@@ -16,6 +19,8 @@ public class LottoController {
     public void run() {
         LottoGame lottoGame = createNewLottoGame();
         outputView.renderGeneratedLottos(lottoGame);
+
+        MyLotto myLotto = createMyLotto();
     }
 
     private LottoGame createNewLottoGame() {
@@ -28,6 +33,29 @@ public class LottoController {
         } catch (IllegalArgumentException e) {
             outputView.renderErrorMessage(e.getMessage());
             return getPurchaseAmount();
+        }
+    }
+
+    private MyLotto createMyLotto() {
+        final List<Integer> winningNumbers = getWinningNumbers();
+        return new MyLotto(winningNumbers, getBonusNumber(winningNumbers));
+    }
+
+    private List<Integer> getWinningNumbers() {
+        try {
+            return inputView.getWinningNumbersInput();
+        } catch (IllegalArgumentException e) {
+            outputView.renderErrorMessage(e.getMessage());
+            return getWinningNumbers();
+        }
+    }
+
+    private Integer getBonusNumber(final List<Integer> winningNumbers) {
+        try {
+            return inputView.getBonusNumberInput(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            outputView.renderErrorMessage(e.getMessage());
+            return getBonusNumber(winningNumbers);
         }
     }
 }
