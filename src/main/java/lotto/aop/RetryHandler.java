d1709ceb29f609fb9d0ffdb9556689286ba11148
이창근 <dlckdrms0517@gmail.com>
2024-11-03 21:51:34 +0900

    feat: RetryHandler 재시도 기능 작성
    - IllegalArgumentException에 대해서만 재시도하고, 나머지의 경우에는 던집니다.

--- src/main/java/lotto/aop/RetryHandler.java
+++ src/main/java/lotto/aop/RetryHandler.java
@@ -1,4 +1,30 @@
 package lotto.aop;
 
+import lotto.io.OutputHandler;
+
 public class RetryHandler {
+
+    private final OutputHandler outputHandler;
+
+    public RetryHandler(OutputHandler outputHandler) {
+        this.outputHandler = outputHandler;
+    }
+
+    public <T> T tryUntilSuccess(final IllegalArgumentExceptionThrower<T> thrower) {
+        while (true) {
+            try {
+                return thrower.run();
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputHandler.handleExceptionMessage(illegalArgumentException);
+            } catch (Exception exception) {
+                outputHandler.handleExceptionMessage(exception);
+                throw exception;
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface IllegalArgumentExceptionThrower<T> {
+        T run() throws IllegalArgumentException;
+    }
 }
