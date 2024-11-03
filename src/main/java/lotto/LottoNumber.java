7e7e70cc5efce79bb29b11d3a3828f5ea90c61fb
장영재 <urinaner@naver.com>
2024-11-03 14:55:27 +0900

    feat(lottoNumber): record로 변경

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,17 +1,16 @@
 package lotto;
 
-public class LottoNumber {
-    private final static int MIN_LOTTO_NUMBER = 1;
-    private final static int MAX_LOTTO_NUMBER = 45;
-    private final static String OUT_OF_RANGE = "로또번호는 1~45의 범위입니다.";
-    private int lottoNumber;
+public record LottoNumber(int lottoNumber) {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final String OUT_OF_RANGE = "로또번호는 1~45의 범위입니다.";
 
-    public LottoNumber(int lottoNumber) {
+    public LottoNumber {
         validateLottoNumber(lottoNumber);
-        this.lottoNumber = lottoNumber;
     }
+
     private void validateLottoNumber(int lottoNumber) {
-        if (lottoNumber < MIN_LOTTO_NUMBER | lottoNumber > MAX_LOTTO_NUMBER) {
+        if (lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(OUT_OF_RANGE);
         }
     }
