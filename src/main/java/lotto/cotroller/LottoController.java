f209699ed3f7e3f99986501904c4e38215b81fe1
peanut990 <peanut1998@naver.com>
2024-11-04 01:22:38 +0900

    feat(controller): 로또 게임 진행 기능

---
+++ src/main/java/lotto/cotroller/LottoController.java
@@ -0,0 +1,71 @@
+package lotto.cotroller;
+
+import static lotto.utils.Parser.parseBonusNum;
+import static lotto.utils.Parser.parseMoney;
+import static lotto.utils.Parser.parseWinningNumbs;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoRevenueCalculator;
+import lotto.service.LottoWinningChecker;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+public class LottoController {
+    private final LottoInputView lottoInputView;
+    private final LottoOutputView lottoOutputView;
+    private final LottoGenerator lottoGenerator;
+    private final LottoWinningChecker lottoWinningChecker;
+    private final LottoRevenueCalculator lottoRevenueCalculator;
+
+    public LottoController(LottoInputView lottoInputView, LottoOutputView lottoOutputView,
+                           LottoGenerator lottoGenerator, LottoWinningChecker lottoWinningChecker,
+                           LottoRevenueCalculator lottoRevenueCalculator) {
+        this.lottoInputView = lottoInputView;
+        this.lottoOutputView = lottoOutputView;
+        this.lottoGenerator = lottoGenerator;
+        this.lottoWinningChecker = lottoWinningChecker;
+        this.lottoRevenueCalculator = lottoRevenueCalculator;
+    }
+
+    public void run() {
+        int money = requestMoney();
+        List<Lotto> lottos = createLottos(money);
+
+        requestWinningNumbs();
+        requestBonusNumb();
+
+        response(lottos, money);
+    }
+
+    private int requestMoney() {
+        String inputMoney = lottoInputView.inputMoney();
+        int money = parseMoney(inputMoney);
+        return money;
+    }
+
+    private List<Lotto> createLottos(int money) {
+        List<Lotto> lottos = lottoGenerator.generateMany(money);
+        lottoOutputView.printLottos(lottos);
+        return lottos;
+    }
+
+    private void requestWinningNumbs() {
+        String inputWinningNums = lottoInputView.inputWinningNums();
+        List<Integer> winningNumbs = parseWinningNumbs(inputWinningNums);
+        lottoWinningChecker.saveWinningNumbs(winningNumbs);
+    }
+
+    private void requestBonusNumb() {
+        String inputBonusNum = lottoInputView.inputBonusNum();
+        int bonusNum = parseBonusNum(inputBonusNum);
+        lottoWinningChecker.saveBonusNumber(bonusNum);
+    }
+
+    private void response(List<Lotto> lottos, int money) {
+        int[] checkRanks = lottoWinningChecker.checkRanks(lottos);
+        float rateOfRevenue = lottoRevenueCalculator.calculateRateOfRevenue(checkRanks, money);
+        lottoOutputView.printResult(checkRanks, rateOfRevenue);
+    }
+}
