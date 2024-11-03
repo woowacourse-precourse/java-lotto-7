c3eacfe90b102c7bf1234b73b2a3f10803bf3070
seungchan60 <157793981+seungchan60@users.noreply.github.com>
2024-11-03 21:27:24 +0900

    fix: 유효성 검사 및 수정 가능한 리스트로 변경 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,7 @@
 package lotto;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.HashSet;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,12 +15,17 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
-
-    // TODO: 추가 기능 구현
 
     public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(numbers);
+        return numbers;
     }
-
 }
