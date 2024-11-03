3fdf508079402a00daba1cfc7552a5c0a6d41fdd
kkmdevel <kkm.devel@gmail.com>
2024-11-03 14:56:54 +0900

    feat(Payment): 결제 금액 검증 로직 추가
    - 0 이하의 금액 입력 시 예외 발생하도록 검증 로직 추가
    - 결제 금액이 로또 금액 단위로 나누어지지 않을 경우 예외 발생

--- src/main/java/lotto/domain/Payment.java
+++ src/main/java/lotto/domain/Payment.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.exception.PaymentException;
+import lotto.exception.message.PaymentExceptionMessage;
+
 public class Payment {
 
     private static final int LOTTO_PRICE = 1000;
@@ -11,6 +14,7 @@ public class Payment {
     }
 
     public static Payment of(int money){
+        validateMoney(money);
         return new Payment(calculateLottoCount(money));
     }
 
@@ -18,6 +22,22 @@ public class Payment {
         return this.lottoCount;
     }
 
+    private static void validateMoney(int money) {
+        validateIsPositiveNumber(money);
+        validateDivisibleByLottoUnit(money);
+    }
+
+    private static void validateIsPositiveNumber(int money){
+        if (money <= 0) {
+            throw new PaymentException(PaymentExceptionMessage.NOT_POSITIVE_NUMBER);
+        }
+    }
+
+    private static void validateDivisibleByLottoUnit(int money){
+        if (money % LOTTO_PRICE != 0) {
+            throw new PaymentException(PaymentExceptionMessage.CANT_DIVIDE);
+        }
+    }
 
     private static int calculateLottoCount(int money) {
         return money / LOTTO_PRICE;
