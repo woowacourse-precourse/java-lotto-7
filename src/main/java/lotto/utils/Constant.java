a96edea84f27a9d49f6b5d7c82d57fa7f8e57410
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:20:13 +0900

    feat (Constant) : 새로운 상수 추가

--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -4,6 +4,8 @@ public class Constant {
     public static final int LOTTO_PRICE = 1000;
     public static final int MINIMUM_PRICE = 0;
     public static final String COMMA = ",";
+    public static final int LOTTO_SIZE = 6;
+
 
     private Constant() {
     }
