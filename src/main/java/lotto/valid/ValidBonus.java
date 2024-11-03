20debaa391ef47c1ce0933e84c88cacd5b64684f
mingking2 <mingee641@gmail.com>
2024-11-03 16:42:49 +0900

    feat: 보너스 검증 클래스 추가

---
+++ src/main/java/lotto/valid/ValidBonus.java
@@ -0,0 +1,22 @@
+package lotto.valid;
+
+import java.util.List;
+import lotto.config.exception.bonus.BonusException;
+import lotto.config.exception.bonus.BonusExceptionMessage;
+
+
+public class ValidBonus {
+
+    public static void checkInputDuplicate(List<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
+            throw new BonusException(BonusExceptionMessage.HAS_DUPLICATE_BONUS_NUMBER);
+        }
+    }
+
+    public static void checkRangeNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new BonusException(BonusExceptionMessage.NOT_RANGE_BONUS_NUMBER);
+        }
+    }
+
+}
