c578618c72366e22dbaeed846c76678bcf4654d9
imjunghun <dlawndgns9@naver.com>
2024-11-03 18:33:24 +0900

    refactor(Rank): 랭크를 찾는 메서드 BiPredicate 함수형 인터페이스로 구현
    - match 메서드를 Java API BiPredicate를 이용해 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,43 +1,53 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum Rank {
 
-    NONE(0, 0, false),
-    FIFTH(3, 5_000, false),
-    FOURTH(4, 50_000, false),
-    THIRD(5, 1_500_000, false),
-    SECOND(5, 30_000_000, true),
-    FIRST(6, 2_000_000_000, false);
+    NONE(0,
+            0,
+            false,
+            (count, isBonus) -> count < 3),
+    FIFTH(3,
+            5_000,
+            false,
+            (count, isBonus) -> count.equals(3)),
+    FOURTH(4,
+            50_000,
+            false,
+            (count, isBonus) -> count.equals(4)),
+    THIRD(5,
+            1_500_000,
+            false,
+            (count, isBonus) -> count.equals(5) && !isBonus),
+    SECOND(5,
+            30_000_000,
+            true,
+            (count, isBonus) -> count.equals(5) && isBonus),
+    FIRST(6,
+            2_000_000_000,
+            false,
+            (count, isBonus) -> count.equals(6));
 
     private final int correctCount;
     private final int prize;
     private final boolean hasBonus;
+    private final BiPredicate<Integer, Boolean> predicate;
 
-    Rank(int correctCount, int prize, boolean hasBonus) {
+    Rank(int correctCount, int prize, boolean hasBonus,
+         BiPredicate<Integer, Boolean> predicate) {
         this.correctCount = correctCount;
         this.prize = prize;
         this.hasBonus = hasBonus;
+        this.predicate = predicate;
     }
 
     public static Rank find(int count, boolean isBonus) {
         return Arrays.stream(values())
-                .filter(rank -> rank.match(count, isBonus))
+                .filter(rank -> rank.predicate.test(count, isBonus))
                 .findFirst()
                 .orElse(NONE);
-    }
-
-    private boolean match(int count, boolean isBonus) {
-        if (this == FIRST) {
-            return count == this.correctCount;
-        }
-
-        if (this == SECOND || this == THIRD) {
-            return count == this.correctCount && isBonus == this.hasBonus;
-        }
-
-        return count == this.correctCount;
     }
 
     public long calculatePrize(int count) {
