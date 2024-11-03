e43d241da8e1c4d73c5719e9e766c9ad553df290
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 22:13:47 +0900

    :necktie: feat : printResult 메서드 추가
    - calculateProfitRate 메서드 추가
    - 당첨 통계 및 수익률 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
 
 public class OutputView {
 
@@ -10,4 +13,20 @@ public class OutputView {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 
+    public static void printResult(LottoResult result, int purchaseAmount){
+        System.out.println("당첨 통계\n---");
+        Map<LottoRank, Integer> rankCount = result.getRankCount();
+        rankCount.forEach((rank, count) -> {
+            if (rank != LottoRank.NONE){
+                System.out.printf("%d개 일치 (%d)원 - %d개%n",
+                        rank.getMatchCount(), rank.getPrize(), count);
+            }
+        });
+        double profitRate = calculateProfitRate(result.getTotalPrize(), purchaseAmount);
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
+
+    public static double calculateProfitRate(long totalPrize, int purchaseAmount){
+        return ((double) totalPrize / purchaseAmount) * 100;
+    }
 }
