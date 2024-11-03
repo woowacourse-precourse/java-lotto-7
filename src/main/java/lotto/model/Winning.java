4cf927a1fee145d9e1ee0976bd67cd0db6efcc51
Chan531 <chan657312@gmail.com>
2024-11-03 20:45:25 +0900

    feat(Winning): 당첨 정보 추가

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
 public enum Winning {
+	FIFTH(3, 5000, false),
+	FOURTH(4, 50000, false),
+	THIRD(5, 1500000, false),
+	SECOND(5, 30000000, true),
+	FIRST(6, 2000000000, false);
 
 	private final int count;
 	private final int prize;
