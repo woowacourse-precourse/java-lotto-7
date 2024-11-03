4ac4f4efa8ff9c0aa3e5bb500c417f8dbfa40472
CHISANW <keuye06380618@gmail.com>
2024-11-03 17:14:30 +0900

    feat(ErrorMessage): 예외메시지 클래스 추가
    - 예외메시지를 매직상수를 통해 사용하도록 클래스 추가

---
+++ src/main/java/lotto/vaildate/ErrorMessage.java
@@ -0,0 +1,13 @@
+package lotto.vaildate;
+
+public class ErrorMessage {
+    
+    public final static String INPUT_AMOUNT_IN_THOUSANDS = "[ERROR] 천원단위로 입력해주세요";
+    public final static String ONLY_NUMERIC_INPUT_ALLOWED = "[ERROR] 숫자 형식으로만 입력해주세여";
+    public final static String NUMBER_ONE_TO_FORTY_FIVE = "[ERROR] 당첨 번호는 1과 45 사이여야 합니다.";
+    public final static String UNIQUE_NUMBER = "[ERROR] 당첨 번호는 중복된 될수 없습니다.";
+    public final static String INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public final static String INVALID_BONUS_NUMBER = "[ERROR] 올바른 보너스 번호를 입력해주세요.";
+    public final static String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+}
