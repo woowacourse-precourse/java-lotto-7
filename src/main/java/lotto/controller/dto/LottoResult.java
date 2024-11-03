5d475cf41bf0b4b05d88d28c8fe23abf303f77a5
gikhoon <ghikhoon@naver.com>
2024-11-04 03:30:02 +0900

    feat: 로또 결과 변수명 변경

--- src/main/java/lotto/controller/dto/LottoResult.java
+++ src/main/java/lotto/controller/dto/LottoResult.java
@@ -4,9 +4,9 @@ import java.util.List;
 import lotto.model.enums.Rank;
 
 public record LottoResult(
-        List<Rank> lottoResult
+        List<Rank> ranks
 ) {
-    public LottoResult(List<Rank> lottoResult) {
-        this.lottoResult = lottoResult;
+    public LottoResult(List<Rank> ranks) {
+        this.ranks = ranks;
     }
 }
