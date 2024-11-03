ce899cd82d0d133e30dd74a8ae58e9883b5666d9
USER <cji513@naver.com>
2024-11-03 23:38:50 +0900

    feat(LottoMachine): 당첨 내역 계산 메소드 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,7 +2,10 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class LottoMachine {
@@ -16,9 +19,40 @@ public class LottoMachine {
         int quantity = price / LOTTO_PRICE;
 
         return Stream.generate(
-                        () -> new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT)))
+                () -> new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT)))
                 .limit(quantity)
                 .toList();
+    }
+
+    public Map<Integer, Integer> calculateStats(List<Lotto> lottos, List<Integer> winningNumber, int bonusNumber) {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+
+        for (Lotto lotto : lottos) {
+            List<Integer> lottoNumber = lotto.numbers();
+            int sameCount = compareWinningNumber(lottoNumber, winningNumber, bonusNumber);
+
+            updateMap(resultMap, sameCount);
+        }
+
+        return resultMap;
+    }
+
+    private int compareWinningNumber(List<Integer> lottoNumber, List<Integer> winningNumber, int bonusNumber) {
+        int sameCount = (int) lottoNumber.stream()
+                .filter(n -> winningNumber.stream()
+                .anyMatch(Predicate.isEqual(n)))
+                .count();
+
+        if (sameCount == 5 && lottoNumber.contains(bonusNumber) || sameCount == 6) {
+            return sameCount + 1;
+        }
+
+        return sameCount;
+    }
+
+    private void updateMap(Map<Integer, Integer> resultMap, int sameCount) {
+        int prize = 8 - sameCount;
+        resultMap.merge(prize, 1, Integer::sum);
     }
 
 }
