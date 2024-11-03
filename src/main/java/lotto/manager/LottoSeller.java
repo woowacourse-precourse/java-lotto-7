c91561bd0eb7318eb469dcf24098493c235ca3c6
HyeonJeong <wgwjh05169@gmail.com>
2024-11-03 19:13:24 +0900

    feat(LottoSeller): 로또 판매

--- src/main/java/lotto/manager/LottoSeller.java
+++ src/main/java/lotto/manager/LottoSeller.java
@@ -1,6 +1,11 @@
 package lotto.manager;
 
+import lotto.domain.Lotto;
+import lotto.domain.Player;
 import lotto.util.LottoNumberSupplier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoSeller {
     private static final int LOTTO_PRISE = 1000;
@@ -8,6 +13,19 @@ public class LottoSeller {
 
     public LottoSeller(LottoNumberSupplier lottoNumberSupplier) {
         this.lottoNumberSupplier = lottoNumberSupplier;
+    }
+
+    public Player sell(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        int count = calculateLottoCount(money);
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumbers = lottoNumberSupplier.getLottoNumbers();
+            lottos.add(new Lotto(lottoNumbers));
+        }
+
+        return new Player(lottos, money);
     }
 
     private int calculateLottoCount(int money) {
