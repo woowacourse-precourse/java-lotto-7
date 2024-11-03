45df0ae193b7025b25eb8d7bd616467438d29c09
이준범 <jach1206@naver.com>
2024-11-03 21:33:47 +0900

    feat(domain): 보너스 번호와 당첨 번호가 중복될 시 예외 처리

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.HashSet;
+import java.util.Set;
 import lotto.exception.BonusNumberErrorMessage;
 
 public class BonusNumber {
@@ -7,14 +9,20 @@ public class BonusNumber {
     private final int number;
 
     public BonusNumber(String number) {
-        int userInput = validateNumber(number);
-        validateRange(userInput);
-
+        int userInput = validate(number);
         this.number = userInput;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    private int validate(String number) {
+        int result = validateNumber(number);
+        validateRange(result);
+        validateDuplicate(result);
+
+        return result;
     }
 
     private int validateNumber(String number) {
@@ -28,6 +36,14 @@ public class BonusNumber {
     private void validateRange(int number) {
         if (number < 1 || number > 45) {
             throw new IllegalArgumentException(BonusNumberErrorMessage.IS_NOT_POSSIBLE_RANGE.getMessage());
+        }
+    }
+
+    private void validateDuplicate(int number) {
+        Set<Integer> winningNumbers = new HashSet<>(WinningNumbers.getInstance().getWinningNumbers());
+
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException(BonusNumberErrorMessage.IS_DUPLICATED_NUMBER.getMessage());
         }
     }
 }
--- src/main/java/lotto/exception/BonusNumberErrorMessage.java
+++ src/main/java/lotto/exception/BonusNumberErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.exception;
 
 public enum BonusNumberErrorMessage {
     IS_NOT_POSSIBLE_RANGE("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
-    IS_NOT_NUMBER("[ERROR] 숫자가 아닌 값이 입력되었거나, 허용 범위를 벗어난 값이 입력되었습니다.");
+    IS_NOT_NUMBER("[ERROR] 숫자가 아닌 값이 입력되었거나, 허용 범위를 벗어난 값이 입력되었습니다."),
+    IS_DUPLICATED_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
 
     private final String message;
 
