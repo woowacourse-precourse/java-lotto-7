e69c4421d07bac605c8da78d2a8a3911d6bbcec1
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:34:06 +0900

    :sparkles: feat(OutputHandler): 출력 관련 메시지 제공

--- src/main/java/lotto/handler/OutputHandler.java
+++ src/main/java/lotto/handler/OutputHandler.java
@@ -1,4 +1,10 @@
 package lotto.handler;
 
 public class OutputHandler {
+    public static final String OUTPUT_LOTTO_TICKETS_COUNT = "\n%d개를 구매했습니다.";
+    public static final String OUTPUT_WINNING_STATISTIC = "\n당첨 통계\n---";
+
+    public static final String OUTPUT_LOTTO_RANK = "%d개 일치 (%,d원) - %d개\n";
+    public static final String OUTPUT_LOTTO_RANK_BONUS = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    public static final String OUTPUT_LOTTO_PROFIT = "총 수익률은 %.1f%%입니다.\n";
 }
