26bace6a2159389e83919bc3efc72ae17e108584
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-03 13:09:13 +0900

    feat: Rank Enum 정의 및 등수별 상금 설정

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
+
+    Rank(int matchCount, boolean bonusMatch, int prize) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+}
