19af380118307a8c73ca42dd56a2b5f173fd7185
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-03 22:26:42 +0900

    feat(domain): 보너스 번호 검증 기능
    - 보너스 번호의 범위 검증

--- src/main/java/lotto/model/domain/BonusNumber.java
+++ src/main/java/lotto/model/domain/BonusNumber.java
@@ -4,7 +4,17 @@ public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        bonusNumberRangeValidator(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
+    private void bonusNumberRangeValidator(int bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
