89fa0302f2002040aa0553371c4a64c270e5ee21
changuii <rhljh7410@gmail.com>
2024-11-03 21:16:39 +0900

    refactor : Application의 의존성을 WoowaLottoControllerFactory로 옮겨라
    - 프리코스 미션의 조건을 만족하는 LottoController를 생성해주는 팩토리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,22 +1,11 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.enums.LottoConfig;
-import lotto.handler.RetryHandler;
-import lotto.view.LottoInputParser;
-import lotto.view.LottoInputValidator;
-import lotto.view.LottoInputView;
-import lotto.view.LottoOutputView;
+import lotto.factory.WoowaLottoControllerFactory;
 
 public class Application {
     public static void main(String[] args) {
-        LottoConfig config = LottoConfig.WOOWA_CONFIG;
-        LottoController controller = new LottoController(
-                new LottoInputView(new LottoInputValidator(), new LottoInputParser(config)),
-                new LottoOutputView(),
-                new RetryHandler(),
-                config
-        );
+        LottoController controller = WoowaLottoControllerFactory.create();
         controller.run();
     }
 }
---
+++ src/main/java/lotto/factory/WoowaLottoControllerFactory.java
@@ -0,0 +1,21 @@
+package lotto.factory;
+
+import lotto.controller.LottoController;
+import lotto.enums.LottoConfig;
+import lotto.handler.RetryHandler;
+import lotto.view.LottoInputParser;
+import lotto.view.LottoInputValidator;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+public class WoowaLottoControllerFactory {
+    private static final LottoConfig CONFIG = LottoConfig.WOOWA_CONFIG;
+
+    public static LottoController create(){
+        LottoInputView lottoInputView = new LottoInputView(new LottoInputValidator(), new LottoInputParser(CONFIG));
+        LottoOutputView lottoOutputView = new LottoOutputView();
+        RetryHandler retryHandler = new RetryHandler();
+
+        return new LottoController(lottoInputView, lottoOutputView, retryHandler, CONFIG);
+    }
+}
