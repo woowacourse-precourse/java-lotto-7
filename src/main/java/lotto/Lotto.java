7d29f74908d68eaa6f1cc1cbcd80318878eff34a
moeun9309 <moeun9309@gmail.com>
2024-11-04 08:49:47 +0900

    feat: purchase count calculator

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +18,14 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private List<Integer> pickNewLotto() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private int purchaseCalculate(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원으로 나누어 떨어져야 합니다.");
+        }
+        return money / 1000;
+    }
 }
