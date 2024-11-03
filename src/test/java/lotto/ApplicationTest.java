b3b8499c97d01069a7441d410dfd93fb9ecab698
soeun2537 <soeun2537@gmail.com>
2024-11-03 13:24:58 +0900

    test: 재입력 로직 추가에 따른 통합 테스트 수정

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -53,9 +52,8 @@ class ApplicationTest extends NsTest {
     @DisplayName("예외_테스트_로또_구입_금액_문자열")
     void 예외_테스트() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("1000j"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
@@ -220,261 +218,240 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트_로또_구입_금액_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("a", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("a", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_특수_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("$", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("$", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_빈_값() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_공백() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException(" ", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException(" ", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_중간에_공백() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("1 000", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("1 000", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_개행_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("\n", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("\n", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_음수() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("-4000", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("-4000", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_로또_구입_금액_배수_아님() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("2500", "1,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("2500", "1,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_당첨_번호_리스트_아님() {
+        assertSimpleTest(() -> {
+            runException("4000", "12", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_문자_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,a,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,a,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_특수_문자_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,$,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,$,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_빈_값_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_공백_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2, ,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2, ,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_중간에_공백_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3 3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3 3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_개행_문자_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,\n,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,\n,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_음수_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,-3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,-3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_중복된_번호_포함() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,6,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,6,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_범위_초과() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,46", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,46", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_범위_미달() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "0,2,3,4,5,6", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "0,2,3,4,5,6", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_개수_초과() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6,7", "8"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6,7", "8");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_당첨_번호_개수_미달() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5", "7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "a"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "a");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_특수_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "$"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "$");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_공백() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", " "))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", " ");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_중간에_공백() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "7 8"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "7 8");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_개행_문자() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "\n"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "\n");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_음수() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "-7"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "-7");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_당첨_번호와_중복() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "6"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "6");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_범위_초과() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "46"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "46");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
     void 예외_테스트_보너스_번호_범위_미달() {
         assertSimpleTest(() -> {
-            assertThatThrownBy(() -> runException("4000", "1,2,3,4,5,6", "0"))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining(ERROR_MESSAGE);
+            runException("4000", "1,2,3,4,5,6", "0");
+            assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
