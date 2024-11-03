0dbc1cdda9e9af9dc32c51bfc3fddd7a3817a605
tentenball <lth8905@gmail.com>
2024-11-04 03:26:20 +0900

    style: 코드 공백 삭제

--- src/main/java/lotto/service/LottoResultFactory.java
+++ src/main/java/lotto/service/LottoResultFactory.java
@@ -10,7 +10,6 @@ public class LottoResultFactory {
 	public Map<LottoPrize, Integer> generateLottoResult() {
 		Map<LottoPrize, Integer> initialPrizeMap = new LinkedHashMap<>();
 
-
 		for (LottoPrize prize : LottoPrize.values()) {
 			initialPrizeMap.put(prize, 0);
 		}
