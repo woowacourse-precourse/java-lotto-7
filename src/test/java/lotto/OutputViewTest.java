3ae3786c0b1037e1195f49b9af1954f996e9fb20
jinwook <wjswlsdnr333@gmail.com>
2024-11-03 17:16:00 +0900

    test(OutputViewTest): 발행한 로또 출력 테스트

---
+++ src/test/java/lotto/OutputViewTest.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class OutputViewTest{
+    @Test
+    public void 로또_출력_테스트() {
+        String input = "8000\n1,2,3,4,5,6\n7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        outputView.generateLotto(inputView.getLottoCount());
+        inputView.inputStart(outputView);
+
+        List<List<Integer>> outputLottoNumbers = outputView.getBoughtLottoNumbers();
+
+        assertEquals(8000, inputView.getLottoPurchase());
+        assertEquals(outputLottoNumbers.size(),8);
+        assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, inputView.getLottoNumbers().toArray(new Integer[0]));
+        assertEquals(7, inputView.getBonusNumber());
+    }
+}
