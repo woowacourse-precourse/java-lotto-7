bafda1f53648165a3cafcf86c07cee7db25d9d26
김성민 <klg1425@gmail.com>
2024-11-04 00:03:46 +0900

    test: LottoTest 테스트 케이스 추가
    테스트케이스 3개추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,8 @@ package lotto;
 import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 import java.util.List;
 
@@ -20,6 +22,29 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("당첨 번호 입력시 1~45 숫자가 아닐시 예외처리 ")
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 1과 45 사이의 숫자여야 합니다.");
+    }
+    @DisplayName("로또 번호는 1부터 45 사이의 6개 고유 번호로 구성")
+    @Test
+    void 로또_번호_생성_테스트() {
+        Lotto lotto = new Lotto(List.of(3, 11, 22, 28, 33, 44));
+        assertThat(lotto.lottoNumbers()).hasSize(6);
+        assertThat(lotto.lottoNumbers()).doesNotHaveDuplicates();
+        assertThat(lotto.lottoNumbers()).allMatch(number -> number >= 1 && number <= 45);
+    }
+    @DisplayName("당첨 번호와의 매칭 개수 계산")
+    @Test
+    void 당첨_번호와_매칭_개수_테스트() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+        int matchCount = lotto.countMatchingNumbers(winningNumbers);
+        assertThat(matchCount).isEqualTo(3);
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
