b8b9d55805bcc7747241c3546c2084c465ae42de
Seungwan Yoo <ysw99@kakao.com>
2024-11-03 20:51:28 +0900

    feat: 당첨 번호, 보너스 번호 입력 관련 예외 메시지 추가

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -3,6 +3,9 @@ package lotto.exception;
 public enum LottoException {
     NUMBER_FORMAT_NOT_VALID("[ERROR] 숫자만 입력 가능합니다."),
     PURCHASE_AMOUNT_NOT_VALID("[ERROR] 구입 금액은 1,000원 단위여야 합니다."),
+    LOTTO_LENGTH_NOT_VALID("[ERROR] 로또 번호는 6자리로 입력해주세요."),
+    LOTTO_RANGE_NOT_VALID("[ERROR] 로또 번호는 1~45 사이만 입력 가능합니다."),
+    BONUS_NUMBER_DUPLICATED("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.")
     ;
 
     private final String message;
