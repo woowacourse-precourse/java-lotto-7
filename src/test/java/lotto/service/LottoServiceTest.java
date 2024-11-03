150bf14fd526b1703f496a7516d715579cef0a49
gahyeon <1000ji1@naver.com>
2024-11-03 17:21:05 +0900

    test: 구입한 개수만큼 로또 뽑는지 확인 테스트

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -2,16 +2,33 @@ package lotto.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LottoServiceTest {
+    private LottoService lottoService;
+
+    @BeforeEach
+    void setUp() {
+        lottoService = new LottoService();
+    }
 
     @Test
     void 구입_개수_테스트() {
         int purchaseAmount = 8000;
 
-        LottoService lottoService = new LottoService();
         assertThat(lottoService.getPurchaseCount(purchaseAmount))
                 .isEqualTo(8);
     }
+
+    @Test
+    void 구입한_개수만큼_로또_번호_발행() {
+        int purchaseCount = 8;
+        List<Lotto> lottos = lottoService.makeLottos(purchaseCount);
+
+        assertThat(lottos.size()).isEqualTo(purchaseCount);
+    }
+
 }
