1e15b0ffff54510dcab4b0ba9bf57e9914d7445c
Jewan1120 <hawaii1468@gmail.com>
2024-11-03 14:54:45 +0900

    refactor(Service): Randoms 라이브러리에 있는 함수를 이용하여 기존 함수를 대체

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,6 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.enums.Constants;
@@ -11,15 +10,9 @@ public class LottoService {
     static final String DELIMITER = ",";
 
     public List<Integer> getLottoNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < Constants.LOTTO_NUMBER_SIZE.getValue()) {
-            int number = Randoms.pickNumberInRange(Constants.LOTTO_LOWER_BOUND.getValue(),
-                    Constants.LOTTO_UPPER_BOUND.getValue());
-            if (!numbers.contains(number)) {
-                numbers.add(number);
-            }
-        }
-        return numbers;
+        return Randoms.pickUniqueNumbersInRange(Constants.LOTTO_LOWER_BOUND.getValue(),
+                                                Constants.LOTTO_UPPER_BOUND.getValue(),
+                                                Constants.LOTTO_NUMBER_SIZE.getValue());
     }
 
     public List<Integer> getWinningNumbers(String inputWinningNumbers) {
