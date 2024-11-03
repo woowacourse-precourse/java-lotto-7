7f12376d880dfd88c8bfaa9ca58c882bf03e3359
SeongbinPark <hyucse22@gmail.com>
2024-11-03 13:15:31 +0900

    test(LottoTest): 로또 번호가 6개가 아닌 경우 예외가 발생한다.

---
+++ src/test/java/lotto/unit/LottoTest.java
@@ -0,0 +1,22 @@
+package lotto.unit;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.constants.ErrorMessages;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("로또 번호가 6개가 아닌 경우 예외가 발생한다.")
+    void lottoNumbersSizeNotSix() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_COUNT);
+    }
+}
