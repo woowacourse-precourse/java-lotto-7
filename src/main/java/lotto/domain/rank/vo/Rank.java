fdf354616112362408536abb7c55430a25848854
minSsan <msp1125@hanyang.ac.kr>
2024-11-03 18:37:38 +0900

    feat(Rank): rank 필드 삭제

--- src/main/java/lotto/domain/rank/vo/Rank.java
+++ src/main/java/lotto/domain/rank/vo/Rank.java
@@ -5,19 +5,17 @@ import java.util.Comparator;
 import java.util.List;
 
 public enum Rank {
-    FIRST(1, 6, 0, 2_000_000_000),
-    SECOND(2, 5, 1, 30_000_000),
-    THIRD(3, 5, 0, 1_500_000),
-    FOURTH(4, 4, 0, 50_000),
-    FIFTH(5, 3, 0, 5_000);
+    FIRST(6, 0, 2_000_000_000),
+    SECOND(5, 1, 30_000_000),
+    THIRD(5, 0, 1_500_000),
+    FOURTH(4, 0, 50_000),
+    FIFTH(3, 0, 5_000);
 
-    private final int rank;
     private final int match;
     private final int bonus;
     private final int prize;
 
-    private Rank(int rank, int match, int bonus, int prize) {
-        this.rank = rank;
+    private Rank(int match, int bonus, int prize) {
         this.match = match;
         this.bonus = bonus;
         this.prize = prize;
@@ -36,9 +34,8 @@ public enum Rank {
 
     public static List<Rank> getSortedRanks() {
         return Arrays.stream(values())
-                .sorted(Comparator.comparingInt(value -> value.rank))
-                .toList()
-                .reversed();
+                .sorted(Comparator.comparingInt(rank -> rank.prize))
+                .toList();
     }
 
     @Override
