2205d3cc2adb04d0f5442490fc0bf2516e37782a
33jyu33 <jy04169@gmail.com>
2024-11-03 12:57:25 +0900

    fix(WinningNumber): add a bonus number field
    Not only the winning number but also the bonus number is treated as an object

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class WinningNumber {
     private final List<Integer> numbers;
+    private final Integer bonus;
 
-    public WinningNumber(List<Integer> numbers) {
+    public WinningNumber(List<Integer> numbers, Integer bonus) {
         this.numbers = numbers;
+        this.bonus = bonus;
     }
 
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    public Integer getBonus(){
+        return this.bonus;
     }
 }
