4324eaa3fdceb7b27ff6b4557ebc714448dbbcc9
JoonKyo <joonkyolee@gmail.com>
2024-11-03 23:47:00 +0900

    feat(Profit): 수익률을 처리하는 클래스와 수익률을 계산하는 메서드 구현

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -1,2 +1,27 @@
-package lotto.domain;public class Profit {
+package lotto.domain;
+
+public class Profit {
+    private final Money money;
+    private final LottoResult lottoResult;
+
+    private Profit(Money money, LottoResult lottoResult) {
+        this.money = money;
+        this.lottoResult = lottoResult;
+    }
+
+    public static Profit of(Money money, LottoResult lottoResult) {
+        return new Profit(money, lottoResult);
+    }
+
+    public double calculateProfit() {
+        return (double) calculateTotalPrize() / money.getMoney() * 100;
+    }
+
+    private int calculateTotalPrize() {
+        return lottoResult.calculateRankDistribution()
+                .entrySet()
+                .stream()
+                .mapToInt(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum();
+    }
 }
