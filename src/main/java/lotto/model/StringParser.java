a1504821bb9de8b69ef5a56be55ad42eeadb4653
eunseobb <seobseob0810@naver.com>
2024-11-04 02:45:00 +0900

    feat(model): 입력한 로또 번호 검증 및 정수 변환 메서드 추가
    StringParser에서 입력한 로또 번호를 쉼표로 분리하고 적절한 숫자인지 검증하는 메서드 추가

--- src/main/java/lotto/model/StringParser.java
+++ src/main/java/lotto/model/StringParser.java
@@ -1,14 +1,39 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static lotto.constant.ErrorMessage.*;
 
 public class StringParser {
-    static final int UNIT_AMOUNT = 1000;
+    private static final int UNIT_AMOUNT = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public Integer findLottoCount(String rawAmount) {
         int amount = validateInteger(rawAmount);
-        int lottoCount = validatePositive(amount);
-        return validateAmount(lottoCount);
+        isPositive(amount);
+        return validateAmount(amount);
+    }
+
+    public List<Integer> findMyNumbers(String rawNumbers) {
+        List<String> splitNumbers = Arrays.stream(rawNumbers.split(","))
+            .map(String::trim)
+            .toList();
+
+        List<Integer> myNumbers = validateMyNumbers(splitNumbers);
+
+        return myNumbers;
+    }
+
+    private List<Integer> validateMyNumbers(List<String> splitNumbers) {
+        List<Integer> myNumbers = splitNumbers.stream().map(this::validateInteger)
+            .filter(this::isPositive)
+            .filter(this::validateRange)
+            .toList();
+
+        return myNumbers;
     }
 
     private int validateInteger(String rawAmount) {
@@ -19,11 +44,11 @@ public class StringParser {
         }
     }
 
-    private int validatePositive(int amount) {
+    private boolean isPositive(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException(NEGATIVE_ERROR.getMessage());
         }
-        return amount;
+        return true;
     }
 
     private int validateAmount(int amount) {
@@ -32,4 +57,12 @@ public class StringParser {
         }
         throw new IllegalArgumentException(THOUSANDS_ERROR.getMessage());
     }
+
+    private boolean validateRange(int number) {
+        if (number >= MIN_NUMBER && number <= MAX_NUMBER) {
+            return true;
+        }
+        throw new IllegalArgumentException(RANGE_ERROR.getMessage());
+    }
+
 }
