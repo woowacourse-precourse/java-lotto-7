666a870508a75a27da5b1aea8ae8d6cb2d959953
GO-TE <alsrudxo9810@naver.com>
2024-11-03 18:07:46 +0900

    feat(OutputMessage): InputView 상수 메세지 열거형 구현

---
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public enum OutputMessage {
+    INPUT_BUDGET("구입금액을 입력해 주세요."),
+    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
