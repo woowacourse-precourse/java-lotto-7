2e6ec5b0a6ee87869fc4e06d6dd75cfca4cf5bcf
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 17:48:40 +0900

    feat: 구입 금액이 1,000원 단위로 나누어 떨어지는지 검증하는 기능 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -14,4 +14,10 @@ public class PurchaseAmount {
             throw new IllegalArgumentException("구매 금액은 0 이상이어야 합니다.");
         }
     }
+
+    private void validateDivisibility(int purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("구매 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.");
+        }
+    }
 }
