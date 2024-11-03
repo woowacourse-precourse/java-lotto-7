64b20f0ea6ea7ad4c7b32e859646c02960b4e39d
dongri <l0o0lv@github.com>
2024-11-04 00:08:10 +0900

    refactor: 구입금액 입력 시 유효성 검증 메소드를 별도의 PurchaseAmountValidator 로 이동

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -1,36 +1,16 @@
 package lotto.model;
 
+import lotto.validator.PurchaseAmountValidator;
+
 public class PurchaseAmount {
-    private static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_MAX_PRICE = 100000; //실제 복권과 동일하도록 1회 구입 10만원 제한
+    public static final int LOTTO_PRICE = 1000;
     private final int lottoCount;
     private final int amount;
 
     public PurchaseAmount(int amount) {
-        validateAmount(amount);
-        this. amount = amount;
+        PurchaseAmountValidator.validateAmount(amount);
+        this.amount = amount;
         this.lottoCount = calculateLottoCount(amount);
-    }
-
-    private void validateAmount(int amount) {
-        validateMinPrice(amount);
-        validateMaxPrice(amount);
-        validateDivisibility(amount);
-    }
-
-    private void validateMinPrice(int amount){
-        if(amount < LOTTO_PRICE)
-            throw new IllegalArgumentException();
-    }
-
-    private void validateMaxPrice(int amount){
-        if(amount > LOTTO_MAX_PRICE)
-            throw new IllegalArgumentException();
-    }
-
-    private void validateDivisibility(int amount){
-        if(amount % LOTTO_PRICE != 0)
-            throw new IllegalArgumentException();
     }
 
     private int calculateLottoCount(int amount) {
---
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -0,0 +1,28 @@
+package lotto.validator;
+
+import static lotto.model.PurchaseAmount.LOTTO_PRICE;
+
+public class PurchaseAmountValidator {
+    private static final int LOTTO_MAX_PRICE = 100000; //실제 복권과 동일하도록 1회 구입 10만원 제한
+
+    public static void validateAmount(int amount) {
+        validateMinPrice(amount);
+        validateMaxPrice(amount);
+        validateDivisibility(amount);
+    }
+
+    private static void validateMinPrice(int amount){
+        if(amount < LOTTO_PRICE)
+            throw new IllegalArgumentException();
+    }
+
+    private static void validateMaxPrice(int amount){
+        if(amount > LOTTO_MAX_PRICE)
+            throw new IllegalArgumentException();
+    }
+
+    private static void validateDivisibility(int amount){
+        if(amount % LOTTO_PRICE != 0)
+            throw new IllegalArgumentException();
+    }
+}
