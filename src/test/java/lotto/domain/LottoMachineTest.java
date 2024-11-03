e21b41077eba31913f9ce1aaaaf5f6291a215c30
Yeongjae Kim <sees77770@gmail.com>
2024-11-04 03:22:31 +0900

    test: LottoMachine으로 로또 생성 테스트
    LottoMachine으로 로또를 생성하므로 테스트를 수정한다.

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -6,15 +6,16 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LottoTicketTest {
+class LottoMachineTest {
 
     @DisplayName("여러 개의 로또를 생성한다.")
     @Test
-    void newLottosTest() {
-        int lottoCount = 2;
+    void generateLottosTest() {
+        int lottoCount = 3;
         NumbersGenerator fixedNumbersGenerator = () -> List.of(1, 2, 3, 4, 5, 6);
+        LottoMachine lottoMachine = new LottoMachine(lottoCount, fixedNumbersGenerator);
 
-        LottoTicket lottoTicket = new LottoTicket(lottoCount, fixedNumbersGenerator);
+        LottoTicket lottoTicket = new LottoTicket(lottoMachine.generateLottos());
 
         lottoTicket.getLottos().forEach(lotto ->
                 assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6))
