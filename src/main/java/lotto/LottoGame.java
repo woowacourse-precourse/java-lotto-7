c7bf817e6cd0d5ac66bf86a07b1292a6b89b1c18
mete0rfish <sungwon326@naver.com>
2024-11-03 18:47:07 +0900

    feat(domain): LottoGame 모든 기능 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,21 +1,35 @@
 package lotto;
 
 import lotto.view.LottoInput;
+import lotto.view.LottoOutput;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static lotto.RandomLottoGenerator.*;
 
 public class LottoGame {
 
     public void run() {
         LottoCost lottoCost = creatLottoCost();
-        CorrectLotto correctLotto = createCorrectLotto();
 
+        int lottoCount = lottoCost.divideCostByUnit();
+        List<Lotto> myLottos = createMyLottos(lottoCount);
+
+        LottoOutput.outputLottoNumbers(lottoCost, myLottos);
+
+        CorrectLotto correctLotto = createCorrectLotto();
+        RankCalculator rankCalculator = calculateLottoRanks(myLottos, correctLotto);
+        LottoOutput.outputRankResult(rankCalculator);
     }
 
     private LottoCost creatLottoCost() {
         try {
-            int cost = LottoInput.inputCost();
-            return new LottoCost(cost);
+            String input = LottoInput.inputCost();
+            return LottoCost.valueOf(input);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return creatLottoCost();
@@ -36,5 +50,22 @@ public class LottoGame {
             System.out.println(e.getMessage());
             return createCorrectLotto();
         }
+    }
+
+    private List<Lotto> createMyLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>(count);
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(generateSixRandomNumber()));
+        }
+
+        return lottos;
+    }
+
+    private RankCalculator calculateLottoRanks(List<Lotto> lottos, CorrectLotto correctLotto) {
+        List<Rank> ranks = lottos.stream()
+                .map(correctLotto::match)
+                .toList();
+        return RankCalculator.from(ranks);
     }
 }
