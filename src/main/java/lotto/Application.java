21ff9f6d17db7ae341d93bc09ef9f0954b8a7a96
un-known0 <nam0221@naver.com>
2024-11-04 00:17:05 +0900

    feat(Application): 구입 금액 입력 및 유효성 검증 기능 추가
    - 유효한 구입 금액이 입력될 때까지 반복 입력 받는 inputPrice 메서드 구현
    - 잘못된 입력 시 에러 메시지를 출력하고 재입력 요청
    - 입력 값이 숫자가 아닐 경우 예외 처리 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
+
+    static LottoMachine machine = new LottoMachine();
+
+    public static int parseInt(String s){
+        try{
+            return Integer.parseInt(s);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    public static List<Lotto> inputPrice(){
+        System.out.println("구입금액을 입력해 주세요.");
+        while(true){
+            try{
+                return machine.buyLottos(parseInt(Console.readLine()));
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] "+e.getMessage());
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Lotto> lottos = inputPrice();
+
+
     }
 }
