84d8244231707df5110768210cdacb39c5a478c2
CHISANW <keuye06380618@gmail.com>
2024-11-04 05:33:06 +0900

    refactor(ErrorMessage): 오타 수정
    - 숫자형식의 매직상수의 오타를 수정

--- src/main/java/lotto/vaildate/ErrorMessage.java
+++ src/main/java/lotto/vaildate/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.vaildate;
 public class ErrorMessage {
     
     public final static String INPUT_AMOUNT_IN_THOUSANDS = "[ERROR] 천원단위로 입력해주세요";
-    public final static String ONLY_NUMERIC_INPUT_ALLOWED = "[ERROR] 숫자 형식으로만 입력해주세여";
+    public final static String ONLY_NUMBER_INPUT_ALLOWED = "[ERROR] 숫자 형식으로만 입력해주세여";
     public final static String NUMBER_ONE_TO_FORTY_FIVE = "[ERROR] 당첨 번호는 1과 45 사이여야 합니다.";
     public final static String UNIQUE_NUMBER = "[ERROR] 당첨 번호는 중복된 될수 없습니다.";
     public final static String INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
