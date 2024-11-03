3183bafd0962508ae55945b8514c49dbf3662782
gahyeon <1000ji1@naver.com>
2024-11-03 20:05:12 +0900

    feat: 6개의 당첨 번호를 쉼표 기준으로 입력했는지 확인

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,13 +1,17 @@
 package lotto.utils;
 
+import java.util.List;
+
 public class Validator {
     private static final int DIVIDED_AMOUNT = 1000;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    public static final int LOTTO_LENGTH = 6;
 
     public static final String INVALID_NUMBER_MESSAGE = "[ERROR] 숫자 입력하셔야 합니다.";
     public static final String INVALID_AMOUNT_MESSAGE = "[ERROR] 1000원 단위로 입력하셔야 합니다.";
     public static final String INVALID_NUMBER_RANGE_MESSAGE = "[ERROR] 1~45 사이의 숫자여야합니다.";
+    public static final String INVALID_NUMBER_LENGTH_MESSAGE = "[ERROR] 로또 번호 6개 입력하셔야 합니다.";
 
     private Validator() {
     }
@@ -29,6 +33,12 @@ public class Validator {
     public static void numberInRange(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(INVALID_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    public static void numbersLength(List<String> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NUMBER_LENGTH_MESSAGE);
         }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,40 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.utils.Validator;
 
 public class InputView {
     private static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public static int purchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT);
         String amount = read();
         int parseAmount = parseAndValidateAmount(amount);
         return parseAmount;
+    }
+
+    public static void winningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS);
+        String numbers = read();
+
+        String cleanedNumbers = trimSpaces(numbers);
+        List<String> splitNumbers = splitByComma(cleanedNumbers);
+
+        Validator.numbersLength(splitNumbers);
+    }
+
+
+    private static List<String> splitByComma(String cleanedNumbers) {
+        return Stream.of(cleanedNumbers.split(","))
+                .collect(Collectors.toList());
+    }
+
+    private static String trimSpaces(String numbers) {
+        return numbers.replaceAll("\\s+", "");
     }
 
     private static int parseAndValidateAmount(String amount) {
