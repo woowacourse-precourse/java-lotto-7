605ea43cf18dafee60d74944864c6670a3d9010d
Leegeonwoo7 <workgeon07@naver.com>
2024-11-04 03:12:35 +0900

    feat(Person): Person 구현
    로또를 구매하는 역할을 수행

--- src/main/java/lotto/Person.java
+++ src/main/java/lotto/Person.java
@@ -1,2 +1,22 @@
-package lotto;public class Person {
+package lotto;
+
+public class Person {
+    private Lottos lottos;
+    private Money money;
+
+    public Person(Money money) {
+        this.money = money;
+    }
+
+    public Lottos getLottos() {
+        return lottos;
+    }
+
+    public void setLottos(Lottos lottos) {
+        this.lottos = lottos;
+    }
+
+    public Money pay() {
+        return money;
+    }
 }
