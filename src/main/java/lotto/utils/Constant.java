06e9521fba9579d932a2b1195dd7810ff04ffff5
sleepyhoon <pokjm99@gmail.com>
2024-11-04 02:30:13 +0900

    feat (Constant) : LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX 추가

--- src/main/java/lotto/utils/Constant.java
+++ src/main/java/lotto/utils/Constant.java
@@ -6,6 +6,8 @@ public class Constant {
     public static final String COMMA = ",";
     public static final int LOTTO_SIZE = 6;
 
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
 
     private Constant() {
     }
