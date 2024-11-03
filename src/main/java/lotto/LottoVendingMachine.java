70efc745c83e6f8139d8b41b0c749f6bebd9bc6c
taehyun <xogus5070318@gmail.com>
2024-11-03 16:29:44 +0900

    feat(LottoVendingMachine): 로또 구입금액 입력 예외처리 추가
    로또 구입금액 입력 부분에서 다음과 같은 요구사항을 충족시키기 위해 예외처리를 추가하였습니다.
    1. 로또 구입금액은 숫자로 입력받아야 한다.
    2. 로또 구입금액은 1000원으로 나누어 떨어져야 한다.
    3. 로또는 최소 1장부터, 최대 50장까지 구매 가능하다.
    
    BREAKING CHANGE: None

--- src/main/java/lotto/LottoVendingMachine.java
+++ src/main/java/lotto/LottoVendingMachine.java
@@ -2,26 +2,56 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoVendingMachine {
 
-    public List<Lotto> purchaseLottos(){
+    public List<Lotto> purchaseLottos() {
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = Console.readLine();
-        validateMoney(userInput);
+
+        while (true) {
+            if (validateMoney(userInput)) break;
+        }
+
         int money = Integer.parseInt(userInput);
         List<Lotto> lottos = new ArrayList<>();
 
-        for(int i=1;i<=(money/1000);i++){
+        for (int i = 1; i <= (money / 1000); i++) {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
 
         return lottos;
     }
 
-    public void validateMoney(String userInput){
+    public boolean validateMoney(String userInput) {
 
+        try {
+            int money = Integer.parseInt(userInput);
+
+            if(money < 1000){
+                throw new IllegalArgumentException("로또는 최소 1장 이상 구매해야 합니다.");
+            }
+
+            if(money > 50000){
+                throw new IllegalArgumentException("로또는 최대 50장까지 구매해야 합니다.");
+            }
+
+            if(money % 1000 != 0){
+                throw new IllegalArgumentException("로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.");
+            }
+
+
+            return true;
+
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 유효한 숫자를 입력해야 합니다.");
+            return false;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] "+e);
+            return false;
+        }
     }
 }
