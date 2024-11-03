137714741cbd4ff562c887e9fe4b80b5d00cbab7
taek2222 <oot309@daum.net>
2024-11-04 07:54:26 +0900

    refactor(TicketCount): 티켓 객체 검증 추가
    - 티켓 카운터 객체에 금액을 받는 걸 String 으로 변경했다.
    
    - 테스트 결과 2,000 이런식으로 입력 시 통과되는 문제점을 해결하기 위해 변경했다.

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     INVALID_ZERO_AMOUNT("구입금액은 0원일 수 없습니다."),
     INVALID_AMOUNT_UNIT("구입금액의 단위는 1,000원 단위입니다."),
     INVALID_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_BONUS_NUMBER_COUNT("보너스 번호는 1개의 숫자만 입력 가능합니다"),
+    INVALID_NUMBER_PARSE("입력하신 숫자가 유효하지 않습니다, 단일 숫자만 입력 가능합니다"),
     BONUS_NUMBER_ALREADY_IN_WINNING_NUMBERS("해당 보너스 번호는 당첨 번호에 입력되어 있습니다"),
     DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다.");
 
--- src/main/java/lotto/domain/TicketCount.java
+++ src/main/java/lotto/domain/TicketCount.java
@@ -1,21 +1,28 @@
 package lotto.domain;
 
-import static lotto.domain.PurchaseAmount.*;
+import static lotto.constant.LottoConstant.LOTTO_PRICE;
+import static lotto.util.LottoValidator.parseNumber;
 
 public class TicketCount {
 
+    private final PurchaseAmount amount;
     private final Integer count;
 
-    public TicketCount(int inputAmount) {
-        PurchaseAmount purchaseAmount = new PurchaseAmount(inputAmount);
-        this.count = calculateTicket(purchaseAmount);
+    public TicketCount(String purchaseAmount) {
+        int parseNumber = parseNumber(purchaseAmount);
+        this.amount = new PurchaseAmount(parseNumber);
+        this.count = calculateTicket(amount);
     }
 
     public Integer getCount() {
         return count;
     }
 
+    public Integer getAmount() {
+        return amount.amount();
+    }
+
     private int calculateTicket(PurchaseAmount purchaseAmount) {
-        return purchaseAmount.getAmount() / LOTTO_PRICE;
+        return purchaseAmount.amount() / LOTTO_PRICE.getValue();
     }
 }
