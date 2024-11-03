53d6cfa92256b8ace1834d752a7414b1acf509a5
scc9811 <wjsrudtjr98@naver.com>
2024-11-04 08:12:54 +0900

    refactor : run 메서드의 역할 분산
    기존의 Controller <-> Service 에서
    Controller <-> Handler <-> Service로 변경함으로써,
    run 메서드 역할이 분산되어 Controller 책임이 단순해짐.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.Lotto;
+import lotto.handler.MainLogicHandler;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -9,63 +10,74 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoController {
-    private final LottoService lottoService;
+//    private final LottoService lottoService;
+    private final MainLogicHandler mainLogicHandler;
 
     public LottoController() {
-        this.lottoService = new LottoService();
+//        this.lottoService = new LottoService();
+        this.mainLogicHandler = new MainLogicHandler();
     }
 
     public void run() {
         OutputView.printInputPurchaseMoneyMessage();
-        int purchaseMoney, lottoCount;
-        while (true) {
-            try {
-                String money = InputView.getUserInput();
-                purchaseMoney = lottoService.getPurchaseMoney(money);
-                lottoCount = lottoService.getLottoCount(purchaseMoney);
-                break;
-            }
-            catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        mainLogicHandler.handlePurchaseMoney();
+//        int purchaseMoney, lottoCount;
+//        while (true) {
+//            try {
+//                String money = InputView.getUserInput();
+//                purchaseMoney = lottoService.getPurchaseMoney(money);
+//                lottoCount = lottoService.getLottoCount(purchaseMoney);
+//                break;
+//            }
+//            catch (IllegalArgumentException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
-        List<Lotto> lottos = lottoService.generateLottos(lottoCount);
-        OutputView.printLottoCountMessage(lottoCount);
-        OutputView.printLottoNumbers(lottos);
+//        List<Lotto> lottos = lottoService.generateLottos(mainLogicHandler.getLottoCount());
+        mainLogicHandler.handleLottos();
+//        OutputView.printLottoCountMessage(mainLogicHandler.getLottoCount());
+//        OutputView.printLottoNumbers(mainLogicHandler.getLottos());
 
         OutputView.printInputWinningNumbers();
-        Lotto winningLotto;
-        while (true) {
-            try {
-                String winningNumbersInput = InputView.getUserInput();
-                winningLotto = lottoService.getWinningLotto(winningNumbersInput);
-                break;
-            }
-            catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        mainLogicHandler.handleWinningLotto();
+//        Lotto winningLotto;
+//        while (true) {
+//            try {
+//                String winningNumbersInput = InputView.getUserInput();
+//                winningLotto = lottoService.getWinningLotto(winningNumbersInput);
+//                break;
+//            }
+//            catch (IllegalArgumentException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
         OutputView.printInputBonusNumber();
-        int bonusNumber;
-        while (true) {
-            try {
-                String bonusNumberInput = InputView.getUserInput();
-                bonusNumber = lottoService.getBonusNumber(bonusNumberInput, winningLotto.getNumbers());
-                break;
-            }
-            catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        mainLogicHandler.handleBonusNumber();
+//        int bonusNumber;
+//        while (true) {
+//            try {
+//                String bonusNumberInput = InputView.getUserInput();
+//                bonusNumber = lottoService.getBonusNumber(bonusNumberInput, winningLotto.getNumbers());
+//                break;
+//            }
+//            catch (IllegalArgumentException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
-        Map<String, Integer> matchCounts = lottoService.getMatchCounts(lottos, winningLotto, bonusNumber);
-        OutputView.printPrizeStatistics(matchCounts);
+//        Map<String, Integer> matchCounts = lottoService.getMatchCounts(
+//                lottos,
+//                mainLogicHandler.getWinningLotto(),
+//                mainLogicHandler.getBonusNumber());
+        mainLogicHandler.handleMatchCounts();
+//        OutputView.printPrizeStatistics(mainLogicHandler.getMatchCounts());
 
-        long totalPrizeMoney = lottoService.getPrizeMoney(matchCounts);
-        String rateOfReturn = lottoService.getRateOfReturn(totalPrizeMoney, purchaseMoney);
-        OutputView.printRateOfReturn(rateOfReturn);
+//        long totalPrizeMoney = lottoService.getPrizeMoney(mainLogicHandler.getMatchCounts());
+//        String rateOfReturn = lottoService.getRateOfReturn(totalPrizeMoney, mainLogicHandler.getPurchaseMoney());
+        mainLogicHandler.handleRateOfReturn();
+        OutputView.printRateOfReturn(mainLogicHandler.getRateOfReturn());
 
     }
 }
---
+++ src/main/java/lotto/handler/MainLogicHandler.java
@@ -0,0 +1,100 @@
+package lotto.handler;
+
+import lotto.Lotto;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
+public class MainLogicHandler {
+    private final LottoService lottoService;
+    int purchaseMoney, lottoCount, bonusNumber;
+    Lotto winningLotto;
+    List<Lotto> lottos;
+    Map<String, Integer> matchCounts;
+    String rateOfReturn;
+
+    public MainLogicHandler() {
+        this.lottoService = new LottoService();
+    }
+
+    public void handlePurchaseMoney() {
+        try {
+            String money = InputView.getUserInput();
+            purchaseMoney = lottoService.getPurchaseMoney(money);
+            lottoCount = lottoService.getLottoCount(purchaseMoney);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            handlePurchaseMoney();
+        }
+    }
+
+    public void handleLottos() {
+        lottos = lottoService.generateLottos(lottoCount);
+        OutputView.printLottoCountMessage(lottoCount);
+        OutputView.printLottoNumbers(lottos);
+    }
+
+    public void handleWinningLotto() {
+        try {
+            String winningNumbersInput = InputView.getUserInput();
+            winningLotto = lottoService.getWinningLotto(winningNumbersInput);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            handleWinningLotto();
+        }
+    }
+
+    public void handleBonusNumber() {
+        try {
+            String bonusNumberInput = InputView.getUserInput();
+            bonusNumber = lottoService.getBonusNumber(bonusNumberInput, winningLotto.getNumbers());
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            handleBonusNumber();
+        }
+    }
+
+    public void handleMatchCounts() {
+        matchCounts = lottoService.getMatchCounts(lottos, winningLotto, bonusNumber);
+        OutputView.printPrizeStatistics(matchCounts);
+    }
+
+    public void handleRateOfReturn() {
+        long totalPrizeMoney = lottoService.getPrizeMoney(matchCounts);
+        rateOfReturn = lottoService.getRateOfReturn(totalPrizeMoney, purchaseMoney);
+    }
+
+    public int getPurchaseMoney() {
+        return purchaseMoney;
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public Map<String, Integer> getMatchCounts() {
+        return matchCounts;
+    }
+
+    public String getRateOfReturn() {
+        return rateOfReturn;
+    }
+}
