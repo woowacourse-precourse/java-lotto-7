cbf3a69db332aa8d78e7ed65d5a41696d7fa9bdf
jijongkwon <help3451@naver.com>
2024-11-03 17:54:36 +0900

    feat(util): LottoMachine 싱글톤 패턴 및 메서드 구현
    - savePurchaseAmount : 금액 저장
    - getPurchaseAmount : 금액 반환

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
 public class LottoMachine {
+    private static final LottoMachine instance = new LottoMachine();
+    private int purchaseAmount;
+
+    private LottoMachine() {
+
+    }
+
+    public static LottoMachine getInstance() {
+        return instance;
+    }
+
+    public void savePurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
 }
