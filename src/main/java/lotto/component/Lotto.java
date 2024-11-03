8e101b4c4a4c5fbcda0d5f5ed2495d9bfbd72303
장혜민 <gpals5987@gmail.com>
2024-11-03 10:40:21 +0900

    [style] 상수 처리

--- src/main/java/lotto/component/Lotto.java
+++ src/main/java/lotto/component/Lotto.java
@@ -1,6 +1,7 @@
-package lotto;
+package lotto.component;
 
-import java.util.Collections;
+import lotto.constant.LottoConstants;
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,16 +14,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoConstants.LOTTO_NUMBERS_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         for(int number: numbers){
-            if(number < 1 || number > 45){
+            if(number < LottoConstants.MIN_RANGE_NUMBER || number > LottoConstants.MAX_RANGE_NUMBER ){
                 throw new IllegalArgumentException("[ERROR]");
             }
         }
         HashSet<Integer> duplicate = new HashSet<>(numbers);
-        if(duplicate.size() != 6){
+        if(duplicate.size() != LottoConstants.LOTTO_NUMBERS_COUNT){
             throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력 할 수 없습니다");
         }
     }
--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -5,4 +5,8 @@ public class LottoConstants {
     public static final int MAX_RANGE_NUMBER = 45;
 
     public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final int WINNING_LOTTO_NUMBERS_COUNT = 7;
+
+
+    public static final int LOTTO_PRICE = 1000;
 }
