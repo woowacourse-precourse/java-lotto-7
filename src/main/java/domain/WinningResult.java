ddf9044064db68da7ee3f660c8a1e65e646f0016
ziyoungkwon <gy011003@naver.com>
2024-11-04 08:46:24 +0900

    docs: README.md 보너스 번호 입력 관련 예외 추가
    보너스 번호가 당첨 번호와 중복되는 경우 예외 발생

--- src/main/java/domain/WinningResult.java
+++ src/main/java/domain/WinningResult.java
@@ -1,6 +1,14 @@
 package domain;
 
+import java.util.List;
+
 public class WinningResult {
 
-
+//    private final List<Integer> winning;
+//    private final int bonus;
+//
+//    public WinningResult(List<Integer> winning, int bonus) {
+//        this.winning = winning;
+//        this.bonus = bonus;
+//    }
 }
