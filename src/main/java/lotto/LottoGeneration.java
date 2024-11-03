8e0fc661f0e4b25f61bbac6a279fbdc99001cb29
youjeans <dbyj2810@naver.com>
2024-11-03 22:37:00 +0900

    feat: 구매금액 입력갑 유효성 검증 기능

--- src/main/java/lotto/LottoGeneration.java
+++ src/main/java/lotto/LottoGeneration.java
@@ -22,4 +22,19 @@ public class LottoGeneration {
         }
         return -1;
     }
+
+    public static void validatePurchaseAmount(String input) {
+        if (!Util.isInteger(input)) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000 단위 정수만 입력되야 합니다.");
+        }
+
+        int purchaseAmount = Integer.parseInt(input);
+
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상이어야 합니다.");
+        }
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 나누어 떨어져야 합니다.");
+        }
+    }
 }
