497efe1aae7a8ba9bae37806177e8ee8f22075b6
SinnoLn <sintmtn69@gmail.com>
2024-11-03 21:48:13 +0900

    feat: LottoService 클래스에 로또 티켓 발행 및 당첨 결과 계산 로직 추가
    - LottoMachine을 활용하여 구입 금액에 따른 로또 티켓 발행 기능 구현
    - LottoResultCalculator를 사용하여 당첨 번호와 비교 후 당첨 결과 계산 기능 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,45 @@
+package lotto.service;
+
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.model.LottoMachine;
+import lotto.model.LottoRank;
+import lotto.model.LottoResultCalculator;
+import lotto.model.strategy.NumberGenerationStrategy;
+
+public class LottoService {
+
+    private final LottoMachine lottoMachine;
+
+    public LottoService(NumberGenerationStrategy strategy) {
+        this.lottoMachine = new LottoMachine(strategy);
+    }
+
+    /**
+     * 구입 금액을 기반으로 로또 티켓을 발행
+     *
+     * @param amount 구입 금액
+     * @return 발행된 로또 티켓 리스트
+     */
+    public List<Lotto> purchaseLottoTickets(int amount) {
+        return lottoMachine.issueLottoTickets(amount);
+    }
+
+    /**
+     * 당첨 결과를 계산
+     *
+     * @param tickets 사용자 로또 티켓 리스트
+     * @param winningNumbers 당첨 번호 리스트
+     * @param bonusNumber 보너스 번호
+     * @return 당첨 등수별 당첨 횟수 Map
+     */
+    public Map<LottoRank, Integer> calculateResults(
+            List<Lotto> tickets,
+            List<Integer> winningNumbers,
+            int bonusNumber
+    ) {
+        LottoResultCalculator calculator = new LottoResultCalculator(winningNumbers, bonusNumber);
+        return calculator.calculateResults(tickets);
+    }
+}
