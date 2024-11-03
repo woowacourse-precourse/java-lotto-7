eae7bab08501915ae77d52e7fc7b7167455e3d80
Jinu Yi <dionisos198@naver.com>
2024-11-03 16:42:48 +0900

    feat: 로또 숫자 랜덤 발생 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,16 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
+import lotto.exception.ErrorCode;
 
 public class Lotto {
     private final List<Integer> numbers;
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_START_NUM = 1;
+    private static final int LOTTO_FINAL_NUM = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +18,22 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_SIZE_MUST_BE_SIX.getMessage());
+        }
+
+        if (!numbers.stream().allMatch(num -> num >= LOTTO_START_NUM && num <= LOTTO_FINAL_NUM)) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_NUMBER_MUST_BE_IN_CORRECT_RANGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto createRandomLotto(){
+
+        return new Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_START_NUM, LOTTO_FINAL_NUM, LOTTO_SIZE));
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
 }
