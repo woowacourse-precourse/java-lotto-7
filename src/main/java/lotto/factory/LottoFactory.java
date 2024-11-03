4067733a7acdbefeb3e705dcb14ea3cc74f40786
geonwoo <kysub99@naver.com>
2024-11-04 08:51:20 +0900

    fix: 로또 번호 생성 시 번호를 오름차순으로 정렬

--- src/main/java/lotto/factory/LottoFactory.java
+++ src/main/java/lotto/factory/LottoFactory.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoFactory {
     public static final int PRICE = 1000;
@@ -13,7 +15,11 @@ public class LottoFactory {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             // 1부터 45 사이의 중복되지 않는 6개의 랜덤 숫자를 선택
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                    .stream()
+                    .sorted()  // 오름차순 정렬
+                    .collect(Collectors.toList());
+
             lottos.add(new Lotto(numbers));
         }
 
