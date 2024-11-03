5792405e6077338f1a7c99026eb9989f74801b11
차왕준 <ckdhdkwns@gmail.com>
2024-11-04 04:49:35 +0900

    test(domain): 당첨 설명 확인 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class RankTest {
-    @DisplayName("당첨금이 일치한다")
+    @DisplayName("당첨금이 일치한다.")
     @Test
     void 당첨금이_일치한다() {
         assertThat(Rank.FIRST.getPrize()).isEqualTo(2_000_000_000);
@@ -16,5 +16,15 @@ public class RankTest {
         assertThat(Rank.FOURTH.getPrize()).isEqualTo(50_000);
         assertThat(Rank.FIFTH.getPrize()).isEqualTo(5_000);
         assertThat(Rank.NONE.getPrize()).isZero();
+    }
+
+    @DisplayName("당첨 설명을 확인한다.")
+    @Test
+    void 당첨_설명을_확인한다() {
+        assertThat(Rank.FIRST.getDescription()).contains("6개");
+        assertThat(Rank.SECOND.getDescription()).contains("5개", "보너스");
+        assertThat(Rank.THIRD.getDescription()).contains("5개");
+        assertThat(Rank.FOURTH.getDescription()).contains("4개");
+        assertThat(Rank.FIFTH.getDescription()).contains("3개");
     }
 }
