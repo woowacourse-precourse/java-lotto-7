4cc229f63d28b8d62968fddf97e64a15962b9d84
gobchang7 <anchovyzzang@gmail.com>
2024-11-03 22:10:58 +0900

    feat: 유효성 검사를 위한 기능 구현

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.List;
+
+public class Validator {
+
+    public static void validateAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액을 1,000원 단위로 입력해주세요.");
+        }
+        if (amount > 10000) {
+            throw new IllegalArgumentException("[ERROR] 최대 구입 가능 금액은 10,000원 입니다.");
+        }
+    }
+
+    public static void validateBonus(int bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public static void validateWinningNumbers(List<String> winningNumbers) {
+        for (String number : winningNumbers) {
+            validateWinningNumber(number);
+        }
+    }
+
+    public static void validateWinningNumber(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 1 || num > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+}
