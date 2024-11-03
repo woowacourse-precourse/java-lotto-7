f6df5e43f77a58117d3b9529f9a4320b5c6413e1
HJ <gmlwnchlrh@gmail.com>
2024-11-03 23:25:06 +0900

    feat: Lotto.java 패키지 변경하고 모델 구현

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = List.copyOf(numbers);
+    }
+
+    public static Lotto generateRandomLotto() {
+        List<Integer> generatedNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT));
+        Collections.sort(generatedNumbers);
+        return new Lotto(generatedNumbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다. 현재 크기: " + numbers.size());
+        }
+        if (numbers.stream().anyMatch(n -> n < MIN_LOTTO_NUMBER || n > MAX_LOTTO_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이여야 합니다. 현재 번호: " + numbers);
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다. 현재 번호: " + numbers);
+        }
+    }
+}
