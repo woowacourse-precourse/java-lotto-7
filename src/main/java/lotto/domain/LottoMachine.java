5d8f5ef69a23339c4d79e6dc7323b869db8c6344
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-04 02:03:56 +0900

    feat(LottoMachine): 로또 구입 메서드 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,14 +2,43 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.IntStream;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoMachine {
 
+    private List<Lotto> lottos;
     private List<Prize> prizes;
 
+    public void run() {
+        PurchaseAmount purchaseAmount = InputView.readPurchaseAmount();
+        System.out.println();
+
+        purchaseLottos(purchaseAmount);
+        System.out.println();
+    }
+
+    private void purchaseLottos(final PurchaseAmount purchaseAmount) {
+        int purchaseQuantity = calculatePurchaseQuantity(purchaseAmount);
+        OutputView.printPurchaseQuantity(purchaseQuantity);
+
+        this.lottos = IntStream.range(0, purchaseQuantity)
+                .mapToObj(i -> generateRandomLotto())
+                .toList();
+        OutputView.printLottos(this.lottos);
+    }
+
     private Lotto generateRandomLotto() {
-        List<Integer> numbers =
-                Randoms.pickUniqueNumbersInRange(LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER, Lotto.LOTTO_SIZE);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LottoNumber.MIN_NUMBER,
+                LottoNumber.MAX_NUMBER,
+                Lotto.LOTTO_SIZE
+        );
         return Lotto.from(numbers);
+    }
+
+    private int calculatePurchaseQuantity(PurchaseAmount purchaseAmount) {
+        return purchaseAmount.amount() / PurchaseAmount.UNIT_AMOUNT;
     }
 }
