cfdcee81cc8319c2a60e6e17fff3b93b98760fb0
BHC-Chicken <tij05078@gmail.com>
2024-11-04 01:47:45 +0900

    feat(input): 로또 구매, 당첨 번호 입력 기능
    로또 구매 및 당첨번호, 보너스 번호 입력을 위한 view입니다.
    로또 구매는 1,000원단위, 보너스 번호는 1 ~ 45, 당첨번호 외 숫자를 입력 받고 외 입력시 예외 처리됩니다.

---
+++ src/main/java/lotto/LottoView.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoView {
+    public String moneyInput() {
+        System.out.println("구입 금액을 입력해 주세요.");
+
+        return Console.readLine();
+    }
+
+    public String lottoNumbersInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        return Console.readLine();
+    }
+
+    public String bonusNumberInput() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+
+        return Console.readLine();
+    }
+}
