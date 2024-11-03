8d9f3db7331e0819a02f1fff80e7628604f7f511
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:31:45 +0900

    feat(DrawNumbersBuilder): 추첨 번호와 보너스 번호의 검증 로직의 분리를 위해 빌더 패턴 다시 도입

---
+++ src/main/java/lotto/model/lotto_result/DrawNumbersBuilder.java
@@ -0,0 +1,27 @@
+package lotto.model.lotto_result;
+
+import lotto.utils.DrawNumbersValidator;
+
+import java.util.List;
+
+public class DrawNumbersBuilder {
+
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+    private final DrawNumbersValidator drawNumbersValidator = new DrawNumbersValidator();
+
+    public DrawNumbersBuilder winningNumbers(String rawWinningNumbers) {
+        this.winningNumbers = drawNumbersValidator.validateWinningNumbers(rawWinningNumbers);
+        return this;
+    }
+
+    public DrawNumbersBuilder bonusNumber(String rawBonusNumber) {
+        this.bonusNumber = drawNumbersValidator.validateBonusNumber(rawBonusNumber);
+        drawNumbersValidator.validateAssociateWinningAndBonusNumbers(winningNumbers, bonusNumber);
+        return this;
+    }
+
+    public DrawNumbers build() {
+        return new DrawNumbers(winningNumbers, bonusNumber);
+    }
+}
