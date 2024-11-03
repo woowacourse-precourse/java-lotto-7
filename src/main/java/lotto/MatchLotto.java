5fb6156b2d17f46b283c65f122c72515ba6f8d10
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-11-04 03:05:51 +0900

    feat(MatchLotto): 전체 로또 당첨 결과를 관리하는 클래스를 만들고 전체 결과를 얻는 메소드 추가

---
+++ src/main/java/lotto/MatchLotto.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MatchLotto {
+    private final int totalLottoPrice;
+    private final List<Integer> winningNumber;
+    private final int bonusNumber;
+    private final LottoNumbers lottoNumbers;
+
+    public MatchLotto(int totalLottoPrice, List<Integer> winningNumber, int bonusNumber, LottoNumbers lottoNumbers) {
+        this.totalLottoPrice = totalLottoPrice;
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private Map<Rank, Integer> getTotalResult() {
+        Map<Rank, Integer> rankCountMap = new TreeMap<>(Comparator.reverseOrder());
+        for(Rank rank:Rank.values()){
+            rankCountMap.put(rank, 0);
+        }
+        for(Lotto lotto:lottoNumbers.getLottos()){
+            int matchCount = lotto.matchWinningNumber(winningNumber);
+            boolean bonusMatch = lotto.matchBonusNumber(bonusNumber);
+            if(matchCount >= 3) {
+                Rank rank = Rank.fromValue(matchCount, bonusMatch);
+                rankCountMap.put(rank, rankCountMap.get(rank) + 1);
+            }
+        }
+        return rankCountMap;
+    }
+
+}
