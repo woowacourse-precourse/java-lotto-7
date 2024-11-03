b8bf7fc0291b9cf231b3b26d05703b47dddcf36a
tentenball <lth8905@gmail.com>
2024-11-04 01:41:23 +0900

    refactor: ApplicationTest 오류 해결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import lotto.domains.customer.Customer;
 import lotto.domains.lotto.domain.LottoPrizeNumbers;
@@ -40,8 +41,8 @@ public class Application {
 
 		OutputInterface.printMessage(OutputInterface.WINNING_STATISTICS_INFORMATION);
 
-		Map<LottoPrize,Integer> lottoResult = lottoResultFactory.generateLottoResult();
-		List<Map<Integer,Boolean>> winningStatus = customer.checkWinningStatus(lottoTickets, lottoPrizeNumbers);
+		Map<LottoPrize, Integer> lottoResult = lottoResultFactory.generateLottoResult();
+		List<Map<Integer, Boolean>> winningStatus = customer.checkWinningStatus(lottoTickets, lottoPrizeNumbers);
 
 		String customerLottoResult = lottoTicketMachine.registerLottoResult(lottoResult, winningStatus);
 		OutputInterface.printMessage(customerLottoResult);
@@ -54,27 +55,25 @@ public class Application {
 
 	private static Customer purchaseLottoTickets(InputInterface inputInterface) {
 		while (true) {
+			OutputInterface.printMessage(OutputInterface.ENTER_PURCHASE_PRICE);
+			String price = inputInterface.readLine();
 			try {
-				OutputInterface.printMessage(OutputInterface.ENTER_PURCHASE_PRICE);
-				String price = inputInterface.readLine();
 				OutputInterface.printNewLine();
-
 				return Customer.from(TypeConverter.convertStringToInteger(price));
-			} catch (IllegalArgumentException exception) {
-				processException(exception);
+			} catch (NoSuchElementException | IllegalArgumentException exception) {
+				processException(exception.getMessage());
 			}
 		}
 	}
 
 	private static List<Integer> drawWinningNumbers(InputInterface inputInterface, LottoService lottoService) {
 		while (true) {
+			OutputInterface.printMessage(OutputInterface.ENTER_WINNING_NUMBERS);
+			String winningNumbers = inputInterface.readLine();
 			try {
-				OutputInterface.printMessage(OutputInterface.ENTER_WINNING_NUMBERS);
-				String winningNumbers = inputInterface.readLine();
-
 				return lottoService.drawWinningNumbers(winningNumbers.trim());
-			} catch (IllegalArgumentException exception) {
-				processException(exception);
+			} catch (NoSuchElementException | IllegalArgumentException exception) {
+				processException(exception.getMessage());
 			}
 		}
 	}
@@ -82,19 +81,17 @@ public class Application {
 	private static int drawBonusNumber(InputInterface inputInterface, LottoService lottoService,
 		List<Integer> winningNumbers) {
 		while (true) {
+			OutputInterface.printMessage(OutputInterface.ENTER_BONUS_NUMBER);
+			String bonusNumber = inputInterface.readLine();
 			try {
-				OutputInterface.printMessage(OutputInterface.ENTER_BONUS_NUMBER);
-				String bonusNumber = inputInterface.readLine();
-
 				return lottoService.drawBonusNumber(bonusNumber, winningNumbers);
-			} catch (IllegalArgumentException exception) {
-				processException(exception);
+			} catch (NoSuchElementException | IllegalArgumentException exception) {
+				processException(exception.getMessage());
 			}
 		}
 	}
 
-	private static void processException(IllegalArgumentException exception) {
-		OutputInterface.printMessage(exception.getMessage());
-		OutputInterface.printNewLine();
+	private static void processException(String message) {
+		OutputInterface.printMessage(message);
 	}
 }
--- src/main/java/lotto/view/InputInterface.java
+++ src/main/java/lotto/view/InputInterface.java
@@ -3,13 +3,14 @@ package lotto.view;
 import java.util.NoSuchElementException;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.ExceptionMessage;
 
 public class InputInterface {
 	public String readLine() {
 		try {
 			return Console.readLine();
 		} catch (NoSuchElementException exception) {
-			throw new IllegalArgumentException(exception);
+			throw new IllegalArgumentException(ExceptionMessage.NOT_EMPTY.toString());
 		}
 	}
 }
