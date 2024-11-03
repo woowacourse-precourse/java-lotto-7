647433f71f44c6d711c1c7afa6acf5ae3432644d
YongLeeCode <yonghwan311@gmail.com>
2024-11-03 20:50:40 +0900

    feat: 기능 동작 활성화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+
+        Integer pay = input.getMoney();
+        List<Integer> inputPrizeNumber = input.getPrizeNumbers();
+        Integer bonus = input.getBonus();
+        Lotto lotto = new Lotto(
+                inputPrizeNumber,
+                pay,
+                bonus
+        );
+
+        lotto.play();
     }
 }
