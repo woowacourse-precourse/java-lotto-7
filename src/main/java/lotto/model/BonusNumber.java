2513119d78a646b8524f2b6cca66a8739c62be0a
dongkyunKim <kyun010713@gmail.com>
2024-11-03 19:46:04 +0900

    refacor(model): 에러 메시지 형식에 맞게 변경

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -13,13 +13,13 @@ public class BonusNumber {
 
     private void validateBonusNumber(int bonusNumber) {
         if (isNumberOutOfRange(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
     private void validateBonusNumberUniqueness(int bonusNumber, List<Integer> winningNumbers) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 겹칠 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 겹칠 수 없습니다.");
         }
     }
 
