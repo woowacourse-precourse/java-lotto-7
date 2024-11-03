a4f477ee3e93b9ea86ee9c428c81656d32cfb384
배성연 <bsy309@ajou.ac.kr>
2024-11-04 01:44:51 +0900

    feat(MyLottoNum): 내가 구입한 금액에 따라 Lotto 생성기능 작성

---
+++ src/main/java/lotto/controller/MyLottoNum.java
@@ -0,0 +1,34 @@
+package lotto.controller;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.LottoList;
+
+public class MyLottoNum {
+    private final List<Lotto> lottoList = new ArrayList<Lotto>();
+
+    private MyLottoNum() {
+    }
+
+    public List<Lotto> getLottoList(Integer times) {
+        return lottoList;
+    }
+
+    public void makeMyLotto(Integer times) {
+        for (int i = 0; i < times; i++) {
+            List<Integer> uniqueLotto = pickUniqueNumbersInRange(1, 45, 6);
+            Lotto t_lotto = new Lotto(uniqueLotto);
+            lottoList.add(t_lotto);
+        }
+    }
+
+    public void printMyLottoList() {
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto);
+        }
+    }
+}
--- src/main/java/lotto/model/LottoList.java
+++ src/main/java/lotto/model/LottoList.java
@@ -1,10 +1,11 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
 
 public class LottoList {
-    private List<Lotto> lottos;
+    private final List<Lotto> lottos;
 
     public LottoList(List<Lotto> lottos) {
         this.lottos = lottos;
