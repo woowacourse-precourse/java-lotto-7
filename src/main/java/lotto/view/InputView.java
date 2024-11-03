4fdd3eadb78e350f5a56263f80d23c4613520886
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2024-11-03 21:33:38 +0900

    :necktie: feat : validateBonusNumberInput 메서드 추가
    - 보너스 번호 유효성 검증 메서드 추가
    - 보너스 번호 입력 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -69,4 +70,29 @@ public class InputView {
     public static void validateNumberRange(int number){
         if (number < 1 || number > 45) throw new IllegalArgumentException("[ERROR] 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
+
+    public static int inputBonusNumber(List<Integer> winningNumbers){
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+
+        validateBonusNumberInput(input, winningNumbers);
+        return Integer.parseInt(input);
+    }
+
+    public static void validateBonusNumberInput(String input, List<Integer> winningNumbers){
+        if (isEmptyString(input)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호를 입력해 주세요.");
+        }
+        if (!isNumeric(input)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+
+        int bonusNumber = Integer.parseInt(input);
+        validateNumberRange(bonusNumber);
+
+        if (winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        }
+    }
+
 }
