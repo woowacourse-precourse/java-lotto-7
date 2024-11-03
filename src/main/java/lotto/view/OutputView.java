2c5185c8f59bd1857c3af0fde7c045707be2230d
장영재 <urinaner@naver.com>
2024-11-04 02:32:07 +0900

    feat(OutputView): 출력 메세지 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import lotto.domain.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    private static final String OUTPUT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_STATISTIC_HEADER = "당첨 통계\n---";
+    private static final String WINNING_STATISTIC_FORMAT = "%d개 일치 (%,d원) - %d개";
+    private static final String WINNING_STATISTIC_SECOND_FORMAT = "5개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
 }
