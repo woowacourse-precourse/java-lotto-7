85a89949025fe3c54d8ca83253180546055e1996
yush <yush2202@gmail.com>
2024-11-04 04:10:47 +0900

    feat($ Constants): 상수 추가
    이익 포맷 상수 추가

--- src/main/java/lotto/constants/Constants.java
+++ src/main/java/lotto/constants/Constants.java
@@ -14,7 +14,12 @@ public class Constants {
     public static final int MONEY_UNIT = 1_000;
     public static final int LOTTO_SIZE = 6;
     public static final int ONE = 1;
+    public static final int ZERO = 0;
+    public static final int MAX_USER_MONEY = 100_000;
+    public static final int MIN_USER_MONEY = 0;
+
 
     public static final String DELIMITER = ",";
-    
+    public static final String PROFIT_FORMAT = "#,##0.0";
+
 }
