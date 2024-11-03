88e1a73c1c34972a5d79a67dd44bb72fc9fe0822
minseo <alstj778@gmail.com>
2024-11-04 01:05:36 +0900

    refactor(OutputView): 출력 메시지 별도 상수 클래스로 추출

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,44 +3,50 @@ package lotto.view;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lotto.constant.OutputMessage;
 import lotto.dto.LottoNumbers;
 import lotto.model.Prize;
 
 public class OutputView {
     public void displayPurchasePriceRequest() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(OutputMessage.PURCHASE_PRICE_REQUEST);
     }
 
     public void displayPurchaseQuantity(final int quantity) {
-        System.out.println();
-        System.out.printf("%d개를 구매했습니다.%n", quantity);
+        System.out.printf(OutputMessage.PURCHASE_QUANTITY_FORMAT, quantity);
     }
 
     public void displayLottoNumbers(final List<LottoNumbers> lottoNumbers) {
-        String result = lottoNumbers.stream()
+        System.out.println(OutputMessage.LINE_SEPARATOR + buildLottoNumbers(lottoNumbers));
+    }
+
+    private String buildLottoNumbers(final List<LottoNumbers> lottoNumbers) {
+        return lottoNumbers.stream()
                 .map(LottoNumbers::numbers)
                 .map(List::toString)
-                .collect(Collectors.joining("\n"));
-        System.out.println(result);
+                .collect(Collectors.joining(OutputMessage.LINE_SEPARATOR));
     }
 
     public void displayWinningNumbersRequest() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
+        System.out.println(OutputMessage.WINNING_NUMBERS_REQUEST);
     }
 
     public void displayBonusNumberRequest() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+        System.out.println(OutputMessage.BONUS_NUMBER_REQUEST);
     }
 
     public void displayWinningResult(final Map<Prize, Integer> result, final double rateOfReturn) {
-        System.out.println("\n당첨 통계\n---");
-        System.out.println(buildResult(result));
-        System.out.printf("총 수익률은 %.2f%%입니다.%n", rateOfReturn);
+        String output = OutputMessage.WINNING_RESULT_PREFIX + buildWinningStat(result) + formatRateOfReturn(rateOfReturn);
+        System.out.println(output);
     }
 
-    private String buildResult(final Map<Prize, Integer> result) {
+    private String buildWinningStat(final Map<Prize, Integer> result) {
         return result.entrySet().stream()
                 .map(entry -> String.format(entry.getKey().getResultMessage(), entry.getValue()))
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(OutputMessage.LINE_SEPARATOR));
+    }
+
+    private String formatRateOfReturn(final double rateOfReturn) {
+        return String.format(OutputMessage.RATE_OF_RETURN_FORMAT, rateOfReturn);
     }
 }
