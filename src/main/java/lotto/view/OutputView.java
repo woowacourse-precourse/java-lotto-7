b140b3bb2412958692704d842f6be465a0935f0c
unluckyboy214 <janggoon0214@naver.com>
2024-11-03 17:52:05 +0900

    feat: 랭킹 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,4 +5,13 @@ public class OutputView {
     public static void printLottoCount(int count){
         System.out.println(count + "개를 구매했습니다.");
     }
+
+    public static void printSuccessResult(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public static void printSuccessMessage(String message, int numberOfMatch) {
+        System.out.println(message + numberOfMatch + "개");
+    }
 }
