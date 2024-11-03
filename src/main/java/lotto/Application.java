8e376946cee6cb4a4d49f5af37634f58fd7ed179
mintos00 <kimhm2170@gmail.com>
2024-11-04 00:35:05 +0900

    feat(lottoRandom): extract a six-digit Lotto numbers
    - extract lotto numbers as many as lotto available for purchase

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Long money = inputMoney();
         Long countLotto = availableLotto(money);
-
+        List<List<Integer>> lottoList = lottoRandom(countLotto);
+        haveLotto(lottoList);
     }
 
     public static Long inputMoney() {
@@ -27,4 +31,21 @@ public class Application {
         System.out.println(countLotto + "개를 구매했습니다.");
         return countLotto;
     }
+
+    public static List<List<Integer>> lottoRandom(Long countLotto) {
+        List<List<Integer>> lottoList = new ArrayList<>();
+
+        for (long i = 0; i < countLotto; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoList.add(lottoNumbers);
+        }
+        return lottoList;
+    }
+
+    public static void haveLotto(List<List<Integer>> lottoList) {
+        for (List<Integer> lotto : lottoList) {
+            System.out.println(lotto);
+        }
+    }
+
 }
