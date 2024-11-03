899b331ff1d3be6511874830b06f9a19935fac19
fbehddn <tom0611@naver.com>
2024-11-04 01:21:35 +0900

    fix: 에러 메세지 양식에 맞게 수정

--- src/main/java/lotto/exception/Money/NegativeMoneyException.java
+++ src/main/java/lotto/exception/Money/NegativeMoneyException.java
@@ -1,7 +1,7 @@
 package lotto.exception.Money;
 
 public class NegativeMoneyException extends IllegalArgumentException {
-    private final static String ERROR_MESSAGE = "[Error] 금액은 양의 정수여야합니다.";
+    private final static String ERROR_MESSAGE = "[ERROR] 금액은 양의 정수여야 합니다.";
 
     public NegativeMoneyException() {
         super(ERROR_MESSAGE);
