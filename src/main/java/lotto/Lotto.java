e0b2beeff6a299e0fa94e85b426a5256ecbcc015
dongyeop <ds06041@gmail.com>
2024-11-03 22:05:52 +0900

    feat: 구매한 로또 번호의 유효성 체크를 위한 테스트 코드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,9 +9,12 @@ import java.util.Set;
 public class Lotto {
     private final List<Integer> numbers;
 
+    // 생성자이므로 void를 붙이지 않고 일반 메서드와 구분한다.
+    // 생성자는 객체를 초기화 할 때만 호출된다.
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         duplicatesCheck(numbers);
+        rangeCheck(numbers);
         this.numbers = numbers;
 
     }
@@ -25,12 +28,20 @@ public class Lotto {
 
     // 로또 번호 중복 체크
     private void duplicatesCheck(List<Integer> numbers) {
-        Set<Integer> duplicatesNumber = new HashSet<>(numbers);
-        if (duplicatesNumber.size() != numbers.size()) {
+        Set<Integer> uniqueNumber = new HashSet<>(numbers);
+        if (uniqueNumber.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
+    // 로또 번호 범위 체크
+    private void rangeCheck(List<Integer> numbers) {
+        for (int number:numbers){
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45사이어야 합니다.");
+            }
+        }
+    }
 
     // 구매한 로또의 개수 반환
     public static int lottoCount(int lottoPurchaseAmount){
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,7 +3,9 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,6 +23,17 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void WinningNumbersRangeCheck() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 구입 금액에 맞는 로또의 개수를 반환 하는지 확인")
     @Test
     void lottoCountTest(){
@@ -28,4 +41,56 @@ class LottoTest {
         assertThat(Lotto.lottoCount(8000)).isEqualTo(8);
     }
 
+
+    @DisplayName("구매한 로또 번호를 담은 리스트의 사이즈 확인")
+    @Test
+    void purchaseLottoNumbersCountTest(){
+        assertThat(Lotto.purchaseLottoNumbers(8).length).isEqualTo(8);
+    }
+
+    @DisplayName("구매한 로또 번호의 길이 확인")
+    @Test
+    void purchaseLottoNumbersSizeTest(){
+        int[][] lottoNumbers = Lotto.purchaseLottoNumbers(8);
+        for(int[] lottoNumber: lottoNumbers){
+            assertThat(lottoNumber.length).isEqualTo(6);
+        }
+    }
+
+    // 구매한 로또 번호의 범위 확인
+    private boolean lottoNumberRangeCheck(int[] lottoNumbers){
+        for(int lottoNumber: lottoNumbers){
+            if(lottoNumber<1 || lottoNumber>45){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @DisplayName("구매한 로또 번호가 1- 45 사이인지 확인")
+    @Test
+    void purchaseLottoNumbersRangeTest(){
+        int[][] lottoNumbers = Lotto.purchaseLottoNumbers(8);
+        for(int[] lottoNumber: lottoNumbers){
+            assertThat(lottoNumberRangeCheck(lottoNumber)).isTrue();
+        }
+    }
+
+    // 구매한 로또 번호 중복 확인
+    private boolean lottoNumbersUniqueCheck(int[] lottoNumbers){
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for(int number: lottoNumbers){
+            uniqueNumbers.add(number);
+        }
+        return uniqueNumbers.size() == lottoNumbers.length;
+    }
+
+    @DisplayName("구매한 로또 번호가 중복되는지 확인")
+    @Test
+    void purchaseLottoNumbersUniqueTest(){
+        int[][] lottoNumbers = Lotto.purchaseLottoNumbers(8);
+        for(int[] lottoNumber: lottoNumbers){
+            assertThat(lottoNumbersUniqueCheck(lottoNumber)).isTrue();
+        }
+    }
 }
