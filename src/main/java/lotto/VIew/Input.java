2f26b322606abf4302525683772fa5d9234c2d86
hojae <ghwo336@naver.com>
2024-11-04 00:08:48 +0900

    feat:Input 구현

---
+++ src/main/java/lotto/VIew/Input.java
@@ -0,0 +1,9 @@
+package lotto.VIew;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static String getInput(){
+        return Console.readLine().replace(" ","");
+    }
+}
