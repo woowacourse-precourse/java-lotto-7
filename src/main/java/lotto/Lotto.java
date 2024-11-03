351975c2b25a4a9fb3a725936412e1ff925bd3bd
skadjs <skadjs11@gmail.com>
2024-11-04 06:35:06 +0900

    refactor(Lotto): 띄어쓰기 수정
    가독성을 높이고자 불필요한 띄어쓰기를 없앰.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,11 +15,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-
         if (new HashSet<>(numbers).size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
-
         for (int number : numbers) {
             checkIsValidRange(number);
         }
@@ -28,7 +26,6 @@ public class Lotto {
     private void checkIsValidRange(int input) {
         int MIN = 1;
         int MAX = 45;
-
         if (input < MIN || input > MAX) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
         }
