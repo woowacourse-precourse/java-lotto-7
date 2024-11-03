50643deb0d4a1ddce8b56fc40db4c964f2a343bd
ratatuwee <pkqwaszx@gmail.com>
2024-11-03 16:53:08 +0900

    feat(win record) : 당첨 현황 도메인 추가
    당첨된 현황을 보관하는 도메인을 추가하였습니다.
    당첨 현황은 구매한 직후에 확인되어 변경 및 재생성이 필요 없다고 판단하여 싱글톤으로 구현하였습니다.
    당첨 금액 상수를 보관하게 하는 것이 일관성 있게 접근할 수 있어 해당 도메인에서 불변 리스트로 선언하였습니다.

---
+++ src/main/java/lotto/domain/WinRecord.java
@@ -0,0 +1,65 @@
+package lotto.domain;
+
+import java.util.*;
+
+public class WinRecord {
+    public static final List<Integer> WINNING_PRIZES = Collections.unmodifiableList(Arrays.asList(0, 2_000_000_000, 30_000_000, 1_500_000, 50_000, 5_000));
+    private static WinRecord winRecordInstance;
+    private final List<Integer> winRecord = new ArrayList<>(Collections.nCopies(ValidatorWinRecord.MIN_WIN_RANK + 1, 0));
+
+    private WinRecord(List<Integer> winRecordInput) {
+        new ValidatorWinRecord(winRecordInput);
+        setWinRecord(winRecordInput);
+    }
+
+    public static WinRecord getWinRecord(List<Integer> winRecordInput) {
+        if (winRecordInstance == null) winRecordInstance = new WinRecord(winRecordInput);
+        return winRecordInstance;
+    }
+
+    private void setWinRecord(List<Integer> winRecordInput) {
+        for (Integer winRank : winRecordInput) {
+            if (winRank != ValidatorWinRecord.NO_WIN_NUM) {
+                winRecord.set(winRank, winRecord.get(winRank) + 1);
+            }
+        }
+    }
+
+    public List<Integer> getWinRecordCounts(){
+        return winRecord;
+    }
+
+    public long getTotalWinningPrize() {
+        long totalWinningPrize = 0;
+        for (int i = 1; i < winRecord.size(); i++) {
+            totalWinningPrize += winRecord.get(i) * WINNING_PRIZES.get(i);
+        }
+        return totalWinningPrize;
+    }
+
+    public static void resetInstance() {
+        if ("true".equals(System.getProperty("IS_TEST_ENV"))) {
+            winRecordInstance = null;
+        }
+    }
+
+
+    private static class ValidatorWinRecord {
+        private static final int MIN_WIN_RANK = 1;
+        private static final int MAX_WIN_RANK = 5;
+        private static final int NO_WIN_NUM = 0;
+
+        public ValidatorWinRecord(List<Integer> winRecordInput) {
+            checkBoundaryWinRecode(winRecordInput);
+        }
+
+        private static void checkBoundaryWinRecode(List<Integer> winRecordInput) {
+            for (Integer winRank : winRecordInput) {
+                if (winRank != NO_WIN_NUM && winRank > MAX_WIN_RANK)
+                    throw new IllegalArgumentException("[ERROR] 당첨 등수로 들어올 수 없는 값이 들어왔습니다.");
+                if (winRank != NO_WIN_NUM && winRank < MIN_WIN_RANK)
+                    throw new IllegalArgumentException("[ERROR] 당첨 등수로 들어올 수 없는 값이 들어왔습니다.");
+            }
+        }
+    }
+}
