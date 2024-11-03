9a34c5cb2b24504da1cf619c2063aaf8350e95c9
chanwon <clw8679@naver.com>
2024-11-04 00:15:41 +0900

    style: 당첨 번호 에러 문구 수정

--- src/main/java/lotto/io/Validator.java
+++ src/main/java/lotto/io/Validator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public abstract class Validator {
     private static final String NOT_POSITIVE_NUMBER_ERROR = "[ERROR] 양수를 입력해 주세요";
     private static final String PRICE_NOT_DIVIDED_BY_1000_ERROR = "[ERROR] 로또 구입은 1,000원 단위로 구입해야 합니다.";
-    private static final String WINNING_NUMBER_FORMAT_ERROR = "[ERROR] 당첨 번호는 쉼표로 구분된 6개의 숫자여야 합니다.";
+    private static final String WINNING_NUMBER_FORMAT_ERROR = "[ERROR] 당첨 번호는 공백을 포함하지 않고 쉼표로 구분된 6개의 숫자여야 합니다.";
     private static final String WINNING_NUMBER_RANGE_ERROR = "[ERROR] 당첨 번호는 1~45숫자만 가능합니다.";
     private static final String WINNING_NUMBER_DUPLICATE_ERROR = "[ERROR] 당첨 번호는 중복될 수 없습니다.";
     private static final String BONUS_NUMBER_RANGE_ERROR = "[ERROR] 보너스 번호는 1~45숫자만 가능합니다.";
