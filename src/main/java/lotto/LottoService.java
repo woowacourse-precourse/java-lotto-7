d0517f91823c613906640f5650b4cda42c579911
woongjee <jeewoong7753@naver.com>
2024-11-03 14:21:37 +0900

    feat(LottoService) : 실행 흐름을 관리하는 기능 구현

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -4,5 +4,40 @@ public class LottoService {
     private final Selling selling = new Selling();
     private final Benefit benefit = new Benefit();
 
+    public void run() {
+        try {
+            int amount = getPurchaseAmount();
+            Lotto[] userLottos = selling.purchaseLottos(amount);
+
+            int[] winningNumbers = getWinningNumbers();
+            int bonusNumber = getBonusNumber();
+            Winning winning = new Winning(winningNumbers, bonusNumber);
+
+            int[] ranks = calculateResults(userLottos, winning);
+            displayResults(ranks);
+            displayBenefit(amount, ranks);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private int[] calculateResults(Lotto[] userLottos, Winning winning) {
+    }
+
+    private int getBonusNumber() {
+    }
+
+    private int[] getWinningNumbers() {
+    }
+
+    private int getPurchaseAmount() {
+    }
+
+    private void displayBenefit(int amount, int[] ranks) {
+    }
+
+    private void displayResults(int[] ranks) {
+    }
 
 }
