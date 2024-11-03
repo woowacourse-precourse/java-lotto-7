5215014d8d78c1610a7f85b1e3a21f27fb1a06ec
승일 <seoung1515@naver.com>
2024-11-03 18:57:11 +0900

    build: 실제 기능 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import lotto.dto.InputDTO;
+import lotto.dto.Lotto;
+import lotto.exceptioin.InputException;
+import lotto.handler.OutputHandler;
+import lotto.service.LottoNumberService;
+import lotto.service.LottoResultService;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputException inputException = new InputException();
+        OutputHandler outputHandler = new OutputHandler();
+        LottoNumberService lottoNumberService = new LottoNumberService();
+        LottoResultService lottoResultService = new LottoResultService();
+
+        int money = inputException.getValidMoney();
+
+        List<Lotto> purchasedLottos = lottoNumberService.generateLottos(money);
+        outputHandler.printLottoNumbers(purchasedLottos);
+
+        List<Integer> winningNumbers = inputException.getValidWinningNumbers();
+
+        int bonusNumber = inputException.getValidBonusNumber();
+
+        InputDTO inputDTO = new InputDTO(winningNumbers, bonusNumber, money);
+
+        lottoResultService.calculateWinningsStatistics(inputDTO, purchasedLottos);
+        double profitRate = lottoNumberService.calculateProfitRate(inputDTO, purchasedLottos, money);
+        outputHandler.printProfitRate(profitRate);
     }
 }
