9635a88ee5dd25b605cd4d2bfa130bf8703b34c8
tentenball <lth8905@gmail.com>
2024-11-03 18:03:50 +0900

    test: 로또 최소 구매 금액에 대한 테스트 작성

--- src/test/java/lotto/domain/CustomerTest.java
+++ src/test/java/lotto/domain/CustomerTest.java
@@ -3,10 +3,13 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.domains.customer.Customer;
+import lotto.domains.lotto.Lotto;
 
 public class CustomerTest {
 	@DisplayName("Customer 클래스가 올바르게 생성된다.")
@@ -20,5 +23,12 @@ public class CustomerTest {
 	void 올바른_로또_개수를_계산한다() {
 		Customer customer = Customer.from(3000);
 		assertThat(customer.calculateAmount()).isEqualTo(3);
+	}
+
+	@DisplayName("천원 미만의 금액을 입력하면 예외가 발생한다.")
+	@Test
+	void 천원_미만의_금액을_입력하면_예외가_발생한다() {
+		assertThatThrownBy(() -> Customer.from(999))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
