c45af045f8f2c6de321e4da0d5b0237d96bfec6e
Park, Sojeong <sojeong2101@gmail.com>
2024-11-03 23:39:16 +0900

    test($BonusBallTest): 당첨 번호와 중복될 시 예외 처리 테스트

--- src/test/java/lotto/statistics/BonusBallTest.java
+++ src/test/java/lotto/statistics/BonusBallTest.java
@@ -9,10 +9,20 @@ import org.junit.jupiter.api.Test;
 
 class BonusBallTest {
 
+    List<Integer> winningLotto = List.of(10, 15, 20, 25, 30, 35);
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void shouldThrowIllegalArgumentExceptionWhenIsNotInRangeInputWinningNumbers() {
+        assertThatThrownBy(() -> new BonusBall(10, winningLotto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
     @DisplayName("로또 번호가 1~45가 아니면 예외가 발생한다.")
     @Test
     void shouldThrowIllegalArgumentExceptionWhenIsNotInRangeBonus() {
-        assertThatThrownBy(() -> new BonusBall(65))
+        assertThatThrownBy(() -> new BonusBall(65, winningLotto))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호는 1~45만 가능합니다.");
     }
@@ -21,7 +31,7 @@ class BonusBallTest {
     @Test
     void shouldReturnTrueWhenHasBonus() {
         // give
-        BonusBall bonusBall = new BonusBall(1);
+        BonusBall bonusBall = new BonusBall(1, winningLotto);
         List<Integer> myLotto = List.of(1, 2, 3, 4, 5, 6);
         boolean expectedMatchResult = true;
 
@@ -36,7 +46,7 @@ class BonusBallTest {
     @Test
     void shouldReturnFalseWhenHasNotBonus() {
         // give
-        BonusBall bonusBall = new BonusBall(7);
+        BonusBall bonusBall = new BonusBall(7, winningLotto);
         List<Integer> myLotto = List.of(1, 2, 3, 4, 5, 6);
         boolean expectedMatchResult = false;
 
