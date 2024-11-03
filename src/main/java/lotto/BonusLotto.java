24fe9181107171588e44626565eecc2208f0172d
hhh88261 <hhh88261@gmail.com>
2024-11-03 16:25:20 +0900

    feat : 보너스 번호 검증 기능 추가

--- src/main/java/lotto/BonusLotto.java
+++ src/main/java/lotto/BonusLotto.java
@@ -1,2 +1,23 @@
-package lotto;public class BonusLotto {
+package lotto;
+
+import java.util.List;
+
+public class BonusLotto {
+    private final int bonusNumber;
+    private List<Integer> numbers;
+
+    public BonusLotto(int bonusNumber, List<Integer> numbers) {
+        BonusValidate(bonusNumber, numbers);
+        this.bonusNumber = bonusNumber;
+        this.numbers = numbers;
+    }
+
+    private void BonusValidate(int bonusNumber, List<Integer> numbers) {
+        if(bonusNumber < 1 || bonusNumber > 45 ) {
+            throw new IllegalArgumentException("[ERROR] 1~45 사이의 번호를 입력해야 합니다.");
+        }
+        if(numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 중복되는 로또 번호는 입력할 수 없습니다.");
+        }
+    }
 }
