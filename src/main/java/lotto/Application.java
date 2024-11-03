3385f32f460284a6e823e205a6da3a5c6fc906fc
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-03 23:48:02 +0900

    feat(Application) : 1.3 구입 금액이 양수가 아닌 경우 예외 처리하는 기능
    - 입력된 문자열을 정수로 변환한 결과가 양의 정수가 아니면, IllegalArgumentException 발생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,6 +12,9 @@ public class Application {
         try {
             // 1.2 입력된 구입 금액을 정수로 변환할 수 없는 경우 예외 처리하는 기능
             validateNumericString(purchaseAmountInput);
+            // 1.3 구입 금액이 양수가 아닌 경우 예외 처리하는 기능
+            int purchaseAmount = Integer.parseInt(purchaseAmountInput);
+            validatePositiveNumber(purchaseAmount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -26,4 +29,11 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 숫자로 변환할 수 없는 문자열입니다.");
         }
     }
+
+    private static void validatePositiveNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수가 아닙니다.");
+        }
+    }
+
 }
