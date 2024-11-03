aeb60622b5d6120010def57689ee59e156496d02
dongri <l0o0lv@github.com>
2024-11-03 19:38:15 +0900

    refactor(Constants): 상수 클래스 삭제 후 모든 상수 OutputView 로 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,12 +4,24 @@ import lotto.model.Lotto;
 
 import java.util.List;
 
-import static lotto.util.Constants.INPUT_PURCHASE_AMOUNT;
-import static lotto.util.Constants.OUTPUT_LOTTO_COUNT;
-
 public class OutputView {
+    public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String OUTPUT_LOTTO_COUNT = "개를 구매했습니다.";
+
     public void printInputPurchaseAmount(){
         System.out.println(INPUT_PURCHASE_AMOUNT);
+    }
+
+    public void printInputWinnerNumber(){
+        System.out.println();
+        System.out.println(INPUT_WINNING_NUMBER);
+    }
+
+    public void printInputBonusNumber(){
+        System.out.println();
+        System.out.println(INPUT_BONUS_NUMBER);
     }
 
     public void printOutputLottoCount(int lottoCount){
