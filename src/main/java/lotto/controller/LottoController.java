7e2b33b8f6879957f35cf45182bed0aaa995c5a8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-03 22:54:51 +0900

    feat(controller): 오류 발생시 다시 입력받기
    - 입력 받는 부분에서 오류 발생시 다시 입력받는 기능
    - 로또 당첨번호 객체 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,7 +2,9 @@ package lotto.controller;
 
 import java.util.List;
 import lotto.model.domain.BonusNumber;
+import lotto.model.domain.LottoWinningNumbers;
 import lotto.model.domain.Pocket;
+import lotto.model.domain.UserStatus;
 import lotto.model.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -16,18 +18,54 @@ public class LottoController {
     }
 
     public void run() {
+        List<Integer> winningLottoNumbers;
+        BonusNumber bonusNumber;
+
+        while (true) {
+            try {
+                buyLottoWithMoney();
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                winningLottoNumbers = setWinningNumbers();
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                bonusNumber = setWinningBonusNumber();
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+        LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers(winningLottoNumbers, bonusNumber);
+
+    }
+
+    private void buyLottoWithMoney() {
         String inputMoney = InputView.requestMoney();
         int money = lottoService.moneyValidator(inputMoney);
         Pocket pocket = new Pocket(lottoService.activateLottoMachine(money));
         OutputView.printPurchasedLottoCount(pocket);
+    }
 
+    private List<Integer> setWinningNumbers() {
         String inputWinningNumbers = InputView.requestLottoWinningNumbers();
-        List<Integer> winningLottoNumbers = lottoService.winningNumbersGenerator(inputWinningNumbers);
+        return lottoService.winningNumbersGenerator(inputWinningNumbers);
+    }
 
+    private BonusNumber setWinningBonusNumber() {
         String inputBonusNumber = InputView.requestLottoBonusNumber();
-        BonusNumber bonusNumber = lottoService.bonusNumberGenerator(inputBonusNumber);
-
-        
-
+        return lottoService.bonusNumberGenerator(inputBonusNumber);
     }
 }
