50f5447be51ef53b20c5fe76bda9d441dea98084
CinaDor <cinador@github.com>
2024-11-03 23:56:36 +0900

    refactor: InputController Test
    사용하지 않는 final 변수 제거

--- src/test/java/lotto/controller/InputControllerPurchaseTest.java
+++ src/test/java/lotto/controller/InputControllerPurchaseTest.java
@@ -13,8 +13,6 @@ public class InputControllerPurchaseTest extends NsTest {
     private static final String NOT_INTEGER = "[ERROR] 정수가 아니거나 처리할 수 없는 범위의 정수를 입력 받았습니다.";
     private static final String NOT_PURCHASE_AMOUNT = "[ERROR] 로또 구입 금액은 1,000이상 100,000이하여의 정수여야 합니다.";
     private static final String NOT_ROUND_THOUSAND = "[ERROR] 로또 구입 금액은 1,000으로 나누어 떨어져야 합니다.";
-    private static final String NOT_LOTTO_NUMBER = "[ERROR] 로또 번호는 1이상 45이하여야 합니다.";
-    private static final String DUPLICATE_BONUS_NUMBER = "[ERROR] 입력받은 당첨 번호에 이미 포함된 수입니다.";
 
     InputController inputController = InputController.getInstance();
 
