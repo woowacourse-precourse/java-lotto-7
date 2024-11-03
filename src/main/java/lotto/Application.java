d61db7c045443aecf3dba77de269bb888a6dd2f7
minseojeong <jgoldstone7@gmail.com>
2024-11-03 17:20:04 +0900

    feat: 구입 금액 1000 단위로 입력받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int amount = getAmount();
     }
+
+    public static int getAmount() {
+        String inputAmount = Console.readLine();
+        return validateAmountValue(inputAmount);
+    }
+
+    private static int validateAmountValue(String inputAmount) {
+        try {
+            int amount = Integer.parseInt(inputAmount);
+            if (amount < 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 양수여야 합니다");
+            }
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.")
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 타입입니다.");
+        }
+
+    }
+
 }
