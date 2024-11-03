c973a8f5dbdbb8512278426e540b9740ee3831b8
jijongkwon <help3451@naver.com>
2024-11-03 19:25:59 +0900

    test(util): 1000단위 개수 반환 테스트 코드 구현

--- src/test/java/lotto/util/LottoParserTest.java
+++ src/test/java/lotto/util/LottoParserTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoParserTest {
 
@@ -17,5 +19,16 @@ class LottoParserTest {
 
         // then
         assertEquals(List.of(1, 2, 3, 4, 5, 6), parsingNumbers);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1000, 1",
+            "2000, 2",
+            "10000, 10",
+            "20000, 20"
+    })
+    void 천원단위로_개수_반환하다(int price, int expected) {
+        assertEquals(expected, LottoParser.parsingPrice(price));
     }
 }
