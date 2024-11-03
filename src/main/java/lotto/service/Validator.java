aae7c25a733c1353d65e38dd5480c557ce7734ab
j4821_s <sonhm48021@gmail.com>
2024-11-03 21:54:02 +0900

    feat: 예외 처리 서비스 목록 작성
    - 미입력 처리
    - 양수가 아닌 수 입력 처리
    - 문자열 숫자 오입력 처리
    - 지불 금액 단위 미준수 입력 처리
    - 로또 번호 리스트 오입력 처리
    - 로또 번호 입력 개수 미준수 처리
    - 로또 번호 입력 범위 처리

---
+++ src/main/java/lotto/service/Validator.java
@@ -0,0 +1,57 @@
+package lotto.service;
+
+public class Validator {
+    public void handleEmptyInput() {
+        throw new IllegalArgumentException(
+            "[ERROR] 아무것도 입력하지 않았습니다. 다시 입력해주세요."
+        );
+    }
+
+    public void validateNegativeInput(int input) {
+        if(input <= 0) {
+            throw new IllegalArgumentException(
+                "[ERROR] 0 또는 음수룰 입력할 수 없습니다 : " + input
+            );
+        }
+    }
+
+    public void handleNonNumericInput() {
+        throw new IllegalArgumentException(
+            "[ERROR] 숫자 외 다른 문자가 입력되었습니다."
+        );
+    }
+
+    public void validateThousandUnitInput(int money) {
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException(
+                "[ERROR] 지불 금액은 1000원 단위로 입력해주세요."
+            );
+        }
+    }
+
+    public void handleInvalidCharacterInNumber() {
+        throw new IllegalArgumentException(
+            "[ERROR] 숫자, 쉼표(,) 외 다른 문자가 입력되었습니다."
+        );
+    }
+
+    public void handleEmptyInputInNumbers() {
+        throw new IllegalArgumentException(
+            "[ERROR] 입력되지 않은 번호가 있습니다."
+        );
+    }
+
+    public void handleInvalidNumberCount(int size) {
+        throw new IllegalArgumentException(
+            "[ERROR] 6개의 숫자를 입력해야 합니다. size: " + size
+        );
+    }
+
+    public void validateNumberInRange(int number) {
+        if(number < 1 || number > 45) {
+            throw new IllegalArgumentException(
+                "[ERROR] 1~45 사이의 숫자만 입력해주세요 : " + number
+            );
+        }
+    }
+}
