236fec85646d581c30374fe40778bd4418d99190
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:33:50 +0900

    feat: 랜덤 로또 발행 결과, 일치 결과, 수익률 outputView 로직 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,26 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
+import lotto.domain.LottoRank;
 
 public class OutputView {
-    public void result(List<List<Integer>> list){
+    public void result(List<List<Integer>> lottoTickets) {
         System.out.println();
-        System.out.println(list.size() + "개를 구매했습니다.");
-        for(List<Integer> l : list){
-            System.out.println(l);
+        System.out.println(lottoTickets.size() + "개를 구매했습니다.");
+        for (List<Integer> lotto : lottoTickets) {
+            System.out.println(lotto);
         }
+    }
+
+    public void printresult(Map<String, Integer> result) {
+        for (LottoRank rank : LottoRank.values()) {
+            int num = result.get(rank.name());
+            System.out.println(rank.getMatchCount() + "개 일치 " + rank.getOutputPrize() + "원" + " - " + num + "개");
+        }
+    }
+
+    public void printRate(double num) {
+        System.out.println("총 수익률은 " + String.format("%.2f", num) + "%입니다.");
     }
 }
