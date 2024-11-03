3203184ad3b4c44db74135fdf1b1ecdee387175d
taek2222 <oot309@daum.net>
2024-11-04 07:06:21 +0900

    feat(LottoValidator): 로또 검증 클래스 정의
    - 재사용에 용이하도록, 로또 관련 검증 로직을 분리했습니다.

---
+++ src/main/java/lotto/util/LottoValidator.java
@@ -0,0 +1,36 @@
+package lotto.util;
+
+import static lotto.constant.ErrorMessage.*;
+import static lotto.constant.LottoConstant.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoValidator {
+
+    public static void validateNumberCount(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_LENGTH.getValue()) {
+            throw new IllegalArgumentException(INVALID_LOTTO_LENGTH.getMessage());
+        }
+    }
+
+    public static void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateSingleNumberRange(number);
+        }
+    }
+
+    public static void validateNoDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateSingleNumberRange(int number) {
+        if (number < NUMBER_RANGE_MIN.getValue() || number > NUMBER_RANGE_MAX.getValue()) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getMessage());
+        }
+    }
+}
