8839f285c0f93f96bde899f54fef685926c460a4
minzzun99 <skg06123@naver.com>
2024-11-03 18:40:24 +0900

    feat(WinningResult): 당첨된 로또의 개수와 당첨 금액을 관리하는 WinningResult 클래스 추가
    - Map을 사용해서 당첨된 로또를 관리
    - 당첨 로또와 구매한 로또의 숫자를 비교하는 기능
    - 로또의 랭크를 반환하는 기능
    - 당첨 통계를 반환하는 기능
    - 수익률을 계산하는 기능

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,87 @@
+package lotto.domain;
+
+import static lotto.constant.LottoMessage.COUNT_MESSAGE;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WinningResult {
+    private static final String ZERO_PERCENT = "0.0";
+    private static final String TENTH_PLACE_VALUE = "%.1f";
+    private static final int COUNT_INITIALIZE = 0;
+
+    private final Map<WinningRank, Integer> winningCount;
+    private int prizeMoney;
+    private final WinningRank[] winningRanks = WinningRank.values();
+
+    public WinningResult() {
+        this.winningCount = new HashMap<>();
+
+        for (int i = 1; i < winningRanks.length; i++) {
+            winningCount.put(winningRanks[i], COUNT_INITIALIZE);
+        }
+    }
+
+    public void checkPurchaseLotto(PurchaseLotto purchaseLotto, WinningLotto winningLotto) {
+        List<Integer> winningNumbers = winningLotto.getWinningLotto().getNumbers();
+        int bonusNumber = winningLotto.getBonusNumber();
+
+        for (Lotto lotto : purchaseLotto.getPurchaseLotto()) {
+            WinningRank winningRank = compareLottoNumbers(lotto, winningNumbers, bonusNumber);
+            updateWinningCount(winningRank);
+        }
+    }
+
+    private WinningRank compareLottoNumbers(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
+        int matchCount = 0;
+        boolean matchBonus = false;
+
+        for (int number : lotto.getNumbers()) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+                continue;
+            }
+
+            if (bonusNumber == number) {
+                matchBonus = true;
+            }
+        }
+        return getLottoRank(matchCount, matchBonus);
+    }
+
+    private WinningRank getLottoRank(int matchCount, boolean matchBonus) {
+        return WinningRank.getRank(matchCount, matchBonus);
+    }
+
+    private void updateWinningCount(WinningRank winningRank) {
+        if (!winningRank.equals(WinningRank.NONE)) {
+            winningCount.put(winningRank, winningCount.get(winningRank) + 1);
+            prizeMoney += winningRank.getPrizeMoney();
+        }
+    }
+
+    public String getWinningStatistics() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < winningRanks.length; i++) {
+            sb.append(winningRanks[i].getMessage())
+                    .append(winningCount.get(winningRanks[i]))
+                    .append(COUNT_MESSAGE.getMessage());
+        }
+
+        return sb.toString();
+    }
+
+    public String getRateOfReturn(int amount) {
+        if (prizeMoney == 0) {
+            return ZERO_PERCENT;
+        }
+
+        return String.format(TENTH_PLACE_VALUE, getRate(amount));
+    }
+
+    private double getRate(int amount) {
+        return (double) prizeMoney / amount * 100;
+    }
+}
