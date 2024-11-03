b2fe4d350fe1ccebe44afa5d002324dec65dba5a
mete0rfish <sungwon326@naver.com>
2024-11-03 18:46:15 +0900

    fix(view): 출력 구문 추가

--- src/main/java/lotto/view/LottoInput.java
+++ src/main/java/lotto/view/LottoInput.java
@@ -7,14 +7,17 @@ import java.util.List;
 
 public class LottoInput {
     public static String inputCost() {
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
     public static List<String> inputCorrectNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         return Arrays.stream(Console.readLine().split(",")).toList();
     }
 
     public static String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 }
