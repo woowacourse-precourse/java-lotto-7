87f590f583bfd47871c8cb59c93515adbc4aeb63
GO-TE <alsrudxo9810@naver.com>
2024-11-03 10:50:21 +0900

    feat(Lotto): 로또 숫자 중복 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -11,6 +11,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumberCount(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        boolean hasDuplication = numbers.size() != numbers.stream()
+                .distinct()
+                .count();
+        if (hasDuplication) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
