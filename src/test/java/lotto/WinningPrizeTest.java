3f63f48a68ffa879d944ebbc9c2ce338adba3ba3
sihyun10 <hyhy200170@gmail.com>
2024-11-04 01:29:57 +0900

    test(WinningPrizeTest) : 해당 LottoPrize 반환 여부
    입력된 당첨번호와 보너스번호를 가지고, 사용자의 로또 티켓 번호들과
    일치여부를 판단하고, 각 당첨 기준에 맞게 잘 반환하고 있는지
    확인하였습니다. 특히 당첨기준에 해당하지 않는 두개 번호 이하부터
    일치하면 당첨이 안되는것도 잘 돌아가는지 확인할 수 있었다.

---
+++ src/test/java/lotto/WinningPrizeTest.java
@@ -0,0 +1,62 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
+import lotto.domain.WinningPrize;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningPrizeTest {
+    private WinningPrize winningPrize;
+
+    @BeforeEach
+    void setUp() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+        winningPrize = new WinningPrize(winningNumbers, bonusNumber);
+    }
+
+    @Test
+    @DisplayName("6개 번호 일치하면 1등 당첨")
+    void 여섯개번호_일치하면_1등_당첨() {
+        //given
+        Lotto userLottoTicket = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        //when
+        LottoPrize prize = winningPrize.getPrize(userLottoTicket);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.FIRST);
+    }
+
+    @Test
+    @DisplayName("5개 번호 + 보너스 번호 일치하면 2등 당첨")
+    void 다섯개번호_보너스번호_다_일치하면_2등_당첨() {
+        //given
+        Lotto userLottoTicket = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        //when
+        LottoPrize prize = winningPrize.getPrize(userLottoTicket);
+
+        //then
+        assertThat(prize).isEqualTo(LottoPrize.SECOND);
+    }
+
+    @Test
+    @DisplayName("2개 번호 이하 일치하면 당첨이 안된다")
+    void 두개번호_이하_일치하면_당첨이_안된다() {
+        //given
+        Lotto userLottoTicket = new Lotto(Arrays.asList(1, 20, 24, 30, 40, 45));
+
+        //when
+        LottoPrize prize = winningPrize.getPrize(userLottoTicket);
+
+        //then
+        assertThat(prize).isNull();
+    }
+}
