1ccb89d6a46f876668c049a126bd59d904fe0a8c
bogyeom0922 <pbk0398@gmail.com>
2024-11-04 03:01:40 +0900

    Feat: 당첨 통계 출력
    일치하는 로또의 개수, 총 수익률 출력

--- src/main/java/lotto/view/OutputWriter.java
+++ src/main/java/lotto/view/OutputWriter.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class OutputWriter {
@@ -10,5 +11,16 @@ public class OutputWriter {
         for (Set<Integer> lottoNumbers : totalLottoNumbers) {
             System.out.println(lottoNumbers);
         }
+    }
+
+    public void printWinningStatistics(String winningRate, Map<Integer, Integer> totalMatchCounts, int bonusCount) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + totalMatchCounts.get(3) + "개");
+        System.out.println("4개 일치 (50,000원) - " + totalMatchCounts.get(4) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + totalMatchCounts.get(5) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + bonusCount + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + totalMatchCounts.get(6) + "개");
+        System.out.println("총 수익률은 " + winningRate + "%입니다.");
     }
 }
