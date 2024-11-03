3c0a8e4f63c9709e4bf5a9fd12bb17e32f59b2c4
Koo-EunSung <ku903@naver.com>
2024-11-03 20:08:00 +0900

    Feat: 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,18 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_MONEY_INFORM_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String INPUT_MONEY_INFORM_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
+
     public String getMoney() {
         System.out.println(INPUT_MONEY_INFORM_MESSAGE);
+
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String getWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
 
         String input = Console.readLine();
         return input;
