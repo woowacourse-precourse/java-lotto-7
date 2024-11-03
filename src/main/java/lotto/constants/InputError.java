ae75c562994b247e957ebdfb7f9e833db7269909
whrjsgml0000 <whrjsgml4515@naver.com>
2024-11-03 22:53:15 +0900

    refactor(constants) : 코드 가독성 향상

--- src/main/java/lotto/constants/InputError.java
+++ src/main/java/lotto/constants/InputError.java
@@ -7,14 +7,10 @@ public enum InputError {
     PURCHASE_PRICE_SHOULD_EXIST("로또 구입 금액을 입력하셔야 합니다."),
     PURCHASE_PRICE_NOT_A_NUMBER("로또 구입 금액에는 숫자만 입력하셔야 합니다."),
     PURCHASE_PRICE_NOT_REACH_AT_LEAST("최소 한 개 이상의 로또를 구입할 수 있는 가격을 입력하셔야 합니다."
-            + "(최소 금액 : "
-            + LottoInteger.LOTTO_PRICE.getValue()
-            + "원)"),
-    PURCHASE_PRICE_OVER_PROGRAM_MAX(Integer.MAX_VALUE
-            + " 보다 큰 금액은 지원하지 않습니다."),
+            + "(최소 금액 : " + LottoInteger.LOTTO_PRICE.getValue() + "원)"),
+    PURCHASE_PRICE_OVER_PROGRAM_MAX(Integer.MAX_VALUE + " 보다 큰 금액은 지원하지 않습니다."),
     PURCHASE_PRICE_SHOULD_BE_DIVIDED_CLEARLY("로또 구입 금액에는 "
-            + LottoInteger.LOTTO_PRICE.getValue()
-            + "으로 나눠지는 수를 입력하셔야 합니다."),
+            + LottoInteger.LOTTO_PRICE.getValue() + "으로 나눠지는 수를 입력하셔야 합니다."),
     // 로또 당첨 번호 예외
     LOTTO_WIN_NUMBER_SHOULD_EXIST("로또 당첨 번호를 입력하셔야 합니다."),
     LOTTO_WIN_NUMBER_CONTAIN_BAD_INPUT("로또 당첨 번호에는 숫자 "
