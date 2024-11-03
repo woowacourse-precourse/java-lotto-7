3c66d2bd22a16d2fe0e1cb0026f2e48a6b1960d8
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-03 21:17:38 +0900

    feat: 입력값 변환하는 parse 메소드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,9 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
 
     static final String INPUT_ORDER_PRICE = "구입금액을 입력해 주세요.";
@@ -21,5 +24,20 @@ public class InputView {
     public static String inputBounsNumber(){
         System.out.println(INPUT_BONUS_NUMBER);
         return Console.readLine();
+    }
+
+    public static int parseOrder(String inputOrderPrice){
+        // Valid 추가 ( 1000원 단위 )
+        return Integer.parseInt(inputOrderPrice)/1000;
+    }
+
+    public static List<Integer> parseWinningNumber(String inputWinningNumber){
+        return Arrays.stream(inputWinningNumber.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+    }
+    public static int parseBonusNumber(String inputBonusNumber) {
+        return Integer.parseInt(inputBonusNumber);
     }
 }
