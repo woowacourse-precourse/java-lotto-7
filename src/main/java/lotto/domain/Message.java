0342838387f1adb1c0d846f066a839dda7d012ec
fortune-man <wnguddl96@knou.ac.kr>
2024-11-04 07:35:00 +0900

    refactor: enum 갱성자 추가

--- src/main/java/lotto/domain/Message.java
+++ src/main/java/lotto/domain/Message.java
@@ -1,7 +1,16 @@
 package lotto.domain;
 
 public enum Message {
-  FIRST("6개 일치 (2,000,000,000원) - 0"), SECOND("4개 일치 (50,000원) - 0"), THIRD("5개 일치 (1,500,000원) - 0개"), FOURTH("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개"), FIFTH("3개 일치 (5,000원) - 1개\n");
+  FIRST("6개 일치 (2,000,000,000원) - 0"),
+  SECOND("4개 일치 (50,000원) - 0"),
+  THIRD("5개 일치 (1,500,000원) - 0개"),
+  FOURTH("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개"),
+  FIFTH("3개 일치 (5,000원) - 1개\n");
+
+  Message(String message) {
+    this.message = message;
+  }
+
   private String message;
 
   public String getMessage() {
