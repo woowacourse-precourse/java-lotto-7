75d73d3e35c1c6429b73771396407136142955c6
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:58:48 +0900

    feat: LottoChecker 클래스 구현

--- src/main/java/lotto/model/LottoChecker.java
+++ src/main/java/lotto/model/LottoChecker.java
@@ -1,4 +1,59 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import lotto.Rank;
+
 public class LottoChecker {
+
+    private final Lottos lottos;
+    private final WinningNumber winningNumber;
+    private List<Rank> result = new ArrayList<>();
+
+    public LottoChecker(Lottos lottos, WinningNumber winningNumber) {
+        this.lottos = lottos;
+        this.winningNumber = winningNumber;
+    }
+
+    public List<Rank> checkLottos() {
+        for (Lotto lotto : lottos.getLottos()) {
+            result.add(checkLotto(lotto));
+        }
+
+        return result;
+    }
+
+    private Rank checkLotto(Lotto lotto) {
+        int match = 0;
+        boolean isBonus = false;
+
+        for (int number : lotto.getNumbers()) {
+
+            if (winningNumber.getWinningNumbers().contains(number)) {
+                match += 1;
+            }
+
+            if (match == 5 && lotto.getNumbers().contains(winningNumber.getBonusNumber())) {
+                isBonus = true;
+            }
+        }
+
+        return Rank.of(match, isBonus);
+    }
+
+    public List<Rank> getResult() {
+        return result;
+    }
+
+    public Map<Integer, Integer> getResultMap() {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        for (Rank rank : result) {
+            resultMap.put(rank.getPlace(), resultMap.getOrDefault(rank.getPlace(), 0) + 1);
+        }
+
+        return resultMap;
+    }
 }
