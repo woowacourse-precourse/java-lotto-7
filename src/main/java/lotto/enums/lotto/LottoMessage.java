3371734e467676a585229f69a296b63f81a73cfb
jijongkwon <help3451@naver.com>
2024-11-03 17:57:07 +0900

    feat(enums): exception 메세지 추가

--- src/main/java/lotto/enums/lotto/LottoMessage.java
+++ src/main/java/lotto/enums/lotto/LottoMessage.java
@@ -4,9 +4,11 @@ public enum LottoMessage {
     // print
     PRINT_INPUT_LOTTO_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PRINT_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    PRINT_INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요.");
+    PRINT_INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
 
-    // error
+    // exception
+    EXCEPTION_NUMBER_VALID("[ERROR] 올바른 형식의 숫자가 아닙니다."),
+    EXCEPTION_PRICE_UNIT("[ERROR] 1,000원 단위가 아닙니다.");
 
     private final String message;
 
