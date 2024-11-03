aa7fdcec0d6dcfc9dc1de5e1889f56dcdf032fab
encoreJeong <zzaibean@gmail.com>
2024-11-04 00:20:12 +0900

    refactor: LottoGameSetter. set() 메서드의 책임을 분산

--- src/main/java/lotto/LottoGameSetter.java
+++ src/main/java/lotto/LottoGameSetter.java
@@ -11,32 +11,35 @@ public class LottoGameSetter {
 
     public LottoGame set() {
 
-        System.out.println("구입금액을 입력해 주세요.");
-        Integer totalPrice = Integer.parseInt(readLine());
-        if(totalPrice % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 1000의 배수인 정수를 입력해야 합니다.");
+        Integer totalPrice = setTotalPrice();
+
+        Integer totalLottoCount = setTotalLottoCount(totalPrice);
+
+        List<Lotto> lottos = drawLottery(totalLottoCount);
+
+        printDrawingResult(lottos);
+
+        Set<Integer> winningNumbers = setWinningNumbers();
+
+        Integer bonusNumber = setBonusNumber(winningNumbers);
+
+        return LottoGame.of(totalPrice, lottos, winningNumbers, bonusNumber);
+    }
+
+    private static Integer setBonusNumber(Set<Integer> winningNumbers) {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusNumberInput = readLine();
+        Integer bonusNumber = Integer.parseInt(bonusNumberInput);
+        if(bonusNumber > 45 || bonusNumber <1) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 한가지 수여야 합니다.");
         }
-
-        Integer totalLottoCount = totalPrice / 1000;
-        List<Lotto> lottos = new ArrayList<>();
-        for(int i = 0; i < totalLottoCount; i++) {
-            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(lotto);
-            lottos.add(new Lotto(lotto));
+        if(winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 한가지 수여야 합니다.");
         }
+        return bonusNumber;
+    }
 
-        StringBuilder lottoPurchaseResult = new StringBuilder();
-        lottoPurchaseResult.append("\n").append(totalLottoCount).append("개를 구매했습니다.\n");
-        lottos.stream()
-                .forEach(lotto -> {
-                    lottoPurchaseResult.append("[");
-                    lottoPurchaseResult.append(lotto.getNumbers().stream().map(String::valueOf).collect(Collectors.joining(", ")));
-                    lottoPurchaseResult.append("]\n");
-                });
-
-        System.out.println(lottoPurchaseResult);
-
-
+    private static Set<Integer> setWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = readLine();
         String[] inputNumbers = input.split(",");
@@ -55,17 +58,43 @@ public class LottoGameSetter {
         if(winningNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 서로 중복되지 않는 1~45 사이의 6가지 수여야 합니다.");
         }
+        return winningNumbers;
+    }
 
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        String bonusNumberInput = readLine();
-        Integer bonusNumber = Integer.parseInt(bonusNumberInput);
-        if(bonusNumber > 45 || bonusNumber <1) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 한가지 수여야 합니다.");
-        }
-        if(winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45 사이의 한가지 수여야 합니다.");
-        }
+    private static void printDrawingResult(List<Lotto> lottos) {
+        StringBuilder lottoPurchaseResult = new StringBuilder();
+        lottoPurchaseResult.append("\n").append(lottos.size()).append("개를 구매했습니다.\n");
+        lottos.stream()
+                .forEach(lotto -> {
+                    lottoPurchaseResult.append("[");
+                    lottoPurchaseResult.append(lotto.getNumbers().stream().map(String::valueOf).collect(Collectors.joining(", ")));
+                    lottoPurchaseResult.append("]\n");
+                });
 
-        return LottoGame.of(totalPrice, lottos, winningNumbers, bonusNumber);
+        System.out.println(lottoPurchaseResult);
+    }
+
+    private static List<Lotto> drawLottery(Integer totalLottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < totalLottoCount; i++) {
+            List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(lotto);
+            lottos.add(new Lotto(lotto));
+        }
+        return lottos;
+    }
+
+    private static Integer setTotalLottoCount(Integer totalPrice) {
+        Integer totalLottoCount = totalPrice / 1000;
+        return totalLottoCount;
+    }
+
+    private static Integer setTotalPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        Integer totalPrice = Integer.parseInt(readLine());
+        if(totalPrice % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1000의 배수인 정수를 입력해야 합니다.");
+        }
+        return totalPrice;
     }
 }
