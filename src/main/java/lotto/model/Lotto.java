c9e9b8c292eef1cf43084f9f57bb505ec0f0bbbf
ddddabi <ii887742@gmail.com>
2024-11-03 23:16:08 +0900

    fix: 로또 번호 생성 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -32,7 +32,7 @@ public class Lotto {
     public static List<Lotto> generateLottos(int count) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             Collections.sort(numbers);
             lottos.add(new Lotto(numbers));
         }
