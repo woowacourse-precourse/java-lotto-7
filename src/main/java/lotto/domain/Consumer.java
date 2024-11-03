40c2a6ebade641a24cd58751b87a6ae60b64175c
JS <zks145@naver.com>
2024-11-03 14:36:17 +0900

    feat : 로또 랭킹 초기값 생성

--- src/main/java/lotto/domain/Consumer.java
+++ src/main/java/lotto/domain/Consumer.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static lotto.validation.Validation.*;
 
@@ -9,13 +11,15 @@ public class Consumer {
 
     private final static int MAX_COST = 100000;
     private final static int MIN_COST = 1000;
-
+    private final static int ZERO = 0;
     private final ArrayList<Lotto> lottoes = new ArrayList<>();
+    private final HashMap<LottoRank, Integer> lottoRankResult = new HashMap<>();
     private final int money;
 
     public Consumer(int money) {
         validateMoney(money);
         this.money = money;
+        initLottoRank();
     }
 
     private void validateMoney(int money) {
@@ -35,5 +39,13 @@ public class Consumer {
     public ArrayList<Lotto> getLottoes() {
         return lottoes;
     }
+
+    public void initLottoRank() {
+        for (LottoRank rank : LottoRank.values()) {
+            lottoRankResult.put(rank, ZERO);
+        }
+    }
+
+
 
 }
