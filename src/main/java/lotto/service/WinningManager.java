cb27df37de697c626283170b761bdb55a496573b
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:52:56 +0900

    feat: 모든 로또 번호에 대해서 당첨 여부 계산하는 매니저

--- src/main/java/lotto/service/WinningManager.java
+++ src/main/java/lotto/service/WinningManager.java
@@ -1,4 +1,23 @@
 package lotto.service;
 
-public class WinningCheckManager {
+import java.util.Map;
+import lotto.domain.LottoRank;
+import lotto.domain.WinningChecker;
+import lotto.repository.LottoRepository;
+
+public class WinningManager {
+    public Map<String, Integer> checkWinning(LottoRepository lottoRepository) {
+        Map<String, Integer> matchingResult = new WinningChecker(lottoRepository).checkWinning();
+        calculateRate(matchingResult, lottoRepository);
+        return matchingResult;
+    }
+
+    public double calculateRate(Map<String, Integer> matchingResult, LottoRepository lottoRepository) {
+        double sum = 0;
+        for (LottoRank rank : LottoRank.values()) {
+            sum += rank.getPrize() * matchingResult.get(rank.name());
+        }
+        double result = sum / lottoRepository.getLottoNumbers().size();
+        return result;
+    }
 }
