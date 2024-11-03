c2e5103c8f483e9248bf0223ce654c0221936447
GoGradually <chkun3109@gmail.com>
2024-11-03 19:41:31 +0900

    feat(Lotto.java): 로또 객체 생성 기능 구현
    - 객체 생성 기능 구현
    - 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -8,13 +9,24 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        this.numbers.sort(Integer::compareTo);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if(numbers.stream().distinct().count() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정렬된 상태로 입력되어야 합니다.");
+        }
+        if(numbers.getFirst() < 1 || numbers.getLast() > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1 이상 45 이하여야 합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return numbers.stream().toList();
+    }
 }
