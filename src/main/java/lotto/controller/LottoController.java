893b244d606b239c7b25fb58d56dad70cb1f75bc
smyeongL <sangmyeonglee123@gmail.com>
2024-11-03 22:13:22 +0900

    feat(controller): add lotto controller
    Call service logic for the generating lotto

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import lotto.dto.PurchaseAmountRequestDto;
+import lotto.model.Lottos;
+import lotto.service.genaration.GenerateService;
+import lotto.view.OutputView;
+
+public class LottoController {
+    public static void run(PurchaseAmountRequestDto purchaseAmountRequestDto) {
+        int numberOfLotto = purchaseAmountRequestDto.getPurchaseAmount() / 1000;
+        GenerateService lottoGenerator = new GenerateService(numberOfLotto);
+
+        Lottos lottos = lottoGenerator.generateLotos();
+        OutputView.printLottos(lottos);
+    }
+}
