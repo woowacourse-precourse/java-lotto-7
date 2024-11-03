584fe1e87ebfb7ec5c928e36a382736e374723d4
sangjun <sangjoon12345@gmail.com>
2024-11-03 19:53:54 +0900

    feat: 로또 당첨 결과 출력 기능 구현

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,16 +3,17 @@ package lotto.model;
 import java.util.HashMap;
 
 public enum LottoRank {
-    FIRST(6,2000000000),
-    SECOND(5,30000000,true),
-    THIRD(5,1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    MISS(0,0);
+    FIRST(6,2000000000,"6개 일치 (2,000,000,000원) - "),
+    SECOND(5,30000000,true,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5,1500000,"5개 일치 (1,500,000원) - "),
+    FOURTH(4,50000,"4개 일치 (50,000원) - "),
+    FIFTH(3,5000,"3개 일치 (5,000원) - "),
+    MISS(0,0,"낙첨");
 
     private final int hitCount;
     private final int prizeMoney;
     private final boolean hasBonus;
+    private final String message;
 
     private static final HashMap<Integer,LottoRank> rankMap = new HashMap<>();
 
@@ -21,15 +22,21 @@ public enum LottoRank {
             rankMap.put(lottoRank.hitCount, lottoRank);
         }
     }
-    LottoRank(int hitCount, int prizeMoney){
+    LottoRank(int hitCount, int prizeMoney, String message){
         this.hitCount = hitCount;
         this.prizeMoney = prizeMoney;
         this.hasBonus = false;
+        this.message = message;
     }
-    LottoRank(int hitCount, int prizeMoney, boolean hasBonus){
+    LottoRank(int hitCount, int prizeMoney, boolean hasBonus, String message){
         this.hitCount = hitCount;
         this.prizeMoney = prizeMoney;
         this.hasBonus = hasBonus;
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
     }
 
     public int getHitCount(){
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,18 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
     private static final String INFORM_LOTTO_COUNT = "%d개를 구매했습니다.";
     private static final String REQUEST_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_LOTTO = "보너스 번호를 입력해 주세요.";
+    private static final String STARTING_WINNING_RESULT_MESSAGE = "당첨 통계";
     private static final String NEW_LINE = System.lineSeparator();
 
     public void requestLottoPrice() {
@@ -31,5 +35,17 @@ public class OutputView {
     public void requestBonusLottoNumbers(){
         System.out.print(NEW_LINE);
         System.out.println(REQUEST_BONUS_LOTTO);
+    }
+
+    public void displayWinningResults(Map<LottoRank,Long>lottoResults){
+        System.out.print(NEW_LINE);
+        System.out.println(STARTING_WINNING_RESULT_MESSAGE + NEW_LINE + "---" + NEW_LINE);
+        for(LottoRank rank : LottoRank.values()){
+            if(rank == LottoRank.MISS){
+                continue;
+            }
+            long count = lottoResults.getOrDefault(rank,0L);
+            System.out.println(rank.getMessage() + count);
+        }
     }
 }
