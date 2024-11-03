92402352504c4c2ef8a1766d0d79610f380e0401
smyeongL <sangmyeonglee123@gmail.com>
2024-11-04 02:11:01 +0900

    feat(util): add error messages for handling winning numbers

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -1,7 +1,12 @@
 package lotto.util;
 
 public class ErrorMessage {
-    public static final String EMPTY_STRING_ERROR = "[ERROR] Purchase Amount is empty";
+    public static final String EMPTY_PURCHASE_AMOUNT_STRING_ERROR = "[ERROR] Purchase Amount is empty";
     public static final String NOT_INTEGER_STRING_ERROR = "[ERROR] Purchase Amount is not Integer Format";
     public static final String NOT_DIVISIBLE_BY_1000_ERROR = "[ERROR] Purchase Amount is not divisible by 1000";
+
+    public static final String EMPTY_WINNING_NUMBERS_STRING_ERROR = "[ERROR] Winning Numbers is empty";
+    public static final String INVALID_WINNING_NUMBERS_LENGTH_ERROR = "[ERROR] Winning Numbers has invalid length";
+    public static final String WINNING_NUMBER_NOT_INTEGER_ERROR = "[ERROR] Winning Number is not Integer";
+    public static final String WINNING_NUMBER_NOT_VALID_RANGE = "[ERROR] Winning Numbers is not between 1-45";
 }
