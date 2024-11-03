6d35d68431644975aeea8ee19180d818af69e69a
dd-jiyun <dlwldbs8015@gmail.com>
2024-11-03 19:15:12 +0900

    feat: 로또 게임 초기화 및 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.handler.PurchaseRequestHandler;
+import lotto.model.handler.WinningLottoRequestHandler;
+import lotto.service.LottoService;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PurchaseRequestHandler purchaseRequestHandler = new PurchaseRequestHandler();
+        WinningLottoRequestHandler winningLottoRequestHandler = new WinningLottoRequestHandler();
+
+        LottoService lottoGameService = new LottoService(purchaseRequestHandler, winningLottoRequestHandler);
+
+        LottoController lottoController = new LottoController(lottoGameService);
+
+        lottoController.run();
     }
 }
