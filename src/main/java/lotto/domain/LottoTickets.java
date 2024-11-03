ba89e7d77739d13ab2ddb4b6c69fb75882d1091d
qyinm <qusseun@gmail.com>
2024-11-03 13:26:02 +0900

    feat: 로또 등수 별 개수 map 반환 기능 작성

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoTickets {
@@ -18,5 +22,25 @@ public class LottoTickets {
 
     public int getLottoCount() {
         return lottos.size();
+    }
+
+    public Map<LottoPrize, Integer> getLottoPrizesMap(WinningLotto winningLotto) {
+        // EnumMap 초기화
+        Map<LottoPrize, Integer> lottoPrizes = new EnumMap<>(LottoPrize.class);
+
+        // 모든 LottoPrize에 대해 0으로 초기화
+        for (LottoPrize lottoPrize : LottoPrize.values()) {
+            lottoPrizes.put(lottoPrize, 0);
+        }
+
+        // lottos의 각 로또에 대해 당첨 번호를 계산하고, 결과를 lottoPrizes에 저장
+        lottos.stream()
+                .map(winningLotto::getLottoPrize)
+                .filter(lottoPrize -> lottoPrize != LottoPrize.NO_PRIZE)
+                .forEach(lottoPrize ->
+                        lottoPrizes.put(lottoPrize, lottoPrizes.get(lottoPrize) + 1) // 카운트 증가
+                );
+
+        return lottoPrizes;
     }
 }
