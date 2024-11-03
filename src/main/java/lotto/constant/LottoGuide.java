0117a473d48b7c818dc72c9230e7ea5fc4e0039b
33jyu33 <jy04169@gmail.com>
2024-11-04 01:41:59 +0900

    feat(OutputView): output the results of each rank
    Function printResult:
    - printResultGuide()
    - Results for each number of matching numbers
    - The second place is the number of bonusNumber
    - Third place is the number of lotto
    that match five numbers minus the number of second place

--- src/main/java/lotto/constant/LottoGuide.java
+++ src/main/java/lotto/constant/LottoGuide.java
@@ -4,7 +4,14 @@ public enum LottoGuide {
     PRICE_GUIDE("구입금액을 입력해 주세요."),
     PURCHASE_COUNT("%d개를 구매했습니다."),
     WINNING_NUMBER_GUIDE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_GUIDE("보너스 번호를 입력해 주세요.");
+    BONUS_NUMBER_GUIDE("보너스 번호를 입력해 주세요."),
+    RESULT_GUIDE("당첨 통계"),
+    LINE_SEPARATOR("---"),
+    RESULT_FIFTH("3개 일치 (5,000원) - %d개\n"),
+    RESULT_FOURTH("4개 일치 (50,000원) - %d개\n"),
+    RESULT_THIRD("5개 일치 (1,500,000원) - %d개\n"),
+    RESULT_SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
+    RESULT_FIRST("6개 일치 (2,000,000,000원) - %d개\n");
 
     private final String message;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -22,8 +22,22 @@ public class OutputView {
         System.out.println(LottoGuide.WINNING_NUMBER_GUIDE.getMessage());
     }
 
-    public static void printBonusNumberGuide(){
+    public static void printBonusNumberGuide() {
         System.out.println(LottoGuide.BONUS_NUMBER_GUIDE.getMessage());
+    }
+
+    public static void printResultGuide(){
+        System.out.println(LottoGuide.RESULT_GUIDE.getMessage());
+        System.out.println(LottoGuide.LINE_SEPARATOR.getMessage());
+    }
+
+    public static void printResult(int[] matchingNumber, int bonusNumber){
+        printResultGuide();
+        System.out.printf(LottoGuide.RESULT_FIFTH.getMessage(), matchingNumber[3]);
+        System.out.printf(LottoGuide.RESULT_FOURTH.getMessage(), matchingNumber[4]);
+        System.out.printf(LottoGuide.RESULT_THIRD.getMessage(), matchingNumber[5]-bonusNumber);
+        System.out.printf(LottoGuide.RESULT_SECOND.getMessage(), bonusNumber);
+        System.out.printf(LottoGuide.RESULT_FIRST.getMessage(), matchingNumber[6]);
     }
 
     public static void changeLine(){
