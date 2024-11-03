94300cb5587d0c9227cba74edf97f9ba6f9fa244
Goohong Chung <9rrong@uoslife.team>
2024-11-03 22:21:57 +0900

    feat(Lotto): 로또 애플리케이션의 MVP 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,62 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.model.Lotto;
+import lotto.model.LottoPrize;
+import lotto.model.LottoPrizes;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String lottoBudgetInput = Console.readLine();
+        int lottoBudget = Integer.parseInt(lottoBudgetInput);
+
+        int lottoCount = lottoBudget / 1000;
+        System.out.println(lottoCount + "개를 구매했습니다.");
+
+        List<Lotto> lottos = Stream.generate(() ->
+                        {
+                            List<Integer> randomNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+                            randomNumbers.sort(Comparator.naturalOrder());
+                            return new Lotto(randomNumbers);
+                        }
+                )
+                .limit(lottoCount)
+                .toList();
+
+        lottos.stream()
+                .map(Lotto::getNumbers)
+                .forEach(System.out::println);
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String WinningNumbersInput = Console.readLine();
+        String[] WinningNumberStrings = WinningNumbersInput.split(",");
+        List<Integer> winningNumbers = Arrays.stream(WinningNumberStrings)
+                .map(Integer::parseInt)
+                .toList();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String BonusNumberInput = Console.readLine();
+        int bonusNumber = Integer.parseInt(BonusNumberInput);
+
+        LottoPrizes lottoPrizes = new LottoPrizes(lottos.stream().map(lotto -> {
+            int matchCount = lotto.countMatchingNumbers(winningNumbers);
+            boolean containsBonusNumber = lotto.containsNumber(bonusNumber);
+            return LottoPrize.getLottoPrize(matchCount, containsBonusNumber);
+        }).toList());
+
+        String yield = lottoPrizes.calculateYield(lottoBudget);
+        System.out.println("당첨 통계" + System.lineSeparator() + "---");
+
+        List<String> matchStatistics = lottoPrizes.calculateMatchStatistics();
+        matchStatistics.forEach(System.out::println);
+
+        System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 }
