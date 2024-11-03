0388da44ead29618fa3f39ae4278d1bd590257cc
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-03 22:25:26 +0900

    feat(service): 로또 당첨 번호 생성 검증 구현
    - 당첨 번호가 비어있으면 안됨
    - 당첨 번호를 콤마로 분리해준다.
    - 당첨 번호의 구성요소는 숫자이어야 한다.

--- src/main/java/lotto/model/service/LottoService.java
+++ src/main/java/lotto/model/service/LottoService.java
@@ -1,6 +1,9 @@
 package lotto.model.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import lotto.model.domain.BonusNumber;
 import lotto.model.domain.Lotto;
 import lotto.model.domain.LottoMachine;
 
@@ -15,6 +18,20 @@ public class LottoService {
         LottoMachine lottoMachine = new LottoMachine(money);
         return lottoMachine.multipleLottoGenerator();
     }
+    public List<Integer> winningNumbersGenerator(String inputWinningNumbers) {
+        winningNumbersNotNullValidator(inputWinningNumbers);
+        return winningNumbersParser(inputWinningNumbers);
+    }
+
+
+
+    public void bonusNumberValidator(String bonusNumber) {
+
+    }
+
+    public void setWinningNumbers(List<Integer> winningNumbers, BonusNumber bonusNumber) {
+
+    }
 
     private static void moneyNotNullValidator(String inputMoney) {
         if (inputMoney.isBlank()) {
@@ -27,6 +44,26 @@ public class LottoService {
             return Integer.parseInt(inputMoney);
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 로또를 구매하려는 금액을 숫자로 입력해 주세요.");
+        }
+    }
+    private void winningNumbersNotNullValidator(String inputWinningNumbers) {
+        if (inputWinningNumbers.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 입력되지 않았습니다.");
+        }
+    }
+    private List<Integer> winningNumbersParser(String inputWinningNumbers) {
+        List<String> splitLottoMainNumbers = Arrays.stream(inputWinningNumbers.split(",",-1)).toList();
+        List<Integer> winningNumbers = new ArrayList<>();
+        for(String splitNumber : splitLottoMainNumbers) {
+            winningNumbers.add(winningNumbersNumericValidator(splitNumber));
+        }
+        return winningNumbers;
+    }
+    private int winningNumbersNumericValidator(String inputWinningNumbers) {
+        try{
+            return Integer.parseInt(inputWinningNumbers);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야합니다.");
         }
     }
 
