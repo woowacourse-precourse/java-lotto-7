827910b874b29130307e63e3dc30dba71788a5d4
Jin409 <sualng123@gmail.com>
2024-11-04 00:10:03 +0900

    refactor: 메서드를 분리해 각 메서드가 하나의 일을 하도록 해라

--- src/main/java/lotto/utils/RankDescriptionGenerator.java
+++ src/main/java/lotto/utils/RankDescriptionGenerator.java
@@ -6,16 +6,23 @@ public class RankDescriptionGenerator {
 
     public static String makeDescription(Rank rank) {
         StringBuilder description = new StringBuilder();
-        description.append(rank.getMatchingCount()).append("개 일치");
 
+        description.append(rank.getMatchingCount()).append("개 일치");
+        appendConditionAboutBonusNumber(rank, description);
+        appendInformationAboutPrizeMoney(rank, description);
+
+        return description.toString();
+    }
+
+    private static void appendConditionAboutBonusNumber(Rank rank, StringBuilder description) {
         if (rank.isRequiresBonusMatch()) {
             description.append(", 보너스 볼 일치");
         }
+    }
 
+    private static void appendInformationAboutPrizeMoney(Rank rank, StringBuilder description) {
         description.append(" (")
                 .append(String.format("%,d", rank.getPrizeMoney()))
                 .append("원)");
-
-        return description.toString();
     }
 }
