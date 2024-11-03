d47985a37a4f48e351933f8d158a85f1371cc805
TaegeunYou <dbxorms2310@naver.com>
2024-11-03 12:11:54 +0900

    feat(Amount): 입력된 금액이 올바른 범위내에 있는지 검증

--- src/main/java/lotto/view/domain/Amount.java
+++ src/main/java/lotto/view/domain/Amount.java
@@ -4,6 +4,8 @@ import lotto.view.global.exception.CustomException;
 import lotto.view.global.exception.ErrorMessage;
 
 public class Amount {
+    private static final int MINIMUM_AMOUNT = 1000;
+    private static final int MAXIMUM_AMOUNT = 100000000;
     private Integer value;
 
     public Amount(Integer amount) {
@@ -13,11 +15,18 @@ public class Amount {
 
     private void validateAmount(Integer amount) {
         validateDivisibleByThousand(amount);
+        validateAmountRange(amount);
     }
 
     private void validateDivisibleByThousand(Integer amount) {
         if (amount % 1000 != 0) {
             throw CustomException.of(ErrorMessage.INVALID_AMOUNT_DIVISIBILITY_ERROR);
+        }
+    }
+
+    private void validateAmountRange(Integer amount) {
+        if (amount < MINIMUM_AMOUNT || amount > MAXIMUM_AMOUNT) {
+            throw CustomException.of(ErrorMessage.INVALID_AMOUNT_TYPE_ERROR);
         }
     }
 }
--- src/main/java/lotto/view/global/exception/ErrorMessage.java
+++ src/main/java/lotto/view/global/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.view.global.exception;
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
     INVALID_AMOUNT_DIVISIBILITY_ERROR("입력된 금액은 1000으로 나누어떨어져야 합니다."),
-    INVALID_AMOUNT_TYPE_ERROR("입력된 금액은 양의 정수여야 합니다.");
+    INVALID_AMOUNT_TYPE_ERROR("입력된 금액은 양의 정수여야 합니다."),
+    INVALID_AMOUNT_RANGE_ERROR("입력된 금액은 올바른 범위 내에 있어야 합니다.");
 
     private final String message;
 
