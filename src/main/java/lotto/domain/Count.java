8505935aaf10deecb57e5bc57f1d13a295b3b86f
wogus <ind07152@naver.com>
2024-11-03 18:28:34 +0900

    refactor(InputView): 숫자 입력 검증 추가
    Count 클래스 내부에서 숫자 입력 검증을 InputView로 이동시킨다.

--- src/main/java/lotto/domain/Count.java
+++ src/main/java/lotto/domain/Count.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import static lotto.exception.ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT;
 import static lotto.exception.ErrorMessage.INVALID_PURCHASE_MIN;
-import static lotto.exception.ErrorMessage.INVALID_PURCHASE_TYPE;
 
 import lotto.exception.LottoException;
 
@@ -13,27 +12,19 @@ public class Count {
         this.count = count;
     }
 
-    public static Count from(String money) {
+    public static Count from(int money) {
         int count = calculateCount(money);
         return new Count(count);
     }
 
-    private static int calculateCount(String money) {
-        CountValidator.validateType(money);
-        int purchase = Integer.parseInt(money);
-        CountValidator.validateUnit(purchase);
-        int count = purchase / 1000;
+    private static int calculateCount(int money) {
+        CountValidator.validateUnit(money);
+        int count = money / 1000;
         CountValidator.validateCount(count);
         return count;
     }
 
     private static class CountValidator {
-        private static void validateType(String money) {
-            if (!money.matches("\\d+")) {
-                throw new LottoException(INVALID_PURCHASE_TYPE);
-            }
-        }
-
         private static void validateUnit(int money) {
             if (money % 1000 != 0) {
                 throw new LottoException(INVALID_PURCHASE_AMOUNT_UNIT);
