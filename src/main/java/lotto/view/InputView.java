1f24b797de913f2060e450f6a06a2da4d9153d8d
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-03 21:40:19 +0900

    feat : 구입 금액, 당첨 번호, 보너스 번호 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,23 @@
 package lotto.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView implements UserInput {
+    @Override
+    public String inputPurchaseMoney() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    @Override
+    public String inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    @Override
+    public String inputBonusBumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }
