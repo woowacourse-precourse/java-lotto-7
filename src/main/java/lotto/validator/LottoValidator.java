a363445f1722537bfcbb50f78737dad8da933753
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 04:39:40 +0900

    refactor(validator): LottoValidator 클래스 수정 -exception클래스를 선언하여 예외를 던지는 책임은 exception으로 분리하였습니다.

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,5 +1,7 @@
 package lotto.validator;
 
+import lotto.exception.LottoException;
+
 import java.util.List;
 
 public class LottoValidator {
@@ -8,19 +10,15 @@ public class LottoValidator {
   private static final int MIN_LOTTO_NUMBER = 1;
   private static final int MAX_LOTTO_NUMBER = 45;
 
-  private static final String ERROR_INVALID_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
-  private static final String ERROR_DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
-  private static final String ERROR_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이여야 합니다.";
-
   public static void validateLottoNumbers(List<Integer> numbers) {
     if (numbers.size() != LOTTO_NUMBER_COUNT) {
-      throw new IllegalArgumentException(ERROR_INVALID_SIZE);
+      LottoException.throwInvalidSizeException();
     }
     if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
-      throw new IllegalArgumentException(ERROR_DUPLICATE_NUMBER);
+      LottoException.throwDuplicateNumberException();
     }
     if (numbers.stream().anyMatch(num -> num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER)) {
-      throw new IllegalArgumentException(ERROR_OUT_OF_RANGE);
+      LottoException.throwOutOfRangeException();
     }
   }
 }
