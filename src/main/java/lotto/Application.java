2de1267c8f3a008bef341a67f96ff6c235eb1064
Yeongjae Kim <sees77770@gmail.com>
2024-11-03 18:57:19 +0900

    feat: 로또 생성
    로또의 랜덤 번호를 생성한다.
    - Randoms API의 pickUniqueNumbersInRange를 이용한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
         
     }
 }
