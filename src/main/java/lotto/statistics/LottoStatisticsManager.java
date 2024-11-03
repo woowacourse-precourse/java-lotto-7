80a04f3b577368def3725936a64d121b1e070c30
Park, Sojeong <sojeong2101@gmail.com>
2024-11-04 01:14:42 +0900

    feat($LottoStatisticsManager): 당첨 통계 서브 시스템을 제어하는 LottoStatisticsManager 객체 구현
    1. 당첨 번호와 보너스 번호 입력 메서드 구현
    2. 입력 부분에서 에러 발생 시 에러 메시지 출력 후 재입력 구현
    3. 받은 로또 번호들로 일치 개수 카운터를 작동하고 이를 당첨결과지로 반환

---
+++ src/main/java/lotto/statistics/LottoStatisticsManager.java
@@ -0,0 +1,57 @@
+package lotto.statistics;
+
+import java.util.List;
+import lotto.purchase.MyLotto;
+
+public class LottoStatisticsManager {
+
+    private final List<MyLotto> myLotteries;
+    private final DrawInput drawInput = new DrawInput();
+    private final WinningNumbersExtractor winningNumbersExtractor = new WinningNumbersExtractor();
+    private final MatchCounter matchCounter = new MatchCounter();
+
+    public LottoStatisticsManager(List<MyLotto> myLotteries) {
+        this.myLotteries = myLotteries;
+    }
+
+    public DrawResultSheet process() {
+        Lotto lotto = receiveWinningLotto();
+        BonusBall bonusBall = receiveBonusBall(lotto.getNumbers());
+
+        return new DrawResultSheet(totalMatchCount(lotto, bonusBall, myLotteries));
+    }
+
+    private Lotto receiveWinningLotto() {
+        try {
+            String rawWinningNumbers = drawInput.getWinningNumbers();
+
+            return new Lotto(winningNumbersExtractor.extract(rawWinningNumbers));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+            return receiveWinningLotto();
+        }
+    }
+
+    private BonusBall receiveBonusBall(List<Integer> winningNumbers) {
+        try {
+            String rawBonusNumber = drawInput.getBonusNumber();
+
+            return new BonusBall(winningNumbersExtractor.extract(rawBonusNumber).get(0), winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+            return receiveBonusBall(winningNumbers);
+        }
+    }
+
+    private List<Integer> totalMatchCount(Lotto winningLotto, BonusBall bonusBall, List<MyLotto> myLotteries) {
+        for (MyLotto myLotto : myLotteries) {
+            int matchedNumbers = winningLotto.countMatches(myLotto.numbers());
+            boolean matchedBonusResult = bonusBall.matchBonus(myLotto.numbers());
+            matchCounter.updateMatchResult(matchedNumbers, matchedBonusResult);
+        }
+
+        return matchCounter.getMatchedNumbers();
+    }
+}
