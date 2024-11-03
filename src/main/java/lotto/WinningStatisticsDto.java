4135b0e0ce743b952631a0d7f165aec68bb65f35
chyo1 <hyowon3856@gmail.com>
2024-11-04 02:47:09 +0900

    feat : 당첨 정보를 가진 WinningStatisticsDto 생성

---
+++ src/main/java/lotto/WinningStatisticsDto.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningStatisticsDto {
+    private Map<Rank, Integer> winningStatistics;
+
+    WinningStatisticsDto(Map<Rank, Integer> winningStatistics) {
+        this.winningStatistics = initializeMap();
+        this.winningStatistics.putAll(winningStatistics);
+    }
+
+    private Map<Rank, Integer> initializeMap() {
+        Map<Rank, Integer> map = new HashMap<>();
+        for (Rank a : Rank.values()) {
+            map.put(a, 0); // 모든 Enum 값에 대해 0으로 초기화
+        }
+        return map;
+    }
+}
