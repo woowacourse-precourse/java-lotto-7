098a7d530978d6fb88dde1f10ca2bc973055e08f
haebyun <bhk4392@gmail.com>
2024-11-03 22:05:48 +0900

    feat(Lotto): Lotto 클래스에 번호 리스트 반환 메서드 추가
    - Lotto 클래스에 getNumbersAsUnmodifiableList 메서드 추가
    - LottoNumber 리스트를 정수 리스트로 변환 후, 외부에서 수정 불가하도록 unmodifiableList로 반환

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -48,4 +48,10 @@ public class Lotto {
         return numbers.stream()
                 .sorted().toList();
     }
+
+    public List<Integer> getNumbersAsUnmodifiableList() {
+        return numbers.stream()
+                .map(LottoNumber::getValue)
+                .toList(); // unmodifiableList 반환
+    }
 }
