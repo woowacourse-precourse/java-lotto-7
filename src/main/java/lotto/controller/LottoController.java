0e80d7027111dbc6e175c6578501f1788fafdf21
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-03 17:12:00 +0900

    feat: 전체 프로그램 구동

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,68 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoResult;
+import lotto.model.PurchaseAmount;
+import lotto.service.BonusNumber;
+import lotto.service.LottoMachine;
+import lotto.service.LottoResultCalculator;
+import lotto.service.WinningNumber;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoController {
+    private final List<Lotto> lottos; // 로또 번호 리스트
+    private LottoMachine lottoMachine; // 로또 머신
+    private LottoResult lottoResult;
+
+    public LottoController() {
+        this.lottos = new ArrayList<>();
+    }
+
+    public void run() {
+        PurchaseAmount purchaseAmount = null;
+        while (purchaseAmount == null) {
+            try {
+                int purchaseAmountValue = InputView.readPurchaseAmount();
+                purchaseAmount = new PurchaseAmount(purchaseAmountValue);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        lottoMachine = new LottoMachine(purchaseAmount, lottos);
+        OutputView.printPurchaseAmount(purchaseAmount);
+        OutputView.printLottos(lottoMachine.getLottos());
+
+        List<Integer> winningNumbers = null;
+        while (winningNumbers == null) {
+            try {
+                winningNumbers = InputView.readWinningNumbers();
+                new WinningNumber(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                winningNumbers = null;
+            }
+        }
+
+        int bonusNumberValue = 0;
+        boolean validBonusNumber = false;
+        while (!validBonusNumber) {
+            try {
+                bonusNumberValue = InputView.readBonusNumber();
+                BonusNumber bonusNumber = new BonusNumber(bonusNumberValue, new WinningNumber(winningNumbers));
+                validBonusNumber = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        LottoResultCalculator calculator = new LottoResultCalculator();
+        lottoResult = calculator.calculateResults(lottoMachine.getLottos(), new WinningNumber(winningNumbers), new BonusNumber(bonusNumberValue, new WinningNumber(winningNumbers)));
+
+        OutputView.printResult(lottoResult);
+    }
+}
