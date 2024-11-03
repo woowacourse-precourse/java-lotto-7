8633a66dd2c4a3288075923ec02f251e207e9d02
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 23:47:19 +0900

    test(output view): 출력뷰 테스트 추가

--- src/test/java/lotto/OutputViewTest.java
+++ src/test/java/lotto/OutputViewTest.java
@@ -18,6 +18,25 @@ import java.util.stream.Collectors;
 
 public class OutputViewTest {
 
+    private static final int LOTTO_COUNT = 5;
+    private static final DrawNumberHacker HACKED_PLACE = DrawNumberHacker.HACK_THIRD_PLACE;
+
+    private NumberGenerator testNumberGenerator;
+
+    @BeforeEach
+    void initTestNumberGenerator() {
+        testNumberGenerator = new TestNumberGenerator(
+                DrawNumberHacker.MIN_WINNING_NUMBER,
+                DrawNumberHacker.MAX_WINNING_NUMBER
+        );
+    }
+
+    @AfterEach
+    void winningCountClear() {
+        Arrays.stream(Winning.values())
+                .forEach(Winning::clear);
+    }
+
     @Test
     void printLottoNumbersTest() {
         LottoPublisher lottoPublisher = makeLottoPublisher();
@@ -28,6 +47,44 @@ public class OutputViewTest {
         Lotto lotto = lottos.getFirst();
 
         Assertions.assertEquals(lotto.numbersToString(), expect);
+    }
+
+    @Test
+    void winningCountTest() {
+        LottoPublisher lottoPublisher = makeLottoPublisher();
+        LottoMachine lottoMachine = new LottoMachine(HACKED_PLACE.getDrawNumbers());
+
+        List<Lotto> lottos = lottoPublisher.publishLotto(LOTTO_COUNT);
+        lottoMachine.examineLotto(lottos, LOTTO_COUNT);
+        Assertions.assertEquals(LOTTO_COUNT, Winning.THIRD_PLACE.getCount());
+    }
+
+    @Test
+    void resultToStringTest() {
+        OutputView outputView = new OutputView();
+
+        LottoPublisher lottoPublisher = makeLottoPublisher();
+        LottoMachine lottoMachine = new LottoMachine(HACKED_PLACE.getDrawNumbers());
+
+        List<Lotto> lottos = lottoPublisher.publishLotto(LOTTO_COUNT);
+        double revenueRate = lottoMachine.examineLotto(lottos, LOTTO_COUNT);
+        double roundRevenueRate = Math.round(revenueRate * 100) / 100.0;
+
+        StringBuilder sb = new StringBuilder();
+        makeExpectResult(sb, roundRevenueRate);
+
+        Assertions.assertEquals(outputView.resultToString(revenueRate), sb.toString());
+    }
+
+    private LottoPublisher makeLottoPublisher() {
+        return new LottoPublisher(testNumberGenerator);
+    }
+
+    private static void makeExpectResult(StringBuilder sb, double roundRevenueRate) {
+        sb.append("당첨 통계\n")
+                .append("---\n");
+        Arrays.stream(Winning.values()).forEach(winning -> sb.append(winning.toString()));
+        sb.append("총 수익률은 ").append(roundRevenueRate).append("%입니다.");
     }
 
     private static String createExpect(List<Integer> numbers) {
