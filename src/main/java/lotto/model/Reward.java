ef59b47b9cd324b9c53f1ded8888e9ee96f5d268
ufo9430 <ufo9430@naver.com>
2024-11-03 15:12:05 -0500

    feat: 당첨 보상 데이터에 등수 요소 추가

--- src/main/java/lotto/model/Reward.java
+++ src/main/java/lotto/model/Reward.java
@@ -1,21 +1,24 @@
 package lotto.model;
 
 public enum Reward {
-    FIRST(6,false,2_000_000_000),
-    SECOND(5,true,30_000_000),
-    THIRD(5,false,1_500_000),
-    FOURTH(4,false,50_000),
-    FIFTH(3,false,5_000),
-    NONE(0,false,0);
+    FIRST(6,false,2_000_000_000,1),
+    SECOND(5,true,30_000_000,2),
+    THIRD(5,false,1_500_000,3),
+    FOURTH(4,false,50_000,4),
+    FIFTH(3,false,5_000,5),
+    NONE(0,false,0,6);
 
     private final int count;
     private final boolean bonus;
     private final int prize;
 
-    Reward(int count, boolean bonus, int prize) {
+    private final int rank;
+
+    Reward(int count, boolean bonus, int prize, int rank) {
         this.count = count;
         this.bonus = bonus;
         this.prize = prize;
+        this.rank = rank;
     }
 
     public int getPrize() {
