dd561b39887fed0f09ea1fea6f7aa6ef6ec73542
윤혜진 <lold2424@gmail.com>
2024-11-03 18:49:37 +0900

    fix: 문제에서 요구하는 형식대로 출력하도록 메시지 수정

--- src/main/java/lotto/message/WinningNumMessage.java
+++ src/main/java/lotto/message/WinningNumMessage.java
@@ -4,7 +4,7 @@ public enum WinningNumMessage {
     MATCHES_3_WINNING("3개 일치 (5,000원) - %d개\n"),
     MATCHES_4_WINNING("4개 일치 (50,000원) - %d개\n"),
     MATCHES_5_WINNING("5개 일치 (1,500,000원) - %d개\n"),
-    MATCHES_BONUS_WINNING("5개 일치,보너스 볼 일치 (30,000,000원) - %d개\n"),
+    MATCHES_BONUS_WINNING("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
     MATCHES_6_WINNING("6개 일치 (2,000,000,000원) - %d개\n");
 
     private String message;
