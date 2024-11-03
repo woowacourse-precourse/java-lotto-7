9260f182a4879a80c787d77aef8a935943a5cb74
KoYunHyuk <aes2036@daum.net>
2024-11-03 23:02:14 +0900

    feat: Add Test of '예외_테스트_1000원단위출력'

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,14 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 예외_테스트_1000원단위출력() {
+        assertSimpleTest(() -> {
+            runException("1001");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
