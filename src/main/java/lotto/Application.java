ec97ebf8e6bf1bef474b00bcce9516e78adad7a9
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-11-04 05:41:09 +0900

    feat: 어플리케이션 구성 및 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.domain.PrizeCalculator;
+import lotto.io.input.InputHandler;
+import lotto.io.input.validation.UserInputValidation;
+import lotto.io.output.OutputHandler;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		LottoMachine lottoMachine = new LottoMachine(
+			new OutputHandler(),
+			new InputHandler(new UserInputValidation()),
+			new PrizeCalculator()
+		);
+
+		lottoMachine.run();
+	}
+
 }
