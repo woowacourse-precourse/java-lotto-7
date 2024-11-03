0e9c74b03364548773b8f1583f2e7aa5e0323377
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-03 22:09:54 +0900

    refactor(Application, InputView): 입력 로직을 InputView 클래스로 분리하여 코드 개선
    Application 클래스에서 사용자 입력 처리 로직을 InputView 클래스로 분리하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,9 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         // 구매 금액 입력
         System.out.println("구입금액을 입력해 주세요.");
-        int purchaseAmount = inputPurchaseAmount();
-
-
-    }
-
-    private static int inputPurchaseAmount() {
-        return Integer.parseInt(Console.readLine());
+        int purchaseAmount = InputView.inputPurchaseAmount();
     }
 }
---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int inputPurchaseAmount() {
+        return Integer.parseInt(Console.readLine());
+    }
+}
