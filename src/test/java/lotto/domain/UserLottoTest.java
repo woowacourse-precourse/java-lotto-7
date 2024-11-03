4c41a06b8854f8dee351c98efc10532864a5e8e0
yush <yush2202@gmail.com>
2024-11-03 19:38:15 +0900

    test($ UserLottoTest): 보너스 숫자 검증 테스트
    보너스 숫자가 범위 바깥이면 오류가 발생하는 기능 테스트

--- src/test/java/lotto/domain/UserLottoTest.java
+++ src/test/java/lotto/domain/UserLottoTest.java
@@ -4,7 +4,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.constants.ErrorMessages;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserLottoTest {
 
@@ -12,9 +15,19 @@ public class UserLottoTest {
     int testBonus = 6;
 
     @Test
+    @DisplayName("보너스 번호와, 로또 번호 6개가 중복되면 오류가 발생합니다.")
     void 보너스번호와_로또번호가_중복이면_오류() {
         assertThatThrownBy(() -> new UserLotto(testLotto, testBonus))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.INVALID_LOTTO_DUPLICATED.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호가 범위 바깥에 있으면 오류가 발생합니다.")
+    @ValueSource(ints = {0, 46, -100, 100, -1})
+    void 보너스번호가_범위_바깥이면_오류(int target) {
+        assertThatThrownBy(() -> new UserLotto(testLotto, target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE.getMessage());
     }
 }
