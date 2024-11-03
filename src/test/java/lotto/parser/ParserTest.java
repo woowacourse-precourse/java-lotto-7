ba01bb2a905f0b636e3b2d78d483effb706ae1cf
na0th <105775683+na0th@users.noreply.github.com>
2024-11-03 13:46:46 +0900

    test : Parser 클래스 단위 테스트 추가
    추가한 내용 :
    1.Parser 클래스 단위 테스트를 추가

---
+++ src/test/java/lotto/parser/ParserTest.java
@@ -0,0 +1,33 @@
+package lotto.parser;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    private Parser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new Parser();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "19999", "20000"})
+    @DisplayName("ParseStringToInt 테스트")
+    void String이_int로_변환되면_성공한다(String input) {
+        //given
+        int expected = Integer.parseInt(input);
+        //when
+        int parsedInput = parser.parseStringToInt(input);
+        //then
+        Assertions.assertThat(parsedInput).isEqualTo(expected);
+    }
+
+}
