8857ccf45216bf2fd14050093334a052abb1e416
inha0319@naver.com <inha031913@gmail.com>
2024-11-03 18:19:11 +0900

    [test] 수정된 코드에 맞게 입력 예외처리와 관련된 테스트코드 수정

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -2,22 +2,49 @@ package lotto.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class InputViewTest {
-    @DisplayName("구입 금액이 1,000원 단위가 아니면 예외가 발생한다.")
+
     @Test
+    @DisplayName("구입 금액이 1,000원 단위가 아니면 예외가 발생하고 재입력이 가능하다.")
     void 구입_금액_예외_처리() {
-        int invalidAmount = 1500;
-        assertThatThrownBy(() -> InputView.validatePurchaseAmount(invalidAmount))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        String input = "1500\n1000\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        int result = InputView.purchase();
+
+        assertEquals(1000, result);
     }
 
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복될 경우 예외가 발생하고 재입력이 가능하다.")
+    void 보너스_번호_중복_예외() {
+        String input = "3\n7\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        int result = InputView.bonusNumber(winningNumbers);
+
+        assertEquals(7, result);
+    }
+
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되지 않을 경우 정상 작동한다.")
+    void 보너스_번호_중복_아닐_경우_정상_작동() {
+        String input = "7\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        int result = InputView.bonusNumber(winningNumbers);
+        assertEquals(7, result);
+    }
 }
