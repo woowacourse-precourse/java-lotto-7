6b23dd66a48aaf1dc4718b0b6408f33575fa95c8
youngseo3 <wys4475@gmail.com>
2024-11-03 18:25:53 +0900

    test(LottoTest): 로또 번호가 6개가 넘지 않는 경우 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -14,6 +14,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 로또_번호의_개수가_6개가_넘지_않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
