fde775f518b7c86925987e707c1b3ca140cb6808
hyeonjeong9911 <knk990101@naver.com>
2024-11-03 21:12:58 +0900

    feat: 당첨 번호 입력 안내 문자열 출력

--- src/main/java/lotto/view/OutputController.java
+++ src/main/java/lotto/view/OutputController.java
@@ -11,4 +11,7 @@ public class OutputController {
         System.out.println(InputService.lottoAmount + "개를 구매했습니다.");
     }
 
+    public static void winningNumPrint() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }
