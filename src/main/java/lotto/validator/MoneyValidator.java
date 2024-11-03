dc930c911827b79cfaf6e23a7f17372f43c829d3
gikhoon <ghikhoon@naver.com>
2024-11-04 00:06:37 +0900

    feat: 돈의 범위를 검사하는 로직 구현

--- src/main/java/lotto/validator/MoneyValidator.java
+++ src/main/java/lotto/validator/MoneyValidator.java
@@ -1,11 +1,31 @@
 package lotto.validator;
 
+import java.math.BigInteger;
+import lotto.exception.MoneyErrorCode;
+
 public class MoneyValidator {
+    private static final int MIN_MONEY = 0;
+    private static final int MAX_MONEY = Integer.MAX_VALUE;
+
     public void validateNumeric(String money) {
-        try {
-            Integer.parseInt(money);
+        try{
+            BigInteger moneyAmount = new BigInteger(money);
+            validateRangeMax(moneyAmount);
+            validateRangeMin(moneyAmount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자여야 합니다");
+            throw new IllegalArgumentException(MoneyErrorCode.MONEY_PARSE_ERROR.getMessage());
+        }
+    }
+
+    private void validateRangeMax(BigInteger money) {
+        if (money.compareTo(BigInteger.valueOf(MAX_MONEY)) > 0) {
+            throw new IllegalArgumentException(MoneyErrorCode.MONEY_MAX_ERROR.getMessage());
+        }
+    }
+
+    private void validateRangeMin(BigInteger money) {
+        if (money.compareTo(BigInteger.valueOf(MIN_MONEY)) <= 0) {
+            throw new IllegalArgumentException(MoneyErrorCode.MONEY_MIN_ERROR.getMessage());
         }
     }
 }
