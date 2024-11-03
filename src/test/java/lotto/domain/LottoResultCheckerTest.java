d29fcefbac5686590b38abb512933fa41e1871d2
himinqq <alsrud0966@naver.com>
2024-11-03 20:26:54 +0900

    test(LottoResultCheckerTest): 수익률 계산 기능 테스트

--- src/test/java/lotto/domain/LottoResultCheckerTest.java
+++ src/test/java/lotto/domain/LottoResultCheckerTest.java
@@ -28,12 +28,23 @@ class LottoResultCheckerTest {
 
     @BeforeEach
     void createLotto() {
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
-        lottos.add(new Lotto(List.of(10, 11, 12, 13, 14, 15)));
+//        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+//        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+//        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+//        lottos.add(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
+//        lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+//        lottos.add(new Lotto(List.of(10, 11, 12, 13, 14, 15)));
+
+        lottos.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+        lottos.add(new Lotto(List.of(3, 5, 11, 16, 32, 38)));
+        lottos.add(new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+        lottos.add(new Lotto(List.of(1, 8, 11, 31, 41, 42)));
+        lottos.add(new Lotto(List.of(13, 14, 16, 38, 42, 45)));
+        lottos.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+        lottos.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+        lottos.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+
+
     }
 
     @Test
@@ -49,12 +60,17 @@ class LottoResultCheckerTest {
 
         Map<Rank, Integer> rank = lottoResultChecker.findRank();
 
-        assertThat(rank.get(Rank.FIRST)).isEqualTo(1);
-        assertThat(rank.get(Rank.SECOND)).isEqualTo(1);
-        assertThat(rank.get(Rank.THIRD)).isEqualTo(1);
-        assertThat(rank.get(Rank.FOURTH)).isEqualTo(1);
         assertThat(rank.get(Rank.FIFTH)).isEqualTo(1);
-        assertThat(rank.get(Rank.NONE)).isEqualTo(1);
+
+    }
+
+    @Test
+    void 수익률을_계산한다(){
+        lottoResultChecker = new LottoResultChecker(lottoRaffle, lottos);
+        Calculator calculator = new Calculator(lottoResultChecker, new Money(8000));
+        double rateOfReturn = calculator.calculateRateOfReturn();
+
+        assertThat(rateOfReturn).isEqualTo(62.5);
 
     }
 }
