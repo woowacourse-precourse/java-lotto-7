4a912cb6f9e8154a68b9869197d49f06b2c9d9c5
likerhythm <wjdalsdl0412@gmail.com>
2024-11-03 15:57:00 +0900

    feat(Winning): rank 정보 추가

--- src/main/java/lotto/model/Winning.java
+++ src/main/java/lotto/model/Winning.java
@@ -4,28 +4,41 @@ import java.util.Arrays;
 
 public enum Winning {
 
-    NO_WIN      ("0", "0", 0),
-    FIFTH_PLACE ("3", "0", 5000),
-    FOURTH_PLACE("4", "0", 50000),
-    THIRD_PLACE ("5", "0", 1500000),
-    SECOND_PLACE("5", "1", 30000000),
-    FIRST_PLACE ("6", "0", 2000000000);
+    NO_WIN      (0, "0", "0", 0),
+    FIFTH_PLACE (5, "3", "0", 5000),
+    FOURTH_PLACE(4, "4", "0", 50000),
+    THIRD_PLACE (3, "5", "0", 1500000),
+    SECOND_PLACE(2, "5", "1", 30000000),
+    FIRST_PLACE (1, "6", "0", 2000000000);
 
+    private final int rank;
     private final String winningNumberMatch;
     private final String bonusNumberMatch;
     private final long prizeMoney;
 
-    Winning(String winningNumberMatch, String bonusNumberMatch, long prizeMoney) {
+    Winning(int rank, String winningNumberMatch, String bonusNumberMatch, long prizeMoney) {
+        this.rank = rank;
         this.winningNumberMatch = winningNumberMatch;
         this.bonusNumberMatch = bonusNumberMatch;
         this.prizeMoney = prizeMoney;
     }
 
-    public static Winning getPlace(int winningNumberMatch, int bonusNumberMatch) {
+    public static Winning getPlaceByMatch(int winningNumberMatch, int bonusNumberMatch) {
         return Arrays.stream(Winning.values())
                 .filter(winning -> winning.isMatch(String.valueOf(winningNumberMatch), String.valueOf(bonusNumberMatch)))
                 .findFirst()
                 .orElse(Winning.NO_WIN);
+    }
+
+    public static Winning getPlaceByRank(int rank) {
+        return Arrays.stream(Winning.values())
+                .filter(winning -> winning.rank == rank)
+                .findFirst()
+                .orElse(Winning.NO_WIN);
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
     }
 
     private boolean isMatch(String winningNumberMatch, String bonusNumberMatch) {
