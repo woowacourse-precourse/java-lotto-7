99d7f1cf24e6f0d898af6c258f2992811fd07a04
jinno Yun <jinno321@naver.com>
2024-11-04 01:33:11 +0900

    로또 관리 시스템에서 등수 결정 메서드 생성

--- src/main/java/lotto/model/LottoManagementSystem.java
+++ src/main/java/lotto/model/LottoManagementSystem.java
@@ -7,7 +7,7 @@ public class LottoManagementSystem {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
-    public LottoManagementSystem(List<Integer> winningNumbers,int bonusNumber) {
+    public LottoManagementSystem(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -20,15 +20,34 @@ public class LottoManagementSystem {
         return bonusNumber;
     }
 
-    public int compare(List<Integer> numbers){
+    public int returnRank(List<Integer> numbers) {
         int matchCount = 0;
+
         for (Integer number : numbers) {
             if (winningNumbers.contains(number)) {
                 matchCount++;
             }
         }
-        return matchCount;
+
+        return checkRank(numbers, matchCount);
     }
 
+    private int checkRank(List<Integer> numbers, int matchCount) {
+        switch (matchCount) {
+            case 6:
+                return 1;
+            case 5:
+                if (numbers.contains(bonusNumber)) {
+                    return 2;
+                }
+                return 3;
+            case 4:
+                return 4;
+            case 3:
+                return 5;
+            default:
+                return 6;
+        }
+    }
 
 }
