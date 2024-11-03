d7e785a3af3471d0e6c52c00cd9297d9e4f07b77
Yehyeok Bang <qkddpgur318@gmail.com>
2024-11-04 03:44:29 +0900

    refactor: 메서드 분리
    하나의 일만 수행하도록 메서드를 분리합니다.

--- src/main/java/lotto/model/shop/LottoMachine.java
+++ src/main/java/lotto/model/shop/LottoMachine.java
@@ -1,8 +1,8 @@
 package lotto.model.shop;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.util.Validator;
 import lotto.model.ticket.Lotto;
 import lotto.model.ticket.LottoTickets;
@@ -12,26 +12,38 @@ public class LottoMachine {
 
     public LottoTickets issueTicket(int quantity) {
         validateQuantity(quantity);
-        return generateLottoTickets(quantity);
+        return createLottoTickets(quantity);
     }
 
     private void validateQuantity(int quantity) {
         Validator.checkPurchaseQuantity(quantity);
     }
 
-    private LottoTickets generateLottoTickets(int quantity) {
-        List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < quantity; i++) {
-            lottoTickets.add(new Lotto(generateLottoNumbers()));
-        }
+    private LottoTickets createLottoTickets(int quantity) {
+        List<Lotto> lottoTickets = generateLottoByQuantity(quantity);
         return new LottoTickets(lottoTickets);
     }
 
+    private List<Lotto> generateLottoByQuantity(int quantity) {
+        return IntStream.range(0, quantity)
+                .mapToObj(i -> createLotto())
+                .toList();
+    }
+
+    private Lotto createLotto() {
+        return new Lotto(generateLottoNumbers());
+    }
+
     private List<Integer> generateLottoNumbers() {
-        List<Integer> uniqueNumbers = Randoms.pickUniqueNumbersInRange(
-                LottoRule.MIN_LOTTO_NUMBER, LottoRule.MAX_LOTTO_NUMBER, LottoRule.LOTTO_NUMBERS_COUNT
-        );
+        List<Integer> uniqueNumbers = getUniqueNumbers();
         return sortByAsc(uniqueNumbers);
+    }
+
+    private List<Integer> getUniqueNumbers() {
+        int startInclusive = LottoRule.MIN_LOTTO_NUMBER.get();
+        int endInclusive = LottoRule.MAX_LOTTO_NUMBER.get();
+        int count = LottoRule.LOTTO_NUMBERS_COUNT.get();
+        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, count);
     }
 
     private List<Integer> sortByAsc(List<Integer> numbers) {
