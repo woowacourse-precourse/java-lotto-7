e7e990b49404e6ce44384c357877c395ac2fae13
btongtong <leeym2615@naver.com>
2024-11-04 01:59:26 +0900

    refactor(CommonValidation): Simplify convertStringToInt() return

--- src/main/java/lotto/CommonValidation.java
+++ src/main/java/lotto/CommonValidation.java
@@ -2,12 +2,10 @@ package lotto;
 
 public class CommonValidation {
     public static int convertStringToInt(String input) {
-        int numericValue;
         try {
-            numericValue = Integer.parseInt(input);
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.NUMERIC_VALUE_ERROR.getMessage());
         }
-        return numericValue;
     }
 }
