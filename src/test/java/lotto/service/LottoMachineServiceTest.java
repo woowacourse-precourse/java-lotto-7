b8ba96c42c854930be37205b96a1f18939cd6792
jijongkwon <help3451@naver.com>
2024-11-03 19:05:53 +0900

    test(service): 로또 당첨번호 입력 테스트코드 구현
    - 로또 당첨번호를 입력받는다
    - 로또번호 크기가 6이 아닌 경우 예외를 던진다

--- src/test/java/lotto/service/LottoMachineServiceTest.java
+++ src/test/java/lotto/service/LottoMachineServiceTest.java
@@ -1,10 +1,14 @@
 package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import lotto.domain.LottoMachine;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoMachineServiceTest {
 
@@ -17,6 +21,7 @@ class LottoMachineServiceTest {
         lottoMachineService = new LottoMachineService();
     }
 
+    // SUCCESS
     @Test
     void 로또_구입금액_입력받는다() {
         // given
@@ -39,5 +44,25 @@ class LottoMachineServiceTest {
 
         // then
         assertEquals(12, lottoMachine.getBonusNumber());
+    }
+
+    @Test
+    void 로또_당첨번호를_입력받는다() {
+        // given
+        String numbers = "1,2,3,4,5,6";
+
+        // when
+        lottoMachineService.inputWinningNumbers(numbers);
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), lottoMachine.getWinningLotto().getNumbers());
+    }
+
+    // EXCEPTION
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3", "1,2,3,4"})
+    void 로또번호_크기가_6이_아닌경우_예외를_던진다(String numbers) {
+        assertThrows(IllegalArgumentException.class,
+                () -> lottoMachineService.inputWinningNumbers(numbers));
     }
 }
