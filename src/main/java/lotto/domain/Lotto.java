5737cf07e52985a12b4ff957066270ed200042bb
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 03:10:00 +0900

    feat: 로또 클래스 메서드 추가 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Lotto {
 
     private static final int LOTTO_COUNT = 6;
-    private static final int LOTTO_START_NUMBER = 1;
-    private static final int LOTTO_END_NUMBER = 45;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
 
     private final List<Integer> numbers;
 
@@ -16,13 +16,18 @@ public class Lotto {
         Collections.sort(numbers);
     }
 
+    public int countSameNumbers(Lotto lotto) {
+        return (int) java.util.stream.IntStream.rangeClosed(START_NUMBER, END_NUMBER)
+                .filter(i -> this.contains(i) && lotto.contains(i))
+                .count();
+    }
+
     public boolean contains(int otherNumber) {
-        for (Integer number : numbers) {
-            if (number == otherNumber) {
-                return true;
-            }
-        }
-        return false;
+        return numbers.stream().anyMatch(number -> number == otherNumber);
+    }
+
+    public boolean hasBonus(Bonus bonus) {
+        return numbers.stream().anyMatch(bonus::isSame);
     }
 
     private void validate(List<Integer> numbers) {
@@ -39,7 +44,7 @@ public class Lotto {
 
     private void checkIfNumberRangeIsValid(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER) {
+            if (number < START_NUMBER || number > END_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
@@ -50,10 +55,6 @@ public class Lotto {
         if (numbers.size() != set.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호 내에 중복된 숫자가 존재합니다.");
         }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 
     @Override
