c2129252773221d1f13633a65c41f9c7f5e2bdf4
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 05:31:45 +0900

    refactor(inputparser): 당첨 번호 저장 메서드 삭제
    - InputParser에서 당첨 번호를 저장하는 메서드를 삭제하고, 파싱 기능만 담당하도록 구조를 단순화했습니다.
    - 당첨 번호를 저장하는 책임은 게임 초기 설정 클래스에서 관리하도록 변경했습니다.

--- src/main/java/lotto/input/InputParser.java
+++ src/main/java/lotto/input/InputParser.java
@@ -5,11 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputParser {
-    private final List<Integer> WINNING_LOTTO;
-
-    public InputParser(String winningNumbers) {
-        this.WINNING_LOTTO = splitWinningNumbers(winningNumbers);
-    }
     public List<Integer> splitWinningNumbers(String winningNumbers) {
         return Arrays.stream(winningNumbers.split(","))
                 .map(Integer::parseInt)
