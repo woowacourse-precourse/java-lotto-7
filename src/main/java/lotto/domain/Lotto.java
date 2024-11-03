cb710af3f7250921b99824e0922d0c722f4a79a8
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 06:52:07 +0900

    refactor(lotto): 로또 번호 리스트 반환 메서드 추가
    - Lotto 클래스에 로또 번호 리스트를 반환하는 메서드를 추가했습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,6 +15,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
+    public List<Integer> lottoNumbers() {
+        return numbers;
+    }
 }
