60222b3b43fb0a346e45e4ea4d8b6ec2d063bcd1
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-03 15:08:06 +0900

    chore: 사용하지 않은 라이브러리 삭제

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,7 +3,6 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
