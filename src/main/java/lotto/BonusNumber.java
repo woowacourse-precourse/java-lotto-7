d64690fb8d149f9bca47f34efeab2ec793c9e46d
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 01:03:58 +0900

    feat(BonusNumber): 보너스 번호 범위 기능 검증 구현

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -2,9 +2,16 @@ package lotto;
 
 public class BonusNumber {
 
-    int value;
+    private final int number;
 
-    public BonusNumber(int value) {
-        this.value = value;
+    public BonusNumber(int number) {
+        validateNumberRange(number);
+        this.number = number;
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < 0 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
