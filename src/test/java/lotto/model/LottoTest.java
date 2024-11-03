24fb30fa42ac9c347f4516be1e0044907c8ee93b
‘sangyong-99’ <tkddyd1126@gmail.com>
2024-11-04 00:06:18 +0900

    test: Lotto클래스 로또 숫자, 보너스 숫자 매치 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -20,5 +21,20 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("일치하는 로또 숫자 개수 확인하기")
+    public void 로또_당첨_숫자_개수_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto purchaseLotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        assertThat(winningLotto.matchLottoCount(purchaseLotto)).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("보너스 숫자, 당첨 로또 숫자와 일치 확인하기")
+    public void 보너스_숫자_일치_테스트() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(winningLotto.matchBonus(6)).isEqualTo(true);
     }
 }
