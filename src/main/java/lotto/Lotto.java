d282b35ea6637a2780b00e25e177a9ff89fb96be
hojooo <ghwn5833@gmail.com>
2024-11-04 04:02:36 +0900

    feat: Add verification logic for lotto numbers to lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,6 +19,12 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
