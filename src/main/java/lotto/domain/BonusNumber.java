e90710bba1aa70d8c6c8e142543f90fced0c7117
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-03 22:18:35 +0900

    feat: 보너스 번호가 로또 번호와 겹치는 경우 예외를 발생시킨다.

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,16 +1,19 @@
-package lotto;
+package lotto.domain;
 
 public class BonusNumber {
     private static int BONUS_NUMBER;
 
-    public BonusNumber(int bonusNumber) {
-        validate(bonusNumber);
+    public BonusNumber(int bonusNumber, Lotto winningLotto) {
+        validate(bonusNumber, winningLotto);
         this.BONUS_NUMBER = bonusNumber;
     }
 
-    private void validate(int bonusNumber) {
+    private void validate(int bonusNumber, Lotto winningLotto) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 ~ 45 사이어야 합니다.");
+        }
+        if (winningLotto.getNumbers().contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 겹치면 안됩니다.");
         }
     }
 
