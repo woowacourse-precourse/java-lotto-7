38fa93f5328dc0370657f0a39dba7794c9cf3dbb
J0YERIM <chosarah70@gmail.com>
2024-11-04 06:05:13 +0900

    test(domain): 로또 개수 계산 테스트 추가
    - 주어진 구입 금액에 따라 올바른 로또 개수가 계산되는지 검증하는 테스트 케이스 추가

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,10 +1,12 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PurchaseAmountTest {
@@ -30,5 +32,14 @@ class PurchaseAmountTest {
     @DisplayName("유효한 구입 금액일 때 예외 발생하지 않음")
     void 유효한_구입_금액일_때_예외_미발생(int amount) {
         assertDoesNotThrow(() -> new PurchaseAmount(amount));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1000, 1", "2000, 2", "5000, 5", "10000, 10"})
+    @DisplayName("구입 금액에 따라 로또 개수 계산 테스트")
+    void 로또_개수_계산_테스트(int amount, int expectedCount) {
+        PurchaseAmount purchaseAmount = new PurchaseAmount(amount);
+        int lottoCount = purchaseAmount.calculateLottoCount();
+        assertThat(lottoCount).isEqualTo(expectedCount);
     }
 }
