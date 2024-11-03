1786b63be598bf6e6a584be60f0ccdd23e19e8b8
kim-su-mi <ksmm9607@naver.com>
2024-11-03 18:25:55 +0900

    Feat : 1. 로또 구입금액 입력 및 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validatePurchaseAmount(Integer.parseInt(input));
+    }
+
+    private static void validatePurchaseAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+    
     }
 }
