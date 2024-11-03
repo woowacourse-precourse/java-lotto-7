c7d87819d6e124d64718e0dde9b0f3bdb01dbed3
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-04 00:47:25 +0900

    feat(Application): 로또 당첨 결과 출력 기능 추가
    - 당첨 번호와 보너스 번호 입력 후, 각 등수별 당첨 통계 출력 기능 구현
    - 총 상금 계산 후 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,31 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
+import lotto.controller.LottoNumberController;
 import lotto.controller.LottoPurchaseController;
+import lotto.controller.LottoResultChecker;
+import lotto.model.Lotto;
+import lotto.model.LottoBonus;
+import lotto.model.LottoRank;
+import lotto.model.LottoTicket;
+import lotto.view.OutputView;
 
 public class Application {
 	public static void main(String[] args) {
-		LottoPurchaseController LottoPurchaseController = new LottoPurchaseController();
-		LottoPurchaseController.run();
+		LottoPurchaseController lottoPurchaseController = new LottoPurchaseController();
+		List<LottoTicket> tickets = lottoPurchaseController.purchaseLottoTickets();
+
+		LottoNumberController lottoNumberController = new LottoNumberController();
+		Lotto winningLotto = lottoNumberController.generateWinningLotto();
+		LottoBonus bonusNumber = lottoNumberController.generateBonusNumber(winningLotto.getNumbers());
+
+		LottoResultChecker resultChecker = new LottoResultChecker();
+		Map<LottoRank, Integer> resultCount = resultChecker.checkResults(tickets, winningLotto, bonusNumber);
+		OutputView.displayResult(resultCount);
+
+		int totalPrize = resultChecker.calculateTotalPrize(resultCount);
+		OutputView.displayTotalPrize(totalPrize);
 	}
 }
