d730ef03073e5b19ad50394e3683c46ca43d9985
Park, Sojeong <sojeong2101@gmail.com>
2024-11-03 20:54:46 +0900

    feat($MatchCounter): 당첨 순위 카운터 구현
    일치 개수와 보너스 번호 유무에 따라 해당 당첨 순위의 카운터 값을 증가

---
+++ src/main/java/lotto/statistics/MatchCounter.java
@@ -0,0 +1,33 @@
+package lotto.statistics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchCounter {
+
+    private static final int COUNTER_CHANGE_POINT = 5;
+    private static final int COUNTER_UP = 1;
+
+    private final List<Integer> matchedNumbers = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0));
+
+    public void updateMatchResult(int matchedCount, boolean matchingBonusResult) {
+        int index = matchedCount;
+
+        if (matchedCount >= COUNTER_CHANGE_POINT) {
+            index = checkMatchedAllOrBonus(matchedCount, matchingBonusResult);
+        }
+        matchedNumbers.set(index, matchedNumbers.get(index) + COUNTER_UP);
+    }
+
+    public List<Integer> getMatchedNumbers() {
+        return matchedNumbers;
+    }
+
+    private int checkMatchedAllOrBonus(int matchedCount, boolean matchingBonusResult) {
+        if (!matchingBonusResult) {
+            return matchedCount;
+        }
+        return ++matchedCount;
+    }
+}
