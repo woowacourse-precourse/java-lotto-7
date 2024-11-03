0254d420bfee43614d7db2f13a1e9215fd7528f0
yeonnhuu <yeonnhuu@gmail.com>
2024-11-03 22:52:44 +0900

    :sparkles: feat(LottoGenerator): 로또 번호 생성

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,4 +1,26 @@
 package lotto.domain;
 
+import static lotto.handler.ConstantHandler.LOTTO_SIZE;
+import static lotto.handler.ConstantHandler.MAX_LOTTO_NUMBER;
+import static lotto.handler.ConstantHandler.MIN_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoGenerator {
+    public List<Lotto> generateLottos(int ticketCount) {
+        return IntStream.range(0, ticketCount)
+                .mapToObj(i -> generateLotto())
+                .collect(Collectors.toList());
+    }
+
+    private Lotto generateLotto() {
+        return new Lotto(generateLottoNumbers());
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
+    }
 }
