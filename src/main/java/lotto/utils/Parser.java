388d012cdc1dee054744715821f1987f4ae72d89
peanut990 <peanut1998@naver.com>
2024-11-04 01:58:27 +0900

    style(Parser): 가독성 개선을 위해 개행 문자 추가 및 static import 추가

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,7 +1,10 @@
 package lotto.utils;
 
 import static lotto.domain.LottoInfo.PRICE_FOR_ONE;
+import static lotto.utils.Utils.changeStringToNum;
 import static lotto.utils.Utils.makeErrorMessage;
+import static lotto.validation.LottoValidator.validateEmptyString;
+import static lotto.validation.LottoValidator.validateWinningNumbsSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,17 +13,20 @@ import lotto.validation.LottoValidator;
 public class Parser {
     public static List<Integer> parseWinningNumbs(String inputWinningNumbs) {
         String[] winningNumbsStrs = inputWinningNumbs.split(",");
+
         for (String winningNumbsStr : winningNumbsStrs) {
-            LottoValidator.validateEmptyString(winningNumbsStr);
+            validateEmptyString(winningNumbsStr);
         }
-        LottoValidator.validateWinningNumbsSize(winningNumbsStrs);
+
+        validateWinningNumbsSize(winningNumbsStrs);
 
         return  createWinningnumbs(winningNumbsStrs);
     }
 
     public static int parseBonusNum(String inputBonusNumber) {
-        LottoValidator.validateEmptyString(inputBonusNumber);
-        int bonusNumber = Utils.changeStringToNum(inputBonusNumber);
+        validateEmptyString(inputBonusNumber);
+
+        int bonusNumber = changeStringToNum(inputBonusNumber);
 
         LottoValidator.isLottoNumInRange(bonusNumber);
 
@@ -28,8 +34,8 @@ public class Parser {
     }
 
     public static int parseMoney(String inputMoney) {
-        LottoValidator.validateEmptyString(inputMoney);
-        int money = Utils.changeStringToNum(inputMoney);
+        validateEmptyString(inputMoney);
+        int money = changeStringToNum(inputMoney);
         if (money <= 0) {
             throw new IllegalArgumentException(makeErrorMessage("금액은 양수이어야 합니다."));
         }
@@ -42,7 +48,7 @@ public class Parser {
     private static List<Integer> createWinningnumbs(String[] winningNumbsStrs) {
         List<Integer> winningNumbs = new ArrayList<>();
         for (String winningNumStr : winningNumbsStrs) {
-            int winningNum = Utils.changeStringToNum(winningNumStr);
+            int winningNum = changeStringToNum(winningNumStr);
             LottoValidator.isLottoNumInRange(winningNum);
             winningNumbs.add(winningNum);
         }
