01d3ece3d71b6cb4cf82d2481eade6e186e3b31a
yyysolhhh <solbaram37@gmail.com>
2024-11-03 22:24:03 +0900

    feat(MatchResult): 일치 번호 정보 담는 클래스 생성
    - 일치하는 번호 개수, 보너스 번호 일치 여부 담는 클래스
    - 각각의 로또에 대한 당첨 결과

---
+++ src/main/java/lotto/domain/MatchResult.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class MatchResult {
+    private final int matchCount;
+    private final boolean matchBonus;
+
+    public MatchResult(int matchCount, boolean matchBonus) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isMatchBonus() {
+        return matchBonus;
+    }
+}
