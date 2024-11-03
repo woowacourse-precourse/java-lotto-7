6cfa0e3ce43087fad8a1f8cdb8e2ee07c25c36e5
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-03 23:37:06 +0900

    feat(Application) : 1.1 로또 구입 금액을 입력 받는 기능
    - camp.nextstep.edu.missionutils.Console의 readLine()을 통해 문자열을 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1.1 로또 구입 금액을 입력 받는 기능
+        System.out.println("구입 금액을 입력해 주세요.");
+        String purchaseAmountInput = Console.readLine();
+
     }
 }
