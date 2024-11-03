45e465cd11647870c84c36133b6dd3dc8b20bfbb
world-dv <goodforx0147@naver.com>
2024-11-03 22:24:37 +0900

    test: 구입 금액에 따른 로또 개수를 검증하는 테스트 코드 추가
    - 구입 금액에 따라 로또 개수가 올바르게 생성되는지 검증하는 테스트 코드를 추가합니다.

---
+++ src/test/java/lotto/service/PaymentTest.java
@@ -0,0 +1,25 @@
+package lotto.service;
+
+import lotto.domain.Wallet;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PaymentTest {
+
+    private static final Integer MIN_MONEY_UNIT = 1000;
+
+    @DisplayName("구입한 로또의 개수를 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 13000, 140000, 1500000, 132000, 333000})
+    void 구입한_로또의_개수를_확인한다(Integer input) {
+        Wallet wallet = new Wallet(input);
+
+        Payment payment = Payment.create(wallet);
+
+        assertThat(payment.getWalletMoney()).isEqualTo(input);
+        assertThat(payment.getLottoCount()).isEqualTo(input / MIN_MONEY_UNIT);
+    }
+}
