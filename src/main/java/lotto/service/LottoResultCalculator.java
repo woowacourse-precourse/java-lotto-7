c375ca141d084603481d28ae858bcb92d61ec0dc
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-03 17:25:31 +0900

    refactor: if-else문 제거

--- src/main/java/lotto/service/LottoResultCalculator.java
+++ src/main/java/lotto/service/LottoResultCalculator.java
@@ -3,27 +3,21 @@ package lotto.service;
 import lotto.model.Lotto;
 import lotto.model.LottoResult;
 import lotto.model.PurchaseAmount;
+import lotto.model.Rank;
 
 import java.util.List;
 
 public class LottoResultCalculator {
     public LottoResult calculateResults(List<Lotto> lottos, WinningNumber winningNumber, BonusNumber bonusNumber) {
-        int[] rankCounts = new int[6];
+        int[] rankCounts = new int[5];
 
         for (Lotto lotto : lottos) {
             int matchingCount = getMatchingCount(lotto.getNumbers(), winningNumber.getNumbers());
-            if (matchingCount == 6) {
-                rankCounts[4]++; // 1등
-            } else if (matchingCount == 5) {
-                if (bonusNumber.getBonus() != null && lotto.getNumbers().contains(bonusNumber.getBonus())) {
-                    rankCounts[3]++; // 2등
-                } else {
-                    rankCounts[2]++; // 3등
-                }
-            } else if (matchingCount == 4) {
-                rankCounts[1]++; // 4등
-            } else if (matchingCount == 3) {
-                rankCounts[0]++; // 5등
+            boolean hasBonus = bonusNumber.getBonus() != null && lotto.getNumbers().contains(bonusNumber.getBonus());
+
+            Rank rank = Rank.getRank(matchingCount, hasBonus);
+            if (rank != null) {
+                rankCounts[rank.getIndex()]++;
             }
         }
 
