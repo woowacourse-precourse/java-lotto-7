357a29fc2627e3b0598773784e94c74a14230a16
kwongio <rldh11111@naver.com>
2024-11-03 16:46:47 +0900

    refactor: 로또 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,30 +1,11 @@
 package lotto;
 
-import java.util.List;
-
-import lotto.controller.LottoController;
-import lotto.domain.Lotto;
-import lotto.domain.LottoResult;
-import lotto.generator.SortedLottoNumberGenerator;
-import lotto.service.LottoService;
-import lotto.view.LottoInputView;
-import lotto.view.LottoOutputView;
+import lotto.config.factory.LottoGameFactory;
+import lotto.game.LottoGame;
 
 public class Application {
     public static void main(String[] args) {
-        int money = LottoInputView.getMoney();
-        LottoController lottoController = new LottoController(new LottoService(new SortedLottoNumberGenerator()));
-        List<Lotto> lottos = lottoController.purchase(money);
-
-        LottoOutputView.printPurchaseLotto(lottos);
-
-        List<Integer> winNumber = LottoInputView.getWinNumber();
-        System.out.println(winNumber);
-
-        int bonusNumber = LottoInputView.getBonusNumber(winNumber);
-        System.out.println(bonusNumber);
-
-        LottoResult match = lottoController.match(lottos, winNumber, bonusNumber);
-        LottoOutputView.printResult(match, money);
+        LottoGame lottoGame = LottoGameFactory.create();
+        lottoGame.play();
     }
 }
