bc1287101217716556116ccf94be1f02e93d2ee7
양효빈 <hyobin3026@naver.com>
2024-11-03 21:26:45 +0900

    feat(NumberParserWithComma): Add class to parse numbers with comma - Implements NumberParser interface - Injects NumberConverter for number conversion

---
+++ src/main/java/lotto/util/NumberParserWithComma.java
@@ -0,0 +1,34 @@
+package lotto.util;
+
+import lotto.exception.GeneralExceptionMessages;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberParserWithComma implements NumberParser {
+
+    private final NumberConverter numberConverter;
+
+    public NumberParserWithComma(NumberConverter numberConverter) {
+        this.numberConverter = numberConverter;
+    }
+
+    @Override
+    public Set<Integer> parseNumbers(String input) {
+        Set<Integer> parsedNumbers = new HashSet<>();
+
+        Arrays.stream(input.split(","))
+                .map(this::parseNumber)
+                .forEach(number -> {
+                    if (!parsedNumbers.add(number)) {
+                        throw new IllegalArgumentException(GeneralExceptionMessages.DUPLICATE_NUMBER);
+                    }
+                });
+        return parsedNumbers;
+    }
+
+    private int parseNumber(String numberString) {
+        return numberConverter.convertNumber(numberString);
+    }
+}
