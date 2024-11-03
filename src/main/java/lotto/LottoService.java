860a22c8749a395ae65eff117726baccb736bd14
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-03 20:31:08 +0900

    feat: 1개의 로또를 발행할 때 중복되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -8,6 +8,14 @@ public class LottoService {
     private final static int LOTTO_MIN_NUMBER = 1;
     private final static int LOTTO_MAX_NUMBER = 45;
 
+    public List<Lotto> buyLottos(int lottoCount){
+        List<Lotto> lottos = new ArrayList<>();
+        while(lottoCount-->0){
+            lottos.add(new Lotto(makeLandomNumber()));
+        }
+        return lottos;
+    }
+
     public List<Integer> makeLandomNumber(){
         return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, 6);
     }
