dfd2c8272e7f0fcc86022073a28929b410c199ad
hyeonjeong9911 <knk990101@naver.com>
2024-11-03 20:26:52 +0900

    feat: 구입한 로또 개수 출력 기능

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,10 +1,12 @@
 package lotto.controller;
 
+import lotto.view.InputController;
 import lotto.view.OutputController;
 
 public class MainController {
     public static void run() {
         OutputController.purchasePrint();
-
+        InputController.purchaseInput();
+        OutputController.lottoAmountPrint();
     }
 }
--- src/main/java/lotto/view/OutputController.java
+++ src/main/java/lotto/view/OutputController.java
@@ -1,7 +1,14 @@
 package lotto.view;
 
+import lotto.service.InputService;
+
 public class OutputController {
     public static void purchasePrint() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void lottoAmountPrint() {
+        System.out.println(InputService.lottoAmount + "개를 구매했습니다.");
+    }
+
 }
