33b3ea688900ab8ca1eccca3595db3fc08d96e30
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-03 19:21:57 +0900

    Refactor : LottoFactory 변수 추가
    Lotto의 가격을 Factory가 갖는 것으로 함

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -6,13 +6,15 @@ import java.util.stream.IntStream;
 
 public class LottoFactory {
 
+    public static final int LOTTO_PRICE = 1000;
     private final Supplier<List<Integer>> numberMachine;
 
     public LottoFactory(Supplier<List<Integer>> numberMachine) {
         this.numberMachine = numberMachine;
     }
 
-    public Lottos generateLottos(int count) {
+    public Lottos generateLottos(long payment) {
+        int count = (int) (payment / 1000);
         List<Lotto> lottos = IntStream.range(0, count)
                 .mapToObj(i -> new Lotto(numberMachine.get()))
                 .toList();
