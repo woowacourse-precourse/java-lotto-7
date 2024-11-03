0cc5b5c606c6cfa91a1abbe2ef790d4a3af79b9e
btongtong <leeym2615@naver.com>
2024-11-04 01:34:22 +0900

    feat(UserLottos): Add UserLottos for purchase Info

---
+++ src/main/java/lotto/UserLottos.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class UserLottos {
+    private final int purchaseAmount;
+
+    public UserLottos(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+}
