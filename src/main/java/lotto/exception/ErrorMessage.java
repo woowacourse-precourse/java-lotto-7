1561233cf3c83169181159132ab29dc9f844a7fa
yuntasha <jaljayo85@gmail.com>
2024-11-03 11:36:15 +0900

    feat(ErrorMessage): add error case
    Add NOT_INIT_STATE

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -6,9 +6,11 @@ public enum ErrorMessage {
     NOT_INPUT_FORMAT("형식에 맞지 않습니다."),
     NOT_NATURAL_NUM("자연수로 입력해주세요."),
 
+
     // LottoMachine Error
     ILLEGAL_GAMBLING("복권 100장 초과 구매는 불법입니다."),
     UNDIVIDED_THOUSAND("복권은 한장당 1000원입니다."),
+    NOT_INIT_STATE("초기 설정이 되지않은 상태입니다"),
 
     // Lotto Error
     NOT_SIX_NUM("복권은 6개의 숫자로 이루어져있습니다."),
