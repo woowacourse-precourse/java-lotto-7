c96469f7f651d39c3059afb718ba00066ff3b53c
Gahyeon <best11gh@naver.com>
2024-11-04 00:12:03 +0900

    feat: LottoService 생성

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,17 @@
+package lotto.service;
+
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumberGenerator;
+import lotto.domain.Lottos;
+
+public class LottoService {
+    public Lottos generateLottos(int purchaseAmount) {
+        List<Lotto> lottos = Stream.generate(() -> new Lotto(LottoNumberGenerator.generate()))
+                .limit(purchaseAmount)
+                .toList();
+        return new Lottos(lottos);
+    }
+
+}
