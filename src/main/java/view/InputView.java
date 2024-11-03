464fcaf715d8c383fb9f2eb4ae441d3ea9424e5f
pakaheo <k70923@konkuk.ac.kr>
2024-11-04 00:11:52 +0900

    refactor : Separate methods

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,42 +1,51 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
-import domain.winningLotto.BonusNumber;
 import domain.lotto.PurchaseAmount;
+import domain.winningLotto.BonusNumber;
 import domain.winningLotto.WinningNumbers;
 
 public class InputView {
 
     public PurchaseAmount inputPurchaseAmount() {
-        while (true) {
-            System.out.println("구입금액을 입력해 주세요.");
-            try {
-                return new PurchaseAmount(Console.readLine());
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
+        System.out.println("구입금액을 입력해 주세요.");
+        return createPurchaseAmount(Console.readLine());
+    }
+
+    public PurchaseAmount createPurchaseAmount(String input) {
+        try {
+            return new PurchaseAmount(input);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputPurchaseAmount();
         }
     }
 
     public WinningNumbers inputWinningNumbers() {
-        while (true) {
-            System.out.println("당첨 번호를 입력해 주세요.");
-            try {
-                return new WinningNumbers(Console.readLine());
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return createWinningNumbers(Console.readLine());
+    }
+
+    public WinningNumbers createWinningNumbers(String input) {
+        try {
+            return new WinningNumbers(input);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputWinningNumbers();
         }
     }
 
     public BonusNumber inputBonusNumber(WinningNumbers winningNumbers) {
-        while (true) {
-            System.out.println("보너스 번호를 입력해 주세요.");
-            try {
-                return new BonusNumber(Console.readLine(), winningNumbers);
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return createBonusNumber(Console.readLine(), winningNumbers);
+    }
+
+    public BonusNumber createBonusNumber(String input, WinningNumbers winningNumbers) {
+        try {
+            return new BonusNumber(input, winningNumbers);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputBonusNumber(winningNumbers);
         }
     }
 }
