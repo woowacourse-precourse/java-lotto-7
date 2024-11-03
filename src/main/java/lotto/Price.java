d562a900ba9dbe5b34e1252514cc0a5053000f6d
daeyeon <kdy4410121@naver.com>
2024-11-03 15:27:32 +0900

    feat: enum에 집합 추가

--- src/main/java/lotto/Price.java
+++ src/main/java/lotto/Price.java
@@ -1,5 +1,26 @@
 package lotto;
 
 public enum Price {
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    FIVE_BONUS(5, 30000000),
+    SIX(6, 2000000000);
+
+    private final int number;
+    private final int amount;
+
+    Price(int number, int amount){
+        this.number = number;
+        this.amount = amount;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
 
 }
