4f284e8c43d3876b5435a7e7f7ddd5c342d9c4a2
imjaehyeong <dlawogud517@gmail.com>
2024-11-04 01:56:12 +0900

    feat(Message): 로또 관련 출력 메시지 작성

---
+++ src/main/java/util/Message.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Message {
+    public static final String PURCHASE_COST = "구입금액을 입력해 주세요.";
+    public static final String TICKET_COUNT = "개를 구매했습니다";
+    public static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String WIN_STATISTICS_HEADER = "당첨 통계\n---";
+    public static final String MATCH_3 = "3개 일치 (5,000원) - %d개";
+    public static final String MATCH_4 = "4개 일치 (50,000원) - %d개";
+    public static final String MATCH_5 = "5개 일치 (1,500,000원) - %d개";
+    public static final String MATCH_5_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String MATCH_6 = "6개 일치 (2,000,000,000원) - %d개";
+    public static final String PROFIT_RATE = "총 수익률은 %.1f%%입니다.%n";
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,7 @@
+package view;
+
+public class InputView {
+    public void printPurchasePrompt() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+}
