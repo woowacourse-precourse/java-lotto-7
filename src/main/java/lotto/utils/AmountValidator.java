4736e669c7a99a68e7947903e6d5702dd1853f45
sc@t0a14 <gusdn6671@gmail.com>
2024-11-03 22:47:33 +0900

    feat(AmountValidator): 금액 유효성 검증 메서드 추가

--- src/main/java/lotto/utils/AmountValidator.java
+++ src/main/java/lotto/utils/AmountValidator.java
@@ -1,6 +1,14 @@
 package lotto.utils;
 
 public class AmountValidator {
+
+    public static void validateAmount(String input) {
+        validateNotBlank(input);
+        validateIsNumeric(input);
+        validateMultipleOfThousand(input);
+        validateNotZero(input);
+    }
+
     private static void validateNotBlank(String input) {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException("입력이 공백일 수 없습니다.");
