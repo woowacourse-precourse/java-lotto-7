8951c3f13bc62365f79ba988a3fc845f0f04b030
eeun1225 <eeun9108@gmail.com>
2024-11-03 16:12:38 +0900

    feat: Lotto 클래스에 번호 정렬 기능 추가
    - Lotto 번호를 생성 시 자동으로 오름차순 정렬되도록 sortNumbers 메서드 추가
    - getNumbers 메서드를 통해 정렬된 번호 목록 반환

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -7,7 +7,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +16,13 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
