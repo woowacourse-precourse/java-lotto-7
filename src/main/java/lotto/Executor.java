8ca9bc41c4bfe0769fbe0687cce2ce5d9e9676ef
chyo1 <hyowon3856@gmail.com>
2024-11-04 03:27:48 +0900

    feat : 구매 금액, 당첨 번호, 보너스 번호 입력 후 저장 기능 추가

--- src/main/java/lotto/Executor.java
+++ src/main/java/lotto/Executor.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Executor {
     IOController ioController;
     Validator validator;
@@ -9,5 +11,60 @@ public class Executor {
         this.ioController = ioController;
         this.validator = validator;
         this.parser = parser;
+    }
+
+    public void run() {
+        int purchaseAmount = getPurchaseAmount();
+
+        // 구입금액만큼 lotto 생성 후 출력
+        int purchaseCount = purchaseAmount / 1000;
+        Lottos issuedLottos = Lottos.generateLottosByCount(purchaseCount);
+        ioController.printLottos(issuedLottos.getLottos());
+
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber(winningNumbers);
+        WinningLotto winningLotto = WinningLotto.of(winningNumbers, bonusNumber);
+    }
+
+    private int getPurchaseAmount() {
+        String purchaseAmountInput;
+        while (true) {
+            try {
+                purchaseAmountInput = ioController.inputPurchaseAmount();
+                validator.validatePurchaseAmount(purchaseAmountInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return parser.parsePurchaseAmount(purchaseAmountInput);
+    }
+
+    private int getBonusNumber(List<Integer> winningNumbers) {
+        String bonusNumberInput;
+        while (true) {
+            try {
+                bonusNumberInput = ioController.inputBonusNumber();
+                validator.validateBonusNumber(bonusNumberInput, winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return parser.parseBonusNumber(bonusNumberInput);
+    }
+
+    private List<Integer> getWinningNumbers() {
+        String winningNumbersInput;
+        while (true) {
+            try {
+                winningNumbersInput = ioController.inputWinningNumbers();
+                validator.validateWinningNumbers(winningNumbersInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return parser.parseWinningNumbers(winningNumbersInput);
     }
 }
