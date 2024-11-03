c223a4df82eb8b623da02110a815d399155aa756
USER <cji513@naver.com>
2024-11-04 01:03:36 +0900

    refactor(ErrorMessages): 에러 출력문 수정

--- src/main/java/lotto/exception/ErrorMessages.java
+++ src/main/java/lotto/exception/ErrorMessages.java
@@ -5,7 +5,7 @@ public enum ErrorMessages {
     CHECK_INT("입력값은 숫자여야 합니다."),
     THOUSAND_UNIT("1000원 단위로 입력해야 합니다."),
     MIN_RANGE("최솟값은 1000입니다."),
-    CHECK_SIZE("6개의 숫자를 입력해야 합니다."),
+    CHECK_SIZE("로또 번호는 6개여야 합니다."),
     CHECK_DUPLICATE("중복되는 숫자가 없어야 합니다."),
     CHECK_DUPLICATE_WITH_WINNING_NUMBER("당첨 번호와 보너스 번호는 중복될 수 없습니다."),
     OUT_OF_RANGE("숫자의 범위는 1부터 45입니다.");
