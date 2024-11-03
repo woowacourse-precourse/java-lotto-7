f27005b8fbdf308b666ee2f738fb6af48db02d3e
choign3123 <choign3123@naver.com>
2024-11-03 23:07:10 +0900

    feat(Application): 광역 에러 캐치 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,11 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame();
-        lottoGame.run();
+        try {
+            LottoGame lottoGame = new LottoGame();
+            lottoGame.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
