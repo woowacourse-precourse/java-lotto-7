3ef372b15612d7b8e9808992fb622124adaf12f1
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:56:14 +0900

    feat: Trial 클래스 구현

--- src/main/java/lotto/model/Trial.java
+++ src/main/java/lotto/model/Trial.java
@@ -1,4 +1,14 @@
 package lotto.model;
 
 public class Trial {
+
+    private final int trial;
+
+    public Trial(int money) {
+        this.trial = money / 1000;
+    }
+
+    public int getTrial() {
+        return trial;
+    }
 }
