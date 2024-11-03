b0544863d44edb0ff55998422bb9aa53c1e6269f
DoomchitYJ <poj1004@inha.edu>
2024-11-04 00:28:22 +0900

    refactor(LottoService): 서비스 계층 구현
    - Controller의 역할을 분리하고 가독성과 코드 재사용성을 높임

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -2,45 +2,26 @@ package lotto.controller;
 
 import java.util.List;
 import lotto.Lotto;
-import lotto.domain.BonusNumber;
-import lotto.domain.LottoMarket;
-import lotto.domain.NumbersComparator;
-import lotto.domain.ProfitRatioCalculator;
-import lotto.domain.WinningNumbers;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Controller {
+
+    private final LottoService lottoService = new LottoService();
+
     public void run() {
 
-        // 구입 금액을 입력
         int inputMoney = InputView.readMoney();
-
-        // 로또 번호 생성
-        LottoMarket lottoMarket = new LottoMarket();
-        List<Lotto> myLottos = lottoMarket.buyLotto(inputMoney);
-
-        // 발급된 로또 번호를 출력
+        List<Lotto> myLottos = lottoService.buyLotto(inputMoney);
         OutputView.showLottoNumbers(myLottos);
 
-        // 당첨 번호를 입력
         String inputNumbers = InputView.readWinningNumbers();
-
-        // 보너스 번호를 입력
         String inputBonusNumber = InputView.readBonusNumber();
-
-        // 번호 비교
-        BonusNumber bonusNumber = new BonusNumber(inputBonusNumber);
-        WinningNumbers winningNumbers = new WinningNumbers(inputNumbers, bonusNumber);
-        NumbersComparator numbersComparator = new NumbersComparator(myLottos, winningNumbers);
-        int[] result = numbersComparator.determineRank();
-
-        // 수익률 계산
-        ProfitRatioCalculator profitRatioCalculator = new ProfitRatioCalculator(result, myLottos);
-
-        // 당첨 통계와 수익률 출력
+        int[] result = lottoService.compareNumbers(myLottos, inputNumbers, inputBonusNumber);
+        float profit = lottoService.calculateProfit(result, myLottos);
         OutputView.showWinningResult(result);
-        OutputView.showProfitRatio(profitRatioCalculator.calculateProfitRatio());
-
+        OutputView.showProfitRatio(profit);
+    
     }
 }
---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.domain.BonusNumber;
+import lotto.domain.LottoMarket;
+import lotto.domain.NumbersComparator;
+import lotto.domain.ProfitRatioCalculator;
+import lotto.domain.WinningNumbers;
+
+public class LottoService {
+
+    public List<Lotto> buyLotto(int money) {
+        LottoMarket lottoMarket = new LottoMarket();
+        return lottoMarket.buyLotto(money);
+    }
+
+    public int[] compareNumbers(List<Lotto> myLottos, String inputNumbers, String inputBonusNumber) {
+        BonusNumber bonusNumber = new BonusNumber(inputBonusNumber);
+        WinningNumbers winningNumbers = new WinningNumbers(inputNumbers, bonusNumber);
+        NumbersComparator numbersComparator = new NumbersComparator(myLottos, winningNumbers);
+        return numbersComparator.determineRank();
+    }
+
+    public float calculateProfit(int[] result, List<Lotto> myLottos) {
+        ProfitRatioCalculator profitRatioCalculator = new ProfitRatioCalculator(result, myLottos);
+        return profitRatioCalculator.calculateProfitRatio();
+    }
+}
