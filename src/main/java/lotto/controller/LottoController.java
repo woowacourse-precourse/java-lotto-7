cac2fb1a3e06ab7e90c1afd92de84ec96ab070a6
lucku <lsw6731@naver.com>
2024-11-03 17:25:08 +0900

    feat: LottoController 클래스 추가 및 핵심 로또 로직 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,79 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.LottoBuyAmount;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningLotto;
+import lotto.service.LottoService;
+import lotto.util.StringUtils;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final LottoService lottoService;
+
+    public LottoController() {
+        lottoService = new LottoService();
+    }
+
+    public void run() {
+        buyAndPrintLottos();
+        calculateAndPrintResult();
+    }
+
+    private void buyAndPrintLottos() {
+        List<Lotto> lottos = lottoService.buyLotto(inputBuyAmount());
+
+        OutputView.printLottos(lottos);
+    }
+
+    private LottoBuyAmount inputBuyAmount() {
+        try {
+            int amount = StringUtils.parseToInt(InputView.getBuyAmount());
+
+            return new LottoBuyAmount(amount);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputBuyAmount();
+        }
+    }
+
+    private void calculateAndPrintResult() {
+        WinningLotto winningLotto = inputWinningLotto();
+        BonusNumber bonusNumber = inputBonusNumber(winningLotto.getWinningNumbers());
+        LottoResult result = lottoService.calculateResult(winningLotto, bonusNumber);
+        double profitRate = lottoService.calculateProfitRate(result);
+
+        printResult(result, profitRate);
+    }
+
+    private WinningLotto inputWinningLotto() {
+        try {
+            List<Integer> winningNumbers = StringUtils.parseNumbers(InputView.getWinningNumbers());
+
+            return new WinningLotto(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputWinningLotto();
+        }
+    }
+
+    private BonusNumber inputBonusNumber(List<Integer> winningNumbers) {
+        try {
+            int bonusNumber = StringUtils.parseToInt(InputView.getBonusNumber());
+
+            return new BonusNumber(bonusNumber, winningNumbers);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputBonusNumber(winningNumbers);
+        }
+    }
+
+    private void printResult(LottoResult result, double profitRate) {
+        OutputView.printResultMessage();
+        OutputView.printMatchCounts(result);
+        OutputView.printProfitRate(profitRate);
+    }
+}
