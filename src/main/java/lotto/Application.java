feb211f8339867cad18d576a0a830d8e7551c19a
haeun <haeun9634@naver.com>
2024-11-03 22:21:50 +0900

    Refactor: 사용자가 잘못 입력해도 제대로 된 값을 입력할 때까지 다시 입력하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        Integer money = Input.inputMoney();
+        Integer money = inputMoney();
         List<Lotto> lottos = Lotto.start(money);
-        List <Integer> winNumbers = Input.inputWinNumbers();
-        Integer bonusNumber = Input.inputBonusNumber(winNumbers);
+        List <Integer> winNumbers = inputWinNumbers();
+        Integer bonusNumber = inputBonusNumber(winNumbers);
 
         LottoStatistics statistics = new LottoStatistics();
 
@@ -18,5 +18,36 @@ public class Application {
         }
 
         Print.printStatistics(statistics.getStatistics());
+    }
+
+
+    private static Integer inputMoney() {
+        while (true) {
+            try {
+                return Input.inputMoney();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static List<Integer> inputWinNumbers() {
+        while (true) {
+            try {
+                return Input.inputWinNumbers();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static Integer inputBonusNumber(List<Integer> winNumbers) {
+        while (true) {
+            try {
+                return Input.inputBonusNumber(winNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
