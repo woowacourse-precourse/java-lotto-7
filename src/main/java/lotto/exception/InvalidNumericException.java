03b32790f36f25a89eb74f0f113ba9394933a06f
immyeong <wpfls6670@naver.com>
2024-11-03 19:46:11 +0900

    feat: 번호가 숫자가 아닌 경우 예외 추가

---
+++ src/main/java/lotto/exception/InvalidNumericException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class InvalidNumericException extends IllegalArgumentException{
+    public InvalidNumericException(String message) {
+        super(message);
+    }
+}
