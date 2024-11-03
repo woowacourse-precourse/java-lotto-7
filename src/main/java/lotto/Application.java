088cff5034400c6091a92166e817e1002f6cfddc
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2024-11-03 14:16:40 +0900

    feat: TUI 관련 ENUM 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -58,6 +58,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -25,4 +25,20 @@ public class Constants {
         public String getCondition() { return this.condition; }
         public int getPrize() { return this.prize; }
     }
+    public enum TUINotice {
+        NOTICE_INPUT_PAYMENT("구입금액을 입력해 주세요.", "" ),
+        NOTICE_INPUT_BUY_COUNT("개를 구매했습니다.", "" ),
+        NOTICE_INPUT_WINNING_NUMBER( "당첨 번호를 입력해 주세요.", "" ),
+        NOTICE_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.", "" ),
+        NOTICE_OUTPUT_WIN_RESULT( "당첨 통계", "---" ),
+        NOTICE_OUTPUT_EARN_RATE( "총 수익률은", "입니다." );
+        private final String output;
+        private final String helper;
+        TUINotice( String output, String helper ){
+            this.output = output;
+            this.helper = helper;
+        }
+        public String getOutput() { return this.output; }
+        public String getHelper() { return this.helper; }
+    }
 }
