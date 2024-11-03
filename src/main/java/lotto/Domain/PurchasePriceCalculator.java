6ec29e049afcf49906e5a2f573955223b30f72f9
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 01:39:35 +0900

    feat(PurchasePriceCalculator): 사용자가 입력한 금액에 해당하는 로또의 수량을 계산하는 로직 추가
    - 장당 1,000원의 가격을 고려해서 장수를 반환하게끔 구현

--- src/main/java/lotto/Domain/PurchasePriceCalculator.java
+++ src/main/java/lotto/Domain/PurchasePriceCalculator.java
@@ -1,4 +1,9 @@
 package lotto.Domain;
 
 public class PurchasePriceCalculator {
+    private static Integer PRICE_PER_ONE_LOTTO = 1000;
+
+    public static Integer calculateLottoPrice(Integer paymentPrice) {
+        return paymentPrice / PRICE_PER_ONE_LOTTO;
+    }
 }
