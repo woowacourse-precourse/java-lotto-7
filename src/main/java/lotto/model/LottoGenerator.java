5c65077b7d603f795fb362f9b551ca2f29ae1093
TalkingPotato90 <star901210@hanmail.net>
2024-11-03 16:54:55 +0900

    refactor: 로또 생성 기능 세분화

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -2,13 +2,27 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoGenerator {
+    private Lotto buildLotto(List<Integer> orderedNumbers) {
+        return new Lotto(orderedNumbers);
+    }
+
+    private List<Integer> createRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private void arrangeLottoNumbers(List<Integer> numbers) {
+        numbers.sort(Comparator.naturalOrder());
+    }
+
     public Lotto createSingleLotto() {
-        List<Integer> singleLottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return new Lotto(singleLottoNumber);
+        List<Integer> rawLotto = createRandomLottoNumbers();
+        arrangeLottoNumbers(rawLotto);
+        return buildLotto(rawLotto);
     }
 
     public List<Lotto> createMultipleLottos(int ticketCount) {
