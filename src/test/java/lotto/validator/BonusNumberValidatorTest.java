e63942b33bdd8338c5cc65387ed1a8dc8089a12e
navangs <43688529+insertend23@users.noreply.github.com>
2024-11-04 04:15:58 +0900

    Test: write test code for new features

---
+++ src/test/java/lotto/validator/BonusNumberValidatorTest.java
@@ -0,0 +1,33 @@
+package lotto.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BonusNumberValidatorTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private final List<Integer> mockWinningNumbers = List.of(1, 2, 3, 4, 5, 6);
+    private BonusNumberValidator validator;
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void bonusNumberOutOfRangeTest(int value) {
+        validator = new BonusNumberValidator(mockWinningNumbers, value);
+
+        assertThatThrownBy(() -> validator.validate())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    void bonusNumberDuplicatedTest() {
+        validator = new BonusNumberValidator(mockWinningNumbers, 1);
+        
+        assertThatThrownBy(() -> validator.validate())
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(ERROR_MESSAGE);
+    }
+}
