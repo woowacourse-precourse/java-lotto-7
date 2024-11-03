f1b60c05b383e42a821372a9c056fce0d2bea242
SukJin Kim <rkathrwks@gmail.com>
2024-11-03 11:50:23 +0900

    test(model): 발행 수량만큼 로또 발행 기능 유닛 테스트 구현

---
+++ src/test/java/lotto/model/LottoGroupTest.java
@@ -0,0 +1,69 @@
+package lotto.model;
+
+import static lotto.constants.CommonConstants.LOTTO_SIZE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class LottoGroupTest {
+
+    @Test
+    void 로또_그룹은_지정된_갯수만큼_로또를_생성한다() {
+        // Given
+        long numLotto = 5;
+
+        // When
+        LottoGroup lottoGroup = new LottoGroup(numLotto);
+        List<Lotto> lottos = lottoGroup.getLottos();
+
+        // Then
+        assertEquals(numLotto, lottos.size());
+    }
+
+    @Test
+    void 로또_그룹에서_생성된_모든_로또는_고유하다() {
+        // Given
+        long numLotto = 100;
+
+        // When
+        LottoGroup lottoGroup = new LottoGroup(numLotto);
+        List<Lotto> lottos = lottoGroup.getLottos();
+
+        // Then
+        Set<Integer> uniqueIds = new HashSet<>();
+        for (Lotto lotto : lottos) {
+            uniqueIds.add(lotto.getId());
+        }
+
+        assertEquals(numLotto, uniqueIds.size());
+    }
+
+    @Test
+    void 로또_그룹에서_생성된_모든_로또는_유효하다() {
+        // Given
+        long numLotto = 10;
+
+        // When
+        LottoGroup lottoGroup = new LottoGroup(numLotto);
+        List<Lotto> lottos = lottoGroup.getLottos();
+
+        // Then
+        for (Lotto lotto : lottos) {
+            assertNotNull(lotto);
+            assertEquals(LOTTO_SIZE, lotto.getNumbers().size());
+            assertTrue(lotto.getNumbers().stream().allMatch(num -> num >= 1 && num <= 45));
+
+            List<Integer> numbers = lotto.getNumbers();
+            boolean isSorted = IntStream.range(0, numbers.size() - 1)
+                    .allMatch(i -> numbers.get(i) < numbers.get(i + 1));
+            assertTrue(isSorted);
+        }
+    }
+
+}
