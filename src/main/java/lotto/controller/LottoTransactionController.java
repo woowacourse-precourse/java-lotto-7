e0c5c69e684eb1919652e8ca30348aa34f04bcb7
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-03 19:43:34 +0900

    feat: sellManualLotto() 추가
    - sellManualLotto(): 수기로 로또 번호 기입 로또 구매 기능 추가
    - sellLotto => sellAutoLotto()
    - LottoTransactionControllerTest에 적용
    
    1. 랜덤한 숫자로 로또를 발급하는 것은 테스트 코드 작성에 어려움 있기에 수동 발급 함수 추가했습니다.
    2.. 새로운 함수 추가로 기존에 있던 함수명이 의미하는 바를 명확하게 전달하기 위해서 수정하였습니다

--- src/main/java/lotto/controller/LottoTransactionController.java
+++ src/main/java/lotto/controller/LottoTransactionController.java
@@ -2,11 +2,14 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import lotto.model.Lotto;
 import lotto.model.LottoTransaction;
+import lotto.model.PrizeRank;
 
 public class LottoTransactionController {
   final int LOTTO_PRICE = 1000; // TODO MainController 말고 다른 곳에 정의할지 생각
@@ -18,7 +21,7 @@ public class LottoTransactionController {
   }
 
   // TODO amount 대한 vaildate는 sellLotto 또는 inputView에서 할지 생각
-  public List<Lotto> sellLotto(int amount) {
+  public List<Lotto> sellAutoLotto(int amount) {
     List<Lotto> lottos = new ArrayList<>();
     int count = amount / LOTTO_PRICE;
 
@@ -26,6 +29,23 @@ public class LottoTransactionController {
       Lotto lotto = produceLotto();
       lottos.add(lotto);
     }
+    System.out.println(count + "개를 구매했습니다."); // TODO 출력 처리 이동
+    lottoTransaction.setPurchasedLottos(lottos);
+    lottoTransaction.setAmount(amount);
+
+    return lottos;
+  }
+
+
+  public List<Lotto> sellManualLotto(List<List<Integer>> lottoNumbers, int amount) {
+    List<Lotto> lottos = new ArrayList<>();
+    int count = amount / LOTTO_PRICE;
+
+    for (List<Integer> lottoNumber : lottoNumbers) {
+      Lotto lotto = new Lotto(lottoNumber);
+      lottos.add(lotto);
+    }
+
     System.out.println(count + "개를 구매했습니다."); // TODO 출력 처리 이동
     lottoTransaction.setPurchasedLottos(lottos);
     lottoTransaction.setAmount(amount);
--- src/test/java/lotto/controller/LottoTransactionControllerTest.java
+++ src/test/java/lotto/controller/LottoTransactionControllerTest.java
@@ -21,7 +21,7 @@ public class LottoTransactionControllerTest {
   @Test
   public void 로또_구매_기능() {
     int money = 5000;
-    List<Lotto> lottos = controller.sellLotto(money);
+    List<Lotto> lottos = controller.sellAutoLotto(money);
 
     assertEquals(5, lottos.size(), "5개를 구매했습니다.");
   }
@@ -29,7 +29,7 @@ public class LottoTransactionControllerTest {
   @Test
   public void 로또_구매_0원_금액() {
     int money = 0;
-    List<Lotto> lottos = controller.sellLotto(money);
+    List<Lotto> lottos = controller.sellAutoLotto(money);
 
     // TODO 0원 처럼 아예 살 수 없는 경우 예외처리 class 정하기
     assertEquals(0, lottos.size(), "0원의 금액으로는 로또를 구매할 수 없습니다.");
@@ -40,7 +40,7 @@ public class LottoTransactionControllerTest {
     int money = 5000;
     List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-    List<Lotto> lottos = controller.sellLotto(money);
+    List<Lotto> lottos = controller.sellAutoLotto(money);
 
     controller.compareWinningNumbers(winningNumbers, 45);
   }
