23f6e22c46bb12dfcb6b7c457a4c4b0a85373bec
윤민수 <koelysa5696@naver.com>
2024-11-03 21:39:49 +0900

    chore: 캐시 삭제

---
+++ src/main/java/lotto/Application.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.controller.LottoController;
+
+public class Application {
+    public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.run();
+        // TODO: 프로그램 구현
+    }
+}
---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String InputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String InputWinNumber() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String InputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+}
