37af7d608381bde0d0b065d99f94a351cf6f57f5
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:49:31 +0900

    feat: Lotto 클래스의 matching 메소드를 호출하여 로또 추첨

--- src/main/java/lotto/domain/WinningChecker.java
+++ src/main/java/lotto/domain/WinningChecker.java
@@ -1,2 +1,27 @@
-package lotto.domain;public class WinningChecker {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lotto.repository.LottoRepository;
+
+public class WinningChecker {
+    private LottoRepository lottoRepository;
+    private List<int[]> matchingResult = new ArrayList<>();
+
+    public Map<String, Integer> checkWinning() {
+        Bonus bonus = lottoRepository.getBonusNumber();
+        Lotto lotto = lottoRepository.getWinningNumbers();
+
+        for (List<Integer> userLotto : lottoRepository.getLottoNumbers()) {
+            matchingResult.add(lotto.matching(userLotto, bonus));
+        }
+        return new LottoRankCounter().countRanks(matchingResult);
+    }
+
+    public WinningChecker(LottoRepository lottoRepository) {
+        this.lottoRepository = lottoRepository;
+
+    }
+
 }
