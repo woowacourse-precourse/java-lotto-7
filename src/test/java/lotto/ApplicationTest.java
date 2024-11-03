4cb155b7928b78f74f3ffff7aa2fdd29491ddc14
yaongmeow <minnie_00@naver.com>
2024-11-03 14:54:06 +0900

    test(Application): 타입 오류 수정
    - 불변 정스트를 가변 리스트로 수정

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
@@ -35,14 +36,14 @@ class ApplicationTest extends NsTest {
                             "총 수익률은 62.5%입니다."
                     );
                 },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
+                new ArrayList<>(List.of(8, 21, 23, 41, 42, 43)),
+                new ArrayList<>(List.of(3, 5, 11, 16, 32, 38)),
+                new ArrayList<>(List.of(7, 11, 16, 35, 36, 44)),
+                new ArrayList<>(List.of(1, 8, 11, 31, 41, 42)),
+                new ArrayList<>(List.of(13, 14, 16, 38, 42, 45)),
+                new ArrayList<>(List.of(7, 11, 30, 40, 42, 43)),
+                new ArrayList<>(List.of(2, 13, 22, 32, 38, 45)),
+                new ArrayList<>(List.of(1, 3, 5, 14, 22, 45))
         );
     }
 
@@ -80,6 +81,7 @@ class ApplicationTest extends NsTest {
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
     @Test
     void 예외_테스트_당첨_번호_형식_2() {
         assertSimpleTest(() -> {
