d25ce3645c61918b1a211e16fd3a20c03e782454
wgrgwg <kyoeng14697@naver.com>
2024-11-04 03:35:06 +0900

    fix: Fix calculateLottoResult logic
    Fixed calculateLottoResult logic to handle bigger number.

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -1,10 +1,12 @@
 package lotto.model;
 
+import java.math.BigInteger;
 import java.util.Map;
 import lotto.constants.PrizeRank;
 
 public class LottoGame {
 
+    private static final double TO_ROUND = 10.0;
     private final LottoTickets lottoTickets;
     private final WinningLotto winningLotto;
     private final int money;
@@ -18,10 +20,12 @@ public class LottoGame {
     public LottoGameResult calculateLottoResults() {
         Map<PrizeRank, Integer> prizeResultMap = lottoTickets.getPrizeRankCountMap(winningLotto);
 
-        long prizeMoney = prizeResultMap.entrySet().stream()
-                .mapToLong(entry -> (long) entry.getKey().getPrizeMoney() * entry.getValue())
-                .sum();
-        double profitRate = Math.round((double) prizeMoney / money);
+        BigInteger prizeMoney = prizeResultMap.entrySet().stream()
+                .map(entry -> BigInteger.valueOf(entry.getKey().getPrizeMoney())
+                        .multiply(BigInteger.valueOf(entry.getValue())))
+                .reduce(BigInteger.ZERO, BigInteger::add);
+
+        double profitRate = Math.round(TO_ROUND * prizeMoney.doubleValue() / money) / TO_ROUND;
 
         return new LottoGameResult(prizeResultMap, profitRate);
     }
