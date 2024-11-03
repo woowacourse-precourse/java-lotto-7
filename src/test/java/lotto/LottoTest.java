3ec24ff2ffde42ad8a9ef4be4aeb97ec073ed5e0
howareyoo <newworld121@naver.com>
2024-11-03 11:01:09 +0900

    test: unit tests for Buy class

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,13 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.dto.WinningResultDto;
+import lotto.dto.WinningResultsDto;
 import lotto.model.Bonus;
 import lotto.model.Buy;
 import lotto.model.Lotto;
+import lotto.model.Winning;
+import lotto.view.LottoView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,20 +30,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
+   @Test
     void 로또_번호의_입력_값_숫자_입력후_리스트_반환(){
         String userInput = "1, 2, 3, 4, 5, 6";
-        LottoController lottoController = new LottoController();
-        List<Integer> userNumbers = lottoController.getUserNumbers(userInput);
+       LottoView view = new LottoView();
+
+       LottoController lottoController = new LottoController(view);
+        List<Integer> userNumbers = lottoController.setUserNumbers(userInput);
         assertThat(userNumbers).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
     @Test
     void 로또_번호의_입력_값_숫자가_아닌_입력값이_있으면_예외가_발생한다(){
         String userInput = "1,e,f";
-        LottoController lottoController = new LottoController();
+        LottoView view = new LottoView();
 
-        assertThatThrownBy(() -> lottoController.getUserNumbers(userInput))
+        LottoController lottoController = new LottoController(view);
+        assertThatThrownBy(() -> lottoController.setUserNumbers(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 로또 번호는 숫자여야 합니다.");
     }
@@ -74,7 +81,6 @@ class LottoTest {
                 .hasMessage("[ERROR] 로또 구매 금액 단위는 1000원 단위 입니다.");
 
     }
-
     @Test
     void 구매_금액_양수가_아니면_예외가_발생한다(){
         assertThatThrownBy(() -> new Buy(-1000))
@@ -83,5 +89,25 @@ class LottoTest {
 
     }
 
+    @Test
+    void 유저_구매_횟수만큼_로또_발행(){
+        Integer userPurcharsePriceCount = 4;
+        Winning winning = new Winning(userPurcharsePriceCount);
+
+        assertThat(winning.getWinningTickets().size()).isEqualTo(4);
+    }
+
+    @Test
+    void 당첨_결과_리스트_담기(){
+        Integer userPurcharsePriceCount = 3;
+        Winning winning = new Winning(userPurcharsePriceCount);
+        List<Integer> userNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Integer userBonusNumber = 7;
+
+        WinningResultsDto winningResultsDto = winning.compareNumbers(userNumbers, userBonusNumber);
+        List<WinningResultDto> results = winningResultsDto.getResults();
+
+        assertThat(results.size()).isEqualTo(3);
+    }
 
 }
