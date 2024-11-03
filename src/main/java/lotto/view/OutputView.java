fbb9286b59e1356999e5be61ec184b5ffe5058bd
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-03 13:34:54 +0900

    feat: 결과 출력을 담당하는 OutputView 클래스 생성
    - 구매한 로또 리스트를 출력하는 displayPurchasedLottos 메서드 추가
    - 당첨 통계를 출력하는 displayResults 메서드 추가
      - 각 Rank에 해당하는 당첨 개수 및 상금을 출력
      - 보너스 볼 일치 여부에 따른 출력 처리
    - 총 수익률을 출력하는 displayProfitRate 메서드 추가
      - 수익률을 소수점 첫째 자리까지 포맷팅하여 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import lotto.model.Lotto;
+import lotto.model.Rank;
+
+import java.util.List;
+import java.util.Map;
+
+public class OutputView {
+
+    public static void displayPurchasedLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void displayResults(Map<Rank, Integer> rankResults) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            if (rankResults.containsKey(rank) && rank != Rank.NONE) {
+                System.out.println(rank.getMatchCount() + "개 일치" +
+                        (rank.isBonusMatch() ? ", 보너스 볼 일치" : "") +
+                        " (" + rank.getPrize() + "원) - " + rankResults.get(rank) + "개");
+            }
+        }
+    }
+
+    public static void displayProfitRate(double profitRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
+}
