2d27c19e2f06d3d838bf4ef582e51d80cc5e0ad0
donghan378 <ehdgksqorgh@naver.com>
2024-11-04 02:35:00 +0900

    feat(Lotto.java): add printNums method
    로또 번호를 출력하는 printNums 메소드 추가, Lotto 생성자에서 오름차순 정렬 추

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import java.util.List;
+import java.util.Collections;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +18,21 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void printNums() {
+        int i = 0;
+        for(Integer number : numbers) {
+            if(i == 0) {
+                System.out.print("[");
+                System.out.print(number);
+                i++;
+                continue;
+            }
+            System.out.print(", " + number);
+
+            if(i == 5) {
+                System.out.println("]");
+            }
+            i++;
+        }
+    }
 }
