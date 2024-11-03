6994b73587ecdb8184b94e1338df53b2d9f50bae
MinseokGo <rhalstjr1999@naver.com>
2024-11-03 18:43:08 +0900

    feat: 당첨 상태를 나타내는 객체 생성 기능

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -11,4 +11,16 @@ public class WinningLotto {
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
+
+    public List<WinningStatus> compareNumbersAndBonusNumber(final List<Lotto> lottos) {
+        return lottos.stream()
+                .map(this::createWinningStatus)
+                .toList();
+    }
+
+    private WinningStatus createWinningStatus(final Lotto lotto) {
+        final int matchedNumberCount = lotto.countMatchedNumber(numbers);
+        final boolean isBonusNumberMatched = lotto.isMatchedBonusNumber(bonusNumber);
+        return new WinningStatus(matchedNumberCount, isBonusNumberMatched);
+    }
 }
