9ed49226b1bc1145c3277c9b9bf556bef7ee5ebf
변우영 <likevangogh24@gmail.com>
2024-11-03 21:03:09 +0900

    test(InputValidator): 입력된 당첨 번호가 쉼표로 구분되지 않은 경우 예외 테스트 추가

--- src/test/java/lotto/validation/InputValidatorTest.java
+++ src/test/java/lotto/validation/InputValidatorTest.java
@@ -18,4 +18,13 @@ class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력값이 비어 있거나 공백으로만 구성되었습니다.");
     }
+
+    @DisplayName("입력된 당첨 번호가 쉼표로 구분되지 않은 경우, 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.2.3.4.5.6", "1;2;3;4;5;6", "1/2/3/4/5/6"})
+    void validateDelimitedByComma(String input) {
+        Assertions.assertThatThrownBy(() -> InputValidator.validateDelimitedByComma(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호 입력 시, 쉼표(,)로 구분해야 합니다.");
+    }
 }
