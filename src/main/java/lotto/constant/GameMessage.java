e10cc03aebe3cc98e1e15a04ab05f4324ba48173
chahyunsoo <chahs1012@naver.com>
2024-11-03 18:59:16 +0900

    feat(OutPutViewImpl,GameMessage) : 당첨통계 출력 및 수익률 출력 메소드 구현 및 출력 메시지 enum 정의

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -4,7 +4,10 @@ public enum GameMessage {
     PRINT_BUYING_PRICE_MESSAGE("구입금액을 입력해 주세요."),
     PRINT_WINNING_NUMBERS_MESSAGE("\n당첨 번호를 입력해 주세요."),
     PRINT_BONUS_NUMBERS_MESSAGE("\n보너스 번호를 입력해 주세요."),
-    BOUGHT_LOTTO_MESSAGE("\n%d개를 구매했습니다.\n");
+    PRINT_BOUGHT_LOTTO_MESSAGE("\n%d개를 구매했습니다.\n"),
+    PRINT_WINNING_STATISTIC_MESSAGE("\n당첨 통계\\n---"),
+    PRINT_HOW_MANY_WINNING_MESSAGE("%d개 일치 (%s원) - %d개\n"),
+    PRINT_PROFIT_RATE_MESSAGE("\n총 수익률은 %.1f%%입니다.");
 
     private final String message;
 
--- src/main/java/lotto/view/impl/OutViewImpl.java
+++ src/main/java/lotto/view/impl/OutViewImpl.java
@@ -1,15 +1,43 @@
 package lotto.view.impl;
 
-import static lotto.constant.GameMessage.BOUGHT_LOTTO_MESSAGE;
+import static lotto.constant.GameMessage.PRINT_BOUGHT_LOTTO_MESSAGE;
+import static lotto.constant.GameMessage.PRINT_HOW_MANY_WINNING_MESSAGE;
+import static lotto.constant.GameMessage.PRINT_PROFIT_RATE_MESSAGE;
+import static lotto.constant.GameMessage.PRINT_WINNING_STATISTIC_MESSAGE;
+import static lotto.constant.WinningRank.NONE;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import lotto.Lotto;
+import lotto.constant.WinningRank;
 import lotto.view.OutPutView;
 
 public class OutViewImpl implements OutPutView {
+    static NumberFormat numberFormat = NumberFormat.getInstance(Locale.KOREA);
+
     @Override
     public void printBoughtLotto(List<Lotto> boughtLotto) {
-        BOUGHT_LOTTO_MESSAGE.printGameMessage(boughtLotto.size());
+        PRINT_BOUGHT_LOTTO_MESSAGE.printGameMessage(boughtLotto.size());
         boughtLotto.forEach(lotto -> System.out.println(lotto.toString()));
+    }
+
+    @Override
+    public void printWinningStatistic(Map<WinningRank, Integer> results) {
+        PRINT_WINNING_STATISTIC_MESSAGE.printGameMessage();
+
+        for (WinningRank rank : WinningRank.values()) {
+            if (rank != NONE) {
+                String formattedPrize = numberFormat.format(rank.getPrize());
+                PRINT_HOW_MANY_WINNING_MESSAGE.printGameMessage(rank.getMatchCount(), formattedPrize,
+                        results.getOrDefault(rank, 0));
+            }
+        }
+    }
+
+    @Override
+    public void printProfitRate(double profitRate) {
+        PRINT_PROFIT_RATE_MESSAGE.printGameMessage(profitRate);
     }
 }
