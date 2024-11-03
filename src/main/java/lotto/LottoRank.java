5739017ba816bcaee87d555d04a73c96d760b686
LaiTial <asna90@naver.com>
2024-11-04 04:16:23 +0900

    enum class end

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,36 @@
+package lotto;
+
+public enum LottoRank {
+    FIRST(6, false, 2_000_000_000),    // 6개 일치
+    SECOND(5, true, 30_000_000),        // 5개 + 보너스 일치
+    THIRD(5, false, 1_500_000),         // 5개 일치
+    FOURTH(4, false, 50_000),           // 4개 일치
+    FIFTH(3, false, 5_000),             // 3개 일치
+    NONE(0, false, 0);                  // 꽝
+
+    private final int matchCount;        // 일치하는 번호 개수
+    private final boolean matchBonus;    // 보너스 번호 일치 여부
+    private final int prize;             // 당첨 상금
+
+    // 생성자
+    LottoRank(int matchCount, boolean matchBonus, int prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    // 당첨금 반환 메서드
+    public int getPrize() {
+        return prize;
+    }
+
+    // 맞는 당첨 등수 반환 메서드
+    public static LottoRank getRank(int matchCount, boolean matchBonus) {
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount && rank.matchBonus == matchBonus) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+}
