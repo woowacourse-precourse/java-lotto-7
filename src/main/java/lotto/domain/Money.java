4cc7153230622d5018d3f1a2e4359b2717730092
DaeyeonCho <daecho8@gmail.com>
2024-11-03 15:36:57 +0900

    refactor: 로또 구매 단위 상수로 전환

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class Money {
 
+    public static final int MIN_AMOUNT_TO_BUY_LOTTO = 1000;
     private final int money;
 
     public Money(int money) {
@@ -14,7 +15,7 @@ public class Money {
     }
 
     private void validate(int money) {
-        if ((money % 1000) * 1000 != money) {
+        if ((money % MIN_AMOUNT_TO_BUY_LOTTO) * MIN_AMOUNT_TO_BUY_LOTTO != money) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 구매 가능 합니다.");
         }
     }
