f4a1ceb7aa720939c3c39985988536b0b48db868
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-03 23:33:29 +0900

    feat : 구입 개수만큼 로또를 생성한다.

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -2,9 +2,14 @@ package lotto;
 
 import static lotto.LottoConstant.*;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoFactory {
 
-    public void createByCount(int count) {
-        Lotto lotto = new Lotto(RandomNumbersCreator.create(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT));
+    public List<Lotto> createByCount(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(index -> Lotto.create(RandomNumbersCreator.create(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT)))
+                .toList();
     }
 }
--- src/main/java/lotto/PurchaseLottoService.java
+++ src/main/java/lotto/PurchaseLottoService.java
@@ -7,6 +7,6 @@ public class PurchaseLottoService implements PurchaseLottoUseCase {
     @Override
     public void purchase(int money) {
         int purchaseCount = money / UNIT_OF_MONEY;
-
+        List<Lotto> purchasedLottos = lottoFactory.createByCount(purchaseCount);
     }
 }
