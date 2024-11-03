6f1969fee27242947edd801fa13738b912a86fee
smyeongL <sangmyeonglee123@gmail.com>
2024-11-03 22:15:48 +0900

    feat(view): add output view for displaying generated lottos

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import lotto.model.Lottos;
+import lotto.util.OutputMessage;
+
+public class OutputView {
+    public static void printLottos(Lottos lottos) {
+        int lottoSize = lottos.getLottosSize();
+        System.out.println(lottoSize + OutputMessage.SUCCESS_TICKET_PURCHASE);
+        for(int i = 0; i < lottoSize; i++) {
+            System.out.println(lottos.getLotto(i).getNumbers());
+        }
+    }
+}
