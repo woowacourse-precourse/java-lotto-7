2bdde6c675cccb76967ebfc74d1876672041bbac
koosco <rnxogud123@naver.com>
2024-11-04 02:27:58 +0900

    refactor: 가격 검증 로직 도메인으로 변경
    가격 검증 로직을 도메인쪽으로 이동했습니다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,16 +9,7 @@ public class InputView {
 
     public int getPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        int price = Integer.parseInt(Console.readLine());
-        if (price % 1000 != 0) {
-            throw new IllegalArgumentException("금액은 1,000원 단위로 입력해야 합니다");
-        }
-
-        if (price < 0) {
-            throw new IllegalArgumentException("음이 아닌 정수여야 합니다");
-        }
-        
-        return price;
+        return Integer.parseInt(Console.readLine());
     }
 
     public WinningNumberRequestDto getEntireNumber() {
