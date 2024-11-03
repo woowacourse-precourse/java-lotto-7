94540e4a82b5d726f72de01596eafb017177ad74
yush <yush2202@gmail.com>
2024-11-04 05:43:56 +0900

    refactor($ LottoComparator): 인스턴스 변수 자료형 수정

--- src/main/java/lotto/service/LottoComparator.java
+++ src/main/java/lotto/service/LottoComparator.java
@@ -1,25 +1,25 @@
 package lotto.service;
 
-import lotto.domain.Lotties;
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
 import lotto.domain.UserLotto;
 
 public class LottoComparator {
-    private final Lotties lotties;
+    private final List<Lotto> lotties;
     private final UserLotto userLotto;
 
-    private LottoComparator(Lotties lotties, UserLotto userLotto) {
+    private LottoComparator(List<Lotto> lotties, UserLotto userLotto) {
         this.lotties = lotties;
         this.userLotto = userLotto;
     }
 
-    public static LottoComparator from(Lotties lotties, UserLotto userLotto) {
+    public static LottoComparator from(List<Lotto> lotties, UserLotto userLotto) {
         return new LottoComparator(lotties, userLotto);
     }
 
     public void compare(LottoResult lottoResult) {
-        for (Lotto lotto : lotties.getLotties()) {
+        for (Lotto lotto : lotties) {
             int count = lotto.countDuplication(userLotto.getUserLotto());
             boolean isBonus = lotto.hasNumber(userLotto.getBonusBall());
 
