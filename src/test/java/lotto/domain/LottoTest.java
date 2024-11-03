bce9892a86876142e27d7612549f16dd064153ed
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-03 15:01:37 +0900

    chore(LottoTest): 미사용 importation 제거

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,7 +6,6 @@ import lotto.constants.value.LottoRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
