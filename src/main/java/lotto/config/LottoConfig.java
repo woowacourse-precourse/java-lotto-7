7b076aa425f505ade6738e73294a9f183dacb65d
Yunsu4 <ysa020428@gmail.com>
2024-11-03 22:21:41 +0900

    refactor(LottoConfig): 생성자에 직접 필드 주입하도록 수정

--- src/main/java/lotto/config/LottoConfig.java
+++ src/main/java/lotto/config/LottoConfig.java
@@ -12,6 +12,7 @@ public class LottoConfig {
 
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+
     private static final LottoNumberParser lottoNumberParser = new LottoNumberParser();
     private static final AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
     private static final LottoMatcher lottoMatcher = new LottoMatcher();
@@ -20,38 +21,14 @@ public class LottoConfig {
     private LottoConfig() {
     }
 
-    private static InputView createInputView() {
-        return inputView;
-    }
-
-    private static OutputView createOutputView() {
-        return outputView;
-    }
-
-    private static LottoNumberParser createlottoNumberParser() {
-        return lottoNumberParser;
-    }
-
-    private static AutoLottoGenerator createautoLottoGenerator() {
-        return autoLottoGenerator;
-    }
-
-    private static LottoMatcher createlottoMatcher() {
-        return lottoMatcher;
-    }
-
-    private static YieldCalculator createyieldCalculator() {
-        return yieldCalculator;
-    }
-
     public static LottoController lottoController() {
         return new LottoController(
-                createInputView(),
-                createOutputView(),
-                createlottoNumberParser(),
-                createautoLottoGenerator(),
-                createlottoMatcher(),
-                createyieldCalculator()
+                inputView,
+                outputView,
+                lottoNumberParser,
+                autoLottoGenerator,
+                lottoMatcher,
+                yieldCalculator
         );
     }
 }
