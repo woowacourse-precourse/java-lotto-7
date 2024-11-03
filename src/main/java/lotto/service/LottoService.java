b7c02e55028a43f82c052a73199123e75e6a38d6
josephuk77 <josephuk77@gmail.com>
2024-11-04 08:34:05 +0900

    feat(sortLotto): 로또 번호 오름차순 정렬 메서드 구현
    로또를 생성할 때 List<Integer>배열을 받아 자바에서 제공하는 메서드를 사용해 정렬하는 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.Lotto;
 
@@ -13,9 +14,17 @@ public class LottoService {
 
         for (int i = 0; i < amount / 1000; i++) {
             number = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            number = sortLotto(number);
             Lotto lotto = new Lotto(number);
             lottos.add(lotto);
         }
         return lottos;
+    }
+
+
+    public List<Integer> sortLotto(List<Integer> numbers) {
+        Collections.sort(numbers);
+
+        return numbers;
     }
 }
