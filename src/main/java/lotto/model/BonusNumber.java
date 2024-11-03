af8c91e8862fac4d107885fba79a7bea80d5279c
Parkjyun <jaeyun0507@naver.com>
2024-11-03 21:07:28 +0900

    fix(BonusNumber): LottoNumber 멤버 변수로 갖도록 변경

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -4,40 +4,13 @@ import java.util.regex.Pattern;
 import lotto.model.exception.DomainExceptionMessage;
 
 public class BonusNumber {
-    private static final Pattern numberPattern = Pattern.compile("^[0-9]+$");
-    private static final int LOWER_LIMIT = 1;
-    private static final int UPPER_LIMIT = 45;
-
-    private final int number;
+    private final LottoNumber number;
 
     public BonusNumber(final String number) {
-        validate(number);
-        this.number = Integer.parseInt(number);
+        this.number = new LottoNumber(number);
     }
 
     public boolean match(int number) {
-        return this.number == number;
-    }
-
-    private void validate(final String number) {
-        validateNumberPattern(number);
-        validateValue(number);
-    }
-
-    private void validateNumberPattern(final String number) {
-        if (!numberPattern.matcher(number).matches()) {
-            throw new IllegalArgumentException(
-                    DomainExceptionMessage.INVALID_BONUS_NUMBER_FORMAT.getMessage()
-            );
-        }
-    }
-
-    private void validateValue(final String number) {
-        int parsedNumber = Integer.parseInt(number);
-        if (parsedNumber < LOWER_LIMIT || parsedNumber > UPPER_LIMIT) {
-            throw new IllegalArgumentException(
-                    DomainExceptionMessage.INVALID_BONUS_NUMBER_VALUE.getMessage()
-            );
-        }
+        return this.number.equals(new LottoNumber(String.valueOf(number)));
     }
 }
