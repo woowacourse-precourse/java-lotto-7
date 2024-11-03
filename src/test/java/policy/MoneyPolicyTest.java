bc7ad8a3cfffbfc91919bae3ec5ad847d487c6ea
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-04 01:30:40 +0900

    test(MoneyPolicyTest): MoneyPolicy 테스트 코드 작성

---
+++ src/test/java/policy/MoneyPolicyTest.java
@@ -0,0 +1,65 @@
+package policy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MoneyPolicyTest {
+    MoneyPolicy moneyPolicy = new MoneyPolicyImpl();
+
+    @Test
+    public void 돈은_숫자만_입력_되어야_한다() {
+        // given
+        String money = "asdf";
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            moneyPolicy.checkMoneyPolicy(money);
+        });
+
+        // then
+        org.assertj.core.api.Assertions.assertThat(exception.getMessage()).contains("[ERROR]");
+    }
+
+    @Test
+    public void 아무것도_입력되지_않으면_예외를_발생시킨다() {
+        // given
+        String money = "";
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            moneyPolicy.checkMoneyPolicy(money);
+        });
+
+        // then
+        org.assertj.core.api.Assertions.assertThat(exception.getMessage()).contains("[ERROR]");
+    }
+
+    @Test
+    public void 돈이_1000으로_나누어_떨어지지않으면_예외를_발생시킨다() {
+        // given
+        String money = "11100";
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            moneyPolicy.checkMoneyPolicy(money);
+        });
+
+        // then
+        org.assertj.core.api.Assertions.assertThat(exception.getMessage()).contains("[ERROR]");
+    }
+
+    @Test
+    public void 돈이_int_범위를_넘어서면_예외를_발생시킨다() {
+        // given
+        String money = "10000000000000000000000000000000000000000000000000";
+
+        // when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            moneyPolicy.checkMoneyPolicy(money);
+        });
+
+        // then
+        org.assertj.core.api.Assertions.assertThat(exception.getMessage()).contains("[ERROR]");
+    }
+
+}
