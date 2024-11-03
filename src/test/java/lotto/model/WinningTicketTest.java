1b9fa8f7a26773359b972ee43bbd19a434ea24d1
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-03 17:47:05 +0900

    test(lotto/logic): 로또 등수 반환 기능 테스트 추가

--- src/test/java/lotto/model/WinningTicketTest.java
+++ src/test/java/lotto/model/WinningTicketTest.java
@@ -1,11 +1,23 @@
 package lotto.model;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningTicketTest {
+
+    @Test
+    @DisplayName("로또 등수 반환")
+    void getRank() {
+        assertSimpleTest(() -> {
+            WinningTicket winningTicket = WinningTicket.of("1,2,3,4,5,6", "7");
+            Rank rank = winningTicket.getRank(Lotto.from("1,3,5,14,22,45"));
+            assertThat(rank).isEqualTo(Rank.FIFTH);
+        });
+    }
 
     @Test
     @DisplayName("로또 번호의 개수가 6개가 아니라면 예외가 발생한다")
