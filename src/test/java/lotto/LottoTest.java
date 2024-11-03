341decf2f1033549b11a7f5e85cdeb70d56bc649
Yun-tellije <dbswjddms517@naver.com>
2024-11-04 03:08:35 +0900

    test: 구매 금액 단위 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.model.Lotto;
+import lotto.model.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,13 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    @DisplayName("구매 금액이 1000원 단위가 아니라면 예외가 발생한다.")
+    @Test
+    void 구매_금액이_1000원_단위가_아니라면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money(1234))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -28,4 +36,5 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
