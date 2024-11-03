8dbd04bcd7e4e2ea2c33662008be8b1180b4ec9f
kssk3 <kss8014@daum.net>
2024-11-03 18:49:23 +0900

    perf 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,6 +10,35 @@ public class OutputView {
     private static final String DIVIDER = "==========";
     private static final int PROFIT_BASIS = 1;
 
+    public static void printResult(Map<Rank, Integer>result, double yield) {
+        StringBuilder stringBuilder = new StringBuilder();
+        appendResultHeader(stringBuilder);
+        appendResult(stringBuilder, result);
+        System.out.print(stringBuilder);
+        printYield(yield);
+    }
+
+
+    private static void appendResultHeader(StringBuilder stringBuilder) {
+        stringBuilder.append(RESULT_MESSAGE)
+                .append(System.lineSeparator())
+                .append(DIVIDER)
+                .append(System.lineSeparator());
+    }
+
+    private static void appendResult(StringBuilder stringBuilder, Map<Rank, Integer>result) {
+        for (Rank rank : Rank.getRanks()) {
+            stringBuilder.append(rank.getCount())
+                    .append("개 일치")
+                    .append(getBonus(rank));
+            stringBuilder.append("(")
+                    .append(rank.getAmount()).append("원) - ")
+                    .append(getCount(result, rank))
+                    .append("개")
+                    .append(System.lineSeparator());
+        }
+    }
+
 
     private static String getBonus(Rank rank) {
         if (rank.isBonus()) {
