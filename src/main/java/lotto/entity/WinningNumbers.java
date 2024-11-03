c14dc7521490715c10dfecf33681dcfac34183cb
yeong-hwan <jghff700@naver.com>
2024-11-03 20:16:21 +0900

    refactor(entity): only validation logic at winning numbers domain

--- src/main/java/lotto/entity/WinningNumbers.java
+++ src/main/java/lotto/entity/WinningNumbers.java
@@ -1,110 +1,54 @@
 package lotto.entity;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 import lotto.enums.ErrorMessage;
 import lotto.enums.LottoConstants;
 
 import java.util.List;
-import lotto.enums.NotificationMessage;
 
 public class WinningNumbers {
     private final List<Integer> mainNumbers;
     private final int bonusNumber;
-    private static final String DELIMITER = ",";
 
-    public WinningNumbers() {
-        this.mainNumbers = inputMainNumbers();
-        this.bonusNumber = inputBonusNumber();
+    public WinningNumbers(List<Integer> mainNumbers, int bonusNumber) {
+        validateMainNumbers(mainNumbers);
+        validateBonusNumber(bonusNumber, mainNumbers);
+        this.mainNumbers = mainNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
-    private List<Integer> inputMainNumbers() {
-        while (true) {
-            try {
-                System.out.println(NotificationMessage.WINNING_NUMBERS.getMessage());
-
-                String[] inputs = Console.readLine().trim().split(DELIMITER);
-                Arrays.stream(inputs).forEach(this::validateInteger);
-
-                validateMainNumber(inputs);
-
-                List<Integer> mainNumbers = Arrays.stream(inputs)
-                        .map(input -> Integer.parseInt(input.trim()))
-                        .collect(Collectors.toList());
-                
-                System.out.println(NotificationMessage.DIVIDER.getMessage());
-                return mainNumbers;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static void validateMainNumbers(List<Integer> mainNumbers) {
+        validateMainNumbersCount(mainNumbers);
+        validateMainNumbersDuplication(mainNumbers);
+        mainNumbers.forEach(WinningNumbers::validateNumberInRange);
     }
 
-    private int inputBonusNumber() {
-        while (true) {
-            try {
-                System.out.println(NotificationMessage.BONUS_NUMBER.getMessage());
-
-                String bonusInput = Console.readLine().trim();
-                validateInteger(bonusInput);
-                int bonusNumber = Integer.parseInt(bonusInput);
-
-                validateBonusNumber(bonusNumber);
-
-                System.out.println(NotificationMessage.DIVIDER.getMessage());
-                return bonusNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static void validateBonusNumber(int bonusNumber, List<Integer> mainNumbers) {
+        validateNumberInRange(bonusNumber);
+        validateBonusNumberDuplication(bonusNumber, mainNumbers);
     }
 
-    private void validateMainNumber(String[] inputs) {
-        validateMainNumbersCount(inputs);
-
-        for (String input : inputs) {
-            int number = Integer.parseInt(input.trim());
-            validatePositiveInteger(number);
-            validateNumberInRange(number);
-        }
-    }
-
-    private void validateMainNumbersCount(String[] inputs) {
-        if (inputs.length != LottoConstants.LOTTO_NUMBERS_COUNT.getValue()) {
+    private static void validateMainNumbersCount(List<Integer> mainNumbers) {
+        if (mainNumbers.size() != LottoConstants.LOTTO_NUMBERS_COUNT.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.getMessage());
         }
     }
 
-    private void validateNumberInRange(int number) {
+    private static void validateMainNumbersDuplication(List<Integer> mainNumbers) {
+        // check if there are duplicated numbers
+        if (mainNumbers.size() != mainNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateNumberInRange(int number) {
         if (number < LottoConstants.MIN_LOTTO_NUMBER.getValue() || number > LottoConstants.MAX_LOTTO_NUMBER.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBERS_RANGE.getMessage());
         }
     }
 
-    private void validateBonusNumber(int bonusNumber) {
-        validatePositiveInteger(bonusNumber);
-        validateNumberInRange(bonusNumber);
-        validateBonusNumberDuplication(bonusNumber);
-    }
-
-    private void validateBonusNumberDuplication(int bonusNumber) {
+    private static void validateBonusNumberDuplication(int bonusNumber, List<Integer> mainNumbers) {
         if (mainNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
-        }
-    }
-
-    private void validateInteger(String input) {
-        try {
-            int number = Integer.parseInt(input);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INTEGER.getMessage());
-        }
-    }
-
-    private void validatePositiveInteger(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INTEGER.getMessage());
         }
     }
 
