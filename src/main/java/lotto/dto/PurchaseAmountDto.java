277512e54577be8f3b3e9481391b3133bb3a7278
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-11-03 18:12:59 +0900

    feat: dto에서 entity로 변환하는 메서드 구현

--- src/main/java/lotto/dto/PurchaseAmountDto.java
+++ src/main/java/lotto/dto/PurchaseAmountDto.java
@@ -1,10 +1,16 @@
 package lotto.dto;
 
+import lotto.domain.PurchaseAmount;
+
 public class PurchaseAmountDto {
 
     public int value;
 
     public PurchaseAmountDto(final int value) {
         this.value = value;
+    }
+
+    public PurchaseAmount toEntity() {
+        return new PurchaseAmount(value);
     }
 }
