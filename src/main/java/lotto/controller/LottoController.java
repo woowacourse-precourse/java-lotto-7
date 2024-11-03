03ac13925d4df4bb681f535aeecddbcd73160fb8
jae woo Jung <awfjol2008@gmail.com>
2024-11-03 21:38:14 +0900

    feat: 로또를 발행하고 발행 결과를 출력하는 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,21 +1,28 @@
 package lotto.controller;
 
 import lotto.domain.PurchaseAmount;
+import lotto.dto.LottoResponse;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoService lottoService;
 
-    public LottoController(InputView inputView, OutputView outputView) {
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoService = lottoService;
     }
 
     public void run() {
         PurchaseAmount purchaseAmount = readPurchaseAmount();
-
+        List<LottoResponse> lottoResponses = generateLottos(purchaseAmount);
+        outputView.printFormattedLottoNumbers(lottoResponses);
     }
 
     private PurchaseAmount readPurchaseAmount() {
@@ -23,5 +30,12 @@ public class LottoController {
         int purchaseAmountInput = inputView.readPurchaseAmountInput();
 
         return new PurchaseAmount(purchaseAmountInput);
+    }
+
+    private List<LottoResponse> generateLottos(PurchaseAmount purchaseAmount) {
+        int lottoCount = purchaseAmount.calculatePurchasableLottoCount();
+        outputView.printPurchasableLottoCount(lottoCount);
+
+        return lottoService.generateLottos(lottoCount);
     }
 }
