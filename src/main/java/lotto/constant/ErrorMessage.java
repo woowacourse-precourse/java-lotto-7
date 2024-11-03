7f1d0687f591153e9b4176735d2bdbc732b7ffaa
taek2222 <oot309@daum.net>
2024-11-04 07:38:07 +0900

    feat(Bonus): 보너스 번호 예외 처리 추가
    - 보너스 번호 부분에 예외 처리를 추가했습니다.
    
    - 변환되지 않을 경우, input 과정에서 콤마로 로직 우회로 인한 방지를 했습니다.
    
    - 이미 보너스 볼이 포함되어 있는지, 확인하는 로직도 추가했습니다.

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -3,10 +3,12 @@ package lotto.constant;
 public enum ErrorMessage {
     INVALID_LOTTO_LENGTH("로또 번호는 6개여야 합니다."),
     INVALID_NULL_OR_BLANK("입력값이 NULL 혹은 공백일 수 없습니다."),
-    INVALID_NON_NUMERIC_INPUT("숫자만 입력 가능합니다."),
+    INVALID_NON_NUMERIC_INPUT("양수인 숫자만 입력 가능합니다."),
     INVALID_ZERO_AMOUNT("구입금액은 0원일 수 없습니다."),
     INVALID_AMOUNT_UNIT("구입금액의 단위는 1,000원 단위입니다."),
     INVALID_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_BONUS_NUMBER_COUNT("보너스 번호는 1개의 숫자만 입력 가능합니다"),
+    BONUS_NUMBER_ALREADY_IN_WINNING_NUMBERS("해당 보너스 번호는 당첨 번호에 입력되어 있습니다"),
     DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,14 +1,33 @@
 package lotto.domain;
 
+import static lotto.constant.ErrorMessage.*;
+import static lotto.util.LottoValidator.*;
+
 public class Bonus {
 
     private final Integer number;
 
-    public Bonus(Integer number) {
-        this.number = number;
+    public Bonus(String number, Lotto winningLotto) {
+        int parsedNumber = parseNumber(number);
+        validateSingleNumberRange(parsedNumber);
+        this.number = parsedNumber;
+        validateNotContainsBonusBall(winningLotto);
     }
 
-    public boolean isContainsBonusBall(Lotto lotto) {
+    public boolean containsBonusBall(Lotto lotto) {
         return lotto.getNumbers().contains(number);
+    }
+
+    private int parseNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    private void validateNotContainsBonusBall(Lotto winningLotto) {
+        if (containsBonusBall(winningLotto))
+            throw new IllegalArgumentException(BONUS_NUMBER_ALREADY_IN_WINNING_NUMBERS.getMessage());
     }
 }
