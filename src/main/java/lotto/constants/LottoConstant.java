dabbbe8a1d4a7227f5e2c455a7341f11159eaf9c
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2024-11-03 17:30:57 +0900

    refactor: 로또 관련 상수화 실행

---
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -0,0 +1,12 @@
+package lotto.constants;
+
+public final class LottoConstant {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    public static final int PICK_COUNT = 6;
+    public static final int PROFIT_RATE = 100;
+    public static final int LOTTO_PER_PRICE = 1000;
+    public static final int NATURAL_STANDARD = 1;
+
+    public static final String REGEX = ",";
+}
