257ef3e346623a7ef1c5abc2ad8cca04c0003aca
woong2e <tlswodnd12345@naver.com>
2024-11-03 18:10:51 +0900

    feat: implement DrawController for checking lotto results and saving statistics

---
+++ src/main/java/lotto/draw/DrawController.java
@@ -0,0 +1,21 @@
+package lotto.draw;
+
+import lotto.donghang.WinningLotto;
+
+import java.util.Map;
+
+public class DrawController {
+
+    private final DrawService drawService;
+
+    public DrawController(DrawService drawService) {
+        this.drawService = drawService;
+    }
+
+    public void checkResult(WinningLotto winningLotto) {
+        Map<Rank, Integer> result = drawService.checkLotto(winningLotto);
+        drawService.saveStatistics(result);
+    }
+
+
+}
