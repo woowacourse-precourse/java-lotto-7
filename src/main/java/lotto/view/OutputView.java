49dfe0e8b7d6fd4dfafc1b57d4e312e09f729451
이준범 <jach1206@naver.com>
2024-11-03 23:19:05 +0900

    feat(view): 당첨 내역을 출력하는 기능 추가
    - printWinningInformation 메서드: priceAndCount 변수의 첫 번째 값은 낙첨된 경우에 대한 정보이기 때문에 건너 뛰고 출력하였음.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,17 +1,40 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.WinningInformation;
+import lotto.domain.WinningPrize;
 
 public class OutputView {
 
+    public static final String AFTER_BUYING_MESSAGE = "개를 구매했습니다.";
+    public static final String START_STATISTICS_MESSAGE = "당첨 통계";
+    public static final String DIVIDER = "---";
+    public static final String NEWLINE = "\n";
+
     public void printLottoNumber(List<Lotto> lottos, int purchaseAmount) {
-        System.out.println("\n" + purchaseAmount + "개를 구매했습니다.");
+        System.out.println(NEWLINE + purchaseAmount + AFTER_BUYING_MESSAGE);
 
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers().toString());
         }
 
         System.out.println();
+    }
+
+    // priceAndCount의 첫 번째 값은 낙첨된 경우에 대한 정보이기 때문에 건너뛰고 출력하였다.
+    public void printWinningInformation() {
+        Map<WinningPrize, Integer> priceAndCount = WinningInformation.getInstance().getPriceAndCount();
+        System.out.println(NEWLINE + START_STATISTICS_MESSAGE);
+        System.out.println(DIVIDER);
+
+        priceAndCount.entrySet().stream()
+                .skip(1)
+                .forEach(entry ->
+                        System.out.println(
+                                entry.getKey().getMatchCount() + " (" + entry.getKey().getFormattedPrize() + "원) - "
+                                        + entry.getValue() + "개")
+                );
     }
 }
