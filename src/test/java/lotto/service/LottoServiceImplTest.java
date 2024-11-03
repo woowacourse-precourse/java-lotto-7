0a1f84ac5a9dba13587faf63c9a609f6ca03fe2e
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-04 03:06:32 +0900

    test: LottoServiceImplt 테스트 - 당첨 통계 계산

--- src/test/java/lotto/service/LottoServiceImplTest.java
+++ src/test/java/lotto/service/LottoServiceImplTest.java
@@ -1,8 +1,12 @@
 package lotto.service;
 
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+import lotto.model.RankCount;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,5 +34,39 @@ class LottoServiceImplTest {
         assertTrue(numbers.stream().allMatch(num -> num >= expectedRangeStart && num <= expectedRangeEnd));
         assertEquals(expectedCount, numbers.size());
         assertEquals(expectedCount, numbers.stream().distinct().count());
+    }
+
+    @Test
+    void 당첨_통계를_올바르게_계산한다() {
+        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Integer bonusNumber = 7;
+
+        Lottos lottos = createLottos();
+
+        List<RankCount> rankCounts = lottoService.calculateWinningStatistics(lottos, winningNumbers, bonusNumber);
+
+        int expectedResult = 1;
+        assertEquals(expectedResult, rankCounts.get(4).getCount());
+        assertEquals(expectedResult, rankCounts.get(3).getCount());
+        assertEquals(expectedResult, rankCounts.get(2).getCount());
+        assertEquals(expectedResult, rankCounts.get(1).getCount());
+        assertEquals(expectedResult, rankCounts.get(0).getCount());
+    }
+
+    private Lottos createLottos() {
+        List<Lotto> createdLottos = Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 10, 20)),
+                new Lotto(Arrays.asList(1, 2, 3, 10, 20, 30)),
+                new Lotto(Arrays.asList(10, 11, 12, 13, 14, 15))
+        );
+
+        Lottos lottos = new Lottos();
+        for (Lotto lotto : createdLottos) {
+            lottos.addLotto(lotto);
+        }
+        return lottos;
     }
 }
