5a5076049ad0881fc141a8f74b71d6b551e40c26
sejinezy <psj8880@naver.com>
2024-11-04 00:11:45 +0900

    refactor(LottoResult): PurchaseAmount , LottoNumbers 클래스 추가로 인한 수정

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,43 +1,40 @@
 package lotto;
 
-import static lotto.Input.*;
-
 import java.util.List;
 
 public class LottoMachine {
     public static final int PRICE_OF_ONE_LOTTERY_TICKET = 1000;
 
-    public LottoTicket generateLottoTicket(int amount) {
-        int lottoCount = calculateLottoCount(amount);
+    public LottoTicket generateLottoTicket(PurchaseAmount purchaseAmount) {
+        int lottoCount = calculateLottoCount(purchaseAmount);
         List<Lotto> lottos = LottoGenerator.generateLottos(lottoCount);
         return new LottoTicket(lottos);
     }
 
-    private int calculateLottoCount(int amount) {
-        return amount / PRICE_OF_ONE_LOTTERY_TICKET;
+    private int calculateLottoCount(PurchaseAmount purchaseAmount) {
+        return purchaseAmount.getAmount() / PRICE_OF_ONE_LOTTERY_TICKET;
     }
 
     public void run() {
 
         try {
-            int purchaseAmount = validateDivisibleByThousand(
-                    toInt(validateIsNumber(validateNotEmpty(getPurchaseAmount()))));
+
+            Input input = new Input();
+            PurchaseAmount purchaseAmount = new PurchaseAmount(input.getPurchaseAmount());
+
 
             LottoTicket lottoTicket = generateLottoTicket(purchaseAmount);
             lottoTicket.showLottoTicket();
 
-            List<Integer> winningNumbers = toInt(validateIsNumber(split(getWinningNumber())));
-
-            int bonusNumber = toInt(validateIsNumber(validateNotEmpty(getBonusNumber())));
-
-            WinningNumbers winningNumberObject = new WinningNumbers(winningNumbers, bonusNumber);
+            LottoNumbers lottoNumbers = new LottoNumbers(input.getWinningNumbers(), input.getBonusNumber());
 
             LottoResult lottoResult = new LottoResult();
-            lottoResult.evaluateLottoResults(lottoTicket, winningNumberObject);
+            lottoResult.evaluateLottoResults(lottoTicket, lottoNumbers);
             lottoResult.displayWinningStatistics();
 
             double rateOfReturn = lottoResult.calculateRateOfReturn(purchaseAmount);
             lottoResult.displayRateOfReturn(rateOfReturn);
+
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
