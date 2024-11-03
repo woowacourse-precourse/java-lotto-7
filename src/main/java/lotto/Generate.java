516df991f45e7618fa1fab0901440e3679158f9f
HeungY <rkrkrkrk02@naver.com>
2024-11-03 22:06:45 +0900

    feat: create Generate Class for generate lotto

---
+++ src/main/java/lotto/Generate.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Generate {
+
+    public List<Lotto> lottos(int cost){
+        List<Lotto> lottos = new ArrayList<>();
+        System.out.println(cost + "개를 구매했습니다.");
+
+        for (int i = 0; i < cost; i++) {
+            List<Integer> randomNumbers = randomNumbers();
+            Collections.sort(randomNumbers);
+
+            lottos.add(new Lotto(randomNumbers));
+            System.out.println(randomNumbers);
+
+        }
+
+        return lottos;
+    }
+
+
+    private List<Integer> randomNumbers(){
+        return Randoms.pickUniqueNumbersInRange(1,45,6);
+    }
+}
