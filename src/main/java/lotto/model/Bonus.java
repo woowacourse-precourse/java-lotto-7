7e0ff5657dff5c8a8170394f82df47b8e845cb19
skhnpark <skhnpark125@gmail.com>
2024-11-03 14:23:59 +0900

    feat: 보너스 번호가 정상 범위인지 확인하는 기능 구현

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -4,6 +4,17 @@ public class Bonus {
     private final int number;
 
     public Bonus(int number) {
+        validate(number);
         this.number = number;
+    }
+
+    private void validate(int number) {
+        validateRange(number);
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
