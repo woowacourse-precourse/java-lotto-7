fe45ecf52973425182635e207e3737e7e3436ac0
장영재 <urinaner@naver.com>
2024-11-04 02:21:17 +0900

    feat(money): 돈 검증, 티켓 추출 메서드 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,5 +1,37 @@
 package lotto.domain;
 
 public class Money {
+    private static final int ZERO = 0;
+    private static final int THOUSAND = 1000;
+    private final int money;
 
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int inputValue) {
+        validateZero(inputValue);
+        validateDivideMoney(inputValue);
+    }
+
+    private void validateZero(int inputValue) {
+        if (inputValue == ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivideMoney(int inputValue) {
+        if (inputValue % THOUSAND != ZERO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getTicket() {
+        return money / 1000;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
