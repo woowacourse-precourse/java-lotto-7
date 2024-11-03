01502d2cb35847646c94db678d51bcf8e1a5e912
slay <palgalow@gmail.com>
2024-11-03 20:13:30 +0900

    refactor(LottoRankTest): 2등 매칭 테스트 기능 추가

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -7,11 +7,19 @@ import org.junit.jupiter.api.Test;
 
 public class LottoRankTest {
 
-    @DisplayName("일등 매칭")
+    @DisplayName("1등 매칭")
     @Test
     void matchingFirst() {
         LottoRank rank = LottoRank.valueOf(6, false);
 
         assertThat(rank).isEqualTo(LottoRank.FIRST);
+    }
+
+    @DisplayName("2등 매칭")
+    @Test
+    void matchingSecond() {
+        LottoRank rank = LottoRank.valueOf(5, true);
+
+        assertThat(rank).isEqualTo(LottoRank.SECOND);
     }
 }
