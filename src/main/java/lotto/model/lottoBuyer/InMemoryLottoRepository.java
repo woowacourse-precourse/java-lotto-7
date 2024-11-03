0d4e1361b3c51ac594df9c3bb682cf0231e46ae6
양효빈 <hyobin3026@naver.com>
2024-11-04 02:48:09 +0900

    feat(InMemoryLottoRepository): Add class to store lottos - Implements LottoRepository

---
+++ src/main/java/lotto/model/lottoBuyer/InMemoryLottoRepository.java
@@ -0,0 +1,26 @@
+package lotto.model.lottoBuyer;
+
+import lotto.model.lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryLottoRepository implements LottoRepository {
+
+    private final List<Lotto> issuedLottoRepository = new ArrayList<>();
+
+    @Override
+    public void saveLotto(Lotto lotto){
+        issuedLottoRepository.add(lotto);
+    }
+
+    @Override
+    public List<Lotto> findAllLotto(){
+        return issuedLottoRepository;
+    }
+
+}
+
+
+
+
