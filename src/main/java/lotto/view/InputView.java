7a9d8a573854261e1448ef347b63e52884c15975
injae-348 <ant0593@naver.com>
2024-11-03 16:49:07 +0900

    refactor(InputView): static import를 통해 상수 호출

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,27 @@
 package lotto.view;
 
+import static lotto.constant.LottoInputMessage.INPUT_BONUS_NUMBER_MESSAGE;
+import static lotto.constant.LottoInputMessage.INPUT_MONEY_MESSAGE;
+import static lotto.constant.LottoInputMessage.INPUT_WINNING_NUMBER_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
-import lotto.constant.LottoInputMessage;
 
 public class InputView {
 
     public String inputMoney() {
-        System.out.println(LottoInputMessage.INPUT_MONEY_MESSAGE);
+        System.out.println(INPUT_MONEY_MESSAGE);
         return Console.readLine();
     }
 
     public String inputWinnerNumbers() {
         System.out.println();
-        System.out.println(LottoInputMessage.INPUT_WINNING_NUMBER_MESSAGE);
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
         System.out.println();
-        System.out.println(LottoInputMessage.INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
