35a32d03fddc662de73eea025fd52c43ebe02202
SinnoLn <sintmtn69@gmail.com>
2024-11-03 16:24:12 +0900

    feat: 랜덤 로또 번호 생성 기능 추가
    - RandomNumberGeneration 클래스에서 1에서 45 사이의 중복되지 않는 6개의 번호를 생성

---
+++ src/main/java/lotto/model/strategy/RandomNumberGeneration.java
@@ -0,0 +1,22 @@
+package lotto.model.strategy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 랜덤 번호 생성 클래스
+ * NumberGenerationStrategy 인터페이스를 구현하여,
+ * 1에서 45 사이의 중복되지 않는 6개의 숫자를 생성.
+ */
+public class RandomNumberGeneration implements NumberGenerationStrategy{
+
+    /**
+     * 1에서 45 사이의 중복되지 않는 정수 6개를 랜덤하게 생성하여 반환
+     * @return 로또 번호 리스트
+     */
+    @Override
+    public List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
