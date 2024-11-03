960a13c2590fa177f45b269980b64cb14e45e912
JINSU LEE <bluesea9519@gmail.com>
2024-11-03 23:22:15 +0900

    feat: lotto 애플리케이션 진입점 및 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,46 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import lotto.domain.Lotto;
+import lotto.domain.LottoCalculator;
 import lotto.io.InputReader;
+import lotto.io.OutputWriter;
 import lotto.strategy.LotteryNumberGenerator;
 
 public class Application {
     public static void main(String[] args) {
 
-        InputReader inputReader = new InputReader();
+        OutputWriter.message("구입금액을 입력해 주세요.");
+        int purchaseAmount = InputReader.totalPurchaseAmount();
+        OutputWriter.addBlankLine();
 
-        System.out.println("구입금액을 입력해 주세요.");
-        int purchaseAmount = inputReader.totalPurchaseAmount();
-        System.out.println();
+        OutputWriter.message(purchaseAmount + "개를 구매했습니다.");
+        List<List<Integer>> issuedNumbers = LotteryNumberGenerator.generateNumbers(purchaseAmount);
+        OutputWriter.issuedNumbers(issuedNumbers);
+        OutputWriter.addBlankLine();
 
-        System.out.println(purchaseAmount + "개를 구매했습니다.");
-        List<List<Integer>> a = LotteryNumberGenerator.generateNumbers(purchaseAmount);
-        System.out.println(a);
+        OutputWriter.message("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = InputReader.winningNumbers();
+        OutputWriter.addBlankLine();
 
+        OutputWriter.message("보너스 번호를 입력해 주세요.");
+        int bonusNumber = InputReader.bonusNumber(winningNumbers);
+        OutputWriter.addBlankLine();
 
+        List<Lotto> tickets = issuedNumbers.stream()
+                .map(Lotto::new)
+                .collect(Collectors.toList());
+
+        OutputWriter.message("당첨 통계");
+        OutputWriter.message("---");
+
+        LottoCalculator calculator = new LottoCalculator();
+        calculator.calculateResults(tickets,winningNumbers,bonusNumber);
+
+        OutputWriter.lottoCalculateResults(calculator.results());
+        OutputWriter.profitRate(calculator.profitRate(purchaseAmount*1000));
 
     }
 }
