05ac170a639ef0702606f49768b3b9fc571672a9
Chanyoo Kim <cksdb012@gmail.com>
2024-11-03 17:20:17 +0900

    refactor: 오류 메시지 수정
    "[ERROR] 보너스 번호는 당첨 번호와 중복되어서는 안됩니다"로 오류 메시지를 수정

--- src/main/java/lotto/exception/BonusNumberDuplicationException.java
+++ src/main/java/lotto/exception/BonusNumberDuplicationException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class BonusNumberDuplicationException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되어서는 안된다";
+    private static final String ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되어서는 안됩니다";
 
     public BonusNumberDuplicationException() {
         super(ERROR_MESSAGE);
