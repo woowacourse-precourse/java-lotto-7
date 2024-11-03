021693c737811e40b2da866902ab65a8cbc79ed4
차왕준 <ckdhdkwns@gmail.com>
2024-11-03 16:34:39 +0900

    test(domain) 1등 당첨 테스트

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoResultTest {
+    @DisplayName("1등에 당첨된다.")
+    @Test
+    void 일등에_당첨된다() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, 7);
+
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));  // 1등
+
+        LottoResult lottoResult = new LottoResult(List.of(lotto), winningLotto);
+
+        assertThat(lottoResult.getCountByRank(Rank.FIRST)).isEqualTo(1);
+    }
+}
