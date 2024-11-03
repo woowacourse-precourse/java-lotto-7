e6f77caf10afa5b8cc0e786b0500e4c34d502b41
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 06:16:55 +0900

    test: 도메인 영역의 테스트 추가

--- src/test/java/lotto/BonusTest.java
+++ src/test/java/lotto/BonusTest.java
@@ -1,2 +1,16 @@
-package lotto;public class BonusTest {
+package lotto;
+
+import lotto.domain.Bonus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BonusTest {
+
+    @DisplayName("보너스 번호는 1과 45사이여야 한다.")
+    @Test
+    void validate() {
+        Assertions.assertThatThrownBy(() -> new Bonus(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
