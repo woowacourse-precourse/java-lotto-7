21b72ab0df3bf417b8155185f77f2604175d63d9
MinJae00 <exceedmin@gmail.com>
2024-11-03 17:41:30 +0900

    feat: 보너스 번호를 일급 컬렉션으로 관리, Lotto클래스에 메소드 호출하여 중복 예외 처리

--- src/main/java/lotto/domain/Bonus.java
+++ src/main/java/lotto/domain/Bonus.java
@@ -1,2 +1,33 @@
-package lotto.domain;public class Bonus {
+package lotto.domain;
+
+public class Bonus {
+    private final int bonusNumber;
+
+    public Bonus(int bonusNumber, Lotto lotto) {
+        this.bonusNumber = bonusNumber;
+        validate(lotto);
+    }
+
+    //숫자가 1 ~ 45 사이인지
+    //숫자가 당첨번호랑 겹치지 않는지
+    private void validate(Lotto lotto) {
+        rangeValidate();
+        hasDuplicated(lotto);
+    }
+
+    private void rangeValidate() {
+        if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+        }
+    }
+
+    private void hasDuplicated(Lotto lotto) {
+        if (lotto.hasDuplicatedBonusNumber(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복 될수 없습니다.");
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
