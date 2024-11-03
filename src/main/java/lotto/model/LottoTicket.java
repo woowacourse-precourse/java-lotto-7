d81c374b364291ec90732694319df76ec19d408c
Hyso <hsk7953@gmail.com>
2024-11-03 21:17:05 +0900

    feat : 로또 구매 모델 구현 - 구매 금액 유혀성 검사 - 금액에 따른 로또 수량 계산 로직 - 금액에 따른 로또 생성 로직

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,43 @@
+package lotto.model;
+
+import lotto.validator.AmountValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicket {
+
+    private final List<Lotto> lotteries;
+    private final int purchase;
+
+    private LottoTicket(List<Lotto> lotteries, int purchase) {
+        this.lotteries =lotteries;
+        this.purchase = purchase;
+    }
+
+    public static LottoTicket create(String input, LottoMachine lottoMachine, AmountValidator amountValidator){
+        int purchaseAmount = validateAmount(input,amountValidator);
+        int quantity = calculateQuantity(purchaseAmount);
+        List<Lotto> lotteries = generateLotteries(quantity,lottoMachine);
+        return new LottoTicket(lotteries, purchaseAmount);
+    }
+
+    private static int validateAmount(String input, AmountValidator amountValidator){
+        amountValidator.validate(input);
+        return Integer.parseInt(input);
+    }
+
+    private static int calculateQuantity(int amount){
+        return amount / 1000;
+    }
+
+    private static List<Lotto> generateLotteries(int quantity, LottoMachine lottoMachine){
+        List<Lotto> lotteries = new ArrayList<>();
+        for(int i = 0; i < quantity; i++){
+            lotteries.add(lottoMachine.generate());
+        }
+        return  lotteries;
+    }
+
+
+}
