d827314c6a0dde2c2a26ebc80a87c955e8c30274
dradnats1012 <herjebi1012@gmail.com>
2024-11-03 17:52:38 +0900

    feat: RandomNumberGenerator 클래스 구현

--- src/main/java/lotto/model/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/lotto/model/numbergenerator/RandomNumberGenerator.java
@@ -1,4 +1,13 @@
 package lotto.model.numbergenerator;
 
-public class RandomNumberGenerator {
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    @Override
+    public List<Integer> generateNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }
