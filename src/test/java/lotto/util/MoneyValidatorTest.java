039ee52907283febbe652f972c511432b3bead00
alswnsp411 <alswnsp411@naver.com>
2024-11-03 20:07:21 +0900

    test: 구입 금액이 1000원 단위의 숫자인지 검증하는 테스트

--- src/test/java/lotto/util/MoneyValidatorTest.java
+++ src/test/java/lotto/util/MoneyValidatorTest.java
@@ -1,36 +1,30 @@
-package lotto.model;
+package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PurchaseLottoTest {
+class MoneyValidatorTest {
 
     @Test
-    public void 올바른_형식의_구매_금액() throws NoSuchFieldException, IllegalAccessException {
-        PurchaseLotto purchaseLotto = new PurchaseLotto("10000");
-        Field moneyField = purchaseLotto.getClass().getDeclaredField("money");
-        moneyField.setAccessible(true);
-
-        assertEquals(10000, moneyField.get(purchaseLotto));
+    public void 올바른_형식의_구매_금액() {
+        assertEquals(10000, MoneyValidator.validate("10000"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-1000", "1000원", "만원", " ", ""})
     void 구매_금액을_양수로_입력하지_않으면_예외가_발생한다(String input) {
-        assertThatThrownBy(() -> new PurchaseLotto(input))
+        assertThatThrownBy(() -> MoneyValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"0", "1"})
     void 구매_금액을_천원단위로_입력하지_않으면_예외가_발생한다(String input) {
-        assertThatThrownBy(() -> new PurchaseLotto(input))
+        assertThatThrownBy(() -> MoneyValidator.validate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
