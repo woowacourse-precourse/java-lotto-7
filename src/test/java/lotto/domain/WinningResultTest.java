45533d90a4d8ebc0ee4c62063441fc17328a6e2d
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-03 20:02:36 +0900

    test(WinningResultTest): 당첨 결과 도메인 테스트
    테스트한 내용은 다음과 같습니다:
    - 당첨 결과를 집계 한다

---
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningResultTest {
+
+    @Test
+    @DisplayName("당첨 결과를 집계 한다.")
+    void getLottoRanksTest() {
+        //given
+        final Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        final BonusNumber bonusNumber = new BonusNumber(10);
+        final WinningNumbers winningNumbers = new WinningNumbers(winningLotto, bonusNumber);
+        final List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(1, 2, 3, 4, 5, 10)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+
+        //when
+        final WinningResult winningResult = new WinningResult(winningNumbers, lottos);
+        final Map<LottoRank, Integer> lottoRanks = winningResult.getLottoRanks();
+
+        //then
+        assertThat(lottoRanks).containsAllEntriesOf(Map.of(
+                LottoRank.RANK_1, 1,
+                LottoRank.RANK_2, 1,
+                LottoRank.RANK_3, 1,
+                LottoRank.RANK_4, 0,
+                LottoRank.RANK_5, 0
+        ));
+    }
+}
