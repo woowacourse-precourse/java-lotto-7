4fcc5070aa5dbd38ef9869df21d1146f29a57a67
hojooo <ghwn5833@gmail.com>
2024-11-04 02:54:02 +0900

    feat: Add validation for winning and bonus numbers within range 1-45
    - add a feature to the InputHandler that checks that the winning number and bonus number are between 1 and 45 and there are no duplicates

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Set;
+
 public class InputHandler {
     private static final int TICKET_PRICE = 1000;
 
@@ -13,5 +15,21 @@ public class InputHandler {
 
     public static int getTicketPrice() {
         return TICKET_PRICE;
+    }
+
+    public static void validateWinningNumbers(Set<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6 || winningNumbers.stream().anyMatch(InputHandler::isValidLottoNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다.");
+        }
+    }
+
+    public static void validateBonusNumber(int bonusNumber, Set<Integer> winningNumbers) {
+        if (isValidLottoNumber(bonusNumber) || winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 당첨 번호와 중복되지 않는 숫자여야 합니다.");
+        }
+    }
+
+    private static boolean isValidLottoNumber(int number) {
+        return number < 1 || number > 45;
     }
 }
--- src/test/java/lotto/InputHandlerTest.java
+++ src/test/java/lotto/InputHandlerTest.java
@@ -3,6 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,5 +23,33 @@ class InputHandlerTest {
         assertThatThrownBy(() -> InputHandler.getPurchaseAmount("1500"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호의 숫자 범위가 올바르지 않을 때 예외가 발생한다.")
+    @Test
+    void 당첨_번호의_숫자_범위가_올바르지_않을_때_예외가_발생() {
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 46);
+        assertThatThrownBy(() -> InputHandler.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다.");
+    }
+
+    @DisplayName("당첨 번호의 숫자가 중복되는 경우 예외가 발생한다.")
+    @Test
+    void 당첨_번호의_숫자가_중복될_때_예외가_발생() {
+        Set<Integer> winningNumbers = new HashSet<>(List.of(1, 2, 3, 4, 5, 5));
+        assertThatThrownBy(() -> InputHandler.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호는 1부터 45 사이의 중복되지 않는 숫자 6개여야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호 6개 중 중복이 있을 때 예외가 발생한다.")
+    @Test
+    void 보너스_번호와_당첨_번호가_중복될_때_예외가_발생() {
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 6;
+        assertThatThrownBy(() -> InputHandler.validateBonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 1부터 45 사이의 당첨 번호와 중복되지 않는 숫자여야 합니다.");
     }
 }
