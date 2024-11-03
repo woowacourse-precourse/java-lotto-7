aa352c8465180a04ae83784cb77a6215b00c8abe
(minjae8563) <(minjae8563@naver.com)>
2024-11-03 14:55:16 +0900

    refactor: NUMERIC_REGEX final 선언

--- src/main/java/lotto/common/Constant.java
+++ src/main/java/lotto/common/Constant.java
@@ -8,6 +8,6 @@ public class Constant {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final double TEN_DOUBLE = 10.0;
-    public static String NUMERIC_REGEX = "^[0-9]*$";
+    public static final String NUMERIC_REGEX = "^[0-9]*$";
 
 }
