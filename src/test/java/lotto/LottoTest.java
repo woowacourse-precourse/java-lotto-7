c253161f6b9c8aa078b251b5640ea1c734e93317
sinji <tobeabestns@naver.com>
2024-11-04 02:06:51 +0900

    refactor(LottoTest): 불필요 import 및 개행 삭제

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,8 +8,10 @@ import view.InputView;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
