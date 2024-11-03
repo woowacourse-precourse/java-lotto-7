7fdf9a4dbf289e4d3188e620b9d29602db5d2330
LaiTial <asna90@naver.com>
2024-11-04 00:28:26 +0900

    ãget console input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력 받기
+        System.out.println("구입금액을 입력해 주세요.");
+        String buy = Console.readLine();  // 구입 금액
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String lotto = Console.readLine(); // 당첨 번호
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonus = Console.readLine(); // 보너스 번호
     }
 }
