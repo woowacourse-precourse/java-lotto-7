1f5e35a4da53f3039d270f68ea1b8b8b915bad29
kim-su-mi <ksmm9607@naver.com>
2024-11-03 18:32:26 +0900

    Feat : 2. 로또 번호 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,26 @@
 package lotto;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-    	System.out.println("구입금액을 입력해 주세요.");
-        String input = Console.readLine();
-        validatePurchaseAmount(Integer.parseInt(input));
+    	try {
+    		System.out.println("구입금액을 입력해 주세요.");
+            String input = Console.readLine();
+            validatePurchaseAmount(Integer.parseInt(input));
+            
+            int amount = Integer.parseInt(input);
+            List<Lotto> lottos = LottoMachine.issue(amount);
+            
+            System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+            lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    	}catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    	
     }
 
     private static void validatePurchaseAmount(int amount) {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,7 +10,10 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        validateDuplication(numbers);
+        validateNumberRange(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +22,20 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
 }
