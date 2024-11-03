0a3a009d4c17d9b0e368cd8e9851ce4c588274d8
icebear0111 <wltjq0901@naver.com>
2024-11-03 15:52:21 +0900

    refactor: change String rank to description and add detail
    기존의 1등, 2등, ... 대신 등수별 설명을 추가하여 출력에 활용할 수 있도록 개선
    의미가 드러나도록 변수명을 rank에서 description으로 변경

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,23 +1,23 @@
 package lotto.model;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000, "1등"),
-    SECOND(5, true, 30_000_000, "2등"),
-    THIRD(5, false, 1_500_000, "3등"),
-    FOURTH(4, false, 50_000, "4등"),
-    FIFTH(3, false, 5_000, "5등"),
-    NONE(0, false, 0, "낙첨");
+    NONE(0, false, 0, "낙첨"),
+    FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)");
 
     private final int matchCount;
     private final boolean matchBonus;
     private final int prize;
-    private final String rank;
+    private final String description;
 
-    Rank(int matchCount, boolean matchBonus, int prize, String rank) {
+    Rank(int matchCount, boolean matchBonus, int prize, String description) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prize = prize;
-        this.rank = rank;
+        this.description = description;
     }
 
     public static Rank valueOf(int matchCount, boolean matchBonus) {
@@ -33,7 +33,7 @@ public enum Rank {
         return prize;
     }
 
-    public String getRank() {
-        return rank;
+    public String getDescription() {
+        return description;
     }
 }
