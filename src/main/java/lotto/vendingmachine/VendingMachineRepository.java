a4d653c45c4de73e0fee4f59d1776706eebe5cc7
woong2e <tlswodnd12345@naver.com>
2024-11-03 15:37:48 +0900

    feat: add functionality for statistics

--- src/main/java/lotto/vendingmachine/VendingMachineRepository.java
+++ src/main/java/lotto/vendingmachine/VendingMachineRepository.java
@@ -1,10 +1,17 @@
 package lotto.vendingmachine;
 
+import lotto.draw.Rank;
+
 import java.util.List;
+import java.util.Map;
 
 public interface VendingMachineRepository {
 
     void save(List<Lotto> lottos);
 
+    void saveWinningStatistics(Map<Rank, Integer> statistics);
+
     List<Lotto> getStoredLottoTickets();
+
+    Map<Rank, Integer> getWinningStatistics();
 }
