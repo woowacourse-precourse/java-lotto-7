d9d9052abb55a498b2a780cb342ccf4294d9ff66
Parkjyun <jaeyun0507@naver.com>
2024-11-03 18:53:11 +0900

    refactor(BonusNumber): 패키지 위치 변경

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -1,7 +1,6 @@
-package lotto;
+package lotto.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import lotto.model.BonusNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
