53ad3c50c4ea631ccc32a3c45fdbed5cad66cc1f
조민규 <ariettys2@naver.com>
2024-11-03 21:23:30 +0900

    feat : 당첨 번호와 비교하여 일치 개수 확인 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -10,6 +11,10 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);  // 불변성을 위해 새로운 리스트 반환
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
@@ -18,6 +23,17 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public int countMatchNumbers(Lotto winningLotto) {
+        int matchCount = 0;
+        List<Integer> winningNumbers = winningLotto.getNumbers();
+
+        for (int number : numbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
     public boolean checkBonus(int number) {
         for(int num : numbers){
             if(num == number){
