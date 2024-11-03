3261dca9e14581baa34e91f60f5ca0b0dd8fad98
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-04 02:42:44 +0900

    feat : 로또 구입 개수를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
-public class OutputView {
+public class OutputView implements UserOutput{
+    @Override
+    public void outputLottoCount(int lottoCount) {
+        System.out.println(String.format("%d개를 구매했습니다."));
+    }
+
+    @Override
+    public void outputStatistics() {
+
+    }
 }
--- src/main/java/lotto/view/UserOutput.java
+++ src/main/java/lotto/view/UserOutput.java
@@ -1,4 +1,7 @@
 package lotto.view;
 
 public interface UserOutput {
+    public void outputLottoCount(int lottoCount);
+
+    public void outputStatistics();
 }
