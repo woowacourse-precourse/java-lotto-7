4a5a029ae9eec112ab525fdcbfe48eba279ea59d
kaswhy <sxshx818@naver.com>
2024-11-03 18:01:06 +0900

    fix(lotto): 구입 금액 입력 시 예외 발생하면 다시 입력받도록 구현
    변경 전
    - 구입 금액을 잘못된 금액으로 입력 시 예외 발생하며 비정상 종료
    
    변경 후
    - 예외 발생 시 다시 입력 받을 수 있도록 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoSalesController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoSalesController lottoSalesController = new LottoSalesController(inputView, outputView);
+
+        lottoSalesController.run();
     }
 }
--- src/main/java/lotto/controller/LottoSalesController.java
+++ src/main/java/lotto/controller/LottoSalesController.java
@@ -16,14 +16,34 @@ public class LottoSalesController {
     }
 
     public void run() {
-        InputAmount inputAmount = prepareInputAmount();
+        InputAmount inputAmount = repeatGetAmountUntilValid();
+
+        // 구입 개수 구하기 & 출력
+        // 로또 자동 발행
+
+        // 당첨 번호 입력받기
+        // 보너스 번호 입력받기
+
 
         Console.close();
     }
 
-    private InputAmount prepareInputAmount() {
-        outputView.printInputAmount();
-        return new InputAmount(inputView.getAmount());
+    private InputAmount repeatGetAmountUntilValid() {
+        InputAmount inputAmount = null;
+        while(inputAmount == null) {
+            inputAmount = tryGetAmount();
+        }
+        return inputAmount;
+    }
+
+    private InputAmount tryGetAmount() {
+        try {
+            outputView.printInputAmount();
+            return new InputAmount(inputView.getAmount());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
 }
