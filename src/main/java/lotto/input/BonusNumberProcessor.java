831fa37b0645a7f372c7b838b45193782c2a17a8
Gahyeon <best11gh@naver.com>
2024-11-03 13:25:51 +0900

    feat(BonusNumberProcessor): 보너스 번호 처리 로직

--- src/main/java/lotto/input/BonusNumberProcessor.java
+++ src/main/java/lotto/input/BonusNumberProcessor.java
@@ -1,5 +1,8 @@
 package lotto.input;
 
+import static lotto.exception.ExceptionMessage.*;
+
+import java.util.List;
 import lotto.util.InputUtil;
 
 public class BonusNumberProcessor {
@@ -7,9 +10,27 @@ public class BonusNumberProcessor {
     private BonusNumberProcessor() {
     }
 
-    public static int validateAndParse(String input) {
+    public static int validateAndParse(List<Integer> winNumber,String input) {
         InputUtil.validateEmptyInput(input);
+        InputUtil.validatePositiveInteger(input);
 
-        return 7;
+        int bonusNumber = Integer.parseInt(input);
+
+        validateRange(bonusNumber);
+        validateDuplicate(winNumber, bonusNumber);
+
+        return bonusNumber;
+    }
+
+    private static void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> winNumber, int bonusNumber) {
+        if (winNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_WITH_WINNING_NUMBER.getMessage());
+        }
     }
 }
--- src/test/java/lotto/input/BonusNumberProcessorTest.java
+++ src/test/java/lotto/input/BonusNumberProcessorTest.java
@@ -5,11 +5,13 @@ import static lotto.input.BonusNumberProcessor.validateAndParse;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
 public class BonusNumberProcessorTest {
+    private final List<Integer> winNumbers = List.of(1, 2, 3, 4, 5, 6);
 
     @DisplayName("빈 문자열 또는 공백인 경우 - IllegalArgumentException 반환")
     @ParameterizedTest
@@ -17,7 +19,7 @@ public class BonusNumberProcessorTest {
     @ValueSource(strings = {"  ", "\t", "\n"})
     void testEmptyOrBlank(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> validateAndParse(input))
+                .isThrownBy(() -> validateAndParse(winNumbers, input))
                 .withMessage(EMPTY_INPUT.getMessage());
     }
 
@@ -26,34 +28,43 @@ public class BonusNumberProcessorTest {
     @ValueSource(strings = {"하나", "둘", "셋"})
     void testString(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> validateAndParse(input))
+                .isThrownBy(() -> validateAndParse(winNumbers, input))
                 .withMessage(NON_NUMERIC_INPUT.getMessage());
+    }
+
+    @DisplayName("음수, 0, 실수인 경우 - IllegalArgumentException 반환")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "1.1", "   -  2", "    0   ", "   5.  4"})
+    void testInvalidIntInput(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> validateAndParse(winNumbers, input))
+                .withMessage(ONLY_POSITIVE_INPUT.getMessage());
     }
 
     @DisplayName("숫자 범위가 1 ~ 45를 벗어난 경우 - IllegalArgumentException 반환")
     @ParameterizedTest
-    @ValueSource(strings = {"0", "-5", "47"})
+    @ValueSource(strings = {"46", "47", "100"})
     void testOutOfRage(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> validateAndParse(input))
+                .isThrownBy(() -> validateAndParse(winNumbers, input))
                 .withMessage(OUT_OF_RANGE_NUMBER.getMessage());
     }
 
     @DisplayName("당첨 번호의 숫자와 겹치는 경우 - IllegalArgumentException 반환")
     @ParameterizedTest
-    @ValueSource(strings = {"0", "-5", "47"})
+    @ValueSource(strings = {"1", "2", "6"})
     void testDuplicate(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> validateAndParse(input))
+                .isThrownBy(() -> validateAndParse(winNumbers, input))
                 .withMessage(DUPLICATE_WITH_WINNING_NUMBER.getMessage());
     }
 
     @DisplayName("유효한 입력 - 보너스 번호 반환")
     @ParameterizedTest
-    @CsvSource(value = {"1,1", "10,10", "45,45"})
-    void testDuplicate(String input, int expected) {
+    @CsvSource(value = {"7,7", "10,10", "45,45"})
+    void testValid(String input, int expected) {
         // given, then
-        int result = validateAndParse(input);
+        int result = validateAndParse(winNumbers, input);
 
         // then
         assertEquals(expected, result);
