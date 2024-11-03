0aa69434fe2e1ab20793c1d75c4870e10db74070
ouvir <kilhyunii@naver.com>
2024-11-03 18:00:02 +0900

    refactor(MoneyValidator): private 메서드명 변경 및 구조 변경
    코드 컨벤션 유지 및 가독성을 위한 메서드명 변경 및 구조 변경:
    - textValidate > validateText
    - priceValidate > validatePrice
    - validatePrice가 int 값을 받도록 구조 변경

--- src/main/java/lotto/validator/MoneyValidator.java
+++ src/main/java/lotto/validator/MoneyValidator.java
@@ -9,17 +9,33 @@ public class MoneyValidator implements Validator {
     private static final String NUMBER_PATTERN = "-?\\d+(\\.\\d+)?";
 
     @Override
-    public void validate(String text) throws IllegalArgumentException {
-        textValidate(text);
-        priceValidate(text);
+    public void validate(String text) throws MoneyException {
+        validateText(text);
+
+        try {
+            int price = Integer.parseInt(text);
+            validatePrice(price);
+        } catch (NumberFormatException e) {
+            throw new MoneyException(MONEY_RANGE_ERROR);
+        }
     }
 
-    private void textValidate(String text) throws MoneyException {
+    private void validateText(String text) throws MoneyException {
         if (isNullOrEmpty(text)) {
             throw new MoneyException(MONEY_EMPTY_ERROR);
         }
         if (!isNumber(text)) {
             throw new MoneyException(MONEY_NAN_ERROR);
+        }
+    }
+
+    private void validatePrice(int price) throws MoneyException {
+        if (!isValidRange(price)) {
+            throw new MoneyException(MONEY_RANGE_ERROR);
+        }
+
+        if (!isValidUnit(price)) {
+            throw new MoneyException(MONEY_UNIT_ERROR);
         }
     }
 
@@ -29,22 +45,6 @@ public class MoneyValidator implements Validator {
 
     private boolean isNumber(String text) {
         return text.matches(NUMBER_PATTERN);
-    }
-
-    private void priceValidate(String text) throws MoneyException {
-        try {
-            int price = Integer.parseInt(text);
-
-            if (!isValidRange(price)) {
-                throw new MoneyException(MONEY_RANGE_ERROR);
-            }
-
-            if (!isValidUnit(price)) {
-                throw new MoneyException(MONEY_UNIT_ERROR);
-            }
-        } catch (NumberFormatException e) {
-            throw new MoneyException(MONEY_RANGE_ERROR);
-        }
     }
 
     private boolean isValidRange(int price) {
