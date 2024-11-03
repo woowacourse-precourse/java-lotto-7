16a7f5dcee02755655549f546694b4a07a83e384
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-03 13:55:17 +0900

    feat: view클래스 추가
    - 입력관련 출력과 입력을 받는 Inputview 클래스 추가
    - 로또 관련 출력을  담당하는 LottoView 클래스 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,34 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.util.validator.InputValidator;
+
+public class InputView implements InputViewInterface{
+
+    @Override
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        InputValidator.validateInputValue(input);
+        return input;
+    }
+
+    @Override
+    public String readWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        InputValidator.validateInputLottoValue(input);
+        return input;
+
+    }
+
+    @Override
+    public String readBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        InputValidator.validateInputValue(input);
+        return input;
+
+    }
+
+}
--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,4 +1,24 @@
 package lotto.view;
 
-public class LottoView {
+import lotto.model.domain.Lotto;
+import lotto.model.domain.Lottos;
+
+public class LottoView implements LottoViewInterface{
+
+    @Override
+    public void printTotalLottoCount(int LottoCount) {
+        System.out.println("\n"+LottoCount +"개를 구매했습니다.");
+    }
+
+    @Override
+    public void printCreatedLotto(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    @Override
+    public String printWinningStatus() {
+        return "";
+    }
 }
