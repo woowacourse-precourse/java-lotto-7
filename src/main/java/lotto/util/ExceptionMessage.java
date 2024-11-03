0d9e44ad867c8fa0bff7fb61380469c09d6a79b0
yunjeooong <yunjeongssu@gmail.com>
2024-11-03 17:39:25 +0900

    feat(BudgetValidator): 입력값이 유효 범위가 아닌 경우 예외 처리

--- src/main/java/lotto/util/ExceptionMessage.java
+++ src/main/java/lotto/util/ExceptionMessage.java
@@ -3,6 +3,7 @@ package lotto.util;
 public enum ExceptionMessage {
 
     NOT_NUMERIC("숫자로 입력하세요."),
+    NOT_IN_RANNGE_OF_BUDGET("구매 금액은 1000원부터 10만원까지 입력 가능합니다."),
     INVALID_RANGE("1부터 45까지의 숫자만 입력 가능합니다.");
 
     public static final String BASE_MESSAGE = "[ERROR] %s";
@@ -11,6 +12,7 @@ public enum ExceptionMessage {
     ExceptionMessage(String message) {
         this.message = String.format(BASE_MESSAGE, message);
     }
+
     public String getMessage() {
         return message;
     }
--- src/main/java/lotto/util/validator/BudgetValidator.java
+++ src/main/java/lotto/util/validator/BudgetValidator.java
@@ -5,6 +5,8 @@ import lotto.util.ExceptionMessage;
 
 public class BudgetValidator implements Validator {
     private static final Pattern NUMBER_REGEX = Pattern.compile("^[0-9]*$");
+    private static final int MIN_BUDGET = 1_000;
+    private static final int MAX_BUDGET = 100_000;
 
 
     @Override
@@ -13,14 +15,24 @@ public class BudgetValidator implements Validator {
         validate(budget);
 
     }
+
     private static String removeSpace(String input) {
         return input.replaceAll(" ", "");
     }
+
     private void validateNumber(String budget) {
         if (NUMBER_REGEX.matcher(budget).matches()) {
             return;
         }
         throw new IllegalArgumentException(ExceptionMessage.NOT_NUMERIC.getMessage());
     }
+
+    private void validateRange(String budget) {
+        int budgetValue = Integer.parseInt(budget);
+        if (budgetValue < MIN_BUDGET || budgetValue > MAX_BUDGET) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_IN_RANNGE_OF_BUDGET.getMessage());
+        }
+    }
+
 
 }
