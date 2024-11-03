c5e2bd3d3329efe57323478f593b3a04c09b7482
taek2222 <oot309@daum.net>
2024-11-04 02:22:08 +0900

    style(InputView): Input 클래스 상수 분리
    - 출력과 줄내림을 상수화해 가독성을 향상시켰습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,13 @@
 package lotto.view;
 
+import static lotto.constant.MessageConstant.*;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     public String readPurchaseAmount() {
-        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+        System.out.println(INPUT_PURCHASE_AMOUNT.getMessage());
         String amount = Console.readLine();
 
         validateNotNullOrBlank(amount);
@@ -19,8 +17,8 @@ public class InputView {
     }
 
     public String readWinningNumber() {
-        System.out.println();
-        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        System.out.printf(NEWLINE.getMessage());
+        System.out.println(INPUT_WINNING_NUMBER.getMessage());
         String inputWinning = Console.readLine();
 
         validateNotNullOrBlank(inputWinning);
@@ -29,8 +27,8 @@ public class InputView {
     }
 
     public String readBonusNumber() {
-        System.out.println();
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.printf(NEWLINE.getMessage());
+        System.out.println(INPUT_BONUS_NUMBER.getMessage());
         String inputBonus = Console.readLine();
 
         validateNotNullOrBlank(inputBonus);
