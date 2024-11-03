7ed9be618be01a6d2f856798999f12ce8a2657ab
JINSU LEE <bluesea9519@gmail.com>
2024-11-03 23:19:13 +0900

    feat: 입력 로직

--- src/main/java/lotto/io/InputReader.java
+++ src/main/java/lotto/io/InputReader.java
@@ -1,27 +1,64 @@
 package lotto.io;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import lotto.validation.InputValidator;
 
 public class InputReader {
+    private static final int DIVISION_UNIT = 1000;
 
-    public int totalPurchaseAmount() {
-        int amount = stringToInt(readInput());
-        InputValidator.purchaseAmountValidate(amount);
-        return amount / 1000;
+    public static int totalPurchaseAmount() {
+        while (true) {
+            try {
+                int amount = stringToInt(readInput());
+                InputValidator.purchaseAmountValidate(amount);
+                return amount / DIVISION_UNIT;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public List<String> winningNumbers() {
-        String input = readInput();
-        return List.of(input.split(",")); // 여기까지 일단 진행 ,,,,
+    public static List<Integer> winningNumbers() {
+        while (true) {
+            try {
+                List<Integer> winningNumbers = Arrays.stream(readInput().split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                InputValidator.winningNumbersValidate(winningNumbers);
+                return winningNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private String readInput() {
+    public static int bonusNumber(List<Integer> winningNumbers) {
+        while (true) {
+            try {
+                int bonusNumber = stringToInt(readInput());
+                InputValidator.bonusNumberValidate(bonusNumber, winningNumbers);
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static String readInput() {
         return Console.readLine();
     }
 
-    private int stringToInt(String string) {
+    private static int stringToInt(String string) {
+        try {
+            InputValidator.integerValidate(string);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return Integer.parseInt(string);
     }
 }
