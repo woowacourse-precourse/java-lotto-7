e1fee55d0274b405b07ec471ddc12ce7b1deda1f
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-03 21:27:59 +0900

    feat: 로또 끼리 비교하는 메서드, 보너스번호 체크 메서드, toString 메서드 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,6 +3,8 @@ package lotto.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -17,5 +19,23 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    public int compare(Lotto winningLotto) {
+        List<Integer> match = this.numbers.stream()
+                .filter(o -> winningLotto.numbers.stream().anyMatch(Predicate.isEqual(o)))
+                .collect(Collectors.toList());
+
+        return (int)match.size();
+    }
+
+    public boolean bonuscheck(int bonusNumber) {
+        return this.numbers.stream()
+                .anyMatch(number -> number == bonusNumber);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }
