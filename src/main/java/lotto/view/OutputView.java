f25f0859bd49fe81bcbd8d94f919e3a422f5adb3
scc9811 <wjsrudtjr98@naver.com>
2024-11-03 23:01:36 +0900

    refactor : 상수 변수명 대문자 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,38 +7,38 @@ import java.util.Map;
 
 public class OutputView {
 
-    private static final String purchaseAmountMessage = "구입금액을 입력해 주세요.";
-    private static final String winningNumbersMessage = "당첨 번호를 입력해 주세요.";
-    private static final String bonusNumberMessage = "보너스 번호를 입력해 주세요.";
-    private static final String lottoCountMessage = "%d개를 구매했습니다.\n";
-    private static final String prizeStatisticsHeader = "당첨 통계\n---";
-    private static final String[] matchNumbers = {"3", "4", "5", "5.5", "6"};
-    private static final String[] matchPrefixMessage = {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String PRIZE_STATISTICS_HEADER = "당첨 통계\n---";
+    private static final String[] MATCH_NUMBERS = {"3", "4", "5", "5.5", "6"};
+    private static final String[] MATCH_PREFIX_MESSAGE = {
             "3개 일치 (5,000원)",
             "4개 일치 (50,000원)",
             "5개 일치 (1,500,000원)",
             "5개 일치, 보너스 볼 일치 (30,000,000원)",
             "6개 일치 (2,000,000,000원)"
     };
-    private static final String rateOfReturnMessage = "총 수익률은 %s%%입니다.\n";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %s%%입니다.\n";
 
     private OutputView() {
     }
 
     public static void printInputPurchaseAmountMessage() {
-        System.out.println(purchaseAmountMessage);
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
     }
 
     public static void printInputWinningNumbers() {
-        System.out.println(winningNumbersMessage);
+        System.out.println(WINNING_NUMBERS_MESSAGE);
     }
 
     public static void printInputBonusNumber() {
-        System.out.println(bonusNumberMessage);
+        System.out.println(BONUS_NUMBER_MESSAGE);
     }
 
     public static void printLottoCountMessage(int lottoCount) {
-        System.out.printf(lottoCountMessage, lottoCount);
+        System.out.printf(LOTTO_COUNT_MESSAGE, lottoCount);
     }
 
     public static void printLottoNumbers(List<Lotto> lottos) {
@@ -48,13 +48,13 @@ public class OutputView {
     }
 
     public static void printPrizeStatistics(Map<String, Integer> matchCounts) {
-        System.out.println(prizeStatisticsHeader);
-        for (int i = 0; i < matchNumbers.length; i++) {
-            System.out.printf("%s - %d개%n", matchPrefixMessage[i], matchCounts.get(matchNumbers[i]));
+        System.out.println(PRIZE_STATISTICS_HEADER);
+        for (int i = 0; i < MATCH_NUMBERS.length; i++) {
+            System.out.printf("%s - %d개%n", MATCH_PREFIX_MESSAGE[i], matchCounts.get(MATCH_NUMBERS[i]));
         }
     }
 
     public static void printRateOfReturn(String rateOfReturn) {
-        System.out.printf(rateOfReturnMessage, rateOfReturn);
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 }
