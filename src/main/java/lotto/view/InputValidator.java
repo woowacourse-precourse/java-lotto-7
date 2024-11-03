4abee9d8ce46e352d82b6d1b5da4b68f8ae1ed1e
koosco <rnxogud123@naver.com>
2024-11-04 04:01:22 +0900

    fix: 1000으로 나누는 검증 로직 수정
    잘못된 검증 로직을 수정

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -5,7 +5,7 @@ import lotto.exception.ErrorMessage;
 public class InputValidator {
 
     public void isDivisibleByThousand(long price) {
-        if (price != 1000) {
+        if (price % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessage.NUMBER_NOT_DIVIDE_THOUSAND.get());
         }
     }
