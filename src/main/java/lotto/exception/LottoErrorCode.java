f33e84ac1357c73ff23250cf4b1819364dfbe69f
injae-348 <ant0593@naver.com>
2024-11-03 20:47:51 +0900

    feat(Validator): 생성된 로또 번호에 대한 검증 추가

--- src/main/java/lotto/exception/LottoErrorCode.java
+++ src/main/java/lotto/exception/LottoErrorCode.java
@@ -8,6 +8,11 @@ public enum LottoErrorCode {
     LOTTO_PRICE_NOT_UNDER_1_000("[ERROR] 구입 금액은 1,000원 이상이어야 합니다."),
     LOTTO_PRICE_NOT_OVER_1_000_000("[ERROR] 구입 금액은 1,000,000원 이하이어야 합니다."),
 
+    GENERATED_LOTTO_NUMBERS_SIZE_NOT_6("[ERROR] 로또 번호는 6개여야 합니다."),
+    GENERATED_LOTTO_NUMBERS_DUPLICATED("[ERROR] 로또 번호는 중복되지 않아야 합니다."),
+    GENERATED_LOTTO_NUMBERS_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이여야 합니다."),
+    GENERATED_LOTTO_NUMBERS_NOT_SORTED("[ERROR] 로또 번호는 오름차순으로 정렬되어야 합니다."),
+    
     ;
 
     private final String message;
--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -1,5 +1,9 @@
 package lotto.validator;
 
+import static lotto.exception.LottoErrorCode.GENERATED_LOTTO_NUMBERS_DUPLICATED;
+import static lotto.exception.LottoErrorCode.GENERATED_LOTTO_NUMBERS_NOT_SORTED;
+import static lotto.exception.LottoErrorCode.GENERATED_LOTTO_NUMBERS_OUT_OF_RANGE;
+import static lotto.exception.LottoErrorCode.GENERATED_LOTTO_NUMBERS_SIZE_NOT_6;
 import static lotto.exception.LottoErrorCode.LOTTO_PRICE_NOT_BLANK;
 import static lotto.exception.LottoErrorCode.LOTTO_PRICE_NOT_IN_1_000;
 import static lotto.exception.LottoErrorCode.LOTTO_PRICE_NOT_OVER_1_000_000;
@@ -22,11 +26,12 @@ public class LottoValidator {
     }
 
     public void validateGeneratedLottoNumbers(List<Integer> lottoNumbers) {
-        if (lottoNumbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
-        }
+        isGeneratedNumbersSize6(lottoNumbers);
+        isGeneratedNumberDuplicated(lottoNumbers);
+        isGeneratedNumberOutOfRange(lottoNumbers);
+        isGeneratedNumberSorted(lottoNumbers);
     }
-
+    
     public void validateWinnerLottoNumbers(String winnerNumbers) {
         if (winnerNumbers.isBlank()) {
             throw new IllegalArgumentException("당첨 번호를 입력해 주세요.");
@@ -39,6 +44,30 @@ public class LottoValidator {
         }
         if (bonusNumber.length() != 1) {
             throw new IllegalArgumentException("보너스 번호는 1자리여야 합니다.");
+        }
+    }
+
+    private static void isGeneratedNumberSorted(List<Integer> lottoNumbers) {
+        if (lottoNumbers.stream().sorted().toList().equals(lottoNumbers)) {
+            throw new IllegalArgumentException(GENERATED_LOTTO_NUMBERS_NOT_SORTED.getMessage());
+        }
+    }
+
+    private static void isGeneratedNumberOutOfRange(List<Integer> lottoNumbers) {
+        if (lottoNumbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException(GENERATED_LOTTO_NUMBERS_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private static void isGeneratedNumberDuplicated(List<Integer> lottoNumbers) {
+        if (lottoNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException(GENERATED_LOTTO_NUMBERS_DUPLICATED.getMessage());
+        }
+    }
+
+    private static void isGeneratedNumbersSize6(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(GENERATED_LOTTO_NUMBERS_SIZE_NOT_6.getMessage());
         }
     }
 
