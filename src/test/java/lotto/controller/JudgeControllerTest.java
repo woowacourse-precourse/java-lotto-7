7a5b53a1cf45dd20fb4ab2c7e2c34c1f3a42b13e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-03 14:12:31 +0900

    refactor(JudgeControllerTest): 각 등수에 대한 로또 번호를 부여하고 검증하도록 변경

--- src/test/java/lotto/controller/JudgeControllerTest.java
+++ src/test/java/lotto/controller/JudgeControllerTest.java
@@ -4,31 +4,49 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.util.Grade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class JudgeControllerTest {
+    private JudgeController judgeController;
     private List<Lotto> lottos;
+    private Lotto winning;
+    private int bonusNumber;
 
     @BeforeEach
     void setUp() {
         lottos = List.of(
-                new Lotto(List.of(8, 21, 23, 41, 42, 43)),
-                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
-                new Lotto(List.of(7, 11, 16, 35, 36, 44)),
-                new Lotto(List.of(1, 8, 11, 31, 41, 42)),
-                new Lotto(List.of(13, 14, 16, 38, 42, 45)),
-                new Lotto(List.of(7, 11, 30, 40, 42, 43)),
-                new Lotto(List.of(2, 13, 22, 32, 38, 45)),
-                new Lotto(List.of(1, 3, 5, 14, 22, 45))
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 45)),
+                new Lotto(List.of(1, 2, 3, 4, 44, 45)),
+                new Lotto(List.of(1, 2, 3, 43, 44, 45)),
+                new Lotto(List.of(1, 2, 42, 43, 44, 45)),
+                new Lotto(List.of(1, 41, 42, 43, 44, 45)),
+                new Lotto(List.of(40, 41, 42, 43, 44, 45))
         );
+        winning = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        bonusNumber = 7;
+        judgeController = new JudgeController(lottos, winning, bonusNumber);
     }
 
-    @Test
-    void 당첨_번호와_로또_번호가_일치하는_개수를_구한다() {
-        Lotto lotto = lottos.getFirst();
-        Lotto winning = new Lotto(List.of(8, 21, 23, 41, 42, 43));
-        assertThat(new JudgeController(lottos, winning).countMatchingDigits(lotto))
-                .isEqualTo(6);
+    @ParameterizedTest
+    @CsvSource({
+            "0, 6",
+            "1, 5",
+            "2, 5",
+            "3, 4",
+            "4, 3",
+            "5, 2",
+            "6, 1",
+            "7, 0"
+    })
+    void 당첨_번호와_로또_번호가_일치하는_개수를_구한다(int lottoIndex, int expectedCount) {
+        Lotto lotto = lottos.get(lottoIndex);
+        assertThat(judgeController.countMatchingDigits(lotto))
+                .isEqualTo(expectedCount);
     }
 }
