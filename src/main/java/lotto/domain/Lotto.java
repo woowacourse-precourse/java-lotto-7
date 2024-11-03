1cbb260c0d44b2d631a51b73fadfda831710a603
injae-348 <ant0593@naver.com>
2024-11-03 23:32:49 +0900

    feat(Lotto): 정렬 되었는지 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.exception.LottoErrorCode.LOTTO_NUMBERS_DUPLICATED;
+import static lotto.exception.LottoErrorCode.LOTTO_NUMBERS_NOT_SORTED;
 import static lotto.exception.LottoErrorCode.LOTTO_NUMBERS_OUT_OF_RANGE;
 
 import java.util.List;
@@ -23,6 +24,18 @@ public class Lotto {
         if (numbers.stream().distinct().count() != 6) {
             throw new IllegalArgumentException(LOTTO_NUMBERS_DUPLICATED.getMessage());
         }
+        if (!isSorted(numbers)) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_NOT_SORTED.getMessage());
+        }
+    }
+
+    private boolean isSorted(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i + 1 < numbers.size() && numbers.get(i) > numbers.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public List<Integer> getNumbers() {
