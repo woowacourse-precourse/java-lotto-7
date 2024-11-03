ad76f06a75916d79654a7abd2f2254182c3e407b
kim jihyeon <jihyeon546@gmail.com>
2024-11-03 17:43:05 +0900

    feat: 로또 구입 금액 입력 기능 추가

---
+++ src/main/java/lotto/view/input/PromptDisplayer.java
@@ -0,0 +1,9 @@
+package lotto.view.input;
+
+public class PromptDisplayer {
+    private static final String PURCHASE_PRICE_PROMPT = "구입금액을 입력해 주세요.";
+
+    public void showPurchasePrompt() {
+        System.out.println(PURCHASE_PRICE_PROMPT);
+    }
+}
