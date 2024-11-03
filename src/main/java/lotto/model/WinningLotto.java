4e263c6e84911ae9623ea484b0544671a04d0362
icebear0111 <wltjq0901@naver.com>
2024-11-03 13:51:45 +0900

    refactor: Update WinningLotto to use Enum
    WinningLotto 클래스의 결과 계산 과정에 Enum 클래스를 적용하여 계산하도록 수정

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -14,32 +14,15 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
     }
 
-    public Map<String, Integer> calculateResult(List<Lotto> lottos) {
-        Map <String, Integer> lottoResult = new HashMap<>();
+    public Map<Rank, Integer> calculateResult(List<Lotto> lottos) {
+        Map <Rank, Integer> lottoResult = new HashMap<>();
 
         for (Lotto lotto : lottos) {
             int matchCount = countMatchingNumbers(lotto);
             boolean containsBonusNumber = lotto.getNumbers().contains(bonusNumber);
 
-            if (matchCount == 6) {
-                lottoResult.put("1등", lottoResult.getOrDefault("1등", 0) + 1);
-                continue;
-            }
-            if (matchCount == 5 && containsBonusNumber) {
-                lottoResult.put("2등", lottoResult.getOrDefault("2등", 0) + 1);
-                continue;
-            }
-            if (matchCount == 5) {
-                lottoResult.put("3등", lottoResult.getOrDefault("3등", 0) + 1);
-                continue;
-            }
-            if (matchCount == 4) {
-                lottoResult.put("4등", lottoResult.getOrDefault("4등", 0) + 1);
-                continue;
-            }
-            if (matchCount == 3) {
-                lottoResult.put("5등", lottoResult.getOrDefault("5등", 0) + 1);
-            }
+            Rank rank = Rank.valueOf(matchCount, containsBonusNumber);
+            lottoResult.put(rank, lottoResult.getOrDefault(rank.getRank(), 0) + 1);
         }
 
         return lottoResult;
