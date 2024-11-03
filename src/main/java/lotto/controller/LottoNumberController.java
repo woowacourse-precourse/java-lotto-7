94beb8767a8ebdb762efb201cfaa55ea71eeb31e
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-03 23:00:22 +0900

    refactor(controller, model): LottoBonus 모델의 검증 로직을 LottoNumberController로 이동
    - LottoBonus 생성 시 번호 검증을 LottoNumberController에서 수행하도록 변경
    - LottoBonus 모델의 책임을 단순화하고, 컨트롤러에서 검증 로직을 관리하여 코드 일관성 향상

--- src/main/java/lotto/controller/LottoNumberController.java
+++ src/main/java/lotto/controller/LottoNumberController.java
@@ -3,7 +3,9 @@ package lotto.controller;
 import java.util.List;
 
 import lotto.model.Lotto;
+import lotto.model.LottoBonus;
 import lotto.validator.InputValidator;
+import lotto.validator.LottoBonusValidator;
 import lotto.validator.LottoValidator;
 import lotto.view.InputView;
 
@@ -19,9 +21,26 @@ public class LottoNumberController {
 		}
 	}
 	
+	public LottoBonus generateBonusNumber(List<Integer> winningNumbers) {
+		while(true) {
+			String input = InputView.inputBonusNumber();
+			try {
+				return generateBonusNumber(input, winningNumbers);
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
 	public Lotto generateWinningLotto(String input) {
 		List<Integer> winningNumbers = InputValidator.validateWinningNumbers(input);
 		LottoValidator.validateLottoNumbers(winningNumbers);
 		return new Lotto(winningNumbers);
 	}
-}
+	
+	public LottoBonus generateBonusNumber(String input, List<Integer> winningNumbers) {
+		int bonusNumber = InputValidator.validateInteger(input);
+		LottoBonusValidator.validateBonusNumber(bonusNumber, winningNumbers);
+		return new LottoBonus(bonusNumber);
+	}
+} 
--- src/main/java/lotto/model/LottoBonus.java
+++ src/main/java/lotto/model/LottoBonus.java
@@ -1,18 +1,13 @@
 package lotto.model;
 
-import java.util.List;
-
-import lotto.validator.LottoBonusValidator;
-
 public class LottoBonus {
 	private final int bonusNumber;
 
-	public LottoBonus(int bonusNumber, List<Integer> lottoNumbers) {
-		LottoBonusValidator.validateBonusNumber(bonusNumber, lottoNumbers);
+	public LottoBonus(int bonusNumber) {
 		this.bonusNumber = bonusNumber;
 	}
-	
-	public int getBonusNumbers() {
+
+	public int getBonusNumber() {
 		return bonusNumber;
 	}
 }
