c647a573ec11a32a989aff9cb9484df5a39654d5
Avocado <ujubongbong@gmail.com>
2024-11-04 01:06:46 +0900

    :sparkles: feat: 지불금액/보너스번호 입력이 숫자인지 확인하는 함수

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -34,4 +34,12 @@ public class Input {
 			throw new IllegalArgumentException("[ERROR] 당첨번호 입력은 숫자만 가능합니다.");
 		}
 	}
+
+	public void validateInteger(String input) {
+		try {
+			Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다.");
+		}
+	}
 }
