645937db8838a9d3137f794c94b9a498946c4f9a
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-03 17:09:57 +0900

    test(LottoTest): Lotto.from을 사용하도록 테스트 수정, 올바른 생성 검증 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,26 +5,40 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호가 1과 45 사이의 값이 아니면 예외가 발생한다.")
     @Test
     void 로또_번호가_1과_45_사이의_값이_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.from(List.of(0, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호가_조건을_만족할_때_올바르게_생성된다() {
+        // given
+        List<Integer> numbers = List.of(1,2,3,4,5,6);
+
+        //when
+        Lotto lotto = Lotto.from(numbers);
+
+        //Then
+        assertThat(lotto).isNotNull();
+        assertThat(lotto.numbers()).hasSize(6);
     }
 }
