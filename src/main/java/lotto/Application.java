b08f041bcca25a49ffebf3ff1f9901e17d2356fe
hyunjilee1132 <hyunji.lee1132@gmail.com>
2024-11-03 23:44:23 +0900

    feat: 구입금액 입력받는 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import static java.lang.Integer.parseInt;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static final int PER_LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchasePrice; // 로또 구입금액
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String purchasePriceStr = Console.readLine();
+                purchasePrice = parseInt(purchasePriceStr);
+                if (purchasePrice <= 0 || purchasePrice % PER_LOTTO_PRICE != 0) {
+                    throw new IllegalArgumentException("[ERROR] 1,000원 단위의 올바른 금액을 입력하세요.");
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
