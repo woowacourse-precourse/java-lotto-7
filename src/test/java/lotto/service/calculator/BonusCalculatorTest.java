736d76b075f2ca1d88c5f4d6ac3f709656e0e197
world-dv <goodforx0147@naver.com>
2024-11-04 00:31:46 +0900

    test: 보너스 번호 일치 여부 검증 테스트 코드 추가
    - 로또 번호와 보너스 번호가 일치하면 1, 일치하지 않으면 0을 담은 배열을 반환하는지 검증하는 테스트 코드를 추가합니다.

---
+++ src/test/java/lotto/service/calculator/BonusCalculatorTest.java
@@ -0,0 +1,36 @@
+package lotto.service.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import lotto.domain.Bonus;
+import lotto.domain.Lotto;
+import lotto.factory.BonusFactory;
+import lotto.factory.LottoFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BonusCalculatorTest {
+
+    private List<Lotto> lottoTicket;
+
+    @BeforeEach
+    void before_each() {
+        lottoTicket = List.of(LottoFactory.create(List.of(1, 2, 3, 4, 5, 6)),
+                LottoFactory.create(List.of(7, 8, 9, 10, 11, 12)));
+    }
+
+    @DisplayName("로또와 보너스 번호를 비교해 일치 여부를 반환한다.")
+    @Test
+    void 로또와_보너스_번호를_비교해_일치_여부를_반환한다() {
+        Bonus bonus = BonusFactory.create(2);
+        List<Integer> correctResult = List.of(1, 0);
+
+        List<Integer> bonusResult = BonusCalculator.create(lottoTicket, bonus).getBonusResult();
+
+        assertThat(bonusResult).isEqualTo(correctResult);
+    }
+}
