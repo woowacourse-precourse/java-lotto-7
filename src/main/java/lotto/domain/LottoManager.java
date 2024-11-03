d50e8bf5dfafa7437ae0266eb34127093d5cdfc5
rbgusdlza <rbgusdlza@naver.com>
2024-11-04 03:10:39 +0900

    feat: 로또 결과를 관리하는 클래스 생성 및 메서드 구현

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -1,2 +1,30 @@
-package lotto.domain;public class LottoManager {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoManager {
+
+    private final Map<LottoResult, Integer> lottoResultMap = new HashMap<>();
+    private final List<Lotto> lottos;
+
+    public LottoManager(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public void getLottoResultFrom(Lotto winningLotto, Bonus bonus) {
+        lottos.forEach(lotto -> compareWith(winningLotto, bonus, lotto));
+    }
+
+    private void compareWith(Lotto winningLotto, Bonus bonus, Lotto lotto) {
+        int matchingCount = findMatchingCount(winningLotto, lotto);
+        boolean doesBonusMatch = lotto.hasBonus(bonus);
+        LottoResult lottoResult = LottoResult.getLottoResult(matchingCount, doesBonusMatch);
+        lottoResultMap.put(lottoResult, lottoResultMap.getOrDefault(lottoResult, 0) + 1);
+    }
+
+    private int findMatchingCount(Lotto winningLotto, Lotto lotto) {
+        return winningLotto.countSameNumbers(lotto);
+    }
 }
