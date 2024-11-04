e514343a6a23630b5f7a3eea34397da5491f18d6
Hyeo_Ni__ <maehyong02@gmail.com>
2024-11-04 17:54:22 +0900

    feat(lotto): Add validation for lotto numbers range (1-45)

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -6,7 +6,8 @@ public enum Error {
     INVALID_AMOUNT_UNIT("[ERROR] 천원 단위만 입력 가능합니다."),
     INVALID_LOTTO_NUMBERS("[ERROR] 로또 번호는 6개여야 합니다."),
     DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호에 중복된 숫자가 있습니다."),
-    BONUS_NUMBER_DUPLICATE("[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+    BONUS_NUMBER_DUPLICATE("[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다."),
+    INVALID_LOTTO_NUMBER_RANGE("[ERROR] 당첨 번호와 보너스 번호는 1~45이내여야 합니다.");
 
     private final String message;
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,8 +15,19 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(Error.INVALID_LOTTO_NUMBERS.getMessage());
         }
+
         if (new HashSet<>(numbers).size() != numbers.size()) {
             throw new IllegalArgumentException(Error.DUPLICATE_WINNING_NUMBER.getMessage());
+        }
+
+        for (Integer number : numbers) {
+            validateNumberRange(number); // 범위 유효성 검사 호출
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(Error.INVALID_LOTTO_NUMBER_RANGE.getMessage());
         }
     }
 
