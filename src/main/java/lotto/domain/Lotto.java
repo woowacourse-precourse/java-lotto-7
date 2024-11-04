2f71fc31d2599174199ff4cd79f9ca0872d60074
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 22:36:02 +0900

    refactor(lotto): 로또 번호 중복 유효성 검사 로직 추가
    - 로또 번호 입력 시 중복된 숫자가 포함될 경우 예외를 발생시키는 유효성 검사 로직을 추가했습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,18 @@
 package lotto.domain;
 
+import lotto.validation.Validation;
+import lotto.validation.ValidationType;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Validation.validateLottoDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +21,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+
     public List<Integer> lottoNumbers() {
         return numbers;
     }
