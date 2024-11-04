457a6609344bae7d56f4b8e7cf8b289ad6187723
Lee Chae Rin <chaerin0916@gmail.com>
2024-11-04 18:30:06 +0900

    fix: Lotto 클래스 빌드 에러 해결
    불변 리스트를 가변 리스트로 변환

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,13 +7,24 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
         ascendingSort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자를 뽑아야 합니다.");
+        }
+
+        // 1부터 45 사이의 숫자 검사
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
