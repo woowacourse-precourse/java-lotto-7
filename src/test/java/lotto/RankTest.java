9eb64a5a8dc24a0398ce530e00d5731e3a017ce7
chyo1 <hyowon3856@gmail.com>
2024-11-04 13:09:22 +0900

    test : 당첨 순위 구하는 기능 테스트 구현

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    @Test
+    public void 당첨_순위_구하기() throws Exception {
+    	//given
+    	int testMatchingCount = 3;
+        boolean testIsBonusMatched = false;
+
+    	//when
+        Rank actualRank = Rank.getRank(testMatchingCount, testIsBonusMatched);
+
+    	//then
+        assertEquals(Rank.THREE, actualRank);
+    }
+}
