e7752517c34da93257337b0ed010b4be98fd4d77
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 11:56:40 +0900

    refactor(ValidatorFactory): Validator 유형 객체 Enum 삭제 후 직접 해당 Validator 생성하도록 변경

--- src/main/java/lotto/factory/ValidatorFactory.java
+++ src/main/java/lotto/factory/ValidatorFactory.java
@@ -1,29 +1,20 @@
 package lotto.factory;
 
-import static lotto.factory.ValidatorType.BONUS_NUMBER;
-import static lotto.factory.ValidatorType.PRICE;
-import static lotto.factory.ValidatorType.WINNING_NUMBER;
-
 import lotto.validator.BonusNumberValidator;
-import lotto.validator.PriceValidator;
-import lotto.validator.Validator;
+import lotto.validator.PurchaseAmountValidator;
 import lotto.validator.WinningNumberValidator;
 
 public class ValidatorFactory {
 
-    public Validator create(ValidatorType type) {
-        if (type == PRICE) {
-            return new PriceValidator();
-        }
+    public PurchaseAmountValidator createPurchaseAmountValidator() {
+        return new PurchaseAmountValidator();
+    }
 
-        if (type == WINNING_NUMBER) {
-            return new WinningNumberValidator();
-        }
+    public WinningNumberValidator createWinningNumberValidator() {
+        return new WinningNumberValidator();
+    }
 
-        if (type == BONUS_NUMBER) {
-            return new BonusNumberValidator();
-        }
-
-        throw new IllegalStateException("[ERROR] 올바른 Validator 유형을 입력해주세요.");
+    public BonusNumberValidator createBonusNumberValidator() {
+        return new BonusNumberValidator();
     }
 }
