cdb47cec6f0543c4fdcc47198032772764bb9fab
min429 <lsm990429@gmail.com>
2024-11-04 15:26:46 +0900

    feat: 예외 테스트를 추가한다.

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -48,10 +48,8 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() -> {
-            runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
+        assertThatThrownBy(() -> runException("1000j"))
+            .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Override
