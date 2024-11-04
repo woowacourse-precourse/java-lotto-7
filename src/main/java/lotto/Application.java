b757799131cd7659b77105766b8ae5e90be5c296
Avocado <ujubongbong@gmail.com>
2024-11-04 19:50:36 +0900

    :sparkles: feat: 메인에 Lotto 구현 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,158 +1,69 @@
 package lotto;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-	// 지불금액 입력받는 함수
-	public static void getPayment() {
-		String input = Console.readLine();
-		validatePayment(input);
+	// 뽑는 횟수를 계산하는 함수
+	public static int getCount(int payment) {
+		return payment / 1000;
 	}
 
-	// 당첨번호 입력받는 함수
-	public static List<Integer> getLuckyNumber() {
-		String input = Console.readLine();
-		validateLuckyNumber(input);
-
-	    return Arrays.stream(input.split(","))
-			.map(Integer::parseInt)
-			.toList();
-	}
-
-	// 보너스번호 입력받는 함수
-	public static void getBonusNumber(List<Integer> numbers) {
-		String input = Console.readLine();
-		validateBonusNumber(numbers, input);
-	}
-
-	// 입력한 지불금액이 1000으로 나눴을때 0으로 떨어지는지 확인하는 함수
-	public static void validateRemainder(String input) {
-		if (Integer.parseInt(input) % 1000 != 0) {
-			throw new IllegalArgumentException("구입 금액은 1000원 단위로 입력해야 합니다.");
+	// 로또를 뽑는 함수
+	public static List<Lotto> getLotto(int count) {
+		List<Lotto> lottos = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
 		}
+		return lottos;
 	}
 
-	// 당첨번호가 1-45까지의 입력인지 확인하는 함수
-	public static void validateLuckyNumberRange(String input) {
-		for (String s : input.split(",")) {
-			int number = Integer.parseInt(s);
-			validateNumberRange(number);
+	// 로또 번호 중에 보너스 번호가 존재하는지 확인하는 함수
+	public static boolean isBonusMatch(List<Integer> lottos, int bonusNumber) {
+		for (Integer num : lottos) {
+			if (num == bonusNumber)
+				return true;
 		}
+		return false;
 	}
 
-	// 번호가 1-45까지의 입력인지 확인하는 함수
-	public static void validateNumberRange(int input) {
-		if (input <= 0 || input > 45) {
-			throw new IllegalArgumentException("번호는 1부터 45까지의 수만 입력 가능합니다.");
+	// 로또 번호와 당첨번호를 비교하는 함수
+	public static int compareNumbers(List<Integer> lottos, List<Integer> luckyNumber, int bonusNumber) {
+		return (int)lottos.stream()
+			.filter(luckyNumber::contains)
+			.filter(num -> lottos.contains(bonusNumber))
+			.count();
+	}
+
+	// 전체 수익을 계산하는 함수
+	public static long calculateTotalEarningRate(List<Lotto> lottos, List<Integer> luckyNumber, int bonusNumber) {
+		long total = 0;
+		for (Lotto lotto : lottos) {
+			List<Integer> numbers = lotto.getNumbers();
+			int matchCount = compareNumbers(numbers, luckyNumber, bonusNumber);
+			total += Prize.getPrize(matchCount, isBonusMatch(numbers, bonusNumber)).getReward();
 		}
+		return total;
 	}
 
-	// 당첨번호 입력이 6개인지 확인하는 함수
-	public static void validateNumberCount(String input) {
-		if (Arrays.stream(input.split(",")).count() != 6) {
-			throw new IllegalArgumentException("당첨번호는 6개 입력해야 합니다.");
-		}
-	}
-
-	// 당첨번호가 숫자인지 확인하는 함수
-	public static void validateLuckyNumberInteger(String input) {
-		if (Arrays.stream(input.split(","))
-			.anyMatch(s -> !Character.isDigit(s.charAt(0)))) {
-			throw new IllegalArgumentException("당첨번호 입력은 숫자만 가능합니다.");
-		}
-	}
-
-	// 입력이 숫자인지 확인하는 함수
-	public static void validateInteger(String input) {
-		try {
-			Integer.parseInt(input);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("입력은 숫자만 가능합니다.");
-		}
-	}
-
-	// 당첨번호 입력에 중복이 존재하는지 확인하는 함수
-	public static void validateDuplicate(String input) {
-		Set<String> numbers = new HashSet<>();
-		for (String s : input.split(",")) {
-			if (!numbers.add(s)) {
-				throw new IllegalArgumentException("당첨번호에 중복이 있을 수 없습니다.");
-			}
-		}
-	}
-
-	// 보너스번호가 당첨번호 중에 존재하는지 확인하는 함수
-	public static void validateDuplicateBonusNumber(List<Integer> numbers, String input) {
-		if (numbers.contains(Integer.parseInt(input))) {
-			throw new IllegalArgumentException("보너스번호와 당첨번호가 중복될 수 없습니다.");
-		}
-	}
-
-	// 입력이 빈 문자열인지 확인하는 함수
-	public static void validateEmpty(String input) {
-		if (input.isEmpty()) {
-			throw new IllegalArgumentException("입력이 비어있을 수 없습니다.");
-		}
-	}
-
-	// 당첨번호 입력에 공백이 존재하는지 확인하는 함수
-	public static void validateBlank(String input) {
-		if (input.contains(" ")) {
-			throw new IllegalArgumentException("입력에 공백이 포함되어 있을 수 없습니다.");
-		}
-	}
-
-	// 당첨번호 입력이 숫자로 끝나는지 확인하는 함수
-	public static void validateSeparatorSplit(String input) {
-		if (input.endsWith(",")) {
-			throw new IllegalArgumentException("입력은 숫자로 끝나야 합니다.");
-		}
-	}
-
-	// 당첨번호 앞뒤에 쉼표가 존재하는지 확인하는 함수
-	public static void validateSeparator(String input) {
-		for (String s : input.split(",")) {
-			if (s == null || s.isEmpty()) {
-				throw new IllegalArgumentException("입력은 숫자, 쉼표(,) 형식으로 작성해야 합니다.");
-			}
-		}
-	}
-
-	// 지불금액 입력 검증 함수
-	public static void validatePayment(String input) {
-		validateEmpty(input);
-		validateInteger(input);
-		validateRemainder(input);
-	}
-
-	// 당첨번호 입력 검증 함수
-	public static void validateLuckyNumber(String input) {
-		validateEmpty(input);
-		validateBlank(input);
-		validateSeparator(input);
-		validateSeparatorSplit(input);
-		validateDuplicate(input);
-		validateLuckyNumberInteger(input);
-		validateLuckyNumberRange(input);
-		validateNumberCount(input);
-	}
-
-	// 보너스번호 입력 검증 함수
-	public static void validateBonusNumber(List<Integer> numbers, String input) {
-		validateEmpty(input);
-		validateInteger(input);
-		validateNumberRange(Integer.parseInt(input));
-		validateDuplicateBonusNumber(numbers, input);
+	// 수익률 계산하는 함수
+	public static double calculateEarningRate(long total, int payment) {
+		return (double) total / payment * 100;
 	}
 
 	public static void main(String[] args) {
-		getPayment();
-		List<Integer> numbers = getLuckyNumber();
-		getBonusNumber(numbers);
+		Input input = new Input();
+
+		int payment = input.getPayment();
+		int count = getCount(payment);
+
+		List<Lotto> lottos = getLotto(count);
+		List<Integer> luckyNumbers = input.getLuckyNumber();
+		int bonusNumber = input.getBonusNumber(luckyNumbers);
+
+		long total = calculateTotalEarningRate(lottos, luckyNumbers, bonusNumber);
+		double earningRate = calculateEarningRate(total, payment);
 	}
 }
