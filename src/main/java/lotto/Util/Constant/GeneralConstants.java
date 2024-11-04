ba9b3b384f92c38e4b2d1e935f114ee8143dcc15
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 13:50:33 +0900

    feat(GeneralConstants): 로또 당첨 통계와 관련된 숫자를 상수화
    - 티켓의 가격, 등수별 당첨금액, 퍼센트화를 위한 정수를 상수화

--- src/main/java/lotto/Util/Constant/GeneralConstants.java
+++ src/main/java/lotto/Util/Constant/GeneralConstants.java
@@ -1,4 +1,21 @@
 package lotto.Util.Constant;
 
-public class GeneralConstants {
+public enum GeneralConstants {
+    TICKET_PRICE(1000),
+    PRIZE_3_MATCH(5000),
+    PRIZE_4_MATCH(50000),
+    PRIZE_5_MATCH(1500000),
+    PRIZE_5_MATCH_WITH_BONUS(30000000),
+    PRIZE_6_MATCH(2000000000),
+    PERCENT_CONVERSION(100);
+
+    private final int value;
+
+    GeneralConstants(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
