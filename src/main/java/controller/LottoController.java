7282f28270283e52abe6079caefb2eda0974c923
imjaehyeong <dlawogud517@gmail.com>
2024-11-04 12:04:08 +0900

    feat(view): 로또 관련 입출력 뷰  구현
    InputView: 구매금액, 당첨 번호, 보너스 번호 입력 기능 구현
    OutputView: 티켓 수, 로또 출력, 당첨결과, 수익률 출력 구현

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import service.LottoService;
+import view.InputView;
+import view.OutputView;
+
 public class LottoController {
+    private final LottoService lottoService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(LottoService lottoService, InputView inputView, OutputView outputView) {
+        this.lottoService = lottoService;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,35 @@
 package view;
 
+import model.Lotto;
+import model.LottoResult;
+import util.Message;
+
+import java.util.List;
+
 public class OutputView {
 
+    public void printTicketCount(int ticketCount) {
+        System.out.println(ticketCount + Message.TICKET_COUNT);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void printResults(List<LottoResult> results) {
+        System.out.println(Message.WIN_STATISTICS_HEADER);
+        System.out.printf(Message.MATCH_3 + "\n", countResults(results, LottoResult.FIFTH));
+        System.out.printf(Message.MATCH_4 + "\n", countResults(results, LottoResult.FOURTH));
+        System.out.printf(Message.MATCH_5 + "\n", countResults(results, LottoResult.THIRD));
+        System.out.printf(Message.MATCH_5_WITH_BONUS + "\n", countResults(results, LottoResult.SECOND));
+        System.out.printf(Message.MATCH_6 + "\n", countResults(results, LottoResult.FIRST));
+    }
+
+    public void printProfitRate(double rate) {
+        System.out.printf(Message.PROFIT_RATE, rate);
+    }
+
+    private long countResults(List<LottoResult> results, LottoResult resultType) {
+        return results.stream().filter(result -> result == resultType).count();
+    }
 }
