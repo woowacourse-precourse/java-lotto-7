fec0db3a95c749f8afb68fb9ac6e95f15911a772
junest66 <chlwndks333@gmail.com>
2024-11-04 16:57:11 +0900

    test(LottoTest): 로또 기능 테스트 추가
    - 로또 생성 정상 테스트 추가
    - 로또 숫자 범위 테스트 추가
    - 로또 오름차순 정렬 테스트 추가
    - 로또 번호 일치 갯수 검사 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,12 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
@@ -22,5 +22,44 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호가_정상적으로_생성된다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).containsOnly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 로또_번호에_음수가_포함되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호가_오름차순으로_정렬된다() {
+        Lotto lotto = new Lotto(List.of(6, 3, 1, 5, 2, 4));
+        assertThat(lotto.getSortedAscendingNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 로또_번호에_특정_번호가_포함되어_있는지_확인한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.hasNumber(3)).isTrue();
+        assertThat(lotto.hasNumber(7)).isFalse();
+    }
+
+    @Test
+    void 로또_번호와_당첨_번호의_일치_개수_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = new Lotto(List.of(3, 4, 5, 6, 7, 8));
+        assertThat(lotto.matchCount(winningLotto)).isEqualTo(4);
+    }
 }
