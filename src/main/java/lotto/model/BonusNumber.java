5e8ac094a6972c7379d9f4d63533ed8cd156e57b
jisukimtech <jisukim.tech@gmail.com>
2024-11-04 23:12:24 +0900

    feat(BonusNumber): 보너스 번호의 범위가 1 이상, 45 이하가 아닌 경우에 대한 예외 처리 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,10 +1,21 @@
 package lotto.model;
 
+import lotto.model.constant.ErrorMessage;
+
 public class BonusNumber {
     private final int number;
+    private static final int MIN_NUMBER_VALUE = 1;
+    private static final int MAX_NUMBER_VALUE = 45;
 
     public BonusNumber(int number) {
+        validate(number);
         this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_NUMBER_VALUE || number > MAX_NUMBER_VALUE) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE);
+        }
     }
 
     public int getNumber() {
