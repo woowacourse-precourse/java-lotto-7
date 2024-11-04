492e939f9228423c3171ee289ed7dcf402257b3d
SuyeonShinnnn <suyeons5536@gmail.com>
2024-11-04 14:26:09 +0900

    fix: number List 저장 방식 변경

--- src/main/java/lotto/LottoModel.java
+++ src/main/java/lotto/LottoModel.java
@@ -2,12 +2,15 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LottoModel {
     public Lotto getLottoNumber(){
-        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        lotto.sortNumbers();
+        List<Integer> numbers = new LinkedList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(numbers);
+        Lotto lotto = new Lotto(numbers);
 
         return lotto;
     }
