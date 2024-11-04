a14a23edf3949f3b68302711eb1b06dd6fb44e9d
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-04 21:25:17 +0900

    feat : 보너스 번호와 당첨 번호가 겹칠 경우 예외처리

--- src/main/java/lotto/common/ExceptionMessage.java
+++ src/main/java/lotto/common/ExceptionMessage.java
@@ -4,7 +4,7 @@ public final class ExceptionMessage {
 
     private static final String EXCEPTION_PREFIX = "[ERROR] ";
     public static final String OUT_OF_RANGE_NUMBER = EXCEPTION_PREFIX + "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-
+    public static final String NO_OVERLAP_NUMBER = EXCEPTION_PREFIX + "당첨 번호와 보너스 번호는 겹치면 안됩니다.";
     private ExceptionMessage() {
 
     }
--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -1,11 +1,16 @@
 package lotto.domain;
 
+import static lotto.common.ExceptionMessage.NO_OVERLAP_NUMBER;
+
+import java.util.List;
+
 public class WinLotto {
 
     private Lotto lotto;
     private int bonusNumber;
 
     private WinLotto(Lotto lotto, int bonusNumber) {
+        validateNoOverlap(lotto.getNumbers(), bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
@@ -20,5 +25,14 @@ public class WinLotto {
 
     public static WinLotto of(Lotto lotto, int bonusNumber) {
         return new WinLotto(lotto, bonusNumber);
+    }
+
+    private void validateNoOverlap(List<Integer> numbers, int bonusNumber) {
+        boolean isOverlap = numbers.stream()
+                .anyMatch(number -> number == bonusNumber);
+
+        if (isOverlap) {
+            throw new IllegalArgumentException(NO_OVERLAP_NUMBER);
+        }
     }
 }
