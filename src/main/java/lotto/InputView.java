804203dc8ed2a680396c1fd3346862fff039dda9
Hajuuu <hpt0127@naver.com>
2024-11-04 21:05:08 +0900

    fix: 예외 처리 오류 해결
    예외 발생 후 프로그램이 종료되는 것이 아닌 예외가 발생한 지점에서 다시 입력받도록 변경

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -9,16 +9,22 @@ public class InputView {
 
     private static final int AMOUNT_UNIT = 1000;
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_LOTTO_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
     private static final String ERROR_MESSAGE_AMOUNT = "[ERROR] 구입금액은 1000원 단위로 입력해 주세요.";
     private static final String ERROR_MESSAGE_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_BONUS_NUMBER = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자 중 하나를 입력해야 합니다.";
 
 
     public int inputPurchaseAmount() {
-        System.out.println(INPUT_AMOUNT_MESSAGE);
-        return checkAmount(readLine());
+        while (true) {
+            try {
+                System.out.println(INPUT_AMOUNT_MESSAGE);
+                return checkAmount(readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public int checkAmount(String input) {
@@ -35,10 +41,17 @@ public class InputView {
     }
 
     public Lotto inputWinningLottoNumbers() {
-        System.out.println(WINNING_LOTTO_NUMBERS_MESSAGE);
-        return new Lotto(
-                Arrays.stream(readLine().split(",")).map(num -> checkWinningLottoNumbers(num))
-                        .collect(Collectors.toList()));
+        while (true) {
+            try {
+                System.out.println(WINNING_LOTTO_NUMBERS_MESSAGE);
+                return new Lotto(
+                        Arrays.stream(readLine().split(",")).map(num -> checkWinningLottoNumbers(num.trim()))
+                                .collect(Collectors.toList()));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
     public int checkWinningLottoNumbers(String input) {
@@ -55,8 +68,14 @@ public class InputView {
     }
 
     public int inputBonus(Lotto winningLotto) {
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
-        return checkInputBonusNumber(readLine(), winningLotto);
+        while (true) {
+            try {
+                System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+                return checkInputBonusNumber(readLine(), winningLotto);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public int checkInputBonusNumber(String input, Lotto winningLotto) {
