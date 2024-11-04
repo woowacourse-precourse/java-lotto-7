bf24c37f5fe70f89364c23b5b175ce9b03618af3
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-04 15:02:10 +0900

    feat(model): Lotto 생성 대한 유효성 검사 추가
    - 로또 번호가 1~45 범위 밖에 있을 때 예외처리
    - 로또 번호가 중복되면 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,7 +2,9 @@ package lotto.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,9 +15,9 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        validateNumbersCount(numbers);
+        validateNumberRange(numbers);
+        validateNoDuplicateNumbers(numbers);
     }
 
     // TODO: 추가 기능 구현
@@ -40,5 +42,26 @@ public class Lotto {
     public String toString() {
         List<Integer> sortedNumbers = getSortedNumbers();
         return sortedNumbers.toString();
+    }
+
+    private void validateNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number > 45 || number < 0) {
+                throw new IllegalArgumentException("[Error] 로또 번호의 범위는 1~45 입니다.");
+            }
+        }
+    }
+
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 }
