0b7562c337ac9574fe1c157160ca8860fc1966db
DongHoon Lee <dhl1924@naver.com>
2024-11-04 15:55:24 +0900

    refactor(Lotto): 로또 번호 리스트를 Integer에서 Number VO로 변경
    - Lotto 클래스의 번호 리스트 타입을 `Integer`에서 `Number` VO로 변경하여 도메인 유효성 강화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,20 @@
 package lotto.domain;
 
+import lotto.domain.vo.Number;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Number> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Number> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Number> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
 }
