7ae7cb677d516302580d80bd9a263b710d5d0904
GrO0vy <lmc2819@naver.com>
2024-11-04 09:06:41 +0900

    test: 로또 결과 검증 기능 테스트 추가

---
+++ src/test/java/lotto/validator/LottoResultValidatorTest.java
@@ -0,0 +1,28 @@
+package lotto.validator;
+
+import lotto.entity.BonusNumber;
+import lotto.entity.WinningNumber;
+import lotto.validator.model.LottoResultValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoResultValidatorTest {
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다")
+    @ParameterizedTest
+    @MethodSource
+    void 당청_번호와_보너스_번호가_중복되면_예외가_발생한다(WinningNumber winningNumber, BonusNumber bonusNumber){
+        assertThatThrownBy(() -> new LottoResultValidator(winningNumber, bonusNumber).validate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> 당청_번호와_보너스_번호가_중복되면_예외가_발생한다(){
+        return Stream.of(Arguments.arguments(new WinningNumber("1,2,3,4,5,6"), new BonusNumber("1")));
+    }
+}
