85f6555ad948bf3b433fd03d330d2f720a821d43
gyuoo <yeomky98@naver.com>
2024-11-05 01:37:20 +0900

    feat(util): add validation to ensure bonus number does not overlap with winning numbers

--- src/main/java/lotto/util/DuplicateValidator.java
+++ src/main/java/lotto/util/DuplicateValidator.java
@@ -7,10 +7,17 @@ import java.util.Set;
 import lotto.exception.lottoticketexception.DuplicateException;
 
 public class DuplicateValidator {
-	public static <T> void validateNoDuplicates(List<T> numbers) {
+	public <T> List validateNoDuplicates(List<T> numbers) {
 		Set<T> uniqueNumbers = new HashSet<>(numbers);
 
 		if (uniqueNumbers.size() != numbers.size()) {
+			throw new DuplicateException();
+		}
+		return uniqueNumbers.stream().toList();
+	}
+
+	public <T> void validateNoOverlapWithWinningNumbers(int bonusNumber, List<T> winningNumbers) {
+		if (winningNumbers.contains(bonusNumber)) {
 			throw new DuplicateException();
 		}
 	}
