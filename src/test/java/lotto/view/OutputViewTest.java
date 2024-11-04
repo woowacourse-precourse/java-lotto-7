7453a17827b60e81ca1e5831a9e364d7244c9d18
ouvir <kilhyunii@naver.com>
2024-11-04 16:32:21 +0900

    test(OutputViewTest): 당첨 통계 출력 테스트 작성

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,12 +1,15 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.utils.LottoRules;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +27,7 @@ class OutputViewTest extends NsTest {
     @Test
     @DisplayName("구매한 로또 번호 출력")
     void displayLottoTickets() {
-        List<List<Integer>> lottoTickets = List.of(List.of(1,2,3,4,5,6));
+        List<List<Integer>> lottoTickets = List.of(List.of(1, 2, 3, 4, 5, 6));
         outputView.displayPurchasedLottoTickets(lottoTickets);
         assertTrue(output().contains("[1, 2, 3, 4, 5, 6]"));
     }
@@ -32,9 +35,30 @@ class OutputViewTest extends NsTest {
     @Test
     @DisplayName("구매한 로또 번호 낮은 번호부터 출력")
     void displayLottoTicketToSorted() {
-        List<List<Integer>> lottoTickets = List.of(List.of(1,3,2,6,5,4));
+        List<List<Integer>> lottoTickets = List.of(List.of(1, 3, 2, 6, 5, 4));
         outputView.displayPurchasedLottoTickets(lottoTickets);
         assertTrue(output().contains("[1, 2, 3, 4, 5, 6]"));
+    }
+
+    @Test
+    @DisplayName("당첨 통계 출력 테스트")
+    void displayWinningRankCount() {
+        Map<LottoRules.Winning, Integer> winningRankCount = new HashMap<>();
+        winningRankCount.put(LottoRules.Winning.WINNING_RANK_1, 1);
+        winningRankCount.put(LottoRules.Winning.WINNING_RANK_2, 2);
+        winningRankCount.put(LottoRules.Winning.WINNING_RANK_3, 3);
+        winningRankCount.put(LottoRules.Winning.WINNING_RANK_4, 4);
+        winningRankCount.put(LottoRules.Winning.WINNING_RANK_5, 5);
+        outputView.displayWinningRankCount(winningRankCount);
+        assertTrue(output().contains(
+                "당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - 5개\n" +
+                "4개 일치 (50,000원) - 4개\n" +
+                "5개 일치 (1,500,000원) - 3개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 2개\n" +
+                "6개 일치 (2,000,000,000원) - 1개"
+        ));
     }
 
     @Override
