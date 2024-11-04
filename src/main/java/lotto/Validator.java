57835acff1db559fdd309375e24083d35f589c66
JEN <ekffuclxk@gmail.com>
2024-11-04 12:56:54 +0900

    refactor(UserInput): Valitor 클래스로 유효성 검사 분리

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Validator {
+
+    public static void validatePurchaseAmount(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    public static void validatePositiveAmount(int amount) {
+        if (amount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
+        }
+    }
+
+    public static void isNumber(String input) {
+        if (input.matches("\\d+")) {
+            throw new NumberFormatException("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+        }
+    }
+
+    public static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
+        if (checkDuplicateNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 포함될 수 없습니다.");
+        }
+    }
+
+    public static void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static void validateNumberRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+}
--- src/main/java/lotto/view/UserInput.java
+++ src/main/java/lotto/view/UserInput.java
@@ -2,9 +2,9 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
+import lotto.Validator;
 
 public class UserInput {
     public int getPurchaseAmount() {
@@ -12,8 +12,8 @@ public class UserInput {
         String input = Console.readLine();
         int amount = Integer.parseInt(input);
 
-        validatePositiveAmount(amount);
-        validatePurchaseAmount(amount);
+        Validator.validatePositiveAmount(amount);
+        Validator.validatePurchaseAmount(amount);
 
         return amount;
     }
@@ -27,11 +27,11 @@ public class UserInput {
 
         for (String number : numbers) {
             int num = Integer.parseInt(number);
-            validateNumberRange(num);
+            Validator.validateNumberRange(num);
             winNumbers.add(num);
         }
-        validateLottoSize(winNumbers);
-        validateDuplicate(winNumbers);
+        Validator.validateLottoSize(winNumbers);
+        Validator.validateDuplicate(winNumbers);
 
         return winNumbers;
     }
@@ -40,48 +40,12 @@ public class UserInput {
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
 
-        validateNumberRange(bonusNumber);
+        Validator.validateNumberRange(bonusNumber);
 
         winNumbers.add(bonusNumber);
-        validateDuplicate(winNumbers);
+        Validator.validateDuplicate(winNumbers);
 
         return bonusNumber;
     }
 
-    private static void validatePurchaseAmount(int amount) {
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
-        }
-    }
-
-    private static void validatePositiveAmount(int amount) {
-        if (amount < 1000) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
-        }
-    }
-
-    private static void isNumber(String input) {
-        if (input.matches("\\d+")) {
-            throw new NumberFormatException("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
-        }
-    }
-
-    private static void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
-        if (checkDuplicateNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 포함될 수 없습니다.");
-        }
-    }
-
-    private static void validateLottoSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    private static void validateNumberRange(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
 }
