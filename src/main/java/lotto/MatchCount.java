a83e46836bc77fa352ae25742e92a3ea1046b74f
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 20:17:33 +0900

    refactor: enum 클래스에 당첨 금액을 추가

--- src/main/java/lotto/MatchCount.java
+++ src/main/java/lotto/MatchCount.java
@@ -1,10 +1,18 @@
 package lotto;
 
 public enum MatchCount {
-    NOT_MATCH,
-    THREE_MATCH,
-    FOUR_MATCH,
-    FIVE_MATCH,
-    FIVE_MATCH_WITH_BONUS_NUMBER,
-    SIX_MATCH
+    NOT_MATCH("0"),
+    THREE_MATCH("5000"),
+    FOUR_MATCH("50000"),
+    FIVE_MATCH("1500000"),
+    FIVE_MATCH_WITH_BONUS_NUMBER("30000000"),
+    SIX_MATCH("2000000000");
+    private final String winningMoney;
+
+    MatchCount(String money){
+        this.winningMoney = money;
+    }
+    String getMoney(){
+        return winningMoney;
+    }
 }
