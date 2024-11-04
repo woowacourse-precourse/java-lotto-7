d28c2d94b849db1acd59aa41233a8b39d9597240
na0th <105775683+na0th@users.noreply.github.com>
2024-11-04 15:16:54 +0900

    feat : LottoResult 도메인 클래스 추가
    추가한 내용 :
    1. LottoResult 도메인 클래스 추가
    -> Rank를 세고, ProfitRate를 건넬 DTO
    --> View에서 출력하기 쉽도록

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<LottoRank, Integer> rankCounts;
+    private final double totalProfitRate;
+    private final long totalPrize;
+
+    public LottoResult(Map<LottoRank, Integer> rankCounts, double totalProfitRate, long totalPrize) {
+        this.rankCounts = rankCounts;
+        this.totalProfitRate = totalProfitRate;
+        this.totalPrize = totalPrize;
+    }
+
+    public Map<LottoRank, Integer> getRankCounts() {
+        return rankCounts;
+    }
+
+    public double getTotalReturnRate() {
+        return totalProfitRate;
+    }
+
+    public long getTotalPrize() {
+        return totalPrize;
+    }
+}
