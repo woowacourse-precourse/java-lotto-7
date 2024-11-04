6f72fc94848c8d59fe6ff368790fdb66f310e298
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 20:38:19 +0900

    refact(LottoValidator): 하나의 메서드에는 하나의 기능만 하도록 수정

--- src/main/java/lotto/util/LottoValidator.java
+++ src/main/java/lotto/util/LottoValidator.java
@@ -14,6 +14,11 @@ public class LottoValidator extends Validator<List<Integer>> {
         validateDuplicateNumbers(numbers);
     }
 
+    public void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+        validateBonusNumberRange(bonusNumber);
+        validateDuplicateBonusNumbers(numbers, bonusNumber);
+    }
+
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_PREFIX + "로또 번호는 6개여야 합니다.");
@@ -36,10 +41,13 @@ public class LottoValidator extends Validator<List<Integer>> {
         }
     }
 
-    public void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+    private void validateBonusNumberRange(int bonusNumber) {
         if (isOutOfRange(bonusNumber)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    private void validateDuplicateBonusNumbers(List<Integer> numbers, int bonusNumber) {
         if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
         }
