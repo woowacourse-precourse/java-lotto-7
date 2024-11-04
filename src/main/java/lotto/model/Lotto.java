66904e43a5df4bf8c0ee22e4176b5b5b19db6f2f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 23:03:22 +0900

    refactor(Lotto): 유효성 검증 로직 분리
    - 번호 개수 및 중복 검사를 `LottoValidator`로 분리하여 책임 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,33 +1,18 @@
 package lotto.model;
 
-import java.math.BigInteger;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.exception.ErrorMessage;
-import lotto.exception.LottoException;
+import lotto.validator.LottoValidator;
 
 public class Lotto {
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator validator = new LottoValidator();
+        validator.numberCount(numbers);
+        validator.duplicate(numbers);
+
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw LottoException.from(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX);
-        }
-
-        Set<Integer> seen = new HashSet<>();
-        numbers.stream()
-                .filter(number -> !seen.add(number))
-                .findFirst()
-                .ifPresent(duplicate -> {
-                    throw LottoException.of(ErrorMessage.LOTTO_NUMBERS_MUST_NOT_DUPLICATE, duplicate);
-                });
     }
 
     public List<Integer> getNumbers() {
