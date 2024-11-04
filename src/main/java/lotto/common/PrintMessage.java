1adce033e0ad2a5ef303bbdc3d5b9b679696e8d5
minjae k <rkdalswoals@gmail.com>
2024-11-04 15:20:11 +0900

    refactor: update PrintMessage enum for message formatting

--- src/main/java/lotto/common/PrintMessage.java
+++ src/main/java/lotto/common/PrintMessage.java
@@ -4,11 +4,10 @@ public enum PrintMessage {
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    PURCHASED_TICKETS_MESSAGE("%d개를 구매했습니다.\n"),
-    TICKET_NUMBERS_MESSAGE("%s\n"),
-    RESULT_STATISTICS_HEADER("\n당첨 통계\n---\n"),
-    RANK_RESULT_MESSAGE("%d개 일치 (%s원) - %d개\n"),
-    PROFIT_RATE_MESSAGE("총 수익률은 %.1f%%입니다.\n");
+    PURCHASED_TICKETS_MESSAGE("%d개를 구매했습니다.%n"),
+    TICKET_NUMBERS_MESSAGE("%s%n"),
+    RESULT_STATISTICS_HEADER("%n당첨 통계%n---%n"),
+    PROFIT_RATE_MESSAGE("총 수익률은 %.1f%%입니다.%n");
 
     private final String message;
 
@@ -20,4 +19,3 @@ public enum PrintMessage {
         return message;
     }
 }
-
