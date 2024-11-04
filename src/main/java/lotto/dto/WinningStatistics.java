e0d9c46c2c6dc0f5a19649a7771239a8207afb0b
Parkjyun <jaeyun0507@naver.com>
2024-11-04 22:17:59 +0900

    refactor(WinningStatistics): 매직넘버 삭제

--- src/main/java/lotto/dto/WinningStatistics.java
+++ src/main/java/lotto/dto/WinningStatistics.java
@@ -4,7 +4,9 @@ import java.util.Map;
 import lotto.model.WinningType;
 
 public record WinningStatistics(Map<WinningType, Integer> statistics) {
+    private static final int DEFAULT_VALUE = 0;
+
     public int getWinningCount(WinningType type) {
-        return statistics.getOrDefault(type, 0);
+        return statistics.getOrDefault(type, DEFAULT_VALUE);
     }
 }
