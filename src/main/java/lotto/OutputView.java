5df9c5818a0eba418bf26517fd6d2800eb9e5798
youngseo3 <wys4475@gmail.com>
2024-11-04 22:04:23 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -5,7 +5,15 @@ public class OutputView {
     public void printLottoPurchaseDetails(Lottos lottos) {
         System.out.println();
         System.out.printf("%d개를 구매했습니다.\n", lottos.getCount());
-        System.out.printf("%s", lottos.toString());
+        System.out.printf("%s\n", lottos.toString());
+    }
+
+    public void printStatistics(LottoResult lottoResult, int purchaseAmount) {
+        System.out.println();
+        System.out.println("당첨 통계\n---");
+        System.out.println(lottoResult.getWinningStatistics());
+        System.out.println(String.format("총 수익률은 %.1f%%입니다.",
+                lottoResult.calculateProfitRate(purchaseAmount)));
     }
 
 }
