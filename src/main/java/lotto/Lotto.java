92002727984f72693c38af316558dfd4c98b1260
zerocat36 <Seo.james2020@gmail.com>
2024-11-04 21:06:19 +0900

    fix($Lotto.java): change function 'validate' name into 'isAllowedCount'
    - changed function name to enhance readability

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,13 +6,13 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        isAllowedCount(numbers);
         isDuplicate(numbers);
         isAllowedRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void isAllowedCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
