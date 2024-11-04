9baeb5fe6b42650536af8ddd06ce84c19dc6cfc8
badgelatte <saucy5939@gmail.com>
2024-11-04 22:10:41 +0900

    try-catch문으로 던진 예외를 잡도록 함
    던진 예외의 메시지를 보여주면서 끝냄

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,27 +5,32 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입 금액을 입력해 주세요.");
-        String purchase = Console.readLine();
-        System.out.println();
+        try{
 
-        Customer customer = new Customer();
-        customer.buyLottos(purchase);
+            System.out.println("구입 금액을 입력해 주세요.");
+            String purchase = Console.readLine();
+            System.out.println();
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String inputWinningNumbers = Console.readLine();
-        System.out.println();
+            Customer customer = new Customer();
+            customer.buyLottos(purchase);
 
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String inputBonusNumber = Console.readLine();
-        System.out.println();
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String inputWinningNumbers = Console.readLine();
+            System.out.println();
 
-        LottoNumberFormatter formatter = new LottoNumberFormatter();
-        List<Integer> winningNums = formatter.splitInput(inputWinningNumbers);
-        int bonusNum = formatter.convertToBonusNum(inputBonusNumber);
-        formatter.hasDuplicateNum(winningNums, bonusNum);
+            System.out.println("보너스 번호를 입력해 주세요.");
+            String inputBonusNumber = Console.readLine();
+            System.out.println();
 
-        LottoChecker lottoChecker = new LottoChecker(winningNums, bonusNum);
-        lottoChecker.lottoCheck(customer);
+            LottoNumberFormatter formatter = new LottoNumberFormatter();
+            List<Integer> winningNums = formatter.splitInput(inputWinningNumbers);
+            int bonusNum = formatter.convertToBonusNum(inputBonusNumber);
+            formatter.hasDuplicateNum(winningNums, bonusNum);
+
+            LottoChecker lottoChecker = new LottoChecker(winningNums, bonusNum);
+            lottoChecker.lottoCheck(customer);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
