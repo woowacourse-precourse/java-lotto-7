3c5553c2fc0772dea63c784f782904d523f2afae
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-11-04 22:09:46 +0900

    fix: 로또 번호에 중복된 번호가 존재하면 에러발생기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복된 숫자가 있을 수 없습니다.");
+        }
     }
 
     // TODO: 추가 기능
