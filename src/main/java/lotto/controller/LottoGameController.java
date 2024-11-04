009fdef7cf7964f55699531dbac43842c773b77f
JoonKyo <joonkyolee@gmail.com>
2024-11-04 09:07:01 +0900

    feat(LottoGameController): 로또 결과를 처리하고 사용자에게 출력하기 위해 결과를 전달하는 메서드 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -5,8 +5,10 @@ import java.util.function.Supplier;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
+import lotto.domain.Profit;
 import lotto.domain.WinNumber;
 import lotto.exception.InputException;
 import lotto.util.ConvertInput;
@@ -21,6 +23,7 @@ public class LottoGameController {
         showLottoNumbers(money, lottos);
 
         WinNumber winNumber = makeWinNumber();
+        processResult(lottos, winNumber, money);
     }
 
     private Money readMoney() {
@@ -53,6 +56,12 @@ public class LottoGameController {
     private BonusNumber readBonusNumber() {
         OutputView.askBonusNumber();
         return BonusNumber.from(InputView.readInput());
+    }
+
+    private void processResult(Lottos lottos, WinNumber winNumber, Money money) {
+        LottoResult lottoResult = LottoResult.of(lottos, winNumber);
+        Profit profit = Profit.of(money, lottoResult);
+        OutputView.printLottoResult(lottoResult, profit);
     }
 
     private <T> T repeatUntilReadValidInput(Supplier<T> supplier) {
