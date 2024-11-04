65ac708327f723ab213a0a3cd1358632a5fbf1ad
sanghun <vitash1215@gmail.com>
2024-11-04 21:54:59 +0900

    feat(model) : 당첨번호로 만들어진 Lotto 생성
    당첨번호를 통해 만들어질 Lotto 객체 즉, 우승 로또 객체를 생성, 기본 틀은 Lotto로 잡고 추가적으로 보너스 번호 상태 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,7 +4,7 @@ public enum ErrorMessage {
     INVALID_NUMBER("[ERROR] 숫자만 입력 가능합니다."),
     DUPLICATE_NUMBER("[ERROR] 중복된 숫자가 있습니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
-
+    INVALID_BONUS_NUMBER("[ERROR] 보너스 번호는 1부터 45 사이의 당첨 번호와 중복되지 않은 수여야 합니다"),
     ;
     private final String message;
 
