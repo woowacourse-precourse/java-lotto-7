3c6c2ec674c4e287e77ea0a8149429882001821e
waffle <nate.crema@gmail.com>
2024-11-04 18:17:53 +0900

    refactor(Lotto): 로또번호 비교메서드 이름변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -29,7 +29,7 @@ public class Lotto {
      * @return 로또 당첨등수. 당첨되지 않은 경우 -1을 반환합니다.
      */
     public int grade(List<Integer> gradingNumbers, int bonusNumber) {
-        int[] compareResult = calculateSameCount(gradingNumbers, bonusNumber);
+        int[] compareResult = getSameNumberCount(gradingNumbers, bonusNumber);
         int sameCount = compareResult[0];
         boolean isBonusInclude = compareResult[1] == 1;
 
@@ -47,7 +47,7 @@ public class Lotto {
      * @param bonusNumber    비교대상이 될 로또 보너스번호
      * @return 길이 2인 배열의 0번째에는 일치하는 번호의 개수, 1번째에는 보너스번호의 일치여부가 담겨 반환됩니다.
      */
-    private int[] calculateSameCount(List<Integer> gradingNumbers, int bonusNumber) {
+    private int[] getSameNumberCount(List<Integer> gradingNumbers, int bonusNumber) {
         int gradingNumbersPointer = 0, numbersPointer = 0;
         int sameCount = 0, isBonusInclude = 0;
         List<Integer> orderedNumbers = this.getSortedNumbers();
