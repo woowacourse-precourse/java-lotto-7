8dd5c97c7121bc3815a251b575aba5a37fccbb0d
윤민수 <koelysa5696@naver.com>
2024-11-04 18:52:31 +0900

    Style: 오류 메시지 구체화

--- src/main/java/lotto/constants/InputException.java
+++ src/main/java/lotto/constants/InputException.java
@@ -1,8 +1,8 @@
 package lotto.constants;
 
 public enum InputException {
-    VALID_AMOUNT_INPUT("[ERROR] 금액은 천의 배수 단위의 정수로만 입력하세요."),
-    VALID_WIN_NUMBER("[ERROR] 당첨 번호는 중복 없이 1~45의 정수로 6개 입력하세요."),
+    VALID_AMOUNT_INPUT("[ERROR] 금액은 천의 배수 단위의 양의 정수로만 입력하세요."),
+    VALID_WIN_NUMBER("[ERROR] 당첨 번호는 중복 없이 1~45의 정수로 6개 띄어쓰기 없이 입력하세요."),
     VALID_INPUT_BONUS("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않는 1~45의 정수를 입력하세요,");
 
     private final String message;
