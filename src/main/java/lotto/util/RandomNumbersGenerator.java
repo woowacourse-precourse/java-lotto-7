42fb8cb805586882494faf816949b77de9e73a32
yunho_yun <yunho_yun@naver.com>
2024-11-04 22:02:01 +0900

    feat: 로또 번호 중복 검사 로직 구현

--- src/main/java/lotto/util/RandomNumbersGenerator.java
+++ src/main/java/lotto/util/RandomNumbersGenerator.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 public class RandomNumbersGenerator {
     public static List<Integer> create() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        validateUniqueNumbers(numbers);
+        return numbers;
+    }
+
+    public static void validateUniqueNumbers(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.");
+        }
     }
 }
