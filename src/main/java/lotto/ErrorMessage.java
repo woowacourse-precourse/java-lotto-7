603a665cf1e179c27a76335b06b076824614ab6d
전민정 <minj_1203@naver.com>
2024-11-04 19:49:59 +0900

    feat(exception): 예외처리 에러문구 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -2,6 +2,7 @@ package lotto;
 
 public enum ErrorMessage {
     AMOUNT_INPUT_EXCEPTION("[ERROR] 로또는 1장 당 1000원입니다. 1000원 단위로 입력해주세요."),
+    AMOUNT_ZERO_EXCEPTION("[ERROR] 0원으로는 로또를 살 수 없습니다! 최소 1000원 이상 입력해주세요."),
     NUMBER_COUNT_EXCEPTION("[ERROR] 당첨 번호는 6개의 숫자만 가능합니다."),
     NUMBER_DIVISION_EXCEPTION("[ERROR] 당첨 번호를 쉼표(,)로 구분해주세요."),
     INPUT_NOT_NUMBER_EXCEPTION("[ERROR] 당첨 번호는 숫자만 입력할 수 있습니다."),
