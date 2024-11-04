5a2365ff57fe98f12c15f0058c49cc2c52e36ed0
MinseokGo <rhalstjr1999@naver.com>
2024-11-04 14:13:12 +0900

    feat: 수익률을 출력하는 기능

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -21,6 +21,19 @@ public class LottoGame {
         PrizeProvider prizeProvider = new PrizeProvider(winningStatuses);
         Map<WinningRank, Long> prizes = prizeProvider.getPrizes();
         showWinningResult(prizes);
+
+        final double rateOfReturn = prizeProvider.calculateRateOfReturn(purchaseAmount);
+        showRateOfReturn(rateOfReturn);
+    }
+
+    private LottoManager getLottoManager(final List<Lotto> lottos, final WinningLotto winningLotto) {
+        return new LottoManager(lottos, winningLotto);
+    }
+
+    private WinningLotto getWinningLotto() {
+        final List<Integer> winningLottoNumbers = InputHandler.inputWinningLottoNumbers();
+        final int bonusNumber = InputHandler.inputBonusNumber(winningLottoNumbers);
+        return new WinningLotto(winningLottoNumbers, bonusNumber);
     }
 
     private void showWinningResult(Map<WinningRank, Long> prizes) {
@@ -42,13 +55,7 @@ public class LottoGame {
                 });
     }
 
-    private LottoManager getLottoManager(final List<Lotto> lottos, final WinningLotto winningLotto) {
-        return new LottoManager(lottos, winningLotto);
-    }
-
-    private WinningLotto getWinningLotto() {
-        final List<Integer> winningLottoNumbers = InputHandler.inputWinningLottoNumbers();
-        final int bonusNumber = InputHandler.inputBonusNumber(winningLottoNumbers);
-        return new WinningLotto(winningLottoNumbers, bonusNumber);
+    private void showRateOfReturn(final double rateOfReturn) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
     }
 }
