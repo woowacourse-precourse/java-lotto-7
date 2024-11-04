5592b98bb0443a3e88ea1a670f7696e06f0dc650
eeun1225 <eeun9108@gmail.com>
2024-11-04 21:49:16 +0900

    refactor: 구입 금액 양수 검증과 중복 체크 로직 추가
    - MIN_VALID_AMOUNT 상수를 사용하여 구입 금액 양수 검증 추가
    - validateWinningNumbers에서 HashSet을 사용하여 중복 검사 추가

--- src/main/java/lotto/validate/ValidateInput.java
+++ src/main/java/lotto/validate/ValidateInput.java
@@ -1,14 +1,22 @@
 package lotto.validate;
 
-import lotto.LottoConstants;
+import lotto.domain.Lotto;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static lotto.validate.LottoConstants.LOTTO_PRICE;
+import static lotto.validate.LottoConstants.MIN_VALID_AMOUNT;
 
 public class ValidateInput {
     public static int validateAmount(String amountInput) {
         int amount = validateNumeric(amountInput);
 
-        if (amount % LottoConstants.LOTTO_PRICE.getValue() != 0) {
+        if (amount < MIN_VALID_AMOUNT.getValue()) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+        }
+
+        if (amount % LOTTO_PRICE.getValue() != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
 
@@ -16,17 +24,20 @@ public class ValidateInput {
     }
 
     public static void validateWinningNumbers(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != LottoConstants.LOTTO_NUMBER_COUNT.getValue()) {
-            throw new IllegalArgumentException("[ERROR] 6개의 중복되지 않는 숫자를 입력해야 합니다.");
+        if (winningNumbers.size() != new HashSet<>(winningNumbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
         }
 
         winningNumbers.forEach(ValidateInput::validateNumberInRange);
     }
 
-    public static int validateBonusNumber(String inputBonusNumber) {
+    public static int validateBonusNumber(String inputBonusNumber, Lotto winningLotto) {
         int bonusNumber = validateNumeric(inputBonusNumber);
-
         validateNumberInRange(bonusNumber);
+
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
 
         return bonusNumber;
     }
