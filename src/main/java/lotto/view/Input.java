4a0de531bf3c93e17410362f2d0c71f54a276540
eeun1225 <eeun9108@gmail.com>
2024-11-04 15:29:11 +0900

    fix: 사용자 입력 로직 수정
    - 입력값이 유효하지 않을 경우 오류 메시지를 출력하고 재입력을 요청하도록 구현
    - 구입 금액, 당첨 번호, 보너스 번호 입력 메서드에 반복 구조 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,30 +1,51 @@
 package lotto.view;
 
-import lotto.Lotto;
-
+import lotto.domain.Lotto;
 import java.util.Arrays;
 import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.validate.ValidateInput.*;
 
 public class Input {
-    public void inputPurchaseAmount(){
-        System.out.println("구입금액을 입력해 주세요.");
-        int amount = validateAmount(readLine());
+    public int inputPurchaseAmount(){
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String inputAmount = readLine();
+
+                return validateAmount(inputAmount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public Lotto inputWinningNumbers(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        List<Integer> winningNumbers = parseWinningNumbers(readLine());
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                List<Integer> winningNumbers = parseWinningNumbers(readLine());
 
-        validateWinningNumbers(winningNumbers);
+                validateWinningNumbers(winningNumbers);
 
-        return new Lotto(winningNumbers);
+                return new Lotto(winningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public void inputBonusNumber(){
-        System.out.println("보너스 번호를 입력해 주세요.");
-        int bonusNumber = validateBonusNumber(readLine());
+    public int inputBonusNumber(){
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String inputBonusNumber = readLine();
+
+                return validateBonusNumber(inputBonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<Integer> parseWinningNumbers(String inputNumbers) {
@@ -35,6 +56,4 @@ public class Input {
                 .distinct()
                 .toList();
     }
-
-
 }
