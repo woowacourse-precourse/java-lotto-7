5f8cf005dd72bdd75b5dbbc6391c8a9ccf927fdb
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-04 16:09:56 +0900

    feat(error): 에러 메시지 추가

--- src/main/java/lotto/error/ErrorMessage.java
+++ src/main/java/lotto/error/ErrorMessage.java
@@ -1,13 +1,16 @@
 package lotto.error;
 
 public enum ErrorMessage {
-    INVALID_INTEGER("유효한 정수를 입력해야 합니다."),
+    INVALID_INTEGER("정수로 입력해야하는 값입니다."),
     NOT_MULTIPLE_OF_THOUSAND("금액은 1000원 단위로 입력해야 합니다."),
     BELOW_MINIMUM_AMOUNT("최소 금액은 1000원 이상이어야 합니다."),
     OUT_OF_RANGE("입력값이 유효한 범위를 벗어났습니다."),
     BONUS_NUMBER_DUPLICATE("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     NUMBER_OUT_OF_RANGE("로또 번호의 범위는 1~45입니다."),
-    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다.");
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
+    NEGATIVE_NUMBER("음수는 입력할 수 없습니다."),
+    INVALID_DELIMITER("숫자는 쉼표(,)로 구분되어야 합니다.");
+
 
     private final String message;
 
