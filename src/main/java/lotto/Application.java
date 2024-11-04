6701aa5e85c628dcd201209e84fd4287fdd855be
chahyunsoo <chahs1012@naver.com>
2024-11-04 12:28:05 +0900

    feat(LottoApplication) : Application과 LottoController의 중간 역할인 LottoApplication 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,11 @@
 package lotto;
 
-import lotto.application.LottoController;
-import lotto.view.impl.InputViewImpl;
-import lotto.view.impl.OutViewImpl;
+import lotto.application.LottoApplication;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController lottoController = new LottoController(new InputViewImpl(), new OutViewImpl());
-        lottoController.run();
+        LottoApplication lottoApplication = new LottoApplication();
+        lottoApplication.run();
     }
 }
