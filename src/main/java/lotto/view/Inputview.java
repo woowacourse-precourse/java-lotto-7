8b76e636936dcd592f1ca8230ef1437d1ef2da63
YunCheol1 <naro2727@naver.com>
2024-11-04 13:27:54 +0900

    feat(Inputview):멘트 추가 -요구조건에 맞게 출력되는 멘트 추가

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Inputview {
     public static int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
         if (money % 1000 != 0) {
             throw new IllegalArgumentException();
@@ -12,10 +13,12 @@ public class Inputview {
     }
 
     public static String inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public static int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
     }
 }
