7efd44f525d1acc695a9f1a713b46e150375ccd5
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-04 22:42:51 +0900

    feat: 한 줄 엔터 메서드 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -7,18 +7,27 @@ public class InputView {
     public static final String LOTTO_CAST_LOTS_LOT_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public String lottoPurchase() {
+    public static String lottoPurchase() {
         System.out.println(LOTTO_PURCHASE_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        System.out.println();
+
+        return input;
     }
 
-    public String lottoCastLotsLot() {
+    public static String lottoCastLotsLot() {
         System.out.println(LOTTO_CAST_LOTS_LOT_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        System.out.println();
+
+        return input;
     }
 
-    public String bonusNumber() {
+    public static String bonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+        System.out.println();
+
+        return input;
     }
 }
