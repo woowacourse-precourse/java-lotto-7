71079f3b4f5adc0b322d8f2b3dbc172f9203ae9e
AhnJoonSung <tjdtna01@naver.com>
2024-11-04 20:30:25 +0900

    feat: Lotto 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,10 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int PRICE = 1000;
+    public static final int START_RANGE = 1;
+    public static final int END_RANGE = 45;
+    public static final int NUMBER_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Verifier.validateLottoNumbers(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
