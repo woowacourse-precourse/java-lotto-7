fe4415100add38811cf18ba2e614a12228aa5c6d
gikhoon <ghikhoon@naver.com>
2024-11-04 14:13:33 +0900

    feat: 최종 수익률 계산 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import lotto.controller.dto.LottoResult;
 import lotto.model.Lotto;
@@ -7,6 +9,8 @@ import lotto.model.LottoPurchaseHistory;
 import lotto.model.lottoInfo.LottoGame;
 
 public class LottoService {
+    private static final Integer PERCENT = 100;
+
     public LottoPurchaseHistory buyLotto(int amount) {
         return new LottoPurchaseHistory(amount);
     }
@@ -17,5 +21,13 @@ public class LottoService {
         return new LottoResult(purchasedLotto.stream()
                 .map(lotto -> lottoGame.calculateRank(lotto.getNumbers()))
                 .toList());
+    }
+
+    public BigDecimal calculatePrizeRate(LottoGame lottoGame, LottoResult lottoResult, int money) {
+        long totalPrize = lottoGame.calculateTotalPrize(lottoResult.ranks());
+
+        return BigDecimal.valueOf(totalPrize)
+                .multiply(BigDecimal.valueOf(PERCENT))
+                .divide(BigDecimal.valueOf(money), 2, RoundingMode.HALF_UP);
     }
 }
