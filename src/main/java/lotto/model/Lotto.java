b9440ca05f86be96ece53d0848bc59ea1579c1f4
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-04 20:13:07 +0900

    feat(Lotto): 로또를 생성하는 정적 팩토리 메서드 생성

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static Lotto from(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+}
