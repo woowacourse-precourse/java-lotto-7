2aa8b3012f0b79ad1138f5959aa334178613f70f
dltmddus <tmddus4671@gmail.com>
2024-11-04 15:36:40 +0900

    test: 입력한 로또 번호에 잘못된 형식이 포함된 경우 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,18 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.validation.LottoNumbersValidator;
+import lotto.view.input.InvalidInputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개 이상이면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +26,15 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @ParameterizedTest
+    @ValueSource(strings = {"123456", "1234, ,5"})
+    @DisplayName("입력한 로또 번호에 잘못된 형식이 포함되면 예외가 발생한다.")
+    void 입력한_로또_번호에_잘못된_형식이_포함되면_예외가_발생한다(String input) {
+        // when & then
+        assertThatThrownBy(() -> {
+            LottoNumbersValidator.validateAboutComma(input);
+        }).isInstanceOf(InvalidInputException.class);
+    }
+
+
 }
