a6f4b7c86fd43c8187535503c98c0fb9cb2b8de8
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-04 18:18:55 +0900

    refactor(Prize): 일치하는 번호 개수와 보너스 번호 매치 여부를 요소로 가지고 있도록 변경
    - matchCount와 needBonusMatch를 가지고 있도록 변경
    - matchCount와 needBonusMatch를 바탕으로 순위를 확인할 수 있도록 of 메서드 구현

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,18 +1,44 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Prize {
 
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000),
-    MISS(0),
+    FIRST(2_000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FOURTH(50_000, 4, false),
+    FIFTH(5_000, 3, false),
+    MISS(0, 0, false),
     ;
 
     private final int amount;
+    private final int matchCount;
+    private final boolean needBonusMatch;
 
-    Prize(final int amount) {
+    Prize(final int amount, final int mtachCount, final boolean needBonusMatch) {
         this.amount = amount;
+        this.matchCount = mtachCount;
+        this.needBonusMatch = needBonusMatch;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public int getMatchCount() {
+        return this.matchCount;
+    }
+
+    public boolean getNeedBonusMatch() {
+        return this.needBonusMatch;
+    }
+
+    public static Prize of(int matchCount, boolean bonusMatch) {
+        return Arrays.stream(values())
+                .filter(prize -> prize.matchCount == matchCount)
+                .filter(prize -> !prize.needBonusMatch || bonusMatch)
+                .findFirst()
+                .orElse(Prize.MISS);
     }
 }
