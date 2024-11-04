0e1afb77e5ba990c7a84dc0b4f4308b3fe485572
threepebbles <threepebbles@naver.com>
2024-11-04 19:08:46 +0900

    refactor: 하드코딩된 상수들을 static final로 수정

--- src/main/java/lotto/domain/CustomLottoGenerator.java
+++ src/main/java/lotto/domain/CustomLottoGenerator.java
@@ -5,8 +5,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class CustomLottoGenerator implements LottoGenerator {
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_SIZE = 6;
+
     public List<Integer> generate() {
-        return generateRandomUniqueNumberInRange(1, 45, 6);
+        return generateRandomUniqueNumberInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_SIZE);
     }
 
     public List<Integer> generateRandomUniqueNumberInRange(int startInclusive, int endInclusive, int count) {
