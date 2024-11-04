ecb8efa3e27bbac5bf7b8aba1247bae65dfcad29
euni <kongeuni1004@gmail.com>
2024-11-04 14:43:19 +0900

    refactor: LottoResult 클래스를 record 클래스로 변경
    - LottoResult를 record 클래스로 변경하여 불변성과 코드 간결성 확보
    - 간결한 데이터 저장 구조를 사용하여 코드 유지보수성 향상

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,20 +1,5 @@
 package lotto;
 
-public class LottoResult {
+public record LottoResult(int totalWinnings, int[] rankCount) {
 
-    private final int totalWinnings;
-    private final int[] rankCount;
-
-    public LottoResult(int totalWinnings, int[] rankCount) {
-        this.totalWinnings = totalWinnings;
-        this.rankCount = rankCount;
-    }
-
-    public int getTotalWinnings() {
-        return totalWinnings;
-    }
-
-    public int[] getRankCount() {
-        return rankCount;
-    }
 }
