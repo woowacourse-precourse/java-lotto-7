51229f60076d51b902f6b19e85f15eb6a2831e80
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 14:59:54 +0900

    feat(LottoNumber): 로또 번호의 숫자 범위는 1~45이다

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,43 @@
+package lotto;
+
+public class LottoNumber {
+
+    static final int RANGE_MINIMUM = 1;
+    static final int RANGE_MAXIMUM = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        validateRange(number);
+        this.number = number;
+    }
+
+    private void validateRange(int number) {
+        if (number < RANGE_MINIMUM) {
+            throw new IllegalArgumentException("로또 번호는 1~45 내에 있어야 합니다");
+        }
+        if (number > RANGE_MAXIMUM) {
+            throw new IllegalArgumentException("로또 번호는 1~45 내에 있어야 합니다");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        LottoNumber that = (LottoNumber)o;
+
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return number;
+    }
+}
