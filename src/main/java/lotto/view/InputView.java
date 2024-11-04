57bf18533ddd7898cf4775339c365627fe3275a1
kingrangE <yuvw123@gmail.com>
2024-11-04 16:16:22 +0900

    refactor(InputView): handle errors recursively in inputPurchaseAmount and trim spaces in inputWinningNumbers
    inputPurchaseAmount에서 에러 메시지 출력 후, 재귀적으로 다시 입력받게 하였습니다.
    inputWinningNumbers에서 공백을 제거하도록 수정하였습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,17 @@
 package lotto.view;
+
 import camp.nextstep.edu.missionutils.Console;
 import lotto.model.CustomException;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private final String INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private final String INPUT_WINNING_NUMBER_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private final String INPUT_BONUS_NUMBER_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final String INPUT_BONUS_NUMBER_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final InputView instance = new InputView();
     private final CustomException customException;
 
@@ -22,17 +25,27 @@ public class InputView {
 
     public int inputPurchaseAmount() {
         System.out.println(INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE);
-        String price = Console.readLine();
-        customException.purchaseAmountInputCheck(price);
-        return Integer.parseInt(price);
+        try {
+            String price = Console.readLine();
+            customException.purchaseAmountInputCheck(price);
+            return Integer.parseInt(price);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputPurchaseAmount();
+        }
     }
 
-    public int[] inputWinningNumber() {
+    public List<Integer> inputWinningNumbers() {
         System.out.println(INPUT_WINNING_NUMBER_GUIDE_MESSAGE);
         String winningNumber = Console.readLine();
         customException.winningNumberInputCheck(winningNumber);
-        return Arrays.stream(winningNumber.split(",")).mapToInt(Integer::parseInt).toArray();
+
+        return Arrays.stream(winningNumber.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt) // Integer로 변환
+                .collect(Collectors.toList());
     }
+
     public int inputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER_GUIDE_MESSAGE);
         String bonusNumber = Console.readLine();
