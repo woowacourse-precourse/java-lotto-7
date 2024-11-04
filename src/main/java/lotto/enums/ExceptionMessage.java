a3bcfbcdcb0853c3474ae045fe47484839125423
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-04 16:07:11 +0900

    fix : 에러 메시지 수정

--- src/main/java/lotto/enums/ExceptionMessage.java
+++ src/main/java/lotto/enums/ExceptionMessage.java
@@ -8,7 +8,7 @@ public enum ExceptionMessage {
     ZERO_EXCEPTION("0원이 넘는 금액을 입력해주세요."),
     INVALID_NUMBER_OF_WINNING_NUMBERS(String.format("당첨 번호의 갯수는 %d개여야 합니다.",
             NUMBER_OF_LOTTO_NUMBERS.getValue())),
-    INVALID_LOTTO_NUM_RANGE(String.format("로또 번호는 %d이상 %d이여야 합니다.",
+    INVALID_LOTTO_NUM_RANGE(String.format("로또 번호는 %d이상 %d이하여야 합니다.",
             MIN_LOTTO_NUM.getValue(),MAX_LOTTO_NUM.getValue())),
     INVALID_WINNING_NUM_DUPLICATE("중복된 당첨번호가 있어서는 안됩니다.")
     ;
