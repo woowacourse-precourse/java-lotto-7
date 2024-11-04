abec23ce83bf444780e9641152265dba98cd3121
shortboy7 <thdwldnd7@naver.com>
2024-11-04 16:25:33 +0900

    chore: LottoException 안에 코드를 저장하도록 변경

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,7 +1,12 @@
 package lotto.exception;
 
 public class LottoException extends RuntimeException {
+    private LottoExceptionCode code;
     public LottoException(LottoExceptionCode code) {
         super("[ERROR] "+ code.getMessage());
+        this.code = code;
+    }
+    public LottoExceptionCode getCode() {
+        return this.code;
     }
 }
