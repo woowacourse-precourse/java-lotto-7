c7d51d8acc1f5d0a899ca1cdcdf58c65308f3870
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 12:06:10 +0900

    chore(PurchaseAmountValidatorTest): 구입 금액 관련 변수명 변경

--- src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
+++ src/test/java/lotto/validator/PurchaseAmountValidatorTest.java
@@ -1,6 +1,6 @@
 package lotto.validator;
 
-import static lotto.validator.ValidatorUtils.PRICE_ERROR_MESSAGE;
+import static lotto.validator.ValidatorUtils.PURCHASE_AMOUNT_ERROR_MESSAGE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -8,27 +8,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class PriceValidatorTest {
+public class PurchaseAmountValidatorTest {
 
-    private PriceValidator priceValidator;
+    private PurchaseAmountValidator purchaseAmountValidator;
 
     @BeforeEach
     void setUp() {
-        priceValidator = new PriceValidator();
+        purchaseAmountValidator = new PurchaseAmountValidator();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1000, 2000, 3000, 4000})
-    void 유효한_구입금액_검사(int price) {
-        assertThatCode(() -> priceValidator.validate(price))
+    void 유효한_구입금액_검사(int purchaseAmount) {
+        assertThatCode(() -> purchaseAmountValidator.validate(purchaseAmount))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1000, 0, 1001, 1500})
-    void 유효하지않은_구입금액_검사(int price) {
-        assertThatThrownBy(() -> priceValidator.validate(price))
+    void 유효하지않은_구입금액_검사(int purchaseAmount) {
+        assertThatThrownBy(() -> purchaseAmountValidator.validate(purchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(PRICE_ERROR_MESSAGE);
+                .hasMessageContaining(PURCHASE_AMOUNT_ERROR_MESSAGE);
     }
 }
