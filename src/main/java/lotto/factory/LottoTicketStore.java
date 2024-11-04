bdf826f1280e54ae880c69a5a15104e06bf09b7f
gyuoo <yeomky98@naver.com>
2024-11-05 01:09:13 +0900

    fix(factory): add sorting logic

--- src/main/java/lotto/factory/LottoTicketStore.java
+++ src/main/java/lotto/factory/LottoTicketStore.java
@@ -4,18 +4,22 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import lotto.domain.Lotto;
+import lotto.util.LottoNumberSorter;
 import lotto.util.RandomNumberGenerator;
 
 public class LottoTicketStore {
 
 	private final RandomNumberGenerator randomNumberGenerator;
+	private final LottoNumberSorter lottoNumberSorter;
 
-	public LottoTicketStore(RandomNumberGenerator randomNumberGenerator) {
+	public LottoTicketStore(RandomNumberGenerator randomNumberGenerator, LottoNumberSorter lottoNumberSorter) {
 		this.randomNumberGenerator = randomNumberGenerator;
+		this.lottoNumberSorter = lottoNumberSorter;
 	}
 
 	public Lotto createLottoTicket() {
 		List<Integer> numbers = randomNumberGenerator.pickLottoNumbers();
+		lottoNumberSorter.sortInAscendingOrder(numbers);
 		return new Lotto(numbers);
 	}
 
