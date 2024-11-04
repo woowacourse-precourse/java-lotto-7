73bdaba4016bd32d289573c4347c90c6442fe46c
김현재 <nowalex322@naver.com>
2024-11-04 10:10:42 +0900

    feat: 로또 구매 및 번호 생성
    - 구매 금액 입력 및 로또 생성 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.view.view.InputView;
+import lotto.view.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int amount = InputView.readPurchaseAmount();
+            int count = amount / 1000;
+
+            List<Lotto> lottos = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+                lottos.add(new Lotto(numbers));
+            }
+
+            OutputView.printPurchaseCount(count);
+            OutputView.printLottos(lottos);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
