d3d6f343466c9fed1d3546f008ab8afb45832c44
leejunnyeop <daffodil8620@naver.com>
2024-11-04 14:10:11 +0900

    [refactor] Lotto(model) - 빈문자열 에러 처리

--- src/main/java/lotto/week3/domain/Lotto.java
+++ src/main/java/lotto/week3/domain/Lotto.java
@@ -1,27 +1,48 @@
 package lotto.week3.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
-
-    /*
-      NOTE 불변객체로 생성 : 객체 무결성을 보장함으로 사용함
-     */
   public Lotto(List<Integer> numbers) {
-        this.numbers = List.copyOf(numbers);
+        this.numbers = numbers;
         validate(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 null일 수 없습니다.");
+        }
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 비어 있을 수 없습니다.");
+        }
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+        if (!areNumbersInRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        }
     }
+
+    private Boolean hasDuplicates(List<Integer> numbers){
+        Set<Integer> set = new HashSet<>(numbers);
+        return set.size() != numbers.size();
+    }
+
+    private boolean areNumbersInRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(num -> num >= MIN_NUMBER && num <= MAX_NUMBER);
+    }
+
 
 
 
