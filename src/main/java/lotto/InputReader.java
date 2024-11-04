e9cc3651e9e9cded925301c71377fa4e5b84052e
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 10:59:38 +0900

    feat: 제공되는 Console에서 입력을 받는 메서드 readUserInput 메서드 추가

---
+++ src/main/java/lotto/InputReader.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class InputReader {
+    public String readUserInput(){
+        String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+        return userInput;
+    }
+}
