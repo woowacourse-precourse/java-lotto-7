907fcd11310c364d20f88cd60dc450b2b4228c8a
da1hwang <GDMxKyio@gmail.com>
2024-11-04 18:39:17 +0900

    feat: add getWinningNumbers method to InputHandler

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,11 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputHandler {
 
     public int getPurchaseAmount() {
         String input = Console.readLine();
         return LottoPurchaseValidator.validateAmount(input);
+    }
+
+    public List<Integer> getWinningNumbers() {
+        String input = Console.readLine();
+        return WinningNumbersValidator.validate(input);
     }
 }
