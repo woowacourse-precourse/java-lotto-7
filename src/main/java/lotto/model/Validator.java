0838c87cb9d4139f526dac4193cdd71253d3ccff
Hyejeong <thswjd0609@gmail.com>
2024-11-04 19:17:27 +0900

    feat: 보너스 번호 중복 검증 기능

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.List;
+
 public class Validator {
 
     public void validatePositiveAmount(int purchaseAmount) {
@@ -17,6 +19,12 @@ public class Validator {
     public void validateBonusRange(int bonusNum) {
         if(bonusNum < 1 || bonusNum > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public void validateBonusDuplicate(List<Integer> winningNumbers, int bonusNum) {
+        if (winningNumbers.contains(bonusNum)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 보너스 번호는 중복 불가 입니다.");
         }
     }
 
