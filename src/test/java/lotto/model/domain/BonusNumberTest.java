af79d6a46012843623b0aa1b2132e03c3b6c9c73
kimchanho97 <nh0903@pusan.ac.kr>
2024-11-04 20:37:05 +0900

    test(BonusNumberTest): 보너스 번호 정상 생성 테스트

--- src/test/java/lotto/model/domain/BonusNumberTest.java
+++ src/test/java/lotto/model/domain/BonusNumberTest.java
@@ -1,5 +1,16 @@
 package lotto.model.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class BonusNumberTest {
 
+    @Test
+    @DisplayName("보너스 번호가 로또 번호를 래핑하여 정상 생성된다.")
+    void testBonusNumberWrapLottoNumber() {
+        BonusNumber bonusNumber = new BonusNumber("7");
+        assertThat(bonusNumber.getBonusNumber().getNumber()).isEqualTo(7);
+    }
 }
