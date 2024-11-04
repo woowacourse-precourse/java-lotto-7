b579586cf40c94570079527e104d0ec4ef1c3523
su-jin-shin <sujinshin1@gmail.com>
2024-11-04 22:35:16 +0900

    test(LottoServiceTest.java): 당첨 내역 출력 테스트

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,10 +1,14 @@
 package lotto;
 
-import lotto.service.LottoService;
 import lotto.constant.LottoConstants;
+import lotto.model.LottoResult;
+import lotto.service.LottoService;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,5 +28,21 @@ class LottoServiceTest {
         int expectedTicketCount = VALID_PURCHASE_AMOUNT / LottoConstants.TICKET_PRICE;
 
         assertThat(tickets).hasSize(expectedTicketCount);
+    }
+
+    @DisplayName("당첨 번호가 5개 일치하는 경우 보너스 번호에 따라 당첨 내역을 출력한다.")
+    @Test
+    void 다섯_개_일치_당첨_통계() {
+        List<List<Integer>> totalRandomNumbers = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 9), //5개 일치
+                Arrays.asList(1, 2, 3, 4, 5, 7) //5개 일치, 보너스 볼 일치
+        );
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        lottoService.setBonusNumber(7); // 보너스 번호 설정
+
+        lottoService.updateLottoResults(totalRandomNumbers, winningNumbers);
+
+        assertThat(LottoResult.FIVE_MATCH.getWinCount()).isEqualTo(1);
+        assertThat(LottoResult.FIVE_MATCH_BONUS.getWinCount()).isEqualTo(1);
     }
 }
