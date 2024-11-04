237ad330931e11f29be7ba9057e18d8e1dfa8fd0
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 19:59:34 +0900

    feat(LottoRound): 계산 된 로또 구매 횟수를 저장할 클래스 추가
    -계산 된 로또 구매 횟수를 저장할 클래스가 필요하여 이를 추가함.

---
+++ src/main/java/lotto/domain/LottoRound.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class LottoRound {
+    private final int lottoRound;
+
+    public LottoRound(int lottoRound) {
+        this.lottoRound = lottoRound;
+    }
+
+    public int getLottoRound() {
+        return lottoRound;
+    }
+}
