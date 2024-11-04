e2c7c1ed75be409860a6a95860b5fcf961fb5926
Seol_JY <wlsdud5654@gmail.com>
2024-11-04 15:17:29 +0900

    feat(vo): 로또 번호에서 vo를 사용해 검증로직 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,25 +7,39 @@ import static lotto.constant.LottoConstants.MAXIMUM_LOTTO_NUMBER;
 import static lotto.constant.LottoConstants.MINIMUM_LOTTO_NUMBER;
 import static lotto.constant.LottoConstants.NUMBERS_PER_TICKET;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import lotto.vo.LottoNumber;
 
+/**
+ * 고유하고 검증된 번호 목록을 가진 로또 티켓을 나타냅니다.
+ * <p>
+ * 이 클래스는 미션 요구사항에 따라 {@code numbers} 이외의 추가 필드를 가질 수 없고, {@code numbers} 필드에 대해 private 접근 제한자를 유지해야 합니다. 이러한 제한을 준수하면서
+ * 번호 검증을 수행하기 위해 {@link LottoNumber} 객체를 직접 사용하지 않고, 각 번호를 {@link LottoNumber}를 통해 검증한 후 정수 값을 저장하여 유효한 번호 리스트를 유지합니다.
+ * </p>
+ */
 public class Lotto {
     private final List<Integer> numbers;
 
     private Lotto(List<Integer> numbers) {
-        validateSizeExact(numbers);
-        validateNoDuplicate(numbers);
-        validateNumbersInRange(numbers);
+        List<Integer> validatedNumbers = validateAndConvertNumbers(numbers);
 
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        validateSizeExact(validatedNumbers);
+        validateNoDuplicate(validatedNumbers);
+        validateNumbersInRange(validatedNumbers);
+
+        this.numbers = validatedNumbers;
     }
 
     public static Lotto from(List<Integer> numbers) {
-        numbers = new ArrayList<>(numbers);
-        return new Lotto(numbers);
+        return new Lotto(List.copyOf(numbers));
+    }
+
+    private List<Integer> validateAndConvertNumbers(List<Integer> inputNumbers) {
+        return inputNumbers.stream()
+                .map(LottoNumber::from)
+                .map(LottoNumber::getValue)
+                .sorted()
+                .toList();
     }
 
     private void validateSizeExact(List<Integer> numbers) {
