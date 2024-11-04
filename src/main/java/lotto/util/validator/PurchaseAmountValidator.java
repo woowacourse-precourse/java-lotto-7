c672bf11f75bae009b868d2b1981ec48efe3ddaf
023-dev <0_2_3@naver.com>
2024-11-04 17:07:52 +0900

    feat(PurchaseAmountValidator): 로또 구입 금액 검증 기능 추가

---
+++ src/main/java/lotto/util/validator/PurchaseAmountValidator.java
@@ -0,0 +1,50 @@
+package lotto.util.validator;
+
+
+import static lotto.common.Constants.LOTTO_PRICE;
+import static lotto.util.ErrorMessage.*;
+
+public class PurchaseAmountValidator implements Validator<String> {
+
+    @Override
+    public void validate(String input) {
+        checkNullOrBlank(input);
+        int amount = parseAmount(input);
+        checkNaturalNumber(amount);
+        checkAmountMin(amount);
+        checkAmountUnit(amount);
+    }
+
+    private void checkNullOrBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_BLANK.getMessage());
+        }
+    }
+
+    private int parseAmount(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_FORMAT.getFormattedMessage(input));
+        }
+    }
+
+    private void checkNaturalNumber(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_NATURAL_NUMBER.getMessage());
+        }
+    }
+
+    private void checkAmountMin(int amount) {
+        if (amount < LOTTO_PRICE) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MIN.getMessage());
+        }
+    }
+
+    private void checkAmountUnit(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_UNIT.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/lotto/util/validator/Validator.java
@@ -0,0 +1,6 @@
+package lotto.util.validator;
+
+public interface Validator<T> {
+
+    void validate(T input);
+}
