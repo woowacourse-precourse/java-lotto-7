23dc73b9f6594c8b50c8679fa32215bf8d2f925c
heeyeop Kim <ssucgoo@gmail.com>
2024-11-04 23:11:57 +0900

    feat: 가격을 입력 받아 정수로 변환하는 기능 구현

--- src/main/java/lotto/InputDevice.java
+++ src/main/java/lotto/InputDevice.java
@@ -1,6 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputDevice {
+    public int receivePrice() {
+        String input = Console.readLine();
+        try {
+            int price = Integer.parseInt(input);
+            validatePrice(price);
+            return price;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 가격 형식입니다.");
+        }
+    }
+
     private void validatePrice(int price) {
         if ((price % 1000) != 0) {
             throw new IllegalArgumentException("[ERROR] 입력한 금액이 1000으로 나누어 떨어지지 않습니다.");
