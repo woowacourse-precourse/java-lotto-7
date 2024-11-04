a202d24816ace456b216ee96ec6317a14ab63ea3
gyuoo <yeomky98@naver.com>
2024-11-05 01:02:20 +0900

    feat(factory): add LottoTicketStore class

--- src/main/java/lotto/factory/LottoTicketStore.java
+++ src/main/java/lotto/factory/LottoTicketStore.java
@@ -1,12 +1,27 @@
 package lotto.factory;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import lotto.domain.Lotto;
+import lotto.util.RandomNumberGenerator;
 
 public class LottoTicketStore {
 
-	public Lotto createLottoTicket(List<Integer> numbers) {
+	private final RandomNumberGenerator randomNumberGenerator;
+
+	public LottoTicketStore(RandomNumberGenerator randomNumberGenerator) {
+		this.randomNumberGenerator = randomNumberGenerator;
+	}
+
+	public Lotto createLottoTicket() {
+		List<Integer> numbers = randomNumberGenerator.pickLottoNumbers();
 		return new Lotto(numbers);
+	}
+
+	public List<Lotto> createLottoTickets(int buyCount) {
+		return Stream.generate(this::createLottoTicket)
+			.limit(buyCount)
+			.toList();
 	}
 }
