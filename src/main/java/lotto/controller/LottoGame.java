007aeba0b1df503f7912326f4e82b5a95f5a0a3e
JMJ <ghtntkdnsk@naver.com>
2024-11-04 19:42:01 +0900

    구입 금액 입력 및 검증 로직 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,10 +1,13 @@
 package lotto.controller;
 
+import lotto.error.ErrorMessage;
 import lotto.service.LottoGameService;
+import lotto.validation.Validation;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGame {
+    private final static int ONE_LOTTO_PRICE = 1000;
     private final InputView inputView;
     private final OutputView outputView;
     private final LottoGameService lottoGameService;
@@ -16,6 +19,44 @@ public class LottoGame {
     }
 
     public void start() {
+        purchaseLottos();
+    }
 
+    private void purchaseLottos() {
+        while (true) {
+            try {
+                int purchasePrice = getValidPurchasePrice();
+                int purchaseCount = getPurchaseCount(purchasePrice);
+                outputView.printPurchasedLottoCount(purchaseCount);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int getPurchaseCount(int purchasePrice) {
+        return purchasePrice / ONE_LOTTO_PRICE;
+    }
+
+    private int getValidPurchasePrice() {
+        while (true) {
+            try {
+                String input = inputView.inputPurchasePrice();
+                int purchasePrice = parseInputToInt(input);
+                Validation.validatePurchasePrice(purchasePrice);
+                return purchasePrice;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int parseInputToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_PRICE_FORMAT.getMessage());
+        }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,8 @@
 package lotto.view;
 
 public class OutputView {
+
+    public void printPurchasedLottoCount(int purchaseCount) {
+        System.out.println(purchaseCount + "개를 구매했습니다.");
+    }
 }
