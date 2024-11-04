f4666ed63da275abb670188f223fd14e857d1cc5
ahpicl64 <ahpicl64@gmail.com>
2024-11-04 16:12:30 +0900

    fix: InputView 클래스 내 미사용 메서드 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String REQUEST_PURCHASE_MOUNT = "구입금액을 입력해 주세요.";
-    private static final String PURCHASE_CONFIRM = "개를 구매했습니다.";
     private static final String REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_NUM = "\n보너스 번호를 입력해 주세요.";
     private int paidMoney;
@@ -18,10 +17,6 @@ public class InputView {
         paidMoney = Integer.parseInt(Console.readLine());
 
         return paidMoney;
-    }
-
-    private void showDrawAmount(int drawAmount) {
-        System.out.println(drawAmount + PURCHASE_CONFIRM);
     }
 
     public List<Integer> inputWinningNumber() {
