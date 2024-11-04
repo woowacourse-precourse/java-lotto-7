2bede3f89fd76df54e388150f0a234e76c91a328
hojae <ghwo336@naver.com>
2024-11-04 21:19:34 +0900

    fix:소숫점아래2자리에서 반올림으로수정

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -10,7 +10,6 @@ public class Output {
     }
 
     public static void requestHowManyLottos(int money) {
-        System.out.println(" ");
         System.out.println(money / 1000 + "개를 구매했습니다.");
     }
 
@@ -19,12 +18,11 @@ public class Output {
     }
 
     public static void requestWinningNumbers() {
-        System.out.println(" ");
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
     public static void requestBonusNumber() {
-        System.out.println(" ");
+
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 
@@ -33,18 +31,17 @@ public class Output {
         System.out.println(" ");
         System.out.println("당첨통계");
         System.out.println("---");
-        System.out.println("3개 일치 ("+Rank.FIFTH.winningPrizeString()+"원) - "+winningResult.get(3)+"개");
-        System.out.println("4개 일치 ("+Rank.FIRTH.winningPrizeString()+"원) - "+winningResult.get(4)+"개");
-        System.out.println("5개 일치 ("+Rank.THIRD.winningPrizeString()+"원) - "+winningResult.get(5)+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 ("+Rank.SECOND.winningPrizeString()+"원) - "+five_bonus+"개");
-        System.out.println("6개 일치 ("+Rank.FIRST.winningPrizeString()+"원) - "+winningResult.get(6)+"개");
+        System.out.println("3개 일치 (" + Rank.FIFTH.winningPrizeString() + "원) - " + winningResult.get(3) + "개");
+        System.out.println("4개 일치 (" + Rank.FIRTH.winningPrizeString() + "원) - " + winningResult.get(4) + "개");
+        System.out.println("5개 일치 (" + Rank.THIRD.winningPrizeString() + "원) - " + winningResult.get(5) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + Rank.SECOND.winningPrizeString() + "원) - " + five_bonus + "개");
+        System.out.println("6개 일치 (" + Rank.FIRST.winningPrizeString() + "원) - " + winningResult.get(6) + "개");
     }
 
-    public static void requestProfitAbility(double profitAbility){
-        System.out.println("총 수익률은 "+String.format("%.2f",profitAbility)+"%입니다.");
+    public static void requestProfitAbility(double profitAbility) {
+        System.out.println("총 수익률은 " + String.format("%.1f", profitAbility) + "%입니다.");
 
     }
-
 
 
 }
