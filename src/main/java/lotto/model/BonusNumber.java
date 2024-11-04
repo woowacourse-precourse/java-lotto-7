4292ede48a3bec27ff8b2e5f2689fddae29a4a07
ryan-dia <cjfdnjs158@naver.com>
2024-11-04 20:29:28 +0900

    feat: 보너스 번호 유효성 검사

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -7,6 +7,7 @@ public class BonusNumber {
 
     private BonusNumber(String userInputNumber, List<Integer> winningNumbers) {
         int number = convertToType(userInputNumber);
+        validate(number, winningNumbers);
         this.bonusNumber = number;
     }
 
@@ -22,9 +23,16 @@ public class BonusNumber {
         }
     }
 
+    private void validate(int bonusNumber, List<Integer> winningNumbers) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복되는 숫자가 존재합니다.");
+        }
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45사이의 숫자만 입력이 가능합니다.");
+        }
+    }
 
     public int getBonusNumber() {
         return bonusNumber;
     }
-
 }
