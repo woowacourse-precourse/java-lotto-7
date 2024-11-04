989422ba989f44355136530185383c68a2ed86d0
waffle <nate.crema@gmail.com>
2024-11-04 12:39:55 +0900

    refactor(LottoGuideMessage): 상수 관리파일 이름변경 (-> LottoPrintMessage)

---
+++ src/main/java/lotto/constant/LottoPrintMessage.java
@@ -0,0 +1,9 @@
+package lotto.constant;
+
+public class LottoPrintMessage {
+    public static final String GUIDE_MESSAGE_A = "구입금액을 입력해 주세요.";
+    public static final String GUIDE_MESSAGE_B = "당첨 번호를 입력해 주세요.";
+    public static final String GUIDE_MESSAGE_C = "보너스 번호를 입력해 주세요.";
+
+    public static final String RESPONSE_MESSAGE_A_FORMAT = " %d개를 구매했습니다.";
+}
