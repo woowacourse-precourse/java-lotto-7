3fceebc012eb72507526f18293ac18708fc2e310
SeuneKeun <eiziong1@gmail.com>
2024-11-04 14:22:19 +0900

    feat: 사용자 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String prizeNum = Console.readLine();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNum = Console.readLine();
     }
 }
