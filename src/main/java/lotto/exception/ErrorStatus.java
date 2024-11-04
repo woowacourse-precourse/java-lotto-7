2f1e1f842bb88a68299ecb53e80eec3806651058
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 18:33:45 +0900

    refactor: 오류 이름을 좀 더 명확하게 수정

--- src/main/java/lotto/exception/ErrorStatus.java
+++ src/main/java/lotto/exception/ErrorStatus.java
@@ -6,9 +6,10 @@ public enum ErrorStatus {
     DUPLICATE_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
     DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다."),
     NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
-    INVALID_LOTTO_NUMBER_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    INVALID_COUNT_OF_LOTTO_NUMBERS("[ERROR] 로또 번호는 6개여야 합니다."),
     INVALID_LOTTO_INPUT("[ERROR] 로또 번호 입력은 숫자와 쉼표(,)만 허용됩니다."),
-    INVALID_BONUS_INPUT("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
+    INVALID_BONUS_INPUT("[ERROR] 보너스 번호는 숫자만 입력 가능합니다."),
+    MONEY_OUT_OF_RANGE("[ERROR] 로또 구입 금액은 2,147,483,000원까지 허용됩니다.");
 
     private final String message;
 
