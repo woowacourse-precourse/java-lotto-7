38e3bec051ff3e453f9a134263a67165bae951db
문혜교 <3hilney@gmail.com>
2024-11-04 16:52:18 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/main/java/lotto/OutputManager.java
+++ src/main/java/lotto/OutputManager.java
@@ -2,6 +2,7 @@ package lotto;
 
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputManager {
 
@@ -14,5 +15,15 @@ public class OutputManager {
             System.out.println(ticket);
         }
     }
+
+    public static void printWinningStatistics(Map<MatchCountMessage, Integer> winningStatistics) {
+        System.out.println(OutputMessage.WINNING_STATISTICS.getMessage());
+
+        for (MatchCountMessage match : MatchCountMessage.values()) {
+            int count = winningStatistics.getOrDefault(match, 0);
+            System.out.println(match.getMessage() + count + "개");
+        }
+    }
+
 
 }
