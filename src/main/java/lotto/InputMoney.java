f563cc12a89600643b8683b4b379dac83c20aa39
seungheejang <justinholla908@gmail.com>
2024-11-04 11:06:34 +0900

    style(InputMoney): Make methods and fields static, remove comma-removal method
    - Changed methods and fields to static to allow class-level access without instantiation
    - Removed method for removing commas from inputs, as it’s no longer required

--- src/main/java/lotto/InputMoney.java
+++ src/main/java/lotto/InputMoney.java
@@ -3,18 +3,17 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputMoney {
-    public String inputMoney;
-    public int paidPrice;
-    public int purchaseNumber;
+    public static String inputMoney;
+    public static int paidPrice;
+    public static int purchaseNumber;
 
-    public int inputMoneyToBuyLotto() {
+    public static int inputMoneyToBuyLotto() {
         boolean continueLoop = true;
 
         while (continueLoop) {      // 예외 발생 시 재입력 반복
             System.out.println("구입금액을 입력해 주세요.");
             inputMoney = Console.readLine();
 
-            removeComma(inputMoney);                // 입력 금액 쉼표(,) 제거
             validateInputMoney(inputMoney);         // 입력 금액 검증
             continueLoop = false;
         }
@@ -23,16 +22,8 @@ public class InputMoney {
         return purchaseNumber;
     }
 
-    // 쉼표가 존재하고 쉼표 뒤에 3자리의 숫자 연속되면 쉼표 제거
-    public String removeComma(String inputMoney) {
-        if (inputMoney.contains(",") && inputMoney.matches(".*,[0-9]{3,}.*")) {
-            return inputMoney.replaceAll(",", "");
-        }
-        return inputMoney;
-    }
-
     // 입력 금액 검증
-    public int validateInputMoney(String inputMoney) {
+    public static int validateInputMoney(String inputMoney) {
         paidPrice = checkForNonNumericCharacters(inputMoney);       // 문자나 기호 체크 후 정수 변환
         checkNegative(paidPrice);           // 음수 체크
         checkIfThousandUnit(paidPrice);     // 1000의 배수 체크
@@ -40,23 +31,23 @@ public class InputMoney {
     }
 
     // 문자나 기호 체크 후 정수 변환
-    public int checkForNonNumericCharacters(String inputMoney) {
+    public static int checkForNonNumericCharacters(String inputMoney) {
         try {
             return Integer.parseInt(inputMoney);    // 정수 변환
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자와 3자리 단위를 구분하는 쉼표(,)로만 입력 가능합니다.\n다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 구입금액은 문자, 기호 없이 정수만 입력 가능합니다.\n다시 입력해 주세요.");
         }
     }
 
     // 음수 체크
-    public void checkNegative(int paidPrice) {
+    public static void checkNegative(int paidPrice) {
         if (paidPrice < 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 음수가 될 수 없습니다.\n다시 입력해 주세요.");
         }
     }
 
     // 1000 의 배수 체크
-    public void checkIfThousandUnit(int paidPrice) {
+    public static void checkIfThousandUnit(int paidPrice) {
         if (paidPrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위로만 입력 가능합니다.\n다시 입력해 주세요.");
         }
