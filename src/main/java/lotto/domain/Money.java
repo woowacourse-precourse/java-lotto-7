698edf904caaa290a4ac60b8bcb35d043cf14e5a
ParkSangsin <pss6039@naver.com>
2024-11-04 19:44:47 +0900

    feat: Money 클래스 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,2 +1,31 @@
-package lotto.domain;public class Money {
+package lotto.domain;
+
+import lotto.exception.InvalidInputMoneyDivideException;
+import lotto.exception.InvalidInputMoneyZeroException;
+
+public class Money {
+    private static final Integer THOUSAND = 1000;
+    private final Integer money;
+
+    public Money(Integer money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(Integer money) {
+        validateZero(money);
+        validateDivide(money);
+    }
+
+    private void validateZero(Integer money) {
+        if (money == 0) {
+            throw new InvalidInputMoneyZeroException();
+        }
+    }
+
+    private void validateDivide(Integer money) {
+        if(money % THOUSAND != 0) {
+            throw new InvalidInputMoneyDivideException();
+        }
+    }
 }
