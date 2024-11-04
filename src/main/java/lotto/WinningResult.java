e2f019a63c8f4189e555ee3bd7f21362934d8089
G-Lake <note125@naver.com>
2024-11-04 22:36:46 +0900

    Feat(WinningResult) : 로또, 당첨번호, 보너스번호 상수 설정
    당첨 결과 계산 및 출력 파트

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -1,4 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class WinningResult {
+    private final List<Lotto> tickets;
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public WinningResult(List<Lotto> tickets, List<Integer> winningNumbers, int bonusNumber) {
+        this.tickets = tickets;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void printResult() {
+        // 당첨 결과 계산 및 출력 로직
+    }
 }
