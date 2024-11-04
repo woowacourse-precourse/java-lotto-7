1dad67e84fe92b1777006923270ec39c284d4747
yunju <juile0827@naver.com>
2024-11-04 12:35:26 +0900

    refactor(model): Lotto 클래스 중복 체크 메서드 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,15 +19,13 @@ public class Lotto {
     }
 
     private void validateDuplication(List<Integer> numbers) {
-        boolean duplication = false;
+        List<Integer> duplication = numbers.stream()
+                .filter(i -> numbers.indexOf(i) != numbers.lastIndexOf(i))
+                .distinct()
+                .collect(Collectors.toList());
 
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                if (numbers.get(i).equals(numbers.get(j))) {
-                    duplication = true;
-                    throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
-                }
-            }
+        if(!duplication.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
         }
     }
 
