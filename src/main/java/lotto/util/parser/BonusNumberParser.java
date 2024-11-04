10637bc8f56438c00773255efab2d80d14ce627a
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 20:58:30 +0900

    refact(BonusNumberParser): 좀 더 세밀한 예외처리를 위한 NumberFormatException 사용

--- src/main/java/lotto/util/parser/BonusNumberParser.java
+++ src/main/java/lotto/util/parser/BonusNumberParser.java
@@ -1,7 +1,11 @@
 package lotto.util.parser;
 
 public class BonusNumberParser {
-    public static int toIntStringBonusNumberParser(String bonusNumber) {
-        return Integer.parseInt(bonusNumber);
+    public static int toIntStringBonusNumberParser(String bonusNumber) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보너스 번호는 숫자만 입력 가능합니다.");
+        }
     }
 }
