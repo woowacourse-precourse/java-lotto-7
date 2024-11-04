7f069913717232a250608c7eeffdebfeab8f4831
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 18:24:47 +0900

    refactor: 사용하지 않는 부분 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,15 +6,10 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+    public List<Integer> getNumbers() {
+        return numbers;
     }
-
-    // TODO: 추가 기능 구현
 }
