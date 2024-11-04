cc9d9b7707710d504aa49a548900eced4477dc99
SongJaeHoonn <thdwogns121@naver.com>
2024-11-04 22:03:30 +0900

    feat: 로또 상금 상수화

---
+++ src/main/java/global/LottoPrize.java
@@ -0,0 +1,34 @@
+package global;
+
+public enum LottoPrize {
+
+    FIRST(6, false, "2,000,000"),
+    SECOND(5, true, "30,000,000"),
+    THIRD(5, false, "1,500,000"),
+    FOURTH(4, false, "50,000"),
+    FIFTH(3, false, "5,000"),
+    NONE(0, false, "0");
+
+    private final int matchNumberCount;
+    private final boolean matchBonusNumber;
+    private final String prizeMoney;
+
+    LottoPrize(int matchNumberCount, boolean matchBonusNumber, String prizeMoney) {
+        this.matchNumberCount = matchNumberCount;
+        this.matchBonusNumber = matchBonusNumber;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public String getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public static LottoPrize from(int matchNumberCount, boolean matchBonusNumber) {
+        for (LottoPrize prize : values()) {
+            if (prize.matchNumberCount == matchNumberCount && prize.matchBonusNumber == matchBonusNumber) {
+                return prize;
+            }
+        }
+        return NONE;
+    }
+}
