5401bd53ecfda394dcdf60a2fdbfdb524d8e07a4
woojin cheon <qpqpqp0102@naver.com>
2024-11-04 23:10:45 +0900

    feat(Lotto): 유효성 검사 메서드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,19 +1,34 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import lotto.enums.ErrorMessage;
 
 public class Lotto {
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] " + ErrorMessage.INVALID_SIZE.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] " + ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] " + ErrorMessage.INVALID_RANGE.getMessage());
         }
     }
 
