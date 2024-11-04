a503cc2c9b81cafcfe9b716e758e078c4c21feee
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 19:14:51 +0900

    refactor(InputView): 문자열 상수화

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -9,17 +9,22 @@ public class InputView {
 
     private final static Pattern NUMBER_ONLY_PATTERN = Pattern.compile("\\d+");
     private final static Pattern LOTTO_NUMBERS_FORMAT = Pattern.compile("\\d+,\\d+,\\d+,\\d+,\\d+,\\d+");
-    private static final String LOTTO_NUMBERS_DELIMITER = ",";
+    private final static String LOTTO_NUMBERS_DELIMITER = ",";
+    private final static String PROMPT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
+    private final static String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private final static String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private final static String NUMBER_INPUT_EXCEPTION_MESSAGE = "숫자를 입력하셔야 합니다";
+    private final static String LOTTO_FORMAT_EXCEPTION_MESSAGE = "로또 입력 형식과 맞지 않습니다";
 
     public long readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PROMPT_PURCHASE_AMOUNT);
         String input = Console.readLine();
         validateNumber(input);
         return Long.parseLong(input);
     }
 
     public List<Integer> readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(PROMPT_WINNING_NUMBERS);
         String input = Console.readLine();
         validateLottoNumbersFormat(input);
         return Arrays.stream(input.split(LOTTO_NUMBERS_DELIMITER))
@@ -28,7 +33,7 @@ public class InputView {
     }
 
     public int readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(PROMPT_BONUS_NUMBER);
         String input = Console.readLine();
         validateNumber(input);
         return Integer.parseInt(input);
@@ -36,13 +41,13 @@ public class InputView {
 
     private void validateNumber(String input) {
         if (!NUMBER_ONLY_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException("숫자를 입력하셔야 합니다");
+            throw new IllegalArgumentException(NUMBER_INPUT_EXCEPTION_MESSAGE);
         }
     }
 
     private void validateLottoNumbersFormat(String input) {
         if (!LOTTO_NUMBERS_FORMAT.matcher(input).matches()) {
-            throw new IllegalArgumentException("로또 입력 형식과 맞지 않습니다");
+            throw new IllegalArgumentException(LOTTO_FORMAT_EXCEPTION_MESSAGE);
         }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,11 +8,13 @@ import java.util.Set;
 public class Lotto {
 
     private final static int NUMBERS_LENGTH = 6;
+    private final static String LENGTH_EXCEPTION_MESSAGE = "로또 번호는 6개여야 합니다.";
+    private final static String NO_DUPLICATE_EXCEPTION_MESSAGE = "로또 번호는 중복되면 안됩니다.";
     private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
-        validateDuplicate(numbers);
+        validateDuplicated(numbers);
         this.numbers = numbers.stream()
                 .map(LottoNumber::new)
                 .toList();
@@ -29,14 +31,14 @@ public class Lotto {
 
     private void validateLength(List<Integer> numbers) {
         if (numbers.size() != NUMBERS_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
-        if (nonDuplicateNumbers.size() != NUMBERS_LENGTH) {
-            throw new IllegalArgumentException("로또 번호는 중복되면 안됩니다.");
+    private void validateDuplicated(List<Integer> numbers) {
+        Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
+        if (nonDuplicatedNumbers.size() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException(NO_DUPLICATE_EXCEPTION_MESSAGE);
         }
     }
 
