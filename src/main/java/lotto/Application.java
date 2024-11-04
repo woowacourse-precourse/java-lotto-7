036c2e205f0de5597490d97b46587b2b79a3919e
ktaekim <ktaekim2@gmail.com>
2024-11-04 21:55:02 +0900

    feat: 로또 구입 및 검증 기능 및 단위 테스트 작성
    - 구입 금액은 1,000원 단위여야 함.
    - 잘못된 값 입력 시, 입력을 다시 받는다.
    - 구입 금액은 0보다 커야 함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int lottoPurchaseAmount = getValidatedPurchaseAmount();
+    }
+
+    private static int getValidatedPurchaseAmount() {
+        while (true) {
+            try {
+                System.out.println("구입 금액을 입력해 주세요.");
+                int lottoPurchaseAmount = Integer.parseInt(Console.readLine());
+                validatePurchaseAmount(lottoPurchaseAmount);
+                return lottoPurchaseAmount;
+
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자 형식이 올바르지 않습니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    // 금액 유효성 검증 메서드
+    public static void validatePurchaseAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1보다 커야 합니다.");
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
     }
 }
