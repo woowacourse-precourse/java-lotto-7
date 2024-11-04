51a957b1b95d85d0c787760779a88d0080c47bbc
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-04 06:10:18 +0000

    fix: view에서 계산하던 기능 controller로 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -22,6 +22,6 @@ public class OutputView {
     }
 
     public static void printTotalProfit(Double profit) {
-        System.out.println("총 수익률은 " + profit * 100 + "%입니다.");
+        System.out.println("총 수익률은 " + profit + "%입니다.");
     }
 }
