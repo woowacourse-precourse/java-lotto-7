66e0426e938a078ac3cd3bc08a7e2983d3d3d4dc
hootzz <wjqwjql3@gmail.com>
2024-11-04 15:35:21 +0900

    refactor: WinningLotto의 검증 로직 책임 분리

--- src/main/java/lotto/domain/ticket/WinningLotto.java
+++ src/main/java/lotto/domain/ticket/WinningLotto.java
@@ -1,38 +1,20 @@
 package lotto.domain.ticket;
 
+import lotto.domain.number.BonusNumber;
 import lotto.domain.result.LottoRank;
 
 public class WinningLotto {
     private final Lotto winningNumbers;
-    private final int bonusNumber;
+    private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto winningNumbers, int bonusNumber) {
-        validateBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+        this.bonusNumber = BonusNumber.of(bonusNumber, winningNumbers);
     }
 
-    private void validateBonusNumber(Lotto winningNumbers, int bonusNumber) {
-        validateBonusRange(bonusNumber);
-        validateBonusDuplicate(winningNumbers, bonusNumber);
-    }
-
-    private void validateBonusRange(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private void validateBonusDuplicate(Lotto winningNumbers, int bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
-        }
-    }
-
-    public LottoRank calculateRank(Lotto lotto) {
-        int matchCount = winningNumbers.countMatch(lotto);
-        boolean hasBonusNumber = lotto.contains(bonusNumber);
+    public LottoRank match(Lotto userLotto) {
+        int matchCount = winningNumbers.countMatch(userLotto);
+        boolean hasBonusNumber = userLotto.contains(bonusNumber.getNumber());
         return LottoRank.of(matchCount, hasBonusNumber);
     }
-
 }
