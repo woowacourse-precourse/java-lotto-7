f2864f655645aa9a3a9ee2c75aa538345fb92cec
coolman0123 <socool4m@naver.com>
2024-11-04 16:19:58 +0900

    feat: 사용자에게 보너스 번호를 입력 받아라

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -24,5 +24,14 @@ public class InputView {
         return winNum;
     }
 
+    public void requestWinningBounusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public String inputWinningBonusNumber() {
+        String winzBonusNum = Console.readLine();
+        return winzBonusNum;
+    }
 
 }
