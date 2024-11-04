ab0df51702d9cb4611e8aeb49556bfad9da7c55f
Dong Han <lux00leo@gmail.com>
2024-11-04 21:52:37 +0900

    refactor: 입력 끝난후 빈 라인 추가하여 가독성 향상시킨다

--- src/main/java/lotto/view/OutView.java
+++ src/main/java/lotto/view/OutView.java
@@ -1,14 +1,28 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
 import lotto.domain.LottoWinningTable;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class OutView {
-    public static final String RESULT_DISPLAY_FORMAT = "%s - %d개\n";
+    private static final String NUMBER_OF_PURCHASED_LOTTO_DISPLAY_FORMAT = "%d개를 구매했습니다.\n";
+    private static final String RESULT_TITLE_FORMAT = "당첨 통계\n---";
+    private static final String RESULT_DISPLAY_FORMAT = "%s - %d개\n";
     private static final String PROFIT_PRINT_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+
+    public static void showWholeLottoInUserWallet(final List<Lotto> userWallet) {
+        System.out.printf(NUMBER_OF_PURCHASED_LOTTO_DISPLAY_FORMAT, userWallet.size());
+        for (Lotto userLotto :
+                userWallet) {
+            System.out.println(userLotto.getNumbers());
+        }
+        System.out.println();
+    }
     public static void showWinningResult(final Map<LottoWinningTable, Integer> winningResultTable,final long purchasePrice) {
+        System.out.println(RESULT_TITLE_FORMAT);
         Set<Map.Entry<LottoWinningTable, Integer>> entries = winningResultTable.entrySet();
         for (LottoWinningTable table : LottoWinningTable.values()) {
             System.out.printf(RESULT_DISPLAY_FORMAT, table.toString(), winningResultTable.get(table));
