c23163d771883e1253ff35a43a0cb459e7518f64
yunsseong <aster20@hanyang.ac.kr>
2024-11-04 22:47:56 +0900

    feat: 시스템 메세지 enum 구현

--- src/main/java/lotto/view/message/SystemMessage.java
+++ src/main/java/lotto/view/message/SystemMessage.java
@@ -2,7 +2,10 @@ package lotto.view.message;
 
 public enum SystemMessage {
 
-    INPUT_PURCHASE_MONEY_AMOUNT("구입금액을 입력해주세요");
+    INPUT_PURCHASE_MONEY_AMOUNT("구입금액을 입력해주세요"),
+    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요"),
+    PRINT_STATICS("당첨 통계\n---");
 
     private String message;
 
