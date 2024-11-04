b943fb7a9433ff9ec3d69d640aa3a6298ccf886d
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 12:27:53 +0900

    feat(WinningRank): 우승 등급 반환 기능 구현

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum WinningRank {
+    NOTHING(0, false, 0),
     FIFTH(3, false, 5000),
     FOURTH(4, false, 50000),
     THIRD(5, false, 1500000),
@@ -16,4 +19,11 @@ public enum WinningRank {
     private final int matchCount;
     private final boolean matchBonus;
     private final int prizeAmount;
+
+    public static WinningRank getWinningRank(int matchCount, boolean matchBonus) {
+        return Arrays.stream(WinningRank.values())
+            .filter(rank -> rank.matchCount == matchCount && rank.matchBonus == matchBonus)
+            .findFirst()
+            .orElse(NOTHING);
+    }
 }
