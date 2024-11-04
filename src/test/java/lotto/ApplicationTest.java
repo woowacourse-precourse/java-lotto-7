48e43ef5b69fabf30e705942f938c35cdde839b9
pokm321 <pokm90@gmail.com>
2024-11-04 15:18:52 +0900

    test: add test cases for the entire run

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -7,8 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import lotto.util.Errors;
-import lotto.view.Outputs;
+import lotto.util.MessageParser;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
 
@@ -70,21 +73,119 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    @Test
-    void 숫자가_아닌_예외_테스트() {
+    @ParameterizedTest
+    @CsvSource({
+            "'', '1,2,3,4,5,6', '7'",
+            "'4000', '   ', '7'",
+            "'4000', '1,2,3,4,5,6', '\n'"})
+    void 빈_값_예외_테스트(String money, String lotto, String bonus) {
         assertSimpleTest(() -> {
-            runException("1000j");
+            runException(money, lotto, bonus);
             assertThat(output()).contains(
-                    Errors.ERROR.getMessage() + Outputs.SPACE.getMessage() + Errors.NOT_A_WHOLE_NUMBER.getMessage());
+                    MessageParser.getErrorMessage(Errors.NULL_OR_EMPTY_INPUT.getMessage()));
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1000j', '1,2,3,4,5,6', '7'",
+            "'-', '1,2,3,4,5,6', '7'",
+            "'4000', '1,2,3,4,5,6.', '7'",
+            "'4000', '1,a,3,4,5,6', '7'",
+            "'4000', '1,2,3,4,5,6', '.'"})
+    void 숫자가_아닌_예외_테스트(String money, String number, String bonus) {
+        assertSimpleTest(() -> {
+            runException(money, number, bonus);
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.NOT_A_WHOLE_NUMBER.getMessage()));
         });
     }
 
     @Test
-    void 범위_예외_테스트() {
+    void 금액_데이터타입_범위_예외_테스트() {
         assertSimpleTest(() -> {
-            runException("1000", "1,2,46,3,4,5");
+            runException("99999999999999999999999999");
             assertThat(output()).contains(
-                    Errors.ERROR.getMessage() + Outputs.SPACE.getMessage() + Errors.NOT_IN_LOTTO_RANGE.getMessage());
+                    MessageParser.getErrorMessage(Errors.NOT_A_LONG.getMessage()));
+        });
+    }
+
+    @Test
+    void 로또_데이터타입_범위_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("4000", "1,2,99999999999999999999999999,4,5,6");
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.NOT_INTEGER.getMessage()));
+        });
+    }
+
+    @Test
+    void 보너스_데이터타입_범위_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("4000", "1,2,3,4,5,6", "99999999999999999999999999");
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.NOT_INTEGER.getMessage()));
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1000', '1,2,3,46,5,6', '7'",
+            "'1000', '1,2,3,4,5,6', '0'"})
+    void 로또_보너스_범위_예외_테스트(String money, String number, String bonus) {
+        assertSimpleTest(() -> {
+            runException(money, number, bonus);
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.NOT_IN_LOTTO_RANGE.getMessage()));
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10000000000", "0", "-1000"})
+    void 금액_범위_예외_테스트(String money) {
+        assertSimpleTest(() -> {
+            runException(money);
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.NOT_IN_MONEY_RANGE.getMessage()));
+        });
+    }
+
+    @Test
+    void 로또_콤마_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6,", "7");
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.ENDING_WITH_COMMA.getMessage()));
+        });
+    }
+
+    @Test
+    void 로또_개수_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5", "7");
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.LOTTO_NUMBER_COUNT_ERROR.getMessage()));
+        });
+    }
+
+    @Test
+    void 금액_나머지값_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("4010");
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.REMAINDER_EXISTENT.getMessage()));
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1000', '1,2,43,43,5,6', '7'",
+            "'1000', '1,2,3,4,5,6', '6'"})
+    void 중복_번호_예외_테스트(String money, String number, String bonus) {
+        assertSimpleTest(() -> {
+            runException(money, number, bonus);
+            assertThat(output()).contains(
+                    MessageParser.getErrorMessage(Errors.DUPLICATE_NUMBERS.getMessage()));
         });
     }
 
