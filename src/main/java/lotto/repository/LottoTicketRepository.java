251a38627c6d3f98d666eda3883f34d945d0f36e
sytTest <syt1128@naver.com>
2024-11-04 19:15:15 +0900

    feat(LottoTicketRepository): 로또 게임을 저장 하는 기능 추가

---
+++ src/main/java/lotto/repository/LottoTicketRepository.java
@@ -0,0 +1,31 @@
+package lotto.repository;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import lotto.domain.GameStatus;
+import lotto.domain.Lotto;
+
+public class LottoTicketRepository {
+
+    private static LottoTicketRepository instance;
+    private static Deque<Lotto> lottoReceipt;
+    private static Long totalGameCount = GameStatus.LOWEST_GAME_COUNT.get();
+
+    private LottoTicketRepository() {
+        lottoReceipt = new ArrayDeque<>();
+    }
+
+    public void fillReceipt(Lotto lotto) {
+        lottoReceipt.add(lotto);
+        totalGameCount += GameStatus.ADDED_GAME_COUNT.get();
+    }
+
+    public static LottoTicketRepository getTicket() {
+        if (instance == null) {
+            instance = new LottoTicketRepository();
+            return instance;
+        }
+        return instance;
+    }
+}
