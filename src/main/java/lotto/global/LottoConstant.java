0cebde2c40aab2659df914b7b2d0dbb939749afc
mikekks <thdalsrb79@naver.com>
2024-11-04 14:52:29 +0900

    fix(LottoConstant): 공백 문자 추가

--- src/main/java/lotto/global/LottoConstant.java
+++ src/main/java/lotto/global/LottoConstant.java
@@ -6,4 +6,5 @@ public final class LottoConstant {
 	public static final int LOTTO_MAXIMUM_NUMBER = 45;
 	public static final int LOTTO_NUMBER_COUNT = 6;
 	public static final String LOTTO_NUMBER_SEPARATOR = ",";
+	public static final String BLANK = " ";
 }
