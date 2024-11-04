8729697b9c9c20a7bb3f7e2e104c78bcc27a538e
LeeHaeJun <haejun0410@gmail.com>
2024-11-04 21:00:08 +0900

    feat: 구입한 수량만큼 로또를 생성하는 기능

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,21 +2,13 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 
 public class LottoService {
     private static final int LOTTO_PRICE = 1000;
     private static final int NUMBER_CNT = 6;
-
-    private Lotto generateLotto() {
-        List<LottoNumber> numbers = Randoms.pickUniqueNumbersInRange(LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER,
-                        NUMBER_CNT)
-                .stream()
-                .map(LottoNumber::new)
-                .toList();
-        return new Lotto(numbers);
-    }
 
     public int calculateLottoCount(int amount) {
         validateAmount(amount);
@@ -30,5 +22,20 @@ public class LottoService {
         if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.");
         }
+    }
+
+    private Lotto generateLotto() {
+        List<LottoNumber> numbers = Randoms.pickUniqueNumbersInRange(LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER,
+                        NUMBER_CNT)
+                .stream()
+                .map(LottoNumber::new)
+                .toList();
+        return new Lotto(numbers);
+    }
+
+    public List<Lotto> generateLottos(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> generateLotto())
+                .toList();
     }
 }
