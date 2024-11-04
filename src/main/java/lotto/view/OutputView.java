19cc73c0122ebd0fb41b133d6446e6ff2573c012
Goohong Chung <9rrong@uoslife.team>
2024-11-04 15:42:39 +0900

    feat(OutputView): 발행한 로또 번호를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public void printLottoCount(String lottoCount) {
         System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printLottoNumbers(List<String> lottoNumbers) {
+        lottoNumbers.forEach(System.out::println);
     }
 }
