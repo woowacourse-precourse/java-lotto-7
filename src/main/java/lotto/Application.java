8ff79ee4050f8588cc01555c91995cf1a6d90bda
sarang565 <gsarang56@gmail.com>
2024-11-04 16:56:33 +0900

    2. 숫자 1 ~ 45까지의 변수 만들기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Lotto lotto = new Lotto();
+        List<Integer> lottoNumbers = lotto.LottoNumbers();
+        System.out.println(lottoNumbers);
+
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    // 로또 번호 자동 생성자
+    public Lotto() {
+        this.numbers = LottoNumbers();
+    }
+
+    // 로또 번호 입력 생성자
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    // 숫자 1 ~ 45까지의 변수 만들기
+    public List<Integer> LottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     private void validate(List<Integer> numbers) {
