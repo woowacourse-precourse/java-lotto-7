df7c4c90ec7087152e2ab2e619ae24e29e9e1417
hyuuuun <pam4522@gmail.com>
2024-11-04 21:25:00 +0900

    feat (Utils) : str -> int, str->List<int>

---
+++ src/main/java/lotto/Utils.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+    public static int parseNumber(String str){
+        try {
+            return Integer.parseInt(str);
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
+        }
+
+    }
+    public static List<Integer> parseNumbers(String str){
+        validString(str);
+        String[] numberString = str.split(",");
+        List<Integer> numbers = new ArrayList<>();
+
+        for(String numStr : numberString){
+            numbers.add(parseNumber(numStr.trim()));
+        }
+        return numbers;
+    }
+    private static void validString(String str){
+        if(str.matches(".*[^0-9,\\s].*")){
+            throw new IllegalArgumentException("[ERROR] 숫자와 , 를 입력해주세요");
+        }
+    }
+}
