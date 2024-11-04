487aca6edc446f105452c17cb802c5caa06fbca5
JiEung2 <181757@jnu.ac.kr>
2024-11-04 21:15:28 +0900

    refactor(lottoRanking): 낮은 일치부터 출력될 수 있도록 enum 순서 변경

--- src/main/java/lotto/domain/LottoRanking.java
+++ src/main/java/lotto/domain/LottoRanking.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRanking {
-    FIRST(6, false, "6개 일치 (2,000,000,000원)", 2_000_000_000),
-    SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
-    THIRD(5, false, "5개 일치 (1,500,000원)", 1_500_000),
+    FIFTH(3, false, "3개 일치 (5,000원)", 5_000),
     FOURTH(4, false, "4개 일치 (50,000원)", 50_000),
-    FIFTH(3, false, "3개 일치 (5,000원)", 5_000);
+    THIRD(5, false, "5개 일치 (1,500,000원)", 1_500_000),
+    SECOND(5, true, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    FIRST(6, false, "6개 일치 (2,000,000,000원)", 2_000_000_000),;
 
     private final int matchCount;
     private final boolean matchBonus;
