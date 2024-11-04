f03a73718bda5b8df1fd5125c8998dea25797378
DongHoon Lee <dhl1924@naver.com>
2024-11-04 16:55:58 +0900

    fix(Lotto): 테스트 호환성을 위한 생성자 변경
    - List<Integer>를 받아 Number VO로 변환하는 생성자로 수정하여 테스트와의 호환성 유지

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,15 +6,24 @@ import java.util.List;
 public class Lotto {
     private final List<Number> numbers;
 
-    public Lotto(List<Number> numbers) {
+    public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = convertIntegerToNumber(numbers);
     }
 
-    private void validate(List<Number> numbers) {
+    private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+
+    private List<Number> convertIntegerToNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Number::newInstance)
+                .toList();
+    }
 }
