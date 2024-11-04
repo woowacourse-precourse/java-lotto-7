245ab964caf1073919fd4c6478b91fe6840b7022
josephuk77 <josephuk77@gmail.com>
2024-11-04 10:07:30 +0900

    feat(lotto): 당첨 번호 객체 구현
    lotto객체와 유효성을 검사하는 기능을 구현했습니다.

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -1,10 +1,11 @@
 package lotto.message;
 
 public enum ExceptionMessage {
-
     INVALID_AMOUNT_TYPE("[ERROR] 금액은 숫자만 입력 가능합니다."),
     INVALID_AMOUNT_MIN("[ERROR] 금액은 최소 1000원 이상이어야 합니다."),
-    INVALID_AMOUNT_DIVISION("[ERROR] 금액은 1000원으로 나눠떨어져야 합니다.");
+    INVALID_AMOUNT_DIVISION("[ERROR] 금액은 1000원으로 나눠떨어져야 합니다."),
+    INVALID_LOTTO_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LOTTO_OVERLAP("[ERROR] 로또 번호는 중복일 수 없습니다.");
 
     private final String message;
 
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import lotto.Lotto;
+import lotto.model.Lotto;
 
 public class LottoService {
 
