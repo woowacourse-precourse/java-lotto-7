e720a93514145e1cfa4cf32de900f2629a1d4ae6
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-04 11:48:48 +0900

    test($PurchaseLottoResponseTest): PurchaseLottoResponse 테스트 추가
    - Lottos 로부터 생성.

---
+++ src/test/java/lotto/view/response/PurchaseLottoResponseTest.java
@@ -0,0 +1,61 @@
+package lotto.view.response;
+
+import lotto.mock.number_generator.RealRandomNumberGenerator;
+import lotto.model.Lottos;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("PurchaseLottoResponse 테스트")
+public class PurchaseLottoResponseTest {
+
+    private static final RealRandomNumberGenerator realRandomNumberGenerator = new RealRandomNumberGenerator();
+
+    static Stream<Arguments> 로또_일급_컬렉션_객체로부터_생성된다_테스트_케이스() {
+        return Stream.of(
+                makeArguments(Lottos.generateBy(realRandomNumberGenerator, 3)),
+                makeArguments(Lottos.generateBy(realRandomNumberGenerator, 6)),
+                makeArguments(Lottos.generateBy(realRandomNumberGenerator, 10))
+        );
+    }
+
+    static Arguments makeArguments(Lottos lottos) {
+        return Arguments.of(
+                lottos,
+                lottos.getAllLottoNumbers().stream().map(LottoNumberResponse::from).toList(),
+                lottos.getAllLottoNumbers().size()
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또_일급_컬렉션_객체로부터_생성된다_테스트_케이스")
+    void 로또_일급_컬렉션_객체로부터_생성된다(
+            Lottos lottos,
+            List<LottoNumberResponse> expectedLottoNumberResponses,
+            int expectedPurchasedAmount) {
+
+        // when
+        PurchaseLottoResponse purchaseLottoResponse = PurchaseLottoResponse.from(lottos);
+        List<LottoNumberResponse> actualLottoNumberResponses = purchaseLottoResponse.getLottoNumberResponses();
+        int actualPurchasedAmount = purchaseLottoResponse.getPurchasedAmount();
+
+        // then
+        for (int i = 0; i < actualLottoNumberResponses.size(); i++) {
+            List<Integer> actualLottoNumbers = getLottoNumbers(actualLottoNumberResponses, i);
+            List<Integer> expectedLottoNumbers = getLottoNumbers(expectedLottoNumberResponses, i);
+
+            assertThat(actualLottoNumbers).containsExactlyElementsOf(expectedLottoNumbers);
+        }
+        assertThat(actualPurchasedAmount).isEqualTo(expectedPurchasedAmount);
+    }
+
+    private List<Integer> getLottoNumbers(List<LottoNumberResponse> lottoNumberResponses, int index) {
+        return lottoNumberResponses.get(index).getLottoNumbers();
+    }
+}
