f059f76ab210647dcabf9f00c12ec3939cd74da3
임채연 <dlacodus0407@gmail.com>
2024-11-04 17:40:51 +0900

    test(LottoTest) : 로또 클래스의 단위 테스트를 추가한다
    - 로또생성시 발생하는 예외상황을 테스트한다.
    - 예외가 존재하지 않을때, 로또가 잘 생성되는지 확인한다.
    - 로또와 당첨번호 비교 메서드가 잘 동작하는지 확인한다.

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,64 @@
+package lotto;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+    @Test
+    void 로또를_생성한다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when, then
+        Assertions.assertDoesNotThrow(() -> {
+            new Lotto(numbers);
+        });
+    }
+
+    @Test
+    void 로또의_개수는_6개이어야한다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+    }
+
+    @Test
+    void 로또는_중복된_수를_갖을_수_없다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 2, 4, 5, 6, 7);
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+    }
+
+    @Test
+    void 로또는_1에서_45_사이의_수를_가져야한다() {
+        List<Integer> numbers = List.of(1000, 45, 3, 4, 5, 6, 7);
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Lotto(numbers);
+        });
+    }
+
+    @Test
+    void 발행한_로또와_당첨번호를_비교한다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+        List<Integer> winLottoNumbers = List.of(1, 2, 3, 4, 5, 7);
+
+        // when
+        Integer matched = lotto.compareWithWinLotto(winLottoNumbers);
+
+        // then
+        Assertions.assertEquals(matched, 5);
+    }
+}
