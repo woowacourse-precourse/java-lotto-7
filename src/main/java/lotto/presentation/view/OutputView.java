73cc70807fd36a6e4bbabfd38bc106ec8399d528
ppparkta <pppppark2@gmail.com>
2024-11-04 10:08:44 +0900

    refactor(OutputView): 로또 통계 출력 메서드 분리

--- src/main/java/lotto/presentation/view/OutputView.java
+++ src/main/java/lotto/presentation/view/OutputView.java
@@ -10,9 +10,7 @@ import lotto.domain.dto.LottoStatisticsDto;
 public class OutputView {
     public void printIssuedLottos(List<Lotto> issuedLottos) {
         System.out.println(issuedLottos.size() + "개를 구매했습니다.");
-        for (Lotto issuedLotto : issuedLottos) {
-            printIssuedLotto(issuedLotto);
-        }
+        issuedLottos.forEach(this::printIssuedLotto);
     }
 
     public void printLottoRateOfProfit(LottoStatisticsDto lottoStatisticsDto) {
@@ -21,13 +19,22 @@ public class OutputView {
         System.out.println("총 수익률은 " + lottoStatisticsDto.lottoRateOfProfit() + "%입니다.");
     }
 
+    private void printIssuedLotto(Lotto issuedLotto) {
+        List<String> toStringLottoNumber = issuedLotto.getNumbers().stream()
+                .map(lottoNumber -> String.valueOf(lottoNumber))
+                .toList();
+        System.out.println("[" + String.join(", ", toStringLottoNumber) + "]");
+    }
+
     private void printLottoDrawResults(List<LottoRank> lottoRanks) {
         Arrays.stream(LottoRank.values())
                 .sorted(Comparator.reverseOrder())
-                .forEach(currentRank -> {
-                    long lottoHitCount = countHitLotto(lottoRanks, currentRank);
-                    System.out.println(printLottoRankResult(currentRank, lottoHitCount));
-                });
+                .forEach(currentRank -> printSingleRankResult(lottoRanks, currentRank));
+    }
+
+    private void printSingleRankResult(List<LottoRank> lottoRanks, LottoRank currentRank) {
+        long lottoHitCount = countHitLotto(lottoRanks, currentRank);
+        System.out.println(generateLottoRankResult(currentRank, lottoHitCount));
     }
 
     private long countHitLotto(List<LottoRank> lottoRanks, LottoRank currentRank) {
@@ -36,18 +43,11 @@ public class OutputView {
                 .count();
     }
 
-    private String printLottoRankResult(LottoRank rank, long count) {
-        String formattedPrize = String.format("%,d", rank.getPrize());
+    private String generateLottoRankResult(LottoRank rank, long count) {
         if (rank.isBonusHit()) {
-            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", rank.getHitCount(), formattedPrize, count);
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", rank.getHitCount(),
+                    String.format("%,d", rank.getPrize()), count);
         }
-        return String.format("%d개 일치 (%s원) - %d개", rank.getHitCount(), formattedPrize, count);
-    }
-
-    private void printIssuedLotto(Lotto issuedLotto) {
-        List<String> toStringLottoNumber = issuedLotto.getNumbers().stream()
-                .map(lottoNumber -> String.valueOf(lottoNumber))
-                .toList();
-        System.out.println("[" + String.join(", ", toStringLottoNumber) + "]");
+        return String.format("%d개 일치 (%s원) - %d개", rank.getHitCount(), String.format("%,d", rank.getPrize()), count);
     }
 }
