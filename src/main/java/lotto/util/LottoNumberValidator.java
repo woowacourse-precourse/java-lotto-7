e472d4a1ae12624c024004e556dbc2e47876bc65
GO-TE <alsrudxo9810@naver.com>
2024-11-04 20:42:25 +0900

    feat(LottoNumberValidator): 로또 숫자 범위 검증 기능 구현
    - 로또 검증하는 메소드 명 validate -> validateLottoNumber로 변경

--- src/main/java/lotto/util/LottoNumberValidator.java
+++ src/main/java/lotto/util/LottoNumberValidator.java
@@ -3,7 +3,7 @@ package lotto.util;
 import java.util.List;
 
 public class LottoNumberValidator {
-    public static void validate(List<Integer> numbers) {
+    public static void validateLottoNumbers(List<Integer> numbers) {
         validateNumberCount(numbers);
         validateDuplication(numbers);
         validateRange(numbers);
@@ -29,6 +29,12 @@ public class LottoNumberValidator {
             if (number < 0 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
             }
+        }
+    }
+
+    public static void validateRange(int number) {
+        if (number < 0 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
         }
     }
 }
