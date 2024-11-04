4f8d74a3ab24492f278bbf4612bcfa59fad44f21
imjunghun <dlawndgns9@naver.com>
2024-11-04 16:07:12 +0900

    refactor(Writer): 출력 매개변수로 ViewMessage도 받을 수 있게 Object로 수정

--- src/main/java/lotto/view/console/Writer.java
+++ src/main/java/lotto/view/console/Writer.java
@@ -2,7 +2,7 @@ package lotto.view.console;
 
 public class Writer {
 
-    public static void println(String message) {
+    public static void println(Object message) {
         System.out.println(message);
     }
 }
