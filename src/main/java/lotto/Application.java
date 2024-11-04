848a0382412d33a0aa473a7dab21609424b7fe22
김주언 <wndjs803@naver.com>
2024-11-04 13:54:40 +0900

    feat: 로또 구매 단계 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputVIew;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputVIew inputVIew = new InputVIew();
+        OutputView outputView = new OutputView();
+        LottoController lottoController = new LottoController(inputVIew, outputView);
+        lottoController.startLotto();
     }
 }
