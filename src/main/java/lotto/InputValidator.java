b00d1cffedb9d28c6388155dadcb2148beb8cc20
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 18:39:10 +0900

    refactor(InputValidator): 공백 입력 시 예외 처리 로직 분리
    - validateBlank 메서드에서 사용되는 조건 isBlank 메서드로 분리

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -2,8 +2,12 @@ package lotto;
 
 public class InputValidator {
     public static void validateBlank(String value) {
-        if (value == null || value.isBlank()) {
+        if (isBlank(value)) {
             throw new IllegalArgumentException("[ERROR] 입력은 공백이 아닌 숫자여야 합니다.");
         }
+    }
+
+    private static boolean isBlank(String value) {
+        return value == null || value.isBlank();
     }
 }
