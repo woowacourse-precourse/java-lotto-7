533296effbdbbdafc1e95a15485fe0df2dfd889e
ssuukko <tlstpstpdl11@gmail.com>
2024-11-04 22:11:56 +0900

    Refactor: Lotto 클래스의 유효성 검사 로직 분리
    - 유효성 검사 로직을 LottoValidator 클래스로 분리하여 코드의 가독성을 향상시켰습니다.
    - getSortedNumbers 메서드를 추가하여 정렬된 로또 번호를 반환하도록 개선하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,23 +1,28 @@
 package lotto.domain;
 
+import lotto.validator.LottoValidator;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final LottoValidator validator = new LottoValidator();
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validator.validateLottoNumbers(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public List<Integer> getSortedNumbers() {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 
     // TODO: 추가 기능 구현
