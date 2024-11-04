9fa4490e45e72f1c8f6034cfa3db67bd9705fd52
gyuoo <yeomky98@naver.com>
2024-11-04 22:30:37 +0900

    feat(view): add method to get purchase amount from user

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+	private final OutputView outputView;
+
+	public InputView(OutputView outputView) {
+		this.outputView = outputView;
+	}
+
 	public String readLine() {
 		return Console.readLine();
+	}
+
+	public String getPurchaseAmount() {
+		outputView.printPurchaseAmountMessage();
+		return readLine();
 	}
 }
