a02e740b8a79873a8e41a531a83f94eac0605ed8
Lee Chae Rin <chaerin0916@gmail.com>
2024-11-04 11:44:14 +0900

    feat: 오름차순 정렬 확인용 toString 메서드 오버라이드

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,12 +8,22 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        ascendingSort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void ascendingSort(List<Integer> numbers){
+        numbers.sort(Integer::compareTo);   // 오름차순 정렬
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 
     // TODO: 추가 기능 구현
