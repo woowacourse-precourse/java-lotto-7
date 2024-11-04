123ab2aaeccc124dc3b762c46c15df9016d83615
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 22:21:29 +0900

    feat: ConsoleOutput message list 한 번에 출력하는 메서드 구현

--- src/main/java/lotto/view/ConsoleOutput.java
+++ src/main/java/lotto/view/ConsoleOutput.java
@@ -1,7 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+
 public class ConsoleOutput {
     public void print(String message) {
         System.out.println(message);
+    }
+
+    public void print(List<String> messages) {
+        for(String message: messages) {
+            print(message);
+        }
     }
 }
