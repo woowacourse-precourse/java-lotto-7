de028c0ec74d02289ce66e41e311eec1033420db
jungsu <vwx1@naver.com>
2024-11-04 21:13:45 +0900

    refactor: LottoErrorMessages Enum 자료형으로 변경

--- src/main/java/lotto/constants/LottoErrorMessages.java
+++ src/main/java/lotto/constants/LottoErrorMessages.java
@@ -1,14 +1,20 @@
 package lotto.constants;
 
-public class LottoErrorMessages {
+public enum LottoErrorMessages {
 
-  public static final String ERROR_PREFIX = "[ERROR] ";
-  public static final String INVALID_LOTTO_NUMBER= ERROR_PREFIX + "유효하지 않은 로또 번호입니다.";
-  public static final String INVALID_PURCHASE_AMOUNT = ERROR_PREFIX + "유효하지 않은 금액입니다.";
-  public static final String INVALID_WINNING_NUMBER = ERROR_PREFIX + "유효하지 않은 당첨 번호입니다.";
-  public static final String INVALID_BONUS_NUMBER = ERROR_PREFIX + "유효하지 않은 보너스 번호입니다.";
+  INVALID_LOTTO_NUMBER("유효하지 않은 로또 번호입니다."),
+  INVALID_PURCHASE_AMOUNT("유효하지 않은 금액입니다."),
+  INVALID_WINNING_NUMBER("유효하지 않은 당첨 번호입니다."),
+  INVALID_BONUS_NUMBER("유효하지 않은 보너스 번호입니다.");
 
-  private LottoErrorMessages() {
-    // 인스턴스화 방지
+  private static final String ERROR_PREFIX = "[ERROR] ";
+  private final String message;
+
+  LottoErrorMessages(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return ERROR_PREFIX + message;
   }
 }
