c53880b218440746f93c18c2f4db02e19b124a51
정종두 <jongdoo2001@naver.com>
2024-11-04 17:42:32 +0900

    refactor: Application 정리 및 ErrorMessage 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,5 @@
 package lotto;
 
-import lotto.service.Lotto;
-import lotto.service.Purchase;
-import lotto.view.input.Input;
-import lotto.view.output.Output;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/lotto/view/exception/ErrorMessage.java
+++ src/main/java/lotto/view/exception/ErrorMessage.java
@@ -2,9 +2,9 @@ package lotto.view.exception;
 
 public class ErrorMessage {
 
-    public static final String MultipleOfThousand = "[ERROR] 1000원 단위로 적어주세요.";
-    public static final String numberFormat = "[ERROR] 숫자만 입력해주세요.";
-    public static final String numberCommerForat = "[ERROR] 숫자와 ,만 입력해주세요";
+    public static final String MultipleOfThousand = "[ERROR] 1000원 단위로 적어 주세요.";
+    public static final String numberFormat = "[ERROR] 숫자만 입력해 주세요.";
+    public static final String numberCommerForat = "[ERROR] 숫자와 ,만 입력해 주세요.";
     public static final String validateLottoNumberCount = "[ERROR] 로또 번호는 6개여야 합니다.";
     public static final String validateLottoNumberRange = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String validateLottoNumberDuplicate = "[ERROR] 로또 번호는 중복된 수를 작성할 수 없습니다.";
