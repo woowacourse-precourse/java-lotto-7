e5821b1b033043e0f9a03475bfbcfa7ded469e1b
문혜교 <3hilney@gmail.com>
2024-11-04 15:00:43 +0900

    test: 로또 번호 중복 없이 6개 무작위 선택 기능 테스트

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
@@ -14,6 +16,17 @@ class LottoMachineTest {
         assertThatThrownBy(() -> lottoMachine.calculateLottoCount(1300))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.INVALID_PURCHASE_AMOUNT.getErrorMessage());
+    }
+
+    @Test
+    void 중복_없이_번호_6개_뽑는_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Integer> lottoNumbers = lottoMachine.generateLottoNumbers();
+
+        assertThat(lottoNumbers).hasSize(6);
+        assertThat(lottoNumbers).allMatch(num -> num >= 1 && num <= 45);
+        assertThat(lottoNumbers).doesNotHaveDuplicates();
+
     }
 
 }
