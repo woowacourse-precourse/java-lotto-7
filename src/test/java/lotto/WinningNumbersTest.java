2c6a65295130412010db628de721aa66c638574f
JinHyung-Park <trupjh@gmail.com>
2024-11-04 20:56:35 +0900

    refactor(WinningNumbersTest) : 등수 계산 로직 테스트 재작성 및 완료

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.WinningNumbers;
+import lotto.domain.WinningResult;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumbersTest {
@@ -13,11 +13,22 @@ public class WinningNumbersTest {
     void 당첨_계산_테스트() {
         WinningNumbers wn = new WinningNumbers(List.of(1,2,3,4,5,6), 7);
         Lotto lotto = new Lotto(List.of(3,4,5,6,7,8));
-        lotto.setWinningNumberMatch(wn.matchNumberCount(lotto));
+        assertEquals(WinningResult.FOURTH, lotto.checkWinningResult(wn.matchNumberCount(lotto), wn.isBonusNumberMatch(lotto)));
+    }
 
-        assertEquals(4, lotto.getWinningNumberMatch());
-        assertTrue(wn.isBonusNumberMatch(lotto));
+    @Test
+    void 당첨_2등_테스트() {
+        WinningNumbers wn = new WinningNumbers(List.of(1,2,3,4,5,6), 7);
+        Lotto lotto = new Lotto(List.of(2,3,4,5,6,7));
+        assertEquals(WinningResult.SECOND, lotto.checkWinningResult(wn.matchNumberCount(lotto), wn.isBonusNumberMatch(lotto)));
+    }
 
+    @Test
+    void 당첨_3등_테스트() {
+        WinningNumbers wn = new WinningNumbers(List.of(1,2,3,4,5,6), 7);
+        Lotto lotto = new Lotto(List.of(2,3,4,5,6,8));
+
+        assertEquals(WinningResult.THIRD, lotto.checkWinningResult(wn.matchNumberCount(lotto), wn.isBonusNumberMatch(lotto)));
     }
 
 
