ec1afbb529db020b86416ee335f1515ad6336fdb
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-11-04 23:28:49 +0900

    FEAT: Lotto 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,41 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 당첨 번호를 입력할 수 없습니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public int getDuplicateNumbers(Lotto lotto) {
+        Set<Integer> set1 = new HashSet<>(this.numbers);
+        Set<Integer> set2 = new HashSet<>(lotto.numbers);
+
+        set1.retainAll(set2);
+
+        return set1.size();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
