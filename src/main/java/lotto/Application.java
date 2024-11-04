e89175f2e3173ffb5f0fc7e7830bca0ac40e77d5
박규영 <magic9282m@gmail.com>
2024-11-04 22:21:09 +0900

    fix: modify validateAmount method
    validateAmount 메서드가 유효성 검증만 수행하도록 변경
    - input을 그대로 반환하지 않고, 유효성 검증만 수행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,10 +4,9 @@ public class Application {
     public static void main(String[] args) {
 
     }
-    public static int validateAmount(int input) {
+    public static void validateAmount(int input) {
         if(input <= 0 || input % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
         }
-        return input;
     }
 }
