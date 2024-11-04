c4a1f48cbd845e1e1efc7af0dbf4cb2eec39c912
SuJeong <sujenghong@gmail.com>
2024-11-04 21:12:46 +0900

    feat: add winning number and bonus number input validation

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,6 +19,10 @@ public class Application {
             // 로또 번호 발행
             List<Lotto> userLottos = publishLottos(purchaseAmount);
             System.out.println(userLottos.size() + "개를 구매했습니다.");
+
+            // 당첨 번호 및 보너스 번호 입력
+            List<Integer> winningNumbers = getWinningNumbers();
+            int bonusNumber = getBonusNumber();
 
 
         } catch (IllegalArgumentException e) {
@@ -51,6 +57,29 @@ public class Application {
         }
         return lottos;
     }
+
+    //당첨 번호 및 보너스 번호 입력 기능 추가
+    private static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] input = Console.readLine().split(",");
+        List<Integer> winningNumbers = Arrays.stream(input)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
+        }
+        return winningNumbers;
+    }
+
+    private static int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        return bonusNumber;
+    }
+
 
 
 
