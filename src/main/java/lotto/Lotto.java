e5f874f22eff7ff73ca401d80b584a7664f65b2e
swhyeon98 <smreo75@gmail.com>
2024-11-04 20:31:01 +0900

    feat(Lotto): 보너스 번호 중복 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,10 +3,15 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         LottoValidator.validate(numbers);
         this.numbers = numbers;
+    }
+
+    public boolean hasBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 }
