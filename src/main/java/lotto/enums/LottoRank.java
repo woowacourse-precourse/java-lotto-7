c1b5fce762e707790e06cee21598370076cb3a9c
byunghyunkim0 <bb05qudgus@gamil.com>
2024-11-04 23:43:20 +0900

    fix: enum count값 수정

--- src/main/java/lotto/enums/LottoRank.java
+++ src/main/java/lotto/enums/LottoRank.java
@@ -1,11 +1,11 @@
 package lotto.enums;
 
 public enum LottoRank {
-    FIRST_RANK(1, 3, 2_000_000_000, "2,000,000,000"),
-    SECOND_RANK(2, 4, 30_000_000, "30,000,000"),
+    FIRST_RANK(1, 6, 2_000_000_000, "2,000,000,000"),
+    SECOND_RANK(2, 5, 30_000_000, "30,000,000"),
     THIRD_RANK(3, 5, 1_500_000, "1,500,000"),
-    FOURTH_RANK(4, 5, 50_000, "50,000"),
-    FIFTH_RANK(5, 6, 5_000, "5,000"),
+    FOURTH_RANK(4, 4, 50_000, "50,000"),
+    FIFTH_RANK(5, 3, 5_000, "5,000"),
     NONE_RANK(0, 0, 0, "0");
 
     private final int rank;
