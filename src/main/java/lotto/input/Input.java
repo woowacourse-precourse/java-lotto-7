25e6bdefe62ce6023771dde410cb303003c6aa86
sejinezy <psj8880@naver.com>
2024-11-04 20:39:37 +0900

    refactor(Input): 하드코딩 수정 및 코드 가독성 향상

--- src/main/java/lotto/input/Input.java
+++ src/main/java/lotto/input/Input.java
@@ -1,41 +1,48 @@
 package lotto.input;
 
+import static lotto.input.Validator.*;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Input {
 
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
+    private static final String SPLIT_DELIMITER = ",";
+
     public int getPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = input();
-        Validator.notEmpty(input);
-        Validator.isNumber(input);
-        return Parser.toInt(input); // 수정
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        String input = readInput();
+        notEmpty(input);
+        isNumber(input);
+        return Parser.toInt(input);
     }
 
     public List<Integer> getWinningNumbers() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
-        String input = input();
-        Validator.notEmpty(input);
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        String input = readInput();
+        notEmpty(input);
         String[] splitNumbers = split(input);
-        Validator.isNumber(splitNumbers);
+        isNumber(splitNumbers);
         return Parser.toInts(splitNumbers);
     }
 
     public int getBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        String input = input();
-        Validator.notEmpty(input);
-        Validator.isNumber(input);
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        String input = readInput();
+        notEmpty(input);
+        isNumber(input);
         return Parser.toInt(input);
     }
 
-    private String input() {
+    private String readInput() {
         return Console.readLine();
     }
 
     private String[] split(String input) {
-        return input.split(",");
+        return input.split(SPLIT_DELIMITER);
     }
 
 }
