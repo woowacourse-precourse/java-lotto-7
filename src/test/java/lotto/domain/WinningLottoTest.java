103c0179648b927b46250acb07710326c931896e
MyStraw <kikongdosa@gmail.com>
2024-11-04 22:03:45 +0900

    fix(test): Modify test to use mutable List
    - Use ArrayList instead of List.of() in tests
    - Keep original Lotto class implementation untouched
    - Fix UnsupportedOperationException in WinningLottoTest

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,7 +13,7 @@ public class WinningLottoTest {
     @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다")
     @Test
     void validateDuplicateBonusNumber() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
         assertThatThrownBy(() -> new WinningLotto(winningNumbers, 6))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -20,7 +21,7 @@ public class WinningLottoTest {
     @DisplayName("보너스 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다")
     @Test
     void validateBonusNumberRange() {
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
         assertThatThrownBy(() -> new WinningLotto(winningNumbers, 46))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -28,16 +29,18 @@ public class WinningLottoTest {
     @DisplayName("로또 번호와 당첨 번호를 비교하여 일치하는 번호의 개수를 반환한다")
     @Test
     void matchNumbers() {
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        WinningLotto winningLotto = new WinningLotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 7, 8, 9)));
+
         assertThat(winningLotto.matchNumbers(lotto)).isEqualTo(3);
     }
 
     @DisplayName("보너스 번호와 일치하는지 확인한다")
     @Test
     void matchBonusNumber() {
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        WinningLotto winningLotto = new WinningLotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 7)));
+
         assertThat(winningLotto.hasBonusNumber(lotto)).isTrue();
     }
 }
