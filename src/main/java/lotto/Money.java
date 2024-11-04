550e389c225e23897a5fb0d4b673f7e0e651e56f
전민정 <minj_1203@naver.com>
2024-11-04 19:59:53 +0900

    feat(money): 유효성 검사 및 예외처리

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,6 +1,9 @@
 package lotto;
 
 public class Money {
+    private static final int ZERO = 0;
+    private static final int DIVISIBLE_UNIT = 1000;
+
     private final int amount;
 
     public Money(int amount){
@@ -8,8 +11,17 @@ public class Money {
     }
 
     private void validate(int amount){
-
+        validateIsNegative(amount);
+        validateDivisible(amount);
     }
 
-    private void validateZero
+    private void validateIsNegative(int amount){
+        if(amount < ZERO)
+            throw new IllegalArgumentException(ErrorMessage.AMOUNT_NEGATIVE_EXCEPTION.getMessage());
+    }
+
+    private void validateDivisible(int amount){
+        if(amount % DIVISIBLE_UNIT != ZERO)
+            throw new IllegalArgumentException(ErrorMessage.AMOUNT_INPUT_EXCEPTION.getMessage());
+    }
 }
