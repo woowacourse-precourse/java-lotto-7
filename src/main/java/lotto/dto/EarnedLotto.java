6a745854a3966d2412ca3277962ddfeed0664197
Parkjyun <jaeyun0507@naver.com>
2024-11-04 19:59:47 +0900

    fix(EarnedLotto): 정렬가능하도록 변경

--- src/main/java/lotto/dto/EarnedLotto.java
+++ src/main/java/lotto/dto/EarnedLotto.java
@@ -1,11 +1,13 @@
 package lotto.dto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public record EarnedLotto(List<Integer> numbers) {
     public static EarnedLotto of(List<Integer> numbers) {
-        Collections.sort(numbers);
+        ArrayList<Integer> copiedNumbers = new ArrayList<>(numbers);
+        Collections.sort(copiedNumbers);
         return new EarnedLotto(numbers);
     }
 }
