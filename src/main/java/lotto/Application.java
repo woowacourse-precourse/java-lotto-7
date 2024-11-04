b190e059f1cc967099cb771f599bc287413eac42
haebyun <bhk4392@gmail.com>
2024-11-04 16:21:17 +0900

    feat(Application): 전체 애플리케이션 실행을 시작하는 LottoController 호출 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.config.Config;
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Config config = new Config();
+        LottoController lottoController = config.lottoController();
+        lottoController.run();
     }
 }
