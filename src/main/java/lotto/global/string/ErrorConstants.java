d6d4457eaca58ccfd64b2efec9d6728d32ddaf81
yoona <103414935+yoonali@users.noreply.github.com>
2024-11-04 19:02:41 +0900

    feat : 1000단위 입력 검증 추가

---
+++ src/main/java/lotto/global/string/ErrorConstants.java
@@ -0,0 +1,5 @@
+package lotto.global.string;
+
+public class ErrorConstants {
+    public static final String THOUSAND_UNITS_ERROR_MSG = "[ERROR] 구입금액은 1,000원 단위로만 입력해야 합니다.";
+}
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,20 @@
 package lotto.service;
 
+import lotto.global.string.ErrorConstants;
+
 public class LottoService {
     private int money;
 
     public int getAmount(int money) {
+        validateAmountIsMultipleOfThousand(money);
         this.money = money;
         return money / 1000;
     }
+
+    private void validateAmountIsMultipleOfThousand(int money) {
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorConstants.THOUSAND_UNITS_ERROR_MSG);
+        }
+    }
+
 }
