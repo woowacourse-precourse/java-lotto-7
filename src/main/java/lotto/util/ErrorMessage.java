42928178333539a5f8f5c901ef5e12fe3d0deeed
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-11-04 23:13:45 +0900

    refactor: Enum prefix 수정

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -2,11 +2,13 @@ package lotto.util;
 
 public enum ErrorMessage {
 
-    PURCHASE_AMOUNT_NOT_DIVISION_ERROR("[ERROR] " + "입력하신 금액이 나누어떨어지지 않습니다. "),
-    WINNING_NUMBERS_SIZE_ERROR("[ERROR] " + "당첨번호는 6개여야합니다. "),
-    WINNING_NUMBERS_DUPLICATE_ERROR("[ERROR] " + "당첨번호는 중복되지 않아야합니다. "),
-    LOTTO_NUMBER_OUT_OF_RANGE_ERROR("[ERROR] " + "로또 번호는 1 ~ 46 사이의 숫자여야 합니다. "),
-    INVALID_INPUT_TYPE_INT("[ERROR] " + "잘못된 입력입니다. 숫자를 입력해주세요. ")
+    ERROR_PREFIX("[ERROR] "),
+
+    PURCHASE_AMOUNT_NOT_DIVISION_ERROR(ERROR_PREFIX.message + "입력하신 금액이 나누어떨어지지 않습니다. "),
+    WINNING_NUMBERS_SIZE_ERROR(ERROR_PREFIX.message + "당첨번호는 6개여야합니다. "),
+    WINNING_NUMBERS_DUPLICATE_ERROR(ERROR_PREFIX.message + "당첨번호는 중복되지 않아야합니다. "),
+    LOTTO_NUMBER_OUT_OF_RANGE_ERROR(ERROR_PREFIX.message + "로또 번호는 1 ~ 46 사이의 숫자여야 합니다. "),
+    INVALID_INPUT_TYPE_INT(ERROR_PREFIX.message + "잘못된 입력입니다. 숫자를 입력해주세요. ")
     ;
 
 
