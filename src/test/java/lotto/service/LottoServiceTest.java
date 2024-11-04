731f5abf33fbe8f2e5fcc3deae41c27f7594f299
xeimun <hinote444@naver.com>
2024-11-04 19:15:04 +0900

    test(LottoServiceTest): 수익률 계산 테스트 구현

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,43 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.LottoPurchaseInfo;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoServiceTest {
+
+    private final LottoService lottoService = new LottoService();
+
+    @Test
+    @DisplayName("8,000원을 투자해 5,000원의 이익이 발생하면 수익률은 62.5%이다.")
+    void 수익률을_계산한다() {
+        // given
+        BigDecimal purchaseAmount = BigDecimal.valueOf(8_000);
+        LottoPurchaseInfo purchaseInfo = new LottoPurchaseInfo(purchaseAmount, List.of(1, 2, 3, 4, 5, 6), 7);
+
+        LottoResult lottoResult = new LottoResult();
+        Map<LottoRank, Integer> rankCounts = new EnumMap<>(LottoRank.class);
+        rankCounts.put(LottoRank.NONE, 0);
+        rankCounts.put(LottoRank.FIFTH, 1);
+        rankCounts.put(LottoRank.FOURTH, 0);
+        rankCounts.put(LottoRank.THIRD, 0);
+        rankCounts.put(LottoRank.SECOND, 0);
+        rankCounts.put(LottoRank.FIRST, 0);
+        lottoResult.getRankCounts()
+                   .putAll(rankCounts);
+
+        // when
+        BigDecimal returnOnInvestment = lottoService.calculateReturnOnInvestment(purchaseInfo, lottoResult);
+
+        // then
+        assertThat(returnOnInvestment).isEqualTo("62.5");
+    }
+}
