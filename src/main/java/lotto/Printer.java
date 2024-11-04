362215e86933b7a03d1cb20b59c19f9a1989fbdd
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 21:11:02 +0900

    feat(Printer): Printer 클래스에 최종 결과를 반환하는 함수를 생성한다
    최종 결과를 반환하기 위해 LottoResultProcessor객체 값을 파라미터로
    받는다. 출력 형식에 맞게 enum을 활용하여 출력하도록 만든다. 해시
    맵안에 키 값을 정렬하기 위해 key값을 리스트에 정렬하여 저장한다. 최종
    비율을 getTotalProfit함수를 활용하여 출력한다.

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -1,15 +1,21 @@
 package lotto;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Printer {
     public Printer() {
     }
 
-    public void printBudgetNotice(){
+    public void printBudgetNotice() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void printLottoNumbers(int num){
-        System.out.println(num+"개를 구매했습니다.");
+    public void printLottoNumbers(int num) {
+        System.out.println(num + "개를 구매했습니다.");
     }
 
     public void printWinNumbersNotice() {
@@ -18,5 +24,20 @@ public class Printer {
 
     public void printBonusNumberNotice() {
         System.out.println("보너스 번호를 입력해주세요.");
+    }
+
+    public void printFinalResult(LottoResultProcessor result){
+        int totalMoney= 0;
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        List<String> keys = new ArrayList<>(result.enumResult.keySet());
+        Collections.sort(keys);
+        for(String name: keys){
+            String tag = EnumValue.valueOf(name).getTag();
+            String winningMoney= EnumValue.valueOf(name).getMoneyStr();
+            int matchCount =  result.enumResult.get(name);
+            System.out.println(tag+" ("+ winningMoney+ "원) - "+ matchCount+"개");
+        }
+        System.out.println("총 수익률은 "+ result.getTotalProfit()+"%입니다.");
     }
 }
