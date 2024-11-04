9498013ee25ae9d86db9282afa5555557d1e9bd7
김현재 <nowalex322@naver.com>
2024-11-04 10:04:49 +0900

    feat: 로또 구매 및 번호 생성
    - LottoNumber enum 추가(1~45)

--- src/test/java/lotto/domain/LottoNumber.java
+++ src/test/java/lotto/domain/LottoNumber.java
@@ -3,15 +3,17 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public enum LottoNumber {
-    NONE(0);
+    NONE(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
+    TEN(10), ELEVEN(11), TWELVE(12), THIRTEEN(13), FOURTEEN(14), FIFTEEN(15), SIXTEEN(16), SEVENTEEN(17), EIGHTEEN(18), NINETEEN(19),
+    TWENTY(20), TWENTY_ONE(21), TWENTY_TWO(22), TWENTY_THREE(23), TWENTY_FOUR(24), TWENTY_FIVE(25), TWENTY_SIX(26), TWENTY_SEVEN(27), TWENTY_EIGHT(28), TWENTY_NINE(29), THIRTY(30),
+    THIRTY_ONE(31), THIRTY_TWO(32), THIRTY_THREE(33), THIRTY_FOUR(34), THIRTY_FIVE(35), THIRTY_SIX(36), THIRTY_SEVEN(37), THIRTY_EIGHT(38), THIRTY_NINE(39),
+    FORTY(40), FORTY_ONE(41), FORTY_TWO(42), FORTY_THREE(43), FORTY_FOUR(44), FORTY_FIVE(45);
 
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    private static final List<LottoNumber> CACHE = IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
-            .mapToObj(i -> new LottoNumber(i))
+    private static final List<LottoNumber> CACHE = Arrays.stream(values())
             .collect(Collectors.toList());
 
     private final int number;
@@ -22,7 +24,7 @@ public enum LottoNumber {
 
     public static LottoNumber of(int number) {
         validateNumberRange(number);
-        return CACHE.get(number - 1);
+        return CACHE.get(number);
     }
 
     private static void validateNumberRange(int number) {
