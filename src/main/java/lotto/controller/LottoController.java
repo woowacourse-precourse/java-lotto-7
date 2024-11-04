667aa835fb9511e201e2449ac1d2786598eb7e4c
Goohong Chung <9rrong@uoslife.team>
2024-11-04 17:33:35 +0900

    feat(Lotto): 입력이 잘못된 경우 에러 메시지를 출력한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,11 @@
 package lotto.controller;
 
-import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 import java.util.List;
+import lotto.model.Lotto;
 import lotto.model.LottoBudget;
 import lotto.model.LottoPrizes;
 import lotto.model.Lottos;
+import lotto.model.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -32,22 +32,19 @@ public class LottoController {
         List<String> lottoNumbers = lottos.getLottoNumbers();
         outputView.printLottoNumbers(lottoNumbers);
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String WinningNumbersInput = Console.readLine();
-        String[] WinningNumberStrings = WinningNumbersInput.split(",");
-        List<Integer> winningNumbers = Arrays.stream(WinningNumberStrings)
-                .map(Integer::parseInt)
-                .toList();
+        String WinningNumbersInput = inputView.readWinningNumbers();
 
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String BonusNumberInput = Console.readLine();
-        int bonusNumber = Integer.parseInt(BonusNumberInput);
+        Lotto mainNumbers = Lotto.of(WinningNumbersInput);
 
-        LottoPrizes lottoPrizes = lottos.getPrizes(winningNumbers, bonusNumber);
+        String bonusNumber = inputView.readBonusNumber();
+
+        WinningNumbers winningNumbers = new WinningNumbers(mainNumbers, bonusNumber);
+
+        LottoPrizes lottoPrizes = new LottoPrizes(lottos, winningNumbers);
 
         String yield = lottoPrizes.calculateYield(lottoBudget.getValue());
-        System.out.println("당첨 통계" + System.lineSeparator() + "---");
 
+        System.out.println("당첨 통계" + System.lineSeparator() + "---");
         List<String> matchStatistics = lottoPrizes.calculateMatchStatistics();
         matchStatistics.forEach(System.out::println);
 
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,14 +18,17 @@ public class Lotto {
         return Collections.unmodifiableList(numbers);
     }
 
-    public int countMatchingNumbers(List<Integer> numbersToCompare) {
-        int count = 0;
-        for (Integer number : numbers) {
-            if (numbersToCompare.contains(number)) {
-                count += 1;
-            }
+    public static Lotto of(String rawNumbers) {
+        String[] WinningNumberStrings = rawNumbers.split(",");
+        try {
+            List<Integer> winningNumbers = Arrays.stream(WinningNumberStrings)
+                    .map(Integer::parseInt)
+                    .toList();
+            return new Lotto(winningNumbers);
         }
-        return count;
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야합니다.");
+        }
     }
 
     public boolean containsNumber(int numberToCheck) {
@@ -30,8 +36,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumbersRange(numbers);
+        validateNumberSize(numbers);
+        validateNumbersDuplicate(numbers);
+    }
+
+    private void validateNumberSize(List<Integer> numbers){
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumbersDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        numbers.forEach(this::validateNumberRange);
+    }
+
+    private void validateNumberRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.");
         }
     }
 }
