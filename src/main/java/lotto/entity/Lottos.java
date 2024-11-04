00abcb71230a47e92c813fcdf0255af383f0836b
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 19:16:32 +0900

    feat: 로또 금액 만큼 로또 발급하는 코드 구현

--- src/main/java/lotto/entity/Lottos.java
+++ src/main/java/lotto/entity/Lottos.java
@@ -17,6 +17,13 @@ public class Lottos {
         return lottos;
     }
 
+    public void buyLottos(Long money) {
+        int numberOfLottos = (int) (money / 1000);
+        for (int i = 0; i < numberOfLottos; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+
     public void setWinningNumbers(List<Integer> numbers, Integer bonus) {
         this.winningNumbers = numbers;
         this.bonusNumber = bonus;
