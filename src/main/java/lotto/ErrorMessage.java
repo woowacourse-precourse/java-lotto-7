22d12d42e5ac334dc68624e909f8a4b5186eb41e
전민정 <minj_1203@naver.com>
2024-11-04 20:39:49 +0900

    fix(exception): 예외처리 에러 문구 수정

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     AMOUNT_NEGATIVE_EXCEPTION("[ERROR] 로또를 살 수 없습니다! 최소 1000원 이상 입력해주세요."),
     AMOUNT_NOT_NUMBER_EXCEPTION("[ERROR] 숫자만 입력할 수 있습니다. 예) 1000"),
     NUMBER_COUNT_EXCEPTION("[ERROR] 당첨 번호는 6개의 숫자만 가능합니다."),
-    NUMBER_DIVISION_EXCEPTION("[ERROR] 당첨 번호를 쉼표(,)로 구분해주세요."),
+    NUMBER_DIVISION_EXCEPTION("[ERROR] 당첨 번호를 쉼표(,)로 구분해주세요. 예) 1,2,3,4,5,6"),
     INPUT_NOT_NUMBER_EXCEPTION("[ERROR] 당첨 번호는 숫자만 입력할 수 있습니다."),
     NUMBER_RANGE_EXCEPTION("`[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.`"),
     BONUS_NUMBER_COUNT_EXCEPTION("[ERROR] 보너스 번호는 1개의 숫자만 가능합니다.");
