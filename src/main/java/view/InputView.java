58b87060cbf463a9c3e6144b09c5e61f0edebc01
SongJaeHoonn <thdwogns121@naver.com>
2024-11-04 15:42:39 +0900

    refactor: 보너스 번호 입력 view 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,4 +13,9 @@ public class InputView {
         System.out.println("당첨 번호를 입력해주세요.");
         Console.readLine();
     }
+
+    public static void inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해주세요");
+        Console.readLine();
+    }
 }
