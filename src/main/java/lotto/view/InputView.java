851f155653ce30711993c1f8c2ed465cea1aef5b
seunghee <seunghee_chae3@tmax.co.kr>
2024-11-04 18:33:40 +0900

    feat(InputView): 로또 구입 금액 입력 메세지 출력

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class InputView {
+
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static void printPurchaseAmountInputMessage() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+}
