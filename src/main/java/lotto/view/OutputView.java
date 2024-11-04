5d1a05206eb94cb44435ad9e45ab7838d5b3dda9
SeongJunChun <csj991210@naver.com>
2024-11-04 18:07:06 +0900

    feat: 결과 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+import static lotto.message.SystemMessage.PURCHASED_LOTTO_COUNT;
+import static lotto.message.SystemMessage.SECTION_DIVIDER;
+import static lotto.message.SystemMessage.TOTAL_PROFIT_RATE;
+import static lotto.message.SystemMessage.WINNING_STATISTICS_HEADER;
+
+import lotto.dto.ResultDto;
+
+public class OutputView {
+
+    public void printPurchasedLotto(int count, String lottos) {
+        System.out.println(PURCHASED_LOTTO_COUNT.getMessage());
+        System.out.println(lottos);
+    }
+
+    public void printWinningStatic(ResultDto resultDto) {
+        System.out.println(WINNING_STATISTICS_HEADER.getMessage());
+        System.out.println(SECTION_DIVIDER.getMessage());
+        System.out.println(resultDto.toResults());
+    }
+
+    public void printProfitRate(Double profitRate) {
+        System.out.printf(TOTAL_PROFIT_RATE.getMessage(), profitRate);
+        System.out.print(System.lineSeparator());
+    }
+
+}
