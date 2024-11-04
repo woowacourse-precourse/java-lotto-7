3471b0c58da3288ca2f834520e07ba170fced2f4
byeongdoohan <quden04@gmail.com>
2024-11-04 12:59:33 +0900

    feat: 구입한 로또 목록 생성 기능 추가, 수익률 계산을 위한 리스트 생성 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -7,8 +7,20 @@ import java.util.Set;
 import lotto.generator.LottoGenerator;
 
 public class LottoService {
+    private static final int MATCHED_LIST_LENGTH = 5;
+    private static final int BONUS_CHECK_NUMBER = 3;
+    private static final int THREE_MATCHED = 0;
+    private static final int FOUR_MATCHED = 1;
+    private static final int FIVE_MATCHED = 2;
+    private static final int FIVE_BONUS_MATCHED = 3;
+    private static final int SIX_MATCHED = 4;
+    private static final int THREE = 3;
+    private static final int FOUR = 4;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
     ValidationService validationService = ValidationService.createValidationService();
     LottoGenerator lottoGenerator = LottoGenerator.createLottoGenerator();
+
     public int countMatches(List<Integer> winnerNumbers, List<Integer> numbers) {
         winnerNumbers.retainAll(numbers);
         return winnerNumbers.size();
@@ -32,7 +44,7 @@ public class LottoService {
 
     public List<Set<Integer>> generateManualNumberSet(int manualAmount, String manualMode) {
         List<Set<Integer>> manualNumberList = new ArrayList<>();
-        while (manualAmount>0){
+        while (manualAmount > 0) {
             manualNumberList.add(validationService.validateCorrectManualNumber(manualAmount, manualMode));
             manualAmount--;
         }
@@ -45,5 +57,48 @@ public class LottoService {
             autoLottoList.add(new HashSet<>(lottoGenerator.getLottoNumbers()));
         }
         return autoLottoList;
+    }
+
+    public List<Set<Integer>> getTotalLottoList(List<Set<Integer>> manualLottoList, List<Set<Integer>> autoLottoList) {
+        List<Set<Integer>> totalLottoList = new ArrayList<>();
+        totalLottoList.addAll(autoLottoList);
+        totalLottoList.addAll(manualLottoList);
+        return totalLottoList;
+    }
+
+    public List<int[]> calculateMatched(List<Set<Integer>> totalLottoList, List<Set<Integer>> winnerLotto,
+                                        int bonusNumber) {
+        List<int[]> matchedList = new ArrayList<>();
+        for (Set<Integer> set : totalLottoList) {
+            Set<Integer> checkSet = new HashSet<>(set);
+            checkSet.retainAll(winnerLotto.getFirst());
+            if (checkSet.size() == BONUS_CHECK_NUMBER && set.contains(bonusNumber)) {
+                int[] matched = addMatched(BONUS_CHECK_NUMBER);
+                matchedList.add(matched);
+            }
+            if (checkSet.size() == SIX) {
+                int[] matched = addMatched(SIX_MATCHED);
+                matchedList.add(matched);
+            }
+            if (checkSet.size() == FIVE) {
+                int[] matched = addMatched(FIVE_MATCHED);
+                matchedList.add(matched);
+            }
+            if (checkSet.size() == FOUR) {
+                int[] matched = addMatched(FOUR_MATCHED);
+                matchedList.add(matched);
+            }
+            if (checkSet.size() == THREE) {
+                int[] matched = addMatched(THREE_MATCHED);
+                matchedList.add(matched);
+            }
+        }
+        return matchedList;
+    }
+
+    private int[] addMatched(int bonusCheckNumber) {
+        int[] matched = new int[MATCHED_LIST_LENGTH];
+        matched[bonusCheckNumber]++;
+        return matched;
     }
 }
