17ec1ae007ba13b9c9f3481b5be709a28079ef12
EP45 <5510816+EP45@users.noreply.github.com>
2024-11-04 19:51:51 +0900

    feat(LottoRegulation): add class for storing constants
    This class stores principal rules of lotto game:
    - The number of numbers per lotto
    - Min and max of each number
    - Price of lotto

--- src/main/java/lotto/LottoRegulation.java
+++ src/main/java/lotto/LottoRegulation.java
@@ -4,4 +4,5 @@ public class LottoRegulation {
     public static final int LOTTO_NUMBERS_COUNT = 6;
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_PRICE = 1_000;
 }
