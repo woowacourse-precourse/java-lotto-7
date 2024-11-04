ab50f6c4f7b6064674579b4cd08eed643a1f4e1f
chahyunsoo <chahs1012@naver.com>
2024-11-04 14:10:39 +0900

    style(LottoController) : 임시 주석을 통한 리팩토링 가독성 향상

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -24,20 +24,28 @@ public class LottoController {
 
     public void run() {
         try {
+            //로또 티켓 구매 수량
             int ticketCount = inputMiddleController.getValidatedBuyingAmount();
+
+            //로또 티켓 발행
+            LottoNumberGeneratorService lottoNumberGeneratorService = new LottoNumberGeneratorService();
             LottoTicketIssueService lottoTicketIssueService = new LottoTicketIssueService(ticketCount,
-                    new LottoNumberGeneratorService());
+                    lottoNumberGeneratorService);
             List<Lotto> issuedLotto = lottoTicketIssueService.issueLotto();
             outPutView.printBoughtLotto(issuedLotto);
 
+            //당첨번호와 보너스 번호 발급
             WinningLotto winningLotto = inputMiddleController.getValidatedWinningLotto();
 
+            //당첨통계 계산
             LottoMatchNumberService matchService = new LottoMatchNumberService(winningLotto);
             Map<WinningRank, Integer> results = matchService.calculateResults(issuedLotto);
-
             outPutView.printWinningStatistic(results);
+
+            //수익률 계산
             double profitRate = CalculateProfitRateService.calculateProfitRate(results, ticketCount);
             outPutView.printProfitRate(profitRate);
+
         } catch (IllegalArgumentException e) {
             System.out.println(PRINT_ERROR_MESSAGE.getMessage() + e.getMessage());
         }
