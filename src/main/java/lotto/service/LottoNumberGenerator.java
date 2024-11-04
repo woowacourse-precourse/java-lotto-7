fd67758e8b6d228a470959f1d900011ed0072f1e
na0th <105775683+na0th@users.noreply.github.com>
2024-11-04 19:09:18 +0900

    refactor : LottoNumberGenerator가 ArrayList 객체로 생성
    변경한 내용 :
    1. ArrayList 생성하는 것으로 변경
    ->테스트에서 List.of 불변 리스트를 써서 테스트 케이스 성공을 위해서는 ArrayList로 sort를 해야 했음

--- src/main/java/lotto/service/LottoNumberGenerator.java
+++ src/main/java/lotto/service/LottoNumberGenerator.java
@@ -2,10 +2,15 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
     public List<Integer> createLottoNumbers(int start, int end, int count) {
-        return Randoms.pickUniqueNumbersInRange(start, end, count);
+        //Randoms.pickUniqueNumbersInRange()가 불변 리스트를 반환할 수 있다는 가정(테스트 코드)
+        List<Integer> lottoNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(start, end, count));
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
 }
