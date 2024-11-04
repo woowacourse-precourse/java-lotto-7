780ffad2c0e1b15f17166a1c952cb6dedab9cf41
Nestory <xofks777@naver.com>
2024-11-04 17:21:31 +0900

    fix(LottoGenerator): fix generate method and delete constructor of class

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Random;
+import java.util.stream.Collectors;
 
 public class LottoGenerator {
     private final static int LOTTO_PRICE = 1000;
@@ -13,20 +13,15 @@ public class LottoGenerator {
     private final static int MAX_RANGE_NUMBER = 45;
     private final static int LOTTO_NUMBERS_AMOUNT = 6;
 
-    private final int quantity;
-
-    public LottoGenerator(int purchaseAmount) {
-        validatePurchaseAmount(purchaseAmount);
-        quantity = purchaseAmount / LOTTO_PRICE;
-    }
-
     private void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
     }
 
-    public List<Lotto> generate() {
+    public List<Lotto> generate(int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        int quantity = purchaseAmount / LOTTO_PRICE;
         List<Lotto> lotties = new ArrayList<>();
 
         for (int i = 0; i < quantity; i++) {
@@ -38,7 +33,9 @@ public class LottoGenerator {
     }
 
     public List<Integer> generateNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER,
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE_NUMBER, MAX_RANGE_NUMBER,
                 LOTTO_NUMBERS_AMOUNT);
+        numbers = numbers.stream().sorted().collect(Collectors.toList());
+        return numbers;
     }
 }
