b35cb9938196c93dd46d0351324a8ac18d54c905
yuhoyuho <leeyuho123@naver.com>
2024-11-04 19:21:06 +0900

    요구한 기능을 구현하여 프로그래밍

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
-
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +15,20 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if(new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        for(int number : numbers) {
+            if(number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     // TODO: 추가 기능 구현
