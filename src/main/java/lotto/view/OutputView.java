293c466fa987b8b571bb3ee3fd073285c967dbbe
kimgoat <kimgoatlove@gmail.com>
2024-11-04 19:36:25 +0900

    feat(OutputView): 로또 결과 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import lotto.common.constant.LottoPrizeRank;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,5 +24,30 @@ public class OutputView {
                 .sorted()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    public void printWinningStatistics(LottoResult result) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        printWinningResults(result);
+        printProfitRate(result);
+    }
+
+    private void printWinningResults(LottoResult result) {
+        LottoPrizeRank.getWinningRanks().forEach(rank ->
+                System.out.printf("%s - %d개\n",
+                        formatWinningRank(rank),
+                        result.getWinningStatistics().getOrDefault(rank, 0)));
+    }
+
+    private String formatWinningRank(LottoPrizeRank rank) {
+        return String.format("%s (%,d원)",
+                rank.getDescription(),
+                rank.getPrizeMoney());
+    }
+
+    private void printProfitRate(LottoResult result) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n",
+                result.calculateProfitRate());
     }
 }
