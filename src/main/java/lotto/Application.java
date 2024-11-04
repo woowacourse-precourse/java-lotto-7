5633868b075afe7ff10d4585eeb44f4e07611634
Ju-Hyun-J <jujung967@gmail.com>
2024-11-04 21:53:32 +0900

    구입금액 및 로또 번호 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int inputPurchaseAmount = Integer.parseInt(Console.readLine());
+        String inputWinningNumber = Console.readLine();
+        int inputBonusNumber = Integer.parseInt(Console.readLine());
+
+        setAmount(inputPurchaseAmount);
+
+        List<Integer> lottoNumbers = Arrays.stream(inputWinningNumber.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        Lotto lotto = new Lotto(lottoNumbers);
+
+
+
+
+    }
+
+    public static void setAmount(int inputPurchaseAmount) {
+        if (inputPurchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 구입가능합니다.");
+        }
     }
 }
