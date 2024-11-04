cc04272e263b1c62963c77f8da274ec638c180fb
Avocado <ujubongbong@gmail.com>
2024-11-04 21:08:01 +0900

    :sparkles: feat: 구매 내역 출력 함수

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -4,4 +4,8 @@ public class Output {
 	public void init() {
 		System.out.println("구입금액을 입력해 주세요.");
 	}
+
+	public void printLottoCount(int lottoCount) {
+		System.out.println(lottoCount + "개를 구매했습니다.");
+	}
 }
