aa1289784da3e09ae8514034a487a4172c374482
zerocat36 <Seo.james2020@gmail.com>
2024-11-04 21:57:42 +0900

    feat(Application.java): add buyLotto function

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Application {
     private static List<Integer> getWinningNumbers() {
@@ -13,7 +15,7 @@ public class Application {
         try {
             return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
         } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호가 잘못된 형식으로 입력되었습니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 로또 번호가 입력되었습니다.");
         }
     }
 
@@ -23,8 +25,29 @@ public class Application {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 잘못된 형식으로 입력되었습니다.");
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 보너스 번호가 입력되었습니다.");
         }
+    }
+
+    private static List<List<Integer>> buyLotto() {
+        int amount, price = 1000;
+        List<List<Integer>> lotto_numbers = new ArrayList<>(List.of());
+
+        System.out.println("구입 금액을 입력해 주세요.");
+
+        try {
+            amount = Integer.parseInt(Console.readLine());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 구입 금액이 입력되었습니다.");
+        }
+
+        int count = amount / price;
+        for (int i = 0; i < count; i++) {
+            List<Integer> buffer = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lotto_numbers.add(buffer);
+            System.out.println(buffer);
+        }
+        return lotto_numbers;
     }
 
     public static void main(String[] args) {
