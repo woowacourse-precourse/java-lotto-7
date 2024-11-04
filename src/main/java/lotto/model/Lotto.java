a68316ee417a19cbbd3f9447af895c78f5d128f2
Hyejeong <thswjd0609@gmail.com>
2024-11-04 17:20:47 +0900

    refactor: 로또 개수 검증 함수 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,5 @@
-package lotto;
+package lotto.model;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -13,27 +12,11 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         validateSize(numbers);
-        validateRange(numbers);
-        validateDuplicate(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    public void validateRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-        }
-    }
-
-    public void validateDuplicate(List<Integer> numbers) {
-        if (new HashSet<>(numbers).size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
