b552eab087965a2a2294bec515ec9058cc2460b6
bokyeong <wntjdals0412@gmail.com>
2024-11-04 17:19:23 +0900

    feat : 콘솔 입력 메시지 출력

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -28,6 +28,7 @@ public class InputHandler {
     public List<Integer> getWinnerNumber() {
         while (true) {
             try {
+                System.out.println("당첨 번호를 입력해 주세요.");
                 String input = Console.readLine();
                 List<Integer> winnerNumbers = Arrays.stream(input.split(","))
                         .map(Integer::parseInt)
@@ -44,6 +45,7 @@ public class InputHandler {
     public int getBonusNumber(List<Integer> winnerNumbers) {
         while (true) {
             try {
+                System.out.println("보너스 번호를 입력해 주세요.");
                 int bonusNumber = Integer.parseInt(Console.readLine());
                 checkNumber(bonusNumber);
                 checkBonusNumberDuplicateWithWinnerNumbers(winnerNumbers, bonusNumber);
