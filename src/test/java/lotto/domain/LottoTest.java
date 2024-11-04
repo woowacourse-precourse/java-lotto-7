e3ca780129fec33a9a8065df2e721f7a3b5717ef
이준범 <jach1206@naver.com>
2024-11-04 21:08:17 +0900

    test: 발행한 로또 번호가 6개로 알맞게 발행되는지 테스트 코드 추가
    - LottoTest 클래스: 로또 번호가 6개가 발급되는지에 대한 테스트 코드 추가 및,
    기존 Arrays.asList로 발급 받던 로또 번호를 직접 구현한 서비스 메서드로 변경했다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,14 +4,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.service.Random;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
-    private static final List<Integer> FIVE_NUMBER = Arrays.asList(1, 2, 3, 4, 5);
-    private static final List<Integer> DUPLICATED_NUMBER = Arrays.asList(1, 1, 2, 3, 4, 5);
-    private static final List<Integer> OUT_OF_RANGE = Arrays.asList(1, 2, 3, 4, 5, 46);
+    private static final Random random = new Random();
 
+    private static final List<Integer> FIVE_NUMBER = random.NumberIssue(1, 45, 5);
+    private static final List<Integer> DUPLICATED_NUMBER = Arrays.asList(1, 1, 2, 3, 4, 5);
+    private static final List<Integer> OUT_OF_RANGE = random.NumberIssue(45, 90, 6);
+
+    @Test
+    void 로또_번호가_6개가_발급되는지_테스트를_진행한다() {
+        List<Integer> integers = random.NumberIssue(1, 45, 6);
+        Assertions.assertThat(integers.size()).isEqualTo(6);
+    }
 
     @Test
     void 로또_번호가_6개가_아닌_경우_예외가_발생한다() {
