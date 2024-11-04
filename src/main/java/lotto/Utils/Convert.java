6671dfecefa154415a7f5c599959d68b894ece91
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-04 18:02:25 +0900

    feat: 타입 변환 시 예외 처리 추가
    - 프로그램에서 3가지 입력을 받습니다. 이에 3가지 입력에 따른 예외 처리
      로직을 추가하였습니다.
    
    - 이렇게 작성한 이유는 NumberFormatException 발생시 예외를 발생시키기
      위해서 입니다. 원래라면 타입 변환 기능만 가져야 하지만 변환시에 예외
      발생또한 타입 변환의 책임이라고 생각했습니다.

--- src/main/java/lotto/Utils/Convert.java
+++ src/main/java/lotto/Utils/Convert.java
@@ -5,13 +5,45 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Convert {
-    public static int convertStringToInt(String input) {
-        return Integer.parseInt(input);
+    private static final String NUMBER_RANGE = "[ERROR] 입력한 값이 유효하지 않습니다. 1에서 45 사이의 숫자만 입력할 수 있습니다.";
+    private static final String ONLY_INTEGER = "[ERROR] 정수만 입력할 수 있습니다.";
+
+    public static int convertInputPurchaseStringToInt(String input) {
+        if (!input.matches("-?\\d+")) {
+            throw new IllegalArgumentException(ONLY_INTEGER);
+        }
+
+        try {
+            return Integer.parseInt(input); // 문자열을 정수로 변환
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_INTEGER); // 변환 실패 시 예외 발생
+        }
     }
 
     public static List<Integer> convertStringToList(String input) {
-        return Arrays.stream(input.split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        if (!input.matches("^[0-9,]+$")) {
+            throw new IllegalArgumentException(ONLY_INTEGER);
+        }
+
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_INTEGER); // 변환 실패 시 예외 발생
+        }
+
+    }
+
+    public static int convertBonusNumberStringToInt(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(NUMBER_RANGE);
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ONLY_INTEGER);
+        }
     }
 }
