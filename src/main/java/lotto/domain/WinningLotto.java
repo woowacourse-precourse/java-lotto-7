7a7f6089a057f78b04801e8eac776c17bb96780e
JMJ <ghtntkdnsk@naver.com>
2024-11-04 21:21:13 +0900

    feat(WinningLotto) : 당첨 로또인 WinningLotto 생성

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,2 +1,13 @@
-package lotto.domain;public class WinningLotto {
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningLotto = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
 }
