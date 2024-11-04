2b72e053c22fd709b3792998e1e06aadd9add44a
na0th <105775683+na0th@users.noreply.github.com>
2024-11-04 14:32:33 +0900

    feat : Parser 클래스 메서드 추가
    추가한 내용 :
    1. 구분자를 단위로 split해서 String - > List<Integer>로 변환

--- src/main/java/lotto/parser/Parser.java
+++ src/main/java/lotto/parser/Parser.java
@@ -1,9 +1,17 @@
 package lotto.parser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
+    private static final String DELIMITER = ",";
 
-    public void parseInputToNumbers(String input) {
-
+    public List<Integer> parseInputToNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
     public int parseStringToInt(String input) {
         return Integer.parseInt(input);
