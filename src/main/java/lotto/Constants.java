015b6871cbfe4dd4f46dfcee0cb3d644c3bfbce7
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-04 21:21:09 +0900

    feat(Constants): add 로또 시스템의 다양한 상수 정의
    - 텍스트 출력 템플릿 상수 추가 (구매 정보, 당첨 통계, 수익률 출력 등)
    - 로또 구매 단가, 번호 범위, 구매 가능 최대 수량 등 금액 관련 상수 추가
    - 사용자 메시지 출력 템플릿 상수 추가 (구입 금액 입력, 당첨 번호 입력, 보너스 번호 입력 안내)

---
+++ src/main/java/lotto/Constants.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    // 출력 템플릿 상수
+    public static final String MATCH_TEXT_TEMPLATE = "%d개 일치 (%d원) - %d개%n";
+    public static final String PURCHASE_INFO_TEMPLATE = "%n%d개를 구매했습니다.%n";
+    public static final String WINNING_STATISTICS_HEADER = "%n당첨 통계%n---%n";
+    public static final String PROFIT_RATE_TEMPLATE = "총 수익률은 %.1f%%입니다.%n";
+
+    // 금액 관련 상수
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+    public static final long MAX_PURCHASABLE_LOTTOS = 1_000_000_000;
+    public static final int PROFIT_RATE_ROUNDING_SCALE = 2;
+
+    // 메시지 관련 상수
+    public static final String LOTTO_PURCHASE_MONEY_PROMPT = "구입 금액을 입력해 주세요.%n";
+    public static final String WINNING_NUMBER_PROMPT = "%n당첨 번호를 입력해 주세요.%n";
+    public static final String BONUS_NUMBER_PROMPT = "%n보너스 번호를 입력해 주세요.%n";
+}
