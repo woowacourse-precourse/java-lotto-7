31d947217632560699fc14a69275b6454787c078
dongwoo <woomacho@gmail.com>
2024-11-04 18:43:07 +0900

    feat: LottoController 기능 구현
    - 1. 로또 구매 2. 당첨 금액, 보너스 번호 설정 3. 로또 결과 확인에 대한 view와 service를 연결한다.
    - service에서 예외가 발생하면 예외 message를 사용자에게 출력 후 예외가 발생한 로직을 다시 수행하게 한다.

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+    private final LottoView lottoView;
+
+    protected LottoController(LottoService lottoService, LottoView lottoView) {
+        this.lottoService = lottoService;
+        this.lottoView = lottoView;
+    }
+
+    public List<Lotto> purchaseLottoTickets() {
+        String purchaseAmount = lottoView.purchaseLotto();
+        try {
+            List<Lotto> lottoResult = lottoService.purchaseLottoTickets(purchaseAmount);
+            lottoView.displayPurchasedLotto(lottoResult);
+            return lottoResult;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return purchaseLottoTickets();
+        }
+
+    }
+
+    public void setWinningAndBonusLottoNumbers() {
+        final String winningNumbers = lottoView.getWinningLottoNumbers();
+        final String bonusNumber = lottoView.getBonusLottoNumbers();
+        try {
+            lottoService.setWinningLottoStore(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            this.setWinningAndBonusLottoNumbers();
+        }
+    }
+
+    public void lottoResults(final List<Lotto> userLottoTickets) {
+        LottoResultDto lottoResult = lottoService.getLottoResults(userLottoTickets);
+        lottoView.displayLottoResults(lottoResult);
+    }
+}
