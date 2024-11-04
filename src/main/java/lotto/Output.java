545bfc1fca24b2978fdf3ef7893184d18d00fa64
Avocado <ujubongbong@gmail.com>
2024-11-04 21:32:53 +0900

    :sparkles: feat: 보너스 번호 입력 안내 출력 함수

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -18,4 +18,8 @@ public class Output {
 	public void printLuckyNumber() {
 		System.out.println("당첨 번호를 입력해 주세요.");
 	}
+
+	public void printBonusNumber() {
+		System.out.println("보너스 번호를 입력해 주세요.");
+	}
 }
