cce61f5b0b697ecde0e48aa10d3bce8988772eac
MaiBoii <20183010@gs.cwnu.ac.kr>
2024-11-04 10:26:12 +0900

    feat: 로또 당첨 결과 Rank Enum 추가
    로또 당첨 결과를 판단하는 Enum 작성

--- src/main/java/lotto/model/result/Rank.java
+++ src/main/java/lotto/model/result/Rank.java
@@ -1,4 +1,45 @@
 package lotto.model.result;
 
-public class Rank {
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank getRank(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && bonusMatch) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
 }
