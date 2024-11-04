96c98fa56565f19a8e158cad397e80b7797d43cd
aaahyunseo <hs030608@naver.com>
2024-11-04 23:08:45 +0900

    feat(Constants) : 정규 표현식에 공백 문자열 추가

--- src/main/java/service/Constants.java
+++ src/main/java/service/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public final static int LOTTO_MIN = 1;
     public final static int LOTTO_MAX = 45;
     public final static int LOTTO_COUNT = 6;
-    public final static String REGEX = "^[0-9]+(,[0-9]+)*$";
+    public final static String REGEX = "^[0-9]*(,[ ]*[0-9]*)*$";
 }
