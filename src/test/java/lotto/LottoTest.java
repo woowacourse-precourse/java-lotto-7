b90016748a3733350ec8e16c5c4880125e5edddd
cherryiJuice <chldlwn711@gmail.com>
2024-11-04 13:30:15 +0900

    test: 로또 생성 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,25 +1,71 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
+import lotto.generator.LottoGenerator;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+    private final LottoGenerator lottoGenerator = () -> List.of(1, 2, 3, 4, 5, 6);
+
+    @Test
+    void 정해진_숫자의_로또_생성_성공() {
+        Lotto lotto = Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers())
+                .isEqualTo(lotto.getNumbers());
+    }
+
+    @Test
+    void 랜점_로또_생성_성공() {
+        Lotto lotto = Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(Lotto.createRandomNumberLotto(lottoGenerator).getNumbers())
+                .isEqualTo(lotto.getNumbers());
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호의_개수가_6개가_안되면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_범위가_45보다_크면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호가_중복되면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_중복되면_예외가_발생한다() {
+        Lotto lotto = Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> lotto.validateBonusNumber("6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_숫자가_아니면_예외가_발생한다() {
+        Lotto lotto = Lotto.createFixedNumberLotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> lotto.validateBonusNumber("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
