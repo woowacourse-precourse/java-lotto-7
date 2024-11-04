dacf8e6dc4b22a7ab8d1fce01ddf365ad2ddc1b6
yush <yush2202@gmail.com>
2024-11-04 17:09:59 +0900

    test($ NumberValidatorTest): 정수 확인 테스트 추가

--- src/test/java/lotto/utils/NumberValidatorTest.java
+++ src/test/java/lotto/utils/NumberValidatorTest.java
@@ -9,11 +9,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberValidatorTest {
     @ParameterizedTest
-    @DisplayName("잘못된 범위 내의 숫자가 들어왔을시, 오류를 발생합니다.,")
+    @DisplayName("잘못된 범위 내의 숫자가 들어왔을시, 오류를 발생합니다.")
     @ValueSource(ints = {0, 46, -1, 50})
     void 숫자_범위를_확인합니다(int target) {
         assertThatThrownBy(() -> NumberValidator.validateNumberRange(target, 1, 45))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("정수가 아닌 입력이 들어왔을 경우 오류를 발생합니다.")
+    @ValueSource(strings = {" ", "\n", ".", "/", "dd", "^"})
+    void 정수인지_확인합니다(String target) {
+        assertThatThrownBy(() -> NumberValidator.validateInt(target))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_DIGIT.getMessage());
     }
 }
