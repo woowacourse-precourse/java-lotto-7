36126790d6fc2a5463b531c5ec55f093cef84f8d
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 18:08:16 +0900

    feat: LottoMatch enum 작성 (matchCount, prize, statements)

---
+++ src/main/java/lotto/domain/LottoMatch.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+public enum LottoMatch {
+    SIX_MATCH(6, 2_000_000_000, "6개 일치"),
+    FIVE_MATCH_AND_BONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIVE_MATCH(5, 1_500_000, "5개 일치"),
+    FOUR_MATCH(4, 50_000, "4개 일치"),
+    THREE_MATCH(3, 5_000, "3개 일치"),
+    NONE(6, 2_000_000_000, "6개 일치");
+
+
+    private final int matchCount;
+    private final int prize;
+    private final String statements;
+
+    LottoMatch(int matchCount, int prize, String statements) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.statements = statements;
+    }
+
+    public static LottoMatch getLottoMatch(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6)
+            return SIX_MATCH;
+        if (matchCount == 5 && bonusMatch)
+            return FIVE_MATCH_AND_BONUS;
+        if (matchCount == 5)
+            return FIVE_MATCH;
+        if (matchCount == 4)
+            return FOUR_MATCH;
+        if (matchCount == 3)
+            return THREE_MATCH;
+        return NONE;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getStatements() {
+        return statements;
+    }
+}
