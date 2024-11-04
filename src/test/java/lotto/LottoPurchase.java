7178e2348fa3f5f7729af0d97558c515471955f1
Haneul Shin <shn7980@gmail.com>
2024-11-04 12:23:05 +0900

    test: LottoPurchase 테스트 코드 작성

---
+++ src/test/java/lotto/LottoPurchase.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class LottoPurchase {
+    public LottoPurchase(String input) {
+        // 구입 금액에 따른 발행할 로또 개수 설정
+    }
+
+    public int getLottoAmount() {
+        // 발행된 로또 개수 반환
+        return 0;
+    }
+}
---
+++ src/test/java/lotto/LottoPurchaseTest.java
@@ -0,0 +1,50 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoPurchaseTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("구입 금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new LottoPurchase("5300"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE + " 1000원 단위로 입력해야 합니다.");
+    }
+
+    @DisplayName("구입 금액이 음수이면 예외가 발생한다.")
+    @Test
+    void 구입_금액이_음수이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new LottoPurchase("-5000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE + " 최소 1000원 이상 입력해야 합니다.");
+    }
+
+    @DisplayName("구입 금액이 0원이면 예외가 발생한다.")
+    @Test
+    void 구입_금액이_0원이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new LottoPurchase("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE + " 최소 1000원 이상 입력해야 합니다.");
+    }
+
+    @DisplayName("구입 금액이 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 구입_금액이_숫자가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new LottoPurchase("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE + " 숫자만 입력해야 합니다.");
+    }
+
+    @DisplayName("올바른 구입 금액을 입력하면 정확한 로또 개수가 생성된다.")
+    @Test
+    void 기능_테스트_올바른_구입_금액을_입력한다() {
+        LottoPurchase lottoPurchase = new LottoPurchase("5000");
+        assertThat(lottoPurchase.getLottoAmount()).isEqualTo(5);
+    }
+}
