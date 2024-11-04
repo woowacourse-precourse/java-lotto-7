18a117831b2adb3767f22f998e41e43919c7ccdf
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-04 18:59:12 +0900

    refactor(input): 메시지 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,8 +6,8 @@ import lotto.validate.InputValidator;
 public class InputView {
 
     private static final String AMOUNT_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_INPUT_GUIDE_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_INPUT_GUIDE_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_GUIDE_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_GUIDE_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
     private final InputValidator validator = new InputValidator();
 
