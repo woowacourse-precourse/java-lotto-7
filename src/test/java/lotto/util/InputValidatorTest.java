6368b0741f1911e703980d689783b66f2b7a4692
V4N1LLA <bubble1549@gmail.com>
2024-11-04 12:28:16 +0900

    style: 테스트 코드 리포매팅

--- src/test/java/lotto/util/InputValidatorTest.java
+++ src/test/java/lotto/util/InputValidatorTest.java
@@ -1,9 +1,10 @@
 package lotto.util;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.*;
 
 class InputValidatorTest {
 
