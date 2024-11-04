ae1ef45cbbf96bace6a87c0e8a34c13a0a92f29a
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2024-11-04 16:18:04 +0900

    fix(model) : Lotto 수정
    validate 메소드 예외처리 추가
    getNumbers() 메소드 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,15 @@
 package lotto.model;
 
+import static lotto.utils.ExceptionConstants.DUPLICATED_NUM_INPUT;
+import static lotto.utils.ExceptionConstants.NUMBER_OUT_OF_RANGE;
+import static lotto.utils.ExceptionConstants.REQUIRED_SIX_NUMBERS;
+import static lotto.utils.LottoConfig.LOTTO_MAX_NUM;
+import static lotto.utils.LottoConfig.LOTTO_MIN_NUM;
+import static lotto.utils.LottoConfig.LOTTO_SIZE;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +20,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_SIZE.getValue()) {
+            throw new IllegalArgumentException(REQUIRED_SIX_NUMBERS.getExceptionMessage());
+        }
+        for(Integer number : numbers) {
+            if (number < LOTTO_MIN_NUM.getValue() || number > LOTTO_MAX_NUM.getValue()) {
+                throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.getExceptionMessage());
+            }
+        }
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NUM_INPUT.getExceptionMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
