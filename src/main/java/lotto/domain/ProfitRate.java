733b1cd6265d07412f6684b24d007bc166194d0f
IM-GYURI <kgulr0517@gmail.com>
2024-11-04 16:02:20 +0900

    feat : ProfitRate에 getProfitRate() 추가 및 수익률 계산식 수정
    - xx.x% 로 표현해야 하기 때문에 *100.0으로 수정

--- src/main/java/lotto/domain/ProfitRate.java
+++ src/main/java/lotto/domain/ProfitRate.java
@@ -8,11 +8,15 @@ public class ProfitRate {
         this.profitRate = calculateProfitRate(money, prizeResult);
     }
 
+    public double getProfitRate() {
+        return profitRate;
+    }
+
     private double calculateProfitRate(Money money, PrizeResult prizeResult) {
         int prizeProfit = prizeResult.getSumOfProfit();
         int tryMoney = money.money();
 
-        double rawProfitRate = prizeProfit * 1.0 / tryMoney;
+        double rawProfitRate = prizeProfit * 100.0 / tryMoney;
         return Math.round(rawProfitRate * 10) / 10.0;
     }
 }
