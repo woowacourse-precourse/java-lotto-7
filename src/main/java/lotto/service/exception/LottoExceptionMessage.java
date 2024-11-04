bdc3815803a26d48b527366cffb7a2b723d73e0a
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:31:20 +0900

    Feat: 당첨 번호가 6개가 아닐 경우 예외 메시지 상수 구현

--- src/main/java/lotto/service/exception/LottoExceptionMessage.java
+++ src/main/java/lotto/service/exception/LottoExceptionMessage.java
@@ -4,6 +4,8 @@ import lotto.exception.ExceptionMessage;
 
 public enum LottoExceptionMessage implements ExceptionMessage {
 
+    // 당첨 번호
+    INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 1 이상 45 이하의 숫자 6개를 콤마로 구분하여 입력해야 합니다.")
     ;
 
     private final String message;
