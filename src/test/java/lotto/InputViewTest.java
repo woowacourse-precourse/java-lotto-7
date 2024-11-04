c7e85f7afa6a0537f4e72972f1e4daf7f72edf2c
Hajuuu <hpt0127@naver.com>
2024-11-04 18:39:19 +0900

    test: test 추가
    보너스 번호가 로또 번호와 중복일 때 예외가 발생하는지 확인하는 테스트 작성

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,12 +1,9 @@
 package lotto;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -26,7 +23,7 @@ class InputViewTest {
     @DisplayName("당첨 번호 입력 예외 발생")
     @ValueSource(strings = {"0", "46", "a", "-1", "", " "})
     void checkInputLottoNumbers(String input) {
-        assertThatThrownBy(() -> inputView.checkInputLottoNumbers(input)).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> inputView.checkWinningLottoNumbers(input)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageMatching("\\[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
@@ -34,7 +31,20 @@ class InputViewTest {
     @DisplayName("보너스 번호 입력 예외 발생")
     @ValueSource(strings = {"0", "46", "a", "-1", "1,2", " ", ""})
     void checkInputBonusNumbers(String input) {
-        assertThatThrownBy(() -> inputView.checkInputBonusNumber(input)).isInstanceOf(IllegalArgumentException.class)
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> inputView.checkInputBonusNumber(input, lotto)).isInstanceOf(
+                        IllegalArgumentException.class)
                 .hasMessageMatching("\\[ERROR] 보너스 번호는 1부터 45 사이의 숫자 중 하나를 입력해야 합니다.");
     }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호가 로또 번호가 중복일시 예외 발생")
+    @ValueSource(strings = {"1", "2", "3", "4", "5", "6"})
+    void checkDuplicateBonusNumbers(String input) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> inputView.checkInputBonusNumber(input, lotto)).isInstanceOf(
+                        IllegalArgumentException.class)
+                .hasMessageMatching("\\[ERROR] 보너스 번호는 로또와 중복되지 않는 숫자여야 합니다.");
+    }
+
 }
