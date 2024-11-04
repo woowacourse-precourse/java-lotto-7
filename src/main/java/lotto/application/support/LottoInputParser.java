6b782f143a98a50e98fb2e92cbc15ca020204452
bellmin <whdlas011203@naver.com>
2024-11-04 23:05:38 +0900

    feat(LottoInputParser): Cache 추가, validateBounusNumber 로직 추가
    - validate 할 때만 사용하는 cache 추가 (Map)
    - validateBonusNumber에 winningNumber와 겹치지 중복 확인 로직 추가

--- src/main/java/lotto/application/support/LottoInputParser.java
+++ src/main/java/lotto/application/support/LottoInputParser.java
@@ -1,23 +1,31 @@
 package lotto.application.support;
 
+import static lotto.common.Consts.CLASS_CAST_NOT_ACCEPTABLE_ERROR;
 import static lotto.common.Consts.LOTTO_NUMBER_MUST_BE_BETWEEN_ONE_FORTY_FIVE_ERROR;
 import static lotto.common.Consts.WINNING_NUMBER_NOT_DUPLICATED_ERROR;
 import static lotto.common.Consts.INPUT_MUST_BE_NUMBER_ERROR;
 import static lotto.common.Consts.INPUT_MUST_BE_POSITIVE_NUMBER_ERROR;
 import static lotto.common.Consts.LOTTO_PRICE;
 import static lotto.common.Consts.PURCHASE_AMOUNT_1000_UNIT_ERROR;
+import static lotto.common.Consts.WINNING_NUMBER_SIZE_MUST_BE_6_ERROR;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lotto.common.Errors;
 
 public class LottoInputParser {
+
+    private Map<String, Object> cache = new HashMap<>();
 
     public int parseAmount(String amount){
         try{
             int parsed = Integer.parseInt(amount);
             validateAmount(parsed);
+
+            registerInCache("amount", parsed);
 
             return parsed;
         } catch (NumberFormatException e) {
@@ -40,6 +48,9 @@ public class LottoInputParser {
         try {
             List<Integer> winningNumbers = winningNumbertoList(winningNumber);
             validateWinningNumber(winningNumbers);
+
+            registerInCache("winningNumber", winningNumbers);
+
             return winningNumbers;
         } catch (IllegalArgumentException e){
             return Collections.emptyList();
@@ -60,14 +71,19 @@ public class LottoInputParser {
         if(winningNumbers.stream().distinct().count() != winningNumbers.size()){
             Errors.IllegalArgumentException(WINNING_NUMBER_NOT_DUPLICATED_ERROR);
         }
+
+        if(winningNumbers.size()!=6){
+            Errors.IllegalArgumentException(WINNING_NUMBER_SIZE_MUST_BE_6_ERROR);
+        }
     }
 
     public int parseBonusNumber(String bonusNumber){
         int parsed;
         try{
             parsed = Integer.parseInt(bonusNumber);
-
             validateBonusNumber(parsed);
+
+            registerInCache("bonusNumber", parsed);
 
             return parsed;
         } catch (NumberFormatException e) {
@@ -80,5 +96,22 @@ public class LottoInputParser {
         if(bonusNumber < 0 || bonusNumber > 45){
             Errors.IllegalArgumentException(LOTTO_NUMBER_MUST_BE_BETWEEN_ONE_FORTY_FIVE_ERROR);
         }
+
+        if(!isListInstance("winningNumber")){
+            Errors.ClassCastException(CLASS_CAST_NOT_ACCEPTABLE_ERROR);
+        }
+
+        if(((List<Integer>)cache.get("winningNumber")).contains(bonusNumber)){
+            Errors.IllegalArgumentException(WINNING_NUMBER_NOT_DUPLICATED_ERROR);
+        }
     }
+
+    private void registerInCache(String key, Object value){
+        this.cache.put(key, value);
+    }
+
+    private boolean isListInstance(String key){
+        return cache.get(key) instanceof List<?>;
+    }
+
 }
