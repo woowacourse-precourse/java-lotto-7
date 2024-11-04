45df7a488392d5ac13fc109a7d4ebf4e68526692
Hyunuk <dksgusdnr17@naver.com>
2024-11-04 20:23:02 +0900

    feat(sevice): add method to check winning result for lotto

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -46,4 +46,33 @@ public class LottoService {
                 .sorted()
                 .toList();
     }
+
+    public Map<Rank, Integer> checkWinning(List<Lotto> tickets, List<Integer> winningNumbers, Integer bonusNumber) {
+        initWinCount();
+        for (Lotto ticket : tickets) {
+            List<Integer> ticketNumbers = ticket.getNumbers();
+            long matchCount = ticketNumbers.stream()
+                    .filter(winningNumbers::contains)
+                    .count();
+            boolean bonusMatched = ticketNumbers.contains(bonusNumber);
+
+            updateWinCount(matchCount, bonusMatched);
+        }
+
+        return new HashMap<>(winCount);
+    }
+
+    private void updateWinCount(long matchCount, boolean bonusMatched) {
+        if (matchCount == 6) {
+            winCount.put(Rank.FIRST_PLACE, winCount.get(Rank.FIRST_PLACE) + 1);
+        } else if (matchCount == 5 && bonusMatched) {
+            winCount.put(Rank.SECOND_PLACE, winCount.get(Rank.SECOND_PLACE) + 1);
+        } else if (matchCount == 5) {
+            winCount.put(Rank.THIRD_PLACE, winCount.get(Rank.THIRD_PLACE) + 1);
+        } else if (matchCount == 4) {
+            winCount.put(Rank.FOURTH_PLACE, winCount.get(Rank.FOURTH_PLACE) + 1);
+        } else if (matchCount == 3) {
+            winCount.put(Rank.FIFTH_PLACE, winCount.get(Rank.FIFTH_PLACE) + 1);
+        }
+    }
 }
