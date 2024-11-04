5db0046b1812e5743f5d613b2db05408720bb135
GrO0vy <lmc2819@naver.com>
2024-11-04 10:14:13 +0900

    feat: 출력 메세지를 관리하는 열거 클래스 추가

---
+++ src/main/java/lotto/enums/OutputMessage.java
@@ -0,0 +1,18 @@
+package lotto.enums;
+
+public enum OutputMessage {
+    LOTTO_COUNT("%d개를 구매했습니다.\n"),
+    WINNING_HISTORY_TITLE("당첨 통계\n---"),
+    WINNING_HISTORY("%d개 일치 (%,d원) - %d개\n"),
+    TOTAL_RATE_OF_RETURN("총 수익률은 %.1f%%입니다.");
+
+    private final String message;
+
+    private OutputMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
