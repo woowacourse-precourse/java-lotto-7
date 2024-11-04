e8f3ff7100c18a1c4f80c375843cf9042e1f859d
dnjswns98 <tngkrt2004@gmail.com>
2024-11-04 19:22:17 +0900

    fix(LottoRank): Modify LottoResult logic
    Modifying logic to handle Lotto 2nd place correctly

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,21 +3,31 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    MISS(0, false, 0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
     private final int matchCount;
     private final boolean matchBonus;
     private final int prize;
 
-    LottoRank(int matchCount, boolean matchBonus, int prize) {
+    LottoRank(int matchCount, int prize) {
+        this(matchCount, prize, false);
+    }
+
+    LottoRank(int matchCount, int prize, boolean matchBonus) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prize = prize;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount && (!rank.matchBonus || matchBonus))
+                .findFirst() .orElse(MISS);
     }
 
     public int getPrize() {
@@ -30,15 +40,5 @@ public enum LottoRank {
 
     public boolean getMatchBonus() {
         return matchBonus;
-    }
-
-    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
-        return Arrays.stream(values())
-                .filter(rank -> rank.matchCount == matchCount && rank.matchBonus == matchBonus)
-                .findFirst()
-                .orElseGet(() -> {
-                    if (matchCount == 5) return SECOND;
-                    return MISS;
-                });
     }
 }
