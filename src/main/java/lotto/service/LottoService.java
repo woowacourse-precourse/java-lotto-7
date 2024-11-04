47f2d635501776571a0b2647ee8af6dae0e2faf2
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:53:40 +0900

    Feat: 입력받은 가격으로 Customer 객체 생성 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import lotto.domain.Customer;
 import lotto.domain.Price;
 import lotto.exceptionHandler.ExceptionHandler;
 import lotto.view.InputView;
@@ -7,11 +8,16 @@ import lotto.view.InputView;
 public class LottoService {
 
     private Price price;
+    private Customer customer;
 
     public void inputPrice() {
         ExceptionHandler.handle(() -> {
             int value = InputView.inputPrice();
             price = Price.from(value);
         });
+    }
+
+    public void initCustomer() {
+        customer = Customer.from(price.getValue());
     }
 }
