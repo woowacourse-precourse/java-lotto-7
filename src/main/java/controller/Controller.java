66e81f528e43ef47de29a797fed779ce8184396d
Cmk1031 <maple1031@naver.com>
2024-11-04 19:29:35 +0900

    feat: 로또 구매 입출력 작성

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,26 @@
+package controller;
+
+import domain.User;
+import view.InputView;
+import view.OutputView;
+
+public class Controller {
+    User user;
+
+    public void run() {
+        buying();
+    }
+
+    private void buying() {
+        OutputView.outBuyingPriceView(); //구입 금액을 입력해주세요.
+        try {
+            user = new User(InputView.inputBuyingPriceView());
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 정수만 입력 가능합니다.");
+            buying();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 1000원 단위만 입력 가능합니다.");
+            buying();
+        }
+    }
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,9 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String inputBuyingPriceView() {
+        return Console.readLine();
+    }
+}
