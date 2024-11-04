03eb8eac492003bca2c22d92fcae1562b00fca68
EgPwl <egpwl07@gmail.com>
2024-11-04 20:56:28 +0900

    feat: 당첨 번호와 로또 번호 일치 횟수

--- src/main/java/lotto/domain/ProfitCalculator.java
+++ src/main/java/lotto/domain/ProfitCalculator.java
@@ -2,17 +2,30 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProfitCalculator {
 
     public Map<Ranking, Integer> getWinningInfo(Lottos lottos, WinningBonus winningBonus) {
         Map<Ranking, Integer> winningInfo = generateWinningInfo();
+        for (Lotto lotto : lottos.getLottos()) {
+            int matchingCount = matchLottoAndWinning(lotto, winningBonus);
+        }
     }
 
-    public Map<Ranking, Integer> generateWinningInfo() {
+    private Map<Ranking, Integer> generateWinningInfo() {
         Map<Ranking, Integer> winningInfo = new EnumMap<>(Ranking.class);
         Arrays.stream(Ranking.values()).forEach(ranking -> winningInfo.put(ranking, 0));
         return winningInfo;
     }
+
+    private int matchLottoAndWinning(Lotto lotto, WinningBonus winningBonus) {
+        List<Integer> numbers = lotto.getNumbers();
+        List<Integer> winningNumbers = winningBonus.getWinningNumbers();
+        return (int)numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
 }
