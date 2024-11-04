5a8c3fdeec052bcda906285a1a2356cfb71baa90
euni <kongeuni1004@gmail.com>
2024-11-04 14:53:34 +0900

    feat: 당첨 통계 및 수익률 출력 메서드 추가
    - `printStatistics` 메서드를 통해 당첨 결과 통계 출력 기능 구현
    - 당첨 결과에 따른 3~6개 일치 통계 출력
    - `printRateOfReturn` 메서드를 통해 총 수익률 계산 및 출력 기능 구현

--- src/main/java/lotto/PrintController.java
+++ src/main/java/lotto/PrintController.java
@@ -45,8 +45,17 @@ public class PrintController {
     //당첨통계
     public void printStatistics(List<Lotto> lottos, WinNumbers winNumbers) {
         System.out.println("당첨 통계\n---");
-
+        LottoResult lottoResult = lottoController.calculateWinnings(lottos, winNumbers);
+        System.out.printf("3개 일치 (5,000원) - %d개\n",lottoResult.rankCount()[4]);
+        System.out.printf("4개 일치 (50,000원) - %d개\n",lottoResult.rankCount()[3]);
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n",lottoResult.rankCount()[2]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",lottoResult.rankCount()[1]);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n",lottoResult.rankCount()[0]);
     }
 
     //수익률
+    public void printRateOfReturn(LottoResult lottoResult, int purchaseNumber) {
+        double rateOfReturn = lottoController.calculateRateOfReturn(lottoResult.totalWinnings(),purchaseNumber);
+        System.out.printf("총슈익률은 %f입니다.",rateOfReturn);
+    }
 }
