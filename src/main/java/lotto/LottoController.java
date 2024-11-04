0e800ba5d08034111cc0d22cecf85402235d429a
seyeon22 <sywolleych@gmail.com>
2024-11-04 16:00:44 +0900

    feat: 함수 전체 실행 위한 컨트롤러 생성

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,4 +1,34 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class LottoController {
+    public void run() {
+        // 구입 금액 입력
+        PriceInput priceInput = new PriceInput();
+        int price = priceInput.inputPrice();
+
+        // 랜덤 로또 번호 출력
+        LottoNumbersPrint lottoNumbersPrint = new LottoNumbersPrint();
+        List<List<Integer>> lottoNumberLists = lottoNumbersPrint.printLottoNumbers(price);
+
+        // 당첨 번호 입력 받기
+        WinningNumbersInput winningNumbersInput = new WinningNumbersInput();
+        Lotto lotto = winningNumbersInput.inputNumbers();
+
+        // 보너스 번호 입력 받기
+        BonusNumberInput bonusNumberInput = new BonusNumberInput();
+        int bonusNumber = bonusNumberInput.inputBonusNumber(lotto.getNumbers());
+
+        // 당첨 결과 계산
+        LottoCompare lottoCompare = new LottoCompare();
+        List<Integer> result = lottoCompare.compareLottoNumbers(lotto.getNumbers(), lottoNumberLists, bonusNumber);
+        Map<WinningType, Integer> counts = lottoCompare.storeNumbers(result);
+
+        // 당첨 내역 출력
+        WinningDetailsDisplay winningDetailsDisplay = new WinningDetailsDisplay();
+        winningDetailsDisplay.printWinningDetails(counts);
+        winningDetailsDisplay.printRate(price, counts);
+    }
 }
--- src/main/java/lotto/WinningDetailsDisplay.java
+++ src/main/java/lotto/WinningDetailsDisplay.java
@@ -1,7 +1,5 @@
 package lotto;
 
-import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
 
 public class WinningDetailsDisplay {
