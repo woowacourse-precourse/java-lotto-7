de3693fdc9ee6392a9c1bffa3ada0976d5489344
dk7648 <dk7648@korea.ac.kr>
2024-11-04 14:22:10 +0900

    refactor: 문자열 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,29 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import lotto.model.Result;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    private static final String REQUEST_MONEY = "구입금액을 입력해 주세요";
+    private static final String REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String STATISTICS_HEADER = "당첨통계";
+    private static final String STATISTICS_DIVIDER = "'---";
+    private static final String ERROR_MONEY = "[ERROR] : 구입금액이 잘못 입력 되었습니다.";
+    private static final String ERROR_WINNING_NUMBERS = "[ERROR] : 당첨 번호가 잘못 입력 되었습니다.";
+    private static final String ERROR_BONUS_NUMBER = "[ERROR] : 보너스 번호가 잘못 입력 되었습니다.";
+    private static final String LOTTO_BOUGHT_MESSAGE = "개를 구매했습니다.";
+    private static final String STATISTICS_MESSAGE = "%s (%s) - %d개\n";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
+
     public static void requestMoney() {
-        System.out.println("구입금액을 입력해 주세요");
+        System.out.println(REQUEST_MONEY);
     }
 
     public static void printLottos(List<Lotto> lottos) {
@@ -19,7 +34,7 @@ public class OutputView {
     }
 
     private static void printNumberOfLotto(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매했습니다.");
+        System.out.println(lottos.size() + LOTTO_BOUGHT_MESSAGE);
     }
 
     public static void printNumbers(List<Integer> numbers) {
@@ -30,36 +45,45 @@ public class OutputView {
     }
 
     public static void requestWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(REQUEST_WINNING_NUMBERS);
     }
 
     public static void requestBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(REQUEST_BONUS_NUMBER);
     }
 
     public static void printStastistics(Map<String, Integer> statistics) {
-        System.out.println("당첨통계");
-        System.out.println("'---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", statistics.getOrDefault("3개 일치", 0));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", statistics.getOrDefault("4개 일치", 0));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", statistics.getOrDefault("5개 일치", 0));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", statistics.getOrDefault("5개 일치, 보너스 볼 일치", 0));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", statistics.getOrDefault("6개 일치", 0));
+        System.out.println(STATISTICS_HEADER);
+        System.out.println(STATISTICS_DIVIDER);
+        printResultInfo(statistics, Result.FIFTH);
+        printResultInfo(statistics, Result.FOURTH);
+        printResultInfo(statistics, Result.THIRD);
+        printResultInfo(statistics, Result.SECOND);
+        printResultInfo(statistics, Result.FIRST);
+    }
+
+    private static void printResultInfo(Map<String, Integer> statistics, Result nResult) {
+        System.out.printf(STATISTICS_MESSAGE, nResult.message, printFormattedAmount(nResult.winningAmount), statistics.getOrDefault(nResult.message, 0));
+    }
+
+    private static String printFormattedAmount(int amount) {
+        NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+        return formatter.format(amount) + "원";
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", rateOfReturn);
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
     }
 
     public static void printErrorOfRequestMoney() {
-        System.out.println("[ERROR] : 구입금액이 잘못 입력 되었습니다.");
+        System.out.println(ERROR_MONEY);
     }
 
     public static void printErrorOfRequestNumbers() {
-        System.out.println("[ERROR] : 당첨 번호가 잘못 입력 되었습니다.");
+        System.out.println(ERROR_WINNING_NUMBERS);
     }
 
     public static void printErrorOfRequestNumber() {
-        System.out.println("[ERROR] : 보너스 번호가 잘못 입력 되었습니다.");
+        System.out.println(ERROR_BONUS_NUMBER);
     }
 }
