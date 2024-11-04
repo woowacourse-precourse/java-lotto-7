dbf95f56992575a32473cce4e9ae4d556efebd4b
haebyun <bhk4392@gmail.com>
2024-11-04 14:14:24 +0900

    feat(ConsoleInputView): 콘솔 입력을 처리하는 구현 클래스 추가
    - 구매 금액 입력을 위한 메시지와 입력 처리 메서드 inputPurchaseAmount 추가
    - 당첨 로또 번호 입력을 위한 메시지와 입력 처리 메서드 inputWinningLottoNumbers 추가
    - 보너스 번호 입력을 위한 메시지와 입력 처리 메서드 inputWinningLottoBonusNumber 추가
    - 입력된 값을 InputParser로 파싱하여 DTO로 변환 후 반환

---
+++ src/main/java/lotto/view/input/ConsoleInputView.java
@@ -0,0 +1,43 @@
+package lotto.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.dto.request.PurchaseAmountDTO;
+import lotto.dto.request.WinningLottoBonusNumberDTO;
+import lotto.dto.request.WinningLottoNumbersDTO;
+import lotto.view.input.util.InputParser;
+
+public class ConsoleInputView implements InputView {
+    private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_WINNING_LOTTO_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    @Override
+    public PurchaseAmountDTO inputPurchaseAmount() {
+        System.out.println(REQUEST_PURCHASE_AMOUNT);
+        String amountInput = Console.readLine();
+        System.out.println();
+        return PurchaseAmountDTO.of(
+                InputParser.parsePurchaseAmount(amountInput)
+        );
+    }
+
+    @Override
+    public WinningLottoNumbersDTO inputWinningLottoNumbers() {
+        System.out.println(REQUEST_WINNING_LOTTO_NUMBERS);
+        String lottoNumbersInput = Console.readLine();
+        System.out.println();
+        return WinningLottoNumbersDTO.of(
+                InputParser.parseWinningLottoNumbers(lottoNumbersInput)
+        );
+    }
+
+    @Override
+    public WinningLottoBonusNumberDTO inputWinningLottoBonusNumber() {
+        System.out.println(REQUEST_WINNING_LOTTO_BONUS_NUMBER);
+        String bonusNumberInput = Console.readLine();
+        System.out.println();
+        return WinningLottoBonusNumberDTO.of(
+                InputParser.parseWinningLottoBonusNumber(bonusNumberInput)
+        );
+    }
+}
