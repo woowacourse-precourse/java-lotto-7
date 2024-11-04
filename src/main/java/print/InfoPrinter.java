ee042bc01a8a8eaf52e01ba592e590bfcbcd316a
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-04 14:17:22 +0900

    feat(정상 정보 출력 기능): add info printer code.
    add infor printer code. that print the code to help user can show a result

--- src/main/java/print/InfoPrinter.java
+++ src/main/java/print/InfoPrinter.java
@@ -1,19 +1,40 @@
 package print;
 
 import java.util.List;
+import lotto.Lotto;
 
 public class InfoPrinter {
-    public void purchasedLottoInfoPrint(int sum, List<List<Integer>> lottos) {
+    public void purchasedLottoInfoPrint(int sum, List<Lotto> lottos) {
         System.out.println(sum + "개를 구매했습니다");
-        for (List<Integer> lotto : lottos) {
-            System.out.println(lotto);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
         }
     }
 
     /**
      * 차후에 변경할 메서드입니다. 존재의 의미를 잊지 않기 위해 임시로 구현해두었습니다.
      */
-    public void winningResultPrint(int matchCount, boolean isBonusMatch) {
-        System.out.println(matchCount + "개 일치" + (isBonusMatch ? ", 보너스 볼 일치" : ""));
+    public void winningResultPrint(List<Integer> results) {
+        for (int i = 6; i > 0; i--) {
+            revenue(i, results.get(i));
+        }
+    }
+
+    private void revenue(int rank, int count) {
+        if (rank == 1) {
+            System.out.println("6개 일치 (2,000,000,000원) - " + count + "개");
+        }
+        if (rank == 2) {
+            System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + count + "개");
+        }
+        if (rank == 3) {
+            System.out.println("5개 일치 (1,500,000원) - " + count + "개");
+        }
+        if (rank == 4) {
+            System.out.println("4개 일치 (50,000원) - " + count + "개");
+        }
+        if (rank == 5) {
+            System.out.println("3개 일치 (5,000원) - " + count + "개");
+        }
     }
 }
