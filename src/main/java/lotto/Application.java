96b59f15f1895a59726d20295aaf38489c4b0c96
DongHoon Lee <dhl1924@naver.com>
2024-11-04 14:18:31 +0900

    feat(controller): LottoController와 Application 초기 설정
    - `LottoController` 생성자에서 `InputView`와 `OutputView` 주입을 통해 입력 및 출력 의존성 주입 설정
    - `Application`의 `main` 메서드에서 `AppConfig` 싱글톤을 통해 `LottoController` 생성 및 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import config.AppConfig;
+import controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = AppConfig.getInstance();
+        LottoController lottoController = appConfig.lottoController();
+        lottoController.run();
     }
 }
