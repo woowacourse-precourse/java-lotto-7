08208a1a3ae90041fa6a8b7910daadfbdc827f0b
20160295 <dorem96@icloud.com>
2024-11-04 22:14:37 +0900

    feat: 로또 번호 생성
    - 로또 번호를 생성
    - 사용자가 입력한 금액에 맞춰 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,26 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoDTO;
+import lotto.model.LottoRank;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoController {
+    public void run() {
+        int purchaseAmount = InputView.getPurchaseAmount();
+        int numberOfLottos = purchaseAmount / 1000;
+        List<Lotto> lottos = generateLottos(numberOfLottos);
+    }
+
+    public List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return lottos;
+    }
+}
