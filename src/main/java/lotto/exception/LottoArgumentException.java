6179276bfc56b2fc4e5af27d20be6c1f37e5e0a6
huhdy32 <mong3268@gmail.com>
2024-11-04 18:58:04 +0900

    refactor(LottoArgumentException): 문자열 포맷으로 수정

--- src/main/java/lotto/exception/LottoArgumentException.java
+++ src/main/java/lotto/exception/LottoArgumentException.java
@@ -2,9 +2,9 @@ package lotto.exception;
 
 public class LottoArgumentException extends IllegalArgumentException {
 
-    private static final String PREFIX = "[ERROR] ";
+    private static final String ERROR_FORMAT = "[ERROR] %s";
 
     public LottoArgumentException(final String message) {
-        super(PREFIX + message);
+        super(String.format(ERROR_FORMAT, message));
     }
 }
