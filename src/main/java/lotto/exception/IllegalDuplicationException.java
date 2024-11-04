9d27a1c958379c41fcc5479161d38e30a05c71a2
gorapaduckoo <jioung93@naver.com>
2024-11-04 18:04:50 +0900

    fix: IllegalDuplicationException이 IllegalArgumentException을 상속하도록 수정

--- src/main/java/lotto/exception/IllegalDuplicationException.java
+++ src/main/java/lotto/exception/IllegalDuplicationException.java
@@ -1,6 +1,6 @@
 package lotto.exception;
 
-public class IllegalDuplicationException extends RuntimeException {
+public class IllegalDuplicationException extends IllegalArgumentException {
     public IllegalDuplicationException(String message) {
         super(message);
     }
