1145cc1f594cb68f6f36fe692048e5b29dcaa36d
JinHyung-Park <trupjh@gmail.com>
2024-11-04 20:56:01 +0900

    feat(outputView) : 통계 결과 출력 메소드 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,14 @@ public class OutputView {
     public void printPurchaseResult(int lottoCount) {
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
+
+    public void printLottoResult(String result) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(result);
+    }
+
+    public void printWinningPercent(double percent) {
+        System.out.println("총 수익률은 " + percent + "%입니다.");
+    }
 }
