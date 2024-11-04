21537cf5bdd600ca8bfadaf5033ed796876a0db2
SuyeonShinnnn <suyeons5536@gmail.com>
2024-11-04 14:48:17 +0900

    test: 구매 로또 번호와 당첨 로또 번호가 일치하는 개수 리턴하는 함수 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,13 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @Test
@@ -20,6 +24,23 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @Test
+    void getMatchedSize(){
+
+        // 주어진 로또 번호
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        // 당첨 번호
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 7, 8, 9);
+
+        // 구매 로또 번호와 당첨 번호가 일치하는 개수
+        int matchedSize = lotto.getMatchedSize(winningNumbers);
+
+        // 예상 결과 (1, 2, 3 일치 => 예상 결과 3)
+        assertEquals(3, matchedSize);
+    }
+
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
