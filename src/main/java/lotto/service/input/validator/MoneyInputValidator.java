8b3ad245b503ec1ca92002046ee928db2538ac5b
sytTest <syt1128@naver.com>
2024-11-04 18:37:59 +0900

    feat(MoneyInputValidator) 입력 받은 돈 값 검증 기능 추가
    잘못된 값 입력 시 예외 발생

---
+++ src/main/java/lotto/service/input/validator/MoneyInputValidator.java
@@ -0,0 +1,40 @@
+package lotto.service.input.validator;
+
+import java.math.BigInteger;
+
+public class MoneyInputValidator extends CommonValidator implements InputValidatorService {
+
+    @Override
+    public void validate(String input) {
+        super.validate(input);
+        noneNumericCharsExist(input);
+        tooMuchMoneyToBuy(input);
+        inputZero(input);
+        changeExist(input);
+    }
+
+    private void noneNumericCharsExist (String input) {
+        if(!input.matches("^[0-9]+")){
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 문자가 섞여 있습니다.");
+        }
+    }
+
+    private void tooMuchMoneyToBuy(String input) {
+        BigInteger inputMoney = new BigInteger(input);
+        if(inputMoney.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0){
+            throw new IllegalArgumentException("[ERROR] 범위를 넘어가는 숫자가 입력되었습니다.");
+        }
+    }
+
+    private void inputZero(String input) {
+        if(input.equals("0")){
+            throw new IllegalArgumentException("[ERROR] 0을 입력 하셨습니다.");
+        }
+    }
+
+    private void changeExist(String input) {
+        if(Long.parseLong(input) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 나누어 떨어지는 값을 입력해야 합니다.");
+        }
+    }
+}
