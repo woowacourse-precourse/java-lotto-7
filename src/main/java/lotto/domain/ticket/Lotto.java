497b8ada8ee1cbf81f51f1903b201b4525831ee4
hootzz <wjqwjql3@gmail.com>
2024-11-04 19:18:18 +0900

    refactor(ticket): Lotto class의 검증 로직을 제거

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -1,41 +1,17 @@
 package lotto.domain.ticket;
 
+import lotto.validator.LottoValidator;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validateLottoNumbers(numbers);
         this.numbers = new ArrayList<>(numbers);
         sort();
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-        validateDuplicate(numbers);
-        validateRange(numbers);
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
-        }
-    }
-
-    private void validateRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-        }
     }
 
     private void sort() {
@@ -48,8 +24,8 @@ public class Lotto {
 
     public int countMatch(Lotto other) {
         int matchCount = 0;
-        for (int number : this.numbers) {
-            if (other.numbers.contains(number)) {
+        for (int number : numbers) {
+            if (other.contains(number)) {
                 matchCount++;
             }
         }
