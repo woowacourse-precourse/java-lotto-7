483c61ee651ee560cffdf44cc807a4d93e1998f8
YujinJuhn <holsya@naver.com>
2024-11-04 20:37:35 +0900

    feat: main 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        LottoMachine lottoMachine = new LottoMachine();
+
+        ResultView.displayRequestPurchaseAmount();
+        int purchaseAmount = inputView.inputPurchaseAmount();
+        ResultView.displayPurchasedLottoCount(purchaseAmount/1000);
+
+        lottoMachine.generateLottos(purchaseAmount);
+        List<Lotto> myLottos = lottoMachine.getMyLottos();
+        ResultView.displayMyLottos(myLottos);
+
+        ResultView.displayRequestWinningNumbers();
+        List<Integer> winningNumbers = inputView.inputWinningNumbers();
+
+        ResultView.displayRequestBonusNumber();
+        int bonusNumber = inputView.inputBonusNumber(winningNumbers);
+
+        List<Prize> results = lottoMachine.calculateStatistics(myLottos, winningNumbers, bonusNumber);
+        ResultView.displayStatistics(results);
+
+        double profitRate = lottoMachine.calculateProfitRate(results, purchaseAmount);
+        ResultView.displayProfitRate(profitRate);
     }
 }
