93ecd35f6964a1987e35454c25b49c6acb3c0c1a
양효빈 <hyobin3026@naver.com>
2024-11-04 18:41:55 +0900

    refactor(Application): Refactor code to close Console

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,13 +3,15 @@ package lotto;
 
 import lotto.config.AppConfig;
 import lotto.controller.LottoController;
+import lotto.view.ConsoleInputProvider;
 
 public class Application {
     public static void main(String[] args) {
-
+        ConsoleInputProvider inputProvider = new ConsoleInputProvider();
         AppConfig appConfig = new AppConfig();
         LottoController controller = appConfig.createLottoController();
         controller.startLotto();
+        inputProvider.closeConsole();
 
     }
 }
