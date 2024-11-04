4e8007ff09ce067eab7fd782e428a1b7601ab919
okod <okodee@naver.com>
2024-11-04 20:13:34 +0900

    test: 보너스 번호 중복 테스트 추가

--- src/test/java/lotto/LottoViewTest.java
+++ src/test/java/lotto/LottoViewTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,23 @@ class LottoViewTest {
                 .isInstanceOf(IllegalArgumentException.class);
 
         System.setIn(originalIn);
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
+        // given
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 1;
+
+        // then
+        assertThatThrownBy(() -> validateBonusNumber(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
     }
 }
