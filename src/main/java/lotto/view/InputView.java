207c8f0a2669c91e87ea31d4169d530e054483ce
Hyun0828 <spdlqj4818@naver.com>
2024-11-04 15:15:04 +0900

    chore(inputView): 주석 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,6 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Scanner;
 
 public class InputView {
 
@@ -9,23 +8,19 @@ public class InputView {
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    private static final Scanner scanner = new Scanner(System.in);
 
     public String enterPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return Console.readLine();
-//        return scanner.nextLine();
     }
 
     public String enterWinningNumber() {
         System.out.println(WINNING_NUMBER_MESSAGE);
         return Console.readLine();
-//        return scanner.nextLine();
     }
 
     public String enterBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return Console.readLine();
-//        return scanner.nextLine();
     }
 }
