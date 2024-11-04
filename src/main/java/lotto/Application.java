c4480a4fd0a8cbaa05825f36ae7a346170860c60
egaeng09 <egaeng09@gmail.com>
2024-11-04 22:01:13 +0900

    refactor(Application): controller 의존성 주입 Application에서 하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.Cashier;
+import lotto.model.LottoManager;
+import lotto.model.LottoResult;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoManager lottoManager = new LottoManager();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoResult lottoResult = new LottoResult();
+        Cashier cashier = new Cashier();
+
+        LottoController lottoController = new LottoController(inputView, outputView, lottoManager, lottoResult, cashier);
+        lottoController.run();
     }
 }
