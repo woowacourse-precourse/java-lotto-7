167ce53919dd8247388e1485d8972dfedc50c90c
MinseokGo <rhalstjr1999@naver.com>
2024-11-04 13:14:49 +0900

    refactor: 당첨 등수 판별 로직 분리

--- src/main/java/lotto/LottoReferee.java
+++ src/main/java/lotto/LottoReferee.java
@@ -10,10 +10,7 @@ public class LottoReferee {
         this.lottoManager = lottoManager;
     }
 
-    public void judgeWinning() {
-        final List<WinningStatus> winningStatuses = lottoManager.compareWinningLotto();
-        final List<WinningRank> winningRanks = winningStatuses.stream()
-                .map(WinningStatus::getWinningRank)
-                .toList();
+    public List<WinningStatus> judgeWinning() {
+        return lottoManager.compareWinningLotto();
     }
 }
