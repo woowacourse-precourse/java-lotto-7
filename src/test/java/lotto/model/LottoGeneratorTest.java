14f5a2b51c8a69ae22523e69ce7fea8cba2fc565
GrO0vy <lmc2819@naver.com>
2024-11-04 10:43:51 +0900

    test: NULL 값 테스트 케이스 추가

--- src/test/java/lotto/model/LottoGeneratorTest.java
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -3,6 +3,7 @@ package lotto.model;
 import lotto.entity.Price;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,6 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoGeneratorTest {
     // 로또 번호 생성 기능 ( LottoGenerator.java ) 테스트
     private final int UNIT_PRICE = 1000;
+
+    @DisplayName("[LottoGeneratorTest] 구입 금액에 빈 값이 입력되면 예외가 발생한다")
+    @ParameterizedTest
+    @NullSource
+    void 구입_금액에_빈_값이_입력되면_예외가_발생한다(String input){
+        assertThatThrownBy(() -> new LottoGenerator(new Price(input)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("[LottoGeneratorTest] 구입 금액에 숫자 외의 문자가 있으면 예외가 발생한다")
     @ParameterizedTest
