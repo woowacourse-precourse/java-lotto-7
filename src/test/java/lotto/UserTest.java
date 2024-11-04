6433d5a19d15ecf9e014a020c781e560058830f4
Hajuuu <hpt0127@naver.com>
2024-11-04 16:29:28 +0900

    test: test 추가
    등수별 당첨 개수 계산 기능을 확인하는 테스트 구현

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.entry;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,18 @@ class UserTest {
     @Test
     void createLotto() {
         assertThat(user.createLotto(10).size()).isEqualTo(10);
+    }
+
+    @DisplayName("당첨 개수 계산")
+    @Test
+    void countRank() {
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(12, 13, 14, 15, 23, 26)),
+                new Lotto(List.of(5, 7, 8, 19, 20, 45)), new Lotto(List.of(1, 2, 3, 4, 7, 9)));
+        Lotto winnigNumbers = new Lotto(List.of(1, 2, 3, 4, 9, 10));
+        int bonusNumber = 7;
+
+        assertThat(user.countRank(lottos, winnigNumbers, bonusNumber)).hasSize(3).contains(entry(Rank.SECOND, 1))
+                .contains(entry(Rank.FOURTH, 1))
+                .contains(entry(Rank.ZERO, 2));
     }
 }
