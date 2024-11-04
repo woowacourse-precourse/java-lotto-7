1f7e250b7c1c50e61b8e6dfed9d77b3720930242
byeongdoohan <quden04@gmail.com>
2024-11-04 13:01:47 +0900

    feat: 보너스 번호 생성 기능 추가

--- src/main/java/lotto/generator/LottoGenerator.java
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -7,9 +7,14 @@ public class LottoGenerator {
     private static final int START_NUM = 1;
     private static final int END_NUM = 45;
     private static final int COUNT_NUM = 6;
+    private static final int BONUS_NUM = 1;
 
     public List<Integer> getLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, COUNT_NUM);
+    }
+
+    public int getBonusNumber(){
+        return Randoms.pickNumberInRange(START_NUM, END_NUM);
     }
 
     private LottoGenerator(){
