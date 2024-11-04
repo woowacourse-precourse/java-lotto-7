2084a2ca936e7f20ff893740021b708a9301b222
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 14:04:07 +0900

    feat(LottoGame): 구매한 로또와 당첨 번호를 비교하여 당첨 결과 계산 기능 추가
    - LottoGame 클래스에 calculateResult 메서드 구현
    - 구매한 로또와 당첨 번호를 비교하여 당첨 등수를 결정하고 LottoResult에 저장

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,11 +3,22 @@ package lotto;
 import java.util.List;
 
 public class LottoGame {
-    private final List<Integer> winningNumber;
+    private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public LottoGame(List<Integer> winningNumber, int bonusNumber) {
-        this.winningNumber = winningNumber;
+    public LottoGame(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningLotto = new Lotto(winningNumbers);
         this.bonusNumber = bonusNumber;
+    }
+
+    public LottoResult calculateResult(List<Lotto> purchasedLottos) {
+        LottoResult result = new LottoResult();
+        for (Lotto lotto : purchasedLottos) {
+            int matchCount = lotto.getMatchCount(winningLotto);
+            boolean bonusMatch = lotto.containsNumber(bonusNumber);
+            Rank rank = Rank.valueOf(matchCount, bonusMatch);
+            result.addResult(rank);
+        }
+        return result;
     }
 }
