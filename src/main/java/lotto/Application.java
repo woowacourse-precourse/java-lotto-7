9d1962e4f598299ad91782e8b198ff30ae1bffc0
donghan378 <ehdgksqorgh@naver.com>
2024-11-04 09:34:33 +0900

    feat(Application): add try-catch iteration
    사용자의 입력이 잘못되었을때 에러 메세지를 출력하고 다시 그 부분부터 이어갈 수 있도록 반복문과 try-catch문을 활용하여 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,9 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,20 +11,49 @@ public class Application {
         List<Integer> winningNum;
         int bonusNum;
 
-        purchaseNum = getPurchaseNum();
-        Lotto[] lottos = new Lotto[purchaseNum];
-        System.out.println("purchaseNum" + "개를 구매했습니다.");
+        while (true) {
+            try {
+                purchaseNum = getPurchaseNum();
+                break;
+            }catch(NumberFormatException e) {
+                System.out.println("[ERROR] 정수를 입력하세요.");
+            }catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
+        Lotto[] lottos = new Lotto[purchaseNum];
+        System.out.println(purchaseNum + "개를 구매했습니다.");
         for(int i = 0; i < purchaseNum; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottos[i] = new Lotto(numbers);
             lottos[i].printNums();
         }
-        winningNum = getWinningNum();
-        bonusNum = getBonusNum();
+
+        while(true) {
+            try {
+                winningNum = getWinningNum();
+                break;
+            }catch(NumberFormatException e) {
+                System.out.println("[ERROR] 정수를 입력하세요.");
+            }catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while(true) {
+            try {
+                bonusNum = getBonusNum();
+                break;
+            }catch(NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 1자리 정수여야 합니다.");
+            }catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public static int getPurchaseNum() {
+    public static int getPurchaseNum() throws NumberFormatException {
         System.out.println("구입금액을 입력해 주세요.");
         int sum = Integer.parseInt(Console.readLine());
 
@@ -36,7 +63,7 @@ public class Application {
         return sum / 1000;
     }
 
-    public static List<Integer> getWinningNum() {
+    public static List<Integer> getWinningNum() throws NumberFormatException {
         System.out.println("당첨 번호를 입력해 주세요.");
         String nums = Console.readLine();
         String[] numbers = nums.split(",");
@@ -52,11 +79,17 @@ public class Application {
             }
             winningNum.add(Integer.parseInt(numbers[i].trim()));
         }
+
+        Set<Integer> numSet = new HashSet<Integer>(winningNum); // 중복 확인
+        if(winningNum.size() != numSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 수가 존재합니다.");
+        }
+
         Collections.sort(winningNum);
         return winningNum;
     }
 
-    public static int getBonusNum() {
+    public static int getBonusNum() throws NumberFormatException {
         System.out.println("보너스 번호를 입력해 주세요.");
         String num = Console.readLine();
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,7 +19,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
 
-        Set<Integer> numSet = new HashSet<>(numbers);
+        Set<Integer> numSet = new HashSet<Integer>(numbers); // 중복 확인
         if(numbers.size() != numSet.size()) {
             throw new IllegalArgumentException("[ERROR] 중복된 수가 존재합니다.");
         }
