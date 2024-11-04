eed5e37a1da4a2126ab13b696e8caa0bb15959e1
yunho_yun <yunho_yun@naver.com>
2024-11-04 22:34:39 +0900

    fix: 로또 번호 검증 로직 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -19,6 +19,9 @@ public class Lotto {
         if (!numbers.stream().allMatch(this::validateNumberRange)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 중복되지 않는 6개의 숫자여야 합니다.");
+        }
     }
 
     public boolean validateNumberRange(int number) {
