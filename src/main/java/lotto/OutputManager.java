f3295b0142201c8b4ca35fa60777cfe61f4c9a15
문혜교 <3hilney@gmail.com>
2024-11-04 16:40:54 +0900

    feat: 로또 목록 출력 기능 구현

--- src/main/java/lotto/OutputManager.java
+++ src/main/java/lotto/OutputManager.java
@@ -1,10 +1,18 @@
 package lotto;
 
 
+import java.util.List;
+
 public class OutputManager {
 
     public static void printLottoCount(int count) {
         System.out.printf((InputMessage.LOTTO_COUNT.getInputMessage()) + "%n", count);
+    }
+
+    public static void printLottoTickets(List<List<Integer>> lottoTickets) {
+        for (List<Integer> ticket : lottoTickets) {
+            System.out.println(ticket);
+        }
     }
 
 }
