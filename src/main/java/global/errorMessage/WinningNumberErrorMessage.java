79381d6177bd410d2343b637e87fbeccbee11cc9
sleepyhoon <pokjm99@gmail.com>
2024-11-04 17:50:07 +0900

    fix (WinningNumberErrorMessage) : 에러 메시지를 좀 더 자세히 표시

--- src/main/java/global/errorMessage/WinningNumberErrorMessage.java
+++ src/main/java/global/errorMessage/WinningNumberErrorMessage.java
@@ -1,7 +1,7 @@
 package global.errorMessage;
 
 public enum WinningNumberErrorMessage {
-    INVALID_WINNING_NUMBER_FORMAT("당첨 번호를 쉼표(,)를 사용하여 올바른 방식으로 입력하세요."),
+    INVALID_WINNING_NUMBER_FORMAT("당첨 번호를 쉼표(,)를 사용하여 올바른 방식으로 6개를 입력하세요."),
     INVALID_WINNING_NUMBER_SIZE("당첨 번호는 6개여야 합니다.")
     ;
     private final String message;
