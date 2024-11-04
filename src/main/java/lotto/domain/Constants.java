20a6a3255f27e0a8f8d17bb569f40541630bb952
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 12:29:24 +0900

    feat: Constants 클래스 생성

---
+++ src/main/java/lotto/domain/Constants.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class Constants {
+    public static final int THREE_MATCHES_PRIZE = 5000;
+    public static final int FOUR_MATCHES_PRIZE = 50000;
+    public static final int FIVE_MATCHES_PRIZE = 1500000;
+    public static final int FIVE_MATCHES_WITH_BONUS_PRIZE = 30000000;
+    public static final int SIX_MATCHES_PRIZE = 2000000000;
+    public static final int LOTTO_MIN_NUM = 1;
+    public static final int LOTTO_MAX_NUM = 45;
+    public static final int LOTTO_SIZE = 6;
+}
