562567ed20c540e26c5027e753008f346b920c20
kyunghoon01 <sports0124@naver.com>
2024-11-04 12:28:20 +0900

    feat: 비교결과를 통해 당첨 결과와 수익률 출력 기능 구현

--- src/main/java/view/LottoView.java
+++ src/main/java/view/LottoView.java
@@ -5,9 +5,12 @@ import static util.LottoFormatter.formatLotto;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.Lotto;
+import lotto.LottoResult;
 import lotto.PurchaseCount;
 
 public class LottoView {
+    private final static int LOTTO_PRICE = 1_000;
+
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String purchaseAmount = Console.readLine();
@@ -18,6 +21,7 @@ public class LottoView {
         int count = purchaseCount.getPurchaseCount();
         String result = count + "개를 구매했습니다.";
         System.out.println(result);
+        System.out.println();
         return result;
     }
 
@@ -25,6 +29,7 @@ public class LottoView {
         for (Lotto lotto : lottoList) {
             System.out.println(formatLotto(lotto.getNumbers()));
         }
+        System.out.println();
     }
 
     public String inputWinningNumbers() {
@@ -33,7 +38,20 @@ public class LottoView {
     }
 
     public String inputBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
+    }
+
+    public void printMatchingResult(LottoResult lottoResult, int lottoCount) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + lottoResult.getThreeMatchesCount() + "개");
+        System.out.println("4개 일치 (50,000원) - " + lottoResult.getFourMatchesCount() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.getFiveMatchesCount() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.getFiveMatchesWithBonusCount() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.getSixMatchesCount() + "개");
+        System.out.println("총 수익률은 " + lottoResult.getYield(lottoCount * LOTTO_PRICE) + "%입니다.");
     }
 }
