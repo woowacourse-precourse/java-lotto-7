a3006dec9ec28c886f4b5f37799882cb06fe57a7
jinhyeon <tiemo0708@naver.com>
2024-11-04 22:37:58 +0900

    refactor:메소드 분리 및 예외메시지와 숫자들 상수처리

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,32 +1,42 @@
 package lotto.domain.model;
 
+import lotto.util.ErrorMessages;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 public class Lotto {
+    private static final int REQUIRED_NUMBER_COUNT = 6;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumberRange(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != REQUIRED_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_WINNING_NUMBER_COUNT);
         }
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        }
-
-        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> num < MINIMUM_NUMBER || num > MAXIMUM_NUMBER)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_WINNING_NUMBER_COUNT);
         }
     }
 
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessages.DUPLICATE_WINNING_NUMBER);
+        }
+    }
     public List<Integer> getNumbers() {
         return numbers;
     }
