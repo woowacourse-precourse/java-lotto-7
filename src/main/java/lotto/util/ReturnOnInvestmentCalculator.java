10902d99ca6fc7aa1a0772f456752bc4f1720d02
venus01 <geumsung7769@naver.com>
2024-11-04 21:25:43 +0900

    Refactor : 부적절한 정적 변수를 삭제
    부적절한 정적 변수 사용으로 인해 테스트가 원활하게 진행되지 않은 것을 확인하였음
    
    구체적인 상황: 첫 번쨰 테스트에서 당첨금이 5천원으로 집계된 것이 초기화되지 않고,
    다음 테스트를 진행하고 다음 테스트에서 당첨금이 5천원이 추가로 합산되어 총 10000원의수익을 올린 것으로 오류가 발생하였음
    
    해결: 정적 변수 대신 메서드 내에서 수익금을 계산하는 변수를 생성하는 것으로 대체하였음

--- src/main/java/lotto/util/ReturnOnInvestmentCalculator.java
+++ src/main/java/lotto/util/ReturnOnInvestmentCalculator.java
@@ -10,20 +10,24 @@ public class ReturnOnInvestmentCalculator {
 
     private static final double ROUNDING_SCALE = 10.0;
     private static final int PERCENTAGE_MULTIPLIER = 100;
-    private static double totalProceeds = INITIAL_VALUE.getValue();
+
 
     public static double calculateReturnOnInvestment(Map<LottoGrade, Integer> lottoGradeMap, int purchaseAmount) {
 
-        calculateTotalProceeds(lottoGradeMap);
+        double totalProceeds = calculateTotalProceeds(lottoGradeMap);
 
         return ((double) Math.round((totalProceeds / purchaseAmount * PERCENTAGE_MULTIPLIER) * ROUNDING_SCALE)) / ROUNDING_SCALE;
     }
 
-    private static void calculateTotalProceeds(Map<LottoGrade, Integer> lottoGradeMap) {
+    private static double calculateTotalProceeds(Map<LottoGrade, Integer> lottoGradeMap) {
+        double totalProceeds = INITIAL_VALUE.getValue();
+
         totalProceeds += lottoGradeMap.get(LottoGrade.THREE_MATCHED) * LottoGrade.THREE_MATCHED.getWinningAmount();
         totalProceeds += lottoGradeMap.get(LottoGrade.FOUR_MATCHED) * LottoGrade.FOUR_MATCHED.getWinningAmount();
         totalProceeds += lottoGradeMap.get(LottoGrade.FIVE_MATCHED) * LottoGrade.FIVE_MATCHED.getWinningAmount();
         totalProceeds += lottoGradeMap.get(LottoGrade.BONUS_MATCHED) * LottoGrade.BONUS_MATCHED.getWinningAmount();
         totalProceeds += lottoGradeMap.get(LottoGrade.ALL_MATCHED) * LottoGrade.ALL_MATCHED.getWinningAmount();
+
+        return totalProceeds;
     }
 }
