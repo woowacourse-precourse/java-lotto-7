a11808ecc1ff11dde12f3c942d69df0767e1c579
양효빈 <hyobin3026@naver.com>
2024-11-04 09:51:08 +0900

    refactor(Lotto): Add method to validate duplication

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -2,19 +2,29 @@ package lotto.model.lotto;
 
 import lotto.exception.LottoException;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
         validateLottoNumberCount(lottoNumbers);
+        validateLottoNumberDuplication(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
     private void validateLottoNumberCount(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != LottoConstant.NUMBER_COUNT) {
             throw new IllegalArgumentException(LottoException.INVALID_COUNT.getMessage());
+        }
+    }
+
+    private void validateLottoNumberDuplication(List<Integer> lottoNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
+        if (uniqueNumbers.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException(LottoException.DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
