faaf0d6270df6ab70eb54659743466b019afd59d
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 18:00:47 +0900

    refactor: 가독성 수정 및 예외 값 수정

--- src/main/java/lotto/LottoCheck.java
+++ src/main/java/lotto/LottoCheck.java
@@ -4,22 +4,12 @@ import java.util.List;
 
 public class LottoCheck {   // 로또 당첨 및 등수확인
     public int lottoRankResult (int match, boolean bonus) {     // 1~5등 조건
-        if (match == 6) {
-            return 1;
-        }
-        if (match == 5 && bonus) {
-            return 2;
-        }
-        if (match == 5) {
-            return 3;
-        }
-        if (match == 4) {
-            return 4;
-        }
-        if (match == 3) {
-            return 5;
-        }
-        return 0;
+        if (match == 6) return 1;
+        if (match == 5 && bonus) return 2;
+        if (match == 5) return 3;
+        if (match == 4) return 4;
+        if (match == 3) return 5;
+        return -1;
     }
 
     public int countMatches(List<Integer> userPurchasedLotto, List<Integer> winningNumber) {
