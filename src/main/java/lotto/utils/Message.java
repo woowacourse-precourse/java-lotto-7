7c2afcaf78715d773a5e3c577ec2752856ba5f1d
hyuntae99 <hyuntae9912@naver.com>
2024-11-04 15:23:50 +0900

    feat: ErrorMessage 및 Message 클래스 구현

---
+++ src/main/java/lotto/utils/Message.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class Message {
+    public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String PURCHASED_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String WINNING_STATISTICS_HEADER = "당첨 통계\n---";
+    public static final String TOTAL_PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+}
