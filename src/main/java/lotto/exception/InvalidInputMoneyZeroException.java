6cb58f7b2bfd05b16224a819255e69d1b57dfe7a
ParkSangsin <pss6039@naver.com>
2024-11-04 19:43:37 +0900

    feat: 로또 구입 금액이 0이 아닌지 검증을 위한 사용자 정의 예외 클래스 추가

--- src/main/java/lotto/exception/InvalidInputMoneyZeroException.java
+++ src/main/java/lotto/exception/InvalidInputMoneyZeroException.java
@@ -1,2 +1,9 @@
-package lotto.exception;public class InvalidInputMoneyZeroException {
+package lotto.exception;
+
+public class InvalidInputMoneyZeroException extends IllegalArgumentException {
+    private static final String ERROR_MESSAGE = "로또 구입 금액은 0보다 커야 합니다.";
+
+    public InvalidInputMoneyZeroException() {
+        super(ERROR_MESSAGE);
+    }
 }
