212a7f0b8f810af75453a52a01a7084b7ba19716
023-dev <0_2_3@naver.com>
2024-11-04 17:05:06 +0900

    feat(PurchaseAmount): 도메인으로 로또 구입 금액 관리 기능 추가

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import static lotto.common.Constants.LOTTO_PRICE;
+
+public class PurchaseAmount {
+
+    private final int amount;
+
+    public PurchaseAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int calculateLottoCount() {
+        return this.amount / LOTTO_PRICE;
+    }
+}
