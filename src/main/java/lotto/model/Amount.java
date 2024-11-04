04e662b14a7c298f6ffcbe87799376950e08f70b
shin-chaeeun <chaeeun0217@naver.com>
2024-11-04 16:10:21 +0900

    Feat(Amount): 0만 입력한 경우 처리하는 메서드 구현

--- src/main/java/lotto/model/Amount.java
+++ src/main/java/lotto/model/Amount.java
@@ -4,15 +4,23 @@ public class Amount extends ValidationManager {
     private final int amount;
     private static final String REQUEST_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private final String NOT_DIVISIBLE_BY_THOUSAND_ERROR = "[ERROR] 로또의 금액이 1000원 단위가 아닙니다.";
+    private final String INPUT_ZERO_ERROR = "[ERROR] 로또의 금액은 0보다 커야합니다.";
     private final int amountDivisor = 1000;
 
     public Amount(String amountInput) {
         isNumber(amountInput);
         isNotEmptyInput(amountInput);
+        isInputZero(amountInput);
         this.amount = isDivideByThousand(Integer.parseInt(amountInput));
     }
 
-    public int isDivideByThousand(int amountInput) {
+    private void isInputZero(String input) {
+        if(input.matches("0*")){
+            throw new IllegalArgumentException(INPUT_ZERO_ERROR);
+        }
+    }
+
+    private int isDivideByThousand(int amountInput) {
         if (amountInput % amountDivisor == 0) {
             return amountInput;
         }
