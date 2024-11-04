56319058dc03d57d405b78200af25ba38384a5ed
sun <50648835+kimseongyu@users.noreply.github.com>
2024-11-04 17:39:39 +0900

    feat: add rank enum object

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,35 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private static final long DEFAULT_PRICE = 0;
+
+    private final int matchCount;
+    private final boolean matchBonus;
+    private final long prize;
+
+    Rank(int matchCount, boolean matchBonus, long prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    public static Rank getMatchedRank(int matchCount, boolean matchBonus) {
+        for (Rank rank : Rank.values()) {
+            if (rank.matchCount == matchCount && rank.matchBonus == matchBonus) {
+                return rank;
+            }
+        }
+        return NONE;
+    }
+
+    public long getPrize() {
+        return this.prize;
+    }
+}
