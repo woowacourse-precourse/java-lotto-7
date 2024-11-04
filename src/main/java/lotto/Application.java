3d42a3841ad63c25ec98bdbde287065db84fb838
박규영 <magic9282m@gmail.com>
2024-11-04 23:00:05 +0900

    feat: add new method purchaseLottery
    로또 구입금액으로 로또를 구입하는  purchaseLottery 작성한다.
    - `로또구입금액 / priceOfLottery(1000)` 개의 로또를 생성한다.
      - 생성 전, 몇 개의 로또를 구매했는지 출력한다.
      - 또한, Randoms.pickUniqueNumbersInRange()을 이용해 생성한 난수들을 로또 생성자의 인수로 사용한다.
    - 또한, 생성해낸 난수들을 List.toString()을 이용해 출력한다.
    
    그리고 구매한 로또들을 저장할 배열인 Lotto[] purchasedLotteries와 로또 하나의 가격을 저장하는 상수 priceOfLottery를 선언한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,18 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
+    static final int priceOfLottery = 1000;
     static int amountForLottery;
+    static Lotto[] purchasedLotteries;
+
     public static void main(String[] args) {
         readAmount();
+        purchaseLottery();
     }
     public static void validateAmount(int input) {
         if(input <= 0 || input % 1000 != 0) {
@@ -22,5 +29,15 @@ public class Application {
         }
         validateAmount(input);
         amountForLottery = input;
+    }
+    public static void purchaseLottery() {
+        int numberOfPurchase = amountForLottery / priceOfLottery;
+        System.out.printf("%d개를 구매했습니다.", numberOfPurchase);
+        purchasedLotteries = new Lotto[numberOfPurchase];
+        for(int i=0; i<numberOfPurchase; i++) {
+            List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            System.out.println(randomNumbers.toString());
+            purchasedLotteries[i] = new Lotto(randomNumbers);
+        }
     }
 }
