c76186b6855ca1b91aba6ddbe97bec6c4e8a3ba5
JoonKyo <joonkyolee@gmail.com>
2024-11-04 23:38:47 +0900

    fix(Profit): 높은 상금에 대한 수익률을 처리하지 못한 부분 수정을 위해 long, BigDecimal 도입

--- src/main/java/lotto/domain/Profit.java
+++ src/main/java/lotto/domain/Profit.java
@@ -1,6 +1,11 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Profit {
+    private static final int DECIMAL_POINT = 1;
+    private static final int PERCENTAGE_CONVERSION = 100;
     private final Money money;
     private final LottoResult lottoResult;
 
@@ -13,15 +18,17 @@ public class Profit {
         return new Profit(money, lottoResult);
     }
 
-    public double calculateProfit() {
-        return (double) calculateTotalPrize() / money.getMoney() * 100;
+    public String calculateProfit() {
+        double profit = (double) calculateTotalPrize() / money.getMoney() * PERCENTAGE_CONVERSION;
+        BigDecimal profitPercent = new BigDecimal(profit).setScale(DECIMAL_POINT, RoundingMode.HALF_UP);
+        return profitPercent.toString();
     }
 
-    private int calculateTotalPrize() {
+    private long calculateTotalPrize() {
         return lottoResult.calculateRankDistribution()
                 .entrySet()
                 .stream()
-                .mapToInt(entry -> entry.getKey().getPrize() * entry.getValue())
+                .mapToLong(entry -> (long) entry.getKey().getPrize() * entry.getValue())
                 .sum();
     }
 }
