834d24f2bfef708252f7527d2fe9f9b8ddfa560f
김주언 <wndjs803@naver.com>
2024-11-04 23:30:59 +0900

    feat: 로또 번호 오름차순 정렬

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,13 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
+
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers){
+        numbers.sort(Integer::compareTo);
+        return numbers;
     }
 
     public List<Integer> getNumbers() {
