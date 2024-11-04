99a12001f51cc268b62fb09c7c07c37c9ef4dda1
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-04 20:00:27 +0900

    refactor : PurchaseMoneyTest 내 메서드명 변경

--- src/test/java/lotto/model/domain/PurchaseMoneyTest.java
+++ src/test/java/lotto/model/domain/PurchaseMoneyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class PurchaseMoneyTest {
 	@DisplayName("구입 금액이 1,000원 미만일 경우 예외 발생")
 	@Test
-	void 구입금액이_천원미만이면_예외발생() {
+	void 구입금액이_천원미만일_경우() {
 		assertThatThrownBy(() -> new PurchaseMoney(500))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
@@ -16,7 +16,7 @@ class PurchaseMoneyTest {
 
 	@DisplayName("구입 금액이 1,000원 단위가 아닐 경우 예외 발생")
 	@Test
-	void 구입금액이_천원단위가_아니면_예외발생() {
+	void 구입금액이_천원단위가_아닐경우() {
 		assertThatThrownBy(() -> new PurchaseMoney(2500))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
@@ -24,7 +24,7 @@ class PurchaseMoneyTest {
 
 	@DisplayName("구입 금액에 따른 로또 개수 확인")
 	@Test
-	void 구입금액에_따른_로또개수() {
+	void 구입금액에_따른_로또개수_확인() {
 		PurchaseMoney purchaseMoney = new PurchaseMoney(5000);
 		assertThat(purchaseMoney.getLottoCount()).isEqualTo(5);
 	}
