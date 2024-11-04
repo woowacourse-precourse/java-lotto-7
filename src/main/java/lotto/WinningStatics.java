b21593b363cd9c9f2bb426de56d50289d8622ec5
gorapaduckoo <jioung93@naver.com>
2024-11-04 20:54:38 +0900

    feat: 등수별 당첨 개수 저장 및 반환 기능 추가

---
+++ src/main/java/lotto/WinningStatics.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class WinningStatics {
+    private final Map<Rank, Integer> winningCount = new EnumMap<>(Rank.class);
+
+    public WinningStatics() {
+        for (Rank rank : Rank.values()) {
+            winningCount.put(rank, 0);
+        }
+    }
+
+    public void addWinning(Rank rank) {
+        winningCount.put(rank, winningCount.get(rank) + 1);
+    }
+
+    public int getWinningCount(Rank rank) {
+        return winningCount.get(rank);
+    }
+}
