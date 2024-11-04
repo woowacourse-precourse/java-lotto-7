c8bf0c1d94bb2788a7742ea39ce66ed7701033c5
전민정 <minj_1203@naver.com>
2024-11-04 19:52:17 +0900

    fix(exception): 상수 이름 수정

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,15 @@
+package lotto;
+
+public class Money {
+    private final int amount;
+
+    public Money(int amount){
+        this.amount = amount;
+    }
+
+    private void validate(int amount){
+
+    }
+
+    private void validateZero
+}
