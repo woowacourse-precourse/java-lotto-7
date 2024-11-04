6c67c621159e0436208d4df24e3caae22912319a
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-04 15:49:24 +0900

    refactor : 상수 타입을 문자열에서 정수로 변경

--- src/main/java/lotto/enums/WinningMoney.java
+++ src/main/java/lotto/enums/WinningMoney.java
@@ -1,15 +1,19 @@
 package lotto.enums;
 
 public enum WinningMoney {
-    FIRST_PRIZE_MONEY("2,000,000,000원"),
-    SECOND_PRIZE_MONEY("30,000,000원"),
-    THIRD_PRIZE_MONEY("1,500,000원"),
-    FOURTH_PRIZE_MONEY("50,000원"),
-    FIFTH_PRIZE_MONEY("5,000원");
+    FIRST_PRIZE_MONEY(2000000000),
+    SECOND_PRIZE_MONEY(30000000),
+    THIRD_PRIZE_MONEY(1500000),
+    FOURTH_PRIZE_MONEY(50000),
+    FIFTH_PRIZE_MONEY(5000);
 
-    private final String prizeMoney;
+    private final int prizeMoney;
 
-    WinningMoney(String prizeMoney) {
+    WinningMoney(int prizeMoney) {
         this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 }
