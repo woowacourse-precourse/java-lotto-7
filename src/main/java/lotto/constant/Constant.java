2a9aac80857db39d39235de8498f3ffd773a1105
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-11-04 14:40:51 +0900

    refactor : Constant 클래스 추상 클래스로 변경

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -1,6 +1,6 @@
 package lotto.constant;
 
-public class Constant {
+public abstract class Constant {
     public static final int LOTTO_PRICE = 1000;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
