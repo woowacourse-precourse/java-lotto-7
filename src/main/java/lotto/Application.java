651a6121f2d53e11b2d68205644f6cb2ba71b7bb
KHE <99ant@naver.com>
2024-11-04 13:13:14 +0900

    feat(Application): add enter the lotto purchase amount in 1,000 won units

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        private static int inputPurchaseAmount() {
+            System.out.println("구입금액을 입력해 주세요.");
+            int amount = Integer.parseInt(Console.readLine().trim());
+
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 구입금액은 1,000원 단위여야 합니다.");
+            }
+            return amount;
+        }
+
     }
 }
