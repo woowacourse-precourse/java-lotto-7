941e3f4f955f9b91d2ffea79553843694f5b9b89
SuyeonShinnnn <suyeons5536@gmail.com>
2024-11-04 20:29:49 +0900

    test: LottoController 단위 테스트 작성 완료

--- src/test/java/lotto/LottoControllerTest.java
+++ src/test/java/lotto/LottoControllerTest.java
@@ -1,15 +1,71 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LottoControllerTest {
+    @Test
+    public void createLottoNumbers(){
+        List<Lotto> lottoNumbers = new LinkedList<>();
+        // 로또 번호 생성 및 리스트에 추가
+        for(int i = 0; i < 1; i++){
+            try {
+                lottoNumbers.add(new Lotto(List.of(1,2,3,4,5,6,7)));
+            }catch (IllegalArgumentException e){
+                // 로또 번호 개수가 6개가 아니거나 중복된 숫자가 포함 돼 있을 경우 예외 발생
+                // i-- 후 로또 번호 재생성
+                i--;
+
+                // for문이 한 번 반복되고 나면 +1 되므로 지금 i는 -1이 돼야 함
+                assertEquals(i, -1);
+                break;
+            }
+        }
+    }
+    void testSetWinningNumberNumberFormatException(){
+        String includeAphabet = "1,2,3,e";
+        List<Integer> winningNumbers = new LinkedList<>();
+
+        String message = "";
+        for(String n:includeAphabet.split(",")){
+            message = Assertions.assertThrows(NumberFormatException.class, () -> {
+                winningNumbers.add(Integer.parseInt(n));
+            }).getMessage();
+        }
+        assertEquals(message, "[ERROR] 당첨번호는 숫자와 콤마(,)로만 이루어져야합니다.");
+    }
+    @Test
+    void testSetWinningNumberCheckLength(){
+        String checkLength = "1,2,3";
+        List<Integer> winningNumbers = new LinkedList<>();
+
+        for(String n:checkLength.split(",")){
+            winningNumbers.add(Integer.parseInt(n));
+        }
+        assertThatThrownBy(() -> new Lotto(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 로또 번호는 6개여야 합니다.");
+
+    }
+    @Test
+    void testSetWinningNumberDuplicated(){
+        String duplicated = "1,2,3,4,5,5";
+        List<Integer> winningNumbers = new LinkedList<>();
+
+        for(String n:duplicated.split(",")){
+            winningNumbers.add(Integer.parseInt(n));
+        }
+        assertThatThrownBy(() -> new Lotto(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 로또 번호는 중복될 수 없습니다.");
+
+    }
     @Test
     void testMatchNumbers(){
         List<Lotto> lottoNumbers = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 3개 일치
