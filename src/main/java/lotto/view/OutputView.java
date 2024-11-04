17a25d9a2aab8adbba2aaa19e99368afea79b589
JMJ <ghtntkdnsk@naver.com>
2024-11-04 20:30:45 +0900

    feat(OutputView) : 구매개수만큼 생성된 로또들을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
 
     public void printPurchasedLottoCount(int purchaseCount) {
         System.out.println(purchaseCount + "개를 구매했습니다.");
+    }
+
+    public void printPurchaserLottos(List<Lotto> purchasedLottos) {
+        for (Lotto purchasedLotto : purchasedLottos) {
+            System.out.println(purchasedLotto.getNumbers());
+        }
+        System.out.println();
     }
 }
