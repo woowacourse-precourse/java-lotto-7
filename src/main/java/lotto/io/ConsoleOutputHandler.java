3d1ff99d0c81c15bc587ca70bbdfaa3e3848b9fd
kangsan <devstar1223@gmail.com>
2024-11-04 15:24:33 +0900

    feat: 로또 구입금액 입력 안내 메세지 출력

---
+++ src/main/java/lotto/io/ConsoleOutputHandler.java
@@ -0,0 +1,8 @@
+package lotto.io;
+
+public class ConsoleOutputHandler {
+    private final String buyAmountMessage = "구입금액을 입력해 주세요.";
+    public void BuyAmountMessage() {
+        System.out.println(buyAmountMessage);
+    }
+}
--- src/main/java/lotto/simulation/LottoSimulator.java
+++ src/main/java/lotto/simulation/LottoSimulator.java
@@ -1,6 +1,10 @@
 package lotto.simulation;
 
+import lotto.io.ConsoleOutputHandler;
+
 public class LottoSimulator {
+    private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
     public void run() {
+        consoleOutputHandler.BuyAmountMessage();
     }
 }
