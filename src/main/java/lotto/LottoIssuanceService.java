67fe235675707062f099af550ef91771982172e0
seungheejang <justinholla908@gmail.com>
2024-11-04 11:59:34 +0900

    refactor(LottoIssuanceService): Rename class, update methods for clarity
    - Renamed class to better reflect its purpose
    - Changed randomLottoIssue() method to void, as it no longer requires a return value
    - Removed parameter from printIssuedLottoNumbers() method to simplify usage
    - Updated method to save lotto numbers in ascending order

--- src/main/java/lotto/LottoIssuanceService.java
+++ src/main/java/lotto/LottoIssuanceService.java
@@ -3,18 +3,18 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class LottoService {
+public class LottoIssuanceService {
     public List<Integer> lottoNumbers;  // 로또 번호 저장할 리스트
 
     // 로또 번호 생성 메소드
-    public List<Integer> randomLottoIssue() {
+    public void randomLottoIssue() {
         this.lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        return lottoNumbers;
     }
 
-    // 로또 번호 출력 메소드
-    public void printIssuedLottoNumbers(List<Integer> lottoNumbers) {
+    // 로또 번호 오름차순 출력 메소드
+    public void printIssuedLottoNumbers() {
         lottoNumbers.sort(Integer::compareTo);  // 오름차순 정렬
-        System.out.println(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;       // 오름차순 저장
+        System.out.println(this.lottoNumbers);  // 정렬된 리스트 출력
     }
 }
