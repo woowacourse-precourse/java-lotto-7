3b8b65ee2db4153eb79d05d25a74956c03d0d77e
Parkjyun <jaeyun0507@naver.com>
2024-11-04 23:25:28 +0900

    refactor(LottoTest): 패키지 변경

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,10 +1,9 @@
-package lotto;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
-import lotto.model.Lotto;
 import lotto.model.exception.DomainExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
