f816a098d132f7bf30cb93601501b1a504c2987c
Yunsu4 <ysa020428@gmail.com>
2024-11-04 16:53:34 +0900

    feat(Rank): 일치하는 숫자 개수와 보너스 숫자 매칭 여부만 다루는 클래스 추가

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,8 @@
+package lotto.model;
+
+public record Rank(int matchCount, boolean isBonusMatched) {
+    public boolean matches(int sameNumberCount, boolean matchBonusNumber){
+        return this.matchCount == sameNumberCount &&
+                this.isBonusMatched == matchBonusNumber;
+    }
+}
