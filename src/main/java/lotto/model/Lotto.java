e2f57bd0c8d59d3924fa973f6ffea727aa26b72e
Jewoo Lee <jaewoo0254@gmail.com>
2024-11-04 09:53:32 +0900

    feat(Lotto): 생성한 6개의 랜덤 문자에 대헤 중복 검사 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.model;
 
+import lotto.constant.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +16,24 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(
+                    ErrorMessage.NOT_SIX_LOTTO_NUMBERS.getMessage());
         }
+        if (hasDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private boolean hasDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // TODO: 추가 기능 구현
