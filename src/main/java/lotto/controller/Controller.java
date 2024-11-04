6acbfb10abe28eab9d7f1af8d49c6527a3d765ea
kssk3 <kss8014@daum.net>
2024-11-04 11:47:42 +0900

    Refactor Controller

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.FirstTicket;
+import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.Ticket;
+import lotto.utils.RandomNumbersGenerator;
+import lotto.utils.StringUtil;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,7 +14,56 @@ public class Controller {
     private static final String SPLIT_DELIMITER = ", ";
 
 
-    
+    public void run() {
+        Lotto lotto = createLotto();
+
+        createAutoTickets(lotto);
+
+        FirstTicket firstTicket = createFirstTicket();
+        OutputView.printResult(lotto.getResult(firstTicket), lotto.getYield(firstTicket));
+    }
+
+    private Lotto createLotto() {
+        try {
+            return new Lotto(requestAmount());
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return createLotto();
+        }
+    }
+
+    private FirstTicket createFirstTicket() {
+        Ticket firstTicket = requestFirstNumbers();
+        LottoNumber bonusNumber = requestBonusNumber();
+        try {
+            return new FirstTicket(firstTicket, bonusNumber);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return createFirstTicket();
+        }
+    }
+
+    private Ticket requestFirstNumbers() {
+        try {
+            List<Integer> integers = StringUtil.splitToIntegers(InputView.requestFirstNumbers(), SPLIT_DELIMITER);
+            return Ticket.createByIntegers(integers, false);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return requestFirstNumbers();
+        }
+    }
+
+    private int requestAmount() {
+        return InputView.requestAmount();
+    }
+
+
+    private void createAutoTickets(Lotto lotto) {
+        int numberOfPurchasableCount = lotto.findCountOfPurchasable();
+        for (int i = 0; i < numberOfPurchasableCount; i++) {
+            lotto.addTicket(Ticket.createByImplementation(new RandomNumbersGenerator(), true));
+        }
+    }
 
     private LottoNumber requestBonusNumber() {
         try {
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,10 @@
 package lotto.view;
 
 import java.util.Scanner;
-import java.util.Set;
 
 public class InputView {
     private static final String REQUEST_BONUS_NUMBER = "보너스 볼을 입력해 주새요.";
+    private static final String REQUEST_FIRST_NUMBER = "당첨 번호를 입력해주세요.";
     private static final String REQUEST_AMOUNT = "구입금액을 입력해주세요.";
     private static final String REQUEST_NUMBER = "숫자를 입력해주세요.";
     private static final Scanner scanner = new Scanner(System.in);
@@ -18,6 +18,11 @@ public class InputView {
             System.out.println(REQUEST_NUMBER);
             return requestAmount();
         }
+    }
+
+    public static String requestFirstNumbers() {
+        System.out.println(REQUEST_FIRST_NUMBER);
+        return scanner.nextLine();
     }
 
     public static int requestBonusNumber() {
