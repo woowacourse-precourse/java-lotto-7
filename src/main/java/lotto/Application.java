cd14359af60368bd5ed2710bc61068ee339d143d
EunSeo <gmjea023@naver.com>
2024-11-04 20:07:11 +0900

    fix(main): main 클레스 수정 - 전체 시스템 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,15 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGenerator lottoGenerator = new RandomLottoGenerator();
+        LottoMatcher lottoMatcher = new LottoMatcher();
+        Input inputHandler = new InputHandler();
+        LottoPurchase lottoPurchase = new LottoPurchase(lottoGenerator);
+        ResultCalculator resultCalculator = new ResultCalculator(lottoMatcher);
+        Output outputHandler = new OutputHandler();
+
+        LottoController lottoController = new LottoController(inputHandler, lottoPurchase, resultCalculator, outputHandler);
+
+        lottoController.play();
     }
 }
