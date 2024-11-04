32c7522216faff82ce19660d0205ea6c44b68409
(Jinriro) <(riro12@hanyang.ac.kr)>
2024-11-04 18:48:07 +0900

    test(EarningRateTest):수익률 로직 테스트 케이스 추가

--- src/test/java/lotto/model/EarningRateTest.java
+++ src/test/java/lotto/model/EarningRateTest.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
-public class EarningRateTest {
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EarningRateTest {
+
+    @Test
+    void earningRate_shouldReturnCorrectEarningRate() {
+        // Given
+        int totalPrize = 2000000; // 예시: 총 당첨금
+        int moneySpent = 1000000; // 예시: 소비한 금액
+
+        LottoStatisticsCalculator calculator = new LottoStatisticsCalculator();
+
+        // When
+        double earningRate = calculator.EarningRate(totalPrize, moneySpent);
+
+        // Then
+        assertThat(earningRate).isEqualTo(200.0); // 수익률 계산: (2000000 / 1000000) * 100 = 200.0
+    }
 }
