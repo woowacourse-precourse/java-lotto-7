6f100ddb0f80174e28691c553e2d32d1f93d32aa
Sooyong97 <01071236398y@gmail.com>
2024-11-04 14:33:52 +0900

    feat: 로또 구입 금액 입력 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import camp.nextstep.edu.missionutils.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +17,13 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    //로또 구입 금액 입력
+    public int buy_lotto(){
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        if (amount % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위여야 합니다.");
+        }
+        return amount;
+    }
 }
