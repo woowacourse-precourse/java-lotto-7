5a4d966d014bb74e615cc541aed669ccab24020c
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-04 23:07:44 +0900

    feat: Add error for duplicate lotto num
    로또 번호가 중복 일 시 에러를 내는 코드를 추가함

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,6 @@
 package lotto;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,6 +13,11 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
 
