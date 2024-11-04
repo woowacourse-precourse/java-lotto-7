9604a6c4dd05316e8260cd383be07643820630d2
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-04 22:52:07 +0900

    feat(inputHandler): 입력받은 당첨 번호는 쉼표를 기준으로 구분받도록 검증하는 로직 구현

--- src/main/java/lotto/controller/InputHandler.java
+++ src/main/java/lotto/controller/InputHandler.java
@@ -4,7 +4,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lotto.model.Lotto;
+import lotto.util.LottoInfo;
 
 public class InputHandler {
 
@@ -17,6 +20,7 @@ public class InputHandler {
     public Lotto readWinningNumber() {
         final String regex = ",";
         final String input = readLine();
+        validateNumbersFormat(input);
         final List<Integer> number = Arrays.stream(input.split(regex))
                 .map(Integer::parseInt)
                 .toList();
@@ -41,6 +45,16 @@ public class InputHandler {
         final int zero = 0;
         if (input < zero) {
             throw new IllegalArgumentException("[ERROR] 양수만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateNumbersFormat(String input) {
+        final String delimiter = ",";
+        final String regex = "\\d[" + delimiter + "\\d]+" + "{" + (LottoInfo.NUMBER_COUNT.getNum() - 1) + "}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 " + delimiter + "를 기준으로 작성해야 합니다.");
         }
     }
 }
