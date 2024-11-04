d510ed1afffb9e3cf2903ef0678d932e64ac2476
bellmin <whdlas011203@naver.com>
2024-11-04 18:59:13 +0900

    feat(WinningRanking): NON_MATCH, findWinningRankingByMatchedCount 메소드 추가
    - NON_MATCH : 어떠한 매치도 되지 않았음을 뜻한다
    - findWinningRankingByMatchedCount : 매치 카운트를 바탕으로 WinningRanking 객체를 찾는 메소드

--- src/main/java/lotto/application/model/WinningRanking.java
+++ src/main/java/lotto/application/model/WinningRanking.java
@@ -6,13 +6,16 @@ import static lotto.common.Consts.FOURTH_PRIZE_MONEY;
 import static lotto.common.Consts.SECOND_PRIZE_MONEY;
 import static lotto.common.Consts.THIRD_PRIZE_MONEY;
 
-public enum WinningRanking {
+import java.util.Arrays;
+
+public enum WinningRanking implements Model{
 
     FIRST(6,false, FIRST_PRIZE_MONEY),
     SECOND(5,true, SECOND_PRIZE_MONEY),
     THIRD(5,false, THIRD_PRIZE_MONEY),
     FOURTH(4,false, FOURTH_PRIZE_MONEY),
-    FIFTH(3,false, FIFTH_PRIZE_MONEY);
+    FIFTH(3,false, FIFTH_PRIZE_MONEY),
+    NON_MATCH(0,false,0);
 
     private int matchedCount;
     private boolean isBonusMatched;
@@ -22,6 +25,21 @@ public enum WinningRanking {
         this.matchedCount = matchedCount;
         this.isBonusMatched = isBonusMatched;
         this.prizeMoney = prizeMoney;
+    }
+
+    @Override
+    public String toString(){
+        return this.isBonusMatched + "" + this.matchedCount + this.prizeMoney;
+    }
+
+    public static WinningRanking findWinningRankingByMatchedCount(int matchedCount) {
+        return Arrays.stream(WinningRanking.values())
+                .filter(winningRanking -> {
+                    if(winningRanking.matchedCount==matchedCount) return true;
+                    return false;
+                })
+                .findFirst()
+                .orElse(WinningRanking.NON_MATCH);
     }
 
 }
