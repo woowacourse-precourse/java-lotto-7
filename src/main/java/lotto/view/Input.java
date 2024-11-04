d317e2bb3e4915f7d838b3f76045b24859ee87e7
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 15:25:45 +0900

    feat: 당첨 번호 예외 기능 작성 Input 클래스 inputWinningNumbers() checkWinningNumbersStr() checkWinningNumbers() checkDuplicateNumber() checkRangeNumber()

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -16,11 +16,14 @@ public class Input {
         return purchasePrice;
     }
 
-    public static void inputWinningNumbers() {
+    public static List<Integer> inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         checkInput(input);
         List<String> winningNumbersStr = Arrays.asList(input.split(","));
+        List<Integer> winningNumbers = checkWinningNumbersStr(winningNumbersStr);
+        checkWinningNumbers(winningNumbers);
+        return winningNumbers;
     }
 
     private static void checkInput(String input) {
@@ -39,8 +42,33 @@ public class Input {
     private static void checkUnit(int number) {
         if (number < 1000)
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상이어야 합니다." + number);
-        if(number % 1000 != 0)
+        if (number % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다." + number);
     }
 
+    private static List<Integer> checkWinningNumbersStr(List<String> winningNumbersStr) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String winningNumberStr : winningNumbersStr) {
+            checkInput(winningNumberStr);
+            winningNumbers.add(checkNumber(winningNumberStr.trim()));
+        }
+        return winningNumbers;
+    }
+
+    private static void checkWinningNumbers(List<Integer> winningNumbers) {
+        checkDuplicateNumber(winningNumbers);
+        checkRangeNumber(winningNumbers);
+    }
+
+    private static void checkDuplicateNumber(List<Integer> numbers) {
+        if(numbers.size()!=numbers.stream().distinct().count())
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않는 숫자로 이루어져야 합니다.");
+    }
+
+    private static void checkRangeNumber(List<Integer> numbers) {
+        for(Integer number:numbers) {
+            if(number < 1 || number > 45)
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 범위여야 합니다." + number);
+        }
+    }
 }
