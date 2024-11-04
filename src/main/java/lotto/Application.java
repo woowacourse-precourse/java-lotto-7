3f561cbb639c3aed658786b8a4558b9672174fd1
hmlssghm <kwonse326@gmail.com>
2024-11-04 17:21:43 +0900

    feat(Application): 실행 로직 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.vo.Payment;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
