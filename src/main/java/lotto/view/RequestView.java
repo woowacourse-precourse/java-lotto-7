0c95f3c9cfff046085e98836eb1145fd18b6b4f3
KIMSEI1124 <workju1124@gmail.com>
2024-11-04 18:45:04 +0900

    feat: 문자 입력 기능 구현

--- src/main/java/lotto/view/RequestView.java
+++ src/main/java/lotto/view/RequestView.java
@@ -1,30 +1,20 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.common.ErrorMessage;
 
 public class RequestView {
-	private static final String LOTTO_PURCHASE_AMOUNT_MESSAGE = "구매금액을 입력해 주세요.";
 
-	public int inputBuyPrice() {
-		System.out.println(LOTTO_PURCHASE_AMOUNT_MESSAGE);
-		String input = Console.readLine();
-		int amount = toNumber(input);
-		validateAmount(amount);
-		return amount;
+	private static final RequestView INSTANCE = new RequestView();
+
+	private RequestView() {
 	}
 
-	private int toNumber(String numberAsString) {
-		try {
-			return Integer.parseInt(numberAsString);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_INPUT.getMessage());
-		}
+	public static RequestView getInstance() {
+		return INSTANCE;
 	}
 
-	private void validateAmount(int amount) {
-		if (amount % 1_000 != 0) {
-			throw new IllegalArgumentException(ErrorMessage.INVALID_AMOUNT.getMessage());
-		}
+	public String inputWithMessage(String message) {
+		System.out.println(message);
+		return Console.readLine();
 	}
 }
