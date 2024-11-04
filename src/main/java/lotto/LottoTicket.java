8f84ffd9218728c63b1b8ffbb286645f1b667ebd
kim-se-hee <saree98@naver.com>
2024-11-04 14:54:23 +0900

    feat(LottoTicket): 로또 티켓 클래스 구현

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,2 +1,69 @@
-package lotto;public class LottoTicket {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoTicket {
+    private final List<Integer> numbers;
+    private LottoPlace place;
+
+    public LottoTicket() {
+        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        this.place = LottoPlace.LOSE;
+    }
+    public LottoTicket(List<Integer> numbers) {
+        this.numbers = numbers;
+        this.place = LottoPlace.LOSE;
+    }
+
+    public void decideLottoPlaceBy(Lotto lotto, int bonusNumber) {
+        int matchingNumberCount = lotto.compareNumberWithTicket(numbers);
+        boolean hasBonusNumber = numbers.contains(bonusNumber);
+
+        if (matchingNumberCount == 6) {
+            place = LottoPlace.FIRST;
+            return;
+        }
+
+        if (matchingNumberCount == 5 && hasBonusNumber) {
+            place = LottoPlace.SECOND;
+            return;
+        }
+        if (matchingNumberCount == 5) {
+            place = LottoPlace.THIRD;
+            return;
+        }
+        if (matchingNumberCount == 4) {
+            place = LottoPlace.FORTH;
+            return;
+        }
+        if (matchingNumberCount == 3) {
+            place = LottoPlace.FIFTH;
+        }
+    }
+
+    public void decideLottoPlaceBy2(Lotto lotto, int bonusNumber) {
+        int matchingNumberCount = lotto.compareNumberWithTicket(numbers);
+        boolean hasBonusNumber = numbers.contains(bonusNumber);
+
+        this.place = LottoPlaceDecider.findLottoPlaceBy(matchingNumberCount, hasBonusNumber);
+    }
+
+    public boolean doesContainBonusNumber(Integer bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public LottoPlace getPlace() {
+        return place;
+    }
+
+    public int getWinningMoney() {
+        return place.getWinningMoney();
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
