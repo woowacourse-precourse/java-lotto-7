1e8eefe236b81c4dc0cdcbb57f22b7a0eaa487d5
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 19:13:04 +0900

    feat: 당첨 내역 출력 기능 추가 Output 클래스  + printStatistics()

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -2,12 +2,26 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoCreate;
+import lotto.domain.LottoMatch;
+import lotto.domain.LottoResult;
 
 public class Output {
     public static void printLottos(LottoCreate lottoCreate) {
         System.out.println(lottoCreate.getLottoCount() + "개를 구매했습니다.");
-        for(Lotto lotto : lottoCreate.getLottos())
+        for (Lotto lotto : lottoCreate.getLottos())
             System.out.println(lotto.getNumbers().toString());
         System.out.println();
+    }
+
+    public static void printStatistics(LottoResult lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (LottoMatch lottoMatch : LottoMatch.values()) {
+            if (lottoMatch != LottoMatch.NONE) {
+                System.out.printf("%s (%,d원) - %d개%n", lottoMatch.getStatements(),
+                        lottoMatch.getPrize(),
+                        lottoResult.getLottoResult(lottoMatch));
+            }
+        }
     }
 }
