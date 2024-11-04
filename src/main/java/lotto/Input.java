035f2599f1fed3fdc580f00ef6766a18d1dbe991
전민정 <minj_1203@naver.com>
2024-11-04 21:39:02 +0900

    feat(input): 당첨번호 입력 기능 구현

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,12 +2,17 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
+    private static final String INPUT_DELIM = ",";
 
     private String inputAmount(){
         System.out.println(InputMessage.AMOUNT_INPUT_MESSAGE);
         String amount = Console.readLine();
-        InputValidator.validateIsNumber(amount);
+        InputValidator.validateMoneyIsNumber(amount);
 
         return amount;
     }
@@ -17,6 +22,31 @@ public class Input {
         int amount = Integer.parseInt(input);
 
         return new Money(amount);
+    }
+
+    private String winnigNumbers(){
+        System.out.println(InputMessage.NUMBER_INPUT_MESSAGE);
+        String input = Console.readLine();
+        InputValidator.validateNumbers(input);
+
+        return input;
+    }
+
+    public Lotto setWinningNumbers(){
+        Lotto winning = new Lotto(createWinningNumber());
+
+        return winning;
+    }
+
+    private List<Integer> createWinningNumber(){
+        String input = winnigNumbers();
+        List<String> list = Arrays.asList(input.split(INPUT_DELIM));
+        List<Integer> numbers = new ArrayList<>();
+        for(String s : list){
+            numbers.add(Integer.parseInt(s));
+        }
+
+        return numbers;
     }
 
 }
--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -2,9 +2,39 @@ package lotto;
 
 public class InputValidator {
     private static final String DIGIT_REGEX = "^-?[0-9]*$";
+    private static final String INPUT_DELIM = ",";
 
-    public static void validateIsNumber(String amount){
+    public static void validateMoneyIsNumber(String amount){
         if(!amount.matches(DIGIT_REGEX))
             throw new IllegalArgumentException(ErrorMessage.AMOUNT_NOT_NUMBER_EXCEPTION.getMessage());
     }
+
+    public static void validateNumbers(String input){
+        validateIsDivisble(input);
+        validateCountNumber(input);
+        validateIsNumber(input);
+    }
+
+    private static void validateIsDivisble(String input){
+        try {
+            String[] inputs = input.split(INPUT_DELIM);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_DIVISION_EXCEPTION.getMessage());
+        }
+    }
+
+    private static void validateCountNumber(String input){
+        if(input.split(INPUT_DELIM).length != 6)
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_COUNT_EXCEPTION.getMessage());
+    }
+
+    private static void validateIsNumber(String input){
+        String[] inputs = input.split(INPUT_DELIM);
+        for(String s : inputs){
+            if(!s.matches(DIGIT_REGEX))
+                throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_NUMBER_EXCEPTION.getMessage());
+        }
+
+    }
+
 }
