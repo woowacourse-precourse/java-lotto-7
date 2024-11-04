3391f4956d670e7473e09848c38f54e14cf3833d
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 20:25:15 +0900

    feat: 로또 구입 금액 입력이 1,000원으로 나누어 떨어지지 않는 경우 예외 발생 기능 추가
    - checkDivisibilityByLottoPrice() 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -6,6 +6,7 @@ public class InputValidator {
         try {
             int payment = Integer.parseInt(input);
             checkMinimumPayment(payment);
+            checkDivisibilityByLottoPrice(payment);
             return payment;
 
         } catch (NumberFormatException e) {
@@ -16,6 +17,12 @@ public class InputValidator {
     private void checkMinimumPayment(int payment) {
         if (payment < LOTTO_PRICE) {
             throw new IllegalArgumentException("[ERROR] 로또는 1개 이상 구매 가능합니다.");
+        }
+    }
+
+    private void checkDivisibilityByLottoPrice(int payment) {
+        if (payment % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위만 가능합니다.");
         }
     }
 }
