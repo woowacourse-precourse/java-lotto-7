f8a1531ec4411c0d5956e514f14ec77c3634b10b
Koo-EunSung <ku903@naver.com>
2024-11-04 14:42:54 +0900

    Refactor: 상수명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,26 +3,26 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_MONEY_INFORM_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String MONEY_INFORM_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public String getMoney() {
-        System.out.println(INPUT_MONEY_INFORM_MESSAGE);
+        System.out.println(MONEY_INFORM_MESSAGE);
 
         String input = Console.readLine();
         return input;
     }
 
     public String getWinningNumber() {
-        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+        System.out.println(WINNING_NUMBER_MESSAGE);
 
         String input = Console.readLine();
         return input;
     }
 
     public String getBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.println(BONUS_NUMBER_MESSAGE);
 
         String input = Console.readLine();
         return input;
