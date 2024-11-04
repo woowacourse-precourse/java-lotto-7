25d82766d367d25a35e8cb9f9c95557528236a3f
btongtong <leeym2615@naver.com>
2024-11-04 20:45:22 +0900

    feat(Ranking): Add enum class for ranking information
    Add hitCount, bonusHit, prize variables for each ranking
    Add getEnumName() to get enum name based on hitCount and bonusHit

---
+++ src/main/java/lotto/Ranking.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Ranking {
+    NO_RANK(0, false, 0),
+    FIFTH_PLACE(3, false, 5000),
+    FOURTH_PLACE(4, false, 50000),
+    THIRD_PLACE(5, false, 1500000),
+    SECOND_PLACE(5, true, 30000000),
+    FIRST_PLACE(6, false, 2000000000);
+
+    private final int hitCount;
+    private final boolean bonusHit;
+    private final long prize;
+
+    Ranking(int hitCount, boolean bonusHit, long prize) {
+        this.hitCount = hitCount;
+        this.bonusHit = bonusHit;
+        this.prize = prize;
+    }
+
+    public static String getEnumName(int hitCount, boolean bonusHit) {
+        Ranking ranking = Arrays.stream(Ranking.values())
+                .filter(rank -> rank.hitCount == hitCount && rank.bonusHit == bonusHit)
+                .findFirst().orElse(NO_RANK);
+        return ranking.name();
+    }
+}
