b97928b30eeaa0c63207f560d62899e475b7715a
jungsu <vwx1@naver.com>
2024-11-04 14:55:54 +0900

    feat: controller 생성 및 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,27 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.service.LottosServiceInterface;
+import lotto.validator.InputValidator;
+import lotto.validator.InputValidatorInterface;
+import lotto.view.InputView;
+import lotto.view.InputViewInterface;
+import lotto.view.OutputView;
+import lotto.view.OutputViewInterface;
+
 public class Application {
 
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
+    LottosServiceInterface lottoService = new LottoService();
+    InputViewInterface inputView = new InputView();
+    OutputViewInterface outputView = new OutputView();
+    InputValidatorInterface inputValidator = new InputValidator();
+
+    LottoController controller = new LottoController(inputView, outputView, lottoService,
+        inputValidator);
+
+    controller.run();
   }
+
 }
