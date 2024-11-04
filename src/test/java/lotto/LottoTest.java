812ac712917bc32215fb1f24cd1b402d827cd135
SuyeonShinnnn <suyeons5536@gmail.com>
2024-11-04 14:59:18 +0900

    test: 보너스 포함 여부 확인 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
     @Test
@@ -24,8 +24,10 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
     @Test
-    void getMatchedSize(){
+    void 번호_일치_개수(){
 
         // 주어진 로또 번호
         List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
@@ -40,7 +42,24 @@ class LottoTest {
         // 예상 결과 (1, 2, 3 일치 => 예상 결과 3)
         assertEquals(3, matchedSize);
     }
+    @Test
+    void 보너스_번호_있음(){
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(lottoNumbers);
 
+        int bonusNumber = 7;
+        boolean hasBonusNumber = lotto.hasBonusNumber(bonusNumber);
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+        assertFalse(hasBonusNumber);
+    }
+    @Test
+    void 보너스_번호_없음(){
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        int bonusNumber = 6;
+        boolean hasBonusNumber = lotto.hasBonusNumber(bonusNumber);
+
+        assertTrue(hasBonusNumber);
+    }
 }
