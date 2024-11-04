9a6b7640835a73188895d9cf797ece0cd06790e0
persi <jina8151010@gmai.com>
2024-11-04 17:34:24 +0900

    test: 변경된 LottoGenerator를 테스트 코드에 적용

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -14,7 +14,7 @@ class LottoGeneratorTest {
     @Test
     void 로또_티켓_개수_계산() {
         // Given
-        LottoGenerator generator = new LottoGenerator();
+        LottoGenerator generator = new DefaultLottoGenerator();
         int purchaseAmount = 5000;
 
         // When
@@ -28,20 +28,22 @@ class LottoGeneratorTest {
     @Test
     void 로또_티켓_번호_중복_확인() {
         // Given
-        LottoGenerator generator = new LottoGenerator();
+        DefaultLottoGenerator generator = new DefaultLottoGenerator();
 
         // When
         Lotto lotto = generator.generateLotto();
 
         // Then
-        assertThat(lotto).isNotNull();  // 생성된 lotto 객체가 유효한지 검증
+        assertThat(lotto).isNotNull(); // 생성된 lotto 객체가 유효한지 검증
+        assertThat(lotto.getNumbers()).hasSize(6); // 숫자 개수 확인
+        assertThat(lotto.getNumbers().stream().distinct().count()).isEqualTo(6); // 중복 확인
     }
 
     @DisplayName("로또 번호 생성 중 예외 발생 시 예외 메시지 출력")
     @Test
     void 로또번호_생성_예외발생시_예외메시지_출력() {
         // Given
-        LottoGenerator generator = new LottoGenerator() {
+        LottoGenerator generator = new DefaultLottoGenerator() {
             @Override
             protected Lotto generateLotto() {
                 throw new IllegalArgumentException("[ERROR] 임의로 예외 발생");
