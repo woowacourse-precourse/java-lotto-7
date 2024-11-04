9a41f347c3d26f781e771e2d2a9c7730b0f2774f
Su-daa <subin.kk019@gmail.com>
2024-11-04 16:38:06 +0900

    feat: 로또 구매 금액 입력 시 예외 처리 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,8 @@ package lotto.controller;
 
 import lotto.model.LottoBuyer;
 import lotto.model.LottoMachine;
+import lotto.model.Money;
+import lotto.view.InputView;
 
 public class LottoController {
     private final LottoBuyer lottoBuyer;
@@ -9,5 +11,15 @@ public class LottoController {
     public LottoController() {
         LottoMachine lottoMachine = new LottoMachine();
         this.lottoBuyer = new LottoBuyer(lottoMachine);
+    }
+
+    private Money inputPurchaseAmount() {
+        while (true) {
+            try {
+                return InputView.inputPurchaseAmount();
+            } catch (IllegalArgumentException e) {
+                InputView.errorPrint(e.getMessage());
+            }
+        }
     }
 }
