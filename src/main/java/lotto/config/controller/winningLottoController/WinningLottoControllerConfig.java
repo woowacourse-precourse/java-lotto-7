980d1d4c0596a4944110ff77e156b5451649d04a
이창근 <dlckdrms0517@gmail.com>
2024-11-04 11:54:38 +0900

    feat: WinningLottoControllerConfig 작성

---
+++ src/main/java/lotto/config/controller/winningLottoController/WinningLottoControllerConfig.java
@@ -0,0 +1,8 @@
+package lotto.config.controller.winningLottoController;
+
+import lotto.controller.winningLottoController.WinningLottoController;
+
+public interface WinningLottoControllerConfig {
+
+    WinningLottoController getWinningLottoController();
+}
