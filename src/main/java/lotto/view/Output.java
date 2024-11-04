047d5b4a8f4b4b358ad349603f8588b7e630d508
yubin8773 <hiyubin.lee@gmail.com>
2024-11-04 23:43:50 +0900

    feat : 로또 결과 출력

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,4 +1,30 @@
 package lotto.view;
 
+import lotto.run.Lotto;
+import lotto.run.Prize;
+
+import java.util.List;
+import java.util.Map;
+
 public class Output {
+
+    public static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+        System.out.println();
+    }
+
+    public static void printLottoState(Map<Prize, Integer> lottoStats) {
+        System.out.println("\n당첨 통계\n---");
+        lottoStats.forEach((prize, count) -> {
+            if (prize != Prize.NO_PRIZE) {
+                System.out.printf("%s (%,d원)- %d개%n", prize.getDisplayName(), prize.getPrizeNum(), count);
+            }
+        });
+    }
+
+    public static void printTotalPrizeRate(double totalPrizeRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.", totalPrizeRate);
+    }
 }
