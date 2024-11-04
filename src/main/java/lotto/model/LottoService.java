c643e833f3ee451622f5c264b307564aa7a86a4f
eunseobb <seobseob0810@naver.com>
2024-11-04 20:00:07 +0900

    feat(model): 구매한 로또와 입력한 로또 번호 비교 기능 추가
    - 구매한 각 로또와 입력한 로또 번호를 비교하여 번호가 일치하는 개수를 반환하는 메서드 추가
    - 일치하는 번호의 개수가 5개일
    때 보너스 번호와 일치하는 로또의 개수를 반환하는 메서드 추가

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -2,10 +2,13 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class LottoService {
+    public static final int BONUS_KEY = 55;
+    private static final int CHECK_BONUS = 5;
+
     public List<Lotto> pickLottoNumbers(int lottoCount) {
         List<Lotto> lottos = IntStream.range(0, lottoCount)
             .mapToObj(i -> new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
@@ -14,5 +17,46 @@ public class LottoService {
         return lottos;
     }
 
+    public Map<Integer, Integer> checkWinningNumber(List<Lotto> lottos, List<Integer> myNumbers, Integer bonusNumber) {
+        Map<Integer, Integer> winningCount = initNumberCount();
+        int bonusCount = checkBonusPrize(lottos, myNumbers, bonusNumber);
+        lottos.forEach(lotto -> {
+            long matchCount = lotto.getNumbers().stream()
+                .filter(myNumbers::contains)
+                .count();
+            if (matchCount >= 3) {
+                winningCount.put((int) matchCount, winningCount.get((int) matchCount) + 1);
+            }
+            if (matchCount == CHECK_BONUS) {
+                winningCount.put(BONUS_KEY, bonusCount);
+                winningCount.put(CHECK_BONUS, winningCount.get(CHECK_BONUS) - bonusCount);
+            }
+        });
+        return winningCount;
+    }
+
+
+    private Integer checkBonusPrize(List<Lotto> lottos, List<Integer> myNumbers, Integer bonusNumber) {
+        return (int) lottos.stream()
+            .filter(lotto -> {
+                long matchCount = lotto.getNumbers().stream()
+                    .filter(myNumbers::contains)
+                    .count();
+                return matchCount == CHECK_BONUS && lotto.getNumbers().contains(bonusNumber);
+            })
+            .count();
+    }
+
+    private Map<Integer, Integer> initNumberCount() {
+        Map<Integer, Integer> matchCount = new HashMap<>();
+
+        matchCount.put(3, 0);
+        matchCount.put(4, 0);
+        matchCount.put(5, 0);
+        matchCount.put(6, 0);
+        matchCount.put(BONUS_KEY, 0); // 5개 일치 + 보너스 번호
+
+        return matchCount;
+    }
 
 }
