d59f2de003da6bfa7264e9eb57a25936790b6e9f
Hyejeong <thswjd0609@gmail.com>
2024-11-04 19:16:28 +0900

    feat: 보너스 번호 범위 검증 기능

--- src/main/java/lotto/model/Validator.java
+++ src/main/java/lotto/model/Validator.java
@@ -13,4 +13,11 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위 입니다.");
         }
     }
+
+    public void validateBonusRange(int bonusNum) {
+        if(bonusNum < 1 || bonusNum > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
 }
