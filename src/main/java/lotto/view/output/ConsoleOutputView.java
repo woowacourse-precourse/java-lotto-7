7fb54590649040f974c15db24df09f19410ca77b
haebyun <bhk4392@gmail.com>
2024-11-04 15:18:03 +0900

    feat(ConsoleOutputView): 구매한 로또 목록 출력 기능 추가
    - printPurchasedLottos 메서드 추가하여 구매한 로또의 개수와 번호 목록을 출력하도록 구현
    - OutputFormatter를 사용하여 개수와 번호 목록을 형식에 맞추도록 구현
    - 로또 개수 메시지와 번호 메시지를 결합하여 출력

--- src/main/java/lotto/view/output/ConsoleOutputView.java
+++ src/main/java/lotto/view/output/ConsoleOutputView.java
@@ -1,8 +1,24 @@
 package lotto.view.output;
 
+import lotto.dto.response.PurchasedLottosDTO;
+import lotto.view.output.util.OutputFormatter;
+
 public class ConsoleOutputView implements OutputView {
     @Override
     public void printError(String message) {
         System.out.println(message + "\n");
+    }
+
+    @Override
+    public void printPurchasedLottos(PurchasedLottosDTO purchasedLottosDTO) {
+        String purchaseCountMessage = OutputFormatter.formatPurchaseLottoCount(
+                purchasedLottosDTO.purchasedLottoDTOs().size()
+        );
+        String lottoNumbersMessage = OutputFormatter.formatLottoNumbers(
+                purchasedLottosDTO.purchasedLottoDTOs()
+        );
+        String output = purchaseCountMessage + lottoNumbersMessage;
+
+        System.out.println(output);
     }
 }
