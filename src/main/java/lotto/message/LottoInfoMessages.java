de0814658562661e22ed70e6374b1c246c23c11c
byeongdoohan <quden04@gmail.com>
2024-11-04 19:37:09 +0900

    refactor: text 일부 변경

--- src/main/java/lotto/message/LottoInfoMessages.java
+++ src/main/java/lotto/message/LottoInfoMessages.java
@@ -2,7 +2,7 @@ package lotto.message;
 
 public enum LottoInfoMessages {
     INSERT_PAY("구입금액을 입력해 주세요."),
-    NOTICE_BUY_AMOUNT_START("개를 구매했습니다. (잔돈 : "),
+    NOTICE_BUY_AMOUNT_START("개를 구매했습니다."),
     NOTICE_BUY_AMOUNT_END("원)"),
     INSERT_MANUAL_AMOUNT_START("수동 번호로 구매할 갯수를 입력해주세요. (최대 "),
     INSERT_MANUAL_AMOUNT_END("개 입력 가능)"),
@@ -12,7 +12,7 @@ public enum LottoInfoMessages {
     INSERT_NUMBERS_MIDDLE("""
             (수동 구매 갯수가 [\s"""),
     INSERT_NUMBERS_END(" ]개 남았습니다.)"),
-    TOTAL_BUY_AMOUNT("""
+    BUY_LOTTO_LIST("""
         [구입 번호 목록]"""),
     INSERT_WINNING_NUMBER("""
             6개의 당첨 번호를 입력해 주세요. (1~45 사이의 겹치지 않는 6개의 숫자를 쉼표로 구분해 입력, Enter를 누르면 자동으로 번호를 입력합니다.)
