8d4d79fb923459822619c14a85df2d59de6f48c4
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2024-11-04 20:13:03 +0900

    refactor : LottoResultTest의 returnRate 검증 결과 값 및 출력값 수정

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -17,7 +17,7 @@ public class LottoResultTest {
     @Test
     void 로또_결과_출력_형식과_반환율_확인() {
         //Given
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(9, 10, 11, 12, 13, 14), 7);
         List<Lotto> purchasedLotto = Arrays.asList(
                 new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), //6개 일치(1등)
                 new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)), //5개 + 보너스 일치(2등)
@@ -33,12 +33,12 @@ public class LottoResultTest {
         double returnRate = lottoResult.getReturnRate();
 
         //Then
-        assertThat(returnRate).isGreaterThan(0);
+        assertThat(returnRate).isEqualTo(100);
         String expectOutput = "3개 일치 (5,000원) - 1개\n"
-                + "4개 일치 (50,000원) - 1개\n"
-                + "5개 일치 (1,500,000원) - 1개\n"
-                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n"
-                + "6개 일치 (2,000,000,000원) - 1개";
+                + "4개 일치 (50,000원) - 0개\n"
+                + "5개 일치 (1,500,000원) - 0개\n"
+                + "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개\n"
+                + "6개 일치 (2,000,000,000원) - 0개";
 
         assertThat(lottoResult.formatResults()).isEqualTo(expectOutput);
     }
