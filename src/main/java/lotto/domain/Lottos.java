7dcc528b2e46798b29e287c41c89ab187701faad
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-04 19:42:11 +0900

    Feat: Lotto의 일급컬렉션 Lottos 생성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
+import lotto.common.LottoRank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }
