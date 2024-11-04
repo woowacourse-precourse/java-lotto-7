f90b8325a3f23dfd972c13cb70167b51c6415d0e
hakSick <koukai@kakao.com>
2024-11-04 22:45:01 +0900

    feat(LottoMachine): 로또 티켓 생성

--- src/main/java/model/LottoMachine.java
+++ src/main/java/model/LottoMachine.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
+    private final List<Lotto> lottoTickets = new ArrayList<>();
+
+    public void buyLottoTickets(int money) {
+        int ticketCount = money / 1000;
+        for (int i = 0; i < ticketCount; i++) {
+            lottoTickets.add(new Lotto(Lotto.generateLottoNumbers()));
+        }
+    }
+
+    public List<Lotto> getLottoTickets() {
+        return lottoTickets;
+    }
 }
