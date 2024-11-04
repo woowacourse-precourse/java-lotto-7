c978d42ff97b24838c7f59935d16efd923baabc2
SinnoLn <sintmtn69@gmail.com>
2024-11-04 17:39:10 +0900

    feat: Application 클래스에서 로또 프로그램 실행 로직 구현
    - main 메서드에서 InputView, ResultView, LottoMachine, LottoService, LottoController 인스턴스 생성
    - LottoController의 run 메서드를 호출하여 프로그램 실행 흐름을 제어
    - 프로그램 초기화 및 실행 설정을 main 메서드에서 구현하여 전체 구조를 간결하고 명확하게 구성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.LottoMachine;
+import lotto.model.strategy.RandomNumberGeneration;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        LottoMachine lottoMachine = new LottoMachine(new RandomNumberGeneration());
+        LottoService lottoService = new LottoService(lottoMachine);
+        LottoController lottoController = new LottoController(lottoService, inputView, resultView);
+
+        lottoController.run();
     }
 }
