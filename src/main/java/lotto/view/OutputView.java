be6338b39596e86ed19e8505f926bc8f6dc42354
cherryiJuice <chldlwn711@gmail.com>
2024-11-04 17:37:18 +0900

    refactor: 출력 형식 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     private static final String PURCHASE_COUNT = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS = "당첨 통계\n---";
     private static final String PRIZE_DETAIL = "%s (%,d원) - %d개\n";
-    private static final String REVENUE_RESULT = "총 수익률은 %.1f%%입니다.";
+    private static final String REVENUE_RESULT = "총 수익률은 %,.1f%%입니다.";
 
 
     public void printPurchaseCount(int purchaseCount) {
