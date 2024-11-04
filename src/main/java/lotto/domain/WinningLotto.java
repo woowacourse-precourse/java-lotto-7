458fd054570ca0fd8c6278392f5907c8ad0b872d
BusanGukbap <dong2857210@gmail.com>
2024-11-04 15:43:28 +0900

    feat(Domain) : WinningLotto에 당첨 번호 및 보너스 번호 저장

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
+import lotto.util.LottoValidator;
+
+import java.util.List;
+
 public class WinningLotto {
+
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        LottoValidator.validateBonusNumber(bonusNumber, winningNumbers);
+        this.winningLotto = new Lotto (winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
