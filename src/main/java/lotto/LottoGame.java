ec623e7046b8cb4814f591b6de72e23b4ef5669d
bjy <qkswnsdud@gmail.com>
2024-11-04 18:31:58 +0900

    feat(InputAmount): InputAmount 기능 구현
    로또 구매액을 사용자로부터 입력받는 기능을 구현했습니다. 입력값이
    올바르지 않은 경우 ERROR를 발생시킵니다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,5 +2,14 @@ package lotto;
 
 public class LottoGame {
     private static final int LOTTO_PRICE = 1000;
+    LottoGameValidator lottoGameValidate = new LottoGameValidator();
 
+    int amount;
+    int lottoQuantity;
+
+    public void inputAmount() {
+        // 로또 구매액 설정
+        this.amount = lottoGameValidate.readAmount(LOTTO_PRICE);
+        this.lottoQuantity = amount / LOTTO_PRICE;
+    }
 }
--- src/main/java/lotto/LottoGameValidator.java
+++ src/main/java/lotto/LottoGameValidator.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoGameValidator {
+
+    public void validateAmount(int amount, int LOTTO_PRICE) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+        }
+    }
+
+    public int readAmount(int LOTTO_PRICE) {
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int amount = Integer.parseInt(Console.readLine());
+                validateAmount(amount, LOTTO_PRICE);
+                return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자 형식이 올바르지 않습니다. 다시 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
