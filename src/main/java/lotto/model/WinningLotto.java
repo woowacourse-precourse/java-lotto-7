2ff0b3df4c9161722581c1dbba46b579324c4a63
eunho <eunho1032@gmail.com>
2024-11-04 13:54:22 +0900

    refactor: remove exception

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -8,9 +8,6 @@ public class WinningLotto {
     private final int bonusNumber;
 
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
-        if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-        }
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
