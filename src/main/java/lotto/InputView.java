be5e0ba7469989df11515601c926360e686d7697
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 18:05:25 +0900

    feat(InputView): 당첨 번호를 입력받는다

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,12 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
+
+    private static final String LOTTO_NUMBERS_DELIMITER = ",";
 
     public long promptAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         return Long.parseLong(input);
+    }
+
+    public List<Integer> promptWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(LOTTO_NUMBERS_DELIMITER))
+                .map(Integer::parseInt)
+                .toList();
     }
 }
