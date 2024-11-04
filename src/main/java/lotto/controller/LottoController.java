3996e5149854e5c5ca2b445b4065afb5dc19be6e
byeongdoohan <quden04@gmail.com>
2024-11-04 13:03:07 +0900

    feat: 프로그램 전체 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,34 +1,60 @@
 package lotto.controller;
 
-import lotto.exception.LottoErrorMessages;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import lotto.service.LottoService;
+import lotto.service.PrintService;
 import lotto.service.ValidationService;
-import lotto.view.LottoInfoMessages;
-import lotto.view.StatisticsView;
 
 public class LottoController {
-    LottoInfoMessages lottoInfoMessages;
-    StatisticsView statisticsView = StatisticsView.createStatisticsView();
+    private static final String MANUAL = "manual";
+    private static final String WINNER = "winner";
+    private static final int WINNER_UNIT = 1;
+    private static final int CALCULATE_UNIT = 1000;
+
+    private int pay = 0;
+    private int amount = 0;
+    private int manualAmount = 0;
+    private int change = 0;
+    private int bonusNumber = 0;
+    private List<Set<Integer>> manualLottoList = new ArrayList<>();
+    private List<Set<Integer>> autoLottoList = new ArrayList<>();
+    private List<Set<Integer>> totalLottoList = new ArrayList<>();
+    private List<Set<Integer>> winnerLotto = new ArrayList<>();
+    private List<int[]> matchedList = new ArrayList<>();
+
     LottoService lottoService = LottoService.createLottoService();
     ValidationService validationService = ValidationService.createValidationService();
+    PrintService printService = PrintService.createPrintService();
 
-    private int pay;
-    private int amount;
-    private int change;
 
     public void runLottoProgram() {
         // 구입 금액 입력
         pay = validationService.validatePayInput();
         amount = lottoService.calculateAmount(pay);
         change = lottoService.calculateChange(pay);
-        lottoService.printNoticeBuyAmount(amount, change);
+        printService.printNoticeBuyAmount(amount, change);
         // 수동 구매 갯수 입력
-//        lottoView.printInsertManualAmount(amount);
+        manualAmount = validationService.validateManualAmountIsInteger(amount);
+        // 수동 번호 입력
+        manualLottoList = lottoService.generateManualNumberSet(manualAmount, MANUAL);
+        // 남은 갯수만큼 자동번호 생성
+        autoLottoList = lottoService.generateAutoNumberSet(amount - manualLottoList.size());
+        // 모든 로또 번호 목록 갱신
+        totalLottoList = lottoService.getTotalLottoList(manualLottoList, autoLottoList);
+        // 구매 번호 출력
+        printService.printTotalLottoList(amount, autoLottoList, manualLottoList);
+        // 당첨 번호 입력
+        printService.printWinnerNumbersInfo();
+        winnerLotto = lottoService.generateManualNumberSet(WINNER_UNIT, WINNER);
+        // 당첨 번호 출력
+        printService.printWinnerNumbers(winnerLotto);
+        // 보너스 번호 입력
+        printService.printBonusNumbersInfo();
+        bonusNumber = validationService.validateBonusNumber(winnerLotto);
+        // 당첨 통계 계산
+        matchedList = lottoService.calculateMatched(totalLottoList, winnerLotto, bonusNumber);
+        printService.printStatistics(matchedList, amount * CALCULATE_UNIT);
     }
-
-    private void purchaseLotto() {
-
-    }
-
-
 }
