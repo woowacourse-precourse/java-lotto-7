10cb3534137ec383c43392b3fabd7d95a8489087
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 17:01:34 +0900

    test(validator): 당첨 번호 검증 테스트 추가(문자열인 경우 예외 발생)

--- src/test/java/lotto/domain/ValidatorTest.java
+++ src/test/java/lotto/domain/ValidatorTest.java
@@ -11,7 +11,7 @@ class ValidatorTest {
     @Test
     void 구입_금액이_숫자가_아니면_예외가_발생한다() {
         // given & when
-        assertThatThrownBy(() -> Validator.validatePurchaseAmount("abc"))
+        assertThatThrownBy(() -> Validator.validateAndParsePurchaseAmount("abc"))
         // then
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
@@ -21,7 +21,7 @@ class ValidatorTest {
     @Test
     void 구입_금액이_1000원_단위가_아니면_예외가_발생한다() {
         // given & when
-        assertThatThrownBy(() -> Validator.validatePurchaseAmount("1500"))
+        assertThatThrownBy(() -> Validator.validateAndParsePurchaseAmount("1500"))
         // then
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
@@ -30,6 +30,16 @@ class ValidatorTest {
     @DisplayName("구입 금액이 올바르면 예외가 발생하지 않는다.")
     @Test
     void 구입_금액이_올바르면_예외가_발생하지_않는다() {
-        Validator.validatePurchaseAmount("2000");
+        Validator.validateAndParsePurchaseAmount("2000");
+    }
+
+    @DisplayName("당첨 번호 입력 시 문자열이 포함된 경우 예외가 발생한다.")
+    @Test
+    void 당첨_번호에_문자열이_포함된_경우_예외가_발생한다() {
+        // given & when
+        assertThatThrownBy(() -> Validator.validateAndParseWinningNumbers("1,2,3,a,5,6"))
+        // then
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 숫자로 입력해야 합니다.");
     }
 }
