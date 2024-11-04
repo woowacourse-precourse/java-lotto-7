19a928aea6203383ff03f31e6d12ecaa53d83083
moonbinchoi <whsahd12@gmail.com>
2024-11-04 18:47:20 +0900

    refactor(Application): LottoService의 인자에 BonusNumberParser를 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import java.util.List;
 import lotto.domain.controller.LottoController;
 import lotto.domain.service.LottoService;
+import lotto.domain.util.parser.BonusNumberParser;
 import lotto.domain.util.parser.DelimitedNumberParser;
 import lotto.domain.util.parser.MoneyParser;
 import lotto.domain.util.parser.StringParser;
@@ -10,13 +11,15 @@ import lotto.global.io.InputConsole;
 import lotto.global.io.OutputConsole;
 
 public class Application {
+
     public static void main(String[] args) {
         StringParser<List<Integer>> numberParser = DelimitedNumberParser.getInstance();
+        StringParser<Integer> bonusParser = BonusNumberParser.getInstance();
         StringParser<Integer> moneyParser = MoneyParser.getInstance();
         OutputConsole.init(System.out);
         OutputConsole output = OutputConsole.getInstance();
 
-        LottoService.init(numberParser, moneyParser);
+        LottoService.init(numberParser, bonusParser, moneyParser);
         LottoService service = LottoService.getInstance();
 
         try (InputConsole input = new InputConsole()) {
