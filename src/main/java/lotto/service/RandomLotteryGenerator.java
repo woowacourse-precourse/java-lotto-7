fdee1f00e7d125b919031603f87058bf41c57074
MinjuKwak01 <mkwak1125@gmail.com>
2024-11-04 19:06:04 +0900

    refactor (RandomLotteryGenerator) : 랜덤 값 추출할 때 사용되는 값 상수로 선언

--- src/main/java/lotto/service/RandomLotteryGenerator.java
+++ src/main/java/lotto/service/RandomLotteryGenerator.java
@@ -8,8 +8,12 @@ import lotto.domain.LottoGroup;
 
 public class RandomLotteryGenerator {
 
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int COUNT_NUMBER = 6;
+
     public Lotto generateEachLotto() {
-        List<Integer> lotteryNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotteryNumbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, COUNT_NUMBER);
         return new Lotto(lotteryNumbers);
     }
 
