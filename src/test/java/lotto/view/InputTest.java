946d94f3de27cb74fe7e765b545c2e1f8bef76dc
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 22:12:12 +0900

    refact: 사용하지 않는 import 삭제

--- src/test/java/lotto/view/InputTest.java
+++ src/test/java/lotto/view/InputTest.java
@@ -1,11 +1,8 @@
 package lotto.view;
 
 import java.io.ByteArrayInputStream;
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
