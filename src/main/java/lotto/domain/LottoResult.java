3780e39e0ed93e0b4f342b7159c0e29ccf10f2a0
oznchex <114240463+oznchex@users.noreply.github.com>
2024-11-04 22:38:45 +0900

    fix(domain): LottoPrize enum을 사용하는 로직으로 LottoResult 메서드 수정
    - 로또 결과와 통계를 출력하는 메서드를 prize에 구현하고 이를 호출하도록 변경
    - Map 자료구조를 사용해서 LottoPrize를 불러옴

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,43 +1,29 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class LottoResult {
-    private int threeNumberMatchCount;
-    private int fourNumberMatchCount;
-    private int fiveNumberMatchCount;
-    private int fiveNumberAndBonusMatchCount;
-    private int sixNumberMatchCount;
+    private final Map<LottoPrize, Integer> prizeCountMap;
 
     public LottoResult() {
-        this.threeNumberMatchCount = 0;
-        this.fourNumberMatchCount = 0;
-        this.fiveNumberMatchCount = 0;
-        this.fiveNumberAndBonusMatchCount = 0;
-        this.sixNumberMatchCount = 0;
+        prizeCountMap = new EnumMap<>(LottoPrize.class);
+        for (LottoPrize prize : LottoPrize.values()) {
+            prizeCountMap.put(prize, 0);
+        }
     }
 
     public void updateLottoResult(final int matchingCount, final boolean isBonus) {
-        if (matchingCount == 3) {
-            threeNumberMatchCount += 1;
-        }
-        if (matchingCount == 4) {
-            fourNumberMatchCount += 1;
-        }
-        if (matchingCount == 5 && !isBonus) {
-            fiveNumberMatchCount += 1;
-        }
-        if (matchingCount == 5 && isBonus) {
-            fiveNumberAndBonusMatchCount += 1;
-        }
-        if (matchingCount == 6) {
-            sixNumberMatchCount += 1;
+        LottoPrize prize = LottoPrize.valueOf(matchingCount, isBonus);
+        if (prize != null) {
+            prizeCountMap.put(prize, prizeCountMap.get(prize) + 1);
         }
     }
-    
+
     public void printLottoWinningStatistics() {
-        System.out.println("3개 일치 (5,000원) - " + threeNumberMatchCount +"개");
-        System.out.println("4개 일치 (50,000원) - " + fourNumberMatchCount +"개");
-        System.out.println("5개 일치 (1,500,000원) - " + fiveNumberMatchCount +"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + fiveNumberAndBonusMatchCount +"개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + sixNumberMatchCount +"개");
+        for (LottoPrize prize : LottoPrize.values()) {
+            int count = prizeCountMap.get(prize);
+            System.out.println(prize.getDescription() + " - " + count + "개");
+        }
     }
 }
