19d4864a15dff505b0f31998e72076443f80b575
sjchae <soojch12@gmail.com>
2024-11-04 22:58:39 +0900

    feat: 당첨 로또 번호 클래스 추가

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto {
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+        validate(numbers, bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다. ");
+        }
+    }
+}
