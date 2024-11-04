32896ad4303ef982d8b9fa19f16e418e51a8ec38
LSM <leesung2925@gmail.com>
2024-11-04 15:22:43 +0900

    chore: 프로젝트 구조 설정 및 기본 클래스 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -14,7 +17,20 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        for(int number : numbers){
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
+    public static Lotto generateRandomLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(randomNumbers);
+    }
 }
