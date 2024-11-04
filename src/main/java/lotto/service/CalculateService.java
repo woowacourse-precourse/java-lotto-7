3b5519772a95978755730d3d969127fbb5018b0f
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-04 20:18:13 +0900

    feat: CalculateService 구현

---
+++ src/main/java/lotto/service/CalculateService.java
@@ -0,0 +1,37 @@
+package lotto.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import lotto.domain.dto.LottoResult;
+
+public class CalculateService {
+
+    // 수익률 계산
+    public BigDecimal calculateRatio(int amount, Map<LottoResult, Integer> drawResult){
+        BigDecimal ratio = BigDecimal.valueOf(calculatePrize(drawResult))
+                .divide(BigDecimal.valueOf(amount))
+                .multiply(BigDecimal.valueOf(100))
+                .stripTrailingZeros(); // 뒤에 불필요한 0 제거
+        return new BigDecimal(ratio.toPlainString()); // 일반 숫자 형식으로 변환
+    }
+
+    // 당첨금액 계산
+    private int calculatePrize(Map<LottoResult, Integer> drawResult){
+        int sum = 0;
+        for (LottoResult lottoResult: drawResult.keySet()){
+            int matchingNumberCount = lottoResult.getMatchingNumberCount();
+            System.out.println(matchingNumberCount);
+            if (matchingNumberCount == 5 && lottoResult.isBonusMatch()){
+                sum += MatchPrize.FIVE_MATCH_WITH_BONUS.getPrize();
+                System.out.println(sum);
+            }
+            sum += MatchPrize.getPrizeByMatchCount(lottoResult.getMatchingNumberCount());
+            System.out.println(sum);
+        }
+        return sum;
+    }
+
+
+}
+
