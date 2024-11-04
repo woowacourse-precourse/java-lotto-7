1bb26a420456ad221b042d9fc1a7e0af4be355a8
jisukimtech <jisukim.tech@gmail.com>
2024-11-04 14:37:51 +0900

    refactor(service): service 패키지 생성 및 구조 변경
    - LottoGenerator 클래스를 service 패키지로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.service.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         LottoGameController lottoGameController = new LottoGameController(new InputView(), new OutputView(),
