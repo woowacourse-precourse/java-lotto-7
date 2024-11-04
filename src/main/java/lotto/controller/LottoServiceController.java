0efdaf8563faccd390a3721b3cceebfc6c0c71aa
ratatuwee <pkqwaszx@gmail.com>
2024-11-04 16:21:57 +0900

    feat(lotto service) : 로또 서비스 컨트롤러 추가
    유저가 구매한 로또 횟수만큼 로또를 발행하고 당첨 현황 및 수익율을 계산하는 컨트롤러입니다.
    도메인 로직에 종속되어 의존성이 높아지지 않게 데이터만을 뷰에 넘겨 출력하도록 구현하였습니다.

---
+++ src/main/java/lotto/controller/LottoServiceController.java
@@ -0,0 +1,77 @@
+package lotto.controller;
+
+import lotto.Lotto;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinRecord;
+import lotto.domain.WinningLotto;
+import lotto.utility.Utils;
+import lotto.view.ResultOutput;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
+public class LottoServiceController {
+    private final PurchaseAmount purchaseAmount;
+    private final WinningLotto winningLotto;
+    private final List<Lotto> userLottos;
+    private final WinRecord winRecord;
+    private final ResultOutput resultOutput = new ResultOutput();
+
+    public LottoServiceController() {
+        purchaseAmount = PurchaseAmount.getPurchaseAmount();
+        winningLotto = WinningLotto.getWinningLotto();
+        userLottos = createUserLottos(purchaseAmount.getPurchaseCount());
+        winRecord = WinRecord.getWinRecord(getWinningSummary());
+    }
+
+
+    private List<Lotto> createUserLottos(int purchaseCount) {
+        List<Lotto> userLottos = new ArrayList<>();
+        for (int i = 0; i < purchaseCount; i++) {
+            userLottos.add(new Lotto
+                    (Utils.getRandomNumbers
+                            (Lotto.MIN_LOTTO_NUMBER, Lotto.MAX_LOTTO_NUMBER, Lotto.LOTTO_NUMBER_COUNT)));
+        }
+        userLottos = Collections.unmodifiableList(userLottos);
+        return userLottos;
+    }
+
+    private List<Integer> getWinningSummary() {
+        List<Integer> winningSummary = new ArrayList<>();
+        for (Lotto userLotto : userLottos) {
+            winningSummary.add(getWinningCount
+                    (userLotto.getLottoNumbers()));
+        }
+        return winningSummary;
+    }
+
+    private int getWinningCount(List<Integer> userLotto) {
+        List<Integer> checkDuplication = new ArrayList<>(userLotto);
+        checkDuplication.retainAll(winningLotto.getLottoNumbers());
+        return checkDuplication.size();
+    }
+
+    private double getTotalYield(){
+        BigDecimal totalPurchaseAmount = new BigDecimal(purchaseAmount.getPurchaseAmountValue());
+        BigDecimal totalWinningPrize = new BigDecimal(winRecord.getTotalWinningPrize());
+        BigDecimal result = totalWinningPrize.divide(totalPurchaseAmount, 3, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
+        return result.doubleValue();
+    }
+
+
+    public void displayUserLottos(){
+        resultOutput.displayPurchaseCount(purchaseAmount.getPurchaseCount());
+        for(Lotto userLotto : userLottos){
+            resultOutput.displayUserLotto(userLotto.getLottoNumbers());
+        }
+    }
+
+    public void displayWinRecord(){
+        resultOutput.displayWinRecord(winRecord.getWinRecordCounts());
+    }
+
+    public void displayTotalYield(){
+        resultOutput.displayTotalYield(getTotalYield());
+    }
+}
