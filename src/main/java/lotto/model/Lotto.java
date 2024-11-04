ba35216f8cf32f5ca254b13eb003f69d326ae673
heejjinkim <hjhj12@dgu.ac.kr>
2024-11-04 23:00:14 +0900

    fix: Lotto 예외 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
+import static lotto.enums.ErrorCode.DUPLICATE_LOTTO_NUMBER;
 import static lotto.enums.ErrorCode.INVALID_LOTTO_NUMBER_COUNT;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
@@ -15,6 +18,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        checkNumberCount(numbers);
+        checkUniqueNumbers(numbers);
+    }
+
+    private void checkUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private void checkNumberCount(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNT.getMessage());
         }
