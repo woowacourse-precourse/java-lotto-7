919299cf16d40de6284bc0058532c5094de5d091
su-jin-shin <sujinshin1@gmail.com>
2024-11-04 21:39:25 +0900

    test(LottoInputHandlerTest.java): 사용자의 당첨 번호 입력에 대한 유효성 검증 테스트

--- src/test/java/lotto/LottoInputHandlerTest.java
+++ src/test/java/lotto/LottoInputHandlerTest.java
@@ -7,9 +7,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoInputHandlerTest {
     @Test
-    void 유효하지_않은_입력이_들어오면_예외가_발생한다() {
+    void 정수가_아닌_입력이_들어오면_예외가_발생한다() {
         LottoInputHandler inputHandler = new LottoInputHandler();
         assertThatThrownBy(() -> inputHandler.parseInput("abc"))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessageContaining("For input string");
+    }
+
+    @Test
+    void 정수_리스트가_아닌_입력이_들어오면_예외가_발생한다() {
+        LottoInputHandler inputHandler = new LottoInputHandler();
+        String invalidInput = "1, 2, three, 4, 5, 6";
+
+        assertThatThrownBy(() -> inputHandler.parseNumberList(invalidInput))
                 .isInstanceOf(NumberFormatException.class)
                 .hasMessageContaining("For input string");
     }
