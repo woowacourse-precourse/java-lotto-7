c6b92a8f22c02f86d236537594785683716f6650
sese2204 <sese2204@naver.com>
2024-11-04 11:12:22 +0900

    refactor(exception): 사용하지 않는 exception 삭제

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -10,7 +10,6 @@ public class LottoException extends IllegalArgumentException {
     public static final String INVALID_BONUS_NUMBER_RANGE = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
     public static final String INVALID_NUMBER_FORMAT = "[ERROR] 숫자 형식이 올바르지 않습니다.";
-    public static final String INVALID_WINNING_NUMBER_FORMAT = "[ERROR] 당첨 번호는 쉼표(,)로 구분하여 입력해 주세요.";
 
     public LottoException(String message) {
         super(message);
