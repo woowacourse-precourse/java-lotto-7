20f18c827197d397b7ec10eecc048eb204ec6c43
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 19:07:10 +0900

    feat: 로또 뭉치에 대한 기본적인 method 구현 및 variable 추가

--- src/main/java/lotto/entity/Lottos.java
+++ src/main/java/lotto/entity/Lottos.java
@@ -6,8 +6,19 @@ import java.util.List;
 public class Lottos {
 
     private final List<Lotto> lottos;
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
 
     public Lottos() {
         lottos = new ArrayList<>();
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public void setWinningNumbers(List<Integer> numbers, Integer bonus) {
+        this.winningNumbers = numbers;
+        this.bonusNumber = bonus;
     }
 }
