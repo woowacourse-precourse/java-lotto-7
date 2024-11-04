df40f493c9b384d48b8a7364d6d8bf6f65d963d5
wodus4145@naver.com <wodus4145@naver.com>
2024-11-04 18:03:24 +0900

    refactor: Add methods for validation
    검증을 위한 메서드들을 추가하고, 당첨번호를 Lotto객체가 아니라 List형태로 바꿈

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class InputHandler {
@@ -11,7 +12,7 @@ public class InputHandler {
         while (true) {
             try {
                 int price = receivePayment();
-                validPrice(price);
+                checkValidPrice(price);
                 return price;
             } catch (NumberFormatException e) {
                 System.out.println(ErrorMessage.INVALID_NUMBER_FORMAT);
@@ -27,18 +28,18 @@ public class InputHandler {
         return Integer.parseInt(inputPrice);
     }
 
-    public void validPrice(int price) throws IllegalArgumentException {
+    public void checkValidPrice(int price) throws IllegalArgumentException {
         if (price % 1000 != 0) throw new IllegalArgumentException(ErrorMessage.INVALID_PRICE_UNIT.toString());
     }
 
-    public Lotto getWinningNumbers() {
+    public List<Integer> getWinningNumbers() {
         while (true) {
             try {
                 String winningNumbers = receiveWinningNumbers();
                 List<Integer> winningNumberList = parseWinningNumbers(winningNumbers);
-                return new Lotto(winningNumberList);
+                return winningNumberList;
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 유효한 당첨번호를 입력하세요.");
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -51,22 +52,43 @@ public class InputHandler {
     public List<Integer> parseWinningNumbers(String winningNumbers) throws IllegalArgumentException {
         List<Integer> winNumberList = new ArrayList<>();
         String[] winNumberArray = winningNumbers.trim().split(",");
+        checkDuplication(winNumberArray);
         for (String winNumber : winNumberArray) {
-            winNumberList.add(Integer.parseInt(winNumber.trim()));
+            int singleWinNumber = Integer.parseInt(winNumber.trim());
+            checkValidRange(singleWinNumber);
+            winNumberList.add(singleWinNumber);
         }
         return winNumberList;
     }
 
-    public int getBonusNumber(Lotto winningLotto) {
+    public void checkValidRange(int inputNumber) throws IllegalArgumentException {
+        if(inputNumber < 1 || inputNumber > 45) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE.toString());
+        }
+    }
+
+    public void checkDuplication(String[] winNumberArray) throws IllegalArgumentException {
+        HashSet<Integer> duplicationCheck = new HashSet<>();
+        for (String winNumber : winNumberArray) {
+            int number = Integer.parseInt(winNumber);
+            if (!duplicationCheck.add(number)) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.toString());
+            }
+        }
+    }
+
+    public int getBonusNumber(List<Integer> winningNumberList) {
         while (true) {
             try {
-                String bonusNumber = receiveBonusNumber();
-                if (!winningLotto.getNumberString().contains(bonusNumber)) {
-                    return Integer.parseInt(bonusNumber);
-                }
-                System.out.println(ErrorMessage.INVALID_BONUS);
+                String inputBonusNumber = receiveBonusNumber();
+                int bonusNumber = Integer.parseInt(inputBonusNumber);
+                checkValidRange(bonusNumber);
+                checkValidBonusNumber(winningNumberList, bonusNumber);
+                return bonusNumber;
             } catch (NumberFormatException e) {
                 System.out.println(ErrorMessage.INVALID_NUMBER_FORMAT);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -74,5 +96,11 @@ public class InputHandler {
     public String receiveBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine().trim();
+    }
+
+    public void checkValidBonusNumber(List<Integer> winningNumberList, int bonusNumber) throws IllegalArgumentException {
+        if(winningNumberList.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS.toString());
+        }
     }
 }
