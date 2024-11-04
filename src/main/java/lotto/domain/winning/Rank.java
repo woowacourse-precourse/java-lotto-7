58cfe3dd3c3243a505f1c9467d229517590b86ac
Yeongjae Kim <sees77770@gmail.com>
2024-11-04 18:38:16 +0900

    fix: Rank의 순서 수정
    3개 일치부터 출력하는 요구사항에 따라 Rank의 순서를 바꾼다.

--- src/main/java/lotto/domain/winning/Rank.java
+++ src/main/java/lotto/domain/winning/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain.winning;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
     FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000),
     NONE(0, 0);
 
     private final int matchCount;
