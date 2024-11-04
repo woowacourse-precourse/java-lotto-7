5412b027ff7279524d0b306eb8731a7bd9ee474a
zelatndnjs <zelatndnjs@naver.com>
2024-11-04 20:46:57 +0900

    feat(Lotto.java) : generateRandomLotto 메소드 생성
    랜덤 로또 번호를 생성해주는 generateRandomLotto를 만들었다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,23 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public static Lotto generateRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_COUNT);
+        return new Lotto(numbers);
     }
 
     private void validate(List<Integer> numbers) {
