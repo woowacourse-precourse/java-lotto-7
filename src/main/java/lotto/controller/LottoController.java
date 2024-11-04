15132d865bfd46c557df139ccc8b6169bda2bd3a
sonshn <sonshn@naver.com>
2024-11-04 20:15:00 +0900

    feat(LottoController): run 메서드 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,12 @@ import lotto.view.InputView;
 
 public class LottoController {
 
-    private final InputView inputView = new InputView();
+    public void run() {
+        String purchaseAmount = InputView.getPurchaseAmount();
+        String winningNumbers = InputView.getLotteryWinningNumbers();
+        String lotteryNumber = InputView.getLotteryBonusNumber();
+
+        InputView.closeConsole();
+    }
     
 }
