b0d3f3e69c43e571e8b49a685e142e5a0affe552
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-04 22:17:44 +0900

    refact: 출력 메시지 줄바꿈 추가

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -3,7 +3,7 @@ package lotto.view;
 public enum InputMessage {
 
     INPUT_AMOUNT("구입금액을 입력해 주세요."),
-    INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     ;
 
