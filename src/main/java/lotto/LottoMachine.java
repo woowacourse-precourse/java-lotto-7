544f0571fd38a61f7f90ef75f232b7c453e4c2cd
문혜교 <3hilney@gmail.com>
2024-11-04 14:59:53 +0900

    feat: 로또 티켓 및 로또 번호 생성 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,16 +1,35 @@
 package lotto;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int LOTTO_NUMBER_MIN = 1;
-    private static final int LOTTO_NUMBER_MAX = 6;
+    private static final int LOTTO_NUMBER_MAX = 45;
 
     public int calculateLottoCount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_PURCHASE_AMOUNT.getErrorMessage());
         }
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public List<List<Integer>> generateLottoTickets(int count) {
+        List<List<Integer>> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottoTickets.add(generateLottoNumbers());
+        }
+        return lottoTickets;
+    }
+
+    public List<Integer> generateLottoNumbers() {
+        Set<Integer> numbers = new HashSet<>(Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT));
+        return new ArrayList<>(numbers);
     }
 }
