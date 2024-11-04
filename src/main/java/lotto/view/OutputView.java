1cb0d3368504292fa5c4015d2a2ee59858be3c86
gyuoo <yeomky98@naver.com>
2024-11-05 00:36:23 +0900

    feat(view): update OutputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import static lotto.constants.LottoConstantNumbers.*;
 import static lotto.exception.ErrorMessage.*;
 import static lotto.view.ConsoleMessage.*;
 
@@ -25,12 +26,28 @@ public class OutputView {
 		System.out.println(MAX_PURCHASE_EXCEED.getMessage());
 	}
 
+	public void printInvalidLottoNumberSizeMessage() {
+		System.out.println(String.format(INVALID_LOTTO_NUMBERS_COUNT.getMessage(), LOTTO_NUMBERS_COUNT));
+	}
+
 	public void printPurchaseCountMessage(int purchaseCount) {
 		System.out.println(String.format(PURCHASE_COUNT_MESSAGE.getMessage(), purchaseCount));
 	}
 
+	public void printOutOfRangeNumberMessage() {
+		System.out.println(String.format(OUT_OF_RANGE_NUMBER.getMessage(), MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER));
+	}
+
 	public void printToGetWinningNumbers() {
 		System.out.println(INPUT_WINNING_NUMBERS.getMessage());
+	}
+
+	public void printDuplicateNumberMessage() {
+		System.out.println(DUPLICATE_NUMBER.getMessage());
+	}
+
+	public void printToGetBonusNumbers() {
+		System.out.println(INPUT_BONUS_NUMBER.getMessage());
 	}
 
 	public void printLottoList(List<Lotto> totalLottos) {
