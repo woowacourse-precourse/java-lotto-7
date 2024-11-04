f8949a94ec7f55ab1cee151364ed9d9f6d784e2c
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-04 21:29:19 +0900

    feat(Lotto): add 로또 번호 관리 및 당첨 결과 계산 기능
    - 생성 및 초기화 시 유효성 검증 추가 (6개 번호, 1~45 범위, 중복 불가)
    - 당첨 번호와 보너스 번호에 따른 당첨 랭크 계산 가능
    - LottoDto를 통해 생성할 수 있는 보조 생성자 추가
    - getNumbers()로 안전하게 로또 번호 목록 반환

---
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -0,0 +1,71 @@
+package lotto.model.domain;
+
+import lotto.PrizeRank;
+import lotto.dto.LottoDto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static lotto.Constants.MAXIMUM_LOTTO_NUMBER;
+import static lotto.Constants.MINIMUM_LOTTO_NUMBER;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
+    }
+
+    public Lotto(LottoDto lottoDto) {
+        this(lottoDto.numbers());
+    }
+
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+    public PrizeRank calculateResult(LottoWinningNumbers winningNumbers) {
+        int matchCount = countMatches(winningNumbers.getWinningNumbers());
+        boolean bonusMatched = containsNumber(winningNumbers.getBonusNumber());
+
+        return PrizeRank.fromMatchCountAndBonus(matchCount, bonusMatched);
+    }
+
+    private int countMatches(Lotto winningNumbers) {
+        return (int) numbers.stream()
+                .filter(winningNumbers.getNumbers()::contains)
+                .count();
+    }
+
+    private boolean containsNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicates(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(num -> num < MINIMUM_LOTTO_NUMBER || num > MAXIMUM_LOTTO_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    private void validateDuplicates(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+}
