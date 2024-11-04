699512586caea23040cee52c3f6cd1382e5888cb
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-04 19:43:58 +0900

    test: LottoGroupService 테스트코드 추가

--- src/test/java/lotto/service/LottoGroupServiceTest.java
+++ src/test/java/lotto/service/LottoGroupServiceTest.java
@@ -2,16 +2,35 @@ package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGroup;
+import lotto.domain.Winning;
 import org.junit.jupiter.api.Test;
 
 class LottoGroupServiceTest {
-    @Test
-    void 맞춘_숫자_계산() {
-
-    }
+    LottoGroupService lottoGroupService = new LottoGroupService();
 
     @Test
-    void 보너스_숫자가_있는지_확인() {
+    void 결과_계산_테스트() {
+        Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
 
+        List<Lotto> userLottos = Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), // 6개 일치
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7))  // 5개 일치 + 보너스
+        );
+        LottoGroup lottoGroup = new LottoGroup(userLottos);
+
+        lottoGroupService.calculateResults(lottoGroup, winningLotto, bonusNumber);
+
+        Map<Winning, Integer> matchCounts = lottoGroup.getMatchCounts();
+        assertEquals(1, matchCounts.get(Winning.SIXTH));  // 1등 당첨 개수
+        assertEquals(1, matchCounts.get(Winning.FIFTH_WITH_BONUS));
+
+        int expectedTotalPrize = Winning.SIXTH.getPrice() + Winning.FIFTH_WITH_BONUS.getPrice();
+        assertEquals(expectedTotalPrize, lottoGroup.getTotalPrize());
     }
 }
