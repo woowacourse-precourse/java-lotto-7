3f1594671697a71e9caf1ea0eac76a768eae3115
LeeDongWoo99 <dleh684@gmail.com>
2024-11-04 23:28:20 +0900

    feat(service): LottoMachine(로또 구매 로직) 클래스 생성

---
+++ src/main/java/lotto/service/LottoMachine.java
@@ -0,0 +1,45 @@
+package lotto.service;
+
+import lotto.constants.LottoConstants;
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoCounter;
+import lotto.domain.Money;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private final LottoGenerator lottoGenerator;
+    private final LottoNumberChecker lottoNumberChecker;
+    private final LottoCounter rankCounter;
+
+    public LottoMachine(LottoGenerator lottoGenerator, LottoNumberChecker lottoNumberChecker) {
+        this.lottoGenerator = lottoGenerator;
+        this.lottoNumberChecker = lottoNumberChecker;
+        this.rankCounter = new LottoCounter();
+    }
+
+    public List<Lotto> purchaseLottos(Money money) {
+        int totalAmount = money.getAmount();
+        int numberOfLottos = totalAmount / LottoConstants.LOTTO_PRICE; // 구매 가능한 로또 수 계산
+        List<Lotto> purchasedLottos = new ArrayList<>();
+
+        for (int i = 0; i < numberOfLottos; i++) {
+            purchasedLottos.add(lottoGenerator.generateLotto()); // 로또 생성
+        }
+
+        return purchasedLottos;
+    }
+
+    public LottoCounter getRankCounter() {
+        return rankCounter;
+    }
+
+    public void checkWinningNumbers(List<Lotto> purchasedLottos, List<Integer> winningNumbers, int bonusNumber) {
+        for (Lotto lotto : purchasedLottos) {
+            LottoRank rank = lottoNumberChecker.check(lotto.getNumbers(), winningNumbers, bonusNumber);
+            rankCounter.addResult(rank);
+        }
+    }
+}
