e302e253c217066aa89bb0d46ee90fa05d51c809
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 18:49:20 +0900

    feat: LottoGrade 상금 추가

--- src/main/java/lotto/common/LottoGrade.java
+++ src/main/java/lotto/common/LottoGrade.java
@@ -1,12 +1,18 @@
 package lotto.common;
 
 public enum LottoGrade {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    NONE;
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    NONE(0);
+
+    private final int cost;
+
+    LottoGrade(int cost) {
+        this.cost = cost;
+    }
 
     public static LottoGrade getGrade(int count, boolean bonus) {
         if (count == 3) {
