7246995d40aed32b7a9f7236fdeb9e6226b1eccd
Cheoroo <195822@jnu.ac.kr>
2024-11-04 14:57:59 +0900

    feat : 로또 입출력 뷰 기능을 구현한다
    - InputView에서 사용자에게 구입금액과 당첨번호 보너스번호를 입력 구현
    - OutputView에서 로또 발행 내역 및 당첨 결과 출력 기능 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+    }
+
+    public String readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        }
+    }
+}
