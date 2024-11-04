a5e5472a6fc5088701eb5547d69a9b4753a289d0
JinGoo <zzgg3040@naver.com>
2024-11-04 17:04:16 +0900

    test: 당첨 로또 일치 개수에 따라 등수 초기화

---
+++ src/test/java/lotto/model/LottoRankTest.java
@@ -0,0 +1,61 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.model.db.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoRankTest {
+
+    @DisplayName("buyer 의 로또와 당첨 로또를 비교하여 등수의 카운트를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:0", "3:0", "4:0", "5:2"}, delimiter = ':')
+    void parseRank(int rank, int rankCnt) {
+        // given
+        List<Lotto> buyerLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(7, 8, 9, 10, 11, 121)));
+        // when
+        Lotto winningLotto =
+                new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        int bonus = 45;
+        LottoRank lottoRank = new LottoRank(buyerLotto, winningLotto, bonus);
+        // then
+        assertThat(lottoRank.getCnt(rank)).isEqualTo(rankCnt);
+    }
+
+    @DisplayName("5개 일치할 때 bonus 이 일치하면 2등으로 판별한다.")
+    @Test
+    void parseSecondRank() {
+        // given
+        List<Lotto> buyerLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 45)));
+        // when
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 45;
+        LottoRank lottoRank = new LottoRank(buyerLotto, winningLotto, bonus);
+        // then
+        assertThat(lottoRank.getCnt(2)).isEqualTo(1);
+        assertThat(lottoRank.getCnt(3)).isEqualTo(0);
+    }
+
+    @DisplayName("5개 일치할 때 bonus 이 일치하지 않으면 3등으로 판별한다.")
+    @Test
+    void parseRank() {
+        // given
+        List<Lotto> buyerLotto = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 45)));
+        // when
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 7;
+        LottoRank lottoRank = new LottoRank(buyerLotto, winningLotto, bonus);
+        // then
+        assertThat(lottoRank.getCnt(2)).isEqualTo(0);
+        assertThat(lottoRank.getCnt(3)).isEqualTo(1);
+    }
+
+}
