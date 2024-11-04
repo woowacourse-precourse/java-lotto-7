68ed176b8c17121ee55a5fe3fe40d3467f32d2e6
kaswhy <sxshx818@naver.com>
2024-11-04 17:09:49 +0900

    test(BonusNumberTest): 입력받은 값이 1부터 45 사이가 아니면 예외 발생 테스트
    변경 후
    - 해당 사항 구현

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static lotto.constants.ErrorMessage.INPUT_CAN_NOT_BE_BLANK;
 import static lotto.constants.ErrorMessage.LOTTO_CAN_NOT_HAVE_CHARACTER;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_MUST_BE_ONE_TO_FORTY_FIVE;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BonusNumberTest {
 
@@ -43,5 +45,17 @@ public class BonusNumberTest {
 
     }
 
+    @DisplayName("입력받은 값이 1부터 45 사이의 값이 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "47"}) // given
+    public void ThrowExceptionIfNumberNOTOneToFortyFive(String inputNumber){
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            BonusNumber bonusNumber = new BonusNumber(inputNumber);
+        });
 
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUMBER_MUST_BE_ONE_TO_FORTY_FIVE.get());
+    }
 }
