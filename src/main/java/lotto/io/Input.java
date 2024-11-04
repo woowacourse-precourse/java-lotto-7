fd142af7f89904173a4041457df0870b0f29de16
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-04 11:43:43 +0900

    feat(input): 당첨 번호 입력 기능 추가
    당첨 번호 입력 기능을 구현했습니다.

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -6,17 +6,27 @@ import lotto.message.ErrorMessage;
 public class Input {
     private final int LOTTO_PRICE = 1000;
     private final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
 
     public int getAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
 
         String amount = Console.readLine();
-        validate(amount);
+        validateAmount(amount);
 
         return Integer.parseInt(amount);
     }
 
-    private void validate(String amount) {
+    public String getWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
+
+        String winningNumber = Console.readLine();
+        validateWinningNumber(winningNumber);
+
+        return winningNumber;
+    }
+
+    private void validateAmount(String amount) {
         if (amount.isEmpty() || amount.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_EXIST);
         }
@@ -34,8 +44,20 @@ public class Input {
         }
     }
 
-    private boolean isDigit(String amount) {
-        for (char c : amount.toCharArray()){
+    private void validateWinningNumber(String winningNumber) {
+        if (winningNumber.isEmpty() || winningNumber.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_EXIST);
+        }
+
+        winningNumber = winningNumber.replace(",", "");
+
+        if (!isDigit(winningNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER);
+        }
+    }
+
+    private boolean isDigit(String input) {
+        for (char c : input.toCharArray()){
             if (!Character.isDigit(c)){
                 return false;
             }
