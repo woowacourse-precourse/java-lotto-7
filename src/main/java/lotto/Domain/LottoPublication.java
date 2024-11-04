4c9e10d6742fa61d57b30c5cd17535eecfb97f9b
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-04 22:45:01 +0900

    feat: 예외 처리 메서드 추가
    - Lotto 클래스에서는 입력에 대한 예외를 발생시키고 정상적인 입력을
      반환합니다.

--- src/main/java/lotto/Domain/LottoPublication.java
+++ src/main/java/lotto/Domain/LottoPublication.java
@@ -6,10 +6,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoPublication {
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+    private static final int COUNT = 6;
+    private static final int PRICE = 1000;
+    private final int purchaseLotto;
+
+    public LottoPublication(int purchaseLotto) {
+        this.purchaseLotto = purchaseLotto;
+    }
 
     public List<Integer> lottoNumberPublication() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(START_NUM, END_NUM, COUNT);
         return numbers;
+    }
+
+    public int theNumberOfLotto() {
+        return purchaseLotto / PRICE;
     }
 
     public List<Integer> getSortedLottoNumbers() {
@@ -18,8 +31,9 @@ public class LottoPublication {
         return numbers;
     }
 
-    public List<List<Integer>> totalLotto(int count) {
+    public List<List<Integer>> totalLotto() {
         List<List<Integer>> publicationNumbers = new ArrayList<>();
+        int count = theNumberOfLotto();
 
         for (int i = 0; i < count; i++) {
             publicationNumbers.add(getSortedLottoNumbers());
