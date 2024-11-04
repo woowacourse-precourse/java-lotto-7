d18978e2aebf01210c6f481ba16fd68756dcf197
FhRh <yiyop@naver.com>
2024-11-04 21:02:33 +0900

    refactor(LottoInputConverterTest) : 패키지 위치 변경

--- src/test/java/lotto/view/converter/LottoInputConverterTest.java
+++ src/test/java/lotto/view/converter/LottoInputConverterTest.java
@@ -1,10 +1,9 @@
-package lotto.service;
+package lotto.view.converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.view.converter.LottoInputConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
