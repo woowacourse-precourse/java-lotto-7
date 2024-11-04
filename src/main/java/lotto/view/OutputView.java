6ef89f7c07a12044867839d20a67c7526408ed21
18hailey <hailey9459@gmail.com>
2024-11-04 23:11:42 +0900

    feat(OutputView): print output messages to user

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,45 @@
+package lotto.view;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Map;
+import lotto.constant.Constant;
+import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.service.LottoService;
+
+public class OutputView {
+
+    private final LottoService lottoService;
+
+    public OutputView(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    public int lottoCount(int purchasePrice) {
+        int lottoCount = lottoService.calculateLottoCount(purchasePrice);
+        System.out.printf(Constant.LOTTO_PURCHASE_PRINT_MESSAGE, lottoCount);
+        return lottoCount;
+    }
+
+    public void printLottos(int purchasePrice) {
+        List<Lotto> lottos = lottoService.createLottos(purchasePrice);
+        for (Lotto lotto : lottos) {
+            lotto.printNumbers();
+        }
+    }
+
+    public void printWinningResults(int purchasePrice) {
+        System.out.println(Constant.WINNING_RESULT_MESSAGE);
+        List<Lotto> lottos = lottoService.createLottos(purchasePrice);
+        Map<LottoRank, Integer> resultCount = lottoService.calculateTotalWinnings(lottos);
+        NumberFormat nf = NumberFormat.getInstance();
+        for (LottoRank lottoRank : resultCount.keySet()) {
+            System.out.printf(Constant.EACH_RANK_COUNT + "\n", lottoRank.getSameNumberCount(), nf.format(lottoRank.getWinningPrice()),
+                    resultCount.get(lottoRank));
+        }
+        double rateOfReturn = lottoService.calculateRateOfReturn(resultCount, purchasePrice);
+        System.out.printf(Constant.RATE_OF_RETURN_MESSAGE, rateOfReturn);
+    }
+
+}
