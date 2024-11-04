d2626949a0a23195868601c3e5f391ab2f93e577
hanwool <hanwe92@gmail.com>
2024-11-04 15:26:29 +0900

    feat(ListEnum): Add List<DrawerNumbers> get method

--- src/test/java/lotto/enums/ListEnum.java
+++ src/test/java/lotto/enums/ListEnum.java
@@ -1,6 +1,7 @@
 package lotto.enums;
 
 import java.util.List;
+import lotto.domain.DrawNumbers;
 
 public enum ListEnum {
     WINNING_LOTTO_NUMBERS(List.of(1, 2, 3, 4, 5, 6)),
@@ -9,6 +10,7 @@ public enum ListEnum {
     FIVE_MATCH_LOTTO_NUMBERS(List.of(1, 2, 3, 4, 5, 9)),
     ;
     private final List<Integer> list;
+    private static List<DrawNumbers> drawSets;
 
     ListEnum(List<Integer> list) {
         this.list = list;
@@ -16,5 +18,38 @@ public enum ListEnum {
 
     public List<Integer> getList() {
         return list;
+    }
+
+    public static List<DrawNumbers> getDrawSets() {
+        //random drawn numbers set
+        DrawNumbers threeMatchNumber = new DrawNumbers(
+                ListEnum.THREE_MATCH_LOTTO_NUMBERS.getList(),
+                IntEnum.NOT_MATCH_BONUS_NUMBERS.getNumber()
+        );
+        DrawNumbers fourMatchNumber = new DrawNumbers(
+                ListEnum.FOUR_MATCH_LOTTO_NUMBERS.getList(),
+                IntEnum.NOT_MATCH_BONUS_NUMBERS.getNumber()
+        );
+        DrawNumbers fiveMatchNumber = new DrawNumbers(
+                ListEnum.FIVE_MATCH_LOTTO_NUMBERS.getList(),
+                IntEnum.NOT_MATCH_BONUS_NUMBERS.getNumber()
+        );
+        DrawNumbers fiveMatchWithBonusNumber = new DrawNumbers(
+                ListEnum.FIVE_MATCH_LOTTO_NUMBERS.getList(),
+                IntEnum.WINNING_BONUS_NUMBERS.getNumber()
+        );
+        DrawNumbers sixMatchNumber = new DrawNumbers(
+                ListEnum.WINNING_LOTTO_NUMBERS.getList(),
+                IntEnum.NOT_MATCH_BONUS_NUMBERS.getNumber()
+        );
+
+        drawSets = List.of(
+                threeMatchNumber,
+                fourMatchNumber,
+                fiveMatchNumber,
+                fiveMatchWithBonusNumber,
+                sixMatchNumber
+        );
+        return drawSets;
     }
 }
