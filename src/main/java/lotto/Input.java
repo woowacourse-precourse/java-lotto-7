248cc4789cdecc36f666b40b0406ebfc3237c7a9
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 18:59:50 +0900

    feat(Input): 구입 금액, 당첨 번호, 보너스 번호 입력 받기 기능 추가

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static PurchaseAmount getPurchaseAmount(){
+        try{
+            String input = Console.readLine();
+            InputValidator.validateBlank(input);
+            InputValidator.validateWhitespace(input);
+            return new PurchaseAmount(input);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getPurchaseAmount();
+        }
+    }
+
+    public static WinningNumber getWinningNumber(){
+        try{
+            String input = Console.readLine();
+            InputValidator.validateBlank(input);
+            InputValidator.validateWhitespace(input);
+            return new WinningNumber(input);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getWinningNumber();
+        }
+    }
+
+    public static BonusNumber getBonusNumber(WinningNumber winningNumber) {
+        try{
+            String input = Console.readLine();
+            InputValidator.validateBlank(input);
+            InputValidator.validateWhitespace(input);
+            return new BonusNumber(new LottoNumber(input), winningNumber);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return getBonusNumber(winningNumber);
+        }
+    }
+}
