7c0d2e553cde1a585b50b9950c518999166a5c5b
G-Lake <note125@naver.com>
2024-11-04 21:39:52 +0900

    Feat(Application, LottoRank) : 입력 양식, 사용값 정의 및 반환
    Application.java - 기본 입력양식 메서드(구입금액, 당첨 번호, 보너스 번호)
    LottoRank.java - 각종 상수 정의(등수와 당첨금), 일치 여부에 따른 값 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static int getInputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        return amount;
+    }
+
+    private static List<Integer> getInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine().trim().split(",");
+    }
+
+    private static int getInputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine().trim());
     }
 }
