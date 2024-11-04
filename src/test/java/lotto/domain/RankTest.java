8db5f6c8a8cad1ade86d2650bbb79cd1df7fa5da
injae-348 <ant0593@naver.com>
2024-11-04 16:12:23 +0900

    refactor(RankTest): jupiter Assertions에서 assertj Assertions로 변경

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,65 +1,64 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RankTest {
-    // Todo : domain - service - validator - repository - controller
-    // Todo : AppConfig, constant, exception 테스트 불필요
+
     @Test
-    @DisplayName("각 Rank에 해당하는 matchCount와 isBonusMathch를 입력하면 해당 Rank를 반환한다.")
+    @DisplayName("각 Rank에 해당하는 matchCount와 isBonusMatch를 입력하면 해당 Rank를 반환한다.")
     void RankValueOfTest() {
-        assertEquals(Rank.FIRST, Rank.valueOf(6, false));
-        assertEquals(Rank.SECOND, Rank.valueOf(5, true));
-        assertEquals(Rank.THIRD, Rank.valueOf(5, false));
-        assertEquals(Rank.FOURTH, Rank.valueOf(4, false));
-        assertEquals(Rank.FIFTH, Rank.valueOf(3, false));
-        assertEquals(Rank.NONE, Rank.valueOf(0, false));
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.NONE);
     }
 
     @Test
     @DisplayName("각 Rank에 해당하는 상금을 반환한다.")
     void getPrizeTest() {
-        assertEquals(2_000_000_000, Rank.FIRST.getPrize());
-        assertEquals(30_000_000, Rank.SECOND.getPrize());
-        assertEquals(1_500_000, Rank.THIRD.getPrize());
-        assertEquals(50_000, Rank.FOURTH.getPrize());
-        assertEquals(5_000, Rank.FIFTH.getPrize());
-        assertEquals(0, Rank.NONE.getPrize());
+        assertThat(Rank.FIRST.getPrize()).isEqualTo(2_000_000_000);
+        assertThat(Rank.SECOND.getPrize()).isEqualTo(30_000_000);
+        assertThat(Rank.THIRD.getPrize()).isEqualTo(1_500_000);
+        assertThat(Rank.FOURTH.getPrize()).isEqualTo(50_000);
+        assertThat(Rank.FIFTH.getPrize()).isEqualTo(5_000);
+        assertThat(Rank.NONE.getPrize()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("각 Rank에 해당하는 상금을 포맷팅하여 반환한다.")
     void getFormattedPrizeTest() {
-        assertEquals(" (2,000,000,000원)", Rank.FIRST.getFormattedPrize());
-        assertEquals(" (30,000,000원)", Rank.SECOND.getFormattedPrize());
-        assertEquals(" (1,500,000원)", Rank.THIRD.getFormattedPrize());
-        assertEquals(" (50,000원)", Rank.FOURTH.getFormattedPrize());
-        assertEquals(" (5,000원)", Rank.FIFTH.getFormattedPrize());
-        assertEquals(" (0원)", Rank.NONE.getFormattedPrize());
+        assertThat(Rank.FIRST.getFormattedPrize()).isEqualTo(" (2,000,000,000원)");
+        assertThat(Rank.SECOND.getFormattedPrize()).isEqualTo(" (30,000,000원)");
+        assertThat(Rank.THIRD.getFormattedPrize()).isEqualTo(" (1,500,000원)");
+        assertThat(Rank.FOURTH.getFormattedPrize()).isEqualTo(" (50,000원)");
+        assertThat(Rank.FIFTH.getFormattedPrize()).isEqualTo(" (5,000원)");
+        assertThat(Rank.NONE.getFormattedPrize()).isEqualTo(" (0원)");
     }
 
     @Test
     @DisplayName("각 Rank에 해당하는 메시지를 반환한다.")
-    void getMessage() {
-        assertEquals("6개 일치", Rank.FIRST.getMessage());
-        assertEquals("5개 일치, 보너스 볼 일치", Rank.SECOND.getMessage());
-        assertEquals("5개 일치", Rank.THIRD.getMessage());
-        assertEquals("4개 일치", Rank.FOURTH.getMessage());
-        assertEquals("3개 일치", Rank.FIFTH.getMessage());
-        assertEquals("0개 일치", Rank.NONE.getMessage());
+    void getMessageTest() {
+        assertThat(Rank.FIRST.getMessage()).isEqualTo("6개 일치");
+        assertThat(Rank.SECOND.getMessage()).isEqualTo("5개 일치, 보너스 볼 일치");
+        assertThat(Rank.THIRD.getMessage()).isEqualTo("5개 일치");
+        assertThat(Rank.FOURTH.getMessage()).isEqualTo("4개 일치");
+        assertThat(Rank.FIFTH.getMessage()).isEqualTo("3개 일치");
+        assertThat(Rank.NONE.getMessage()).isEqualTo("0개 일치");
     }
 
     @Test
-    @DisplayName("Rank에 해당하는 matchCount와 isBonusMathch가 일치 하는지 확인한다.")
+    @DisplayName("Rank에 해당하는 matchCount와 isBonusMatch가 일치 하는지 확인한다.")
     void RankValueOfMatchCountAndIsBonusMatchTest() {
-        assertEquals(Rank.FIRST, Rank.valueOf(6, false));
-        assertEquals(Rank.SECOND, Rank.valueOf(5, true));
-        assertEquals(Rank.THIRD, Rank.valueOf(5, false));
-        assertEquals(Rank.FOURTH, Rank.valueOf(4, false));
-        assertEquals(Rank.FIFTH, Rank.valueOf(3, false));
-        assertEquals(Rank.NONE, Rank.valueOf(0, false));
+        assertThat(Rank.valueOf(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.NONE);
     }
 }
