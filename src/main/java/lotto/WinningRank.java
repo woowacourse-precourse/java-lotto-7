4bf0cdd6309933968a39e423372e367a49718346
seoyoungjin23 <ascd050@gmail.com>
2024-11-04 11:23:12 +0900

    refactor(WinningRank): 출력이 순서대로 나오도록 코드 순서 변경

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -1,11 +1,11 @@
 package lotto;
 
 public enum WinningRank {
-    FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
+    FIFTH(3, 5_000, false),
     FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false);
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
 
     private final int matchCount;
     private final int prize;
