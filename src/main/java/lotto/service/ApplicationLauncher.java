1f1b822c7bab9697c65161213eeaffbeefc70e34
whdmscks11 <whdmscks11@gmail.com>
2024-11-04 17:08:23 +0900

    fix(ApplicationLauncher): 잘못된 입력 시 예외 발생 후 재입력 받도록 각 입력에 while문 분리

--- src/main/java/lotto/service/ApplicationLauncher.java
+++ src/main/java/lotto/service/ApplicationLauncher.java
@@ -9,32 +9,23 @@ import lotto.io.OutputHandler;
 public class ApplicationLauncher {
     public static void run(){
         InputHandler inputHandler = new InputHandler();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoWinningNumbers winningLotto = new LottoWinningNumbers();
+        Lotto[] userLotto;
+
+        String firstLine;
+        String secondLine;
+        String thirdLine;
 
         while(true){
             try {
                 OutputHandler.print(OutputHandler.ENTER_PURCHASE_AMOUNT);
-                String firstLine = inputHandler.readline();
+                firstLine = inputHandler.readline();
 
-                LottoGenerator lottoGenerator = new LottoGenerator();
-                Lotto[] userLotto = lottoGenerator.generate(firstLine);
+                userLotto = lottoGenerator.generate(firstLine);
 
                 OutputHandler.printPurchasedQuantity(lottoGenerator);
                 OutputHandler.printPurchasedLotto(lottoGenerator);
-
-                OutputHandler.print(OutputHandler.ENTER_WINNING_NUMBERS);
-                String secondLine = inputHandler.readline();
-
-                OutputHandler.print(OutputHandler.ENTER_BONUS_NUMBER);
-                String thirdLine = inputHandler.readline();
-
-                LottoWinningNumbers winningLotto = new LottoWinningNumbers(secondLine, thirdLine);
-                winningLotto.generate();
-
-                LottoResultAnalyzer lottoResultAnalyzer = new LottoResultAnalyzer();
-                lottoResultAnalyzer.analyze(userLotto, winningLotto);
-
-                OutputHandler.printCompareResult(lottoResultAnalyzer);
-                OutputHandler.printTotalYield(lottoResultAnalyzer, lottoGenerator);
 
                 break;
 
@@ -42,6 +33,40 @@ public class ApplicationLauncher {
                 System.out.println(e.getMessage());
             }
         }
+
+        while(true){
+            try {
+                OutputHandler.print(OutputHandler.ENTER_WINNING_NUMBERS);
+                secondLine = inputHandler.readline();
+                winningLotto.setMainNumbers(secondLine);
+
+                break;
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while(true){
+            try {
+                OutputHandler.print(OutputHandler.ENTER_BONUS_NUMBER);
+                thirdLine = inputHandler.readline();
+                winningLotto.setBonusNumber(thirdLine);
+
+                break;
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        winningLotto.generate();
+
+        LottoResultAnalyzer lottoResultAnalyzer = new LottoResultAnalyzer();
+        lottoResultAnalyzer.analyze(userLotto, winningLotto);
+
+        OutputHandler.printCompareResult(lottoResultAnalyzer);
+        OutputHandler.printTotalYield(lottoResultAnalyzer, lottoGenerator);
 
         Console.close();
     }
