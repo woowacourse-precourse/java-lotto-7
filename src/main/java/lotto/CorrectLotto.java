e515e7f904da6ffad63a6ce162d7ae9359854c1d
mete0rfish <sungwon326@naver.com>
2024-11-04 13:50:55 +0900

    fix(domain): 당첨번호가 서로 중복되는 유효성 검사 추가

--- src/main/java/lotto/CorrectLotto.java
+++ src/main/java/lotto/CorrectLotto.java
@@ -2,7 +2,9 @@ package lotto;
 
 import lotto.view.LottoInput;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CorrectLotto {
     private final Lotto lotto;
@@ -16,22 +18,25 @@ public class CorrectLotto {
     public static Lotto createCorrectNumber(List<String> inputs) {
         List<Integer> numbers = inputs.stream()
                 .map(
-                    input -> {
-                    try {
-                        return Integer.parseInt(input);
-                    } catch (NumberFormatException e) {
-                        throw new IllegalArgumentException("[ERROR] 당첨번호는 1~45의 정수이어야 합니다.");
-                    }
-                })
+                        input -> {
+                            try {
+                                return Integer.parseInt(input);
+                            } catch (NumberFormatException e) {
+                                throw new IllegalArgumentException("[ERROR] 당첨번호는 1~45의 정수이어야 합니다.");
+                            }
+                        })
                 .toList();
+        validateDuplicates(numbers);
         return new Lotto(numbers);
     }
 
-    private void validateDuplicate(Lotto lotto, int bonusNumber) {
-        if(lotto.numbers().contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호와 정답 번호는 중복될 수 없습니다.");
+    public static void validateDuplicates(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 중복될 수 없습니다.");
         }
     }
+
 
     public Rank match(Lotto other) {
         int matchCount = (int) lotto.numbers().stream()
