cb7ea368374e91fd748663fb980809623ade4e53
younyoung <00younyoung@naver.com>
2024-11-04 22:32:32 +0900

    Feat: 입력값들을 순서에 맞게 해당하는 객체로 연결하도록 구현 Controller에서 입력받은 값들을 순서에 맞게 객체로 연결하도록 구현

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,2 +1,61 @@
-package lotto;public class Controller {
+package lotto;
+
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.domain.ResultSheet;
+import lotto.domain.WinningLotto;
+import lotto.message.ViewMessage;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class Controller {
+    public static void run() {
+        Lottos purchasedLottos = purchasedLottos();
+        OutputView.printLottos(purchasedLottos);
+        WinningLotto winningLotto = winningNumbers();
+        ResultSheet resultSheet = winningLotto.calculateResult(purchasedLottos);
+        OutputView.printResult(resultSheet);
+    }
+
+    private static Lottos purchasedLottos() {
+        try {
+            Money money = new Money(InputView.readInteger(ViewMessage.INPUT_PURCHASE_MONEY));
+            return Lottos.purchaseLottos(money);
+        } catch (IllegalArgumentException e) {
+            OutputView.printException(e);
+            return purchasedLottos();
+        }
+    }
+
+    private static WinningLotto winningNumbers() {
+        Lotto winningNumbers = initLotto();
+        BonusNumber bonus = initBonus();
+
+        return new WinningLotto(winningNumbers, bonus);
+    }
+
+    private static Lotto initLotto() {
+        try {
+            String lottoNumberString = InputView.readString(ViewMessage.INPUT_WINNING_NUMBERS);
+            List<Integer> lottoNumbers = Utils.convertStringToIntegerList(lottoNumberString,
+                ",");
+            return new Lotto(lottoNumbers);
+        } catch (IllegalArgumentException e) {
+            OutputView.printException(e);
+            return initLotto();
+        }
+    }
+
+    private static BonusNumber initBonus() {
+        try {
+            int bonus = InputView.readInteger(ViewMessage.INPUT_BONUS_NUMBER);
+            return new BonusNumber(bonus);
+        } catch (IllegalArgumentException e) {
+            OutputView.printException(e);
+            return initBonus();
+        }
+    }
 }
