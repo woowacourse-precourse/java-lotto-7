3209fab7150a46190b59e8f47667adb855b9f01e
LeeHaeJun <haejun0410@gmail.com>
2024-11-04 21:03:57 +0900

    feat: 당첨 결과를 계산하는 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,14 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.Prize;
+import lotto.domain.WinningResult;
 
 public class LottoService {
     private static final int LOTTO_PRICE = 1000;
@@ -37,5 +41,22 @@ public class LottoService {
         return IntStream.range(0, count)
                 .mapToObj(i -> generateLotto())
                 .toList();
+    }
+
+    public Map<Prize, Integer> cacluateReults(List<Lotto> lottos, Lotto winningLotto, LottoNumber bonusNumber) {
+        Map<Prize, Integer> results = new EnumMap<>(Prize.class);
+        for (Prize prize : Prize.values()) {
+            results.put(prize, 0);
+        }
+
+        for (Lotto lotto : lottos) {
+            int matchCount = (int) lotto.getNumbers().stream()
+                    .filter(winningLotto::contains)
+                    .count();
+            boolean matchBonus = lotto.contains(bonusNumber);
+            Prize prize = new WinningResult(matchCount, matchBonus).calculatePrize();
+            results.put(prize, results.get(prize) + 1);
+        }
+        return results;
     }
 }
