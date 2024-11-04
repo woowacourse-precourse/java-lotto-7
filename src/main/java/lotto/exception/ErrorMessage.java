1a5cea1684f94f50f40f6c267d8d3536cf09915c
DongHoon Lee <dhl1924@naver.com>
2024-11-04 16:07:28 +0900

    feat(exception): 번호 범위 에러 처리 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,8 +4,8 @@ public enum ErrorMessage {
     NULL_INPUT_ERROR("null은 입력할 수 없는 값입니다. 다시 입력해 주세요."),
     EMPTY_INPUT_ERROR("빈 문자열은 입력할 수 없습니다. 다시 입력해 주세요."),
     INVALID_AMOUNT_PATTERN_ERROR("구입 금액은 1000원 단위의 양수여야 합니다. 다시 입력해 주세요."),
-    MAX_AMOUNT_ERROR("로또는 1인당 최대 100장까지 구매할 수 있습니다. 다시 입력해 주세요.")
-    ;
+    MAX_AMOUNT_ERROR("로또는 1인당 최대 100장까지 구매할 수 있습니다. 다시 입력해 주세요."),
+    INVALID_NUMBER_RANGE_ERROR("번호는 1에서 45 사이의 숫자여야 합니다. 유효한 범위 내의 숫자를 입력해 주세요.");
 
     private final String message;
 
