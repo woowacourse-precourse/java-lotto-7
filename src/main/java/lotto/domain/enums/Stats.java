531087d65a5857633216c63d8d82a080ebd048b5
gahyeon <1000ji1@naver.com>
2024-11-04 23:02:35 +0900

    style: enum 상수 이름 변경

--- src/main/java/lotto/domain/enums/Stats.java
+++ src/main/java/lotto/domain/enums/Stats.java
@@ -5,7 +5,7 @@ public enum Stats {
     FOUR_MATCH(4, 50000, "4개 일치 (50,000원) - "),
     FIVE_MATCH(5, 1500000, "5개 일치 (1,500,000원) - "),
     FIVE_MATCH_BONUS(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    SIX_MATCH_BONUS(6, 200000000, "6개 일치 (2,000,000,000원) - ");
+    SIX_MATCH(6, 200000000, "6개 일치 (2,000,000,000원) - ");
 
     private final int matchCount;
     private final int prize;
