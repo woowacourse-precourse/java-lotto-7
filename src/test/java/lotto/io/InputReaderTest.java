bc33fc738605415dcc06ca78b00df8e8ef5c02f9
woong2e <tlswodnd12345@naver.com>
2024-11-04 20:03:56 +0900

    test: add test for non-numeric input

--- src/test/java/lotto/io/InputReaderTest.java
+++ src/test/java/lotto/io/InputReaderTest.java
@@ -14,12 +14,22 @@ class InputReaderTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR] ";
 
     @Test
-    @DisplayName("구입금액 입력값 테스트")
-    void 구입금액_입력값_테스트() {
+    @DisplayName("구입금액 입력값 숫자범위 테스트")
+    void 구입금액_입력값_숫자범위_테스트() {
         assertSimpleTest(() -> {
             runException("2147483678");
             assertThat(output()).contains(
                     ERROR_MESSAGE + "구매할 수 있는 최대 금액을 초과했습니다.");
+        });
+    }
+
+    @Test
+    @DisplayName("구입금액 입력값 문자 입력 테스트")
+    void 구입금액_입력값_문자_입력_테스트() {
+        assertSimpleTest(() -> {
+            runException("asdfg43t5ger");
+            assertThat(output()).contains(
+                    ERROR_MESSAGE + "숫자를 입력하지 않았습니다.");
         });
     }
 
