9b4363a0ec44c44a8c9aee0b73578602a56e3cdc
belight2 <chlwnsgur090@gmail.com>
2024-11-04 19:25:23 +0900

    feat(Lotto): 로또 번호에 자릿수 값 반환 기능 추가
    getNumber(index)로 로또 번호에 index번 째 자리값을 반환하는 함수 추가
    index가 1 ~ 6 범위의 값이 아닌 경우 예외처리를 하였음.
    위 함수는 당첨 번호와 비교하기 위해 추가하였습니다.
    
    toString()을 Override하여 사용자가 구매한 로또들을 출력할 때 용이할 수
    있도록 하였습니다.

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -15,6 +15,18 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
+    private void getNumberValidate(int index){
+        if(index < 1 || index > 6){
+            throw new IllegalArgumentException("[ERROR] 로또는 1 ~ 6번째 자릿수만 존재합니다.");
+        }
+    }
     // TODO: 추가 기능 구현
+    public Integer getNumber(int index){
+        getNumberValidate(index);
+        return numbers.get(index-1);
+    }
+
+    public String toString(){
+        return numbers.toString();
+    }
 }
