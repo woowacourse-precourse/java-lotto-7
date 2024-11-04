d8856d18869d31b4925b6d078812eaf4f98c26fc
woojin cheon <qpqpqp0102@naver.com>
2024-11-04 22:40:01 +0900

    fix(InputView): 입력 메서드 수정
    - 반환 타입 변경
    - 반환값 간소화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,21 +2,23 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
 
-    public static int inputPurchasingMoney() {
-        return Integer.parseInt(Console.readLine());
+    private static final String DELIMITER = ",";
+
+    public static String inputPurchasingMoney() {
+        return Console.readLine();
     }
 
-    public static List<Integer> inputDefaultLottoNumbers() {
-        List<Integer> defaultLottoNumbers = new ArrayList<>();
-        defaultLottoNumbers.add(Integer.parseInt(Console.readLine()));
-        return defaultLottoNumbers;
+    public static List<String> inputDefaultLottoNumbers() {
+        return new ArrayList<>(
+            Arrays.asList(Console.readLine().split(DELIMITER)));
     }
 
-    public static int inputBonusNumber() {
-        return Integer.parseInt(Console.readLine());
+    public static String inputBonusNumber() {
+        return Console.readLine();
     }
 }
