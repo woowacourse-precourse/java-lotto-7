781f9599eb033593ddc447ca587d1edd183c1c81
euni <kongeuni1004@gmail.com>
2024-11-04 15:47:56 +0900

    style: Rank enum 클래스 코드 컨벤션에 맞춰 띄어쓰기 수정
    - 코드 컨벤션 준수를 위해 띄어쓰기 및 공백 정리

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -4,10 +4,10 @@ public enum Rank {
 
     FIRST(6, 2000000000),
     SECOND(5, true, 30000000),
-    THIRD(5,  1500000),
-    FOURTH(4,  50000),
-    FIFTH(3,  5000),
-    NONE(0,0);
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
 
     private final int matchCount;
     private final boolean requireBonus;
@@ -24,8 +24,8 @@ public enum Rank {
     }
 
     public static Rank valueOf(int matchCount, boolean matchBonus) {
-        if(matchCount == 5){
-            if(matchBonus){
+        if (matchCount == 5) {
+            if (matchBonus) {
                 return SECOND;
             }
             return THIRD;
@@ -34,8 +34,8 @@ public enum Rank {
     }
 
     public static Rank getRankWithoutBonus(int matchCount) {
-        for(Rank rank : Rank.values()){
-            if(rank.matchCount == matchCount && !rank.requireBonus){
+        for (Rank rank : Rank.values()) {
+            if (rank.matchCount == matchCount && !rank.requireBonus) {
                 return rank;
             }
         }
