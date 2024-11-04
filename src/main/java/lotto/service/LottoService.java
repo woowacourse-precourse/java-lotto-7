9ec660375b401991f62f18931d0d1ecc40ad4480
shinebyul <qufdl8382@gmail.com>
2024-11-04 18:00:23 +0900

    [Feat] 예외메세지 사용

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import java.util.List;
 import java.util.Scanner;
+import lotto.global.exception.Messages;
 
 public class LottoService {
     private static final String GET_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
@@ -17,7 +18,7 @@ public class LottoService {
             price = scanner.nextInt();
 
             if(price%1000!=0){
-                throw new IllegalArgumentException("");
+                throw new IllegalArgumentException(Messages.DEFAULT_ERROR + Messages.PRICE_ERROR);
             }
         }
 
