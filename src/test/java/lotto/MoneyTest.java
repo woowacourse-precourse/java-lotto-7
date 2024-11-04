9ca1f712009bbcc854c4044f13c74e38252caae5
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:48:48 +0900

    test(Money): 잔돈의 유무를 알 수 있다

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,13 +1,22 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class MoneyTest {
 
     @Test
     void 금액이_음수면_예외를_발생시킨다() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Money(-1000));
+    }
+
+    @ParameterizedTest(name = "{0}으로 {1}짜리를 사면 잔돈이 생긴다={2}")
+    @CsvSource(value = {"15000,1500,false", "10000,1100,true"}, delimiter = ',')
+    void 잔돈의_유무를_알_수_있다(int amount, int price, boolean expected) {
+        assertThat(new Money(amount).hasChangesWith(price)).isEqualTo(expected);
     }
 }
