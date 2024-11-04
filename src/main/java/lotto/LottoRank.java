a2e02aaeef409358baf88bb200d19d8a169a5529
heo5620 <tnqls1035@naver.com>
2024-11-04 22:49:30 +0900

    fix : LottoRank description 수정

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -6,7 +6,7 @@ public enum LottoRank {
     FOURTH(4, 50000, "4개 일치 (50,000원)"),
     THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
     SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    FIRST(6, 2000000000, "5개 일치, 보너스 볼 일치 (30,000,000원)");
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
     private final int matchCount;
     private final int prizeMoney;
@@ -51,7 +51,7 @@ public enum LottoRank {
             }
         }
 
-        boolean isMatchBonusNumber = userLotto.contains(bonusNumber);
+        boolean isMatchBonusNumber = winningLotto.contains(bonusNumber);
 
         return LottoRank.of(matchCount, isMatchBonusNumber);
     }
