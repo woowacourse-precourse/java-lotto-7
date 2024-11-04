7fa21f69e5e379dd346d011593509f2bb6705720
jungsu <vwx1@naver.com>
2024-11-04 18:26:25 +0900

    refactor: LottoPrize를 사용하게 LottoService 변경
    - LottoPrize enum을 사용하게 LottoService를 변경했습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.constants.LottoPrize;
 
 public class LottoService implements LottosServiceInterface {
 
   @Override
-  public int getNumOfLottos(int purchaseAmount){
-    return purchaseAmount/1000;
+  public int getNumOfLottos(int purchaseAmount) {
+    return purchaseAmount / 1000;
   }
+
   @Override
   public List<Lotto> generateLottosByAmount(int numOfLottos) {
     List<Lotto> lottos = new ArrayList<>();
@@ -23,29 +25,27 @@ public class LottoService implements LottosServiceInterface {
   }
 
 
-
   @Override
   public List<Integer> checkWinningNumbers(List<Lotto> lottos, List<Integer> winningNumbers,
       int bonusNumber) {
-    List<Integer> winningCounts = Arrays.asList(0, 0, 0, 0,
-        0);
+    List<Integer> winningCounts = Arrays.asList(0, 0, 0, 0, 0);
 
     for (Lotto lotto : lottos) {
       long matchCount = lotto.getNumbers().stream()
           .filter(winningNumbers::contains)
           .count();
 
-      if (matchCount == 3) {
+      if (matchCount == LottoPrize.FIFTH.getMatchCount()) {
         winningCounts.set(0, winningCounts.get(0) + 1);
-      } else if (matchCount == 4) {
+      } else if (matchCount == LottoPrize.FOURTH.getMatchCount()) {
         winningCounts.set(1, winningCounts.get(1) + 1);
-      } else if (matchCount == 5) {
+      } else if (matchCount == LottoPrize.THIRD.getMatchCount()) {
         if (lotto.getNumbers().contains(bonusNumber)) {
           winningCounts.set(3, winningCounts.get(3) + 1);
         } else {
           winningCounts.set(2, winningCounts.get(2) + 1);
         }
-      } else if (matchCount == 6) {
+      } else if (matchCount == LottoPrize.FIRST.getMatchCount()) {
         winningCounts.set(4, winningCounts.get(4) + 1);
       }
     }
@@ -55,11 +55,11 @@ public class LottoService implements LottosServiceInterface {
   @Override
   public double calculateYield(List<Integer> winningCounts, int purchaseAmount) {
 
-    int totalPrize = (winningCounts.get(0) * Prize.FIFTH.getAmount()) +
-        (winningCounts.get(1) * Prize.FOURTH.getAmount()) +
-        (winningCounts.get(2) * Prize.THIRD.getAmount()) +
-        (winningCounts.get(3) * Prize.SECOND.getAmount()) +
-        (winningCounts.get(4) * Prize.FIRST.getAmount());
+    int totalPrize = (winningCounts.get(0) * LottoPrize.FIFTH.getPrizeAmount()) +
+        (winningCounts.get(1) * LottoPrize.FOURTH.getPrizeAmount()) +
+        (winningCounts.get(2) * LottoPrize.THIRD.getPrizeAmount()) +
+        (winningCounts.get(3) * LottoPrize.SECOND.getPrizeAmount()) +
+        (winningCounts.get(4) * LottoPrize.FIRST.getPrizeAmount());
 
     double yield = (double) totalPrize / purchaseAmount * 100;
 
