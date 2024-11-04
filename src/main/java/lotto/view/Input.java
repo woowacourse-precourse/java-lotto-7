b9aa7c69041d9d871c542d3a3014e1d1140254c6
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 15:14:37 +0900

    feat: 당첨 번호 입력 기능 작성 Input 클래스 inputWinningNumbers()

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -2,13 +2,25 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
     public static int inputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         checkInput(input);
         int purchasePrice = checkNumber(input);
         checkUnit(purchasePrice);
         return purchasePrice;
+    }
+
+    public static void inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        checkInput(input);
+        List<String> winningNumbersStr = Arrays.asList(input.split(","));
     }
 
     private static void checkInput(String input) {
@@ -30,4 +42,5 @@ public class Input {
         if(number % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다." + number);
     }
+
 }
