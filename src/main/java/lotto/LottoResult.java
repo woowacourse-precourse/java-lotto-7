68fcb7b20f1a86b0b016680a2719aabd6ed6f691
Kiminni <imk0980@gmail.com>
2024-11-04 22:07:46 +0900

    feat: LottoResult 추가

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,28 @@
+// LottoResult.java
+
+package lotto;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+    public static Map<Rank, Integer> calculateResults(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        Map<Rank, Integer> results = initializeResults();
+        for (Lotto lotto : lottos) {
+            Rank rank = lotto.getRank(winningNumbers, bonusNumber);
+            if (rank != null) {
+                results.put(rank, results.get(rank) + 1);
+            }
+        }
+        return results;
+    }
+
+    private static Map<Rank, Integer> initializeResults() {
+        Map<Rank, Integer> results = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            results.put(rank, 0);
+        }
+        return results;
+    }
+}
