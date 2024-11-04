0ee493a8413a1f6651ec4b4fdc11e52eab9c08fb
yoona <103414935+yoonali@users.noreply.github.com>
2024-11-04 19:00:43 +0900

    feat : 로또 번호 생성할 횟수 반환

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,17 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.global.string.QuestionConstants;
+import lotto.service.LottoService;
+
 public class LottoController {
+    private LottoService lottoService = new LottoService();
+
+    public void getAmount() {
+        System.out.println(QuestionConstants.INPUT_MONEY);
+        String inputText = Console.readLine().trim();
+        int money = Integer.parseInt(inputText);
+
+        lottoService.getAmount(money);
+    }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,10 @@
 package lotto.service;
 
 public class LottoService {
+    private int money;
+
+    public int getAmount(int money) {
+        this.money = money;
+        return money / 1000;
+    }
 }
