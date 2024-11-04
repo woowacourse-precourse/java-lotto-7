4d4d7848f636b3b671df52d9bfd93e222978003f
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-11-04 10:01:57 +0900

    feat(InputView) : 입력 êµ기능 일부 구현

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class LottoController {
+
+    public void run() {
+        System.out.println("실행");
+    }
+
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,8 +9,24 @@ package view;
 //6개 일치 (2,000,000,000원) - 0개
 //총 수익률은 62.5%입니다.
 
+
+import camp.nextstep.edu.missionutils.Console;
+
+//구입금액을 입력해 주세요.
+//8개를 구매했습니다.
+//당첨 번호를 입력해 주세요.
+//보너스 번호를 입력해 주세요.
 public class InputView {
+    Messages messages;
 
+    public String getCost() {
+        System.out.println(messages.PAYMENT_INPUT_MSG.getMessage());
+        return Console.readLine();
+    }
 
+    public String getLottoNumbers(){
+        System.out.println(messages.LOTTO_INPUT_MSG.getMessage());
+        return Console.readLine();
+    }
 
 }
