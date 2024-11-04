c65a41b0b9d159e5eea3fc0f1b6b74b8992f1f82
DoomchitYJ <poj1004@inha.edu>
2024-11-04 18:10:02 +0900

    fix(LottoMaker): sort 오류 해결

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -1,6 +1,11 @@
 package lotto.domain;
 
+import static lotto.constant.Constant.NUMBERS_MAX_COUNT;
+import static lotto.constant.Constant.NUMBERS_RANGE_END;
+import static lotto.constant.Constant.NUMBERS_RANGE_START;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.Lotto;
@@ -8,8 +13,9 @@ import lotto.Lotto;
 public class LottoMaker {
 
     public Lotto makeLotto() {
-        List<Integer> numbers = Randoms
-                .pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>
+                (Randoms.pickUniqueNumbersInRange(NUMBERS_RANGE_START,
+                        NUMBERS_RANGE_END, NUMBERS_MAX_COUNT));
         sortNumbers(numbers);
 
         return new Lotto(numbers);
