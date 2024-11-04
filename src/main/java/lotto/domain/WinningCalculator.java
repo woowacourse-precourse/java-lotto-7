7043ee767f842c22b404051aee8ce358ffd5e52e
junest66 <chlwndks333@gmail.com>
2024-11-04 16:22:35 +0900

    feat(WinningCalculator): 당첨 결과를 계산하는 클래스 추가

---
+++ src/main/java/lotto/domain/WinningCalculator.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+public class WinningCalculator {
+    private static final String DUPLICATE_BONUS_NUMBER_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+    private PurchaseLottos purchaseLottos;
+    private Lotto winningLotto;
+    private int bonusNumber;
+
+    public void saveLottos(PurchaseLottos purchaseLottos) {
+        this.purchaseLottos = purchaseLottos;
+    }
+
+    public void saveWinningLotto(Lotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public void saveBonusNumber(int bonusNumber) {
+        validate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public WinningResult calculate() {
+        WinningResult winningResult = new WinningResult();
+        for (Lotto lotto : purchaseLottos.getLottos()) {
+            Rank rank = Rank.calculate(lotto.matchCount(winningLotto), lotto.hasNumber(bonusNumber));
+            winningResult.add(rank);
+        }
+        return winningResult;
+    }
+
+    private void validate(int bonusNumber) {
+        if (winningLotto.hasNumber(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER_WITH_WINNING_NUMBER);
+        }
+    }
+}
