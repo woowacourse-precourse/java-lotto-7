1f5b160f536710eed0460daaffaccf727011a4c6
Chan531 <chan657312@gmail.com>
2024-11-04 18:41:58 +0900

    test(LottoBundleTest): 로또 개수 생성 테스트
    구입 금액에 따라 로또 개수 생성

---
+++ src/test/java/lotto/model/LottoBundleTest.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoBundleTest {
+
+	@Test
+	@DisplayName("구입 금액에 따라 로또 개수를 생성할 수 있다.")
+	void 구입_금액에_따라_로또_개수를_생성한다() {
+		// given
+		LottoCreator lottoCreator = new LottoCreator();
+		int lottoPrice = 1000;
+		int count = 10;
+		Price purchasePrice = new Price(lottoPrice * count);
+
+		// when
+		LottoBundle lottoBundle = new LottoBundle(purchasePrice, lottoCreator);
+
+		// then
+		assertEquals(count, lottoBundle.getLottos().size());
+	}
+}
