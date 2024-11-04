b91af67ad7922edc117af070b0f10be76af1376d
eunho <eunho1032@gmail.com>
2024-11-04 13:53:06 +0900

    refactor: add newline

--- src/main/java/lotto/view/Prompt.java
+++ src/main/java/lotto/view/Prompt.java
@@ -2,8 +2,8 @@ package lotto.view;
 
 public enum Prompt {
     ENTER_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    ENTER_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    ENTER_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    ENTER_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.");
 
     private final String message;
 
