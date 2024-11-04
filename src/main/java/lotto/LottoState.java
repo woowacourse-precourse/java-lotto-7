a49daaf312f69d80d6492384c6e3f9e1b3df8df8
heeyeop Kim <ssucgoo@gmail.com>
2024-11-04 23:16:11 +0900

    feat: 정답 현황을 보여주는 클래스 구현

---
+++ src/main/java/lotto/LottoState.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class LottoState {
+    private int bonusCount;
+    private int normalCount;
+
+    public LottoState() {
+        this.bonusCount = 0;
+        this.normalCount = 0;
+    }
+}
