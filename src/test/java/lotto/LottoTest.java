4add4d10016f008a7bdd0136621d094ce6c84c7d
esfuerzo <dkdldrha@naver.com>
2024-11-04 23:34:56 +0900

    test : Lotto.class test
    Lotto.class test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,6 +20,12 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호_출력() {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)).getNumbers())
+                .isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
