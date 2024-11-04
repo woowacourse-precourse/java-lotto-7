33c2af8357bc32a4014e7678c596344a3467976c
gyuoo <yeomky98@naver.com>
2024-11-04 21:46:36 +0900

    feat(exception): add LottoException base class for custom exceptions

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,22 @@
+package lotto.exception;
+
+public abstract class LottoException extends RuntimeException {
+
+	private final String ERROR_MESSAGE_PREFIX = "[ERROR]";
+	private final String SPACE = " ";
+	private final ErrorMessage errorMessage;
+
+	public LottoException(ErrorMessage errorMessage) {
+		super(errorMessage.getErrorMessage());
+		this.errorMessage = errorMessage;
+	}
+
+	public ErrorMessage getErrorMessage() {
+		return errorMessage;
+	}
+
+	@Override
+	public String getMessage() {
+		return ERROR_MESSAGE_PREFIX + SPACE + super.getMessage();
+	}
+}
