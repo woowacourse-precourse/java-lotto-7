8c0afe4ae50276cc7da60df52de5e59d9a833b01
SongJaeHoonn <thdwogns121@naver.com>
2024-11-04 14:54:29 +0900

    refactor: 로또 구입 금액 입력 view 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static void inputLottoPurchase() {
+        System.out.println("구입 금액을 입력해주세요.");
+        Console.readLine();
+    }
+}
