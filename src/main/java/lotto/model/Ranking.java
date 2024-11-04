1eea5df9cfbd9280e15b11bb9949492552a34974
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 19:08:42 +0900

    feat(Ranking): 당첨 금액 분ë 분류 기능 구현

--- src/main/java/lotto/model/Ranking.java
+++ src/main/java/lotto/model/Ranking.java
@@ -1,4 +1,44 @@
 package lotto.model;
 
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+
 public enum Ranking {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Ranking(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public static Ranking getRanking(int matchCount, boolean matchBonus) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && matchBonus) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
