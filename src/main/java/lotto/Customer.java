929004a18fbbe2df2f6bf380cd1599d820637915
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 21:06:12 +0900

    feat(Customer): 투입한 금액을 반환하는 함수를 생성한다
    투입한 금액을 반환해주는 getMoney함수를 생성한다.

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -1,23 +1,34 @@
 package lotto;
 
 public class Customer {
-    static public int lottoCount;
+    public int lottoCount;
+    public int budget;
 
     public Customer(String budget) {
         calculateLottoCount(budget); // 생성자에서 static 변수에 값 할당
     }
 
     private void calculateLottoCount(String budget) {
-        int totalBudget = Integer.parseInt(budget);
+        int totalBudget = 0;
+        try {
+            totalBudget = Integer.parseInt(budget);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("ERROR");
+        }
         // 입력하는 값이 숫자인지도 검증해야됨
         if (totalBudget % 1000 > 0) {
             throw new IllegalArgumentException("[ERROR] 입력하는 돈이 1000으로 나누어 떨어져야합니다.");
         }
         lottoCount = totalBudget / 1000;
+        this.budget = totalBudget;
     }
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    public int getMoney() {
+        return budget;
     }
 
 }
