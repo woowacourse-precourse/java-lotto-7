ab6f624353a3602abd701c0442d0f68a6f12ead7
eunho <eunho1032@gmail.com>
2024-11-04 13:22:04 +0900

    refactor: reduce hard code

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -8,6 +8,12 @@ import java.util.Map;
 
 public class Output {
 
+    private final String MATCHSIX = "2,000,000,000원";
+    private final String MATCHFIVEBONUS = "5,000,000원";
+    private final String MATCHFIVE = "1,500,000원";
+    private final String MATCHFOUR = "50,000원";
+    private final String MATCHTHREE = "5,000원";
+
     public void printPrompt(Prompt prompt) {
         System.out.println(prompt.getMessage());
     }
@@ -27,14 +33,15 @@ public class Output {
     }
 
     public void printStatistics(Statistics statistics, int purchaseAmount) {
+        Map<Integer, Integer> results = statistics.getResults();
+
         System.out.println("당첨 통계");
         System.out.println("---");
-        Map<String, Integer> results = statistics.getResults();
-        System.out.println("3개 일치 (5,000원) - " + results.getOrDefault("5등", 0) + "개");
-        System.out.println("4개 일치 (50,000원) - " + results.getOrDefault("4등", 0) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + results.getOrDefault("3등", 0) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + results.getOrDefault("2등", 0) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + results.getOrDefault("1등", 0) + "개");
+        System.out.println("6개 일치 (" + MATCHSIX + ") - " + results.getOrDefault(1, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + MATCHFIVEBONUS + ") - " + results.getOrDefault(2, 0) + "개");
+        System.out.println("5개 일치 (" + MATCHFIVE + ") - " + results.getOrDefault(3, 0) + "개");
+        System.out.println("4개 일치 (" + MATCHFOUR + ") - " + results.getOrDefault(4, 0) + "개");
+        System.out.println("3개 일치 (" + MATCHTHREE + ") - " + results.getOrDefault(5, 0) + "개");
 
         double profitRate = ((double) statistics.getTotalPrize() / purchaseAmount) * 100;
         System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
