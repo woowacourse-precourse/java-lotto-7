2fdc0c65f670413ee2a301451cb975a10047d129
kangsan <devstar1223@gmail.com>
2024-11-04 19:22:29 +0900

    feat: 로또 당첨 등 수 Enum 추가

---
+++ src/main/java/lotto/model/LottoRank.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+public enum LottoRank {
+    FIRST(1, "6개 일치", 2000000000),
+    SECOND(2, "5개 일치, 보너스 볼 일치", 30000000),
+    THIRD(3, "5개 일치", 1500000),
+    FOURTH(4, "4개 일치", 50000),
+    FIFTH(5, "3개 일치", 5000),
+    NONE(0, "꽝", 0);
+
+    private final int rank;
+    private final String description;
+    private final int prize;
+
+    LottoRank(int rank, String description, int prize) {
+        this.rank = rank;
+        this.description = description;
+        this.prize = prize;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrize() {
+        return prize; // 당첨 금액 반환
+    }
+}
