11866d3a789e510b59f5eff542691ba121408c3e
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:29:05 +0900

    Feat: 당첨 결과 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,13 @@
 package lotto.view;
 
+import java.util.EnumMap;
 import lotto.domain.Customer;
+import lotto.domain.Rank;
 
 public class OutputView {
 
     private static final String LOTTOS_PURCHASED_MESSAGE = "\n%d개를 구매했습니다.";
+    private static final String RESULT_MESSAGE = "\n당첨 통계\n---";
 
     public static void lottosPurchasedMessage(Customer customer) {
         System.out.println(String.format(LOTTOS_PURCHASED_MESSAGE, customer.getPrice().getCount()));
@@ -15,5 +18,10 @@ public class OutputView {
                 .getValues()
                 .stream()
                 .forEach(lotto -> System.out.println(lotto));
+    }
+
+    public static void resultMessage(EnumMap<Rank, Integer> rankMap) {
+        System.out.print(RESULT_MESSAGE);
+        rankMap.forEach((rank, matchCount) -> System.out.println(String.format(rank.getMessage(), matchCount)));
     }
 }
