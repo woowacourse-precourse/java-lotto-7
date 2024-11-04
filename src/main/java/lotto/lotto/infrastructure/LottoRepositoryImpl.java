aad3e60d4331839703d1da611d40274eb4af0a38
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-11-04 16:40:47 +0900

    refactor(repository): repository에 예외 사항을 추가한다
    각 애그리거트의 repository에 예외 사항을 추가하고 Optional을 사용하여 예외를 던진다.

--- src/main/java/lotto/lotto/infrastructure/LottoRepositoryImpl.java
+++ src/main/java/lotto/lotto/infrastructure/LottoRepositoryImpl.java
@@ -1,6 +1,7 @@
 package lotto.lotto.infrastructure;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import lotto.lotto.domain.LottoResults;
 import lotto.lotto.service.port.LottoRepository;
@@ -10,11 +11,16 @@ public class LottoRepositoryImpl implements LottoRepository {
     private static LottoRepository instance;
     private final Map<String, LottoResults> lottoResultsMap = new ConcurrentHashMap<>();
 
-    private LottoRepositoryImpl() {}
+    private LottoRepositoryImpl() {
+    }
 
     public static LottoRepository getInstance() {
-        if(instance == null) {
-            instance = new LottoRepositoryImpl();
+        if (instance == null) {
+            synchronized (LottoRepositoryImpl.class) {
+                if (instance == null) {
+                    instance = new LottoRepositoryImpl();
+                }
+            }
         }
         return instance;
     }
@@ -28,9 +34,10 @@ public class LottoRepositoryImpl implements LottoRepository {
     @Override
     public LottoResults findById(String id) {
         try {
-            return lottoResultsMap.get(id);
-        } catch (Exception e) {
-            throw new RuntimeException("[ERROR] 해당 id 값이 존재하지 않습니다");
+            return Optional.ofNullable(lottoResultsMap.get(id))
+                    .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 id 값이 존재하지 않습니다."));
+        } catch (NullPointerException e) {
+            throw new IllegalStateException("[ERROR] id는 null이 될 수 없습니다.");
         }
     }
 
--- src/main/java/lotto/purchase/infrastructure/PurchaseRepositoryImpl.java
+++ src/main/java/lotto/purchase/infrastructure/PurchaseRepositoryImpl.java
@@ -1,6 +1,7 @@
 package lotto.purchase.infrastructure;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import lotto.purchase.domain.Purchase;
 import lotto.purchase.service.port.PurchaseRepository;
@@ -8,13 +9,18 @@ import lotto.purchase.service.port.PurchaseRepository;
 public class PurchaseRepositoryImpl implements PurchaseRepository {
 
     private static PurchaseRepository instance;
-    private final Map<String, Purchase> purchaseResultMap = new ConcurrentHashMap<>();
+    private final Map<String, Purchase> purchaseCache = new ConcurrentHashMap<>();
 
-    private PurchaseRepositoryImpl() {}
+    private PurchaseRepositoryImpl() {
+    }
 
     public static PurchaseRepository getInstance() {
         if (instance == null) {
-            instance = new PurchaseRepositoryImpl();
+            synchronized (PurchaseRepositoryImpl.class) {
+                if (instance == null) {
+                    instance = new PurchaseRepositoryImpl();
+                }
+            }
         }
         return instance;
     }
@@ -22,15 +28,20 @@ public class PurchaseRepositoryImpl implements PurchaseRepository {
     @Override
     public Purchase findById(String id) {
         try {
-            return purchaseResultMap.get(id);
+            return Optional.ofNullable(purchaseCache.get(id))
+                    .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 id값이 존재하지 않습니다."));
         } catch (NullPointerException e) {
-            throw new NullPointerException("[ERROR] 해당 id 값이 존재하지 않습니다.");
+            throw new IllegalStateException("[ERROR] 해당 id 값이 null일 수 없습니다.");
         }
     }
 
     @Override
     public Purchase save(String id, Purchase purchase) {
-        purchaseResultMap.put(id, purchase);
-        return purchase;
+        try {
+            purchaseCache.put(id, purchase);
+            return purchase;
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("[ERROR] key 또는 value에 null이 존재할 수 없습니다");
+        }
     }
 }
