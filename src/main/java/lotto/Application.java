b2aab56184c3bf2dbd4a8578cccf4eeb051363c5
threepebbles <threepebbles@naver.com>
2024-11-04 14:28:42 +0900

    feat(MainController): 컨트롤러 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import com.sun.tools.javac.Main;
+import lotto.controller.MainController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        MainController mainController = new MainController(inputView, outputView);
+        mainController.run();
     }
 }
---
+++ src/main/java/lotto/controller/MainController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class MainController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MainController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        Integer price = inputView.scanPrice();
+
+    }
+}
