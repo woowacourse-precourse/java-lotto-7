8e425888089e8fa6ab305c85476156c084f9027b
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-04 18:58:44 +0900

    test(Money): 구입 금액에 대한 테스트 케이스 추가
    Money 클래스의 calcRateOfReturn 메서드에 대한 테스트 케이스를 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import lotto.exception.ExceptionMessages;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
@@ -33,5 +37,21 @@ public class MoneyTest {
     @ValueSource(ints = {55000, 10000, 32000})
     void 입력받은_금액이_로또_가격으로_나누어_떨어지는_경우_예외가_발생하지_않는다(int amount) {
         assertThatCode(() -> new Money(amount)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("amountAndSumOfPrize")
+    void 매개변수로_받은_금액을_바탕으로_수익률을_반환한다(int amount, long sumOfPrize, double rateOfReturn) {
+        Money money = new Money(amount);
+        assertThat(money.calcRateOfReturn(sumOfPrize)).isEqualTo(rateOfReturn);
+    }
+
+    static Stream<Arguments> amountAndSumOfPrize() {
+        return Stream.of(
+                Arguments.of(5000, 5000L, 100.0),
+                Arguments.of(5000, 50000L, 1000.0),
+                Arguments.of(5000, 1500000L, 30000.0),
+                Arguments.of(5000, 30000000L, 600000.0)
+        );
     }
 }
