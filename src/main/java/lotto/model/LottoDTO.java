c755b5398fb79cd6f4b844090ae0e8d1580cd5ef
20160295 <dorem96@icloud.com>
2024-11-04 23:16:50 +0900

    feat: DTO NULL 예외 처리

--- src/main/java/lotto/model/LottoDTO.java
+++ src/main/java/lotto/model/LottoDTO.java
@@ -3,9 +3,13 @@ package lotto.model;
 import java.util.List;
 
 public class LottoDTO {
+    private static final String ERROR_MESSAGE_INVALID_COUNT = "[ERROR] 로또 번호가 이어있습니다.";
     private final List<Integer> numbers;
 
     public LottoDTO(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new NullPointerException(ERROR_MESSAGE_INVALID_COUNT);
+        }
         this.numbers = numbers;
     }
 
