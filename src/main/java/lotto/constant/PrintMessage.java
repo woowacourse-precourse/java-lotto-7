8cb8ef1de4873606a71a629c68d4c89e3c6ae963
agi1004 <agi1009@naver.com>
2024-11-04 22:19:53 +0900

    feat: 출력 메시지 상수 클래스

---
+++ src/main/java/lotto/constant/PrintMessage.java
@@ -0,0 +1,13 @@
+package lotto.constant;
+
+public final class PrintMessage {
+	public static final String REQ_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+	public static final String REQ_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+	public static final String REQ_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+	
+	public static final String RES_PURCHASE_COUNT = "개를 구매했습니다.";
+	public static final String RES_WINNING_DETAILS = "\n당첨 통계\n---";
+	public static final String RES_COUNT = "개";
+	public static final String RES_RATE = "총 수익률은 ";
+	public static final String RES_PERCENT = "%입니다.";
+}
