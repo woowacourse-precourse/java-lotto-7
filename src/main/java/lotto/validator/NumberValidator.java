058c4e2ba3a6de1f5219f36fbd1aebf7c39a159f
Koo-EunSung <ku903@naver.com>
2024-11-04 22:59:51 +0900

    Refactor: 예외 메세지 enum으로 관리

--- src/main/java/lotto/validator/NumberValidator.java
+++ src/main/java/lotto/validator/NumberValidator.java
@@ -1,13 +1,12 @@
 package lotto.validator;
 
+import static lotto.message.ExceptionMessage.DUPLICATE;
+import static lotto.message.ExceptionMessage.ONLY_NUMBER;
+import static lotto.message.ExceptionMessage.OUT_OF_RANGE;
+
 import java.util.List;
 
 public class NumberValidator {
-    private static final String PREFIX = "[ERROR]";
-    private static final String OUT_OF_RANGE_MESSAGE = PREFIX + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-    private static final String ONLY_NUMBER_MESSAGE = PREFIX + " 입력값은 숫자여야 합니다.";
-    private static final String DUPLICATE_MESSAGE = PREFIX + " 로또 번호는 중복되는 숫자가 없어야 합니다.";
-
     private static final Integer MIN = 1;
     private static final Integer MAX = 45;
 
@@ -15,7 +14,7 @@ public class NumberValidator {
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException(OUT_OF_RANGE_MESSAGE);
+            throw new NumberFormatException(ONLY_NUMBER.getMessage());
         }
     }
 
@@ -27,12 +26,12 @@ public class NumberValidator {
 
     public void checkInRange(Integer value) {
         if (value < MIN || value > MAX) {
-            throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
+            throw new IllegalArgumentException(OUT_OF_RANGE.getMessage());
         }
     }
 
     public void isDuplicateNumber(List<Integer> number) {
         if(number.size() != number.stream().distinct().count())
-            throw new IllegalArgumentException(DUPLICATE_MESSAGE);
+            throw new IllegalArgumentException(DUPLICATE.getMessage());
     }
 }
