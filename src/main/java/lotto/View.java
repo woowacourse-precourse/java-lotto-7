d4eb5988e620056eafbf461ba8ab8444c63893ce
bee9827 <ehfrhfo9494@naver.com>
2024-11-04 17:19:18 +0900

    feat: Create View
    안내문구 출력과
    List로된 값들을 각 줄마다 출력한다.

---
+++ src/main/java/lotto/View.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class View {
+    public static final String COST_INFORMATION = "구입금액을 입력해 주세요.";
+    public static final String NUMBERS_INFORMATION = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INFORMATION = "보너스 번호를 입력해 주세요.";
+
+    private String getInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
+    public String getPurchaseCost() {
+        return getInput(COST_INFORMATION);
+    }
+
+    public String getWinningNumbers() {
+        return getInput(NUMBERS_INFORMATION);
+    }
+
+    public String getBonusNumber() {
+        return getInput(BONUS_NUMBER_INFORMATION);
+    }
+
+    public void println(List<String> messages) {
+        messages.forEach(System.out::println);
+    }
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+}
