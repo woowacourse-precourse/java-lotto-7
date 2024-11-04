fe73dc56f187a27ea3f0e9f1442b80c8ae7fb6af
hooniverse <sung010326@gmail.com>
2024-11-04 18:39:18 +0900

    refactor: 구매 메세지 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,12 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public int getLottoCost(){
+    private static final String PURCHASE_MESSAGE = "\n구입금액을 입력해 주세요.";
+    private static final String WINNING_LOTTO_MESSAGE = "\n당첨 번호를 입력해 주세요.";
 
+    public int getLottoCost() {
+        System.out.println(PURCHASE_MESSAGE);
         return Integer.parseInt(Console.readLine());
     }
 
-    public String getWinningLotto(){
+    public String getWinningLotto() {
+        System.out.println(WINNING_LOTTO_MESSAGE);
         return Console.readLine();
     }
 }
