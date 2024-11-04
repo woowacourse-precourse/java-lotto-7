c621ad2beed8d489e5540c21415e435f166a6b5d
dltmddus <tmddus4671@gmail.com>
2024-11-04 15:35:18 +0900

    refactor: matchedCount 상수화

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,10 +1,16 @@
 package lotto.domain;
 
+import static lotto.util.LottoConstants.DEFAULT;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
+    private static final int MATCHED_COUNT_FOR_FIRST_PLACE = 6;
+    private static final int MATCHED_COUNT_FOR_SECOND_OR_THIRD_PLACE = 5;
+    private static final int MATCHED_COUNT_FOR_FOURTH_PLACE = 4;
+    private static final int MATCHED_COUNT_FOR_FIFTH_PLACE = 3;
     private final Map<Rank, Integer> matchNumber;
     private final LottoShop lottoShop;
     private final Lotto winningLotto;
@@ -33,7 +39,7 @@ public class LottoResult {
         Rank rank = getCorrectRank(lotto, matchedCount);
 
         if (rank != Rank.NOT_MATCH) {
-            matchNumber.put(rank, matchNumber.getOrDefault(rank, 0) + 1);
+            matchNumber.put(rank, matchNumber.getOrDefault(rank, DEFAULT.getValue()) + 1);
         }
     }
 
@@ -44,16 +50,16 @@ public class LottoResult {
     }
 
     private Rank getCorrectRank(Lotto lotto, int matchedCount) {
-        if (matchedCount == 6) {
+        if (matchedCount == MATCHED_COUNT_FOR_FIRST_PLACE) {
             return Rank.FIRST_PLACE;
         }
-        if (matchedCount == 5) {
+        if (matchedCount == MATCHED_COUNT_FOR_SECOND_OR_THIRD_PLACE) {
             return getSecondOrThirdPlaceRank(lotto);
         }
-        if (matchedCount == 4) {
+        if (matchedCount == MATCHED_COUNT_FOR_FOURTH_PLACE) {
             return Rank.FOURTH_PLACE;
         }
-        if (matchedCount == 3) {
+        if (matchedCount == MATCHED_COUNT_FOR_FIFTH_PLACE) {
             return Rank.FIFTH_PLACE;
         }
         return Rank.NOT_MATCH;
