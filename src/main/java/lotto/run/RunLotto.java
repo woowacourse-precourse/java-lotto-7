059ade0bc1edfb1ca31a652e82a152c2076428c0
yubin8773 <hiyubin.lee@gmail.com>
2024-11-04 23:28:10 +0900

    feat : 로또 번호 램덤 생성 및 오름차순으로 정렬

--- src/main/java/lotto/run/RunLotto.java
+++ src/main/java/lotto/run/RunLotto.java
@@ -3,14 +3,24 @@ package lotto.run;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.view.Output;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Choice {
+public class RunLotto {
     static List<Lotto> lottos;
+    static final int LOTTO_PRICE = 1000;
 
-    private List<Lotto> generateLottos(long purchasePrice) {
-        issueLottos();
+    public RunLotto() {
+
+    }
+
+    public static List<Lotto> generateLottos(long purchasePrice) {
+        int lotteryCnt = Math.toIntExact(purchasePrice / LOTTO_PRICE);
+        lottos = new ArrayList<>();
+        for (int i = 0; i < lotteryCnt; i++) {
+            issueLottos();
+        }
         List<Lotto> lottos = getLottos();
         Output.printLottos(lottos);
         return lottos;
@@ -25,7 +35,7 @@ public class Choice {
         lottos.add(lotto);
     }
 
-    public List<Lotto> getLottos() {
+    public static List<Lotto> getLottos() {
         return lottos;
     }
 }
