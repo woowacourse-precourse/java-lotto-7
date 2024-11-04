dcbfbbb9988f734be9090ce98bcbd169bfab7136
BusanGukbap <dong2857210@gmail.com>
2024-11-04 14:20:36 +0900

    feat(Util) : Add lotto number generate
    로또 번호 생성하는 메소드 추가

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -1,6 +1,14 @@
 package lotto.util;
 
-public class LottoNumberGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.*;
 
-    
+import java.util.List;
+
+public class LottoNumberGenerator {
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(
+                LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER, 6
+        ).stream().sorted().toList();
+    }
 }
