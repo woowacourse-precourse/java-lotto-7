f0c099e32e694dad5d5644f6708c6846226b6ae0
hwangdaesun <hds650588@gmail.com>
2024-11-04 18:53:29 +0900

    refactor : import static 사용하도록 수정

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,10 +1,11 @@
 package lotto.validator;
 
 import static lotto.common.ErrorMessage.INPUT_IS_NOT_BLANK;
+import static lotto.common.ErrorMessage.INPUT_MONEY_IS_DIGIT;
+import static lotto.common.ErrorMessage.LOTTO_NUMBER_RANGE;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
-import lotto.common.ErrorMessage;
 
 public class InputValidator {
     public static final String MONEY_NUMBER_REGEX = "^[1-9][0-9]*$";
@@ -18,7 +19,7 @@ public class InputValidator {
 
     public void validateMoneyFormat(String input) {
         if (!Pattern.matches(MONEY_NUMBER_REGEX, input)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_MONEY_IS_DIGIT.getMessage());
+            throw new IllegalArgumentException(INPUT_MONEY_IS_DIGIT.getMessage());
         }
     }
 
@@ -28,7 +29,7 @@ public class InputValidator {
 
     public void validateLottoNumber(String rawNumber) {
         if (!Pattern.matches(LOTTO_NUMBER_REGEX, rawNumber)) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getMessage());
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE.getMessage());
         }
     }
 }
