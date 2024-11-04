6ccfe80696b02a388f39c43343528a169f2371bd
G-Lake <note125@naver.com>
2024-11-04 21:59:41 +0900

    Fix(Application) : 당첨 번호 입력 파트 수정
    해당 입력값을 ","로 나눠 구분하는 부분에서 오류
    - split()로 받은 String을 List<Integer>로 변환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,9 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,7 +18,9 @@ public class Application {
 
     private static List<Integer> getInputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine().trim().split(",");
+        return Arrays.stream(Console.readLine().trim().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     private static int getInputBonusNumber() {
