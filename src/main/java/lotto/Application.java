3fe8a949b28ecfe994dad1a319b9f6b53e7f4767
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-04 20:27:40 +0900

    feat: Application 메인함수 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoManager LottoGame = new LottoManager();
+        LottoGame.run();
     }
 }
