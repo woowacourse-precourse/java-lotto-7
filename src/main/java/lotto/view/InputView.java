35354ed72e8645581b162542a885f364180e5ba6
young-in <hardworkingini@gmail.com>
2024-11-04 22:15:54 +0900

    feat(InputView.java) : 당첨 번호 입력을 위한 method 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.SplittingWinningNumbers;
 
 public class InputView {
     public String inputMoney(){
         return Console.readLine();
+    }
+
+    public String[] inputWinningNumbers(){
+        String winningNumbers = Console.readLine();
+        return SplittingWinningNumbers.splitWinningNumbers(winningNumbers);
     }
 }
