80df86371b2bd5fde6b425d962c40896f5a198f4
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-04 18:23:25 +0900

    refactor(ErrorMessage): INPUT_NOT_EXIST 내용 수정
    여러 곳에서 사용할 수 있게 INPUT_NOT_EXIST의 내용을 수정했습니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -5,6 +5,6 @@ public class ErrorMessage {
     public static final String DUPLICATE_LOTTO_NUMBERS = "[ERROR] 중복된 로또 번호 입니다.";
     public static final String INDIVISIBLE_NUMBER = "[ERROR] 로또 구입 금액은 1000원 단위로 입력해주세요.";
     public static final String NEGATIVE_NUMBER = "[ERROR] 로또 구입 금액은 양수로 입력해주세요.";
-    public static final String INPUT_NOT_EXIST = "[ERROR] 구입금액을 입력해 주세요.";
+    public static final String INPUT_NOT_EXIST = "[ERROR] 값을 입력해 주세요.";
     public static final String NOT_NUMBER = "[ERROR] 로또 구입 금액은 숫자로 입력해주세요.";
 }
