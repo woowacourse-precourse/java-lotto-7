7e312314b340b8d63e1b5748f39569d0f01baf33
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 19:27:15 +0900

    refact(Winning): 필요없는 변수 match 삭제

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -2,18 +2,17 @@ package lotto.domain;
 
 public enum Winning {
 
-    FIRST(6, 2000000000),
-    SECOND(7, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    NONE(0, 0);
+    FIRST( 2000000000),
+    SECOND( 30000000),
+    THIRD(1500000),
+    FOURTH( 50000),
+    FIFTH( 5000),
+    NONE( 0);
 
-    private final int match;
+
     private final int prize;
 
-    Winning(int match, int prize) {
-        this.match = match;
+    Winning( int prize) {
         this.prize = prize;
     }
 
@@ -21,12 +20,4 @@ public enum Winning {
         return prize;
     }
 
-    public Winning valueOf(int match) {
-        for (Winning winning : Winning.values()) {
-            if (match == winning.match) {
-                return winning;
-            }
-        }
-        return null;
-    }
 }
