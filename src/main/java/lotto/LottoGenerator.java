809d399ec73ee0ba28b9d88d8643544ee918f71f
bokyeong <wntjdals0412@gmail.com>
2024-11-04 17:33:07 +0900

    feat : 랜덤 로또 번호 생성

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,18 +1,25 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
-
-/**
- * 로또번호를 생성한다
- */
 
 public class LottoGenerator {
     private static final int LOTTO_NUM_SIZE = 6;
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MIN_NUMBER = 1;
 
-    public List<Integer> getWinnerNumberAndBonusNumber() {
+    public List<Lotto> lottoGenerator(int lottoAmount) {
+        List<Lotto> generatedLotto = new ArrayList<>();
+
+        for (int i = 0; i < lottoAmount; i++) {
+            List<Integer> generatedNumbers = generateNumbers();
+            generatedLotto.add(new Lotto(generatedNumbers));
+        }
+
+        return generatedLotto;
+    }
+    public List<Integer> generateNumbers() {
         return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUM_SIZE);
     }
 }
