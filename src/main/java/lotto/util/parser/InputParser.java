abbd0d977d1a2fa722b18952e2a897564e6179ba
coolman0123 <socool4m@naver.com>
2024-11-04 16:24:35 +0900

    feat: 구입금액 integer 형변환해라

---
+++ src/main/java/lotto/util/parser/InputParser.java
@@ -0,0 +1,11 @@
+package lotto.util.parser;
+
+public class InputParser {
+
+    public int convertPurchaseAmountToInt(String input) {
+        int purchaseAmount = Integer.parseInt(input);
+
+        return purchaseAmount;
+    }
+
+}
