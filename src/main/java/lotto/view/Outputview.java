bf85e141b8427e8e5064c1924f79fff8ceb2fc0c
YunCheol1 <naro2727@naver.com>
2024-11-04 13:41:19 +0900

    feat(Outputview): 로또 결과를 출력하는 클래스 생성 - 로또 티켓 구매 수량 및 번호 출력 기능 추가 - 당첨 통계 및 각 등수별 당첨 개수 출력 기능 구현 - 총 수익률 출력 기능 구현

--- src/main/java/lotto/view/Outputview.java
+++ src/main/java/lotto/view/Outputview.java
@@ -1,5 +1,45 @@
 package lotto.view;
 
-public class Outputview {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.Rank;
 
+public class Outputview {
+    public static void printLottoTickets(List<Lotto> lottoTickets) {
+        System.out.printf("%d개를 구매했습니다.%n", lottoTickets.size());
+        for (Lotto lotto : lottoTickets) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void printResults(List<Rank> results) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        int[] rankCounts = new int[Rank.values().length];
+
+        for (Rank rank : results) {
+            rankCounts[rank.ordinal()]++;
+        }
+
+        // Rank.values()를 List로 변환한 뒤 역순으로 정렬
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        Collections.reverse(ranks);
+
+        // 역순으로 정렬된 ranks 리스트 사용
+        for (Rank rank : ranks) {
+            if (rank == Rank.NONE) {
+                continue;
+            }
+            String bonusInfo = rank == Rank.SECOND ? ", 보너스 볼 일치" : "";
+            String prizeFormatted = String.format("%,d", rank.getPrize()); // 천 단위 쉼표 추가
+            System.out.printf("%d개 일치%s (%s원) - %d개%n",
+                    rank.getMatchCount(), bonusInfo, prizeFormatted, rankCounts[rank.ordinal()]);
+        }
+    }
+
+    public static void printYield(double yield) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
+    }
 }
