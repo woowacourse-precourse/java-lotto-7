6f7f65dae78c798b659e2b0d324f23c0634d1294
cjh-19 <gns8812@naver.com>
2024-11-04 20:29:19 +0900

    feat(test): Lotto 클래스 단위 테스트 추가
    - 로또 오름차순 기능 테스트
    - 로또 번호와 당첨 번호 일치하는 숫자 개수 반환 메소드 테스트

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,5 +23,33 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 오름차순으로 정렬되는지 확인한다.")
+    @Test
+    void 로또_번호가_오름차순으로_정렬되는지_확인한다() {
+        Lotto lotto = new Lotto(List.of(42, 8, 23, 21, 43, 41));
+
+        assertThat(lotto.getNumbers()).containsExactly(8, 21, 23, 41, 42, 43);
+    }
+
+    @DisplayName("로또 번호와 당첨 번호 간의 일치하는 숫자 개수를 반환한다.")
+    @Test
+    void 로또_번호와_당첨_번호_간의_일치하는_숫자_개수를_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+
+        int matchCount = lotto.matchCount(winningNumber);
+
+        assertThat(matchCount).isEqualTo(3);
+    }
+
+    @DisplayName("모든 번호가 일치하지 않는 경우 0을 반환해야 한다.")
+    @Test
+    void 모든_번호가_일치하지_않는_경우_0을_반환해야_한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumber = new Lotto(List.of(13, 14, 15, 10, 11, 12));
+
+        int matchCount = lotto.matchCount(winningNumber);
+
+        assertThat(matchCount).isEqualTo(0);
+    }
 }
