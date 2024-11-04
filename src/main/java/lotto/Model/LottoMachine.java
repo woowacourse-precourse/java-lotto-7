32f99fbdc8271c0410b33357c3416f08759a77a0
seunghui-park <ridia1233@naver.com>
2024-11-04 21:59:34 +0900

    feat(LottoMachine): 로또 번호 자동 생성 기능 추가
    - 구매 금액에 따라 로또 개수 생성
    - 1부터 45까지의 숫자 중 6개의 고유한 숫자 선택
    - 로또 당 가격을 1000원으로 설정

--- src/main/java/lotto/Model/LottoMachine.java
+++ src/main/java/lotto/Model/LottoMachine.java
@@ -1,5 +1,25 @@
 package lotto.Model;
 
-public class LottoMachine {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    public static List<Lotto> generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+        return lottos;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER,LOTTO_SIZE));
+    }
 }
