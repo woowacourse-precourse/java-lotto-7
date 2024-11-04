edb3883ebfdc44ea818952c5b208d86637e3cfa5
nsb329 <nsb329@naver.com>
2024-11-04 16:55:47 +0900

    구입 금액이 1000원 단위 또는 문자가 들어왔을 경우 예외 처리하는 메소드

--- src/main/java/lotto/input.java
+++ src/main/java/lotto/input.java
@@ -8,11 +8,40 @@ public class input {
 		String put_money = Console.readLine();
 		int count = 0;
 		try {
+			paymentEx2(put_money);
 			int money = Integer.parseInt(put_money);
+			count = try_number(money);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			put_money();
 		}
 		return count;
+	}
+
+	public int try_number(int money) {
+		int count = money / 1000;
+		int trash = money % 1000;
+		try {
+			paymentEx(trash, money);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			put_money();
+		}
+		return count;
+	}
+
+	public void paymentEx(int trash, int money) {
+		if (trash != 0) {
+			throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+		}
+	}
+
+	public void paymentEx2(String input) {
+		for (char ch : input.toCharArray()) {
+			int asciiValue = (int) ch;
+			if (asciiValue < 48 || asciiValue > 57) {
+				throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+			}
+		}
 	}
 }
