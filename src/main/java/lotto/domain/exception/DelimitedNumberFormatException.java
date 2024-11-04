074cc1624b11b8f792bd5f25692d6a41e5a40688
moonbinchoi <whsahd12@gmail.com>
2024-11-04 18:45:37 +0900

    refactor(DelimitedNumberFormatException): 중복되는 예외 삭제

--- src/main/java/lotto/domain/exception/DelimitedNumberFormatException.java
+++ src/main/java/lotto/domain/exception/DelimitedNumberFormatException.java
@@ -10,15 +10,8 @@ public class DelimitedNumberFormatException extends NumberFormatException {
         super(message);
     }
 
-    public static DelimitedNumberFormatException invalidCount() {
-        return new DelimitedNumberFormatException("당첨 번호는 정확히 6개여야 합니다.");
-    }
-
     public static DelimitedNumberFormatException invalidNumber() {
         return new DelimitedNumberFormatException("당첨 번호는 숫자와 구분자로만 이루어져야 합니다.");
     }
 
-    public static DelimitedNumberFormatException outOfRange() {
-        return new DelimitedNumberFormatException("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
-    }
 }
