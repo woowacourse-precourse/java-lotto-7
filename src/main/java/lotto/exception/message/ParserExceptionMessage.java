37db2087a79480a7d123bdd146aa8d85044aecd8
kkmdevel <kkm.devel@gmail.com>
2024-11-04 17:23:12 +0900

    style(ParserExceptionMessage): 상수 메시지에 마침표 추가
    - ParserExceptionMessage의 NOT_NUMBER 메시지에 마침표(.) 추가

--- src/main/java/lotto/exception/message/ParserExceptionMessage.java
+++ src/main/java/lotto/exception/message/ParserExceptionMessage.java
@@ -2,7 +2,7 @@ package lotto.exception.message;
 
 public enum ParserExceptionMessage{
     EMPTY_INPUT("입력되지 않았습니다."),
-    NOT_NUMBER("숫자를 입력해주세요");
+    NOT_NUMBER("숫자를 입력해주세요.");
     private final String message;
 
     ParserExceptionMessage(String message) {
