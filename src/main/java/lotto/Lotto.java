2130b2023514d1b49a7a473fd129b6cf7b86f0a0
moeun9309 <moeun9309@gmail.com>
2024-11-04 09:50:20 +0900

    refactor: input manager and new parser

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
@@ -29,26 +28,60 @@ public class Lotto {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
-    private int purchaseCalculate(int money) {
+    private int countCalculator(String input) {
+        int money = integerParser(input);
+
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원으로 나누어 떨어져야 합니다.");
         }
+
         return money / 1000;
     }
 
-    public void inputManager() {
-        String input = Console.readLine();
+    private int integerParser(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 있습니다.");
+        }
+    }
+
+    public void moneyInput() {
+        printGuide("구입금액을 입력해 주세요.");
+        int cnt = countCalculator(Console.readLine());
+
+    }
+
+    public void numbersInput() {
+        printGuide("당첨 번호를 입력해 주세요.");
+        saveNumbers(Console.readLine());
+    }
+
+    public void bonusInput() {
+        printGuide("보너스 번호를 입력해 주세요.");
+        saveNumbers(Console.readLine());
+
+    }
+
+    private void saveNumbers(String input) {
         String[] numbersString = input.split(",");
-        List<Integer> newNumbers = new ArrayList<Integer>();
+        List<Integer> winningNumbers = new ArrayList<>();
 
         for (String number : numbersString) {
-            try {
-                newNumbers.add(Integer.parseInt(number.trim()));
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 있습니다.");
-            }
+            int numberInt = integerParser(number);
+            winningNumbers.add(numberInt);
         }
 
-        numbers.addAll(newNumbers);
+        validate(winningNumbers);
+        numbers.addAll(winningNumbers);
+    }
+
+    public void printNewLotto(int cnt) {
+        printGuide(cnt + "개를 구매했습니다.");
+
+    }
+
+    public void printGuide(String message) {
+        System.out.println(message);
     }
 }
