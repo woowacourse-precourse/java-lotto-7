5d0f3237e08624e12ec0a0c15655c9a4537bbba2
EP45 <5510816+EP45@users.noreply.github.com>
2024-11-04 20:03:46 +0900

    feat(Money): wrap money to avoid using primitives
    It's a wrapper for money. It has following abilities:
    - Set amount with integer string
    - Return current amount

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,28 @@
+package lotto;
+
+public class Money {
+    private final int amount;
+
+    public Money(String amount) {
+        this.amount = parseAmount(amount);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    private int parseAmount(String amountRaw) {
+        int amount;
+
+        if (amountRaw == null || amountRaw.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 비거나 null이 될 수 없습니다.");
+        }
+        try {
+            amount = Integer.parseInt(amountRaw);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 양의 정수여야 합니다.");
+        }
+
+        return amount;
+    }
+}
