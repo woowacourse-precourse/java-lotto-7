997bd2510195ff19b8c3038312bc6259c957ca58
u <mjkim2304@gmail.com>
2024-11-04 20:16:14 +0900

    feat: 사용자의 입력값을 받는 기능

--- src/main/java/lotto/InputReader.java
+++ src/main/java/lotto/InputReader.java
@@ -2,33 +2,36 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 
 public class InputReader {
 
-    public void readInput() {
+    private final InputValidator inputValidator;
+
+    public InputReader(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public int readAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        int givenAmount = Integer.parseInt(input);
+        System.out.println();
+        return inputValidator.validateAmount(input);
     }
 
-    private String checkNotEmpty(String input) {
-        input = input.trim();
-        if (input.isEmpty() || input == null) {
-            throw new IllegalArgumentException("값을 입력해 주세요.");
-        }
-        return input;
+    public List<Integer> readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println();
+        return inputValidator.validateWinningNumbers(input);
     }
 
-    private void validatePositiveAmount(int amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("구입 금액은 0 이상 이어야 합니다.");
-        }
+    public int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return inputValidator.validateBonusNumber(input);
     }
 
-    private void validateAmountInThousands(int amount) {
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("구입 금액은 1000원 단위여야 합니다.");
-        }
-    }
 
 }
