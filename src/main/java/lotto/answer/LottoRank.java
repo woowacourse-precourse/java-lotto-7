941e66a461df235967d91d2ce818bc64eb64480c
minjun <ijmtylhtf@naver.com>
2024-11-04 09:53:45 +0900

    refactor : 출력 메시지 변수로 추가

--- src/main/java/lotto/answer/LottoRank.java
+++ src/main/java/lotto/answer/LottoRank.java
@@ -1,31 +1,31 @@
 package lotto.answer;
 
 public enum LottoRank {
-	FIFTH(3, false, 5000) {
+	FIFTH(3, false, 5000, "3개 일치 (5,000원) - ") {
 		@Override
 		public void printInfo() {
 			System.out.print("3개 일치 (5,000원) - ");
 		}
 	},
-	FOURTH(4, false, 50000) {
+	FOURTH(4, false, 50000, "4개 일치 (50,000원) - ") {
 		@Override
 		public void printInfo() {
 			System.out.print("4개 일치 (50,000원) - ");
 		}
 	},
-	THIRD(5, false, 1500000) {
+	THIRD(5, false, 1500000, "5개 일치 (1,500,000원) - ") {
 		@Override
 		public void printInfo() {
 			System.out.print("5개 일치 (1,500,000원) - ");
 		}
 	},
-	SECOND(5, true, 30000000) {
+	SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - ") {
 		@Override
 		public void printInfo() {
 			System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
 		}
 	},
-	FIRST(6, false, 2000000000) {
+	FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원) - ") {
 		@Override
 		public void printInfo() {
 			System.out.print("6개 일치 (2,000,000,000원) - ");
@@ -38,11 +38,13 @@ public enum LottoRank {
 	private final int matchCount;
 	private final boolean bonusMatch;
 	private final int prizeAmount;
+	private final String message;
 
-	LottoRank(int matchCount, boolean bonusMatch, int prizeAmount) {
+	LottoRank(int matchCount, boolean bonusMatch, int prizeAmount, String message) {
 		this.matchCount = matchCount;
 		this.bonusMatch = bonusMatch;
 		this.prizeAmount = prizeAmount;
+		this.message = message;
 	}
 
 	public static LottoRank findRankByMatch(int matchCount, boolean bonusMatch) {
