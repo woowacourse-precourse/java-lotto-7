5de01696c1a7fe0976d59504e9f5f4080831ab4f
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-04 19:50:46 +0900

    Feat: LottoGenerator 클래스 생성

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,4 +1,16 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.common.Limit;
+
+import java.util.List;
+
 public class LottoGenerator {
+    public Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Limit.LOTTO_MIN_NUMBER, Limit.LOTTO_MAX_NUMBER, Limit.LOTTO_NUMBER_COUNT)
+                .stream()
+                .sorted()
+                .toList();
+        return new Lotto(numbers);
+    }
 }
