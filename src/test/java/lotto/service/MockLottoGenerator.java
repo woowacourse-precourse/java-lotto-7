062aca3d454a32ec05004f55465911bbbdbddfdc
SeongJunChun <csj991210@naver.com>
2024-11-04 17:03:07 +0900

    feat: 사용자가 지정한 로또 생성 기능 구현

---
+++ src/test/java/lotto/service/MockLottoGenerator.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import lotto.domain.Lotto;
+
+public class MockLottoGenerator implements Generator<Lotto> {
+
+    private final Queue<Lotto> lottos;
+
+    public MockLottoGenerator(List<Lotto> lottos) {
+        this.lottos = new LinkedList<Lotto>();
+        this.lottos.addAll(lottos);
+    }
+
+    @Override
+    public Lotto generate() {
+        validateEmpty();
+        return lottos.poll();
+    }
+
+    private void validateEmpty() {
+        if (lottos.isEmpty()) {
+            throw new IllegalStateException();
+        }
+    }
+
+}
