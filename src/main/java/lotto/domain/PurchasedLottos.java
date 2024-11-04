8e01854984b0c3b874630a1c7776040e983cb8ad
haebyun <bhk4392@gmail.com>
2024-11-04 12:34:04 +0900

    feat(PurchasedLottos): 당첨된 등수에 따른 로또 개수 계산 기능 추가
    - calculateRankCounts 메서드 추가: WinningLotto와 비교하여 각 LottoRank별 당첨 개수 계산
    - EnumMap을 사용해 등급별 로또 개수 저장
    - getOrDefault를 활용하여 초기값 0에서 증가시키는 방식으로 개수 누적

--- src/main/java/lotto/domain/PurchasedLottos.java
+++ src/main/java/lotto/domain/PurchasedLottos.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import lotto.domain.vo.LottoRank;
 
 public class PurchasedLottos {
     private final List<Lotto> lottos;
@@ -16,5 +18,17 @@ public class PurchasedLottos {
 
     public List<Lotto> getLottosAsUnmodifiableList() {
         return Collections.unmodifiableList(lottos); // unmodifiableList 반환
+    }
+
+    public EnumMap<LottoRank, Integer> calculateRankCounts(WinningLotto winningLotto) {
+        EnumMap<LottoRank, Integer> rankCounts = new EnumMap<>(LottoRank.class);
+        lottos.forEach(lotto -> {
+            LottoRank rank = LottoRank.of(
+                    winningLotto.getCountMatchedNumber(lotto),
+                    winningLotto.checkBonusNumberMatched(lotto)
+            );
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        });
+        return rankCounts;
     }
 }
