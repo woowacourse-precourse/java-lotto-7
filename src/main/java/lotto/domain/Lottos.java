7989b0ec1289ef4120f1f73b3f20c6a1db286b63
DongHoon Lee <dhl1924@naver.com>
2024-11-04 23:03:35 +0900

    feat(Lottos): 빈 티켓 목록을 가질 수 없도록 유효성 처리 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,15 +1,24 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
 
 public class Lottos {
     private final List<Lotto> tickets;
 
     public Lottos(List<Lotto> tickets) {
+        validate(tickets);
         this.tickets = tickets;
     }
 
     public List<Lotto> getTickets() {
         return tickets;
+    }
+
+    private void validate(List<Lotto> tickets) {
+        if (tickets == null || tickets.isEmpty()) {
+            throw LottoException.from(ErrorMessage.EMPTY_LOTTO_LIST_ERROR);
+        }
     }
 }
