6cf95913dba3fea2aeebe5e14d2067c6dd945f71
heeyeop Kim <ssucgoo@gmail.com>
2024-11-04 23:12:22 +0900

    feat: 로또 숫자를 입력받는 기능 구현

--- src/main/java/lotto/InputDevice.java
+++ src/main/java/lotto/InputDevice.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputDevice {
     public int receivePrice() {
         String input = Console.readLine();
@@ -11,6 +14,19 @@ public class InputDevice {
             return price;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 가격 형식입니다.");
+        }
+    }
+
+    public List<Integer> receiveNumbers() {
+        String input = Console.readLine();
+        String[] splitInput = input.split(",");
+
+        try {
+            return Arrays.stream(splitInput)
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 (,) 구분자만 입력해주세요.");
         }
     }
 
