9ba4ab76477be878f11eae140750037860de869e
CheonMinJae <cmjcmj1011@gmail.com>
2024-11-04 23:22:14 +0900

    feat: Error class 를 통해 execption 관리

--- src/main/java/lotto/config/LottoConfig.java
+++ src/main/java/lotto/config/LottoConfig.java
@@ -3,4 +3,9 @@ package lotto.config;
 public final class LottoConfig {
     public static final int PRICE = 1000;
     public static final int NUMBER_COUNT = 6;
+    public static final String ERROR_MESSAGE = "[ERROR]";
+
+    public static String errorMessage(String message) {
+        return ERROR_MESSAGE + " " + message;
+    }
 }
