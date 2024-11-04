a80783393a8589bc4b6f429fd67be5bf15efcb37
choign3123 <choign3123@naver.com>
2024-11-04 10:04:01 +0900

    update(LottoGame): 등수 결정 로직 변경
    변경 이유:
    우테코 요구사항 중
    - switch-catch문 사용 금지
    - indent depth 2 이하
    를 지키기 위하여 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -41,21 +41,20 @@ public class Lotto {
     }
 
     private LottoRank determineRank(int numOfMatch, int bonus) {
-        switch (numOfMatch) { // TODO: map에 당첨 개수별 등수를 저장해두어 리턴하면 라인수 줄일 수 있음
-            case 6:
-                return LottoRank.GRADE_1TH;
-            case 5:
-                if (numbers.contains(bonus)) {
-                    return LottoRank.GRADE_2TH;
-                }
-                return LottoRank.GRADE_3TH;
-            case 4:
-                return LottoRank.GRADE_4TH;
-            case 3:
-                return LottoRank.GRADE_5TH;
-            default:
-                return LottoRank.NONE;
+        if (numOfMatch == 6) {
+            return LottoRank.GRADE_1TH;
+        } else if (numOfMatch == 5) {
+            if (numbers.contains(bonus)) {
+                return LottoRank.GRADE_2TH;
+            }
+            return LottoRank.GRADE_3TH;
+        } else if (numOfMatch == 4) {
+            return LottoRank.GRADE_4TH;
+        } else if (numOfMatch == 3) {
+            return LottoRank.GRADE_5TH;
         }
+
+        return LottoRank.NONE;
     }
 
     public List<Integer> getNumbers() {
