027886e9aa2a95e0f65d89620e3a6c11004b8fea
JEN <ekffuclxk@gmail.com>
2024-11-04 13:54:18 +0900

    feat(LottoGenerator): 로또들 발행 메서드 추가

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.List;
+import lotto.Lotto;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+
+}
+
--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -4,10 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
+import lotto.model.Lottos;
 
 public class LottoGenerator {
     public Lotto generateLotto() {
         List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(lotto);
+    }
+
+    public Lottos getnerateLottos(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        int count = amount / 1000;
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateLotto());
+        }
+        return new Lottos(lottos);
     }
 }
