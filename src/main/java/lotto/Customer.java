d7bcf3922da85a3f8e7d197a61354c7ff1787253
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 21:43:26 +0900

    refactor(Customer): 필요없는 검증 코드 삭제한다
    입력을 받을 때, 시행하는 값의 검증하는 중복된 부분을 삭제한다.

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -9,16 +9,7 @@ public class Customer {
     }
 
     private void calculateLottoCount(String budget) {
-        int totalBudget = 0;
-        try {
-            totalBudget = Integer.parseInt(budget);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("ERROR");
-        }
-        // 입력하는 값이 숫자인지도 검증해야됨
-        if (totalBudget % 1000 > 0) {
-            throw new IllegalArgumentException("[ERROR] 입력하는 돈이 1000으로 나누어 떨어져야합니다.");
-        }
+        int totalBudget = Integer.parseInt(budget);
         lottoCount = totalBudget / 1000;
         this.budget = totalBudget;
     }
