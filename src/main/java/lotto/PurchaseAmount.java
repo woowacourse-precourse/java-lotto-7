b9863e436c38666b148db4735fb3a5f9ba98da9a
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 09:51:13 +0900

    refactor(PurchaseAmount): 구입 금액 양수 미입력 예외 처리 로직 분리
    - validatePurchaseAmount 메서드에서 예외를 던지는 부분을 validatePositive 메서드로 분리
    - validatePositive 메서드에서 사용되는 조건을 isPositive 메서드로 분리

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -10,9 +10,7 @@ public record PurchaseAmount(int amount) {
     private static int validatePurchaseAmount(String input) {
         validateLetter(input);
         int purchaseAmount = parseInt(input);
-        if (purchaseAmount < 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 양수인 숫자여야 합니다.");
-        }
+        validatePositive(purchaseAmount);
         return purchaseAmount;
     }
 
@@ -22,8 +20,18 @@ public record PurchaseAmount(int amount) {
         }
     }
 
+    private static void validatePositive(int purchaseAmount) {
+        if (!isPositive(purchaseAmount)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 양수인 숫자여야 합니다.");
+        }
+    }
+
     private static boolean isLetter(String value) {
         return !value.matches(SIGNED_NUMBER_REGEX);
+    }
+
+    private static boolean isPositive(int purchaseAmount) {
+        return purchaseAmount > 0;
     }
 
     private static int parseInt(String amount) {
