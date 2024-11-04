7fce092b7a384e5f71a735e0877c5371a546413e
wooyoungKwon <rnjsdndud00@naver.com>
2024-11-04 10:18:19 +0900

    feat(lotto): 1000원 단위가 아닐 경우에 대한 예외 추가

--- src/test/java/lotto/LottoShopTest.java
+++ src/test/java/lotto/LottoShopTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.lotto.Lotto;
@@ -27,6 +28,12 @@ public class LottoShopTest {
     @DisplayName("입력한 금액이 1,000원 단위가 아니라면 예외가 발생한다")
     @Test
     void 입력한_금액이_1000원_단위가_아니라면_예외가_발생한다() {
+        // given
+        LottoShop lottoShop = new LottoShop();
+        Long money = 1234L;
 
+        // when & then
+        assertThatThrownBy(() -> lottoShop.buyLottos(money))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
