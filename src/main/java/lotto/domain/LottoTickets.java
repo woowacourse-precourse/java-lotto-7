784aa594eaa51f4d15459e9b1ea2df65bc9a2e56
wlsdlf2 <wlsdlfzg@naver.com>
2024-11-04 20:05:28 +0900

    refactor: 당첨 통계 계산하는 메서드 추가

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -2,16 +2,43 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import lotto.util.Ranking;
 
 public class LottoTickets {
+    private static final int WINNING_COUNT_INCREMENT = 1;
+    private static final int DEFAULT_WINNING_COUNT = 0;
+
     private List<Lotto> lottos;
 
     public LottoTickets(int purchaseAmount) {
         lottos = new ArrayList<>();
         generateLottoTickets(purchaseAmount);
     }
+
+    public String getLottoTicket() {
+        StringBuilder lottoTicket = new StringBuilder();
+
+        for (Lotto lotto : lottos) {
+            lottoTicket.append(lotto.getNumbers().toString())
+                    .append("\n");
+        }
+        return lottoTicket.toString().trim();
+    }
+
+    public Map<Ranking, Integer> calculateWinningStatistics(List<Integer> winningNumbers, int bonusNumber) {
+        Map<Ranking, Integer> winningStatistics = new EnumMap<>(Ranking.class);
+
+        for (Lotto lotto : lottos) {
+            Ranking rank = lotto.calculateRank(winningNumbers, bonusNumber);
+            winningStatistics.put(rank, winningStatistics.getOrDefault(rank, DEFAULT_WINNING_COUNT) + WINNING_COUNT_INCREMENT);
+        }
+
+        return winningStatistics;
+    }
+
 
     private void generateLottoTickets(int ticketCount) {
         for (int i = 0; i < ticketCount; i++) {
