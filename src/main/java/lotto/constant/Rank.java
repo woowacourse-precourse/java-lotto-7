3de48690cda84354a5538bc2ed7911045aaacfd9
lucku <lsw6731@naver.com>
2024-11-04 23:18:30 +0900

    refactor: getRank 메서드 조건문으로 변경하여 가독성 개선

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -1,7 +1,5 @@
 package lotto.constant;
 
-import java.util.Map;
-
 public enum Rank {
     THREE_MATCH(3, 5000),
     FOUR_MATCH(4, 50000),
@@ -9,21 +7,6 @@ public enum Rank {
     FIVE_MATCH_BONUS(5, 30000000),
     SIX_MATCH(6, 2000000000),
     NO_MATCH(0, 0);
-
-    private static final String MATCH_BONUS_SEPARATOR = "-";
-    private static final String SIX_MATCH_KEY = "6";
-    private static final String FIVE_MATCH_BONUS_KEY = "5" + MATCH_BONUS_SEPARATOR + "true";
-    private static final String FIVE_MATCH_KEY = "5" + MATCH_BONUS_SEPARATOR + "false";
-    private static final String FOUR_MATCH_KEY = "4";
-    private static final String THREE_MATCH_KEY = "3";
-
-    private static final Map<String, Rank> rankMap = Map.of(
-            SIX_MATCH_KEY, SIX_MATCH,
-            FIVE_MATCH_BONUS_KEY, FIVE_MATCH_BONUS,
-            FIVE_MATCH_KEY, FIVE_MATCH,
-            FOUR_MATCH_KEY, FOUR_MATCH,
-            THREE_MATCH_KEY, THREE_MATCH
-    );
 
     private final int matchCount;
     private final long winnings;
@@ -34,10 +17,22 @@ public enum Rank {
     }
 
     public static Rank getRank(int matchCount, boolean hasBonus) {
-        String key = matchCount == FIVE_MATCH.getMatchCount()
-                ? matchCount + MATCH_BONUS_SEPARATOR + hasBonus
-                : Integer.toString(matchCount);
-        return rankMap.getOrDefault(key, NO_MATCH);
+        if (matchCount == SIX_MATCH.getMatchCount()) {
+            return SIX_MATCH;
+        }
+        if (matchCount == FIVE_MATCH_BONUS.getMatchCount() && hasBonus) {
+            return FIVE_MATCH_BONUS;
+        }
+        if (matchCount == FIVE_MATCH.getMatchCount()) {
+            return FIVE_MATCH;
+        }
+        if (matchCount == FOUR_MATCH.getMatchCount()) {
+            return FOUR_MATCH;
+        }
+        if (matchCount == THREE_MATCH.getMatchCount()) {
+            return THREE_MATCH;
+        }
+        return NO_MATCH;
     }
 
     public int getMatchCount() {
