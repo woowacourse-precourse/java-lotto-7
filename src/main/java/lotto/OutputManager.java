3eda62d574983534293189e53c7eba9555bef60f
문혜교 <3hilney@gmail.com>
2024-11-04 16:39:26 +0900

    feat: 로또 구입 수량 출력

---
+++ src/main/java/lotto/OutputManager.java
@@ -0,0 +1,10 @@
+package lotto;
+
+
+public class OutputManager {
+
+    public static void printLottoCount(int count) {
+        System.out.printf((InputMessage.LOTTO_COUNT.getInputMessage()) + "%n", count);
+    }
+
+}
