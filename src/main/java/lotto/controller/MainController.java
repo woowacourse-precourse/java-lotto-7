d06365ee6f9c1b5dc6699dd3baccfa68e24b458e
threepebbles <threepebbles@naver.com>
2024-11-04 17:06:06 +0900

    refactor(MainController): 공백 및 주석 제거

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -19,24 +19,16 @@ public class MainController {
     }
 
     public void run() {
-        // 구입금액 입력
         Integer price = inputView.scanPrice();
-
-        // 구매내역 출력
         LottoMachine lottoMachine = new LottoMachine(new CustomLottoGenerator());
         List<Lotto> purchasedLottos = lottoMachine.generateLottos(price);
         outputView.printPurchasedLottoList(purchasedLottos.stream()
                 .map(lotto -> new LottoDto(lotto.getNumbers()))
                 .toList());
 
-        // 당첨 번호 입력
         Lotto winningLotto = new Lotto(inputView.scanWinningLotto()
                 .getNumbers());
-
-        // 보너스 번호 입력
         Integer bonusNumber = inputView.scanBonusNumber(new LottoDto(winningLotto.getNumbers()));
-
-        // 당첨 통계 출력
         outputView.printLottoStatistics(
                 LottoStatistics.createLottoStatisticsDto(
                         LottoStatistics.calcStatistics(purchasedLottos, winningLotto, bonusNumber))
