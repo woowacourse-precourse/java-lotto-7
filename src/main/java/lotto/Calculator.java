55bdb616858b665d8bc8edf6f2a6bb020c6f0785
Leegeonwoo7 <workgeon07@naver.com>
2024-11-04 19:57:39 +0900

    feat(Calculator): Calculator 구현
    수익률을 구하는 객체

--- src/main/java/lotto/Calculator.java
+++ src/main/java/lotto/Calculator.java
@@ -1,2 +1,11 @@
-package lotto;public class Calculator {
+package lotto;
+
+public class Calculator {
+
+    public static double calculateYield(Long totalPrizeMoney, Money money) {
+        Integer amount = money.getAmount();
+
+        double yield = (double) totalPrizeMoney / (double) amount * 100;
+        return Math.round(yield * 100) / 100.0;
+    }
 }
