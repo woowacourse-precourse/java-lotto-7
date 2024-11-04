e70685f52dd931e89df579020e0b9aa85426d136
Hyejeong <thswjd0609@gmail.com>
2024-11-04 21:23:09 +0900

    refactor: Rank Description 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -25,11 +25,11 @@ public class OutputView {
 
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d개\n", lottoStats.getOrDefault(Rank.FIFTH, 0));
-        System.out.printf("4개 일치 (50,000원) - %d개\n", lottoStats.getOrDefault(Rank.FOURTH, 0));
-        System.out.printf("5개 일치 (1,500,000원) - %d개\n", lottoStats.getOrDefault(Rank.THIRD, 0));
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", lottoStats.getOrDefault(Rank.SECOND, 0));
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", lottoStats.getOrDefault(Rank.FIRST, 0));
+        System.out.printf("%s - %d개\n", Rank.FIFTH.getDescription(), lottoStats.getOrDefault(Rank.FIFTH, 0));
+        System.out.printf("%s - %d개\n", Rank.FOURTH.getDescription(), lottoStats.getOrDefault(Rank.FOURTH, 0));
+        System.out.printf("%s - %d개\n", Rank.THIRD.getDescription(), lottoStats.getOrDefault(Rank.THIRD, 0));
+        System.out.printf("%s - %d개\n", Rank.SECOND.getDescription(), lottoStats.getOrDefault(Rank.SECOND, 0));
+        System.out.printf("%s - %d개\n", Rank.FIRST.getDescription(), lottoStats.getOrDefault(Rank.FIRST, 0));
 
         System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
     }
