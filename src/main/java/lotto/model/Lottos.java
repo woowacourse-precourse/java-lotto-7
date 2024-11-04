6c81e6361d8935d9184e67c81f195f730b7674ab
Su-daa <subin.kk019@gmail.com>
2024-11-04 12:00:40 +0900

    feat: 등수별 당첨 갯수 집계 메서드 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private List<Lotto> lottos;
@@ -11,5 +13,11 @@ public class Lottos {
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+
+    public Map<Rank, Long> countMatching(WinningLotto winningLotto) {
+        return lottos.stream()
+                .map(winningLotto::findRank)
+                .collect(Collectors.groupingBy(match -> match, Collectors.counting()));
     }
 }
