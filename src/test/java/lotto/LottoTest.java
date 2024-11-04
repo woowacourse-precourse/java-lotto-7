d57e0e1684b4fd8fca556815d37c73c6f64aa240
Avocado <ujubongbong@gmail.com>
2024-11-04 20:21:56 +0900

    :white_check_mark: test: 로또 번호에 보너스 번호가 포함되어 있는지 검증 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
+    Lotto lotto;
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +29,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호에 보너스 번호가 포함되어 있는지 검증 테스트")
+    void 로또_번호에_보너스_번호가_포함되어_있는지_검증하는_테스트() {
+        List<Integer> lottos = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 1;
+        assertEquals(lotto.isBonusMatch(lottos, bonusNumber), true);
+    }
 }
