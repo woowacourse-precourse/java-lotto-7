8a2e8fe42aad0079e44b1234cfa573af1faeee33
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-04 14:30:11 +0900

    feat: Validator를 상속 받는 클래스로 변경
    - exception 로직 throwInvalidInputException으로 대체
    - 모드 String 필드 "[error]" 제거

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -4,22 +4,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 정적(static) 클래스, 인스턴스화 불가능 유용성 검사를 위해서만 사용됨
- */
-public class LottoValidator {
+
+public class LottoValidator extends Validator {
     static public final int NUMBERS_LENGTH = 6;
     static public final int LOTTO_MIN_NUMBER = 1;
     static public final int LOTTO_MAX_NUMBER = 45;
 
-    static final String ERROR_INVALID_LENGTH = "[ERROR] 로또 번호는 6개여야 합니다.";
-    static final String ERROR_DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복될 수 없습니다.";
-    static final String ERROR_INVALID_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    static final String ERROR_INVALID_LENGTH = "로또 번호는 6개여야 합니다.";
+    static final String ERROR_DUPLICATE_NUMBER = "로또 번호는 중복될 수 없습니다.";
+    static final String ERROR_INVALID_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
 
-    /**
-     * 정적(static) 클래스 인스턴스화 불가능 유용성 검사를 위해서만 사용됨
-     */
     private LottoValidator() {
         // 인스턴스화 방지를 위해서 사용
     }
@@ -34,7 +29,7 @@ public class LottoValidator {
     static void checkLength(List<Integer> numbers) {
 
         if (numbers.size() != NUMBERS_LENGTH) {
-            throw new IllegalArgumentException(ERROR_INVALID_LENGTH);
+            throwInvalidInputException(ERROR_INVALID_LENGTH);
         }
     }
 
@@ -42,7 +37,7 @@ public class LottoValidator {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
 
         if (uniqueNumbers.size() < NUMBERS_LENGTH) {
-            throw new IllegalArgumentException(ERROR_DUPLICATE_NUMBER);
+            throwInvalidInputException(ERROR_DUPLICATE_NUMBER);
         }
     }
 
@@ -50,7 +45,7 @@ public class LottoValidator {
 
         for (Integer i : numbers) {
             if (i < LOTTO_MIN_NUMBER || i > LOTTO_MAX_NUMBER) {
-                throw new IllegalArgumentException(ERROR_INVALID_RANGE);
+                throwInvalidInputException(ERROR_INVALID_RANGE);
             }
         }
 
