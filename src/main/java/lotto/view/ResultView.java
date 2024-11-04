efe9f068f44aa8670a365411842c02c65995af03
YujinJuhn <holsya@naver.com>
2024-11-04 20:34:32 +0900

    feat: 모든 출력 관련 메서드 추가

---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,48 @@
+package lotto.view;
+
+import lotto.ConsoleMessage;
+import lotto.Lotto;
+import lotto.Prize;
+
+import java.util.List;
+
+public class ResultView {
+    public static void displayRequestPurchaseAmount(){
+        System.out.println(ConsoleMessage.REQ_PURCHASE_AMOUNT);
+    }
+
+    public static void displayRequestWinningNumbers(){
+        System.out.println(ConsoleMessage.REQ_WINNING_NUMBERS);
+    }
+
+    public static void displayRequestBonusNumber(){
+        System.out.println(ConsoleMessage.REQ_BONUS_NUMBERS);
+    }
+
+    public static void displayPurchasedLottoCount(int lottoCount){
+        System.out.printf(ConsoleMessage.ACK_LOTTO_COUNT, lottoCount);
+    }
+
+    public static void displayMyLottos(List<Lotto> myLottos){
+        myLottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public static void displayStatistics(List<Prize> results){
+        System.out.println(ConsoleMessage.LOTTO_STATISTICS_HEADER);
+        for(Prize rank : Prize.values()){
+            long count = results.stream().filter(result -> result == rank).count();
+
+            if(rank == Prize.FIVE_MATCH_WITH_BONUS){
+                System.out.printf((ConsoleMessage.MATCH_COUNT_AND_PRIZE) + "%n", rank.getMatchCount(), ", 보너스 볼 일치", rank.getPrize(), count);
+                continue;
+            }
+            if(rank != Prize.NO_MATCH){
+                System.out.printf((ConsoleMessage.MATCH_COUNT_AND_PRIZE) + "%n", rank.getMatchCount(), "", rank.getPrize(), count);
+            }
+        }
+    }
+
+    public static void displayProfitRate(double profitRate){
+        System.out.printf(ConsoleMessage.MATCH_COUNT_AND_PRIZE_BONUS, profitRate);
+    }
+}
