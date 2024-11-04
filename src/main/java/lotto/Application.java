86ace4b46f002927cf20757a890959332911af10
bellmin <whdlas011203@naver.com>
2024-11-04 20:04:50 +0900

    feat(Application): Application 클래스 구현
    - Controller 초기화
    - Controller에 필요한 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import lotto.application.service.LottoMachine;
+import lotto.application.service.LottoAnchor;
+import lotto.application.service.LottoService;
+import lotto.application.support.LottoInputParser;
+import lotto.controller.LottoController;
+import lotto.view.KeyboardLottoInputView;
+import lotto.view.MonitorLottoOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController controller = new LottoController(
+                keyboardLottoInputView(),
+                monitorLottoOutputView(),
+                lottoService(),
+                lottoInputParser()
+        );
+
+        controller.run();
+    }
+
+    private static KeyboardLottoInputView keyboardLottoInputView(){
+        return new KeyboardLottoInputView();
+    }
+
+    private static MonitorLottoOutputView monitorLottoOutputView(){
+        return new MonitorLottoOutputView();
+    }
+
+    private static LottoInputParser lottoInputParser(){
+        return new LottoInputParser();
+    }
+
+    private static LottoService lottoService(){
+        return new LottoService(
+                new LottoMachine(),
+                new LottoAnchor()
+        );
     }
 }
