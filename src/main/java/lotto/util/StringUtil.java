d116bd7bd18d5717ceec3bf2a8719edf64bf6597
whdmscks11 <whdmscks11@gmail.com>
2024-11-04 11:09:38 +0900

    fix(StringUtil): 구매 금액 입력 시 예외 처리 개선
    - 입력 값 앞뒤에 공백이 있는 경우 strip 메서드를 사용해 제거
    - 입력 값에 공백 또는 쉼표가 포함된 경우, 두 개 이상의 값이 입력된 것으로 판단하여 예외를 던지도록 수정

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -3,13 +3,21 @@ package lotto.util;
 public class StringUtil {
     public static Integer parseToPositiveInt(String strNum){
         try{
+            // 입력 값 앞 뒤 공백 제거
+            strNum = strNum.strip();
+
+            // 공백 제거 후에도 공백이 있거나 쉼표가 포함 되어 있으면 에러 발생
+            if(strNum.contains(" ") || strNum.contains(",")){
+                throw new IllegalArgumentException("[ERROR] 하나의 숫자만 입력 할 수 있습니다.");
+            }
+
             int num = Integer.parseInt(strNum);
             if (num <= 0) {
                 throw new IllegalArgumentException("[ERROR] 0 또는 음수 값을 입력 할 수 없습니다.");
             }
             return num;
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 1-45 사이의 값을 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다. 다시 시도해주세요.");
         }
     }
 }
