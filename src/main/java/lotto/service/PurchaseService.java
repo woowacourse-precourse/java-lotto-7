bc3a60a645c2e88d3e8624521b1affb6c4091f27
hanwool <hanwe92@gmail.com>
2024-11-04 14:09:39 +0900

    fix(PurchaseService): Remove try-catch to controller not to use InputView

--- src/main/java/lotto/service/PurchaseService.java
+++ src/main/java/lotto/service/PurchaseService.java
@@ -5,21 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.DrawNumbers;
 import lotto.domain.Purchase;
-import lotto.view.InputView;
 
 public class PurchaseService {
 
     public Purchase purchaseLotto(int purchaseAmountNumber) {
-        Purchase purchase = null;
-        while (purchase == null) {
-            try {
-                purchase = new Purchase(purchaseAmountNumber);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                purchaseAmountNumber = InputView.getPurchaseAmount();
-            }
-        }
-        return purchase;
+        return new Purchase(purchaseAmountNumber);
     }
 
     public List<DrawNumbers> getRandomDrawNumbersByTicketCount(int ticketCount) {
