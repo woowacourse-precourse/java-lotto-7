4fa242559f0876d04d1d9b87bc798ee5bb594931
seophohoho <seop0937@gmail.com>
2024-11-04 12:54:32 +0900

    feat(Output): 지불 금액 입력 view 메소드 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,2 +1,15 @@
-package lotto.view;public class Output {
+package lotto.view;
+
+import java.util.List;
+import lotto.model.Lotto;
+
+public class Output {
+    public void printPayResult(List<Lotto> lottos) {
+        System.out.printf("%d개를 구매했습니다.%n", lottos.size());
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+
+    }
 }
