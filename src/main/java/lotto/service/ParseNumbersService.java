bdad13967a82f9e0a172972928d67ebea8901f0e
kaswhy <sxshx818@naver.com>
2024-11-04 15:36:31 +0900

    feat(ParseNumbersService): 당첨 번호 문자열을 잘랐을 때 공백이면 예외 반환
    변경 후
    - 문자열 잘라서 해당 기능 검증 후 List<Integer>로 형변환한 값 반환

---
+++ src/main/java/lotto/service/ParseNumbersService.java
@@ -0,0 +1,48 @@
+package lotto.service;
+
+import static lotto.constants.ErrorMessage.INPUT_CAN_NOT_BE_BLANK;
+import static lotto.constants.ErrorMessage.LOTTO_CAN_NOT_HAVE_CHARACTER;
+import static lotto.constants.ErrorMessage.LOTTO_CAN_NOT_HAVE_BLANK;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import lotto.model.InputWinningNumbers;
+
+public class ParseNumbersService {
+
+    private static final Pattern HAS_CHARACTER_PATTERN = Pattern.compile("[^0-9]");
+    private static final String NUMBERS_DELIMITER = ",";
+    private static final String BLANK = " ";
+    private static final int INCLUDE_AFTER_DELIMITER = -1;
+
+    public ParseNumbersService() {
+    }
+
+    public List<Integer> getWinningLotto(InputWinningNumbers inputWinningNumbers) {
+        List<String> winningNumbers = Arrays.stream(inputWinningNumbers.get()
+                        .split(NUMBERS_DELIMITER, INCLUDE_AFTER_DELIMITER))
+                .map(String::strip).toList();
+
+        validate(winningNumbers);
+
+        return winningNumbers.stream().mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            validateIsBlank(winningNumber);
+        }
+    }
+
+    private void validateIsBlank(String winningNumber) {
+        if (winningNumber == null || winningNumber.isBlank()) {
+            throw new IllegalArgumentException(INPUT_CAN_NOT_BE_BLANK.get());
+        }
+    }
+
+
+}
