460e4b80e8eb83abac4fe8447c28061e28c813aa
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:45:18 +0900

    feat(Money): 금액이 음수면 예외를 발생시킨다

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public class Money {
+
+    private final long amount;
+
+    public Money(long amount) {
+        validateNonNegative(amount);
+        this.amount = amount;
+    }
+
+    private void validateNonNegative(long amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("금액은 음수일 수 없습니다");
+        }
+    }
+}
+
