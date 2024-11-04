a33e0994b5b91104610a8e7fec86c9702f84ed52
jeonghyeon-ohk <ok6737@naver.com>
2024-11-04 23:37:28 +0900

    feat(lotto): 실제 프로그램이 작동 할 수 있도록 Application 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import lotto.controller.Draw;
+import lotto.domain.Lotto;
+import lotto.domain.Store;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int amount = InputView.getAmount();
+        ArrayList<Lotto> tickets = Store.ticketMachine(amount);
+        OutputView.printTickets(tickets, amount);
+
+        Lotto winningNumber = InputView.getWinningNumber();
+        int bonus = InputView.getBonus(winningNumber);
+
+        Draw draw = new Draw(amount, tickets, winningNumber, bonus);
+        draw.run();
     }
 }
