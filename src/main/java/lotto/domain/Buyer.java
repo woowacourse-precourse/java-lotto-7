7ee55055a951bba14259bdd05b8ab91de3445fa1
Awon01 <bec00@naver.com>
2024-11-04 20:37:05 +0900

    feat(service,domain): 로또 발급 기능

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,5 +1,39 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+
 public class Buyer {
+    private ArrayList<Lotto> purchasedLotteries = new ArrayList<>();
+    private int purchaseAmount;
+    private long winningAmount = 0;
+    private double yield = 0;
+
+    public Buyer(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public void buyLotto(Lotto lotto) {
+        purchasedLotteries.add(lotto);
+    }
+
+    public ArrayList<Lotto> getPurchasedLotteries() {
+        return purchasedLotteries;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public void setPurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public long getWinningAmount() {
+        return winningAmount;
+    }
+
+    public double getYield() {
+        return yield;
+    }
 
 }
