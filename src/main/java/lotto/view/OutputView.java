ed4dec74a164b003003f6db7f2f8bd95078a4433
DongHoon Lee <dhl1924@naver.com>
2024-11-04 21:17:41 +0900

    feat(view): 당첨 통계 및 수익률 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
 import lotto.domain.Lottos;
+import lotto.domain.WinningResult;
 
 public interface OutputView {
     void printPurchasedLottoCount(int count);
 
     void printLottoTickets(Lottos lottos);
+
+    void printResults(WinningResult winningResult);
+
+    void printProfitRate(double profitRate);
 }
