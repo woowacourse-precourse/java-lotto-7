0755043b63c08465147ca2d7015ec0aa2d43ed18
agi1004 <agi1009@naver.com>
2024-11-04 22:36:24 +0900

    feat: 당첨 등수를 관리하는 enum 클래스
    FIFTH - 5등 (번호 3개 일치, 보너스 볼 X, 5,000원)
    FOURTH - 4등 (번호 4개 일치, 보너스 볼 X, 50,000원)
    THRID - 3등 (번호 5개 일치, 보너스 볼 X, 1,500,000원)
    SECOND - 2등 (번호 5개 일치, 보너스 볼 O, 30,000,000원)
    FIRST - 1등 (번호 6개 일치, 보너스 볼 X, 2,000,000,000원)

--- src/main/java/lotto/prize/Prize.java
+++ src/main/java/lotto/prize/Prize.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public enum Prize {
 	FIFTH(3, false, 5_000),
-	FOURTH(4, false, 5_0000),
+	FOURTH(4, false, 50_000),
 	THIRD(5, false, 1_500_000),
 	SECOND(5, true, 30_000_000),
 	FIRST(6, false, 2_000_000_000);
