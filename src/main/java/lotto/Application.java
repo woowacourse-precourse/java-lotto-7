93a04fef3aa69215f6abd50043d58df4f457edf0
yubin8773 <hiyubin.lee@gmail.com>
2024-11-04 23:26:33 +0900

    feat : 로또 번호 랜덤 생

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -12,5 +12,9 @@ public class Application {
         long purchasePrice = Input.inputNum();
         System.out.println(purchasePrice / 1000 + "개를 구매했습니다.");
 
+        // 로또 번호 랜덤 선택 및 출력
+        List<Lotto> lottos = RunLotto.generateLottos(purchasePrice);
+
+
     }
 }
