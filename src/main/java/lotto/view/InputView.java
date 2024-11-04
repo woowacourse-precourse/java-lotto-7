05d32721852131110dee9fe51dbf1c30f0ab4b08
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 19:22:42 +0900

    refact(InputView): BuyingPrice 검증 로직 삭제

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,15 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public int inputBuyingPriceView() {
-        try {
-            String buyingPrice = Console.readLine();
-            BuyingPriceValidator validator = new BuyingPriceValidator();
-            validator.validate(buyingPrice);
-            return Integer.parseInt(buyingPrice);
-        } catch (IllegalArgumentException exception) {
-            throw new IllegalArgumentException(exception.getMessage());
-        }
+    public String inputBuyingPriceView() {
+        String buyingPrice = Console.readLine();
+        return buyingPrice;
     }
 
     public String inputLottoNumbersView() {
