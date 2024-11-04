86784ca229e5711e3d32eadb6bc363b06e5026e3
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 16:41:43 +0900

    feat: 등수에 따른 설명과 상금 정보 추가
    enum으로 관리

---
+++ src/main/java/lotto/entity/prize.java
@@ -0,0 +1,18 @@
+package lotto.entity;
+
+public enum prize {
+
+    FIRST("6개 일치 (2,000,000,000원)", 2000000000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
+    THIRD("5개 일치 (1,500,000원)", 1500000),
+    FOURTH("4개 일치 (50,000원)", 50000),
+    FIFTH("3개 일치 (5,000원)", 5000);
+
+    private final String description;
+    private final int amount;
+
+    prize(String description, int amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+}
