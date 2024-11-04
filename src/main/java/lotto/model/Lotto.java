120f37b5ec5a9975727428d977010fe575e7d997
kaswhy <sxshx818@naver.com>
2024-11-04 16:10:45 +0900

    feat(Lotto): 로또 클래스 패키지 변경 및 중복된 숫자 검증
    변경 전
    - lotto 패키지에 Lotto 위치
    - test 패키지에 LottoTest 위치
    
    변경 후
    - lotto.model로 Lotto 위치 변경
    - test.model로 LottoTest위치 변경
    - Lotto 클래스에서 입력된 List에 중복된 값이 있으면 예외 던지는 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,6 @@
-package lotto;
+package lotto.model;
+
+import static lotto.constants.ErrorMessage.LOTTO_NUMBERS_CAN_NOT_BE_DUPLICATED;
 
 import java.util.List;
 
@@ -13,6 +15,13 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        validateHasDuplicatedNumbers(numbers);
+    }
+
+    private void validateHasDuplicatedNumbers(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException(LOTTO_NUMBERS_CAN_NOT_BE_DUPLICATED.get());
         }
     }
 
