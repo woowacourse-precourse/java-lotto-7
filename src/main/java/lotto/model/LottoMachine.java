4bbcd4541c244b6c84afd8dd8462048838d54e10
Su-daa <subin.kk019@gmail.com>
2024-11-04 20:55:37 +0900

    refactor: LottoGenerator를 인스턴스 기반으로 변경

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,7 +1,14 @@
 package lotto.model;
 
 public class LottoMachine {
+
+    private final LottoGenerator lottoGenerator;
+
+    public LottoMachine(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
     public Lottos sell(Money buyAmount) {
-        return LottoGenerator.generate(buyAmount);
+        return lottoGenerator.generate(buyAmount);
     }
 }
