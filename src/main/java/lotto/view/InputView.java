cb659aa983a1c58907b296014424c9a681f42c19
yunhacandy <yunhacandy@gmail.com>
2024-11-04 12:34:22 +0900

    feat (InputView): 입력 값이 없는 경우에 대해 예외처리 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,12 @@ import lotto.constant.RequestMessage;
 public class InputView {
     public static int inputPurchaseAmount() {
         System.out.println(RequestMessage.REQUEST_LOTTO_PURCHASE_AMOUNT.getMessage());
-        return Integer.parseInt(Console.readLine().trim());
+        String input = Console.readLine().trim();
+
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(PurchaseException.EMPTY_INPUT.getMessage());
+        }
+
+        return parseAmount(input);
     }
 }
