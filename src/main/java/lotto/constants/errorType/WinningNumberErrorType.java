d3df909fab68062f7bfc6f5b5b5212d246bde7ac
Teddysir <sdzx0719@naver.com>
2024-11-04 18:59:20 +0900

    test: 당첨 번호 에러 메시지 출력 테스트

--- src/main/java/lotto/constants/errorType/WinningNumberErrorType.java
+++ src/main/java/lotto/constants/errorType/WinningNumberErrorType.java
@@ -2,7 +2,6 @@ package lotto.constants.errorType;
 
 public enum WinningNumberErrorType {
 
-    INVALID_WINNING_NUMBERS_COUNT("[ERROR] 로또 당첨 번호는 6개를 입력해 주세요."),
     INVALID_WINNING_NUMBER_RANGE("[ERROR] 로또 당첨 번호는 1~45의 숫자만 입력해 주세요."),
     INVALID_WINNING_NUMBER_FORMAT("[ERROR] 지원하지 않는 당첨 번호 형식입니다."),
     INVALID_WINNING_NUMBER_DUPLICATE("[ERROR] 로또 번호에 중복된 번호가 존재합니다.");
---
+++ src/test/java/lotto/view/WinningNumberMessageTest.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
+import lotto.constants.errorType.BonusNumberErrorType;
+import lotto.constants.errorType.WinningNumberErrorType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningNumberMessageTest extends NsTest {
+
+    @Test
+    @DisplayName("로또 당첨 번호가 1~45가 아닐 경우 테스트")
+    void 로또_당첨_번호_범위_테스트() {
+        assertSimpleTest(() -> {
+            runException("5000", "1,2,46,4,5");
+            assertThat(output()).contains(WinningNumberErrorType.INVALID_WINNING_NUMBER_RANGE.getMessage());
+        });
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호가 올바른 형식이 아닐 경우 테스트")
+    void 로또_당첨_번호_형식_테스트() {
+        assertSimpleTest(() -> {
+            runException("5000", "1,2,삼,4,5");
+            assertThat(output()).contains(WinningNumberErrorType.INVALID_WINNING_NUMBER_FORMAT.getMessage());
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
+}
