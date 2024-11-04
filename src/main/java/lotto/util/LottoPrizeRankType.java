b4b70cd335f5be4d76b190d07c5d0bbe6c3eb825
dongwoo <woomacho@gmail.com>
2024-11-04 20:32:19 +0900

    fix: 로또 결과 출력 순서 수정
    - 입력 요구사항에 맞게 3개 일치 ~ 6개 일치 순서로 변경
    - 테스트 코드 문자열 순서는 상관없어서 다시 복구

--- src/main/java/lotto/util/LottoPrizeRankType.java
+++ src/main/java/lotto/util/LottoPrizeRankType.java
@@ -5,12 +5,12 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public enum LottoPrizeRankType {
-    FIRST(6, false, 2000000000L),
-    SECOND(5, true, 30000000L),
-    THIRD(5, false, 1500000L),
-    FOURTH(4, false, 50000L),
+    ZERO(0, false, 0L),
     FIFTH(3, false, 5000L),
-    ZERO(0, false, 0L);
+    FOURTH(4, false, 50000L),
+    THIRD(5, false, 1500000L),
+    SECOND(5, true, 30000000L),
+    FIRST(6, false, 2000000000L);
 
     private final int matchCount;
     private final boolean bonusMatch;
