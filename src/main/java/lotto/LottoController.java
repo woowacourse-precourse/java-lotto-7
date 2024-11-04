6e67d2ec3f5913b09ed45ff4423f67633f8b0d22
johyewon <johyewon4601@gmail.com>
2024-11-04 16:13:58 +0900

    refactor: View에서 발생하는 예외처리 삭제 후 Controller로 이동

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -8,17 +8,21 @@ public class LottoController {
     private final LottoResult lottoResult = new LottoResult();
 
     public void start() {
-        int purchaseAmount = lottoView.inputPurchaseAmount();
-        List<Lotto> lottos = lottoMachine.issueLottos(purchaseAmount);
-        lottoView.printLotto(lottos);
+        try{
+            int purchaseAmount = lottoView.inputPurchaseAmount();
+            List<Lotto> lottos = lottoMachine.issueLottos(purchaseAmount);
+            lottoView.printLotto(lottos);
 
-        List<Integer> winningNumbers = lottoView.inputWinningNumbers();
-        int bonusNumber = lottoView.inputBonusNumber();
-        Lotto winningLotto = new Lotto(winningNumbers);
+            List<Integer> winningNumbers = lottoView.inputWinningNumbers();
+            int bonusNumber = lottoView.inputBonusNumber();
+            Lotto winningLotto = new Lotto(winningNumbers);
 
-        int[] resultCnt = lottoResult.calculateRank(lottos, winningLotto, bonusNumber);
-        double profitRate = lottoResult.calculateProfitRate(resultCnt, purchaseAmount);
+            int[] resultCnt = lottoResult.calculateRank(lottos, winningLotto, bonusNumber);
+            double profitRate = lottoResult.calculateProfitRate(resultCnt, purchaseAmount);
 
-        lottoView.printResult(resultCnt, profitRate);
+            lottoView.printResult(resultCnt, profitRate);
+        } catch(IllegalArgumentException e){
+            System.out.println("[ERROR] 형식에 맞게 제대로 입력해야 합니다.");
+        }
     }
 }
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -7,19 +7,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class LottoView {
-    private static final String ERROR_INVALID_AMOUNT = "[ERROR] 구매 금액은 숫자여야 합니다.";
-
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-
-        int input = 0;
-
-        try{
-            input = Integer.parseInt(Console.readLine());
-        } catch (IllegalArgumentException e) {
-            System.out.println(ERROR_INVALID_AMOUNT);
-        }
-        return input;
+        return Integer.parseInt(Console.readLine());
     }
 
     public List<Integer> inputWinningNumbers() {
