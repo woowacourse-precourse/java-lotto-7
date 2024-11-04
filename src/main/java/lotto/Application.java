bd3485a50c6cccc0247c921de122ba11be89e063
양효빈 <hyobin3026@naver.com>
2024-11-04 23:24:27 +0900

    fear(LottoService): Create service layer for Lotto functionality - Refactor LottoController - Refactor Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,12 @@
 package lotto;
 
-
 import lotto.config.AppConfig;
 import lotto.controller.LottoController;
-import lotto.view.ConsoleInputProvider;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleInputProvider inputProvider = new ConsoleInputProvider();
         AppConfig appConfig = new AppConfig();
         LottoController controller = appConfig.createLottoController();
         controller.startLotto();
-        inputProvider.closeConsole();
-
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,7 @@ import lotto.model.lotto.Lotto;
 import lotto.model.lotto.LottoConstants;
 import lotto.model.lotto.LottoWinningNumbers;
 import lotto.model.lottoPurchaser.LottoPurchaser;
+import lotto.service.LottoService;
 import lotto.util.*;
 import lotto.view.InputProvider;
 import lotto.view.LottoInputViewFactory;
@@ -17,71 +18,89 @@ import java.util.Set;
 
 public class LottoController {
 
-    private final LottoPurchaseInputView lottoPurchaseInputView;
-    private final WinningLottoInputView winningLottoInputView;
-    private final LottoPurchaseOutputView lottoPurchaseOutputView;
-    private final WinningLottoOutputView winningLottoOutputView;
-    private final LottoProfitOutputView lottoProfitOutputView;
+//    private final LottoPurchaseInputView lottoPurchaseInputView;
+//    private final WinningLottoInputView winningLottoInputView;
+//    private final LottoPurchaseOutputView lottoPurchaseOutputView;
+//    private final WinningLottoOutputView winningLottoOutputView;
+//    private final LottoProfitOutputView lottoProfitOutputView;
+//
+//
+//    public LottoController(InputProvider inputProvider, LottoInputViewFactory lottoInputViewFactory,
+//                           LottoOutputViewFactory outputViewFactory
+//    ) {
+//        NumberConverter numberConverter = new NumberConverter();
+//        this.lottoPurchaseInputView = lottoInputViewFactory.createLottoPurchaseInputView(inputProvider, numberConverter);
+//        this.winningLottoInputView = lottoInputViewFactory.createWinningLottoInputView(inputProvider, numberConverter);
+//        this.lottoPurchaseOutputView = outputViewFactory.createLottoPurchaseOutputView();
+//        this.winningLottoOutputView = outputViewFactory.createWinningLottoOutputView();
+//        this.lottoProfitOutputView = outputViewFactory.createLottoProfitOutputView();
+//    }
+//
+//    public void startLotto() {
+//        int lottoPurchasePrice = getLottoPurchasePrice();
+//        int lottoPurchaseCount = calculateLottoCount(lottoPurchasePrice);
+//
+//        LottoPurchaser lottoPurchaser = new LottoPurchaser(lottoPurchasePrice);
+//
+//        showLottoPurchaseCount(lottoPurchaseCount);
+//        issueLottos(lottoPurchaser, lottoPurchaseCount);
+//        showLottoResult(lottoPurchaser, getWinningLottoNumbers());
+//    }
+//
+//    private int getLottoPurchasePrice() {
+//        return lottoPurchaseInputView.getPurchasePrice();
+//    }
+//
+//    private int calculateLottoCount(int lottoPurchasePrice) {
+//        return lottoPurchasePrice / LottoConstants.PRICE;
+//    }
+//
+//    private void showLottoPurchaseCount(int lottoCount) {
+//        lottoPurchaseOutputView.showLottoPurchaseCount(lottoCount);
+//    }
+//
+//    private void issueLottos(LottoPurchaser lottoPurchaser, int lottoCount) {
+//        for (int i = 0; i < lottoCount; i++) {
+//            List<Integer> issuedLotto = issueOneLotto();
+//            lottoPurchaser.addLotto(new Lotto(issuedLotto));
+//            showIssuedLotto(issuedLotto);
+//        }
+//    }
+//
+//    private List<Integer> issueOneLotto() {
+//        return LottoNumberGenerator.generateRandomNumbers();
+//    }
+//
+//    private void showIssuedLotto(List<Integer> issuedLotto) {
+//        lottoPurchaseOutputView.showIssuedLottoNumbers(issuedLotto);
+//    }
+//
+//    private LottoWinningNumbers getWinningLottoNumbers() {
+//        Set<Integer> winningLottoNumbers = winningLottoInputView.inputWinningLottoNumbers();
+//        int bonusNumber = winningLottoInputView.inputBonusNumbers(winningLottoNumbers);
+//        return new LottoWinningNumbers(winningLottoNumbers, bonusNumber);
+//    }
+//
+//    private void showLottoResult(LottoPurchaser lottoPurchaser, LottoWinningNumbers winningNumbers) {
+//        winningLottoOutputView.showLottoResult(lottoPurchaser.calculateLottoResult(winningNumbers));
+//        lottoProfitOutputView.showLottoProfitRate(lottoPurchaser.calculateProfitRate());
+//    }
 
+    private final LottoService lottoService;
 
-    public LottoController(InputProvider inputProvider, LottoInputViewFactory lottoInputViewFactory,
-                           LottoOutputViewFactory outputViewFactory
-    ) {
+    public LottoController(InputProvider inputProvider, LottoInputViewFactory lottoInputViewFactory, LottoOutputViewFactory outputViewFactory) {
         NumberConverter numberConverter = new NumberConverter();
-        this.lottoPurchaseInputView = lottoInputViewFactory.createLottoPurchaseInputView(inputProvider, numberConverter);
-        this.winningLottoInputView = lottoInputViewFactory.createWinningLottoInputView(inputProvider, numberConverter);
-        this.lottoPurchaseOutputView = outputViewFactory.createLottoPurchaseOutputView();
-        this.winningLottoOutputView = outputViewFactory.createWinningLottoOutputView();
-        this.lottoProfitOutputView = outputViewFactory.createLottoProfitOutputView();
+        LottoPurchaseInputView lottoPurchaseInputView = lottoInputViewFactory.createLottoPurchaseInputView(inputProvider, numberConverter);
+        WinningLottoInputView winningLottoInputView = lottoInputViewFactory.createWinningLottoInputView(inputProvider, numberConverter);
+
+        this.lottoService = new LottoService(lottoPurchaseInputView, winningLottoInputView,
+                outputViewFactory.createLottoPurchaseOutputView(),
+                outputViewFactory.createWinningLottoOutputView(),
+                outputViewFactory.createLottoProfitOutputView());
     }
 
-    public void startLotto(){
-        int lottoPurchasePrice = getLottoPurchasePrice();
-        int lottoPurchaseCount = calculateLottoCount(lottoPurchasePrice);
-
-        LottoPurchaser lottoPurchaser = new LottoPurchaser(lottoPurchasePrice);
-
-        showLottoPurchaseCount(lottoPurchaseCount);
-        issueLottos(lottoPurchaser, lottoPurchaseCount);
-        showLottoResult(lottoPurchaser, getWinningLottoNumbers());
+    public void startLotto() {
+        lottoService.startLotto();
     }
 
-    private int getLottoPurchasePrice(){
-        return lottoPurchaseInputView.getPurchasePrice();
-    }
-
-    private int calculateLottoCount(int lottoPurchasePrice){
-        return lottoPurchasePrice/ LottoConstants.PRICE;
-    }
-
-    private void showLottoPurchaseCount(int lottoCount){
-        lottoPurchaseOutputView.showLottoPurchaseCount(lottoCount);
-    }
-
-    private void issueLottos(LottoPurchaser lottoPurchaser, int lottoCount){
-        for (int i=0; i<lottoCount; i++){
-            List<Integer> issuedLotto = issueOneLotto();
-            lottoPurchaser.addLotto(new Lotto(issuedLotto));
-            showIssuedLotto(issuedLotto);
-        }
-    }
-
-    private List<Integer> issueOneLotto(){
-        return LottoNumberGenerator.generateRandomNumbers();
-    }
-
-    private void showIssuedLotto(List<Integer> issuedLotto){
-        lottoPurchaseOutputView.showIssuedLottoNumbers(issuedLotto);
-    }
-
-    private LottoWinningNumbers getWinningLottoNumbers() {
-        Set<Integer> winningLottoNumbers = winningLottoInputView.inputWinningLottoNumbers();
-        int bonusNumber = winningLottoInputView.inputBonusNumbers(winningLottoNumbers);
-        return new LottoWinningNumbers(winningLottoNumbers, bonusNumber);
-    }
-
-    private void showLottoResult(LottoPurchaser lottoPurchaser, LottoWinningNumbers winningNumbers){
-        winningLottoOutputView.showLottoResult(lottoPurchaser.calculateLottoResult(winningNumbers));
-        lottoProfitOutputView.showLottoProfitRate(lottoPurchaser.calculateProfitRate());
-    }
 }
