d25742aab91f3b67c7a0cd7a4b515e5caf302567
wlsdlf2 <wlsdlfzg@naver.com>
2024-11-04 21:36:56 +0900

    chore: 예외 메시지 수정

--- src/main/java/lotto/util/LottoValidator.java
+++ src/main/java/lotto/util/LottoValidator.java
@@ -8,29 +8,29 @@ import java.util.List;
 public class LottoValidator {
     public static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < 0) {
-            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 음수는 허용되지 않습니다.");
         } else if (!isValidAmountInThousand(purchaseAmount)) {
-            throw new IllegalArgumentException("1000원 단위로만 구매가 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로만 구매가 가능합니다.");
         }
     }
 
     public static void validateLottoNumbers (List<Integer> numbers) {
         if (hasDuplicates(numbers)) {
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         } else if (!isValidWinningNumber(numbers)) {
-            throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다.");
         } else if (numbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
     public static void validateBonusNumber(int bonusNumber, List<Integer> numbers) {
         if (bonusNumber < 0) {
-            throw new IllegalArgumentException("보너스 번호는 음수가 허용되지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 음수가 허용되지 않습니다.");
         } else if (!isValidBonusNumber(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이의 숫자여야 합니다.");
         } else if (isInWinningNumbers(bonusNumber, numbers)) {
-            throw new IllegalArgumentException("보너스 번호와 당첨 번호는 중복될 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호와 당첨 번호는 중복될 수 없습니다.");
         }
     }
 
