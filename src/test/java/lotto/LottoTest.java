c45d8923d7829eb55d99cc0864785b6fc5037e03
acecic82 <102938al@naver.com>
2024-11-04 18:50:41 +0900

    fix(LottoTest): LottoTest class 파일 수정
    수정된 내용
    
    - 보너스 숫자가 lotto 숫자와 일치하는지 확인하는 테스트 추가.
    - lotto 숫자가 당첨 숫자와 몇 개가 일치하는지 확인하는 테스트 추가.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +23,26 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @DisplayName("로또 번호와 일치하는 숫자의 갯수를 리턴한다")
+    @Test
+    void 로또_번호와_일치하는_숫자의_갯수를_리턴한다() {
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        List<Integer> winNumber = List.of(1,2,12,13,14,15);
+
+        int matchNumber = lotto.calculateMatchCount(winNumber);
+
+        Assertions.assertEquals(2, matchNumber);
+    }
+
+    @DisplayName("로또 번호와 일치하는 보너스 번호가 있는지 확인한다")
+    @Test
+    void 로또_번호와_일치하는_보너스_번호가_있는지_확인한다() {
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+
+        boolean isMatched = lotto.findMatchBonusNumber(1);
+
+        Assertions.assertEquals(true, isMatched);
+    }
+
 }
