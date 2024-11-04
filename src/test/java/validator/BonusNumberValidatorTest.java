9cf23e0a6a8988a3311d42f82e912085576f0187
xeimun <hinote444@naver.com>
2024-11-04 20:15:36 +0900

    test(BonusNumberValidatorTest): 공통 기능 추출

--- src/test/java/validator/BonusNumberValidatorTest.java
+++ src/test/java/validator/BonusNumberValidatorTest.java
@@ -4,17 +4,24 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BonusNumberValidatorTest {
+    
+    private List<Integer> lottoNumbers;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+    }
 
     @Test
     @DisplayName("보너스 번호가 1 이상 45 이하의 범위일 때 예외가 발생하지 않는다.")
     void 보너스_번호가_허용된_범위_내에_있어_예외가_발생하지_않는다() {
         // given
         int validBonusNumber = 15;
-        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
 
         // when, then
         assertThatCode(() -> BonusNumberValidator.validateBonusNumber(validBonusNumber, lottoNumbers))
@@ -26,7 +33,6 @@ public class BonusNumberValidatorTest {
     void 보너스_번호가_허용된_범위_미만이어서_예외가_발생한다() {
         // given
         int invalidBonusNumber = 0;
-        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
 
         // when, then
         assertThatThrownBy(() -> BonusNumberValidator.validateBonusNumber(invalidBonusNumber, lottoNumbers))
@@ -38,7 +44,6 @@ public class BonusNumberValidatorTest {
     void 보너스_번호가_허용된_범위를_초과해서_예외가_발생한다() {
         // given
         int invalidBonusNumber = 46;
-        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
 
         // when, then
         assertThatThrownBy(() -> BonusNumberValidator.validateBonusNumber(invalidBonusNumber, lottoNumbers))
