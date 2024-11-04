8f60db6447842fba3628a677df4bf463abc68876
양효빈 <hyobin3026@naver.com>
2024-11-04 10:03:23 +0900

    feat(NumberFormatter): Add interface to format number

--- src/main/java/lotto/util/NumberFormatter.java
+++ src/main/java/lotto/util/NumberFormatter.java
@@ -1,12 +1,5 @@
 package lotto.util;
 
-import java.text.DecimalFormat;
-
-// 이름 변경해야 할 듯
-public class NumberFormatter{
-
-    public String formatNumber(int number) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###");
-        return decimalFormat.format(number);
-    }
+public interface NumberFormatter {
+    String formatNumber(double number);
 }
