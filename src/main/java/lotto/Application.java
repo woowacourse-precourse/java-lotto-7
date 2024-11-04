d4c846ae16d8cab15ece9a777d922495709fa6a2
siyoung <wfs0502@gmail.com>
2024-11-04 14:32:40 +0900

    feat: 입력 예외 발생 시 재입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PurchaseAmount purchaseAmount = promptForPurchaseAmount();
+        WinningLotto winningLotto = promptForWinningLotto();
+    }
+
+    private static PurchaseAmount promptForPurchaseAmount() {
+        try {
+            int amount = Input.readPurchaseAmount();
+            return new PurchaseAmount(amount);
+        } catch (IllegalArgumentException e) {
+            return promptForPurchaseAmount();
+        }
+    }
+
+    private static WinningLotto promptForWinningLotto() {
+        try {
+            Lotto winningNumbers = Input.readWinningNumbers();
+            int bonusNumber = Input.readBonusNumber();
+            return new WinningLotto(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            return promptForWinningLotto();
+        }
     }
 }
