4f7c244daa3d89d5dd0dd6bf6e7f7d40809437f2
Koo-EunSung <ku903@naver.com>
2024-11-04 23:28:17 +0900

    ãFeat: 로또 결과 관련 비즈니스 로직

---
+++ src/main/java/lotto/model/LottoGame.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoGame {
+    private List<Lotto> lottos;
+    private List<Integer> winningNumbers;
+    private Integer bonusNumber;
+    private Map<Prize, Integer> results = new HashMap<>();
+
+    public LottoGame(List<Lotto> lottos, List<Integer> winningNumbers, Integer bonusNumber) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Map<Prize, Integer> getResult() {
+        for (Lotto lotto : lottos) {
+            Prize result = lotto.checkResult(winningNumbers, bonusNumber);
+            results.put(result, results.getOrDefault(result, 0) + 1);
+        }
+
+        return results;
+    }
+}
