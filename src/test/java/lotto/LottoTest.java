d53d18b7f45bb46866673db32b8fd1a37f47771c
HyejinYoo <yyyhj0114@gmail.com>
2024-11-04 18:17:17 +0900

    test(LottoTest): 로또 번호 예외 처리 및 정상 생성 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,9 +2,8 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -20,6 +19,40 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    
+    @Test
+    @DisplayName("로또 번호가 정상적으로 생성되는 경우")
+    void 로또_번호_정상_생성() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호가 6개가 아닐 경우 예외 발생")
+    void 로또_번호_개수_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 번호에 중복된 숫자가 있을 경우 예외 발생")
+    void 로또_번호_중복_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호에는 중복된 숫자가 없어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있을 경우 예외 발생")
+    void 로또_번호_범위_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
 }
