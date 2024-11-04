c8c18949a202d56cb0a05a81c2ab7741a7071ce0
양효빈 <hyobin3026@naver.com>
2024-11-04 09:58:13 +0900

    refactor(NumberConverter): Add logics to convert number - Replace interface with class in number converter module

--- src/main/java/lotto/util/NumberConverter.java
+++ src/main/java/lotto/util/NumberConverter.java
@@ -1,5 +1,19 @@
 package lotto.util;
 
-public interface NumberConverter {
-    int convertNumber(String input);
+import lotto.exception.GeneralExceptionMessages;
+
+public class NumberConverter{
+
+    public int convertNumber(String input) {
+        String trimmedInput = input.trim();
+        if (trimmedInput.isEmpty() || !trimmedInput.matches("\\d+")) {
+            throw new IllegalArgumentException(GeneralExceptionMessages.INVALID_NUMBER);
+        }
+
+        try {
+            return Integer.parseInt(trimmedInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(GeneralExceptionMessages.INVALID_NUMBER);
+        }
+    }
 }
