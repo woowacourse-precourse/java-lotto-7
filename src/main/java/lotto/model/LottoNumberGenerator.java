76f300f48049fccbafc04d4683863ef43c9cfb33
ruudska6 <feels19@naver.com>
2024-11-04 16:28:58 +0900

    <reafactor> LottoNumberGenerator static으로 수정

--- src/main/java/lotto/model/LottoNumberGenerator.java
+++ src/main/java/lotto/model/LottoNumberGenerator.java
@@ -5,18 +5,19 @@ import java.util.List;
 
 public class LottoNumberGenerator {
 
-    public List<Integer> generateLottoNumbers() {
+    private LottoNumberGenerator() {
+    }
+
+    public static List<Integer> generateLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
-    public int generateBonusNumber(List<Integer> numbers) {
+    public static int generateBonusNumber(List<Integer> numbers) {
         int bonus;
         do {
             bonus = Randoms.pickNumberInRange(1, 45);
         } while (numbers.contains(bonus));
         return bonus;
     }
-
-
 
 }
