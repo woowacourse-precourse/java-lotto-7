9f033aaf282c5de1c8fd646018f7530025600998
btongtong <leeym2615@naver.com>
2024-11-04 17:37:32 +0900

    test(CommonValidationTest): Add test for bonus number validation
    Add test to ensure error is thrown when:
        Bonus number is out of the 1-45 range
        Bonus number duplicates one of the winning numbers

--- src/test/java/lotto/CommonValidationTest.java
+++ src/test/java/lotto/CommonValidationTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class CommonValidationTest {
     @Test
     void 입력값이_Integer_형식이_아니면_에러가_난다() {
@@ -22,5 +24,20 @@ public class CommonValidationTest {
         String input = "134239";
         assertThat(CommonValidation.convertStringToInt(input))
                 .isEqualTo(134239);
+    }
+
+    @Test
+    void 보너스가_1_45_범위를_벗어나면_에러가_난다() {
+        int bonus = 82;
+        assertThatThrownBy(() -> CommonValidation.validateNumbersRange(bonus))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스가_당첨번호와_같은_번호면_에러가_난다() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonus = 2;
+        assertThatThrownBy(() -> CommonValidation.validateBonusNumberDuplication(winningLotto, bonus))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
