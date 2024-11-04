63214cde21d4a7e04996d93ecf455ec9abcde837
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-11-04 13:23:12 +0900

    refactor : (,) 구분자 상수형 변수로 변경

--- src/main/java/lotto/util/LottoNumberParser.java
+++ src/main/java/lotto/util/LottoNumberParser.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class LottoNumberParser {
 
     public static final int SPLIT_LIMIT = -1;
+    private static final String DELIMITER = ",";
 
     /**
      * 문자열 당첨 번호를 구분자를 기준으로 정수형 리스트 형식으로 변경해주는 메서드
@@ -18,7 +19,7 @@ public class LottoNumberParser {
     public static List<Integer> parseLottoNumbers(String numbers){
         try{
             return new ArrayList<>(
-                            Arrays.stream(numbers.split(",", SPLIT_LIMIT))
+                            Arrays.stream(numbers.split(DELIMITER, SPLIT_LIMIT))
                             .map(Integer::parseInt)
                             .toList()
             );
