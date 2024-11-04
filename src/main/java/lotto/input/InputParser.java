fee2efa675d0691b57bc91aa55fe5bf2f6d7a6bb
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 22:00:30 +0900

    refactor(inputparser): 당첨금액 예외처리 로직 적용
    - 당첨금액의 유효성 검사를 위한 예외 처리를 추가했습니다.

--- src/main/java/lotto/input/InputParser.java
+++ src/main/java/lotto/input/InputParser.java
@@ -1,13 +1,19 @@
 package lotto.input;
 
+import lotto.validation.Validation;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class InputParser {
-    public List<Integer> splitWinningNumbers(String winningNumbers) {
-        return Arrays.stream(winningNumbers.split(","))
+    public List<Integer> splitWinningNumbers(String winningNumber) {
+        List<Integer> winningNumbers = Arrays.stream(winningNumber.split(","))
                 .map(Integer::parseInt)
                 .toList();
+        Validation.validatedWinnigNumbers(winningNumbers);
+
+
+        return winningNumbers;
     }
 }
