c395c6946ba3603e7c2ff2a49bb50e393913a58c
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-04 20:33:06 +0900

    feat: Lotto 예외처리 (입력 받은 당첨 번호에 중복된 숫자가 있을 때, 입력 받은 숫자가 6개 미만이거나 6개 초과일 때, 로또 번호가 1~45 사이의 숫자가 아닐 때)

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,29 +1,45 @@
 package lotto.model;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_NUMBERS_COUNT = 6;
+    private static final int LOTTO_START = 1;
+    private static final int LOTTO_END = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        Collections.sort(numbers); // 오름차순 정렬
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateLength(numbers);
+        validateRange(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void validateRange(List<Integer> numbers) {
         for (int number : numbers) {
-            if (number < 1 || number > 45) {
+            if (number < LOTTO_START || number > LOTTO_END) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
 
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
     public List<Integer> getNumbers() {
-        return numbers;
+        return this.numbers;
     }
 }
