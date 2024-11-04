68600d2bf278a3b7a478bbc0bc355d5bae995ba1
josephuk77 <josephuk77@gmail.com>
2024-11-04 16:11:21 +0900

    feat(ranking): 등수를 저장하는 객체를 enum으로 생성
    등수와 상금은 항상 동일하기 때문에 enum을 사용해서 구현했습니다.

---
+++ src/main/java/lotto/model/Ranking.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+public enum Ranking {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchNumber;
+    private final boolean isBonusMatched;
+    private final int prize;
+    private int count = 0; // 당첨 횟수 저장 필드
+
+    Ranking(int matchNumber, boolean isBonusMatched, int prize) {
+        this.matchNumber = matchNumber;
+        this.isBonusMatched = isBonusMatched;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+
+    public void resetCount() {
+        this.count = 0;
+    }
+}
