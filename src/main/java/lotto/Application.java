ef705e1e284efc204318a65fe66d457f8b36853f
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-11-04 22:39:21 +0900

    feat(Application) : Application 작성
    controller 생성 및 run 메소드 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+
+import lotto.controller.LottoMachineController;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMaker;
+import lotto.domain.LottoMatcher;
+import lotto.domain.WinningNumberMaker;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoMaker lottoMaker = new LottoMaker();
+        WinningNumberMaker winningNumberMaker = new WinningNumberMaker();
+        LottoMatcher lottoMatcher = new LottoMatcher();
+        LottoMachineController lottoMachineController = new LottoMachineController(lottoMaker, winningNumberMaker, lottoMatcher);
+        lottoMachineController.run();
+
     }
 }
