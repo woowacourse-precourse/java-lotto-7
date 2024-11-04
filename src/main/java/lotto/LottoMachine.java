8eba364a3c9fbb68fe8f06f0afebd65a45204bde
seyeonsim <seyeonsimw@gmail.com>
2024-11-04 23:45:40 +0900

    feat: 로또 번호 생성 구현

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    public static final int LOTTO_PRICE = 1000;
+
+    public List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+}
