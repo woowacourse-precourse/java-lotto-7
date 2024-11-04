1b93b673c84facef5c75f94f8f794d64bc846c52
kim-se-hee <saree98@naver.com>
2024-11-04 14:57:40 +0900

    feat(LottoPlace): 로또 순위를 나태니는 enum 구현

--- src/main/java/lotto/LottoPlace.java
+++ src/main/java/lotto/LottoPlace.java
@@ -1,17 +1,67 @@
-public enum LottoPlace {
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FORTH(50000),
-    FIFTH(5000);
+package lotto;
 
-    private int winningMoney;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-    LottoPlace(int winningMoney) {
+public enum LottoPlace implements Comparable<LottoPlace> {
+    FIRST(
+            2000000000,
+            "6개 일치 (2,000,000,000원) - "
+    ),
+
+    SECOND(
+            30000000,
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+    ),
+
+    THIRD(
+            1500000,
+            "5개 일치 (1,500,000원) - "
+    ),
+
+    FORTH(
+            50000,
+            "4개 일치 (50,000원) - "
+    ),
+
+    FIFTH(
+            5000,
+            "3개 일치 (5,000원) - "
+    ),
+
+    LOSE(
+            0,
+            "당첨되지 않았습니다."
+    );
+
+    public static final String WINNING__NUMBER_FORMAT = "#,###";
+    private final int winningMoney;
+    private final String summaryComment;
+
+    LottoPlace(int winningMoney, String summaryComment) {
         this.winningMoney = winningMoney;
+        this.summaryComment = summaryComment;
+    }
+
+    public static List<LottoPlace> getWinningPlaces() {
+        return Arrays.stream(values())
+                .filter(lottoPlace -> lottoPlace.winningMoney > 0)
+                .sorted(Collections.reverseOrder())
+                .toList();
     }
 
     public int getWinningMoney() {
         return winningMoney;
+    }
+
+    public String provideSummaryComment(int numberOfTicket) {
+        return this.summaryComment + numberOfTicket + "개";
+    }
+
+    public String getFormattedWinningMoney() {
+        DecimalFormat formatter = new DecimalFormat(WINNING__NUMBER_FORMAT);
+        return formatter.format(winningMoney);
     }
 }
