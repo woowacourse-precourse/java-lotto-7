1b9db5752aa767a09b720c874ea4d1813ae67b96
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-11-04 20:00:58 +0900

    feat: 로또 구입 금액 입력 및 구매 수량을 구하는 기능 구현
    - 구입 금액을 입력 받은 다음, 1000으로 나눈 값을 lottoCount(구매 수량) 변수에 저장
    - 입력 받은 값이 숫자가 아닌 경우 IllegalArgumentException 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            int lottoCount = Integer.parseInt(Console.readLine()) / 1000;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자로 입력해야 합니다.");
+        }
     }
 }
