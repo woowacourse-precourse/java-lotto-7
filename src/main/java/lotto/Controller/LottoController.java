f023fed2fc8c56d98ebaae9baf534971f5b64cc6
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-04 23:03:16 +0900

    feat(LottoController): startLotto 메서드 추가
    - 문자열 입력 시 IllegalArgumentException 발생하면 다시 입력을 받습니다.
    
    - Controller와 Service의 개념의 이해가 부족했던 것 같습니다.

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,4 +1,71 @@
 package lotto.Controller;
 
+import static lotto.Utils.Convert.convertBonusNumberStringToInt;
+import static lotto.Utils.Convert.convertInputPurchaseStringToInt;
+import static lotto.Utils.Convert.convertStringToList;
+
+import java.util.List;
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoPublication;
+import lotto.Domain.LottoWinnings;
+import lotto.Service.LottoService;
+import lotto.View.InputView;
+import lotto.View.OutputView;
+
 public class LottoController {
+
+    public void startLotto() {
+        int lottoPurchase = getValidPurchaseInput();
+        LottoPublication lottoPublication = new LottoPublication(lottoPurchase);
+        OutputView.printLotto(lottoPublication.totalLotto(), lottoPublication.theNumberOfLotto());
+
+        Lotto inputLotto = getValidLottoNumberInput();
+
+        int bonusNumber = getValidBonusInput();
+        LottoWinnings lottoWinnings = new LottoWinnings(inputLotto.getNumbers(), bonusNumber,
+                lottoPublication.totalLotto());
+
+        OutputView.printWinningStatistics(lottoWinnings.findMatchNumberCount());
+
+        LottoService lottoService = new LottoService(lottoWinnings);
+        OutputView.printEarningRate(lottoService.earningRate(lottoPurchase));
+    }
+
+    public static int getValidPurchaseInput() {
+        while (true) {
+            String inputPurchase = InputView.lottoPurchase();
+
+            try {
+                return convertInputPurchaseStringToInt(inputPurchase);
+            } catch (IllegalArgumentException e) {
+                System.out.println("오류: " + e.getMessage() + " 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    public static int getValidBonusInput() {
+        while (true) {
+            String inputBonus = InputView.bonusNumber();
+
+            try {
+                return convertBonusNumberStringToInt(inputBonus);
+            } catch (IllegalArgumentException e) {
+                System.out.println("오류: " + e.getMessage() + " 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    public Lotto getValidLottoNumberInput() {
+
+        while (true) {
+            String inputLottoNum = InputView.lottoCastLotsLot();
+
+            try {
+                List<Integer> numbers = convertStringToList(inputLottoNum);
+                return new Lotto(numbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println("오류: " + e.getMessage() + " 다시 입력해 주세요.");
+            }
+        }
+    }
 }
