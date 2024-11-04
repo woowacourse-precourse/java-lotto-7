8a370eba16a04be91807c77ab08fef0345af72dd
woong2e <tlswodnd12345@naver.com>
2024-11-04 19:23:55 +0900

    feat: add exception about input value range

--- src/main/java/lotto/io/InputReader.java
+++ src/main/java/lotto/io/InputReader.java
@@ -7,7 +7,11 @@ public class InputReader {
     public int readPurchaseAmount() {
         try {
             System.out.println("구입금액을 입력해 주세요.");
-            return Integer.parseInt(Console.readLine());
+            long input = Long.parseLong(Console.readLine());
+            if (input > Integer.MAX_VALUE) {
+                throw new IllegalArgumentException("[ERROR] 구매할 수 있는 최대 금액을 초과했습니다.");
+            }
+            return (int) input;
         } catch (NumberFormatException nfe) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력하지 않았습니다.");
         }
