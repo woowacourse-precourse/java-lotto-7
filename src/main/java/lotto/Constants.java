af8fd32d27075cf5786cfed653442abd0ff7d00e
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 19:56:37 +0900

    refactor(OutputView): ì 당첨결과 출력 기능 수정

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -6,6 +6,15 @@ public class Constants {
     public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
+    public static final String OUTPUT_TOTAL_RESULT = "당첨통계" + "\n" + "---";
+    public static final String OUTPUT_BUY_COUNT = "개를 구매했습니다.";
+    public static final String OUTPUT_MATCHING_THREE = "3개 일치 (5,000원) - %d개";
+    public static final String OUTPUT_MATCHING_FOUR = "4개 일치 (50,000원) - %d개";
+    public static final String OUTPUT_MATCHING_FIVE_AND_BONUS = "5개 일치 (1,500,000원) - %d개";
+    public static final String OUTPUT_MATCHING_FIVE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String OUTPUT_MATCHING_SIX = "6개 일치 (2,000,000,000원) - %d개";
+    public static final String OUTPUT_TOTAL_RATE = "총 수익률은 %.1f%%입니다.\n";
+
     public static final Integer MIN_NUMBER = 1;
     public static final Integer MAX_NUMBER = 45;
     public static final Integer PICK_NUMBER = 6;
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,26 +1,41 @@
 package lotto.view;
 
+import lotto.Constants;
 import lotto.model.Lotto;
 import lotto.model.LottoResult;
 import lotto.model.Money;
+import lotto.model.Ranking;
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static lotto.Constants.*;
 
 public class OutputView {
 
-    public static void printBuyLottoCount(Money money){
-        System.out.printf(("%d개를 구매했습니다.") + "%n", money.getLottoCount());
+
+
+    public static void printBuyLottoCount(int count){
+        System.out.println(count + OUTPUT_BUY_COUNT);
     }
 
     public static void printLottoList(List<Lotto> lottos){
-        lottos.getLotto().forEach(System.out::println);
-        System.out.println();
+        lottos.forEach(lotto -> System.out.println(lotto.toString()));
     }
 
     public static void printLottResult(LottoResult result){
-        System.out.println("당첨통계");
-        System.out.println("---");
+        System.out.println(OUTPUT_TOTAL_RESULT);
+
+        System.out.println(String.format(OUTPUT_MATCHING_THREE, result.getCountByRank(Ranking.FIFTH)));
+        System.out.println(String.format(OUTPUT_MATCHING_FOUR, result.getCountByRank(Ranking.FOURTH)));
+        System.out.println(String.format(OUTPUT_MATCHING_FIVE_AND_BONUS, result.getCountByRank(Ranking.THIRD)));
+        System.out.println(String.format(OUTPUT_MATCHING_FIVE, result.getCountByRank(Ranking.SECOND)));
+        System.out.println(String.format(OUTPUT_MATCHING_SIX, result.getCountByRank(Ranking.FIRST)));
     }
 
+
+    public void printRate(double rate){
+        System.out.printf(OUTPUT_TOTAL_RATE, rate);
+    }
 }
