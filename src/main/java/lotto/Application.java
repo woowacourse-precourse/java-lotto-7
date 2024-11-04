1762f8b2e529d3176e9dd529d3580e0275a5b614
SuJeong <sujenghong@gmail.com>
2024-11-04 20:45:09 +0900

    feat: add lotto number generation with unique, sorted numbers

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static Lotto generateLotto() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        numbers.sort(Integer::compareTo); // 번호 오름차순 정렬
+        return new Lotto(numbers);
     }
 }
