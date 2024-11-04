0ae66c95901343773aecbdf267f295044f604dbd
yushinc <cys74351@gmail.com>
2024-11-04 22:14:14 +0900

    refactor(Application): LottoGenerator 클래스 삭제 후 객체 생성 로직 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,10 @@ package lotto;
 
 import lotto.controller.GameController;
 import lotto.service.GameService;
-import lotto.util.LottoGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        GameService service = new GameService(lottoGenerator);
+        GameService service = new GameService();
         GameController controller = new GameController(service);
         controller.run();
     }
