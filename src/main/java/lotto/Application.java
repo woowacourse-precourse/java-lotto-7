545925b37a4bdfc1a237db9e4f5d861c6584678e
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-04 20:46:16 +0900

    feat: 구매금액 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int money = getInputMoney();
     }
+
+    public static int getInputMoney() {
+        while (true) {
+            System.out.println("구입금액을 입력해주세요.");
+            try {
+                int money = Integer.parseInt(Console.readLine());
+                validateInputMoney(money);
+                return money;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+    }
+
+    public static void validateInputMoney(int money) {
+        if (money % LOTTO_PRICE != 0 || money <= 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
+        }
+    }
+
+
 }
