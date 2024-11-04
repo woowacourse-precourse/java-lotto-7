e00687c2f3183b89fb851627c1a16ce2706782c8
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 10:18:27 +0900

    feat(InputView): 당첨 번호 및 보너스 번호 입력 기능 구현
    - 당첨 번호를 입력받습니다.
    - 보너스 번호를 입력받습니다.
    - 당첨 번호는 Lotto 객체로 받습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,28 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import lotto.model.Lotto;
 
 public class InputView {
     public String inputCost() {
         return Console.readLine();
+    }
+
+    public Lotto inputWinningLottoNumbers() {
+        String input = Console.readLine();
+        List<Integer> winningLottoNumbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .sorted()
+                .toList();
+        return new Lotto(winningLottoNumbers);
+    }
+
+    public int bonusWinningLottoNumber() {
+        String input = Console.readLine();
+        Console.close();
+        return Integer.parseInt(input);
     }
 }
