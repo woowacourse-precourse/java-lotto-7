51d3178c5498f164413bff2ccbd7185149926579
in seong Park <123macanic@naver.com>
2024-11-04 23:43:01 +0900

    feat: 전체 흐름 진행 로직 구현
    전체 프로그램 실행을 위해 다음과 같이 코드를 구현
    
    추가된 기능:
    - 위의 기능을 구현하기 위해 Application 로직을 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,57 @@
 package lotto;
 
+import lotto.dto.UserInputDTO;
+import lotto.dto.WinningResultDTO;
+import lotto.input.BonusNumberInput;
+import lotto.input.CloseInput;
+import lotto.input.LottoPurchaseAmountInput;
+import lotto.input.WinningNumbersInput;
+import lotto.input.matcher.LottoNumbersMatcher;
+import lotto.input.producer.AllLottoNumbersProducer;
+import lotto.input.validator.BonusNumberValidator;
+import lotto.input.validator.LottoPurchaseAmountValidator;
+import lotto.input.validator.WinningNumbersValidator;
+import lotto.model.Lotto;
+import lotto.output.WinningStatisticsOutput;
+import lotto.util.WinningCalculator;
+
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoPurchaseAmountInput lottoPurchaseAmountInput =
+                new LottoPurchaseAmountInput(new LottoPurchaseAmountValidator());
+        WinningNumbersInput winningNumbersInput =
+                new WinningNumbersInput(new WinningNumbersValidator());
+        BonusNumberInput bonusNumberInput =
+                new BonusNumberInput(new BonusNumberValidator());
+
+        LottoNumbersMatcher lottoNumbersMatcher =
+                new LottoNumbersMatcher();
+        AllLottoNumbersProducer allLottoNumbersProducer =
+                new AllLottoNumbersProducer();
+        WinningCalculator winningCalculator =
+                new WinningCalculator(lottoNumbersMatcher);
+
+        WinningStatisticsOutput winningStatisticsOutput =
+                new WinningStatisticsOutput();
+
+
+
+        Integer purchaseAmount = lottoPurchaseAmountInput.run();
+
+        List<Lotto> allLottoNumbers = allLottoNumbersProducer.getAllLottoNumbersByCount(purchaseAmount / 1000);
+
+        Lotto lotto = winningNumbersInput.run();
+        Integer bonusNumber = bonusNumberInput.run();
+        CloseInput.run();
+
+        UserInputDTO userInputDTO = new UserInputDTO(lotto, bonusNumber);
+
+        WinningResultDTO winningResultDTO =
+                winningCalculator.getWinningResult(userInputDTO, allLottoNumbers);
+
+        winningStatisticsOutput.run(winningResultDTO, purchaseAmount);
     }
 }
