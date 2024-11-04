505fe89e80ec46fa0068410383acf8c68c82f912
jinno Yun <jinno321@naver.com>
2024-11-04 18:46:32 +0900

    로또 당첨 번호 경계값 검증 메서드 작성

--- src/main/java/lotto/util/Validation.java
+++ src/main/java/lotto/util/Validation.java
@@ -3,7 +3,13 @@ package lotto.util;
 public class Validation {
     public static void validateWinningLength(int length) {
         if (length != 6){
-            throw new IllegalArgumentException("[ERROR] 이동 횟수가 올바르지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 6개여야 합니다.");
+        }
+    }
+
+    public static void validateNumBoundary(int number){
+        if (number < 1 || number > 45){
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 1에서 45 사이여야 합니다.");
         }
     }
 }
