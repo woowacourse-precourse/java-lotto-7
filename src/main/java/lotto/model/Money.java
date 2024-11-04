47eae856e27930bb7ed6f8c74e55fb6fc47daa71
woojin cheon <qpqpqp0102@naver.com>
2024-11-04 22:42:35 +0900

    feat(Money): 구입 금액을 관리하는 클래스 작성

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import lotto.enums.ErrorMessage;
+
+public class Money {
+
+    private static final int LOTTO_PRICE = 1000;
+    private final int purchasingMoney;
+
+    public Money(int purchasingMoney) {
+        validateAmount(purchasingMoney);
+        this.purchasingMoney = purchasingMoney;
+    }
+
+    private void validateAmount(int purchasingAmount) {
+        if (purchasingAmount > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(
+                "[ERROR] " + ErrorMessage.EXCEED_MONEY_AMOUNT.getMessage());
+        }
+        if (purchasingAmount <= 0 || purchasingAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(
+                "[ERROR] " + ErrorMessage.INVALID_PURCHASING_AMOUNT.getMessage());
+        }
+
+    }
+
+    public int getPurchasingMoney() {
+        return purchasingMoney;
+    }
+
+    public int getLottoCount() {
+        return purchasingMoney / LOTTO_PRICE;
+    }
+}
