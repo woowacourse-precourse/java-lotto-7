4e9b4f7577d2e3a3ddfae61def5c084ae0b1daee
Timothy <timothy@jungnanum.com>
2024-11-04 22:33:07 +0900

    feat: MVC 모델의 View를 이용하여, 추첨자의 입력값 처리
    - outputView로 로또 구입 금액에 따른 구매 내역 출력 : 발행한 로또 수량 및 번호를 출력한다. 로또 번호는 오름차순으로 정렬하여 보여준다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,17 @@
 package lotto.view;
 
+import java.util.List;
 import lotto.enums.UserInterfaceMessage;
+import lotto.repository.Lotto;
 
 public class OutputView {
     public void printPublishCount(int size) {
         System.out.printf(UserInterfaceMessage.COUNT_BOUGHT.getValue() + "\n", size);
+    }
+
+    public void printPublishedLottoNumbers(List<Lotto> publishedLottoNumbers) {
+        for (Lotto publishedLotto : publishedLottoNumbers) {
+            System.out.println(publishedLotto.getNumbers());
+        }
     }
 }
