d0322bcb61c5ae9582dc5fdff9493521400dcd50
eunseobb <seobseob0810@naver.com>
2024-11-04 19:54:19 +0900

    feat(view): 결과 출력 메서드 추가
    당첨 금액, 개수와 수익률을 출력하는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,9 @@ package lotto.view;
 import lotto.model.Lotto;
 
 import java.util.List;
+import java.util.Map;
+
+import static lotto.model.LottoService.BONUS_KEY;
 
 public class OutputView {
     public void printLottoCount(int lottoCount) {
@@ -11,6 +14,27 @@ public class OutputView {
 
     public void printLottoNumbers(List<Lotto> lottos) {
         lottos.stream().map(Lotto::getNumbers).forEach(System.out::println);
+    }
+
+    public void printResult(Map<Integer, Integer> result, Long rateOfReturn) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        result.forEach((key, value) -> System.out.printf("%d개 일치 (%,d원) - %d개%n", key, getPrizeAmount(key), value));
+
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", rateOfReturn);
+
+    }
+
+    private long getPrizeAmount(int matchCount) {
+        return switch (matchCount) {
+            case 3 -> 5000;
+            case 4 -> 50000;
+            case 5 -> 1500000;
+            case BONUS_KEY -> 30000000; // 5개 + 보너스
+            case 6 -> 2000000000;
+            default -> 0;
+        };
     }
 
 }
