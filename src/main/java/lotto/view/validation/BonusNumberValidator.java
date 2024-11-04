bc5fe93004b76e6fde76f64033ad479412c3c3b9
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:18:58 +0900

    Feat: BonusNumberValidator 에 보너스 번호가 1 이상 45 이하가 아닌지 검증 구현

--- src/main/java/lotto/view/validation/BonusNumberValidator.java
+++ src/main/java/lotto/view/validation/BonusNumberValidator.java
@@ -1,15 +1,25 @@
 package lotto.view.validation;
 
 import static lotto.service.exception.LottoExceptionMessage.BONUS_NUMBER_DUPLICATED;
+import static lotto.service.exception.LottoExceptionMessage.INVALID_BONUS_NUMBER;
 
 import lotto.domain.dto.WinningNumbersDto;
 import lotto.service.exception.LottoException;
 
 public class BonusNumberValidator {
 
+    private static final String BONUS_NUMBER_REGEX = "^(?:[1-9]|[1-3][0-9]|4[0-5])$";
+
     public static int validate(WinningNumbersDto winningNumbersDto, String bonusNumber) {
+        validateBonusNumber(bonusNumber);
         validateWinningNumbersContainsBonusNumber(winningNumbersDto, bonusNumber);
         return Integer.parseInt(bonusNumber);
+    }
+
+    private static void validateBonusNumber(String bonusNumber) {
+        if (!bonusNumber.matches(BONUS_NUMBER_REGEX)) {
+            throw new LottoException(INVALID_BONUS_NUMBER);
+        }
     }
 
     private static void validateWinningNumbersContainsBonusNumber(WinningNumbersDto winningNumbersDto, String bonusNumber) {
@@ -20,4 +30,6 @@ public class BonusNumberValidator {
                     throw new LottoException(BONUS_NUMBER_DUPLICATED);
                 });
     }
+
+
 }
