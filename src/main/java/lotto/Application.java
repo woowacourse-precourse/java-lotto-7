6a03fa402ed3a85187f3cb1fc9f1992cd027198c
thsghdud13 <thsgdud25@naver.com>
2024-11-04 21:56:35 +0900

    feat: main 함수에서 실행되도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        appConfig.lottoGameController().run();
+        Console.close();
     }
 }
