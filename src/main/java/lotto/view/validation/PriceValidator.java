411fb3991068849c3ea5900f0df2ea339a84ec52
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:49:27 +0900

    Feat: PriceValidator 에 가격이 정수형 최대값을 초과할 경우 검증 구현

--- src/main/java/lotto/view/validation/PriceValidator.java
+++ src/main/java/lotto/view/validation/PriceValidator.java
@@ -1,6 +1,7 @@
 package lotto.view.validation;
 
 import static lotto.service.exception.LottoExceptionMessage.PRICE_NOT_POSITIVE_INTEGER;
+import static lotto.service.exception.LottoExceptionMessage.PRICE_OVERFLOW;
 
 import lotto.service.exception.LottoException;
 
@@ -10,12 +11,21 @@ public class PriceValidator {
 
     public static int validate(String price) {
         validatePositiveInteger(price);
+        validateOverflow(price);
         return Integer.parseInt(price);
     }
 
     private static void validatePositiveInteger(String price) {
         if (!price.matches(POSITIVE_INTEGER_REGEX)) {
             throw new LottoException(PRICE_NOT_POSITIVE_INTEGER);
+        }
+    }
+
+    private static void validateOverflow(String price) {
+        try {
+            Integer.parseInt(price);
+        } catch (NumberFormatException e) {
+            throw new LottoException(PRICE_OVERFLOW);
         }
     }
 }
