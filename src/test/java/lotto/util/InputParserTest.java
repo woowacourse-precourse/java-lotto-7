fe2e60c3c4290ca299b25c0326a028e11e12f491
sanghee0820 <lsh277604@gmail.com>
2024-11-04 15:05:10 +0900

    test: winningNumber 파싱 테스트 추가
    1. input이 비어있을 경우
    2. input이 null일 경우
    3. input 형식이 숫자형식이 아닐경우

--- src/test/java/lotto/util/InputParserTest.java
+++ src/test/java/lotto/util/InputParserTest.java
@@ -3,6 +3,7 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import lotto.exception.InputErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,5 +59,55 @@ class InputParserTest {
 
         // then
         assertThat(result).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력값이 비어있으면 IllegalArgumentException 발생")
+    void parseWinningNumbers_EmptyInput_ExceptionThrown() {
+        // given
+        String input = "";
+
+        // when, then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputErrorMessage.INPUT_IS_EMPTY.getMessage());
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력값이 null이면 IllegalArgumentException 발생")
+    void parseWinningNumbers_NullInput_ExceptionThrown() {
+        // given
+        String input = null;
+
+        // when, then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputErrorMessage.INPUT_IS_EMPTY.getMessage());
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력값이 숫자가 아니면 IllegalArgumentException 발생")
+    void parseWinningNumbers_NotNumberInput_ExceptionThrown() {
+        // given
+        String input = "1,2,3,4,a";
+
+        // when, then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage(InputErrorMessage.INPUT_IS_NOT_NUMBER.getMessage());
+    }
+
+    @ParameterizedTest(name = "당첨 번호 입력값이 {0}이면 파싱 성공")
+    @ValueSource(strings = {"1, 2, 3, 4, 5, 6", "1,2,3,4,5,6"})
+    @DisplayName("당첨 번호 입력값 파싱 성공")
+    void parseWinningNumbers_Success(String input) {
+        // given
+        final List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        var result = InputParser.parseWinningNumbers(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
