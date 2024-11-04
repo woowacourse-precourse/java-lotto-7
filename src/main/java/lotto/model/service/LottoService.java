061679a49187e4d913bd893b14c3546ea86ab62d
JUNYEON WON <wnsdus1008@gmail.com>
2024-11-04 22:18:03 +0900

    refactor: Wrap primitive types

--- src/main/java/lotto/model/service/LottoService.java
+++ src/main/java/lotto/model/service/LottoService.java
@@ -1,44 +1,25 @@
 package lotto.model.service;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.List;
-import lotto.model.domain.Lotto;
-import lotto.model.domain.LottoWinningNumbers;
+import lotto.model.domain.LottoPrizes;
+import lotto.model.domain.LottoWinning;
+import lotto.model.domain.Lottos;
 import lotto.model.domain.ProfitRatio;
 import lotto.model.domain.PurchaseAmount;
 
-public class LottoDrawService {
+public class LottoService {
 
-    public List<LottoPrize> drawWinners(List<Lotto> lottos, LottoWinningNumbers lottoWinningNumbers) {
-        List<LottoPrize> winners = new ArrayList<>();
-
-        Set<Integer> winningNumberSet = new HashSet<>(lottoWinningNumbers.getNumbers());
-
-        for (Lotto lotto : lottos) {
-            LottoPrize prize = getPrizeIfWinner(lotto, winningNumberSet, lottoWinningNumbers.getBonusNumber());
-            if (prize != null) {
-                winners.add(prize);
-            }
-        }
-        return winners;
+    public Lottos createLottos(PurchaseAmount purchaseAmount) {
+        int count = purchaseAmount.getLottoCount();
+        return new Lottos(count);
     }
 
-    public ProfitRatio calculateProfitRatio(PurchaseAmount purchaseAmount, List<LottoPrize> winners) {
-        int totalAmount = winners.stream()
-                .mapToInt(LottoPrize::getPrize)
-                .sum();
-        return new ProfitRatio(totalAmount, purchaseAmount.get());
+    public LottoPrizes drawWinners(Lottos lottos, LottoWinning lottoWinning) {
+        return lottoWinning.getLottoPrizes(lottos);
     }
 
-
-    private LottoPrize getPrizeIfWinner(Lotto lotto, Set<Integer> winningNumberSet, int bonusNumber) {
-        Set<Integer> lottoNumberSet = new HashSet<>(lotto.getNumbers());
-        boolean isBonusCorrect = lottoNumberSet.contains(bonusNumber);
-        lottoNumberSet.retainAll(winningNumberSet);
-
-        return LottoPrize.findBy(lottoNumberSet.size(), isBonusCorrect);
+    public ProfitRatio getProfitRatio(PurchaseAmount purchaseAmount, LottoPrizes winners) {
+        return winners.getProfitRatio(purchaseAmount.get());
     }
+
 
 }
