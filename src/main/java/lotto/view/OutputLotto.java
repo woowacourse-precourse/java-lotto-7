c590de9fc4c2401054ca8642a2baa11911e8d530
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 19:56:06 +0900

    feat(view): OutputLotto클래스 추가 -구매한 로또 티켓 양을 출력합니다.

---
+++ src/main/java/lotto/view/OutputLotto.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputLotto {
+
+  private static final String MESSAGE_PURCHASED_TICKETS = "%d개를 구매했습니다.";
+
+  public static void printPurchasedTickets(List<Lotto> tickets) {
+    System.out.println(String.format(MESSAGE_PURCHASED_TICKETS, tickets.size()));
+    tickets.forEach(ticket -> System.out.println(ticket.getNumbers()));
+  }
+}
