9da44fc403cb487a92a9a2d4834da2bbf7bd58e1
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:46:45 +0900

    Feat: 가격이 양의 정수가 아닐 경우 출력할 예외 메시지 구현

--- src/main/java/lotto/service/exception/LottoExceptionMessage.java
+++ src/main/java/lotto/service/exception/LottoExceptionMessage.java
@@ -6,6 +6,7 @@ public enum LottoExceptionMessage implements ExceptionMessage {
 
     // 가격
     PRICE_NOT_THOUSANDS_UNIT("[ERROR] 가격은 1,000원 단위로 입력해야 합니다."),
+    PRICE_NOT_POSITIVE_INTEGER("[ERROR] 가격은 양의 정수이어야 합니다."),
     // 당첨 번호
     INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 1 이상 45 이하의 숫자 6개를 콤마로 구분하여 입력해야 합니다."),
     // 보너스 번호
