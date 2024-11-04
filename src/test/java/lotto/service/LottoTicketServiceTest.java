e57de231a95a717e6dcc0cb9af33372b613ebb58
MinJae00 <exceedmin@gmail.com>
2024-11-04 17:22:08 +0900

    feat: 로또 티켓 발급 서비스 테스트 코드, 구입 금액에 대한 예외처리, 장 수 만큼 생성된 로또를 잘 받아 오는지 확인

--- src/test/java/lotto/service/LottoTicketServiceTest.java
+++ src/test/java/lotto/service/LottoTicketServiceTest.java
@@ -1,4 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.service;
+
+import lotto.constant.LottoValueConstant;
+import lotto.reposi.InMemoryLottoRepository;
+import lotto.reposi.LottoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class LottoTicketServiceTest {
-  
+
+    private LottoTicketService lottoTicketService;
+    private LottoRepository lottoRepository;
+
+    @BeforeEach
+    void setUp() {
+        lottoTicketService = new LottoTicketService();
+        lottoRepository = new InMemoryLottoRepository();
+    }
+
+    @Test
+    void purchaseLottoTickets_validPrice_returnsTicketCount() {
+        int price = 10000;
+        int expectedTickets = price / LottoValueConstant.LOTTO_PRICE;
+
+        int actualTickets = lottoTicketService.purchaseLottoTickets(price);
+
+        assertEquals(expectedTickets, actualTickets);
+    }
+
+    @Test
+    void generateLottoNumbers_generatesAndSavesTickets() {
+        int numberOfLottos = 5;
+
+        lottoTicketService.generateLottoNumbers(numberOfLottos, lottoRepository);
+
+        List<List<Integer>> savedLottoNumbers = lottoRepository.getLottoNumbers();
+
+        assertEquals(numberOfLottos, savedLottoNumbers.size());
+
+        for (List<Integer> lottoNumbers : savedLottoNumbers) {
+            for (int i = 1; i < lottoNumbers.size(); i++) {
+                assert(lottoNumbers.get(i - 1) <= lottoNumbers.get(i));
+            }
+        }
+    }
+
+    @Test
+    void getLottoNumbers_retrievesLottoNumbersFromRepository() {
+        int numberOfLottos = 3;
+
+        lottoTicketService.generateLottoNumbers(numberOfLottos, lottoRepository);
+
+        List<List<Integer>> retrievedLottoNumbers = lottoTicketService.getLottoNumbers(lottoRepository);
+
+        assertEquals(lottoRepository.getLottoNumbers(), retrievedLottoNumbers);
+    }
 }
