6a2b7dcea55d8a610d74b84ef61cf279f1942ba0
yush <yush2202@gmail.com>
2024-11-04 19:46:18 +0900

    test($ LottoBowlTest): 로또 발급 서비스 테스트
    정확한 갯수의 로또를 발급하는지 테스트

---
+++ src/test/java/lotto/services/LottoBowlTest.java
@@ -0,0 +1,26 @@
+package lotto.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Ticket;
+import lotto.service.LottoBowl;
+import lotto.vo.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoBowlTest {
+
+    @DisplayName("티켓 숫자와 알맞는 사이즈의 로또 묶음을 반환하는지 테스트합니다.")
+    @Test
+    void 로또_갯수_테스트() {
+        Money testMoney = new Money(6000);
+        Ticket testTicket = Ticket.from(testMoney);
+
+        LottoBowl testLottoBowl = LottoBowl.from(testTicket);
+        List<Lotto> testLotties = testLottoBowl.publishLotties().getLotties();
+
+        assertThat(testLotties.size()).isEqualTo(6);
+    }
+}
