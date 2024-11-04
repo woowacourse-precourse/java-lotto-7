69e0e79eb5689f41598798b8a948ae3a8851d5f7
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-11-04 20:46:33 +0900

    feat(test): 수익률 테스트 추가

--- src/test/java/lotto/domain/LottoManagerTest.java
+++ src/test/java/lotto/domain/LottoManagerTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -75,6 +77,32 @@ class LottoManagerTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
 
+    @Test
+    void 수익률_계산_성공() {
+        // given
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(7, 8, 9, 10, 11, 12))
+        );
+        WinnerLotto winnerLotto = new WinnerLotto(List.of(1, 2, 3, 4, 5, 6)).addBonusNumber("7");
+        LottoManager lottoManager = new LottoManager(lottos).withWinningLotto(winnerLotto);
+
+        // when
+        double profitRate = lottoManager.getProfitRate();
+
+        // then
+        assertThat(profitRate).isEqualTo(100000000.0);
+    }
+
+    @Test
+    void 로또_구입금액만큼_로또_발생_성공() {
+        // given
+        int purchaseAmount = 3000;
+        LottoManager lottoManager = LottoManager.from(purchaseAmount);
+
+        // then
+        assertThat(lottoManager.getPublishedLottos()).hasSize(3);
     }
 }
