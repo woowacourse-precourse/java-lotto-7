328d99ce9103f274eedcd22bc59651bd390e07ba
ilwoo <53050413+Oilwoo@users.noreply.github.com>
2024-11-04 11:27:19 +0900

    refactor: change creator

--- src/main/java/lotto/LottoPrize.java
+++ src/main/java/lotto/LottoPrize.java
@@ -7,13 +7,13 @@ public class LottoPrize {
     private final int bonusNumber;
 
     public LottoPrize(List<Integer> numbers, Integer bonusNumber) {
+        validate(numbers, bonusNumber);
         this.lotto = new Lotto(numbers);
-        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
-    public void validate(int bonusNumber) {
-        if (lotto.getNumbers().contains(bonusNumber)) {
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
         }
     }
