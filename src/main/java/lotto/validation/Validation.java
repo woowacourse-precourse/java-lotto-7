69d5b78474d5ba181fdf094f98fb71e59eeb62d0
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 21:58:24 +0900

    feat(validation): 당첨 번호 유효성 검사 로직 추가
    - 당첨 번호 유효성 검사를 위한 메서드를 추가했습니다.
    - 당첨 번호가 1에서 45 사이의 범위를 벗어나는 경우 예외가 발생합니다.
    - 당첨 번호가 5개의 쉼포료 구분되지 않는 경우 예외가 발생합니다.
    - 문자열이 포함되는 경우 예외가 발생합니다.
    - 당첨 번호가 6개가 아닌 경우 예외가 발생합니다.

--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -1,5 +1,9 @@
 package lotto.validation;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validation {
     public static void validatedPurchaseAmount(String input) {
         if (input == null || input.isEmpty()) {
@@ -18,6 +22,39 @@ public class Validation {
         }
         catch (NumberFormatException e) {
             throw new IllegalArgumentException(ValidationType.NOT_NUMERIC.validationMsg());
+        }
+    }
+
+    public static void validatedWinnigNumbers(List<Integer> winningNumbers) {
+        Set<Integer> numberSet = new HashSet<>();
+        validatedWinningNumbers(winningNumbers);
+        for (Integer number : winningNumbers) {
+            outOfRangeWinningNumber(number);
+            duplicateWinningNumber(number, numberSet);
+        }
+    }
+    public static void validateWinningNumberFormat(String winningNumber) {
+        String regex ="^(\\d{1,2})(,\\d{1,2}){5}$";
+        if (!winningNumber.matches(regex)) {
+            throw new IllegalArgumentException(ValidationType.INVALID_FORMAT.validationMsg());
+        }
+    }
+
+    private static void validatedWinningNumbers(List<Integer> winningNumbers) {
+        if (!(winningNumbers.size() == 6)) {
+            throw new IllegalArgumentException(ValidationType.INVALID_COUNT.validationMsg());
+        }
+    }
+
+    private static void outOfRangeWinningNumber(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException(ValidationType.OUT_OF_RANGE.validationMsg());
+        }
+    }
+
+    private static void duplicateWinningNumber(Integer number, Set<Integer> numberSet) {
+        if (!numberSet.add(number)) {
+            throw new IllegalArgumentException(ValidationType.DUPLICATE_NUMBERS.validationMsg());
         }
     }
 }
--- src/test/java/lotto/validation/ValidationTest.java
+++ src/test/java/lotto/validation/ValidationTest.java
@@ -3,6 +3,9 @@ package lotto.validation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationTest {
@@ -37,6 +40,38 @@ class ValidationTest {
         String input = "6100";
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.validatedThousandUnitAmount(input));
         assertEquals("[ERROR]구입 금액은 1,000원 단위여야 합니다.", exception.getMessage());
+    }
+
+    @DisplayName("당첨 번호가 1에서 45 사이의 범위를 벗어나거나 중복되는 경우 예외가 발생한다.")
+    @Test
+    void 당첨번호가_1에서_45_범위를_벗어날_시_예외처리() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 48, 1);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.validatedWinnigNumbers(winningNumbers));
+        assertEquals("[ERROR]당첨 번호는 1에서 45 사이여야 합니다.", exception.getMessage());
+    }
+
+    @DisplayName("당첨 번호의 값이 중복될 시 예외가 발생한다.")
+    @Test
+    void 당첨번호_값이_중복될_시_예외처리() {
+        List<Integer> winningNumbers = Arrays.asList(1, 1, 3, 4, 42, 1);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.validatedWinnigNumbers(winningNumbers));
+        assertEquals("[ERROR]당첨 번호는 중복될 수 없습니다.", exception.getMessage());
+    }
+
+    @DisplayName("당첨 번호가 6개가 아닐 시 예외가 발생한다.")
+    @Test
+    void 당첨번호_값이_6개가_아닐_시_예외처리() {
+        List<Integer> winningNumbers = Arrays.asList(1, 3, 4, 42, 1);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.validatedWinnigNumbers(winningNumbers));
+        assertEquals("[ERROR]당첨 번호는 6개여야 합니다.", exception.getMessage());
+    }
+
+    @DisplayName("당첨 번호 5개의 쉼표로 구분되지 않는 경우 예외가 발생한다.")
+    @Test
+    void 당첨번호가_5개의_쉼표로_구분되지_않을_시_예외처리() {
+        String winningNumbers = "1,2,3,4,11";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.validateWinningNumberFormat(winningNumbers));
+        assertEquals("[ERROR]당첨 번호는 6개의 쉼표로 구분되어야 합니다.", exception.getMessage());
     }
 
 }
