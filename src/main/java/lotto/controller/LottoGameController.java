e584a3aa722783b2b4a8860e1a93f7b7ebca2d8b
Chan Mi <rkdcksal02@gmail.com>
2024-11-04 13:44:51 +0900

    feat(LottoGameController): 로또 게임 실행 제어 기능 추가

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,35 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoResult;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+import java.util.List;
+
+public class LottoGameController {
+    private final LottoInputView inputView = new LottoInputView();
+    private final LottoOutputView outputView = new LottoOutputView();
+    private final LottoMachine lottoMachine = new LottoMachine();
+
+    public void start() {
+        try {
+            int purchaseAmount = inputView.inputPurchaseAmount();
+            List<Lotto> tickets = generateTickets(purchaseAmount);
+            outputView.printTickets(tickets);
+
+            List<Integer> winningNumbers = inputView.inputWinningNumbers();
+            int bonusNumber = inputView.inputBonusNumber();
+
+            LottoResult result = lottoMachine.calculateResult(tickets, winningNumbers, bonusNumber);
+            outputView.printResult(result, purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private List<Lotto> generateTickets(int purchaseAmount) {
+        return lottoMachine.generateTickets(purchaseAmount);
+    }
+}
