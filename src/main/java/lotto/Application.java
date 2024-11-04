6b8e3a136906b4834d3e3e1139018ce532f909f3
waffle <nate.crema@gmail.com>
2024-11-04 23:11:48 +0900

    refactor(Application): 서비스흐름 유관로직 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,35 +1,14 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import lotto.domain.Lotto;
-import lotto.domain.LottoRound;
-import lotto.dto.IssuedLottoDTO;
-import lotto.dto.LottoGradingNumbersDTO;
-import lotto.dto.LottoWinStatisticDTO;
-import lotto.view.LottoInput;
-import lotto.view.LottoOutput;
-
-import java.util.List;
+import lotto.service.LottoService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int issueCount = LottoInput.getIssueCount().count();
+        LottoService lottoService = new LottoService();
 
-        LottoRound lottoRound = new LottoRound();
-        lottoRound.issueLottoes(issueCount);
-
-        LottoOutput.printIssuedLotto(new IssuedLottoDTO(lottoRound));
-
-        LottoGradingNumbersDTO gradingNumbersDTO = LottoInput.getGradingNumbers();
-
-        List<Integer> gradingLottoNumbers = gradingNumbersDTO.winNumbers();
-        int bonusLottoNumber = gradingNumbersDTO.bonusNumber();
-        lottoRound.setWinNumbers(gradingLottoNumbers, bonusLottoNumber);
-
-        List<Integer> winHistory = lottoRound.getWinHistory();
-        float profitRate = lottoRound.getProfitRate();
-
-        LottoOutput.printWinStatistic(new LottoWinStatisticDTO(winHistory, profitRate));
+        lottoService.issue();
+        lottoService.lot();
+        lottoService.scratch();
     }
 }
