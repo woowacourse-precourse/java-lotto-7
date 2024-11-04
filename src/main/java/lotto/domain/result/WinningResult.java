d57ac61e115460490aa2171d0a4c042c7929b0ee
hootzz <wjqwjql3@gmail.com>
2024-11-04 17:07:06 +0900

    refactor(result): WinningResult에서 Money 값 객체 활용하도록 개선

--- src/main/java/lotto/domain/result/WinningResult.java
+++ src/main/java/lotto/domain/result/WinningResult.java
@@ -1,27 +1,30 @@
 package lotto.domain.result;
 
+import lotto.domain.money.Money;
+
 public class WinningResult {
     private final WinningStatistics statistics;
-    private final double profitRate;
+    private final Money purchaseMoney;
 
-    private WinningResult(WinningStatistics statistics, double profitRate) {
+    private WinningResult(WinningStatistics statistics, Money purchaseMoney) {
         this.statistics = statistics;
-        this.profitRate = profitRate;
+        this.purchaseMoney = purchaseMoney;
     }
 
-    public static WinningResult of(WinningStatistics statistics, double profitRate) {
-        return new WinningResult(statistics, profitRate);
+    public static WinningResult of(WinningStatistics statistics, Money purchaseMoney) {
+        return new WinningResult(statistics, purchaseMoney);
     }
 
-    public WinningStatistics getStatistics() {
-        return statistics;
-    }
-
-    public double getProfitRate() {
-        return profitRate;
+    public double calculateProfitRate() {
+        Money totalPrize = statistics.calculateTotalPrize();
+        return purchaseMoney.calculateProfitRate(totalPrize);
     }
 
     public int getWinningCount(LottoRank rank) {
         return statistics.getWinningCount(rank);
+    }
+
+    public WinningStatistics getStatistics() {
+        return statistics;
     }
 }
