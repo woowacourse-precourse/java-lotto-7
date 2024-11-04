89b5e14981dc441fdf65c0a1b126d17b717596fb
pil-go <pilbongx@naver.com>
2024-11-04 20:23:44 +0900

    feat(WinningAnalyzerTest): 당첨 결과 확인 테스트 추가

--- src/main/java/lotto/Domain/Lottos.java
+++ src/main/java/lotto/Domain/Lottos.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Lottos {
     private final List<Lotto> lottos;
 
-    private Lottos(List<Lotto> lottos) {
+    public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
