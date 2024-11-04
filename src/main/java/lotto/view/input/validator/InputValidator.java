c99b83fc35231376e13e95d8536bc47c9629e929
EHCTH <lees90896@gmail.com>
2024-11-04 13:14:39 +0900

    style(InputValidator): RegexPattern 메서드명 변경 * style: match -> unMatch

--- src/main/java/lotto/view/input/validator/InputValidator.java
+++ src/main/java/lotto/view/input/validator/InputValidator.java
@@ -4,12 +4,12 @@ import lotto.constant.ErrorMessage;
 
 public class InputValidator {
     public static void moneyValidate(String input) {
-        if (!RegexPattern.NUMBER_REGEX.match(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
+        if (RegexPattern.NUMBER_REGEX.unMatch(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
     }
     public static void bonusNumberValidate(String input) {
-        if (!RegexPattern.NUMBER_REGEX.match(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
+        if (RegexPattern.NUMBER_REGEX.unMatch(input)) ErrorMessage.NUMBER_REGEX_PATTERN.throwIllegalArgumentException();
     }
     public static void winningLottoValidate(String input) {
-        if (!RegexPattern.LOTTO_REGEX.match(input)) ErrorMessage.LOTTO_REGEX_PATTERN.throwIllegalArgumentException();
+        if (RegexPattern.LOTTO_REGEX.unMatch(input)) ErrorMessage.LOTTO_REGEX_PATTERN.throwIllegalArgumentException();
     }
 }
