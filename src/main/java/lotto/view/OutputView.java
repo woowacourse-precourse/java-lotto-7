8d9563fb07aefaf90cc067e42d92e5e906035715
junest66 <chlwndks333@gmail.com>
2024-11-04 16:37:27 +0900

    fix(OutputView): 수익률 출력 포맷 버그 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,7 +9,7 @@ import lotto.dto.PurchaseLotto;
 public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String RESULT_MESSAGE_FORMAT = "%s - %d개";
-    private static final String PROFIT_MESSAGE_FORMAT = "총 수익률은 %.2f%%입니다.";
+    private static final String PROFIT_MESSAGE_FORMAT = "총 수익률은 %s%%입니다.";
 
     public static void printErrorMessage(String message) {
         System.out.println(message);
