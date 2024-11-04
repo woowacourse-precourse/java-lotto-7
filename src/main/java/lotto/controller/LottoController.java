6e5b99dcb7af6a429f3e459ea1a5955735a2cb6f
배현서 <haulqogustj@naver.com>
2024-11-04 18:17:04 +0900

    refactor(controller): 컨트롤러 계층 분리
    컨트롤러의 책임을 명확히 하고 단일 책임 원칙을 준수하기 위해 구조를 개선했습니다.
    - MainController, InputController, OutputController로 분리
    - 각 컨트롤러의 책임 명확화
    - 에러 처리 일원화
    
    BREAKING CHANGE: Controller 클래스 구조가 완전히 변경됨
    
    Resolve: 컨트롤러 책임 분리

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,36 @@
+package lotto.controller;
+
+import lotto.model.*;
+import lotto.service.*;
+import lotto.util.InputValidator;
+import lotto.view.*;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputController inputController;
+    private final OutputController outputController;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView,
+                           InputValidator inputValidator, LottoFactory lottoFactory) {
+        this.inputController = new InputController(inputView, inputValidator);
+        this.outputController = new OutputController(outputView);
+        this.lottoService = new LottoService(lottoFactory);
+    }
+
+    public void run() {
+        try {
+            Money money = inputController.getMoney();
+            List<Lotto> lottos = lottoService.createLottos(money);
+            outputController.printLottoPurchaseInfo(lottos);
+
+            WinningInfo winningInfo = inputController.getWinningInfo();
+            LottoResult result = lottoService.calculateResult(lottos, winningInfo);
+            outputController.printGameResult(result);
+
+        } catch (IllegalStateException e) {
+            outputController.printError(e.getMessage());
+        }
+    }
+}
