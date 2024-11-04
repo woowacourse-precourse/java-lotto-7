2ed56133701a782b9d512b2c6bfb12d5a8314d33
jjunh0 <wnsgh732@gmail.com>
2024-11-04 23:03:06 +0900

    test: 통합 테스트 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,75 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    @Test
+    void 구매_가격_1000원_단위_미입력시_에외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1012");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스_숫자_문자_입력시_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "5j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스_숫자_1에서_45사이_미입력시_예외_테스트1() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "-5");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스_숫자_1에서_45사이_미입력시_예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "70");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 보너스_숫자_당첨_번호와_중복_번호_입력시_예외_테스트1() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "1", "7");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 기능_테스트2() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("5000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "5개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "[1, 2, 3, 4, 5, 8]",
+                            "[1, 2, 3, 4, 8, 9]",
+                            "[1, 2, 3, 8, 9, 10]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 1개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 1개",
+                            "총 수익률은 40631100.0%입니다."
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 8),
+                List.of(1, 2, 3, 4, 8, 9),
+                List.of(1, 2, 3, 8, 9, 10)
+        );
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
