deec7ae2b68989179dab73dc3e7fbfec826f7dc2
이준범 <jach1206@naver.com>
2024-11-04 20:48:57 +0900

    test: 구매한 금액에 맞는 로또 갯수가 발행되는지 테스트 코드 추가

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -2,14 +2,21 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PurchaseAmountTest {
 
+    private static final String CAN_BUY_SEVEN = "7000";
     private static final String NOT_DIVIDE_UP = "1234";
     private static final String NOT_POSSIBLE_RANGE = "2147483649";
     private static final String NEGATIVE = "-1";
 
+    @Test
+    void 구입금액에_알맞은_로또_개수가_나오는지_테스트_한다() {
+        PurchaseAmount purchaseAmount = new PurchaseAmount(CAN_BUY_SEVEN);
+        Assertions.assertThat(purchaseAmount.getCanBuyLottoCount()).isEqualTo(7);
+    }
 
     @Test
     void 구입금액이_1000원으로_나누어_떨어지지_않으면_예외가_발생한다() {
