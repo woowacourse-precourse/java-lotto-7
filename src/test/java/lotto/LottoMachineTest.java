93c36a663b0fb3e65cf00f5948fe36975be62edc
YujinJuhn <holsya@naver.com>
2024-11-04 20:23:49 +0900

    test: 로또 발행이 정상적으로 이루어지는지 검사하는 테스트 추가

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoMachineTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private LottoMachine lottoMachine;
+
+    @BeforeEach
+    void setUp() {
+        lottoMachine = new LottoMachine();
+    }
+
+    @DisplayName("정상적으로 로또가 발행되는 경우")
+    @Test
+    void issue_Lotto_Test(){
+        int purchaseAmount = 4000;
+
+        lottoMachine.generateLottos(purchaseAmount);
+        List<Lotto> myLottos = lottoMachine.getMyLottos();
+
+        int expectedLottosCount = purchaseAmount / LOTTO_PRICE;
+        assertThat(myLottos).hasSize(expectedLottosCount);
+
+        for(Lotto lotto: myLottos) {
+            assertThat(lotto.getNumbers()).hasSize(LOTTO_NUMBER_COUNT);
+            assertThat(lotto.getNumbers()).isSorted();
+            assertThat(lotto.getNumbers()).doesNotHaveDuplicates();
+            assertThat(lotto.getNumbers()).allMatch(number -> number >= LOTTO_NUMBER_MIN && number <= LOTTO_NUMBER_MAX);
+        }
+    }
+}
