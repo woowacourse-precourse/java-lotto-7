dff9ab2713e4337be8f1645cc554b1c13e855612
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 11:28:31 +0900

    feat: 금액에 맞는 로또를 발매하는 vendorLotto 메서드 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,17 +1,39 @@
 package lotto;
 
-public class LottoMachine {
-    private static final int LOTTO_PER_PRICE= 1000;
-    private int userMoney;
-    private static InputReader inputReader = new InputReader();
+import lotto.numberGenerator.LottoRandomNumberGenerator;
 
-    public void readLottoPrice(){
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PER_PRICE = 1000;
+    private int userMoney;
+    private List<List<Integer>> lotto = new ArrayList<>();
+    private static InputReader inputReader = new InputReader();
+    private static LottoRandomNumberGenerator lottoRandomNumberGenerator = new LottoRandomNumberGenerator();
+
+    public void readLottoPrice() {
         System.out.println("구입금액을 입력해 주세요");
         String money = inputReader.readUserInput();
-        while(Integer.parseInt(money) / LOTTO_PER_PRICE != 0){
+        while (Integer.parseInt(money) / LOTTO_PER_PRICE != 0) {
             System.out.println("[ERROR] 구입 금액은 " + LOTTO_PER_PRICE + "원 단위로 입력해주세요.");
             money = inputReader.readUserInput();
         }
         userMoney = Integer.parseInt(money);
+    }
+
+    public int calculateLottoCount(int userMoney) {
+        return userMoney / LOTTO_PER_PRICE;
+    }
+
+    public List<Integer> purchaseLotto() {
+        return lottoRandomNumberGenerator.generateLottoNumbers();
+    }
+
+    public void vendorLotto(){
+        int lottoCount = calculateLottoCount(userMoney);
+        for(int i=0; i<lottoCount; ++i){
+            lotto.add(purchaseLotto());
+        }
     }
 }
