d222027cf4e05d116b0c3f2757715103a12a2928
dayeondayeon <dayeon2438@gmail.com>
2024-11-04 15:58:56 +0900

    feat : 발행된 로또 번호들을 화면에 출력해주는 기능 구현

--- src/main/java/lotto/service/OutputService.java
+++ src/main/java/lotto/service/OutputService.java
@@ -2,8 +2,16 @@ package lotto.service;
 
 import lotto.constants.OutputViewConstants;
 
+import java.util.List;
+
 public class OutputService {
     public void showPurchaseAmount(int purchaseAmount) {
         System.out.println(purchaseAmount + OutputViewConstants.PURCHASE_AMOUNT_DESCRIPTION);
+    }
+
+    public void showGeneratedLottoNumbers(List<List<Integer>> generatedLottoNumbers) {
+        for (List<Integer> generatedLottoNumber : generatedLottoNumbers) {
+            System.out.println(generatedLottoNumber);
+        }
     }
 }
