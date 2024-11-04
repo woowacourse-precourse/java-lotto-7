2961097419e29a60be073921ce07090da014aeb8
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 22:11:44 +0900

    test: "보너스 번호가 숫자가 아니면 예외가 발생한다.

--- src/test/java/lotto/view/InputTest.java
+++ src/test/java/lotto/view/InputTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputTest {
     private Input input = new Input();
+
     @DisplayName("로또 번호가 1000원 단위가 아니면 예외가 발생한다.")
     @Test
     void 로또_금액이_1000원_단위가_아니면_예외가_발생한다() {
@@ -25,7 +26,6 @@ class InputTest {
     @DisplayName("로또 금액이 숫자가 아니면 예외가 발생한다.")
     @Test
     void 로또_금액이_숫자가_아니면_예외가_발생한다() {
-        // 가상 입력을 설정
         String inputAmount = "abc\n";
         System.setIn(new ByteArrayInputStream(inputAmount.getBytes()));
 
@@ -37,12 +37,22 @@ class InputTest {
     @DisplayName("당첨 번호가 숫자가 아니면 예외가 발생한다.")
     @Test
     void 당첨_번호가_숫자가_아니면_예외가_발생한다() {
-        // 가상 입력을 설정
         String inputAmount = "a,1,2,3,4,5\n";
         System.setIn(new ByteArrayInputStream(inputAmount.getBytes()));
 
         assertThatThrownBy(() -> input.readWinningNumber())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 당첨 번호는 숫자만 입력해 주세요");
+    }
+
+    @DisplayName("보너스 번호가 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_숫자가_아니면_예외가_발생한다() {
+        String inputAmount = "a\n";
+        System.setIn(new ByteArrayInputStream(inputAmount.getBytes()));
+
+        assertThatThrownBy(() -> input.readBonusNumber())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 숫자만 입력해 주세요");
     }
 }
