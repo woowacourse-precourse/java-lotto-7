8e85674a6e9aab069d22026db573de8e67c378c1
hongjungin <jungini0000@gmail.com>
2024-11-04 16:17:09 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -1,10 +1,32 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGenerator {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     public static int calculateLottoCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public static List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, 6);
+    }
+
+    public static List<List<Integer>> generateLottoTickets(int purchaseAmount) {
+        int count = calculateLottoCount(purchaseAmount);
+        List<List<Integer>> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottoTickets.add(generateLottoNumbers());
+        }
+
+        return lottoTickets;
     }
 }
