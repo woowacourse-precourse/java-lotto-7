08086ebe1e41b893ee977e60657b065c82df0e8a
88dldl <chltnrgml0999@gmail.com>
2024-11-04 15:28:31 +0900

    feat(Starter): 금액 입력과 결과 출력 기능 통합

--- src/main/java/lotto/service/Starter.java
+++ src/main/java/lotto/service/Starter.java
@@ -1,10 +1,34 @@
 package lotto.service;
 
+import lotto.controller.LottoController;
+import lotto.dto.request.MoneyRequest;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 
 public class Starter {
+    private final LottoController lottoController;
+
+    public Starter(LottoController lottoController) {
+        this.lottoController = lottoController;
+    }
+
     public void run() {
-        int money = InputView.getMoney();
+        int money = getMoney();
+        List<List<Integer>> tickets = runBuyTicket(money);
+    }
+
+    private int getMoney() {
+        return InputView.getMoney();
+    }
+
+    private List<List<Integer>> runBuyTicket(int money) {
+        List<List<Integer>> tickets = lottoController.buyTicket(MoneyRequest.of(money)).tickets();
+
+        OutputView.printTicketsInfo(tickets);
+
+        return tickets;
     }
 }
