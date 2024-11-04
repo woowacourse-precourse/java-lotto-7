0528e2b30d8d0163f5e98ed0de7695cd920778d1
soonvro <soonvro@gmail.com>
2024-11-04 20:45:26 +0900

    feat: 로또 번호에 중복된 숫자가 있으면 예외 발생 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,9 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
