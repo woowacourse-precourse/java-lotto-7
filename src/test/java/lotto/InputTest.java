1787c8625d4e54fd223a97320fc60f8105ef0d19
mjk <mjk3220@ajou.ac.kr>
2024-11-04 20:42:22 +0900

    style: testcase display

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class InputTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @DisplayName("숫자가 큰 보너스 번호인 경우 예외가 발생한다.")
     @Test
     void 숫자가_큰_보너스번호() {
         assertSimpleTest(() -> {
@@ -17,6 +20,7 @@ class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("숫자가 아닌 보너스 번호인 경우 예외가 발생한다.")
     @Test
     void 숫자가_아닌_보너스번호() {
         assertSimpleTest(() -> {
@@ -25,6 +29,7 @@ class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("음수인 보너스 번호인 경우 예외가 발생한다.")
     @Test
     void 음수인_보너스번호() {
         assertSimpleTest(() -> {
@@ -33,6 +38,7 @@ class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("숫자가 아닌 로또 번호인 경우 예외가 발생한다.")
     @Test
     void 숫자가_아닌_로또번호() {
         assertSimpleTest(() -> {
@@ -41,6 +47,7 @@ class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("로또 번호와 보너스 번호가 중복인 경우 예외가 발생한다.")
     @Test
     void 보너스_번호와_로또_번호가_중복() {
         assertSimpleTest(() -> {
@@ -49,6 +56,7 @@ class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("금액이 1000원으로 나누어떨어지지 않는 경우 예외가 발생한다.")
     @Test
     void 금액이_1000으로_나누어떨어지지_않는_경우() {
         assertSimpleTest(() -> {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,18 +22,21 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("숫자가 큰 경우 예외가 발생한다.")
     @Test
     void 숫자가_큰_경우() {
         assertThatThrownBy(() -> new Lotto(List.of(46, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("중복 제거 시 숫자가 6개인 경우 예외가 발생한다.")
     @Test
     void 중복_제거시_숫자가_6개_경우() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 1, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("숫자가 음수인 경우 예외가 발생한다.")
     @Test
     void 숫자가_음수인_경우() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, -9)))
