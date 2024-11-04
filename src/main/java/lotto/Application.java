d15fd78a4f1c7e35a570438ff577dd58ddc2a499
josephuk77 <josephuk77@gmail.com>
2024-11-04 19:35:05 +0900

    feat(controller): 구현한 프로그램에 맞게 메서드 실행
    구현한 프로그램에 맞게 메서드를 실행하도록 컨트롤러를 구현했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.service.RankingService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+        RankingService rankingService = new RankingService();
+
+        LottoController lottoController = new LottoController(lottoService, rankingService);
+
+        lottoController.run();
     }
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.Amount;
+import lotto.model.Bonus;
+import lotto.model.Lotto;
+import lotto.model.Ranking;
+import lotto.service.LottoService;
+import lotto.service.RankingService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+    private final RankingService rankingService;
+
+    public LottoController(LottoService lottoService, RankingService rankingService) {
+        this.lottoService = lottoService;
+        this.rankingService = rankingService;
+    }
+
+    public void run() {
+        Amount amount = InputView.inputAmount();
+        OutputView.printLottoCount(amount.getAmount());
+
+        List<Lotto> lottos = lottoService.buyLotto(amount.getAmount());
+        OutputView.printLottoNumbers(lottos);
+
+        Lotto winningLotto = InputView.inputWinningLotto();
+
+        Bonus bonus = InputView.inputBonusNumber(winningLotto);
+
+        rankingService.evaluateAllLottoResults(lottos, winningLotto, bonus.getNumber());
+        
+        OutputView.printWinningStatisticsMessage(Ranking.calculateYield(amount.getAmount()));
+    }
+}
