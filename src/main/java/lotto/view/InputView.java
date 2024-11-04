a63d1d9a6fe9de09fd454cf5d4e41bf984eb0e02
YJ9985 <lucy1486@naver.com>
2024-11-04 20:40:23 +0900

    [feat] 사용자 인터페이스 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,26 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
-    // TODO: 구현 예정
+    public static int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    public static int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,37 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
-    // TODO: 구현 예정
+    public static void printPurchaseCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.forEach(System.out::println);
+    }
+
+    public static void printWinningStatistics(Map<LottoRank, Integer> results) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---------");
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank != LottoRank.NONE) {
+                System.out.printf("%s (%,d원) - %d개\n",
+                        rank.getDescription(),
+                        rank.getPrize(),
+                        results.getOrDefault(rank, 0));
+            }
+        }
+    }
+
+    public static void printReturnRate(double returnRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", returnRate);
+    }
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
 }
