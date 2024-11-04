1112f2a7155951d66dd3c563f51dfa4fa32fa3e7
gorapaduckoo <jioung93@naver.com>
2024-11-04 17:14:04 +0900

    feat: 로또 번호 목록 파싱 기능 추가

--- src/main/java/lotto/validator/WinningLottoValidator.java
+++ src/main/java/lotto/validator/WinningLottoValidator.java
@@ -1,17 +1,33 @@
 package lotto.validator;
 
-import java.util.ArrayList;
+import static lotto.message.ExceptionMessage.INVALID_TYPE_INPUT;
+
 import java.util.Arrays;
 import java.util.List;
+import lotto.exception.IllegalTypeException;
 
 public class WinningLottoValidator {
+
+    private static final String INPUT = "로또 번호";
+    private static final String TYPE = "정수";
     private static final String DELIMITER = ",";
+    private static final int MIN_VALUE = 1, MAX_VALUE = 45;
 
     public static List<Integer> validateWinningNumbers(String input) {
-        return new ArrayList<>();
+        return Arrays.stream(input.split(DELIMITER)).map(WinningLottoValidator::validateType).toList();
     }
 
     public static int validateBonusNumber(String input) {
         return Integer.parseInt(input);
+    }
+
+    private static int validateType(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalTypeException(
+                    String.format(INVALID_TYPE_INPUT.getMessage(), INPUT, TYPE)
+            );
+        }
     }
 }
