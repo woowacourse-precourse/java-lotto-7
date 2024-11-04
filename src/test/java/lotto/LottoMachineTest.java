225dc367f6bd04e0565aa00ac782d8a4fc92407b
JunHyuk Yu <ericyoo0107@gmail.com>
2024-11-04 12:25:45 +0900

    test: 로또 결과 입력 후, 로또 당첨 내역 테스트 작성

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,9 +1,15 @@
 package lotto;
 
 import lotto.data.Database;
+import lotto.data.Lotto;
+import lotto.data.Result;
 import lotto.service.LottoMachine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +27,25 @@ public class LottoMachineTest {
         lottoMachine.buyLotto(money);
         // then
         assertThat(lottoMachine.getPurchasedLotto().size()).isEqualTo(14);
+    }
+
+    @DisplayName("로또 구입 결과 : 1등 1개, 2등 1개, 3등 1개, 4등 1개 당첨.")
+    @Test
+    void calculateResultTest() {
+        // given
+        database.purchaseLottoList.addAll(List.of(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 45)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 8, 9))
+        ));
+        // when
+        lottoMachine.calculateResult(List.of(1, 2, 3, 4, 5, 6), 7);
+        // then
+        Result result = lottoMachine.getResult();
+        assertThat(result.getSixNumberMatch()).isEqualTo(1);
+        assertThat(result.getBonusNumberMatch()).isEqualTo(1);
+        assertThat(result.getFiveNumberMatch()).isEqualTo(1);
+        assertThat(result.getFourNumberMatch()).isEqualTo(1);
     }
 }
