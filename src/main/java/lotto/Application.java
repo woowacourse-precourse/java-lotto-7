3f8824e3e365ec05f0bd714e51e063b0efac91f7
AwesomeMinsik <beaut3579@gmail.com>
2024-11-04 23:47:41 +0900

    refactor :  역할 분리및 클래스 추가에 따른 Application 로직을 업데이트 하였습니다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,29 +1,28 @@
 package lotto;
 
-import lotto.IO.InputHandler;
+import lotto.constants.LottoRank;
+import lotto.controller.InputController;
+import lotto.inputoutput.OutputHandler;
+import lotto.model.LottoList;
 
 import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
-        int price = InputHandler.getPrice();
-        LottoList lottoList = new LottoList(LottoManager.getTotal(price));
+        int price = InputController.getPrice();
+        LottoList lottoList = LottoManager.getLottoList(price);
 
-        System.out.println(lottoList.getLottoList().size() + "개를 구매했습니다.");
-        for (Lotto lotto : lottoList.getLottoList()) {
-            System.out.println(lotto.getNumbers());
-        }
+        OutputHandler.printLottoStatus(lottoList);
 
-        List<Integer> winningNumber = InputHandler.getWinningLottoNumber();
-        int bonusNumber = InputHandler.getBonusNumber(winningNumber);
+        List<Integer> winningNumber = InputController.getWinningLottoNumber();
+        int bonusNumber = InputController.getBonusNumber(winningNumber);
 
         System.out.println("당첨 확인 결과:");
         Map<LottoRank, Integer> result = LottoManager.findWinningLotto(lottoList, winningNumber, bonusNumber);
 
         double profit = LottoManager.calculateProfit(result, price);
-        System.out.println("총 수익률은 " + profit + "%입니다.");
+        OutputHandler.printLottoResult(profit);
     }
 }
