e7ef44ce94ea84fc66485bfdd65885fe8544fbe9
dongyeopwoo <dongyeopwoo@naver.com>
2024-11-04 22:46:04 +0900

    feat(WinningNumbers): 당첨 번호 클래스 구현 사용자로부터 당첨 번호와 보너스 번호 입력을 받아 유효성 검사를 진행.

--- src/main/java/model/WinningNumbers.java
+++ src/main/java/model/WinningNumbers.java
@@ -1,2 +1,32 @@
-package model;public class WinningNumbers {
+package model;
+
+import static content.ErrorMessage.WINNING_NUMBERS_OVERLAP;
+
+import content.LottoConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningNumbers {
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public WinningNumbers(String[] winningNumberInput, int bonusNumber) {
+        this.winningNumbers = new ArrayList<>();
+        for (String number : winningNumberInput) {
+            this.winningNumbers.add(Integer.parseInt(number.trim()));
+        }
+        if (this.winningNumbers.size() != LottoConstants.LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_OVERLAP.getMessage());
+        }
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
