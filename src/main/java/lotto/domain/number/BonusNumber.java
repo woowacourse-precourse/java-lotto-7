bb0a3511832c1d576081ed64f5e515c9d8b1aba2
hootzz <wjqwjql3@gmail.com>
2024-11-04 16:06:40 +0900

    refactor(number): BonusNumber 원시값 포장 추가

--- src/main/java/lotto/domain/number/BonusNumber.java
+++ src/main/java/lotto/domain/number/BonusNumber.java
@@ -1,20 +1,28 @@
 package lotto.domain.number;
 
+import lotto.domain.ticket.Lotto;
+
 public class BonusNumber {
     private static final String ERROR_DUPLICATE = "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.";
 
     private final LottoNumber number;
 
-    private BonusNumber(LottoNumber number) {
+    private BonusNumber(LottoNumber number, Lotto winningLotto) {
+        validateDuplicate(number, winningLotto);
         this.number = number;
+    }
+
+    public static BonusNumber of(LottoNumber number, Lotto winningLotto) {
+        return new BonusNumber(number, winningLotto);
+    }
+
+    private void validateDuplicate(LottoNumber number, Lotto winningLotto) {
+        if (winningLotto.contains(number)) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
     }
 
     public LottoNumber getNumber() {
         return number;
-    }
-
-    @Override
-    public String toString() {
-        return number.toString();
     }
 }
