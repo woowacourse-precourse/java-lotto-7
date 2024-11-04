ea2c3ff90b75ec18ea54060d4861df7310957de8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-04 21:25:51 +0900

    feat(Money): add 금액 검증 기능
    - Money 클래스에 금액 검증 로직 추가
        - 양의 정수 여부를 확인하여 0 이하 금액 시 예외 발생
        - 1000의 배수 여부를 확인하여 그렇지 않을 경우 예외 발생
    - 유효한 금액은 필드로 저장

---
+++ src/main/java/lotto/model/domain/Money.java
@@ -0,0 +1,31 @@
+package lotto.model.domain;
+
+import lotto.MoneyInputErrorMessage;
+
+import static lotto.Constants.LOTTO_PRICE;
+
+public class Money {
+    private final long amount;
+
+    public Money(long amount) {
+        validatePositiveAmount(amount);
+        validateMultipleOfThousand(amount);
+        this.amount = amount;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    private void validatePositiveAmount(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(MoneyInputErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+        }
+    }
+
+    private void validateMultipleOfThousand(long amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(MoneyInputErrorMessage.NON_THOUSAND_MULTIPLE.getMessage());
+        }
+    }
+}
