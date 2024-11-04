9fb15b706998febabccfae0ddcf1998149197fc9
dongwoo <woomacho@gmail.com>
2024-11-04 18:44:08 +0900

    feat: LottoView 기능 구현
    - 로또 구매, 당첨 번호와 보너스번호 입력, 로또 결과 확인에 대한 입출력 로직을 담당한다.

---
+++ src/main/java/lotto/LottoView.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.util.LottoPrizeRankType;
+
+//enum
+public class LottoView {
+
+    protected LottoView() {
+    }
+
+    public String purchaseLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public void displayPurchasedLotto(List<Lotto> lottoTickets) {
+        System.out.println(lottoTickets.size() + "개를 구매하셨습니다.");
+        lottoTickets.forEach(lotto ->
+                System.out.println(lotto.getNumbers()));
+    }
+
+    public String getWinningLottoNumbers() {
+        System.out.println("당첨번호 6자리를 쉼표(,)로 구분하여 입력하세요");
+        return Console.readLine();
+    }
+
+    public String getBonusLottoNumbers() {
+        System.out.println("보너스 번호를 입력해주세요.");
+        return Console.readLine();
+    }
+
+    public void displayLottoResults(LottoResultDto lottoResult) {
+        Map<LottoPrizeRankType, Long> rankLotto = lottoResult.rankLotto();
+        float rateOfReturn = lottoResult.rateOfReturn();
+        System.out.println("당첨 통계");
+        System.out.println("-------");
+        for (Entry<LottoPrizeRankType, Long> rank : rankLotto.entrySet()) {
+            System.out.printf("%d개 일치 (%,d원) - %d개 %n",
+                    rank.getKey().getMatchCount(), rank.getKey().getPrizeMoney(), rank.getValue());
+        }
+        System.out.printf("총 수익률은 %.1f%%", rateOfReturn);
+    }
+}
