4174b61ee9c4b6448d041489bc6ab0ba545b3e53
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 20:57:43 +0900

    feat(model): 구매한 만큼 로또를 발행하는 기능 추가
    - generateLottos() 추가

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -5,8 +5,18 @@ import static lotto.Constants.MAX_NUMBER;
 import static lotto.Constants.MIN_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoGenerator {
+
+    public List<Lotto> generateLottos(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+        return lottos;
+    }
 
     public List<Integer> generateLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBERS_SIZE);
