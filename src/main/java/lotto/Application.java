986f95e421a587608f077b5ce34167652a75909d
btongtong <leeym2615@naver.com>
2024-11-04 15:17:12 +0900

    feat(Application): Print user lottos based on quantity

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         UserLottos userLottos = createUserLottos();
+        showUserLottos(userLottos);
     }
 
     private static UserLottos createUserLottos() {
@@ -21,5 +22,19 @@ public class Application {
     private static String inputPurchaseAmount() {
         System.out.println(IOMessage.PURCHASE_AMOUNT.getMessage());
         return readLine();
+    }
+
+    private static void showUserLottos(UserLottos userLottos) {
+        System.out.println();
+        System.out.println(userLottos.getQuantity() + IOMessage.PURCHASE_QUANTITY.getMessage());
+        System.out.println(getUserLottos(userLottos));
+    }
+
+    private static String getUserLottos(UserLottos userLottos) {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : userLottos.getLottos()) {
+            sb.append(lotto.getNumbers().toString()).append("\n");
+        }
+        return sb.toString().trim();
     }
 }
