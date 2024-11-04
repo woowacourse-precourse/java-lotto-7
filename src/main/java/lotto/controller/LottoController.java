1308d22c1127ad52f4e8bdd77ca0ad5631ac5a31
DongHoon Lee <dhl1924@naver.com>
2024-11-04 17:15:22 +0900

    feat(controller): 로또 발급 프로세스 구현
    - 사용자의 구입 금액을 입력받아 로또 수량 계산
    - 발급된 로또 수량과 번호를 출력하는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,18 +1,29 @@
 package lotto.controller;
 
+import lotto.domain.LottoMachine;
+import lotto.domain.Lottos;
+import lotto.dto.PurchaseAmountRequest;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoMachine lottoMachine;
 
-    public LottoController(InputView inputView, OutputView outputView) {
+    public LottoController(InputView inputView, OutputView outputView, LottoMachine lottoMachine) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoMachine = lottoMachine;
     }
 
     public void run() {
+        PurchaseAmountRequest purchaseAmountRequest = inputView.readPurchaseAmount();
+        int lottoCount = lottoMachine.calculateLottoCount(purchaseAmountRequest.getAmount());
+        outputView.printPurchasedLottoCount(lottoCount);
 
+        Lottos lottos = lottoMachine.issueLottoTickets(lottoCount);
+
+        outputView.printLottoTickets(lottos);
     }
 }
