7c4a964037193aa38351e7beb5c6106a05f1fb0e
Hyejeong <thswjd0609@gmail.com>
2024-11-04 17:58:01 +0900

    feat: 당첨 번호 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,9 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     public int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -9,6 +12,21 @@ public class InputView {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자 형식이어야 합니다.");
+        }
+    }
+
+    public List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] inputNumbers = Console.readLine().split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        try {
+            for (String num : inputNumbers) {
+                winningNumbers.add(Integer.parseInt(num));
+            }
+            return winningNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
         }
     }
 }
