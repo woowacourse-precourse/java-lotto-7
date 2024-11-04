1ac64399408685b033a58a57a191f820d20b959f
Jo Eungyeong <joeungyeong23@gmail.com>
2024-11-04 11:53:17 +0900

    refactor: BigDecimal.valueOf() 사용

--- src/main/java/lotto/domain/ProfitRate.java
+++ src/main/java/lotto/domain/ProfitRate.java
@@ -6,7 +6,7 @@ import java.math.RoundingMode;
 public class ProfitRate {
     private static final int SCALE_DIVIDE = 3;
     private static final int SCALE_RESULT = 1;
-    private static final double HUNDRED = 100.0;
+    private static final BigDecimal HUNDRED = BigDecimal.valueOf(100);
 
     private final double profitRate;
 
@@ -15,10 +15,10 @@ public class ProfitRate {
     }
 
     public static ProfitRate of(double purchaseAmount, double totalPrize) {
-        BigDecimal amount = new BigDecimal(String.valueOf(purchaseAmount));
-        BigDecimal prize = new BigDecimal(String.valueOf(totalPrize));
+        BigDecimal amount = BigDecimal.valueOf(purchaseAmount);
+        BigDecimal prize = BigDecimal.valueOf(totalPrize);
         BigDecimal profitRate = prize.divide(amount, SCALE_DIVIDE, RoundingMode.HALF_UP)
-                .multiply(new BigDecimal(String.valueOf(HUNDRED)))
+                .multiply(HUNDRED)
                 .setScale(SCALE_RESULT, RoundingMode.HALF_UP);
         return new ProfitRate(profitRate.doubleValue());
     }
