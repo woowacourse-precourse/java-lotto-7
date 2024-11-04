cd8dddfdb53afc2fc6145a1fbf0ede76b3436009
hoooonshub <yhn515@naver.com>
2024-11-04 21:51:50 +0900

    refactor: 과한 Supplier 일반 값으로 변경

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -2,12 +2,12 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class LottoShop {
-	private static final Supplier<List<Integer>> RANDOM_SIX_NUMBERS =
-			() -> Randoms.pickUniqueNumbersInRange(1, 45, 6);
+	private static final int MIN_RANGE = 1;
+	private static final int MAX_RANGE = 45;
+	private static final int LOTTO_SIZE = 6;
 
 	private static final int AMOUNT_UNIT = 1000;
 
@@ -15,7 +15,7 @@ public class LottoShop {
 		int quantity = money / AMOUNT_UNIT;
 
 		return IntStream.range(0, quantity)
-				.mapToObj(it -> new Lotto(RANDOM_SIX_NUMBERS.get()))
+				.mapToObj(it -> new Lotto(Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_SIZE)))
 				.toList();
 	}
 }
