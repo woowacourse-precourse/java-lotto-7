27f91680dcf8d35257272e0795a8d756f427e2e9
likerhythm <wjdalsdl0412@gmail.com>
2024-11-04 15:00:20 +0900

    refactor(package): controller 패키지 구성

--- src/main/java/lotto/controller/LottoAppController.java
+++ src/main/java/lotto/controller/LottoAppController.java
@@ -1,12 +1,10 @@
-package lotto;
+package lotto.controller;
 
-import lotto.error.SystemError;
 import lotto.model.payment.Payment;
 import lotto.model.lotto.Lotto;
 import lotto.model.lotto.LottoChecker;
 import lotto.model.lotto.LottoPublisher;
 import lotto.model.draw_numbers.DrawNumbers;
-import lotto.model.draw_numbers.builder.DrawNumbersBuilder;
 import lotto.model.number_generator.LottoNumberGenerator;
 import lotto.model.number_generator.NumberGenerator;
 import lotto.view.InputView;
@@ -14,30 +12,34 @@ import lotto.view.OutputView;
 
 import java.util.List;
 
-public class LottoController {
+public class LottoAppController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private LottoChecker lottoChecker;
-    private LottoPublisher lottoPublisher;
+    private final LottoChecker lottoChecker;
+    private final LottoPublisher lottoPublisher;
 
-    public LottoController(InputView inputView, OutputView outputView) {
+    public LottoAppController(InputView inputView, OutputView outputView, LottoChecker lottoChecker, LottoPublisher lottoPublisher) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoChecker = lottoChecker;
+        this.lottoPublisher = lottoPublisher;
     }
 
-    public void buyLotto() {
-        setLottoPublisher();
+    public DrawNumbers draw() {
+        return inputView.getDrawNumbers();
+    }
 
+    public List<Lotto> buyLottos() {
         Payment payment = inputView.getPayment();
         long lottoCount = printLottoCount(payment);
 
-        List<Lotto> lottos = publishLotto(lottoCount);
+        return publishLotto(lottoCount);
+    }
 
-        DrawNumbers drawNumbers = inputView.getDrawNumbers();
-        lottoChecker = new LottoChecker(drawNumbers);
-
-        printResult(lottos, lottoCount);
+    public void printResult(List<Lotto> lottos, DrawNumbers drawNumbers) {
+        double revenueRate = lottoChecker.calcRevenueRate(lottos, drawNumbers);
+        System.out.println(outputView.resultToString(revenueRate));
     }
 
     private List<Lotto> publishLotto(long lottoCount) {
@@ -46,19 +48,9 @@ public class LottoController {
         return lottos;
     }
 
-    private void printResult(List<Lotto> lottos, long lottoCount) {
-        double revenueRate = lottoChecker.calcRevenueRate(lottos, lottoCount);
-        System.out.println(outputView.resultToString(revenueRate));
-    }
-
     private long printLottoCount(Payment payment) {
         long lottoCount = payment.calcLottoCount();
         System.out.println(lottoCount + "개를 구매했습니다.");
         return lottoCount;
-    }
-
-    private void setLottoPublisher() {
-        NumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        lottoPublisher = new LottoPublisher(lottoNumberGenerator);
     }
 }
