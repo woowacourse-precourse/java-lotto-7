ae7dc68460766b8df7893a005d18f28d08d78e39
SeongbinPark <hyucse22@gmail.com>
2024-11-04 10:33:05 +0900

    test(LottoTest): 로또 번호에 음수가 포함된 경우 예외가 발생한다.

--- src/test/java/lotto/unit/LottoTest.java
+++ src/test/java/lotto/unit/LottoTest.java
@@ -53,4 +53,13 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, Integer.parseInt("a"))))
                 .isInstanceOf(NumberFormatException.class);
     }
+
+    @Test
+    @DisplayName("로또 번호에 음수가 포함된 경우 예외가 발생한다.")
+    void lottoNumbersContainNegative() {
+        List<Integer> numbers = Arrays.asList(-1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_LOTTO_NUMBER_RANGE);
+    }
 }
