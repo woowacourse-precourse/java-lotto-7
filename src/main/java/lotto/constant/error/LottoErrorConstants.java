1a58cf53c830a488ccb58864ab5858c52e12254b
qyinm <qusseun@gmail.com>
2024-11-04 12:42:37 +0900

    feat: class 상수에서 enum 상수로 변경 수정

--- src/main/java/lotto/constant/error/LottoErrorConstants.java
+++ src/main/java/lotto/constant/error/LottoErrorConstants.java
@@ -1,5 +1,16 @@
 package lotto.constant.error;
 
-public class LottoErrorConstants {
-    public static final String PREFIX_ERROR_MESSAGE = "[ERROR] ";
+public enum LottoErrorConstants {
+    PREFIX_ERROR_MESSAGE("[ERROR] ");
+
+    private final String message;
+
+    LottoErrorConstants(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }
