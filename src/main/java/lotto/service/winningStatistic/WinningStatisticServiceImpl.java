be0924ddc48d1453f17f8c7b7e9d8e4d576e1e3b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-04 15:02:50 +0900

    feat(service): 당첨 통계 로직을 처리하는 WinningStatisticService 구현

---
+++ src/main/java/lotto/service/winningStatistic/WinningStatisticServiceImpl.java
@@ -0,0 +1,27 @@
+package lotto.service.winningStatistic;
+
+import lotto.factory.WinningStatisticFactory;
+import lotto.model.Lottos;
+import lotto.model.WinningNumbers;
+import lotto.model.WinningStatistic;
+import lotto.service.prize.TotalPrizeCalculatorService;
+import lotto.util.Calculator;
+
+public class WinningStatisticServiceImpl implements WinningStatisticService {
+    private final TotalPrizeCalculatorService totalPrizeCalculatorService;
+
+    public WinningStatisticServiceImpl(TotalPrizeCalculatorService totalPrizeCalculatorService) {
+        this.totalPrizeCalculatorService = totalPrizeCalculatorService;
+    }
+
+    @Override
+    public WinningStatistic calculateWinningStatistic(int cost, Lottos lottos, WinningNumbers winningNumbers) {
+        WinningStatistic winningStatistic = new WinningStatistic();
+
+        int totalPrize = totalPrizeCalculatorService.calculateTotalPrize(lottos, winningNumbers, winningStatistic);
+
+        double profitRate = Calculator.calculateProfitRate(cost, totalPrize);
+
+        return WinningStatisticFactory.createWinningStatisticWithProfitRate(winningStatistic, profitRate);
+    }
+}
