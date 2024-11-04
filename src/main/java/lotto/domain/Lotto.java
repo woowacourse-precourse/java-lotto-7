93fbd5e73baf07d4fe4e7dfb94aef5af8abbf606
davemins <davemins4125@naver.com>
2024-11-04 21:04:03 +0900

    feat(Domain): 로또 객체에 유효성 검사 추가 Lotto 클래스에 유효성 검사를 하는 기능들을 추가했습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,17 +4,41 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_GAME_NUMBERS_SIZE = 6;
+    private static final int LOTTO_GAME_START_NUMBER = 1;
+    private static final int LOTTO_GAME_END_NUMBER = 45;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!isSixNumbers(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isBetweenLottoRange(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isDuplicateNumber(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isSixNumbers(List<Integer> numbers) {
+        return numbers.size() == LOTTO_GAME_NUMBERS_SIZE;
+    }
+
+    private boolean isBetweenLottoRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(number ->
+                number >= LOTTO_GAME_START_NUMBER && number <= LOTTO_GAME_END_NUMBER);
+    }
+
+    private boolean isDuplicateNumber(List<Integer> numbers) {
+        return numbers.size() == numbers.stream().distinct().count();
+    }
 }
