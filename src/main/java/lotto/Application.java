c9f6c8652df37bed3ba14d1e281fb769188ec9f5
moongua404 <hogun.soongit@gmail.com>
2024-11-04 21:53:17 +0900

    feat(errorMessage): 에러메시지 로깅
    - 에러메시지 로깅 기능 Application에 넣음
    - 전역적으로 적용시키기 위함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,10 +8,14 @@ import lotto.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        LottoService lottoService = new LottoService();
-        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
-        lottoController.run();
+        try {
+            InputView inputView = new InputView();
+            OutputView outputView = new OutputView();
+            LottoService lottoService = new LottoService();
+            LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+            lottoController.run();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
