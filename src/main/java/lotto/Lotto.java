8d7fabcc72a743317269bf338ed025dc4f1d073d
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 14:55:46 +0900

    feat(Lotto): 로또 번호는 중복될 수 없다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,30 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private final static int NUMBERS_LENGTH = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLength(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != NUMBERS_LENGTH) {
+            throw new IllegalArgumentException("로또 번호는 중복되면 안됩니다.");
+        }
+    }
 }
