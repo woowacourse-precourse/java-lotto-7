0acd44b88272193395a1b817a4192dd337d46634
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 21:21:47 +0900

    refactor(InputView): SRP에 맞게 클래스의 역할 재조정
    -Validator는 검증만 하고 이후 예외 메시지 출력과 재입력은 InputView에서 처리하도록 책임 분리함.

--- src/main/java/lotto/validator/WinningNumberValidator.java
+++ src/main/java/lotto/validator/WinningNumberValidator.java
@@ -3,35 +3,24 @@ package lotto.validator;
 import java.util.HashSet;
 import java.util.Set;
 import lotto.message.ErrorMessage;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class WinningNumberValidator {
     public static Set<Integer> validateWinningNumber(String[] winningNumbers) {
-        Set<Integer> winningNumberList = new HashSet<>();
+        Set<Integer> winningNumberSet = new HashSet<>();
         checkWinningNumbersAmount(winningNumbers);
-        for (var winningNumber : winningNumbers) {
+        for (String winningNumber : winningNumbers) {
             checkNullOrEmptyNumber(winningNumber);
             checkInteger(winningNumber);
             checkValidWinningNumber(winningNumber);
-            checkDuplicateWinningNumber(winningNumber, winningNumberList);
-            winningNumberList.add(Integer.parseInt(winningNumber));
+            checkDuplicateWinningNumber(winningNumber, winningNumberSet);
+            winningNumberSet.add(Integer.parseInt(winningNumber));
         }
-        return winningNumberList;
+        return winningNumberSet;
     }
 
     private static void checkNullOrEmptyNumber(String winningNumber) {
         if (winningNumber == null || winningNumber.isBlank()) {
-            handleNullOrEmptyNumber();
-        }
-    }
-
-    private static void handleNullOrEmptyNumber() {
-        try {
             throw new IllegalArgumentException(ErrorMessage.NULL_OR_EMPTY_WINNING_NUMBER.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.inputWinningNumber();
         }
     }
 
@@ -39,63 +28,26 @@ public class WinningNumberValidator {
         try {
             Integer.parseInt(winningNumber);
         } catch (NumberFormatException e) {
-            handleNumberFormatExecption();
-        }
-    }
-
-    private static void handleNumberFormatExecption(){
-        try {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER.getMessage());
-        }
-        catch(IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.inputWinningNumber();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER.getMessage());
         }
     }
 
     private static void checkWinningNumbersAmount(String[] winningNumbers) {
         if (winningNumbers.length != 6) {
-            handleInvalidWinningNumbersAmount();
-        }
-    }
-
-    private static void handleInvalidWinningNumbersAmount() {
-        try {
             throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER_COUNT.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.inputWinningNumber();
         }
     }
 
     private static void checkValidWinningNumber(String winningNumber) {
         int winningNumberInt = Integer.parseInt(winningNumber);
         if (winningNumberInt > 45 || winningNumberInt < 1) {
-            handleInvalidWinningNumber();
-        }
-    }
-
-    private static void handleInvalidWinningNumber() {
-        try {
             throw new IllegalArgumentException(ErrorMessage.INVALID_WINNING_NUMBER.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.inputWinningNumber();
         }
     }
 
-    private static void checkDuplicateWinningNumber(String winningNumber, Set<Integer> winningNumberList) {
-        if (winningNumberList.contains(Integer.parseInt(winningNumber))) {
-            handleDuplicateWinningNumber();
-        }
-    }
-
-    private static void handleDuplicateWinningNumber() {
-        try {
+    private static void checkDuplicateWinningNumber(String winningNumber, Set<Integer> winningNumberSet) {
+        if (winningNumberSet.contains(Integer.parseInt(winningNumber))) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.inputWinningNumber();
         }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,31 +11,42 @@ import lotto.validator.LottoAmountValidator;
 public class InputView {
     public static int buyLotto() {
         OutputView.printLottoAmountInput();
-        String purchaseAmout = Console.readLine();
-        purchaseAmout = purchaseAmout.trim();
-        return LottoAmountValidator.validatePurchaseAmout(purchaseAmout);
+        while (true) {
+            try {
+                String purchaseAmount = Console.readLine().trim();
+                return LottoAmountValidator.validatePurchaseAmout(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 
     public static Set<Integer> inputWinningNumber() {
         OutputView.printWinningNumberInput();
-        String winningNumber = Console.readLine();
-        winningNumber = winningNumber.trim();
-        if (winningNumber.startsWith(",") || winningNumber.endsWith(",") || winningNumber.contains(",,")) {
-            try{
-                throw new IllegalArgumentException(ErrorMessage.INVALID_COMMA_FORMAT.getMessage());
-            }catch(IllegalArgumentException e){
+        while (true) {
+            try {
+                String winningNumber = Console.readLine().trim();
+                if (winningNumber.startsWith(",") || winningNumber.endsWith(",") || winningNumber.contains(",,")) {
+                    throw new IllegalArgumentException(ErrorMessage.INVALID_COMMA_FORMAT.getMessage());
+                }
+                String[] winningNumbers = winningNumber.split(",");
+                return WinningNumberValidator.validateWinningNumber(winningNumbers);
+            } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());
-                return inputWinningNumber();
             }
         }
-        String[] winningNumbers = winningNumber.split(",");
-        return WinningNumberValidator.validateWinningNumber(winningNumbers);
     }
 
-    public static int inputBonusNumber(){
+    public static int inputBonusNumber() {
         OutputView.printBonusNumberInput();
-        String bonusNumber = Console.readLine();
-        bonusNumber = bonusNumber.trim();
-        return BonusNumberValidator.validateBonusNumber(bonusNumber, WinningNumber.getWinningNumbers());
+        while (true) {
+            try {
+                String bonusNumber = Console.readLine().trim();
+                return BonusNumberValidator.validateBonusNumber(bonusNumber, WinningNumber.getWinningNumbers());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
+
 }
