8813a3e1b20bf5aabce422ed8ccec110a7d951ca
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 17:54:41 +0900

    test(winningLotto): 올바른 당첨 번호 및 보너스 번호 테스트 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
 
-    @DisplayName("올바른 당첨 번호 입력 시 예외가 발생하지 않고 저장된다.")
+    @DisplayName("올바른 당첨 번호와 보너스 번호 입력 시 예외가 발생하지 않고 저장된다.")
     @Test
-    void 올바른_당첨_번호_입력() {
+    void 올바른_당첨_번호와_보너스_번호_입력() {
         // given
         String validWinningNumbers = "1,2,3,4,5,6";
+        String validBonusNumber = "7";
         // when
-        WinningLotto winningLotto = new WinningLotto(validWinningNumbers);
+        WinningLotto winningLotto = new WinningLotto(validWinningNumbers, validBonusNumber);
         // then
         assertThat(winningLotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
     }
 }
