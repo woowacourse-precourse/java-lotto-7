2a408b04ff1db76013558fbc937d2185693cf07e
양효빈 <hyobin3026@naver.com>
2024-11-04 18:05:27 +0900

    refactor(LottoConstant): Add constant for required match count for bonus

--- src/main/java/lotto/model/lotto/LottoConstant.java
+++ src/main/java/lotto/model/lotto/LottoConstant.java
@@ -7,5 +7,6 @@ public class LottoConstant {
     public static final int NUMBER_COUNT = 6;
     public static final int PRICE = 1000;
     public static final int MAX_PURCHASE_AMOUNT = 100_000;
+    public static final int REQUIRED_MATCH_COUNT_FOR_BONUS = 5;
 
 }
