557d2a100055b7ee3b0986ad95bcb5f98f95e492
Avocado <ujubongbong@gmail.com>
2024-11-04 20:05:22 +0900

    :sparkles: feat: 로또 번호 중에 보너스번호가 존재하는지 확인하는 함수

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,32 +6,41 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
 
-    public int getLottoCount(int payment) {
-        return payment / 1000;
-    }
+	public int getLottoCount(int payment) {
+		return payment / 1000;
+	}
 
-    public List<Lotto> getLotto(int lottoCount) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i=0; i < lottoCount; i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-        }
-        return lottos;
-    }
+	public List<Lotto> getLotto(int lottoCount) {
+		List<Lotto> lottos = new ArrayList<>();
+		for (int i = 0; i < lottoCount; i++) {
+			lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+		}
+		return lottos;
+	}
+
+	public boolean isBonusMatch(List<Integer> lottos, int bonusNumber) {
+		for (Integer num : lottos) {
+			if (num == bonusNumber) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
