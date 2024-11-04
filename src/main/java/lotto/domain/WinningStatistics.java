ef2bdcca2500545334dbea1ebd8aa9607434fca7
yyysolhhh <solbaram37@gmail.com>
2024-11-04 15:23:56 +0900

    feat(WinningStatistics): 당첨 결과 통계 클래스 생성
    WinningStatistics
    - EnumMap으로 LottoRank를 키로 하는 Map 생성
    - Map 초기화
    - 일치 개수 별 로또의 개수 저장
    
    initRankCount
    - Map 초기화하는 메서드
    
    putRankCount
    - Map 수정하는 메서드
    - MatchResult를 매개변수로 받아서 일치 개수별 로또 개수 카운트
    
    calculateProfitRate
    - 수익률 계산하는 메서드
    
    calculateTotalPrize
    - 총 수익 계산하는 메서드
    
    getRankCount
    - Map 반환하는 메서드
    - getOrDefault를 이용하여 0개인 경우도 반환

---
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import lotto.common.config.LottoRank;
+
+public class WinningStatistics {
+    private final Map<LottoRank, Integer> rankCounts;
+    private long totalPrize;
+//    private int profitRate;
+
+    public WinningStatistics() {
+        this.rankCounts = new EnumMap<>(LottoRank.class);
+        initRankCount();
+    }
+
+    private void initRankCount() {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            rankCounts.put(lottoRank, 0);
+        }
+    }
+
+    public void putRankCount(MatchResult result) {
+        if (result.getRank() == null) {
+            return;
+        }
+        LottoRank rank = result.getRank();
+        int currentCount = rankCounts.get(rank);
+        int newCount = currentCount + 1;
+        rankCounts.put(rank, newCount);
+//        if (result.getRank() != null) {
+//            rankCounts.merge(result.getRank(), 1, Integer::sum);
+//        }
+    }
+
+    public double calculateProfitRate(int purchaseAmount) {
+        calculateTotalPrize();
+        return (double) totalPrize / purchaseAmount * 100;
+    }
+
+    private void calculateTotalPrize() {
+        for (Entry<LottoRank, Integer> entry : rankCounts.entrySet()) {
+            totalPrize += entry.getKey().getPrize() * entry.getValue();
+        }
+    }
+
+    public int getRankCount(LottoRank rank) {
+        return rankCounts.getOrDefault(rank, 0);
+    }
+}
