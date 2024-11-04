b96fe4289d4d6112abf6cfccec6f0f87af12544a
18hailey <hailey9459@gmail.com>
2024-11-04 21:32:58 +0900

    style(Constant): create a Constant class to manage constant values

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,21 @@
+package lotto.constant;
+
+public class Constant {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int WON_UNIT = 1_000;
+    public static final int MAX_PRICE = 100_000;
+    public static final String COMMA_SEPARATOR = ",";
+
+    public static final String LOTTO_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_PURCHASE_PRINT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String LOTTO_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String LOTTO_BONUS_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_RESULT_MESSAGE = "당첨 통계\n---";
+
+    public static final String EACH_RANK_COUNT = "%d개 일치 (%s원) - %d개";
+    public static final String SECOND_RANK_COUNT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    public static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
+}
