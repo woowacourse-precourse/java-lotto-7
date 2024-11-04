49b92d2ee30d45102a7654aa4259ef4d19815947
eunho <eunho1032@gmail.com>
2024-11-04 17:45:42 +0900

    refactor: change reward print

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,18 +1,13 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import lotto.model.LottoReward;
 import lotto.model.Statistics;
 
 import java.util.List;
 import java.util.Map;
 
 public class Output {
-
-    private final String MATCHSIX = "2,000,000,000원";
-    private final String MATCHFIVEBONUS = "5,000,000원";
-    private final String MATCHFIVE = "1,500,000원";
-    private final String MATCHFOUR = "50,000원";
-    private final String MATCHTHREE = "5,000원";
 
     public void printPrompt(Prompt prompt) {
         System.out.println(prompt.getMessage());
@@ -37,13 +32,26 @@ public class Output {
 
         System.out.println("\n당첨 통계");
         System.out.println("---");
-        System.out.println("6개 일치 (" + MATCHSIX + ") - " + results.getOrDefault(1, 0) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (" + MATCHFIVEBONUS + ") - " + results.getOrDefault(2, 0) + "개");
-        System.out.println("5개 일치 (" + MATCHFIVE + ") - " + results.getOrDefault(3, 0) + "개");
-        System.out.println("4개 일치 (" + MATCHFOUR + ") - " + results.getOrDefault(4, 0) + "개");
-        System.out.println("3개 일치 (" + MATCHTHREE + ") - " + results.getOrDefault(5, 0) + "개");
+        System.out.println(
+            "6개 일치 (" + formatReward(LottoReward.MATCH_6_REWARD.getReward()) + ") - " + results.getOrDefault(1, 0)
+                + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + formatReward(LottoReward.MATCH_5_BONUS_REWARD.getReward()) + ") - "
+            + results.getOrDefault(2, 0) + "개");
+        System.out.println(
+            "5개 일치 (" + formatReward(LottoReward.MATCH_5_REWARD.getReward()) + ") - " + results.getOrDefault(3, 0)
+                + "개");
+        System.out.println(
+            "4개 일치 (" + formatReward(LottoReward.MATCH_4_REWARD.getReward()) + ") - " + results.getOrDefault(4, 0)
+                + "개");
+        System.out.println(
+            "3개 일치 (" + formatReward(LottoReward.MATCH_3_REWARD.getReward()) + ") - " + results.getOrDefault(5, 0)
+                + "개");
 
         double profitRate = ((double) statistics.getTotalPrize() / purchaseAmount) * 100;
         System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+    }
+
+    private String formatReward(int reward) {
+        return String.format("%,d원", reward);
     }
 }
