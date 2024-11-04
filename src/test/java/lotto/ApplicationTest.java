e6b98981a154b17a9f28f53229451f16fc5f324a
bogyeom0922 <pbk0398@gmail.com>
2024-11-04 14:47:56 +0900

    Refactor: 예제 테스트 오류 해결
    변경한 변수 이름 복구

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.validator.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest extends NsTest {
+    @Test
+    void 로또_번호기_비어있는_경우_예외_발생() {
+        assertThatThrownBy(() -> runException("8000", " ", "7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호를 입력해주세요.");
+    }
+
+    @Test
+    void 로또_번호에_공백이_포함된_경우_예외_발생() {
+        assertThatThrownBy(() -> runException("8000", "1, 2, 3, 4, 5, ", "7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호에_정수가_이닌_값이_있는_경우_예외_발생() {
+        assertThatThrownBy(() -> runException("8000", "1, 2, 3%%, 4, 5!!@, 6", "7"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호에 유효하지 않은 값이 포함되어 있습니다.");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
