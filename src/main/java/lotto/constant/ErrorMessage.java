ee484d6ebebf2817d2c74cc1a164e5a5ca830820
eb9862 <luclead622@gmail.com>
2024-11-04 15:14:24 +0900

    refactor: 에러 메시지 앞에 붙는 '[ERROR]' 표시를 상수로 선언

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -2,14 +2,15 @@ package lotto.constant;
 
 public class ErrorMessage {
 
-    public static final String BUDGET_NOT_NATURAL_NUMBER = "[ERROR] 구입 금액이 자연수가 아닙니다.";
-    public static final String BUDGET_NOT_DIVIDED_ONE_THOUSAND = "[ERROR] 구입 금액이 1000원으로 나누어 떨어지지 않습니다.";
+    public static final String ERROR_TAG = "[ERROR] ";
+    public static final String BUDGET_NOT_NATURAL_NUMBER = "구입 금액이 자연수가 아닙니다.";
+    public static final String BUDGET_NOT_DIVIDED_ONE_THOUSAND = "구입 금액이 1000원으로 나누어 떨어지지 않습니다.";
 
-    public static final String NUMBER_COUNT_NOT_VALID = "[ERROR] 로또 번호 개수가 일치하지 않습니다.";
-    public static final String NUMBER_OUT_OF_RANGE = "[ERROR] 로또 번호의 숫자 범위를 벗어납니다.";
-    public static final String NUMBER_DUPLICATED = "[ERROR] 번호에 중복이 존재합니다.";
+    public static final String NUMBER_COUNT_NOT_VALID = "로또 번호 개수가 일치하지 않습니다.";
+    public static final String NUMBER_OUT_OF_RANGE = "로또 번호의 숫자 범위를 벗어납니다.";
+    public static final String NUMBER_DUPLICATED = "번호에 중복이 존재합니다.";
 
-    public static final String WINNING_NUMBERS_NOT_NATURAL_NUMBERS = "[ERROR] 당첨 번호가 자연수가 아닙니다.";
+    public static final String WINNING_NUMBERS_NOT_NATURAL_NUMBERS = "당첨 번호가 자연수가 아닙니다.";
 
     private ErrorMessage() {
     }
