3d8d8c3372cdd6ebfd119d96e455d5caf4c2d65c
xeimun <hinote444@naver.com>
2024-11-04 15:54:28 +0900

    feat(LottoPurchaseInfo, ExceptionMessage): 보너스 번호가 당첨 번호와 중복될 경우 예외 처리 구현

--- src/main/java/lotto/LottoPurchaseInfo.java
+++ src/main/java/lotto/LottoPurchaseInfo.java
@@ -5,6 +5,7 @@ import static view.message.ExceptionMessage.MAX_PURCHASE_AMOUNT_TEN_THOUSAND;
 import static view.message.ExceptionMessage.LIMIT_EXCEED_EXCEPTION_MESSAGE;
 import static view.message.ExceptionMessage.THOUSAND_UNIT;
 import static view.message.ExceptionMessage.INVALID_UNIT_EXCEPTION_MESSAGE;
+import static view.message.ExceptionMessage.DUPLICATE_BONUS_NUMBER_EXCEPTION_MESSAGE;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,6 +16,7 @@ public record LottoPurchaseInfo(BigDecimal purchaseAmount, Lotto lottoNumbers, i
         validateNegativeNumber(purchaseAmount);
         validatePurchaseAmountLimit(purchaseAmount);
         validateThousandUnit(purchaseAmount);
+        validateBonusNumberDuplicate(lottoNumbers, bonusNumber);
     }
 
     public LottoPurchaseInfo(BigDecimal purchaseAmount, List<Integer> lottoNumbers, int bonusNumber) {
@@ -37,6 +39,13 @@ public record LottoPurchaseInfo(BigDecimal purchaseAmount, Lotto lottoNumbers, i
         if (purchaseAmount.remainder(THOUSAND_UNIT)
                           .compareTo(BigDecimal.ZERO) != 0) {
             throw new IllegalArgumentException(INVALID_UNIT_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateBonusNumberDuplicate(Lotto lottoNumbers, int bonusNumber) {
+        if (lottoNumbers.getNumbers()
+                        .contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 }
