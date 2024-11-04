9fd005e4a8e8c0cad2e3334cac8df5b69ee4cecc
persi <jina8151010@gmai.com>
2024-11-04 14:02:05 +0900

    test: 당첨 번호 입력 유효성 검증 테스트 추가

---
+++ src/test/java/lotto/WinningNumbersInputValidatorTest.java
@@ -0,0 +1,55 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersInputValidatorTest {
+
+    @DisplayName("정확히 6개의 숫자가 입력되지 않으면 예외가 발생한다")
+    @Test
+    void 숫자_개수가_6개가_아니면_예외가_발생한다() {
+        String input = "1,2,3,4,5";
+        assertThatThrownBy(() -> InputView.parseAndValidateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorConstants.INVALID_WINNING_NUMBERS_COUNT);
+    }
+
+    @DisplayName("입력된 숫자가 1에서 45 범위를 벗어나면 예외가 발생한다")
+    @Test
+    void 숫자가_범위를_벗어나면_예외가_발생한다() {
+        String input = "0,2,3,4,5,6";
+        assertThatThrownBy(() -> InputView.parseAndValidateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorConstants.INVALID_WINNING_NUMBER_RANGE);
+    }
+
+    @DisplayName("입력된 숫자에 중복이 있으면 예외가 발생한다")
+    @Test
+    void 중복된_숫자가_있으면_예외가_발생한다() {
+        String input = "1,2,3,4,5,5";
+        assertThatThrownBy(() -> InputView.parseAndValidateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorConstants.INVALID_WINNING_NUMBER_DUPLICATE);
+    }
+
+    @DisplayName("입력된 값이 숫자가 아닌 경우 예외가 발생한다")
+    @Test
+    void 입력값이_숫자가_아니면_예외가_발생한다() {
+        String input = "1,2,3,4,5,abc";
+        assertThatThrownBy(() -> InputView.parseAndValidateWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorConstants.INVALID_WINNING_NUMBER_FORMAT);
+    }
+
+    @DisplayName("유효한 당첨 번호 입력이 성공적으로 처리된다")
+    @Test
+    void 유효한_당첨번호_입력_성공() {
+        String input = "1,2,3,4,5,6";
+        List<Integer> result = InputView.parseAndValidateWinningNumbers(input);
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+}
