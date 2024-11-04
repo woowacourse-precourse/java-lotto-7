331b3d5646bcd67d5508b377a6274f107c308569
bokyeong <wntjdals0412@gmail.com>
2024-11-04 17:18:04 +0900

    feat : 잘못된 입력을 받을 경우 에러메시지 출력 후 다시 입력받기

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -6,34 +6,55 @@ import java.util.List;
 import java.util.Set;
 
 public class InputHandler {
-
+    private static final String ERROR_PREFIX = "[ERROR] ";
     private final static int LOTTO_NUM_SIZE = 6;
     public int getLottoAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        int amount = Integer.parseInt(Console.readLine());
-        validateInputMoneyAmount(amount);
+        while (true) {
+            try {
+                int amount = Integer.parseInt(Console.readLine());
+                validateInputMoneyAmount(amount);
 
-        return amount / 1000;
+                return amount / 1000;
+            }
+
+            catch (IllegalArgumentException e) {
+                System.out.println(ERROR_PREFIX + e.getMessage());
+            }
+        }
+
     }
 
     public List<Integer> getWinnerNumber() {
-        String input = Console.readLine();
-        List<Integer> winnerNumbers = Arrays.stream(input.split(","))
-                .map(Integer::parseInt)
-                .toList();
-        validateNumberInput(winnerNumbers);
-        checkDuplicateWinnerNumber(winnerNumbers);
-        checkWinnerNumberRange(winnerNumbers);
-
-        return winnerNumbers;
+        while (true) {
+            try {
+                String input = Console.readLine();
+                List<Integer> winnerNumbers = Arrays.stream(input.split(","))
+                        .map(Integer::parseInt)
+                        .toList();
+                validateNumberInput(winnerNumbers);
+                return winnerNumbers;
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println(ERROR_PREFIX + e.getMessage());
+            }
+        }
     }
 
     public int getBonusNumber(List<Integer> winnerNumbers) {
-        int bonusNumber = Integer.parseInt(Console.readLine());
-        checkNumber(bonusNumber);
-        checkBonusNumberDuplicateWithWinnerNumbers(winnerNumbers, bonusNumber);
+        while (true) {
+            try {
+                int bonusNumber = Integer.parseInt(Console.readLine());
+                checkNumber(bonusNumber);
+                checkBonusNumberDuplicateWithWinnerNumbers(winnerNumbers, bonusNumber);
 
-        return bonusNumber;
+                return bonusNumber;
+            }
+
+            catch (IllegalArgumentException e) {
+                System.out.println(ERROR_PREFIX + e.getMessage());
+            }
+        }
     }
 
     private void checkBonusNumberDuplicateWithWinnerNumbers(List<Integer> winnerNumbers, int bonusNumber) {
@@ -43,18 +64,6 @@ public class InputHandler {
         }
     }
 
-    private void checkDuplicateWinnerNumber(List<Integer> winnerNumbers) {
-        Set<Integer> check = new HashSet<>();
-        for(Integer number : winnerNumbers) {
-            if (!check.add(number)) {
-                throw new IllegalArgumentException("로또 숫자는 중복될 수 없습니다. 중복 번호 : " + number);
-            }
-        }
-    }
-
-    private void checkWinnerNumberRange(List<Integer> winnerNumbers) {
-        winnerNumbers.forEach(this::checkNumber);
-    }
     private void checkNumber(int number) {
         if (!(number >= 1 && number <= 45)) {
             throw new IllegalArgumentException("1부터 45까지만 가능합니다.");
@@ -70,8 +79,27 @@ public class InputHandler {
 
 
     private void validateNumberInput(List<Integer> winnerNumbers) {
+        checkInputNumberCount(winnerNumbers);
+        checkDuplicateWinnerNumber(winnerNumbers);
+        checkWinnerNumberRange(winnerNumbers);
+    }
+
+    private void checkInputNumberCount(List<Integer> winnerNumbers) {
         if (winnerNumbers.size() != LOTTO_NUM_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
+    }
+
+    private void checkDuplicateWinnerNumber(List<Integer> winnerNumbers) {
+        Set<Integer> check = new HashSet<>();
+        for(Integer number : winnerNumbers) {
+            if (!check.add(number)) {
+                throw new IllegalArgumentException("로또 숫자는 중복될 수 없습니다. 중복 번호 : " + number);
+            }
+        }
+    }
+
+    private void checkWinnerNumberRange(List<Integer> winnerNumbers) {
+        winnerNumbers.forEach(this::checkNumber);
     }
 }
