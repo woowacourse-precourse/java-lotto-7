004ad6e0f7ecf22d988dc8c17db206d44ad44db4
speculatingwook <bwook9908@gmail.com>
2024-11-04 13:56:04 +0900

    chore: add annotations for container, config

--- src/main/java/lotto/config/Container.java
+++ src/main/java/lotto/config/Container.java
@@ -4,8 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+/**
+ * Container 클래스는 객체 생성을 관리하는 간단한 DI(Dependency Injection) 컨테이너 역할을 한다.
+ * 이 클래스는 등록된 객체 타입의 인스턴스를 제공하고, 인스턴스를 싱글톤으로 관리한다.
+ */
 public class Container {
+    // 등록된 타입과 해당 객체를 생성하는 Supplier를 매핑하여 저장하는 맵
     private static final Map<Class<?>, Supplier<?>> registry = new HashMap<>();
+    // 생성된 싱글톤 인스턴스를 타입별로 저장하는 맵
     private static final Map<Class<?>, Object> singletons = new HashMap<>();
 
     private Container() {
--- src/main/java/lotto/config/LottoConfig.java
+++ src/main/java/lotto/config/LottoConfig.java
@@ -44,6 +44,16 @@ public class LottoConfig {
                 ));
     }
 
+    /**
+     * 특정 작업이 실패할 경우 재시도하는 기능을 제공하는 메서드.
+     * 주어진 Supplier가 예외를 발생시킬 경우, 계속해서 재시도한다.
+     * 실패 시 사용자가 에러 메시지를 볼 수 있도록 처리하고,
+     * 특정 예외(NoSuchElementException) 발생 시에는 그대로 예외를 던진다.
+     *
+     * @param supplier 실행할 작업을 제공하는 Supplier
+     * @param <T> Supplier가 반환하는 객체의 타입
+     * @return Supplier가 제공하는 객체
+     */
     private static <T> Supplier<T> retryOnFail(Supplier<T> supplier) {
         return () -> {
             while (true) {
