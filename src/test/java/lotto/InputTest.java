abdcc6f27c6fcfce0ef13d0225fb80173b60a3da
Avocado <ujubongbong@gmail.com>
2024-11-04 11:47:37 +0900

    :white_check_mark: test: 당첨번호가 1-45까지의 입력인지 검증 테스트 추가

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -23,7 +23,14 @@ public class InputTest {
 	@Test
 	@DisplayName("번호가 1-45까지의 입력 검증 테스트")
 	void 입력이_1부터_45까지의_번호인지_검증하는_테스트() {
-		String numbers = "1,2,3,4,5,46";
+		int number = 0;
+		assertThrows(IllegalArgumentException.class, () -> input.validateNumberRange(number));
+	}
+
+	@Test
+	@DisplayName("당첨번호가 1-45까지의 입력인지 확인하는 함수")
+	void 당첨번호가_1부터_45까지의_입력인지_확인하는_함수() {
+		String numbers = "0,2,3,4,5,45";
 		assertThrows(IllegalArgumentException.class, () -> input.validateLuckyNumberRange(numbers));
 	}
 }
