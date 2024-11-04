26ed96691ab12ecd52baaa92aa2cbf61ffa480fd
soonhong99 <nanayah99@naver.com>
2024-11-04 19:50:25 +0900

    test(Quantity): 필요없는 테스트 제외
    - 로또 구입 수량에서 1000~50000 숫자에서 1000원단위가 아닌 숫자는
    - 그전 validator에서 판단하여 제거하기 떄문에, 중복되는 테스트 제거
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

--- src/test/java/lotto/lottoMachine/undoPackage/calculateManager/LottoQuantityManagerTest.java
+++ src/test/java/lotto/lottoMachine/undoPackage/calculateManager/LottoQuantityManagerTest.java
@@ -33,36 +33,4 @@ class LottoQuantityManagerTest {
         String expectedOutput = System.lineSeparator() + "5개를 구매했습니다." + System.lineSeparator();
         assertThat(outContent.toString()).isEqualTo(expectedOutput);
     }
-
-    @DisplayName("로또 구입 금액이 0인 경우 티켓 수가 0인지 테스트")
-    @Test
-    void 로또_구입_금액이_0인_경우_티켓_수가_0인지_테스트() {
-        // Given
-        int lottoPurchaseAmount = 0;
-        LottoQuantityManager lottoQuantityManager = new LottoQuantityManager(lottoPurchaseAmount);
-
-        // When
-        int lottoQuantity = lottoQuantityManager.getLottoQuantity();
-
-        // Then
-        assertThat(lottoQuantity).isEqualTo(0);
-        String expectedOutput = System.lineSeparator() + "0개를 구매했습니다." + System.lineSeparator();
-        assertThat(outContent.toString()).isEqualTo(expectedOutput);
-    }
-
-    @DisplayName("로또 구입 금액이 1000의 배수가 아닌 경우 티켓 수가 올바르게 계산되는지 테스트")
-    @Test
-    void 로또_구입_금액이_1000의_배수가_아닌_경우_티켓_수가_올바르게_계산되는지_테스트() {
-        // Given
-        int lottoPurchaseAmount = 3500;
-        LottoQuantityManager lottoQuantityManager = new LottoQuantityManager(lottoPurchaseAmount);
-
-        // When
-        int lottoQuantity = lottoQuantityManager.getLottoQuantity();
-
-        // Then
-        assertThat(lottoQuantity).isEqualTo(3);
-        String expectedOutput = System.lineSeparator() + "3개를 구매했습니다." + System.lineSeparator();
-        assertThat(outContent.toString()).isEqualTo(expectedOutput);
-    }
 }
