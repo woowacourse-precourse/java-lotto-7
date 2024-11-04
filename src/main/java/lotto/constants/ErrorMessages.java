b35a7de6900f9fb8f0ab8ead06b05a7884aa1e25
ryan-dia <cjfdnjs158@naver.com>
2024-11-04 23:23:41 +0900

    refactor: 에러 메시지 상수화
    화# 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)

--- src/main/java/lotto/constants/ErrorMessages.java
+++ src/main/java/lotto/constants/ErrorMessages.java
@@ -7,7 +7,10 @@ public enum ErrorMessages {
     INVALID_AMOUNT("유효하지 않은 금액입니다. 숫자 형식으로 입력해주세요."),
     START_END_DELIMITER("시작과 끝부분에 구분자가 포함될 수 없습니다."),
     WHITESPACE_IN_DELIMITER("구분자 사이에 공백이 포함될 수 없습니다."),
-    INVALID_CHARACTERS("당첨 번호에는 숫자와 허용된 구분자 외의 문자가 포함될 수 없습니다.");
+    INVALID_CHARACTERS("당첨 번호에는 숫자와 허용된 구분자 외의 문자가 포함될 수 없습니다."),
+    NON_NUMERIC_VALUE("숫자가 아닌값은 입력할 수 없습니다."),
+    DUPLICATE_WITH_WINNING_NUMBER("당첨 번호에 중복되는 숫자가 존재합니다."),
+    BONUS_NUMBER_OUT_OF_RANGE("보너스 번호는 1~45사이의 숫자만 입력이 가능합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -2,9 +2,10 @@ package lotto.model;
 
 import java.util.List;
 import lotto.Lotto;
+import lotto.constants.ErrorMessages;
 
 public class BonusNumber {
-    private int bonusNumber;
+    private final int bonusNumber;
 
     private BonusNumber(String userInputNumber, List<Integer> winningNumbers) {
         int number = convertToType(userInputNumber);
@@ -20,16 +21,16 @@ public class BonusNumber {
         try {
             return Integer.parseInt(userInputNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자가 아닌값은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(ErrorMessages.NON_NUMERIC_VALUE.formatMessage());
         }
     }
 
     private void validate(int bonusNumber, List<Integer> winningNumbers) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복되는 숫자가 존재합니다.");
+            throw new IllegalArgumentException(ErrorMessages.DUPLICATE_WITH_WINNING_NUMBER.formatMessage());
         }
-        if (bonusNumber < Lotto.END_NUMBER || bonusNumber > Lotto.END_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45사이의 숫자만 입력이 가능합니다.");
+        if (bonusNumber < Lotto.START_NUMBER || bonusNumber > Lotto.END_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessages.BONUS_NUMBER_OUT_OF_RANGE.formatMessage());
         }
     }
 
