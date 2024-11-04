ed05c0b417caf12e2d28519dcf5412fd71d846ea
siyoung <wfs0502@gmail.com>
2024-11-04 20:22:55 +0900

    fix: Rank 보너스 일치 경우 수정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, true, 2_000_000_000),
+    FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
