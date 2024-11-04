7e2f044050572a0a69275b0b48461ad025ac41e3
likerhythm <wjdalsdl0412@gmail.com>
2024-11-04 14:12:23 +0900

    test(package): payment 패키지로 테스트 이동

--- src/test/java/lotto/model/payment/PaymentValidatorTest.java
+++ src/test/java/lotto/model/payment/PaymentValidatorTest.java
@@ -1,6 +1,5 @@
-package lotto.utils;
+package lotto.model.payment;
 
-import lotto.model.payment.PaymentValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
