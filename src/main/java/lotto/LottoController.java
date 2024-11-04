b683fac5589b91d21905c3dcc9de814a1a8f137f
bee9827 <ehfrhfo9494@naver.com>
2024-11-04 17:21:58 +0900

    feat: Create lotto controller
    각 클래스들을 컨트롤한다.

---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,84 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class LottoController {
+    private Lotto winningLotto;
+    private Integer bonusNumber;
+    private LottoGroup lottoGroup;
+    private LottoRankGroup lottoRankGroup;
+    private final View view = new View();
+
+    public LottoController() {
+    }
+
+    void run() {
+        purchaseLotto(view.getPurchaseCost());
+        view.println(lottoGroup.getLottoGroupSize());
+        view.println(lottoGroup.getLottoNumbers());
+
+        getWinningNumbers(view.getWinningNumbers());
+        getBonusNumber(view.getBonusNumber());
+        lottoRankGroup = LottoRankGroup.of(lottoGroup, winningLotto, bonusNumber);
+
+        view.println(lottoRankGroup.getRankInstructions());
+        view.println(lottoRankGroup.getRateOfRevenue());
+    }
+
+    void purchaseLotto(String input) {
+        while (true) {
+            try {
+                lottoGroup = LottoGroup.create(getPurchaseAmount(Integer.parseInt(input)));
+                break;
+            } catch (IllegalArgumentException e) {
+                view.println("[ERROR] " + e.getMessage());
+                input = view.getPurchaseCost();
+            }
+        }
+    }
+
+    private int getPurchaseAmount(int purchaseCost) {
+        validatePurchaseCost(purchaseCost);
+        return purchaseCost / Lotto.PRICE;
+    }
+
+    private void validatePurchaseCost(int purchaseCost) {
+        if (purchaseCost % Lotto.PRICE != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    void getWinningNumbers(String input) {
+        while (true) {
+            try {
+                winningLotto = new Lotto(getNumbers(input));
+                break;
+            } catch (IllegalArgumentException e) {
+                view.println("[ERROR] " + e.getMessage());
+                input = view.getWinningNumbers();
+            }
+        }
+    }
+
+    private List<Integer> getNumbers(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    void getBonusNumber(String input) {
+        while (true) {
+            try {
+                winningLotto.validateBonusNumber(bonusNumber = Integer.parseInt(input));
+                break;
+            } catch (IllegalArgumentException e) {
+                view.println("[ERROR] " + e.getMessage());
+                input = view.getBonusNumber();
+            }
+        }
+    }
+}
