1fabfb74737f4e485e94e6b65ed0d99e772896a7
caresulseop510 <wndtjq0510@gmail.com>
2024-11-04 20:48:32 +0900

    feat: Implement lotto number range and unique number selection
    - Lotto numbers are now ensured to be within the range 1 to 45
    - Each lotto ticket now selects 6 unique numbers without duplication

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,12 +3,18 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또를 발행해야 한다. 로또 1장의 가격은 1,000원이다.
+// 로또 번호의 숫자 범위는 1~45까지이다.
+// 1개의 로또를 발행할 때 중복되지 않는 6개의 숫자를 뽑는다.
 public class Application {
     public static void main(String[] args) {
         int purchaseAmount = inputPurchaseAmount();
         int lottoCount = countLottos(purchaseAmount);
-        System.out.println(lottoCount);
+        List<Lotto> lottos = generateLottos(lottoCount);
+        displayLottos(lottos);
     }
 
     private static int inputPurchaseAmount() {
@@ -38,5 +44,25 @@ public class Application {
 
     private static int countLottos(int purchaseAmount) {
         return purchaseAmount / 1000;
+    }
+
+    private static List<Lotto> generateLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < lottoCount; i++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
+    }
+
+    private static Lotto generateLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    private static void displayLottos(List<Lotto> lottos) {
+        System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
