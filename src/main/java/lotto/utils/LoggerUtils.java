f6905d3fb72b2c260d55ae178d9830707fdaa9b0
SinnoLn <sintmtn69@gmail.com>
2024-11-04 17:28:16 +0900

    refactor: LoggerUtils 클래스 로깅 구조 개선
    - ConsoleHandler와 SimpleFormatter를 사용하여 로그 출력 형식 통일
    - logDebug 및 logWarn 메서드 제거하여 불필요한 로깅 레벨 정리
    - logError 메서드에서 SEVERE 레벨 사용으로 오류 로그의 명확성 강화
    - logger 상위 핸들러 비활성화로 불필요한 중복 출력 방지

--- src/main/java/lotto/utils/LoggerUtils.java
+++ src/main/java/lotto/utils/LoggerUtils.java
@@ -1,21 +1,29 @@
 package lotto.utils;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class LoggerUtils {
+
+    private static final Logger logger = Logger.getLogger(LoggerUtils.class.getName());
+
+    static {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setFormatter(new SimpleFormatter());
+        consoleHandler.setLevel(Level.INFO);
+        logger.addHandler(consoleHandler);
+        logger.setUseParentHandlers(false);
+    }
+
     private LoggerUtils() {}
 
     public static void logInfo(String message) {
-        System.out.println("[INFO] " + message);
+        logger.log(Level.INFO, message);
     }
 
     public static void logError(String message) {
-        System.err.println("[ERROR] " + message);
-    }
-
-    public static void logDebug(String message) {
-        System.out.println("[DEBUG] " + message);
-    }
-
-    public static void logWarn(String message) {
-        System.out.println("[WARN] " + message);
+        logger.log(Level.SEVERE, message);
     }
 }
