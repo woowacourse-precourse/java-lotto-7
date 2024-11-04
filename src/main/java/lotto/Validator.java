56deee7065729521ec41f45f35ee18d36e9f3ebe
zelatndnjs <zelatndnjs@naver.com>
2024-11-04 20:24:52 +0900

    feat(Validator.java) : validatePositiveAmount 메소드 생성
    돈의 액수가 음수가 0이 아니게끔 천원 이상인지 판단하는 validatePositiveAmount 메소드를 생성했다.

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -2,6 +2,13 @@ package lotto;
 
 public class Validator {
     private static final int LOTTO_PRICE = 1000;
+
+    public static void validatePurchaseAmount(String input) {
+        validateNumeric(input);
+        int price = Integer.parseInt(input);
+        validateDivisibleByLottoPrice(price);
+        validatePositiveAmount(price);
+    }
     public static void validateNumeric(String input) {
         if (!input.matches("\\d+")) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 함");
@@ -11,6 +18,12 @@ public class Validator {
     public static void validateDivisibleByLottoPrice(int price) {
         if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 천원 단위로 나눠 떨어져야 함");
+        }
+    }
+
+    public static void validatePositiveAmount(int price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 천원 이상이어야 함");
         }
     }
 }
