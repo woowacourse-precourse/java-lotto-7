007b65b72e367fca3e818ea90ab2ccf3e3bc637e
josephuk77 <josephuk77@gmail.com>
2024-11-04 19:15:54 +0900

    fix: 순위, 수익률 출력 메서드 매개변수 수정
    매개 변수로 각 순위의 숫자를 받게하는 것에서 Ranking에서 직접 꺼내 쓰는 방식으로 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,26 +3,27 @@ package lotto.view;
 import java.util.List;
 import lotto.message.RunMessage;
 import lotto.model.Lotto;
+import lotto.model.Ranking;
 
 public class OutputView {
 
-    public static void printLottoNumbers(List<Lotto> lottos){
+    public static void printLottoNumbers(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers().toString());
         }
     }
 
-    public static void printLottoCount(int amount){
+    public static void printLottoCount(int amount) {
         System.out.printf(RunMessage.PRINT_LOTTO_COUNT.getMessage(), amount);
     }
 
-    public static void printWinningStatisticsMessage(int first, int second, int third, int fourth, int fifth, double yield){
+    public static void printWinningStatisticsMessage(double yield) {
         System.out.print(RunMessage.PRINT_WINNING_STATISTIC_MESSAGE.getMessage());
-        System.out.printf(RunMessage.PRINT_WINNING_FIFTH.getMessage(),fifth);
-        System.out.printf(RunMessage.PRINT_WINNING_FOURTH.getMessage(),fourth);
-        System.out.printf(RunMessage.PRINT_WINNING_THIRD.getMessage(),third);
-        System.out.printf(RunMessage.PRINT_WINNING_SECOND.getMessage(),second);
-        System.out.printf(RunMessage.PRINT_WINNING_FIRST.getMessage(),first);
-        System.out.printf(RunMessage.PRINT_WINNING_STATISTIC.getMessage(),yield);
+        System.out.printf(RunMessage.PRINT_WINNING_FIFTH.getMessage(), Ranking.FIFTH.getCount());
+        System.out.printf(RunMessage.PRINT_WINNING_FOURTH.getMessage(), Ranking.FOURTH.getCount());
+        System.out.printf(RunMessage.PRINT_WINNING_THIRD.getMessage(), Ranking.THIRD.getCount());
+        System.out.printf(RunMessage.PRINT_WINNING_SECOND.getMessage(), Ranking.SECOND.getCount());
+        System.out.printf(RunMessage.PRINT_WINNING_FIRST.getMessage(), Ranking.FIRST.getCount());
+        System.out.printf(RunMessage.PRINT_WINNING_STATISTIC.getMessage(), yield);
     }
 }
