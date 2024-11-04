fac74a7ce1bfaac642a0463b348e5a3499824e84
Avocado <ujubongbong@gmail.com>
2024-11-04 23:00:15 +0900

    :sparkles: feat: main 함수 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,69 +1,37 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
-	// 뽑는 횟수를 계산하는 함수
-	public static int getCount(int payment) {
-		return payment / 1000;
-	}
-
-	// 로또를 뽑는 함수
-	public static List<Lotto> getLotto(int count) {
-		List<Lotto> lottos = new ArrayList<>();
-		for (int i = 0; i < count; i++) {
-			lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-		}
-		return lottos;
-	}
-
-	// 로또 번호 중에 보너스 번호가 존재하는지 확인하는 함수
-	public static boolean isBonusMatch(List<Integer> lottos, int bonusNumber) {
-		for (Integer num : lottos) {
-			if (num == bonusNumber)
-				return true;
-		}
-		return false;
-	}
-
-	// 로또 번호와 당첨번호를 비교하는 함수
-	public static int compareNumbers(List<Integer> lottos, List<Integer> luckyNumber, int bonusNumber) {
-		return (int)lottos.stream()
-			.filter(luckyNumber::contains)
-			.filter(num -> lottos.contains(bonusNumber))
-			.count();
-	}
-
-	// 전체 수익을 계산하는 함수
-	public static long calculateTotalEarningRate(List<Lotto> lottos, List<Integer> luckyNumber, int bonusNumber) {
-		long total = 0;
-		for (Lotto lotto : lottos) {
-			List<Integer> numbers = lotto.getNumbers();
-			int matchCount = compareNumbers(numbers, luckyNumber, bonusNumber);
-			total += Prize.getPrize(matchCount, isBonusMatch(numbers, bonusNumber)).getReward();
-		}
-		return total;
-	}
-
-	// 수익률 계산하는 함수
-	public static double calculateEarningRate(long total, int payment) {
-		return (double) total / payment * 100;
-	}
-
 	public static void main(String[] args) {
-		Input input = new Input();
+		try {
+			Input input = new Input();
+			Output output = new Output();
+			LottoGame lottoGame = new LottoGame();
 
-		int payment = input.getPayment();
-		int count = getCount(payment);
+			output.init();
+			int payment = input.getPayment();
 
-		List<Lotto> lottos = getLotto(count);
-		List<Integer> luckyNumbers = input.getLuckyNumber();
-		int bonusNumber = input.getBonusNumber(luckyNumbers);
+			int lottoCount = lottoGame.getLottoCount(payment);
+			output.printLottoCount(lottoCount);
 
-		long total = calculateTotalEarningRate(lottos, luckyNumbers, bonusNumber);
-		double earningRate = calculateEarningRate(total, payment);
+			List<Lotto> lottos = lottoGame.getLotto(lottoCount);
+			output.printLottos(lottos);
+
+			output.printLuckyNumber();
+			List<Integer> luckyNumbers = input.getLuckyNumber();
+
+			output.printBonusNumber();
+			int bonusNumber = input.getBonusNumber(luckyNumbers);
+
+			output.printResult(lottos, luckyNumbers, bonusNumber);
+
+			long total = lottoGame.calculateTotalEarningRate(lottos, luckyNumbers, bonusNumber);
+			double earningRate = lottoGame.calculateEarningRate(total, payment);
+
+			output.printEarningRate(earningRate);
+		} catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
 	}
 }
