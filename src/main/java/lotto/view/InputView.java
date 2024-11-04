d0f592d2b5f50d85f0d40967324cb4054b0c3800
jeonghyeon-ohk <ok6737@naver.com>
2024-11-04 21:52:23 +0900

    feat(view): 보너스 번호 입력받기 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,29 +4,28 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.domain.ValueExtractor.convertNumericList;
 import static lotto.domain.ValueExtractor.getDelimitedValue;
 
-import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.validator.NumberValidate;
 
 public class InputView {
-    public static int getAmount(){
-        while(true){
-            try{
+    public static int getAmount() {
+        while (true) {
+            try {
                 System.out.println("구입금액을 입력해 주세요.");
                 String input = readLine();
 
                 NumberValidate.validateAmount(input);
                 return Integer.parseInt(input);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
     public static Lotto getWinningNumber() {
-        while(true){
-            try{
+        while (true) {
+            try {
                 System.out.println("당첨 번호를 입력해 주세요.");
                 String input = readLine();
                 String[] delimitedValue = getDelimitedValue(input);
@@ -34,7 +33,22 @@ public class InputView {
                 List<Integer> WinningNumber = convertNumericList(delimitedValue);
 
                 return new Lotto(WinningNumber);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static int getBonus() {
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String input = readLine();
+
+                NumberValidate.validateBonus(input);
+
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
