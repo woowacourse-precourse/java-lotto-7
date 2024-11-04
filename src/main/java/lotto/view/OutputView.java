c944c15932e41031489813d5c0f1998ede6f7dce
hyosang <billy04100410@gmail.com>
2024-11-04 13:14:34 +0900

    feat(OutputView): 발행 로또 개수 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,13 +7,15 @@ public class OutputView {
 
     private static final String WINNING_STAT_MESSAGE = "당첨 통계\n---\n";
 
+    public void printPublishCountMessage(int publishCount) {
+        System.out.printf("%d개를 구매했습니다.", publishCount);
+    }
+
     public void printWinningStatMessage(Map<Prize, Integer> winningCounts) {
         System.out.printf(WINNING_STAT_MESSAGE);
         for (Prize prize : Prize.values()) {
             System.out.printf("%s (%,d원) - %d개\n", prize.getMessage(), prize.getPrizeAmount(), winningCounts.getOrDefault(prize, 0));
         }
     }
-
-
 
 }
