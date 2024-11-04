47e75deac6ca22d582410996426e5228f956d2a2
tjfwltn <tjfwltn1234@gmail.com>
2024-11-04 18:17:49 +0900

    refactor : LottoErrorMessage INVALID_NUMBER_FORMAT 내용 변경

--- src/main/java/lotto/constants/LottoErrorMessage.java
+++ src/main/java/lotto/constants/LottoErrorMessage.java
@@ -4,7 +4,7 @@ import static lotto.constants.ErrorConstants.*;
 
 public enum LottoErrorMessage implements ErrorMessage {
     INVALID_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
-    INVALID_NUMBER_FORMAT("로또 번호는 숫자여야 합니다."),
+    INVALID_NUMBER_FORMAT("잘못된 형식입니다."),
     NUMBER_OUT_OF_RANGE("로또 번호는 1 이상 45 이하여야 합니다."),
     DUPLICATE_INPUT_NUMBER("중복된 숫자가 입력되었습니다.");
 
