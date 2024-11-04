7a45bd5d8191991df89a4a4b6a36a9a24d0ccb2a
gyuoo <yeomky98@naver.com>
2024-11-05 02:08:54 +0900

    feat(controller): add logic for all lotto games

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
+import lotto.domain.WinningNumber;
 import lotto.service.LottoGameService;
 import lotto.service.LottoPurchaseService;
+import lotto.service.LottoStatisticsService;
 
 public class LottoController {
 	private final LottoGameService lottoGameService;
 	private final LottoPurchaseService lottoPurchaseService;
+	private final LottoStatisticsService lottoStatisticsService;
 
-	public LottoController(LottoGameService lottoGameService, LottoPurchaseService lottoPurchaseService) {
+	public LottoController(LottoGameService lottoGameService, LottoPurchaseService lottoPurchaseService,
+		LottoStatisticsService lottoStatisticsService) {
 		this.lottoGameService = lottoGameService;
 		this.lottoPurchaseService = lottoPurchaseService;
+		this.lottoStatisticsService = lottoStatisticsService;
 	}
 
 	public void play() {
-		List<Lotto> lottos = lottoPurchaseService.buyLottos();
-		LottoResult lottoResult = lottoGameService.playLottoGame(lottos);
+		int purchaseAmount = lottoPurchaseService.purchaseForLottos();
+		List<Lotto> lottos = lottoPurchaseService.buyLottos(purchaseAmount);
+		WinningNumber winningNumber = lottoGameService.setWinningNumber();
+		LottoResult lottoResult = lottoGameService.playLottoGame(lottos, winningNumber);
+		lottoStatisticsService.summarizeStatistics(purchaseAmount, lottos, winningNumber);
 	}
 }
