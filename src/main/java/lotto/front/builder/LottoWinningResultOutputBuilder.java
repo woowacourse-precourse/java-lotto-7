9de24152550d3130ee9c3ef2b09f44be9b22943f
whatsgoodg <rlwjddl1596@naver.com>
2024-11-04 13:54:26 +0900

    refactor(LottoWinningResultOutputBuilder): 코드 컨벤션에 따른 static import 삭제

--- src/main/java/lotto/front/builder/LottoWinningResultOutputBuilder.java
+++ src/main/java/lotto/front/builder/LottoWinningResultOutputBuilder.java
@@ -1,10 +1,8 @@
 package lotto.front.builder;
 
 import java.util.Map;
+import lotto.global.enums.OutputMessage;
 import lotto.global.enums.WinningLottoRank;
-
-import static lotto.global.enums.WinningLottoRank.*;
-import static lotto.global.enums.OutputMessage.*;
 
 public class LottoWinningResultOutputBuilder {
     private static final String NEW_LINE = "\n";
@@ -13,14 +11,14 @@ public class LottoWinningResultOutputBuilder {
     public static String build(Map<WinningLottoRank, Integer> winningCount, Double rateOfReturn) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append(NEW_LINE).append(LOTTO_WINNING_STAT.getMessage()).append(NEW_LINE);
+        stringBuilder.append(NEW_LINE).append(OutputMessage.LOTTO_WINNING_STAT.getMessage()).append(NEW_LINE);
         // 각 LottoDrawRank의 winningCount 수에 맞게 메시지를 생성
         winningCount.forEach((winningLottoRank, count) -> {
             String winningResultMessage = getWinningResultMessage(winningLottoRank);
             String winningResult = formatWinningResult(winningResultMessage, winningLottoRank, count);
             stringBuilder.append(winningResult).append(NEW_LINE);
         });
-        stringBuilder.append(formatRateOfReturn(RATE_OF_RETURN.getMessage(), rateOfReturn));
+        stringBuilder.append(formatRateOfReturn(OutputMessage.RATE_OF_RETURN.getMessage(), rateOfReturn));
 
         return stringBuilder.toString();
     }
@@ -32,10 +30,10 @@ public class LottoWinningResultOutputBuilder {
 
     // LottoDrawRank에 맞는 메시지를 가져옴.
     private static String getWinningResultMessage(WinningLottoRank winningLottoRank) {
-        if (winningLottoRank == SECOND_PLACE) {
-            return SECOND_PLACE_RESULT.getMessage();
+        if (winningLottoRank == WinningLottoRank.SECOND_PLACE) {
+            return OutputMessage.SECOND_PLACE_RESULT.getMessage();
         }
-        return OTHER_PLACE_RESULT.getMessage();
+        return OutputMessage.OTHER_PLACE_RESULT.getMessage();
     }
 
     // 돈을 천 단위로 끊고 결과 메시지를 포맷팅
