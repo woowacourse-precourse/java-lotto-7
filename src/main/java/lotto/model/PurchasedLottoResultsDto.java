675b6e8581e79f597050551dfc7f2abb3e5325ea
Chan531 <chan657312@gmail.com>
2024-11-04 17:59:35 +0900

    feat(PurchasedLottoResultsDto): 생성 메소드 추가

--- src/main/java/lotto/model/PurchasedLottoResultsDto.java
+++ src/main/java/lotto/model/PurchasedLottoResultsDto.java
@@ -6,6 +6,10 @@ public record PurchasedLottoResultsDto(
 	List<PurchasedLottoResultDto> purchasedLottoResults
 ) {
 
+	public static PurchasedLottoResultsDto from(LottoBundle lottoBundle) {
+		return new PurchasedLottoResultsDto(getPurchasedLottoResultDto(lottoBundle));
+	}
+
 	private static List<PurchasedLottoResultDto> getPurchasedLottoResultDto(LottoBundle lottoBundle) {
 		return lottoBundle.getLottos().stream()
 				.map(lotto -> PurchasedLottoResultDto.from(lotto.getNumbers()))
