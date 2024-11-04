ad3ac493e1ac4581eae34f30455280a1327f7c39
likerhythm <wjdalsdl0412@gmail.com>
2024-11-04 15:39:45 +0900

    chore: 코드 가독성을 위해 상수 순서 변경

--- src/main/java/lotto/error/PaymentError.java
+++ src/main/java/lotto/error/PaymentError.java
@@ -4,10 +4,10 @@ import lotto.error.format.ErrorMessageFormat;
 
 public enum PaymentError {
 
-    WRONG_PAYMENT_FORMAT("구입 금액이 잘못된 형식입니다."),
     NEGATIVE_PAYMENT("구입 금액은 음이 아닌 정수를 입력해주세요."),
-    NOT_MULTIPLE_OF_THOUSAND_PAYMENT("구입 금액은 1000의 배수로 입력해주세요."),
-    EXCEED_MAX_PAYMENT("구입 금액이 너무 큽니다.");
+    EXCEED_MAX_PAYMENT("구입 금액이 너무 큽니다."),
+    WRONG_PAYMENT_FORMAT("구입 금액이 잘못된 형식입니다."),
+    NOT_MULTIPLE_OF_THOUSAND_PAYMENT("구입 금액은 1000의 배수로 입력해주세요.");
 
     private final String message;
     private final String prefix = ErrorMessageFormat.PREFIX.getMessage();
