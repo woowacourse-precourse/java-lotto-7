b8b39264c412fb2adb2fe8cac75201be28230982
hanmh319 <hanmh319@naver.com>
2024-11-04 18:06:11 +0900

    refactor: 메서드 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,60 +2,61 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.Locale;
 
 public class Application {
     public static void main(String[] args) {
-        int attempts= validateBetAmount();
+        int attempts = validateBetAmount();
         List<List<Integer>> entryLists = printEntries(attempts);
         List<Integer> lottoNumbers = enterLottoNumbers();
         int bonusNumber = enterBonusNumber(lottoNumbers);
-        printResults(entryLists, lottoNumbers, bonusNumber, attempts);
+        EnumMap<LottoResults, Integer> calculator = resetEnumMap();
+        printResults(entryLists, lottoNumbers, bonusNumber, attempts, calculator);
     }
 
-    private static int validateBetAmount(){
+    private static int validateBetAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         if (!input.matches("\\d+")) {
             throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
         }
         int betAmount = Integer.parseInt(input);
-        if(betAmount<1000){
+        if (betAmount < 1000) {
             throw new IllegalArgumentException("베팅 금액이 1000원 미만입니다");
         }
-        if(betAmount%1000!=0) {
+        if (betAmount % 1000 != 0) {
             throw new IllegalArgumentException("베팅 금액이 1000원 단위가 아닙니다");
         }
-        int attempts = betAmount/1000;
+        int attempts = betAmount / 1000;
         System.out.println(attempts + "개를 구매했습니다.");
         return attempts;
     }
+
     private static List<List<Integer>> printEntries(int attempts) {
         List<List<Integer>> entryLists = new ArrayList<>();
-        for(int i=0; i<attempts; i++){
+        for (int i = 0; i < attempts; i++) {
             List<Integer> entries;
-            entries = Randoms.pickUniqueNumbersInRange(1,45,6);
+            entries = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             entries.sort(Comparator.naturalOrder());
             entryLists.add(entries);
         }
-        for(int j=0; j<attempts; j++){
+        for (int j = 0; j < attempts; j++) {
             System.out.println(entryLists.get(j));
         }
-
         return entryLists;
     }
 
-    private static List<Integer> enterLottoNumbers(){
+    private static List<Integer> enterLottoNumbers() {
         List<Integer> lottoNumbers = new ArrayList<>();
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         String[] splitInput = input.split(",");
-        for(String number : splitInput){
+        for (String number : splitInput) {
             lottoNumbers.add(Integer.parseInt(number));
         }
         lottoNumbers.sort(Comparator.naturalOrder());
@@ -63,47 +64,70 @@ public class Application {
         return lottoNumbers;
     }
 
-    private static int enterBonusNumber(List<Integer> enterLottoNumbers){
+    private static int enterBonusNumber(List<Integer> enterLottoNumbers) {
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
         boolean containNumber = enterLottoNumbers.contains(bonusNumber);
-        if(containNumber){
+        if (containNumber) {
             throw new IllegalArgumentException("당첨 번호와 중복된 숫자입니다");
         }
-        if(bonusNumber>45 || bonusNumber<1){
+        if (bonusNumber > 45 || bonusNumber < 1) {
             throw new IllegalArgumentException("보너스 숫자는 1 이상, 45 이하입니다.");
         }
         return bonusNumber;
     }
 
-    private static void printResults(List<List<Integer>> entryLists, List<Integer> lottoNumbers, int bonusNumber, int attempts){
-        EnumMap<LottoResults, Integer> calculateResults = new EnumMap<>(LottoResults.class);
+    public static EnumMap<LottoResults, Integer> resetEnumMap() {
+        EnumMap<LottoResults, Integer> calculator = new EnumMap<>(LottoResults.class);
+        for (LottoResults result : LottoResults.values()) {
+            calculator.put(result, 0);
+        }
+        return calculator;
+    }
+
+    private static void printResults(List<List<Integer>> entryLists, List<Integer> lottoNumbers, int bonusNumber,
+                                     int attempts, EnumMap<LottoResults, Integer> calculator) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        for(LottoResults result : LottoResults.values()){
-            calculateResults.put(result,0); // 각 값 초기화, 0으로 초기화
-        }
-        for(int i=0; i<attempts; i++){
+        for (int i = 0; i < attempts; i++) {
             List<Integer> matchedNumbers = new ArrayList<>(entryLists.get(i));
             matchedNumbers.retainAll(lottoNumbers);
-            if(matchedNumbers.size()==3){
-                calculateResults.put(LottoResults.THREE_MATCH, calculateResults.get(LottoResults.THREE_MATCH) + 1);
-            }
-            if(matchedNumbers.size()==4) {
-                calculateResults.put(LottoResults.FOUR_MATCH, calculateResults.get(LottoResults.FOUR_MATCH) + 1);
-            }
-            if(matchedNumbers.size()==5){
-                calculateResults.put(LottoResults.FIVE_MATCH, calculateResults.get(LottoResults.FIVE_MATCH) + 1);
-            }
-            if(matchedNumbers.size()==5 && entryLists.get(i).contains(bonusNumber)){
-                calculateResults.put(LottoResults.FIVE_BONUS_MATCH, calculateResults.get(LottoResults.FIVE_BONUS_MATCH) + 1);
-            }
-            if(matchedNumbers.size()==6){
-                calculateResults.put(LottoResults.SIX_MATCH, calculateResults.get(LottoResults.SIX_MATCH) + 1);
-            }
+            checkNumbers(i, matchedNumbers, calculator, entryLists, bonusNumber);
         }
-        for(LottoResults result : LottoResults.values()){
-            System.out.println(result.getHowMany() +" (" + result.getPrize()+ "원) : " + calculateResults.get(result) + "개");
+        NumberFormat formatter = NumberFormat.getInstance(Locale.KOREA);
+        for (LottoResults result : LottoResults.values()) {
+            String formattedPrize = formatter.format(result.getPrize());
+            System.out.println(
+                    result.getHowMany() + " (" + formattedPrize + "원) : " + calculator.get(result) + "개");
+        }
+    }
+
+    private static void checkNumbers(int i, List<Integer> matchedNumbers,
+                                     EnumMap<LottoResults, Integer> calculator,
+                                     List<List<Integer>> entryLists, int bonusNumber) {
+        if (matchedNumbers.size() == 3) {
+            updateResults(calculator, LottoResults.THREE_MATCH);
+        }
+        if (matchedNumbers.size() == 4) {
+            updateResults(calculator, LottoResults.FOUR_MATCH);
+        }
+        if (matchedNumbers.size() == 5) {
+            updateResults(calculator, LottoResults.FIVE_MATCH);
+            checkBonus(i, entryLists, bonusNumber, calculator);
+        }
+        if (matchedNumbers.size() == 6) {
+            updateResults(calculator, LottoResults.SIX_MATCH);
+        }
+    }
+
+    private static void updateResults(EnumMap<LottoResults, Integer> calculator, LottoResults result) {
+        calculator.put(result, calculator.get(result) + 1);
+    }
+
+    private static void checkBonus(int i, List<List<Integer>> entryLists, int bonusNumber,
+                                   EnumMap<LottoResults, Integer> calculator) {
+        if (entryLists.get(i).contains(bonusNumber)) {
+            updateResults(calculator, LottoResults.FIVE_BONUS_MATCH);
         }
     }
 
--- src/main/java/lotto/LottoResults.java
+++ src/main/java/lotto/LottoResults.java
@@ -2,7 +2,7 @@ package lotto;
 
 
     public enum LottoResults{
-        THREE_MATCH("3개 일치", 5000), FOUR_MATCH("4개 일치", 50000), FIVE_MATCH("5개 일치", 1500000), FIVE_BONUS_MATCH("5개 일치, 보너스 볼 일치", 30000000), SIX_MATCH("6개 일치", 200000000);
+        THREE_MATCH("3개 일치", 5000), FOUR_MATCH("4개 일치", 50000), FIVE_MATCH("5개 일치", 1500000), FIVE_BONUS_MATCH("5개 일치, 보너스 볼 일치", 30000000), SIX_MATCH("6개 일치", 2000000000);
         private final String howMany;
         private final int prize;
 
