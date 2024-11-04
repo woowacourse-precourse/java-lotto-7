1c2e78080cbd7e0667082d9370e6b3b28128edc8
hmlssghm <kwonse326@gmail.com>
2024-11-04 16:06:33 +0900

    feat(Payment): 게터 추가

--- src/main/java/lotto/vo/Payment.java
+++ src/main/java/lotto/vo/Payment.java
@@ -27,4 +27,8 @@ public class Payment {
         Validator.checkNegative(money);
         Validator.checkMultiplesOfThousand(money);
     }
+
+    public int getMoney() {
+        return money;
+    }
 }
