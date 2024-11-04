f9860c18a01198ccb1f3f288c50090d01145730d
gikhoon <ghikhoon@naver.com>
2024-11-04 13:01:47 +0900

    feat: 상금 목록에 액수 정보 추가

--- src/main/java/lotto/model/enums/Prize.java
+++ src/main/java/lotto/model/enums/Prize.java
@@ -1,17 +1,23 @@
 package lotto.model.enums;
 
 public enum Prize {
-    FIRST_PRICE("2,000,000,000원"),
-    SECOND_PRICE("30,000,000원"),
-    THIRD_PRICE("1,500,000원"),
-    FOURTH_PRICE("50,000원"),
-    FIFTH_PRICE("5,000원"),
+    FIRST_PRICE(2000000000, "2,000,000,000원"),
+    SECOND_PRICE(30000000, "30,000,000원"),
+    THIRD_PRICE(1500000, "1,500,000원"),
+    FOURTH_PRICE(50000,"50,000원"),
+    FIFTH_PRICE(5000,"5,000원"),
     ;
 
+    private final int amount;
     private final String message;
 
-    Prize(String message) {
+    Prize(int amount, String message) {
+        this.amount = amount;
         this.message = message;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 
     public String getMessage() {
