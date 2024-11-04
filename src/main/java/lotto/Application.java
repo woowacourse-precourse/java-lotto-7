c28270fbe9dfa9a2dda72fcb878b247f32f11756
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-04 14:46:52 +0900

    feat: Implementing user purchase amount input func
    유저 입력 함수 구현
    
    1,000 단위로 떨어져야 하는 것과 모두 숫자로 구성되야 함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final String error_message = "[ERROR]";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int purchaseAmount = getPurchaseAmount();
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            String input = Console.readLine();
+            int purchaseAmount = Integer.parseInt(input);
+            if (purchaseAmount % 1000 != 0){
+                throw new IllegalArgumentException(error_message + "로또 구입 금액은 1,000 단위로 나누어 떨어져야 합니다.");
+            }
+            return purchaseAmount / 1000;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(error_message + "로또 금액은 숫자여야 합니다.");
+        }
     }
 }
