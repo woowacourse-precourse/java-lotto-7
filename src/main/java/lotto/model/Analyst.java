288c50c906d2d7968ec65adc61309a0e3b0b4ce9
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 19:56:54 +0900

    Refactor(Analyst): enum class 생성에 따른 리팩토링
    - 클래스가 가지고 있는 필드를 모두 삭제했습니다.
    - 이 클래스는 데이터를 가지는 게 아니라 메서드만을 통해서 분석과 통계 반환 두 개의 일만 진행합니다.
    - 스트링을 반환하는 게 아니라 수익률만 반환합니다.

--- src/main/java/lotto/model/Analyst.java
+++ src/main/java/lotto/model/Analyst.java
@@ -1,59 +1,28 @@
 package lotto.model;
 
-import java.text.DecimalFormat;
+import static lotto.constant.LottoValue.LOTTO_PRICE;
+import static lotto.constant.LottoValue.LOTTO_RANK_GRADE;
+
 import java.util.List;
 
 public class Analyst {
-    private static final int FIFTH_WINNINGS_MONEY = 5000;
-    private static final int FOURTH_WINNINGS_MONEY = 50000;
-    private static final int THIRD_WINNINGS_MONEY = 1500000;
-    private static final int SECOND_WINNINGS_MONEY = 30000000;
-    private static final int FIRST_WINNINGS_MONEY = 2000000000;
-
-    private int totalEarn;
-    private String yield;
-    private int[] winLottos = new int[6];
-    private static final int[] winningsMoney = new int[]{
-            FIFTH_WINNINGS_MONEY,
-            FOURTH_WINNINGS_MONEY,
-            THIRD_WINNINGS_MONEY,
-            SECOND_WINNINGS_MONEY,
-            FIRST_WINNINGS_MONEY
-    };
-
-    public Analyst(List<LottoResult> results, int inputMoney) {
-        calculateWinLotto(results);
-        calculateTotalEarn(results);
-        calculateYield(inputMoney);
-    }
-
-    public int[] getWinLottos() {
-        return winLottos;
-    }
-
-    public int[] winningsMoney() {
-        return winningsMoney;
-    }
-
-    public String getYield() {
-        return yield;
-    }
-
-    private void calculateWinLotto(List<LottoResult> results) {
-        for (LottoResult result : results) {
-            winLottos[Integer.parseInt(result.getWinningScore())]++;
+    public int[] calculateWinLottoCount(List<LottoResult> LottoResults) {
+        int[] winLottoCount = new int[LOTTO_RANK_GRADE.getValue()];
+        for (LottoResult lottoResult : LottoResults) {
+            int winLottoIndex = Integer.parseInt(lottoResult.getLottoRank());
+            winLottoCount[winLottoIndex]++;
         }
+        return winLottoCount;
     }
 
-    private void calculateTotalEarn(List<LottoResult> results) {
-        totalEarn = results.stream().
-                mapToInt(LottoResult::getWinningsMoney)
+    public double calculateYield(List<LottoResult> LottoResults) {
+        int totalEarn = calculateTotalEarn(LottoResults);
+        return ((double) totalEarn / (LottoResults.size() * LOTTO_PRICE.getValue()) * 100);
+    }
+
+    private int calculateTotalEarn(List<LottoResult> results) {
+        return results.stream()
+                .mapToInt(LottoResult::getWinnings)
                 .sum();
-    }
-
-    private void calculateYield(int inputMoney) {
-        double earningsRate = ((double) totalEarn / inputMoney) * 100;
-        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0#%");
-        yield = decimalFormat.format(earningsRate / 100);
     }
 }
