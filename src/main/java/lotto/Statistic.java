1fd6cb7a435242e7c2f517fe1669146eab88b3c1
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 17:01:59 +0900

    feat(Statistic): 등수에 따른 통계를 만들 수 있다

---
+++ src/main/java/lotto/Statistic.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Statistic {
+
+    private final Map<Rank, Integer> statistics;
+
+    public Statistic(List<Rank> ranks) {
+        this.statistics = createStatistics(ranks);
+    }
+
+    private Map<Rank, Integer> createStatistics(List<Rank> ranks) {
+        Map<Rank, Integer> newStatistics = new TreeMap<>();
+        for (Rank rank : Rank.values()) {
+            int frequency = countRankFrequency(ranks, rank);
+            newStatistics.put(rank, frequency);
+        }
+        return newStatistics;
+    }
+
+    private int countRankFrequency(List<Rank> ranks, Rank objectiveRank) {
+        return Collections.frequency(ranks, objectiveRank);
+    }
+}
