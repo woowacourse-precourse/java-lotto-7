b69b950dad081cffe8e9e788107cdfee8f734b2b
josephuk77 <josephuk77@gmail.com>
2024-11-04 18:44:43 +0900

    feat(printWinningStatistics): 당첨 등수별 횟수 총 수익률 출력
    총 수익률과 담첨 등수를 출력하는 메서드를 구현했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -12,4 +12,14 @@ public class OutputView {
     public static void printLottoCount(int amount){
         System.out.printf(RunMessage.PRINT_LOTTO_COUNT.getMessage(), amount);
     }
+
+    public void printWinningStatisticsMessage(int first, int second, int third, int fourth, int fifth, double yield){
+        System.out.print(RunMessage.PRINT_WINNING_STATISTIC_MESSAGE.getMessage());
+        System.out.printf(RunMessage.PRINT_WINNING_FIFTH.getMessage(),fifth);
+        System.out.printf(RunMessage.PRINT_WINNING_FOURTH.getMessage(),fourth);
+        System.out.printf(RunMessage.PRINT_WINNING_THIRD.getMessage(),third);
+        System.out.printf(RunMessage.PRINT_WINNING_SECOND.getMessage(),second);
+        System.out.printf(RunMessage.PRINT_WINNING_FIRST.getMessage(),first);
+        System.out.printf(RunMessage.PRINT_WINNING_STATISTIC.getMessage(),yield);
+    }
 }
