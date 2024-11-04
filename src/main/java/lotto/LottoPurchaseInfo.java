57143fa82b8802848e835148b0f4fe3584d2f366
xeimun <hinote444@naver.com>
2024-11-04 13:58:00 +0900

    feat(LottoPurchaseInfo, ExceptionMessage): 구입 금액이 최대 허용 금액을 초과할 경우 예외 처리 구현

--- src/main/java/lotto/LottoPurchaseInfo.java
+++ src/main/java/lotto/LottoPurchaseInfo.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import static view.message.ExceptionMessage.NEGATIVE_NUMBER_EXCEPTION_MESSAGE;
+import static view.message.ExceptionMessage.MAX_PURCHASE_AMOUNT_TEN_THOUSAND;
+import static view.message.ExceptionMessage.LIMIT_EXCEEDED_MESSAGE;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -9,6 +11,7 @@ public record LottoPurchaseInfo(BigDecimal purchaseAmount, Lotto lottoNumbers, i
 
     public LottoPurchaseInfo {
         validateNegativeNumber(purchaseAmount);
+        validatePurchaseAmountLimit(purchaseAmount);
     }
 
     public LottoPurchaseInfo(BigDecimal purchaseAmount, List<Integer> lottoNumbers, int bonusNumber) {
@@ -18,6 +21,12 @@ public record LottoPurchaseInfo(BigDecimal purchaseAmount, Lotto lottoNumbers, i
     private void validateNegativeNumber(BigDecimal purchaseAmount) {
         if (purchaseAmount.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException(NEGATIVE_NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validatePurchaseAmountLimit(BigDecimal purchaseAmount) {
+        if (purchaseAmount.compareTo(MAX_PURCHASE_AMOUNT_TEN_THOUSAND) > 0) {
+            throw new IllegalArgumentException(LIMIT_EXCEEDED_MESSAGE);
         }
     }
 }
--- src/main/java/view/message/ExceptionMessage.java
+++ src/main/java/view/message/ExceptionMessage.java
@@ -1,9 +1,13 @@
 package view.message;
 
+import java.math.BigDecimal;
+
 public class ExceptionMessage {
 
     public static final String BANK_EXCEPTION_MESSAGE = "입력값이 빈 공백이거나 비어있으면 안 됩니다.";
     public static final String NUMBER_EXCEPTION_MESSAGE = "입력값은 숫자여야 합니다.";
     public static final String NUMBER_REGEX = "\\d+";
     public static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "구입 금액은 음수이면 안됩니다.";
+    public static final String LIMIT_EXCEEDED_MESSAGE = "로또 구입 금액은 10만 원을 초과할 수 없습니다.";
+    public static final BigDecimal MAX_PURCHASE_AMOUNT_TEN_THOUSAND = BigDecimal.valueOf(100_000);
 }
