8bccdc2bbc45557160c4381a58fb55a620239f44
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-04 14:33:44 +0900

    feat: 로또번호 정렬

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,18 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.config.LottoSettings;
 import lotto.error.Error;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int MIN_NUMBER = LottoSettings.MIN_NUMBER.getValue();
+    private static final int MAX_NUMBER = LottoSettings.MAX_NUMBER.getValue();
+    private static final int COUNT = LottoSettings.COUNT.getValue();
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,8 +21,13 @@ public class Lotto {
     }
 
     public static Lotto newInstance() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1,45, 6);
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER,MAX_NUMBER, COUNT);
+        Collections.sort(randomNumbers);
         return new Lotto(randomNumbers);
+    }
+
+    protected List<Integer> getNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -27,14 +37,14 @@ public class Lotto {
     }
 
     private void validateCount(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != COUNT) {
             throw new IllegalArgumentException(Error.INVALID_LOTTO_COUNT.getMessage());
         }
     }
 
     private void validateWithinRange(List<Integer> numbers) {
         for (int num: numbers) {
-            if (num < 1 || num > 45) {
+            if (num < MIN_NUMBER || num > MAX_NUMBER) {
                 throw new IllegalArgumentException(Error.OUT_OF_RANGE.getMessage());
             }
         }
