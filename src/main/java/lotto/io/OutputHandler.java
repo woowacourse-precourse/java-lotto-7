e4cc851e80b336eecac80ae5b1204676cb581a8c
wooyoungKwon <rnjsdndud00@naver.com>
2024-11-04 15:39:55 +0900

    feat(io): 출력 함수 추가

--- src/main/java/lotto/io/OutputHandler.java
+++ src/main/java/lotto/io/OutputHandler.java
@@ -1,7 +1,23 @@
 package lotto.io;
 
+import static lotto.constant.PrintMessageConstants.PURCHASE_COUNT;
+
+import java.util.List;
+import lotto.lotto.object.Lotto;
+
 public class OutputHandler {
-    public void promptPrint(String message) {
+    public void printPrompt(String message) {
         System.out.println(message);
+    }
+
+    public void printPurchasedLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + PURCHASE_COUNT);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+    public void printLineBreak() {
+        System.out.println("\n");
     }
 }
