70128f7231b5aa32b3a6ca3f5b8377a8a7ce1990
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-11-04 23:11:35 +0900

    refactor: 에러메시지 콘솔 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,20 +5,29 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        int purchaseAmount = InputView.requestPurchaseAmount();
-        List<Lotto> lottos = LottoGenerator.generateLottos(purchaseAmount);
+        try {
+            int purchaseAmount = InputView.requestPurchaseAmount();
+            List<Lotto> lottos = LottoGenerator.generateLottos(purchaseAmount);
 
-        System.out.printf("\n%d개를 구매했습니다.\n", lottos.size());
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto);
+            System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+            for (Lotto lotto : lottos) {
+                System.out.println(lotto);
+            }
+
+            List<Integer> winningNumbers = WinningNumberInput.requestWinningNumbers();
+            int bonusNumber = BonusNumberInput.requestBonusNumber(winningNumbers);
+
+            Map<Integer, Integer> resultCounts = LottoResultChecker.checkLottoResults(lottos, winningNumbers, bonusNumber);
+            LottoProfitCalculator.calculateAndPrintProfit(
+                    purchaseAmount,
+                    resultCounts,
+                    resultCounts.getOrDefault(6, 0),
+                    resultCounts.getOrDefault(5, 0) - resultCounts.getOrDefault(5, 0)
+            );
+
+        } catch (IllegalArgumentException e) {
+            // 예외 메시지를 콘솔에 출력하고 프로그램 종료
+            System.out.println(e.getMessage());
         }
-        List<Integer> winningNumbers = WinningNumberInput.requestWinningNumbers();
-        int bonusNumber = BonusNumberInput.requestBonusNumber(winningNumbers);
-
-        Map<Integer, Integer> resultCounts = LottoResultChecker.checkLottoResults(lottos, winningNumbers, bonusNumber);
-        LottoProfitCalculator.calculateAndPrintProfit(purchaseAmount, resultCounts, resultCounts.getOrDefault(6, 0), resultCounts.getOrDefault(5, 0) - resultCounts.getOrDefault(5, 0));
     }
-
-
-
 }
