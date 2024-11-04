ebc8ecd2dc19326c527f7a92da2781cb699fcb32
최영우 <allnstar@naver.com>
2024-11-04 14:54:12 +0900

    style: 줄인 파라미터명 수정

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -3,7 +3,7 @@ package lotto.utils;
 public class Utils {
     private static final String POSITIVE_INTEGER_REGEX = "^[0-9]\\d*$";
 
-    public static boolean isNumeric(String str) {
-        return str.matches(POSITIVE_INTEGER_REGEX);
+    public static boolean isNumeric(String input) {
+        return input.matches(POSITIVE_INTEGER_REGEX);
     }
 }
