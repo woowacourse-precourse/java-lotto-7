5b05efbbb6d5ac5e76ba70ab49d677e30d4a94f7
xeimun <hinote444@naver.com>
2024-11-04 19:57:08 +0900

    test(LottoService): 로또 결과를 계산하는 테스트 구현

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -6,9 +6,12 @@ import java.math.BigDecimal;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import lotto.TestNumberGenerator;
+import lotto.domain.Lotto;
 import lotto.domain.LottoPurchaseInfo;
 import lotto.domain.LottoRank;
 import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +42,43 @@ public class LottoServiceTest {
 
         // then
         assertThat(returnOnInvestment).isEqualTo("62.5");
+    }
+
+    @Test
+    @DisplayName("꽝 7개, 5등 당첨 1개")
+    void 로또_결과를_계산한다() {
+        // given
+        Map<LottoRank, Integer> expectedRankCounts = new EnumMap<>(LottoRank.class);
+        expectedRankCounts.put(LottoRank.NONE, 7);
+        expectedRankCounts.put(LottoRank.FIFTH, 1);
+        expectedRankCounts.put(LottoRank.FOURTH, 0);
+        expectedRankCounts.put(LottoRank.THIRD, 0);
+        expectedRankCounts.put(LottoRank.SECOND, 0);
+        expectedRankCounts.put(LottoRank.FIRST, 0);
+
+        List<List<Integer>> testNumbers = List.of(
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+
+        TestNumberGenerator testNumberGenerator = new TestNumberGenerator(testNumbers);
+        BigDecimal purchaseQuantity = BigDecimal.valueOf(testNumbers.size());
+        Lottos lottos = Lottos.of(purchaseQuantity, testNumberGenerator);
+
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        // When
+        LottoResult lottoResult = lottoService.calculateLottoResult(lottos, userLotto, bonusNumber);
+
+        // Then
+        assertThat(lottoResult.getRankCounts()).isEqualTo(expectedRankCounts);
     }
 
     @Test
