15805bff54bf3f7e45c872f0170053fbbd94aabc
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 18:25:13 +0900

    feat(Money): 로또 í 구입 금액 및 티켓 개수 기능 구현

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final Integer MIN_NUMBER = 1;
     public static final Integer MAX_NUMBER = 45;
     public static final Integer PICK_NUMBER = 6;
+
+    public static final int LOTTO_UNIT = 1000;
 }
--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,4 +1,22 @@
 package lotto.model;
 
+import lotto.Constants;
+
+import static lotto.Constants.*;
+
 public class Money {
+
+    private final int money;
+
+    public Money(String stringMoney){
+    this.money = Integer.parseInt(stringMoney);
+    }
+
+    public int getMoney(){
+        return money;
+    }
+
+    public int getLottoCount(){
+        return money/ LOTTO_UNIT;
+    }
 }
