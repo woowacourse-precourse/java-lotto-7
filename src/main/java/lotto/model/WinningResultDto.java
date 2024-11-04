06e49748d28e4b9f10b57053f83ba72912a340fe
Chan531 <chan657312@gmail.com>
2024-11-04 17:14:15 +0900

    refactor(WinningResultDto): 인스턴스 변수 추가 및 생성자 수정
    보너스 번호 포함 여부 추가

--- src/main/java/lotto/model/WinningResultDto.java
+++ src/main/java/lotto/model/WinningResultDto.java
@@ -3,10 +3,11 @@ package lotto.model;
 public record WinningResultDto(
 	int matchCount,
 	long prize,
+	boolean hasBonusNumber,
 	int winningCount
 ) {
 
 	public static WinningResultDto from(Winning winning, int count) {
-		return new WinningResultDto(winning.getCount(), winning.getPrize(), count);
+		return new WinningResultDto(winning.getCount(), winning.getPrize(), winning.getHasBonusNumber(), count);
 	}
 }
