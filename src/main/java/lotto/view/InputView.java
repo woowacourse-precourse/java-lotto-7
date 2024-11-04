ceff9805607779bedcbf7d0c18270dad887935a4
mikekks <thdalsrb79@naver.com>
2024-11-04 11:30:14 +0900

    feat(InputView): 로또 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import static lotto.global.ErrorMessage.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -9,6 +11,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 	private static final Pattern NUMBER_FORMAT = Pattern.compile("\\d+");
+	private static final String LOTTO_NUMBER_SEPARATOR = ",";
 
 	public static int readLottoPurchaseMoney() {
 		OutputView.printMessage("구입금액을 입력해 주세요.");
@@ -19,7 +22,23 @@ public class InputView {
 		return Integer.parseInt(amount);
 	}
 
-	public static void validateNumber(String input) {
+	public static List<Integer> readWinningNumbers() {
+		OutputView.printMessage("당첨 번호를 입력해 주세요.");
+		String[] winningNumbers = Console.readLine().split(LOTTO_NUMBER_SEPARATOR);
+		validateNumbers(winningNumbers);
+
+		return Arrays.stream(winningNumbers)
+			.map(Integer::parseInt)
+			.toList();
+	}
+
+	private static void validateNumbers(String[] inputs) {
+		for (String input : inputs) {
+			validateNumber(input);
+		}
+	}
+
+	private static void validateNumber(String input) {
 		if (!NUMBER_FORMAT.matcher(input).matches()) {
 			throw new IllegalArgumentException(INVALID_MONEY_TYPE_ERROR.getMessage());
 		}
