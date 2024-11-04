59950f6edc6033e4f543cd6d3b31aa08758c49ca
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 12:04:52 +0900

    chore(InputView): 구입 금액 관련 메서드명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,8 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String inputPrice() {
-        printInputPrice();
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String inputPurchaseAmount() {
+        printInputPurchaseAmount();
         return Console.readLine();
     }
 
@@ -19,16 +23,16 @@ public class InputView {
         return Console.readLine();
     }
 
-    private void printInputPrice() {
-        System.out.println("구입 금액을 입력해 주세요.");
+    private void printInputPurchaseAmount() {
+        System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
     }
 
     private void printInputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
     }
 
     private void printInputBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
     }
 
 }
