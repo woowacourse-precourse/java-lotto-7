b0fe9b4e5f3b39152a03f7116ee1d94c61c3f1f0
hootzz <wjqwjql3@gmail.com>
2024-11-04 17:10:28 +0900

    refactor(calculator): WinningCalculator 로직 스트림 대신 명시적 반복문 사용, lottos의  forEach 메서드  활용

--- src/main/java/lotto/domain/calculator/WinningCalculator.java
+++ src/main/java/lotto/domain/calculator/WinningCalculator.java
@@ -2,6 +2,7 @@ package lotto.domain.calculator;
 
 import lotto.domain.result.LottoRank;
 import lotto.domain.result.WinningStatistics;
+import lotto.domain.ticket.Lotto;
 import lotto.domain.ticket.Lottos;
 import lotto.domain.ticket.WinningLotto;
 
@@ -11,31 +12,29 @@ import java.util.Map;
 public class WinningCalculator {
     public WinningStatistics calculate(Lottos lottos, WinningLotto winningLotto) {
         Map<LottoRank, Integer> rankCounts = initializeRankCounts();
-        updateRankCounts(rankCounts, lottos, winningLotto);
+        countWinningResults(lottos, winningLotto, rankCounts);
         return WinningStatistics.from(rankCounts);
     }
 
     private Map<LottoRank, Integer> initializeRankCounts() {
         Map<LottoRank, Integer> rankCounts = new EnumMap<>(LottoRank.class);
-        initializeAllRanks(rankCounts);
-        return rankCounts;
-    }
-
-    private void initializeAllRanks(Map<LottoRank, Integer> rankCounts) {
         for (LottoRank rank : LottoRank.values()) {
             rankCounts.put(rank, 0);
         }
+        return rankCounts;
     }
 
-    private void updateRankCounts(Map<LottoRank, Integer> rankCounts, Lottos lottos, WinningLotto winningLotto) {
-        lottos.applyToEachLotto((lotto) -> {
-            LottoRank rank = winningLotto.calculateRank(lotto);
+    private void countWinningResults(
+            Lottos lottos,
+            WinningLotto winningLotto,
+            Map<LottoRank, Integer> rankCounts) {
+        lottos.forEach(lotto -> {
+            LottoRank rank = winningLotto.match(lotto);
             addCount(rankCounts, rank);
         });
     }
 
     private void addCount(Map<LottoRank, Integer> rankCounts, LottoRank rank) {
-        int count = rankCounts.getOrDefault(rank, 0);
-        rankCounts.put(rank, count + 1);
+        rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
     }
 }
