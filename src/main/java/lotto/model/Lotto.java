1d0362b47a508b593a41422eb29fde519a58fb60
MinSang22Kim <powerminsang9901@gmail.com>
2024-11-04 23:34:49 +0900

    refactor: Lotto 검증 로직 LottoValidator 클래스로 분리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,50 +1,18 @@
 package lotto.model;
 
+import lotto.exception.LottoException;
 import lotto.message.ErrorMessage;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import static lotto.validator.LottoValidator.validateLottoNumbers;
 
 public class Lotto {
-    private static final int REQUIRED_NUMBER_COUNT = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(List<Integer> numbers) throws LottoException {
+        validateLottoNumbers(numbers);
         this.numbers = numbers.stream()
                 .sorted()
                 .toList();
-    }
-
-    private void validate(List<Integer> numbers) {
-        validateSize(numbers);
-        validateUnique(numbers);
-        validateRange(numbers);
-    }
-
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != REQUIRED_NUMBER_COUNT) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_COUNT_INVALID.getMessage());
-        }
-    }
-
-    private void validateUnique(List<Integer> numbers) {
-        Set<Integer> numberSet = new HashSet<>(numbers);
-        if (numberSet.size() != REQUIRED_NUMBER_COUNT) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_DUPLICATE.getMessage());
-        }
-    }
-
-    private void validateRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < MIN_NUMBER || number > MAX_NUMBER) {
-                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_OUT_OF_RANGE.getMessage());
-            }
-        }
     }
 
     public List<Integer> getNumbers() {
