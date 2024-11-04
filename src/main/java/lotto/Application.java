79b37e7e160d7d372031db38ebfd24300bb93cb9
wouldu <jhw664089@gmail.com>
2024-11-04 10:14:29 +0900

    feat: 구입 금액 입력 및 검증 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+
+    public static int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int amount = Integer.parseInt(input);
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return amount;
     }
 }
