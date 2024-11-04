9241a6c5fa31e33c1fd558cbd516b8deec7b9c1a
wouldu <jhw664089@gmail.com>
2024-11-04 22:53:25 +0900

    refactor: LottoGame에서 분리된 서비스 사용하도록 수정

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -9,24 +9,39 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import lotto.service.LottoGenerationService;
+import lotto.service.LottoResultService;
+import lotto.service.LottoStatisticsService;
 import lotto.model.Lotto;
 import lotto.model.Rank;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
 public class LottoGame {
+    private final LottoGenerationService generationService;
+    private final LottoResultService resultService;
+    private final LottoStatisticsService statisticsService;
+
+    public LottoGame() {
+        this.generationService = new LottoGenerationService();
+        this.resultService = new LottoResultService();
+        this.statisticsService = new LottoStatisticsService();
+    }
 
     public void start() {
         int purchaseAmount = InputView.inputPurchaseAmount();
-        int numberOfLottos = purchaseAmount / 1000;
-        List<Lotto> lottos = generateLottos(numberOfLottos);
+        List<Lotto> lottos = generationService.generateLottos(purchaseAmount);
         ResultView.printLottos(lottos);
 
         List<Integer> winningNumbers = InputView.inputWinningNumbers();
         int bonusNumber = InputView.inputBonusNumber();
 
-        Map<Rank, Integer> results = calculateResults(lottos, winningNumbers, bonusNumber);
-        ResultView.printResults(results, purchaseAmount);
+        Map<Rank, Integer> results = resultService.calculateResults(lottos, winningNumbers, bonusNumber);
+        int totalPrize = statisticsService.calculateTotalPrize(results);
+        double profitRate = statisticsService.calculateProfitRate(totalPrize, purchaseAmount);
+
+        ResultView.printResults(results, profitRate);
     }
 
     public static List<Lotto> generateLottos(int count) {
