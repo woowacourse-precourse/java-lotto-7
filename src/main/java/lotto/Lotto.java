6282c6d3ecae2faab2acc0fc13980e6e8c3686d8
wjdgkrud1 <wjdgkrud1@naver.com>
2024-11-04 16:22:44 +0900

    기존 로또 클래스에 중복 예외 처리 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,19 +2,35 @@ package lotto;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		checkForDuplicates(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	private void checkForDuplicates(List<Integer> numbers) {
+		for (int i = 0; i < numbers.size(); i++) {
+			checkIfDuplicate(numbers, numbers.get(i), i);
+		}
+	}
+
+	private void checkIfDuplicate(List<Integer> numbers, Integer number, int index) {
+		for (int j = 0; j < index; j++) {
+			if (numbers.get(j).equals(number)) {
+				throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+			}
+		}
+
+	}
 }
