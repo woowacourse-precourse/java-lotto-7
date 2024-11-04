8d3370411c506538aca06eb962ce1d8e8f9e0c21
maedachaeva <parkchaeyeon.b@gmail.com>
2024-11-04 11:32:42 +0900

    refactor(Lotto): 잘못된 구현 삭제

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,15 +1,8 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
 
 public class Lotto {
-    private static final int LOTTO_MIN_NUMBER = 1;
-    private static final int LOTTO_MAX_NUMBER = 45;
-    private static final int LOTTO_NUMBER_COUNT = 6;
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,21 +14,5 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
-
-    public static Lotto generateWinningNumbers() {
-        List<Integer> winningNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
-        return new Lotto(winningNumbers);
-    }
-
-    public static int generateBonusNumber(Lotto winningNumbers) {
-        Set<Integer> uniqueWinningNumbers = new HashSet<>(winningNumbers.numbers);
-        int bonusNumber;
-
-        do {
-            bonusNumber = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, 1).get(0);
-        } while (uniqueWinningNumbers.contains(bonusNumber));
-
-        return bonusNumber;
     }
 }
