e291f0b5eb62c1c6897a22528dfea3f3eb586400
sodychoe <sodychoe@icloud.com>
2024-11-04 21:51:41 +0900

    feat(Application) : main 메서드에서 애플리케이션 시작점 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = AppConfig.getAppConfig();
+        LottoMachine lottoMachine = new LottoMachine(appConfig);
+
+        lottoMachine.run();
     }
 }
