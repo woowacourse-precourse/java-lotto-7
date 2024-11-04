6d08c129c56baef92489e54b238ac1af3fb2f957
soonhong99 <nanayah99@naver.com>
2024-11-04 11:29:28 +0900

    feat(Lotto Number Generator): 로또 숫자 생성기 구현
    로또 머신 패키지 안에 넣은 이유:
    - 로또 기계의 가장 중요한 역할이라 판단
    - 따라서 최상단에 해당 클래스 넣음
    
    Signed-off-by: soonhong99 <nanayah99@naver.com>

---
+++ src/main/java/lottoMachine/LottoNumberGenerator.java
@@ -0,0 +1,25 @@
+package lottoMachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import utils.StaticFinalMessages;
+
+public class LottoNumberGenerator {
+    public List<Integer> generateLottoNumbers() {
+        // 새로운 ArrayList를 생성하여 수정 가능한 리스트로 만듦
+        // 왜 기존코드는 불변인지는 모르겠엄;;;
+        List<Integer> numbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(
+                        StaticFinalMessages.MINIMUM_LOTTO_NUMBER,
+                        StaticFinalMessages.MAXIMUM_LOTTO_NUMBER,
+                        StaticFinalMessages.AMOUNT_OF_LOTTO_NUMBERS
+                )
+        );
+
+        Collections.sort(numbers);
+
+        return numbers;
+    }
+}
