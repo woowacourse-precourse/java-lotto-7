cad844c1324b3f21c0e733042fce74c737c03fd7
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-04 16:58:42 +0900

    refactor : Application 클래스 포매팅

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,8 +3,8 @@ package lotto;
 import lotto.controller.LottoController;
 
 public class Application {
-    public static void main(String[] args) {
-        LottoController lottoController = LottoController.getInstance();
-        lottoController.start();
-    }
+	public static void main(String[] args) {
+		LottoController lottoController = LottoController.getInstance();
+		lottoController.start();
+	}
 }
