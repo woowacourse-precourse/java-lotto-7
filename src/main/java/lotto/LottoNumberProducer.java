0a1b72bd93573d6c11e5cd8f1b532d5e6952c61b
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 21:03:28 +0900

    feat(LottoNumberProducer): 로또 번호들을 반환하는 함수를 생성한다
    로또 번호들을 반환하는 getLottoNumbers함수를 생성한다.

--- src/main/java/lotto/LottoNumberProducer.java
+++ src/main/java/lotto/LottoNumberProducer.java
@@ -5,25 +5,32 @@ import java.util.List;
 import java.util.Random;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.internal.Integers;
 
 public class LottoNumberProducer {
-    public  List<List<Integer>> lottoNumbers;
-    private  int lottoNum;
-    LottoNumberProducer(int lottoNum){
+    public List<List<Integer>> lottoNumbers;
+    private int lottoNum;
+
+    LottoNumberProducer(int lottoNum) {
         this.lottoNum = lottoNum;
         this.lottoNumbers = new ArrayList<>();  // 리스트 초기화
     }
 
-    public void createRandomNumbers(){
+    public void createRandomNumbers() {
 
-        for(int i=0; i<lottoNum; i++){
-           lottoNumbers.add(Randoms.pickUniqueNumbersInRange(1,45,6));
+        for (int i = 0; i < lottoNum; i++) {
+            lottoNumbers.add(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         }
     }
 
-    public void displayLottoNumbers(){
-        for(int i=0; i<lottoNum; i++){
+    public void displayLottoNumbers() {
+        //오름차순으로 보여줘야함
+        for (int i = 0; i < lottoNum; i++) {
             System.out.println(lottoNumbers.get(i));
         }
+    }
+
+    public List<List<Integer>> getLottoNumbers() {
+        return this.lottoNumbers;
     }
 }
