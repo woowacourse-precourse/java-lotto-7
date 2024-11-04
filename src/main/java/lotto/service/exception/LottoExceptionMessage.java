248bd79856e58464c63559bccf04b7cfe74e565c
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:38:42 +0900

    Feat: 당첨 번호가 보너스 번호가 중복될 경우 출력할 예외 메시지 구현

--- src/main/java/lotto/service/exception/LottoExceptionMessage.java
+++ src/main/java/lotto/service/exception/LottoExceptionMessage.java
@@ -5,7 +5,9 @@ import lotto.exception.ExceptionMessage;
 public enum LottoExceptionMessage implements ExceptionMessage {
 
     // 당첨 번호
-    INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 1 이상 45 이하의 숫자 6개를 콤마로 구분하여 입력해야 합니다.")
+    INVALID_WINNING_NUMBERS("[ERROR] 당첨 번호는 1 이상 45 이하의 숫자 6개를 콤마로 구분하여 입력해야 합니다."),
+    // 보너스 번호
+    BONUS_NUMBER_DUPLICATED("[ERROR] 당첨 번호와 보너스 번호가 중복되었습니다."),
     ;
 
     private final String message;
