d45f006af4a66974713a225fd2381927c6ae2c62
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-04 23:51:25 +0900

    refactor: InputView의 매직 리터럴 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String CHECK_NUMBER_REGEX = "\\d+";
+    private static final String DELIMITER = ",";
 
     private InputView() {
     }
@@ -70,7 +71,7 @@ public class InputView {
 
     private static List<Integer> splitAndConvertUserInput(String userInput) {
         try {
-            return List.of(userInput.split(",")).stream()
+            return List.of(userInput.split(DELIMITER)).stream()
                     .map(String::strip)
                     .map(Integer::parseInt)
                     .toList();
