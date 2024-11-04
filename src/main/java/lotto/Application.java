b9383885ee1a1c73addc9cb9a8e8e163f6612b4e
kimsongie12 <song3078@naver.com>
2024-11-04 17:51:12 +0900

    feat: 실행 역할 사용자 인터페이스와 게임 로직 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import javax.swing.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+        LottoGame game = new LottoGame();
+        LottoController controller = new LottoController(view, game);
+
+        controller.start();
     }
 }
