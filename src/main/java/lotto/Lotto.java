8e28e385c9f688a29fc7b2133ae0ce866ba61725
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-04 21:54:21 +0900

    refactor : Error duplicate numbers in lotto numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,7 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,7 +18,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 숫자가 있을 수 없습니다.");
+        }
     }
+
+
 
     public static List<Integer> pickRandomNumber() {
             return Randoms.pickUniqueNumbersInRange(1, 45, 6);
