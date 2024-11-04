7194eae5a7335e5408f8495a045eb2f1f156451d
LeeHaeJun <haejun0410@gmail.com>
2024-11-04 21:14:25 +0900

    feat: 당첨 번호 입력받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 
 public class InputView {
 
@@ -12,5 +17,16 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 올바른 숫자 금액을 입력해주세요.");
         }
+    }
+
+    public static Lotto inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해주세요");
+        String input = Console.readLine();
+        List<LottoNumber> numbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+        return new Lotto(numbers);
     }
 }
