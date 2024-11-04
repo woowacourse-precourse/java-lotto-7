14d3f74f45d3207c900bdca0837a7e2cf85e904f
Timothy <timothy@jungnanum.com>
2024-11-04 22:28:00 +0900

    feat: MVC 모델의 View를 이용하여, 추첨자의 입력값 처리
    ApplicationTest.java "예외_테스트" 테스트 메소드 : View를 통해서 입력 값을 입력받을 때, "1000j"가 입력되면 예외가 발생하는지 테스트

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,41 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.repository.Lotto;
+import lotto.service.LottoPublishService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    LottoPublishService lottoPublishService = LottoPublishService.getInstance();
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void startLottoGame() {
+        this.buyLotto();
+        this.lottoPublish();
+    }
+
+    private void buyLotto() {
+        this.requestInputPrice();
+    }
+
+    private void requestInputPrice() {
+        try {
+            lottoPublishService.lottoPublish(inputView.requestInputPrice());
+        } catch (IllegalArgumentException e) {
+            inputView.printMessage(e.getMessage());
+            this.requestInputPrice();
+        }
+    }
+
+    private void lottoPublish() {
+        List<Lotto> lottos = lottoPublishService.getPublishedLottoNumbers();
+        outputView.printPublishCount(lottos.size());
+    }
+}
--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,18 +1,19 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
+    @Disabled
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
