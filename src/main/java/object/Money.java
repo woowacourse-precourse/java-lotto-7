fbc0201ee6c08d6a15ccaaf2d4af801a9dbe6040
HARDY0070 <hwv1219@gmail.com>
2024-11-04 18:09:31 +0900

    style(object): Money 클래스 예외처리 메소드명 수정

--- src/main/java/object/Money.java
+++ src/main/java/object/Money.java
@@ -4,18 +4,18 @@ public class Money {
     private final int money;
 
     public Money(int money) {
-        validateMinimumPrice(money);
-        validateBuyRegulation(money);
+        minimumPriceValidate(money);
+        buyRegulationValidate(money);
         this.money = money;
     }
 
-    private void validateMinimumPrice(int money) {
+    private void minimumPriceValidate(int money) {
         if (money < 1000) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최소 1000원 입니다.");
         }
     }
 
-    private void validateBuyRegulation(int money) {
+    private void buyRegulationValidate(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구입할 수 있습니다.");
         }
