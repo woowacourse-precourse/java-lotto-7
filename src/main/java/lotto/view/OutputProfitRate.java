09f4afc7faedfc2941b122060d2f3c3986a452c3
JiHyeonSu <itip1@mju.ac.kr>
2024-11-04 20:06:57 +0900

    feat(view): OutputProfitRate 추가 -총 수익률을 출력합니다.

---
+++ src/main/java/lotto/view/OutputProfitRate.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputProfitRate {
+
+  private static final String MESSAGE_PROFIT_RATE = "총 수익률은 %.1f%%입니다.";
+
+  public static void printProfitRate(double profitRate) {
+    System.out.println(String.format(MESSAGE_PROFIT_RATE, profitRate));
+  }
+}
