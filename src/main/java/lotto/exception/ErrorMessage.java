a61364fab5ecacc032c2547556ce9c74161410f4
gyuoo <yeomky98@naver.com>
2024-11-04 23:28:47 +0900

    feat(exception): add error messages

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
 	INVALID_PURCHASE_AMOUNT("구입 금액은 " + LOTTO_PRICE + " 단위의 양의 정수를 입력해야 합니다."),
 	NOT_DIVISIBLE_BY_LOTTO_PRICE("구입 금액은 " + LOTTO_PRICE + "원 단위여야 합니다."),
 	MAX_PURCHASE_EXCEED("1회 최대 구매 가능 금액은 " + MAX_PURCHASE_AMOUNT + "입니다."),
-	INVALID_LOTTO_NUMBERS_COUNT("로또 번호는 " + LOTTO_NUMBERS_COUNT + "개여야 합니다.");
+	INVALID_LOTTO_NUMBERS_COUNT("로또 번호는 " + LOTTO_NUMBERS_COUNT + "개여야 합니다."),
+	OUT_OF_RANGE_WINNING_NUMBER("로또 번호는 " + MIN_LOTTO_NUMBER + "부터" + MAX_LOTTO_NUMBER + "까지의 범위여야 합니다."),
+	DUPLICATE_WINNING_NUMBER("당첨 번호는 중복될 수 없습니다.");
 
 	private final String message;
 
