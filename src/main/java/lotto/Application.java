70d25269c87df6f21737492ea7ff54bdf9734385
minjun <ijmtylhtf@naver.com>
2024-11-04 15:59:45 +0900

    feat : application 로직 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import java.util.List;
+
+import lotto.answer.Answer;
+import lotto.answer.LottoRank;
+import lotto.io.Input;
+import lotto.provider.LottoProvider;
+import lotto.user.User;
+import lotto.validator.Validator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = Input.readPurchaseAmount();
+
+        User user = new User();
+        LottoProvider lottoProvider = new LottoProvider(purchaseAmount, user);
+        lottoProvider.pickLottoNumbers();
+        lottoProvider.printPickedLottoResults();
+
+        List<Integer> answerLottos = Input.readAnswerLotto();
+        int bonusLotto = Input.readBonusLotto();
+        Answer answer = new Answer(answerLottos, bonusLotto);
+
+        List<LottoRank> lottoRanks = lottoProvider.calculateRank(answer);
+        for (LottoRank lottoRank : lottoRanks) {
+            user.updateRank(lottoRank);
+        }
+        user.calculateRateOfReturns();
+        user.printResult();
     }
 }
