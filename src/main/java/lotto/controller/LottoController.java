0513e4e646566fc0e06ba5b3abfd48da23e6d8a9
정재웅 <jweidon0821@gmail.com>
2024-11-04 20:05:44 +0900

    feat(#2 로또 발행): 구입 금액에 따라 수량 계산 및 정렬된 6개의 랜덤 번호 발행 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,13 +1,29 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Money;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+    public static List<Lotto> myLottos = new ArrayList<>();
 
     public void play() {
         OutputView.printAskMoneyMessage();
         Money money = new Money(InputView.inputMoney());
+        buyLottos(money.trial);
+    }
+
+    public void buyLottos(int trial) {
+        OutputView.printNoticeTrialMessage(trial);
+        do {
+            List<Integer> numbers = LottoNumber.pickRandomNumbers(6);
+            myLottos.add(new Lotto(numbers));
+        } while (myLottos.size() < trial);
+        OutputView.printBoughtLottos(myLottos);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,28 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.dto.NumbersParam;
+
+import java.util.List;
+
 public class OutputView {
     public static final String askMoneyMessage = "구입금액을 입력해 주세요.";
+    public static final String noticeTrialMessage = "개를 구매했습니다.";
 
     public static void printAskMoneyMessage() {
         System.out.println(askMoneyMessage);
+    }
+
+    public static void printNoticeTrialMessage(int trial) {
+        System.out.println("\n" + trial + noticeTrialMessage);
+    }
+
+    public static void printBoughtLottos(List<Lotto> lottos) {
+        List<NumbersParam> numbersParams = lottos.stream()
+                .map(lotto -> NumbersParam.from(lotto.getNumbers()))
+                .toList();
+        for (NumbersParam param : numbersParams) {
+            System.out.println("[" + String.join(", ", param.numbers()) + "]");
+        }
     }
 }
