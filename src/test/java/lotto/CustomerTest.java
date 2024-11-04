3c25f385de23c1805d4fa29eca4c23f8f6099988
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 21:54:45 +0900

    refactor(CustomerTest): CustomerTest를 변경한다
    함수가 달라지면서 클래스의 기능을 검증하는 테스트 함수도 변경시킨다.

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -2,13 +2,19 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
 
     @Test
     void 소비자테스트() {
-        assertThatThrownBy(() -> new Customer("100100"))
-                .isInstanceOf(IllegalArgumentException.class);
+        int result = 10;
+        Customer customer= new Customer("10000");
+        assertEquals(result,customer.lottoCount);
     }
 }
