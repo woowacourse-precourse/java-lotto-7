34a356fcc81b214ab40cfe5f20e47f68f333eeba
yunho_yun <yunho_yun@naver.com>
2024-11-04 22:35:02 +0900

    fix: 번호 오름차순 기능 수정

--- src/main/java/lotto/util/NumberSorter.java
+++ src/main/java/lotto/util/NumberSorter.java
@@ -1,11 +1,13 @@
 package lotto.util;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class NumberSorter {
     public static List<Integer> sort(List<Integer> numbers) {
-        Collections.sort(numbers);
-        return numbers;
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 }
