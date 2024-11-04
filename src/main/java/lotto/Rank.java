5eaddb1230b6894b5e2079b85bf7c8980d009b03
Hojoomoney <kimhojoo51@gmail.com>
2024-11-04 22:08:44 +0900

    feat : 출력 포맷 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,19 +1,21 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(4, 1500000),
-    FOURTH(4,50000),
-    FIFTH(3,5000),
-    MISS(0,0);
+    FIFTH(3,5000, "3개 일치 (%,d원) - %d개"),
+    FOURTH(4,50000, "4개 일치 (%,d원) - %d개"),
+    THIRD(5, 1500000, "5개 일치 (%,d원) - %d개"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (%,d원) - %d개"),
+    FIRST(6, 2000000000, "6개 일치 (%,d원) - %d개");
 
     private final int matchCount;
     private final int prize;
+    private final String format;
 
-    Rank(int matchCount, int prize) {
+
+    Rank(int matchCount, int prize, String format) {
         this.matchCount = matchCount;
         this.prize = prize;
+        this.format = format;
     }
 
     public static Rank valueOf(int matchCount, boolean matchBonus){
@@ -22,10 +24,22 @@ public enum Rank {
         if (matchCount == 5) return THIRD;
         if (matchCount == 4) return FOURTH;
         if (matchCount == 3) return FIFTH;
-        return MISS;
+        return null;
     }
 
     public int getPrize() {
         return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String formatOutput(long count) {
+        return String.format(format, prize, count);
     }
 }
