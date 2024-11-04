415c9c64291433cd3e1c1ff4bf017191db954131
김주언 <wndjs803@naver.com>
2024-11-04 13:41:52 +0900

    fix: 로또 구매 개수 출력 메서드 명 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import lotto.model.Amount;
 
 public class OutputView {
-    public void printAmount(Amount amount){
+    public void printCount(Amount amount){
         System.out.println(amount.getCount() + "개를 구입하셨습니다.");
     }
 }
