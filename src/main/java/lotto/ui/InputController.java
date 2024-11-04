19ef568890d93199d3b5fb21fa16dce18801508d
huhdy32 <mong3268@gmail.com>
2024-11-04 18:55:54 +0900

    fix(InputController): 잘못된 로또 당첨 번호 입력 검증 로직 추가

--- src/main/java/lotto/ui/InputController.java
+++ src/main/java/lotto/ui/InputController.java
@@ -12,7 +12,13 @@ public class InputController {
 
     private static final String BLANK_INPUT_ERROR_MESSAGE = "공백은 허용되지 않습니다.";
     private static final String ITS_NOT_NUMBER_ERROR_MESSAGE = "숫자가 아닙니다.";
+    private static final String REPEATED_SEPARATOR_ERROR_MESSAGE = "구분자가 반복되고 있습니다.";
+    private static final String START_WITH_SEPARATOR_ERROR_MESSAGE = "구분자로 시작되고 있습니다.";
+    private static final String END_WITH_SEPARATOR_ERROR_MESSAGE = "구분자로 종료되고 있습니다.";
     private static final String SEPARATOR = ",";
+    private static final String SPACE = " ";
+    private static final String BLANK = "";
+
     private final InputUi inputUi;
 
     InputController(final InputUi inputUi) {
@@ -21,17 +27,15 @@ public class InputController {
 
     public LottoPayment getPayment() {
         final String input = this.readTrimmedInput();
-        validateNumber(input);
+        validateSingleNumber(input);
         return new LottoPayment(input);
     }
 
     public Lotto getLotto() {
         final String input = this.readTrimmedInput();
-        final List<String> lottoNumbers = Arrays.stream(input.split(SEPARATOR))
-                .map(number -> number.trim())
-                .collect(Collectors.toList());
+        final List<String> lottoNumbers = split(input);
         for (final String number : lottoNumbers) {
-            validateNumber(number);
+            validateSingleNumber(number);
         }
         return new Lotto(lottoNumbers.stream()
                 .map(LottoNumber::new).collect(Collectors.toList()));
@@ -39,15 +43,47 @@ public class InputController {
 
     public LottoNumber getBonusNumber() {
         final String input = this.readTrimmedInput();
-        this.validateNumber(input);
+        this.validateSingleNumber(input);
         return new LottoNumber(input);
     }
 
-    private String readTrimmedInput() {
-        return inputUi.readLine().trim();
+    private List<String> split(final String inputLottoNumbers) {
+        validateNumbers(inputLottoNumbers);
+        return Arrays.stream(inputLottoNumbers.split(SEPARATOR))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
-    private void validateNumber(final String numberInput) {
+    private void validateNumbers(final String spaceRemovedInputNumbers) {
+        validateStartsWithSeparator(spaceRemovedInputNumbers);
+        validateEndWithSeparator(spaceRemovedInputNumbers);
+        validateRepeatedSeparator(spaceRemovedInputNumbers);
+    }
+
+    private void validateRepeatedSeparator(final String inputNumbers) {
+        if (inputNumbers.contains(SEPARATOR + SEPARATOR)) {
+            throw new LottoArgumentException(REPEATED_SEPARATOR_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateStartsWithSeparator(final String inputNumbers) {
+        if (inputNumbers.startsWith(SEPARATOR)) {
+            throw new LottoArgumentException(START_WITH_SEPARATOR_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateEndWithSeparator(final String inputNumbers) {
+        if (inputNumbers.endsWith(SEPARATOR)) {
+            throw new LottoArgumentException(END_WITH_SEPARATOR_ERROR_MESSAGE);
+        }
+    }
+
+    private String readTrimmedInput() {
+        final String trimmedInput = inputUi.readLine().trim();
+        return trimmedInput.replaceAll(SPACE, BLANK);
+    }
+
+    private void validateSingleNumber(final String numberInput) {
         isBlank(numberInput);
         isPositiveNumber(numberInput);
     }
