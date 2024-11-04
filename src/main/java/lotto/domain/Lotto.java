dee8c16b4a60e3b7576792aa5660bcd2d643b6fe
yunho_yun <yunho_yun@naver.com>
2024-11-04 14:45:45 +0900

    feat: 로또 숫자 범위 검증 로직 추가
    Lotto.java

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,7 +14,13 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (!numbers.stream().allMatch(this::validateNumberRange)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
+    private boolean validateNumberRange(int number) {
+        return 1 <= number && number <= 45;
+    }
     // TODO: 추가 기능 구현
 }
