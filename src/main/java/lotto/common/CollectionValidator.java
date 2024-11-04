442672211711ce41af5cb0990e1939a7eeb7cdeb
YeaChan05 <qkenrdl05@gmail.com>
2024-11-04 14:38:32 +0900

    feat(CollectionValidator.java): 범위 검증기 구현
    컬렉션이나 단일 정수의 범위 검증 메서드 구현

--- src/main/java/lotto/common/CollectionValidator.java
+++ src/main/java/lotto/common/CollectionValidator.java
@@ -3,10 +3,12 @@ package lotto.common;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class CollectionValidator {
     private static final String DUPLICATE_NUMBERS_EXIST = "중복된 숫자가 존재합니다.";
     private static final String SIZE_INVALID = "크기가 적절하지 않습니다. 현재 크기: %d";
+    private static final String NUMBER_OUT_OF_RANGE = "제한 범위 밖의 수 입니다. 제한범위:%d~%d";
 
     public static void validateDuplicate(List<Integer> numbers) {
         Set<Integer> nonDuplicatedNumbers = new HashSet<>(numbers);
@@ -15,9 +17,20 @@ public class CollectionValidator {
         }
     }
 
-    public static void validateSize(List<Integer> winningNumbers, int size) {
-        if (winningNumbers.size() != size) {
-            throw new IllegalArgumentException(String.format(SIZE_INVALID, winningNumbers.size()));
+    public static void validateSize(List<Integer> elements, int size) {
+        if (elements.size() != size) {
+            throw new IllegalArgumentException(String.format(SIZE_INVALID, elements.size()));
         }
+    }
+
+    public static void validateRange(int min, int max, Integer element) {
+        boolean anyMatch = element >= min && element <= max;
+        if (!anyMatch) {
+            throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE.formatted(min, max));
+        }
+    }
+
+    public static void validateRange(int min, int max, List<Integer> elements) {
+        elements.forEach(element -> validateRange(min, max,element));
     }
 }
