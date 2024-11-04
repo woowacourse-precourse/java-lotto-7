16c7541f364ed312ef5428a45d4de1c4b0988bff
YunCheol1 <naro2727@naver.com>
2024-11-04 20:35:49 +0900

    refactor(Inputview): 유효성 검사 메서드 Validator클래스로 이동 및 로직 수정

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -1,29 +1,42 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.util.Config;
+import java.util.List;
+import lotto.util.Validator;
 
 public class Inputview {
+
+    // 구입 금액 입력
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        try {
-            int money = Integer.parseInt(Console.readLine());
-            if (money % 1000 != 0) {
-                throw new IllegalArgumentException(Config.ERROR_INVALID_TICKET_COUNT);
+        while (true) {
+            try {
+                int money = Validator.validateAndParseNumber(Console.readLine()); // 숫자 형식 검사
+                Validator.validatePurchaseAmount(money); // 1,000원 단위 검사
+                return money;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-            return money;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Config.ERROR_INVALID_MONEY_FORMAT);
         }
     }
 
+    // 당첨 번호 입력
     public static String inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
+    // 보너스 번호 입력
     public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                int bonusNumber = Validator.validateAndParseNumber(Console.readLine()); // 숫자 형식 검사 및 파싱
+                Validator.validateLottoNumberRange(List.of(bonusNumber)); // 보너스 번호 범위 검사
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
