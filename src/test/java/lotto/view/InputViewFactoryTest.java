a76671b9a683da196047f5a627f309e89d491411
kimsehw <seunghwan9703@gmail.com>
2024-11-04 16:52:08 +0900

    refactor(InputView): View 구현 삭제 및 InputView 팩토리 패턴 적용
    유효하지 않은 경우 해당 부분 부터 다시 입력 받아야 함에 따라 getInput()의 사용을 위해 View 구현을 삭제하고 독립적으로 작동 시킵니다.
    팩토리 패턴을 적용하여 InputView 들을 필요한 타입에 맞게 생성합니다.

---
+++ src/test/java/lotto/view/InputViewFactoryTest.java
@@ -0,0 +1,21 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class InputViewFactoryTest {
+
+    @DisplayName("타입에 따른 인풋 뷰 생성 테스트")
+    @ParameterizedTest
+    @CsvSource({InputViewFactory.AMOUNT + "," + InputViewFactory.AMOUNT_INPUT_REQUEST_MESSAGE,
+            InputViewFactory.WINNING_NUMBER + "," + InputViewFactory.WINNING_NUMBER_INPUT_REQUEST_MESSAGE,
+            InputViewFactory.BONUS_NUMBER + "," + InputViewFactory.BONUS_NUMBER_INPUT_REQUEST_MESSAGE})
+    void variousTypeOfInputViewFactoryCreateTest(String input, String expectedMessage) {
+        assertThat(InputViewFactory.createInputView(input))
+                .extracting("requestMessage")
+                .isEqualTo(expectedMessage);
+    }
+}
