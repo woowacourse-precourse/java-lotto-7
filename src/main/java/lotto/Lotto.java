a5417d09f81c6693d24730231ebb69a4b891e5ce
JuSeong1130 <jsmoon1130@naver.com>
2024-11-04 16:02:35 +0900

    refactor(Lotto) :  getNumbers 불변으로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -27,6 +28,6 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 }
