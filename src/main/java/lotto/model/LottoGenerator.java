d02fb0bd7e7a19f1a209a2c94c1cc02bea03550d
Hyejeong <thswjd0609@gmail.com>
2024-11-04 19:08:56 +0900

    feat: 로또 번호 랜덤 생성 기능

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,15 +1,31 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
+    private static final int STARTINCLUSIVE = 1;
+    private static final int ENDINCLUSIVE = 45;
+    private static final int COUNT = 6;
     private int lottoCnt;
     private List<Lotto> lottos;
 
     public LottoGenerator(int lottoCnt) {
         this.lottoCnt = lottoCnt;
         this.lottos = new ArrayList<>();
+    }
+
+    public List<Lotto> generateLottos() {
+        for (int i = 0; i < lottoCnt; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+        return lottos;
+    }
+
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(STARTINCLUSIVE, ENDINCLUSIVE, COUNT);
     }
 
     public List<Lotto> getLottos() {
