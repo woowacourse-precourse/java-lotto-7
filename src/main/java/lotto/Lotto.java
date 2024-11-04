8497ee7274662521fd40c82466b95a12d1079a53
seha9631 <gktkddms9631@gmail.com>
2024-11-04 16:18:30 +0900

    chore(Constants): 상수 값을 저장하는 클래스 추가
    - 코드에서 사용되는 공통 상수를 Constants 클래스로 분리
    - 기존 코드에 상수 적용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,16 +4,18 @@ import java.util.List;
 import java.util.HashSet;
 
 public class Lotto {
-    private static final int LOTTO_NUMBER_MIN = 1;
-    private static final int LOTTO_NUMBER_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void validateLottoNumbers(List<Integer> numbers) {
         validateCountOfNumbers(numbers);
         validateDuplicateNumber(numbers);
         validateRangeOfNumbers(numbers);
@@ -21,25 +23,21 @@ public class Lotto {
 
     private static void validateCountOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호의 개수는 6개여야 합니다.");
+            throw new IllegalArgumentException("\n[ERROR] 로또 번호의 개수는 6개여야 합니다.");
         }
     }
 
     private void validateRangeOfNumbers(List<Integer> numbers) {
-        boolean isOutOfRange = numbers.stream().anyMatch(num -> num < LOTTO_NUMBER_MIN || num > LOTTO_NUMBER_MAX);
+        boolean isOutOfRange = numbers.stream().anyMatch(num -> num < Constants.LOTTO_NUMBER_MIN || num > Constants.LOTTO_NUMBER_MAX);
         if (isOutOfRange) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("\n[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
     private void validateDuplicateNumber(List<Integer> numbers) {
-        boolean isDuplicated = new HashSet<>(numbers).size() != 6;
+        boolean isDuplicated = new HashSet<>(numbers).size() != Constants.LOTTO_NUMBERS_LENGTH;
         if(isDuplicated) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야만 합니다.");
+            throw new IllegalArgumentException("\n[ERROR] 로또 번호는 중복이 없어야만 합니다.");
         }
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 }
--- src/main/java/lotto/LottoPurchaser.java
+++ src/main/java/lotto/LottoPurchaser.java
@@ -6,11 +6,10 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class LottoPurchaser {
-    private static final int LOTTO_PRICE = 1000;
     private final int lottoPurchaseCount;
 
     public LottoPurchaser(String purchaseAmountInput) {
-        this.lottoPurchaseCount = Integer.parseInt(purchaseAmountInput) / LOTTO_PRICE;
+        this.lottoPurchaseCount = Integer.parseInt(purchaseAmountInput) / Constants.LOTTO_PRICE;
     }
 
     public List<Lotto> purchaseLotto() {
