00b3a810146d8bc1077d61793b61fbcc5ce61c77
choign3123 <choign3123@naver.com>
2024-11-04 09:37:23 +0900

    update(Application): 광역 try-catch문 삭제
    변경 이유:
    예제 테스트가 통과하지 못하는 이유가 모든 예외를 잡지 않아서가 아니었음.
    그러니 필요 없는 try-catch문이라 판단하여 삭제.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,11 +2,7 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            LottoGame lottoGame = new LottoGame();
-            lottoGame.run();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.run();
     }
 }
