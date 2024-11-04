ca14bce2d79a4ce1883fe816b318f3d5a6be98ca
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 09:37:17 +0900

    refactor(BonusNumber): 당첨 번호와 중복된 보너스 번호 입력 예외 처리 로직 분리
    - BonusNumber 생성자에서 예외를 던지는 부분을 validateBonusNumber 메서드로 분리
    - validateBonusNumber 메서드에서 사용되는 조건을 isDuplicateBonusInWinning 메서드로 분리

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -4,9 +4,17 @@ public class BonusNumber {
     private final LottoNumber bonusNumber;
 
     public BonusNumber(LottoNumber bonusNumber, WinningNumber winningNumber) {
-        if(winningNumber.contains(bonusNumber)){
+        validateBonusNumber(bonusNumber, winningNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(LottoNumber bonusNumber, WinningNumber winningNumber) {
+        if(isDuplicateBonusInWinning(bonusNumber, winningNumber)){
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호에 중복되지 않는 숫자여야 합니다.");
         }
-        this.bonusNumber = bonusNumber;
+    }
+
+    private Boolean isDuplicateBonusInWinning(LottoNumber bonusNumber, WinningNumber winningNumber){
+        return winningNumber.contains(bonusNumber);
     }
 }
