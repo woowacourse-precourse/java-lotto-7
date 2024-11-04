2e26cc92f1f1ceb9e1a3b64560e2935b56bd68ef
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:01:46 +0900

    Feat: WinningNumberValidator 에 당첨 번호가 숫자와 콤마로 이뤄져있지 않은지 검증 구현

---
+++ src/main/java/lotto/view/validation/WinningNumbersValidator.java
@@ -0,0 +1,27 @@
+package lotto.view.validation;
+
+import static lotto.service.exception.LottoExceptionMessage.INVALID_WINNING_NUMBERS;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.service.exception.LottoException;
+
+public class WinningNumbersValidator {
+
+    private static final String WINNING_NUMBERS_REGEX = "^(?:(?:[1-9]|[1-3][0-9]|4[0-5])(,(?:[1-9]|[1-3][0-9]|4[0-5])){5})?$";
+    private static final String SEPARATOR = ",";
+
+    public static List<Integer> validate(String winningNumbers) {
+        validateWinningNumbersAndSeparator(winningNumbers);
+        return Arrays.stream(winningNumbers.split(SEPARATOR))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateWinningNumbersAndSeparator(String winningNumbers) {
+        if (!winningNumbers.matches(WINNING_NUMBERS_REGEX)) {
+            throw new LottoException(INVALID_WINNING_NUMBERS);
+        }
+    }
+}
