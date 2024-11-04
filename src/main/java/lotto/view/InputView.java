6d54262d5c0a80403a9b71a826f1af8fab96e54c
SeongJunChun <csj991210@naver.com>
2024-11-04 16:31:05 +0900

    feat: 입력을 관리하는 view 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+import static lotto.message.SystemMessage.REQUEST_BONUS_NUMBER;
+import static lotto.message.SystemMessage.REQUEST_PURCHASE_AMOUNT;
+import static lotto.message.SystemMessage.REQUEST_WINNING_NUMBERS;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readPurchaseAmount() {
+        return readLine(REQUEST_PURCHASE_AMOUNT.getMessage());
+    }
+
+    public String readWinningNumber() {
+        return readLine(REQUEST_WINNING_NUMBERS.getMessage());
+    }
+
+    public String readBonusNumber() {
+        return readLine(REQUEST_BONUS_NUMBER.getMessage());
+    }
+
+    private String readLine(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
+}
