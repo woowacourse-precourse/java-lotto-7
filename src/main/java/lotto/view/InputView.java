60615d71104a80d52ba6f67b40913d36922bd7bf
LeeDongHoon <likelion.leedh@gmail.com>
2024-11-04 16:35:14 +0900

    [REFACTOR] 입력 return 값 String 으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,6 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.dto.BonusNumberDto;
-import lotto.dto.PurchaseAmountDto;
-import lotto.dto.WinningLotteryDto;
 
 public class InputView {
 
@@ -11,24 +8,18 @@ public class InputView {
     private static final String INPUT_WINNING_LOTTERY_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static PurchaseAmountDto inputPurchaseAmount(){
+    public static String inputPurchaseAmount(){
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
-        return PurchaseAmountDto.from(
-                Console.readLine()
-        );
+        return Console.readLine();
     }
 
-    public static WinningLotteryDto inputWinningLottery(){
+    public static String inputWinningLottery(){
         System.out.println(INPUT_WINNING_LOTTERY_MESSAGE);
-        return WinningLotteryDto.from(
-                Console.readLine()
-        );
+        return Console.readLine();
     }
 
-    public static BonusNumberDto inputBonusNumber(){
+    public static String inputBonusNumber(){
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
-        return BonusNumberDto.from(
-                Console.readLine()
-        );
+        return Console.readLine();
     }
 }
