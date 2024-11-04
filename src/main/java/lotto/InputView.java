43de9c5ecd6af0aa051fc33468e07bf956df2af9
da1hwang <GDMxKyio@gmail.com>
2024-11-04 18:46:44 +0900

    feat: add readWinningNumbers method and fix typo
    - add readWinningNumbers to InputView for user input of winning numbers
    - correct typo in PURCHASE_AMOUNT_MESSAGE ("구입급액" -> "구입금액")

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import java.util.List;
+
 public class InputView {
-    private static final String PURCHASE_AMOUNT_MESSAGE = "구입급액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final InputHandler inputHandler;
 
     public InputView() {
@@ -11,5 +14,10 @@ public class InputView {
     public int readPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return inputHandler.getPurchaseAmount();
+    }
+
+    public List<Integer> readWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        return inputHandler.getWinningNumbers();
     }
 }
