30d75104eb82924e44428855d0234bc4b06c7810
양효빈 <hyobin3026@naver.com>
2024-11-04 18:41:03 +0900

    refactor(NumberConverter): Add constant for regular expression

--- src/main/java/lotto/util/NumberConverter.java
+++ src/main/java/lotto/util/NumberConverter.java
@@ -3,6 +3,7 @@ package lotto.util;
 import lotto.exception.GeneralExceptionMessages;
 
 public class NumberConverter{
+    private static final String REGEXP_PATTERN_NUMBER = "^[\\d]*$";
 
     public int convertNumber(String input) {
         validateInput(input);
@@ -11,7 +12,7 @@ public class NumberConverter{
 
     private void validateInput(String input) {
         String trimmedInput = input.trim();
-        if (trimmedInput.isEmpty() || !trimmedInput.matches(RegexConstants.NUMBER_PATTERN)) {
+        if (trimmedInput.isEmpty() || !trimmedInput.matches(REGEXP_PATTERN_NUMBER)) {
             throw new IllegalArgumentException(GeneralExceptionMessages.INVALID_NUMBER);
         }
     }
