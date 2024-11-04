df3029432d62ee6d283ac3d35ed777cf441f2507
Leegeonwoo7 <workgeon07@naver.com>
2024-11-04 18:00:48 +0900

    feat(WinningNumber): WinningNumber 구현
    당첨번호를 관리하는 객체

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,2 +1,21 @@
-package lotto;public class WinningNumber {
+package lotto;
+
+import java.util.List;
+
+public class WinningNumber {
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public WinningNumber(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
