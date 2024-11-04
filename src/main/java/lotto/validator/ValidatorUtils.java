2a799b6d54b91fe29e371e062feab108da962ea9
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 12:02:04 +0900

    chore(ValidatorUtils): 구입 금액 에러 메시지 변수명 변경

--- src/main/java/lotto/validator/ValidatorUtils.java
+++ src/main/java/lotto/validator/ValidatorUtils.java
@@ -2,7 +2,7 @@ package lotto.validator;
 
 public class ValidatorUtils {
 
-    public static final String PRICE_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위의 양수를 입력해 주세요.";
+    public static final String PURCHASE_AMOUNT_ERROR_MESSAGE = "[ERROR] 구입 금액은 1,000원 단위의 양수를 입력해 주세요.";
     public static final String WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨 번호는 중복되지 않도록 1 ~ 45 사이의 6개의 정수를 입력해주세요";
     public static final String BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않도록 1 ~ 45 사이의 정수를 입력해주세요.";
 
