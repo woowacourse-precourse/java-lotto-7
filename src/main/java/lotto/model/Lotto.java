847e5b9839fb8a5a47864b50112e9c0efcee9518
Minseok <saltishDev@gmail.com>
2024-11-04 15:16:28 +0900

    feat: 로또 번호가 6개가 아닌 경우 예외 처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,25 @@
 package lotto.model;
 
+import lotto.exception.ErrorMessage;
+
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
         Collections.sort(numbers);
+        this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_COUNT.getErrorMessage());
         }
     }
 
