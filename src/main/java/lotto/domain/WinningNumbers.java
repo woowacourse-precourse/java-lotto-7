152e0c74c978fe83935268713a4185e185d11756
김현재 <nowalex322@naver.com>
2024-11-04 10:15:10 +0900

    feat: 당첨 번호 입력 및 검증 기능 구현
    - - WinningNumbers 클래스 추가 (당첨 번호와 보너스 번호 관리)

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import lotto.Lotto;
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumbers(Lotto winningLotto, int bonusNumber) {
+        validateBonusNumber(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningLotto, int bonusNumber) {
+        LottoNumber.of(bonusNumber); // 1-45 범위 검증
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public int matchCount(Lotto lotto) {
+        return (int) lotto.getNumbers().stream()
+                .filter(number -> winningLotto.getNumbers().contains(number))
+                .count();
+    }
+
+    public boolean hasBonusNumber(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+}
