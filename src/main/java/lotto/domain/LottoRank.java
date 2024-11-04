b62129479861d1519a1cbdb1131888a4fbc3a481
choiminu <alsdn4590@gmail.com>
2024-11-04 12:30:16 +0900

    refactor(model): 로또 등수의 상수 순서 변경 및 matchCount getter 추가하였음
    - LottoRank 열거형의 상수 순서를 가독성을 위해 역순으로 변경하였음

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,12 +1,12 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)"),
-    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
-    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    OTHER(0, false, 0, "꽝"),
     FIFTH(3, false, 5_000, "3개 일치 (5,000원)"),
-    OTHER(0, false, 0, "꽝");
+    FOURTH(4, false, 50_000, "4개 일치 (50,000원)"),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원)"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원)");
 
     private final int matchCount;
     private final boolean bonusMatch;
@@ -31,6 +31,10 @@ public enum LottoRank {
 
     private boolean isMatchCountAndBonus(int matchCount, boolean isBonus) {
         return this.matchCount == matchCount && this.bonusMatch == isBonus;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 
     public int getPrizeAmount() {
