5ec3fa064dd81818cfb45bfe4b3314af68ab9884
kimdevspace <ejk5148@gmail.com>
2024-11-04 15:18:02 +0900

    refactor: 테스트 코드용 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -26,8 +26,20 @@ public class LottoController {
         int cnt = lottoService.calculateLottoCount(purchaseAmount);
 
         resultView.responseCntLotto(cnt);
+        //실제 동작하는 코드입니다.
+//        List<List<Integer>> lottoNumbersList = lottoService.lottoNumbers(cnt);
 
-        List<List<Integer>> lottoNumbersList = lottoService.lottoNumbers(cnt);
+        //테스트용 리스트입니다.
+        List<List<Integer>> lottoNumbersList = List.of(
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
         resultView.responseLottoNumberList(lottoNumbersList);
 
         List<Integer> winningNumbers = inputView.requestLottoNumbers();
