f3380eb0cde05f6adba7b967b33e0ccc54abc3ca
LeeJU <thirdspirit@naver.com>
2024-11-04 15:51:06 +0900

    fix(LottoPrizeType): enhance readability
    underscore on Long number

--- src/main/java/lotto/service/LottoPrizeType.java
+++ src/main/java/lotto/service/LottoPrizeType.java
@@ -1,11 +1,11 @@
 package lotto.service;
 
 public enum LottoPrizeType {
-    THREE_MATCH(5000L),
-    FOUR_MATCH(50000L),
-    FIVE_MATCH(1500000L),
-    FIVE_MATCH_WITH_BONUS(30000000L),
-    SIX_MATCH(2000000000L),
+    THREE_MATCH(5_000L),
+    FOUR_MATCH(50_000L),
+    FIVE_MATCH(1_500_000L),
+    FIVE_MATCH_WITH_BONUS(30_000_000L),
+    SIX_MATCH(2_000_000_000L),
     NO_MATCH(0L);
 
     private final Long type;
