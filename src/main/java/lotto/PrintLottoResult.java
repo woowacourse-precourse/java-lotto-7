d8233e8d2715ef202d9ffadf459af124cb70d4ec
Hujin0322 <hongyuj0322@naver.com>
2024-11-04 22:09:32 +0900

    feat(PrintLottoResult): 로또 수익률 출력 기능 구현 - printResults 메서드에 로또 수익률 출력 기능 추가

--- src/main/java/lotto/PrintLottoResult.java
+++ src/main/java/lotto/PrintLottoResult.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import java.util.List;
+
 public class PrintLottoResult {
-    public void printResults(LottoResult lottoResult) {
+    public void printResults(List<List<Integer>> lottos, Lotto winningLotto, int bonusNumber, int totalSpent) {
+        LottoResult lottoResult = new LottoResult();
+        lottoResult.calculateResults(lottos, winningLotto, bonusNumber);
+
         System.out.println("당첨 통계");
         System.out.println("---");
         System.out.printf("3개 일치 (%d원) - %d개%n", LottoRank.FIFTH.getPrizeAmount(), lottoResult.getCount(LottoRank.FIFTH));
@@ -9,5 +14,11 @@ public class PrintLottoResult {
         System.out.printf("5개 일치 (%d원) - %d개%n", LottoRank.THIRD.getPrizeAmount(), lottoResult.getCount(LottoRank.THIRD));
         System.out.printf("5개 일치, 보너스 볼 일치 (%d원) - %d개%n", LottoRank.SECOND.getPrizeAmount(), lottoResult.getCount(LottoRank.SECOND));
         System.out.printf("6개 일치 (%d원) - %d개%n", LottoRank.FIRST.getPrizeAmount(), lottoResult.getCount(LottoRank.FIRST));
+
+        LottoProfit lottoProfit = new LottoProfit(totalSpent, lottoResult);
+        double profitRate = lottoProfit.calculateProfitRate();
+
+        profitRate = Math.round(profitRate * 100.0) / 100.0;
+        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
     }
 }
