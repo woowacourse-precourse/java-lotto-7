40d06db2ad59c160e3a5f4150dd301634a44a7cd
kkmdevel <kkm.devel@gmail.com>
2024-11-04 16:56:50 +0900

    feat(LottoController): 로또 당첨 번호와 보너스 번호 생성 메서드 추가
    - 로또 당첨 번호를 입력받아 Lotto 객체를 생성하는 getWinningLotto 메서드 추가
    - 보너스 번호를 입력받아 LottoDraw 객체를 생성하는 generateLottoDraw 메서드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,14 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoDraw;
 import lotto.domain.LottoPaper;
 import lotto.domain.Payment;
+import lotto.exception.LottoException;
+import lotto.exception.ParserException;
+import lotto.utils.Parser;
+import lotto.view.InputReader;
 import lotto.view.OutputView;
 
 public class LottoController {
@@ -15,6 +22,40 @@ public class LottoController {
         return lottoPaper;
     }
 
+    public LottoDraw generateLottoDraw() {
+        Lotto winningLotto = getWinningLotto();
+        while (true) {
+            try {
+                int bonusNumber = getBonusNumber();
+                return LottoDraw.of(winningLotto, bonusNumber);
+            } catch (LottoException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Lotto getWinningLotto() {
+        while(true) {
+            try {
+                OutputView.printWinningLottoMessage();
+                String winningLottoInput = InputReader.readLine();
+                List<Integer> winningLotto = Parser.splitBySeparator(winningLottoInput);
+                return new Lotto(winningLotto);
+            } catch (LottoException | ParserException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private int getBonusNumber() {
+        while (true) {
+            try {
+                OutputView.printBonusNumberMessage();
+                String bonusNumberInput = InputReader.readLine();
+                return Parser.parseStringToInt(bonusNumberInput);
+            } catch (ParserException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }
-
-
