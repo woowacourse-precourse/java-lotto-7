3f11de50845574ca7c01118cd028382b942e28fb
KIMSEI1124 <workju1124@gmail.com>
2024-11-04 18:53:06 +0900

    test: Lotto 저장소 작성

---
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoRepository {
+	private static final LottoRepository INSTANCE = new LottoRepository();
+	private static final List<Lotto> repository = new ArrayList<>();
+
+	private LottoRepository() {
+	}
+
+	public static LottoRepository getInstance() {
+		return INSTANCE;
+	}
+
+	public Lotto save(Lotto lotto) {
+		repository.add(lotto);
+		return lotto;
+	}
+
+	public List<Lotto> findAll() {
+		return repository;
+	}
+}
