144cdf435a0940fcc0ba4faf607a2d1a2478b74b
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-04 19:35:53 +0900

    docs: 로또그룹서비스 테스트 추가

--- src/test/java/lotto/service/LottoGroupServiceTest.java
+++ src/test/java/lotto/service/LottoGroupServiceTest.java
@@ -2,6 +2,16 @@ package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LottoGroupServiceTest {
+import org.junit.jupiter.api.Test;
 
+class LottoGroupServiceTest {
+    @Test
+    void 맞춘_숫자_계산() {
+
+    }
+
+    @Test
+    void 보너스_숫자가_있는지_확인() {
+
+    }
 }
