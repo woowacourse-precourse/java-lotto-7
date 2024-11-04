f48eff8039e9a40104da09ee1498bafa6cdef71b
wouldu <jhw664089@gmail.com>
2024-11-04 22:57:06 +0900

    refactor: LottoGame 사용하지 않는 로직 제거

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,12 +1,5 @@
 package lotto.controller;
 
-import static lotto.constants.LottoConstants.LOTTO_NUMBER_COUNT;
-import static lotto.constants.LottoConstants.MAX_LOTTO_NUMBER;
-import static lotto.constants.LottoConstants.MIN_LOTTO_NUMBER;
-
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -42,38 +35,5 @@ public class LottoGame {
         double profitRate = statisticsService.calculateProfitRate(totalPrize, purchaseAmount);
 
         ResultView.printResults(results, profitRate);
-    }
-
-    public static List<Lotto> generateLottos(int count) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
-                    LOTTO_NUMBER_COUNT);
-            Lotto lotto = new Lotto(numbers);
-            lottos.add(lotto);
-        }
-        return lottos;
-    }
-
-    public static Map<Rank, Integer> calculateResults(List<Lotto> lottos, List<Integer> winningNumbers,
-            int bonusNumber) {
-        Map<Rank, Integer> resultMap = new HashMap<>();
-        for (Lotto lotto : lottos) {
-            int matchCount = getMatchCount(lotto.getNumbers(), winningNumbers);
-            boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
-            Rank rank = Rank.valueOf(matchCount, bonusMatch);
-            resultMap.put(rank, resultMap.getOrDefault(rank, 0) + 1);
-        }
-        return resultMap;
-    }
-
-    public static int getMatchCount(List<Integer> numbers, List<Integer> winningNumbers) {
-        int count = 0;
-        for (int number : numbers) {
-            if (winningNumbers.contains(number)) {
-                count++;
-            }
-        }
-        return count;
     }
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -20,21 +20,10 @@ public class ResultView {
         }
     }
 
-    public static void printResults(Map<Rank, Integer> results, int purchaseAmount) {
+    public static void printResults(Map<Rank, Integer> results, double profitRate) {
         System.out.println(RESULT_HEADER);
-        int totalPrize = calculateTotalPrize(results);
         printRankResults(results);
-        printProfit(totalPrize, purchaseAmount);
-    }
-
-    private static int calculateTotalPrize(Map<Rank, Integer> results) {
-        int totalPrize = 0;
-        for (Map.Entry<Rank, Integer> entry : results.entrySet()) {
-            Rank rank = entry.getKey();
-            int count = entry.getValue();
-            totalPrize += rank.getPrize() * count;
-        }
-        return totalPrize;
+        System.out.printf((TOTAL_PROFIT) + "%n", profitRate);
     }
 
     private static void printRankResults(Map<Rank, Integer> results) {
@@ -54,11 +43,5 @@ public class ResultView {
             return;
         }
         System.out.printf((MATCH_COUNT_RESULT) + "%n", rank.getMatchCount(), prize, count);
-    }
-
-    private static void printProfit(int totalPrize, int purchaseAmount) {
-        double profit = ((double) totalPrize / purchaseAmount) * 100;
-        profit = Math.round(profit * 10) / 10.0;
-        System.out.printf((TOTAL_PROFIT) + "%n", profit);
     }
 }
