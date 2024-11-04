516d64bc6b8f2fe0e895877e8ca8eb5a9a504b55
문혜교 <3hilney@gmail.com>
2024-11-04 17:17:48 +0900

    test: 당첨 통계 출력 기능 테스트

--- src/test/java/lotto/OutputManagerTest.java
+++ src/test/java/lotto/OutputManagerTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class OutputManagerTest {
@@ -37,6 +38,35 @@ class OutputManagerTest {
         assertThat(actualOutput).contains(
                 "[8, 21, 23, 41, 42, 43]",
                 "[3, 5, 11, 16, 32, 38]"
+        );
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void 당첨_통계_출력_테스트() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        Map<MatchCountMessage, Integer> winningStatistics = Map.of(
+                MatchCountMessage.THREE_MATCH, 1,
+                MatchCountMessage.FOUR_MATCH, 0,
+                MatchCountMessage.FIVE_MATCH, 0,
+                MatchCountMessage.FIVE_MATCH_WITH_BONUS, 0,
+                MatchCountMessage.SIX_MATCH, 0
+        );
+
+        OutputManager.printWinningStatistics(winningStatistics);
+
+        String actualOutput = outputStream.toString();
+        assertThat(actualOutput).contains(
+                "당첨 통계",
+                "---",
+                "3개 일치 (5,000원) - 1개",
+                "4개 일치 (50,000원) - 0개",
+                "5개 일치 (1,500,000원) - 0개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                "6개 일치 (2,000,000,000원) - 0개"
         );
 
         System.setOut(System.out);
