f7453dcae36d8c753208846622d9518b65b53be9
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-04 19:02:23 +0900

    Feat : 컨트롤러 LottoMachine 작성
    IllegalArgumentException 발생 시 예외 발생 후 다시 입력 받음

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -1,2 +1,60 @@
-package lotto.controller;public class LottoMachine {
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.service.ProfitCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class LottoMachine {
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    LottoFactory lottoFactory = new LottoFactory(RandomNumberGenerator::generate);
+
+    public void run() {
+        Lottos lottos = prepareLottos();
+        TargetLotto targetLotto = prepareTargetLotto();
+
+        Map<LottoGrade, Integer> lottoCountMap = lottos.convertGrades(targetLotto);
+        outputView.printResult(lottoCountMap);
+
+        ProfitCalculator profitCalculator = new ProfitCalculator(lottos, targetLotto);
+        double profitRate = profitCalculator.calculateProfit();
+        outputView.printProfit(profitRate);
+    }
+    
+    private Lottos prepareLottos() {
+        Lottos lottos = repeatOnIllegalArgument(() -> {
+            Long payment = inputView.readPayment();
+            return lottoFactory.generateLottos(payment);
+        });
+        outputView.printLottos(lottos);
+
+        return lottos;
+    }
+
+    private <T> T repeatOnIllegalArgument (Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private TargetLotto prepareTargetLotto() {
+        TargetLotto targetLotto = repeatOnIllegalArgument(() -> {
+            List<Integer> targetNumbers = inputView.readTargetNumber();
+            Lotto target = new Lotto(targetNumbers);
+            int bonusNumber = inputView.readBonusNumber();
+            return new TargetLotto(target, bonusNumber);
+        });
+
+        return targetLotto;
+    }
 }
