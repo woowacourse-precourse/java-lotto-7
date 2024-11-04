c4ebc00328a23a8ddbb102f967ed1c556d03f0f1
yunhacandy <yunhacandy@gmail.com>
2024-11-04 19:03:25 +0900

    refactor (StatisticsOutputMessage): 통계 출력에 줄바꿈 추가

--- src/main/java/lotto/constant/StatisticsOutputMessage.java
+++ src/main/java/lotto/constant/StatisticsOutputMessage.java
@@ -3,8 +3,8 @@ package lotto.constant;
 public enum StatisticsOutputMessage {
     START_COMMENT("\n당첨 통계"),
     START_LINE("---"),
-    CONTENT_OF_LOTTO_RESULT("%d개 일치 (%s원) - %d개"),
-    CONTENT_OF_RESULT_FOR_BONUS_NUMBER("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    CONTENT_OF_LOTTO_RESULT("%d개 일치 (%s원) - %d개\n"),
+    CONTENT_OF_RESULT_FOR_BONUS_NUMBER("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
     PROFIT_RATE("총 수익률은 %.1f%%입니다.");
 
     private final String message;
