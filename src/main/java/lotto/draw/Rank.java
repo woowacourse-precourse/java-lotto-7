269aa35fa2c6c1c04c628442ffd57df0ca21f82c
woong2e <tlswodnd12345@naver.com>
2024-11-04 13:08:02 +0900

    feat: add message element and getter
    Added getter method to format and return message

--- src/main/java/lotto/draw/Rank.java
+++ src/main/java/lotto/draw/Rank.java
@@ -4,26 +4,32 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FIRST(2_000_000_000, 6, false),
-    SECOND(30_000_000, 5, true),
-    THIRD(1_500_000, 5, false),
-    FOURTH(50_000, 4, false),
-    FIFTH(5_000, 3, false),
-    LOSING_TICKET(0, -1, false);
+    LOSING_TICKET(0, -1, false, ""),
+    FIFTH(5_000, 3, false, "3개 일치 (5,000원) - %d개"),
+    FOURTH(50_000, 4, false, "4개 일치 (50,000원) - %d개"),
+    THIRD(1_500_000, 5, false, "5개 일치 (1,500,000원) - %d개"),
+    SECOND(30_000_000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST(2_000_000_000, 6, false, "6개 일치 (2,000,000,000원) - %d개");
 
     private final int prizeMoney;
     private final int matchingCount;
     private final boolean bonus;
+    private final String message;
 
 
-    Rank(int prizeMoney, int matchingCount, boolean bonus) {
+    Rank(int prizeMoney, int matchingCount, boolean bonus, String message) {
         this.prizeMoney = prizeMoney;
         this.matchingCount = matchingCount;
         this.bonus = bonus;
+        this.message = message;
     }
 
     public int getProfit(int count) {
         return this.prizeMoney * count;
+    }
+
+    public String getMessage(int count) {
+        return String.format(this.message, count);
     }
 
     public static Rank getWinningRank(int matchingCount, boolean bonus) {
