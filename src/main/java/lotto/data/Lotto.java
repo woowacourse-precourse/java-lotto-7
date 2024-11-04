e690b02e1111a0cb4122e6df106cfb6613f42775
JeongSH1 <jyt001532@gmail.com>
2024-11-04 11:48:41 +0900

    fix(Lotto): 로또 번호는 오름차순으로 정렬한다
    수정된 사항:
    - Lotto 객체에서 생성자에서 로또 번호를 오름차순으로 정렬

--- src/main/java/lotto/data/Lotto.java
+++ src/main/java/lotto/data/Lotto.java
@@ -5,6 +5,7 @@ import static lotto.config.constant.ExceptionMessageConstant.INVALID_LOTTO_NUMBE
 import static lotto.config.constant.ExceptionMessageConstant.INVALID_LOTTO_REQUIRED_COUNT;
 import static lotto.config.constant.LottoNumberConstant.*;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +15,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
