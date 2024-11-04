61340fbbb708726b8392afc224f3b8970dcf0efc
zerocat36 <Seo.james2020@gmail.com>
2024-11-04 23:59:18 +0900

    feat(Lotto.java): add printResult function

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,23 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        isAllowedCount(numbers);
-        isDuplicate(numbers);
-        isAllowedRange(numbers);
-        this.numbers = numbers;
+        try {
+            int bonus_number;
+
+            isAllowedCount(numbers);
+            isDuplicate(numbers);
+            isAllowedRange(numbers);
+            this.numbers = numbers;
+
+            List<List<Integer>> lotto_numbers = buyLotto();
+
+            numbers = getWinningNumbers();
+            bonus_number = getBonusNumber();
+            printResult(lotto_numbers, numbers, bonus_number);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 
     private void isAllowedCount(List<Integer> numbers) {
@@ -38,6 +51,7 @@ public class Lotto {
     private static List<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
+        System.out.println();
 
         try {
             return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
@@ -62,6 +76,7 @@ public class Lotto {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             throw new IllegalArgumentException("[ERROR] 잘못된 형식의 구입 금액이 입력되었습니다.");
         }
     }
@@ -71,11 +86,13 @@ public class Lotto {
         int price = 1000;
 
         int count = getPurchaseAmount() / price;
+        System.out.println("\n" + count + "개를 구매했습니다.");
         for (int i = 0; i < count; i++) {
             List<Integer> buffer = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             numbers.add(buffer);
             System.out.println(buffer);
         }
+        System.out.println();
         return numbers;
     }
 
@@ -83,5 +100,39 @@ public class Lotto {
         return numbers.stream().filter(winning_numbers::contains).toList().size();
     }
 
+    private static void printResult(List<List<Integer>> numbers, List<Integer> winning_numbers, int bonus_number) {
+        List<Integer> result = new ArrayList<>();
+        float yield = 0;
 
+        for (int i = 0; i < 5; i++) { //Initialize list space for 1st ~ 5th winner count
+            result.add(0);
+        }
+
+        for (List<Integer> number : numbers) {
+            if (matchNumbers(number, winning_numbers) == 6) {
+                result.set(0, result.getFirst() + 1);
+            }
+            if (matchNumbers(number, winning_numbers) == 5 && number.contains(bonus_number)) {
+                result.set(1, result.get(1) + 1);
+            }
+            if (matchNumbers(number, winning_numbers) == 5 && !number.contains(bonus_number)) {
+                result.set(2, result.get(2) + 1);
+            }
+            if (matchNumbers(number, winning_numbers) == 4) {
+                result.set(3, result.get(3) + 1);
+            }
+            if (matchNumbers(number, winning_numbers) == 3) {
+                result.set(4, result.get(4) + 1);
+            }
+        }
+        yield = (float) (2000000000 * result.get(0) + 30000000 * result.get(1) + 1500000 * result.get(2) + 50000 * result.get(3) + 5000 * result.get(4)) / (numbers.size() * 10);
+
+        System.out.println("\n당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + result.get(4) + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get(3) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get(1) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get(0) + "개");
+        System.out.println("총 수익률은 " + yield + "%입니다.");
+    }
 }
