110fe422029d5307f31c10826e12acbe80652e2e
making-a-scene <4936joo@naver.com>
2024-11-04 22:01:42 +0900

    chore: scanner 메모리 할당 해제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.PurchaseController;
 
 public class Application {
     public static void main(String[] args) {
         final PurchaseController purchaseController = new PurchaseController();
         purchaseController.processLottoGame();
+        Console.close();
     }
 }
