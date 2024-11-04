373441b39b749c116a50a54f26562e6f5480bc40
LSM <leesung2925@gmail.com>
2024-11-04 17:52:48 +0900

    feat: Application 클래스에서 로또 프로그램 초기화 및 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.LottoView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoView view = new LottoView();
+        LottoService service = new LottoService();
+
+        LottoController controller = new LottoController(view, service);
+
+        controller.run();
     }
 }
