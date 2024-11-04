165437f9cf194fa09c0ad31ca0e86657b8c23d92
kyunghoon01 <sports0124@naver.com>
2024-11-04 14:34:24 +0900

    refactor: 상수화

--- src/main/java/util/LottoResultCalculator.java
+++ src/main/java/util/LottoResultCalculator.java
@@ -5,9 +5,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoResultCalculator {
+    private static final int THREE_MATCHES = 3;
+    private static final int SIX_MATCHES = 6;
+    private static final int FIVE_MATCHES_WITH_BONUS = -1;
+
     public static Map<Integer, Long> countMatchingResults(List<Integer> matchResultList) {
         return matchResultList.stream()
-                .filter(count -> count >= 3 && count <= 6 || count == -1)
+                .filter(count -> count >= THREE_MATCHES && count <= SIX_MATCHES || count == FIVE_MATCHES_WITH_BONUS)
                 .collect(Collectors.groupingBy(count -> count, Collectors.counting()));
     }
 }
