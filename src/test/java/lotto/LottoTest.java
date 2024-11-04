7d909e9f166ae1bb1dfbc47d0d99e5902c650d52
kim-se-hee <saree98@naver.com>
2024-11-04 15:03:08 +0900

    test(LottoTest): 로또에 관한 단위 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +23,46 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    public void 로또_번호와_티켓_번호가_6개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(6,5,4,2,1,3));
+        assertThat(matchingCount).isEqualTo(6);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_5개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(1, 2, 3, 4, 5, 9));
+        assertThat(matchingCount).isEqualTo(5);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_4개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(1, 2, 3, 4, 7, 8));
+        assertThat(matchingCount).isEqualTo(4);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_3개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(1, 2, 3, 7, 8, 9));
+        assertThat(matchingCount).isEqualTo(3);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_2개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(1, 2, 7, 8, 9, 10));
+        assertThat(matchingCount).isEqualTo(2);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_1개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(1, 7, 8, 9, 10, 11));
+        assertThat(matchingCount).isEqualTo(1);
+    }
+    @Test
+    public void 로또_번호와_티켓_번호가_0개_일치() throws Exception{
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        int matchingCount = lotto.compareNumberWithTicket(List.of(7, 8, 9, 10, 11, 12));
+        assertThat(matchingCount).isEqualTo(0);
+    }
 }
