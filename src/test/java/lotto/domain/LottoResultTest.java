e8af5b146d8177d5dbea252e1eaa6496f5e8b7b1
JoonKyo <joonkyolee@gmail.com>
2024-11-04 19:05:09 +0900

    test(LottoResultTest): 각 로또에 대한 당첨 결과를 잘 계산하는지 확인하는 테스트 구현

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoResultTest {
     private final Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
@@ -33,5 +37,38 @@ public class LottoResultTest {
         assertThat(rankDistribution.get(Rank.FOURTH_RANK)).isEqualTo(1);
         assertThat(rankDistribution.get(Rank.FIFTH_RANK)).isEqualTo(1);
         assertThat(rankDistribution.get(Rank.NO_RANK)).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateEachLottoData")
+    void 각_로또에_대한_당첨_결과를_잘_계산하는지_확인한다(List<Lotto> userLotto, Rank rank, int expectedRankCount) {
+        Lottos userLottos = Lottos.from(userLotto);
+        LottoResult lottoResult = LottoResult.of(userLottos, winNumber);
+        Map<Rank, Integer> rankDistribution = lottoResult.calculateRankDistribution();
+
+        assertThat(rankDistribution.get(rank)).isEqualTo(expectedRankCount);
+    }
+
+    static Stream<Arguments> generateEachLottoData() {
+        return Stream.of(
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)))
+                        , Rank.FIRST_RANK
+                        , 1),
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)))
+                        , Rank.SECOND_RANK
+                        , 1),
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)))
+                        , Rank.THIRD_RANK
+                        , 1),
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 4, 7, 8)))
+                        , Rank.FOURTH_RANK
+                        , 1),
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 3, 9, 10, 11)))
+                        , Rank.FIFTH_RANK
+                        , 1),
+                Arguments.of(List.of(new Lotto(List.of(1, 2, 10, 11, 12, 13)))
+                        , Rank.NO_RANK
+                        , 1)
+        );
     }
 }
