e4602657f64f70167e924121c07a301732c3537e
su-jin-shin <sujinshin1@gmail.com>
2024-11-04 21:50:36 +0900

    feat: 당첨 번호 입력에 대한 예외 처리 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,13 +2,16 @@ package lotto.model;
 
 import lotto.constant.LottoConstants;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateRangeAndUniqueness(numbers);
         this.numbers = numbers;
     }
 
@@ -20,4 +23,17 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public void validateRangeAndUniqueness(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (number < LottoConstants.MIN_NUMBER || number > LottoConstants.MAX_NUMBER) {
+                throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.",
+                        LottoConstants.MIN_NUMBER, LottoConstants.MAX_NUMBER));
+            }
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(String.format("[ERROR] 중복된 로또 번호(%d)를 입력하였습니다.", number));
+            }
+        }
+    }
 }
