7e40a27a295753f1525b5d552bfbda51057f75af
IM-GYURI <kgulr0517@gmail.com>
2024-11-04 20:09:12 +0900

    refactor : PrizeConstants 추가
    - Prize에서 하드코딩되던 값들을 상수화 함
    - 각각의 당첨 금액과 당첨되기 위해 필요한 일치하는 숫자의 개수를 상수화 함

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -4,15 +4,19 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum Prize {
-    EMPTY(0, 0, (matchCount, containsBonusNumber) -> matchCount < 3),
-    THREE_MATCH(5_000, 3, (matchCount, containBonusNumber) -> matchCount == 3),
-    FOUR_MATCH(50_000, 4, (matchCount, containBonusNumber) -> matchCount == 4),
-    FIVE_MATCH_WITHOUT_BONUS_BALL(1_500_000, 5,
-            (matchCount, containBonusNumber) -> matchCount == 5 && !containBonusNumber),
-    FIVE_MATCH_WITH_BONUS_BALL(30_000_000, 5,
-            (matchCount, containBonusNumber) -> matchCount == 5 && containBonusNumber),
-    SIX_MATCH(2_000_000_000, 6, (matchCount, containBonusNumber) -> matchCount == 6);
-
+    EMPTY(PrizeConstants.EMPTY_PRIZE_MONEY, PrizeConstants.EMPTY_MATCH_COUNT,
+            (matchCount, containsBonusNumber) -> matchCount < PrizeConstants.THREE_MATCH_COUNT),
+    THREE_MATCH(PrizeConstants.THREE_MATCH_PRIZE_MONEY, PrizeConstants.THREE_MATCH_COUNT,
+            (matchCount, containBonusNumber) -> matchCount == PrizeConstants.THREE_MATCH_COUNT),
+    FOUR_MATCH(PrizeConstants.FOUR_MATCH_PRIZE_MONEY, PrizeConstants.FOUR_MATCH_COUNT,
+            (matchCount, containBonusNumber) -> matchCount == PrizeConstants.FOUR_MATCH_COUNT),
+    FIVE_MATCH_WITHOUT_BONUS_BALL(PrizeConstants.FIVE_MATCH_WITHOUT_BONUS_PRIZE_MONEY, PrizeConstants.FIVE_MATCH_COUNT,
+            (matchCount, containBonusNumber) -> matchCount == PrizeConstants.FIVE_MATCH_COUNT && !containBonusNumber),
+    FIVE_MATCH_WITH_BONUS_BALL(PrizeConstants.FIVE_MATCH_WITH_BONUS_PRIZE_MONEY, PrizeConstants.FIVE_MATCH_COUNT,
+            (matchCount, containBonusNumber) -> matchCount == PrizeConstants.FIVE_MATCH_COUNT && containBonusNumber),
+    SIX_MATCH(PrizeConstants.SIX_MATCH_PRIZE_MONEY, PrizeConstants.SIX_MATCH_COUNT,
+            (matchCount, containBonusNumber) -> matchCount == PrizeConstants.SIX_MATCH_COUNT);
+    
     private final int prizeMoney;
     private final int matchCount;
     private final BiPredicate<Integer, Boolean> isWinningCondition;
@@ -36,5 +40,19 @@ public enum Prize {
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    private static class PrizeConstants {
+        public static final int EMPTY_PRIZE_MONEY = 0;
+        public static final int EMPTY_MATCH_COUNT = 0;
+        public static final int THREE_MATCH_COUNT = 3;
+        public static final int THREE_MATCH_PRIZE_MONEY = 5_000;
+        public static final int FOUR_MATCH_COUNT = 4;
+        public static final int FOUR_MATCH_PRIZE_MONEY = 50_000;
+        public static final int FIVE_MATCH_COUNT = 5;
+        public static final int FIVE_MATCH_WITHOUT_BONUS_PRIZE_MONEY = 1_500_000;
+        public static final int FIVE_MATCH_WITH_BONUS_PRIZE_MONEY = 30_000_000;
+        public static final int SIX_MATCH_COUNT = 6;
+        public static final int SIX_MATCH_PRIZE_MONEY = 2_000_000_000;
     }
 }
