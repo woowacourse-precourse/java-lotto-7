3c405154ce53c226a2ac48123b87623a4a622785
DongHoon Lee <dhl1924@naver.com>
2024-11-04 22:33:44 +0900

    feat(Lotto): 로또 번호가 중복일 경우 에러처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import lotto.domain.vo.Number;
 import java.util.List;
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
     private final List<Number> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,8 +17,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw LottoException.from(ErrorMessage.INVALID_LOTTO_NUMBER_COUNT_ERROR);
+        }
+
+        if (new HashSet<>(numbers).size() != LOTTO_NUMBER_COUNT) {
+            throw LottoException.from(ErrorMessage.DUPLICATE_LOTTO_NUMBERS_ERROR);
         }
     }
 
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -8,7 +8,10 @@ public enum ErrorMessage {
     INVALID_NUMBER_RANGE_ERROR("번호는 1에서 45 사이의 숫자여야 합니다. 유효한 범위 내의 숫자를 다시 입력해 주세요."),
     INVALID_LOTTO_COUNT_ERROR("로또 구매 수량은 1장 이상 100장 이하이어야 합니다. 다시 입력해 주세요."),
     INVALID_WINNING_NUMBERS_ERROR("당첨 번호는 6개의 숫자로 구성되어야 합니다. 다시 입력해 주세요."),
-    DUPLICATE_WINNING_NUMBER_ERROR("당첨 번호에 중복된 숫자가 있습니다. 중복 없이 6개의 숫자를 다시 입력해 주세요.");
+    DUPLICATE_WINNING_NUMBER_ERROR("당첨 번호에 중복된 숫자가 있습니다. 중복 없이 6개의 숫자를 다시 입력해 주세요."),
+    INVALID_LOTTO_NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBERS_ERROR("로또 번호는 중복될 수 없습니다.")
+    ;
 
     private final String message;
 
