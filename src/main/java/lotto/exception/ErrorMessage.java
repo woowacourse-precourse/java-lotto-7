028cac4efe9c554d0bc0c124ec6d7cbdde85516d
sanghun <vitash1215@gmail.com>
2024-11-04 22:14:18 +0900

    feat(Service) : LottoService class 작성
    로또 구매 및 결과 계산 로직 구현을 담당하는 class 즉 메인 로직 class 작성, Controller의 로직 복잡도를 낮추기 위해 분리

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,6 +4,8 @@ public enum ErrorMessage {
     INVALID_NUMBER("[ERROR] 숫자만 입력 가능합니다."),
     DUPLICATE_NUMBER("[ERROR] 중복된 숫자가 있습니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1000원 이상이어야 합니다."),
+    INVALID_PURCHASE_AMOUNT_UNIT("[ERROR] 구입 금액은 1000원 단위여야 합니다."),
     INVALID_BONUS_NUMBER("[ERROR] 보너스 번호는 1부터 45 사이의 당첨 번호와 중복되지 않은 수여야 합니다"),
     ;
     private final String message;
--- src/main/java/lotto/exception/ExceptionHandler.java
+++ src/main/java/lotto/exception/ExceptionHandler.java
@@ -1,8 +1,6 @@
 package lotto.exception;
 
 public class ExceptionHandler {
-
-
     public static void throwIllegalArgException(ErrorMessage error) {
         throw new IllegalArgumentException(error.getMessage());
     }
