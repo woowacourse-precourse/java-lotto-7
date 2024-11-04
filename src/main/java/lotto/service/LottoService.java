8b0ddbf67e632fa7d308cfa5c675e0f6b24894e1
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-11-04 17:02:44 +0900

    feat : 로또 구매 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,26 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LottoService {
+
+    private final List<Lotto> purchasedLottos = new ArrayList<>();
+
+    public void purchaseLottos(int amout){
+        for(int i=0;i<amout/1000;i++){
+            purchasedLottos.add(createNewLotto());
+        }
+    }
+
+    public Lotto createNewLotto(){
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6));
+    }
+
+
+
 }
