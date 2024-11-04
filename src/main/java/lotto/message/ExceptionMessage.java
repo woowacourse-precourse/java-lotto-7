2db86c51823712209f5a6732c230b2bfd9d2acab
gorapaduckoo <jioung93@naver.com>
2024-11-04 18:23:32 +0900

    style: ExceptionMessage.INVALID_NUMBER_INPUT 조사 수정

--- src/main/java/lotto/message/ExceptionMessage.java
+++ src/main/java/lotto/message/ExceptionMessage.java
@@ -4,7 +4,7 @@ public enum ExceptionMessage {
     INVALID_BLANK_INPUT("null 또는 빈 값은 입력할 수 없습니다."),
     INVALID_TYPE_INPUT("%s은(는) %s여야 합니다."),
     INVALID_RANGE_INPUT("%s은(는) %d부터 %d 사이의 숫자여야 합니다."),
-    INVALID_NUMBER_INPUT("%s는 %d로 나누어 떨어져야 합니다."),
+    INVALID_NUMBER_INPUT("%s은(는) %d로 나누어 떨어져야 합니다."),
     INVALID_DUPLICATION_INPUT("%s에 중복값이 있습니다.");
 
     private static final String PREFIX = "[ERROR] ";
