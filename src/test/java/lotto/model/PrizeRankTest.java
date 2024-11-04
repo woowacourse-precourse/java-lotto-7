81a9b45a66db13fca7b206eebd56fc51da3aa475
choi <nippert@naver.com>
2024-11-04 14:18:37 +0900

    test: 상금 통계 도메인 테스트 작성

--- src/test/java/lotto/model/PrizeRankTest.java
+++ src/test/java/lotto/model/PrizeRankTest.java
@@ -1,6 +1,5 @@
 package lotto.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,7 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class PrizeRankTest {
 
