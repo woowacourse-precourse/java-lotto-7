c296c21318b5dfb03cde0e447bb0bc29a125d1cb
gabean13 <choegabin171@gmail.com>
2024-11-04 22:49:02 +0900

    test(ApplicationTest) : application 통합 테스트 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,17 +1,18 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ApplicationTest extends NsTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class ApplicationTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
+    @DisplayName("조건에 맞춰서 입력한다")
     @Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
@@ -46,11 +47,67 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("조건에 맞춰서 입력하지 않아 다시 입력한다")
     @Test
-    void 예외_테스트() {
+    void 기능_테스트2() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("81234", "8000", "1,2,3,4,5,악", "1,2,3,4,5,6", "6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 5, 14, 22, 45]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 62.5%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 5, 14, 22, 45)
+        );
+    }
+
+    @Test
+    @DisplayName("구매 금액에 숫자가 아닌 값을 입력하면 예외가 발생한다")
+    void 예외_테스트1() {
         assertSimpleTest(() -> {
             runException("1000j");
-            assertThat(output()).contains(ERROR_MESSAGE);
+            assertThat(output()).contains(ERROR_MESSAGE + "구매 금액은 숫자어야합니다.");
+        });
+    }
+
+
+
+    @Test
+    @DisplayName("로또 번호에 숫자가 아닌 값을 입력하면 예외가 발생한다")
+    void 예외_테스트2() {
+        assertSimpleTest(() -> {
+            runException("1000", "가,나,다,라,마,바");
+            assertThat(output()).contains(ERROR_MESSAGE + "로또 번호는 숫자어야합니다.");
+        });
+    }
+
+    @Test
+    @DisplayName("보너스 번호에 숫자가 아닌 값을 입력하면 예외가 발생한다")
+    void 예외_테스트3() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,6", "냐냐");
+            assertThat(output()).contains(ERROR_MESSAGE + "보너스 번호는 1~45사이의 숫자여야합니다.");
         });
     }
 
