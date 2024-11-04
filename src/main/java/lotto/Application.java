01dc11a8c8a57a23ffcf77ca1ac57d8bdb286492
zerocat36 <Seo.james2020@gmail.com>
2024-11-04 22:15:00 +0900

    fix(Application.java): remove methods not related to the class
    - moved methods to Lotto.java class

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,57 +1,6 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
 public class Application {
-    private static List<Integer> getWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String input = Console.readLine();
-
-        try {
-            return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 로또 번호가 입력되었습니다.");
-        }
-    }
-
-    private static int getBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 보너스 번호가 입력되었습니다.");
-        }
-    }
-
-    private static int getPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
-
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 구입 금액이 입력되었습니다.");
-        }
-    }
-
-    private static List<List<Integer>> buyLotto() {
-        List<List<Integer>> lotto_numbers = new ArrayList<>(List.of());
-        int price = 1000;
-
-        int count = getPurchaseAmount() / price;
-        for (int i = 0; i < count; i++) {
-            List<Integer> buffer = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            lotto_numbers.add(buffer);
-            System.out.println(buffer);
-        }
-        return lotto_numbers;
-    }
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
