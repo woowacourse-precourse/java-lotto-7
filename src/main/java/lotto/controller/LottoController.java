75f54146b264c32a94ee3b776f0ab0d03a049726
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-04 20:07:57 +0900

    feat(LottoController): 입력한 금액을 통해 로또 개수 반환

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,5 +10,9 @@ public class LottoController {
 
     private void issue() {
         int cost = Integer.parseInt(InputView.requestCost());
+        Lottos lottos = Lottos.from(getCountFrom(cost));
+    }
+    private int getCountFrom(int cost) {
+        return cost / 1000;
     }
 }
