d85d913828bd83093927d30bb687ed513f25d1ae
kellyihyeon <kellykang13@gmail.com>
2024-11-04 22:18:53 +0900

    feat: 로또 수익률을 구하는 기능

--- src/main/java/lotto/LottoResultCalculator.java
+++ src/main/java/lotto/LottoResultCalculator.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LottoResultCalculator {
 
     private final static int BONUS_NUMBER_MATCH_COUNT = 5;
+    private final static int FIRST_PLACE_PRIZE = 2000000000;
+    private final static int SECOND_PLACE_PRIZE = 30000000;
+    private final static int THIRD_PLACE_PRIZE = 1500000;
+    private final static int FORTH_PLACE_PRIZE = 50000;
+    private final static int FIFTH_PLACE_PRIZE = 5000;
+
     private WinningNumber winningNumber;
 
 
@@ -15,5 +25,31 @@ public class LottoResultCalculator {
         boolean matchedBonusNumber = matchCount == BONUS_NUMBER_MATCH_COUNT && winningNumber.hasMatchingBonusNumberWith(userLotto);
 
         return Rank.findRankByMatchCount(matchCount, matchedBonusNumber);
+    }
+
+    public Map<Rank, Integer> calculateRanking(List<Lotto> lotteries) {
+        Map<Rank, Integer> statistics = new HashMap<>();
+        lotteries.forEach(lotto -> {
+            Rank rank = calculateRanking(lotto);
+            statistics.put(rank, statistics.getOrDefault(rank, 0) + 1);
+        });
+
+        return statistics;
+    }
+
+    public double calculateRateOfReturn(Map<Rank, Integer> results, int totalAmount) {
+        int totalRevenue = 0;
+
+        totalRevenue += results.getOrDefault(Rank.FIRST_PLACE, 0) * FIRST_PLACE_PRIZE;
+        totalRevenue += results.getOrDefault(Rank.SECOND_PLACE, 0) * SECOND_PLACE_PRIZE;
+        totalRevenue += results.getOrDefault(Rank.THIRD_PLACE, 0) * THIRD_PLACE_PRIZE;
+        totalRevenue += results.getOrDefault(Rank.FORTH_PLACE, 0) * FORTH_PLACE_PRIZE;
+        totalRevenue += results.getOrDefault(Rank.FIFTH_PLACE, 0) * FIFTH_PLACE_PRIZE;
+
+        if (totalAmount > 0) {
+            return ((double) totalRevenue / totalAmount) * 100;
+        }
+
+        return 0;
     }
 }
--- src/test/java/lotto/LottoResultCalculatorTest.java
+++ src/test/java/lotto/LottoResultCalculatorTest.java
@@ -2,10 +2,33 @@ package lotto;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class LottoResultCalculatorTest {
+
+    @Test
+    void 로또_수익률을_구한다() {
+        WinningNumber winningNumber = new WinningNumber(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+
+        List<Lotto> lotteries = new ArrayList<>();
+        lotteries.add(new Lotto(List.of(8, 21, 23, 41, 42, 43)));
+        lotteries.add(new Lotto(List.of(3, 5, 11, 16, 32, 38)));
+        lotteries.add(new Lotto(List.of(7, 11, 16, 35, 36, 44)));
+        lotteries.add(new Lotto(List.of(1, 8, 11, 31, 41, 42)));
+        lotteries.add(new Lotto(List.of(13, 14, 16, 38, 42, 45)));
+        lotteries.add(new Lotto(List.of(7, 11, 30, 40, 42, 43)));
+        lotteries.add(new Lotto(List.of(2, 13, 22, 32, 38, 45)));
+        lotteries.add(new Lotto(List.of(1, 3, 5, 14, 22, 45)));
+
+        LottoResultCalculator lottoResultCalculator = new LottoResultCalculator(winningNumber);
+        Map<Rank, Integer> ranking = lottoResultCalculator.calculateRanking(lotteries);
+        double rateOfReturn = lottoResultCalculator.calculateRateOfReturn(ranking, 8000);
+
+        assertEquals(62.5, rateOfReturn);
+    }
 
     @Test
     void 당첨_번호와_일치하는_번호가_6개_이면_1등이다() {
