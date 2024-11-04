606f040e274613b18958aad8be65c3f3aa359106
whdmscks11 <whdmscks11@gmail.com>
2024-11-04 17:18:03 +0900

    feat(LottoWinningNumbers): 멤버 변수 초기화를 생성자에서 setter 메서드로 변경하고, 입력값 검증 메서드 추가
    - ApplicationLauncher에서 각 입력을 while문으로 분리 했으므로 생성자 초기화가 부적절해져 setter로 초기화하도록 변경
    - 각 입력값이 요구사항에 맞는지 확인하는 검증 메서드 구현

--- src/main/java/lotto/domain/LottoWinningNumbers.java
+++ src/main/java/lotto/domain/LottoWinningNumbers.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import lotto.util.StringUtil;
 
@@ -9,24 +11,67 @@ public class LottoWinningNumbers {
     private static final String DELIMITER = ",";
 
     private final List<Integer> winningNumbers;
-    private final String mainNumbers;
-    private final int bonusNumber;
+    private String[] mainNumbers;
+    private int bonusNumber;
 
-    public LottoWinningNumbers(String secondLine, String thirdLine) {
-        // 검증 메서드 필요 : DELIMITER 사용 했는지
-        this.mainNumbers = secondLine;
-        // 검증 메서드 필요 : secondLine != thirdLine
-        Integer bonus = StringUtil.parseToPositiveInt(thirdLine);
-        this.bonusNumber = bonus;
+    public LottoWinningNumbers() {
         this.winningNumbers = new ArrayList<>();
     }
 
     public void generate() {
-        String[] splitNumbers = mainNumbers.split(DELIMITER);
-        // 검증 메서드 필요 : splitNumbers의 길이가 6인지
-        for (String splitNumber : splitNumbers) {
+        for (String splitNumber : mainNumbers) {
             Integer number = StringUtil.parseToPositiveInt(splitNumber);
             winningNumbers.add(number);
+        }
+    }
+
+    public void setMainNumbers(String secondLine) {
+        StringUtil.checkIfNull(secondLine);
+
+        validateDelimiter(secondLine);
+
+        String[] splitNumbers = secondLine.split(DELIMITER);
+        validateDuplicateInput(splitNumbers);
+
+        checkIfSixNumbers(splitNumbers);
+
+        this.mainNumbers = splitNumbers;
+    }
+
+    public void setBonusNumber(String thirdLine) {
+        StringUtil.checkIfNull(thirdLine);
+
+        checkIfBonusNumberDuplicated(thirdLine);
+        this.bonusNumber = StringUtil.parseToPositiveInt(thirdLine);
+    }
+
+    private void validateDelimiter(String input){
+        if (!input.contains(DELIMITER)) {
+            throw new IllegalArgumentException("[ERROR] 쉼표를 구분자로 사용해 주세요.");
+        }
+    }
+
+    private void validateDuplicateInput(String[] firstInput){
+        long count = Arrays.stream(firstInput)
+                .filter(o -> Collections.frequency(Arrays.asList(firstInput), o) > 1)
+                .count();
+
+        if (count > 0) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복 될 수 없습니다.");
+        }
+    }
+
+    private void checkIfBonusNumberDuplicated(String thirdLine) {
+        for (String mainNumber : mainNumbers) {
+            if(mainNumber.equals(thirdLine)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호로 사용된 번호와 같을 수 없습니다.");
+            }
+        }
+    }
+
+    private void checkIfSixNumbers(String[] splitNumbers){
+        if (splitNumbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자를 입력해야 합니다.");
         }
     }
 
