4c8a2200b476c757ceef6fa420883bde4c5ac200
SeongJunChun <csj991210@naver.com>
2024-11-04 19:13:59 +0900

    refactor: 상수클래스 private 생성자 추가

--- src/main/java/lotto/util/Constants.java
+++ src/main/java/lotto/util/Constants.java
@@ -2,12 +2,12 @@ package lotto.util;
 
 public final class Constants {
 
-    private Constants() {
-    }
-
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
     public static final int LOTTO_PRICE = 1000;
+
+    private Constants() {
+    }
 
 }
