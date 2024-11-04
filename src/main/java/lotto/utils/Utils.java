7d3bbf1712fcf81a924b10980749145da4c4efae
Awon01 <bec00@naver.com>
2024-11-04 21:03:57 +0900

    feat(utils): 두 리스트 비교 후 중복 요소 확인 메서드

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Utils {
+    private static final int SAME = 1;
+    private static final int NOT_SAME = 0;
 
     public static List<Integer> randomUniqueNumberGenerate(int start, int end, int count) {
         return Randoms.pickUniqueNumbersInRange(start, end, count);
@@ -24,4 +26,18 @@ public class Utils {
         return Arrays.stream(stringToIntArray(string)).boxed().collect(Collectors.toList());
     }
 
+    public static int countSameNumber(int number1, int number2) {
+        if (number1 == number2) {
+            return SAME;
+        }
+        return NOT_SAME;
+    }
+
+    public static int countSameElements(List<Integer> list1, List<Integer> list2) {
+        int count = 0;
+        for (int index = 0; index < list1.size(); index++) {
+            count += countSameNumber(list1.get(index), list2.get(index));
+        }
+        return count;
+    }
 }
