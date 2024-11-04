8895560a49fd1c85a39841dd3e5a767066b099ea
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-04 21:25:45 +0900

    feat(OutputView): 수량에 맞게 로또의 숫자들을 출력하는 함수 생성

--- src/main/java/lotto/view/OutView.java
+++ src/main/java/lotto/view/OutView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
+import lotto.model.Lotto;
 import lotto.model.Lottos;
 
 public class OutView {
 
     public static void printLottos(Lottos lottos) {
         System.out.println(lottos.getSize() + "개를 구매했습니다.");
+
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(lotto);
+        }
     }
 }
