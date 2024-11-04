b97dbcc5f7eb55147a445e352c28d871778bb551
wvssm <neoramiregi@gmail.com>
2024-11-04 17:13:28 +0900

    feat: 보너스 번호 입력 요청 메시지를 출력한다

--- src/main/java/lotto/view/OuputView.java
+++ src/main/java/lotto/view/OuputView.java
@@ -12,6 +12,7 @@ public class OuputView {
     private static final String LOTTO_COST_PROMPT = "구입금액을 입력해 주세요.";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
     public void displayLottoCostPrompt(){
         System.out.println(LOTTO_COST_PROMPT);
@@ -29,5 +30,9 @@ public class OuputView {
 
     public void displayWinningNumbersPrompt(){
         System.out.println(WINNING_NUMBERS_PROMPT);
+    }
+
+    public void displayBonusNumberPrompt(){
+        System.out.println(BONUS_NUMBER_PROMPT);
     }
 }
