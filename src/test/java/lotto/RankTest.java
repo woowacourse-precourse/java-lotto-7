df58d823503c69c07ede0229dc8359bce42ed3a2
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 16:23:18 +0900

    test(Rank): 일치하는 번호의 수를 비교하여 등수를 알 수 있다

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class RankTest {
+
+    @Test
+    void 세개_맞으면_5등이다() {
+        int matchCount = 3;
+        boolean hasBonusNumber = false;
+
+        assertThat(Rank.of(matchCount, hasBonusNumber)).isEqualTo(Rank.FIFTH);
+    }
+
+
+    @Test
+    void 네개_맞으면_4등이다() {
+        int matchCount = 4;
+        boolean hasBonusNumber = false;
+
+        assertThat(Rank.of(matchCount, hasBonusNumber)).isEqualTo(Rank.FOURTH);
+    }
+
+
+    @Test
+    void 보너스_없이_5개를_맞으면_3등이다() {
+        int matchCount = 5;
+        boolean hasBonusNumber = false;
+
+        assertThat(Rank.of(matchCount, hasBonusNumber)).isEqualTo(Rank.THIRD);
+    }
+
+
+    @Test
+    void 보너스에_더해_5개를_맞으면_2등이다() {
+        int matchCount = 5;
+        boolean hasBonusNumber = true;
+
+        assertThat(Rank.of(matchCount, hasBonusNumber)).isEqualTo(Rank.SECOND);
+    }
+
+
+    @Test
+    void 여섯개를_맞으면_1등이다() {
+        int matchCount = 6;
+        boolean hasBonusNumber = false;
+
+        assertThat(Rank.of(matchCount, hasBonusNumber)).isEqualTo(Rank.FIRST);
+    }
+}
