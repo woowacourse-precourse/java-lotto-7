99c440905656fcdb4b7e27cfa5682f94ff42b39f
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 19:29:14 +0900

    feat(WinningNumbers): 당첨 번호 관리 기능 및 중복 검증 기능 구현
    - 당첨 번호 관리 기능을 구현했습니다.
    - 당첨 번호 및 보너스 번호 중복 검증 기능을 구현했습니다.

---
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import static lotto.Exception.ExceptionErrorMessage.DUPLICATED_LOTTO_NUMBER_MESSAGE;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WinningNumbers {
+    private final Lotto winningLottoNumber;
+    private final int bonusLottoNumber;
+
+    public WinningNumbers(Lotto winningLottoNumber, int bonusLottoNumber) {
+        validateWinningNumbersAndBonusNumber(winningLottoNumber, bonusLottoNumber);
+        this.winningLottoNumber = winningLottoNumber;
+        this.bonusLottoNumber = bonusLottoNumber;
+    }
+
+    public Lotto getWinningLottoNumber() {
+        return winningLottoNumber;
+    }
+
+    public int getBonusLottoNumber() {
+        return bonusLottoNumber;
+    }
+
+    private void validateWinningNumbersAndBonusNumber(Lotto winningNumbers, int bonusNumber) {
+        Set<Integer> winningNumber = new HashSet<>(winningNumbers.getLottoNumbers());
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBER_MESSAGE.toString());
+        }
+    }
+}
