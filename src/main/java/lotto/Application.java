d5a3be04c11e8ee8f409f54d1bea52eb1e690a97
박규영 <magic9282m@gmail.com>
2024-11-04 21:54:27 +0900

    feat: add new method validateAmount
    인자로 주어진 값이 로또 구입 금액의 조건과 부합한지 검사하는 메서드.
    - 부합하지 않을 경우, IllegalArgumentException 예외를 던짐.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,12 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+    public static int validateAmount(int input) {
+        if(input <= 0 || input % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return input;
     }
 }
