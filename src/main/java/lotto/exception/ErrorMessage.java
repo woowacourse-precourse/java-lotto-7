93b9e26faba151b8e7fc077dff6726126549c77d
DongHoon Lee <dhl1924@naver.com>
2024-11-04 19:06:00 +0900

    feat(exception): 당첨 번호 에러처리 관련 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -5,9 +5,10 @@ public enum ErrorMessage {
     EMPTY_INPUT_ERROR("빈 문자열은 입력할 수 없습니다. 다시 입력해 주세요."),
     INVALID_AMOUNT_PATTERN_ERROR("구입 금액은 1000원 단위의 양수여야 합니다. 다시 입력해 주세요."),
     MAX_AMOUNT_ERROR("로또는 1인당 최대 100장까지 구매할 수 있습니다. 다시 입력해 주세요."),
-    INVALID_NUMBER_RANGE_ERROR("번호는 1에서 45 사이의 숫자여야 합니다. 유효한 범위 내의 숫자를 입력해 주세요."),
-    INVALID_LOTTO_COUNT_ERROR("로또 구매 수량은 1장 이상 100장 이하이어야 합니다.")
-    ;
+    INVALID_NUMBER_RANGE_ERROR("번호는 1에서 45 사이의 숫자여야 합니다. 유효한 범위 내의 숫자를 다시 입력해 주세요."),
+    INVALID_LOTTO_COUNT_ERROR("로또 구매 수량은 1장 이상 100장 이하이어야 합니다. 다시 입력해 주세요."),
+    INVALID_WINNING_NUMBERS_ERROR("당첨 번호는 6개의 숫자로 구성되어야 합니다. 다시 입력해 주세요."),
+    DUPLICATE_WINNING_NUMBER_ERROR("당첨 번호에 중복된 숫자가 있습니다. 중복 없이 6개의 숫자를 다시 입력해 주세요.");
 
     private final String message;
 
