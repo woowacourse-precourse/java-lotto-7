d483eeb747b0b7adfa0ad11599f62e91dfafb4bc
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-04 14:07:12 +0900

    refactor : 검증 로직 통합

--- src/main/java/lotto/validator/BonusWinningNumberValidator.java
+++ src/main/java/lotto/validator/BonusWinningNumberValidator.java
@@ -1,17 +1,18 @@
 package lotto.validator;
 
 public class BonusWinningNumberValidator {
-    public int validateBonusWinningNumber(String inputBonusNumber) {
+
+    public static int validateBonusWinningNumber(String inputBonusNumber) {
+
         if (!inputBonusNumber.matches("^\\d+$")) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자 하나만 입력해 주세요.");
         }
+
         int bonusNumber = Integer.parseInt(inputBonusNumber);
-        if (bonusNumber <= 0) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1 이상의 양수로 입력해 주세요.");
-        }
-        if (bonusNumber > 45) {
+        if (bonusNumber <= 0 || bonusNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+
         return bonusNumber;
     }
 }
--- src/main/java/lotto/validator/WinningNumbersValidator.java
+++ src/main/java/lotto/validator/WinningNumbersValidator.java
@@ -4,14 +4,21 @@ import java.util.Set;
 
 public class WinningNumbersValidator {
 
-    public void validateWinningNumbers(String winningNumbersInput) {
+    public static void validateWinningNumbers(String winningNumbersInput) {
+
         if (!winningNumbersInput.matches("^\\d+(,\\d+)*$")) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 쉼표(,)를 기준으로, 6개 입력해 주세요.");
         }
+
         String[] inputNumbers = winningNumbersInput.split(",");
         if (inputNumbers.length != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 쉼표(,)를 기준으로, 6개 입력해 주세요.");
         }
+
+        validateInputNumbers(inputNumbers);
+    }
+
+    private static void validateInputNumbers(String[] inputNumbers) {
 
         for (String inputNumber : inputNumbers) {
             if (!isNumeric(inputNumber)) {
@@ -19,16 +26,13 @@ public class WinningNumbersValidator {
             }
 
             int number = Integer.parseInt(inputNumber);
-            if (number <= 0) {
-                throw new IllegalArgumentException("[ERROR] 1 이상의 양수로 입력해 주세요.");
-            }
-            if (number > 45) {
+            if (number <= 0 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
         }
     }
 
-    public void validateDuplicateWinningNumbers(Set<Integer> winningNumbers) {
+    public static void validateDuplicateWinningNumbers(Set<Integer> winningNumbers) {
         if (winningNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복일 수 없습니다.");
         }
