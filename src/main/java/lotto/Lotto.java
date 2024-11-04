e76ed3704108b51096a5554228bbf92148e0d385
gichan222 <>
2024-11-04 17:09:09 +0900

    feat(Lotto) : 당첨 확인 메서드 추가
    당첨 로또 번호와 일치하는 개수 카운트 메서드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,10 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public int countMatchingNumbers(List<Integer> winningNumbers) {
+        return (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
 }
