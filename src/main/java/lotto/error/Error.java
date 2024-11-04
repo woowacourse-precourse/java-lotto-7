97e50b02460edbd2fcf4ecca37ec60c332702d97
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-04 20:09:58 +0900

    refactor: 에러 메시지 수정

--- src/main/java/lotto/error/Error.java
+++ src/main/java/lotto/error/Error.java
@@ -6,7 +6,7 @@ public enum Error {
     NOT_A_NUMBER("[ERROR] 숫자를 입력해 주세요."),
     OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다."),
-    DUPLICATE_WITH_WINNING_NUMBERS("[ERROR] 입력한 당첨 번호와 중복되지 않는 숫자를 입력해주세요. 입력한 당첨 번호: %s");
+    DUPLICATE_WITH_WINNING_NUMBERS("[ERROR] 입력한 당첨 번호와 중복되지 않는 숫자를 입력해주세요");
 
     private final String message;
 
