e14d8ff05ef55c3ad88d9e4208154b0ce3c71e2c
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-11-04 20:09:39 +0900

    Feat: LottoGame 생성: 랜덤 수 생성함수 호출과 수익률 계산 담당

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,4 +1,39 @@
 package lotto.controller;
 
+import lotto.common.Limit;
+import lotto.common.LottoRank;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoGame {
+    private final LottoGenerator lottoGenerator = new LottoGenerator();
+    private final List<Lotto> purchasedLottos;
+
+    public LottoGame(int purchaseAmount) {
+        int numberOfLottos = purchaseAmount / Limit.LOTTO_PRICE;
+        this.purchasedLottos = generateLottos(numberOfLottos);
+    }
+
+    private List<Lotto> generateLottos(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> lottoGenerator.generate())
+                .collect(Collectors.toList());
+    }
+
+    public List<Lotto> getPurchasedLottos() {
+        return purchasedLottos;
+    }
+
+    public Map<LottoRank, Integer> calculateStatistics(Lotto winningLotto, int bonusNumber) {
+        Lottos lottos = new Lottos(purchasedLottos);
+        return lottos.calculateRank(winningLotto, bonusNumber);
+    }
 }
+
+
