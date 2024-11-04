ca66e9fa88d7355d2269a8bb6cd09e6da7300b92
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-04 10:07:18 +0900

    feat(startLottoBusiness): 로또 로직 시작 함수에 집중
    - input로직 추가
    - lottoTransactionController의 핵심 기능 추가
    - 테스트 완료
    
    main()에서 실행하는게 아닌 MainController에서 시작하기에 설정 추후에 새로운 controller가 추가시에 MainController로 모여져서 사용될 예정이기에 이와 같이 구현했습니다

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,17 +1,29 @@
 package lotto.controller;
 
 
+import java.util.List;
+import lotto.view.InputView;
+
 public class MainController {
   final private LottoTransactionController lottoTransactionController;
+  final private InputView inputView;
 
   public MainController() {
 
     this.lottoTransactionController = new LottoTransactionController();
+    this.inputView = new InputView();
   }
 
   public void startLottoBusiness() {
-    // TODO 새롭게 코드 구현
+    int amount = inputView.requestPurchaseAmount();
+    lottoTransactionController.sellAutoLotto(amount);
 
+    List<Integer> numbers = inputView.requestWinningNumbers();
+    int bonus = inputView.requestBonusNumber();
+
+    lottoTransactionController.compareWinningNumbers(numbers, bonus);
+
+    lottoTransactionController.showLottoStatistics();
   }
 
 }
