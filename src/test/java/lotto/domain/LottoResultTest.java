a71021394cc2c43ab2e9c3b6c0b0767ab4d02406
wogus <ind07152@naver.com>
2024-11-04 16:10:05 +0900

    test(LottoResultTest): 테스트 메소드 설명 추가
    어떤 메소드를 테스트하는지 설명을 추가한다.

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -12,7 +12,7 @@ class LottoResultTest {
             "2,false,NONE", "2,true,NONE", "3,false,FIFTH", "3,true,FIFTH",
             "4,false,FOURTH", "4,true,FOURTH", "5,false,THIRD", "5,true,SECOND",
             "6,false,FIRST", "6,true,FIRST"})
-    @DisplayName("맞춘 개수와 보너스 숫자 판별 여부로 결과가 정확히 정해진다.")
+    @DisplayName("findLottoResult - 맞춘 개수와 보너스 숫자 판별 여부로 결과가 정확히 정해진다.")
     void findLottoResult(int matchedCount, boolean isBonusMatched, LottoResult expected) {
         LottoResult lottoResult = LottoResult.findLottoResult(matchedCount, isBonusMatched);
         assertThat(lottoResult).isEqualTo(expected);
