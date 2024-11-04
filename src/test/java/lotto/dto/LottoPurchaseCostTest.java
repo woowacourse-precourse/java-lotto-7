8cdc4c16dc0b944e520fdedd0ed87815e6ba80da
신관규 <soundbar91@gmail.com>
2024-11-04 10:16:43 +0900

    test: dto 테스트 추가

---
+++ src/test/java/lotto/dto/LottoPurchaseCostTest.java
@@ -0,0 +1,31 @@
+package lotto.dto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoPurchaseCostTest {
+    @Test
+    void 구입_금액이_1000원_단위가_아니면_예외를_발생한다() {
+        assertThatThrownBy(() -> LottoPurchaseCost.of("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_양수가_아니면_예외를_발생한다() {
+        assertThatThrownBy(() -> LottoPurchaseCost.of("-1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_숫자가_아니면_예외를_발생한다() {
+        assertThatThrownBy(() -> LottoPurchaseCost.of("o.o"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입_금액이_자료형_범위를_벗어나면_아니면_예외를_발생한다() {
+        assertThatThrownBy(() -> LottoPurchaseCost.of("2147483648"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
