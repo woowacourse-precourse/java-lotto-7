8ce59a60721145722813f52749f7a948fdc4b07a
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:57:32 +0900

    test(Money): 금액끼리 더할 수 있다

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -30,5 +30,24 @@ public class Money {
     public Money add(Money money) {
         return new Money(amount + money.amount);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Money money = (Money) o;
+
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (amount ^ (amount >>> 32));
+    }
 }
 
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -15,14 +15,22 @@ public class MoneyTest {
     }
 
     @ParameterizedTest(name = "{0}으로 {1}짜리를 사면 잔돈이 생긴다={2}")
-    @CsvSource(value = {"15000,1500,false", "10000,1100,true"}, delimiter = ',')
+    @CsvSource(value = {"8000,800,false", "9000,1100,true"}, delimiter = ',')
     void 잔돈의_유무를_알_수_있다(int amount, int price, boolean expected) {
         assertThat(new Money(amount).hasChangesWith(price)).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "{0}으로 {1}짜리를 {2}개 살 수 있다")
-    @CsvSource(value = {"15000,1500,10", "10000,1100,9"}, delimiter = ',')
+    @CsvSource(value = {"8000,800,10", "9000,1100,8"}, delimiter = ',')
     void 투입_금액으로_살_수_있는_양을_알_수_있다(int amount, int price, int expected) {
         assertThat(new Money(amount).countAffordableFor(price)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource(value = {"8000,9000,17000", "9000,1100,10100"}, delimiter = ',')
+    void 금액끼리_더할_수_있다(int amount, int otherAmount, int expected) {
+        Money addedMoney = new Money(amount).add(new Money(otherAmount));
+
+        assertThat(addedMoney).isEqualTo(new Money(expected));
     }
 }
