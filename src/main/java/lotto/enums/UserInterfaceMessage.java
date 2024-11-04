af710ab1d1c5194f821e1a590fcb83f61f421151
Timothy <timothy@jungnanum.com>
2024-11-04 23:32:44 +0900

    feat: 사용자의 입력값과 추첨자의 입력값을 서로 비교 및 결과 처리
    - 오타수정

--- src/main/java/lotto/enums/UserInterfaceMessage.java
+++ src/main/java/lotto/enums/UserInterfaceMessage.java
@@ -3,7 +3,7 @@ package lotto.enums;
 public enum UserInterfaceMessage {
     REQUEST_INPUT_PRICE("구입금액을 입력해 주세요."),
     REQUEST_INPUT_PRICE_INFO("로또 당첨 번호는 %d ~ %d까지 6개를 입력받습니다. 번호는 쉼표로 구분해주세요."),
-    COUNT_BOUGHT("%d개를 구매했습니다"),
+    COUNT_BOUGHT("%d개를 구매했습니다."),
     REQUEST_INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     REQUEST_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     REQUEST_INPUT_BONUS_NUMBER_INFO("보너스 번호는 1개를 입력받습니다."),
