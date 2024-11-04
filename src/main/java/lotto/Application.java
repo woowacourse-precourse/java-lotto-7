7338270c2d8501e7010a089b7022858d0aa98f47
lrive <dlwldms9803@gmail.com>
2024-11-04 22:58:31 +0900

    feat: 에러가 발생하면 잘못 입력된 부분부터 다시 입력받는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -18,22 +18,40 @@ public class Application {
     }
 
     private static int getPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        int amount = Integer.parseInt(Console.readLine());
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int amount = Integer.parseInt(Console.readLine());
+                if (amount % 1000 != 0) {
+                    throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+                }
+                return amount;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
         }
-        return amount;
     }
 
     private static WinNumbers getWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String[] numbers = Console.readLine().split(",");
-        return new WinNumbers(numbers);
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String[] numbers = Console.readLine().split(",");
+                return new WinNumbers(numbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
     }
 
     private static int getBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                return Integer.parseInt(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
     }
 }
