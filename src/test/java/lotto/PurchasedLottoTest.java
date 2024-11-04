577589190add5cb34e091b98835322bf3241dd53
hongkikii <hyangmeeh@icloud.com>
2024-11-04 19:02:02 +0900

    refactor(PurchasedLottoTest): 메서드 추출

--- src/test/java/lotto/PurchasedLottoTest.java
+++ src/test/java/lotto/PurchasedLottoTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PurchasedLottoTest {
-
     @DisplayName("1000원당 로또 1장을 발행한다.")
     @Test
     void 천원당_로또_한장을_발행한다() {
-        Payment payment = Payment.from("1000");
+        validateLottoCount("1000", 1);
+        validateLottoCount("2000", 2);
+        validateLottoCount("5000", 5);
+    }
+
+    void validateLottoCount(String paymentAmount, int expectedCount) {
+        Payment payment = Payment.from(paymentAmount);
         PurchasedLotto purchasedLotto = PurchasedLotto.from(payment);
-        assertEquals(purchasedLotto.getCount(), 1);
+        assertEquals(expectedCount, purchasedLotto.getCount());
     }
 }
