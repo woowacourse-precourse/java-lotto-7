e6ed1f6c80eaaf74b49c48d8346df6ce727d5912
Parkjyun <jaeyun0507@naver.com>
2024-11-04 22:23:15 +0900

    refactor(LottoGenerator): 매직넘버 삭제

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
+    private static final int START_INDEX = 0;
     private final NumberGenerator numberGenerator;
 
     public LottoGenerator(NumberGenerator numberGenerator) {
@@ -12,7 +13,7 @@ public class LottoGenerator {
 
     public List<Lotto> generate(int numberOfLotto) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < numberOfLotto; i++) {
+        for (int i = START_INDEX; i < numberOfLotto; i++) {
             lottos.add(new Lotto(numberGenerator.generate()));
         }
         return lottos;
