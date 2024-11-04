0d05f33aadf1144b7e4bde0a8a338c70ebec99ff
badgelatte <saucy5939@gmail.com>
2024-11-04 17:30:57 +0900

    새로 복사하여 저장하는 방식으로 수정
    List.of()로 만든 경우 UnsupportedOperationException이 발생하여 이를 해결하기 위해
    새로 생성하는 방식으로 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -13,8 +14,8 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         hasDuplicateNum(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     public List<Integer> getNumbers() {
