dcd0ebe4541f1ba3c390b34d07b1bf545e442425
SeongJunChun <csj991210@naver.com>
2024-11-04 13:09:08 +0900

    feat: 당첨 로또 기능 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class WinningLotto {
+
+    private final Lotto winngLotto;
+    private final BonusNumber bonusNumber;
+
+    private WinningLotto(Lotto winngLotto, BonusNumber bonusNumber) {
+        validateDuplicate(winngLotto, bonusNumber);
+        this.winngLotto = winngLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto createWinningLotto(Lotto winngLotto, BonusNumber bonusNumber) {
+        return new WinningLotto(winngLotto, bonusNumber);
+    }
+
+    public Rank getRank(Lotto lotto) {
+        return Rank.calculateRank(winngLotto.countSameNumbers(lotto), bonusNumber.hasBonusNumber(lotto));
+    }
+
+    private void validateDuplicate(Lotto lotto, BonusNumber bonusNumber) {
+        if (bonusNumber.hasBonusNumber(lotto)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
