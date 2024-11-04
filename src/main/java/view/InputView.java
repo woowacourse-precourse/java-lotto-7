6ba70ec89a2ffa9ab876af88a4240ea49a6006e1
xeimun <hinote444@naver.com>
2024-11-04 18:04:24 +0900

    refactor(InputView, ExceptionMessage): 숫자 예외 처리에서 양의 정수 예외 처리로 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,8 +4,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import static java.lang.Integer.parseInt;
 import static view.message.ExceptionMessage.BANK_EXCEPTION_MESSAGE;
 import static view.message.ExceptionMessage.COMMA_SEPARATED_NUMBERS_REGEX;
-import static view.message.ExceptionMessage.NUMBER_EXCEPTION_MESSAGE;
-import static view.message.ExceptionMessage.NUMBER_REGEX;
+import static view.message.ExceptionMessage.POSITIVE_NUMBER_EXCEPTION_MESSAGE;
+import static view.message.ExceptionMessage.POSITIVE_NUMBER_REGEX;
 import static view.message.ExceptionMessage.FORMAT_EXCEPTION_MESSAGE;
 
 import java.math.BigDecimal;
@@ -17,7 +17,7 @@ public class InputView {
     public static BigDecimal readPurchaseAmount() {
         String purchaseAmount = readLine();
         validateBlank(purchaseAmount);
-        validateNumber(purchaseAmount);
+        validatePositiveNumber(purchaseAmount);
         return new BigDecimal(purchaseAmount);
     }
 
@@ -33,7 +33,7 @@ public class InputView {
     public static int readBonusNumber() {
         String bonusNumber = readLine();
         validateBlank(bonusNumber);
-        validateNumber(bonusNumber);
+        validatePositiveNumber(bonusNumber);
         return parseInt(bonusNumber);
     }
 
@@ -43,9 +43,9 @@ public class InputView {
         }
     }
 
-    private static void validateNumber(String input) {
-        if (!input.matches(NUMBER_REGEX)) {
-            throw new IllegalArgumentException(NUMBER_EXCEPTION_MESSAGE);
+    private static void validatePositiveNumber(String input) {
+        if (!input.matches(POSITIVE_NUMBER_REGEX)) {
+            throw new IllegalArgumentException(POSITIVE_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
--- src/main/java/view/message/ExceptionMessage.java
+++ src/main/java/view/message/ExceptionMessage.java
@@ -5,8 +5,8 @@ import java.math.BigDecimal;
 public class ExceptionMessage {
 
     public static final String BANK_EXCEPTION_MESSAGE = "입력값이 빈 공백이거나 비어있으면 안 됩니다.";
-    public static final String NUMBER_EXCEPTION_MESSAGE = "입력값은 숫자여야 합니다.";
-    public static final String NUMBER_REGEX = "\\d+";
+    public static final String POSITIVE_NUMBER_REGEX = "^[1-9]\\d*$";
+    public static final String POSITIVE_NUMBER_EXCEPTION_MESSAGE = "입력값은 양의 정수여야 합니다.";
     public static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "구입 금액은 음수이면 안됩니다.";
     public static final String LIMIT_EXCEED_EXCEPTION_MESSAGE = "로또 구입 금액은 10만 원을 초과할 수 없습니다.";
     public static final BigDecimal MAX_PURCHASE_AMOUNT_TEN_THOUSAND = BigDecimal.valueOf(100_000);
