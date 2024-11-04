2ffec7ec8029b5a7dc0a61d995827c7eb91a6a75
KHE <99ant@naver.com>
2024-11-04 13:55:29 +0900

    feat(Application): enter bonus number

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -46,6 +46,18 @@ public class Application {
             return winningNumbers;
         }
 
+        private static int inputBonusNumber(List<Integer> winningNumbers) {
+            System.out.println("보너스 번호를 입력해 주세요.");
+            int bonusNumber = Integer.parseInt(Console.readLine().trim());
+
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            } else if (winningNumbers.contain(bonusNumber)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+            }
+            return bonusNumber;
+        }
+
         private static boolean isValidLottoNumbers(List<Integar> numbers) {
             return numbers.stream().allMatch(num -> num >= 1 && num <= 45) &&
                     numbers.stream().distinct().count() == numbers.size();
