1237a88920525b272226abf6b9aa48c9e2124eed
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-04 11:27:20 +0900

    fix : Change all method logics
    - 한 기능씩 수정해야하지만 전체적으로 문제가 있어 모든 메서드를 수정함.
    - main 메서드에서 정상적으로 동작하도록 수정함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,23 +10,41 @@ public class Application {
     private static final String LOTTO_PURCHASE_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String LOTTO_BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_QUANTITY_MESSAGE = "개를 구매했습니다.";
 
     public static void main(String[] args) {
+        try {
+            System.out.println(LOTTO_PURCHASE_PRICE_INPUT_MESSAGE);
+            List<Integer> lottoPurchaseAmount = getLottoPurchaseAmount(Console.readLine());
 
+            getWinningNumber();
+            getBonusNumber();
+
+            System.out.println(getLottoCount(lottoPurchaseAmount) + LOTTO_QUANTITY_MESSAGE);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public int getLottoPurchaseAmount() {
-        System.out.println(LOTTO_PURCHASE_PRICE_INPUT_MESSAGE);
-        int lottoPurchaseAmount = Integer.parseInt(Console.readLine());
+    public static List<Integer> getLottoPurchaseAmount(String lottoPurchaseAmount) {
+        List<Integer> purchaseAmount = new ArrayList<>();
 
-        if (lottoPurchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로만 입력이 가능합니다.");
+        for (String amount : lottoPurchaseAmount.split(",")) {
+            int parsedAmount = Integer.parseInt(amount.trim());
+
+            if (parsedAmount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로만 입력이 가능합니다.");
+            }
+
+            purchaseAmount.add(parsedAmount);
         }
 
-        return lottoPurchaseAmount;
+        return purchaseAmount;
     }
 
-    public List<Integer> getWinningNumber() {
+
+    public static List<Integer> getWinningNumber() {
         System.out.println(LOTTO_WINNING_NUMBER_INPUT_MESSAGE);
         List<String> winningNumbers = new ArrayList<>(List.of(Console.readLine().split(",")));
 
@@ -39,12 +57,18 @@ public class Application {
         return integerList;
     }
 
-    public int getBonusNumber() {
+    public static int getBonusNumber() {
         System.out.println(LOTTO_BONUS_NUMBER_INPUT_MESSAGE);
         return Integer.parseInt(Console.readLine());
     }
 
-    public int getLottoCount() {
-        return getLottoPurchaseAmount() / 1000;
+    public static int getLottoCount(List<Integer> purchaseAmounts) {
+        int totalAmount = 0;
+
+        for (int amount : purchaseAmounts) {
+            totalAmount += amount;
+        }
+
+        return totalAmount / 1000;
     }
 }
