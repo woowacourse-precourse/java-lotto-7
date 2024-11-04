97ba39946701f1741941bc409a38e17cf60fcf5a
davemins <davemins4125@naver.com>
2024-11-04 22:51:49 +0900

    refactor(OutputView): 문자열 상수 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class OutputView {
     private static final String NUMBER_OF_LOTTOS_PURCHASED_MESSAGE = "\n%d개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String WINNING_STATISTICS_DIVIDER = "---";
+    private static final String TOTAL_RETURN_RATE_MESSAGE = "총 수익률은 ";
+    private static final String PERCENTAGE_MESSAGE = "%입니다.";
+    private static final String MATCH_THREE_MESSAGE = "3개 일치 (5,000원) - %d개";
+    private static final String MATCH_FOUR_MESSAGE = "4개 일치 (50,000원) - %d개";
+    private static final String MATCH_FIVE_MESSAGE = "5개 일치 (1,500,000원) - %d개";
+    private static final String MATCH_FIVE_WITH_BONUS_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    private static final String MATCH_SIX_MESSAGE = "6개 일치 (2,000,000,000원) - %d개";
 
     public static void printNumberOfLottoPurchased(int numberOfLottosPurchased) {
         System.out.printf(NUMBER_OF_LOTTOS_PURCHASED_MESSAGE, numberOfLottosPurchased);
@@ -18,14 +27,14 @@ public class OutputView {
         }
     }
 
-    public static void printStatics(List<Integer> finalMatchNumbers, double RateOfReturn) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + finalMatchNumbers.get(4) + "개");
-        System.out.println("4개 일치 (50,000원) - " + finalMatchNumbers.get(3) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + finalMatchNumbers.get(2) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + finalMatchNumbers.get(1) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + finalMatchNumbers.get(0) + "개");
-        System.out.println("총 수익률은 " + RateOfReturn + "%입니다.");
+    public static void printStatics(List<Integer> finalMatchNumbers, double rateOfReturn) {
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(WINNING_STATISTICS_DIVIDER);
+        System.out.printf((MATCH_THREE_MESSAGE) + "\n", finalMatchNumbers.get(4));
+        System.out.printf((MATCH_FOUR_MESSAGE) + "\n", finalMatchNumbers.get(3));
+        System.out.printf((MATCH_FIVE_MESSAGE) + "\n", finalMatchNumbers.get(2));
+        System.out.printf((MATCH_FIVE_WITH_BONUS_MESSAGE) + "\n", finalMatchNumbers.get(1));
+        System.out.printf((MATCH_SIX_MESSAGE) + "\n", finalMatchNumbers.get(0));
+        System.out.println(TOTAL_RETURN_RATE_MESSAGE + rateOfReturn + PERCENTAGE_MESSAGE);
     }
 }
