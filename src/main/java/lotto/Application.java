c9a70fb29df1c4ec778cbc09231712f08f177fff
bbangHo <sonsins1@naver.com>
2024-11-04 23:20:29 +0900

    refactor: 파일을 분리한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,34 +2,13 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.config.ApplicationConfiguration;
-
-import java.util.List;
-import java.util.Map;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
         ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
-        InputView inputView = applicationConfiguration.inputView();
-        OutputView outputView = applicationConfiguration.outputView();
-        LottoProcessor lottoProcessor = applicationConfiguration.lottoProcessor();
-
-        int purchaseAmount = inputView.readPurchaseAmount();
-        int purchaseCount = purchaseAmount / 1000;
-
-        outputView.printPurchaseNumber(purchaseAmount);
-        List<List<Integer>> purchaseLottoNumbers = LottoUtils.generate(purchaseCount);
-
-        outputView.PrintPurchaseHistory(purchaseLottoNumbers);
-        List<Integer> inputWinningNumbers = inputView.readWinningNumbers();
-
-        int inputBonusNumber = inputView.readBonusNumber();
-
-        Lotto lotto = new Lotto(inputWinningNumbers);
-
-        Map<LottoType, Integer> lottoPickResult = lottoProcessor.lottoPick(purchaseLottoNumbers, lotto, inputBonusNumber);
-        outputView.printWinningStatistics(lottoPickResult);
-
-        outputView.printTotalReturn(lottoProcessor, purchaseAmount);
+        LottoController lottoController = applicationConfiguration.lottoController();
+        lottoController.lottoProcess();
         Console.close();
     }
 }
