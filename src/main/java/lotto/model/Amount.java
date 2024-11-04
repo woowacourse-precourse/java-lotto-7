73039d60a33031ee0cd13d38e925c8336eb306dd
김주언 <wndjs803@naver.com>
2024-11-04 13:32:02 +0900

    feat: 로또 구매 개수 출력 기능

--- src/main/java/lotto/model/Amount.java
+++ src/main/java/lotto/model/Amount.java
@@ -2,21 +2,27 @@ package lotto.model;
 
 public class Amount {
     private final int amount;
+    private final int count;
     private final int UNIT_AMOUNT = 1000;
 
     public Amount(String input){
         int amountNumber = parseInput(input);
         validateDivisible(amountNumber);
         this.amount = amountNumber;
+        this.count = amount / UNIT_AMOUNT;
     }
 
     private void validateDivisible(int amountNumber){
-        if(amountNumber / UNIT_AMOUNT != 0){
+        if(amountNumber % UNIT_AMOUNT != 0){
             throw new IllegalArgumentException("[ERROR] 로또는 개당 1000원 입니다.");
         }
     }
 
     private int parseInput(String input){
         return Integer.parseInt(input);
+    }
+
+    public int getCount(){
+        return this.count;
     }
 }
