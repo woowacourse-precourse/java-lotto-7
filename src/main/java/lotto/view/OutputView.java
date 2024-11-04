9049c02422946cecc721a921ec4ea8470be2e97e
Minseok <saltishDev@gmail.com>
2024-11-04 11:44:54 +0900

    refactor: OutputView에서의 개행은 함수를 사용하지 않도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,14 +13,14 @@ public class OutputView {
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계" + NEXT_LINE + "---";
     private static final String QUANTITY_MESSAGE_FORMAT = "%d개를 구매했습니다.";
     private static final String RATE_OF_RETURN_MESSAGE_FORMAT = "총 수익률은 %.1f%%입니다.";
+    private static final String LOTTO_RESULT_MESSAGE_FORMAT = "%s%d" + COUNT;
 
     public void printPurchaseAmountMessage() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
     }
 
     public void printQuantity(int quantity) {
-        System.out.printf(QUANTITY_MESSAGE_FORMAT, quantity);
-        nextLine();
+        System.out.printf(QUANTITY_MESSAGE_FORMAT + NEXT_LINE, quantity);
     }
 
     public void printLottos(Lottos lottos) {
@@ -38,10 +38,9 @@ public class OutputView {
     public void showLottosResult(LottosResult lottosResult) {
         System.out.println(WINNING_STATISTICS_MESSAGE);
         for (LottoRank lottoRank : LottoRank.getAllLottoRank(false)) {
-            System.out.println(lottoRank.getDescription() + lottosResult.get(lottoRank) + COUNT);
+            System.out.printf(LOTTO_RESULT_MESSAGE_FORMAT + NEXT_LINE, lottoRank.getDescription(), lottosResult.get(lottoRank));
         }
-        System.out.printf(RATE_OF_RETURN_MESSAGE_FORMAT, lottosResult.getRateOfReturn());
-        nextLine();
+        System.out.printf(RATE_OF_RETURN_MESSAGE_FORMAT + NEXT_LINE, lottosResult.getRateOfReturn());
     }
 
     public void printErrorMessage(String errorMessage) {
