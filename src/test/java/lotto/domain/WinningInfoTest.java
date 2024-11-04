b5755ecc39ad8eec127f20cc4e18aa631d539f8f
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-04 18:42:51 +0900

    test(WinningInfo): 당첨 정보에 대한 테스트 케이스 추가
    - WinningInfo 클래스의 isWinningNumberContainThisNumber 메서드에 대한 테스트 케이스 추가
    - WinningInfo 클래스의 isBonusNumberSameAsThis 메서드에 대한 테스트 케이스 추가

--- src/test/java/lotto/domain/WinningInfoTest.java
+++ src/test/java/lotto/domain/WinningInfoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -26,5 +27,37 @@ public class WinningInfoTest {
 
         assertThatCode(() -> new WinningInfo(winningNumbers, bonusNumber))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 매개변수로_받은_번호가_당첨_번호에_존재할_경우_1을_반환한다() {
+        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(10);
+        WinningInfo winningInfo = new WinningInfo(winningNumbers, bonusNumber);
+        assertThat(winningInfo.isWinningNumbersContainThisNumber(3)).isEqualTo(1);
+    }
+
+    @Test
+    void 매개변수로_받은_번호가_당첨_번호에_존재하지_않을_경우_0을_반환한다() {
+        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(10);
+        WinningInfo winningInfo = new WinningInfo(winningNumbers, bonusNumber);
+        assertThat(winningInfo.isWinningNumbersContainThisNumber(10)).isEqualTo(0);
+    }
+
+    @Test
+    void 매개변수로_받은_번호가_보너스_번호와_같을_경우_1을_반환한다() {
+        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(10);
+        WinningInfo winningInfo = new WinningInfo(winningNumbers, bonusNumber);
+        assertThat(winningInfo.isBonusNumberSameAsThis(10)).isEqualTo(1);
+    }
+
+    @Test
+    void 매개변수로_받은_번호가_보너스_번호와_다를_경우_0을_반환한다() {
+        Lotto winningNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(10);
+        WinningInfo winningInfo = new WinningInfo(winningNumbers, bonusNumber);
+        assertThat(winningInfo.isBonusNumberSameAsThis(20)).isEqualTo(0);
     }
 }
