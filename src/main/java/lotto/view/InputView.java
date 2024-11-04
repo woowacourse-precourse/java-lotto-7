ef026459947902543d059999389f666a9896d11e
hyeblee <hyeb1224@naver.com>
2024-11-04 15:10:01 +0900

    refactor(view): 사용자에게 구입금액 입력 요청 안내
    -promptPurchaseAmount에 입력 요청 메시지 출력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     Scanner sc = new Scanner(System.in);
 
     public String promptPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
         String inputAmount = sc.next();
         return inputAmount;
     }
