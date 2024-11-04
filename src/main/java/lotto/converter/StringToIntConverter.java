11d56ad872d4f7394ef8aa3c606d82f19568da66
GiSeok Kang <ime.rokz@gmail.com>
2024-11-04 12:32:07 +0900

    refactor: 공백과 null에 대한 예외처리 추가

--- src/main/java/lotto/converter/StringToIntConverter.java
+++ src/main/java/lotto/converter/StringToIntConverter.java
@@ -10,6 +10,7 @@ public class StringToIntConverter {
 
     public int convertStringNumberToInteger(String rawNum) {
         try {
+            validate(rawNum);
             return Integer.parseInt(rawNum.trim());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMsg());
@@ -20,11 +21,29 @@ public class StringToIntConverter {
         List<Integer> numbers = new ArrayList<>();
         try {
             for (String rawNumber : rawNums.split(DELIMITER)) {
+                validate(rawNumber);
                 numbers.add(convertStringNumberToInteger(rawNumber));
             }
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMsg());
         }
         return numbers;
+    }
+
+    private void validate(String rawNumber) {
+        validateIsNull(rawNumber);
+        validateIsBlank(rawNumber);
+    }
+
+    private void validateIsNull(String rawNumber) {
+        if (rawNumber == null) {
+            throw new NullPointerException(ErrorMessage.IS_NULL.getMsg());
+        }
+    }
+
+    private void validateIsBlank(String rawNumber) {
+        if (rawNumber.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.IS_BLANK.getMsg());
+        }
     }
 }
