6c6180ad86fffd79dc8eef96f37e833c7fbb60e5
thsghdud13 <thsgdud25@naver.com>
2024-11-04 21:48:43 +0900

    feat: 입력 기능 및 입력 메세지 정의

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,11 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.InputMessage;
 
 public class InputView {
-    public static String getInput(String message) {
-        System.out.println(message);
+    public static String getInput(InputMessage message) {
+        System.out.println(message.getMessage());
         return Console.readLine();
     }
 }
