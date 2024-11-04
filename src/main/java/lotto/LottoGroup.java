7c11f43837d7567372ce55bac7d4044fd63ecdce
bee9827 <ehfrhfo9494@naver.com>
2024-11-04 12:51:38 +0900

    feat: Get all lotto numbers to List<String>
    로또 넘버를 List<String> 으로 받는다.

--- src/main/java/lotto/LottoGroup.java
+++ src/main/java/lotto/LottoGroup.java
@@ -1,8 +1,5 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +18,20 @@ public class LottoGroup {
         return new LottoGroup(lottos);
     }
 
+    public int getLottoGroupSize(){
+        return lottos.size();
+    }
+
+    public List<String> lottoNumbers() {
+        return lottos.stream()
+                .map(Lotto::toString).toList();
+    }
+
     // TODO
     //  ### 로또그룹
     //  - [ ] 일급 컬렉션으로 구현한다.
     //  - [x] N개 만큼 로또 생성
-    //  - [ ] 로또에서 받은 번호를 N개 출력한다
+    //  - [x] 로또에서 받은 번호를 N개 출력한다
     //  - [ ] 로또 등수를 이용하여 통계를 출력한다.
     //  - [ ] 로또 등수를 이용하여 수익율을 계산한다.
     //  - [ ] 로또 수익율 출력한다.
