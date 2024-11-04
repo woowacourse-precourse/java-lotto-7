200a546d8437cb2522e5938eb98d170a0a799e79
o-jeong <ljj020304@gmail.com>
2024-11-04 16:16:10 +0900

    feat: 수익률 계산기 객체 구현

---
+++ src/main/java/lotto/domain/ROICalculator.java
@@ -0,0 +1,8 @@
+package lotto.domain;
+
+public class ROICalculator {
+    public static double calculate(int totalSpent, int totalPrize) {
+        if (totalSpent == 0) return 0;
+        return Math.round((totalPrize / (double) totalSpent) * 1000) / 10.0;
+    }
+}
