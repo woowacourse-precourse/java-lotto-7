eb3f9445fb9a7cd9ceac51a4d35a927a91626788
AhnJoonSung <tjdtna01@naver.com>
2024-11-04 22:21:27 +0900

    feat: main 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        runLottoSystem();
+    }
+
+    private static void runLottoSystem() {
+        Input input = new Input();
+        Output output = new Output();
+        Customer customer = new Customer();
+
+        boolean isRunning = true;
+        while (isRunning) {
+            try {
+                startLottoProcess(input, output, customer);
+                isRunning = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("다시 시도해 주세요.");
+            }
+        }
+    }
+
+    private static void startLottoProcess(Input input, Output output, Customer customer) {
+        output.printGetMoneyMessage();
+        int money = input.getCustomerMoney();
+        customer.purchaseLotto(money);
+        output.printPurchaseHistory(customer);
+
+        output.printGetWinningNumbersMessage();
+        List<Integer> winningNumbers = input.getWinningNumbers();
+
+        output.printGetBonusNumberMessage();
+        int bonusNumber = input.getBonusNumber(winningNumbers);
+
+        List<Rank> rankResult = customer.checkMyLotto(winningNumbers, bonusNumber);
+        output.printWinningStatistics(rankResult);
     }
 }
