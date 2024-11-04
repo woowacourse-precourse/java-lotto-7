b69e61a6a68d88293a54166e09a74bc7d41a851f
goochul <gooch123@naver.com>
2024-11-04 15:28:33 +0900

    refactor(LottoWinning): 출력 문구 수정

--- src/main/java/lotto/LottoWinning.java
+++ src/main/java/lotto/LottoWinning.java
@@ -54,7 +54,7 @@ public class LottoWinning {
         System.out.printf("4개 일치 (%s원) - %d개\n", WinningAmountEnum.FOUR.getStringValue(), winning4thCount);
         System.out.printf("5개 일치 (%s원) - %d개\n", WinningAmountEnum.THREE.getStringValue(), winning3rdCount);
         System.out.printf("5개 일치, 보너스 볼 일치 (%s원) - %d개\n", WinningAmountEnum.TWO.getStringValue(), winning2ndCount);
-        System.out.printf("3개 일치 (%s원) - %d개\n", WinningAmountEnum.ONE.getStringValue(), winning1stCount);
+        System.out.printf("6개 일치 (%s원) - %d개\n", WinningAmountEnum.ONE.getStringValue(), winning1stCount);
         System.out.print("총 수익률은 " + getYield() + "%입니다" );
     }
 
