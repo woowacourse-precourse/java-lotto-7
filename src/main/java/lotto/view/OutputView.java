f085e037b17a80bb74cc562d81a70f9351d60e6e
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-11-04 10:33:09 +0900

    refactor: 결과 출력 형식 변경
    당첨금, 당첨 기준, 당첨 개수가 변화해도 출력에 영향이 없도록 변경
    (Rank를 값 변경에 유현하게 변화하도록 변경)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -17,18 +17,22 @@ public class OutputView {
     }
 
     // 당첨 통계와 수익률을 포함한 전체 결과 출력
-    public void printResult(Map<Rank,Integer> result, double profitRate){
+    public void printResult(Map<Rank, Integer> result, double profitRate) {
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        System.out.println("3개 일치 (5,000원) - "+ result.get(Rank.FIFTH) +"개");
-        System.out.println("4개 일치 (50,000원) - "+result.get(Rank.FOURTH)+"개");
-        System.out.println("5개 일치 (1,500,000원) - "+result.get(Rank.THIRD)+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+result.get(Rank.SECOND)+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - "+result.get(Rank.FIRST)+"개");
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.NONE) continue;
+            System.out.printf(
+                "%d개 일치%s (%s원) - %d개%n",
+                rank.getMatchCount(),
+                rank.isMatchBonus() ? ", 보너스 볼 일치" : "",
+                rank.getPrizeToString(),
+                result.getOrDefault(rank, 0)
+            );
+        }
 
         System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
-
     }
 
 
