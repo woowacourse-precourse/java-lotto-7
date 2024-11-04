544c4e75ec3bc2f01a14e52da3f856550aa3a387
YujinJuhn <holsya@naver.com>
2024-11-04 16:46:25 +0900

    style: 코드 포매팅 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,81 +12,81 @@ public class InputView {
     private static final int LOTTO_NUMBER_MIN = 1;
     private static final int LOTTO_NUMBER_MAX = 45;
 
-    public int inputPurchaseAmount(){
-        while(true){
-            try{
+    public int inputPurchaseAmount() {
+        while (true) {
+            try {
                 int purchaseAmount = Integer.parseInt(Console.readLine());
                 validatePurchaseAmount(purchaseAmount);
                 return purchaseAmount;
             } catch (NumberFormatException e) {
                 System.out.println(ErrorMessage.INVALID_PURCHASE_TYPE);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private void validatePurchaseAmount(int purchaseAmount){
-        if(purchaseAmount <= 0 || purchaseAmount % LOTTO_PRICE != 0){
+    private void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount <= 0 || purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT);
         }
     }
 
-    public List<Integer> inputWinningNumbers(){
-        while(true){
-            try{
+    public List<Integer> inputWinningNumbers() {
+        while (true) {
+            try {
                 String winningNumbersLine = Console.readLine();
                 return parseNumbers(winningNumbersLine);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private List<Integer> parseNumbers(String winningNumbersLine){
-        try{
+    private List<Integer> parseNumbers(String winningNumbersLine) {
+        try {
             List<Integer> numbers = Stream.of(winningNumbersLine.split(","))
                     .map(String::trim)
                     .map(Integer::parseInt)
                     .toList();
             validateNumbers(numbers);
             return numbers;
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS_TYPE);
         }
     }
 
-    private void validateNumbers(List<Integer> numbers){
-        if(numbers.size() != LOTTO_NUMBER_COUNT){
+    private void validateNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBERS_COUNT);
         }
-        if(numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT){
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBERS);
         }
-        if(numbers.stream().anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)){
+        if (numbers.stream().anyMatch(number -> number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE);
         }
     }
 
-    public int inputBonusNumber(List<Integer> winningNumbers){
-        while(true){
-            try{
+    public int inputBonusNumber(List<Integer> winningNumbers) {
+        while (true) {
+            try {
                 int bonusNumber = Integer.parseInt(Console.readLine());
                 validateBonusNumbers(bonusNumber, winningNumbers);
                 return bonusNumber;
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println(ErrorMessage.INVALID_LOTTO_NUMBERS_TYPE);
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private void validateBonusNumbers(int bonusNumber, List<Integer> winningNumbers){
-        if(bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX){
+    private void validateBonusNumbers(int bonusNumber, List<Integer> winningNumbers) {
+        if (bonusNumber < LOTTO_NUMBER_MIN || bonusNumber > LOTTO_NUMBER_MAX) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_RANGE);
         }
-        if(winningNumbers.contains(bonusNumber)){
+        if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBERS);
         }
     }
