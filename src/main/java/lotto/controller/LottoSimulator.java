b133ec60ba828b0823c895c1441b43cd859597bd
sun <50648835+kimseongyu@users.noreply.github.com>
2024-11-04 17:32:36 +0900

    feat: publish lottos and display it

--- src/main/java/lotto/controller/LottoSimulator.java
+++ src/main/java/lotto/controller/LottoSimulator.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.Result;
 import lotto.model.User;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -15,11 +16,27 @@ public class LottoSimulator {
     public void run() {
         init();
         getCorrectInput(() -> user.setPrice(InputView.inputPurchasePrice()));
+        publishLottos();
+        displayPublishedLottos();
     }
 
     private void init() {
         user = new User();
         lottos = new ArrayList<>();
+    }
+
+    private void publishLottos() {
+        int cnt = user.getPrice() / Lotto.LOTTO_PRICE;
+        for (int i = 0; i < cnt; i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    private void displayPublishedLottos() {
+        List<List<Integer>> lottosNumbers = lottos.stream()
+                .map(Lotto::getNumbers)
+                .toList();
+        OutputView.outputPublishedLotto(lottos.size(), lottosNumbers);
     }
 
     private void getCorrectInput(Runnable function) {
