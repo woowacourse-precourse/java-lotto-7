44225a086e7a794223b587452325ace40b172dcd
mikekks <thdalsrb79@naver.com>
2024-11-04 10:59:10 +0900

    feat(Lotto): 특정 로또 번호가 있는지 확인하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,24 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
 
-    // TODO: 추가 기능 구현
+	public List<Integer> getNumbers(){
+		return numbers;
+	}
+
+	public boolean containNumber(Integer number) {
+		return numbers.contains(number);
+	}
 }
