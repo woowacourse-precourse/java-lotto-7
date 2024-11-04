bb3a335f09ca1d09109edc22d9f93ee59930c2c6
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2024-11-04 17:22:59 +0900

    refactor(WinningNumber) : 당첨 번호 클래스 예외 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -8,13 +8,22 @@ public class WinningNumber {
 
     public WinningNumber(List<Integer> numbers, int bonusNumber) {
         this.numbers = numbers;
+        validateWinningNumber(numbers);
         validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateWinningNumber(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호의 숫자는 1~45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
     private void validateBonusNumber(int bonusNumber) {
         if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
-            throw new IllegalArgumentException("[Error] 로또 번호는 1이상 45이하의 정수여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1이상 45이하의 정수여야 합니다.");
         }
     }
 
