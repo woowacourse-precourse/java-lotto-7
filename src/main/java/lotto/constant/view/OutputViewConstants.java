c9d08ece29a6f73fabf14fc04cb494d4baa3adc0
qyinm <qusseun@gmail.com>
2024-11-04 12:44:04 +0900

    refactor: 복수형으로 class 이름 변경

--- src/main/java/lotto/constant/view/OutputViewConstants.java
+++ src/main/java/lotto/constant/view/OutputViewConstants.java
@@ -1,6 +1,6 @@
 package lotto.constant.view;
 
-public enum OutputViewConstant {
+public enum OutputViewConstants {
     LOTTO_TICKETS_COUNT_FORMAT("%d개를 구매했습니다."),
     INPUT_LOTTO_PRICE_PREFIX("구입금액을 입력해 주세요."),
     INPUT_LOTTO_NUMBERS_PREFIX("당첨 번호를 입력해 주세요."),
@@ -8,14 +8,13 @@ public enum OutputViewConstant {
 
     private final String message;
 
-    OutputViewConstant(String message) {
+    OutputViewConstants(String message) {
         this.message = message;
     }
 
     public String formatMessage(Object... args) {
         return String.format(message, args);
     }
-
 
     @Override
     public String toString() {
