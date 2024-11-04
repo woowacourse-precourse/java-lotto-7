5d0fea394488236f1d4ce99d42378b52d1a6e8a5
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-04 13:54:32 +0900

    feat: 유용성 검사 & 에러시 재입력 기능 추가
    - 입력된 값 유용성 검사
    - 입력시 에러 발생 하면 정상적인 값 받을 때까지 재입력
    - splitWinningNumber(): 기존 requestWinningNumbers()에서 split 로직 함수로 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,51 +3,81 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import lotto.validator.InputValidator;
+import lotto.validator.LottoValidator;
 
 // TODO 유용성 검사 기능 추가
 // 1. 숫자인지 확인하는 유용성 검사 필요
 // 2. amount 1000원 단윈인지 확인 필요 => InputView말고 LottoTransaction 에서 필요
 public class InputView {
-  static private final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
-  static private final String WINNING_NUMBERS_GUIDE = "당첨 번호를 입력해 주세요";
-  static private final String BONUS_NUMBER_GUIDE = "보너스 번호를 입력해 주세요";
+  static private final String PURCHASE_AMOUNT_GUIDE = "\n구입금액을 입력해 주세요.";
+  static private final String WINNING_NUMBERS_GUIDE = "\n당첨 번호를 입력해 주세요";
+  static private final String BONUS_NUMBER_GUIDE = "\n보너스 번호를 입력해 주세요";
+  private static final String COMMA = ",";
+
+  static private void printInputMessage(String message) {
+    System.out.println(message);
+  }
 
   public int requestPurchaseAmount() {
-    printInputMessage(PURCHASE_AMOUNT_GUIDE);
-    String amount = Console.readLine();
+    while (true) {
+      printInputMessage(PURCHASE_AMOUNT_GUIDE);
+      try {
+        String input = Console.readLine();
 
-    return Integer.parseInt(amount);
+        InputValidator.validatePurchaseAmount(input);
+
+        return Integer.parseInt(input);
+
+      } catch (IllegalArgumentException e) {
+        printInputMessage(e.getMessage());
+      }
+    }
   }
 
   // TODO 함수 분할하기 & 하드코딩 제거
   public List<Integer> requestWinningNumbers() {
-    printInputMessage(WINNING_NUMBERS_GUIDE);
+    while (true) {
+      printInputMessage(WINNING_NUMBERS_GUIDE);
+      try {
+        String input = Console.readLine();
+        InputValidator.validateInputWinningNumbers(input);
 
-    List<Integer> numbers = new ArrayList<>();
-    String input = Console.readLine();
+        return splitWinningNumber(input);
 
-    String[] inputNumbers = input.split(",");
-    try {
-      for (String numStr : inputNumbers) {
-
-        int number = Integer.parseInt(numStr.trim()); // 공백 제거 후 정수로 변환
-        numbers.add(number);
-
+      } catch (IllegalArgumentException e) {
+        printInputMessage(e.getMessage());
       }
-    } catch (NumberFormatException e) {
-      System.out.println("[ERROR] 숫자만 입력해주세요. ex)1,2,3,4,5,6");
     }
+  }
+
+  public int requestBonusNumber(List<Integer> numbers) {
+    while (true) {
+      printInputMessage(BONUS_NUMBER_GUIDE);
+      try {
+        String input = Console.readLine();
+        InputValidator.validateInputBonusNumber(input);
+        InputValidator.findBonusOnWinningNumbers(Integer.parseInt(input), numbers);
+
+        return Integer.parseInt(input);
+
+      } catch (IllegalArgumentException e) {
+        printInputMessage(e.getMessage());
+      }
+    }
+  }
+
+
+  private List<Integer> splitWinningNumber(String winningNumber) {
+    List<Integer> numbers = new ArrayList<>();
+
+    String[] inputNumbers = winningNumber.split(COMMA);
+
+    for (String inputNumber : inputNumbers) {
+      numbers.add(Integer.parseInt(inputNumber.trim()));
+    }
+    LottoValidator.validate(numbers);
+
     return numbers;
-  }
-
-  public int requestBonusNumber() {
-    printInputMessage(BONUS_NUMBER_GUIDE);
-    String amount = Console.readLine();
-
-    return Integer.parseInt(amount);
-  }
-
-  static private void printInputMessage(String message) {
-    System.out.println(message);
   }
 }
