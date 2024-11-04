bdf5f0c4916d2f822bd1b62bfbd27cb00ddb7bbd
yoona <103414935+yoonali@users.noreply.github.com>
2024-11-04 19:39:13 +0900

    feat : 로또 당첨 번호 입력 중복 검증 처리 로직

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.global.string.ErrorConstants;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -8,6 +11,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        IsNumberDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -26,6 +30,16 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void IsNumberDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.add(number)) {
+                throw new IllegalArgumentException(ErrorConstants.DUPLICATE_NUMBER_ERROR_MSG + number);
+            }
         }
     }
 }
