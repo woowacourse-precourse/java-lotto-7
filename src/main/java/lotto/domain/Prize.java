44511664dbc6d22902d1ffafaae8bdf6aa48e740
hyosang <billy04100410@gmail.com>
2024-11-04 11:48:16 +0900

    feat(Prize): Prize 도메인 클래스 생성

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+public enum Prize {
+    
+    FIRST(6, false, 2_000_000_000, "6개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    FIFTH(3, false, 5_000, "3개 일치");
+
+    private final int matchCount;
+    private final boolean requiresBonusMatch;
+    private final int prizeAmount;
+    private final String message;
+
+    Prize(int matchCount, boolean requiresBonusMatch, int prizeAmount, String message) {
+        this.matchCount = matchCount;
+        this.requiresBonusMatch = requiresBonusMatch;
+        this.prizeAmount = prizeAmount;
+        this.message = message;
+    }
+
+    public static Prize getPrize(int matchCount, boolean bonusMatch) {
+        for (Prize prize : values()) {
+            if (prize.matchCount == matchCount && (!prize.requiresBonusMatch || bonusMatch)) {
+                return prize;
+            }
+        }
+        return null;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
