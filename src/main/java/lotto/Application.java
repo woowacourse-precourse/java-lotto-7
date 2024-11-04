fb155390c309fab169b5d66a2a3b62737500f806
박규영 <magic9282m@gmail.com>
2024-11-04 22:26:13 +0900

    feat: add new method readAmount
    사용자로부터 로또 구입금액을 입력받는 함수 readAmount 작성
    - 사용자로부터 값을 입력 받은 뒤, int로 형변환을 수행
      - 정수가 아닌 경우, 즉 NumberFormatException이 발생한 경우 IllegalArgumentException을 던진다.
    - 이후 해당 값이 로또 구입금액의 조건에 부합하는지 유효성 검사를 수행한다.
      - 조건을 만족하지 않는 경우, 즉 1000원으로 나눠떨어지지 않는 경우 IllegalArgumentException을 던진다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,26 @@
 package lotto;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Console;
 
+public class Application {
+    static int amountForLottery;
+    public static void main(String[] args) {
+        readAmount();
     }
     public static void validateAmount(int input) {
         if(input <= 0 || input % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위여야 합니다.");
         }
+    }
+    public static void readAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int input;
+        try {
+            input = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 올바른 정수여야 합니다.");
+        }
+        validateAmount(input);
+        amountForLottery = input;
     }
 }
