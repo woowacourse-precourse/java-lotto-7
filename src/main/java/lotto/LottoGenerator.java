fad2dad6b0666ab7a7f84d517d0f1adb1ef48a05
Chaewon <jamjam12@ajou.ac.kr>
2024-11-04 23:32:39 +0900

    fix : 오름차순 배열 오류 해결

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -2,10 +2,10 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
-
 
   public List<Lotto> generateLotto(int purchaseQuantity) {
     List<Lotto> lottoPaper = new ArrayList<>();
@@ -22,9 +22,9 @@ public class LottoGenerator {
   }
 
   public List<Integer> generateOneLotto() {
-
-    return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-
+    List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    Collections.sort(lottoNumbers);
+    return lottoNumbers;
   }
 
 }
