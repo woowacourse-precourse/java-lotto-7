35ad961e2dc9a5041f7676106a7485e6c2409e5c
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 17:45:17 +0900

    feat: LottoGame 클래스의 속성 추가 값

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -5,10 +5,18 @@ import java.util.List;
 public class LottoGame {
     private final UserInputLotto userInputLotto;
     private final LottoGeneration lottoGeneration;
+    private final LottoCheck lottoCheck;
+    private final OutputLottoResult outputLottoResult;
+    private int[] lottoResult;
+    private int totalPrize;
 
     public LottoGame() {
         this.userInputLotto = new UserInputLotto();
         this.lottoGeneration = new LottoGeneration();
+        this.lottoCheck = new LottoCheck();
+        this.outputLottoResult = new OutputLottoResult();
+        this.lottoResult = new int[5];
+        this.totalPrize = 0;
     }
 
     public void strat() {
