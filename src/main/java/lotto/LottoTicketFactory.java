5711921d32656e6dd2f81cdd1258a105da268e16
badgelatte <saucy5939@gmail.com>
2024-11-04 21:21:29 +0900

    금액 입금에 숫자 이외의 것을 입력할 시 예외처리하는 메서드 구현
    금액 입력할 때 숫자 이외의 것을 입력시 예외처리하는 메서드 추가 구현

--- src/main/java/lotto/LottoTicketFactory.java
+++ src/main/java/lotto/LottoTicketFactory.java
@@ -4,11 +4,21 @@ public class LottoTicketFactory {
     private static final int TICKET_PRICE = 1000;
 
     public static int getLottoTicketCount(String money) {
-        int moneyInt = Integer.parseInt(money);
+
+        int moneyInt = isNumeric(money);
 
         if (moneyInt % TICKET_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위의 금액이 아닙니다.");
         }
         return moneyInt / TICKET_PRICE;
+    }
+
+    public static int isNumeric(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자 이외의 입력이 감지되었습니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자 이외의 입력이 감지되었습니다.");
+        }
     }
 }
