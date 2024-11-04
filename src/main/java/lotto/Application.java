47bce14520321009d92bc870a84694c5e9d9a198
Haeun Bae <haeun_bae@tmax.co.kr>
2024-11-04 19:28:06 +0900

    feat: 당첨번호와 보너스 번호 입력
    - 당첨번호와 보너스 번호를 입력받는다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,7 +10,9 @@ public class Application {
         LottoMachine lottoMachine = new LottoMachine();
         lottoMachine.generateLottos(lottoCount);
         // 3. 당첨 번호 입력
+        List<Integer> winningNumbers = InputUtil.insertWinningNumbers();
         // 4. 보너스 번호 입력
+        Integer bonusNumber = InputUtil.insertBonusNumber();
         // 5. 당첨 통계 출력
     }
 }
