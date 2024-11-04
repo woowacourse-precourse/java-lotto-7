8c67d569acfd56b3ea1d11275f431b95f1132d73
seophohoho <seop0937@gmail.com>
2024-11-04 14:36:21 +0900

    feat(Controller): Controller 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.model.BonusNumber;
+import lotto.model.Seller;
+import lotto.model.WinningNumber;
+import lotto.view.Input;
+import lotto.view.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        Seller seller = new Seller();
+        WinningNumber winningNumber = new WinningNumber();
+        BonusNumber bonusNumber = new BonusNumber();
+        
+        Controller controller = new Controller(input, output, seller, winningNumber, bonusNumber);
+
+        controller.run();
     }
 }
--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,2 +1,93 @@
-package lotto.controller;public class Controller {
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.BonusNumber;
+import lotto.model.Buyer;
+import lotto.model.Lotto;
+import lotto.model.Rank;
+import lotto.model.Seller;
+import lotto.model.WinningNumber;
+import lotto.view.Input;
+import lotto.view.Output;
+
+public class Controller {
+    private final Input input;
+    private final Output output;
+    private final Seller seller;
+    private Buyer buyer;
+    private WinningNumber winningNumber;
+    private BonusNumber bonusNumber;
+
+    public Controller(Input input, Output output, Seller seller, WinningNumber winningNumber, BonusNumber bonusNumber) {
+        this.input = input;
+        this.output = output;
+        this.seller = seller;
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void run() {
+        orderLotto();
+        setWinningNumber();
+        setBonusNumber();
+        matchLotto();
+        resultLotto();
+    }
+
+    private void orderLotto() {
+        while (true) {
+            try {
+                String money = input.pay();
+                seller.validate(money);
+                List<Lotto> lottos = seller.createLottoTickets(Integer.parseInt(money));
+                output.printPayResult(lottos);
+                buyer = new Buyer(lottos);
+                break;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setWinningNumber() {
+        while (true) {
+            try {
+                String winningNumberStr = input.winningNumber();
+                winningNumber.validate(winningNumberStr);
+                winningNumber.setNumbers(winningNumberStr);
+                break;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setBonusNumber() {
+        while (true) {
+            try {
+                String bonusNumberStr = input.bonusNumber();
+                bonusNumber.validate(bonusNumberStr);
+                bonusNumber.setNumber(Integer.parseInt(bonusNumberStr));
+                break;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void matchLotto() {
+        buyer.matchLottos(winningNumber.getNumbers(), bonusNumber.getNumber());
+    }
+
+    private void resultLotto() {
+        output.lottoResultTitle();
+
+        output.lottoResultWinning(Rank.FIFTH, buyer.getResultLotto().get(Rank.FIFTH));
+        output.lottoResultWinning(Rank.FOURTH, buyer.getResultLotto().get(Rank.FOURTH));
+        output.lottoResultWinning(Rank.THIRD, buyer.getResultLotto().get(Rank.THIRD));
+        output.lottoResultWinning(Rank.SECOND, buyer.getResultLotto().get(Rank.SECOND));
+        output.lottoResultWinning(Rank.FIRST, buyer.getResultLotto().get(Rank.FIRST));
+
+        output.lottoResultTotalRevenue(buyer.getTotalRevenue());
+    }
 }
