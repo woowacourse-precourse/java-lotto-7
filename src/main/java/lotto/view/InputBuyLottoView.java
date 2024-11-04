dee69adb9e768267d41255b8dd3f9da51ddbbf1d
ParkSangsin <pss6039@naver.com>
2024-11-04 20:01:38 +0900

    feat: 로또 구입 금액 입력을 위한 뷰 추가

--- src/main/java/lotto/view/InputBuyLottoView.java
+++ src/main/java/lotto/view/InputBuyLottoView.java
@@ -1,2 +1,27 @@
-package lotto.view;public class InputBuyLottoView {
+package lotto.view;
+
+import java.util.regex.Pattern;
+import lotto.exception.InvalidInputMoneyFormatException;
+
+public class InputBuyLottoView extends InputView{
+    private static final Pattern PATTERN = Pattern.compile("\\d+");
+    private static final String INPUT_MESSAGE = "구입 금액을 입력해 주세요.";
+
+    public Integer getMoneyValue() {
+        System.out.println(INPUT_MESSAGE);
+        String moneyValue = inputValue();
+        System.out.println();
+        validate(moneyValue);
+        return Integer.parseInt(moneyValue);
+    }
+
+    private void validate(String moneyValue) {
+        validateFormat(moneyValue);
+    }
+
+    private void validateFormat(String moneyValue) {
+        if(!PATTERN.matcher(moneyValue).matches()) {
+            throw new InvalidInputMoneyFormatException();
+        }
+    }
 }
