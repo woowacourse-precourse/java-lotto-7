f48a491571f05cab111b45040163a851c9ac118c
Chanwoo yang <moomsss@naver.com>
2024-11-04 16:37:34 +0900

    refactor: WinningLotto 테스트 주석 추가

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -12,9 +12,9 @@ class WinningLottoTest {
 
     @Test
     void hasBonusNumber() {
-        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)),7);
-        Lotto lottoWithBonusNumber = new Lotto(List.of(1,2,3,4,5,7));
-        Lotto lottoWithoutBonusNumber = new Lotto(List.of(1,2,3,4,5,6));
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)),7); //당첨 로또 번호가 1,2,3,4,5,6 보너스 7
+        Lotto lottoWithBonusNumber = new Lotto(List.of(1,2,3,4,5,7)); //보너스 번호가 있는 로또
+        Lotto lottoWithoutBonusNumber = new Lotto(List.of(1,2,3,4,5,6)); //보너스 번호가 없는 로또
 
         assertThat(winningLotto.hasBonusNumber(lottoWithBonusNumber.getNumbers())).isEqualTo(true);
         assertThat(winningLotto.hasBonusNumber(lottoWithoutBonusNumber.getNumbers())).isEqualTo(false);
