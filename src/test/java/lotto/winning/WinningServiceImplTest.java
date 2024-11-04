5475ac052da9cde91d11936764470e0781d1b91b
woong2e <tlswodnd12345@naver.com>
2024-11-04 20:36:09 +0900

    test: add test for store statistics

--- src/test/java/lotto/winning/WinningServiceImplTest.java
+++ src/test/java/lotto/winning/WinningServiceImplTest.java
@@ -1,14 +1,21 @@
 package lotto.winning;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.as;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+
 import lotto.AppConfig;
 import lotto.donghang.WinningLotto;
 import lotto.vendingmachine.Lotto;
 import lotto.vendingmachine.VendingMachineRepository;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -68,6 +75,30 @@ class WinningServiceImplTest {
         assertEquals(Integer.valueOf(0), result.get(Rank.THIRD));
         assertEquals(Integer.valueOf(0), result.get(Rank.FOURTH));
         assertEquals(Integer.valueOf(1), result.get(Rank.FIFTH));
+    }
+
+    @Test
+    @DisplayName("통계 저장 테스트")
+    void 통계_저장_테스트() {
+        Map<Rank, Integer> result = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            if (rank != Rank.LOSING_TICKET) {
+                result.put(rank, 0);
+            }
+        }
+        vendingMachineRepository.save(List.of(new Lotto(List.of(1,2,3,4,5,6))
+                , new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(1,2,3,4,5,6))));
+        result.put(Rank.FIRST, result.get(Rank.FIRST) + 3);
+        winningService.saveStatistics(result);
+
+        Map<Rank, Integer> winningStatistics = vendingMachineRepository.getWinningStatistics();
+        assertThat(winningStatistics)
+                .extractingByKey(Rank.FIRST, as(InstanceOfAssertFactories.INTEGER))
+                .isEqualTo(3);
+
+        assertThat(winningStatistics)
+                .extractingByKey(Rank.SECOND, as(InstanceOfAssertFactories.INTEGER))
+                .isEqualTo(0);
     }
 
 
