47133b4b486480f377eb632ec373d547e8d12f17
wvssm <neoramiregi@gmail.com>
2024-11-04 10:32:15 +0900

    feat: 당첨 번호가 서로 중복되면 예외가 발생한다

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class WinningNumbers {
     private Lotto winningNumbers;
+
+    private WinningNumbers(Lotto winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public static WinningNumbers from(List<Integer> numbers){
+        Lotto lotto = new Lotto(numbers);
+        return new WinningNumbers(lotto);
+    }
 }
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -3,8 +3,9 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.Arrays;
 
+import static lotto.exception.ErrorMessages.DUPLICATE_LOTTO_NUMBER_ERROR;
 import static lotto.exception.ErrorMessages.INVALID_LOTTO_NUMBER_RANGE_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,8 +13,16 @@ class WinningNumbersTest {
     @DisplayName("당첨 번호가 1 이상 45 이하가 아니면 예외가 발생한다.")
     @Test
     void 당첨_번호가_1이상_45이하가_아니면_예외가_발생한다(){
-        assertThatThrownBy(() -> new Lotto(List.of(46,47,48,49,50,51)))
+        assertThatThrownBy(() -> WinningNumbers.from(Arrays.asList(46,45,1,4,3,5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_LOTTO_NUMBER_RANGE_ERROR);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> WinningNumbers.from(Arrays.asList(1,2,3,4,5,5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATE_LOTTO_NUMBER_ERROR);
     }
 }
