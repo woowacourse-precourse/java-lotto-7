26c61326a441192d96bb65123993e68d14dc7e9d
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-11-04 17:28:59 +0900

    docs: 구매한 금액으로 몇 장이 나오는지 계산하는 로직

--- src/main/java/InputOutput/Output.java
+++ src/main/java/InputOutput/Output.java
@@ -1,10 +1,10 @@
 package InputOutput;
 
 public class Output {
-    public static void 첫_번째_출력메세지(){
+    public static void firstMessage(){
         System.out.println("구입금액을 입력해 주세요.");
     }
-    public static void 계산된_금액을_통해_몇_장이_나오는_계산(int price){
+    public static void calculateNumberOfSheetsFromAmount(int price){
         if (price < 1000) {
             throw new IllegalArgumentException("[ERROR] 최소 금액은 1,000원 이상이어야 합니다.");
         }
