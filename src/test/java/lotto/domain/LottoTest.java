18efbf161d6e13a3fa537cbd2824961dd7248a2e
JoonKyo <joonkyolee@gmail.com>
2024-11-04 16:09:19 +0900

    test(LottoTest): 로또 숫자 범위를 벗어난 수를 입력했을 때 예외 처리를 진행하는지 확인하는 테스트 구현

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -20,5 +20,9 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_숫자_범위가_아닌_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
