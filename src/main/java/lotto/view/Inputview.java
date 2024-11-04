32656dec93bda2972fa53357b74b3ba126bd966a
YunCheol1 <naro2727@naver.com>
2024-11-04 15:51:50 +0900

    fix(Inputview): 잘못된 입력에 대한 예외처리 로직 수정

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -1,15 +1,20 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.util.Config;
 
 public class Inputview {
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException();
+        try {
+            int money = Integer.parseInt(Console.readLine());
+            if (money % 1000 != 0) {
+                throw new IllegalArgumentException(Config.ERROR_INVALID_TICKET_COUNT);
+            }
+            return money;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Config.ERROR_INVALID_MONEY_FORMAT);
         }
-        return money;
     }
 
     public static String inputWinningNumbers() {
