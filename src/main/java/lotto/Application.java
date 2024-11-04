bcea380547031b7e3eaef12d3a7576be26477b46
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 15:24:53 +0900

    feat: 최초 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.repository.LottoRepository;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = InputView.getInstance();
+        OutputView outputView = OutputView.getInstance();
+        LottoRepository lottoRepository = new LottoRepository();
+        LottoService lottoService = new LottoService(lottoRepository);
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+
+        lottoController.run();
+
     }
 }
