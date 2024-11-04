cddeb7b711708871324e52a1e82d8fc97028e5c0
SeongJunChun <csj991210@naver.com>
2024-11-04 19:30:18 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,8 +1,6 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
