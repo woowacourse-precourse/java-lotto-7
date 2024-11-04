b8b4ec33ede484305842a2779d270d75585c566d
choitaehoon <hugebigdream@gmail.com>
2024-11-04 18:00:03 +0900

    refactor : OutputView 클래스 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,10 +10,10 @@ public class Application {
         int lottoPurchasePrice = InputView.getValidPurchasePrice();
         int lottoPurchaseQuantity = calculateLottoQuantity(lottoPurchasePrice);
 
-        printPurchaseInfo(lottoPurchaseQuantity);
+        OutputView.printPurchaseInfo(lottoPurchaseQuantity);
 
         List<Lotto> lottos = createLottos(lottoPurchaseQuantity);
-        printLottos(lottos);
+        OutputView.printLottos(lottos);
 
         List<Integer> lottoWinningNumbers = InputView.getValidWinningNumbers();
         int bonusNumber = InputView.getValidBonusNumber(lottoWinningNumbers);
@@ -21,13 +21,9 @@ public class Application {
         WinningLotto winningLotto = new WinningLotto(lottoWinningNumbers, bonusNumber);
         LottoResult result = matchLottos(lottos, winningLotto);
 
-        printResult(result, lottoPurchasePrice);
+        OutputView.printResults(result, lottoPurchasePrice);
     }
 
-
-    private static void printPurchaseInfo(int quantity) {
-        System.out.println(quantity + "개를 구매했습니다.");
-    }
 
     private static List<Lotto> createLottos(int quantity) {
         List<Lotto> lottos = new ArrayList<>();
@@ -41,25 +37,11 @@ public class Application {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream().sorted().collect(Collectors.toList());
     }
 
-    private static void printLottos(List<Lotto> lottos) {
-        lottos.forEach(Lotto::printNumbers);
-    }
 
     private static LottoResult matchLottos(List<Lotto> lottos, WinningLotto winningLotto) {
         LottoResult result = new LottoResult();
         lottos.forEach(lotto -> result.addResult(winningLotto.match(lotto)));
         return result;
-    }
-
-    private static void printResult(LottoResult result, int purchaseAmount) {
-        System.out.println("\n당첨 통계");
-        System.out.println("---");
-        for (Rank rank : Rank.values()) {
-            if (rank != Rank.NONE) {
-                System.out.printf("%s - %d개\n", rank.getDescription(), result.getCount(rank));
-            }
-        }
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", result.calculateProfitRate(purchaseAmount));
     }
 
     public static int calculateLottoQuantity(int purchasePrice) {
---
+++ src/main/java/lotto/OutputView.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.List;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printPurchaseInfo(int quantity) {
+        System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.forEach(Lotto::printNumbers);
+    }
+
+    public static void printResults(LottoResult result, int purchaseAmount) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            if (rank != Rank.NONE) {
+                System.out.printf("%s - %d개\n", rank.getDescription(), result.getCount(rank));
+            }
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", result.calculateProfitRate(purchaseAmount));
+    }
+}
