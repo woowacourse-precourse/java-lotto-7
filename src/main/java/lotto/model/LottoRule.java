531c416924e54ae6cfb95a5d6f4fd03ffa52c0d5
JEN <ekffuclxk@gmail.com>
2024-11-04 15:20:45 +0900

    chore(LottoRule): 상수 정보 오타 수정

--- src/main/java/lotto/model/LottoRule.java
+++ src/main/java/lotto/model/LottoRule.java
@@ -3,11 +3,10 @@ package lotto.model;
 public enum LottoRule {
 
     SIX_MATCH(6, "6개 일치 (2,000,000,000원)", 2_000_000_000),
-    FIVE_MATCH_BONUS(5, "5개 일치, 보너스 일치 (30,000,000원)", 30_000_000, true),
+    FIVE_MATCH_BONUS(5, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000, true),
     FIVE_MATCH(5, "5개 일치 (1,500,000원)", 1_500_000),
     FOUE_MATCH(4, "4개 일치 (50,000원)", 50_000),
-    THREE_MATCH(3, "3개 일치 (5,000원)", 5_000),
-    NO_MATCH(0, "일치 없음", 0);
+    THREE_MATCH(3, "3개 일치 (5,000원)", 5_000);
 
     private final int matchCount;
     private final String info;
@@ -47,6 +46,6 @@ public enum LottoRule {
                 return value;
             }
         }
-        return NO_MATCH;
+        return null;
     }
 }
