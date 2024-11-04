b5e5660bac5f5764fbc4353d76a8293ed344d207
onesay17 <onesay17@gmail.com>
2024-11-04 23:08:56 +0900

    refactor: 상수적용

--- src/main/java/lotto/validation/Validator.java
+++ src/main/java/lotto/validation/Validator.java
@@ -12,27 +12,33 @@ import lotto.util.MessageSource;
 public class Validator {
 
     public void validatePurchaseAmount(Integer purchaseAmount) {
+
         if (purchaseAmount % LOTTO_PRICE_UNIT != 0) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.invalid_Amount_Unit"));
         }
+
         if (purchaseAmount > MAX_PURCHASE_AMOUNT) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.exceeds_max_amount"));
         }
     }
 
     public void validateWinningNumbers(List<Integer> winningNumbers) {
+
         if (winningNumbers.size() != WINNING_NUMBER_COUNT) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.invalid_winning_number_count"));
         }
-        if (winningNumbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+
+        if (winningNumbers.stream().anyMatch(num -> num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER)) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.invalid_number_range"));
         }
     }
 
-    public void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+    public void validateBonusNumber(Integer bonusNumber, List<Integer> winningNumbers) {
+
         if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.invalid_number_range"));
         }
+
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(MessageSource.getMessage("error.duplicate_bonus_number"));
         }
--- src/test/java/lotto/validation/ValidatorTest.java
+++ src/test/java/lotto/validation/ValidatorTest.java
@@ -1,8 +1,11 @@
 package lotto.validation;
 
+import static lotto.util.Constant.MAX_PURCHASE_AMOUNT;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.List;
+import lotto.util.MessageSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,10 +21,20 @@ class ValidatorTest {
 
     @Test
     @DisplayName("구입 금액이 1000원 단위가 아닐 경우 예외가 발생해야 한다.")
-    void 구입_금액_검증() {
+    void 구입_금액_단위_검증() {
         assertThatThrownBy(() -> validator.validatePurchaseAmount(1200))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+                .hasMessageContaining(MessageSource.getMessage("error.invalid_Amount_Unit"));
+    }
+
+    @Test
+    @DisplayName("구매 금액이 최대 금액을 초과하면 예외가 발생해야 한다.")
+    void 구매_금액_초과_검증() {
+        int purchaseAmount = MAX_PURCHASE_AMOUNT + 1000;
+
+        assertThatThrownBy(() -> validator.validatePurchaseAmount(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageSource.getMessage("error.exceeds_max_amount"));
     }
 
     @Test
@@ -29,7 +42,17 @@ class ValidatorTest {
     void 당첨_번호_갯수_검증() {
         assertThatThrownBy(() -> validator.validateWinningNumbers(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 정확히 6개여야 합니다.");
+                .hasMessageContaining(MessageSource.getMessage("error.invalid_winning_number_count"));
+    }
+
+    @Test
+    @DisplayName("당첨 번호에 범위를 벗어난 숫자가 포함되면 예외가 발생해야 한다.")
+    void 당첨_번호_범위_검증() {
+        List<Integer> winningNumbers = Arrays.asList(3, 12, 25, 41, 48, 7);
+
+        assertThatThrownBy(() -> validator.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageSource.getMessage("error.invalid_number_range"));
     }
 
     @Test
@@ -37,6 +60,17 @@ class ValidatorTest {
     void 보너스_번호_중복_검증() {
         assertThatThrownBy(() -> validator.validateBonusNumber(1, List.of(1, 2, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+                .hasMessageContaining(MessageSource.getMessage("error.duplicate_bonus_number"));
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 범위를 벗어나면 예외가 발생해야 한다.")
+    void 보너스_번호_범위_검증() {
+        List<Integer> winningNumbers = Arrays.asList(3, 12, 25, 41, 7, 9);
+        int bonusNumber = 50;
+
+        assertThatThrownBy(() -> validator.validateBonusNumber(bonusNumber, winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MessageSource.getMessage("error.invalid_number_range"));
     }
 }
