164c73e3d1486c6ad5ce9178cfe99d35b1ebffeb
youngkwanglim <richard08@naver.com>
2024-11-04 21:40:26 +0900

    test: 당첨 번호가 6개가 아닌 경우 예외 처리 테스트

--- src/test/java/lotto/LottoValidatorTest.java
+++ src/test/java/lotto/LottoValidatorTest.java
@@ -2,12 +2,19 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoValidatorTest {
     @Test
     void 구입_금액이_1000원_단위가_아닌_경우_예외가_발생한다() {
         assertThatThrownBy(() -> LottoValidator.validatePurchaseAmount(1500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호가_6개가_아닌_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> LottoValidator.validateWinningNumbers(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
