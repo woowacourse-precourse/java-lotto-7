3a6e1c0765e48aab8fe467930dd9f4f0c03e8bca
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 09:30:07 +0900

    refactor(WinningNumber): 당첨 번호 쉼표 구분 미입력 예외 처리 로직 분리
    - validateWinningNumber 메서드에서 입력된 당첨 번호를 List<String>으로
    바꾸는 부분을 createStringList 메서드로 분리
    - validateWinningNumber 메서드에서 사용되는 조건을 hasInvalidDelimiter 메서드로 분리
    - validateWinningNumber 메서드에서 List<String>을 List<Integer>으로
      바꾸는 부분을 createIntegerList 메서드로 분리

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -12,14 +12,25 @@ public class WinningNumber {
     }
 
     private Lotto validateWinningNumber(String inputWinningNumber) {
-        List<String> winningNumber = Arrays.stream(inputWinningNumber.split(",")).toList();
-        if (winningNumber.stream().anyMatch(s -> s.matches(NON_DIGIT_PATTERN))) {
+        List<String> winningNumber = createStringList(inputWinningNumber);
+        if (hasInvalidDelimiter(winningNumber)) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분한 숫자들이어야 합니다.");
         }
-        List<Integer> lotto = winningNumber.stream()
+        return new Lotto(createIntegerList(winningNumber));
+    }
+
+    private boolean hasInvalidDelimiter(List<String> winningNumber) {
+        return winningNumber.stream().anyMatch(s -> s.matches(NON_DIGIT_PATTERN));
+    }
+
+    private List<String> createStringList(String inputWinningNumber) {
+        return Arrays.stream(inputWinningNumber.split(",")).toList();
+    }
+
+    private List<Integer> createIntegerList(List<String> winningNumber){
+        return winningNumber.stream()
                 .map(LottoNumber::new)
                 .map(LottoNumber::value)
                 .toList();
-        return new Lotto(lotto);
     }
 }
