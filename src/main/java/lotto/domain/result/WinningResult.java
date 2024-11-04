0ce1faedb450b80728809af9891a660e336b4ec3
hootzz <wjqwjql3@gmail.com>
2024-11-04 14:36:54 +0900

    feat(result): 최종 결과 관리 WinningResult class 구현

--- src/main/java/lotto/domain/result/WinningResult.java
+++ src/main/java/lotto/domain/result/WinningResult.java
@@ -1,4 +1,27 @@
 package lotto.domain.result;
 
 public class WinningResult {
+    private final WinningStatistics statistics;
+    private final double profitRate;
+
+    private WinningResult(WinningStatistics statistics, double profitRate) {
+        this.statistics = statistics;
+        this.profitRate = profitRate;
+    }
+
+    public static WinningResult of(WinningStatistics statistics, double profitRate) {
+        return new WinningResult(statistics, profitRate);
+    }
+
+    public WinningStatistics getStatistics() {
+        return statistics;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+
+    public int getWinningCount(LottoRank rank) {
+        return statistics.getWinningCount(rank);
+    }
 }
