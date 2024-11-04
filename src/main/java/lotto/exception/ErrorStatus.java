84734553d88e853f7aed63338d5d2c4e827dcff1
thsghdud13 <thsgdud25@naver.com>
2024-11-04 21:39:50 +0900

    feat: 당첨번호 저장 객체

--- src/main/java/lotto/exception/ErrorStatus.java
+++ src/main/java/lotto/exception/ErrorStatus.java
@@ -8,6 +8,9 @@ public enum ErrorStatus implements BaseErrorCode {
     LOTTO_OUT_RANGE_ERROR("로또 숫자의 범위가 1~45사이를 벗어납니다."),
     LOTTO_SIZE_ERROR("로또 숫자들의 개수가 여섯개가 아닙니다."),
     LOTTO_UNIQUE_ERROR("로또 숫자들이 서로 겹치지 않아야 합니다."),
+    //당첨 번호 관련 에러
+    BONUS_NUMBER_UNIQUE_ERROR("보너스 넘버가 겹치지 않아야 합니다."),
+    BONUS_NUMBER_OUT_RANGE_ERROR("로또 숫자들이 서로 겹치지 않아야 합니다."),
     //돈 관련 에러
     MONEY_NON_POSITIVE_ERROR("돈은 0 이상이여야 합니다."),
     MONEY_MIN_UNIT_ERROR("돈은 1000원 단위여야 합니다.");
