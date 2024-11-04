98ac80bfc843c182253abbaa79aaa99d1b1d2339
AndBlack99 <jwoo1687@icloud.com>
2024-11-04 21:57:18 +0900

    feat:로또발행 기능 추가
      - generateLotto함수
      - generateLottos함수
      - printLottoNumbers함수
      - printSortedLotto함수
        : 랜덤숫자를 받아 구매한 로또 갯수에 맞춰 로또를 발행하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,22 +1,27 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.Collections;
 
 public class Application {
 	private static final int LOTTO_PRICE = 1000;
+	private static final int MAX_NUMBER = 45;
 	
     public static void main(String[] args) {
     	while(true)
 	    	try {
 		    	int amount = getPurchaseAmount();
+		    	int lottoCount = amount / LOTTO_PRICE;
+	            Lotto[] purchasedLottos = generateLottos(lottoCount);
+	            printLottoNumbers(purchasedLottos);
 		        break;
 	    	} catch (IllegalArgumentException e) {
 	    		System.out.println("[ERROR] " + e.getMessage());
 	    	}
     }
-    
-    
-    
     
     public static int getPurchaseAmount() {
 		while (true) {
@@ -35,6 +40,31 @@ public class Application {
 	public static void validateAmount(int amount) {
 		if (amount <= 0 || amount % LOTTO_PRICE != 0) 
 			throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위 입니다.");
+	}
+	
+	public static Lotto[] generateLottos (int count) {
+		Lotto[] lottos = new Lotto[count];
+		for(int i=0; i<count; i++) {
+			lottos[i] = generateLotto();
+		}
+		return lottos;
+	}
+	
+	public static Lotto generateLotto() {
+		List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, MAX_NUMBER, 6);
+		return new Lotto(lottoNumbers);
+	}
+	
+	public static void printLottoNumbers(Lotto[] purchasedLottos) {
+		System.out.println(purchasedLottos.length + "개를 구매했습니다.");
+		for(Lotto lotto : purchasedLottos) 
+			printSortedLotto(lotto);  
+	}
+	
+	public static void printSortedLotto(Lotto lotto) {
+		List<Integer> sortedLotto = lotto.getNumbers();
+		Collections.sort(sortedLotto);
+		System.out.println(sortedLotto);
 	}
     
     
