fd78ed2191ffdef55304a644afed9d72c4a95b35
iamdonghyuk <iamdonghyuk@gmail.com>
2024-11-04 20:01:12 +0900

    test: 보너스 테스트 결과 성공

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -60,21 +60,40 @@ class ApplicationTest extends NsTest {
     void 구매값_입력_에러() {
         assertThatThrownBy(() -> runException("1000j"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE); // 이 부분에 정확한 메시지를 넣어야 합니다.
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
     void 나눌수없는값_입력_에러() {
         assertThatThrownBy(() -> runException("1050"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE); // 이 부분에 정확한 메시지를 넣어야 합니다.
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
+    @Test
+    void 보너스_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "1개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 3000000.0%입니다."
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 7)
+        );
+    }
     @Test
     void 당첨_번호_6개가_아닌_경우() {
         assertThatThrownBy(() -> runException("1000","1,2,3,4,5,6,7"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ERROR_MESSAGE); // 이 부분에 정확한 메시지를 넣어야 합니다.
+                .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Override
