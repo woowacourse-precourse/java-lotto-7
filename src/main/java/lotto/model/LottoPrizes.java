c1cd61358a5a5f43301c8f546579e14f082c62f0
Goohong Chung <9rrong@uoslife.team>
2024-11-04 19:45:03 +0900

    refactor(LottoPrizes): LottoPrizeCalculator로 당첨 통계 로직 추출

--- src/main/java/lotto/model/LottoPrizes.java
+++ src/main/java/lotto/model/LottoPrizes.java
@@ -1,70 +1,28 @@
 package lotto.model;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoPrizes {
-    private static final int YIELD_SCALE = 1;
-    private static final int DIVISION_SCALE = 3;
-    private static final BigDecimal HUNDRED = BigDecimal.valueOf(100);
-    private static final String PRIZE_STATISTICS_FORMAT = "%s%d개";
-
     private final List<LottoPrize> lottoPrizes;
-    private final WinningNumbers winningNumbers;
 
     public LottoPrizes(Lottos lottos, WinningNumbers winningNumbers) {
-        this.winningNumbers = winningNumbers;
-        this.lottoPrizes = getPrizes(lottos);
+        this.lottoPrizes = generatePrizes(lottos, winningNumbers);
     }
 
-    private List<LottoPrize> getPrizes(Lottos lottos) {
-        return lottos
-                .getLottos()
-                .stream()
-                .map(this::getPrize)
-                .toList();
+    private List<LottoPrize> generatePrizes(Lottos lottos, WinningNumbers winningNumbers) {
+        return lottos.getLottos().stream()
+                .map(lotto -> calculatePrize(lotto, winningNumbers))
+                .collect(Collectors.toList());
     }
 
-    private LottoPrize getPrize(Lotto lotto) {
+    private LottoPrize calculatePrize(Lotto lotto, WinningNumbers winningNumbers) {
         int matchCount = winningNumbers.countMatchingNumbers(lotto);
         boolean containsBonusNumber = winningNumbers.containsNumber(lotto);
         return LottoPrize.fromMatchCountAndBonus(matchCount, containsBonusNumber);
     }
 
-    public String calculateYield(int purchaseAmount) {
-        int totalPrizeAmount = lottoPrizes.stream()
-                .mapToInt(LottoPrize::getPrizeAmount)
-                .sum();
-
-        BigDecimal yield = calculateYield(totalPrizeAmount, purchaseAmount);
-        return yield.toString();
-    }
-
-    private BigDecimal calculateYield(int totalPrizeAmount, int lottoBudget) {
-        return BigDecimal.valueOf(totalPrizeAmount)
-                .divide(BigDecimal.valueOf(lottoBudget), DIVISION_SCALE, RoundingMode.HALF_UP)
-                .multiply(HUNDRED)
-                .setScale(YIELD_SCALE, RoundingMode.HALF_UP);
-    }
-
-    public List<String> calculateMatchStatistics() {
-        return Arrays.stream(LottoPrize.values())
-                .filter(prize -> prize != LottoPrize.NO_PRIZE)
-                .map(this::formatPrizeStatistics)
-                .collect(Collectors.toList());
-    }
-
-    private String formatPrizeStatistics(LottoPrize prize) {
-        long prizeCount = countPrizes(prize);
-        return String.format(PRIZE_STATISTICS_FORMAT, prize, prizeCount);
-    }
-
-    private long countPrizes(LottoPrize prize) {
-        return lottoPrizes.stream()
-                .filter(lottoPrize -> lottoPrize == prize)
-                .count();
+    public List<LottoPrize> getLottoPrizes() {
+        return lottoPrizes;
     }
 }
