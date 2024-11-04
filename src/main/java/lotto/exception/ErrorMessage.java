710e04d17de09ff06e155f13c7e7f4c4b01b4074
wogus <ind07152@naver.com>
2024-11-04 17:02:32 +0900

    feat(ErrorMessage): 로또 구매 개수 관련 예외 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -9,6 +9,7 @@ public enum ErrorMessage {
     INVALID_PURCHASE_TYPE("구매 금액은 숫자로만 입력해주세요."),
     INVALID_PURCHASE_AMOUNT_UNIT("구매 금액은 1000원 단위만 가능합니다."),
     INVALID_PURCHASE_MIN("구매 금액은 1000원 이상이여야합니다."),
+    INVALID_PURCHASE_COUNT("로또는 1개 이상 구매해야합니다."),
     // 보너스 숫자 예외 메시지
     INVALID_BONUS_NUMBER_RANGE("보너스 숫자는 1에서 45사이의 숫자여야합니다."),
     INVALID_BONUS_NUMBER_TYPE("보너스 숫자는 숫자만 입력 가능합니다."),
