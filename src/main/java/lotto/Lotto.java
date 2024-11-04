3e56c3dffcb45cfa2d94ff6d225391328eab5a6c
siyoung <wfs0502@gmail.com>
2024-11-04 15:18:05 +0900

    refactor: 상수 LottoConstants 클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,12 +2,12 @@ package lotto;
 
 import java.util.List;
 
+import static lotto.LottoConstants.*;
+
 public class Lotto {
     private static final String COUNT_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
     private static final String RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String DUPLICATE_ERROR = "[ERROR] 로또 번호는 중복될 수 없습니다.";
-    private static final int MAX_NUMBER = 45;
-    private static final int MIN_NUMBER = 1;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -16,11 +16,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != NUMBER_COUNT) {
             throw new IllegalArgumentException(COUNT_ERROR);
         }
 
-        if (numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
+        if (numbers.stream()
+                .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
             throw new IllegalArgumentException(RANGE_ERROR);
         }
 
