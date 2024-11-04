8184e2b9956f213934013167088c9292e044d129
hmlssghm <kwonse326@gmail.com>
2024-11-04 16:05:49 +0900

    refactor(Ticket): 매직넘버 상수화

--- src/main/java/lotto/vo/Ticket.java
+++ src/main/java/lotto/vo/Ticket.java
@@ -1,13 +1,22 @@
 package lotto.vo;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class Ticket {
+    private static final int RANGE_START = 1;
+    private static final int RANGE_END = 45;
+    private static final int WINNING_NUMBER_COUNT = 6;
+
     private final List<Integer> ticket;
 
-    public Ticket(List<Integer> ticket) {
-        this.ticket = ticket;
+    public Ticket() {
+        this.ticket = pickRandom();
     }
 
     // [8, 21, 23, 41, 42, 43]
+    private List<Integer> pickRandom() {
+        return Randoms.pickUniqueNumbersInRange(RANGE_START, RANGE_END, WINNING_NUMBER_COUNT);
+    }
 }
