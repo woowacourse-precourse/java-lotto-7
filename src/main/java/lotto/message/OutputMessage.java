e95cfa29929cd317fc6a12d11016a1e12ff62480
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 15:36:21 +0900

    feat(LottoNumberGenerator): 로또 번호를 생성하는 기능 구현
    - 로또 구입 금액에 따른 횟수 만큼 로또 번호를 생성하고 이를 출력하는 기능 구현함.

--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -1,7 +1,8 @@
 package lotto.message;
 
 public enum OutputMessage {
-    REQUEST_INPUT_AMOUNT("구입금액을 입력해 주세요.");
+    REQUEST_INPUT_AMOUNT("구입금액을 입력해 주세요."),
+    PRINT_LOTTO_NUMBER("개를 구매했습니다.");
 
     private String message;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,22 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.message.OutputMessage;
 
 public class OutputView {
     public static void printErrorMessage(String errorMessage){
         System.out.println(errorMessage);
     }
+
     public static void printLottoAmountInput(){
         System.out.println(OutputMessage.REQUEST_INPUT_AMOUNT);
+    }
+
+    public static void printLottoNumberOutput(int lottoAmount, List<Lotto> lottos) {
+        System.out.println(lottoAmount + OutputMessage.PRINT_LOTTO_NUMBER.getMessage());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
