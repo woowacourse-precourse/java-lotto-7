00480c5e3cd4d20483baa581c55e62165d950510
iceprins <junghankyul@gmail.com>
2024-11-04 23:58:11 +0900

    refactor: 오작동 부분 수정

--- src/main/java/lotto/view/constant/InputConstant.java
+++ src/main/java/lotto/view/constant/InputConstant.java
@@ -2,6 +2,6 @@ package lotto.view.constant;
 
 public class InputConstant {
     public static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String ENTER_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    public static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String ENTER_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    public static final String ENTER_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 }
