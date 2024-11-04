a1d7b38cb4e5b65da76d0d438042e152305a1e74
gyuoo <yeomky98@naver.com>
2024-11-05 00:03:25 +0900

    feat(util): add method to sort lotto numbers in ascending order

---
+++ src/main/java/lotto/util/LottoNumberSorter.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberSorter {
+	public List<Integer> sortInAscendingOrder(List<Integer> numbers) {
+		Collections.sort(numbers);
+		return numbers;
+	}
+}
