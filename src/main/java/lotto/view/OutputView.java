98c0fb895a0b87f9cf3961521bd913ffd50b5eb2
JMJ <ghtntkdnsk@naver.com>
2024-11-04 22:20:56 +0900

    feat(OutputView) : 당첨 통계 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.Rank;
 
 public class OutputView {
 
@@ -14,5 +16,14 @@ public class OutputView {
             System.out.println(purchasedLotto.getNumbers());
         }
         System.out.println();
+    }
+
+    public void printWinningStatistics(Map<Rank, Integer> rankResult, String profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            System.out.println(rank.getMatchDescription() + " - " + rankResult.get(rank) + "개");
+        }
+        System.out.println("총 수익률은" + profitRate + "입니다.");
     }
 }
