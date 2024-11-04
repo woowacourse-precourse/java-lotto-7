ba3f8f5c305feb171bf400f25a5283232e0db47e
acecic82 <102938al@naver.com>
2024-11-04 18:52:05 +0900

    fix(Application): Application class 파일 수정
    수정된 내용
    
    - Machine 을 추가해서 사용자의 입력을 받고 결과를 보여준다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,11 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Machine machine = new Machine();
+        try {
+            machine.init();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
