e2f942568bcae6a3a0f7a9a9becfdd799f7a09e8
jieunpark <jieun20626@gmail.com>
2024-11-04 20:15:05 +0900

    feat: 중복된 당첨 번호 입력 시 에러

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -1,5 +1,23 @@
 package lotto;
 
-class InputTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+
+class InputTest extends NsTest {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void duplicate_winning_numbers() {
+        runException("1000", "1,2,3,4,5,5");
+        assertThat(output()).contains(ERROR_MESSAGE);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
 }
