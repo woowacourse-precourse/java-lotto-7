17c249ebd0b85dbcd4dae16a651b941e10d53d29
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-11-04 20:57:18 +0900

    test: test 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -53,6 +54,28 @@ class LottoTest {
         assertThrows(IllegalArgumentException.class, () -> Validator.validateNumberCount(numbers));
     }
 
+    @DisplayName("로또 번호에 정수 이외의 값은 올 수 없다.")
+    @Test
+    void 로또_번호의_입력_검증() {
+        String numbers = "1,2,a,4,5";
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateCommaSeparated(numbers));
+
+    }
+
+    @DisplayName("입력 값은 쉼표로 구분되어야 한다.")
+    @Test
+    void 쉼표로_구분되지않은_입력_검증() {
+        String numbers = "1 2 3 4 5 6";
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateCommaSeparated(numbers));
+    }
+
+    @DisplayName("빈 문자열은 올 수 없다.")
+    @Test
+    void 빈_문자열_검증() {
+        String numbers = "";
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateCommaSeparated(numbers));
+    }
+
     @DisplayName("로또 숫자의 범위가 잘못된 경우 예외 처리한다.")
     @ParameterizedTest
     @MethodSource("lottoNumberRangeTest")
@@ -62,5 +85,40 @@ class LottoTest {
                 Validator.validateRange(number);
             }
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 정수가 아니라면 예외 처리한다.")
+    void 보너스_번호_정수_외의값() {
+        String bonus = "a";
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateBonusNumber(bonus));
+    }
+
+    @Test
+    @DisplayName("구입 금액의 정상 입력")
+    void 구입금액_정상입력() {
+        int validMoney = 1000;
+        assertDoesNotThrow(() -> Validator.validateMoneyUnit(validMoney));
+    }
+
+    @Test
+    @DisplayName("구입 금액의 음수 입력")
+    void 구입금액_음수입력() {
+        int invalidMoney = -1000;
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateMoneyUnit(invalidMoney));
+    }
+
+    @Test
+    @DisplayName("구입 금액의 1,000원의 배수가 아닌 입력")
+    void 구입금액_1000원_배수입력() {
+        int invalidMoney = 1500;
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateMoneyUnit(invalidMoney));
+    }
+
+    @Test
+    @DisplayName("구입 금액의 0원 입력")
+    void 구입금액_0원입력() {
+        int invalidMoney = 0;
+        assertThrows(IllegalArgumentException.class, () -> Validator.validateMoneyUnit(invalidMoney));
     }
 }
