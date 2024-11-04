ca71e0d8ea3df9a0ea3771975207561710ab15e4
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 16:36:09 +0900

    feat(Seller): 잔돈이 생기면 예외를 발생시킨다

--- src/main/java/lotto/Seller.java
+++ src/main/java/lotto/Seller.java
@@ -8,7 +8,14 @@ public class Seller {
     private static final int LOTTO_PRICE = 1000;
 
     public List<Lotto> issueLottoesWith(Money money) {
+        validateNoChanges(money);
         return issueRandomLottoes(money.countAffordableFor(LOTTO_PRICE));
+    }
+
+    private void validateNoChanges(Money money) {
+        if (money.hasChangesWith(LOTTO_PRICE)) {
+            throw new IllegalArgumentException("금액이 나눠 떨어지지 않습니다");
+        }
     }
 
     private List<Lotto> issueRandomLottoes(long count) {
