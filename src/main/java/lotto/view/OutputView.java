bea3270fd49b74139609e43d67272c4caf789b12
Gahyeon <best11gh@naver.com>
2024-11-04 13:42:46 +0900

    feat(OutputView): 프린트 순서 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,32 +1,33 @@
 package lotto.view;
 
-import java.util.Map;
+import java.util.*;
+import java.util.TreeMap;
 import lotto.domain.*;
 import lotto.service.ResultCalculator;
 
 public class OutputView {
+
     public void printLottoCount(int lottoCount) {
-        System.out.println();
-        System.out.printf("%d개를 구매했습니다.%n", lottoCount);
+        System.out.printf("%n%d개를 구매했습니다.%n", lottoCount);
     }
 
     public void printLottoNumbers(Lottos lottos) {
         lottos.getLottos().stream()
                 .map(Lotto::getNumbers)
                 .forEach(System.out::println);
+        System.out.println();
     }
 
 
     public void printWinningDetails(ResultCalculator resultCalculator) {
-        System.out.println();
-        System.out.println("당첨 통계\n---");
+        System.out.println("\n당첨 통계\n---");
 
-        Map<LottoRank, Integer> results = resultCalculator.getResult();
-        results.forEach((rank, count) -> {
-            if (rank == LottoRank.NONE) return;;
-            String resultMessage = formatPrizeMessage(rank, count);
-            System.out.println(resultMessage);
-        });
+        Map<LottoRank, Integer> results = new TreeMap<>(resultCalculator.getResult());
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank == LottoRank.NONE) continue;
+            int count = results.getOrDefault(rank, 0);
+            System.out.println(formatPrizeMessage(rank, count));
+        }
 
     }
 
@@ -35,7 +36,8 @@ public class OutputView {
                 count);
     }
 
+
     public void printYield(double yield) {
-        System.out.printf("총 수익률은 %.2f%%입니다.%n", yield);
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
     }
 }
