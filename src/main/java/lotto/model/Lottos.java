2b74aa6df628cd9464b71228b73b30932c33f862
JEN <ekffuclxk@gmail.com>
2024-11-04 19:19:19 +0900

    refactor(Lottos): 미사용 메서드 제거

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -19,24 +19,5 @@ public class Lottos {
     public int getSize(){
         return lottos.size();
     }
-
-    public Map<LottoRule, Integer> calculateMatch(List<Integer> winNumbers, int bonusNumber){
-        Map<LottoRule, Integer> matchInfo = new HashMap<>();
-
-        for (Lotto lotto : lottos) {
-            int matchCount = 0;
-
-            for (Integer winNumber : winNumbers) {
-                if(lotto.getNumbers().contains(winNumber)){
-                    matchCount++;
-                }
-            }
-
-            boolean bonusMatch = (matchCount == 5) && lotto.getNumbers().contains(bonusNumber);
-            LottoRule rule = LottoRule.getWinInfo(matchCount, bonusMatch);
-            matchInfo.put(rule, matchInfo.getOrDefault(rule, 0) + 1);
-        }
-        return matchInfo;
-    }
 }
 
