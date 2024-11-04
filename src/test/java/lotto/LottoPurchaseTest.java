41ffcfe803b63b412cdbfd7ddb667b03304e7ae9
JeongBuBu <zhfl1234@email.com>
2024-11-04 14:37:32 +0900

    test 단위 클래스

---
+++ src/test/java/lotto/LottoPurchaseTest.java
@@ -0,0 +1,15 @@
+package lotto;
+import Model.LottoPurchase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoPurchaseTest {
+    @DisplayName("구입 금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다.")
+    @Test
+    void createPurchaseAmountByNotDivideWith1000() {
+        assertThatThrownBy(() -> new LottoPurchase(1500))  // 수정된 부분
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
