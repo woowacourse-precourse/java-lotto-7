3c49585e956a4ead76f379e61341674d7fd16a01
junest66 <chlwndks333@gmail.com>
2024-11-04 16:35:27 +0900

    feat(Application): 로또 프로그램 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.dto.LottoResult;
+import lotto.dto.PurchaseLotto;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+
+        PurchaseLotto purchaseLotto = lottoController.purchaseLotto();
+        OutputView.printPurchaseLotto(purchaseLotto);
+        lottoController.registerWinningLotto();
+
+        LottoResult lottoResult = lottoController.getLottoResult();
+        OutputView.printResult(lottoResult);
     }
 }
