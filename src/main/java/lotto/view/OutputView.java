a652023d0bc21468f5acd8733366621e8bde0d25
김주언 <wndjs803@naver.com>
2024-11-04 13:54:02 +0900

    feat: 로또 번호 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
 import lotto.model.Amount;
+import lotto.model.Lotto;
+import lotto.model.LottoList;
+
+import java.util.List;
 
 public class OutputView {
     public void printCount(Amount amount){
         System.out.println(amount.getCount() + "개를 구입하셨습니다.");
+    }
+
+    public void printLottoList(LottoList lottoList){
+        for (Lotto lotto : lottoList.getLottoList()) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
