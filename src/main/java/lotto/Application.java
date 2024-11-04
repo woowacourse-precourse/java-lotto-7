833c863970acda97cc7f5a143d5cb0c0594898d2
thoi2 <kinggun1505@naver.com>
2024-11-04 23:59:51 +0900

    fix 출력화면 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -162,12 +162,12 @@ public class Application {
     }
 
     public static void printStatistics(int[] rankCounts) {
-        System.out.println("당첨 통계\n---");
-        System.out.printf("3개 일치 (%d원) - %d개%n", PrizeMoneyByRank.FIFTH.getPrizeMoney(), rankCounts[PrizeMoneyByRank.FIFTH.ordinal()]);
-        System.out.printf("4개 일치 (%d원) - %d개%n", PrizeMoneyByRank.FOURTH.getPrizeMoney(), rankCounts[PrizeMoneyByRank.FOURTH.ordinal()]);
-        System.out.printf("5개 일치 (%d원) - %d개%n", PrizeMoneyByRank.THIRD.getPrizeMoney(), rankCounts[PrizeMoneyByRank.THIRD.ordinal()]);
-        System.out.printf("5개 일치, 보너스 볼 일치 (%d원) - %d개%n", PrizeMoneyByRank.SECOND.getPrizeMoney(), rankCounts[PrizeMoneyByRank.SECOND.ordinal()]);
-        System.out.printf("6개 일치 (%d원) - %d개%n", PrizeMoneyByRank.FIRST.getPrizeMoney(), rankCounts[PrizeMoneyByRank.FIRST.ordinal()]);
+        System.out.println("\n당첨 통계\n---");
+        System.out.printf("3개 일치 (5,000원) - %d개%n", rankCounts[PrizeMoneyByRank.FIFTH.ordinal()]);
+        System.out.printf("4개 일치 (50,000원) - %d개%n", rankCounts[PrizeMoneyByRank.FOURTH.ordinal()]);
+        System.out.printf("5개 일치 (1,500,000원) - %d개%n", rankCounts[PrizeMoneyByRank.THIRD.ordinal()]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n", rankCounts[PrizeMoneyByRank.SECOND.ordinal()]);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개%n", rankCounts[PrizeMoneyByRank.FIRST.ordinal()]);
     }
 
     public static double calculateTotalProfit(int[] rankCounts) {
