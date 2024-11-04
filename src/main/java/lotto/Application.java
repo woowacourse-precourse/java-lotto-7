9b140132cc47632d719fdf660b9f48528217fdf4
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-11-04 21:08:53 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,13 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoGame lottoGame = new LottoGame();
+        try {
+            int amount = lottoGame.getPurchaseAmount();
+            System.out.println(amount / 1000 + "개를 구매했습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +16,34 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    // TODO: 추가 기능
+
+    public Lotto() {
+        this.numbers = generateUniqueRandomNumbers();
+    }
+
+    // 중복되지 않는 랜덤 숫자 생성 메서드
+    private List<Integer> generateUniqueRandomNumbers() {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        Random random = new Random();
+
+        while (uniqueNumbers.size() < 6) {
+            int number = random.nextInt(45) + 1;  // 1부터 45 사이의 난수 생성
+            uniqueNumbers.add(number);
+        }
+
+        List<Integer> sortedNumbers = new ArrayList<>(uniqueNumbers);
+        Collections.sort(sortedNumbers);  // 오름차순 정렬
+        return sortedNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
 }
