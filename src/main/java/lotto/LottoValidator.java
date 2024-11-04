f0d2aff8b888a744915e34a497a46b5ce6024064
youngkwanglim <richard08@naver.com>
2024-11-04 17:42:46 +0900

    feat: 당첨 번호가 6개가 아닐 경우 예외 처리

--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -6,6 +6,7 @@ public class LottoValidator {
     private static final int LOTTO_PRICE = 1000;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
 
     public static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
@@ -14,6 +15,9 @@ public class LottoValidator {
     }
 
     public static void validateWinningNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
         for (int number : numbers) {
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 정수여야 합니다.");
