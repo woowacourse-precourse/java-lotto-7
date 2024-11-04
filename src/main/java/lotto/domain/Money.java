41c792c29777e472c24b8df1e293d422a3f8f2c3
young-in <hardworkingini@gmail.com>
2024-11-04 21:45:15 +0900

    chore(Money.java) : method 명 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,9 +4,9 @@ public record Money(int money) {
     private static final int LOTTO_PRICE = 1000;
 
     public Money{
-        ValidateLottoMoney(money);
+        validateLottoMoney(money);
     }
-    private void ValidateLottoMoney(final int money){
+    private void validateLottoMoney(final int money){
         if (money <= 0 || money % LOTTO_PRICE != 0){
             throw new IllegalArgumentException("[ERROR] 로또 구매 금액은 1,000원 단위로 입력 해야합니다.");
         }
