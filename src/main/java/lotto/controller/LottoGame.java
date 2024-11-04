4b4279a12a4355d4b93d8ba878e9a98ada36b566
JMJ <ghtntkdnsk@naver.com>
2024-11-04 19:26:52 +0900

    feat(LottoGame) : LottoGame 기본 구조 설정

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,4 +1,21 @@
 package lotto.controller;
 
+import lotto.service.LottoGameService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoGame {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoGameService lottoGameService;
+
+    public LottoGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.lottoGameService = new LottoGameService();
+    }
+
+    public void start() {
+
+    }
 }
