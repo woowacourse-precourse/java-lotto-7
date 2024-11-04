fbfbb88d34c054263eb584def3a4fccba7642464
최영우 <allnstar@naver.com>
2024-11-04 17:03:20 +0900

    feat: 로또 번호가 중복될 경우 예외를 발생시키는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,21 +1,33 @@
 package lotto.model;
 
+import lotto.error.ErrorMessage;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private static final String DELIMITER = ", ";
+    private static final int CORRECT_LOTTO_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicatedNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != CORRECT_LOTTO_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_COUNT.getMessage());
+        }
+    }
+
+    private void validateDuplicatedNumber(List<Integer> numbers) {
+        HashSet<Integer> hashSet = new HashSet<>(numbers);
+        if (hashSet.size() != CORRECT_LOTTO_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBER.getMessage());
         }
     }
 
