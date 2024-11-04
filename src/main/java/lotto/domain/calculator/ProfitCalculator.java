cdbc9775a02ca7975fac0e10b5bf060dee3205ae
hootzz <wjqwjql3@gmail.com>
2024-11-04 17:09:02 +0900

    refactor(calculator): ProfitCalculator에서 Money 값 객체 활용

--- src/main/java/lotto/domain/calculator/ProfitCalculator.java
+++ src/main/java/lotto/domain/calculator/ProfitCalculator.java
@@ -6,16 +6,6 @@ import lotto.domain.result.WinningStatistics;
 
 public class ProfitCalculator {
     public WinningResult calculateResult(WinningStatistics statistics, Money purchaseMoney) {
-        Money totalPrize = statistics.calculateTotalPrize();
-        double profitRate = calculateProfitRate(purchaseMoney, totalPrize);
-        return createWinningResult(statistics, profitRate);
-    }
-
-    private double calculateProfitRate(Money purchaseMoney, Money totalPrize) {
-        return purchaseMoney.calculateProfitRate(totalPrize);
-    }
-
-    private WinningResult createWinningResult(WinningStatistics statistics, double profitRate) {
-        return WinningResult.of(statistics, profitRate);
+        return WinningResult.of(statistics, purchaseMoney);
     }
 }
