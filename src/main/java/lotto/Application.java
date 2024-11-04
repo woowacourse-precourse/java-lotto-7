763ad17ebf0cf714f3e23323d9a6d9fa3fd7e750
SuJeong <sujenghong@gmail.com>
2024-11-04 20:56:17 +0900

    feat: add purchase amount input and validation

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -8,12 +9,35 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        // 구입 금액 입력 및 유효성 검증
+        try {
+            // 구입 금액 입력 및 유효성 검증
+            int purchaseAmount = validatePurchaseAmount();
 
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+    }
+    //로또 번호 생성 기능
     private static Lotto generateLotto() {
         List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         numbers.sort(Integer::compareTo); // 번호 오름차순 정렬
         return new Lotto(numbers);
     }
+    //구입 금액 입력 및 유효성 검증 기능 추가
+    private static int validatePurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            int amount = Integer.parseInt(Console.readLine());
+            if (amount % 1000 != 0) {
+                throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효한 숫자를 입력해 주세요.");
+        }
+    }
+
+
 }
