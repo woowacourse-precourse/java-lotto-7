c3248cf5ef74b70ab9fdc472d1141f45e846772d
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 21:01:22 +0900

    refact(BuyingPriceParser): 좀 더 세밀한 예외처리를 위한 NumberFormatException 사용

--- src/main/java/lotto/util/parser/BuyingPriceParser.java
+++ src/main/java/lotto/util/parser/BuyingPriceParser.java
@@ -1,7 +1,11 @@
 package lotto.util.parser;
 
 public class BuyingPriceParser {
-    public static int toIntStringPriceParser(String price) {
-        return Integer.parseInt(price);
+    public static int toIntStringPriceParser(String buyingPriceInput) throws IllegalArgumentException  {
+        try {
+            return Integer.parseInt(buyingPriceInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구입금액은 숫자만 입력 가능합니다.");
+        }
     }
 }
