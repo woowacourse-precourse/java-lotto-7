98926e014b7601d65cafd5a0a90c92a85586cfbe
zerocat36 <Seo.james2020@gmail.com>
2024-11-04 21:32:34 +0900

    feat($Application.java): add getWinningNumbers function

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+    private static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+
+        try {
+            return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 잘못된 형식으로 입력되었습니다.");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
