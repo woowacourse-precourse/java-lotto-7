0e13a2eb2088e7da882c1f0a18a02c3fb2cd23ec
coolman0123 <socool4m@naver.com>
2024-11-04 16:59:14 +0900

    feat: 총 로또 개수에 대한 각 로또를 생성해라
    createLottoTickets: 총 로또 생성 로직
    generateSingleLottoTicket: 개별 로또 생성 로직

--- src/main/java/lotto/model/LottoPurchaseMachine.java
+++ src/main/java/lotto/model/LottoPurchaseMachine.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.Lotto;
 
 public class LottoPurchaseMachine {
@@ -15,5 +18,22 @@ public class LottoPurchaseMachine {
         return purchaseAmount / Lotto.PRICE_PER_TICKET;
     }
 
+    public List<Lotto> generateLottoTickets() {
+        int lottoAmount = calculateLottoAmount();
+        return createLottoTickets(lottoAmount);
+    } // 로또 개수 저장, 생성
 
+    private List<Lotto> createLottoTickets(int lottoAmount) {
+        List<Lotto> lottoTickets = new ArrayList<>();
+        for (int i = 0; i < lottoAmount; i++) {
+            lottoTickets.add(generateSingleLottoTicket());
+        } //총 로또 생성 로직
+        return lottoTickets;
+    }
+
+    private Lotto generateSingleLottoTicket() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, Lotto.NUMBERS_COUNT);
+        return new Lotto(numbers);
+    } // 개별 로또 생성 로직
 }
+
