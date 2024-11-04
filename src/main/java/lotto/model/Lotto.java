a50aadefab98e8a4c543b412f6b82ca6fce18cb8
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 20:38:16 +0900

    feat(Lotto): 예외처리 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,8 +1,14 @@
 package lotto.model;
 
+import lotto.Constants;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static lotto.Constants.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -20,8 +26,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != PICK_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(this::validateRange);
+    }
+
+    private void validateRange(Integer number) {
+        if (!(MIN_NUMBER <= number && number <= MAX_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 1 ~ 45 사이의 수가 아닙니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if (nonDuplicateNumbers.size() != PICK_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 번호가 중복되는 로또 번호가 존재합니다.");
         }
     }
 
