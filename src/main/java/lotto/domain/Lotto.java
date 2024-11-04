43fd26cb87cb9a431038d74a2859e1f80df73eef
LeeHaeJun <haejun0410@gmail.com>
2024-11-04 20:41:44 +0900

    feat: Lotto 클래스 정의

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,34 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::new)
+                .sorted((a, b) -> a.value() - b.value())
+                .toList();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[Error] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public List<LottoNumber> getNumbers() {
+        return new ArrayList<>(numbers);
+    }
+
+    public boolean contains(LottoNumber number) {
+        return numbers.stream()
+                .anyMatch(n -> n.value() == number.value());
+    }
 }
