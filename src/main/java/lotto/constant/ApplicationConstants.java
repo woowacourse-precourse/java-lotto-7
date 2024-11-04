88a332046f28902c0e563fdbe9eb25fae9c0b5e7
HIDE_MAIN <ra___be@naver.com>
2024-11-04 22:59:26 +0900

    feat: 출력문, 기본조건 상수 정의

---
+++ src/main/java/lotto/constant/ApplicationConstants.java
@@ -0,0 +1,18 @@
+package lotto.constant;
+
+public class ApplicationConstants {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_RANGE_MIN = 1;
+    public static final int LOTTO_RANGE_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String PURCHASE_SUCCESS_MESSAGE = "%d개를 구매했습니다.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String WINNING_STATISTICS_HEADER = "당첨 통계\n---";
+    public static final String MATCH_RESULT_MESSAGE1 = "%d개 일치";
+    public static final String MATCH_RESULT_MESSAGE2 = ", 보너스 볼 일치";
+    public static final String MATCH_RESULT_MESSAGE = "%s (%s원) - %d개";
+    public static final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+}
