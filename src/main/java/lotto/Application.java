36b25e58774314ded6e3ed8e6ee3ec60d7fc5ae6
hootzz <wjqwjql3@gmail.com>
2024-11-04 17:47:24 +0900

    feat(main): main 동작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import lotto.domain.ticket.Lotto;
+import lotto.domain.ticket.Lottos;
+import lotto.domain.ticket.WinningLotto;
+import lotto.domain.result.WinningResult;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        try {
+            runGame(lottoService, inputView, outputView);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void runGame(
+            LottoService lottoService,
+            InputView inputView,
+            OutputView outputView) {
+        int amount = inputView.readPurchaseAmount();
+        Lottos lottos = lottoService.purchaseLottos(amount);
+        outputView.printPurchaseResult(lottos);
+
+        WinningLotto winningLotto = createWinningLotto(inputView);
+        WinningResult result = lottoService.createWinningResult(lottos, winningLotto, amount);
+        outputView.printWinningStatistics(result);
+    }
+
+    private static WinningLotto createWinningLotto(InputView inputView) {
+        Lotto winningNumbers = (Lotto) inputView.readWinningNumbers();
+        int bonusNumber = inputView.readBonusNumber();
+        return new WinningLotto(winningNumbers, bonusNumber);
     }
 }
