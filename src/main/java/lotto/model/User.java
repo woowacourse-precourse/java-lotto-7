d0256ce197fa31ffc8870d6ea5f529f22c1505cf
sun <50648835+kimseongyu@users.noreply.github.com>
2024-11-04 17:23:36 +0900

    feat: validate bonus number and price when set it

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -21,6 +21,12 @@ public class User {
     }
 
     public void setBonusNumber(int bonusNumber) {
+        if (bonusNumber < Lotto.NUMBER_MIN || bonusNumber > Lotto.NUMBER_MAX) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_BONUS_NUMBER_RANGE.getMessage());
+        }
+        if (lotto.matchBonusNumber(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessages.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
         this.bonusNumber = bonusNumber;
     }
 
@@ -29,6 +35,9 @@ public class User {
     }
 
     public void setPrice(int price) {
+        if (price % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_PURCHASE_PRICE.getMessage());
+        }
         this.price = price;
     }
 }
