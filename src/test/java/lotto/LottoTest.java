975a1fcda7995918dba4c60d59306a65015856a4
이창헌 <changbill@naver.com>
2024-11-04 13:23:40 +0900

    test: add integration test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.exception.administrator.LottoNumbersDuplicationException;
 import lotto.exception.administrator.LottoNumbersMustBeSixException;
+import lotto.exception.administrator.OutOfRangeLottoNumberException;
 import lotto.model.administrator.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,10 @@ class LottoTest {
                 .isInstanceOf(LottoNumbersDuplicationException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호의_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(OutOfRangeLottoNumberException.class);
+    }
 }
