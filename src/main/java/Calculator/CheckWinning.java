53b41304885f102588e5bf3f23c9ee698d5728d4
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-04 14:49:24 +0900

    fix(로또 당첨 계산 기능): change some code to clarify
    change ranking calculating method code clarfy that.

--- src/main/java/Calculator/CheckWinning.java
+++ src/main/java/Calculator/CheckWinning.java
@@ -16,54 +16,49 @@ public class CheckWinning {
         String userInputWinningNumbers = inputRequest.winningNumbersRequest();
         String userInputBonusNumber = inputRequest.bonusNumberRequest();
 
-        HashSet<Integer> winningNumbers = strToSet(userInputWinningNumbers);
+        HashSet<Integer> parsedWinningNumbers = strToSet(userInputWinningNumbers);
         int bonusNumber = Integer.parseInt(userInputBonusNumber);
 
-        List<Integer> result = check(myLottos, winningNumbers, bonusNumber);
-
-        return result;
+        return check(myLottos, parsedWinningNumbers, bonusNumber);
     }
 
-    //보너스 번호 없이 확인하는 경우 1 ~ 5등까지 확인
-    //단 2등의 경우 보너스 확인까지 필요.
+    // 보너스 번호 없이 확인하는 경우 1 ~ 5등까지 확인
     public List<Integer> check(List<Lotto> myLottos, HashSet<Integer> winningNumbers, int bonusNumber) {
         List<Integer> localResult = new ArrayList<>(8);
         for (int i = 0; i < 8; i++) {
             localResult.add(0);
         }
+
         for (Lotto myLotto : myLottos) {
             int rank = checkDetail(myLotto.getNumbers(), winningNumbers, bonusNumber);
-            localResult.add(rank, localResult.get(rank) + 1);
+            localResult.set(rank, localResult.get(rank) + 1);
         }
+
         return localResult;
     }
 
     public int checkDetail(List<Integer> myLotto, HashSet<Integer> winningNumbers, int bonusNumber) {
         int rank = compare(winningNumbers, myLotto);
-        if (rank == 2) {
-            if (!myLotto.contains(bonusNumber)) {
-                return 3;
-            }
+        if (rank == 2 && !myLotto.contains(bonusNumber)) {
+            return 3;
         }
         return rank;
     }
 
     public int compare(HashSet<Integer> winningNumbers, List<Integer> singleLotto) {
         HashSet<Integer> myLotto = new HashSet<>(singleLotto);
-
-        winningNumbers.removeAll(myLotto);
-
-        return winningNumbers.size() + 1;
+        myLotto.retainAll(winningNumbers); // 교집합 구하기
+        return 7 - myLotto.size(); // 일치하는 숫자에 따라 순위 반환
     }
 
     private HashSet<Integer> strToSet(String str) {
         String[] winningNumbersArray = str.split(",");
 
-        HashSet<Integer> winningNumbers = new HashSet<>();
+        HashSet<Integer> parsedWinningNumbers = new HashSet<>();
         for (String winningNumber : winningNumbersArray) {
-            winningNumbers.add(Integer.parseInt(winningNumber));
+            parsedWinningNumbers.add(Integer.parseInt(winningNumber));
         }
 
-        return winningNumbers;
+        return parsedWinningNumbers;
     }
 }
