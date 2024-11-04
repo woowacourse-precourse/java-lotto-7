24121e5747c0069ac01a1141cd57ff8643f1913b
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:58:25 +0900

    feat(Money): 수익률을 계산할 수 있다

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -31,6 +31,10 @@ public class Money {
         return new Money(amount + money.amount);
     }
 
+    public double calculateProfitRateOf(Money profit) {
+        return (double) profit.amount / amount;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
