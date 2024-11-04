1f5cafb9e0a4860d125ac5ee44047e8b42f3ff7b
yunhacandy <yunhacandy@gmail.com>
2024-11-04 13:19:23 +0900

    refactor (LottoGameController): 구입금액 입력 메소드 service 클래스로 이동

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -4,28 +4,14 @@ import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.LottoPurchase;
 import lotto.service.LottoGameService;
-import lotto.validator.LottoPurchaseValidator;
-import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
     private final LottoGameService lottoGameService = new LottoGameService();
 
     public void run() {
-        LottoPurchase lottoPurchase = inputPurchaseAmount();
+        LottoPurchase lottoPurchase = lottoGameService.inputPurchaseAmount();
         List<Lotto> purchasedLotto = lottoGameService.generateLottoTickets(lottoPurchase);
         OutputView.printPurchasedLotto(purchasedLotto);
-    }
-
-    private LottoPurchase inputPurchaseAmount() {
-        while (true) {
-            try {
-                String input = InputView.inputPurchaseAmount();
-                int purchaseAmount = LottoPurchaseValidator.validateAndParse(input);
-                return lottoGameService.createLottoPurchase(purchaseAmount);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
 }
--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -5,12 +5,26 @@ import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.LottoIssuer;
 import lotto.model.LottoPurchase;
+import lotto.validator.LottoPurchaseValidator;
+import lotto.view.InputView;
 
 public class LottoGameService {
     private final LottoIssuer lottoIssuer = new LottoIssuer();
 
     public LottoPurchase createLottoPurchase(int purchaseAmount) {
         return new LottoPurchase(purchaseAmount);
+    }
+
+    public LottoPurchase inputPurchaseAmount() {
+        while (true) {
+            try {
+                String input = InputView.inputPurchaseAmount();
+                int purchaseAmount = LottoPurchaseValidator.validateAndParse(input);
+                return createLottoPurchase(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public List<Lotto> generateLottoTickets(LottoPurchase lottoPurchase) {
