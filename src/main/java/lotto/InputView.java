92a5914863926589e15ff00c30faf61544162c94
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 16:07:52 +0900

    feat(InputView): 사용자의 구매금액 입력값에 대한 예외처리
    아래 예외 경우에 대해서 에러 메세지 출력 후 종료되도록 했습니다.
    - 숫자가 아닌 경우
    - 1000으로 나눠 떨어지지 않는 경우
    - 0또는 음수 값을 갖는 경우

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,7 +7,22 @@ import java.util.List;
 
 public class InputView {
     public static int inputPurchaseAmount() {
-        return Integer.parseInt(Console.readLine());
+        try {
+            int purchaseAmount = Integer.parseInt(Console.readLine());
+            validatePurchaseAmount(purchaseAmount);
+            return purchaseAmount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private static void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상이어야 합니다.");
+        }
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        }
     }
 
     public static List<Integer> inputWinningNumber() {
