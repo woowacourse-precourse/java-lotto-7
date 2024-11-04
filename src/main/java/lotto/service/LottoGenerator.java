1bdf6fb78b522de9f720b460345e06ae9f0234c9
hongjungin <jungini0000@gmail.com>
2024-11-04 21:19:16 +0900

    refactor: LOTTO_NUMBER_COUNT 로 수정

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -10,13 +10,14 @@ public class LottoGenerator {
     private static final int LOTTO_PRICE = 1000;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     public static int calculateLottoCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
     }
 
     public static List<Integer> generateLottoNumbers() {
-        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, 6);
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_NUMBER_COUNT);
         lottoNumbers.sort(Integer::compareTo);
         return lottoNumbers;
     }
