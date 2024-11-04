a103191ea60b20d284931c0437947271676fa83e
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 20:22:33 +0900

    feat: LottoService 클래스 제거

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,25 +1,24 @@
-package lotto.service;
-
-import lotto.domain.PurchaseAmount;
-import lotto.dto.PrizeResponse;
-import lotto.repository.LottoRepository;
-
-import java.util.List;
-
-public class LottoService {
-    private final LottoRepository lottoRepository;
-
-    public LottoService(LottoRepository lottoRepository) {
-        this.lottoRepository = lottoRepository;
-    }
-
-    public double calculateProfitRate(PurchaseAmount purchaseAmount, List<PrizeResponse> prizeResponses) {
-        int totalProfit = 0;
-        for (PrizeResponse prizeResponse : prizeResponses) {
-            totalProfit += prizeResponse.prizeMoney() * prizeResponse.winningCount();
-        }
-
-        double profitRate = ((double) totalProfit / purchaseAmount.getAmount()) * 100;
-        return Math.round(profitRate * 10.0) / 10.0;
-    }
-}
+//package lotto.service;
+//
+//import lotto.domain.PurchaseAmount;
+//import lotto.dto.PrizeResponse;
+//import lotto.repository.LottoRepository;
+//
+//import java.util.List;
+//
+//public class LottoService {
+//    private final LottoRepository lottoRepository;
+//
+//    public LottoService(LottoRepository lottoRepository) {
+//        this.lottoRepository = lottoRepository;
+//    }
+//
+//    public double calculateProfitRate(PurchaseAmount purchaseAmount, List<PrizeResponse> prizeResponses) {
+//        int totalProfit = prizeResponses.stream()
+//                .mapToInt(PrizeResponse::getProfit)
+//                .sum();
+//
+//        double profitRate = ((double) totalProfit / purchaseAmount.getAmount()) * 100;
+//        return Math.round(profitRate * 10.0) / 10.0;
+//    }
+//}
