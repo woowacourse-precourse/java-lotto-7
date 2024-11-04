2968550f772c0040d7359b1343057d6089a76a4b
AwesomeMinsik <beaut3579@gmail.com>
2024-11-04 15:47:05 +0900

    refactor : 클래스 내부의 입력값 예외에 따라 반복실행하는 로직을 제거하였습니다
    - 예외 발생시점에서 반복실행하는 역할을 InputController 로 분리함에 따라, InputHandler 클래스는 사용자의 입력값 받기만 수행하도록 하였습니다.

--- src/main/java/lotto/IO/InputHandler.java
+++ src/main/java/lotto/IO/InputHandler.java
@@ -2,8 +2,8 @@ package lotto.IO;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.Validator;
-import lotto.messages.ErrorMessage;
-import lotto.messages.SystemMessage;
+import lotto.constants.ErrorMessage;
+import lotto.constants.SystemMessage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,46 +11,27 @@ import java.util.List;
 public abstract class InputHandler {
 
     public static int getPrice() {
-        while (true) {
-            try {
-                System.out.println(SystemMessage.INPUT_PRICE.getMessage());
-                String input = Console.readLine();
-                Validator.validateInput(input);
-                Validator.validateLottoPrice(input);
-                return convertToInt(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessage.PRICE_NOT_NUMBER.getMessage());
-            }
-        }
+        String input = Console.readLine();
+        Validator.validateInput(input);
+        Validator.validateLottoPrice(input);
+        return convertToInt(input);
     }
 
     public static List<Integer> getWinningLottoNumber() {
-        while (true) {
-            try {
-                System.out.println(SystemMessage.INPUT_WINNING_NUMBER.getMessage());
-                String input = Console.readLine();
-                List<Integer> winningNumber = Arrays.stream(input.split(","))
-                        .map(Integer::parseInt)
-                        .toList();
-                Validator.validateInput(winningNumber);
-                return winningNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessage.WINNING_NUMBER_DUPLICATION.getMessage());
-            }
-        }
+        System.out.println(SystemMessage.INPUT_WINNING_NUMBER.getMessage());
+        String input = Console.readLine();
+        List<Integer> winningNumber = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        Validator.validateInput(winningNumber);
+        return winningNumber;
     }
 
     public static int getBonusNumber(List<Integer> winningNumber) {
-        while (true) {
-            try {
-                System.out.println(SystemMessage.INPUT_BONUS_NUMBER.getMessage());
-                String input = Console.readLine();
-                Validator.validateInput(input);
-                return Validator.checkBonusNumberDuplication(convertToInt(input), winningNumber);
-            } catch (IllegalArgumentException e) {
-                System.out.println(ErrorMessage.BONUS_NUMBER_DUPLICATION);
-            }
-        }
+        System.out.println(SystemMessage.INPUT_BONUS_NUMBER.getMessage());
+        String input = Console.readLine();
+        Validator.validateInput(input);
+        return Validator.checkBonusNumberDuplication(convertToInt(input), winningNumber);
     }
 
     public static int convertToInt(String input) {
