571c20617271ed6756c83211ece44019921ee8a0
DongHoon Lee <dhl1924@naver.com>
2024-11-04 16:29:25 +0900

    feat(view): 구매한 로또 수를 출력하는 메서드 구현

--- src/main/java/lotto/view/console/ConsoleWriter.java
+++ src/main/java/lotto/view/console/ConsoleWriter.java
@@ -4,9 +4,15 @@ import lotto.view.OutputView;
 
 public class ConsoleWriter implements OutputView {
     private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PURCHASED_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
 
     @Override
     public void printPurchaseAmountRequest() {
         System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
+    }
+
+    @Override
+    public void printPurchasedLottoCount(int count) {
+        System.out.printf(PURCHASED_LOTTO_COUNT_MESSAGE + "%n", count);
     }
 }
