19288508b1eb3bb2c311c4590ff427f3ab550832
023-dev <0_2_3@naver.com>
2024-11-04 17:03:05 +0900

    feat(Messages): 출력 메시지 상수 관리

--- src/main/java/lotto/common/Messages.java
+++ src/main/java/lotto/common/Messages.java
@@ -2,4 +2,11 @@ package lotto.common;
 
 public class Messages {
     public static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    public static final String RESULT_TITLE = "당첨 통계";
+    public static final String RESULT_DIVIDER = "---";
+    public static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    public static final String WINNING_STATISTICS_FORMAT = "%d개 일치 (%s원) - %d개";
+    public static final String TOTAL_PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
 }
