9e3db3ccc3d4fd869cf60f2737efbfdf6087633d
tjfwltn <tjfwltn1234@gmail.com>
2024-11-04 12:56:32 +0900

    refactor : StringParser에서 ,를 상수 정의
    - ,를 상수로 정의하고 상수를 사용하게 리팩토링

--- src/main/java/lotto/util/StringParser.java
+++ src/main/java/lotto/util/StringParser.java
@@ -7,6 +7,8 @@ import static lotto.constants.LottoErrorMessage.INVALID_NUMBER_FORMAT;
 
 public abstract class StringParser {
 
+    private static final String SEPARATOR = ",";
+
     public static int toInt(String input) {
         try {
             return Integer.parseInt(input);
@@ -16,7 +18,7 @@ public abstract class StringParser {
     }
 
     public static List<Integer> toIntList(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(SEPARATOR))
                 .map(String::trim)
                 .map(StringParser::toInt)
                 .toList();
