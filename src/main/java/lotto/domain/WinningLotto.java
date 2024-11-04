82b6886cc0b5cbc6717bd9e57a96c0e69c139853
hyuuuun <pam4522@gmail.com>
2024-11-04 12:37:11 +0900

    test(WinnigLotto) : 보너스 번호 중복 및 범위 검증

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto extends Lotto{
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+    private void validateBonusNumber(int bonus){
+        new LottoNumber(bonus);
+        if(getNumbers().contains(bonus)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+}
