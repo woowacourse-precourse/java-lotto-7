ace1bc4d4ab38a70212ce04d75974496425da2b6
hobinBae <qoghqls95@gmail.com>
2024-11-04 16:56:58 +0900

    refactor: 메서드 사이 공백 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,13 +17,13 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public void outputLottoNumbers(){
+    public void outputLottoNumbers() {
         System.out.println(Arrays.toString(this.numbers.stream().toArray()));
     }
 
-    public int MatchCount(Lotto winningNumbers){
-        return (int)this.numbers.stream()
-                .filter(winningNumbers ::contains)
+    public int MatchCount(Lotto winningNumbers) {
+        return (int) this.numbers.stream()
+                .filter(winningNumbers::contains)
                 .count();
     }
 
@@ -32,29 +32,31 @@ public class Lotto {
             System.out.println(OutputView.NUMBER_SIZE_WRONG);
             throw new IllegalArgumentException();
         }
-        if(checkForDuplicates(numbers)){
+        if (checkForDuplicates(numbers)) {
             System.out.println(OutputView.NUMBER_OVERLAP);
             throw new IllegalArgumentException();
         }
     }
+
     // TODO: 추가 기능 구현
-    private boolean checkForDuplicates(List<Integer>numbers){
+    private boolean checkForDuplicates(List<Integer> numbers) {
         Set<Integer> duplicationReMove = new HashSet<>(numbers);
         return duplicationReMove.size() != numbers.size();
     }
-    private void checkNumberRange(List<Integer>Numbers){
+
+    private void checkNumberRange(List<Integer> Numbers) {
         for (int number : Numbers) {
-            if(number > 45 || number < 1){
+            if (number > 45 || number < 1) {
                 System.out.println(OutputView.NUMBER_RANGE_OVER);
                 throw new IllegalArgumentException();
             }
         }
     }
 
-    public void checkForBonusNumberDuplicates(int bonus){
+    public void checkForBonusNumberDuplicates(int bonus) {
         Set<Integer> duplicationReMove = new HashSet<>(this.numbers);
         duplicationReMove.add(bonus);
-        if(duplicationReMove.size() != numbers.size() + 1){
+        if (duplicationReMove.size() != numbers.size() + 1) {
             System.out.println(OutputView.BONUS_NUMBER_OVERLAP);
             throw new IllegalArgumentException();
         }
