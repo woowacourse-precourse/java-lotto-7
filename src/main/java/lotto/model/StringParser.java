3b748679b0c9ad93b1955352af680fd91e25ea81
eunseobb <seobseob0810@naver.com>
2024-11-04 22:17:02 +0900

    refactor(model): 접근제어자 수정 및 메서드 분리
    - 상수를 다른 클래스에서 사용하기 위해 private에서 protected로 수정
    - 검증 메서드를 다른 클래스로 분리

--- src/main/java/lotto/model/StringParser.java
+++ src/main/java/lotto/model/StringParser.java
@@ -3,18 +3,22 @@ package lotto.model;
 import java.util.Arrays;
 import java.util.List;
 
-import static lotto.constant.ErrorMessage.*;
 
 public class StringParser {
-    private static final int UNIT_AMOUNT = 1000;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int NUMBER_COUNT = 6;
+    protected static final int UNIT_AMOUNT = 1000;
+    protected static final int MIN_NUMBER = 1;
+    protected static final int MAX_NUMBER = 45;
+    protected static final int NUMBER_COUNT = 6;
+    private final Validation validation;
+
+    public StringParser(Validation validation) {
+        this.validation = validation;
+    }
 
     public Integer findLottoCount(String rawAmount) {
-        int amount = validateInteger(rawAmount);
-        isPositive(amount);
-        return validateAmount(amount);
+        int amount = validation.validateInteger(rawAmount);
+        validation.isPositive(amount);
+        return validation.validateAmount(amount);
     }
 
     public List<Integer> findMyNumbers(String rawNumbers) {
@@ -28,70 +32,23 @@ public class StringParser {
     }
 
     public Integer findBonusNumber(List<Integer> myNumbers, String rawBonusNumber) {
-        Integer bonusNumber = validateInteger(rawBonusNumber);
-        isPositive(bonusNumber);
-        validateRange(bonusNumber);
-        isUnique(myNumbers, bonusNumber);
+        int bonusNumber = validation.validateInteger(rawBonusNumber);
+        validation.isPositive(bonusNumber);
+        validation.validateRange(bonusNumber);
+        validation.isUnique(myNumbers, bonusNumber);
         return bonusNumber;
     }
 
     private List<Integer> validateMyNumbers(List<String> splitNumbers) {
-        List<Integer> myNumbers = splitNumbers.stream().map(this::validateInteger)
-            .filter(this::isPositive)
-            .filter(this::validateRange)
+        List<Integer> myNumbers = splitNumbers.stream().map(validation::validateInteger)
+            .filter(validation::isPositive)
+            .filter(validation::validateRange)
             .toList();
 
-        isUnique(myNumbers);
-        validateSize(myNumbers);
+        validation.isUnique(myNumbers);
+        validation.validateSize(myNumbers);
 
         return myNumbers;
-    }
-
-    private int validateInteger(String rawNumber) {
-        try {
-            return Integer.parseInt(rawNumber);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INTEGER_ERROR.getMessage());
-        }
-    }
-
-    private boolean isPositive(int amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException(NEGATIVE_ERROR.getMessage());
-        }
-        return true;
-    }
-
-    private int validateAmount(int amount) {
-        if ((amount % UNIT_AMOUNT) == 0) {
-            return amount / UNIT_AMOUNT;
-        }
-        throw new IllegalArgumentException(THOUSANDS_ERROR.getMessage());
-    }
-
-    private boolean validateRange(int number) {
-        if (number >= MIN_NUMBER && number <= MAX_NUMBER) {
-            return true;
-        }
-        throw new IllegalArgumentException(RANGE_ERROR.getMessage());
-    }
-
-    private void validateSize(List<Integer> myNumbers) {
-        if (myNumbers.size() > NUMBER_COUNT) {
-            throw new IllegalArgumentException(NUMBER_COUNT_ERROR.getMessage());
-        }
-    }
-
-    private void isUnique(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != NUMBER_COUNT) {
-            throw new IllegalArgumentException(REPEAT_ERROR.getMessage());
-        }
-    }
-
-    private void isUnique(List<Integer> numbers, Integer bonusNumber) {
-        if (numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException(REPEAT_ERROR.getMessage());
-        }
     }
 
 
