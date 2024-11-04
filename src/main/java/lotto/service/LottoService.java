99fe173e81020471f718da07bdbc8a2d8f590103
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-11-04 14:22:49 +0900

    feat: 당첨 번호 비교 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,47 +1,80 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.enums.Ranking;
 import lotto.model.Lotto;
-import lotto.utils.InputHandler;
+import lotto.model.Money;
+import lotto.model.WinningNumber;
 
-import java.util.ArrayList;
-import java.util.List;
+import lotto.view.OutputView;
+
+import java.util.*;
+
+import static lotto.enums.Constants.*;
+import static lotto.enums.Ranking.MISS;
 
 public class LottoService {
-    private final InputHandler inputHandler;
 
-    public LottoService(InputHandler inputHandler) {
-        this.inputHandler = inputHandler;
+    public Money moneyGenerate(int input) {
+        return new Money(input);
     }
 
-    public List<Lotto> lottoGenerate(int gameTrial) {
+    public Lotto lottoGenerate(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public WinningNumber winningNumberGenerate(Lotto lotto, int bonusNumber) {
+        return new WinningNumber(lotto, bonusNumber);
+    }
+
+    public List<Lotto> lottosGenerate(Money money) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int i=0; i<gameTrial; i++){
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        int trial = money.getMoney()/1000;
+
+        for (int i = 0; i < trial; i++) {
+            Lotto lotto =lottoGenerate(Randoms.pickUniqueNumbersInRange(
+                    MIN_LOTTO_NUM.getValue(), MAX_LOTTO_NUM.getValue(),NUMBER_OF_LOTTO_NUMBERS.getValue()));
+            lottos.add(lotto);
         }
+
         return lottos;
     }
+    public Map<Ranking,Integer> compareLottosWithWinning(List<Lotto> lottos, WinningNumber winningNumber) {
+        Map<Ranking, Integer> result = resultInit();
 
-    public List<Integer> compareLottosWithWinning(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNum) {
-        List<Integer> result = new ArrayList<>();
         for (Lotto lotto : lottos) {
-            result.add(compareLottoWithWinning(winningNumbers, lotto,bonusNum));
+            int lottoResult = compareLottoWithWinning(winningNumber, lotto);
+            Ranking rank = Ranking.valueOf(lottoResult);
+
+            if (rank!= MISS ){
+                result.compute(rank, (ranking, integer) -> integer + 1);
+            }
         }
         return result;
     }
-
-    private int compareLottoWithWinning(List<Integer> winningNumbers, Lotto lotto, int bonusNum) {
+    private int compareLottoWithWinning(WinningNumber winningNumber, Lotto lotto) {
         List<Integer> numbers = lotto.getNumbers();
         int cnt =0;
         for (Integer number : numbers) {
-            if (winningNumbers.contains(number)) {
+            if (winningNumber.getWinningNumber().contains(number)) {
                 cnt+=1;
             }
         }
-        if (cnt==5 && numbers.contains(bonusNum)){
+        if (cnt==5 && numbers.contains(winningNumber)){
             cnt+=2;
         }
         return cnt;
+    }
+
+    private Map<Ranking, Integer> resultInit() {
+        Map<Ranking, Integer> result = new LinkedHashMap<>();
+        Ranking[] values = Ranking.values();
+        for (Ranking ranking : values) {
+            if (ranking!=MISS){
+                result.put(ranking, 0);
+            }
+        }
+        return result;
     }
 
 
