da0aca5f9d64af71b9f253488995f2974334de5d
ljhee92 <weilainib7@gmail.com>
2024-11-04 11:20:01 +0900

    test: 1,000원 단위 구분자 추가
    가독성을 위해 1,000원 단위 _ 구분자 추가

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("구입 금액 검증")
 class MoneyTest {
     @ParameterizedTest
-    @ValueSource(ints = {1000, 100_000})
+    @ValueSource(ints = {1_000, 100_000})
     @DisplayName("1,000원으로 나누어 떨어지는지")
     void validMoney(int inputMoney) {
         assertThat(Money.from(inputMoney)).isInstanceOf(Money.class);
@@ -27,20 +27,25 @@ class MoneyTest {
     @ParameterizedTest(name = "{index} : {3}")
     @MethodSource("generateExceptionData")
     @DisplayName("예외 검증")
-    void invalidMoney(int inputMoney, Class expectedClass, String errorMessage, String message) {
+    void invalidMoney(int inputMoney, Class expectedExceptionClass, String errorMessage, String message) {
         assertThatThrownBy(() -> {
             Money.from(inputMoney);
-        }).isInstanceOf(expectedClass)
+        }).isInstanceOf(expectedExceptionClass)
                 .hasMessage(errorMessage);
     }
 
     static Stream<Arguments> generateExceptionData() {
         return Stream.of(
-                Arguments.of(0, ZeroMoneyException.class, ErrorMessage.PURCHASE_MONEY_MINIMUM, "0원일 때"),
-                Arguments.of(100, DivideMoneyException.class, ErrorMessage.PURCHASE_MONEY_DIVIDE, "1,000원으로 나누어 떨어지지 않을 때"),
-                Arguments.of(1001, DivideMoneyException.class, ErrorMessage.PURCHASE_MONEY_DIVIDE, "1,000원으로 나누어 떨어지지 않을 때"),
-                Arguments.of(100_001, MaximunMoneyException.class, ErrorMessage.PURCHASE_MONEY_MAXIMUM, "100,000원 이상일 때"),
-                Arguments.of(200_000, MaximunMoneyException.class, ErrorMessage.PURCHASE_MONEY_MAXIMUM, "100,000원 이상일 때")
+                Arguments.of(0, ZeroMoneyException.class,
+                        ErrorMessage.PURCHASE_MONEY_MINIMUM, "0원일 때"),
+                Arguments.of(100, DivideMoneyException.class,
+                        ErrorMessage.PURCHASE_MONEY_DIVIDE, "1,000원으로 나누어 떨어지지 않을 때"),
+                Arguments.of(1_001, DivideMoneyException.class,
+                        ErrorMessage.PURCHASE_MONEY_DIVIDE, "1,000원으로 나누어 떨어지지 않을 때"),
+                Arguments.of(100_001, MaximunMoneyException.class,
+                        ErrorMessage.PURCHASE_MONEY_MAXIMUM, "100,000원 이상일 때"),
+                Arguments.of(200_000, MaximunMoneyException.class,
+                        ErrorMessage.PURCHASE_MONEY_MAXIMUM, "100,000원 이상일 때")
         );
     }
 }
