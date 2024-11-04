4e721d918c3527fc5a4376c33236a6c21578729a
byeongdoohan <quden04@gmail.com>
2024-11-04 22:28:49 +0900

    refactor: 사용하지 않는 text 제거

--- src/main/java/lotto/message/LottoInfoMessages.java
+++ src/main/java/lotto/message/LottoInfoMessages.java
@@ -3,15 +3,6 @@ package lotto.message;
 public enum LottoInfoMessages {
     INSERT_PAY("구입금액을 입력해 주세요."),
     NOTICE_BUY_AMOUNT_START("개를 구매했습니다."),
-    NOTICE_BUY_AMOUNT_END("원)"),
-    INSERT_MANUAL_AMOUNT_START("수동 번호로 구매할 갯수를 입력해주세요. (최대 "),
-    INSERT_MANUAL_AMOUNT_END("개 입력 가능)"),
-    INSERT_NUMBERS_START("""
-            1~45 사이의 겹치지 않는 6개의 번호를 쉼표로 구분해 입력해주세요.
-            Enter를 누르면 자동으로 번호를 입력합니다."""),
-    INSERT_NUMBERS_MIDDLE("""
-            (수동 구매 갯수가 [\s"""),
-    INSERT_NUMBERS_END(" ]개 남았습니다.)"),
     BUY_LOTTO_LIST("""
         [구입 번호 목록]"""),
     INSERT_WINNING_NUMBER("""
