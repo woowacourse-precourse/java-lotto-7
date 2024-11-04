ab8167e33d1a1a28a4aced6f8fe11471b9dae72b
KIMSEI1124 <workju1124@gmail.com>
2024-11-04 19:58:28 +0900

    feat: 검증 메시지 추가

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -2,7 +2,10 @@ package lotto.common;
 
 public enum ErrorMessage {
 	NON_NUMERIC_INPUT("[ERROR] 입력 값은 숫자만 허용됩니다. 올바른 숫자를 입력해 주세요."),
-	INVALID_AMOUNT("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다. 올바른 금액을 입력해 주세요.");
+	INVALID_AMOUNT("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다. 올바른 금액을 입력해 주세요."),
+	NUMBER_OUT_OF_RANGE("[ERROR] 숫자는 {0}부터 {1} 사이의 숫자여야 합니다."),
+	DUPLICATE_NUMBERS("[ERROR] 로또 번호는 {0}개의 고유한 숫자여야 합니다."),
+	BONUS_NUMBER_DUPLICATE("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");;
 
 	private final String message;
 
