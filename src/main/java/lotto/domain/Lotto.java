294cdedbfc82526e0053fb063f8dd5e7b4e565f6
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-04 16:48:16 +0900

    feat : 도메인 검증로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -8,6 +9,7 @@ public class Lotto {
     private Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateEachNumber(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
@@ -54,12 +56,16 @@ public class Lotto {
 
     private void validateEachNumber(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number <= 0) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1 이상의 양수로 입력해 주세요.");
-            }
-            if (number > 45) {
+            if (number <= 0 || number > 45) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복일 수 없습니다.");
         }
     }
 }
