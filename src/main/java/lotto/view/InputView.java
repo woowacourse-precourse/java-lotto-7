0ffb36d3a771d4de9be2ae34dab2a7a9fbe64122
김주언 <wndjs803@naver.com>
2024-11-04 17:37:56 +0900

    fix: InputView 클래스 이름 오타 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,7 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputVIew {
+public class InputView {
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
