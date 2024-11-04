dbd4587c0883fe234f0ce8f0fab1657473b69447
hootzz <wjqwjql3@gmail.com>
2024-11-04 14:22:04 +0900

    feat(ticket): 당첨 확인 WinningLotto class 구현

--- src/main/java/lotto/domain/ticket/WinningLotto.java
+++ src/main/java/lotto/domain/ticket/WinningLotto.java
@@ -1,4 +1,29 @@
 package lotto.domain.ticket;
 
 public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validateBonusNumber(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningNumbers, int bonusNumber) {
+        validateBonusRange(bonusNumber);
+        validateBonusDuplicate(winningNumbers, bonusNumber);
+    }
+
+    private void validateBonusRange(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateBonusDuplicate(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.");
+        }
+    }
 }
