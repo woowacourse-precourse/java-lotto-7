33dcc845fce549aab2707eefee641c1a47cdb5bb
CHISANW <keuye06380618@gmail.com>
2024-11-04 23:29:25 +0900

    refactor(ErrorMessage): 상수 선언 순서 변경
    - 상수의 선언 순서를 public static final로 변경하여 코드 스타일 일관성 향상

--- src/main/java/lotto/vaildate/ErrorMessage.java
+++ src/main/java/lotto/vaildate/ErrorMessage.java
@@ -2,13 +2,13 @@ package lotto.vaildate;
 
 public class ErrorMessage {
     
-    public final static String INPUT_AMOUNT_IN_THOUSANDS = "[ERROR] 천원단위로 입력해주세요";
-    public final static String ONLY_NUMBER_INPUT_ALLOWED = "[ERROR] 숫자 형식으로만 입력해주세여";
-    public final static String NUMBER_ONE_TO_FORTY_FIVE = "[ERROR] 당첨 번호는 1과 45 사이여야 합니다.";
-    public final static String UNIQUE_NUMBER = "[ERROR] 당첨 번호는 중복된 될수 없습니다.";
-    public final static String INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
-    public final static String INVALID_BONUS_NUMBER = "[ERROR] 올바른 보너스 번호를 입력해주세요.";
-    public final static String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
-    public final static String INPUT_PROMPT = "[ERROR] 값을 입력해주세요.";
+    public static final String INPUT_AMOUNT_IN_THOUSANDS = "[ERROR] 천원단위로 입력해주세요";
+    public static final String ONLY_NUMBER_INPUT_ALLOWED = "[ERROR] 숫자 형식으로만 입력해주세여";
+    public static final String NUMBER_ONE_TO_FORTY_FIVE = "[ERROR] 당첨 번호는 1과 45 사이여야 합니다.";
+    public static final String UNIQUE_NUMBER = "[ERROR] 당첨 번호는 중복된 될수 없습니다.";
+    public static final String INVALID_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String INVALID_BONUS_NUMBER = "[ERROR] 올바른 보너스 번호를 입력해주세요.";
+    public static final String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+    public static final String INPUT_PROMPT = "[ERROR] 값을 입력해주세요.";
 
 }
