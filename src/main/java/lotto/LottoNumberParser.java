02eabc61baf4db05b86949631cef31373635a01c
0tae <choiyt3465@naver.com>
2024-11-04 21:33:41 +0900

    feat: 로또 넘버 파싱 로직 작성

---
+++ src/main/java/lotto/LottoNumberParser.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import lotto.enums.LottoConfig;
+import lotto.exception.ParserExceptionMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoNumberParser implements Parser<String, List<Integer>> {
+    @Override
+    public List<Integer> parse(String givenInput) {
+        String[] lottoNumbers = givenInput.split(",");
+        return convert2lottoNumbers(lottoNumbers);
+    }
+
+    private List<Integer> convert2lottoNumbers(String[] givenNumbers){
+        try{
+            return Arrays.stream(givenNumbers).map(Integer::parseInt).toList();
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(ParserExceptionMessage.NUMBER_FORMAT_INCORRECT.getMessage());
+        }
+    }
+}
