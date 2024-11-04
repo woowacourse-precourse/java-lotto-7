ca27d382cd45f0b8aeb2b269811dcea3d7b622e2
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 13:25:33 +0900

    feat: 로또 구매 금액 예외 기능 작성 checkInput(), checkNumber(), checkUnit()

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -6,7 +6,8 @@ public class Input {
     public static int inputPurchasePrice() {
         String input = Console.readLine();
         checkInput(input);
-        int purchasePrice = Integer.parseInt(input);
+        int purchasePrice = checkNumber(input);
+        checkUnit(purchasePrice);
         return purchasePrice;
     }
 
@@ -15,5 +16,18 @@ public class Input {
             throw new IllegalArgumentException("[ERROR] 입력이 비어있습니다.");
     }
 
+    private static int checkNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
+        }
+    }
 
+    private static void checkUnit(int number) {
+        if (number < 1000)
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상이어야 합니다." + number);
+        if(number % 1000 != 0)
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다." + number);
+    }
 }
