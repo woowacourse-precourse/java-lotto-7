7b5c0279a1235237eee6934ec341ca4852d0ac78
SeongJunChun <csj991210@naver.com>
2024-11-04 13:51:21 +0900

    test: 로또 당첨 결과 저장 기능 테스트

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoResultTest {
+
+    @DisplayName("당첨 결과에 따른 당첨금 합계 테스트")
+    @Test
+    void totalPrizeTest() {
+        List<Rank> ranks = List.of(Rank.FIRST, Rank.THIRD, Rank.FIFTH);
+        LottoResult result = LottoResult.createResult(ranks);
+        assertThat(result.getTotalPrize()).isEqualTo(2_001_505_000);
+    }
+
+    @DisplayName("당첨 결과에 따른 수익률 테스트")
+    @Test
+    void revenueRateTest() {
+        List<Rank> ranks = List.of(Rank.FIRST, Rank.THIRD, Rank.FIFTH);
+        LottoResult result = LottoResult.createResult(ranks);
+        int payment = 5000;
+        assertThat(result.calculateRevenueRate(payment)).isEqualTo(40_030_100);
+    }
+
+}
