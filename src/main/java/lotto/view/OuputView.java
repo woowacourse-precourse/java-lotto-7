80cbad4d73606e0fa31c420f54d74f34c0c81b06
wvssm <neoramiregi@gmail.com>
2024-11-04 16:50:56 +0900

    feat: 로또 구입 금액 입력 안내문을 띄운다

---
+++ src/main/java/lotto/view/OuputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OuputView {
+    private static final String LOTTO_COST_PROMPT = "구입금액을 입력해 주세요.";
+
+    public void displayLottoCostPrompt(){
+        System.out.println(LOTTO_COST_PROMPT);
+    }
+}
