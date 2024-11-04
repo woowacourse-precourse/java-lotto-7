bee3381d42de7b22c68a4ae9d261da8db13afe11
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 20:37:46 +0900

    feat: 미사용 객체 선언 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,12 @@
 package lotto;
 
 import lotto.controller.LottoController;
-import lotto.repository.LottoRepository;
-import lotto.service.LottoService;
 import lotto.view.ConsoleView;
 
 public class Application {
     public static void main(String[] args) {
         ConsoleView consoleView = ConsoleView.getInstance();
-        LottoRepository lottoRepository = new LottoRepository();
-        LottoService lottoService = new LottoService(lottoRepository);
-        LottoController lottoController = new LottoController(consoleView, lottoService);
+        LottoController lottoController = new LottoController(consoleView);
 
         lottoController.run();
 
