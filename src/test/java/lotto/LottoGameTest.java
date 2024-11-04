bd83b3bbb7868d181124c215451bf067b868fc61
bjy <qkswnsdud@gmail.com>
2024-11-04 18:49:50 +0900

    feat(WinningNumberTestCord): LottoGameTest 테스트 코드 추가
    LottoGame 클레스 내의 당첨번호 입력받는 기능에 대한 테스트 코드를
    추가했습니다.

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoGameTest {
@@ -16,4 +18,38 @@ class LottoGameTest {
         assertThatThrownBy(() -> validator.validateAmount(2500, LOTTO_PRICE))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("당첨 번호의 개수가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void 당첨_번호의_개수가_6개가_아니면_예외가_발생한다() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> validator.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 1보다 작거나 45보다 크면 예외가 발생한다.")
+    @Test
+    void 당첨_번호가_1보다_작거나_45보다_크면_예외가_발생한다() {
+        List<Integer> winningNumbers = List.of(-1, 0, 1, 2, 3, 46);
+        assertThatThrownBy(() -> validator.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복된 당첨 번호가 있으면 예외가 발생한다.")
+    @Test
+    void 중복된_당첨_번호가_있으면_예외가_발생한다() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 5);
+        assertThatThrownBy(() -> validator.validateWinningNumbers(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 콤마로 구분되지 않은 숫자 문자열이 들어오면 예외가 발생한다.")
+    @Test
+    void 당첨_번호가_콤마로_구분되지_않은_숫자_문자열이_들어오면_예외가_발생한다() {
+        String input = "1,2,3,4,5;6";
+        assertThatThrownBy(() -> validator.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
