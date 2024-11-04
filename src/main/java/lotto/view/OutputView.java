fd84985d27c7948a63aae387e3fc93e0a4c2c9ba
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-04 19:00:11 +0900

    feat(output): 당첨 내역 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,28 @@
 package lotto.view;
 
+import java.util.Map;
+import lotto.domain.Reward;
 import lotto.domain.Tickets;
+import lotto.domain.TotalResult;
 
 public class OutputView {
 
     private static final String ENTER = "\n";
     private static final String LOTTERY_AMOUNT_TAIL_MESSAGE = "개를 구매했습니다.";
-    private static final String RESULT_HEADER = "당첨 통계\n";
+    private static final String RESULT_HEADER = "\n당첨 통계\n---";
+    private static final String STATISTICS_FORMAT = "%d개 일치 (%,d원) - %d개\n";
+    private static final String STATISTICS_SECOND_PLACE_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    private static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %.1f%%입니다.";
 
     public void printGeneratedTickets(Tickets tickets) {
         int ticketAmount = tickets.getTicketAmount();
         System.out.println(ENTER + ticketAmount + LOTTERY_AMOUNT_TAIL_MESSAGE);
         tickets.getUnmodifiableTickets().forEach(ticket -> System.out.println(ticket.getUnmodifiableNumbers()));
+    }
+
+    public void printResult(TotalResult totalResult) {
+        System.out.println(RESULT_HEADER);
+        Map<Reward, Integer> result = totalResult.getUnmodifiableTotalResult();
+        result.forEach(this::printEachStatistic);
     }
 }
