e033056937d1a2a248d2de41f796c20a4adefb33
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 16:02:04 +0900

    feat, docs: 로또 번호와 보너스 번호에 대한 검증 추가
    - 로또 번호의 개수가 잘못된 경우
    - 로또 번호에 중복이 있는 경우
    - 로또 번호가 잘못된 범위의 숫자인 경우
    - 보너스 번호가 당첨 번호와 겹치는 경우
    - 보너스 번호가 잘못된 범위의 숫자인 경우
    - 기능 구현에 따른 README 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -2,12 +2,18 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import lotto.exception.ExceptionMessage;
 
 public class LottoMachine {
+    private static final Integer MINIMUM_LOTTO_NUMBER = 1;
+    private static final Integer MAXIMUM_LOTTO_NUMBER = 45;
+    private static final Integer LOTTO_NUMBER_COUNT = 6;
+
     private final List<Integer> numbers;
     private final Integer bonusNumber;
 
     public LottoMachine(List<Integer> numbers, Integer bonusNumber) {
+        validate(numbers, bonusNumber);
         this.numbers = new ArrayList<>(numbers);
         this.bonusNumber = bonusNumber;
     }
@@ -18,5 +24,33 @@ public class LottoMachine {
 
     public Integer getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, Integer bonusNumber) {
+        validateNumbers(numbers);
+        validateBonusNumber(numbers, bonusNumber);
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            validateNumber(number);
+        }
+
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_NUMBER_LOTTO.getMessage());
+        }
+    }
+
+    private void validateNumber(Integer number) {
+        if (number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, Integer bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_BONUS_NUMBER.getMessage());
+        }
+        validateNumber(bonusNumber);
     }
 }
--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -3,7 +3,9 @@ package lotto.exception;
 public enum ExceptionMessage {
     TOO_MANY_NUMBERS("로또 번호는 6개여야 합니다."),
     DUPLICATED_NUMBER_LOTTO("로또에 중복된 번호가 있습니다."),
-    INVALID_PURCHASE_PRICE("잘못된 단위의 가격입니다.");
+    INVALID_PURCHASE_PRICE("잘못된 단위의 가격입니다."),
+    INVALID_LOTTO_NUMBER("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATED_BONUS_NUMBER("보너스 번호에 중복된 번호가 있습니다.");
 
     private final String message;
 
