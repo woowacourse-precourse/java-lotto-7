09fbb33d17bfd7058d859a8f408e620c64ded823
haebyun <bhk4392@gmail.com>
2024-11-04 15:11:43 +0900

    feat(OutputFormatter): 당첨 결과 리스트 출력 포맷팅 메서드 추가
    - formatWinningResults 메서드 추가하여 당첨 결과 리스트를 형식에 맞춰 반환
    - 각 등수별 당첨 개수를 출력하는 formatWinningDescription 메서드와 함께 사용
    - 보너스 매칭 여부에 따라 다른 포맷을 적용하여 반환

--- src/main/java/lotto/view/output/util/OutputFormatter.java
+++ src/main/java/lotto/view/output/util/OutputFormatter.java
@@ -2,11 +2,15 @@ package lotto.view.output.util;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.domain.vo.LottoRank;
 import lotto.dto.response.PurchasedLottoDTO;
+import lotto.dto.response.WinningResultDTO;
 
 public final class OutputFormatter {
     private static final String PURCHASE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
     private static final String RESULT_PREFIX = "당첨 통계\n---\n";
+    private static final String WINNING_DESCRIPTION_WITH_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private static final String WINNING_DESCRIPTION_FORMAT = "%d개 일치 (%,d원) - %d개";
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
     public static String formatPurchaseLottoCount(Integer count) {
@@ -21,5 +25,28 @@ public final class OutputFormatter {
 
     public static String formatResultPrefix() {
         return RESULT_PREFIX;
+    }
+
+    public static String formatWinningResults(List<WinningResultDTO> results) {
+        return results.stream()
+                .map(dto -> formatWinningDescription(dto.rank(), dto.count()))
+                .collect(Collectors.joining(LINE_SEPARATOR)) + LINE_SEPARATOR;
+    }
+
+    private static String formatWinningDescription(LottoRank rank, int winCount) {
+        if (rank.isBonusMatch()) {
+            return String.format(
+                    WINNING_DESCRIPTION_WITH_BONUS_FORMAT,
+                    rank.getMatchingCount(),
+                    rank.getPrize(),
+                    winCount
+            );
+        }
+        return String.format(
+                WINNING_DESCRIPTION_FORMAT,
+                rank.getMatchingCount(),
+                rank.getPrize(),
+                winCount
+        );
     }
 }
