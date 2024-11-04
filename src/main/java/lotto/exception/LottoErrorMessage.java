4b1376879f5ab0cfb1716696af3d634adf6a3f31
이준범 <jach1206@naver.com>
2024-11-04 21:10:45 +0900

    style: 에러 문구 수정

--- src/main/java/lotto/exception/LottoErrorMessage.java
+++ src/main/java/lotto/exception/LottoErrorMessage.java
@@ -5,7 +5,7 @@ public enum LottoErrorMessage {
     IS_NOT_POSSIBLE_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     IS_NOT_CORRECT_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
     HAVE_DUPLICATED_NUMBER("[ERROR] 로또 번호 중 중복되는 숫자가 존재하면 안 됩니다."),
-    SEPARATE_ONLY_COMMA("[ERROR] 당첨 번호는 쉼표(,)를 기준으로 구분합니다.");
+    SEPARATE_ONLY_COMMA("[ERROR] 당첨 번호는 공백 없이 숫자를 쉼표(,)를 기준으로 구분합니다.");
 
     private final String message;
 
