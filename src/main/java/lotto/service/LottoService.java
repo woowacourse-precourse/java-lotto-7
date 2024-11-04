500cdd476410e7844f127db9de99267fd705d48c
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 17:52:54 +0900

    feat: 로또 당첨 번호 등록 서비스 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lotto.domain.Exchanger;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.LottoMachine;
 
 public class LottoService {
     private final LottoGenerator lottoGenerator;
@@ -16,5 +17,9 @@ public class LottoService {
 
     public List<Lotto> purchaseLotto(Long purchasePrice) {
         return lottoGenerator.purchaseLotto(purchasePrice);
+    }
+
+    public LottoMachine generateLottoMachine(List<Integer> numbers, Integer bonusNumber) {
+        return new LottoMachine(numbers, bonusNumber);
     }
 }
