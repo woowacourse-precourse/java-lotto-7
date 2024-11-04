0a0eafd45665e68b31d09861e034723ab1ec338b
agi1004 <agi1009@naver.com>
2024-11-04 22:17:00 +0900

    feat: 프로그램 실행 클래스

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.controller.ControllerImpl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = ControllerImpl.getInstance();
+        controller.run();
     }
 }
