1ac14f1ea4b250bff652fc4fa1ed420d33522119
seunghee <seunghee_chae3@tmax.co.kr>
2024-11-04 19:00:19 +0900

    feat(InputView): 로또 금액 입력값 없을 시 유효성 검사

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,25 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Objects;
+
 public class InputView {
 
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
 
     public static void printPurchaseAmountInputMessage() {
         System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+    }
+
+    public static int getPurchaseAmount() {
+        String input = Console.readLine();
+        validateInputIsNull(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateInputIsNull(String input) {
+        if (Objects.isNull(input) || input.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
+        }
     }
 }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,4 +1,33 @@
 package lotto.view;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
 public class InputViewTest {
+
+    private final InputStream originalIn = System.in;   // 원래의 System.in 저장
+
+    @AfterEach
+    void restoreSystemIn() {
+        // 각 테스트 이후 원래 System.in 복원 및 Scanner 종료
+        System.setIn(originalIn);
+        Console.close();    // Scanner를 종료하여 초기화
+    }
+
+    @Test
+    void 입력_값_없을시_에러_발생() {
+        // given
+        String invalidInput = "\n";
+        System.setIn(new ByteArrayInputStream(invalidInput.getBytes()));
+
+        // when, then
+        assertThatThrownBy(InputView::getPurchaseAmount)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력값이 없습니다.");
+    }
 }
