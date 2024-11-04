d8fece45e3515675a529e5a6a621b6766b48d86a
KJungW <sagakkb0707@naver.com>
2024-11-04 19:26:34 +0900

    refactor(input): 입력기능에 대한 불필요한 로직 제거
    변경 내용
    - 입력 기능에서 불필요한 null 검증 로직을 제거
    
    변경 이유
    - Console.readLine()이 null을 리턴하지 않기 때문에
      불필요해진 null 검증 로직을 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,14 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Optional;
 
 public class InputView {
 
-    private static final String EMPTY_INPUT = "";
-
     public String inputContent() {
-        Optional<String> input = Optional.of(Console.readLine());
-        return input.orElse(EMPTY_INPUT);
+        return Console.readLine();
     }
 }
