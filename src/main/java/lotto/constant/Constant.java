6b285c0072437983f66c8cd816177ba0c7b0057f
Jo Eungyeong <joeungyeong23@gmail.com>
2024-11-04 13:49:07 +0900

    refactor: 당첨금 상수로 정의

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -6,4 +6,9 @@ public class Constant {
     public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int THREE_MATCHES_PRIZE = 5000;
+    public static final int FOUR_MATCHES_PRIZE = 50000;
+    public static final int FIVE_MATCHES_PRIZE = 1500000;
+    public static final int FIVE_MATCHES_BONUS_MATCH_PRIZE = 30000000;
+    public static final int SIX_MATCHES_PRIZE = 2000000000;
 }
