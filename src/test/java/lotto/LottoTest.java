5d98b35f56b2ceceb2341b15e116184d8122a913
bee9827 <ehfrhfo9494@naver.com>
2024-11-04 12:06:42 +0900

    refactor: Use parameterized test
    여러개의 테스트 케이스를 @ParameterizedTest를 이용하여 전달한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,7 +3,9 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -13,9 +15,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+    static final Lotto winner = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    static final List<List<Integer>> rangeExceptionNumbers = List.of(
+            List.of(1, 2, 3, 4, 5, Lotto.NUMBER_MAX + 1),
+            List.of(1, 2, 3, 4, 5, Lotto.NUMBER_MIN -1)
+    );
+    static final List<List<Integer>> sizeExceptionNumbers = List.of(
+            List.of(1, 2, 3, 4, 5),
+            List.of(1, 2, 3, 4, 5, 6, 7)
+    );
+
+    @ParameterizedTest
+    @FieldSource("sizeExceptionNumbers")
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,21 +39,20 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 번호_범위_예외() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+    @ParameterizedTest
+    @FieldSource("rangeExceptionNumbers")
+    void 번호_범위_예외(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 번호_출력() {
-        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)).toString()).contains("[1, 2, 3, 4, 5, 6]");
+        assertThat(winner.toString()).contains("[1, 2, 3, 4, 5, 6]");
     }
 
     @Test
-    void 번호_정렬(){
+    void 번호_정렬() {
         assertThat(new Lotto(List.of(5, 4, 3, 2, 1, 6)).toString()).contains("[1, 2, 3, 4, 5, 6]");
     }
 
