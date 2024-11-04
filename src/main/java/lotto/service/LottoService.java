5902d07fbb18c1e6cddad258ddbdf70d6e74d8cb
YunCheol1 <naro2727@naver.com>
2024-11-04 13:11:21 +0900

    feat(LottoService):당첨번호와 보너스번호 저장하는 기능 구현 -로또의 당첨 번호와 보너스 번호를 파싱하고 저장하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,21 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
-import lotto.model.LottoWinningNumbers;
+import lotto.model.WinningLotto;
 
 public class LottoService {
     private final List<Lotto> lottoTickets = new ArrayList<>();
-    private LottoWinningNumbers winningLotto;
+    private WinningLotto winningLotto;
 
     public void issueLottoTickets(int money) {
         for (int i = 0; i < money; i++) {
             lottoTickets.add(new Lotto(generateLottoNumbers()));
         }
-
     }
 
     private List<Integer> generateLottoNumbers() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6)); // 복사하여 수정 가능한 리스트로 변환
         sortLottoNumbers(numbers);
         return numbers;
     }
@@ -27,4 +26,20 @@ public class LottoService {
         numbers.sort(Integer::compareTo);
         return numbers;
     }
+
+    public void setWinningNumbers(String winningNumbers, int bonusNumber) {
+        List<Integer> winningNumberList = parseWinningNumbers(winningNumbers);
+        this.winningLotto = new WinningLotto(winningNumberList, bonusNumber);
+    }
+
+    private List<Integer> parseWinningNumbers(String winningNumbers) {
+        String[] numberStrings = winningNumbers.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String num : numberStrings) {
+            numbers.add(Integer.parseInt(num.trim()));
+        }
+        return numbers;
+    }
+
+
 }
