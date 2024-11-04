3f9198d17af76b9d27f8d60572af0525ae017b28
Awon01 <bec00@naver.com>
2024-11-04 20:47:45 +0900

    feat(utils): 입력 문자열 숫자 리스트로 전환

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -3,6 +3,8 @@ package lotto.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -12,6 +14,14 @@ public class Utils {
 
     public static void sortListNaturalOrder(List<Integer> list) {
         Collections.sort(list);
+    }
+
+    public static int[] stringToIntArray(String string) {
+        return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static List<Integer> stringToIntegerList(String string) {
+        return Arrays.stream(stringToIntArray(string)).boxed().collect(Collectors.toList());
     }
 
 }
