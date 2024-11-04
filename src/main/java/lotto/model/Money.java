be58a0fdb4986156ace0e3f0f0eaa40331e21824
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 20:44:15 +0900

    feat(Money): 예외처리 구현

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -9,7 +9,26 @@ public class Money {
     private final int money;
 
     public Money(String stringMoney){
-    this.money = Integer.parseInt(stringMoney);
+        int money=Integer.parseInt(stringMoney);
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int inputValue) {
+        validateZero(inputValue);
+        validateDivideMoney(inputValue);
+    }
+
+    private void validateZero(int inputValue) {
+        if (inputValue == 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
+        }
+    }
+
+    private void validateDivideMoney(int inputValue) {
+        if (inputValue % LOTTO_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위의 금액을 입력해야합니다.");
+        }
     }
 
     public int getMoney(){
