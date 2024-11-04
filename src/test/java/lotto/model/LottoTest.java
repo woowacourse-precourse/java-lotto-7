54948a137eaa4047e8c77131b0cd37999cf12d87
hwangdaesun <hds650588@gmail.com>
2024-11-04 15:55:39 +0900

    test : 예외 메시지 검증 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import static lotto.common.ErrorMessage.LOTTO_NUMBERS_MUST_SIX;
+import static lotto.common.ErrorMessage.LOTTO_NUMBER_DISTINCT;
+import static lotto.common.ErrorMessage.LOTTO_NUMBER_RANGE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,21 +14,24 @@ class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBERS_MUST_SIX.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBER_DISTINCT.getMessage());
     }
 
     @DisplayName("로또 번호의 범위가 1~45가 아니면 예외가 발생한다.")
     @Test
     void 로또_번호가_1에서_45사이가_아니면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(LOTTO_NUMBER_RANGE.getMessage());
     }
 
     @DisplayName("로또 번호는 오름차순으로 생성된다.")
