c0f4aec4126b5abddcb9e28b5eb09d500f6fa4c9
mikekks <thdalsrb79@naver.com>
2024-11-04 11:00:29 +0900

    refactor(LottoConstant): 로또 관련된 상수들 전역변수로 처리

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static lotto.global.ErrorMessage.*;
+import static lotto.global.LottoConstant.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +11,6 @@ import lotto.domain.common.Random;
 public class LottoMachine {
 
 	private static final int LOTTO_PRICE = 1000;
-	private static final int LOTTO_START_NUMBER = 1;
-	private static final int LOTTO_END_NUMBER = 45;
-	private static final int LOTTO_NUMBER_COUNT = 6;
 
 	public List<Lotto> purchaseLottos(Money money, Random randoms) {
 		validatePurchaseMoneyUnit(money);
@@ -32,7 +30,7 @@ public class LottoMachine {
 	private List<Lotto> generateLottos(int lottoCount, Random randoms) {
 		List<Lotto> lottos = new ArrayList<>();
 		for(int i = 0; i< lottoCount; i++) {
-			List<Integer> lottoNumbers = randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_NUMBER_COUNT);
+			List<Integer> lottoNumbers = randoms.pickUniqueNumbersInRange(LOTTO_MINIMUM_NUMBER, LOTTO_MAXIMUM_NUMBER, LOTTO_NUMBER_COUNT);
 			lottos.add(new Lotto(lottoNumbers));
 		}
 		return lottos;
---
+++ src/main/java/lotto/global/LottoConstant.java
@@ -0,0 +1,8 @@
+package lotto.global;
+
+public final class LottoConstant {
+
+	public static final int LOTTO_MINIMUM_NUMBER = 1;
+	public static final int LOTTO_MAXIMUM_NUMBER = 45;
+	public static final int LOTTO_NUMBER_COUNT = 6;
+}
