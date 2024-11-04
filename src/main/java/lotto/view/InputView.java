32145a08293867a989fb5f15222fdbf84377fc4d
sonshn <sonshn@naver.com>
2024-11-04 19:42:16 +0900

    fix(InputView): InputView 클래스 내부의 메서드를 static 메서드로 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,22 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String getPurchaseAmount() {
+    public static String getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String getLotteryWinningNumbers() {
+    public static String getLotteryWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public String getLotteryBonusNumber() {
+    public static String getLotteryBonusNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
-    public void closeConsole() {
+    public static void closeConsole() {
         Console.close();
     }
 
