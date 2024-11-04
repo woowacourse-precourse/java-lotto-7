ed6ebe8bb805d475c90d716ef69be0f1d2012c07
김호연 <yun1094@naver.com>
2024-11-04 21:25:23 +0900

    하드 코딩 진행

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -49,11 +49,18 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
-            runException("1000j");
+                runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
+    @Test
+    void testIsNotDivisibleByThousand() {
+        assertSimpleTest(() -> {
+            runException("8500");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
