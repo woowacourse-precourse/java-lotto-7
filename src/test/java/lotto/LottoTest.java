a9b81fc09d8776502d9fb6a264616e32b36e3dc6
88dldl <chltnrgml0999@gmail.com>
2024-11-04 15:59:47 +0900

    feat(LottoTest): 예외처리 관련 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,25 @@
 package lotto;
 
+import lotto.error.ErrorStatus;
+import lotto.service.Lotto;
+import lotto.service.LottoTickets;
+import lotto.utils.StringParser;
+import lotto.view.InputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private static Stream<String> invalidInput() {
+        return Stream.of(null, "");
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +33,39 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("입력값이 null이거나 빈 문자열일 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidInput")
+    void 입력값이_NULL이거나_빈_문자열이면_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> InputView.getValidMoney(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorStatus.NULL_OR_EMPTY_INPUT.getMessage());
+    }
+
+    @DisplayName("로또 번호가 음수이면 예외가 발생한다.")
+    @Test
+    void 로또번호가_음수이면_예외가_발생한다() {
+        String winningNumbers = "1,2,3,4,5,-6";
+        assertThatThrownBy(() -> StringParser.parseStringToIntegerList(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorStatus.INVALID_NEGATIVE_NUMBER.getMessage());
+    }
+
+    @DisplayName("로또 금액이 1000원 단위가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_금액이_1000원_단위가_아니면_예외가_발생한다() {
+        LottoTickets lottoTickets = new LottoTickets();
+        int money = 1500;
+        assertThatThrownBy(() -> lottoTickets.buyTicket(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorStatus.INVALID_MONEY_AMOUNT.getMessage());
+    }
+
+    @DisplayName("로또 번호가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_6개가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorStatus.INVALID_NUMBER_SIZE.getMessage());
+    }
 }
