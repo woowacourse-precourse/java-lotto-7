62e5906455a966d93fc99d9308217bd28e9ead0a
Chan Mi <rkdcksal02@gmail.com>
2024-11-04 21:30:08 +0900

    style(LottoResultTest): 불필요한 코드 제거

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoResultTest {
 
