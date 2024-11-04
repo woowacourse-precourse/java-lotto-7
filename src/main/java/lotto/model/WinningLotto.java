4df59c9f944c1811241e93ad1ebef139923433c4
wooqqq <sge0225@naver.com>
2024-11-04 20:30:30 +0900

    refactor: 테스트 코드를 통한 유효성 검사 추가(당첨 번호와 보너스 번호 중복)

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,11 +1,14 @@
 package lotto.model;
 
+import lotto.util.Validator;
+
 public class WinningLotto {
 
     private final Lotto winningNumbers;
     private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto winningNumbers, BonusNumber bonusNumber) {
+        validate(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -15,5 +18,9 @@ public class WinningLotto {
         boolean containsBonusNumber = lotto.contains(bonusNumber.bonusNumber());
 
         return Rank.valueOf(correctCount, containsBonusNumber);
+    }
+
+    private void validate(Lotto winningNumbers, BonusNumber bonusNumber) {
+        Validator.validateIsDuplicate(winningNumbers, bonusNumber.bonusNumber());
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import lotto.model.BonusNumber;
 import lotto.model.Lotto;
+import lotto.model.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +30,16 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 같으면 예외가 발생한다.")
+    @Test
+    void 당첨번호와_보너스번호가_같으면_예외가_발생한다() {
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber(6); // 동일한 번호
+
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
