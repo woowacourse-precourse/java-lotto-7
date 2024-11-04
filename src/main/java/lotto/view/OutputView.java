0f477ff38e6b51541806fd9fc2335f47a4996c28
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-11-04 15:04:22 +0900

    refactor: 문구 상수화 및 printf 포맷팅 사용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,51 +2,70 @@ package lotto.view;
 
 import java.text.NumberFormat;
 import java.util.List;
+import java.util.Set;
+import lotto.config.LottoConfig;
 import lotto.model.domain.WinningMatch;
 
 public class OutputView {
     public static final NumberFormat THOUSAND_SEPARATOR = NumberFormat.getInstance();
 
+    public final static String BEFORE_PURCHASE = "구입금액을 입력해 주세요.";
+    public final static String COUNT_PURCHASE = "%d개를 구매했습니다.";
+    public final static String WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    public final static String BOUNS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public final static String WINNING_STATISTIC = "당첨 통계";
+    public final static String SEPERATOR = "---";
+    public final static String STATISTIC_INFO = "%s (%s원) - %d개";
+    public final static String TOTAL_RETURN_RATE = "총 수익률은 %s 입니다";
+
     // 메서드 명 길이에 대해 고민해보기
     // 메서드 명
     public void purchaseLottoAmountMesssage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(BEFORE_PURCHASE);
     }
 
     public void purchaseLottoCountMessage(int ticketCount) {
-        System.out.println(ticketCount + "개를 구매했습니다.");
+        System.out.println();
+        System.out.printf(COUNT_PURCHASE, ticketCount);
     }
 
     public void printLottoNumbers(List<String> lottoNumbers) {
         for (String lotterNumber : lottoNumbers) {
             System.out.println(lotterNumber);
         }
-    }
-
-    public void enterWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-    }
-
-    public void enterBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-    }
-
-    public void WinningStatistics() {
-        System.out.println("당첨 통계");
-        System.out.print("---");
         System.out.println();
     }
 
+    public void enterWinningNumbers() {
+        System.out.println(WINNING_NUMBERS);
+    }
+
+    public void enterBonusNumber() {
+        System.out.println();
+        System.out.println(BOUNS_NUMBER);
+
+    }
+
+    public void WinningStatistics() {
+        System.out.println();
+        System.out.println(WINNING_STATISTIC);
+        System.out.println(SEPERATOR);
+    }
+
     public void matchWinningCount(List<Integer> winningCounts) {
-        for (int rank = winningCounts.size(); rank > 0; rank--) {
+        for (int rank = winningCounts.size(); rank > LottoConfig.ZERO; rank--) {
             int winningCount = winningCounts.get(rank - 1);
-            System.out.println(WinningMatch.valueOfRank(rank).getMatchAmount() + " (" + THOUSAND_SEPARATOR.format(
-                    WinningMatch.valueOfRank(rank).getPriceAmount()) + "원) - " + winningCount + "개");
+            System.out.printf(STATISTIC_INFO,
+                    WinningMatch.valueOfRank(rank).getMatchAmount(),
+                    THOUSAND_SEPARATOR.format(WinningMatch.valueOfRank(rank).getPriceAmount()),
+                    winningCount
+            );
+            System.out.println();
         }
     }
 
     public void promptTotalReturnRate(String totalReturnRate) {
-        System.out.println("총 수익률은 " + totalReturnRate + "입니다.");
+        System.out.printf(TOTAL_RETURN_RATE, totalReturnRate);
     }
 
     public void printErrorMessage(String message) {
