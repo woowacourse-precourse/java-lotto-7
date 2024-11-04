d4e64790c5dba24168fa3791c5178112308bb5d3
koojun99 <koojun99@naver.com>
2024-11-04 18:00:54 +0900

    refactor(README): update feature checklist
    - 기능 명세서 업데이트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,6 @@ import static java.util.Arrays.*;
 
 import camp.nextstep.edu.missionutils.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
