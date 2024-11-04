ebb2d45d5e5ccd9b54e491cbb089d3b218378f08
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-11-04 16:41:23 +0900

    fix: 오름차순 정렬을 위해 코드 수정

--- src/main/java/lotto/controller/LottoNumberGenerator.java
+++ src/main/java/lotto/controller/LottoNumberGenerator.java
@@ -2,16 +2,22 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static lotto.constants.ModelContstants.*;
+import static lotto.constants.Constants.*;
 
 public class LottoNumberGenerator {
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(
-                LOTTO_STARTING_RANGE.getValue(),
-                LOTTO_END_RANGE.getValue(),
-                LOTTO_COUNT.getValue()
+        List<Integer> lottoNumbers = new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(
+                        LOTTO_STARTING_RANGE.getValue(),
+                        LOTTO_END_RANGE.getValue(),
+                        LOTTO_COUNT.getValue()
+                )
         );
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
 }
