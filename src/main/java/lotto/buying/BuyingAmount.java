b63e89885ef367c0308bc055249c258e95057dcd
sodychoe <sodychoe@icloud.com>
2024-11-04 16:51:08 +0900

    feat(BuyingAmount) : 천원 단위가 아닌 구입금액 예외처리 구현

--- src/main/java/lotto/buying/BuyingAmount.java
+++ src/main/java/lotto/buying/BuyingAmount.java
@@ -3,10 +3,13 @@ package lotto.buying;
 import java.util.Objects;
 
 public class BuyingAmount {
+    public static final int LOTTO_AMOUNT_UNIT = 1000;
+
     private final int buyingAmount;
 
     private BuyingAmount(int buyingAmount) {
         validateLessThanMinimumAmount(buyingAmount);
+        validateNotAmountUnit(buyingAmount);
         this.buyingAmount = buyingAmount;
     }
 
@@ -17,6 +20,12 @@ public class BuyingAmount {
     private void validateLessThanMinimumAmount(int buyingNumber) {
         if (buyingNumber < 1000) {
             throw new IllegalArgumentException("[ERROR] 구입 금액이 천원 미만입니다.");
+        }
+    }
+
+    private void validateNotAmountUnit(int buyingAmount) {
+        if (Math.floorMod(buyingAmount, LOTTO_AMOUNT_UNIT) != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 천원 단위여야 합니다.");
         }
     }
 
