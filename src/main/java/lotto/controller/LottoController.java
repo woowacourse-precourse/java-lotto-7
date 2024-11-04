15de5b704397ff8c470d01a7c51d6f47aad4d973
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 21:31:27 +0900

    feat: controller에 서비스 흐름대로 코드 작성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 import lotto.view.LottoOutput;
 import lotto.entity.Lottos;
 
@@ -11,5 +13,43 @@ public class LottoController {
     public LottoController(Lottos lottos, LottoOutput output) {
         this.lottos = lottos;
         this.lottoOutput = output;
+    }
+
+    public void startLotto() {
+        // to output -> "구입 금액을 입력해주세요."
+        String moneyString = Console.readLine();
+        Long money = changeStringToMoney(moneyString);
+        lottos.buyLottos(money);
+
+        // to output -> "X개를 구매했습니다. \n~~~  \n~~~"
+
+        // to output -> "당첨 번호를 입력해 주세요."
+        String winningNumbersString = Console.readLine();
+        List<Integer> winningNumbers = changeStringToNumberList(winningNumbersString);
+
+        // to output -> "보너스 번호를 입력해 주세요."
+        String bonusNumberString = Console.readLine();
+        Integer bonusNumber = changeStringToNumber(bonusNumberString);
+
+        lottos.setWinningNumbers(winningNumbers, bonusNumber);
+
+        // to output -> "당첨 통계 ~~"
+    }
+
+    private Long changeStringToMoney(String moneyString) {
+        validateMoney(moneyString);
+        return Long.parseLong(moneyString);
+    }
+
+    private void validateMoney(String moneyString) {
+
+    }
+
+    private List<Integer> changeStringToNumberList(String numbersString) {
+
+    }
+
+    private Integer changeStringToNumber(String numberString) {
+
     }
 }
