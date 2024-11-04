02f1e31c54ecc4f1a80ad5d3da0bcd11af57bfa9
Minuring <mw9505@gmail.com>
2024-11-04 16:08:32 +0900

    refactor(lotto): 검증 유틸을 사용하도록 변경
    검증 유틸의 검증 로직을 재사용하도록 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,11 +3,9 @@ package lotto.domain;
 import static lotto.constant.Error.DUPLICATED_LOTTO_NUMBERS;
 import static lotto.constant.Error.RANGE_LOTTO_NUMBER;
 import static lotto.constant.Error.SIZE_LOTTO_NUMBERS;
-import static lotto.constant.LottoConstant.MAX_NUMBER;
-import static lotto.constant.LottoConstant.MIN_NUMBER;
-import static lotto.constant.LottoConstant.SIZE_NUMBERS;
 
 import java.util.List;
+import lotto.validation.NumbersValidation;
 
 public class Lotto {
 
@@ -23,16 +21,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != SIZE_NUMBERS) {
-            throw new IllegalArgumentException(SIZE_LOTTO_NUMBERS);
-        }
-
-        if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS);
-        }
-
-        if (numbers.stream().anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER)) {
-            throw new IllegalArgumentException(RANGE_LOTTO_NUMBER);
-        }
+        NumbersValidation.validateSize(numbers, SIZE_LOTTO_NUMBERS);
+        NumbersValidation.validateDuplicate(numbers, DUPLICATED_LOTTO_NUMBERS);
+        NumbersValidation.validateAllRange(numbers, RANGE_LOTTO_NUMBER);
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.Stream;
+import lotto.constant.Error;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,33 +24,38 @@ public class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(Error.SIZE_LOTTO_NUMBERS);
     }
 
     @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(Error.SIZE_LOTTO_NUMBERS);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(Error.DUPLICATED_LOTTO_NUMBERS);
     }
 
     @DisplayName("로또 번호가 1보다 작으면 예외가 발생한다.")
     @Test
     void 로또_번호가_1보다_작으면_예외가_발생한다() throws Exception {
         assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(Error.RANGE_LOTTO_NUMBER);
     }
 
     @Test
     void 로또_번호가_45보다_크면_예외가_발생한다() throws Exception {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(Error.RANGE_LOTTO_NUMBER);
     }
 }
