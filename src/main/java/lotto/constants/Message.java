79a72a7f1827c57c354fc3dfb7feb3a10a64c6a4
codrin2 <hssang127@gmail.com>
2024-11-04 23:16:30 +0900

    refactor(Message): 일반 메시지 상수 처리

---
+++ src/main/java/lotto/constants/Message.java
@@ -0,0 +1,8 @@
+package lotto.constants;
+
+public class Message {
+    public static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    public static final String PURCHASED_TICKET_COUNT = "{count}개를 구매했습니다.";
+}
