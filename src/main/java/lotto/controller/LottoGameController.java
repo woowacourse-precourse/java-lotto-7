02ae65039549ce3c65dfcba47c1647f2e22a1845
JoonKyo <joonkyolee@gmail.com>
2024-11-04 09:01:28 +0900

    feat(LottoGameController): 사용자가 구입금액에 대해 적절한 금액을 입력할 때까지 반복해서 입력받는 메서드 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,11 +1,19 @@
 package lotto.controller;
 
 import java.util.function.Supplier;
+import lotto.domain.Money;
 import lotto.exception.InputException;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameController {
     public void run() {
+        Money money = repeatUntilReadValidInput(this::readMoney);
+    }
+
+    private Money readMoney() {
+        OutputView.askPurchasePrice();
+        return Money.from(InputView.readInput());
     }
 
     private <T> T repeatUntilReadValidInput(Supplier<T> supplier) {
