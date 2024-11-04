d3c8fdf63a39b3794eaee5e5a0383a9160ef0ffe
goochul <gooch123@naver.com>
2024-11-04 15:31:45 +0900

    feat(Facade): Facade 패턴 추가

---
+++ src/main/java/lotto/Facade.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.List;
+
+public class Facade {
+
+    LottoInput lottoInput;
+    LottoShop lottoShop;
+    LottoWinning lottoWinning;
+
+    public Facade() {
+        this.lottoInput = new LottoInput();
+        this.lottoShop = new LottoShop();
+    }
+
+    public void start() {
+        int money = lottoInput.inputMoneyLoop();
+        List<Lotto> lottos = purchaseLottos(money);
+        List<Integer> winningNumbers = lottoInput.inputWinningNumbersLoop();
+        int bonusNumber = lottoInput.inputBonusNumberLoop();
+        lottoWinning = new LottoWinning(winningNumbers, bonusNumber, lottos);
+        lottoWinning.start();
+    }
+
+    private List<Lotto> purchaseLottos(int money) {
+        List<Lotto> lottos = lottoShop.purchaseLotto(money);
+        for (Lotto lotto : lottos) {
+            lotto.showLottoNumbers(); //로또 번호들을 출력
+        }
+        return lottos;
+    }
+
+}
