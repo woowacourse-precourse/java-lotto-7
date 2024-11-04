e63715510caf7e617075a8d879dfbc99a607ef5d
양효빈 <hyobin3026@naver.com>
2024-11-04 10:01:18 +0900

    feat(LottoWinningOutputViewFactory): Add factory for lotto winning output view

---
+++ src/main/java/lotto/view/lottoWinningView/LottoWinningOutputViewFactory.java
@@ -0,0 +1,14 @@
+package lotto.view.lottoWinningView;
+
+import lotto.util.NumberFormatterWithComma;
+import lotto.util.NumberFormatterWithPercentage;
+
+public class LottoWinningOutputViewFactory {
+    public WinningLottoOutputView createWinningLottoOutputView() {
+        return new WinningLottoOutputView(new NumberFormatterWithComma());
+    }
+
+    public LottoProfitOutputView createLottoProfitOutputView() {
+        return new LottoProfitOutputView(new NumberFormatterWithPercentage());
+    }
+}
