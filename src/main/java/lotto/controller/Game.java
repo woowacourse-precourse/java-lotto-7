db29ee37071b16d93d9c038a2e980c8ae578190a
hyuuuun <pam4522@gmail.com>
2024-11-04 21:45:47 +0900

    feat (Game) : 게임실행 기능

---
+++ src/main/java/lotto/controller/Game.java
@@ -0,0 +1,47 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningLotto;
+import lotto.service.Calculating;
+import lotto.service.Issuing;
+import lotto.service.Ranking;
+import lotto.view.Input;
+import lotto.view.Output;
+
+import java.util.List;
+
+
+public class Game {
+
+    private LottoTicket lottoTicket;
+    private WinningLotto winLotto;
+
+    public void run() {
+        before();
+        start();
+        end();
+    }
+
+    private void before() {
+        Input.askPrice();
+        lottoTicket = new LottoTicket(Input.number());
+        Output.printCount(lottoTicket.getCount());
+    }
+    private void start(){
+        Issuing.lottoTickets(lottoTicket);
+        for(Lotto lotto : lottoTicket.getLottoTickets()){
+            Output.printNumbers(lotto.getNumbers());
+        }
+        Input.askWinNumbers();
+        List<Integer> winNumbers = Input.numbers();
+        Input.askBonusNumber();
+        winLotto = new WinningLotto(winNumbers,Input.number());
+    }
+    private void end(){
+        Ranking.matchLotto(lottoTicket,winLotto);
+        Output.printResult(lottoTicket.getResult());
+        Output.printEarningRate(Calculating.earningRate(lottoTicket));
+    }
+}
+
