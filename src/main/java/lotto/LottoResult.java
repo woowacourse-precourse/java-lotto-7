f8e82836cecefc1be3bd0dbfb36592e8f822fd1f
Jungwoo Chu <cjw980930@gmail.com>
2024-11-04 19:26:10 +0900

    refactor: 매개변수명 bonus 를 bonusNumber로 바꿔서 통일시킴

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoResult {
-    public Map<Rank,Integer> getFinalResult(List<Lotto> lottoTickets, List<Integer> winningNumbers, int bonus){
+    public Map<Rank,Integer> getFinalResult(List<Lotto> lottoTickets, List<Integer> winningNumbers, int bonusNumber){
         Map<Rank,Integer> result = new HashMap<>();
         for(Rank rank : Rank.values()){
             result.put(rank,0);
         }
         for (Lotto lotto : lottoTickets){
-            Rank rank = lotto.getResult(winningNumbers,bonus);
+            Rank rank = lotto.getResult(winningNumbers,bonusNumber);
             result.put(rank,result.get(rank)+1);
         }
         return result;
