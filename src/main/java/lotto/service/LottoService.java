6515510bbba6f1e8f3fba1b02794e229998db8b3
Jewan1120 <hawaii1468@gmail.com>
2024-11-04 18:57:49 +0900

    feat(Service): 모든 로또의 등수를 계산하는 서비스 로직 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,6 +3,7 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.enums.Constants;
 
 public class LottoService {
@@ -11,11 +12,20 @@ public class LottoService {
 
     public List<Integer> getLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(Constants.LOTTO_LOWER_BOUND.getValue(),
-                                                Constants.LOTTO_UPPER_BOUND.getValue(),
-                                                Constants.LOTTO_NUMBER_SIZE.getValue());
+                Constants.LOTTO_UPPER_BOUND.getValue(),
+                Constants.LOTTO_NUMBER_SIZE.getValue());
     }
 
     public List<Integer> getWinningNumbers(String inputWinningNumbers) {
         return Arrays.stream(inputWinningNumbers.split(DELIMITER)).map(Integer::parseInt).toList();
+    }
+
+    public List<Integer> calculateLottoResult(List<Lotto> lottos, List<Integer> winningNumbers, int bounsNumber) {
+        int[] lottoResult = new int[6];
+        for (Lotto lotto : lottos) {
+            int rank = lotto.calculateRank(winningNumbers, bounsNumber);
+            lottoResult[rank]++;
+        }
+        return Arrays.stream(lottoResult).boxed().toList();
     }
 }
