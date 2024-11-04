7e4e0f19cae7faeeb5b1bc09475c6cc67abcb60c
Lee Chae Rin <chaerin0916@gmail.com>
2024-11-04 11:45:23 +0900

    feat: 구매에 따른 로또 번호 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,8 @@
 package lotto;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -7,6 +10,16 @@ public class Application {
         int totalLotto = Integer.parseInt(Console.readLine());
         if(totalLotto % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+        }
+        System.out.println();
+
+        int cntLotto = totalLotto / 1000;
+        System.out.println(cntLotto + "개를 구매했습니다.");
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < cntLotto; i++) {
+            Lotto randLotto = new Lotto(Randoms.pickUniqueNumbersInRange(1,45, 6));
+            lottos.add(randLotto);
+            System.out.println(randLotto.toString());
         }
     }
 }
