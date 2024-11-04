6d84c1cf2536eac48df08cd85d6d94ed5558e1b4
threeony <threeknot1250@cau.ac.kr>
2024-11-04 23:07:37 +0900

    refactor: format code

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -7,7 +7,7 @@ public enum Rank {
     THIRD("5개 일치", 1500000),
     FOURTH("4개 일치", 50000),
     FIFTH("3개 일치", 5000),
-    NONE("",0);
+    NONE("", 0);
     private final String description;
     private final Integer prize;
 
