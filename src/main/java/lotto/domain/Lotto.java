5aa1ce5bb7dfc82c4c57f040aa9c59cf9d8a8c15
Chan Mi <rkdcksal02@gmail.com>
2024-11-04 13:18:26 +0900

    feat(Lotto): 로또 번호 입력 및 유효성 검사 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    public LottoRank match(List<Integer> winningNumbers, int bonusNumber) {
+        int matchCount = (int) numbers.stream().filter(winningNumbers::contains).count();
+        boolean matchBonus = numbers.contains(bonusNumber);
+
+        return LottoRank.valueOf(matchCount, matchBonus);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
