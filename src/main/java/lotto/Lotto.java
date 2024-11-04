9605784e1e950919f700a6f19d11f47854dd93f6
JEANS <songkc5671@naver.com>
2024-11-04 23:29:38 +0900

    feat(Lotto) : 당첨번호 List 형으로 저장
    * 다양한 헬퍼함수와, 로또 번호에 대한 Validation을 진행했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,12 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<Integer> numbers) throws IllegalArgumentException {
         validate(numbers);
         this.numbers = numbers;
     }
@@ -14,7 +15,24 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (numbers.stream().distinct().count() < numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다..");
+        }
+        // 각 번호가 1부터 45 사이인지 검증
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public int getNode(int idx) {
+        return numbers.get(idx);
+    }
+
+    // numbers 리스트의 방어적 복사본을 반환하는 getter 메서드
+    public List<Integer> getNumbers() {
+        return new ArrayList<>(numbers); // 방어적 복사
+    }
 }
