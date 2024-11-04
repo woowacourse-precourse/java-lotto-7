18e5827d939af4bb84716be3bddb7e0e47ecb165
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-04 20:33:52 +0900

    feat: 보너스 번호 예외처리 (입력 받은 당첨 번호들 중 하나와 중복된 숫자가 있을 때, 1~45 사이의 숫자가 아닐 때)

---
+++ src/main/java/lotto/model/Bonus.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Bonus {
+    private final int number;
+
+    public Bonus(int number, List<Integer> numbers) {
+        validate(number, numbers);
+        this.number = number;
+    }
+
+    private void validate(int number, List<Integer> numbers) {
+        validateRange(number);
+        validateDuplicateNumber(number, numbers);
+    }
+
+    private void validateRange(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateNumber(int number, List<Integer> numbers) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
