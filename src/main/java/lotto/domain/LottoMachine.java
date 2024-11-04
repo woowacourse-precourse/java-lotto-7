f192f9faf05a9cfadeb20e2243919fda511d7125
sytTest <syt1128@naver.com>
2024-11-04 19:02:00 +0900

    feat(LottoMachine): 무작위 6개의 숫자 뽑기 기능 추가

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+
+public class LottoMachine implements DrawTool{
+
+    private Long leftGames;
+
+    public LottoMachine(Long gamesLeft) {
+        this.leftGames = gamesLeft;
+    }
+
+    @Override
+    public List<Integer> quickPicks() {
+        leftGames--;
+        return null;
+    }
+
+    private List<Integer> mixedEntry() {
+        return pickUniqueNumbersInRange(
+                GameStatus.START_NUMBER.getInt(),
+                GameStatus.END_NUMBER.getInt(),
+                GameStatus.TOTAL_BALL_COUNT.getInt());
+    }
+
+    @Override
+    public Boolean gamesLeft() {
+        return leftGames > GameStatus.LOWEST_GAME_COUNT.get();
+    }
+}
