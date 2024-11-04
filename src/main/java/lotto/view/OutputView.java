df490b06f23bf2e632af4e992efc474b6cf04740
GiHwan2 <sandwich5011@naver.com>
2024-11-04 19:31:39 +0900

    feat(OutputView): 출력을 담당하는 클래스 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,20 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getSortedNumbers());
+        }
+    }
+
+    public void printResults(String result) {
+        System.out.println("---");
+        System.out.println(result);
+    }
+}
