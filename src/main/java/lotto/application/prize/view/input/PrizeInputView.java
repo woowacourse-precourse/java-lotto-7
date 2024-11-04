b2fdd67be00fbb4b7590e1b95c4322bec8f0e1f6
plan11plan <tjtfy7028@gmail.com>
2024-11-04 13:12:31 +0900

    feat(PrizeInputView): 메서드 분리
    - inputPrizeNumber 개발됨
    - inputBonusNumber 개발됨

--- src/main/java/lotto/application/prize/view/input/PrizeInputView.java
+++ src/main/java/lotto/application/prize/view/input/PrizeInputView.java
@@ -7,20 +7,37 @@ import static lotto.application.util.StringConverter.toInts;
 import static lotto.application.util.StringConverter.trim;
 
 import java.util.List;
+import lotto.application.common.OutputPrinter;
 
 public class PrizeInputView {
 
     private final String INPUT_PRIZE_NUMBER = "당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private final OutputPrinter printer;
+
+    public PrizeInputView() {
+        this.printer = new OutputPrinter();
+    }
 
     public PrizeViewRequest initialize() {
-        System.out.println(INPUT_PRIZE_NUMBER);
-        List<Integer> prizeNumber = toInts(split(trim(readLine())));
-
-        System.out.println(INPUT_BONUS_NUMBER);
-        int bonusNumber = toInt(trim(readLine()));
+        List<Integer> prizeNumber = inputPrizeNumber();
+        int bonusNumber = inputBonusNumber();
 
         return new PrizeViewRequest(prizeNumber, bonusNumber);
+    }
+
+    private List<Integer> inputPrizeNumber() {
+        printer.appendWithLine(INPUT_PRIZE_NUMBER);
+        printer.execute();
+
+        return toInts(split(trim(readLine())));
+    }
+
+    private int inputBonusNumber() {
+        printer.appendWithLine(INPUT_BONUS_NUMBER);
+        printer.execute();
+
+        return toInt(trim(readLine()));
     }
 
 }
