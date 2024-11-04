641d24315755df253a10f91a2cc75c61dccd39e8
euni <kongeuni1004@gmail.com>
2024-11-04 20:32:08 +0900

    test: StatisticsController의 수익률 계산 테스트 추가
    - 수익률 계산 메서드의 올바른 동작을 검증하는 테스트 작성
    - 총 당첨금과 구입 금액을 기반으로 수익률 계산 검증

--- src/test/java/lotto/StatisticsControllerTest.java
+++ src/test/java/lotto/StatisticsControllerTest.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class StatisticsControllerTest {
+    @Test
+    @DisplayName("수익률 계산이 올바르게 수행된다.")
+    void 수익률_계산_테스트() {
+        StatisticsController statisticsController = new StatisticsController();
+        double rateOfReturn = statisticsController.calculateRateOfReturn(5000, 8);
+        assertThat(rateOfReturn).isEqualTo(62.5);
+    }
 }
