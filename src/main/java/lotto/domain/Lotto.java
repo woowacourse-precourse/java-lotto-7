1401d1d35ce9ec1b4304c4d87ac3a7ce33af3790
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 12:48:14 +0900

    fix: Lotto 클래스 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,29 +9,27 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        // 불변 리스트를 수정 가능한 리스트로 복사하여 정렬 가능하도록 합니다.
+        this.numbers = new ArrayList<>(numbers);
         sortLottoNumbers();
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return Collections.unmodifiableList(numbers); // 불변 리스트로 반환하여 외부 수정 방지
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != Constants.LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-        for(Integer number : numbers){
+        for (Integer number : numbers) {
             if (number < Constants.LOTTO_MIN_NUM || number > Constants.LOTTO_MAX_NUM) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이어야 합니다.");
             }
         }
-        if(numbers.stream().distinct().count() < numbers.size()){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복된 값을 가질 수 없습니다.");
-        }
     }
 
     private void sortLottoNumbers() {
-        Collections.sort(this.numbers);
+        Collections.sort(this.numbers); // 수정 가능한 리스트로 정렬 가능
     }
 }
