9f554b6c52f42b0ba30a9f165ff7112f3f4a48d1
Hyejeong <thswjd0609@gmail.com>
2024-11-04 19:39:49 +0900

    feat: 로또 구입 금액 입력 및 재입력 기능 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import lotto.model.LottoGenerator;
+import lotto.model.Validator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoGenerator lottoGenerator;
+    private Validator validator;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        validator = new Validator();
+        int purchaseAmount = validatePurchaseAmount();
+    }
+
+    public int validatePurchaseAmount() {
+        int purchaseAmount = 0;
+        while (true) {
+            try {
+                purchaseAmount = inputView.getPurchaseAmount();
+                validator.validatePositiveAmount(purchaseAmount);
+                validator.validateAmountUnit(purchaseAmount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return purchaseAmount;
+    }
+}
