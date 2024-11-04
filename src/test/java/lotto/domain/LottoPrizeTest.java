b9ce7310088348bdb5772ecc4266c97d52aa9358
threepebbles <threepebbles@naver.com>
2024-11-04 19:58:39 +0900

    test(LottoPrize): 일치하는 로또 번호 개수에 따라 알맞는 enum을 반환하는지 테스트하는 기능 추가

---
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottoPrizeTest {
+    @Test
+    void 당첨_개수에_맞는_enum을_반환하는지_테스트() {
+        Assertions.assertThat(LottoPrize.valueOf(6, false))
+                .isEqualTo(LottoPrize.FIRST);
+        Assertions.assertThat(LottoPrize.valueOf(5, true))
+                .isEqualTo(LottoPrize.SECOND);
+        Assertions.assertThat(LottoPrize.valueOf(5, false))
+                .isEqualTo(LottoPrize.THIRD);
+        Assertions.assertThat(LottoPrize.valueOf(4, false))
+                .isEqualTo(LottoPrize.FOURTH);
+        Assertions.assertThat(LottoPrize.valueOf(4, true))
+                .isEqualTo(LottoPrize.FOURTH);
+        Assertions.assertThat(LottoPrize.valueOf(3, false))
+                .isEqualTo(LottoPrize.FIFTH);
+        Assertions.assertThat(LottoPrize.valueOf(3, true))
+                .isEqualTo(LottoPrize.FIFTH);
+    }
+}
