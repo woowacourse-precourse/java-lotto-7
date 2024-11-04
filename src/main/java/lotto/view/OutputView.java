636874520315c31104514565c16405ecc84906e8
Dohun Kim <dohun8758@naver.com>
2024-11-04 20:36:26 +0900

    refactor: 매직 스트링 상수로 추출

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,42 +8,52 @@ import lotto.model.PurchasedLottos;
 
 public class OutputView {
 
+    public static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String LOTTO_PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.\n";
+    public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
+    public static final String LINE_SEPARATOR = "---";
+    public static final String LOTTO_RANK_COUNT_MESSAGE = "%s (%s원) - %d개\n";
+    public static final String LOTTO_REVENUE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+    public static final String THOUSAND_SEPARATOR_FORMAT = "%,d";
+
     public void showMoneyInputComments() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(MONEY_INPUT_MESSAGE);
     }
 
     public void showLottoWinningNumbersInputComments() {
         System.out.println();
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
     }
 
     public void showLottoBonusNumberInputComments() {
         System.out.println();
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
     }
 
     public void showPurchasedLottos(int purchasedCount, PurchasedLottos purchasedLottos) {
         System.out.println();
-        System.out.printf("%d개를 구매했습니다.\n", purchasedCount);
+        System.out.printf(LOTTO_PURCHASE_RESULT_MESSAGE, purchasedCount);
         List<Lotto> lottos = purchasedLottos.getLottos();
         lottos.forEach(System.out::println);
     }
 
     public void showLottoResult(Map<LottoRank, Integer> lottoResult) {
         System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        System.out.println(LINE_SEPARATOR);
         List<LottoRank> orderedRanks = List.of(LottoRank.FIFTH, LottoRank.FOURTH, LottoRank.THIRD, LottoRank.SECOND,
                 LottoRank.FIRST);
         orderedRanks.forEach(rank ->
-                System.out.printf("%s (%s원) - %d개\n", rank.getMessage(),
-                        String.format("%,d", rank.getPrizeAmount()),
+                System.out.printf(LOTTO_RANK_COUNT_MESSAGE, rank.getMessage(),
+                        String.format(THOUSAND_SEPARATOR_FORMAT, rank.getPrizeAmount()),
                         lottoResult.getOrDefault(rank, 0))
         );
     }
 
     public void showLottoRevenue(double revenue) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", revenue);
+        System.out.printf(LOTTO_REVENUE_MESSAGE, revenue);
     }
 
     public void showExceptionMessage(Exception e) {
