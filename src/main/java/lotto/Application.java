3e6ee504af3edfb5111abd0b7452174fcbcee5b0
EP45 <5510816+EP45@users.noreply.github.com>
2024-11-04 20:08:43 +0900

    feat(Application): add game flow
    This adds overall flow for lotto game.
    Need to implement statistics for the game.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,50 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame game = new LottoGame();
+
+        Money money = null;
+        while (money == null || game.getLottoCount() == 0) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String moneyInput = Console.readLine();
+            try {
+                money = new Money(moneyInput);
+                game.purchaseLotto(money);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        game.assignRandomNumberToLotto();
+        System.out.println();
+        game.prettyPrintPurchasedLottos();
+        System.out.println();
+
+        while (game.getWinningLotto() == null) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            String winningNumbersInput = Console.readLine();
+            try {
+                game.assignWinningLotto(winningNumbersInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (game.getBonusNumber() == 0) {
+            System.out.println();
+            System.out.println("보너스 번호를 입력해 주세요.");
+            String bonusNumberInput = Console.readLine();
+            try {
+                game.assignBonusNumber(bonusNumberInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println();
+        // TODO: show statistics
     }
 }
