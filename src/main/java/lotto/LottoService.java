92ba5c7a4f36aa719e8f62d999798f3c050840bf
Seongmo Ahn <djgood780@gmail.com>
2024-11-04 20:02:16 +0900

    feat(LottoService): 당첨 비교 결과와 수익률 계산 로직 추가

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -21,4 +21,31 @@ public class LottoService {
                 .map(lotto -> lotto.contains(bonusNumber.getBonusNumber()))
                 .toList();
     }
+
+    public List<LottoWinningRanks> summarizeLottoRanks(List<Integer> lottoResults, List<Boolean> bonusResults) {
+        List<LottoWinningRanks> lottoWinningRanks = new ArrayList<>();
+        for (int i = 0; i < lottoResults.size(); i++) {
+            int sameCount = lottoResults.get(i);
+            boolean isBonusMatched = bonusResults.get(i);
+            lottoWinningRanks.add(LottoWinningRanks.getRank(sameCount, isBonusMatched));
+        }
+        return lottoWinningRanks;
+    }
+
+    public Map<LottoWinningRanks, Integer> summarizeRanksToCounts(List<LottoWinningRanks> lottoWinningRanks) {
+        Map<LottoWinningRanks, Integer> lottoRankMap = new HashMap<>();
+        for (LottoWinningRanks rank : lottoWinningRanks) {
+            lottoRankMap.put(rank, lottoRankMap.getOrDefault(rank, 0) + 1);
+        }
+        return lottoRankMap;
+    }
+
+    public double calculateRateOfReturn(List<LottoWinningRanks> prizeResults, LottoBuyer lottoBuyer) {
+        double finalValue = 0;
+        double investment = lottoBuyer.getPurchaseAmount();
+        for(LottoWinningRanks winningRank : prizeResults) {
+            finalValue += winningRank.getMoney();
+        }
+        return (finalValue / investment) * 100;
+    }
 }
