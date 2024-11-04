9548574cf8dce3fabe096c419fa76df647159c0d
SyingSHY <posti1x38@gmail.com>
2024-11-04 21:05:12 +0900

    feat: 기본 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,71 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input_text = "";
+        int purchaseAmount = 0;
+        int bonusNum = 0;
+        List<Integer> winningNums = new ArrayList<>();
+        List<Lotto> lottos = new ArrayList<Lotto>();
+        int[] winningResult = new int[6];
+
+        InputHandler inputHandler = new InputHandler();
+        LottoWinningChecker winningChecker;
+
+        while (true) {
+            try {
+                // 구매 금액 입력 안내 메시지 출력
+                input_text = Console.readLine();
+                purchaseAmount = inputHandler.readPurchaseAmount(input_text);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        for (int i = 0; i < purchaseAmount; i++) {
+            List<Integer> nums = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+            lottos.add(new Lotto(nums));
+        }
+
+        // 구매한 로또 번호 출력
+
+        while (true) {
+            try {
+                // 당첨 번호 입력 안내 메시지 출력
+                input_text = Console.readLine();
+                winningNums = inputHandler.readWinningNumbers(input_text);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                // 보너스 번호 입력 안내 메시지 출력
+                input_text = Console.readLine();
+                bonusNum = inputHandler.readBonusNumber(input_text);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        winningChecker = new LottoWinningChecker(winningNums, bonusNum);
+
+        for (Lotto lotto : lottos) {
+            int winningGrade = winningChecker.getWinningGrade(lotto);
+            winningResult[winningGrade] += 1;
+        }
+
+        // 당첨 통계 출력
     }
 }
--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -1,2 +1,65 @@
-package lotto;public class InputHandler {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputHandler {
+
+    public int readPurchaseAmount(String input_text) {
+        int result_num = 0;
+
+        for (char c : input_text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                result_num *= 10;
+                result_num += c - '0';
+                continue;
+            }
+            throw new IllegalArgumentException(ExceptionMessage.NUM_NOT_NUM.getMessage());
+        }
+
+        return result_num / 1000;
+    }
+
+    public List<Integer> readWinningNumbers(String input_text) {
+        List<Integer> winning_nums = new ArrayList<Integer>();
+        int result_num = 0;
+
+        for (char c : input_text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                result_num *= 10;
+                result_num += c - '0';
+                continue;
+            }
+            if (c == ',') {
+                winning_nums.add(result_num);
+                result_num = 0;
+                continue;
+            }
+            throw new IllegalArgumentException(ExceptionMessage.NUM_NOT_NUM.getMessage());
+        }
+        winning_nums.add(result_num);
+
+        return winning_nums;
+    }
+
+    public int readBonusNumber(String input_text) {
+        int result_num = 0;
+
+        for (char c : input_text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                result_num *= 10;
+                result_num += c - '0';
+                continue;
+            }
+            throw new IllegalArgumentException(ExceptionMessage.NUM_NOT_NUM.getMessage());
+        }
+
+        return result_num;
+    }
+
+    private void checkBlankString(String input_text) {
+        if (input_text.isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessage.INPUT_EMPTY.getMessage());
+        }
+    }
 }
