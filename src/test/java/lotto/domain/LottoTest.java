3aa7ae66aa734bf9c1a6de88db3873fe5962cc02
inti0 <142914095+inti0@users.noreply.github.com>
2024-11-04 19:38:59 +0900

    Test : LottoTest 추가
    로또 번호가 6개보다 적으면 예외가 발생하는지 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -24,7 +24,12 @@ class LottoTest {
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
     @Test
-    void 로또_번가_범위를_넘어가면_예외가_발생한다() {
+    void 로또_번호가_6개보다_적으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 로또_번호가_범위를_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
