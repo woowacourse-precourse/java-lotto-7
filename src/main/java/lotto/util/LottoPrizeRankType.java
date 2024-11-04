a626ce038ba7e1383a548c2555abe8638b60d882
dongwoo <woomacho@gmail.com>
2024-11-04 18:32:53 +0900

    feat: 로또 당첨 기준, 금액과 판매금액 타입설정
    - 1~5등 당첨 개수, 보너스 번호 매칭, 당첨금의 타입 안정성과, 일괄적인 관리를 위해 enum으로 구현했다.

---
+++ src/main/java/lotto/util/LottoPrizeRankType.java
@@ -0,0 +1,58 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public enum LottoPrizeRankType {
+    FIRST(6, false, 2000000000L),
+    SECOND(5, true, 30000000L),
+    THIRD(5, false, 1500000L),
+    FOURTH(4, false, 50000L),
+    FIFTH(3, false, 5000L),
+    ZERO(0, false, 0L);
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final Long prizeMoney;
+
+    LottoPrizeRankType(int matchCount, boolean bonusMatch, Long prizeMoney) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static LottoPrizeRankType findByMatchCountAndBonusMatch(int matchCount,
+            boolean bonusMatch) {
+        for (LottoPrizeRankType lottoPrizeRankType : values()) {
+            if (isPrizeRank(lottoPrizeRankType, matchCount, bonusMatch)) {
+                return lottoPrizeRankType;
+            }
+        }
+        return ZERO;
+    }
+
+    public static boolean isPrizeRank(LottoPrizeRankType lottoPrizeRankType, int matchCount,
+            boolean bonusMatch) {
+        if (lottoPrizeRankType.matchCount != matchCount) {
+            return false;
+        }
+        return lottoPrizeRankType.bonusMatch == bonusMatch;
+    }
+
+    public int getMatchCount() {
+        return this.matchCount;
+    }
+
+    public Long getPrizeMoney() {
+        return this.prizeMoney;
+    }
+
+    public static Map<LottoPrizeRankType, Long> getRankCountMap() {
+        final EnumMap<LottoPrizeRankType, Long> map = new EnumMap<>(LottoPrizeRankType.class);
+        Arrays.stream(LottoPrizeRankType.values())
+                .filter(rank -> rank != LottoPrizeRankType.ZERO)
+                .forEach(rank -> map.put(rank, 0L));
+        return map;
+    }
+}
