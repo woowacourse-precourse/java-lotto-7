89288149ac08fdd30c8bf9a1836a9f87a2499a7f
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 18:06:16 +0900

    feat(InputView): 보너스 번호를 입력 받는다

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -21,4 +21,10 @@ public class InputView {
                 .map(Integer::parseInt)
                 .toList();
     }
+
+    public int promptBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
