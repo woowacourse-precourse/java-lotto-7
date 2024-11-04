2f28d9a995ec2c3799beb80e3ccd4577cb8bbbcc
sonshn <sonshn@naver.com>
2024-11-04 19:43:03 +0900

    fix(InputView): 보너스 번호 입력 문구 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -15,7 +15,7 @@ public class InputView {
     }
 
     public static String getLotteryBonusNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
