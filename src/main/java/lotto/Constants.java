c91a4dbd71b62c175abb81dbf4cd8797fcfc02f8
Layla Oh <crispylemon7120@gmail.com>
2024-11-04 19:06:43 +0900

    refactor(error): error 메세지 수정

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -26,7 +26,7 @@ public class Constants {
         INPUT_TOTAL_AMOUNT_NOT_LARGER_THAN_1000("[ERROR] 구입 최소 금액은 1000원입니다.\n"),
         INPUT_TOTAL_AMOUNT_ERROR("[ERROR] 입력값은 1000원 단위여야 합니다.\n"),
         LOTTO_COUNT_ERROR("[ERROR] 로또 번호는 6개여야 합니다."),
-        NUMBER_RANGE_ERROR("[ERROR] 번호는 1~45 이어야 합니다."),
+        NUMBER_RANGE_ERROR("[ERROR]  로또 번호는 1부터 45 사이의 숫자여야 합니다."),
         DUPLICATE_ERROR("[ERROR] 번호는 중복되면 안됩니다."),
         NUMBER_FORMAT_ERROR("[ERROR] 숫자만 입력하세요.");
 
