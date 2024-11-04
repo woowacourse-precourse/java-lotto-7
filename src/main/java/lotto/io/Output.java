604218ce5b6166e3c31e5275846f49757fcb0306
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-04 15:15:17 +0900

    feat(output): 당첨 통계 출력 기능 추가

--- src/main/java/lotto/io/Output.java
+++ src/main/java/lotto/io/Output.java
@@ -5,6 +5,8 @@ import lotto.domain.Lotto;
 
 public class Output {
     private final String PRINT_LOTTO_QUANTITY = "개를 구매했습니다.";
+    private final String PRINT_WINNING_STATISTICS = "당첨 통계";
+    private final String SEPARATING_LINE = "---";
 
     public void printLottoQuantity(int lottoQuantity) {
         printNewLine();
@@ -15,6 +17,12 @@ public class Output {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+    }
+
+    public void printWinningStatistics() {
+        printNewLine();
+        System.out.println(PRINT_WINNING_STATISTICS);
+        System.out.println(SEPARATING_LINE);
     }
 
     private void printNewLine() {
