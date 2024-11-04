3cea23e881d90c46418cf21471d2463663b5eab3
Yunsu4 <ysa020428@gmail.com>
2024-11-04 16:24:28 +0900

    refactor(Application): LottoController의 메서드명 변경 반영

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,6 +10,6 @@ public class Application {
     public static void main(String[] args) {
         LottoController lottoController = LottoConfig.lottoController();
 
-        lottoController.run();
+        lottoController.startLottoProcess();
     }
 }
