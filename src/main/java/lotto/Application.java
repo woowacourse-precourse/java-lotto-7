2b7b64d53e020d128613d3e5bc59b0eb8197c8c7
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-04 20:40:32 +0900

    fix(로또 발급기): chagne out message and data type of check WinLotto result
    chagne out message and data type of check WinLotto result. Format of the outmessage is not same with request so i chagned. And when i fix another bug i changed data type so chaged data type to uniform them.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import Calculator.CheckWinning;
 import Calculator.Profit;
 import buy.BuyingLotto;
 import java.util.List;
+import java.util.Map;
 import print.InfoPrinter;
 import print.InputRequest;
 
@@ -19,10 +20,10 @@ public class Application {
         infoPrinter.purchasedLottoInfoPrint(money / 1000, purchasedLottos);
 
         CheckWinning checkWinning = new CheckWinning();
-        List<Integer> result = checkWinning.checkWinLottoTotal(purchasedLottos);
+        Map<Integer, Integer> result = checkWinning.checkWinLottoTotal(purchasedLottos);
 
         infoPrinter.winningResultPrint(result);
         double profit = new Profit().calculate(result, money);
-        System.out.println("총 수익률은 " + profit + " %입니다.");
+        System.out.println("총 수익률은 " + profit + "%입니다.");
     }
 }
