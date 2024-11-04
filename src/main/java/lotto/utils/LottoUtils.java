368f98542ea8879914dc5bd4bbb4a30a18eb037d
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 12:34:39 +0900

    feat(lottoutils): 로또 당첨 갯수 계산 및 당첨 통계 출력 기능 추가
    - 로또 당첨 결과를 집계하여 MatchResult 별로 개수를 계산하는 기능을 추가했습니다.
    - 당첨 통계를 화면에 출력하는 메서드를 통해 각 당첨 결과와 해당 개수를 출력하도록 구현했습니다.

--- src/main/java/lotto/utils/LottoUtils.java
+++ src/main/java/lotto/utils/LottoUtils.java
@@ -5,16 +5,39 @@ import lotto.domain.LottoResult;
 import lotto.domain.MatchResult;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoUtils {
 
-    int lottoRateOfReturn = 0;
+    int totalPrize = 0;
     ArrayList<MatchResult> lottoResults = new ArrayList<>();
 
-    public void calculateWinningResult(List<Integer> winning, List<Lotto> lottos, int bonus) {
+    public void calculateWinningResult(List<Integer> winning, List<Lotto> lottos, int bonus, int purchaseAmount) {
         createLottoResults(winning, lottos, bonus);
+        Map<MatchResult, Integer> matchResultMap = countMatchResults();
+        printMatchResults(matchResultMap);
+    }
 
+    Map<MatchResult, Integer> countMatchResults() {
+        Map<MatchResult, Integer> matchCounts = new EnumMap<>(MatchResult.class);
+        for (MatchResult matchResult : MatchResult.values()){
+            matchCounts.put(matchResult, 0);}
+        for (MatchResult matchResult : lottoResults){
+            matchCounts.put(matchResult, matchCounts.get(matchResult) + 1);
+        }
+        return matchCounts;
+    }
+
+    private void printMatchResults(Map<MatchResult, Integer> matchResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + matchResult.get(MatchResult.THREE_MATCH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + matchResult.get(MatchResult.FOUR_MATCH) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + matchResult.get(MatchResult.FIVE_MATCH) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchResult.get(MatchResult.FIVE_MATCH_BONUS) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + matchResult.get(MatchResult.SIX_MATCH) + "개");
     }
 
     private void createLottoResults(List<Integer> winning, List<Lotto> lottos, int bonus) {
@@ -27,7 +50,7 @@ public class LottoUtils {
 
     private MatchResult determineMatchResult(LottoResult lottoResult){
         if (lottoResult.matchingCount() < 3){
-            return null;
+            return MatchResult.NONE;
         }
         if (lottoResult.matchingCount() == 3) {
             return MatchResult.THREE_MATCH;
@@ -47,5 +70,4 @@ public class LottoUtils {
         }
         return MatchResult.FIVE_MATCH;
     }
-
 }
--- src/test/java/lotto/utils/LottoUtilsTest.java
+++ src/test/java/lotto/utils/LottoUtilsTest.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 class LottoUtilsTest {
 
     LottoUtils lottoUtils = new LottoUtils();
-    LottoResult lottoResut;
-    int lottoRateOfReturn;
     List<Integer> winningNumbers;
     List<Lotto> lottos;
+    int purchaseAmount;
     int bonusNumber;
 
 
@@ -25,25 +25,28 @@ class LottoUtilsTest {
         lottoUtils = new LottoUtils();
         winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         lottos = Arrays.asList(
-                new Lotto(Arrays.asList(1, 2, 3, 4, 7, 8)),
-                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
-                new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)),
-                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8))
+                new Lotto(Arrays.asList(1, 2, 3, 9, 10, 11)),
+                new Lotto(Arrays.asList(7, 8, 9, 10, 11, 12)),
+                new Lotto(Arrays.asList(13, 14, 15, 16, 17, 18)),
+                new Lotto(Arrays.asList(19, 20, 21, 22, 23, 24)),
+                new Lotto(Arrays.asList(19, 20, 21, 22, 23, 24)),
+                new Lotto(Arrays.asList(19, 20, 21, 22, 23, 24))
+
         );
         bonusNumber = 8;
+        purchaseAmount = 8000;
     }
 
     @Test
     void 로또_결과_계산_테스트() {
-        lottoUtils.calculateWinningResult(winningNumbers, lottos, bonusNumber);
-
-        // 예상 결과와 비교
-        assertEquals(4, lottoUtils.lottoResults.size(), "로또 결과 개수가 맞지 않습니다.");
-        assertEquals(MatchResult.FOUR_MATCH, lottoUtils.lottoResults.get(0), "첫 번째 로또 결과가 예상과 다릅니다.");
-        assertEquals(MatchResult.SIX_MATCH, lottoUtils.lottoResults.get(1), "두 번째 로또 결과가 예상과 다릅니다.");
-        assertEquals(MatchResult.THREE_MATCH, lottoUtils.lottoResults.get(2), "세 번째 로또 결과가 예상과 다릅니다.");
-        assertEquals(MatchResult.FIVE_MATCH_BONUS, lottoUtils.lottoResults.get(3), "네 번째 로또 결과가 예상과 다릅니다.");
-
+        lottoUtils.calculateWinningResult(winningNumbers, lottos, bonusNumber, purchaseAmount);
+        assertEquals(6, lottoUtils.lottoResults.size(), "로또 결과 개수가 맞지 않습니다.");
+        assertEquals(MatchResult.THREE_MATCH, lottoUtils.lottoResults.get(0), "첫 번째 로또 결과가 예상과 다릅니다.");
+        assertEquals(MatchResult.NONE, lottoUtils.lottoResults.get(1), "두 번째 로또 결과가 예상과 다릅니다.");
+        assertEquals(MatchResult.NONE, lottoUtils.lottoResults.get(2), "세 번째 로또 결과가 예상과 다릅니다.");
+        assertEquals(MatchResult.NONE, lottoUtils.lottoResults.get(3), "네 번째 로또 결과가 예상과 다릅니다.");
+        assertEquals(MatchResult.NONE, lottoUtils.lottoResults.get(4), "다섯 번째 로또 결과가 예상과 다릅니다.");
+        assertEquals(MatchResult.NONE, lottoUtils.lottoResults.get(5), "여섯 번째 로또 결과가 예상과 다릅니다.");
     }
 
 
