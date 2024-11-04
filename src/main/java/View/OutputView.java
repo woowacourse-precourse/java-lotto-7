ddd5f95117a3165b24265b59e566f5eac01a491e
김호연 <yun1094@naver.com>
2024-11-04 21:21:46 +0900

    당첨 통계 표시 함수 구현

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -6,6 +6,10 @@ import Constant.Constant;
 import java.util.List;
 
 public class OutputView {
+    private static List<Lotto> buyLottos;
+    private static List<Integer> winningNumbers;
+    private static Integer bonusNumber;
+    private static Integer bounsWinCount = 0;
 
     public static void printTrialLottoCount(Integer trialLottoCount) {
         System.out.println(String.format(Constant.MessageConstant.OUTPUT_PROMPT, trialLottoCount));
@@ -15,5 +19,9 @@ public class OutputView {
         for (Lotto lotto : buyLotto) {
             System.out.println(lotto.toString());
         }
+    }
+
+    public static void printCheckStart() {
+        System.out.println(Constant.MessageConstant.WINNING_STATISTICS);
     }
 }
