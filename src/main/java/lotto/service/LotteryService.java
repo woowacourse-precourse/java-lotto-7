72df8cfb81e7f4b799cb514e9dc888226d1c3c23
Gothax <scott990328@gmail.com>
2024-11-04 20:45:05 +0900

    feat: 당첨 결과, 수익률 계산 기능 추가
    - winning enum class 이용

--- src/main/java/lotto/service/LotteryService.java
+++ src/main/java/lotto/service/LotteryService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.Winning;
 import lotto.repository.LottoRepository;
 
 import java.util.ArrayList;
@@ -20,11 +21,43 @@ public class LotteryService {
         return getBuyResult();
     }
 
+    public List<String> confirmWinnings(List<String> winningNums, String bonusNum) {
+        for (Lotto lotto : lottoRepository.findAllLottos()) {
+            lotto.match(winningNums, bonusNum);
+            incrementWinningByLotto(lotto);
+        }
+        return getWinningResult();
+    }
+
+    private List<String> getWinningResult() {
+        List<String> result = new ArrayList<>();
+        result.add("당첨 통계");
+        result.add("---");
+        lottoRepository.getWinningMap().forEach((winning, count) -> {
+            result.add(winning.getWinningMessage(count));
+        });
+        result.add(getProfitRate());
+        return result;
+    }
+
+    private String getProfitRate() {
+        int totalPrize = lottoRepository.getTotalPrize();
+        int purchaseMoney = lottoRepository.getLottoCount() * 1000;
+        double rate = (double) totalPrize / purchaseMoney * 100;
+        double roundedRate = Math.round(rate * 10) / 10.0;
+        return "총 수익률은 " + roundedRate + "%입니다.";
+    }
+
     private List<String> getBuyResult() {
         List<String> result = new ArrayList<>();
-        result.add(lottoRepository.getLottoCount()+"개를 구매했습니다.");
-        lottoRepository.findAll().forEach(lotto -> result.add(lotto.toString()));
+        result.add("\n" + lottoRepository.getLottoCount()+"개를 구매했습니다.");
+        lottoRepository.findAllLottos().forEach(lotto -> result.add(lotto.toString()));
         return result;
+    }
+
+    private void incrementWinningByLotto(Lotto lotto) {
+        Winning.getWinning(lotto.getMatchCount(), lotto.getBonusExist())
+                .ifPresent(lottoRepository::incrementWinning);
     }
 
     private void createLottos(Integer lottoAmount) {
@@ -38,25 +71,7 @@ public class LotteryService {
     }
 
     private Integer getAmountByMoney(Integer purchaseMoney) {
-        validatePurchaseMoney(purchaseMoney);
         return purchaseMoney / 1000;
-    }
-
-    private void validatePurchaseMoney(Integer purchaseMoney) {
-        validateMinimum(purchaseMoney);
-        validateChange(purchaseMoney);
-    }
-
-    private void validateChange(Integer purchaseMoney) {
-        if (purchaseMoney % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다.");
-        }
-    }
-
-    private void validateMinimum(Integer purchaseMoney) {
-        if (purchaseMoney < 1000) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 이상이어야 합니다.");
-        }
     }
 
 }
