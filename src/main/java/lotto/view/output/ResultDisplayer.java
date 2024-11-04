19d2f59fe9ea75e2bb68d9ea52dc2011bd570272
kim jihyeon <jihyeon546@gmail.com>
2024-11-04 18:08:18 +0900

    fix: 출력 형식 변경

--- src/main/java/lotto/view/output/ResultDisplayer.java
+++ src/main/java/lotto/view/output/ResultDisplayer.java
@@ -8,6 +8,11 @@ public class ResultDisplayer {
 
     private static final String LOTTO_COUNT_MESSAGE_SUFFIX = "개를 구매했습니다.";
     private static final String YIELD_MESSAGE = "총 수익률은 ";
+    private static final String MATCH_COUNT_MESSAGE = "%d개 일치";
+    private static final String MATCH_COUNT_WITH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치";
+    private static final String PRIZE_MESSAGE = "(%,d원) - %d개%n";
+    private static final String STATISTICS_HEADER = "당첨 통계";
+    private static final String SEPARATOR = "---";
 
     public void showPurchasedLottos(int lottoCount, Lottos lottos) {
         System.out.println(lottoCount + LOTTO_COUNT_MESSAGE_SUFFIX);
@@ -17,21 +22,23 @@ public class ResultDisplayer {
     }
 
     public void showWinningStatistics(Map<WinningRank, Integer> winningCounts) {
-        System.out.println("---");
+        System.out.println(STATISTICS_HEADER);
+        System.out.println(SEPARATOR);
 
-        winningCounts.forEach((rank, count) -> {
+        for (WinningRank rank : WinningRank.values()) {
             if (rank == WinningRank.SECOND_PRIZE_WITH_BONUS) {
-                System.out.printf("%d개 일치, 보너스 볼 일치 (%,d원) - %d개%n",
+                System.out.printf(MATCH_COUNT_WITH_BONUS_MESSAGE + " " + PRIZE_MESSAGE,
                         rank.getRequiredMatchingCount(),
                         rank.getPrizeAmount(),
-                        count);
-            } else if (rank != WinningRank.NO_PRIZE) {
-                System.out.printf("%d개 일치 (%,d원) - %d개%n",
-                        rank.getRequiredMatchingCount(),
-                        rank.getPrizeAmount(),
-                        count);
+                        winningCounts.getOrDefault(rank, 0));
             }
-        });
+            if (rank != WinningRank.NO_PRIZE && rank != WinningRank.SECOND_PRIZE_WITH_BONUS) {
+                System.out.printf(MATCH_COUNT_MESSAGE + " " + PRIZE_MESSAGE,
+                        rank.getRequiredMatchingCount(),
+                        rank.getPrizeAmount(),
+                        winningCounts.getOrDefault(rank, 0));
+            }
+        }
     }
 
     public void showYield(double yield) {
