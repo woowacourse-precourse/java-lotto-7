64348a7f3a483c320292f505962b90e3ba77e9d5
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-04 19:25:47 +0900

    refactor: 에러 문구 수정 및 application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoInput input = new LottoInput();
+        LottoGenerator generator = new LottoGenerator();
+        LottoOutput output = new LottoOutput();
+        LottoResultCalculator calculator = new LottoResultCalculator();
+
+        try {
+            int purchaseAmount = input.inputPurchaseAmount();
+            List<Lotto> lottos = generator.generateLottos(purchaseAmount);
+            output.printLottos(lottos);
+
+            List<Integer> winningNumbers = input.inputWinningNumbers();
+            int bonusNumber = input.inputBonusNumber();
+
+            calculator.calculateResult(lottos, winningNumbers, bonusNumber);
+            Map<String, Integer> results = calculator.getResults();
+
+            output.printResults(results);
+            double profitRate = calculateProfitRate(results, purchaseAmount);
+            output.printProfit(profitRate);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+    }
+
+    private static double calculateProfitRate(Map<String, Integer> results, int purchaseAmount) {
+        double totalProfit = results.getOrDefault("1등", 0) * 2000000000 +
+                results.getOrDefault("2등", 0) * 30000000 +
+                results.getOrDefault("3등", 0) * 1500000 +
+                results.getOrDefault("4등", 0) * 50000 +
+                results.getOrDefault("5등", 0) * 5000;
+
+        return (totalProfit / purchaseAmount) * 100;
     }
 }
--- src/main/java/lotto/LottoInput.java
+++ src/main/java/lotto/LottoInput.java
@@ -1,11 +1,29 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class LottoInput {
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return validatePurchaseAmount(Console.readLine());
+        String input = Console.readLine();
+        try {
+            return validatePurchaseAmount(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식이 올바르지 않습니다.");
+        }
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return LottoValidator.validateWinningNumbers(input);
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return LottoValidator.validateBonusNumber(input);
     }
 
     private int validatePurchaseAmount(String input) {
