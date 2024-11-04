1c12e317bfaca4c5f32a8460c7b4fbaa8fd6bbac
문고은 <>
2024-11-04 15:32:20 +0900

    refactor(enum) : 로또 당첨 상금 enum 추가

--- src/main/java/lotto/EarningsRateCalculator.java
+++ src/main/java/lotto/EarningsRateCalculator.java
@@ -1,13 +1,10 @@
 package lotto;
 
+import lotto.enums.Prize;
+
 import java.util.List;
 
 public class EarningsRateCalculator {
-    private static final long FIRST_PLACE_PRIZE = 2000000000;
-    private static final long SECOND_PLACE_PRIZE = 30000000;
-    private static final long THIRD_PLACE_PRIZE = 1500000;
-    private static final long FOURTH_PLACE_PRIZE = 50000;
-    private static final long FIFTH_PLACE_PRIZE = 5000;
     private final List<Integer> placeCount;
     private final long purchaseAmount;
 
@@ -21,10 +18,10 @@ public class EarningsRateCalculator {
     }
 
     private long calculateNetProfit() {
-        return placeCount.get(0) * FIRST_PLACE_PRIZE
-                + placeCount.get(1) * SECOND_PLACE_PRIZE
-                + placeCount.get(2) * THIRD_PLACE_PRIZE
-                + placeCount.get(3) * FOURTH_PLACE_PRIZE
-                + placeCount.get(4) * FIFTH_PLACE_PRIZE;
+        return placeCount.get(0) * Prize.FIRST_PLACE.getMoney()
+                + placeCount.get(1) * Prize.SECOND_PLACE.getMoney()
+                + placeCount.get(2) * Prize.THIRD_PLACE.getMoney()
+                + placeCount.get(3) * Prize.FOURTH_PLACE.getMoney()
+                + placeCount.get(4) * Prize.FIFTH_PLACE.getMoney();
     }
 }
