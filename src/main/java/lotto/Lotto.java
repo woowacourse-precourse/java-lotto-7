f2f3106ee78796362af04e7e0cbabf4b49be60d8
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 21:25:39 +0900

    fix: 이전 branch와 충돌 제거

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -14,6 +17,14 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        hasDuplicate(numbers);
+    }
+
+    private void hasDuplicate(List<Integer> numbers) {
+        ArrayList<Integer> list = new ArrayList<>(numbers);
+        HashSet<Integer> hashSet = new HashSet<>(numbers);
+        if(list.size() != hashSet.size())
+            throw new IllegalArgumentException("중복되는 번호가 있습니다.");
     }
 
     public List<Integer> getNumbers() {
