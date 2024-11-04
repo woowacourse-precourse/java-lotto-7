494bc3c594b08f0fbb7a91d5bddb30420bdb281d
minjae k <rkdalswoals@gmail.com>
2024-11-04 15:12:51 +0900

    refactor: remove zero constant from LottoConstants

--- src/main/java/lotto/common/LottoConstants.java
+++ src/main/java/lotto/common/LottoConstants.java
@@ -2,8 +2,7 @@ package lotto.common;
 
 public class LottoConstants {
     public static final int TICKET_PRICE = 1000;
-    public static final int ZERO = 0;
-    public static final int LOTTO_NUMBER_COUNT = 6;
-    public static final int LOTTO_MIN_NUMBER = 1;
-    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int NUMBER_COUNT = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
 }
