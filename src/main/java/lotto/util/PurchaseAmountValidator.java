5a59eac1894f2ebd8e05ad88af8ba92528242b3d
gyuoo <yeomky98@naver.com>
2024-11-04 23:22:38 +0900

    feat(util): add purchase amount validation logic

---
+++ src/main/java/lotto/util/PurchaseAmountValidator.java
@@ -0,0 +1,43 @@
+package lotto.util;
+
+import lotto.constants.LottoConstantNumbers;
+import lotto.exception.InvalidPurchaseAmountException;
+import lotto.exception.MaxPurchaseExceedException;
+import lotto.exception.NotDivisibleByLottoPriceException;
+
+public class PurchaseAmountValidator {
+
+	public int validateInput(String input) {
+		int parsedInput = parseStringToInt(input);
+		if (isNegativeNumber(parsedInput)) {
+			throw new InvalidPurchaseAmountException();
+		}
+		if (!canDivideWithPrice(parsedInput)) {
+			throw new NotDivisibleByLottoPriceException();
+		}
+		if (isExceedMaxPurchaseAmount(parsedInput)) {
+			throw new MaxPurchaseExceedException();
+		}
+		return parsedInput;
+	}
+
+	private int parseStringToInt(String input) {
+		try {
+			return Integer.parseInt(input);
+		} catch (InvalidPurchaseAmountException e) {
+			throw new InvalidPurchaseAmountException();
+		}
+	}
+
+	private boolean isNegativeNumber(int purchaseAmount) {
+		return purchaseAmount < 0;
+	}
+
+	private boolean canDivideWithPrice(int purchaseAmount) {
+		return purchaseAmount % LottoConstantNumbers.LOTTO_PRICE.getValue() == 0;
+	}
+
+	private boolean isExceedMaxPurchaseAmount(int purchaseAmount) {
+		return purchaseAmount > LottoConstantNumbers.MAX_PURCHASE_AMOUNT.getValue();
+	}
+}
