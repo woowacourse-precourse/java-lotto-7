755b5085a61b8abfc6321e671036c0889e6882c7
kaswhy <sxshx818@naver.com>
2024-11-04 16:19:19 +0900

    feat(Lotto): 로또 번호가 1부터 45 사이의 값인지 검증 기능 구현
    변경 후
    - List 순회하면서 해당 번호가 1부터 45 사이의 값인지 검증

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
 import static lotto.constants.ErrorMessage.LOTTO_NUMBERS_CAN_NOT_BE_DUPLICATED;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_MUST_BE_ONE_TO_FORTY_FIVE;
+import static lotto.constants.LottoCondition.MAX_LOTTO_NUMBER;
+import static lotto.constants.LottoCondition.MIN_LOTTO_NUMBER;
 
 import java.util.List;
 
@@ -17,11 +20,24 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         validateHasDuplicatedNumbers(numbers);
+        validateNumbersRange(numbers);
     }
 
     private void validateHasDuplicatedNumbers(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()){
             throw new IllegalArgumentException(LOTTO_NUMBERS_CAN_NOT_BE_DUPLICATED.get());
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        for (Integer lottoNumber : numbers) {
+            validateIsRangeOneToFortyFive(lottoNumber);
+        }
+    }
+
+    private void validateIsRangeOneToFortyFive(Integer lottoNumber) {
+        if (lottoNumber < MIN_LOTTO_NUMBER.get() || lottoNumber > MAX_LOTTO_NUMBER.get()) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_MUST_BE_ONE_TO_FORTY_FIVE.get());
         }
     }
 
