58ebcce2c13c855cfa077445405ff3f31503272d
Jewan1120 <hawaii1468@gmail.com>
2024-11-04 15:39:51 +0900

    test(Validator): 보너스 번호 입력의 유효성 검증할 테스트 추가

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto.validator;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -16,9 +17,18 @@ public class LottoValidatorTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    @ValueSource(strings = {",","1,a,b,4,5,6", "1,2,3,4,5", "1,1,3,3,5,6", "0,2,3,4,5,6", "0,2,3,4,5,46"})
+    @ValueSource(strings = {",", "1,a,b,4,5,6", "1,2,3,4,5", "1,1,3,3,5,6", "0,2,3,4,5,6", "0,2,3,4,5,46"})
     void 로또_당첨_번호_예외_발생(String inputString) {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> LottoValidator.validateWinningNumbers(inputString));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {",", "1", "-1", "0", "46"})
+    void 로또_보너스_번호_예외_발생(String inputString) {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> LottoValidator.validateBonusNumber(inputString, winningNumbers));
     }
 }
