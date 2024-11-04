1a0de54ee4064874d48dac1bbe14a438e080ed2b
ratatuwee <pkqwaszx@gmail.com>
2024-11-04 21:43:27 +0900

    test(lotto) : 로또 테스트 추가
    로또에 예외와 정상 동작을 확인하기위한 코드를 추가하였습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,17 @@
 package lotto;
 
+import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
     @Test
@@ -22,4 +28,36 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @DisplayName("로또 번호 6개 미만 예외 확인")
+    @Test
+    void checkLottoNumber5Error() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 번호 범위 예외 확인")
+    @ValueSource(ints = {0, 46, 100, -1})
+    void checkBoundaryLottoNumbersError(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        test.add(testNum);
+        String errorMessage = "[ERROR] 로또 번호가 범위를 벗어나는 번호가 있습니다.";
+
+        assertThatThrownBy(() -> new Lotto(test))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(errorMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("정상 동작 확인")
+    @ValueSource(ints = {1, 45, 10, 30})
+    void checkLotto(int testNum) {
+        WinningLotto.resetInstance();
+        List<Integer> test = new ArrayList<>(Arrays.asList(3, 20, 33, 13, 44));
+        test.add(testNum);
+
+        assertDoesNotThrow(() -> new Lotto(test));
+    }
 }
