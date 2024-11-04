fb5d1b980d0c5a366ff04dc6470588cfebadde40
yunsseong <aster20@hanyang.ac.kr>
2024-11-04 22:32:23 +0900

    refactor: 로또 생성 메서드 분리

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,46 +1,35 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 import lotto.model.Lotto;
-import lotto.validator.Validator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 import lotto.view.message.SystemMessage;
 
 public class LottoMachine {
     private final InputView inputView = new InputView();
-    private final Validator validator = new Validator();
     private final OutputView outputView = new OutputView();
-
     public List<Lotto> getLotteries() {
-        String input = "";
-        do {
-            input = inputView.input(SystemMessage.INPUT_PURCHASE_MONEY_AMOUNT.getMessage());
-        } while (!validateMoneyAmount(input));
+        int moneyAmount = inputView.moneyAmountInput(SystemMessage.INPUT_PURCHASE_MONEY_AMOUNT.getMessage());
+        int lottoAmount = calculateNumberLotto(moneyAmount);
+        outputView.print(lottoAmount + "개를 구매했습니다.");
+        return generateLotto(lottoAmount);
+    }
 
-        int moneyAmount = Integer.parseInt(input);
-        return IntStream.range(0, calculateNumberLotto(moneyAmount))
-                .mapToObj(num -> new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
-                .toList();
+    public List<Lotto> generateLotto(int lottoAmount) {
+        return IntStream.range(0, lottoAmount)
+                .mapToObj(num ->
+                        new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
+                                .sorted()
+                                .toList()
+                        )
+                ).toList();
     }
 
     public int calculateNumberLotto(int moneyAmount) {
         return moneyAmount / 1000;
-    }
-
-    public boolean validateMoneyAmount(String moneyAmount) {
-        try {
-            validator.isNotNull(moneyAmount);
-            validator.isNumber(moneyAmount);
-            int number = Integer.parseInt(moneyAmount);
-            validator.isOverMinimumPurchaseAmount(number);
-        } catch (IllegalArgumentException e) {
-            outputView.printError(e.getMessage());
-            return false;
-        }
-        return true;
     }
 }
