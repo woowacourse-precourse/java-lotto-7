c3de25663ec80aa9774f087de2e13514dfe2d742
장영재 <urinaner@naver.com>
2024-11-04 13:43:02 +0900

    feat(lottoException): 로또 예외 처리 구현

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -1,5 +1,13 @@
 package lotto.exception;
 
-public class LottoException  {
+public class LottoException extends BaseException {
+    private final LottoExceptionType exceptionType;
 
+    public LottoException(LottoExceptionType exceptionType){
+        this.exceptionType = exceptionType;
+    }
+    @Override
+    public BaseExceptionType exceptionType() {
+        return exceptionType;
+    }
 }
