4c18016674673c2226db6018ffb03c91b6d0cf1c
홍성문 <2020041412@hanyang.ac.kr>
2024-11-04 21:18:57 +0900

    feat: all last function complete

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,14 +13,17 @@ public class Application {
         int buyCount = io.inputBuyLottoPrice();
         for(int i = 0; i < buyCount; i++) {
             Lotto buyLotto = new Lotto(makeRandomLotto());
+
             customer.addLotto(buyLotto);
         }
         customer.printCustomerLotto();
 
         List<Integer> jackpotNumbers = io.inputCorrectNumbers();
+        Lotto jackpotLotto = new Lotto(jackpotNumbers);
         int bonusNum = io.inputBonusNumber(jackpotNumbers);
-
-        // 당첨 통계 출력
+        customer.setCorrectNumbers(jackpotLotto,bonusNum);
+        customer.checkJackpot();
+        customer.printJackpotList();
     }
 
     private static List<Integer> makeRandomLotto(){
@@ -26,6 +31,11 @@ public class Application {
         for(int i=0; i<6; i++){
             int randomNum = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1,45);
             lotto.add(randomNum);
+        }
+
+        Set<Integer> set = new HashSet<>(lotto);
+        if (set.size() != lotto.size()) {
+            return makeRandomLotto();
         }
         return lotto;
     }
--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Customer {
     private ArrayList<Lotto> lottos;
-    private int jackpot;
+    private long jackpot;
     private Lotto correctNumbers;
     private int bonusNumber;
     private ArrayList<Integer> jackpotArray;
@@ -13,6 +13,8 @@ public class Customer {
         lottos = new ArrayList<>();
         jackpot = 0;
         jackpotArray = new ArrayList<>(8);
+        for(int i=0; i<8; i++)
+            jackpotArray.add(0);
     }
 
     public void addLotto(Lotto lotto){
@@ -23,6 +25,13 @@ public class Customer {
         this.bonusNumber = bonusNumber;
     }
 
+    public void checkJackpot(){
+        for(Lotto tmp : lottos){
+            calculate(tmp);
+        }
+        // 여기에 총상금 계산및 출력하기
+        calculateJackpot();
+    }
 
     public void calculate(Lotto lotto){
         int count = 0;
@@ -34,7 +43,6 @@ public class Customer {
                 bonus = true;
         }
         addJackpotArray(count, bonus);
-        // 여기에 총상금 계산및 출력하기
     }
 
     private boolean numberInCorrect(int num){
@@ -55,6 +63,13 @@ public class Customer {
         jackpotArray.set(count, jackpotArray.get(count)+1);
     }
 
+    private void calculateJackpot(){
+        this.jackpot = jackpotArray.get(3)*5000
+                +jackpotArray.get(4)*50000
+                +jackpotArray.get(5)*1500000
+                +jackpotArray.get(6)*2000000000
+                +jackpotArray.get(7)*30000000;
+    }
     public void printCustomerLotto(){
         System.out.println(lottos.size()+"개를 구매했습니다.");
         for(int i=0; i<lottos.size(); i++){
@@ -64,5 +79,15 @@ public class Customer {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public void printJackpotList(){
+        System.out.println("당첨 통계\n---");
+        System.out.println("3개 일치 (5,000원) - " + jackpotArray.get(3) + "개");
+        System.out.println("4개 일치 (50,000원) - " + jackpotArray.get(4) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + jackpotArray.get(5) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + jackpotArray.get(7) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + jackpotArray.get(6) + "개");
+        System.out.println("총 수익률은 " + this.jackpot/(lottos.size()*10) + "%입니다");
     }
 }
