25ca0317476e4e34eabc2bb7594d4ced2a849018
Cheoroo <195822@jnu.ac.kr>
2024-11-04 16:53:15 +0900

    refactor : BonusNumbers의 검증 로직을 개선한다
    - Validator를 통해 검증을 위임
    - 생성자에서 Validator호출

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,20 +1,24 @@
 package lotto.domain;
 
+import lotto.utils.ValidatorFactory;
+import lotto.validation.Validator;
+
 public class BonusNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private final int number;
 
     public BonusNumber(int number, WinningNumbers winningNumbers) {
-        validate(number, winningNumbers);
-        this.number = number;
-    }
+        Validator<Integer> rangeValidator = ValidatorFactory.createNumberRangeValidator(
+                MIN_NUMBER, MAX_NUMBER, "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
+        );
+        Validator<WinningNumbers> nonDuplicateValidator = ValidatorFactory.createBonusNumberValidator(number,
+                "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
 
-    private void validate(int number, WinningNumbers winningNumbers) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (winningNumbers.getNumbers().contains(number)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
-        }
+        rangeValidator.validate(number);
+        nonDuplicateValidator.validate(winningNumbers);
+        this.number = number;
     }
 
     public int getNumber() {
