baa6a54271af9ea3bf18611d41512d3537987c95
waffle <nate.crema@gmail.com>
2024-11-04 16:50:31 +0900

    test(LottoOutput): 출력형식 테스트케이스 추가

--- src/test/java/lotto/unit/view/LottoOutputTest.java
+++ src/test/java/lotto/unit/view/LottoOutputTest.java
@@ -21,27 +21,33 @@ public class LottoOutputTest {
         assertThat(LottoOutput.prettyPrint(10) ).isEqualTo("10");
     }
 
-    @DisplayName("prettyPrint: 기능체크/3자리 숫자")
+    @DisplayName("prettyPrint: 기능체크/3자리 미만숫자 3")
     @Test
     void prettyPrintTest3() {
+        assertThat(LottoOutput.prettyPrint(0) ).isEqualTo("0");
+    }
+
+    @DisplayName("prettyPrint: 기능체크/3자리 숫자")
+    @Test
+    void prettyPrintTest4() {
         assertThat(LottoOutput.prettyPrint(100) ).isEqualTo("100");
     }
 
     @DisplayName("prettyPrint: 기능체크/3자리 초과숫자 1")
     @Test
-    void prettyPrintTest4() {
+    void prettyPrintTest5() {
         assertThat(LottoOutput.prettyPrint(10000) ).isEqualTo("10,000");
     }
 
     @DisplayName("prettyPrint: 기능체크/3자리 초과숫자 2")
     @Test
-    void prettyPrintTest5() {
+    void prettyPrintTest6() {
         assertThat(LottoOutput.prettyPrint(154322234) ).isEqualTo("154,322,234");
     }
 
     @DisplayName("prettyPrint: 기능체크/3자리 초과숫자 3")
     @Test
-    void prettyPrintTest6() {
+    void prettyPrintTest7() {
         assertThat(LottoOutput.prettyPrint(76854347) ).isEqualTo("76,854,347");
     }
 }
