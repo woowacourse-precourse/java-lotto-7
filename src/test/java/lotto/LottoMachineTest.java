8c441abbbeec96ecbb5822ad7e376bad4c02d7ae
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 16:03:10 +0900

    test(LottoMachineTest): 현금으로 로또 구매 테스트 작성

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.store.machine.LottoMachine;
+import lotto.store.user.Cash;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+
+    private final LottoMachine lottoMachine = new LottoMachine();
+
+    @Test
+    void 현금으로_로또를_구매한다() {
+        //given
+        Cash cash = Cash.from(8000);
+
+        //when
+        lottoMachine.purchase(cash);
+
+        //then
+        assertThat(lottoMachine.currentLottoTicketCount()).isEqualTo(8);
+    }
+
+}
