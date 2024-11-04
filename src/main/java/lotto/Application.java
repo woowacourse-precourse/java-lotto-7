ac34448e5c23baf9a8ffab5119c8aec7bbb53488
koojun99 <koojun99@naver.com>
2024-11-04 16:24:24 +0900

    feat(Application): add common input validation logic
    - 입력값이 비어있거나 공백이 포함된 경우 예외 발생 처리 추가
    - 공통적으로 사용하는 validateInput 메서드로 입력값 검증 로직 통합
    - 예외 메시지 형식을 "[ERROR]"로 시작하도록 통일하여 사용자 입력 오류에 대한 일관성 유지

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,23 @@ package lotto;
 import camp.nextstep.edu.missionutils.*;
 
 public class Application {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입 금액을 입력해주세요.");
+        String input = Console.readLine();
+        validateInput(input);
+    }
+
+    public static void validateInput(String input) {
+
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력값이 비어 있습니다. 다시 입력해 주세요.");
+        }
+
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + " 입력에 공백이 포함되어 있습니다. 다시 입력해 주세요.");
+        }
     }
 }
