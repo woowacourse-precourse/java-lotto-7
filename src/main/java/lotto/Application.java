8507e46aa790ad0ed62f840bef258f0295d8f1f1
Hyso <hsk7953@gmail.com>
2024-11-04 19:58:10 +0900

    feat(Application) : 로또 게임 프로그램 구현 - LottoController 를 통해 게임 진행 흐름 제어

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.model.LottoMachine;
+import lotto.service.GameStatisticService;
+import lotto.service.LottoPurchaseService;
+import lotto.service.WinningNumberService;
+import lotto.validator.AmountValidator;
+import lotto.validator.BonusValidator;
+import lotto.validator.LottoValidator;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        WinningNumberService winningNumberService = new WinningNumberService(new LottoValidator(),new BonusValidator());
+        GameStatisticService gameStatisticService = new GameStatisticService();
+        OutputView outputView = new OutputView();
+        LottoPurchaseService lottoPurchaseService = new LottoPurchaseService(new AmountValidator(),new LottoMachine());
+        LottoGameController lottoGameController = new LottoGameController(lottoPurchaseService,winningNumberService,
+                gameStatisticService,outputView);
+
+        lottoGameController.start();
     }
 }
