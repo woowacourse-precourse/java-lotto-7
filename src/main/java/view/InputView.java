aa18f3603e890366e87e7abcd91e1d5ddab2d61d
changyunyeong <sally0109277@naver.com>
2024-11-04 23:16:43 +0900

    feat: 입력 view 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,26 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    public int inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+}
