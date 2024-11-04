63a668a4806a2029830fa5a1e8c08f26a4773c62
bbangHo <sonsins1@naver.com>
2024-11-04 15:21:27 +0900

    feat: 전체적으로 로직이 돌아가도록 구현한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,31 +1,65 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        List<Integer> winningNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(winningNumbers);
+
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = Integer.parseInt(Console.readLine());
+        int purchaseNumber = purchaseAmount / 1000;
 
-        System.out.println(purchaseAmount / 1000 + "개를 구매했습니다.");
-        // TODO: 로또 번호 출력
+        System.out.println(purchaseNumber + "개를 구매했습니다.");
+        List<List<Integer>> purchaseLottoNumbers = new ArrayList<>();
+        for (int i = 0; i < purchaseNumber; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoNumbers.sort(Integer::compareTo);
+
+            System.out.print("[");
+            lottoNumbers.forEach(num -> System.out.print(num + ", "));
+            System.out.println("]");
+
+            purchaseLottoNumbers.add(lottoNumbers);
+        }
 
         System.out.println("당첨 번호를 입력해 주세요.");
-        List<Integer> numbers = Arrays.stream(Console.readLine().split(","))
+        List<Integer> inputWinningNumbers = Arrays.stream(Console.readLine().split(","))
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .toList();
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        int bonusNumber = Integer.parseInt(Console.readLine());
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int inputBonusNumber = Integer.parseInt(Console.readLine());
 
-        System.out.println("당첨 통계\n---\n");
-        // TODO: 당첨 갯수 출력
+        int[] ranking = new int[6 + 1];
+        for (List<Integer> lottoNumbers : purchaseLottoNumbers) {
+            Integer matchingNumberCount = lotto.getMatchingNumberCount(lottoNumbers, inputBonusNumber);
 
-        System.out.println("총 수익률은 " + null + "%입니다.");
+            if(matchingNumberCount == 5 && lotto.isBonusNumberMatching(inputBonusNumber)) {
+                ranking[0]++;
+            }
+
+            ranking[matchingNumberCount]++;
+        }
+
+        System.out.println("당첨 통계\n---");
+        System.out.println(WinningCriteria.FIFTH_PLACE.getInformation() + ranking[3]);
+        System.out.println(WinningCriteria.FOURTH_PLACE.getInformation() + ranking[4]);
+        System.out.println(WinningCriteria.THIRD_PLACE.getInformation() + ranking[5]);
+        System.out.println(WinningCriteria.SECOND_PLACE.getInformation() + ranking[0]);
+        System.out.println(WinningCriteria.FIRST_PLACE.getInformation() + ranking[6]);
+
+        int revenue = 5_000 * ranking[3] + 50_000 * ranking[4] + 1_500_000 * ranking[5] + 30_000_000 * ranking[0]
+                + 2_000_000_000 * ranking[6];
+
+        System.out.printf("총 수익률은 %.1f%%입니다.", (float)revenue / purchaseAmount * 100);
         Console.close();
     }
 }
