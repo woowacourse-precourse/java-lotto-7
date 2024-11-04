4cdfb30ea7f804af10ec174231ed3ccc651ec960
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 20:22:06 +0900

    refactor: LottoGenerator 클래스 패키지 이동

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,6 +1,7 @@
-package lotto.domain.lotto;
+package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.lotto.Lotto;
 
 import java.util.List;
 import java.util.Objects;
