23ba25f98488206ab26b2cf7817e5ca460b71f86
seunghui-park <ridia1233@naver.com>
2024-11-04 22:38:18 +0900

    fix(LottoMachine): 발급된 로또 번호 오름차순 정렬 적용
    - LottoMachine의 generateLottoNumbers 메서드에서 로또 번호를 오름차순으로 정렬하여 반환
    - Collections.sort를 사용해 로또 번호가 항상 정렬된 상태로 출력되도록 수정

--- src/main/java/lotto/Model/LottoMachine.java
+++ src/main/java/lotto/Model/LottoMachine.java
@@ -2,6 +2,7 @@ package lotto.Model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
@@ -11,7 +12,7 @@ public class LottoMachine {
     private static final int LOTTO_SIZE = 6;
 
     public static List<Lotto> generateLottos(int purchaseAmount) {
-        validatePurchaseAmount(purchaseAmount);  // 구입 금액 검증
+        validatePurchaseAmount(purchaseAmount);
 
         int count = purchaseAmount / LOTTO_PRICE;
         List<Lotto> lottos = new ArrayList<>();
@@ -21,13 +22,15 @@ public class LottoMachine {
         return lottos;
     }
 
-    public static void validatePurchaseAmount(int purchaseAmount) {
+    private static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
     }
 
-    public static List<Integer> generateLottoNumbers() {
-        return new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE));
+    private static List<Integer> generateLottoNumbers() {
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE));
+        Collections.sort(numbers);
+        return numbers;
     }
 }
