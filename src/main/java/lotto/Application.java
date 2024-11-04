7e8756d494c3fc546efce47c98d12326dece9d98
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-04 16:23:38 +0900

    feat: Implement getLottoNumbers func
    로또 번호를 받아오는 함수를 구현 했습니다.
    
    하나의 리스트로 관리 하는게 좋을 것 같아 보너스 번호도 리스트에 넣어주었습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,16 +2,39 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     private static final String error_message = "[ERROR]";
 
     public static void main(String[] args) {
         try {
             int purchaseAmount = getPurchaseAmount();
+            List<Integer> lottoNumbers = getLottoNumbers();
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static List<Integer> getLottoNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        List<Integer> lottoNumbers = new ArrayList<>();
+        String[] numbers = input.split(",");
+
+        for (String number : numbers) {
+                lottoNumbers.add(Integer.parseInt(number.trim()));
+        }
+        lottoNumbers.sort(Integer::compareTo);
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusInput = Console.readLine();
+
+        Integer bonusNumber = Integer.parseInt(bonusInput.trim());
+        lottoNumbers.add(bonusNumber);
+
+        return lottoNumbers;
     }
 
     private static int getPurchaseAmount() {
@@ -22,7 +45,9 @@ public class Application {
             if (purchaseAmount % 1000 != 0){
                 throw new IllegalArgumentException(error_message + "로또 구입 금액은 1,000 단위로 나누어 떨어져야 합니다.");
             }
-            return purchaseAmount / 1000;
+            int numTickets = purchaseAmount / 1000;
+            System.out.println(numTickets + "개를 구매했습니다.");
+            return numTickets;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(error_message + "로또 금액은 숫자여야 합니다.");
         }
