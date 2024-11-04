f30c9b0894d8145b9d3990b73b57c1f7cf003cb5
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-04 17:50:14 +0900

    feat: Application 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,62 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String money = getMoney();
+
+        List<Lotto> lottoList = getRandomLottos(money);
+
+        LottoResultChecker lottoResultChecker = new LottoResultChecker();
+        lottoResultChecker.setWinningNumbers(getNumbers());
+        lottoResultChecker.setBonusNumber(getNumber());
+
+        Integer[] result = lottoResultChecker.getLottoResult(lottoList);
+
+        ResultReport resultReport = new ResultReport(result);
+        resultReport.printReport();
+    }
+
+    static String getMoney(){
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = readLine();
+        System.out.println();
+
+        return input;
+    }
+
+    static List<Lotto> getRandomLottos(String money){
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        try{
+            lottoGenerator.purchaseLotto(money);
+            System.out.println();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return lottoGenerator.lottoList;
+    }
+
+    static String getNumbers(){
+
+        System.out.println("당첨 번호를 입력해주세요.");
+        String input1 = readLine();
+        System.out.println();
+
+        return input1;
+    }
+
+    static String getNumber(){
+
+        System.out.println("보너스 번호를 입력해주세요.");
+        String input2 = readLine();
+        System.out.println();
+
+        return input2;
     }
 }
