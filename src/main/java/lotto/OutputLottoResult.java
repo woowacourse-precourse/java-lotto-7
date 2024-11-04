6a4c27c71b013b2de06f8baa5e62f9d0a12da0be
Shrimptempura <ongtzxowns98@gmail.com>
2024-11-04 18:02:12 +0900

    feat: 출력문구 클래스

---
+++ src/main/java/lotto/OutputLottoResult.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import java.util.List;
+
+public class OutputLottoResult {    // 로또 결과 출력
+    public void printLottoInfo (int lottoCount, List<Lotto> purchasedLotto) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        for (Lotto lotto : purchasedLotto) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void printLottoResults(int[] lottoResult, int totalPrize, int puchaseAmount) {
+        System.out.println("당첨 내역 출력");
+        printLottoResult("3개 일치 (5,000원)", lottoResult[4]);
+        printLottoResult("4개 일치 (50,000원)", lottoResult[3]);
+        printLottoResult("5개 일치 (1,500,000원)", lottoResult[2]);
+        printLottoResult("5개 일치, 보너스 볼 일치 (30,000,000원)", lottoResult[1]);
+        printLottoResult("6개 일치 (2,000,000,000원)", lottoResult[0]);
+    }
+
+    private void printLottoResult(String message, int count) {
+        System.out.println(message + " - " + count + "개");
+    }
+}
