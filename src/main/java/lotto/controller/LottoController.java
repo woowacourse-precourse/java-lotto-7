2bb8252553aa3141e95da17611878e1bc8aa8551
yooo <ghddbtl@gmail.com>
2024-11-04 18:59:18 +0900

    feat : create LottoController.java
    지금 까지 만들었던 유효성 검사를 호출하고 로또를 생성하여 당첨티켓을 골라낸수 최종으로 출력되게끔 했다.

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,62 @@
+package lotto.controller;
+
+import lotto.model.Lotto;
+import lotto.model.LottoDomain;
+import lotto.model.LottoService;
+import lotto.status.LottoConstants;
+import lotto.validator.Amount;
+import lotto.validator.BonusNumber;
+import lotto.validator.LuckyNumbers;
+import lotto.view.Output;
+
+import java.util.List;
+
+public class LottoController {
+    LottoService lottoService;
+    List<Lotto> lottoTickets;
+    private List<Integer> luckyNumbers;
+    private int bonusNumber;
+    private int amount;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        lottoTickets = purchaseLotto();
+        Output.printLottoTickets(lottoTickets);
+
+        setLuckyNumbers();
+
+        new Output(ticketRank(), profitRate());
+    }
+
+
+    private double profitRate() {
+        return lottoService.calculateProfitRate(sumPrize(), amount);
+    }
+
+    private long sumPrize() {
+        return lottoService.sumPrize(ticketRank());
+    }
+
+    private int[] ticketRank() {
+        return lottoService.calculateTicketRank(lottoTickets, luckyNumbers, bonusNumber);
+    }
+
+    private List<Lotto> purchaseLotto() {
+        amount = new Amount().processSetAmount()
+                             .getAmount();
+        LottoDomain lotto = new LottoDomain();
+
+        return lotto.createLottoBundle(amount / LottoConstants.LOTTO_UNIT_PRICE);
+    }
+
+    private void setLuckyNumbers() {
+        luckyNumbers = new LuckyNumbers().processSetWinningNumbers()
+                                         .getLuckyNumbers();
+        bonusNumber = new BonusNumber(luckyNumbers).processSetBonusNumber()
+                                                   .getBonusNumber();
+    }
+
+}
