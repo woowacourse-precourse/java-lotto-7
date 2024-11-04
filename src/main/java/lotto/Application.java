53a69574f5cb0d23cd4831fd6414d03d38c35ae9
hongjungin <jungini0000@gmail.com>
2024-11-04 20:19:07 +0900

    refactor: 메인 메서드 로직을 메서드로 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,11 +16,7 @@ public class Application {
         // 구입 금액 입력 및 검증
         int purchaseAmount = InputView.getPurchaseAmount();
 
-        // 로또 번호 생성
-        List<List<Integer>> lottoTickets = LottoGenerator.generateLottoTickets(purchaseAmount);
-
-        // 구매한 로또 번호 출력
-        OutputView.printPurchasedLottoTickets(lottoTickets);
+        List<List<Integer>> lottoTickets = generateAndPrintLottoTickets(purchaseAmount);
 
         // 당첨 번호 입력 및 검증
         List<Integer> winningNumbers = InputView.getWinningNumbers();
@@ -29,16 +25,7 @@ public class Application {
         int bonusNumber = InputView.getBonusNumber(winningNumbers);
 
         // 로또 당첨 결과 계산
-        Map<LottoRank, Integer> rankCounts = new HashMap<>();
-        for (List<Integer> ticket : lottoTickets) {
-            Lotto lotto = new Lotto(ticket);
-            int matchCount = lotto.matchCount(winningNumbers);
-            boolean isBonusMatch = lotto.isBonusMatch(bonusNumber);
-
-            // 로또 등수 계산
-            LottoRank rank = LottoRank.determineRank(matchCount, isBonusMatch);
-            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
-        }
+        Map<LottoRank, Integer> rankCounts = calculateLottoResults(lottoTickets, winningNumbers, bonusNumber);
 
         // 당첨 통계 출력
         OutputView.printLottoStatistics(rankCounts);
@@ -50,5 +37,23 @@ public class Application {
         double revenueRate = LottoResultCalculator.calculateRevenueRate(totalPrize, purchaseAmount);
         OutputView.printRevenueRate(revenueRate);
 
+    }
+
+    private static List<List<Integer>> generateAndPrintLottoTickets(int purchaseAmount) {
+        List<List<Integer>> lottoTickets = LottoGenerator.generateLottoTickets(purchaseAmount);
+        OutputView.printPurchasedLottoTickets(lottoTickets);
+        return lottoTickets;
+    }
+
+    private static Map<LottoRank, Integer> calculateLottoResults(List<List<Integer>> lottoTickets, List<Integer> winningNumbers, int bonusNumber) {
+        Map<LottoRank, Integer> rankCounts = new HashMap<>();
+        for (List<Integer> ticket : lottoTickets) {
+            Lotto lotto = new Lotto(ticket);
+            int matchCount = lotto.matchCount(winningNumbers);
+            boolean isBonusMatch = lotto.isBonusMatch(bonusNumber);
+            LottoRank rank = LottoRank.determineRank(matchCount, isBonusMatch);
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        }
+        return rankCounts;
     }
 }
