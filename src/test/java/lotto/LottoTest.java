cf4dd29c6327a80d5a09c4134b9dae8d23e071de
bongj9 <lollypark501@naver.com>
2024-11-04 22:30:00 +0900

    feat(LottoTest):랜덤으로 로또 번호 생성되는지 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -21,6 +21,7 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
     //로또번호 1-45사이의 숫자의 검증
     @DisplayName("로또 번호가 1과 45 사이의 숫자가 아니면 예외가 발생한다")
     @Test
@@ -35,6 +36,7 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 1과 45사이의 숫자입니다");
     }
+
     //정상적인 객체생성하는지 확인  && 정렬 확인
     @DisplayName("정상적인 로또 번호 리스트로 객체를 생성할 수 있다")
     @Test
@@ -42,5 +44,19 @@ class LottoTest {
         Lotto lotto = new Lotto(List.of(8, 21, 35, 11, 42, 7));
 
         assertThat(lotto.getNumbers()).containsExactly(7, 8, 11, 21, 35, 42);
+    }
+
+    //랜덤으로 로또 번호 생성되는지 확인
+    @DisplayName("랜덤으로 로또 번호를 생성할 수 있다")
+    @Test
+    void 로또_랜덤_번호_생성() {
+        Lotto lotto = Lotto.generateRandomNumbers();
+
+        assertThat(lotto.getNumbers()).hasSize(6);
+
+        assertThat(lotto.getNumbers()).allMatch(number -> number >= 1 && number <= 45);
+
+        long uniqueCount = lotto.getNumbers().stream().distinct().count();
+        assertThat(uniqueCount).isEqualTo(6);
     }
 }
