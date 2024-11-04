8b9c0c54011b0a8a4afa8ddbea577461cac02556
LSM <leesung2925@gmail.com>
2024-11-04 19:33:17 +0900

    refactor: Random 로또 번호 생성 시 ArrayList로 변환하여 가변 리스트로 저장

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.valid.LottoNumbersValidator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,11 +21,11 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(numbers);
+        return numbers;
     }
 
     public static Lotto generateRandomLotto() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> randomNumbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         Collections.sort(randomNumbers);
         return new Lotto(randomNumbers);
     }
