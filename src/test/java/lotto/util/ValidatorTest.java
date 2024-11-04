473873f0e92c2a6ff94c543bdad4644596792b8b
tjfwltn <tjfwltn1234@gmail.com>
2024-11-04 13:41:15 +0900

    test : Validator 테스트 코드 다시 작성
    - 하나의 Validator로 합쳤기 때문에
    - 각각 맞는 메서드를 호출하는 테스트 케이스들 작성

---
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -0,0 +1,69 @@
+package lotto.util;
+
+import lotto.constants.LottoErrorMessage;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.constants.LottoErrorMessage.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class ValidatorTest {
+
+    @Test
+    void zeroTest() {
+        //given
+        int input = 0;
+        // when & then
+        assertThatThrownBy(() -> Validator.validateTotalAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void hasMinimumTest() {
+        // given
+        int input = 999;
+        // when & then
+        assertThatThrownBy(() -> Validator.validateTotalAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isDivisibleByMinimumAmountTest() {
+        // given
+        int input = 1999;
+        // when & then
+        assertThatThrownBy(() -> Validator.validateTotalAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void sizeTest() {
+        // given
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // when & then
+        assertThatThrownBy(() -> Validator.validateIntList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NUMBER_SIZE.getMessage());
+    }
+
+    @Test
+    void duplicateTest() {
+        // given
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 5);
+        // when & then
+        assertThatThrownBy(() -> Validator.validateIntList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(DUPLICATE_INPUT_NUMBER.getMessage());
+    }
+
+    @Test
+    void rangeTest() {
+        // given
+        List<Integer> input = List.of(1,12,3,4,5,46);
+        // when & then
+        assertThatThrownBy(() -> Validator.validateIntList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NUMBER_OUT_OF_RANGE.getMessage());
+    }
+}
