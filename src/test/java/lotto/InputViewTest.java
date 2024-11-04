b7adae532cfe915190e12f60837308aa486d76f7
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 12:37:48 +0900

    test(InputViewTest): 당첨 번호 및 보너스 번호 입력 기능에 대한 테스트 코드 추가
    - InputView 클래스의 inputWinningNumber와 inputBonusNumber 메소드를 테스트하는 코드를 작성했습니다.
    - 올바른 당첨 번호와 보너스 번호가 입력될 때 정상적으로 파싱되는지 확인합니다.
    - 당첨 번호와 보너스 번호의 유효성을 검증하여 입력에 따른 예외 처리가 올바르게 작동하는지 확인g합니다.

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,7 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class InputViewTest extends NsTest {
@@ -21,6 +25,39 @@ public class InputViewTest extends NsTest {
         }).isInstanceOf(NumberFormatException.class);
     }
 
+    @Test
+    @DisplayName("당첨 번호 입력 - 정상 입력")
+    void 당첨번호_입력_정상입력() {
+        run("1, 2, 3, 4, 5, 6");
+        List<Integer> winningNumbers = InputView.inputWinningNumber();
+        assertThat(winningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 - 비정상 입력 예외 발생")
+    void 당첨번호_입력_비정상입력_예외발생() {
+        run("1, 2, 3, a, 5, 6");
+        assertThatThrownBy(InputView::inputWinningNumber)
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessageContaining("For input string: \"a\"");
+    }
+
+    @Test
+    @DisplayName("보너스 번호 입력 - 정상 입력")
+    void 보너스번호_입력_정상입력() {
+        run("7");
+        int bonusNumber = InputView.inputBonusNumber();
+        assertThat(bonusNumber).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 입력 - 비정상 입력 예외 발생")
+    void 보너스번호_입력_비정상입력_예외발생() {
+        run("notANumber");
+        assertThatThrownBy(InputView::inputBonusNumber)
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessageContaining("For input string: \"notANumber\"");
+    }
     @Override
     protected void runMain() {
 
