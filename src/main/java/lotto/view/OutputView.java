cd79cb0d348f027fcc01463f81bf5a26432380ce
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 17:44:04 +0900

    feat(OutputView): 출력 View 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,26 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.LottoResult;
+import lotto.model.Money;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+
 public class OutputView {
+
+    public static void printBuyLottoCount(Money money){
+        System.out.printf(("%d개를 구매했습니다.") + "%n", money.getLottoCount());
+    }
+
+    public static void printLottoList(List<Lotto> lottos){
+        lottos.getLotto().forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printLottResult(LottoResult result){
+        System.out.println("당첨통계");
+        System.out.println("---");
+    }
+
 }
