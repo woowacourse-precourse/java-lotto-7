baa99c717ecf6b535b0c7fc83ea5d8710f29865d
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 19:58:01 +0900

    fix(Lotto): 하드 코딩 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final static int LOTTO_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -16,7 +17,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
