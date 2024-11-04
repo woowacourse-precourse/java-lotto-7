45162c612ac3f065bab66667ec1b1f04854119e0
YunCheol1 <naro2727@naver.com>
2024-11-04 20:06:43 +0900

    feat(Config): 에러메시지 추가

--- src/main/java/lotto/util/Config.java
+++ src/main/java/lotto/util/Config.java
@@ -8,5 +8,5 @@ public class Config {
     public static final String ERROR_OUT_OF_RANGE_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     public static final String ERROR_BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
     public static final String ERROR_INVALID_WINNING_NUMBER_COUNT = "[ERROR] 6개의 숫자를 쉼표로 구분하여 입력해 주세요.";
-
+    public static final String ERROR_INVALID_LOTTO_NUMBER_COUNT = "[ERROR] 로또 번호는 6개여야 합니다.";
 }
