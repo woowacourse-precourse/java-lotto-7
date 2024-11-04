5ecf4328d9f90301972d258da24f810c2ff09ad9
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-11-04 19:46:14 +0900

    feat: 당첨결과 확인 클래스추가

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -1,7 +1,6 @@
-package lotto.dto;
+package lotto.domain;
 
-import lotto.domain.Ranking;
-
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -9,14 +8,52 @@ import java.util.stream.Collectors;
 
 public class Result {
 
+    private static final Integer LOTTO_PRICE = 1000;
+
     private long totalPrice;
-    private Map<Ranking,Integer> rankResults;
+    private double profitRate;
+    private Map<Ranking,Long> rankResults;
+
+    public Result(List<Ranking> ranks) {
+        this.totalPrice = calcTotalPrice(ranks);
+        this.rankResults = countRanks(ranks);
+        this.profitRate = calcProfitRate(ranks);
+    }
 
     private Map<Ranking, Long> countRanks(List<Ranking> ranks) {
+        Map<Ranking, Long> result = Arrays.stream(Ranking.values())
+                .collect(Collectors.toMap(Function.identity(), rank -> 0L));
+
+        ranks.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach(result::put);
+        return result;
+    }
+
+    private long calculatePaidAmount(List<Ranking> ranks) {
+        return ranks.size() * LOTTO_PRICE;
+    }
+
+    private double calcProfitRate(List<Ranking> ranks) {
+        long amount = calculatePaidAmount(ranks);
+        return (double) totalPrice / amount * 100;
+    }
+
+    private long calcTotalPrice(List<Ranking> ranks) {
         return ranks.stream()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting())
-                );
+                .mapToLong(Ranking::getPrice)
+                .sum();
+    }
+
+    public long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+
+    public Map<Ranking, Long> getRankResults() {
+        return rankResults;
     }
 }
