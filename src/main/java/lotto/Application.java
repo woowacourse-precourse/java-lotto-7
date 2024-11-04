1a460cf1f3abe7af24e9763fe40c1527bf3a296b
bbangHo <sonsins1@naver.com>
2024-11-04 14:05:03 +0900

    feat: 입출력 코드를 간단하게 구현한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
+
+        System.out.println(purchaseAmount / 1000 + "개를 구매했습니다.");
+        // TODO: 로또 번호 출력
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> numbers = Arrays.stream(Console.readLine().split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        int bonusNumber = Integer.parseInt(Console.readLine());
+
+        System.out.println("당첨 통계\n---\n");
+        // TODO: 당첨 갯수 출력
+
+        System.out.println("총 수익률은 " + null + "%입니다.");
+        Console.close();
     }
 }
