6c78b21199a2829ed1413c2a13cf26e6610c847b
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-04 19:18:10 +0900

    refactor(ConsoleInputView): 매직 넘버 상수화 및 공백 제거 부분 수정

--- src/main/java/lotto/view/console/ConsoleInputView.java
+++ src/main/java/lotto/view/console/ConsoleInputView.java
@@ -8,17 +8,19 @@ import lotto.view.InputView;
 public class ConsoleInputView implements InputView {
 
     private static final String DELIMITER_COMMA = ",";
+    private static final String REGEX_SPACE = "\\s";
+    private static final String BLANK = "";
 
     @Override
     public int readNumber() {
         final String input = readInput();
-        return InputConvertor.parseToInt(input.trim());
+        return InputConvertor.parseToInt(input.replaceAll(REGEX_SPACE, BLANK));
     }
 
     @Override
     public List<Integer> readNumbers() {
         final String input = readInput();
-        return InputConvertor.parseToNumbers(DELIMITER_COMMA, input.trim());
+        return InputConvertor.parseToNumbers(DELIMITER_COMMA, input.replaceAll(REGEX_SPACE, BLANK));
     }
 
     private String readInput() {
