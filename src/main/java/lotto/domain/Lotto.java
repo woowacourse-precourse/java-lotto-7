79eb5bbad6cc1d6f16d40ca1604f907d364e372b
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 22:51:36 +0900

    fix: Lotto 클래스 validate 메서드 수정 로또 번호가 1~45 범위를 벗어날 경우 예외 발생

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -21,6 +21,11 @@ public class Lotto {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (numbers.size() != uniqueNumbers.size())
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 숫자로 이루어져야 합니다.");
+
+        for(int number : numbers) {
+            if (number < 1 || number > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위여야 합니다.");
+        }
     }
 
     public List<Integer> getNumbers() {
