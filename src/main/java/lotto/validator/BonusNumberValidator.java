5c8263ea41ffa2902e148676e52a5c63bf19f740
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 11:58:15 +0900

    feat(BonusNumberValidator): 당첨 번호와 중복 확인 기능 추가

--- src/main/java/lotto/validator/BonusNumberValidator.java
+++ src/main/java/lotto/validator/BonusNumberValidator.java
@@ -3,11 +3,20 @@ package lotto.validator;
 import static lotto.validator.ValidatorUtils.BONUS_NUMBER_ERROR_MESSAGE;
 import static lotto.validator.ValidatorUtils.isNumberInRange;
 
+import java.util.List;
+
 public class BonusNumberValidator implements Validator<Integer> {
 
     @Override
     public void validate(Integer bonusNumber) {
         validateBonusNumberRange(bonusNumber);
+    }
+
+    public void validateDuplicate(Integer bonusNumber, List<Integer> winningNumbers) {
+        validate(bonusNumber);
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private void validateBonusNumberRange(Integer bonusNumber) {
