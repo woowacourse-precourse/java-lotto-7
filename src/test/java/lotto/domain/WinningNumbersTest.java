fb792dd02c06a56e1d81b3f089f791c065200be5
이준범 <jach1206@naver.com>
2024-11-04 21:39:45 +0900

    test: 당첨 번호 입력시 발생하는 예외에 대한 테스트 코드 작성
    - WinningNumberParserTest 클래스: 사용자에게 입력 받을 때의 형식과 관련된 예외 처리 테스트 코드를 작성했다.
    
    - WinningNumbersTest 클래스: 입력 받은 값을 WinningNumbers 클래스로 변환 후,
    번호가 6개이거나, 중복된 번호가 존재하는지, 1~45 사이를 벗어난 값이 존재하는지 등
    WinningNumbers 클래스와 관련된 예외 처리 테스트 코드를 작성했다.

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+
+    private static final List<Integer> FIVE_NUMBER = Arrays.asList(1, 2, 3, 4, 5);
+    private static final List<Integer> DUPLICATED_NUMBER = Arrays.asList(1, 1, 2, 3, 4, 5);
+    private static final List<Integer> OUT_OF_RANGE = Arrays.asList(1, 2, 3, 4, 5, 46);
+
+    @Test
+    void 번호가_6개가_아닌_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> WinningNumbers.initialInstance(FIVE_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 중복된_번호가_있는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> WinningNumbers.initialInstance(DUPLICATED_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 범위를_벗어난_번호가_있는_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> WinningNumbers.initialInstance(OUT_OF_RANGE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
