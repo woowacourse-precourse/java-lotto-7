fcee6ecca533a7630ca853a0b5572135501f0811
ratatuwee <pkqwaszx@gmail.com>
2024-11-04 20:45:51 +0900

    refactor(application controller) : 예외 메시지 출력 추가
    기본 테스트 조건에 맞춰 예외가 발생하면 메시지를 출력하게 추가하였습니다.

--- src/main/java/lotto/controller/ApplicationController.java
+++ src/main/java/lotto/controller/ApplicationController.java
@@ -4,13 +4,18 @@ import lotto.view.UserInput;
 
 public class ApplicationController {
     private final UserInputController userInputController = new UserInputController();
-    private final LottoServiceController lottoServiceController;
+    private LottoServiceController lottoServiceController;
     public ApplicationController(){
-        userInputController.purchaseAmountInput();
-        lottoServiceController = new LottoServiceController();
-        lottoServiceController.displayUserLottos();
-        userInputController.winningLottoInput();
-        lottoServiceController.displayWinRecord();
-        lottoServiceController.displayTotalYield();
+        try{
+            userInputController.purchaseAmountInput();
+            lottoServiceController = new LottoServiceController();
+            lottoServiceController.displayUserLottos();
+            userInputController.winningLottoInput();
+            lottoServiceController.displayWinRecord();
+            lottoServiceController.displayTotalYield();
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
