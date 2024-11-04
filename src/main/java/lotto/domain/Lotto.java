c9ad1fc1deb36ff4155b655eb1e2909320523c85
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-04 21:05:33 +0900

    feat : 로또 번호가 범위에 알맞지 않은경우 예외처리, 중복된 경우 예외처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.common.LottoConstant.LOTTO_MAX_NUMBER;
+import static lotto.common.LottoConstant.LOTTO_MIN_NUMBER;
+import static lotto.common.LottoConstant.LOTTO_NUMBER_COUNT;
+
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +11,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumberRange(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -25,8 +31,27 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        boolean isOutOfRange = numbers.stream()
+                .anyMatch(number -> number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER);
+
+        if (isOutOfRange) {
+            throw new IllegalArgumentException("숫자는 1에서 45 사이여야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        boolean isDuplicate = numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+
+        if (isDuplicate) {
+            throw new IllegalArgumentException("로또 번호가 중복되서는 안됩니다.");
         }
     }
 }
