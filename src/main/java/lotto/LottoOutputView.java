b018974597e1e2af548dfdc95197e2acada41d91
soojinlee <sojinlee1004@naver.com>
2024-11-04 21:10:55 +0900

    refactor : 로또 결과 출력 코드 리팩토링

--- src/main/java/lotto/LottoOutputView.java
+++ src/main/java/lotto/LottoOutputView.java
@@ -3,27 +3,44 @@ package lotto;
 import java.util.*;
 
 public class LottoOutputView {
+
+    private static void printLottoCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
     public static void printPurchasedLottoCount(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매했습니다.");
+        printLottoCount(lottos.size());
         for (Lotto lotto : lottos) {
             List<Integer> numbers = lotto.getNumbers();
-            Collections.sort(numbers);
-            System.out.println(numbers);
+            printSortedNumber(numbers);
         }
+    }
+
+    private static void printSortedNumber(List<Integer> numbers) {
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+
+    private static void printRankResults(Map<LottoRank, Integer> result, List<LottoRank> sortedRanks) {
+        for (LottoRank rank : sortedRanks) {
+            int count = result.getOrDefault(rank, 0);
+            String formattedPrize = String.format("%,d", rank.getPrize());
+            System.out.printf("%s (%s원) - %d개%n", rank.getRank(), formattedPrize, count);
+        }
+    }
+
+    private static List<LottoRank> sortedRanks() {
+        List<LottoRank> sortedRanks = Arrays.asList(LottoRank.values());
+        sortedRanks.sort(Comparator.comparingInt(LottoRank::getMatchCount));
+        return sortedRanks;
     }
 
     public static void printResult(Map<LottoRank, Integer> result) {
         System.out.println("당첨 통계");
         System.out.println("---");
 
-        List<LottoRank> sortedRanks = Arrays.asList(LottoRank.values());
-        sortedRanks.sort(Comparator.comparingInt(LottoRank::getMatchCount));
+        List<LottoRank> sortedRanks = sortedRanks();
 
-        for (LottoRank rank : sortedRanks) {
-            int count = result.getOrDefault(rank, 0);
-            String formattedPrize = String.format("%,d", rank.getPrize());
-            System.out.printf("%s (%s원) - %d개%n", rank.getRank(), formattedPrize, count);
-        }
+        printRankResults(result, sortedRanks);
     }
 
     public static void printrateOfReturn(double rate) {
