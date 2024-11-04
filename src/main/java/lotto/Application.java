62b164c6d31a27f81546e17bbcbf541fdd9eb919
김현재 <nowalex322@naver.com>
2024-11-04 10:15:31 +0900

    feat: 당첨 번호 입력 및 검증 기능 구현
    - 당첨 번호 검증 로직 구현 (범위, 중복 체크)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.view.view.InputView;
+import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
 import lotto.view.view.OutputView;
 
 import java.util.ArrayList;
@@ -10,20 +11,33 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         try {
-            int amount = InputView.readPurchaseAmount();
-            int count = amount / 1000;
+            List<Lotto> purchasedLottos = purchaseLottos();
+            OutputView.printPurchaseCount(purchasedLottos.size());
+            OutputView.printLottos(purchasedLottos);
 
-            List<Lotto> lottos = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-                lottos.add(new Lotto(numbers));
-            }
-
-            OutputView.printPurchaseCount(count);
-            OutputView.printLottos(lottos);
+            WinningNumbers winningNumbers = inputWinningNumbers();
+            // 다음 단계에서 당첨 결과 계산 및 출력 구현 예정
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static List<Lotto> purchaseLottos() {
+        int amount = InputView.readPurchaseAmount();
+        int count = amount / 1000;
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private static WinningNumbers inputWinningNumbers() {
+        Lotto winningLotto = InputView.readWinningNumbers();
+        int bonusNumber = InputView.readBonusNumber();
+        return new WinningNumbers(winningLotto, bonusNumber);
     }
 }
