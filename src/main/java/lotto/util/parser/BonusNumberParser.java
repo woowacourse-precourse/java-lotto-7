6261f8c9a7140109ae52d6beec1dafe3a0e9eefd
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 21:07:14 +0900

    fix(BonusNumberParser): 에러 메세지 수정

--- src/main/java/lotto/util/parser/BonusNumberParser.java
+++ src/main/java/lotto/util/parser/BonusNumberParser.java
@@ -1,11 +1,13 @@
 package lotto.util.parser;
 
 public class BonusNumberParser {
+    private static final String INVALID_FORMAT_ERROR = "보너스 번호는 숫자만 입력 가능합니다.";
+
     public static int toIntStringBonusNumberParser(String bonusNumber) throws IllegalArgumentException {
         try {
             return Integer.parseInt(bonusNumber);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("보너스 번호는 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
         }
     }
 }
