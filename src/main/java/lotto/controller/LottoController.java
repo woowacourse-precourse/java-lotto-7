aa8809c4f1ad5fb39ed8e18cce5eaf074a04bfb3
JEN <ekffuclxk@gmail.com>
2024-11-04 17:19:08 +0900

    refactor(LottoController): 미사용 메서드 삭제

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -37,18 +37,4 @@ public class LottoController {
 
         double profitRate = profitCalculator.calculateProfitRate(matchedLotto,amount);
     }
-
-    private void printStatistics(Map<LottoRule, Integer> statistics, int amount) {
-        System.out.println("당첨 통계\n---");
-        int totalPrize = 0;
-
-        for (LottoRule rule : LottoRule.values()) {
-            int count = statistics.getOrDefault(rule, 0);
-            System.out.printf("%s - %d개\n", rule.getInfo(), count);
-            totalPrize += count * rule.getPrize();
-        }
-
-        double profitRate = (double) totalPrize / amount * 100;
-        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
-    }
 }
