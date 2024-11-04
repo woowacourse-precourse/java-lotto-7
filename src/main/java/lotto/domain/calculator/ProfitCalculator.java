362ed51d2a8889bc7bb560af508b16d987e078bd
hootzz <wjqwjql3@gmail.com>
2024-11-04 14:49:28 +0900

    feat(calculator): profit 계산 관리 ProfitCalculator class 구현

--- src/main/java/lotto/domain/calculator/ProfitCalculator.java
+++ src/main/java/lotto/domain/calculator/ProfitCalculator.java
@@ -1,4 +1,21 @@
 package lotto.domain.calculator;
 
+import lotto.domain.money.Money;
+import lotto.domain.result.WinningResult;
+import lotto.domain.result.WinningStatistics;
+
 public class ProfitCalculator {
+    public WinningResult calculateResult(WinningStatistics statistics, Money purchaseMoney) {
+        Money totalPrize = statistics.calculateTotalPrize();
+        double profitRate = calculateProfitRate(purchaseMoney, totalPrize);
+        return createWinningResult(statistics, profitRate);
+    }
+
+    private double calculateProfitRate(Money purchaseMoney, Money totalPrize) {
+        return purchaseMoney.calculateProfitRate(totalPrize);
+    }
+
+    private WinningResult createWinningResult(WinningStatistics statistics, double profitRate) {
+        return WinningResult.of(statistics, profitRate);
+    }
 }
