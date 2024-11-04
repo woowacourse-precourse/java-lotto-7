28b38ae657939bba3a2c2b0f2b8ced55f2fb809b
SyingSHY <posti1x38@gmail.com>
2024-11-04 21:02:27 +0900

    fix: 테스트 내 문법 오류 수정

--- src/test/java/lotto/InputHandlerTest.java
+++ src/test/java/lotto/InputHandlerTest.java
@@ -9,21 +9,24 @@ public class InputHandlerTest {
     @DisplayName("입력된 구입 금액이 1000 단위가 아닌 경우 예외가 발생한다")
     @Test
     void 입력된_구입_금액이_1000_단위가_아닌_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new InputHandler.readPurchaseAmount("1001")
+        InputHandler inputHandler = new InputHandler();
+        assertThatThrownBy(() -> inputHandler.readPurchaseAmount("1001"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("입력된 구입 금액이 1000 미만인 경우 예외가 발생한다")
     @Test
     void 입력된_구입_금액이_1000_미만인_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new InputHandler.readPurchaseAmount("990")
+        InputHandler inputHandler = new InputHandler();
+        assertThatThrownBy(() -> inputHandler.readPurchaseAmount("990"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("입력 중 숫자가 아닌 다른 값이 들어올 경우 예외가 발생한다")
     @Test
     void 입력_중_숫자가_아닌_다른_값이_들어올_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new InputHandler.readPurchaseAmount("1000ab")
+        InputHandler inputHandler = new InputHandler();
+        assertThatThrownBy(() -> inputHandler.readPurchaseAmount("1000ab"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
