18dfe917c12100e2c9948c71d8eb8fa5e4fe84d2
oznchex <114240463+oznchex@users.noreply.github.com>
2024-11-04 13:51:26 +0900

    feat(domain): 로또 보너스 번호를 저장하는 객체 생성
    - LottoBonus 클래스는 보너스 번호를 가지고 있다.
    - 해당 클래스 객체를 통해 보너스 번호를 조회할 수 있다.

---
+++ src/main/java/lotto/domain/LottoBonus.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class LottoBonus {
+    private final int bonusNumber;
+
+    public LottoBonus(final int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
