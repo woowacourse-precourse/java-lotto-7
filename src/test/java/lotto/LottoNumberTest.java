68bbdea43aa5e63c01a88820ad1121fd490b8391
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:06:45 +0900

    test(LottoNumber): 로또 번호가 같은지 비교할 수 있다

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,7 +11,13 @@ public class LottoNumberTest {
 
     @ParameterizedTest(name = "{0}은 예외발생")
     @ValueSource(ints = {0, 46})
-    void 범위_밖의_로또번호는_예외를_발생시킨다(Integer number) {
+    void 범위_밖의_로또_번호는_예외를_발생시킨다(Integer number) {
         assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(number));
+    }
+
+    @Test
+    void 로또_번호가_같은지_비교할_수_있다() {
+        assertThat(new LottoNumber(1)).isEqualTo(new LottoNumber(1));
+        assertThat(new LottoNumber(2)).isNotEqualTo(new LottoNumber(1));
     }
 }
