a3b66d720faa78990adeae9df347c52ddce5ea62
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 21:08:47 +0900

    feat(view): 발행한 로또 번호를 출력하는 기능 추가
    -  printLottos() 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,17 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.model.Lotto;
+
 public class OutputView {
 
     public void printPurchasedLottoAmount(int amount) {
         System.out.println(amount + "개를 구매했습니다.");
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
