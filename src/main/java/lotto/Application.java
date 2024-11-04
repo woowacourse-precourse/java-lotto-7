244276923ad7e4c83eabb97fa3c03d7517969c05
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-11-04 23:01:42 +0900

    feat: 당첨 통계(수익률, 총 당첨 내역) 출력 기능 구현
    - 당첨 통계: 당첨 결과 배열을 인덱스를 이용해 꺼내어 각 당첨 케이스 별로 출력함
    
    - 수익률: 소수점 둘째 자리에서 반올림하도록 포맷하여 출력함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -71,9 +71,18 @@ public class Application {
             }
         }
 
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + matchCounts[3] + "개");
+        System.out.println("4개 일치 (50,000원) - " + matchCounts[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + matchCounts[5] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + matchCounts[7] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + matchCounts[6] + "개");
+
         long totalMoney = matchCounts[3] * 5_000L + matchCounts[4] * 50_000L +
                 matchCounts[5] * 1_500_000L + matchCounts[7] * 30_000_000L + matchCounts[6] * 2_000_000_000L;
 
         double returnRate = totalMoney * 100.0 / money;
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", returnRate);
     }
 }
