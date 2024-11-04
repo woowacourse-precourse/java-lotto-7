4202f7020dac3d540976db0501f258c787ff89ab
choi <nippert@naver.com>
2024-11-04 12:34:15 +0900

    test: 구입 금액으로부터 수익률과 로또개수를 계산 테스트 추가

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -1,12 +1,15 @@
 package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import view.exception.LottoException;
 import view.validation.InputValidator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static view.validation.InputErrorMessage.MONEY_EMPTY_MESSAGE;
 import static view.validation.InputErrorMessage.MONEY_FORMAT_MESSAGE;
@@ -37,5 +40,21 @@ public class MoneyTest {
     public void 구입금액_1000원_단위어야_한다(int money) {
         assertThatThrownBy(() -> new Money(money))
                 .isInstanceOf(LottoException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입금액과 수익금으로부터 수익률을 계산한다")
+    @CsvSource(value = {"1000, 16.666666666666664", "5000, 83.33333333333334", "12000, 200"})
+    public void 수익률_계산(int profit, double ratio) {
+        Money money = new Money(6000);
+
+        assertThat(money.calculateProfitRatio(profit)).isEqualTo(ratio);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구입금액으로부터 구매한 로또의 개수를 구한다")
+    @CsvSource(value = {"1000, 1", "40000, 40"})
+    public void 구매_로또개수_계산(int money, int count) {
+        assertThat(new Money(money).calculateCountLotto()).isEqualTo(count);
     }
 }
