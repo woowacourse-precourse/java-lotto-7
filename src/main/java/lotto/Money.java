777566e9c4ca1fea11b78bf1ab55c40ba3b2054b
전민정 <minj_1203@naver.com>
2024-11-04 22:56:18 +0900

    feat(money): 로또 구매 가능 개수

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -10,18 +10,23 @@ public class Money {
         this.amount = amount;
     }
 
-    private void validate(int amount){
+    private static void validate(int amount){
         validateIsNegative(amount);
         validateDivisible(amount);
     }
 
-    private void validateIsNegative(int amount){
+    private static void validateIsNegative(int amount){
         if(amount < ZERO)
             throw new IllegalArgumentException(ErrorMessage.AMOUNT_NEGATIVE_EXCEPTION.getMessage());
     }
 
-    private void validateDivisible(int amount){
+    private static void validateDivisible(int amount){
         if(amount % DIVISIBLE_UNIT != ZERO)
             throw new IllegalArgumentException(ErrorMessage.AMOUNT_INPUT_EXCEPTION.getMessage());
+    }
+
+    public static int getAmount(int amount){
+        validate(amount);
+        return amount/DIVISIBLE_UNIT;
     }
 }
