4947c6717fbc1170a09624ef064cd599634fb7f4
hoooonshub <yhn515@naver.com>
2024-11-04 20:57:17 +0900

    feat: 당첨 번호 입력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,16 +2,20 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
 public class Application {
+    private static final String DELIMITER = ",";
 
     public static void main(String[] args) {
         LottoShop shop = new LottoShop();
         int money = repeatUntilNoException(() -> inputMoney());
         List<Lotto> lottos = shop.sell(money);
         printLottos(lottos);
+
+        List<Integer> winningNumbers = repeatUntilNoException(() -> inputWinningNumbers());
     }
 
     private static int inputMoney() {
@@ -29,6 +33,31 @@ public class Application {
 
     private static void printLottos(List<Lotto> lottos) {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    private static List<Integer> inputWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String rawInput = Console.readLine().trim();
+
+        InputValidator.validateContainsDelimiter(rawInput);
+        String[] splitInputs = rawInput.split(DELIMITER);
+
+        List<Integer> numbers = parseNumbers(splitInputs);
+
+        InputValidator.validateSixNumbers(numbers);
+        InputValidator.validateWinningNumbersDuplicate(numbers);
+
+        return numbers;
+    }
+
+    private static List<Integer> parseNumbers(String[] splitInputs) {
+        return Arrays.stream(splitInputs)
+                .map(String::trim)
+                .map(input -> {
+                    InputValidator.validateNumberInLottoRange(input);
+                    return Integer.parseInt(input);
+                })
+                .toList();
     }
 
     private static <T> T repeatUntilNoException(Supplier<T> inputFunction) {
--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -1,9 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class InputValidator {
 	private static final int POSITIVE_NUMBER = 1;
 	private static final int LOTTO_AMOUNT_UNIT = 1000;
 	private static final int MAX_PURCHASE_AMOUNT = 100_000;
+	private static final String DELIMITER = ",";
+	private static final int REQUIRED_LOTTO_NUMBERS = 6;
+	private static final int MIN_LOTTO_NUMBER = 1;
+	private static final int MAX_LOTTO_NUMBER = 45;
 
 	public static void validateNaturalNumber(String input) {
 		if (input == null || input.isBlank()) {
@@ -30,6 +37,33 @@ public class InputValidator {
 	public static void validateMaxPurchaseAmount(int money) {
 		if (money > MAX_PURCHASE_AMOUNT) {
 			throw new IllegalArgumentException("[ERROR] 최대 구매 한도는 10만원입니다.");
+		}
+	}
+
+	public static void validateContainsDelimiter(String input) {
+		if (!input.contains(DELIMITER)) {
+			throw new IllegalArgumentException("[ERROR] 쉼표(,)를 구분자로 입력해주세요.");
+		}
+	}
+
+	public static void validateSixNumbers(List<Integer> numbers) {
+		if (numbers.size() != REQUIRED_LOTTO_NUMBERS) {
+			throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
+		}
+	}
+
+	public static void validateWinningNumbersDuplicate(List<Integer> winningNumbers) {
+		if (winningNumbers.size() != new HashSet<>(winningNumbers).size()) {
+			throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 포함되어 있습니다.");
+		}
+	}
+
+	public static void validateNumberInLottoRange(String rawNumber) {
+		validateNaturalNumber(rawNumber);
+		int number = Integer.parseInt(rawNumber);
+
+		if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45의 범위여야 합니다");
 		}
 	}
 }
