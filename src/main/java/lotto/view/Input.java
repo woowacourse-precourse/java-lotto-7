508f4cf55202ced4cfecb1df6b6dfdcb93e5e080
yooo <ghddbtl@gmail.com>
2024-11-04 20:10:44 +0900

    chore : remove string
    필요없는 문자열 지움

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public final static String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해주세요.";
-    public final static String WINNING_NUMBERS_PROMPT = "\n담청 번호를 입력해주세요.(숫자는 ,(콤마)로 구분합니다.)";
+    public final static String WINNING_NUMBERS_PROMPT = "\n담청 번호를 입력해주세요.";
     public final static String BONUS_NUMBER_PROMPT = "\n보너스 번호를 입력해주세요.";
 
     public static String request(String message) {
