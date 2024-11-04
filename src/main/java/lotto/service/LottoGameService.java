8879b0434162c98438cd44cfa46775525811e70c
JMJ <ghtntkdnsk@naver.com>
2024-11-04 19:55:12 +0900

    feat(LottoGameService) : 로또 번호 생성 및 오름차순 정렬 기능 추가

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,4 +1,30 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class LottoGameService {
+
+    public List<Lotto> generatePurchaserLottos(int purchaseCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseCount; i++) {
+            List<Integer> randomLottoNumbers = generateRandomLottoNumbers();
+            Lotto lotto = new Lotto(randomLottoNumbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private List<Integer> generateRandomLottoNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        sortNumbersAscending(randomNumbers);
+        return randomNumbers;
+    }
+
+    private void sortNumbersAscending(List<Integer> randomNumbers) {
+        Collections.sort(randomNumbers);
+    }
 }
