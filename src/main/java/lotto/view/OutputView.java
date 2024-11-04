274854f2234756f79b9cbf89ef1566f57e2c1685
mikekks <thdalsrb79@naver.com>
2024-11-04 14:04:55 +0900

    feat(OutputView): 구입 로또 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,27 +1,45 @@
 package lotto.view;
 
+import static lotto.global.LottoConstant.*;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import lotto.domain.GameResults;
+import lotto.domain.Lotto;
 import lotto.domain.enums.Rank;
 
 public class OutputView {
-
 	public static void printMessage(String message) {
 		System.out.println(message);
 	}
 
 	public static void printGameResult(GameResults gameResults) {
-		System.out.println("당첨 통계");
-		System.out.println("---");
+		printMessage("당첨 통계");
+		printMessage("---");
 
 		Map<Rank, Integer> gameResultMap = gameResults.getGameResultMap();
 
 		for (Rank rank : Rank.values()) {
 			String resultMessage = String.format(rank.getWinningMessage(), gameResultMap.getOrDefault(rank, 0));
-			System.out.println(resultMessage);
+			printMessage(resultMessage);
 		}
 
-		System.out.println("총 수익률은 " + gameResults.getRoundedProfitRate() + "%입니다.");
+		printMessage("총 수익률은 " + gameResults.getRoundedProfitRate() + "%입니다.");
+	}
+
+	public static void printPurchaseLotto(List<Lotto> lottos) {
+		String purchaseCount = lottos.size() + "개를 구매했습니다.";
+		printMessage(purchaseCount);
+
+		for (Lotto lotto : lottos) {
+			String lottoNumbers = lotto.getNumbers().stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining(LOTTO_NUMBER_SEPARATOR));
+
+			printMessage(lottoNumbers);
+		}
+
 	}
 }
