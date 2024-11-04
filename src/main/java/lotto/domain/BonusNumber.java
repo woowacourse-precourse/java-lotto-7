61d2dbcf7b48f12e6b01bc1d29f8fde5b0232059
wvssm <neoramiregi@gmail.com>
2024-11-04 10:46:14 +0900

    feat: 보너스번호가 1 이상 45 이하가 아니면 예외가 발생한다

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import java.util.Objects;
 
 import static lotto.exception.ErrorMessages.INPUT_EMPTY_ERROR;
+import static lotto.exception.ErrorMessages.INVALID_LOTTO_NUMBER_RANGE_ERROR;
 import static lotto.util.Validator.isEmpty;
+import static lotto.util.Validator.isInteger;
 
 public class BonusNumber {
     private int value;
@@ -17,6 +19,15 @@ public class BonusNumber {
         if(isEmpty(value)){
             throw new IllegalArgumentException(INPUT_EMPTY_ERROR);
         }
+
+        if(!isInteger(value) || !isInRange1To45(value)){
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_RANGE_ERROR);
+        }
+    }
+
+    private boolean isInRange1To45(String value){
+        int number = Integer.parseInt(value);
+        return number > 0 && number < 46;
     }
 
     @Override
--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static lotto.exception.ErrorMessages.INPUT_EMPTY_ERROR;
+import static lotto.exception.ErrorMessages.INVALID_LOTTO_NUMBER_RANGE_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BonusNumberTest {
@@ -21,5 +22,21 @@ class BonusNumberTest {
         assertThatThrownBy(()->new BonusNumber("     "))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INPUT_EMPTY_ERROR);
+    }
+
+    @Test
+    @DisplayName("보너스번호가 1 이상 45 이하가 아니면 예외가 발생한다 - 범위에 해당하지 않는 숫자일 경우")
+    void 보너스번호가_1_이상_45_이하가_아니면_예외가_발생한다(){
+        assertThatThrownBy(()->new BonusNumber("46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_LOTTO_NUMBER_RANGE_ERROR);
+    }
+
+    @Test
+    @DisplayName("보너스번호가 1 이상 45 이하가 아니면 예외가 발생한다 - 문자일 경우")
+    void 보너스번호가_1_이상_45_이하가_아니면_예외가_발생한다2(){
+        assertThatThrownBy(()->new BonusNumber("jinyoung"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_LOTTO_NUMBER_RANGE_ERROR);
     }
 }
