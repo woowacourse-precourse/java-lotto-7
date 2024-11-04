0c4e8cba9be1946430df484aa69d2fe226e4682f
EgPwl <egpwl07@gmail.com>
2024-11-04 23:10:09 +0900

    refactor: third 오타 수정

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -6,7 +6,7 @@ public enum Ranking {
 
     FIRST_PLACE(6, false, 2_000_000_000),
     SECOND_PLACE(5, true, 30_000_000),
-    THIRED_PLACE(5, false, 1_500_000),
+    THIRD_PLACE(5, false, 1_500_000),
     FOURTH_PLACE(4, false, 50_000),
     FIFTH_PLACE(3, false, 5_000),
     LAST_PLACE(0, false, 0);
