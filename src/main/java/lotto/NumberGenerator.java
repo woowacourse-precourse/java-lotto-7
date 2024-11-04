eb735b272bcc5bceb7508f7dde59521ff2a9108b
0tae <choiyt3465@naver.com>
2024-11-04 21:29:23 +0900

    feat: 숫자 랜덤 생성 기능 구현

---
+++ src/main/java/lotto/NumberGenerator.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class NumberGenerator {
+    public List<Integer> generateUniqueNumber(int start, int end, int count) {
+         return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+}
