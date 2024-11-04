6e318454051494869a7e68f7bb61aaf195ec2d17
yunsseong <aster20@hanyang.ac.kr>
2024-11-04 22:49:23 +0900

    test: 로또 머신 테스트

--- src/test/java/lotto/service/LottoMachineTest.java
+++ src/test/java/lotto/service/LottoMachineTest.java
@@ -4,49 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 import lotto.model.Lotto;
 import lotto.view.message.ErrorMessage;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
 
-    LottoMachine lottoMachine = new LottoMachine();
-
     @Test
     void 로또_생성_테스트() {
-        String moneyAmount = "10000";
-        System.setIn(new ByteArrayInputStream(moneyAmount.getBytes()));
-
-        List<Lotto> lotteries = lottoMachine.getLotteries();
+        LottoMachine lottoMachine = new LottoMachine();
+        int lottoAmount = 10;
+        List<Lotto> lotteries = lottoMachine.generateLotto(lottoAmount);
         Assertions.assertThat(lotteries).hasSize(10);
     }
 
     @Test
     void 로또_구매_장수_계산_테스트() {
+        LottoMachine lottoMachine = new LottoMachine();
         int number = lottoMachine.calculateNumberLotto(10000);
         Assertions.assertThat(number).isEqualTo(10);
-    }
-
-    @Test
-    void 구입_금액_검증_테스트() {
-        String moneyAmount = "10000";
-        boolean result = lottoMachine.validateMoneyAmount(moneyAmount);
-        Assertions.assertThat(result).isEqualTo(true);
-    }
-
-    @Test
-    void 구입_금액_검증_예외_테스트() {
-        String moneyAmount = "999";
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
-        boolean result = lottoMachine.validateMoneyAmount(moneyAmount);
-
-        Assertions.assertThat(result).isEqualTo(false);
-        Assertions.assertThat(outputStream.toString().trim()).isEqualTo("[ERROR] 최소 구매 금액은 1000원부터 입니다.");
     }
 }
