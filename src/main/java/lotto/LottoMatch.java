6398ec44cda68aad616c8a2094fc4a5d17202599
hobinBae <qoghqls95@gmail.com>
2024-11-04 16:57:27 +0900

    refactor: 메서드 사이 공백 추가

--- src/main/java/lotto/LottoMatch.java
+++ src/main/java/lotto/LottoMatch.java
@@ -1,8 +1,8 @@
 package lotto;
 
 public enum LottoMatch {
-    FIFTH_PLACE(3, false, 5_000 , "3개 일치 (5,000원) - "),
-    FOURTH_PLACE(4, false, 50_000,  "4개 일치 (50,000원) - "),
+    FIFTH_PLACE(3, false, 5_000, "3개 일치 (5,000원) - "),
+    FOURTH_PLACE(4, false, 50_000, "4개 일치 (50,000원) - "),
     THIRD_PLACE(5, false, 1_500_000, "5개 일치 (1,500,000원) - "),
     SECOND_PLACE(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     FIRST_PLACE(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
@@ -11,6 +11,7 @@ public enum LottoMatch {
     private final boolean matchBonus;
     private final int prize;
     private final String winningStatistics;
+
     LottoMatch(int matchCount, boolean matchBonus, int prize, String winningStatistics) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
@@ -28,12 +29,12 @@ public enum LottoMatch {
         return null;
     }
 
-    public int getPrize(){
+    public int getPrize() {
         return prize;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return winningStatistics;
     }
 }
