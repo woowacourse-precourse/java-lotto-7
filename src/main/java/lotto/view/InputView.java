4d1ef173f26b6aac1b3bf71138bcf224b1e96a35
Lee Chae Rin <chaerin0916@gmail.com>
2024-11-04 22:10:11 +0900

    docs: 당첨번호, 보너스 번호 입력 변경
    - 잘못 입력시, 제대로 입력할 때까지 재입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,23 +7,37 @@ import lotto.Lotto;
 
 public class InputView {
     public static int buyLotto() {
-        System.out.println("구입 금액을 입력해 주세요.");
-        int totalLotto = Integer.parseInt(Console.readLine());
-        if (totalLotto % 1000 != 0) {
+        while (true) {
+            try {
+                System.out.println("구입 금액을 입력해 주세요.");
+                int totalLotto = Integer.parseInt(Console.readLine());
+                checkThousand(totalLotto);
+                return totalLotto;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자만 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 1000원 단위로 입력해 주세요.");
+            }
+        }
+    }
+
+    private static void checkThousand(int total) {
+        if (total % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
         }
-        return totalLotto;
     }
 
     public static Lotto enterWinNum() {
-        List<Integer> winLottoNumbers = new ArrayList<>();
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String input = Console.readLine();
-        try {
-            splitByComma(input, winLottoNumbers);
-            return new Lotto(winLottoNumbers);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 포함해야 합니다.");
+        while (true) {
+            try {
+                List<Integer> winLottoNumbers = new ArrayList<>();
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String input = Console.readLine();
+                splitByComma(input, winLottoNumbers);
+                return new Lotto(winLottoNumbers);
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 당첨 번호는 숫자만 포함해야 합니다.");
+            }
         }
     }
 
@@ -35,13 +49,17 @@ public class InputView {
     }
 
     public static int enterBonusNum() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-        try {
-            int bonusNum = Integer.parseInt(Console.readLine());
-            rangeCheck(bonusNum);
-            return bonusNum;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자로 입력해 주세요.");
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                int bonusNum = Integer.parseInt(Console.readLine());
+                rangeCheck(bonusNum);
+                return bonusNum;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 숫자로 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
         }
     }
 
