5cb7d373d64818644e3f76ca3095d7e0202bf36b
Awon01 <bec00@naver.com>
2024-11-04 19:54:00 +0900

    feat(service): 구매희망 금액 입력

--- src/main/java/lotto/view/InputMessage.java
+++ src/main/java/lotto/view/InputMessage.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
-public class InputMessage {
-
+public final class InputMessage {
+    public static void inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 }
