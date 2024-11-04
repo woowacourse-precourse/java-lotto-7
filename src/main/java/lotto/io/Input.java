b5779d63f9f0240fecd636678f53777ceb883cbc
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-11-04 10:30:08 +0900

    feat(input): 구입 금액 입력 기능 추가
    1000으로 나눠 떨어지는 구입 금액 입력 기능을 구현했습니다.

--- src/main/java/lotto/io/Input.java
+++ src/main/java/lotto/io/Input.java
@@ -1,4 +1,23 @@
 package lotto.io;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.message.ErrorMessage;
+
 public class Input {
+    private final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public int getAmount() {
+        System.out.println(INPUT_AMOUNT_MESSAGE);
+
+        int amount = Integer.parseInt(Console.readLine());
+        validate(amount);
+
+        return amount;
+    }
+
+    private void validate(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INDIVISIBLE_NUMBER);
+        }
+    }
 }
--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -3,4 +3,5 @@ package lotto.message;
 public class ErrorMessage {
     public static final String LOTTO_NUMBER_EXCEED = "[ERROR] 로또 번호는 6개여야 합니다.";
     public static final String DUPLICATE_LOTTO_NUMBERS = "[ERROR] 중복된 로또 번호 입니다.";
+    public static final String INDIVISIBLE_NUMBER = "[Error] 로또 구입 금액은 1000원 단위로 입력해주세요.";
 }
