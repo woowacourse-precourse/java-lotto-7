8308cd996a72062b9c2ff8ce5bb965debfbeace5
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:39:33 +0900

    Test: 당첨 번호가 콤마로 끝난다면 예외 발생하는지 테스트

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -31,4 +31,15 @@ public class WinningNumbersTest {
         });
         assertEquals(INVALID_WINNING_NUMBERS.message(), e.getMessage());
     }
+
+    @Test
+    void 당첨_번호가_콤마로_끝난다면_예외발생() {
+        // given
+        String winningNumbers = "1,2,3,4,5,6,";
+        // when & then
+        LottoException e = assertThrows(LottoException.class, () -> {
+            WinningNumbersValidator.validate(winningNumbers);
+        });
+        assertEquals(INVALID_WINNING_NUMBERS.message(), e.getMessage());
+    }
 }
