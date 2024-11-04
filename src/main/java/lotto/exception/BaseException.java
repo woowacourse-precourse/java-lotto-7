e0fe7b873fa10bfbf8308b8e6017d8810a015176
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:27:40 +0900

    Feat: IllegalArgumentException 을 상속받는 커스텀 예외 클래스 BaseException 구현

---
+++ src/main/java/lotto/exception/BaseException.java
@@ -0,0 +1,11 @@
+package lotto.exception;
+
+public class BaseException extends IllegalArgumentException {
+
+    private final ExceptionMessage exceptionMessage;
+
+    public BaseException(ExceptionMessage exceptionMessage) {
+        super(exceptionMessage.message());
+        this.exceptionMessage = exceptionMessage;
+    }
+}
