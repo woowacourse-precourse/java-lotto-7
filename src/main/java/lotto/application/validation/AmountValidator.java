dc0617a0c53fc0704df493dc9c3642bb9b69eb15
jinhyeon <tiemo0708@naver.com>
2024-11-04 22:28:13 +0900

    refactor: 매직 넘버 처리

--- src/main/java/lotto/application/validation/AmountValidator.java
+++ src/main/java/lotto/application/validation/AmountValidator.java
@@ -5,6 +5,7 @@ import lotto.util.ErrorMessages;
 public class AmountValidator implements AmountValidation{
     private static final int MINIMUM_AMOUNT = 1000;
     private static final int AMOUNT_UNIT = 1000;
+    private static final int VALID_AMOUNT_REMAINDER = 0;
 
     @Override
     public Integer validate(String input) {
@@ -14,7 +15,7 @@ public class AmountValidator implements AmountValidation{
     public int validateAndParseAmount(String amountInput) {
         try {
             int amount = Integer.parseInt(amountInput);
-            if (amount < MINIMUM_AMOUNT || amount % AMOUNT_UNIT != 0) {
+            if (amount < MINIMUM_AMOUNT || amount % AMOUNT_UNIT != VALID_AMOUNT_REMAINDER) {
                 throw new IllegalArgumentException(ErrorMessages.INVALID_AMOUNT_UNIT);
             }
             return amount;
