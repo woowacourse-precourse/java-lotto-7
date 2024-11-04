35efb082966050789cb8af905d77f92082e240a9
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:31:36 +0900

    Feat: 수익률 계산 출력 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.EnumMap;
 import lotto.domain.Customer;
 import lotto.domain.Rank;
@@ -8,6 +9,7 @@ public class OutputView {
 
     private static final String LOTTOS_PURCHASED_MESSAGE = "\n%d개를 구매했습니다.";
     private static final String RESULT_MESSAGE = "\n당첨 통계\n---";
+    private static final String CALCULATE_MESSAGE = "총 수익률은 %s입니다.";
 
     public static void lottosPurchasedMessage(Customer customer) {
         System.out.println(String.format(LOTTOS_PURCHASED_MESSAGE, customer.getPrice().getCount()));
@@ -23,5 +25,11 @@ public class OutputView {
     public static void resultMessage(EnumMap<Rank, Integer> rankMap) {
         System.out.print(RESULT_MESSAGE);
         rankMap.forEach((rank, matchCount) -> System.out.println(String.format(rank.getMessage(), matchCount)));
+    }
+
+    public static void calculateMessage(double earningRate) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0%");
+        String earningRateFormatted = decimalFormat.format(earningRate);
+        System.out.println(String.format(CALCULATE_MESSAGE, earningRateFormatted));
     }
 }
