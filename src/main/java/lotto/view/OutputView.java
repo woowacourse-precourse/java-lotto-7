517db9907615d5f88822f90caca9bd859a8aa35f
young-in <hardworkingini@gmail.com>
2024-11-04 22:05:31 +0900

    feat(OutputView.java) : 당첨 번호를 입력받기 위한 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 public class OutputView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String LOTTO_COUNT = "개를 구매했습니다.";
+    private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public void printRequestMoney(){
         System.out.println(INPUT_MONEY);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public void printLottoCount(int lottoCount){
         System.out.println(lottoCount + LOTTO_COUNT);
+    }
+
+    public void printRequestWinningNumbers(){
+        System.out.println(INPUT_WINNING_NUMBERS);
     }
 }
