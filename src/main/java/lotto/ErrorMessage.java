7dae11e489d2435ed2f3f7e2042664d2b1c910d7
wodus4145@naver.com <wodus4145@naver.com>
2024-11-04 18:00:58 +0900

    refactor: Add error message

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     INVALID_PRICE_UNIT("[ERROR] 1000원 단위 숫자로 입력해 주세요."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개입니다."),
     INVALID_RANGE("[ERROR] 1부터 45까지의 숫자를 입력해주세요."),
+    INVALID_WINNING_NUMBER("[ERROR] 유효한 당첨번호를 입력해 주세요."),
     DUPLICATE_NUMBER("[ERROR] 중복된 숫자를 입력할 수 없습니다."),
     INVALID_BONUS("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
 
