9d2bff45a246ff1f65ab34b3f6dbfccf9d173616
SeongJunChun <csj991210@naver.com>
2024-11-04 12:40:18 +0900

    test: 로또 숫자 일치 갯수 반환 기능 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,4 +40,22 @@ class LottoTest {
         Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
         assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
+
+    @DisplayName("로또 숫자 일치 갯수 테스트")
+    @ParameterizedTest
+    @MethodSource("lottos")
+    void lottoContainNumberTest(List<Integer> numbers,List<Integer> winningNumbers,int expected) {
+        Lotto lotto = new Lotto(numbers);
+        Lotto winningLotto = new Lotto(winningNumbers);
+        assertThat(winningLotto.countSameNumbers(lotto)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> lottos() {
+        return Stream.of(
+                Arguments.of(List.of(1,2,3,4,5,6),List.of(5,6,7,8,9,10),2),
+                Arguments.of(List.of(1,2,3,4,5,6),List.of(1,2,3,4,5,6),6),
+                Arguments.of(List.of(1,2,3,4,5,6),List.of(7,8,9,10,11,12),0)
+        );
+    }
+
 }
