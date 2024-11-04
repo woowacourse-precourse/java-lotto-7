4683345161c78c617f73b888b11b380265fcaa27
bjy <qkswnsdud@gmail.com>
2024-11-04 18:37:26 +0900

    feat(LottoValidate): Lotto 예외 처리 기능 추가
    Lotto 클레스와 LottoTest 테스트 클레스에 예외 처리 로직을 더
    추가했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,14 +6,36 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateNumber(numbers);
+        validateReduplication(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    // numbers 필드의 getter 메서드
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
+    private void validateNumber(List<Integer> numbers) {
+        for (int i = 0; i < 6; i++){
+            int indexNumber = numbers.get(i);
+            if (indexNumber < 1 || indexNumber > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+
+        }
+    }
+
+    private void validateReduplication(List<Integer> numbers){
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }
