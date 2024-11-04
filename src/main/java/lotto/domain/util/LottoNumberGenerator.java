19766861453c9cc10c40f9f538e8dd1f1329143f
moonbinchoi <whsahd12@gmail.com>
2024-11-04 16:46:41 +0900

    refactor(LottoNumberGenerator): 상수들의 접근 제어자를 public으로 변경

--- src/main/java/lotto/domain/util/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/util/LottoNumberGenerator.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 public final class LottoNumberGenerator {
 
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_NUMBERS_SIZE = 6;
-    private static final int BONUS_SIZE = 1;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBERS_SIZE = 6;
 
     private LottoNumberGenerator() {}
 
