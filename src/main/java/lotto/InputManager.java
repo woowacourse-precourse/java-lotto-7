e4218bdede49bd525d8aca2d86c26415fc2bd1b3
문혜교 <3hilney@gmail.com>
2024-11-04 13:36:19 +0900

    feat: 입력 기능 구현
    - 구입 금액, 당첨 번호, 보너스 번호 입력 기능
    - 입력값에 대한 검증 로직 적용
    - 문자열을 받아 List<Integer>형태로 변환하는 기능 구현

---
+++ src/main/java/lotto/InputManager.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputManager {
+    private static final String SEPARATOR = ",";
+
+    public static int readPurchaseAmount() {
+        System.out.println(OutputMessage.ENTER_PURCHASE_AMOUNT.getMessage());
+        String input = Console.readLine();
+
+        new LottoInputValidator(input);
+
+        return Integer.parseInt(input.trim());
+    }
+
+    public static List<Integer> readWinningNumbers() {
+        System.out.println(OutputMessage.ENTER_WINNING_NUMBERS.getMessage());
+        String input = Console.readLine();
+
+        new LottoInputValidator(input);
+        List<Integer> numbers = parseToNumberList(input);
+
+        new Lotto(numbers);
+
+        return numbers;
+    }
+
+    public static int readBonusNumber() {
+        System.out.println(OutputMessage.ENTER_BONUS_NUMBER.getMessage());
+        String input = Console.readLine();
+
+        new LottoInputValidator(input);
+
+        return Integer.parseInt(input.trim());
+    }
+
+
+
+    private static List<Integer> parseToNumberList(String input) {
+        String[] parts = input.split(SEPARATOR);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String part : parts) {
+            numbers.add(Integer.parseInt(part.trim()));
+        }
+
+        return numbers;
+    }
+
+
+}
