84a34179a812c65a76b2808416109005cf851d54
sjmmics <sjmmics@gmail.com>
2024-11-04 19:58:01 +0900

    test : ($PrizeGradeTest) 테스트 코드 추가

--- src/test/java/lotto/domain/PrizeGradeTest.java
+++ src/test/java/lotto/domain/PrizeGradeTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static lotto.model.PrizeGrade.THREE_MATCH;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
