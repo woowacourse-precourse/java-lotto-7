2d1ad3386be306921d046e48f0a927c51d4b7b88
yoona <103414935+yoonali@users.noreply.github.com>
2024-11-04 19:40:48 +0900

    fix : 로또 번호 검증 예외 처리 로직 수정

--- src/main/java/lotto/global/string/ErrorConstants.java
+++ src/main/java/lotto/global/string/ErrorConstants.java
@@ -3,4 +3,5 @@ package lotto.global.string;
 public class ErrorConstants {
     public static final String THOUSAND_UNITS_ERROR_MSG = "[ERROR] 구입금액은 1,000원 단위로만 입력해야 합니다.";
     public static final String DUPLICATE_NUMBER_ERROR_MSG = "[ERROR] 중복된 숫자가 존재합니다: ";
+    public static final String VALIDATE_NUMBER_MSG = "[ERROR] 로또 번호는 6개여야 합니다.";
 }
