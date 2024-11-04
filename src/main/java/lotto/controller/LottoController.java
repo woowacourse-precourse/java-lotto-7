e8dac46ba72869b6fb4766d0ccf505976b4af475
haebyun <bhk4392@gmail.com>
2024-11-04 16:17:54 +0900

    feat(LottoController): 당첨 결과 및 수익률 계산 및 출력 기능 추가
    - displayResults 메서드 추가하여 당첨 결과와 수익률을 계산하고 출력
    - generateWinningResultDTOs 메서드를 통해 각 당첨 등수를 DTO 리스트로 변환
    - LottoResult 객체의 수익률을 계산하여 OutputView에 전달

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,16 +1,21 @@
 package lotto.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
 import lotto.domain.LottoVendingMachine;
 import lotto.domain.PurchasedLottos;
 import lotto.domain.WinningLotto;
 import lotto.domain.vo.LottoNumber;
+import lotto.domain.vo.LottoRank;
 import lotto.domain.vo.PurchaseAmount;
 import lotto.dto.request.PurchaseAmountDTO;
 import lotto.dto.request.WinningLottoBonusNumberDTO;
 import lotto.dto.request.WinningLottoNumbersDTO;
 import lotto.dto.response.PurchasedLottosDTO;
+import lotto.dto.response.WinningResultDTO;
+import lotto.dto.response.WinningResultsDTO;
 import lotto.util.Repeater;
 import lotto.view.input.InputView;
 import lotto.view.output.OutputView;
@@ -80,5 +85,18 @@ public class LottoController {
     private LottoNumber generateBonusNumberFromInput() {
         WinningLottoBonusNumberDTO winningLottoBonusNumberDTO = inputView.inputWinningLottoBonusNumber();
         return LottoNumber.of(winningLottoBonusNumberDTO.bonusNumber());
+    }
+
+    private void displayResults(LottoResult lottoResult, PurchaseAmount purchaseAmount) {
+        List<WinningResultDTO> winningResultDTOs = generateWinningResultDTOs(lottoResult);
+        Double profitRate = lottoResult.calculateProfitRate(purchaseAmount.getAmount());
+        outputView.printWinningResults(WinningResultsDTO.of(winningResultDTOs, profitRate));
+    }
+
+    private List<WinningResultDTO> generateWinningResultDTOs(LottoResult lottoResult) {
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank != LottoRank.NONE)
+                .map(rank -> WinningResultDTO.of(rank, lottoResult.getCountByRank(rank)))
+                .toList();
     }
 }
