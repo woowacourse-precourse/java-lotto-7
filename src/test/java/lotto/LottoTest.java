6b26a3675c6be00cf1a49a3445691fe0e3633e06
Hyejeong <thswjd0609@gmail.com>
2024-11-04 17:51:59 +0900

    test: 로또 번호 범위 검증

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 범위가 1에서 45사이를벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_범위가_1에서_45사이를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 60, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
