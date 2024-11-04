b755c4a6b838e0c6459962188e1c71150cc97884
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 13:21:28 +0900

    feat(Lotto): 로또 클래스에 데이터 검증하는 함수를 추가한다
    당첨된 번호들에 중복된 숫자가 존재하는 지 확인하는 함수인
    hasDupliates함수를 추가한다. 그리고 보너스 번호가 기존 번호와 중복되는지
    확인하는 함수 bonusValidate를 추가한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,18 +1,38 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        hasDuplicate(numbers);
         this.numbers = numbers;
     }
 
+    // 중복된 숫자인지 확인하는 거 필요하다.
+    // 보너스 번호 중복인지 확인 필요하다.
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 중복됩니다.");
+        }
+        return true;
+    }
+
+    public void bonusValidate(String number) {
+        int bonusNumber = Integer.parseInt(number);
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 기존 번호와 보너스 번호가 중복된니다.");
         }
     }
 
