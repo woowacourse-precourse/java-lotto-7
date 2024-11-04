deeaea4f115920ffa0d100f5713d0c7910b4acca
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 14:50:36 +0900

    feat(ErrorMessage): 에러메시지 추가
    -입력 값이 NULL이거나 공백일 경우에 대한 에러메시지를 추가함.

--- src/main/java/lotto/execption/ErrorMessage.java
+++ src/main/java/lotto/execption/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호는 중복될 수 없습니다."),
     INVALID_WINNING_NUMBER_COUNT("[ERROR] 당첨 번호는 총 6개여야 합니다."),
     INVALID_BONUS_NUMBER("[ERROR] 보너스 번호는 1부터 45까지의 숫자여야 합니다."),
-    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    INPUT_NULL_OR_EMPTY("[ERROR] 입력값은 비어 있을 수 없습니다.");
 
     private final String message;
 
