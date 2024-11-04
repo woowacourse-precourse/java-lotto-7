169178013d42772f4b32f57de672c45c136c199b
Su-daa <subin.kk019@gmail.com>
2024-11-04 12:21:50 +0900

    feat: 로또 결과 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,4 +24,42 @@ public class OutputView {
     private static void printLotto(Lotto lotto) {
         System.out.println(lotto);
     }
+
+    public static void printWinningMatchCount(RankResult rankResult) {
+        Map<Rank, Long> result = rankResult.getRankResult();
+        System.out.println("\n당첨 통계");
+        System.out.println("-----------");
+        Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
+                .forEach(rank -> printRankResult(rank, result.getOrDefault(rank, 0L)));
+    }
+
+    private static void printRankResult(Rank rank, Long count) {
+        if (rank == Rank.MISS) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(rank.getCount()).append("개 일치");
+
+        if (rank == Rank.SECOND) {
+            sb.append(",");
+        }
+        sb.append(" ");
+        if (rank == Rank.SECOND) {
+            sb.append("보너스 볼 일치 ");
+        }
+
+        sb.append("(")
+                .append(String.format("%,d원", rank.getPrize().toLong()))
+                .append(") - ")
+                .append(count)
+                .append("개");
+        System.out.println(sb);
+    }
+
+    public static void printRateOfEarning(double rateOfEarning) {
+        double roundedRate = Math.round(rateOfEarning * 10000.0) / 100.0;
+        String message = "총 수익률은 %.1f%%입니다.";
+        System.out.printf(message, roundedRate);
+    }
 }
