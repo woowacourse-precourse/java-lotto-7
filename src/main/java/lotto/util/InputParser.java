da14d411771b8a053765682a0488038a3bbede52
ssuukko <tlstpstpdl11@gmail.com>
2024-11-04 22:11:44 +0900

    Refactor: InputParser 클래스의 메서드 개선
    - parsePurchaseAmount 메서드에 예외 처리를 추가하였습니다.
    - parseWinningNumbers 메서드에서 유효성 검사 로직을 변경하여 입력값 검증을 강화하였습니다.
    - parseBonusNumber 메서드에 예외 처리를 추가하여 더 안전하게 동작하도록 하였습니다

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,42 +1,32 @@
 package lotto.util;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputParser {
 
     public static int parsePurchaseAmount(String input) {
-        int amount = Integer.parseInt(input);
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
         }
-        return amount;
     }
 
     public static List<Integer> parseWinningNumbers(String input) {
-        List<Integer> winningNumbers = Arrays.stream(input.split(","))
+        String[] splitNumbers = input.split(",");
+        return Stream.of(splitNumbers)
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        validateWinningNumbers(winningNumbers);
-        return winningNumbers;
     }
 
     public static int parseBonusNumber(String input) {
-        int bonusNumber = Integer.parseInt(input);
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        return bonusNumber;
-    }
-
-    private static void validateWinningNumbers(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != 6 || winningNumbers.stream().anyMatch(n -> n < 1 || n > 45)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자 6개여야 합니다.");
-        }
-        if (winningNumbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
         }
     }
 }
