b8bca184595d123b8a38549e15ebcdd5f8e62bff
Jo Myeong-geun <happy63590@gmail.com>
2024-11-04 19:37:20 +0900

    test: 로또 당첨번호 체크 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,13 +1,14 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
@@ -28,5 +29,31 @@ class LottoTest {
     void 로또_번호가_1보다_작거나_45보다_크면_예외가_발생한다(int invalidNumber) {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, invalidNumber)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호와_당첨_번호의_일치_개수를_카운트한다() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(3, 4, 5, 6, 7, 8);
+
+        //when
+        int countMatchingNumbers = lotto.countMatchingNumbers(winningNumbers);
+
+        //then
+        assertEquals(4, countMatchingNumbers);
+    }
+
+    @Test
+    void 로또_번호에_보너스_번호의_포함_여부를_반환한다() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        //when
+        boolean contains = lotto.containsBonusNumber(bonusNumber);
+
+        //then
+        assertFalse(contains);
     }
 }
