29d966c37088e4f5cc4e35bb938018bf14176701
SeongJunChun <csj991210@naver.com>
2024-11-04 17:03:29 +0900

    test: LottoMachineTest 수정

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,17 +5,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import lotto.service.LottoGenerator;
+
+import lotto.service.MockLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoMachineTest {
 
+    private static final String EXPECT = """
+            [1, 2, 3, 4, 5, 6]
+            [7, 8, 9, 10, 11, 12]
+            [13, 14, 15, 16, 17, 18]
+            """;
+
     @DisplayName("지정 갯수 만큼 로또 발행")
     @Test
     void buyLottoTest() {
-        List<Lotto> lottos = LottoMachine.buyLotto(5, new LottoGenerator()).getLottos();
-        assertThat(lottos).hasSize(5);
+        Lotto mockLotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto mockLotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        Lotto mockLotto3 = new Lotto(List.of(13, 14, 15, 16, 17, 18));
+        List<Lotto> mockLottos = List.of(mockLotto1, mockLotto2, mockLotto3);
+
+        String lottos = LottoMachine.buyLotto(3, new MockLottoGenerator(mockLottos)).getLottos();
+        assertThat(lottos).isEqualTo(EXPECT);
     }
 
 }
