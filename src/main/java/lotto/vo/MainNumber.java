0a3ef0d29ec51f723eb131476856d1b0193d1388
hmlssghm <kwonse326@gmail.com>
2024-11-04 12:33:02 +0900

    feat(MainNumber): 당첨 번호 vo 생성

--- src/main/java/lotto/vo/MainNumber.java
+++ src/main/java/lotto/vo/MainNumber.java
@@ -1,20 +1,40 @@
 package lotto.vo;
 
+import lotto.util.Parser;
+import lotto.util.Validator;
+
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.common.ErrorMessage.NOT_NUMBER_OR_RANGE_EXCESS;
 
 public class MainNumber {
     private final List<Integer> numbers;
 
-    public MainNumber(List<Integer> numbers) {
+    public MainNumber(final String inputValue) {
+        List<String> elements = separate(inputValue);
+        List<Integer> numbers = convertType(elements);
         validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            // TODO: 에러 발생
-        }
+    private List<String> separate(String inputValue) {
+        return Parser.parseElements(inputValue);
     }
 
-    // TODO: 추가 기능 구현
+    private void validate(List<Integer> elements) {
+        Validator.checkElementCount(elements);
+        Validator.checkNumberRange(elements);
+        Validator.checkDuplicate(elements);
+    }
+
+    private List<Integer> convertType(List<String> elements) throws IllegalArgumentException {
+        try {
+            return elements.stream()
+                    .map(Parser::parseStringToInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER_OR_RANGE_EXCESS.getMessage());
+        }
+    }
 }
