f32cecf018bf56f5d834765a115266aa61c471cb
btongtong <leeym2615@naver.com>
2024-11-04 13:52:00 +0900

    feat(Application): Prompt user to re-enter amount if input is invalid

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
+        UserLottos userLottos = createUserLottos();
+    }
+
+    private static UserLottos createUserLottos() {
+        while (true) {
+            try {
+                int purchaseAmount = CommonValidation.convertStringToInt(inputPurchaseAmount());
+                return new UserLottos(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static String inputPurchaseAmount() {
         System.out.println(IOMessage.PURCHASE_AMOUNT.getMessage());
+        return readLine();
     }
 }
