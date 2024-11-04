a6f60ec90920b6a15eb96707efc667105c1053d8
eunji <kuji5256@gmail.com>
2024-11-04 23:33:56 +0900

    refactor: 테스트 코드를 기능별로 분리 - InputValidationTest : 입력값 검증 테스트 - LottoGenerationTest : 로또 구입 금액과 로또 발행 테스트 - LottoResultTest : 로또 당첨 결과 테스트 - LottoTest : 로또 객체 관련 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -28,30 +28,6 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 구입 금액이 1000으로 나누어떨어지지 않으면 예외가 발생한다.")
-    @Test
-    void 로또_구입_금액이_1000으로_나누어떨어지지_않으면_예외가_발생한다() {
-        assertThat(InputValidation.NOT_DIVISIBLE_BY_1000.validate("100")).isFalse();
-    }
-
-    @DisplayName("입력값(금액, 당첨번호, 보너스번호)이 빈 칸이면 예외가 발생한다.")
-    @Test
-    void 입력값이_빈_칸인지_확인한다() {
-        assertThat(InputValidation.NOT_BLANK.validate("")).isFalse();
-    }
-
-    @DisplayName("입력값(금액, 당첨번호, 보너스번호)이 숫자가 아니면 예외가 발생한다.")
-    @Test
-    void 입력값이_숫자인지_확인한다() {
-        assertThat(InputValidation.NOT_NUMBER.validate("a")).isFalse();
-    }
-
-    @DisplayName("입력값(금액, 당첨번호, 보너스번호)이 정수가 아니면 예외가 발생한다.")
-    @Test
-    void 입력값이_정수가_아니면_예외가_발생한다() {
-        assertThat(InputValidation.NOT_INTEGER.validate("1.1")).isFalse();
-    }
-
     @DisplayName("입력값(당첨번호, 보너스번호)이 주어진 범위(1~45) 내에 존재하지 않으면 예외가 발생한다.")
     @Test
     void 입력값이_주어진_범위_내에_존재하지_않으면_예외가_발생한다() {
@@ -62,70 +38,6 @@ class LottoTest {
     void 로또_객체_내_numbers_필드를_리턴한다() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
-    }
-
-    @Test
-    void 구입_금액을_검증하고_정수형으로_리턴한다() {
-        int purchaseAmount = ValidateValues.purchaseAmount("1000");
-        assertThat(purchaseAmount).isEqualTo(1000);
-    }
-
-    @Test
-    void 당첨번호_또는_보너스번호를_검증하고_불린형으로_리턴한다() {
-        boolean rangeCheck1 = ValidateValues.winningNumberOrBonusNumber("0");
-        assertThat(rangeCheck1).isEqualTo(false);
-        boolean rangeCheck2 = ValidateValues.winningNumberOrBonusNumber("1");
-        assertThat(rangeCheck2).isEqualTo(true);
-
-        boolean notNumberCheck1 = ValidateValues.winningNumberOrBonusNumber("a");
-        assertThat(notNumberCheck1).isEqualTo(false);
-        boolean notNumberCheck2 = ValidateValues.winningNumberOrBonusNumber("1");
-        assertThat(notNumberCheck2).isEqualTo(true);
-
-        boolean notIntegerCheck1 = ValidateValues.winningNumberOrBonusNumber("1.1");
-        assertThat(notIntegerCheck1).isEqualTo(false);
-        boolean notIntegerCheck2 = ValidateValues.winningNumberOrBonusNumber("1");
-        assertThat(notIntegerCheck2).isEqualTo(true);
-    }
-
-    @Test
-    void 구입금액만큼_로또를_발행하고_로또_객체_리스트를_리턴한다() {
-        String purchaseAmount = "8000";
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        assertThat(lottoGenerator.generateLotto(purchaseAmount).size()).isEqualTo(8);
-    }
-
-    @Test
-    void 로또번호와_당첨번호를_비교한다() {
-        LottoResultManager lottoResultManager = new LottoResultManager();
-
-        List<Integer> lotto = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        int bonusNumber = 7;
-
-        int result = lottoResultManager.compareLottoNumber(lotto, winningLotto, bonusNumber);
-        assertThat(result).isEqualTo(5);
-    }
-
-    @Test
-    void 당첨결과를_저장한다() {
-        LottoResultManager lottoResultManager = new LottoResultManager();
-
-        List<Lotto> lottos = new ArrayList<>();
-        lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 9, 20)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
-        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
-
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        int bonusNumber = 7;
-
-        int[] result = lottoResultManager.saveLottoWinningResult(lottos, winningLotto, bonusNumber);
-        assertThat(result[0]).isEqualTo(1);
-        assertThat(result[1]).isEqualTo(1);
-        assertThat(result[2]).isEqualTo(0);
-        assertThat(result[3]).isEqualTo(1);
-        assertThat(result[4]).isEqualTo(1);
     }
 
 }
