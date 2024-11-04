76965e83928feaed8c0b0e68e670774f19d6c27c
sodychoe <sodychoe@icloud.com>
2024-11-04 16:54:29 +0900

    test : 천윈 단위가 아닌 구입금액 테스트 완료

--- src/test/java/lotto/buying/BuyingAmountTest.java
+++ src/test/java/lotto/buying/BuyingAmountTest.java
@@ -18,4 +18,16 @@ class BuyingAmountTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액이 천원 미만입니다.");
     }
+
+    @Test
+    @DisplayName("로또는 천원 단위로만 구입한다")
+    void notAmountUnitThrowException() {
+        // given
+        int notUnit = 1550;
+
+        // when then
+        assertThatThrownBy(() -> BuyingAmount.from(notUnit))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 천원 단위여야 합니다.");
+    }
 }
