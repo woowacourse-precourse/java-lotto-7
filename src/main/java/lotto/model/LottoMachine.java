2e931a4353728641e3b8433d9353750b9b8df3f5
dudujh0106 <rkdwlgh01@naver.com>
2024-11-04 11:26:21 +0900

    feat(LottoMachine): add methods for winning reuslt
    - Added method to display lottos as strings
    - Added method to calculate winning count
    - Added method to calculate return rate

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,9 +1,13 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.dto.WinningNumberDto;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoMachine {
     private final List<Lotto> lottos;
@@ -13,6 +17,12 @@ public class LottoMachine {
         validateMoney(money);
         this.lottos = createLotto(money);
         this.money = money;
+    }
+
+    // Test용 생성자
+    public LottoMachine(List<Lotto> lottos) {
+        this.lottos = lottos;
+        this.money = lottos.size() * 1000;
     }
 
     private void validateMoney(int money) {
@@ -29,6 +39,33 @@ public class LottoMachine {
             lottos.add(new Lotto(numbers));
         }
         return lottos;
+    }
+
+    public List<String> displayLottos() {
+        return lottos.stream()
+                .map(Lotto::toLottoDto)
+                .map(lotto -> lotto.numbers().toString())
+                .collect(Collectors.toList());
+    }
+
+    public Map<Prize, Integer> calculateWinningCounts(WinningNumber winningNumber) {
+        Map<Prize, Integer> prizeCount = new EnumMap<>(Prize.class);
+        for (Lotto lotto : lottos) {
+            Prize prize = lotto.determinePrize(winningNumber.getNumbers(), winningNumber.getBonus());
+            prizeCount.put(prize, prizeCount.getOrDefault(prize, 0) + 1);
+        }
+        return prizeCount;
+    }
+
+    public double calculateReturnRate(Map<Prize, Integer> prizeCount) {
+        long totalPrize = calculateTotalPrize(prizeCount);
+        return (double) totalPrize / money * 100;
+    }
+
+    private long calculateTotalPrize(Map<Prize, Integer> prizeCount) {
+        return prizeCount.entrySet().stream()
+                .mapToLong(entry -> (long) entry.getKey().getMoney() * entry.getValue())
+                .sum();
     }
 
     public List<Lotto> getLottos() {
