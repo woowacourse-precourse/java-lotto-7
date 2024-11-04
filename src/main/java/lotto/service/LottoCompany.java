35657ddd1195cabaf80cea5ff3922c501242bd96
yunsseong <aster20@hanyang.ac.kr>
2024-11-04 22:44:48 +0900

    feat: 당첨 내역 출력 기능 구현

--- src/main/java/lotto/service/LottoCompany.java
+++ src/main/java/lotto/service/LottoCompany.java
@@ -1,5 +1,7 @@
 package lotto.service;
+import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.Match;
 import lotto.view.InputView;
 import lotto.view.message.SystemMessage;
 
@@ -7,6 +9,14 @@ public class LottoCompany {
     private Lotto winningNumber;
     private Integer bonusNumber;
     private final InputView inputView = new InputView();
+
+    public LottoCompany() {
+    }
+
+    public LottoCompany(Lotto winningNumber, Integer bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
 
     public Lotto setWinningNumber() {
         this.winningNumber = new Lotto(inputView.lottoInput(SystemMessage.INPUT_WINNING_NUMBER.getMessage()));
@@ -16,5 +26,11 @@ public class LottoCompany {
     public Integer setBonusNumber(Lotto lotto) {
         this.bonusNumber = inputView.lottoNumberInput(SystemMessage.INPUT_BONUS_NUMBER.getMessage(), lotto);
         return bonusNumber;
+    }
+
+    public List<Match> getWinningStatics(List<Lotto> lotteries) {
+        return lotteries.stream()
+                .map(lotto -> Match.findMatch(lotto.countMatch(winningNumber), lotto.checkBonusMatch(bonusNumber))
+                ).toList();
     }
 }
