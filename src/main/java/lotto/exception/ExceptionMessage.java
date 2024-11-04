caab77ef64af86701741dd876d04074b7f1089a3
minjun <ijmtylhtf@naver.com>
2024-11-04 11:57:57 +0900

    feat : 구매 금액 입력 형식 에러 메시지 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -1,6 +1,7 @@
 package lotto.exception;
 
 public enum ExceptionMessage {
+	INVALID_PURCHASE_AMOUNT_FORMAT_INPUT("[ERROR] 구입 금액 형식이 올바르지 않습니다."),
 	INVALID_PURCHASE_AMOUNT_INPUT("[ERROR] 구입 금액은 1,000원 단위로 입력 받습니다.");
 	private final String message;
 
