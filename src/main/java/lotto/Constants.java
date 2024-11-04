77aa9073ea0528e201d52273f9a5de28803867d4
[ddhi7] <[tjsanf211323@gmail.com]>
2024-11-04 18:17:48 +0900

    feat(LottoGenerator): 로또생성 기능 구현

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public static final Integer MIN_NUMBER = 1;
+    public static final Integer MAX_NUMBER = 45;
+    public static final Integer PICK_NUMBER = 6;
 }
--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,4 +1,27 @@
 package lotto.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.Constants.*;
+
 public class LottoGenerator {
+
+    public List<Lotto> generateLottos(int lottoCount){
+        List<Lotto> lottos=new ArrayList<>();
+
+        while(lottos.size()<lottoCount){
+            List<Integer> lottoNumbers = generateLotto();
+            lottos.add(new Lotto(lottoNumbers));
+        }
+
+        return lottos;
+    }
+
+    private List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, PICK_NUMBER);
+    }
 }
