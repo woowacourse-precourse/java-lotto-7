377ba11e85b7d987416177f2bdf12b16fcdb0f17
jinhyeon <tiemo0708@naver.com>
2024-11-04 22:04:30 +0900

    refactor: 에러메시지 상수처리

--- src/main/java/lotto/domain/model/WinningLotto.java
+++ src/main/java/lotto/domain/model/WinningLotto.java
@@ -1,5 +1,7 @@
 package lotto.domain.model;
 
+import lotto.util.ErrorMessages;
+
 import java.util.List;
 
 public class WinningLotto {
@@ -15,7 +17,7 @@ public class WinningLotto {
 
     private void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+            throw new IllegalArgumentException(ErrorMessages.DUPLICATE_BONUS_NUMBER);
         }
     }
 
