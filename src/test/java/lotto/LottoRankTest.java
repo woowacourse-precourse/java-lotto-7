c41931195646992ad8a846636cf5d86983fe84c9
xeimun <hinote444@naver.com>
2024-11-04 21:27:04 +0900

    test(LottoRankTest): 등수에 해당하는 상금에 당첨 횟수를 곱하는 테스트 구현

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import lotto.domain.LottoRank;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,5 +26,22 @@ public class LottoRankTest {
 
         // then
         assertThat(resultRank).isEqualTo(expectedRank);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "FIRST, 1, 2000000000",
+            "SECOND, 2, 60000000",
+            "THIRD, 3, 4500000",
+            "FOURTH, 4, 200000",
+            "FIFTH, 5, 25000",
+            "NONE, 10, 0"
+    })
+    void 등수에_해당하는_상금에_당첨_횟수를_곱한다(LottoRank rank, int rankCount, long expectedPrize) {
+        // when
+        BigDecimal prize = rank.calculatePrizeByRankCount(rankCount);
+
+        // then
+        assertThat(prize).isEqualByComparingTo(BigDecimal.valueOf(expectedPrize));
     }
 }
