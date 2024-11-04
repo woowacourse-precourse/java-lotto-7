53e0c350de1c3794b27d375176fc1e9e9b6dbcce
byeongdoohan <quden04@gmail.com>
2024-11-04 18:05:27 +0900

    feat: 일급 컬렉션으로 변경, 생성 메서드 추가, 보너스번호 검증 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,13 +1,23 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.PatternSyntaxException;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    private Lotto(Set<Integer> numbers) {
+        List<Integer> result = new ArrayList<>(numbers);
+        validate(result);
+        this.numbers = List.copyOf(result);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +26,35 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public static Lotto createLotto(Set<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public static Lotto createLottoByString(String input) {
+        try {
+            String[] numbers = input.split(",");
+            List<Integer> numberList = new ArrayList<>();
+            for (String s : numbers) {
+                numberList.add(Integer.parseInt(s));
+            }
+            Set<Integer> numberSet = new HashSet<>(numberList);
+            if (numberList.size() != numberSet.size()) {
+                throw new IllegalArgumentException();
+            }
+            return new Lotto(numberSet);
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getString() {
+        return Arrays.toString(numbers.toArray());
+    }
+
+    public boolean validateBonusNumberIsDuplicated(int bonusNumber) {
+        if (this.numbers.contains(bonusNumber)) {
+            return true;
+        }
+        return false;
+    }
 }
