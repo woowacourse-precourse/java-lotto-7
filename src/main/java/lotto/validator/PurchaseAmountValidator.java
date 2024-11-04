7070ac166c6a21a084586288730e5d3d04ebe520
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 11:59:57 +0900

    refactor(PurchasAmountValidator): 로또 가격 상수화, 객체명 변경(PriceValidator -> PurchaseAmountValidator)

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -1,24 +1,25 @@
 package lotto.validator;
 
-import static lotto.validator.ValidatorUtils.PRICE_ERROR_MESSAGE;
+import static lotto.controller.LottoController.LOTTO_PRICE;
+import static lotto.validator.ValidatorUtils.PURCHASE_AMOUNT_ERROR_MESSAGE;
 
-public class PriceValidator implements Validator<Integer> {
+public class PurchaseAmountValidator implements Validator<Integer> {
 
     @Override
-    public void validate(Integer price) {
-        validatePositive(price);
-        validateThousandUnit(price);
+    public void validate(Integer purchaseAmount) {
+        validatePositive(purchaseAmount);
+        validateThousandUnit(purchaseAmount);
     }
 
-    private void validatePositive(Integer price) {
-        if (price <= 0) {
-            throw new IllegalArgumentException(PRICE_ERROR_MESSAGE);
+    private void validatePositive(Integer purchaseAmount) {
+        if (purchaseAmount <= 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR_MESSAGE);
         }
     }
 
-    private void validateThousandUnit(Integer price) {
-        if (price % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(PRICE_ERROR_MESSAGE);
+    private void validateThousandUnit(Integer purchaseAmount) {
+        if (purchaseAmount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_ERROR_MESSAGE);
         }
     }
 }
