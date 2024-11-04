26962397f03bc9755aa3141f8774a363bb82a4f8
Awon01 <bec00@naver.com>
2024-11-04 19:46:32 +0900

    feat(service): 로또번호 생성 기능

--- src/main/java/lotto/service/Service.java
+++ src/main/java/lotto/service/Service.java
@@ -1,5 +1,22 @@
 package lotto.service;
 
-public class Service {
+import lotto.domain.Lotto;
+import lotto.utils.Utils;
+import lotto.validation.Validation;
+import java.util.Collections;
+import java.util.List;
 
+public class Service {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
+    private static final int LOTTO_LENGTH = 6;
+
+    public Lotto generateLottoNumber() {
+        List<Integer> numbers = Utils.randomUniqueNumberGenerate(LOTTO_START_NUMBER, LOTTO_LAST_NUMBER, LOTTO_LENGTH);
+        Validation.validateLengthOfList(numbers, LOTTO_LENGTH);
+        Validation.validateDuplicationList(numbers);
+        Validation.validateListNumberInRange(numbers, LOTTO_START_NUMBER, LOTTO_LAST_NUMBER);
+        Utils.sortListNaturalOrder(numbers);
+        return new Lotto(numbers);
+    }
 }
