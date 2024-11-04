839081bf069990901ad922ccc28c0933c5fae063
BusanGukbap <dong2857210@gmail.com>
2024-11-04 16:17:53 +0900

    feat(Controller) : 당첨 번호 및 보너스 번호 입력 후 결과 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -11,9 +11,15 @@ public class LottoController {
 
     public void run() {
         try {
-            int purcaseAmount = readPurchaseAmount();
-            lottoService.generateLottos(purcaseAmount);
+            int purchaseAmount = readPurchaseAmount();
+            lottoService.generateLottos(purchaseAmount);
             OutputView.printPurchasedLottos(lottoService.getLottos());
+
+            WinningLotto winningLotto = readWinningLotto();
+            OutputView.printStatistics(
+                    lottoService.calculateResults(winningLotto),
+                    lottoService.calculateProfitRate(winningLotto, purchaseAmount)
+            );
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -25,4 +31,13 @@ public class LottoController {
         return Integer.parseInt(input);
     }
 
+    private WinningLotto readWinningLotto() {
+        String winningNumbersInput = InputView.inputWinningNumbers();
+        String bonusNumberInput = InputView.inputWinningNumbers();
+
+        return new WinningLotto(
+                LottoValidator.parseLottoNumbers(winningNumbersInput),
+                Integer.parseInt(bonusNumberInput)
+        );
+    }
 }
