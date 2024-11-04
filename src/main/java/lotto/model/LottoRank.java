f40cecacbde1a057d860fa017aff5f8811b67d6f
SinnoLn <sintmtn69@gmail.com>
2024-11-04 17:24:04 +0900

    refactor: LottoRank에 등수 설명 필드 추가 및 예외 메시지 개선
    - LottoRank enum에 각 등수에 대한 설명(description) 필드 추가로 사용자에게 당첨 결과에 대한 명확한 정보를 제공
    - parseAndValidateWinningNumbers 메서드에서 잘못된 입력에 대해 일관된 예외 메시지를 ErrorMessages를 통해 관리하도록 개선

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -1,12 +1,12 @@
 package lotto.model;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FIFTH(3, false, 5_000, "3개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, false, 2_000_000_000, "6개 일치"),
+    NONE(0, false, 0, "");
 
     private static final int MIN_MATCH_COUNT_FOR_RANKING = 3;
     private static final int MATCH_COUNT_FOR_SECOND = 5;
@@ -14,23 +14,26 @@ public enum LottoRank {
     private final int matchCount;
     private final boolean matchBonus;
     private final int prize;
+    private final String description;
 
     LottoRank(
             int matchCount,
             boolean matchBonus,
-            int prize
+            int prize,
+            String description
     ) {
         this.matchCount = matchCount;
         this.matchBonus = matchBonus;
         this.prize = prize;
+        this.description = description;
     }
 
     public int getPrize() {
         return prize;
     }
 
-    public int getMatchCount() {
-        return matchCount;
+    public String getDescription() {
+        return description;
     }
 
     public static LottoRank valueOf(
