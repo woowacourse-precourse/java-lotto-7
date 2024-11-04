1235904205369f5ec35a96aa02708bcefd6028a6
DoomchitYJ <poj1004@inha.edu>
2024-11-04 21:47:49 +0900

    feat(LottoService): WinningNumbers 분리에 따른 로직 변화
    - getWinningNumbers(): 6개 당첨 번호를 입력받고 바로 검증을 수행하여 예외 처리하도록 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,10 +2,13 @@ package lotto.service;
 
 import java.util.List;
 import lotto.Lotto;
+import lotto.domain.BonusNumber;
 import lotto.domain.LottoMarket;
 import lotto.domain.NumbersComparator;
 import lotto.domain.ProfitRatioCalculator;
+import lotto.domain.SixNumbers;
 import lotto.domain.WinningNumbers;
+import lotto.view.InputView;
 
 public class LottoService {
 
@@ -14,8 +17,16 @@ public class LottoService {
         return lottoMarket.buyLotto(money);
     }
 
-    public int[] compareNumbers(List<Lotto> myLottos, List<Integer> inputNumbers, int inputBonusNumber) {
-        WinningNumbers winningNumbers = new WinningNumbers(inputNumbers, inputBonusNumber);
+    public WinningNumbers getWinningNumbers() {
+        List<Integer> inputNumbers = InputView.readWinningNumbers();
+        SixNumbers sixNumbers = new SixNumbers(inputNumbers);
+        int inputBonusNumber = InputView.readBonusNumber();
+        BonusNumber bonusNumber = new BonusNumber(inputBonusNumber);
+
+        return new WinningNumbers(sixNumbers, bonusNumber);
+    }
+
+    public int[] compareNumbers(List<Lotto> myLottos, WinningNumbers winningNumbers) {
         NumbersComparator numbersComparator = new NumbersComparator(myLottos, winningNumbers);
         return numbersComparator.determineRank();
     }
