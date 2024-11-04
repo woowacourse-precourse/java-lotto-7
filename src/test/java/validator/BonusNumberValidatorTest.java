94022b7a8025b1adcf750a86c25424c69f5fc17d
xeimun <hinote444@naver.com>
2024-11-04 20:10:10 +0900

    test(BonusNumberValidatorTest): 보너스 번호가 허용된 범위 미만인 경우 테스트 구현

--- src/test/java/validator/BonusNumberValidatorTest.java
+++ src/test/java/validator/BonusNumberValidatorTest.java
@@ -1,6 +1,7 @@
 package validator;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -18,5 +19,17 @@ public class BonusNumberValidatorTest {
         // when & then
         assertThatCode(() -> BonusNumberValidator.validateBonusNumber(validBonusNumber, lottoNumbers))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 0 이하일 때 예외가 발생한다.")
+    void 보너스_번호가_허용된_범위_미만이어서_예외가_발생한다() {
+        // given
+        int invalidBonusNumber = 0;
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when, then
+        assertThatThrownBy(() -> BonusNumberValidator.validateBonusNumber(invalidBonusNumber, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
