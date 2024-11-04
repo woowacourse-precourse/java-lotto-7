457a26aefd682696a5c0509a5b825bd1ec27a253
Lee Chae Rin <chaerin0916@gmail.com>
2024-11-04 11:09:45 +0900

    feat: 로또 구입 금액 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입 금액을 입력해 주세요.");
+        int totalLotto = Integer.parseInt(Console.readLine());
+        if(totalLotto % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+        }
     }
 }
