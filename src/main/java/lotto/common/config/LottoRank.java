f7bbe83b2f1aacb48480f971707487ecb912085f
yyysolhhh <solbaram37@gmail.com>
2024-11-04 15:20:58 +0900

    feat(LottoRank): getMatchCount 메서드 추가, 순서 변경
    - 출력 시 순서 맞추기 위해 오름차순으로 정렬

--- src/main/java/lotto/common/config/LottoRank.java
+++ src/main/java/lotto/common/config/LottoRank.java
@@ -3,11 +3,11 @@ package lotto.common.config;
 import java.util.Arrays;
 
 public enum LottoRank {
-    MATCH_6(6, false, 2_000_000_000),
-    MATCH_5_BONUS(5, true, 30_000_000),
-    MATCH_5(5, false, 1_500_000),
-    MATCH_4(4, false, 50_000),
     MATCH_3(3, false, 5_000),
+    MATCH_4(4, false, 50_000),
+    MATCH_5(5, false, 1_500_000),
+    MATCH_5_BONUS(5, true, 30_000_000),
+    MATCH_6(6, false, 2_000_000_000),
     ;
 
     private final int matchCount;
@@ -30,5 +30,9 @@ public enum LottoRank {
 
     public int getPrize() {
         return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
