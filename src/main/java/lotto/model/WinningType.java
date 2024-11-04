352a7f968ee13691d43d0a6eca79b0e488df4e80
Parkjyun <jaeyun0507@naver.com>
2024-11-04 20:53:36 +0900

    feat(WinningType): 순서 변경

--- src/main/java/lotto/model/WinningType.java
+++ src/main/java/lotto/model/WinningType.java
@@ -3,12 +3,12 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum WinningType {
-    FIRST(6, false, 2000000000),
-    SECOND(5, true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
+    NONE(0, false, 0),
     FIFTH(3, false, 5000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000);
 
     private final int matchCount;
     private final boolean matchBonus;
