5b829f427e6ba4f2b8f435895bf9febd24c3929b
gyuoo <yeomky98@naver.com>
2024-11-04 23:21:59 +0900

    feat(view): add input validation for purchase amount

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,42 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.exception.InvalidPurchaseAmountException;
+import lotto.exception.MaxPurchaseExceedException;
+import lotto.exception.NegativePurchaseAmountException;
+import lotto.exception.NotDivisibleByLottoPriceException;
+import lotto.util.PurchaseAmountValidator;
 
 public class InputView {
 
 	private final OutputView outputView;
+	private final PurchaseAmountValidator purchaseAmountValidator;
 
-	public InputView(OutputView outputView) {
+	public InputView(OutputView outputView, PurchaseAmountValidator purchaseAmountValidator) {
 		this.outputView = outputView;
+		this.purchaseAmountValidator = purchaseAmountValidator;
 	}
 
 	public String readLine() {
 		return Console.readLine();
 	}
 
-	public String getPurchaseAmount() {
-		outputView.printPurchaseAmountMessage();
-		return readLine();
+	public int getPurchaseAmount() {
+		while (true) {
+			outputView.printPurchaseAmountMessage();
+			String input = readLine();
+
+			try {
+				return purchaseAmountValidator.validateInput(input);
+			} catch (InvalidPurchaseAmountException e) {
+				outputView.printInvalidPurchaseAmountMessage();
+			} catch (NegativePurchaseAmountException e) {
+				outputView.printInvalidPurchaseAmountMessage();
+			} catch (NotDivisibleByLottoPriceException e) {
+				outputView.printNotDivisibleByLottoPriceMessage();
+			} catch (MaxPurchaseExceedException e) {
+				outputView.printMaxPurchaseExceedMessage();
+			}
+		}
 	}
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,23 @@
 package lotto.view;
 
+import lotto.exception.ErrorMessage;
+
 public class OutputView {
 
 	public void printPurchaseAmountMessage() {
 		System.out.println(ConsoleMessage.INPUT_PURCHASE_AMOUNT.getMessage());
 	}
+
+	public void printInvalidPurchaseAmountMessage() {
+		System.out.println(ErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
+	}
+
+	public void printNotDivisibleByLottoPriceMessage() {
+		System.out.println(ErrorMessage.NOT_DIVISIBLE_BY_LOTTO_PRICE.getMessage());
+	}
+
+	public void printMaxPurchaseExceedMessage() {
+		System.out.println(ErrorMessage.MAX_PURCHASE_EXCEED.getMessage());
+	}
+
 }
