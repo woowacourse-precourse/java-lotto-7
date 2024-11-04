5260763e0ee23ff5abd5d081b186a9b109ba0cf6
badgelatte <saucy5939@gmail.com>
2024-11-04 19:05:49 +0900

    lottoRankCheck 메서드 테스트 구현
    lottoRankCheck 메서드에서 고객의 로또와 비교한 후 꽝부터 5등까지 순위를 매겨 int 배열형으로 출력하는 기능을 테스트

--- src/test/java/lotto/LottoCheckerTest.java
+++ src/test/java/lotto/LottoCheckerTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoCheckerTest {
     private LottoChecker lottoChecker;
@@ -45,5 +47,73 @@ public class LottoCheckerTest {
                 Arguments.of((Object) new int[]{1, 0, 0, 0, 0, 0}),
                 Arguments.of((Object) new int[]{0, 0, 0, 0, 0, 1})
         );
+    }
+
+
+    @DisplayName("고객이 가진 로또들 중 당첨 내역 확인하는 메서드")
+    @ParameterizedTest
+    @MethodSource("lottoRankCheck")
+    void lottoRankCheckTest(Customer customer) {
+        customer.buyLottos("8000");
+
+        int[] result = lottoChecker.lottoRankCheck(customer);
+
+        for (int i = 0; i < result.length; i++) {
+            log.info(i + "등 : " + result[i]);
+        }
+
+        assertTrue(result.length >= 6);
+    }
+
+    public static Stream<Arguments> lottoRankCheck() {
+        return Stream.of(
+                Arguments.of(new Customer())
+        );
+    }
+
+    @DisplayName("고객이 가진 로또가 1등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {6})
+    void calculateRank1FromCountTest(int rank) {
+        assertEquals(1, lottoChecker.calculateRankFromCount(rank));
+    }
+
+    @DisplayName("고객이 가진 로또가 2등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {15})
+    void calculateRank2FromCountTest(int rank) {
+        assertEquals(2, lottoChecker.calculateRankFromCount(rank));
+    }
+
+
+    @DisplayName("고객이 가진 로또가 3등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {5})
+    void calculateRank3FromCountTest(int rank) {
+        assertEquals(3, lottoChecker.calculateRankFromCount(rank));
+    }
+
+
+    @DisplayName("고객이 가진 로또가 4등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 14})
+    void calculateRank4FromCountTest(int rank) {
+        assertEquals(4, lottoChecker.calculateRankFromCount(rank));
+    }
+
+
+    @DisplayName("고객이 가진 로또가 1등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 13})
+    void calculateRank5FromCountTest(int rank) {
+        assertEquals(5, lottoChecker.calculateRankFromCount(rank));
+    }
+
+
+    @DisplayName("고객이 가진 로또가 5등인 지 확인하는 메서드")
+    @ParameterizedTest
+    @ValueSource(ints = {2, 1, 0})
+    void calculateNoRankFromCountTest(int rank) {
+        assertEquals(0, lottoChecker.calculateRankFromCount(rank));
     }
 }
