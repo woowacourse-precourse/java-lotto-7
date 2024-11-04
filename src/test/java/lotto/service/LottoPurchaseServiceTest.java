db527d236e3f1a16db7d4569fc079e4055980c82
Hyso <hsk7953@gmail.com>
2024-11-04 15:52:12 +0900

    refactor(LottoPurchaseServiceTest) : 테스트 구조 개선

--- src/test/java/lotto/service/LottoPurchaseServiceTest.java
+++ src/test/java/lotto/service/LottoPurchaseServiceTest.java
@@ -16,28 +16,31 @@ import static org.assertj.core.api.Assertions.*;
 class LottoPurchaseServiceTest {
     private AmountValidator amountValidator;
     private LottoMachine lottoMachine;
+    private LottoPurchaseService lottoPurchaseService;
     @BeforeEach
     void setup(){
         amountValidator = new AmountValidator();
         lottoMachine = new LottoMachine();
+        lottoPurchaseService = new LottoPurchaseService(amountValidator,lottoMachine);
     }
 
     @DisplayName("올바른 로또 구매 케이스")
     @Test
     void purchaseLotto_success(){
-        LottoPurchaseService lottoPurchaseService = new LottoPurchaseService(amountValidator,lottoMachine);
-        LottoTicket lottoTicket = lottoPurchaseService.purchaseLotto("1000");
+        String input = "2000";
+        int expectedTicket = 2;
 
-        assertThat(lottoTicket).isNotNull();
-        assertThat(lottoTicket.getLotteries().size()).isEqualTo(1);
-        assertThat(lottoTicket.getPurchase()).isEqualTo(1000);
+        LottoTicket lottoTicket = lottoPurchaseService.purchaseLotto(input);
+
+        assertThat(expectedTicket).isEqualTo(2);
+        assertThat(lottoTicket.getPurchase()).isEqualTo(2000);
+
     }
 
     @DisplayName("잘못된 금액으로 로또 구매 시 , 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1500", "900"})
     void purchaseLotto_wrong_amount_throwsException(String input){
-        LottoPurchaseService lottoPurchaseService = new LottoPurchaseService(amountValidator,lottoMachine);
 
         assertThatThrownBy(() -> lottoPurchaseService.purchaseLotto(input))
                 .isInstanceOf(IllegalArgumentException.class)
