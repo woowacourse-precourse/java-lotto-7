cc2bbbbc7d2cf57867ab1d7d66df5e40be3a6ef5
Haneul Shin <shn7980@gmail.com>
2024-11-04 12:51:49 +0900

    feat: 로또 구입 금액에 따른 로또 발행 개수 로직 작성

--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -1,12 +1,46 @@
 package lotto;
 
 public class LottoPurchase {
+    private final int money;
+    private final int lottoAmount;
+
     public LottoPurchase(String input) {
         // 구입 금액에 따른 발행할 로또 개수 설정
+        isInputNumeric(input);
+        int inputToInt = Integer.parseInt(input);
+        this.money = validateAmount(inputToInt);
+        this.lottoAmount = calculateLottoAmount();
     }
 
     public int getLottoAmount() {
-        // 발행된 로또 개수 반환
-        return 0;
+        return this.lottoAmount;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+    private int calculateLottoAmount() {
+        return (this.money / 1000);
+    }
+
+    private void isInputNumeric(String input) {
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+        }
+    }
+
+    private boolean isNumeric(String input) {
+        return input.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private int validateAmount(int amount) {
+        if (amount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 최소 1000원 이상 입력해야 합니다.");
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해야 합니다.");
+        }
+        return amount;
     }
 }
