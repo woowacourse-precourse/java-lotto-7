a10b4443c5fd517579305a0b0da5faf46fe299e3
Park-Min-Jun <gomdole3963@gmail.com>
2024-11-04 20:58:45 +0900

    feat: 당첨 통계 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,7 +16,7 @@ public class Application {
             printLottos(purchasedLottos);
             List<Integer> winningNumbers = inputWinningNumbers();
             int bonusNumber = inputBonusNumber();
-            calculateResults(purchasedLottos, winningNumbers, bonusNumber);
+            printResult(calculateResults(purchasedLottos, winningNumbers, bonusNumber), amount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -97,5 +97,14 @@ public class Application {
             results[rank.ordinal()]++;
         }
         return results;
+    }
+
+    private static void printResult(int[] results, int amount) {
+        System.out.println("\n당첨 통계\n---");
+        System.out.printf("3개 일치 (5,000원) - %d개\n", results[Rank.FIFTH.ordinal()]);
+        System.out.printf("4개 일치 (50,000원) - %d개\n", results[Rank.FOURTH.ordinal()]);
+        System.out.printf("5개 일치 (1,500,000원) - %d개\n", results[Rank.THIRD.ordinal()]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", results[Rank.SECOND.ordinal()]);
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", results[Rank.FIRST.ordinal()]);
     }
 }
