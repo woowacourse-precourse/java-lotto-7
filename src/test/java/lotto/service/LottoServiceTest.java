1451b68470662cddc905ebc48f59eda9d0471d73
byeongdoohan <quden04@gmail.com>
2024-11-04 23:56:47 +0900

    test: 테스트 오류 수정

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -2,18 +2,33 @@ package lotto.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lotto.generator.LottoGenerator;
 import lotto.model.Lotto;
 import lotto.model.LottoGroup;
 import lotto.model.Pay;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoServiceTest {
+    @AfterEach
+    void tearDown() {
+        Console.close();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
+    }
 
     @Test
     @DisplayName("로또 목록에서 당첨 로또 리스트 반환 테스트")
@@ -30,7 +45,9 @@ class LottoServiceTest {
         lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));  // 5개 + 보너스 일치
 
         LottoGroup lottoGroup = LottoGroup.createLottoGroup(lottoList);
-        LottoService lottoService = LottoService.createLottoService();
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
         List<int[]> matchedList = lottoService.calculateMatched(lottoGroup, winnerLotto, bonusNumber);
 
         // 예상 결과
@@ -53,9 +70,10 @@ class LottoServiceTest {
         String input = "asdf\n25\n8000\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-
-        LottoService payService = LottoService.createLottoService();
-        Pay pay = payService.payInput();
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
+        Pay pay = lottoService.payInput();
 
         assertNotNull(pay, "Pay 객체가 null이 아닙니다.");
         assertEquals(8000, pay.getMoney(), "금액이 예상과 다릅니다.");
@@ -67,8 +85,9 @@ class LottoServiceTest {
         String input = "asdf\n1,2,3,4,5,6\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-
-        LottoService lottoService = LottoService.createLottoService();
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
         Lotto lotto = lottoService.generateWinnerLotto();
         assertNotNull(lotto, "lotto 객체가 null이 아닙니다.");
         assertEquals("[1, 2, 3, 4, 5, 6]", lotto.getNumbers().toString(), "입력값이 예상과 다릅니다.");
@@ -80,8 +99,9 @@ class LottoServiceTest {
         String input = "\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-
-        LottoService lottoService = LottoService.createLottoService();
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
         Lotto lotto = lottoService.generateWinnerLotto();
         assertNotNull(lotto, "lotto 객체가 null이 아닙니다.");
         assertEquals(lotto.getNumbers().size(), 6, "입력값이 예상과 다릅니다.");
@@ -93,8 +113,9 @@ class LottoServiceTest {
         String input = "1,2,3,4,5,6\nasdf\n1\n7\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-
-        LottoService lottoService = LottoService.createLottoService();
+        PrintService printService = new PrintService();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        LottoService lottoService = new LottoService(printService, lottoGenerator);
         Lotto winnerLotto = lottoService.generateWinnerLotto();
         int bonusNumber = lottoService.validateBonusNumber(winnerLotto);
         assertNotNull(bonusNumber, "bonusNumber가 null이 아닙니다.");
