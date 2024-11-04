e49704dd95278d978ef0153afbf1cb09bdfa1c3f
dayeondayeon <dayeon2438@gmail.com>
2024-11-04 14:07:20 +0900

    feat : 구매금액에 대한 검증 기능 구현

--- src/main/java/lotto/constants/ConstraintConstants.java
+++ src/main/java/lotto/constants/ConstraintConstants.java
@@ -2,4 +2,5 @@ package lotto.constants;
 
 public class ConstraintConstants {
     public static final int MAX_PURCHASE_AMOUNT = 100000;
+    public static final int PURCHASE_UNIT = 1000;
 }
--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,11 +1,25 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.constants.InputConstants;
+import lotto.constants.ConstraintConstants;
+import lotto.constants.InputViewConstants;
 
 public class InputService {
-    public void getPurchaseAmount() {
-        System.out.println(InputConstants.PURCHASE_AMOUNT_INSTRUCTION);
-        Console.readLine();
+    public int getPurchaseAmount() {
+        System.out.println(InputViewConstants.PURCHASE_AMOUNT_INSTRUCTION);
+        try {
+            int purchaseAmount = Integer.parseInt(Console.readLine());
+            if (validatePurchaseAmount(purchaseAmount)) {
+                return purchaseAmount / ConstraintConstants.PURCHASE_UNIT;
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean validatePurchaseAmount(int purchaseAmount) {
+        return purchaseAmount % ConstraintConstants.PURCHASE_UNIT == 0
+                && purchaseAmount <= ConstraintConstants.MAX_PURCHASE_AMOUNT;
     }
 }
