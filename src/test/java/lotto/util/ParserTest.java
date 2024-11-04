eef9aac43fc3a243c2705f3644db1355f7295784
hmlssghm <kwonse326@gmail.com>
2024-11-04 21:51:53 +0900

    test(ParserTest): 정상 동작 테스트 추가

--- src/test/java/lotto/util/ParserTest.java
+++ src/test/java/lotto/util/ParserTest.java
@@ -3,9 +3,25 @@ package lotto.util;
 import lotto.vo.Payment;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
+    @Test
+    void 문자열_정수_정상_파싱() {
+        int parsedNum = Parser.parseStringToInt("5");
+        assertEquals(parsedNum, 5);
+    }
+
+    @Test
+    void 문자열_분리() {
+        List<String> parsed = Parser.parseElements("0,1");
+        assertEquals("0", parsed.get(0));
+        assertEquals("1", parsed.get(1));
+    }
+
     @Test
     void 인티저_범위_초과() {
         assertThatThrownBy(() -> Parser.parseStringToInt("2147483648"))
