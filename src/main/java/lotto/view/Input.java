21ecf4262d57c0d12f6ee259de646afa76e4b2f3
yubin8773 <hiyubin.lee@gmail.com>
2024-11-04 23:23:35 +0900

    feat : 로또 구매 금액 입력

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,16 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import lotto.validation.InputNumValidation;
+
 public class Input {
+    public static long inputNum() {
+        String num;
+        do {
+            System.out.println("구매금액을 입력해 주세요.");
+            num = Console.readLine();
+        } while (!InputNumValidation.checkInputNum(num));
+        return Long.parseLong(num);
+    }
 }
