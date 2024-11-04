a8ba9d28875dafc06127aed3a9f0b75918f73d25
DongHoon Lee <dhl1924@naver.com>
2024-11-04 19:17:13 +0900

    feat(util): 당첨번호 각각을 숫자로 바꿔주는 유틸성 메서드 구현

--- src/main/java/lotto/util/NumberParser.java
+++ src/main/java/lotto/util/NumberParser.java
@@ -1,10 +1,21 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public final class NumberParser {
     private NumberParser() {
     }
 
     public static int parsePurchaseAmountToInt(final String purchaseAmount) {
         return Integer.parseInt(purchaseAmount);
+    }
+
+    public static List<Integer> parseWinningNumbers(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
