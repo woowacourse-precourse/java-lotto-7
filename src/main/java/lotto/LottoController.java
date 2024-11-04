47b9b470cd7f7f35198cba9dcdb1d51c69e721a4
kimsongie12 <song3078@naver.com>
2024-11-04 17:37:00 +0900

    feat: 전체적인 로직 제어 view, game객체 구매 금액 입력처리 및 당첨번호와 보너스 번호 유효성 검사 유효성 검사1)당첨 번호 6개 유효성 검사2)보너스 번호는 당첨번호에 포함될 수 없고, 중복 불가능 유효성 검사3)보너스 번호는 1-45사이어야하고, 중복 불가능

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,4 +1,62 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoController {
+    private final LottoView view;
+    private final LottoGame game;
+    public LottoController(LottoView view, LottoGame game){
+        this.view = view;
+        this.game = game;
+    }
+
+    public void start() {
+        try {
+            int purchaseAmount = view.inputPurchaseAmount();
+
+
+            if (purchaseAmount < 1000) {
+                throw new IllegalArgumentException("[ERROR] 최소 1000원 이상 구매해야 합니다.");
+            }
+
+
+            int numberOfLottos = purchaseAmount / 1000;
+            List<Lotto> purchasedLottos = game.generateLottos(numberOfLottos);
+            view.displayPurchasedLottos(purchasedLottos);
+
+            List<Integer> winningNumbers = view.inputWinningNumbers();
+            int bonusNumber = view.inputBonusNumber();
+
+
+            validateWinningNumbers(winningNumbers, bonusNumber);
+
+            LottoResult result = game.calculateResults(purchasedLottos, winningNumbers, bonusNumber);
+            view.displayResult(result, purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            view.displayError("[ERROR]"+ e.getMessage());
+        }
+    }
+    private void validateWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호에 포함될 수 없습니다.");
+        }
+
+        if (winningNumbers.stream().anyMatch(num -> num < 1 || num > 45)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1에서 45 사이여야 합니다.");
+        }
+        if (winningNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1에서 45 사이여야 합니다.");
+        }
+    }
+
 }
