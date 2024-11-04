222de2f9aed9e8784524ff71b087bef1d083b336
hwangdaesun <hds650588@gmail.com>
2024-11-04 14:44:23 +0900

    refactor : ErrorMessage를 Lotto 관련 상수로 구성하여 로또 규칙에 게는 에러 메시지 관리하도록 수정

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -1,12 +1,17 @@
 package lotto.common;
 
+import static lotto.model.Lotto.getLottoPrice;
+import static lotto.model.Lotto.getLottoSize;
+import static lotto.model.Lotto.getMaxNumber;
+import static lotto.model.Lotto.getMinNumber;
+
 public enum ErrorMessage {
 
     INPUT_MONEY_IS_DIGIT("[ERROR] 구입 금액은 양수입니다."),
-    LOTTO_NUMBERS_MUST_SIX("[ERROR] 로또 번호는 6개여야 합니다."),
-    LOTTO_NUMBER_RANGE("[ERROR] 로또 번호의 숫자 범위는 1~45까지입니다."),
+    LOTTO_NUMBERS_MUST_SIX(String.format("[ERROR] 로또 번호는 %d개여리야 합니다.", getLottoSize())),
+    LOTTO_NUMBER_RANGE(String.format("[ERROR] 로또 번호의 숫자 범위는 %d~%d까지입니다.", getMinNumber(), getMaxNumber())),
     LOTTO_NUMBER_DISTINCT("[ERROR] 로또 번호는 중복이 없어야합니다."),
-    INPUT_MONEY_IS_MULTIPLE_1000("[ERROR] 구입 금액은 1,000원 단위로 입력받습니다."),
+    INPUT_MONEY_IS_MULTIPLE_1000(String.format("[ERROR] 구입 금액은 %,d원 단위로 입력받습니다.", getLottoPrice())),
     INPUT_LOTTO_NUMBER_IS_NOT_NEGATIVE_INTEGER("[ERROR] 입력이 0이상인 정수여야합니다.");
 
     private final String message;
