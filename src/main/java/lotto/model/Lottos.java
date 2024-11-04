63b2d341b70a8289b1a298ecaacfd1ab211f078c
seunghee <seunghee_chae3@tmax.co.kr>
2024-11-04 20:37:07 +0900

    feat(Lottos): 등수당 당첨 통계 계산

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,13 +1,19 @@
 package lotto.model;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import lotto.util.RandomNumber;
 
 public class Lottos {
 
     private List<Lotto> lottos;
     private int number;
+    private Map<Rank, Integer> winningStatistics;
 
     public Lottos(int number) {
         List<Lotto> generatedLotto = new ArrayList<>();
@@ -15,5 +21,14 @@ public class Lottos {
             generatedLotto.add(new Lotto(RandomNumber.createRandomNumbers()));
         }
         this.lottos = generatedLotto;
+    }
+
+    public void calculateWinningStatistics(WinningLotto winningLotto) {
+        winningStatistics = lottos.stream()
+                .collect(groupingBy(
+                        lotto -> lotto.getLottoRanking(winningLotto),
+                        summingInt(lotto -> 1)
+                ));
+        Arrays.stream(Rank.values()).forEach(rank -> winningStatistics.putIfAbsent(rank, 0));
     }
 }
