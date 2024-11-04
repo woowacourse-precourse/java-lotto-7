407db2ca16c754ef2e60b8f5d94f2c798b125512
gichan222 <>
2024-11-04 19:13:59 +0900

    feat(Controller) : 서비스와 사용자 입출력을 연결하는 컨트롤러 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.global.Validator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
+    private static final InputView inputView = InputView.getInstance();
+    private static final Validator validator = Validator.getInstance();
+    private static final OutputView outputView = OutputView.getInstance();
+    private static final LottoService lottoService = new LottoService();
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController controller = new LottoController(inputView, validator, outputView,lottoService);
+        controller.run();
     }
 }
