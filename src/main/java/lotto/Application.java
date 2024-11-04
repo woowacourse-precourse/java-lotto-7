63b3172808b91f25181d1c7e5f93113acd0c3959
LeeJuHeon <12160569@inha.edu>
2024-11-04 19:56:52 +0900

    feat: Application.java 동작 방식 변경
    재귀 대신 루프 활용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            InputView inputView = new InputView();
-            int purchaseAmount = inputView.readPurchaseAmount();
+        while (true) {
+            try {
+                InputView inputView = new InputView();
+                int purchaseAmount = inputView.readPurchaseAmount();
 
-            LottoGenerator generator = new LottoGenerator();
-            List<Lotto> lottos = generator.generateLottos(purchaseAmount);
+                LottoGenerator generator = new LottoGenerator();
+                List<Lotto> lottos = generator.generateLottos(purchaseAmount);
 
-            OutputView.printLottoCount(lottos.size());
-            OutputView.printLottos(lottos);
+                OutputView.printLottoCount(lottos.size());
+                OutputView.printLottos(lottos);
 
-            WinningNumbers winningNumbers = inputView.readWinningNumbers();
-            ResultCalculator calculator = new ResultCalculator(winningNumbers, lottos);
-            Statistics statistics = calculator.calculate();
+                WinningNumbers winningNumbers = inputView.readWinningNumbers();
+                ResultCalculator calculator = new ResultCalculator(winningNumbers, lottos);
+                Statistics statistics = calculator.calculate();
 
-            OutputView.printStatistics(statistics);
-        } catch (IllegalArgumentException | IllegalStateException e) {
-            System.out.println(e.getMessage());
-            main(args);
+                OutputView.printStatistics(statistics);
+                break;
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.out.println(e.getMessage());
+
+            }
         }
     }
 }
