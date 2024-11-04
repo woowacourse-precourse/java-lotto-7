79ac6f5045537cef4591fcf61ef0902641a12d89
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-04 17:55:41 +0900

    refactor: Lotto 클래스를 Record로 변환

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -14,18 +14,11 @@ import java.util.List;
 import java.util.Set;
 import lotto.exception.LottoException;
 
-public class Lotto {
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers) {
+public record Lotto(List<Integer> numbers) {
+    public Lotto {
         validateNumberCount(numbers);
         validateNoDuplicate(numbers);
         validateNumberRange(numbers);
-        this.numbers = numbers;
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers;
     }
 
     private void validateNumberCount(List<Integer> numbers) {
