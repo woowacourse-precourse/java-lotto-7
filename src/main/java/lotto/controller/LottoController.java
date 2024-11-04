ae6c7a8ae5c8208ebbf304b0e26994abe3fc4175
FhRh <yiyop@naver.com>
2024-11-04 17:49:47 +0900

    feat(Controller) : 컨트롤러 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,42 @@
 package lotto.controller;
 
-import javax.swing.text.View;
+import java.util.List;
+import lotto.domain.LottoRank;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLotto;
+import lotto.dto.LottoStatisticsDto;
+import lotto.service.LottoCheckService;
+import lotto.service.LottoCreateService;
+import lotto.service.LottoStatisticsService;
+import lotto.view.View;
 
 public class LottoController {
-    private View view;
+
+    private final View view;
+    private final LottoCreateService lottoCreateService;
+    private final LottoCheckService lottoCheckService;
+    private final LottoStatisticsService lottoStatisticsService;
 
     public LottoController(View view) {
         this.view = view;
+        lottoCreateService = new LottoCreateService();
+        lottoCheckService = new LottoCheckService();
+        lottoStatisticsService = new LottoStatisticsService();
     }
 
     public void run() {
+        String money = view.getMoney();
+        Lottos lottos = lottoCreateService.createLottosWithMoney(money);
+        view.printLottos(lottos);
+
+        String winningNumbers = view.getWinningNumbers();
+        String bonusNumber = view.getBonusNumber();
+        WinningLotto winningLotto = lottoCreateService.createWinningLotto(winningNumbers, bonusNumber);
+
+        List<LottoRank> lottoRanks = lottoCheckService.checkRanks(winningLotto, lottos);
+
+        LottoStatisticsDto result = lottoStatisticsService.getLottoStatistics(lottoRanks);
+        view.printResult(result);
 
     }
 }
