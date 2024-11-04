487d8e70d90bcd5af51e7d2b792078a2f6c2d2f1
yunhacandy <yunhacandy@gmail.com>
2024-11-04 15:49:48 +0900

    feat (LottoBonusNumberValidator): 보너스 번호 입력 검증 기능 구현

--- src/main/java/lotto/exception/InputNumberException.java
+++ src/main/java/lotto/exception/InputNumberException.java
@@ -9,6 +9,7 @@ public enum InputNumberException {
     INVALID_LENGTH(ERROR_PREFIX + " 로또 번호는 " + Constants.LOTTO_NUMBER_LENGTH + "개여야 합니다."),
     INVALID_RANGE(ERROR_PREFIX + " 로또 번호는 1부터 45 사이여야 합니다."),
     DUPLICATE_NUMBERS(ERROR_PREFIX + " 로또 번호에 중복이 없어야 합니다."),
+    DUPLICATE_NUMBER_WITH_WINNING_NUMBERS(ERROR_PREFIX + " 보너스 번호가 당첨 번호와 같으면 중복이 없어야 합니다."),
     INVALID_FORMAT(ERROR_PREFIX + "당첨 번호는 숫자여야 합니다."),
     ENDS_WITH_COMMA(ERROR_PREFIX + "입력 값은 쉼표로 끝날 수 없습니다."),
     EMPTY_INPUT(ERROR_PREFIX + "입력 값이 없습니다.");
