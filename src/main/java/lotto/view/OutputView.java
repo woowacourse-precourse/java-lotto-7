29ead7b165889eea46d43e5ed732090b69d0ec91
jungsu <vwx1@naver.com>
2024-11-04 14:43:54 +0900

    feat: printWinningResults 구현
    로또 당첨 결과 테스트를 통과하는 printWinningResults 구현했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -17,7 +17,13 @@ public class OutputView implements OutputViewInterface {
   }
 
   public void printWinningResults(List<Integer> winningCounts) {
-
+    System.out.println("당첨 통계");
+    System.out.println("---");
+    System.out.println("3개 일치 (5,000원) - " + winningCounts.get(0) + "개");
+    System.out.println("4개 일치 (50,000원) - " + winningCounts.get(1) + "개");
+    System.out.println("5개 일치 (1,500,000원) - " + winningCounts.get(2) + "개");
+    System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningCounts.get(3) + "개");
+    System.out.println("6개 일치 (2,000,000,000원) - " + winningCounts.get(4) + "개");
   }
 
   public void printYield(double yield) {
