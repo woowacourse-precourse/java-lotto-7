2e47f860c887cc4f06b3f39aed4d39bb1d6b6add
davemins <davemins4125@naver.com>
2024-11-04 21:36:05 +0900

    feat(Domain): 예외 처리 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,10 +3,13 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
     private static final int LOTTO_GAME_NUMBERS_SIZE = 6;
     private static final int LOTTO_GAME_START_NUMBER = 1;
     private static final int LOTTO_GAME_END_NUMBER = 45;
+    private static final String IS_SIX_NUMBERS_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    private static final String IS_BETWEEN_LOTTO_RANGE_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String IS_NOT_DUPPLICATE_NUMBER_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 중복되지 않는 숫자여야 합니다.";
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -19,13 +22,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (!isSixNumbers(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(IS_SIX_NUMBERS_EXCEPTION_MESSAGE);
         }
         if (!isBetweenLottoRange(numbers)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(IS_BETWEEN_LOTTO_RANGE_EXCEPTION_MESSAGE);
         }
-        if (!isDuplicateNumber(numbers)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (!isNotDuplicateNumber(numbers)) {
+            throw new IllegalArgumentException(IS_NOT_DUPPLICATE_NUMBER_EXCEPTION_MESSAGE);
         }
     }
 
@@ -38,7 +41,7 @@ public class Lotto {
                 number >= LOTTO_GAME_START_NUMBER && number <= LOTTO_GAME_END_NUMBER);
     }
 
-    private boolean isDuplicateNumber(List<Integer> numbers) {
+    private boolean isNotDuplicateNumber(List<Integer> numbers) {
         return numbers.size() == numbers.stream().distinct().count();
     }
 }
