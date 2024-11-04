a9cd9010f9584c1c7b7bd143f102289386265eac
MinJae00 <exceedmin@gmail.com>
2024-11-04 18:56:51 +0900

    refactor: 수익률, % 문구 상수 값 추가

--- src/main/java/lotto/constant/LottoPrintConstant.java
+++ src/main/java/lotto/constant/LottoPrintConstant.java
@@ -9,5 +9,8 @@ public class LottoPrintConstant {
     public static final String STATISTICS = "당첨 통계";
     public static final String RESULT_LINE = "---";
     public static final String PRINT_BUY = "개를 구매했습니다.";
+    public static final String TOTAL_PRINT = "총 수익률은 ";
+
+    public static final String PERCENT_PRINT = "%입니다.";
 
 }
