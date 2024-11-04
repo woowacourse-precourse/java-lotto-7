bd39a0c2f3c58f0a3518c4ef19dd1feda708ac69
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-04 22:56:07 +0900

    feat(Random): 로또 번호 오름차순 정렬

--- src/main/java/lotto/model/Random.java
+++ src/main/java/lotto/model/Random.java
@@ -1,12 +1,15 @@
 package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
 import java.util.List;
 import lotto.util.LottoInfo;
 import lotto.util.Range;
 
 public class Random {
     public static List<Integer> pickUniqueNumbersInRange() {
-        return Randoms.pickUniqueNumbersInRange(Range.MIN.getNum(), Range.MAX.getNum(), LottoInfo.NUM_COUNT.getNum());
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Range.MIN.getNum(), Range.MAX.getNum(), LottoInfo.NUMBER_COUNT.getNum());
+        numbers.sort(Comparator.naturalOrder());
+        return numbers;
     }
 }
