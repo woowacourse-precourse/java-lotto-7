c610ff763f2244e5c1339745eda2df9db817e806
BusanGukbap <dong2857210@gmail.com>
2024-11-04 14:43:40 +0900

    feat(Controller) : 구매 금액 입력 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,24 @@
 package lotto.controller;
 
+import lotto.domain.*;
+import lotto.service.*;
+import lotto.view.*;
+import lotto.util.*;
+
 public class LottoController {
+
+    private final LottoService lottoService = new LottoService();
+
+    public void run() {
+        try {
+            int purcaseAmount = readPurchaseAmount();
+        }
+
+    }
+
+    private int readPurchaseAmount() {
+        String input = InputView.inputPurchaseAmount();
+        return Integer.parseInt(input);
+    }
+
 }
