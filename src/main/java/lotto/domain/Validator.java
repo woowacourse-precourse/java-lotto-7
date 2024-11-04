6fa6408891655679ffc8892a9b379c459134c272
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 17:05:50 +0900

    feat: 당첨 번호 검증(중복된 숫자가 존재할 경우 예외 발생)

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Validator {
@@ -8,6 +10,7 @@ public class Validator {
     public static final String INVALID_PURCHASE_AMOUNT_DIVISIBILITY_ERROR = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
     public static final String INVALID_LOTTO_NUMBER_COUNT_ERROR = "[ERROR] 로또 번호는 6개여야 합니다.";
     public static final String INVALID_WINNING_NUMBERS_TYPE_ERROR = "[ERROR] 당첨 번호는 숫자로 입력해야 합니다.";
+    public static final String DUPLICATE_WINNING_NUMBER_ERROR = "[ERROR] 당첨 번호에 중복된 숫자가 있습니다.";
 
 
     public static int validateAndParsePurchaseAmount(String input) {
@@ -37,13 +40,23 @@ public class Validator {
     }
 
     public static List<Integer> validateAndParseWinningNumbers(String input) {
+        List<Integer> numbers;
         try {
-            return List.of(input.split(","))
+            numbers = List.of(input.split(","))
                     .stream()
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_WINNING_NUMBERS_TYPE_ERROR);
         }
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER_ERROR);
+        }
+        return numbers;
+    }
+
+    private static boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
     }
 }
