fbe64df2e04eb1e2af046f414fb378609be21f98
dayeondayeon <dayeon2438@gmail.com>
2024-11-04 13:53:04 +0900

    refactor : 입력 안내를 위한 상수 클래스 명 InputViewConstants로 변경

--- src/main/java/lotto/constants/InputViewConstants.java
+++ src/main/java/lotto/constants/InputViewConstants.java
@@ -1,6 +1,6 @@
 package lotto.constants;
 
-public class InputConstants {
+public class InputViewConstants {
     public static final String PURCHASE_AMOUNT_INSTRUCTION = "구입금액을 입력해 주세요";
     public static final String WINNING_NUMBER_INSTRUCTION = "당첨 번호를 입력해 주세요";
     public static final String BONUS_NUMBER_INSTRUCTION = "보너스 번호를 입력해 주세요";
