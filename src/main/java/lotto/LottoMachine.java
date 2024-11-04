a08483c2336df60e542164fcd1a22756b875f595
LeeJungBum <jungbum05114@gmail.com>
2024-11-04 16:57:47 +0900

    feat(machine): 로또 생성기 구현
    로또 번호를 자동으로 생성하는 LottoMachine 클래스 구현:
    - 유틸리티성 메서드로 구현 (private 생성자로 인스턴스화 방지)
    - 1부터 45까지의 숫자 중 6개를 랜덤 선택
    - 중복없는 번호 생성을 위해 Randoms.pickUniqueNumbersInRange 활용
    - 생성된 번호는 오름차순 정렬
    
    구현 메소드:
    - private 생성자: 인스턴스화 방지
    - generateLotto: 최종 로또 객체 생성
    - generateNumbers: 랜덤 번호 생성
    - sortNumbers: 번호 정렬
    
    자바독 주석 추가로 코드 문서화 완료

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,58 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * class: LottoMachine.
+ *
+ * 로또를 생성하는 기계를 의미하는 객체
+ * @author JBumLee
+ * @version 2024/11/04
+ */
+public class LottoMachine {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    /**
+     * 유틸리티 클래스의 인스턴스화 방지
+     */
+    private LottoMachine() {
+    }
+
+    /**
+     * 1부터 45까지의 숫자 중 6개를 랜덤하게 선택하여 로또를 생성합니다.
+     * 선택된 숫자들은 오름차순으로 정렬됩니다.
+     *
+     * @return 생성된 로또 객체
+     */
+    public static Lotto generateLotto() {
+        List<Integer> numbers = generateNumbers();
+        sortNumbers(numbers);
+        return new Lotto(numbers);
+    }
+
+    /**
+     * 1부터 45까지의 숫자 중 중복없이 6개를 랜덤하게 선택합니다.
+     *
+     * @return 선택된 6개의 숫자 리스트
+     */
+    private static List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                LOTTO_NUMBER_SIZE
+        );
+    }
+
+    /**
+     * 주어진 리스트의 숫자들을 오름차순으로 정렬합니다.
+     *
+     * @param numbers 정렬할 숫자들이 담긴 리스트
+     */
+    private static void sortNumbers(List<Integer> numbers) {
+        Collections.sort(numbers);
+    }
+}
