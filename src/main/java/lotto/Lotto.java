409097d254f8aef2c2cd88b5f0ed32c7f1081493
dongwoo <woomacho@gmail.com>
2024-11-04 18:19:27 +0900

    feat: Lotto Entity 구현
    - 로또 번호 6자리를 저장하기 때문에 번호 개수, 중복 검증을 한다.
    - 저장되는 숫자는 오름차순으로 정렬한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,42 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateCount(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
+
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있으면 안됩니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개 입니다.");
+        }
+    }
+
+    public List<Integer> sortNumbers() {
+        return this.numbers.stream().sorted(Integer::compareTo).toList();
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    public int countMatchingNumbers(Lotto otherLotto) {
+        return Math.toIntExact(this.numbers.stream()
+                .filter(otherLotto.getNumbers()::contains).count());
+    }
+
+    public boolean isContain(int number) {
+        return this.numbers.contains(number);
+    }
 }
