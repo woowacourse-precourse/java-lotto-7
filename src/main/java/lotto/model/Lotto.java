fe997c306f847168df8885b218fd4e69d054bffe
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-04 18:53:37 +0900

    refactor: 유효성 검증 클래스 구현에 따라 검증 메소드 삭제

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.validator.LottoValidator;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,15 +12,9 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validateLottoNumbers(numbers);
         this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
     }
 
     public int countMatchingNumbers(Lotto winningLotto) {
