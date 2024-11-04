2d8eb091954f893d2585b506f91d2051ea493a59
Awon01 <bec00@naver.com>
2024-11-04 19:44:37 +0900

    feat(utils): 지정 범위 내에서 원하는 개수의 수를 무작위 생성하는 기능

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Utils {
+
+    public static List<Integer> randomUniqueNumberGenerate(int start, int end, int count) {
+        return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+
     public static void sortListNaturalOrder(List<Integer> list) {
         Collections.sort(list);
     }
--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -4,6 +4,13 @@ import java.util.HashSet;
 import java.util.List;
 
 public final class Validation {
+
+    public static void validateListNumberInRange(List<Integer> numbers, int startInclusive, int endInclusive) {
+        for (int number : numbers) {
+            validateNumberInRange(number, startInclusive, endInclusive);
+        }
+    }
+
     public static void validateNumberInRange(int number, int startInclusive, int endInclusive) {
         if (number < startInclusive || number > endInclusive) {
             throw new IllegalArgumentException();
