0e9ca06e1af8cc65896c88306530082785efe619
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 16:57:08 +0900

    feat: 당첨 번호 입력값의 분리된 각 번호가 정수인지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,42 +6,60 @@ import java.util.List;
 public class Lotto {
     private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<String> numbers) {
         validate(numbers);
         this.numbers = numbers.stream()
-            .map(LottoNumber::new)
-            .toList();
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .toList();
     }
 
     protected boolean contains(LottoNumber number) {
         return numbers.stream()
-            .anyMatch(lottoNumber -> lottoNumber == number);
+                .anyMatch(lottoNumber -> lottoNumber == number);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<String> numbers) {
         validateNumberCount(numbers);
+        validateInteger(numbers);
         validateUnique(numbers);
     }
 
-    private void validateNumberCount(List<Integer> numbers) {
+    private void validateNumberCount(List<String> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    private void validateUnique(List<Integer> numbers) {
+    private void validateInteger(List<String> numbers) {
+        if (!isInteger(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정수여야 합니다.");
+        }
+    }
+
+    private boolean isInteger(List<String> numbers) {
+        try {
+            numbers.forEach(Integer::parseInt);
+
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void validateUnique(List<String> numbers) {
         if (containsDuplicates(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
     }
 
-    private boolean containsDuplicates(List<Integer> lottoNumbers) {
+    private boolean containsDuplicates(List<String> lottoNumbers) {
         return new HashSet<>(lottoNumbers).size() != lottoNumbers.size();
     }
 
     public List<Integer> getNumbers() {
         return numbers.stream()
-            .map(LottoNumber::getNumber)
-            .toList();
+                .map(LottoNumber::getNumber)
+                .toList();
     }
 }
