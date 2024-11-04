37b95eee75ef4ddf9b7fda62f4001ff9200c19e0
mhj1205 <lightnow1205@gmail.com>
2024-11-04 14:29:23 +0900

    fetch: 로또 구입 금액 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = sc.nextInt();
+
     }
 }
