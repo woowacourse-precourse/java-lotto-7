a0a1b1a87f7c562517c52718717e7343736de229
Avocado <ujubongbong@gmail.com>
2024-11-04 22:58:44 +0900

    :recycle: refactor: Prize message 수정

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public enum Prize {
 
-	LOSE(0, 0, false, "0개 일치 - "),
-	FIFTH(5000, 3, false, "3개 일치 (5,000원) - "),
-	FOURTH(50000, 4, false, "4개 일치 (50,000원) - "),
-	THIRD(1500000, 5, false, "5개 일치 (1,500,000원) - "),
-	SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-	FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원) - ");
+	LOSE(0, 0, false, "0개 일치 -"),
+	FIFTH(5000, 3, false, "3개 일치 (5,000원) -"),
+	FOURTH(50000, 4, false, "4개 일치 (50,000원) -"),
+	THIRD(1500000, 5, false, "5개 일치 (1,500,000원) -"),
+	SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) -"),
+	FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원) -");
 
 	private final int reward;
 	private final int matchCount;
