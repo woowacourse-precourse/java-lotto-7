b5b3f0b2654377c83499daba83ee94472c39daf7
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:53:49 +0900

    Fix: PriceValidator 에 1,000원 단위로 가격 입력하는지 검증 로직 구현

--- src/main/java/lotto/view/validation/PriceValidator.java
+++ src/main/java/lotto/view/validation/PriceValidator.java
@@ -1,6 +1,7 @@
 package lotto.view.validation;
 
 import static lotto.service.exception.LottoExceptionMessage.PRICE_NOT_POSITIVE_INTEGER;
+import static lotto.service.exception.LottoExceptionMessage.PRICE_NOT_THOUSANDS_UNIT;
 import static lotto.service.exception.LottoExceptionMessage.PRICE_OVERFLOW;
 
 import lotto.service.exception.LottoException;
@@ -8,10 +9,12 @@ import lotto.service.exception.LottoException;
 public class PriceValidator {
 
     private static final String POSITIVE_INTEGER_REGEX = "^[1-9]\\d*$";
+    private static final int PRICE_UNIT = 1_000;
 
     public static int validate(String price) {
         validatePositiveInteger(price);
         validateOverflow(price);
+        validateThousandsUnit(price);
         return Integer.parseInt(price);
     }
 
@@ -26,6 +29,12 @@ public class PriceValidator {
             Integer.parseInt(price);
         } catch (NumberFormatException e) {
             throw new LottoException(PRICE_OVERFLOW);
+        }
+    }
+
+    private static void validateThousandsUnit(String price) {
+        if (Integer.parseInt(price) % PRICE_UNIT != 0) {
+            throw new LottoException(PRICE_NOT_THOUSANDS_UNIT);
         }
     }
 }
