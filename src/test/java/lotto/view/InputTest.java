6e16f233a019e89fd5804066bca6c90835a047b3
junho767 <junho767@naver.com>
2024-11-04 22:21:26 +0900

    test: 복권 구매 금액에 대한 테스트

--- src/test/java/lotto/view/InputTest.java
+++ src/test/java/lotto/view/InputTest.java
@@ -4,8 +4,10 @@ import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputTest {
@@ -23,17 +25,31 @@ public class InputTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호의 범위가 1보다 작고 45보다 크면 예외가 발생한다.")
+    @DisplayName("로또 번호의 범위가 45보다 크면 예외가 발생한다.")
     @Test
     void 로또_번호의_범위가_45보다_크면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(4, 6, 8, 10, 55, 1)))
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
+
     @DisplayName("로또 번호의 범위가 1보다 작으면 예외가 발생한다.")
     @Test
     void 로또_번호의_범위가_1보다_작으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 올바르면 정상적으로 생성된다")
+    void 로또_번호가_올바르면_정상적으로_생성된다() {
+        // Given
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // When
+        Lotto lotto = new Lotto(validNumbers);
+
+        // Then
+        assertThat(lotto.getNumbers()).isEqualTo(validNumbers);
     }
 }
