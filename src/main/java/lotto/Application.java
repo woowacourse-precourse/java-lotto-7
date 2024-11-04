a913c7c49fb537069b2897976746367908993094
JeongHwan Kim <silverice1973@naver.com>
2024-11-04 09:09:11 +0900

    feat: 로또 애플리케이션 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import lotto.machine.LottoMachine;
+import lotto.result.LottoRank;
+import lotto.result.LottoResult;
+import lotto.statistics.ProfitCalculator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = InputView.inputPurchaseAmount();
+
+        List<Lotto> lottos = LottoMachine.generateLottos(purchaseAmount);
+        OutputView.printPurchaseInfo(lottos);
+
+        List<Integer> winningNumbers = InputView.inputWinningNumbers();
+        int bonusNumber = InputView.inputBonusNumber(winningNumbers);
+
+        LottoResult lottoResult = new LottoResult(lottos, winningNumbers, bonusNumber);
+        Map<LottoRank, Integer> resultStatistics = lottoResult.getStatistics();
+
+        List<Integer> matchCounts = lottoResult.extractMatchCounts();
+
+        OutputView.printWinningStatistics(
+                matchCounts,
+                ProfitCalculator.calculateProfitRate(resultStatistics, purchaseAmount)
+        );
     }
 }
