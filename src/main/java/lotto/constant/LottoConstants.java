19216187332c1443ba14cef913685e9407a6e6e3
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-04 10:19:55 +0900

    refactor: 매직넘버 상수화

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -10,4 +10,7 @@ public class LottoConstants {
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_PRICE = 1_000;
     public static final int ZERO = 0;
+    public static final int INITIAL_COUNTER_NUMBER = 0;
+    public static final int COUNTER_STEP = 1;
+    public static final double PERCENTAGE_SCALE = 100.0;
 }
