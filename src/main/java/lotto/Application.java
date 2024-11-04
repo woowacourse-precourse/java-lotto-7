efadbf2c9716b7fe36f9c625ba198dab78517cbc
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-04 22:19:27 +0900

    feat: 메인 함수 동작 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.CalculateService;
+import lotto.service.LottoDrawService;
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+        LottoDrawService lottoDrawService = new LottoDrawService();
+        CalculateService calculateService = new CalculateService();
+
+        LottoController lottoController = new LottoController(lottoService,lottoDrawService,calculateService);
+        lottoController.run();
     }
 }
