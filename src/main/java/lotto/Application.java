030ea7e41d9173d382b6c554c4c5f85ce1e06e09
haennni <ggowater33@kangnam.ac.kr>
2024-11-04 20:23:51 +0900

    refactor(application): main 메서드에 프로그램 실행 로직 추가
    - Application 클래스의 main 메서드에 프로그램 실행 로직을 추가했습니다.
    - 전체 프로그램 실행 흐름을 main 메서드에 통합하여 실행 가능하게 구현했습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.config.LottoConfiguration;
+import lotto.input.InputHandler;
+import lotto.input.InputParser;
+import lotto.service.LottoGame;
+import lotto.utils.LottoUtils;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler();
+        InputParser inputParser = new InputParser();
+        LottoConfiguration configuration = new LottoConfiguration(inputHandler, inputParser);
+        LottoGame lottoGame = new LottoGame(configuration, new LottoUtils());
+        lottoGame.run();
     }
 }
