6de04301bcb8034f18c2e8e23b84dc6b076ab91a
woojin cheon <qpqpqp0102@naver.com>
2024-11-04 13:36:31 +0900

    feat(Lotto): 로또 구입 메서드, getter
    - 로또 한 장을 구입하고 반환하는 메서드
    - model 패키지로 변경

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    /*
+    Lotto에 numbers 이외의 필드(인스턴스 변수)를 추가할 수 없다.
+    numbers의 접근 제어자인 private은 변경할 수 없다.
+     */
+
+    public Lotto buyOneLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Lotto lotto = new Lotto(numbers);
+        return lotto;
+    }
+
+}
