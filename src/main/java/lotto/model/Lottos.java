b3961b78e5f069d004d09333f2400e189c9cd727
JEN <ekffuclxk@gmail.com>
2024-11-04 15:09:46 +0900

    feat(Lottos): 당첨 번호와 로또들을 매칭하여 일치 개수 계산 후 당첨 등수 결정 메서드 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,7 +1,9 @@
 package lotto.model;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -16,6 +18,26 @@ public class Lottos {
 
     public int getSize(){
         return lottos.size();
+    }
+
+    public Map<LottoRule, Integer> calculateMatch(List<Integer> winNumbers, int bonusNumber){
+        Map<LottoRule, Integer> matchInfo = new HashMap<>();
+
+        for (Lotto lotto : lottos) {
+            int matchCount = 0;
+
+            for (Integer winNumber : winNumbers) {
+                if(lotto.getNumbers().contains(winNumber)){
+                    matchCount++;
+                }
+            }
+
+            boolean bonusMatch = (matchCount == 5) && lotto.getNumbers().contains(bonusNumber);
+
+            LottoRule rule = LottoRule.getWinInfo(matchCount, bonusMatch);
+            matchInfo.put(rule, matchInfo.getOrDefault(rule, 0) + 1);
+        }
+        return matchInfo;
     }
 }
 
