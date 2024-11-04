f654c16227c18741aca60eac3e7a3a79376fa72f
Dongjun Lee <dongjle2@student.42heilbronn.de>
2024-11-04 02:51:09 +0100

    Feat(Lotto): add Lotto class
    Added validation for lotto numbers and return method

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,9 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static final int NUM_SIZE = 6;
+    private static final int NUM_MIN = 1;
+    private static final int NUM_MAX = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,32 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        validateNumberSize(numbers);
+        validateDuplicate(numbers);
+        validateEachNumberRange(numbers);
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != NUM_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateEachNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < NUM_MIN || NUM_MAX < number) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이입니다.");
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
