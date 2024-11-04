b1cb72a7c80cd27761d22533b1d7f89222a0e658
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 16:09:09 +0900

    feat: 구입 금액 검증(1000원으로 나누어 떨어지지 않는 경우 예외 발생)

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -2,9 +2,11 @@ package lotto.domain;
 
 public class Validator {
     public static final String INVALID_PURCHASE_AMOUNT_TYPE_ERROR = "[ERROR] 구입 금액은 숫자로 입력해야 합니다.";
+    public static final String INVALID_PURCHASE_AMOUNT_DIVISIBILITY_ERROR = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
 
     public static void validatePurchaseAmount(String input) {
         int purchaseAmount = parsePurchaseAmount(input);
+        validateDivisibility(purchaseAmount);
     }
 
     private static int parsePurchaseAmount(String input) {
@@ -12,6 +14,12 @@ public class Validator {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_TYPE_ERROR);
+        }
+    }
+
+    private static void validateDivisibility(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_DIVISIBILITY_ERROR);
         }
     }
 }
