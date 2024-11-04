803d6eb3d9ecf272a120a9bf373621273177c0c4
kim-se-hee <saree98@naver.com>
2024-11-04 14:52:16 +0900

    feat(Lotto): 당첨 로또 클래스 구현
    해당 클래스는 당첨 로또와 관련된 로직 수행
    - 사용자의 번호와 당첨 번호를 비교

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,11 +13,37 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public int compareNumberWithTicket(List<Integer> numbersInTicket) {
+        int matchingNumerCount = 0;
+
+        for (int i = 0; i < 6; i++) {
+            if (numbersInTicket.contains(numbers.get(i))) {
+                matchingNumerCount++;
+            }
+        }
+
+        return matchingNumerCount;
+    }
+
     private void validate(List<Integer> numbers) {
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        checkEachNumberIsUnique(numbers);
     }
 
-    // TODO: 추가 기능 구현
+    private void checkEachNumberIsUnique(List<Integer> numbers) {
+        Set<Integer> usedToken = new HashSet<>();
+
+        for (Integer number : numbers) {
+
+            if (usedToken.contains(number)) {
+                throw new IllegalArgumentException();
+            }
+
+            usedToken.add(number);
+        }
+    }
 }
