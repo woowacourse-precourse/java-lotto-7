7eae5ddbba5ae0b50f4c4c794ff35b446ec60ad1
Minuring <mw9505@gmail.com>
2024-11-04 19:20:27 +0900

    feat(main): 로또 애플리케이션 main
    프론트 컨트롤러를 통해 다른 컨트롤러들을 호출하는 방식으로 main()작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.presentation.controller.BonusNumberController;
+import lotto.presentation.controller.PurchaseController;
+import lotto.presentation.controller.StatisticsController;
+import lotto.presentation.controller.WinningNumbersController;
+import lotto.presentation.controller.common.FrontController;
+import lotto.presentation.model.Model;
+
 public class Application {
+
+    private static final AppConfig appConfig = AppConfig.getInstance();
+    private static final FrontController controller = appConfig.frontController();
+    private static final PurchaseController purchaseController = appConfig.purchaseController();
+    private static final WinningNumbersController winningNumbersController = appConfig.winningNumbersController();
+    private static final BonusNumberController bonusNumberController = appConfig.bonusNumberController();
+    private static final StatisticsController statisticsController = appConfig.statisticsController();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Model model = new Model();
+
+        controller.process(purchaseController, model);
+        controller.process(winningNumbersController, model);
+        controller.process(bonusNumberController, model);
+        controller.process(statisticsController, model);
     }
 }
