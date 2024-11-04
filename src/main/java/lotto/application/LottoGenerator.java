5bdab531f0f1cb3556a0baa50ca2fec23d460776
SongJaeHoonn <thdwogns121@naver.com>
2024-11-04 22:58:05 +0900

    refactor: 컴포넌트 클래스를 싱글톤으로 변환

--- src/main/java/lotto/application/LottoGenerator.java
+++ src/main/java/lotto/application/LottoGenerator.java
@@ -7,6 +7,19 @@ import java.util.List;
 
 public class LottoGenerator {
 
+    private static LottoGenerator instance;
+
+    private LottoGenerator() {
+
+    }
+
+    public static LottoGenerator getInstance() {
+        if (instance == null) {
+            instance = new LottoGenerator();
+        }
+        return instance;
+    }
+
     public Lotto generate() {
         List<Integer> LottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return Lotto.of(LottoNumbers);
