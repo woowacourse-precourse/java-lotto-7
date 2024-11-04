fa8f838fd2a422e646d63c34dc007a07c0dedc99
Koo-EunSung <ku903@naver.com>
2024-11-04 23:51:08 +0900

    Feat: 비즈니스 로직

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,7 +6,7 @@ import lotto.model.Lotto;
 import lotto.model.Prize;
 
 public class OutputView {
-    public void printLottoBuyHistory(List<Lotto> lottos) {
+    public static void printLottoBuyHistory(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             List<Integer> sortedNumbers = lotto.getNumbers()
@@ -18,7 +18,7 @@ public class OutputView {
         }
     }
 
-    public void printResult(Map<Prize, Integer> result, double profitRate) {
+    public static void printResult(Map<Prize, Integer> result, double profitRate) {
         System.out.println("당첨 통계\n---");
         for (int i = Prize.values().length - 2; i >= 0; i--) {
             Prize rank = Prize.values()[i];
