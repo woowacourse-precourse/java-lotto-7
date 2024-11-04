dd95a55863e70b3a48182faa9a550d1140779b3b
dk7648 <dk7648@korea.ac.kr>
2024-11-04 13:18:51 +0900

    feat: 안내문 출력 기능 추가

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -1,4 +1,29 @@
 package lotto.controller;
 
+import lotto.model.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
 public class Game {
+    public void start() {
+        OutputView.requestMoney();
+        Money money = new Money(InputView.readMoney());
+        LottoManager lottoManager = new LottoManager();
+        Store store = new Store();
+        lottoManager.buyLotto(store, money);
+        OutputView.printLottos(lottoManager.getLottos());
+        OutputView.requestWinningNumbers();
+        Lotto winningNumbers = new Lotto(InputView.readNumbers());
+        OutputView.requestBonusNumber();
+        int bonusNumber = InputView.readNumber();
+        Comparator comparator = new Comparator();
+        List<Result> lottoResults =  comparator.compareLottos(lottoManager.getLottos(), winningNumbers, bonusNumber);
+        lottoManager.setResults(lottoResults);
+        money.receiveWinningAmount(lottoManager.getWinningAmount());
+        OutputView.printStastistics();
+        OutputView.printRateOfReturn(money.getRateOfReturn());
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,26 +2,50 @@ package lotto.view;
 
 import lotto.model.Lotto;
 import lotto.model.LottoManager;
+import lotto.model.Result;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    public static void requestMoney() {
+        System.out.println("구입금액을 입력해 주세요");
+    }
     public static void printLottos(List<Lotto> lottos) {
         printNumberOfLotto(lottos);
         for (Lotto lotto : lottos) {
             printNumbers(lotto.getNumbers());
         }
     }
-
-    public static void printNumberOfLotto(List<Lotto> lottos) {
+    private static void printNumberOfLotto(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
     }
 
-    public static void printNumbers(List<Integer> numbers) {
+    private static void printNumbers(List<Integer> numbers) {
         String result = "[" + numbers.stream()
                 .map(String::valueOf).
                 collect(Collectors.joining(", ")) + "]";
         System.out.println(result);
+    }
+
+    public static void requestWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void requestBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printStastistics() {
+        System.out.println("당첨통계");
+        System.out.println("'---");
+        System.out.println("3개 일치 (5,000원) - ");
+        System.out.println("4개 일치 (50,000원) - ");
+        System.out.println("5개 일치 (1,500,000원) - ");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
+        System.out.println("6개 일치 (2,000,000,000원) - ");
+    }
+    public static void printRateOfReturn(double rateOfReturn) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", rateOfReturn);
     }
 }
