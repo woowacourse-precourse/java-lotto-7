48d508ac9b86256f52a7213b6b1591ea876f22f7
woong2e <tlswodnd12345@naver.com>
2024-11-04 13:00:39 +0900

    feat: add method to output statistics

--- src/main/java/lotto/io/OutputWriter.java
+++ src/main/java/lotto/io/OutputWriter.java
@@ -1,23 +1,33 @@
 package lotto.io;
 
+import lotto.draw.Rank;
 import lotto.vendingmachine.Lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputWriter {
+    private final StringBuilder sb = new StringBuilder();
 
     public void write(List<Lotto> lottos) {
-        StringBuilder sb = new StringBuilder();
-
         sb.append("\n").append(lottos.size()).append("개를 구매했습니다.").append("\n");
         for (Lotto lotte : lottos) {
             sb.append(lotte.toString()).append("\n");
         }
 
         System.out.println(sb);
+        sb.setLength(0);
     }
 
-    public void printStatistics() {
+    public void printStatistics(Map<Rank, Integer> lottoStatistics, double earningsRate) {
+        sb.append("\n").append("당첨 통계").append("\n").append("---").append("\n");
 
+        for (Map.Entry<Rank, Integer> entry : lottoStatistics.entrySet()) {
+            sb.append(entry.getKey().getMessage(entry.getValue())).append("\n");
+        }
+
+        sb.append("총 수익률은 ").append(earningsRate).append("%입니다.\n");
+
+        System.out.print(sb);
     }
 }
