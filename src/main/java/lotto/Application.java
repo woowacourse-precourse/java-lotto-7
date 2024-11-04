81b9666a810b490c9c9b8ea3200c53b078822938
kcs19 <changsung19@gmail.com>
2024-11-04 09:35:03 +0900

    변경사항 없음

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lottoController.GenerateLottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/lottoController/GenerateLottoController.java
+++ src/main/java/lottoController/GenerateLottoController.java
@@ -1,4 +1,14 @@
 package lottoController;
 
+import lotto.Lotto;
+import view.InputView;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateLottoController {
+
+    private static final int LOTTO_PRICE = 1000;
+    List<Lotto> lottoes = new ArrayList<>();
+
 }
