66ae8d0f064d7c2087cbc0ec18bc52862c25a1d4
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-11-04 18:17:55 +0900

    test: LottoResultTest

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class LottoResultTest {
     LottoResult lottoResult;
 
@@ -12,9 +15,27 @@ public class LottoResultTest {
     void setup() {
         lottoResult = new LottoResult();
     }
+
+    @DisplayName("등수 계산 시 1보다 작은 값이 들어오면 예외가 발생한다.")
+    @Test
+    void 등수_계산_시_1보다_작은_값이_들어오면_예외가_발생한다() {
+        assertThatThrownBy(() -> lottoResult.updateLottoRankSize(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("등수 계산 시 5보다 큰 값이 들어오면 예외가 발생한다.")
+    @Test
+    void 등수_계산_시_5보다_큰_값이_들어오면_예외가_발생한다() {
+        assertThatThrownBy(() -> lottoResult.updateLottoRankSize(6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("상금 계산기 테스트")
     @Test
     void 상금_계산기_테스트() {
         lottoResult.updateLottoRankSize(1);
+        int sum = lottoResult.calculPrizeMoney();
+
+        assertThat(sum).isEqualTo(2000000000);
     }
 }
