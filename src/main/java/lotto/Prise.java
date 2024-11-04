1a794c6494f7d26584f0957f379e812b01fa7cc4
moeun9309 <moeun9309@gmail.com>
2024-11-04 14:16:43 +0900

    feat: prise Enum

---
+++ src/main/java/lotto/Prise.java
@@ -0,0 +1,35 @@
+package lotto;
+
+public enum Prise {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FORTH(4, 50000),
+    FIFTH(3, 5000),
+    SIXTH(2, 0);
+
+    private final int match;
+    private final int money;
+
+    Prise(int match, int money) {
+        this.match = match;
+        this.money = money;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public static Prise getRank(int match, boolean bonusMatch) {
+        for (Prise p : Prise.values()) {
+            if (p.getMatch() == match) {
+                return p;
+            }
+        }
+        return SIXTH;
+    }
+}
