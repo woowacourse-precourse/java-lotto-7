164bf62c8bab403a3074460b7c8bb6f9e75d7801
mhj1205 <lightnow1205@gmail.com>
2024-11-04 15:26:12 +0900

    feat: 당첨 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -11,6 +13,8 @@ public class Application {
             int purchaseAmount = getPurchaseAmount();
             List<Lotto> purchasedLottos = generateLottos(purchaseAmount / 1000);
             printPurchasedLottos(purchasedLottos);
+
+            List<Integer> winningNumbers = getWinningNumbers();
     }
 
     // 구입 금액 입력 처리
@@ -38,5 +42,26 @@ public class Application {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
+    }
+
+    // 당첨 번호 입력
+    private static List<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] inputs = Console.readLine().split(",");
+        if (inputs.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        Set<Integer> winningNumbers = new HashSet<>();
+        for (String input : inputs) {
+            int number = Integer.parseInt(input.trim());
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 번호는 1에서 45 사이여야 합니다.");
+            }
+            winningNumbers.add(number);
+        }
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복되지 않아야 합니다.");
+        }
+        return new ArrayList<>(winningNumbers);
     }
 }
