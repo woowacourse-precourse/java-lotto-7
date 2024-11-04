58e071b8babfb5628d243fb319c0e540daa0f47d
seoyoungjin23 <ascd050@gmail.com>
2024-11-04 09:20:11 +0900

    feat(InputHandler): 입력 기능 추가

---
+++ src/main/java/lotto/InputHandler.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    private static final String PURCHASE_AMOUNT_PROMPT = "구입 금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+
+    public String getPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_PROMPT);
+        return Console.readLine();
+    }
+
+    public String getWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_PROMPT);
+        return Console.readLine();
+    }
+
+    public String getBonusNumber() {
+        System.out.println(BONUS_NUMBER_PROMPT);
+        return Console.readLine();
+    }
+}
