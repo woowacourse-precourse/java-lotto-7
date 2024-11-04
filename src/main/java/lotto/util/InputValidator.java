cc3f97d35e3ef4e8e9c4494d3afaa1f5f28c3df2
chaeyeon0130 <k84292330@gmail.com>
2024-11-04 16:30:54 +0900

    feat(InputValidator): 구입 금액이 10만원을 초과하는 경우 에외 처리

--- src/main/java/lotto/util/InputValidator.java
+++ src/main/java/lotto/util/InputValidator.java
@@ -3,11 +3,8 @@ package lotto.util;
 import java.util.List;
 
 public class InputValidator {
-    public void checkDividedBy1000(int purchaseAmount) {
-        if (purchaseAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위이어야 합니다.");
-        }
-    }
+    private static final int MIN_PURCHASE_AMOUNT = 1000;
+    private static final int MAX_PURCHASE_AMOUNT = 100000;
 
     public void checkInRange1To45(int num) {
         if (num < 1 || num > 45) {
@@ -15,18 +12,36 @@ public class InputValidator {
         }
     }
 
-    public void checkMinimumAmount(int purchaseAmount) {
+    public void checkBonusNotInWinning(int bonusNum, List<Integer> winningNums) {
+        if (winningNums.contains(bonusNum)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public void validateAmount(int purchaseAmount) {
+        checkMinimumAmount(purchaseAmount);
+        checkMaximumAmount(purchaseAmount);
+        checkDividedBy1000(purchaseAmount);
+    }
+
+    private void checkMinimumAmount(int purchaseAmount) {
         if (purchaseAmount <= 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
         }
-        if (purchaseAmount < 1000) {
+        if (purchaseAmount < MIN_PURCHASE_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최소 1000원 이상이어야 합니다.");
         }
     }
 
-    public void checkBonusNotInWinning(int bonusNum, List<Integer> winningNums) {
-        if (winningNums.contains(bonusNum)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    private void checkMaximumAmount(int purchaseAmount) {
+        if (purchaseAmount > MAX_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 최대 100,000원까지 가능합니다.");
+        }
+    }
+
+    private void checkDividedBy1000(int purchaseAmount) {
+        if (purchaseAmount % MIN_PURCHASE_AMOUNT != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위이어야 합니다.");
         }
     }
 }
