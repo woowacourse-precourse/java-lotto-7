bb9d30fe3301472cd9df42b03a7c97fa2053e420
junsoo0719 <junsoo0719@gmail.com>
2024-11-04 14:17:57 +0900

    feat: 로또 발행 기능 작성 LottoCreate 클래스 생성자, createLottos(),getLottoCount()

---
+++ src/main/java/lotto/model/LottoCreate.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoCreate {
+    private int lottoCount;
+    private List<Lotto> lottos;
+
+    public LottoCreate(int purchasePrice) {
+        lottoCount = purchasePrice / 1000;
+        createLottos();
+    }
+
+    public void createLottos() {
+        lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+}
