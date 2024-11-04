c178a4466676c0969bfd239b975d895788a01de9
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 20:10:08 +0900

    test: lottos 객체의 당첨 번호 setting과 관련한 test 구현

--- src/test/java/lotto/entity/LottosTest.java
+++ src/test/java/lotto/entity/LottosTest.java
@@ -3,6 +3,7 @@ package lotto.entity;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -48,6 +49,91 @@ public class LottosTest {
             // 1000원 이하
             Lottos lottos = new Lottos();
             assertThatThrownBy(() -> lottos.buyLottos(50000000000L))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("로또 당첨 번호 세팅 Test")
+    class SetWinningNumberTest {
+
+        @DisplayName("당첨 번호 개수 예외 (7개)")
+        @Test
+        void countOfWinningNumbersError1() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+            Integer bonusNumber = 9;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 개수 예외 (5개)")
+        @Test
+        void countOfWinningNumbersError2() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5);
+            Integer bonusNumber = 9;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 중복 예외")
+        @Test
+        void duplicationOfWinningNumbersError() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 6, 6);
+            Integer bonusNumber = 9;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 범위 예외 (초과)")
+        @Test
+        void winningNumbersOutOfRangeError1() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 46);
+            Integer bonusNumber = 9;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 범위 예외 (미만)")
+        @Test
+        void winningNumbersOutOfRangeError2() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 0);
+            Integer bonusNumber = 9;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 범위 예외 (초과)")
+        @Test
+        void bonusNumberOutOfRangeError1() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+            Integer bonusNumber = 55;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("당첨 번호 범위 예외 (미만)")
+        @Test
+        void bonusNumberOutOfRangeError2() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+            Integer bonusNumber = 0;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("보너스 번호 중복")
+        @Test
+        void duplicationOfBonusNumberError() {
+            List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+            Integer bonusNumber = 1;
+            Lottos lottos = new Lottos();
+            assertThatThrownBy(() -> lottos.setWinningNumbers(winningNumbers, bonusNumber))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
