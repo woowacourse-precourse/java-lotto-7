721b69f239654c430fefdc0442fb3210188d177a
YongLeeCode <yonghwan311@gmail.com>
2024-11-04 22:30:08 +0900

    feat: 당첨번호 적합성 테스트 구현

---
+++ src/test/java/validator/PrizeNumberValidatorTest.java
@@ -0,0 +1,45 @@
+package validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+public class PrizeNumberValidatorTest {
+
+    @Test
+    void 당첨_번호가_범위에서_벗어난_경우() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 66);
+
+        // when
+        assertThatThrownBy(() -> PrizeNumberValidator.validate(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.PRIZE_NUMBER_OUT_OF_RANGE); // then
+
+    }
+
+    @Test
+    void 당첨_번호가_중복_되는_경우() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 3, 4, 5);
+
+        // when
+        assertThatThrownBy(() -> PrizeNumberValidator.validate(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.PRIZE_NUMBER_DUPLICATION); // then
+    }
+
+    @Test
+    void 당첨_번호_수가_6개가_아닌_경우() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        // when
+        assertThatThrownBy(() -> PrizeNumberValidator.validate(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.PRIZE_COUNT_OUT_OF_SIX); // then
+    }
+
+}
