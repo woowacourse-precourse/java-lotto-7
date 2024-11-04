0405843582d36ab7a81ec38d31372ca46c327e67
나상연 <reddynor@naver.com>
2024-11-04 12:38:55 +0900

    test(LottoValidator): Add bonus number validation tests for duplication and range
    - Added tests to check if the bonus number overlaps with any winning numbers
      - Validates that an exception is thrown when the bonus number duplicates a winning number
      - Confirms successful validation when the bonus number is unique within the winning numbers
    - Enhanced input validation for bonus number range and format

--- src/test/java/lotto/validator/LottoValidatorTest.java
+++ src/test/java/lotto/validator/LottoValidatorTest.java
@@ -2,6 +2,10 @@ package lotto.validator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static lotto.validator.LottoValidator.stringToInt;
 import static lotto.validator.LottoValidator.unitValidator;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,6 +44,23 @@ class LottoValidatorTest {
         int invalidBonusNum = 50; // 1~45 범위를 벗어난 경우
         assertThatThrownBy(() -> LottoValidator.checkRangeLotto(invalidBonusNum))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스번호_당첨번호_간에_공통수_있는경우_테스트(){
+        int duplicateBonusNum = 25;
+        List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5, 25);
+
+        assertThatThrownBy(() -> LottoValidator.checkDuplicate(numList, duplicateBonusNum))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스번호_당첨번호_간에_다른경우_테스트() {
+        int duplicateBonusNum = 26;
+        List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5, 25);
+
+        assertThat(LottoValidator.checkDuplicate(numList, duplicateBonusNum)).isTrue();
     }
 
 
