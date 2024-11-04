0e7dc4aa0b89dd9314cb493dd60c065cbb653db3
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-11-04 09:49:30 +0900

    feat(Messages) : 출력 ㅜ문구 클래스 생성  및 구현

---
+++ src/main/java/view/Messages.java
@@ -0,0 +1,18 @@
+package view;
+
+public enum Messages {
+    PAYMENT_INPUT_MSG("구입금액을 입력해 주세요."),
+    AMOUNT_PURCHASE_MSG("개를 구매했습니다."),
+    LOTTO_INPUT_MSG("당첨 번호를 입력해 주세요."),
+    BONUS_INPUT_MSG("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
