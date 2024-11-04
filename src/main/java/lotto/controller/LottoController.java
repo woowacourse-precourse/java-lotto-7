97d8ce99a0f9eb6d0079b3b822121cc4ab11ccf1
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-11-04 23:04:20 +0900

    refactor: Controller 메소드 리팩토링

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -8,7 +8,6 @@ import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -21,17 +20,35 @@ public class LottoController {
     }
 
     public void run() {
+        int purchaseAmount = processPurchase();
+        PurchasedLotto purchasedLotto = createPurchasedLotto(purchaseAmount);
+        Map<LottoWinnerPrize, Integer> winningResults = processAndPrintWinningResults(purchasedLotto);
+        processAndPrintRateOfResult(winningResults, purchaseAmount);
+    }
+
+    private int processPurchase() {
         int purchaseAmount = getPurchaseAmount();
         int lottoQuantity = lottoService.calculateLottoQuantity(purchaseAmount);
         OutputView.printPurchaseAmount(lottoQuantity);
+        return purchaseAmount;
+    }
 
+    private PurchasedLotto createPurchasedLotto(int purchaseAmount) {
+        int lottoQuantity = lottoService.calculateLottoQuantity(purchaseAmount);
         PurchasedLotto purchasedLotto = lottoService.createPurchasedLotto(lottoQuantity);
         OutputView.printPurchasedLottos(purchasedLotto.getLottos());
+        return purchasedLotto;
+    }
 
+    private Map<LottoWinnerPrize, Integer> processAndPrintWinningResults(PurchasedLotto purchasedLotto) {
         LottoManager lottoManager = lottoService.createLottoManager(getWinningNumbers(), getBonusNumber());
         Map<LottoWinnerPrize, Integer> prizeCount = lottoManager.getWinningPrizes(purchasedLotto.getLottos());
-        printPrize(prizeCount);
 
+        printPrizeResult(prizeCount);
+        return prizeCount;
+    }
+
+    private void processAndPrintRateOfResult(Map<LottoWinnerPrize, Integer> prizeCount, int purchaseAmount) {
         double rateOfReturn = lottoService.getRateOfReturn(prizeCount, purchaseAmount);
         OutputView.printRateOfReturn(rateOfReturn);
     }
@@ -48,7 +65,7 @@ public class LottoController {
         return validateInput(() -> lottoService.getBonusNumber(InputView.getBonusNumber()));
     }
 
-    private void printPrize(Map<LottoWinnerPrize, Integer> prizeCount) {
+    private void printPrizeResult(Map<LottoWinnerPrize, Integer> prizeCount) {
         OutputView.promptWINNING_STATISTICS();
         lottoService.getPrizeList(prizeCount)
                 .forEach(prize -> OutputView.printPrizeCount(prize.getDescription(), prizeCount.get(prize)));
