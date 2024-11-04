5e9105902f91772fd1de210a6003d9aa4dba50e7
Gothax <scott990328@gmail.com>
2024-11-04 20:43:35 +0900

    feat: match 함수 추가, 정렬방식 변경 - test code 미통과해서 sort 방식 변경 기존 Integer::compareTo 통과하지 못함 - 인스턴스 변수 로또가 몇개 일치하는지 확인하는 match 메서드

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private Integer matchCount = 0;
+    private Boolean bonusExist = false;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,11 +16,33 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public void match(List<String> winningNums, String bonusNum) {
+        for (String winningNum : winningNums) {
+            if (numbers.contains(Integer.parseInt(winningNum))) {
+                this.matchCount++;
+            }
+        }
+        if (numbers.contains(Integer.parseInt(bonusNum))) {
+            bonusExist = true;
+        }
+    }
+
+    public Integer getMatchCount() {
+        return matchCount;
+    }
+
+    public Boolean getBonusExist() {
+        return bonusExist;
     }
 
     @Override
     public String toString() {
-        numbers.sort(Integer::compareTo);
-        return numbers.toString();
+        List<Integer> collected = numbers.stream().sorted().toList();
+        return collected.toString();
     }
 }
