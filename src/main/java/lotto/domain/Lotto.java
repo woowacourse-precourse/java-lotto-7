363df9eb5b4b5744c4d23de490404da4a1dc08f8
DawnGlow <ferrater1013@gmail.com>
2024-11-04 17:34:51 +0900

    feat: 로또 숫자 범위 validation 추가
    로또의 숫자 범위가 올바른지 validation 하는 기능을 추가합니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,12 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 유효한 범위의 숫자가 아닙니다.");
         }
     }
 }
