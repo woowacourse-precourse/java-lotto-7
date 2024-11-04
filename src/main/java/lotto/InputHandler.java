fb94acd385b922647e4495d03c7c719318498b83
soonvro <soonvro@gmail.com>
2024-11-04 21:16:41 +0900

    feat: 로또 구입 금액이 1,000으로 나누어 떨어지지 않을 경우 예외 발생 기능 추가

--- src/main/java/lotto/InputHandler.java
+++ src/main/java/lotto/InputHandler.java
@@ -5,5 +5,8 @@ public class InputHandler {
         if (budget <= 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 0원 이상이어야 합니다.");
         }
+        if (budget % 1_000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
     }
 }
--- src/test/java/lotto/InputHandlerTest.java
+++ src/test/java/lotto/InputHandlerTest.java
@@ -14,4 +14,15 @@ public class InputHandlerTest {
         assertThatThrownBy(() -> InputHandler.validateLottoBudget(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 구입 금액이 1,000으로 나누어 떨어지지 않을 경우 예외가 발생한다.")
+    @Test
+    void 로또_구입_금액이_1_000으로_나누어_떨어지지_않을_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> InputHandler.validateLottoBudget(1))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputHandler.validateLottoBudget(999))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> InputHandler.validateLottoBudget(1_001))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
