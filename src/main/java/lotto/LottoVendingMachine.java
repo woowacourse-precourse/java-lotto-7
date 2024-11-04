d7f2ee798dfdc5e11e5d259bcf2840e2a8406c2c
taehyun <xogus5070318@gmail.com>
2024-11-04 16:44:16 +0900

    fix(LottoVendingMachine): 로또 생성자 리스트를 가변리스트로 변경
    테스트 중 UnsupportedOperationException 예외가 발생해서 이를 해결하던 중,
    Randoms.pickUniqueNumbersInRange(1, 45, 6)로 생성한 배열이 불변 리스트라는 것을 알게되었습니다.
    해당 배열을 정렬하는 과정에서 오류가 발생했다는 것을 발견하였고, 가변 리스트로 변경하여 Lotto 클래스의 생성자로 입력하였습니다.
    
    BREAKING CHANGE: None

--- src/main/java/lotto/LottoVendingMachine.java
+++ src/main/java/lotto/LottoVendingMachine.java
@@ -59,7 +59,8 @@ public class LottoVendingMachine {
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 1; i <= (money / 1000); i++) {
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(new Lotto(numbers));
         }
 
         return lottos;
