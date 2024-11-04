e7a34a22d2e84fc26d5e51831d47aef4d8f3d45b
heeyeop Kim <ssucgoo@gmail.com>
2024-11-04 23:18:45 +0900

    feat: 로또 번호 동일한 개수를 점수로 계산하는 기능 구현

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -10,4 +10,12 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
         this.lotto = new Lotto(numbers);
     }
+
+    public int getScore(Lotto lotto) {
+        int normalCount = lotto.compare(this.lotto);
+        int bonusCount = lotto.compare(this.bonusNumber);
+
+        LottoState state = new LottoState(normalCount, bonusCount);
+        return state.getScore();
+    }
 }
