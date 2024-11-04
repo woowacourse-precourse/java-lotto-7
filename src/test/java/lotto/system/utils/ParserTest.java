9edd8e9b23c380ba015d094d7d77c5cb957a09bc
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 22:19:33 +0900

    test: Parser 클래스 메서드 테스트 추가

---
+++ src/test/java/lotto/system/utils/ParserTest.java
@@ -0,0 +1,57 @@
+package lotto.system.utils;
+
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+import lotto.system.unit.LottoNumber;
+import lotto.system.unit.LottoTicket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ParserTest {
+
+    @DisplayName("LottoTicket 리스트를 구분자로 연결된 문자열로 변환 테스트")
+    @Test
+    void joinWithDelimiterTest() {
+        // given
+        List<LottoTicket> tickets = List.of(
+                LottoTicket.ofLottoNumbers(List.of(LottoNumber.of(1), LottoNumber.of(2), LottoNumber.of(3), LottoNumber.of(4), LottoNumber.of(5), LottoNumber.of(6))),
+                LottoTicket.ofLottoNumbers(List.of(LottoNumber.of(7), LottoNumber.of(8), LottoNumber.of(9), LottoNumber.of(10), LottoNumber.of(11), LottoNumber.of(12)))
+        );
+        String delimiter = ";";
+
+        // when
+        String result = Parser.joinWithDelimiter(tickets, delimiter);
+
+        // then
+        assertThat(result).isEqualTo("[1, 2, 3, 4, 5, 6]; [7, 8, 9, 10, 11, 12]");
+    }
+
+    @DisplayName("구분자를 사용한 문자열을 정수 리스트로 변환 테스트")
+    @Test
+    void parseWithDelimiterTest() {
+        // given
+        String input = "1,2,3,4,5,6";
+        String delimiter = ",";
+
+        // when
+        List<Integer> result = Parser.parseWithDelimiter(input, delimiter);
+
+        // then
+        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("기본 구분자를 사용한 문자열을 정수 리스트로 변환 테스트")
+    @Test
+    void parseWithDefaultDelimiterTest() {
+        // given
+        String input = "7,8,9,10,11,12";
+
+        // when
+        List<Integer> result = Parser.parseWithDelimiter(input);
+
+        // then
+        assertThat(result).containsExactly(7, 8, 9, 10, 11, 12);
+    }
+}
