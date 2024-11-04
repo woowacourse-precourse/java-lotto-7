34de0b2b3d824f62939fd9b74f99ab8110217f21
88dldl <chltnrgml0999@gmail.com>
2024-11-04 15:41:07 +0900

    feat(ErrorStatus): 예외 상태 추가

--- src/main/java/lotto/error/ErrorStatus.java
+++ src/main/java/lotto/error/ErrorStatus.java
@@ -1,7 +1,12 @@
 package lotto.error;
 
 public enum ErrorStatus {
-    INVALID_INPUT_INTEGER("[ERROR] 입력값은 정수여야 합니다.");
+    NULL_OR_EMPTY_INPUT("[ERROR] 입력값은 null이거나 빈 문자열일 수 없습니다."),
+    INVALID_NEGATIVE_NUMBER("[ERROR] 음수는 입력할 수 없습니다."),
+    INVALID_MONEY_AMOUNT("[ERROR] 로또 금액은 1000원 단위여야 합니다."),
+    INVALID_INPUT_INTEGER("[ERROR] 입력값은 정수여야 합니다."),
+    DUPLICATE_NUMBER("[ERROR] 입력값에 중복된 숫자가 있습니다: "),
+    INVALID_NUMBER_SIZE("[ERROR] 로또 번호는 6개여야 합니다.");
     private final String message;
 
     ErrorStatus(String message) {
