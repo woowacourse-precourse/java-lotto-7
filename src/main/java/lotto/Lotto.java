fb938feade2599cf15027607c2c2bf5210172712
Avocado <ujubongbong@gmail.com>
2024-11-04 22:52:33 +0900

    :sparkles: feat: 로또 번호에 중복이 있는지 확인하는 함수

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,22 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-
-import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Set;
 
 public class Lotto {
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
 		validate(numbers);
-		this.numbers = numbers;
+		this.numbers = new ArrayList<>(numbers);
+		Collections.sort(this.numbers);
 	}
 
 	private void validate(List<Integer> numbers) {
+		validateDuplicate(numbers);
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
 		}
@@ -23,45 +26,12 @@ public class Lotto {
 		return numbers;
 	}
 
-	public int getLottoCount(int payment) {
-		return payment / 1000;
-	}
-
-	public List<Lotto> getLotto(int lottoCount) {
-		List<Lotto> lottos = new ArrayList<>();
-		for (int i = 0; i < lottoCount; i++) {
-			lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-		}
-		return lottos;
-	}
-
-	public boolean isBonusMatch(List<Integer> lottos, int bonusNumber) {
-		for (Integer num : lottos) {
-			if (num == bonusNumber) {
-				return true;
+	public void validateDuplicate(List<Integer> numbers) {
+		Set<Integer> uniqueNumbers = new HashSet<>();
+		for (Integer s : numbers) {
+			if (!uniqueNumbers.add(s)) {
+				throw new IllegalArgumentException("[ERROR] 로또 번호에 중복이 있을 수 없습니다.");
 			}
 		}
-		return false;
-	}
-
-	public int getMatchCount(List<Integer> lottos, List<Integer> luckyNumber, int bonusNumber) {
-		return (int) lottos.stream()
-			.filter(num -> luckyNumber.stream()
-			.anyMatch(i -> i == num) || num == bonusNumber)
-			.count();
-	}
-
-	public long calculateTotalEarningRate(List<Lotto> lottos, List<Integer> luckyNumber, int bonusNumber) {
-		long total = 0;
-		for(Lotto lotto : lottos) {
-			List<Integer> numbers = lotto.getNumbers();
-			int matchCount = getMatchCount(numbers, luckyNumber, bonusNumber);
-			total += Prize.getPrize(matchCount, isBonusMatch(numbers, bonusNumber)).getReward();
-		}
-		return total;
-	}
-
-	public double calculateEarningRate(long total, int payment) {
-		return (double) total / payment * 100;
 	}
 }
