00b6e66138683ba9487d1c16a80dee7212ac80f7
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 20:15:15 +0900

    refactor(Application): Applcation 주석, 줄바꿈 리팩토링
    주석수정과 줄바꿈을 이용해서 가독성을 향상시켰습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,21 +4,25 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        // 로또 번호 생성기 초기화
         LottoNumGenerator lottoNumGenerator = new LottoNumGenerator();
 
-        // 구매 금액 입력
+        // 구매 금액 입력 및 로또 구매
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = InputView.inputPurchaseAmount();
         Buyer buyer = new Buyer(purchaseAmount);
         buyer.purchaseLottos(lottoNumGenerator);
+
         // 구매한 로또 번호 출력
         buyer.printPurchasedLottos();
 
-        // 당첨/보너스 번호 입력받기
+        // 당첨 번호 및 보너스 번호 입력
         System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> winningNumber = InputView.inputWinningNumber();
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = InputView.inputBonusNumber();
+
+        // 로또 게임 객체 생성 및 당첨 번호 설정
         LottoGame lottoGame = new LottoGame(winningNumber, bonusNumber);
 
         // 당첨 결과 출력
