0040f2e6dddc9408d948764298ed682e1cbb0656
AppleMint98 <hong721816@konkuk.ac.kr>
2024-11-04 14:58:52 +0900

    feat : 잘못된 값을 입력한 경우 다시 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,35 +14,49 @@ public class InputView {
 
     public static long inputPurchaseMoney() {
 
-        System.out.println("구입금액을 입력해 주세요.");
-        String inputPurchaseMoney = Console.readLine().trim();
-        validatePurchaseMoney(inputPurchaseMoney);
-        System.out.println();
-
-        return Long.parseLong(inputPurchaseMoney);
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String inputPurchaseMoney = Console.readLine().trim();
+                validatePurchaseMoney(inputPurchaseMoney);
+                System.out.println();
+                return Long.parseLong(inputPurchaseMoney);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static Set<Integer> inputWinningNumbers() {
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String winningNumbersInput = Console.readLine().trim();
-        validateWinningNumbers(winningNumbersInput);
-
-        Set<Integer> winningNumbers = convertToTreeSet(winningNumbersInput);
-        validateDuplicateWinningNumbers(winningNumbers);
-        System.out.println();
-
-        return winningNumbers;
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String winningNumbersInput = Console.readLine().trim();
+                validateWinningNumbers(winningNumbersInput);
+                Set<Integer> winningNumbers = convertToTreeSet(winningNumbersInput);
+                validateDuplicateWinningNumbers(winningNumbers);
+                System.out.println();
+                return winningNumbers;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static int inputBonusWinningNumber() {
 
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String inputBonusNumber = Console.readLine().trim();
-        validateBonusWinningNumber(inputBonusNumber);
-        System.out.println();
-
-        return Integer.parseInt(inputBonusNumber);
+        while (true) {
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String inputBonusNumber = Console.readLine().trim();
+                validateBonusWinningNumber(inputBonusNumber);
+                System.out.println();
+                return Integer.parseInt(inputBonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 }
