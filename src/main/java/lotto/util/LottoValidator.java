020a4251fe5fce4d515e527b2e82623c4229d284
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 20:23:17 +0900

    feat: 로또 검증을 하는 LottoValidator 객체 추가

---
+++ src/main/java/lotto/util/LottoValidator.java
@@ -0,0 +1,47 @@
+package lotto.util;
+
+import java.util.List;
+
+public class LottoValidator extends Validator<List<Integer>> {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+
+    @Override
+    public void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateNumberRange(numbers);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(this::isOutOfRange)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM;
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public void validateBonusNumber(List<Integer> numbers, int bonusNumber) {
+        if (isOutOfRange(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+}
