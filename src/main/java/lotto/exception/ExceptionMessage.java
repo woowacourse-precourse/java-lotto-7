385645d5851c20438c77281d49020e262db1b15a
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 20:50:21 +0900

    style: 예외 메세지 오타 수정

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -6,7 +6,7 @@ public enum ExceptionMessage {
     INVALID_PURCHASE_PRICE("잘못된 단위의 가격입니다."),
     INVALID_LOTTO_NUMBER("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATED_BONUS_NUMBER("보너스 번호에 중복된 번호가 있습니다."),
-    NOT_NUMBER_STRING("입력값이 숫자가 아닙니다ㅣ."),
+    NOT_NUMBER_STRING("입력값이 숫자가 아닙니다."),
     NULL_OF_NOT_NULL("null 값이 존재합니다.");
 
     private final String message;
