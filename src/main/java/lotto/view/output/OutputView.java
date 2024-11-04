032abb60bb995176df84aca855fbf54907cf1329
sung <salamander011017@gmail.com>
2024-11-04 14:58:12 +0900

    feat : 결과를 사용자에게 보여주는 OutputView 클래스 구현 - printPurchaseCount() : 몇개를 구매했는지 출력 - printLotto() : 임의로 생성한 로또 번호 출력 - printResult() : 당첨 통계 출력 - printRankResult() : 일치하는 등급을 확인 - printProfitRate() : 수익률 출력 - printError() : 에러메세지 반환

---
+++ src/main/java/lotto/view/output/OutputView.java
@@ -0,0 +1,44 @@
+package lotto.view.output;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.config.LottoRank;
+import lotto.model.domain.Lotto;
+import lotto.model.domain.LottoResult;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printPurchaseCount(int count) {
+        System.out.printf("\n%d개를 구매했습니다.\n", count);
+    }
+
+    public static void printLotto(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+        System.out.println(numbers.toString());
+    }
+
+    public static void printResult(LottoResult result) {
+        System.out.println("\n당첨 통계\n---");
+        Arrays.stream(LottoRank.values())
+                .filter(rank -> rank != LottoRank.NONE)
+                .forEach(rank -> printRankResult(rank, result.getRankCounts().get(rank)));
+    }
+
+    private static void printRankResult(LottoRank rank, int count) {
+        System.out.printf("%d개 일치 (%,d원) - %d개\n",
+                rank.getMatchCount(),
+                rank.getPrize(),
+                count);
+    }
+
+    public static void printProfitRate(double profitRate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitRate);
+    }
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+}
