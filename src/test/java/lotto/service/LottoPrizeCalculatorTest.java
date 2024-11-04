7947fcbfb24d904415fa8b154a9d6701dbdf3bd4
kim jihyeon <jihyeon546@gmail.com>
2024-11-04 16:35:05 +0900

    test: 당첨 계산 기능 테스트 수정

--- src/test/java/lotto/service/LottoPrizeCalculatorTest.java
+++ src/test/java/lotto/service/LottoPrizeCalculatorTest.java
@@ -1,62 +1,73 @@
 package lotto.service;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.*;
-
+import lotto.model.Lotto;
+import lotto.model.Lottos;
+import java.util.List;
 public class LottoPrizeCalculatorTest {
-
     private LottoPrizeCalculator calculator;
-
     @BeforeEach
     public void setUp() {
         calculator = new LottoPrizeCalculator();
     }
 
+    @DisplayName("로또 1등 (6개 일치) 테스트")
     @Test
-    public void testFirstPrize() {
-        int matchedNumbers = 6;
-        boolean bonusMatched = false;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnFirstPrizeWhenSixNumbersMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(2_000_000_000, prize);
     }
 
+    @DisplayName("로또 2등 (5개 일치 + 보너스 번호 일치) 테스트")
     @Test
-    public void testSecondPrize() {
-        int matchedNumbers = 5;
-        boolean bonusMatched = true;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnSecondPrizeWhenFiveNumbersAndBonusMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(30_000_000, prize);
     }
 
+    @DisplayName("로또 3등 (5개 일치) 테스트")
     @Test
-    public void testThirdPrize() {
-        int matchedNumbers = 5;
-        boolean bonusMatched = false;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnThirdPrizeWhenFiveNumbersMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(1_500_000, prize);
     }
 
+    @DisplayName("로또 4등 (4개 일치) 테스트")
     @Test
-    public void testFourthPrize() {
-        int matchedNumbers = 4;
-        boolean bonusMatched = false;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnFourthPrizeWhenFourNumbersMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 3, 4, 9, 10))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(50_000, prize);
     }
 
+    @DisplayName("로또 5등 (3개 일치) 테스트")
     @Test
-    public void testFifthPrize() {
-        int matchedNumbers = 3;
-        boolean bonusMatched = false;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnFifthPrizeWhenThreeNumbersMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 3, 11, 12, 13))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(5_000, prize);
     }
 
+    @DisplayName("로또 당첨 없음 (2개 이하 일치) 테스트")
     @Test
-    public void testNoPrize() {
-        int matchedNumbers = 2;
-        boolean bonusMatched = false;
-        int prize = calculator.calculatePrize(matchedNumbers, bonusMatched);
+    public void shouldReturnNoPrizeWhenLessThanThreeNumbersMatch() {
+        Lottos lottos = new Lottos(List.of(new Lotto(List.of(1, 2, 10, 11, 12, 13))));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        int prize = calculator.calculatePrize(lottos, winningNumbers, bonusNumber);
         assertEquals(0, prize);
-    }}
+    }
+}
