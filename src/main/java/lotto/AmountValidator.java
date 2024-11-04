630fd0d741de919764e312f612b045d8892268df
Hujin0322 <hongyuj0322@naver.com>
2024-11-04 22:58:01 +0900

    feat(AmountValidator): 최대 금액 초과 시 예외 사항  처리 추가
    - 로또 최대 구입 금액은 1,000,000원으로 설정 후 초과 시 예외 사항 처리

--- src/main/java/lotto/AmountValidator.java
+++ src/main/java/lotto/AmountValidator.java
@@ -7,17 +7,15 @@ public class AmountValidator {
     private static final String MINIMUM_AMOUNT_ERROR = "[ERROR] 로또 구입 최소 금액은 1,000원입니다.";
     private static final String AMOUNT_UNIT_ERROR = "[ERROR] 1,000원 단위로 입력해주세요.";
     private static final String NEGATIVE_AMOUNT_ERROR = "[ERROR] 금액은 0 이상의 숫자여야 합니다.";
-
+    private static final String MAXIMUM_AMOUNT_ERROR = "[ERROR] 로또 구입 금액은 1,000,000원 이하여야 합니다.";
 
     public void validateInput(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
         }
-
         if (input.replace(",", "").isEmpty()) {
             throw new IllegalArgumentException(NOT_NUMBER_ERROR);
         }
-
         if (!(input.matches("\\d{1,3}(,\\d{3})*") || input.matches("\\d+"))) {
             throw new IllegalArgumentException(COMMA_FORMAT_ERROR);
         }
@@ -38,6 +36,9 @@ public class AmountValidator {
         }
         if (lottoAmount < 1000) {
             throw new IllegalArgumentException(MINIMUM_AMOUNT_ERROR);
+        }
+        if (lottoAmount > 1000000) {
+            throw new IllegalArgumentException(MAXIMUM_AMOUNT_ERROR);
         }
         if (lottoAmount % 1000 != 0) {
             throw new IllegalArgumentException(AMOUNT_UNIT_ERROR);
