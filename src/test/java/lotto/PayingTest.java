a475bb9cc22a47cdfc94172c24e17e37852ca29f
wjdgkrud1 <wjdgkrud1@naver.com>
2024-11-04 16:41:05 +0900

    1000원 단위가 아닌 값 입력 시 예외가 발생하는지 확인하는 테스트 코드 구현

--- src/test/java/lotto/PayingTest.java
+++ src/test/java/lotto/PayingTest.java
@@ -28,6 +28,8 @@ class PayingTest {
 				() -> paying.paymentExcception("abc"));
 		assertEquals("[ERROR] : 숫자만 입력해 주세요.", ex.getMessage());
 
-
+		// 1000원 단위가 아닌 값 입력 시 예외 발생 확인
+		ex = assertThrows(IllegalArgumentException.class, () -> paying.paymentExcception("1500"));
+		assertEquals("[ERROR] : 1000원 단위로 입력해주새요.", ex.getMessage());
 	}
 }
