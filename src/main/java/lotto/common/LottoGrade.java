00ff32ab2defbc7cfe211c192e7219571561859a
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 20:53:01 +0900

    feat: LottoGrade reason 추가 및 Message return 함수 작성

--- src/main/java/lotto/common/LottoGrade.java
+++ src/main/java/lotto/common/LottoGrade.java
@@ -1,17 +1,22 @@
 package lotto.common;
 
+import lotto.helper.CurrencyFormatter;
+
 public enum LottoGrade {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    NONE(0);
+    NONE(0, "해당 되는 경우 없음"),
+    FIFTH(5_000, "3개 일치"),
+    FOURTH(50_000, "4개 일치"),
+    THIRD(1_500_000, "5개 일치"),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(2_000_000_000, "6개 일치"),
+    ;
 
     private final int cost;
+    private final String reason;
 
-    LottoGrade(int cost) {
+    LottoGrade(int cost, String reason) {
         this.cost = cost;
+        this.reason = reason;
     }
 
     public static LottoGrade getGrade(int count, boolean bonus) {
@@ -31,5 +36,13 @@ public enum LottoGrade {
             return FIRST;
         }
         return NONE;
+    }
+
+    public String getMessage() {
+        return String.format("%s (%s원)", reason, CurrencyFormatter.format(cost));
+    }
+
+    public float calcAmount(int count) {
+        return (float) cost * count;
     }
 }
