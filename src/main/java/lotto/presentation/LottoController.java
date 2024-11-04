31a07cc4c666e19fa7e1e74ff0d3798584b48a11
imscow11253 <seaweed8759@naver.com>
2024-11-04 19:49:28 +0900

    feat : presentation layer LottoController 클래스 구현

---
+++ src/main/java/lotto/presentation/LottoController.java
@@ -0,0 +1,17 @@
+package lotto.presentation;
+
+import lotto.application.Service;
+
+public class LottoController {
+
+    private final Service service;
+
+    public LottoController(Service service) {
+        this.service = service;
+    }
+
+    public void run(){
+        service.buyLottoHandler();
+        service.matchLottoHandler();
+    }
+}
