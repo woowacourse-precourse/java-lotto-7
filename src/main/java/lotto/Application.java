28defdfd13f11c6ca619ebccdc715dc319f97757
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-11-04 22:10:26 +0900

    feat : Create a lottery rank to use an enum

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -144,6 +144,25 @@ public class Application {
         }
     }
 
+    public enum LottoRank {
+        FIRST_RANK(6, false, 2_000_000_000),
+        SECOND_RANK(5, true, 30_000_000),
+        THIRD_RANK(5, false, 1_500_000),
+        FOURTH_RANK(4, false, 50_000),
+        FIFTH_RANK(3, false, 5_000);
+
+        private final int matchCount;
+        private final boolean bonusMatch;
+        private final int prize;
+
+        LottoRank(int matchCount, boolean bonusMatch, int prize) {
+            this.matchCount = matchCount;
+            this.bonusMatch = bonusMatch;
+            this.prize = prize;
+        }
+    }
+
+
     public static Map<Integer, Integer> calculateAllLineMatches(List<Integer> winningNumbers, List<Lotto> lottoNumbers, int bonusNumber) {
         Map<Integer, Integer> matchCounts = new HashMap<>();
         matchCounts.put(3, 0);
