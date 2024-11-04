ae41d1b8e6f831d4f60cd20d796ecd12806a49c9
eeekeee <ekrtionl@gmail.com>
2024-11-04 12:53:29 +0900

    test: Money 예외 테스트 구현

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -15,4 +15,22 @@ class MoneyTest {
 
         assertThat(money.getValue()).isEqualTo(Integer.parseInt(input));
     }
+
+    @Test
+    @DisplayName("입력 금액이 숫자가 아닐 시 예외 발생")
+    void invalidMoneyInputException() {
+        String input = "1000a";
+
+        assertThatThrownBy(() -> new Money(input))
+                .isInstanceOf(NumberFormatException.class);
+    }
+
+    @Test
+    @DisplayName("입력 금액이 1000으로 나누어 떨어지지 않을 시 예외 발생")
+    void notDivisibleByThousandException() {
+        String input = "1500";
+
+        assertThatThrownBy(() -> new Money(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
