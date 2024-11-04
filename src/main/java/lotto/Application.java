8e568c68396c5df7973cfd5331f4010c832f2835
onesay17 <onesay17@gmail.com>
2024-11-04 12:47:52 +0900

    feat: 로또 발매기 전체 동작 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.service.RandomLottoNumberGenerator;
+import lotto.validation.Validator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Validator validator = new Validator();
+        RandomLottoNumberGenerator randomLottoNumberGenerator = new RandomLottoNumberGenerator();
+        LottoService lottoService = new LottoService(randomLottoNumberGenerator);
+
+        LottoController lottoController = new LottoController(lottoService, validator);
+        lottoController.run();
     }
 }
