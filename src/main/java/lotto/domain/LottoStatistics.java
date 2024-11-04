91e4a82c2f12844a4c27545b0ce1cd30adfe5d64
threepebbles <threepebbles@naver.com>
2024-11-04 16:31:28 +0900

    feat(LottoStatistics): 당첨 통계 계산 기능 추가

---
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.view.LottoStatisticsDto;
+
+public class LottoStatistics {
+    private final Map<LottoPrize, Integer> prizeCount;
+    private final double rateOfReturn;
+
+    public LottoStatistics(Map<LottoPrize, Integer> prizeCount, double rateOfReturn) {
+        this.prizeCount = prizeCount;
+        this.rateOfReturn = rateOfReturn;
+    }
+
+    public static LottoStatisticsDto createLottoStatisticsDto(LottoStatistics lottoStatistics) {
+        return new LottoStatisticsDto(lottoStatistics.getPrizeCount(), lottoStatistics.getRateOfReturn());
+    }
+
+    public static LottoStatistics calcStatistics(List<Lotto> purchasedLottos, Lotto WinningLotto, Integer bonusNumber) {
+        Map<LottoPrize, Integer> prizeCount = new HashMap<>();
+        long sumOfPrizeMoney = 0;
+        for (Lotto purchasedLotto : purchasedLottos) {
+            int matchCount = calcMatchCount(purchasedLotto, WinningLotto);
+            boolean isBonusNumberMatched = purchasedLotto.contains(bonusNumber);
+            LottoPrize prize = LottoPrize.valueOf(matchCount, isBonusNumberMatched);
+            addPrize(prizeCount, prize);
+            sumOfPrizeMoney += prize.getMoney();
+        }
+        double rateOfReturn =
+                (double) sumOfPrizeMoney / (purchasedLottos.size() * LottoMachine.LOTTO_UNIT_PRICE) * 100.0;
+        return new LottoStatistics(prizeCount, rateOfReturn);
+    }
+
+    private static void addPrize(Map<LottoPrize, Integer> prizeCount, LottoPrize prize) {
+        if (prize == LottoPrize.NOTHING) {
+            return;
+        }
+
+        if (prizeCount.get(prize) == null) {
+            prizeCount.put(prize, 1);
+            return;
+        }
+        prizeCount.put(prize, prizeCount.get(prize) + 1);
+    }
+
+    public static int calcMatchCount(Lotto purchasedLotto, Lotto winningLotto) {
+        return (int) winningLotto.getNumbers().stream()
+                .filter(purchasedLotto.getNumbers()::contains)
+                .count();
+    }
+
+    public Map<LottoPrize, Integer> getPrizeCount() {
+        return prizeCount;
+    }
+
+    public double getRateOfReturn() {
+        return rateOfReturn;
+    }
+}
