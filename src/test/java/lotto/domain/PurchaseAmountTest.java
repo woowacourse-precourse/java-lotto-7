ec5e97e9940ec82fa498504045fad0006afe6d88
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 21:54:47 +0900

    feat: 구매 금액 테스트 구현

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import lotto.domain.lotto.WinningLottoWithBonus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static lotto.exception.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("구매 금액 테스트")
+class PurchaseAmountTest {
+    @DisplayName("구매 금액이 1000원 미만일 경우 예외를 발생시킨다.")
+    @Test
+    void 구매_금액이_1000원_미만일_경우_예외를_발생시킨다() {
+        // given
+        int amount = 999;
+
+        // when, then
+        assertThatThrownBy(() -> new PurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PURCHASE_AMOUNT_NOT_ENOUGH.getMessage());
+    }
+
+    @DisplayName("구매 금액이 1000원으로 나누어 떨어지지 않을 경우 예외를 발생시킨다.")
+    @Test
+    void 구매_금액이_1000원으로_나누어_떨어지지_않을_경우_예외를_발생시킨다() {
+        // given
+        int amount = 1500;
+
+        // when, then
+        assertThatThrownBy(() -> new PurchaseAmount(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(PURCHASE_AMOUNT_NOT_DIVISIBLE.getMessage());
+    }
+
+    @DisplayName("총 수익이 주어질 떄 수익률을 계산한다.")
+    @Test
+    void 총_수익이_주어질_떄_수익률을_계산한다() {
+        // given
+        int amount = 10000;
+        PurchaseAmount purchaseAmount = new PurchaseAmount(amount);
+        int totalProfit = 20000;
+
+        // when
+        double profitRate = purchaseAmount.calculateProfitRate(totalProfit);
+
+        // then
+        assertEquals(profitRate, 200);
+    }
+}
