04220a10cbb11646b9450cdc642297d3b1ed97f2
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 18:33:27 +0900

    fix: 정렬 예외 문제 해결

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,8 +9,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         Validator.validateLottoNumbers(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     public List<Integer> getNumbers() {
