e3d238776ed835fc97ccf84300cb51f069e7d86c
yushinc <cys74351@gmail.com>
2024-11-04 20:53:44 +0900

    feat(Application): GameController의 run() 메서드 호출 및 파라미터 객체 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,14 @@
 package lotto;
 
 import lotto.controller.GameController;
+import lotto.service.GameService;
+import lotto.util.LottoGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.run();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        GameService service = new GameService(lottoGenerator);
+        GameController controller = new GameController(service);
+        controller.run();
     }
 }
