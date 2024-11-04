557e0e18fb8f748afe21c716dd27466b40e5f639
dongwoo <woomacho@gmail.com>
2024-11-04 18:48:27 +0900

    feat: 로또 당첨번호, 보너스 번호, 판매 가격 저장소 구현
    - 로또 번호와, 당첨 번호 둘 다 설정되고 조회가 가능하다.
    - 당첨 번호와 보너스 번호 검증을 수행한다.

---
+++ src/main/java/lotto/util/WinningLottoStore.java
@@ -0,0 +1,54 @@
+package lotto.util;
+
+import lotto.Lotto;
+
+public class WinningLottoStore {
+
+    private static Lotto winningLotto;
+    private static int bonusNumber;
+    private static final int LOTTO_PRICE = 1000;
+    private static boolean isInitialized = false;
+
+    private WinningLottoStore() {
+    }
+
+    public static Lotto getWinningLotto() {
+        checkInitialization();
+        return winningLotto;
+    }
+
+    public static int getBonusNumber() {
+        checkInitialization();
+        return bonusNumber;
+    }
+
+    public static int getLottoPrice() {
+        return LOTTO_PRICE;
+    }
+
+    public static void setUpLottoStore(Lotto lotto, int bonusNum) {
+        validateLotto(lotto);
+        validateLottoNumber(bonusNum);
+        winningLotto = lotto;
+        bonusNumber = bonusNum;
+        isInitialized = true;
+    }
+
+    private static void validateLotto(Lotto lotto) {
+        if (lotto == null) {
+            throw new IllegalArgumentException("[ERROR] 유효한 로또가 아닙니다.");
+        }
+    }
+
+    private static void validateLottoNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 유효한 로또 번호가 아닙니다.");
+        }
+    }
+
+    private static void checkInitialization() {
+        if (!isInitialized) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 등록되지 않았습니다.");
+        }
+    }
+}
