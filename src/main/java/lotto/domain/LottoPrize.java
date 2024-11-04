3ee5e3543a06decf233408913870706b223e63b9
ParkSangsin <pss6039@naver.com>
2024-11-04 22:34:47 +0900

    refactor: int -> Integer 수정

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -11,28 +11,28 @@ public enum LottoPrize {
     SECOND(30_000_000, 5, (matchCount, isContainBonusNumber) -> matchCount == 5 && isContainBonusNumber),
     FIRST(2_000_000_000, 6, (matchCount, isContainBonusNumber) -> matchCount == 6);
 
-    private final int reward;
-    private final int matchCount;
+    private final Integer reward;
+    private final Integer matchCount;
     private final BiPredicate<Integer, Boolean> isMatch;
 
-    LottoPrize(final int reward, final int matchCount, final BiPredicate<Integer, Boolean> isMatch) {
+    LottoPrize(final Integer reward, final Integer matchCount, final BiPredicate<Integer, Boolean> isMatch) {
         this.reward = reward;
         this.matchCount = matchCount;
         this.isMatch = isMatch;
     }
 
-    public static LottoPrize getLottoPrize(final int matchCount, final boolean isContainBonusNumber) {
+    public static LottoPrize getLottoPrize(final Integer matchCount, final boolean isContainBonusNumber) {
         return Arrays.stream(LottoPrize.values())
                 .filter(lottoPrize -> lottoPrize.isMatch.test(matchCount, isContainBonusNumber))
                 .findAny()
                 .orElse(EMPTY);
     }
 
-    public int getReward() {
+    public Integer getReward() {
         return reward;
     }
 
-    public int getMatchCount() {
+    public Integer getMatchCount() {
         return matchCount;
     }
 }
