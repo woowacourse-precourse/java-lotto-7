8c28dbb698675b27bd1d981fe05147c5b01d0cc5
mirinae700 <mirinae219@gmail.com>
2024-11-04 15:14:10 +0900

    refactor(lottocreate) : 구매금액 1000원 미만 입력 시 예외발생 될 수 있도록 조건 수정

--- src/main/java/lotto/LottoCreate.java
+++ src/main/java/lotto/LottoCreate.java
@@ -26,7 +26,7 @@ public class LottoCreate {
     private int validate(String input) {
         try {
             int amount = Integer.parseInt(input);
-            if(amount % 1000 > 0) {
+            if(amount < 1000 || amount % 1000 > 0) {
                 throw new IllegalArgumentException("[ERROR] 구입금액은 1,000 단위로 입력하셔야 합니다.");
             }
             return amount;
