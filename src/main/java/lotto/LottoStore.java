150659d183c35cd8ccbcbe02b7503dad87b32817
GaBaljaintheroom <junsu1222@naver.com>
2024-11-04 13:58:46 +0900

    refactor(LottoStore): open 메서드 분리

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -8,14 +8,29 @@ public class LottoStore {
     private final StatisticalMachine statisticalMachine = new StatisticalMachine();
 
     public void open() {
+        Cash cash = purchaseLottoByCash();
+        
+        WinningLotto winningLotto = createWinningLotto();
+
+        showStatistics(winningLotto, cash);
+    }
+
+    private Cash purchaseLottoByCash() {
         Cash cash = inputHandler.inputCash();
         lottoMachine.purchase(cash);
         outputView.printLottoTickets(lottoMachine);
+        
+        return cash;
+    }
 
+    private WinningLotto createWinningLotto() {
         Lotto lotto = inputHandler.inputWinningNumbers();
         BonusNumber bonusNumber = inputHandler.inputBonusNumber(lotto);
-        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        
+        return new WinningLotto(lotto, bonusNumber);
+    }
 
+    private void showStatistics(WinningLotto winningLotto, Cash cash) {
         statisticalMachine.calculate(lottoMachine.currentLottoTickets(), winningLotto);
         outputView.printWinningStatistics(statisticalMachine);
         outputView.printTotalProfit(statisticalMachine.getTotalProfit(cash));
