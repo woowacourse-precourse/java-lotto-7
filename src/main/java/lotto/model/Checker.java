cf90cc9e6b034c299537c278987184efcf8af1f5
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 19:58:56 +0900

    Refactor(Checker): enum class 생성에 따른 리팩토링
    - 클래스가 가지고 있는 상수 필드를 모두 삭제했습니다.
    - 동일한 숫자가 있는지 확인하고 이를 계산하는데 집중하도록 만들었습니다.
    - 상수 및 랭크 메기는 메서드 모두 enum class를 통해 진행하도록 수정했습니다.

--- src/main/java/lotto/model/Checker.java
+++ src/main/java/lotto/model/Checker.java
@@ -1,88 +1,29 @@
 package lotto.model;
 
+import lotto.constant.LottoRank;
+
 public class Checker {
-    private Lotto winningNumbers;
-    private int bonusNumber;
-    private static final int NO_WINNINGS_MONEY = 0;
-    private static final int FIFTH_WINNINGS_MONEY = 5000;
-    private static final int FOURTH_WINNINGS_MONEY = 50000;
-    private static final int THIRD_WINNINGS_MONEY = 1500000;
-    private static final int SECOND_WINNINGS_MONEY = 30000000;
-    private static final int FIRST_WINNINGS_MONEY = 2000000000;
-    private static final int MIN_GET_WINNINGS_COUNT = 3;
-    private static final int MAX_GET_WINNINGS_COUNT = 6;
+    private final WinningNumbers winningNumbers;
 
-
-    public Checker(Lotto winningNumbers, int bonusNumber) {
+    public Checker(WinningNumbers winningNumbers) {
         this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
     }
 
     public LottoResult check(Lotto lotto) {
-        int winningCount = calculateWinningCount(lotto);
-        int bonusCount = calculateBonusCount(lotto);
+        int matchCount = calculateWinningCount(lotto);
+        boolean isMatchBonusCount = hasBonusMatch(lotto);
 
-        if (!isGetWinnigsMoney(winningCount)) {
-            return new LottoResult("0", NO_WINNINGS_MONEY);
-        }
-
-        String winningRank = getWinningRank(winningCount, bonusCount);
-        int winningsMoney = getWinningsMoney(winningRank);
-
-        return new LottoResult(winningRank, winningsMoney);
+        LottoRank rank = LottoRank.findRank(matchCount, isMatchBonusCount);
+        return new LottoResult(rank);
     }
 
     private int calculateWinningCount(Lotto lotto) {
         return (int) lotto.getLottoNumbers().stream()
-                .filter(number -> winningNumbers.getLottoNumbers().contains(number))
+                .filter(winningNumbers.getWinningLottoNumber().getLottoNumbers()::contains)
                 .count();
     }
 
-    private int calculateBonusCount(Lotto lotto) {
-        return (int) lotto.getLottoNumbers().stream()
-                .filter(number -> number == bonusNumber)
-                .count();
-    }
-
-    private boolean isGetWinnigsMoney(int winningCount) {
-        return winningCount >= MIN_GET_WINNINGS_COUNT;
-    }
-
-    private String getWinningRank(int winningCount, int bonusCount) {
-        if (winningCount + bonusCount == MAX_GET_WINNINGS_COUNT) {
-            return getHighRank(bonusCount);
-        }
-        if (winningCount > MIN_GET_WINNINGS_COUNT) {
-            if (winningCount == 5) {
-                return "3";
-            }
-            if (winningCount == 4) {
-                return "4";
-            }
-        }
-        return "5";
-    }
-
-    private String getHighRank(int bonusCount) {
-        if (bonusCount != 1) {
-            return "1";
-        }
-        return "2";
-    }
-
-    private int getWinningsMoney(String winningRank) {
-        if (winningRank.equals("1")) {
-            return FIRST_WINNINGS_MONEY;
-        }
-        if (winningRank.equals("2")) {
-            return SECOND_WINNINGS_MONEY;
-        }
-        if (winningRank.equals("3")) {
-            return THIRD_WINNINGS_MONEY;
-        }
-        if (winningRank.equals("4")) {
-            return FOURTH_WINNINGS_MONEY;
-        }
-        return FIFTH_WINNINGS_MONEY;
+    private boolean hasBonusMatch(Lotto lotto) {
+        return lotto.getLottoNumbers().contains(winningNumbers.getBonusLottoNumber());
     }
 }
