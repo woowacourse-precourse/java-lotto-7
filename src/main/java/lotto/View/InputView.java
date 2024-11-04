9c86dad23412e22eec3ca99233972282518d4993
Youn Sang Hyun <younsang523@gmail.com>
2024-11-04 18:51:48 +0900

    feat: view 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -1,9 +1,21 @@
 package lotto.View;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    public void requestLottoNumber() {
-        System.out.println("로또 번호를 생성합니다.");
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
     }
 }
-
--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -3,9 +3,27 @@ package lotto.View;
 import java.util.List;
 
 public class OutputView {
+    public void printPurchaseResult(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+
     public void printLottoNumbers(List<Integer> numbers) {
-        System.out.println("생성된 로또 번호");
         System.out.println(numbers);
     }
-}
 
+    public void printStatisticsTitle() {
+        System.out.println("\n당첨 통계\n---");
+    }
+
+    public void printMatchResult(String message, int count) {
+        System.out.println(message + " - " + count + "개");
+    }
+
+    public void printProfitRate(double rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", rate);
+    }
+
+    public void printError(String message) {
+        System.out.println(message);
+    }
+}
