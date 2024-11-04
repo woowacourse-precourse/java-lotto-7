73a6368c1a750bcaa36ba2132603260dd3a8afd0
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 13:52:04 +0900

    feat(IOMessage): 로또 당첨 통계 출력 메시지 수정
    - 하나의 메시지를 사용하려 했으나, 포매팅 하는 방식으로 출력시에 메서드의 길이가 너무 길어졌기 때문에 개별 메시지로 분리
    - 로또의 수익률은 소수점 둘째 자리에서 반올림할 수 있도록 %.1f로 포매팅

--- src/main/java/lotto/Util/Constant/IOMessage.java
+++ src/main/java/lotto/Util/Constant/IOMessage.java
@@ -5,8 +5,13 @@ public enum IOMessage {
     OUTPUT_LOTTO_COUNT("%d개를 구매했습니다."),
     INPUT_WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
-    OUTPUT_PRIZE_STATISTICS("당첨 통계\n---"),
-    OUTPUT_INDIVIDUAL_PRIZE_STATISTICS("%d개 일치 (%s원) - %d개"),
+    OUTPUT_PRIZE_STATISTICS("\n당첨 통계\n---"),
+    OUTPUT_PRIZE_STATISTICS_MATCHED_3("3개 일치 (5,000원) - %d개"),
+    OUTPUT_PRIZE_STATISTICS_MATCHED_4("4개 일치 (50,000원) - %d개"),
+    OUTPUT_PRIZE_STATISTICS_MATCHED_5("5개 일치 (1,500,000원) - %d개"),
+    OUTPUT_PRIZE_STATISTICS_MATCHED_5_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    OUTPUT_PRIZE_STATISTICS_MATCHED_6("6개 일치 (2,000,000,000원) - %d개"),
+
     OUTPUT_TOTAL_PROFIT("총 수익률은 %.1f%%입니다.");
 
     private final String message;
