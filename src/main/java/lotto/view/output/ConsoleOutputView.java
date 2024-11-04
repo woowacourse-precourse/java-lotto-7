2c1879827fbd9d1aa22a23002de1684cfcf6b672
haebyun <bhk4392@gmail.com>
2024-11-04 15:18:49 +0900

    feat(ConsoleOutputView): 당첨 결과 및 수익률 출력 기능 추가
    - printWinningResults 메서드 추가하여 당첨 결과와 수익률을 출력하도록 구현
    - OutputFormatter를 사용해 당첨 통계와 수익률 메시지를 형식에 맞추도록 구현
    - 당첨 결과와 수익률 메시지를 결합하여 출력

--- src/main/java/lotto/view/output/ConsoleOutputView.java
+++ src/main/java/lotto/view/output/ConsoleOutputView.java
@@ -1,6 +1,7 @@
 package lotto.view.output;
 
 import lotto.dto.response.PurchasedLottosDTO;
+import lotto.dto.response.WinningResultsDTO;
 import lotto.view.output.util.OutputFormatter;
 
 public class ConsoleOutputView implements OutputView {
@@ -18,6 +19,20 @@ public class ConsoleOutputView implements OutputView {
                 purchasedLottosDTO.purchasedLottoDTOs()
         );
         String output = purchaseCountMessage + lottoNumbersMessage;
+
+        System.out.println(output);
+    }
+
+    @Override
+    public void printWinningResults(WinningResultsDTO winningResultsDTO) {
+        String resultPrefixMessage = OutputFormatter.formatResultPrefix();
+        String winningResultsMessage = OutputFormatter.formatWinningResults(
+                winningResultsDTO.winningResultDTOs()
+        );
+        String profitRateMessage = OutputFormatter.formatProfitRate(
+                winningResultsDTO.profitRate()
+        );
+        String output = resultPrefixMessage + winningResultsMessage + profitRateMessage;
 
         System.out.println(output);
     }
