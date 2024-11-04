14b967ae6b8c6e96832938ade3f99af1c679f1b1
waffle <nate.crema@gmail.com>
2024-11-04 18:29:22 +0900

    feat(LottoRound): 로또 발행메서드 추가

--- src/main/java/lotto/domain/LottoRound.java
+++ src/main/java/lotto/domain/LottoRound.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.LottoConstant;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,32 @@ public class LottoRound {
         return this.lottos;
     }
 
-    public void addLotto(Lotto lotto) {
+    /**
+     * 지정된 수량만큼 로또를 발행합니다
+     *
+     * @param count 발행할 로또 수
+     */
+    public void issueLottoes(int count) {
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(createLottoNumber());
+            addLotto(lotto);
+        }
+    }
+
+    /**
+     * Randoms 라이브러리를 사용하여 랜덤한 로또번호를 뽑아 가져옵니다.
+     *
+     * @return 길이가 6인 로또번호를 담은 정수리스트
+     */
+    private List<Integer> createLottoNumber() {
+        return Randoms.pickUniqueNumbersInRange(
+                LottoConstant.MIN_LOTTO_NUMBER,
+                LottoConstant.MAX_LOTTO_NUMBER,
+                LottoConstant.LOTTO_DIGITS
+        );
+    }
+
+    private void addLotto(Lotto lotto) {
         lottos.add(lotto);
     }
 }
