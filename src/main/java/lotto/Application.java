40627018426627a0813e280ef1e812ad179efb22
AndBlack99 <jwoo1687@icloud.com>
2024-11-04 21:28:07 +0900

    feat:로또 구입 금액 입력받기
      - getPurchaseAmount 함수
      - validateAmount 함수
      - main 함수
      - 콘솔입력을 통해 구입 급액을 받고 유효한 숫자인지,0원 이상의 1,000원 단위의 금액인지 확인합니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,45 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+	private static final int LOTTO_PRICE = 1000;
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	while(true)
+	    	try {
+		    	int amount = getPurchaseAmount();
+		        break;
+	    	} catch (IllegalArgumentException e) {
+	    		System.out.println("[ERROR] " + e.getMessage());
+	    	}
     }
+    
+    
+    
+    
+    public static int getPurchaseAmount() {
+		while (true) {
+            System.out.println("로또 구입 금액을 입력하세요");
+            String input = Console.readLine();
+            try {
+                int amount = Integer.parseInt(input);
+                validateAmount(amount);
+                return amount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효한 숫자를 입력하세요.");
+            }
+        }
+	}
+	
+	public static void validateAmount(int amount) {
+		if (amount <= 0 || amount % LOTTO_PRICE != 0) 
+			throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위 입니다.");
+	}
+    
+    
+    
+    
+    
+    
 }
