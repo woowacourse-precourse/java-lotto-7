7f36f0d0ff485b7df38dd682a4416ecbab0a61b9
badgelatte <saucy5939@gmail.com>
2024-11-04 21:13:37 +0900

    로또 갯수 출력 부분 수정
    예시와 다른 점을 찾아 같도록 수정함

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -33,7 +33,7 @@ public class Customer {
     }
 
     public void printLottoInfo(int count) {
-        System.out.println(count + "를 구매했습니다.");
+        System.out.println(count + "개를 구매했습니다.");
 
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
