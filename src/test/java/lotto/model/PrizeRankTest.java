498ec47ed3ade3366dfcc11b9417d7d0ad90e8e4
choi <nippert@naver.com>
2024-11-04 12:48:54 +0900

    test: 등수 도메인 테스트 작성

---
+++ src/test/java/lotto/model/PrizeRankTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+public class PrizeRankTest {
+
+    private static Stream<org.junit.jupiter.params.provider.Arguments> prizeRankParameters() {
+        return Stream.of(
+                of(6, false, PrizeRank.FIRST),
+                of(5, true, PrizeRank.SECOND),
+                of(5, false, PrizeRank.THIRD),
+                of(4, false, PrizeRank.FOURTH),
+                of(3, false, PrizeRank.FIFTH)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("일치하는 번호의 개수와 보너스 번호의 일치 여부로 등수를 계산한다")
+    @MethodSource("prizeRankParameters")
+    public void 등수_계산(int matchCount, boolean bonusMatch, PrizeRank expectedRank) {
+        PrizeRank prizeRank = PrizeRank.of(matchCount, bonusMatch);
+        assertThat(prizeRank).isEqualTo(expectedRank);
+    }
+}
