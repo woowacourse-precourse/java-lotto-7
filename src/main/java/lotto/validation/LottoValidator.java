3480cce608c77364c1e12bbca569198a8388721a
imscow11253 <seaweed8759@naver.com>
2024-11-04 13:56:26 +0900

    feat : 로또에 중복된 값이 있을시 예외 발생

--- src/main/java/lotto/validation/LottoValidator.java
+++ src/main/java/lotto/validation/LottoValidator.java
@@ -1,5 +1,6 @@
 package lotto.validation;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static lotto.common.constant.ErrorMessage.*;
@@ -10,11 +11,18 @@ public class LottoValidator {
 
     public static void validateLotto(List<Integer> numbers){
         throwExceptionIfListSizeIsInvalid(numbers);
+        throwExceptionIfThereIsDuplicateNumber(numbers);
     }
 
     private static void throwExceptionIfListSizeIsInvalid(List<Integer> numbers) {
         if (numbers.size() != lottoNumberSize) {
             throw new IllegalArgumentException(LOTTO_SHOULD_BE_SIX.getErrorMessage());
+        }
+    }
+
+    private static void throwExceptionIfThereIsDuplicateNumber(List<Integer> numbers) {
+        if(numbers.size() != new HashSet<>(numbers).size()){
+            throw new IllegalArgumentException(THERE_IS_DUPLICATE_NUMBER_IN_LOTTO.getErrorMessage());
         }
     }
 }
