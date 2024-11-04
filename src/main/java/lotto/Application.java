99e240428821442eeebb204cd3c3fa9731887694
023-dev <0_2_3@naver.com>
2024-11-04 17:13:57 +0900

    feat(Application): 시작점 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.console.ConsoleInput;
+import lotto.console.ConsoleOutput;
+import lotto.controller.LottoController;
+import lotto.service.LottoValidationService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleInput input = new ConsoleInput();
+        ConsoleOutput output = new ConsoleOutput();
+        LottoValidationService lottoValidationService = new LottoValidationService();
+        LottoController lottoController = new LottoController(input, output, lottoValidationService);
+        lottoController.run();
     }
 }
