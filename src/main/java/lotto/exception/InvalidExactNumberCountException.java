9fb05254ca26f700495ba28ba8c5e7815ea4a18f
waffle <nate.crema@gmail.com>
2024-11-04 14:30:04 +0900

    refactor(InvalidExactNumberCountException): 입력 예외객체 삭제

--- src/main/java/lotto/exception/InvalidExactNumberCountException.java
+++
@@ -1,9 +0,0 @@
-package lotto.exception;
-
-import lotto.constant.LottoErrorMessage;
-
-public class InvalidExactNumberCountException extends IllegalArgumentException {
-    public InvalidExactNumberCountException() {
-        super(LottoErrorMessage.LOTTO_WIN_NUMBER_COUNT_ERROR);
-    }
-}
