7c21485966b2f2d5a7b8618f6bf80938a90ff7d1
AndBlack99 <jwoo1687@icloud.com>
2024-11-04 22:34:54 +0900

    feat:로또 당첨 확인
      - analyzeResults함수
      - getMatchCount함수
      - updatePrizeCount함수
      - intcrementPrizeCount함수
      - printPrizeResults함수
      - printPrizeCount함수
      - printProfitRate함수
      - calculatreTotalEarnings함수
       : 로또 당첨 내역 확인 후 출력 / 잘못된 입,출력은 IllegalArgumentException 오류 발생 및 오류메시지 출력 후 재입,출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,12 +5,37 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 import java.util.Set;
+
 import java.util.Collections;
 import java.util.HashSet;
 
 public class Application {
 	private static final int LOTTO_PRICE = 1000;
 	private static final int MAX_NUMBER = 45;
+	
+	public enum Prize {
+		FIRST(200000000, "6개 일치"),
+		SECOND(3000000, "5개 일치 + 보너스 번호 일치"),
+		THIRD(1500000, "5개 일치"),
+		FOURTH(50000, "4개 일치"),
+		FIFTH(5000, "3개 일치");
+	
+		private final int amount;
+		private final String description;
+	
+		Prize(int amount, String description) {
+			this.amount = amount;
+			this.description = description;
+		}
+	
+		public int getAmount() {
+			return amount;
+		}
+	
+		public String getDescription() {
+			return description;
+		}
+	}
 	
     public static void main(String[] args) {
     	while(true)
@@ -119,8 +144,82 @@ public class Application {
 			throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 1~45 사이 입니다.");
 	}
     
+	public static void analyzeResults(Lotto[] purchasedLottos, Set<Integer> winningNumbers, int bonusNumber) {
+		int[] prizeCounts = new int[5];
+		for (Lotto lotto : purchasedLottos) {
+			int matchCount = getMatchCount(lotto, winningNumbers);
+			boolean bonusMatched = lotto.getNumbers().contains(bonusNumber);
+			updatePrizeCounts(prizeCounts, matchCount, bonusMatched);
+		}
+		printPrizeResults(prizeCounts);
+		printProfitRate(purchasedLottos, prizeCounts);
+	}
+	
+	public static int getMatchCount(Lotto lotto, Set<Integer> winningNumbers) {
+		int count = 0;
+		for(Integer number : lotto.getNumbers()) {
+			if(winningNumbers.contains(number)) 
+				count++;
+		}
+		return count;
+	}
+	
+	public static void updatePrizeCounts(int[] prizeCounts, int matchCount, boolean bonusMatched) {
+		if(matchCount == 6) {
+			incrementPrizeCount(prizeCounts, 0);
+			return ;
+		}
+		 if (matchCount == 5) {
+	        if (bonusMatched) {
+	        	incrementPrizeCount(prizeCounts, 1);
+	            return;
+	        }
+	        incrementPrizeCount(prizeCounts, 2);
+	     	return;
+		 }
+		 if (matchCount == 4) {
+			 incrementPrizeCount(prizeCounts, 3);
+	         return;
+	     }
+	     if (matchCount == 3) {
+	         incrementPrizeCount(prizeCounts, 4);
+	     }
+	}
+	
+	public static void incrementPrizeCount(int[] prizeCounts, int index) {
+		prizeCounts[index]++;
+	}
     
-    
-    
+	public static void printPrizeResults(int[] prizeCounts) {
+		System.out.println("당첨 통계");
+		System.out.println("---");
+		printPrizeCount(Prize.FIFTH, prizeCounts[4]);
+        printPrizeCount(Prize.FOURTH, prizeCounts[3]);
+        printPrizeCount(Prize.THIRD, prizeCounts[2]);
+        printPrizeCount(Prize.SECOND, prizeCounts[1]);
+        printPrizeCount(Prize.FIRST, prizeCounts[0]);
+	}
+	
+	public static void printPrizeCount(Prize prize, int count) {
+		System.out.printf("%s (%d원) - %d개%n", prize.getDescription(), prize.getAmount(),count);
+	}
+	
+	public static void printProfitRate(Lotto[] purchasedLottos, int[] prizeCounts) {
+		int totalEarnings = calculateTotalEarnings(prizeCounts);
+		int totalSpent = purchasedLottos.length * LOTTO_PRICE;
+	    double profitRate = (double) totalEarnings / totalSpent * 100;
+
+	    System.out.printf("총 수익률: %.1f%%입니다.%n", profitRate);
+	}
+	
+	private static int calculateTotalEarnings(int[] prizeCounts) {
+		return 	prizeCounts[0] * Prize.FIRST.getAmount() +
+				prizeCounts[1] * Prize.SECOND.getAmount() +
+	            prizeCounts[2] * Prize.THIRD.getAmount() +
+	            prizeCounts[3] * Prize.FOURTH.getAmount() +
+	            prizeCounts[4] * Prize.FIFTH.getAmount();
+	}
+ 
+}
     
 }
