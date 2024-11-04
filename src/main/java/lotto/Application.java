f398659f3d747888e4707a2cb67b464ed5e0eb76
minjcho <117441212+minjcho@users.noreply.github.com>
2024-11-04 22:08:48 +0900

    feat: 로또 발행 및 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.InputView;
+import lotto.ResultView;
 import lotto.Lotto;
 import lotto.LottoGenerator;
 import java.util.List;
@@ -11,6 +12,7 @@ public class Application {
             int purchaseAmount = InputView.readPurchaseAmount();
             int lottoCount = purchaseAmount / 1000;
             List<Lotto> lottos = LottoGenerator.generateLottos(lottoCount);
+            ResultView.printLottos(lottos);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
---
+++ src/main/java/lotto/ResultView.java
@@ -0,0 +1,16 @@
+// ResultView.java
+package lotto;
+
+import lotto.Lotto;
+import java.util.List;
+
+public class ResultView {
+
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+        System.out.println();
+    }
+}
