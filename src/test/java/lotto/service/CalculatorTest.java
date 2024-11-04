bba1b7f6c799eb74cab67b92d521d2ad4c3031fd
이준범 <jach1206@naver.com>
2024-11-04 23:02:07 +0900

    test: 수익률 계산과 관련된 기능의 테스트 코드 작성
    - 발행한 로또 전체의 수익 합을 구하는 기능과, 소수점 둘째 자리에서 반올림 하는 기능,
    수익률 계산하는 기능의 테스트 코드를 작성했다.

--- src/test/java/lotto/service/CalculatorTest.java
+++ src/test/java/lotto/service/CalculatorTest.java
@@ -3,10 +3,12 @@ package lotto.service;
 import java.util.Arrays;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningInformation;
 import lotto.domain.WinningNumbers;
 import lotto.domain.WinningPrize;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -19,11 +21,18 @@ class CalculatorTest {
     private static final Lotto SECOND_LOTTO = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7));
     private static final Lotto FIRST_LOTTO = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
 
+    private static final PurchaseAmount CAN_BUY_FIVE_LOTTO = new PurchaseAmount("5000");
 
-    @BeforeEach
-    void setUp() throws Exception {
+
+    @BeforeAll
+    static void setUpBeforeAll() throws Exception {
+        // 당첨 번호를 입력한다.
         WinningNumbers.initialInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
         BonusNumber.initialInstance("7");
+
+        // 유저는 5등과 4등에 당첨되었다.
+        WinningInformation.getInstance().addWinningCount(WinningPrize.FIFTH);
+        WinningInformation.getInstance().addWinningCount(WinningPrize.FOURTH);
     }
 
     @Test
@@ -62,4 +71,20 @@ class CalculatorTest {
         int count = calculator.matchCount(FIRST_LOTTO);
         Assertions.assertThat(calculator.rank(count, FIRST_LOTTO)).isEqualTo(WinningPrize.FIRST);
     }
+
+    @Test
+    void 발행한_로또_전체의_수익_합을_구하는_기능_테스트() {
+        Assertions.assertThat(calculator.sumOfPrize()).isEqualTo(55000);
+    }
+
+    @Test
+    void 소수점_둘째_자리에서_반올림_하는_기능_테스트() {
+        Assertions.assertThat(calculator.roundOfTwo(55.56)).isEqualTo(55.6);
+    }
+
+    @Test
+    void 수익률_계산하는_기능_테스트() {
+        Assertions.assertThat(calculator.rateOfReturn(CAN_BUY_FIVE_LOTTO)).isEqualTo(1100.0);
+    }
+
 }
