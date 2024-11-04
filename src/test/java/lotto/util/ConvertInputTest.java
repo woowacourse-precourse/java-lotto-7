539f2fcf45e457c74d338eaa4839607a5bf694a1
JoonKyo <joonkyolee@gmail.com>
2024-11-04 19:38:31 +0900

    test(ConvertInputTest): 로또 번호 입력이 수로만 구성되면 예외가 발생하지 않는지 확인하는 테스트 구현

--- src/test/java/lotto/util/ConvertInputTest.java
+++ src/test/java/lotto/util/ConvertInputTest.java
@@ -3,9 +3,13 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ConvertInputTest {
     @Test
@@ -26,4 +30,17 @@ public class ConvertInputTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @MethodSource("generateLottoData")
+    void 입력한_문자열이_수로만_구성되면_예외가_발생하지_않는다(String input, List<Integer> expected) {
+        assertThat(ConvertInput.makeWinningNumberToList(input)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generateLottoData() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6,7", List.of(1, 2, 3, 4, 5, 6, 7)),
+                Arguments.of("1,45,2,9,0", List.of(1, 45, 2, 9, 0)),
+                Arguments.of("1,2,3,4,5,6", List.of(1, 2, 3, 4, 5, 6))
+        );
+    }
 }
