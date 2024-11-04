e249be1d5fca10d39f1f30761e7cb2c5ec19a5b7
dongyeopwoo <dongyeopwoo@naver.com>
2024-11-04 22:44:55 +0900

    feat(Separator): 출력 구분자 정의 추가 결과 및 당첨 번호 입력 시 필요한 구분자를 Enum으로 정의하여 중복을 제거.

--- src/main/java/content/Separator.java
+++ src/main/java/content/Separator.java
@@ -1,2 +1,17 @@
-package content;public enum Separator {
+package content;
+
+public enum Separator {
+    RESULT_SEPARATOR("---"),
+    WINNING_SEPARATOR(",");
+
+
+    private final String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    Separator(String message) {
+        this.message = message;
+    }
 }
