07b88417264f364e5b1ddca877ff70a2ffd5b18b
kimsongie12 <song3078@naver.com>
2024-11-04 13:53:19 +0900

    feat: 로또 티켓 생성 및 당첨 번호 비교 구매한 티켓 리스트 입력 금액에 따른 티켓 생성 당첨 결과 확인 후 총 수익 계산해서 반환

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,5 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
     //로또 생성, 당첨 번호와 비교 후 결과 계산
+    private final List<Lotto> tickets = new ArrayList<>();
+
+    public void purchaseTickets(int amount){
+        int ticketCount = amount / 1000;
+        for (int i =0; i< ticketCount; i++){
+            tickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
+        }
+    }
+
+    public List<Lotto> getTickets(){
+        return tickets;
+    }
+
+    public int calculateProfit(WinningNumbers winningNumbers){
+        return tickets.stream()
+                .mapToInt(ticket ->Rank.valueOf(
+                        winningNumbers.countMatchingNumbers(ticket.getNumbers()),
+                        ticket.getNumbers().contains(winningNumbers.getBonusNumber()))
+                        .getPrize()).sum();
+    }
 }
