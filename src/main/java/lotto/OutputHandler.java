25b9b2ea90da4882b7af9ec64ca8a096e941f0c7
suhyun9764 <suhyun9764@naver.com>
2024-11-04 09:35:44 +0900

    feat() : 구매 티켓 출력하기 구현

--- src/main/java/lotto/OutputHandler.java
+++ src/main/java/lotto/OutputHandler.java
@@ -3,6 +3,9 @@ package lotto;
 import static lotto.OutputMessage.INPUT_BONUS_NUMBER_MESSAGE;
 import static lotto.OutputMessage.INPUT_PURCHASE_AMOUNT_MESSAGE;
 import static lotto.OutputMessage.INPUT_WINNING_NUMBERS_MESSAGE;
+import static lotto.OutputMessage.PURCHASE_QUANTITY_INFORMATION_MESSAGE;
+
+import java.util.List;
 
 public class OutputHandler {
     private OutputHandler() {
@@ -16,7 +19,14 @@ public class OutputHandler {
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
     }
 
-    public static void printInputBonusNumbers() {
+    public static void printInputBonusNumbers(){
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    public static void printLottoTickets(LottoTickets lottoTickets) {
+        List<Lotto> tickets = lottoTickets.getLottoTickets();
+
+        System.out.println(tickets.size() + PURCHASE_QUANTITY_INFORMATION_MESSAGE);
+        tickets.forEach(ticket -> System.out.println(ticket));
     }
 }
--- src/main/java/lotto/OutputMessage.java
+++ src/main/java/lotto/OutputMessage.java
@@ -7,4 +7,5 @@ public class OutputMessage {
     public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String PURCHASE_QUANTITY_INFORMATION_MESSAGE = "개를 구매했습니다.";
 }
