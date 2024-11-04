b6e13bc4548115b5f8d277782e512b39d98f743c
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 20:43:09 +0900

    feat: 에러 메시지 앞에 접두사 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -25,13 +25,13 @@ public class PurchaseAmount {
 
     private void validateMinimumPurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < LOTTO_PRICE) {
-            throw new IllegalArgumentException("구매 금액은 0 이상이어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 0 이상이어야 합니다.");
         }
     }
 
     private void validateDivisibility(int purchaseAmount) {
         if (purchaseAmount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException("구매 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 " + LOTTO_PRICE + "원 단위여야 합니다.");
         }
     }
 
