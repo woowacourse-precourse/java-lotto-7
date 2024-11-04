3538f5e6a4a5525c7bfe90c2d0ca154ffb54d426
sanghun <vitash1215@gmail.com>
2024-11-04 22:38:48 +0900

    refactor : Lotto 중복 및 유효 숫자 검사
    WinningLotto를 Lotto로 만들어 중복 숫자가 있는지와 숫자의 범위가 알맞는 지 검사할 필요가 생김. 이에 따라 유효성 검사를 추가하고 test code를 통해 정상 작동 확인

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -9,12 +9,28 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumber(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             ExceptionHandler.throwIllegalArgException(ErrorMessage.INVALID_LOTTO_SIZE);
+        }
+    }
+
+    // 로또 번호 범위 검사
+    private void validateNumber(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            ExceptionHandler.throwIllegalArgException(ErrorMessage.INVALID_NUMBER);
+        }
+    }
+
+    // 중복 검사
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != 6) {
+            ExceptionHandler.throwIllegalArgException(ErrorMessage.DUPLICATE_NUMBER);
         }
     }
 
--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,9 +1,12 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import lotto.model.lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
@@ -14,4 +17,33 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호의 개수가 6개 보다 적을 경우 예외 발생")
+    void lottoInvalidSize() {
+        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5); // 개수 5개
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(invalidNumbers));
+    }
+
+    @Test
+    @DisplayName("로또 번호가 중첩될 경우 예외 발생")
+    void lottoDuplicatedNumbers() {
+        List<Integer> duplicatedNumbers = List.of(1, 2, 3, 4, 5, 5); // 중복 번호 5
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(duplicatedNumbers));
+    }
+
+    @Test
+    @DisplayName("로또 번호가 1에서 45 사이를 벗어날 경우 예외 발생")
+    void lottoNumbersOutOfRange() {
+        List<Integer> invalidNumbers = List.of(0, 2, 3, 4, 5, 6); // 0은 유효하지 않은 값
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(invalidNumbers));
+    }
+
+
+    @Test
+    @DisplayName("올바른 개수의 로또 번호가 입력되면 객체가 정상적으로 생성된다")
+    void lottoValidSize() {
+        List<Integer> validNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(validNumbers);
+        assertEquals(validNumbers, lotto.getNumbers());
+    }
 }
