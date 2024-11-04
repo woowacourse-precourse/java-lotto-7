2c1bd70060a540cc0818917d5cc93de2a4bf7feb
Minuring <mw9505@gmail.com>
2024-11-04 18:46:44 +0900

    fix(service): winning 서비스가 null을 리턴하던 것을 수정
    createWinning()이 null을 리턴하던 버그를 수정

--- src/main/java/lotto/service/winning/WinningServiceImpl.java
+++ src/main/java/lotto/service/winning/WinningServiceImpl.java
@@ -3,6 +3,7 @@ package lotto.service.winning;
 import static lotto.constant.LottoConstant.PRICE;
 
 import java.util.List;
+import lotto.constant.Error;
 import lotto.domain.LottoPurchase;
 import lotto.domain.Rank;
 import lotto.domain.winning.BonusNumber;
@@ -13,7 +14,14 @@ public class WinningServiceImpl implements WinningService {
 
     @Override
     public Winning createWinning(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
-        return null;
+        validate(winningNumbers, bonusNumber);
+        return new Winning(winningNumbers, bonusNumber);
+    }
+
+    private void validate(WinningNumbers winningNumbers, BonusNumber bonusNumber) {
+        if (winningNumbers.getNumbers().contains(bonusNumber.getNumber())) {
+            throw new IllegalArgumentException(Error.DUPLICATED_WINNING_BONUS_NUMBERS);
+        }
     }
 
     @Override
