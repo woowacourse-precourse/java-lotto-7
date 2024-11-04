5eb10de8559d1f07355a3a67b0ba4f1224ae9811
GO-TE <alsrudxo9810@naver.com>
2024-11-04 20:50:53 +0900

    feat(OutputView): 로또 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,42 @@
 package lotto.view;
 
+import java.util.Map;
+import java.util.Map.Entry;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRanking;
 import lotto.domain.Lottos;
 
 public class OutputView {
     public void printPurchasedLottos(Lottos lottos) {
-        System.out.printf("%d개를 구매했습니다.\n", lottos.getSize());
+        System.out.printf("%d개를 구매했습니다.%n", lottos.getSize());
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.getNumbers());
         }
         System.out.println();
+    }
+
+    public void printResult(Map<LottoRanking, Integer> result, double rateOfReturn) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Map.Entry<LottoRanking, Integer> entry : result.entrySet()) {
+            printEachResult(entry);
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", rateOfReturn);
+    }
+
+    private void printEachResult(Entry<LottoRanking, Integer> entry) {
+        if (entry.getKey() == LottoRanking.SECOND) {
+            System.out.printf("%d개 일치, 보너스 볼 일치 ", entry.getKey().getMatchCount());
+            System.out.printf("(%,d원) - ", entry.getKey().getReward());
+            System.out.printf("%d개%n", entry.getValue());
+        }
+        if (entry.getKey() == LottoRanking.NONE) {
+            return;
+        }
+        if (entry.getKey() != LottoRanking.SECOND) {
+            System.out.printf("%d개 일치 ", entry.getKey().getMatchCount());
+            System.out.printf("(%,d원) - ", entry.getKey().getReward());
+            System.out.printf("%d개%n", entry.getValue());
+        }
     }
 }
