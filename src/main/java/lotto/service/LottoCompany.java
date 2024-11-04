95ed04ab74ee82137d3ef495a5d2ca1fff33bbdd
yunsseong <aster20@hanyang.ac.kr>
2024-11-04 22:42:51 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/main/java/lotto/service/LottoCompany.java
+++ src/main/java/lotto/service/LottoCompany.java
@@ -5,10 +5,16 @@ import lotto.view.message.SystemMessage;
 
 public class LottoCompany {
     private Lotto winningNumber;
+    private Integer bonusNumber;
     private final InputView inputView = new InputView();
 
     public Lotto setWinningNumber() {
         this.winningNumber = new Lotto(inputView.lottoInput(SystemMessage.INPUT_WINNING_NUMBER.getMessage()));
         return winningNumber;
+    }
+
+    public Integer setBonusNumber(Lotto lotto) {
+        this.bonusNumber = inputView.lottoNumberInput(SystemMessage.INPUT_BONUS_NUMBER.getMessage(), lotto);
+        return bonusNumber;
     }
 }
