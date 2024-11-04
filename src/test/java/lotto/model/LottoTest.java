8b78ed9c03c1937ea4cdb2bc7eeab01316f94ce9
iceprins <junghankyul@gmail.com>
2024-11-04 23:43:09 +0900

    refactor: Lotto 클래스에 대한 테스트 코드 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,8 +1,9 @@
-package lotto;
+package lotto.model;
 
-import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, 4, 5, 6, 7"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -22,5 +23,25 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 유효한 범위가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_유효한_범위가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 11, 21, 23, 43, 47)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void 로또_번호를_입력하지_않으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호를 유효한 구분자로 구분하지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1.2,3,4,5,6", "1.2.3.4.5.6", "1,2,3.4,5,6"})
+    void 로또_번호를_유효한_구분자로_구분하지_않으면_예외가_발생한다(String numbers) {
+        assertThatThrownBy(() -> new Lotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
