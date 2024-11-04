f7a64aa014389eb1d2ad84620426e26c2c7dfa20
iceprins <junghankyul@gmail.com>
2024-11-04 23:37:39 +0900

    refactor: ErrorConstant 클래스 일부 문자열 수정

--- src/main/java/lotto/view/constant/ErrorConstant.java
+++ src/main/java/lotto/view/constant/ErrorConstant.java
@@ -6,7 +6,7 @@ public class ErrorConstant {
     public static final String NOT_NUMBER = "숫자를 입력해야 합니다.";
     public static final String NOT_POSITIVE = "로또는 한 개 이상 구입해야 합니다.";
     public static final String NOT_MULTIPLE_OF_THOUSAND = "1000 단위로 입력해야 합니다.";
-    public static final String NOT_VALID_DELIMITER = "당첨 번호는 쉼표(,)로 구분해 입력해야 합니다.";
-    public static final String NOT_WITHIN_VALID_RANGE = "1부터 45 사이의 숫자를 입력해야 합니다.";
-    public static final String NOT_CORRECT_NUMBER_COUNT = "당첨 번호는 숫자 6개를 입력해야 합니다.";
+    public static final String NOT_VALID_FORMAT = "당첨 번호는 숫자 6개를 쉼표(,)로 구분해 입력해야 합니다.";
+    public static final String NOT_WITHIN_VALID_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String NOT_UNIQUE_NUMBER = "중복되는 숫자가 존재합니다.";
 }
