fb033cb69692bddd49e7223af4c6c64bffb4dd0b
cyunlee <siyunkr@gmail.com>
2024-11-04 20:43:31 +0900

    feat(util): 당첨 숫자 개수 관련 에러 처리 메서드 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -38,16 +38,18 @@ public class Validator {
         if(lottoNumber.isBlank() || lottoNumber==null) {
             throw new IllegalArgumentException("[ERROR] 당첨 숫자는 공백이나 null값이 될 수 없습니다.");
         }else if(!lottoNumber.matches("[0-9]+")){
-            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 숫자만 입력할 수 있습니다.")
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 숫자만 입력할 수 있습니다.");
         }else if(Integer.parseInt(lottoNumber) < 1 && Integer.parseInt(lottoNumber) > 45) {
             throw new IllegalArgumentException(("[ERROR] 당첨 숫자는 1 ~ 45 범위여야 합니다."));
         }
     }
 
-    public void isLottoNumberDuplicated(List<Integer> lottoNumbers) {
+    public void isLottoNumbersValid(List<Integer> lottoNumbers) {
         Set<Integer> removeDuplicates = new HashSet<>(lottoNumbers);
         if(removeDuplicates.size() != lottoNumbers.size()) {
             throw new IllegalArgumentException("[ERROR] 당첨 숫자는 중복될 수 없습니다.");
+        }else if(lottoNumbers.size() > 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 숫자는 6개를 넘을 수 없습니다.");
         }
     }
 
