e491cab97e262cdf6b483c913b5bf6d9ee147fc3
mhj1205 <lightnow1205@gmail.com>
2024-11-04 15:14:30 +0900

    feat: 구입 금액 입력받기 및 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,20 @@
 package lotto;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("구입금액을 입력해 주세요.");
-        int amount = sc.nextInt();
-
+            int purchaseAmount = getPurchaseAmount();
     }
+
+    // 구입 금액 입력 처리
+    private static int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        if (amount < 1000 || amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 유효한 금액을 입력해 주세요 (1,000원 단위).");
+        }
+        return amount;
+    }
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    // 생성자와 유효성 검증
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
     }
 
+    // 유효성 검사
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
-    // TODO: 추가 기능 구현
 }
