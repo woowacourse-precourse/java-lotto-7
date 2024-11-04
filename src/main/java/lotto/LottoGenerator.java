e492ca03d3ea4f6b73fabd7968f967f314c83fec
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 16:40:24 +0900

    feat: 로또 한장을 발행하는 generateLotto 메서드 추가

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 45;
+    private static final int COUNT = 6;
+    public Lotto generateLotto(){
+        List<Integer> pickedNumbers = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+        pickedNumbers.sort(Integer::compareTo);
+        return new Lotto(pickedNumbers);
+    }
+}
