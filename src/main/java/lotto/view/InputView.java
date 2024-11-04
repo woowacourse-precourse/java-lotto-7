2faeefff50246dca38ebf395ad3f7f5319716a2d
GiSeok Kang <ime.rokz@gmail.com>
2024-11-04 10:48:59 +0900

    refactor: InputView 개행 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,23 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static final String ASK_MONEY_INPUT = "구입금액을 입력해 주세요.\n";
-    public static final String ASK_LOTTO_NUM_INPUT = "당첨 번호를 입력해 주세요.\n";
-    public static final String ASK_LOTTO_BONUS_NUM_INPUT = "보너스 번호를 입력해 주세요.\n";
+    public static final String ASK_MONEY_INPUT = "구입금액을 입력해 주세요.";
+    public static final String ASK_LOTTO_NUM_INPUT = "당첨 번호를 입력해 주세요.";
+    public static final String ASK_LOTTO_BONUS_NUM_INPUT = "보너스 번호를 입력해 주세요.";
 
     public String lottoMoneyInput() {
-        System.out.printf(ASK_MONEY_INPUT);
+        System.out.println(ASK_MONEY_INPUT);
         return Console.readLine();
     }
 
     public String lottoNumsInput() {
-        System.out.printf(ASK_LOTTO_NUM_INPUT);
+        System.out.println(ASK_LOTTO_NUM_INPUT);
         return Console.readLine();
     }
 
     public String lottoBonusNumInput() {
-        System.out.printf(System.lineSeparator());
-        System.out.printf(ASK_LOTTO_BONUS_NUM_INPUT);
+        System.out.println(ASK_LOTTO_BONUS_NUM_INPUT);
         return Console.readLine();
     }
 }
