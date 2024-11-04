e34d1c582185feead68b6eeb8e52be9262dfaf6c
westzeroright <kt8267@naver.com>
2024-11-04 20:20:46 +0900

    refactor: 예외 메세지를 enum으로 관리

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,12 +1,22 @@
 package lotto;
 
-public class Exception {
-    public static final String IS_NOT_1000_UNIT = "[ERROR] 로또 구입은 1000원 단위로 할 수 있습니다.";
-    public static final String DONT_NOT_ZERO = "[ERROR] 로또를 하나 이상 구매해주세요";
-    public static final String SIX_WINNING_NUMBER = "[ERROR] 당첨 번호의 개수는 6개입니다";
-    public static final String NUMBER_RANGE = "[ERROR] 1에서 45사이의 번호를 입력하십시오.";
-    public static final String DUPLICATE_WINNING_NUMBER = "[ERROR] 중복되지 않게 당첨 번호를 입력하세요.";
-    public static final String DUPLICATE_BONUS_WINNING = "[ERROR] 당첨번호와 중복된 숫자가 있습니다";
-    public static final String DONT_ENTER_STRING = "[ERROR] 구입 금액을 올바르게 입력하십시오.";
-    public static final String DUPLICATE_NUMBER_LOTTO = "[ERROR] 로또에 중복된 숫자가 있습니다.";
+public enum Exception {
+    IS_NOT_1000_UNIT("[ERROR] 로또 구입은 1000원 단위로 할 수 있습니다."),
+    DONT_NOT_ZERO("[ERROR] 로또를 하나 이상 구매해주세요"),
+    SIX_WINNING_NUMBER("[ERROR] 당첨 번호의 개수는 6개입니다"),
+    NUMBER_RANGE("[ERROR] 1에서 45사이의 번호를 입력하십시오."),
+    DUPLICATE_WINNING_NUMBER("[ERROR] 중복되지 않게 당첨 번호를 입력하세요."),
+    DUPLICATE_BONUS_WINNING("[ERROR] 당첨번호와 중복된 숫자가 있습니다"),
+    DONT_ENTER_STRING("[ERROR] 구입 금액을 올바르게 입력하십시오."),
+    DUPLICATE_NUMBER_LOTTO("[ERROR] 로또에 중복된 숫자가 있습니다.");
+
+    private final String message;
+
+    Exception(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
