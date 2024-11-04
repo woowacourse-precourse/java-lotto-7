93c642ff6708a1da129d6b05e3903f3d4e2f9cd9
나상연 <reddynor@naver.com>
2024-11-04 12:21:39 +0900

    test(LottoManagerTest): Add winning lotto setup and matching test
    - Set up a winning lotto in the test environment for more comprehensive testing
    - Added compareLotto test case to validate the matching count functionality
    - Ensured lotto list generation and sorting functionality remains intact and verified

--- src/test/java/lotto/LottoManagerTest.java
+++ src/test/java/lotto/LottoManagerTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +16,7 @@ class LottoManagerTest {
     void setUp() {
         manager = new LottoManager();
         manager.setPurchaseAmountForTest(3);
+        manager.setWinningLottoForTest(new Lotto(Arrays.asList(1,2,3,4,5,6)));
     }
     @Test
     void 로또_리스트_갯수_테스트(){
@@ -31,5 +33,14 @@ class LottoManagerTest {
             List<Integer> numbers = lotto.getNumbers();
             assertThat(numbers).isSorted();
         }
+    }
+
+    @Test
+    void 로또_당첨_비교_테스트(){
+        Lotto testLotto1 = new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9));
+
+        int compareLotto = manager.compareLotto(testLotto1);
+
+        assertThat(compareLotto).isEqualTo(3);
     }
 }
