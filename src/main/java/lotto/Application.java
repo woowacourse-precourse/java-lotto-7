ba9d6e0f8f556e5db706c157c345ba098d6872de
진민규 <jinmingyu@jinmingyuui-MacBookPro.local>
2024-11-04 18:51:45 +0900

    feat : 로또 티켓 생성 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,17 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int money = inputMoney();
-        System.out.println("구입 금액 : " + money);
+        List<Lotto> tickets = generateLottoTickets(money);
+        printTickets(tickets);
     }
 
     private static int inputMoney() {
@@ -21,4 +26,24 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
     }
+
+    private static List<Lotto> generateLottoTickets(int money) {
+        int ticketCount = money / 1000; // 로또 개당 1000원
+        List<Lotto> tickets = new ArrayList<>();
+        for (int i = 0; i < ticketCount; i++) {
+            tickets.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        }
+        return tickets;
+    }
+
+    // 생성된 티켓 출력
+    private static void printTickets(List<Lotto> tickets) {
+        System.out.println(tickets.size() + "개를 구매했습니다.");
+        for (Lotto ticket : tickets) {
+            System.out.println(ticket.getSortNumbers());
+        }
+    }
 }
+//inputMoney 금액을 입력받고 유효성을 검사하는 로직을 추가
+//generateLottoTickets(int purchaseAmount) 구입 금액에 따라 로또 티켓 수를 계산, 각 티켓을 생성
+//printTickets(List<Lotto> tickets): 생성된 티켓의 개수와 티켓의 번호를 오름차순으로 출력
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private final List<Integer> numbers; //로또 번호를 저장하는 변수
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -15,6 +17,17 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
-
     // TODO: 추가 기능 구현
+
+    // 오름차순으로 정렬 메서드
+    public List<Integer> sortNumbers() {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers); // numbers 리스트의 복사본
+        Collections.sort(sortedNumbers); // 복사본을 정렬
+        return sortedNumbers; // 정렬된 복사본 반환
+    }
+    // numbers 리스트를 반환하는 메서드 추가
+    public List<Integer> getSortNumbers() {
+        return sortNumbers();
+    }
+
 }
