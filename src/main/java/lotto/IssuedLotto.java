0b0a132ce8f5cc2edf23ebe75544e02ec5bf59dd
Grayson1999 <bbt1250912@gmail.com>
2024-11-04 15:33:19 +0900

    feat: 발행 로또 클래스 구현
    - 입력된 로또 번호 검증 기능 추가
    - 입력된 보너스 번호 검증 기능 추가

---
+++ src/main/java/lotto/IssuedLotto.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.List;
+
+public class IssuedLotto extends Lotto{
+    private final int bonusNumber;
+
+    public IssuedLotto(List<Integer> numbers, int bonusNumber) {
+        super(numbers);
+        validateBonusNumber(numbers,bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, int bonusNumber){
+        if (numbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
+        }
+        if(bonusNumber<1 || bonusNumber >45){
+            throw new IllegalArgumentException(("[ERROR] 로또 번호는 1~45범위 내에 있어야 합니다."));
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
---
+++ src/test/java/lotto/IssuedLottoTest.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class IssuedLottoTest extends LottoTest {
+
+    @Test
+    void 보너스번호가_로또번호와_중복된_숫자가_입력되면_예외가_발생한다() {
+        //given
+        List<Integer> lotto_number = List.of(1, 2, 3, 4, 5, 6);
+        int bonus_number = 5;
+        //when
+        Throwable thrown = catchException(() -> new IssuedLotto(lotto_number, bonus_number));
+        //then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @DisplayName("보너스 번호가 1~45사이에 없으면 예외가 발생하한다.")
+    @Test
+    void 보너스_번호가_범위내에_없으면_예외가_발생한다() {
+        //given
+        List<Integer> lotto_number = List.of(1, 2, 3, 4, 5, 6);
+        int bonus_number = 46;
+        //when
+        Throwable thrown = catchException(() -> new IssuedLotto(lotto_number,bonus_number));
+        //then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
