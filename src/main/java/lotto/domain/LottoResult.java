6d46d683ba2b4578e2a19ee3b2ce0beebfe617e4
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2024-11-04 16:54:24 +0900

    feat : 로또 결과 관리 및 통계 계산 기능 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,4 +1,34 @@
 package lotto.domain;
 
+import java.util.Map;
+
 public class LottoResult {
+    private final Map<Rank,Integer> results;
+
+    public LottoResult(Map<Rank,Integer> results){
+        this.results=results;
+    }
+
+    public Map<Rank,Integer> getResults(){
+        return results;
+    }
+
+
+    public double calculateEarningsRate(int purchaseAmount){
+
+        int totalPrize = 0;
+
+        for(Map.Entry<Rank,Integer> entry : results.entrySet()){
+            Rank rank = entry.getKey(); // 당첨 순위
+            int ticketCount = entry.getValue(); // 해당 순위의 티켓 수
+
+            totalPrize +=rank.getPrize() * ticketCount;
+        }
+
+        double earningsRate = (double) totalPrize / purchaseAmount*100;
+
+        return earningsRate;
+    }
+
+
 }
