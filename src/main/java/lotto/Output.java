6c299bd8a4c3ce342ed701cf62795cefeca6ae4c
Avocado <ujubongbong@gmail.com>
2024-11-04 21:30:45 +0900

    :sparkles: feat: 로또 번호 출력 함수

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Output {
 	public void init() {
 		System.out.println("구입금액을 입력해 주세요.");
@@ -7,5 +9,9 @@ public class Output {
 
 	public void printLottoCount(int lottoCount) {
 		System.out.println(lottoCount + "개를 구매했습니다.");
+	}
+
+	public void printLottos(List<Lotto> lottos) {
+		lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
 	}
 }
