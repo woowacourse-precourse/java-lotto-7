59b51b457556d5423ee40f96bc47e51cc9f70fab
JeongSu Park <bechalrang@gmail.com>
2024-11-04 13:42:43 +0900

    refactor(LottoOutput): 거대한 메서드를 작고 의미있는 단위로 분리

--- src/main/java/lotto/lottoapp/view/LottoOutput.java
+++ src/main/java/lotto/lottoapp/view/LottoOutput.java
@@ -2,6 +2,7 @@ package lotto.lottoapp.view;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.lottoapp.model.value.LottoNumbers;
@@ -11,58 +12,59 @@ import lotto.lottoapp.model.value.WinningStatistics;
 public class LottoOutput {
 
     public void showIssuedLottoNumbers(List<LottoNumbers> issuedLottoNumbers) {
-        StringBuilder messageOfBuyLottos = new StringBuilder();
-        messageOfBuyLottos.append(issuedLottoNumbers.size())
-                .append("개를 구매했습니다.")
-                .append(System.lineSeparator());
-
-        issuedLottoNumbers.stream()
-                .map(lottoNumber -> lottoNumber.stream()
-                        .map(number -> String.valueOf(number.getNumber()))
-                        .collect(Collectors.joining(", ", "[", "]")))
-                .forEach(lineOfLotto -> messageOfBuyLottos.append(lineOfLotto).append(System.lineSeparator()));
-
-        System.out.println(messageOfBuyLottos);
+        String issuedLottoMessageFormat = """
+                %d개를 구매했습니다.
+                %s
+                """;
+        System.out.printf(issuedLottoMessageFormat,
+                issuedLottoNumbers.size(),
+                getNumbersOf(issuedLottoNumbers));
     }
 
     public void announceWinningStatistics(WinningStatistics winningStatistics) {
-        StringBuilder messageOfWinningStatistics = new StringBuilder();
-        messageOfWinningStatistics.append("당첨 통계")
-                .append(System.lineSeparator())
-                .append("---")
-                .append(System.lineSeparator());
+        String statisticsMessageFormat = """
+                당첨 통계
+                ---
+                %s
+                총 수익률은 %s%%입니다.
+                """;
+        System.out.printf(statisticsMessageFormat,
+                getTotalStatisticsOfWinningCount(winningStatistics),
+                winningStatistics.getRateReturn());
+    }
 
+    private static String getNumbersOf(List<LottoNumbers> issuedLottoNumbers) {
+        return issuedLottoNumbers.stream()
+                .map(lineOfLotto -> lineOfLotto.stream()
+                        .map(lottoNumber -> String.valueOf(lottoNumber.getNumber()))
+                        .collect(Collectors.joining(", ", "[", "]")))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    private static String getFormattedMessageOfWinningCount(Map.Entry<WinningResult, Long> winCountEntry) {
+        String lineOfSecondWinningMessageFormat = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+        String lineOfWinningMessageFormat = "%d개 일치 (%s원) - %d개";
+
+        if (winCountEntry.getKey() == WinningResult.SECOND) {
+            return String.format(lineOfSecondWinningMessageFormat,
+                    winCountEntry.getKey().minCountOfWinningNumber,
+                    winCountEntry.getKey().prize,
+                    winCountEntry.getValue());
+        }
+        return String.format(lineOfWinningMessageFormat,
+                winCountEntry.getKey().minCountOfWinningNumber,
+                winCountEntry.getKey().prize,
+                winCountEntry.getValue());
+    }
+
+    private static String getTotalStatisticsOfWinningCount(WinningStatistics winningStatistics) {
         int firstRank = 1;
         int fifthRank = 5;
-        IntStream.rangeClosed(firstRank, fifthRank).boxed()
+        return IntStream.rangeClosed(firstRank, fifthRank).boxed()
                 .sorted(Comparator.reverseOrder())
                 .map(winningStatistics::findWinningBy)
-                .forEach(winCountEntry -> {
-                    if (winCountEntry.getKey() == WinningResult.SECOND) {
-                        messageOfWinningStatistics.append(winCountEntry.getKey().minCountOfWinningNumber)
-                                .append("개 일치, 보너스 볼 일치 (")
-                                .append(winCountEntry.getKey().prize)
-                                .append("원) - ")
-                                .append(winCountEntry.getValue())
-                                .append("개")
-                                .append(System.lineSeparator());
-                        return;
-                    }
-                    messageOfWinningStatistics.append(winCountEntry.getKey().minCountOfWinningNumber)
-                            .append("개 일치 (")
-                            .append(winCountEntry.getKey().prize)
-                            .append("원) - ")
-                            .append(winCountEntry.getValue())
-                            .append("개")
-                            .append(System.lineSeparator());
-                });
-
-        messageOfWinningStatistics.append("총 수익률은 ")
-                .append(winningStatistics.getRateReturn())
-                .append("%입니다.")
-                .append(System.lineSeparator());
-
-        System.out.println(messageOfWinningStatistics);
+                .map(LottoOutput::getFormattedMessageOfWinningCount)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 
 }
