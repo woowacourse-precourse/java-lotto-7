2abe3619d4aca8982f363b60678fe74b9912a8e8
(minjae8563) <(minjae8563@naver.com)>
2024-11-04 20:16:36 +0900

    refactor: 생성자 함수에 인자를 바로 전달하는 방식의 테스트 케이스로 수정

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -9,7 +9,7 @@ public class BonusNumberTest {
     void 예외_메시지_테스트_1부터_45까지_숫자만_입력() {
 
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            BonusNumber bonusNumber = new BonusNumber(46);
+            new BonusNumber(46);
         });
 
         Assertions.assertEquals("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.", exception.getMessage());
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -9,7 +9,7 @@ public class MoneyTest {
     void divideByThousand_예외_메시지_테스트_1000원_단위로_입력() {
 
         IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Money money = new Money(1001);
+            new Money(1001);
         });
 
         Assertions.assertEquals("[ERROR] 1000원 단위의 금액을 입력해 주세요.", exception.getMessage());
