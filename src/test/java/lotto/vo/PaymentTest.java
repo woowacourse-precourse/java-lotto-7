9fc1be6aa5603ae5e3393460bddbea3c78fb3e6d
hmlssghm <kwonse326@gmail.com>
2024-11-04 21:45:26 +0900

    test(PaymentTest): 파싱 관련 메서드 추출

--- src/test/java/lotto/vo/PaymentTest.java
+++ src/test/java/lotto/vo/PaymentTest.java
@@ -7,18 +7,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PaymentTest {
     @Test
-    void 파싱_중_정수_범위_초과() {
-        assertThatThrownBy(() -> new Payment("2147483648"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 파싱_실패_예외_처리() {
-        assertThatThrownBy(() -> new Payment("1000a"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
     void 음수_예외_처리() {
         assertThatThrownBy(() -> new Payment("-1000"))
                 .isInstanceOf(IllegalArgumentException.class);
