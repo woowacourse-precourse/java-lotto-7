371cbd7a46f63a60d8615370f8c301403b22a76b
seohee-P <p03061180@gmail.com>
2024-11-04 22:49:16 +0900

    feat: 로또 번호 검증 및 예외처리 기능 추가
    - 아래의 경우에 해당하는지 확인하고, 해당하면 예외처리
    - 로또의 번호가 6개가 아닐 경우
    - 로또 번호가 중복되었을 경우
    - 로또 번호가 1부터 45 사이의 숫자가 아닐 경우

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,19 +2,41 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.exception.ErrorMessage.*;
+
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicated(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(INVALID_NUMBER_SIZE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicated(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(INVALID_NUMBER_DUPLICATE.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto.exception;
 public enum ErrorMessage {
 
     INVALID_PURCHASE_MONEY("구입금액은 1000원 이상이면서 1000원 단위여야 합니다."),
-    INVALID_INPUT_NUMBER("숫자만 입력해 주세요.");
+    INVALID_INPUT_NUMBER("숫자만 입력해 주세요."),
+    INVALID_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
+    INVALID_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_NUMBER_DUPLICATE("로또 번호는 중복될 수 없습니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
 
