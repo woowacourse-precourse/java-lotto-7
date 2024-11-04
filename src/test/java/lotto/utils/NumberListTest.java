66ff38864ce86667fd69eb344703967ec98b8e18
armcortex3445 <wlghks5966@naver.com>
2024-11-04 15:45:48 +0900

    test(NumberList) : delete useless test code
    - delete test code relating to deleted method

--- src/test/java/lotto/utils/NumberListTest.java
+++ src/test/java/lotto/utils/NumberListTest.java
@@ -8,47 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberListTest {
 
-    @Test
-    @DisplayName("Test Add Number to NumberList")
-    void testAddNumberToList(){
 
 
-        NumberList numberList = new NumberList();
-        int newNumber = 4;
-        numberList.addNewNumber(newNumber);
-        assertThat(numberList.size()).isEqualTo(1);
-        assertThat(numberList.getFirst()).isEqualTo(newNumber);
-
-    }
-
-    @Test
-    @DisplayName("Test Exception for adding duplicated Number to NumberList")
-    void testAddDuplicatedNumberToList(){
-
-
-        NumberList numberList = new NumberList();
-        int newNumber = 4;
-
-        numberList.addNewNumber(newNumber);
-        numberList.addNewNumber(newNumber);
-
-        assertThat(numberList.size()).isEqualTo(1);
-        assertThat(numberList.getFirst()).isEqualTo(newNumber);
-
-    }
-
-    @RepeatedTest(value = 10000)
-    @DisplayName("Test generate random number from 1 to 45")
-    void testRandomNumber(){
-
-        NumberList numberList = new NumberList();
-
-        int min = 1;
-        int max = 45;
-
-        assertThat(numberList.generateRandomNumber()).isLessThanOrEqualTo(max).isGreaterThanOrEqualTo(min);
-
-    }
 
     @RepeatedTest(value = 10000)
     @DisplayName("Test generate random number list")
