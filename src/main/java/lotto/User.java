1b12fc53d16cc61692c120a06a039a35c5c424c7
Hajuuu <hpt0127@naver.com>
2024-11-04 16:28:21 +0900

    feat: 등수별 당첨 개수 계산 기능 구현
    발행한 로또와 당첨 번호, 보너스 번호를 통해 당첨된 등수를 받고 이를 당첨 등수와 개수로 반환하는 메서드 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,9 +2,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class User {
+
 
     public List<Lotto> createLotto(int count) {
         List<Lotto> lottos = new ArrayList<>();
@@ -12,5 +17,19 @@ public class User {
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
         }
         return lottos;
+    }
+
+    public Map<Rank, Integer> countRank(List<Lotto> lottos, Lotto winningNumbers, int bonusNumber) {
+        List<Rank> ranks = lottos.stream().map(n -> n.countMatchNumber(winningNumbers, bonusNumber))
+                .collect(Collectors.toList());
+        Map<Rank, Integer> winnings = new HashMap<>();
+        for (Rank rank : ranks) {
+            if (winnings.containsKey(rank)) {
+                winnings.put(rank, winnings.get(rank) + 1);
+                continue;
+            }
+            winnings.put(rank, 1);
+        }
+        return winnings;
     }
 }
