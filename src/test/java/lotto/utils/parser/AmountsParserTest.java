34d0781d39b51afcab5a5253f2bf854776f56466
Hwahyeon Song <71603815+ghkgus@users.noreply.github.com>
2024-11-04 18:06:41 +0900

    test: 구조 조정하며 테스트 케이스 추가

--- src/test/java/lotto/utils/parser/AmountsParserTest.java
+++ src/test/java/lotto/utils/parser/AmountsParserTest.java
@@ -3,7 +3,6 @@ package lotto.utils.parser;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import lotto.constants.ErrorMessage;
-import lotto.utils.validator.AmountsValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,8 +10,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 class AmountsParserTest {
 
     @ParameterizedTest
+    @DisplayName("정수 범위가 아닌 값이 입력되면, 예외가 발생한다.")
+    @ValueSource(strings = {"2147483648", "-2147483649"})
+    void Given_AmountsAreOverIntegerRange_When_Parsing_Then_Error(String input) {
+        assertThatThrownBy(() -> AmountsParser.getAmounts(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ONLY_INTEGER_RANGE_AMOUNTS_ALLOWED.getMessage());
+    }
+
+    @ParameterizedTest
     @DisplayName("1000원보다 적은 금액이 입력되면, 예외가 발생한다.")
-    @ValueSource(strings = "999")
+    @ValueSource(strings = {"999", "900", "100"})
     void Given_AmountsAreOverMinAmounts_When_CheckMinAmounts_Then_Error(String input) {
         assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -21,7 +29,7 @@ class AmountsParserTest {
 
     @ParameterizedTest
     @DisplayName("10만원이 넘는 금액이 입력되면, 예외가 발생한다.")
-    @ValueSource(strings = "100001")
+    @ValueSource(strings = {"100001", "110000"})
     void Given_AmountsAreOverMaxAmounts_When_CheckMaxAmounts_Then_Error(String input) {
         assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -30,7 +38,7 @@ class AmountsParserTest {
 
     @ParameterizedTest
     @DisplayName("1000원 단위가 아닌 금액이 입력되면, 예외가 발생한다.")
-    @ValueSource(strings = "1100")
+    @ValueSource(strings = {"1100", "5500", "99999"})
     void Given_AmountsCanNotDivideByThousand_When_CheckDivisibilityByThousand_Then_Error(String input) {
         assertThatThrownBy(() -> AmountsParser.getAmounts(input))
                 .isInstanceOf(IllegalArgumentException.class)
