2dad40332dd845aafd5368b81ad5922817e5dc52
dbdb456 <adad568@naver.com>
2024-11-04 09:25:49 +0900

    test(validator): 구매 단위가 1000이 아닌경우 예외 테스트 추가

--- src/test/java/lotto/ValidatorsTest.java
+++ src/test/java/lotto/ValidatorsTest.java
@@ -1,5 +1,21 @@
 package lotto;
 
+import lotto.validator.Validators;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ValidatorsTest {
+
+    private final Validators validators = new Validators();
+
+    @DisplayName("구매 금액 단위는 1000원이 아닌경우 예외처리한다.")
+    @Test
+    void 구매_단위_1000원_아니면_예외처리() {
+        int price = 1100;
+
+        Assertions.assertThatThrownBy(() -> validators.validatePurchaseAmountUnit(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
