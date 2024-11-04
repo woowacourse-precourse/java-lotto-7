114fbb02101cb45bee6fba86e896760759021425
sseopsseop <ponde119@outlook.com>
2024-11-04 10:59:36 +0900

    feat: 상수 저장 클래스 필드 추가 로또 번호 범위(최소값, 최대값) 지정할 상수 추가함

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -2,4 +2,6 @@ package util;
 
 public class Constants {
     public static final int LOTTO_SET_SIZE = 6;
+    public static final int LOTTO_NUMBER_OFFSET = 1;
+    public static final int LOTTO_NUMBER_LIMIT = 45;
 }
