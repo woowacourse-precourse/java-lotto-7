b8b7028a51634e59b147989db17fd1feafd5f085
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-11-04 21:18:00 +0900

    feat(Result): 당첨된 결과를 저장하는 객체 구현

---
+++ src/main/java/lotto/domain/Result.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+
+public class Result {
+
+    EnumMap<Ranking, Integer> results;
+
+    private Result() {
+        results = new EnumMap<>(Ranking.class);
+        initResults();
+    }
+
+    public static Result from() {
+        return new Result();
+    }
+
+    private void initResults() {
+        for (Ranking rank : Ranking.values()) {
+            results.put(rank, 0);
+        }
+    }
+
+    public void updateResults(Integer matchingNumber, boolean hasBonusNumber) {
+        Ranking ranking = Ranking.getRanking(matchingNumber, hasBonusNumber);
+        results.merge(ranking, 1, Integer::sum);
+    }
+
+    public Integer getResultCount(Ranking ranking) {
+        return results.get(ranking);
+    }
+
+    public EnumMap<Ranking, Integer> getResults() {
+        return results;
+    }
+}
