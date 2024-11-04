8cb90e8e4d67cecc66c654289001bd3c8257c0aa
yu-k-m <karen0806@naver.com>
2024-11-04 15:10:22 +0900

    feat: add buyLotto feature

---
+++ src/main/java/lotto/util/Lotto.java
@@ -0,0 +1,44 @@
+package lotto.util;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Lotto {
+    static final int LOTTO_PRICE = 1000;
+    private final List<Integer> numbers;
+    int amount;
+
+    //numbers는 곧 당첨 번호
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+
+    public int buyLotto(int money) {
+        int remainder = money % LOTTO_PRICE;
+
+        amount = money / LOTTO_PRICE;
+        if (remainder != 0) {
+            int neededMoney = LOTTO_PRICE - remainder;
+            throw new IllegalArgumentException("[ERROR]" + remainder + "원으로는 구매가 불가능합니다." + neededMoney + "원 더 채워주세요");
+        }
+        return amount;
+
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자 범위는 1~45 까지입니다.");
+        }
+        /*private void amountValidate {
+
+
+        }*/
+    }
+
+    //while()행동시작하고 끝나면 IllegalStatementException e;
+}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import lotto.util.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.processor.MasterDetailListProcessor;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    static int money = 2002;
+    static int lottoPrice = 1000;
+    static int remainder = money % lottoPrice;
+    static int neededAmount = lottoPrice - remainder;
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +29,20 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @Test
+    void 로또를_사고_잔돈이_있을때_예외가_발생한다() {
+        String expectedMessage = String.format("%d원 더 주세요! %d원으로는 구매가 불가능합니다.", neededAmount, remainder);
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)).buyLotto(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(expectedMessage);
+
+    }
+
+    @Test
+    void 금액만큼의_로또를_산다() {
+        assertThat(new Lotto(List.of(12, 3, 4, 5, 23, 1)).buyLotto(10000))
+                .isEqualTo(10);
+    }
 }
