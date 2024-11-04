5233001c20b8e42d83137760941602cab8971c9d
kaswhy <sxshx818@naver.com>
2024-11-04 16:24:20 +0900

    feat(ErrorMessage): 로또 관련 에러메시지 추가
    변경 후
    - 숫자 이외의 문자를 포함하는 경우
    - 1~45까지의 숫자가 아닌 경우
    - 중복된 번호를 고른 경우

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -7,7 +7,9 @@ public enum ErrorMessage {
     AMOUNT_CAN_NOT_BE_ZERO("구입 금액은 0이 될 수 없습니다."),
     AMOUNT_SHOULD_BE_DIVIDED_BY_THOUSAND("구입 금액은 1,000으로 나누어 떨어져야 합니다."),
 
-    LOTTO_CAN_NOT_HAVE_CHARACTER("로또 번호는 숫자 이외의 문자를 포함할 수 없습니다.");
+    LOTTO_CAN_NOT_HAVE_CHARACTER("로또 번호는 숫자 이외의 문자를 포함할 수 없습니다."),
+    LOTTO_NUMBER_MUST_BE_ONE_TO_FORTY_FIVE("로또 번호는 1부터 45까지의 숫자여야 합니다."),
+    LOTTO_NUMBERS_CAN_NOT_BE_DUPLICATED("로또 번호는 중복될 수 없습니다.");
 
     private static final String header = "[ERROR] ";
     private final String errorMessage;
