90c4c0f9a9a3625b1aa0759600e7fa242c30a23e
haebyun <bhk4392@gmail.com>
2024-11-04 15:17:07 +0900

    refactor(OutputView): 메서드 명을 조금 더 의미가 통하도록 변경
    - printSoldLottos -> printPurchasedLottos

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -6,7 +6,7 @@ import lotto.dto.response.WinningResultsDTO;
 public interface OutputView {
     void printError(String message);
 
-    void printSoldLottos(PurchasedLottosDTO soldLottosResponseDTO);
+    void printPurchasedLottos(PurchasedLottosDTO soldLottosResponseDTO);
 
     void printWinningResults(WinningResultsDTO winningResultsDTO);
 }
