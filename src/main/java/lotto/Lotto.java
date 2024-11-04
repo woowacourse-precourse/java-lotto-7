e248c8300644d249023b148ff3f948d476a0716a
박규영 <magic9282m@gmail.com>
2024-11-04 23:46:32 +0900

    feat: add new method calcLotteryPlace
    당첨 번호와 보너스 번호를 인자로 받아, 현재 로또가 몇 위인지 반환하는 메서드 calcLotteryPlace 작성.
    - n^2으로 numbers와 당첨번호 리스트 lotteryWinningNumbers 순회하며 일치하는 숫자 갯수 카운트
    - 또한, numbers의 각 요소에 대해 보너스 번호와 일치하는지도 체크
    - 이후, 일치하는 숫자 갯수와 보너스 번호 일치 여부를 기반으로 몇 위인지 정수로 반환

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,4 +16,38 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+
+    private int calcLotteryPlace(List<Integer> lotteryWinningNumbers, int lotteryBonusNumber) {
+        int matchCount = 0;
+        boolean hasBonusNumber = false;
+
+        int idx = 0;
+        for(int i=0, iLen=numbers.size(); i<iLen; i++) {
+            int num = numbers.get(i);
+            for(int j=0, jLen=lotteryWinningNumbers.size(); j<jLen; j++) {
+                if(num == lotteryWinningNumbers.get(j)) {
+                    matchCount++;
+                }
+            }
+            if(num == lotteryBonusNumber) {
+                hasBonusNumber = true;
+            }
+        }
+        if(matchCount == 6) {
+            return 1;
+        }
+        if(matchCount == 5) {
+            if(hasBonusNumber) {
+                return 2;
+            }
+            return 3;
+        }
+        if(matchCount == 4) {
+            return 4;
+        }
+        if(matchCount == 3) {
+            return 5;
+        }
+        return 6;
+    }
 }
