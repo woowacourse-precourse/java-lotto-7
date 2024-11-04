c7f102d9592c268d5c1a5fdbaa238b0520adb389
smyeongL <sangmyeonglee123@gmail.com>
2024-11-04 16:13:25 +0900

    feat(application): call bonus number and profit controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,17 @@
 package lotto;
 
-import lotto.controller.LottoController;
-import lotto.controller.PurchaseAmountController;
-import lotto.controller.WinningNumbersController;
+import lotto.controller.*;
+import lotto.dto.BonusNumberRequestDto;
 import lotto.dto.PurchaseAmountRequestDto;
 import lotto.dto.WinningNumbersRequestDto;
+import lotto.model.Lottos;
 
 public class Application {
     public static void main(String[] args) {
         PurchaseAmountRequestDto purchaseAmountRequestDto = PurchaseAmountController.run();
-        LottoController.run(purchaseAmountRequestDto);
+        Lottos lottos = LottoController.run(purchaseAmountRequestDto);
         WinningNumbersRequestDto winningNumbersRequestDto = WinningNumbersController.run();
+        BonusNumberRequestDto bonusNumberRequestDto = BonusNumberController.run();
+        ProfitController.run(lottos, winningNumbersRequestDto, bonusNumberRequestDto);
     }
 }
