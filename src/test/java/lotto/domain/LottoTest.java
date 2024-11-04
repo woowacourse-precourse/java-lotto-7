4c038b3fd8b8e3c5344dded6ae2282884ebb9b84
DongHoon Lee <dhl1924@naver.com>
2024-11-04 22:56:46 +0900

    test(LottoTest): Lotto 클래스에 대한 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
+import lotto.domain.vo.Number;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +24,27 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다() {
+        // given // when // then
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(LottoException.class)
+                .hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    void 로또_번호가_올바를_때_정상적으로_생성된다() {
+        // given
+        List<Integer> validNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = new Lotto(validNumbers);
+
+        // then
+        assertThat(lotto.getNumbers()).hasSize(6);
+        assertThat(lotto.getNumbers())
+                .extracting(Number::lottoNumber)
+                .containsExactlyInAnyOrderElementsOf(validNumbers);
+    }
+
 }
