7cde7ac4097e58b07c15948fec943afd373e2716
kssk3 <kss8014@daum.net>
2024-11-04 11:15:10 +0900

    Refactor Lotto class

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import lotto.utils.LottoNumbersGenerator;
 
 public class Lotto {
     private static final int LOTTO_PRICE = 1000;
@@ -9,10 +11,17 @@ public class Lotto {
     private static final String REQUEST_UNIT_OF_1000_MESSAGE = LOTTO_PRICE + "원 단위로 입력해주세요.";
 
     private final Amount inputAmount;
+    private Tickets tickets = new Tickets(new ArrayList<>());
 
     public Lotto(int amount) {
         validateAmount(amount);
         this.inputAmount = new Amount(amount);
+    }
+
+    public Lotto(int amount, LottoNumbersGenerator lottoNumbersGenerator) {
+        validateAmount(amount);
+        this.inputAmount = new Amount(amount);
+        this.tickets = Tickets.of(findCountOfPurchasable(), lottoNumbersGenerator);
     }
 
     private void validateAmount(int amount) {
@@ -25,9 +34,31 @@ public class Lotto {
         return amount % LOTTO_PRICE != 0;
     }
 
+    public int findCountOfPurchasable() {
+        return inputAmount.getAmountDivide(LOTTO_PRICE);
+    }
 
-    public int getAutoTicketCount(){
+    public void addTicket(Ticket ticket) {
+        if (inputAmount.isPurchasable(LOTTO_PRICE)) {
+            tickets.add(ticket);
+            inputAmount.decrease(LOTTO_PRICE);
+        }
+    }
 
+    public Map<Rank, Integer> getResult(FirstTicket firstTicket) {
+        return tickets.getResult(firstTicket);
+    }
+
+    public double getYield(FirstTicket firstTicket) {
+        return tickets.getYield(inputAmount.getOriginalAmount(), firstTicket);
+    }
+
+    public Tickets getTickets() {
+        return tickets;
+    }
+
+    public int getAutoTicketCount() {
+        return tickets.getManualTicketCount();
     }
 
 
