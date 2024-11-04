57bfbfecbcf89bd79b0f42d0d9017b991697e5ee
yunho_yun <yunho_yun@naver.com>
2024-11-04 16:05:48 +0900

    feat: 번호 오름차순 기능 구현
    NumberSorter

---
+++ src/main/java/lotto/util/NumberSorter.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+import java.util.Collections;
+import java.util.List;
+
+public class NumberSorter {
+    public List<Integer> sort(List<Integer> numbers) {
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
