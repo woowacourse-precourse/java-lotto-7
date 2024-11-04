1d17689e8486f556aff4f876790d12f82b29ca6f
JoonKyo <joonkyolee@gmail.com>
2024-11-04 09:02:50 +0900

    feat(LottoGameController): 구입금액을 바탕으로 로또 생성하는 메서드 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,6 +1,10 @@
 package lotto.controller;
 
+import java.util.List;
 import java.util.function.Supplier;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.exception.InputException;
 import lotto.view.InputView;
@@ -9,11 +13,19 @@ import lotto.view.OutputView;
 public class LottoGameController {
     public void run() {
         Money money = repeatUntilReadValidInput(this::readMoney);
+
+        Lottos lottos = generateLottos(money);
     }
 
     private Money readMoney() {
         OutputView.askPurchasePrice();
         return Money.from(InputView.readInput());
+    }
+
+    private Lottos generateLottos(Money money) {
+        LottoMachine lottoMachine = LottoMachine.from(money);
+        List<Lotto> lottos = lottoMachine.publishLotto();
+        return Lottos.from(lottos);
     }
 
     private <T> T repeatUntilReadValidInput(Supplier<T> supplier) {
