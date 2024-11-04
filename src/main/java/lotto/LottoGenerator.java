05f548b2962ed10962cc43fc521150e2c5e6f6b3
bbangHo <sonsins1@naver.com>
2024-11-04 21:51:20 +0900

    feat: 로또 번호를 생성하는 클래스를 구현한다

---
+++ src/main/java/lotto/LottoGenerator.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public List<List<Integer>> generate(int purchaseCount) {
+        List<List<Integer>> purchaseLottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < purchaseCount; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> sortedLottoNumbers = new ArrayList<>(lottoNumbers);
+            sortedLottoNumbers.sort(Integer::compareTo);
+            purchaseLottoNumbers.add(sortedLottoNumbers);
+        }
+
+        return purchaseLottoNumbers;
+    }
+}
