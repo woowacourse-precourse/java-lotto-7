30ae0558719c0a338ee59b341ce04c419d2f36ee
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-04 13:56:53 +0900

    feat : Lotto 도메인 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        boolean isDuplicate = numbers.size() != new HashSet<>(numbers).size();
+        if (isDuplicate) {
+            throw new IllegalArgumentException("중복된 번호가 존재합니다!");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
