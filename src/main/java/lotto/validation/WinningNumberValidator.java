661232302c219165c7987c40c875d007c9d79276
Beomseok Noh <prodigy0831@naver.com>
2024-11-04 22:07:09 +0900

    refactor(WinningNumberValidator): 상수 추가 및 출력 형식 개선

--- src/main/java/lotto/validation/WinningNumberValidator.java
+++ src/main/java/lotto/validation/WinningNumberValidator.java
@@ -1,30 +1,36 @@
 package lotto.validation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WinningNumberValidator {
     private final static String INVALID_WINNING_NUMBER_INPUT = "[ERROR] 올바른 당첨 번호를 입력해 주세요";
+    private final static String INVALID_WINNING_NUMBER_COUNT_INPUT = "[ERROR] 로또 번호는 중복되지 않는 6개의 숫자여야 합니다.";
+    private final static String INVALID_WINNING_NUMBER_RANGE_INPUT = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다";
     private final static String INVALID_BONUS_NUMBER_INPUT = "[ERROR] 올바른 보너스 번호를 입력해 주세요";
-
+    private final static String INVALID_BONUS_NUMBER_RANGE_INPUT = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다";
+    private final static String INVALID_BONUS_NUMBER_DUPLICATE_INPUT = "[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.";
 
     public static List<Integer> parseValidatedWinningNumber(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(INVALID_WINNING_NUMBER_INPUT);
         }
-        List<Integer> numbers = Arrays.stream(input.replaceAll("\\s", "").split(","))
+        Set<Integer> numbers = Arrays.stream(input.replaceAll("\\s", "").split(","))
                 .filter(s -> !s.isBlank())
                 .map(Integer::parseInt)
-                .toList();
+                .collect(Collectors.toSet());
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_INPUT);
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_COUNT_INPUT);
         }
         for (int number : numbers) {
             if (number < 1 || number > 45) {
-                throw new IllegalArgumentException(INVALID_WINNING_NUMBER_INPUT);
+                throw new IllegalArgumentException(INVALID_WINNING_NUMBER_RANGE_INPUT);
             }
         }
-        return numbers;
+        return new ArrayList<>(numbers);
     }
 
     public static int parseValidatedBonusNumber(String input, List<Integer> winningNumber) {
@@ -36,10 +42,10 @@ public class WinningNumberValidator {
         }
         int bonusNumber = Integer.parseInt(input);
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_INPUT);
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_RANGE_INPUT);
         }
-        if(winningNumber.contains(bonusNumber)){
-            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_INPUT);
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER_DUPLICATE_INPUT);
         }
         return bonusNumber;
     }
