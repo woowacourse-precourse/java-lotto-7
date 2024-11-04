b1774de885ace89e89516b3b2b78b7d80a760236
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-11-04 18:38:22 +0900

    test(BonusNumber): 보너스 번호에 대한 테스트 케이스 추가
    BonusNumber 클래스의 compareNumber 클래스에 대한 테스트 케이스를 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.exception.ExceptionMessages;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -20,5 +22,17 @@ public class BonusNumberTest {
     @ValueSource(ints = {10, 43, 21})
     void 입력한_보너스_번호가_1에서_45사이의_숫자인_경우_예외가_발생하지_않는다(int number) {
         assertThatCode(() -> new BonusNumber(number)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 매개변수로_받은_숫자가_보너스_번호와_일치한다면_참이_반환된다() {
+        BonusNumber bonusNumber = new BonusNumber(10);
+        assertThat(bonusNumber.compareNumber(10)).isEqualTo(true);
+    }
+
+    @Test
+    void 매개변수로_받은_숫자가_보너스_번호와_다르다면_거짓이_반환된다() {
+        BonusNumber bonusNumber = new BonusNumber(10);
+        assertThat(bonusNumber.compareNumber(30)).isEqualTo(false);
     }
 }
