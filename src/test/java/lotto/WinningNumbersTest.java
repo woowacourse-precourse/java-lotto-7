1af731ed313601b57f3d4f072ed9a3a0654f6ded
eunseongu <rndmstjs75@gmail.com>
2024-11-04 20:07:25 +0900

    test: 당첨 번호에 대한 테스트 코드를 작성한다.

---
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+    @Test
+    void 당첨_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 당첨_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
