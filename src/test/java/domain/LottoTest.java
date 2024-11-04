b0bc1ce747fe91832c82433b49aa1ea5c31dac93
dudxo <father8475@naver.com>
2024-11-04 16:58:42 +0900

    chore(LottoTest) : LottoTest.class 패키지 위치 변경

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -1,12 +1,10 @@
-package lotto;
-
-import domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
