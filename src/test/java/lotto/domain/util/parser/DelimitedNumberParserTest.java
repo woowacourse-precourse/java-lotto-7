648a08be293ea6435e6e7fa053c2f7fda919b77b
moonbinchoi <whsahd12@gmail.com>
2024-11-04 20:22:52 +0900

    test(DelimitedNumberParser): 단위 테스트 추가

--- src/test/java/lotto/domain/util/parser/DelimitedNumberParserTest.java
+++ src/test/java/lotto/domain/util/parser/DelimitedNumberParserTest.java
@@ -1,8 +1,10 @@
 package lotto.domain.util.parser;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
+import lotto.domain.exception.DelimitedNumberFormatException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
@@ -40,6 +42,20 @@ class DelimitedNumberParserTest {
 
             //then
             assertThat(parse).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    class 당첨번호_파싱_실패 {
+
+        @Test
+        void 숫자로만_이루어지지_않은_경우() {
+            //given
+            String input = "1,2,3,4,5,6a";
+            DelimitedNumberParser parser = DelimitedNumberParser.getInstance();
+
+            //when, then
+            assertThrows(DelimitedNumberFormatException.class, () -> parser.parse(input));
         }
     }
 }
