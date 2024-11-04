4e1863bbb5e862467871157d158910da91f98e74
imjaehyeong <dlawogud517@gmail.com>
2024-11-04 12:54:57 +0900

    refactor(lotto): 예외처리 추가
    로또 번호가 1~45안에 오는지 검증하는 메소드 추가

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -19,6 +19,9 @@ public class Lotto {
         if (hasDuplicates(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
         }
+        if (!isWithinRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     private boolean hasDuplicates(List<Integer> numbers) {
