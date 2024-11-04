ff14c07466f232027b16b9cca198dc8657239f3b
이창근 <dlckdrms0517@gmail.com>
2024-11-04 11:58:21 +0900

    chore: LottoApplicationController의 클래스명을 변경
    - 각 Controller의 Facade처럼 작동하기 때문에, 그에 맞는 클래스명으로 변경하였습니다.

--- src/main/java/lotto/controller/LottoApplicationFacade.java
+++ src/main/java/lotto/controller/LottoApplicationFacade.java
@@ -1,27 +1,22 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.aop.RetryHandler;
 import lotto.controller.lottoController.LottoController;
 import lotto.controller.lottoStaticsController.LottoStaticsController;
 import lotto.controller.moneyController.MoneyController;
 import lotto.controller.winningLottoController.WinningLottoController;
 import lotto.domain.Lotto;
 import lotto.domain.Money;
-import lotto.domain.Number;
 import lotto.domain.WinningLotto;
-import lotto.dto.PurchasedLottos;
-import lotto.io.InputHandler;
-import lotto.io.OutputHandler;
 
-public class LottoApplicationController {
+public class LottoApplicationFacade {
 
     private final MoneyController moneyController;
     private final LottoController lottoController;
     private final WinningLottoController winningLottoController;
     private final LottoStaticsController lottoStaticsController;
 
-    public LottoApplicationController(
+    public LottoApplicationFacade(
             MoneyController moneyController,
             LottoController lottoController,
             WinningLottoController winningLottoController,
