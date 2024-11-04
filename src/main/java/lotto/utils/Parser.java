17adff4721d862127c40c593772be329715a2c6d
MinSang22Kim <powerminsang9901@gmail.com>
2024-11-04 23:50:58 +0900

    feat: 문자열 변환 기능 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -3,6 +3,10 @@ package lotto.utils;
 import static lotto.message.UtilNumber.LOTTO_COST;
 
 public class Parser {
+    public static int parseStringToInt(String stringValue) {
+        return Integer.parseInt(stringValue);
+    }
+
     public static int parsePaymentToCoin(int payment) {
         return payment / LOTTO_COST.getNumber();
     }
