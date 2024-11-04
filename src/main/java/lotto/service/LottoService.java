92dabd4dcc9d1654be818ffbdfa7ca8877e84617
BusanGukbap <dong2857210@gmail.com>
2024-11-04 14:50:01 +0900

    feat(Service) : 로또 번호 생성 및 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,25 @@
 package lotto.service;
 
+import lotto.domain.*;
+import lotto.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
+
+    public static final int LOTTO_PRICE = 1000;
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / LOTTO_PRICE;
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(LottoNumberGenerator.generate()));
+        }
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
 }
