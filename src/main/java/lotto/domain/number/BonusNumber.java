054bcf15d18784f3232ba9fdcb0950b6c8a23ce3
hootzz <wjqwjql3@gmail.com>
2024-11-04 13:40:44 +0900

    feat(number): 보너스 번호를 포장하는 BonusNumber class 구현

--- src/main/java/lotto/domain/number/BonusNumber.java
+++ src/main/java/lotto/domain/number/BonusNumber.java
@@ -1,4 +1,20 @@
 package lotto.domain.number;
 
 public class BonusNumber {
+    private static final String ERROR_DUPLICATE = "[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.";
+
+    private final LottoNumber number;
+
+    private BonusNumber(LottoNumber number) {
+        this.number = number;
+    }
+
+    public LottoNumber getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return number.toString();
+    }
 }
