f0fd636e6cf53563d7294bbce1a1fbdcc30d9872
coolman0123 <socool4m@naver.com>
2024-11-04 16:25:10 +0900

    feat: 당첨 번호를 list로 변환해라

--- src/main/java/lotto/util/parser/InputParser.java
+++ src/main/java/lotto/util/parser/InputParser.java
@@ -1,11 +1,23 @@
 package lotto.util.parser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputParser {
 
     public int convertPurchaseAmountToInt(String input) {
         int purchaseAmount = Integer.parseInt(input);
 
         return purchaseAmount;
+    }
+
+    public List<Integer> getWinNumList(String input) {
+        List<Integer> winNumList = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return winNumList;
     }
 
 }
