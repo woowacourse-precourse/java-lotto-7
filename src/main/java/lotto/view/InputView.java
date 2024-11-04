ab502e1f9f15201dda3a3e0e2e4d877eb56fbe4c
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 18:03:47 +0900

    feat, docs: 당첨 번호 입력 추가
    - 기능 구현에 따른 README 업데이트

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PURCHASE_PRICE_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
 
     public String promptPurchasePrice() {
         System.out.println(PURCHASE_PRICE_REQUEST);
+
+        return Console.readLine();
+    }
+
+    public String promptWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_REQUEST);
 
         return Console.readLine();
     }
