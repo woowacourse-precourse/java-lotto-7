dca2c1b9cad73f6c2ddb41f10e12c0c331b679c4
한관희 <noop103@naver.com>
2024-11-04 15:49:30 +0900

    feat: Main 클래스에 로또 발매 및 통계 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,54 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoResult;
+import lotto.input.ConsoleInputHandler;
+import lotto.input.InputHandler;
+import lotto.output.ConsoleOutputHandler;
+import lotto.output.OutputHandler;
+import lotto.util.RetryUtil;
+
 public class Application {
+
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+    private final LottoMachine lottoMachine;
+
+    public Application() {
+        this.inputHandler = new ConsoleInputHandler();
+        this.outputHandler = new ConsoleOutputHandler();
+        this.lottoMachine = new LottoMachine();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.run();
+    }
+
+    public void run() {
+        int amount = readPurchaseAmount(inputHandler);
+        List<Lotto> lottos = lottoMachine.purchaseLottos(amount);
+        outputHandler.printLottos(lottos);
+
+        Lotto winningLotto = readWinningNumbers(inputHandler);
+        int bonusNumber = readBonusNumber(inputHandler, winningLotto);
+
+        LottoResult result = new LottoResult(lottos, winningLotto, bonusNumber);
+        outputHandler.printResult(result);
+    }
+
+    private Integer readPurchaseAmount(InputHandler inputHandler) {
+        return RetryUtil.retryOnError(inputHandler::readPurchaseAmount);
+    }
+
+    private Lotto readWinningNumbers(InputHandler inputHandler) {
+        List<Integer> winningNumbers = RetryUtil.retryOnError(inputHandler::readWinningNumbers);
+        return new Lotto(winningNumbers);
+    }
+
+    private Integer readBonusNumber(InputHandler inputHandler, Lotto winningLotto) {
+        return RetryUtil.retryOnError(() -> inputHandler.readBonusNumber(winningLotto));
     }
 }
