dca9970f08e71585e62aa40b12304dd62c0ec70b
jungsehui <jungsehui@naver.com>
2024-11-04 23:33:25 +0900

    test(LottoServiceTest.java): 로또 당첨 결과 수행 중 예외가 발생하지 않는지 테스트

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,4 +1,24 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
 public class LottoServiceTest {
+
+    @DisplayName("결과 집계 중 예외가 발생하지 않는다.")
+    @Test
+    void 로또_당첨_결과를_수행한다() {
+        LottoService lottoService = new LottoService(new InputView(), new OutputView());
+        List<Lotto> purchasedLottos = lottoService.setLottos(1000);
+
+        assertThatCode(() -> lottoService.calculateResult(
+                purchasedLottos,
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                7
+                )).doesNotThrowAnyException();
+    }
 }
