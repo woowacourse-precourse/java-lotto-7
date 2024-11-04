ebc58d898d90547de72d5ad9df55d66a851e7e03
seunghee <seunghee_chae3@tmax.co.kr>
2024-11-04 19:06:22 +0900

    test(InputView): 로또 금액 정상 입력 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
@@ -17,6 +18,16 @@ public class InputViewTest {
         // 각 테스트 이후 원래 System.in 복원 및 Scanner 종료
         System.setIn(originalIn);
         Console.close();    // Scanner를 종료하여 초기화
+    }
+
+    @Test
+    void 구매_금액_입력_정상_작동() {
+        // given
+        String validInput = "5000\n";
+        System.setIn(new ByteArrayInputStream(validInput.getBytes()));
+
+        // when, then
+        assertEquals(5000, InputView.getPurchaseAmount());
     }
 
     @Test
