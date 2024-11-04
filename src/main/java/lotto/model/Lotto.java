8b0112dc57ec9a6dfad3a153b54c24b45c9fd663
JEN <ekffuclxk@gmail.com>
2024-11-04 19:17:12 +0900

    refactor(Lotto): 미사용 메서드 제거

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,15 +7,8 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
     }
 
     public List<Integer> getNumbers() {
