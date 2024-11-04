62de4ac2ce6dd656ff611bc1cdfc1c5922d15143
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-11-04 18:04:58 +0900

    feat, docs: 보너스 번호 입력 추가
    - 기능 구현에 따른 README 업데이트

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String PURCHASE_PRICE_REQUEST = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_REQUEST = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요";
 
     public String promptPurchasePrice() {
         System.out.println(PURCHASE_PRICE_REQUEST);
@@ -14,6 +15,12 @@ public class InputView {
 
     public String promptWinningNumbers() {
         System.out.println(WINNING_NUMBERS_REQUEST);
+
+        return Console.readLine();
+    }
+
+    public String promptBonusNumber() {
+        System.out.println(BONUS_NUMBER_REQUEST);
 
         return Console.readLine();
     }
