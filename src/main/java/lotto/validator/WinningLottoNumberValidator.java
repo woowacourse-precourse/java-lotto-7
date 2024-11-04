ebe4ad24996701c0225c83666c8c4dbca42aa097
양효빈 <hyobin3026@naver.com>
2024-11-04 09:56:16 +0900

    refactor(WinningLottoNumberValidator): Add method to validate lotto number

--- src/main/java/lotto/validator/WinningLottoNumberValidator.java
+++ src/main/java/lotto/validator/WinningLottoNumberValidator.java
@@ -5,9 +5,20 @@ import lotto.model.lotto.LottoConstant;
 
 import java.util.Set;
 
-public class LottoNumberValidator {
+public class WinningLottoNumberValidator {
 
-    public static void validateRange(Set<Integer> winningLottoNumbers){
+    private final Set<Integer> winningLottoNumbers;
+
+    public WinningLottoNumberValidator(Set<Integer> winningLottoNumbers){
+        this.winningLottoNumbers = winningLottoNumbers;
+    }
+
+    public void validateAll(){
+        validateRange();
+        validateCount();
+    }
+
+    public void validateRange(){
         winningLottoNumbers.stream()
                 .filter(num -> num < LottoConstant.NUMBER_START_INCLUSIVE || num > LottoConstant.NUMBER_END_INCLUSIVE)
                 .findFirst()
@@ -16,9 +27,15 @@ public class LottoNumberValidator {
                 });
     }
 
-    public static void validateCount(Set<Integer> winningLottoNumbers){
+    public void validateCount(){
         if (winningLottoNumbers.size() != LottoConstant.NUMBER_COUNT){
             throw new IllegalArgumentException(LottoException.INVALID_COUNT.getMessage());
+        }
+    }
+
+    public void validateBonusNumber(int bonusNumber){
+        if (winningLottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoException.DUPLICATE_LOTTO_NUMBER.getMessage());
         }
     }
 
