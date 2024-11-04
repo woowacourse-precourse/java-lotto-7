b51e0bd2b86257f15087763545f5c43428f02caa
Chaewon <jamjam12@ajou.ac.kr>
2024-11-04 23:52:14 +0900

    fix : 출력 오류 해결

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -15,11 +15,11 @@ public class Lotto {
 
   private void validate(List<Integer> numbers) {
     if (numbers.size() != 6) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
     }
     Set<Integer> uniqueNumbers = new HashSet<>(numbers);
     if (uniqueNumbers.size() != numbers.size()) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+      throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
     }
   }
 
