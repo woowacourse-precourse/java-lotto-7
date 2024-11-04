b09119ba775d06723f4c5ae5ab138fd2a7c982df
jher235 <tim668666@gmail.com>
2024-11-04 21:51:35 +0900

    styles: 공백 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     public void purchaseLottoMessage(int count, Lottos lottos) {
         StringBuilder stringBuilder = new StringBuilder("\n" + count + "개를 구매했습니다.\n");
 
-        lottos.getLottos().stream()
+        lottos.getLottos()
                 .forEach(lotto -> stringBuilder.append(lotto.getNumbers() + "\n"));
 
         System.out.println(stringBuilder);
@@ -24,17 +24,17 @@ public class OutputView {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
 
-    public void bonusNumberMessage(){
+    public void bonusNumberMessage() {
         System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
-    public void resultMessage(LottoResult lottoResult, int lottoAmount){
+    public void resultMessage(LottoResult lottoResult, int lottoAmount) {
         StringBuilder stringBuilder = new StringBuilder("\n당첨 통계\n---\n");
-        stringBuilder.append("3개 일치 ("+ LottoPrize.FIFTH.getPrice()+"원) - "+lottoResult.prizeCount(LottoPrize.FIFTH)+"개\n")
-                .append("4개 일치 ("+ LottoPrize.FOURTH.getPrice()+"원) - "+lottoResult.prizeCount(LottoPrize.FOURTH)+"개\n")
-                .append("5개 일치 ("+ LottoPrize.THIRD.getPrice()+"원) - "+lottoResult.prizeCount(LottoPrize.THIRD)+"개\n")
-                .append("5개 일치, 보너스 볼 일치 ("+ LottoPrize.SECOND.getPrice()+"원) - "+lottoResult.prizeCount(LottoPrize.SECOND)+"개\n")
-                .append("6개 일치 ("+ LottoPrize.FIRST.getPrice()+"원) - "+lottoResult.prizeCount(LottoPrize.FIRST)+"개\n")
+        stringBuilder.append("3개 일치 (" + LottoPrize.FIFTH.getPrice() + "원) - " + lottoResult.prizeCount(LottoPrize.FIFTH) + "개\n")
+                .append("4개 일치 (" + LottoPrize.FOURTH.getPrice() + "원) - " + lottoResult.prizeCount(LottoPrize.FOURTH) + "개\n")
+                .append("5개 일치 (" + LottoPrize.THIRD.getPrice() + "원) - " + lottoResult.prizeCount(LottoPrize.THIRD) + "개\n")
+                .append("5개 일치, 보너스 볼 일치 (" + LottoPrize.SECOND.getPrice() + "원) - " + lottoResult.prizeCount(LottoPrize.SECOND) + "개\n")
+                .append("6개 일치 (" + LottoPrize.FIRST.getPrice() + "원) - " + lottoResult.prizeCount(LottoPrize.FIRST) + "개\n")
                 .append(String.format("총 수익률은 %.1f%%입니다.", lottoResult.rateOfReturn(lottoAmount)));
         System.out.println(stringBuilder);
     }
