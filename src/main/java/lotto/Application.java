bd91bcb1028eef2a55e24cae5a18e1cabec80165
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 15:56:58 +0900

    feat(Application): LottoController 실행하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
+
+        LottoController controller = new LottoController();
+        controller.start();
         // TODO: 프로그램 구현
     }
 }
