c445f0ec1169909048bb33c72475c67136183a93
hmlssghm <kwonse326@gmail.com>
2024-11-04 19:54:48 +0900

    fix(Prize): 가격 0 일시 0으로 나누면 NaN이므로 처리 로직 생성

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -11,8 +11,8 @@ public class Prize {
     private static final int THIRD_PRIZE = 1500000;
     private static final int FOURTH_PRIZE = 50000;
     private static final int FIFTH_PRIZE = 5000;
-    private static final List<Integer> prizes = List.of(FIFTH_PRIZE,
-            FOURTH_PRIZE, THIRD_PRIZE, SECOND_PRIZE, FIRST_PRIZE);
+    private static final List<Integer> prizes = List.of(FIRST_PRIZE,
+            SECOND_PRIZE, THIRD_PRIZE, FOURTH_PRIZE, FIFTH_PRIZE);
     private static final int START_INDEX = 0;
     private final List<Integer> rankCount;
     private final int profit;
@@ -33,6 +33,9 @@ public class Prize {
     }
 
     private double calculateProfitRate(int investment, int profit) {
+        if (investment == 0) {
+            return 0;
+        }
         return (double) profit / investment * 100;
     }
 
