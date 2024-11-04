a49b161069fcccd22531147cf041e1b7a05857ee
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 19:40:37 +0900

    fix(InputView): 양수만 입력하도록 한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,13 +7,13 @@ import java.util.regex.Pattern;
 
 public class InputView {
 
-    private final static Pattern NUMBER_ONLY_PATTERN = Pattern.compile("\\d+");
+    private final static Pattern POSITIVE_NUMBER_ONLY_PATTERN = Pattern.compile("[1-9]\\d*");
     private final static Pattern LOTTO_NUMBERS_FORMAT = Pattern.compile("\\d+,\\d+,\\d+,\\d+,\\d+,\\d+");
     private final static String LOTTO_NUMBERS_DELIMITER = ",";
     private final static String PROMPT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.";
     private final static String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private final static String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    private final static String NUMBER_INPUT_EXCEPTION_MESSAGE = "숫자를 입력하셔야 합니다";
+    private final static String NUMBER_INPUT_EXCEPTION_MESSAGE = "양수를 입력하셔야 합니다";
     private final static String LOTTO_FORMAT_EXCEPTION_MESSAGE = "로또 입력 형식과 맞지 않습니다";
 
     public long readPurchaseAmount() {
@@ -40,7 +40,7 @@ public class InputView {
     }
 
     private void validateNumber(String input) {
-        if (!NUMBER_ONLY_PATTERN.matcher(input).matches()) {
+        if (!POSITIVE_NUMBER_ONLY_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(NUMBER_INPUT_EXCEPTION_MESSAGE);
         }
     }
