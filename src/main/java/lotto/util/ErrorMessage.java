0865650eb4155a39404136f79d7c22396ca924e0
023-dev <0_2_3@naver.com>
2024-11-04 17:06:23 +0900

    feat(ErrorMessage): Enum 클래스로 에러 메시지 관리 및 포맷팅 기능 추가

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,25 @@
+package lotto.util;
+
+public enum ErrorMessage {
+
+    INVALID_AMOUNT_BLANK("로또 구입 금액을 입력해야 해요."),
+    INVALID_AMOUNT_FORMAT("로또 구입 금액은 숫자여야 해요."),
+    INVALID_AMOUNT_NATURAL_NUMBER("로또 구입 금액은 0보다 커야 해요."),
+    INVALID_AMOUNT_MIN("로또 구입 금액은 최소 1,000원이어야 해요."),
+    INVALID_AMOUNT_UNIT("로또 구입 금액은 1,000원 단위여야 해요.");
+
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = ERROR_MESSAGE_PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getFormattedMessage(Object... args) {
+        return message.formatted(args);
+    }
+}
