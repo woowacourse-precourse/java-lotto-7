13b210e75a720587211af40ac12ba5976d3d1606
hs98012 <hs98012@daum.net>
2024-11-04 21:43:14 +0900

    [test] 로또번호 정상일 경우 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,16 @@
 package lotto;
 
+import lotto.model.Lotto;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +28,23 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    private LottoService lottoService;
+    @Test
+    void 로또_번호_조합이_정상적으로_생성된다() {
+        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+        int ticketCount = 8;
+
+        List<List<Integer>> lottoNumbers = lottoService.generateLottoTickets(ticketCount);
+
+        assertThat(lottoNumbers).hasSize(ticketCount);
+
+        for (List<Integer> ticket : lottoNumbers) {
+            assertThat(ticket).hasSize(6);
+            assertThat(ticket).allMatch(num -> num >= 1 && num <= 45);
+            Set<Integer> uniqueNumbers = ticket.stream().collect(Collectors.toSet());
+            assertThat(uniqueNumbers).hasSize(6);
+        }
+    }
+
 }
