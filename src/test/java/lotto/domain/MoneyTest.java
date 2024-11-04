c9f3ec5c9b187e33723ab21f375282e5c4d52e7f
JoonKyo <joonkyolee@gmail.com>
2024-11-04 12:29:49 +0900

    test(MoneyTest): 최소 구매 가능 금액보다 작은 금액을 입력하면 예외 처리를 진행하는지 확인하는 테스트 구현

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -16,4 +16,10 @@ public class MoneyTest {
         assertThatThrownBy(() -> Money.from("-35000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 구입_금액이_기준_금액보다_작으면_예외가_발생한다() {
+        assertThatThrownBy(() -> Money.from("500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
