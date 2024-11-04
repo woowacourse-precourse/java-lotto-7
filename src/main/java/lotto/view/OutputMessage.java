103033ac563bbd27f082e42f4734c1bfc32ee91e
Awon01 <bec00@naver.com>
2024-11-04 20:40:17 +0900

    feat(view): 로또번호 출력

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -1,5 +1,13 @@
 package lotto.view;
 
-public class OutputMessage {
+import java.util.List;
 
+public final class OutputMessage {
+    public static void purchaseComplete(int count) {
+        System.out.printf("%d개를 구매했습니다.\n", count);
+    }
+
+    public static void lottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
 }
