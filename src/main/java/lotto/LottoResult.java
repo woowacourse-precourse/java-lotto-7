159bde1b9ea8431e660b0fb0b89552e72c071489
bongj9 <lollypark501@naver.com>
2024-11-04 22:15:50 +0900

    feat(LottoRepository):당첨 결과 계산 & 구매한 로또 번호와 당첨번호 비교 구현

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -15,6 +15,8 @@ public class LottoResult {
         this.purchasedLottos = purchasedLottos;
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+        checkPrizeCount();
+
 
     }
 
@@ -26,4 +28,26 @@ public class LottoResult {
         prizeCount.put(6, 0);
         prizeCount.put(7, 0);
     }
+    //당첨 결과 계산
+    private void calculateResult() {
+        for (Lotto lotto : purchasedLottos) {
+            int matchCount = getMatchCount(lotto);
+            if (matchCount >= 3) {
+                prizeCount.put(matchCount, prizeCount.get(matchCount) + 1);
+            }else if(matchCount == 5 && lotto.getNumbers().contains(bonusNumber)) {
+                prizeCount.put(7, prizeCount.get(7) + 1);
+            }
+        }
+    }
+    //구매한 로또 번호와 당첨번호 비교
+    private int getMatchCount(Lotto lotto) {
+        int matchCount = 0;
+        for (int number : lotto.getNumbers()) {
+            if (winningLotto.getNumbers().contains(number)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }
