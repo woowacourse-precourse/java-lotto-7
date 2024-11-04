edaf4c7b3c84b4539248a452d402274436cc37bd
Koo-EunSung <ku903@naver.com>
2024-11-04 22:56:46 +0900

    Feat: 중복값 검증

--- src/main/java/lotto/validator/NumberValidator.java
+++ src/main/java/lotto/validator/NumberValidator.java
@@ -6,6 +6,7 @@ public class NumberValidator {
     private static final String PREFIX = "[ERROR]";
     private static final String OUT_OF_RANGE_MESSAGE = PREFIX + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
     private static final String ONLY_NUMBER_MESSAGE = PREFIX + " 입력값은 숫자여야 합니다.";
+    private static final String DUPLICATE_MESSAGE = PREFIX + " 로또 번호는 중복되는 숫자가 없어야 합니다.";
 
     private static final Integer MIN = 1;
     private static final Integer MAX = 45;
@@ -28,5 +29,10 @@ public class NumberValidator {
         if (value < MIN || value > MAX) {
             throw new IllegalArgumentException(OUT_OF_RANGE_MESSAGE);
         }
+    }
+
+    public void isDuplicateNumber(List<Integer> number) {
+        if(number.size() != number.stream().distinct().count())
+            throw new IllegalArgumentException(DUPLICATE_MESSAGE);
     }
 }
