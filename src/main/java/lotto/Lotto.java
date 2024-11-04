279b38ea9791a74a282f4efe648022e1a516c2bc
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-04 19:00:47 +0900

    feat: 로또 클래스

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
-public class Lotto {
-    private final List<Integer> numbers;
-
-    public Lotto(List<Integer> numbers) {
+public record Lotto(List<Integer> numbers) {
+    public Lotto {
         validate(numbers);
-        this.numbers = numbers;
+        Collections.sort(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (hasInvalidRange(numbers) || hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 중복되지 않은 숫자여야 합니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasInvalidRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(number -> number < 1 || number > 45);
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+
+    public int matchCount(List<Integer> winningNumbers) {
+        return (int) numbers.stream().filter(winningNumbers::contains).count();
+    }
+
+    public boolean matchBonus(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
 }
