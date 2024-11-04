4b399d7b94107f4eb0af986ed3aeb1874e532896
김정우 <56717798+wookim0719@users.noreply.github.com>
2024-11-04 16:04:41 +0900

    feat(InputHandlerTest): InputHandler 클래스 내의 함수를 테스트한다
    문자열 배열의 값들을 Integer형 List로 반환해주는integerInverter가
    올바르게 작동하는지 테스트한다.

--- src/test/java/lotto/InputHandlerTest.java
+++ src/test/java/lotto/InputHandlerTest.java
@@ -1,4 +1,29 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class InputHandlerTest {
-  
+
+    @Test
+    void 정수변형테스트1() {
+        String[] testValue = {"12", "23", "34"};
+        List<Integer> correctResult = new ArrayList<>(Arrays.asList(12,23,34));
+        InputHandler inputHandler = new InputHandler();
+        List<Integer> result = inputHandler.integerInverter(testValue);
+        assertEquals(result,correctResult);
+    }
+
+    @Test
+    void 정수변형테스트2() {
+        String[] testValue = {"12", "23", "a"};
+        assertThatThrownBy(() -> new InputHandler().integerInverter(testValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
