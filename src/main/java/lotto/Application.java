f140c9c778f2bce27bf429a4fbab59801fcbe268
honga <honga@BOOK-5SF7AISI00>
2024-11-04 22:31:05 +0900

    Feat : 로또 한 장을 의미하는 PurchaseLotto 클래스 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,39 @@
 package lotto;
 
+class PurchaseLotto {
+	Lotto userLotto;
+	int correctNumberCount;
+	int winningGrade;
+
+	public PurchaseLotto(Lotto newLotto) {
+		this.userLotto = newLotto;
+		this.correctNumberCount = 0;
+		this.winningGrade = 0;
+	}
+
+	public Lotto getPurchasedLotto() {
+		return this.userLotto;
+	}
+
+	public int getCorrectNumberCount() {
+		return this.correctNumberCount;
+	}
+
+	public void setCorrectNumberCount(int correctNumberCount) {
+		this.correctNumberCount = correctNumberCount;
+	}
+
+	public int getWinningGrade() {
+		return this.winningGrade;
+	}
+
+	public void setWinningGrade(int winningGrade) {
+		this.winningGrade = winningGrade;
+	}
+}
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		
+	}
 }
