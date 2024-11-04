ceeeb4d10a30735e7f47b1abb381e5e257bc43c7
Yook Halim <yookhalim@Yookui-MacBookPro.local>
2024-11-04 19:33:42 +0900

    fix: lotto number ascending order

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -20,6 +21,7 @@ public class Application {
                 System.out.println(e.getMessage());
             }
         }
+        List<Lotto> lottoList = buyLotto(purchaseMoney);
 
         /*System.out.println("당첨 번호를 입력해 주세요.");
         Console.readLine();
@@ -45,8 +47,10 @@ public class Application {
 
         System.out.println(lottoCount + "개를 구매했습니다.");
         for(int i=0;i<lottoCount;i++){
-            lottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-            System.out.println(lottoList.get(i).getNumbers());
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(lotto.getNumbers());
+            lottoList.add(lotto);
+            System.out.println(lotto.getNumbers());
         }
         return lottoList;
     }
