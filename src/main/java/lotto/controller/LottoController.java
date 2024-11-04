4f3ea90ea1c3187543a7b715e744490aaacd64ff
min429 <lsm990429@gmail.com>
2024-11-04 14:31:49 +0900

    feat: LottoController를 추가한다.

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import static lotto.util.LottoGenerator.*;
+import static lotto.view.InputView.*;
+import static lotto.view.OutputView.*;
+
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+
+public class LottoController {
+	public void run() {
+
+	}
+}
