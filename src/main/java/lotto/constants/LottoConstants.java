3698221670083ea63e8d623de7448ffef0ab6c5f
jungsu <vwx1@naver.com>
2024-11-04 18:33:41 +0900

    feat: 로또 상수 클래스 추가
    - 로또 번호 범위, 가격 및 번호 개수에 대한 상수를 정의한 LottoConstants 클래스를 추가했습니다.
    - 인스턴스화를 방지하기 위해 private 생성자를 사용했습니다.

---
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -0,0 +1,13 @@
+package lotto.constants;
+
+public class LottoConstants {
+
+  public static final int MIN_NUMBER = 1;
+  public static final int MAX_NUMBER = 45;
+  public static final int LOTTO_PRICE = 1000;
+  public static final int NUMBER_COUNT = 6;
+
+  private LottoConstants() {
+    // 인스턴스화 방지
+  }
+}
