2bf99f4e081ee53d6314e01a1b62baba65ec2bbc
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-04 23:23:40 +0900

    feat: 예외상황 추가 검증

--- src/main/java/lotto/validation/AmountValidator.java
+++ src/main/java/lotto/validation/AmountValidator.java
@@ -4,10 +4,21 @@ import lotto.exception.ErrorMessage;
 
 public class AmountValidator {
 
+    public static void validateOnlyNumeric(String input) {
+        if (!hasOnlyDigits(input)) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_AMOUNT_NOT_NUMERIC.toString()); // 예외 메시지 수정
+        }
+    }
+
     public static void validateAmount(int amount) {
         if (isThousand(amount)){
             throw new IllegalArgumentException(ErrorMessage.ERROR_AMOUNT_NOT_DIVISIBLE_BY_THOUSAND.toString());
         }
+    }
+
+    private static boolean hasOnlyDigits(final String input) {
+        return input.chars()
+                .allMatch(Character::isDigit);
     }
 
     private static boolean isThousand(int amount) {
