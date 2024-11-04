b891d5a482592740a6efdff5738ce46c483367c4
sleepyhoon <pokjm99@gmail.com>
2024-11-04 17:56:00 +0900

    fix (OutputView) : 출력 문구를 상수로 관리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,17 +6,19 @@ public class OutputView {
 
     private static final String START_SHOW_RESULT = "당첨 통계";
     private static final String DELIMITER = "---";
+    private static final String PURCHASED_LOTTO_MESSAGE = "개를 구매했습니다.";
+    private static final String NEW_LINE = "\n";
 
     public void displayBuyLottos(Lottos lottos) {
         long lottoAmount = lottos.getSize();
-        System.out.println(lottoAmount + "개를 구매했습니다.");
+        System.out.println(lottoAmount + PURCHASED_LOTTO_MESSAGE);
         System.out.println(lottos.result());
     }
 
     public void displayWinningResult(LottoResult lottoResult) {
         StringBuilder sb = new StringBuilder();
-        sb.append(START_SHOW_RESULT).append("\n");
-        sb.append(DELIMITER).append("\n");
+        sb.append(START_SHOW_RESULT).append(NEW_LINE);
+        sb.append(DELIMITER).append(NEW_LINE);
         sb = lottoResult.result(sb);
         System.out.println(sb);
     }
