a7a726e0cd0bf64ea89b1add574c31ae02bac5ff
jae woo Jung <awfjol2008@gmail.com>
2024-11-04 16:34:03 +0900

    feat: 보너스 번호가 당첨 번호와 중복되지 않는지 검증하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -5,7 +5,18 @@ public class WinningLotto {
     private final LottoNumber bonusNumber;
 
     public WinningLotto(Lotto lotto, LottoNumber bonusNumber) {
+        validate();
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    public void validate() {
+        validateUnique();
+    }
+
+    public void validateUnique() {
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+        }
     }
 }
