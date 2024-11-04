46e149bff8e0566bf77e06bff3c5fa04c6ca06af
양효빈 <hyobin3026@naver.com>
2024-11-04 10:02:46 +0900

    refactor(LottoProfitOutputView): Rename class

---
+++ src/main/java/lotto/view/lottoWinningView/LottoProfitOutputView.java
@@ -0,0 +1,17 @@
+package lotto.view.lottoWinningView;
+
+import lotto.util.NumberFormatter;
+
+public class LottoProfitOutputView {
+
+    private final NumberFormatter numberFormatter;
+
+    public LottoProfitOutputView(NumberFormatter numberFormatter) {
+        this.numberFormatter = numberFormatter;
+    }
+
+    public void showLottoProfitRate(double profitRate){
+        System.out.println("총 수익률은 " + numberFormatter.formatNumber(profitRate) + "%입니다.");
+    }
+
+}
