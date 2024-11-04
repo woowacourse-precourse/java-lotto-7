40f440712c4872d16c9a5dfd4e5fdccfdc56d56e
nsb329 <nsb329@naver.com>
2024-11-04 17:05:33 +0900

    당첨 번호가 1~45이고 중복되지 않은 6개의 번호가 입력되었는지 확인하는 메소드

--- src/main/java/lotto/Win_Num.java
+++ src/main/java/lotto/Win_Num.java
@@ -13,11 +13,13 @@ public class Win_Num {
 		if (putWinNum.contains(",")) {
 			String[] arr = putWinNum.split(",");
 			try {
+				putNumEx(arr);
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 				return winArr();
 			}
 			try {
+				putNumEx2(arr);
 			} catch (IllegalArgumentException e2) {
 				System.out.println(e2.getMessage());
 				return winArr();
@@ -27,6 +29,7 @@ public class Win_Num {
 				winList.add(Integer.parseInt(arr[i].trim()));
 			}
 			try {
+				putNumEx3(winList);
 			} catch (IllegalArgumentException e3) {
 				System.out.println(e3.getMessage());
 				return winArr();
@@ -36,5 +39,26 @@ public class Win_Num {
 		return winArr();
 	}
 
+	public void putNumEx(String[] arr) {
+		if (arr.length != 6) {
+			throw new IllegalArgumentException("[ERROR]당첨 숫자 6개를 입력해주세요.");
+		}
+	}
 
+	public void putNumEx2(String[] arr) {
+		for (int i = 0; i < 6; i++) {
+			if (Integer.parseInt(arr[i]) > 45) {
+				throw new IllegalArgumentException("[ERROR]45이하의 당첨 숫자 6개를 입력해주세요.");
+			}
+		}
+	}
+
+	public void putNumEx3(List<Integer> winList) {
+		for (int i = 0; i < winList.size() - 1; i++) {
+			if (winList.get(i).equals(winList.get(i + 1))) {
+				throw new IllegalArgumentException("[ERROR]중복되지 않은 당첨 숫자 6개를 입력해주세요.");
+			}
+		}
+
+	}
 }
