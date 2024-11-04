c5fc3a815aec0b5b089bd1a79b1b6a6c444a9539
belight2 <chlwnsgur090@gmail.com>
2024-11-04 19:30:20 +0900

    feat(Lotto): 로또 당첨 번호 관리 클래스 추가
    로또 당첨 번호는 6개의 당첨 번호와 보너스 번호를 가지므로 로또 클래스의
    번호 리스트 이외에 보너스 번호 필드가 필요하다.
    
    따라서 당첨 번호를 따로 관리하기 위해 클래스를 생성하였다.

--- src/main/java/model/LottoWinningNumbers.java
+++ src/main/java/model/LottoWinningNumbers.java
@@ -1,2 +1,11 @@
-package model;public class LottoWinningNumbers {
+package model;
+
+public class LottoWinningNumbers {
+    private Lotto winningNumbers;
+    private Integer bonusNumber;
+
+    public LottoWinningNumbers(Lotto winningNumbers, Integer bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
 }
