2e431aad3600a9a9926e60a4ccefe6fc26b7570f
haebyun <bhk4392@gmail.com>
2024-11-04 12:39:06 +0900

    feat(LottoResult): 총 상금 계산 및 수익률 계산 기능 추가
    - calculateProfitRate 메서드 추가: 구입 금액 대비 수익률 계산, 소수점 첫째 자리까지 반올림
    - calculateTotalPrize 메서드 추가: 당첨 등급별 개수를 기반으로 총 상금 계산

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -17,4 +17,16 @@ public class LottoResult {
     public Integer getCountByRank(LottoRank lottoRank) {
         return rankCounts.getOrDefault(lottoRank, 0);
     }
+
+    public Double calculateProfitRate(Integer purchaseAmount) {
+        double rate = (calculateTotalPrize() / (double) purchaseAmount) * 100;
+        return Math.round(rate * 10.0) / 10.0;
+    }
+
+    private Long calculateTotalPrize() {
+        return rankCounts.keySet()
+                .stream()
+                .mapToLong(rank -> (long) rankCounts.get(rank) * rank.getPrize())
+                .sum();
+    }
 }
