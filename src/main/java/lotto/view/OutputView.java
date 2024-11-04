81e9211d1700c7325e928ebce708e0addfcf7639
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-11-04 18:20:49 +0900

    style: 출력 형태 예시대로 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -22,24 +22,24 @@ public class OutputView {
 
         for (int i = 0; i < rankCount.size(); i++) {
             if (i == 0) {
-                String first = "6개 일치 (2,000,000,000원) - %d개".formatted(rankCount.get(i));
-                System.out.println(first);
+                String fifth = "3개 일치 (5,000원) - %d개".formatted(rankCount.get(rankCount.size()-i-1));
+                System.out.println(fifth);
             }
             if (i == 1) {
-                String second = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개".formatted(rankCount.get(i));
-                System.out.println(second);
+                String fourth = "4개 일치 (50,000원) - %d개".formatted(rankCount.get(rankCount.size()-i-1));
+                System.out.println(fourth);
             }
             if (i == 2) {
-                String third = "5개 일치 (1,500,000원) - %d개".formatted(rankCount.get(i));
+                String third = "5개 일치 (1,500,000원) - %d개".formatted(rankCount.size()-i-1);
                 System.out.println(third);
             }
             if (i == 3) {
-                String fourth = "4개 일치 (50,000원) - %d개".formatted(rankCount.get(i));
-                System.out.println(fourth);
+                String second = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개".formatted(rankCount.size()-i-1);
+                System.out.println(second);
             }
             if (i == 4) {
-                String fifth = "3개 일치 (5,000원) - %d개".formatted(rankCount.get(i));
-                System.out.println(fifth);
+                String first = "6개 일치 (2,000,000,000원) - %d개".formatted(rankCount.get(i));
+                System.out.println(first);
             }
         }
         String result = "총 수익률은 %.1f%%입니다.".formatted(earningsRate);
