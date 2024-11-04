3bd989f445bc72a7584b94d33258e57eec6d2f1c
JeongBuBu <zhfl1234@email.com>
2024-11-04 17:23:11 +0900

    검사 메서드 수정

--- src/main/java/Model/LottoPurchase.java
+++ src/main/java/Model/LottoPurchase.java
@@ -3,21 +3,21 @@ package Model;
 import Model.Constant.Constants;
 
 public class LottoPurchase {
-    private int purchaseAmount;
+    private int lottoPurchase;
 
-    public LottoPurchase(int purchaseAmount) {
-        validate(purchaseAmount);
-        this.purchaseAmount = purchaseAmount;
+    public LottoPurchase(int lottoPurchase) {
+        validate(lottoPurchase);
+        this.lottoPurchase = lottoPurchase;
     }
 
-    private void validate(int purchaseAmount) {
-        if (purchaseAmount % Constants.DIVISOR != 0) {
+    private void validate(int lottoPurchase) {
+        if (lottoPurchase % Constants.DIVISOR != 0) {
             throw new IllegalArgumentException(
                     "[ERROR] 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");
         }
     }
 
     public int get() {
-        return purchaseAmount;
+        return lottoPurchase;
     }
 }
