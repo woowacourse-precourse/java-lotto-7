8cad09ebe36525e113e280b78cc06c15d42179fd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 23:13:00 +0900

    refactor(WinnerLottoTest): winningNumbers 입력 방식을 문자열 배열에서 List로 변경
    - `WinnerLotto.from` 메소드의 입력 형식을 문자열 배열에서 `List<Integer>`로 변경
    - 테스트 메소드에서 보너스 번호와 당첨 번호 유효성 검사 개선
    - `LottoValidator`로 중복 검사 메소드 변경

--- src/test/java/lotto/model/WinnerLottoTest.java
+++ src/test/java/lotto/model/WinnerLottoTest.java
@@ -2,7 +2,7 @@ package lotto.model;
 
 import java.util.Arrays;
 import lotto.exception.ErrorMessage;
-import lotto.validator.WinnerLottoValidator;
+import lotto.validator.LottoValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +12,11 @@ class WinnerLottoTest {
 
     @Test
     void 입력된_당첨번호_문자열_배열의_크기가_6이_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6,7"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX.getMessage());
 
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_SIX.getMessage());
     }
@@ -24,11 +24,11 @@ class WinnerLottoTest {
     @DisplayName("숫자 문자열 배열에 숫자 문자열이 1~45사이의 숫자가 아니면 예외가 발생한다.")
     @Test
     void 숫자_문자열_배열에_숫자_문자열이_범위_내_숫자가_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,0"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5, 0)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage() + ": 0");
 
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,46"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage() + ": 46");
     }
@@ -36,27 +36,20 @@ class WinnerLottoTest {
     @DisplayName("입력된 보너스 번호가 1~45사이의 숫자가 아니면 예외가 발생한다.")
     @Test
     void 입력된_보너스_번호가_범위_내_숫자가_아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6").setBonusNumber("0"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)).setBonusNumber(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_BE_BETWEEN_1_AND_45.getMessage() + ": 0");
     }
 
     @Test
     void 입력된_보너스_번호가_당첨번호와_중복되면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6").setBonusNumber("6"))
+        assertThatThrownBy(() -> WinnerLotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)).setBonusNumber(6))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.BONUS_NUMBER_DUPLICATES_WITH_WINNING_NUMBERS.getMessage());
 
-        WinnerLottoValidator validator = new WinnerLottoValidator();
-        assertThatThrownBy(() -> validator.checkForDuplicate(Arrays.asList(1, 2, 3, 4, 5), 5))
+        LottoValidator validator = new LottoValidator();
+        assertThatThrownBy(() -> validator.duplicate(Arrays.asList(1, 2, 3, 4, 5), 5))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_NUMBERS_MUST_NOT_DUPLICATE.getMessage() + ": 5");
-    }
-
-    @Test
-    void 입력된_보너스_번호에_숫자가_아닌값이_포함되면_예외가_발생한다() {
-        assertThatThrownBy(() -> WinnerLotto.from("1,2,3,4,5,6").setBonusNumber("6,"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(ErrorMessage.INPUT_MUST_BE_NUMBER.getMessage());
     }
 }
