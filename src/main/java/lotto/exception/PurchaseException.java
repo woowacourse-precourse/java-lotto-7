70e6903ab8123737621d01c17737eb02c840fd41
yunhacandy <yunhacandy@gmail.com>
2024-11-04 12:34:52 +0900

    feat (InputView): 입력 값이 정수형 범위를 넘을 경우에 대해 예외처리

--- src/main/java/lotto/exception/PurchaseException.java
+++ src/main/java/lotto/exception/PurchaseException.java
@@ -6,6 +6,7 @@ public enum PurchaseException {
     NON_POSITIVE_AMOUNT(ERROR_PREFIX + " 구입 금액은 0보다 커야 합니다."),
     INVALID_UNIT(ERROR_PREFIX + " 구입 금액은 1,000원 단위여야 합니다."),
     EMPTY_INPUT(ERROR_PREFIX + "입력 값이 없습니다."),
+    OUT_OF_INT_RANGE(ERROR_PREFIX+"구입 금액은 정수형 범위를 넘으면 안됩니다."),
     INVALID_FORMAT(ERROR_PREFIX + " 구입 금액은 정수로 입력해야 합니다.");
 
     private final String message;
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.constant.RequestMessage;
+import lotto.exception.PurchaseException;
 
 public class InputView {
     public static int inputPurchaseAmount() {
@@ -13,5 +14,17 @@ public class InputView {
         }
 
         return parseAmount(input);
+    }
+
+    private static int parseAmount(String input) {
+        try {
+            long longAmount = Long.parseLong(input);
+            if (longAmount > Integer.MAX_VALUE || longAmount < Integer.MIN_VALUE) {
+                throw new IllegalArgumentException(PurchaseException.OUT_OF_INT_RANGE.getMessage());
+            }
+            return (int) longAmount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PurchaseException.INVALID_FORMAT.getMessage());
+        }
     }
 }
