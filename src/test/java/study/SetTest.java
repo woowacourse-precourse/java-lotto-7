98c782130e8dc68132d3de990857d0c36cac91fc
zzzyoonnn <dwsrh03@gmail.com>
2024-11-04 17:01:44 +0900

    feat: 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능지를 확인하는 학습 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -36,5 +37,13 @@ public class SetTest {
     @DisplayName("Set의 contains 메소드를 활용해 1,2,3의 값이 존재하는지를 확인하는 학습 테스트")
     public void testSetValue(int index) {
         assertThat(numbers.contains(index)).isTrue();
+    }
+
+    // 요구사항 3
+    @ParameterizedTest
+    @CsvSource(value = {"1, true", "2, true", "3, true", "4, false", "5, false"})
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능지를 확인하는 학습 테스트")
+    public void testSetVariousValue(int index, boolean expected) {
+        assertThat(numbers.contains(index)).isEqualTo(expected);
     }
 }
