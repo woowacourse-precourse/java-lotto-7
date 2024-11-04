6f0189c468fbc4efa212579996f3e67643d7eebb
JoonKyo <joonkyolee@gmail.com>
2024-11-04 18:11:40 +0900

    test(WinNumberTest): 당첨번호에 맞춰 맞은 개수를 잘 판단하는지 확인하는 테스트 구현

--- src/test/java/lotto/domain/WinNumberTest.java
+++ src/test/java/lotto/domain/WinNumberTest.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class WinNumberTest {
     @Test
@@ -12,5 +17,27 @@ public class WinNumberTest {
         BonusNumber bonusNumber = BonusNumber.from("3");
         assertThatThrownBy(() -> WinNumber.of(winningNumber, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateLottoData")
+    void 당첨_번호를_바탕으로_몇_개가_맞았는지_잘_출력한다(Lotto lotto, int expectedMatchCount) {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        BonusNumber bonusNumber = BonusNumber.from("8");
+        WinNumber winNumber = WinNumber.of(winningNumber, bonusNumber);
+
+        int matchCount = winNumber.matchWithLotto(lotto);
+        assertThat(matchCount).isEqualTo(expectedMatchCount);
+    }
+
+    static Stream<Arguments> generateLottoData() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)), 6),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)), 5),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 5),
+                Arguments.of(new Lotto(List.of(3, 4, 5, 6, 7, 8)), 4),
+                Arguments.of(new Lotto(List.of(4, 5, 6, 7, 8, 9)), 3),
+                Arguments.of(new Lotto(List.of(10, 11, 12, 13, 14, 15)), 0)
+        );
     }
 }
