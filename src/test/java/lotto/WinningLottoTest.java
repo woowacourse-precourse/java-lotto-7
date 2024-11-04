827c3e1be939500caacf4a9678aee0a7cd768c60
chyo1 <hyowon3856@gmail.com>
2024-11-04 13:19:26 +0900

    test : 당첨 번호, 보너스 번호 일치 여부 확인 기능 테스트 구현

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @Test
+    public void 당첨_번호_일치_여부_확인() throws Exception {
+    	//given
+        List<Integer> testWinningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int testBonusNumber = 7;
+
+        WinningLotto testWinningLotto = WinningLotto.of(testWinningNumber, testBonusNumber);
+        int testNumber = 3;
+        boolean expectedResult = true;
+
+    	//when
+        boolean actualResult = testWinningLotto.isWinningNumber(testNumber);
+
+        //then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void 보너스_번호_일치_여부_확인() throws Exception {
+        //given
+        List<Integer> testWinningNumber = List.of(1, 2, 3, 4, 5, 6);
+        int testBonusNumber = 7;
+
+        WinningLotto testWinningLotto = WinningLotto.of(testWinningNumber, testBonusNumber);
+        int testNumber = 7;
+        boolean expectedResult = true;
+
+        //when
+        boolean actualResult = testWinningLotto.isBonusNumber(testNumber);
+
+        //then
+        assertEquals(expectedResult, actualResult);
+    }
+}
