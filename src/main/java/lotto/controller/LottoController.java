0f2297e2ca852d808513b04bea3d8f475476037b
신동현 <donghyeun02@gmail.com>
2024-11-04 21:08:00 +0900

    feat : 로또 갯수 출력 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,20 @@
 package lotto.controller;
 
+import lotto.util.PurchaseValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
     public void run() {
-        OutputView.printPurchaseAmountMsg();
-        int purchaseAmount = InputView.getPurchaseAmount();
+        try {
+            OutputView.printPurchaseAmountMsg();
+            int purchaseAmount = InputView.getPurchaseAmount();
+            PurchaseValidator.validatePurchase(purchaseAmount);
+
+            int lottosCount = purchaseAmount / 1000;
+            OutputView.printLottoCount(lottosCount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printPurchaseAmountMsg() {
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public static void printLottoCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
 }
