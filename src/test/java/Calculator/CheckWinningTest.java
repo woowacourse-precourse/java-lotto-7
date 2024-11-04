2b81c803081b373503e6f9ed3674b67c839a1f08
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-04 17:31:46 +0900

    test(로또 당첨 계산 기능): fix test code for methods check of 로또 당첨 계산 기능

--- src/test/java/Calculator/CheckWinningTest.java
+++ src/test/java/Calculator/CheckWinningTest.java
@@ -4,13 +4,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import lotto.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("당첨 번호와 로또 번호 비교 테스트")
 public class CheckWinningTest {
+
+    @Test
+    @DisplayName("로또 당첨 확인 테스트")
+    public void checkTest() {
+        //given
+        List<Lotto> testLottos = new ArrayList<>();
+        HashSet<Integer> testWinningNumbers = new HashSet<>();
+        int bonusNumber = 7;
+
+        List<Integer> testLottoOne = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> testLottoTwo = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 7));
+        List<Integer> testLottoThree = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 8));
+
+        testLottos.add(new Lotto(testLottoOne));
+        testLottos.add(new Lotto(testLottoTwo));
+        testLottos.add(new Lotto(testLottoThree));
+
+        testWinningNumbers.add(1);
+        testWinningNumbers.add(2);
+        testWinningNumbers.add(3);
+        testWinningNumbers.add(4);
+        testWinningNumbers.add(5);
+        testWinningNumbers.add(6);
+
+        //when
+        CheckWinning checkWinning = new CheckWinning();
+        var result = checkWinning.check(testLottos, testWinningNumbers, bonusNumber);
+
+        //then
+        HashMap<Integer, Integer> expected = new HashMap<>();
+        expected.put(1, 1);
+        expected.put(2, 1);
+        expected.put(3, 1);
+        expected.put(4, 0);
+        expected.put(5, 0);
+
+        assertEquals(expected, result);
+    }
 
     @Test
     @DisplayName("등수 확인 테스트")
