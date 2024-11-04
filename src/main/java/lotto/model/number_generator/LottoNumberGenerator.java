5c158a68b453a803dae37ca4245c8689079fb411
likerhythm <wjdalsdl0412@gmail.com>
2024-11-04 10:54:07 +0900

    refactor(constant): 불필요 상수 제거

--- src/main/java/lotto/model/number_generator/LottoNumberGenerator.java
+++ src/main/java/lotto/model/number_generator/LottoNumberGenerator.java
@@ -9,10 +9,9 @@ public class LottoNumberGenerator implements NumberGenerator{
 
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
-    private static final int LENGTH = Lotto.NUMBER_COUNT;
 
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LENGTH);
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, Lotto.NUMBER_COUNT);
     }
 }
