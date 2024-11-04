b8e53e1474b124f96e7c88f69413327455b2343f
wouldu <jhw664089@gmail.com>
2024-11-04 21:54:21 +0900

    refactor: Constants 클래스에서 PRIZES 배열 제거

--- src/main/java/lotto/constants/Constants.java
+++ src/main/java/lotto/constants/Constants.java
@@ -5,5 +5,4 @@ public class Constants {
     public static final int LOTTO_NUMBER_COUNT = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
-    private static final int[] PRIZES = {0, 2000000000, 30000000, 1500000, 50000, 5000};
 }
