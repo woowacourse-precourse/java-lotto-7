b306e4a0ef54ef2fd9d2496c1c2e56531217b3e2
jaehyeonchoi <asia0413@konkuk.ac.kr>
2024-11-04 14:16:21 +0900

    refactor(LottoTickets, LottoNumbers, Validator, Application) : Application의 기능을 클래스들로 분리
    LottoTickets
    - 발행된 로또들을 의미하는 클래스
    - 구입 금액을 입력 받아, 로또를 개수만큼 생성하는 과정을 담당
    
    LottoNumbers
    - 당첨 번호, 보너스 번호를 로또 번호를 의미하는 클래스
    - 당첨 번호와 보너스 번호를 입력 받는 과정을 담당
    
    Validator
    - 각종 validate() 메서드를 모아둔 클래스

---
+++ src/main/java/lotto/LottoNumbers.java
@@ -0,0 +1,86 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
+import static constant.Message.INPUT_REQUEST_BONUS_NUMBER;
+import static constant.Message.INPUT_REQUEST_WINNING_NUMBERS;
+
+public class LottoNumbers {
+    private ArrayList<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public void init() {
+        winningNumbers = initWinningNumbers();
+        bonusNumber = initBonusNumber(winningNumbers);
+    }
+
+    private ArrayList<Integer> initWinningNumbers() {
+        while (true) {
+            String winningNumbersInput = getInputString(INPUT_REQUEST_WINNING_NUMBERS.getMessage());
+            String[] winningNumbersInputSplits = winningNumbersInput.split(",");
+            try {
+                return getValidatedWinningNumbers(winningNumbersInputSplits);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private ArrayList<Integer> getValidatedWinningNumbers(String[] winningNumbersInputSplits) {
+        validateWinningNumbers(winningNumbersInputSplits);
+        return parseIntWinningNumbers(winningNumbersInputSplits);
+    }
+
+    private void validateWinningNumbers(String[] winningNumbersInputSplits) {
+        Validator.validateWinningNumbersCount(winningNumbersInputSplits);
+        Validator.validateNumericStrings(winningNumbersInputSplits);
+        ArrayList<Integer> winningNumbers = parseIntWinningNumbers(winningNumbersInputSplits);
+        Validator.validateLottoNumbersInRange(winningNumbers);
+        Validator.validateUniqueNumbers(winningNumbers);
+    }
+
+    private ArrayList<Integer> parseIntWinningNumbers(String[] winningNumbersInputSplits) {
+        ArrayList<Integer> winningNumbers = new ArrayList<>();
+        for (String winningNumbersInputSplit : winningNumbersInputSplits) {
+            winningNumbers.add(Integer.parseInt(winningNumbersInputSplit));
+        }
+        return winningNumbers;
+    }
+
+    private int initBonusNumber(ArrayList<Integer> winningNumbers) {
+        while (true) {
+            String bonusNumberInput = getInputString(INPUT_REQUEST_BONUS_NUMBER.getMessage());
+            try {
+                return getValidatedBonusNumber(winningNumbers, bonusNumberInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int getValidatedBonusNumber(ArrayList<Integer> winningNumbers, String bonusNumberInput) {
+        validateBonusNumber(winningNumbers, bonusNumberInput);
+        return Integer.parseInt(bonusNumberInput);
+    }
+
+    private void validateBonusNumber(ArrayList<Integer> winningNumbers, String bonusNumberInput) {
+        Validator.validateNumericString(bonusNumberInput);
+        int bonusNumber = Integer.parseInt(bonusNumberInput);
+        Validator.validateLottoNumberInRange(bonusNumber);
+        Validator.validateNewNumber(winningNumbers, bonusNumber);
+    }
+
+    private String getInputString(String message) {
+        System.out.println("\n" + message);
+        return Console.readLine();
+    }
+
+    public ArrayList<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,70 +1,85 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import static constant.Message.*;
 
 public class LottoResult {
+    private static final int RANK_1 = 1;
+    private static final int RANK_2 = 2;
+    private static final int RANK_3 = 3;
+    private static final int RANK_4 = 4;
+    private static final int RANK_5 = 5;
+    private static final int UNRANKED = -1;
+    private static final int RANK_1_THRESHOLD = 6;
+    private static final int RANK_3_THRESHOLD = 5;
+    private static final int RANK_4_THRESHOLD = 4;
+    private static final int RANK_5_THRESHOLD = 3;
+    private static final int PRICE_UNIT = 1000;
+    private static final int PERCENTAGE = 100;
+    private static final int PRIZE_RANK_5 = 5000;
+    private static final int PRIZE_RANK_4 = 50000;
+    private static final int PRIZE_RANK_3 = 1500000;
+    private static final int PRIZE_RANK_2 = 30000000;
+    private static final int PRIZE_RANK_1 = 2000000000;
+
 
     private Map<Integer, Integer> rankCount;
-    private final ArrayList<Lotto> lottoTickets;
-    private final ArrayList<Integer> winningNumbers;
-    private final int bonusNumber;
+    private final LottoTickets lottoTickets;
+    private final LottoNumbers lottoNumbers;
 
-    public LottoResult(ArrayList<Lotto> lottoTickets, ArrayList<Integer> winningNumbers, int bonusNumber) {
+    public LottoResult(LottoTickets lottoTickets, LottoNumbers lottoNumbers) {
         this.lottoTickets = lottoTickets;
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+        this.lottoNumbers = lottoNumbers;
         initRankCount();
         checkResult();
     }
 
     private void initRankCount() {
         rankCount = new HashMap<>();
-        for (int rank = 1; rank <= 5; rank++) {
+        for (int rank = RANK_1; rank <= RANK_5; rank++) {
             rankCount.put(rank, 0);
         }
     }
 
     public void checkResult() {
-        for (Lotto lottoTicket : lottoTickets) {
-            int matchCount = lottoTicket.getMatchCount(winningNumbers);
-            boolean hasBonusNumber = lottoTicket.hasBonusNumber(bonusNumber);
+        for (Lotto lottoTicket : lottoTickets.getList()) {
+            int matchCount = lottoTicket.getMatchCount(lottoNumbers.getWinningNumbers());
+            boolean hasBonusNumber = lottoTicket.hasBonusNumber(lottoNumbers.getBonusNumber());
             update(matchCount, hasBonusNumber);
         }
     }
 
     public void update(int matchCount, boolean hasBonusNumber) {
         int rank = getRank(matchCount, hasBonusNumber);
-        if (rank != -1) {
+        if (rank != UNRANKED) {
             rankCount.put(rank, rankCount.get(rank) + 1);
         }
     }
 
     private int getRank(int matchCount, boolean hasBonusNumber) {
-        if (matchCount == 6)
-            return 1;
-        if (matchCount == 5 && hasBonusNumber)
-            return 2;
-        if (matchCount == 5)
-            return 3;
-        if (matchCount == 4)
-            return 4;
-        if (matchCount == 3)
-            return 5;
-        return -1;
+        if (matchCount == RANK_1_THRESHOLD)
+            return RANK_1;
+        if (matchCount == RANK_3_THRESHOLD && hasBonusNumber)
+            return RANK_2;
+        if (matchCount == RANK_3_THRESHOLD)
+            return RANK_3;
+        if (matchCount == RANK_4_THRESHOLD)
+            return RANK_4;
+        if (matchCount == RANK_5_THRESHOLD)
+            return RANK_5;
+        return UNRANKED;
     }
 
     public void print() {
         System.out.println("\n" + PRINT_LOTTO_STATICS.getMessage());
         System.out.println(PRINT_LINE.getMessage());
-        System.out.println(FIFTH_RANK.getMessage() + rankCount.get(5) + SUFFIX_RANK_COUNT.getMessage());
-        System.out.println(FOURTH_RANK.getMessage() + rankCount.get(4) + SUFFIX_RANK_COUNT.getMessage());
-        System.out.println(THIRD_RANK.getMessage() + rankCount.get(3) + SUFFIX_RANK_COUNT.getMessage());
-        System.out.println(SECOND_RANK.getMessage() + rankCount.get(2) + SUFFIX_RANK_COUNT.getMessage());
-        System.out.println(FIRST_RANK.getMessage() + rankCount.get(1) + SUFFIX_RANK_COUNT.getMessage());
+        System.out.println(FIFTH_RANK.getMessage() + rankCount.get(RANK_5) + SUFFIX_RANK_COUNT.getMessage());
+        System.out.println(FOURTH_RANK.getMessage() + rankCount.get(RANK_4) + SUFFIX_RANK_COUNT.getMessage());
+        System.out.println(THIRD_RANK.getMessage() + rankCount.get(RANK_3) + SUFFIX_RANK_COUNT.getMessage());
+        System.out.println(SECOND_RANK.getMessage() + rankCount.get(RANK_2) + SUFFIX_RANK_COUNT.getMessage());
+        System.out.println(FIRST_RANK.getMessage() + rankCount.get(RANK_1) + SUFFIX_RANK_COUNT.getMessage());
         printEarningRate();
     }
 
@@ -73,19 +88,23 @@ public class LottoResult {
     }
 
     public double getEarningRate() {
-        int purchaseAmount = lottoTickets.size() * 1000;
+        int purchaseAmount = lottoTickets.size() * PRICE_UNIT;
         int earning = getEarning();
-        double earningRate = (double) earning /purchaseAmount * 100;
-        return Math.round(earningRate * 10) / 10.0;
+        double earningRate = (double) earning /purchaseAmount * PERCENTAGE;
+        return roundToOneDecimal(earningRate);
+    }
+
+    private static double roundToOneDecimal(double value) {
+        return Math.round(value * 10) / 10.0;
     }
 
     public int getEarning() {
         int earning = 0;
-        earning += 5000 * rankCount.get(5);
-        earning += 50000 * rankCount.get(4);
-        earning += 1500000 * rankCount.get(3);
-        earning += 30000000 * rankCount.get(2);
-        earning += 2000000000 * rankCount.get(1);
+        earning += PRIZE_RANK_5 * rankCount.get(RANK_5);
+        earning += PRIZE_RANK_4 * rankCount.get(RANK_4);
+        earning += PRIZE_RANK_3 * rankCount.get(RANK_3);
+        earning += PRIZE_RANK_2 * rankCount.get(RANK_2);
+        earning += PRIZE_RANK_1 * rankCount.get(RANK_1);
         return earning;
     }
 }
