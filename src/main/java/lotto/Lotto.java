d3e5b754fd5fecd450249feeaaf4f7e3129db2b9
hnapil <hanpil00@naver.com>
2024-11-04 23:38:22 +0900

    feat: Lotto 클래스 추가 및 6개의 중복되지 않은 숫자 검증 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,19 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream().sorted().toList();
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
