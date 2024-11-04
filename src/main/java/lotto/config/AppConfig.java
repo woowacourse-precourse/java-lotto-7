eaceeb43068ee67d1bb64876e1362093b7dde92e
FhRh <yiyop@naver.com>
2024-11-04 17:49:13 +0900

    fix(AppConfig) : import 수정 및 코드 포맷팅

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,16 +1,18 @@
 package lotto.config;
 
-import javax.swing.text.View;
 import lotto.controller.LottoController;
+import lotto.view.CliView;
+import lotto.view.View;
 
 public class AppConfig {
+
     private final View view;
 
-    public AppConfig(){
+    public AppConfig() {
         this.view = new CliView();
     }
 
-    public LottoController getController(){
+    public LottoController getController() {
         return new LottoController(view);
     }
 
