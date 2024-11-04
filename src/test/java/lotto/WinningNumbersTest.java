ead61813e1e2c61414f901e22a71b888b38e585d
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 21:40:38 +0900

    Test: 당첨 번호가 중복된다면 예외 발생하는지 테스트

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static lotto.service.exception.LottoExceptionMessage.INVALID_WINNING_NUMBERS;
+import static lotto.service.exception.LottoExceptionMessage.WINNING_NUMBERS_DUPLICATED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -63,5 +64,16 @@ public class WinningNumbersTest {
             WinningNumbersValidator.validate(winningNumbers);
         });
         assertEquals(INVALID_WINNING_NUMBERS.message(), e.getMessage());
+    }
+
+    @Test
+    void 당첨_번호가_중복된다면_예외발생() {
+        // given
+        String winningNumbers = "1,2,3,4,5,5";
+        // when & then
+        LottoException e = assertThrows(LottoException.class, () -> {
+            WinningNumbersValidator.validate(winningNumbers);
+        });
+        assertEquals(WINNING_NUMBERS_DUPLICATED.message(), e.getMessage());
     }
 }
