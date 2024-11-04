997cd9c5e17e5f39b8b4bf3c5d555e342e099117
ssuukko <tlstpstpdl11@gmail.com>
2024-11-04 22:12:37 +0900

    Refactor: OutputView 클래스의 출력 형식 개선
    - 출력 형식을 개선하여 가독성을 높였습니다.
    - 사용자에게 제공되는 정보가 더욱 명확하게 전달될 수 있도록 하였습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,38 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OutputView {
+
+    public static void printLottoNumbers(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            List<Integer> sortedNumbers = new ArrayList<>(lotto.getSortedNumbers());
+            Collections.sort(sortedNumbers);
+            System.out.println(sortedNumbers);
+        }
+    }
 
     public static void printLottoResult(int[] winningCounts, double profitRate) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + winningCounts[0] + "개");
-        System.out.println("4개 일치 (50,000원) - " + winningCounts[1] + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + winningCounts[2] + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningCounts[3] + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + winningCounts[4] + "개");
+
+        String[] messages = {
+                "3개 일치 (5,000원) - " + winningCounts[4] + "개",
+                "4개 일치 (50,000원) - " + winningCounts[3] + "개",
+                "5개 일치 (1,500,000원) - " + winningCounts[2] + "개",
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningCounts[1] + "개",
+                "6개 일치 (2,000,000,000원) - " + winningCounts[0] + "개"
+        };
+
+        for (String message : messages) {
+            System.out.println(message);
+        }
+
         System.out.println("총 수익률은 " + profitRate + "%입니다.");
     }
 }
