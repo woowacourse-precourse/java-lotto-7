d3d1abd26b5dda869a26508952ee12b3d4cacf46
이준범 <jach1206@naver.com>
2024-11-04 20:11:04 +0900

    feat: 컨트롤러의 반환값을 출력 뷰로 전달해주는 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import java.util.List;
+import lotto.controller.LottoController;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        OutputView outputView = new OutputView();
+        LottoController controller = new LottoController();
+
+        // 로또 구매
+        PurchaseAmount purchaseAmount = controller.buyLotto();
+
+        // 로또 발행
+        List<Lotto> lottos = controller.issueLotto(purchaseAmount);
+        outputView.printLottoNumber(lottos, purchaseAmount.getCanBuyLottoCount());
+
+        // 당첨 번호 발행
+        controller.issueWinningNumber();
+        BonusNumber bonusNumber = controller.issueBonusNumber();
+
+        // 당첨 결과 계산
+        controller.calculateWinning(lottos, bonusNumber);
+
+        outputView.printWinningInformation();
+        outputView.printReturnOfRate(controller.calculateRateOfReturn(purchaseAmount));
     }
 }
