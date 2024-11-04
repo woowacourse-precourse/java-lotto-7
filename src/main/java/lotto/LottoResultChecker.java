f9ffbe254ef4c2a723803c55c39d5ab2c33dfc0b
문혜교 <3hilney@gmail.com>
2024-11-04 15:50:36 +0900

    feat: 등수 적용 기능 구현

--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class LottoResultChecker {
 
     private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
 
     public LottoResultChecker(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     private int countMatchingNumbers(List<Integer> userNumbers) {
@@ -19,5 +21,27 @@ public class LottoResultChecker {
             }
         }
         return matchCount;
+    }
+
+    public MatchCountMessage checkRanking(List<Integer> userNumbers) {
+        int matchCount = countMatchingNumbers(userNumbers);
+        boolean bonusMatch = userNumbers.contains(bonusNumber);
+
+        if (matchCount == 6) {
+            return MatchCountMessage.SIX_MATCH;
+        }
+        if (matchCount == 5 && bonusMatch) {
+            return MatchCountMessage.FIVE_MATCH_WITH_BONUS;
+        }
+        if (matchCount == 5) {
+            return MatchCountMessage.FIVE_MATCH;
+        }
+        if (matchCount == 4) {
+            return MatchCountMessage.FOUR_MATCH;
+        }
+        if (matchCount == 3) {
+            return MatchCountMessage.THREE_MATCH;
+        }
+        return null;
     }
 }
