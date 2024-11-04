45302f5dee4406674ce94e8b31bbd86f04530b71
SongJaeHoonn <thdwogns121@naver.com>
2024-11-04 15:45:11 +0900

    refactor: inputView 반환 타입 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static void inputLottoPurchase() {
+    public static String inputLottoPurchase() {
         System.out.println("구입 금액을 입력해주세요.");
-        Console.readLine();
+        return Console.readLine();
     }
 
-    public static void inputLottoNumber() {
+    public static String inputLottoNumber() {
         System.out.println("당첨 번호를 입력해주세요.");
-        Console.readLine();
+        return Console.readLine();
     }
 
-    public static void inputBonusNumber() {
+    public static String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해주세요");
-        Console.readLine();
+        return Console.readLine();
     }
 }
