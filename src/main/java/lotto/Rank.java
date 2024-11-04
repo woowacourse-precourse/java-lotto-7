fa433a40d189549fa85062b363ded16c2a999546
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 16:19:54 +0900

    feat(Rank): 일치하는 번호의 수를 비교하여 등수를 알 수 있다

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(2, 0);
+
+    private final int matchCount;
+    private final Money prize;
+
+    Rank(int matchCount, long prize) {
+        this.matchCount = matchCount;
+        this.prize = new Money(prize);
+    }
+
+    public static Rank of(int matchCount, boolean hasBonusNumber) {
+        if (isThird(matchCount, hasBonusNumber)) {
+            return THIRD;
+        }
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private static boolean isThird(int matchCount, boolean hasBonusNumber) {
+        return matchCount == THIRD.matchCount && !hasBonusNumber;
+    }
+}
