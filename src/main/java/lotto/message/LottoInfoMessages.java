1b649d2d591b3c260b9557839b1502a39d332c2b
byeongdoohan <quden04@gmail.com>
2024-11-04 18:01:49 +0900

    style: 출력 문구 수정

--- src/main/java/lotto/message/LottoInfoMessages.java
+++ src/main/java/lotto/message/LottoInfoMessages.java
@@ -13,7 +13,6 @@ public enum LottoInfoMessages {
             (수동 구매 갯수가 [\s"""),
     INSERT_NUMBERS_END(" ]개 남았습니다.)"),
     TOTAL_BUY_AMOUNT("""
-        개를 구매했습니다.
         [구입 번호 목록]"""),
     INSERT_WINNING_NUMBER("""
             6개의 당첨 번호를 입력해 주세요. (1~45 사이의 겹치지 않는 6개의 숫자를 쉼표로 구분해 입력, Enter를 누르면 자동으로 번호를 입력합니다.)
@@ -21,8 +20,8 @@ public enum LottoInfoMessages {
     INSERT_BONUS_NUMBER("""
             보너스 번호를 입력해 주세요. (입력된 당첨번호를 제외한 1개의 번호를 입력해주세요. Enter를 누르면 자동으로 번호를 입력합니다.)
             """),
-    WINNER_NUMBERS("[당첨 번호] : "),
-    BONUS_NUMBER("[보너스 번호] : "),
+    WINNER_NUMBERS("[당첨 번호]"),
+    BONUS_NUMBER("[보너스 번호]"),
     ;
 
     private String text;
