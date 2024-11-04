eef86deacf14b6f147bd503375eaa0fbc0d303ff
aprilnineteen <dencoding@naver.com>
2024-11-04 23:49:02 +0900

    fix: 상수 String 내용 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT = "구입 금액을 입력해 주세요.";
-    private static final String WINNING_NUMBERS_INPUT = "당첨 번호를 입력해 주세요";
+    private static final String WINNING_NUMBERS_INPUT = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
 
     public String readPurchaseAmount() {
