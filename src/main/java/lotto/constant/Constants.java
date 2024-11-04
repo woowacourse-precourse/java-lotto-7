2ad073eb0ffaded93254e33c7454872a695a13ef
yunhacandy <yunhacandy@gmail.com>
2024-11-04 18:43:25 +0900

    feat (Constants): 통계 초기값 상수화

--- src/main/java/lotto/constant/Constants.java
+++ src/main/java/lotto/constant/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final int LOTTO_PURCHASE_PRICE = 1000;
     public static final int MINIMUM_LOTTO_AMOUNT = 0;
     public static final int NO_REMAINDER = 0;
+    public static final int DEFAULT_STATISTIC_COUNT = 0;
     public static final String DELIMITER = ",";
     public static final String ERROR_PREFIX = "[ERROR]";
 }
