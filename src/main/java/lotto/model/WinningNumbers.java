cb427d4a6db9137f5ca957eb3ebb919675d92671
jisukimtech <jisukim.tech@gmail.com>
2024-11-04 22:28:45 +0900

    feat(WinningNumbers): 당첨 번호에 중복이 있는 경우에 대한 예외 처리 추가

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -1,13 +1,29 @@
 package lotto.model;
 
+import static lotto.model.constant.ErrorMessage.DUPLICATE_LOTTO_NUMBER;
+
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumbers {
     private final List<Integer> numbers;
 
     public WinningNumbers(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (hasDuplicateNumbers(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER);
+        }
+    }
+
+    private boolean hasDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() < numbers.size();
     }
 
     public static WinningNumbers from(List<Integer> numbers) {
--- src/main/java/lotto/model/constant/ErrorMessage.java
+++ src/main/java/lotto/model/constant/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String INVALID_PURCHASE_AMOUNT_FORMAT = "로또 구입 금액은 정수 형태여야 합니다.";
     public static final String NOT_MULTIPLE_OF_1000 = "로또 구입 금액은 1000의 배수여야 합니다.";
     public static final String INVALID_LOTTO_NUMBER_FORMAT = "로또 번호는 정수 형태여야 합니다.";
+    public static final String DUPLICATE_LOTTO_NUMBER = "당첨 번호는 중복될 수 없습니다.";
 }
