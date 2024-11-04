cd86356e90bea5e579f76312ca36eb566fdddfe6
AndBlack99 <jwoo1687@icloud.com>
2024-11-04 22:08:01 +0900

    feat:보너스 번호 발행 기능 추가
      - getBonusNumber함수
      - validateBonusNumber함수
       : 보너스 번호를 콘솔입력으로 받은 후 올바른 입력인지 검사한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Set;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class Application {
 	private static final int LOTTO_PRICE = 1000;
@@ -17,6 +19,7 @@ public class Application {
 		    	int lottoCount = amount / LOTTO_PRICE;
 	            Lotto[] purchasedLottos = generateLottos(lottoCount);
 	            printLottoNumbers(purchasedLottos);
+	            Set<Integer> winningNumbers = getWinningNumbers();
 		        break;
 	    	} catch (IllegalArgumentException e) {
 	    		System.out.println("[ERROR] " + e.getMessage());
@@ -66,7 +69,55 @@ public class Application {
 		Collections.sort(sortedLotto);
 		System.out.println(sortedLotto);
 	}
+	
+	public static Set<Integer> getWinningNumbers() {
+		while (true) {
+            System.out.println("당첨 번호를 입력해 주세요");
+            String input = Console.readLine();
+            try {
+                return readLottoNumbers(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+	}
+	
+	public static Set<Integer> readLottoNumbers(String input) {
+		String[] numberStrings = input.split(",");
+		Set<Integer> numbers = new HashSet<>();
+		for(String numStr : numberStrings) {
+			validateLottoNumber(numbers, numStr);
+			numbers.add(Integer.parseInt(numStr.trim()));
+		}
+		return numbers;
+	}
+	
+	public static void validateLottoNumber(Set<Integer> numbers, String numStr) {
+		int number = Integer.parseInt(numStr.trim());
+		if(number > MAX_NUMBER || number < 1 || numbers.contains(number))
+			throw new IllegalArgumentException("[ERROR] 로또 번호의 범위 1~45 사이 입니다.");
+	}
     
+	public static int getBonusNumber(Set<Integer> winningNumbers) {
+		while (true) {
+            System.out.println("보너스 번호를 입력해 주세요");
+            String input = Console.readLine();
+            try {
+                int bonusNumber = Integer.parseInt(input);
+                validateBonusNumber(bonusNumber, winningNumbers);
+                return bonusNumber;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효한 숫자를 입력하세요.");
+            }
+        }
+	}
+	
+	public static void validateBonusNumber (int bonusNumber, Set<Integer> winningNumbers) {
+		if(bonusNumber <1 || bonusNumber > MAX_NUMBER)
+			throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 1~45 사이 입니다.");
+		if(winningNumbers.contains(bonusNumber)) 
+			throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 1~45 사이 입니다.");
+	}
     
     
     
