2cba9d1be8c821678c19d1c35eb43f2a927abbeb
JEN <ekffuclxk@gmail.com>
2024-11-04 19:38:38 +0900

    refactor(LottoGenerator): 로또 랜덤 범위, 단위 가격 상수 추가

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -7,14 +7,20 @@ import lotto.model.Lotto;
 import lotto.model.Lottos;
 
 public class LottoGenerator {
+
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_TOTAL_COUNT = 6;
+    private static final int LOTTO_PRICE_UNIT = 1000;
+
     public Lotto generateLotto() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_TOTAL_COUNT);
         return new Lotto(lotto);
     }
 
     public Lottos getnerateLottos(int amount) {
         List<Lotto> lottos = new ArrayList<>();
-        int count = amount / 1000;
+        int count = amount / LOTTO_PRICE_UNIT;
         for (int i = 0; i < count; i++) {
             lottos.add(generateLotto());
         }
