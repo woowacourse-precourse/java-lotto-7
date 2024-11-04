8c181f0c831d212e68a7a8cabbc67309e2f06239
Park - Jeonghwan <990706leo@gmail.com>
2024-11-04 15:27:40 +0900

    feat(Lotto): 다른 로또와 일치하는 번호의 개수를 구할 수 있다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,12 +7,14 @@ import java.util.Set;
 public class Lotto {
 
     private final static int NUMBERS_LENGTH = 6;
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
         validateDuplicate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::new)
+                .toList();
     }
 
     private void validateLength(List<Integer> numbers) {
@@ -26,5 +28,15 @@ public class Lotto {
         if (nonDuplicateNumbers.size() != NUMBERS_LENGTH) {
             throw new IllegalArgumentException("로또 번호는 중복되면 안됩니다.");
         }
+    }
+
+    public int countMatchesWith(Lotto other) {
+        return Math.toIntExact(numbers.stream()
+                .filter(other::contains)
+                .count());
+    }
+
+    public boolean contains(LottoNumber number) {
+        return numbers.contains(number);
     }
 }
