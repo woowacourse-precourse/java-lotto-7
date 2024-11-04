dac5cc7622ace8f4f14e777d1770371bca1a0b1e
JMJ <ghtntkdnsk@naver.com>
2024-11-04 22:33:37 +0900

    feat(Lotto) : 로또 번호에 대한 검증 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validation.Validation;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +12,7 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        Validation.validateLotto(numbers);
     }
 
     public int countMatchingNumbers(List<Integer> winninNumbers) {
