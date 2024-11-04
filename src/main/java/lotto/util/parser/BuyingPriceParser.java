e74ecced33d0aa8e4be813ec944b08de30a5d059
Shinjongyun <tigerjong123@gmail.com>
2024-11-04 21:07:50 +0900

    fix(BuyingPriceParser): 에러 메세지 수정

--- src/main/java/lotto/util/parser/BuyingPriceParser.java
+++ src/main/java/lotto/util/parser/BuyingPriceParser.java
@@ -1,11 +1,13 @@
 package lotto.util.parser;
 
 public class BuyingPriceParser {
+    private static final String INVALID_FORMAT_ERROR = "구입금액은 숫자만 입력 가능합니다.";
+
     public static int toIntStringPriceParser(String buyingPriceInput) throws IllegalArgumentException  {
         try {
             return Integer.parseInt(buyingPriceInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("구입금액은 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
         }
     }
 }
