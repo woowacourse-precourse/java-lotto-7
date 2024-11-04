44b299dc3440056f70b0b2da66993878ee33f586
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:52:29 +0900

    Feat: InputView 가 입력값을 정수형으로 변환하여 반환하도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.view.validation.PriceValidator;
 
 public class InputView {
 
@@ -8,9 +9,9 @@ public class InputView {
     private static final String WINNING_NUMBER_MESSAGE = "\n당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
-    public static String inputPrice() {
+    public static int inputPrice() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
-        return Console.readLine();
+        return PriceValidator.validate(Console.readLine());
     }
 
     public static String winningNumbers() {
