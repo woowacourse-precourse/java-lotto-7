05b6c825bd60d8cc7721ead09cb117c723bee3a5
LeeHaeJun <haejun0410@gmail.com>
2024-11-04 21:36:48 +0900

    feat: 구매한 로또를 출력하는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+
 public class OutputView {
     public static void printPurchaseCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.forEach(lotto -> {
+            String numbers = lotto.getNumbers().stream()
+                    .map(n -> String.valueOf(n.value()))
+                    .collect(Collectors.joining(", ", "[", "]"));
+            System.out.println(numbers);
+        });
     }
 }
