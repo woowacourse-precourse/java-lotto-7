088306b8220a3f15b8af020ccab20a889f756b2c
FhRh <yiyop@naver.com>
2024-11-04 18:48:10 +0900

    style(Lottocontroller) : 주석 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -25,18 +25,21 @@ public class LottoController {
     }
 
     public void run() {
+        //구입금액을 입력받고 로또를 만듭니다.
         String money = view.getMoney();
         Lottos lottos = lottoCreateService.createLottosWithMoney(money);
         view.printLottos(lottos);
 
+        //당첨번호와 보너스번호를 입력받고 당첨로또를 만듭니다.
         String winningNumbers = view.getWinningNumbers();
         String bonusNumber = view.getBonusNumber();
         WinningLotto winningLotto = lottoCreateService.createWinningLotto(winningNumbers, bonusNumber);
 
+        //로또들과 당첨로또를 비교하여 등수를 반환합니다.
         List<LottoRank> lottoRanks = lottoCheckService.checkRanks(winningLotto, lottos);
 
-        LottoStatisticsDto result = lottoStatisticsService.getLottoStatistics(lottoRanks);
+        //등수를 통계로 만들어 출력합니다.
+        LottoStatisticsDto result = lottoStatisticsService.getLottoStatistics(lottoRanks, Integer.parseInt(money));
         view.printResult(result);
-
     }
 }
