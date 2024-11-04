394900a98ecbde5ca814c4a950bca9eecb4e4417
mingyeongwon <wonmingy@gmail.com>
2024-11-04 22:33:06 +0900

    feat: 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,11 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputView {
     private static String getInputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -23,5 +28,40 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
         }
+    }
+
+    private static String getInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        String input = getInputWinningNumbers();
+        return validateWinningNumbers(input);
+    }
+
+    private static List<Integer> validateWinningNumbers(String input) {
+        String[] inputNumbers = input.split(",");
+        if (inputNumbers.length != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+
+        List<Integer> winningNumbers = new ArrayList<>();
+        Set<Integer> uniqueNumbers = new HashSet<>(winningNumbers);
+        for (String number : inputNumbers) {
+            try {
+                int winningNumber = Integer.parseInt(number.trim());
+                if (winningNumber < 1 || winningNumber > 45) {
+                    throw new IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자여야 합니다.");
+                }
+                if (uniqueNumbers.add(winningNumber)) {
+                    throw new IllegalArgumentException("[ERROR] 번호는 중복될 수 없습니다.");
+                }
+                winningNumbers.add(winningNumber);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+            }
+        }
+        return winningNumbers;
     }
 }
