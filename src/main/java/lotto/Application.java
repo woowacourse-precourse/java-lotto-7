29ba68a2e0bc5bed511a7e927bec18a81e03be99
gyuoo <yeomky98@naver.com>
2024-11-05 02:10:34 +0900

    feat(application): update Application to run the lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.factory.LottoTicketStore;
+import lotto.service.LottoGameService;
+import lotto.service.LottoPurchaseService;
+import lotto.service.LottoResultCalculator;
+import lotto.service.LottoStatisticsService;
+import lotto.util.DuplicateValidator;
+import lotto.util.LottoNumberSorter;
+import lotto.util.NumberValidator;
+import lotto.util.PurchaseAmountValidator;
+import lotto.util.RandomNumberGenerator;
+import lotto.util.WinningNumberSeparator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+		LottoNumberSorter lottoNumberSorter = new LottoNumberSorter();
+		LottoTicketStore lottoTicketStore = new LottoTicketStore(randomNumberGenerator, lottoNumberSorter);
+
+		NumberValidator numberValidator = new NumberValidator();
+		DuplicateValidator duplicateValidator = new DuplicateValidator();
+		WinningNumberSeparator winningNumberSeparator = new WinningNumberSeparator();
+		PurchaseAmountValidator purchaseAmountValidator = new PurchaseAmountValidator(numberValidator);
+
+		OutputView outputView = new OutputView();
+		InputView inputView = new InputView(outputView, purchaseAmountValidator, winningNumberSeparator,
+			duplicateValidator);
+
+		LottoPurchaseService lottoPurchaseService = new LottoPurchaseService(inputView, outputView, lottoTicketStore);
+		LottoGameService lottoGameService = new LottoGameService(inputView);
+		LottoResultCalculator lottoResultCalculator = new LottoResultCalculator();
+		LottoStatisticsService lottoStatisticsService = new LottoStatisticsService(lottoResultCalculator, outputView);
+		LottoController lottoController = new LottoController(lottoGameService, lottoPurchaseService,
+			lottoStatisticsService);
+
+		lottoController.play();
+	}
 }
