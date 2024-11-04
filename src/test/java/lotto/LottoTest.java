f8283edb49f584ca657bb73ec454aca3877035d0
Chan Mi <rkdcksal02@gmail.com>
2024-11-04 19:29:12 +0900

    test(LottoTest): 로또 당첨 결과 검증 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoTest {
@@ -49,5 +51,21 @@ class LottoTest {
         assertThatThrownBy(() -> Lotto.validateBonusNumber(winningNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
+    @DisplayName("로또 당첨 결과를 검증")
+    @Test
+    void 로또_당첨_결과_검증() {
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        assertEquals(LottoRank.NONE, lotto.match(Arrays.asList(10, 11, 12, 13, 14, 15), 7), "0개 일치");
+        assertEquals(LottoRank.FIFTH, lotto.match(Arrays.asList(1, 11, 2, 3, 14, 15), 7), "3개 일치");
+        assertEquals(LottoRank.FOURTH, lotto.match(Arrays.asList(1, 2, 12, 3, 4, 15), 7), "4개 일치");
+        assertEquals(LottoRank.THIRD, lotto.match(Arrays.asList(1, 2, 3, 13, 4, 5), 7), "5개 일치");
+
+        assertEquals(LottoRank.SECOND, lotto.match(Arrays.asList(1, 2, 3, 4, 5, 15), 6), "5개 일치 + 보너스 볼 일치");
+
+        assertEquals(LottoRank.FIRST, lotto.match(Arrays.asList(1, 2, 3, 4, 5, 6), 7), "6개 일치");
     }
 }
