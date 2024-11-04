252c18ea9cda2053c7989465287ffbb07c9da6c6
hyeblee <hyeb1224@naver.com>
2024-11-04 16:16:27 +0900

    refactor(view): 입력 Console.readLine()으로 수정
    - System.in -> Console.readLine()

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,13 @@
 package lotto.view;
 
-import java.util.Scanner;
-
-import static lotto.util.NumberUtil.parsePositiveNumber;
+import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
-    Scanner sc = new Scanner(System.in);
 
     public String promptPurchaseAmount(){
         System.out.println("구입금액을 입력해 주세요.");
-        String inputAmount = sc.next();
+        String inputAmount = Console.readLine();
         return inputAmount;
     }
 
