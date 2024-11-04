b6e2376e65927eadd00ebba71fdeee7ccd690853
haebyun <bhk4392@gmail.com>
2024-11-04 15:07:51 +0900

    feat(OutputFormatter): 로또 번호 리스트 출력 포맷팅 메서드 추가
    - formatLottoNumbers 메서드 추가하여 로또 번호 리스트를 형식에 맞춰 반환
    - 각 로또 번호 리스트를 문자열로 변환하고 형식에 맞추도록 구현

--- src/main/java/lotto/view/output/util/OutputFormatter.java
+++ src/main/java/lotto/view/output/util/OutputFormatter.java
@@ -1,9 +1,20 @@
 package lotto.view.output.util;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.dto.response.PurchasedLottoDTO;
+
 public final class OutputFormatter {
     private static final String PURCHASE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.\n";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
 
     public static String formatPurchaseLottoCount(Integer count) {
         return String.format(PURCHASE_LOTTO_COUNT_FORMAT, count);
+    }
+
+    public static String formatLottoNumbers(List<PurchasedLottoDTO> lottos) {
+        return lottos.stream()
+                .map(lotto -> lotto.numbers().toString())
+                .collect(Collectors.joining(LINE_SEPARATOR)) + LINE_SEPARATOR;
     }
 }
