bce9f045b7e42521e771873e385b3b17845f4092
AndBlack99 <jwoo1687@icloud.com>
2024-11-04 22:48:33 +0900

    feat: 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @Test
@@ -20,6 +21,22 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    
+    @DisplayName("로또 번호는 1과 45 사이의 숫자로 구성되어야 한다.")
+    @Test
+    void 로또_번호가_1과_45_사이의_숫자가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("로또 번호가 올바르게 생성되었는지 확인한다.")
+    @Test
+    void 로또_번호가_정상적으로_생성되었는지_확인한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> numbers = lotto.getNumbers();
+        assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
