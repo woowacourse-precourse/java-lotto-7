258cfb455717c834fd356cb2a65863b044061cb1
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 21:19:14 +0900

    feat(view): 총 수익률을 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,14 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
+import lotto.Rank;
 import lotto.model.Lotto;
 
 public class OutputView {
+
+    private final DecimalFormat df = new DecimalFormat("###,###");
 
     public void printPurchasedLottoAmount(int amount) {
         System.out.println(amount + "개를 구매했습니다.");
@@ -13,5 +18,22 @@ public class OutputView {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printWinningStatic(Map<Rank, Integer> lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            System.out.printf(getDisplayFormat(rank), rank.getMatchNumber(),
+                df.format(rank.getPrize()),
+                lottoResult.get(rank));
+        }
+    }
+
+    private String getDisplayFormat(Rank rank) {
+        if (rank == Rank.SECOND) {
+            return "%d개 일치, 보너스 볼 일치 (%s원) - %d개%n";
+        }
+        return "%d개 일치 (%s원) - %d개%n";
     }
 }
