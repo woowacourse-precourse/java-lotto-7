c8a738b047de63d27ce8a4d3cae35cc522262666
shinebyul <qufdl8382@gmail.com>
2024-11-04 17:45:43 +0900

    [Feat] 구입 금액 입력받는 메서드 작성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final String GET_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -17,4 +19,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    // 구입 금액 입력받기
+    public Integer inputPrice(){
+        System.out.println(GET_PRICE_MESSAGE);
+
+        Scanner scanner = new Scanner(System.in);
+        Integer price = scanner.nextInt();
+
+        return price;
+    }
 }
