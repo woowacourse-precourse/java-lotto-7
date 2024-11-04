59fa5d696f112c18114ecd85c8b66855f8611768
young-in <hardworkingini@gmail.com>
2024-11-04 23:40:29 +0900

    feat(Output.java) : 로또 결과 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.WinningResult;
 
 public class OutputView {
     private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
@@ -15,6 +16,7 @@ public class OutputView {
     private static final String FIVE_MATCHING_AND_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
     private static final String SIX_MATCHING = "6개 일치 (2,000,000,000원) - ";
     private static final String PIECES = "개";
+    private static final String NEXT_LINE = "\n";
 
 
     public void printRequestMoney(){
@@ -22,6 +24,7 @@ public class OutputView {
     }
 
     public void printLottoCount(int lottoCount){
+        System.out.println(NEXT_LINE);
         System.out.println(lottoCount + LOTTO_COUNT);
     }
 
@@ -32,10 +35,25 @@ public class OutputView {
     }
 
     public void printRequestWinningNumbers(){
+        System.out.println(NEXT_LINE);
         System.out.println(INPUT_WINNING_NUMBERS);
     }
 
     public void printRequestBonusNumbers(){
+        System.out.println(NEXT_LINE);
         System.out.println(INPUT_BONUS_NUMBER);
+    }
+
+    public void printWinningResult(WinningResult result) {
+        System.out.println(WINNING_STATISTICS_HEADER);
+        System.out.println(THREE_MATCHING + result.getMatchThreeCount() + PIECES);
+        System.out.println(FOUR_MATCHING + result.getMatchFourCount() + PIECES);
+        System.out.println(FIVE_MATCHING + result.getMatchFiveCount() + PIECES);
+        System.out.println(FIVE_MATCHING_AND_BONUS + result.getMatchFiveAndBonusCount() + PIECES);
+        System.out.println(SIX_MATCHING + result.getMatchSixCount() + PIECES);
+    }
+
+    public void printYield(double yield) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
     }
 }
