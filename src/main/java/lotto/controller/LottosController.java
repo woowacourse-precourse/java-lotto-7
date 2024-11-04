5739e8f2e5e67bb425580a2d3e55955066a02c05
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-04 15:53:00 +0900

    feat : 로또 구매 및 결과 처리 로직 구현

--- src/main/java/lotto/controller/LottosController.java
+++ src/main/java/lotto/controller/LottosController.java
@@ -1,4 +1,54 @@
 package lotto.controller;
 
+import lotto.enums.WinningMoney;
+
+import lotto.service.LottoCalculateService;
+import lotto.service.LottoSaveService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.UserInput;
+import lotto.view.UserOutput;
+
+import java.util.Map;
+
 public class LottosController {
+    private final LottoSaveService lottoService = new LottoSaveService();
+    private final LottoCalculateService lottoCalculateService = new LottoCalculateService();
+    UserInput inputView;
+    UserOutput outputView = new OutputView();
+
+    public LottosController(UserInput inputView, UserOutput outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void buy() {
+        int purchaseCount = lottoService.buyLottos(inputView.inputPurchaseMoney());
+        outputView.outputLottoCount(purchaseCount);
+        lottoService.saveLottos(purchaseCount);
+        outputView.outputStatistics(lottoService.getLottos());
+        save();
+    }
+
+    public void save() {
+        String winningNumber = inputView.inputWinningNumbers();
+        String bonusNumber = inputView.inputBonusBumber();
+        lottoService.saveWinningLotto(winningNumber);
+        lottoService.saveBonusNumber(bonusNumber);
+        matchLotto();
+    }
+
+    public void matchLotto(){
+        lottoService.makeDataForReturn();
+        Map<String,Integer> lottoMatchCount = lottoService.matchLotto();
+        outputView.outputMatchResult(lottoMatchCount);
+        calculate(lottoMatchCount);
+    }
+
+    public void calculate(Map<String,Integer> lottoMatchCount) {
+        double profitRate = lottoCalculateService.calculateProfitRate(lottoMatchCount, lottoService.getPurchasePrice());
+        outputView.outputProfitRate(profitRate);
+    }
+
+
 }
