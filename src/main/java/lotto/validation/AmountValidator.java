6a130363956aef7595fdb9d15a2b20c8bcf1a963
Cheoroo <195822@jnu.ac.kr>
2024-11-04 21:17:37 +0900

    refactor: AmountValidator에서 에러 메시지를 ErrorMessages 상수로 대체한다
    - 생성자에서 에러 메시지 인수를 제거하고 상수 참조로 변경

--- src/main/java/lotto/validation/AmountValidator.java
+++ src/main/java/lotto/validation/AmountValidator.java
@@ -1,13 +1,12 @@
 package lotto.validation;
 
+import lotto.utils.ErrorMessages;
+
 public class AmountValidator implements Validator<Integer> {
     private static final int LOTTO_PRICE = 1000;
-    private static final AmountValidator INSTANCE = new AmountValidator("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+    private static final AmountValidator INSTANCE = new AmountValidator();
 
-    private final String errorMessage;
-
-    private AmountValidator(String errorMessage) {
-        this.errorMessage = errorMessage;
+    private AmountValidator() {
     }
 
     public static AmountValidator getInstance() {
@@ -17,7 +16,7 @@ public class AmountValidator implements Validator<Integer> {
     @Override
     public void validate(Integer amount) {
         if (amount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(errorMessage);
+            throw new IllegalArgumentException(ErrorMessages.PURCHASE_AMOUNT_UNIT);
         }
     }
 }
