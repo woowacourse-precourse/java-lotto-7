da0b945cb937ed29433c702e1d46df064cd81c11
egaeng09 <egaeng09@gmail.com>
2024-11-04 22:53:49 +0900

    feat(WinningLotto): 당첨 번호 중 보너스 번호도 중복될 수 없는 예외 기능 추가

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,12 +1,15 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import lotto.utils.ErrorMessage;
 
 public class WinningLotto{
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
+        validateUniqueBonusNumber(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
     }
@@ -17,5 +20,11 @@ public class WinningLotto{
 
     public boolean matchBonusNumber(List<Integer> numbers) {
         return numbers.contains(bonusNumber);
+    }
+
+    public void validateUniqueBonusNumber(Lotto lotto, int bonusNumber) {
+        if (new HashSet<>(lotto.getNumbers()).contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_MUST_BE_UNIQUE_NUMBER.getMessage());
+        }
     }
 }
