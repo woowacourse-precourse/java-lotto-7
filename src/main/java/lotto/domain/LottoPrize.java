ba5120160d7f4c49aaa76faa19d47d4f7020432d
이창근 <dlckdrms0517@gmail.com>
2024-11-04 19:55:48 +0900

    feat: LottoPrize 타입 안정성 강화
    - 당첨 금액을 long으로 타입 수정하여 총 가격 계산등에서 타입 안정성을 강화했습니다.
    - 추가로 린트를 수정하여 가독성이 좋게 되었습니다.

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -5,19 +5,39 @@ import java.util.Optional;
 
 public enum LottoPrize {
 
-    FIFTH_PRIZE(3, 0, 5000, "3개 일치 (5,000원)"),
-    FOURTH_PRIZE(4, 0, 50000, "4개 일치 (50,000원)"),
-    THIRD_PRIZE(5, 0, 1500000, "5개 일치 (1,500,000원)"),
-    SECOND_PRIZE(5, 1, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    FRIST_PRIZE(6, 0, 2000000000, "6개 일치 (2,000,000,000원)"),
+    FIFTH_PRIZE(
+            3, 0,
+            5_000L,
+            "3개 일치 (5,000원)"
+    ),
+    FOURTH_PRIZE(
+            4, 0,
+            50_000L,
+            "4개 일치 (50,000원)"
+    ),
+    THIRD_PRIZE(
+            5, 0,
+            1_500_000L,
+            "5개 일치 (1,500,000원)"
+    ),
+    SECOND_PRIZE(
+            5, 1,
+            30_000_000L,
+            "5개 일치, 보너스 볼 일치 (30,000,000원)"
+    ),
+    FRIST_PRIZE(
+            6, 0,
+            2_000_000_000L,
+            "6개 일치 (2,000,000,000원)"
+    ),
     ;
 
     private final int numberMatch;
     private final int bonusNumberMatch;
-    public final int prizeMoney;
+    public final long prizeMoney;
     public final String description;
 
-    LottoPrize(int numberMatch, int bonusNumberMatch, int prizeMoney, String description) {
+    LottoPrize(int numberMatch, int bonusNumberMatch, long prizeMoney, String description) {
         this.numberMatch = numberMatch;
         this.bonusNumberMatch = bonusNumberMatch;
         this.prizeMoney = prizeMoney;
