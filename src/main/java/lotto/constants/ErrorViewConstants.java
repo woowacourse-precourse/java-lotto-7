c4d67e30f55c9c6b33f6c48a4c20597ff2449a1e
dayeondayeon <dayeon2438@gmail.com>
2024-11-04 14:10:18 +0900

    feat : error 전달을 위한 ErrorViewConstants 생성

---
+++ src/main/java/lotto/constants/ErrorViewConstants.java
@@ -0,0 +1,7 @@
+package lotto.constants;
+
+public class ErrorViewConstants {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String INVALID_INPUT_TYPE = ERROR_PREFIX + "숫자 형태의 입력만 가능합니다. ";
+    public static final String INVALID_INPUT_CONSTRAINT = ERROR_PREFIX + "1,000원 이상 100,000원 이하의 금액만 가능합니다. ";
+}
