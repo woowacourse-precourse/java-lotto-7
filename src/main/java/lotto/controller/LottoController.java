03d0e76b5d0e23647e0d2028bc7fcb0a90af4551
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-04 18:35:50 +0900

    refacotr: controller 책임 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -18,26 +18,56 @@ public class LottoController {
     private final LottoGroupService lottoGroupService = new LottoGroupService();
 
     public void start() {
+        int purchaseAmount = requestPurchaseAmount();
+        int lottoCount = calculateLottoCount(purchaseAmount);
+
+        LottoGroup lottoGroup = purchaseLottos(lottoCount);
+        outputView.responseUserLottoNumber(lottoGroup);
+
+        Lotto winningLotto = inputWinningLotto();
+        int bonusNumber = inputBonusNumber();
+
+        printResult(lottoGroup, winningLotto, bonusNumber, purchaseAmount);
+    }
+
+    private void printResult(LottoGroup lottoGroup, Lotto winningLotto, int bonusNumber, int purchaseAmount) {
+        lottoGroupService.calculateResults(lottoGroup, winningLotto, bonusNumber);
+        outputView.responseWinningHistory(lottoGroup.getMatchCounts());
+        double yield = lottoGroup.calculateYield(purchaseAmount);
+        outputView.responseYieldOfLotto(String.format(YIELD_FORMAT, yield));
+    }
+
+    private int inputBonusNumber() {
+        outputView.askBonusNumber();
+        int bonusNumber = Integer.parseInt(inputView.inputBonusNumberView());
+
+        return bonusNumber;
+    }
+
+    private Lotto inputWinningLotto() {
+        outputView.askLottoNumbers();
+        List<Integer> winningNumbers = inputView.inputLottoNumbersView();
+        Lotto winningLotto = new Lotto(winningNumbers);
+
+        return winningLotto;
+    }
+
+    private LottoGroup purchaseLottos(int lottoCount) {
+        List<Lotto> userLottos = lottoFactory.createLottos(lottoCount);
+        LottoGroup lottoGroup = new LottoGroup(userLottos);
+
+        return lottoGroup;
+    }
+
+    private static int calculateLottoCount(int purchaseAmount) {
+        return purchaseAmount / LOTTO_TICKET_PRICE;
+    }
+
+    private int requestPurchaseAmount() {
         outputView.printBuyingRequest();
         int purchaseAmount = inputView.inputBuyingPrice();
         outputView.responseQuantity(purchaseAmount);
 
-        int lottoCount = purchaseAmount / LOTTO_TICKET_PRICE;
-        List<Lotto> userLottos = lottoFactory.createLottos(lottoCount);
-        LottoGroup lottoGroup = new LottoGroup(userLottos);
-
-        outputView.responseUserLottoNumber(lottoGroup);
-
-        outputView.askLottoNumbers();
-        List<Integer> winningNumbers = inputView.inputLottoNumbersView();
-        outputView.askBonusNumber();
-        int bonusNumber = Integer.parseInt(inputView.inputBonusNumberView());
-
-        Lotto winningLotto = new Lotto(winningNumbers);
-        lottoGroupService.calculateResults(lottoGroup, winningLotto, bonusNumber);
-
-        outputView.responseWinningHistory(lottoGroup.getMatchCounts());
-        double yield = lottoGroup.calculateYield(purchaseAmount);
-        outputView.responseYieldOfLotto(String.format(YIELD_FORMAT, yield));
+        return purchaseAmount;
     }
 }
