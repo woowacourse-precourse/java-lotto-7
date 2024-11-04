b44304c32317795fb080d03825dde4b2e495fc40
DongHoon Lee <dhl1924@naver.com>
2024-11-04 22:17:53 +0900

    feat(controller): 로또 게임 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,34 +2,66 @@ package lotto.controller;
 
 import lotto.domain.LottoMachine;
 import lotto.domain.Lottos;
-import lotto.domain.vo.Count;
-import lotto.dto.PurchaseAmountRequest;
+import lotto.dto.BonusNumberRequest;
+import lotto.dto.WinningNumbersRequest;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import lotto.domain.vo.Count;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
     private final LottoMachine lottoMachine;
+    private final LottoService lottoService;
 
-    public LottoController(InputView inputView, OutputView outputView, LottoMachine lottoMachine) {
+    public LottoController(InputView inputView, OutputView outputView, LottoMachine lottoMachine,
+                           LottoService lottoService) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.lottoMachine = lottoMachine;
+        this.lottoService = lottoService;
     }
 
     public void run() {
-        PurchaseAmountRequest purchaseAmountRequest = inputView.readPurchaseAmount();
+        int purchaseAmount = inputPurchaseAmount();
+        Lottos lottos = purchaseLottos(purchaseAmount);
 
-        Count count = calculateLottoCount(purchaseAmountRequest.getAmount());
-        outputView.printPurchasedLottoCount(count.lottoCount());
+        WinningNumbersRequest winningNumbers = inputWinningNumbers();
+        BonusNumberRequest bonusNumber = inputBonusNumber(winningNumbers);
 
-        Lottos lottos = lottoMachine.issueLottoTickets(count.lottoCount());
-        outputView.printLottoTickets(lottos);
+        calculateResults(lottos, winningNumbers, bonusNumber);
+        displayResults(purchaseAmount);
     }
 
-    private Count calculateLottoCount(int purchaseAmount) {
-        int lottoCountValue = lottoMachine.calculateLottoCount(purchaseAmount);
-        return Count.newInstance(lottoCountValue);
+    private Lottos purchaseLottos(int purchaseAmount) {
+        Count lottoCount = Count.newInstance(lottoMachine.calculateLottoCount(purchaseAmount));
+        outputView.printPurchasedLottoCount(lottoCount.lottoCount());
+
+        Lottos lottos = lottoMachine.issueLottoTickets(lottoCount.lottoCount());
+        outputView.printLottoTickets(lottos);
+
+        return lottos;
+    }
+
+    private int inputPurchaseAmount() {
+        return inputView.readPurchaseAmount().getAmount();
+    }
+
+    private WinningNumbersRequest inputWinningNumbers() {
+        return inputView.readWinningNumbers();
+    }
+
+    private BonusNumberRequest inputBonusNumber(WinningNumbersRequest winningNumbersRequest) {
+        return inputView.readBonusNumber(winningNumbersRequest);
+    }
+
+    private void calculateResults(Lottos lottos, WinningNumbersRequest winningNumbers, BonusNumberRequest bonusNumber) {
+        lottoService.processWinningResults(lottos, winningNumbers, bonusNumber);
+    }
+
+    private void displayResults(int purchaseAmount) {
+        outputView.printResults(lottoService.getWinningResult());
+        outputView.printProfitRate(lottoService.calculateTotalReturn(purchaseAmount));
     }
 }
