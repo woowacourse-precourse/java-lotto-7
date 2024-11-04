ae0a54393388db4b821e5c450d67270dbf2dee8d
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 16:40:45 +0900

    feat(InputView): 당첨 번호 및 보너스 번호 입력 시 예외 처리 추가
    당첨 번호 및 보너스 번호 입력 시 아래 경우에 대한 예외 처리 추가
    
    - 당첨 번호가 6개가 아닌 경우
    - 당첨/보너스 번호가 숫자가 아닌 경우
    - 당첨 번호가 중복된 경우
    - 당첨 번호가 1~45 범위를 벗어나는 경우
    - 보너스 번호가 1~45 범위를 벗어나는 경우

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -28,13 +28,48 @@ public class InputView {
     public static List<Integer> inputWinningNumber() {
         List<Integer> numbers = new ArrayList<>();
         String[] inputNumbers = Console.readLine().split(",");
-        for(String inputNumber : inputNumbers) {
-            numbers.add(Integer.parseInt(inputNumber));
+        try {
+            for(String inputNumber : inputNumbers) {
+                numbers.add(Integer.parseInt(inputNumber));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
+
+        validateWinningNumber(numbers);
+
         return numbers;
     }
 
+    private static void validateWinningNumber(List<Integer> winningNumber) {
+        if (winningNumber.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        // 1~45 사이의 숫자인지 확인
+        for (int number : winningNumber) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이여야 합니다.");
+            }
+        }
+        // 중복된 숫자가 있는지 확인
+        if (winningNumber.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
     public static int inputBonusNumber() {
-        return Integer.parseInt(Console.readLine());
+        try {
+            int bonusNumber = Integer.parseInt(Console.readLine());
+            validateBonusNumber(bonusNumber);
+            return bonusNumber;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private static void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+        }
     }
 }
