99675f31a2fc2e2a989eee030e917cfc7a825c50
Kiminni <imk0980@gmail.com>
2024-11-04 22:09:40 +0900

    feat: Application 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int purchaseAmount = InputValidator.readPurchaseAmount();
+        List<Lotto> userLottos = LottoGenerator.generateLottos(purchaseAmount);
+        OutputView.printLottoTickets(userLottos);
+
+        List<Integer> winningNumbers = InputValidator.readWinningNumbers();
+        int bonusNumber = InputValidator.readBonusNumber(winningNumbers);
+
+        Map<Rank, Integer> results = LottoResult.calculateResults(userLottos, winningNumbers, bonusNumber);
+        OutputView.printResults(results, purchaseAmount);
     }
 }
