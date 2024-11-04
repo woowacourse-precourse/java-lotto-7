c837a889aa3c8fd0bc51cb00923f3317396a64a6
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 19:42:11 +0900

    Refactor(InputView): 검증 로직을 추가해서 입력값 검증을 진행
    - 만들어놓은 validator를 적용시켰습니다.
    - 파싱 및 컨버터 관련해서 따로 클래스를 생성하지 않고, 책임을 좀 더 주되, 무분별한 클래스 생성을 막았습니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,25 +4,42 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.validator.InputBonusNumberValidator;
+import lotto.validator.InputPurchaseAmountValidator;
+import lotto.validator.InputWinningLottoValidatior;
 
 public class InputView {
-    public String inputCost() {
-        return Console.readLine();
+    private final InputPurchaseAmountValidator inputPurchaseAmountValidator = new InputPurchaseAmountValidator();
+    private final InputWinningLottoValidatior inputWinningLottoValidatior = new InputWinningLottoValidatior();
+    private final InputBonusNumberValidator inputBonusNumberValidator = new InputBonusNumberValidator();
+
+    public String inputPurchaseAmount() {
+        String input = Console.readLine();
+        inputPurchaseAmountValidator.validate(input);
+        return input;
     }
 
     public Lotto inputWinningLottoNumbers() {
         String input = Console.readLine();
-        List<Integer> winningLottoNumbers = Arrays.stream(input.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .sorted()
-                .toList();
-        return new Lotto(winningLottoNumbers);
+        inputWinningLottoValidatior.isValidBeforeParsing(input);
+        return convertInputIntoLotto(input);
     }
 
     public int bonusWinningLottoNumber() {
         String input = Console.readLine();
+        inputBonusNumberValidator.validateBeforeParsing(input);
+        int bonusNumber = Integer.parseInt(input);
+        inputBonusNumberValidator.validateAfterParsing(bonusNumber);
         Console.close();
-        return Integer.parseInt(input);
+        return bonusNumber;
+    }
+
+    private Lotto convertInputIntoLotto(String input) {
+        List<Integer> winningLottoNumbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .filter(inputWinningLottoValidatior::isValidAfterParsing)
+                .map(Integer::parseInt)
+                .toList();
+        return new Lotto(winningLottoNumbers);
     }
 }
