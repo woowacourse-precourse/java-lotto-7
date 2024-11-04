14378dc23767792cdf948751cf9be75929b74f13
TalkingPotato90 <star901210@hanmail.net>
2024-11-04 23:29:42 +0900

    feat: 컨트롤러 추가하여 프로그램 진입점 간소화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,8 @@
 package lotto;
 
-import lotto.controller.BonusNumberController;
-import lotto.controller.LottoController;
-import lotto.controller.PurchaseController;
-import lotto.controller.RankCalculatorController;
-import lotto.controller.WinningNumberGenerationController;
-import lotto.model.BonusNumber;
-import lotto.model.Lotto;
+import lotto.controller.AppController;
 import lotto.model.LottoGenerator;
-import lotto.model.Rank;
 import lotto.model.RankCalculator;
-import lotto.model.WinningNumber;
 import lotto.model.WinningNumberGenerator;
 import lotto.service.TicketService;
 import lotto.util.ControllerFactory;
@@ -18,53 +10,19 @@ import lotto.util.IoComponent;
 import lotto.util.LottoComponent;
 import lotto.util.common.CommonIo;
 
-import java.util.List;
-import java.util.Map;
-
-import static lotto.util.common.RepeatInput.repeatUntilValid;
-
 public class Application {
     public static void main(String[] args) {
         CommonIo commonIo = new CommonIo();
         IoComponent ioComponent = new IoComponent(commonIo);
-
         LottoComponent lottoComponent = new LottoComponent(
                 new TicketService(),
                 new LottoGenerator(),
                 new RankCalculator(),
                 new WinningNumberGenerator()
         );
+        ControllerFactory controllerFactory = new ControllerFactory(ioComponent, lottoComponent);
+        AppController appController = new AppController(controllerFactory, ioComponent, lottoComponent);
 
-        ControllerFactory controllerFactoryFactory = new ControllerFactory(ioComponent, lottoComponent);
-
-        PurchaseController purchaseController = controllerFactoryFactory.createPurchaseController();
-        purchaseController.purchaseLottos();
-
-
-        LottoController lottoController = controllerFactoryFactory.createLottoController();
-
-        List<Lotto> lottos = lottoController.excuteLottos();
-
-        ioComponent.getIoController().printPurchaseLottoNumbers(lottos);
-
-        WinningNumberGenerationController winningNumberGenerationController =
-                controllerFactoryFactory.createWinningNumberController();
-        BonusNumberController bonusNumberController = controllerFactoryFactory.createBonusNumberController();
-
-        WinningNumber winningNumber = repeatUntilValid(() -> {
-            Lotto winningNumbers = winningNumberGenerationController.createWinningNumber();
-            BonusNumber bonusNumber = bonusNumberController.createBonusNumber();
-            return new WinningNumber(winningNumbers, bonusNumber);
-        }, ioComponent.getCommonIo());
-
-        RankCalculatorController rankCalculatorController = controllerFactoryFactory.createRankCalculatorController(winningNumber);
-
-        Map<Rank, Integer> result = rankCalculatorController.calculateResult(lottos);
-
-        rankCalculatorController.printResult(result);
-        float profit = rankCalculatorController.calculateProfit(result, lottoComponent.getTicketService().getTicketCount());
-
-        ioComponent.getOutputView().printProfit(profit);
-
+        appController.runApplication();
     }
 }
