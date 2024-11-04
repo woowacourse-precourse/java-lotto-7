1beda68a3bdb787f84f3012585f072cbe6bdf273
jedo5000 <jsg998877@gmail.com>
2024-11-04 18:37:44 +0900

    refactor($Application.java): refactor code

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,14 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.model.LottoGame;
+import lotto.model.WinningLottoNumbers;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(new OutputView(), new LottoGame(new OutputView(), new InputView()));
+        LottoController lottoController = new LottoController(new OutputView(),
+                new LottoGame(new OutputView(), new WinningLottoNumbers()));
         lottoController.LottoGameStart();
     }
 }
--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -5,7 +5,10 @@ public enum ErrorCode {
     INVALID_NEGATIVE_NUMBER("음수를 입력할 수 없습니다"),
     INVALID_AMOUNT_FORMAT("금액은 숫자만 입력 가능합니다."),
     INVALID_PRICE_UNIT("금액은 1000원 단위만 허용됩니다."),
-    INVALID_LOTTO_NUMBERS_COUNT("로또 번호는 6개여야 합니다.");
+    INVALID_LOTTO_NUMBERS_COUNT("로또 번호는 6개여야 합니다."),
+    INVALID_DELIMITER("당첨 번호는 쉼표로 구분된 양수의 6개의 숫자여야 합니다."),
+    INVALID_LOTTO_NUMBER("당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+
 
     private final String message;
 
