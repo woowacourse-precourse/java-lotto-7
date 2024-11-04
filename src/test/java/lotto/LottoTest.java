76a964d46d8d193811ae927d244c8fb7e138bb34
JuSeong1130 <jsmoon1130@naver.com>
2024-11-04 10:37:20 +0900

    test(LottoTest) : 로또 기능 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -22,4 +25,26 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("1부터 45사이의 범위의 숫자가 아니라면 예외가 발생한다")
+    @Test
+    void 로또_숫자_범위_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    @DisplayName("Lotto에 숫자가 포함되어있다면 True")
+    void hasNumber_True(int number) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Assertions.assertThat(lotto.hasNumber(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8, 9})
+    @DisplayName("Lotto에 숫자가 포함되어있지 않다면 False")
+    void hasNumber_False(int number) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Assertions.assertThat(lotto.hasNumber(number)).isFalse();
+    }
 }
