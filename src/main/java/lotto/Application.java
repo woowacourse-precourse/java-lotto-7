5f0a9970cfd2d10a6f97c7f406568bdd74985ae4
apple-mint <174938449+apple-mint@users.noreply.github.com>
2024-11-04 17:31:18 +0900

    style: 불필요한 주석 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,27 +10,21 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        // 1. 로또 구입 금액 입력
         int number = IntegerValidator.numberValidator(InputView.inputMoney());
         Money money = new Money(number);
 
-        // 2. 로또 번호 추첨
         int count = money.getCount();
         List<List<Integer>> lottos = Lotto.makeRandomNumbers(count);
 
-        // 3. 구매 로또 번호 수량 및 번호 출력
         OutputView.printCount(count);
         OutputView.printLottos(lottos);
 
-        // 4. 로또 당첨 번호 입력
         List<Integer> numbers = IntegerValidator.lottoValidator(InputView.inputNumbers());
         Lotto winningNumbers = new Lotto(numbers);
 
-        // 5. 보너스 번호 입력
         int bonus = IntegerValidator.numberValidator(InputView.inputBonus());
         Bonus bonusNumber = new Bonus(bonus, winningNumbers.getNumbers());
 
-        // 6. 로또가 당첨되었는지 확인
         for (List<Integer> lotto : lottos) {
             int matchCount = Result.matchLotto(lotto, winningNumbers.getNumbers());
             boolean matchBonus = Result.matchBonus(lotto, bonusNumber.getNumber());
@@ -41,7 +35,5 @@ public class Application {
         }
 
         Result.calculateEarningsRate(money.getMoney());
-
-
     }
 }
