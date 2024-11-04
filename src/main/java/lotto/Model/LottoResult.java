c195f8b188e5ac33b4e0eeb97bdc9d3f9cec7ea0
seunghui-park <ridia1233@naver.com>
2024-11-04 22:08:11 +0900

    feat(LottoResult): 로또 결과 및 수익률 계산 기능 추가
    - 당첨 등수를 바탕으로 총 상금 계산
    - 구매 금액 대비 수익률 계산 기능 추가

--- src/main/java/lotto/Model/LottoResult.java
+++ src/main/java/lotto/Model/LottoResult.java
@@ -1,5 +1,16 @@
 package lotto.Model;
 
-public class LottoResult {
+import java.util.List;
 
+public class LottoResult {
+    private final List<Rank> ranks;
+
+    public LottoResult(List<Rank> ranks) {
+        this.ranks = ranks;
+    }
+
+    public double calculateProfitRate(int purchaseAmount) {
+        int totalPrize = ranks.stream().mapToInt(Rank::getPrize).sum();
+        return ((double) totalPrize / purchaseAmount) * 100;
+    }
 }
