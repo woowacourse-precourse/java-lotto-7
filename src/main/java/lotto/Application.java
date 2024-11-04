42890e8fd56d5a0668d345186b8f1d9d0ee9dfb6
kkmdevel <kkm.devel@gmail.com>
2024-11-04 17:26:50 +0900

    feat(Application): 메인 애플리케이션 클래스 구현
    - LottoApplicationController를 실행하는 main 메서드 포함한 Application 클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoApplicationController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoApplicationController lottoApplication = new LottoApplicationController();
+        lottoApplication.run();
     }
 }
