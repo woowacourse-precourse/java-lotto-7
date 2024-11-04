608ae7dbc3552f02772a43121e4c42089ebd2d80
sunwon <jsw5913@naver.com>
2024-11-04 17:48:24 +0900

    chore(ErrorMessage): 입력메시지 문구 숫자만 -> 양수만으로 변경

--- src/main/java/lotto/model/ErrorMessage.java
+++ src/main/java/lotto/model/ErrorMessage.java
@@ -6,7 +6,7 @@ public enum ErrorMessage {
     BONUS_NUMBER_DUPLICATE("[ERROR] 보너스 당첨 숫자는 당첨 숫자와 같으면 안됩니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
     INVALID_ATTEMPT_COUNT("[ERROR] 시도 횟수는 0보다 커야 합니다."),
-    INVALID_NUMBER_FORMAT("[ERROR] 숫자만 입력 가능합니다."),
+    INVALID_NUMBER_FORMAT("[ERROR] 양수만 입력 가능합니다."),
     INVALID_LOTTO_NUMBER_RANGE("[ERROR] 숫자는 1 ~ 45 까지 입력해주세요."),
     MINIMUM_PURCHASE_AMOUNT("[ERROR] 로또 금액은 1000원 이상이어야 합니다."),
     INVALID_PURCHASE_UNIT("[ERROR] 로또 금액은 1000원 단위여야 합니다."),
