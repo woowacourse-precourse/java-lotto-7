d007abaa1e83a3378261a97b6548fd87bac9a4eb
mirolis6636 <mirolis6636@gmail.com>
2024-11-04 11:39:55 +0900

    fix: readLottoPrice 메서드에서 while문의 조건식 변경

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -15,7 +15,7 @@ public class LottoMachine {
     public int readLottoPrice() {
         System.out.println("구입금액을 입력해 주세요");
         String money = inputReader.readUserInput();
-        while (Integer.parseInt(money) / LOTTO_PER_PRICE != 0) {
+        while (Integer.parseInt(money) % LOTTO_PER_PRICE != 0) {
             System.out.println("[ERROR] 구입 금액은 " + LOTTO_PER_PRICE + "원 단위로 입력해주세요.");
             money = inputReader.readUserInput();
         }
