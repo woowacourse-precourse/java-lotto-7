49dd2cc94263204929e017fc64b3ba76c03df443
sytTest <syt1128@naver.com>
2024-11-04 19:57:44 +0900

    feat(HostController): 입력 받는 기능 추가
    올바르지 않은 입력 시 재입력을 받음

---
+++ src/main/java/lotto/controller/HostController.java
@@ -0,0 +1,41 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.service.input.InputService;
+import lotto.service.input.InputServiceImpl;
+import lotto.service.input.converter.BallEntryConverterService;
+import lotto.service.input.converter.BallInputConverterService;
+import lotto.service.input.validator.BallEntryValidator;
+import lotto.service.input.validator.LuckyBallInputValidator;
+import lotto.service.lotteryhost.HostService;
+import lotto.service.lotteryhost.HostServiceImpl;
+import lotto.view.UserInput;
+import lotto.view.constant.InputInfo;
+
+public class HostController {
+
+    private static HostController instance = new HostController();
+
+    private InputService<List<Integer>> ballEntryInputService;
+    private InputService<Integer> luckyBallInputService;
+
+    private HostController() {
+    }
+
+    private void getInput(InputService<?> inputService, InputInfo inputInfo) {
+        String input = UserInput.get(inputInfo.guide());
+        try {
+            inputService.validate(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            getInput(inputService, inputInfo);
+        }
+    }
+
+    public static HostController getController() {
+        if (instance == null) {
+            instance = new HostController();
+        }
+        return instance;
+    }
+}
