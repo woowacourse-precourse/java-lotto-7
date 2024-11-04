f95eac90488cc2ecdb4cab5aa7fb99563a98a145
RIWON <121172065+awrion3@users.noreply.github.com>
2024-11-04 10:22:31 +0900

    feat(tickets): add constructor and generate

---
+++ src/main/java/lotto/model/Tickets.java
@@ -0,0 +1,55 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Tickets {
+    private static final int UNIT = 1000;
+
+    private int investAmount;
+    private int ticketAmount;
+    private List<Lotto> tickets;
+
+    public Tickets(int investAmount) {
+        this.investAmount = investAmount;
+        calculateTicketAmount();
+
+        tickets = new ArrayList<>();
+        generateTickets();
+    }
+
+    private void calculateTicketAmount() {
+        ticketAmount = investAmount / UNIT;
+    }
+
+    private List<Integer> generateTicketNumbers() {
+        List<Integer> ticketNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        ticketNumbers = ticketNumbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        return ticketNumbers;
+    }
+
+    private void generateTickets() {
+        for (int i = 0; i < ticketAmount; i++) {
+            List<Integer> ticketNumbers = generateTicketNumbers();
+
+            tickets.add(new Lotto(ticketNumbers));
+        }
+    }
+
+    public int getInvestAmount() {
+        return investAmount;
+    }
+
+    public int getTicketAmount() {
+        return ticketAmount;
+    }
+
+    public List<Lotto> getTickets() {
+        return tickets;
+    }
+}
