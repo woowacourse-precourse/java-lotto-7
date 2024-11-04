7cefb6518a23e30f5ea9516ed61e45ac5584cd90
zelatndnjs <zelatndnjs@naver.com>
2024-11-04 21:50:18 +0900

    feat(InputView.java) : inputWinningLotto, inputWinningLotto 메소드 생성
    당첨 번호와 보너스 번호를 입력받는 메소드와 번호들을 ,로 나누고 검증한 뒤 리스트에 저장해주는 parseNumbers 메소드 생성

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,10 +2,32 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+    public WinningLotto inputWinningLotto() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        List<Integer> winningNumbers = parseNumbers(input);
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String bonusInput = Console.readLine();
+        Validator.validateBonusNumber(bonusInput, winningNumbers);
+        int bonusNumber = Integer.parseInt(bonusInput);
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private List<Integer> parseNumbers(String input) {
+        List<String> inputs = Arrays.asList(input.split(","));
+        Validator.validateWinningNumbers(inputs);
+        return inputs.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
