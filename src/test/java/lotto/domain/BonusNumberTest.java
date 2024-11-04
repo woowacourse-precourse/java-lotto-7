caaf73668b828e0b04b9391eebe0603b88afae67
이준범 <jach1206@naver.com>
2024-11-04 22:10:49 +0900

    test: 보너스 번호가 입력될 때 발생하는 예외와 관련된 테스트 코드 작성
    - BonusNumberTest 클래스: 보너스 번호에 한 개 이상의 숫자가 입력되거나, 숫자가 아닌 값,
    1~45 사이를 벗어난 값, 당첨 번호와 중복된 값이 입력됐을 때 예외가 발생하는 지 테스트 코드를 작성했다.

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    private static final String TWO_BONUS_NUMBER = "2,3";
+    private static final String NOT_NUMBER = "a";
+    private static final String OUT_OF_RANGE = "46";
+    private static final String DUPLICATED_NUMBER = "1";
+
+
+    @Test
+    void 보너스_번호에_한개_이상의_숫자가_입력되면_예외가_발생한다() {
+        assertThatThrownBy(() -> BonusNumber.initialInstance(TWO_BONUS_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호에_숫자가_아닌_값이_입력되면_예외가_발생한다() {
+        assertThatThrownBy(() -> BonusNumber.initialInstance(NOT_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_1과_45_사이를_벗어났다면_입력되면_예외가_발생한다() {
+        assertThatThrownBy(() -> BonusNumber.initialInstance(OUT_OF_RANGE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호와_중복됐다면_예외가_발생한다() {
+        WinningNumbers.initialInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> BonusNumber.initialInstance(DUPLICATED_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
