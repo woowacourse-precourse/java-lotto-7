1d134034ffdcf3fb68d13449c505379ab6e6f9ac
kaswhy <sxshx818@naver.com>
2024-11-04 17:00:38 +0900

    test(BonusNumberTest): 입력값이 문자를 포함하면 예외 발생 테스트
    변경 후
    - 해당 사항 구현

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -3,8 +3,10 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static lotto.constants.ErrorMessage.INPUT_CAN_NOT_BE_BLANK;
+import static lotto.constants.ErrorMessage.LOTTO_CAN_NOT_HAVE_CHARACTER;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -22,6 +24,23 @@ public class BonusNumberTest {
         // then
         assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INPUT_CAN_NOT_BE_BLANK.get());
+    }
+
+    @DisplayName("입력받은 값이 문자를 포함하면 예외가 발생한다.")
+    @Test
+    public void ThrowExceptionIfHasCharacter(){
+        // given
+        String inputNumber = "-3";
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            BonusNumber bonusNumber = new BonusNumber(inputNumber);
+        });
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_CAN_NOT_HAVE_CHARACTER.get());
+
     }
 
 
