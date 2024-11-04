913658a221086f4c4563800fe20c25b9be2c4b8a
gabean13 <choegabin171@gmail.com>
2024-11-04 15:58:52 +0900

    feat(OutputController) : 출력 기능 구현
    - 사용자가 구매한 로또 개수, 로또 번호 출력 구현
    - 사용자 로또 당첨 결과 및 수익률 출력 구현

---
+++ src/main/java/lotto/view/OutputController.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+import java.util.List;
+import java.util.Map;
+import lotto.lottoMachine.Lotto;
+import lotto.lottoMachine.LottoResult;
+
+public class OutputController {
+    public void printUserLottos (Integer purchaseNum,List<Lotto> userLottos){
+        StringBuilder sb = new StringBuilder();
+        sb.append(purchaseNum).append("개를 구매했습니다. \n");
+        userLottos.forEach(lotto -> sb.append(lotto).append("\n"));
+
+        System.out.println(sb);
+    }
+
+    public void printResult(Map<LottoResult, Integer> resultMap, Double ratioOfProfit) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("당첨통계 \n").append("--- \n");
+        for(Map.Entry<LottoResult, Integer> entry : resultMap.entrySet()) {
+            LottoResult lottoResult = entry.getKey();
+            sb.append(lottoResult.getDescription()).append(entry.getValue()).append("개 \n");
+        }
+
+        sb.append(String.format("총 수익률은 %.1f%%입니다.\n", ratioOfProfit));
+        System.out.println(sb);
+    }
+}
