0ed3eac8d71b078d2d0262e065bc4a928aba701e
nicolao00 <nicolao00@naver.com>
2024-11-04 23:55:41 +0900

    feat: 로또 생성기 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    public static List<Lotto> generateLottos(int purchaseAmount) {
+        int count = purchaseAmount / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+}
