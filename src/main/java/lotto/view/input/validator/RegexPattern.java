2bd6c7792cb1d57589fd4caeb858940ce749354b
EHCTH <lees90896@gmail.com>
2024-11-04 13:12:35 +0900

    fix(RegexPattern): LOTTO_REGEX 정수사이에 0이 들어갈 경우 예외가 발생 안되는 로직 수정 * fix: 정규식을 \\d+ -> [1-9]\\d* 로 변경

--- src/main/java/lotto/view/input/validator/RegexPattern.java
+++ src/main/java/lotto/view/input/validator/RegexPattern.java
@@ -1,16 +1,15 @@
 package lotto.view.input.validator;
-
 import java.util.regex.Pattern;
 
 public enum RegexPattern {
     NUMBER_REGEX(Pattern.compile("^[1-9]\\d*$")),
-    LOTTO_REGEX(Pattern.compile("^\\d+(?:,\\d+)+$"));
+    LOTTO_REGEX(Pattern.compile("^[1-9]\\d*(?:,[1-9]\\d*)+$"));
     private final Pattern pattern;
 
     RegexPattern(Pattern pattern) {
         this.pattern = pattern;
     }
-    public boolean match(String input) {
-        return pattern.matcher(input).matches();
+    public boolean unMatch(String input) {
+        return !pattern.matcher(input).matches();
     }
 }
