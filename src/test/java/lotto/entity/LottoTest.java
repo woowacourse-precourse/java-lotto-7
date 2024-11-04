b847f504a9576f7a96b5c32f5c6e8fff890f83de
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 19:44:36 +0900

    teat: 로또 당첨 확인 test 코드 구현

--- src/test/java/lotto/entity/LottoTest.java
+++ src/test/java/lotto/entity/LottoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -70,6 +71,99 @@ class LottoTest {
             // 미만
             assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
                     .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("로또 당첨 확인")
+    class CheckLottoTest {
+
+        @DisplayName("1등 당첨 Test")
+        @Test
+        void firstPrizeTest() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 3, 4, 5, 6);
+            Integer bonusNumber = 9;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.FIRST);
+        }
+
+        @DisplayName("2등 당첨 Test")
+        @Test
+        void secondPrizeTest() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 3, 4, 5, 7);
+            Integer bonusNumber = 6;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.SECOND);
+        }
+
+        @DisplayName("3등 당첨 Test")
+        @Test
+        void thirdPrizeTest() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 3, 4, 5, 7);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.THIRD);
+        }
+
+        @DisplayName("4등 당첨 Test")
+        @Test
+        void fourthPrizeTest() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 3, 4, 10, 11);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.FOURTH);
+        }
+
+        @DisplayName("5등 당첨 Test")
+        @Test
+        void fifthPrizeTest() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 3, 9, 10, 11);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.FIFTH);
+        }
+
+        @DisplayName("미당첨 Test (당첨 숫자 없음)")
+        @Test
+        void noPrizeTest1() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(10, 11, 12, 13, 14, 15);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.NO_PRIZE);
+        }
+
+        @DisplayName("미당첨 Test (당첨 숫자 1개)")
+        @Test
+        void noPrizeTest2() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 11, 12, 13, 14, 15);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.NO_PRIZE);
+        }
+
+        @DisplayName("미당첨 Test (당첨 숫자 2개)")
+        @Test
+        void noPrizeTest3() {
+            List<Integer> numbersForLotto = Arrays.asList(1, 2, 3, 4, 5, 6);
+            List<Integer> numbersForCheck = Arrays.asList(1, 2, 12, 13, 14, 15);
+            Integer bonusNumber = 8;
+            Lotto lotto = new Lotto(numbersForLotto);
+
+            assertEquals(lotto.checkPrize(numbersForCheck, bonusNumber), Prize.NO_PRIZE);
         }
     }
 }
