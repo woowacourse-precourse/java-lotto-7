465629750e30d405fbd4fc9b0a105dabbe1f3609
SyingSHY <posti1x38@gmail.com>
2024-11-04 21:06:57 +0900

    feat: 로또 기능 구현

--- src/main/java/lotto/LottoWinningChecker.java
+++ src/main/java/lotto/LottoWinningChecker.java
@@ -1,4 +1,38 @@
 package lotto;
 
-public class LottoDrawMatcher {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoWinningChecker {
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public LottoWinningChecker(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getWinningGrade(Lotto lotto) {
+        int winningMatched = 1;
+
+        for (int winningNumber : winningNumbers) {
+            if (lotto.contains(winningNumber)) {
+                winningMatched++;
+            }
+        }
+
+        if (winningMatched == 6) {
+            return 1;
+        }
+        if (winningMatched == 5 && lotto.contains(bonusNumber)) {
+            return 2;
+        }
+        if (winningMatched == 5) {
+            return 3;
+        }
+        if (winningMatched == 4) {
+            return 4;
+        }
+        return 5;
+    }
 }
