4e6f4b54aaff6ac4b051b19d9b6b47f04fa38b2f
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2024-11-04 16:19:56 +0900

    feat(LottoMatchingService) : 로또 번호와 당첨 번호를 비교하는 기능 구현

--- src/main/java/lotto/LottoMatchingService.java
+++ src/main/java/lotto/LottoMatchingService.java
@@ -1,44 +1,51 @@
 package lotto;
 
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-public class LottoService {
-    private static final Map<Rank, Integer> result = new LinkedHashMap<>();
+public class LottoMatchingService {
+    private final LottoResult lottoResult;
+    private final LottoRepository lottoRepository;
 
-    public double getResult(List<Integer> winningNumbers, int bonusNumber, int purchaseAmount) {
-        for (Rank rank : Rank.values()) {
-            result.put(rank, 0);
-        }
-
-        for (Lotto lotto : LottoRepository.getInstance().findAll()) {
-            int matchCount = lotto.getMatchCount(winningNumbers);
-            boolean hasBonus = lotto.containsBonus(bonusNumber);
-
-            Rank rank = Rank.calculateRank(matchCount, hasBonus);
-            result.put(rank, result.get(rank) + 1);
-        }
-
-        int totalPrize = 0;
-        for (Rank rank : result.keySet()) {
-            int count = result.get(rank);
-            totalPrize += rank.getPrize() * count;
-        }
-
-        double profitRate = ((double) totalPrize / purchaseAmount) * 100;
-
-        return (double) Math.round(profitRate * 10) / 10.0;
+    public LottoMatchingService() {
+        this.lottoRepository = LottoRepository.getInstance();
+        this.lottoResult = new LottoResult();
     }
 
-    public void printResult() {
-        for (Rank key : result.keySet()) {
-            if (key == Rank.NONE) {
+    public void checkLotto(WinningNumber winningNumber) {
+        for (Lotto lotto : lottoRepository.findAll()) {
+            int matchCount = lotto.getMatchCount(winningNumber.getNumbers());
+            boolean hasBonus = lotto.containsBonus(winningNumber.getBonusNumber());
+            Rank rank = Rank.calculateRank(matchCount, hasBonus);
+
+            lottoResult.save(rank);
+        }
+    }
+
+    public void printResults(int purchaseAmount) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        // 각 등수별 당첨 결과 출력
+        for (Map.Entry<Rank, Integer> result : lottoResult.getLottoResult().entrySet()) {
+            Rank rank = result.getKey();
+            int count = result.getValue();
+
+            if (rank == Rank.NONE) {
                 continue;
             }
 
-            System.out.printf("%s (%,d원) - %d개%n", key.getMessage(), key.getPrize(),
-                    result.get(key));
+            System.out.printf("%s (%,d원) - %d개%n", rank.getMessage(), rank.getPrize(), count);
         }
+
+        // 수익률 계산 및 출력
+        double profitRate = calculateProfitRate(purchaseAmount);
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
+    }
+
+    private double calculateProfitRate(int purchaseAmount) {
+        int totalPrize = lottoResult.getTotalPrize();
+        double profitRate = ((double) totalPrize / purchaseAmount) * 100;
+
+        return (double) Math.round(profitRate * 10) / 10;
     }
 }
