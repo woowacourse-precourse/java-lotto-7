c11c9db40fda1a62aff749cb73f7f6126def79d0
DongHoon Lee <dhl1924@naver.com>
2024-11-04 12:42:24 +0900

    refactor(view): ConsoleReader 클래스 입력 검증 및 재입력 기능 추가
    - 입력 검증을 위한 InputValidator를 ConsoleReader 클래스 내에 static nested class로 추가
    - null 및 빈 문자열 검증을 수행하도록 validatePurchaseAmount 메서드 추가
    - 예외 발생 시 에러 메시지를 출력하고 재입력을 요청하도록 수정

--- src/main/java/view/console/ConsoleReader.java
+++ src/main/java/view/console/ConsoleReader.java
@@ -2,13 +2,46 @@ package view.console;
 
 import camp.nextstep.edu.missionutils.Console;
 import dto.PurchaseAmountRequest;
+import exception.ErrorMessage;
+import exception.LottoException;
 import view.InputView;
 
 public class ConsoleReader implements InputView {
+    private static final String PURCHASE_AMOUNT_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
 
     @Override
     public PurchaseAmountRequest readPurchaseAmount() {
-        String purchaseAmount = Console.readLine();
-        return new PurchaseAmountRequest(purchaseAmount.trim());
+        System.out.println(PURCHASE_AMOUNT_REQUEST_MESSAGE);
+        try {
+            String purchaseAmount = readAndTrimInput();
+            InputValidator.validatePurchaseAmount(purchaseAmount);
+            return new PurchaseAmountRequest(purchaseAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readPurchaseAmount();
+        }
+    }
+
+    private String readAndTrimInput() {
+        return Console.readLine().trim();
+    }
+
+    private static class InputValidator {
+        private static void validatePurchaseAmount(String purchaseAmount) {
+            validateNotNull(purchaseAmount);
+            validateNotEmpty(purchaseAmount);
+        }
+
+        private static void validateNotNull(String purchaseAmount) {
+            if (purchaseAmount == null) {
+                throw LottoException.from(ErrorMessage.NULL_INPUT_ERROR);
+            }
+        }
+
+        private static void validateNotEmpty(String purchaseAmount) {
+            if (purchaseAmount.isBlank()) {
+                throw LottoException.from(ErrorMessage.EMPTY_INPUT_ERROR);
+            }
+        }
     }
 }
