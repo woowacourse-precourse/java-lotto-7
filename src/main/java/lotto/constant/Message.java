6d23fbf37ff94d8cb5f5cce00746730512b260b1
jihyun <jihyun3066@naver.com>
2024-11-04 20:41:27 +0900

    [ feat ] 상수 선언

---
+++ src/main/java/lotto/constant/Message.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public class Message {
+    public final static String DELIMITER = ",";
+    public final static String OUTPUT_DELIMITER = ", ";
+    public final static String FRONT_MESSAGE = "[";
+    public final static String BACK_MESSAGE = "]";
+
+
+}
---
+++ src/main/java/lotto/constant/Number.java
@@ -0,0 +1,23 @@
+package lotto.constant;
+
+public class Number {
+
+    public static int MIN_LOTTO_NUMBER = 1;
+    public static int MAX_LOTTO_NUMBER = 45;
+    public static int MAX_LOTTO_LENGTH = 6;
+    public static int MAX_PRIZE_NUMBER_LENGTH = 7;
+    public static int LOTTO_TICKET_PRIZE = 7;
+
+
+    public static int FIRST_PRIZE_MONEY = 2000000000;
+    public static int SECOND_PRIZE_MONEY = 30000000;
+    public static int THIRD_PRIZE_MONEY = 1500000;
+    public static int FOURTH_PRIZE_MONEY = 50000;
+    public static int FIFTH_PRIZE_MONEY = 5000;
+
+
+
+
+
+
+}
