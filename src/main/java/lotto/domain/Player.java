bf19d0bdc0b4b5b528071f1083565c69fab4ed55
MinjuKwak01 <mkwak1125@gmail.com>
2024-11-04 21:52:08 +0900

    fix (Player) : 입력 금액값 검증 로직을 객체 내부에서도 하도록 수정

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
+import lotto.error.ErrorMessage;
+
 public class Player {
     private final int money;
     private final int ticketQuantity;
 
     public Player(int money, int ticketQuantity) {
+        validate(money);
         this.money = money;
         this.ticketQuantity = ticketQuantity;
+    }
+
+    private void validate(int money){
+        if (money <= 0 || money % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.AMOUNT_OUT_OF_RANGE.getMessage());
+        }
     }
 
     public int getMoney() {
