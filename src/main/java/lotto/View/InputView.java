9dd9bec71ece20f35568bce1f67ee860382fd692
허재원 <jaewonheo666@gmail.com>
2024-11-04 17:57:08 +0900

    feat(InputView): 금액을 읽어오는 검증하는 기능, 당첨번호를 읽어오고 검증하는 기능, 보너스 번호를 읽어오고 검증하는 기능 구현

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -2,16 +2,66 @@ package lotto.View;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class InputView {
-    public long InputPayment(){
+    public long inputPayment(){
+        printPaymentMessage();
         String payment = Console.readLine();
         validateIsNumber(payment);
         return Long.parseLong(payment);
+    }
+    public List<Integer> inputWinningLottoNumber(){
+        printWinningLottoNumber();
+        String winningLottoNumber = Console.readLine();
+        validateSeparator(winningLottoNumber);
+        return convertStringToList(winningLottoNumber);
+    }
+    public int inputBonusNumber(){
+        pirntBonusNumberMessage();
+        String bonusNumber = Console.readLine();
+        validateBonusNumber(bonusNumber);
+        return Integer.parseInt(bonusNumber);
     }
     private static void validateIsNumber(String input){
         if(!input.chars().allMatch(Character::isDigit)){
             throw new IllegalArgumentException("[ERROR] 입력값은 숫자만 가능합니다.");
         }
     }
+    public void validateSeparator(String input) {
+        String REGEX = "^\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}$";
 
+        if (!Pattern.matches(REGEX, input)) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 쉼표(,)로 구분하여 입력하세요.");
+        }
+    }
+    public void validateBonusNumber(String input){
+        validateIsNumber(input);
+        validateNumberRange(Integer.parseInt(input));
+    }
+    public void validateNumberRange(int input){
+        if(input < 1 || input > 45){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1이상 45이하의 숫자만 가능합니다.");
+        }
+    }
+
+    private void printWinningLottoNumber(){
+        System.out.println("당첨 번호를 입력해 주세요");
+    }
+    private void printPaymentMessage(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    private void pirntBonusNumberMessage(){
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+    private List<Integer> convertStringToList(String input){
+        return Arrays.stream(input.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
 }
