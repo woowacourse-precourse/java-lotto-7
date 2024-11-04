3ccf42e319775e42a9dc1fb7f71a38a67c80e997
kimsongie12 <song3078@naver.com>
2024-11-04 11:58:23 +0900

    feat: 중복 발생 예외처리 보너스 번호 중복 발생 시 예외처리 번호 일치 여부 비교하는 계산

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,6 +1,26 @@
 package lotto;
 
+import java.util.List;
+
 public class WinningNumbers {
     //당첨번호와 보너스 번호 관리, 사용자 번호와 일치하는 개수 계산
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber){
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+    private void validate(List<Integer> winningNumbers, int bonusNumber){
+        if(winningNumbers.size() !=6 || winningNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        }
+    }
+    public int getBonusNumber(){
+        return bonusNumber;
+    }
+    public int countMatchingNumbers(List<Integer> numbers){
+        return (int) numbers.stream().filter(winningNumbers::contains).count();
+    }
 }
