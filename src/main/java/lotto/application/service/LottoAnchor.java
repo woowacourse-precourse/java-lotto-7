b598d04ae903fea4f9e0c992490bb9f1d23cde7c
bellmin <whdlas011203@naver.com>
2024-11-04 20:37:07 +0900

    feat(LottoAnchor): initiatteEnumMap, announce 메소드 추가
    - EnumMap : Key - WinningRanking, Value - count
    - initiateEnumMap : EnumMap의 모든 Key에 대한 Value를 0으로 초기화
    - announce : 로또의 결과를 알려주는 메소드, EnumMap을 통해 알려준다

--- src/main/java/lotto/application/service/LottoAnchor.java
+++ src/main/java/lotto/application/service/LottoAnchor.java
@@ -1,8 +1,11 @@
 package lotto.application.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import lotto.application.model.Lotto;
 import lotto.application.model.WinningRanking;
 
@@ -10,8 +13,19 @@ public class LottoAnchor {
 
     private List<Integer> winningNumbers;
     private int bonusNumber;
+    private EnumMap<WinningRanking, Integer> winningRankings;
 
-    public LottoAnchor() {}
+    public LottoAnchor() {
+        initiateEnumMap();
+    }
+
+    private void initiateEnumMap() {
+        winningRankings = new EnumMap<>(WinningRanking.class);
+
+        Arrays.stream(WinningRanking.values()).forEach(winningRanking -> {
+            winningRankings.put(winningRanking, 0);
+        });
+    }
 
     public void setWinningNumbers(List<Integer> winningNumbers) {
         this.winningNumbers = new ArrayList<>(winningNumbers);
@@ -19,6 +33,16 @@ public class LottoAnchor {
 
     public void setBonusNumber(int bonusNumber) {
         this.bonusNumber = bonusNumber;
+    }
+
+    public EnumMap<WinningRanking, Integer> announce(Collection<Lotto> lottos) {
+        lottos.forEach(lotto -> {
+            WinningRanking key = lotto.match(winningNumbers, bonusNumber);
+
+            winningRankings.compute(key, (k, count) -> count + 1);
+        });
+
+        return winningRankings;
     }
 
 }
