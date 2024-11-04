b23ddd8cffe84d77222b7f756203b7a9809a36e7
byegu <rkdqudrn07@naver.com>
2024-11-04 18:47:14 +0900

    test : 당첨 번호 테스트 케이스 구현

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class WinningNumber {
     private final List<Integer> numbers;
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
     public WinningNumber(List<Integer> numbers) {
         this.numbers = numbers;
     }
