b7ab673223ee4ae3780bf5bf9d24d24c84fce9ab
choiyoung69 <choiyoung609@gmail.com>
2024-11-04 16:41:53 +0900

    feat(OutputView): 로또 내역을 출력하는 메서드 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import static lotto.view.ViewMessages.ENTER_NUMBER_OR_PURCHASES;
+
 import lotto.dto.LottoResult;
 
 public class OutputView {
     public static void printLottoResult(LottoResult lottoResult) {
-        System.out.println();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append(String.format(
+                ENTER_NUMBER_OR_PURCHASES,
+                lottoResult.getPurchaseAmount()));
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append(lottoResult.getLottoNumbers());
+        System.out.println(stringBuilder);
     }
 }
