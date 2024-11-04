9ac1bb176e801ec08b66f048e6f0ecf46deed983
imjanghyeok <qkdrhkgn23@naver.com>
2024-11-04 19:54:32 +0900

    Refactor(OutputView): 하나의 메서드를 두 개로 수정, 메서드 명 변경
    - 결과 출력에서 로또 결과와 수익률을 따로 출력하도록 수정했습니다.
    - 메서드 명을 더 직관적으로 네이밍하여 구현했습니다.
    - 에러 메시지 출력을 구현했습니다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,36 +2,40 @@ package lotto.view;
 
 import java.text.NumberFormat;
 import java.util.stream.Collectors;
-import lotto.model.Analyst;
-import lotto.model.Lotties;
-import lotto.model.Lotto;
+import lotto.constant.LottoRank;
+import lotto.model.Lottos;
 
 public class OutputView {
-    private static String INPUT_MONEY_PROMPT = "구입금액을 입력해 주세요.";
-    private static String LOTTO_COUNTS_PROMPT = "개를 구매했습니다.";
-    private static String INPUT_WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
-    private static String INPUT_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
-    private static String WINNING_STATICS_PROMPT = "당첨통계\n---";
-    private static String[] WINNING_STATICS_FORMAT = new String[]{"3개 일치", "4개 일치", "5개 일치", "5개 일치, 보너스 볼 일치", "6개 일치"};
+    private static final String INPUT_MONEY_PROMPT = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_COUNTS_PROMPT = "개를 구매했습니다.";
+    private static final String INPUT_WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
+    private static final String WINNING_STATICS_PROMPT = "당첨통계\n---";
+    private static final String WINNING_LOTTO_COUNT_FORMAT = "%d개\n";
+    private static final String WINNING_LOTTO_YIELD_FORMAT = "총 수익률은 %.1f%%입니다.\n";
 
-    private NumberFormat numberFormat = NumberFormat.getInstance();
+    private final NumberFormat numberFormat = NumberFormat.getInstance();
 
-    public void printInputMoneyPrompt() {
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public void printInputTicketCountPrompt() {
         System.out.println(INPUT_MONEY_PROMPT);
     }
 
-    public void printBoughtLottoCounts(int lottoCounts) {
-        System.out.println(lottoCounts + LOTTO_COUNTS_PROMPT);
+    public void printBoughtLottoTicketCount(int lottoTicketCount) {
+        System.out.println(lottoTicketCount + LOTTO_COUNTS_PROMPT);
     }
 
-
-    public void printBoughtLottoNumbers(Lotties lotties) {
-        for (Lotto lotto : lotties.getLotties()) {
-            System.out.println(lotto.getLottoNumbers().stream()
-                    .sorted()
-                    .map(Object::toString)
-                    .collect(Collectors.joining(", ","[","]")));
-        }
+    public void printBoughtLottos(Lottos lottos) {
+        lottos.getLottos().forEach(lotto ->
+                System.out.println(lotto.getLottoNumbers().stream()
+                        .sorted()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", ", "[", "]"))
+                )
+        );
         System.out.println();
     }
 
@@ -39,16 +43,21 @@ public class OutputView {
         System.out.println(INPUT_WINNING_NUMBERS_PROMPT);
     }
 
-    public void printInputBonusNumbersPrompt() {
+    public void printInputBonusNumberPrompt() {
         System.out.println(INPUT_BONUS_NUMBER_PROMPT);
     }
 
-    public void printWinningStatics(Analyst analyst) {
+    public void printLottoResults(int[] winLottoCount) {
         System.out.println(WINNING_STATICS_PROMPT);
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("%s (%s원) - %d개\n",
-                    WINNING_STATICS_FORMAT[i], numberFormat.format(analyst.winningsMoney()[i]), analyst.getWinLottos()[5-i]);
+        for (LottoRank lottoRank : LottoRank.values()) {
+            if (lottoRank.isPrint()) {
+                int count = winLottoCount[Integer.parseInt(lottoRank.getRank())];
+                System.out.printf(lottoRank.getScript() + WINNING_LOTTO_COUNT_FORMAT, count);
+            }
         }
-        System.out.printf("총 수익률은 %s입니다.", analyst.getYield());
+    }
+
+    public void printLottoStatics(double yield) {
+        System.out.printf(WINNING_LOTTO_YIELD_FORMAT, yield);
     }
 }
