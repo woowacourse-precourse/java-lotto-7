75d6380deeead922c8c704bd18d11cbe3d65d8e7
Chanwoo yang <moomsss@naver.com>
2024-11-04 16:35:10 +0900

    refactor: PrizeCalculator테스트 주석 추가

--- src/test/java/lotto/service/PrizeCalculatorTest.java
+++ src/test/java/lotto/service/PrizeCalculatorTest.java
@@ -23,17 +23,17 @@ class PrizeCalculatorTest {
     @Test
     void calculatePrizes() {
         WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
-        Lotto winnerLotto = new Lotto(List.of(1,2,3,4,5,6));
-        Lotto secondLotto = new Lotto(List.of(1,2,3,4,5,7));
-        Lotto losingLotto = new Lotto(List.of(8,9,10,11,12,13));
+        Lotto winnerLotto = new Lotto(List.of(1,2,3,4,5,6)); //1등
+        Lotto secondLotto = new Lotto(List.of(1,2,3,4,5,7)); //2등
+        Lotto losingLotto = new Lotto(List.of(8,9,10,11,12,13)); //미당ㅁ
 
         List<Lotto> lottos = List.of(winnerLotto, secondLotto, losingLotto);
 
         prizeCalculator.calculatePrizes(lottos, winningLotto);
 
-        assertThat(prizeCalculator.getPrizeCount().get(Prize.FIRST)).isEqualTo(1);
-        assertThat(prizeCalculator.getPrizeCount().get(Prize.SECOND)).isEqualTo(1);
-        assertThat(prizeCalculator.getPrizeCount().get(Prize.FIFTH)).isEqualTo(0);
+        assertThat(prizeCalculator.getPrizeCount().get(Prize.FIRST)).isEqualTo(1); //1등
+        assertThat(prizeCalculator.getPrizeCount().get(Prize.SECOND)).isEqualTo(1);//2등
+        assertThat(prizeCalculator.getPrizeCount().get(Prize.FIFTH)).isEqualTo(0);//5등
 
 
     }
@@ -43,17 +43,17 @@ class PrizeCalculatorTest {
     @Test
     void calculateWinningRate() {
         WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
-        Lotto winnerLotto = new Lotto(List.of(1,2,3,4,5,6));
-        Lotto secondLotto = new Lotto(List.of(1,2,3,4,5,7));
-        Lotto losingLotto = new Lotto(List.of(8,9,10,11,12,13));
+        Lotto winnerLotto = new Lotto(List.of(1,2,3,4,5,6)); //1등
+        Lotto secondLotto = new Lotto(List.of(1,2,3,4,5,7));//2등
+        Lotto losingLotto = new Lotto(List.of(8,9,10,11,12,13)); //미당첨
 
         List<Lotto> lottos = List.of(winnerLotto, secondLotto, losingLotto);
 
         prizeCalculator.calculatePrizes(lottos, winningLotto);
 
-        double winningRate = prizeCalculator.calculateWinningRate(3000);
+        double winningRate = prizeCalculator.calculateWinningRate(3000); // 3000원치를 샀을때
 
-        assertThat(winningRate).isGreaterThan(100);
+        assertThat(winningRate).isGreaterThan(100); // 당첨률이 100퍼센트 이상
 
     }
 }
