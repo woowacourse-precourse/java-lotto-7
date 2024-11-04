7ce217282012e06b8e1aec5f12907d2aeac3eb43
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 21:24:30 +0900

    test: LottoTest 추가
    - 로또 번호를 오름차순으로 정렬하여 저장한다.
    - 예외: 로또 번호의 개수가 6개가 넘어가면 예외
    - 예외: 로또 번호에 중복된 숫자가 있으면 예외
    - 예외: 로또 번호가 1-45사이가 아니라면 예외

---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,42 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("로또 번호를 오름차순으로 정렬하여 저장한다. ")
+    void testGetSortedLottoNumbersCase() {
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+        assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    @DisplayName("예외: 로또 번호의 개수가 6개가 넘어가면 예외")
+    void errorCase1() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("예외: 로또 번호에 중복된 숫자가 있으면 예외")
+    void errorCase2() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 중복된 번호를 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("예외: 로또 번호가 1-45사이가 아니라면 예외")
+    void errorCase3() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 90)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+}
