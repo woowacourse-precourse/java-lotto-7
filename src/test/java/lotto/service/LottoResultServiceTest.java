1cdf8b78c74a6b8e9843aaeda2432398f280b7b7
chaerish <ccyy8432@naver.com>
2024-11-04 18:18:44 +0900

    refactor(LottoResultServiceTest): 리팩토링된LottoResultService에 맞춰 통과하게 수정

--- src/test/java/lotto/service/LottoResultServiceTest.java
+++ src/test/java/lotto/service/LottoResultServiceTest.java
@@ -1,38 +1,55 @@
 package lotto.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.MatchingCountResult;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoResultServiceTest {
     private LottoResultService lottoResultService;
-    private LottoSeller lottoSeller;
 
-    @BeforeEach
-    void setUp() {
+    /*
+    winningCount: 1,2,3,4,5,6
+    - 1,2,3,4,5,7 (5) + bonus
+    - 1,2,3,4,5,6 (6)
+     */
+    @Test
+    void 사용자의_로또에서_당첨된_숫자와_보너스를_센다() {
+        //given
         List<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 7;
         List<Lotto> purchasedLotto = new ArrayList<>(
                 List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)), new Lotto(List.of(1, 2, 3, 4, 5, 6))));
         lottoResultService = new LottoResultService(purchasedLotto, winningNumbers, bonusNumber);
+        //when
+        List<MatchingCountResult> matchingCountResults = lottoResultService.getWinningCount();
+        //then
+        assertThat(matchingCountResults.get(0).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(1).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(2).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(3).getConditionCount()).isEqualTo(1); //5개+보너스:하나
+        assertThat(matchingCountResults.get(4).getConditionCount()).isEqualTo(1); //6개:하나
     }
 
     @Test
-    void 당첨상태의_개수를_판별한다() {
+    void 사용자의_로또에서_당첨된_숫자를_센다() {
+        //given
+        List<Integer> winningNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> purchasedLotto = new ArrayList<>(
+                List.of(new Lotto(List.of(1, 2, 3, 9, 10, 11)), new Lotto(List.of(1, 2, 3, 22, 23, 24))));
+        lottoResultService = new LottoResultService(purchasedLotto, winningNumbers, bonusNumber);
+        //when
         List<MatchingCountResult> matchingCountResults = lottoResultService.getWinningCount();
-
-        assertEquals(2, matchingCountResults.size());
-        assertEquals(5, matchingCountResults.get(0).getWinningCondition().getMatchCount());
-        assertTrue(matchingCountResults.get(0).getWinningCondition().isBonusMatch());
-
-        assertEquals(5, matchingCountResults.get(0).getWinningCondition().getMatchCount());
-        assertFalse(matchingCountResults.get(1).getWinningCondition().isBonusMatch());
+        //then
+        assertThat(matchingCountResults.get(0).getConditionCount()).isEqualTo(2);
+        assertThat(matchingCountResults.get(1).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(2).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(3).getConditionCount()).isEqualTo(0);
+        assertThat(matchingCountResults.get(4).getConditionCount()).isEqualTo(0);
     }
+
 }
