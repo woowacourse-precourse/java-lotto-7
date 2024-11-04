b1900d646f30774874df966aac5f17de32cedec9
yejin923 <yejin923@naver.com>
2024-11-04 16:50:41 +0900

    FEAT: LottoResult 생성

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,4 +1,16 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoResult {
+    private final List<Lotto> lottos;
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
+
+    public LottoResult(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        this.lottos = lottos;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
 }
