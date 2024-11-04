bd2cd513a8f6c8e21eba4b9507f420a7f322550d
Mint <audwl03071@gmail.com>
2024-11-04 15:55:23 +0900

    test: InputValidator 테스트를 작성한다.
    - 리팩토링을 하며 만들어진 InputValidator에 대한 테스트를 작성한다.

---
+++ src/test/java/lotto/support/validator/InputValidatorTest.java
@@ -0,0 +1,88 @@
+package lotto.support.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
+import lotto.exception.validator.InvalidInputException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("입력값 검증 테스트")
+class InputValidatorTest {
+
+    @Nested
+    @DisplayName("null or blank 검증 테스트")
+    class nullOrBlank_검증_테스트 {
+
+        @Test
+        @DisplayName("null이면 예외가 발생한다.")
+        void 실패_검증_null() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> InputValidator.validateNotNullOrBlank(null, "입력값"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .isExactlyInstanceOf(InvalidInputException.class)
+                    .hasMessageStartingWith("[ERROR] ")
+                    .hasMessageContaining("입력값은(는) null일 수 없습니다.");
+        }
+
+        @Test
+        @DisplayName("비어있으면 예외가 발생한다.")
+        void 실패_검증_empty() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> InputValidator.validateNotNullOrBlank("", "입력값"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .isExactlyInstanceOf(InvalidInputException.class)
+                    .hasMessageStartingWith("[ERROR] ")
+                    .hasMessageContaining("입력값은(는) 빈 문자열이거나 공백일 수 없습니다.");
+        }
+
+        @Test
+        @DisplayName("공백이면 예외가 발생한다.")
+        void 실패_검증_공백() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> InputValidator.validateNotNullOrBlank(" ", "입력값"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .isExactlyInstanceOf(InvalidInputException.class)
+                    .hasMessageStartingWith("[ERROR] ")
+                    .hasMessageContaining("입력값은(는) 빈 문자열이거나 공백일 수 없습니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("null or empty 검증 테스트")
+    class nullOrEmpty_검증_테스트 {
+
+        @Test
+        @DisplayName("null이면 예외가 발생한다.")
+        void 실패_검증_null() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> InputValidator.validateNotNullOrEmpty(null, "입력값"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .isExactlyInstanceOf(InvalidInputException.class)
+                    .hasMessageStartingWith("[ERROR] ")
+                    .hasMessageContaining("입력값은(는) null일 수 없습니다.");
+        }
+
+        @Test
+        @DisplayName("비어있으면 예외가 발생한다.")
+        void 실패_검증_empty() {
+            // Given
+
+            // When & Then
+            assertThatThrownBy(() -> InputValidator.validateNotNullOrEmpty(Collections.emptyList(), "입력값"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .isExactlyInstanceOf(InvalidInputException.class)
+                    .hasMessageStartingWith("[ERROR] ")
+                    .hasMessageContaining("입력값은(는) 빈 문자열일 수 없습니다.");
+        }
+    }
+}
