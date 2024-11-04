976419a766d84c460f82b68d6303a1600b8d8e8c
mikekks <thdalsrb79@naver.com>
2024-11-04 14:58:50 +0900

    feat(Lotto): 로또 번호 중복 여부 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import static lotto.global.ErrorMessage.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 	private final List<Integer> numbers;
@@ -12,7 +16,12 @@ public class Lotto {
 
 	private void validate(List<Integer> numbers) {
 		if (numbers.size() != 6) {
-			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+			throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_COUNT.getMessage());
+		}
+
+		Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+		if (uniqueNumbers.size() != numbers.size()) {
+			throw new IllegalArgumentException(NOT_DUPLICATE_LOTTO_NUMBER.getMessage());
 		}
 	}
 
--- src/main/java/lotto/global/ErrorMessage.java
+++ src/main/java/lotto/global/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
 	MONEY_AMOUNT_ERROR("[ERROR] 0원 이하는 입력할 수 없습니다."),
 	MONEY_UNIT_ERROR("[ERROR] 1000원 단위로 입력할 수 있습니다."),
 	INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1~45 까지만 가능합니다."),
-	INVALID_MONEY_TYPE_ERROR("[ERROR] 숫자만 입력할 수 있습니다.");
+	INVALID_MONEY_TYPE_ERROR("[ERROR] 숫자만 입력할 수 있습니다."),
+	INVALID_LOTTO_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+	NOT_DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다.");
 
 	ErrorMessage(String message) {
 		this.message = message;
