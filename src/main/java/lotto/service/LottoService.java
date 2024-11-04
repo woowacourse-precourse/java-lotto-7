e42edddd9f64d3c9a83f069b07318b9b84dab951
HOKAGO-MEMORIES <minchotejava@gmail.com>
2024-11-04 21:18:56 +0900

    feat(LottoService): 구매한 로또와 당첨 로또를 비교해 결과를 출력하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,7 +3,11 @@ package lotto.service;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.Profit;
+import lotto.domain.Ranking;
+import lotto.domain.Result;
 import lotto.domain.WinningLotto;
+import lotto.domain.checker.WinningChecker;
 import lotto.domain.extractor.LottoExtractor;
 import lotto.io.InputHandler;
 import lotto.io.PurchasePrintHandler;
@@ -26,7 +30,7 @@ public class LottoService {
     public void run() {
         Lottos lottos = getLottos();
         WinningLotto winningLotto = getWinningLotto();
-        
+        playLotto(lottos, winningLotto);
     }
 
     private Lottos getLottos() {
@@ -67,4 +71,26 @@ public class LottoService {
         return WinningLotto.from(lotto, bonusNumber);
     }
 
+    private void playLotto(Lottos lottos, WinningLotto winningLotto) {
+        WinningChecker winningChecker = new WinningChecker(lottos, winningLotto);
+        Result results = winningChecker.checkWinning();
+        printResult(results);
+        int budget = lottos.getAmounts() * 1000;
+        printProfitRate(results, budget);
+    }
+
+    private void printResult(Result results) {
+        resultHandler.printWinningStatics();
+        resultHandler.printFifthPrize(results.getResultCount(Ranking.FIFTH));
+        resultHandler.printFourthPrize(results.getResultCount(Ranking.FOURTH));
+        resultHandler.printThirdPrize(results.getResultCount(Ranking.THIRD));
+        resultHandler.printSecondPrize(results.getResultCount(Ranking.SECOND));
+        resultHandler.printFirstPrize(results.getResultCount(Ranking.FIRST));
+    }
+
+    private void printProfitRate(Result results, int budget) {
+        Profit profit = Profit.from(results, budget);
+        float profitRate = profit.calculateProfitRate();
+        resultHandler.printProfitRate(profitRate);
+    }
 }
