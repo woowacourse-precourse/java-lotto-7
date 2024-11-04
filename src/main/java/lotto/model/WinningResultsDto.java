47380f7e78ae3d1d09af81dc4c16e2e91646bc5a
Chan531 <chan657312@gmail.com>
2024-11-04 16:37:24 +0900

    feat(WinningResultsDto): WinningResultDto 리스트 생성

--- src/main/java/lotto/model/WinningResultsDto.java
+++ src/main/java/lotto/model/WinningResultsDto.java
@@ -1,8 +1,15 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Map;
 
 public record WinningResultsDto(
 	List<WinningResultDto> winningResults
 ) {
+
+	private static List<WinningResultDto> getWinningResults(Map<Winning, Integer> winningResult) {
+		return winningResult.entrySet().stream()
+				.map(winning -> WinningResultDto.from(winning.getKey(), winning.getValue()))
+				.toList();
+	}
 }
