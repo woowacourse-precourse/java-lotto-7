89ceefa40ea2472d9db7e52407417536b3d88863
Parkjyun <jaeyun0507@naver.com>
2024-11-04 21:50:39 +0900

    fix(DomainExceptionMessage): 에러 메시지 오타 수정

--- src/main/java/lotto/model/exception/DomainExceptionMessage.java
+++ src/main/java/lotto/model/exception/DomainExceptionMessage.java
@@ -5,7 +5,7 @@ public enum DomainExceptionMessage {
     INVALID_MONEY_UNIT("[ERROR] 금액은 1000원 단위여야 합니다."),
     INVALID_MONEY_VALUE("[ERROR] 금액은 0원 이상이어야 합니다."),
     INVALID_NUMBER_FORMAT("[ERROR] 형식이 맞지 않습니다. 숫자를 입력해주세요."),
-    INVALID_NUMBER_VALUE("[ERROR] 숫자는 1이상 54이하여야 합니다."),
+    INVALID_NUMBER_VALUE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     INVALID_ANSWER_SIZE("[ERROR] 정답은 6자만 가능합니다."),
     DUPLICATED_ANSWER_NUMBER("[ERROR] 정답 번호는 중복되면 안 됩니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또는 6개의 숫자여야 합니다."),
