7d26685950e1fb2ec43ff5831389b72ffc66dea3
YongLeeCode <yonghwan311@gmail.com>
2024-11-04 23:46:12 +0900

    feat: 구매 적합성 성공 케이스 구현

--- src/test/java/validator/PaymentValidatorTest.java
+++ src/test/java/validator/PaymentValidatorTest.java
@@ -1,6 +1,7 @@
 package validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import lotto.ErrorMessage;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,15 @@ public class PaymentValidatorTest {
         assertThatThrownBy(() -> PaymentValidator.validate(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.PAYMENT_ERROR_IN_MULTIPLE_OF_THOUSAND); // then
+    }
+
+    @Test
+    void 구매_금액이_천_단위인_경우() {
+        // given
+        Integer money = 3000;
+
+        // when
+        // then
+        assertDoesNotThrow(() -> PaymentValidator.validate(money));
     }
 }
