246a8ad6b3da310855ef314f767acdec99bcdeaa
YunCheol1 <naro2727@naver.com>
2024-11-04 20:08:11 +0900

    feat(Lotto): 에러메시지 상수값으로 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.List;
+import lotto.util.Config;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,7 +13,7 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(Config.ERROR_INVALID_LOTTO_NUMBER_COUNT);
         }
     }
 
