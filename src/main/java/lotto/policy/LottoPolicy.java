556ce3d560e9abee0b4f863e154268f68dba9a95
choign3123 <choign3123@naver.com>
2024-11-04 16:25:33 +0900

    feat(LottoPolicy): 로또 관련 데이터 클래스 생성

---
+++ src/main/java/lotto/policy/LottoPolicy.java
@@ -0,0 +1,8 @@
+package lotto.policy;
+
+public class LottoPolicy {
+    public static final int MIN_LOTTO_NUM = 1;
+    public static final int MAX_LOTTO_NUM = 45;
+    public static final int TOTAL_LOTTO_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
+}
