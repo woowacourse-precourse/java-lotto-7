6553dac57bb057b8550108dffcc6490893c662a7
mikekks <thdalsrb79@naver.com>
2024-11-04 10:57:32 +0900

    refactor(WinningLotto): 특정 로또와 비교하여 Rank 반환 기능 구현

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.List;
+import java.util.Optional;
+
+import lotto.domain.enums.Rank;
+
+public class WinningLotto extends Lotto {
+	public WinningLotto(List<Integer> numbers) {
+		super(numbers);
+	}
+
+	public Optional<Rank> calculateRank(Lotto lotto, BonusNumber bonusNumber) {
+		int winCount = getWinCount(lotto);
+		boolean bonusWin = isBonusWin(lotto, bonusNumber);
+
+		return Rank.judgeBy(winCount, bonusWin);
+	}
+
+	private int getWinCount(Lotto lotto) {
+		int matchCount = 0;
+
+		for (Integer number : this.getNumbers()) {
+			if (lotto.containNumber(number)) {
+				matchCount++;
+			}
+		}
+
+		return matchCount;
+	}
+
+	private boolean isBonusWin(Lotto lotto, BonusNumber bonusNumber){
+		return lotto.containNumber(bonusNumber.getNumber());
+	}
+}
