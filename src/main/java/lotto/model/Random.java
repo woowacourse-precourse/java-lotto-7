32fe9341a5bb81a1f6e05b9b900ad28b2c3668d5
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-04 19:28:10 +0900

    refactor(Random): 랜덤 단일값 반환 메서드 삭제 및 랜덤숫자꾸러미 반환 메서드 static 처리

--- src/main/java/lotto/model/Random.java
+++ src/main/java/lotto/model/Random.java
@@ -6,11 +6,7 @@ import lotto.util.LottoInfo;
 import lotto.util.Range;
 
 public class Random {
-    public List<Integer> pickUniqueNumbersInRange() {
+    public static List<Integer> pickUniqueNumbersInRange() {
         return Randoms.pickUniqueNumbersInRange(Range.MIN.getNum(), Range.MAX.getNum(), LottoInfo.NUM_COUNT.getNum());
-    }
-
-    public int pickNumberInRange() {
-        return Randoms.pickNumberInRange(Range.MIN.getNum(), Range.MAX.getNum());
     }
 }
