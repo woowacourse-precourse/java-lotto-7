1935baf9edccf387e92b67788f2370bd7c8ab659
eunseobb <seobseob0810@naver.com>
2024-11-04 22:23:05 +0900

    refactor(constant): 에러메시지 헤더 분리
    헤더의 존재를 명확하게 하고자 에러메시지의 헤더가 되는 "[ERROR]"을 분리

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -1,12 +1,13 @@
 package lotto.constant;
 
 public enum ErrorMessage {
-    INTEGER_ERROR("[ERROR] 정수가 아닌 값이 입력되었습니다."),
-    THOUSANDS_ERROR("[ERROR] 1000원 단위로 입력해주세요."),
-    NEGATIVE_ERROR("[ERROR] 양수를 입력해주세요."),
-    NUMBER_COUNT_ERROR("[ERROR] 로또 번호는 6개여야 합니다."),
-    RANGE_ERROR("[ERROR] 1~45인 숫자를 입력해주세요."),
-    REPEAT_ERROR("[ERROR] 중복된 숫자가 있습니다."),
+    ERROR_MESSAGE("[ERROR]"),
+    INTEGER_ERROR("정수가 아닌 값이 입력되었습니다."),
+    THOUSANDS_ERROR("1000원 단위로 입력해주세요."),
+    NEGATIVE_ERROR("양수를 입력해주세요."),
+    NUMBER_COUNT_ERROR("로또 번호는 6개여야 합니다."),
+    RANGE_ERROR("1~45인 숫자를 입력해주세요."),
+    REPEAT_ERROR("중복된 숫자가 있습니다."),
     ;
 
     private final String message;
