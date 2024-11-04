7175759856fe1e6c2378940e980bd266e1773738
Seungwan Yoo <ysw99@kakao.com>
2024-11-04 11:43:57 +0900

    refactor: Lotto 도메인 리팩토링 - 일부 검증 로직을 도메인에서 수행하면서 공통 처리, 객체 생성 과정에서 검증 하도록 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final Integer LOTTO_SIZE = 6;
+    private static final Integer LOTTO_MIN_NUM = 1;
+    private static final Integer LOTTO_MAX_NUM = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(final List<Integer> numbers) {
@@ -17,6 +21,7 @@ public class Lotto {
     private void validate(final List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
+        numbers.forEach(this::validateRange);
     }
 
     public List<Integer> getNumbers() {
@@ -24,13 +29,18 @@ public class Lotto {
     }
 
     private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6)
+        if (numbers.size() != LOTTO_SIZE) // 로또 번호가 6자리가 아닐 경우
             throw new IllegalArgumentException(LottoException.LOTTO_LENGTH_NOT_VALID.getMessage());
     }
 
     private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        if (uniqueNumbers.size() != numbers.size())
+        if (uniqueNumbers.size() != numbers.size()) // 로또 번호 중 중복 값이 있을 경우
             throw new IllegalArgumentException(LottoException.WINNING_NUMBERS_DUPLICATED.getMessage());
+    }
+
+    private void validateRange(final Integer num) {
+        if (num < LOTTO_MIN_NUM || num > LOTTO_MAX_NUM) // 입력 받은 당첨 번호 혹은 보너스 번호가 1~45 범위를 벗어날 경우
+            throw new IllegalArgumentException(LottoException.LOTTO_RANGE_NOT_VALID.getMessage());
     }
 }
