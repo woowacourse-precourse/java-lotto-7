1d2e4b4b18d9833c7273c8fc5345fe43e921c897
HyeonJeong <wgwjh05169@gmail.com>
2024-11-04 19:51:05 +0900

    test(LottoTest): 로또 번호 범위에 대한 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoTest {
+    private static final String ERROR_PREFIX = "[ERROR]";
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +24,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 1-45 범위를 벗어나는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_1_45_범위를_벗어나는_숫자가_있으면_예외가_발생한다() {
+
+        assertAll(
+                () -> assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageStartingWith(ERROR_PREFIX),
+
+                () -> assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageStartingWith(ERROR_PREFIX),
+
+                () -> assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 6)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageStartingWith(ERROR_PREFIX)
+        );
+
+    }
 }
