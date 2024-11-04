5bce02dff30e7f6b9192024775691e886fa411dc
jiwon <ljw010512@naver.com>
2024-11-04 15:00:22 +0900

    refactor: LottoSystem 메서드 분리

--- src/main/java/lotto/LottoSystem.java
+++ src/main/java/lotto/LottoSystem.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import static lotto.constant.PurchaseConfig.PURCHASE_UNIT;
+
+import java.util.Map;
 import lotto.constant.LottoConfig;
+import lotto.constant.LottoConfig.Rank;
 import lotto.model.Bonus;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
@@ -10,28 +14,44 @@ import lotto.service.LottoTicketing;
 import lotto.util.InputHandler;
 import lotto.util.OutputHandler;
 
-import java.util.Map;
-
-import static lotto.constant.PurchaseConfig.PURCHASE_UNIT;
-
 public class LottoSystem {
     public void run() {
-        Purchase purchase = InputHandler.repeatInputOrderCost();
+        Purchase purchase = settingPurchase();
+        Lottos lottos = settingLottos(purchase);
 
-        LottoTicketing lottoTicketing = new LottoTicketing();
-        Lottos lottos = lottoTicketing.issueTickets(purchase);
-        int count = purchase.getCost() / PURCHASE_UNIT;
-
-        OutputHandler.printCount(count);
-        OutputHandler.printTicket(lottos);
-
-        Lotto lotto = InputHandler.repeatInputLottoNumber();
-        Bonus bonus = InputHandler.repeatInputBonusNumber(lotto);
+        Lotto lotto = getWinningLottoNumbers();
+        Bonus bonus = getBonusNumber(lotto);
 
         LottoPrize lottoPrize = new LottoPrize(lotto, bonus);
         Map<LottoConfig.Rank, Integer> result = lottoPrize.determineLottoPrizes(lottos);
         Double rateOfReturn = lottoPrize.calculateRateOfReturn(result, purchase);
 
+        printLottoSummary(result, rateOfReturn);
+    }
+
+    private Purchase settingPurchase() {
+        Purchase purchase = InputHandler.repeatInputOrderCost();
+        int count = purchase.getCost() / PURCHASE_UNIT;
+        OutputHandler.printCount(count);
+        return purchase;
+    }
+
+    private Lottos settingLottos(Purchase purchase) {
+        LottoTicketing lottoTicketing = new LottoTicketing();
+        Lottos lottos = lottoTicketing.issueTickets(purchase);
+        OutputHandler.printTicket(lottos);
+        return lottos;
+    }
+
+    private static Lotto getWinningLottoNumbers() {
+        return InputHandler.repeatInputLottoNumber();
+    }
+
+    private static Bonus getBonusNumber(Lotto lotto) {
+        return InputHandler.repeatInputBonusNumber(lotto);
+    }
+
+    private static void printLottoSummary(Map<Rank, Integer> result, Double rateOfReturn) {
         OutputHandler.printLottoResult(result);
         OutputHandler.printRateOfReturn(rateOfReturn);
     }
