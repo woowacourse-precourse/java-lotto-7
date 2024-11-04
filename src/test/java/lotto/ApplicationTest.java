e022792d9689773e7fac46f37f41ce5f6d054b96
dbehdgns1215 <dbehdgns1215@naver.com>
2024-11-04 15:18:30 +0900

    feat(ApplicationTest): 수익률 테스트 케이스 추가
    - 수익률이 0%일 때와 50%일 때의 케이스를 추가하여 로직이 의도한 대로 동작하는지 검증
    - 다양한 수익률 케이스를 통해 기능 테스트 보강

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,17 +1,18 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @DisplayName("8장을 구매해서 5000원에 당첨되면 수익률은 62.5%이다.")
     @Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
@@ -46,6 +47,81 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("8장을 구매해서 하나도 당첨되지 않으면 수익률은 0.0%이다.")
+    @Test
+    void 수익률_0_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 7, 14, 22, 45]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 0.0%입니다."
+                    );
+                },
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 7, 14, 22, 45)
+        );
+    }
+
+    @Test
+    @DisplayName("10장을 구매해서 5000원에 당첨되면 수익률은 50.0%이다.")
+    void 수익률_50_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("10000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "10개를 구매했습니다.",
+                            "[1, 2, 3, 7, 21, 23]",
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[7, 11, 30, 40, 42, 43]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "[1, 3, 7, 14, 22, 45]",
+                            "[8, 15, 22, 27, 35, 41]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 50.0%입니다."
+                    );
+                },
+                List.of(1, 2, 3, 7, 21, 23),
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(7, 11, 30, 40, 42, 43),
+                List.of(2, 13, 22, 32, 38, 45),
+                List.of(1, 3, 7, 14, 22, 45),
+                List.of(8, 15, 22, 27, 35, 41)
+        );
+    }
+
+    @DisplayName("구입 금액이 정수가 아니면 [ERROR] 태그가 붙은 에러 메시지를 출력한다.")
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
