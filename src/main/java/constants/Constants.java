f21cf6ad204d79d39100f529ae650e0fbadee0b9
pakaheo <k70923@konkuk.ac.kr>
2024-11-04 10:24:19 +0900

    refactor : Prohibit instantiate constants

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -4,6 +4,10 @@ import java.text.DecimalFormat;
 
 public class Constants {
 
+    private Constants() {
+        throw new IllegalArgumentException("인스턴스화 금지");
+    }
+
     public static final int LOTTO_SIZE = 6;
     public static final DecimalFormat AMOUNT_NOTATION = new DecimalFormat("#,###.##");
 }
--- src/main/java/constants/ErrorMessage.java
+++ src/main/java/constants/ErrorMessage.java
@@ -2,11 +2,15 @@ package constants;
 
 public class ErrorMessage {
 
+    private ErrorMessage() {
+        throw new IllegalArgumentException("인스턴스화 금지");
+    }
+
     public static final String NOT_MATCH_LOTTO_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
     public static final String EXISTS_DUPLICATE_NUMBER = "[ERROR] 로또 번호에 중복된 숫자가 존재합니다.";
     public static final String INVALID_PURCHASE_AMOUNT = "[ERROR] 구매 금액은 숫자로 입력해야 합니다.";
     public static final String UNDIVIDED_THOUSAND = "[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다.";
     public static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호는 1에서 45 사이 숫자여야 합니다.";
     public static final String ENTERED_INVALID_NUMBER = "[ERROR] 유효하지 않은 입력입니다.";
-    public static final String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복일 수 없습니다";
+    public static final String DUPLICATE_BONUS_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복일 수 없습니다.";
 }
