b844c2b4548c1e1c42e760df5aae5c6e14d89a4b
jangsubin <eltm521@naver.com>
2024-11-04 16:32:24 +0900

    test : 보너스 번호가 당첨 번호와 같은 경우 테스트 작성 refactor : 테스트 검증로직 Validator로 변경

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -3,13 +3,30 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.model.WinningLotto;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.WinningNumberValidator;
 import org.junit.jupiter.api.Test;
 
 public class WinningLottoTest {
 
     @Test
+    void 당첨_번호_입력_테스트() {
+        WinningNumberValidator winningNumberValidator = new WinningNumberValidator();
+
+        assertThatThrownBy(() -> winningNumberValidator.validateInput("1,2,3,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> winningNumberValidator.validateInput("1,2,3'4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> winningNumberValidator.validateInput("1,1,3'4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> winningNumberValidator.validateInput("0,10,30,40,50,60"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         String hasSevenElements = "1,2,3,4,5,6,7";
+
         assertThatThrownBy(() -> new WinningLotto(hasSevenElements, 7))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -43,13 +60,20 @@ public class WinningLottoTest {
 
     @Test
     void 보너스_번호가_1에서_45사이가_아닌_경우_예외가_발생한다() {
+        BonusNumberValidator bonusNumberValidator = new BonusNumberValidator();
         String wrongNumber = "1,2,3,4,5,6";
 
-
-        assertThatThrownBy(() -> new WinningLotto(wrongNumber, 0))
+        assertThatThrownBy(() -> bonusNumberValidator.validateBonusNumber("0", wrongNumber))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new WinningLotto(wrongNumber, 46))
+        assertThatThrownBy(() -> bonusNumberValidator.validateBonusNumber("46", wrongNumber))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @Test
+    void 보너스_번호가_당첨번호와_같은_경우() {
+        BonusNumberValidator bonusNumberValidator = new BonusNumberValidator();
+        assertThatThrownBy(() -> bonusNumberValidator.validateBonusNumber("2", "1,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
