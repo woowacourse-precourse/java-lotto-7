ba701363d14d3a366c8cede126f7d727666c3ffa
Yeongjae Kim <sees77770@gmail.com>
2024-11-04 19:27:32 +0900

    refactor: 불필요한 else 문 제거

--- src/main/java/lotto/domain/winning/Rank.java
+++ src/main/java/lotto/domain/winning/Rank.java
@@ -35,10 +35,9 @@ public enum Rank {
         if (this == SECOND) {
             return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개",
                     matchCount, String.format("%,d", winningMoney), count);
-        } else {
-            return String.format("%d개 일치 (%s원) - %d개",
-                    matchCount, String.format("%,d", winningMoney), count);
         }
+        return String.format("%d개 일치 (%s원) - %d개",
+                matchCount, String.format("%,d", winningMoney), count);
     }
 
     public int getWinningMoney() {
