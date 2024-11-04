30dbb7dd060170bdc6b5a3db93ebd30b90eed64c
문준일 <answnsdlfwkd@naver.com>
2024-11-04 22:55:07 +0900

    feat: handler 클래스 추가

---
+++ src/main/java/lotto/handler/InputHandler.java
@@ -0,0 +1,62 @@
+package lotto.handler;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+import lotto.BonusNumber;
+import lotto.Lotto;
+import lotto.PurchaseAmount;
+import lotto.SpecialNumber;
+import lotto.WinningNumber;
+
+public class InputHandler {
+
+    public PurchaseAmount inputPurChaseAmount(){
+
+        System.out.println("구입금액을 입력해 주세요.");
+
+        while(true){
+            try{
+                PurchaseAmount purchaseAmount = new PurchaseAmount(Console.readLine());
+                return purchaseAmount;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public WinningNumber inputWinningLottoNumber(){
+
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Lotto winningRegularLotto = getWinningRegularLotto();
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return getWinningNumber(winningRegularLotto);
+    }
+
+    private  WinningNumber getWinningNumber(final Lotto winningRegularLotto) {
+        while(true){
+            try{
+                BonusNumber specialNumber = new BonusNumber(Console.readLine());
+                WinningNumber winningNumber = new WinningNumber(winningRegularLotto,specialNumber);
+                return winningNumber;
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private  Lotto getWinningRegularLotto() {
+        while(true){
+            try{
+                Lotto winningRegularLotto = Lotto.createWinningRegularLotto(Console.readLine());
+                return winningRegularLotto;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
