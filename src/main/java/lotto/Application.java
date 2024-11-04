a51f35da20ad06db242bbfcc11b1fbff07ab9a24
진민규 <jinmingyu@jinmingyuui-MacBookPro.local>
2024-11-04 17:59:52 +0900

    feat : 입력 기능 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money = inputMoney();
+        System.out.println("구입 금액 : " + money);
+    }
+
+    private static int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        errorMoney(amount);
+        return amount;
+    }
+
+    private static void errorMoney(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
     }
 }
