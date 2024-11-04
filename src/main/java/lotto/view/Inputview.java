89fa5bf4000166035253ae6b412cdf482762921a
YunCheol1 <naro2727@naver.com>
2024-11-04 21:48:07 +0900

    feat(Inputview): 입력값 유효성검사 메서드 이동 및 로직수정 -유효성 검사 메서드를 InputHandler로 이동

--- src/main/java/lotto/view/Inputview.java
+++ src/main/java/lotto/view/Inputview.java
@@ -1,23 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import lotto.util.Validator;
 
 public class Inputview {
 
     // 구입 금액 입력
-    public static int inputMoney() {
+    public static String inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        while (true) {
-            try {
-                int money = Validator.validateAndParseNumber(Console.readLine()); // 숫자 형식 검사
-                Validator.validatePurchaseAmount(money); // 1,000원 단위 검사
-                return money;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return Console.readLine(); // String으로 반환
     }
 
     // 당첨 번호 입력
@@ -27,16 +17,8 @@ public class Inputview {
     }
 
     // 보너스 번호 입력
-    public static int inputBonusNumber() {
+    public static String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        while (true) {
-            try {
-                int bonusNumber = Validator.validateAndParseNumber(Console.readLine()); // 숫자 형식 검사 및 파싱
-                Validator.validateLottoNumberRange(List.of(bonusNumber)); // 보너스 번호 범위 검사
-                return bonusNumber;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return Console.readLine();
     }
 }
