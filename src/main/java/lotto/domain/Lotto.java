28bdfd9efdc523e09bc0969e774df4b76c9d70f7
18hailey <hailey9459@gmail.com>
2024-11-04 21:18:11 +0900

    feat(Lotto): create Lotto class with internal validation process

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constant.Constant;
+import lotto.error.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +15,41 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != Constant.LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBERS.getMessage());
+        }
+        if (!validateDuplication(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_DUPLICATE.getMessage());
+        }
+        if (!validateInRange(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_NOT_IN_RANGE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean validateDuplication(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() == Constant.LOTTO_SIZE;
+
+    }
+
+    private boolean validateInRange(List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(number -> Constant.MIN_NUMBER <= number && number <= Constant.MAX_NUMBER);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void printNumbers() {
+        System.out.print("[");
+        for (int i=0; i < Constant.LOTTO_SIZE; i++) {
+            if(i == numbers.size() - 1) {
+                System.out.print(numbers.get(i));
+                continue;
+            }
+            System.out.print(numbers.get(i) + Constant.COMMA_SEPARATOR + " ");
+        }
+        System.out.println("]");
+    }
 }
