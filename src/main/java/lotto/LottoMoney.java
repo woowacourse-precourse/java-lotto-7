d545356b73c78a61576ba299d66d3e61f657293f
JuSeong1130 <jsmoon1130@naver.com>
2024-11-04 10:24:42 +0900

    feat(LottoMoney) : 생성 기능 구현
    생서 시 0원이 아니고 1000원 단위인지 유효성 검증

---
+++ src/main/java/lotto/LottoMoney.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public class LottoMoney {
+
+    private int money;
+
+    public LottoMoney(int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+    private void validateMoney(int money) {
+        if (money != 0 && money % 1000 != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000원 단위여야 합니다."); // 예외 발생
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
