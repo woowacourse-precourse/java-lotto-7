123f1c87631fa1ef78fc2693ee4a7be103cdde48
heeyeop Kim <ssucgoo@gmail.com>
2024-11-04 23:19:29 +0900

    fix: Rank 필드 명 및 값 변경

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -3,14 +3,14 @@ package lotto;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST("1등", 2_000_000_000, 60), SECOND("2등", 30_000_000, 51), THIRD("3등", 1_500_000, 50), FOURTH("4등", 50_000, 40), FIFTH("5등", 5_000, 30), ETC("기타", 0, -1);
+    FIRST("6개 일치", 2_000_000_000, 60), SECOND("5개 일치, 보너스 볼 일치", 30_000_000, 51), THIRD("5개 일치", 1_500_000, 50), FOURTH("4개 일치", 50_000, 40), FIFTH("3개 일치", 5_000, 30), ETC("2개 미만", 0, -1);
 
-    private String name;
+    private String condition;
     private int prize;
     private int score;
 
-    Rank(String name, int prize, int score) {
-        this.name = name;
+    Rank(String condition, int prize, int score) {
+        this.condition = condition;
         this.prize = prize;
         this.score = score;
     }
