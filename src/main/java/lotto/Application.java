1a05fdd4d79a8bfd2828616d62ed603ee7597124
seyeonsim <seyeonsimw@gmail.com>
2024-11-04 23:19:50 +0900

    feat: 구입 금액 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static int getPurchaseAmount() {
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                int amount = Integer.parseInt(Console.readLine());
+                if (amount % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+                }
+                return amount;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
