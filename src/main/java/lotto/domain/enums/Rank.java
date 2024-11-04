54d1a34b0f531f41ad064f0428ed032455c17628
mikekks <thdalsrb79@naver.com>
2024-11-04 13:47:06 +0900

    refactor(Rank): Rank enum 에 출력문 추가

--- src/main/java/lotto/domain/enums/Rank.java
+++ src/main/java/lotto/domain/enums/Rank.java
@@ -4,20 +4,22 @@ import java.util.Optional;
 
 public enum Rank {
 
-	FIFTH(3, 5_000),
-	FOURTH(4, 50_000),
-	THIRD(5, 1_500_000),
-	SECOND(5, 30_000_000),
-	FIRST(6, 2_000_000_000),
+	FIFTH(3, 5_000, "3개 일치 (5,000원) - %d개"),
+	FOURTH(4, 50_000, "4개 일치 (50,000원) - %d개"),
+	THIRD(5, 1_500_000, "5개 일치 (1,500,000원) - %d개"),
+	SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+	FIRST(6, 2_000_000_000, "6개 일치 (2,000,000,000원) - %d개"),
 	;
 
-	Rank(int matchCount, int prizeMoney) {
+	Rank(int matchCount, int prizeMoney, String winningMessage) {
 		this.matchCount = matchCount;
 		this.prizeMoney = prizeMoney;
+		this.winningMessage = winningMessage;
 	}
 
 	private final int matchCount;
 	private final int prizeMoney;
+	private final String winningMessage;
 
 	public static Optional<Rank> judgeBy(int winCount, boolean isBonusNumberWin) {
 		if (winCount == FIRST.matchCount) {
@@ -33,5 +35,17 @@ public enum Rank {
 		}
 
 		return Optional.empty();
+	}
+
+	public int getMatchCount(){
+		return matchCount;
+	}
+
+	public int getPrizeMoney(){
+		return prizeMoney;
+	}
+
+	public String getWinningMessage(){
+		return winningMessage;
 	}
 }
