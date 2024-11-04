a92e653010f8f6d4b62b3f1e5de2c23049724eb6
moonbinchoi <whsahd12@gmail.com>
2024-11-04 20:22:17 +0900

    test(Lotto): 단위 테스트 추가 및 원인 예외 검증

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,16 +1,16 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import lotto.LottoNumbersTestCase;
+import lotto.domain.exception.LottoException;
 import lotto.global.common.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
@@ -20,16 +20,21 @@ class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(LottoException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(LottoException.class);
     }
 
+    @Test
+    void 로또_번호가_1미만_45초과일_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 99)))
+                .isInstanceOf(LottoException.class);
+    }
 
     @ParameterizedTest
     @EnumSource(value = LottoNumbersTestCase.class)
