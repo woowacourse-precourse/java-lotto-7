87c124fce6637ebdd5593e1a275ab53538ea7dbc
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-11-04 20:05:08 +0900

    feat: 당첨 번호, 보너스 번호 입력 기능 구현
    - 당첨 번호를 입력 받아 lottoNumber 변수에 저장 (parsing을 위해 String으로 입력 받음)
    - 보너스 번호를 입력 받아 bonusNumber 변수에 저장
    - 보너스 번호를 숫자로 입력하지 않을 경우 IllegalArgumentException 반환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,5 +10,15 @@ public class Application {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 숫자로 입력해야 합니다.");
         }
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String lottoNumber = Console.readLine();
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            int bonusNumber = Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 번호는 숫자로 입력해야 합니다.");
+        }
     }
 }
