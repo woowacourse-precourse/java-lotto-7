9d8bb8725b7f825956db4c9b6cb62941222db4d3
bogyeom0922 <pbk0398@gmail.com>
2024-11-04 14:33:11 +0900

    Refactor: 실행 결과 예시에 맞는 출력 문구 수정

--- src/main/java/lotto/view/InputReader.java
+++ src/main/java/lotto/view/InputReader.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputReader {
 
     public String inputPurchaseAmount() {
-        System.out.println("구매금액을 입력해 주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine().trim();
     }
 
