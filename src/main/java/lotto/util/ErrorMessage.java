8a0e8b3d4ae224c31db1f00c22926e62f99d12eb
GiSeok Kang <ime.rokz@gmail.com>
2024-11-04 10:51:30 +0900

    refactor: ErrorMessage 상수 제거 후 LottoMachine 상수로 통일

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -11,7 +11,7 @@ public enum ErrorMessage {
     MONEY_NOT_MODED_PRICE(String.format("[ERROR] 돈은 %d원 단위로 입력해야 합니다.", LottoMachine.LOTTO_PRICE)),
     LOTTO_SIZE_OUT_OF_RANGE("[ERROR] 로또 번호는 6개여야 합니다."),
     LOTTO_NUM_OUT_OF_RANGE(String.format("[ERROR] 로또 번호는 %d와 %d 사이의 숫자여야 합니다.",
-            Constants.LOTTO_RANGE_START, Constants.LOTTO_RANGE_END)),
+            LottoMachine.LOTTO_NUM_START, LottoMachine.LOTTO_NUM_END)),
     LOTTO_NUMS_DUPLICATION("[ERROR] 로또 번호가 중복됩니다."),
     LOTTO_BONUS_NUM_DUPLICATION("[ERROR] 보너스 번호가 로또 번호와 중복됩니다."),
     LOTTO_NUM_NOT_NUMBER("[ERROR] 로또 번호는 숫자여야 합니다.");
@@ -28,10 +28,5 @@ public enum ErrorMessage {
 
     public static void showErrorMsg(String msg) {
         System.out.println(msg);
-    }
-
-    private static class Constants {
-        public static final int LOTTO_RANGE_START = 1;
-        public static final int LOTTO_RANGE_END = 45;
     }
 }
