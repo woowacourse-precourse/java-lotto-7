434c1c727a55bbeac81425f7b893edb159a5790d
kyunghoon01 <sports0124@naver.com>
2024-11-04 14:36:44 +0900

    refactor: 상수화

--- src/main/java/view/LottoView.java
+++ src/main/java/view/LottoView.java
@@ -9,7 +9,12 @@ import lotto.LottoResult;
 import lotto.PurchaseCount;
 
 public class LottoView {
-    private final static int LOTTO_PRICE = 1_000;
+    private static final int LOTTO_PRICE = 1_000;
+    private static final int THREE_MATCH_PRIZE = 5_000;
+    private static final int FOUR_MATCH_PRIZE = 50_000;
+    private static final int FIVE_MATCH_PRIZE = 1_500_000;
+    private static final int FIVE_MATCH_WITH_BONUS_PRIZE = 30_000_000;
+    private static final int SIX_MATCH_PRIZE = 2_000_000_000;
 
     public String inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -47,11 +52,13 @@ public class LottoView {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - " + lottoResult.getThreeMatchesCount() + "개");
-        System.out.println("4개 일치 (50,000원) - " + lottoResult.getFourMatchesCount() + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + lottoResult.getFiveMatchesCount() + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + lottoResult.getFiveMatchesWithBonusCount() + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + lottoResult.getSixMatchesCount() + "개");
+        System.out.println("3개 일치 (" + THREE_MATCH_PRIZE + "원) - " + lottoResult.getThreeMatchesCount() + "개");
+        System.out.println("4개 일치 (" + FOUR_MATCH_PRIZE + "원) - " + lottoResult.getFourMatchesCount() + "개");
+        System.out.println("5개 일치 (" + FIVE_MATCH_PRIZE + "원) - " + lottoResult.getFiveMatchesCount() + "개");
+        System.out.println(
+                "5개 일치, 보너스 볼 일치 (" + FIVE_MATCH_WITH_BONUS_PRIZE + "원) - " + lottoResult.getFiveMatchesWithBonusCount()
+                        + "개");
+        System.out.println("6개 일치 (" + SIX_MATCH_PRIZE + "원) - " + lottoResult.getSixMatchesCount() + "개");
         System.out.println("총 수익률은 " + lottoResult.getYield(lottoCount * LOTTO_PRICE) + "%입니다.");
     }
 }
