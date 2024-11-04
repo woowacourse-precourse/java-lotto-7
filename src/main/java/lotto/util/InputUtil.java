607165bd574c6fec4e5dc6c942eca6d9b7f73ff3
byunghyunkim0 <bb05qudgus@gamil.com>
2024-11-04 15:43:57 +0900

    feat: 로또 번호 리스트 생성 기능 구현

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,13 +1,29 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputUtil {
     static final String DELIMITER = ",";
+
     public String readInput() {
         String input = Console.readLine();
         Validator.validateNullInput(input);
         Validator.validateWhitespaceInput(input);
         return input;
+    }
+
+    public List<Integer> parseInputToLottoList(String input) {
+        Validator.validateRightDelimiter(input);
+        String[] numbers = input.split(DELIMITER);
+        List<Integer> lottoList = new ArrayList<>();
+        for (String number : numbers) {
+            Validator.validateNullInput(number);
+            Validator.validateWhitespaceInput(number);
+            int num = Validator.validateLottoNumber(number);
+            lottoList.add(num);
+        }
+        return lottoList;
     }
 }
