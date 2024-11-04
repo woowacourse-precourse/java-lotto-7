c6c10b07a9cf1ac8f0797103c7cf1643d112bf9d
gichan222 <>
2024-11-04 18:12:45 +0900

    test(Validator) : 당첨 번호에 대한 성공/실패 케이스
    당첨 번호 범위가 아닌 경우
    당첨 번호 개수가 아닌 경우
    입력이 없는 경우
    당첨 번호가 정상인 경우

--- src/test/java/lotto/global/ValidatorTest.java
+++ src/test/java/lotto/global/ValidatorTest.java
@@ -79,4 +79,56 @@ public class ValidatorTest {
                     .hasMessage(NOT_POSITIVE_INPUT.getMessage());
         }
     }
+
+    @Nested
+    @DisplayName("당첨 번호 테스트")
+    class ValidateWinningNumbersTests {
+
+        @Test
+        @DisplayName("1-45 사이의 숫자가 아닌 경우 예외 발생")
+        void 당첨_번호_범위가_아니면_예외가_발생한다() {
+            // given
+            String invalidWinningNumbers = "1,2,3,4,5,46";
+
+            // when & then
+            assertThatThrownBy(() -> validator.validateWinningNumbers(invalidWinningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INVALID_NUMBER_RANGE.getMessage());
+        }
+
+        @Test
+        @DisplayName("6개가 아닌 경우 예외 발생")
+        void 당첨_번호_6개가_아니면_예외가_발생한다() {
+            // given
+            String invalidWinningNumbers = "1,2,3,4,5";
+
+            // when & then
+            assertThatThrownBy(() -> validator.validateWinningNumbers(invalidWinningNumbers))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(INVALID_WINNING_NUMBERS_INPUT.getMessage());
+        }
+
+        @Test
+        @DisplayName("빈입력인 경우 예외 발생")
+        void 당첨_번호_빈입력이면_예외가_발생한다() {
+            // when & then
+            invalidInputs.forEach(input ->
+                    assertThatThrownBy(() -> validator.validateWinningNumbers(input))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage(EMPTY_INPUT.getMessage())
+            );
+        }
+
+        @Test
+        @DisplayName("정상적인 당첨 번호 입력 시 예외 발생하지 않음")
+        void 당첨_번호_정상_입력_테스트() {
+            // given
+            String validWinningNumbers = "1,2,3,4,5,6"; // 예를 들어, 1~6의 정상 입력
+
+            // when & then
+            assertThatCode(() -> validator.validateWinningNumbers(validWinningNumbers))
+                    .doesNotThrowAnyException(); // 예외가 발생하지 않음을 확인
+        }
+    }
+
 }
