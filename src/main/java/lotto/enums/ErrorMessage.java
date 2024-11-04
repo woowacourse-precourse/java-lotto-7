0d9e99e58579bc3eca6876c4aff649b26cdef14e
yereumi <lovelyyeji01@gmail.com>
2024-11-04 18:33:02 +0900

    fix(ErrorMessage): INVALID_LENGTH 추가

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
     // 에러 메시지
     INVALID_LOTTO_COUNT("[ERROR] 로또 구입 금액은 1000으로 나누어 떨어지는 숫자여야 합니다."),
     INVALID_NUMBERS("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_LENGTH("[ERROR] 로또는 6자리의 숫자여야 합니다."),
     DUPLICATE_NUMBERS("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
 
     private final String message;
