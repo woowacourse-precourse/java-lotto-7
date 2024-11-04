98a75088b2c001d89b27aee477711d7cbbf5b248
Youn Sang Hyun <younsang523@gmail.com>
2024-11-04 19:45:02 +0900

    feat: 로또 발행 기능 구현

--- src/main/java/lotto/Domain/LottoMachine.java
+++ src/main/java/lotto/Domain/LottoMachine.java
@@ -1,4 +1,32 @@
 package lotto.Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Messages.ErrorMessages;
+
 public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static List<Lotto> issue(int amount) {
+        validatePurchaseAmount(amount);
+        int count = amount / LOTTO_PRICE;
+        return generateLottos(count);
+    }
+
+    private static void validatePurchaseAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_NEGATIVE.message);
+        }
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessages.MONEY_UNIT.message);
+        }
+    }
+
+    private static List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(new LottoNumber().getNumbers()));
+        }
+        return lottos;
+    }
 }
