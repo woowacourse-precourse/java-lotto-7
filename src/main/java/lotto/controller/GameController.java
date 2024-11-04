d4395606d6a71b20f6042be1d483c2e4863a81e4
davemins <davemins4125@naver.com>
2024-11-04 23:14:45 +0900

    refactor(Controller): 전체적인 게임 진행을 담당하는 구조로 변경

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,101 +1,22 @@
 package lotto.controller;
 
-import lotto.domain.Lotto;
-import lotto.service.*;
-import lotto.view.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class GameController {
-    private String amount;
-    private int numberOfLottoPurchased;
-    private List<Lotto> userLottoNumbers;
-    private List<Integer> winnerLottoNumbers;
-    private Lotto winnerLottoNumber;
-    private int bonusNumber;
-    private int prize = 0;
+    private LottoPurchaseController lottoPurchaseController = new LottoPurchaseController();
+    private LottoResultController lottoResultController = new LottoResultController();
+    private LottoPrizeController lottoPrizeController = new LottoPrizeController();
 
     public void play() {
         try {
-            getAmount();
-            printNumberOfLottoPurchased();
-            generateLotto();
-            getWinnerNumber();
-            getBonusNumber();
-            getPrize();
-            printPrize();
+            lottoPurchaseController.purchaseLotto();
+            lottoResultController.inputWinnerNumbers();
+            lottoPrizeController.calculatePrize(
+                    lottoResultController.getWinnerLottoNumber(),
+                    lottoResultController.getBonusNumber(),
+                    lottoPurchaseController.getUserLottoNumbers(),
+                    lottoPurchaseController.getAmount()
+            );
         } catch (IllegalArgumentException exception) {
             System.out.println(exception.getMessage());
         }
-    }
-
-    private void getAmount() {
-        while (true) {
-            try {
-                amount = InputView.AMOUNT.getInput();
-                Validator.validateAmount(amount);
-                break;
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-        AmountCalculator amountCalculator = new AmountCalculator(Integer.parseInt(amount));
-        numberOfLottoPurchased = amountCalculator.getNumberOfLottoPurchased();
-    }
-
-    private void printNumberOfLottoPurchased() {
-        OutputView.printNumberOfLottoPurchased(numberOfLottoPurchased);
-    }
-
-    private void generateLotto() {
-        userLottoNumbers = new ArrayList<>();
-        for (int i = 0; i < numberOfLottoPurchased; i++) {
-            LottoMachine lottoMachine = new LottoMachine();
-            userLottoNumbers.add(new Lotto(lottoMachine.getRandomNumbers()));
-        }
-        OutputView.printLottos(userLottoNumbers);
-        System.out.println();
-    }
-
-    private void getWinnerNumber() {
-        while (true) {
-            try {
-                String winnerNumber = InputView.LOTTO_NUMBER.getInput();
-                String[] winnerNumberItems = winnerNumber.split(",");
-                winnerLottoNumbers = Arrays.stream(winnerNumberItems).map(Integer::parseInt).collect(Collectors.toList());
-                winnerLottoNumber = new Lotto(winnerLottoNumbers);
-                break;
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-        System.out.println();
-    }
-
-    private void getBonusNumber() {
-        while (true) {
-            try {
-                String bonusNumberItem = InputView.BONUS_NUMBER.getInput();
-                bonusNumber = Integer.parseInt(bonusNumberItem);
-                Validator.validateBonusNumber(winnerLottoNumbers, bonusNumber);
-                break;
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-        System.out.println();
-    }
-
-    private void getPrize() {
-        for (Lotto userNumber : userLottoNumbers) {
-            prize += LottoCalculator.calculatePrize(userNumber.getNumbers(), winnerLottoNumber.getNumbers(), bonusNumber);
-        }
-    }
-
-    private void printPrize() {
-        OutputView.printStatics(LottoCalculator.getFinalMatchNumbers(), RateOfReturnCalculator.calculateRateOfReturn(Integer.parseInt(amount), prize));
     }
 }
