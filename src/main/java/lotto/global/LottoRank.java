17b61228d4ad2475f77253f7b0f3a483f9c37b8a
heehun <slrkqk2@naver.com>
2024-11-04 10:44:03 +0900

    refactor(LottoRank): enum 선언 순서 변경

--- src/main/java/lotto/global/LottoRank.java
+++ src/main/java/lotto/global/LottoRank.java
@@ -3,11 +3,11 @@ package lotto.global;
 import static lotto.global.error.LottoErrorMessages.INVALID_MATCH_COUNT;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000,true),
-    THIRD(5, 1_500_000),
+    FIFTH(3, 5_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000,true),
+    FIRST(6, 2_000_000_000);
 
     private final int matchCount;
     private final int prize;
