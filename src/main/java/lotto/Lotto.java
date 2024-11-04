0fde329132f74ffe163f20e2513bd42d2c521050
hekim52 <acrossnightsky@gmail.com>
2024-11-04 14:38:58 +0900

    feat: 모든 기능
    모든 기능을 완성했다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +21,16 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private void duplicate(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        List<Integer> duplicate = new ArrayList<>(set);
+        if (duplicate.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
+        }
+    }
 }
