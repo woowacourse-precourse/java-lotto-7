11a1b0d84bd2f095f783d33b5526df6fee68440a
codingAtEdgeOfCliff <codinglsb@naver.com>
2024-11-04 11:57:37 +0900

    feat(Lotto): validate 기능 추가, get 메서드 생성, toString 생성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,12 +1,35 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        Set<Integer> checkedDuplicateNumbers = new HashSet<>(numbers);
+        if (numbers.size() != checkedDuplicateNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers + "\n";
     }
 
     // TODO: 추가 기능 구현
