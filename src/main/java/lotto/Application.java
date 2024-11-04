9e64f122cf707d41d3c008ae436702ccd2efd43e
shortboy7 <thdwldnd7@naver.com>
2024-11-04 19:22:21 +0900

    feat: 각 컴포넌트에게 메시지를 전달하는 App 인터페이스 추가 및 CLI App 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import lotto.app.CLILottoApp;
+import lotto.app.LottoApp;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LottoApp app = new CLILottoApp(br, bw);
+        app.run();
+        try {
+            br.close();
+            bw.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } 
     }
 }
---
+++ src/main/java/lotto/app/CLILottoApp.java
@@ -0,0 +1,90 @@
+package lotto.app;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import lotto.Lotto;
+import lotto.LottoAnswer;
+import lotto.LottoFactory;
+import lotto.LottoResult;
+import lotto.LottoStatistics;
+import lotto.exception.LottoException;
+import lotto.interaction.input.LottoInput;
+import lotto.interaction.input.LottoStandardInput;
+import lotto.interaction.output.LottoOutput;
+import lotto.interaction.output.LottoStandardOutput;
+
+public class CLILottoApp implements LottoApp{
+    private final LottoOutput output;
+    private final LottoInput input;
+
+    public CLILottoApp(BufferedReader br, BufferedWriter bw) {
+        this.output = new LottoStandardOutput(bw);
+        this.input = new LottoStandardInput(br);
+    }
+
+    private <T> T assertInput(Supplier<T> inputFunction) {
+        while (true) {
+            try {
+                return inputFunction.get();
+            } catch (LottoException exception) {
+                output.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+
+    private Integer assertGetBonusNumber(Function<List<Integer>, Integer> inputFunction, List<Integer> winningNumbers) {
+        while (true) {
+            try {
+                return inputFunction.apply(winningNumbers);
+            } catch (LottoException exception) {
+                output.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+
+    private int getMoney() {
+        output.printToInputPurchaseMoney();
+        return assertInput(input::inputPurchaseMoney);
+    }
+
+    private List<Lotto> generateLotto(int money) {
+        List<Lotto> lottos = LottoFactory.getInstance().buyLotto(money);
+        output.printPurchasedLotto(lottos);
+        return lottos;
+    }
+
+    private LottoAnswer getAnswer() {
+        output.printToInputWinningNumber();
+        List<Integer> winningNumbers = assertInput(input::inputWinningNumbers);
+        output.printToInputBonusNumber();
+        Integer bonusNumber = assertGetBonusNumber(input::inputBonusNumber, winningNumbers);
+        return new LottoAnswer(winningNumbers, bonusNumber);
+    }
+
+    private List<Optional<LottoResult>> collectLottoResults(List<Lotto> lottoList, LottoAnswer answer) {
+        return lottoList.stream()
+                .map(lotto -> lotto.getResult(answer))
+                .toList();
+    }
+
+    private void outputResultSummary(List<Optional<LottoResult>> lottoResults) {
+        Map<LottoResult, Integer> resultMap = LottoStatistics.countResult(lottoResults);
+        double rateOfReturn = LottoStatistics.getRateOfReturn(lottoResults);
+        output.printWinningStatistics(resultMap, rateOfReturn);
+    }
+
+
+    @Override
+    public void run() {
+        int money = getMoney();
+        List<Lotto> lottoList = generateLotto(money);
+        LottoAnswer answer = getAnswer();
+        List<Optional<LottoResult>> lottoResults = collectLottoResults(lottoList, answer);
+        outputResultSummary(lottoResults);
+    }
+}
