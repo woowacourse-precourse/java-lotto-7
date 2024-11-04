bd83d2c8d90f80f970172e2024951df8be6bfe71
YOON JAEHYEON <yoonjaehyeon@YOONui-MacBookPro.local>
2024-11-04 22:17:16 +0900

    로또 번호 유효성 검사 예외 처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_NUMBER_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -13,6 +14,9 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 
