847052e059be7798e51e86b646b9a827bfe07b77
ParkSangsin <pss6039@naver.com>
2024-11-04 22:28:03 +0900

    refactor: 에러 메세지 형식 수정

--- src/main/java/lotto/exception/InvalidInputMoneyDivideException.java
+++ src/main/java/lotto/exception/InvalidInputMoneyDivideException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class InvalidInputMoneyDivideException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "로또 구입 금액은 1000원 단위로 입력해야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 1000원 단위로 입력해야 합니다.";
 
     public InvalidInputMoneyDivideException() {
         super(ERROR_MESSAGE);
--- src/main/java/lotto/exception/InvalidInputMoneyZeroException.java
+++ src/main/java/lotto/exception/InvalidInputMoneyZeroException.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public class InvalidInputMoneyZeroException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "로또 구입 금액은 0보다 커야 합니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 0보다 커야 합니다.";
 
     public InvalidInputMoneyZeroException() {
         super(ERROR_MESSAGE);
