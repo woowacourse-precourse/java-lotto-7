175e331195eaee2b71c28055cb137052d7aa1b33
DaehoGo <rualneox@dgu.ac.kr>
2024-11-04 19:14:38 +0900

    test(winningLotto): 생성자 수정으로 인한 테스트 코드 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +14,10 @@ class WinningLottoTest {
     @Test
     void 올바른_당첨_번호와_보너스_번호_입력() {
         // given
-        String validWinningNumbers = "1,2,3,4,5,6";
-        String validBonusNumber = "7";
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
         // when
-        WinningLotto winningLotto = new WinningLotto(validWinningNumbers, validBonusNumber);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
         // then
         assertThat(winningLotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
         assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
@@ -24,7 +26,7 @@ class WinningLottoTest {
     @DisplayName("6개 번호가 일치하면 1등을 반환한다.")
     @Test
     void 여섯_개_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         Rank rank = winningLotto.match(lotto);
@@ -35,7 +37,7 @@ class WinningLottoTest {
     @DisplayName("5개 번호와 보너스 번호가 일치하면 2등을 반환한다.")
     @Test
     void 다섯_개_번호_및_보너스_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
 
         Rank rank = winningLotto.match(lotto);
@@ -46,7 +48,7 @@ class WinningLottoTest {
     @DisplayName("5개 번호만 일치하고 보너스 번호가 일치하지 않으면 3등을 반환한다.")
     @Test
     void 다섯_개_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
 
         Rank rank = winningLotto.match(lotto);
@@ -57,7 +59,7 @@ class WinningLottoTest {
     @DisplayName("4개 번호가 일치하면 4등을 반환한다.")
     @Test
     void 네_개_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 8, 9));
 
         Rank rank = winningLotto.match(lotto);
@@ -68,7 +70,7 @@ class WinningLottoTest {
     @DisplayName("3개 번호가 일치하면 5등을 반환한다.")
     @Test
     void 세_개_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 3, 8, 9, 10));
 
         Rank rank = winningLotto.match(lotto);
@@ -79,7 +81,7 @@ class WinningLottoTest {
     @DisplayName("2개 이하의 번호만 일치하면 당첨되지 않음(NONE)을 반환한다.")
     @Test
     void 두_개_이하_번호_일치() {
-        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", "7");
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
         Lotto lotto = new Lotto(List.of(1, 2, 8, 9, 10, 11));
 
         Rank rank = winningLotto.match(lotto);
