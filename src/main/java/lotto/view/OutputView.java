0a2f7c107aefc40f557bc37cfcd646643e33686f
Hyso <hsk7953@gmail.com>
2024-11-04 19:50:37 +0900

    feat(OutputView) : OutputView 에 로또 통계 결과 출력 기능 추가 - 구매한 로또의 갯수와 무작위 번호 출력 구현 - 당첨 통계 및 수익률 출력 구현 - 예외 메시지 출력 매개변수 수정(refactor)   - System.err.println 사용시 순서 보장 안됨 - docs 업데이트

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,11 @@
 package lotto.view;
 
+import lotto.enums.Rank;
 import lotto.enums.RequestMessage;
+import lotto.model.LottoStatistic;
+import lotto.model.LottoTicket;
+
+import java.util.Arrays;
 
 public class OutputView {
     private static final String ERROR_FORMAT = "[ERROR] ";
@@ -17,8 +22,31 @@ public class OutputView {
         System.out.println(RequestMessage.BONUS_NUMBER.getMessage());
     }
 
-    public void printErrorMessage(String errorMessage) {
-        System.err.println(ERROR_FORMAT + errorMessage);
+    public void printErrorMessage(IllegalArgumentException exception) {
+        System.out.println(ERROR_FORMAT + exception.getMessage());
+    }
+
+    public void printLottoTickets(LottoTicket lottoTicket){
+        System.out.printf("%d개를 구매했습니다.\n", lottoTicket.getQuantity());
+        printLotteries(lottoTicket);
+    }
+    private void printLotteries(LottoTicket lottoTicket){
+        StringBuilder printer = new StringBuilder();
+        lottoTicket.getLotteries().stream().forEach(lotto ->
+                printer.append(lotto.getNumbers()).append("\n"));
+        System.out.println(printer);
+    }
+
+    public void printLottoStatistics(LottoStatistic lottoStatistic){
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.NONE)
+                .forEach(rank -> {
+                    int count = lottoStatistic.getRankCount().getOrDefault(rank,0);
+                    System.out.printf("%s (%,d원) - %d개\n", rank.getDescription(),rank.getPrize(),count);
+                });
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", lottoStatistic.getReturnRate());
     }
 
 }
