8511867938dfae60aa621c7fee848806a099ef2b
sonshn <sonshn@naver.com>
2024-11-04 21:00:38 +0900

    feat(LottoService): 로또 복권 생성 호출 함수 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,29 @@
 package lotto.service;
 
+import lotto.lotto.Lotto;
 import lotto.utils.PurchaseAmount;
+
+import java.util.List;
 
 public class LottoService {
 
-    public int makeLotteryTickets(String purchaseAmountInput) {
+    private final Lotto lotto;
+
+    public LottoService(Lotto lotto) {
+        this.lotto = lotto;
+    }
+
+    public int calculateNumberOfLotteryTickets(String purchaseAmountInput) {
         PurchaseAmount.validateInput(purchaseAmountInput);
         int castedPurchaseAmount = PurchaseAmount.validatePurchaseAmount(purchaseAmountInput);
         int numberOfTickets = PurchaseAmount.calculateNumberOfLotteryTickets(castedPurchaseAmount);
 
         return numberOfTickets;
+    }
+
+    public List<List<Integer>> makeLotteryTickets(int numberOfTickets) {
+        List<List<Integer>> lotteryTickets = lotto.generateLotteryTickets(numberOfTickets);
+        return lotteryTickets;
     }
     
 }
