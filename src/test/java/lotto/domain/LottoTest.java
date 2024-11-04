cefaf5ee11f6edb3045dafa37ea78e8a80e22842
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-04 21:56:08 +0900

    test: 로또 번호에 중복된 숫자가 있으면 예외가 발생한다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -21,5 +21,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_0_에서_45_사이가_아니라면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 100)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
