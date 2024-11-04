f2c19aed825b8a3d8dd16d9c44fd24980d124d3b
hyuntae99 <hyuntae9912@naver.com>
2024-11-04 15:21:54 +0900

    feat: WinningStatistics 클래스 구현

---
+++ src/main/java/lotto/service/WinningStatistics.java
@@ -0,0 +1,33 @@
+package lotto.service;
+
+import lotto.domain.Rank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningStatistics {
+    private final Map<Rank, Integer> rankCounts = new HashMap<>();
+
+    public WinningStatistics() {
+        for (Rank rank : Rank.values()) {
+            rankCounts.put(rank, 0);
+        }
+    }
+
+    public void addRank(Rank rank) {
+        rankCounts.put(rank, rankCounts.get(rank) + 1);
+    }
+
+    public int getCount(Rank rank) {
+        return rankCounts.get(rank);
+    }
+
+    public double calculateProfitRate(int purchaseAmount) {
+        long totalPrize = 0;
+        for (Rank rank : Rank.values()) {
+            totalPrize += (long) rank.getPrize() * rankCounts.get(rank);
+        }
+        double profitRate = (double) totalPrize / purchaseAmount * 100;
+        return Math.round(profitRate * 10) / 10.0; // 소수점 둘째 자리에서 반올림
+    }
+}
