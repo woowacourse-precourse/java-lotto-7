64c20108a74a1c5ce6aba8f8565411fc40fa04c9
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 11:30:10 +0900

    refactor: LottoWinningAnalyzer 클래스의 책임 분리 및 가독성 개선

--- src/main/java/lotto/system/formater/winning/LottoWinningAnalyzer.java
+++ src/main/java/lotto/system/formater/winning/LottoWinningAnalyzer.java
@@ -3,11 +3,15 @@ package lotto.system.formater.winning;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import lotto.system.utils.PrizeType;
-import lotto.system.unit.LottoTicket;
 import lotto.system.unit.LottoNumber;
+import lotto.system.unit.LottoTicket;
+import lotto.system.utils.PrizeType;
 
 public class LottoWinningAnalyzer {
+
+    private static final int MATCH_SIX = 6;
+    private static final int MATCH_FIVE_WITH_BONUS = 5;
+    private static final int MINIMUM_MATCH = 3;
 
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
@@ -18,49 +22,50 @@ public class LottoWinningAnalyzer {
     }
 
     public Map<PrizeType, Integer> analyzeWinningStatistics(List<LottoTicket> issueTickets) {
-        Map<PrizeType, Integer> statistics = new HashMap<>();
-
-        for (PrizeType prizeType : PrizeType.values()) {
-            statistics.put(prizeType, 0);
-        }
+        Map<PrizeType, Integer> statistics = initializeStatistics();
 
         for (LottoTicket issueTicket : issueTickets) {
-            PrizeType prizeType = match(issueTicket);
-            if (prizeType == null) {
-                continue;
+            PrizeType prizeType = determinePrizeType(issueTicket);
+            if (prizeType != null) {
+                statistics.put(prizeType, statistics.get(prizeType) + 1);
             }
-            statistics.put(prizeType, statistics.get(prizeType) + 1);
         }
 
         return statistics;
     }
 
-    private PrizeType match(LottoTicket issueTicket) {
-        int count = matchLotto(issueTicket);
-        boolean matchBonus = matchBonus(issueTicket);
+    private Map<PrizeType, Integer> initializeStatistics() {
+        Map<PrizeType, Integer> statistics = new HashMap<>();
+        for (PrizeType prizeType : PrizeType.values()) {
+            statistics.put(prizeType, 0);
+        }
+        return statistics;
+    }
 
-        if (matchBonus && count == 5) {
+    private PrizeType determinePrizeType(LottoTicket issueTicket) {
+        int matchCount = countMatchingNumbers(issueTicket);
+        boolean hasBonusMatch = hasBonusNumberMatch(issueTicket);
+
+        if (hasBonusMatch && matchCount == MATCH_FIVE_WITH_BONUS) {
             return PrizeType.getTypeByCode(6);
-        } else if (count == 6) {
+        }
+        if (matchCount == MATCH_SIX) {
             return PrizeType.getTypeByCode(7);
-        } else if (count >= 3) {
-            return PrizeType.getTypeByCode(count);
-        } else {
-            return null;
         }
+        if (matchCount >= MINIMUM_MATCH) {
+            return PrizeType.getTypeByCode(matchCount);
+        }
+        return null;
     }
 
-    private int matchLotto(LottoTicket issueTicket) {
-        int count = 0;
-        for (LottoNumber number : issueTicket.getTicket()) {
-            if (winningNumbers.contains(number.getValue())) {
-                count++;
-            }
-        }
-        return count;
+    private int countMatchingNumbers(LottoTicket issueTicket) {
+        return (int) issueTicket.getTicket().stream()
+                .filter(number -> winningNumbers.contains(number.getValue()))
+                .count();
     }
 
-    private boolean matchBonus(LottoTicket issueTicket) {
-        return issueTicket.getTicket().stream().anyMatch(number -> number.getValue() == bonusNumber);
+    private boolean hasBonusNumberMatch(LottoTicket issueTicket) {
+        return issueTicket.getTicket().stream()
+                .anyMatch(number -> number.getValue() == bonusNumber);
     }
 }
