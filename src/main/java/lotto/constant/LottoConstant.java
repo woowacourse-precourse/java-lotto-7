ed473014d9e45c262501abbdb28cb9b52c45b3e7
yebrong <officeguider@naver.com>
2024-11-04 21:21:36 +0900

    feat : Constant (Lotto Constant)

---
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -0,0 +1,12 @@
+package lotto.constant;
+
+public class LottoConstant {
+    // 로또와 관련된 상수 관리
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final String DELIMETER = ",";
+
+    public static final int LOTTO_PRICE = 1000;
+
+}
