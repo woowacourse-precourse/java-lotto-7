54b25ac7d3eea0734a5a1f2907d494aa2f8f8991
ssuukko <tlstpstpdl11@gmail.com>
2024-11-04 22:15:11 +0900

    Refactor: LottoService 인터페이스 간소화
    - LottoService 인터페이스의 메서드 선언부에서 공백을 제거하여 코드의 일관성을 높였습니다.
    - 불필요한 줄 바꿈을 제거하여 가독성을 개선하였습니다.

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,10 +5,7 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 public interface LottoService {
-
     List<Lotto> generateLottos(int amount);
-
     int[] getWinningCount(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber);
-
     double calculateProfitRate(int[] winningCounts, int purchaseAmount);
 }
