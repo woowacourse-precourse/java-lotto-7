85181f7bc126bfa63afe382d710f2f244a120d58
JoonKyo <joonkyolee@gmail.com>
2024-11-04 19:34:17 +0900

    test(ConvertInputTest): 제대로 된 구입 금액을 입력하면 예외가 발생하지 않는지 확인하는 테스트 구현

--- src/test/java/lotto/util/ConvertInputTest.java
+++ src/test/java/lotto/util/ConvertInputTest.java
@@ -1,13 +1,22 @@
 package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ConvertInputTest {
     @Test
     void 구입금액에_문자가_포함되면_예외가_발생한다() {
         assertThatThrownBy(() -> ConvertInput.makeMoneyToInt("우테코프리코스"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @CsvSource({"1000,1000", "3000,3000", "450,450"})
+    @ParameterizedTest
+    void 구입금액에_수를_입력하면_예외가_발생하지_않는다(String input, int expected) {
+        assertThat(ConvertInput.makeMoneyToInt(input)).isEqualTo(expected);
     }
 }
