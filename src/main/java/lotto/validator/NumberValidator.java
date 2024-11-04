60a769b8b01af3ac47b5530bb2e9eef9de3c8362
Koo-EunSung <ku903@naver.com>
2024-11-04 22:52:20 +0900

    Feat: 입력값 숫자 검증

--- src/main/java/lotto/validator/NumberValidator.java
+++ src/main/java/lotto/validator/NumberValidator.java
@@ -1,11 +1,28 @@
 package lotto.validator;
 
+import java.util.List;
+
 public class NumberValidator {
     private static final String PREFIX = "[ERROR]";
-    private static final String OUT_OF_RANGE_MESSAGE = PREFIX + " 로또 번호는 1부터 45 사이의 숫자여야 합니다. ";
+    private static final String OUT_OF_RANGE_MESSAGE = PREFIX + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    private static final String ONLY_NUMBER_MESSAGE = PREFIX + " 입력값은 숫자여야 합니다.";
 
     private static final Integer MIN = 1;
     private static final Integer MAX = 45;
+
+    public void isNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(OUT_OF_RANGE_MESSAGE);
+        }
+    }
+
+    public void isNumber(List<String> number) {
+        for(String s : number) {
+                isNumber(s);
+        }
+    }
 
     public void checkInRange(Integer value) {
         if (value < MIN || value > MAX) {
