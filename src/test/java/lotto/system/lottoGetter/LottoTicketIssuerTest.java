83ef3ba6b7ba1d7e8e986d6724fb0c6ca16b147d
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 22:42:36 +0900

    test: 로또 티켓 Test에 중복 제거

--- src/test/java/lotto/system/lottoGetter/LottoTicketIssuerTest.java
+++ src/test/java/lotto/system/lottoGetter/LottoTicketIssuerTest.java
@@ -4,39 +4,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
-import lotto.system.lottoGetter.LottoTicketIssuer;
 import lotto.system.unit.LottoTicket;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTicketIssuerTest {
 
-    @Test
-    @DisplayName("유효한 금액을 입력 받아 올바른 수량의 로또 티켓 생성 테스트")
-    void issueLottoTickets_ShouldGenerateCorrectNumberOfTickets() {
-        // given
-        int totalPayment = 5000; // 5장의 티켓을 구매할 수 있는 금액
-        int expectedQuantity = totalPayment / 1000;
+    private static final int VALID_PAYMENT = 5000;
+    private static final int TICKET_PRICE = 1000;
+    private static final int EXPECTED_TICKET_QUANTITY = VALID_PAYMENT / TICKET_PRICE;
+    private static final int INVALID_PAYMENT = 750;
+    private LottoTicketIssuer issuer;
 
+    @BeforeEach
+    void setUp() {
+        issuer = new LottoTicketIssuer(VALID_PAYMENT);
+    }
+
+    @Test
+    @DisplayName("유효한 금액으로 올바른 수량의 로또 티켓 생성 테스트")
+    void issueLottoTickets_ShouldGenerateCorrectNumberOfTickets() {
         // when
-        LottoTicketIssuer issuer = new LottoTicketIssuer(totalPayment);
         List<LottoTicket> tickets = issuer.issueLottoTickets();
 
         // then : 생성된 티켓 수량이 예상과 일치하는지 확인
-        assertEquals(expectedQuantity, tickets.size(), "생성된 티켓 수량이 예상과 일치하지 않습니다.");
-
-        // then : 각 티켓의 숫자 개수가 6개인지 확인
-        tickets.forEach(ticket -> assertEquals(6, ticket.getTicket().size(), "로또 티켓의 숫자 개수가 6개가 아닙니다."));
+        assertEquals(EXPECTED_TICKET_QUANTITY, tickets.size(), "생성된 티켓 수량이 예상과 일치하지 않습니다.");
+        verifyTicketNumbers(tickets);
     }
 
     @Test
     @DisplayName("유효하지 않은 금액으로 예외 발생 테스트")
     void issueLottoTickets_ShouldThrowExceptionForInvalidPayment() {
-        // given : 티켓 가격의 배수가 아닌 금액
-        int invalidPayment = 750;
-
-        // when & then : 예외가 발생 에상
-        assertThrows(IllegalArgumentException.class, () -> new LottoTicketIssuer(invalidPayment),
+        // when & then : 유효하지 않은 금액이므로 예외 발생 예상
+        assertThrows(IllegalArgumentException.class, () -> new LottoTicketIssuer(INVALID_PAYMENT),
                 "유효하지 않은 금액으로 예외가 발생하지 않았습니다.");
+    }
+
+    private void verifyTicketNumbers(List<LottoTicket> tickets) {
+        // 모든 티켓의 숫자 개수가 6개인지 확인
+        tickets.forEach(ticket ->
+                assertEquals(6, ticket.getTicket().size(), "로또 티켓의 숫자 개수가 6개가 아닙니다.")
+        );
     }
 }
