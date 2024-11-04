e35cba9e4abd1aa110656a787b38a666913a04ca
badgelatte <saucy5939@gmail.com>
2024-11-04 19:30:18 +0900

    makeLotto 메서드 테스트 구현
    makeLotto 메서드에서 1에서 45까지의 범위 내의 6개 랜덤한 숫자를 가진 로또를 생산하는 경우를 테스트

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -1,10 +1,13 @@
 package lotto;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -23,5 +26,14 @@ public class CustomerTest {
     void buyLottosTest(String money) {
         assertDoesNotThrow(() -> customer.buyLottos(money));
         assertFalse(customer.getLottos().isEmpty());
+    }
+
+    @DisplayName("랜덤한 숫자를 가진 로또가 만들어지는 makeLotto 메서드")
+    @Test
+    void makeLottoTest() {
+        Lotto lotto = customer.makeLotto();
+        assertEquals(6, lotto.getNumbers().size());
+        assertTrue(lotto.toString().contains("["));
+        assertTrue(lotto.toString().contains("]"));
     }
 }
