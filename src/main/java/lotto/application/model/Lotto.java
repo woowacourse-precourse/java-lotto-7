c6550605e531955962aa86e0e6ee815874d91a52
bellmin <whdlas011203@naver.com>
2024-11-04 20:36:04 +0900

    feat(Lotto): 로또 번호 중복 체크 로직 추가
    - 발행한 로또에 중복된 번호가 있을 경우 예외

--- src/main/java/lotto/application/model/Lotto.java
+++ src/main/java/lotto/application/model/Lotto.java
@@ -3,6 +3,7 @@ package lotto.application.model;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import lotto.common.Errors;
 
 public class Lotto implements Model {
     private final List<Integer> numbers;
@@ -14,7 +15,11 @@ public class Lotto implements Model {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            Errors.IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+
+        if(numbers.stream().distinct().count() != numbers.size()) {
+            Errors.IllegalArgumentException("로또 번호는 중복이 불가합니다.");
         }
     }
 
