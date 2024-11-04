3e125ac2d428293b68cb6ce6e89e6ee833cea0b0
Kim San <aegis0911@gmail.com>
2024-11-04 22:50:00 +0900

    feat: Application 클래스에서 프로그램 핵심 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("로또 발매기를 시작합니다.");
+
+        InputHandler inputHandler = new InputHandler();
+        int purchaseAmount = inputHandler.getPurchaseAmount();
+        int ticketCount = purchaseAmount / 1000;
+
+        System.out.println(ticketCount + "개의 로또를 구매했습니다.");
+
+        LottoManager lottoManager = new LottoManager();
+        List<Lotto> lottos = lottoManager.generateLottos(ticketCount);
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+
+        List<Integer> winningNumbers = inputHandler.getWinningNumbers();
+        int bonusNumber = inputHandler.getBonusNumber();
+
+        Statistics statistics = new Statistics();
+        statistics.calculateAndDisplay(lottos, winningNumbers, bonusNumber, purchaseAmount);
     }
 }
