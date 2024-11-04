7894d4befeb5c40a2d178ed8b4b845c2076e0e7f
hyuuuun <pam4522@gmail.com>
2024-11-04 21:43:32 +0900

    style (Output) : static 화

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -7,20 +7,20 @@ import java.util.List;
 
 public class Output {
 
-    public void printCount(int count){
-        System.out.printf("%d개를 구매했습니다.",count);
+    public static void printCount(int count){
+        System.out.printf("\n%d개를 구매했습니다.\n",count);
     }
-    public void printNumbers(List<Number> numbers){
+    public static void printNumbers(List<Integer> numbers){
         System.out.println(numbers);
     }
-    public void printResult(HashMap<Rank,Integer> result){
+    public static void printResult(HashMap<Rank,Integer> result){
         System.out.println("\n담첨 통계");
         System.out.println("---");
         for(Rank rank : Rank.values()){
             printRankResult(rank,result);
         }
     }
-    public void printRankResult(Rank rank, HashMap<Rank,Integer> result){
+    public static void printRankResult(Rank rank, HashMap<Rank,Integer> result){
         if(rank.equals(Rank.LOSE)){
             return;
         }
@@ -31,7 +31,7 @@ public class Output {
         }
         System.out.printf("%d개 일치 (%s원) - %d개\n", rank.getMatchLotto(), rank.getPrize(), result.get(rank));
     }
-    public void printEarningRate(double rate){
+    public static void printEarningRate(double rate){
         System.out.printf("총 수익률은 %.1f입니다",rate);
     }
 
