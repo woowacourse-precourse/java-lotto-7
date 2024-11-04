3e6298fb77946065901c3b22c14e8526c60b435e
Youn Sang Hyun <younsang523@gmail.com>
2024-11-04 17:48:25 +0900

    feat: 당첨 여부 확인 기능 구현

--- src/main/java/lotto/Domain/WinningLotto.java
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -1,4 +1,46 @@
 package lotto.Domain;
 
+import java.util.List;
+import lotto.Enum.LottoPrizeRank;
+
 public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.lotto = new Lotto(winningNumbers);
+        validateBonusNumber(bonusNumber, winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber, List<Integer> winningNumbers) {
+        LottoValidator.validateBonusNumber(bonusNumber, winningNumbers);
+    }
+
+    public LottoPrizeRank match(Lotto userLotto) {
+        int matchCount = userLotto.matchNumbers(lotto.getNumbers());
+        boolean matchBonus = matchCount == 5 && userLotto.containsNumber(bonusNumber);
+
+        return calculatePrizeRank(matchCount, matchBonus);
+    }
+
+    private LottoPrizeRank calculatePrizeRank(int matchCount, boolean matchBonus) {
+        if (matchCount == 6) {
+            return LottoPrizeRank.FIRST;
+        }
+        if (matchCount == 5 && matchBonus) {
+            return LottoPrizeRank.SECOND;
+        }
+        if (matchCount == 5) {
+            return LottoPrizeRank.THIRD;
+        }
+        if (matchCount == 4) {
+            return LottoPrizeRank.FOURTH;
+        }
+        if (matchCount == 3) {
+            return LottoPrizeRank.FIFTH;
+        }
+        return LottoPrizeRank.MISS;
+    }
+}
 }
