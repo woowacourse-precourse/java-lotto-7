909b55f6fec459e456b353350ca009367bc389c7
htjworld <116538001+htjworld@users.noreply.github.com>
2024-11-04 15:43:15 +0900

    (refactor) 입력 오류 발생 시 while 루프를 통해 재입력 받도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,24 +8,46 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("구입금액을 입력해 주세요.");
-        String moneyText = Console.readLine();
-        long purchaseAmount = Money.buy(moneyText); // String 변수 moneyText를 long형으로 변환 및 유효성 검사
+        LottoGame lottoGame = null;
+        while (true){
+            try{
+                System.out.println("구입금액을 입력해 주세요.");
+                String moneyText = Console.readLine();
+                long purchaseAmount = Money.buy(moneyText); // String 변수 moneyText를 long형으로 변환 및 유효성 검사
 
-        LottoGame lottoGame =new LottoGame(purchaseAmount);
+                lottoGame =new LottoGame(purchaseAmount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR]" + e.getMessage());
+            }
+        }
 
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String lottoText = Console.readLine();
-        List<Integer> winningNumbers = LottoText.ParseIntegerList(lottoText);
-        lottoGame.setWinningLotto(winningNumbers);
+        List<Integer> winningNumbers = null;
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요.");
+                String lottoText = Console.readLine();
+                winningNumbers = LottoText.ParseIntegerList(lottoText);
+                lottoGame.setWinningLotto(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR]" + e.getMessage());
+            }
+        }
 
+        while (true){
+            try {
+                System.out.println("보너스 번호를 입력해 주세요.");
+                String bonusText = Console.readLine();
+                Bonus bonus = new Bonus(bonusText);
+                lottoGame.setBonusNumber(bonus.getBonus());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR]" + e.getMessage());
+            }
+        }
 
-        System.out.println("보너스 번호를 입력해 주세요.");
-        String bonusText = Console.readLine();
-        Bonus bonus = new Bonus(bonusText);
-        lottoGame.setBonusNumber(bonus.getBonus());
 
         lottoGame.calculateResults();
-
     }
 }
