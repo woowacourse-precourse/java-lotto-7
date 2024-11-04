f4ce7d929a4fc88229243a9a859a36b7f066f08b
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-04 20:23:45 +0900

    feat: 로또 구입 금액이 1,000원 이하라면 예외 발생 기능 추가
    - checkMinimumPayment() 추가

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -5,10 +5,17 @@ public class InputValidator {
     public int getValidPurchasedLottoAmount(String input) {
         try {
             int payment = Integer.parseInt(input);
+            checkMinimumPayment(payment);
             return payment;
 
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 가능합니다.");
+        }
+    }
+
+    private void checkMinimumPayment(int payment) {
+        if (payment < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 로또는 1개 이상 구매 가능합니다.");
         }
     }
 }
