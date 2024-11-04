d348d38c1ffd1a483848b722a5322477f8252a6b
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 20:27:47 +0900

    feat(Lotto): 로또 번호의 중복이 발생 시 에러를 발생시키고 예외 처리하는 코드 작성
    - 로또 번호의 중복이 발생 시 에러를 발생시키고 예외 처리하는 코드 작성함.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,6 +15,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복된 숫자가 없어야 합니다.");
         }
     }
 
