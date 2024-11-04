e599fc61365ac760db01a0e93d98cb839fb9a76e
btongtong <leeym2615@naver.com>
2024-11-04 15:04:52 +0900

    feat(Lotto): Add method to create sorted number list

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,14 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = createSortedNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
@@ -27,5 +28,13 @@ public class Lotto {
         if (distinctCount != numbers.size()) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_DUPLICATION_ERROR.getMessage());
         }
+    }
+
+    private List<Integer> createSortedNumbers(List<Integer> numbers) {
+        return numbers.stream().sorted().collect(Collectors.toList());
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
     }
 }
