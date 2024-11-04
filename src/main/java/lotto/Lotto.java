3c053617b4b54db776749f21cce0db97648e1c2c
moeun9309 <moeun9309@gmail.com>
2024-11-04 09:04:50 +0900

    feat: input manager

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
@@ -16,6 +18,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     private List<Integer> pickNewLotto() {
@@ -27,5 +34,21 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원으로 나누어 떨어져야 합니다.");
         }
         return money / 1000;
+    }
+
+    public void inputManager() {
+        String input = Console.readLine();
+        String[] numbersString = input.split(",");
+        List<Integer> newNumbers = new ArrayList<Integer>();
+
+        for (String number : numbersString) {
+            try {
+                newNumbers.add(Integer.parseInt(number.trim()));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 있습니다.");
+            }
+        }
+
+        numbers.addAll(newNumbers);
     }
 }
