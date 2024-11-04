9079f3be95c060bb719e51e14e5b9fee2ab4cca5
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-11-04 23:15:27 +0900

    Implement: add check range validation func
    범위 유효성 검사 로직 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -19,6 +19,12 @@ public class Lotto {
         if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
 
     public Map<Object, Integer> checkLottoWin(List<List<Integer>> randomNumberList, Integer bonusNumber) {
