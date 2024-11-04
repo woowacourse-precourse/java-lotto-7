5671e4a732cd2091e094436bf47027f68d50a312
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2024-11-04 16:21:55 +0900

    feat(WinningNumber) : 당첨 번호 클래스 구현

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,2 +1,28 @@
-package lotto;public class WinningNumber {
+package lotto;
+
+import java.util.List;
+
+public class WinningNumber {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public WinningNumber(List<Integer> numbers, int bonusNumber) {
+        this.numbers = numbers;
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (!(bonusNumber >= 1 && bonusNumber <= 45)) {
+            throw new IllegalArgumentException("[Error] 로또 번호는 1이상 45이하의 정수여야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
