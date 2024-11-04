60b70031c7123719ee7be1de15bf827ff7e538ea
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-04 21:26:25 +0900

    feat(LottoController): View의 로또 발행 정보를 출력하는 함수 호출

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import lotto.model.Lottos;
 import lotto.view.InputView;
+import lotto.view.OutView;
 
 public class LottoController {
 
@@ -12,7 +13,10 @@ public class LottoController {
     private void issue() {
         int cost = Integer.parseInt(InputView.requestCost());
         Lottos lottos = Lottos.from(getCountFrom(cost));
+        OutView.printLottos(lottos);
+
     }
+
     private int getCountFrom(int cost) {
         return cost / 1000;
     }
