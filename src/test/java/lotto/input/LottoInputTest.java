2741374301b29079707a6651599b06cfdb266201
junyonglee0223 <ljyong3339@gmail.com>
2024-11-04 22:08:14 +0900

    fix: test 코드 equalTo 방식으로 변경

--- src/test/java/lotto/input/LottoInputTest.java
+++ src/test/java/lotto/input/LottoInputTest.java
@@ -2,6 +2,10 @@ package lotto.input;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,35 +20,30 @@ class LottoInputTest {
     }
     @Test
     void 구입_금액이_숫자가_아니면_예외가_발생한다() {
-        System.setIn(new java.io.ByteArrayInputStream("invalid\n".getBytes()));
-        assertThatThrownBy(() -> lottoInput.readCost())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+        System.setIn(new ByteArrayInputStream("invalid\n".getBytes()));
+        int cost = lottoInput.readCost();
+        assertThat(cost).isEqualTo(0);
     }
-
 
     @Test
     void 당첨_번호가_6개_아닐_때_예외_처리() {
-        System.setIn(new java.io.ByteArrayInputStream("1,2,3,4,5\n".getBytes()));
-        assertThatThrownBy(() -> lottoInput.readWinningNumbers())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 6개여야 합니다.");
+        System.setIn(new ByteArrayInputStream("1,2,3,4,5\n".getBytes()));
+        List<Integer> winningNumbers = lottoInput.readWinningNumbers();
+        assertThat(winningNumbers).isEqualTo(Collections.emptyList());
     }
 
     @Test
     void 잘못된_형식의_당첨_번호_입력_시_예외_처리() {
-        System.setIn(new java.io.ByteArrayInputStream("one,two,three\n".getBytes()));
-        assertThatThrownBy(() -> lottoInput.readWinningNumbers())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 숫자로 입력해야 합니다.");
+        System.setIn(new ByteArrayInputStream("one,two,three\n".getBytes()));
+        List<Integer> winningNumbers = lottoInput.readWinningNumbers();
+        assertThat(winningNumbers).isEqualTo(Collections.emptyList());
     }
 
     @Test
     void 잘못된_형식의_보너스_번호_입력_시_예외_처리() {
-        System.setIn(new java.io.ByteArrayInputStream("invalid\n".getBytes()));
-        assertThatThrownBy(() -> lottoInput.readBonus())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 보너스 번호는 숫자로 입력해야 합니다.");
+        System.setIn(new ByteArrayInputStream("invalid\n".getBytes()));
+        int bonusNumber = lottoInput.readBonus();
+        assertThat(bonusNumber).isEqualTo(0);
     }
 
 }
