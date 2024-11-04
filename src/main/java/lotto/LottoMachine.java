9ba6c9ef2f009aaaecd4846ca9f4999bb5dd5572
jisukimtech <jisukim.tech@gmail.com>
2024-11-04 12:58:56 +0900

    refactor(LottoMachine): 로또 번호 생성과 관련된 매직 넘버를 상수로 변경
    - 로또 번호를 생성하는 메서드에서 가독성 및 명확한 의도 전달을 위해 매직 넘버를 상수로 변경

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
 
     public List<List<Integer>> issue(int lottoPurchaseAmount) {
         int lottoCount = calculateLottoCount(lottoPurchaseAmount);
@@ -25,7 +28,7 @@ public class LottoMachine {
     }
 
     private List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_COUNT);
     }
 
     private List<Integer> sortLottoNumbers(List<Integer> lotto) {
