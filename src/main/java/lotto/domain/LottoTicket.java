e9561fec4455791fed1a96ee5ea4c5e353a3c7b3
sejinezy <psj8880@naver.com>
2024-11-04 20:43:14 +0900

    refactor(LottoTicket): 메서드 분리 및 하드코딩 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class LottoTicket {
 
+    private static final String PURCHASE_MESSAGE = "\n%d개를 구매했습니다.\n";
+
     private final List<Lotto> lottos;
 
     public LottoTicket(List<Lotto> lottos) {
@@ -14,11 +16,21 @@ public class LottoTicket {
         return lottos;
     }
 
-   public void showLottoTicket(){
-       System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
-       for (Lotto lotto : lottos) {
-           System.out.println(lotto);
-       }
-   }
+    public void showLottoTicket() {
+        showPurchaseMessage();
+        showLottoNumbers();
+    }
+
+    private void showPurchaseMessage() {
+        System.out.printf(PURCHASE_MESSAGE, lottos.size());
+    }
+
+    private void showLottoNumbers() {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+
 
 }
