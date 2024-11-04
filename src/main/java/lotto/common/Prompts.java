3b3b01638a24869ec357ebe4d66019834a295e37
cyunlee <siyunkr@gmail.com>
2024-11-04 13:18:13 +0900

    feat(common): 프롬프트 상수화 코드 작성

---
+++ src/main/java/lotto/common/Prompts.java
@@ -0,0 +1,17 @@
+package lotto.common;
+
+public class Prompts {
+    public static final String INPUT_PRICE_PROMPT = "구입금액을 입력해 주세요.";
+    public static final String OUTPUT_NUMBER_OF_LOTTO_PROMPT = "를 구매했습니다.";
+    public static final String INPUT_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String OUTPUT_STATISTICS_PROMPT = "당첨 통계";
+    public static final String OUTPUT_EQAUL_LOTTO_PROMT = "개 일치";
+    public static final String OUTPUT_LOTTO_UNIT = "개";
+    public static final String OUTPUT_MONEY_UNIT = "원";
+    public static final String OUTPUT_EQUAL_BONUS_PROMT = ", 보너스 볼 일치";
+    public static final String OUTPUT_PERCENTAGE_UNIT = "%";
+    public static final String OUTPUT_PERCENTAGE_PROMPT_PRE = "총 수익률은 ";
+    public static final String OUTPUT_PERCENTAGE_PROMPT_POST = "입니다.";
+
+}
