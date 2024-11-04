55237bc2f05f1403336d545874b187a8b866a7c2
JuSeong1130 <jsmoon1130@naver.com>
2024-11-04 10:00:29 +0900

    feat(Output) : 로또 당첨 결과 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -23,39 +23,16 @@ public class Application {
         int bonusNumber = input.askBonusNumber();
         WinningLotto winningLotto = new WinningLotto(new Lotto(winningNumbers), bonusNumber);
         Map<String, Integer> winningResult = winningLotto.getWinningResult(playerLottos);
-
-
-        String result = """
-                
-                당첨 통계
-                ---
-                3개 일치 (5,000원) - %s개
-                4개 일치 (50,000원) - %s개
-                5개 일치 (1,500,000원) - %s개
-                5개 일치, 보너스 볼 일치 (30,000,000원) - %s개
-                6개 일치 (2,000,000,000원) - %s개
-                총 수익률은 %s%%입니다.
-                """
-                .formatted(
-                        winningResult.get("MATCH_3"),
-                        winningResult.get("MATCH_4"),
-                        winningResult.get("MATCH_5"),
-                        winningResult.get("MATCH_5_BONUS"),
-                        winningResult.get("MATCH_6"),
-                        calculateYield(money, winningResult)
-                );
-        System.out.println(result);
+        output.printWinningResult(winningResult, calculateYield(money, winningResult));
     }
 
-    public static String calculateYield(int money, Map<String, Integer> winningResult) {
+    public static double calculateYield(int money, Map<String, Integer> winningResult) {
         long totalPrize =
                 (winningResult.get("MATCH_3") * 5_000) +
                         (winningResult.get("MATCH_4") * 50_000) +
                         (winningResult.get("MATCH_5") * 1_500_000) +
                         (winningResult.get("MATCH_5_BONUS") * 30_000_000) +
                         (winningResult.get("MATCH_6") * 2_000_000_000L);
-
-
-        return String.format("%.1f", ((double) totalPrize / money) * 100);
+        return ((double) totalPrize / money) * 100;
     }
 }
--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class Output {
 
@@ -9,5 +10,28 @@ public class Output {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printWinningResult(Map<String, Integer> winningResult, double yield) {
+        String result = """
+                
+                당첨 통계
+                ---
+                3개 일치 (5,000원) - %s개
+                4개 일치 (50,000원) - %s개
+                5개 일치 (1,500,000원) - %s개
+                5개 일치, 보너스 볼 일치 (30,000,000원) - %s개
+                6개 일치 (2,000,000,000원) - %s개
+                총 수익률은 %s%%입니다.
+                """
+                .formatted(
+                        winningResult.get("MATCH_3"),
+                        winningResult.get("MATCH_4"),
+                        winningResult.get("MATCH_5"),
+                        winningResult.get("MATCH_5_BONUS"),
+                        winningResult.get("MATCH_6"),
+                        String.format("%.1f", yield)
+                );
+        System.out.println(result);
     }
 }
