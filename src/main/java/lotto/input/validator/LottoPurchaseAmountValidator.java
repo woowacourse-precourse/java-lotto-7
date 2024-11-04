8a3b90e340ee8a8ee40a2f67c1c01311240379f0
in seong Park <123macanic@naver.com>
2024-11-04 23:41:27 +0900

    refactor: 입력에 대한 유효성 검사 기능 문자 입력시 추가 검사 로직 추가
    입력한 내용이 문자가 아닌 숫자여야만 통과가 가능하게 코드 수정
    
    변경된 점:
    - 다른 정규 표현식 match 검사를 진행

--- src/main/java/lotto/input/validator/LottoPurchaseAmountValidator.java
+++ src/main/java/lotto/input/validator/LottoPurchaseAmountValidator.java
@@ -3,9 +3,15 @@ package lotto.input.validator;
 public class LottoPurchaseAmountValidator {
 
     private final static String LOTTO_PURCHASE_AMOUNT_REGEX = "^[1-9][0-9]*000$";
+    private final static String ONLY_NUMBER_REGEX = "\\d+";
 
     public void validate(String input) {
-        if (!input.matches(LOTTO_PURCHASE_AMOUNT_REGEX))
+        if (!input.matches(ONLY_NUMBER_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자로만 입력되어야 합니다.");
+        }
+
+        if (!input.matches(LOTTO_PURCHASE_AMOUNT_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+        }
     }
 }
