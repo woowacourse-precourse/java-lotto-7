6a1baeb0385a6c753347dbe11d06dba43c39f8ac
dayeondayeon <dayeon2438@gmail.com>
2024-11-04 18:46:05 +0900

    feat : 당첨 출력을 위한 상수 추가

--- src/main/java/lotto/constants/OutputViewConstants.java
+++ src/main/java/lotto/constants/OutputViewConstants.java
@@ -2,5 +2,13 @@ package lotto.constants;
 
 public class OutputViewConstants {
     public static final String PURCHASE_AMOUNT_DESCRIPTION = "개를 구매했습니다.";
-
+    public static final String FIFTH_PRIZE = "3개 일치 (5,000원) - ";
+    public static final String FOURTH_PRIZE = "4개 일치 (50,000원) - ";
+    public static final String THIRD_PRIZE = "5개 일치 (1,500,000원) - ";
+    public static final String SECOND_PRIZE = "5개 일치, 보너스 볼 일치 (30,000,000원) -";
+    public static final String FIRST_PRIZE = "6개 일치 (6개 일치 (2,000,000,000원) -";
+    public static final String STATISTIC_DESCRIPTION_PREFIX = "당첨 통계";
+    public static final String STATISTIC_DESCRIPTION_SUFFIX = "---";
+    public static final String RATE_OF_RETURN_PREFIX = "총 수익률은 ";
+    public static final String RATE_OF_RETURN_SUFFIX = "%입니다.";
 }
