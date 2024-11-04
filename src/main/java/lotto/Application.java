d6701fb2f1fde9403d1110783b5245b9c6fc93fc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-04 23:08:51 +0900

    refactor(Application): LottoController의 생성자 인자 추가로 NumberStringConverter 통합
    - `LottoController`에 `NumberStringConverter`를 추가하여 번호 변환 기능 통합
    - 코드 구조 개선 및 가독성 향상

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.model.LottoNumberGenerator;
+import lotto.model.NumberStringConverter;
 import lotto.model.YieldAnalyst;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -9,8 +10,8 @@ import lotto.view.OutputView;
 public class Application {
 
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController(new InputView(), new OutputView(),
-                new LottoNumberGenerator(), new YieldAnalyst());
+        LottoController lottoController = new LottoController(new InputView(), new OutputView(), new YieldAnalyst(),
+                new LottoNumberGenerator(), new NumberStringConverter());
 
         lottoController.start();
     }
