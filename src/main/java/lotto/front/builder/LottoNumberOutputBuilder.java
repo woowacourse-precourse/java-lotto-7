4df6da83205e5b0253d661b915efe07352469109
whatsgoodg <rlwjddl1596@naver.com>
2024-11-04 13:52:34 +0900

    refactor(LottoNumberOutputBuilder): 코드 컨벤션에 따른 static import 삭제

--- src/main/java/lotto/front/builder/LottoNumberOutputBuilder.java
+++ src/main/java/lotto/front/builder/LottoNumberOutputBuilder.java
@@ -1,8 +1,7 @@
 package lotto.front.builder;
 
 import java.util.List;
-
-import static lotto.global.enums.OutputMessage.*;
+import lotto.global.enums.OutputMessage;
 
 public class LottoNumberOutputBuilder {
     private static final String NEW_LINE = "\n";
@@ -10,7 +9,7 @@ public class LottoNumberOutputBuilder {
     public static String build(List<List<Integer>> lottoNumberSets) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        String purchaseLottoMessage = String.format(LOTTO_PURCHASE.getMessage(), lottoNumberSets.size());
+        String purchaseLottoMessage = String.format(OutputMessage.LOTTO_PURCHASE.getMessage(), lottoNumberSets.size());
         stringBuilder.append(purchaseLottoMessage).append(NEW_LINE);
 
         lottoNumberSets.forEach(purchasedLotto -> stringBuilder.append(purchasedLotto.toString()).append(NEW_LINE));
