49d056507658d333e3fd2613cb506c1ecd3fcae7
gikhoon <ghikhoon@naver.com>
2024-11-04 15:15:05 +0900

    feat: 당첨 번호 시작과 끝이 ,면 에러 발생

--- src/main/java/lotto/util/ParseNumberUtil.java
+++ src/main/java/lotto/util/ParseNumberUtil.java
@@ -8,6 +8,10 @@ public class ParseNumberUtil {
     private static final String NUMBER_SPLIT_SEPARATOR = ",";
 
     public static List<Integer> parseNumber(String numbers) {
+        if (isEmpty(numbers) || isFormatError(numbers)) {
+            throw new IllegalArgumentException(NumberErrorCode.NUMBER_PARSE_ERROR.getMessage());
+        }
+
         try {
             return Stream.of(numbers.split(NUMBER_SPLIT_SEPARATOR))
                     .map(String::trim)
@@ -16,6 +20,14 @@ public class ParseNumberUtil {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NumberErrorCode.NUMBER_PARSE_ERROR.getMessage());
         }
+    }
+
+    private static boolean isFormatError(String numbers) {
+        return numbers.endsWith(NUMBER_SPLIT_SEPARATOR) || numbers.startsWith(NUMBER_SPLIT_SEPARATOR);
+    }
+
+    private static boolean isEmpty(String numbers) {
+        return numbers.trim().isEmpty();
     }
 
     public static Integer parseNumberToInteger(String number) {
