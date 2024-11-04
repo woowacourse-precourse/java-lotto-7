b3f36f1b980fe82c9f9ded85c2db29ea6e780f67
yunho_yun <yunho_yun@naver.com>
2024-11-04 17:03:26 +0900

    feat: 로또 발행 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.util.InputParser;
+import lotto.util.RandomNumbersGenerator;
 import lotto.view.LottoView;
 
 import java.util.List;
@@ -14,5 +15,12 @@ public class LottoController {
         this.purchasePrice = InputParser.parsePurchasePrice(LottoView.inputPurchasePrice());
         this.turn = purchasePrice / 1000;
         this.winningNumbers = InputParser.parseWinningNumbers(LottoView.inputWinningNumbers());
+    }
+
+    public void runLotto() {
+        List<Integer>[] numbers = new List[turn];
+        for (int i = 0; i < turn; i++) {
+            numbers[i] = RandomNumbersGenerator.create();
+        }
     }
 }
