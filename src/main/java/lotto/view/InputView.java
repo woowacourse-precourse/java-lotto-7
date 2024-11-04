b5809da47bd4eaf83a49af33956cec5d608441d7
cyunlee <siyunkr@gmail.com>
2024-11-04 13:24:13 +0900

    feat(view): 사용자로부터 입력 받는 기능 틀 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,21 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.common.Prompts;
+
 public class InputView {
+    public int requirePrice() {
+        System.out.println(Prompts.INPUT_PRICE_PROMPT);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public String requireLottoNumbers() {
+        System.out.println(Prompts.INPUT_LOTTO_NUMBER);
+        return Console.readLine();
+    }
+
+    public String requireBonusNumber() {
+        System.out.println(Prompts.INPUT_BONUS_NUMBER);
+        return Console.readLine();
+    }
 }
