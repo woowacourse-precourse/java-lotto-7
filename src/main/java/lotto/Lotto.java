4f7a4f8bd2b5ece7e561da360cdd663a095f886f
bongj9 <lollypark501@naver.com>
2024-11-04 19:15:43 +0900

    feat(lotto): 로또 1부터 45사이의 숫자 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,30 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
-
+import camp.nextstep.*;
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        Collections.sort(this.numbers); //오름차순 정렬
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        //1부터 45사이의 숫자 검증
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45사이의 숫자입니다");
+            }
+
+        }
     }
 
     // TODO: 추가 기능 구현
+
 }
