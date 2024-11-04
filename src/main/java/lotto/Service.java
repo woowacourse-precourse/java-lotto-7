411432b4a25f24139d0ff2b4c5d64b9d04ddd9b8
HeungY <rkrkrkrk02@naver.com>
2024-11-04 18:21:54 +0900

    test: separate method

--- src/main/java/lotto/Service.java
+++ src/main/java/lotto/Service.java
@@ -9,11 +9,18 @@ public class Service {
     private int bonus;
 
     public void run(){
+        play();
+        result();
+    }
+
+    private void play(){
         purchaseCost = new User().inputCost();
         lottos = new Generate().lottos(purchaseCost);
         userLotto = new User().inputPurchaseLottos();
         bonus = new User().inputBonusNumber();
+    }
 
+    private void result(){
         Draw draw = new Draw();
         draw.lotto(lottos, userLotto, bonus);
         draw.printEarningRate(purchaseCost);
