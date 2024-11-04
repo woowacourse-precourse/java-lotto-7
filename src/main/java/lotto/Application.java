d8a6d5f9c26da528d88e88faf5de189ac386a187
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2024-11-04 12:56:52 +0900

    feat(Application): LottoNumGenerator 객체 생성 및 로또 구매 기능 추가
    -Application 클래스에서 LottoNumGenerator 객체를 생성하고 Buyer의 purchaseLottos 메소드에 전달하여 Application에서 로또 구매 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        LottoNumGenerator lottoNumGenerator = new LottoNumGenerator();
+
         // 구매 금액 입력
         System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = InputView.inputPurchaseAmount();
         Buyer buyer = new Buyer(purchaseAmount);
+        buyer.purchaseLottos(lottoNumGenerator);
+        // 구매한 로또 번호 출력
+        buyer.printPurchasedLottos();
 
         // 당첨/보너스 번호 입력받기
         System.out.println("당첨 번호를 입력해 주세요.");
@@ -15,6 +20,5 @@ public class Application {
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = InputView.inputBonusNumber();
         LottoGame lottoGame = new LottoGame(winningNumber, bonusNumber);
-
     }
 }
