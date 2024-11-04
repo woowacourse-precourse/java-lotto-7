eeeb1799aaf97b6f2082bd1a7a89bf6714f5fb87
Dohun Kim <dohun8758@naver.com>
2024-11-04 15:42:14 +0900

    test: MoneyValidator에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/validator/MoneyValidatorTest.java
@@ -0,0 +1,23 @@
+package lotto.validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyValidatorTest {
+
+    @Test
+    @DisplayName("구입 금액이 1000보다 작은 경우")
+    void 구입_금액이_1000보다_작은_경우() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                MoneyValidator.validateMoney(999));
+    }
+
+    @Test
+    @DisplayName("구입 금액 입력이 1000으로 나누어 떨어지지 않는 경우")
+    void 구입_금액_입력이_1000으로_나누어_떨어지지_않는_경우() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                MoneyValidator.validateMoney(1111));
+    }
+
+}
