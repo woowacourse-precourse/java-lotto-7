f42f5a023da4554d557beddb4f6e9481b79ee185
bellmin <whdlas011203@naver.com>
2024-11-04 17:45:59 +0900

    feat(LottoAnchor): setBonusNumber 추가
    - BonusNumber를 Set할 수 있는 메소드 추가

--- src/main/java/lotto/application/service/LottoAnchor.java
+++ src/main/java/lotto/application/service/LottoAnchor.java
@@ -1,16 +1,24 @@
 package lotto.application.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import lotto.application.model.Lotto;
+import lotto.application.model.WinningRanking;
 
 public class LottoAnchor {
 
     private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public LottoAnchor() {}
 
     public void setWinningNumbers(List<Integer> winningNumbers) {
         this.winningNumbers = new ArrayList<>(winningNumbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
     }
 
 }
