28543aeb410b91ca9c52cde8757f47553899a126
eunji <kuji5256@gmail.com>
2024-11-04 22:46:56 +0900

    test: 당첨 관련 테스트 코드 구현 - 로또번호와 당첨번호를 비교하는 메서드 테스트 - 당첨 결과를 저장하는 메서드 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,9 +4,11 @@ import lotto.exception.InputValidation;
 import lotto.exception.ValidateValues;
 import lotto.model.Lotto;
 import lotto.service.LottoGenerator;
+import lotto.service.LottoResultManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -91,6 +93,39 @@ class LottoTest {
         String purchaseAmount = "8000";
         LottoGenerator lottoGenerator = new LottoGenerator();
         assertThat(lottoGenerator.generateLotto(purchaseAmount).size()).isEqualTo(8);
+    }
+
+    @Test
+    void 로또번호와_당첨번호를_비교한다() {
+        LottoResultManager lottoResultManager = new LottoResultManager();
+
+        List<Integer> lotto = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        int result = lottoResultManager.compareLottoNumber(lotto, winningLotto, bonusNumber);
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void 당첨결과를_저장한다() {
+        LottoResultManager lottoResultManager = new LottoResultManager();
+
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 9, 20)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+        lottos.add(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        int[] result = lottoResultManager.saveLottoWinningResult(lottos, winningLotto, bonusNumber);
+        assertThat(result[0]).isEqualTo(1);
+        assertThat(result[1]).isEqualTo(1);
+        assertThat(result[2]).isEqualTo(0);
+        assertThat(result[3]).isEqualTo(1);
+        assertThat(result[4]).isEqualTo(1);
     }
 
 }
