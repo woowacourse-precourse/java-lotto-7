887604863154dfe8f5e2b7d8d3f3869ee6010dde
leejh <777joonho@kookmin.ac.kr>
2024-11-04 15:38:09 +0900

    feat: 로또 구입 금액 클래스 작성

---
+++ src/main/java/lotto/domain/Cost.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+public class Cost {
+
+    private static final int LOTTO_PRICE = 1000;
+    private final int cost;
+
+    public Cost(int cost) {
+        this.cost = cost;
+    }
+
+    private void validate(int cost) {
+        if(cost < LOTTO_PRICE) {
+            throw new IllegalArgumentException("로또 구입 금액보다 적은 금액을 입력하실 수 없습니다.");
+        }
+    }
+
+    public int calculateLottoAmount() {
+        return cost / LOTTO_PRICE;
+    }
+
+    public int getLottoPurchaseAmount() {
+        return calculateLottoAmount() * LOTTO_PRICE;
+    }
+
+}
