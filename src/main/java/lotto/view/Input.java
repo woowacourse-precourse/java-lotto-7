ab176d9a3ac380aec2bd8cf057ae11822824f12b
yubin8773 <hiyubin.lee@gmail.com>
2024-11-04 23:31:20 +0900

    feat : 당첨 번호 입력

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -3,6 +3,11 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import lotto.validation.InputNumValidation;
+import lotto.validation.NumValidation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
     public static long inputNum() {
@@ -12,5 +17,14 @@ public class Input {
             num = Console.readLine();
         } while (!InputNumValidation.checkInputNum(num));
         return Long.parseLong(num);
+    }
+
+    public static List<Integer> inputWinNum() {
+        String winNums;
+        do {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+            winNums = Console.readLine();
+        } while (!NumValidation.checkWinNum(winNums));
+        return Arrays.stream(winNums.split(",")).map(Integer::valueOf).collect(Collectors.toList());
     }
 }
