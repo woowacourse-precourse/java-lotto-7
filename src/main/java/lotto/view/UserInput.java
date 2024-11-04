7902f9d15c926372de49a61d7aa000564b712fa9
JEN <ekffuclxk@gmail.com>
2024-11-04 11:44:02 +0900

    feat(UserInput): 로또 구입 금액 유효성 검사 추가

--- src/main/java/lotto/view/UserInput.java
+++ src/main/java/lotto/view/UserInput.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserInput {
-    public int getPurchaseAmount(){
+    public int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        int amount = Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        int amount = Integer.parseInt(input);
+
+        isNumber(input);
+        validatePositiveAmount(amount);
+        validatePurchaseAmount(amount);
+
         return amount;
     }
 
-    public static List<Integer> inputWinNumbers(){
+    public static List<Integer> inputWinNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         String[] numbers = input.split(",");
@@ -24,8 +30,26 @@ public class UserInput {
         return winNumbers;
     }
 
-    public static int inputBonusNumber(){
+    public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static void validatePurchaseAmount(int amount) {
+        if (amount % 1000 != 1000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    private static void validatePositiveAmount(int amount) {
+        if (amount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
+        }
+    }
+
+    private static void isNumber(String input) {
+        if (input.matches("\\d+")) {
+            throw new NumberFormatException("[ERROR] 구입 금액은 숫자로 입력해야 합니다.");
+        }
     }
 }
