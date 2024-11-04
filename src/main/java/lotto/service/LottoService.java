2599cf2d412f64cff791c29d461781a4bc60cbc1
KimChanJin97 <cjkimhello97@gmail.com>
2024-11-04 20:57:38 +0900

    Feat: 로또 구매 내역 출력 서비스 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,6 +4,7 @@ import lotto.domain.Customer;
 import lotto.domain.Price;
 import lotto.exceptionHandler.ExceptionHandler;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoService {
 
@@ -23,5 +24,10 @@ public class LottoService {
 
     public void purchaseLottos() {
         customer.purchaseLottos();
+    }
+
+    public void showLottos() {
+        OutputView.lottosPurchasedMessage(customer);
+        OutputView.lottosPurchased(customer);
     }
 }
