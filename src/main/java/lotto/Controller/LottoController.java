b4e59253c8fc8a1119719f0ced2f6e0a1cf07c7c
seunghui-park <ridia1233@naver.com>
2024-11-04 22:14:19 +0900

    feat(LottoController): 로또 게임 진행 로직 구현
    - 구입 금액 입력 및 로또 번호 생성 기능 추가
    - 당첨 번호 및 보너스 번호 입력 기능 추가
    - 각 로또 티켓의 당첨 등수 계산 기능 구현
    - 수익률 계산 및 결과 출력 기능 추가

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,5 +1,43 @@
 package lotto.Controller;
 
-public class LottoController {
+import lotto.Model.Lotto;
+import lotto.Model.LottoMachine;
+import lotto.Model.LottoResult;
+import lotto.Model.Rank;
+import lotto.View.Input;
+import lotto.View.Output;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LottoController {
+    public void run() {
+        int purchaseAmount = Input.inputPurchaseAmount();
+        List<Lotto> lottos = LottoMachine.generateLottos(purchaseAmount);
+        Output.printLottos(lottos);
+
+        Set<Integer> winningNumbers = Stream.of(Input.inputWinningNumbers().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+        int bonusNumber = Input.inputBonusNumber();
+
+        List<Rank> ranks = calculateRanks(lottos, winningNumbers, bonusNumber);
+        LottoResult lottoResult = new LottoResult(ranks);
+        double profitRate = lottoResult.calculateProfitRate(purchaseAmount);
+
+        Output.printResult(ranks, profitRate);
+    }
+
+    private List<Rank> calculateRanks(List<Lotto> lottos, Set<Integer> winningNumbers, int bonusNumber) {
+        List<Rank> ranks = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            long matchCount = lotto.getNumbers().stream().filter(winningNumbers::contains).count();
+            boolean bonusMatch = lotto.getNumbers().contains(bonusNumber);
+            ranks.add(Rank.getRank((int) matchCount, bonusMatch));
+        }
+        return ranks;
+    }
 }
