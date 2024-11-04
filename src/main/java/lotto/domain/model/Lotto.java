60f8365bcc5914d2e32be41ad07fe1f32114817a
jinhyeon <tiemo0708@naver.com>
2024-11-04 22:49:53 +0900

    refactor: 요구조건에 필드(인스턴스 변수) 쓰지말라는 말이 있어서 제거 클래스 변수 이지만 혹시라도 몰라서 제거

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -8,9 +8,6 @@ import java.util.Objects;
 import java.util.Set;
 
 public class Lotto {
-    private static final int REQUIRED_NUMBER_COUNT = 6;
-    private static final int MINIMUM_NUMBER = 1;
-    private static final int MAXIMUM_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,12 +18,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != REQUIRED_NUMBER_COUNT) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_WINNING_NUMBER_COUNT);
         }
     }
     private void validateNumberRange(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(num -> num < MINIMUM_NUMBER || num > MAXIMUM_NUMBER)) {
+        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_WINNING_NUMBER_COUNT);
         }
     }
