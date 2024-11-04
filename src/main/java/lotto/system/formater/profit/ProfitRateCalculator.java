1b0c72fcc6e43ec15f5650acc0e9a7dead60e626
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-04 11:26:14 +0900

    refactor: 수익률 계산 메서드 역할 분리

--- src/main/java/lotto/system/formater/profit/ProfitRateCalculator.java
+++ src/main/java/lotto/system/formater/profit/ProfitRateCalculator.java
@@ -6,12 +6,20 @@ import lotto.system.utils.PrizeType;
 public class ProfitRateCalculator { // 수익률 계산기
 
     public static ProfitRate calculateProfitRate(Map<PrizeType, Integer> statistics, int totalPurchaseAmount) {
-        int totalPrizeMoney = statistics.entrySet().stream()
+        int totalPrizeMoney = getSumPrizeMoney(statistics);
+        double profitRateValue = getProfitRateValue(totalPurchaseAmount, totalPrizeMoney);
+
+        return new ProfitRate(profitRateValue);
+    }
+
+    private static int getSumPrizeMoney(Map<PrizeType, Integer> statistics) {
+        return statistics.entrySet().stream()
                 .mapToInt(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
                 .sum();
+    }
 
-        double profitRateValue = ((double) totalPrizeMoney / (double) totalPurchaseAmount) * 100;
-        return new ProfitRate(profitRateValue);
+    private static double getProfitRateValue(double totalPurchaseAmount, double totalPrizeMoney) {
+        return (totalPrizeMoney / totalPurchaseAmount) * 100;
     }
 
 }
