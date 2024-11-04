9bee2ef926a437f234f205bc8b8625b248df15f4
JEN <ekffuclxk@gmail.com>
2024-11-04 19:08:38 +0900

    refactor(LottoMatcher): 일치하는 번호 개수를 계산하는 메서드 분리

--- src/main/java/lotto/service/LottoMatcher.java
+++ src/main/java/lotto/service/LottoMatcher.java
@@ -8,17 +8,11 @@ import lotto.model.LottoRule;
 import lotto.model.Lottos;
 
 public class LottoMatcher {
-    public Map<LottoRule, Integer> calculateMatch(Lottos lottos, List<Integer> winNumbers, int bonusNumber) {
+    public Map<LottoRule, Integer> calculateMatchResult(Lottos lottos, List<Integer> winNumbers, int bonusNumber) {
         Map<LottoRule, Integer> matchInfo = new HashMap<>();
 
         for (Lotto lotto : lottos.getLottos()) {
-            int matchCount = 0;
-
-            for (Integer winNumber : winNumbers) {
-                if (lotto.getNumbers().contains(winNumber)) {
-                    matchCount++;
-                }
-            }
+            int matchCount = calculateMatchCount(lotto, winNumbers);
 
             boolean bonusMatch = (matchCount == 5) && lotto.getNumbers().contains(bonusNumber);
             LottoRule rule = LottoRule.getWinInfo(matchCount, bonusMatch);
@@ -26,4 +20,15 @@ public class LottoMatcher {
         }
         return matchInfo;
     }
+
+    private int calculateMatchCount(Lotto lotto, List<Integer> winNumbers) {
+        int matchCount = 0;
+        for (Integer winNumber : winNumbers) {
+            if (lotto.getNumbers().contains(winNumber)) {
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
 }
