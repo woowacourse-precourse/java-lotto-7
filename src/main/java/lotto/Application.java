5c6069eba661f05f1f1f5a495d3ab4061b74f922
minjcho <117441212+minjcho@users.noreply.github.com>
2024-11-04 22:07:45 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,16 @@
 package lotto;
 
 import lotto.InputView;
+import lotto.Lotto;
+import lotto.LottoGenerator;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         try {
             int purchaseAmount = InputView.readPurchaseAmount();
+            int lottoCount = purchaseAmount / 1000;
+            List<Lotto> lottos = LottoGenerator.generateLottos(lottoCount);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
