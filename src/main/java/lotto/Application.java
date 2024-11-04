4db3145a0a163a199a8efab47a5bbf13b5c81e90
sanghun <vitash1215@gmail.com>
2024-11-04 22:33:29 +0900

    feat(main) : main 함수 로직 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.parser.InputParser;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            InputView inputView = new InputView(new InputParser());
+            OutputView outputView = new OutputView();
+            LottoService lottoService = new LottoService();
+            LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
