0ad3ed64e62bcf07cae9d20a0e7a405fc66abde9
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-04 23:50:05 +0900

    feat: Add check price validate in Application.java

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+import static lotto.InputUtil.isClearedByNum;
+import static lotto.InputUtil.isNumeric;
+import static lotto.Lotto.LOTTO_BASIC_PRICE;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 구입 금액 입력 받고 유효성 검사
+        int purchasesCount;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.\n");
+                String price = Console.readLine();
+
+                isNumeric(price);
+                isClearedByNum(price, LOTTO_BASIC_PRICE); // 구입금액이 1000원 단위인지 확인
+
+                purchasesCount = Integer.parseInt(price) / LOTTO_BASIC_PRICE;
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("잘못된 입력입니다. 다시 입력해 주세요\n 예외 메세지: " + e.getMessage());
+            }
+        }
+
+
     }
+
+
 }
