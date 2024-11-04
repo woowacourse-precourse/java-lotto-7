5d179e031c0bd9afd86027c71bdccbe91ec3bcc5
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-04 18:51:52 +0900

    feat: 유효성 검증 후 parse 하도록 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.LottoRound;
+import lotto.validator.InputValidator;
+import lotto.validator.LottoValidator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,8 +11,8 @@ import java.util.List;
 public class InputView {
 
     static final String INPUT_ORDER_PRICE = "구입금액을 입력해 주세요.";
-    static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
     static final int LOTTO_PRICE = 1000;
 
     public static String inputOrderPrice() {
@@ -28,18 +31,24 @@ public class InputView {
     }
 
     public static int parseOrder(String inputOrderPrice){
-        // Valid 추가 ( 1000원 단위 )
-        return Integer.parseInt(inputOrderPrice)/LOTTO_PRICE;
+        int orderPrice = Integer.parseInt(inputOrderPrice) ;
+        InputValidator.validateOrderPriceUnit(orderPrice);
+        InputValidator.validateOrderPricePositive(orderPrice);
+        return orderPrice / LOTTO_PRICE;
     }
 
     public static List<Integer> parseWinningNumber(String inputWinningNumber){
-        return Arrays.stream(inputWinningNumber.split(","))
+        List<Integer> numbers = Arrays.stream(inputWinningNumber.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .toList();
+        LottoValidator.validateLottoNumbers(numbers);
+        return numbers;
     }
 
-    public static int parseBonusNumber(String inputBonusNumber) {
-        return Integer.parseInt(inputBonusNumber);
+    public static int parseBonusNumber(String inputBonusNumber, LottoRound lottoRound) {
+        int bonusNumber = Integer.parseInt(inputBonusNumber);
+        LottoValidator.validateBonusNumber(bonusNumber, lottoRound.getWinningLotto());
+        return bonusNumber;
     }
 }
