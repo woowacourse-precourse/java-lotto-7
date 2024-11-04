ece2859e55742312b552bf17d0446201c1018082
YunCheol1 <naro2727@naver.com>
2024-11-04 13:25:42 +0900

    fix(WinningLotto):클래스 이름과 로직 수정 -당첨로또 클래스의 이름과 메서드추가 및 수정

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,28 @@
+package lotto.model;
+
+import java.util.List;
+
+public class WinningLotto {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningLotto = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+        validateBonusNumber();
+    }
+
+    private void validateBonusNumber() {
+        if (winningLotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.");
+        }
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
