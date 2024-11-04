b855e3389a69b5ca0110b477eecef6e81fda08de
sonshn <sonshn@naver.com>
2024-11-04 21:11:56 +0900

    feat(LottoController): 로또 복권 생성 함수 호출 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,23 @@
 package lotto.controller;
 
+import lotto.service.LottoService;
 import lotto.view.InputView;
 
 public class LottoController {
 
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
     public void run() {
         String purchaseAmount = InputView.getPurchaseAmount();
+        lottoService.calculateNumberOfLotteryTickets(purchaseAmount);
         String winningNumbers = InputView.getLotteryWinningNumbers();
         String lotteryNumber = InputView.getLotteryBonusNumber();
 
         InputView.closeConsole();
     }
-    
+
 }
