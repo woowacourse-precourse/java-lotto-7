2638cd70b6c6ac439a7a4f1efaa169c7f9d44939
이윤빈 <kanado20000@naver.com>
2024-11-04 11:30:54 +0900

    :art: refactor: 포맷팅 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,8 +1,10 @@
 package lotto;
+
 import lotto.controller.LottoMachineController;
+
 public class Application {
-    public static void main(String[] args) {
-        lotto.controller.LottoMachineFactory factory = new lotto.controller.LottoMachineFactory();
-        new LottoMachineController(factory).runLottoMachine();
-    }
+  public static void main(String[] args) {
+    lotto.controller.LottoMachineFactory factory = new lotto.controller.LottoMachineFactory();
+    new LottoMachineController(factory).runLottoMachine();
+  }
 }
