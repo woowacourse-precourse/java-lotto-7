005464c74d22277c820c05dd1fe6188aaaf260c1
GyuHo123 <kyuhokim12@gmail.com>
2024-11-04 21:21:47 +0900

    feat(InputView): SRP에 맞게 클래스의 역할 재조정
    -Validator는 검증만 하고 이후 예외 메시지 출력과 재입력은 InputView에서 처리하도록 책임 분리함.

--- src/main/java/lotto/validator/LottoAmountValidator.java
+++ src/main/java/lotto/validator/LottoAmountValidator.java
@@ -1,73 +1,31 @@
 package lotto.validator;
 
 import lotto.message.ErrorMessage;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class LottoAmountValidator {
     public static int validatePurchaseAmout(String lottoAmount) {
         inputNullOrEmpty(lottoAmount);
         int lottoRound = convertPurchaseAmountToInt(lottoAmount);
         if (lottoRound % 1000 != 0) {
-            handleAmountNotDivisibleBy1000();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
         }
         if (lottoRound < 1000) {
-            handleAmountLessThan1000();
+            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_NEGATIVE_OR_ZERO.getMessage());
         }
         return lottoRound / 1000;
     }
 
-    private static void handleAmountNotDivisibleBy1000() {
+    private static int convertPurchaseAmountToInt(String lottoAmount) {
         try {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.buyLotto();
-        }
-    }
-
-    private static void handleAmountLessThan1000() {
-        try {
-            throw new IllegalArgumentException(ErrorMessage.PURCHASE_AMOUNT_NEGATIVE_OR_ZERO.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.buyLotto();
-        }
-    }
-
-    private static int convertPurchaseAmountToInt(String lottoInteger) {
-        int lottoRound = 0;
-        try {
-            lottoRound = Integer.parseInt(lottoInteger);
+            return Integer.parseInt(lottoAmount);
         } catch (NumberFormatException e) {
-            handleNumberFormatExecption();
-        }
-        return lottoRound;
-    }
-
-    private static void handleNumberFormatExecption(){
-        try {
             throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER.getMessage());
         }
-        catch(IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.buyLotto();
-        }
     }
-
 
     public static void inputNullOrEmpty(String input) {
         if (input == null || input.isBlank()) {
-            handleNullOrEmpty();
-        }
-    }
-
-    public static void handleNullOrEmpty() {
-        try {
             throw new IllegalArgumentException(ErrorMessage.INPUT_NULL_OR_EMPTY.getMessage());
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            InputView.buyLotto();
         }
     }
 }
