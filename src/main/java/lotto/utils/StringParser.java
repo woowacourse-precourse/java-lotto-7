1ee71c68193d6ce9bec12c318571f60c566f0417
88dldl <chltnrgml0999@gmail.com>
2024-11-04 15:58:03 +0900

    feat(StringParser, Validation): 입력 값 파싱 및 유효성 검증 기능 추가

--- src/main/java/lotto/utils/StringParser.java
+++ src/main/java/lotto/utils/StringParser.java
@@ -1,11 +1,22 @@
 package lotto.utils;
 
 import lotto.error.ErrorStatus;
+import lotto.validation.Validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringParser {
+    private static final String DELIMITER = ",";
+    private static final String GAP = " ";
+    private static final String START_POINT = "[";
+    private static final String END_POINT = "]";
 
     public static int parseStringToInt(String input) {
+        Validation.validateNotNull(input);
         int parsedInput = parseInteger(input);
+        Validation.validatePositiveInteger(parsedInput);
         return parsedInput;
     }
 
@@ -17,4 +28,32 @@ public class StringParser {
         }
     }
 
+    public static List<Integer> parseStringToIntegerList(String input) {
+        String[] inputArray = splitInputString(input);
+        Validation.validateNumberSize(inputArray.length);
+
+        List<Integer> parsedInput = convertToIntegerList(inputArray);
+
+        Validation.validatePositiveIntegerList(parsedInput);
+        Validation.validateDuplicateNumbers(parsedInput);
+        return convertToIntegerList(inputArray);
+    }
+
+    private static String[] splitInputString(String input) {
+        return input.split(DELIMITER);
+    }
+
+    private static List<Integer> convertToIntegerList(String[] inputArray) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : inputArray) {
+            result.add(parseInteger(s));
+        }
+        return result;
+    }
+
+    public static String formatLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(DELIMITER + GAP, START_POINT, END_POINT));
+    }
 }
--- src/main/java/lotto/validation/Validation.java
+++ src/main/java/lotto/validation/Validation.java
@@ -8,6 +8,25 @@ import java.util.Set;
 
 public class Validation {
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
+    public static void validateNotNull(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(ErrorStatus.NULL_OR_EMPTY_INPUT.getMessage());
+        }
+    }
+
+    public static void validatePositiveInteger(int input) {
+        if (input < 0) {
+            throw new IllegalArgumentException(ErrorStatus.INVALID_NEGATIVE_NUMBER.getMessage());
+        }
+    }
+
+    public static void validatePositiveIntegerList(List<Integer> inputs) {
+        for (Integer input : inputs) {
+            validatePositiveInteger(input);
+        }
+    }
 
     public static void validateMoneyAmount(int input) {
         if (input % LOTTO_PRICE != 0) {
@@ -22,6 +41,18 @@ public class Validation {
             if (!uniqueNumbers.add(number)) {
                 throw new IllegalArgumentException(ErrorStatus.DUPLICATE_NUMBER.getMessage());
             }
+        }
+    }
+
+    public static void validateNumberSize(Integer size) {
+        if (size != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(ErrorStatus.INVALID_NUMBER_SIZE.getMessage());
+        }
+    }
+
+    public static void validateDuplicateNumbersWithBonusNumber(List<Integer> winningNumbers, Integer input) {
+        if (winningNumbers.contains(input)) {
+            throw new IllegalArgumentException(ErrorStatus.DUPLICATE_NUMBER.getMessage());
         }
     }
 }
