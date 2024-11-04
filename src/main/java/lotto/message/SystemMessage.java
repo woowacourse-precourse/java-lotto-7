abc60b4b0514f4aab42585685ad3a9582875ff88
momoysm <90163578+momoysm@users.noreply.github.com>
2024-11-04 18:39:39 +0900

    fix : 로또 구매 문구 수정

--- src/main/java/lotto/message/SystemMessage.java
+++ src/main/java/lotto/message/SystemMessage.java
@@ -5,7 +5,7 @@ public enum SystemMessage implements MessageProvider {
     MESSAGE_INPUT_PURCHASE_AMOUNT("구매금액을 입력해 주세요."),
     MESSAGE_INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     MESSAGE_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    MESSAGE_OUTPUT_PURCHASE_LOTTO_QUANTITY("개를 구매하였습니다."),
+    MESSAGE_OUTPUT_PURCHASE_LOTTO_QUANTITY("개를 구매했습니다."),
     MESSAGE_OUTPUT_RESULT("당첨 통계"),
     MESSAGE_FIRST_RESULT("6개 일치"),
     MESSAGE_SECOND_RESULT("5개 일치, 보너스 볼 일치"),
