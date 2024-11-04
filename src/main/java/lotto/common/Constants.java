76b4f1ea14c909b5aaf767642b4cce059e72b4c7
023-dev <0_2_3@naver.com>
2024-11-04 17:03:46 +0900

    feat(Constrants): 공통 상수 관리

---
+++ src/main/java/lotto/common/Constants.java
@@ -0,0 +1,8 @@
+package lotto.common;
+
+public class Constants {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBERS_COUNT = 6;
+}
