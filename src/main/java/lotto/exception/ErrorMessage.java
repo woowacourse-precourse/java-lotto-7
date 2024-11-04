21cc1178a067c470f26b540f237289909b91eeee
Minseok <saltishDev@gmail.com>
2024-11-04 15:31:19 +0900

    feat: 중복된 로또 번호가 생성된 경우 예외 처리

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -11,7 +11,8 @@ public enum ErrorMessage {
     INVALID_BONUS_NUMBER("보너스 번호는 1 이상, 45 이하의 정수 값이어야 합니다."),
     DUPLICATED_BONUS_NUMBER("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
-    LOTTO_NUMBER_OUT_OF_RANGE("당첨 번호는 1 이상, 45 이하의 정수 값이어야 합니다.");
+    LOTTO_NUMBER_OUT_OF_RANGE("당첨 번호는 1 이상, 45 이하의 정수 값이어야 합니다."),
+    DUPLICATED_LOTTO_NUMBERS("로또 번호는 중복될 수 없습니다.");
 
     private final String message;
 
--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,7 +3,9 @@ package lotto.model;
 import lotto.exception.ErrorMessage;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_MIN_NUMBER = 1;
@@ -21,12 +23,20 @@ public class Lotto {
         if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_COUNT.getErrorMessage());
         }
+        validateUniqueLottoNumbers(numbers);
         numbers.forEach(this::validateLottoNumber);
     }
 
     private void validateLottoNumber(Integer lottoNumber) {
         if (lottoNumber < LOTTO_MIN_NUMBER || lottoNumber > LOTTO_MAX_NUMBER) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE.getErrorMessage());
+        }
+    }
+
+    private void validateUniqueLottoNumbers(List<Integer> lottoNumbers) {
+        Set<Integer> uniqueLottoNumbers = new HashSet<>(lottoNumbers);
+        if (uniqueLottoNumbers.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBERS.getErrorMessage());
         }
     }
 
