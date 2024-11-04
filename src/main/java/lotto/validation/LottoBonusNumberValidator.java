92119a23641803875fccdd3d2ccca7428bbd1eff
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-04 17:29:08 +0900

    refactor : 로또 번호 유효한 범위 수정

--- src/main/java/lotto/validation/LottoBonusNumberValidator.java
+++ src/main/java/lotto/validation/LottoBonusNumberValidator.java
@@ -6,7 +6,7 @@ public class LottoBonusNumberValidator implements Validator<Integer>{
 
     @Override
     public void validate(Integer bonusNumber) {
-        if (bonusNumber <= START_LOTTO_NUMBER || FINISH_LOTTO_NUMBER <= bonusNumber) {
+        if (bonusNumber < START_LOTTO_NUMBER || FINISH_LOTTO_NUMBER < bonusNumber) {
             throw new IllegalArgumentException(String.format("보너스 번호는 %d부터 %d 사이의 숫자여야 합니다.", START_LOTTO_NUMBER, FINISH_LOTTO_NUMBER));
         }
     }
