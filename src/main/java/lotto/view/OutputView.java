a0dcc6d62120080c4890191f5b5d7f4b59ff0295
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-11-04 18:20:40 +0900

    feat(OutputView): 당첨 결과 출력 메서드 구현
    - 출력 결과에 필요한 메세지를 상수로 관리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,24 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
+import lotto.domain.Prize;
+import lotto.domain.WinningResult;
 
 public class OutputView {
 
+    private static final String PURCHASE_QUANTITY_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String STATISTICS_HEADER_MESSAGE = "당첨 통계\n---";
+    private static final String PRIZE_FORMAT_MESSAGE = "%d개 일치%s (%,d원) - %d개\n";
+    private static final String BONUS_MATCH_MESSAGE = ", 보너스 볼 일치";
+    private static final String PROFIT_RATE_FORMAT_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
     public static void printPurchaseQuantity(final int quantity) {
-        System.out.println(quantity + "개를 구매했습니다.");
+        System.out.printf(PURCHASE_QUANTITY_MESSAGE, quantity);
     }
 
     public static void printLottos(final List<Lotto> lottos) {
@@ -22,5 +32,46 @@ public class OutputView {
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ", "[", "]"));
         System.out.println(result);
+    }
+
+    public static void printWinningResult(final WinningResult result) {
+        printStatisticsHeader();
+        printPrizeResults(result);
+        printProfitRate(result);
+    }
+
+    private static void printStatisticsHeader() {
+        System.out.println(STATISTICS_HEADER_MESSAGE);
+    }
+
+    private static void printPrizeResults(final WinningResult result) {
+        getPrizesByAscendingCount().forEach(prize ->
+                printPrizeResult(prize, result));
+    }
+
+    private static List<Prize> getPrizesByAscendingCount() {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize != Prize.MISS)
+                .sorted(Comparator.comparing(Prize::getMatchCount))
+                .toList();
+    }
+
+    private static void printPrizeResult(Prize prize, WinningResult result) {
+        System.out.printf(PRIZE_FORMAT_MESSAGE,
+                prize.getMatchCount(),
+                getBonusMatchText(prize),
+                prize.getAmount(),
+                result.countPrize(prize));
+    }
+
+    private static String getBonusMatchText(Prize prize) {
+        if (prize.getNeedBonusMatch()) {
+            return BONUS_MATCH_MESSAGE;
+        }
+        return "";
+    }
+
+    private static void printProfitRate(final WinningResult result) {
+        System.out.printf(PROFIT_RATE_FORMAT_MESSAGE, result.getProfitRate());
     }
 }
