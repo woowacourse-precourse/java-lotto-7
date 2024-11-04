3804fe4f3597808b53365aed5f9545ade19f47fa
YunCheol1 <naro2727@naver.com>
2024-11-04 15:51:06 +0900

    feat(Config): 상수화한 에러메시지들을 저장하는 클래스 생성

--- src/main/java/lotto/util/Config.java
+++ src/main/java/lotto/util/Config.java
@@ -1,4 +1,12 @@
 package lotto.util;
 
 public class Config {
+    public static final String ERROR_INVALID_MONEY_FORMAT = "[ERROR] 숫자 형식의 금액을 입력해 주세요.";
+    public static final String ERROR_INVALID_TICKET_COUNT = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
+    public static final String ERROR_INVALID_NUMBER_FORMAT = "[ERROR] 숫자 형식의 번호를 입력해 주세요.";
+    public static final String ERROR_DUPLICATE_NUMBER = "[ERROR] 중복된 번호는 허용되지 않습니다.";
+    public static final String ERROR_OUT_OF_RANGE_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String ERROR_BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+    public static final String ERROR_INVALID_WINNING_NUMBER_COUNT = "[ERROR] 6개의 숫자를 쉼표로 구분하여 입력해 주세요.";
+
 }
