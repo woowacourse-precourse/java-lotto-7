f8618fb130ce996ef5466a8099d075c5c3fda22c
masee1994 <masee1994@gmail.com>
2024-11-04 16:43:16 +0900

    feat: 로또 번호 발행 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,22 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
-        int n = money/1000;
+        int n = money / 1000;
 
+        Lotto lotto = new Lotto();
+        ArrayList<Lotto> lottos = new ArrayList<>(lotto.number_generator(n));
+        System.out.println("\n" + n + "개를 구매했습니다.");
+        for (Lotto arr : lottos) {
+            lotto.number_print(arr);
+        }
     }
+
 }
+
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +19,28 @@ public class Lotto {
         }
     }
 
+
     // TODO: 추가 기능 구현
+
+    public Lotto() {
+        this.numbers = new ArrayList<>();
+    }
+
+    public List<Lotto> number_generator(int n) {
+        List<Lotto> lottos = new ArrayList<>();
+        while (n-- > 0) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    public void number_print(Lotto lotto) {
+        String message = "[";
+        for (int n : lotto.numbers) {
+            message += n + ", ";
+        }
+        message = message.substring(0, message.length() - 2) + "]";
+        System.out.println(message);
+    }
 }
