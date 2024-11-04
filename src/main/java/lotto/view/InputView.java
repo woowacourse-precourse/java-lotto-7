1ee112d58712aa1c89f35e836363051c2c1c17b3
hooniverse <sung010326@gmail.com>
2024-11-04 23:24:22 +0900

    feat: 1,000원 단위로 입력받게 예외처리 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,8 +9,20 @@ public class InputView {
     private static final String BONUS_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
     public int getLottoCost() {
-        System.out.println(PURCHASE_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+        while (true) {
+            try {
+                System.out.println(PURCHASE_MESSAGE);
+                int cost = Integer.parseInt(Console.readLine());
+
+                if (cost % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 천원 단위로 입력하세요");
+                }
+
+                return cost; // 올바른 값이면 반환
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // 에러 메시지 출력
+            }
+        }
     }
 
     public String getWinningLotto() {
