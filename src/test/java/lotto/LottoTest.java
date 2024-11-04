14d98d59542740dd9645125cf4a90252b7ddb9b9
changyunyeong <sally0109277@naver.com>
2024-11-04 23:10:17 +0900

    feat: 범위 예외 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -21,5 +21,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 0 이하의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_0이하의_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 45 이상의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_45이상의_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
