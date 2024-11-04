140737dab745b6c1cdd905d72feeb8baeb72a057
Chanwoo yang <moomsss@naver.com>
2024-11-04 16:36:25 +0900

    refactor: LottoManager 테스트 주석 추가

--- src/test/java/lotto/service/LottoManagerTest.java
+++ src/test/java/lotto/service/LottoManagerTest.java
@@ -22,16 +22,16 @@ class LottoManagerTest {
     @Test
     void buyLottos() {
         int count = 10;
-        lottoManager.buyLottos(count);
+        lottoManager.buyLottos(count); //로또를 열개 샀을때
 
-        assertThat(lottoManager.getLottos()).hasSize(10);
+        assertThat(lottoManager.getLottos()).hasSize(10); //사이즈가 10이 맞는지 확인
     }
 
     @Test
     void generateLottoWithNumbers() {
-        List<Integer> numbers = new ArrayList<>(List.of(1,2,3,4,5,6));
+        List<Integer> numbers = new ArrayList<>(List.of(1,2,3,4,5,6)); //1,2,3,4,5,6으로 로또를 샀을떄
         Lotto lotto = lottoManager.generateLottoWithNumbers(numbers);
 
-        assertThat(lotto.getNumbers()).isEqualTo(numbers);
+        assertThat(lotto.getNumbers()).isEqualTo(numbers); //구매한 로또와 번호가 같은지 확인
     }
 }
