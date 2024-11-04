c4af722db8863ee1bd50635c2c24f77632f14dfe
lee-haeseung <lhser123@cau.ac.kr>
2024-11-04 18:34:13 +0900

    feat: 미당첨 추가

--- src/main/java/lotto/entity/prize.java
+++ src/main/java/lotto/entity/prize.java
@@ -1,17 +1,18 @@
 package lotto.entity;
 
-public enum prize {
+public enum Prize {
 
     FIRST("6개 일치 (2,000,000,000원)", 2000000000),
     SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000),
     THIRD("5개 일치 (1,500,000원)", 1500000),
     FOURTH("4개 일치 (50,000원)", 50000),
-    FIFTH("3개 일치 (5,000원)", 5000);
+    FIFTH("3개 일치 (5,000원)", 5000),
+    NO_PRIZE("미당첨 (0원)", 0);
 
     private final String description;
     private final int amount;
 
-    prize(String description, int amount) {
+    Prize(String description, int amount) {
         this.description = description;
         this.amount = amount;
     }
