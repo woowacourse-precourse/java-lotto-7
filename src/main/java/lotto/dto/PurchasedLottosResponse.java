16f6a3cbd60039ba8a70bba26f88f873a37d57ec
jae woo Jung <awfjol2008@gmail.com>
2024-11-05 17:52:38 +0900

    feat: 구매한 로또 목록을 반환하는 PurchasedLottosResponse DTO 구현

---
+++ src/main/java/lotto/dto/PurchasedLottosResponse.java
@@ -0,0 +1,15 @@
+package lotto.dto;
+
+import lotto.domain.lotto.Lotto;
+
+import java.util.List;
+
+public record PurchasedLottosResponse(List<LottoResponse> lottoResponses) {
+    public static PurchasedLottosResponse from(List<Lotto> lottos) {
+        List<LottoResponse> lottoResponses = lottos.stream()
+                .map(LottoResponse::new)
+                .toList();
+
+        return new PurchasedLottosResponse(lottoResponses);
+    }
+}
