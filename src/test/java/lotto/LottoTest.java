9dae9adcefa1432a04ffd8c5d10af8a2a96ca3b6
KANG JISEOK <rkdwltjr5876@naver.com>
2024-11-05 11:02:56 +0900

    test(Lotto) : 로또 번호의 개수가 6개보다 적으면 예외가 발생한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,11 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +24,15 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+
 }
