145e0102a89b6bdc260441a59ba02aa83edecb35
min429 <lsm990429@gmail.com>
2024-11-08 21:02:45 +0900

    refactor: 중복 코드를 별도의 메소드로 추출한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,14 @@ import static camp.nextstep.edu.missionutils.Console.*;
 import static lotto.validation.LottoValidation.*;
 
 import java.util.List;
+import java.util.function.Function;
+
+import lotto.validation.LottoValidation;
 
 public class InputView {
 
 	public static int getPurchaseAmount() {
-		while (true) {
-			try {
-				String input = readLine();
-				return parseAmount(input);
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			}
-		}
+		return getInput(LottoValidation::parseAmount);
 	}
 
 	public static List<Integer> getWinningNumbers() {
@@ -24,10 +20,14 @@ public class InputView {
 	}
 
 	public static int getBonusNumber(List<Integer> winningNumbers) {
+		return getInput(input -> parseBonusNumber(input, winningNumbers));
+	}
+
+	private static <T> T getInput(Function<String, T> parser) {
 		while (true) {
 			try {
 				String input = readLine();
-				return parseBonusNumber(input, winningNumbers);
+				return parser.apply(input);
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 			}
